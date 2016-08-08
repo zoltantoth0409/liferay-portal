c77e422430c1dc27b55dd26e45fb147f2f6fae32
Juergen Kappler <jurgen.kappler@liferay.com>
2016-08-08 17:56:22 +0200

    LPS-67509 Encode layout friendly URL

--- portal-kernel/src/com/liferay/portal/kernel/util/FriendlyURLNormalizerUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/FriendlyURLNormalizerUtil.java
@@ -41,6 +41,10 @@ public class FriendlyURLNormalizerUtil {
 			friendlyURL, friendlyURLPattern);
 	}
 
+	public static String normalizeWithEncoding(String friendlyURL) {
+		return getFriendlyURLNormalizer().normalizeWithEncoding(friendlyURL);
+	}
+
 	public static String normalizeWithPeriodsAndSlashes(String friendlyURL) {
 		return getFriendlyURLNormalizer().normalizeWithPeriodsAndSlashes(
 			friendlyURL);
