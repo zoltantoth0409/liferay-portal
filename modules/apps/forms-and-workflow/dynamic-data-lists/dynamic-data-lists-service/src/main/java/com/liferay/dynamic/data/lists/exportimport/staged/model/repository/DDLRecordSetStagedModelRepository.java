bfa6be29b16cced4d67538d84524feb522a005b3
hai.yu <hai.yu@liferay.com>
2017-02-22 15:23:58 +0800

    LPS-70790 Set recordSetKey for null when import using "Copy as New" stategy so that it can generated new recordSetKey.

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/exportimport/staged/model/repository/DDLRecordSetStagedModelRepository.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/exportimport/staged/model/repository/DDLRecordSetStagedModelRepository.java
@@ -69,6 +69,9 @@ public class DDLRecordSetStagedModelRepository
 		if (portletDataContext.isDataStrategyMirror()) {
 			serviceContext.setUuid(ddlRecordSet.getUuid());
 		}
+		else {
+			ddlRecordSet.setRecordSetKey(null);
+		}
 
 		return _ddlRecordSetLocalService.addRecordSet(
 			userId, ddlRecordSet.getGroupId(), ddlRecordSet.getDDMStructureId(),
