f831755652ce20112f86d3ee21c9fb2a5e1064b9
Alessio Rendina <asilcomando@gmail.com>
2017-04-03 15:26:00 -0700

    COMMERCE-2 Added Webkeys

--- modules/dxp/apps/commerce/commerce-product-web/src/main/java/com/liferay/commerce/product/web/internal/constants/CommerceProductWebKeys.java
+++ modules/dxp/apps/commerce/commerce-product-web/src/main/java/com/liferay/commerce/product/web/internal/constants/CommerceProductWebKeys.java
@@ -1,42 +1,25 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
 
-package com.liferay.commerce.product.exception;
-
-import aQute.bnd.annotation.ProviderType;
-
-import com.liferay.portal.kernel.exception.NoSuchModelException;
+package com.liferay.commerce.product.web.internal.constants;
 
 /**
- * @author Marco Leo
+ * @author Alessio Antionio Rendina
  */
-@ProviderType
-public class NoSuchProductInstanceException extends NoSuchModelException {
+public class CommerceProductWebKeys {
 
-	public NoSuchProductInstanceException() {
-	}
-
-	public NoSuchProductInstanceException(String msg) {
-		super(msg);
-	}
-
-	public NoSuchProductInstanceException(String msg, Throwable cause) {
-		super(msg, cause);
-	}
-
-	public NoSuchProductInstanceException(Throwable cause) {
-		super(cause);
-	}
+	public static final String COMMERCE_PRODUCT_DEFINITION =
+		"COMMERCE_PRODUCT_DEFINITION";
 
 }
