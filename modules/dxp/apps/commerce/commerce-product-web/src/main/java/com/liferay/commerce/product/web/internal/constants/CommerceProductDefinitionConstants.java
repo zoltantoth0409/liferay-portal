4b55de9794de02a4908383f52c055298e8971579
Alessio Rendina <asilcomando@gmail.com>
2017-04-04 17:13:28 -0700

    COMMERCE-2 Add commerce product definition constants

---
+++ modules/dxp/apps/commerce/commerce-product-web/src/main/java/com/liferay/commerce/product/web/internal/constants/CommerceProductDefinitionConstants.java
@@ -0,0 +1,10 @@
+package com.liferay.commerce.product.web.internal.constants;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceProductDefinitionConstants {
+
+	public static final long DEFAULT_COMMERCE_PRODUCT_DEFINITION_ID = 0;
+
+}
