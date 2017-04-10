8882201b6c7c98db7bfdd3ac3af22159810b062d
Marco Leo <marco.leo@liferay.com>
2017-04-09 23:34:25 -0700

    COMMERCE-2 Implement Simple product type

--- modules/dxp/apps/commerce/commerce-product-type-simple/src/main/java/com/liferay/commerce/product/type/simple/internal/SimpleCommerceProductType.java
+++ modules/dxp/apps/commerce/commerce-product-type-simple/src/main/java/com/liferay/commerce/product/type/simple/internal/SimpleCommerceProductType.java
@@ -1,21 +1,36 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
 
-package com.liferay.commerce.product.internal.constants;
+package com.liferay.commerce.product.type.simple.internal;
+
+import com.liferay.commerce.product.type.CommerceProductType;
+import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Andrea Di Giorgi
+ * @author Marco Leo
  */
-public class CommerceProductConstants {
+@Component(
+	immediate = true,
+	property = {
+		"commerce.product.type.display.order:Integer=5",
+		"commerce.product.type.name=simple"
+	},
+	service = CommerceProductType.class
+)
+public class SimpleCommerceProductType implements  CommerceProductType {
+	@Override
+	public String getName() {
+		return "simple";
+	}
 }
