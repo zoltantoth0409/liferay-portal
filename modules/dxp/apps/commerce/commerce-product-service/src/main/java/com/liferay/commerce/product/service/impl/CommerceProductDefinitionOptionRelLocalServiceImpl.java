c0b3c6f7885a898200bc1cd5d94be094ed8bcf0a
Marco Leo <marco.leo@liferay.com>
2017-04-10 15:01:11 -0700

    COMMERCE-2 Add product definition option value rel CRUD methods

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductDefinitionOptionRelLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductDefinitionOptionRelLocalServiceImpl.java
@@ -14,11 +14,129 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.model.CommerceProductDefinitionOptionRel;
 import com.liferay.commerce.product.service.base.CommerceProductDefinitionOptionRelLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
  */
 public class CommerceProductDefinitionOptionRelLocalServiceImpl
 	extends CommerceProductDefinitionOptionRelLocalServiceBaseImpl {
+
+	public CommerceProductDefinitionOptionRel
+			addCommerceProductDefinitionOptionRel(
+				long commerceProductDefinitionId, long commerceProductOptionId,
+				Map<Locale, String> nameMap, Map<Locale, String> descriptionMap,
+				String ddmFormFieldTypeName, int priority,
+				ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		long commerceProductDefinitionOptionRelId =
+			counterLocalService.increment();
+
+		CommerceProductDefinitionOptionRel commerceProductDefinitionOptionRel =
+			commerceProductDefinitionOptionRelPersistence.create(
+				commerceProductDefinitionOptionRelId);
+
+		commerceProductDefinitionOptionRel.setGroupId(groupId);
+		commerceProductDefinitionOptionRel.setCompanyId(user.getCompanyId());
+		commerceProductDefinitionOptionRel.setUserId(user.getUserId());
+		commerceProductDefinitionOptionRel.setUserName(user.getFullName());
+		commerceProductDefinitionOptionRel.setCommerceProductDefinitionId(
+			commerceProductDefinitionId);
+		commerceProductDefinitionOptionRel.setCommerceProductOptionId(
+			commerceProductOptionId);
+		commerceProductDefinitionOptionRel.setPriority(priority);
+		commerceProductDefinitionOptionRel.setNameMap(nameMap);
+		commerceProductDefinitionOptionRel.setDescriptionMap(descriptionMap);
+		commerceProductDefinitionOptionRel.setDDMFormFieldTypeName(
+			ddmFormFieldTypeName);
+		commerceProductDefinitionOptionRel.setExpandoBridgeAttributes(
+			serviceContext);
+
+		commerceProductDefinitionOptionRelPersistence.update(
+			commerceProductDefinitionOptionRel);
+
+		return commerceProductDefinitionOptionRel;
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CommerceProductDefinitionOptionRel
+			deleteCommerceProductDefinitionOptionRel(
+				CommerceProductDefinitionOptionRel
+					commerceProductDefinitionOptionRel)
+		throws PortalException {
+
+		// Product product definition option rel
+
+		commerceProductDefinitionOptionRelPersistence.remove(
+			commerceProductDefinitionOptionRel);
+
+		// Expando
+
+		expandoRowLocalService.deleteRows(
+			commerceProductDefinitionOptionRel.
+				getCommerceProductDefinitionOptionRelId());
+
+		return commerceProductDefinitionOptionRel;
+	}
+
+	@Override
+	public CommerceProductDefinitionOptionRel
+			deleteCommerceProductDefinitionOptionRel(
+				long commerceProductDefinitionOptionRelId)
+		throws PortalException {
+
+		CommerceProductDefinitionOptionRel commerceProductDefinitionOptionRel =
+			commerceProductDefinitionOptionRelPersistence.findByPrimaryKey(
+				commerceProductDefinitionOptionRelId);
+
+		return commerceProductDefinitionOptionRelLocalService.
+			deleteCommerceProductDefinitionOptionRel(
+				commerceProductDefinitionOptionRel);
+	}
+
+	public CommerceProductDefinitionOptionRel
+			updateCommerceProductDefinitionOptionRel(
+				long commerceProductDefinitionOptionRelId,
+				long commerceProductOptionId, Map<Locale, String> nameMap,
+				Map<Locale, String> descriptionMap, String ddmFormFieldTypeName,
+				int priority, ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		CommerceProductDefinitionOptionRel commerceProductDefinitionOptionRel =
+			commerceProductDefinitionOptionRelPersistence.findByPrimaryKey(
+				commerceProductDefinitionOptionRelId);
+
+		commerceProductDefinitionOptionRel.setCommerceProductOptionId(
+			commerceProductOptionId);
+		commerceProductDefinitionOptionRel.setPriority(priority);
+		commerceProductDefinitionOptionRel.setNameMap(nameMap);
+		commerceProductDefinitionOptionRel.setDescriptionMap(descriptionMap);
+		commerceProductDefinitionOptionRel.setDDMFormFieldTypeName(
+			ddmFormFieldTypeName);
+		commerceProductDefinitionOptionRel.setExpandoBridgeAttributes(
+			serviceContext);
+
+		commerceProductDefinitionOptionRelPersistence.update(
+			commerceProductDefinitionOptionRel);
+
+		return commerceProductDefinitionOptionRel;
+	}
+
 }
