0b8189b3cfccd5cc415808ea0096c028992f96ef
Marco Leo <marco.leo@liferay.com>
2017-04-09 17:24:14 -0700

    COMMERCE-2 New web key

--- modules/dxp/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/constants/CommerceProductWebKeys.java
+++ modules/dxp/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/constants/CommerceProductWebKeys.java
@@ -25,4 +25,9 @@ public class CommerceProductWebKeys {
 	public static final String COMMERCE_PRODUCT_DEFINITIONS =
 		"COMMERCE_PRODUCT_DEFINITIONS";
 
+	public static final String COMMERCE_PRODUCT_OPTION =
+		"COMMERCE_PRODUCT_OPTION";
+
+	public static final String COMMERCE_PRODUCT_OPTIONS =
+		"COMMERCE_PRODUCT_OPTIONS";
 }
