6d427bdac97d967e53ed3dc828251ea0c1534d8a
Marco Leo <marco.leo@liferay.com>
2017-04-12 16:55:35 -0700

    COMMERCE-2 Add option value edit capability

---
+++ modules/dxp/apps/commerce/commerce-product-options-web/src/main/java/com/liferay/commerce/product/options/web/internal/portlet/action/EditCommerceProductOptionValueMVCRenderCommand.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.options.web.internal.portlet.action;
+
+import com.liferay.commerce.product.constants.CommerceProductPortletKeys;
+import com.liferay.commerce.product.constants.CommerceProductWebKeys;
+import com.liferay.commerce.product.exception.NoSuchProductOptionValueException;
+import com.liferay.commerce.product.model.CommerceProductOptionValue;
+import com.liferay.commerce.product.service.CommerceProductOptionValueLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CommerceProductPortletKeys.COMMERCE_PRODUCT_OPTIONS,
+		"mvc.command.name=editProductOptionValue"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditCommerceProductOptionValueMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			setCommerceProductOptionValueRequestAttribute(renderRequest);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchProductOptionValueException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return "/error.jsp";
+			}
+			else {
+				throw new PortletException(e);
+			}
+		}
+
+		return "/edit_product_option_value.jsp";
+	}
+
+	protected void setCommerceProductOptionValueRequestAttribute(
+			RenderRequest renderRequest)
+		throws PortalException {
+
+		long commerceProductOptionValueId = ParamUtil.getLong(
+			renderRequest, "commerceProductOptionValueId");
+
+		CommerceProductOptionValue commerceProductOptionValue = null;
+
+		if (commerceProductOptionValueId > 0) {
+			commerceProductOptionValue =
+				_commerceProductOptionValueLocalService.
+					getCommerceProductOptionValue(commerceProductOptionValueId);
+		}
+
+		renderRequest.setAttribute(
+			CommerceProductWebKeys.COMMERCE_PRODUCT_OPTION_VALUE,
+			commerceProductOptionValue);
+	}
+
+	@Reference
+	private CommerceProductOptionValueLocalService
+		_commerceProductOptionValueLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
---
+++ modules/dxp/apps/commerce/commerce-product-options-web/src/main/resources/META-INF/resources/edit_product_option_value.jsp
@@ -0,0 +1,49 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+CommerceProductOptionValue commerceProductOptionValue = (CommerceProductOptionValue)request.getAttribute(CommerceProductWebKeys.COMMERCE_PRODUCT_OPTION_VALUE);
+
+long commerceProductOptionValueId = BeanParamUtil.getLong(commerceProductOptionValue, request, "commerceProductOptionValueId");
+
+long commerceProductOptionId = ParamUtil.getLong(request, "commerceProductOptionId");
+
+portletDisplay.setShowBackIcon(true);
+portletDisplay.setURLBack(backURL);
+
+renderResponse.setTitle((commerceProductOptionValue == null) ? LanguageUtil.get(request, "new-product-option-value") : commerceProductOptionValue.getTitle(locale));
+%>
+
+<portlet:actionURL name="editProductOptionValue" var="editProductOptionValueActionURL" />
+
+<aui:form action="<%= editProductOptionValueActionURL %>" cssClass="container-fluid-1280" method="post" name="fm">
+	<aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= (commerceProductOptionValue == null) ? Constants.ADD : Constants.UPDATE %>" />
+	<aui:input name="redirect" type="hidden" value="<%= redirect %>" />
+	<aui:input name="backURL" type="hidden" value="<%= backURL %>" />
+	<aui:input name="commerceProductOptionId" type="hidden" value="<%= String.valueOf(commerceProductOptionId) %>" />
+	<aui:input name="commerceProductOptionValueId" type="hidden" value="<%= String.valueOf(commerceProductOptionValueId) %>" />
+
+	<div class="lfr-form-content">
+		<liferay-ui:form-navigator
+			backURL="<%= backURL %>"
+			formModelBean="<%= commerceProductOptionValue %>"
+			id="<%= CommerceProductOptionValueFormNavigatorConstants.FORM_NAVIGATOR_ID_COMMERCE_PRODUCT_OPTION_VALUE %>"
+			markupView="lexicon"
+		/>
+	</div>
+</aui:form>
