b994b6ffc2f419099bdde65415fa307156bb3bb6
Marco Leo <marco.leo@liferay.com>
2017-04-18 22:57:03 -0700

    COMMERCE-2 Add product option item selector view

---
+++ modules/dxp/apps/commerce/commerce-product-item-selector-web/src/main/java/com/liferay/commerce/product/item/selector/web/internal/CommerceProductOptionItemSelectorView.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.item.selector.web.internal;
+
+import com.liferay.commerce.product.item.selector.criterion.CommerceProductOptionItemSelectorCriterion;
+import com.liferay.commerce.product.model.CommerceProductOption;
+import com.liferay.commerce.product.service.CommerceProductOptionLocalService;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(immediate = true, service = ItemSelectorView.class)
+public class CommerceProductOptionItemSelectorView
+	implements ItemSelectorView<CommerceProductOptionItemSelectorCriterion> {
+
+	@Override
+	public Class<CommerceProductOptionItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return CommerceProductOptionItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return LanguageUtil.get(locale, "product-options");
+	}
+
+	@Override
+	public boolean isShowSearch() {
+		return false;
+	}
+
+	@Override
+	public boolean isVisible(ThemeDisplay themeDisplay) {
+		return true;
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest request, ServletResponse response,
+			CommerceProductOptionItemSelectorCriterion
+				commerceProductOptionItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		int delta = GetterUtil.getInteger(
+			request.getParameter(SearchContainer.DEFAULT_DELTA_PARAM),
+			SearchContainer.DEFAULT_DELTA);
+
+		int cur = GetterUtil.getInteger(
+			request.getParameter(SearchContainer.DEFAULT_CUR_PARAM),
+			SearchContainer.DEFAULT_CUR);
+
+		int start = (delta * cur) - delta;
+		int end = (delta * cur) + delta;
+
+		List<CommerceProductOption> commerceProductOptions =
+			_commerceProductOptionLocalService.getCommerceProductOptions(
+				themeDisplay.getScopeGroupId(), start, end);
+
+		long commerceProductOptionsCount =
+			_commerceProductOptionLocalService.getCommerceProductOptionsCount(
+				themeDisplay.getScopeGroupId());
+
+		request.setAttribute("commerceProductOptions", commerceProductOptions);
+		request.setAttribute("itemSelectedEventName", itemSelectedEventName);
+		request.setAttribute("portletURL", portletURL);
+		request.setAttribute("total", commerceProductOptionsCount);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher(
+				"/commerce_product_option_item_selector.jsp");
+
+		requestDispatcher.include(request, response);
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.unmodifiableList(
+			ListUtil.fromArray(
+				new ItemSelectorReturnType[] {
+					new UUIDItemSelectorReturnType()
+				}));
+
+	@Reference
+	private CommerceProductOptionLocalService
+		_commerceProductOptionLocalService;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.product.item.selector.web)"
+	)
+	private ServletContext _servletContext;
+
+}
---
+++ modules/dxp/apps/commerce/commerce-product-item-selector-web/src/main/resources/META-INF/resources/commerce_product_option_item_selector.jsp
@@ -0,0 +1,80 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+PortletURL myportletURL = (PortletURL)request.getAttribute("portletURL");
+List<CommerceProductOption> commerceProductOptions = (List<CommerceProductOption>)request.getAttribute("commerceProductOptions");
+String itemSelectedEventName = (String)request.getAttribute("itemSelectedEventName");
+%>
+
+<div id="<portlet:namespace />commerceProductOptionSelectorWrapper">
+	<liferay-ui:search-container
+		emptyResultsMessage="no-product-options-were-found"
+		iteratorURL="<%= myportletURL %>"
+		total='<%= GetterUtil.getInteger(request.getAttribute("total")) %>'
+	>
+		<liferay-ui:search-container-results
+			results="<%= commerceProductOptions %>"
+		/>
+
+		<liferay-ui:search-container-row
+			className="com.liferay.commerce.product.model.CommerceProductOption"
+			cssClass="commerce-product-option-row" modelVar="commerceProductOption"
+		>
+			<liferay-ui:search-container-column-text>
+				<div class="commerce-product-option-name"
+					data-id="<%= commerceProductOption.getCommerceProductOptionId() %>"
+					<%= commerceProductOption.getName() %>
+				</div>
+			</liferay-ui:search-container-column-text>
+		</liferay-ui:search-container-row>
+
+		<liferay-ui:search-iterator markupView="lexicon" />
+
+		<liferay-ui:search-paginator searchContainer="<%= searchContainer %>" />
+	</liferay-ui:search-container>
+</div>
+
+<aui:script use="product-option-item-selector">
+
+	A.one("#<portlet:namespace />commerceProductOptionSelectorWrapper")
+		.delegate('click',
+			function(event) {
+
+				var por = event.currentTarget;
+
+				A.all('.commerce-product-option-row').removeClass('active');
+
+				por.addClass('active');
+
+				var pon = event.currentTarget.one('.commerce-product-option-name');
+				var id = pon.attr('data-id');
+
+				var data =
+					{
+						data:{
+							returnType: "COMMERCE-PRODUCT-OPTION",
+							value: {id: id}
+						}
+					};
+
+				Liferay.Util.getOpener().Liferay.fire('<%= itemSelectedEventName %>', data);
+			},
+			'.commerce-product-option-row');
+
+</aui:script>
