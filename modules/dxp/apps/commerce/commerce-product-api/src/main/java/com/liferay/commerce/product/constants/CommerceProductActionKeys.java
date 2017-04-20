c6ec357e12d79a425e2ff3e8cd6f04ffdc0d72f2
Marco Leo <marco.leo@liferay.com>
2017-04-20 16:08:16 -0700

    COMMERCE-2 Add commerce action keys

---
+++ modules/dxp/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/constants/CommerceProductActionKeys.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.constants;
+
+/**
+ * @author Marco Leo
+ */
+public class CommerceProductActionKeys {
+
+	public static final String ADD_COMMERCE_PRODUCT_DEFINITION =
+		"ADD_COMMERCE_PRODUCT_DEFINITION";
+
+	public static final String ADD_COMMERCE_PRODUCT_OPTION =
+		"ADD_COMMERCE_PRODUCT_OPTION";
+}
