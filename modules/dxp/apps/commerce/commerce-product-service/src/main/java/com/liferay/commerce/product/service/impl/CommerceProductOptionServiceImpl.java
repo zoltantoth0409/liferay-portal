4c26a33571dfcd8cbc1f1eaa1673152fd8c579e4
Marco Leo <marco.leo@liferay.com>
2017-04-20 16:10:21 -0700

    COMMERCE-2 Add service implementation with permission checker

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductOptionServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductOptionServiceImpl.java
@@ -14,11 +14,100 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.constants.CommerceProductActionKeys;
+import com.liferay.commerce.product.model.CommerceProductOption;
 import com.liferay.commerce.product.service.base.CommerceProductOptionServiceBaseImpl;
+import com.liferay.commerce.product.service.permission.CommerceProductOptionPermission;
+import com.liferay.commerce.product.service.permission.CommerceProductPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
  */
 public class CommerceProductOptionServiceImpl
 	extends CommerceProductOptionServiceBaseImpl {
+
+	@Override
+	public CommerceProductOption addCommerceProductOption(
+			Map<Locale, String> nameMap, Map<Locale, String> descriptionMap,
+			String ddmFormFieldTypeName, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductPermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			CommerceProductActionKeys.ADD_COMMERCE_PRODUCT_OPTION);
+
+		return commerceProductOptionLocalService.addCommerceProductOption(
+			nameMap, descriptionMap, ddmFormFieldTypeName, serviceContext);
+	}
+
+	@Override
+	public CommerceProductOption deleteCommerceProductOption(
+			CommerceProductOption commerceProductOption)
+		throws PortalException {
+
+		CommerceProductOptionPermission.check(
+			getPermissionChecker(), commerceProductOption, ActionKeys.DELETE);
+
+		return commerceProductOptionLocalService.deleteCommerceProductOption(
+			commerceProductOption);
+	}
+
+	@Override
+	public CommerceProductOption deleteCommerceProductOption(
+			long commerceProductOptionId)
+		throws PortalException {
+
+		CommerceProductOptionPermission.check(
+			getPermissionChecker(), commerceProductOptionId, ActionKeys.DELETE);
+
+		return commerceProductOptionLocalService.deleteCommerceProductOption(
+			commerceProductOptionId);
+	}
+
+	@Override
+	public List<CommerceProductOption> getCommerceProductOptions(
+		long groupId, int start, int end) {
+
+		return commerceProductOptionLocalService.getCommerceProductOptions(
+			groupId, start, end);
+	}
+
+	@Override
+	public List<CommerceProductOption> getCommerceProductOptions(
+		long groupId, int start, int end,
+		OrderByComparator<CommerceProductOption> orderByComparator) {
+
+		return commerceProductOptionLocalService.getCommerceProductOptions(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceProductOptionsCount(long groupId) {
+		return commerceProductOptionLocalService.getCommerceProductOptionsCount(
+			groupId);
+	}
+
+	@Override
+	public CommerceProductOption updateCommerceProductOption(
+			long commerceProductOptionId, Map<Locale, String> nameMap,
+			Map<Locale, String> descriptionMap, String ddmFormFieldTypeName,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductOptionPermission.check(
+			getPermissionChecker(), commerceProductOptionId, ActionKeys.UPDATE);
+
+		return commerceProductOptionLocalService.updateCommerceProductOption(
+			commerceProductOptionId, nameMap, descriptionMap,
+			ddmFormFieldTypeName, serviceContext);
+	}
+
 }
