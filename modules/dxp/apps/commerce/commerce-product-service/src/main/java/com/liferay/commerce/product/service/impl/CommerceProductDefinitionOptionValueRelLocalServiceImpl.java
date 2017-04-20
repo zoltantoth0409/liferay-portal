3459df3c8a6e35d363aa82353b37375cd73c936c
Marco Leo <marco.leo@liferay.com>
2017-04-20 14:07:14 -0700

    COMMERCE-2 Add product definition option value local service crud

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductDefinitionOptionValueRelLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductDefinitionOptionValueRelLocalServiceImpl.java
@@ -14,11 +14,157 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.model.CommerceProductDefinitionOptionValueRel;
 import com.liferay.commerce.product.service.base.CommerceProductDefinitionOptionValueRelLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
  */
 public class CommerceProductDefinitionOptionValueRelLocalServiceImpl
 	extends CommerceProductDefinitionOptionValueRelLocalServiceBaseImpl {
+
+	public CommerceProductDefinitionOptionValueRel
+			addCommerceProductDefinitionOptionValueRel(
+				long commerceProductDefinitionOptionRelId,
+				Map<Locale, String> titleMap, int priority,
+				ServiceContext serviceContext)
+		throws PortalException {
+
+		// Commerce product definition option value rel
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		long commerceProductDefinitionOptionValueRelId =
+			counterLocalService.increment();
+
+		CommerceProductDefinitionOptionValueRel
+			commerceProductDefinitionOptionValueRel =
+				commerceProductDefinitionOptionValueRelPersistence.create(
+					commerceProductDefinitionOptionValueRelId);
+
+		commerceProductDefinitionOptionValueRel.setGroupId(groupId);
+		commerceProductDefinitionOptionValueRel.setCompanyId(
+			user.getCompanyId());
+		commerceProductDefinitionOptionValueRel.setUserId(user.getUserId());
+		commerceProductDefinitionOptionValueRel.setUserName(user.getFullName());
+		commerceProductDefinitionOptionValueRel.
+			setCommerceProductDefinitionOptionRelId(
+				commerceProductDefinitionOptionRelId);
+		commerceProductDefinitionOptionValueRel.setPriority(priority);
+		commerceProductDefinitionOptionValueRel.setTitleMap(titleMap);
+		commerceProductDefinitionOptionValueRel.setExpandoBridgeAttributes(
+			serviceContext);
+
+		commerceProductDefinitionOptionValueRelPersistence.update(
+			commerceProductDefinitionOptionValueRel);
+
+		return commerceProductDefinitionOptionValueRel;
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CommerceProductDefinitionOptionValueRel
+			deleteCommerceProductDefinitionOptionValueRel(
+				CommerceProductDefinitionOptionValueRel
+					commerceProductDefinitionOptionValueRel)
+		throws PortalException {
+
+		// Commerce product definition option value rel
+
+		commerceProductDefinitionOptionValueRelPersistence.remove(
+			commerceProductDefinitionOptionValueRel);
+
+		// Expando
+
+		expandoRowLocalService.deleteRows(
+			commerceProductDefinitionOptionValueRel.
+				getCommerceProductDefinitionOptionValueRelId());
+
+		return commerceProductDefinitionOptionValueRel;
+	}
+
+	@Override
+	public CommerceProductDefinitionOptionValueRel
+			deleteCommerceProductDefinitionOptionValueRel(
+				long commerceProductDefinitionOptionValueRelId)
+		throws PortalException {
+
+		CommerceProductDefinitionOptionValueRel
+			commerceProductDefinitionOptionValueRel =
+				commerceProductDefinitionOptionValueRelPersistence.
+					findByPrimaryKey(commerceProductDefinitionOptionValueRelId);
+
+		return commerceProductDefinitionOptionValueRelLocalService.
+			deleteCommerceProductDefinitionOptionValueRel(
+				commerceProductDefinitionOptionValueRel);
+	}
+
+	@Override
+	public List<CommerceProductDefinitionOptionValueRel>
+		getCommerceProductDefinitionOptionValueRels(
+			long commerceProductDefinitionOptionRelId, int start, int end) {
+
+		return commerceProductDefinitionOptionValueRelPersistence.
+			findByCommerceProductDefinitionOptionRelId(
+				commerceProductDefinitionOptionRelId, start, end);
+	}
+
+	@Override
+	public List<CommerceProductDefinitionOptionValueRel>
+		getCommerceProductDefinitionOptionValueRels(
+			long commerceProductDefinitionOptionRelId, int start, int end,
+			OrderByComparator<CommerceProductDefinitionOptionValueRel>
+				orderByComparator) {
+
+		return commerceProductDefinitionOptionValueRelPersistence.
+			findByCommerceProductDefinitionOptionRelId(
+				commerceProductDefinitionOptionRelId, start, end,
+				orderByComparator);
+	}
+
+	@Override
+	public int getCommerceProductDefinitionOptionValueRelsCount(
+		long commerceProductDefinitionOptionRelId) {
+
+		return commerceProductDefinitionOptionValueRelPersistence.
+			countByCommerceProductDefinitionOptionRelId(
+				commerceProductDefinitionOptionRelId);
+	}
+
+	public CommerceProductDefinitionOptionValueRel
+			updateCommerceProductDefinitionOptionValueRel(
+				long commerceProductDefinitionOptionValueRelId,
+				Map<Locale, String> titleMap, int priority,
+				ServiceContext serviceContext)
+		throws PortalException {
+
+		// Commerce product definition option value rel
+
+		CommerceProductDefinitionOptionValueRel
+			commerceProductDefinitionOptionValueRel =
+				commerceProductDefinitionOptionValueRelPersistence.
+					findByPrimaryKey(commerceProductDefinitionOptionValueRelId);
+
+		commerceProductDefinitionOptionValueRel.setTitleMap(titleMap);
+		commerceProductDefinitionOptionValueRel.setPriority(priority);
+		commerceProductDefinitionOptionValueRel.setExpandoBridgeAttributes(
+			serviceContext);
+
+		commerceProductDefinitionOptionValueRelPersistence.update(
+			commerceProductDefinitionOptionValueRel);
+
+		return commerceProductDefinitionOptionValueRel;
+	}
+
 }
