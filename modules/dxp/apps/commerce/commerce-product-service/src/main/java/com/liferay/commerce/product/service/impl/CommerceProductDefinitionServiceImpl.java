2488f71d7690cbf1a461968669ba5655c37c7d5e
Marco Leo <marco.leo@liferay.com>
2017-04-20 22:53:19 -0700

    COMMERCE-2 Add product definition service implementation with permisison checkers

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductDefinitionServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductDefinitionServiceImpl.java
@@ -16,7 +16,21 @@ package com.liferay.commerce.product.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.commerce.product.constants.CommerceProductActionKeys;
+import com.liferay.commerce.product.model.CommerceProductDefinition;
 import com.liferay.commerce.product.service.base.CommerceProductDefinitionServiceBaseImpl;
+import com.liferay.commerce.product.service.permission.CommerceProductDefinitionPermission;
+import com.liferay.commerce.product.service.permission.CommerceProductPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.io.Serializable;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
@@ -24,4 +38,123 @@ import com.liferay.commerce.product.service.base.CommerceProductDefinitionServic
 @ProviderType
 public class CommerceProductDefinitionServiceImpl
 	extends CommerceProductDefinitionServiceBaseImpl {
+
+	@Override
+	public CommerceProductDefinition addCommerceProductDefinition(
+			String baseSKU, Map<Locale, String> titleMap,
+			Map<Locale, String> descriptionMap, String productTypeName,
+			String ddmStructureKey, int displayDateMonth, int displayDateDay,
+			int displayDateYear, int displayDateHour, int displayDateMinute,
+			int expirationDateMonth, int expirationDateDay,
+			int expirationDateYear, int expirationDateHour,
+			int expirationDateMinute, boolean neverExpire,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductPermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			CommerceProductActionKeys.ADD_COMMERCE_PRODUCT_DEFINITION);
+
+		return commerceProductDefinitionLocalService.
+			addCommerceProductDefinition(
+				baseSKU, titleMap, descriptionMap, productTypeName,
+				ddmStructureKey, displayDateMonth, displayDateDay,
+				displayDateYear, displayDateHour, displayDateMinute,
+				expirationDateMonth, expirationDateDay, expirationDateYear,
+				expirationDateHour, expirationDateMinute, neverExpire,
+				serviceContext);
+	}
+
+	@Override
+	public CommerceProductDefinition deleteCommerceProductDefinition(
+			CommerceProductDefinition commerceProductDefinition)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinition,
+			ActionKeys.DELETE);
+
+		return commerceProductDefinitionLocalService.
+			deleteCommerceProductDefinition(commerceProductDefinition);
+	}
+
+	@Override
+	public CommerceProductDefinition deleteCommerceProductDefinition(
+			long commerceProductDefinitionId)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.DELETE);
+
+		return commerceProductDefinitionLocalService.
+			deleteCommerceProductDefinition(commerceProductDefinitionId);
+	}
+
+	@Override
+	public List<CommerceProductDefinition> getCommerceProductDefinitions(
+		long groupId, int start, int end) {
+
+		return commerceProductDefinitionLocalService.
+			getCommerceProductDefinitions(groupId, start, end);
+	}
+
+	@Override
+	public List<CommerceProductDefinition> getCommerceProductDefinitions(
+		long groupId, int start, int end,
+		OrderByComparator<CommerceProductDefinition> orderByComparator) {
+
+		return commerceProductDefinitionLocalService.
+			getCommerceProductDefinitions(
+				groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceProductDefinitionsCount(long groupId) {
+		return commerceProductDefinitionLocalService.
+			getCommerceProductDefinitionsCount(groupId);
+	}
+
+	@Override
+	public CommerceProductDefinition updateCommerceProductDefinition(
+			long commerceProductDefinitionId, String baseSKU,
+			Map<Locale, String> titleMap, Map<Locale, String> descriptionMap,
+			String productTypeName, String ddmStructureKey,
+			int displayDateMonth, int displayDateDay, int displayDateYear,
+			int displayDateHour, int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.UPDATE);
+
+		return commerceProductDefinitionLocalService.
+			updateCommerceProductDefinition(
+				commerceProductDefinitionId, baseSKU, titleMap, descriptionMap,
+				productTypeName, ddmStructureKey, displayDateMonth,
+				displayDateDay, displayDateYear, displayDateHour,
+				displayDateMinute, expirationDateMonth, expirationDateDay,
+				expirationDateYear, expirationDateHour, expirationDateMinute,
+				neverExpire, serviceContext);
+	}
+
+	@Override
+	public CommerceProductDefinition updateStatus(
+			long userId, long commerceProductDefinitionId, int status,
+			ServiceContext serviceContext,
+			Map<String, Serializable> workflowContext)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.UPDATE);
+
+		return commerceProductDefinitionLocalService.updateStatus(
+			userId, commerceProductDefinitionId, status, serviceContext,
+			workflowContext);
+	}
+
 }
