ff1a46f2205745c5f99dfad5ec940caa93da60be
Marco Leo <marco.leo@liferay.com>
2017-04-20 20:25:03 -0700

    COMMERCE-2 Add product instance service implementation with permission checker

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductInstanceServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductInstanceServiceImpl.java
@@ -16,7 +16,19 @@ package com.liferay.commerce.product.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.commerce.product.constants.CommerceProductActionKeys;
+import com.liferay.commerce.product.model.CommerceProductInstance;
 import com.liferay.commerce.product.service.base.CommerceProductInstanceServiceBaseImpl;
+import com.liferay.commerce.product.service.permission.CommerceProductDefinitionPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.io.Serializable;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Marco Leo
@@ -24,4 +36,132 @@ import com.liferay.commerce.product.service.base.CommerceProductInstanceServiceB
 @ProviderType
 public class CommerceProductInstanceServiceImpl
 	extends CommerceProductInstanceServiceBaseImpl {
+
+	@Override
+	public CommerceProductInstance addCommerceProductInstance(
+			long commerceProductDefinitionId, String sku, String ddmContent,
+			int displayDateMonth, int displayDateDay, int displayDateYear,
+			int displayDateHour, int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			CommerceProductActionKeys.ADD_COMMERCE_PRODUCT_INSTANCE);
+
+		return commerceProductInstanceLocalService.addCommerceProductInstance(
+			commerceProductDefinitionId, sku, ddmContent, displayDateMonth,
+			displayDateDay, displayDateYear, displayDateHour, displayDateMinute,
+			expirationDateMonth, expirationDateDay, expirationDateYear,
+			expirationDateHour, expirationDateMinute, neverExpire,
+			serviceContext);
+	}
+
+	@Override
+	public CommerceProductInstance deleteCommerceProductInstance(
+			CommerceProductInstance commerceProductInstance)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.checkCommerceProductInstance(
+			getPermissionChecker(), commerceProductInstance,
+			CommerceProductActionKeys.DELETE_COMMERCE_PRODUCT_INSTANCE);
+
+		return commerceProductInstanceLocalService.
+			deleteCommerceProductInstance(commerceProductInstance);
+	}
+
+	@Override
+	public CommerceProductInstance deleteCommerceProductInstance(
+			long commerceProductInstanceId)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.checkCommerceProductInstance(
+			getPermissionChecker(), commerceProductInstanceId,
+			CommerceProductActionKeys.DELETE_COMMERCE_PRODUCT_INSTANCE);
+
+		return commerceProductInstanceLocalService.
+			deleteCommerceProductInstance(commerceProductInstanceId);
+	}
+
+	@Override
+	public List<CommerceProductInstance> getCommerceProductInstances(
+			long commerceProductDefinitionId, int start, int end)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.VIEW);
+
+		return commerceProductInstanceLocalService.getCommerceProductInstances(
+			commerceProductDefinitionId, start, end);
+	}
+
+	@Override
+	public List<CommerceProductInstance> getCommerceProductInstances(
+			long commerceProductDefinitionId, int start, int end,
+			OrderByComparator<CommerceProductInstance> orderByComparator)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.VIEW);
+
+		return commerceProductInstanceLocalService.getCommerceProductInstances(
+			commerceProductDefinitionId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceProductInstancesCount(
+			long commerceProductDefinitionId)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.VIEW);
+
+		return commerceProductInstanceLocalService.
+			getCommerceProductInstancesCount(commerceProductDefinitionId);
+	}
+
+	@Override
+	public CommerceProductInstance updateCommerceProductInstance(
+			long commerceProductInstanceId, String sku, int displayDateMonth,
+			int displayDateDay, int displayDateYear, int displayDateHour,
+			int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.checkCommerceProductInstance(
+			getPermissionChecker(), commerceProductInstanceId,
+			CommerceProductActionKeys.UPDATE_COMMERCE_PRODUCT_INSTANCE);
+
+		return commerceProductInstanceLocalService.
+			updateCommerceProductInstance(
+				commerceProductInstanceId, sku, displayDateMonth,
+				displayDateDay, displayDateYear, displayDateHour,
+				displayDateMinute, expirationDateMonth, expirationDateDay,
+				expirationDateYear, expirationDateHour, expirationDateMinute,
+				neverExpire, serviceContext);
+	}
+
+	@Override
+	public CommerceProductInstance updateStatus(
+			long userId, long commerceProductInstanceId, int status,
+			ServiceContext serviceContext,
+			Map<String, Serializable> workflowContext)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.checkCommerceProductInstance(
+			getPermissionChecker(), commerceProductInstanceId,
+			CommerceProductActionKeys.UPDATE_COMMERCE_PRODUCT_INSTANCE);
+
+		return commerceProductInstanceLocalService.updateStatus(
+			userId, commerceProductInstanceId, status, serviceContext,
+			workflowContext);
+	}
+
 }
