3098261117b20f07d47f9ee71652416a42710564
Marco Leo <marco.leo@liferay.com>
2017-04-21 15:21:20 -0700

    COMMERCE-2 Add new comparators

--- modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/util/CommerceProductDefinitionsPortletUtil.java
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/util/CommerceProductDefinitionsPortletUtil.java
@@ -16,7 +16,14 @@ package com.liferay.commerce.product.definitions.web.internal.util;
 
 import com.liferay.commerce.product.definitions.web.internal.util.comparator.CommerceProductDefinitionCreateDateComparator;
 import com.liferay.commerce.product.definitions.web.internal.util.comparator.CommerceProductDefinitionDisplayDateComparator;
+import com.liferay.commerce.product.definitions.web.internal.util.comparator.CommerceProductDefinitionOptionRelCreateDateComparator;
+import com.liferay.commerce.product.definitions.web.internal.util.comparator.CommerceProductDefinitionOptionRelNameComparator;
+import com.liferay.commerce.product.definitions.web.internal.util.comparator.CommerceProductInstanceCreateDateComparator;
+import com.liferay.commerce.product.definitions.web.internal.util.comparator.CommerceProductInstanceDisplayDateComparator;
+import com.liferay.commerce.product.definitions.web.internal.util.comparator.CommerceProductInstanceSkuComparator;
 import com.liferay.commerce.product.model.CommerceProductDefinition;
+import com.liferay.commerce.product.model.CommerceProductDefinitionOptionRel;
+import com.liferay.commerce.product.model.CommerceProductInstance;
 import com.liferay.portal.kernel.util.OrderByComparator;
 
 /**
@@ -24,9 +31,36 @@ import com.liferay.portal.kernel.util.OrderByComparator;
  */
 public class CommerceProductDefinitionsPortletUtil {
 
+	public static OrderByComparator<CommerceProductDefinitionOptionRel>
+	getCommerceProductDefinitionOptionRelOrderByComparator(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<CommerceProductDefinitionOptionRel>
+			orderByComparator = null;
+
+		if (orderByCol.equals("create-date")) {
+			orderByComparator =
+				new CommerceProductDefinitionOptionRelCreateDateComparator(
+					orderByAsc);
+		}
+		else if (orderByCol.equals("name")) {
+			orderByComparator =
+				new CommerceProductDefinitionOptionRelNameComparator(
+					orderByAsc);
+		}
+
+		return orderByComparator;
+	}
+
 	public static OrderByComparator<CommerceProductDefinition>
-		getCommerceProductDefinitionOrderByComparator(
-			String orderByCol, String orderByType) {
+	getCommerceProductDefinitionOrderByComparator(
+		String orderByCol, String orderByType) {
 
 		boolean orderByAsc = false;
 
@@ -43,6 +77,34 @@ public class CommerceProductDefinitionsPortletUtil {
 		else if (orderByCol.equals("display-date")) {
 			orderByComparator =
 				new CommerceProductDefinitionDisplayDateComparator(orderByAsc);
+		}
+
+		return orderByComparator;
+	}
+
+	public static OrderByComparator<CommerceProductInstance>
+	getCommerceProductInstanceOrderByComparator(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<CommerceProductInstance> orderByComparator = null;
+
+		if (orderByCol.equals("sku")) {
+			orderByComparator = new CommerceProductInstanceSkuComparator(
+				orderByAsc);
+		}
+		else if (orderByCol.equals("create-date")) {
+			orderByComparator = new CommerceProductInstanceCreateDateComparator(
+				orderByAsc);
+		}
+		else if (orderByCol.equals("display-date")) {
+			orderByComparator =
+				new CommerceProductInstanceDisplayDateComparator(orderByAsc);
 		}
 
 		return orderByComparator;
