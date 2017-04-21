67bee30d8a16ec42e253283a2a8f26d46a15d5ab
Marco Leo <marco.leo@liferay.com>
2017-04-20 17:51:14 -0700

    COMMERCE-2 Add service implementation with permission checkers

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductOptionValueServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductOptionValueServiceImpl.java
@@ -14,11 +14,116 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.constants.CommerceProductActionKeys;
+import com.liferay.commerce.product.model.CommerceProductOptionValue;
 import com.liferay.commerce.product.service.base.CommerceProductOptionValueServiceBaseImpl;
+import com.liferay.commerce.product.service.permission.CommerceProductOptionPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
  */
 public class CommerceProductOptionValueServiceImpl
 	extends CommerceProductOptionValueServiceBaseImpl {
+
+	@Override
+	public CommerceProductOptionValue addCommerceProductOptionValue(
+			long commerceProductOptionId, Map<Locale, String> titleMap,
+			int priority, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductOptionPermission.check(
+			getPermissionChecker(), commerceProductOptionId,
+			CommerceProductActionKeys.ADD_COMMERCE_PRODUCT_OPTION_VALUE);
+
+		return commerceProductOptionValueLocalService.
+			addCommerceProductOptionValue(
+				commerceProductOptionId, titleMap, priority, serviceContext);
+	}
+
+	@Override
+	public CommerceProductOptionValue deleteCommerceProductOptionValue(
+			CommerceProductOptionValue commerceProductOptionValue)
+		throws PortalException {
+
+		CommerceProductOptionPermission.checkCommerceProductOptionValue(
+			getPermissionChecker(),
+			commerceProductOptionValue.getCommerceProductOptionValueId(),
+			CommerceProductActionKeys.DELETE_COMMERCE_PRODUCT_OPTION_VALUE);
+
+		return commerceProductOptionValueLocalService.
+			deleteCommerceProductOptionValue(commerceProductOptionValue);
+	}
+
+	@Override
+	public CommerceProductOptionValue deleteCommerceProductOptionValue(
+			long commerceProductOptionValueId)
+		throws PortalException {
+
+		CommerceProductOptionPermission.checkCommerceProductOptionValue(
+			getPermissionChecker(), commerceProductOptionValueId,
+			CommerceProductActionKeys.DELETE_COMMERCE_PRODUCT_OPTION_VALUE);
+
+		return commerceProductOptionValueLocalService.
+			deleteCommerceProductOptionValue(commerceProductOptionValueId);
+	}
+
+	@Override
+	public List<CommerceProductOptionValue> getCommerceProductOptionValues(
+			long commerceProductOptionId, int start, int end)
+		throws PortalException {
+
+		CommerceProductOptionPermission.check(
+			getPermissionChecker(), commerceProductOptionId, ActionKeys.VIEW);
+		return commerceProductOptionValueLocalService.
+			getCommerceProductOptionValues(commerceProductOptionId, start, end);
+	}
+
+	@Override
+	public List<CommerceProductOptionValue> getCommerceProductOptionValues(
+			long commerceProductOptionId, int start, int end,
+			OrderByComparator<CommerceProductOptionValue> orderByComparator)
+		throws PortalException {
+
+		CommerceProductOptionPermission.check(
+			getPermissionChecker(), commerceProductOptionId, ActionKeys.VIEW);
+		return commerceProductOptionValueLocalService.
+			getCommerceProductOptionValues(
+				commerceProductOptionId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceProductOptionValuesCount(long commerceProductOptionId)
+		throws PortalException {
+
+		CommerceProductOptionPermission.check(
+			getPermissionChecker(), commerceProductOptionId, ActionKeys.VIEW);
+
+		return commerceProductOptionValueLocalService.
+			getCommerceProductOptionValuesCount(commerceProductOptionId);
+	}
+
+	@Override
+	public CommerceProductOptionValue updateCommerceProductOptionValue(
+			long commerceProductOptionValueId, Map<Locale, String> titleMap,
+			int priority, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductOptionPermission.checkCommerceProductOptionValue(
+			getPermissionChecker(), commerceProductOptionValueId,
+			CommerceProductActionKeys.UPDATE_COMMERCE_PRODUCT_OPTION_VALUE);
+
+		return commerceProductOptionValueLocalService.
+			updateCommerceProductOptionValue(
+				commerceProductOptionValueId, titleMap, priority,
+				serviceContext);
+	}
+
 }
