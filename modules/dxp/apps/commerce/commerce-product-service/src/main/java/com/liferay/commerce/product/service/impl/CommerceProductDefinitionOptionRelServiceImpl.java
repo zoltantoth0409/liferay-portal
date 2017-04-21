7ff2e9b0ee2b5d19f0e93271007c2b7951f03e5e
Marco Leo <marco.leo@liferay.com>
2017-04-20 23:13:59 -0700

    COMMERCE-2 Add product definition option rel service implementation with permission checker

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductDefinitionOptionRelServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceProductDefinitionOptionRelServiceImpl.java
@@ -14,11 +14,141 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.model.CommerceProductDefinitionOptionRel;
 import com.liferay.commerce.product.service.base.CommerceProductDefinitionOptionRelServiceBaseImpl;
+import com.liferay.commerce.product.service.permission.CommerceProductDefinitionPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
  */
 public class CommerceProductDefinitionOptionRelServiceImpl
 	extends CommerceProductDefinitionOptionRelServiceBaseImpl {
+
+	@Override
+	public CommerceProductDefinitionOptionRel
+			addCommerceProductDefinitionOptionRel(
+				long commerceProductDefinitionId, long commerceProductOptionId,
+				Map<Locale, String> nameMap, Map<Locale, String> descriptionMap,
+				String ddmFormFieldTypeName, int priority,
+				ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.UPDATE);
+
+		return commerceProductDefinitionOptionRelLocalService.
+			addCommerceProductDefinitionOptionRel(
+				commerceProductDefinitionId, commerceProductOptionId, nameMap,
+				descriptionMap, ddmFormFieldTypeName, priority, serviceContext);
+	}
+
+	@Override
+	public CommerceProductDefinitionOptionRel
+			deleteCommerceProductDefinitionOptionRel(
+				CommerceProductDefinitionOptionRel
+					commerceProductDefinitionOptionRel)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.
+			checkCommerceProductDefinitionOptionRel(
+				getPermissionChecker(), commerceProductDefinitionOptionRel,
+				ActionKeys.UPDATE);
+
+		return commerceProductDefinitionOptionRelLocalService.
+			deleteCommerceProductDefinitionOptionRel(
+				commerceProductDefinitionOptionRel);
+	}
+
+	@Override
+	public CommerceProductDefinitionOptionRel
+			deleteCommerceProductDefinitionOptionRel(
+				long commerceProductDefinitionOptionRelId)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.
+			checkCommerceProductDefinitionOptionRel(
+				getPermissionChecker(), commerceProductDefinitionOptionRelId,
+				ActionKeys.UPDATE);
+
+		return commerceProductDefinitionOptionRelLocalService.
+			deleteCommerceProductDefinitionOptionRel(
+				commerceProductDefinitionOptionRelId);
+	}
+
+	@Override
+	public List<CommerceProductDefinitionOptionRel>
+			getCommerceProductDefinitionOptionRels(
+				long commerceProductDefinitionId, int start, int end)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.VIEW);
+
+		return commerceProductDefinitionOptionRelLocalService.
+			getCommerceProductDefinitionOptionRels(
+				commerceProductDefinitionId, start, end);
+	}
+
+	@Override
+	public List<CommerceProductDefinitionOptionRel>
+			getCommerceProductDefinitionOptionRels(
+				long commerceProductDefinitionId, int start, int end,
+				OrderByComparator<CommerceProductDefinitionOptionRel>
+					orderByComparator)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.VIEW);
+
+		return commerceProductDefinitionOptionRelLocalService.
+			getCommerceProductDefinitionOptionRels(
+				commerceProductDefinitionId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceProductDefinitionOptionRelsCount(
+			long commerceProductDefinitionId)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.check(
+			getPermissionChecker(), commerceProductDefinitionId,
+			ActionKeys.VIEW);
+
+		return commerceProductDefinitionOptionRelLocalService.
+			getCommerceProductDefinitionOptionRelsCount(
+				commerceProductDefinitionId);
+	}
+
+	@Override
+	public CommerceProductDefinitionOptionRel
+			updateCommerceProductDefinitionOptionRel(
+				long commerceProductDefinitionOptionRelId,
+				long commerceProductOptionId, Map<Locale, String> nameMap,
+				Map<Locale, String> descriptionMap, String ddmFormFieldTypeName,
+				int priority, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceProductDefinitionPermission.
+			checkCommerceProductDefinitionOptionRel(
+				getPermissionChecker(), commerceProductDefinitionOptionRelId,
+				ActionKeys.UPDATE);
+
+		return commerceProductDefinitionOptionRelLocalService.
+			updateCommerceProductDefinitionOptionRel(
+				commerceProductDefinitionOptionRelId, commerceProductOptionId,
+				nameMap, descriptionMap, ddmFormFieldTypeName, priority,
+				serviceContext);
+	}
+
 }
