16f31a1cc2421d065cb7dd575cf4842b72c728e7
Alessio Rendina <asilcomando@gmail.com>
2017-04-27 12:37:09 -0700

    COMMERCE-2 Fix empty result message

--- modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/display/context/CPDefinitionOptionValueRelDisplayContext.java
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/display/context/CPDefinitionOptionValueRelDisplayContext.java
@@ -144,7 +144,7 @@ public class CPDefinitionOptionValueRelDisplayContext
 		searchContainer.setOrderByComparator(orderByComparator);
 		searchContainer.setOrderByType(getOrderByType());
 		searchContainer.setEmptyResultsMessage(
-			"no-options-value-were-found");
+			"no-option-values-were-found");
 		searchContainer.setRowChecker(getRowChecker());
 
 		int total =
