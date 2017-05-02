d9f2d18b04477264d27f779443967602c843139c
Marco Leo <marco.leo@liferay.com>
2017-05-02 15:59:09 -0700

    COMMERCE-19 Add Commerce Searcher

---
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/search/CPSearcher.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.search;
+
+import com.liferay.commerce.product.model.CPOption;
+import com.liferay.portal.kernel.search.BaseSearcher;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Indexer;
+
+/**
+ * @author Marco Leo
+ */
+public class CPSearcher extends BaseSearcher {
+
+	public static final String[] CLASS_NAMES = {
+		CPOption.class.getName()
+	};
+
+	public static Indexer<?> getInstance() {
+		return new CPSearcher();
+	}
+
+	public CPSearcher() {
+		setDefaultSelectedFieldNames(
+			Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK, Field.UID);
+		setFilterSearch(true);
+		setPermissionAware(true);
+	}
+
+	@Override
+	public String[] getSearchClassNames() {
+		return CLASS_NAMES;
+	}
+
+}
