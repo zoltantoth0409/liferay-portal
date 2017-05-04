d7c592b1665c8620a7ad574bcb78da2ca5128e5d
Marco Leo <marco.leo@liferay.com>
2017-05-03 23:39:29 -0700

    COMMERCE-2 Add definition indexer

---
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/search/CPDefinitionIndexer.java
@@ -0,0 +1,198 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.search;
+
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CPInstanceLocalService;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.BaseIndexer;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.IndexWriterHelper;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import java.util.Locale;
+
+/**
+ * @author Marco Leo
+ */
+@Component(immediate = true, service = Indexer.class)
+public class CPDefinitionIndexer extends BaseIndexer<CPDefinition> {
+
+	public CPDefinitionIndexer() {
+		setDefaultSelectedFieldNames(
+			Field.COMPANY_ID, Field.ENTRY_CLASS_NAME,
+			Field.ENTRY_CLASS_PK, Field.GROUP_ID, Field.MODIFIED_DATE,
+			Field.SCOPE_GROUP_ID, Field.NAME, Field.UID);
+		setFilterSearch(true);
+		setPermissionAware(true);
+	}
+
+	public static final String CLASS_NAME = CPDefinition.class.getName();
+
+	@Override
+	public String getClassName() {
+		return CLASS_NAME;
+	}
+
+	@Override
+	protected void doDelete(CPDefinition cpDefinition) throws Exception {
+		deleteDocument(cpDefinition.getCompanyId(), cpDefinition.getCPDefinitionId());
+	}
+
+
+	@Override
+	protected Document doGetDocument(CPDefinition cpDefinition)
+		throws Exception {
+		if (_log.isDebugEnabled()) {
+			_log.debug("Indexing definition " + cpDefinition);
+		}
+
+		Document document = getBaseModelDocument(CLASS_NAME, cpDefinition);
+
+		String cpDefinitionDefaultLanguageId = LocalizationUtil.
+			getDefaultLanguageId(cpDefinition.getTitle());
+
+		String[] languageIds = LocalizationUtil.getAvailableLanguageIds(
+			cpDefinition.getTitle());
+
+		for (String languageId : languageIds) {
+			String description = cpDefinition.getDescription(languageId);
+			String title = cpDefinition.getTitle(languageId);
+
+			if (languageId.equals(cpDefinitionDefaultLanguageId)) {
+				document.addText(Field.DESCRIPTION, description);
+				document.addText(Field.TITLE, title);
+				document.addText("defaultLanguageId", languageId);
+			}
+
+			document.addText(
+				LocalizationUtil.getLocalizedName(Field.TITLE, languageId),
+				title);
+			document.addText(
+				LocalizationUtil.getLocalizedName(
+					Field.DESCRIPTION, languageId),
+				description);
+
+			document.addText(Field.CONTENT, description);
+		}
+
+		document.addKeyword("baseSKU", cpDefinition.getBaseSKU());
+
+		String[] skus =  _cpInstanceLocalService.getSKUs(
+			cpDefinition.getCPDefinitionId());
+
+		document.addKeyword("skus", skus);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Document " + cpDefinition + " indexed successfully");
+		}
+
+		return document;
+	}
+
+	@Override
+	protected Summary doGetSummary(
+		Document document, Locale locale, String snippet,
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		Summary summary = createSummary(
+			document, Field.TITLE, Field.DESCRIPTION);
+
+		summary.setMaxContentLength(200);
+
+		return summary;
+	}
+
+	@Override
+	protected void doReindex(CPDefinition cpDefinition) throws Exception {
+		Document document = getDocument(cpDefinition);
+
+		_indexWriterHelper.updateDocument(
+			getSearchEngineId(), cpDefinition.getCompanyId(), document,
+			isCommitImmediately());
+	}
+
+	@Override
+	protected void doReindex(String className, long classPK) throws Exception {
+		CPDefinition cpDefinition =
+			_cpDefinitionLocalService.getCPDefinition(classPK);
+
+		doReindex(cpDefinition);
+	}
+
+	@Override
+	protected void doReindex(String[] ids) throws Exception {
+		long companyId = GetterUtil.getLong(ids[0]);
+
+		reindexCPDefinitions(companyId);
+	}
+
+	protected void reindexCPDefinitions(long companyId) throws PortalException {
+		final IndexableActionableDynamicQuery indexableActionableDynamicQuery =
+			_cpDefinitionLocalService.getIndexableActionableDynamicQuery();
+
+		indexableActionableDynamicQuery.setCompanyId(companyId);
+		indexableActionableDynamicQuery.setPerformActionMethod(
+			new ActionableDynamicQuery.PerformActionMethod<CPDefinition>() {
+
+				@Override
+				public void performAction(CPDefinition cpDefinition) {
+					try {
+						Document document = getDocument(cpDefinition);
+
+						indexableActionableDynamicQuery.addDocuments(document);
+					}
+					catch (PortalException pe) {
+						if (_log.isWarnEnabled()) {
+							_log.warn(
+								"Unable to index commerce product option " +
+									cpDefinition.getCPDefinitionId(),
+								pe);
+						}
+					}
+				}
+
+			});
+		indexableActionableDynamicQuery.setSearchEngineId(getSearchEngineId());
+
+		indexableActionableDynamicQuery.performActions();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDefinitionIndexer.class);
+
+	@Reference
+	private CPDefinitionLocalService _cpDefinitionLocalService;
+
+	@Reference
+	private CPInstanceLocalService _cpInstanceLocalService;
+
+	@Reference
+	private IndexWriterHelper _indexWriterHelper;
+
+}
