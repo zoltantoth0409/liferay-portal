5dacf5010d24b875e20026e92ff0620bb44cbc44
Marco Leo <marco.leo@liferay.com>
2017-05-03 22:06:45 -0700

    COMMERCE-23 Refactor

--- modules/dxp/apps/commerce/commerce-product-options-web/src/main/java/com/liferay/commerce/product/options/web/internal/display/context/CPOptionDisplayContext.java
+++ modules/dxp/apps/commerce/commerce-product-options-web/src/main/java/com/liferay/commerce/product/options/web/internal/display/context/CPOptionDisplayContext.java
@@ -17,32 +17,18 @@ package com.liferay.commerce.product.options.web.internal.display.context;
 import com.liferay.commerce.product.model.CPOption;
 import com.liferay.commerce.product.options.web.internal.portlet.action.ActionHelper;
 import com.liferay.commerce.product.options.web.internal.util.CPOptionsPortletUtil;
-import com.liferay.commerce.product.search.CPSearcher;
 import com.liferay.commerce.product.service.CPOptionService;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeServicesTracker;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.search.Document;
-import com.liferay.portal.kernel.search.DocumentImpl;
-import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.Hits;
-import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.QueryConfig;
-import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
-import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -96,8 +82,9 @@ public class CPOptionDisplayContext
 			return searchContainer;
 		}
 
-		ThemeDisplay themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
 
 		SearchContainer<CPOption> searchContainer = new SearchContainer<>(
 			liferayPortletRequest, getPortletURL(), null, null);
@@ -128,42 +115,15 @@ public class CPOptionDisplayContext
 				sort = new Sort("name", Sort.STRING_TYPE, orderByAsc);
 			}
 
-			LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+			BaseModelSearchResult<CPOption> cpOptionBaseModelSearchResult =
+				_cpOptionService.searchCPOptions(themeDisplay.getCompanyId(),
+					themeDisplay.getScopeGroupId(), getKeywords(),
+					new LinkedHashMap<>(), searchContainer.getStart(),
+					searchContainer.getEnd(), sort);
 
-			params.put("expandoAttributes", getKeywords());
-
-			Indexer indexer = CPSearcher.getInstance();
-
-			SearchContext searchContext = buildSearchContext(
-				themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId(),
-				CPOption.class.getName(), getKeywords(),
-				params, searchContainer.getStart(),
-				searchContainer.getEnd(), sort);
-
-			Hits hits = indexer.search(searchContext);
-
-			int total = hits.getLength();
-
-			searchContainer.setTotal(total);
-
-			List results = new ArrayList<>();
-
-			Document[] documents = hits.getDocs();
-
-			for (int i = 0; i < documents.length; i++) {
-				Document document = documents[i];
-
-				CPOption cpOption = null;
-				long classPK = GetterUtil.getLong(
-					document.get(Field.ENTRY_CLASS_PK));
-
-				cpOption =
-					_cpOptionService.getCPOption(classPK);
-
-				results.add(cpOption);
-			}
-
-			searchContainer.setResults(results);
+			searchContainer.setTotal(cpOptionBaseModelSearchResult.getLength());
+			searchContainer.setResults(
+				cpOptionBaseModelSearchResult.getBaseModels());
 
 		}
 		else {
@@ -180,76 +140,6 @@ public class CPOptionDisplayContext
 		this.searchContainer = searchContainer;
 
 		return this.searchContainer;
-	}
-
-	protected SearchContext buildSearchContext(
-		long companyId, long groupId,
-		String className,
-		String keywords, LinkedHashMap<String, Object> params, int start,
-		int end, Sort sort) {
-
-		String cpOptionId = null;
-		String name = null;
-		String description = null;
-		boolean andOperator = false;
-
-		if (Validator.isNotNull(keywords)) {
-			cpOptionId = keywords;
-			name = keywords;
-			description = keywords;
-		}
-		else {
-			andOperator = true;
-		}
-
-		if (params != null) {
-			params.put("keywords", keywords);
-		}
-
-		SearchContext searchContext = new SearchContext();
-
-		searchContext.setAndSearch(andOperator);
-
-		Map<String, Serializable> attributes = new HashMap<>();
-
-		attributes.put(Field.CLASS_PK, cpOptionId);
-		attributes.put(Field.ENTRY_CLASS_NAME, className);
-		attributes.put(Field.DESCRIPTION, description);
-		attributes.put(Field.NAME, name);
-		attributes.put("params", params);
-
-		searchContext.setAttributes(attributes);
-
-		searchContext.setCompanyId(companyId);
-		searchContext.setEnd(end);
-		searchContext.setGroupIds(new long[] {groupId});
-
-		if (params != null) {
-			keywords = (String)params.remove("keywords");
-
-			if (Validator.isNotNull(keywords)) {
-				searchContext.setKeywords(keywords);
-			}
-		}
-
-		searchContext.setAttribute("params", params);
-		searchContext.setEnd(end);
-		searchContext.setStart(start);
-
-		QueryConfig queryConfig = new QueryConfig();
-
-		queryConfig.setHighlightEnabled(false);
-		queryConfig.setScoreEnabled(false);
-
-		searchContext.setQueryConfig(queryConfig);
-
-		if (sort != null) {
-			searchContext.setSorts(sort);
-		}
-
-		searchContext.setStart(start);
-
-		return searchContext;
 	}
 
 	private final CPOptionService _cpOptionService;
