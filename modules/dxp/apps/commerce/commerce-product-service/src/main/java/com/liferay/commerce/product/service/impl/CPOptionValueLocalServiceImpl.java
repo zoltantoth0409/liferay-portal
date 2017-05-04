c782eb7b651559303bdeb2917c8bc4b283f9979e
Marco Leo <marco.leo@liferay.com>
2017-05-03 22:55:25 -0700

    COMMERCE-19 Add option value local service search methods

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPOptionValueLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPOptionValueLocalServiceImpl.java
@@ -18,12 +18,29 @@ import com.liferay.commerce.product.model.CPOptionValue;
 import com.liferay.commerce.product.service.base.CPOptionValueLocalServiceBaseImpl;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.QueryConfig;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Validator;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -142,5 +159,113 @@ public class CPOptionValueLocalServiceImpl
 
 		return cpOptionValue;
 	}
+
+	@Override
+	public Hits search(SearchContext searchContext) {
+
+		try {
+			Indexer<CPOptionValue> indexer =
+				IndexerRegistryUtil.nullSafeGetIndexer(CPOptionValue.class);
+
+			return indexer.search(searchContext);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+	}
+
+	@Override
+	public BaseModelSearchResult<CPOptionValue> searchCPOptionValues(
+		long companyId, long groupId, long cpOptionId,String keywords,
+		 int start, int end, Sort sort)
+		throws PortalException {
+
+		SearchContext searchContext = buildSearchContext(
+			companyId,groupId,cpOptionId ,keywords,start,end,sort);
+
+		return searchCPOptions(searchContext);
+	}
+
+	protected SearchContext buildSearchContext(
+		long companyId, long groupId, long cpOptionId,
+		String keywords, int start, int end, Sort sort) {
+
+		SearchContext searchContext = new SearchContext();
+
+
+		LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+		params.put("keywords", keywords);
+
+		Map<String, Serializable> attributes = new HashMap<>();
+
+		attributes.put(Field.ENTRY_CLASS_PK, keywords);
+		attributes.put(Field.TITLE, keywords);
+		attributes.put(Field.CONTENT, keywords);
+		attributes.put("CPOptionId", cpOptionId);
+		attributes.put("params", params);
+
+		searchContext.setAttributes(attributes);
+		searchContext.setCompanyId(companyId);
+		searchContext.setStart(start);
+		searchContext.setEnd(end);
+		searchContext.setGroupIds(new long[] {groupId});
+
+		if (Validator.isNotNull(keywords)) {
+			searchContext.setKeywords(keywords);
+		}
+
+		QueryConfig queryConfig = new QueryConfig();
+
+		queryConfig.setHighlightEnabled(false);
+		queryConfig.setScoreEnabled(false);
+
+		searchContext.setQueryConfig(queryConfig);
+
+		if (sort != null) {
+			searchContext.setSorts(sort);
+		}
+
+		return searchContext;
+	}
+
+	protected BaseModelSearchResult<CPOptionValue> searchCPOptions(
+		SearchContext searchContext)
+		throws PortalException {
+
+		Indexer<CPOptionValue> indexer =
+			IndexerRegistryUtil.nullSafeGetIndexer(CPOptionValue.class);
+
+		List<CPOptionValue> cpOptionValues = new ArrayList<>();
+
+
+		for (int i = 0; i < 10; i++) {
+			Hits hits = indexer.search(
+				searchContext, SELECTED_FIELD_NAMES);
+
+			Document[] documents = hits.getDocs();
+
+			for (Document document : documents) {
+
+				long classPK = GetterUtil.getLong(
+					document.get(Field.ENTRY_CLASS_PK));
+
+				CPOptionValue cpOptionValue =
+					getCPOptionValue(classPK);
+
+				cpOptionValues.add(cpOptionValue);
+			}
+
+			if (cpOptionValues != null) {
+				return new BaseModelSearchResult<>(cpOptionValues, hits.getLength());
+			}
+		}
+
+		throw new SearchException(
+			"Unable to fix the search index after 10 attempts");
+	}
+
+	public static final String[] SELECTED_FIELD_NAMES =
+		{Field.ENTRY_CLASS_PK, Field.COMPANY_ID, Field.GROUP_ID, Field.UID};
+
 
 }
