692048d4f9e220ca3346ed21900fb6861867eb92
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-05-05 11:45:06 -0700

    COMMERCE-20 Add XStream configurator

---
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/xstream/configurator/CPXStreamConfigurator.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.internal.xstream.configurator;
+
+import com.liferay.commerce.product.model.CPAttachmentFileEntry;
+import com.liferay.commerce.product.model.impl.CPDefinitionImpl;
+import com.liferay.commerce.product.model.impl.CPDefinitionOptionRelImpl;
+import com.liferay.commerce.product.model.impl.CPDefinitionOptionValueRelImpl;
+import com.liferay.commerce.product.model.impl.CPInstanceImpl;
+import com.liferay.commerce.product.model.impl.CPOptionCategoryImpl;
+import com.liferay.commerce.product.model.impl.CPOptionImpl;
+import com.liferay.commerce.product.model.impl.CPOptionValueImpl;
+import com.liferay.exportimport.kernel.xstream.XStreamAlias;
+import com.liferay.exportimport.kernel.xstream.XStreamConverter;
+import com.liferay.exportimport.kernel.xstream.XStreamType;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.xstream.configurator.XStreamConfigurator;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+@Component(immediate = true, service = XStreamConfigurator.class)
+public class CPXStreamConfigurator implements XStreamConfigurator {
+
+	@Override
+	public List<XStreamType> getAllowedXStreamTypes() {
+		return null;
+	}
+
+	@Override
+	public List<XStreamAlias> getXStreamAliases() {
+		return ListUtil.toList(_xStreamAliases);
+	}
+
+	@Override
+	public List<XStreamConverter> getXStreamConverters() {
+		return null;
+	}
+
+	@Activate
+	protected void activate() {
+		_xStreamAliases = new XStreamAlias[] {
+			new XStreamAlias(
+				CPAttachmentFileEntry.class, "CPAttachmentFileEntry"),
+			new XStreamAlias(CPDefinitionImpl.class, "CPDefinition"),
+			new XStreamAlias(
+				CPDefinitionOptionRelImpl.class, "CPDefinitionOptionRel"),
+			new XStreamAlias(
+				CPDefinitionOptionValueRelImpl.class,
+				"CPDefinitionOptionValueRel"),
+			new XStreamAlias(CPInstanceImpl.class, "CPInstance"),
+			new XStreamAlias(CPOptionCategoryImpl.class, "CPOptionCategory"),
+			new XStreamAlias(CPOptionImpl.class, "CPOption"),
+			new XStreamAlias(CPOptionValueImpl.class, "CPOptionValue")
+		};
+	}
+
+	private XStreamAlias[] _xStreamAliases;
+
+}
