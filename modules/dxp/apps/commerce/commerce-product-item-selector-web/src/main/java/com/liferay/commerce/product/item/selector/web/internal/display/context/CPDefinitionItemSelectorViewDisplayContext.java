f98ab1279736ccc0803aeb814d9b8ed40f6caa80
Alessio Rendina <asilcomando@gmail.com>
2017-05-08 15:36:31 -0700

    COMMERCE-56 Add new display context

---
+++ modules/dxp/apps/commerce/commerce-product-item-selector-web/src/main/java/com/liferay/commerce/product/item/selector/web/internal/display/context/CPDefinitionItemSelectorViewDisplayContext.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.item.selector.web.internal.display.context;
+
+import com.liferay.commerce.product.item.selector.web.internal.util.CPItemSelectorViewUtil;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.commerce.product.type.CPType;
+import com.liferay.commerce.product.type.CPTypeServicesTracker;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.List;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CPDefinitionItemSelectorViewDisplayContext
+	extends BaseCPItemSelectorViewDisplayContext {
+
+	public CPDefinitionItemSelectorViewDisplayContext(
+		HttpServletRequest httpServletRequest, PortletURL portletURL,
+		String itemSelectedEventName, CPDefinitionService cpDefinitionService,
+		CPTypeServicesTracker cpTypeServicesTracker) {
+
+		super(
+			httpServletRequest, portletURL, itemSelectedEventName,
+			"CPDefinitionItemSelectorView");
+
+		_cpDefinitionService = cpDefinitionService;
+		_cpTypeServicesTracker = cpTypeServicesTracker;
+	}
+
+	public CPType getCPType(String name) {
+		return _cpTypeServicesTracker.getCPType(name);
+	}
+
+	public SearchContainer<CPDefinition> getSearchContainer()
+		throws PortalException {
+
+		if (searchContainer != null) {
+			return searchContainer;
+		}
+
+		SearchContainer<CPDefinition> searchContainer = new SearchContainer<>(
+			liferayPortletRequest, getPortletURL(), null, null);
+
+		searchContainer.setEmptyResultsMessage("no-products-were-found");
+
+		OrderByComparator<CPDefinition> orderByComparator =
+			CPItemSelectorViewUtil.getCPDefinitionOrderByComparator(
+				getOrderByCol(), getOrderByType());
+
+		searchContainer.setOrderByCol(getOrderByCol());
+		searchContainer.setOrderByComparator(orderByComparator);
+		searchContainer.setOrderByType(getOrderByType());
+		searchContainer.setRowChecker(getRowChecker());
+
+		int total = _cpDefinitionService.getCPDefinitionsCount(
+			getScopeGroupId(), WorkflowConstants.STATUS_ANY);
+
+		searchContainer.setTotal(total);
+
+		List<CPDefinition> results = _cpDefinitionService.getCPDefinitions(
+			getScopeGroupId(), WorkflowConstants.STATUS_ANY,
+			searchContainer.getStart(), searchContainer.getEnd(),
+			orderByComparator);
+
+		searchContainer.setResults(results);
+
+		this.searchContainer = searchContainer;
+
+		return this.searchContainer;
+	}
+
+	private final CPDefinitionService _cpDefinitionService;
+	private final CPTypeServicesTracker _cpTypeServicesTracker;
+
+}
