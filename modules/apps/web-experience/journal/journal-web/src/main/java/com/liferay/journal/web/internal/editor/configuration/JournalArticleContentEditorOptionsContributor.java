279bf5fed091ed059b4c8f744c4576a931486107
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-05-08 12:12:43 +0200

    LPS-72391 Contributor for editors in journal portlet to handle the upload of images

---
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/editor/configuration/JournalArticleContentEditorOptionsContributor.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.editor.configuration;
+
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.portal.kernel.editor.configuration.EditorOptions;
+import com.liferay.portal.kernel.editor.configuration.EditorOptionsContributor;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	property = {"javax.portlet.name=" + JournalPortletKeys.JOURNAL},
+	service = EditorOptionsContributor.class
+)
+public class JournalArticleContentEditorOptionsContributor
+	implements EditorOptionsContributor {
+
+	@Override
+	public void populateEditorOptions(
+		EditorOptions editorOptions,
+		Map<String, Object> inputEditorTaglibAttributes,
+		ThemeDisplay themeDisplay,
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		if (Validator.isNull(portletDisplay.getId())) {
+			return;
+		}
+
+		PortletURL portletURL = requestBackedPortletURLFactory.createActionURL(
+			portletDisplay.getId());
+
+		portletURL.setParameter(
+			ActionRequest.ACTION_NAME, "/journal/upload_image");
+
+		editorOptions.setUploadURL(portletURL.toString());
+	}
+
+}
