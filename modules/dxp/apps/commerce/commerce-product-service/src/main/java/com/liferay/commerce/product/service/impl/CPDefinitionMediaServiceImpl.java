c4a8ed01b6222ea1cf77327baa7d387a5497d5c3
Marco Leo <marco.leo@liferay.com>
2017-05-08 20:53:06 -0700

    COMMERCE-16 Add remote methods

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionMediaServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionMediaServiceImpl.java
@@ -14,11 +14,90 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.model.CPDefinitionMedia;
+import com.liferay.commerce.product.model.CPMediaType;
 import com.liferay.commerce.product.service.base.CPDefinitionMediaServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
 
 /**
  * @author Marco Leo
  */
 public class CPDefinitionMediaServiceImpl
 	extends CPDefinitionMediaServiceBaseImpl {
+
+	@Override
+	public CPDefinitionMedia addCPDefinitionMedia(
+			long cpDefinitionId, long fileEntryId, String ddmContent,
+			int priority, long cpMediaTypeId, ServiceContext serviceContext)
+		throws PortalException {
+
+		return cpDefinitionMediaLocalService.addCPDefinitionMedia(
+			cpDefinitionId, fileEntryId, ddmContent, priority, cpMediaTypeId,
+			serviceContext);
+	}
+
+	@Override
+	public CPMediaType deleteCPMediaType(long cpMediaTypeId)
+		throws PortalException {
+
+		return cpDefinitionMediaLocalService.deleteCPMediaType(cpMediaTypeId);
+	}
+
+	@Override
+	public List<CPDefinitionMedia> getDefinitionMedias(
+		long cpDefinitionId, int start, int end) {
+
+		return cpDefinitionMediaLocalService.getDefinitionMedias(
+			cpDefinitionId, start, end);
+	}
+
+	@Override
+	public List<CPDefinitionMedia> getDefinitionMedias(
+		long cpDefinitionId, int start, int end,
+		OrderByComparator<CPDefinitionMedia> orderByComparator) {
+
+		return cpDefinitionMediaLocalService.getDefinitionMedias(
+			cpDefinitionId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getDefinitionMediasCount(long cpDefinitionId) {
+		return cpDefinitionMediaLocalService.getDefinitionMediasCount(
+			cpDefinitionId);
+	}
+
+	@Override
+	public Hits search(SearchContext searchContext) {
+		return cpDefinitionMediaLocalService.search(searchContext);
+	}
+
+	@Override
+	public BaseModelSearchResult<CPDefinitionMedia> searchCPDefinitionMedias(
+			long companyId, long groupId, long cpDefinitionId, String keywords,
+			int start, int end, Sort sort)
+		throws PortalException {
+
+		return cpDefinitionMediaLocalService.searchCPDefinitionMedias(
+			companyId, groupId, cpDefinitionId, keywords, start, end, sort);
+	}
+
+	@Override
+	public CPDefinitionMedia updateCPDefinitionMedia(
+			long cpDefinitionMediaId, String ddmContent, int priority,
+			long cpMediaTypeId, ServiceContext serviceContext)
+		throws PortalException {
+
+		return cpDefinitionMediaLocalService.updateCPDefinitionMedia(
+			cpDefinitionMediaId, ddmContent, priority, cpMediaTypeId,
+			serviceContext);
+	}
+
 }
