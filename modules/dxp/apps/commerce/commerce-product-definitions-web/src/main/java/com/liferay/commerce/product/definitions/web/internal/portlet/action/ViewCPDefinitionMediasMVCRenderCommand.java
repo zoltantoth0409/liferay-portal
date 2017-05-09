fcc22435c253365a8279dd6df5c76eab08abc940
Marco Leo <marco.leo@liferay.com>
2017-05-08 20:59:26 -0700

    COMMERCE-16 Add view action command

---
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/portlet/action/ViewCPDefinitionMediasMVCRenderCommand.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.portlet.action;
+
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.commerce.product.definitions.web.internal.display.context.CPDefinitionMediaDisplayContext;
+import com.liferay.commerce.product.service.CPDefinitionMediaService;
+import com.liferay.commerce.product.service.CPInstanceService;
+import com.liferay.commerce.product.service.CPMediaTypeService;
+import com.liferay.commerce.product.util.CPInstanceHelper;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CPPortletKeys.COMMERCE_PRODUCT_DEFINITIONS,
+		"mvc.command.name=viewDefinitionMedias"
+	},
+	service = MVCRenderCommand.class
+)
+public class ViewCPDefinitionMediasMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			HttpServletRequest httpServletRequest =
+				_portal.getHttpServletRequest(renderRequest);
+
+			CPDefinitionMediaDisplayContext cpInstanceDisplayContext =
+				new CPDefinitionMediaDisplayContext(
+					_actionHelper, httpServletRequest,
+					_cpDefinitionMediaService);
+
+			renderRequest.setAttribute(
+				WebKeys.PORTLET_DISPLAY_CONTEXT, cpInstanceDisplayContext);
+		}
+		catch (PortalException pe) {
+			SessionErrors.add(renderRequest, pe.getClass());
+		}
+
+		return "/definition_medias.jsp";
+	}
+
+	@Reference
+	private ActionHelper _actionHelper;
+
+	@Reference
+	private CPDefinitionMediaService _cpDefinitionMediaService;
+
+	@Reference
+	private Portal _portal;
+
+}
