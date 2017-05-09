d7ffea3b7572e6e5940e17e1e064714c83990f8b
Alessio Rendina <asilcomando@gmail.com>
2017-05-08 18:47:28 -0700

    COMMERCE-26 Add methods to data creator implementation

--- modules/dxp/apps/commerce/commerce-product-demo-data-creator-impl/src/main/java/com/liferay/commerce/product/demo/data/creator/internal/CPDemoDataCreatorImpl.java
+++ modules/dxp/apps/commerce/commerce-product-demo-data-creator-impl/src/main/java/com/liferay/commerce/product/demo/data/creator/internal/CPDemoDataCreatorImpl.java
@@ -15,13 +15,21 @@
 package com.liferay.commerce.product.demo.data.creator.internal;
 
 import com.liferay.commerce.product.demo.data.creator.CPDemoDataCreator;
+import com.liferay.commerce.product.demo.data.creator.internal.util.CPDefinitionDemoDataCreatorHelper;
+import com.liferay.commerce.product.demo.data.creator.internal.util.CPOptionDemoDataCreatorHelper;
+import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
 
 import java.io.IOException;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alessio Antonio Rendina
@@ -32,13 +40,49 @@ public class CPDemoDataCreatorImpl implements CPDemoDataCreator {
 	@Override
 	public void create(long userId, long groupId, boolean buildSkus)
 		throws IOException, PortalException {
+
+		JSONArray catalog = _cpDefinitionDemoDataCreatorHelper.getCatalog();
+
+		for (int i = 0; i < catalog.length(); i++) {
+			JSONObject product = catalog.getJSONObject(i);
+
+			String baseSKU = product.getString("baseSKU");
+			String name = product.getString("name");
+			String title = product.getString("title");
+			String description = product.getString("description");
+			String productTypeName = product.getString("productTypeName");
+
+			Map<Locale, String> titleMap = new HashMap<>();
+			Map<Locale, String> descriptionMap = new HashMap<>();
+
+			titleMap.put(Locale.US, title);
+			descriptionMap.put(Locale.US, description);
+
+			CPDefinition cpDefinition =
+				_cpDefinitionDemoDataCreatorHelper.createCPDefinition(
+					userId, groupId, baseSKU, name, titleMap, descriptionMap,
+					productTypeName);
+
+			long cpDefinitionId = cpDefinition.getCPDefinitionId();
+
+			JSONArray options = product.getJSONArray("options");
+
+			_cpOptionDemoDataCreatorHelper.addCPOptions(
+				Locale.US, userId, groupId, cpDefinitionId, options);
+		}
 	}
 
 	@Override
 	public void delete() throws PortalException {
+		_cpDefinitionDemoDataCreatorHelper.deleteCPDefinitions();
+		_cpOptionDemoDataCreatorHelper.deleteCPOptions();
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		CPDemoDataCreatorImpl.class);
+	@Reference
+	private CPDefinitionDemoDataCreatorHelper
+		_cpDefinitionDemoDataCreatorHelper;
+
+	@Reference
+	private CPOptionDemoDataCreatorHelper _cpOptionDemoDataCreatorHelper;
 
 }
