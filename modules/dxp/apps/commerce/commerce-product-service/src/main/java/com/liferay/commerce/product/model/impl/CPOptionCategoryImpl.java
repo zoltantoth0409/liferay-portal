22821ef2e5160b983625a2da38e53176bf3b0aa1
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-05-10 13:15:58 -0700

    COMMERCE-62 Remove autogenerated comments

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPOptionCategoryImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPOptionCategoryImpl.java
@@ -17,22 +17,11 @@ package com.liferay.commerce.product.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the CPOptionCategory service. Represents a row in the &quot;CPOptionCategory&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.commerce.product.model.CPOptionCategory} interface.
- * </p>
- *
  * @author Marco Leo
  */
 @ProviderType
 public class CPOptionCategoryImpl extends CPOptionCategoryBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a cp option category model instance should use the {@link com.liferay.commerce.product.model.CPOptionCategory} interface instead.
-	 */
 	public CPOptionCategoryImpl() {
 	}
 
