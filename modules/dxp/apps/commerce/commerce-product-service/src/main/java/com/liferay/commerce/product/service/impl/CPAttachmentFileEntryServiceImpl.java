c08fbad352f10803f9a9fd6b248bf3dd53192227
Marco Leo <marco.leo@liferay.com>
2017-05-12 00:53:55 -0700

    COMMERCE-16 Add remote services implementation

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPAttachmentFileEntryServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPAttachmentFileEntryServiceImpl.java
@@ -14,11 +14,104 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.model.CPAttachmentFileEntry;
 import com.liferay.commerce.product.service.base.CPAttachmentFileEntryServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
 
 /**
  * @author Marco Leo
  */
 public class CPAttachmentFileEntryServiceImpl
 	extends CPAttachmentFileEntryServiceBaseImpl {
+
+	@Override
+	public CPAttachmentFileEntry addCPAttachmentFileEntry(
+			long classNameId, long classPK, long fileEntryId,
+			int displayDateMonth, int displayDateDay, int displayDateYear,
+			int displayDateHour, int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, String json, int priority, int type,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		return cpAttachmentFileEntryLocalService.addCPAttachmentFileEntry(
+			classNameId, classPK, fileEntryId, displayDateMonth, displayDateDay,
+			displayDateYear, displayDateHour, displayDateMinute,
+			expirationDateMonth, expirationDateDay, expirationDateYear,
+			expirationDateHour, expirationDateMinute, neverExpire, json,
+			priority, type, serviceContext);
+	}
+
+	@Override
+	public CPAttachmentFileEntry deleteCPAttachmentFileEntry(
+			CPAttachmentFileEntry cpAttachmentFileEntry)
+		throws PortalException {
+
+		return cpAttachmentFileEntryLocalService.deleteCPAttachmentFileEntry(
+			cpAttachmentFileEntry);
+	}
+
+	@Override
+	public CPAttachmentFileEntry deleteCPAttachmentFileEntry(
+			long cpAttachmentFileEntryId)
+		throws PortalException {
+
+		return cpAttachmentFileEntryLocalService.deleteCPAttachmentFileEntry(
+			cpAttachmentFileEntryId);
+	}
+
+	@Override
+	public List<CPAttachmentFileEntry> getCPAttachmentFileEntrys(
+			long classNameId, int classPK, int start, int end)
+		throws PortalException {
+
+		return cpAttachmentFileEntryLocalService.getCPAttachmentFileEntrys(
+			classNameId, classPK, start, end);
+	}
+
+	@Override
+	public List<CPAttachmentFileEntry> getCPAttachmentFileEntrys(
+			long classNameId, int classPK, int start, int end,
+			OrderByComparator<CPAttachmentFileEntry> orderByComparator)
+		throws PortalException {
+
+		return cpAttachmentFileEntryLocalService.getCPAttachmentFileEntrys(
+			classNameId, classPK, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCPAttachmentFileEntrysCount(
+		long classNameId, int classPK) {
+
+		return cpAttachmentFileEntryLocalService.getCPAttachmentFileEntrysCount(
+			classNameId, classPK);
+	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CPAttachmentFileEntry updateCPAttachmentFileEntry(
+			long cpAttachmentFileEntryId, int displayDateMonth,
+			int displayDateDay, int displayDateYear, int displayDateHour,
+			int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, String json, int priority, int type,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		return cpAttachmentFileEntryLocalService.updateCPAttachmentFileEntry(
+			cpAttachmentFileEntryId, displayDateMonth, displayDateDay,
+			displayDateYear, displayDateHour, displayDateMinute,
+			expirationDateMonth, expirationDateDay, expirationDateYear,
+			expirationDateHour, expirationDateMinute, neverExpire, json,
+			priority, type, serviceContext);
+	}
+
 }
