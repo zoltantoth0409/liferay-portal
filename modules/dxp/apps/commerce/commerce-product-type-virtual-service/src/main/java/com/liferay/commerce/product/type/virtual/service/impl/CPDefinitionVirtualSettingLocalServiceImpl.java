98be825b5a4b18d28afb0710ee18547ac0ee8dc4
Alessio Rendina <asilcomando@gmail.com>
2017-05-12 15:44:42 -0700

    COMMERCE-27 Add validation methods

--- modules/dxp/apps/commerce/commerce-product-type-virtual-service/src/main/java/com/liferay/commerce/product/type/virtual/service/impl/CPDefinitionVirtualSettingLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-type-virtual-service/src/main/java/com/liferay/commerce/product/type/virtual/service/impl/CPDefinitionVirtualSettingLocalServiceImpl.java
@@ -14,11 +14,163 @@
 
 package com.liferay.commerce.product.type.virtual.service.impl;
 
+import com.liferay.commerce.product.type.virtual.exception.CPDefinitionVirtualSettingFileEntryIdException;
+import com.liferay.commerce.product.type.virtual.exception.CPDefinitionVirtualSettingUrlException;
+import com.liferay.commerce.product.type.virtual.model.CPDefinitionVirtualSetting;
 import com.liferay.commerce.product.type.virtual.service.base.CPDefinitionVirtualSettingLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
+ * @author Alessio Antonio Rendina
  */
 public class CPDefinitionVirtualSettingLocalServiceImpl
 	extends CPDefinitionVirtualSettingLocalServiceBaseImpl {
+
+	@Override
+	public CPDefinitionVirtualSetting addCPDefinitionVirtualSetting(
+			long cpDefinitionId, long fileEntryId, String url,
+			String activationStatus, long duration, int maxUsages,
+			long sampleFileEntryId, String sampleUrl,
+			boolean termsOfUseRequired,
+			Map<Locale, String> termsOfUseContentMap,
+			long termsOfUseJournalArticleId, boolean useUrl, boolean useFileEntry, ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		validate(useUrl, url);
+		validate(useFileEntry, fileEntryId);
+
+		long cpDefinitionVirtualSettingId = counterLocalService.increment();
+
+		CPDefinitionVirtualSetting cpDefinitionVirtualSetting =
+			cpDefinitionVirtualSettingPersistence.create(
+				cpDefinitionVirtualSettingId);
+
+		cpDefinitionVirtualSetting.setGroupId(groupId);
+		cpDefinitionVirtualSetting.setCompanyId(user.getCompanyId());
+		cpDefinitionVirtualSetting.setUserId(user.getUserId());
+		cpDefinitionVirtualSetting.setUserName(user.getFullName());
+		cpDefinitionVirtualSetting.setCPDefinitionId(cpDefinitionId);
+		cpDefinitionVirtualSetting.setFileEntryId(fileEntryId);
+		cpDefinitionVirtualSetting.setUrl(url);
+		cpDefinitionVirtualSetting.setActivationStatus(activationStatus);
+		cpDefinitionVirtualSetting.setDuration(duration);
+		cpDefinitionVirtualSetting.setMaxUsages(maxUsages);
+		cpDefinitionVirtualSetting.setSampleFileEntryId(sampleFileEntryId);
+		cpDefinitionVirtualSetting.setSampleUrl(sampleUrl);
+		cpDefinitionVirtualSetting.setTermsOfUseRequired(termsOfUseRequired);
+		cpDefinitionVirtualSetting.setTermsOfUseContentMap(
+			termsOfUseContentMap);
+		cpDefinitionVirtualSetting.setTermsOfUseJournalArticleId(
+			termsOfUseJournalArticleId);
+		cpDefinitionVirtualSetting.setExpandoBridgeAttributes(serviceContext);
+
+		cpDefinitionVirtualSettingPersistence.update(
+			cpDefinitionVirtualSetting);
+
+		return cpDefinitionVirtualSetting;
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CPDefinitionVirtualSetting deleteCPDefinitionVirtualSetting(
+			CPDefinitionVirtualSetting cpDefinitionVirtualSetting)
+		throws PortalException {
+
+		cpDefinitionVirtualSettingPersistence.remove(
+			cpDefinitionVirtualSetting);
+
+		return cpDefinitionVirtualSetting;
+	}
+
+	@Override
+	public CPDefinitionVirtualSetting deleteCPDefinitionVirtualSetting(
+			long cpDefinitionVirtualSettingId)
+		throws PortalException {
+
+		CPDefinitionVirtualSetting cpDefinitionVirtualSetting =
+			cpDefinitionVirtualSettingPersistence.findByPrimaryKey(
+				cpDefinitionVirtualSettingId);
+
+		return cpDefinitionVirtualSettingLocalService.
+			deleteCPDefinitionVirtualSetting(cpDefinitionVirtualSetting);
+	}
+
+	@Override
+	public CPDefinitionVirtualSetting getCPDefinitionVirtualSetting(
+			long cpDefinitionId)
+		throws PortalException {
+
+		return cpDefinitionVirtualSettingPersistence.findByCPDefinitionId(
+			cpDefinitionId);
+	}
+
+	@Override
+	public CPDefinitionVirtualSetting updateCPDefinitionVirtualSetting(
+			long cpDefinitionVirtualSettingId, long fileEntryId, String url,
+			String activationStatus, long duration, int maxUsages,
+			long sampleFileEntryId, String sampleUrl,
+			boolean termsOfUseRequired,
+			Map<Locale, String> termsOfUseContentMap,
+			long termsOfUseJournalArticleId, boolean useUrl, boolean useFileEntry, ServiceContext serviceContext)
+		throws PortalException {
+
+		CPDefinitionVirtualSetting cpDefinitionVirtualSetting =
+			cpDefinitionVirtualSettingPersistence.findByPrimaryKey(
+				cpDefinitionVirtualSettingId);
+
+		validate(useUrl, url);
+		validate(useFileEntry, fileEntryId);
+
+		cpDefinitionVirtualSetting.setFileEntryId(fileEntryId);
+		cpDefinitionVirtualSetting.setUrl(url);
+		cpDefinitionVirtualSetting.setActivationStatus(activationStatus);
+		cpDefinitionVirtualSetting.setDuration(duration);
+		cpDefinitionVirtualSetting.setMaxUsages(maxUsages);
+		cpDefinitionVirtualSetting.setSampleFileEntryId(sampleFileEntryId);
+		cpDefinitionVirtualSetting.setSampleUrl(sampleUrl);
+		cpDefinitionVirtualSetting.setTermsOfUseRequired(termsOfUseRequired);
+		cpDefinitionVirtualSetting.setTermsOfUseContentMap(
+			termsOfUseContentMap);
+		cpDefinitionVirtualSetting.setTermsOfUseJournalArticleId(
+			termsOfUseJournalArticleId);
+		cpDefinitionVirtualSetting.setExpandoBridgeAttributes(serviceContext);
+
+		cpDefinitionVirtualSettingPersistence.update(
+			cpDefinitionVirtualSetting);
+
+		return cpDefinitionVirtualSetting;
+	}
+
+	protected void validate(boolean useFileEntry, long fileEntryId)
+		throws PortalException {
+
+		if (useFileEntry) {
+			if (fileEntryId <= 0) {
+				throw new CPDefinitionVirtualSettingFileEntryIdException;
+			}
+		}
+	}
+
+	protected void validate(boolean useUrl, String url)
+		throws PortalException {
+
+		if (useUrl) {
+			if (Validator.isNull(url)) {
+				throw new CPDefinitionVirtualSettingUrlException;
+			}
+		}
+	}
+
 }
