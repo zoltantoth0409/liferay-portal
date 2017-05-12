a181e7f7d7edb53af33ccc30cc20302c47575f93
Alessio Rendina <asilcomando@gmail.com>
2017-05-12 16:37:24 -0700

    COMMERCE-27 Add new permission checkers

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/permission/CPDefinitionPermission.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/permission/CPDefinitionPermission.java
@@ -23,6 +23,8 @@ import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.CPDefinitionOptionRelLocalService;
 import com.liferay.commerce.product.service.CPDefinitionOptionValueRelLocalService;
 import com.liferay.commerce.product.service.CPInstanceLocalService;
+import com.liferay.commerce.product.type.virtual.model.CPDefinitionVirtualSetting;
+import com.liferay.commerce.product.type.virtual.service.CPDefinitionVirtualSettingLocalService;
 import com.liferay.exportimport.kernel.staging.permission.StagingPermissionUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
@@ -112,6 +114,29 @@ public class CPDefinitionPermission implements BaseModelPermissionChecker {
 
 		checkCPDefinitionOptionValueRel(
 			permissionChecker, cpDefinitionOptionValueRel, actionId);
+	}
+
+	public static void checkCPDefinitionVirtualSetting(
+			PermissionChecker permissionChecker,
+			CPDefinitionVirtualSetting cpDefinitionVirtualSetting, String actionId)
+		throws PortalException {
+
+		long cpDefinitionId = cpDefinitionVirtualSetting.getCPDefinitionId();
+
+		check(permissionChecker, cpDefinitionId, actionId);
+	}
+
+	public static void checkCPDefinitionVirtualSetting(
+			PermissionChecker permissionChecker,
+			long cpDefinitionVirtualSettingId, String actionId)
+		throws PortalException {
+
+		CPDefinitionVirtualSetting cpDefinitionVirtualSetting =
+			_cpDefinitionVirtualSettingLocalService.getCPDefinitionVirtualSetting(
+					cpDefinitionVirtualSettingId);
+
+		checkCPDefinitionVirtualSetting(
+			permissionChecker, cpDefinitionVirtualSetting, actionId);
 	}
 
 	public static void checkCPInstance(
@@ -206,6 +231,13 @@ public class CPDefinitionPermission implements BaseModelPermissionChecker {
 	}
 
 	@Reference(unbind = "-")
+	protected void setCPDefinitionVirtualSettingLocalService(
+		CPDefinitionVirtualSettingLocalService cpDefinitionVirtualSettingLocalService) {
+
+		_cpDefinitionVirtualSettingLocalService = cpDefinitionVirtualSettingLocalService;
+	}
+
+	@Reference(unbind = "-")
 	protected void setCPInstanceLocalService(
 		CPInstanceLocalService cpInstanceLocalService) {
 
@@ -217,6 +249,7 @@ public class CPDefinitionPermission implements BaseModelPermissionChecker {
 		_cpDefinitionOptionRelLocalService;
 	private static CPDefinitionOptionValueRelLocalService
 		_cpDefinitionOptionValueRelLocalService;
+	private static CPDefinitionVirtualSettingLocalService _cpDefinitionVirtualSettingLocalService;
 	private static CPInstanceLocalService _cpInstanceLocalService;
 
 }
