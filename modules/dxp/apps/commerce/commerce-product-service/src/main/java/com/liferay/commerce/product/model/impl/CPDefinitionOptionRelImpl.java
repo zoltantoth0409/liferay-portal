3a5243623b9fedb3af92e83f5935ea93ab068c91
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-05-18 10:23:40 -0700

    COMMERCE-20 Add method to get the definition and option models of a rel

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPDefinitionOptionRelImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPDefinitionOptionRelImpl.java
@@ -16,14 +16,20 @@ package com.liferay.commerce.product.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPDefinitionOptionValueRel;
+import com.liferay.commerce.product.model.CPOption;
+import com.liferay.commerce.product.service.CPDefinitionLocalServiceUtil;
 import com.liferay.commerce.product.service.CPDefinitionOptionValueRelLocalServiceUtil;
+import com.liferay.commerce.product.service.CPOptionLocalServiceUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 
 import java.util.List;
 
 /**
  * @author Marco Leo
+ * @author Andrea Di Giorgi
  */
 @ProviderType
 public class CPDefinitionOptionRelImpl extends CPDefinitionOptionRelBaseImpl {
@@ -32,11 +38,22 @@ public class CPDefinitionOptionRelImpl extends CPDefinitionOptionRelBaseImpl {
 	}
 
 	@Override
+	public CPDefinition getCPDefinition() throws PortalException {
+		return CPDefinitionLocalServiceUtil.getCPDefinition(
+			getCPDefinitionId());
+	}
+
+	@Override
 	public List<CPDefinitionOptionValueRel> getCPDefinitionOptionValueRels() {
 		return CPDefinitionOptionValueRelLocalServiceUtil.
 			getCPDefinitionOptionValueRels(
 				getCPDefinitionOptionRelId(), QueryUtil.ALL_POS,
 				QueryUtil.ALL_POS);
+	}
+
+	@Override
+	public CPOption getCPOption() throws PortalException {
+		return CPOptionLocalServiceUtil.getCPOption(getCPOptionId());
 	}
 
 }
