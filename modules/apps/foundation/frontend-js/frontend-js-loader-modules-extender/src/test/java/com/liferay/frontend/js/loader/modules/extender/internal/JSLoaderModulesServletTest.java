d18f5962a69809244ca842efeace9ce21786f12f
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-05-18 13:33:47 +0200

    LPS-72576 Fix failing test

--- modules/apps/foundation/frontend-js/frontend-js-loader-modules-extender/src/test/java/com/liferay/frontend/js/loader/modules/extender/internal/JSLoaderModulesServletTest.java
+++ modules/apps/foundation/frontend-js/frontend-js-loader-modules-extender/src/test/java/com/liferay/frontend/js/loader/modules/extender/internal/JSLoaderModulesServletTest.java
@@ -18,6 +18,7 @@ import aQute.bnd.osgi.Constants;
 
 import aQute.lib.converter.Converter;
 
+import com.liferay.frontend.js.loader.modules.extender.internal.npm.NPMRegistry;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -445,6 +446,10 @@ public class JSLoaderModulesServletTest extends PowerMockito {
 
 		jsLoaderModulesServlet.setJSLoaderModulesTracker(
 			jsLoaderModulesTracker);
+
+		NPMRegistry npmRegistry = new NPMRegistry();
+
+		jsLoaderModulesServlet.setNPMRegistry(npmRegistry);
 
 		return jsLoaderModulesServlet;
 	}
