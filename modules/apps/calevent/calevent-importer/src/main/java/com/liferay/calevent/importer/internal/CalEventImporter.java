cfe269429ed67485a672844a2f50b76dd969012a
Preston-Crary <preston.crary@liferay.com>
2017-05-19 14:43:43 -0700

    LPS-72604 Remove usages of deprecated ratings APIs.

--- modules/apps/calevent/calevent-importer/src/main/java/com/liferay/calevent/importer/internal/CalEventImporter.java
+++ modules/apps/calevent/calevent-importer/src/main/java/com/liferay/calevent/importer/internal/CalEventImporter.java
@@ -1163,18 +1163,12 @@ public class CalEventImporter {
 				className, classPK, ratingsEntry.getScore());
 		}
 
-		List<Long> oldClassPKs = new ArrayList<>();
+		RatingsStats ratingsStats = _ratingsStatsLocalService.fetchStats(
+			oldClassName, oldClassPK);
 
-		oldClassPKs.add(oldClassPK);
-
-		List<RatingsStats> ratingsStatsList =
-			_ratingsStatsLocalService.getStats(oldClassName, oldClassPKs);
-
-		if (ratingsStatsList.isEmpty()) {
+		if (ratingsStats == null) {
 			return;
 		}
-
-		RatingsStats ratingsStats = ratingsStatsList.get(0);
 
 		addRatingsStats(
 			_counterLocalService.increment(), className, classPK,
