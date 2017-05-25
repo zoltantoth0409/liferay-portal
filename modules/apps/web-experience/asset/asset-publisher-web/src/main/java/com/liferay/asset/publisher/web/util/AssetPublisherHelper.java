fda6423f95838a7b876478fd6a7e663fc656cf44
Pavel Savinov <pavel.savinov@liferay.com>
2017-05-25 17:38:26 +0200

    LPS-72756 inheritRedirect prevents base find action helper from using redirect parameter from request

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherHelper.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherHelper.java
@@ -123,8 +123,6 @@ public class AssetPublisherHelper {
 					!Objects.equals(viewURL, noSuchEntryRedirect)) {
 
 					viewURL = HttpUtil.setParameter(
-						viewURL, "inheritRedirect", Boolean.TRUE);
-					viewURL = HttpUtil.setParameter(
 						viewURL, "redirect",
 						PortalUtil.getCurrentURL(liferayPortletRequest));
 				}
