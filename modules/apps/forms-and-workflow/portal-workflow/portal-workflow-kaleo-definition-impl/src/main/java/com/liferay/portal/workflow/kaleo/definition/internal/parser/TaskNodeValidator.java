f4ae770ff204fce11273703135f231458eb3a913
Inacio Nery <inacio.nery@liferay.com>
2017-05-29 15:24:37 -0300

    LPS-58297 Apply the KaleoDefinitionValidationException

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/parser/TaskNodeValidator.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/parser/TaskNodeValidator.java
@@ -14,10 +14,10 @@
 
 package com.liferay.portal.workflow.kaleo.definition.internal.parser;
 
-import com.liferay.portal.kernel.workflow.WorkflowException;
 import com.liferay.portal.workflow.kaleo.definition.Assignment;
 import com.liferay.portal.workflow.kaleo.definition.Definition;
 import com.liferay.portal.workflow.kaleo.definition.Task;
+import com.liferay.portal.workflow.kaleo.definition.exception.KaleoDefinitionValidationException;
 import com.liferay.portal.workflow.kaleo.definition.parser.NodeValidator;
 
 import java.util.Set;
@@ -36,23 +36,23 @@ public class TaskNodeValidator extends BaseNodeValidator<Task> {
 
 	@Override
 	protected void doValidate(Definition definition, Task task)
-		throws WorkflowException {
+		throws KaleoDefinitionValidationException {
 
 		if (task.getIncomingTransitionsCount() == 0) {
-			throw new WorkflowException(
-				"No incoming transition found for task " + task.getName());
+			throw new KaleoDefinitionValidationException.
+				MustSetIncomingTransition(task.getName());
 		}
 
 		if (task.getOutgoingTransitionsCount() == 0) {
-			throw new WorkflowException(
-				"No outgoing transition found for task " + task.getName());
+			throw new KaleoDefinitionValidationException.
+				MustSetOutgoingTransition(task.getName());
 		}
 
 		Set<Assignment> assignments = task.getAssignments();
 
 		if ((assignments == null) || assignments.isEmpty()) {
-			throw new WorkflowException(
-				"No assignments for task " + task.getName());
+			throw new KaleoDefinitionValidationException.MustSetAssignments(
+				task.getName());
 		}
 	}
 
