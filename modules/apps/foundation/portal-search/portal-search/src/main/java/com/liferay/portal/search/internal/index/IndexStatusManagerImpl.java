4561e41add40cb2112839aa4fcae909fdf4d47cc
Michael Han <michael.han@liferay.com>
2017-05-31 08:05:21 -0500

    LPS-72643 check thread local to determine index read only status

--- modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/index/IndexStatusManagerImpl.java
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/index/IndexStatusManagerImpl.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.search.internal.index;
 
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.search.IndexStatusManagerThreadLocal;
 import com.liferay.portal.search.configuration.IndexStatusManagerConfiguration;
 import com.liferay.portal.search.index.IndexStatusManager;
 
@@ -35,7 +36,8 @@ public class IndexStatusManagerImpl implements IndexStatusManager {
 
 	@Override
 	public boolean isIndexReadOnly() {
-		return _indexReadOnly;
+		return IndexStatusManagerThreadLocal.isIndexReadOnly() ||
+			_indexReadOnly;
 	}
 
 	@Override
