829de99bcfcd74471a29a38549a2b7fcbb4c200e
Preston-Crary <preston.crary@liferay.com>
2017-06-01 13:59:49 -0700

    LPS-72883 Implement default method.

--- modules/apps/collaboration/message-boards/message-boards-comment/src/main/java/com/liferay/message/boards/comment/internal/MBDiscussionPermissionImpl.java
+++ modules/apps/collaboration/message-boards/message-boards-comment/src/main/java/com/liferay/message/boards/comment/internal/MBDiscussionPermissionImpl.java
@@ -14,7 +14,9 @@
 
 package com.liferay.message.boards.comment.internal;
 
+import com.liferay.message.boards.kernel.model.MBMessage;
 import com.liferay.portal.kernel.comment.BaseDiscussionPermission;
+import com.liferay.portal.kernel.comment.Comment;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
@@ -45,6 +47,23 @@ public class MBDiscussionPermissionImpl extends BaseDiscussionPermission {
 
 		return MBDiscussionPermission.contains(
 			_permissionChecker, commentId, actionId);
+	}
+
+	@Override
+	public boolean hasPermission(Comment comment, String actionId)
+		throws PortalException {
+
+		if (comment instanceof MBCommentImpl) {
+			MBCommentImpl mbCommentImpl = (MBCommentImpl)comment;
+
+			MBMessage mbMessage = mbCommentImpl.getMessage();
+
+			return MBDiscussionPermission.contains(
+				_permissionChecker, mbMessage, actionId);
+		}
+
+		return MBDiscussionPermission.contains(
+			_permissionChecker, comment.getCommentId(), actionId);
 	}
 
 	@Override
