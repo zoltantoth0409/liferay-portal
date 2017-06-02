a697db6c265bd06e089956a7ab22f766202b6b46
drewbrokke <drew.brokke@liferay.com>
2017-06-02 10:26:43 -0700

    LPS-69208 document-library-api - adds DLFileEntryConfiguration.java

---
+++ modules/apps/collaboration/document-library/document-library-api/src/main/java/com/liferay/document/library/configuration/DLFileEntryConfiguration.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.configuration;
+
+import aQute.bnd.annotation.ProviderType;
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Drew Brokke
+ */
+@ExtendedObjectClassDefinition(category = "collaboration")
+@Meta.OCD(
+	id = "com.liferay.document.library.configuration.DLFileEntryConfiguration",
+	localization = "content/Language", name = "dl-file-entry-configuration-name"
+)
+@ProviderType
+public interface DLFileEntryConfiguration {
+
+	@Meta.AD(
+		deflt = "104857600",
+		description = "previewable-processor-max-size-help", required = false
+	)
+	public long previewableProcessorMaxSize();
+
+}
