7822caf51af8f7f6e937d053cce3352c615cc5e4
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-06-05 10:02:44 -0700

    COMMERCE-20 Add util methods

--- modules/dxp/apps/commerce/commerce-product-type-virtual-service/src/main/java/com/liferay/commerce/product/type/virtual/model/impl/CPDefinitionVirtualSettingImpl.java
+++ modules/dxp/apps/commerce/commerce-product-type-virtual-service/src/main/java/com/liferay/commerce/product/type/virtual/model/impl/CPDefinitionVirtualSettingImpl.java
@@ -16,14 +16,84 @@ package com.liferay.commerce.product.type.virtual.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionLocalServiceUtil;
+import com.liferay.document.library.kernel.service.DLAppLocalServiceUtil;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+
 /**
  * @author Marco Leo
+ * @author Andrea Di Giorgi
  */
 @ProviderType
 public class CPDefinitionVirtualSettingImpl
 	extends CPDefinitionVirtualSettingBaseImpl {
 
 	public CPDefinitionVirtualSettingImpl() {
+	}
+
+	@Override
+	public CPDefinition getCPDefinition() throws PortalException {
+		return CPDefinitionLocalServiceUtil.getCPDefinition(
+			getCPDefinitionId());
+	}
+
+	@Override
+	public FileEntry getFileEntry() throws PortalException {
+		if (isUseUrl()) {
+			return null;
+		}
+
+		return DLAppLocalServiceUtil.getFileEntry(getFileEntryId());
+	}
+
+	@Override
+	public FileEntry getSampleFileEntry() throws PortalException {
+		if (isUseSampleUrl()) {
+			return null;
+		}
+
+		return DLAppLocalServiceUtil.getFileEntry(getSampleFileEntryId());
+	}
+
+	@Override
+	public JournalArticle getTermsOfUseJournalArticle() throws PortalException {
+		if (!isUseTermsOfUseJournal()) {
+			return null;
+		}
+
+		return JournalArticleLocalServiceUtil.getLatestArticle(
+			getTermsOfUseJournalArticleResourcePrimKey());
+	}
+
+	@Override
+	public boolean isUseSampleUrl() {
+		if (getSampleFileEntryId() > 0) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean isUseTermsOfUseJournal() {
+		if (getTermsOfUseJournalArticleResourcePrimKey() > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isUseUrl() {
+		if (getFileEntryId() > 0) {
+			return false;
+		}
+
+		return true;
 	}
 
 }
