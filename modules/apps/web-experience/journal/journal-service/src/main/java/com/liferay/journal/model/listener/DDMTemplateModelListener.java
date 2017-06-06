70eefa062b59c3c54ef70ea795b107d5b53236a1
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2017-06-06 13:15:14 +0200

    LPS-72906 Add template model listener to clear page cache on remove/update.

---
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/model/listener/DDMTemplateModelListener.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.model.listener;
+
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.servlet.filters.cache.CacheUtil;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class DDMTemplateModelListener extends BaseModelListener<DDMTemplate> {
+
+	@Override
+	public void onAfterRemove(DDMTemplate ddmTemplate) {
+		clearCache(ddmTemplate);
+	}
+
+	@Override
+	public void onAfterUpdate(DDMTemplate ddmTemplate) {
+		clearCache(ddmTemplate);
+	}
+
+	protected void clearCache(DDMTemplate ddmTemplate) {
+		if (ddmTemplate == null) {
+			return;
+		}
+
+		// Layout cache
+
+		CacheUtil.clearCache(ddmTemplate.getCompanyId());
+	}
+
+}
