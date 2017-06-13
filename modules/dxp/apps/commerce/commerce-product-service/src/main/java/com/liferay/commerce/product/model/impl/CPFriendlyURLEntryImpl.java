9ab8233ee700d1b55cee16ef5c1a6f5fa4ab14b9
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-06-13 08:35:04 -0700

    COMMERCE-87 Remove generated comments

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPFriendlyURLEntryImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPFriendlyURLEntryImpl.java
@@ -17,22 +17,11 @@ package com.liferay.commerce.product.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the CPFriendlyURLEntry service. Represents a row in the &quot;CPFriendlyURLEntry&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.commerce.product.model.CPFriendlyURLEntry} interface.
- * </p>
- *
  * @author Marco Leo
  */
 @ProviderType
 public class CPFriendlyURLEntryImpl extends CPFriendlyURLEntryBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a cp friendly url entry model instance should use the {@link com.liferay.commerce.product.model.CPFriendlyURLEntry} interface instead.
-	 */
 	public CPFriendlyURLEntryImpl() {
 	}
 
