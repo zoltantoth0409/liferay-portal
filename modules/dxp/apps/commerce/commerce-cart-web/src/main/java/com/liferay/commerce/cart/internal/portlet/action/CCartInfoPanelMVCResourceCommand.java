f9c6bff8d5d9a01a876baec3d5c7e3dcb7839ac2
Alessio Rendina <asilcomando@gmail.com>
2017-06-19 17:30:20 +0200

    COMMERCE-31 Added info panel

---
+++ modules/dxp/apps/commerce/commerce-cart-web/src/main/java/com/liferay/commerce/cart/internal/portlet/action/CCartInfoPanelMVCResourceCommand.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.cart.internal.portlet.action;
+
+import com.liferay.commerce.cart.constants.CCartPortletKeys;
+import com.liferay.commerce.cart.constants.CCartWebKeys;
+import com.liferay.commerce.cart.model.CCart;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+import java.util.List;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CCartPortletKeys.COMMERCE_CART_ADMIN,
+		"mvc.command.name=cCartInfoPanel"
+	},
+	service = MVCResourceCommand.class
+)
+public class CCartInfoPanelMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		List<CCart> cCarts = _actionHelper.getCCarts(resourceRequest);
+
+		resourceRequest.setAttribute(CCartWebKeys.C_CARTS, cCarts);
+
+		include(resourceRequest, resourceResponse, "/cart_info_panel.jsp");
+	}
+
+	@Reference
+	private ActionHelper _actionHelper;
+
+}
---
+++ modules/dxp/apps/commerce/commerce-cart-web/src/main/resources/META-INF/resources/cart_info_panel.jsp
@@ -0,0 +1,74 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+List<CCart> cCarts = (List<CCart>)request.getAttribute(CCartWebKeys.C_CARTS);
+
+if (ListUtil.isEmpty(cCarts)) {
+	cCarts = Collections.emptyList();
+}
+%>
+
+<c:choose>
+	<c:when test="<%= cCarts.size() == 1 %>">
+
+		<%
+		CCart cCart = cCarts.get(0);
+
+		request.setAttribute("info_panel.jsp-entry", cCart);
+		%>
+
+		<div class="sidebar-header">
+			<ul class="sidebar-header-actions">
+				<li>
+					<liferay-util:include
+						page="/cart_action.jsp"
+						servletContext="<%= application %>"
+					/>
+				</li>
+			</ul>
+
+			<h4><%= HtmlUtil.escape(cCart.getTitle()) %></h4>
+		</div>
+
+		<aui:nav-bar markupView="lexicon">
+			<aui:nav cssClass="navbar-nav">
+				<aui:nav-item label="details" selected="<%= true %>" />
+			</aui:nav>
+		</aui:nav-bar>
+
+		<div class="sidebar-body">
+			<h5><liferay-ui:message key="id" /></h5>
+
+			<p>
+				<%= HtmlUtil.escape(String.valueOf(cCart.getCCartId())) %>
+			</p>
+		</div>
+	</c:when>
+	<c:otherwise>
+		<aui:nav-bar>
+			<aui:nav cssClass="navbar-nav">
+				<aui:nav-item label="details" selected="<%= true %>" />
+			</aui:nav>
+		</aui:nav-bar>
+
+		<div class="sidebar-header">
+			<h4><liferay-ui:message arguments="<%= cCarts.size() %>" key="x-items-are-selected" /></h4>
+		</div>
+	</c:otherwise>
+</c:choose>
