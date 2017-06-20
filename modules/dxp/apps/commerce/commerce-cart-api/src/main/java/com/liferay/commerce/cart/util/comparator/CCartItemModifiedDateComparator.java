49b05c6dd28a0b58878ff79562b5aa48afb7fe48
Alessio Rendina <asilcomando@gmail.com>
2017-06-20 14:30:47 +0200

    COMMERCE-31 Added order by modified date

---
+++ modules/dxp/apps/commerce/commerce-cart-api/src/main/java/com/liferay/commerce/cart/util/comparator/CCartItemModifiedDateComparator.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.cart.util.comparator;
+
+import com.liferay.commerce.cart.model.CCartItem;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CCartItemModifiedDateComparator
+	extends OrderByComparator<CCartItem> {
+
+	public static final String ORDER_BY_ASC = "modifiedDate ASC";
+
+	public static final String ORDER_BY_DESC = "modifiedDate DESC";
+
+	public static final String[] ORDER_BY_FIELDS = {"modifiedDate"};
+
+	public CCartItemModifiedDateComparator() {
+		this(false);
+	}
+
+	public CCartItemModifiedDateComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	@Override
+	public int compare(CCartItem cCartItem1, CCartItem cCartItem2) {
+		int value = DateUtil.compareTo(
+			cCartItem1.getModifiedDate(), cCartItem2.getModifiedDate());
+
+		if (_ascending) {
+			return value;
+		}
+		else {
+			return -value;
+		}
+	}
+
+	@Override
+	public String getOrderBy() {
+		if (_ascending) {
+			return ORDER_BY_ASC;
+		}
+		else {
+			return ORDER_BY_DESC;
+		}
+	}
+
+	@Override
+	public String[] getOrderByFields() {
+		return ORDER_BY_FIELDS;
+	}
+
+	@Override
+	public boolean isAscending() {
+		return _ascending;
+	}
+
+	private final boolean _ascending;
+
+}
