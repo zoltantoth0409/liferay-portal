8d9cb155ba044a88702f0eed9ddb6bb00e782a2e
drewbrokke <drew.brokke@liferay.com>
2017-06-20 13:46:22 -0700

    LPS-73119 portal-configuration-test-util - adds ConfigurationTemporarySwapper.java and Exceptions

---
+++ modules/apps/foundation/portal-configuration/portal-configuration-test-util/src/main/java/com/liferay/portal/configuration/test/util/ConfigurationTemporarySwapperException.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configuration.test.util;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Drew Brokke
+ */
+public class ConfigurationTemporarySwapperException extends PortalException {
+
+	public ConfigurationTemporarySwapperException(String msg) {
+		super(msg);
+	}
+
+	public static class MustFindService
+		extends ConfigurationTemporarySwapperException {
+
+		public MustFindService(String serviceClass) {
+			super(
+				String.format(
+					"No service found that implements interface \"%s\"",
+					serviceClass));
+
+			this.serviceClass = serviceClass;
+		}
+
+		public final String serviceClass;
+
+	}
+
+	public static class ServiceMustConsumeConfiguration
+		extends ConfigurationTemporarySwapperException {
+
+		public ServiceMustConsumeConfiguration(
+			String componentName, String pid) {
+
+			super(
+				String.format(
+					"Component \"%s\" does not consume configuration \"%s\"",
+					componentName, pid));
+
+			this.componentName = componentName;
+			this.pid = pid;
+		}
+
+		public final String componentName;
+		public final String pid;
+
+	}
+
+	public static class ServiceMustHaveBundle
+		extends ConfigurationTemporarySwapperException {
+
+		public ServiceMustHaveBundle(String serviceClass) {
+			super(
+				String.format(
+					"No bundle found for the service \"%s\"", serviceClass));
+
+			this.serviceClass = serviceClass;
+		}
+
+		public final String serviceClass;
+
+	}
+
+}
