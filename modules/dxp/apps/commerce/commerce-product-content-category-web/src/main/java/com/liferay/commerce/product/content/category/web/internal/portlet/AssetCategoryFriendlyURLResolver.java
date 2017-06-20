d7aeee3cc36c8c0bafe90980b66e8f8ee82aafae
Marco Leo <marco.leo@liferay.com>
2017-06-20 15:25:38 +0200

    COMMERCE-86 Add Category url resolver

---
+++ modules/dxp/apps/commerce/commerce-product-content-category-web/src/main/java/com/liferay/commerce/product/content/category/web/internal/portlet/AssetCategoryFriendlyURLResolver.java
@@ -0,0 +1,221 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.content.category.web.internal.portlet;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.AssetCategoryService;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
+import com.liferay.commerce.product.constants.CPConstants;
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPDisplayLayout;
+import com.liferay.commerce.product.model.CPFriendlyURLEntry;
+import com.liferay.commerce.product.service.CPDisplayLayoutLocalService;
+import com.liferay.commerce.product.service.CPFriendlyURLEntryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutFriendlyURLComposite;
+import com.liferay.portal.kernel.portlet.FriendlyURLResolver;
+import com.liferay.portal.kernel.service.LayoutService;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.InheritableMap;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Marco Leo
+ */
+@Component(service = FriendlyURLResolver.class)
+public class AssetCategoryFriendlyURLResolver implements FriendlyURLResolver {
+
+	@Override
+	public String getActualURL(
+			long companyId, long groupId, boolean privateLayout,
+			String mainPath, String friendlyURL, Map<String, String[]> params,
+			Map<String, Object> requestContext)
+		throws PortalException {
+
+		HttpServletRequest request = (HttpServletRequest)requestContext.get(
+			"request");
+
+		Locale locale = _portal.getLocale(request);
+
+		String languageId = LanguageUtil.getLanguageId(locale);
+
+		String urlTitle = friendlyURL.substring(
+			CPConstants.ASSET_CATEGORY_URL_SEPARATOR.length());
+
+		long classNameId = _portal.getClassNameId(AssetCategory.class);
+
+		CPFriendlyURLEntry cpFriendlyURLEntry =
+			_cpFriendlyURLEntryLocalService.getCPFriendlyURLEntry(
+				groupId, companyId, classNameId, languageId, urlTitle);
+
+		if (!cpFriendlyURLEntry.isMain()) {
+			cpFriendlyURLEntry =
+				_cpFriendlyURLEntryLocalService.fetchCPFriendlyURLEntry(
+					groupId, companyId, classNameId,
+					cpFriendlyURLEntry.getPrimaryKey(), languageId, true);
+		}
+
+		AssetCategory assetCategory = _assetCategoryService.fetchCategory(
+			cpFriendlyURLEntry.getClassPK());
+
+		if(assetCategory == null){
+			return null;
+		}
+
+		Layout layout = getAssetCategoryLayout(
+			groupId, privateLayout, cpFriendlyURLEntry.getClassPK());
+
+		String layoutActualURL = _portal.getLayoutActualURL(layout, mainPath);
+
+		InheritableMap<String, String[]> actualParams = new InheritableMap<>();
+
+		if (params != null) {
+			actualParams.setParentMap(params);
+		}
+
+		actualParams.put("p_p_id", new String[] {CPPortletKeys.CP_CATEGORY_CONTENT_WEB});
+		actualParams.put("p_p_lifecycle", new String[] {"0"});
+		actualParams.put("p_p_mode", new String[] {"view"});
+
+		request.setAttribute(WebKeys.ASSET_CATEGORY, assetCategory);
+
+		String queryString = _http.parameterMapToString(actualParams, false);
+
+		if (layoutActualURL.contains(StringPool.QUESTION)) {
+			layoutActualURL =
+				layoutActualURL + StringPool.AMPERSAND + queryString;
+		}
+		else {
+			layoutActualURL =
+				layoutActualURL + StringPool.QUESTION + queryString;
+		}
+
+		_portal.addPageSubtitle(assetCategory.getTitle(languageId), request);
+		_portal.addPageDescription(
+			assetCategory.getDescription(languageId), request);
+
+		List<AssetTag> assetTags = _assetTagLocalService.getTags(
+			AssetCategory.class.getName(), assetCategory.getPrimaryKey());
+
+		if (!assetTags.isEmpty()) {
+			_portal.addPageKeywords(
+				ListUtil.toString(assetTags, AssetTag.NAME_ACCESSOR), request);
+		}
+
+		return layoutActualURL;
+	}
+
+	@Override
+	public LayoutFriendlyURLComposite getLayoutFriendlyURLComposite(
+			long companyId, long groupId, boolean privateLayout,
+			String friendlyURL, Map<String, String[]> params,
+			Map<String, Object> requestContext)
+		throws PortalException {
+
+		HttpServletRequest request = (HttpServletRequest)requestContext.get(
+			"request");
+
+		Locale locale = _portal.getLocale(request);
+
+		String languageId = LanguageUtil.getLanguageId(locale);
+
+		String urlTitle = friendlyURL.substring(
+			CPConstants.ASSET_CATEGORY_URL_SEPARATOR.length());
+		long classNameId = _portal.getClassNameId(AssetCategory.class);
+
+		CPFriendlyURLEntry cpFriendlyURLEntry =
+			_cpFriendlyURLEntryLocalService.getCPFriendlyURLEntry(
+				groupId, companyId, classNameId, languageId, urlTitle);
+
+		if (!cpFriendlyURLEntry.isMain()) {
+			cpFriendlyURLEntry =
+				_cpFriendlyURLEntryLocalService.fetchCPFriendlyURLEntry(
+					groupId, companyId, classNameId,
+					cpFriendlyURLEntry.getPrimaryKey(), languageId, true);
+		}
+
+		AssetCategory assetCategory = _assetCategoryService.fetchCategory(
+			cpFriendlyURLEntry.getClassPK());
+
+		if(assetCategory == null){
+			return null;
+		}
+
+		Layout layout = getAssetCategoryLayout(
+			groupId, privateLayout, cpFriendlyURLEntry.getClassPK());
+
+		return new LayoutFriendlyURLComposite(
+			layout, getURLSeparator() + cpFriendlyURLEntry.getUrlTitle());
+	}
+
+	@Override
+	public String getURLSeparator() {
+		return CPConstants.ASSET_CATEGORY_URL_SEPARATOR;
+	}
+
+	protected Layout getAssetCategoryLayout(
+			long groupId, boolean privateLayout, long categoryId)
+		throws PortalException {
+
+		CPDisplayLayout cpDisplayLayout =
+			_cpDisplayLayoutLocalService.fetchCPDisplayLayout(
+				AssetCategory.class, categoryId);
+
+		if (cpDisplayLayout == null) {
+			return null;
+		}
+
+		String layoutUuid =  cpDisplayLayout.getLayoutUuid();
+
+		return _layoutService.getLayoutByUuidAndGroupId(
+			layoutUuid, groupId, privateLayout);
+	}
+
+	@Reference
+	private AssetCategoryService _assetCategoryService;
+
+	@Reference
+	private AssetTagLocalService _assetTagLocalService;
+
+	@Reference
+	private CPDisplayLayoutLocalService _cpDisplayLayoutLocalService;
+
+	@Reference
+	private CPFriendlyURLEntryLocalService _cpFriendlyURLEntryLocalService;
+
+	@Reference
+	private Http _http;
+
+	@Reference
+	private LayoutService _layoutService;
+
+	@Reference
+	private Portal _portal;
+
+}
