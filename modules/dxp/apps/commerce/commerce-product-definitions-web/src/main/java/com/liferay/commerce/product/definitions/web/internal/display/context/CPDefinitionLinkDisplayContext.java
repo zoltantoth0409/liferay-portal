1f04780c3c4a79e19fe1e012a781c017cb7ec052
Alessio Rendina <asilcomando@gmail.com>
2017-06-21 15:35:01 +0200

    COMMERCE-108 Added new display context

---
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/display/context/CPDefinitionLinkDisplayContext.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.display.context;
+
+import com.liferay.commerce.product.definitions.web.display.context.BaseCPDefinitionsSearchContainerDisplayContext;
+import com.liferay.commerce.product.definitions.web.internal.util.CPDefinitionsPortletUtil;
+import com.liferay.commerce.product.definitions.web.portlet.action.ActionHelper;
+import com.liferay.commerce.product.item.selector.criterion.CPDefinitionItemSelectorCriterion;
+import com.liferay.commerce.product.model.CPDefinitionLink;
+import com.liferay.commerce.product.model.CPDefinitionOptionRel;
+import com.liferay.commerce.product.service.CPDefinitionLinkService;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletURL;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CPDefinitionLinkDisplayContext extends
+	BaseCPDefinitionsSearchContainerDisplayContext<CPDefinitionLink> {
+
+	public CPDefinitionLinkDisplayContext(
+			ActionHelper actionHelper, HttpServletRequest httpServletRequest,
+			CPDefinitionLinkService cpDefinitionLinkService, ItemSelector itemSelector)
+		throws PortalException {
+
+		super(
+			actionHelper, httpServletRequest,
+			CPDefinitionOptionRel.class.getSimpleName());
+
+		setDefaultOrderByCol("priority");
+
+		_cpDefinitionLinkService = cpDefinitionLinkService;
+		_itemSelector = itemSelector;
+	}
+
+	public CPDefinitionLink getCPDefinitionLink() throws PortalException {
+		if (_cpDefinitionLink != null) {
+			return _cpDefinitionLink;
+		}
+
+		_cpDefinitionLink = actionHelper.getCPDefinitionLink(
+			cpRequestHelper.getRenderRequest());
+
+		return _cpDefinitionLink;
+	}
+
+	public long getCPDefinitionLinkId() throws PortalException {
+		CPDefinitionLink cpDefinitionLink = getCPDefinitionLink();
+
+		if (cpDefinitionLink == null) {
+			return 0;
+		}
+
+		return cpDefinitionLink.getCPDefinitionLinkId();
+	}
+
+	public String getItemSelectorUrl() {
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory =
+			RequestBackedPortletURLFactoryUtil.create(
+				cpRequestHelper.getRenderRequest());
+
+		List<ItemSelectorReturnType> desiredItemSelectorReturnTypes =
+			new ArrayList<>();
+
+		desiredItemSelectorReturnTypes.add(new UUIDItemSelectorReturnType());
+
+		CPDefinitionItemSelectorCriterion cpDefinitionItemSelectorCriterion =
+			new CPDefinitionItemSelectorCriterion();
+
+		cpDefinitionItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
+			desiredItemSelectorReturnTypes);
+
+		PortletURL itemSelectorURL = _itemSelector.getItemSelectorURL(
+			requestBackedPortletURLFactory, "productDefinitionsSelectItem",
+			cpDefinitionItemSelectorCriterion);
+
+		return itemSelectorURL.toString();
+	}
+
+	@Override
+	public PortletURL getPortletURL() throws PortalException {
+		PortletURL portletURL = super.getPortletURL();
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "viewCPDefinitionLinks");
+
+		return portletURL;
+	}
+
+	@Override
+	public SearchContainer<CPDefinitionLink> getSearchContainer()
+		throws PortalException {
+
+		if (searchContainer != null) {
+			return searchContainer;
+		}
+
+		searchContainer = new SearchContainer<>(
+			liferayPortletRequest, getPortletURL(), null, null);
+
+		searchContainer.setEmptyResultsMessage(
+			"no-related-products-were-found");
+
+		OrderByComparator<CPDefinitionLink> orderByComparator =
+			CPDefinitionsPortletUtil.getCPDefinitionLinkOrderByComparator(
+				getOrderByCol(), getOrderByType());
+
+		searchContainer.setOrderByCol(getOrderByCol());
+		searchContainer.setOrderByComparator(orderByComparator);
+		searchContainer.setOrderByType(getOrderByType());
+		searchContainer.setRowChecker(getRowChecker());
+
+		int total = _cpDefinitionLinkService.getCPDefinitionLinksCount(
+			getCPDefinitionId());
+
+		searchContainer.setTotal(total);
+
+		List<CPDefinitionLink> results =
+			_cpDefinitionLinkService.getCPDefinitionLinks(
+				getCPDefinitionId(), searchContainer.getStart(),
+				searchContainer.getEnd(), orderByComparator);
+
+		searchContainer.setResults(results);
+
+		return searchContainer;
+	}
+
+	private CPDefinitionLink _cpDefinitionLink;
+	private final CPDefinitionLinkService _cpDefinitionLinkService;
+	private final ItemSelector _itemSelector;
+
+}
