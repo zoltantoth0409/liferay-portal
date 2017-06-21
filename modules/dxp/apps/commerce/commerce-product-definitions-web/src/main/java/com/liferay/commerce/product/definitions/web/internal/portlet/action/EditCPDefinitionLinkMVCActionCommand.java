afa7f6ce3e5442b7a78e858ab1f1b59d50f7a182
Alessio Rendina <asilcomando@gmail.com>
2017-06-21 16:18:17 +0200

    COMMERCE-108 Added edit definition link render and action commands

---
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/portlet/action/EditCPDefinitionLinkMVCActionCommand.java
@@ -0,0 +1,172 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.portlet.action;
+
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPDefinitionLink;
+import com.liferay.commerce.product.model.CPDefinitionOptionRel;
+import com.liferay.commerce.product.service.CPDefinitionLinkService;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletURL;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CPPortletKeys.COMMERCE_PRODUCT_DEFINITIONS,
+		"mvc.command.name=editCPDefinitionLink"
+	},
+	service = MVCActionCommand.class
+)
+public class EditCPDefinitionLinkMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	protected void addCPDefinitionLinks(ActionRequest actionRequest)
+		throws Exception {
+
+		long[] addCPDefinitionIds = null;
+
+		long cpDefinitionId = ParamUtil.getLong(
+			actionRequest, "cpDefinitionId");
+		long cpDefinitionLinkId = ParamUtil.getLong(actionRequest, "cpDefinitionLinkId");
+
+		if (cpDefinitionLinkId > 0) {
+			addCPDefinitionIds = new long[] {cpDefinitionLinkId};
+		}
+		else {
+			addCPDefinitionIds = StringUtil.split(
+				ParamUtil.getString(actionRequest, "cpDefinitionIds"), 0L);
+		}
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			CPDefinitionOptionRel.class.getName(), actionRequest);
+
+		_cpDefinitionLinkService.updateCPDefinitionLinks(
+			cpDefinitionId, addCPDefinitionIds, 0, serviceContext);
+	}
+
+	protected void deleteCPDefinitionLinks(ActionRequest actionRequest)
+		throws Exception {
+
+		long[] deleteCPDefinitionLinkIds = null;
+
+		long cpDefinitionLinkId = ParamUtil.getLong(
+			actionRequest, "cpDefinitionLinkId");
+
+		if (cpDefinitionLinkId > 0) {
+			deleteCPDefinitionLinkIds = new long[] {cpDefinitionLinkId};
+		}
+		else {
+			deleteCPDefinitionLinkIds = StringUtil.split(
+				ParamUtil.getString(
+					actionRequest, "deleteCPDefinitionLinkIds"), 0L);
+		}
+
+		for (long deleteCPDefinitionLinkId : deleteCPDefinitionLinkIds) {
+			_cpDefinitionLinkService.deleteCPDefinitionLink(
+				deleteCPDefinitionLinkId);
+		}
+
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		long cpDefinitionId = ParamUtil.getLong(
+			actionRequest, "cpDefinitionId");
+
+		String redirect = ParamUtil.getString(actionRequest, "redirect");
+		String backURL = ParamUtil.getString(
+			actionRequest, "backURL", redirect);
+
+		redirect = getSaveAndContinueRedirect(
+			actionRequest, cpDefinitionId, redirect, backURL);
+
+		if (cmd.equals(Constants.ADD)) {
+			addCPDefinitionLinks(actionRequest);
+
+			sendRedirect(actionRequest, actionResponse, redirect);
+		}
+		else if (cmd.equals(Constants.UPDATE)) {
+			updateCPDefinitionLink(actionRequest);
+
+			sendRedirect(actionRequest, actionResponse, redirect);
+		}
+		else if (cmd.equals(Constants.DELETE)) {
+			deleteCPDefinitionLinks(actionRequest);
+		}
+	}
+
+	protected String getSaveAndContinueRedirect(
+			ActionRequest actionRequest, long cpDefinitionId, String redirect,
+			String backURL)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PortletURL portletURL = PortletProviderUtil.getPortletURL(
+			actionRequest, themeDisplay.getScopeGroup(),
+			CPDefinition.class.getName(), PortletProvider.Action.EDIT);
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "viewCPDefinitionLinks");
+		portletURL.setParameter("redirect", redirect);
+		portletURL.setParameter("backURL", backURL);
+		portletURL.setParameter(
+			"cpDefinitionId", String.valueOf(cpDefinitionId));
+
+		return portletURL.toString();
+	}
+
+	protected CPDefinitionLink updateCPDefinitionLink(
+			ActionRequest actionRequest)
+		throws Exception {
+
+		long cpDefinitionLinkId = ParamUtil.getLong(
+			actionRequest, "cpDefinitionLinkId");
+
+		double priority = ParamUtil.getDouble(actionRequest, "priority");
+
+		return _cpDefinitionLinkService.updateCPDefinitionLink(
+			cpDefinitionLinkId, priority);
+	}
+
+	@Reference
+	private CPDefinitionLinkService _cpDefinitionLinkService;
+
+}
