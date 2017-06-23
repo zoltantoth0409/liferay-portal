7e51f85ef10ba013e9bbfbbebf5c66e7912c12b4
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-06-23 11:42:02 -0700

    COMMERCE-108 Remove generated comments

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPDefinitionLinkImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPDefinitionLinkImpl.java
@@ -20,22 +20,11 @@ import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.service.CPDefinitionLocalServiceUtil;
 
 /**
- * The extended model implementation for the CPDefinitionLink service. Represents a row in the &quot;CPDefinitionLink&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.commerce.product.model.CPDefinitionLink} interface.
- * </p>
- *
  * @author Alessio Antonio Rendina
  */
 @ProviderType
 public class CPDefinitionLinkImpl extends CPDefinitionLinkBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a cp definition link model instance should use the {@link com.liferay.commerce.product.model.CPDefinitionLink} interface instead.
-	 */
 	public CPDefinitionLinkImpl() {
 	}
 
