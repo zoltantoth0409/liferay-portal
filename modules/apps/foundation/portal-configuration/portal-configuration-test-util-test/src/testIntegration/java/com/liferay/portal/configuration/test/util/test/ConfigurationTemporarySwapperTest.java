050893b148de0fcf6f4dcb26221edad21193cef1
shuyangzhou <shuyang.zhou@liferay.com>
2017-06-22 19:16:42 -0700

    LPS-73119 Remove redundant assert

--- modules/apps/foundation/portal-configuration/portal-configuration-test-util-test/src/testIntegration/java/com/liferay/portal/configuration/test/util/test/ConfigurationTemporarySwapperTest.java
+++ modules/apps/foundation/portal-configuration/portal-configuration-test-util-test/src/testIntegration/java/com/liferay/portal/configuration/test/util/test/ConfigurationTemporarySwapperTest.java
@@ -195,8 +195,6 @@ public class ConfigurationTemporarySwapperTest {
 
 			Assert.assertNotNull(testProperties);
 
-			Assert.assertNotNull(testProperties.get(testKey));
-
 			Assert.assertEquals(testValue, testProperties.get(testKey));
 		}
 	}
