c75a7d0017c630317928b4c2cdd2d6bcf9659ffd
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2017-06-26 11:01:11 -0300

    LPS-73047 Revert "LPS-42478 Set context classloader to null to make hibernate use its own classloader rather than context classloader"
    This reverts commit c42ad11e2d66337dfa6bca4bcf14f977b1424946.

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/QueryImpl.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/QueryImpl.java
@@ -21,7 +21,6 @@ import com.liferay.portal.kernel.dao.orm.Query;
 import com.liferay.portal.kernel.dao.orm.ScrollableResults;
 import com.liferay.portal.kernel.security.pacl.DoPrivileged;
 import com.liferay.portal.kernel.security.pacl.NotPrivileged;
-import com.liferay.portal.kernel.util.ClassLoaderUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 
@@ -115,11 +114,6 @@ public class QueryImpl implements Query {
 	public List<?> list(boolean copy, boolean unmodifiable)
 		throws ORMException {
 
-		ClassLoader contextClassLoader =
-			ClassLoaderUtil.getContextClassLoader();
-
-		ClassLoaderUtil.setContextClassLoader(null);
-
 		try {
 			List<?> list = _query.list();
 
@@ -135,27 +129,16 @@ public class QueryImpl implements Query {
 		catch (Exception e) {
 			throw ExceptionTranslator.translate(e);
 		}
-		finally {
-			ClassLoaderUtil.setContextClassLoader(contextClassLoader);
-		}
 	}
 
 	@NotPrivileged
 	@Override
 	public ScrollableResults scroll() throws ORMException {
-		ClassLoader contextClassLoader =
-			ClassLoaderUtil.getContextClassLoader();
-
-		ClassLoaderUtil.setContextClassLoader(null);
-
 		try {
 			return new ScrollableResultsImpl(_query.scroll());
 		}
 		catch (Exception e) {
 			throw ExceptionTranslator.translate(e);
-		}
-		finally {
-			ClassLoaderUtil.setContextClassLoader(contextClassLoader);
 		}
 	}
 
