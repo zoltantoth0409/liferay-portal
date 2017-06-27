fb879a45714215a7d55f432c89b07d3694c2be71
Pablo Molina <p2kmgcl@gmail.com>
2017-06-27 15:01:19 +0200

    LPS-72641 added manual focus to tag input
    The size of the generated input field makes difficult adding new
    tags when a mouse click is needed. With this behaviour the user can
    click anywhere around the input field and the real input will be
    focused.

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/resources/META-INF/resources/js/TagSelector.es.js
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/resources/META-INF/resources/js/TagSelector.es.js
@@ -16,13 +16,15 @@ class TagSelector extends Component {
 		AUI().use(
 			'liferay-asset-taglib-tags-selector', 
 			function(A) {
+				this.input_ = this.element.querySelector('input[type="text"]');
+
 				const config = {
 					allowAddEntry: true,
 					contentBox: this.element,
 					eventName: this.eventName,
 					groupIds: this.groupIds,
 					hiddenInput: `#${this.element.querySelector('input[type="hidden"]').getAttribute('id')}`,
-					input: `#${this.element.querySelector('input[type="text"]').getAttribute('id')}`,
+					input: `#${this.input_.getAttribute('id')}`,
 					portletURL: this.tagSelectorURL,
 					tagNames: this.rule.queryValues || ''
 				};
@@ -31,6 +33,7 @@ class TagSelector extends Component {
 
 				const entries = this.tagsSelector_.entries;
 
+				this.element.addEventListener('click', this.focusTagInput_.bind(this));
 				entries.after('add', this.onEntriesChanged_, this);
 				entries.after('remove', this.onEntriesChanged_, this);
 				
@@ -46,6 +49,16 @@ class TagSelector extends Component {
 	 */
 	onEntriesChanged_() {
 		this.rule.queryValues = this.tagsSelector_.entries.keys.join();
+	}
+
+	/**
+	 * Focuses the input field using for adding new tags.
+	 * This method uses the private input_ attribute, which is
+	 * stablished inside rendered() method.
+	 * @private
+	 */
+	focusTagInput_() {
+		this.input_.focus();
 	}
 }
 
