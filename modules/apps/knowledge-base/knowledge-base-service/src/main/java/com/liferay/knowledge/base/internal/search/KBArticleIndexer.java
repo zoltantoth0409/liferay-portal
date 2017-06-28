843161755dbe8f14274212b48265d21470572fbf
Bryan Engler <bryan.engler@liferay.com>
2017-06-28 11:53:41 -0700

    LPS-72670 knowledge-base-service: Use pre-highlighted snippets from search engine
    https://issues.liferay.com/browse/LPS-72670

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/search/KBArticleIndexer.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/search/KBArticleIndexer.java
@@ -48,7 +48,7 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
-import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
@@ -150,19 +150,26 @@ public class KBArticleIndexer extends BaseIndexer<KBArticle> {
 		Document document, Locale locale, String snippet,
 		PortletRequest portletRequest, PortletResponse portletResponse) {
 
-		String title = document.get(Field.TITLE);
+		String prefix = Field.SNIPPET + StringPool.UNDERLINE;
+
+		String title = document.get(prefix + Field.TITLE, Field.TITLE);
 
 		String content = snippet;
 
 		if (Validator.isNull(snippet)) {
-			content = document.get(Field.DESCRIPTION);
+			content = document.get(
+				prefix + Field.DESCRIPTION, Field.DESCRIPTION);
 
 			if (Validator.isNull(content)) {
-				content = StringUtil.shorten(document.get(Field.CONTENT), 200);
+				content = document.get(prefix + Field.CONTENT, Field.CONTENT);
 			}
 		}
 
-		return new Summary(title, content);
+		Summary summary = new Summary(title, content);
+
+		summary.setMaxContentLength(200);
+
+		return summary;
 	}
 
 	@Override
