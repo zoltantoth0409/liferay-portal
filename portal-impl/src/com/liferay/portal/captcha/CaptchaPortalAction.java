6e1aaa5ce4a9618c069c25f9c5b4d0ead7be8e09
Pei-Jung Lan <pei-jung.lan@liferay.com>
2017-06-28 17:08:53 -0700

    LPS-73286 Deprecate old classes. There's no usage of these action classes in portal, and these are the only places that the old CaptchaUtil is used.

--- portal-impl/src/com/liferay/portal/captcha/CaptchaPortalAction.java
+++ portal-impl/src/com/liferay/portal/captcha/CaptchaPortalAction.java
@@ -27,7 +27,9 @@ import org.apache.struts.action.ActionMapping;
 
 /**
  * @author Brian Wing Shun Chan
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class CaptchaPortalAction extends Action {
 
 	@Override
