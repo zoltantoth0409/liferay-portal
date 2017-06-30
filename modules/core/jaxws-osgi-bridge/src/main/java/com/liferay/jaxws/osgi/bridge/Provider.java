40cf8f97d44ec32c332f5003ab451f37d97dce6b
Carlos Sierra <carlos.sierra@liferay.com>
2017-06-30 11:03:49 +0200

    LPS-73197 Avoid NPE and provide better message

--- modules/core/jaxws-osgi-bridge/src/main/java/com/liferay/jaxws/osgi/bridge/Provider.java
+++ modules/core/jaxws-osgi-bridge/src/main/java/com/liferay/jaxws/osgi/bridge/Provider.java
@@ -165,7 +165,16 @@ public class Provider extends javax.xml.ws.spi.Provider {
 
 	protected javax.xml.ws.spi.Provider getProvider() {
 		try {
-			return _serviceTracker.waitForService(10 * 1000L);
+			javax.xml.ws.spi.Provider provider = _serviceTracker.waitForService(
+				10_000L);
+
+			if (provider == null) {
+				throw new IllegalStateException(
+					"JAX-WS API bridge has been used but it is not properly " +
+						"initialized. Please configure it in System Settings");
+			}
+
+			return provider;
 		}
 		catch (InterruptedException ie) {
 			throw new RuntimeException(ie);
