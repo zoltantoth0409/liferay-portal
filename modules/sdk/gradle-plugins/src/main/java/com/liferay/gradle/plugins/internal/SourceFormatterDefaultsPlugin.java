e01973056d557495a1448d6b735cacf750b73bac
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-07-06 15:40:15 -0700

    LPS-73489 Reuse variable

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/SourceFormatterDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/SourceFormatterDefaultsPlugin.java
@@ -65,17 +65,17 @@ public class SourceFormatterDefaultsPlugin
 	private void _configureTasksFormatSource(
 		FormatSourceTask formatSourceTask) {
 
+		Project project = formatSourceTask.getProject();
+
 		String gitWorkingBranchName = GradleUtil.getProperty(
-			formatSourceTask.getProject(), "git.working.branch.name",
-			(String)null);
+			project, "git.working.branch.name", (String)null);
 
 		if (Validator.isNotNull(gitWorkingBranchName)) {
 			formatSourceTask.setGitWorkingBranchName(gitWorkingBranchName);
 		}
 
 		String includeSubrepositories = GradleUtil.getProperty(
-			formatSourceTask.getProject(),
-			"source.formatter.include.subrepositories", (String)null);
+			project, "source.formatter.include.subrepositories", (String)null);
 
 		if (Validator.isNotNull(includeSubrepositories)) {
 			formatSourceTask.setIncludeSubrepositories(
@@ -83,16 +83,14 @@ public class SourceFormatterDefaultsPlugin
 		}
 
 		String maxLineLength = GradleUtil.getProperty(
-			formatSourceTask.getProject(), "source.formatter.max.line.length",
-			(String)null);
+			project, "source.formatter.max.line.length", (String)null);
 
 		if (Validator.isNotNull(maxLineLength)) {
 			formatSourceTask.setMaxLineLength(Integer.parseInt(maxLineLength));
 		}
 
 		String showDocumentation = GradleUtil.getProperty(
-			formatSourceTask.getProject(),
-			"source.formatter.show.documentation", (String)null);
+			project, "source.formatter.show.documentation", (String)null);
 
 		if (Validator.isNotNull(showDocumentation)) {
 			formatSourceTask.setShowDocumentation(
@@ -100,8 +98,7 @@ public class SourceFormatterDefaultsPlugin
 		}
 
 		String processorThreadCount = GradleUtil.getProperty(
-			formatSourceTask.getProject(),
-			"source.formatter.processor.thread.count", (String)null);
+			project, "source.formatter.processor.thread.count", (String)null);
 
 		if (Validator.isNotNull(processorThreadCount)) {
 			formatSourceTask.setProcessorThreadCount(
