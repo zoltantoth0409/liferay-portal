8b6c07767bae3fcddb2b4bcc8fd0994bb937ddf3
Kenji Heigel <kenji.heigel@liferay.com>
2017-07-06 13:54:42 -0700

    LRQA-32801 Add support for 'for' elements

---
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/ForElement.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.elements;
+
+import org.dom4j.Element;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kenji Heigel
+ */
+public class ForElement extends PoshiElement {
+
+	public ForElement(Element element) {
+		super("for", element);
+	}
+
+	public ForElement(String readableSyntax) {
+		super("for", readableSyntax);
+	}
+
+	@Override
+	public String getBlockName() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(" (");
+
+		String param = attributeValue("param");
+
+		sb.append(param);
+
+		sb.append(" : \"");
+
+		String list = attributeValue("list");
+
+		sb.append(list);
+
+		sb.append("\")");
+
+		return "for" + sb.toString();
+	}
+
+	@Override
+	public void parseReadableSyntax(String readableSyntax) {
+		for (String readableBlock : getReadableBlocks(readableSyntax)) {
+			if (readableBlock.startsWith("for (")) {
+				String parentheticalContent = getParentheticalContent(
+					readableBlock);
+
+				String[] parentheticalContentArray =
+					parentheticalContent.split(":");
+
+				String param = parentheticalContentArray[0].trim();
+
+				addAttribute("param", param);
+
+				String list = getQuotedContent(
+					parentheticalContentArray[1].trim());
+
+				addAttribute("list", list);
+
+				continue;
+			}
+
+			addElementFromReadableSyntax(readableBlock);
+		}
+	}
+
+	@Override
+	public String toReadableSyntax() {
+		String readableSyntax = super.toReadableSyntax();
+
+		return "\n" + createReadableBlock(readableSyntax);
+	}
+
+	protected List<String> getReadableBlocks(String readableSyntax) {
+		StringBuilder sb = new StringBuilder();
+
+		List<String> readableBlocks = new ArrayList<>();
+
+		for (String line : readableSyntax.split("\n")) {
+			if (line.startsWith("for (")) {
+				readableBlocks.add(line);
+
+				continue;
+			}
+
+			String readableBlock = sb.toString();
+
+			if (isValidReadableBlock(readableBlock)) {
+				readableBlocks.add(readableBlock);
+
+				sb.setLength(0);
+			}
+
+			sb.append(line);
+		}
+
+		return readableBlocks;
+	}
+
+	protected boolean isBalanceValidationRequired(String readableSyntax) {
+		readableSyntax = readableSyntax.trim();
+
+		if (readableSyntax.endsWith(";") || readableSyntax.endsWith("}")) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
