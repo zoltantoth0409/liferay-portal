a851ed64a7b1569d984be67b5c5754968b6014fb
akosthurzo <akos.thurzo@liferay.com>
2017-07-06 15:09:37 +0200

    LPS-73137 Add verify process to clean unnecessary entries

---
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/systemevent/internal/verify/SystemEventVerifyProcess.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.systemevent.internal.verify;
+
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.SystemEventLocalService;
+import com.liferay.portal.verify.VerifyProcess;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Akos Thurzo
+ */
+@Component(
+	immediate = true,
+	property = {"verify.process.name=com.liferay.systemevent.internal.verify"},
+	service = VerifyProcess.class
+)
+public class SystemEventVerifyProcess extends VerifyProcess {
+
+	protected void deleteInvalidSystemEvents() throws PortalException {
+		List<Group> groups = _groupLocalService.getGroups(
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		for (Group group : groups) {
+			if (!_systemEventLocalService.validateGroup(group.getGroupId())) {
+				_systemEventLocalService.deleteSystemEvents(group.getGroupId());
+			}
+		}
+	}
+
+	@Override
+	protected void doVerify() throws Exception {
+		deleteInvalidSystemEvents();
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private SystemEventLocalService _systemEventLocalService;
+
+}
