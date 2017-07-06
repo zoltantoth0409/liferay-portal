37bf08d5dd41f523100c2746e1ad082b08e5a9e2
tina.tian <tina.tian@liferay.com>
2017-07-06 16:58:59 -0700

    LPS-73283 Use portal-kernel's SerializableObjectWrapper

--- modules/apps/foundation/portal-cache/portal-cache-ehcache/src/main/java/com/liferay/portal/cache/ehcache/event/EhcachePortalCacheListenerAdapter.java
+++ modules/apps/foundation/portal-cache/portal-cache-ehcache/src/main/java/com/liferay/portal/cache/ehcache/event/EhcachePortalCacheListenerAdapter.java
@@ -16,10 +16,10 @@ package com.liferay.portal.cache.ehcache.event;
 
 import com.liferay.portal.cache.ehcache.EhcacheUnwrapUtil;
 import com.liferay.portal.cache.ehcache.internal.event.ConfigurableEhcachePortalCacheListener;
-import com.liferay.portal.cache.io.SerializableObjectWrapper;
 import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.cache.PortalCacheException;
 import com.liferay.portal.kernel.cache.PortalCacheListener;
+import com.liferay.portal.kernel.io.SerializableObjectWrapper;
 
 import java.io.Serializable;
 
