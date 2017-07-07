c5f353198543c5e7dc821d418bbe85377c25248a
Samuel Tran <samuel.tran@liferay.com>
2017-07-07 16:43:35 -0700

    LPS-65627 Compare shas

--- tools/subrepo/push_to_subrepos.sh
+++ tools/subrepo/push_to_subrepos.sh
@@ -14,69 +14,102 @@ error() {
 }
 
 if [[ "${PWD}" != *"/tools/subrepo" ]]; then
-  error "This script can only be run from within the tools/subrepo directory."
+	error "This script can only be run from within the tools/subrepo directory."
 fi
 
 if [[ "${PWD}" != *"liferay-portal/tools/subrepo" ]]; then
-  error "This script can only be run from within the liferay-portal repository."
+	error "This script can only be run from within the liferay-portal repository."
 fi
 
 if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
-  error "This script can only be run from the master branch."
+	error "This script can only be run from the master branch."
 fi
 
 if [[ -z "${GITHUB_API_TOKEN}" ]]; then
-  GITHUB_API_TOKEN="$(git config github.token)"
+	GITHUB_API_TOKEN="$(git config github.token)"
 fi
 
 if [[ -z "${GITHUB_API_TOKEN}" ]]; then
-  error "Your GitHub api token is not set. Set it using the GITHUB_API_TOKEN environment variable, or with 'git config github.token'."
+	error "Your GitHub api token is not set. Set it using the GITHUB_API_TOKEN environment variable, or with 'git config github.token'."
 fi
 
 GRADLE_FILES=(
-  "gradle/wrapper/gradle-wrapper.jar"
-  "gradle/wrapper/gradle-wrapper.properties"
-  "gradlew"
-  "gradlew.bat"
+	"gradle/wrapper/gradle-wrapper.jar"
+	"gradle/wrapper/gradle-wrapper.properties"
+	"gradlew"
+	"gradlew.bat"
 )
 
-GRADLE_FILE_SHAS=($(
-  for FILE in "${GRADLE_FILES[@]}"; do
-    SHA="$(git hash-object "${FILE}")"
+LOCAL_SHAS=($(
+	for FILE in "${GRADLE_FILES[@]}"; do
+		SHA="$(git hash-object "${FILE}")"
 
-    if [[ -z "${SHA}" ]]; then
-      error "Unable to parse sha for the file '${FILE}'."
-    fi
+		if [[ -z "${SHA}" ]]; then
+			error "Unable to parse sha for the file '${FILE}'."
+		fi
 
-    echo "${FILE}:${SHA}"
-  done
+		echo "${FILE}:${SHA}"
+	done
 ))
 
 SUBREPO=$1
 
+if [[ -z "${SUBREPO}" ]]; then
+	echo "Usage: ./push_to_subrepos.sh SUBREPO" >&2
+
+	exit 1
+fi
+
 # if [[ "${SUBREPO}" == *"-private" ]]; then
-#   BRANCHES=(
-#     "7.0.x-private"
-#     "master-private"
-#   )
+# 	BRANCHES=(
+# 		"7.0.x-private"
+# 		"master-private"
+# 	)
 # else
-#   BRANCHES=(
-#     "7.0.x"
-#     "7.0.x-private"
-#     "master"
-#     "master-private"
-#   )
+# 	BRANCHES=(
+# 		"7.0.x"
+# 		"7.0.x-private"
+# 		"master"
+# 		"master-private"
+# 	)
 # fi
 
 BRANCHES=(
-  "7.0.x"
-  "master"
+	"7.0.x"
+	"master"
 )
 
 for BRANCH in "${BRANCHES[@]}"; do
-  REMOTE="liferay/${SUBREPO}"
+	REMOTE="liferay/${SUBREPO}"
 
-  if [[ "${BRANCH}" == *"-private" ]] && [[ "${SUBREPO}" != *"-private" ]]; then
-    REMOTE="${REMOTE}-private"
-  fi
+	if [[ "${BRANCH}" == *"-private" ]] && [[ "${SUBREPO}" != *"-private" ]]; then
+		REMOTE="${REMOTE}-private"
+	fi
+
+	GRADLE_WRAPPER_JSON=
+
+	for i in {1..2}; do
+		GRADLE_WRAPPER_JSON=($(curl --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO}/contents/gradle/wrapper?ref=${BRANCH}" -X GET | tr '\n' ' ' | sed 's/ //g' | sed 's/},/\'$'\n/g'))
+
+		if [[ $(echo "${GRADLE_WRAPPER_JSON[@]}" | grep '\"sha\"') ]]; then
+			break;
+		fi
+	done
+
+	if [[ -z $(echo "${GRADLE_WRAPPER_JSON[@]}" | grep '\"sha\"') ]] && [[ -z $(echo "${GRADLE_WRAPPER_JSON[@]}" | grep '\"NotFound\"') ]]; then
+		error "Failed to get contents of gradle/wrapper directory via the api for liferay/${SUBREPO}:${BRANCH}"
+	fi
+
+	GRADLE_WRAPPER_JAR_REMOTE_SHA="$(printf '%s\n' "${GRADLE_WRAPPER_JSON[@]}" | grep '"gradle-wrapper.jar"' | sed 's/",/&\'$'\n/g' | grep '"sha"' | sed 's/"[^"]*$//' | sed 's/.*"//')"
+	GRADLE_WRAPPER_PROPERTIES_REMOTE_SHA="$(printf '%s\n' "${GRADLE_WRAPPER_JSON[@]}" | grep '"gradle-wrapper.properties"' | sed 's/",/&\'$'\n/g' | grep '"sha"' | sed 's/"[^"]*$//' | sed 's/.*"//')"
+
+	GRADLEW_REMOTE_SHA="$(curl --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO}/contents/gradlew?ref=${BRANCH}" -X GET | grep '"sha"' | sed 's/"[^"]*$//' | sed 's/.*"//')"
+	GRADLEW_BAT_REMOTE_SHA="$(curl --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO}/contents/gradlew.bat?ref=${BRANCH}" -X GET | grep '"sha"' | sed 's/"[^"]*$//' | sed 's/.*"//')"
+
+	REMOTE_SHAS=(
+		"gradle/wrapper/gradle-wrapper.jar:${GRADLE_WRAPPER_JAR_REMOTE_SHA}"
+		"gradle/wrapper/gradle-wrapper.properties:${GRADLE_WRAPPER_PROPERTIES_REMOTE_SHA}"
+		"gradlew:${GRADLEW_REMOTE_SHA}"
+		"gradlew.bat:${GRADLEW_BAT_REMOTE_SHA}"
+	)
 done
