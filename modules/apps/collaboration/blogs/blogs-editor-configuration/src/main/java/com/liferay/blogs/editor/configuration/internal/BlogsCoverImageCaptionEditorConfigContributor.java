8058f1e81d8d8152e2e76824ca93079d5255d693
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-07-10 13:59:28 +0200

    LPS-73393 Move AlloyEditor specific configuration

--- modules/apps/collaboration/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsCoverImageCaptionEditorConfigContributor.java
+++ modules/apps/collaboration/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsCoverImageCaptionEditorConfigContributor.java
@@ -49,7 +49,6 @@ public class BlogsCoverImageCaptionEditorConfigContributor
 
 		jsonObject.put("allowedContent", "a");
 		jsonObject.put("disallowedContent", "br");
-		jsonObject.put("toolbars", getToolbarsJSONObject());
 	}
 
 	protected JSONObject getToolbarsJSONObject() {
