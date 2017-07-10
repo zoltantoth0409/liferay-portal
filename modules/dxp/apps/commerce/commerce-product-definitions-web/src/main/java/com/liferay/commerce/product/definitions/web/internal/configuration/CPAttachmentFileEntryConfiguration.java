119e1a0f26c6f24c8273809acd8972d4cb0121f7
Alessio Rendina <asilcomando@gmail.com>
2017-07-10 19:37:17 +0200

    COMMERCE-130 Added new configurations

---
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/configuration/CPAttachmentFileEntryConfiguration.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@ExtendedObjectClassDefinition(
+	category = "commerce", scope = ExtendedObjectClassDefinition.Scope.SYSTEM
+)
+@Meta.OCD(
+	id = "com.liferay.commerce.product.definitions.web.internal.configuration.CPAttachmentFileEntryConfiguration",
+	localization = "content/Language",
+	name = "commerce.attachment.configuration.name"
+)
+public interface CPAttachmentFileEntryConfiguration {
+
+	@Meta.AD(
+		deflt = "false",
+		description = "changeable.default.language.description",
+		name = "changeable.default.language", required = false
+	)
+	public boolean changeableDefaultLanguage();
+
+	@Meta.AD(deflt = "15", required = false)
+	public int checkInterval();
+
+	@Meta.AD(deflt = "descriptive", required = false)
+	public String defaultDisplayView();
+
+	@Meta.AD(deflt = "icon|descriptive|list", required = false)
+	public String[] displayViews();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean definitionForceAutogenerateId();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean definitionsSearchWithIndex();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean publishToLiveByDefault();
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean publishVersionHistoryByDefault();
+
+}
