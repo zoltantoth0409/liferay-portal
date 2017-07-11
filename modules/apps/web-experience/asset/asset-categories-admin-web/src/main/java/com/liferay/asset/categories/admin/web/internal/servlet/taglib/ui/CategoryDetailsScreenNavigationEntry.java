9eba4f463098933a2ea6e58772d1432df4c45914
Juergen Kappler <jurgen.kappler@liferay.com>
2017-07-11 15:54:48 +0200

    LPS-73558 Navigation keys for screen navigation of categories

---
+++ modules/apps/web-experience/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/servlet/taglib/ui/CategoryDetailsScreenNavigationEntry.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.internal.servlet.taglib.ui;
+
+import com.liferay.asset.categories.admin.web.constants.AssetCategoriesConstants;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	property = {"screen.navigation.entry.order:Integer=10"},
+	service = ScreenNavigationEntry.class
+)
+public class CategoryDetailsScreenNavigationEntry
+	implements ScreenNavigationEntry {
+
+	@Override
+	public String getCategoryKey() {
+		return "details";
+	}
+
+	@Override
+	public String getEntryKey() {
+		return "details";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "details");
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return AssetCategoriesConstants.CATEGORY_KEY_GENERAL;
+	}
+
+	@Override
+	public void render(HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		_jspRenderer.renderJSP(request, response, "/category/details.jsp");
+	}
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+}
---
+++ modules/apps/web-experience/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/servlet/taglib/ui/CategoryPropertiesScreenNavigationEntry.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.internal.servlet.taglib.ui;
+
+import com.liferay.asset.categories.admin.web.constants.AssetCategoriesConstants;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.User;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	property = {"screen.navigation.entry.order:Integer=20"},
+	service = ScreenNavigationEntry.class
+)
+public class CategoryPropertiesScreenNavigationEntry
+	implements ScreenNavigationEntry {
+
+	@Override
+	public String getCategoryKey() {
+		return "properties";
+	}
+
+	@Override
+	public String getEntryKey() {
+		return "properties";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "properties");
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return AssetCategoriesConstants.CATEGORY_KEY_GENERAL;
+	}
+
+	@Override
+	public boolean isVisible(User user, Object screenModelBean) {
+		return false;
+	}
+
+	@Override
+	public void render(HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		_jspRenderer.renderJSP(request, response, "/category/properties.jsp");
+	}
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+}
