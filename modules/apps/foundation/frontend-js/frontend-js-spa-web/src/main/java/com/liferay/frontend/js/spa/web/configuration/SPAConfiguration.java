2ee27f2694367ad2f56c91127724a216e2ad47e9
Gergely Száz <gergely.szaz@liferay.com>
2017-07-11 12:42:58 +0200

    LPS-72041 Add a description of the SPA configuration settings

--- modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/configuration/SPAConfiguration.java
+++ modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/java/com/liferay/frontend/js/spa/web/configuration/SPAConfiguration.java
@@ -23,6 +23,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
  */
 @ExtendedObjectClassDefinition(category = "foundation")
 @Meta.OCD(
+	description = "spa-configuration-description",
 	id = "com.liferay.frontend.js.spa.web.configuration.SPAConfiguration",
 	localization = "content/Language", name = "spa-configuration-name"
 )
