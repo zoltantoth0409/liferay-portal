21ab1ba1dd2fe759b53186102aceb33e76b4c43d
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-07-11 13:38:50 +0200

    LPS-73391 Configure "allowedContent" attribute properly

--- modules/apps/adaptive-media/adaptive-media-journal-editor-configuration/src/main/java/com/liferay/adaptive/media/journal/editor/configuration/internal/DynamicAdaptiveMediaJournalEditorConfigContributor.java
+++ modules/apps/adaptive-media/adaptive-media-journal-editor-configuration/src/main/java/com/liferay/adaptive/media/journal/editor/configuration/internal/DynamicAdaptiveMediaJournalEditorConfigContributor.java
@@ -62,11 +62,11 @@ public class DynamicAdaptiveMediaJournalEditorConfigContributor
 
 		String allowedContent = jsonObject.getString("allowedContent");
 
-		if (Validator.isNotNull(allowedContent)) {
-			allowedContent += StringPool.SPACE + _IMG_TAG_RULE;
+		if (allowedContent.equals(Boolean.TRUE.toString())) {
+			allowedContent = null;
 		}
-		else {
-			allowedContent = _IMG_TAG_RULE;
+		else if (Validator.isNotNull(allowedContent)) {
+			allowedContent += StringPool.SPACE + _IMG_TAG_RULE;
 		}
 
 		jsonObject.put("allowedContent", allowedContent);
