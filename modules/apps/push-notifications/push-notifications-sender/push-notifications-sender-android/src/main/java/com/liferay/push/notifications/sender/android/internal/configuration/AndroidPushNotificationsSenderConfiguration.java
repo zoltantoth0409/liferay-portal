02347d25c68291a89da893e5ece0e2444e58b757
Hugo Huijser <hugo.huijser@liferay.com>
2017-07-11 08:17:17 +0800

    LPS-73178 push-notifications - Values for 'name' and 'description' should use '-' instead of '.'

--- modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-android/src/main/java/com/liferay/push/notifications/sender/android/internal/configuration/AndroidPushNotificationsSenderConfiguration.java
+++ modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-android/src/main/java/com/liferay/push/notifications/sender/android/internal/configuration/AndroidPushNotificationsSenderConfiguration.java
@@ -25,18 +25,18 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 @Meta.OCD(
 	id = "com.liferay.push.notifications.sender.android.internal.configuration.AndroidPushNotificationsSenderConfiguration",
 	localization = "content/Language",
-	name = "android.push.notifications.sender.configuration.name"
+	name = "android-push-notifications-sender-configuration-name"
 )
 public interface AndroidPushNotificationsSenderConfiguration {
 
 	@Meta.AD(
-		description = "android.api.key.description",
-		name = "android.api.key.name", required = false
+		description = "android-api-key-description",
+		name = "android-api-key-name", required = false
 	)
 	public String apiKey();
 
 	@Meta.AD(
-		description = "retries.description", name = "retries.name",
+		description = "retries-description", name = "retries-name",
 		required = false
 	)
 	public int retries();
