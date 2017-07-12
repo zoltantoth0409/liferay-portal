87f3433b07425f44009ca42e97a8aa51c37ab35d
Preston-Crary <preston.crary@liferay.com>
2017-07-12 11:45:08 -0700

    LPS-73548 Partial revert of 91c224d3ecb9690bc4eac48a0ba264bd44314fd2, we do not have a 'release.schema.version' for blogs service in ee.

--- modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/template/BlogsPortletDisplayTemplateHandler.java
+++ modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/template/BlogsPortletDisplayTemplateHandler.java
@@ -21,7 +21,6 @@ import com.liferay.blogs.kernel.service.BlogsEntryService;
 import com.liferay.blogs.web.constants.BlogsPortletKeys;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
 import com.liferay.portal.kernel.portletdisplaytemplate.PortletDisplayTemplateManager;
 import com.liferay.portal.kernel.template.TemplateHandler;
@@ -119,10 +118,5 @@ public class BlogsPortletDisplayTemplateHandler
 
 	@Reference
 	private Portal _portal;
-
-	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.blogs.service)(release.schema.version=1.1.0))"
-	)
-	private Release _release;
 
 }
