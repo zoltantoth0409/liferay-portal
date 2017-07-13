7f107b7f26db1d7ca1f9594548da2274a34f0991
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-07-13 11:05:51 +0200

    LPS-67760 Moves staging check to journal display context

--- modules/apps/web-experience/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
+++ modules/apps/web-experience/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
@@ -60,6 +60,8 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PredicateFilter;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -830,16 +832,22 @@ public class JournalContentDisplayContext {
 			return _showEditArticleIcon;
 		}
 
-		JournalArticle latestArticle = getLatestArticle();
-
 		_showEditArticleIcon = false;
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)_portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Group group = themeDisplay.getScopeGroup();
+
+		if (group.hasStagingGroup() && _STAGING_LIVE_GROUP_LOCKING_ENABLED) {
+			return _showEditArticleIcon;
+		}
+
+		JournalArticle latestArticle = getLatestArticle();
 
 		if (latestArticle == null) {
 			return _showEditArticleIcon;
 		}
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)_portletRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
 
 		try {
 			_showEditArticleIcon = JournalArticlePermission.contains(
@@ -959,6 +967,10 @@ public class JournalContentDisplayContext {
 
 		return ddmTemplate;
 	}
+
+	private static final boolean _STAGING_LIVE_GROUP_LOCKING_ENABLED =
+		GetterUtil.getBoolean(
+			PropsUtil.get(PropsKeys.STAGING_LIVE_GROUP_LOCKING_ENABLED));
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		JournalContentDisplayContext.class);
