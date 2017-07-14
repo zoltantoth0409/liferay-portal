4c8226c794fa2f66789a9cbfcde62223633f730a
shuyangzhou <shuyang.zhou@liferay.com>
2017-07-14 15:11:06 -0700

    Revert "LPS-73626 Ignoring UserIndexerTest to pass CI, see ticket for detail"
    This reverts commit 4c99e4bb7efe158173151a37fff97450f53a83e4.

--- modules/apps/foundation/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/indexer/test/UserIndexerTest.java
+++ modules/apps/foundation/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/indexer/test/UserIndexerTest.java
@@ -45,7 +45,6 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -53,7 +52,6 @@ import org.junit.runner.RunWith;
 /**
  * @author André de Oliveira
  */
-@Ignore
 @RunWith(Arquillian.class)
 @Sync
 public class UserIndexerTest {
