153de0018763c080efa0a9131ae8d5b05d12fb92
tomwang2011 <tong.wang@liferay.com>
2017-07-14 08:07:32 -0700

    LPS-69476 Convert UserIndexerTest to Arquillian

--- modules/apps/foundation/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/indexer/test/UserIndexerTest.java
+++ modules/apps/foundation/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/indexer/test/UserIndexerTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.users.admin.indexer.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
@@ -48,10 +49,12 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author André de Oliveira
  */
+@RunWith(Arquillian.class)
 @Sync
 public class UserIndexerTest {
 
