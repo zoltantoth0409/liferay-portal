d0d0d40ac5c703090f8e1cb04531d23f78111d4e
Preston-Crary <preston.crary@liferay.com>
2017-07-14 09:40:42 -0700

    LPS-73600 Create CollatorUtil and new 'collator.rules' property to make it possible to configure text sorting without a Java extention.

---
+++ portal-kernel/src/com/liferay/portal/kernel/util/CollatorUtil.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import com.liferay.portal.kernel.configuration.Filter;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.text.Collator;
+import java.text.ParseException;
+import java.text.RuleBasedCollator;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Preston Crary
+ */
+public class CollatorUtil {
+
+	public static Collator getInstance(Locale locale) {
+		String rules = _cache.get(locale);
+
+		if (rules == null) {
+			String languageId = LocaleUtil.toLanguageId(locale);
+
+			rules = PropsUtil.get("collator.rules", new Filter(languageId));
+
+			if (Validator.isNull(rules)) {
+				rules = StringPool.BLANK;
+			}
+
+			_cache.put(locale, rules);
+		}
+
+		if (rules != StringPool.BLANK) {
+			try {
+				return new RuleBasedCollator(rules);
+			}
+			catch (ParseException pe) {
+				_log.error(pe, pe);
+
+				_cache.put(locale, StringPool.BLANK);
+			}
+		}
+
+		return Collator.getInstance(locale);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(CollatorUtil.class);
+
+	private static final Map<Locale, String> _cache = new ConcurrentHashMap<>();
+
+}
---
+++ portal-kernel/test/unit/com/liferay/portal/kernel/util/CollatorUtilTest.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import com.liferay.portal.kernel.test.CaptureHandler;
+import com.liferay.portal.kernel.test.JDKLoggerTestUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.NewEnv;
+import com.liferay.portal.kernel.test.rule.NewEnvTestRule;
+
+import java.text.Collator;
+import java.text.RuleBasedCollator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+/**
+ * @author Preston Crary
+ */
+@NewEnv(type = NewEnv.Type.CLASSLOADER)
+public class CollatorUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final TestRule testRule = NewEnvTestRule.INSTANCE;
+
+	@Test
+	public void testGetInstanceWithInvalidProperty() {
+		ReflectionTestUtil.setFieldValue(
+			PropsUtil.class, "_props",
+			ProxyUtil.newProxyInstance(
+				CollatorUtilTest.class.getClassLoader(),
+				new Class<?>[] {Props.class}, (proxy, method, args) -> "<<<"));
+
+		try (CaptureHandler captureHandler =
+				JDKLoggerTestUtil.configureJDKLogger(
+					CollatorUtil.class.getName(), Level.ALL)) {
+
+			CollatorUtil.getInstance(LocaleUtil.getDefault());
+
+			List<LogRecord> logRecords = captureHandler.getLogRecords();
+
+			Assert.assertEquals(logRecords.toString(), 1, logRecords.size());
+
+			LogRecord logRecord = logRecords.get(0);
+
+			String message = logRecord.getMessage();
+
+			Assert.assertTrue(
+				logRecord.toString(),
+				message.contains("java.text.ParseException"));
+		}
+	}
+
+	@Test
+	public void testGetInstanceWithoutProperty() {
+		ReflectionTestUtil.setFieldValue(
+			PropsUtil.class, "_props",
+			ProxyUtil.newProxyInstance(
+				CollatorUtilTest.class.getClassLoader(),
+				new Class<?>[] {Props.class}, (proxy, method, args) -> null));
+
+		Collator collator = CollatorUtil.getInstance(LocaleUtil.getDefault());
+
+		Assert.assertEquals(Collator.getInstance(), collator);
+
+		List<String> expected = new ArrayList<>();
+
+		expected.add("AB");
+		expected.add("A B");
+
+		List<String> actual = new ArrayList<>();
+
+		actual.add("A B");
+		actual.add("AB");
+
+		actual.sort(collator);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testGetInstanceWithProperty() {
+		ReflectionTestUtil.setFieldValue(
+			PropsUtil.class, "_props",
+			ProxyUtil.newProxyInstance(
+				CollatorUtilTest.class.getClassLoader(),
+				new Class<?>[] {Props.class}, (proxy, method, args) -> _RULES));
+
+		Collator collator = CollatorUtil.getInstance(LocaleUtil.getDefault());
+
+		RuleBasedCollator ruleBasedCollator = (RuleBasedCollator)collator;
+
+		Assert.assertEquals(_RULES, ruleBasedCollator.getRules());
+
+		List<String> expected = new ArrayList<>();
+
+		expected.add("A B");
+		expected.add("AB");
+
+		List<String> actual = new ArrayList<>();
+
+		actual.add("AB");
+		actual.add("A B");
+
+		actual.sort(collator);
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	private static final String _RULES = "=A<b,' '<A";
+
+}
