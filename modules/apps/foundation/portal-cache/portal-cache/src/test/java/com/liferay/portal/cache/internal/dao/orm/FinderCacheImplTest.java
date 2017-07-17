6bcbbf8c86646e9d6776510059455a191ebfec77
shuyangzhou <shuyang.zhou@liferay.com>
2017-07-17 11:13:12 -0700

    LPS-54509 This needs fresh FinderCache every time, otherwise the internally cached PortalCache will be out of date.

--- modules/apps/foundation/portal-cache/portal-cache/src/test/java/com/liferay/portal/cache/internal/dao/orm/FinderCacheImplTest.java
+++ modules/apps/foundation/portal-cache/portal-cache/src/test/java/com/liferay/portal/cache/internal/dao/orm/FinderCacheImplTest.java
@@ -18,6 +18,7 @@ import com.liferay.portal.cache.key.HashCodeHexStringCacheKeyGenerator;
 import com.liferay.portal.kernel.cache.MultiVMPool;
 import com.liferay.portal.kernel.cache.key.CacheKeyGenerator;
 import com.liferay.portal.kernel.cache.key.CacheKeyGeneratorUtil;
+import com.liferay.portal.kernel.dao.orm.FinderCache;
 import com.liferay.portal.kernel.dao.orm.FinderPath;
 import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.ProxyUtil;
@@ -63,10 +64,6 @@ public class FinderCacheImplTest {
 			true, true, FinderCacheImplTest.class,
 			FinderCacheImplTest.class.getName(), "test",
 			new String[] {String.class.getName()});
-
-		_finderCacheImpl = new FinderCacheImpl();
-
-		_finderCacheImpl.setProps(_props);
 	}
 
 	@Test
@@ -88,36 +85,42 @@ public class FinderCacheImplTest {
 			_cacheKeyGenerator.getCacheKey(_key2));
 	}
 
-	private void _activateFinderCache(MultiVMPool multiVMPool) {
+	private FinderCache _activateFinderCache(MultiVMPool multiVMPool) {
+		FinderCacheImpl finderCacheImpl = new FinderCacheImpl();
+
 		EntityCacheImpl entityCacheImpl = new EntityCacheImpl();
 
 		entityCacheImpl.setMultiVMPool(multiVMPool);
 
-		_finderCacheImpl.setEntityCache(entityCacheImpl);
+		finderCacheImpl.setEntityCache(entityCacheImpl);
 
-		_finderCacheImpl.setMultiVMPool(multiVMPool);
+		finderCacheImpl.setMultiVMPool(multiVMPool);
 
-		_finderCacheImpl.activate();
+		finderCacheImpl.setProps(_props);
+
+		finderCacheImpl.activate();
+
+		return finderCacheImpl;
 	}
 
 	private void _assertPutEmptyListInvalid(MultiVMPool multiVMPool) {
-		_activateFinderCache(multiVMPool);
+		FinderCache finderCache = _activateFinderCache(multiVMPool);
 
-		_finderCacheImpl.putResult(
+		finderCache.putResult(
 			_finderPath, _key1, Collections.emptyList(), true);
 
-		Object result = _finderCacheImpl.getResult(_finderPath, _key2, null);
+		Object result = finderCache.getResult(_finderPath, _key2, null);
 
 		Assert.assertNull(result);
 	}
 
 	private void _assertPutEmptyListValid(MultiVMPool multiVMPool) {
-		_activateFinderCache(multiVMPool);
+		FinderCache finderCache = _activateFinderCache(multiVMPool);
 
-		_finderCacheImpl.putResult(
+		finderCache.putResult(
 			_finderPath, _key1, Collections.emptyList(), true);
 
-		Object result = _finderCacheImpl.getResult(_finderPath, _key1, null);
+		Object result = finderCache.getResult(_finderPath, _key1, null);
 
 		Assert.assertSame(Collections.emptyList(), result);
 	}
@@ -132,7 +135,6 @@ public class FinderCacheImplTest {
 	private static Props _props;
 	private static MultiVMPool _serializedMultiVMPool;
 
-	private FinderCacheImpl _finderCacheImpl;
 	private FinderPath _finderPath;
 
 }
