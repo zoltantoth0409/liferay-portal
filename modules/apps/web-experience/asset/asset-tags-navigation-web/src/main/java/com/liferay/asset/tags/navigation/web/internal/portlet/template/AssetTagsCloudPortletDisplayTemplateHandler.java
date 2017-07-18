79371f58a7a0e54579e8745b1219fd77099f6c64
Juergen Kappler <jurgen.kappler@liferay.com>
2017-07-18 13:20:27 +0200

    LPS-72095 Create Asset Tags Cloud portlet display template handler

---
+++ modules/apps/web-experience/asset/asset-tags-navigation-web/src/main/java/com/liferay/asset/tags/navigation/web/internal/portlet/template/AssetTagsCloudPortletDisplayTemplateHandler.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.navigation.web.internal.portlet.template;
+
+import com.liferay.asset.tags.navigation.web.constants.AssetTagsNavigationPortletKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.template.TemplateHandler;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AssetTagsNavigationPortletKeys.ASSET_TAGS_CLOUD
+	},
+	service = TemplateHandler.class
+)
+public class AssetTagsCloudPortletDisplayTemplateHandler
+	extends AssetTagsNavigationPortletDisplayTemplateHandler {
+
+	@Override
+	public String getName(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		String portletTitle = portal.getPortletTitle(
+			AssetTagsNavigationPortletKeys.ASSET_TAGS_CLOUD, resourceBundle);
+
+		return portletTitle.concat(StringPool.SPACE).concat(
+			LanguageUtil.get(locale, "template"));
+	}
+
+	@Override
+	public String getResourceName() {
+		return AssetTagsNavigationPortletKeys.ASSET_TAGS_CLOUD;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortal(Portal portal) {
+		this.portal = portal;
+	}
+
+}
