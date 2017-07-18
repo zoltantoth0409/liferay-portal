d25c9da872e200a72420d4dd94691a33d3af97fb
tina.tian <tina.tian@liferay.com>
2017-07-17 17:10:59 -0700

    LPS-73658 Copy portal-cluster-multiple from ee-7.0.x

---
+++ modules/apps/foundation/portal/portal-cluster-multiple/src/main/java/com/liferay/portal/cluster/multiple/internal/portal/profile/ModulePortalProfile.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cluster.multiple.internal.portal.profile;
+
+import com.liferay.portal.cluster.multiple.internal.ClusterExecutorImpl;
+import com.liferay.portal.cluster.multiple.internal.ClusterLinkImpl;
+import com.liferay.portal.cluster.multiple.internal.ClusterMasterExecutorImpl;
+import com.liferay.portal.cluster.multiple.internal.jgroups.JGroupsClusterChannelFactory;
+import com.liferay.portal.profile.BaseDSModulePortalProfile;
+import com.liferay.portal.profile.PortalProfile;
+
+import java.util.Collections;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Shuyang Zhou
+ */
+@Component(immediate = true, service = PortalProfile.class)
+public class ModulePortalProfile extends BaseDSModulePortalProfile {
+
+	@Activate
+	public void activate(ComponentContext componentContext) {
+		init(
+			componentContext,
+			Collections.singleton(PortalProfile.PORTAL_PROFILE_NAME_CE),
+			ClusterExecutorImpl.class.getName(),
+			ClusterLinkImpl.class.getName(),
+			ClusterMasterExecutorImpl.class.getName(),
+			JGroupsClusterChannelFactory.class.getName());
+	}
+
+}
