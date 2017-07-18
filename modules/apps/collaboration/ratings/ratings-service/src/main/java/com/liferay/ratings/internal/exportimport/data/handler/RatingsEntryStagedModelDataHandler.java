dcb71a1ba0e00707e8cfc74263b9655bbe231df3
Daniel Kocsis <daniel.kocsis@liferay.com>
2017-07-18 17:08:40 +0200

    LPS-72347 Fix the actual issue via try to fetch the referre by its resource if it was a ResourcedModel

--- modules/apps/collaboration/ratings/ratings-service/src/main/java/com/liferay/ratings/internal/exportimport/data/handler/RatingsEntryStagedModelDataHandler.java
+++ modules/apps/collaboration/ratings/ratings-service/src/main/java/com/liferay/ratings/internal/exportimport/data/handler/RatingsEntryStagedModelDataHandler.java
@@ -22,10 +22,13 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.PersistedModel;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalServiceRegistryUtil;
+import com.liferay.portal.kernel.service.PersistedResourcedModelLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.ratings.kernel.model.RatingsEntry;
@@ -121,7 +124,25 @@ public class RatingsEntryStagedModelDataHandler
 				PersistedModelLocalServiceRegistryUtil.
 					getPersistedModelLocalService(entry.getClassName());
 
-			persistedModelLocalService.getPersistedModel(newClassPK);
+			if (persistedModelLocalService instanceof
+					PersistedResourcedModelLocalService) {
+
+				PersistedResourcedModelLocalService
+					persistedResourcedModelLocalService =
+						(PersistedResourcedModelLocalService)
+							persistedModelLocalService;
+
+				List<? extends PersistedModel> persistedModels =
+					persistedResourcedModelLocalService.getPersistedModel(
+						newClassPK);
+
+				if (ListUtil.isEmpty(persistedModels)) {
+					return;
+				}
+			}
+			else {
+				persistedModelLocalService.getPersistedModel(newClassPK);
+			}
 		}
 		catch (PortalException pe) {
 			if (_log.isWarnEnabled()) {
