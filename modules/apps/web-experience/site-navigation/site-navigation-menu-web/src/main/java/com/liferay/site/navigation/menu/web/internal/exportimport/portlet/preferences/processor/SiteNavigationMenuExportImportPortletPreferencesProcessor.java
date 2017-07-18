2be588515f34f63d2e6d788842d7f6ab3fc39729
Juergen Kappler <jurgen.kappler@liferay.com>
2017-07-18 08:41:28 +0200

    LPS-72095 Site Navigation Menu: Add ADT import/export capability

---
+++ modules/apps/web-experience/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/exportimport/portlet/preferences/processor/SiteNavigationMenuExportImportPortletPreferencesProcessor.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.menu.web.internal.exportimport.portlet.preferences.processor;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.PortletDataException;
+import com.liferay.exportimport.portlet.preferences.processor.Capability;
+import com.liferay.exportimport.portlet.preferences.processor.ExportImportPortletPreferencesProcessor;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portlet.display.template.exportimport.portlet.preferences.processor.PortletDisplayTemplateExportCapability;
+import com.liferay.portlet.display.template.exportimport.portlet.preferences.processor.PortletDisplayTemplateImportCapability;
+import com.liferay.site.navigation.menu.web.internal.constants.SiteNavigationMenuPortletKeys;
+
+import java.util.List;
+
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + SiteNavigationMenuPortletKeys.SITE_NAVIGATION_MENU
+	},
+	service = ExportImportPortletPreferencesProcessor.class
+)
+public class SiteNavigationMenuExportImportPortletPreferencesProcessor
+	implements ExportImportPortletPreferencesProcessor {
+
+	@Override
+	public List<Capability> getExportCapabilities() {
+		return ListUtil.toList(
+			new Capability[] {_portletDisplayTemplateExportCapability});
+	}
+
+	@Override
+	public List<Capability> getImportCapabilities() {
+		return ListUtil.toList(
+			new Capability[] {_portletDisplayTemplateImportCapability});
+	}
+
+	@Override
+	public PortletPreferences processExportPortletPreferences(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws PortletDataException {
+
+		return null;
+	}
+
+	@Override
+	public PortletPreferences processImportPortletPreferences(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws PortletDataException {
+
+		return null;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortletDisplayTemplateExportCapability(
+		PortletDisplayTemplateExportCapability
+			portletDisplayTemplateExportCapability) {
+
+		_portletDisplayTemplateExportCapability =
+			portletDisplayTemplateExportCapability;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortletDisplayTemplateImportCapability(
+		PortletDisplayTemplateImportCapability
+			portletDisplayTemplateImportCapability) {
+
+		_portletDisplayTemplateImportCapability =
+			portletDisplayTemplateImportCapability;
+	}
+
+	private PortletDisplayTemplateExportCapability
+		_portletDisplayTemplateExportCapability;
+	private PortletDisplayTemplateImportCapability
+		_portletDisplayTemplateImportCapability;
+
+}
