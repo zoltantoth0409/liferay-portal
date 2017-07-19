d1095f9a6a32b48d124dc5c1965a6effee15ad2c
Alessio Rendina <asilcomando@gmail.com>
2017-07-19 20:39:03 +0200

    COMMERCE-144 Added grouped product type template handler

---
+++ modules/dxp/apps/commerce/commerce-product-type-grouped-web/src/main/java/com/liferay/commerce/product/type/grouped/web/internal/template/GroupedCPTypeDisplayTemplateHandler.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.type.grouped.web.internal.template;
+
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.commerce.product.type.grouped.web.internal.GroupedCPTypeRenderer;
+import com.liferay.commerce.product.type.grouped.web.internal.display.context.CPGroupedCPTypeDisplayContext;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
+import com.liferay.portal.kernel.template.TemplateHandler;
+import com.liferay.portal.kernel.template.TemplateVariableGroup;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + CPPortletKeys.CP_CONTENT_WEB,
+	service = TemplateHandler.class
+)
+public class GroupedCPTypeDisplayTemplateHandler
+	extends BasePortletDisplayTemplateHandler {
+
+	@Override
+	public String getClassName() {
+		return GroupedCPTypeRenderer.class.getName();
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		String portletTitle = _portal.getPortletTitle(
+			CPPortletKeys.CP_CONTENT_WEB, resourceBundle);
+
+		return portletTitle.concat(StringPool.SPACE).concat(
+			LanguageUtil.get(locale, "template"));
+	}
+
+	@Override
+	public String getResourceName() {
+		return CPPortletKeys.CP_CONTENT_WEB;
+	}
+
+	@Override
+	public Map<String, TemplateVariableGroup> getTemplateVariableGroups(
+			long classPK, String language, Locale locale)
+		throws Exception {
+
+		Map<String, TemplateVariableGroup> templateVariableGroups =
+			super.getTemplateVariableGroups(classPK, language, locale);
+
+		TemplateVariableGroup templateVariableGroup =
+			templateVariableGroups.get("fields");
+
+		templateVariableGroup.empty();
+
+		templateVariableGroup.addVariable(
+			"cp-grouped-cp-type-display-context",
+			CPGroupedCPTypeDisplayContext.class,
+			"cpGroupedCPTypeDisplayContext");
+		templateVariableGroup.addVariable(
+			"cp-definition", CPDefinition.class, "cpDefinition", "title");
+
+		String[] restrictedVariables = getRestrictedVariables(language);
+
+		TemplateVariableGroup cpDefinitionsServicesTemplateVariableGroup =
+			new TemplateVariableGroup(
+				"cp-definition-services", restrictedVariables);
+
+		cpDefinitionsServicesTemplateVariableGroup.setAutocompleteEnabled(
+			false);
+
+		cpDefinitionsServicesTemplateVariableGroup.addServiceLocatorVariables(
+			CPDefinitionLocalService.class, CPDefinitionService.class);
+
+		templateVariableGroups.put(
+			cpDefinitionsServicesTemplateVariableGroup.getLabel(),
+			cpDefinitionsServicesTemplateVariableGroup);
+
+		return templateVariableGroups;
+	}
+
+	@Override
+	protected String getTemplatesConfigPath() {
+		return "com/liferay/commerce/product/type/grouped/web/internal" +
+			"/template/dependencies/portlet-display-templates.xml";
+	}
+
+	@Reference
+	private Portal _portal;
+
+}
