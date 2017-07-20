c162b35e1af1377c476f0a70e44920571f68432d
shuyangzhou <shuyang.zhou@liferay.com>
2017-07-20 09:19:45 -0700

    LPS-73405 In case we do have the userId, avoid getting the unneeded lifecycle parameter.

--- portal-impl/src/com/liferay/portal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/MainServlet.java
@@ -1249,9 +1249,9 @@ public class MainServlet extends ActionServlet {
 			HttpServletResponse response)
 		throws IOException, ServletException {
 
-		int lifecycle = ParamUtil.getInteger(request, "p_p_lifecycle");
+		if ((userId > 0) ||
+			(ParamUtil.getInteger(request, "p_p_lifecycle") == 2)) {
 
-		if ((userId > 0) || (lifecycle == 2)) {
 			sendError(
 				HttpServletResponse.SC_UNAUTHORIZED, t, request, response);
 
