058fbfb5b227d0ecedeb18e42e0c02dac8b044d0
Marco Leo <marco.leo@liferay.com>
2017-07-20 17:51:43 +0200

    COMMERCE-144 Simplify

---
+++ modules/dxp/apps/commerce/commerce-product-type-grouped-web/src/main/java/com/liferay/commerce/product/type/grouped/web/internal/display/context/GroupedCPTypeDisplayContext.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.type.grouped.web.internal.display.context;
+
+import com.liferay.commerce.product.content.web.configuration.CPContentConfigurationHelper;
+import com.liferay.commerce.product.content.web.display.context.CPTypeDisplayContext;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPAttachmentFileEntryService;
+import com.liferay.commerce.product.type.grouped.model.CPDefinitionGroupedEntry;
+import com.liferay.commerce.product.type.grouped.service.CPDefinitionGroupedEntryService;
+import com.liferay.commerce.product.type.grouped.util.comparator.CPDefinitionGroupedEntryPriorityComparator;
+import com.liferay.commerce.product.util.CPInstanceHelper;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Marco Leo
+ */
+public class GroupedCPTypeDisplayContext extends CPTypeDisplayContext {
+
+	public GroupedCPTypeDisplayContext(
+			CPAttachmentFileEntryService cpAttachmentFileEntryService,
+			CPContentConfigurationHelper cpContentConfigurationHelper,
+			CPDefinition cpDefinition,
+			CPDefinitionGroupedEntryService cpDefinitionGroupedEntryService,
+			CPInstanceHelper cpInstanceHelper,
+			HttpServletRequest httpServletRequest, Portal portal)
+		throws ConfigurationException {
+
+		super(
+			cpAttachmentFileEntryService, cpContentConfigurationHelper,
+			cpDefinition, cpInstanceHelper, httpServletRequest, portal);
+
+		_cpDefinitionGroupedEntryService = cpDefinitionGroupedEntryService;
+	}
+
+	public List<CPDefinitionGroupedEntry> getCPDefinitionGroupedEntry()
+		throws PortalException {
+
+		return _cpDefinitionGroupedEntryService.getCPDefinitionGroupedEntries(
+			cpDefinition.getCPDefinitionId(), QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS,
+			new CPDefinitionGroupedEntryPriorityComparator());
+	}
+
+	private final CPDefinitionGroupedEntryService
+		_cpDefinitionGroupedEntryService;
+
+}
---
+++ modules/dxp/apps/commerce/commerce-product-type-virtual-web/src/main/java/com/liferay/commerce/product/type/virtual/web/internal/display/context/VirtualCPTypeDisplayContext.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.type.virtual.web.internal.display.context;
+
+import com.liferay.commerce.product.content.web.configuration.CPContentConfigurationHelper;
+import com.liferay.commerce.product.content.web.display.context.CPTypeDisplayContext;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPAttachmentFileEntryService;
+import com.liferay.commerce.product.type.virtual.model.CPDefinitionVirtualSetting;
+import com.liferay.commerce.product.type.virtual.service.CPDefinitionVirtualSettingLocalService;
+import com.liferay.commerce.product.util.CPInstanceHelper;
+import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.document.library.kernel.util.DLUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringPool;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Marco Leo
+ */
+public class VirtualCPTypeDisplayContext extends CPTypeDisplayContext {
+
+	public VirtualCPTypeDisplayContext(
+			CPAttachmentFileEntryService cpAttachmentFileEntryService,
+			CPContentConfigurationHelper cpContentConfigurationHelper,
+			CPDefinition cpDefinition,
+			CPDefinitionVirtualSettingLocalService
+				cpDefinitionVirtualSettingLocalService,
+			DLAppService dlAppService, CPInstanceHelper cpInstanceHelper,
+			HttpServletRequest httpServletRequest, Portal portal)
+		throws ConfigurationException {
+
+		super(
+			cpAttachmentFileEntryService, cpContentConfigurationHelper,
+			cpDefinition, cpInstanceHelper, httpServletRequest, portal);
+
+		_dlAppService = dlAppService;
+
+		_cpDefinitionVirtualSetting =
+			cpDefinitionVirtualSettingLocalService.
+				fetchCPDefinitionVirtualSettingByCPDefinitionId(
+					cpDefinition.getCPDefinitionId());
+	}
+
+	public CPDefinitionVirtualSetting getCPDefinitionVirtualSetting() {
+		return _cpDefinitionVirtualSetting;
+	}
+
+	public String getSampleURL(ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		if (!_cpDefinitionVirtualSetting.isUseSample()) {
+			return StringPool.BLANK;
+		}
+
+		if (_cpDefinitionVirtualSetting.isUseSampleUrl()) {
+			return _cpDefinitionVirtualSetting.getSampleUrl();
+		}
+
+		FileEntry fileEntry = _dlAppService.getFileEntry(
+			_cpDefinitionVirtualSetting.getSampleFileEntryId());
+
+		return DLUtil.getDownloadURL(
+			fileEntry, fileEntry.getFileVersion(), themeDisplay,
+			StringPool.BLANK);
+	}
+
+	public boolean hasSampleURL() {
+		return _cpDefinitionVirtualSetting.isUseSample();
+	}
+
+	private final CPDefinitionVirtualSetting _cpDefinitionVirtualSetting;
+	private final DLAppService _dlAppService;
+
+}
