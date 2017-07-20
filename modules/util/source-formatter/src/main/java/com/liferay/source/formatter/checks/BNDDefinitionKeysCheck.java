ae3feb8a76e19e493b226bf3acb993eb8c2cdf02
Peter Shin <peter.shin@liferay.com>
2017-07-20 10:14:14 -0700

    LPS-72705 Autofix bnd errors caused by unneeded ",\"

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDDefinitionKeysCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDDefinitionKeysCheck.java
@@ -61,6 +61,12 @@ public class BNDDefinitionKeysCheck extends DefinitionKeysCheck {
 		Map<String, Map<String, String>> fileSpecificDefinitionKeysMap,
 		Map<String, String> generalDefinitionKeysMap) {
 
+		if (definition.endsWith(",\\")) {
+			return StringUtil.replace(
+				content, definition,
+				definition.substring(0, definition.length() - 2));
+		}
+
 		Matcher matcher = _definitionKeyPattern.matcher(definition);
 
 		if (!matcher.find()) {
