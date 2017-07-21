f542d43c0d09bd7d622d4c69cd827876059672c0
akosthurzo <akos.thurzo@liferay.com>
2017-07-21 10:17:41 +0200

    LPS-72938 Add new interface and update impl

---
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/util/LayoutsTree.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.layoutsadmin.util;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Akos Thurzo
+ */
+public interface LayoutsTree {
+
+	public String getLayoutsJSON(
+			HttpServletRequest request, long groupId, boolean privateLayout,
+			long parentLayoutId, boolean incomplete, String treeId)
+		throws Exception;
+
+	public String getLayoutsJSON(
+			HttpServletRequest request, long groupId, boolean privateLayout,
+			long layoutId, int max)
+		throws Exception;
+
+	public String getLayoutsJSON(
+			HttpServletRequest request, long groupId, boolean privateLayout,
+			long parentLayoutId, long[] expandedLayoutIds, boolean incomplete,
+			String treeId)
+		throws Exception;
+
+	public String getLayoutsJSON(
+			HttpServletRequest request, long groupId, String treeId)
+		throws Exception;
+
+}
