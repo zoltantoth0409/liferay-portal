ea69b6fc20ab0a3840e7fce6787ea6e68347ae46
Samuel Tran <samuel.tran@liferay.com>
2017-07-21 11:20:05 -0700

    LPS-65627 SF

--- tools/subrepo/push_to_subrepos.sh
+++ tools/subrepo/push_to_subrepos.sh
@@ -81,21 +81,26 @@ shift $((OPTIND-1))
 
 SUBREPO_NAME=$1
 
-if [[ "$2" ]]; then
+if [[ "$2" ]]
+then
 	help
 
 	exit 1
 fi
 
-if [[ -z "${PATTERN}" ]] && [[ -z "${OPTION_ALL}" ]] && [[ -z "${SUBREPO_NAME}" ]]; then
+if [[ -z "${PATTERN}" ]] && [[ -z "${OPTION_ALL}" ]] && [[ -z "${SUBREPO_NAME}" ]]
+then
 	help
 
 	exit 1
-elif [[ "${SUBREPO_NAME}" ]] && [[ "${PATTERN}" || "${OPTION_ALL}" ]]; then
+elif [[ "${SUBREPO_NAME}" ]] && [[ "${PATTERN}" || "${OPTION_ALL}" ]]
+then
 	error "Bad usage. Specifiying SUBREPO_NAME cannot be used together with -a or -p."
-elif [[ "${PATTERN}" ]] && [[ "${OPTION_ALL}" ]]; then
+elif [[ "${PATTERN}" ]] && [[ "${OPTION_ALL}" ]]
+then
 	error "Bad usage. The -a option cannot be combined with -p."
-elif [[ "${OPTION_FORCE}" ]] && [[ "${OPTION_ALL}" || "${PATTERN}" ]]; then
+elif [[ "${OPTION_FORCE}" ]] && [[ "${OPTION_ALL}" || "${PATTERN}" ]]
+then
 	error "Bad usage. The -f option can only be used with SUBREPO_NAME."
 fi
 
@@ -103,19 +108,23 @@ fi
 # Verify environment.
 #
 
-if [[ "${PWD}" != *"/tools/subrepo" ]]; then
+if [[ "${PWD}" != *"/tools/subrepo" ]]
+then
 	error "This script can only be run from within the tools/subrepo directory."
 fi
 
-if [[ "${PWD}" != *"liferay-portal/tools/subrepo" ]]; then
+if [[ "${PWD}" != *"liferay-portal/tools/subrepo" ]]
+then
 	error "This script can only be run from within the liferay-portal repository."
 fi
 
-if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
+if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]
+then
 	error "This script can only be run from the master branch."
 fi
 
-if [[ ! -e ../../../liferay-portal-ee ]]; then
+if [[ ! -e ../../../liferay-portal-ee ]]
+then
 	error "liferay-portal-ee does not exist at ../../../liferay-portal-ee"
 fi
 
@@ -125,11 +134,13 @@ git diff-files --quiet --ignore-submodules -- || error "Unstaged changes."
 
 git diff-index --cached --quiet HEAD -- || error "Uncommitted changes."
 
-if [[ -z "${GITHUB_API_TOKEN}" ]]; then
+if [[ -z "${GITHUB_API_TOKEN}" ]]
+then
 	GITHUB_API_TOKEN="$(git config github.token)"
 fi
 
-if [[ -z "${GITHUB_API_TOKEN}" ]]; then
+if [[ -z "${GITHUB_API_TOKEN}" ]]
+then
 	error "Your GitHub API token is not set. Set it using the GITHUB_API_TOKEN environment variable, or with 'git config github.token'."
 fi
 
@@ -144,20 +155,24 @@ SUBREPO_SEARCH_PARAMETERS=(
 	"master:../..:modules/apps"
 )
 
-if [[ "${SUBREPO_NAME}" ]]; then
-	if [[ "${OPTION_FORCE}" ]]; then
+if [[ "${SUBREPO_NAME}" ]]
+then
+	if [[ "${OPTION_FORCE}" ]]
+	then
 		VALID_BRANCHES="
 7.0.x
 master
 "
 
-		if [[ "${SUBREPO_NAME}" == *-private ]]; then
+		if [[ "${SUBREPO_NAME}" == *-private ]]
+		then
 			VALID_BRANCHES=$(echo "${VALID_BRANCHES}" | sed 's/[a-z0-9]$/&-private/')
 		fi
 
 		OUTPUT=$(curl --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO_NAME}/branches" -X GET 2>&1)
 
-		if [[ "$?" != "0" ]] || [[ "$(echo "${OUTPUT}" | grep -i '\"not found\"')" ]]; then
+		if [[ "$?" != "0" ]] || [[ "$(echo "${OUTPUT}" | grep -i '\"not found\"')" ]]
+		then
 			warn "${OUTPUT}"
 
 			error "Failed to list the remote branches at liferay/${SUBREPO_NAME} via the GitHub API."
@@ -165,48 +180,57 @@ master
 
 		REMOTE_BRANCHES=($(echo "${OUTPUT}" | grep '"name"' | sed 's/"[^"]*$//' | sed 's/.*"//'))
 
-		if [[ -z "$(echo "${REMOTE_BRANCHES[@]}" | grep '[a-zA-Z]')" ]]; then
+		if [[ -z "$(echo "${REMOTE_BRANCHES[@]}" | grep '[a-zA-Z]')" ]]
+		then
 			error "No branches found on GitHub at liferay/${SUBREPO_NAME}."
 		fi
 
 		SUBREPO_BRANCHES=()
 
-		for REMOTE_BRANCH in "${REMOTE_BRANCHES[@]}"; do
-			if [[ "$(echo "${VALID_BRANCHES}" | grep "^${REMOTE_BRANCH}\$")" ]]; then
+		for REMOTE_BRANCH in "${REMOTE_BRANCHES[@]}"
+		do
+			if [[ "$(echo "${VALID_BRANCHES}" | grep "^${REMOTE_BRANCH}\$")" ]]
+			then
 				SUBREPO_BRANCHES=("${SUBREPO_BRANCHES[@]}" "${REMOTE_BRANCH}:${SUBREPO_NAME}")
 			fi
 		done
 
-		if [[ -z "$(echo "${SUBREPO_BRANCHES[@]}" | grep '[a-zA-Z]')" ]]; then
+		if [[ -z "$(echo "${SUBREPO_BRANCHES[@]}" | grep '[a-zA-Z]')" ]]
+		then
 			error "No valid branches found on GitHub at liferay/${SUBREPO_NAME}."
 		fi
 	else
 		SUBREPO_BRANCHES=()
 
-		for SUBREPO_SEARCH_PARAMETER in "${SUBREPO_SEARCH_PARAMETERS[@]}"; do
+		for SUBREPO_SEARCH_PARAMETER in "${SUBREPO_SEARCH_PARAMETERS[@]}"
+		do
 			BRANCH_NAME="${SUBREPO_SEARCH_PARAMETER%%:*}"
 			REPO_PATH="$(echo "${SUBREPO_SEARCH_PARAMETER}" | sed 's/:[^:]*$//' | sed 's/.*://')"
 			SUBREPOS_PATH="${SUBREPO_SEARCH_PARAMETER##*:}"
 
 			GITREPO_PATH="$(git -C "${REPO_PATH}" grep -l "/${SUBREPO_NAME}.git" "${BRANCH_NAME}" -- '*.gitrepo' | grep ":${SUBREPOS_PATH}/" | sed 's/.*://')"
 
-			if [[ -z "${GITREPO_PATH}" ]]; then
+			if [[ -z "${GITREPO_PATH}" ]]
+			then
 				continue
 			fi
 
-			if [[ "$(git -C "${REPO_PATH}" grep 'mode.*=.*pull' "${BRANCH_NAME}" -- "${GITREPO_PATH##*:}")" ]]; then
+			if [[ "$(git -C "${REPO_PATH}" grep 'mode.*=.*pull' "${BRANCH_NAME}" -- "${GITREPO_PATH##*:}")" ]]
+			then
 				SUBREPO_BRANCHES=("${SUBREPO_BRANCHES[@]}" "$(git -C "${REPO_PATH}" grep 'git@github.com'  "${BRANCH_NAME}" -- "${GITREPO_PATH##*:}" | sed 's@:.*/@:@' | sed 's/\.git//')")
 			fi
 		done
 
-		if [[ -z "$(echo "${SUBREPO_BRANCHES[@]}" | grep '[a-zA-Z]')" ]]; then
+		if [[ -z "$(echo "${SUBREPO_BRANCHES[@]}" | grep '[a-zA-Z]')" ]]
+		then
 			error "Failed to find a valid .gitrepo file and validate mode for ${SUBREPO_NAME}. Use the -f flag to force update the subrepo."
 		fi
 	fi
 else
 	ALL_SUBREPOS=()
 
-	for SUBREPO_SEARCH_PARAMETER in "${SUBREPO_SEARCH_PARAMETERS[@]}"; do
+	for SUBREPO_SEARCH_PARAMETER in "${SUBREPO_SEARCH_PARAMETERS[@]}"
+	do
 		BRANCH_NAME="${SUBREPO_SEARCH_PARAMETER%%:*}"
 		REPO_PATH="$(echo "${SUBREPO_SEARCH_PARAMETER}" | sed 's/:[^:]*$//' | sed 's/.*://')"
 		SUBREPOS_PATH="${SUBREPO_SEARCH_PARAMETER##*:}"
@@ -224,8 +248,10 @@ else
 
 	SUBREPO_BRANCHES=($(printf '%s\n' "${ALL_SUBREPOS[@]}" | grep "^[^:]*:.*${PATTERN}"))
 
-	if [[ -z "$(echo "${SUBREPO_BRANCHES[@]}" | grep '[a-zA-Z]')" ]]; then
-		if [[ "${PATTERN}" ]]; then
+	if [[ -z "$(echo "${SUBREPO_BRANCHES[@]}" | grep '[a-zA-Z]')" ]]
+	then
+		if [[ "${PATTERN}" ]]
+		then
 			error "No pull-mode subrepos found matching the specified filter."
 		else
 			error "No pull-mode subrepos found."
@@ -244,14 +270,17 @@ GRADLE_FILES=(
 	"gradlew.bat"
 )
 
-for GRADLE_FILE in "${GRADLE_FILES[@]}"; do
-	if [[ ! -e "${GRADLE_FILE}" ]]; then
+for GRADLE_FILE in "${GRADLE_FILES[@]}"
+do
+	if [[ ! -e "${GRADLE_FILE}" ]]
+	then
 		error "${GRADLE_FILE} does not exist."
 	fi
 
 	CONTENT="$(cat "${GRADLE_FILE}" | base64 -b 60 | awk '{printf "%s\\n", $0}')"
 
-	if [[ "$?" != "0" ]]; then
+	if [[ "$?" != "0" ]]
+	then
 		error "Failed to parse the sha for ${GRADLE_FILE}"
 	fi
 
@@ -261,10 +290,12 @@ for GRADLE_FILE in "${GRADLE_FILES[@]}"; do
 done
 
 LOCAL_SHAS="$(
-	for FILE in "${GRADLE_FILES[@]}"; do
+	for FILE in "${GRADLE_FILES[@]}"
+	do
 		SHA="$(git hash-object "${FILE}")"
 
-		if [[ -z "${SHA}" ]]; then
+		if [[ -z "${SHA}" ]]
+		then
 			error "Unable to parse sha for the file '${FILE}'."
 		fi
 
@@ -278,14 +309,16 @@ LOCAL_SHAS="$(
 
 REMAINING_RATE="$(get_remaining_rate)"
 
-if [[ "${REMAINING_RATE}" ]]; then
+if [[ "${REMAINING_RATE}" ]]
+then
 	info "Remaining GitHub rate limit: ${REMAINING_RATE}"
 fi
 
 TOTAL_SUBREPOS="$(printf '%s\n' "${SUBREPO_BRANCHES[@]}" | sed 's/.*://' | sort -u | wc -l | sed 's/ //g')"
 TOTAL_BRANCHES="$(printf '%s\n' "${SUBREPO_BRANCHES[@]}" | wc -l | sed 's/ //g')"
 
-if [[ "${OPTION_VERIFY}" ]]; then
+if [[ "${OPTION_VERIFY}" ]]
+then
 	info "Verifying ${TOTAL_BRANCHES} branches across ${TOTAL_SUBREPOS} subrepos..."
 else
 	info "Updating ${TOTAL_BRANCHES} branches across ${TOTAL_SUBREPOS} subrepos..."
@@ -295,15 +328,18 @@ fi
 MINIMUM_API_CALLS_PER_BRANCH=3
 MAXIMUM_API_CALLS_PER_BRANCH=7
 
-if [[ "${REMAINING_RATE}" ]]; then
+if [[ "${REMAINING_RATE}" ]]
+then
 	MINIMUM_RATE=$((MINIMUM_API_CALLS_PER_BRANCH*TOTAL_BRANCHES))
 	MAXIMUM_RATE=$((MAXIMUM_API_CALLS_PER_BRANCH*TOTAL_BRANCHES))
 
-	if ((REMAINING_RATE < MINIMUM_RATE)); then
+	if ((REMAINING_RATE < MINIMUM_RATE))
+	then
 		error "Your current rate limit is insufficient, a minimum of ${MINIMUM_RATE} API calls will be made. Try again in an hour when your rate limit resets."
 	fi
 
-	if ((REMAINING_RATE < MAXIMUM_RATE)); then
+	if ((REMAINING_RATE < MAXIMUM_RATE))
+	then
 		warn "Warning: your current rate limit may be insufficient, up to ${MAXIMUM_RATE} API calls may be made."
 	fi
 fi
@@ -312,7 +348,8 @@ info
 
 TOTAL_FILES_COUNTER=0
 
-for SUBREPO_BRANCH in "${SUBREPO_BRANCHES[@]}"; do
+for SUBREPO_BRANCH in "${SUBREPO_BRANCHES[@]}"
+do
 	BRANCH="${SUBREPO_BRANCH%%:*}"
 	SUBREPO="${SUBREPO_BRANCH##*:}"
 
@@ -323,15 +360,18 @@ for SUBREPO_BRANCH in "${SUBREPO_BRANCHES[@]}"; do
 
 	GRADLE_WRAPPER_JSON=
 
-	for i in {1..2}; do
+	for i in {1..2}
+	do
 		GRADLE_WRAPPER_JSON=$(curl --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO}/contents/gradle/wrapper?ref=${BRANCH}" -X GET | tr '\n' ' ' | sed 's/ //g' | sed 's/},/\'$'\n/g')
 
-		if [[ $(echo "${GRADLE_WRAPPER_JSON}" | grep '\"sha\"') ]]; then
+		if [[ $(echo "${GRADLE_WRAPPER_JSON}" | grep '\"sha\"') ]]
+		then
 			break;
 		fi
 	done
 
-	if [[ -z $(echo "${GRADLE_WRAPPER_JSON}" | grep '\"sha\"') ]] && [[ -z $(echo "${GRADLE_WRAPPER_JSON}" | grep -i '\"notfound\"') ]]; then
+	if [[ -z $(echo "${GRADLE_WRAPPER_JSON}" | grep '\"sha\"') ]] && [[ -z $(echo "${GRADLE_WRAPPER_JSON}" | grep -i '\"notfound\"') ]]
+	then
 		warn "Skipping liferay/${SUBREPO}:${BRANCH}."
 		warn ".. Failed to get contents of the gradle/wrapper directory via the GitHub API."
 
@@ -346,15 +386,18 @@ for SUBREPO_BRANCH in "${SUBREPO_BRANCHES[@]}"; do
 	# for gradlew.
 	#
 
-	for i in {1..2}; do
+	for i in {1..2}
+	do
 		BRANCH_JSON="$(curl -s "https://api.github.com/repos/liferay/${SUBREPO}/branches/${BRANCH}" -X GET --header "Authorization: token ${GITHUB_API_TOKEN}")"
 
-		if [[ "$(echo "${BRANCH_JSON}" | grep '"sha"')" ]]; then
+		if [[ "$(echo "${BRANCH_JSON}" | grep '"sha"')" ]]
+		then
 			break;
 		fi
 	done
 
-	if [[ -z "$(echo "${BRANCH_JSON}" | grep '"sha"')" ]]; then
+	if [[ -z "$(echo "${BRANCH_JSON}" | grep '"sha"')" ]]
+	then
 		error "Failed to retrieve the branch information for ${BRANCH} via the GitHub API."
 	fi
 
@@ -364,15 +407,18 @@ for SUBREPO_BRANCH in "${SUBREPO_BRANCHES[@]}"; do
 
 	CURRENT_BRANCH_SHA="$(echo "${BRANCH_JSON}" | tr '\n' ' ' | sed 's/\[.*\]//' | sed 's/ //g' | sed 's/"tree":{[^}]*}//' | tr ',' '\n' | grep '"sha"' | head -n 1 | sed 's/"[^"]*$//' | sed 's/.*"//')"
 
-	for i in {1..2}; do
+	for i in {1..2}
+	do
 		TREE_CONTENT="$(curl -s "${CURRENT_TREE_URL}" -X GET --header "Authorization: token ${GITHUB_API_TOKEN}" | tr '\n' ' ' | sed 's/ //g' | sed 's/},/\'$'\n/g')"
 
-		if [[ "$(echo "${TREE_CONTENT}" | grep "\"${CURRENT_TREE_URL}\"")" ]]; then
+		if [[ "$(echo "${TREE_CONTENT}" | grep "\"${CURRENT_TREE_URL}\"")" ]]
+		then
 			break;
 		fi
 	done
 
-	if [[ -z "$(echo "${TREE_CONTENT}" | grep "\"${CURRENT_TREE_URL}\"")" ]]; then
+	if [[ -z "$(echo "${TREE_CONTENT}" | grep "\"${CURRENT_TREE_URL}\"")" ]]
+	then
 		error "Failed to retrieve the tree content for ${BRANCH} via the GitHub API."
 	fi
 
@@ -389,12 +435,15 @@ gradlew.bat:${GRADLEW_BAT_REMOTE_SHA}
 
 	GRADLEW_UPDATE=
 
-	for GRADLE_FILE in "${GRADLE_FILES[@]}"; do
+	for GRADLE_FILE in "${GRADLE_FILES[@]}"
+	do
 		LOCAL_SHA="$(echo "${LOCAL_SHAS}" | grep "^${GRADLE_FILE}:" | sed 's/.*://')"
 		REMOTE_SHA="$(echo "${REMOTE_SHAS}" | grep "^${GRADLE_FILE}:" | sed 's/.*://')"
 
-		if [[ "${LOCAL_SHA}" != "${REMOTE_SHA}" ]]; then
-			if [[ "${OPTION_VERIFY}" ]]; then
+		if [[ "${LOCAL_SHA}" != "${REMOTE_SHA}" ]]
+		then
+			if [[ "${OPTION_VERIFY}" ]]
+			then
 				info "liferay/${SUBREPO}:${BRANCH}:${GRADLE_FILE} content"
 			else
 				COMMIT_MESSAGE="LPS-0 Update ${GRADLE_FILE} content"
@@ -402,24 +451,29 @@ gradlew.bat:${GRADLEW_BAT_REMOTE_SHA}
 
 				DATA="\"branch\":\"${BRANCH}\",\"content\":\"${!CONTENT_VAR}\",\"message\":\"${COMMIT_MESSAGE}\",\"path\": \"${GRADLE_FILE}\""
 
-				if [[ "${REMOTE_SHA}" ]]; then
+				if [[ "${REMOTE_SHA}" ]]
+				then
 					DATA="${DATA},\"sha\": \"${REMOTE_SHA}\""
 				fi
 
-				for i in {1..2}; do
+				for i in {1..2}
+				do
 					OUTPUT=$(curl -d "{${DATA}}" --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO}/contents/${GRADLE_FILE}" -X PUT 2>&1)
 
-					if [[ "$(echo ${OUTPUT} | grep "message.*${COMMIT_MESSAGE}")" ]]; then
+					if [[ "$(echo ${OUTPUT} | grep "message.*${COMMIT_MESSAGE}")" ]]
+					then
 						break
 					fi
 				done
 
-				if [[ -z "$(echo ${OUTPUT} | grep "message.*${COMMIT_MESSAGE}")" ]]; then
+				if [[ -z "$(echo ${OUTPUT} | grep "message.*${COMMIT_MESSAGE}")" ]]
+				then
 					warn "${OUTPUT}"
 
 					warn "Failed to update ${GRADLE_FILE} content at liferay/${SUBREPO}:${BRANCH}."
 
-					if [[ "${GRADLE_FILE}" == "gradlew" ]]; then
+					if [[ "${GRADLE_FILE}" == "gradlew" ]]
+					then
 						GRADLEW_UPDATE=fail
 					fi
 
@@ -430,7 +484,8 @@ gradlew.bat:${GRADLEW_BAT_REMOTE_SHA}
 				CURRENT_TREE_SHA="$(echo "${OUTPUT}" | grep '/git/trees/' | sed 's@.*/@@' | sed 's/".*//')"
 			fi
 
-			if [[ "${GRADLE_FILE}" == "gradlew" ]]; then
+			if [[ "${GRADLE_FILE}" == "gradlew" ]]
+			then
 				GRADLEW_UPDATE=success
 			fi
 
@@ -438,21 +493,26 @@ gradlew.bat:${GRADLEW_BAT_REMOTE_SHA}
 		fi
 	done
 
-	if [[ "${GRADLEW_FILE_MODE}" != "100755" ]] && [[ "${GRADLEW_UPDATE}" != "fail" ]]; then
-		if [[ "${OPTION_VERIFY}" ]]; then
+	if [[ "${GRADLEW_FILE_MODE}" != "100755" ]] && [[ "${GRADLEW_UPDATE}" != "fail" ]]
+	then
+		if [[ "${OPTION_VERIFY}" ]]
+		then
 			info "liferay/${SUBREPO}:${BRANCH}:gradlew file mode"
 		else
 			FILE_SHA="$(echo "${LOCAL_SHAS}" | grep "^gradlew:" | sed 's/.*://')"
 
-			for i in {1..2}; do
+			for i in {1..2}
+			do
 				OUTPUT=$(curl -d "{\"base_tree\": \"${CURRENT_TREE_SHA}\",\"tree\":[{\"mode\":\"100755\",\"path\": \"gradlew\",\"sha\":\"${FILE_SHA}\"}]}" --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO}/git/trees" -X POST)
 
-				if [[ "$(echo ${OUTPUT} | grep '"sha"')" ]]; then
+				if [[ "$(echo ${OUTPUT} | grep '"sha"')" ]]
+				then
 					break
 				fi
 			done
 
-			if [[ -z "$(echo ${OUTPUT} | grep '"sha"')" ]]; then
+			if [[ -z "$(echo ${OUTPUT} | grep '"sha"')" ]]
+			then
 				warn "${OUTPUT}"
 
 				warn "Failed to update gradlew file mode (creating tree via GitHub API) at liferay/${SUBREPO}:${BRANCH}."
@@ -462,19 +522,23 @@ gradlew.bat:${GRADLEW_BAT_REMOTE_SHA}
 
 			NEW_TREE_SHA="$(echo "${OUTPUT}" | tr '\n' '%' | sed 's/\[.*\]//' | sed 's/ *//g' | sed 's/.*"sha":"//' | sed 's/".*//')"
 
-			if [[ "${NEW_TREE_SHA}" == "${CURRENT_TREE_SHA}" ]]; then
+			if [[ "${NEW_TREE_SHA}" == "${CURRENT_TREE_SHA}" ]]
+			then
 				continue
 			fi
 
-			for i in {1..2}; do
+			for i in {1..2}
+			do
 				OUTPUT=$(curl -d "{\"message\": \"LPS-0 Update gradlew file mode\",\"tree\": \"${NEW_TREE_SHA}\",\"parents\": [\"${CURRENT_BRANCH_SHA}\"]}" --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO}/git/commits" -X POST)
 
-				if [[ "$(echo ${OUTPUT} | grep '"sha"')" ]]; then
+				if [[ "$(echo ${OUTPUT} | grep '"sha"')" ]]
+				then
 					break
 				fi
 			done
 
-			if [[ -z "$(echo ${OUTPUT} | grep '"sha"')" ]]; then
+			if [[ -z "$(echo ${OUTPUT} | grep '"sha"')" ]]
+			then
 				warn "${OUTPUT}"
 
 				warn "Failed to update gradlew file mode (creating commit via GitHub API) at liferay/${SUBREPO}:${BRANCH}."
@@ -484,15 +548,18 @@ gradlew.bat:${GRADLEW_BAT_REMOTE_SHA}
 
 			NEW_COMMIT_SHA="$(echo "${OUTPUT}" | grep -v "${CURRENT_BRANCH_SHA}" | grep -v "${NEW_TREE_SHA}" | grep '"sha"' | sed 's/"[^"]*$//' | sed 's/.*"//')"
 
-			for i in {1..2}; do
+			for i in {1..2}
+			do
 				OUTPUT=$(curl -d "{\"sha\": \"${NEW_COMMIT_SHA}\"}" --header "Authorization: token ${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/liferay/${SUBREPO}/git/refs/heads/${BRANCH}" -X PATCH)
 
-				if [[ "$(echo ${OUTPUT} | grep '"ref"')" ]]; then
+				if [[ "$(echo ${OUTPUT} | grep '"ref"')" ]]
+				then
 					break
 				fi
 			done
 
-			if [[ -z "$(echo ${OUTPUT} | grep '"ref"')" ]]; then
+			if [[ -z "$(echo ${OUTPUT} | grep '"ref"')" ]]
+			then
 				warn "${OUTPUT}"
 
 				warn "Failed to update gradlew file mode (updating ref via GitHub API) at liferay/${SUBREPO}:${BRANCH}."
@@ -501,7 +568,8 @@ gradlew.bat:${GRADLEW_BAT_REMOTE_SHA}
 			fi
 		fi
 
-		if [[ -z "${GRADLEW_UPDATE}" ]]; then
+		if [[ -z "${GRADLEW_UPDATE}" ]]
+		then
 			let TOTAL_FILES_COUNTER++
 		fi
 	fi
@@ -509,8 +577,10 @@ done
 
 info
 
-if ((TOTAL_FILES_COUNTER > 0)); then
-	if [[ "${OPTION_VERIFY}" ]]; then
+if ((TOTAL_FILES_COUNTER > 0))
+then
+	if [[ "${OPTION_VERIFY}" ]]
+	then
 		info "${TOTAL_FILES_COUNTER} files require updating."
 	else
 		info "${TOTAL_FILES_COUNTER} files were successfully updated."
@@ -521,6 +591,7 @@ fi
 
 REMAINING_RATE="$(get_remaining_rate)"
 
-if [[ "${REMAINING_RATE}" ]]; then
+if [[ "${REMAINING_RATE}" ]]
+then
 	info "Remaining GitHub rate limit: ${REMAINING_RATE}"
 fi
