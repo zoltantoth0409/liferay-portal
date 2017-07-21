4ac34c5b6d43ff0946d5a741db607d608115daaa
Brian Chan <brian.chan@liferay.com>
2017-07-21 13:58:45 -0700

    LPS-73586 javadoc

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -682,7 +682,7 @@ public class PropsValues {
 	public static final int INDEX_SEARCH_COLLATED_SPELL_CHECK_RESULT_SCORES_THRESHOLD = GetterUtil.getInteger(PropsUtil.get(PropsKeys.INDEX_SEARCH_COLLATED_SPELL_CHECK_RESULT_SCORES_THRESHOLD));
 
 	/**
-	 * @deprecated As of 7.0.0
+	 * @deprecated As of 7.0.0, with no direct replacement
 	 */
 	@Deprecated
 	public static final boolean INDEX_SEARCH_HIGHLIGHT_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.INDEX_SEARCH_HIGHLIGHT_ENABLED));
