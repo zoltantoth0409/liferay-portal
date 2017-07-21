8ea447ccba827239941a03d6174cea63c9cfa685
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2017-07-21 14:42:55 +0200

    LPS-55244 Fix race condition - delete associated permissions only when the resource block was deleted

--- portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceBlockLocalServiceImpl.java
@@ -477,10 +477,10 @@ public class ResourceBlockLocalServiceImpl
 
 						qPos.add(resourceBlockId);
 
-						sqlQuery.executeUpdate();
-
-						resourceBlockPermissionLocalService.
-							deleteResourceBlockPermissions(resourceBlockId);
+						if (sqlQuery.executeUpdate() > 0) {
+							resourceBlockPermissionLocalService.
+								deleteResourceBlockPermissions(resourceBlockId);
+						}
 
 						PermissionCacheUtil.clearResourceBlockCache(
 							resourceBlock.getCompanyId(),
