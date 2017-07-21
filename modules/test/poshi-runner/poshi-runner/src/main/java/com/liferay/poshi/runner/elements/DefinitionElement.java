942d7505733b54646536c5ff1256a7334aaabe75
Kenji Heigel <kenji.heigel@liferay.com>
2017-07-21 16:26:09 -0700

    LRQA-32801 'var' elements in the definition level need to be translated to the readable syntax

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/DefinitionElement.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/DefinitionElement.java
@@ -73,6 +73,14 @@ public class DefinitionElement extends PoshiElement {
 
 		content.append("\n");
 
+		for (PoshiElement poshiElement :
+			toPoshiElements(elements("var"))) {
+
+			content.append(poshiElement.toReadableSyntax());
+		}
+
+		content.append("\n");
+
 		for (PoshiElement poshiElement : toPoshiElements(elements("set-up"))) {
 			content.append(poshiElement.toReadableSyntax());
 		}
