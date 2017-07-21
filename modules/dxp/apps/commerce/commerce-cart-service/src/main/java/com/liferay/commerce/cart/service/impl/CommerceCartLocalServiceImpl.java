409575dc860ecaf6da451551bc0c6bd16ef400b9
Marco Leo <marco.leo@liferay.com>
2017-07-21 11:55:27 +0200

    COMMERCE-31 Remore cart user id

--- modules/dxp/apps/commerce/commerce-cart-service/src/main/java/com/liferay/commerce/cart/service/impl/CommerceCartLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-cart-service/src/main/java/com/liferay/commerce/cart/service/impl/CommerceCartLocalServiceImpl.java
@@ -121,7 +121,6 @@ public class CommerceCartLocalServiceImpl
 		}
 
 		return addCommerceCart(
-			serviceContext.getUserId(),
 			CommerceCartConstants.COMMERCE_CART_DEFAULT_TITLE, type,
 			serviceContext);
 	}
