7daecb7312c1223f3c93a1295013203cdfba97cf
tina.tian <tina.tian@liferay.com>
2017-07-25 10:18:06 -0700

    LPS-73659 Add portal-cache-ehcache-multiple to support RMI cache replicator and bootstrap loader

---
+++ modules/apps/foundation/portal-cache/portal-cache-ehcache-multiple/src/main/java/com/liferay/portal/cache/ehcache/multiple/configuration/EhcacheMultipleConfiguration.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cache.ehcache.multiple.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Tina Tian
+ */
+@ExtendedObjectClassDefinition(category = "foundation")
+@Meta.OCD(
+	id = "com.liferay.portal.cache.ehcache.multiple.configuration.EhcacheMultipleConfiguration",
+	localization = "content/Language",
+	name = "ehcache-multiple-configuration-name"
+)
+public interface EhcacheMultipleConfiguration {
+
+	@Meta.AD(
+		deflt = "com.liferay.portal.cache.ehcache.multiple.internal.bootstrap.RMIBootstrapCacheLoaderFactory",
+		required = false
+	)
+	public String bootstrapCacheLoaderFactoryClass();
+
+	@Meta.AD(
+		deflt = "com.liferay.portal.cache.ehcache.multiple.internal.distribution.RMICacheReplicatorFactory",
+		required = false
+	)
+	public String cacheReplicatorFactoryClass();
+
+}
---
+++ modules/apps/foundation/portal-cache/portal-cache-ehcache-multiple/src/main/java/com/liferay/portal/cache/ehcache/multiple/internal/portal/profile/ModulePortalProfile.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.cache.ehcache.multiple.internal.portal.profile;
+
+import com.liferay.portal.cache.ehcache.multiple.internal.bootstrap.EhcachePortalCacheBootstrapLoaderFactory;
+import com.liferay.portal.cache.ehcache.multiple.internal.distribution.EhcachePortalCacheReplicatorFactory;
+import com.liferay.portal.profile.BaseDSModulePortalProfile;
+import com.liferay.portal.profile.PortalProfile;
+
+import java.util.Collections;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Shuyang Zhou
+ */
+@Component(immediate = true, service = PortalProfile.class)
+public class ModulePortalProfile extends BaseDSModulePortalProfile {
+
+	@Activate
+	public void activate(ComponentContext componentContext) {
+		init(
+			componentContext,
+			Collections.singleton(PortalProfile.PORTAL_PROFILE_NAME_CE),
+			EhcachePortalCacheBootstrapLoaderFactory.class.getName(),
+			EhcachePortalCacheReplicatorFactory.class.getName());
+	}
+
+}
