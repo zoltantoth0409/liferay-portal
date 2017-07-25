ff102338b75aa91a32fbed530d68ca304ed2743c
Olaf Kock <github@olafkock.de>
2017-07-25 11:27:14 +0200

    LPS-88554 Update setenv.bat
    This file referenced jre1.6.0_20 for a long time, containing dead code for a long time. Since it also doesn't have Windows line-ending-encoding, editing it in Notepad is quite hard. It's best to keep this file with a single line in the default deployment. As the deleted code was ineffective anyways, I didn't bother opening an issue - hope this is sufficient.

--- tools/servers/tomcat/bin/setenv.bat
+++ tools/servers/tomcat/bin/setenv.bat
@@ -1,9 +1,1 @@
-if exist "%CATALINA_HOME%/jre@java.version@/win" (
-	if not "%JAVA_HOME%" == "" (
-		set JAVA_HOME=
-	)
-
-	set "JRE_HOME=%CATALINA_HOME%/jre@java.version@/win"
-)
-
 set "CATALINA_OPTS=%CATALINA_OPTS% -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xms1280m -Xmx1280m -XX:MaxNewSize=256m -XX:NewSize=256m -XX:MaxMetaspaceSize=384m -XX:MetaspaceSize=384m -XX:SurvivorRatio=7"
