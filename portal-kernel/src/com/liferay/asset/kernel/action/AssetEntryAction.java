e8af9efa8671b6f0a703e72e15144d314c8058a4
Juergen Kappler <jurgen.kappler@liferay.com>
2017-07-25 14:08:28 +0200

    LPS-67760 New interface for asset entry actions

---
+++ portal-kernel/src/com/liferay/asset/kernel/action/AssetEntryAction.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.kernel.action;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Jürgen Kappler
+ */
+@ProviderType
+public interface AssetEntryAction<T> {
+
+	public String getDialogTitle(Locale locale);
+
+	public String getMenuTitle(Locale locale);
+
+	public String getPortletURL(
+			HttpServletRequest request, AssetRenderer<T> assetRenderer)
+		throws PortalException;
+
+	public boolean hasPermission(
+			PermissionChecker permissionChecker, AssetRenderer<T> assetRenderer)
+		throws PortalException;
+
+}
