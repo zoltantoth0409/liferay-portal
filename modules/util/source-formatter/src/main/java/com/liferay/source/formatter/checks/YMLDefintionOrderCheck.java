dc3e51208a635a36f6ef771c70d055c3095d31fb
Peter Shin <peter.shin@liferay.com>
2017-07-25 09:01:52 -0700

    LPS-72705 Scope travis definition logic

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLDefintionOrderCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLDefintionOrderCheck.java
@@ -35,7 +35,7 @@ public class YMLDefintionOrderCheck extends BaseFileCheck {
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
 
-		return _sortDefinitions(content);
+		return _sortDefinitions(fileName, content);
 	}
 
 	private List<String> _getDefinitions(String content) {
@@ -50,10 +50,11 @@ public class YMLDefintionOrderCheck extends BaseFileCheck {
 		return definitions;
 	}
 
-	private String _sortDefinitions(String content) {
+	private String _sortDefinitions(String fileName, String content) {
 		List<String> definitions = _getDefinitions(content);
 
-		DefinitionComparator definitionComparator = new DefinitionComparator();
+		DefinitionComparator definitionComparator = new DefinitionComparator(
+			fileName);
 
 		for (int i = 1; i < definitions.size(); i++) {
 			String definition = definitions.get(i);
@@ -83,16 +84,22 @@ public class YMLDefintionOrderCheck extends BaseFileCheck {
 	private static class DefinitionComparator
 		implements Comparator<String>, Serializable {
 
+		public DefinitionComparator(String fileName) {
+			_fileName = fileName;
+		}
+
 		@Override
 		public int compare(String definition1, String definition2) {
 			String definitionKey1 = _getDefinitionKey(definition1);
 			String definitionKey2 = _getDefinitionKey(definition2);
 
-			int definitionKeyWeight1 = _getDefinitionKeyWeight(definitionKey1);
-			int definitionKeyWeight2 = _getDefinitionKeyWeight(definitionKey2);
+			if (_fileName.endsWith("/.travis.yml")) {
+				int weight1 = _getTravisDefinitionKeyWeight(definitionKey1);
+				int weight2 = _getTravisDefinitionKeyWeight(definitionKey2);
 
-			if ((definitionKeyWeight1 != -1) || (definitionKeyWeight2 != -1)) {
-				return definitionKeyWeight1 - definitionKeyWeight2;
+				if ((weight1 != -1) || (weight2 != -1)) {
+					return weight1 - weight2;
+				}
 			}
 
 			return definitionKey1.compareTo(definitionKey2);
@@ -108,33 +115,34 @@ public class YMLDefintionOrderCheck extends BaseFileCheck {
 			return definition;
 		}
 
-		private int _getDefinitionKeyWeight(String definitionKey) {
-			if (_definitionKeyWeightMap.containsKey(definitionKey)) {
-				return _definitionKeyWeightMap.get(definitionKey);
+		private int _getTravisDefinitionKeyWeight(String definitionKey) {
+			if (_travisDefinitionKeyWeightMap.containsKey(definitionKey)) {
+				return _travisDefinitionKeyWeightMap.get(definitionKey);
 			}
 
 			return -1;
 		}
 
-		private static final Map<String, Integer> _definitionKeyWeightMap =
-			new HashMap<>();
+		private static final Map<String, Integer>
+			_travisDefinitionKeyWeightMap = new HashMap<>();
 
 		static {
-			_definitionKeyWeightMap.put("after_deploy", 11);
-			_definitionKeyWeightMap.put("after_failure", 8);
-			_definitionKeyWeightMap.put("after_script", 12);
-			_definitionKeyWeightMap.put("after_success", 7);
-			_definitionKeyWeightMap.put("before_cache", 5);
-			_definitionKeyWeightMap.put("before_deploy", 9);
-			_definitionKeyWeightMap.put("before_install", 1);
-			_definitionKeyWeightMap.put("before_script", 3);
-			_definitionKeyWeightMap.put("cache", 6);
-			_definitionKeyWeightMap.put("deploy", 10);
-			_definitionKeyWeightMap.put("install", 2);
-			_definitionKeyWeightMap.put("script", 4);
+			_travisDefinitionKeyWeightMap.put("after_deploy", 11);
+			_travisDefinitionKeyWeightMap.put("after_failure", 8);
+			_travisDefinitionKeyWeightMap.put("after_script", 12);
+			_travisDefinitionKeyWeightMap.put("after_success", 7);
+			_travisDefinitionKeyWeightMap.put("before_cache", 5);
+			_travisDefinitionKeyWeightMap.put("before_deploy", 9);
+			_travisDefinitionKeyWeightMap.put("before_install", 1);
+			_travisDefinitionKeyWeightMap.put("before_script", 3);
+			_travisDefinitionKeyWeightMap.put("cache", 6);
+			_travisDefinitionKeyWeightMap.put("deploy", 10);
+			_travisDefinitionKeyWeightMap.put("install", 2);
+			_travisDefinitionKeyWeightMap.put("script", 4);
 		}
 
 		private final Pattern _definitionKeyPattern = Pattern.compile("(.*?):");
+		private final String _fileName;
 
 	}
 
