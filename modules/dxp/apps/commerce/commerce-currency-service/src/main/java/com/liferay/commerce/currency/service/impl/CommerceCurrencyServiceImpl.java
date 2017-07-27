65082f6821b49cf8e5d5df7af9b2fd3ca37a58ca
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-07-27 10:47:16 -0700

    COMMERCE-158 CommerceCurrency remote service

--- modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/service/impl/CommerceCurrencyServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/service/impl/CommerceCurrencyServiceImpl.java
@@ -14,11 +14,115 @@
 
 package com.liferay.commerce.currency.service.impl;
 
+import com.liferay.commerce.currency.constants.CommerceCurrencyActionKeys;
+import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.currency.service.base.CommerceCurrencyServiceBaseImpl;
+import com.liferay.commerce.currency.service.permission.CommerceCurrencyPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Andrea Di Giorgi
  */
 public class CommerceCurrencyServiceImpl
 	extends CommerceCurrencyServiceBaseImpl {
+
+	@Override
+	public CommerceCurrency addCommerceCurrency(
+			Map<Locale, String> codeMap, Map<Locale, String> nameMap,
+			double rate, String roundingType, boolean primary, double priority,
+			boolean active, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceCurrencyPermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			CommerceCurrencyActionKeys.MANAGE_COMMERCE_CURRENCIES);
+
+		return commerceCurrencyLocalService.addCommerceCurrency(
+			codeMap, nameMap, rate, roundingType, primary, priority, active,
+			serviceContext);
+	}
+
+	@Override
+	public void deleteCommerceCurrency(long commerceCurrencyId)
+		throws PortalException {
+
+		CommerceCurrency commerceCurrency =
+			commerceCurrencyPersistence.findByPrimaryKey(commerceCurrencyId);
+
+		CommerceCurrencyPermission.check(
+			getPermissionChecker(), commerceCurrency.getGroupId(),
+			CommerceCurrencyActionKeys.MANAGE_COMMERCE_CURRENCIES);
+
+		commerceCurrencyLocalService.deleteCommerceCurrency(commerceCurrency);
+	}
+
+	@Override
+	public CommerceCurrency fetchPrimaryCommerceCurrency(long groupId) {
+		return commerceCurrencyLocalService.fetchPrimaryCommerceCurrency(
+			groupId);
+	}
+
+	@Override
+	public List<CommerceCurrency> getCommerceCurrencies(
+		long groupId, boolean active, int start, int end,
+		OrderByComparator<CommerceCurrency> orderByComparator) {
+
+		return commerceCurrencyLocalService.getCommerceCurrencies(
+			groupId, active, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<CommerceCurrency> getCommerceCurrencies(
+		long groupId, int start, int end,
+		OrderByComparator<CommerceCurrency> orderByComparator) {
+
+		return commerceCurrencyLocalService.getCommerceCurrencies(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceCurrenciesCount(long groupId) {
+		return commerceCurrencyLocalService.getCommerceCurrenciesCount(groupId);
+	}
+
+	@Override
+	public int getCommerceCurrenciesCount(long groupId, boolean active) {
+		return commerceCurrencyLocalService.getCommerceCurrenciesCount(
+			groupId, active);
+	}
+
+	@Override
+	public CommerceCurrency getCommerceCurrency(long commerceCurrencyId)
+		throws PortalException {
+
+		return commerceCurrencyLocalService.getCommerceCurrency(
+			commerceCurrencyId);
+	}
+
+	@Override
+	public CommerceCurrency updateCommerceCurrency(
+			long commerceCurrencyId, Map<Locale, String> codeMap,
+			Map<Locale, String> nameMap, double rate, String roundingType,
+			boolean primary, double priority, boolean active,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceCurrency commerceCurrency =
+			commerceCurrencyPersistence.findByPrimaryKey(commerceCurrencyId);
+
+		CommerceCurrencyPermission.check(
+			getPermissionChecker(), commerceCurrency.getGroupId(),
+			CommerceCurrencyActionKeys.MANAGE_COMMERCE_CURRENCIES);
+
+		return commerceCurrencyLocalService.updateCommerceCurrency(
+			commerceCurrency.getCommerceCurrencyId(), codeMap, nameMap, rate,
+			roundingType, primary, priority, active, serviceContext);
+	}
+
 }
