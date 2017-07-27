fc12f0fe48f5c05f9fdfb8704dad7e8b1688fc60
Matthew Tambara <matthew.tambara@liferay.com>
2017-07-27 16:04:23 -0700

    LPS-73760 Move runtime LPKG creation to util

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test/src/test/java/com/liferay/portal/lpkg/deployer/persistence/LPKGPersistenceTest.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test/src/test/java/com/liferay/portal/lpkg/deployer/persistence/LPKGPersistenceTest.java
@@ -14,31 +14,14 @@
 
 package com.liferay.portal.lpkg.deployer.persistence;
 
-import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
-import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
-import com.liferay.portal.kernel.util.StreamUtil;
-import com.liferay.portal.kernel.util.StringBundler;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import com.liferay.portal.lpkg.deployer.util.LPKGTestUtil;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.util.jar.Attributes;
-import java.util.jar.JarFile;
-import java.util.jar.JarOutputStream;
-import java.util.jar.Manifest;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
 import org.junit.Assert;
 import org.junit.Test;
-
-import org.osgi.framework.Constants;
 
 /**
  * @author Matthew Tambara
@@ -56,79 +39,7 @@ public class LPKGPersistenceTest {
 
 		Files.createFile(path);
 
-		_createLPKG(path);
-	}
-
-	private InputStream _createJAR() throws IOException {
-		try (UnsyncByteArrayOutputStream unsyncByteArrayOutputStream =
-				new UnsyncByteArrayOutputStream()) {
-
-			try (JarOutputStream jarOutputStream = new JarOutputStream(
-					unsyncByteArrayOutputStream)) {
-
-				Manifest manifest = new Manifest();
-
-				Attributes attributes = manifest.getMainAttributes();
-
-				attributes.putValue(Constants.BUNDLE_MANIFESTVERSION, "2");
-				attributes.putValue(
-					Constants.BUNDLE_SYMBOLICNAME, _SYMBOLIC_NAME);
-				attributes.putValue(Constants.BUNDLE_VERSION, "1.0.0");
-				attributes.putValue("Manifest-Version", "2");
-
-				jarOutputStream.putNextEntry(
-					new ZipEntry(JarFile.MANIFEST_NAME));
-
-				manifest.write(jarOutputStream);
-
-				jarOutputStream.closeEntry();
-			}
-
-			return new UnsyncByteArrayInputStream(
-				unsyncByteArrayOutputStream.unsafeGetByteArray(), 0,
-				unsyncByteArrayOutputStream.size());
-		}
-	}
-
-	private void _createLPKG(Path path) throws IOException {
-		try (ZipOutputStream zipOutputStream = new ZipOutputStream(
-				new FileOutputStream(path.toFile()))) {
-
-			zipOutputStream.putNextEntry(
-				new ZipEntry("liferay-marketplace.properties"));
-
-			StringBundler sb = new StringBundler(11);
-
-			sb.append("bundles=");
-			sb.append(_SYMBOLIC_NAME);
-			sb.append("#1.0.0##\n");
-			sb.append("category=Test\n");
-			sb.append("context-names=\n");
-			sb.append("description=Test\n");
-			sb.append("icon-url=https://www.liferay.com/web/guest");
-			sb.append("/marketplace/-/mp/asset/icon/71985553\n");
-			sb.append("remote-app-id=Test\n");
-			sb.append("title=Test\n");
-			sb.append("version=1.0");
-
-			String properties = sb.toString();
-
-			zipOutputStream.write(properties.getBytes());
-
-			zipOutputStream.closeEntry();
-
-			zipOutputStream.putNextEntry(
-				new ZipEntry(_SYMBOLIC_NAME.concat("-1.0.0.jar")));
-
-			try (InputStream inputStream = _createJAR();
-				OutputStream outputStream = StreamUtil.uncloseable(
-					zipOutputStream)) {
-
-				StreamUtil.transfer(inputStream, outputStream);
-			}
-
-			zipOutputStream.closeEntry();
-		}
+		LPKGTestUtil.createLPKG(path, _SYMBOLIC_NAME);
 	}
 
 	private static final String _SYMBOLIC_NAME = "lpkg.persistence.test";
