3f83c7fbb7cc779644722c2f8e97f65074033d67
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-07-27 09:25:51 -0700

    COMMERCE-158 CommerceCurrency local service

--- modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/service/impl/CommerceCurrencyLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/service/impl/CommerceCurrencyLocalServiceImpl.java
@@ -14,11 +14,171 @@
 
 package com.liferay.commerce.currency.service.impl;
 
+import com.liferay.commerce.currency.exception.CommerceCurrencyCodeException;
+import com.liferay.commerce.currency.exception.CommerceCurrencyNameException;
+import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.currency.service.base.CommerceCurrencyLocalServiceBaseImpl;
+import com.liferay.commerce.currency.util.comparator.CommerceCurrencyPriorityComparator;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Andrea Di Giorgi
  */
 public class CommerceCurrencyLocalServiceImpl
 	extends CommerceCurrencyLocalServiceBaseImpl {
+
+	@Override
+	public CommerceCurrency addCommerceCurrency(
+			Map<Locale, String> codeMap, Map<Locale, String> nameMap,
+			double rate, String roundingType, boolean primary, double priority,
+			boolean active, ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		if (primary) {
+			rate = 1;
+		}
+
+		validate(0, groupId, codeMap, nameMap, primary);
+
+		long commerceCurrencyId = counterLocalService.increment();
+
+		CommerceCurrency commerceCurrency = commerceCurrencyPersistence.create(
+			commerceCurrencyId);
+
+		commerceCurrency.setUuid(serviceContext.getUuid());
+		commerceCurrency.setGroupId(groupId);
+		commerceCurrency.setCompanyId(user.getCompanyId());
+		commerceCurrency.setUserId(user.getUserId());
+		commerceCurrency.setUserName(user.getFullName());
+		commerceCurrency.setCodeMap(codeMap);
+		commerceCurrency.setNameMap(nameMap);
+		commerceCurrency.setRate(rate);
+		commerceCurrency.setRoundingType(roundingType);
+		commerceCurrency.setPrimary(primary);
+		commerceCurrency.setPriority(priority);
+		commerceCurrency.setActive(active);
+
+		commerceCurrencyPersistence.update(commerceCurrency);
+
+		return commerceCurrency;
+	}
+
+	@Override
+	public void deleteCommerceCurrencies(long groupId) {
+		commerceCurrencyPersistence.removeByGroupId(groupId);
+	}
+
+	@Override
+	public CommerceCurrency fetchPrimaryCommerceCurrency(long groupId) {
+		return commerceCurrencyPersistence.fetchByG_P_A_First(
+			groupId, true, true, new CommerceCurrencyPriorityComparator());
+	}
+
+	@Override
+	public List<CommerceCurrency> getCommerceCurrencies(
+		long groupId, boolean active, int start, int end,
+		OrderByComparator<CommerceCurrency> orderByComparator) {
+
+		return commerceCurrencyPersistence.findByG_A(
+			groupId, active, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<CommerceCurrency> getCommerceCurrencies(
+		long groupId, int start, int end,
+		OrderByComparator<CommerceCurrency> orderByComparator) {
+
+		return commerceCurrencyPersistence.findByGroupId(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceCurrenciesCount(long groupId) {
+		return commerceCurrencyPersistence.countByGroupId(groupId);
+	}
+
+	@Override
+	public int getCommerceCurrenciesCount(long groupId, boolean active) {
+		return commerceCurrencyPersistence.countByG_A(groupId, active);
+	}
+
+	@Override
+	public CommerceCurrency updateCommerceCurrency(
+			long commerceCurrencyId, Map<Locale, String> codeMap,
+			Map<Locale, String> nameMap, double rate, String roundingType,
+			boolean primary, double priority, boolean active,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceCurrency commerceCurrency =
+			commerceCurrencyPersistence.findByPrimaryKey(commerceCurrencyId);
+
+		if (primary) {
+			rate = 1;
+		}
+
+		validate(
+			commerceCurrency.getCommerceCurrencyId(),
+			commerceCurrency.getGroupId(), codeMap, nameMap, primary);
+
+		commerceCurrency.setCodeMap(codeMap);
+		commerceCurrency.setNameMap(nameMap);
+		commerceCurrency.setRate(rate);
+		commerceCurrency.setRoundingType(roundingType);
+		commerceCurrency.setPrimary(primary);
+		commerceCurrency.setPriority(priority);
+		commerceCurrency.setActive(active);
+
+		commerceCurrencyPersistence.update(commerceCurrency);
+
+		return commerceCurrency;
+	}
+
+	protected void validate(
+			long commerceCurrencyId, long groupId, Map<Locale, String> codeMap,
+			Map<Locale, String> nameMap, boolean primary)
+		throws PortalException {
+
+		Locale locale = LocaleUtil.getSiteDefault();
+
+		String code = codeMap.get(locale);
+
+		if (Validator.isNull(code)) {
+			throw new CommerceCurrencyCodeException();
+		}
+
+		String name = nameMap.get(locale);
+
+		if (Validator.isNull(name)) {
+			throw new CommerceCurrencyNameException();
+		}
+
+		if (primary) {
+			List<CommerceCurrency> commerceCurrencies =
+				commerceCurrencyPersistence.findByG_P(groupId, primary);
+
+			for (CommerceCurrency commerceCurrency : commerceCurrencies) {
+				if (commerceCurrency.getCommerceCurrencyId() !=
+						commerceCurrencyId) {
+
+					commerceCurrency.setPrimary(false);
+
+					commerceCurrencyPersistence.update(commerceCurrency);
+				}
+			}
+		}
+	}
+
 }
