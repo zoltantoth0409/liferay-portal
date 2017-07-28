220558c2ddd8af7f008ce22efaca8bf6c140f3f0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-07-29 00:36:28 +0100

    LPS-73944 Update Message Boards verify test

--- modules/apps/collaboration/message-boards/message-boards-test/src/testIntegration/java/com/liferay/message/boards/verify/test/MessageBoardsServiceVerifyProcessTest.java
+++ modules/apps/collaboration/message-boards/message-boards-test/src/testIntegration/java/com/liferay/message/boards/verify/test/MessageBoardsServiceVerifyProcessTest.java
@@ -14,18 +14,26 @@
 
 package com.liferay.message.boards.verify.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.verify.VerifyMessageBoards;
 import com.liferay.portal.verify.VerifyProcess;
 import com.liferay.portal.verify.test.BaseVerifyProcessTestCase;
+import com.liferay.registry.Filter;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceTracker;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.runner.RunWith;
 
 /**
  * @author Manuel de la Peña
  */
+@RunWith(Arquillian.class)
 public class MessageBoardsServiceVerifyProcessTest
 	extends BaseVerifyProcessTestCase {
 
@@ -34,9 +42,29 @@ public class MessageBoardsServiceVerifyProcessTest
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
+	@BeforeClass
+	public static void setUpClass() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		Filter filter = registry.getFilter(
+			"(&(objectClass=" + VerifyProcess.class.getName() +
+				")(verify.process.name=com.liferay.message.boards.service))");
+
+		_serviceTracker = registry.trackServices(filter);
+
+		_serviceTracker.open();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_serviceTracker.close();
+	}
+
 	@Override
 	protected VerifyProcess getVerifyProcess() {
-		return new VerifyMessageBoards();
+		return _serviceTracker.getService();
 	}
+
+	private static ServiceTracker<VerifyProcess, VerifyProcess> _serviceTracker;
 
 }
