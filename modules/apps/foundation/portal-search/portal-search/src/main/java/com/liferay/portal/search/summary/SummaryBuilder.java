aaf318a7a6ddde94668689c232e347ecdccb9c37
André de Oliveira <andre.oliveira@liferay.com>
2017-07-28 14:40:33 -0700

    LPS-72670 portal-search: Add SummaryBuilder to accept pre-highlighted snippets from search engine
    https://issues.liferay.com/browse/LPS-72670

---
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/summary/SummaryBuilder.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.summary;
+
+import aQute.bnd.annotation.ProviderType;
+
+import java.util.Locale;
+
+/**
+ * @author André de Oliveira
+ * @author Bryan Engler
+ * @author Ryan Park
+ * @author Tibor Lipusz
+ */
+@ProviderType
+public interface SummaryBuilder {
+
+	public Summary build();
+
+	public void setContent(String content);
+
+	public void setEscape(boolean escape);
+
+	public void setHighlight(boolean highlight);
+
+	public void setLocale(Locale locale);
+
+	public void setMaxContentLength(int maxContentLength);
+
+	public void setTitle(String title);
+
+}
