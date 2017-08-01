77aa5358381ab32542ced214e92f69f1a268ad9d
Matthew Tambara <matthew.tambara@liferay.com>
2017-08-01 16:31:27 -0700

    LPS-73760 Add test for inner jars following LPKG state and wars following war wrapper state

---
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test/src/test/java/com/liferay/portal/lpkg/deployer/controller/LPKGControllerTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.lpkg.deployer.controller;
+
+import com.liferay.portal.lpkg.deployer.util.LPKGTestUtil;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Matthew Tambara
+ */
+public class LPKGControllerTest {
+
+	@Test
+	public void testLPKGControllerDeploy() throws Exception {
+		String liferayHome = System.getProperty("liferay.home");
+
+		Assert.assertNotNull(
+			"Missing system property \"liferay.home\"", liferayHome);
+
+		Path path = Paths.get(liferayHome, "osgi/marketplace/Test.lpkg");
+
+		Files.createFile(path);
+
+		LPKGTestUtil.createLPKG(path, _SYMBOLIC_NAME, true);
+	}
+
+	private static final String _SYMBOLIC_NAME = "lpkg.controller.test";
+
+}
