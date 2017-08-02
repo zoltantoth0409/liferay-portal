ae40dd6220584c2e064a00dbaf295128a10815af
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-08-02 15:53:21 -0700

    COMMERCE-158 Regen services

--- modules/dxp/apps/commerce/commerce-currency-api/src/main/java/com/liferay/commerce/currency/service/CommerceCurrencyLocalService.java
+++ modules/dxp/apps/commerce/commerce-currency-api/src/main/java/com/liferay/commerce/currency/service/CommerceCurrencyLocalService.java
@@ -28,11 +28,13 @@ import com.liferay.portal.kernel.dao.orm.Projection;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.PersistedModel;
+import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.BaseLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.transaction.Isolation;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
@@ -99,6 +101,7 @@ public interface CommerceCurrencyLocalService extends BaseLocalService,
 	* @return the commerce currency that was removed
 	*/
 	@Indexable(type = IndexableType.DELETE)
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public CommerceCurrency deleteCommerceCurrency(
 		CommerceCurrency commerceCurrency);
 
