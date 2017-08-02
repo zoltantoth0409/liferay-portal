d932375d0c101767112e3a0af0a6b79b59eef430
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-08-02 15:50:38 -0700

    COMMERCE-158 Add System Event annotation

--- modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/service/impl/CommerceCurrencyLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/service/impl/CommerceCurrencyLocalServiceImpl.java
@@ -20,8 +20,10 @@ import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.currency.service.base.CommerceCurrencyLocalServiceBaseImpl;
 import com.liferay.commerce.currency.util.comparator.CommerceCurrencyPriorityComparator;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
@@ -78,6 +80,25 @@ public class CommerceCurrencyLocalServiceImpl
 	@Override
 	public void deleteCommerceCurrencies(long groupId) {
 		commerceCurrencyPersistence.removeByGroupId(groupId);
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CommerceCurrency deleteCommerceCurrency(
+		CommerceCurrency commerceCurrency) {
+
+		return commerceCurrencyPersistence.remove(commerceCurrency);
+	}
+
+	@Override
+	public CommerceCurrency deleteCommerceCurrency(long commerceCurrencyId)
+		throws PortalException {
+
+		CommerceCurrency commerceCurrency =
+			commerceCurrencyPersistence.findByPrimaryKey(commerceCurrencyId);
+
+		return commerceCurrencyLocalService.deleteCommerceCurrency(
+			commerceCurrency);
 	}
 
 	@Override
