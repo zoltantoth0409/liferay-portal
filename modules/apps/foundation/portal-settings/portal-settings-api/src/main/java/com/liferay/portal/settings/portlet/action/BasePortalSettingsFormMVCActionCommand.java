60902e63ae9834f5afffd0d62c49dbb1c0b51e5f
Michael Han <michael.han@liferay.com>
2017-08-02 19:15:43 -0500

    LPS-73996 deprecate

--- modules/apps/foundation/portal-settings/portal-settings-api/src/main/java/com/liferay/portal/settings/portlet/action/BasePortalSettingsFormMVCActionCommand.java
+++ modules/apps/foundation/portal-settings/portal-settings-api/src/main/java/com/liferay/portal/settings/portlet/action/BasePortalSettingsFormMVCActionCommand.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.settings.portlet.action;
 
+import aQute.bnd.annotation.ProviderType;
+
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseFormMVCActionCommand;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
@@ -39,7 +41,12 @@ import javax.portlet.ValidatorException;
 /**
  * @author Tomas Polesovsky
  * @author Philip Jones
+ * @deprecated As of 2.0.0, since 2.1.0 replaced by
+ *             {@link com.liferay.portal.settings.web.portlet.action.
+ *              PortalSettingsContributor}
  */
+@Deprecated
+@ProviderType
 public abstract class BasePortalSettingsFormMVCActionCommand
 	extends BaseFormMVCActionCommand {
 
