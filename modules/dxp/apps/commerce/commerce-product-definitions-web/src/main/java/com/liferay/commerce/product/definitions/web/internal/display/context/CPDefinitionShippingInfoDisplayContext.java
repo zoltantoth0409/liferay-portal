52d440d97571741e6bf8f47ef2edf75180e36e75
Alessio Rendina <asilcomando@gmail.com>
2017-08-04 16:21:57 +0200

    COMMERCE-24 Added new display context

---
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/display/context/CPDefinitionShippingInfoDisplayContext.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.display.context;
+
+import com.liferay.commerce.product.definitions.web.portlet.action.ActionHelper;
+import com.liferay.commerce.product.model.CPMeasurementUnit;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.commerce.product.service.CPMeasurementUnitService;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CPDefinitionShippingInfoDisplayContext
+	extends CPDefinitionsDisplayContext {
+
+	public CPDefinitionShippingInfoDisplayContext(
+			ActionHelper actionHelper, HttpServletRequest httpServletRequest,
+			CPDefinitionService cpDefinitionService, ItemSelector itemSelector,
+			CPMeasurementUnitService cpMeasurementUnitService)
+		throws PortalException {
+
+		super(
+			actionHelper, httpServletRequest, cpDefinitionService,
+			itemSelector);
+
+		_cpMeasurementUnitService = cpMeasurementUnitService;
+	}
+
+	public String getCPMeasurementUnitName(int type) {
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		CPMeasurementUnit cpMeasurementUnit =
+			_cpMeasurementUnitService.fetchPrimaryCPMeasurementUnit(
+				themeDisplay.getScopeGroupId(), type);
+
+		if (cpMeasurementUnit != null) {
+			return cpMeasurementUnit.getName(themeDisplay.getLanguageId());
+		}
+
+		return StringPool.BLANK;
+	}
+
+	private final CPMeasurementUnitService _cpMeasurementUnitService;
+
+}
