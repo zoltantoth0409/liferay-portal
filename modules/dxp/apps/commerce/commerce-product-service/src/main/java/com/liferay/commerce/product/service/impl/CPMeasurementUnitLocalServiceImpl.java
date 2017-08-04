8a667b6296d34d0202bced605c938dec52274376
Alessio Rendina <asilcomando@gmail.com>
2017-08-04 02:14:13 +0200

    COMMERCE-24 Added local and remote service implementations

---
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPMeasurementUnitLocalServiceImpl.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.service.impl;
+
+import com.liferay.commerce.product.exception.CPMeasurementUnitKeyException;
+import com.liferay.commerce.product.model.CPMeasurementUnit;
+import com.liferay.commerce.product.service.base.CPMeasurementUnitLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Marco Leo
+ * @author Alessio Antonio Rendina
+ */
+public class CPMeasurementUnitLocalServiceImpl
+	extends CPMeasurementUnitLocalServiceBaseImpl {
+
+	@Override
+	public CPMeasurementUnit addCPMeasurementUnit(
+			Map<Locale, String> nameMap, String key, double rate,
+			boolean primary, double priority, int type,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		if (primary) {
+			rate = 1;
+		}
+
+		validate(0, groupId, key, primary, type);
+
+		long cpMeasurementUnitId = counterLocalService.increment();
+
+		CPMeasurementUnit cpMeasurementUnit =
+			cpMeasurementUnitPersistence.create(cpMeasurementUnitId);
+
+		cpMeasurementUnit.setUuid(serviceContext.getUuid());
+		cpMeasurementUnit.setGroupId(groupId);
+		cpMeasurementUnit.setCompanyId(user.getCompanyId());
+		cpMeasurementUnit.setUserId(user.getUserId());
+		cpMeasurementUnit.setUserName(user.getFullName());
+		cpMeasurementUnit.setNameMap(nameMap);
+		cpMeasurementUnit.setKey(key);
+		cpMeasurementUnit.setRate(rate);
+		cpMeasurementUnit.setPrimary(primary);
+		cpMeasurementUnit.setPriority(priority);
+		cpMeasurementUnit.setType(type);
+
+		cpMeasurementUnitPersistence.update(cpMeasurementUnit);
+
+		return cpMeasurementUnit;
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CPMeasurementUnit deleteCPMeasurementUnit(
+		CPMeasurementUnit cpMeasurementUnit) {
+
+		return cpMeasurementUnitPersistence.remove(cpMeasurementUnit);
+	}
+
+	@Override
+	public CPMeasurementUnit deleteCPMeasurementUnit(long cpMeasurementUnitId)
+		throws PortalException {
+
+		CPMeasurementUnit cpMeasurementUnit =
+			cpMeasurementUnitPersistence.findByPrimaryKey(cpMeasurementUnitId);
+
+		return cpMeasurementUnitLocalService.deleteCPMeasurementUnit(
+			cpMeasurementUnit);
+	}
+
+	@Override
+	public List<CPMeasurementUnit> getCPMeasurementUnits(
+		long groupId, int type, int start, int end,
+		OrderByComparator<CPMeasurementUnit> orderByComparator) {
+
+		return cpMeasurementUnitPersistence.findByG_T(
+			groupId, type, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCPMeasurementUnitsCount(long groupId, int type) {
+		return cpMeasurementUnitPersistence.countByG_T(groupId, type);
+	}
+
+	@Override
+	public CPMeasurementUnit updateCPMeasurementUnit(
+			long cpMeasurementUnitId, Map<Locale, String> nameMap, String key,
+			double rate, boolean primary, double priority, int type)
+		throws PortalException {
+
+		CPMeasurementUnit cpMeasurementUnit =
+			cpMeasurementUnitPersistence.findByPrimaryKey(cpMeasurementUnitId);
+
+		if (primary) {
+			rate = 1;
+		}
+
+		validate(
+			cpMeasurementUnit.getCPMeasurementUnitId(),
+			cpMeasurementUnit.getGroupId(), key, primary, type);
+
+		cpMeasurementUnit.setNameMap(nameMap);
+		cpMeasurementUnit.setKey(key);
+		cpMeasurementUnit.setRate(rate);
+		cpMeasurementUnit.setPrimary(primary);
+		cpMeasurementUnit.setPriority(priority);
+		cpMeasurementUnit.setType(type);
+
+		cpMeasurementUnitPersistence.update(cpMeasurementUnit);
+
+		return cpMeasurementUnit;
+	}
+
+	protected void validate(
+			long cpMeasurementUnitId, long groupId, String key, boolean primary,
+			int type)
+		throws PortalException {
+
+		CPMeasurementUnit cpMeasurementUnit =
+			cpMeasurementUnitPersistence.fetchByG_K_T(groupId, key, type);
+
+		if ((cpMeasurementUnit != null) &&
+			(cpMeasurementUnit.getCPMeasurementUnitId() !=
+				cpMeasurementUnitId)) {
+
+			throw new CPMeasurementUnitKeyException();
+		}
+
+		if (primary) {
+			List<CPMeasurementUnit> cpMeasurementUnits =
+				cpMeasurementUnitPersistence.findByG_P_T(
+					groupId, primary, type);
+
+			for (CPMeasurementUnit curCPMeasurementUnit : cpMeasurementUnits) {
+				if (curCPMeasurementUnit.getCPMeasurementUnitId() !=
+						cpMeasurementUnitId) {
+
+					curCPMeasurementUnit.setPrimary(false);
+
+					cpMeasurementUnitPersistence.update(curCPMeasurementUnit);
+				}
+			}
+		}
+	}
+}
---
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPMeasurementUnitServiceImpl.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.service.impl;
+
+import com.liferay.commerce.product.constants.CPActionKeys;
+import com.liferay.commerce.product.model.CPMeasurementUnit;
+import com.liferay.commerce.product.service.base.CPMeasurementUnitServiceBaseImpl;
+import com.liferay.commerce.product.service.permission.CPMeasurementUnitPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Marco Leo
+ * @author Alessio Antonio Rendina
+ */
+public class CPMeasurementUnitServiceImpl
+	extends CPMeasurementUnitServiceBaseImpl {
+
+	@Override
+	public CPMeasurementUnit addCPMeasurementUnit(
+			Map<Locale, String> nameMap, String key, double rate,
+			boolean primary, double priority, int type,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		CPMeasurementUnitPermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_MEASUREMENT_UNIT);
+
+		return cpMeasurementUnitLocalService.addCPMeasurementUnit(
+			nameMap, key, rate, primary, priority, type, serviceContext);
+	}
+
+	@Override
+	public void deleteCPMeasurementUnit(long cpMeasurementUnitId)
+		throws PortalException {
+
+		CPMeasurementUnit cpMeasurementUnit =
+			cpMeasurementUnitPersistence.findByPrimaryKey(cpMeasurementUnitId);
+
+		CPMeasurementUnitPermission.check(
+			getPermissionChecker(), cpMeasurementUnit.getGroupId(),
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_MEASUREMENT_UNIT);
+
+		cpMeasurementUnitLocalService.deleteCPMeasurementUnit(
+			cpMeasurementUnit);
+	}
+
+	@Override
+	public List<CPMeasurementUnit> getCPMeasurementUnits(
+		long groupId, int type, int start, int end,
+		OrderByComparator<CPMeasurementUnit> orderByComparator) {
+
+		return cpMeasurementUnitLocalService.getCPMeasurementUnits(
+			groupId, type, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCPMeasurementUnitsCount(long groupId, int type) {
+		return cpMeasurementUnitLocalService.getCPMeasurementUnitsCount(
+			groupId, type);
+	}
+
+	@Override
+	public CPMeasurementUnit updateCPMeasurementUnit(
+			long cpMeasurementUnitId, Map<Locale, String> nameMap, String key,
+			double rate, boolean primary, double priority, int type)
+		throws PortalException {
+
+		CPMeasurementUnit cpMeasurementUnit =
+			cpMeasurementUnitPersistence.findByPrimaryKey(cpMeasurementUnitId);
+
+		CPMeasurementUnitPermission.check(
+			getPermissionChecker(), cpMeasurementUnit.getGroupId(),
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_MEASUREMENT_UNIT);
+
+		return cpMeasurementUnitLocalService.updateCPMeasurementUnit(
+			cpMeasurementUnitId, nameMap, key, rate, primary, priority, type);
+	}
+}
