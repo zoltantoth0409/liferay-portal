a992030a095af79fad2a34dda69ff9bd92f2a75a
Bruno Basto <bruno.basto@liferay.com>
2017-08-04 16:19:56 -0300

    LPS-73994 Fix tests

--- modules/apps/foundation/portal-portlet-bridge/portal-portlet-bridge-soy/src/test/java/com/liferay/portal/portlet/bridge/soy/internal/SoyPortletHelperTest.java
+++ modules/apps/foundation/portal-portlet-bridge/portal-portlet-bridge-soy/src/test/java/com/liferay/portal/portlet/bridge/soy/internal/SoyPortletHelperTest.java
@@ -16,180 +16,197 @@ package com.liferay.portal.portlet.bridge.soy.internal;
 
 import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.portlet.FriendlyURLMapper;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCCommandCache;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.util.HtmlUtil;
-import com.liferay.portal.kernel.util.ProxyUtil;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.util.HtmlImpl;
 
-import java.io.Writer;
-
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
+import java.net.URL;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Marcellus Tavares
  */
+@PrepareForTest(FrameworkUtil.class)
+@RunWith(PowerMockRunner.class)
 public class SoyPortletHelperTest {
 
 	@Before
-	public void setUp() {
-		setUpHtmlUtil();
+	public void setUpFrameworkUtil() {
 		setUpJSONFactoryUtil();
+
+		PowerMockito.spy(FrameworkUtil.class);
 	}
 
-	@Test
-	public void testgetPortletJavaScriptWithBundleWithoutPackageFile()
-		throws Exception {
-
-		String portletJavaScript = getPortletJavaScript(_mockBundle, "View");
-
-		Assert.assertEquals(StringPool.BLANK, portletJavaScript);
-	}
-
-	@Test
-	public void testgetPortletJavaScriptWithBundleWithPackageFile()
-		throws Exception {
-
-		Bundle bundle = getBundleWithPackageFile();
-
-		String portletJavaScript = getPortletJavaScript(bundle, "View");
-
-		Assert.assertNotEquals(StringPool.BLANK, portletJavaScript);
-	}
-
-	@Test
-	public void testgetRequiredModulesWithBundleWithPackageFile()
-		throws Exception {
-
-		Bundle bundle = getBundleWithPackageFile();
-
-		SoyPortletHelper soyPortletHelper = new SoyPortletHelper(bundle);
-
-		Set<String> expectedRequiredModules = new HashSet<>();
-
-		expectedRequiredModules.add("SampleModuleName/View.soy");
-
-		Set<String> actualRequiredModules = soyPortletHelper.getRequiredModules(
-			"View", Collections.<String>emptySet());
-
-		Assert.assertEquals(expectedRequiredModules, actualRequiredModules);
-	}
-
-	@Test
-	public void testTemplateNamespace() throws Exception {
-		String path = "View";
-
-		SoyPortletHelper soyPortletHelper = new SoyPortletHelper(_mockBundle);
-
-		Assert.assertEquals(
-			path.concat(".render"),
-			soyPortletHelper.getTemplateNamespace(path));
-	}
-
-	protected Bundle getBundleWithPackageFile() {
-		Bundle bundle = (Bundle)ProxyUtil.newProxyInstance(
-			Bundle.class.getClassLoader(), new Class<?>[] {Bundle.class},
-			new InvocationHandler() {
-
-				@Override
-				public Object invoke(Object proxy, Method method, Object[] args)
-					throws NoSuchMethodException {
-
-					if (method.equals(
-							Bundle.class.getMethod("getEntry", String.class)) &&
-						"package.json".equals(args[0])) {
-
-						return SoyPortletHelperTest.class.getResource(
-							"dependencies/package.json");
-					}
-
-					return null;
-				}
-
-			});
-
-		return bundle;
-	}
-
-	protected String getPortletJavaScript(Bundle bundle, String path)
-		throws Exception {
-
-		SoyPortletHelper soyPortletHelper = new SoyPortletHelper(bundle);
-
-		Template template = new MockTemplate();
-
-		String portletJavaScript = soyPortletHelper.getPortletJavaScript(
-			template, path, StringUtil.randomString(),
-			Collections.<String>emptySet());
-
-		return portletJavaScript;
-	}
-
-	protected void setUpHtmlUtil() {
+	@Before
+	public void setUpHtmlUtil() {
 		HtmlUtil htmlUtil = new HtmlUtil();
 
 		htmlUtil.setHtml(new HtmlImpl());
 	}
 
-	protected void setUpJSONFactoryUtil() {
+	@Before
+	public void setUpJSONFactoryUtil() {
 		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
 
 		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
 	}
 
-	private final Bundle _mockBundle = (Bundle)ProxyUtil.newProxyInstance(
-		Bundle.class.getClassLoader(), new Class<?>[] {Bundle.class},
-		new InvocationHandler() {
+	@Test(expected = Exception.class)
+	public void testGetJavaScriptControllerModuleWithoutPackageJSON()
+		throws Exception {
 
-			@Override
-			public Object invoke(Object proxy, Method method, Object[] args) {
-				return null;
+		Bundle bundle = _mockBundleWithoutPackage();
+
+		MVCCommandCache mvcCommandCache = _mockEmptyMVCCommandCache();
+
+		FriendlyURLMapper friendlyURLMapper = Mockito.mock(
+			FriendlyURLMapper.class);
+
+		SoyPortletHelper soyPortletHelper = new SoyPortletHelper(
+			bundle, mvcCommandCache, friendlyURLMapper);
+
+		soyPortletHelper.getJavaScriptLoaderModule("Path");
+	}
+
+	@Test
+	public void testgetJavaScriptLoaderModule() throws Exception {
+		Bundle bundle = _mockBundleWithPackageFile();
+
+		MVCCommandCache mvcCommandCache = _mockEmptyMVCCommandCache();
+
+		FriendlyURLMapper friendlyURLMapper = Mockito.mock(
+			FriendlyURLMapper.class);
+
+		SoyPortletHelper soyPortletHelper = new SoyPortletHelper(
+			bundle, mvcCommandCache, friendlyURLMapper);
+
+		String javaScriptLoaderModule =
+			soyPortletHelper.getJavaScriptLoaderModule("JavaScriptCommand");
+
+		Assert.assertEquals(
+			"package-with-version@1.0.0/JavaScriptCommand",
+			javaScriptLoaderModule);
+	}
+
+	@Test
+	public void testgetJavaScriptLoaderModuleForES6() throws Exception {
+		Bundle bundle = _mockBundleWithPackageFile();
+
+		MVCCommandCache mvcCommandCache = _mockEmptyMVCCommandCache();
+
+		FriendlyURLMapper friendlyURLMapper = Mockito.mock(
+			FriendlyURLMapper.class);
+
+		SoyPortletHelper soyPortletHelper = new SoyPortletHelper(
+			bundle, mvcCommandCache, friendlyURLMapper);
+
+		String javaScriptLoaderModule =
+			soyPortletHelper.getJavaScriptLoaderModule("ES6Command");
+
+		Assert.assertEquals(
+			"package-with-version@1.0.0/ES6Command.es", javaScriptLoaderModule);
+	}
+
+	@Test
+	public void testgetJavaScriptLoaderModuleForSoy() throws Exception {
+		Bundle bundle = _mockBundleWithPackageFile();
+
+		MVCCommandCache mvcCommandCache = _mockEmptyMVCCommandCache();
+
+		FriendlyURLMapper friendlyURLMapper = Mockito.mock(
+			FriendlyURLMapper.class);
+
+		SoyPortletHelper soyPortletHelper = new SoyPortletHelper(
+			bundle, mvcCommandCache, friendlyURLMapper);
+
+		String javaScriptLoaderModule =
+			soyPortletHelper.getJavaScriptLoaderModule("SoyCommand");
+
+		Assert.assertEquals(
+			"package-with-version@1.0.0/SoyCommand.soy",
+			javaScriptLoaderModule);
+	}
+
+	private Bundle _mockBundleWithoutPackage() {
+		Bundle bundle = Mockito.mock(Bundle.class);
+
+		Mockito.when(
+			bundle.getEntry("package.json")
+		).then(
+			new Answer<URL>() {
+
+				public URL answer(InvocationOnMock invocationOnMock) {
+					return null;
+				}
+
 			}
+		);
 
-		});
+		return bundle;
+	}
 
-	private static class MockTemplate
-		extends HashMap<String, Object> implements Template {
+	private Bundle _mockBundleWithPackageFile() {
+		Bundle bundle = Mockito.mock(Bundle.class);
 
-		@Override
-		public void doProcessTemplate(Writer writer) {
-		}
+		Mockito.when(
+			bundle.getEntry(Matchers.endsWith("ES6Command.es.js"))
+		).thenReturn(
+			SoyPortletHelperTest.class.getResource(
+				"dependencies/ES6Command.es.js")
+		);
 
-		@Override
-		public Object get(String key) {
-			return super.get(key);
-		}
+		Mockito.when(
+			bundle.getEntry(Matchers.endsWith("JavaScriptCommand.js"))
+		).thenReturn(
+			SoyPortletHelperTest.class.getResource(
+				"dependencies/JavaScriptCommand.js")
+		);
 
-		@Override
-		public String[] getKeys() {
-			Set<String> keys = keySet();
+		Mockito.when(
+			bundle.getEntry(Matchers.endsWith("package.json"))
+		).thenReturn(
+			SoyPortletHelperTest.class.getResource(
+				"dependencies/package-with-version.json")
+		);
 
-			return keys.toArray(new String[keys.size()]);
-		}
+		Mockito.when(
+			bundle.getEntry(Matchers.endsWith("SoyCommand.soy"))
+		).thenReturn(
+			SoyPortletHelperTest.class.getResource(
+				"dependencies/SoyCommand.soy")
+		);
 
-		@Override
-		public void prepare(HttpServletRequest request) {
-		}
+		return bundle;
+	}
 
-		@Override
-		public void processTemplate(Writer writer) {
-		}
+	private MVCCommandCache _mockEmptyMVCCommandCache() {
+		MVCCommandCache mvcCommandCache = Mockito.mock(MVCCommandCache.class);
 
+		Mockito.when(
+			mvcCommandCache.getMVCCommand(Matchers.anyString())
+		).thenReturn(
+			MVCRenderCommand.EMPTY
+		);
+
+		return mvcCommandCache;
 	}
 
 }
