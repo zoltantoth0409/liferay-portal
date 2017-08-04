f252872749c437687b1724090c0455d9489beb14
Hugo Huijser <hugo.huijser@liferay.com>
2017-08-04 14:18:32 +0800

    LPS-74034 Add formatting whitespace in CODEOWNERS, 4 spaces between pattern and first user, single space between users

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/CodeownersWhitespaceCheck.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+
+/**
+ * @author Hugo Huijser
+ */
+public class CodeownersWhitespaceCheck extends WhitespaceCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		content = super.doProcess(fileName, absolutePath, content);
+
+		StringBundler sb = new StringBundler();
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				if (line.startsWith(StringPool.POUND)) {
+					sb.append(line);
+				}
+				else {
+					String[] words = line.split("\\s+");
+
+					if (words.length == 1) {
+						sb.append(line);
+					}
+					else {
+						sb.append(words[0]);
+						sb.append(StringPool.FOUR_SPACES);
+
+						for (int i = 1; i < words.length - 1; i++) {
+							sb.append(words[i]);
+							sb.append(StringPool.SPACE);
+						}
+
+						sb.append(words[words.length - 1]);
+					}
+				}
+
+				sb.append("\n");
+			}
+		}
+
+		content = sb.toString();
+
+		if (content.endsWith("\n")) {
+			content = content.substring(0, content.length() - 1);
+		}
+
+		return content;
+	}
+
+}
