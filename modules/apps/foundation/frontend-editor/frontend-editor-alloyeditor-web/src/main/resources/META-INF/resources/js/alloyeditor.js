67a32c63299329f71861909a1e00aee7c26966b6
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2017-08-04 13:08:35 +0200

    LPS-72963 Fix ckeditor bug with the resize plugin in firefox.
    More info: https://dev.ckeditor.com/ticket/10197

--- modules/apps/foundation/frontend-editor/frontend-editor-alloyeditor-web/src/main/resources/META-INF/resources/js/alloyeditor.js
+++ modules/apps/foundation/frontend-editor/frontend-editor-alloyeditor-web/src/main/resources/META-INF/resources/js/alloyeditor.js
@@ -288,6 +288,21 @@ AUI.add(
 						// LPS-73775
 
 						instance.getNativeEditor().editable().$.addEventListener('compositionend', A.bind('_onChange', instance));
+
+						//LPS-72963
+
+						var editorConfig = instance.getNativeEditor().config;
+
+						var removeResizePlugin = editorConfig.removePlugins && editorConfig.removePlugins.indexOf('ae_dragresize') != -1;
+
+						if (CKEDITOR.env.gecko && removeResizePlugin) {
+							var doc = instance.getNativeEditor().document.$;
+
+							doc.designMode = 'on';
+							doc.execCommand('enableObjectResizing', false, false);
+							doc.execCommand('enableInlineTableEditing', false, false);
+							doc.designMode = 'off';
+						}
 					},
 
 					_onKey: function(event) {
