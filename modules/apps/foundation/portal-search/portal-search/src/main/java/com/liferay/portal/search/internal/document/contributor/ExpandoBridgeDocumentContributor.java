8ac5357903282a6f5c889aa7a657f2c9a31cbee8
Brian Chan <brian.chan@liferay.com>
2017-08-07 13:07:09 -0700

    LPS-74032 Rename ExpandoDocumentContributor to ExpandoBridgeDocumentContributor

--- modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/document/contributor/ExpandoBridgeDocumentContributor.java
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/document/contributor/ExpandoBridgeDocumentContributor.java
@@ -26,7 +26,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Michael C. Han
  */
 @Component(immediate = true, service = DocumentContributor.class)
-public class ExpandoDocumentContributor implements DocumentContributor {
+public class ExpandoBridgeDocumentContributor implements DocumentContributor {
 
 	@Override
 	public void contribute(Document document, BaseModel baseModel) {
