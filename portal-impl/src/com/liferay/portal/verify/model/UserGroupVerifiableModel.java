f5a3b11a97d4db1a510f5b010b68946bc6e997ae
mbowerman <michael.bowerman@liferay.com>
2017-08-07 16:01:47 -0700

    LPS-74078 Make UserGroupVerifiableModel also implement VerifiableResourcedModel

--- portal-impl/src/com/liferay/portal/verify/model/UserGroupVerifiableModel.java
+++ portal-impl/src/com/liferay/portal/verify/model/UserGroupVerifiableModel.java
@@ -14,18 +14,26 @@
 
 package com.liferay.portal.verify.model;
 
+import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.verify.model.VerifiableAuditedModel;
+import com.liferay.portal.kernel.verify.model.VerifiableResourcedModel;
 import com.liferay.portal.kernel.verify.model.VerifiableUUIDModel;
 
 /**
  * @author Miguel Pastor
  */
 public class UserGroupVerifiableModel
-	implements VerifiableAuditedModel, VerifiableUUIDModel {
+	implements VerifiableAuditedModel, VerifiableResourcedModel,
+			   VerifiableUUIDModel {
 
 	@Override
 	public String getJoinByTableName() {
 		return null;
+	}
+
+	@Override
+	public String getModelName() {
+		return UserGroup.class.getName();
 	}
 
 	@Override
@@ -46,6 +54,11 @@ public class UserGroupVerifiableModel
 	@Override
 	public String getTableName() {
 		return "UserGroup";
+	}
+
+	@Override
+	public String getUserIdColumnName() {
+		return "userId";
 	}
 
 	@Override
