3cb313eb774aa33edd4a748f8ec490eb6903f629
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-08-08 14:03:45 +0200

    LPS-73992 SF - Same as in doExport

--- modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/internal/exportimport/data/handler/WikiPageStagedModelDataHandler.java
+++ modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/internal/exportimport/data/handler/WikiPageStagedModelDataHandler.java
@@ -273,7 +273,12 @@ public class WikiPageStagedModelDataHandler
 		}
 
 		if (existingPage != null) {
-			_deleteExistingAttachments(existingPage);
+			for (FileEntry fileEntry :
+					existingPage.getAttachmentsFileEntries()) {
+
+				PortletFileRepositoryUtil.deletePortletFileEntry(
+					fileEntry.getFileEntryId());
+			}
 		}
 
 		if (page.isHead()) {
@@ -389,17 +394,6 @@ public class WikiPageStagedModelDataHandler
 		WikiPageResourceLocalService wikiPageResourceLocalService) {
 
 		_wikiPageResourceLocalService = wikiPageResourceLocalService;
-	}
-
-	private void _deleteExistingAttachments(WikiPage page)
-		throws PortalException {
-
-		List<FileEntry> existingAttachments = page.getAttachmentsFileEntries();
-
-		for (FileEntry existingAttachment : existingAttachments) {
-			PortletFileRepositoryUtil.deletePortletFileEntry(
-				existingAttachment.getFileEntryId());
-		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
