cf74295db6da1a9f2942704a8078ee974f187367
Michael Han <michael.han@liferay.com>
2017-08-08 08:21:28 -0500

    LPS-73996 add interface for composition instead of inheritance

---
+++ modules/apps/foundation/portal-settings/portal-settings-web/src/main/java/com/liferay/portal/settings/web/portlet/action/PortalSettingsParameterUtil.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.settings.web.portlet.action;
+
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+
+/**
+ * @author Michael C. Han
+ */
+public class PortalSettingsParameterUtil {
+
+	public static boolean getBoolean(
+		ActionRequest actionRequest,
+		PortalSettingsFormContributor portalSettingsFormContributor,
+		String name) {
+
+		return ParamUtil.getBoolean(
+			actionRequest,
+			portalSettingsFormContributor.getParameterNamespace() + name);
+	}
+
+	public static String getString(
+		ActionRequest actionRequest,
+		PortalSettingsFormContributor portalSettingsFormContributor,
+		String name) {
+
+		return ParamUtil.getString(
+			actionRequest,
+			portalSettingsFormContributor.getParameterNamespace() + name);
+	}
+
+}
