2a32bcdf6d965ec036b783f7c30ec4655f4d60ee
Preston-Crary <preston.crary@liferay.com>
2017-08-08 14:32:39 -0700

    LPS-73967 Refactor to also test Wabs.

---
+++ modules/apps/foundation/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/ModuleBuildAutoUpgradeTest.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.test;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.sql.Types;
+
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+import java.util.zip.ZipEntry;
+
+import org.junit.Before;
+
+import org.osgi.framework.Constants;
+
+/**
+ * @author Preston Crary
+ */
+public class ModuleBuildAutoUpgradeTest extends BaseBuildAutoUpgradeTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_jarBytes[0] = _createBundleBytes(
+			new Object[][] {{"id_", Types.BIGINT}, {"data_", Types.VARCHAR}},
+			1);
+
+		_jarBytes[1] = _createBundleBytes(
+			new Object[][] {
+				{"id_", Types.BIGINT}, {"data_", Types.VARCHAR},
+				{"data2", Types.VARCHAR}
+			},
+			2);
+
+		_jarBytes[2] = _createBundleBytes(
+			new Object[][] {{"id_", Types.BIGINT}, {"data2", Types.VARCHAR}},
+			3);
+
+		_jarBytes[3] = _createBundleBytes(
+			new Object[][] {{"id_", Types.BIGINT}, {"data_", Types.VARCHAR}},
+			4);
+	}
+
+	@Override
+	protected InputStream getBundleInputStream(int version) {
+		return new UnsyncByteArrayInputStream(_jarBytes[version - 1]);
+	}
+
+	private byte[] _createBundleBytes(Object[][] tableColumns, int version)
+		throws IOException {
+
+		try (UnsyncByteArrayOutputStream unsyncbyteArrayOutputStream =
+				new UnsyncByteArrayOutputStream();
+			JarOutputStream jarOutputStream =
+				new JarOutputStream(unsyncbyteArrayOutputStream)) {
+
+			Manifest manifest = new Manifest();
+
+			Attributes attributes = manifest.getMainAttributes();
+
+			attributes.putValue("Manifest-Version", "1.0");
+			attributes.putValue(Constants.BUNDLE_MANIFESTVERSION, "2");
+			attributes.putValue(
+				Constants.BUNDLE_NAME, "Build Auto Upgrade Test");
+			attributes.putValue(
+				Constants.BUNDLE_SYMBOLICNAME, BUNDLE_SYMBOLICNAME);
+			attributes.putValue(Constants.BUNDLE_VERSION, "1.0.0");
+			attributes.putValue("Liferay-Require-SchemaVersion", "1.0.0");
+			attributes.putValue("Liferay-Service", Boolean.TRUE.toString());
+			attributes.putValue("Liferay-Spring-Context", "META-INF/spring");
+
+			jarOutputStream.putNextEntry(new ZipEntry(JarFile.MANIFEST_NAME));
+
+			manifest.write(jarOutputStream);
+
+			jarOutputStream.closeEntry();
+
+			String createSQL = toCreateSQL(tableColumns);
+
+			addClass(ENTITY_PATH, jarOutputStream, tableColumns, createSQL);
+
+			addResource("META-INF/portlet-model-hints.xml", jarOutputStream);
+			addResource("META-INF/spring/module-spring.xml", jarOutputStream);
+
+			addEmptyResource("META-INF/sql/indexes.sql", jarOutputStream);
+			addEmptyResource("META-INF/sql/sequences.sql", jarOutputStream);
+
+			addResource(
+				"META-INF/sql/tables.sql", createSQL.getBytes(StringPool.UTF8),
+				jarOutputStream);
+
+			addServiceProperties(
+				version, "service.properties", jarOutputStream);
+
+			jarOutputStream.finish();
+
+			return unsyncbyteArrayOutputStream.toByteArray();
+		}
+	}
+
+	private final byte[][] _jarBytes = new byte[4][];
+
+}
---
+++ modules/apps/foundation/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/WabBuildAutoUpgradeTest.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.test;
+
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.URI;
+import java.net.URL;
+
+import java.sql.Types;
+
+import java.util.jar.JarOutputStream;
+
+import org.junit.After;
+import org.junit.Before;
+
+import org.osgi.framework.Constants;
+
+/**
+ * @author Preston Crary
+ */
+public class WabBuildAutoUpgradeTest extends BaseBuildAutoUpgradeTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_warFiles[0] = _createWabBundleFile(
+			new Object[][] {{"id_", Types.BIGINT}, {"data_", Types.VARCHAR}},
+			1);
+
+		_warFiles[1] = _createWabBundleFile(
+			new Object[][] {
+				{"id_", Types.BIGINT}, {"data_", Types.VARCHAR},
+				{"data2", Types.VARCHAR}
+			},
+			2);
+
+		_warFiles[2] = _createWabBundleFile(
+			new Object[][] {{"id_", Types.BIGINT}, {"data2", Types.VARCHAR}},
+			3);
+
+		_warFiles[3] = _createWabBundleFile(
+			new Object[][] {{"id_", Types.BIGINT}, {"data_", Types.VARCHAR}},
+			4);
+
+		for (int i = 0; i < _warURLs.length; i++) {
+			URI uri = _warFiles[i].toURI();
+
+			URL url = uri.toURL();
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append(url.getPath());
+			sb.append("?");
+			sb.append(Constants.BUNDLE_SYMBOLICNAME);
+			sb.append("=BuildAutoUpgradeTest&Web-ContextPath=/");
+			sb.append("BuildAutoUpgradeTest&protocol=file");
+
+			_warURLs[i] = new URL("webbundle", null, sb.toString());
+		}
+	}
+
+	@After
+	public void tearDown() throws Throwable {
+		super.tearDown();
+
+		for (File file : _warFiles) {
+			if (file != null) {
+				file.delete();
+			}
+		}
+	}
+
+	@Override
+	protected InputStream getBundleInputStream(int version) throws IOException {
+		return _warURLs[version - 1].openStream();
+	}
+
+	private File _createWabBundleFile(Object[][] tableColumns, int version)
+		throws IOException {
+
+		File file = FileUtil.createTempFile("war");
+
+		try (FileOutputStream fileOutputStream = new FileOutputStream(file);
+			JarOutputStream jarOutputStream =
+				new JarOutputStream(fileOutputStream)) {
+
+			String createSQL = toCreateSQL(tableColumns);
+
+			addClass(
+				"WEB-INF/classes/" + ENTITY_PATH, jarOutputStream, tableColumns,
+				createSQL);
+
+			addResource(
+				"dependencies/service/META-INF/portlet-model-hints.xml",
+				"WEB-INF/classes/META-INF/portlet-model-hints.xml",
+				jarOutputStream);
+			addResource(
+				"dependencies/service/META-INF/spring/module-spring.xml",
+				"WEB-INF/classes/META-INF/portlet-spring.xml", jarOutputStream);
+
+			addEmptyResource("WEB-INF/sql/indexes.sql", jarOutputStream);
+			addEmptyResource("WEB-INF/sql/sequences.sql", jarOutputStream);
+
+			addResource(
+				"WEB-INF/sql/tables.sql", createSQL.getBytes(StringPool.UTF8),
+				jarOutputStream);
+
+			addServiceProperties(
+				version, "WEB-INF/classes/service.properties", jarOutputStream);
+
+			jarOutputStream.finish();
+
+			return file;
+		}
+	}
+
+	private final File[] _warFiles = new File[4];
+	private final URL[] _warURLs = new URL[4];
+
+}
