fd5df2f1f68e4f1238f7687ce07da3a1930d4037
André de Oliveira <andre.oliveira@liferay.com>
2017-08-07 18:59:43 -0700

    LPS-72670 portal-search-web: Update tests to use SummaryBuilder
    https://issues.liferay.com/browse/LPS-72670

--- modules/apps/foundation/portal-search/portal-search-web/src/test/java/com/liferay/portal/search/web/internal/display/context/SearchDisplayContextTest.java
+++ modules/apps/foundation/portal-search/portal-search-web/src/test/java/com/liferay/portal/search/web/internal/display/context/SearchDisplayContextTest.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.search.summary.SummaryBuilderFactory;
 import com.liferay.portal.search.web.constants.SearchPortletParameterNames;
 import com.liferay.portlet.portletconfiguration.util.ConfigurationRenderRequest;
 
@@ -230,7 +231,8 @@ public class SearchDisplayContextTest {
 			renderRequest, portletPreferences,
 			createPortal(themeDisplay, renderRequest), Mockito.mock(Html.class),
 			Mockito.mock(Language.class), facetedSearcherManager,
-			Mockito.mock(IndexSearchPropsValues.class), portletURLFactory);
+			Mockito.mock(IndexSearchPropsValues.class), portletURLFactory,
+			Mockito.mock(SummaryBuilderFactory.class));
 	}
 
 	protected ThemeDisplay createThemeDisplay() throws Exception {
