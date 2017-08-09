2b257150bf5dbf9ca2654ae16c35e212259109ef
IstvanD <istvan.dezsi@liferay.com>
2017-08-09 13:33:27 +0200

    LPS-73946 Adding resource action to fetch recipient data

---
+++ modules/apps/collaboration/microblogs/microblogs-web/src/main/java/com/liferay/microblogs/web/internal/portlet/action/GetJSONRecipientsMVCResourceCommand.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.microblogs.web.internal.portlet.action;
+
+import com.liferay.microblogs.constants.MicroblogsPortletKeys;
+import com.liferay.microblogs.util.MicroblogsUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.servlet.ServletResponseUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author István András Dézsi
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + MicroblogsPortletKeys.MICROBLOGS,
+		"mvc.command.name=/microblogs/get_json_recipients"
+	},
+	service = MVCResourceCommand.class
+)
+public class GetJSONRecipientsMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	public void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws PortletException {
+
+		try {
+			HttpServletRequest request = _portal.getOriginalServletRequest(
+				_portal.getHttpServletRequest(resourceRequest));
+
+			long userId = ParamUtil.getLong(request, "userId");
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)resourceRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			JSONArray jsonArray = MicroblogsUtil.getJSONRecipients(
+				userId, themeDisplay);
+
+			HttpServletResponse response = _portal.getHttpServletResponse(
+				resourceResponse);
+
+			response.setContentType(ContentTypes.APPLICATION_JSON);
+
+			ServletResponseUtil.write(response, jsonArray.toString());
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		GetJSONRecipientsMVCResourceCommand.class);
+
+	@Reference
+	private Portal _portal;
+
+}
