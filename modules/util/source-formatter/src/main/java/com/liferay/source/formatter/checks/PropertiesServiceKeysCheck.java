9238025c6791cb39756c8c1e180293e6ed750472
Peter Shin <peter.shin@liferay.com>
2017-08-09 15:54:47 -0700

    LPS-73967 Automatically remove build.auto.upgrade from service.properties

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesServiceKeysCheck.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Peter Shin
+ */
+public class PropertiesServiceKeysCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (!fileName.endsWith("/service.properties")) {
+			return content;
+		}
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				String[] array = line.split("=", 2);
+
+				if (array.length < 2) {
+					continue;
+				}
+
+				String key = array[0].trim();
+
+				if (ArrayUtil.contains(_LEGACY_SERVICE_KEYS, key)) {
+					content = StringUtil.replaceFirst(
+						content, line, StringPool.BLANK);
+				}
+			}
+		}
+
+		return content;
+	}
+
+	private static final String[] _LEGACY_SERVICE_KEYS = {"build.auto.upgrade"};
+
+}
