4ce85ca4b552ba3307360dd8135f8cf6fbbb6dbc
Minhchau Dang <minhchau.dang@liferay.com>
2017-08-10 17:34:21 -0700

    LPS-73049 Move logic from DynamicCSSFilter to a shared utility class

---
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/ResourceUtil.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.servlet;
+
+import com.liferay.portal.kernel.util.ObjectValuePair;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * @author Minhchau Dang
+ */
+public class ResourceUtil {
+
+	public static ObjectValuePair<ServletContext, URL> getObjectValuePair(
+			String originalRequestPath, String requestPath,
+			ServletContext _servletContext)
+		throws IOException {
+
+		ObjectValuePair<ServletContext, URL> ovp = null;
+
+		if (ovp == null) {
+			ServletContext servletContext = _servletContext;
+
+			URL resourceURL = servletContext.getResource(requestPath);
+
+			if (resourceURL != null) {
+				ovp = new ObjectValuePair<>(servletContext, resourceURL);
+			}
+		}
+
+		if (ovp == null) {
+			ServletContext servletContext =
+				PortalWebResourcesUtil.getPathServletContext(
+					originalRequestPath);
+
+			URL resourceURL = PortalWebResourcesUtil.getResource(
+				servletContext, originalRequestPath);
+
+			if (resourceURL != null) {
+				ovp = new ObjectValuePair<>(servletContext, resourceURL);
+			}
+		}
+
+		if (ovp == null) {
+			ServletContext servletContext =
+				PortletResourcesUtil.getPathServletContext(
+					originalRequestPath);
+
+			URL resourceURL = PortletResourcesUtil.getResource(
+				servletContext, originalRequestPath);
+
+			if (resourceURL != null) {
+				ovp = new ObjectValuePair<>(servletContext, resourceURL);
+			}
+		}
+
+		if (ovp == null) {
+			ServletContext servletContext =
+				DynamicResourceIncludeUtil.getPathServletContext(
+					originalRequestPath);
+
+			URL resourceURL = DynamicResourceIncludeUtil.getResource(
+				servletContext, originalRequestPath);
+
+			if (resourceURL != null) {
+				ovp = new ObjectValuePair<>(servletContext, resourceURL);
+			}
+		}
+
+		return ovp;
+	}
+
+}
