5eb7a3b72b693b116928239ce9d39fce917baab3
Peter Shin <peter.shin@liferay.com>
2017-08-11 14:09:42 -0700

    LPS-74104 Automatically update links

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesSourceFormatterContentCheck.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Peter Shin
+ */
+public class PropertiesSourceFormatterContentCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (fileName.endsWith("/source-formatter.properties")) {
+			content = _checkSourceFormatterContent(content);
+		}
+
+		return content;
+	}
+
+	private String _checkSourceFormatterContent(String content)
+		throws Exception {
+
+		for (String[] array : _CONVERTED_KEYS) {
+			content = StringUtil.replace(content, array[0], array[1]);
+		}
+
+		return content;
+	}
+
+	private static final String[][] _CONVERTED_KEYS = {
+		new String[] {
+			"blob/master/portal-impl/src/source-formatter.properties",
+			"blob/master/source-formatter.properties"
+		}
+	};
+
+}
