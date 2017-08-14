0ae5e846e0220218f229ee820fe782cb739b4a37
daviddotzhang <david.zhang@liferay.com>
2017-08-14 13:41:23 +0800

    LPS-73870 Tool Tip Message needs to be updated when user Like or Unlike particular content

--- modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/ratings.js
+++ modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/ratings.js
@@ -519,6 +519,7 @@ AUI.add(
 							ratingThumbUp.attr('title', thumbUpMessage);
 							ratingThumbDown.attr('title', thumbDownMessage);
 
+							ratingThumbUp.html(thumbScore.positiveVotes);
 							ratingThumbDown.html(thumbScore.negativeVotes);
 						}
 						else {
