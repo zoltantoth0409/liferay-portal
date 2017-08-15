brandonyee333/test

LPS-72672 Tag Facet Portlet
https://issues.liferay.com/browse/LPS-72672

modules/apps/foundation/portal-search/portal-search-web/src/main/resources/META-INF/resources/tag/facet/view.jsp
@@ -1,0 +1,103 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ page import="com.liferay.portal.kernel.util.HtmlUtil" %><%@
+page import="com.liferay.portal.kernel.util.StringPool" %><%@
+page import="com.liferay.portal.search.web.internal.facet.display.context.AssetTagsSearchFacetDisplayContext" %><%@
+page import="com.liferay.portal.search.web.internal.facet.display.context.AssetTagsSearchFacetTermDisplayContext" %><%@
+page import="com.liferay.portal.search.web.internal.tag.facet.constants.TagFacetWebKeys" %>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
+taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+
+<portlet:defineObjects />
+
+<style>
+	.facet-checkbox-label {
+		display: block;
+	}
+</style>
+
+<%
+AssetTagsSearchFacetDisplayContext assetTagsSearchFacetDisplayContext = (AssetTagsSearchFacetDisplayContext)java.util.Objects.requireNonNull(request.getAttribute(TagFacetWebKeys.DISPLAY_CONTEXT));
+%>
+
+<c:choose>
+	<c:when test="<%= assetTagsSearchFacetDisplayContext.isRenderNothing() %>">
+		<aui:input autocomplete="off" name="<%= HtmlUtil.escapeAttribute(assetTagsSearchFacetDisplayContext.getParameterName()) %>" type="hidden" value="<%= assetTagsSearchFacetDisplayContext.getParameterValue() %>" />
+	</c:when>
+	<c:otherwise>
+		<liferay-ui:panel-container extended="<%= true %>" id='<%= renderResponse.getNamespace() + "facetAssetTagsPanelContainer" %>' markupView="lexicon" persistState="<%= true %>">
+			<liferay-ui:panel collapsible="<%= true %>" cssClass="search-facet" id='<%= renderResponse.getNamespace() + "facetAssetTagsPanel" %>' markupView="lexicon" persistState="<%= true %>" title="tag">
+				<aui:form method="post" name="assetTagsFacetForm">
+					<aui:input autocomplete="off" name="<%= HtmlUtil.escapeAttribute(assetTagsSearchFacetDisplayContext.getParameterName()) %>" type="hidden" value="<%= assetTagsSearchFacetDisplayContext.getParameterValue() %>" />
+					<aui:input cssClass="facet-parameter-name" name="facet-parameter-name" type="hidden" value="<%= assetTagsSearchFacetDisplayContext.getParameterName() %>" />
+
+					<aui:fieldset>
+						<ul class="<%= assetTagsSearchFacetDisplayContext.isCloudWithCount() ? "tag-cloud" : "tag-list" %> list-unstyled">
+
+							<%
+							int i = 0;
+
+							for (AssetTagsSearchFacetTermDisplayContext assetTagsSearchFacetTermDisplayContext : assetTagsSearchFacetDisplayContext.getTermDisplayContexts()) {
+								i++;
+							%>
+
+								<li class="facet-value tag-popularity-<%= assetTagsSearchFacetTermDisplayContext.getPopularity() %>">
+									<label class="facet-checkbox-label" for="<portlet:namespace />term_<%= i %>">
+										<input
+											class="facet-term"
+											data-term-id="<%= assetTagsSearchFacetTermDisplayContext.getValue() %>"
+											id="<portlet:namespace />term_<%= i %>"
+											name="<portlet:namespace />term_<%= i %>"
+											onChange='Liferay.Search.FacetUtil.changeSelection(event);'
+											type="checkbox"
+											<%= assetTagsSearchFacetTermDisplayContext.isSelected() ? "checked" : StringPool.BLANK %>
+										/>
+
+										<span class="term-name">
+											<%= HtmlUtil.escape(assetTagsSearchFacetTermDisplayContext.getDisplayName()) %>
+										</span>
+
+										<c:if test="<%= assetTagsSearchFacetTermDisplayContext.isFrequencyVisible() %>">
+											<small class="term-count">
+												(<%= assetTagsSearchFacetTermDisplayContext.getFrequency() %>)
+											</small>
+										</c:if>
+									</label>
+								</li>
+
+							<%
+							}
+							%>
+
+						</ul>
+					</aui:fieldset>
+
+					<c:if test="<%= !assetTagsSearchFacetDisplayContext.isNothingSelected() %>">
+						<aui:a cssClass="text-default" href="javascript:;" onClick="Liferay.Search.FacetUtil.clearSelections(event);"><small><liferay-ui:message key="clear" /></small></aui:a>
+					</c:if>
+				</aui:form>
+			</liferay-ui:panel>
+		</liferay-ui:panel-container>
+	</c:otherwise>
+</c:choose>
+
+<aui:script use="liferay-search-facet-util"></aui:script>

zoltantoth0409/liferay-portal

modules/apps/foundation/portal-search/portal-search-web/src/main/resources/META-INF/resources/tag/facet/view.jsp
@@ -1,0 +1,103 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ page import="com.liferay.portal.kernel.util.HtmlUtil" %><%@
+page import="com.liferay.portal.kernel.util.StringPool" %><%@
+page import="com.liferay.portal.kernel.util.WebKeys" %><%@
+page import="com.liferay.portal.search.web.internal.facet.display.context.AssetTagsSearchFacetDisplayContext" %><%@
+page import="com.liferay.portal.search.web.internal.facet.display.context.AssetTagsSearchFacetTermDisplayContext" %>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
+taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+
+<portlet:defineObjects />
+
+<style>
+	.facet-checkbox-label {
+		display: block;
+	}
+</style>
+
+<%
+AssetTagsSearchFacetDisplayContext assetTagsSearchFacetDisplayContext = (AssetTagsSearchFacetDisplayContext)java.util.Objects.requireNonNull(request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT));
+%>
+
+<c:choose>
+	<c:when test="<%= assetTagsSearchFacetDisplayContext.isRenderNothing() %>">
+		<aui:input autocomplete="off" name="<%= HtmlUtil.escapeAttribute(assetTagsSearchFacetDisplayContext.getParameterName()) %>" type="hidden" value="<%= assetTagsSearchFacetDisplayContext.getParameterValue() %>" />
+	</c:when>
+	<c:otherwise>
+		<liferay-ui:panel-container extended="<%= true %>" id='<%= renderResponse.getNamespace() + "facetAssetTagsPanelContainer" %>' markupView="lexicon" persistState="<%= true %>">
+			<liferay-ui:panel collapsible="<%= true %>" cssClass="search-facet" id='<%= renderResponse.getNamespace() + "facetAssetTagsPanel" %>' markupView="lexicon" persistState="<%= true %>" title="tag">
+				<aui:form method="post" name="assetTagsFacetForm">
+					<aui:input autocomplete="off" name="<%= HtmlUtil.escapeAttribute(assetTagsSearchFacetDisplayContext.getParameterName()) %>" type="hidden" value="<%= assetTagsSearchFacetDisplayContext.getParameterValue() %>" />
+					<aui:input cssClass="facet-parameter-name" name="facet-parameter-name" type="hidden" value="<%= assetTagsSearchFacetDisplayContext.getParameterName() %>" />
+
+					<aui:fieldset>
+						<ul class="<%= assetTagsSearchFacetDisplayContext.isCloudWithCount() ? "tag-cloud" : "tag-list" %> list-unstyled">
+
+							<%
+							int i = 0;
+
+							for (AssetTagsSearchFacetTermDisplayContext assetTagsSearchFacetTermDisplayContext : assetTagsSearchFacetDisplayContext.getTermDisplayContexts()) {
+								i++;
+							%>
+
+								<li class="facet-value tag-popularity-<%= assetTagsSearchFacetTermDisplayContext.getPopularity() %>">
+									<label class="facet-checkbox-label" for="<portlet:namespace />term_<%= i %>">
+										<input
+											class="facet-term"
+											data-term-id="<%= assetTagsSearchFacetTermDisplayContext.getValue() %>"
+											id="<portlet:namespace />term_<%= i %>"
+											name="<portlet:namespace />term_<%= i %>"
+											onChange='Liferay.Search.FacetUtil.changeSelection(event);'
+											type="checkbox"
+											<%= assetTagsSearchFacetTermDisplayContext.isSelected() ? "checked" : StringPool.BLANK %>
+										/>
+
+										<span class="term-name">
+											<%= HtmlUtil.escape(assetTagsSearchFacetTermDisplayContext.getDisplayName()) %>
+										</span>
+
+										<c:if test="<%= assetTagsSearchFacetTermDisplayContext.isFrequencyVisible() %>">
+											<small class="term-count">
+												(<%= assetTagsSearchFacetTermDisplayContext.getFrequency() %>)
+											</small>
+										</c:if>
+									</label>
+								</li>
+
+							<%
+							}
+							%>
+
+						</ul>
+					</aui:fieldset>
+
+					<c:if test="<%= !assetTagsSearchFacetDisplayContext.isNothingSelected() %>">
+						<aui:a cssClass="text-default" href="javascript:;" onClick="Liferay.Search.FacetUtil.clearSelections(event);"><small><liferay-ui:message key="clear" /></small></aui:a>
+					</c:if>
+				</aui:form>
+			</liferay-ui:panel>
+		</liferay-ui:panel-container>
+	</c:otherwise>
+</c:choose>
+
+<aui:script use="liferay-search-facet-util"></aui:script>