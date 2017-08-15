34065b30167b27525e53ee19b002ac3134db1c09
Matthew Tambara <matthew.tambara@liferay.com>
2017-08-14 17:11:53 -0700

    LPS-73562 Extract runtime LPKG creation to public util

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test-util/src/main/java/com/liferay/portal/lpkg/deployer/test/util/LPKGTestUtil.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test-util/src/main/java/com/liferay/portal/lpkg/deployer/test/util/LPKGTestUtil.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.lpkg.deployer.util;
+package com.liferay.portal.lpkg.deployer.test.util;
 
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test/src/test/java/com/liferay/portal/lpkg/deployer/controller/LPKGControllerTest.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-test/src/test/java/com/liferay/portal/lpkg/deployer/controller/LPKGControllerTest.java
@@ -14,7 +14,7 @@
 
 package com.liferay.portal.lpkg.deployer.controller;
 
-import com.liferay.portal.lpkg.deployer.util.LPKGTestUtil;
+import com.liferay.portal.lpkg.deployer.test.util.LPKGTestUtil;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
