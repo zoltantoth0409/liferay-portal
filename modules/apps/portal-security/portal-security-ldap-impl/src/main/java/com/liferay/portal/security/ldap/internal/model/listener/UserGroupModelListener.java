5ea8d426777e591b0ad372dbeb9c6190935b1a42
mbowerman <michael.bowerman@liferay.com>
2017-08-16 16:19:26 -0700

    LPS-74221 Apply the same solution from LPS-71933 to UserGroupModelListener

--- modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/model/listener/UserGroupModelListener.java
+++ modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/model/listener/UserGroupModelListener.java
@@ -21,9 +21,12 @@ import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
 import com.liferay.portal.security.exportimport.UserExporter;
 import com.liferay.portal.security.exportimport.UserOperation;
 import com.liferay.portal.security.ldap.internal.UserImportTransactionThreadLocal;
+
+import java.util.concurrent.Callable;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -78,24 +81,32 @@ public class UserGroupModelListener extends BaseModelListener<UserGroup> {
 	}
 
 	protected void exportToLDAP(
-			long userId, long userGroupId, UserOperation userOperation)
+			final long userId, final long userGroupId,
+			final UserOperation userOperation)
 		throws Exception {
 
 		if (UserImportTransactionThreadLocal.isOriginatesFromImport()) {
 			return;
 		}
 
-		_userExporter.exportUser(userId, userGroupId, userOperation);
+		Callable<Void> callable = new Callable<Void>() {
 
-		if (_log.isDebugEnabled()) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(
-					StringBundler.concat(
-						"Exporting user ", userId, " to user group ",
-						userGroupId, " with user operation ",
-						userOperation.name()));
-			}
-		}
+			@Override
+			public Void call() throws Exception {
+				_userExporter.exportUser(userId, userGroupId, userOperation);
+
+				if (_log.isDebugEnabled()) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(
+							"Exporting user " + userId + " to user group " +
+								userGroupId + " with user operation " +
+									userOperation.name());
+					}
+				}
+
+		};
+
+		TransactionCommitCallbackUtil.registerCallback(callable);
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
