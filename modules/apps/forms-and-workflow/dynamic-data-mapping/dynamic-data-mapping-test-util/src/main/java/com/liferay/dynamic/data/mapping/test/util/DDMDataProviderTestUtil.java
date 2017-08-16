ed5b969fd25edec7a72bd92d0f5425131a35d5c7
Leonardo Barros <leonardo.barros@liferay.com>
2017-08-16 16:35:32 -0300

    LPS-74058 Add integration tests

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test-util/src/main/java/com/liferay/dynamic/data/mapping/test/util/DDMDataProviderTestUtil.java
@@ -0,0 +1,176 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.test.util;
+
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderInputParametersSettings;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderOutputParametersSettings;
+import com.liferay.dynamic.data.mapping.model.DDMDataProviderInstance;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.util.DDMFormFactory;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DDMDataProviderTestUtil {
+
+	public static DDMDataProviderInstance createDDMRestDataProviderInstance(
+			Group group,
+			List<DDMDataProviderInputParametersSettings> inputParameterSettings,
+			List<DDMDataProviderOutputParametersSettings>
+				outputParameterSettings)
+		throws Exception {
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		DDMDataProvider[] ddmDataProviders = registry.getServices(
+			"com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider",
+			"(ddm.data.provider.type=rest)");
+
+		DDMDataProvider restDDMDataProvider = ddmDataProviders[0];
+
+		DDMForm ddmForm = DDMFormFactory.create(
+			restDDMDataProvider.getSettings());
+
+		DDMFormValues ddmFormValues = DDMFormValuesTestUtil.createDDMFormValues(
+			ddmForm);
+
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"cacheable", Boolean.FALSE.toString()));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"filterable", Boolean.FALSE.toString()));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"filterParameterName", StringPool.BLANK));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"pagination", Boolean.TRUE.toString()));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"password", "test"));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"paginationEndParameterName", "end"));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"paginationStartParameterName", "start"));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"url",
+				"http://localhost:8080/api/jsonws/country/get-countries"));
+		ddmFormValues.addDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"username", "test@liferay.com"));
+
+		if (inputParameterSettings != null) {
+			for (DDMDataProviderInputParametersSettings inputParameterSetting :
+					inputParameterSettings) {
+
+				ddmFormValues.addDDMFormFieldValue(
+					createInputParameter(inputParameterSetting));
+			}
+		}
+
+		if (outputParameterSettings != null) {
+			for (DDMDataProviderOutputParametersSettings
+					outputParameterSetting : outputParameterSettings) {
+
+				ddmFormValues.addDDMFormFieldValue(
+					createOutputParameter(outputParameterSetting));
+			}
+		}
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.getSiteDefault(), "Data provider");
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				group, TestPropsValues.getUserId());
+
+		return DDMDataProviderInstanceLocalServiceUtil.addDataProviderInstance(
+			TestPropsValues.getUserId(), group.getGroupId(), nameMap, nameMap,
+			ddmFormValues, "rest", serviceContext);
+	}
+
+	protected static DDMFormFieldValue createInputParameter(
+		DDMDataProviderInputParametersSettings inputParameterSetting) {
+
+		DDMFormFieldValue outputParameters =
+			DDMFormValuesTestUtil.createDDMFormFieldValue(
+				"inputParameters", null);
+
+		outputParameters.addNestedDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"inputParameterLabel",
+				inputParameterSetting.inputParameterLabel()));
+
+		outputParameters.addNestedDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"inputParameterName",
+				inputParameterSetting.inputParameterName()));
+
+		outputParameters.addNestedDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"inputParameterType",
+				inputParameterSetting.inputParameterType()));
+
+		return outputParameters;
+	}
+
+	protected static DDMFormFieldValue createOutputParameter(
+		DDMDataProviderOutputParametersSettings outputParameterSetting) {
+
+		DDMFormFieldValue outputParameters =
+			DDMFormValuesTestUtil.createDDMFormFieldValue(
+				"outputParameters", null);
+
+		outputParameters.addNestedDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"outputParameterName",
+				outputParameterSetting.outputParameterName()));
+
+		outputParameters.addNestedDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"outputParameterPath",
+				outputParameterSetting.outputParameterPath()));
+
+		outputParameters.addNestedDDMFormFieldValue(
+			DDMFormValuesTestUtil.createUnlocalizedDDMFormFieldValue(
+				"outputParameterType",
+				outputParameterSetting.outputParameterType()));
+
+		return outputParameters;
+	}
+
+}
---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/form/evaluator/test/DDMFormFieldTypeSettingsEvaluatorTest.java
@@ -0,0 +1,444 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.evaluator.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderOutputParametersSettings;
+import com.liferay.dynamic.data.mapping.form.evaluator.DDMFormEvaluationResult;
+import com.liferay.dynamic.data.mapping.form.evaluator.DDMFormEvaluator;
+import com.liferay.dynamic.data.mapping.form.evaluator.DDMFormEvaluatorContext;
+import com.liferay.dynamic.data.mapping.form.evaluator.DDMFormFieldEvaluationResult;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeServicesTracker;
+import com.liferay.dynamic.data.mapping.model.DDMDataProviderInstance;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.UnlocalizedValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.test.util.DDMDataProviderTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
+import com.liferay.dynamic.data.mapping.util.DDMFormFactory;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.util.KeyValuePair;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Leonardo Barros
+ */
+@RunWith(Arquillian.class)
+public class DDMFormFieldTypeSettingsEvaluatorTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE);
+
+	@Test
+	public void testSelectCallGetDataProviderInstanceOutputParameters()
+		throws Exception {
+
+		List<DDMDataProviderOutputParametersSettings> outputParametersSettings =
+			ListUtil.fromArray(
+				new DDMDataProviderOutputParametersSettings[] {
+					new DDMDataProviderOutputParametersSettings() {
+
+						@Override
+						public String outputParameterName() {
+							return "Countries";
+						}
+
+						@Override
+						public String outputParameterPath() {
+							return "nameCurrentValue";
+						}
+
+						@Override
+						public String outputParameterType() {
+							return "[\"list\"]";
+						}
+
+					}
+				});
+
+		DDMFormFieldEvaluationResult
+			ddmDataProviderInstanceOutputFieldEvaluationResult =
+				evaluateCallFunctionExpression(outputParametersSettings);
+
+		JSONArray jsonArray =
+			ddmDataProviderInstanceOutputFieldEvaluationResult.getValue();
+
+		Assert.assertEquals(1, jsonArray.length());
+
+		Assert.assertEquals("Countries", jsonArray.getString(0));
+	}
+
+	@Test
+	public void testSelectCallGetDataProviderInstanceOutputParameters2()
+		throws Exception {
+
+		List<DDMDataProviderOutputParametersSettings> outputParametersSettings =
+			ListUtil.fromArray(
+				new DDMDataProviderOutputParametersSettings[] {
+					new DDMDataProviderOutputParametersSettings() {
+
+						@Override
+						public String outputParameterName() {
+							return "Countries";
+						}
+
+						@Override
+						public String outputParameterPath() {
+							return "nameCurrentValue";
+						}
+
+						@Override
+						public String outputParameterType() {
+							return "[\"list\"]";
+						}
+
+					},
+					new DDMDataProviderOutputParametersSettings() {
+
+						@Override
+						public String outputParameterName() {
+							return "Countries2";
+						}
+
+						@Override
+						public String outputParameterPath() {
+							return "nameCurrentValue";
+						}
+
+						@Override
+						public String outputParameterType() {
+							return "[\"list\"]";
+						}
+
+					}
+				});
+
+		DDMFormFieldEvaluationResult
+			ddmDataProviderInstanceOutputFieldEvaluationResult =
+				evaluateCallFunctionExpression(outputParametersSettings);
+
+		List<KeyValuePair> options =
+			ddmDataProviderInstanceOutputFieldEvaluationResult.getProperty(
+				"options");
+
+		Assert.assertEquals(options.toString(), 2, options.size());
+
+		KeyValuePair keyValuePair = options.get(0);
+
+		Assert.assertEquals("Countries", keyValuePair.getKey());
+
+		keyValuePair = options.get(1);
+
+		Assert.assertEquals("Countries2", keyValuePair.getKey());
+	}
+
+	@Test
+	public void testSelectDataSourceTypeDataProvider() throws Exception {
+		Registry registry = RegistryUtil.getRegistry();
+
+		DDMFormFieldTypeServicesTracker ddmFormFieldTypeServicesTracker =
+			registry.getService(DDMFormFieldTypeServicesTracker.class);
+
+		DDMFormFieldType ddmFormFieldType =
+			ddmFormFieldTypeServicesTracker.getDDMFormFieldType("select");
+
+		DDMForm ddmForm = DDMFormFactory.create(
+			ddmFormFieldType.getDDMFormFieldTypeSettings());
+
+		DDMFormValues ddmFormValues =
+			DDMFormValuesTestUtil.createDDMFormValuesWithDefaultFieldValues(
+				ddmForm, SetUtil.fromArray(new Locale[] {LocaleUtil.US}),
+				LocaleUtil.US);
+
+		Map<String, List<DDMFormFieldValue>> ddmFormFieldValueMap =
+			ddmFormValues.getDDMFormFieldValuesMap();
+
+		List<DDMFormFieldValue> ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"dataSourceType");
+
+		DDMFormFieldValue dataSourceTypeFormFieldValue = ddmFormFieldValues.get(
+			0);
+
+		dataSourceTypeFormFieldValue.setValue(
+			new UnlocalizedValue("data-provider"));
+
+		DDMFormEvaluator ddmFormEvaluator = registry.getService(
+			DDMFormEvaluator.class);
+
+		DDMFormEvaluatorContext ddmFormEvaluatorContext =
+			new DDMFormEvaluatorContext(ddmForm, ddmFormValues, LocaleUtil.US);
+
+		ddmFormEvaluatorContext.addProperty("groupId", 1L);
+
+		DDMFormEvaluationResult ddmFormEvaluationResult =
+			ddmFormEvaluator.evaluate(ddmFormEvaluatorContext);
+
+		Map<String, DDMFormFieldEvaluationResult>
+			ddmFormFieldEvaluationResultsMap =
+				ddmFormEvaluationResult.getDDMFormFieldEvaluationResultsMap();
+
+		ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"ddmDataProviderInstanceId");
+
+		DDMFormFieldValue ddmDataProviderInstanceIdFormFieldValue =
+			ddmFormFieldValues.get(0);
+
+		DDMFormFieldEvaluationResult
+			ddmDataProviderInstanceIdFieldEvaluationResult =
+				ddmFormFieldEvaluationResultsMap.get(
+					String.format(
+						"ddmDataProviderInstanceId_INSTANCE_%s",
+						ddmDataProviderInstanceIdFormFieldValue.
+							getInstanceId()));
+
+		Assert.assertTrue(
+			ddmDataProviderInstanceIdFieldEvaluationResult.isVisible());
+		Assert.assertTrue(
+			ddmDataProviderInstanceIdFieldEvaluationResult.isRequired());
+
+		ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"ddmDataProviderInstanceOutput");
+
+		DDMFormFieldValue ddmDataProviderInstanceOutputFormFieldValue =
+			ddmFormFieldValues.get(0);
+
+		DDMFormFieldEvaluationResult
+			ddmDataProviderInstanceOutputFieldEvaluationResult =
+				ddmFormFieldEvaluationResultsMap.get(
+					String.format(
+						"ddmDataProviderInstanceOutput_INSTANCE_%s",
+						ddmDataProviderInstanceOutputFormFieldValue.
+							getInstanceId()));
+
+		Assert.assertTrue(
+			ddmDataProviderInstanceOutputFieldEvaluationResult.isVisible());
+		Assert.assertTrue(
+			ddmDataProviderInstanceOutputFieldEvaluationResult.isRequired());
+
+		ddmFormFieldValues = ddmFormFieldValueMap.get("options");
+
+		DDMFormFieldValue optionsDDMFormFieldValue = ddmFormFieldValues.get(0);
+
+		DDMFormFieldEvaluationResult optionsFieldEvaluationResult =
+			ddmFormFieldEvaluationResultsMap.get(
+				String.format(
+					"options_INSTANCE_%s",
+					optionsDDMFormFieldValue.getInstanceId()));
+
+		Assert.assertFalse(optionsFieldEvaluationResult.isVisible());
+		Assert.assertFalse(optionsFieldEvaluationResult.isRequired());
+	}
+
+	@Test
+	public void testSelectDataSourceTypeManual() throws Exception {
+		Registry registry = RegistryUtil.getRegistry();
+
+		DDMFormFieldTypeServicesTracker ddmFormFieldTypeServicesTracker =
+			registry.getService(DDMFormFieldTypeServicesTracker.class);
+
+		DDMFormFieldType ddmFormFieldType =
+			ddmFormFieldTypeServicesTracker.getDDMFormFieldType("select");
+
+		DDMForm ddmForm = DDMFormFactory.create(
+			ddmFormFieldType.getDDMFormFieldTypeSettings());
+
+		DDMFormValues ddmFormValues =
+			DDMFormValuesTestUtil.createDDMFormValuesWithDefaultFieldValues(
+				ddmForm, SetUtil.fromArray(new Locale[] {LocaleUtil.US}),
+				LocaleUtil.US);
+
+		Map<String, List<DDMFormFieldValue>> ddmFormFieldValueMap =
+			ddmFormValues.getDDMFormFieldValuesMap();
+
+		List<DDMFormFieldValue> ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"dataSourceType");
+
+		DDMFormFieldValue dataSourceTypeFormFieldValue = ddmFormFieldValues.get(
+			0);
+
+		dataSourceTypeFormFieldValue.setValue(new UnlocalizedValue("manual"));
+
+		DDMFormEvaluator ddmFormEvaluator = registry.getService(
+			DDMFormEvaluator.class);
+
+		DDMFormEvaluatorContext ddmFormEvaluatorContext =
+			new DDMFormEvaluatorContext(ddmForm, ddmFormValues, LocaleUtil.US);
+
+		ddmFormEvaluatorContext.addProperty("groupId", 1L);
+
+		DDMFormEvaluationResult ddmFormEvaluationResult =
+			ddmFormEvaluator.evaluate(ddmFormEvaluatorContext);
+
+		Map<String, DDMFormFieldEvaluationResult>
+			ddmFormFieldEvaluationResultsMap =
+				ddmFormEvaluationResult.getDDMFormFieldEvaluationResultsMap();
+
+		ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"ddmDataProviderInstanceId");
+
+		DDMFormFieldValue ddmDataProviderInstanceIdFormFieldValue =
+			ddmFormFieldValues.get(0);
+
+		DDMFormFieldEvaluationResult
+			ddmDataProviderInstanceIdFieldEvaluationResult =
+				ddmFormFieldEvaluationResultsMap.get(
+					String.format(
+						"ddmDataProviderInstanceId_INSTANCE_%s",
+						ddmDataProviderInstanceIdFormFieldValue.
+							getInstanceId()));
+
+		Assert.assertFalse(
+			ddmDataProviderInstanceIdFieldEvaluationResult.isVisible());
+		Assert.assertFalse(
+			ddmDataProviderInstanceIdFieldEvaluationResult.isRequired());
+
+		ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"ddmDataProviderInstanceOutput");
+
+		DDMFormFieldValue ddmDataProviderInstanceOutputFormFieldValue =
+			ddmFormFieldValues.get(0);
+
+		DDMFormFieldEvaluationResult
+			ddmDataProviderInstanceOutputFieldEvaluationResult =
+				ddmFormFieldEvaluationResultsMap.get(
+					String.format(
+						"ddmDataProviderInstanceOutput_INSTANCE_%s",
+						ddmDataProviderInstanceOutputFormFieldValue.
+							getInstanceId()));
+
+		Assert.assertFalse(
+			ddmDataProviderInstanceOutputFieldEvaluationResult.isVisible());
+		Assert.assertFalse(
+			ddmDataProviderInstanceOutputFieldEvaluationResult.isRequired());
+
+		ddmFormFieldValues = ddmFormFieldValueMap.get("options");
+
+		DDMFormFieldValue optionsDDMFormFieldValue = ddmFormFieldValues.get(0);
+
+		DDMFormFieldEvaluationResult optionsFieldEvaluationResult =
+			ddmFormFieldEvaluationResultsMap.get(
+				String.format(
+					"options_INSTANCE_%s",
+					optionsDDMFormFieldValue.getInstanceId()));
+
+		Assert.assertTrue(optionsFieldEvaluationResult.isVisible());
+		Assert.assertTrue(optionsFieldEvaluationResult.isRequired());
+	}
+
+	protected DDMFormFieldEvaluationResult evaluateCallFunctionExpression(
+			List<DDMDataProviderOutputParametersSettings>
+				outputParametersSettings)
+		throws Exception {
+
+		DDMDataProviderInstance ddmDataProviderInstance =
+			DDMDataProviderTestUtil.createDDMRestDataProviderInstance(
+				GroupTestUtil.addGroup(), null, outputParametersSettings);
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		DDMFormFieldTypeServicesTracker ddmFormFieldTypeServicesTracker =
+			registry.getService(DDMFormFieldTypeServicesTracker.class);
+
+		DDMFormFieldType ddmFormFieldType =
+			ddmFormFieldTypeServicesTracker.getDDMFormFieldType("select");
+
+		DDMForm ddmForm = DDMFormFactory.create(
+			ddmFormFieldType.getDDMFormFieldTypeSettings());
+
+		DDMFormValues ddmFormValues =
+			DDMFormValuesTestUtil.createDDMFormValuesWithDefaultFieldValues(
+				ddmForm, SetUtil.fromArray(new Locale[] {LocaleUtil.US}),
+				LocaleUtil.US);
+
+		Map<String, List<DDMFormFieldValue>> ddmFormFieldValueMap =
+			ddmFormValues.getDDMFormFieldValuesMap();
+
+		List<DDMFormFieldValue> ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"dataSourceType");
+
+		DDMFormFieldValue dataSourceTypeFormFieldValue = ddmFormFieldValues.get(
+			0);
+
+		dataSourceTypeFormFieldValue.setValue(
+			new UnlocalizedValue("data-provider"));
+
+		ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"ddmDataProviderInstanceId");
+
+		DDMFormFieldValue ddmDataProviderInstanceIdFormFieldValue =
+			ddmFormFieldValues.get(0);
+
+		ddmDataProviderInstanceIdFormFieldValue.setValue(
+			new UnlocalizedValue(
+				String.format(
+					"['%d']", ddmDataProviderInstance.getPrimaryKey())));
+
+		DDMFormEvaluator ddmFormEvaluator = registry.getService(
+			DDMFormEvaluator.class);
+
+		DDMFormEvaluatorContext ddmFormEvaluatorContext =
+			new DDMFormEvaluatorContext(ddmForm, ddmFormValues, LocaleUtil.US);
+
+		ddmFormEvaluatorContext.addProperty("groupId", 1L);
+
+		DDMFormEvaluationResult ddmFormEvaluationResult =
+			ddmFormEvaluator.evaluate(ddmFormEvaluatorContext);
+
+		ddmFormFieldValues = ddmFormFieldValueMap.get(
+			"ddmDataProviderInstanceOutput");
+
+		DDMFormFieldValue ddmDataProviderInstanceOutputFormFieldValue =
+			ddmFormFieldValues.get(0);
+
+		Map<String, DDMFormFieldEvaluationResult>
+			ddmFormFieldEvaluationResultsMap =
+				ddmFormEvaluationResult.getDDMFormFieldEvaluationResultsMap();
+
+		DDMFormFieldEvaluationResult
+			ddmDataProviderInstanceOutputFieldEvaluationResult =
+				ddmFormFieldEvaluationResultsMap.get(
+					String.format(
+						"ddmDataProviderInstanceOutput_INSTANCE_%s",
+						ddmDataProviderInstanceOutputFormFieldValue.
+							getInstanceId()));
+
+		return ddmDataProviderInstanceOutputFieldEvaluationResult;
+	}
+
+}
