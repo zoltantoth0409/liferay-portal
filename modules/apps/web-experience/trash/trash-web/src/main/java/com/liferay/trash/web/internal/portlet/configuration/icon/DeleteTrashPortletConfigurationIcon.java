55048aa76c8d3c45b3701f4142928fb613e35b9c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-08-17 19:03:24 +0200

    LPS-74119 Adds restore and delete actions to view content, when it is not a container

---
+++ modules/apps/web-experience/trash/trash-web/src/main/java/com/liferay/trash/web/internal/portlet/configuration/icon/DeleteTrashPortletConfigurationIcon.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.trash.web.internal.portlet.configuration.icon;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.trash.TrashHandler;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.trash.web.internal.constants.TrashPortletKeys;
+import com.liferay.trash.web.internal.display.context.TrashDisplayContext;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + TrashPortletKeys.TRASH, "path=/view_content.jsp"
+	},
+	service = PortletConfigurationIcon.class
+)
+public class DeleteTrashPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "delete");
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		try {
+			TrashDisplayContext trashDisplayContext = new TrashDisplayContext(
+				_portal.getHttpServletRequest(portletRequest),
+				_portal.getLiferayPortletResponse(portletResponse));
+
+			PortletURL deleteURL = _portal.getControlPanelPortletURL(
+				portletRequest, TrashPortletKeys.TRASH,
+				PortletRequest.ACTION_PHASE);
+
+			deleteURL.setParameter(ActionRequest.ACTION_NAME, "deleteEntries");
+
+			deleteURL.setParameter(
+				"redirect", trashDisplayContext.getViewContentRedirectURL());
+			deleteURL.setParameter(
+				"className", trashDisplayContext.getClassName());
+			deleteURL.setParameter(
+				"classPK", String.valueOf(trashDisplayContext.getClassPK()));
+
+			return deleteURL.toString();
+		}
+		catch (Exception e) {
+		}
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public double getWeight() {
+		return 200.0;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		TrashDisplayContext trashDisplayContext = new TrashDisplayContext(
+			_portal.getHttpServletRequest(portletRequest), null);
+
+		TrashHandler trashHandler = trashDisplayContext.getTrashHandler();
+
+		if (trashHandler == null) {
+			return false;
+		}
+
+		if (!trashHandler.isDeletable()) {
+			return false;
+		}
+
+		if (trashHandler.isContainerModel()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Reference
+	private Portal _portal;
+
+}
---
+++ modules/apps/web-experience/trash/trash-web/src/main/java/com/liferay/trash/web/internal/portlet/configuration/icon/RestoreRootTrashPortletConfigurationIcon.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.trash.web.internal.portlet.configuration.icon;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.trash.TrashHandler;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.trash.model.TrashEntry;
+import com.liferay.trash.web.internal.constants.TrashPortletKeys;
+import com.liferay.trash.web.internal.display.context.TrashDisplayContext;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + TrashPortletKeys.TRASH, "path=/view_content.jsp"
+	},
+	service = PortletConfigurationIcon.class
+)
+public class RestoreRootTrashPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "restore");
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		try {
+			TrashDisplayContext trashDisplayContext = new TrashDisplayContext(
+				_portal.getHttpServletRequest(portletRequest),
+				_portal.getLiferayPortletResponse(portletResponse));
+
+			PortletURL restoreURL = _portal.getControlPanelPortletURL(
+				portletRequest, TrashPortletKeys.TRASH,
+				PortletRequest.ACTION_PHASE);
+
+			restoreURL.setParameter(
+				ActionRequest.ACTION_NAME, "restoreEntries");
+
+			restoreURL.setParameter(
+				"redirect", trashDisplayContext.getViewContentRedirectURL());
+			restoreURL.setParameter(
+				"trashEntryId",
+				String.valueOf(trashDisplayContext.getTrashEntryId()));
+
+			return restoreURL.toString();
+		}
+		catch (Exception e) {
+		}
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public double getWeight() {
+		return 100.0;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		TrashDisplayContext trashDisplayContext = new TrashDisplayContext(
+			_portal.getHttpServletRequest(portletRequest), null);
+
+		TrashEntry trashEntry = trashDisplayContext.getTrashEntry();
+
+		if (trashEntry == null) {
+			return false;
+		}
+
+		TrashHandler trashHandler = trashDisplayContext.getTrashHandler();
+
+		if (trashHandler == null) {
+			return false;
+		}
+
+		try {
+			if (!trashHandler.isRestorable(trashEntry.getClassPK())) {
+				return false;
+			}
+
+			if (trashHandler.isInTrashContainer(trashEntry.getClassPK())) {
+				return false;
+			}
+		}
+		catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Reference
+	private Portal _portal;
+
+}
