2f41a3c99af56eacb580794ebc34f6e6c70ca9a2
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-08-17 15:49:55 -0700

    COMMERCE-2 SF

--- modules/dxp/apps/commerce/commerce-cart-content-web/src/main/java/com/liferay/commerce/cart/content/web/internal/display/context/CommerceCartContentDisplayContext.java
+++ modules/dxp/apps/commerce/commerce-cart-content-web/src/main/java/com/liferay/commerce/cart/content/web/internal/display/context/CommerceCartContentDisplayContext.java
@@ -56,7 +56,6 @@ public class CommerceCartContentDisplayContext {
 			httpServletRequest);
 
 		liferayPortletRequest = cpRequestHelper.getLiferayPortletRequest();
-
 		liferayPortletResponse = cpRequestHelper.getLiferayPortletResponse();
 	}
 
