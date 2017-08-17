4677c56caa1b0a54ff8e6210402d2fb4f1d25718
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-08-17 09:17:24 -0300

    LPS-74208 Test SF: use util method to create entities.

--- modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/upgrade/v2_0_0/test/UpgradeSchemaTest.java
+++ modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/upgrade/v2_0_0/test/UpgradeSchemaTest.java
@@ -17,27 +17,17 @@ package com.liferay.calendar.upgrade.v2_0_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.calendar.model.Calendar;
 import com.liferay.calendar.model.CalendarBooking;
-import com.liferay.calendar.model.CalendarBookingConstants;
-import com.liferay.calendar.model.CalendarResource;
-import com.liferay.calendar.service.CalendarBookingLocalServiceUtil;
+import com.liferay.calendar.test.util.CalendarBookingTestUtil;
+import com.liferay.calendar.test.util.CalendarTestUtil;
 import com.liferay.calendar.test.util.CheckBookingsMessageListenerTestUtil;
 import com.liferay.calendar.upgrade.v2_0_0.UpgradeSchema;
-import com.liferay.calendar.util.CalendarResourceUtil;
 import com.liferay.calendar.util.CalendarUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
-import com.liferay.portal.kernel.test.util.UserTestUtil;
-import com.liferay.portal.kernel.util.DateUtil;
-import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.SynchronousMailTestRule;
 
@@ -69,7 +59,8 @@ public class UpgradeSchemaTest extends UpgradeSchema {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
-		_user = UserTestUtil.addUser();
+
+		_calendar = CalendarTestUtil.addCalendar(_group);
 
 		CheckBookingsMessageListenerTestUtil.setUp();
 	}
@@ -97,7 +88,8 @@ public class UpgradeSchemaTest extends UpgradeSchema {
 
 	@Test
 	public void testUpgradeSetsRecurringCalendarBookingId() throws Exception {
-		CalendarBooking calendarBooking = addCalendarBooking();
+		CalendarBooking calendarBooking =
+			CalendarBookingTestUtil.addRegularCalendarBooking(_calendar);
 
 		long recurringCalendarBookingId =
 			calendarBooking.getRecurringCalendarBookingId();
@@ -122,35 +114,6 @@ public class UpgradeSchemaTest extends UpgradeSchema {
 		}
 	}
 
-	protected Calendar addCalendar() throws PortalException {
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext();
-
-		serviceContext.setCompanyId(_user.getCompanyId());
-
-		CalendarResource calendarResource =
-			CalendarResourceUtil.getUserCalendarResource(
-				_user.getUserId(), serviceContext);
-
-		return calendarResource.getDefaultCalendar();
-	}
-
-	protected CalendarBooking addCalendarBooking() throws PortalException {
-		Calendar calendar = addCalendar();
-
-		long startTime = DateUtil.newTime();
-
-		return CalendarBookingLocalServiceUtil.addCalendarBooking(
-			_user.getUserId(), calendar.getCalendarId(), new long[0],
-			CalendarBookingConstants.PARENT_CALENDAR_BOOKING_ID_DEFAULT,
-			CalendarBookingConstants.RECURRING_CALENDAR_BOOKING_ID_DEFAULT,
-			RandomTestUtil.randomLocaleStringMap(),
-			RandomTestUtil.randomLocaleStringMap(),
-			RandomTestUtil.randomString(), startTime, startTime + Time.HOUR,
-			false, null, 0, null, 0, null,
-			ServiceContextTestUtil.getServiceContext());
-	}
-
 	protected void dropColumnRecurringCalendarBookingId() throws Exception {
 		try (Connection con = DataAccess.getUpgradeOptimizedConnection()) {
 			connection = con;
@@ -172,9 +135,9 @@ public class UpgradeSchemaTest extends UpgradeSchema {
 	}
 
 	@DeleteAfterTestRun
-	private Group _group;
+	private Calendar _calendar;
 
 	@DeleteAfterTestRun
-	private User _user;
+	private Group _group;
 
 }
