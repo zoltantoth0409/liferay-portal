mtambara/com-liferay-journal

LPS-67037 Rewrite js to use AUI script

journal-content-web/src/main/resources/META-INF/resources/configuration.jsp
@@ -18,8 +18,6 @@
 
 <%
 String redirect = ParamUtil.getString(request, "redirect");
-
-JournalArticle article = journalContentDisplayContext.getArticle();
 %>
 
 <liferay-ui:error exception="<%= NoSuchArticleException.class %>" message="the-web-content-could-not-be-found" />
@@ -38,72 +36,10 @@ JournalArticle article = journalContentDisplayContext.getArticle();
 		<div class="container-fluid-1280">
 			<aui:fieldset-group markupView="lexicon">
 				<aui:fieldset>
-					<div class="article-preview row">
-						<div class="col-md-3 col-sm-6 col-xs-12">
-							<p class="text-muted"><liferay-ui:message key="layout.types.article" /></p>
-
-							<div class="hidden loading-animation"></div>
-
-							<div class="alert alert-danger hidden">
-								<liferay-ui:message key="an-unexpected-error-occurred" />
-							</div>
-
-							<div class="article-preview-content-container">
-								<c:if test="<%= article != null %>">
-									<liferay-util:include page="/journal_article_resources.jsp" servletContext="<%= application %>">
-										<liferay-util:param name="articleId" value="<%= article.getArticleId() %>" />
-									</liferay-util:include>
-								</c:if>
-							</div>
-
-							<div class="button-holder">
-								<aui:button cssClass="web-content-selector" name="webContentSelector" value='<%= Validator.isNull(article) ? "select" : "change" %>' />
-							</div>
-						</div>
-					</div>
-
-					<div class="row template-preview <%= article == null ? "hidden" : "" %>">
-						<div class="col-md-3 col-sm-6 col-xs-12">
-							<div class="hidden loading-animation"></div>
-
-							<div class="alert alert-danger hidden">
-								<liferay-ui:message key="an-unexpected-error-occurred" />
-							</div>
-
-							<div class="template-preview-content-container">
-								<c:if test="<%= article != null %>">
-									<liferay-util:include page="/journal_template_resources.jsp" servletContext="<%= application %>">
-										<liferay-util:param name="articleId" value="<%= article.getArticleId() %>" />
-									</liferay-util:include>
-								</c:if>
-							</div>
-						</div>
-					</div>
-
-					<div class="<%= article == null ? "hidden" : "" %> configuration-options-container row">
-						<aui:fieldset>
-							<aui:field-wrapper label="user-tools">
-								<liferay-ui:asset-addon-entry-selector
-									assetAddonEntries="<%= (List<AssetAddonEntry>)(List<?>)journalContentDisplayContext.getEnabledUserToolAssetAddonEntries() %>"
-									hiddenInput="preferences--userToolAssetAddonEntryKeys--"
-									id="userToolsAssetAddonEntriesSelector"
-									selectedAssetAddonEntries="<%= (List<AssetAddonEntry>)(List<?>)journalContentDisplayContext.getSelectedUserToolAssetAddonEntries() %>"
-									title="select-user-tools"
-								/>
-							</aui:field-wrapper>
-
-							<aui:field-wrapper label="content-metadata">
-								<liferay-ui:asset-addon-entry-selector
-									assetAddonEntries="<%= (List<AssetAddonEntry>)(List<?>)journalContentDisplayContext.getEnabledContentMetadataAssetAddonEntries() %>"
-									hiddenInput="preferences--contentMetadataAssetAddonEntryKeys--"
-									id="contentMetadataAssetAddonEntriesSelector"
-									selectedAssetAddonEntries="<%= (List<AssetAddonEntry>)(List<?>)journalContentDisplayContext.getSelectedContentMetadataAssetAddonEntries() %>"
-									title="select-content-metadata"
-								/>
-							</aui:field-wrapper>
-
-							<aui:input name="preferences--enableViewCountIncrement--" type="toggle-switch" value="<%= journalContentDisplayContext.isEnableViewCountIncrement() %>" />
-						</aui:fieldset>
+					<div id="<portlet:namespace />articlePreview">
+						<liferay-util:include page="/journal_resources.jsp" servletContext="<%= application %>">
+							<liferay-util:param name="refererPortletName" value="<%= renderResponse.getNamespace() %>" />
+						</liferay-util:include>
 					</div>
 				</aui:fieldset>
 			</aui:fieldset-group>
@@ -117,42 +53,14 @@ JournalArticle article = journalContentDisplayContext.getArticle();
 	</aui:button-row>
 </aui:form>
 
-<aui:script sandbox="<%= true %>">
-	var STR_HIDDEN = 'hidden';
+<aui:script sandbox="<%= true %>" use="aui-io-request,aui-parse-content">
+	var form = A.one('#<portlet:namespace />fm');
 
-	var form = AUI.$(document.<portlet:namespace />fm);
+	var articlePreview = A.one('#<portlet:namespace />articlePreview');
 
-	var assetClassPK = '<%= (article != null) ? JournalArticleAssetRenderer.getClassPK(article) : "" %>';
-	var articlePreviewNode = $('.article-preview');
-	var templatePreviewNode = $('.template-preview');
+	articlePreview.plug(A.Plugin.ParseContent);
 
-	var showLoading = function(element) {
-		element.find('.loading-animation').removeClass(STR_HIDDEN);
-	};
-
-	var hideLoading = function(element) {
-		element.find('.loading-animation').addClass(STR_HIDDEN);
-	};
-
-	var showError = function(element) {
-		element.find('.alert').removeClass(STR_HIDDEN);
-	};
-
-	var hideError = function(element) {
-		element.find('.alert').addClass(STR_HIDDEN);
-	};
-
-	var showArticleError = function() {
-		showError(articlePreviewNode);
-	};
-
-	var showTemplateError = function() {
-		templatePreviewNode.removeClass(STR_HIDDEN);
-
-		showError(templatePreviewNode);
-	};
-
-	$('#<portlet:namespace />webContentSelector').on(
+	articlePreview.delegate(
 		'click',
 		function(event) {
 			event.preventDefault();
@@ -182,104 +90,39 @@ JournalArticle article = journalContentDisplayContext.getArticle();
 					eventName: 'selectContent',
 					id: 'selectContent',
 					title: '<liferay-ui:message key="select-web-content" />',
-					uri: baseSelectWebContentURI.replace(encodeURIComponent('[$ARTICLE_REFERER_ASSET_ENTRY_ID$]'), form.fm('assetEntryId').val())
+					uri: baseSelectWebContentURI.replace(encodeURIComponent('[$ARTICLE_REFERER_ASSET_ENTRY_ID$]'), form.attr('<portlet:namespace/>assetEntryId').val())
 				},
 				function(event) {
-					<liferay-portlet:resourceURL portletName="<%= JournalContentPortletKeys.JOURNAL_CONTENT %>" var="journalArticleResource">
-						<portlet:param name="mvcPath" value="/journal_resources.jsp" />
-						<portlet:param name="articleResourcePrimKey" value="[$ARTICLE_RESOURCE_PRIMKEY$]" />
-					</liferay-portlet:resourceURL>
+					form.attr('<portlet:namespace/>assetEntryId').val(event.assetentryid);
 
-					var baseJournalArticleResourceUrl = '<%= journalArticleResource.toString() %>';
+					articlePreview.html('<div class="loading-animation"></div>');
 
-					form.fm('assetEntryId').val(event.assetentryid);
-					form.fm('ddmTemplateKey').val('');
-
-					hideError(articlePreviewNode);
-					showLoading(articlePreviewNode);
-
-					templatePreviewNode.addClass(STR_HIDDEN);
-
-					articlePreviewNode.find('.article-preview-content-container').html('');
-					templatePreviewNode.find('.template-preview-content-container').html('');
-
-					articlePreviewNode.find('.web-content-selector').html('<liferay-ui:message key="change" />');
-
-					assetClassPK = event.assetclasspk;
-
-					$.ajax(
-						baseJournalArticleResourceUrl.replace(encodeURIComponent('[$ARTICLE_RESOURCE_PRIMKEY$]'), assetClassPK),
+					var data = Liferay.Util.ns(
+						'<%= PortalUtil.getPortletNamespace(JournalContentPortletKeys.JOURNAL_CONTENT) %>',
 						{
-							error: function() {
-								hideLoading(articlePreviewNode);
-								showArticleError();
-							},
-							success: function(responseData) {
-								var responseDataNode = $(responseData);
+							articleResourcePrimKey: event.assetclasspk
+						}
+					);
 
-								var articlePreviewContent = responseDataNode.find('.article-preview-content');
-								var templatePreviewContent = responseDataNode.find('.template-preview-content');
+					A.io.request(
+						'<liferay-portlet:resourceURL portletName="<%= JournalContentPortletKeys.JOURNAL_CONTENT %>" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>"><portlet:param name="mvcPath" value="/journal_resources.jsp" /><portlet:param name="refererPortletName" value="<%= renderResponse.getNamespace() %>" /></liferay-portlet:resourceURL>',
+						{
+							data: data,
+							on: {
+								failure: function() {
+									articlePreview.html('<div class="alert alert-danger hidden"><liferay-ui:message key="an-unexpected-error-occurred" /></div>');
+								},
+								success: function(event, id, obj) {
+									var responseData = this.get('responseData');
 
-								hideLoading(articlePreviewNode);
-
-								articlePreviewNode.find('.article-preview-content-container').html(articlePreviewContent);
-								templatePreviewNode.find('.template-preview-content-container').html(templatePreviewContent);
-
-								if (articlePreviewContent.length > 0) {
-									$('.configuration-options-container').removeClass(STR_HIDDEN);
-
-									if (templatePreviewContent.length > 0) {
-										var templatePreviewContentNode = templatePreviewNode.find('.template-preview-content');
-
-										templatePreviewNode.removeClass(STR_HIDDEN);
-
-										form.fm('ddmTemplateKey').val(templatePreviewContentNode.attr('data-template-key'));
-									}
-									else {
-										showTemplateError();
-									}
-								}
-								else {
-									showArticleError();
+									articlePreview.setContent(responseData);
 								}
 							}
 						}
 					);
 				}
 			);
-		}
-	);
-
-	Liferay.on(
-		'changeTemplate',
-		function(event) {
-			var ddmTemplateKey = event.ddmTemplateKey;
-
-			<liferay-portlet:resourceURL portletName="<%= JournalContentPortletKeys.JOURNAL_CONTENT %>" var="templateURL" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>">
-				<portlet:param name="mvcPath" value="/journal_template_resources.jsp" />
-				<portlet:param name="articleResourcePrimKey" value="[$ARTICLE_RESOURCE_PRIMKEY$]" />
-				<portlet:param name="ddmTemplateKey" value="[$DDM_TEMPLATE_KEY$]" />
-			</liferay-portlet:resourceURL>
-
-			var templateURL = '<%= templateURL %>';
-
-			templateURL = templateURL.replace(encodeURIComponent('[$ARTICLE_RESOURCE_PRIMKEY$]'), assetClassPK),
-			templateURL = templateURL.replace(encodeURIComponent('[$DDM_TEMPLATE_KEY$]'), ddmTemplateKey),
-
-			templatePreviewNode.find('.template-preview-content-container').html('');
-
-			$.ajax(
-				templateURL,
-				{
-					success: function(responseData) {
-						var responseData = $(responseData);
-
-						templatePreviewNode.find('.template-preview-content-container').html(responseData);
-
-						form.fm('ddmTemplateKey').val(ddmTemplateKey);
-					}
-				}
-			);
-		}
+		},
+		'.web-content-selector'
 	);
 </aui:script>

zoltantoth0409/liferay-portal

modules/apps/web-experience/journal/journal-content-web/src/main/resources/META-INF/resources/configuration.jsp
@@ -18,8 +18,6 @@
 
 <%
 String redirect = ParamUtil.getString(request, "redirect");
-
-JournalArticle article = journalContentDisplayContext.getArticle();
 %>
 
 <liferay-ui:error exception="<%= NoSuchArticleException.class %>" message="the-web-content-could-not-be-found" />
@@ -38,72 +36,10 @@ JournalArticle article = journalContentDisplayContext.getArticle();
 		<div class="container-fluid-1280">
 			<aui:fieldset-group markupView="lexicon">
 				<aui:fieldset>
-					<div class="article-preview row">
-						<div class="col-md-3 col-sm-6 col-xs-12">
-							<p class="text-muted"><liferay-ui:message key="layout.types.article" /></p>
-
-							<div class="hidden loading-animation"></div>
-
-							<div class="alert alert-danger hidden">
-								<liferay-ui:message key="an-unexpected-error-occurred" />
-							</div>
-
-							<div class="article-preview-content-container">
-								<c:if test="<%= article != null %>">
-									<liferay-util:include page="/journal_article_resources.jsp" servletContext="<%= application %>">
-										<liferay-util:param name="articleId" value="<%= article.getArticleId() %>" />
-									</liferay-util:include>
-								</c:if>
-							</div>
-
-							<div class="button-holder">
-								<aui:button cssClass="web-content-selector" name="webContentSelector" value='<%= Validator.isNull(article) ? "select" : "change" %>' />
-							</div>
-						</div>
-					</div>
-
-					<div class="row template-preview <%= article == null ? "hidden" : "" %>">
-						<div class="col-md-3 col-sm-6 col-xs-12">
-							<div class="hidden loading-animation"></div>
-
-							<div class="alert alert-danger hidden">
-								<liferay-ui:message key="an-unexpected-error-occurred" />
-							</div>
-
-							<div class="template-preview-content-container">
-								<c:if test="<%= article != null %>">
-									<liferay-util:include page="/journal_template_resources.jsp" servletContext="<%= application %>">
-										<liferay-util:param name="articleId" value="<%= article.getArticleId() %>" />
-									</liferay-util:include>
-								</c:if>
-							</div>
-						</div>
-					</div>
-
-					<div class="<%= article == null ? "hidden" : "" %> configuration-options-container row">
-						<aui:fieldset>
-							<aui:field-wrapper label="user-tools">
-								<liferay-ui:asset-addon-entry-selector
-									assetAddonEntries="<%= (List<AssetAddonEntry>)(List<?>)journalContentDisplayContext.getEnabledUserToolAssetAddonEntries() %>"
-									hiddenInput="preferences--userToolAssetAddonEntryKeys--"
-									id="userToolsAssetAddonEntriesSelector"
-									selectedAssetAddonEntries="<%= (List<AssetAddonEntry>)(List<?>)journalContentDisplayContext.getSelectedUserToolAssetAddonEntries() %>"
-									title="select-user-tools"
-								/>
-							</aui:field-wrapper>
-
-							<aui:field-wrapper label="content-metadata">
-								<liferay-ui:asset-addon-entry-selector
-									assetAddonEntries="<%= (List<AssetAddonEntry>)(List<?>)journalContentDisplayContext.getEnabledContentMetadataAssetAddonEntries() %>"
-									hiddenInput="preferences--contentMetadataAssetAddonEntryKeys--"
-									id="contentMetadataAssetAddonEntriesSelector"
-									selectedAssetAddonEntries="<%= (List<AssetAddonEntry>)(List<?>)journalContentDisplayContext.getSelectedContentMetadataAssetAddonEntries() %>"
-									title="select-content-metadata"
-								/>
-							</aui:field-wrapper>
-
-							<aui:input name="preferences--enableViewCountIncrement--" type="toggle-switch" value="<%= journalContentDisplayContext.isEnableViewCountIncrement() %>" />
-						</aui:fieldset>
+					<div id="<portlet:namespace />articlePreview">
+						<liferay-util:include page="/journal_resources.jsp" servletContext="<%= application %>">
+							<liferay-util:param name="refererPortletName" value="<%= renderResponse.getNamespace() %>" />
+						</liferay-util:include>
 					</div>
 				</aui:fieldset>
 			</aui:fieldset-group>
@@ -117,42 +53,14 @@ JournalArticle article = journalContentDisplayContext.getArticle();
 	</aui:button-row>
 </aui:form>
 
-<aui:script sandbox="<%= true %>">
-	var STR_HIDDEN = 'hidden';
+<aui:script sandbox="<%= true %>" use="aui-io-request,aui-parse-content">
+	var form = A.one('#<portlet:namespace />fm');
 
-	var form = AUI.$(document.<portlet:namespace />fm);
+	var articlePreview = A.one('#<portlet:namespace />articlePreview');
 
-	var assetClassPK = '<%= (article != null) ? JournalArticleAssetRenderer.getClassPK(article) : "" %>';
-	var articlePreviewNode = $('.article-preview');
-	var templatePreviewNode = $('.template-preview');
+	articlePreview.plug(A.Plugin.ParseContent);
 
-	var showLoading = function(element) {
-		element.find('.loading-animation').removeClass(STR_HIDDEN);
-	};
-
-	var hideLoading = function(element) {
-		element.find('.loading-animation').addClass(STR_HIDDEN);
-	};
-
-	var showError = function(element) {
-		element.find('.alert').removeClass(STR_HIDDEN);
-	};
-
-	var hideError = function(element) {
-		element.find('.alert').addClass(STR_HIDDEN);
-	};
-
-	var showArticleError = function() {
-		showError(articlePreviewNode);
-	};
-
-	var showTemplateError = function() {
-		templatePreviewNode.removeClass(STR_HIDDEN);
-
-		showError(templatePreviewNode);
-	};
-
-	$('#<portlet:namespace />webContentSelector').on(
+	articlePreview.delegate(
 		'click',
 		function(event) {
 			event.preventDefault();
@@ -182,104 +90,39 @@ JournalArticle article = journalContentDisplayContext.getArticle();
 					eventName: 'selectContent',
 					id: 'selectContent',
 					title: '<liferay-ui:message key="select-web-content" />',
-					uri: baseSelectWebContentURI.replace(encodeURIComponent('[$ARTICLE_REFERER_ASSET_ENTRY_ID$]'), form.fm('assetEntryId').val())
+					uri: baseSelectWebContentURI.replace(encodeURIComponent('[$ARTICLE_REFERER_ASSET_ENTRY_ID$]'), form.attr('<portlet:namespace/>assetEntryId').val())
 				},
 				function(event) {
-					<liferay-portlet:resourceURL portletName="<%= JournalContentPortletKeys.JOURNAL_CONTENT %>" var="journalArticleResource">
-						<portlet:param name="mvcPath" value="/journal_resources.jsp" />
-						<portlet:param name="articleResourcePrimKey" value="[$ARTICLE_RESOURCE_PRIMKEY$]" />
-					</liferay-portlet:resourceURL>
+					form.attr('<portlet:namespace/>assetEntryId').val(event.entityid);
 
-					var baseJournalArticleResourceUrl = '<%= journalArticleResource.toString() %>';
+					articlePreview.html('<div class="loading-animation"></div>');
 
-					form.fm('assetEntryId').val(event.entityid);
-					form.fm('ddmTemplateKey').val('');
-
-					hideError(articlePreviewNode);
-					showLoading(articlePreviewNode);
-
-					templatePreviewNode.addClass(STR_HIDDEN);
-
-					articlePreviewNode.find('.article-preview-content-container').html('');
-					templatePreviewNode.find('.template-preview-content-container').html('');
-
-					articlePreviewNode.find('.web-content-selector').html('<liferay-ui:message key="change" />');
-
-					assetClassPK = event.assetclasspk;
-
-					$.ajax(
-						baseJournalArticleResourceUrl.replace(encodeURIComponent('[$ARTICLE_RESOURCE_PRIMKEY$]'), assetClassPK),
+					var data = Liferay.Util.ns(
+						'<%= PortalUtil.getPortletNamespace(JournalContentPortletKeys.JOURNAL_CONTENT) %>',
 						{
-							error: function() {
-								hideLoading(articlePreviewNode);
-								showArticleError();
-							},
-							success: function(responseData) {
-								var responseDataNode = $(responseData);
+							articleResourcePrimKey: event.assetclasspk
+						}
+					);
 
-								var articlePreviewContent = responseDataNode.find('.article-preview-content');
-								var templatePreviewContent = responseDataNode.find('.template-preview-content');
+					A.io.request(
+						'<liferay-portlet:resourceURL portletName="<%= JournalContentPortletKeys.JOURNAL_CONTENT %>" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>"><portlet:param name="mvcPath" value="/journal_resources.jsp" /><portlet:param name="refererPortletName" value="<%= renderResponse.getNamespace() %>" /></liferay-portlet:resourceURL>',
+						{
+							data: data,
+							on: {
+								failure: function() {
+									articlePreview.html('<div class="alert alert-danger hidden"><liferay-ui:message key="an-unexpected-error-occurred" /></div>');
+								},
+								success: function(event, id, obj) {
+									var responseData = this.get('responseData');
 
-								hideLoading(articlePreviewNode);
-
-								articlePreviewNode.find('.article-preview-content-container').html(articlePreviewContent);
-								templatePreviewNode.find('.template-preview-content-container').html(templatePreviewContent);
-
-								if (articlePreviewContent.length > 0) {
-									$('.configuration-options-container').removeClass(STR_HIDDEN);
-
-									if (templatePreviewContent.length > 0) {
-										var templatePreviewContentNode = templatePreviewNode.find('.template-preview-content');
-
-										templatePreviewNode.removeClass(STR_HIDDEN);
-
-										form.fm('ddmTemplateKey').val(templatePreviewContentNode.attr('data-template-key'));
-									}
-									else {
-										showTemplateError();
-									}
-								}
-								else {
-									showArticleError();
+									articlePreview.setContent(responseData);
 								}
 							}
 						}
 					);
 				}
 			);
-		}
-	);
-
-	Liferay.on(
-		'changeTemplate',
-		function(event) {
-			var ddmTemplateKey = event.ddmTemplateKey;
-
-			<liferay-portlet:resourceURL portletName="<%= JournalContentPortletKeys.JOURNAL_CONTENT %>" var="templateURL" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>">
-				<portlet:param name="mvcPath" value="/journal_template_resources.jsp" />
-				<portlet:param name="articleResourcePrimKey" value="[$ARTICLE_RESOURCE_PRIMKEY$]" />
-				<portlet:param name="ddmTemplateKey" value="[$DDM_TEMPLATE_KEY$]" />
-			</liferay-portlet:resourceURL>
-
-			var templateURL = '<%= templateURL %>';
-
-			templateURL = templateURL.replace(encodeURIComponent('[$ARTICLE_RESOURCE_PRIMKEY$]'), assetClassPK),
-			templateURL = templateURL.replace(encodeURIComponent('[$DDM_TEMPLATE_KEY$]'), ddmTemplateKey),
-
-			templatePreviewNode.find('.template-preview-content-container').html('');
-
-			$.ajax(
-				templateURL,
-				{
-					success: function(responseData) {
-						var responseData = $(responseData);
-
-						templatePreviewNode.find('.template-preview-content-container').html(responseData);
-
-						form.fm('ddmTemplateKey').val(ddmTemplateKey);
-					}
-				}
-			);
-		}
+		},
+		'.web-content-selector'
 	);
 </aui:script>