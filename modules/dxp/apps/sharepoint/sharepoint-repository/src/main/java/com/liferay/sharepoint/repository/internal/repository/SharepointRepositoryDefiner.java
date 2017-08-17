942510617e5ad21b88fc569d60e543a21013d8fd
Adolfo Pérez <adolfo.perez@liferay.com>
2017-08-17 11:33:07 +0200

    LPS-74225 Implement repository definer

---
+++ modules/dxp/apps/sharepoint/sharepoint-repository/src/main/java/com/liferay/sharepoint/repository/internal/repository/SharepointRepositoryDefiner.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharepoint.repository.internal.repository;
+
+import com.liferay.document.library.repository.authorization.capability.AuthorizationCapability;
+import com.liferay.document.library.repository.authorization.oauth2.TokenStore;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.repository.DocumentRepository;
+import com.liferay.portal.kernel.repository.RepositoryConfiguration;
+import com.liferay.portal.kernel.repository.RepositoryConfigurationBuilder;
+import com.liferay.portal.kernel.repository.capabilities.ProcessorCapability;
+import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
+import com.liferay.portal.kernel.repository.registry.RepositoryDefiner;
+import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
+import com.liferay.portal.kernel.repository.registry.RepositoryFactoryRegistry;
+import com.liferay.portal.kernel.util.CacheResourceBundleLoader;
+import com.liferay.portal.kernel.util.ClassResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.repository.capabilities.LiferayProcessorCapability;
+import com.liferay.sharepoint.repository.internal.configuration.SharepointRepositoryConfiguration;
+import com.liferay.sharepoint.repository.internal.document.library.repository.authorization.capability.SharepointRepositoryAuthorizationCapability;
+import com.liferay.sharepoint.repository.internal.document.library.repository.authorization.oauth2.SharepointRepositoryTokenBrokerFactory;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	configurationPid = "com.liferay.sharepoint.repository.internal.configuration.SharepointRepositoryConfiguration",
+	configurationPolicy = ConfigurationPolicy.REQUIRE,
+	service = RepositoryDefiner.class
+)
+public class SharepointRepositoryDefiner implements RepositoryDefiner {
+
+	@Override
+	public String getClassName() {
+		return SharepointExtRepository.class.getName() +
+			_sharepointRepositoryConfiguration.name();
+	}
+
+	@Override
+	public RepositoryConfiguration getRepositoryConfiguration() {
+		RepositoryConfigurationBuilder repositoryConfigurationBuilder =
+			new RepositoryConfigurationBuilder(_resourceBundleLoader);
+
+		repositoryConfigurationBuilder.addParameter("site-absolute-url");
+		repositoryConfigurationBuilder.addParameter("library-path");
+
+		return repositoryConfigurationBuilder.build();
+	}
+
+	@Override
+	public String getRepositoryTypeLabel(Locale locale) {
+		ResourceBundle resourceBundle =
+			_resourceBundleLoader.loadResourceBundle(
+				LanguageUtil.getLanguageId(locale));
+
+		String label = ResourceBundleUtil.getString(
+			resourceBundle, "sharepoint");
+
+		return String.format(
+			"%s (%s)", label, _sharepointRepositoryConfiguration.name());
+	}
+
+	@Override
+	public boolean isExternalRepository() {
+		return true;
+	}
+
+	@Override
+	public void registerCapabilities(
+		CapabilityRegistry<DocumentRepository> capabilityRegistry) {
+
+		capabilityRegistry.addSupportedCapability(
+			ProcessorCapability.class, _processorCapability);
+
+		capabilityRegistry.addExportedCapability(
+			AuthorizationCapability.class,
+			new SharepointRepositoryAuthorizationCapability(
+				_tokenStore, _sharepointRepositoryConfiguration,
+				_sharepointRepositoryTokenBrokerFactory.create(
+					_sharepointRepositoryConfiguration)));
+	}
+
+	@Override
+	public void registerRepositoryEventListeners(
+		RepositoryEventRegistry repositoryEventRegistry) {
+	}
+
+	@Override
+	public void registerRepositoryFactory(
+		RepositoryFactoryRegistry repositoryFactoryRegistry) {
+
+		repositoryFactoryRegistry.setRepositoryFactory(
+			_repositoryFactoryProvider.createForConfiguration(
+				_sharepointRepositoryConfiguration));
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		_sharepointRepositoryConfiguration =
+			ConfigurableUtil.createConfigurable(
+				SharepointRepositoryConfiguration.class, properties);
+	}
+
+	private final LiferayProcessorCapability _processorCapability =
+		new LiferayProcessorCapability(
+			LiferayProcessorCapability.ResourceGenerationStrategy.
+				ALWAYS_GENERATE);
+
+	@Reference
+	private SharepointRepositoryFactoryProvider _repositoryFactoryProvider;
+
+	private final ResourceBundleLoader _resourceBundleLoader =
+		new CacheResourceBundleLoader(
+			new ClassResourceBundleLoader(
+				"content.Language", SharepointRepositoryDefiner.class));
+	private SharepointRepositoryConfiguration
+		_sharepointRepositoryConfiguration;
+
+	@Reference
+	private SharepointRepositoryTokenBrokerFactory
+		_sharepointRepositoryTokenBrokerFactory;
+
+	@Reference
+	private TokenStore _tokenStore;
+
+}
