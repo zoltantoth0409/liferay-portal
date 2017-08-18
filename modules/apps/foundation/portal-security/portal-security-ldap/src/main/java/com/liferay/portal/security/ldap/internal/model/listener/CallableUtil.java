8ab1bd44c69a4ccb26ab3e97bb40bb84f78cb07d
Michael Han <michael.han@liferay.com>
2017-08-18 12:25:20 -0500

    LPS-74221 replace duplicate code

--- modules/apps/foundation/portal-security/portal-security-ldap/src/main/java/com/liferay/portal/security/ldap/internal/model/listener/CallableUtil.java
+++ modules/apps/foundation/portal-security/portal-security-ldap/src/main/java/com/liferay/portal/security/ldap/internal/model/listener/CallableUtil.java
@@ -1,8 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.portal.security.ldap.internal.model.listener;
+
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+
+import java.io.Serializable;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
 
 /**
  * @author Michael C. Han
  */
-public class CallableFactory {
+public class CallableUtil {
+
+	public static Callable<Void> getCallable(
+		Consumer<Map<String, Serializable>> consumer) {
+
+		return () -> {
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			Map<String, Serializable> expandoBridgeAttributes = null;
+
+			if (serviceContext != null) {
+				expandoBridgeAttributes =
+					serviceContext.getExpandoBridgeAttributes();
+			}
+
+			consumer.accept(expandoBridgeAttributes);
+
+			return null;
+		};
+	}
 
 }
