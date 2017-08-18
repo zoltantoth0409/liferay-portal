1864c1d6f08cce98202a5b9b2d44b3ed0f1d76fe
Andrea Di Giorgi <adigiorgi@outlook.com>
2017-08-18 14:51:02 -0700

    COMMERCE-163 Add order local service methods

--- modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderLocalServiceImpl.java
@@ -14,11 +14,114 @@
 
 package com.liferay.commerce.service.impl;
 
+import com.liferay.commerce.model.CommerceCart;
+import com.liferay.commerce.model.CommerceCartItem;
+import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.service.base.CommerceOrderLocalServiceBaseImpl;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.util.List;
 
 /**
- * @author Alessio Antonio Rendina
+ * @author Andrea Di Giorgi
  */
 public class CommerceOrderLocalServiceImpl
 	extends CommerceOrderLocalServiceBaseImpl {
+
+	@Override
+	public CommerceOrder addCommerceOrder(
+			long orderUserId, int status, ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		long commerceOrderId = counterLocalService.increment();
+
+		CommerceOrder commerceOrder = commerceOrderPersistence.create(
+			commerceOrderId);
+
+		commerceOrder.setGroupId(groupId);
+		commerceOrder.setCompanyId(user.getCompanyId());
+		commerceOrder.setUserId(user.getUserId());
+		commerceOrder.setUserName(user.getFullName());
+		commerceOrder.setOrderUserId(orderUserId);
+		commerceOrder.setStatus(status);
+		commerceOrder.setExpandoBridgeAttributes(serviceContext);
+
+		commerceOrderPersistence.update(commerceOrder);
+
+		return commerceOrder;
+	}
+
+	@Override
+	public CommerceOrder addCommerceOrderFromCart(
+			long commerceCartId, ServiceContext serviceContext)
+		throws PortalException {
+
+		// Commerce order
+
+		CommerceCart commerceCart = commerceCartLocalService.getCommerceCart(
+			commerceCartId);
+
+		CommerceOrder commerceOrder =
+			commerceOrderLocalService.addCommerceOrder(
+				commerceCart.getUserId(), 0, serviceContext);
+
+		// Commerce order items
+
+		List<CommerceCartItem> commerceCartItems =
+			commerceCartItemLocalService.getCommerceCartItems(
+				commerceCart.getCommerceCartId(), QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+
+		for (CommerceCartItem commerceCartItem : commerceCartItems) {
+			commerceOrderItemLocalService.addCommerceOrderItem(
+				commerceOrder.getCommerceOrderId(),
+				commerceCartItem.getCPDefinitionId(),
+				commerceCartItem.getCPInstanceId(),
+				commerceCartItem.getQuantity(), commerceCartItem.getJson(),
+				serviceContext);
+		}
+
+		// Commerce cart
+
+		commerceCartLocalService.deleteCommerceCart(commerceCart);
+
+		return commerceOrder;
+	}
+
+	@Override
+	public CommerceOrder deleteCommerceOrder(CommerceOrder commerceOrder)
+		throws PortalException {
+
+		// Commerce order
+
+		commerceOrderPersistence.remove(commerceOrder);
+
+		// Commerce order items
+
+		commerceOrderItemLocalService.deleteCommerceOrderItems(
+			commerceOrder.getCommerceOrderId());
+
+		// Expando
+
+		expandoRowLocalService.deleteRows(commerceOrder.getCommerceOrderId());
+
+		return commerceOrder;
+	}
+
+	@Override
+	public CommerceOrder deleteCommerceOrder(long commerceOrderId)
+		throws PortalException {
+
+		CommerceOrder commerceOrder = commerceOrderPersistence.findByPrimaryKey(
+			commerceOrderId);
+
+		return commerceOrderLocalService.deleteCommerceOrder(commerceOrder);
+	}
+
 }
