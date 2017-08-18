9949704c7018a5e89005af2696500e28dcd41b5b
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-08-18 10:28:15 +0200

    LPS-73859 Service tests for Fragment Collection and Fragment Entry

---
+++ modules/apps/web-experience/modern-site-building/modern-site-building-fragment-test/src/testIntegration/java/com/liferay/modern/site/building/fragment/service/test/FragmentCollectionServiceTest.java
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.modern.site.building.fragment.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.modern.site.building.fragment.exception.DuplicateFragmentCollectionException;
+import com.liferay.modern.site.building.fragment.exception.FragmentCollectionNameException;
+import com.liferay.modern.site.building.fragment.model.FragmentCollection;
+import com.liferay.modern.site.building.fragment.service.FragmentCollectionServiceUtil;
+import com.liferay.modern.site.building.fragment.service.FragmentEntryServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jürgen Kappler
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class FragmentCollectionServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test(expected = DuplicateFragmentCollectionException.class)
+	public void testAddDuplicateFragmentCollections() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollectionServiceUtil.addFragmentCollection(
+			_group.getGroupId(), "Fragment Collection", null, serviceContext);
+
+		FragmentCollectionServiceUtil.addFragmentCollection(
+			_group.getGroupId(), "Fragment Collection", null, serviceContext);
+	}
+
+	@Test
+	public void testAddFragmentCollection() throws PortalException {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		Assert.assertEquals(
+			"Fragment Collection", fragmentCollection.getName());
+	}
+
+	@Test(expected = FragmentCollectionNameException.class)
+	public void testAddFragmentCollectionWithEmptyName() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollectionServiceUtil.addFragmentCollection(
+			_group.getGroupId(), StringPool.BLANK, StringPool.BLANK,
+			serviceContext);
+	}
+
+	@Test(expected = FragmentCollectionNameException.class)
+	public void testAddFragmentCollectionWithNullName() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollectionServiceUtil.addFragmentCollection(
+			_group.getGroupId(), null, StringPool.BLANK, serviceContext);
+	}
+
+	@Test
+	public void testAddMultipleFragmentCollections() throws PortalException {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		int originalFragmentCollectionsCount =
+			FragmentCollectionServiceUtil.getGroupFragmentCollectionsCount(
+				_group.getGroupId());
+
+		FragmentCollectionServiceUtil.addFragmentCollection(
+			_group.getGroupId(), "Fragment Collection 1", StringPool.BLANK,
+			serviceContext);
+
+		FragmentCollectionServiceUtil.addFragmentCollection(
+			_group.getGroupId(), "Fragment Collection 2", StringPool.BLANK,
+			serviceContext);
+
+		int actualFragmentCollectionsCount =
+			FragmentCollectionServiceUtil.getGroupFragmentCollectionsCount(
+				_group.getGroupId());
+
+		Assert.assertEquals(
+			originalFragmentCollectionsCount + 2,
+			actualFragmentCollectionsCount);
+	}
+
+	@Test
+	public void testDeleteFragmentCollection() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		FragmentCollectionServiceUtil.deleteFragmentCollection(
+			fragmentCollection.getFragmentCollectionId());
+
+		Assert.assertNull(
+			FragmentCollectionServiceUtil.fetchFragmentCollection(
+				fragmentCollection.getFragmentCollectionId()));
+	}
+
+	@Test
+	public void testDeleteFragmentCollectionWithFragmentEntries()
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			"Fragment Entry", StringPool.BLANK, StringPool.BLANK,
+			StringPool.BLANK, serviceContext);
+
+		FragmentCollectionServiceUtil.deleteFragmentCollection(
+			fragmentCollection.getFragmentCollectionId());
+
+		Assert.assertNull(
+			FragmentCollectionServiceUtil.fetchFragmentCollection(
+				fragmentCollection.getFragmentCollectionId()));
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
---
+++ modules/apps/web-experience/modern-site-building/modern-site-building-fragment-test/src/testIntegration/java/com/liferay/modern/site/building/fragment/service/test/FragmentEntryServiceTest.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.modern.site.building.fragment.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.modern.site.building.fragment.exception.DuplicateFragmentEntryException;
+import com.liferay.modern.site.building.fragment.exception.FragmentEntryNameException;
+import com.liferay.modern.site.building.fragment.model.FragmentCollection;
+import com.liferay.modern.site.building.fragment.model.FragmentEntry;
+import com.liferay.modern.site.building.fragment.service.FragmentCollectionServiceUtil;
+import com.liferay.modern.site.building.fragment.service.FragmentEntryServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jürgen Kappler
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class FragmentEntryServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test(expected = DuplicateFragmentEntryException.class)
+	public void testAddDuplicateFragmentEntries() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			"Fragment Entry", null, null, null, serviceContext);
+
+		FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			"Fragment Entry", null, null, null, serviceContext);
+	}
+
+	@Test
+	public void testAddFragmentEntry() throws PortalException {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		FragmentEntry fragmentEntry = FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			"Fragment Entry", null, null, null, serviceContext);
+
+		Assert.assertEquals("Fragment Entry", fragmentEntry.getName());
+	}
+
+	@Test(expected = FragmentEntryNameException.class)
+	public void testAddFragmentEntryWithEmptyName() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			StringPool.BLANK, null, null, null, serviceContext);
+	}
+
+	@Test(expected = FragmentEntryNameException.class)
+	public void testAddFragmentEntryWithNullName() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			null, null, null, null, serviceContext);
+	}
+
+	@Test
+	public void testAddMultipleFragmentEntries() throws PortalException {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		List<FragmentEntry> originalFragmentEntries =
+			FragmentEntryServiceUtil.fetchFragmentEntries(
+				fragmentCollection.getFragmentCollectionId());
+
+		FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			"Fragment Entry 1", null, null, null, serviceContext);
+
+		FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			"Fragment Entry 2", null, null, null, serviceContext);
+
+		List<FragmentEntry> actualFragmentEntries =
+			FragmentEntryServiceUtil.fetchFragmentEntries(
+				fragmentCollection.getFragmentCollectionId());
+
+		Assert.assertEquals(
+			actualFragmentEntries.toString(),
+			originalFragmentEntries.size() + 2, actualFragmentEntries.size());
+	}
+
+	@Test
+	public void testDeleteFragmentEntry() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionServiceUtil.addFragmentCollection(
+				_group.getGroupId(), "Fragment Collection", StringPool.BLANK,
+				serviceContext);
+
+		FragmentEntry fragmentEntry = FragmentEntryServiceUtil.addFragmentEntry(
+			_group.getGroupId(), fragmentCollection.getFragmentCollectionId(),
+			"Fragment Entry", null, null, null, serviceContext);
+
+		FragmentEntryServiceUtil.deleteFragmentEntry(
+			fragmentEntry.getFragmentEntryId());
+
+		Assert.assertNull(
+			FragmentEntryServiceUtil.fetchFragmentEntry(
+				fragmentEntry.getFragmentEntryId()));
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
