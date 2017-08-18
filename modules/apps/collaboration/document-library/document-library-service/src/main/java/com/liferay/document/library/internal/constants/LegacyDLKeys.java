6a4cb93bdbcb7785eedb7d4759bc8ef80da857cd
drewbrokke <drew.brokke@liferay.com>
2017-08-18 12:04:52 -0700

    LPS-69208 document-library-service - adds LegacyDLKeys.java

---
+++ modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/constants/LegacyDLKeys.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.constants;
+
+/**
+ * @author Drew Brokke
+ */
+public class LegacyDLKeys {
+
+	public static final String DL_FILE_ENTRY_PREVIEWABLE_PROCESSOR_MAX_SIZE =
+		"dl.file.entry.previewable.processor.max.size";
+
+	public static final String DL_FILE_EXTENSIONS = "dl.file.extensions";
+
+	public static final String DL_FILE_MAX_SIZE = "dl.file.max.size";
+
+}
