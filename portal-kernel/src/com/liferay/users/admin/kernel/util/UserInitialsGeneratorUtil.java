6f8374fe5f1171fbf3bc68c601ff2e9023c2f017
drewbrokke <drew.brokke@liferay.com>
2017-08-21 10:34:54 -0700

    LPS-73725 portal-kernel - creates UserInitialsGeneratorUtil.java to wrap calls to the UserInitialsGenerator api

---
+++ portal-kernel/src/com/liferay/users/admin/kernel/util/UserInitialsGeneratorUtil.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.kernel.util;
+
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.util.ServiceProxyFactory;
+
+import java.util.Locale;
+
+/**
+ * @author Drew Brokke
+ */
+public class UserInitialsGeneratorUtil {
+
+	public static String getInitials(
+		Locale locale, String firstName, String middleName, String lastName) {
+
+		return getUserInitialsGenerator().getInitials(
+			locale, firstName, middleName, lastName);
+	}
+
+	public static String getInitials(User user) {
+		return getUserInitialsGenerator().getInitials(user);
+	}
+
+	public static UserInitialsGenerator getUserInitialsGenerator() {
+		return _userInitialsGenerator;
+	}
+
+	private static volatile UserInitialsGenerator _userInitialsGenerator =
+		ServiceProxyFactory.newServiceTrackedInstance(
+			UserInitialsGenerator.class, UserInitialsGeneratorUtil.class,
+			"_userInitialsGenerator", false);
+
+}
