49de34f0e23318a7a2ee891a5cb15fdd022ab83c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-08-22 20:04:30 +0200

    LPS-74303 Deprecate old class

--- portal-impl/src/com/liferay/portlet/asset/util/AssetUtil.java
+++ portal-impl/src/com/liferay/portlet/asset/util/AssetUtil.java
@@ -100,7 +100,10 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * @author Brian Wing Shun Chan
  * @author Jorge Ferrer
+ * @deprecated As of 7.0.0, replaced by {@link
+ *             com.liferay.asset.util.impl.AssetUtil}
  */
+@Deprecated
 public class AssetUtil {
 
 	public static final int ASSET_ENTRY_ABSTRACT_LENGTH = 200;
