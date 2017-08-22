140ef6d87e38353b13219824aca493dcb0af9601
Andrea Di Giorgi <adigiorgi@outlook.com>
2017-08-22 07:14:55 -0700

    COMMERCE-184 Money order payment engine

---
+++ modules/dxp/apps/commerce/commerce-payment-money-order/src/main/java/com/liferay/commerce/payment/money/order/internal/MoneyOrderCommercePaymentEngine.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.payment.money.order.internal;
+
+import com.liferay.commerce.model.CommercePaymentEngine;
+import com.liferay.commerce.payment.money.order.internal.configuration.MoneyOrderCommercePaymentEngineGroupServiceConfiguration;
+import com.liferay.commerce.payment.money.order.internal.constants.MoneyOrderCommercePaymentEngineConstants;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.ModifiableSettings;
+import com.liferay.portal.kernel.settings.ParameterMapSettingsLocator;
+import com.liferay.portal.kernel.settings.Settings;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+@Component(
+	immediate = true, property = "commerce.payment.engine.key=money-order",
+	service = CommercePaymentEngine.class
+)
+public class MoneyOrderCommercePaymentEngine implements CommercePaymentEngine {
+
+	@Override
+	public String getDescription(Locale locale) {
+		ResourceBundle resourceBundle = _getResourceBundle(locale);
+
+		return LanguageUtil.get(resourceBundle, "money-order-description");
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		ResourceBundle resourceBundle = _getResourceBundle(locale);
+
+		return LanguageUtil.get(resourceBundle, "money-order");
+	}
+
+	@Override
+	public void renderConfiguration(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		MoneyOrderCommercePaymentEngineGroupServiceConfiguration
+			moneyOrderCommercePaymentEngineGroupServiceConfiguration =
+				_configurationProvider.getConfiguration(
+					MoneyOrderCommercePaymentEngineGroupServiceConfiguration.
+						class,
+					new ParameterMapSettingsLocator(
+						renderRequest.getParameterMap(),
+						new GroupServiceSettingsLocator(
+							themeDisplay.getScopeGroupId(),
+							MoneyOrderCommercePaymentEngineConstants.
+								SERVICE_NAME)));
+
+		renderRequest.setAttribute(
+			MoneyOrderCommercePaymentEngineGroupServiceConfiguration.class.
+				getName(),
+			moneyOrderCommercePaymentEngineGroupServiceConfiguration);
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			renderRequest);
+		HttpServletResponse httpServletResponse =
+			_portal.getHttpServletResponse(renderResponse);
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/configuration.jsp");
+	}
+
+	@Override
+	public void updateConfiguration(
+			Map<String, String> parameterMap, ServiceContext serviceContext)
+		throws Exception {
+
+		Settings settings = _settingsFactory.getSettings(
+			new GroupServiceSettingsLocator(
+				serviceContext.getScopeGroupId(),
+				MoneyOrderCommercePaymentEngineConstants.SERVICE_NAME));
+
+		ModifiableSettings modifiableSettings =
+			settings.getModifiableSettings();
+
+		for (Map.Entry<String, String> entry : parameterMap.entrySet()) {
+			modifiableSettings.setValue(entry.getKey(), entry.getValue());
+		}
+
+		modifiableSettings.store();
+	}
+
+	private ResourceBundle _getResourceBundle(Locale locale) {
+		return ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+	}
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.payment.money.order)"
+	)
+	private ServletContext _servletContext;
+
+	@Reference
+	private SettingsFactory _settingsFactory;
+
+}
