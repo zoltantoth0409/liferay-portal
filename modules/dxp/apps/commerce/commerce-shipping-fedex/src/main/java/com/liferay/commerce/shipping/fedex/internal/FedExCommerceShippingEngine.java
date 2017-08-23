5cb3f791a311c40d21428623d1c38da5bee70395
Andrea Di Giorgi <adigiorgi@outlook.com>
2017-08-23 10:33:31 -0700

    COMMERCE-191 FedEx shipping engine

---
+++ modules/dxp/apps/commerce/commerce-shipping-fedex/src/main/java/com/liferay/commerce/shipping/fedex/internal/FedExCommerceShippingEngine.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.shipping.fedex.internal;
+
+import com.liferay.commerce.model.CommerceShippingEngine;
+import com.liferay.commerce.shipping.fedex.internal.configuration.FedExCommerceShippingEngineGroupServiceConfiguration;
+import com.liferay.commerce.shipping.fedex.internal.constants.FedExCommerceShippingEngineConstants;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.ModifiableSettings;
+import com.liferay.portal.kernel.settings.ParameterMapSettingsLocator;
+import com.liferay.portal.kernel.settings.Settings;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+@Component(
+	immediate = true, property = "commerce.shipping.engine.key=fedex",
+	service = CommerceShippingEngine.class
+)
+public class FedExCommerceShippingEngine implements CommerceShippingEngine {
+
+	@Override
+	public String getDescription(Locale locale) {
+		ResourceBundle resourceBundle = _getResourceBundle(locale);
+
+		return LanguageUtil.get(resourceBundle, "fedex-description");
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		ResourceBundle resourceBundle = _getResourceBundle(locale);
+
+		return LanguageUtil.get(resourceBundle, "fedex");
+	}
+
+	@Override
+	public void renderConfiguration(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		FedExCommerceShippingEngineGroupServiceConfiguration
+			fedExCommerceShippingEngineGroupServiceConfiguration =
+				_configurationProvider.getConfiguration(
+					FedExCommerceShippingEngineGroupServiceConfiguration.class,
+					new ParameterMapSettingsLocator(
+						renderRequest.getParameterMap(),
+						new GroupServiceSettingsLocator(
+							themeDisplay.getScopeGroupId(),
+							FedExCommerceShippingEngineConstants.
+								SERVICE_NAME)));
+
+		renderRequest.setAttribute(
+			FedExCommerceShippingEngineGroupServiceConfiguration.class.
+				getName(),
+			fedExCommerceShippingEngineGroupServiceConfiguration);
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			renderRequest);
+		HttpServletResponse httpServletResponse =
+			_portal.getHttpServletResponse(renderResponse);
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/configuration.jsp");
+	}
+
+	@Override
+	public void updateConfiguration(
+			Map<String, String> parameterMap, ServiceContext serviceContext)
+		throws Exception {
+
+		Settings settings = _settingsFactory.getSettings(
+			new GroupServiceSettingsLocator(
+				serviceContext.getScopeGroupId(),
+				FedExCommerceShippingEngineConstants.SERVICE_NAME));
+
+		ModifiableSettings modifiableSettings =
+			settings.getModifiableSettings();
+
+		for (Map.Entry<String, String> entry : parameterMap.entrySet()) {
+			modifiableSettings.setValue(entry.getKey(), entry.getValue());
+		}
+
+		modifiableSettings.store();
+	}
+
+	private ResourceBundle _getResourceBundle(Locale locale) {
+		return ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+	}
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.shipping.fedex)"
+	)
+	private ServletContext _servletContext;
+
+	@Reference
+	private SettingsFactory _settingsFactory;
+
+}
