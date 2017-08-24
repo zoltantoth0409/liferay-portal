c7d7b9a118bbbeafd9b41c45d67c5afd93e9da85
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-08-24 13:35:20 -0300

    LPS-74058 Form can be saved when required field configurations are left empty. Create a class to validate the Field's settings of a Form.

---
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/RecordSetFieldSettingsException.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.form.web;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class RecordSetFieldSettingsException extends PortalException {
+
+	public RecordSetFieldSettingsException() {
+	}
+
+	public RecordSetFieldSettingsException(String msg) {
+		super(msg);
+	}
+
+	public RecordSetFieldSettingsException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public RecordSetFieldSettingsException(Throwable cause) {
+		super(cause);
+	}
+
+	public static class MustSetValidValueForProperties
+		extends RecordSetFieldSettingsException {
+
+		public MustSetValidValueForProperties(
+			Map<String, Set<String>> fieldNamePropertiesMap) {
+
+			super(
+				String.format(
+					"Invalid value set for the properties of fields %s",
+					fieldNamePropertiesMap.keySet()));
+
+			_fieldNamePropertiesMap = fieldNamePropertiesMap;
+		}
+
+		public Map<String, Set<String>> getFieldNamePropertiesMap() {
+			return _fieldNamePropertiesMap;
+		}
+
+		private final Map<String, Set<String>> _fieldNamePropertiesMap;
+
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
