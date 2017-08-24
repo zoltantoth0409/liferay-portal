5c4aa9c1e3da35c97847f33420cbd84a867722ca
Pablo Molina <pablo.molina@liferay.com>
2017-08-24 11:58:54 +0200

    LPS-74408 Added AceEditor js component

---
+++ modules/apps/web-experience/modern-site-building/modern-site-building-fragment-web/src/main/resources/META-INF/resources/js/AceEditor.es.js
@@ -0,0 +1,83 @@
+/* global AUI */
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import {Config} from 'metal-state';
+
+import templates from './AceEditor.soy';
+
+/**
+ * Component that creates an instance of Ace editor
+ * to allow code editing.
+ */
+class AceEditor extends Component {
+	/** @inheritdoc */
+	attached() {
+		// this._editor = null;
+		this._editorDocument = null;
+		this._handleDocumentChanged = this._handleDocumentChanged.bind(this);
+
+		AUI().use('aui-ace-editor', (A) => { // eslint-disable-line new-cap
+			const editor = new A.AceEditor({
+				boundingBox: this.refs.wrapper,
+				mode: this.syntax,
+				tabSize: 2,
+				highlightActiveLine: false,
+			});
+
+			this._editorDocument = editor.getSession().getDocument();
+
+			this.refs.wrapper.style.height = '';
+			this.refs.wrapper.style.width = '';
+
+			this._editorDocument.on('change', this._handleDocumentChanged);
+
+			if (this.initialContent) {
+				this._editorDocument.setValue(this.initialContent);
+			}
+		});
+	}
+
+	/**
+	 * Return false so the ace editor does not break
+	 * @inheritdoc
+	 * @return {boolean}
+	 */
+	shouldUpdate() {
+		return false;
+	}
+
+	/**
+	 * Callback executed when the internal Ace editor has been
+	 * modified. It simply propagates the event.
+	 */
+	_handleDocumentChanged() {
+		this.emit('contentChanged', {
+			content: this._editorDocument.getValue(),
+		});
+	}
+}
+
+AceEditor.STATE = {
+	/**
+	 * Initial content sent to the editor
+	 * @default ''
+	 * @instance
+	 * @memberOf AceEditor
+	 * @type {?string}
+	 */
+	initialContent: Config.string().value(''),
+
+	/**
+	 * Syntax used for the editor.
+	 * It will be used for Ace and rendered on the interface.
+	 * @instance
+	 * @memberOf AceEditor
+	 * @type {!string}
+	 */
+	syntax: Config.oneOf(['html', 'css', 'javascript']).required(),
+};
+
+Soy.register(AceEditor, templates);
+
+export default AceEditor;
