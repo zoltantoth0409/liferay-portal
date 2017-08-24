b47de06d63e199c06277031a84149f3459048207
dsanz <daniel.sanz@liferay.com>
2017-08-24 13:29:02 +0200

    LPS-74064 Create control panel entry for license manager. Make sure we inherit from OmniAdminControlPanelEntry

---
+++ modules/apps/foundation/license-manager/license-manager-web/src/main/java/com/liferay/license/manager/web/internal/portlet/LicenseManagerControlPanelEntry.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.license.manager.web.internal.portlet;
+
+import com.liferay.license.manager.web.internal.constants.LicenseManagerPortletKeys;
+import com.liferay.portal.kernel.portlet.ControlPanelEntry;
+import com.liferay.portal.kernel.portlet.OmniadminControlPanelEntry;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Daniel Sanz
+ */
+@Component(
+		immediate = true,
+		property = {"javax.portlet.name=" +
+					LicenseManagerPortletKeys.LICENSE_MANAGER},
+		service = ControlPanelEntry.class
+	)
+public class LicenseManagerControlPanelEntry extends
+	OmniadminControlPanelEntry {}
