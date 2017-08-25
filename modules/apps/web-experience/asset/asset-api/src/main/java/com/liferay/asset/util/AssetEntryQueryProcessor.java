f6463ad84b8272acc299eff25b58efbb8f736e76
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-08-25 12:36:23 +0200

    LPS-74417 Copy AssetEntryQueryProcessor class to api module

---
+++ modules/apps/web-experience/asset/asset-api/src/main/java/com/liferay/asset/util/AssetEntryQueryProcessor.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.util;
+
+import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
+import com.liferay.portal.kernel.model.User;
+
+import javax.portlet.PortletPreferences;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Locale;
+
+/**
+ * @author Jorge Ferrer
+ */
+public interface AssetEntryQueryProcessor {
+
+	/**
+	 * @deprecated As of 7.0.0
+	 */
+	@Deprecated
+	public String getKey();
+
+	/**
+	 * @deprecated As of 7.0.0
+	 */
+	@Deprecated
+	public String getTitle(Locale locale);
+
+	/**
+	 * @deprecated As of 7.0.0
+	 */
+	@Deprecated
+	public void include(
+		HttpServletRequest request, HttpServletResponse response)
+		throws IOException;
+
+	public void processAssetEntryQuery(
+		User user, PortletPreferences portletPreferences,
+		AssetEntryQuery assetEntryQuery)
+		throws Exception;
+
+}
