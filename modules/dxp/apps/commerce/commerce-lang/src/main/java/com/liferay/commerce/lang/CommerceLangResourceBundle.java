1ea40161759126a1074835b307f509a71e2f9c5e
Alessio Rendina <asilcomando@gmail.com>
2017-08-29 17:44:00 +0200

    COMMERCE-188 Added commerce lang resource bundle

---
+++ modules/dxp/apps/commerce/commerce-lang/src/main/java/com/liferay/commerce/lang/CommerceLangResourceBundle.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.lang;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	property = {"language.id=" + StringPool.BLANK},
+	service = ResourceBundle.class
+)
+public class CommerceLangResourceBundle extends ResourceBundle {
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return _resourceBundle.getKeys();
+	}
+
+	@Override
+	protected Object handleGetObject(String key) {
+		return _resourceBundle.getObject(key);
+	}
+
+	private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
+		"content.Language", UTF8Control.INSTANCE);
+
+}
