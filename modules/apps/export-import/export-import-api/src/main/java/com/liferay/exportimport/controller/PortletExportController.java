d249eb99cab6031db9d9bcf85737c788d27b036d
akosthurzo <akos.thurzo@liferay.com>
2017-08-29 13:40:27 +0200

    LPS-72545 Remove custom logic to export expando metadata (and keep support legacy import)

--- modules/apps/export-import/export-import-api/src/main/java/com/liferay/exportimport/controller/PortletExportController.java
+++ modules/apps/export-import/export-import-api/src/main/java/com/liferay/exportimport/controller/PortletExportController.java
@@ -26,9 +26,6 @@ public interface PortletExportController extends ExportController {
 	public void exportAssetLinks(PortletDataContext portletDataContext)
 		throws Exception;
 
-	public void exportExpandoTables(PortletDataContext portletDataContext)
-		throws Exception;
-
 	public void exportLocks(PortletDataContext portletDataContext)
 		throws Exception;
 
