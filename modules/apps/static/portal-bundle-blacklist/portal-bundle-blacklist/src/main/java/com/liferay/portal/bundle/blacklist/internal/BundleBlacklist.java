1adc3f047021d2a63570c2a9bcd055f217fbf30a
shuyangzhou <shuyang.zhou@liferay.com>
2017-08-29 07:46:50 -0700

    Revert "LPS-73562 This is a work around for a fileinstall bug. If you delete the cfg while FileInstall is not running (for example, delete the cfg when portal is off), next start of FileInstall will not uninstall the previously installed Configuration. We should add a protection clean up logic at FileInstall layer later as it is a general issue, but for now to keep things simple, I am only doing this for blacklist, as blacklist must interpret cfg deletion as a full reset."
    This reverts commit d6991cc52b66f8ac24d2f0002237f37910b8fb97.

--- modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist/src/main/java/com/liferay/portal/bundle/blacklist/internal/BundleBlacklist.java
+++ modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist/src/main/java/com/liferay/portal/bundle/blacklist/internal/BundleBlacklist.java
@@ -26,12 +26,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -90,30 +86,6 @@ public class BundleBlacklist {
 		systemBundleContext.addBundleListener(_selfMonitorBundleListener);
 
 		_loadFromBlacklistFile();
-
-		String cfgFilePath = properties.get("felix.fileinstall.filename");
-
-		if (cfgFilePath != null) {
-			try {
-				File cfgFile = new File(new URI(cfgFilePath));
-
-				if (!cfgFile.exists()) {
-					properties = Collections.emptyMap();
-
-					if (_log.isInfoEnabled()) {
-						_log.info(
-							"Reset blacklist to empty because configuration " +
-								"file " + cfgFilePath + " was removed");
-					}
-				}
-			}
-			catch (URISyntaxException urise) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Unable to recreate configuration file URI", urise);
-				}
-			}
-		}
 
 		BundleBlacklistConfiguration bundleBlacklistConfiguration =
 			ConfigurableUtil.createConfigurable(
