1751c79f6e5e732343b537f8158ba8191851c72f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-08-29 10:23:17 +0200

    LPS-74409 It should be part of configuration, like the application display templates

--- modules/apps/web-experience/asset/asset-display-template-web/src/main/java/com/liferay/asset/display/template/web/internal/application/list/AssetDisplayTemplatePanelApp.java
+++ modules/apps/web-experience/asset/asset-display-template-web/src/main/java/com/liferay/asset/display/template/web/internal/application/list/AssetDisplayTemplatePanelApp.java
@@ -29,8 +29,8 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.app.order:Integer=700",
-		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONTENT
+		"panel.app.order:Integer=600",
+		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONFIGURATION
 	},
 	service = PanelApp.class
 )
