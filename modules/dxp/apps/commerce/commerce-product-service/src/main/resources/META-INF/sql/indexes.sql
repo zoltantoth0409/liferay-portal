433e50821fe6323d33d24580ea49a265ff418188
Alessio Rendina <asilcomando@gmail.com>
2017-08-29 12:07:49 +0200

    COMMERCE-185 Removed unique index

--- modules/dxp/apps/commerce/commerce-product-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/resources/META-INF/sql/indexes.sql
@@ -45,7 +45,6 @@ create index IX_393BFCD8 on CPFriendlyURLEntry (groupId, companyId, classNameId,
 create index IX_C13D69E1 on CPFriendlyURLEntry (groupId, companyId, classNameId, classPK, main);
 create index IX_3E26073 on CPFriendlyURLEntry (groupId, companyId, classNameId, languageId[$COLUMN_LENGTH:75$], urlTitle[$COLUMN_LENGTH:255$]);
 create index IX_C7AAAB6C on CPFriendlyURLEntry (groupId, companyId, classNameId, urlTitle[$COLUMN_LENGTH:255$]);
-create unique index IX_4BAE95CD on CPFriendlyURLEntry (groupId, languageId[$COLUMN_LENGTH:75$], urlTitle[$COLUMN_LENGTH:255$]);
 create index IX_BD972D55 on CPFriendlyURLEntry (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_F4450517 on CPFriendlyURLEntry (uuid_[$COLUMN_LENGTH:75$], groupId);
 
