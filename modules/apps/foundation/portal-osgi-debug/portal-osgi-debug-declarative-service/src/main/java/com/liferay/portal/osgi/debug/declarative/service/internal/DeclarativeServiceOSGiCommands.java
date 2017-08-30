2255a8baed065d15aa0f69ee4a361c31d0495246
tina.tian <tina.tian@liferay.com>
2017-08-30 14:11:41 -0700

    LPS-74276 Add gogo shell commands to list unsatisfied declarative services.

---
+++ modules/apps/foundation/portal-osgi-debug/portal-osgi-debug-declarative-service/src/main/java/com/liferay/portal/osgi/debug/declarative/service/internal/DeclarativeServiceOSGiCommands.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.osgi.debug.declarative.service.internal;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.runtime.ServiceComponentRuntime;
+
+/**
+ * @author Tina Tian
+ */
+@Component(
+	immediate = true,
+	property = {"osgi.command.function=unsatisfied", "osgi.command.scope=ds"},
+	service = DeclarativeServiceOSGiCommands.class
+)
+public class DeclarativeServiceOSGiCommands {
+
+	public void unsatisfied() {
+		System.out.println(
+			DeclarativeServiceUtil.listUnsatisfiedDeclarativeServices(
+				_serviceComponentRuntime, _bundleContext.getBundles()));
+	}
+
+	public void unsatisfied(long bundleId) {
+		System.out.println(
+			DeclarativeServiceUtil.listUnsatisfiedDeclarativeServices(
+				_serviceComponentRuntime, _bundleContext.getBundle(bundleId)));
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+	}
+
+	private BundleContext _bundleContext;
+
+	@Reference
+	private ServiceComponentRuntime _serviceComponentRuntime;
+
+}
