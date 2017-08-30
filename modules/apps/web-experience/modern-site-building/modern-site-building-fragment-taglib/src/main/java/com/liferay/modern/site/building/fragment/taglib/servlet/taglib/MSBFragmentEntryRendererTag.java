36b7e3faac1ea04131db2691eb2f72076859d6ee
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-08-30 12:47:56 +0200

    LPS-74416 Override methods in renderer tag, and add attributes to be read in the view

--- modules/apps/web-experience/modern-site-building/modern-site-building-fragment-taglib/src/main/java/com/liferay/modern/site/building/fragment/taglib/servlet/taglib/MSBFragmentEntryRendererTag.java
+++ modules/apps/web-experience/modern-site-building/modern-site-building-fragment-taglib/src/main/java/com/liferay/modern/site/building/fragment/taglib/servlet/taglib/MSBFragmentEntryRendererTag.java
@@ -14,10 +14,77 @@
 
 package com.liferay.modern.site.building.fragment.taglib.servlet.taglib;
 
+import com.liferay.modern.site.building.fragment.model.MSBFragmentEntry;
+import com.liferay.modern.site.building.fragment.service.MSBFragmentEntryServiceUtil;
+import com.liferay.modern.site.building.fragment.taglib.servlet.ServletContextUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
 
 /**
  * @author Jürgen Kappler
  */
 public class MSBFragmentEntryRendererTag extends IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		try {
+			_msbFragmentEntry =
+				MSBFragmentEntryServiceUtil.fetchMSBFragmentEntry(
+					_msbFragmentEntryId);
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to get fragment entry", pe);
+			}
+
+			return SKIP_BODY;
+		}
+
+		return super.doStartTag();
+	}
+
+	public void setMsbFragmentEntryId(long msbFragmentEntryId) {
+		_msbFragmentEntryId = msbFragmentEntryId;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	@Override
+	protected void cleanUp() {
+		_msbFragmentEntry = null;
+		_msbFragmentEntryId = 0;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-modern-site-building-fragment:" +
+				"msb-fragment-entry-renderer:msbFragmentEntry",
+			_msbFragmentEntry);
+	}
+
+	private static final String _PAGE = "/msb_fragment_entry/page.jsp";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		MSBFragmentEntryRendererTag.class);
+
+	private MSBFragmentEntry _msbFragmentEntry;
+	private long _msbFragmentEntryId;
+
 }
