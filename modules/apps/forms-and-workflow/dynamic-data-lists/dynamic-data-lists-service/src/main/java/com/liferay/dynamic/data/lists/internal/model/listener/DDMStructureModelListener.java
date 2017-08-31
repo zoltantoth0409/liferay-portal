59e31b8b34f48b999085d32fd35ae058d5b8f476
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-08-31 13:22:40 -0700

    LPS-74475 SF

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/model/listener/DDMStructureModelListener.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/model/listener/DDMStructureModelListener.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.dynamic.data.lists.internal.model.listener;
 
 import com.liferay.dynamic.data.lists.model.DDLRecordSet;
