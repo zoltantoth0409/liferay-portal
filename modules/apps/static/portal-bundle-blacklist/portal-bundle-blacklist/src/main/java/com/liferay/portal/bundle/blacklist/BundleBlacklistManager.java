53dc9b7431b509ea684f103f14a4b17db75f1f4f
Preston-Crary <preston.crary@liferay.com>
2017-08-31 10:17:30 -0700

    LPS-74456 Add BundleBlacklistManager API with bulk add, bulk remove and query operations.

---
+++ modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist/src/main/java/com/liferay/portal/bundle/blacklist/BundleBlacklistManager.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.bundle.blacklist;
+
+import com.liferay.portal.bundle.blacklist.internal.BundleBlacklist;
+import com.liferay.portal.bundle.blacklist.internal.BundleBlacklistConfiguration;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.io.IOException;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Function;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Preston Crary
+ */
+@Component(immediate = true, service = BundleBlacklistManager.class)
+public class BundleBlacklistManager {
+
+	public void addToBlacklistAndUninstall(String... bundleSymbolicNames)
+		throws IOException {
+
+		_updateProperties(
+			blacklistBundleSymbolicNames -> {
+				if (blacklistBundleSymbolicNames == null) {
+					return bundleSymbolicNames;
+				}
+
+				Set<String> blacklistBundleSymbolicNamesSet = SetUtil.fromArray(
+					blacklistBundleSymbolicNames);
+
+				Collections.addAll(
+					blacklistBundleSymbolicNamesSet, bundleSymbolicNames);
+
+				return blacklistBundleSymbolicNamesSet.toArray(
+					new String[blacklistBundleSymbolicNamesSet.size()]);
+			});
+	}
+
+	public Collection<String> getBlacklistBundleSymbolicNames() {
+		return _bundleBlacklist.getBlacklistBundleSymbolicNames();
+	}
+
+	public void removeFromBlacklistAndInstall(String... bundleSymbolicNames)
+		throws IOException {
+
+		_updateProperties(
+			blacklistBundleSymbolicNames -> {
+				if (blacklistBundleSymbolicNames == null) {
+					return null;
+				}
+
+				Set<String> blacklistBundleSymbolicNamesSet = SetUtil.fromArray(
+					blacklistBundleSymbolicNames);
+
+				for (String bundleSymbolicName : bundleSymbolicNames) {
+					blacklistBundleSymbolicNamesSet.remove(bundleSymbolicName);
+				}
+
+				return blacklistBundleSymbolicNamesSet.toArray(
+					new String[blacklistBundleSymbolicNamesSet.size()]);
+			});
+	}
+
+	private void _updateConfiguration(
+			BundleContext bundleContext, Configuration configuration,
+			Dictionary<String, Object> properties)
+		throws IOException {
+
+		CountDownLatch countDownLatch = new CountDownLatch(1);
+
+		ServiceListener serviceListener = new ServiceListener() {
+
+			@Override
+			public void serviceChanged(ServiceEvent serviceEvent) {
+				if (serviceEvent.getType() != ServiceEvent.MODIFIED) {
+					return;
+				}
+
+				ServiceReference<?> serviceReference =
+					serviceEvent.getServiceReference();
+
+				Object service = bundleContext.getService(serviceReference);
+
+				if (_bundleBlacklist == service) {
+					countDownLatch.countDown();
+				}
+
+				bundleContext.ungetService(serviceReference);
+			}
+
+		};
+
+		bundleContext.addServiceListener(serviceListener);
+
+		try {
+			configuration.update(properties);
+
+			countDownLatch.await();
+		}
+		catch (InterruptedException ie) {
+		}
+		finally {
+			bundleContext.removeServiceListener(serviceListener);
+		}
+	}
+
+	private void _updateProperties(Function<String[], String[]> updateFunction)
+		throws IOException {
+
+		Bundle bundle = FrameworkUtil.getBundle(BundleBlacklistManager.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		ServiceReference<ConfigurationAdmin> serviceReference =
+			bundleContext.getServiceReference(ConfigurationAdmin.class);
+
+		try {
+			ConfigurationAdmin configurationAdmin = bundleContext.getService(
+				serviceReference);
+
+			Configuration configuration = configurationAdmin.getConfiguration(
+				BundleBlacklistConfiguration.class.getName(),
+				StringPool.QUESTION);
+
+			Dictionary<String, Object> properties =
+				configuration.getProperties();
+
+			String[] blacklistBundleSymbolicNames = null;
+
+			if (properties == null) {
+				properties = new HashMapDictionary<>();
+			}
+			else {
+				Object value = properties.get("blacklistBundleSymbolicNames");
+
+				if (value instanceof String) {
+					blacklistBundleSymbolicNames = new String[] {(String)value};
+				}
+				else {
+					blacklistBundleSymbolicNames = (String[])value;
+				}
+			}
+
+			blacklistBundleSymbolicNames = updateFunction.apply(
+				blacklistBundleSymbolicNames);
+
+			if (blacklistBundleSymbolicNames == null) {
+				return;
+			}
+
+			properties.put(
+				"blacklistBundleSymbolicNames", blacklistBundleSymbolicNames);
+
+			_updateConfiguration(bundleContext, configuration, properties);
+		}
+		finally {
+			bundleContext.ungetService(serviceReference);
+		}
+	}
+
+	@Reference
+	private BundleBlacklist _bundleBlacklist;
+
+}
