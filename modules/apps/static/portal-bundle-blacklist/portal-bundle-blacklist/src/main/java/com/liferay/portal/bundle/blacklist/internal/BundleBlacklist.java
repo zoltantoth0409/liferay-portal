f5e269a75f5e3367c231cfc73a398e72a55faad7
Preston-Crary <preston.crary@liferay.com>
2017-08-31 10:17:30 -0700

    LPS-74456 Add BundleBlacklistManager API with bulk add, bulk remove and query operations.

--- modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist/src/main/java/com/liferay/portal/bundle/blacklist/internal/BundleBlacklist.java
+++ modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist/src/main/java/com/liferay/portal/bundle/blacklist/internal/BundleBlacklist.java
@@ -60,6 +60,10 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class BundleBlacklist {
 
+	public List<String> getBlacklistBundleSymbolicNames() {
+		return new ArrayList<>(_uninstalledBundles.keySet());
+	}
+
 	@Activate
 	@Modified
 	protected void activate(
