3a02636be7e32b23190a46bf1b1339dc4d8f0231
shuyangzhou <shuyang.zhou@liferay.com>
2017-08-30 17:22:30 -0700

    LPS-74171 Set session.cookie.use.full.hostname to true only for Tomcat because Tomcat 8.5 has adopted RFC 6265 as default

--- portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/CookieKeys.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.kernel.util;
 
+import com.liferay.portal.kernel.configuration.Filter;
 import com.liferay.portal.kernel.exception.CookieNotSupportedException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -329,7 +330,9 @@ public class CookieKeys {
 
 	private static final boolean _SESSION_COOKIE_USE_FULL_HOSTNAME =
 		GetterUtil.getBoolean(
-			PropsUtil.get(PropsKeys.SESSION_COOKIE_USE_FULL_HOSTNAME));
+			PropsUtil.get(
+				PropsKeys.SESSION_COOKIE_USE_FULL_HOSTNAME,
+				new Filter(ServerDetector.getServerId())));
 
 	private static final boolean _SESSION_ENABLE_PERSISTENT_COOKIES =
 		GetterUtil.getBoolean(
