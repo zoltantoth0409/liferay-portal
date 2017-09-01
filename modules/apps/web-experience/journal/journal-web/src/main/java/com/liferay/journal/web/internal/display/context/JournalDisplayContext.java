f2716104b8b8602111c155afd249891928a76a47
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-09-01 10:42:27 +0200

    LPS-73925 SF

--- modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1154,6 +1154,7 @@ public class JournalDisplayContext {
 		attributes.put("ddmStructureKey", ddmStructureKey);
 		attributes.put("ddmTemplateKey", ddmTemplateKey);
 		attributes.put("params", params);
+
 		searchContext.setAttributes(attributes);
 
 		searchContext.setCompanyId(companyId);
