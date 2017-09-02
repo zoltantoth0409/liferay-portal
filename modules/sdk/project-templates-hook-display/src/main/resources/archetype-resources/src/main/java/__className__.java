1021d6eb91f00f16bd749554a06614b82e548694
Lawrence Lee <lawrence.lee@liferay.com>
2017-09-02 16:26:49 -0500

    LPS-74448 Add Simple Hook Display Project Template

---
+++ modules/sdk/project-templates-hook-display/src/main/resources/archetype-resources/src/main/java/__className__.java
@@ -0,0 +1,16 @@
+package ${package}.${className};
+
+import com.liferay.portal.kernel.events.SimpleAction;
+import com.liferay.portal.kernel.events.ActionException;
+
+/**
+ * @author ${author}
+ */
+public class ${className} extends SimpleAction {
+	public ${className}() {
+		super();
+	}
+
+	public void run(String[] arg0) throws ActionException {
+	}
+}
