79a516dca3a32c7eb598713dfb30e08f9d8d207e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-09-04 09:32:08 +0200

    LPS-74523 Run with arquillian

--- modules/apps/web-experience/asset/asset-test/src/testIntegration/java/com/liferay/asset/model/test/AssetTagDisplayTest.java
+++ modules/apps/web-experience/asset/asset-test/src/testIntegration/java/com/liferay/asset/model/test/AssetTagDisplayTest.java
@@ -14,15 +14,26 @@
 
 package com.liferay.asset.model.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetTagDisplay;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Igor Spasic
  */
+@RunWith(Arquillian.class)
 public class AssetTagDisplayTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
 
 	@Test
 	public void testGetPage() {
