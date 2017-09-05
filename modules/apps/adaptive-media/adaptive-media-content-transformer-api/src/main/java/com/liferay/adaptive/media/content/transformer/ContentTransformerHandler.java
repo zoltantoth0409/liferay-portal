c7145db802e6d7ad1d28f1ad3aaabfeb79870b2a
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-09-05 16:14:24 +0200

    LPS-74382 Exception is handled already. Just show trace if debug is enabled

--- modules/apps/adaptive-media/adaptive-media-content-transformer-api/src/main/java/com/liferay/adaptive/media/content/transformer/ContentTransformerHandler.java
+++ modules/apps/adaptive-media/adaptive-media-content-transformer-api/src/main/java/com/liferay/adaptive/media/content/transformer/ContentTransformerHandler.java
@@ -55,7 +55,9 @@ public class ContentTransformerHandler {
 					transformedContent);
 			}
 			catch (Exception e) {
-				_log.error(e);
+				if (_log.isDebugEnabled()) {
+					_log.error(e);
+				}
 			}
 		}
 
