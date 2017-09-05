ae4d7ea6559d9f2f6974f6f1333256f1d06a3705
shuyangzhou <shuyang.zhou@liferay.com>
2017-09-04 21:26:39 -0700

    LPS-74473 Rename

--- modules/apps/static/file-install-helper/file-install-helper/src/main/java/com/liferay/file.install/helper/ConfigurationResetter.java
+++ modules/apps/static/file-install-helper/file-install-helper/src/main/java/com/liferay/file.install/helper/ConfigurationResetter.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.fileinstall.helper;
+package com.liferay.file.install.helper;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
