857a79eaac7c26223114e5d964a2e15bc626bdeb
Brian Chan <brian.chan@liferay.com>
2017-09-05 16:31:53 -0700

    LPS-74473 move

--- modules/apps/static/felix-file-install-helper/felix-file-install-helper/src/main/java/com/liferay/felix/file/install/helper/ConfigurationResetter.java
+++ modules/apps/static/felix-file-install-helper/felix-file-install-helper/src/main/java/com/liferay/felix/file/install/helper/ConfigurationResetter.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.file.install.helper;
+package com.liferay.felix.file.install.helper;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
