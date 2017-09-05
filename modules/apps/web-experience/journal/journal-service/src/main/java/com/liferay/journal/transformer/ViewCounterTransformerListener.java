f015d22e501d275e3cf025bc08df4b54ca2ba846
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-09-05 12:01:53 +0200

    LPS-59697 Fix call to service

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/transformer/ViewCounterTransformerListener.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/transformer/ViewCounterTransformerListener.java
@@ -63,8 +63,8 @@ public class ViewCounterTransformerListener extends BaseTransformerListener {
 		StringBundler sb = new StringBundler(6);
 
 		sb.append("<script type=\"text/javascript\">");
-		sb.append("Liferay.Service.Asset.AssetEntry.incrementViewCounter");
-		sb.append("({userId:0, className:'");
+		sb.append("Liferay.Service('/assetentry/increment-view-counter',");
+		sb.append("{userId:0, className:'");
 		sb.append("com.liferay.journal.model.JournalArticle', classPK:");
 		sb.append(articleResourcePK);
 		sb.append("});</script>");
