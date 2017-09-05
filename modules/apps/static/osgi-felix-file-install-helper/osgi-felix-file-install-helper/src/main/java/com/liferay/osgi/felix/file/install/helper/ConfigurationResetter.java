e21414465759c44364e3ee08117d75697538c1af
Brian Chan <brian.chan@liferay.com>
2017-09-05 16:40:29 -0700

    LPS-74473 match com.liferay.osgi.felix.util

--- modules/apps/static/osgi-felix-file-install-helper/osgi-felix-file-install-helper/src/main/java/com/liferay/osgi/felix/file/install/helper/ConfigurationResetter.java
+++ modules/apps/static/osgi-felix-file-install-helper/osgi-felix-file-install-helper/src/main/java/com/liferay/osgi/felix/file/install/helper/ConfigurationResetter.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.felix.file.install.helper;
+package com.liferay.osgi.felix.file.install.helper;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
