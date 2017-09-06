9fafcd975860e472d4ec84b86cb01f1b836f05c5
Alessio Rendina <asilcomando@gmail.com>
2017-09-06 11:26:05 +0200

    COMMERCE-31 Added configuration action

---
+++ modules/dxp/apps/commerce/commerce-cart-content-web/src/main/java/com/liferay/commerce/cart/content/web/internal/portlet/action/CommerceCartContentTotalConfigurationAction.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.cart.content.web.internal.portlet.action;
+
+import com.liferay.commerce.cart.content.web.internal.display.context.CommerceCartContentTotalDisplayContext;
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.product.util.CPDefinitionHelper;
+import com.liferay.commerce.product.util.CPInstanceHelper;
+import com.liferay.commerce.service.CommerceCartItemService;
+import com.liferay.commerce.util.CommerceCartHelper;
+import com.liferay.commerce.util.CommercePriceCalculationHelper;
+import com.liferay.commerce.util.CommercePriceFormatter;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletConfig;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + CommercePortletKeys.COMMERCE_CART_CONTENT_TOTAL,
+	service = ConfigurationAction.class
+)
+public class CommerceCartContentTotalConfigurationAction
+	extends DefaultConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest request) {
+		return "/cart_total/configuration.jsp";
+	}
+
+	@Override
+	public void include(
+			PortletConfig portletConfig, HttpServletRequest request,
+			HttpServletResponse response)
+		throws Exception {
+
+		try {
+			CommerceCartContentTotalDisplayContext
+				commerceCartContentTotalDisplayContext =
+					new CommerceCartContentTotalDisplayContext(
+						request, response, _commerceCartHelper,
+						_commerceCartItemService,
+						_commercePriceCalculationHelper,
+						_commercePriceFormatter, _cpDefinitionHelper,
+						_cpInstanceHelper);
+
+			request.setAttribute(
+				WebKeys.PORTLET_DISPLAY_CONTEXT,
+				commerceCartContentTotalDisplayContext);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		super.include(portletConfig, request, response);
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.cart.content.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceCartContentTotalConfigurationAction.class);
+
+	@Reference
+	private CommerceCartHelper _commerceCartHelper;
+
+	@Reference
+	private CommerceCartItemService _commerceCartItemService;
+
+	@Reference
+	private CommercePriceCalculationHelper _commercePriceCalculationHelper;
+
+	@Reference
+	private CommercePriceFormatter _commercePriceFormatter;
+
+	@Reference
+	private CPDefinitionHelper _cpDefinitionHelper;
+
+	@Reference
+	private CPInstanceHelper _cpInstanceHelper;
+
+	@Reference
+	private Portal _portal;
+
+}
