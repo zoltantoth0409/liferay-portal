0fcb83556527d3560ad3828f0713577027a5b891
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-09-06 21:34:26 +0200

    LPS-73791 Rename AdaptiveMediaImageDLPluggableContentDataHandler to AMImageDLPluggableContentDataHandler

--- modules/apps/adaptive-media/adaptive-media-document-library/src/main/java/com/liferay/adaptive/media/document/library/internal/exportimport/data/handler/AMImageDLPluggableContentDataHandler.java
+++ modules/apps/adaptive-media/adaptive-media-document-library/src/main/java/com/liferay/adaptive/media/document/library/internal/exportimport/data/handler/AMImageDLPluggableContentDataHandler.java
@@ -55,7 +55,7 @@ import org.osgi.service.component.annotations.Reference;
 	},
 	service = DLPluggableContentDataHandler.class
 )
-public class AdaptiveMediaImageDLPluggableContentDataHandler
+public class AMImageDLPluggableContentDataHandler
 	implements DLPluggableContentDataHandler<FileEntry> {
 
 	@Override
