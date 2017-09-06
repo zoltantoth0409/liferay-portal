cd316a4bc88529eaf11306755a6f8acb47bf84af
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-09-06 13:37:43 +0200

    LPS-74303 Uses new classes in asset service module

--- modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleIndexableTest.java
+++ modules/apps/web-experience/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleIndexableTest.java
@@ -17,6 +17,7 @@ package com.liferay.journal.search.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
 import com.liferay.asset.test.util.AssetEntryQueryTestUtil;
+import com.liferay.asset.util.impl.AssetUtil;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.model.JournalFolderConstants;
@@ -38,7 +39,6 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.service.test.ServiceTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portlet.asset.util.AssetUtil;
 
 import org.junit.Assert;
 import org.junit.Before;
