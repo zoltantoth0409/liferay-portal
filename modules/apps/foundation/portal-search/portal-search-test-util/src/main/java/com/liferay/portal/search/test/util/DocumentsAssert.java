9802f3ece222b7a40a0bc6885db190303c98e62f
André de Oliveira <andre.oliveira@liferay.com>
2017-09-07 10:52:38 -0700

    LPS-72998 portal-search-test-util: Assert docs returned regardless of relevance
    https://issues.liferay.com/browse/LPS-72998

--- modules/apps/foundation/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/DocumentsAssert.java
+++ modules/apps/foundation/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/DocumentsAssert.java
@@ -16,6 +16,9 @@ package com.liferay.portal.search.test.util;
 
 import com.liferay.portal.kernel.search.Document;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -51,6 +54,17 @@ public class DocumentsAssert {
 			actualValues.toString());
 	}
 
+	public static void assertValuesIgnoreRelevance(
+		String message, Document[] documents, String field,
+		Collection<String> expectedValues) {
+
+		List<String> actualValues = _getValues(field, documents);
+
+		Assert.assertEquals(
+			message + "->" + actualValues, _sort(expectedValues),
+			_sort(actualValues));
+	}
+
 	private static List<String> _getValues(
 		String field, Document... documents) {
 
@@ -60,6 +74,14 @@ public class DocumentsAssert {
 			document -> document.get(field));
 
 		return valuesStream.collect(Collectors.toList());
+	}
+
+	private static String _sort(Collection<String> collection) {
+		List<String> list = new ArrayList<>(collection);
+
+		Collections.sort(list);
+
+		return list.toString();
 	}
 
 }
