7cd4bc9b29659991ae1ecac6e48d280527369cd0
shuyangzhou <shuyang.zhou@liferay.com>
2017-09-07 15:30:01 -0700

    LPS-74553 Use a map to throw away updated bundles.

--- modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
+++ modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -80,6 +80,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -1147,7 +1148,7 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 		bundleContext.registerService(
 			ThrowableCollector.class, throwableCollector, dictionary);
 
-		final List<Bundle> bundles = new ArrayList<>();
+		final Map<String, Bundle> bundles = new LinkedHashMap<>();
 
 		final List<Path> jarPaths = new ArrayList<>();
 
@@ -1218,7 +1219,7 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 			Path filePath = Paths.get(location.substring(prefix.length()));
 
 			if (jarPaths.contains(filePath)) {
-				bundles.add(bundle);
+				bundles.put(bundle.getLocation(), bundle);
 
 				continue;
 			}
@@ -1248,7 +1249,7 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 					_STATIC_JAR.concat(path), inputStream);
 
 				if (bundle != null) {
-					bundles.add(bundle);
+					bundles.put(bundle.getLocation(), bundle);
 
 					overrideStaticFileNames.add(
 						path.substring(path.lastIndexOf(StringPool.SLASH) + 1));
@@ -1332,7 +1333,7 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 							StringPool.SLASH.concat(zipEntryName), inputStream);
 
 						if (bundle != null) {
-							bundles.add(bundle);
+							bundles.put(bundle.getLocation(), bundle);
 						}
 					}
 				}
@@ -1377,7 +1378,7 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 		frameworkStartLevel.setStartLevel(
 			PropsValues.MODULE_FRAMEWORK_BEGINNING_START_LEVEL);
 
-		for (final Bundle bundle : bundles) {
+		for (final Bundle bundle : bundles.values()) {
 			if (_isFragmentBundle(bundle)) {
 				continue;
 			}
