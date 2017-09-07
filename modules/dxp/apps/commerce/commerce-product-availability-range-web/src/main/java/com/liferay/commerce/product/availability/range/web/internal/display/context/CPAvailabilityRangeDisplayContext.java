1e5278bb5bd52aed7e25987759a327649dd89fb5
Alessio Rendina <asilcomando@gmail.com>
2017-09-07 11:35:51 +0200

    COMMERCE-210 Added new display context

---
+++ modules/dxp/apps/commerce/commerce-product-availability-range-web/src/main/java/com/liferay/commerce/product/availability/range/web/internal/display/context/CPAvailabilityRangeDisplayContext.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.availability.range.web.internal.display.context;
+
+import com.liferay.commerce.product.availability.range.web.internal.util.CPAvailabilityRangeAdminModule;
+import com.liferay.commerce.product.availability.range.web.internal.util.CPAvailabilityRangeUtil;
+import com.liferay.commerce.product.model.CPAvailabilityRange;
+import com.liferay.commerce.product.service.CPAvailabilityRangeService;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.dao.search.RowChecker;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import java.util.List;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CPAvailabilityRangeDisplayContext {
+
+	public CPAvailabilityRangeDisplayContext(
+		CPAvailabilityRangeService cpAvailabilityRangeService,
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		_cpAvailabilityRangeService = cpAvailabilityRangeService;
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+	}
+
+	public CPAvailabilityRange getCPAvailabilityRange() throws PortalException {
+		if (_cpAvailabilityRange != null) {
+			return _cpAvailabilityRange;
+		}
+
+		long cpAvailabilityRangeId = ParamUtil.getLong(
+			_renderRequest, "cpAvailabilityRangeId");
+
+		if (cpAvailabilityRangeId > 0) {
+			_cpAvailabilityRange =
+				_cpAvailabilityRangeService.getCPAvailabilityRange(
+					cpAvailabilityRangeId);
+		}
+
+		return _cpAvailabilityRange;
+	}
+
+	public String getOrderByCol() {
+		return ParamUtil.getString(
+			_renderRequest, SearchContainer.DEFAULT_ORDER_BY_COL_PARAM,
+			"priority");
+	}
+
+	public String getOrderByType() {
+		return ParamUtil.getString(
+			_renderRequest, SearchContainer.DEFAULT_ORDER_BY_TYPE_PARAM, "asc");
+	}
+
+	public PortletURL getPortletURL() {
+		PortletURL portletURL = _renderResponse.createRenderURL();
+
+		portletURL.setParameter(
+			"commerceAdminModuleKey", CPAvailabilityRangeAdminModule.KEY);
+		portletURL.setParameter("orderByCol", getOrderByCol());
+		portletURL.setParameter("orderByType", getOrderByType());
+
+		return portletURL;
+	}
+
+	public SearchContainer<CPAvailabilityRange> getSearchContainer() {
+		if (_searchContainer != null) {
+			return _searchContainer;
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)_renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String emptyResultsMessage = "there-are-no-availability-ranges";
+
+		_searchContainer = new SearchContainer<>(
+			_renderRequest, getPortletURL(), null, emptyResultsMessage);
+
+		String orderByCol = getOrderByCol();
+		String orderByType = getOrderByType();
+
+		OrderByComparator<CPAvailabilityRange> orderByComparator =
+			CPAvailabilityRangeUtil.getCPAvailabilityRangeOrderByComparator(
+				orderByCol, orderByType);
+
+		_searchContainer.setOrderByCol(orderByCol);
+		_searchContainer.setOrderByComparator(orderByComparator);
+		_searchContainer.setOrderByType(orderByType);
+		_searchContainer.setRowChecker(getRowChecker());
+
+		int total = _cpAvailabilityRangeService.getCPAvailabilityRangesCount(
+			themeDisplay.getScopeGroupId());
+
+		List<CPAvailabilityRange> results =
+			_cpAvailabilityRangeService.getCPAvailabilityRanges(
+				themeDisplay.getScopeGroupId(), _searchContainer.getStart(),
+				_searchContainer.getEnd(), orderByComparator);
+
+		_searchContainer.setTotal(total);
+		_searchContainer.setResults(results);
+
+		return _searchContainer;
+	}
+
+	protected RowChecker getRowChecker() {
+		if (_rowChecker == null) {
+			_rowChecker = new EmptyOnClickRowChecker(_renderResponse);
+		}
+
+		return _rowChecker;
+	}
+
+	private CPAvailabilityRange _cpAvailabilityRange;
+	private final CPAvailabilityRangeService _cpAvailabilityRangeService;
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+	private RowChecker _rowChecker;
+	private SearchContainer<CPAvailabilityRange> _searchContainer;
+
+}
