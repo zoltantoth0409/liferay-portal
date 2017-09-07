e0b95642545f964c0d22b122106d0e6e3800759b
tina.tian <tina.tian@liferay.com>
2017-09-07 09:21:46 -0700

    LPS-74401 Enable ApplePushNotificationsMessagingConfigurator when service dependency is ready

---
+++ modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-apple/src/main/java/com/liferay/push/notifications/sender/apple/internal/component/enabler/ComponentEnabler.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.push.notifications.sender.apple.internal.component.enabler;
+
+import com.liferay.osgi.util.ComponentUtil;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.push.notifications.constants.PushNotificationsDestinationNames;
+import com.liferay.push.notifications.sender.apple.internal.messaging.ApplePushNotificationsMessagingConfigurator;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Tina Tian
+ */
+@Component(immediate = true)
+public class ComponentEnabler {
+
+	@Activate
+	protected void activate(ComponentContext componentContext) {
+		ComponentUtil.tryEnableComponents(
+			Destination.class,
+			"(destination.name= " +
+				PushNotificationsDestinationNames.PUSH_NOTIFICATION_RESPONSE +
+					")",
+			componentContext,
+			ApplePushNotificationsMessagingConfigurator.class);
+	}
+
+}
