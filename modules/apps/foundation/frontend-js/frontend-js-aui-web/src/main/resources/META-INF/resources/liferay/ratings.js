d8cf919a31cea14f9babe4ecfc82d4e44b842012
Hugo Huijser <hugo.huijser@liferay.com>
2017-09-08 10:09:50 +0800

    LPS-74562 Let's do the same for thumbs up (just to be safe if code changes later on)

--- modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/ratings.js
+++ modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/ratings.js
@@ -510,7 +510,11 @@ AUI.add(
 
 						var ratingThumbUp = elements.item(0);
 
-						var ratingThumbUpCssClassOn = ratingThumbUp.hasClass(cssClassesOn);
+						var ratingThumbUpCssClassOn = false;
+
+						if (ratingThumbUp) {
+							ratingThumbUpCssClassOn = ratingThumbUp.hasClass(cssClassesOn);
+						}
 
 						var thumbDownMessage = '';
 						var thumbUpMessage = '';
