621eb1648078e1c8becec996d3b11b21ce619008
Marco Leo <marco.leo@liferay.com>
2017-09-08 13:14:47 +0200

    COMMERCE-31 Add new commerce tablit for show quantity input

---
+++ modules/dxp/apps/commerce/commerce-taglib/src/main/java/com/liferay/commerce/taglib/servlet/taglib/QuantityInputTag.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.taglib.servlet.taglib;
+
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionServiceUtil;
+import com.liferay.commerce.taglib.servlet.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Marco Leo
+ */
+public class QuantityInputTag extends IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PortletRequest portletRequest = (PortletRequest)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_REQUEST);
+		PortletResponse portletResponse = (PortletResponse)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+		try {
+			_cpDefinition = CPDefinitionServiceUtil.getCPDefinition(
+				_cpDefinitionId);
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to get cpDefinition", pe);
+			}
+
+			return SKIP_BODY;
+		}
+
+		return super.doStartTag();
+	}
+
+	public void setCPDefinitionId(long cpDefinitionId) {
+		_cpDefinitionId = cpDefinitionId;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	public void setUseSelect(boolean useSelect) {
+		_useSelect = useSelect;
+	}
+
+	@Override
+	protected void cleanUp() {
+		_cpDefinition = null;
+		_cpDefinitionId = 0;
+		_useSelect = true;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-commerce:quantity-input:cpDefinition", _cpDefinition);
+		request.setAttribute(
+			"liferay-commerce:quantity-input:useSelect", _useSelect);
+	}
+
+	private static final String _PAGE = "/quantity_input/page.jsp";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		QuantityInputTag.class);
+
+	private CPDefinition _cpDefinition;
+	private long _cpDefinitionId;
+	private boolean _useSelect = true;
+
+}
---
+++ modules/dxp/apps/commerce/commerce-taglib/src/main/resources/META-INF/resources/quantity_input/page.jsp
@@ -0,0 +1,82 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/quantity_input/init.jsp" %>
+
+<%
+CPDefinition cpDefinition = (CPDefinition)request.getAttribute("liferay-commerce:quantity-input:cpDefinition");
+boolean useSelect = (boolean)request.getAttribute("liferay-commerce:quantity-input:useSelect");
+
+long cpDefinitionId = cpDefinition.getCPDefinitionId();
+int minCartQuantity = cpDefinition.getMinCartQuantity();
+int maxCartQuantity = cpDefinition.getMaxCartQuantity();
+int multipleCartQuantity = cpDefinition.getMultipleCartQuantity();
+String allowedCartQuantity = cpDefinition.getAllowedCartQuantities();
+
+int[] allowedCartQuantities = null;
+
+if(Validator.isNotNull(allowedCartQuantity))
+	allowedCartQuantities = StringUtil.split(allowedCartQuantity,0);
+
+	if(allowedCartQuantities != null) {
+		Arrays.sort(allowedCartQuantities);
+	}
+%>
+
+<div class="commerce-quantity-container">
+	<c:choose>
+		<c:when test="<%=!useSelect%>">
+			<aui:input type="text" name='<%= cpDefinitionId + "Quantity" %>' label="quantity">
+				<aui:validator name="number" />
+				<aui:validator name="min"><%= minCartQuantity%></aui:validator>
+				<aui:validator name="max"><%= maxCartQuantity%></aui:validator>
+			</aui:input>
+		</c:when>
+		<c:when test="<%= allowedCartQuantities != null%>">
+			<aui:select name='<%= cpDefinitionId + "Quantity" %>' label="quantity">
+				<% for(int curQuantity : allowedCartQuantities){ %>
+					<aui:option value="<%= curQuantity %>" label="<%= curQuantity %>" />
+				<%}%>
+			</aui:select>
+		</c:when>
+		<c:otherwise>
+			<aui:select name='<%= cpDefinitionId + "Quantity" %>' label="quantity">
+				<%
+				int quantity = 1;
+
+				if(minCartQuantity > 1){
+					quantity = minCartQuantity;
+				}
+
+				for(int i=1;i <10; i++){ %>
+					<aui:option value="<%= quantity %>" label="<%= quantity %>" />
+				<%
+					if((maxCartQuantity > 0) && (quantity == maxCartQuantity)){
+						break;
+					}
+
+					if(multipleCartQuantity > 1){
+						quantity = quantity + multipleCartQuantity;
+					}
+					else{
+						quantity++;
+					}
+				}
+				%>
+			</aui:select>
+		</c:otherwise>
+	</c:choose>
+</div>
