147e3f08eab18939db90bcd054c5c38204e8bbb0
Lawrence Lee <lawrence.lee@liferay.com>
2017-09-08 15:09:37 -0500

    LPS-74448 rename project template

--- modules/sdk/project-templates-war-hook/src/main/resources/archetype-resources/src/main/java/__className__.java
+++ modules/sdk/project-templates-war-hook/src/main/resources/archetype-resources/src/main/java/__className__.java
@@ -2,15 +2,16 @@ package ${package}.${className};
 
 import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.util.GetterUtil;
 
 /**
  * @author ${author}
  */
 public class ${className} extends SimpleAction {
-	public ${className}() {
-		super();
-	}
-
+	
+	@Override
 	public void run(String[] arg0) throws ActionException {
+		System.out.println("Currently logged in as: " + System.getProperty("user.name"));
 	}
+	
 }
