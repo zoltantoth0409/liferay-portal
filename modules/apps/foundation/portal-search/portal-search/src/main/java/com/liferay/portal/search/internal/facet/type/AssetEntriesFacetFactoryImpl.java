11da580143334e59dc4558df7f50c4b00602baa9
André de Oliveira <andre.oliveira@liferay.com>
2017-09-08 17:50:01 -0700

    LPS-72998 portal-search: onward AssetEntriesFacetFactory, with selection support
    https://issues.liferay.com/browse/LPS-72998

---
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/facet/type/AssetEntriesFacetFactoryImpl.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.facet.type;
+
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.facet.util.FacetFactory;
+import com.liferay.portal.search.facet.Facet;
+import com.liferay.portal.search.facet.type.AssetEntriesFacetFactory;
+import com.liferay.portal.search.internal.facet.FacetImpl;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author André de Oliveira
+ */
+@Component(
+	immediate = true,
+	service = {AssetEntriesFacetFactory.class, FacetFactory.class}
+)
+public class AssetEntriesFacetFactoryImpl implements AssetEntriesFacetFactory {
+
+	@Override
+	public String getFacetClassName() {
+		return Field.ENTRY_CLASS_NAME;
+	}
+
+	@Override
+	public Facet newInstance(SearchContext searchContext) {
+		return new FacetImpl(Field.ENTRY_CLASS_NAME, searchContext);
+	}
+
+}
