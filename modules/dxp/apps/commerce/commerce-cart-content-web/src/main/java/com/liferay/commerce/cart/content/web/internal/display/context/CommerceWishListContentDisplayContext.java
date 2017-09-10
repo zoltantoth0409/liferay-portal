e3b0e4f45cc466af84510b7bd1d9c62595034453
Alessio Rendina <asilcomando@gmail.com>
2017-09-10 12:53:27 +0200

    COMMERCE-31 Added new display context

---
+++ modules/dxp/apps/commerce/commerce-cart-content-web/src/main/java/com/liferay/commerce/cart/content/web/internal/display/context/CommerceWishListContentDisplayContext.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.cart.content.web.internal.display.context;
+
+import com.liferay.commerce.constants.CommerceConstants;
+import com.liferay.commerce.model.CommerceCartItem;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.util.CPDefinitionHelper;
+import com.liferay.commerce.product.util.CPInstanceHelper;
+import com.liferay.commerce.service.CommerceCartItemService;
+import com.liferay.commerce.util.CommerceCartHelper;
+import com.liferay.commerce.util.CommercePriceCalculationHelper;
+import com.liferay.commerce.util.CommercePriceFormatter;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceWishListContentDisplayContext
+	extends CommerceCartContentDisplayContext {
+
+	public CommerceWishListContentDisplayContext(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse,
+		CommerceCartHelper commerceCartHelper,
+		CommerceCartItemService commerceCartItemService,
+		CommercePriceCalculationHelper commercePriceCalculationHelper,
+		CommercePriceFormatter commercePriceFormatter,
+		CPDefinitionHelper cpDefinitionHelper,
+		CPInstanceHelper cpInstanceHelper) {
+
+		super(
+			httpServletRequest, httpServletResponse, commerceCartHelper,
+			commerceCartItemService, commercePriceCalculationHelper,
+			commercePriceFormatter, cpDefinitionHelper, cpInstanceHelper);
+	}
+
+	public boolean canSellWithoutOptionsCombination(
+			CommerceCartItem commerceCartItem)
+		throws PortalException {
+
+		CPDefinition cpDefinition = commerceCartItem.getCPDefinition();
+
+		if (cpDefinition.getCanSellWithoutOptionsCombination()) {
+			return true;
+		}
+		else {
+			if (commerceCartItem.getCPInstanceId() > 0) {
+				return true;
+			}
+
+			return false;
+		}
+	}
+
+	@Override
+	public int getCommerceCartType() {
+		return CommerceConstants.COMMERCE_CART_TYPE_WISH_LIST;
+	}
+
+}
