ebf1918b180a5f246086fb0c732bcac7c61852df
André de Oliveira <andre.oliveira@liferay.com>
2017-09-11 15:57:14 -0700

    LPS-72998 LPS-73809 portal-search-test: Replicate the bug with unit tests
    https://issues.liferay.com/browse/LPS-72998
    https://issues.liferay.com/browse/LPS-73809

---
+++ modules/apps/foundation/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/facet/faceted/searcher/test/AssetEntriesVersionFacetedSearcherTest.java
@@ -0,0 +1,180 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.facet.faceted.searcher.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.facet.AssetEntriesFacetFactory;
+import com.liferay.portal.kernel.search.facet.Facet;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.search.test.journal.util.JournalArticleBuilder;
+import com.liferay.portal.search.test.journal.util.JournalArticleContent;
+import com.liferay.portal.search.test.journal.util.JournalArticleTitle;
+import com.liferay.portal.search.test.util.DocumentsAssert;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author André de Oliveira
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class AssetEntriesVersionFacetedSearcherTest
+	extends BaseFacetedSearcherTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Test
+	public void testOriginal() throws Exception {
+		String keyword = RandomTestUtil.randomString();
+
+		Group group = userSearchFixture.addGroup();
+
+		index(keyword, group);
+
+		SearchContext searchContext = getSearchContext(keyword);
+
+		Facet facet = createFacet(searchContext);
+
+		searchContext.addFacet(facet);
+
+		Hits hits = search(searchContext);
+
+		assertVersions(Arrays.asList("1.0"), hits, keyword);
+
+		assertFrequencies(
+			facet.getFieldName(), searchContext,
+			Collections.singletonMap(JournalArticle.class.getName(), 1));
+	}
+
+	@Test
+	public void testVersioned() throws Exception {
+		String keyword = RandomTestUtil.randomString();
+
+		Group group = userSearchFixture.addGroup();
+
+		JournalArticle journalArticle = index(keyword, group);
+
+		update(journalArticle);
+
+		SearchContext searchContext = getSearchContext(keyword);
+
+		Facet facet = createFacet(searchContext);
+
+		searchContext.addFacet(facet);
+
+		Hits hits = search(searchContext);
+
+		assertVersions(Arrays.asList("1.1"), hits, keyword);
+
+		assertFrequencies(
+			facet.getFieldName(), searchContext,
+			Collections.singletonMap(JournalArticle.class.getName(), 1));
+	}
+
+	protected void assertVersions(
+		List<String> expectedValues, Hits hits, String keyword) {
+
+		DocumentsAssert.assertValuesIgnoreRelevance(
+			keyword, hits.getDocs(), "version", expectedValues);
+	}
+
+	protected Facet createFacet(SearchContext searchContext) {
+		return assetEntriesFacetFactory.newInstance(searchContext);
+	}
+
+	protected JournalArticleBuilder createJournalArticleBuilder(
+		String title, Group group) {
+
+		JournalArticleBuilder journalArticleBuilder =
+			new JournalArticleBuilder();
+
+		journalArticleBuilder.setContent(
+			new JournalArticleContent() {
+				{
+					name = "content";
+					defaultLocale = LocaleUtil.US;
+
+					put(LocaleUtil.US, RandomTestUtil.randomString());
+				}
+			});
+		journalArticleBuilder.setGroupId(group.getGroupId());
+		journalArticleBuilder.setTitle(
+			new JournalArticleTitle() {
+				{
+					put(LocaleUtil.US, title);
+				}
+			});
+
+		return journalArticleBuilder;
+	}
+
+	protected JournalArticle index(String keyword, Group group)
+		throws Exception {
+
+		JournalArticleBuilder journalArticleBuilder =
+			createJournalArticleBuilder(keyword, group);
+
+		JournalArticle journalArticle = journalArticleSearchFixture.addArticle(
+			journalArticleBuilder);
+
+		User user = UserTestUtil.getAdminUser(group.getCompanyId());
+
+		PermissionThreadLocal.setPermissionChecker(
+			permissionCheckerFactory.create(user));
+
+		return journalArticle;
+	}
+
+	protected void update(JournalArticle journalArticle) throws Exception {
+		journalArticleSearchFixture.updateArticle(journalArticle);
+	}
+
+	@Inject
+	protected AssetEntriesFacetFactory assetEntriesFacetFactory;
+
+	@Inject
+	protected JournalArticleLocalService journalArticleLocalService;
+
+	@Inject
+	protected PermissionCheckerFactory permissionCheckerFactory;
+
+}
