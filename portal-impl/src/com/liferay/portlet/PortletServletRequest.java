972a7ff15104d8cb9494bf1c1556f086238bcc66
Dante Wang <dante.wang@liferay.com>
2017-09-11 10:17:57 +0800

    LPS-74517 Also fix the behavior for getDateHeader

--- portal-impl/src/com/liferay/portlet/PortletServletRequest.java
+++ portal-impl/src/com/liferay/portlet/PortletServletRequest.java
@@ -194,7 +194,13 @@ public class PortletServletRequest extends HttpServletRequestWrapper {
 
 	@Override
 	public long getDateHeader(String name) {
-		return GetterUtil.getLong(getHeader(name), -1);
+		String header = getHeader(name);
+
+		if (header == null) {
+			return -1;
+		}
+
+		return GetterUtil.getLongStrict(getHeader(name));
 	}
 
 	@Override
