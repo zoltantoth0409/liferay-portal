d9dc67f93018e249514303d37e2d791fc2c709e9
Minhchau Dang <minhchau.dang@liferay.com>
2017-09-11 18:07:25 -0700

    LPS-73238 Simplify check for ampersand at the end by ensuring that question or ampersand are always at the end

--- portal-impl/src/com/liferay/portlet/PortletURLImpl.java
+++ portal-impl/src/com/liferay/portlet/PortletURLImpl.java
@@ -950,8 +950,6 @@ public class PortletURLImpl
 			renderParams = _mergeWithRenderParameters(renderParams);
 		}
 
-		int previousSbIndex = sb.index();
-
 		for (Map.Entry<String, String[]> entry : renderParams.entrySet()) {
 			String name = entry.getKey();
 			String[] values = entry.getValue();
@@ -976,15 +974,15 @@ public class PortletURLImpl
 			}
 		}
 
-		if (sb.index() > previousSbIndex) {
-			sb.setIndex(sb.index() - 1);
-		}
-
 		if (_encrypt) {
-			sb.append(StringPool.AMPERSAND);
 			sb.append(WebKeys.ENCRYPT);
 			sb.append("=1");
 		}
+		else {
+			sb.setIndex(sb.index() - 1);
+		}
+
+		String result = sb.toString();
 
 		if (PropsValues.PORTLET_URL_ANCHOR_ENABLE) {
 			if (_anchor && (_windowStateString != null) &&
@@ -994,34 +992,15 @@ public class PortletURLImpl
 				!_windowStateString.equals(
 					LiferayWindowState.POP_UP.toString())) {
 
-				String lastString = sb.stringAt(sb.index() - 1);
+				sb.setIndex(0);
 
-				char lastChar = lastString.charAt(lastString.length() - 1);
-
-				if ((lastChar != CharPool.AMPERSAND) &&
-					(lastChar != CharPool.QUESTION)) {
-
-					sb.append(StringPool.AMPERSAND);
-				}
-
+				sb.append(result);
 				sb.append("#p_");
 				sb.append(URLCodec.encodeURL(_portlet.getPortletId()));
+
+				result = sb.toString();
 			}
 		}
-
-		String lastString = sb.stringAt(sb.index() - 1);
-
-		char lastChar = lastString.charAt(lastString.length() - 1);
-
-		if ((lastChar == CharPool.AMPERSAND) ||
-			(lastChar == CharPool.QUESTION)) {
-
-			sb.setStringAt(
-				lastString.substring(0, lastString.length() - 1),
-				sb.index() - 1);
-		}
-
-		String result = sb.toString();
 
 		if (!CookieKeys.hasSessionId(_request)) {
 			result = PortalUtil.getURLWithSessionId(
