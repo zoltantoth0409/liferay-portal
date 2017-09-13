e4c31cd317feda71fa69f99d7fb4133b27381b93
Michael Han <michael.han@liferay.com>
2017-09-13 05:45:58 -0500

    LPS-74666 add related entry indexer registry

---
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/RelatedEntryIndexerRegistryImpl.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.search.RelatedEntryIndexer;
+import com.liferay.portal.kernel.search.RelatedEntryIndexerRegistry;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(immediate = true, service = RelatedEntryIndexerRegistry.class)
+public class RelatedEntryIndexerRegistryImpl
+	implements RelatedEntryIndexerRegistry {
+
+	@Override
+	public List<RelatedEntryIndexer> getRelatedEntryIndexers() {
+		Collection<List<RelatedEntryIndexer>> relatedEntryIndexerLists =
+			serviceTrackerMap.values();
+
+		List<RelatedEntryIndexer> relatedEntryIndexers = new ArrayList<>();
+
+		for (List<RelatedEntryIndexer> relatedEntryIndexerList :
+				relatedEntryIndexerLists) {
+
+			relatedEntryIndexers.addAll(relatedEntryIndexerList);
+		}
+
+		return relatedEntryIndexers;
+	}
+
+	@Override
+	public List<RelatedEntryIndexer> getRelatedEntryIndexers(Class clazz) {
+		return serviceTrackerMap.getService(clazz.getName());
+	}
+
+	@Override
+	public List<RelatedEntryIndexer> getRelatedEntryIndexers(String className) {
+		return serviceTrackerMap.getService(className);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		serviceTrackerMap = ServiceTrackerMapFactory.multiValueMap(
+			bundleContext, RelatedEntryIndexer.class,
+			"related.entry.indexer.class.name");
+
+		serviceTrackerMap.open();
+	}
+
+	@Deactivate
+	protected void deactivate(BundleContext bundleContext) {
+		serviceTrackerMap.close();
+	}
+
+	protected ServiceTrackerMap<String, List<RelatedEntryIndexer>>
+		serviceTrackerMap;
+
+}
---
+++ portal-kernel/src/com/liferay/portal/kernel/search/RelatedEntryIndexerRegistryUtil.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.search;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.util.ServiceProxyFactory;
+
+import java.util.List;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public class RelatedEntryIndexerRegistryUtil {
+
+	public static List<RelatedEntryIndexer> getRelatedEntryIndexers() {
+		return _relatedEntryIndexerRegistry.getRelatedEntryIndexers();
+	}
+
+	public static List<RelatedEntryIndexer> getRelatedEntryIndexers(
+		Class clazz) {
+
+		return _relatedEntryIndexerRegistry.getRelatedEntryIndexers(clazz);
+	}
+
+	public static List<RelatedEntryIndexer> getRelatedEntryIndexers(
+		String className) {
+
+		return _relatedEntryIndexerRegistry.getRelatedEntryIndexers(className);
+	}
+
+	private static volatile RelatedEntryIndexerRegistry
+		_relatedEntryIndexerRegistry =
+			ServiceProxyFactory.newServiceTrackedInstance(
+				RelatedEntryIndexerRegistry.class,
+				RelatedEntryIndexerRegistryUtil.class,
+				"_relatedEntryIndexerRegistry", false);
+
+}
