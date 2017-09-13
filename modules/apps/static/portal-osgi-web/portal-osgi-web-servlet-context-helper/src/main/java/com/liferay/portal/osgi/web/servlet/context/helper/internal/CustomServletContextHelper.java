ef1d8ccc2391f1db87fe213a21110ae7e38a42d1
Dante Wang <dante.wang@liferay.com>
2017-09-13 15:42:32 +0800

    LPS-74825 Only do handleSecurity for ASYNC and REQUEST

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-context-helper/src/main/java/com/liferay/portal/osgi/web/servlet/context/helper/internal/CustomServletContextHelper.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-context-helper/src/main/java/com/liferay/portal/osgi/web/servlet/context/helper/internal/CustomServletContextHelper.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.servlet.DispatcherType;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -130,17 +129,13 @@ public class CustomServletContextHelper
 	public boolean handleSecurity(
 		HttpServletRequest request, HttpServletResponse response) {
 
-		String path = null;
+		if ((request.getDispatcherType() != DispatcherType.ASYNC) &&
+			(request.getDispatcherType() != DispatcherType.REQUEST)) {
 
-		if (request.getDispatcherType() == DispatcherType.INCLUDE) {
-			String pathInfo = (String)request.getAttribute(
-				RequestDispatcher.INCLUDE_PATH_INFO);
+			return true;
+		}
 
-			path = pathInfo;
-		}
-		else {
-			path = request.getPathInfo();
-		}
+		String path = request.getPathInfo();
 
 		if (path == null) {
 			return true;
