2044a13c9c60dc4b7c7936f104427c8d5529e388
akosthurzo <akos.thurzo@liferay.com>
2017-09-14 14:32:06 +0200

    LPS-74271 Do not return because that prevents importing the logo deletion

--- modules/apps/web-experience/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
+++ modules/apps/web-experience/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
@@ -481,10 +481,6 @@ public class StagedLayoutSetStagedModelDataHandler
 
 		String logoPath = headerElement.attributeValue("logo-path");
 
-		if (Validator.isNull(logoPath)) {
-			return;
-		}
-
 		byte[] iconBytes = portletDataContext.getZipEntryAsByteArray(logoPath);
 
 		try {
