469695f212b1ebbdb7c1291cf4e79cbf6c6761d0
Michael Hashimoto <michael.hashimoto@liferay.com>
2017-09-15 15:21:22 -0700

    LRQA-34432 Add a SlaveOfflineRule class to determine when to generate slave offline rules

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/SlaveOfflineRule.java
@@ -0,0 +1,153 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Michael Hashimoto
+ * @author Peter Yoo
+ */
+public class SlaveOfflineRule {
+
+	public static List<SlaveOfflineRule> getSlaveOfflineRules() {
+		if (_slaveOfflineRules != null) {
+			return _slaveOfflineRules;
+		}
+
+		Properties buildProperties = null;
+
+		try {
+			buildProperties = JenkinsResultsParserUtil.getBuildProperties();
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(
+				"Unable to load slave offline rules", ioe);
+		}
+
+		_slaveOfflineRules = new ArrayList<>();
+
+		for (Object propertyNameObject : buildProperties.keySet()) {
+			String propertyName = propertyNameObject.toString();
+
+			if (propertyName.startsWith("slave.offline.rule[")) {
+				String ruleName = propertyName.substring(
+					"slave.offline.rule[".length(),
+					propertyName.lastIndexOf("]"));
+
+				_slaveOfflineRules.add(
+					new SlaveOfflineRule(
+						buildProperties.getProperty(propertyName), ruleName));
+			}
+		}
+
+		return _slaveOfflineRules;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getNotificationList() {
+		return notificationList;
+	}
+
+	public boolean matches(Build build) {
+		Matcher matcher = null;
+
+		if (consolePattern != null) {
+			String consoleText = build.getConsoleText();
+
+			for (String line : consoleText.split("\n")) {
+				matcher = consolePattern.matcher(line);
+
+				if (matcher.find()) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		if (consolePattern != null) {
+			sb.append("console=");
+			sb.append(consolePattern.pattern());
+			sb.append("\n");
+		}
+
+		sb.append("name=");
+		sb.append(name);
+		sb.append("\n");
+
+		if (notificationList != null) {
+			sb.append("notificationList=");
+			sb.append(notificationList);
+			sb.append("\n");
+		}
+
+		return sb.toString();
+	}
+
+	protected Pattern consolePattern;
+	protected String name;
+	protected String notificationList;
+
+	private SlaveOfflineRule(String configurations, String ruleName) {
+		name = ruleName;
+
+		for (String configuration : configurations.split("\n")) {
+			int x = configuration.indexOf("=");
+
+			String name = configuration.substring(0, x);
+			String value = configuration.substring(x + 1);
+
+			value = value.trim();
+
+			if (value.isEmpty()) {
+				continue;
+			}
+
+			if (name.equals("notificationList")) {
+				notificationList = value;
+
+				continue;
+			}
+
+			Pattern pattern = Pattern.compile(value);
+
+			if (name.equals("console")) {
+				consolePattern = pattern;
+
+				continue;
+			}
+		}
+	}
+
+	private static List<SlaveOfflineRule> _slaveOfflineRules;
+
+}
