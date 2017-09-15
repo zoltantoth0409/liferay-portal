26d36d4b826b9e3a9d6d695f2a31ec5d331a8c8a
Michael Han <michael.han@liferay.com>
2017-09-15 12:26:14 -0500

    LPS-80469 calendar-service: integrate calendar into new model indexer
    https://issues.liferay.com/browse/LPS-80469
    
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: Michael Han <michael.han@liferay.com>

---
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/search/CalendarModelDocumentContributor.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.internal.search;
+
+import com.liferay.calendar.model.Calendar;
+import com.liferay.calendar.model.CalendarResource;
+import com.liferay.calendar.service.CalendarResourceLocalService;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.calendar.model.Calendar",
+	service = ModelDocumentContributor.class
+)
+public class CalendarModelDocumentContributor
+	implements ModelDocumentContributor<Calendar> {
+
+	@Override
+	public void contribute(Document document, Calendar calendar) {
+		document.addLocalizedKeyword(
+			Field.DESCRIPTION, calendar.getDescriptionMap(), true);
+		document.addLocalizedKeyword(Field.NAME, calendar.getNameMap(), true);
+		document.addKeyword("calendarId", calendar.getCalendarId());
+
+		Locale defaultLocale = LocaleUtil.getSiteDefault();
+
+		String defaultLanguageId = LocaleUtil.toLanguageId(defaultLocale);
+
+		document.addText("defaultLanguageId", defaultLanguageId);
+
+		CalendarResource calendarResource =
+			calendarResourceLocalService.fetchCalendarResource(
+				calendar.getCalendarResourceId());
+
+		if (calendarResource != null) {
+			document.addLocalizedKeyword(
+				CalendarField.RESOURCE_NAME, calendarResource.getNameMap(),
+				true);
+		}
+	}
+
+	@Reference
+	protected CalendarResourceLocalService calendarResourceLocalService;
+
+}
---
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/search/CalendarModelIndexerWriterContributor.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.internal.search;
+
+import com.liferay.calendar.model.Calendar;
+import com.liferay.calendar.service.CalendarLocalService;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.calendar.model.Calendar",
+	service = ModelIndexerWriterContributor.class
+)
+public class CalendarModelIndexerWriterContributor
+	implements ModelIndexerWriterContributor<Calendar> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(Calendar calendar) -> {
+				batchIndexingActionable.addDocuments(
+					modelIndexerWriterDocumentHelper.getDocument(calendar));
+			});
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				calendarLocalService.getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(Calendar calendar) {
+		return calendar.getCompanyId();
+	}
+
+	@Override
+	public void modelIndexed(Calendar calendar) {
+		calendarBookingBatchReindexer.reindex(
+			calendar.getCalendarId(), calendar.getCompanyId());
+	}
+
+	@Reference
+	protected CalendarBookingBatchReindexer calendarBookingBatchReindexer;
+
+	@Reference
+	protected CalendarLocalService calendarLocalService;
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+}
