vicnate5/com-liferay-portal-workflow

LPS-73735 Replace description for title in advanced search.

portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/display/context/WorkflowDefinitionDisplayContext.java
@@ -33,8 +33,8 @@ import com.liferay.portal.kernel.workflow.WorkflowDefinitionManagerUtil;
 import com.liferay.portal.workflow.web.internal.display.context.util.WorkflowDefinitionRequestHelper;
 import com.liferay.portal.workflow.web.internal.search.WorkflowDefinitionSearchTerms;
 import com.liferay.portal.workflow.web.internal.util.WorkflowDefinitionPortletUtil;
+import com.liferay.portal.workflow.web.internal.util.filter.WorkflowDefinitionDescriptionPredicateFilter;
 import com.liferay.portal.workflow.web.internal.util.filter.WorkflowDefinitionNamePredicateFilter;
-import com.liferay.portal.workflow.web.internal.util.filter.WorkflowDefinitionTitlePredicateFilter;
 
 import java.util.List;
 
@@ -90,7 +90,7 @@ public class WorkflowDefinitionDisplayContext {
 		if (searchTerms.isAdvancedSearch()) {
 			workflowDefinitions = filter(
 				workflowDefinitions, searchTerms.getName(),
-				searchTerms.getTitle(), searchTerms.isAndOperator());
+				searchTerms.getDescription(), searchTerms.isAndOperator());
 		}
 		else {
 			workflowDefinitions = filter(
@@ -144,7 +144,7 @@ public class WorkflowDefinitionDisplayContext {
 	}
 
 	protected PredicateFilter<WorkflowDefinition> createPredicateFilter(
-		String name, String title, boolean andOperator) {
+		String name, String description, boolean andOperator) {
 
 		AggregatePredicateFilter<WorkflowDefinition> aggregatePredicateFilter =
 			new AggregatePredicateFilter<>(
@@ -152,26 +152,26 @@ public class WorkflowDefinitionDisplayContext {
 
 		if (andOperator) {
 			aggregatePredicateFilter.and(
-				new WorkflowDefinitionTitlePredicateFilter(title));
+				new WorkflowDefinitionDescriptionPredicateFilter(description));
 		}
 		else {
 			aggregatePredicateFilter.or(
-				new WorkflowDefinitionTitlePredicateFilter(title));
+				new WorkflowDefinitionDescriptionPredicateFilter(description));
 		}
 
 		return aggregatePredicateFilter;
 	}
 
 	protected List<WorkflowDefinition> filter(
-		List<WorkflowDefinition> workflowDefinitions, String name, String title,
-		boolean andOperator) {
+		List<WorkflowDefinition> workflowDefinitions, String name,
+		String description, boolean andOperator) {
 
-		if (Validator.isNull(name) && Validator.isNull(title)) {
+		if (Validator.isNull(name) && Validator.isNull(description)) {
 			return workflowDefinitions;
 		}
 
 		PredicateFilter<WorkflowDefinition> predicateFilter =
-			createPredicateFilter(name, title, andOperator);
+			createPredicateFilter(name, description, andOperator);
 
 		return ListUtil.filter(workflowDefinitions, predicateFilter);
 	}

portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/search/WorkflowDefinitionSearch.java
@@ -18,7 +18,9 @@ import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.workflow.WorkflowDefinition;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
@@ -32,10 +34,13 @@ public class WorkflowDefinitionSearch
 	public static final String EMPTY_RESULTS_MESSAGE = "no-entries-were-found";
 
 	public static List<String> headerNames = new ArrayList<>();
+	public static Map<String, String> orderableHeaders = new HashMap<>();
 
 	static {
 		headerNames.add("name");
-		headerNames.add("title");
+		headerNames.add("description");
+
+		orderableHeaders.put("name", "name");
 	}
 
 	public WorkflowDefinitionSearch(
@@ -53,7 +58,10 @@ public class WorkflowDefinitionSearch
 		iteratorURL.setParameter(
 			WorkflowDefinitionDisplayTerms.NAME, displayTerms.getName());
 		iteratorURL.setParameter(
-			WorkflowDefinitionDisplayTerms.TITLE, displayTerms.getTitle());
+			WorkflowDefinitionDisplayTerms.DESCRIPTION,
+			displayTerms.getDescription());
+
+		setOrderableHeaders(orderableHeaders);
 	}
 
 }

zoltantoth0409/liferay-portal

modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/display/context/WorkflowDefinitionDisplayContext.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.workflow.WorkflowDefinitionManagerUtil;
 import com.liferay.portal.workflow.web.internal.display.context.util.WorkflowDefinitionRequestHelper;
 import com.liferay.portal.workflow.web.internal.search.WorkflowDefinitionSearchTerms;
 import com.liferay.portal.workflow.web.internal.util.WorkflowDefinitionPortletUtil;
+import com.liferay.portal.workflow.web.internal.util.filter.WorkflowDefinitionDescriptionPredicateFilter;
 import com.liferay.portal.workflow.web.internal.util.filter.WorkflowDefinitionNamePredicateFilter;
 import com.liferay.portal.workflow.web.internal.util.filter.WorkflowDefinitionTitlePredicateFilter;
 
@@ -89,7 +90,7 @@ public class WorkflowDefinitionDisplayContext {
 
 		if (searchTerms.isAdvancedSearch()) {
 			workflowDefinitions = filter(
-				workflowDefinitions, searchTerms.getName(),
+				workflowDefinitions, searchTerms.getDescription(),
 				searchTerms.getTitle(), searchTerms.isAndOperator());
 		}
 		else {
@@ -144,34 +145,34 @@ public class WorkflowDefinitionDisplayContext {
 	}
 
 	protected PredicateFilter<WorkflowDefinition> createPredicateFilter(
-		String name, String title, boolean andOperator) {
+		String description, String title, boolean andOperator) {
 
 		AggregatePredicateFilter<WorkflowDefinition> aggregatePredicateFilter =
 			new AggregatePredicateFilter<>(
-				new WorkflowDefinitionNamePredicateFilter(name));
+				new WorkflowDefinitionTitlePredicateFilter(title));
 
 		if (andOperator) {
 			aggregatePredicateFilter.and(
-				new WorkflowDefinitionTitlePredicateFilter(title));
+				new WorkflowDefinitionDescriptionPredicateFilter(description));
 		}
 		else {
 			aggregatePredicateFilter.or(
-				new WorkflowDefinitionTitlePredicateFilter(title));
+				new WorkflowDefinitionDescriptionPredicateFilter(description));
 		}
 
 		return aggregatePredicateFilter;
 	}
 
 	protected List<WorkflowDefinition> filter(
-		List<WorkflowDefinition> workflowDefinitions, String name, String title,
-		boolean andOperator) {
+		List<WorkflowDefinition> workflowDefinitions, String description,
+		String title, boolean andOperator) {
 
-		if (Validator.isNull(name) && Validator.isNull(title)) {
+		if (Validator.isNull(title) && Validator.isNull(description)) {
 			return workflowDefinitions;
 		}
 
 		PredicateFilter<WorkflowDefinition> predicateFilter =
-			createPredicateFilter(name, title, andOperator);
+			createPredicateFilter(description, title, andOperator);
 
 		return ListUtil.filter(workflowDefinitions, predicateFilter);
 	}

modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/search/WorkflowDefinitionSearch.java
@@ -18,7 +18,9 @@ import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.workflow.WorkflowDefinition;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
@@ -32,10 +34,13 @@ public class WorkflowDefinitionSearch
 	public static final String EMPTY_RESULTS_MESSAGE = "no-entries-were-found";
 
 	public static List<String> headerNames = new ArrayList<>();
+	public static Map<String, String> orderableHeaders = new HashMap<>();
 
 	static {
-		headerNames.add("name");
+		headerNames.add("description");
 		headerNames.add("title");
+
+		orderableHeaders.put("title", "modifiedDate");
 	}
 
 	public WorkflowDefinitionSearch(
@@ -51,9 +56,12 @@ public class WorkflowDefinitionSearch
 			(WorkflowDefinitionDisplayTerms)getDisplayTerms();
 
 		iteratorURL.setParameter(
-			WorkflowDefinitionDisplayTerms.NAME, displayTerms.getName());
+			WorkflowDefinitionDisplayTerms.DESCRIPTION,
+			displayTerms.getDescription());
 		iteratorURL.setParameter(
 			WorkflowDefinitionDisplayTerms.TITLE, displayTerms.getTitle());
+
+		setOrderableHeaders(orderableHeaders);
 	}
 
 }