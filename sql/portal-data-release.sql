7869aebcf5c68049cc31b428b72be7bfb9c0adce
Alberto Chaparro <alberto.chaparro@liferay.com>
2017-09-15 19:51:53 +0200

    LPS-73481 Add version in sql for new installations

--- sql/portal-data-release.sql
+++ sql/portal-data-release.sql
@@ -1,1 +1,1 @@
-insert into Release_ (releaseId, createDate, modifiedDate, servletContextName, schemaVersion, buildNumber, verified) values (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'portal', '7.0.4', 7004, FALSE);
+insert into Release_ (releaseId, createDate, modifiedDate, servletContextName, schemaVersion, buildNumber, verified) values (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'portal', '7.1.0', 7100, FALSE);
