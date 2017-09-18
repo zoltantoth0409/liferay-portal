22d31448a59d3e57eebe90c583bb5dde061b93ca
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-09-18 11:01:50 +0200

    LPS-74640 Remove files, so they can be auto generated with service.xml changes

--- modules/apps/web-experience/fragment/fragment-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,5 +0,0 @@
-create unique index IX_F0F44819 on MSBFragmentCollection (groupId, name[$COLUMN_LENGTH:75$]);
-
-create index IX_F131CCB6 on MSBFragmentEntry (groupId, msbFragmentCollectionId, name[$COLUMN_LENGTH:75$]);
-create unique index IX_30585F1D on MSBFragmentEntry (groupId, name[$COLUMN_LENGTH:75$]);
-create index IX_8DA20B89 on MSBFragmentEntry (msbFragmentCollectionId);
