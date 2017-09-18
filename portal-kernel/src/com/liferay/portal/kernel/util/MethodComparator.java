1eb012b2e38c53c97723211cf82ac7ea6cfd21c0
Preston-Crary <preston.crary@liferay.com>
2017-09-18 16:31:31 -0700

    LPS-74775 Deprecate MethodComparator as there are no usages.

--- portal-kernel/src/com/liferay/portal/kernel/util/MethodComparator.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/MethodComparator.java
@@ -21,7 +21,9 @@ import java.util.Comparator;
 /**
  * @author Shuyang Zhou
  * @author Brian Wing Shun Chan
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class MethodComparator implements Comparator<Method> {
 
 	@Override
