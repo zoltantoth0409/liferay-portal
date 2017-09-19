3118794af03bb9e87e568685f6da9f38520d7ead
Marco Leo <marco.leo@liferay.com>
2017-09-19 15:00:32 +0200

    COMMERCE-163 Add new is visible method

--- modules/dxp/apps/commerce/commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/util/CommerceCheckoutStep.java
+++ modules/dxp/apps/commerce/commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/util/CommerceCheckoutStep.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -14,24 +14,36 @@
 
 package com.liferay.commerce.checkout.web.util;
 
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Locale;
 
 /**
  * @author Marco Leo
  */
 public interface CommerceCheckoutStep {
 
+	public boolean action(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception;
+
 	public String getLabel(Locale locale);
 
-	public void render(
-		HttpServletRequest httpServletRequest,
-		HttpServletResponse httpServletResponse)
+	public boolean isActive(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
 		throws Exception;
 
-	public void action(
-		HttpServletRequest httpServletRequest,
-		HttpServletResponse httpServletResponse)
+	public boolean isVisible(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
 		throws Exception;
+
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception;
+
 }
