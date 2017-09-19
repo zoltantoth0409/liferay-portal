44492cf21b7786b007399a5a1b40b68124446a0e
Inacio Nery <inacio.nery@liferay.com>
2017-09-19 13:50:18 -0300

    LPS-74684 Create an integration test

--- modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CalendarWorkflowTestUtil.java
+++ modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CalendarWorkflowTestUtil.java
@@ -15,10 +15,26 @@
 package com.liferay.calendar.test.util;
 
 import com.liferay.calendar.model.CalendarBooking;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalServiceUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.workflow.WorkflowTask;
+import com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil;
+import com.liferay.portal.test.log.CaptureAppender;
+import com.liferay.portal.test.log.Log4JLoggerTestUtil;
+
+import java.util.List;
+
+import org.apache.log4j.Level;
+
+import org.junit.Assert;
 
 /**
  * @author Inácio Nery
@@ -32,10 +48,39 @@ public class CalendarWorkflowTestUtil {
 			"Single Approver", 1);
 	}
 
-	public static void deactivateWorkflow(Group group) throws PortalException {
-		WorkflowDefinitionLinkLocalServiceUtil.updateWorkflowDefinitionLink(
-			TestPropsValues.getUserId(), TestPropsValues.getCompanyId(),
-			group.getGroupId(), CalendarBooking.class.getName(), 0, 0, null);
+	public static void completeWorkflow(Group group) throws Exception {
+		try (CaptureAppender captureAppender =
+				Log4JLoggerTestUtil.configureLog4JLogger(
+					_MAIL_ENGINE_CLASS_NAME, Level.OFF)) {
+
+			List<WorkflowTask> workflowTasks =
+				WorkflowTaskManagerUtil.getWorkflowTasksByUserRoles(
+					TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
+					false, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+			Assert.assertEquals(
+				workflowTasks.toString(), 1, workflowTasks.size());
+
+			WorkflowTask workflowTask = workflowTasks.get(0);
+
+			PermissionChecker userPermissionChecker =
+				PermissionCheckerFactoryUtil.create(TestPropsValues.getUser());
+
+			PermissionThreadLocal.setPermissionChecker(userPermissionChecker);
+
+			WorkflowTaskManagerUtil.assignWorkflowTaskToUser(
+				group.getCompanyId(), TestPropsValues.getUserId(),
+				workflowTask.getWorkflowTaskId(), TestPropsValues.getUserId(),
+				StringPool.BLANK, null, null);
+
+			WorkflowTaskManagerUtil.completeWorkflowTask(
+				group.getCompanyId(), TestPropsValues.getUserId(),
+				workflowTask.getWorkflowTaskId(), Constants.APPROVE,
+				StringPool.BLANK, null);
+		}
 	}
+
+	private static final String _MAIL_ENGINE_CLASS_NAME =
+		"com.liferay.util.mail.MailEngine";
 
 }
