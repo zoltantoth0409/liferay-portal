0d7d5cd68803fccb579ba354ca92903c1e85cb70
Brian Chan <brian.chan@liferay.com>
2017-09-19 15:24:52 -0700

    LPS-73290 Are these sorted?

--- modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/data/handler/FileEntryStagedModelDataHandler.java
+++ modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/data/handler/FileEntryStagedModelDataHandler.java
@@ -256,8 +256,8 @@ public class FileEntryStagedModelDataHandler
 
 		FileVersion fileVersion = fileEntry.getFileVersion();
 
-		fileEntryElement.addAttribute("version", fileEntry.getVersion());
 		fileEntryElement.addAttribute("fileVersionUuid", fileVersion.getUuid());
+		fileEntryElement.addAttribute("version", fileEntry.getVersion());
 
 		LiferayFileEntry liferayFileEntry = (LiferayFileEntry)fileEntry;
 
