860989d7614d4157c08126abbbec2d4682466328
Marco Leo <marco.leo@liferay.com>
2017-09-19 15:01:34 +0200

    COMMERCE-163 Add default action command

---
+++ modules/dxp/apps/commerce/commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/internal/portlet/action/SaveStepMVCActionCommand.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.checkout.web.internal.portlet.action;
+
+import com.liferay.commerce.checkout.web.util.CommerceCheckoutStep;
+import com.liferay.commerce.checkout.web.util.CommerceCheckoutStepServicesTracker;
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.service.CommerceCartService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CommercePortletKeys.COMMERCE_CHECKOUT,
+		"mvc.command.name=saveStep"
+	},
+	service = MVCActionCommand.class
+)
+public class SaveStepMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			actionRequest);
+		HttpServletResponse httpServletResponse =
+			_portal.getHttpServletResponse(actionResponse);
+
+		String checkoutStepName = ParamUtil.getString(
+			actionRequest, "checkoutStepName");
+
+		CommerceCheckoutStep commerceCheckoutStep =
+			_commerceCheckoutStepServicesTracker.getCommerceCheckoutStep(
+				checkoutStepName);
+
+		commerceCheckoutStep.action(httpServletRequest, httpServletResponse);
+
+		hideDefaultSuccessMessage(actionRequest);
+	}
+
+	@Reference
+	private CommerceCartService _commerceCartService;
+
+	@Reference
+	private CommerceCheckoutStepServicesTracker
+		_commerceCheckoutStepServicesTracker;
+
+	@Reference
+	private Portal _portal;
+
+}
