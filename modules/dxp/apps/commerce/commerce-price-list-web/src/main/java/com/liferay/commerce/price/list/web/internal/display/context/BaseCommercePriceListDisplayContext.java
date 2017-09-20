44b58fed5492c21becd9a15155bb3a6f03833779
Alessio Rendina <asilcomando@gmail.com>
2017-09-20 12:42:37 +0200

    COMMERCE-241 Added base display context

---
+++ modules/dxp/apps/commerce/commerce-price-list-web/src/main/java/com/liferay/commerce/price/list/web/internal/display/context/BaseCommercePriceListDisplayContext.java
@@ -0,0 +1,189 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.price.list.web.internal.display.context;
+
+import com.liferay.commerce.model.CommercePriceList;
+import com.liferay.commerce.price.list.web.internal.portlet.action.ActionHelper;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.dao.search.RowChecker;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public abstract class BaseCommercePriceListDisplayContext<T> {
+
+	public BaseCommercePriceListDisplayContext(
+		ActionHelper actionHelper, RenderRequest renderRequest,
+		RenderResponse renderResponse) {
+
+		this.actionHelper = actionHelper;
+		this.renderRequest = renderRequest;
+		this.renderResponse = renderResponse;
+
+		_defaultOrderByCol = "create-date";
+		_defaultOrderByType = "desc";
+	}
+
+	public CommercePriceList getCommercePriceList() throws PortalException {
+		if (_commercePriceList != null) {
+			return _commercePriceList;
+		}
+
+		_commercePriceList = actionHelper.getCommercePriceList(renderRequest);
+
+		return _commercePriceList;
+	}
+
+	public long getCommercePriceListId() throws PortalException {
+		CommercePriceList commercePriceList = getCommercePriceList();
+
+		if (commercePriceList == null) {
+			return 0;
+		}
+
+		return commercePriceList.getCommercePriceListId();
+	}
+
+	public String getKeywords() {
+		if (_keywords != null) {
+			return _keywords;
+		}
+
+		_keywords = ParamUtil.getString(renderRequest, "keywords");
+
+		return _keywords;
+	}
+
+	public String getOrderByCol() {
+		return ParamUtil.getString(
+			renderRequest, SearchContainer.DEFAULT_ORDER_BY_COL_PARAM,
+			_defaultOrderByCol);
+	}
+
+	public String getOrderByType() {
+		return ParamUtil.getString(
+			renderRequest, SearchContainer.DEFAULT_ORDER_BY_TYPE_PARAM,
+			_defaultOrderByType);
+	}
+
+	public PortletURL getPortletURL() throws PortalException {
+		PortletURL portletURL = renderResponse.createRenderURL();
+
+		CommercePriceList commercePriceList = getCommercePriceList();
+
+		if (commercePriceList != null) {
+			portletURL.setParameter(
+				"commercePriceListId",
+				String.valueOf(getCommercePriceListId()));
+		}
+
+		String delta = ParamUtil.getString(renderRequest, "delta");
+
+		if (Validator.isNotNull(delta)) {
+			portletURL.setParameter("delta", delta);
+		}
+
+		String deltaEntry = ParamUtil.getString(renderRequest, "deltaEntry");
+
+		if (Validator.isNotNull(deltaEntry)) {
+			portletURL.setParameter("deltaEntry", deltaEntry);
+		}
+
+		String keywords = ParamUtil.getString(renderRequest, "keywords");
+
+		if (Validator.isNotNull(keywords)) {
+			portletURL.setParameter("keywords", keywords);
+		}
+
+		String orderByCol = getOrderByCol();
+
+		if (Validator.isNotNull(orderByCol)) {
+			portletURL.setParameter("orderByCol", orderByCol);
+		}
+
+		String orderByType = getOrderByType();
+
+		if (Validator.isNotNull(orderByType)) {
+			portletURL.setParameter("orderByType", orderByType);
+		}
+
+		String redirect = ParamUtil.getString(renderRequest, "redirect");
+
+		if (Validator.isNotNull(redirect)) {
+			portletURL.setParameter("redirect", redirect);
+		}
+
+		return portletURL;
+	}
+
+	public RowChecker getRowChecker() {
+		if (_rowChecker == null) {
+			_rowChecker = new EmptyOnClickRowChecker(renderResponse);
+		}
+
+		return _rowChecker;
+	}
+
+	public abstract SearchContainer<T> getSearchContainer()
+		throws PortalException;
+
+	public boolean isSearch() {
+		if (Validator.isNotNull(getKeywords())) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isShowInfoPanel() {
+		if (isSearch()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public void setDefaultOrderByCol(String defaultOrderByCol) {
+		_defaultOrderByCol = defaultOrderByCol;
+	}
+
+	public void setDefaultOrderByType(String defaultOrderByType) {
+		_defaultOrderByType = defaultOrderByType;
+	}
+
+	protected String getNavigation() {
+		return ParamUtil.getString(renderRequest, "navigation");
+	}
+
+	protected final ActionHelper actionHelper;
+	protected final RenderRequest renderRequest;
+	protected final RenderResponse renderResponse;
+	protected SearchContainer<T> searchContainer;
+
+	private CommercePriceList _commercePriceList;
+	private String _defaultOrderByCol;
+	private String _defaultOrderByType;
+	private String _keywords;
+	private RowChecker _rowChecker;
+
+}
