67d5fe89aef44bb6ea065a81a559997192cc3d41
Alec Shay <Alec.Shay@liferay.com>
2017-09-20 10:23:29 -0700

    LPS-74810 Set addDefaultResource to true for RolesAdminPortlet so users with permission may view it (gain access through portlet authentication token)

--- modules/apps/foundation/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/portlet/RolesAdminPortlet.java
+++ modules/apps/foundation/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/portlet/RolesAdminPortlet.java
@@ -94,6 +94,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
+		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-users-admin",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
