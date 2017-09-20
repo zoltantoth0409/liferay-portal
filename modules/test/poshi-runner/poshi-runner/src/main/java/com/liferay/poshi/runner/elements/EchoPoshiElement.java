49cfd8361ef840fb054563bdb7b1b5c7a0b0030e
Kenji Heigel <kenji.heigel@liferay.com>
2017-09-20 16:33:46 -0700

    LRQA-32801 Add support for 'echo' elements

---
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/EchoPoshiElement.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.elements;
+
+import org.dom4j.Element;
+
+/**
+ * @author Kenji Heigel
+ */
+public class EchoPoshiElement extends BasePoshiElement {
+
+	@Override
+	public PoshiElement clone(Element element) {
+		if (isElementType(_ELEMENT_NAME, element)) {
+			return new EchoPoshiElement(element);
+		}
+
+		return null;
+	}
+
+	@Override
+	public PoshiElement clone(
+		PoshiElement parentPoshiElement, String readableSyntax) {
+
+		if (_isElementType(readableSyntax)) {
+			return new EchoPoshiElement(readableSyntax);
+		}
+
+		return null;
+	}
+
+	@Override
+	public void parseReadableSyntax(String readableSyntax) {
+		String content = getQuotedContent(readableSyntax);
+
+		addAttribute("message", content);
+	}
+
+	@Override
+	public String toReadableSyntax() {
+		String message = attributeValue("message");
+
+		return createReadableBlock(message);
+	}
+
+	protected EchoPoshiElement() {
+	}
+
+	protected EchoPoshiElement(Element element) {
+		super(_ELEMENT_NAME, element);
+	}
+
+	protected EchoPoshiElement(String readableSyntax) {
+		super(_ELEMENT_NAME, readableSyntax);
+	}
+
+	@Override
+	protected String createReadableBlock(String content) {
+		StringBuilder sb = new StringBuilder();
+
+		String blockName = getBlockName();
+		String pad = getPad();
+
+		sb.append("\n\n");
+		sb.append(pad);
+		sb.append(blockName);
+		sb.append("(\"");
+
+		String trimmedContent = content.trim();
+
+		sb.append(trimmedContent);
+
+		sb.append("\");");
+
+		return sb.toString();
+	}
+
+	@Override
+	protected String getBlockName() {
+		return "echo";
+	}
+
+	private boolean _isElementType(String readableSyntax) {
+		readableSyntax = readableSyntax.trim();
+
+		if (!isBalancedReadableSyntax(readableSyntax)) {
+			return false;
+		}
+
+		if (!readableSyntax.endsWith(");")) {
+			return false;
+		}
+
+		if (!readableSyntax.startsWith("echo(")) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private static final String _ELEMENT_NAME = "echo";
+
+}
