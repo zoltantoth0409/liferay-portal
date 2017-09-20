b894e87e8a0bc1ed1bab61aebae0e9d7c034d04b
shuyangzhou <shuyang.zhou@liferay.com>
2017-09-19 20:26:18 -0700

    LPS-74796 Add context info into Exception message

--- modules/apps/foundation/portal/portal-spring-extender/src/main/java/com/liferay/portal/spring/extender/internal/context/ModuleApplicationContextExtender.java
+++ modules/apps/foundation/portal/portal-spring-extender/src/main/java/com/liferay/portal/spring/extender/internal/context/ModuleApplicationContextExtender.java
@@ -306,22 +306,43 @@ public class ModuleApplicationContextExtender extends AbstractExtender {
 							"sequences.sql");
 						String indexesSQL = getSQLTemplateString("indexes.sql");
 
-						try {
-							if (tablesSQL != null) {
+						if (tablesSQL != null) {
+							try {
 								db.runSQLTemplateString(tablesSQL, true, true);
 							}
+							catch (Exception e) {
+								throw new UpgradeException(
+									"Unable to run tablesSQL: \"" + tablesSQL +
+										"\" for bundle: " + _bundle,
+									e);
+							}
+						}
 
-							if (sequencesSQL != null) {
+						if (sequencesSQL != null) {
+							try {
 								db.runSQLTemplateString(
 									sequencesSQL, true, true);
 							}
-
-							if (indexesSQL != null) {
-								db.runSQLTemplateString(indexesSQL, true, true);
+							catch (Exception e) {
+								throw new UpgradeException(
+									"Unable to run sequencesSQL: \"" +
+										sequencesSQL + "\" for bundle: " +
+											_bundle,
+									e);
 							}
 						}
-						catch (Exception e) {
-							throw new UpgradeException(e);
+
+						if (indexesSQL != null) {
+							try {
+								db.runSQLTemplateString(indexesSQL, true, true);
+							}
+							catch (Exception e) {
+								throw new UpgradeException(
+									"Unable to run indexesSQL: \"" +
+										indexesSQL + "\" for bundle: " +
+											_bundle,
+									e);
+							}
 						}
 					}
 
