8fb138396ec00d9b94648e24f560094a84faf0c2
shuyangzhou <shuyang.zhou@liferay.com>
2017-09-21 15:29:53 -0700

    LPS-74897 Remove deprecated methods

--- modules/apps/foundation/petra/petra-lang/src/main/java/com/liferay/portal/kernel/util/AutoResetThreadLocal.java
+++ modules/apps/foundation/petra/petra-lang/src/main/java/com/liferay/portal/kernel/util/AutoResetThreadLocal.java
@@ -29,14 +29,4 @@ public class AutoResetThreadLocal<T> extends InitialThreadLocal<T> {
 		super(name, supplier, true);
 	}
 
-	/**
-	 * @deprecated As of 7.0.0, replaced by {@link #AutoResetThreadLocal(String,
-	 *             Supplier)}
-	 */
-	@Deprecated
-	@SuppressWarnings("deprecation")
-	public AutoResetThreadLocal(String name, T initialValue) {
-		super(name, initialValue, true);
-	}
-
 }
