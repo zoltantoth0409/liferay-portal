26232d9cf70bfa0699f2ac43871d2fcbb2e473cf
Pavel Savinov <pavel.savinov@liferay.com>
2017-09-21 10:35:25 +0200

    LPS-74653 Use highlighted snippet for the search summary

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/search/JournalArticleIndexer.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/search/JournalArticleIndexer.java
@@ -765,7 +765,16 @@ public class JournalArticleIndexer
 				LocaleUtil.toLanguageId(snippetLocale), 1, portletRequestModel,
 				themeDisplay);
 
-			content = HtmlUtil.stripHtml(articleDisplay.getDescription());
+			String description = document.get(
+				snippetLocale,
+				Field.SNIPPET + StringPool.UNDERLINE + Field.DESCRIPTION,
+				Field.DESCRIPTION);
+
+			if (Validator.isNull(description)) {
+				description = articleDisplay.getDescription();
+			}
+
+			content = _stripAndHighlight(description);
 
 			content = HtmlUtil.replaceNewLine(content);
 
@@ -950,6 +959,24 @@ public class JournalArticleIndexer
 	protected void setJournalConverter(JournalConverter journalConverter) {
 		_journalConverter = journalConverter;
 	}
+
+	private String _stripAndHighlight(String text) {
+		text = StringUtil.replace(
+			text, _HIGHLIGHT_TAGS, _ESCAPE_SAFE_HIGHLIGHTS);
+
+		text = HtmlUtil.stripHtml(text);
+
+		text = StringUtil.replace(
+			text, _ESCAPE_SAFE_HIGHLIGHTS, _HIGHLIGHT_TAGS);
+
+		return text;
+	}
+
+	private static final String[] _ESCAPE_SAFE_HIGHLIGHTS =
+		{"[@HIGHLIGHT1@]", "[@HIGHLIGHT2@]"};
+
+	private static final String[] _HIGHLIGHT_TAGS =
+		{HighlightUtil.HIGHLIGHT_TAG_OPEN, HighlightUtil.HIGHLIGHT_TAG_CLOSE};
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		JournalArticleIndexer.class);
