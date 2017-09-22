24a35188aaf9aab3bdfbf23bda8a199f10e5f712
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-09-22 13:57:51 -0700

    LPS-74448 Simplify

--- modules/sdk/project-templates-war-hook/src/main/resources/archetype-resources/src/main/java/__className__StartupAction.java
+++ modules/sdk/project-templates-war-hook/src/main/resources/archetype-resources/src/main/java/__className__StartupAction.java
@@ -3,8 +3,6 @@ package ${package};
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.SimpleAction;
 
-import java.util.stream.Stream;
-
 /**
  * @author ${author}
  */
@@ -12,13 +10,9 @@ public class ${className}StartupAction extends SimpleAction {
 
 	@Override
 	public void run(String[] lifecycleEventIds) throws ActionException {
-		Stream<String> lifecycleEventIdsStream = Stream.of(lifecycleEventIds);
-
-		lifecycleEventIdsStream.map(
-			id -> "startup event id " + id
-		).forEach(
-			System.out::println
-		);
+		for (String eventId : lifecycleEventIds) {
+			System.out.println("Startup event ID " + eventId);
+		}
 	}
 
 }
