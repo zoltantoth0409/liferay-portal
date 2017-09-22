cc3880231c9858a31860ec59a74452e5970e8f11
Alessio Rendina <asilcomando@gmail.com>
2017-09-22 14:45:26 +0200

    COMMERCE-241 Moved all methods from CommercePriceListPortletUtil into CommerceUtil

--- modules/dxp/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/util/CommerceUtil.java
+++ modules/dxp/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/util/CommerceUtil.java
@@ -14,11 +14,15 @@
 
 package com.liferay.commerce.util;
 
+import com.liferay.commerce.model.CommercePriceEntry;
 import com.liferay.commerce.model.CommercePriceList;
+import com.liferay.commerce.model.CommerceTierPriceEntry;
 import com.liferay.commerce.model.CommerceWarehouse;
 import com.liferay.commerce.model.CommerceWarehouseItem;
+import com.liferay.commerce.util.comparator.CommercePriceEntryCreateDateComparator;
 import com.liferay.commerce.util.comparator.CommercePriceListCreateDateComparator;
 import com.liferay.commerce.util.comparator.CommercePriceListDisplayDateComparator;
+import com.liferay.commerce.util.comparator.CommerceTierPriceEntryCreateDateComparator;
 import com.liferay.commerce.util.comparator.CommerceWarehouseCityComparator;
 import com.liferay.commerce.util.comparator.CommerceWarehouseItemQuantityComparator;
 import com.liferay.commerce.util.comparator.CommerceWarehouseItemWarehouseNameComparator;
@@ -32,8 +36,130 @@ import java.util.Objects;
 
 /**
  * @author Andrea Di Giorgi
+ * @author Alessio Antonio Rendina
  */
 public class CommerceUtil {
+
+	public static OrderByComparator<CommercePriceEntry>
+		getCommercePriceEntryOrderByComparator(
+			String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<CommercePriceEntry> orderByComparator = null;
+
+		if (orderByCol.equals("create-date")) {
+			orderByComparator = new CommercePriceEntryCreateDateComparator(
+				orderByAsc);
+		}
+
+		return orderByComparator;
+	}
+
+	public static Sort getCommercePriceEntrySort(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (Objects.equals(orderByType, "asc")) {
+			orderByAsc = true;
+		}
+
+		Sort sort = null;
+
+		if (orderByCol.equals("create-date")) {
+			sort = SortFactoryUtil.create(Field.CREATE_DATE, orderByAsc);
+		}
+
+		return sort;
+	}
+
+	public static OrderByComparator<CommercePriceList>
+		getCommercePriceListOrderByComparator(
+			String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<CommercePriceList> orderByComparator = null;
+
+		if (orderByCol.equals("create-date")) {
+			orderByComparator = new CommercePriceListCreateDateComparator(
+				orderByAsc);
+		}
+		else if (orderByCol.equals("display-date")) {
+			orderByComparator = new CommercePriceListDisplayDateComparator(
+				orderByAsc);
+		}
+
+		return orderByComparator;
+	}
+
+	public static Sort getCommercePriceListSort(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (Objects.equals(orderByType, "asc")) {
+			orderByAsc = true;
+		}
+
+		Sort sort = null;
+
+		if (orderByCol.equals("create-date")) {
+			sort = SortFactoryUtil.create(Field.CREATE_DATE, orderByAsc);
+		}
+		else if (orderByCol.equals("display-date")) {
+			sort = SortFactoryUtil.create("display-date", orderByAsc);
+		}
+
+		return sort;
+	}
+
+	public static OrderByComparator<CommerceTierPriceEntry>
+		getCommerceTierPriceEntryOrderByComparator(
+			String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<CommerceTierPriceEntry> orderByComparator = null;
+
+		if (orderByCol.equals("create-date")) {
+			orderByComparator = new CommerceTierPriceEntryCreateDateComparator(
+				orderByAsc);
+		}
+
+		return orderByComparator;
+	}
+
+	public static Sort getCommerceTierPriceEntrySort(
+		String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (Objects.equals(orderByType, "asc")) {
+			orderByAsc = true;
+		}
+
+		Sort sort = null;
+
+		if (orderByCol.equals("create-date")) {
+			sort = SortFactoryUtil.create(Field.CREATE_DATE, orderByAsc);
+		}
+
+		return sort;
+	}
 
 	public static OrderByComparator<CommerceWarehouseItem>
 		getCommerceWarehouseItemOrderByComparator(
@@ -79,49 +205,6 @@ public class CommerceUtil {
 		}
 
 		return orderByComparator;
-	}
-
-	public static OrderByComparator<CommercePriceList>
-		getCommercePriceListOrderByComparator(
-			String orderByCol, String orderByType) {
-
-		boolean orderByAsc = false;
-
-		if (orderByType.equals("asc")) {
-			orderByAsc = true;
-		}
-
-		OrderByComparator<CommercePriceList> orderByComparator = null;
-
-		if (orderByCol.equals("create-date")) {
-			orderByComparator = new CommercePriceListCreateDateComparator(orderByAsc);
-		}
-		else if (orderByCol.equals("display-date")) {
-			orderByComparator = new CommercePriceListDisplayDateComparator(orderByAsc);
-		}
-
-		return orderByComparator;
-	}
-
-	public static Sort getCommercePriceListSort(
-		String orderByCol, String orderByType) {
-
-		boolean orderByAsc = false;
-
-		if (Objects.equals(orderByType, "asc")) {
-			orderByAsc = true;
-		}
-
-		Sort sort = null;
-
-		if (orderByCol.equals("create-date")) {
-			sort = SortFactoryUtil.create(Field.CREATE_DATE, orderByAsc);
-		}
-		else if (orderByCol.equals("display-date")) {
-			sort = SortFactoryUtil.create("display-date", orderByAsc);
-		}
-
-		return sort;
 	}
 
 }
