f45b4e07a129997b47a97a33db768030be579fa2
Chema Balsas <jose.balsas@liferay.com>
2017-09-22 10:31:37 +0200

    LPS-74526 Excludes *.es.js files from checks since they are formatted and checked independently

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JSSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JSSourceProcessor.java
@@ -24,9 +24,9 @@ public class JSSourceProcessor extends BaseSourceProcessor {
 	@Override
 	protected List<String> doGetFileNames() throws Exception {
 		String[] excludes = {
-			"**/*.min.js", "**/*.nocsf.js", "**/*.soy.js", "**/aui/**",
-			"**/jquery/**", "**/lodash/**", "**/misc/**", "**/r2.js",
-			"**/tools/**"
+			"**/*.es.js", "**/*.min.js", "**/*.nocsf.js", "**/*.soy.js",
+			"**/aui/**", "**/jquery/**", "**/lodash/**", "**/misc/**",
+			"**/r2.js", "**/tools/**"
 		};
 
 		return getFileNames(excludes, getIncludes());
