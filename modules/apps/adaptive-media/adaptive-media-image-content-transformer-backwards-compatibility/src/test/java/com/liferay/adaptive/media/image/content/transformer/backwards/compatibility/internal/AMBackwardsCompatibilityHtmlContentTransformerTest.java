e58f3a31f9ed5148efd87f693c76d99d8b0d9528
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-09-25 17:56:13 +0200

    LPS-74842 Fix compilation issue

--- modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/test/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformerTest.java
+++ modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/test/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformerTest.java
@@ -18,6 +18,7 @@ import com.liferay.adaptive.media.content.transformer.constants.ContentTransform
 import com.liferay.adaptive.media.image.html.AMImageHTMLTagFactory;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import org.junit.Assert;
