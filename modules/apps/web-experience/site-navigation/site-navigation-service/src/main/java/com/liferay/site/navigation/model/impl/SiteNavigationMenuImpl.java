a056adfe373f1744a71d8775c0be32f506215b5d
Brian Chan <brian.chan@liferay.com>
2017-09-24 23:09:21 -0700

    LPS-74834 SF

--- modules/apps/web-experience/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/model/impl/SiteNavigationMenuImpl.java
+++ modules/apps/web-experience/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/model/impl/SiteNavigationMenuImpl.java
@@ -17,22 +17,11 @@ package com.liferay.site.navigation.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the SiteNavigationMenu service. Represents a row in the &quot;SiteNavigationMenu&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.site.navigation.model.SiteNavigationMenu} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class SiteNavigationMenuImpl extends SiteNavigationMenuBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a site navigation menu model instance should use the {@link com.liferay.site.navigation.model.SiteNavigationMenu} interface instead.
-	 */
 	public SiteNavigationMenuImpl() {
 	}
 
--- modules/apps/web-experience/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/model/impl/SiteNavigationMenuItemImpl.java
+++ modules/apps/web-experience/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/model/impl/SiteNavigationMenuItemImpl.java
@@ -17,22 +17,11 @@ package com.liferay.site.navigation.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the SiteNavigationMenuItem service. Represents a row in the &quot;SiteNavigationMenuItem&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.site.navigation.model.SiteNavigationMenuItem} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class SiteNavigationMenuItemImpl extends SiteNavigationMenuItemBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a site navigation menu item model instance should use the {@link com.liferay.site.navigation.model.SiteNavigationMenuItem} interface instead.
-	 */
 	public SiteNavigationMenuItemImpl() {
 	}
 
