f2fdf843dbe6b57e1e69e47d08038675a5b03e8c
JavierMoral <javier.moral@liferay.com>
2017-09-26 15:26:51 +0200

    LPS-74663 Avoid the creation of temp files in range requests when ranges are sorted and do not overlap

--- portal-kernel/src/com/liferay/portal/kernel/servlet/ServletResponseUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/ServletResponseUtil.java
@@ -324,8 +324,13 @@ public class ServletResponseUtil {
 
 				response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
 
+				Range previousRange = null;
+
+				final boolean rangesSortedAndNotOverlapped =
+					rangesSortedAndNotOverlapped(ranges);
+
 				for (int i = 0; i < ranges.size(); i++) {
-					Range range = ranges.get(i);
+					Range curRange = ranges.get(i);
 
 					servletOutputStream.println();
 					servletOutputStream.println(
@@ -334,12 +339,29 @@ public class ServletResponseUtil {
 						HttpHeaders.CONTENT_TYPE + ": " + contentType);
 					servletOutputStream.println(
 						HttpHeaders.CONTENT_RANGE + ": " +
-							range.getContentRange());
+							curRange.getContentRange());
 					servletOutputStream.println();
 
-					inputStream = copyRange(
-						inputStream, outputStream, range.getStart(),
-						range.getLength());
+					if (rangesSortedAndNotOverlapped) {
+						long offset = curRange.getStart();
+
+						if (previousRange != null) {
+							offset =
+								curRange.getStart() -
+									previousRange.getEnd() - 1;
+						}
+
+						previousRange = curRange;
+
+						inputStream = copyRange(
+							offset, inputStream, servletOutputStream, false,
+							curRange.getLength());
+					}
+					else {
+						inputStream = copyRange(
+							inputStream, servletOutputStream,
+							curRange.getStart(), curRange.getLength());
+					}
 				}
 
 				servletOutputStream.println();
@@ -633,6 +655,22 @@ public class ServletResponseUtil {
 			inputStream, outputStream, StreamUtil.BUFFER_SIZE, cleanUp, length);
 
 		return inputStream;
+	}
+
+	protected static boolean rangesSortedAndNotOverlapped(List<Range> ranges) {
+		Range previousRange = null;
+
+		for (Range range : ranges) {
+			if ((previousRange != null) &&
+				(range.getStart() <= previousRange.getEnd())) {
+
+				return false;
+			}
+
+			previousRange = range;
+		}
+
+		return true;
 	}
 
 	protected static void setContentLength(
