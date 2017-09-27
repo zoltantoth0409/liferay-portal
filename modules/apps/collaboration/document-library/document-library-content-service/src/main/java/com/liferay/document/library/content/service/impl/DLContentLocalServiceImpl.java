f653359bb8610e97c9f226f37603a6c56ab6b8ab
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-09-27 12:16:12 +0200

    LPS-74889 Adding custom methods from the original service

--- modules/apps/collaboration/document-library/document-library-content-service/src/main/java/com/liferay/document/library/content/service/impl/DLContentLocalServiceImpl.java
+++ modules/apps/collaboration/document-library/document-library-content-service/src/main/java/com/liferay/document/library/content/service/impl/DLContentLocalServiceImpl.java
@@ -14,7 +14,22 @@
 
 package com.liferay.document.library.content.service.impl;
 
+import com.liferay.document.library.content.exception.NoSuchContentException;
+import com.liferay.document.library.content.model.DLContent;
 import com.liferay.document.library.content.service.base.DLContentLocalServiceBaseImpl;
+import com.liferay.document.library.content.service.util.comparator.DLContentVersionComparator;
+import com.liferay.portal.kernel.dao.jdbc.OutputBlob;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.List;
 
 /**
  * The implementation of the document library content local service.
@@ -37,4 +52,181 @@ public class DLContentLocalServiceImpl extends DLContentLocalServiceBaseImpl {
 	 *
 	 * Never reference this class directly. Always use {@link com.liferay.document.library.content.service.DLContentLocalServiceUtil} to access the document library content local service.
 	 */
+	@Override
+	public DLContent addContent(
+		long companyId, long repositoryId, String path, String version,
+		byte[] bytes) {
+
+		long contentId = counterLocalService.increment();
+
+		DLContent dlContent = dlContentPersistence.create(contentId);
+
+		dlContent.setCompanyId(companyId);
+		dlContent.setRepositoryId(repositoryId);
+		dlContent.setPath(path);
+		dlContent.setVersion(version);
+
+		UnsyncByteArrayInputStream unsyncByteArrayInputStream =
+			new UnsyncByteArrayInputStream(bytes);
+
+		OutputBlob dataOutputBlob = new OutputBlob(
+			unsyncByteArrayInputStream, bytes.length);
+
+		dlContent.setData(dataOutputBlob);
+
+		dlContent.setSize(bytes.length);
+
+		dlContentPersistence.update(dlContent);
+
+		return dlContent;
+	}
+
+	@Override
+	public DLContent addContent(
+		long companyId, long repositoryId, String path, String version,
+		InputStream inputStream, long size) {
+
+		DLContent dlContent = null;
+
+		try (InputStream is = inputStream) {
+			long contentId = counterLocalService.increment();
+
+			dlContent = dlContentPersistence.create(contentId);
+
+			dlContent.setCompanyId(companyId);
+			dlContent.setRepositoryId(repositoryId);
+			dlContent.setPath(path);
+			dlContent.setVersion(version);
+
+			OutputBlob dataOutputBlob = new OutputBlob(is, size);
+
+			dlContent.setData(dataOutputBlob);
+
+			dlContent.setSize(size);
+
+			dlContentPersistence.update(dlContent);
+		}
+		catch (IOException ioe) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(ioe, ioe);
+			}
+		}
+
+		return dlContent;
+	}
+
+	@Override
+	public void deleteContent(
+			long companyId, long repositoryId, String path, String version)
+		throws PortalException {
+
+		dlContentPersistence.removeByC_R_P_V(
+			companyId, repositoryId, path, version);
+	}
+
+	@Override
+	public void deleteContents(long companyId, long repositoryId, String path) {
+		dlContentPersistence.removeByC_R_P(companyId, repositoryId, path);
+	}
+
+	@Override
+	public void deleteContentsByDirectory(
+		long companyId, long repositoryId, String dirName) {
+
+		if (!dirName.endsWith(StringPool.SLASH)) {
+			dirName = dirName.concat(StringPool.SLASH);
+		}
+
+		dirName = dirName.concat(StringPool.PERCENT);
+
+		dlContentPersistence.removeByC_R_LikeP(
+			companyId, repositoryId, dirName);
+	}
+
+	@Override
+	public DLContent getContent(long companyId, long repositoryId, String path)
+		throws NoSuchContentException {
+
+		OrderByComparator<DLContent> orderByComparator =
+			new DLContentVersionComparator();
+
+		List<DLContent> dlContents = dlContentPersistence.findByC_R_P(
+			companyId, repositoryId, path, 0, 1, orderByComparator);
+
+		if ((dlContents == null) || dlContents.isEmpty()) {
+			throw new NoSuchContentException(path);
+		}
+
+		return dlContents.get(0);
+	}
+
+	@Override
+	public DLContent getContent(
+			long companyId, long repositoryId, String path, String version)
+		throws NoSuchContentException {
+
+		return dlContentPersistence.findByC_R_P_V(
+			companyId, repositoryId, path, version);
+	}
+
+	@Override
+	public List<DLContent> getContents(long companyId, long repositoryId) {
+		return dlContentPersistence.findByC_R(companyId, repositoryId);
+	}
+
+	@Override
+	public List<DLContent> getContents(
+		long companyId, long repositoryId, String path) {
+
+		return dlContentPersistence.findByC_R_P(companyId, repositoryId, path);
+	}
+
+	@Override
+	public List<DLContent> getContentsByDirectory(
+		long companyId, long repositoryId, String dirName) {
+
+		if (!dirName.endsWith(StringPool.SLASH)) {
+			dirName = dirName.concat(StringPool.SLASH);
+		}
+
+		dirName = dirName.concat(StringPool.PERCENT);
+
+		return dlContentPersistence.findByC_R_LikeP(
+			companyId, repositoryId, dirName);
+	}
+
+	@Override
+	public boolean hasContent(
+		long companyId, long repositoryId, String path, String version) {
+
+		int count = dlContentPersistence.countByC_R_P_V(
+			companyId, repositoryId, path, version);
+
+		if (count > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	@Override
+	public void updateDLContent(
+		long companyId, long oldRepositoryId, long newRepositoryId,
+		String oldPath, String newPath) {
+
+		List<DLContent> dlContents = dlContentPersistence.findByC_R_P(
+			companyId, oldRepositoryId, oldPath);
+
+		for (DLContent dLContent : dlContents) {
+			dLContent.setRepositoryId(newRepositoryId);
+			dLContent.setPath(newPath);
+
+			dlContentPersistence.update(dLContent);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DLContentLocalServiceImpl.class);
+
 }
