9d5a2b14243e77ebe0521933485d3e05b9d8dfbd
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-09-27 16:04:07 +0200

    LPS-74879 Don't set the icon

--- modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/portlet/SiteNavigationAdminPortlet.java
+++ modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/portlet/SiteNavigationAdminPortlet.java
@@ -30,7 +30,6 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-navigation-admin",
 		"com.liferay.portlet.display-category=category.hidden",
-		"com.liferay.portlet.icon=/icons/navigation.png",
 		"com.liferay.portlet.layout-cacheable=true",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
