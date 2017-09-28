93fbd96263ba6c4d2ada01b9520f27d2309b1d05
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-09-28 11:42:10 -0300

    LPS-74975 / LPS-73731 Invoke preprocessing methods directly from portlet tabs.

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/BaseWorkflowPortlet.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/BaseWorkflowPortlet.java
@@ -72,10 +72,8 @@ public abstract class BaseWorkflowPortlet extends MVCPortlet {
 		actionRequest.setAttribute(
 			WorkflowWebKeys.WORKFLOW_DEFAULT_TAB, getDefaultTab());
 
-		for (String tabName : getWorkflowTabNames()) {
-			WorkflowPortletTab dynamicInclude = _dynamicIncludes.get(tabName);
-
-			dynamicInclude.prepareProcessAction(actionRequest, actionResponse);
+		for (WorkflowPortletTab portletTab : getPortletTabs()) {
+			portletTab.prepareProcessAction(actionRequest, actionResponse);
 		}
 
 		super.processAction(actionRequest, actionResponse);
@@ -88,10 +86,8 @@ public abstract class BaseWorkflowPortlet extends MVCPortlet {
 
 		addRenderRequestAttributes(renderRequest);
 
-		for (String tabName : getWorkflowTabNames()) {
-			WorkflowPortletTab dynamicInclude = _dynamicIncludes.get(tabName);
-
-			dynamicInclude.prepareRender(renderRequest, renderResponse);
+		for (WorkflowPortletTab portletTab : getPortletTabs()) {
+			portletTab.prepareRender(renderRequest, renderResponse);
 		}
 
 		super.render(renderRequest, renderResponse);
@@ -117,11 +113,8 @@ public abstract class BaseWorkflowPortlet extends MVCPortlet {
 			include("/instance/error.jsp", renderRequest, renderResponse);
 		}
 		else {
-			for (String tabName : getWorkflowTabNames()) {
-				WorkflowPortletTab dynamicInclude = _dynamicIncludes.get(
-					tabName);
-
-				dynamicInclude.prepareDispatch(renderRequest, renderResponse);
+			for (WorkflowPortletTab portletTab : getPortletTabs()) {
+				portletTab.prepareDispatch(renderRequest, renderResponse);
 			}
 
 			super.doDispatch(renderRequest, renderResponse);
