c8a632c3c4e869ed1fe479006fc3574f32bc1f04
Preston-Crary <preston.crary@liferay.com>
2017-09-28 10:51:17 -0700

    LPS-74999 Deprecate FileFilter and DirectoryFilter.

--- portal-kernel/src/com/liferay/portal/kernel/io/DirectoryFilter.java
+++ portal-kernel/src/com/liferay/portal/kernel/io/DirectoryFilter.java
@@ -23,7 +23,9 @@ import java.util.regex.Pattern;
 /**
  * @author Brian Wing Shun Chan
  * @author Alexander Chow
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class DirectoryFilter implements FileFilter {
 
 	public DirectoryFilter() {
