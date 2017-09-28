49f1ed9bf5786ce5f892939f580e15490878acdb
Marco Leo <marco.leo@liferay.com>
2017-09-28 17:03:53 +0200

    CPOMMERCE-163 SF

--- modules/dxp/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/portlet/action/CheckCPInstanceMVCActionCommand.java
+++ modules/dxp/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/portlet/action/CheckCPInstanceMVCActionCommand.java
@@ -88,6 +88,7 @@ public class CheckCPInstanceMVCActionCommand extends BaseMVCActionCommand {
 					httpServletRequest, cpInstance.getPrice());
 
 				jsonObject.put("price", formattedPrice);
+
 				jsonObject.put("sku", cpInstance.getSku());
 			}
 			else {
