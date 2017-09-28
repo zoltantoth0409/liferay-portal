c321616d5082e555d1b67fad068382264bcdad18
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-09-28 14:51:47 +0200

    LPS-75282 Create empty display context class and initial empty view for layout page template collections

---
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutPageTemplateDisplayContext.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.display.context;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class LayoutPageTemplateDisplayContext {
+
+	public LayoutPageTemplateDisplayContext(
+			RenderRequest renderRequest, RenderResponse renderResponse,
+			HttpServletRequest request)
+		throws PortalException {
+
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+
+		_request = request;
+	}
+
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+	private final HttpServletRequest _request;
+
+}
