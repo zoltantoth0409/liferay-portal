a753c9096affc758ba471c7e698453f756269490
Preston-Crary <preston.crary@liferay.com>
2017-09-27 17:00:18 -0700

    LPS-74980 Move CoalescedPipe to portal-cache-multiple.

--- portal-kernel/src/com/liferay/portal/kernel/concurrent/CoalescedPipe.java
+++ portal-kernel/src/com/liferay/portal/kernel/concurrent/CoalescedPipe.java
@@ -22,7 +22,9 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class CoalescedPipe<E> {
 
 	public CoalescedPipe() {
