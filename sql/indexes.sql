66ec2c5dc1a70358f17e9aa81062f41622f13502
Adolfo Pérez <adolfo.perez@liferay.com>
2017-09-29 14:15:39 +0200

    LPS-75031 Remove SQL definitions

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -142,9 +142,6 @@ create index IX_6747B2BC on DLFolder (repositoryId, parentFolderId);
 create index IX_DA448450 on DLFolder (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_3CC1DED2 on DLFolder (uuid_[$COLUMN_LENGTH:75$], groupId);
 
-create index IX_3D8E1607 on DLSyncEvent (modifiedTime);
-create unique index IX_57D82B06 on DLSyncEvent (typePK);
-
 create index IX_2A2CB130 on EmailAddress (companyId, classNameId, classPK, primary_);
 create index IX_7B43CD8 on EmailAddress (userId);
 create index IX_F74AB912 on EmailAddress (uuid_[$COLUMN_LENGTH:75$], companyId);
--- sql/portal-tables.sql
+++ sql/portal-tables.sql
@@ -454,15 +454,6 @@ create table DLFolder (
 	statusDate DATE null
 );
 
-create table DLSyncEvent (
-	syncEventId LONG not null primary key,
-	companyId LONG,
-	modifiedTime LONG,
-	event VARCHAR(75) null,
-	type_ VARCHAR(75) null,
-	typePK LONG
-);
-
 create table EmailAddress (
 	mvccVersion LONG default 0 not null,
 	uuid_ VARCHAR(75) null,
