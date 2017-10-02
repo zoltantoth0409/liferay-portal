8725b273d96887efdb3125bd1556cbf1c361b4ec
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-10-02 16:20:57 +0200

    LPS-74679 Use handler class

--- modules/apps/web-experience/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/AddFragmentEntryMVCActionCommand.java
+++ modules/apps/web-experience/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/AddFragmentEntryMVCActionCommand.java
@@ -15,25 +15,18 @@
 package com.liferay.fragment.web.internal.portlet.action;
 
 import com.liferay.fragment.constants.FragmentPortletKeys;
-import com.liferay.fragment.exception.DuplicateFragmentEntryException;
-import com.liferay.fragment.exception.FragmentEntryNameException;
 import com.liferay.fragment.model.FragmentEntry;
 import com.liferay.fragment.service.FragmentEntryService;
+import com.liferay.fragment.web.internal.handler.FragmentEntryExceptionRequestHandler;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.ResourceBundleLoader;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import java.util.ResourceBundle;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -85,44 +78,16 @@ public class AddFragmentEntryMVCActionCommand extends BaseMVCActionCommand {
 				actionRequest, actionResponse, jsonObject);
 		}
 		catch (PortalException pe) {
-			handlePortalException(actionRequest, actionResponse, pe);
+			_fragmentEntryExceptionRequestHandler.handlePortalException(
+				actionRequest, actionResponse, pe);
 		}
-	}
-
-	protected void handlePortalException(
-			ActionRequest actionRequest, ActionResponse actionResponse,
-			PortalException pe)
-		throws Exception {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-
-		String errorMessage = "an-unexpected-error-occurred";
-
-		if (pe instanceof DuplicateFragmentEntryException) {
-			errorMessage = "a-fragment-entry-with-that-name-already-exists";
-		}
-		else if (pe instanceof FragmentEntryNameException) {
-			errorMessage = "this-field-is-required";
-		}
-
-		ResourceBundle resourceBundle =
-			_resourceBundleLoader.loadResourceBundle(themeDisplay.getLocale());
-
-		jsonObject.put("error", LanguageUtil.get(resourceBundle, errorMessage));
-
-		JSONPortletResponseUtil.writeJSON(
-			actionRequest, actionResponse, jsonObject);
 	}
 
 	@Reference
-	private FragmentEntryService _fragmentEntryService;
+	private FragmentEntryExceptionRequestHandler
+		_fragmentEntryExceptionRequestHandler;
 
-	@Reference(
-		target = "(bundle.symbolic.name=com.liferay.fragment.web)", unbind = "-"
-	)
-	private ResourceBundleLoader _resourceBundleLoader;
+	@Reference
+	private FragmentEntryService _fragmentEntryService;
 
 }
