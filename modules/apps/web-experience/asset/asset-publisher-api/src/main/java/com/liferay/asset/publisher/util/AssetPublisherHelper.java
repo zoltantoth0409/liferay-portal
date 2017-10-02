f08350f5c709177777cc01b098394ce3e5d75b34
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-10-02 17:00:56 +0200

    LPS-75143 Creates new AssetPublisherHelper only with the necessary methods, copy from AssetPublisherUtil (no logic changes)

---
+++ modules/apps/web-experience/asset/asset-publisher-api/src/main/java/com/liferay/asset/publisher/util/AssetPublisherHelper.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.publisher.util;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import java.io.Serializable;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public interface AssetPublisherHelper {
+
+	public static final String SCOPE_ID_CHILD_GROUP_PREFIX = "ChildGroup_";
+
+	public static final String SCOPE_ID_GROUP_PREFIX = "Group_";
+
+	public static final String SCOPE_ID_LAYOUT_PREFIX = "Layout_";
+
+	public static final String SCOPE_ID_LAYOUT_UUID_PREFIX = "LayoutUuid_";
+
+	public static final String SCOPE_ID_PARENT_GROUP_PREFIX = "ParentGroup_";
+
+	public long[] getAssetCategoryIds(PortletPreferences portletPreferences);
+
+	public BaseModelSearchResult<AssetEntry> getAssetEntries(
+			AssetEntryQuery assetEntryQuery, Layout layout,
+			PortletPreferences portletPreferences, String portletName,
+			Locale locale, TimeZone timeZone, long companyId, long scopeGroupId,
+			long userId, Map<String, Serializable> attributes, int start,
+			int end)
+		throws Exception;
+
+	public List<AssetEntry> getAssetEntries(
+			PortletRequest portletRequest,
+			PortletPreferences portletPreferences,
+			PermissionChecker permissionChecker, long[] groupIds,
+			boolean deleteMissingAssetEntries, boolean checkPermission)
+		throws Exception;
+
+	public List<AssetEntry> getAssetEntries(
+			PortletRequest portletRequest,
+			PortletPreferences portletPreferences,
+			PermissionChecker permissionChecker, long[] groupIds,
+			boolean deleteMissingAssetEntries, boolean checkPermission,
+			boolean includeNonVisibleAssets)
+		throws Exception;
+
+	public List<AssetEntry> getAssetEntries(
+			PortletRequest portletRequest,
+			PortletPreferences portletPreferences,
+			PermissionChecker permissionChecker, long[] groupIds,
+			boolean deleteMissingAssetEntries, boolean checkPermission,
+			boolean includeNonVisibleAssets, int type)
+		throws Exception;
+
+	public List<AssetEntry> getAssetEntries(
+			PortletRequest portletRequest,
+			PortletPreferences portletPreferences,
+			PermissionChecker permissionChecker, long[] groupIds,
+			long[] allCategoryIds, String[] allTagNames,
+			boolean deleteMissingAssetEntries, boolean checkPermission)
+		throws Exception;
+
+	public AssetEntryQuery getAssetEntryQuery(
+			PortletPreferences portletPreferences, long groupId, Layout layout,
+			long[] overrideAllAssetCategoryIds,
+			String[] overrideAllAssetTagNames)
+		throws PortalException;
+
+	public List<AssetEntryResult> getAssetEntryResults(
+			SearchContainer searchContainer, AssetEntryQuery assetEntryQuery,
+			Layout layout, PortletPreferences portletPreferences,
+			String portletName, Locale locale, TimeZone timeZone,
+			long companyId, long scopeGroupId, long userId, long[] classNameIds,
+			Map<String, Serializable> attributes)
+		throws Exception;
+
+	public String[] getAssetTagNames(PortletPreferences portletPreferences);
+
+	public long[] getClassNameIds(
+		PortletPreferences portletPreferences, long[] availableClassNameIds);
+
+	public long getGroupIdFromScopeId(
+			String scopeId, long siteGroupId, boolean privateLayout)
+		throws PortalException;
+
+	public long[] getGroupIds(
+		PortletPreferences portletPreferences, long scopeGroupId,
+		Layout layout);
+
+	public String getScopeId(Group group, long scopeGroupId);
+
+}
