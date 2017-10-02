ffb197b3bdeffb098645ec6556ee38d3afe845b7
Preston-Crary <preston.crary@liferay.com>
2017-10-02 09:39:06 -0700

    LPS-75010 Fix packages.

--- modules/core/petra/petra-string/src/test/java/com/liferay/petra/string/StringBundlerTest.java
+++ modules/core/petra/petra-string/src/test/java/com/liferay/petra/string/StringBundlerTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.kernel.util;
+package com.liferay.petra.string;
 
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
