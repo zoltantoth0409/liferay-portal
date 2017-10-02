3393e42ae3620100c5618418f2f93f4bba6220ad
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-10-02 16:18:18 +0200

    LPS-74679 Handler class for fragment entries exceptions

---
+++ modules/apps/web-experience/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/handler/FragmentEntryExceptionRequestHandler.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.handler;
+
+import com.liferay.fragment.exception.DuplicateFragmentEntryException;
+import com.liferay.fragment.exception.FragmentEntryNameException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.ResourceBundle;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true, service = FragmentEntryExceptionRequestHandler.class
+)
+public class FragmentEntryExceptionRequestHandler {
+
+	public void handlePortalException(
+			ActionRequest actionRequest, ActionResponse actionResponse,
+			PortalException pe)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		String errorMessage = "an-unexpected-error-occurred";
+
+		if (pe instanceof DuplicateFragmentEntryException) {
+			errorMessage = "a-fragment-entry-with-that-name-already-exists";
+		}
+		else if (pe instanceof FragmentEntryNameException) {
+			errorMessage = "this-field-is-required";
+		}
+
+		ResourceBundle resourceBundle =
+			_resourceBundleLoader.loadResourceBundle(themeDisplay.getLocale());
+
+		jsonObject.put("error", LanguageUtil.get(resourceBundle, errorMessage));
+
+		JSONPortletResponseUtil.writeJSON(
+			actionRequest, actionResponse, jsonObject);
+	}
+
+	@Reference(
+		target = "(bundle.symbolic.name=com.liferay.fragment.web)", unbind = "-"
+	)
+	private ResourceBundleLoader _resourceBundleLoader;
+
+}
