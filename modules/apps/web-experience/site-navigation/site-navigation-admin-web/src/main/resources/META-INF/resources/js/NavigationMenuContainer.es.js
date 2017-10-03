fdf3ab081737136d6b7104b9aed07263b6cca5ab
Pavel Savinov <pavel.savinov@liferay.com>
2017-10-03 15:02:01 +0200

    LPS-75693 Menu builder items container

---
+++ modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/js/NavigationMenuContainer.es.js
@@ -0,0 +1,17 @@
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+
+import templates from './NavigationMenuContainer.soy';
+
+/**
+ * NavigationMenuContainer
+ *
+ */
+class NavigationMenuContainer extends Component {
+
+}
+
+Soy.register(NavigationMenuContainer, templates);
+
+export { NavigationMenuContainer }
+export default NavigationMenuContainer;
