71231f211ba472a3b4c3c86fcae28946e2c35caa
Pablo Molina <pablo.molina@liferay.com>
2017-10-04 09:38:15 +0200

    LPS-74836 Moved form registered callback to a separated function

--- modules/apps/web-experience/product-navigation/product-navigation-control-menu-theme-contributor/src/main/resources/META-INF/resources/product_navigation_control_menu.js
+++ modules/apps/web-experience/product-navigation/product-navigation-control-menu-theme-contributor/src/main/resources/META-INF/resources/product_navigation_control_menu.js
@@ -3,10 +3,15 @@
         window.scrollBy(0, -65);
     };
 
-    Liferay.on(
-        'form:registered',
-        function(event) {
-            AUI().Do.after(adjustScrollTop, event.form.formValidator, 'focusInvalidField', event.form.formValidator);
-        }
-    );
+	var handleFormRegistered = function(event) {
+		if (event.form && event.form.formValidator) {
+			AUI().Do.after(
+				adjustScrollTop,
+				event.form.formValidator,
+				'focusInvalidField'
+			);
+		}
+	};
+
+	Liferay.on('form:registered', handleFormRegistered);
 }());
