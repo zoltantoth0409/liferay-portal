27d4395223d1aa3c4532d07464bcf82d21a91a09
shuyangzhou <shuyang.zhou@liferay.com>
2017-10-05 10:55:18 -0700

    LPS-75109 Simplify orphan noticeable future cancellation

--- portal-kernel/src/com/liferay/portal/kernel/concurrent/AsyncBroker.java
+++ portal-kernel/src/com/liferay/portal/kernel/concurrent/AsyncBroker.java
@@ -15,13 +15,7 @@
 package com.liferay.portal.kernel.concurrent;
 
 import com.liferay.petra.concurrent.ConcurrentReferenceValueHashMap;
-import com.liferay.petra.memory.FinalizeAction;
 import com.liferay.petra.memory.FinalizeManager;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-
-import java.lang.ref.Reference;
-import java.lang.reflect.Field;
 
 import java.util.Collections;
 import java.util.Map;
@@ -40,7 +34,14 @@ public class AsyncBroker<K, V> {
 
 	public NoticeableFuture<V> post(K key) {
 		DefaultNoticeableFuture<V> defaultNoticeableFuture =
-			new DefaultNoticeableFuture<>();
+			new DefaultNoticeableFuture<V>() {
+
+				@Override
+				protected void finalize() {
+					cancel(true);
+				}
+
+			};
 
 		NoticeableFuture<V> previousNoticeableFuture = _post(
 			key, defaultNoticeableFuture);
@@ -131,69 +132,11 @@ public class AsyncBroker<K, V> {
 
 			});
 
-		if (_REFERENT_FIELD != null) {
-			FinalizeManager.register(
-				defaultNoticeableFuture, new CancellationFinalizeAction(key),
-				FinalizeManager.PHANTOM_REFERENCE_FACTORY);
-		}
-
 		return null;
-	}
-
-	private static final Field _REFERENT_FIELD;
-
-	private static final Log _log = LogFactoryUtil.getLog(AsyncBroker.class);
-
-	static {
-		Field referentField = null;
-
-		try {
-			referentField = Reference.class.getDeclaredField("referent");
-
-			referentField.setAccessible(true);
-		}
-		catch (Throwable t) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Cancellation of orphaned noticeable futures is disabled " +
-						"because the JVM does not support phantom reference " +
-							"resurrection",
-					t);
-			}
-		}
-
-		_REFERENT_FIELD = referentField;
 	}
 
 	private final ConcurrentMap<K, DefaultNoticeableFuture<V>>
 		_defaultNoticeableFutures = new ConcurrentReferenceValueHashMap<>(
 			FinalizeManager.WEAK_REFERENCE_FACTORY);
-
-	private static class CancellationFinalizeAction implements FinalizeAction {
-
-		public CancellationFinalizeAction(Object key) {
-			_key = key;
-		}
-
-		@Override
-		public void doFinalize(final Reference<?> reference) {
-			try {
-				NoticeableFuture<?> noticeableFuture =
-					(NoticeableFuture<?>)_REFERENT_FIELD.get(reference);
-
-				if (noticeableFuture.cancel(true) && _log.isWarnEnabled()) {
-					_log.warn(
-						"Cancelled orphan noticeable future " +
-							noticeableFuture + " with key " + _key);
-				}
-			}
-			catch (Exception e) {
-				_log.error("Unable to access referent of " + reference, e);
-			}
-		}
-
-		private final Object _key;
-
-	}
 
 }
