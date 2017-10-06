f7e2854a3d55694d3688057e2cf9f94f89b85525
Michael Han <michael.han@liferay.com>
2017-10-06 10:43:38 -0500

    LPS-80470 document-library-service: integrate DLFolder into model indexer
    https://issues.liferay.com/browse/LPS-80470
    
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: Michael Han <michael.han@liferay.com>

---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/DLFolderModelDocumentContributor.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.search;
+
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+import com.liferay.trash.TrashHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.document.library.kernel.model.DLFolder",
+	service = ModelDocumentContributor.class
+)
+public class DLFolderModelDocumentContributor
+	implements ModelDocumentContributor<DLFolder> {
+
+	@Override
+	public void contribute(Document document, DLFolder dlFolder) {
+		if (_log.isDebugEnabled()) {
+			_log.debug("Indexing folder " + dlFolder);
+		}
+
+		document.addText(Field.DESCRIPTION, dlFolder.getDescription());
+		document.addKeyword(Field.FOLDER_ID, dlFolder.getParentFolderId());
+		document.addKeyword(
+			Field.HIDDEN, dlFolder.isHidden() || dlFolder.isInHiddenFolder());
+
+		String title = dlFolder.getName();
+
+		if (dlFolder.isInTrash()) {
+			title = trashHelper.getOriginalTitle(title);
+		}
+
+		document.addText(Field.TITLE, title);
+
+		document.addKeyword(Field.TREE_PATH, dlFolder.getTreePath());
+		document.addKeyword(
+			Field.TREE_PATH,
+			StringUtil.split(dlFolder.getTreePath(), CharPool.SLASH));
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Document " + dlFolder + " indexed successfully");
+		}
+	}
+
+	@Reference
+	protected TrashHelper trashHelper;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DLFolderModelDocumentContributor.class);
+
+}
---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/DLFolderModelIndexerWriterContributor.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.search;
+
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.kernel.service.DLFolderLocalService;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.document.library.kernel.model.DLFolder",
+	service = ModelIndexerWriterContributor.class
+)
+public class DLFolderModelIndexerWriterContributor
+	implements ModelIndexerWriterContributor<DLFolder> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setAddCriteriaMethod(
+			dynamicQuery -> {
+				Property property = PropertyFactoryUtil.forName("mountPoint");
+
+				dynamicQuery.add(property.eq(false));
+			});
+
+		batchIndexingActionable.setPerformActionMethod(
+			(DLFolder dlFolder) -> {
+				batchIndexingActionable.addDocuments(
+					modelIndexerWriterDocumentHelper.getDocument(dlFolder));
+			});
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				dlFolderLocalService.getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(DLFolder dlFolder) {
+		return dlFolder.getCompanyId();
+	}
+
+	@Reference
+	protected DLFolderLocalService dlFolderLocalService;
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+}
