e33549b0e215492c8eb0f9c428745d391b5877a6
Pavel Savinov <pavel.savinov@liferay.com>
2017-10-06 13:49:18 +0200

    LPS-75693 Metal component which provides a url view for menu builder toolbox

---
+++ modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/js/NavigationMenuFieldset.es.js
@@ -0,0 +1,15 @@
+import {Component} from 'metal-component';
+import Soy from 'metal-soy';
+
+import templates from './NavigationMenuFieldset.soy';
+
+/**
+ * NavigationMenuFieldset
+ *
+ */
+class NavigationMenuFieldset extends Component {}
+
+Soy.register(NavigationMenuFieldset, templates);
+
+export { NavigationMenuFieldset }
+export default NavigationMenuFieldset;
