06305dc0004c31991f9dec174ddd03da5fdf2fde
Brian Chan <brian.chan@liferay.com>
2017-10-06 21:00:30 -0700

    Revert "LPS-74143 Service Builder should not generate deprecated tests."
    This reverts commit ac6f2343ca88b86a34759018778f560790ab2833.

--- modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -2930,10 +2930,6 @@ public class ServiceBuilder {
 	}
 
 	private void _createPersistenceTest(Entity entity) throws Exception {
-		if (entity.isDeprecated()) {
-			return;
-		}
-
 		Map<String, Object> context = _getContext();
 
 		context.put("entity", entity);
