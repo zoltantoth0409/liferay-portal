99bbdf0bcc44e328744cd0dbfb2fd865232d07fd
Alan Huang <alan.huang@liferay.com>
2017-10-09 11:07:10 +0800

    LPS-74240 Reindex users when remove site

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -888,6 +888,15 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 					}
 				}
 
+				long[] userId = getUserPrimaryKeys(group.getGroupId());
+
+				TransactionCommitCallbackUtil.registerCallback(
+					() -> {
+						reindex(group.getCompanyId(), userId);
+
+						return null;
+					});
+
 				groupPersistence.remove(group);
 			}
 
