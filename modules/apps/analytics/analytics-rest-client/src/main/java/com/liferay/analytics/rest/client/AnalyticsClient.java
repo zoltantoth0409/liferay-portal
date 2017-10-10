77fde75c581c407b200ee5feb979c2fa781aae22
Eduardo Garcia <eduardo.garcia@liferay.com>
2017-10-10 19:22:06 +0200

    LPS-75174 Add analytics REST client based on com.liferay.lcs.messaging.analytics.AnalyticsEventsMessage.

---
+++ modules/apps/analytics/analytics-rest-client/src/main/java/com/liferay/analytics/rest/client/AnalyticsClient.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.rest.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class AnalyticsClient {
+
+	public Response addAnalytics(AnalyticsEventsMessage analyticsEventMessage) {
+		WebTarget webTarget = _client.target(_REST_URI);
+
+		Invocation.Builder request = webTarget.request(
+			MediaType.APPLICATION_JSON);
+
+		return request.post(
+			Entity.entity(analyticsEventMessage, MediaType.APPLICATION_JSON));
+	}
+
+	private static final String _REST_URI =
+		"http://54.235.215.13:8095/api/analyticsgateway/send-analytics-events";
+
+	private final Client _client = ClientBuilder.newClient();
+
+}
