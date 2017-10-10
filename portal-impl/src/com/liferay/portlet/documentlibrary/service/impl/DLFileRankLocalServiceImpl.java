1a6d9677221516167493a8b4b18ce1a32b072b7c
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-10-10 12:43:56 +0200

    LPS-74888 Removing service implementation from core

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileRankLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileRankLocalServiceImpl.java
@@ -16,16 +16,10 @@ package com.liferay.portlet.documentlibrary.service.impl;
 
 import com.liferay.document.library.kernel.model.DLFileRank;
 import com.liferay.document.library.kernel.model.DLFolder;
-import com.liferay.document.library.kernel.util.comparator.FileRankCreateDateComparator;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
-import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.documentlibrary.service.base.DLFileRankLocalServiceBaseImpl;
 
 import java.util.List;
@@ -40,147 +34,95 @@ public class DLFileRankLocalServiceImpl extends DLFileRankLocalServiceBaseImpl {
 		long groupId, long companyId, long userId, long fileEntryId,
 		ServiceContext serviceContext) {
 
-		long fileRankId = counterLocalService.increment();
-
-		DLFileRank dlFileRank = dlFileRankPersistence.create(fileRankId);
-
-		dlFileRank.setGroupId(groupId);
-		dlFileRank.setCompanyId(companyId);
-		dlFileRank.setUserId(userId);
-		dlFileRank.setCreateDate(serviceContext.getCreateDate(null));
-		dlFileRank.setFileEntryId(fileEntryId);
-		dlFileRank.setActive(true);
-
-		try {
-			dlFileRankPersistence.update(dlFileRank);
-		}
-		catch (SystemException se) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Add failed, fetch {companyId=" + companyId + ", userId=" +
-						userId + ", fileEntryId=" + fileEntryId + "}");
-			}
-
-			dlFileRank = dlFileRankPersistence.fetchByC_U_F(
-				companyId, userId, fileEntryId, false);
-
-			if (dlFileRank == null) {
-				throw se;
-			}
-		}
-
-		return dlFileRank;
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public void checkFileRanks() {
-		List<Object[]> staleFileRanks = dlFileRankFinder.findByStaleRanks(
-			PropsValues.DL_FILE_RANK_MAX_SIZE);
-
-		for (Object[] staleFileRank : staleFileRanks) {
-			long groupId = (Long)staleFileRank[0];
-			long userId = (Long)staleFileRank[1];
-
-			List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByG_U_A(
-				groupId, userId, true, PropsValues.DL_FILE_RANK_MAX_SIZE,
-				QueryUtil.ALL_POS, new FileRankCreateDateComparator());
-
-			for (DLFileRank dlFileRank : dlFileRanks) {
-				long fileRankId = dlFileRank.getFileRankId();
-
-				try {
-					dlFileRankPersistence.remove(dlFileRank);
-				}
-				catch (Exception e) {
-					if (_log.isWarnEnabled()) {
-						_log.warn("Unable to remove file rank " + fileRankId);
-					}
-				}
-			}
-		}
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public void deleteFileRank(DLFileRank dlFileRank) {
-		dlFileRankPersistence.remove(dlFileRank);
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public void deleteFileRank(long fileRankId) throws PortalException {
-		DLFileRank dlFileRank = dlFileRankPersistence.findByPrimaryKey(
-			fileRankId);
-
-		dlFileRankLocalService.deleteFileRank(dlFileRank);
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public void deleteFileRanksByFileEntryId(long fileEntryId) {
-		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
-			fileEntryId);
-
-		for (DLFileRank dlFileRank : dlFileRanks) {
-			dlFileRankLocalService.deleteFileRank(dlFileRank);
-		}
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public void deleteFileRanksByUserId(long userId) {
-		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByUserId(
-			userId);
-
-		for (DLFileRank dlFileRank : dlFileRanks) {
-			dlFileRankLocalService.deleteFileRank(dlFileRank);
-		}
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public void disableFileRanks(long fileEntryId) {
-		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
-			fileEntryId);
-
-		for (DLFileRank dlFileRank : dlFileRanks) {
-			dlFileRank.setActive(false);
-
-			dlFileRankPersistence.update(dlFileRank);
-		}
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public void disableFileRanksByFolderId(long folderId)
 		throws PortalException {
 
-		DLFolder dlFolder = dlFolderLocalService.getDLFolder(folderId);
-
-		updateFileRanks(dlFolder, false);
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public void enableFileRanks(long fileEntryId) {
-		List<DLFileRank> dlFileRanks = dlFileRankPersistence.findByFileEntryId(
-			fileEntryId);
-
-		for (DLFileRank dlFileRank : dlFileRanks) {
-			dlFileRank.setActive(true);
-
-			dlFileRankPersistence.update(dlFileRank);
-		}
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public void enableFileRanksByFolderId(long folderId)
 		throws PortalException {
 
-		DLFolder dlFolder = dlFolderLocalService.getDLFolder(folderId);
-
-		updateFileRanks(dlFolder, true);
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
 	public List<DLFileRank> getFileRanks(long groupId, long userId) {
-		return dlFileRankPersistence.findByG_U_A(
-			groupId, userId, true, 0, PropsValues.DL_FILE_RANK_MAX_SIZE,
-			new FileRankCreateDateComparator());
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	@Override
@@ -188,55 +130,17 @@ public class DLFileRankLocalServiceImpl extends DLFileRankLocalServiceBaseImpl {
 		long groupId, long companyId, long userId, long fileEntryId,
 		ServiceContext serviceContext) {
 
-		if (!PropsValues.DL_FILE_RANK_ENABLED) {
-			return null;
-		}
-
-		DLFileRank dlFileRank = dlFileRankPersistence.fetchByC_U_F(
-			companyId, userId, fileEntryId);
-
-		if (dlFileRank != null) {
-			dlFileRank.setCreateDate(serviceContext.getCreateDate(null));
-
-			try {
-				dlFileRankPersistence.update(dlFileRank);
-			}
-			catch (Exception e) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Update failed, fetch {companyId=" + companyId +
-							", userId=" + userId + ", fileEntryId=" +
-								fileEntryId + "}");
-				}
-			}
-		}
-		else {
-			dlFileRank = addFileRank(
-				groupId, companyId, userId, fileEntryId, serviceContext);
-		}
-
-		return dlFileRank;
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
 
 	protected void updateFileRanks(DLFolder dlFolder, boolean active) {
-		List<DLFolder> dlFolders = dlFolderPersistence.findByG_P(
-			dlFolder.getGroupId(), dlFolder.getFolderId());
-
-		for (DLFolder curDLFolder : dlFolders) {
-			updateFileRanks(curDLFolder, active);
-		}
-
-		List<DLFileRank> dlFileRanks = dlFileRankFinder.findByFolderId(
-			dlFolder.getFolderId());
-
-		for (DLFileRank dlFileRank : dlFileRanks) {
-			dlFileRank.setActive(active);
-
-			dlFileRankPersistence.update(dlFileRank);
-		}
+		throw new UnsupportedOperationException(
+			"This class is deprecated and replaced by " +
+				"com.liferay.document.library.file.rank.service.impl." +
+					"DLFileRankLocalServiceImpl");
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		DLFileRankLocalServiceImpl.class);
 
 }
