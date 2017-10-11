626b6c3f69dc6f3bb1e5811020d63159ad834baa
Michael Hashimoto <michael.hashimoto@liferay.com>
2017-10-11 14:58:56 -0700

    LRQA-35314 Add method to find all 'DataArchives' in data archive branches

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/DataArchiveBranch.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/DataArchiveBranch.java
@@ -15,6 +15,19 @@
 package com.liferay.jenkins.results.parser;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
 
 /**
  * @author Michael Hashimoto
@@ -36,6 +49,8 @@ public class DataArchiveBranch {
 		_portalLegacyGitWorkingDirectory.reset("--hard");
 
 		_portalLegacyGitWorkingDirectory.clean();
+
+		_dataArchives = _getDataArchives(_getBuildProperties());
 	}
 
 	public File getGeneratedDataArchiveDirectory() {
@@ -46,6 +61,150 @@ public class DataArchiveBranch {
 		return _portalLegacyGitWorkingDirectory.getWorkingDirectory();
 	}
 
+	private Properties _getBuildProperties() {
+		Properties buildProperties = new Properties();
+
+		File portalLegacyWorkingDirectory =
+			_portalLegacyGitWorkingDirectory.getWorkingDirectory();
+
+		File buildPropertiesFile = new File(
+			portalLegacyWorkingDirectory, "build.properties");
+
+		try (FileInputStream fileInputStream = new FileInputStream(
+				buildPropertiesFile)) {
+
+			buildProperties.load(fileInputStream);
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(
+				JenkinsResultsParserUtil.combine(
+					"Unable to load ", buildPropertiesFile.getPath()),
+				ioe);
+		}
+
+		return buildProperties;
+	}
+
+	private List<DataArchive> _getDataArchives(Properties buildProperties) {
+		List<DataArchive> dataArchives = new ArrayList<>();
+
+		File portalLegacyWorkingDirectory =
+			_portalLegacyGitWorkingDirectory.getWorkingDirectory();
+
+		Set<String> portalVersions = _getPortalVersions(buildProperties);
+
+		for (String portalVersion : portalVersions) {
+			Set<String> dataArchiveTypes;
+
+			try {
+				dataArchiveTypes = _getDataArchiveTypes(
+					portalLegacyWorkingDirectory, portalVersion);
+			}
+			catch (DocumentException | IOException e) {
+				throw new RuntimeException(
+					JenkinsResultsParserUtil.combine(
+						"Unable to get data archive names in ",
+						portalLegacyWorkingDirectory.toString(),
+						" for portal version ", portalVersion),
+					e);
+			}
+
+			for (String dataArchiveType : dataArchiveTypes) {
+				Set<String> databaseNames = _getDatabaseNames(
+					buildProperties, portalVersion);
+
+				for (String databaseName : databaseNames) {
+					DataArchive dataArchive = new DataArchive(
+						this, dataArchiveType, databaseName, portalVersion);
+
+					dataArchives.add(dataArchive);
+				}
+			}
+		}
+
+		return dataArchives;
+	}
+
+	private Set<String> _getDataArchiveTypes(
+			File portalLegacyWorkingDirectory, String portalVersion)
+		throws DocumentException, IOException {
+
+		Set<String> dataArchiveTypes = new HashSet<>();
+
+		List<File> testcaseFiles = JenkinsResultsParserUtil.findFiles(
+			new File(portalLegacyWorkingDirectory, portalVersion),
+			".*\\.testcase");
+
+		for (File testcaseFile : testcaseFiles) {
+			Document document = Dom4JUtil.parse(
+				JenkinsResultsParserUtil.read(testcaseFile));
+
+			Element rootElement = document.getRootElement();
+
+			dataArchiveTypes.addAll(
+				_getPoshiPropertyValues(rootElement, "data.archive.type"));
+		}
+
+		return dataArchiveTypes;
+	}
+
+	private Set<String> _getDatabaseNames(
+		Properties buildProperties, String portalVersion) {
+
+		String dataArchiveDatabaseNames = buildProperties.getProperty(
+			"data.archive.database.names");
+
+		String databaseNamesPortalVersionKey = JenkinsResultsParserUtil.combine(
+			"data.archive.database.names[", portalVersion, "]");
+
+		if (buildProperties.containsKey(databaseNamesPortalVersionKey)) {
+			dataArchiveDatabaseNames = buildProperties.getProperty(
+				databaseNamesPortalVersionKey);
+		}
+
+		return new HashSet<>(
+			Arrays.asList(dataArchiveDatabaseNames.split(",")));
+	}
+
+	private Set<String> _getPortalVersions(Properties buildProperties) {
+		String dataArchivePortalVersions = buildProperties.getProperty(
+			"data.archive.portal.versions");
+
+		return new HashSet<>(
+			Arrays.asList(dataArchivePortalVersions.split(",")));
+	}
+
+	private Set<String> _getPoshiPropertyValues(
+		Element element, String targetPoshiPropertyName) {
+
+		Set<String> poshiPropertyValues = new HashSet<>();
+
+		List<Element> childElements = element.elements();
+
+		if (childElements.isEmpty()) {
+			return poshiPropertyValues;
+		}
+
+		for (Element childElement : childElements) {
+			String childElementName = childElement.getName();
+
+			if (childElementName.equals("property")) {
+				String poshiPropertyName = childElement.attributeValue("name");
+
+				if (poshiPropertyName.equals(targetPoshiPropertyName)) {
+					poshiPropertyValues.add(
+						childElement.attributeValue("value"));
+				}
+			}
+
+			poshiPropertyValues.addAll(
+				_getPoshiPropertyValues(childElement, targetPoshiPropertyName));
+		}
+
+		return poshiPropertyValues;
+	}
+
+	private final List<DataArchive> _dataArchives;
 	private final File _generatedDataArchiveDirectory;
 	private final GitWorkingDirectory _portalLegacyGitWorkingDirectory;
 
