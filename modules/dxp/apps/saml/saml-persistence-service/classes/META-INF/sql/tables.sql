36ff2e823f5135037ad41d5c753c68a420db6abc
Brian Chan <brian.chan@liferay.com>
2017-10-11 15:43:28 -0700

    saml-persistence-service 1.0.0 packageinfo

---
+++ modules/dxp/apps/saml/saml-persistence-service/classes/META-INF/sql/tables.sql
@@ -0,0 +1,100 @@
+create table SamlIdpSpConnection (
+	samlIdpSpConnectionId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	samlSpEntityId VARCHAR(1024) null,
+	assertionLifetime INTEGER,
+	attributeNames STRING null,
+	attributesEnabled BOOLEAN,
+	attributesNamespaceEnabled BOOLEAN,
+	enabled BOOLEAN,
+	metadataUrl VARCHAR(1024) null,
+	metadataXml TEXT null,
+	metadataUpdatedDate DATE null,
+	name VARCHAR(75) null,
+	nameIdAttribute VARCHAR(1024) null,
+	nameIdFormat VARCHAR(1024) null
+);
+
+create table SamlIdpSpSession (
+	samlIdpSpSessionId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	samlIdpSsoSessionId LONG,
+	samlSpEntityId VARCHAR(1024) null,
+	nameIdFormat VARCHAR(1024) null,
+	nameIdValue VARCHAR(1024) null
+);
+
+create table SamlIdpSsoSession (
+	samlIdpSsoSessionId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	samlIdpSsoSessionKey VARCHAR(75) null
+);
+
+create table SamlSpAuthRequest (
+	samlSpAuthnRequestId LONG not null primary key,
+	companyId LONG,
+	createDate DATE null,
+	samlIdpEntityId VARCHAR(1024) null,
+	samlSpAuthRequestKey VARCHAR(75) null
+);
+
+create table SamlSpIdpConnection (
+	samlSpIdpConnectionId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	samlIdpEntityId VARCHAR(1024) null,
+	assertionSignatureRequired BOOLEAN,
+	clockSkew LONG,
+	enabled BOOLEAN,
+	forceAuthn BOOLEAN,
+	ldapImportEnabled BOOLEAN,
+	metadataUrl VARCHAR(1024) null,
+	metadataXml TEXT null,
+	metadataUpdatedDate DATE null,
+	name VARCHAR(75) null,
+	nameIdFormat VARCHAR(1024) null,
+	signAuthnRequest BOOLEAN,
+	userAttributeMappings STRING null
+);
+
+create table SamlSpMessage (
+	samlSpMessageId LONG not null primary key,
+	companyId LONG,
+	createDate DATE null,
+	samlIdpEntityId VARCHAR(1024) null,
+	samlIdpResponseKey VARCHAR(75) null,
+	expirationDate DATE null
+);
+
+create table SamlSpSession (
+	samlSpSessionId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	samlSpSessionKey VARCHAR(75) null,
+	assertionXml TEXT null,
+	jSessionId VARCHAR(200) null,
+	nameIdFormat VARCHAR(1024) null,
+	nameIdNameQualifier VARCHAR(1024) null,
+	nameIdSPNameQualifier VARCHAR(1024) null,
+	nameIdValue VARCHAR(1024) null,
+	sessionIndex VARCHAR(75) null,
+	terminated_ BOOLEAN
+);
