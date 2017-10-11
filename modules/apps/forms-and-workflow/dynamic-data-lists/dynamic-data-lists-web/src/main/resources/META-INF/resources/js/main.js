16e1c817a7e9ea743e47e79cc63ecac482e69c86
Lino Alves <lino.alves@liferay.com>
2017-10-11 13:48:40 -0300

    LPS-73361 adding color picker to spreadsheet view

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-web/src/main/resources/META-INF/resources/js/main.js
@@ -53,6 +53,7 @@ AUI.add(
 
 				TYPE_EDITOR: {
 					'checkbox': A.CheckboxCellEditor,
+					'ddm-color': FormBuilder.CUSTOM_CELL_EDITORS['color-cell-editor'],
 					'ddm-date': A.DateCellEditor,
 					'ddm-decimal': A.TextCellEditor,
 					'ddm-documentlibrary': FormBuilder.CUSTOM_CELL_EDITORS['document-library-file-entry-cell-editor'],
