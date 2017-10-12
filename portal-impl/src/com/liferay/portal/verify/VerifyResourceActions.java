a156c8c43b525bda31670fcbd2458ba4e6dbef0d
Minhchau Dang <minhchau.dang@liferay.com>
2017-10-11 19:26:12 -0700

    LPS-66047 ResourceAction is a small table, so simplify logic to make it more clear what it's trying to do, fixing the extra deletes in the process

--- portal-impl/src/com/liferay/portal/verify/VerifyResourceActions.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyResourceActions.java
@@ -23,62 +23,64 @@ import com.liferay.portal.kernel.util.StringBundler;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * @author Michael Bowerman
  */
 public class VerifyResourceActions extends VerifyProcess {
 
 	protected void deleteDuplicateBitwiseValuesOnResource() throws Exception {
-		StringBundler sb = new StringBundler(5);
+		StringBundler sb = new StringBundler(3);
 
-		sb.append("select ra1.resourceActionId, ra1.name, ra1.actionId, ");
-		sb.append("ra1.bitwiseValue from ResourceAction ra1 inner join ");
-		sb.append("ResourceAction ra2 on (ra1.name = ra2.name) and ");
-		sb.append("(ra1.bitwiseValue = ra2.bitwiseValue) and (ra1.actionId ");
-		sb.append("!= ra2.actionId) order by ra1.resourceActionId");
+		sb.append("select name, bitwiseValue, min(resourceActionId) as ");
+		sb.append("minResourceActionId from ResourceAction group by name, ");
+		sb.append("bitwiseValue having count(resourceActionId) > 1");
+
+		String sql1 = sb.toString();
+
+		sb.setIndex(0);
+
+		sb.append("select resourceActionId, actionId from ResourceAction ");
+		sb.append("where name = ? and bitwiseValue = ? and ");
+		sb.append("resourceActionId != ?");
+
+		String sql2 = sb.toString();
 
 		try (LoggingTimer loggingTimer = new LoggingTimer();
-			PreparedStatement ps = connection.prepareStatement(sb.toString());
-			ResultSet rs = ps.executeQuery()) {
+			PreparedStatement ps1 = connection.prepareStatement(sql1);
+			PreparedStatement ps2 = connection.prepareStatement(sql2);
+			ResultSet rs1 = ps1.executeQuery()) {
 
-			Map<String, Set<Long>> nameBitwiseValuesPairs = new HashMap<>();
+			while (rs1.next()) {
+				String name = rs1.getString("name");
+				long bitwiseValue = rs1.getLong("bitwiseValue");
+				long minResourceActionId = rs1.getLong("minResourceActionId");
 
-			while (rs.next()) {
-				String name = rs.getString("name");
+				ps2.setString(1, name);
+				ps2.setLong(2, bitwiseValue);
+				ps2.setLong(3, minResourceActionId);
 
-				Set<Long> bitwiseValues = nameBitwiseValuesPairs.get(name);
+				try (ResultSet rs2 = ps2.executeQuery()) {
+					while (rs2.next()) {
+						if (_log.isInfoEnabled()) {
+							sb = new StringBundler(7);
 
-				if (bitwiseValues == null) {
-					bitwiseValues = new HashSet<>();
+							sb.append("Deleting resource action ");
+							sb.append(rs2.getString("actionId"));
+							sb.append(" from resource ");
+							sb.append(name);
+							sb.append(" because its bitwise value is the ");
+							sb.append("same as another resource action on ");
+							sb.append("the same resource");
 
-					nameBitwiseValuesPairs.put(name, bitwiseValues);
+							_log.info(sb.toString());
+						}
+
+						long resourceActionId = rs2.getLong("resourceActionId");
+
+						ResourceActionLocalServiceUtil.deleteResourceAction(
+							resourceActionId);
+					}
 				}
-
-				if (bitwiseValues.add(rs.getLong("bitwiseValue"))) {
-					continue;
-				}
-
-				if (_log.isInfoEnabled()) {
-					sb = new StringBundler(7);
-
-					sb.append("Deleting resource action ");
-					sb.append(rs.getString("actionId"));
-					sb.append(" from resource ");
-					sb.append(name);
-					sb.append(" because its bitwise value is the ");
-					sb.append("same as another resource action on ");
-					sb.append("the same resource");
-
-					_log.info(sb.toString());
-				}
-
-				ResourceActionLocalServiceUtil.deleteResourceAction(
-					rs.getLong("resourceActionId"));
 			}
 		}
 	}
