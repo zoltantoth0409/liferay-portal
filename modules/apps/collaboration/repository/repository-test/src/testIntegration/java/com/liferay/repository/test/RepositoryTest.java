350173a41cc87ff0653f56b9bc9710ce3e212512
shuyangzhou <shuyang.zhou@liferay.com>
2017-10-12 07:59:51 -0700

    LPS-75211 Fix RepositoryTest race condition

--- modules/apps/collaboration/repository/repository-test/src/testIntegration/java/com/liferay/repository/test/RepositoryTest.java
+++ modules/apps/collaboration/repository/repository-test/src/testIntegration/java/com/liferay/repository/test/RepositoryTest.java
@@ -32,6 +32,8 @@ import com.liferay.portal.kernel.service.RepositoryLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
@@ -59,6 +61,7 @@ import org.junit.runner.RunWith;
  * @author Alexander Chow
  */
 @RunWith(Arquillian.class)
+@Sync
 public class RepositoryTest {
 
 	@ClassRule
@@ -66,7 +69,8 @@ public class RepositoryTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(),
-			PermissionCheckerTestRule.INSTANCE);
+			PermissionCheckerTestRule.INSTANCE,
+			SynchronousDestinationTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
