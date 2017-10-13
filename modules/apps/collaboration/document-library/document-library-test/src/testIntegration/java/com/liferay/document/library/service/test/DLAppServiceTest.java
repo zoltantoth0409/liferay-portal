a5cdcc65c80337d37e968ece48e0884eb464be7b
drewbrokke <drew.brokke@liferay.com>
2017-10-13 10:18:29 -0700

    LPS-75201 document-library-test - DLAppServiceTest.java - ignores tests until we can stabilize the swapper

--- modules/apps/collaboration/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/service/test/DLAppServiceTest.java
+++ modules/apps/collaboration/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/service/test/DLAppServiceTest.java
@@ -192,6 +192,7 @@ public class DLAppServiceTest extends BaseDLAppTestCase {
 			addFileEntry(group.getGroupId(), parentFolder.getFolderId());
 		}
 
+		@Ignore
 		@Test(expected = FileSizeException.class)
 		public void shouldFailIfSizeLimitExceeded() throws Exception {
 			try (PrefsPropsTemporarySwapper prefsPropsReplacement =
@@ -240,6 +241,7 @@ public class DLAppServiceTest extends BaseDLAppTestCase {
 				group.getGroupId(), parentFolder.getFolderId(), sourceFileName);
 		}
 
+		@Ignore
 		@Test(expected = FileExtensionException.class)
 		public void shouldFailIfSourceFileNameExtensionNotSupported()
 			throws Exception {
@@ -1401,6 +1403,7 @@ public class DLAppServiceTest extends BaseDLAppTestCase {
 			}
 		}
 
+		@Ignore
 		@Test(expected = FileSizeException.class)
 		public void shouldFailIfSizeLimitExceeded() throws Exception {
 			String fileName = RandomTestUtil.randomString();
