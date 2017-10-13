3a30a93a052e71bc752c183e02ccd139643b47e6
Lance Ji <zheyi.ji@liferay.com>
2017-10-13 09:44:06 -0700

    LPS-75383 Fixed localization of portal-security-sso-google

--- modules/apps/foundation/portal-security-sso/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/configuration/GoogleAuthorizationConfiguration.java
+++ modules/apps/foundation/portal-security-sso/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/configuration/GoogleAuthorizationConfiguration.java
@@ -29,13 +29,22 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface GoogleAuthorizationConfiguration {
 
-	@Meta.AD(deflt = "false", description = "enabled-help", required = false)
+	@Meta.AD(
+		deflt = "false", description = "enabled-help", name = "enabled",
+		required = false
+	)
 	public boolean enabled();
 
-	@Meta.AD(deflt = "", description = "client-id-help", required = false)
+	@Meta.AD(
+		deflt = "", description = "client-id-help", name = "client-id",
+		required = false
+	)
 	public String clientId();
 
-	@Meta.AD(deflt = "", description = "client-secret-help", required = false)
+	@Meta.AD(
+		deflt = "", description = "client-secret-help", name = "client-secret",
+		required = false
+	)
 	public String clientSecret();
 
 }
