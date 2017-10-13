45fa3ba9dede9b64ccf40908079d27f9153f4ace
tomwang2011 <tong.wang@liferay.com>
2017-10-13 09:45:02 -0700

    LPS-75235 Give LogRecord a meaningful toString() method

--- portal-test/src/com/liferay/portal/kernel/test/CaptureHandler.java
+++ portal-test/src/com/liferay/portal/kernel/test/CaptureHandler.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.kernel.test;
 
+import com.liferay.portal.kernel.util.StringBundler;
+
 import java.io.Closeable;
 
 import java.util.List;
@@ -73,7 +75,22 @@ public class CaptureHandler extends Handler implements Closeable {
 
 	@Override
 	public void publish(LogRecord logRecord) {
-		_logRecords.add(logRecord);
+		PrintableLogRecord printableLogRecord = new PrintableLogRecord(
+			logRecord.getLevel(), logRecord.getMessage());
+
+		printableLogRecord.setLoggerName(logRecord.getLoggerName());
+		printableLogRecord.setMillis(logRecord.getMillis());
+		printableLogRecord.setParameters(logRecord.getParameters());
+		printableLogRecord.setResourceBundle(logRecord.getResourceBundle());
+		printableLogRecord.setResourceBundleName(
+			logRecord.getResourceBundleName());
+		printableLogRecord.setSequenceNumber(logRecord.getSequenceNumber());
+		printableLogRecord.setSourceClassName(logRecord.getSourceClassName());
+		printableLogRecord.setSourceMethodName(logRecord.getSourceMethodName());
+		printableLogRecord.setThreadID(logRecord.getThreadID());
+		printableLogRecord.setThrown(logRecord.getThrown());
+
+		_logRecords.add(printableLogRecord);
 	}
 
 	public List<LogRecord> resetLogLevel(Level level) {
@@ -89,5 +106,44 @@ public class CaptureHandler extends Handler implements Closeable {
 	private final Logger _logger;
 	private final List<LogRecord> _logRecords = new CopyOnWriteArrayList<>();
 	private final boolean _useParentHandlers;
+
+	private static class PrintableLogRecord extends LogRecord {
+
+		@Override
+		public String toString() {
+			StringBundler sb = new StringBundler();
+
+			sb.append('{');
+
+			Level level = getLevel();
+
+			if (level == null) {
+				sb.append("No Level Found");
+			}
+			else {
+				sb.append(level.toString());
+			}
+
+			sb.append(": ");
+
+			String message = getMessage();
+
+			if (message == null) {
+				sb.append("No Message Found");
+			}
+			else {
+				sb.append(message);
+			}
+
+			sb.append('}');
+
+			return sb.toString();
+		}
+
+		private PrintableLogRecord(Level level, String msg) {
+			super(level, msg);
+		}
+
+	}
 
 }
