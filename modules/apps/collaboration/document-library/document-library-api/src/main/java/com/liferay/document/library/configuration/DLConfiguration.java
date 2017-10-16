2a8164f0189c0787d1be32b3f65e5a1185b53976
Lance Ji <zheyi.ji@liferay.com>
2017-10-16 16:42:25 -0700

    LPS-75272 Fixed localization of document-libray-api

--- modules/apps/collaboration/document-library/document-library-api/src/main/java/com/liferay/document/library/configuration/DLConfiguration.java
+++ modules/apps/collaboration/document-library/document-library-api/src/main/java/com/liferay/document/library/configuration/DLConfiguration.java
@@ -30,18 +30,18 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 @ProviderType
 public interface DLConfiguration {
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(deflt = "true", name = "add-default-structures", required = false)
 	public boolean addDefaultStructures();
 
 	@Meta.AD(
 		deflt = "text/asp|text/css|text/ecmascript|text/html|text/javascript|text/x-c|text/x-fortran|text/x-java-source|text/x-pascal|text/x-script.perl|text/x-script.perl-module|text/xml",
-		required = false
+		name = "code-file-mime-types", required = false
 	)
 	public String[] codeFileMimeTypes();
 
 	@Meta.AD(
 		deflt = "application/x-7z-compressed|application/x-ace-compressed|application/x-compressed|application/x-rar-compressed|application/x-zip-compressed|application/zip",
-		required = false
+		name = "compressed-file-mime-types", required = false
 	)
 	public String[] compressedFileMimeTypes();
 
@@ -51,22 +51,25 @@ public interface DLConfiguration {
 	 */
 	@Meta.AD(
 		deflt = "com/liferay/document/library/web/template/dependencies/portlet-display-templates.xml",
-		required = false
+		name = "display-templates-config", required = false
 	)
 	public String displayTemplatesConfig();
 
-	@Meta.AD(deflt = "audio|image|video", required = false)
+	@Meta.AD(
+		deflt = "audio|image|video", name = "multimedia-file-mime-types",
+		required = false
+	)
 	public String[] multimediaFileMimeTypes();
 
 	@Meta.AD(
 		deflt = "application/mspowerpoint|application/powerpoint|application/vnd.apple.keynote|application/vnd.ms-powerpoint|application/vnd.oasis.opendocument.presentation|application/vnd.openxmlformats-officedocument.presentationml.presentation|application/x-mspowerpoint",
-		required = false
+		name = "presentation-file-mime-types", required = false
 	)
 	public String[] presentationFileMimeTypes();
 
 	@Meta.AD(
 		deflt = "application/excel|application/vnd.ms-excel|application/vnd.oasis.opendocument.spreadsheet|application/vnd.openxmlformats-officedocument.spreadsheetml.sheet|application/vnd.sun.xml.calc|application/x-excel|application/x-msexcel",
-		required = false
+		name = "spread-sheet-file-mime-types", required = false
 	)
 	public String[] spreadSheetFileMimeTypes();
 
@@ -75,16 +78,22 @@ public interface DLConfiguration {
 	 * TemporaryFileEntriesMessageListener will run to check for expired
 	 * temporary file entries.
 	 */
-	@Meta.AD(deflt = "1", required = false)
+	@Meta.AD(
+		deflt = "1", name = "temporary-file-entries-check-interval",
+		required = false
+	)
 	public int temporaryFileEntriesCheckInterval();
 
 	@Meta.AD(
 		deflt = "application/msword|application/vnd.oasis.opendocument.text|application/vnd.openxmlformats-officedocument.wordprocessingml.document|text/plain",
-		required = false
+		name = "text-file-mime-types", required = false
 	)
 	public String[] textFileMimeTypes();
 
-	@Meta.AD(deflt = "application/pdf", required = false)
+	@Meta.AD(
+		deflt = "application/pdf", name = "vectorial-file-mime-types",
+		required = false
+	)
 	public String[] vectorialFileMimeTypes();
 
 }
