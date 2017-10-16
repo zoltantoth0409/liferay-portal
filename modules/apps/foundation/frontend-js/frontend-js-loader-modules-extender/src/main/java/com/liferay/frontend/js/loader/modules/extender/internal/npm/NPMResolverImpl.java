2d906dea218d028e8dd4e9de356290797f9a4fc2
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-10-16 14:40:08 +0200

    LPS-75257 Create a common facility to be able to resolve NPM versions from the server

---
+++ modules/apps/foundation/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/npm/NPMResolverImpl.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.loader.modules.extender.internal.npm;
+
+import com.liferay.frontend.js.loader.modules.extender.npm.JSPackage;
+import com.liferay.frontend.js.loader.modules.extender.npm.JSPackageDependency;
+import com.liferay.frontend.js.loader.modules.extender.npm.ModuleNameUtil;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMRegistry;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.net.URL;
+
+import org.osgi.framework.Bundle;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+public class NPMResolverImpl implements NPMResolver {
+
+	public NPMResolverImpl(
+		Bundle bundle, JSONFactory jsonFactory, NPMRegistry npmRegistry) {
+
+		_jsonFactory = jsonFactory;
+		_npmRegistry = npmRegistry;
+		_jsPackageIdentifier = _resolveJSPackageIndentifier(bundle);
+	}
+
+	@Override
+	public JSPackage getDependencyJSPackage(String packageName) {
+		JSPackage jsPackage = _npmRegistry.getJSPackage(_jsPackageIdentifier);
+
+		JSPackageDependency jsPackageDependency =
+			jsPackage.getJSPackageDependency(packageName);
+
+		if (jsPackageDependency == null) {
+			return null;
+		}
+
+		return _npmRegistry.resolveJSPackageDependency(jsPackageDependency);
+	}
+
+	@Override
+	public String resolveModuleName(String moduleName) {
+		String packageName = ModuleNameUtil.getPackageName(moduleName);
+
+		JSPackage jsPackage = getDependencyJSPackage(packageName);
+
+		if (jsPackage == null) {
+			return null;
+		}
+
+		String packagePath = ModuleNameUtil.getPackagePath(moduleName);
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append(jsPackage.getName());
+		sb.append(StringPool.AT);
+		sb.append(jsPackage.getVersion());
+		sb.append(StringPool.SLASH);
+		sb.append(packagePath);
+
+		return sb.toString();
+	}
+
+	private String _resolveJSPackageIndentifier(Bundle bundle) {
+		try {
+			URL url = bundle.getResource("META-INF/resources/package.json");
+
+			String content = StringUtil.read(url.openStream());
+
+			JSONObject jsonObject = _jsonFactory.createJSONObject(content);
+
+			String name = jsonObject.getString("name");
+
+			String version = jsonObject.getString("version");
+
+			return
+				bundle.getBundleId() + StringPool.SLASH + name + StringPool.AT +
+					version;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private final JSONFactory _jsonFactory;
+	private final String _jsPackageIdentifier;
+	private final NPMRegistry _npmRegistry;
+
+}
---
+++ modules/apps/foundation/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/npm/NPMResolverServiceFactory.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.loader.modules.extender.internal.npm;
+
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMRegistry;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.portal.kernel.json.JSONFactory;
+
+import java.util.Hashtable;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceFactory;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@Component(immediate = true)
+public class NPMResolverServiceFactory implements ServiceFactory<NPMResolver> {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		_serviceRegistration = bundleContext.registerService(
+			NPMResolver.class, this, new Hashtable<String, Object>());
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Override
+	public NPMResolver getService(
+		Bundle bundle, ServiceRegistration<NPMResolver> registration) {
+
+		return new NPMResolverImpl(bundle, _jsonFactory, _npmRegistry);
+	}
+
+	@Override
+	public void ungetService(
+		Bundle bundle, ServiceRegistration<NPMResolver> registration,
+		NPMResolver service) {
+	}
+
+	@Reference(policyOption = ReferencePolicyOption.GREEDY)
+	private JSONFactory _jsonFactory;
+
+	@Reference(policyOption = ReferencePolicyOption.GREEDY)
+	private NPMRegistry _npmRegistry;
+
+	private ServiceRegistration<NPMResolver> _serviceRegistration;
+
+}
