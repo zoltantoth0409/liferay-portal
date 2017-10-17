4731edc4e961df6038f18ead56be3d79eb92b7e4
Lance Ji <zheyi.ji@liferay.com>
2017-10-16 17:06:01 -0700

    LPS-75272 Fixed localization of mentions-api

--- modules/apps/collaboration/mentions/mentions-api/src/main/java/com/liferay/mentions/configuration/MentionsGroupServiceConfiguration.java
+++ modules/apps/collaboration/mentions/mentions-api/src/main/java/com/liferay/mentions/configuration/MentionsGroupServiceConfiguration.java
@@ -35,25 +35,25 @@ public interface MentionsGroupServiceConfiguration {
 
 	@Meta.AD(
 		deflt = "${resource:com/liferay/mentions/configuration/dependencies/asset_entry_mention_email_body.tmpl}",
-		required = false
+		name = "asset-entry-mention-email-body", required = false
 	)
 	public LocalizedValuesMap assetEntryMentionEmailBody();
 
 	@Meta.AD(
 		deflt = "${resource:com/liferay/mentions/configuration/dependencies/asset_entry_mention_email_subject.tmpl}",
-		required = false
+		name = "asset-entry-mention-email-subject", required = false
 	)
 	public LocalizedValuesMap assetEntryMentionEmailSubject();
 
 	@Meta.AD(
 		deflt = "${resource:com/liferay/mentions/configuration/dependencies/comment_mention_email_body.tmpl}",
-		required = false
+		name = "comment-mention-email-body", required = false
 	)
 	public LocalizedValuesMap commentMentionEmailBody();
 
 	@Meta.AD(
 		deflt = "${resource:com/liferay/mentions/configuration/dependencies/comment_mention_email_subject.tmpl}",
-		required = false
+		name = "comment-mention-email-subject", required = false
 	)
 	public LocalizedValuesMap commentMentionEmailSubject();
 
