06d53cbda74660db532175e4eeea641db789d64a
Lance Ji <zheyi.ji@liferay.com>
2017-10-16 17:40:50 -0700

    LPS-75272 Fixed localization of wiki-api

--- modules/apps/collaboration/wiki/wiki-api/src/main/java/com/liferay/wiki/configuration/WikiGroupServiceConfiguration.java
+++ modules/apps/collaboration/wiki/wiki-api/src/main/java/com/liferay/wiki/configuration/WikiGroupServiceConfiguration.java
@@ -36,52 +36,56 @@ public interface WikiGroupServiceConfiguration {
 	/**
 	 * Set the default wiki format.
 	 */
-	@Meta.AD(deflt = "creole", required = false)
+	@Meta.AD(deflt = "creole", name = "default-format", required = false)
 	public String defaultFormat();
 
 	@Meta.AD(
 		deflt = "${server-property://com.liferay.portal/admin.email.from.address}",
-		required = false
+		name = "email-from-address", required = false
 	)
 	public String emailFromAddress();
 
 	@Meta.AD(
 		deflt = "${server-property://com.liferay.portal/admin.email.from.name}",
-		required = false
+		name = "email-from-name", required = false
 	)
 	public String emailFromName();
 
 	@Meta.AD(
 		deflt = "${resource:com/liferay/wiki/configuration/dependencies/email_page_added_body.tmpl}",
-		required = false
+		name = "email-page-added-body", required = false
 	)
 	public LocalizedValuesMap emailPageAddedBody();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(
+		deflt = "true", name = "email-page-added-enabled", required = false
+	)
 	public boolean emailPageAddedEnabled();
 
 	@Meta.AD(
 		deflt = "${resource:com/liferay/wiki/configuration/dependencies/email_page_added_subject.tmpl}",
-		required = false
+		name = "email-page-added-subject", required = false
 	)
 	public LocalizedValuesMap emailPageAddedSubject();
 
 	@Meta.AD(
 		deflt = "${resource:com/liferay/wiki/configuration/dependencies/email_page_updated_body.tmpl}",
-		required = false
+		name = "email-page-updated-body", required = false
 	)
 	public LocalizedValuesMap emailPageUpdatedBody();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(
+		deflt = "true", name = "email-page-updated-enabled", required = false
+	)
 	public boolean emailPageUpdatedEnabled();
 
 	@Meta.AD(
 		deflt = "${resource:com/liferay/wiki/configuration/dependencies/email_page_updated_subject.tmpl}",
-		required = false
+		name = "email-page-updated-subject", required = false
 	)
 	public LocalizedValuesMap emailPageUpdatedSubject();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(deflt = "true", name = "enable-rss", required = false)
 	public boolean enableRss();
 
 	/**
@@ -92,66 +96,77 @@ public interface WikiGroupServiceConfiguration {
 	 * letters. See http://www.usemod.com/cgi-bin/wiki.pl?WhatIsaWiki for more
 	 * information on wiki naming conventions.
 	 */
-	@Meta.AD(deflt = "FrontPage", required = false)
+	@Meta.AD(deflt = "FrontPage", name = "front-page-name", required = false)
 	public String frontPageName();
 
-	@Meta.AD(deflt = "alloyeditor_creole", required = false)
+	@Meta.AD(
+		deflt = "alloyeditor_creole", name = "get-creole-editor",
+		required = false
+	)
 	public String getCreoleEditor();
 
-	@Meta.AD(deflt = "alloyeditor", required = false)
+	@Meta.AD(deflt = "alloyeditor", name = "get-htmleditor", required = false)
 	public String getHTMLEditor();
 
-	@Meta.AD(deflt = "simple", required = false)
+	@Meta.AD(deflt = "simple", name = "get-media-wiki-editor", required = false)
 	public String getMediaWikiEditor();
 
 	/**
 	 * Set the name of the default node that will be automatically created when
 	 * the Wiki portlet is first used in a site.
 	 */
-	@Meta.AD(deflt = "Main", required = false)
+	@Meta.AD(deflt = "Main", name = "initial-node-name", required = false)
 	public String initialNodeName();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(deflt = "true", name = "page-comments-enabled", required = false)
 	public boolean pageCommentsEnabled();
 
 	/**
 	 * Set this to <code>true</code> to enable social activity notifications on
 	 * minor edits of a wiki page.
 	 */
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(
+		deflt = "true", name = "page-minor-edit-add-social-activity",
+		required = false
+	)
 	public boolean pageMinorEditAddSocialActivity();
 
 	/**
 	 * Set this to <code>true</code> to enable email notifications on minor
 	 * edits of a wiki page.
 	 */
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(
+		deflt = "false", name = "page-minor-edit-send-email", required = false
+	)
 	public boolean pageMinorEditSendEmail();
 
 	/**
 	 * Specify the supported protocols for the Creole parser.
 	 */
-	@Meta.AD(deflt = "ftp://|http://|https://|mailto:|mms://", required = false)
+	@Meta.AD(
+		deflt = "ftp://|http://|https://|mailto:|mms://",
+		name = "parsers-creole-supported-protocols", required = false
+	)
 	public String[] parsersCreoleSupportedProtocols();
 
-	@Meta.AD(deflt = "200", required = false)
+	@Meta.AD(deflt = "200", name = "rss-abstract-length", required = false)
 	public int rssAbstractLength();
 
 	@Meta.AD(
 		deflt = "${server-property://com.liferay.portal/search.container.page.default.delta}",
-		required = false
+		name = "rss-delta", required = false
 	)
 	public String rssDelta();
 
 	@Meta.AD(
 		deflt = "${server-property://com.liferay.portal/rss.feed.display.style.default}",
-		required = false
+		name = "rss-display-style", required = false
 	)
 	public String rssDisplayStyle();
 
 	@Meta.AD(
 		deflt = "${server-property://com.liferay.portal/rss.feed.type.default}",
-		required = false
+		name = "rss-feed-type", required = false
 	)
 	public String rssFeedType();
 
