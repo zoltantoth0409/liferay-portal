65ef5a7d8777f820b746b477fe738b950d9eceae
Adolfo Pérez <adolfo.perez@liferay.com>
2017-10-17 16:18:45 +0200

    LPS-52817 Register/unregister ImageIO plugins using activator

---
+++ modules/apps/collaboration/document-library/document-library-imageio-plugin/src/main/java/com/liferay/document/library/imageio/plugin/internal/activator/ImageIOPluginBundleActivator.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.imageio.plugin.internal.activator;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.imageio.spi.IIORegistry;
+import javax.imageio.spi.ImageReaderSpi;
+import javax.imageio.spi.ImageWriterSpi;
+import javax.imageio.spi.ServiceRegistry;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class ImageIOPluginBundleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		_register(ImageReaderSpi.class, _imageReaderSpiSet);
+		_register(ImageWriterSpi.class, _imageWriterSpiSet);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		_unregister(_imageReaderSpiSet);
+		_unregister(_imageWriterSpiSet);
+	}
+
+	private <T> void _register(Class<T> clazz, Set<T> registeredProviders) {
+		IIORegistry iioRegistry = IIORegistry.getDefaultInstance();
+
+		Iterator<T> providers = ServiceRegistry.lookupProviders(
+			clazz, ImageIOPluginBundleActivator.class.getClassLoader());
+
+		while (providers.hasNext()) {
+			registeredProviders.add(providers.next());
+		}
+
+		iioRegistry.registerServiceProviders(registeredProviders.iterator());
+	}
+
+	private <T> void _unregister(Set<T> registeredProviders) {
+		IIORegistry iioRegistry = IIORegistry.getDefaultInstance();
+
+		for (T provider : registeredProviders) {
+			iioRegistry.deregisterServiceProvider(provider);
+		}
+
+		registeredProviders.clear();
+	}
+
+	private final Set<ImageReaderSpi> _imageReaderSpiSet = new HashSet<>();
+	private final Set<ImageWriterSpi> _imageWriterSpiSet = new HashSet<>();
+
+}
