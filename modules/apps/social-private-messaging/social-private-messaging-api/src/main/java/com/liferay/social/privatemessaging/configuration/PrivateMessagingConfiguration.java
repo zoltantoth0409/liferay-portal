ad016ccace5ebc98826cf13770e2301d4e661469
Lance Ji <zheyi.ji@liferay.com>
2017-10-17 14:31:30 -0700

    LPS-75302 Fixed localization of social-private-messaging-api

--- modules/apps/social-private-messaging/social-private-messaging-api/src/main/java/com/liferay/social/privatemessaging/configuration/PrivateMessagingConfiguration.java
+++ modules/apps/social-private-messaging/social-private-messaging-api/src/main/java/com/liferay/social/privatemessaging/configuration/PrivateMessagingConfiguration.java
@@ -33,23 +33,30 @@ public interface PrivateMessagingConfiguration {
 
 	@Meta.AD(
 		deflt = "com/liferay/social/privatemessaging/dependencies/notification_message_body.tmpl",
-		required = false
+		name = "email-body", required = false
 	)
 	public String emailBody();
 
-	@Meta.AD(deflt = "20", required = false)
+	@Meta.AD(
+		deflt = "20", name = "autocomplete-recipient-max", required = false
+	)
 	public int autocompleteRecipientMax();
 
-	@Meta.AD(deflt = "", required = false)
+	@Meta.AD(
+		deflt = "", name = "autocomplete-recipient-site-excludes",
+		required = false
+	)
 	public String[] autocompleteRecipientSiteExcludes();
 
 	@Meta.AD(
 		deflt = "com/liferay/social/privatemessaging/dependencies/notification_message_subject.tmpl",
-		required = false
+		name = "email-subject", required = false
 	)
 	public String emailSubject();
 
-	@Meta.AD(deflt = "all", required = false)
+	@Meta.AD(
+		deflt = "all", name = "autocomplete-recipient-type", required = false
+	)
 	public String autocompleteRecipientType();
 
 }
