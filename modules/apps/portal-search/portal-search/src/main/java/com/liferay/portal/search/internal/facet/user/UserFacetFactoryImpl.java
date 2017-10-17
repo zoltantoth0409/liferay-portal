16f581287787caace9bea1f895085e7908d87c50
Bryan Engler <bryan.engler@liferay.com>
2017-10-17 11:38:24 -0700

    LPS-64398 portal-search: Add UserFacetFactoryImpl
    https://issues.liferay.com/browse/LPS-64398
    
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/facet/user/UserFacetFactoryImpl.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.facet.user;
+
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.search.facet.Facet;
+import com.liferay.portal.search.facet.FacetFactory;
+import com.liferay.portal.search.facet.user.UserFacetFactory;
+import com.liferay.portal.search.internal.facet.FacetImpl;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Bryan Engler
+ */
+@Component(
+	immediate = true, service = {FacetFactory.class, UserFacetFactory.class}
+)
+public class UserFacetFactoryImpl implements UserFacetFactory {
+
+	@Override
+	public String getFacetClassName() {
+		return Field.USER_NAME;
+	}
+
+	@Override
+	public Facet newInstance(SearchContext searchContext) {
+		return new FacetImpl(Field.USER_NAME, searchContext);
+	}
+
+}
