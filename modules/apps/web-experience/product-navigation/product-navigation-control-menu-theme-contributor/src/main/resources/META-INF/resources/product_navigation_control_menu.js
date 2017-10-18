9f26fc92add8aae9e895fe27da6fd10e69163951
Pablo Molina <pablo.molina@liferay.com>
2017-10-18 08:18:10 +0200

    LPS-74836 Added error field label scroll

--- modules/apps/web-experience/product-navigation/product-navigation-control-menu-theme-contributor/src/main/resources/META-INF/resources/product_navigation_control_menu.js
+++ modules/apps/web-experience/product-navigation/product-navigation-control-menu-theme-contributor/src/main/resources/META-INF/resources/product_navigation_control_menu.js
@@ -3,6 +3,16 @@
 		var controlMenu;
 		var controlMenuId;
 		var controlMenuScroll;
+		var errorFieldLabel;
+		var labelScroll;
+
+		errorFieldLabel = document.querySelector('.form-group.has-error .control-label');
+
+		if (errorFieldLabel) {
+			labelScroll = (errorFieldLabel.clientHeight || 0);
+
+			window.scrollBy(0, -labelScroll);
+		}
 
 		if (Liferay.ControlMenu) {
 			controlMenuId = Liferay.ControlMenu._namespace + 'ControlMenu';
