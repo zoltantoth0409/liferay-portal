5de813fcc4eb76bbdb2acfedd0529378e82800e2
Peter Shin <peter.shin@liferay.com>
2017-10-18 16:02:46 -0700

    LPS-74536 Gradle file parser

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BaseGradleFileCheck.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.source.formatter.parser.GradleFile;
+
+/**
+ * @author Peter Shin
+ */
+public abstract class BaseGradleFileCheck
+	extends BaseSourceCheck implements GradleFileCheck {
+
+	@Override
+	public String process(
+			String fileName, String absolutePath, GradleFile gradleFile,
+			String content)
+		throws Exception {
+
+		clearSourceFormatterMessages(fileName);
+
+		return doProcess(fileName, absolutePath, gradleFile, content);
+	}
+
+	protected abstract String doProcess(
+			String filename, String absolutePath, GradleFile gradleFile,
+			String fileContent)
+		throws Exception;
+
+}
---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/parser/GradleFile.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.parser;
+
+import java.util.Set;
+
+/**
+ * @author Peter Shin
+ */
+public class GradleFile {
+
+	public GradleFile(
+		Set<String> applyPlugins, String bodyBlock, String buildScriptBlock,
+		String content, String extScriptBlock, String fileName,
+		Set<String> imports, String initializeBlock, Set<String> properties,
+		Set<String> tasks) {
+
+		_applyPlugins = applyPlugins;
+		_bodyBlock = bodyBlock;
+		_buildScriptBlock = buildScriptBlock;
+		_content = content;
+		_extScriptBlock = extScriptBlock;
+		_fileName = fileName;
+		_imports = imports;
+		_initializeBlock = initializeBlock;
+		_properties = properties;
+		_tasks = tasks;
+	}
+
+	public Set<String> getApplyPlugins() {
+		return _applyPlugins;
+	}
+
+	public String getBodyBlock() {
+		return _bodyBlock;
+	}
+
+	public String getBuildScriptBlock() {
+		return _buildScriptBlock;
+	}
+
+	public String getContent() {
+		return _content;
+	}
+
+	public String getExtScriptBlock() {
+		return _extScriptBlock;
+	}
+
+	public String getFileName() {
+		return _fileName;
+	}
+
+	public Set<String> getImports() {
+		return _imports;
+	}
+
+	public String getInitializeBlock() {
+		return _initializeBlock;
+	}
+
+	public Set<String> getProperties() {
+		return _properties;
+	}
+
+	public Set<String> getTasks() {
+		return _tasks;
+	}
+
+	private final Set<String> _applyPlugins;
+	private final String _bodyBlock;
+	private final String _buildScriptBlock;
+	private final String _content;
+	private final String _extScriptBlock;
+	private final String _fileName;
+	private final Set<String> _imports;
+	private final String _initializeBlock;
+	private final Set<String> _properties;
+	private final Set<String> _tasks;
+
+}
