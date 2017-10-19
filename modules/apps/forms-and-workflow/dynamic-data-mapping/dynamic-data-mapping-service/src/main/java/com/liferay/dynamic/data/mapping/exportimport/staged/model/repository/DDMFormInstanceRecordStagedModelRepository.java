58914aa85facf00a4716548822a1c1c21311e1e1
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-10-19 11:28:24 -0300

    LPS-75159 Copy and adapt staging classes

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/exportimport/staged/model/repository/DDMFormInstanceRecordStagedModelRepository.java
@@ -0,0 +1,315 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.exportimport.staged.model.repository;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecordVersion;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordVersionLocalService;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerRegistryUtil;
+import com.liferay.exportimport.kernel.lar.StagedModelModifiedDateComparator;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.exportimport.staged.model.repository.base.BaseStagedModelRepository;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tamas Molnar
+ */
+@Component(
+	immediate = true,
+	property = {
+		"model.class.name=com.liferay.dynamic.data.lists.model.DDMFormInstanceRecord"
+	},
+	service = {
+		DDMFormInstanceRecordStagedModelRepository.class,
+		StagedModelRepository.class
+	}
+)
+public class DDMFormInstanceRecordStagedModelRepository
+	extends BaseStagedModelRepository<DDMFormInstanceRecord> {
+
+	@Override
+	public DDMFormInstanceRecord addStagedModel(
+			PortletDataContext portletDataContext,
+			DDMFormInstanceRecord ddmFormInstanceRecord)
+		throws PortalException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public DDMFormInstanceRecord addStagedModel(
+			PortletDataContext portletDataContext,
+			DDMFormInstanceRecord ddmFormInstanceRecord,
+			DDMFormValues ddmFormValues)
+		throws PortalException {
+
+		long userId = portletDataContext.getUserId(
+			ddmFormInstanceRecord.getUserUuid());
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			ddmFormInstanceRecord);
+
+		if (portletDataContext.isDataStrategyMirror()) {
+			serviceContext.setUuid(ddmFormInstanceRecord.getUuid());
+		}
+
+		DDMFormInstanceRecord importedFormInstanceRecord =
+			_ddmFormInstanceRecordLocalService.addFormInstanceRecord(
+				userId, ddmFormInstanceRecord.getGroupId(),
+				ddmFormInstanceRecord.getFormInstanceId(), ddmFormValues,
+				serviceContext);
+
+		updateVersions(
+			importedFormInstanceRecord, ddmFormInstanceRecord.getVersion());
+
+		return importedFormInstanceRecord;
+	}
+
+	@Override
+	public void deleteStagedModel(DDMFormInstanceRecord ddmFormInstanceRecord)
+		throws PortalException {
+
+		_ddmFormInstanceRecordLocalService.deleteFormInstanceRecord(
+			ddmFormInstanceRecord);
+	}
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException {
+
+		DDMFormInstanceRecord ddlRrecord = fetchStagedModelByUuidAndGroupId(
+			uuid, groupId);
+
+		if (ddlRrecord != null) {
+			deleteStagedModel(ddlRrecord);
+		}
+	}
+
+	@Override
+	public void deleteStagedModels(PortletDataContext portletDataContext)
+		throws PortalException {
+	}
+
+	@Override
+	public DDMFormInstanceRecord fetchStagedModelByUuidAndGroupId(
+		String uuid, long groupId) {
+
+		return _ddmFormInstanceRecordLocalService.
+			fetchDDMFormInstanceRecordByUuidAndGroupId(uuid, groupId);
+	}
+
+	@Override
+	public List<DDMFormInstanceRecord> fetchStagedModelsByUuidAndCompanyId(
+		String uuid, long companyId) {
+
+		return _ddmFormInstanceRecordLocalService.
+			getDDMFormInstanceRecordsByUuidAndCompanyId(
+				uuid, companyId, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				new StagedModelModifiedDateComparator<DDMFormInstanceRecord>());
+	}
+
+	@Override
+	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
+		PortletDataContext portletDataContext) {
+
+		ExportActionableDynamicQuery exportActionableDynamicQuery =
+			_ddmFormInstanceRecordLocalService.getExportActionableDynamicQuery(
+				portletDataContext);
+
+		final ActionableDynamicQuery.AddCriteriaMethod addCriteriaMethod =
+			exportActionableDynamicQuery.getAddCriteriaMethod();
+
+		exportActionableDynamicQuery.setAddCriteriaMethod(
+			new ActionableDynamicQuery.AddCriteriaMethod() {
+
+				@Override
+				public void addCriteria(DynamicQuery dynamicQuery) {
+					addCriteriaMethod.addCriteria(dynamicQuery);
+
+					Property formInstanceRecordIdProperty =
+						PropertyFactoryUtil.forName("formInstanceRecordId");
+
+					DynamicQuery formInstanceRecordVersionDynamicQuery =
+						getRecordVersionDynamicQuery();
+
+					dynamicQuery.add(
+						formInstanceRecordIdProperty.in(
+							formInstanceRecordVersionDynamicQuery));
+
+					Property formInstanceIdProperty =
+						PropertyFactoryUtil.forName("formInstanceId");
+
+					DynamicQuery formInstanceDynamicQuery =
+						getFormInstanceDynamicQuery();
+
+					dynamicQuery.add(
+						formInstanceIdProperty.in(formInstanceDynamicQuery));
+				}
+
+			});
+
+		return exportActionableDynamicQuery;
+	}
+
+	@Override
+	public DDMFormInstanceRecord saveStagedModel(
+			DDMFormInstanceRecord ddmFormInstanceRecord)
+		throws PortalException {
+
+		return _ddmFormInstanceRecordLocalService.updateDDMFormInstanceRecord(
+			ddmFormInstanceRecord);
+	}
+
+	@Override
+	public DDMFormInstanceRecord updateStagedModel(
+			PortletDataContext portletDataContext,
+			DDMFormInstanceRecord ddmFormInstanceRecord)
+		throws PortalException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public DDMFormInstanceRecord updateStagedModel(
+			PortletDataContext portletDataContext,
+			DDMFormInstanceRecord ddmFormInstanceRecord,
+			DDMFormValues ddmFormValues)
+		throws PortalException {
+
+		long userId = portletDataContext.getUserId(
+			ddmFormInstanceRecord.getUserUuid());
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			ddmFormInstanceRecord);
+
+		DDMFormInstanceRecord importedFormInstanceRecord =
+			_ddmFormInstanceRecordLocalService.updateFormInstanceRecord(
+				userId, ddmFormInstanceRecord.getFormInstanceRecordId(), false,
+				ddmFormValues, serviceContext);
+
+		updateVersions(
+			importedFormInstanceRecord, ddmFormInstanceRecord.getVersion());
+
+		return importedFormInstanceRecord;
+	}
+
+	protected DynamicQuery getFormInstanceDynamicQuery() {
+		StagedModelDataHandler<?> stagedModelDataHandler =
+			StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(
+				DDMFormInstanceRecord.class.getName());
+
+		Class<?> clazz = stagedModelDataHandler.getClass();
+
+		DynamicQuery formInstanceDynamicQuery =
+			DynamicQueryFactoryUtil.forClass(
+				DDMFormInstance.class, "formInstance", clazz.getClassLoader());
+
+		formInstanceDynamicQuery.setProjection(
+			ProjectionFactoryUtil.property("formInstanceId"));
+
+		formInstanceDynamicQuery.add(
+			RestrictionsFactoryUtil.eqProperty(
+				"formInstance.formInstanceId", "formInstanceId"));
+
+		return formInstanceDynamicQuery;
+	}
+
+	protected DynamicQuery getRecordVersionDynamicQuery() {
+		StagedModelDataHandler<?> stagedModelDataHandler =
+			StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(
+				DDMFormInstanceRecord.class.getName());
+
+		Class<?> clazz = stagedModelDataHandler.getClass();
+
+		DynamicQuery formInstanceRecordVersionDynamicQuery =
+			DynamicQueryFactoryUtil.forClass(
+				DDMFormInstanceRecordVersion.class, "formInstanceRecordVersion",
+				clazz.getClassLoader());
+
+		formInstanceRecordVersionDynamicQuery.setProjection(
+			ProjectionFactoryUtil.property("formInstanceRecordVersionId"));
+
+		Property statusProperty = PropertyFactoryUtil.forName("status");
+
+		formInstanceRecordVersionDynamicQuery.add(
+			statusProperty.in(stagedModelDataHandler.getExportableStatuses()));
+
+		formInstanceRecordVersionDynamicQuery.add(
+			RestrictionsFactoryUtil.eqProperty(
+				"formInstanceRecordVersion.version", "version"));
+		formInstanceRecordVersionDynamicQuery.add(
+			RestrictionsFactoryUtil.eqProperty(
+				"formInstanceRecordVersion.formInstanceRecordVersionId",
+				"formInstanceRecordVersionId"));
+
+		return formInstanceRecordVersionDynamicQuery;
+	}
+
+	protected void updateVersions(
+			DDMFormInstanceRecord importedFormInstanceRecord, String version)
+		throws PortalException {
+
+		if (Objects.equals(importedFormInstanceRecord.getVersion(), version)) {
+			return;
+		}
+
+		DDMFormInstanceRecordVersion importedFormInstanceRecordVersion =
+			importedFormInstanceRecord.getFormInstanceRecordVersion();
+
+		importedFormInstanceRecordVersion.setVersion(version);
+
+		_ddmFormInstanceVersionLocalService.updateDDMFormInstanceRecordVersion(
+			importedFormInstanceRecordVersion);
+
+		importedFormInstanceRecord.setVersion(version);
+
+		_ddmFormInstanceRecordLocalService.updateDDMFormInstanceRecord(
+			importedFormInstanceRecord);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMFormInstanceRecordStagedModelRepository.class);
+
+	@Reference
+	private DDMFormInstanceRecordLocalService
+		_ddmFormInstanceRecordLocalService;
+
+	@Reference
+	private DDMFormInstanceRecordVersionLocalService
+		_ddmFormInstanceVersionLocalService;
+
+}
---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/exportimport/staged/model/repository/DDMFormInstanceStagedModelRepository.java
@@ -0,0 +1,206 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.exportimport.staged.model.repository;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.util.comparator.DDMFormInstanceNameComparator;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.exportimport.kernel.lar.StagedModelModifiedDateComparator;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.exportimport.staged.model.repository.base.BaseStagedModelRepository;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tamas Molnar
+ */
+@Component(
+	immediate = true,
+	property = {
+		"model.class.name=com.liferay.dynamic.data.lists.model.DDMFormInstance"
+	},
+	service = {
+		DDMFormInstanceStagedModelRepository.class, StagedModelRepository.class
+	}
+)
+public class DDMFormInstanceStagedModelRepository
+	extends BaseStagedModelRepository<DDMFormInstance> {
+
+	@Override
+	public DDMFormInstance addStagedModel(
+			PortletDataContext portletDataContext,
+			DDMFormInstance ddmFormInstance)
+		throws PortalException {
+
+		long userId = portletDataContext.getUserId(
+			ddmFormInstance.getUserUuid());
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			ddmFormInstance);
+
+		if (portletDataContext.isDataStrategyMirror()) {
+			serviceContext.setUuid(ddmFormInstance.getUuid());
+		}
+
+		return _ddmFormInstanceLocalService.addFormInstance(
+			userId, ddmFormInstance.getGroupId(),
+			ddmFormInstance.getStructureId(), ddmFormInstance.getNameMap(),
+			ddmFormInstance.getDescriptionMap(), ddmFormInstance.getSettings(),
+			serviceContext);
+	}
+
+	@Override
+	public void deleteStagedModel(DDMFormInstance ddmFormInstance)
+		throws PortalException {
+
+		_ddmFormInstanceLocalService.deleteFormInstance(ddmFormInstance);
+	}
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException {
+
+		DDMFormInstance ddmFormInstance = fetchStagedModelByUuidAndGroupId(
+			uuid, groupId);
+
+		if (ddmFormInstance != null) {
+			deleteStagedModel(ddmFormInstance);
+		}
+	}
+
+	@Override
+	public void deleteStagedModels(PortletDataContext portletDataContext)
+		throws PortalException {
+
+		Set<Long> formInstanceDDMStructureIds = new HashSet<>();
+
+		List<DDMFormInstance> formInstances =
+			_ddmFormInstanceLocalService.search(
+				portletDataContext.getCompanyId(),
+				portletDataContext.getScopeGroupId(), null, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS, new DDMFormInstanceNameComparator());
+
+		for (DDMFormInstance formInstance : formInstances) {
+			formInstanceDDMStructureIds.add(formInstance.getStructureId());
+
+			_ddmFormInstanceLocalService.deleteFormInstance(formInstance);
+		}
+
+		deleteDDMStructures(formInstanceDDMStructureIds);
+	}
+
+	@Override
+	public DDMFormInstance fetchStagedModelByUuidAndGroupId(
+		String uuid, long groupId) {
+
+		return _ddmFormInstanceLocalService.
+			fetchDDMFormInstanceByUuidAndGroupId(uuid, groupId);
+	}
+
+	@Override
+	public List<DDMFormInstance> fetchStagedModelsByUuidAndCompanyId(
+		String uuid, long companyId) {
+
+		return _ddmFormInstanceLocalService.
+			getDDMFormInstancesByUuidAndCompanyId(
+				uuid, companyId, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				new StagedModelModifiedDateComparator<DDMFormInstance>());
+	}
+
+	@Override
+	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
+		final PortletDataContext portletDataContext) {
+
+		ExportActionableDynamicQuery exportActionableDynamicQuery =
+			_ddmFormInstanceLocalService.getExportActionableDynamicQuery(
+				portletDataContext);
+
+		exportActionableDynamicQuery.setPerformActionMethod(
+			new ActionableDynamicQuery.PerformActionMethod<DDMFormInstance>() {
+
+				@Override
+				public void performAction(DDMFormInstance ddmFormInstance)
+					throws PortalException {
+
+					StagedModelDataHandlerUtil.exportStagedModel(
+						portletDataContext, ddmFormInstance);
+
+					StagedModelDataHandlerUtil.exportStagedModel(
+						portletDataContext, ddmFormInstance.getStructure());
+				}
+
+			});
+
+		return exportActionableDynamicQuery;
+	}
+
+	@Override
+	public DDMFormInstance saveStagedModel(DDMFormInstance ddmFormInstance)
+		throws PortalException {
+
+		return _ddmFormInstanceLocalService.updateDDMFormInstance(
+			ddmFormInstance);
+	}
+
+	@Override
+	public DDMFormInstance updateStagedModel(
+			PortletDataContext portletDataContext,
+			DDMFormInstance ddmFormInstance)
+		throws PortalException {
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			ddmFormInstance);
+
+		return _ddmFormInstanceLocalService.updateFormInstance(
+			ddmFormInstance.getFormInstanceId(),
+			ddmFormInstance.getStructureId(), ddmFormInstance.getNameMap(),
+			ddmFormInstance.getDescriptionMap(), ddmFormInstance.getSettings(),
+			serviceContext);
+	}
+
+	protected void deleteDDMStructures(Set<Long> ddmStructureIds)
+		throws PortalException {
+
+		for (Long ddmStructureId : ddmStructureIds) {
+			_ddmStructureLocalService.deleteStructure(ddmStructureId);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMFormInstanceStagedModelRepository.class);
+
+	@Reference
+	private DDMFormInstanceLocalService _ddmFormInstanceLocalService;
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+}
