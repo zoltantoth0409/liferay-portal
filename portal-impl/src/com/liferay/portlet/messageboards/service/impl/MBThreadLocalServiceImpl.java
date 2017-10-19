96719b613e41d3450cc34a11fefad2a4117c75f1
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-10-19 19:11:23 +0200

    LPS-75320 Remove dependencies from MBThreadLocalService

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadLocalServiceImpl.java
@@ -63,9 +63,7 @@ import com.liferay.trash.kernel.model.TrashVersion;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author Brian Wing Shun Chan
@@ -199,13 +197,6 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 			// Indexer
 
 			messageIndexer.delete(message);
-
-			// Statistics
-
-			if (!message.isDiscussion()) {
-				mbStatsUserLocalService.updateStatsUser(
-					message.getGroupId(), message.getUserId());
-			}
 
 			// Workflow
 
@@ -525,8 +516,6 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 			long groupId, long threadId, long trashEntryId)
 		throws PortalException {
 
-		Set<Long> userIds = new HashSet<>();
-
 		MBThread thread = mbThreadLocalService.getThread(threadId);
 
 		List<MBMessage> messages = mbMessageLocalService.getThreadMessages(
@@ -545,8 +534,6 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 			message.setStatus(WorkflowConstants.STATUS_IN_TRASH);
 
 			mbMessagePersistence.update(message);
-
-			userIds.add(message.getUserId());
 
 			// Trash
 
@@ -590,12 +577,6 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 					message.getCompanyId(), message.getGroupId(),
 					MBMessage.class.getName(), message.getMessageId());
 			}
-		}
-
-		// Statistics
-
-		for (long userId : userIds) {
-			mbStatsUserLocalService.updateStatsUser(groupId, userId);
 		}
 	}
 
@@ -790,8 +771,6 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 	public void restoreDependentsFromTrash(long groupId, long threadId)
 		throws PortalException {
 
-		Set<Long> userIds = new HashSet<>();
-
 		MBThread thread = mbThreadLocalService.getThread(threadId);
 
 		List<MBMessage> messages = mbMessageLocalService.getThreadMessages(
@@ -817,8 +796,6 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 			message.setStatus(oldStatus);
 
 			mbMessagePersistence.update(message);
-
-			userIds.add(message.getUserId());
 
 			// Trash
 
@@ -848,12 +825,6 @@ public class MBThreadLocalServiceImpl extends MBThreadLocalServiceBaseImpl {
 				MBMessage.class);
 
 			indexer.reindex(message);
-		}
-
-		// Statistics
-
-		for (long userId : userIds) {
-			mbStatsUserLocalService.updateStatsUser(groupId, userId);
 		}
 	}
 
