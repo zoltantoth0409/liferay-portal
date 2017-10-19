7b54b3a0250bc54f73d67690b182917fd2b14e19
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-10-19 15:17:02 +0200

    LPS-75320 SF

--- modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/model/impl/MBStatsUserImpl.java
+++ modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/model/impl/MBStatsUserImpl.java
@@ -17,21 +17,8 @@ package com.liferay.message.boards.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the MBStatsUser service. Represents a row in the &quot;MBStatsUser&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.message.boards.model.MBStatsUser} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class MBStatsUserImpl extends MBStatsUserBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a message boards stats user model instance should use the {@link com.liferay.message.boards.model.MBStatsUser} interface instead.
-	 */
-	public MBStatsUserImpl() {
-	}
 }
