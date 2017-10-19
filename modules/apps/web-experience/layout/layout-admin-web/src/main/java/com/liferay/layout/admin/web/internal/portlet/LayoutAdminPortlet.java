9ab34e8a3b8d2f326f4984e1767ca5d1c9fb28a7
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-10-19 11:34:53 +0200

    LPS-75282 Capture exception in portlet

--- modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/LayoutAdminPortlet.java
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/LayoutAdminPortlet.java
@@ -22,6 +22,8 @@ import com.liferay.exportimport.kernel.staging.Staging;
 import com.liferay.item.selector.ItemSelector;
 import com.liferay.layout.admin.web.internal.constants.LayoutAdminPortletKeys;
 import com.liferay.layout.admin.web.internal.constants.LayoutAdminWebKeys;
+import com.liferay.layout.page.template.exception.DuplicateLayoutPageTemplateCollectionException;
+import com.liferay.layout.page.template.exception.LayoutPageTemplateCollectionNameException;
 import com.liferay.mobile.device.rules.model.MDRAction;
 import com.liferay.mobile.device.rules.model.MDRRuleGroupInstance;
 import com.liferay.mobile.device.rules.service.MDRActionLocalService;
@@ -1060,10 +1062,12 @@ public class LayoutAdminPortlet extends MVCPortlet {
 	@Override
 	protected boolean isSessionErrorException(Throwable cause) {
 		if (cause instanceof AssetCategoryException ||
+			cause instanceof DuplicateLayoutPageTemplateCollectionException ||
 			cause instanceof ImageTypeException ||
 			cause instanceof LayoutFriendlyURLException ||
 			cause instanceof LayoutFriendlyURLsException ||
 			cause instanceof LayoutNameException ||
+			cause instanceof LayoutPageTemplateCollectionNameException ||
 			cause instanceof LayoutParentLayoutIdException ||
 			cause instanceof LayoutSetVirtualHostException ||
 			cause instanceof LayoutTypeException ||
