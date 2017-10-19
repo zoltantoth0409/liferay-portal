ede768eb295325311254d73f5cc3ef1ec28e87e9
Pavel Savinov <pavel.savinov@liferay.com>
2017-10-19 11:11:27 +0200

    LPS-75373 Default implementation of menu item type controller to be able to add urls to navigation menu

---
+++ modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/type/controller/URLSiteNavigationMenuItemTypeController.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.admin.web.internal.type.controller;
+
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.type.controller.SiteNavigationMenuItemTypeController;
+import com.liferay.site.navigation.type.controller.impl.BaseSiteNavigationMenuItemTypeControllerImpl;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true, property = {"site.navigation.menu.item.type=url"},
+	service = SiteNavigationMenuItemTypeController.class
+)
+public class URLSiteNavigationMenuItemTypeController
+	extends BaseSiteNavigationMenuItemTypeControllerImpl {
+
+	@Override
+	public JSONObject getEditContext(
+			HttpServletRequest request, HttpServletResponse response,
+			SiteNavigationMenuItem siteNavigationMenuItem)
+		throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public String getIcon() {
+		return "link";
+	}
+
+	@Override
+	public String getType() {
+		return "url";
+	}
+
+	@Override
+	public String getURL(
+			HttpServletRequest request, HttpServletResponse response,
+			SiteNavigationMenuItem siteNavigationMenuItem)
+		throws Exception {
+
+		UnicodeProperties properties = getTypeSettingsProperties(
+			siteNavigationMenuItem);
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray(
+			properties.getProperty("value"));
+
+		JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+		return jsonObject.getString("value");
+	}
+
+	@Override
+	public JSONObject getViewContext(
+			HttpServletRequest request, HttpServletResponse response,
+			SiteNavigationMenuItem siteNavigationMenuItem)
+		throws Exception {
+
+		return null;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.site.navigation.admin.web)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+}
