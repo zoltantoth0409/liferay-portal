bfdb5fdbdffa380afac33b69732f22c064c5f2b9
Preston-Crary <preston.crary@liferay.com>
2017-10-19 15:44:08 -0700

    LPS-75364 Remove usages of UnsafeRunnable.

--- portal-impl/src/com/liferay/portal/repository/temporaryrepository/TemporaryFileEntryRepository.java
+++ portal-impl/src/com/liferay/portal/repository/temporaryrepository/TemporaryFileEntryRepository.java
@@ -25,12 +25,12 @@ import com.liferay.document.library.kernel.service.DLFileVersionLocalService;
 import com.liferay.document.library.kernel.service.DLFileVersionService;
 import com.liferay.document.library.kernel.service.DLFolderLocalService;
 import com.liferay.document.library.kernel.service.DLFolderService;
+import com.liferay.petra.function.UnsafeSupplier;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.RepositoryLocalService;
 import com.liferay.portal.kernel.service.RepositoryService;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.systemevent.SystemEventHierarchyEntryThreadLocal;
-import com.liferay.portal.kernel.util.UnsafeRunnable;
 import com.liferay.portal.repository.liferayrepository.LiferayRepository;
 
 /**
@@ -65,29 +65,54 @@ public class TemporaryFileEntryRepository extends LiferayRepository {
 
 	@Override
 	public void deleteAll() {
-		_runWithoutSystemEvents(super::deleteAll);
+		_runWithoutSystemEvents(
+			() -> {
+				super.deleteAll();
+
+				return null;
+			});
 	}
 
 	@Override
 	public void deleteFileEntry(long fileEntryId) throws PortalException {
-		_runWithoutSystemEvents(() -> super.deleteFileEntry(fileEntryId));
+		_runWithoutSystemEvents(
+			() -> {
+				super.deleteFileEntry(fileEntryId);
+
+				return null;
+			});
 	}
 
 	@Override
 	public void deleteFileEntry(long folderId, String title)
 		throws PortalException {
 
-		_runWithoutSystemEvents(() -> super.deleteFileEntry(folderId, title));
+		_runWithoutSystemEvents(
+			() -> {
+				super.deleteFileEntry(folderId, title);
+
+				return null;
+			});
 	}
 
 	@Override
 	public void deleteFileShortcut(long fileShortcutId) throws PortalException {
-		_runWithoutSystemEvents(() -> super.deleteFileShortcut(fileShortcutId));
+		_runWithoutSystemEvents(
+			() -> {
+				super.deleteFileShortcut(fileShortcutId);
+
+				return null;
+			});
 	}
 
 	@Override
 	public void deleteFileShortcuts(long toFileEntryId) throws PortalException {
-		_runWithoutSystemEvents(() -> super.deleteFileShortcuts(toFileEntryId));
+		_runWithoutSystemEvents(
+			() -> {
+				super.deleteFileShortcuts(toFileEntryId);
+
+				return null;
+			});
 	}
 
 	@Override
@@ -95,29 +120,43 @@ public class TemporaryFileEntryRepository extends LiferayRepository {
 		throws PortalException {
 
 		_runWithoutSystemEvents(
-			() -> super.deleteFileVersion(fileEntryId, version));
+			() -> {
+				super.deleteFileVersion(fileEntryId, version);
+
+				return null;
+			});
 	}
 
 	@Override
 	public void deleteFolder(long folderId) throws PortalException {
-		_runWithoutSystemEvents(() -> super.deleteFolder(folderId));
+		_runWithoutSystemEvents(
+			() -> {
+				super.deleteFolder(folderId);
+
+				return null;
+			});
 	}
 
 	@Override
 	public void deleteFolder(long parentFolderId, String name)
 		throws PortalException {
 
-		_runWithoutSystemEvents(() -> super.deleteFolder(parentFolderId, name));
+		_runWithoutSystemEvents(
+			() -> {
+				super.deleteFolder(parentFolderId, name);
+
+				return null;
+			});
 	}
 
 	private <T extends Throwable> void _runWithoutSystemEvents(
-			UnsafeRunnable<T> unsafeRunnable)
+			UnsafeSupplier<Void, T> unsafeSupplier)
 		throws T {
 
 		SystemEventHierarchyEntryThreadLocal.push(DLFileEntry.class);
 
 		try {
-			unsafeRunnable.run();
+			unsafeSupplier.get();
 		}
 		finally {
 			SystemEventHierarchyEntryThreadLocal.pop(DLFileEntry.class);
