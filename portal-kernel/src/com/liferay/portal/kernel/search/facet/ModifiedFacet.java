83130503eb1d875e2a719f641f3d3aa6d23a567f
Bryan Engler <bryan.engler@liferay.com>
2017-10-20 13:46:50 -0700

    LPS-64398 portal-kernel: Deprecate ModifiedFacet
    https://issues.liferay.com/browse/LPS-64398
    
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

--- portal-kernel/src/com/liferay/portal/kernel/search/facet/ModifiedFacet.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/facet/ModifiedFacet.java
@@ -29,8 +29,10 @@ import java.text.DateFormat;
 import java.util.Calendar;
 
 /**
- * @author Raymond Augé
+ * @author     Raymond Augé
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class ModifiedFacet extends RangeFacet {
 
 	public ModifiedFacet(SearchContext searchContext) {
--- portal-kernel/src/com/liferay/portal/kernel/search/facet/ModifiedFacetFactory.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/facet/ModifiedFacetFactory.java
@@ -18,8 +18,11 @@ import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.facet.util.FacetFactory;
 
 /**
- * @author Raymond Augé
+ * @author     Raymond Augé
+ * @deprecated As of 7.0.0, replaced by {@link
+ *             com.liferay.portal.search.facet.modified.ModifiedFacetFactory}
  */
+@Deprecated
 public class ModifiedFacetFactory implements FacetFactory {
 
 	@Override
