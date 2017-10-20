d6857b18089ab609fc68ea85e755c2ae69e3e845
Máté Thurzó <mate.thurzo@liferay.com>
2017-10-20 17:21:04 +0200

    LPS-74083 Update tests

--- modules/apps/web-experience/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/internal/content/processor/test/DefaultExportImportContentProcessorTest.java
+++ modules/apps/web-experience/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/internal/content/processor/test/DefaultExportImportContentProcessorTest.java
@@ -362,7 +362,10 @@ public class DefaultExportImportContentProcessorTest {
 		Assert.assertFalse(
 			content.contains(
 				PropsValues.LAYOUT_FRIENDLY_URL_PUBLIC_SERVLET_MAPPING));
-		Assert.assertFalse(content.contains(_stagingGroup.getFriendlyURL()));
+		Assert.assertTrue(
+			content.contains(
+				"@data_handler_group_friendly_url@@" +
+					_stagingGroup.getFriendlyURL() + "@"));
 		Assert.assertFalse(content.contains(PortalUtil.getPathContext()));
 		Assert.assertFalse(content.contains("/en/en"));
 
@@ -419,7 +422,10 @@ public class DefaultExportImportContentProcessorTest {
 		Assert.assertFalse(
 			content.contains(
 				PropsValues.LAYOUT_FRIENDLY_URL_PUBLIC_SERVLET_MAPPING));
-		Assert.assertFalse(content.contains(_stagingGroup.getFriendlyURL()));
+		Assert.assertTrue(
+			content.contains(
+				"@data_handler_group_friendly_url@@" +
+					_stagingGroup.getFriendlyURL() + "@"));
 		Assert.assertFalse(content.contains("/en/en"));
 
 		setFinalStaticField(
