8bfda6ad6d9d014c0673854793735f29242ac4fc
Adolfo Pérez <adolfo.perez@liferay.com>
2017-10-20 11:34:55 +0200

    LPS-75317 Autogenerated

--- modules/apps/collaboration/message-boards/message-boards-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/collaboration/message-boards/message-boards-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,2 +1,8 @@
+create index IX_69951A25 on MBBan (banUserId);
+create unique index IX_8ABC4E3B on MBBan (groupId, banUserId);
+create index IX_48814BBA on MBBan (userId);
+create index IX_4F841574 on MBBan (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_2A3B68F6 on MBBan (uuid_[$COLUMN_LENGTH:75$], groupId);
+
 create unique index IX_9168E2C9 on MBStatsUser (groupId, userId);
 create index IX_847F92B5 on MBStatsUser (userId);
--- modules/apps/collaboration/message-boards/message-boards-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/collaboration/message-boards/message-boards-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,3 +1,16 @@
+create table MBBan (
+	uuid_ VARCHAR(75) null,
+	banId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	banUserId LONG,
+	lastPublishDate DATE null
+);
+
 create table MBStatsUser (
 	statsUserId LONG not null primary key,
 	groupId LONG,
