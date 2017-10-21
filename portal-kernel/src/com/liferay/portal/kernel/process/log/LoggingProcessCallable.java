68a66871089dd1b30b04a7ea7dd59f1fba8c54ba
shuyangzhou <shuyang.zhou@liferay.com>
2017-10-20 22:06:46 -0700

    LPS-75426 Deprecated LoggingProcessCallable

--- portal-kernel/src/com/liferay/portal/kernel/process/log/LoggingProcessCallable.java
+++ portal-kernel/src/com/liferay/portal/kernel/process/log/LoggingProcessCallable.java
@@ -23,7 +23,9 @@ import java.io.IOException;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class LoggingProcessCallable implements ProcessCallable<String> {
 
 	public LoggingProcessCallable(byte[] bytes) {
