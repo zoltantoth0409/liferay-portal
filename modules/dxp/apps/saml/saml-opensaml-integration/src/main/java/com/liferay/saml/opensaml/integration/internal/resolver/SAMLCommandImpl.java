7ec614ead25a66a1114003fbc509a873a7733096
Carlos Sierra <carlos.sierra@liferay.com>
2017-10-23 17:25:05 +0200

    LPS-73885 Generic implementation for SAMLCommand and SAMLContext

---
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/resolver/SAMLCommandImpl.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.saml.opensaml.integration.internal.resolver;
+
+import com.liferay.saml.opensaml.integration.resolver.Resolver;
+import com.liferay.saml.opensaml.integration.resolver.Resolver.SAMLCommand;
+
+import java.util.function.Function;
+
+import org.opensaml.common.SAMLObject;
+import org.opensaml.common.binding.SAMLMessageContext;
+import org.opensaml.saml2.core.NameID;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public class SAMLCommandImpl
+	<InboundMessageType extends SAMLObject,
+		OutboundMessageType extends SAMLObject, T, R extends Resolver>
+			implements SAMLCommand<T, R> {
+
+	public SAMLCommandImpl(
+		Function<
+			SAMLMessageContext<
+				InboundMessageType, OutboundMessageType, NameID>, T>
+					samlMessageContextFunction) {
+
+		_samlMessageContextFunction = samlMessageContextFunction;
+	}
+
+	protected Function
+		<SAMLMessageContext<InboundMessageType, OutboundMessageType, NameID>, T>
+			getSamlMessageContextFunction() {
+
+		return _samlMessageContextFunction;
+	}
+
+	private final Function
+		<SAMLMessageContext
+			<InboundMessageType, OutboundMessageType, NameID>, T>
+				_samlMessageContextFunction;
+
+}
---
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/resolver/SAMLContextImpl.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.saml.opensaml.integration.internal.resolver;
+
+import com.liferay.saml.opensaml.integration.resolver.Resolver;
+import com.liferay.saml.opensaml.integration.resolver.Resolver.SAMLCommand;
+import com.liferay.saml.opensaml.integration.resolver.Resolver.SAMLContext;
+
+import org.opensaml.common.SAMLObject;
+import org.opensaml.common.binding.SAMLMessageContext;
+import org.opensaml.saml2.core.NameID;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class SAMLContextImpl
+	<InboundMessageType extends SAMLObject,
+		OutboundMessageType extends SAMLObject, R extends Resolver>
+			implements SAMLContext<R> {
+
+	public SAMLContextImpl(
+		SAMLMessageContext<InboundMessageType, OutboundMessageType, NameID>
+			samlMessageContext) {
+
+		_samlMessageContext = samlMessageContext;
+	}
+
+	public <T> T resolve(SAMLCommand<T, ? super R> command) {
+		return ((SAMLCommandImpl
+			<InboundMessageType, OutboundMessageType, T, R>)command).
+				getSamlMessageContextFunction().apply(_samlMessageContext);
+	}
+
+	private final SAMLMessageContext
+		<InboundMessageType, OutboundMessageType, NameID> _samlMessageContext;
+
+}
