e9965e4784ad3d04aa9706d0d9105ad129f8a8fb
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-10-24 20:50:50 -0300

    LPS-75159 Copy and adapt Asset Renderer classes

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/asset/DDMFormAssetRenderer.java
@@ -0,0 +1,202 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.asset;
+
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.asset.kernel.model.BaseJSPAssetRenderer;
+import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderer;
+import com.liferay.dynamic.data.mapping.form.values.factory.DDMFormValuesFactory;
+import com.liferay.dynamic.data.mapping.form.web.internal.constants.DDMFormWebKeys;
+import com.liferay.dynamic.data.mapping.form.web.internal.display.context.DDMFormViewFormInstanceRecordDisplayContext;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecordVersion;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalService;
+import com.liferay.dynamic.data.mapping.service.permission.DDMFormInstancePermission;
+import com.liferay.dynamic.data.mapping.util.DDMFormValuesMerger;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DDMFormAssetRenderer
+	extends BaseJSPAssetRenderer<DDMFormInstanceRecord> {
+
+	public DDMFormAssetRenderer(
+		DDMFormInstanceRecord formInstanceRecord,
+		DDMFormInstanceRecordVersion formInstanceRecordVersion,
+		DDMFormInstanceRecordLocalService ddmFormInstanceRecordLocalService,
+		DDMFormRenderer ddmFormRenderer,
+		DDMFormValuesFactory ddmFormValuesFactory,
+		DDMFormValuesMerger ddmFormValuesMerger) {
+
+		_formInstanceRecord = formInstanceRecord;
+		_formInstanceRecordVersion = formInstanceRecordVersion;
+		_ddlRecordLocalService = ddmFormInstanceRecordLocalService;
+		_ddmFormRenderer = ddmFormRenderer;
+		_ddmFormValuesFactory = ddmFormValuesFactory;
+		_ddmFormValuesMerger = ddmFormValuesMerger;
+
+		DDMFormInstance formInstance = null;
+
+		try {
+			formInstance = _formInstanceRecordVersion.getFormInstance();
+		}
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(e, e);
+			}
+		}
+
+		_formInstance = formInstance;
+	}
+
+	@Override
+	public DDMFormInstanceRecord getAssetObject() {
+		return _formInstanceRecord;
+	}
+
+	@Override
+	public AssetRendererFactory<DDMFormInstanceRecord>
+		getAssetRendererFactory() {
+
+		return new DDMFormAssetRendererFactory();
+	}
+
+	@Override
+	public String getClassName() {
+		return DDMFormInstanceRecord.class.getName();
+	}
+
+	@Override
+	public long getClassPK() {
+		return _formInstanceRecord.getFormInstanceRecordId();
+	}
+
+	@Override
+	public long getGroupId() {
+		return _formInstanceRecord.getGroupId();
+	}
+
+	@Override
+	public String getJspPath(HttpServletRequest request, String template) {
+		if (template.equals(TEMPLATE_ABSTRACT) ||
+			template.equals(TEMPLATE_FULL_CONTENT)) {
+
+			return "/asset/full_content.jsp";
+		}
+		else {
+			return null;
+		}
+	}
+
+	@Override
+	public int getStatus() {
+		return _formInstanceRecordVersion.getStatus();
+	}
+
+	@Override
+	public String getSummary(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return _formInstance.getName(locale);
+	}
+
+	@Override
+	public String getURLViewInContext(
+			LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse,
+			String noSuchEntryRedirect)
+		throws Exception {
+
+		return noSuchEntryRedirect;
+	}
+
+	@Override
+	public long getUserId() {
+		return _formInstanceRecord.getUserId();
+	}
+
+	@Override
+	public String getUserName() {
+		return _formInstanceRecord.getUserName();
+	}
+
+	@Override
+	public String getUuid() {
+		return _formInstanceRecord.getUuid();
+	}
+
+	@Override
+	public boolean hasViewPermission(PermissionChecker permissionChecker) {
+		return DDMFormInstancePermission.contains(
+			permissionChecker, _formInstance, ActionKeys.VIEW);
+	}
+
+	@Override
+	public boolean include(
+			HttpServletRequest request, HttpServletResponse response,
+			String template)
+		throws Exception {
+
+		request.setAttribute(
+			DDMFormWebKeys.DYNAMIC_DATA_MAPPING_FORM_INSTANCE_RECORD,
+			_formInstanceRecord);
+
+		DDMFormViewFormInstanceRecordDisplayContext
+			ddmFormViewFormInstanceRecordDisplayContext =
+				new DDMFormViewFormInstanceRecordDisplayContext(
+					request, response, _ddlRecordLocalService, _ddmFormRenderer,
+					_ddmFormValuesFactory, _ddmFormValuesMerger);
+
+		request.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			ddmFormViewFormInstanceRecordDisplayContext);
+
+		return super.include(request, response, template);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMFormAssetRenderer.class);
+
+	private final DDMFormInstanceRecordLocalService _ddlRecordLocalService;
+	private final DDMFormRenderer _ddmFormRenderer;
+	private final DDMFormValuesFactory _ddmFormValuesFactory;
+	private final DDMFormValuesMerger _ddmFormValuesMerger;
+	private final DDMFormInstance _formInstance;
+	private final DDMFormInstanceRecord _formInstanceRecord;
+	private final DDMFormInstanceRecordVersion _formInstanceRecordVersion;
+
+}
---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/asset/DDMFormAssetRendererFactory.java
@@ -0,0 +1,162 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.asset;
+
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.asset.kernel.model.BaseAssetRendererFactory;
+import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderer;
+import com.liferay.dynamic.data.mapping.form.values.factory.DDMFormValuesFactory;
+import com.liferay.dynamic.data.mapping.form.web.internal.constants.DDMFormPortletKeys;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecordVersion;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordVersionLocalService;
+import com.liferay.dynamic.data.mapping.service.permission.DDMFormInstanceRecordPermission;
+import com.liferay.dynamic.data.mapping.util.DDMFormValuesMerger;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Leonardo Barros
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + DDMFormPortletKeys.DYNAMIC_DATA_MAPPING_FORM
+	},
+	service = AssetRendererFactory.class
+)
+public class DDMFormAssetRendererFactory
+	extends BaseAssetRendererFactory<DDMFormInstanceRecord> {
+
+	public static final String TYPE = "form";
+
+	public DDMFormAssetRendererFactory() {
+		setCategorizable(false);
+		setClassName(DDMFormInstanceRecord.class.getName());
+		setPortletId(DDMFormPortletKeys.DYNAMIC_DATA_MAPPING_FORM);
+		setSearchable(true);
+		setSelectable(false);
+	}
+
+	@Override
+	public AssetRenderer<DDMFormInstanceRecord> getAssetRenderer(
+			long classPK, int type)
+		throws PortalException {
+
+		DDMFormInstanceRecord formInstanceRecord =
+			_ddlRecordLocalService.fetchDDMFormInstanceRecord(classPK);
+
+		DDMFormInstanceRecordVersion formInstanceRecordVersion = null;
+
+		if (formInstanceRecord == null) {
+			formInstanceRecordVersion =
+				_ddlRecordVersionLocalService.getFormInstanceRecordVersion(
+					classPK);
+
+			formInstanceRecord =
+				formInstanceRecordVersion.getFormInstanceRecord();
+		}
+		else {
+			if (type == TYPE_LATEST) {
+				formInstanceRecordVersion =
+					formInstanceRecord.getLatestFormInstanceRecordVersion();
+			}
+			else if (type == TYPE_LATEST_APPROVED) {
+				formInstanceRecordVersion =
+					formInstanceRecord.getFormInstanceRecordVersion();
+			}
+			else {
+				throw new IllegalArgumentException(
+					"Unknown asset renderer type " + type);
+			}
+		}
+
+		return createAssetRenderer(
+			formInstanceRecord, formInstanceRecordVersion, type);
+	}
+
+	@Override
+	public String getClassName() {
+		return DDMFormInstanceRecord.class.getName();
+	}
+
+	@Override
+	public String getIconCssClass() {
+		return "dynamic-data-mapping";
+	}
+
+	@Override
+	public String getType() {
+		return TYPE;
+	}
+
+	@Override
+	public boolean hasPermission(
+			PermissionChecker permissionChecker, long classPK, String actionId)
+		throws Exception {
+
+		return DDMFormInstanceRecordPermission.contains(
+			permissionChecker, classPK, actionId);
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.dynamic.data.mapping.form.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	protected AssetRenderer<DDMFormInstanceRecord> createAssetRenderer(
+		DDMFormInstanceRecord formInstanceRecord,
+		DDMFormInstanceRecordVersion formInstanceRecordVersion, int type) {
+
+		DDMFormAssetRenderer ddmFormAssetRenderer = new DDMFormAssetRenderer(
+			formInstanceRecord, formInstanceRecordVersion,
+			_ddlRecordLocalService, _ddmFormRenderer, _ddmFormValuesFactory,
+			_ddmFormValuesMerger);
+
+		ddmFormAssetRenderer.setAssetRendererType(type);
+		ddmFormAssetRenderer.setServletContext(_servletContext);
+
+		return ddmFormAssetRenderer;
+	}
+
+	@Reference
+	private DDMFormInstanceRecordLocalService _ddlRecordLocalService;
+
+	@Reference
+	private DDMFormInstanceRecordVersionLocalService
+		_ddlRecordVersionLocalService;
+
+	@Reference
+	private DDMFormRenderer _ddmFormRenderer;
+
+	@Reference
+	private DDMFormValuesFactory _ddmFormValuesFactory;
+
+	@Reference
+	private DDMFormValuesMerger _ddmFormValuesMerger;
+
+	private ServletContext _servletContext;
+
+}
