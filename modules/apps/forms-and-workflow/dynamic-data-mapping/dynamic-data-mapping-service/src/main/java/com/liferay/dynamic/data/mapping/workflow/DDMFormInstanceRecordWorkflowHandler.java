0342e25dfa53d605bb22ec9f64d3ef937f69eed5
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-10-24 20:49:22 -0300

    LPS-75159 Copy and adapt workflow handler classes

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/workflow/DDMFormInstanceRecordWorkflowHandler.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.workflow;
+
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecordVersion;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordVersionLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.WorkflowDefinitionLink;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.BaseWorkflowHandler;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.workflow.WorkflowHandler;
+
+import java.io.Serializable;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Leonardo Barros
+ */
+@Component(immediate = true, service = WorkflowHandler.class)
+public class DDMFormInstanceRecordWorkflowHandler
+	extends BaseWorkflowHandler<DDMFormInstanceRecord> {
+
+	@Override
+	public AssetRenderer<DDMFormInstanceRecord> getAssetRenderer(long classPK)
+		throws PortalException {
+
+		AssetRendererFactory<DDMFormInstanceRecord> assetRendererFactory =
+			getAssetRendererFactory();
+
+		if (assetRendererFactory == null) {
+			return null;
+		}
+
+		DDMFormInstanceRecordVersion formInstanceRecordVersion =
+			_ddmFormInstanceRecordVersionLocalService.
+				getFormInstanceRecordVersion(classPK);
+
+		return assetRendererFactory.getAssetRenderer(
+			formInstanceRecordVersion.getFormInstanceRecordId(),
+			AssetRendererFactory.TYPE_LATEST);
+	}
+
+	@Override
+	public String getClassName() {
+		return DDMFormInstanceRecord.class.getName();
+	}
+
+	@Override
+	public String getType(Locale locale) {
+		return ResourceActionsUtil.getModelResource(locale, getClassName());
+	}
+
+	@Override
+	public WorkflowDefinitionLink getWorkflowDefinitionLink(
+			long companyId, long groupId, long classPK)
+		throws PortalException {
+
+		DDMFormInstanceRecordVersion formInstanceRecordVersion =
+			_ddmFormInstanceRecordVersionLocalService.
+				getFormInstanceRecordVersion(classPK);
+
+		DDMFormInstanceRecord formInstanceRecord =
+			formInstanceRecordVersion.getFormInstanceRecord();
+
+		return _workflowDefinitionLinkLocalService.fetchWorkflowDefinitionLink(
+			companyId, groupId, DDMFormInstance.class.getName(),
+			formInstanceRecord.getFormInstanceId(), 0);
+	}
+
+	@Override
+	public boolean isVisible() {
+		return false;
+	}
+
+	@Override
+	public DDMFormInstanceRecord updateStatus(
+			int status, Map<String, Serializable> workflowContext)
+		throws PortalException {
+
+		long userId = GetterUtil.getLong(
+			(String)workflowContext.get(WorkflowConstants.CONTEXT_USER_ID));
+		long classPK = GetterUtil.getLong(
+			(String)workflowContext.get(
+				WorkflowConstants.CONTEXT_ENTRY_CLASS_PK));
+
+		ServiceContext serviceContext = (ServiceContext)workflowContext.get(
+			"serviceContext");
+
+		return _ddmFormInstanceRecordLocalService.updateStatus(
+			userId, classPK, status, serviceContext);
+	}
+
+	@Reference
+	private DDMFormInstanceRecordLocalService
+		_ddmFormInstanceRecordLocalService;
+
+	@Reference
+	private DDMFormInstanceRecordVersionLocalService
+		_ddmFormInstanceRecordVersionLocalService;
+
+	@Reference
+	private WorkflowDefinitionLinkLocalService
+		_workflowDefinitionLinkLocalService;
+
+}
