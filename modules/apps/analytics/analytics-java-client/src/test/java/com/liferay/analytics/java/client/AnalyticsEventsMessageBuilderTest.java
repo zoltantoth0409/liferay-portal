2576a1912fc5ec58f00a9f23ec175bf32dc0b257
Eduardo Garcia <eduardo.garcia@liferay.com>
2017-10-24 10:30:26 +0200

    LPS-75174 Fix compilation error

--- modules/apps/analytics/analytics-java-client/src/test/java/com/liferay/analytics/java/client/AnalyticsEventsMessageBuilderTest.java
+++ modules/apps/analytics/analytics-java-client/src/test/java/com/liferay/analytics/java/client/AnalyticsEventsMessageBuilderTest.java
@@ -21,7 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 
 import org.junit.Assert;
