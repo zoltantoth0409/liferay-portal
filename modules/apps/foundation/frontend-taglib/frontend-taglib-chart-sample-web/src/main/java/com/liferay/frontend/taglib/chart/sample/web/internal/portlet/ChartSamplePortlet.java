502cd5d02803b996b86f2f4a97d51b0ab6b2b53e
Chema Balsas <jose.balsas@liferay.com>
2017-10-24 09:16:36 +0200

    LPS-74831 Uses portlet default template

--- modules/apps/foundation/frontend-taglib/frontend-taglib-chart-sample-web/src/main/java/com/liferay/frontend/taglib/chart/sample/web/internal/portlet/ChartSamplePortlet.java
+++ modules/apps/foundation/frontend-taglib/frontend-taglib-chart-sample-web/src/main/java/com/liferay/frontend/taglib/chart/sample/web/internal/portlet/ChartSamplePortlet.java
@@ -35,7 +35,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.remoteable=true",
 		"com.liferay.portlet.render-weight=50",
-		"com.liferay.portlet.use-default-template=false",
+		"com.liferay.portlet.use-default-template=true",
 		"javax.portlet.display-name=Chart Sample",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/",
