700fbd05b6c7244aebba988c34b1919a273fa35e
Clóvis Neto <clovis.neto@liferay.com>
2017-10-24 13:56:54 -0300

    LPS-75356 Add edit field action in edit button

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder.js
@@ -5,11 +5,12 @@ AUI.add(
 
 		var FormBuilderConfirmDialog = Liferay.DDM.FormBuilderConfirmationDialog;
 
+		var FormBuilderUtil = Liferay.DDM.FormBuilderUtil;
+
 		var FieldSets = Liferay.DDM.FieldSets;
 
 		var FieldTypes = Liferay.DDM.Renderer.FieldTypes;
 
-		var FormBuilderUtil = Liferay.DDM.FormBuilderUtil;
 		var CSS_RESIZE_COL_DRAGGABLE = A.getClassName('layout', 'builder', 'resize', 'col', 'draggable');
 
 		var CSS_RESIZE_COL_DRAGGABLE_BORDER = A.getClassName('layout', 'builder', 'resize', 'col', 'draggable', 'border');
@@ -17,6 +18,8 @@ AUI.add(
 		var CSS_RESIZE_COL_DRAGGABLE_HANDLE = A.getClassName('layout', 'builder', 'resize', 'col', 'draggable', 'handle');
 
 		var Lang = A.Lang;
+
+		var CSS_EDIT_FIELD_BUTTON = A.getClassName('lfr-edit-field');
 
 		var CSS_FIELD = A.getClassName('form', 'builder', 'field');
 
@@ -137,17 +140,15 @@ AUI.add(
 						var instance = this;
 
 						var boundingBox = instance.get('boundingBox');
-						
+
 						instance._eventHandlers = [
-							boundingBox.delegate('click', A.bind('_afterFieldClick', instance), '.' + CSS_FIELD, instance),
+							boundingBox.delegate('click', A.bind('_afterFieldClick', instance), '.' + CSS_EDIT_FIELD_BUTTON, instance),
 							boundingBox.delegate('click', instance._onClickPaginationItem, '.pagination li a'),
 							instance.after('editingLanguageIdChange', instance._afterEditingLanguageIdChange),
 							instance.after('liferay-ddm-form-builder-field-list:fieldsChange', instance._afterFieldListChange, instance),
 							instance.after('render', instance._afterFormBuilderRender, instance),
 							instance.after(instance._afterRemoveField, instance, 'removeField')
 						];
-						
-						
 					},
 
 					destructor: function() {
@@ -551,7 +552,7 @@ AUI.add(
 					_afterFieldClick: function(event) {
 						var instance = this;
 
-						var field = event.currentTarget.getData('field-instance');
+						var field = event.currentTarget.ancestor('.' + CSS_FIELD).getData('field-instance');
 
 						instance.editField(field);
 					},
@@ -566,34 +567,15 @@ AUI.add(
 						var instance = this;
 
 						instance._fieldToolbar.destroy();
-						
+
 						instance.getFieldSettingsPanel();
+						instance._renderArrowActions();
 						instance._renderFields();
 						instance._renderPages();
 						instance._renderRequiredFieldsWarning();
 						instance._syncRequiredFieldsWarning();
 						instance._syncRowsLastColumnUI();
 						instance._syncRowIcons();
-					},
-					
-					_createFieldActions: function(){
-						var instance = this;
-
-						instance.eachFields(function(field){
-							var field = field.get('container');
-							
-							field.append(instance._getFieldActionsLayout());
-						});
-					},
-					
-					_getFieldActionsLayout: function(){
-						var instance = this;
-						
-						return "<div class='lfr-ddm-field-actions-container'> " +
-							"<button class='btn btn-monospaced btn-sm label-primary' type='button'>"+Liferay.Util.getLexiconIconTpl('pencil')+"</button>"+
-							"<button class='btn btn-monospaced btn-sm label-primary' type='button'>"+Liferay.Util.getLexiconIconTpl('move')+"</button>"+
-							"<button class='btn btn-monospaced btn-sm label-primary' type='button'>"+Liferay.Util.getLexiconIconTpl('trash')+"</button>"+
-							"</div>";
 					},
 
 					_afterLayoutColsChange: function(event) {
@@ -635,6 +617,16 @@ AUI.add(
 						instance.createNewField(event.fieldType);
 					},
 
+					_createFieldActions: function() {
+						var instance = this;
+
+						instance.eachFields(
+							function(field) {
+								field.get('container').append(instance._getFieldActionsLayout());
+							}
+						);
+					},
+
 					_currentRowIndex: function() {
 						var instance = this;
 
@@ -653,6 +645,16 @@ AUI.add(
 						}
 
 						return 0;
+					},
+
+					_getFieldActionsLayout: function() {
+						var instance = this;
+
+						return '<div class="lfr-ddm-field-actions-container">' +
+							'<button class="btn btn-monospaced btn-sm label-primary lfr-edit-field" type="button">' + Liferay.Util.getLexiconIconTpl('pencil') + '</button>' +
+							'<button class="btn btn-monospaced btn-sm label-primary" type="button">' + Liferay.Util.getLexiconIconTpl('move') + '</button>' +
+							'<button class="btn btn-monospaced btn-sm label-primary" type="button">' + Liferay.Util.getLexiconIconTpl('trash') + '</button>' +
+							'</div>';
 					},
 
 					_getFieldSettingsPanel: function(fieldSettingsPanel) {
@@ -840,7 +842,6 @@ AUI.add(
 								var row = instance.getFieldRow(field);
 
 								activeLayout.normalizeColsHeight(new A.NodeList(row));
-								
 								field.get('container').append(instance._getFieldActionsLayout());
 							}
 						);
@@ -856,8 +857,6 @@ AUI.add(
 						visitor.set('fieldHandler', A.bind('_renderField', instance));
 
 						visitor.visit();
-						
-						instance._createFieldActions();
 					},
 
 					_renderPages: function() {
