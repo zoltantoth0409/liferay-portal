2117b7c87cd74a7db967c6c9ad3201ca694d680e
Andrea Di Giorgi <adigiorgi@outlook.com>
2017-10-24 08:22:52 -0700

    COMMERCE-255 Rename module to accommodate multiple admin pages

---
+++ modules/dxp/apps/commerce/commerce-shipping-web/src/main/java/com/liferay/commerce/shipping/web/internal/portlet/action/EditCommerceShippingMethodMVCActionCommand.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.shipping.web.internal.portlet.action;
+
+import com.liferay.commerce.admin.web.constants.CommerceAdminPortletKeys;
+import com.liferay.commerce.exception.CommerceShippingMethodNameException;
+import com.liferay.commerce.exception.NoSuchShippingMethodException;
+import com.liferay.commerce.model.CommerceShippingMethod;
+import com.liferay.commerce.service.CommerceShippingMethodService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropertiesParamUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CommerceAdminPortletKeys.COMMERCE_ADMIN,
+		"mvc.command.name=editCommerceShippingMethod"
+	},
+	service = MVCActionCommand.class
+)
+public class EditCommerceShippingMethodMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	protected void deleteCommerceShippingMethod(ActionRequest actionRequest)
+		throws PortalException {
+
+		long commerceShippingMethodId = ParamUtil.getLong(
+			actionRequest, "commerceShippingMethodId");
+
+		_commerceShippingMethodService.deleteCommerceShippingMethod(
+			commerceShippingMethodId);
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		try {
+			if (cmd.equals(Constants.DELETE)) {
+				deleteCommerceShippingMethod(actionRequest);
+			}
+
+			if (cmd.equals(Constants.ADD) || cmd.equals(Constants.UPDATE)) {
+				updateCommerceShippingMethod(actionRequest);
+			}
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchShippingMethodException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(actionRequest, e.getClass());
+
+				actionResponse.setRenderParameter("mvcPath", "/error.jsp");
+			}
+			else if (e instanceof CommerceShippingMethodNameException) {
+				hideDefaultErrorMessage(actionRequest);
+				hideDefaultSuccessMessage(actionRequest);
+
+				SessionErrors.add(actionRequest, e.getClass());
+
+				actionResponse.setRenderParameter(
+					"mvcRenderCommandName", "editCommerceShippingMethod");
+
+				SessionErrors.add(actionRequest, e.getClass());
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	protected CommerceShippingMethod updateCommerceShippingMethod(
+			ActionRequest actionRequest)
+		throws PortalException {
+
+		long commerceShippingMethodId = ParamUtil.getLong(
+			actionRequest, "commerceShippingMethodId");
+
+		Map<Locale, String> nameMap = LocalizationUtil.getLocalizationMap(
+			actionRequest, "name");
+		Map<Locale, String> descriptionMap =
+			LocalizationUtil.getLocalizationMap(actionRequest, "description");
+		String engineKey = ParamUtil.getString(actionRequest, "engineKey");
+		UnicodeProperties engineParameterMap =
+			PropertiesParamUtil.getProperties(actionRequest, "settings--");
+		double priority = ParamUtil.getDouble(actionRequest, "priority");
+		boolean active = ParamUtil.getBoolean(actionRequest, "active");
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			CommerceShippingMethod.class.getName(), actionRequest);
+
+		CommerceShippingMethod commerceShippingMethod = null;
+
+		if (commerceShippingMethodId <= 0) {
+			commerceShippingMethod =
+				_commerceShippingMethodService.addCommerceShippingMethod(
+					nameMap, descriptionMap, engineKey, engineParameterMap,
+					priority, active, serviceContext);
+		}
+		else {
+			commerceShippingMethod =
+				_commerceShippingMethodService.updateCommerceShippingMethod(
+					commerceShippingMethodId, nameMap, descriptionMap,
+					engineParameterMap, priority, active, serviceContext);
+		}
+
+		return commerceShippingMethod;
+	}
+
+	@Reference
+	private CommerceShippingMethodService _commerceShippingMethodService;
+
+}
