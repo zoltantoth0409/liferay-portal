594b061e47e550b6310a0298962f24528ac4d86b
Brian Chan <brian.chan@liferay.com>
2017-10-24 10:42:52 -0700

    Revert "LPS-75320 Upgrade"
    This reverts commit f5d4d5f45ff878efe08b7c5ae7ecb7c46e74c13c.

--- portal-impl/src/com/liferay/portal/upgrade/v7_1_0/UpgradeModules.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_1_0/UpgradeModules.java
@@ -33,7 +33,6 @@ public class UpgradeModules
 	private static final String[] _BUNDLE_SYMBOLIC_NAMES = {
 		"com.liferay.document.library.file.rank.service",
 		"com.liferay.document.library.sync.service",
-		"com.liferay.message.boards.service",
 		"com.liferay.subscription.service", "com.liferay.trash.service"
 	};
 
