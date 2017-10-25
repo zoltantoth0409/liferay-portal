607fe04a64c4db2ae80c62b987bf7a9b6bd0dc29
Lance Ji <zheyi.ji@liferay.com>
2017-10-25 14:33:18 -0700

    LPS-75272 Fixed localization of document-library-file-rank-service

--- modules/apps/collaboration/document-library/document-library-file-rank-service/src/main/java/com/liferay/document/library/file/rank/internal/configuration/DLFileRankServiceConfiguration.java
+++ modules/apps/collaboration/document-library/document-library-file-rank-service/src/main/java/com/liferay/document/library/file/rank/internal/configuration/DLFileRankServiceConfiguration.java
@@ -29,7 +29,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface DLFileRankServiceConfiguration {
 
-	@Meta.AD(deflt = "5", required = false)
+	@Meta.AD(deflt = "5", name = "max-size", required = false)
 	public int maxSize();
 
 }
