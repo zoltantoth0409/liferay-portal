b63bef1b629b9ad89a784d44f0da12b1955df182
Lance Ji <zheyi.ji@liferay.com>
2017-10-26 16:08:21 -0700

    LPS-75199 Test all modules for localization

---
+++ modules/test/module-configuration-localization-test/src/testIntegration/java/com/liferay/module/configuration/localization/test/ModuleConfigurationLocalizationTest.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.module.configuration.localization.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.configuration.metatype.definitions.ExtendedAttributeDefinition;
+import com.liferay.portal.configuration.metatype.definitions.ExtendedMetaTypeInformation;
+import com.liferay.portal.configuration.metatype.definitions.ExtendedMetaTypeService;
+import com.liferay.portal.configuration.metatype.definitions.ExtendedObjectClassDefinition;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleLoaderUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Lance Ji
+ */
+@RunWith(Arquillian.class)
+public class ModuleConfigurationLocalizationTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testBundleLocalization() {
+		Bundle currentBundle = FrameworkUtil.getBundle(
+			ModuleConfigurationLocalizationTest.class);
+
+		BundleContext bundleContext = currentBundle.getBundleContext();
+
+		StringBundler sb = new StringBundler();
+
+		for (Bundle bundle : bundleContext.getBundles()) {
+			String bundleError = _collectBundleError(bundle);
+
+			if (bundleError.isEmpty()) {
+				continue;
+			}
+
+			sb.append("\nBundle {id: ");
+			sb.append(bundle.getBundleId());
+			sb.append(", name: ");
+			sb.append(bundle.getSymbolicName());
+			sb.append(", version: ");
+			sb.append(bundle.getVersion());
+			sb.append("}");
+			sb.append(bundleError);
+		}
+
+		Assert.assertEquals(sb.toString(), 0, sb.length());
+	}
+
+	private String _collectBundleError(Bundle bundle) {
+		ExtendedMetaTypeInformation extendedMetaTypeInformation =
+			_extendedMetaTypeService.getMetaTypeInformation(bundle);
+
+		List<String> pids = new ArrayList<>();
+
+		Collections.addAll(pids, extendedMetaTypeInformation.getFactoryPids());
+		Collections.addAll(pids, extendedMetaTypeInformation.getPids());
+
+		if (pids.isEmpty()) {
+			return StringPool.BLANK;
+		}
+
+		StringBundler sb = new StringBundler();
+
+		ResourceBundleLoader resourceBundleLoader =
+			ResourceBundleLoaderUtil.
+				getResourceBundleLoaderByBundleSymbolicName(
+					bundle.getSymbolicName());
+
+		if (resourceBundleLoader == null) {
+			sb.append(
+				"\n\tMissing default language file for configuration pids: ");
+
+			for (String pid : pids) {
+				sb.append(pid);
+				sb.append(",");
+			}
+
+			sb.setIndex(sb.index() - 1);
+
+			return sb.toString();
+		}
+
+		for (Locale locale : LanguageUtil.getAvailableLocales()) {
+			if (locale.equals(LocaleUtil.getDefault())) {
+				continue;
+			}
+
+			if (resourceBundleLoader.loadResourceBundle(locale) == null) {
+				sb.append("\n\tMissing generated language files, ");
+				sb.append("need to regenerate language files for this bundle.");
+			}
+
+			break;
+		}
+
+		ResourceBundle resourceBundle = resourceBundleLoader.loadResourceBundle(
+			LocaleUtil.getDefault());
+
+		for (String pid : pids) {
+			String metaInfoErrorMessage = _collectMetaInfoError(
+				pid, extendedMetaTypeInformation, resourceBundle);
+
+			if (metaInfoErrorMessage.isEmpty()) {
+				continue;
+			}
+
+			sb.append("\n\tConfiguration {pid:");
+			sb.append(pid);
+			sb.append("}");
+			sb.append(metaInfoErrorMessage);
+		}
+
+		return sb.toString();
+	}
+
+	private String _collectMetaInfoError(
+		String pid, ExtendedMetaTypeInformation extendedMetaTypeInformation,
+		ResourceBundle resourceBundle) {
+
+		StringBundler sb = new StringBundler();
+
+		ExtendedObjectClassDefinition extendedObjectClassDefinition =
+			extendedMetaTypeInformation.getObjectClassDefinition(
+				pid, LocaleUtil.getDefault().getLanguage());
+
+		if (ResourceBundleUtil.getString(
+				resourceBundle, extendedObjectClassDefinition.getName()) ==
+					null) {
+
+			sb.append("\n\t\tMissing localization for configuration pid: ");
+			sb.append(extendedObjectClassDefinition.getID());
+		}
+
+		List<String> missingAttributeNames = new ArrayList<>();
+
+		for (ExtendedAttributeDefinition extendedAttributeDefinition :
+				extendedObjectClassDefinition.getAttributeDefinitions(
+					ExtendedObjectClassDefinition.ALL)) {
+
+			if (ResourceBundleUtil.getString(
+					resourceBundle, extendedAttributeDefinition.getName()) ==
+						null) {
+
+				missingAttributeNames.add(extendedAttributeDefinition.getID());
+			}
+		}
+
+		if (!missingAttributeNames.isEmpty()) {
+			sb.append("\n\t\tMissing localization for attributes: ");
+
+			for (String attributeName : missingAttributeNames) {
+				sb.append(attributeName);
+				sb.append(",");
+			}
+
+			sb.setIndex(sb.index() - 1);
+		}
+
+		return sb.toString();
+	}
+
+	@Inject
+	private static ExtendedMetaTypeService _extendedMetaTypeService;
+
+}
