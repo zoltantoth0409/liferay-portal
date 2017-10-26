dc73f3a4224a11f6dec21536e90c2019588d74b2
Rafael Praxedes <rafael.praxedes@liferay.com>
2017-10-26 15:50:57 -0300

    LPS-75500 Create an upgrade to replace the old DDL Form portlet by the DDM Form portlet

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upgrade/DDMFormWebUpgrade.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.upgrade;
+
+import com.liferay.dynamic.data.mapping.form.web.internal.upgrade.v1_0_0.UpgradeDDMFormWebPortletId;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(
+	immediate = true,
+	service = {DDMFormWebUpgrade.class, UpgradeStepRegistrator.class}
+)
+public class DDMFormWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"com.liferay.dynamic.data.mapping.form.web", "0.0.0", "1.0.0",
+			new UpgradeDDMFormWebPortletId());
+	}
+
+	@Reference
+	private PortletPreferencesLocalService _portletPreferencesLocalService;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+}
---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upgrade/v1_0_0/UpgradeDDMFormWebPortletId.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.upgrade.v1_0_0;
+
+import com.liferay.dynamic.data.mapping.form.web.internal.constants.DDMFormPortletKeys;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class UpgradeDDMFormWebPortletId
+	extends com.liferay.portal.kernel.upgrade.BaseUpgradePortletId {
+
+	@Override
+	protected String[][] getRenamePortletIdsArray() {
+		return new String[][] {
+			new String[] {
+				_OLD_PORTLET_NAME, DDMFormPortletKeys.DYNAMIC_DATA_MAPPING_FORM
+			}
+		};
+	}
+
+	private static final String _OLD_PORTLET_NAME =
+		"com_liferay_dynamic_data_lists_form_web_portlet_DDLFormPortlet";
+
+}
