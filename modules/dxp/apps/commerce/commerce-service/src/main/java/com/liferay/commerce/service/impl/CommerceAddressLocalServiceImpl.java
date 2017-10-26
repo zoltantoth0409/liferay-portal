9a1fcf6e2739b6a842a87a766c224118dfaa6fd8
Andrea Di Giorgi <adigiorgi@outlook.com>
2017-10-26 14:31:36 -0700

    COMMERCE-255 Add service method to geolocate an address

--- modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceAddressLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceAddressLocalServiceImpl.java
@@ -20,12 +20,14 @@ import com.liferay.commerce.exception.CommerceAddressNameException;
 import com.liferay.commerce.exception.CommerceAddressStreetException;
 import com.liferay.commerce.model.CommerceAddress;
 import com.liferay.commerce.model.CommerceCart;
+import com.liferay.commerce.model.CommerceGeocoder;
 import com.liferay.commerce.service.base.CommerceAddressLocalServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.spring.extender.service.ServiceReference;
 
 import java.util.List;
 
@@ -153,6 +155,24 @@ public class CommerceAddressLocalServiceImpl
 		for (CommerceAddress commerceAddress : commerceAddresses) {
 			commerceAddressLocalService.deleteCommerceAddress(commerceAddress);
 		}
+	}
+
+	@Override
+	public CommerceAddress geolocateCommerceAddress(long commerceAddressId)
+		throws PortalException {
+
+		CommerceAddress commerceAddress =
+			commerceAddressPersistence.findByPrimaryKey(commerceAddressId);
+
+		double[] coordinates = commerceGeocoder.getCoordinates(
+			commerceAddress.getStreet1(), commerceAddress.getCity(),
+			commerceAddress.getZip(), commerceAddress.getCommerceRegion(),
+			commerceAddress.getCommerceCountry());
+
+		commerceAddress.setLatitude(coordinates[0]);
+		commerceAddress.setLongitude(coordinates[1]);
+
+		return commerceAddressPersistence.update(commerceAddress);
 	}
 
 	@Override
@@ -290,5 +310,8 @@ public class CommerceAddressLocalServiceImpl
 			}
 		}
 	}
+
+	@ServiceReference(type = CommerceGeocoder.class)
+	protected CommerceGeocoder commerceGeocoder;
 
 }
