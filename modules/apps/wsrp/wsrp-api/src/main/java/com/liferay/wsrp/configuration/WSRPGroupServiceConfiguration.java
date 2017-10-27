51c81ccf330b4023463ec38fa21d90cbdf571346
Lance Ji <zheyi.ji@liferay.com>
2017-10-27 11:03:44 -0700

    LPS-75302 Fixed localization of wsrp-api

--- modules/apps/wsrp/wsrp-api/src/main/java/com/liferay/wsrp/configuration/WSRPGroupServiceConfiguration.java
+++ modules/apps/wsrp/wsrp-api/src/main/java/com/liferay/wsrp/configuration/WSRPGroupServiceConfiguration.java
@@ -30,28 +30,37 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface WSRPGroupServiceConfiguration {
 
-	@Meta.AD(deflt = "", required = false)
+	@Meta.AD(deflt = "", name = "consumer-request-extensions", required = false)
 	public String[] consumerRequestExtensions();
 
 	@Meta.AD(
 		deflt = "com.liferay.wsrp.util.AttributeExtensionHelper",
-		required = false
+		name = "extension-helper-impl", required = false
 	)
 	public String extensionHelperImpl();
 
-	@Meta.AD(deflt = "0", required = false)
+	@Meta.AD(
+		deflt = "0", name = "failed-consumers-check-interval", required = false
+	)
 	public int failedConsumersCheckInterval();
 
-	@Meta.AD(deflt = "127.0.0.1|SERVER_IP", required = false)
+	@Meta.AD(
+		deflt = "127.0.0.1|SERVER_IP", name = "proxy-url-ips-allowed",
+		required = false
+	)
 	public String[] proxyUrlIpsAllowed();
 
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(
+		deflt = "false", name = "secure-resource-urls-enabled", required = false
+	)
 	public boolean secureResourceUrlsEnabled();
 
-	@Meta.AD(deflt = "salt", required = false)
+	@Meta.AD(
+		deflt = "salt", name = "secure-resource-urls-salt", required = false
+	)
 	public String secureResourceUrlsSalt();
 
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(deflt = "false", name = "soap-debug", required = false)
 	public boolean soapDebug();
 
 }
