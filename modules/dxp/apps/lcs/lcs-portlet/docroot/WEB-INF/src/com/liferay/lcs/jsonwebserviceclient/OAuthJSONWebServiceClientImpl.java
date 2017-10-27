36796f9de51cfdb09ccab8af63886f0b29acd54a
Brian Chan <brian.chan@liferay.com>
2017-10-26 22:11:08 -0700

    CLDSVCS-2922 Hugo SF

--- modules/dxp/apps/lcs/lcs-portlet/docroot/WEB-INF/src/com/liferay/lcs/jsonwebserviceclient/OAuthJSONWebServiceClientImpl.java
+++ modules/dxp/apps/lcs/lcs-portlet/docroot/WEB-INF/src/com/liferay/lcs/jsonwebserviceclient/OAuthJSONWebServiceClientImpl.java
@@ -66,7 +66,7 @@ public class OAuthJSONWebServiceClientImpl extends JSONWebServiceClientImpl {
 	@Override
 	protected String execute(HttpRequestBase httpRequestBase)
 		throws JSONWebServiceInvocationException,
-			JSONWebServiceTransportException {
+			   JSONWebServiceTransportException {
 
 		if ((_accessToken == null) && (_accessSecret == null)) {
 			throw new JSONWebServiceTransportException.AuthenticationFailure(
