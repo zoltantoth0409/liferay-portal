7ed180e583f4e6e6d30ab3dbd9cf2b770beffb5e
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-10-30 14:51:05 +0100

    LPS-75288 null check added

--- modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/data/handler/FileEntryStagedModelDataHandler.java
+++ modules/apps/collaboration/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/data/handler/FileEntryStagedModelDataHandler.java
@@ -631,7 +631,9 @@ public class FileEntryStagedModelDataHandler
 		}
 		finally {
 			try {
-				is.close();
+				if(is != null){
+					is.close();
+				}
 			}
 			catch (IOException ioe) {
 				_log.error(ioe, ioe);
