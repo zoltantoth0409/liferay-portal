91c568b90e690cde48a95716f7a6864e28262b97
Pablo Molina <pablo.molina@liferay.com>
2017-10-31 15:39:02 +0100

    LPS-75540 Added LayoutPageTemplateFragmentCollection metal component

---
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/resources/META-INF/resources/js/LayoutPageTemplateFragmentCollection.es.js
@@ -0,0 +1,77 @@
+import Component from 'metal-component';
+import {Config} from 'metal-state';
+import Soy from 'metal-soy';
+
+import templates from './LayoutPageTemplateFragmentCollection.soy';
+
+/**
+ * LayoutPageTemplateFragmentCollection
+ */
+class LayoutPageTemplateFragmentCollection extends Component {
+	/**
+	 * Callback that is executed when a fragment entry is clicked.
+	 * It propagates a collectionEntryClick event with the fragment information.
+	 * @param {Event} event
+	 * @private
+	 */
+	_handleEntryClick(event) {
+		const fragmentEntryId = event.delegateTarget.dataset.fragmentEntryId;
+		const fragmentName = this.collection.entries.find(
+			entry => entry.fragmentEntryId === fragmentEntryId
+		).name;
+
+		this.emit('collectionEntryClick', {
+			fragmentEntryId,
+			fragmentName,
+		});
+	}
+}
+
+/**
+ * State definition.
+ * @type {!Object}
+ * @static
+ */
+LayoutPageTemplateFragmentCollection.STATE = {
+	/**
+	 * Available entries that can be dragged inside the existing Page Template,
+	 * organized by fragment categories.
+	 * @default undefined
+	 * @instance
+	 * @memberOf LayoutPageTemplateFragmentCollection
+	 * @type {!Array<object>}
+	 */
+	collection: Config.shapeOf({
+		fragmentCollectionId: Config.string().required(),
+		name: Config.string().required(),
+		entries: Config.arrayOf(
+			Config.shapeOf({
+				fragmentEntryId: Config.string().required(),
+				name: Config.string().required(),
+			})
+		).required(),
+	}),
+
+	/**
+	 * Portlet namespace needed for prefixing form inputs
+	 * @default undefined
+	 * @instance
+	 * @memberOf LayoutPageTemplateFragmentCollection
+	 * @type {!string}
+	 */
+	portletNamespace: Config.string().required(),
+
+	/**
+	 * Path of the available icons.
+	 * @default undefined
+	 * @instance
+	 * @memberOf LayoutPageTemplateFragmentCollection
+	 * @type {!string}
+	 */
+	spritemap: Config.string().required(),
+};
+
+Soy.register(LayoutPageTemplateFragmentCollection, templates);
+
+export {LayoutPageTemplateFragmentCollection};
+export default LayoutPageTemplateFragmentCollection;
