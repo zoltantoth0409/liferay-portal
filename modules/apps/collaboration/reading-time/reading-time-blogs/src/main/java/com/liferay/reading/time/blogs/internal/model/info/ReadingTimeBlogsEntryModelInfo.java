c1f80ca91335bc95b499930deecca3a1e4205c0c
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-11-02 14:07:39 +0100

    LPS-74943 Adding reading time to blogs via a service wrapper

---
+++ modules/apps/collaboration/reading-time/reading-time-blogs/src/main/java/com/liferay/reading/time/blogs/internal/model/info/ReadingTimeBlogsEntryModelInfo.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.reading.time.blogs.internal.model.info;
+
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.reading.time.calculator.ReadingTimeModelInfo;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.blogs.model.BlogsEntry",
+	service = ReadingTimeModelInfo.class
+)
+public class ReadingTimeBlogsEntryModelInfo
+	implements ReadingTimeModelInfo<BlogsEntry> {
+
+	@Override
+	public String getContent(BlogsEntry model) {
+		return model.getContent();
+	}
+
+	@Override
+	public String getContentType(BlogsEntry model) {
+		return ContentTypes.TEXT_HTML;
+	}
+
+	@Override
+	public Locale getLocale(BlogsEntry model) {
+		return LocaleUtil.getDefault();
+	}
+
+}
---
+++ modules/apps/collaboration/reading-time/reading-time-blogs/src/main/java/com/liferay/reading/time/blogs/internal/service/wrapper/ReadingTimeBlogsEntryLocalServiceWrapper.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.reading.time.blogs.internal.service.wrapper;
+
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryLocalService;
+import com.liferay.blogs.service.BlogsEntryLocalServiceWrapper;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.servlet.taglib.ui.ImageSelector;
+import com.liferay.reading.time.service.ReadingTimeEntryLocalService;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(immediate = true, service = ServiceWrapper.class)
+public class ReadingTimeBlogsEntryLocalServiceWrapper
+	extends BlogsEntryLocalServiceWrapper {
+
+	public ReadingTimeBlogsEntryLocalServiceWrapper() {
+		super(null);
+	}
+
+	public ReadingTimeBlogsEntryLocalServiceWrapper(
+		BlogsEntryLocalService blogsEntryLocalService) {
+
+		super(blogsEntryLocalService);
+	}
+
+	@Override
+	public BlogsEntry addEntry(
+			long userId, String title, String subtitle, String urlTitle,
+			String description, String content, Date displayDate,
+			boolean allowPingbacks, boolean allowTrackbacks,
+			String[] trackbacks, String coverImageCaption,
+			ImageSelector coverImageImageSelector,
+			ImageSelector smallImageImageSelector,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		BlogsEntry blogsEntry = super.addEntry(
+			userId, title, subtitle, urlTitle, description, content,
+			displayDate, allowPingbacks, allowTrackbacks, trackbacks,
+			coverImageCaption, coverImageImageSelector, smallImageImageSelector,
+			serviceContext);
+
+		_readingTimeEntryLocalService.updateReadingTimeEntry(blogsEntry);
+
+		return blogsEntry;
+	}
+
+	@Override
+	public BlogsEntry deleteEntry(BlogsEntry blogsEntry)
+		throws PortalException {
+
+		blogsEntry = super.deleteEntry(blogsEntry);
+
+		_readingTimeEntryLocalService.deleteReadingTimeEntry(blogsEntry);
+
+		return blogsEntry;
+	}
+
+	@Override
+	public BlogsEntry updateEntry(
+			long userId, long entryId, String title, String subtitle,
+			String urlTitle, String description, String content,
+			Date displayDate, boolean allowPingbacks, boolean allowTrackbacks,
+			String[] trackbacks, String coverImageCaption,
+			ImageSelector coverImageImageSelector,
+			ImageSelector smallImageImageSelector,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		BlogsEntry blogsEntry = super.updateEntry(
+			userId, entryId, title, subtitle, urlTitle, description, content,
+			displayDate, allowPingbacks, allowTrackbacks, trackbacks,
+			coverImageCaption, coverImageImageSelector, smallImageImageSelector,
+			serviceContext);
+
+		_readingTimeEntryLocalService.updateReadingTimeEntry(blogsEntry);
+
+		return blogsEntry;
+	}
+
+	@Reference
+	private ReadingTimeEntryLocalService _readingTimeEntryLocalService;
+
+}
