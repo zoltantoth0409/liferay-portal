ac790f3ee236ee16f8bdb8605e34fbd5ab22a5b2
Alessio Rendina <asilcomando@gmail.com>
2017-11-02 16:53:32 +0100

    LPS-75413 Added role item selector view

---
+++ modules/apps/foundation/roles/roles-item-selector-web/src/main/java/com/liferay/roles/item/selector/web/internal/RoleItemSelectorView.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.roles.item.selector.web.internal;
+
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.roles.item.selector.criterion.RoleItemSelectorCriterion;
+import com.liferay.roles.item.selector.web.internal.display.context.RoleItemSelectorViewDisplayContext;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(immediate = true, service = ItemSelectorView.class)
+public class RoleItemSelectorView
+	implements ItemSelectorView<RoleItemSelectorCriterion> {
+
+	@Override
+	public Class<RoleItemSelectorCriterion> getItemSelectorCriterionClass() {
+		return RoleItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "roles");
+	}
+
+	@Override
+	public boolean isShowSearch() {
+		return true;
+	}
+
+	@Override
+	public boolean isVisible(ThemeDisplay themeDisplay) {
+		return true;
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest request, ServletResponse response,
+			RoleItemSelectorCriterion roleItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+
+		RoleItemSelectorViewDisplayContext roleItemSelectorViewDisplayContext =
+			new RoleItemSelectorViewDisplayContext(
+				_roleLocalService, httpServletRequest, portletURL,
+				itemSelectedEventName);
+
+		request.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			roleItemSelectorViewDisplayContext);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher("/role_item_selector.jsp");
+
+		requestDispatcher.include(request, response);
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.unmodifiableList(
+			ListUtil.fromArray(
+				new ItemSelectorReturnType[] {
+					new UUIDItemSelectorReturnType()
+				}));
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.roles.item.selector.web)"
+	)
+	private ServletContext _servletContext;
+
+}
