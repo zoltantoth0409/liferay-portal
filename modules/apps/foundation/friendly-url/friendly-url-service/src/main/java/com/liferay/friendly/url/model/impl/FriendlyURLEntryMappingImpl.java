9b350b8a633eba39f8aa93d851ba28b90633cdb3
Brian Chan <brian.chan@liferay.com>
2017-11-02 14:45:13 -0700

    LPS-72193 SF

--- modules/apps/foundation/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/model/impl/FriendlyURLEntryMappingImpl.java
+++ modules/apps/foundation/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/model/impl/FriendlyURLEntryMappingImpl.java
@@ -17,23 +17,12 @@ package com.liferay.friendly.url.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the FriendlyURLEntryMapping service. Represents a row in the &quot;FriendlyURLEntryMapping&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.friendly.url.model.FriendlyURLEntryMapping} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class FriendlyURLEntryMappingImpl
 	extends FriendlyURLEntryMappingBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a friendly url entry mapping model instance should use the {@link com.liferay.friendly.url.model.FriendlyURLEntryMapping} interface instead.
-	 */
 	public FriendlyURLEntryMappingImpl() {
 	}
 
