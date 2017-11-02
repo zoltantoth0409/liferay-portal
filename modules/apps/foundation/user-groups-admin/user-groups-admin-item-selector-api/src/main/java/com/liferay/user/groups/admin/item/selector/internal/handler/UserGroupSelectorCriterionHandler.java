330d7b09b2cbb272a526d4c9d0d28ebe9d178a76
Alessio Rendina <asilcomando@gmail.com>
2017-11-02 17:31:42 +0100

    LPS-75414 Added user group item selector criterion handler

---
+++ modules/apps/foundation/user-groups-admin/user-groups-admin-item-selector-api/src/main/java/com/liferay/user/groups/admin/item/selector/internal/handler/UserGroupSelectorCriterionHandler.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.groups.admin.item.selector.internal.handler;
+
+import com.liferay.item.selector.BaseItemSelectorCriterionHandler;
+import com.liferay.item.selector.ItemSelectorCriterionHandler;
+import com.liferay.user.groups.admin.item.selector.criterion.UserGroupItemSelectorCriterion;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(service = ItemSelectorCriterionHandler.class)
+public class UserGroupSelectorCriterionHandler
+	extends BaseItemSelectorCriterionHandler <UserGroupItemSelectorCriterion> {
+
+	@Override
+	public Class<UserGroupItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return UserGroupItemSelectorCriterion.class;
+	}
+
+	@Activate
+	@Override
+	protected void activate(BundleContext bundleContext) {
+		super.activate(bundleContext);
+	}
+
+	@Deactivate
+	@Override
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+}
