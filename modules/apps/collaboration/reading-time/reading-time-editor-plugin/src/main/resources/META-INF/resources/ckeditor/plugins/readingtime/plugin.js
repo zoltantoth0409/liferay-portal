24897cab8f5f6f84333d407f354c95c211a7fc8a
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-11-02 12:53:22 +0100

    LPS-74943 Providing reading time through an editor plugin

---
+++ modules/apps/collaboration/reading-time/reading-time-editor-plugin/src/main/resources/META-INF/resources/ckeditor/plugins/readingtime/plugin.js
@@ -0,0 +1,46 @@
+(function() {
+	var NAMESPACE = '_com_liferay_reading_time_web_portlet_ReadingTimePortlet_';
+
+	CKEDITOR.plugins.add(
+		'readingtime',
+		{
+			init: function(editor) {
+				AUI().use(
+					'aui-debounce',
+					function(A) {
+						editor.on(
+							'change',
+							A.debounce(function() {
+								fetch(
+									editor.config.calculateReadingTimeURL,
+									{
+										body: new URLSearchParams(Liferay.Util.ns(
+											NAMESPACE,
+											{
+												content: editor.getData(),
+												contentType: 'text/html'
+											}
+										)),
+										credentials: 'same-origin',
+										method: 'POST'
+									}
+								).then(function(response) {
+									return response.json();
+								}).then(function(message) {
+									var readingTimeElement = A.one('#' + editor.config.readingTimeElementId);
+
+									if (readingTimeElement) {
+										readingTimeElement.html(message.readingTimeMessage);
+										readingTimeElement.setAttribute('datetime', message.readingTimeInSeconds + 's');
+									}
+
+									editor.fire('readingTime', message);
+								});
+							}, 500)
+						);
+					}
+				);
+			}
+		}
+	);
+})();
