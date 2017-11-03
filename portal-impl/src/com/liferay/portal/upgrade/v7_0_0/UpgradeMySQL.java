2e5900251dc95dc9466e20ca5087a5684c9bb180
amadeafejes <amadea.fejes@liferay.com>
2017-11-03 15:03:49 +0100

    LPS-75522 Check if table is a portal table by defining the tables statically

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeMySQL.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeMySQL.java
@@ -22,12 +22,17 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Amadea Fejes
@@ -77,9 +82,15 @@ public class UpgradeMySQL extends UpgradeProcess {
 			ResultSet rs = databaseMetaData.getTables(null, null, null, null)) {
 
 			while (rs.next()) {
+				String tableName = rs.getString("TABLE_NAME");
+
+				if (!_tableNames.contains(StringUtil.toLowerCase(tableName))) {
+					continue;
+				}
+
 				upgradeDatetimePrecisionForTable(
 					databaseMetaData, statement, rs.getString("TABLE_CAT"),
-					rs.getString("TABLE_SCHEM"), rs.getString("TABLE_NAME"));
+					rs.getString("TABLE_SCHEM"), tableName);
 			}
 		}
 	}
@@ -129,5 +140,55 @@ public class UpgradeMySQL extends UpgradeProcess {
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(UpgradeMySQL.class);
+
+	private static final Set<String> _tableNames = new HashSet<>(
+		Arrays.asList(
+			"account_", "address", "announcementsdelivery",
+			"announcementsentry", "announcementsflag", "assetcategory",
+			"assetcategoryproperty", "assetentries_assetcategories",
+			"assetentries_assettags", "assetentry", "assetlink", "assettag",
+			"assettagstats", "assetvocabulary", "backgroundtask", "blogsentry",
+			"blogsstatsuser", "bookmarksentry", "bookmarksfolder",
+			"browsertracker", "classname_", "clustergroup", "company",
+			"contact_", "counter", "country", "ddlrecord", "ddlrecordset",
+			"ddlrecordversion", "ddmcontent", "ddmstoragelink", "ddmstructure",
+			"ddmstructurelink", "ddmtemplate", "dlcontent", "dlfileentry",
+			"dlfileentrymetadata", "dlfileentrytype",
+			"dlfileentrytypes_dlfolders", "dlfilerank", "dlfileshortcut",
+			"dlfileversion", "dlfolder", "dlsyncevent", "emailaddress",
+			"expandocolumn", "expandorow", "expandotable", "expandovalue",
+			"exportimportconfiguration", "group_", "groups_orgs",
+			"groups_roles", "groups_usergroups", "image", "journalarticle",
+			"journalarticleimage", "journalarticleresource",
+			"journalcontentsearch", "journalfeed", "journalfolder", "layout",
+			"layoutbranch", "layoutfriendlyurl", "layoutprototype",
+			"layoutrevision", "layoutset", "layoutsetbranch",
+			"layoutsetprototype", "listtype", "lock_", "mbban", "mbcategory",
+			"mbdiscussion", "mbmailinglist", "mbmessage", "mbstatsuser",
+			"mbthread", "mbthreadflag", "mdraction", "mdrrule", "mdrrulegroup",
+			"mdrrulegroupinstance", "membershiprequest", "organization_",
+			"orggrouprole", "orglabor", "passwordpolicy", "passwordpolicyrel",
+			"passwordtracker", "phone", "pluginsetting", "pollschoice",
+			"pollsquestion", "pollsvote", "portalpreferences", "portlet",
+			"portletitem", "portletpreferences", "ratingsentry", "ratingsstats",
+			"recentlayoutbranch", "recentlayoutrevision",
+			"recentlayoutsetbranch", "region", "release_", "repository",
+			"repositoryentry", "resourceaction", "resourceblock",
+			"resourceblockpermission", "resourcepermission",
+			"resourcetypepermission", "role_", "servicecomponent",
+			"shoppingcart", "shoppingcategory", "shoppingcoupon",
+			"shoppingitem", "shoppingitemfield", "shoppingitemprice",
+			"shoppingorder", "shoppingorderitem", "socialactivity",
+			"socialactivityachievement", "socialactivitycounter",
+			"socialactivitylimit", "socialactivityset", "socialactivitysetting",
+			"socialrelation", "socialrequest", "subscription", "systemevent",
+			"team", "ticket", "trashentry", "trashversion",
+			"usernotificationdelivery", "user_", "usergroup",
+			"usergroupgrouprole", "usergrouprole", "usergroups_teams",
+			"useridmapper", "usernotificationevent", "users_groups",
+			"users_orgs", "users_roles", "users_teams", "users_usergroups",
+			"usertracker", "usertrackerpath", "virtualhost", "webdavprops",
+			"website", "wikinode", "wikipage", "wikipageresource",
+			"workflowdefinitionlink", "workflowinstancelink"));
 
 }
