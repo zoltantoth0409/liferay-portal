68515606f4d90b180f8cebb618d83cd2c660f90e
Brian Chan <brian.chan@liferay.com>
2017-11-03 08:50:56 -0700

    LPS-75010 regen

--- modules/apps/collaboration/document-library/document-library-content-service/src/main/java/com/liferay/document/library/content/service/persistence/impl/DLContentPersistenceImpl.java
+++ modules/apps/collaboration/document-library/document-library-content-service/src/main/java/com/liferay/document/library/content/service/persistence/impl/DLContentPersistenceImpl.java
@@ -22,6 +22,8 @@ import com.liferay.document.library.content.model.impl.DLContentImpl;
 import com.liferay.document.library.content.model.impl.DLContentModelImpl;
 import com.liferay.document.library.content.service.persistence.DLContentPersistence;
 
+import com.liferay.petra.string.CharPool;
+
 import com.liferay.portal.kernel.dao.orm.EntityCache;
 import com.liferay.portal.kernel.dao.orm.FinderCache;
 import com.liferay.portal.kernel.dao.orm.FinderPath;
@@ -34,7 +36,6 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.service.persistence.CompanyProvider;
 import com.liferay.portal.kernel.service.persistence.CompanyProviderWrapper;
 import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
-import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.util.SetUtil;
