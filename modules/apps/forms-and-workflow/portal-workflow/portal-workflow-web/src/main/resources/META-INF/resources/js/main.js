4dbca94e5abbb929eff0d0071c0f10d531b34795
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2017-11-03 13:09:37 -0300

    LPS-73719 Javascript function to handle the icon button click and open the dialog

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/resources/META-INF/resources/js/main.js
@@ -2,6 +2,62 @@ AUI.add(
 	'liferay-workflow-web',
 	function(A) {
 		var WorkflowWeb = {
+			previewBeforeRevert: function(event, renderUrl, actionUrl, title) {
+
+				var form = A.Node.create('<form />');
+
+				form.setAttribute('action', actionUrl);
+				form.setAttribute('method', 'POST');
+
+				var dialog	=	Liferay.Util.Window.getWindow(
+					{
+						dialog: {
+							destroyOnHide: true,
+							modal: true,
+							toolbars: {
+								footer: [
+									{
+										cssClass: 'btn btn-link',
+										discardDefaultButtonCssClasses: true,
+										label: Liferay.Language.get('cancel'),
+										on: {
+											click: function() {
+												dialog.destroy();
+											}
+										}
+									},
+									{
+										cssClass: 'btn btn-lg btn-primary',
+										discardDefaultButtonCssClasses: true,
+										label: Liferay.Language.get('restore'),
+										on: {
+											click: function() {
+												submitForm(form);
+											}
+										}
+									}
+								],
+								header: [
+									{
+										cssClass: 'close',
+										discardDefaultButtonCssClasses: true,
+										labelHTML: '<svg class="lexicon-icon" focusable="false"><use data-href="' + Liferay.ThemeDisplay.getPathThemeImages() + '/lexicon/icons.svg#times" /><title>' + Liferay.Language.get('close') + '</title></svg>',
+										on: {
+											click: function(event) {
+												dialog.destroy();
+
+												event.domEvent.stopPropagation();
+											}
+										}
+									}
+								]
+							}
+						},
+						title: title,
+						uri: renderUrl
+					}
+				);
+			},
 			showSuccessMessage: function() {
 				var instance = this;
 
@@ -34,13 +90,13 @@ AUI.add(
 
 				instance._alert = alert;
 			},
-
 			_alert: null
 		};
+
 		Liferay.WorkflowWeb = WorkflowWeb;
 	},
 	'',
 	{
-		requires: ['liferay-alert']
+		requires: ['liferay-alert', 'liferay-util-window']
 	}
 );
