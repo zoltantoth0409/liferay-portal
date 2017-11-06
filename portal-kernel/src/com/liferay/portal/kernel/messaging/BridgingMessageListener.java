45464c71b17de9e116771f7ffb61f01ca3349295
shuyangzhou <shuyang.zhou@liferay.com>
2017-11-06 09:52:57 -0800

    LPS-76672 Deprecated BridgingMessageListener, since there is no usage of it at all.

--- portal-kernel/src/com/liferay/portal/kernel/messaging/BridgingMessageListener.java
+++ portal-kernel/src/com/liferay/portal/kernel/messaging/BridgingMessageListener.java
@@ -18,7 +18,9 @@ import com.liferay.portal.kernel.messaging.sender.SingleDestinationMessageSender
 
 /**
  * @author Michael C. Han
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class BridgingMessageListener implements MessageListener {
 
 	public BridgingMessageListener() {
