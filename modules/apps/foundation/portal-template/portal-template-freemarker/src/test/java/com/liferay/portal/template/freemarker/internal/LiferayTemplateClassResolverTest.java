26f717b51211fa4ff1341a132259a1c7a2ca22bf
tina.tian <tina.tian@liferay.com>
2017-11-06 10:15:21 -0800

    LPS-75727 Make it a unit test

--- modules/apps/foundation/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/internal/LiferayTemplateClassResolverTest.java
+++ modules/apps/foundation/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/internal/LiferayTemplateClassResolverTest.java
@@ -14,88 +14,34 @@
 
 package com.liferay.portal.template.freemarker.internal;
 
-import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.template.freemarker.configuration.FreeMarkerEngineConfiguration;
 
 import freemarker.core.TemplateClassResolver;
 
 import freemarker.template.TemplateException;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
 import java.util.Dictionary;
 import java.util.Hashtable;
-import java.util.concurrent.CountDownLatch;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
-import org.osgi.framework.ServiceReference;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
-import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @author Tomas Polesovsky
  * @author Manuel de la Peña
  */
-@RunWith(Arquillian.class)
 public class LiferayTemplateClassResolverTest {
-
-	@BeforeClass
-	public static void setUpClass() {
-		Bundle bundle = FrameworkUtil.getBundle(
-			LiferayTemplateClassResolverTest.class);
-
-		_serviceTracker = new ServiceTracker<>(
-			bundle.getBundleContext(), TemplateClassResolver.class, null);
-
-		_serviceTracker.open();
-
-		_liferayTemplateClassResolver = _serviceTracker.getService();
-	}
-
-	@AfterClass
-	public static void tearDownClass() {
-		_serviceTracker.close();
-	}
 
 	@Before
 	public void setUp() throws Exception {
-		Bundle bundle = FrameworkUtil.getBundle(
-			_liferayTemplateClassResolver.getClass());
+		_liferayTemplateClassResolver = new LiferayTemplateClassResolver();
 
-		BundleContext bundleContext = bundle.getBundleContext();
-
-		ServiceReference<ConfigurationAdmin> serviceReference =
-			bundleContext.getServiceReference(ConfigurationAdmin.class);
-
-		try {
-			ConfigurationAdmin configurationAdmin = bundleContext.getService(
-				serviceReference);
-
-			_freemarkerTemplateConfiguration =
-				configurationAdmin.getConfiguration(
-					"com.liferay.portal.template.freemarker.configuration." +
-						"FreeMarkerEngineConfiguration",
-					null);
-
-			_properties = _freemarkerTemplateConfiguration.getProperties();
-		}
-		finally {
-			bundleContext.ungetService(serviceReference);
-		}
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		_updateProperties(_properties);
+		_updateProperties(new Hashtable<>());
 	}
 
 	@Test
@@ -218,50 +164,48 @@ public class LiferayTemplateClassResolverTest {
 	private void _updateProperties(Dictionary<String, Object> dictionary)
 		throws Exception {
 
-		final CountDownLatch countDownLatch = new CountDownLatch(1);
+		String allowedClasses = (String)dictionary.get("allowedClasses");
+		String restrictedClasses = (String)dictionary.get("restrictedClasses");
 
-		Bundle bundle = FrameworkUtil.getBundle(
-			_liferayTemplateClassResolver.getClass());
+		Object freeMarkerEngineConfiguration = ProxyUtil.newProxyInstance(
+			LiferayTemplateClassResolverTest.class.getClassLoader(),
+			new Class<?>[] {FreeMarkerEngineConfiguration.class},
+			new InvocationHandler() {
 
-		final BundleContext bundleContext = bundle.getBundleContext();
+				@Override
+				public Object invoke(Object proxy, Method method, Object[] args)
+					throws Throwable {
 
-		ServiceListener serviceListener = new ServiceListener() {
+					String methodName = method.getName();
 
-			@Override
-			public void serviceChanged(ServiceEvent serviceEvent) {
-				if (serviceEvent.getType() != ServiceEvent.MODIFIED) {
-					return;
+					if (methodName.equals("allowedClasses")) {
+						if (allowedClasses != null) {
+							return new String[] {allowedClasses};
+						}
+
+						return null;
+					}
+					else if (methodName.equals("restrictedClasses")) {
+						if (restrictedClasses != null) {
+							return new String[] {restrictedClasses};
+						}
+
+						return new String[] {
+							"java.lang.Class", "java.lang.ClassLoader",
+							"java.lang.Thread"
+						};
+					}
+
+					return null;
 				}
 
-				ServiceReference<?> serviceReference =
-					serviceEvent.getServiceReference();
+			});
 
-				Object service = bundleContext.getService(serviceReference);
-
-				if (service == _liferayTemplateClassResolver) {
-					countDownLatch.countDown();
-				}
-			}
-
-		};
-
-		bundleContext.addServiceListener(serviceListener);
-
-		try {
-			_freemarkerTemplateConfiguration.update(dictionary);
-
-			countDownLatch.await();
-		}
-		finally {
-			bundleContext.removeServiceListener(serviceListener);
-		}
+		ReflectionTestUtil.setFieldValue(
+			_liferayTemplateClassResolver, "_freemarkerEngineConfiguration",
+			freeMarkerEngineConfiguration);
 	}
 
-	private static TemplateClassResolver _liferayTemplateClassResolver;
-	private static ServiceTracker<TemplateClassResolver, TemplateClassResolver>
-		_serviceTracker;
-
-	private Configuration _freemarkerTemplateConfiguration;
-	private Dictionary<String, Object> _properties;
+	private TemplateClassResolver _liferayTemplateClassResolver;
 
 }
