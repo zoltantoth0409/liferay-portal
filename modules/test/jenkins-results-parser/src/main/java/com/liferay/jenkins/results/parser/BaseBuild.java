87203f68e41b2532d086e8f8e64457fb4d449419
Kenji Heigel <kenji.heigel@liferay.com>
2017-11-06 17:57:22 -0800

    LRQA-35664 Remove unused methods

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
@@ -1263,97 +1263,6 @@ public abstract class BaseBuild implements Build {
 
 	}
 
-	protected static List<String> getUpstreamJobFailures(String type) {
-		List<String> upstreamFailures = new ArrayList<>();
-
-		JSONArray failedBatchesJSONArray =
-			upstreamFailuresJobJSONObject.getJSONArray("failedBatches");
-
-		for (int i = 0; i < failedBatchesJSONArray.length(); i++) {
-			JSONObject failedBatchJSONObject =
-				failedBatchesJSONArray.getJSONObject(i);
-
-			JSONArray failedTestsJSONArray = failedBatchJSONObject.getJSONArray(
-				"failedTests");
-
-			String jobVariant = failedBatchJSONObject.getString("jobVariant");
-
-			if (type.equals("build")) {
-				if (failedTestsJSONArray.length() == 0) {
-					upstreamFailures.add(
-						JenkinsResultsParserUtil.combine(
-							jobVariant, ",",
-							failedBatchJSONObject.getString("result")));
-				}
-			}
-			else if (type.equals("test")) {
-				for (int j = 0; j < failedTestsJSONArray.length(); j++) {
-					Object object = failedTestsJSONArray.get(j);
-
-					upstreamFailures.add(
-						JenkinsResultsParserUtil.combine(
-							object.toString(), ",", jobVariant));
-				}
-			}
-		}
-
-		return upstreamFailures;
-	}
-
-	protected static String getUpstreamJobFailuresSHA() {
-		try {
-			return upstreamFailuresJobJSONObject.getString("SHA");
-		}
-		catch (JSONException jsone) {
-			System.out.println(
-				"Unable to get upstream acceptance failure data");
-
-			jsone.printStackTrace();
-
-			return "";
-		}
-	}
-
-	protected static boolean isBuildFailingInUpstreamJob(Build build) {
-		try {
-			List<TestResult> testResults = new ArrayList<>();
-
-			testResults.addAll(build.getTestResults("FAILED"));
-			testResults.addAll(build.getTestResults("REGRESSION"));
-
-			if (testResults.isEmpty()) {
-				String jobVariant = build.getJobVariant();
-				String result = build.getResult();
-
-				if (jobVariant.contains("/")) {
-					int index = jobVariant.lastIndexOf("/");
-
-					jobVariant = jobVariant.substring(0, index);
-				}
-
-				for (String upstreamJobFailure :
-						getUpstreamJobFailures("build")) {
-
-					if (upstreamJobFailure.contains(jobVariant) &&
-						upstreamJobFailure.contains(result)) {
-
-						return true;
-					}
-				}
-			}
-
-			return false;
-		}
-		catch (Exception e) {
-			System.out.println(
-				"Unable to get upstream acceptance failure data.");
-
-			e.printStackTrace();
-
-			return false;
-		}
-	}
-
 	protected static boolean isHighPriorityBuildFailureElement(
 		Element gitHubMessage) {
 
@@ -1373,38 +1282,6 @@ public abstract class BaseBuild implements Build {
 		}
 
 		return false;
-	}
-
-	protected static boolean isTestFailingInUpstreamJob(TestResult testResult) {
-		try {
-			for (String failure : getUpstreamJobFailures("test")) {
-				Build build = testResult.getBuild();
-
-				String jobVariant = build.getJobVariant();
-
-				if (jobVariant.contains("/")) {
-					int index = jobVariant.lastIndexOf("/");
-
-					jobVariant = jobVariant.substring(0, index);
-				}
-
-				if (failure.contains(jobVariant) &&
-					failure.contains(testResult.getDisplayName())) {
-
-					return true;
-				}
-			}
-
-			return false;
-		}
-		catch (Exception e) {
-			System.out.println(
-				"Unable to get upstream acceptance failure data.");
-
-			e.printStackTrace();
-
-			return false;
-		}
 	}
 
 	protected BaseBuild(String url) {
@@ -2199,28 +2076,6 @@ public abstract class BaseBuild implements Build {
 		}
 	}
 
-	protected void loadUpstreamJobFailuresJSONObject() {
-		try {
-			if (getJobName().contains("pullrequest")) {
-				String upstreamJobName =
-					getJobName().replace("pullrequest", "upstream");
-
-				String url = JenkinsResultsParserUtil.getLocalURL(
-					UPSTREAM_FAILURES_JOB_BASE_URL + upstreamJobName +
-						"/builds/latest/test.results.json");
-
-				upstreamFailuresJobJSONObject =
-					JenkinsResultsParserUtil.toJSONObject(url);
-			}
-		}
-		catch (IOException ioe) {
-			System.out.println(
-				"Unable to set upstream acceptance failure data.");
-
-			ioe.printStackTrace();
-		}
-	}
-
 	protected void reset() {
 		result = null;
 
@@ -2397,8 +2252,6 @@ public abstract class BaseBuild implements Build {
 			"buildWithParameters\\?(?<queryString>.*)"));
 	protected static final Pattern jobNamePattern = Pattern.compile(
 		"(?<baseJob>[^\\(]+)\\((?<branchName>[^\\)]+)\\)");
-	protected static JSONObject upstreamFailuresJobJSONObject = new JSONObject(
-		"{\"SHA\":\"\",\"failedBatches\":[]}");
 
 	protected String archiveName;
 	protected List<Integer> badBuildNumbers = new ArrayList<>();
