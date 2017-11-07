42a3db4b0c574f0e55a0ab8b1a6bdfeb48b1e270
riccardo-ferrari <riccardo.ferrari@liferay.com>
2017-11-07 17:33:43 +0100

    CLDSVCS-2939 lcs-messaging Update AnalyticsEventsMessage format

--- modules/dxp/apps/lcs/lcs-messaging/src/main/java/com/liferay/lcs/messaging/analytics/AnalyticsEventsMessage.java
+++ modules/dxp/apps/lcs/lcs-messaging/src/main/java/com/liferay/lcs/messaging/analytics/AnalyticsEventsMessage.java
@@ -14,91 +14,63 @@
 
 package com.liferay.lcs.messaging.analytics;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import com.liferay.lcs.messaging.Message;
 
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * @author Ivica Cardic
  * @author Riccardo Ferrari
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonSubTypes(
-	{@JsonSubTypes.Type(name = "AT", value = AnalyticsEventsMessage.class)}
-)
 @JsonTypeInfo(
+	defaultImpl = AnalyticsEventsMessage.class,
 	include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "messageFormat",
 	use = JsonTypeInfo.Id.NAME, visible = true
 )
 public class AnalyticsEventsMessage extends Message {
 
+	@NotNull
 	public String getAnalyticsKey() {
 		return _analyticsKey;
-	}
-
-	public long getAnonymousUserId() {
-		return _anonymousUserId;
-	}
-
-	public String getApplicationId() {
-		return _applicationId;
-	}
-
-	public String getChannel() {
-		return _channel;
 	}
 
 	public String getClientIP() {
 		return _clientIP;
 	}
 
-	public Context getContext() {
+	public Map<String, String> getContext() {
 		return _context;
 	}
 
+	@Size(min = 1)
+	@Valid
 	public List<Event> getEvents() {
 		return _events;
 	}
 
-	public String getMessageFormat() {
-		return _messageFormat;
-	}
-
-	public String getUserAgent() {
-		return _userAgent;
+	public String getUserId() {
+		return _userId;
 	}
 
 	public void setAnalyticsKey(String analyticsKey) {
 		_analyticsKey = analyticsKey;
 	}
 
-	public void setAnonymousUserId(long anonymousUserId) {
-		_anonymousUserId = anonymousUserId;
-	}
-
-	public void setApplicationId(String applicationId) {
-		_applicationId = applicationId;
-	}
-
-	public void setChannel(String channel) {
-		_channel = channel;
-	}
-
 	public void setClientIP(String clientIP) {
 		_clientIP = clientIP;
 	}
 
-	public void setContext(Context context) {
+	public void setContext(Map<String, String> context) {
 		_context = context;
 	}
 
@@ -106,154 +78,49 @@ public class AnalyticsEventsMessage extends Message {
 		_events = events;
 	}
 
-	public void setMessageFormat(String messageFormat) {
-		_messageFormat = messageFormat;
-	}
-
-	public void setUserAgent(String userAgent) {
-		_userAgent = userAgent;
-	}
-
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Context {
-
-		public long getInstanceId() {
-			return _instanceId;
-		}
-
-		public String getLanguageId() {
-			return _languageId;
-		}
-
-		public String getURL() {
-			return _url;
-		}
-
-		public long getUserId() {
-			return _userId;
-		}
-
-		public void setInstanceId(long instanceId) {
-			_instanceId = instanceId;
-		}
-
-		public void setLanguageId(String languageId) {
-			_languageId = languageId;
-		}
-
-		public void setURL(String url) {
-			_url = url;
-		}
-
-		public void setUserId(long userId) {
-			_userId = userId;
-		}
-
-		private long _instanceId;
-		private String _languageId;
-		private String _url;
-		private long _userId;
-
+	public void setUserId(String userId) {
+		_userId = userId;
 	}
 
 	@JsonIgnoreProperties(ignoreUnknown = true)
 	public static class Event {
 
-		public String getAdditionalInfo() {
-			return _additionalInfo;
+		@NotNull
+		public String getApplicationId() {
+			return _applicationId;
 		}
 
-		public String getEvent() {
-			return _event;
+		@NotNull
+		public String getEventId() {
+			return _eventId;
 		}
 
 		public Map<String, String> getProperties() {
 			return Collections.unmodifiableMap(_properties);
 		}
 
-		public List<Referrer> getReferrers() {
-			return Collections.unmodifiableList(_referrers);
+		public void setApplicationId(String applicationId) {
+			_applicationId = applicationId;
 		}
 
-		public Date getTimestamp() {
-			return _timestamp;
-		}
-
-		public void setAdditionalInfo(String additionalInfo) {
-			_additionalInfo = additionalInfo;
-		}
-
-		public void setEvent(String event) {
-			_event = event;
+		public void setEventId(String eventId) {
+			_eventId = eventId;
 		}
 
 		public void setProperties(Map<String, String> properties) {
 			_properties = properties;
 		}
 
-		public void setReferrers(List<Referrer> referrers) {
-			_referrers = referrers;
-		}
-
-		public void setTimestamp(Date timestamp) {
-			_timestamp = timestamp;
-		}
-
-		private String _additionalInfo;
-		private String _event;
+		private String _applicationId;
+		private String _eventId;
 		private Map<String, String> _properties = Collections.emptyMap();
-		private List<Referrer> _referrers = Collections.emptyList();
-
-		@JsonFormat(
-			pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
-			shape = JsonFormat.Shape.STRING, timezone = "UTC"
-		)
-		private Date _timestamp;
 
 	}
 
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Referrer {
-
-		public List<String> getReferrerEntityIds() {
-			return _referrerEntityIds;
-		}
-
-		public String getReferrerEntityType() {
-			return _referrerEntityType;
-		}
-
-		public void setReferrerEntityIds(List<String> referrerEntityIds) {
-			_referrerEntityIds = referrerEntityIds;
-		}
-
-		public void setReferrerEntityType(String referrerEntityType) {
-			_referrerEntityType = referrerEntityType;
-		}
-
-		private List<String> _referrerEntityIds = Collections.emptyList();
-		private String _referrerEntityType;
-
-	}
-
-	@NotNull
 	private String _analyticsKey;
-
-	private long _anonymousUserId;
-
-	@NotNull
-	private String _applicationId;
-
-	@NotNull
-	private String _channel;
-
 	private String _clientIP;
-	private Context _context;
+	private Map<String, String> _context = Collections.emptyMap();
 	private List<Event> _events = Collections.emptyList();
-
-	@NotNull
-	private String _messageFormat;
-
-	private String _userAgent;
+	private String _userId;
 
 }
