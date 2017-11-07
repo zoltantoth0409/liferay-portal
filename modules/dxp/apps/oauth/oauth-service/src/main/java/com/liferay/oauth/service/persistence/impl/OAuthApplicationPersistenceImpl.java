a2fa5c3db1f4998e0f1bbd0a1ef8c50dc9e8a1ce
tomwang2011 <tong.wang@liferay.com>
2017-11-06 16:40:00 -0800

    LPS-75010 Rebuild services

--- modules/dxp/apps/oauth/oauth-service/src/main/java/com/liferay/oauth/service/persistence/impl/OAuthApplicationPersistenceImpl.java
+++ modules/dxp/apps/oauth/oauth-service/src/main/java/com/liferay/oauth/service/persistence/impl/OAuthApplicationPersistenceImpl.java
@@ -22,6 +22,8 @@ import com.liferay.oauth.model.impl.OAuthApplicationImpl;
 import com.liferay.oauth.model.impl.OAuthApplicationModelImpl;
 import com.liferay.oauth.service.persistence.OAuthApplicationPersistence;
 
+import com.liferay.petra.string.CharPool;
+
 import com.liferay.portal.kernel.dao.orm.EntityCache;
 import com.liferay.portal.kernel.dao.orm.FinderCache;
 import com.liferay.portal.kernel.dao.orm.FinderPath;
@@ -36,7 +38,6 @@ import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.persistence.CompanyProvider;
 import com.liferay.portal.kernel.service.persistence.CompanyProviderWrapper;
 import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
-import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
