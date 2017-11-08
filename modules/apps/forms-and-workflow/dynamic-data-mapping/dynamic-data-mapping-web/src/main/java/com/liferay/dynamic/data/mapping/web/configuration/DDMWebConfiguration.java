461843acdcb4827e3635da5a38a974efd0d61eb4
Lance Ji <zheyi.ji@liferay.com>
2017-11-08 12:23:42 -0800

    LPS-75783 Fix localization of dynamic-data-mapping-web

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/configuration/DDMWebConfiguration.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/configuration/DDMWebConfiguration.java
@@ -37,7 +37,7 @@ public interface DDMWebConfiguration {
 	 */
 	@Meta.AD(
 		deflt = "true", description = "autogenerate-structure-key-description",
-		required = false
+		name = "autogenerate-structure-key", required = false
 	)
 	public boolean autogenerateStructureKey();
 
@@ -50,7 +50,7 @@ public interface DDMWebConfiguration {
 	 */
 	@Meta.AD(
 		deflt = "true", description = "autogenerate-template-key-description",
-		required = false
+		name = "autogenerate-template-key", required = false
 	)
 	public boolean autogenerateTemplateKey();
 
