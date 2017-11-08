7fd4ac7c8da992a6c0a5418191cee1aef64df8c8
shuyangzhou <shuyang.zhou@liferay.com>
2017-11-08 14:50:50 -0800

    LPS-75938 Rename packages

--- modules/core/petra/petra-process/src/test/java/com/liferay/petra/process/ProcessUtilTest.java
+++ modules/core/petra/petra-process/src/test/java/com/liferay/petra/process/ProcessUtilTest.java
@@ -12,10 +12,17 @@
  * details.
  */
 
-package com.liferay.portal.kernel.process;
+package com.liferay.petra.process;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.process.CollectorOutputProcessor;
+import com.liferay.portal.kernel.process.ConsumerOutputProcessor;
+import com.liferay.portal.kernel.process.LoggingOutputProcessor;
+import com.liferay.portal.kernel.process.OutputProcessor;
+import com.liferay.portal.kernel.process.ProcessException;
+import com.liferay.portal.kernel.process.ProcessUtil;
+import com.liferay.portal.kernel.process.TerminationProcessException;
 import com.liferay.portal.kernel.test.CaptureHandler;
 import com.liferay.portal.kernel.test.JDKLoggerTestUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
