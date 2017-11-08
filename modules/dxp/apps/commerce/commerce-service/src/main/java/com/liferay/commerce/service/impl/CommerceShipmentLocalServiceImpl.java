4838a7cfcde58d532ccce6c3b84c0979edfe66d7
Alessio Rendina <asilcomando@gmail.com>
2017-11-08 17:39:16 +0100

    COMMERCE-231 Added commerce shipment remote and local service implementations

---
+++ modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceShipmentLocalServiceImpl.java
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.service.impl;
+
+import com.liferay.commerce.exception.CommerceShipmentExpectedDateException;
+import com.liferay.commerce.exception.CommerceShipmentShippingDateException;
+import com.liferay.commerce.model.CommerceShipment;
+import com.liferay.commerce.service.base.CommerceShipmentLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceShipmentLocalServiceImpl
+	extends CommerceShipmentLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CommerceShipment addCommerceShipment(
+			long shipmentUserId, long commerceAddressId,
+			long commerceShippingMethodId, String carrier,
+			String trackingNumber, int expectedDuration, int status,
+			int shippingDateMonth, int shippingDateDay, int shippingDateYear,
+			int shippingDateHour, int shippingDateMinute, int expectedDateMonth,
+			int expectedDateDay, int expectedDateYear, int expectedDateHour,
+			int expectedDateMinute, ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		Date shippingDate = PortalUtil.getDate(
+			shippingDateMonth, shippingDateDay, shippingDateYear,
+			shippingDateHour, shippingDateMinute, user.getTimeZone(),
+			CommerceShipmentShippingDateException.class);
+
+		Date expectedDate = PortalUtil.getDate(
+			expectedDateMonth, expectedDateDay, expectedDateYear,
+			expectedDateHour, expectedDateMinute, user.getTimeZone(),
+			CommerceShipmentExpectedDateException.class);
+
+		long commerceShipmentId = counterLocalService.increment();
+
+		CommerceShipment commerceShipment = commerceShipmentPersistence.create(
+			commerceShipmentId);
+
+		commerceShipment.setGroupId(groupId);
+		commerceShipment.setCompanyId(user.getCompanyId());
+		commerceShipment.setUserId(user.getUserId());
+		commerceShipment.setUserName(user.getFullName());
+		commerceShipment.setShipmentUserId(shipmentUserId);
+		commerceShipment.setCommerceAddressId(commerceAddressId);
+		commerceShipment.setCommerceShippingMethodId(commerceShippingMethodId);
+		commerceShipment.setCarrier(carrier);
+		commerceShipment.setTrackingNumber(trackingNumber);
+		commerceShipment.setExpectedDuration(expectedDuration);
+		commerceShipment.setStatus(status);
+		commerceShipment.setShippingDate(shippingDate);
+		commerceShipment.setExpectedDate(expectedDate);
+
+		return commerceShipmentPersistence.update(commerceShipment);
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CommerceShipment deleteCommerceShipment(
+		CommerceShipment commerceShipment) {
+
+		// Commerce shipment
+
+		commerceShipmentPersistence.remove(commerceShipment);
+
+		// Commerce shipment items
+
+		commerceShipmentItemLocalService.deleteCommerceShipmentItems(
+			commerceShipment.getGroupId(),
+			commerceShipment.getCommerceShipmentId());
+
+		return commerceShipment;
+	}
+
+	@Override
+	public CommerceShipment deleteCommerceShipment(long commerceShipmentId)
+		throws PortalException {
+
+		CommerceShipment commerceShipment =
+			commerceShipmentPersistence.findByPrimaryKey(commerceShipmentId);
+
+		return commerceShipmentLocalService.deleteCommerceShipment(
+			commerceShipment);
+	}
+
+	@Override
+	public List<CommerceShipment> getCommerceShipments(
+		long groupId, int start, int end,
+		OrderByComparator<CommerceShipment> orderByComparator) {
+
+		return commerceShipmentPersistence.findByGroupId(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceShipmentsCount(long groupId) {
+		return commerceShipmentPersistence.countByGroupId(groupId);
+	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CommerceShipment updateCommerceShipment(
+			long commerceShipmentId, long shipmentUserId,
+			long commerceAddressId, long commerceShippingMethodId,
+			String carrier, String trackingNumber, int expectedDuration,
+			int status, int shippingDateMonth, int shippingDateDay,
+			int shippingDateYear, int shippingDateHour, int shippingDateMinute,
+			int expectedDateMonth, int expectedDateDay, int expectedDateYear,
+			int expectedDateHour, int expectedDateMinute)
+		throws PortalException {
+
+		CommerceShipment commerceShipment =
+			commerceShipmentPersistence.findByPrimaryKey(commerceShipmentId);
+
+		User user = userLocalService.getUser(commerceShipment.getUserId());
+
+		Date shippingDate = PortalUtil.getDate(
+			shippingDateMonth, shippingDateDay, shippingDateYear,
+			shippingDateHour, shippingDateMinute, user.getTimeZone(),
+			CommerceShipmentShippingDateException.class);
+
+		Date expectedDate = PortalUtil.getDate(
+			expectedDateMonth, expectedDateDay, expectedDateYear,
+			expectedDateHour, expectedDateMinute, user.getTimeZone(),
+			CommerceShipmentExpectedDateException.class);
+
+		commerceShipment.setShipmentUserId(shipmentUserId);
+		commerceShipment.setCommerceAddressId(commerceAddressId);
+		commerceShipment.setCommerceShippingMethodId(commerceShippingMethodId);
+		commerceShipment.setCarrier(carrier);
+		commerceShipment.setTrackingNumber(trackingNumber);
+		commerceShipment.setExpectedDuration(expectedDuration);
+		commerceShipment.setStatus(status);
+		commerceShipment.setShippingDate(shippingDate);
+		commerceShipment.setExpectedDate(expectedDate);
+
+		return commerceShipmentPersistence.update(commerceShipment);
+	}
+
+}
---
+++ modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceShipmentServiceImpl.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.service.impl;
+
+import com.liferay.commerce.constants.CommerceActionKeys;
+import com.liferay.commerce.model.CommerceShipment;
+import com.liferay.commerce.service.base.CommerceShipmentServiceBaseImpl;
+import com.liferay.commerce.service.permission.CommerceShipmentPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceShipmentServiceImpl
+	extends CommerceShipmentServiceBaseImpl {
+
+	@Override
+	public CommerceShipment addCommerceShipment(
+			long shipmentUserId, long commerceAddressId,
+			long commerceShippingMethodId, String carrier,
+			String trackingNumber, int expectedDuration, int status,
+			int shippingDateMonth, int shippingDateDay, int shippingDateYear,
+			int shippingDateHour, int shippingDateMinute, int expectedDateMonth,
+			int expectedDateDay, int expectedDateYear, int expectedDateHour,
+			int expectedDateMinute, ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceShipmentPermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			CommerceActionKeys.MANAGE_COMMERCE_SHIPMENTS);
+
+		return commerceShipmentLocalService.addCommerceShipment(
+			shipmentUserId, commerceAddressId, commerceShippingMethodId,
+			carrier, trackingNumber, expectedDuration, status,
+			shippingDateMonth, shippingDateDay, shippingDateYear,
+			shippingDateHour, shippingDateMinute, expectedDateMonth,
+			expectedDateDay, expectedDateYear, expectedDateHour,
+			expectedDateMinute, serviceContext);
+	}
+
+	@Override
+	public void deleteCommerceShipment(long commerceShipmentId)
+		throws PortalException {
+
+		CommerceShipment commerceShipment =
+			commerceShipmentPersistence.findByPrimaryKey(commerceShipmentId);
+
+		CommerceShipmentPermission.check(
+			getPermissionChecker(), commerceShipment.getGroupId(),
+			CommerceActionKeys.MANAGE_COMMERCE_SHIPMENTS);
+
+		commerceShipmentLocalService.deleteCommerceShipment(commerceShipment);
+	}
+
+	@Override
+	public List<CommerceShipment> getCommerceShipments(
+		long groupId, int start, int end,
+		OrderByComparator<CommerceShipment> orderByComparator) {
+
+		return commerceShipmentLocalService.getCommerceShipments(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceShipmentsCount(long groupId) {
+		return commerceShipmentLocalService.getCommerceShipmentsCount(groupId);
+	}
+
+	@Override
+	public CommerceShipment updateCommerceShipment(
+			long commerceShipmentId, long shipmentUserId,
+			long commerceAddressId, long commerceShippingMethodId,
+			String carrier, String trackingNumber, int expectedDuration,
+			int status, int shippingDateMonth, int shippingDateDay,
+			int shippingDateYear, int shippingDateHour, int shippingDateMinute,
+			int expectedDateMonth, int expectedDateDay, int expectedDateYear,
+			int expectedDateHour, int expectedDateMinute)
+		throws PortalException {
+
+		CommerceShipment commerceShipment =
+			commerceShipmentPersistence.findByPrimaryKey(commerceShipmentId);
+
+		CommerceShipmentPermission.check(
+			getPermissionChecker(), commerceShipment.getGroupId(),
+			CommerceActionKeys.MANAGE_COMMERCE_SHIPMENTS);
+
+		return commerceShipmentLocalService.updateCommerceShipment(
+			commerceShipmentId, shipmentUserId, commerceAddressId,
+			commerceShippingMethodId, carrier, trackingNumber, expectedDuration,
+			status, shippingDateMonth, shippingDateDay, shippingDateYear,
+			shippingDateHour, shippingDateMinute, expectedDateMonth,
+			expectedDateDay, expectedDateYear, expectedDateHour,
+			expectedDateMinute);
+	}
+
+}
