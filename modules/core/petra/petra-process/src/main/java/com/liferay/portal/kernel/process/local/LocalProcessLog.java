1d095eb8c43142554b883bf1759a9ccccbca5b98
shuyangzhou <shuyang.zhou@liferay.com>
2017-11-08 11:13:00 -0800

    LPS-75938 Copy over non-deprecated classes

---
+++ modules/core/petra/petra-process/src/main/java/com/liferay/portal/kernel/process/local/LocalProcessLog.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.process.local;
+
+import com.liferay.petra.lang.HashUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.process.ProcessLog;
+import com.liferay.portal.kernel.process.ProcessLog.Level;
+
+import java.util.Objects;
+
+/**
+ * @author Shuyang Zhou
+ */
+class LocalProcessLog implements ProcessLog {
+
+	LocalProcessLog(Level level, String message, Throwable throwable) {
+		_level = level;
+		_message = message;
+		_throwable = throwable;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof ProcessLog)) {
+			return false;
+		}
+
+		ProcessLog processLog = (ProcessLog)obj;
+
+		if ((_level == processLog.getLevel()) &&
+			Objects.equals(_message, processLog.getMessage()) &&
+			Objects.equals(_throwable, processLog.getThrowable())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public Level getLevel() {
+		return _level;
+	}
+
+	@Override
+	public String getMessage() {
+		return _message;
+	}
+
+	@Override
+	public Throwable getThrowable() {
+		return _throwable;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = HashUtil.hash(0, _level);
+
+		hash = HashUtil.hash(hash, _message);
+		hash = HashUtil.hash(hash, _throwable);
+
+		return hash;
+	}
+
+	@Override
+	public String toString() {
+		StringBundler sb = new StringBundler(7);
+
+		sb.append("{level=");
+		sb.append(_level);
+		sb.append(", message=");
+		sb.append(_message);
+		sb.append(", throwable=");
+		sb.append(_throwable);
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	private final Level _level;
+	private final String _message;
+	private final Throwable _throwable;
+
+}
---
+++ modules/core/petra/petra-process/src/main/java/com/liferay/portal/kernel/process/local/RequestProcessCallable.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.process.local;
+
+import com.liferay.portal.kernel.process.ProcessCallable;
+import com.liferay.portal.kernel.process.ProcessException;
+import com.liferay.portal.kernel.process.local.LocalProcessLauncher.ProcessContext;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class RequestProcessCallable<T extends Serializable>
+	implements ProcessCallable<T> {
+
+	public RequestProcessCallable(long id, ProcessCallable<T> processCallable) {
+		_id = id;
+		_processCallable = processCallable;
+	}
+
+	@Override
+	public T call() throws ProcessException {
+		T result = null;
+		Throwable throwable = null;
+
+		try {
+			result = _processCallable.call();
+
+			return result;
+		}
+		catch (Throwable t) {
+			throwable = t;
+
+			throw t;
+		}
+		finally {
+			try {
+				ProcessContext.writeProcessCallable(
+					new ResponseProcessCallable<>(_id, result, throwable));
+			}
+			catch (IOException ioe) {
+				throw new ProcessException(ioe);
+			}
+		}
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	private final long _id;
+	private final ProcessCallable<T> _processCallable;
+
+}
