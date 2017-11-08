f32d127571a5aedba3b18c53401abbdbe6f87300
Lance Ji <zheyi.ji@liferay.com>
2017-11-08 10:43:06 -0800

    LPS-75299 Fixe localization of journal-web

--- modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/configuration/JournalWebConfiguration.java
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/configuration/JournalWebConfiguration.java
@@ -35,13 +35,21 @@ public interface JournalWebConfiguration {
 	)
 	public boolean changeableDefaultLanguage();
 
-	@Meta.AD(deflt = "descriptive", required = false)
+	@Meta.AD(
+		deflt = "descriptive", name = "default-display-view", required = false
+	)
 	public String defaultDisplayView();
 
-	@Meta.AD(deflt = "icon|descriptive|list", required = false)
+	@Meta.AD(
+		deflt = "icon|descriptive|list", name = "display-views",
+		required = false
+	)
 	public String[] displayViews();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(
+		deflt = "true", name = "journal-article-force-autogenerate-id",
+		required = false
+	)
 	public boolean journalArticleForceAutogenerateId();
 
 	/**
@@ -51,20 +59,28 @@ public interface JournalWebConfiguration {
 	@Meta.AD(
 		deflt = "true",
 		description = "journal-article-search-with-index-description",
-		required = false
+		name = "journal-articles-search-with-index", required = false
 	)
 	public boolean journalArticlesSearchWithIndex();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(
+		deflt = "true", name = "journal-feed-force-autogenerate-id",
+		required = false
+	)
 	public boolean journalFeedForceAutogenerateId();
 
-	@Meta.AD(deflt = "7", required = false)
+	@Meta.AD(deflt = "7", name = "max-add-menu-items", required = false)
 	public int maxAddMenuItems();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(
+		deflt = "true", name = "publish-to-live-by-default", required = false
+	)
 	public boolean publishToLiveByDefault();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(
+		deflt = "true", name = "publish-version-history-by-default",
+		required = false
+	)
 	public boolean publishVersionHistoryByDefault();
 
 }
