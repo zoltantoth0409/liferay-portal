1c37a1f297ee617af6483fb05ef092a2ae282a87
Marco Leo <marco.leo@liferay.com>
2017-11-09 14:43:19 +0100

    COMMERCE-238 Add new CPoptionValueList MetalJs componet

---
+++ modules/dxp/apps/commerce/commerce-product-options-web/src/main/resources/META-INF/resources/CPOptionValueList.es.js
@@ -0,0 +1,52 @@
+import Component from 'metal-component';
+import { Config } from 'metal-state';
+import Dropdown from 'metal-dropdown';
+import Soy from 'metal-soy';
+import {dom, globalEval} from 'metal-dom';
+import { CancellablePromise } from 'metal-promise';
+import { async, core } from 'metal';
+
+import templates from './CPOptionValueList.soy';
+
+/**
+ * CPOptionValueList
+ *
+ */
+class CPOptionValueList extends Component {
+
+	_handleAddOptionValueClick(event) {
+
+		event.stopImmediatePropagation();
+		event.preventDefault();
+
+		this.emit('addOptionValue');
+	}
+
+	_handleSelectOptionValueClick(event) {
+		var target = event.target;
+
+		var row = dom.closest(target, 'tr');
+
+		var  cpOptionValueId =  row.getAttribute('data-id');
+
+		this.emit('optionValueSelected', cpOptionValueId);
+	}
+}
+
+/**
+ * State definition.
+ * @type {!Object}
+ * @static
+ */
+CPOptionValueList.STATE = {
+
+	optionValues : Config.array().value([]),
+
+	currentCPOptionValueId  : Config.string()
+
+};
+
+// Register component
+Soy.register(CPOptionValueList, templates);
+
+export default CPOptionValueList;
