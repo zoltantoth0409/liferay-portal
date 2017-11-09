fa0453aad3df1439edc14ec06dccde23cf6d38dd
Marco Leo <marco.leo@liferay.com>
2017-11-09 14:45:34 +0100

    COMMERCE-238 Refactor to save and return json

--- modules/dxp/apps/commerce/commerce-product-options-web/src/main/java/com/liferay/commerce/product/options/web/internal/portlet/action/EditCPOptionValueMVCRenderCommand.java
+++ modules/dxp/apps/commerce/commerce-product-options-web/src/main/java/com/liferay/commerce/product/options/web/internal/portlet/action/EditCPOptionValueMVCRenderCommand.java
@@ -33,7 +33,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Alessio Antonio Rendina
+ * @author Marco Leo
  */
 @Component(
 	immediate = true,
