44408506caacae4a9e4d5b85d9ef1fd5e9dbba05
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-11-12 11:04:25 +0100

    LPS-75773 Adds new action command for add new site navigation menu item

---
+++ modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/portlet/action/AddSiteNavigationMenuItemMVCActionCommand.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.admin.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropertiesParamUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.site.navigation.admin.web.internal.constants.SiteNavigationAdminPortletKeys;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemService;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + SiteNavigationAdminPortletKeys.SITE_NAVIGATION_ADMIN,
+		"mvc.command.name=/navigation_menu/add_site_navigation_menu_item"
+	},
+	service = MVCActionCommand.class
+)
+public class AddSiteNavigationMenuItemMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long siteNavigationMenuId = ParamUtil.getLong(
+			actionRequest, "siteNavigationMenuId");
+
+		String type = ParamUtil.getString(actionRequest, "type");
+
+		UnicodeProperties typeSettingsProperties =
+			PropertiesParamUtil.getProperties(
+				actionRequest, "TypeSettingsProperties--");
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			actionRequest);
+
+		_siteNavigationMenuItemService.addSiteNavigationMenuItem(
+			themeDisplay.getScopeGroupId(), siteNavigationMenuId, 0, type,
+			typeSettingsProperties.toString(), serviceContext);
+	}
+
+	@Reference
+	private SiteNavigationMenuItemService _siteNavigationMenuItemService;
+
+}
