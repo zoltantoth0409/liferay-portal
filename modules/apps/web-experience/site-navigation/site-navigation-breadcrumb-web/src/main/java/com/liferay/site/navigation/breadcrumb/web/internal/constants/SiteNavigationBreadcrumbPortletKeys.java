8ab0b9e1978538dc3a582943157f55dc8acaae00
Matthew Tambara <matthew.tambara@liferay.com>
2017-11-13 09:27:53 -0800

    LPS-75871 Move to internal

--- modules/apps/web-experience/site-navigation/site-navigation-breadcrumb-web/src/main/java/com/liferay/site/navigation/breadcrumb/web/internal/constants/SiteNavigationBreadcrumbPortletKeys.java
+++ modules/apps/web-experience/site-navigation/site-navigation-breadcrumb-web/src/main/java/com/liferay/site/navigation/breadcrumb/web/internal/constants/SiteNavigationBreadcrumbPortletKeys.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.site.navigation.breadcrumb.web.constants;
+package com.liferay.site.navigation.breadcrumb.web.internal.constants;
 
 /**
  * @author Eudaldo Alonso
