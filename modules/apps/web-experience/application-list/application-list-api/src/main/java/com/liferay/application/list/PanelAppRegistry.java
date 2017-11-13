f440b30ef48b4685451d9a32a7ac8bd0a167142f
drewbrokke <drew.brokke@liferay.com>
2017-11-13 11:59:04 -0800

    LPS-74362 application-list-api - PanelAppRegistry.java - removes permission logic since it shouldn't be in this class anyways

--- modules/apps/web-experience/application-list/application-list-api/src/main/java/com/liferay/application/list/PanelAppRegistry.java
+++ modules/apps/web-experience/application-list/application-list-api/src/main/java/com/liferay/application/list/PanelAppRegistry.java
@@ -22,37 +22,22 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.Portlet;
-import com.liferay.portal.kernel.model.PortletConstants;
-import com.liferay.portal.kernel.model.ResourceConstants;
-import com.liferay.portal.kernel.model.Role;
-import com.liferay.portal.kernel.model.RoleConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactory;
-import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.PortletCategoryKeys;
-import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.PredicateFilter;
 import com.liferay.portal.kernel.util.PrefsProps;
-import com.liferay.portal.kernel.util.StringPool;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
-import javax.portlet.PortletPreferences;
-import javax.portlet.ReadOnlyException;
-import javax.portlet.ValidatorException;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -280,13 +265,6 @@ public class PanelAppRegistry {
 			if (portlet != null) {
 				portlet.setControlPanelEntryCategory(panelCategoryKey);
 
-				try {
-					initPersonalControlPanelPortletPermission(portlet);
-				}
-				catch (Exception e) {
-					_log.error(e, e);
-				}
-
 				panelApp.setPortlet(portlet);
 			}
 			else if (_log.isDebugEnabled()) {
@@ -299,58 +277,6 @@ public class PanelAppRegistry {
 			ServiceTrackerMap<String, List<PanelApp>> serviceTrackerMap,
 			String panelCategoryKey, PanelApp panelApp,
 			List<PanelApp> panelApps) {
-		}
-
-		protected void initPersonalControlPanelPortletPermission(
-				Portlet portlet)
-			throws IOException, PortalException, ReadOnlyException,
-				   ValidatorException {
-
-			String category = portlet.getControlPanelEntryCategory();
-
-			if ((category == null) ||
-				!category.equals(PortletCategoryKeys.USER_MY_ACCOUNT)) {
-
-				return;
-			}
-
-			long companyId = portlet.getCompanyId();
-			String portletId = portlet.getPortletId();
-
-			PortletPreferences portletPreferences =
-				_portletPreferencesFactory.getLayoutPortletSetup(
-					companyId, companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
-					LayoutConstants.DEFAULT_PLID, portletId,
-					PortletConstants.DEFAULT_PREFERENCES);
-
-			if (_prefsProps.getBoolean(
-					portletPreferences,
-					"myAccountAccessInControlPanelPermissionsInitialized")) {
-
-				return;
-			}
-
-			Role userRole = _roleLocalService.getRole(
-				companyId, RoleConstants.USER);
-
-			List<String> actionIds =
-				ResourceActionsUtil.getPortletResourceActions(
-					portlet.getRootPortletId());
-
-			String actionId = ActionKeys.ACCESS_IN_CONTROL_PANEL;
-
-			if (actionIds.contains(actionId)) {
-				_resourcePermissionLocalService.addResourcePermission(
-					companyId, portlet.getRootPortletId(),
-					ResourceConstants.SCOPE_COMPANY, String.valueOf(companyId),
-					userRole.getRoleId(), actionId);
-			}
-
-			portletPreferences.setValue(
-				"myAccountAccessInControlPanelPermissionsInitialized",
-				StringPool.TRUE);
-
-			portletPreferences.store();
 		}
 
 	}
