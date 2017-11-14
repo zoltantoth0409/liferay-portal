564d11a5ed4456ab7f988e925e61c6a27c57d8df
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-11-14 17:24:15 +0100

    LPS-75920 SiteNavigationMenuItemType for Page Set/Node

---
+++ modules/apps/web-experience/site-navigation/site-navigation-menu-item-node/src/main/java/com/liferay/site/navigation/menu/item/node/internal/type/NodeSiteNavigationMenuItemType.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.menu.item.node.internal.type;
+
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.site.navigation.menu.item.node.internal.constants.SiteNavigationMenuItemTypeNodeConstants;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.type.SiteNavigationMenuItemType;
+
+import java.io.IOException;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {"site.navigation.menu.item.type=" + SiteNavigationMenuItemTypeNodeConstants.NODE},
+	service = SiteNavigationMenuItemType.class
+)
+public class NodeSiteNavigationMenuItemType
+	implements SiteNavigationMenuItemType {
+
+	@Override
+	public String getIcon() {
+		return "pages-tree";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle =
+			_resourceBundleLoader.loadResourceBundle(locale);
+
+		return LanguageUtil.get(resourceBundle, "page-set");
+	}
+
+	@Override
+	public String getTitle(
+		SiteNavigationMenuItem siteNavigationMenuItem, Locale locale) {
+
+		UnicodeProperties typeSettingsProperties = new UnicodeProperties();
+
+		typeSettingsProperties.fastLoad(
+			siteNavigationMenuItem.getTypeSettings());
+
+		return typeSettingsProperties.get("name");
+	}
+
+	@Override
+	public String getType() {
+		return SiteNavigationMenuItemTypeNodeConstants.NODE;
+	}
+
+	@Override
+	public JSONObject getViewContext(
+			HttpServletRequest request, HttpServletResponse response,
+			SiteNavigationMenuItem siteNavigationMenuItem)
+		throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public void renderAddPage(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		_jspRenderer.renderJSP(_servletContext, request, response, "/node.jsp");
+	}
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference(
+		target = "(bundle.symbolic.name=com.liferay.site.navigation.menu.item.node)",
+		unbind = "-"
+	)
+	private ResourceBundleLoader _resourceBundleLoader;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.site.navigation.menu.item.node)",
+		unbind = "-"
+	)
+	private ServletContext _servletContext;
+
+}
