b836631e06c88020ffdb632861b470e7322063a1
Alejandro Tardín <alejandro.tardin@liferay.com>
2017-11-14 17:02:08 +0100

    LPS-74943 Adding extra methods to service

--- modules/apps/collaboration/reading-time/reading-time-service/src/main/java/com/liferay/reading/time/service/impl/ReadingTimeEntryLocalServiceImpl.java
+++ modules/apps/collaboration/reading-time/reading-time-service/src/main/java/com/liferay/reading/time/service/impl/ReadingTimeEntryLocalServiceImpl.java
@@ -14,8 +14,16 @@
 
 package com.liferay.reading.time.service.impl;
 
+import com.liferay.portal.kernel.model.GroupedModel;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+import com.liferay.reading.time.calculator.ReadingTimeCalculator;
 import com.liferay.reading.time.model.ReadingTimeEntry;
 import com.liferay.reading.time.service.base.ReadingTimeEntryLocalServiceBaseImpl;
+
+import java.time.Duration;
+
+import java.util.Optional;
 
 /**
  * @author Alejandro Tardín
@@ -25,7 +33,17 @@ public class ReadingTimeEntryLocalServiceImpl
 
 	@Override
 	public ReadingTimeEntry addReadingTimeEntry(
-		long groupId, long classNameId, long classPK, long readingTime) {
+		GroupedModel groupedModel, Duration readingTime) {
+
+		return addReadingTimeEntry(
+			groupedModel.getGroupId(),
+			_classNameLocalService.getClassNameId(groupedModel.getModelClass()),
+			(Long)groupedModel.getPrimaryKeyObj(), readingTime);
+	}
+
+	@Override
+	public ReadingTimeEntry addReadingTimeEntry(
+		long groupId, long classNameId, long classPK, Duration readingTime) {
 
 		long entryId = counterLocalService.increment();
 
@@ -34,9 +52,55 @@ public class ReadingTimeEntryLocalServiceImpl
 		entry.setClassNameId(classNameId);
 		entry.setClassPK(classPK);
 		entry.setGroupId(groupId);
-		entry.setReadingTime(readingTime);
+		entry.setReadingTime(readingTime.toMillis());
 
 		return readingTimeEntryPersistence.update(entry);
+	}
+
+	@Override
+	public ReadingTimeEntry deleteReadingTimeEntry(GroupedModel groupedModel) {
+		ReadingTimeEntry readingTimeEntry = fetchReadingTimeEntry(groupedModel);
+
+		if (readingTimeEntry != null) {
+			return deleteReadingTimeEntry(readingTimeEntry);
+		}
+
+		return null;
+	}
+
+	@Override
+	public ReadingTimeEntry deleteReadingTimeEntry(
+		long groupId, long classNameId, long classPK) {
+
+		ReadingTimeEntry readingTimeEntry = fetchReadingTimeEntry(
+			groupId, classNameId, classPK);
+
+		if (readingTimeEntry != null) {
+			return deleteReadingTimeEntry(readingTimeEntry);
+		}
+
+		return null;
+	}
+
+	@Override
+	public ReadingTimeEntry fetchOrAddReadingTimeEntry(
+		GroupedModel groupedModel) {
+
+		ReadingTimeEntry readingTimeEntry = fetchReadingTimeEntry(groupedModel);
+
+		if (readingTimeEntry != null) {
+			return readingTimeEntry;
+		}
+
+		return updateReadingTimeEntry(groupedModel);
+	}
+
+	@Override
+	public ReadingTimeEntry fetchReadingTimeEntry(GroupedModel groupedModel) {
+		return fetchReadingTimeEntry(
+			groupedModel.getGroupId(),
+			_classNameLocalService.getClassNameId(groupedModel.getModelClass()),
+			(Long)groupedModel.getPrimaryKeyObj());
 	}
 
 	@Override
@@ -46,5 +110,46 @@ public class ReadingTimeEntryLocalServiceImpl
 		return readingTimeEntryPersistence.fetchByG_C_C(
 			groupId, classNameId, classPK);
 	}
+
+	@Override
+	public ReadingTimeEntry updateReadingTimeEntry(GroupedModel groupedModel) {
+		Optional<Duration> readingTimeOptional =
+			_readingTimeCalculator.calculate(groupedModel);
+
+		if (readingTimeOptional.isPresent()) {
+			return updateReadingTimeEntry(
+				groupedModel.getGroupId(),
+				_classNameLocalService.getClassNameId(
+					groupedModel.getModelClass()),
+				(Long)groupedModel.getPrimaryKeyObj(),
+				readingTimeOptional.get());
+		}
+
+		return null;
+	}
+
+	@Override
+	public ReadingTimeEntry updateReadingTimeEntry(
+		long groupId, long classNameId, long classPK, Duration readingTime) {
+
+		ReadingTimeEntry readingTimeEntry = fetchReadingTimeEntry(
+			groupId, classNameId, classPK);
+
+		if (readingTimeEntry == null) {
+			return addReadingTimeEntry(
+				groupId, classNameId, classPK, readingTime);
+		}
+		else {
+			readingTimeEntry.setReadingTime(readingTime.toMillis());
+
+			return updateReadingTimeEntry(readingTimeEntry);
+		}
+	}
+
+	@ServiceReference(type = ClassNameLocalService.class)
+	private ClassNameLocalService _classNameLocalService;
+
+	@ServiceReference(type = ReadingTimeCalculator.class)
+	private ReadingTimeCalculator _readingTimeCalculator;
 
 }
