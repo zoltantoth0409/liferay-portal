c33e62989a170f3c3fac4ac90a5da4ef7786f3f1
Iván Zaera Avellón <ivan.zaera@liferay.com>
2017-11-14 14:28:11 +0100

    LPS-75889 Modify blade's Angular template to make it truly instanceable

--- modules/sdk/project-templates/project-templates-npm-angular-portlet/src/main/resources/archetype-resources/src/main/resources/META-INF/resources/js/app/app.module.ts
+++ modules/sdk/project-templates/project-templates-npm-angular-portlet/src/main/resources/archetype-resources/src/main/resources/META-INF/resources/js/app/app.module.ts
@@ -10,6 +10,13 @@ import { AppComponent }  from './app.component';
 	declarations: [
 		AppComponent
 	],
-	bootstrap: [ AppComponent ]
+	entryComponents: [AppComponent],
+	bootstrap: [], // Don't bootstrap anything (see ngDoBootstrap() below)
+	providers: []
 })
-export class AppModule { }
+export class AppModule {
+	// Avoid bootstraping any component statically because we need to attach to
+	// the portlet's DOM, which is different for each portlet instance and,
+	// thus, cannot be determined until the page is rendered (during runtime).
+	ngDoBootstrap() {}
+}
