aa2e2c465dadef78c534b4859cda4635a3398192
Preston-Crary <preston.crary@liferay.com>
2017-11-15 10:15:23 -0800

    LPS-75909 Make constants internal.

--- modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/GetFolderMVCRenderCommand.java
+++ modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/GetFolderMVCRenderCommand.java
@@ -15,7 +15,7 @@
 package com.liferay.document.library.web.internal.portlet.action;
 
 import com.liferay.document.library.kernel.exception.NoSuchFolderException;
-import com.liferay.document.library.web.constants.DLWebKeys;
+import com.liferay.document.library.web.internal.constants.DLWebKeys;
 import com.liferay.document.library.web.internal.util.DLTrashUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.repository.model.Folder;
