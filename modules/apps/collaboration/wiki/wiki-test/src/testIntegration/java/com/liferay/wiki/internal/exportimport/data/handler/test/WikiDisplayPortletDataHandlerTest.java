2990cff012ba6eae73ca82d067c39ace650c5f18
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-15 16:35:46 +0100

    LPS-75933 PortletDataHandlerTests - WikiDisplayPortletDataHandlerTest.

---
+++ modules/apps/collaboration/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/internal/exportimport/data/handler/test/WikiDisplayPortletDataHandlerTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.internal.exportimport.data.handler.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.exportimport.kernel.lar.DataLevel;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.model.WikiNode;
+import com.liferay.wiki.model.WikiPage;
+import com.liferay.wiki.util.test.WikiTestUtil;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Zoltan Csaszi
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class WikiDisplayPortletDataHandlerTest
+	extends BasePortletDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@Override
+	protected void addStagedModels() throws Exception {
+		WikiNode defaultWikiNode = WikiTestUtil.addDefaultNode(
+			stagingGroup.getGroupId());
+		WikiNode wikiNode = WikiTestUtil.addNode(stagingGroup.getGroupId());
+
+		WikiPage wikiPage = WikiTestUtil.addPage(
+			stagingGroup.getGroupId(), wikiNode.getNodeId(), true);
+	}
+
+	@Override
+	protected DataLevel getDataLevel() {
+		return DataLevel.PORTLET_INSTANCE;
+	}
+
+	@Override
+	protected String getPortletId() {
+		return WikiPortletKeys.WIKI_DISPLAY;
+	}
+
+	@Override
+	protected boolean isDataPortalLevel() {
+		return false;
+	}
+
+	@Override
+	protected boolean isDataPortletInstanceLevel() {
+		return true;
+	}
+
+	@Override
+	protected boolean isDataSiteLevel() {
+		return false;
+	}
+
+}
