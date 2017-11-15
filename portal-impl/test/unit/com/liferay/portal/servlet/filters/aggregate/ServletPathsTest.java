82c34062a1ff0b2073a04783c0700a72228f45a4
gregory-bretall <gregory.bretall@liferay.com>
2017-11-15 10:53:09 -0800

    LPS-75855 Add test cases to testDown for paths with a ? and paths with ./

--- portal-impl/test/unit/com/liferay/portal/servlet/filters/aggregate/ServletPathsTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/filters/aggregate/ServletPathsTest.java
@@ -124,6 +124,15 @@ public class ServletPathsTest {
 		ServletPaths servletPaths5 = servletPaths4.down("test2");
 
 		Assert.assertEquals("test1/test2", servletPaths5.getResourcePath());
+
+		ServletPaths servletPaths6 = servletPaths1.down(
+			"/test2?extraparameters");
+
+		Assert.assertEquals("/test1/test2", servletPaths6.getResourcePath());
+
+		ServletPaths servletPaths7 = servletPaths1.down("../test2");
+
+		Assert.assertEquals("/test2", servletPaths7.getResourcePath());
 	}
 
 	@Test
