835dbd4031fd1e90fc464605fb708488f60f97f6
Preston-Crary <preston.crary@liferay.com>
2017-11-16 14:21:09 -0800

    LPS-75969 Update usages.

--- modules/apps/collaboration/document-library/document-library-layout-set-prototype/src/main/java/com/liferay/document/library/layout/set/prototype/internal/instance/lifecycle/AddLayoutSetPrototypePortalInstanceLifecycleListener.java
+++ modules/apps/collaboration/document-library/document-library-layout-set-prototype/src/main/java/com/liferay/document/library/layout/set/prototype/internal/instance/lifecycle/AddLayoutSetPrototypePortalInstanceLifecycleListener.java
@@ -30,7 +30,7 @@ import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.DefaultLayoutPrototypesUtil;
 import com.liferay.portal.kernel.util.DefaultLayoutSetPrototypesUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.rss.web.constants.RSSPortletKeys;
+import com.liferay.rss.constants.RSSPortletKeys;
 import com.liferay.site.navigation.language.constants.SiteNavigationLanguagePortletKeys;
 import com.liferay.social.activities.constants.SocialActivitiesPortletKeys;
 
--- modules/apps/web-experience/rss/rss-web/src/main/java/com/liferay/rss/web/internal/portlet/template/RSSPortletDisplayTemplateHandler.java
+++ modules/apps/web-experience/rss/rss-web/src/main/java/com/liferay/rss/web/internal/portlet/template/RSSPortletDisplayTemplateHandler.java
@@ -22,7 +22,7 @@ import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portlet.display.template.PortletDisplayTemplateConstants;
-import com.liferay.rss.web.constants.RSSPortletKeys;
+import com.liferay.rss.constants.RSSPortletKeys;
 import com.liferay.rss.web.internal.display.context.RSSDisplayContext;
 import com.liferay.rss.web.util.RSSFeed;
 
