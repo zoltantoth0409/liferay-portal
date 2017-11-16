4823f8c22280a1cc2d01305d2db719b6902d4acb
Brian Chan <brian.chan@liferay.com>
2017-11-16 10:34:51 -0800

    Revert "LPS-75929 Applies automatic formatting to flags-taglib (auto-generated)"
    This reverts commit 657eb0b5c9468be0965ac70efffd78bcc86d6b75.

--- modules/apps/collaboration/flags/flags-taglib/src/main/resources/META-INF/resources/flags/Flags.es.js
+++ modules/apps/collaboration/flags/flags-taglib/src/main/resources/META-INF/resources/flags/Flags.es.js
@@ -27,10 +27,9 @@ class Flags extends PortletBase {
 		let reason;
 
 		if (this.refs.modal.refs.otherReason) {
-			reason =
-				this.refs.modal.refs.otherReason.value ||
-				Liferay.Language.get('no-reason-specified');
-		} else {
+			reason = this.refs.modal.refs.otherReason.value || Liferay.Language.get('no-reason-specified');
+		}
+		else {
 			reason = this.refs.modal.refs.reason.value;
 		}
 
@@ -79,9 +78,7 @@ class Flags extends PortletBase {
 	 */
 	_sendReport() {
 		this.formData[this.ns('reason')] = this._getReason();
-		this.formData[
-			this.ns('reporterEmailAddress')
-		] = this.refs.modal.refs.reporterEmailAddress.value;
+		this.formData[this.ns('reporterEmailAddress')] = this.refs.modal.refs.reporterEmailAddress.value;
 
 		let formData = new FormData();
 
@@ -92,18 +89,18 @@ class Flags extends PortletBase {
 		fetch(this.uri, {
 			body: formData,
 			credentials: 'include',
-			method: 'post',
+		 	method: 'post'
 		})
-			.then(xhr => {
-				if (xhr.status === Liferay.STATUS_CODE.OK) {
-					this._showConfirmationMessage = true;
-				}
-			})
-			.catch(() => {
-				this._showErrorMessage = true;
-			});
+		.then((xhr) => {
+			if (xhr.status === Liferay.STATUS_CODE.OK) {
+				this._showConfirmationMessage = true;
+			}
+		})
+		.catch(() => {
+			this._showErrorMessage = true;
+		});
 	}
-}
+};
 
 /**
  * State definition.
@@ -119,9 +116,7 @@ Flags.STATE = {
 	 * @memberof Flags
 	 * @type {Boolean}
 	 */
-	_reportDialogOpen: Config.bool()
-		.internal()
-		.value(false),
+	_reportDialogOpen: Config.bool().internal().value(false),
 
 	/**
 	 * Flag to indicate if dialog should show the confirmation message.
@@ -130,9 +125,7 @@ Flags.STATE = {
 	 * @memberof Flags
 	 * @type {Boolean}
 	 */
-	_showConfirmationMessage: Config.bool()
-		.internal()
-		.value(false),
+	_showConfirmationMessage: Config.bool().internal().value(false),
 
 	/**
 	 * Flag to indicate if dialog should show the error message.
@@ -141,9 +134,7 @@ Flags.STATE = {
 	 * @memberof Flags
 	 * @type {Boolean}
 	 */
-	_showErrorMessage: Config.bool()
-		.internal()
-		.value(false),
+	_showErrorMessage: Config.bool().internal().value(false),
 
 	/**
 	 * Selected reason to flag.
@@ -283,7 +274,7 @@ Flags.STATE = {
 	 * @memberof Flags
 	 * @type {String}
 	 */
-	uri: Config.string().required(),
+	uri: Config.string().required()
 };
 
 // Register component
