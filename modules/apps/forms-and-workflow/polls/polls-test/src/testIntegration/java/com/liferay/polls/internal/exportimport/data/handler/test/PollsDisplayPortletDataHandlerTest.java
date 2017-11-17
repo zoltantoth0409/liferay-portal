0e77b2ec70a1cb1ce8febde3ed979dfcc0c95a17
Zoltán Császi <zoltan.csaszi@liferay.com>
2017-11-17 11:54:24 +0100

    LPS-75933 PortletDataHandlerTests - PollsDisplayPortletDataHandlerTest

---
+++ modules/apps/forms-and-workflow/polls/polls-test/src/testIntegration/java/com/liferay/polls/internal/exportimport/data/handler/test/PollsDisplayPortletDataHandlerTest.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.polls.internal.exportimport.data.handler.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.exportimport.kernel.lar.DataLevel;
+import com.liferay.polls.constants.PollsPortletKeys;
+import com.liferay.polls.model.PollsChoice;
+import com.liferay.polls.model.PollsQuestion;
+import com.liferay.polls.util.test.PollsTestUtil;
+import com.liferay.portal.lar.test.BasePortletDataHandlerTestCase;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Zsolt Berentey
+ */
+@RunWith(Arquillian.class)
+public class PollsDisplayPortletDataHandlerTest
+	extends BasePortletDataHandlerTestCase {
+
+	@Rule
+	public final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Override
+	protected void addStagedModels() throws Exception {
+		PollsQuestion question = PollsTestUtil.addQuestion(
+			stagingGroup.getGroupId());
+
+		PollsChoice choice = PollsTestUtil.addChoice(
+			stagingGroup.getGroupId(), question.getQuestionId());
+
+		PollsTestUtil.addVote(
+			stagingGroup.getGroupId(), question.getQuestionId(),
+			choice.getChoiceId());
+	}
+
+	@Override
+	protected DataLevel getDataLevel() {
+		return DataLevel.PORTLET_INSTANCE;
+	}
+
+	@Override
+	protected String getPortletId() {
+		return PollsPortletKeys.POLLS_DISPLAY;
+	}
+
+	@Override
+	protected boolean isDataPortalLevel() {
+		return false;
+	}
+
+	@Override
+	protected boolean isDataPortletInstanceLevel() {
+		return true;
+	}
+
+	@Override
+	protected boolean isDataSiteLevel() {
+		return false;
+	}
+
+}
