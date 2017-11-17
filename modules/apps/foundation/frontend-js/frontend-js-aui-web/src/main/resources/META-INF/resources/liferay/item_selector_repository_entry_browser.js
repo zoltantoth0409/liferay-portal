2e9136e86f21c3b65cb7ad8e65583a71068fb186
Renato Rego <renato.rego@liferay.com>
2017-11-17 15:00:18 -0300

    LPS-75493 Make sure an error message exists before display it.

--- modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/item_selector_repository_entry_browser.js
+++ modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/item_selector_repository_entry_browser.js
@@ -201,7 +201,9 @@ AUI.add(
 							var errorType = error.errorType;
 
 							if (errorType === STATUS_CODE.SC_FILE_ANTIVIRUS_EXCEPTION) {
-								message = error.message;
+								if (error.message) {
+									message = error.message;
+								}
 							}
 							else if (errorType === STATUS_CODE.SC_FILE_EXTENSION_EXCEPTION) {
 								message = Lang.sub(Liferay.Language.get('please-enter-a-file-with-a-valid-extension-x'), [error.message]);
