ee7a177e6e22263ca995b4d6e1ad2f36e20baa0d
pavel-savinov <pavel.savinov@liferay.com>
2017-11-17 11:24:59 +0100

    LPS-76053 JS build files

---
+++ modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/js/config.js
@@ -0,0 +1,8 @@
+Liferay.Loader.addModule(
+	{
+		dependencies: [],
+		exports: 'SiteNavigationMenuEditor',
+		name: 'SiteNavigationMenuEditor',
+		path: MODULE_PATH + '/js/SiteNavigationMenuEditor.es.js'
+	}
+);
