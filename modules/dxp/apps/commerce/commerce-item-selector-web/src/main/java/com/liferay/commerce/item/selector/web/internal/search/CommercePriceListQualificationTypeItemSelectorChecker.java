3106a9aea9374e92612ea43ed5617871b7625b0a
Alessio Rendina <asilcomando@gmail.com>
2017-11-20 12:30:50 +0100

    COMMERCE-244 Added new item selector row checker

---
+++ modules/dxp/apps/commerce/commerce-item-selector-web/src/main/java/com/liferay/commerce/item/selector/web/internal/search/CommercePriceListQualificationTypeItemSelectorChecker.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.item.selector.web.internal.search;
+
+import com.liferay.commerce.price.CommercePriceListQualificationType;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommercePriceListQualificationTypeItemSelectorChecker
+	extends EmptyOnClickRowChecker {
+
+	public CommercePriceListQualificationTypeItemSelectorChecker(
+		RenderResponse renderResponse,
+		String[] checkedCommercePriceListQualificationTypeKeys) {
+
+		super(renderResponse);
+
+		_checkedCommercePriceListQualificationTypeKeys =
+			checkedCommercePriceListQualificationTypeKeys;
+	}
+
+	@Override
+	public boolean isChecked(Object obj) {
+		CommercePriceListQualificationType commercePriceListQualificationType =
+			(CommercePriceListQualificationType)obj;
+
+		return ArrayUtil.contains(
+			_checkedCommercePriceListQualificationTypeKeys,
+			commercePriceListQualificationType.getKey());
+	}
+
+	@Override
+	public boolean isDisabled(Object obj) {
+		return isChecked(obj);
+	}
+
+	private final String[] _checkedCommercePriceListQualificationTypeKeys;
+
+}
