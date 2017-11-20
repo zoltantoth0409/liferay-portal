1f2c2bd4c28b4a5a34529fe8e6a7b4f869173bb2
Alessio Rendina <asilcomando@gmail.com>
2017-11-20 12:39:17 +0100

    COMMERCE-244 Added new commerce price list qualification type implementations

---
+++ modules/dxp/apps/commerce/commerce-price-list-qualification-type-web/src/main/java/com/liferay/commerce/price/list/qualification/type/web/internal/price/RoleCommercePriceListQualificationTypeImpl.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.price.list.qualification.type.web.internal.price;
+
+import com.liferay.commerce.price.CommercePriceListQualificationType;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"commerce.price.list.qualification.type.key=" + RoleCommercePriceListQualificationTypeImpl.KEY,
+		"commerce.price.list.qualification.type.priority:Integer=40"
+	},
+	service = CommercePriceListQualificationType.class
+)
+public class RoleCommercePriceListQualificationTypeImpl
+	implements CommercePriceListQualificationType {
+
+	public static final String KEY = "roles";
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getLabel(HttpServletRequest httpServletRequest) {
+		return LanguageUtil.get(httpServletRequest, KEY);
+	}
+
+	@Override
+	public boolean isQualified(HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		return false;
+	}
+
+}
