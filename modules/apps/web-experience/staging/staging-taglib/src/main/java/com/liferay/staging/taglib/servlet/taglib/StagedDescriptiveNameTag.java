76cc86181a4f3001daa5d0df107fded968c065eb
balazssk <balazs.safrany-kovalik@liferay.com>
2017-11-21 16:44:08 +0100

    LPS-74467 SF

--- modules/apps/web-experience/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/servlet/taglib/StagedDescriptiveNameTag.java
+++ modules/apps/web-experience/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/servlet/taglib/StagedDescriptiveNameTag.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.staging.taglib.servlet.taglib;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -12,6 +26,9 @@ import com.liferay.taglib.util.IncludeTag;
 
 import javax.servlet.jsp.JspWriter;
 
+/**
+ * @author Balázs Sáfrány-Kovalik
+ */
 public class StagedDescriptiveNameTag extends IncludeTag {
 
 	@Override
