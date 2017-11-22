fb4a6552318848da41af71e51c109f51f27218d2
Alessio Rendina <asilcomando@gmail.com>
2017-11-22 17:39:26 +0100

    COMMERCE-268 Added commerce cart validator result

---
+++ modules/dxp/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/cart/CommerceCartValidatorResult.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.cart;
+
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceCartValidatorResult {
+
+	public CommerceCartValidatorResult(boolean valid) {
+		this(0, valid, StringPool.BLANK, StringPool.BLANK);
+	}
+
+	public CommerceCartValidatorResult(boolean valid, String messageKey) {
+		this(0, valid, messageKey, StringPool.BLANK);
+	}
+
+	public CommerceCartValidatorResult(
+		boolean valid, String messageKey, String argument) {
+
+		this(0, valid, messageKey, argument);
+	}
+
+	public CommerceCartValidatorResult(
+		long commerceCartItemId, boolean valid, String messageKey) {
+
+		this(commerceCartItemId, valid, messageKey, StringPool.BLANK);
+	}
+
+	public CommerceCartValidatorResult(
+		long commerceCartItemId, boolean valid, String messageKey,
+		String argument) {
+
+		_commerceCartItemId = commerceCartItemId;
+		_valid = valid;
+		_messageKey = messageKey;
+		_argument = argument;
+	}
+
+	public String getArgument() {
+		return _argument;
+	}
+
+	public long getCommerceCartItemId() {
+		return _commerceCartItemId;
+	}
+
+	public String getMessage() {
+		return _messageKey;
+	}
+
+	public boolean hasArgument() {
+		if (Validator.isNotNull(getArgument())) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean hasMessageResult() {
+		if (Validator.isNotNull(getMessage())) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isValid() {
+		return _valid;
+	}
+
+	private String _argument;
+	private long _commerceCartItemId;
+	private String _messageKey;
+	private boolean _valid;
+
+}
