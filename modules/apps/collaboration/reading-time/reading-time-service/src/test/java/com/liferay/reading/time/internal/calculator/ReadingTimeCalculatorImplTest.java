40e1c0aa84253ff7773c369d84e29e756ac49cee
Brian Chan <brian.chan@liferay.com>
2017-11-22 11:38:12 -0800

    LPS-74943 rename

--- modules/apps/collaboration/reading-time/reading-time-service/src/test/java/com/liferay/reading/time/internal/calculator/ReadingTimeCalculatorImplTest.java
+++ modules/apps/collaboration/reading-time/reading-time-service/src/test/java/com/liferay/reading/time/internal/calculator/ReadingTimeCalculatorImplTest.java
@@ -131,7 +131,7 @@ public class ReadingTimeCalculatorImplTest {
 	}
 
 	@Test
-	public void testDoesNotAcceptNonLatinLanguages() {
+	public void testDoesNotAcceptNonlatinLanguages() {
 		ReadingTimeCalculator readingTimeCalculator =
 			new ReadingTimeCalculatorImpl();
 
