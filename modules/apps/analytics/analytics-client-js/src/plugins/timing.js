09237e15ce1e7036504d82a56035c594e930570b
Bence Varga <vbence86@gmail.com>
2017-11-23 10:51:29 +0100

    LPS-76022 Add functionality to capture when the user leaves the page

--- modules/apps/analytics/analytics-client-js/src/plugins/timing.js
+++ modules/apps/analytics/analytics-client-js/src/plugins/timing.js
@@ -7,14 +7,25 @@ function onload(analytics) {
 	analytics.send('load', 'timing', props);
 }
 
+function unload(analytics) {
+	const perfData = window.performance.timing; 
+	const viewDuration = new Date().getTime() - perfData.navigationStart;
+	const props = {
+		viewDuration
+	};
+	analytics.send('unload', 'timing', props);
+	// Flush is mandantory in this case
+	analytics.flush();
+}
+
 /**
  * Plugin function that registers listeners against browser time events
  * @param {object} analytics
  */
 function timing(analytics) {
 	window.addEventListener('load', onload.bind(null, analytics));
+	window.addEventListener('unload', unload.bind(null, analytics));
 };
 
 export { timing };
 export default timing;
-
