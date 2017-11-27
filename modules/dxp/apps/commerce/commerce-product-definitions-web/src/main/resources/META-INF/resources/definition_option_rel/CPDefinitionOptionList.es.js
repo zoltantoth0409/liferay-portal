0b252b780ab759195fe9f5af1b510fd4e049b468
Marco Leo <marco.leo@liferay.com>
2017-11-27 08:58:47 +0100

    COMMERCE-238 Add new definition option list MetalJs component

---
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/definition_option_rel/CPDefinitionOptionList.es.js
@@ -0,0 +1,55 @@
+import Component from 'metal-component';
+import { Config } from 'metal-state';
+import Dropdown from 'metal-dropdown';
+import Soy from 'metal-soy';
+import {dom, globalEval} from 'metal-dom';
+import { CancellablePromise } from 'metal-promise';
+import { async, core } from 'metal';
+
+import templates from './CPDefinitionOptionList.soy';
+
+/**
+ * CPDefinitionOptionList
+ *
+ */
+class CPDefinitionOptionList extends Component {
+
+	_handleEditValues(event) {
+		var target = event.currentTarget;
+
+		var cpOptionId = target.getAttribute('data-id');
+
+		this.emit('optionSelected', cpOptionId);
+		this.emit('editValues', cpOptionId);
+	}
+
+	_handleAddOptionClick(event) {
+		event.stopImmediatePropagation();
+		event.preventDefault();
+
+		this.emit('addOption');
+	}
+
+	_handleSelectOptionClick(event) {
+		var target = event.currentTarget;
+
+		var cpOptionId = target.getAttribute('data-id');
+
+		this.emit('optionSelected', cpOptionId);
+	}
+}
+
+/**
+ * State definition.
+ * @type {!Object}
+ * @static
+ */
+CPDefinitionOptionList.STATE = {
+	currentCPDefinitionOptionId: Config.string(),
+	cpDefinitionOptions: Config.array().value([])
+};
+
+// Register component
+Soy.register(CPDefinitionOptionList, templates);
+
+export default CPDefinitionOptionList;
