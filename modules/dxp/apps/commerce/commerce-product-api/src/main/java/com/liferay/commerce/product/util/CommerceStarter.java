bc49b04858a69ababa9773d4de86db4df34d794a
Alessio Rendina <asilcomando@gmail.com>
2017-11-27 12:22:38 +0100

    COMMERCE-218 Added commerce starter interface

---
+++ modules/dxp/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/util/CommerceStarter.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public interface CommerceStarter {
+
+	public void create(HttpServletRequest httpServletRequest) throws Exception;
+
+	public String getDescription(HttpServletRequest httpServletRequest);
+
+	public String getKey();
+
+	public String getName(HttpServletRequest httpServletRequest);
+
+	public String getPreviewImageSrc(HttpServletRequest httpServletRequest);
+
+	public boolean hasSampleData(HttpServletRequest httpServletRequest);
+
+	public boolean isActive(HttpServletRequest httpServletRequest);
+
+	public void renderPreview(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception;
+
+}
