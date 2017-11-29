06283dd26e619cc7de5978a01b7ab728f7d65ac7
Andrea Di Giorgi <adigiorgi@outlook.com>
2017-11-29 15:34:19 -0800

    COMMERCE-218 Remove generated comments

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPGroupImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPGroupImpl.java
@@ -17,22 +17,11 @@ package com.liferay.commerce.product.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the CPGroup service. Represents a row in the &quot;CPGroup&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.commerce.product.model.CPGroup} interface.
- * </p>
- *
  * @author Marco Leo
  */
 @ProviderType
 public class CPGroupImpl extends CPGroupBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a cp group model instance should use the {@link com.liferay.commerce.product.model.CPGroup} interface instead.
-	 */
 	public CPGroupImpl() {
 	}
 
