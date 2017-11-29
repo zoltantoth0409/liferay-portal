22bc5dcb682c449e3bda4b837f71086dde320d13
Michael Hashimoto <michael.hashimoto@liferay.com>
2017-11-28 18:08:45 -0800

    LRQA-36285 Move logic to find database names & data archive types into data archive portal version objects

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/LegacyDataArchivePortalVersion.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/LegacyDataArchivePortalVersion.java
@@ -14,6 +14,21 @@
 
 package com.liferay.jenkins.results.parser;
 
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+
 /**
  * @author Michael Hashimoto
  */
@@ -24,9 +39,112 @@ public class LegacyDataArchivePortalVersion {
 
 		_legacyDataArchiveUtil = legacyDataArchiveUtil;
 		_portalVersion = portalVersion;
+
+		_legacyGitWorkingDirectory =
+			_legacyDataArchiveUtil.getLegacyGitWorkingDirectory();
+
+		_portalVersionDirectory = new File(
+			_legacyGitWorkingDirectory.getWorkingDirectory(), _portalVersion);
+
+		_portalVersionTestDirectory = new File(_portalVersionDirectory, "test");
+
+		_dataArchiveTypes = _getDataArchiveTypes();
+		_databaseNames = _getDatabaseNames();
 	}
 
+	private List<String> _getDataArchiveTypes() {
+		Set<String> dataArchiveTypeSet = new HashSet<>();
+
+		try {
+			List<File> testcaseFiles = JenkinsResultsParserUtil.findFiles(
+				_portalVersionTestDirectory, ".*\\.testcase");
+
+			for (File testcaseFile : testcaseFiles) {
+				Document document = Dom4JUtil.parse(
+					JenkinsResultsParserUtil.read(testcaseFile));
+
+				Element rootElement = document.getRootElement();
+
+				dataArchiveTypeSet.addAll(
+					_getPoshiPropertyValues(rootElement, "data.archive.type"));
+			}
+		}
+		catch (DocumentException | IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		List<String> dataArchiveTypes = new ArrayList<>(dataArchiveTypeSet);
+
+		Collections.sort(dataArchiveTypes);
+
+		return dataArchiveTypes;
+	}
+
+	private List<String> _getDatabaseNames() {
+		Properties buildProperties =
+			_legacyDataArchiveUtil.getBuildProperties();
+
+		String legacyDataArchiveDatabaseNames = buildProperties.getProperty(
+			"legacy.data.archive.database.names");
+
+		String databaseNamesPortalVersionKey = JenkinsResultsParserUtil.combine(
+			"legacy.data.archive.database.names[", _portalVersion, "]");
+
+		if (buildProperties.containsKey(databaseNamesPortalVersionKey)) {
+			legacyDataArchiveDatabaseNames = buildProperties.getProperty(
+				databaseNamesPortalVersionKey);
+		}
+
+		List<String> databaseNames = Arrays.asList(
+			legacyDataArchiveDatabaseNames.split(","));
+
+		Collections.sort(databaseNames);
+
+		return databaseNames;
+	}
+
+	private Set<String> _getPoshiPropertyValues(
+		Element element, String targetPoshiPropertyName) {
+
+		Set<String> poshiPropertyValues = new HashSet<>();
+
+		List<Element> childElements = new ArrayList<>();
+
+		for (Object elementObject : element.elements()) {
+			if (elementObject instanceof Element) {
+				childElements.add((Element)elementObject);
+			}
+		}
+
+		if (childElements.isEmpty()) {
+			return poshiPropertyValues;
+		}
+
+		for (Element childElement : childElements) {
+			String childElementName = childElement.getName();
+
+			if (childElementName.equals("property")) {
+				String poshiPropertyName = childElement.attributeValue("name");
+
+				if (poshiPropertyName.equals(targetPoshiPropertyName)) {
+					poshiPropertyValues.add(
+						childElement.attributeValue("value"));
+				}
+			}
+
+			poshiPropertyValues.addAll(
+				_getPoshiPropertyValues(childElement, targetPoshiPropertyName));
+		}
+
+		return poshiPropertyValues;
+	}
+
+	private final List<String> _dataArchiveTypes;
+	private final List<String> _databaseNames;
 	private final LegacyDataArchiveUtil _legacyDataArchiveUtil;
+	private final GitWorkingDirectory _legacyGitWorkingDirectory;
 	private final String _portalVersion;
+	private final File _portalVersionDirectory;
+	private final File _portalVersionTestDirectory;
 
 }
