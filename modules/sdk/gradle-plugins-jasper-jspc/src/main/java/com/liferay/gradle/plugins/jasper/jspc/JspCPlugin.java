ad3cbcf7370124c0b352f0029cbf5a1f2d964378
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2017-11-29 13:34:26 -0800

    LPS-76202 Defer temporary dir evaluation

--- modules/sdk/gradle-plugins-jasper-jspc/src/main/java/com/liferay/gradle/plugins/jasper/jspc/JspCPlugin.java
+++ modules/sdk/gradle-plugins-jasper-jspc/src/main/java/com/liferay/gradle/plugins/jasper/jspc/JspCPlugin.java
@@ -63,7 +63,7 @@ public class JspCPlugin implements Plugin<Project> {
 		final CompileJSPTask generateJSPJavaTask = _addTaskGenerateJSPJava(
 			project, jspCConfiguration, jspCToolConfiguration);
 
-		_addTaskCompileJSP(
+		final JavaCompile compileJSPTask = _addTaskCompileJSP(
 			generateJSPJavaTask, jspCConfiguration, jspCToolConfiguration);
 
 		project.afterEvaluate(
@@ -72,6 +72,7 @@ public class JspCPlugin implements Plugin<Project> {
 				@Override
 				public void execute(Project project) {
 					_addDependenciesJspC(project);
+					_configureTaskCompileJSP(compileJSPTask);
 				}
 
 			});
@@ -147,7 +148,6 @@ public class JspCPlugin implements Plugin<Project> {
 
 		javaCompile.setClasspath(jspCToolConfiguration.plus(jspCConfiguration));
 		javaCompile.setDescription("Compile JSP files to check for errors.");
-		javaCompile.setDestinationDir(javaCompile.getTemporaryDir());
 		javaCompile.setGroup(JavaBasePlugin.VERIFICATION_GROUP);
 		javaCompile.setSource(generateJSPJavaTask.getOutputs());
 
@@ -207,6 +207,12 @@ public class JspCPlugin implements Plugin<Project> {
 			});
 
 		return compileJSPTask;
+	}
+
+	private void _configureTaskCompileJSP(JavaCompile compileJSPTask) {
+		if (compileJSPTask.getDestinationDir() == null) {
+			compileJSPTask.setDestinationDir(compileJSPTask.getTemporaryDir());
+		}
 	}
 
 	private void _configureTaskGenerateJSPJavaForWarPlugin(
