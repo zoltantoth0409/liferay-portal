608e77c90e7d0e286011fc46569ec4950fc12155
Adolfo Pérez <adolfo.perez@liferay.com>
2017-11-29 14:16:22 +0100

    LPS-76108 Extract logic into util class

--- modules/apps/adaptive-media/adaptive-media-document-library-thumbnails/src/main/java/com/liferay/adaptive/media/document/library/thumbnails/internal/upgrade/v1_0_0/UpgradeDocumentLibraryThumbnailsConfiguration.java
+++ modules/apps/adaptive-media/adaptive-media-document-library-thumbnails/src/main/java/com/liferay/adaptive/media/document/library/thumbnails/internal/upgrade/v1_0_0/UpgradeDocumentLibraryThumbnailsConfiguration.java
@@ -14,20 +14,11 @@
 
 package com.liferay.adaptive.media.document.library.thumbnails.internal.upgrade.v1_0_0;
 
-import com.liferay.adaptive.media.exception.AMImageConfigurationException;
+import com.liferay.adaptive.media.document.library.thumbnails.internal.util.AMCompanyThumbnailConfigurationInitializer;
 import com.liferay.adaptive.media.image.configuration.AMImageConfigurationHelper;
-import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.util.PrefsPropsUtil;
-
-import java.io.IOException;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author Roberto Díaz
@@ -47,68 +38,12 @@ public class UpgradeDocumentLibraryThumbnailsConfiguration
 	@Override
 	protected void doUpgrade() throws Exception {
 		try (LoggingTimer loggingTimer = new LoggingTimer()) {
-			int dlFileEntryThumbnailMaxHeight = PrefsPropsUtil.getInteger(
-				PropsKeys.DL_FILE_ENTRY_THUMBNAIL_MAX_HEIGHT);
-			int dlFileEntryThumbnailMaxWidth = PrefsPropsUtil.getInteger(
-				PropsKeys.DL_FILE_ENTRY_THUMBNAIL_MAX_WIDTH);
+			AMCompanyThumbnailConfigurationInitializer
+				amCompanyThumbnailConfigurationInitializer =
+					new AMCompanyThumbnailConfigurationInitializer(
+						_amImageConfigurationHelper, _companyLocalService);
 
-			if ((dlFileEntryThumbnailMaxHeight > 0) &&
-				(dlFileEntryThumbnailMaxWidth > 0)) {
-
-				_createAMDocumentLibraryThumbnailConfiguration(
-					dlFileEntryThumbnailMaxHeight,
-					dlFileEntryThumbnailMaxWidth);
-			}
-
-			int dlFileEntryThumbnailCustom1MaxHeight =
-				PrefsPropsUtil.getInteger(
-					PropsKeys.DL_FILE_ENTRY_THUMBNAIL_CUSTOM_1_MAX_HEIGHT);
-			int dlFileEntryThumbnailCustom1MaxWidth = PrefsPropsUtil.getInteger(
-				PropsKeys.DL_FILE_ENTRY_THUMBNAIL_CUSTOM_1_MAX_WIDTH);
-
-			if ((dlFileEntryThumbnailCustom1MaxHeight > 0) &&
-				(dlFileEntryThumbnailCustom1MaxWidth > 0)) {
-
-				_createAMDocumentLibraryThumbnailConfiguration(
-					dlFileEntryThumbnailCustom1MaxHeight,
-					dlFileEntryThumbnailCustom1MaxWidth);
-			}
-
-			int dlFileEntryThumbnailCustom2MaxHeight =
-				PrefsPropsUtil.getInteger(
-					PropsKeys.DL_FILE_ENTRY_THUMBNAIL_CUSTOM_2_MAX_HEIGHT);
-			int dlFileEntryThumbnailCustom2MaxWidth = PrefsPropsUtil.getInteger(
-				PropsKeys.DL_FILE_ENTRY_THUMBNAIL_CUSTOM_2_MAX_WIDTH);
-
-			if ((dlFileEntryThumbnailCustom2MaxHeight > 0) &&
-				(dlFileEntryThumbnailCustom2MaxWidth > 0)) {
-
-				_createAMDocumentLibraryThumbnailConfiguration(
-					dlFileEntryThumbnailCustom2MaxHeight,
-					dlFileEntryThumbnailCustom2MaxWidth);
-			}
-		}
-	}
-
-	private void _createAMDocumentLibraryThumbnailConfiguration(
-			int maxHeight, int maxWidth)
-		throws AMImageConfigurationException, IOException {
-
-		String name = String.format(
-			"%s %dx%d", "Thumbnail", maxWidth, maxHeight);
-
-		Map<String, String> properties = new HashMap<>();
-
-		properties.put("max-height", String.valueOf(maxHeight));
-		properties.put("max-width", String.valueOf(maxWidth));
-
-		List<Company> companies = _companyLocalService.getCompanies();
-
-		for (Company company : companies) {
-			_amImageConfigurationHelper.addAMImageConfigurationEntry(
-				company.getCompanyId(), name,
-				"This image resolution was automatically added.", name,
-				properties);
+			amCompanyThumbnailConfigurationInitializer.initializeCompany();
 		}
 	}
 
