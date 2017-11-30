50de850579eaa2c64a150ddd50788fbfaf1ecbca
Alessio Rendina <asilcomando@gmail.com>
2017-11-30 12:15:03 +0100

    COMMERCE-218 Added theme portlet settings

--- modules/dxp/apps/commerce/commerce-starter-lotus/src/main/java/com/liferay/commerce/starter/lotus/internal/util/LotusCommerceStarterImpl.java
+++ modules/dxp/apps/commerce/commerce-starter-lotus/src/main/java/com/liferay/commerce/starter/lotus/internal/util/LotusCommerceStarterImpl.java
@@ -14,6 +14,8 @@
 
 package com.liferay.commerce.starter.lotus.internal.util;
 
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
 import com.liferay.commerce.product.demo.data.creator.CPDemoDataCreator;
 import com.liferay.commerce.product.importer.CPFileImporter;
 import com.liferay.commerce.product.service.CPGroupLocalService;
@@ -23,18 +25,30 @@ import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.Theme;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ThemeLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Iterator;
+
+import javax.portlet.PortletPreferences;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -57,12 +71,20 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class LotusCommerceStarterImpl implements CommerceStarter {
 
+	public static final String CP_ASSET_CATEGORIES_NAVIGATION_PORTLET_NAME =
+		"com_liferay_commerce_product_asset_categories_navigation_web_" +
+			"internal_portlet_CPAssetCategoriesNavigationPortlet";
+
 	public static final String DEPENDECY_PATH =
 		"com/liferay/commerce/starter/lotus/internal/dependencies/";
 
 	public static final String KEY = "lotus";
 
 	public static final String LOTUS_THEME_ID = "lotus_WAR_commercethemelotus";
+
+	public static final String SITE_NAVIGATION_MENU_PORTLET_NAME =
+		"com_liferay_site_navigation_menu_web_portlet_" +
+			"SiteNavigationMenuPortlet";
 
 	@Override
 	public void create(HttpServletRequest httpServletRequest) throws Exception {
@@ -82,20 +104,17 @@ public class LotusCommerceStarterImpl implements CommerceStarter {
 
 		_cpGroupLocalService.addCPGroup(serviceContext);
 
-		createSampleData(httpServletRequest, serviceContext);
+		createSampleData(serviceContext);
+
+		setThemePortletSettings(serviceContext);
 	}
 
-	public void createSampleData(
-			HttpServletRequest httpServletRequest,
-			ServiceContext serviceContext)
+	public void createSampleData(ServiceContext serviceContext)
 		throws Exception {
 
-		if (hasSampleData(httpServletRequest)) {
-			_cpDemoDataCreator.init();
-			_cpDemoDataCreator.create(
-				serviceContext.getUserId(), serviceContext.getScopeGroupId(),
-				true);
-		}
+		_cpDemoDataCreator.init();
+		_cpDemoDataCreator.create(
+			serviceContext.getUserId(), serviceContext.getScopeGroupId(), true);
 	}
 
 	@Override
@@ -131,11 +150,6 @@ public class LotusCommerceStarterImpl implements CommerceStarter {
 		serviceContext.setUserId(user.getUserId());
 
 		return serviceContext;
-	}
-
-	@Override
-	public boolean hasSampleData(HttpServletRequest httpServletRequest) {
-		return ParamUtil.getBoolean(httpServletRequest, "sampleData");
 	}
 
 	@Override
@@ -205,11 +219,155 @@ public class LotusCommerceStarterImpl implements CommerceStarter {
 		JSONArray jsonArray = JSONFactoryUtil.createJSONArray(layoutsJSON);
 
 		_cpFileImporter.createLayouts(
-			jsonArray, clazz.getClassLoader(), DEPENDECY_PATH, serviceContext);
+			jsonArray, null, clazz.getClassLoader(), DEPENDECY_PATH,
+			serviceContext);
+	}
+
+	protected JSONArray getThemePortletSettingJSONArray() throws Exception {
+		Class<?> clazz = getClass();
+
+		String themePortletSettingsPath =
+			DEPENDECY_PATH + "theme-portlet-settings.json";
+
+		String themePortletSettingsJSON = StringUtil.read(
+			clazz.getClassLoader(), themePortletSettingsPath, false);
+
+		return JSONFactoryUtil.createJSONArray(themePortletSettingsJSON);
+	}
+
+	protected void setCPAssetCategoriesNavigationPortletSettings(
+			JSONObject jsonObject, String portletName,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		if (portletName.equals(CP_ASSET_CATEGORIES_NAVIGATION_PORTLET_NAME)) {
+			String instanceId = jsonObject.getString("instanceId");
+			String vocabularyName = jsonObject.getString("vocabularyName");
+
+			JSONObject portletPreferencesJSONObject = jsonObject.getJSONObject(
+				"portletPreferences");
+
+			String portletId = PortletIdCodec.encode(portletName, instanceId);
+
+			PortletPreferences portletSetup =
+				PortletPreferencesFactoryUtil.getLayoutPortletSetup(
+					serviceContext.getCompanyId(),
+					serviceContext.getScopeGroupId(),
+					PortletKeys.PREFS_OWNER_TYPE_LAYOUT,
+					LayoutConstants.DEFAULT_PLID, portletId, StringPool.BLANK);
+
+			Iterator<String> iterator = portletPreferencesJSONObject.keys();
+
+			while (iterator.hasNext()) {
+				String key = iterator.next();
+
+				String value = portletPreferencesJSONObject.getString(key);
+
+				if (key.equals("assetVocabularyId")) {
+					AssetVocabulary assetVocabulary = null;
+
+					if (Validator.isNotNull(vocabularyName)) {
+						assetVocabulary =
+							_assetVocabularyLocalService.fetchGroupVocabulary(
+								serviceContext.getScopeGroupId(),
+								vocabularyName);
+					}
+
+					if (assetVocabulary == null) {
+						value = StringPool.BLANK;
+					}
+					else {
+						value = String.valueOf(
+							assetVocabulary.getVocabularyId());
+					}
+				}
+
+				portletSetup.setValue(key, value);
+			}
+
+			portletSetup.store();
+		}
+	}
+
+	protected void setSiteNavigationMenuPortletSettings(
+			JSONObject jsonObject, String portletName,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		if (portletName.equals(SITE_NAVIGATION_MENU_PORTLET_NAME)) {
+			String instanceId = jsonObject.getString("instanceId");
+			String rootLayoutFriendlyURL = jsonObject.getString(
+				"rootLayoutFriendlyURL");
+
+			JSONObject portletPreferencesJSONObject = jsonObject.getJSONObject(
+				"portletPreferences");
+
+			Layout layout = null;
+
+			if (Validator.isNotNull(rootLayoutFriendlyURL)) {
+				layout = _layoutLocalService.fetchLayoutByFriendlyURL(
+					serviceContext.getScopeGroupId(), false,
+					rootLayoutFriendlyURL);
+			}
+
+			String portletId = PortletIdCodec.encode(portletName, instanceId);
+
+			PortletPreferences portletSetup =
+				PortletPreferencesFactoryUtil.getLayoutPortletSetup(
+					serviceContext.getCompanyId(),
+					serviceContext.getScopeGroupId(),
+					PortletKeys.PREFS_OWNER_TYPE_LAYOUT,
+					LayoutConstants.DEFAULT_PLID, portletId, StringPool.BLANK);
+
+			Iterator<String> iterator = portletPreferencesJSONObject.keys();
+
+			while (iterator.hasNext()) {
+				String key = iterator.next();
+
+				String value = portletPreferencesJSONObject.getString(key);
+
+				if (key.equals("displayStyleGroupId")) {
+					value = String.valueOf(serviceContext.getScopeGroupId());
+				}
+				else if (key.equals("rootLayoutUuid")) {
+					if (layout == null) {
+						value = StringPool.BLANK;
+					}
+					else {
+						value = layout.getUuid();
+					}
+				}
+
+				portletSetup.setValue(key, value);
+			}
+
+			portletSetup.store();
+		}
+	}
+
+	protected void setThemePortletSettings(ServiceContext serviceContext)
+		throws Exception {
+
+		JSONArray jsonArray = getThemePortletSettingJSONArray();
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			String portletName = jsonObject.getString("portletName");
+
+			setCPAssetCategoriesNavigationPortletSettings(
+				jsonObject, portletName, serviceContext);
+
+			setSiteNavigationMenuPortletSettings(
+				jsonObject, portletName, serviceContext);
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		LotusCommerceStarterImpl.class);
+
+	@Reference
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
 
 	@Reference
 	private CommerceStarterRegistry _commerceStarterRegistry;
@@ -227,7 +385,13 @@ public class LotusCommerceStarterImpl implements CommerceStarter {
 	private JSPRenderer _jspRenderer;
 
 	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
 	private Portal _portal;
+
+	@Reference
+	private PortletPreferencesLocalService _portletPreferencesLocalService;
 
 	@Reference(
 		target = "(osgi.web.symbolicname=com.liferay.commerce.starter.lotus)"
