2f8ac4f6eadd8450fb39052109e7cd858825c716
Hugo Huijser <hugo.huijser@liferay.com>
2017-11-30 14:53:27 +0800

    LPS-75798 Enable checks for subrepos (reverted from commit 086d96375dfb46faa9de3922a697fc0f678e7167)

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaFinderImplCustomSQLCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaFinderImplCustomSQLCheck.java
@@ -46,6 +46,10 @@ public class JavaFinderImplCustomSQLCheck extends BaseFileCheck {
 			String fileName, String absolutePath, String content)
 		throws Exception {
 
+		if (isSubrepository() || isReadOnly(absolutePath)) {
+			return content;
+		}
+
 		if (fileName.endsWith("FinderImpl.java")) {
 			_checkCustomSQL(fileName, absolutePath, content);
 		}
