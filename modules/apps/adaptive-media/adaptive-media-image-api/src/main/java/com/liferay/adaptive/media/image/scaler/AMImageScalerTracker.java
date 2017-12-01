5811a87cc0fe751fea241974553e2efe2f7af321
Nick Gaskill <nicholas.gaskill@liferay.com>
2017-12-01 13:52:52 -0500

    LRDOCS-4102 Javadoc review

--- modules/apps/adaptive-media/adaptive-media-image-api/src/main/java/com/liferay/adaptive/media/image/scaler/AMImageScalerTracker.java
+++ modules/apps/adaptive-media/adaptive-media-image-api/src/main/java/com/liferay/adaptive/media/image/scaler/AMImageScalerTracker.java
@@ -15,48 +15,38 @@
 package com.liferay.adaptive.media.image.scaler;
 
 /**
- * This interface is responsible for tracking the image scalers and return the
- * one that will be used for a specific mime type.
+ * Tracks the image scalers and returns the one used for a specific MIME type.
  *
  * <p>
- * Every image scaler registered in OSGi with the property
- * <code>mime.type</code> is tracked. Multiple image scalers can be registered
- * for the same mime type, but only one will be returned by the tracker.
+ * Every image scaler registered in OSGi with the <code>mime.type</code>
+ * property is tracked. Multiple image scalers can be registered for the same
+ * MIME type, but the tracker returns only one.
  * </p>
  *
  * <p>
- * The image scalers are grouped by the mime.type and they are ordered based on
- * the <code>service.ranking</code> property. The image scaler will return the
- * first image scaler of the group whose method
- * {@link AMImageScaler#isEnabled()} returns true.
+ * The image scalers are grouped by <code>mime.type</code> and ordered by the
+ * <code>service.ranking</code> property. The image scaler returns the first 
+ * image scaler of the group whose {@link AMImageScaler#isEnabled()} method
+ * returns true.
  * </p>
  *
  * <p>
- * If there is no enabled image scaler or there are not image scalers registered
- * for the specific mime type, it will search for the image scaler with higher
- * service ranking that is registered using <code>*</code> as mime type and
- * whose method {@link AMImageScaler#isEnabled()} returns true.
+ * If no enabled image scalers are registered for the specific MIME type, it
+ * searches for the enabled image scaler with the highest 
+ * <code>service.ranking</code> that's registered with the <code>*</code> MIME 
+ * type. If no enabled image scalers are registered for the <code>*</code> MIME
+ * type, it returns <code>null</code>.
  * </p>
- *
- * <p>
- * If there are not enabled image scalers registered for the <code>*</code> mime
- * type it will return <code>null</code>.
- * </p>
- *
- * @review
  *
  * @author Sergio González
  */
 public interface AMImageScalerTracker {
 
 	/**
-	 * Returns the image scaler enabled for the specific mime type.
+	 * Returns the enabled image scaler for the specific MIME type.
 	 *
-	 * @param  mimeType the mime type of the image that needs to handle the
-	 *         image scaler
+	 * @param  mimeType the MIME type
 	 * @return the image scaler
-	 *
-	 * @review
 	 */
 	public AMImageScaler getAMImageScaler(String mimeType);
 
