c4a5a4304ff4b3173679a6e669d49c41c55ae01e
pavel-savinov <pavel.savinov@liferay.com>
2017-12-01 12:11:22 +0100

    LPS-76165 HTML preview processor interface and processors tracker

---
+++ modules/apps/html-preview/html-preview-api/src/main/java/com/liferay/html/preview/processor/HtmlPreviewProcessorTracker.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.html.preview.processor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(immediate = true, service = HtmlPreviewProcessorTracker.class)
+public class HtmlPreviewProcessorTracker {
+
+	public static List<HtmlPreviewProcessor> getHtmlPreviewProcessors() {
+		return new ArrayList<>(_htmlPreviewProcessors.values());
+	}
+
+	public HtmlPreviewProcessor getHtmlPreviewProcessor(String mimeType) {
+		return _htmlPreviewProcessors.get(mimeType);
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC, unbind = "unsetHtmlPreviewProcessor"
+	)
+	protected void setHtmlPreviewProcessor(
+		HtmlPreviewProcessor htmlPreviewProcessor) {
+
+		_htmlPreviewProcessors.put(
+			htmlPreviewProcessor.getMimeType(), htmlPreviewProcessor);
+	}
+
+	protected void unsetHtmlPreviewProcessor(
+		HtmlPreviewProcessor htmlPreviewProcessor) {
+
+		_htmlPreviewProcessors.remove(htmlPreviewProcessor.getMimeType());
+	}
+
+	private static final Map<String, HtmlPreviewProcessor>
+		_htmlPreviewProcessors = new ConcurrentHashMap<>();
+
+}
