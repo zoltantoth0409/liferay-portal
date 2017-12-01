922bd1174bd95ac150b305a07aba8ee11c91c985
pavel-savinov <pavel.savinov@liferay.com>
2017-12-01 12:14:08 +0100

    LPS-76165 HTML preview service implementation

--- modules/apps/html-preview/html-preview-service/src/main/java/com/liferay/html/preview/model/impl/HtmlPreviewImpl.java
+++ modules/apps/html-preview/html-preview-service/src/main/java/com/liferay/html/preview/model/impl/HtmlPreviewImpl.java
@@ -17,21 +17,8 @@ package com.liferay.html.preview.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the HtmlPreview service. Represents a row in the &quot;HtmlPreview&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.html.preview.model.HtmlPreview} interface.
- * </p>
- *
- * @author Brian Wing Shun Chan
+ * @author Pavel Savinov
  */
 @ProviderType
 public class HtmlPreviewImpl extends HtmlPreviewBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a html preview model instance should use the {@link com.liferay.html.preview.model.HtmlPreview} interface instead.
-	 */
-	public HtmlPreviewImpl() {
-	}
 }
--- modules/apps/html-preview/html-preview-service/src/main/java/com/liferay/html/preview/service/impl/HtmlPreviewLocalServiceImpl.java
+++ modules/apps/html-preview/html-preview-service/src/main/java/com/liferay/html/preview/service/impl/HtmlPreviewLocalServiceImpl.java
@@ -14,26 +14,101 @@
 
 package com.liferay.html.preview.service.impl;
 
+import com.liferay.html.preview.constants.HtmlPreviewConstants;
+import com.liferay.html.preview.exception.InvalidMimeTypeException;
+import com.liferay.html.preview.model.HtmlPreview;
+import com.liferay.html.preview.processor.HtmlPreviewProcessor;
+import com.liferay.html.preview.processor.HtmlPreviewProcessorTracker;
 import com.liferay.html.preview.service.base.HtmlPreviewLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * The implementation of the html preview local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.html.preview.service.HtmlPreviewLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see HtmlPreviewLocalServiceBaseImpl
- * @see com.liferay.html.preview.service.HtmlPreviewLocalServiceUtil
+ * @author Pavel Savinov
  */
-public class HtmlPreviewLocalServiceImpl extends HtmlPreviewLocalServiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.html.preview.service.HtmlPreviewLocalServiceUtil} to access the html preview local service.
-	 */
+public class HtmlPreviewLocalServiceImpl
+	extends HtmlPreviewLocalServiceBaseImpl {
+
+	@Override
+	public HtmlPreview generateHtmlPreview(
+			long userId, long groupId, long classNameId, long classPK,
+			String content, String mimeType, boolean asynchronous,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		HtmlPreview htmlPreview = htmlPreviewPersistence.fetchByG_C_C(
+			groupId, classNameId, classPK);
+
+		if (htmlPreview == null) {
+			User user = userLocalService.getUser(userId);
+
+			long htmlPreviewId = counterLocalService.increment();
+
+			htmlPreview = htmlPreviewPersistence.create(htmlPreviewId);
+
+			htmlPreview.setGroupId(groupId);
+			htmlPreview.setCompanyId(user.getCompanyId());
+			htmlPreview.setUserId(user.getUserId());
+			htmlPreview.setUserName(user.getFullName());
+			htmlPreview.setCreateDate(serviceContext.getCreateDate(new Date()));
+			htmlPreview.setModifiedDate(
+				serviceContext.getModifiedDate(new Date()));
+
+			htmlPreview.setClassNameId(classNameId);
+			htmlPreview.setClassPK(classPK);
+
+			htmlPreviewPersistence.update(htmlPreview);
+		}
+
+		if (asynchronous) {
+			Message message = new Message();
+
+			Map<String, Object> payload = new HashMap<>();
+
+			payload.put("classNameId", classNameId);
+			payload.put("classPK", classPK);
+			payload.put("content", content);
+			payload.put("groupId", groupId);
+			payload.put("mimeType", mimeType);
+			payload.put("userId", userId);
+
+			message.setPayload(payload);
+
+			MessageBusUtil.sendMessage(
+				HtmlPreviewConstants.HTML_PREVIEW_GENERATOR, message);
+
+			return htmlPreview;
+		}
+
+		HtmlPreviewProcessor htmlPreviewProcessor =
+			_htmlPreviewProcessorTracker.getHtmlPreviewProcessor(mimeType);
+
+		if (htmlPreviewProcessor == null) {
+			throw new InvalidMimeTypeException(
+				"No HTML preview processor available for MIME type " +
+					mimeType);
+		}
+
+		FileEntry fileEntry = htmlPreviewProcessor.generateHtmlPreviewFileEntry(
+			userId, groupId, content);
+
+		htmlPreview.setFileEntryId(fileEntry.getFileEntryId());
+
+		htmlPreviewPersistence.update(htmlPreview);
+
+		return htmlPreview;
+	}
+
+	@ServiceReference(type = HtmlPreviewProcessorTracker.class)
+	private HtmlPreviewProcessorTracker _htmlPreviewProcessorTracker;
+
 }
