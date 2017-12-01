a7380856b0e733289ec566f4c7bbec151d33ab38
Bryan Engler <bryan.engler@liferay.com>
2017-12-01 11:59:22 -0800

    LPS-76324 portal-search-elasticsearch: Add test: modified date field mapping
    https://issues.liferay.com/browse/LPS-76324

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/index/LiferayIndexFixture.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/index/LiferayIndexFixture.java
@@ -20,6 +20,9 @@ import com.liferay.portal.search.elasticsearch.internal.connection.IndexCreator;
 import com.liferay.portal.search.elasticsearch.internal.connection.IndexName;
 import com.liferay.portal.search.elasticsearch.internal.connection.LiferayIndexCreator;
 
+import java.util.Map;
+
+import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.client.Client;
 
 /**
@@ -54,11 +57,39 @@ public class LiferayIndexFixture {
 		_elasticsearchFixture.tearDown();
 	}
 
+	public void assertAnalyzer(String field, String analyzer) throws Exception {
+		FieldMappingAssert.assertAnalyzer(
+			analyzer, field, LiferayTypeMappingsConstants.LIFERAY_DOCUMENT_TYPE,
+			_index.getName(), _elasticsearchFixture.getIndicesAdminClient());
+	}
+
+	public void assertType(String field, String type) throws Exception {
+		FieldMappingAssert.assertType(
+			type, field, LiferayTypeMappingsConstants.LIFERAY_DOCUMENT_TYPE,
+			_index.getName(), _elasticsearchFixture.getIndicesAdminClient());
+	}
+
+	protected IndexRequestBuilder getIndexRequestBuilder() {
+		Client client = _elasticsearchFixture.getClient();
+
+		return client.prepareIndex(
+			_index.getName(),
+			LiferayTypeMappingsConstants.LIFERAY_DOCUMENT_TYPE);
+	}
+
 	protected Index createIndex() {
 		IndexCreator indexCreator = new LiferayIndexCreator(
 			_elasticsearchFixture);
 
 		return indexCreator.createIndex(_indexName);
+	}
+
+	protected void index(Map<String, Object> map) {
+		IndexRequestBuilder indexRequestBuilder = getIndexRequestBuilder();
+
+		indexRequestBuilder.setSource(map);
+
+		indexRequestBuilder.get();
 	}
 
 	private final ElasticsearchFixture _elasticsearchFixture;
