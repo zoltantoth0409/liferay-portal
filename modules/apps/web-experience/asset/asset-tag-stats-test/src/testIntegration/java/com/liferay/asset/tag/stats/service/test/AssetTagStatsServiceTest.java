85cdff8f5a1255e8087e285c5c1382c88e14e323
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-12-02 10:29:24 +0100

    LPS-76252 Moves asset tag stats test

--- modules/apps/web-experience/asset/asset-tag-stats-test/src/testIntegration/java/com/liferay/asset/tag/stats/service/test/AssetTagStatsServiceTest.java
+++ modules/apps/web-experience/asset/asset-tag-stats-test/src/testIntegration/java/com/liferay/asset/tag/stats/service/test/AssetTagStatsServiceTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.asset.service.test;
+package com.liferay.asset.tag.stats.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetTag;
