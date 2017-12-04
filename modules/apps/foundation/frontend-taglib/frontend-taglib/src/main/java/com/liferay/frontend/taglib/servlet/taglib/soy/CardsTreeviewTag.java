452e589a96b6f3f907091d4f4ed23768b79eedd4
Chema Balsas <jose.balsas@liferay.com>
2017-12-04 12:00:23 +0100

    LPS-74297 Creates JSP wrapper tag

---
+++ modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/soy/CardsTreeviewTag.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib.soy;
+
+import com.liferay.frontend.taglib.soy.servlet.taglib.TemplateRendererTag;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+/**
+ * @author Chema Balsas
+ */
+public class CardsTreeviewTag extends TemplateRendererTag {
+
+	@Override
+	public int doStartTag() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		putValue("pathThemeImages", themeDisplay.getPathThemeImages());
+
+		setTemplateNamespace("liferay.frontend.CardsTreeview.render");
+
+		return super.doStartTag();
+	}
+
+	@Override
+	public String getModule() {
+		return "frontend-taglib/cards_treeview/CardsTreeview.es";
+	}
+
+	public void setNodes(Object nodes) {
+		putValue("nodes", nodes);
+	}
+
+	public void setViewType(String viewType) {
+		putValue("viewType", viewType);
+	}
+
+}
