a5cd85f58668ea243389b22dab13fb47e2d99641
wade.cao <wade.cao@liferay.com>
2017-12-05 08:18:16 -0800

    LPS-76324 portal-search-elasticsearch: aggregate on keyword fields only
    https://issues.liferay.com/browse/LPS-76324

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/groupby/GroupByTest.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/groupby/GroupByTest.java
@@ -16,6 +16,8 @@ package com.liferay.portal.search.elasticsearch.internal.groupby;
 
 import com.liferay.portal.search.elasticsearch.internal.ElasticsearchIndexingFixture;
 import com.liferay.portal.search.elasticsearch.internal.connection.ElasticsearchFixture;
+import com.liferay.portal.search.elasticsearch.internal.connection.LiferayIndexCreator;
+import com.liferay.portal.search.elasticsearch.internal.count.ElasticsearchCountTest;
 import com.liferay.portal.search.test.util.groupby.BaseGroupByTestCase;
 import com.liferay.portal.search.test.util.indexing.BaseIndexingTestCase;
 import com.liferay.portal.search.test.util.indexing.IndexingFixture;
@@ -47,9 +49,12 @@ public class GroupByTest extends BaseGroupByTestCase {
 
 	@Override
 	protected IndexingFixture createIndexingFixture() {
+		ElasticsearchFixture elasticsearchFixture = new ElasticsearchFixture(
+			ElasticsearchCountTest.class.getSimpleName());
+
 		return new ElasticsearchIndexingFixture(
-			new ElasticsearchFixture(GroupByTest.class.getSimpleName()),
-			BaseIndexingTestCase.COMPANY_ID);
+			elasticsearchFixture, BaseIndexingTestCase.COMPANY_ID,
+			new LiferayIndexCreator(elasticsearchFixture));
 	}
 
 }
