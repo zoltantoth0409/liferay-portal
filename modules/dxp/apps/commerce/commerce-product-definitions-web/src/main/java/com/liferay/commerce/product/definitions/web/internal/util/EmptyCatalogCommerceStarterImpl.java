34cdc9d136b848195172e8cf03a794087c79fbe0
Alessio Rendina <asilcomando@gmail.com>
2017-12-06 12:29:05 +0100

    COMMERCE-218 Added new commerce starter implementation

---
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/util/EmptyCatalogCommerceStarterImpl.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.util;
+
+import com.liferay.commerce.product.service.CPGroupLocalService;
+import com.liferay.commerce.product.util.CommerceStarter;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"commerce.starter.key=" + EmptyCatalogCommerceStarterImpl.KEY,
+		"commerce.starter.order:Integer=1"
+	},
+	service = CommerceStarter.class
+)
+public class EmptyCatalogCommerceStarterImpl implements CommerceStarter {
+
+	public static final String KEY = "empty-catalog";
+
+	@Override
+	public void create(HttpServletRequest httpServletRequest) throws Exception {
+		ServiceContext serviceContext = getServiceContext(httpServletRequest);
+
+		_cpGroupLocalService.addCPGroup(serviceContext);
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "empty-catalog-description");
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "empty-catalog");
+	}
+
+	public ServiceContext getServiceContext(
+			HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		User user = _portal.getUser(httpServletRequest);
+
+		long groupId = _portal.getScopeGroupId(httpServletRequest);
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setCompanyId(user.getCompanyId());
+		serviceContext.setScopeGroupId(groupId);
+		serviceContext.setTimeZone(user.getTimeZone());
+		serviceContext.setUserId(user.getUserId());
+
+		return serviceContext;
+	}
+
+	@Override
+	public String getThumbnailSrc() {
+		String contextPath = _servletContext.getContextPath();
+
+		String thumbnailSrc = contextPath + "/images/thumbnail.png";
+
+		return thumbnailSrc;
+	}
+
+	@Override
+	public boolean isActive(HttpServletRequest httpServletRequest) {
+		return true;
+	}
+
+	@Override
+	public void renderPreview(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		httpServletRequest.setAttribute(
+			"render.jsp-servletContext", _servletContext);
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/starter/render.jsp");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EmptyCatalogCommerceStarterImpl.class);
+
+	@Reference
+	private CPGroupLocalService _cpGroupLocalService;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.product.definitions.web)"
+	)
+	private ServletContext _servletContext;
+
+}
---
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/starter/render.jsp
@@ -0,0 +1,107 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+ServletContext servletContext = (ServletContext)request.getAttribute("render.jsp-servletContext");
+%>
+
+<div class="commerce-starter-content row">
+	<div class="carousel slide w100" data-interval="false" id="carousel-commerce-starter-default">
+		<div class="carousel-inner" role="listbox">
+			<div class="active carousel-item">
+				<img src="<%= servletContext.getContextPath() %>/images/carousel_01.png" />
+			</div>
+
+			<div class="carousel-item">
+				<img src="<%= servletContext.getContextPath() %>/images/carousel_02.png" />
+			</div>
+		</div>
+
+		<a class="carousel-control carousel-control-left left" data-slide="prev" href="#carousel-commerce-starter-default" role="button">
+			<liferay-ui:icon
+				cssClass="commerce-wizard-icon-angle-left"
+				icon="angle-left"
+				markupView="lexicon"
+			/>
+
+			<span class="sr-only">Previous</span>
+		</a>
+
+		<a class="carousel-control carousel-control-right right" data-slide="next" href="#carousel-commerce-starter-default" role="button">
+			<liferay-ui:icon
+				cssClass="commerce-wizard-icon-angle-right"
+				icon="angle-right"
+				markupView="lexicon"
+			/>
+
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+
+	<div class="row" style="margin-top:50px;">
+		<div class="col-md-6">
+			<h3>Key Features Include</h3>
+
+			<p>Vitae elit vel eros rhoncus auctor hac habitasse platea dictumst. Proin lobortis dia Nunc quis neque semper, mollis eros et Suspendisse ac laoreet orci, eu dapiho ncus auctor hac habitasse platea dictumst. Proin lobortis dia Nunc quis</p>
+		</div>
+
+		<div class="col-md-6">
+			<p>
+				Morbi vitae elit vel eros rhoncus auctor
+				In hac habitasse platea dictumst. Proin lobortis dia
+				Nunc quis neque semper, mollis eros et
+				Suspendisse ac laoreet orci, eu dapibus metus. Etiam nec lobortis urna. Integer rhoncu
+				Proin lobortis diam et consequat dignissim
+			</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	#carousel-commerce-starter-default {
+		position: relative;
+	}
+
+	#carousel-commerce-starter-default .carousel-item > img {
+		border-radius: 4px;
+		width: 100%;
+	}
+
+	#carousel-commerce-starter-default .carousel-control {
+		background-color: #303140;
+		padding: 6px 10px;
+		border-radius: 50%;
+		color: #FFF;
+		opacity: 0.7;
+		position: absolute;
+	}
+
+	#carousel-commerce-starter-default .carousel-control-left {
+		left: 5%;
+		top: 50%;
+	}
+
+	#carousel-commerce-starter-default .carousel-control-right {
+		right: 5%;
+		top: 50%;
+	}
+
+	.commerce-starter-content {
+		padding: 0 35px;
+	}
+</style>
