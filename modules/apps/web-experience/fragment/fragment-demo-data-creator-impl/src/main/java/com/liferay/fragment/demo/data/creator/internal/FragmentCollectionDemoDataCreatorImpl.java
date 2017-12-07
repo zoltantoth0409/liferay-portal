99d3e1cfe6b5f876332abd052a510353a7680154
Brian Chan <brian.chan@liferay.com>
2017-12-06 16:25:43 -0800

    LPS-75250 Fix inaccuracy

--- modules/apps/web-experience/fragment/fragment-demo-data-creator-impl/src/main/java/com/liferay/fragment/demo/data/creator/internal/FragmentCollectionDemoDataCreatorImpl.java
+++ modules/apps/web-experience/fragment/fragment-demo-data-creator-impl/src/main/java/com/liferay/fragment/demo/data/creator/internal/FragmentCollectionDemoDataCreatorImpl.java
@@ -50,17 +50,17 @@ public class FragmentCollectionDemoDataCreatorImpl
 			_fragmentCollectionLocalService.addFragmentCollection(
 				userId, groupId, name, null, serviceContext);
 
-		_entryIds.add(fragmentCollection.getFragmentCollectionId());
+		_fragmentCollectionIds.add(fragmentCollection.getFragmentCollectionId());
 
 		return fragmentCollection;
 	}
 
 	@Override
 	public void delete() throws PortalException {
-		for (long entryId : _entryIds) {
+		for (long fragmentCollectionId : _fragmentCollectionIds) {
 			try {
 				_fragmentCollectionLocalService.deleteFragmentCollection(
-					entryId);
+					fragmentCollectionId);
 			}
 			catch (NoSuchCollectionException nsce) {
 				if (_log.isWarnEnabled()) {
@@ -68,14 +68,15 @@ public class FragmentCollectionDemoDataCreatorImpl
 				}
 			}
 
-			_entryIds.remove(entryId);
+			_fragmentCollectionIds.remove(fragmentCollectionId);
 		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		FragmentCollectionDemoDataCreatorImpl.class);
 
-	private final List<Long> _entryIds = new CopyOnWriteArrayList<>();
+	private final List<Long> _fragmentCollectionIds =
+		new CopyOnWriteArrayList<>();
 
 	@Reference
 	private FragmentCollectionLocalService _fragmentCollectionLocalService;
