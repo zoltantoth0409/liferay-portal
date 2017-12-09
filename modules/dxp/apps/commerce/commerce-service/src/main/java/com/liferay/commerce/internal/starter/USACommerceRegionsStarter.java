320993cc5c38a8d5789b2a9661b368a0d50b7950
Marco Leo <marco.leo@liferay.com>
2017-12-09 17:23:47 +0100

    COMMERCE-164 Add regions starters

---
+++ modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/starter/USACommerceRegionsStarter.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.starter;
+
+import com.liferay.commerce.model.CommerceCountry;
+import com.liferay.commerce.service.CommerceCountryLocalService;
+import com.liferay.commerce.starter.CommerceRegionsStarter;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(immediate = true, service = CommerceRegionsStarter.class)
+public class USACommerceRegionsStarter extends BaseCommerceRegionsStarter {
+
+	public static final int USA_NUMERIC_ISO_CODE = 840;
+
+	@Override
+	public CommerceCountry getCommerceCountry(long groupId)
+		throws PortalException {
+
+		return _commerceCountryLocalService.fetchCommerceCountry(
+			groupId, USA_NUMERIC_ISO_CODE);
+	}
+
+	@Override
+	public JSONArray getCommerceRegionsJSONArray() throws Exception {
+		Class<?> clazz = getClass();
+
+		String layoutsPath = "com/liferay/commerce/internal/usa.json";
+
+		String regionsJSON = StringUtil.read(
+			clazz.getClassLoader(), layoutsPath, false);
+
+		return JSONFactoryUtil.createJSONArray(regionsJSON);
+	}
+
+	@Reference
+	private CommerceCountryLocalService _commerceCountryLocalService;
+
+}
