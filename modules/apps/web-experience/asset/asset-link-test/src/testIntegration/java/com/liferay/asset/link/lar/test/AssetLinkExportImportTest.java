abe471b798742ec36db9a1337f9a4d032a8d1f19
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-12-10 10:14:53 +0000

    LPS-76398 Moves tests

--- modules/apps/web-experience/asset/asset-link-test/src/testIntegration/java/com/liferay/asset/link/lar/test/AssetLinkExportImportTest.java
+++ modules/apps/web-experience/asset/asset-link-test/src/testIntegration/java/com/liferay/asset/link/lar/test/AssetLinkExportImportTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.asset.lar.test;
+package com.liferay.asset.link.lar.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetEntry;
--- modules/apps/web-experience/asset/asset-link-test/src/testIntegration/java/com/liferay/asset/link/service/test/AssetLinkLocalServiceTest.java
+++ modules/apps/web-experience/asset/asset-link-test/src/testIntegration/java/com/liferay/asset/link/service/test/AssetLinkLocalServiceTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.asset.service.test;
+package com.liferay.asset.link.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetEntry;
