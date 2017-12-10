233c83fb7d2ae2ce06c05030d228600483897027
Marco Leo <marco.leo@liferay.com>
2017-12-10 19:37:29 +0100

    COMMERCE-2 Add Exchange rate provider registry implementation

---
+++ modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/service/util/ExchangeRateProviderRegistryImpl.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.currency.service.util;
+
+import com.liferay.commerce.currency.util.ExchangeRateProvider;
+import com.liferay.commerce.currency.util.ExchangeRateProviderRegistry;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Marco Leo
+ */
+@Component(immediate = true, service = ExchangeRateProviderRegistry.class)
+public class ExchangeRateProviderRegistryImpl
+	implements ExchangeRateProviderRegistry {
+
+	@Override
+	public ExchangeRateProvider getExchangeRateProvider(String key) {
+		return _serviceTrackerMap.getService(key);
+	}
+
+	@Override
+	public Map<String, ExchangeRateProvider> getExchangeRateProviderMap() {
+		Map<String, ExchangeRateProvider> exchangeRateProviderMap =
+			new HashMap<>();
+
+		for (String key : _serviceTrackerMap.keySet()) {
+			exchangeRateProviderMap.put(
+				key, _serviceTrackerMap.getService(key));
+		}
+
+		return Collections.unmodifiableMap(exchangeRateProviderMap);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, ExchangeRateProvider.class,
+			"commerce.exchange.provider.key");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ExchangeRateProviderRegistryImpl.class);
+
+	private ServiceTrackerMap<String, ExchangeRateProvider> _serviceTrackerMap;
+
+}
