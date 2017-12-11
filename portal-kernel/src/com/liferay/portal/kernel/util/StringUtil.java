5255af533c23fff0ea39b106039304c1678cb196
Hugo Huijser <hugo.huijser@liferay.com>
2017-12-11 08:56:04 +0800

    LPS-76278 SF

--- portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java
@@ -4855,7 +4855,7 @@ public class StringUtil {
 	 *         empty
 	 */
 	public static String unquote(String s) {
-		if (Validator.isNull(s) || (s.length() < 2)) {
+		if (Validator.isNull(s) || (s.length() == 1)) {
 			return s;
 		}
 
