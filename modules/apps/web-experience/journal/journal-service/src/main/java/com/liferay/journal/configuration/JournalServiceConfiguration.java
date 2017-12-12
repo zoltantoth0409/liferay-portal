42f041a00d1fcdb3beed22ea8febe8e59868a84a
pavel-savinov <pavel.savinov@liferay.com>
2017-12-12 17:20:31 +0100

    LPS-75984 Add browse structures ordered by name configuration property

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/configuration/JournalServiceConfiguration.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/configuration/JournalServiceConfiguration.java
@@ -127,6 +127,13 @@ public interface JournalServiceConfiguration {
 	public boolean versionHistoryByDefaultEnabled();
 
 	@Meta.AD(
+		deflt = "false",
+		description = "journal-browse-by-structures-sorted-by-name",
+		name = "journal-browse-by-structures-sorted-by-name", required = false
+	)
+	public boolean structuresSortedByName();
+
+	@Meta.AD(
 		deflt = "false", description = "sync-content-search-on-startup",
 		required = false
 	)
