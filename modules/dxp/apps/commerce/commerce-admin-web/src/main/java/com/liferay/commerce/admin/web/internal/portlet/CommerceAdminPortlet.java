348fcf2e3449fb312c5f71298bdea797a59a9928
Alessio Rendina <asilcomando@gmail.com>
2017-12-12 19:38:03 +0100

    COMMERCE-2 Added css

--- modules/dxp/apps/commerce/commerce-admin-web/src/main/java/com/liferay/commerce/admin/web/internal/portlet/CommerceAdminPortlet.java
+++ modules/dxp/apps/commerce/commerce-admin-web/src/main/java/com/liferay/commerce/admin/web/internal/portlet/CommerceAdminPortlet.java
@@ -38,6 +38,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-commerce-admin",
 		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.layout-cacheable=true",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.preferences-unique-per-layout=false",
