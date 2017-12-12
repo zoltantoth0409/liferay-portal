f1af65112bed1b927d75c4357ffe76d3691b4bfc
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2017-12-12 15:02:24 -0300

    LPS-76316 Add specific success message to the workflow reversion action

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/action/RevertWorkflowDefinitionMVCActionCommand.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/portlet/action/RevertWorkflowDefinitionMVCActionCommand.java
@@ -14,18 +14,30 @@
 
 package com.liferay.portal.workflow.web.internal.portlet.action;
 
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowDefinition;
 import com.liferay.portal.kernel.workflow.WorkflowDefinitionManagerUtil;
 import com.liferay.portal.workflow.web.internal.constants.WorkflowPortletKeys;
 
+import java.text.Format;
+
+import java.util.Locale;
+
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Inácio Nery
@@ -41,6 +53,55 @@ import org.osgi.service.component.annotations.Component;
 public class RevertWorkflowDefinitionMVCActionCommand
 	extends UpdateWorkflowDefinitionMVCActionCommand {
 
+	/**
+	 * Adds a success message to the workflow definition reversion action
+	 *
+	 * @param actionRequest The actionRequest object of the action
+	 * @param workflowDefinition The workflow definition that will be reverted back to published.
+	 *
+	 * @review
+	 */
+	protected void addSuccessMessage(
+		ActionRequest actionRequest, WorkflowDefinition workflowDefinition) {
+
+		Locale locale = actionRequest.getLocale();
+
+		HttpServletRequest request = _portal.getHttpServletRequest(
+			actionRequest);
+
+		Format dateFormatTime = null;
+
+		if (!DateUtil.isFormatAmPm(locale)) {
+			dateFormatTime = FastDateFormatFactoryUtil.getSimpleDateFormat(
+				"MMM d, yyyy, HH:mm", locale);
+		}
+		else {
+			dateFormatTime = FastDateFormatFactoryUtil.getSimpleDateFormat(
+				"MMM d, yyyy, hh:mm a", locale);
+		}
+
+		String dateTime = dateFormatTime.format(
+			workflowDefinition.getModifiedDate());
+
+		String message = LanguageUtil.get(
+			request, "restored-to-revision-from-x");
+
+		SessionMessages.add(
+			actionRequest, "requestProcessed",
+			LanguageUtil.format(
+				locale,
+				LanguageUtil.get(request, "restored-to-revision-from-x"),
+				dateTime));
+	}
+
+	/**
+	 * Reverts a workflow definition to the published state, creating a new version of it.
+	 *
+	 * @param actionRequest
+	 * @param actionResponse
+	 *
+	 * @review
+	 */
 	@Override
 	protected void doProcessAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
@@ -61,6 +122,11 @@ public class RevertWorkflowDefinitionMVCActionCommand
 		workflowDefinitionManager.deployWorkflowDefinition(
 			themeDisplay.getCompanyId(), themeDisplay.getUserId(),
 			workflowDefinition.getTitle(), content.getBytes());
+
+		addSuccessMessage(actionRequest, workflowDefinition);
 	}
+
+	@Reference
+	private Portal _portal;
 
 }
