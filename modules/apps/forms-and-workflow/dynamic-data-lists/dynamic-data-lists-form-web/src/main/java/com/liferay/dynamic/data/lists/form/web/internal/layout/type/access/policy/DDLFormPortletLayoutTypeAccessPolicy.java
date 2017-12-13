ef3bd635667a5c96ca83e932c580b9296990f5b3
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2017-12-13 14:11:33 +0100

    LPS-75936 Restrict access only to DDMFormPortlet

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/layout/type/access/policy/DDLFormPortletLayoutTypeAccessPolicy.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/layout/type/access/policy/DDLFormPortletLayoutTypeAccessPolicy.java
@@ -14,9 +14,14 @@
 
 package com.liferay.dynamic.data.lists.form.web.internal.layout.type.access.policy;
 
+import com.liferay.dynamic.data.lists.form.web.internal.constants.DDLFormPortletKeys;
 import com.liferay.dynamic.data.lists.form.web.internal.layout.type.constants.DDLFormPortletLayoutTypeConstants;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutTypeAccessPolicy;
-import com.liferay.portal.kernel.model.impl.ModificationDeniedLayoutTypeAccessPolicyImpl;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.impl.DefaultLayoutTypeAccessPolicyImpl;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -29,5 +34,48 @@ import org.osgi.service.component.annotations.Component;
 	service = LayoutTypeAccessPolicy.class
 )
 public class DDLFormPortletLayoutTypeAccessPolicy
-	extends ModificationDeniedLayoutTypeAccessPolicyImpl {
+	extends DefaultLayoutTypeAccessPolicyImpl {
+
+	@Override
+	public boolean isAddLayoutAllowed(
+		PermissionChecker permissionChecker, Layout layout) {
+
+		return false;
+	}
+
+	@Override
+	public boolean isCustomizeLayoutAllowed(
+		PermissionChecker permissionChecker, Layout layout) {
+
+		return false;
+	}
+
+	@Override
+	public boolean isDeleteLayoutAllowed(
+		PermissionChecker permissionChecker, Layout layout) {
+
+		return false;
+	}
+
+	@Override
+	public boolean isUpdateLayoutAllowed(
+		PermissionChecker permissionChecker, Layout layout) {
+
+		return false;
+	}
+
+	@Override
+	protected boolean isAccessGrantedByPortletOnPage(
+		Layout layout, Portlet portlet) {
+
+		if (StringUtil.equalsIgnoreCase(
+				portlet.getPortletId(),
+				DDLFormPortletKeys.DYNAMIC_DATA_LISTS_FORM)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
 }
