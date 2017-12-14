0349938c69edf5c39f77a028f6ef6103ec9e7870
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2017-12-14 11:16:16 -0800

    LPS-76325 Add test case

--- modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/upgrade/v1_0_4/test/UpgradeClassNamesTest.java
+++ modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/upgrade/v1_0_4/test/UpgradeClassNamesTest.java
@@ -15,14 +15,22 @@
 package com.liferay.calendar.upgrade.v1_0_4.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalServiceUtil;
 import com.liferay.calendar.test.util.CalendarUpgradeTestUtil;
 import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.ResourcePermission;
+import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ResourcePermissionTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.model.impl.ResourcePermissionImpl;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
@@ -47,6 +55,18 @@ public class UpgradeClassNamesTest {
 	@Before
 	public void setUp() throws Exception {
 		setUpUpgradeCalendarResource();
+	}
+
+	@Test
+	public void testChangeCalEventClassNameIdInVocabularies() throws Exception {
+		ClassName calEventClassName = ClassNameLocalServiceUtil.addClassName(
+			"com.liferay.portlet.calendar.model.CalEvent");
+
+		addAssetVocabulary(calEventClassName.getClassNameId());
+
+		_upgradeProcess.upgrade();
+
+		assertNewClassNameIdExists();
 	}
 
 	@Test
@@ -156,12 +176,75 @@ public class UpgradeClassNamesTest {
 		assertOldPermissionExists();
 	}
 
+	protected void addAssetVocabulary(long classNameId) throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		long companyId = group.getCompanyId();
+		long groupId = group.getGroupId();
+
+		long pk = RandomTestUtil.nextLong();
+		long userId = TestPropsValues.getUserId();
+
+		_assetVocabulary =
+			AssetVocabularyLocalServiceUtil.createAssetVocabulary(pk);
+
+		_assetVocabulary.setUuid(RandomTestUtil.randomString());
+
+		_assetVocabulary.setGroupId(groupId);
+
+		_assetVocabulary.setCompanyId(companyId);
+
+		_assetVocabulary.setUserId(userId);
+
+		_assetVocabulary.setUserName(RandomTestUtil.randomString());
+
+		_assetVocabulary.setCreateDate(RandomTestUtil.nextDate());
+
+		_assetVocabulary.setModifiedDate(RandomTestUtil.nextDate());
+
+		_assetVocabulary.setName(RandomTestUtil.randomString());
+
+		_assetVocabulary.setTitle(RandomTestUtil.randomString());
+
+		_assetVocabulary.setDescription(RandomTestUtil.randomString());
+
+		StringBundler sb = new StringBundler(3);
+
+		sb.append("multiValued=true\nselectedClassNameIds=");
+		sb.append(classNameId);
+		sb.append(":-1");
+
+		_assetVocabulary.setSettings(sb.toString());
+
+		_assetVocabulary.setLastPublishDate(RandomTestUtil.nextDate());
+
+		AssetVocabularyLocalServiceUtil.addAssetVocabulary(_assetVocabulary);
+	}
+
 	protected ResourcePermission addResourcePermission(
 			long companyId, String name, String primKey, long roleId, int scope)
 		throws Exception {
 
 		return ResourcePermissionTestUtil.addResourcePermission(
 			companyId, name, primKey, roleId, scope);
+	}
+
+	protected void assertNewClassNameIdExists() {
+		long calBookingClassNameId = ClassNameLocalServiceUtil.getClassNameId(
+			"com.liferay.calendar.model.CalendarBooking");
+
+		EntityCacheUtil.clearCache();
+
+		AssetVocabulary assetVocabulary =
+			AssetVocabularyLocalServiceUtil.fetchAssetVocabulary(
+				_assetVocabulary.getVocabularyId());
+
+		String settings = assetVocabulary.getSettings();
+
+		boolean containsNewClassNameId = settings.contains(
+			String.valueOf(calBookingClassNameId));
+
+		Assert.assertTrue(containsNewClassNameId);
 	}
 
 	protected void assertNewResourcePermissionExists() {
@@ -198,6 +281,9 @@ public class UpgradeClassNamesTest {
 		_upgradeClassNames = CalendarUpgradeTestUtil.getUpgradeStep(
 			"v1_0_4.UpgradeClassNames");
 	}
+
+	@DeleteAfterTestRun
+	private AssetVocabulary _assetVocabulary;
 
 	@DeleteAfterTestRun
 	private ResourcePermission _newResourcePermission;
