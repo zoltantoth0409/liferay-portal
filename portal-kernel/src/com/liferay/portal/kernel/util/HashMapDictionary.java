f8f18ec6e7f0991d7de208bc41005cc2d89646b0
shuyangzhou <shuyang.zhou@liferay.com>
2017-12-14 17:07:59 -0800

    LPS-76527 Implements toString()

--- portal-kernel/src/com/liferay/portal/kernel/util/HashMapDictionary.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/HashMapDictionary.java
@@ -64,6 +64,11 @@ public class HashMapDictionary<K, V> extends Dictionary<K, V> {
 		return _map.size();
 	}
 
+	@Override
+	public String toString() {
+		return _map.toString();
+	}
+
 	private final Map<K, V> _map = new HashMap<>();
 
 }
