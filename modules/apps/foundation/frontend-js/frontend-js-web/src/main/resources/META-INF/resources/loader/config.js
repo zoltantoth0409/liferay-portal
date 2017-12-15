61f349d9306676b90abc05b176d1c915a2713a6b
Chema Balsas <jose.balsas@liferay.com>
2017-12-15 12:16:43 +0100

    LPS-76511 SF Order

--- modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/loader/config.js
+++ modules/apps/foundation/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/loader/config.js
@@ -24,7 +24,7 @@
 
 	__CONFIG__.namespace = 'Liferay';
 
-	__CONFIG__.exposeGlobal = Liferay.EXPOSE_GLOBAL;
-
 	__CONFIG__.explainResolutions = Liferay.EXPLAIN_RESOLUTIONS;
+
+	__CONFIG__.exposeGlobal = Liferay.EXPOSE_GLOBAL;
 }());
