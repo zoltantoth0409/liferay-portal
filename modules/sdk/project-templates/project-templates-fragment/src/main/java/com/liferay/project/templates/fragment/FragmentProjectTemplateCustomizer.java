2fcea4bf6adfd0c21a6d1c6b87159e69dd36a5e6
Gregory Amerson <gregory.amerson@liferay.com>
2017-12-14 20:42:54 -0600

    LPS-76524 now that we have a customizer interface, refactor out some of template specific pieces

---
+++ modules/sdk/project-templates/project-templates-fragment/src/main/java/com/liferay/project/templates/fragment/FragmentProjectTemplateCustomizer.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.project.templates.fragment;
+
+import com.liferay.project.templates.ProjectTemplateCustomizer;
+import com.liferay.project.templates.ProjectTemplatesArgs;
+
+import java.io.File;
+
+import java.util.Properties;
+
+import org.apache.maven.archetype.ArchetypeGenerationRequest;
+import org.apache.maven.archetype.ArchetypeGenerationResult;
+
+/**
+ * @author Gregory Amerson
+ */
+public class FragmentProjectTemplateCustomizer
+	implements ProjectTemplateCustomizer {
+
+	@Override
+	public void beforeGenerateProject(
+		ProjectTemplatesArgs projectTemplatesArgs,
+		ArchetypeGenerationRequest archetypeGenerationRequest) {
+
+		Properties properties = archetypeGenerationRequest.getProperties();
+
+		String hostBundleSymbolicName =
+			projectTemplatesArgs.getHostBundleSymbolicName();
+		String hostBundleVersion = projectTemplatesArgs.getHostBundleVersion();
+
+		properties.put("hostBundleSymbolicName", hostBundleSymbolicName);
+		properties.put("hostBundleVersion", hostBundleVersion);
+	}
+
+	@Override
+	public void postGenerateProject(
+		File destinationDir, ArchetypeGenerationResult result) {
+	}
+
+}
