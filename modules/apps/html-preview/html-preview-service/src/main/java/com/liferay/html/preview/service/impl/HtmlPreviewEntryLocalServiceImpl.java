1639dca7c5a8b95d5c3c7d205224643e7695bd6d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-12-16 10:13:05 +0100

    LPS-75578 Do it synchronous

--- modules/apps/html-preview/html-preview-service/src/main/java/com/liferay/html/preview/service/impl/HtmlPreviewEntryLocalServiceImpl.java
+++ modules/apps/html-preview/html-preview-service/src/main/java/com/liferay/html/preview/service/impl/HtmlPreviewEntryLocalServiceImpl.java
@@ -14,19 +14,23 @@
 
 package com.liferay.html.preview.service.impl;
 
-import com.liferay.html.preview.constants.HtmlPreviewConstants;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.html.preview.exception.InvalidHtmlPreviewEntryMimeTypeException;
 import com.liferay.html.preview.model.HtmlPreviewEntry;
+import com.liferay.html.preview.processor.HtmlPreviewProcessor;
+import com.liferay.html.preview.processor.HtmlPreviewProcessorTracker;
 import com.liferay.html.preview.service.base.HtmlPreviewEntryLocalServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.kernel.model.Repository;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+import java.io.File;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Pavel Savinov
@@ -58,9 +62,13 @@ public class HtmlPreviewEntryLocalServiceImpl
 		htmlPreviewEntry.setClassNameId(classNameId);
 		htmlPreviewEntry.setClassPK(classPK);
 
-		htmlPreviewEntryPersistence.update(htmlPreviewEntry);
+		FileEntry fileEntry = _getFileEntry(
+			htmlPreviewEntry.getUserId(), htmlPreviewEntry.getGroupId(),
+			htmlPreviewEntryId, content, mimeType);
 
-		_sendMessage(userId, groupId, htmlPreviewEntryId, content, mimeType);
+		htmlPreviewEntry.setFileEntryId(fileEntry.getFileEntryId());
+
+		htmlPreviewEntryPersistence.update(htmlPreviewEntry);
 
 		return htmlPreviewEntry;
 	}
@@ -100,33 +108,65 @@ public class HtmlPreviewEntryLocalServiceImpl
 		htmlPreviewEntry.setModifiedDate(
 			serviceContext.getModifiedDate(new Date()));
 
-		htmlPreviewEntryPersistence.update(htmlPreviewEntry);
-
-		_sendMessage(
+		FileEntry fileEntry = _getFileEntry(
 			htmlPreviewEntry.getUserId(), htmlPreviewEntry.getGroupId(),
 			htmlPreviewEntryId, content, mimeType);
+
+		htmlPreviewEntry.setFileEntryId(fileEntry.getFileEntryId());
+
+		htmlPreviewEntryPersistence.update(htmlPreviewEntry);
 
 		return htmlPreviewEntry;
 	}
 
-	private void _sendMessage(
-		long userId, long groupId, long htmlPreviewEntryId, String content,
-		String mimeType) {
+	private FileEntry _getFileEntry(
+			long userId, long groupId, long htmlPreviewEntryId, String content,
+			String mimeType)
+		throws PortalException {
 
-		Message message = new Message();
+		HtmlPreviewProcessor htmlPreviewProcessor =
+			_htmlPreviewProcessorTracker.getHtmlPreviewProcessor(mimeType);
 
-		Map<String, Object> payload = new HashMap<>();
+		if (htmlPreviewProcessor == null) {
+			throw new InvalidHtmlPreviewEntryMimeTypeException(
+				"No HTML preview processor available for MIME type " +
+					mimeType);
+		}
 
-		payload.put("content", content);
-		payload.put("groupId", groupId);
-		payload.put("htmlPreviewEntryId", htmlPreviewEntryId);
-		payload.put("mimeType", mimeType);
-		payload.put("userId", userId);
+		File file = null;
 
-		message.setPayload(payload);
+		try {
+			file = htmlPreviewProcessor.generateHtmlPreview(content);
+		}
+		catch (Exception e) {
+			throw new PortalException("Unable to generate html preview", e);
+		}
 
-		MessageBusUtil.sendMessage(
-			HtmlPreviewConstants.DESTINATION_NAME, message);
+		Repository repository =
+			PortletFileRepositoryUtil.fetchPortletRepository(
+				groupId, HtmlPreviewEntry.class.getName());
+
+		if (repository != null) {
+			FileEntry fileEntry =
+				PortletFileRepositoryUtil.fetchPortletFileEntry(
+					groupId, repository.getDlFolderId(),
+					String.valueOf(htmlPreviewEntryId));
+
+			if (fileEntry != null) {
+				PortletFileRepositoryUtil.deletePortletFileEntry(
+					groupId, repository.getDlFolderId(),
+					String.valueOf(htmlPreviewEntryId));
+			}
+		}
+
+		return PortletFileRepositoryUtil.addPortletFileEntry(
+			groupId, userId, HtmlPreviewEntry.class.getName(),
+			htmlPreviewEntryId, HtmlPreviewEntry.class.getName(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, file,
+			String.valueOf(htmlPreviewEntryId), mimeType, false);
 	}
+
+	@ServiceReference(type = HtmlPreviewProcessorTracker.class)
+	private HtmlPreviewProcessorTracker _htmlPreviewProcessorTracker;
 
 }
