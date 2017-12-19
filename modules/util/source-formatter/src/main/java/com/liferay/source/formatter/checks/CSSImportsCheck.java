eadf8324e63d1b3f87c5fa04779629e24242c9ca
Hugo Huijser <hugo.huijser@liferay.com>
2017-12-19 09:48:39 +0800

    LPS-76601 Peter, temporarily disable check for subrepo

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/CSSImportsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/CSSImportsCheck.java
@@ -31,6 +31,10 @@ public class CSSImportsCheck extends BaseFileCheck {
 			String fileName, String absolutePath, String content)
 		throws Exception {
 
+		if (isSubrepository() || isReadOnly(absolutePath)) {
+			return content;
+		}
+
 		Matcher matcher = _importsPattern.matcher(content);
 
 		while (matcher.find()) {
