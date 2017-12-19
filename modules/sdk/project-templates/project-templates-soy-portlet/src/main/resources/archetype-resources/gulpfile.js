190de78b6a1100c68a2b1f3afe7245d4f8e31a71
Gregory Amerson <gregory.amerson@liferay.com>
2017-12-19 10:48:54 -0600

    LPS-76201 js formatting

--- modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/archetype-resources/gulpfile.js
+++ modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/archetype-resources/gulpfile.js
@@ -1,53 +1,58 @@
-var gulp = require('gulp');
 var exec = require('child_process').exec;
+var gulp = require('gulp');
 
 const defaults = {
-		  encoding: 'utf8',
-		  timeout: 0,
-		  maxBuffer: 200 * 1024,
-		  killSignal: 'SIGTERM',
-		  cwd: __dirname + '/target/classes/META-INF/resources',
-		  env: null
-		};
+	cwd: __dirname + '/target/classes/META-INF/resources',
+	encoding: 'utf8',
+	env: null,
+	killSignal: 'SIGTERM',
+	maxBuffer: 200 * 1024,
+	timeout: 0
+};
 
-gulp.task('config-js-modules', function(cb) {
-
+gulp.task('config-js-modules', function(callback) {
 	process.chdir(__dirname + '/target/classes/META-INF/resources');
+
 	exec(__dirname + '/target/node/node ' + __dirname + '/node_modules/liferay-module-config-generator/bin/index.js ' +
-		  '--config= ' +
-		  '--extension= ' +
-		  '--filePattern **/resources/*.js ' +
-		  '--format /_/g,- ' +
-		  '--ignorePath true ' +
-		  '--moduleConfig ' + __dirname + '/package.json ' +
-		  '--namespace Liferay.Loader ' +
-		  '--output ' + __dirname + '/target/classes/META-INF/config.json ' +
-		  '--moduleRoot ' + __dirname + '/target/classes/META-INF/resources ' +
-		  __dirname + '/target/classes/META-INF', function(err, stdout, stderr) {
-	console.log(stdout);
-	console.log(stderr);
-	cb(err);
-}, defaults);
-})
+		'--config= ' +
+		'--extension= ' +
+		'--filePattern **/resources/*.js ' +
+		'--format /_/g,- ' +
+		'--ignorePath true ' +
+		'--moduleConfig ' + __dirname + '/package.json ' +
+		'--namespace Liferay.Loader ' +
+		'--output ' + __dirname + '/target/classes/META-INF/config.json ' +
+		'--moduleRoot ' + __dirname + '/target/classes/META-INF/resources ' +
+		__dirname + '/target/classes/META-INF', function(err, stdout, stderr) {
 
-gulp.task('transpile-js', function(cb) {
+			console.log(stdout);
+			console.log(stderr);
 
+			callback(err);
+
+		}, defaults);
+	})
+
+gulp.task('transpile-js', function(callback) {
 	process.chdir(__dirname + '/target/classes/META-INF/resources');
+
 	exec(__dirname + '/target/node/node ' + __dirname + '/node_modules/metal-cli/index.js build ' +
-		  '--bundleFileName= ' +
-		  '--dest ' + __dirname + '/target/classes/META-INF/resources ' +
-		  '--format amd ' +
-		  '--globalName= ' +
-		  '--moduleName= ' +
-		  '--soyDeps ' +
-		  __dirname + '/node_modules/lexicon*/src/**/*.soy ' +
-		  __dirname + '/node_modules/metal*/src/**/*.soy ' +
-		  '--soyDest ' + __dirname + '/target/classes/META-INF/resources ' +
-		  '--soySrc **/*.soy ' +
-		  '--src **/*.es.js ' +
-		  '**/*.soy.js', function(err, stdout, stderr) {
-	console.log(stdout);
-	console.log(stderr);
-	cb(err);
-}, defaults);
-})
+		'--bundleFileName= ' +
+		'--dest ' + __dirname + '/target/classes/META-INF/resources ' +
+		'--format amd ' +
+		'--globalName= ' +
+		'--moduleName= ' +
+		'--soyDeps ' +
+		__dirname + '/node_modules/lexicon*/src/**/*.soy ' +
+		__dirname + '/node_modules/metal*/src/**/*.soy ' +
+		'--soyDest ' + __dirname + '/target/classes/META-INF/resources ' +
+		'--soySrc **/*.soy ' +
+		'--src **/*.es.js ' +
+		'**/*.soy.js', function(err, stdout, stderr) {
+
+			console.log(stdout);
+			console.log(stderr);
+
+			callback(err);
+		}, defaults);
+	})
