873e578de1b7c2241d27c8d825f22aef1f4ad4e4
Hugo Huijser <hugo.huijser@liferay.com>
2017-12-20 11:25:55 +0800

    LPS-76626 Table names should not exceed 30 characters

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/SQLLongNamesCheck.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringBundler;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class SQLLongNamesCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		_checkTables(fileName, content);
+
+		return content;
+	}
+
+	private void _checkTables(String fileName, String content) {
+		Matcher matcher = _createTablePattern.matcher(content);
+
+		while (matcher.find()) {
+			String tableName = matcher.group(1);
+
+			if (tableName.length() > _MAX_NAME_LENGTH) {
+				addMessage(
+					fileName,
+					StringBundler.concat(
+						"Table name '", tableName, "' should not exceed ",
+						String.valueOf(_MAX_NAME_LENGTH), " characters"),
+					getLineCount(content, matcher.start()));
+			}
+		}
+	}
+
+	private static final int _MAX_NAME_LENGTH = 30;
+
+	private final Pattern _createTablePattern = Pattern.compile(
+		"create table (\\w+) \\(");
+
+}
