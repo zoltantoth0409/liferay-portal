eba2ca1ee62b8cf87db98f712cdb616a29a2cf81
ChrisKian <christopher.kian@liferay.com>
2017-12-20 11:38:17 -0800

    LPS-76652 Keep the calendar booking indexed after recycling it

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/search/CalendarBookingIndexer.java
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/search/CalendarBookingIndexer.java
@@ -214,7 +214,8 @@ public class CalendarBookingIndexer extends BaseIndexer<CalendarBooking> {
 		int status = calendarBooking.getStatus();
 
 		if ((status == CalendarBookingWorkflowConstants.STATUS_APPROVED) ||
-			(status == CalendarBookingWorkflowConstants.STATUS_MAYBE)) {
+			(status == CalendarBookingWorkflowConstants.STATUS_MAYBE) ||
+			(status == CalendarBookingWorkflowConstants.STATUS_IN_TRASH)) {
 
 			Document document = getDocument(calendarBooking);
 
@@ -222,9 +223,7 @@ public class CalendarBookingIndexer extends BaseIndexer<CalendarBooking> {
 				getSearchEngineId(), calendarBooking.getCompanyId(), document,
 				isCommitImmediately());
 		}
-		else if ((status == CalendarBookingWorkflowConstants.STATUS_DENIED) ||
-				 (status == CalendarBookingWorkflowConstants.STATUS_IN_TRASH)) {
-
+		else if (status == CalendarBookingWorkflowConstants.STATUS_DENIED) {
 			doDelete(calendarBooking);
 		}
 	}
@@ -273,7 +272,8 @@ public class CalendarBookingIndexer extends BaseIndexer<CalendarBooking> {
 
 					int[] statuses = {
 						CalendarBookingWorkflowConstants.STATUS_APPROVED,
-						CalendarBookingWorkflowConstants.STATUS_MAYBE
+						CalendarBookingWorkflowConstants.STATUS_MAYBE,
+						CalendarBookingWorkflowConstants.STATUS_IN_TRASH
 					};
 
 					dynamicQuery.add(statusProperty.in(statuses));
