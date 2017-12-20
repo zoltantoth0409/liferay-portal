7ba72bb5e9515b1a06df4f6920a3fce23ef166c5
akosthurzo <akos.thurzo@liferay.com>
2017-12-20 14:14:52 +0100

    LPS-76633 Add MVC commands for the taglibs

---
+++ modules/apps/web-experience/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/action/ExportImportEntityMVCActionCommand.java
@@ -0,0 +1,255 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.taglib.action;
+
+import com.liferay.exportimport.constants.ExportImportPortletKeys;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationConstants;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationParameterMapFactory;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationSettingsMapFactory;
+import com.liferay.exportimport.kernel.lar.ExportImportHelper;
+import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
+import com.liferay.exportimport.kernel.service.ExportImportConfigurationLocalService;
+import com.liferay.exportimport.kernel.service.ExportImportLocalService;
+import com.liferay.exportimport.kernel.staging.Staging;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.PortletLocalService;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.staging.taglib.exception.ExportImportEntityException;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Akos Thurzo
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + ExportImportPortletKeys.ENTITY_SET,
+		"mvc.command.name=exportImportEntity"
+	},
+	service = MVCActionCommand.class
+)
+public class ExportImportEntityMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, "cmd");
+
+		if (cmd.equals(Constants.EXPORT) || cmd.equals(Constants.PUBLISH)) {
+			_processExportAndPublishAction(actionRequest, actionResponse);
+		}
+		else {
+			SessionErrors.add(
+				actionRequest, ExportImportEntityException.class,
+				new ExportImportEntityException(
+					ExportImportEntityException.TYPE_INVALID_COMMAND));
+		}
+	}
+
+	protected void sendRedirect(
+			ActionRequest actionRequest, ActionResponse actionResponse,
+			long backgroundTaskId)
+		throws IOException {
+
+		LiferayPortletResponse liferayPortletResponse =
+			_portal.getLiferayPortletResponse(actionResponse);
+
+		PortletURL renderURL = liferayPortletResponse.createRenderURL(
+			ExportImportPortletKeys.EXPORT_IMPORT);
+
+		renderURL.setParameter("mvcPath", "/view_export_import.jsp");
+		renderURL.setParameter(
+			"backgroundTaskId", String.valueOf(backgroundTaskId));
+		renderURL.setParameter(
+			"backURL", actionRequest.getParameter("backURL"));
+
+		actionRequest.setAttribute(WebKeys.REDIRECT, renderURL.toString());
+
+		hideDefaultSuccessMessage(actionRequest);
+
+		sendRedirect(actionRequest, actionResponse);
+	}
+
+	private void _processExportAndPublishAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws IOException, PortalException {
+
+		String[] classNameClassPKArray;
+
+		if (Validator.isNotNull(
+				actionRequest.getParameter("classNameClassPK"))) {
+
+			classNameClassPKArray = ParamUtil.getStringValues(
+				actionRequest, "classNameClassPK");
+		}
+		else if (Validator.isNotNull(actionRequest.getParameter("className")) &&
+				 Validator.isNotNull(actionRequest.getParameter("classPK"))) {
+
+			String className = ParamUtil.getString(actionRequest, "className");
+			long classPK = ParamUtil.getLong(actionRequest, "classPK");
+
+			classNameClassPKArray =
+				new String[] {className + StringPool.POUND + classPK};
+		}
+		else {
+			SessionErrors.add(
+				actionRequest, ExportImportEntityException.class,
+				new ExportImportEntityException(
+					ExportImportEntityException.TYPE_NO_DATA_FOUND));
+
+			return;
+		}
+
+		Map<String, String[]> parameterMap =
+			ExportImportConfigurationParameterMapFactory.buildParameterMap();
+
+		parameterMap.put("classNameClassPK", classNameClassPKArray);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String cmd = ParamUtil.getString(actionRequest, "cmd");
+
+		String portletId = MapUtil.getString(
+			actionRequest.getParameterMap(), "portletId");
+
+		Portlet portlet = _portletLocalService.getPortletById(portletId);
+
+		if (cmd.equals("export")) {
+			Map<String, Serializable> settingsMap =
+				ExportImportConfigurationSettingsMapFactory.
+					buildExportPortletSettingsMap(
+						themeDisplay.getUser(), themeDisplay.getPlid(),
+						themeDisplay.getScopeGroupId(),
+						ExportImportPortletKeys.ENTITY_SET, parameterMap,
+						_exportImportHelper.getPortletExportFileName(portlet));
+
+			ExportImportConfiguration exportImportConfiguration =
+				_exportImportConfigurationLocalService.
+					addDraftExportImportConfiguration(
+						themeDisplay.getUserId(), portletId,
+						ExportImportConfigurationConstants.TYPE_EXPORT_PORTLET,
+						settingsMap);
+
+			sendRedirect(
+				actionRequest, actionResponse,
+				_exportImportLocalService.exportPortletInfoAsFileInBackground(
+					themeDisplay.getUserId(), exportImportConfiguration));
+		}
+		else if (cmd.equals("publish")) {
+			Group scopeGroup = themeDisplay.getScopeGroup();
+
+			if (!scopeGroup.isStagingGroup() &&
+				!scopeGroup.isStagedRemotely()) {
+
+				SessionErrors.add(
+					actionRequest, ExportImportEntityException.class,
+					new ExportImportEntityException(
+						ExportImportEntityException.TYPE_GROUP_NOT_STAGED));
+
+				return;
+			}
+
+			if (!scopeGroup.isStagedPortlet(portletId)) {
+				SessionErrors.add(
+					actionRequest, ExportImportEntityException.class,
+					new ExportImportEntityException(
+						ExportImportEntityException.TYPE_PORTLET_NOT_STAGED));
+
+				return;
+			}
+
+			long liveGroupId = 0;
+
+			if (scopeGroup.isStagingGroup()) {
+				liveGroupId = scopeGroup.getLiveGroupId();
+			}
+			else if (scopeGroup.isStagedRemotely()) {
+				liveGroupId = scopeGroup.getRemoteLiveGroupId();
+			}
+
+			Map<String, Serializable> settingsMap =
+				ExportImportConfigurationSettingsMapFactory.
+					buildPublishPortletSettingsMap(
+						themeDisplay.getUser(), themeDisplay.getScopeGroupId(),
+						themeDisplay.getPlid(), liveGroupId,
+						themeDisplay.getPlid(),
+						ExportImportPortletKeys.ENTITY_SET, parameterMap);
+
+			ExportImportConfiguration exportImportConfiguration =
+				_exportImportConfigurationLocalService.
+					addDraftExportImportConfiguration(
+						themeDisplay.getUserId(), portletId,
+						ExportImportConfigurationConstants.TYPE_PUBLISH_PORTLET,
+						settingsMap);
+
+			sendRedirect(
+				actionRequest, actionResponse,
+				_staging.publishPortlet(
+					themeDisplay.getUserId(), exportImportConfiguration));
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ExportImportEntityMVCActionCommand.class);
+
+	@Reference
+	private ExportImportConfigurationLocalService
+		_exportImportConfigurationLocalService;
+
+	@Reference
+	private ExportImportHelper _exportImportHelper;
+
+	@Reference
+	private ExportImportLocalService _exportImportLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private PortletLocalService _portletLocalService;
+
+	@Reference
+	private Staging _staging;
+
+}
---
+++ modules/apps/web-experience/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/exception/ExportImportEntityException.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.taglib.exception;
+
+/**
+ * @author Akos Thurzo
+ */
+public class ExportImportEntityException extends Exception {
+
+	public static final int TYPE_GROUP_NOT_STAGED = 1;
+
+	public static final int TYPE_INVALID_COMMAND = 2;
+
+	public static final int TYPE_NO_DATA_FOUND = 3;
+
+	public static final int TYPE_PORTLET_NOT_STAGED = 4;
+
+	public ExportImportEntityException(int type) {
+		_type = type;
+	}
+
+	public int getType() {
+		return _type;
+	}
+
+	private final int _type;
+
+}
