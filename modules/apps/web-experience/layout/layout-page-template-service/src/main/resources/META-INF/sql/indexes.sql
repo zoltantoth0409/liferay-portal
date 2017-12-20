37912e2db4e920d2df1a349561bd0b55c9cf589a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-12-20 10:51:59 +0100

    LPS-76374 Removes table in layout page template

--- modules/apps/web-experience/layout/layout-page-template-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/web-experience/layout/layout-page-template-service/src/main/resources/META-INF/sql/indexes.sql
@@ -2,6 +2,3 @@ create unique index IX_D569E8F2 on LayoutPageTemplateCollection (groupId, name[$
 
 create index IX_30AFAD84 on LayoutPageTemplateEntry (groupId, layoutPageTemplateCollectionId, name[$COLUMN_LENGTH:75$]);
 create unique index IX_A075DAA4 on LayoutPageTemplateEntry (groupId, name[$COLUMN_LENGTH:75$]);
-
-create index IX_F1F89D0C on LayoutPageTemplateFragment (groupId, fragmentEntryId);
-create index IX_3D504FC4 on LayoutPageTemplateFragment (groupId, layoutPageTemplateEntryId, fragmentEntryId);
--- modules/apps/web-experience/layout/layout-page-template-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/web-experience/layout/layout-page-template-service/src/main/resources/META-INF/sql/tables.sql
@@ -22,16 +22,3 @@ create table LayoutPageTemplateEntry (
 	name VARCHAR(75) null,
 	htmlPreviewEntryId LONG
 );
-
-create table LayoutPageTemplateFragment (
-	layoutPageTemplateFragmentId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	layoutPageTemplateEntryId LONG,
-	fragmentEntryId LONG,
-	position INTEGER
-);
