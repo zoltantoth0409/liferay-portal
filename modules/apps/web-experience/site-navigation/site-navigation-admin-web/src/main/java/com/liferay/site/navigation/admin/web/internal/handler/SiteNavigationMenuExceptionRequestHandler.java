252d333b81e17572b820fdf31cee4e84eac5e1dd
Jürgen Kappler <jurgen.kappler@liferay.com>
2017-12-21 16:00:50 +0100

    LPS-75772 Add Exception request handler for adding/editing a site navigation menu through action commands

---
+++ modules/apps/web-experience/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/handler/SiteNavigationMenuExceptionRequestHandler.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.admin.web.internal.handler;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.site.navigation.exception.SiteNavigationMenuNameException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true, service = SiteNavigationMenuExceptionRequestHandler.class
+)
+public class SiteNavigationMenuExceptionRequestHandler {
+
+	public void handlePortalException(
+			ActionRequest actionRequest, ActionResponse actionResponse,
+			PortalException pe)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		String errorMessage = "an-unexpected-error-occurred";
+
+		if (pe instanceof SiteNavigationMenuNameException) {
+			errorMessage = "this-field-is-required";
+		}
+
+		jsonObject.put(
+			"error", LanguageUtil.get(themeDisplay.getLocale(), errorMessage));
+
+		JSONPortletResponseUtil.writeJSON(
+			actionRequest, actionResponse, jsonObject);
+	}
+
+}
