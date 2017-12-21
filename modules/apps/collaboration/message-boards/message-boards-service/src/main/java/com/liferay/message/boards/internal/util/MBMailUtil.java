2e8dca84ad0d72138dfc4b52f158af9bee601aba
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2017-12-21 15:57:02 +0100

    LPS-76509 Move MBMailMessage logic to module

--- modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/util/MBMailUtil.java
+++ modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/util/MBMailUtil.java
@@ -30,7 +30,6 @@ import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.util.PropsValues;
-import com.liferay.portlet.messageboards.util.MBMailMessage;
 
 import java.io.InputStream;
 
