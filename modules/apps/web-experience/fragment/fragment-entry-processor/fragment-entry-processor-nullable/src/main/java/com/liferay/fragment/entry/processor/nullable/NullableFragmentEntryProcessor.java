5ba99ff467efba505b12c0e155ffa384398e6af4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-12-26 12:51:23 +0000

    LPS-76269 Implements nullable fragment entry processor

---
+++ modules/apps/web-experience/fragment/fragment-entry-processor/fragment-entry-processor-nullable/src/main/java/com/liferay/fragment/entry/processor/nullable/NullableFragmentEntryProcessor.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.entry.processor.nullable;
+
+import com.liferay.fragment.processor.FragmentEntryProcessor;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(immediate = true, service = FragmentEntryProcessor.class)
+public class NullableFragmentEntryProcessor implements FragmentEntryProcessor {
+
+	@Override
+	public void validateFragmentEntryHtml(String html) throws PortalException {
+	}
+
+}
