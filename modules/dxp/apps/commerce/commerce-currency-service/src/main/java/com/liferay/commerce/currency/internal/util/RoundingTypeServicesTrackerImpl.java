9e5766403d172579d09621ebf860741d43df4b51
Alessio Rendina <asilcomando@gmail.com>
2017-12-27 16:36:45 +0100

    COMMERCE-220 Refactor rounding type services tracker implementation

--- modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/internal/util/RoundingTypeServicesTrackerImpl.java
+++ modules/dxp/apps/commerce/commerce-currency-service/src/main/java/com/liferay/commerce/currency/internal/util/RoundingTypeServicesTrackerImpl.java
@@ -14,14 +14,21 @@
 
 package com.liferay.commerce.currency.internal.util;
 
+import com.liferay.commerce.currency.internal.util.comparator.RoundingTypeServiceWrapperPriorityComparator;
 import com.liferay.commerce.currency.util.RoundingType;
 import com.liferay.commerce.currency.util.RoundingTypeServicesTracker;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerCustomizerFactory;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.Validator;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.NavigableMap;
-import java.util.TreeMap;
+import java.util.Comparator;
+import java.util.List;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
@@ -30,38 +37,76 @@ import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author Andrea Di Giorgi
+ * @author Alessio Antonio Rendina
  */
 @Component(immediate = true)
 public class RoundingTypeServicesTrackerImpl
 	implements RoundingTypeServicesTracker {
 
 	@Override
-	public NavigableMap<String, RoundingType> getRoundingTypes() {
-		NavigableMap<String, RoundingType> roundingTypes = new TreeMap<>();
-
-		for (String name : _roundingTypeServiceTrackerMap.keySet()) {
-			RoundingType roundingType =
-				_roundingTypeServiceTrackerMap.getService(name);
-
-			roundingTypes.put(name, roundingType);
+	public RoundingType getRoundingType(String name) {
+		if (Validator.isNull(name)) {
+			name = RoundingTypeImpl.NAME;
 		}
 
-		return Collections.unmodifiableNavigableMap(roundingTypes);
+		ServiceTrackerCustomizerFactory.ServiceWrapper<RoundingType>
+			roundingTypeServiceWrapper = _serviceTrackerMap.getService(name);
+
+		if (roundingTypeServiceWrapper == null) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("No rounding types registered with name " + name);
+			}
+
+			return null;
+		}
+
+		return roundingTypeServiceWrapper.getService();
+	}
+
+	@Override
+	public List<RoundingType> getRoundingTypes() {
+		List<RoundingType> roundingTypes = new ArrayList<>();
+
+		List<ServiceTrackerCustomizerFactory.ServiceWrapper<RoundingType>>
+			roundingTypeServiceWrappers = ListUtil.fromCollection(
+				_serviceTrackerMap.values());
+
+		Collections.sort(
+			roundingTypeServiceWrappers,
+			_roundingTypeServiceWrapperPriorityComparator);
+
+		for (ServiceTrackerCustomizerFactory.ServiceWrapper<RoundingType>
+				roundingTypeServiceWrapper : roundingTypeServiceWrappers) {
+
+			roundingTypes.add(roundingTypeServiceWrapper.getService());
+		}
+
+		return Collections.unmodifiableList(roundingTypes);
 	}
 
 	@Activate
 	protected void activate(BundleContext bundleContext) {
-		_roundingTypeServiceTrackerMap =
-			ServiceTrackerMapFactory.openSingleValueMap(
-				bundleContext, RoundingType.class, "rounding.type.name");
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, RoundingType.class, "rounding.type.name",
+			ServiceTrackerCustomizerFactory.<RoundingType>serviceWrapper(
+				bundleContext));
 	}
 
 	@Deactivate
 	protected void deactivate() {
-		_roundingTypeServiceTrackerMap.close();
+		_serviceTrackerMap.close();
 	}
 
-	private ServiceTrackerMap<String, RoundingType>
-		_roundingTypeServiceTrackerMap;
+	private static final Log _log = LogFactoryUtil.getLog(
+		RoundingTypeServicesTrackerImpl.class);
+
+	private static final
+		Comparator<ServiceTrackerCustomizerFactory.ServiceWrapper<RoundingType>>
+			_roundingTypeServiceWrapperPriorityComparator =
+				new RoundingTypeServiceWrapperPriorityComparator();
+
+	private
+		ServiceTrackerMap<String, ServiceTrackerCustomizerFactory.
+			ServiceWrapper<RoundingType>> _serviceTrackerMap;
 
 }
