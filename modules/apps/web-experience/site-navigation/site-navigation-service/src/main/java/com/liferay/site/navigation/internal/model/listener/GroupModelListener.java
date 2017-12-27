9353e9ffe2999a2889ccbb1bee91b314214d78f1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-12-27 18:58:42 +0000

    LPS-75887 Removes site navigation menus before delete the group

---
+++ modules/apps/web-experience/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/internal/model/listener/GroupModelListener.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.internal.model.listener;
+
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.site.navigation.model.SiteNavigationMenu;
+import com.liferay.site.navigation.service.SiteNavigationMenuLocalService;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class GroupModelListener extends BaseModelListener<Group> {
+
+	@Override
+	public void onBeforeRemove(Group group) throws ModelListenerException {
+		try {
+			List<SiteNavigationMenu> siteNavigationMenus =
+				_siteNavigationMenuLocalService.getSiteNavigationMenus(
+					group.getGroupId());
+
+			for (SiteNavigationMenu siteNavigationMenu : siteNavigationMenus) {
+				_siteNavigationMenuLocalService.deleteSiteNavigationMenu(
+					siteNavigationMenu);
+			}
+		}
+		catch (Exception e) {
+			throw new ModelListenerException(e);
+		}
+	}
+
+	@Reference
+	private SiteNavigationMenuLocalService _siteNavigationMenuLocalService;
+
+}
