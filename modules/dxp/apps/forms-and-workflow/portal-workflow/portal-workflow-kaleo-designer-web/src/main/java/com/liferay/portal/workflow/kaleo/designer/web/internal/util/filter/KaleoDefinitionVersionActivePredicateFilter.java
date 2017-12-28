48cd066ab1a3f563408c1703998f494082a304fa
Inacio Nery <inacio.nery@liferay.com>
2017-12-28 19:35:54 -0300

    LPS-73731 Create predicate filter to KaleoDefinitionVersion status

---
+++ modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/util/filter/KaleoDefinitionVersionActivePredicateFilter.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.designer.web.internal.util.filter;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.PredicateFilter;
+import com.liferay.portal.workflow.kaleo.designer.web.internal.constants.KaleoDefinitionVersionConstants;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinition;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
+
+/**
+ * @author Inácio Nery
+ */
+public class KaleoDefinitionVersionActivePredicateFilter
+	implements PredicateFilter<KaleoDefinitionVersion> {
+
+	public KaleoDefinitionVersionActivePredicateFilter(int status) {
+		_status = status;
+	}
+
+	@Override
+	public boolean filter(KaleoDefinitionVersion kaleoDefinitionVersion) {
+		if (_status == KaleoDefinitionVersionConstants.STATUS_ALL) {
+			return true;
+		}
+
+		try {
+			KaleoDefinition kaleoDefinition =
+				kaleoDefinitionVersion.getKaleoDefinition();
+
+			if (_status == KaleoDefinitionVersionConstants.STATUS_PUBLISHED) {
+				return kaleoDefinition.isActive();
+			}
+			else {
+				return !kaleoDefinition.isActive();
+			}
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+
+			if (_status == KaleoDefinitionVersionConstants.STATUS_PUBLISHED) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		KaleoDefinitionVersionActivePredicateFilter.class);
+
+	private final int _status;
+
+}
