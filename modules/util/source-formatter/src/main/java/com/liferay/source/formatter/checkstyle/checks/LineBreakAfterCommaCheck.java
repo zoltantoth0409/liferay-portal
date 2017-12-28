b76741746b2193eb27d6b67fd1660950eb18fb1e
Hugo Huijser <hugo.huijser@liferay.com>
2017-12-28 15:00:01 +0800

    LPS-74544 Add check for incorrect line break after ','

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/LineBreakAfterCommaCheck.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.checkstyle.util.DetailASTUtil;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * @author Hugo Huijser
+ */
+public class LineBreakAfterCommaCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.COMMA};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		DetailAST parentAST = detailAST.getParent();
+
+		if (parentAST.getType() != TokenTypes.ELIST) {
+			return;
+		}
+
+		String line = getLine(detailAST.getLineNo() - 1);
+
+		if ((detailAST.getColumnNo() + 1) == line.length()) {
+			return;
+		}
+
+		DetailAST previousSiblingAST = detailAST.getPreviousSibling();
+
+		if (detailAST.getLineNo() !=
+				DetailASTUtil.getStartLine(previousSiblingAST)) {
+
+			String s = StringUtil.trim(
+				line.substring(0, detailAST.getColumnNo() + 1));
+
+			log(detailAST.getLineNo(), _MSG_INCORRECT_LINE_BREAK, s);
+		}
+	}
+
+	private static final String _MSG_INCORRECT_LINE_BREAK =
+		"line.break.incorrect";
+
+}
