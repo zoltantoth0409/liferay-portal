9389e3379175fc02c666e162f2069d2f9a12f89d
Inacio Nery <inacio.nery@liferay.com>
2017-12-28 19:11:20 -0300

    LPS-73731 Add order by to KaleoDefinitionVersion modifiedDate

---
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/util/comparator/KaleoDefinitionVersionModifiedDateComparator.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.util.comparator;
+
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
+
+import java.util.Date;
+
+/**
+ * @author Inácio Nery
+ */
+public class KaleoDefinitionVersionModifiedDateComparator
+	extends OrderByComparator<KaleoDefinitionVersion> {
+
+	public static final String ORDER_BY_ASC = "modifiedDate ASC";
+
+	public static final String ORDER_BY_DESC = "modifiedDate DESC";
+
+	public static final String[] ORDER_BY_FIELDS = {"modifiedDate"};
+
+	public KaleoDefinitionVersionModifiedDateComparator() {
+		this(false);
+	}
+
+	public KaleoDefinitionVersionModifiedDateComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	@Override
+	public int compare(
+		KaleoDefinitionVersion kaleoDefinitionVersion1,
+		KaleoDefinitionVersion kaleoDefinitionVersion2) {
+
+		Date modifiedDate1 = kaleoDefinitionVersion1.getModifiedDate();
+		Date modifiedDate2 = kaleoDefinitionVersion2.getModifiedDate();
+
+		int value = DateUtil.compareTo(modifiedDate1, modifiedDate2);
+
+		if (_ascending) {
+			return value;
+		}
+		else {
+			return -value;
+		}
+	}
+
+	@Override
+	public String getOrderBy() {
+		if (_ascending) {
+			return ORDER_BY_ASC;
+		}
+		else {
+			return ORDER_BY_DESC;
+		}
+	}
+
+	@Override
+	public String[] getOrderByFields() {
+		return ORDER_BY_FIELDS;
+	}
+
+	@Override
+	public boolean isAscending() {
+		return _ascending;
+	}
+
+	private final boolean _ascending;
+
+}
