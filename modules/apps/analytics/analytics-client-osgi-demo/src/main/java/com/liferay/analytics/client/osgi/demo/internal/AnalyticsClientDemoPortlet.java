ce37442bfdfc5b134698421836d95d6453fa2596
Julio Camarero <julio.camarero@liferay.com>
2017-12-28 09:31:06 +0100

    LPS-76580 SF

--- modules/apps/analytics/analytics-client-osgi-demo/src/main/java/com/liferay/analytics/client/osgi/demo/internal/AnalyticsClientDemoPortlet.java
+++ modules/apps/analytics/analytics-client-osgi-demo/src/main/java/com/liferay/analytics/client/osgi/demo/internal/AnalyticsClientDemoPortlet.java
@@ -31,7 +31,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Liferay
+ * @author Eduardo Garcia
  */
 @Component(
 	immediate = true,
