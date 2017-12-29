8e57d1e1931de0bfef4fac5d7245a604ceb7fcd6
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2017-12-29 08:21:27 +0000

    LPS-76089 Removes TYPE_URL from layout types

--- portal-impl/src/com/liferay/portal/util/LayoutTypeControllerTracker.java
+++ portal-impl/src/com/liferay/portal/util/LayoutTypeControllerTracker.java
@@ -79,7 +79,7 @@ public class LayoutTypeControllerTracker {
 
 	private static final String[] _LAYOUT_TYPES = {
 		LayoutConstants.TYPE_EMBEDDED, LayoutConstants.TYPE_PANEL,
-		LayoutConstants.TYPE_PORTLET, LayoutConstants.TYPE_URL
+		LayoutConstants.TYPE_PORTLET
 	};
 
 	private static final Map<String, LayoutTypeController>
