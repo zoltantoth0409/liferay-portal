9117aa56bfb70fffadf9450db601f37fcc910543
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-12-29 20:18:50 -0200

    LPS-73731 Create portlet configuration icon to delete unpublished workflow.

---
+++ modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/configuration/icon/DeleteDefinitionPortletConfigurationIcon.java
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.designer.web.internal.portlet.configuration.icon;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.workflow.kaleo.designer.web.constants.KaleoDesignerPortletKeys;
+import com.liferay.portal.workflow.kaleo.designer.web.internal.constants.KaleoDesignerWebKeys;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinition;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * Configuration icon to allow the deletion of a workflow definition.
+ *
+ * @author Adam Brandizzi
+ * @review
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + KaleoDesignerPortletKeys.KALEO_DESIGNER,
+		"path=/designer/edit_kaleo_definition_version.jsp"
+	},
+	service = PortletConfigurationIcon.class
+)
+public class DeleteDefinitionPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return LanguageUtil.get(getLocale(portletRequest), "delete");
+	}
+
+	@Override
+	public String getOnClick(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		String portletId = _portal.getPortletId(portletRequest);
+
+		String portletNamespace = _portal.getPortletNamespace(portletId);
+
+		String deleteURL = getURL(portletRequest, portletResponse);
+
+		StringBundler sb = new StringBundler(4);
+
+		sb.append(portletNamespace);
+		sb.append("confirmDeleteDefinition('");
+		sb.append(deleteURL);
+		sb.append("'); return false;");
+
+		return sb.toString();
+	}
+
+	/**
+	 * Creates and returns an action URL passing the workflow definition name
+	 * and version as parameters.
+	 *
+	 * @review
+	 */
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		PortletURL portletURL = _portal.getControlPanelPortletURL(
+			portletRequest, KaleoDesignerPortletKeys.KALEO_DESIGNER,
+			PortletRequest.ACTION_PHASE);
+
+		portletURL.setParameter(
+			ActionRequest.ACTION_NAME, "deleteKaleoDefinitionVersion");
+		portletURL.setParameter("name", portletRequest.getParameter("name"));
+		portletURL.setParameter(
+			"draftVersion", portletRequest.getParameter("draftVersion"));
+
+		return portletURL.toString();
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		KaleoDefinition kaleoDefinition = getKaleoDefinition(portletRequest);
+
+		if ((kaleoDefinition == null) || kaleoDefinition.isActive()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	protected KaleoDefinition getKaleoDefinition(
+		PortletRequest portletRequest) {
+
+		KaleoDefinitionVersion kaleoDefinitionVersion =
+			(KaleoDefinitionVersion)portletRequest.getAttribute(
+				KaleoDesignerWebKeys.KALEO_DRAFT_DEFINITION);
+
+		if (kaleoDefinitionVersion == null) {
+			return null;
+		}
+
+		try {
+			return kaleoDefinitionVersion.getKaleoDefinition();
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+		}
+
+		return null;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DeleteDefinitionPortletConfigurationIcon.class);
+
+	@Reference
+	private Portal _portal;
+
+}
