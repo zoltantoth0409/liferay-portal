4a60906ee0e47aeb479b61e336c86f794741db41
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-01-02 10:43:58 -0800

    COMMERCE-281 Purchase order payment engine

---
+++ modules/dxp/apps/commerce/commerce-payment-engine-purchase-order/src/main/java/com/liferay/commerce/payment/engine/purchase/order/internal/util/PurchaseOrderCommerceCheckoutStep.java
@@ -0,0 +1,191 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.payment.engine.purchase.order.internal.util;
+
+import com.liferay.commerce.checkout.web.constants.CommerceCheckoutWebKeys;
+import com.liferay.commerce.checkout.web.util.BaseCommerceCheckoutStep;
+import com.liferay.commerce.checkout.web.util.CommerceCheckoutStep;
+import com.liferay.commerce.exception.CommerceOrderPurchaseOrderNumberException;
+import com.liferay.commerce.model.CommerceCart;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommercePaymentMethod;
+import com.liferay.commerce.payment.engine.purchase.order.internal.PurchaseOrderCommercePaymentEngine;
+import com.liferay.commerce.payment.engine.purchase.order.internal.display.context.PurchaseOrderCheckoutStepDisplayContext;
+import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.commerce.service.CommercePaymentMethodService;
+import com.liferay.commerce.util.CommerceCartHelper;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Andrea Di Giorgi
+ */
+@Component(
+	immediate = true,
+	property = {
+		"commerce.checkout.step.name=" + PurchaseOrderCommerceCheckoutStep.NAME,
+		"commerce.checkout.step.order:Integer=" + (Integer.MAX_VALUE - 1)
+	},
+	service = CommerceCheckoutStep.class
+)
+public class PurchaseOrderCommerceCheckoutStep
+	extends BaseCommerceCheckoutStep {
+
+	public static final String NAME = "purchase-order";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public boolean isActive(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		long commercePaymentMethodId = 0;
+
+		long commerceOrderId = ParamUtil.getLong(
+			httpServletRequest, "commerceOrderId");
+
+		if (commerceOrderId > 0) {
+			CommerceOrder commerceOrder =
+				_commerceOrderService.getCommerceOrder(commerceOrderId);
+
+			commercePaymentMethodId =
+				commerceOrder.getCommercePaymentMethodId();
+		}
+		else {
+			CommerceCart commerceCart =
+				_commerceCartHelper.getCurrentCommerceCart(
+					httpServletRequest, httpServletResponse);
+
+			commercePaymentMethodId = commerceCart.getCommercePaymentMethodId();
+		}
+
+		if (commercePaymentMethodId <= 0) {
+			return false;
+		}
+
+		CommercePaymentMethod commercePaymentMethod =
+			_commercePaymentMethodService.getCommercePaymentMethod(
+				commercePaymentMethodId);
+
+		if (PurchaseOrderCommercePaymentEngine.KEY.equals(
+				commercePaymentMethod.getEngineKey())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isOrder() {
+		return true;
+	}
+
+	@Override
+	public boolean isVisible(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		return false;
+	}
+
+	@Override
+	public void processAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			updatePurchaseOrderNumber(actionRequest);
+		}
+		catch (CommerceOrderPurchaseOrderNumberException copone) {
+			SessionErrors.add(actionRequest, copone.getClass());
+
+			return;
+		}
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		PurchaseOrderCheckoutStepDisplayContext
+			purchaseOrderCheckoutStepDisplayContext =
+				new PurchaseOrderCheckoutStepDisplayContext(
+					_commerceOrderService, _configurationProvider,
+					httpServletRequest);
+
+		httpServletRequest.setAttribute(
+			CommerceCheckoutWebKeys.COMMERCE_CHECKOUT_STEP_DISPLAY_CONTEXT,
+			purchaseOrderCheckoutStepDisplayContext);
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/checkout_step/purchase_order.jsp");
+	}
+
+	protected void updatePurchaseOrderNumber(ActionRequest actionRequest)
+		throws Exception {
+
+		long commerceOrderId = ParamUtil.getLong(
+			actionRequest, "commerceOrderId");
+
+		String purchaseOrderNumber = ParamUtil.getString(
+			actionRequest, "purchaseOrderNumber");
+
+		_commerceOrderService.updatePurchaseOrderNumber(
+			commerceOrderId, purchaseOrderNumber);
+	}
+
+	@Reference
+	private CommerceCartHelper _commerceCartHelper;
+
+	@Reference
+	private CommerceOrderService _commerceOrderService;
+
+	@Reference
+	private CommercePaymentMethodService _commercePaymentMethodService;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.payment.engine.purchase.order)"
+	)
+	private ServletContext _servletContext;
+
+}
