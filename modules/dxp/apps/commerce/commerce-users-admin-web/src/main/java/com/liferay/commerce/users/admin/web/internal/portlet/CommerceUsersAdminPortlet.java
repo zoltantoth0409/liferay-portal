1618c10e919f474d351af40300694fc320c976b3
Marco Leo <marco.leo@liferay.com>
2018-01-02 11:39:54 +0100

    COMMERCE-230 Add portlet

---
+++ modules/dxp/apps/commerce/commerce-users-admin-web/src/main/java/com/liferay/commerce/users/admin/web/internal/portlet/CommerceUsersAdminPortlet.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.users.admin.web.internal.portlet;
+
+import com.liferay.commerce.users.admin.web.internal.constants.CommerceUsersAdminPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=portlet-commerce-users-admin",
+		"com.liferay.portlet.display-category=commerce",
+		"com.liferay.portlet.layout-cacheable=false",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.preferences-unique-per-layout=false",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.scopeable=true",
+		"javax.portlet.display-name=Commerce Account Management",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + CommerceUsersAdminPortletKeys.COMMERCE_USERS_ADMIN,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = {CommerceUsersAdminPortlet.class, Portlet.class}
+)
+public class CommerceUsersAdminPortlet extends MVCPortlet {
+}
