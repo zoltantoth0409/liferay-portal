f75e02920bfc1723727c16dfee0bc9837c019e8c
David Zhang <david.zhang@liferay.com>
2018-01-03 08:28:09 +0800

    LPS-76111 Use the original reference instead of creating it

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/content/processor/base/BaseTextExportImportContentProcessor.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/content/processor/base/BaseTextExportImportContentProcessor.java
@@ -402,9 +402,7 @@ public class BaseTextExportImportContentProcessor
 				exportedReferenceSB.append("$]");
 
 				if (fileEntry.isInTrash()) {
-					String originalReference = DLUtil.getPreviewURL(
-						fileEntry, fileEntry.getFileVersion(), null,
-						StringPool.BLANK, false, false);
+					String originalReference = sb.substring(beginPos, endPos);
 
 					exportedReferenceSB.append("[#dl-reference=");
 					exportedReferenceSB.append(originalReference);
