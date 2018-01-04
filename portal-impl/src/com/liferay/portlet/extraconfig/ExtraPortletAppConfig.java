5691d3769b344697783ae077bb899f09851965c4
leon.chi <leon.chi@liferay.com>
2018-01-04 10:15:27 +0800

    LPS-76715 Add barebone ExtraPortletAppConfig and ExtraPortletAppConfigRegistry to store configurations and avoid kernel.model changes

---
+++ portal-impl/src/com/liferay/portlet/extraconfig/ExtraPortletAppConfig.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.extraconfig;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author Leon Chi
+ */
+public class ExtraPortletAppConfig {
+
+	public ExtraPortletAppConfig(Map<String, String> localeEncodingMappings) {
+		_localeEncodingMappings = localeEncodingMappings;
+	}
+
+	public Map<String, String> getLocaleEncodingMappings() {
+		return Collections.unmodifiableMap(_localeEncodingMappings);
+	}
+
+	private final Map<String, String> _localeEncodingMappings;
+
+}
---
+++ portal-impl/src/com/liferay/portlet/extraconfig/ExtraPortletAppConfigRegistry.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.extraconfig;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @author Leon Chi
+ */
+public class ExtraPortletAppConfigRegistry {
+
+	public static ExtraPortletAppConfig getExtraPortletAppConfig(
+		String servletContextName) {
+
+		return _extraPortletAppConfigs.get(servletContextName);
+	}
+
+	public static void registerExtraPortletAppConfig(
+		String servletContextName,
+		ExtraPortletAppConfig extraPortletAppConfig) {
+
+		_extraPortletAppConfigs.put(servletContextName, extraPortletAppConfig);
+	}
+
+	public static void unRegisterExtraPortletAppConfig(
+		String servletContextName) {
+
+		_extraPortletAppConfigs.remove(servletContextName);
+	}
+
+	private static final ConcurrentMap<String, ExtraPortletAppConfig>
+		_extraPortletAppConfigs = new ConcurrentHashMap<>();
+
+}
