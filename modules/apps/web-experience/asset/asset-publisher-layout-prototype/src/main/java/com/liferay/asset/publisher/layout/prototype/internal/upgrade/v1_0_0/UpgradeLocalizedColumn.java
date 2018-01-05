e226b43585defc0bf9c46a9dda601a6051a0d10c
leon.chi <leon.chi@liferay.com>
2018-01-05 15:43:06 +0800

    LPS-72169 Upgrade localization XML for asset-publisher-layout-prototype

---
+++ modules/apps/web-experience/asset/asset-publisher-layout-prototype/src/main/java/com/liferay/asset/publisher/layout/prototype/internal/upgrade/v1_0_0/UpgradeLocalizedColumn.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.publisher.layout.prototype.internal.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradeLocalizedColumn;
+import com.liferay.portal.language.LanguageResources;
+import com.liferay.portal.util.PortalInstances;
+
+/**
+ * @author Leon Chi
+ */
+public class UpgradeLocalizedColumn extends BaseUpgradeLocalizedColumn {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		long[] companyIds = PortalInstances.getCompanyIdsBySQL();
+
+		upgradeLocalizedColumn(
+			LanguageResources.RESOURCE_BUNDLE_LOADER, "LayoutPrototype", "name",
+			_NAME, "layout-prototype-web-content-title", "Name", companyIds);
+
+		upgradeLocalizedColumn(
+			LanguageResources.RESOURCE_BUNDLE_LOADER, "LayoutPrototype",
+			"description", _DESCRIPTION,
+			"layout-prototype-web-content-description", "Description",
+			companyIds);
+	}
+
+	private static final String _DESCRIPTION =
+		"Create, edit, and explore web content with this page. Search " +
+			"available content, explore related content with tags, and " +
+				"browse content categories.";
+
+	private static final String _NAME =
+		"<?xml version='1.0' encoding='UTF-8'?><root available-locales=" +
+			"\"en_US\" default-locale=\"en_US\"><Name language-id=\"en_US\">" +
+				"Content Display Page</Name></root>";
+
+}
