16c554993577bea32977e9c26ab2a880ade00a3a
leon.chi <leon.chi@liferay.com>
2018-01-05 15:11:16 +0800

    LPS-72169 Upgrade localization XML for blogs-layout-prototype

---
+++ modules/apps/collaboration/blogs/blogs-layout-prototype/src/main/java/com/liferay/blogs/layout/prototype/internal/upgrade/v1_0_0/UpgradeLocalizedColumn.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.layout.prototype.internal.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.upgrade.BaseUpgradeLocalizedColumn;
+import com.liferay.portal.language.LanguageResources;
+import com.liferay.portal.util.PortalInstances;
+
+/**
+ * @author Leon Chi
+ */
+public class UpgradeLocalizedColumn extends BaseUpgradeLocalizedColumn {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		long[] companyIds = PortalInstances.getCompanyIdsBySQL();
+
+		upgradeLocalizedColumn(
+			LanguageResources.RESOURCE_BUNDLE_LOADER, "LayoutPrototype", "name",
+			_NAME, "layout-prototype-blog-title", "Name", companyIds);
+
+		upgradeLocalizedColumn(
+			LanguageResources.RESOURCE_BUNDLE_LOADER, "LayoutPrototype",
+			"description", _DESCRIPTION, "layout-prototype-blog-description",
+			"Description", companyIds);
+	}
+
+	private static final String _DESCRIPTION =
+		"Create, edit, and view blogs from this page. Explore topics using " +
+			"tags, and connect with other members that blog.";
+
+	private static final String _NAME =
+		"<?xml version='1.0' encoding='UTF-8'?><root available-locales=" +
+			"\"en_US\" default-locale=\"en_US\"><Name language-id=\"en_US\">" +
+				"Blog</Name></root>";
+
+}
