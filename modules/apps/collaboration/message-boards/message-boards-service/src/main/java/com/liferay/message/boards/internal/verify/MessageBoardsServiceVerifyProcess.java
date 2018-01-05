7ea42c141bc82da4e6587a087e076f61f8231193
tomwang2011 <tong.wang@liferay.com>
2018-01-05 09:55:13 -0800

    Revert "LPS-76419 Upgrade to remove no longer valid permissions"
    This reverts commit e78cafb8b476b1a58b69befcc3094ac38edc3a1c.

--- modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/verify/MessageBoardsServiceVerifyProcess.java
+++ modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/verify/MessageBoardsServiceVerifyProcess.java
@@ -61,7 +61,7 @@ public class MessageBoardsServiceVerifyProcess extends VerifyProcess {
 	}
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.message.boards.service)(release.schema.version=1.0.1))",
+		target = "(&(release.bundle.symbolic.name=com.liferay.message.boards.service)(release.schema.version=1.0.0))",
 		unbind = "-"
 	)
 	protected void setRelease(Release release) {
