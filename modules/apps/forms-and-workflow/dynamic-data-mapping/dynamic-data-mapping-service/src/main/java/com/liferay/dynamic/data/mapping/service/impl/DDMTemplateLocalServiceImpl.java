36b7dd26f24705dd01b6f7fa079d860c57d8702d
Renato Rego <renato.rego@liferay.com>
2018-01-08 15:18:40 -0300

    LPS-76501 - Auto SF.

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMTemplateLocalServiceImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMTemplateLocalServiceImpl.java
@@ -49,7 +49,6 @@ import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringBundler;
@@ -1611,6 +1610,17 @@ public class DDMTemplateLocalServiceImpl
 	}
 
 	protected void validate(
+			long groupId, Map<Locale, String> nameMap, String script)
+		throws PortalException {
+
+		validateName(groupId, nameMap);
+
+		if (Validator.isNull(script)) {
+			throw new TemplateScriptException("Script is null");
+		}
+	}
+
+	protected void validate(
 			long groupId, Map<Locale, String> nameMap, String script,
 			boolean smallImage, String smallImageURL, File smallImageFile,
 			byte[] smallImageBytes)
@@ -1659,17 +1669,6 @@ public class DDMTemplateLocalServiceImpl
 					String.valueOf(smallImageBytes.length),
 					" bytes and exceeds the maximum size of ",
 					String.valueOf(smallImageMaxSize)));
-		}
-	}
-
-	protected void validate(
-			long groupId, Map<Locale, String> nameMap, String script)
-		throws PortalException {
-
-		validateName(groupId, nameMap);
-
-		if (Validator.isNull(script)) {
-			throw new TemplateScriptException("Script is null");
 		}
 	}
 
