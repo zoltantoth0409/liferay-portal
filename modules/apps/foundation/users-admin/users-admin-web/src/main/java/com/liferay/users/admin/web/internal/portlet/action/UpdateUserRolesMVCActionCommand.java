2d7eaf958b645d2806aa1c9ed9c2d35388882999
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-01-08 11:46:22 -0800

    LPS-76552 Extract logic for updating user roles to its own MVCActionCommand

---
+++ modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/action/UpdateUserRolesMVCActionCommand.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.exception.NoSuchUserException;
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserGroupRole;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.membershippolicy.MembershipPolicyException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.users.admin.constants.UsersAdminPortletKeys;
+import com.liferay.users.admin.kernel.util.UsersAdmin;
+import com.liferay.users.admin.web.constants.UserFormConstants;
+
+import java.util.Calendar;
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + UsersAdminPortletKeys.MY_ACCOUNT,
+		"javax.portlet.name=" + UsersAdminPortletKeys.MY_ORGANIZATIONS,
+		"javax.portlet.name=" + UsersAdminPortletKeys.USERS_ADMIN,
+		"mvc.command.name=/users_admin/update_user_roles"
+	},
+	service = MVCActionCommand.class
+)
+public class UpdateUserRolesMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			User user = _portal.getSelectedUser(actionRequest);
+
+			Contact contact = user.getContact();
+
+			Calendar birthdayCal = CalendarFactoryUtil.getCalendar();
+
+			birthdayCal.setTime(user.getBirthday());
+
+			long[] roleIds = _usersAdmin.getRoleIds(actionRequest);
+
+			List<UserGroupRole> userGroupRoles = null;
+
+			String addGroupRolesGroupIds = actionRequest.getParameter(
+				"addGroupRolesGroupIds");
+			String addGroupRolesRoleIds = actionRequest.getParameter(
+				"addGroupRolesRoleIds");
+			String deleteGroupRolesGroupIds = actionRequest.getParameter(
+				"deleteGroupRolesGroupIds");
+			String deleteGroupRolesRoleIds = actionRequest.getParameter(
+				"deleteGroupRolesRoleIds");
+
+			if ((addGroupRolesGroupIds != null) ||
+				(addGroupRolesRoleIds != null) ||
+				(deleteGroupRolesGroupIds != null) ||
+				(deleteGroupRolesRoleIds != null)) {
+
+				userGroupRoles = _usersAdmin.getUserGroupRoles(actionRequest);
+			}
+
+			ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				User.class.getName(), actionRequest);
+
+			_userService.updateUser(
+				user.getUserId(), user.getPassword(), null, null,
+				user.getPasswordReset(), null, null, user.getScreenName(),
+				user.getEmailAddress(), user.getFacebookId(), user.getOpenId(),
+				user.getLanguageId(), user.getTimeZoneId(), user.getGreeting(),
+				user.getComments(), user.getFirstName(), user.getMiddleName(),
+				user.getLastName(), contact.getPrefixId(),
+				contact.getSuffixId(), user.isMale(),
+				birthdayCal.get(Calendar.MONTH), birthdayCal.get(Calendar.DATE),
+				birthdayCal.get(Calendar.YEAR), contact.getSmsSn(),
+				contact.getFacebookSn(), contact.getJabberSn(),
+				contact.getSkypeSn(), contact.getTwitterSn(),
+				user.getJobTitle(), user.getGroupIds(),
+				user.getOrganizationIds(), roleIds, userGroupRoles,
+				user.getUserGroupIds(), serviceContext);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchUserException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(actionRequest, e.getClass());
+
+				actionResponse.setRenderParameter("mvcPath", "/error.jsp");
+			}
+			else if (e instanceof MembershipPolicyException) {
+				SessionErrors.add(actionRequest, e.getClass(), e);
+
+				actionResponse.setRenderParameter("mvcPath", "/edit_user.jsp");
+				actionResponse.setRenderParameter(
+					"screenNavigationCategoryKey",
+					UserFormConstants.CATEGORY_KEY_GENERAL);
+				actionResponse.setRenderParameter(
+					"screenNavigationEntryKey",
+					UserFormConstants.ENTRY_KEY_ROLES);
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UsersAdmin _usersAdmin;
+
+	@Reference
+	private UserService _userService;
+
+}
--- modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/portlet/action/EditUserMVCActionCommand.java
+++ modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/portlet/action/EditUserMVCActionCommand.java
@@ -47,7 +47,6 @@ import com.liferay.portal.kernel.model.ListType;
 import com.liferay.portal.kernel.model.ListTypeConstants;
 import com.liferay.portal.kernel.model.Phone;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.model.UserGroupRole;
 import com.liferay.portal.kernel.model.Website;
 import com.liferay.portal.kernel.portlet.DynamicActionRequest;
 import com.liferay.portal.kernel.portlet.PortletProvider;
@@ -146,9 +145,6 @@ public class EditUserMVCActionCommand extends BaseMVCActionCommand {
 		String comments = ParamUtil.getString(actionRequest, "comments");
 		String jobTitle = ParamUtil.getString(actionRequest, "jobTitle");
 		long[] groupIds = UsersAdminUtil.getGroupIds(actionRequest);
-		long[] roleIds = UsersAdminUtil.getRoleIds(actionRequest);
-		List<UserGroupRole> userGroupRoles = UsersAdminUtil.getUserGroupRoles(
-			actionRequest);
 		long[] userGroupIds = UsersAdminUtil.getUserGroupIds(actionRequest);
 		boolean sendEmail = true;
 
@@ -160,27 +156,20 @@ public class EditUserMVCActionCommand extends BaseMVCActionCommand {
 			screenName, emailAddress, facebookId, null,
 			LocaleUtil.fromLanguageId(languageId), firstName, middleName,
 			lastName, prefixId, suffixId, male, birthdayMonth, birthdayDay,
-			birthdayYear, jobTitle, groupIds, null, roleIds, userGroupIds,
+			birthdayYear, jobTitle, groupIds, null, null, userGroupIds,
 			new ArrayList<Address>(), new ArrayList<EmailAddress>(),
 			new ArrayList<Phone>(), new ArrayList<Website>(),
 			new ArrayList<AnnouncementsDelivery>(), sendEmail, serviceContext);
 
-		if (!userGroupRoles.isEmpty()) {
-			for (UserGroupRole userGroupRole : userGroupRoles) {
-				userGroupRole.setUserId(user.getUserId());
-			}
-
-			user = _userService.updateUser(
-				user.getUserId(), StringPool.BLANK, StringPool.BLANK,
-				StringPool.BLANK, user.getPasswordReset(), null, null,
-				user.getScreenName(), user.getEmailAddress(), facebookId,
-				user.getOpenId(), true, null, languageId, user.getTimeZoneId(),
-				user.getGreeting(), comments, firstName, middleName, lastName,
-				prefixId, suffixId, male, birthdayMonth, birthdayDay,
-				birthdayYear, null, null, null, null, null, jobTitle, groupIds,
-				null, roleIds, userGroupRoles, userGroupIds, null, null, null,
-				null, null, serviceContext);
-		}
+		user = _userService.updateUser(
+			user.getUserId(), StringPool.BLANK, StringPool.BLANK,
+			StringPool.BLANK, user.getPasswordReset(), null, null,
+			user.getScreenName(), user.getEmailAddress(), facebookId,
+			user.getOpenId(), true, null, languageId, user.getTimeZoneId(),
+			user.getGreeting(), comments, firstName, middleName, lastName,
+			prefixId, suffixId, male, birthdayMonth, birthdayDay, birthdayYear,
+			null, null, null, null, null, jobTitle, groupIds, null, null, null,
+			userGroupIds, null, null, null, null, null, serviceContext);
 
 		long publicLayoutSetPrototypeId = ParamUtil.getLong(
 			actionRequest, "publicLayoutSetPrototypeId");
@@ -533,18 +522,6 @@ public class EditUserMVCActionCommand extends BaseMVCActionCommand {
 		String jobTitle = BeanParamUtil.getString(
 			user, actionRequest, "jobTitle");
 		long[] groupIds = UsersAdminUtil.getGroupIds(actionRequest);
-		long[] roleIds = UsersAdminUtil.getRoleIds(actionRequest);
-
-		List<UserGroupRole> userGroupRoles = null;
-
-		if ((actionRequest.getParameter("addGroupRolesGroupIds") != null) ||
-			(actionRequest.getParameter("addGroupRolesRoleIds") != null) ||
-			(actionRequest.getParameter("deleteGroupRolesGroupIds") != null) ||
-			(actionRequest.getParameter("deleteGroupRolesRoleIds") != null)) {
-
-			userGroupRoles = UsersAdminUtil.getUserGroupRoles(actionRequest);
-		}
-
 		long[] userGroupIds = UsersAdminUtil.getUserGroupIds(actionRequest);
 
 		ServiceContext serviceContext = ServiceContextFactory.getInstance(
@@ -556,9 +533,8 @@ public class EditUserMVCActionCommand extends BaseMVCActionCommand {
 			!deleteLogo, portraitBytes, languageId, user.getTimeZoneId(),
 			user.getGreeting(), comments, firstName, middleName, lastName,
 			prefixId, suffixId, male, birthdayMonth, birthdayDay, birthdayYear,
-			null, null, null, null, null, jobTitle, groupIds, null, roleIds,
-			userGroupRoles, userGroupIds, null, null, null, null, null,
-			serviceContext);
+			null, null, null, null, null, jobTitle, groupIds, null, null, null,
+			userGroupIds, null, null, null, null, null, serviceContext);
 
 		if (oldScreenName.equals(user.getScreenName())) {
 			oldScreenName = StringPool.BLANK;
