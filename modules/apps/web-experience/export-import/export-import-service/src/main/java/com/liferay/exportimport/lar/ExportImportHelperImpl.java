fadbc1ba2191c0a99c0a834fe92f5eb625bc157d
Tamas Molnar <tamas.molnar@liferay.com>
2018-01-08 17:04:06 +0100

    LPS-74002 Set the scopeGroup to the Live Site during LAR import when the portlet is not staged

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/lar/ExportImportHelperImpl.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/lar/ExportImportHelperImpl.java
@@ -1150,6 +1150,35 @@ public class ExportImportHelperImpl implements ExportImportHelper {
 						}
 					}
 				}
+
+				if (!ExportImportThreadLocal.isStagingInProcess() &&
+					group.isStagingGroup() &&
+					!group.isStagedPortlet(portletDataContext.getPortletId())) {
+
+					scopeGroup = group.getLiveGroup();
+
+					Layout scopeLiveLayout =
+						_layoutLocalService.fetchLayoutByUuidAndGroupId(
+							scopeLayoutUuid, group.getLiveGroupId(),
+							portletDataContext.isPrivateLayout());
+
+					if (scopeLiveLayout != null) {
+						scopeGroup = _groupLocalService.checkScopeGroup(
+							scopeLiveLayout,
+							portletDataContext.getUserId(null));
+					}
+				}
+			}
+			else {
+				Group group = _groupLocalService.getGroup(
+					portletDataContext.getGroupId());
+
+				if (!ExportImportThreadLocal.isStagingInProcess() &&
+					group.isStagingGroup() &&
+					!group.isStagedPortlet(portletDataContext.getPortletId())) {
+
+					scopeGroup = group.getLiveGroup();
+				}
 			}
 
 			if (scopeGroup != null) {
