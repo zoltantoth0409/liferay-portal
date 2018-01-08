372f05b2f612b1d1cc229f895f402b9112a8423b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-01-08 12:15:19 +0000

    LPS-76932 Adds new resource commands to export collections

---
+++ modules/apps/web-experience/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/ExportAllFragmentCollectionsMVCResourceCommand.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.portlet.action;
+
+import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.service.FragmentCollectionService;
+import com.liferay.fragment.web.internal.portlet.util.ExportUtil;
+import com.liferay.portal.kernel.portlet.PortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.util.List;
+
+import javax.portlet.PortletException;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + FragmentPortletKeys.FRAGMENT,
+		"mvc.command.name=/fragment/export_all_fragment_collections"
+	},
+	service = MVCResourceCommand.class
+)
+public class ExportAllFragmentCollectionsMVCResourceCommand
+	implements MVCResourceCommand {
+
+	@Override
+	public boolean serveResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws PortletException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		try {
+			List<FragmentCollection> fragmentCollections =
+				_fragmentCollectionService.getFragmentCollections(
+					themeDisplay.getScopeGroupId());
+
+			File file = ExportUtil.exportFragmentCollections(
+				fragmentCollections);
+
+			String fileName = "collections-" + Time.getTimestamp() + ".zip";
+
+			PortletResponseUtil.sendFile(
+				resourceRequest, resourceResponse, fileName,
+				new FileInputStream(file), ContentTypes.APPLICATION_ZIP);
+		}
+		catch (Exception e) {
+			throw new PortletException(e);
+		}
+
+		return true;
+	}
+
+	@Reference
+	private FragmentCollectionService _fragmentCollectionService;
+
+}
---
+++ modules/apps/web-experience/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/util/ExportUtil.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.portlet.util;
+
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.zip.ZipWriter;
+import com.liferay.portal.kernel.zip.ZipWriterFactoryUtil;
+
+import java.io.File;
+
+import java.util.List;
+
+import javax.portlet.PortletException;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class ExportUtil {
+
+	public static File exportFragmentCollections(
+			List<FragmentCollection> fragmentCollections)
+		throws PortletException {
+
+		ZipWriter zipWriter = ZipWriterFactoryUtil.getZipWriter();
+
+		try {
+			for (FragmentCollection fragmentCollection : fragmentCollections) {
+				fragmentCollection.populateZipWriter(
+					zipWriter, StringPool.BLANK);
+			}
+
+			zipWriter.finish();
+
+			return zipWriter.getFile();
+		}
+		catch (Exception e) {
+			throw new PortletException(e);
+		}
+	}
+
+}
