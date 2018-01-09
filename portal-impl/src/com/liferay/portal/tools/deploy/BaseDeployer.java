c6703a6c54fa21c32d34643e023b0647863ff684
Dante Wang <dante.wang@liferay.com>
2018-01-09 11:43:17 +0800

    LPS-76888 Strip possible leading slash from display name

--- portal-impl/src/com/liferay/portal/tools/deploy/BaseDeployer.java
+++ portal-impl/src/com/liferay/portal/tools/deploy/BaseDeployer.java
@@ -1174,6 +1174,10 @@ public class BaseDeployer implements AutoDeployer, Deployer {
 			double webXmlVersion, File srcFile, String displayName)
 		throws Exception {
 
+		if (displayName.startsWith(StringPool.FORWARD_SLASH)) {
+			displayName = displayName.substring(1);
+		}
+
 		StringBundler sb = new StringBundler(62);
 
 		sb.append("<display-name>");
