dc769051525e04c20d7482dac3198427d6421485
Brian Chan <brian.chan@liferay.com>
2018-01-09 10:20:20 -0800

    LPS-76715 SF

--- portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -2715,7 +2715,6 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 
 				String locale = GetterUtil.getString(
 					localeEncodingMappingElement.elementText("locale"));
-
 				String encoding = GetterUtil.getString(
 					localeEncodingMappingElement.elementText("encoding"));
 
