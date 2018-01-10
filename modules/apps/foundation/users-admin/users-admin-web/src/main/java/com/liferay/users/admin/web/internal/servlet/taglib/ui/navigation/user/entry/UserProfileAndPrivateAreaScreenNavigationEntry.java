0f1d718f29b1d5ed9bb9bd4be392d72cd5924dd1
drewbrokke <drew.brokke@liferay.com>
2018-01-09 16:36:41 -0800

    LPS-76550 users-admin-web - adds UserProfileAndPrivateAreaScreenNavigationEntry.java

---
+++ modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/servlet/taglib/ui/navigation/user/entry/UserProfileAndPrivateAreaScreenNavigationEntry.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.web.internal.servlet.taglib.ui.navigation.user.entry;
+
+import com.liferay.admin.kernel.util.PortalMyAccountApplicationType;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.users.admin.web.constants.UserFormConstants;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	property = {"screen.navigation.entry.order:Integer=50"},
+	service = ScreenNavigationEntry.class
+)
+public class UserProfileAndPrivateAreaScreenNavigationEntry
+	extends BaseUserScreenNavigationEntry {
+
+	@Override
+	public String getActionCommandName() {
+		return "/users_admin/edit_profile_and_private_area";
+	}
+
+	@Override
+	public String getCategoryKey() {
+		return UserFormConstants.CATEGORY_KEY_GENERAL;
+	}
+
+	@Override
+	public String getEntryKey() {
+		return UserFormConstants.ENTRY_KEY_PROFILE_AND_PRIVATE_AREA;
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/user/personal_site.jsp";
+	}
+
+	@Override
+	public boolean isVisible(User user, User selUser) {
+		if (selUser == null) {
+			return false;
+		}
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		String myAccountPortletId = PortletProviderUtil.getPortletId(
+			PortalMyAccountApplicationType.MyAccount.CLASS_NAME,
+			PortletProvider.Action.VIEW);
+
+		if (myAccountPortletId.equals(portletDisplay.getPortletName())) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
