41b7efb3e722423f0074d027f7ba190458bd66b1
Brian Chan <brian.chan@liferay.com>
2018-01-10 14:08:49 -0800

    LPS-76201 How close are we to getting Nate's JS formatter in place? Hugo SF

--- modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/archetype-resources/gulpfile.js
+++ modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/archetype-resources/gulpfile.js
@@ -19,50 +19,54 @@ function callNode(args, callback) {
 	.on('close', callback);
 }
 
-gulp.task('config-js-modules', function(done) {
-	callNode([
-		`${__dirname}/node_modules/liferay-module-config-generator/bin/index.js`,
-		 '--config',
-		 '',
-		 '--extension',
-		 '--filePattern',
-		 '**/resources/*.js',
-		 '--format',
-		 '/_/g,-',
-		 '--ignorePath',
-		 'true',
-		 '--moduleConfig',
-		 `${__dirname}/package.json`,
-		 '--namespace',
-		 'Liferay.Loader',
-		 '--output',
-		 `${__dirname}/target/classes/META-INF/config.json`,
-		 '--moduleRoot',
-		 resourcesDir,
-		 `${__dirname}/target/classes/META-INF`
-	], done);
-});
+gulp.task(
+	'config-js-modules',
+	function(done) {
+		callNode([
+			`${__dirname}/node_modules/liferay-module-config-generator/bin/index.js`,
+			 '--config',
+			 '',
+			 '--extension',
+			 '--filePattern',
+			 '**/resources/*.js',
+			 '--format',
+			 '/_/g,-',
+			 '--ignorePath',
+			 'true',
+			 '--moduleConfig',
+			 `${__dirname}/package.json`,
+			 '--namespace',
+			 'Liferay.Loader',
+			 '--output',
+			 `${__dirname}/target/classes/META-INF/config.json`,
+			 '--moduleRoot',
+			 resourcesDir,
+			 `${__dirname}/target/classes/META-INF`
+		], done);
+	});
 
-gulp.task('transpile-js', function(done) {
-	callNode([
-		`${__dirname}/node_modules/metal-cli/index.js`,
-		'build',
-		'--bundleFileName',
-		`--dest`,
-		resourcesDir,
-		'--format',
-		'amd',
-		'--globalName',
-		'--moduleName',
-		'--soyDeps',
-		`${__dirname}/node_modules/lexicon*/src/**/*.soy`,
-		`${__dirname}/node_modules/metal*/src/**/*.soy`,
-		'--soyDest',
-		resourcesDir,
-		'--soySrc',
-		'**/*.soy',
-		'--src',
-		'**/*.es.js',
-		'**/*.soy.js'
-	], done);
-});
+gulp.task(
+	'transpile-js',
+	function(done) {
+		callNode([
+			`${__dirname}/node_modules/metal-cli/index.js`,
+			'build',
+			'--bundleFileName',
+			`--dest`,
+			resourcesDir,
+			'--format',
+			'amd',
+			'--globalName',
+			'--moduleName',
+			'--soyDeps',
+			`${__dirname}/node_modules/lexicon*/src/**/*.soy`,
+			`${__dirname}/node_modules/metal*/src/**/*.soy`,
+			'--soyDest',
+			resourcesDir,
+			'--soySrc',
+			'**/*.soy',
+			'--src',
+			'**/*.es.js',
+			'**/*.soy.js'
+		], done);
+	});
