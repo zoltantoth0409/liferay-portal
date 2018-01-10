d3d3d3f9dc301d1d4d55d8601f50aa4dc8f9be13
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-01-10 14:37:38 -0800

    COMMERCE-229 Order note local service

--- modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderNoteLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderNoteLocalServiceImpl.java
@@ -14,11 +14,95 @@
 
 package com.liferay.commerce.service.impl;
 
+import com.liferay.commerce.exception.CommerceOrderNoteContentException;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderNote;
 import com.liferay.commerce.service.base.CommerceOrderNoteLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.List;
 
 /**
- * @author Alessio Antonio Rendina
+ * @author Andrea Di Giorgi
  */
 public class CommerceOrderNoteLocalServiceImpl
 	extends CommerceOrderNoteLocalServiceBaseImpl {
+
+	@Override
+	public CommerceOrderNote addCommerceOrderNote(
+			long commerceOrderId, String content, boolean restricted,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		CommerceOrder commerceOrder =
+			commerceOrderLocalService.getCommerceOrder(commerceOrderId);
+		User user = userLocalService.getUser(serviceContext.getUserId());
+
+		validate(content);
+
+		long commerceOrderNoteId = counterLocalService.increment();
+
+		CommerceOrderNote commerceOrderNote =
+			commerceOrderNotePersistence.create(commerceOrderNoteId);
+
+		commerceOrderNote.setGroupId(commerceOrder.getGroupId());
+		commerceOrderNote.setCompanyId(user.getCompanyId());
+		commerceOrderNote.setUserId(user.getUserId());
+		commerceOrderNote.setUserName(user.getFullName());
+		commerceOrderNote.setCommerceOrderId(
+			commerceOrder.getCommerceOrderId());
+		commerceOrderNote.setContent(content);
+		commerceOrderNote.setRestricted(restricted);
+
+		commerceOrderNotePersistence.update(commerceOrderNote);
+
+		return commerceOrderNote;
+	}
+
+	@Override
+	public void deleteCommerceOrderNotes(long commerceOrderId) {
+		commerceOrderNotePersistence.removeByCommerceOrderId(commerceOrderId);
+	}
+
+	@Override
+	public List<CommerceOrderNote> getCommerceOrderNotes(
+		long commerceOrderId, int start, int end) {
+
+		return commerceOrderNotePersistence.findByCommerceOrderId(
+			commerceOrderId, start, end);
+	}
+
+	@Override
+	public int getCommerceOrderNotesCount(long commerceOrderId) {
+		return commerceOrderNotePersistence.countByCommerceOrderId(
+			commerceOrderId);
+	}
+
+	@Override
+	public CommerceOrderNote updateCommerceOrderNote(
+			long commerceOrderNoteId, String content, boolean restricted)
+		throws PortalException {
+
+		CommerceOrderNote commerceOrderNote =
+			commerceOrderNotePersistence.findByPrimaryKey(commerceOrderNoteId);
+
+		validate(content);
+
+		commerceOrderNote.setContent(content);
+		commerceOrderNote.setRestricted(restricted);
+
+		commerceOrderNotePersistence.update(commerceOrderNote);
+
+		return commerceOrderNote;
+	}
+
+	protected void validate(String content) throws PortalException {
+		if (Validator.isNull(content)) {
+			throw new CommerceOrderNoteContentException();
+		}
+	}
+
 }
