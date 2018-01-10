f9176f1be331678cb32bf00cad620272e3e75178
Noah Sherrill <noah.sherrill@liferay.com>
2018-01-09 16:56:48 -0800

    LPS-76029 Add auto-anonymize, delete and export entity commands

---
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/action/AutoAnonymizeUADEntityMVCActionCommand.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.associated.data.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.user.associated.data.aggregator.UADEntityAggregator;
+import com.liferay.user.associated.data.anonymizer.UADEntityAnonymizer;
+import com.liferay.user.associated.data.registry.UADRegistry;
+import com.liferay.users.admin.constants.UsersAdminPortletKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Noah Sherrill
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + UsersAdminPortletKeys.MY_ORGANIZATIONS,
+		"javax.portlet.name=" + UsersAdminPortletKeys.USERS_ADMIN,
+		"mvc.command.name=/users_admin/auto_anonymize_uad_entity"
+	},
+	service = MVCActionCommand.class
+)
+public class AutoAnonymizeUADEntityMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		_autoAnonymizeUADEntity(actionRequest);
+	}
+
+	private void _autoAnonymizeUADEntity(ActionRequest actionRequest)
+		throws Exception {
+
+		String uadEntityId = ParamUtil.getString(actionRequest, "uadEntityId");
+		String uadRegistryKey = ParamUtil.getString(
+			actionRequest, "uadRegistryKey");
+
+		UADEntityAggregator uadEntityAggregator =
+			_uadRegistry.getUADEntityAggregator(uadRegistryKey);
+		UADEntityAnonymizer uadEntityAnonymizer =
+			_uadRegistry.getUADEntityAnonymizer(uadRegistryKey);
+
+		uadEntityAnonymizer.autoAnonymize(
+			uadEntityAggregator.getUADEntity(uadEntityId));
+	}
+
+	@Reference
+	private UADRegistry _uadRegistry;
+
+}
