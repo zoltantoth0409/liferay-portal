b5681cf93622864c81c3a86d6df81f4077cb6dc2
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-01-10 09:49:11 -0800

    COMMERCE-2 Regen services

--- modules/dxp/apps/commerce/commerce-product-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/resources/META-INF/sql/indexes.sql
@@ -46,11 +46,6 @@ create index IX_6A1505A2 on CPFriendlyURLEntry (groupId, classNameId, classPK, l
 create index IX_3F4546AB on CPFriendlyURLEntry (groupId, classNameId, classPK, main);
 create index IX_7A386C69 on CPFriendlyURLEntry (groupId, classNameId, languageId[$COLUMN_LENGTH:75$], urlTitle[$COLUMN_LENGTH:255$]);
 create index IX_496A7862 on CPFriendlyURLEntry (groupId, classNameId, urlTitle[$COLUMN_LENGTH:255$]);
-create index IX_71874AE8 on CPFriendlyURLEntry (groupId, companyId, classNameId, classPK, languageId[$COLUMN_LENGTH:75$], main);
-create index IX_393BFCD8 on CPFriendlyURLEntry (groupId, companyId, classNameId, classPK, languageId[$COLUMN_LENGTH:75$], urlTitle[$COLUMN_LENGTH:255$]);
-create index IX_C13D69E1 on CPFriendlyURLEntry (groupId, companyId, classNameId, classPK, main);
-create index IX_3E26073 on CPFriendlyURLEntry (groupId, companyId, classNameId, languageId[$COLUMN_LENGTH:75$], urlTitle[$COLUMN_LENGTH:255$]);
-create index IX_C7AAAB6C on CPFriendlyURLEntry (groupId, companyId, classNameId, urlTitle[$COLUMN_LENGTH:255$]);
 create index IX_BD972D55 on CPFriendlyURLEntry (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_F4450517 on CPFriendlyURLEntry (uuid_[$COLUMN_LENGTH:75$], groupId);
 
