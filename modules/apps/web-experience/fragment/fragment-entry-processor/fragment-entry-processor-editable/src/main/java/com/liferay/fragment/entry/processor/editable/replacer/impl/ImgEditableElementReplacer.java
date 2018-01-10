d88aa101e4dc0232a227399d079694c3ed304bf5
pavel-savinov <pavel.savinov@liferay.com>
2018-01-10 13:01:35 +0100

    LPS-76972 Editable image element replacer implementation

---
+++ modules/apps/web-experience/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/replacer/impl/ImgEditableElementReplacer.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.entry.processor.editable.replacer.impl;
+
+import com.liferay.fragment.entry.processor.editable.replacer.EditableElementReplacer;
+import com.liferay.portal.kernel.xml.Attribute;
+import com.liferay.portal.kernel.xml.Element;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true, property = {"editable.tag.name=img"},
+	service = EditableElementReplacer.class
+)
+public class ImgEditableElementReplacer implements EditableElementReplacer {
+
+	@Override
+	public void replace(Element element, String value) {
+		Attribute attribute = element.attribute("src");
+
+		attribute.setValue(value);
+	}
+
+}
