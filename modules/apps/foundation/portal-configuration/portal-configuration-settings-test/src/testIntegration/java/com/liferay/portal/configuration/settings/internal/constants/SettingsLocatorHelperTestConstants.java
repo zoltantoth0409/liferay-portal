90c3a4a8b1ffa59d196d1b071691904c3d061ab0
drewbrokke <drew.brokke@liferay.com>
2018-01-11 10:29:56 -0800

    LPS-76946 portal-configuration-settings-test - adds test module and helper files

---
+++ modules/apps/foundation/portal-configuration/portal-configuration-settings-test/src/testIntegration/java/com/liferay/portal/configuration/settings/internal/constants/SettingsLocatorHelperTestConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configuration.settings.internal.constants;
+
+/**
+ * @author Drew Brokke
+ */
+public class SettingsLocatorHelperTestConstants {
+
+	public static final String TEST_CONFIGURATION_PID =
+		"com.liferay.portal.configuration.settings.internal.samples." +
+			"TestConfiguration";
+
+	public static final String TEST_DEFAULT_VALUE = "testDefaultValue";
+
+	public static final String TEST_KEY = "settingsLocatorTestKey";
+
+}
