cc896bbc24baa973b617b6fdb2f77441646cc716
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-01-11 18:23:33 -0800

    LPS-76024 Move code to BaseUADEntityExporter so it can be reused by other Exporter classes

--- modules/apps/foundation/user-associated-data/user-associated-data-api/src/main/java/com/liferay/user/associated/data/exporter/BaseUADEntityExporter.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-api/src/main/java/com/liferay/user/associated/data/exporter/BaseUADEntityExporter.java
@@ -14,11 +14,25 @@
 
 package com.liferay.user.associated.data.exporter;
 
+import com.liferay.document.library.kernel.exception.NoSuchFolderException;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.Repository;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.user.associated.data.entity.UADEntity;
 
 import java.util.List;
+
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author William Newbury
@@ -32,10 +46,59 @@ public abstract class BaseUADEntityExporter implements UADEntityExporter {
 		}
 	}
 
+	protected Folder getFolder(
+			long companyId, String portletId, String folderName)
+		throws PortalException {
+
+		Group guestGroup = groupLocalService.getGroup(
+			companyId, GroupConstants.GUEST);
+
+		Repository repository =
+			PortletFileRepositoryUtil.fetchPortletRepository(
+				guestGroup.getGroupId(), portletId);
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		if (repository == null) {
+			repository = PortletFileRepositoryUtil.addPortletRepository(
+				guestGroup.getGroupId(), portletId, serviceContext);
+		}
+
+		Folder folder = null;
+
+		try {
+			folder = PortletFileRepositoryUtil.getPortletFolder(
+				repository.getRepositoryId(),
+				DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, folderName);
+		}
+		catch (NoSuchFolderException nsfe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(nsfe, nsfe);
+			}
+
+			folder = PortletFileRepositoryUtil.addPortletFolder(
+				userLocalService.getDefaultUserId(companyId),
+				repository.getRepositoryId(),
+				DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, folderName,
+				serviceContext);
+		}
+
+		return folder;
+	}
+
 	protected String getJSON(Object object) {
 		return JSONFactoryUtil.looseSerialize(object);
 	}
 
 	protected abstract List<UADEntity> getUADEntities(long userId);
+
+	@Reference
+	protected GroupLocalService groupLocalService;
+
+	@Reference
+	protected UserLocalService userLocalService;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BaseUADEntityExporter.class);
 
 }
