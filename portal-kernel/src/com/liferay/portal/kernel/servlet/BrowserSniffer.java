38ce7405751ce36b82f3c4a5ec0a6ebca683bdf6
codyhoag <cody.hoag@liferay.com>
2018-01-12 16:37:03 -0500

    LPS-69747 LRDOCS-4155 Edit Javadoc

--- portal-kernel/src/com/liferay/portal/kernel/servlet/BrowserSniffer.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/BrowserSniffer.java
@@ -41,50 +41,52 @@ public interface BrowserSniffer {
 	public BrowserMetadata getBrowserMetadata(HttpServletRequest request);
 
 	/**
-	 * <p>
-	 * Retrieves the browser's version number as a float, compared to 
-	 * {@link BrowserSniffer#getVersion(HttpServletRequest)} that retrieves
-	 * the version number as a {@link String}.
-	 * </p>
+	 * Returns the browser's version number as a float. This differs from
+	 * {@link BrowserSniffer#getVersion(HttpServletRequest)}, which returns
+	 * the version number as a String.
+	 *
 	 * <p>
 	 * Note that the version returned is defined as the real version of the
 	 * browser software, not the one used to render the page. For example, the 
 	 * browser can be IE10 but it may be using a compatibility view emulating IE8 
-	 * to render the Page. In such a case, this method returns "10.0", not "8.0."
+	 * to render the page. In such a case, this method would return
+	 * <code>10.0</code>, not <code>8.0</code>.
 	 * </p>
-	 * @param request
+	 *
+	 * @param request the servlet request
 	 * @return a float representing the version number
 	 * @see BrowserSniffer#getVersion(HttpServletRequest)
 	 */
 	public float getMajorVersion(HttpServletRequest request);
 
 	/**
-	 * <p>
-	 * Retrieves the browser's revision.
-	 * </p>
+	 * Returns the browser's revision.
+	 *
 	 * <p>
 	 * Note that the revision returned is defined as the real revision of the 
 	 * browser software, not the one used to render the page. For example, the 
 	 * browser can be IE10 but it may be using a compatibility view emulating IE8
-	 * to render the page. In such a case, this method returns "10.0", not "8.0".
+	 * to render the page. In such a case, this method would return
+	 * <code>10.0</code>, not <code>8.0</code>.
 	 * </p>
-	 * @param request
+	 *
+	 * @param request the servlet request
 	 * @return a String containing the revision number
 	 */
 	public String getRevision(HttpServletRequest request);
 
 	/**
-	 * <p>
-	 * Retrieves the browser's version.
-	 * </p>
+	 * Returns the browser's version.
+	 *
 	 * <p>
 	 * Note that the version returned is defined as the real version of the 
 	 * browser software, not the one used to render the page. For example, the 
 	 * browser can be an IE10 but it may be using a compatibility view emulating 
-	 * IE8 to render the page. In such a case, this method returns "10.0", not 
-	 * "8.0".
+	 * IE8 to render the page. In such a case, this method would return
+	 * <code>10.0</code>, not <code>8.0</code>.
 	 * </p>
-	 * @param request
+	 *
+	 * @param request the servlet request
 	 * @return a String containing the version number
 	 */
 	public String getVersion(HttpServletRequest request);
