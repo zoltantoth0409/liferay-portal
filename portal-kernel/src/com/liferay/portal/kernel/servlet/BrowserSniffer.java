39eafd1777b29fc756faf58cc56d55c88f4072b5
mwilliams2014 <michael.williams@liferay.com>
2018-01-12 11:06:45 -0500

    LPS-69747 LRDOCS-4155 wordsmithing

--- portal-kernel/src/com/liferay/portal/kernel/servlet/BrowserSniffer.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/BrowserSniffer.java
@@ -40,10 +40,53 @@ public interface BrowserSniffer {
 
 	public BrowserMetadata getBrowserMetadata(HttpServletRequest request);
 
+	/**
+	 * <p>
+	 * Retrieves the browser's version number as a float, compared to 
+	 * {@link BrowserSniffer#getVersion(HttpServletRequest)} that retrieves
+	 * the version number as a {@link String}.
+	 * </p>
+	 * <p>
+	 * Note that the version returned is defined as the real version of the
+	 * browser software, not the one used to render the page. For example, the 
+	 * browser can be IE10 but it may be using a compatibility view emulating IE8 
+	 * to render the Page. In such a case, this method returns "10.0", not "8.0."
+	 * </p>
+	 * @param request
+	 * @return a float representing the version number
+	 * @see BrowserSniffer#getVersion(HttpServletRequest)
+	 */
 	public float getMajorVersion(HttpServletRequest request);
 
+	/**
+	 * <p>
+	 * Retrieves the browser's revision.
+	 * </p>
+	 * <p>
+	 * Note that the revision returned is defined as the real revision of the 
+	 * browser software, not the one used to render the page. For example, the 
+	 * browser can be IE10 but it may be using a compatibility view emulating IE8
+	 * to render the page. In such a case, this method returns "10.0", not "8.0".
+	 * </p>
+	 * @param request
+	 * @return a String containing the revision number
+	 */
 	public String getRevision(HttpServletRequest request);
 
+	/**
+	 * <p>
+	 * Retrieves the browser's version.
+	 * </p>
+	 * <p>
+	 * Note that the version returned is defined as the real version of the 
+	 * browser software, not the one used to render the page. For example, the 
+	 * browser can be an IE10 but it may be using a compatibility view emulating 
+	 * IE8 to render the page. In such a case, this method returns "10.0", not 
+	 * "8.0".
+	 * </p>
+	 * @param request
+	 * @return a String containing the version number
+	 */
 	public String getVersion(HttpServletRequest request);
 
 	public boolean isAir(HttpServletRequest request);
