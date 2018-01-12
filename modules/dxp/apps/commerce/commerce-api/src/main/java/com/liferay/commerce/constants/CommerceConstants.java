c267025b1e5162869d73c341d9701146fbac64fa
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-01-12 09:05:35 -0800

    COMMERCE-220 No need to explicitly use "long" here

--- modules/dxp/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/constants/CommerceConstants.java
+++ modules/dxp/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/constants/CommerceConstants.java
@@ -24,7 +24,7 @@ public class CommerceConstants {
 	public static final String SHIPPING_SERVICE_NAME =
 		"com.liferay.commerce.shipping";
 
-	public static final long WAREHOUSE_DEFAULT_ID = 0L;
+	public static final long WAREHOUSE_DEFAULT_ID = 0;
 
 	public static final String WAREHOUSE_DEFAULT_NAME = "default";
 
