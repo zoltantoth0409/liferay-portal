8a9dab1eb7c0d3a6337c91da5beea9765641c61d
Matthew Tambara <matthew.tambara@liferay.com>
2018-01-15 14:53:03 -0800

    LPS-75330 Move to internal

--- modules/apps/web-experience/asset/asset-service/src/main/java/com/liferay/asset/internal/util/AssetHelperImpl.java
+++ modules/apps/web-experience/asset/asset-service/src/main/java/com/liferay/asset/internal/util/AssetHelperImpl.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.asset.util.impl;
+package com.liferay.asset.internal.util;
 
 import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.model.AssetCategory;
