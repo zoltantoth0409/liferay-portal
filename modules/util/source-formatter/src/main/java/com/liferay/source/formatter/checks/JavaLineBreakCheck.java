a12867dfbfc10d58c8c405e9260cc8f55d4ea57f
Hugo Huijser <hugo.huijser@liferay.com>
2018-01-15 11:07:33 +0800

    LPS-74544 Fix infinite loop

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaLineBreakCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaLineBreakCheck.java
@@ -369,9 +369,9 @@ public class JavaLineBreakCheck extends LineBreakCheck {
 					matcher.start(8));
 			}
 
-			String firstTrailingNonWhitespace = matcher.group(12);
+			String firstTrailingNonWhitespace = matcher.group(13);
 
-			String trailingWhitespace = matcher.group(11);
+			String trailingWhitespace = matcher.group(12);
 
 			if (!trailingWhitespace.contains("\n") &&
 				!firstTrailingNonWhitespace.equals("}")) {
@@ -874,7 +874,7 @@ public class JavaLineBreakCheck extends LineBreakCheck {
 	private final Pattern _classOrEnumPattern = Pattern.compile(
 		"(\n(\t*)(private|protected|public) ((abstract|static) )*" +
 			"(class|enum|interface) ([\\s\\S]*?)\\{)((.*)\\})?" +
-				"(\\Z|\n(\\s*)(\\S))");
+				"([ \t]*(\\Z|\n)(\\s*)(\\S))");
 	private final Pattern _incorrectLineBreakInsideChainPattern1 =
 		Pattern.compile("\n(\t*)\\).*?\\((.+)");
 	private final Pattern _incorrectLineBreakInsideChainPattern2 =
