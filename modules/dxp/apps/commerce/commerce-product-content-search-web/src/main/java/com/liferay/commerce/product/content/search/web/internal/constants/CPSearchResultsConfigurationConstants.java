39c03cfd1ec25558b4ac22fad19cc3fdafd1d080
Alessio Rendina <asilcomando@gmail.com>
2018-01-16 22:00:44 +0100

    COMMERCE-288 Added search result configuration constants

---
+++ modules/dxp/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/constants/CPSearchResultsConfigurationConstants.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.content.search.web.internal.constants;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CPSearchResultsConfigurationConstants {
+
+	public static final String SHOW_CROSS_SELL_PRODUCTS =
+		"show-cross-sell-products";
+
+	public static final String SHOW_RELATED_PRODUCTS = "show-related-products";
+
+	public static final String SHOW_UP_SELL_PRODUCTS = "show-up-sell-products";
+
+	public static final String USE_CATEGORIES = "use-categories";
+
+}
