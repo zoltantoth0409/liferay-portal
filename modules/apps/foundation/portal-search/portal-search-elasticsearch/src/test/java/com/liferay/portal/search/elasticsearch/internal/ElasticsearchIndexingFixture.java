df76e1c92e4b9a9ee9e258d81ef3fef67b27d6c9
André de Oliveira <andre.oliveira@liferay.com>
2018-01-15 20:55:06 -0800

    LPS-76653 portal-search-elasticsearch: Replicate the bug with unit tests
    https://issues.liferay.com/browse/LPS-76653

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/ElasticsearchIndexingFixture.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/test/java/com/liferay/portal/search/elasticsearch/internal/ElasticsearchIndexingFixture.java
@@ -16,6 +16,8 @@ package com.liferay.portal.search.elasticsearch.internal;
 
 import com.liferay.portal.kernel.search.IndexSearcher;
 import com.liferay.portal.kernel.search.IndexWriter;
+import com.liferay.portal.kernel.search.suggest.QuerySuggester;
+import com.liferay.portal.kernel.util.Localization;
 import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.search.elasticsearch.connection.ElasticsearchConnectionManager;
@@ -57,7 +59,12 @@ import com.liferay.portal.search.elasticsearch.internal.query.TermQueryTranslato
 import com.liferay.portal.search.elasticsearch.internal.query.TermRangeQueryTranslatorImpl;
 import com.liferay.portal.search.elasticsearch.internal.query.WildcardQueryTranslatorImpl;
 import com.liferay.portal.search.elasticsearch.internal.stats.DefaultStatsTranslator;
+import com.liferay.portal.search.elasticsearch.internal.suggest.ElasticsearchSuggesterTranslator;
+import com.liferay.portal.search.elasticsearch.internal.suggest.PhraseSuggesterTranslatorImpl;
+import com.liferay.portal.search.elasticsearch.internal.suggest.TermSuggesterTranslatorImpl;
 import com.liferay.portal.search.test.util.indexing.IndexingFixture;
+import com.liferay.portal.util.DigesterImpl;
+import com.liferay.portal.util.LocalizationImpl;
 
 import org.elasticsearch.action.search.SearchRequestBuilder;
 
@@ -179,6 +186,56 @@ public class ElasticsearchIndexingFixture implements IndexingFixture {
 		};
 	}
 
+	protected QuerySuggester createElasticsearchQuerySuggester(
+		final ElasticsearchConnectionManager elasticsearchConnectionManager1,
+		final IndexNameBuilder indexNameBuilder1) {
+
+		return new ElasticsearchQuerySuggester() {
+			{
+				elasticsearchConnectionManager =
+					elasticsearchConnectionManager1;
+				indexNameBuilder = indexNameBuilder1;
+				localization = _localization;
+				suggesterTranslator = createElasticsearchSuggesterTranslator();
+			}
+		};
+	}
+
+	protected ElasticsearchSpellCheckIndexWriter
+		createElasticsearchSpellCheckIndexWriter(
+			final ElasticsearchConnectionManager
+				elasticsearchConnectionManager1,
+			final IndexNameBuilder indexNameBuilder1,
+			final ElasticsearchUpdateDocumentCommand
+				elasticsearchUpdateDocumentCommand1,
+			final IndexWriter indexWriter) {
+
+		return new ElasticsearchSpellCheckIndexWriter() {
+			{
+				elasticsearchConnectionManager =
+					elasticsearchConnectionManager1;
+				elasticsearchUpdateDocumentCommand =
+					elasticsearchUpdateDocumentCommand1;
+				digester = new DigesterImpl();
+				indexNameBuilder = indexNameBuilder1;
+				localization = _localization;
+
+				setIndexWriter(indexWriter);
+			}
+		};
+	}
+
+	protected ElasticsearchSuggesterTranslator
+		createElasticsearchSuggesterTranslator() {
+
+		return new ElasticsearchSuggesterTranslator() {
+			{
+				phraseSuggesterTranslator = new PhraseSuggesterTranslatorImpl();
+				termSuggesterTranslator = new TermSuggesterTranslatorImpl();
+			}
+		};
+	}
+
 	protected void createIndex() {
 		_indexCreator.createIndex(
 			new IndexName(_indexNameBuilder.getIndexName(_companyId)));
@@ -202,6 +259,10 @@ public class ElasticsearchIndexingFixture implements IndexingFixture {
 				searchHitDocumentTranslator =
 					new SearchHitDocumentTranslatorImpl();
 
+				setQuerySuggester(
+					createElasticsearchQuerySuggester(
+						elasticsearchConnectionManager, indexNameBuilder));
+
 				activate(
 					_elasticsearchFixture.
 						getElasticsearchConfigurationProperties());
@@ -213,27 +274,34 @@ public class ElasticsearchIndexingFixture implements IndexingFixture {
 		final ElasticsearchConnectionManager elasticsearchConnectionManager1,
 		final IndexNameBuilder indexNameBuilder1) {
 
-		final ElasticsearchUpdateDocumentCommand updateDocumentCommand =
-			new ElasticsearchUpdateDocumentCommandImpl() {
-				{
-					elasticsearchConnectionManager =
-						elasticsearchConnectionManager1;
-					elasticsearchDocumentFactory =
-						new DefaultElasticsearchDocumentFactory();
-					indexNameBuilder = indexNameBuilder1;
+		final ElasticsearchUpdateDocumentCommand
+			elasticsearchUpdateDocumentCommand1 =
+				new ElasticsearchUpdateDocumentCommandImpl() {
+					{
+						elasticsearchConnectionManager =
+							elasticsearchConnectionManager1;
+						elasticsearchDocumentFactory =
+							new DefaultElasticsearchDocumentFactory();
+						indexNameBuilder = indexNameBuilder1;
 
-					activate(
-						_elasticsearchFixture.
-							getElasticsearchConfigurationProperties());
-				}
-			};
+						activate(
+							_elasticsearchFixture.
+								getElasticsearchConfigurationProperties());
+					}
+				};
 
 		return new ElasticsearchIndexWriter() {
 			{
 				elasticsearchConnectionManager =
 					elasticsearchConnectionManager1;
-				elasticsearchUpdateDocumentCommand = updateDocumentCommand;
+				elasticsearchUpdateDocumentCommand =
+					elasticsearchUpdateDocumentCommand1;
 				indexNameBuilder = indexNameBuilder1;
+
+				setSpellCheckIndexWriter(
+					createElasticsearchSpellCheckIndexWriter(
+						elasticsearchConnectionManager1, indexNameBuilder1,
+						elasticsearchUpdateDocumentCommand1, this));
 			}
 		};
 	}
@@ -270,5 +338,6 @@ public class ElasticsearchIndexingFixture implements IndexingFixture {
 		new TestIndexNameBuilder();
 	private IndexSearcher _indexSearcher;
 	private IndexWriter _indexWriter;
+	private final Localization _localization = new LocalizationImpl();
 
 }
