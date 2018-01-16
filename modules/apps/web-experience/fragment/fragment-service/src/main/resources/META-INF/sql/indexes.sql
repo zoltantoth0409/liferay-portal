2eee2ab89e87d16f61b827f56f9542b5f21eb3ad
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-01-16 16:12:38 +0100

    LPS-77045 Removes index

--- modules/apps/web-experience/fragment/fragment-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/web-experience/fragment/fragment-service/src/main/resources/META-INF/sql/indexes.sql
@@ -2,7 +2,6 @@ create unique index IX_536510F5 on FragmentCollection (groupId, name[$COLUMN_LEN
 
 create index IX_DDB6278B on FragmentEntry (fragmentCollectionId, status);
 create unique index IX_18F9DFE on FragmentEntry (groupId, fragmentCollectionId, name[$COLUMN_LENGTH:75$]);
-create unique index IX_CACC7CC1 on FragmentEntry (groupId, name[$COLUMN_LENGTH:75$]);
 
 create index IX_CC38473A on FragmentEntryInstanceLink (groupId, fragmentEntryId);
 create index IX_2B2B7AB7 on FragmentEntryInstanceLink (groupId, layoutPageTemplateEntryId);
