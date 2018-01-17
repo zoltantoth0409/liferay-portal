54db37ee51190d9fb406ec38f721ff1c3c720aa2
William Newbury <william.newbury@liferay.com>
2018-01-16 16:43:52 -0800

    LPS-76026 Create UAD web portlet for use in migrating uad code out of users-admin

---
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/portlet/UserAssociatedDataBrowsePortletProvider.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package liferay.user.associated.data.web.portlet;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
+import com.liferay.portal.kernel.portlet.BrowsePortletProvider;
+import com.liferay.user.associated.data.constants.UserAssociatedDataPortletKeys;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author William Newbury
+ */
+@Component(
+	immediate = true,
+	property = {"model.class.name=com.liferay.user.associated.data.entity.UADEntity"},
+	service = BrowsePortletProvider.class
+)
+public class UserAssociatedDataBrowsePortletProvider
+	extends BasePortletProvider implements BrowsePortletProvider {
+
+	@Override
+	public String getPortletName() {
+		return UserAssociatedDataPortletKeys.USER_ASSOCIATED_DATA;
+	}
+
+	@Override
+	public PortletURL getPortletURL(HttpServletRequest request)
+		throws PortalException {
+
+		PortletURL portletURL = super.getPortletURL(request);
+
+		portletURL.setParameter(
+			"mvcRenderCommandName",
+			"/users_admin/manage_user_associated_data_bundles");
+
+		return portletURL;
+	}
+
+}
