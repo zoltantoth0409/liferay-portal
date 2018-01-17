48ebfe9dc409a2433fbf79e5bd21b98f1ba44d3d
Inacio Nery <inacio.nery@liferay.com>
2018-01-17 11:46:18 -0300

    LPS-77249 Add a property key to be used as the analytics key

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -19,6 +19,8 @@ package com.liferay.portal.kernel.util;
  */
 public interface PropsKeys {
 
+	public static final String ADMIN_ANALYTICS_KEY = "admin.analytics.key";
+
 	public static final String ADMIN_ANALYTICS_TYPES = "admin.analytics.types";
 
 	public static final String ADMIN_DEFAULT_GROUP_NAMES = "admin.default.group.names";
