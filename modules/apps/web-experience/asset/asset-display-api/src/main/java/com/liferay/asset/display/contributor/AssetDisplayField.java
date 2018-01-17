25e839ed6daf9184862251a4b4ca7e28d70a377c
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-01-17 11:42:43 +0100

    LPS-77245 Interface for Asset Display Contributor

---
+++ modules/apps/web-experience/asset/asset-display-api/src/main/java/com/liferay/asset/display/contributor/AssetDisplayField.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.display.contributor;
+
+import com.liferay.portal.kernel.util.HashUtil;
+
+import java.io.Serializable;
+
+import java.util.Objects;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class AssetDisplayField implements Serializable {
+
+	public AssetDisplayField(String key, Serializable label) {
+		_key = key;
+		_label = label;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof AssetDisplayField)) {
+			return false;
+		}
+
+		AssetDisplayField assetDisplayField = (AssetDisplayField)obj;
+
+		if (Objects.equals(getKey(), assetDisplayField.getKey()) &&
+			Objects.equals(getLabel(), assetDisplayField.getLabel())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public String getKey() {
+		return _key;
+	}
+
+	public Serializable getLabel() {
+		return _label;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = HashUtil.hash(0, _key);
+
+		return HashUtil.hash(hash, _label);
+	}
+
+	private final String _key;
+	private final Serializable _label;
+
+}
