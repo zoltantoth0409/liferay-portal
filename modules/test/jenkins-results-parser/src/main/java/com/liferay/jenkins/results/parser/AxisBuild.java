42eddfffdb76ca73b9fb0a2fbea73f8fb8266a4b
Kenji Heigel <kenji.heigel@liferay.com>
2018-01-18 13:59:34 -0800

    LRQA-37652 'Fixed' is another test status in the family of passing tests

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/AxisBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/AxisBuild.java
@@ -281,7 +281,7 @@ public class AxisBuild extends BaseBuild {
 			for (TestResult testResult : getTestResults(null)) {
 				String testStatus = testResult.getStatus();
 
-				if (testStatus.equals("PASSED") ||
+				if (testStatus.equals("FIXED") || testStatus.equals("PASSED") ||
 					testStatus.equals("SKIPPED")) {
 
 					continue;
