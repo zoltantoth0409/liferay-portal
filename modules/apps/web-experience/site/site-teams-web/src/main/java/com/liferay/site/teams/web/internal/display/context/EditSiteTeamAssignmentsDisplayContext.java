c98fada4702f44bbdb9888241d7b7e89c95fbee0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-01-18 11:39:40 +0100

    LPS-77200 Moves code to display context object

---
+++ modules/apps/web-experience/site/site-teams-web/src/main/java/com/liferay/site/teams/web/internal/display/context/EditSiteTeamAssignmentsDisplayContext.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.teams.web.internal.display.context;
+
+import com.liferay.portal.kernel.model.Team;
+import com.liferay.portal.kernel.service.TeamLocalServiceUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class EditSiteTeamAssignmentsDisplayContext {
+
+	public EditSiteTeamAssignmentsDisplayContext(
+			RenderRequest renderRequest, RenderResponse renderResponse,
+			HttpServletRequest request)
+		throws Exception {
+
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+		_request = request;
+	}
+
+	public PortletURL getEditTeamAssignmentsURL() {
+		PortletURL portletURL = _renderResponse.createRenderURL();
+
+		portletURL.setParameter("mvcPath", "/edit_team_assignments.jsp");
+		portletURL.setParameter("tabs1", getTabs1());
+		portletURL.setParameter("teamId", String.valueOf(getTeamId()));
+
+		return portletURL;
+	}
+
+	public String getTabs1() {
+		if (_tabs1 != null) {
+			return _tabs1;
+		}
+
+		_tabs1 = ParamUtil.getString(_request, "tabs1", "users");
+
+		return _tabs1;
+	}
+
+	public Team getTeam() {
+		if (_team != null) {
+			return _team;
+		}
+
+		_team = TeamLocalServiceUtil.fetchTeam(getTeamId());
+
+		return _team;
+	}
+
+	public long getTeamId() {
+		if (_teamId != null) {
+			return _teamId;
+		}
+
+		_teamId = ParamUtil.getLong(_request, "teamId");
+
+		return _teamId;
+	}
+
+	public String getTeamName() {
+		if (_teamName != null) {
+			return _teamName;
+		}
+
+		Team team = getTeam();
+
+		_teamName = team.getName();
+
+		return _teamName;
+	}
+
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+	private final HttpServletRequest _request;
+	private String _tabs1;
+	private Team _team;
+	private Long _teamId;
+	private String _teamName;
+
+}
