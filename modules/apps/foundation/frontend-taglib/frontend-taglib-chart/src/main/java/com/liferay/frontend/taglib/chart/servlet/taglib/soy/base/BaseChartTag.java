d5c62168a1e42767d26cf19398b33a91b2c943dc
Iván Zaera Avellón <ivan.zaera@liferay.com>
2018-01-19 13:19:45 +0100

    LPS-76773 Change attributes of chart taglibs to adapt them to the new configuration model

--- modules/apps/foundation/frontend-taglib/frontend-taglib-chart/src/main/java/com/liferay/frontend/taglib/chart/servlet/taglib/soy/base/BaseChartTag.java
+++ modules/apps/foundation/frontend-taglib/frontend-taglib-chart/src/main/java/com/liferay/frontend/taglib/chart/servlet/taglib/soy/base/BaseChartTag.java
@@ -16,6 +16,7 @@ package com.liferay.frontend.taglib.chart.servlet.taglib.soy.base;
 
 import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
 import com.liferay.frontend.taglib.chart.internal.js.loader.modules.extender.npm.NPMResolverProvider;
+import com.liferay.frontend.taglib.chart.model.ChartConfig;
 import com.liferay.frontend.taglib.soy.servlet.taglib.TemplateRendererTag;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.servlet.taglib.util.OutputData;
@@ -27,6 +28,8 @@ import com.liferay.portal.kernel.util.WebKeys;
 import java.io.IOException;
 
 import java.net.URL;
+
+import java.util.Map;
 
 import javax.servlet.ServletRequest;
 
@@ -64,12 +67,10 @@ public abstract class BaseChartTag extends TemplateRendererTag {
 			"clay-charts/lib/" + _moduleBaseName);
 	}
 
-	public void setColumns(Object columns) {
-		putValue("columns", columns);
-	}
-
-	public void setGroups(Object groups) {
-		putValue("groups", groups);
+	public void setConfig(ChartConfig config) {
+		for (Map.Entry<String, Object> entry : config.entrySet()) {
+			putValue(entry.getKey(), entry.getValue());
+		}
 	}
 
 	public void setId(String id) {
