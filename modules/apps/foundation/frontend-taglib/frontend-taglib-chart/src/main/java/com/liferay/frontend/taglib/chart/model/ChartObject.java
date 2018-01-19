c54fcf64058fd575ab4c550a30ab18ca88d6d5f7
Iván Zaera Avellón <ivan.zaera@liferay.com>
2018-01-19 13:18:24 +0100

    LPS-76773 Create a helper class to customize serialization to JSON (avoiding nulls, for example)

---
+++ modules/apps/foundation/frontend-taglib/frontend-taglib-chart/src/main/java/com/liferay/frontend/taglib/chart/model/ChartObject.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.chart.model;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+public abstract class ChartObject extends AbstractMap<String, Object> {
+
+	@Override
+	public Set<Entry<String, Object>> entrySet() {
+		return _properties.entrySet();
+	}
+
+	protected <T> T get(String name, Class<T> type) {
+		return get(name, type, true);
+	}
+
+	protected <T> T get(String name, Class<T> type, boolean createIfNotFound) {
+		T value = (T)_properties.get(name);
+
+		if ((value == null) && createIfNotFound) {
+			try {
+				value = type.newInstance();
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+
+			_properties.put(name, value);
+		}
+
+		return value;
+	}
+
+	protected void set(String name, Object value) {
+		if (value == null) {
+			throw new UnsupportedOperationException(
+				"Property " + name + " cannot be set to null");
+		}
+
+		_properties.put(name, value);
+	}
+
+	private final Map<String, Object> _properties = new HashMap<>();
+
+}
