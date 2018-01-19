27e0d6401a2a3acae1b1f43181976ce748c8302b
Alessio Rendina <asilcomando@gmail.com>
2018-01-19 17:41:31 +0100

    COMMERCE-2 Fixed category screen navigation entry label

--- modules/dxp/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/servlet/taglib/ui/CategoryCPDefinitionScreenNavigationEntry.java
+++ modules/dxp/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/servlet/taglib/ui/CategoryCPDefinitionScreenNavigationEntry.java
@@ -26,13 +26,11 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
 
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -66,10 +64,7 @@ public class CategoryCPDefinitionScreenNavigationEntry
 
 	@Override
 	public String getLabel(Locale locale) {
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-			"content.Language", locale, getClass());
-
-		return LanguageUtil.get(resourceBundle, "products");
+		return LanguageUtil.get(locale, "products");
 	}
 
 	@Override
