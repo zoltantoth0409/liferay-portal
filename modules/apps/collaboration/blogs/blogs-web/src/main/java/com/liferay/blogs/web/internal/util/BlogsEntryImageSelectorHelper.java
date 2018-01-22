db90a2de51c23b1d0ee8c9e87c93b46aea64c72e
Matthew Tambara <matthew.tambara@liferay.com>
2018-01-22 09:19:17 -0800

    LPS-75295 Move to web

--- modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/util/BlogsEntryImageSelectorHelper.java
+++ modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/util/BlogsEntryImageSelectorHelper.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.blogs.util;
+package com.liferay.blogs.web.internal.util;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
