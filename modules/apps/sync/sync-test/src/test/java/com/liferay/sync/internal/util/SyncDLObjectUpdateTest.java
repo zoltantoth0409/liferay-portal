9ca8a46353805530250d5a12ddfd24c8f2ce72eb
Matthew Tambara <matthew.tambara@liferay.com>
2018-01-23 11:59:39 -0800

    LPS-77401 Move test

--- modules/apps/sync/sync-test/src/test/java/com/liferay/sync/internal/util/SyncDLObjectUpdateTest.java
+++ modules/apps/sync/sync-test/src/test/java/com/liferay/sync/internal/util/SyncDLObjectUpdateTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.sync.util;
+package com.liferay.sync.internal.util;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.ConfigurationFactoryImpl;
