a3026a697c25b6e485db1620510393c1676de2d1
Kenji Heigel <kenji.heigel@liferay.com>
2018-01-22 17:44:57 -0800

    LRQA-38150 Remove poshi test result specific methods from TestResult interface

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestResult.java
@@ -25,8 +25,6 @@ public interface TestResult {
 
 	public String getClassName();
 
-	public String getConsoleOutputURL(String testrayLogsURL);
-
 	public String getDisplayName();
 
 	public long getDuration();
@@ -35,20 +33,12 @@ public interface TestResult {
 
 	public Element getGitHubElement(String testrayLogsURL);
 
-	public String getLiferayLogURL(String testrayLogsURL);
-
 	public String getPackageName();
-
-	public String getPoshiReportURL(String testrayLogsURL);
-
-	public String getPoshiSummaryURL(String testrayLogsURL);
 
 	public String getStatus();
 
 	public String getTestName();
 
 	public String getTestReportURL();
-
-	public boolean hasLiferayLog(String testrayLogsURL);
 
 }
