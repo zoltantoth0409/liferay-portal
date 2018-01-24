16ce1f80c80fe36eac9c78883e162257c89b04e2
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-01-24 15:19:15 +0100

    LPS-77419 Fix typo

--- modules/apps/collaboration/comment/comment-analytics/src/main/java/com/liferay/comment/analytics/internal/servlet/taglib/CommentAnalyticsDynamicInclude.java
+++ modules/apps/collaboration/comment/comment-analytics/src/main/java/com/liferay/comment/analytics/internal/servlet/taglib/CommentAnalyticsDynamicInclude.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.comment.analytics.internal.server.taglib;
+package com.liferay.comment.analytics.internal.servlet.taglib;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
