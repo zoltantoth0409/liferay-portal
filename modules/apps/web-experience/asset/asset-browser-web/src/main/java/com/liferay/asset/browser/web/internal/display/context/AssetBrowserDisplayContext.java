57fef7886d5ea73e5c54d7b1461197720a2dec18
Brian Chan <brian.chan@liferay.com>
2018-01-24 15:04:49 -0800

    LPS-77038 SF

--- modules/apps/web-experience/asset/asset-browser-web/src/main/java/com/liferay/asset/browser/web/internal/display/context/AssetBrowserDisplayContext.java
+++ modules/apps/web-experience/asset/asset-browser-web/src/main/java/com/liferay/asset/browser/web/internal/display/context/AssetBrowserDisplayContext.java
@@ -371,18 +371,18 @@ public class AssetBrowserDisplayContext {
 
 		portletURL.setParameter("groupId", String.valueOf(getGroupId()));
 
-		long[] selectedGroupIds = getSelectedGroupIds();
-
-		if (selectedGroupIds.length > 0) {
-			portletURL.setParameter(
-				"selectedGroupIds", StringUtil.merge(selectedGroupIds));
-		}
-
 		long selectedGroupId = ParamUtil.getLong(_request, "selectedGroupId");
 
 		if (selectedGroupId > 0) {
 			portletURL.setParameter(
 				"selectedGroupId", String.valueOf(selectedGroupId));
+		}
+
+		long[] selectedGroupIds = getSelectedGroupIds();
+
+		if (selectedGroupIds.length > 0) {
+			portletURL.setParameter(
+				"selectedGroupIds", StringUtil.merge(selectedGroupIds));
 		}
 
 		portletURL.setParameter(
