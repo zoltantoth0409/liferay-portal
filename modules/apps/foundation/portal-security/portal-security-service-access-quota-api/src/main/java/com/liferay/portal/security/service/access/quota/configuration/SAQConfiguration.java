79ed70d407ff1a2e3b88bbf8ac3a0afdfdcc32fc
Hugo Huijser <hugo.huijser@liferay.com>
2018-01-25 12:22:45 +0800

    LPS-77459 No need for description attribute unless we have additional information (other than name)

--- modules/apps/foundation/portal-security/portal-security-service-access-quota-api/src/main/java/com/liferay/portal/security/service/access/quota/configuration/SAQConfiguration.java
+++ modules/apps/foundation/portal-security/portal-security-service-access-quota-api/src/main/java/com/liferay/portal/security/service/access/quota/configuration/SAQConfiguration.java
@@ -34,27 +34,25 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 public interface SAQConfiguration {
 
 	@Meta.AD(
-		deflt = "", description = "saq-configuration-service-signature",
-		name = "service-signature", required = false
+		deflt = "", name = "saq-configuration-service-signature",
+		required = false
 	)
 	public String serviceSignature();
 
 	@Meta.AD(
-		deflt = "60", description = "saq-configuration-service-max", min = "1",
-		name = "max", required = true
+		deflt = "60", name = "saq-configuration-service-max", min = "1",
+		required = true
 	)
 	public int max();
 
 	@Meta.AD(
-		deflt = "60000",
-		description = "saq-configuration-service-interval-millis",
-		name = "interval-millis", required = true
+		deflt = "60000", name = "saq-configuration-service-interval-millis",
+		required = true
 	)
 	public long intervalMillis();
 
 	@Meta.AD(
-		deflt = "", description = "saq-configuration-service-metric",
-		name = "metrics", required = false
+		deflt = "", name = "saq-configuration-service-metrics", required = false
 	)
 	public String[] metrics();
 
