058dd867191fddac965c653a95e0826c8fda8463
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-01-25 14:55:10 -0800

    LPS-77457 Fix 404: main.css not found (unrelated)

--- modules/apps/foundation/expando/expando-web/src/main/java/com/liferay/expando/web/internal/portlet/ExpandoPortlet.java
+++ modules/apps/foundation/expando/expando-web/src/main/java/com/liferay/expando/web/internal/portlet/ExpandoPortlet.java
@@ -69,7 +69,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-expando",
 		"com.liferay.portlet.display-category=category.hidden",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.header-portlet-css=/css/main.scss",
 		"com.liferay.portlet.icon=/icons/expando.png",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
