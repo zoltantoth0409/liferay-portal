ed658436eedbf3425598cfad2337eee32d90eeb7
Alessio Rendina <asilcomando@gmail.com>
2018-01-26 21:47:20 +0100

    COMMERCE-280 Added exchange rate provider group service configuration

---
+++ modules/dxp/apps/commerce/commerce-currency-web/src/main/java/com/liferay/commerce/currency/web/internal/configuration/ExchangeRateProviderGroupServiceConfiguration.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.currency.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@ExtendedObjectClassDefinition(
+	category = "commerce", scope = ExtendedObjectClassDefinition.Scope.GROUP
+)
+@Meta.OCD(
+	id = "com.liferay.commerce.currency.web.internal.configuration.ExchangeRateProviderGroupServiceConfiguration",
+	localization = "content/Language",
+	name = "exchange-rate-group-service-configuration-name"
+)
+public interface ExchangeRateProviderGroupServiceConfiguration {
+
+	@Meta.AD(deflt = "false", required = false)
+	public boolean autoUpdate();
+
+	@Meta.AD(required = false)
+	public String defaultExchangeRateProviderKey();
+
+	@Meta.AD(deflt = "0", required = false)
+	public long groupId();
+
+	@Meta.AD(deflt = "60", required = false)
+	public int updateInterval();
+
+}
