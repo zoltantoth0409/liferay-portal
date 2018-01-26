adfaca301c1b1c84af9112173208dc4c5951ef7e
Máté Thurzó <mate.thurzo@liferay.com>
2018-01-26 16:07:57 +0100

    LPS-77607 Add changeset manager implementation to a new bundle

---
+++ modules/apps/web-experience/export-import/export-import-changeset-service/src/main/java/com/liferay/exportimport/changeset/internal/manager/ChangesetManagerImpl.java
@@ -0,0 +1,172 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.changeset.internal.manager;
+
+import com.liferay.exportimport.changeset.Changeset;
+import com.liferay.exportimport.changeset.ChangesetEnvironment;
+import com.liferay.exportimport.changeset.ChangesetManager;
+import com.liferay.exportimport.changeset.constants.ChangesetPortletKeys;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationConstants;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationParameterMapFactory;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationSettingsMapFactory;
+import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
+import com.liferay.exportimport.kernel.service.ExportImportConfigurationLocalService;
+import com.liferay.exportimport.kernel.staging.Staging;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.io.Serializable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mate Thurzo
+ */
+@Component(immediate = true, service = ChangesetManager.class)
+public class ChangesetManagerImpl implements ChangesetManager {
+
+	@Override
+	public void addChangeset(Changeset changeset) {
+		Objects.nonNull(changeset);
+
+		String changesetUuid = changeset.getUuid();
+
+		if (_changesets.containsKey(changesetUuid)) {
+			return;
+		}
+
+		_changesets.put(changesetUuid, changeset);
+	}
+
+	@Override
+	public void clearChangesets() {
+		_changesets = new HashMap<>();
+	}
+
+	@Override
+	public boolean hasChangeset(String changesetUuid) {
+		return _changesets.containsKey(changesetUuid);
+	}
+
+	@Override
+	public Optional<Changeset> peekChangeset(String changesetUuid) {
+		return Optional.ofNullable(_changesets.get(changesetUuid));
+	}
+
+	@Override
+	public Optional<Changeset> popChangeset(String changesetUuid) {
+		Changeset changeset = _changesets.remove(changesetUuid);
+
+		return Optional.ofNullable(changeset);
+	}
+
+	@Override
+	public void publishChangeset(
+		Changeset changeset, ChangesetEnvironment environment) {
+
+		addChangeset(changeset);
+
+		long groupId = environment.getGroupId();
+
+		Group scopeGroup = _groupLocalService.fetchGroup(groupId);
+
+		if (!scopeGroup.isStagingGroup() && !scopeGroup.isStagedRemotely()) {
+			return;
+		}
+
+		String portletId = environment.getPortletId();
+
+		if (!scopeGroup.isStagedPortlet(portletId)) {
+			return;
+		}
+
+		long liveGroupId = 0;
+
+		if (scopeGroup.isStagingGroup()) {
+			liveGroupId = scopeGroup.getLiveGroupId();
+		}
+		else if (scopeGroup.isStagedRemotely()) {
+			liveGroupId = scopeGroup.getRemoteLiveGroupId();
+		}
+
+		Map<String, String[]> parameterMap =
+			ExportImportConfigurationParameterMapFactory.buildParameterMap();
+
+		parameterMap.put("changesetUuid", new String[] {changeset.getUuid()});
+
+		Map<String, String> envParameterMap = environment.getParameterMap();
+
+		Set<Map.Entry<String, String>> entrySet = envParameterMap.entrySet();
+
+		Stream<Map.Entry<String, String>> entryStream = entrySet.stream();
+
+		entryStream.forEach(
+			entry -> {
+				parameterMap.put(
+					entry.getKey(), new String[] {entry.getValue()});
+			});
+
+		User user = _userLocalService.fetchUser(environment.getUserId());
+
+		Map<String, Serializable> settingsMap =
+			ExportImportConfigurationSettingsMapFactory.
+				buildPublishPortletSettingsMap(
+					user, groupId, environment.getPlid(), liveGroupId,
+					environment.getPlid(), ChangesetPortletKeys.CHANGESET,
+					parameterMap);
+
+		try {
+			ExportImportConfiguration exportImportConfiguration =
+				_exportImportConfigurationLocalService.
+					addDraftExportImportConfiguration(
+						user.getUserId(), portletId,
+						ExportImportConfigurationConstants.TYPE_PUBLISH_PORTLET,
+						settingsMap);
+
+			long backgroundTaskId = _staging.publishPortlet(
+				user.getUserId(), exportImportConfiguration);
+		}
+		catch (PortalException pe) {
+			pe.printStackTrace();
+		}
+	}
+
+	private Map<String, Changeset> _changesets = new HashMap<>();
+
+	@Reference
+	private ExportImportConfigurationLocalService
+		_exportImportConfigurationLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Staging _staging;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
