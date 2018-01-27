d47ee5d360946be81ed23c8d0ebc6b2e80352a95
Jorge Ferrer <jorge.ferrer@liferay.com>
2018-01-27 22:06:53 +0100

    LPS-75377 Create a component for each of the categories of the new UI

---
+++ modules/apps/foundation/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/categories/ThirdPartyUtilitiesConfigurationCategory.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.internal.categories;
+
+import com.liferay.configuration.admin.categories.ConfigurationCategory;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jorge Ferrer
+ */
+@Component
+public class ThirdPartyUtilitiesConfigurationCategory
+	implements ConfigurationCategory {
+
+	public static final String KEY = "third-party-utilities";
+
+	@Override
+	public String getCategorySetKey() {
+		return _CATEGORY_SET_KEY;
+	}
+
+	@Override
+	public String getIcon() {
+		return "magic";
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	private static final String _CATEGORY_SET_KEY = "platform";
+
+}
