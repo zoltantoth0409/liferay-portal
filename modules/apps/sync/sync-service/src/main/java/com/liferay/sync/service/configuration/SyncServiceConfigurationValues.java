8d4e96a65f5ce50432abf92d53bd5ab7c7cf99a6
Matthew Tambara <matthew.tambara@liferay.com>
2018-01-29 16:47:42 -0800

    LPS-77401 Remove deprecated

--- modules/apps/sync/sync-service/src/main/java/com/liferay/sync/service/configuration/SyncServiceConfigurationValues.java
+++ modules/apps/sync/sync-service/src/main/java/com/liferay/sync/service/configuration/SyncServiceConfigurationValues.java
@@ -79,16 +79,6 @@ public class SyncServiceConfigurationValues {
 				SyncServiceConfigurationKeys.
 					SYNC_FILE_CHECKSUM_THRESHOLD_SIZE));
 
-	/**
-	 * @deprecated As of 1.3.0, with no direct replacement
-	 */
-	@Deprecated
-	public static final int SYNC_FILE_DIFF_CACHE_DELETE_INTERVAL =
-		GetterUtil.getInteger(
-			SyncServiceConfigurationUtil.get(
-				SyncServiceConfigurationKeys.
-					SYNC_FILE_DIFF_CACHE_DELETE_INTERVAL));
-
 	public static final boolean SYNC_FILE_DIFF_CACHE_ENABLED =
 		GetterUtil.getBoolean(
 			SyncServiceConfigurationUtil.get(
