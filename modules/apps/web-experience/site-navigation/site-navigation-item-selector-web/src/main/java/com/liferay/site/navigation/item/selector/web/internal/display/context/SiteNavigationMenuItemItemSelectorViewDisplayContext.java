be46b87ef070e682cfac0fe95fbb9e991ac7ebd4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-01-30 19:04:16 +0100

    LPS-77540 Adds new methods to get site navigation menu item tree elements

--- modules/apps/web-experience/site-navigation/site-navigation-item-selector-web/src/main/java/com/liferay/site/navigation/item/selector/web/internal/display/context/SiteNavigationMenuItemItemSelectorViewDisplayContext.java
+++ modules/apps/web-experience/site-navigation/site-navigation-item-selector-web/src/main/java/com/liferay/site/navigation/item/selector/web/internal/display/context/SiteNavigationMenuItemItemSelectorViewDisplayContext.java
@@ -14,7 +14,20 @@
 
 package com.liferay.site.navigation.item.selector.web.internal.display.context;
 
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.site.navigation.model.SiteNavigationMenu;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalServiceUtil;
+import com.liferay.site.navigation.service.SiteNavigationMenuLocalServiceUtil;
+import com.liferay.site.navigation.type.SiteNavigationMenuItemType;
 import com.liferay.site.navigation.type.SiteNavigationMenuItemTypeRegistry;
+
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -33,8 +46,108 @@ public class SiteNavigationMenuItemItemSelectorViewDisplayContext {
 			siteNavigationMenuItemTypeRegistry;
 	}
 
+	public String getItemSelectedEventName() {
+		return _itemSelectedEventName;
+	}
+
+	public JSONArray getSiteNavigationMenuItemsJSONArray() throws Exception {
+		long siteNavigationMenuId = ParamUtil.getLong(
+			_request, "siteNavigationMenuId");
+
+		SiteNavigationMenu siteNavigationMenu =
+			SiteNavigationMenuLocalServiceUtil.fetchSiteNavigationMenu(
+				siteNavigationMenuId);
+
+		JSONArray rootJSONArray = JSONFactoryUtil.createJSONArray();
+
+		JSONArray jsonArray = _getSiteNavigationMenuItemsJSONArray(
+			siteNavigationMenuId, 0);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		jsonObject.put("children", jsonArray);
+		jsonObject.put("disabled", true);
+		jsonObject.put("icon", "blogs");
+		jsonObject.put("id", "0");
+
+		if (siteNavigationMenu != null) {
+			jsonObject.put("name", siteNavigationMenu.getName());
+		}
+
+		rootJSONArray.put(jsonObject);
+
+		return rootJSONArray;
+	}
+
+	private long _getSiteNavigationMenuItemId() {
+		if (_siteNavigationMenuItemId != null) {
+			return _siteNavigationMenuItemId;
+		}
+
+		_siteNavigationMenuItemId = ParamUtil.getLong(
+			_request, "siteNavigationMenuItemId");
+
+		return _siteNavigationMenuItemId;
+	}
+
+	private JSONArray _getSiteNavigationMenuItemsJSONArray(
+			long siteNavigationMenuId, long parentSiteNavigationMenuItemId)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		List<SiteNavigationMenuItem> siteNavigationMenuItems =
+			SiteNavigationMenuItemLocalServiceUtil.getSiteNavigationMenuItems(
+				siteNavigationMenuId, parentSiteNavigationMenuItemId);
+
+		for (SiteNavigationMenuItem siteNavigationMenuItem :
+				siteNavigationMenuItems) {
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			JSONArray childrenJSONArray = _getSiteNavigationMenuItemsJSONArray(
+				siteNavigationMenuId,
+				siteNavigationMenuItem.getSiteNavigationMenuItemId());
+
+			if (childrenJSONArray.length() > 0) {
+				jsonObject.put("children", childrenJSONArray);
+			}
+
+			SiteNavigationMenuItemType siteNavigationMenuItemType =
+				_siteNavigationMenuItemTypeRegistry.
+					getSiteNavigationMenuItemType(
+						siteNavigationMenuItem.getType());
+
+			jsonObject.put("icon", siteNavigationMenuItemType.getIcon());
+
+			jsonObject.put(
+				"id",
+				String.valueOf(
+					siteNavigationMenuItem.getSiteNavigationMenuItemId()));
+
+			jsonObject.put(
+				"name",
+				siteNavigationMenuItemType.getTitle(
+					siteNavigationMenuItem, themeDisplay.getLocale()));
+
+			if (siteNavigationMenuItem.getSiteNavigationMenuItemId() ==
+					_getSiteNavigationMenuItemId()) {
+
+				jsonObject.put("selected", true);
+			}
+
+			jsonArray.put(jsonObject);
+		}
+
+		return jsonArray;
+	}
+
 	private final String _itemSelectedEventName;
 	private final HttpServletRequest _request;
+	private Long _siteNavigationMenuItemId;
 	private final SiteNavigationMenuItemTypeRegistry
 		_siteNavigationMenuItemTypeRegistry;
 
