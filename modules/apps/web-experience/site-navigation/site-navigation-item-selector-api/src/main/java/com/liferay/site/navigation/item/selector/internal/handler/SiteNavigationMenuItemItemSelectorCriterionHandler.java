1a314b261dc3071717697b36d29b7709a48e6747
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-01-30 12:36:23 +0100

    LPS-77540 Adds new criterion for site navigation menu item

---
+++ modules/apps/web-experience/site-navigation/site-navigation-item-selector-api/src/main/java/com/liferay/site/navigation/item/selector/internal/handler/SiteNavigationMenuItemItemSelectorCriterionHandler.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.item.selector.internal.handler;
+
+import com.liferay.item.selector.BaseItemSelectorCriterionHandler;
+import com.liferay.item.selector.ItemSelectorCriterionHandler;
+import com.liferay.site.navigation.item.selector.criterion.SiteNavigationMenuItemItemSelectorCriterion;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = ItemSelectorCriterionHandler.class)
+public class SiteNavigationMenuItemItemSelectorCriterionHandler
+	extends BaseItemSelectorCriterionHandler
+		<SiteNavigationMenuItemItemSelectorCriterion> {
+
+	@Override
+	public Class<SiteNavigationMenuItemItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return SiteNavigationMenuItemItemSelectorCriterion.class;
+	}
+
+	@Activate
+	@Override
+	protected void activate(BundleContext bundleContext) {
+		super.activate(bundleContext);
+	}
+
+	@Deactivate
+	@Override
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+}
