c56b751e4403538e586dec6e7412f70d8d200016
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-01-30 12:47:19 +0100

    LPS-77540 Copy SiteNavigationMenuItemItemSelectorView from SiteNavigationMenuItemSelectorView, no logic changes

---
+++ modules/apps/web-experience/site-navigation/site-navigation-item-selector-web/src/main/java/com/liferay/site/navigation/item/selector/web/internal/SiteNavigationMenuItemItemSelectorView.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.item.selector.web.internal;
+
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.site.navigation.item.selector.criterion.SiteNavigationMenuItemSelectorCriterion;
+import com.liferay.site.navigation.item.selector.web.internal.constants.SiteNavigationItemSelectorWebKeys;
+import com.liferay.site.navigation.item.selector.web.internal.display.context.SiteNavigationMenuItemSelectorViewDisplayContext;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	property = {"item.selector.view.order:Integer=300"},
+	service = ItemSelectorView.class
+)
+public class SiteNavigationMenuItemItemSelectorView
+	implements ItemSelectorView<SiteNavigationMenuItemSelectorCriterion> {
+
+	@Override
+	public Class<SiteNavigationMenuItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return SiteNavigationMenuItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		ResourceBundle resourceBundle =
+			_resourceBundleLoader.loadResourceBundle(locale);
+
+		return ResourceBundleUtil.getString(resourceBundle, "navigation-menus");
+	}
+
+	@Override
+	public boolean isShowSearch() {
+		return true;
+	}
+
+	@Override
+	public boolean isVisible(ThemeDisplay themeDisplay) {
+		return true;
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest request, ServletResponse response,
+			SiteNavigationMenuItemSelectorCriterion
+				siteNavigationMenuItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		SiteNavigationMenuItemSelectorViewDisplayContext
+			siteNavigationMenuItemSelectorViewDisplayContext =
+				new SiteNavigationMenuItemSelectorViewDisplayContext(
+					(HttpServletRequest)request, portletURL,
+					itemSelectedEventName);
+
+		request.setAttribute(
+			SiteNavigationItemSelectorWebKeys.
+				SITE_NAVIGATION_MENU_ITEM_SELECTOR_DISPLAY_CONTEXT,
+			siteNavigationMenuItemSelectorViewDisplayContext);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher(
+				"/view_site_navigation_menus.jsp");
+
+		requestDispatcher.include(request, response);
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.unmodifiableList(
+			ListUtil.fromArray(
+				new ItemSelectorReturnType[] {
+					new UUIDItemSelectorReturnType()
+				}));
+
+	@Reference(
+		target = "(bundle.symbolic.name=com.liferay.site.navigation.item.selector.web)"
+	)
+	private ResourceBundleLoader _resourceBundleLoader;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.site.navigation.item.selector.web)"
+	)
+	private ServletContext _servletContext;
+
+}
