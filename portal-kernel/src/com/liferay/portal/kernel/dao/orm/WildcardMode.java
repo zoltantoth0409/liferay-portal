62076963b7adbf5e471f50ae8ccdd1fc6e1545bf
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2018-01-31 13:34:12 +0100

    LPS-77654 encapsulate logic inside enum.

--- portal-kernel/src/com/liferay/portal/kernel/dao/orm/WildcardMode.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/orm/WildcardMode.java
@@ -14,11 +14,31 @@
 
 package com.liferay.portal.kernel.dao.orm;
 
+import com.liferay.portal.kernel.util.StringPool;
+
 /**
  * @author Adolfo Pérez
  */
 public enum WildcardMode {
 
-	LEADING, SURROUND, TRAILING
+	LEADING(StringPool.PERCENT, StringPool.BLANK),
+	SURROUND(StringPool.PERCENT, StringPool.PERCENT),
+	TRAILING(StringPool.BLANK, StringPool.PERCENT);
+
+	public String leadingSqlWildcard() {
+		return _leadingWildcard;
+	}
+
+	public String trailingSqlWildcard() {
+		return _trailingWildcard;
+	}
+
+	private WildcardMode(String leadingWildcard, String trailingWildcard) {
+		_leadingWildcard = leadingWildcard;
+		_trailingWildcard = trailingWildcard;
+	}
+
+	private final String _leadingWildcard;
+	private final String _trailingWildcard;
 
 }
