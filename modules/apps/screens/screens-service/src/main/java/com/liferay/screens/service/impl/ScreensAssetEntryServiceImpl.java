2ef22513b4c11ce424693056334ec81373cc2403
Brian Chan <brian.chan@liferay.com>
2018-01-31 13:05:22 -0800

    LPS-77591 fix PMD error

--- modules/apps/screens/screens-service/src/main/java/com/liferay/screens/service/impl/ScreensAssetEntryServiceImpl.java
+++ modules/apps/screens/screens-service/src/main/java/com/liferay/screens/service/impl/ScreensAssetEntryServiceImpl.java
@@ -119,8 +119,6 @@ public class ScreensAssetEntryServiceImpl
 			List<Layout> layouts = layoutLocalService.getLayouts(companyId);
 
 			if (!layouts.isEmpty()) {
-				Layout layout = layouts.get(0);
-
 				List<AssetEntry> assetEntries = new ArrayList<>();
 
 				assetEntries = filterAssetEntries(assetEntries);
