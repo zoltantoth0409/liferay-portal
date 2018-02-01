32ec944b285db3289a8b7fcb3d156d540e268cbd
Rafael Praxedes <rafael.praxedes@liferay.com>
2018-02-01 16:07:33 -0300

    Revert "LPS-77168 Add to the export and import operation the DDMFormInstanceVersion and DDMStructureVersion"
    This reverts commit 8331e7c19b917d996acfb6c60db7fe96a1516fcc.

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/exportimport/staged/model/repository/DDMFormInstanceStagedModelRepository.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/exportimport/staged/model/repository/DDMFormInstanceStagedModelRepository.java
@@ -15,9 +15,7 @@
 package com.liferay.dynamic.data.mapping.exportimport.staged.model.repository;
 
 import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
-import com.liferay.dynamic.data.mapping.model.DDMFormInstanceVersion;
 import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalService;
-import com.liferay.dynamic.data.mapping.service.DDMFormInstanceVersionLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.dynamic.data.mapping.util.comparator.DDMFormInstanceNameComparator;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
@@ -161,22 +159,6 @@ public class DDMFormInstanceStagedModelRepository
 					StagedModelDataHandlerUtil.exportStagedModel(
 						portletDataContext, ddmFormInstance);
 
-					List<DDMFormInstanceVersion> formInstanceVersions =
-						_ddmFormInstanceVersionLocalService.
-							getFormInstanceVersions(
-								ddmFormInstance.getFormInstanceId());
-
-					for (DDMFormInstanceVersion ddmFormInstanceVersion :
-							formInstanceVersions) {
-
-						StagedModelDataHandlerUtil.exportStagedModel(
-							portletDataContext, ddmFormInstanceVersion);
-
-						StagedModelDataHandlerUtil.exportStagedModel(
-							portletDataContext,
-							ddmFormInstanceVersion.getStructureVersion());
-					}
-
 					StagedModelDataHandlerUtil.exportStagedModel(
 						portletDataContext, ddmFormInstance.getStructure());
 				}
@@ -223,10 +205,6 @@ public class DDMFormInstanceStagedModelRepository
 
 	@Reference
 	private DDMFormInstanceLocalService _ddmFormInstanceLocalService;
-
-	@Reference
-	private DDMFormInstanceVersionLocalService
-		_ddmFormInstanceVersionLocalService;
 
 	@Reference
 	private DDMStructureLocalService _ddmStructureLocalService;
