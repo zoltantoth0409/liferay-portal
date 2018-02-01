5a20e263b92e87c45ca7892bfb815d60b9d25272
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-02-01 11:37:55 +0100

    LPS-77686 Listener so that the fragments from a page template are applied when creating a content page

---
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-content/src/main/java/com/liferay/layout/type/controller/content/internal/listener/LayoutModelListener.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.type.controller.content.internal.listener;
+
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.type.controller.content.internal.constants.ContentLayoutTypeControllerConstants;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(service = ModelListener.class)
+public class LayoutModelListener extends BaseModelListener<Layout> {
+
+	@Override
+	public void onAfterCreate(Layout layout) throws ModelListenerException {
+		if (!Objects.equals(
+				layout.getType(),
+				ContentLayoutTypeControllerConstants.LAYOUT_TYPE_CONTENT)) {
+
+			return;
+		}
+
+		UnicodeProperties typeSettingsProperties =
+			layout.getTypeSettingsProperties();
+
+		long layoutPageTemplateEntryId = GetterUtil.getLong(
+			typeSettingsProperties.get("layoutPageTemplateEntryId"));
+
+		List<FragmentEntryLink> fragmentEntryLinks =
+			_fragmentEntryLinkLocalService.getFragmentEntryLinks(
+				layout.getGroupId(),
+				_portal.getClassNameId(LayoutPageTemplateEntry.class.getName()),
+				layoutPageTemplateEntryId);
+
+		for (FragmentEntryLink fragmentEntryLink : fragmentEntryLinks) {
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				fragmentEntryLink.getGroupId(),
+				fragmentEntryLink.getFragmentEntryId(),
+				_portal.getClassNameId(Layout.class.getName()),
+				layout.getPlid(), fragmentEntryLink.getCss(),
+				fragmentEntryLink.getHtml(), fragmentEntryLink.getJs(),
+				fragmentEntryLink.getEditableValues(),
+				fragmentEntryLink.getPosition());
+		}
+	}
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
