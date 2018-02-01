d501d4227f1b638ac8adfd15f58ebf14b6ae1417
akosthurzo <akos.thurzo@liferay.com>
2018-02-01 13:20:16 +0100

    Revert "LPS-75131 If the sourceCompanyGroupId doesn't equal the sourceGroupId, then we are done checking for this"
    This reverts commit eefb499

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/controller/LayoutImportController.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/controller/LayoutImportController.java
@@ -932,6 +932,15 @@ public class LayoutImportController implements ImportController {
 			if (sourceCompanyGroupId == sourceGroupId) {
 				companySourceGroup = true;
 			}
+			else if ((group.isStaged() || group.hasStagingGroup()) &&
+					 !(group.isStagedRemotely() &&
+					   group.hasRemoteStagingGroup())) {
+
+				Group sourceGroup = _groupLocalService.fetchGroup(
+					sourceGroupId);
+
+				companySourceGroup = sourceGroup.isCompany();
+			}
 
 			if (group.isCompany() ^ companySourceGroup) {
 				throw new LARTypeException(LARTypeException.TYPE_COMPANY_GROUP);
