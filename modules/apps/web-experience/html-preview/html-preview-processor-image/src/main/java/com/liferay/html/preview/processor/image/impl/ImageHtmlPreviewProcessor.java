51203d722cf1aa9b88559013b8e7740dd8d97e63
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-02-01 10:37:53 +0100

    LPS-77656 Extract code to a method to reuse it

--- modules/apps/web-experience/html-preview/html-preview-processor-image/src/main/java/com/liferay/html/preview/processor/image/impl/ImageHtmlPreviewProcessor.java
+++ modules/apps/web-experience/html-preview/html-preview-processor-image/src/main/java/com/liferay/html/preview/processor/image/impl/ImageHtmlPreviewProcessor.java
@@ -42,18 +42,7 @@ public class ImageHtmlPreviewProcessor implements HtmlPreviewProcessor {
 
 			FileUtil.write(tempFile, content);
 
-			Java2DRenderer renderer = new Java2DRenderer(tempFile, 1024);
-
-			renderer.setBufferedImageType(BufferedImage.TYPE_INT_RGB);
-
-			File outputFile = FileUtil.createTempFile("png");
-
-			FSImageWriter imageWriter = new FSImageWriter();
-
-			imageWriter.write(
-				renderer.getImage(), outputFile.getAbsolutePath());
-
-			return outputFile;
+			return _getFile(new Java2DRenderer(tempFile, 1024));
 		}
 		catch (Exception e) {
 			_log.error("Unable to generate HTML preview", e);
@@ -65,6 +54,18 @@ public class ImageHtmlPreviewProcessor implements HtmlPreviewProcessor {
 	@Override
 	public String getMimeType() {
 		return ContentTypes.IMAGE_PNG;
+	}
+
+	private File _getFile(Java2DRenderer renderer) throws Exception {
+		renderer.setBufferedImageType(BufferedImage.TYPE_INT_RGB);
+
+		File outputFile = FileUtil.createTempFile("png");
+
+		FSImageWriter imageWriter = new FSImageWriter();
+
+		imageWriter.write(renderer.getImage(), outputFile.getAbsolutePath());
+
+		return outputFile;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
