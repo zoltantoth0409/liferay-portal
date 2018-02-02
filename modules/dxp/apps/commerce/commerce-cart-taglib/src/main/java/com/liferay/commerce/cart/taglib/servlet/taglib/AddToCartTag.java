ee46b8bc82c75216c22aa8ca3b31cdad8149a60c
Marco Leo <marco.leo@liferay.com>
2018-02-01 19:13:08 -0800

    COMMERCE-31 New add to cart taglib

---
+++ modules/dxp/apps/commerce/commerce-cart-taglib/src/main/java/com/liferay/commerce/cart/taglib/servlet/taglib/AddToCartTag.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.cart.taglib.servlet.taglib;
+
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.model.CPDefinitionInventory;
+import com.liferay.commerce.model.CPDefinitionInventoryConstants;
+import com.liferay.commerce.model.CommerceCartConstants;
+import com.liferay.commerce.service.CPDefinitionInventoryServiceUtil;
+import com.liferay.frontend.taglib.soy.servlet.taglib.TemplateRendererTag;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+import javax.portlet.WindowStateException;
+
+/**
+ * @author Marco Leo
+ */
+public class AddToCartTag extends TemplateRendererTag {
+
+	@Override
+	public int doStartTag() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String randomNamespace = StringUtil.randomId() + StringPool.UNDERLINE;
+
+		int minCartQuantity =
+			CPDefinitionInventoryConstants.DEFAULT_MIN_CART_QUANTITY;
+
+		try {
+			Map<String, Object> context = getContext();
+
+			putValue("id", randomNamespace + "id");
+
+			String cartType = GetterUtil.getString(
+				context.get("cartType"),
+				String.valueOf(CommerceCartConstants.TYPE_CART));
+
+			putValue("cartType", cartType);
+
+			long cpDefinitionId = GetterUtil.getLong(
+				context.get("cpDefinitionId"));
+
+			CPDefinitionInventory cpDefinitionInventory =
+				CPDefinitionInventoryServiceUtil.
+					fetchCPDefinitionInventoryByCPDefinitionId(cpDefinitionId);
+
+			if (cpDefinitionInventory != null) {
+				minCartQuantity = cpDefinitionInventory.getMinCartQuantity();
+			}
+
+			String quantity = GetterUtil.getString(
+				context.get("quantity"), String.valueOf(minCartQuantity));
+
+			putValue("quantity", quantity);
+
+			String label = GetterUtil.getString(
+				context.get("label"), LanguageUtil.get(request, "add-to-cart"));
+
+			putValue("label", label);
+
+			putValue(
+				"portletNamespace",
+				PortalUtil.getPortletNamespace(
+					CommercePortletKeys.COMMERCE_CART_CONTENT));
+
+			putValue("uri", _getURI());
+		}
+		catch (Exception e) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(e, e);
+			}
+		}
+
+		setTemplateNamespace("AddToCart.render");
+
+		return super.doStartTag();
+	}
+
+	@Override
+	public String getModule() {
+		return "commerce-cart-taglib/add_to_cart/AddToCart.es";
+	}
+
+	public void setCartType(String cartType) {
+		putValue("cartType", cartType);
+	}
+
+	public void setCPDefinitionId(long cpDefinitionId) {
+		putValue("cpDefinitionId", String.valueOf(cpDefinitionId));
+	}
+
+	public void setCPInstanceId(long cpInstanceId) {
+		putValue("cpInstanceId", String.valueOf(cpInstanceId));
+	}
+
+	public void setElementClasses(String elementClasses) {
+		putValue("elementClasses", elementClasses);
+	}
+
+	public void setLabel(String label) {
+		putValue("label", LanguageUtil.get(request, label));
+	}
+
+	public void setProductContentId(String productContentId) {
+		putValue("productContentId", productContentId);
+	}
+
+	public void setQuantity(String quantity) {
+		putValue("quantity", quantity);
+	}
+
+	public void setQuantityInputId(String quantityInputId) {
+		putValue("quantityInputId", quantityInputId);
+	}
+
+	private String _getURI() throws WindowStateException {
+		PortletURL portletURL = PortletURLFactoryUtil.create(
+			request, CommercePortletKeys.COMMERCE_CART_CONTENT,
+			PortletRequest.ACTION_PHASE);
+
+		portletURL.setParameter(
+			ActionRequest.ACTION_NAME, "addCommerceCartItem");
+
+		return portletURL.toString();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(AddToCartTag.class);
+
+}
---
+++ modules/dxp/apps/commerce/commerce-cart-taglib/src/main/resources/META-INF/resources/add_to_cart/AddToCart.es.js
@@ -0,0 +1,220 @@
+import {Config} from 'metal-state';
+import dom from 'metal-dom';
+import Modal from 'metal-modal';
+import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
+import Soy from 'metal-soy';
+
+import templates from './AddToCart.soy';
+
+/**
+ * It opens a dialog where the user can flag the page.
+ * @abstract
+ * @extends {PortletBase}
+ */
+class AddToCart extends PortletBase {
+
+	/**
+	 * Makes an ajax request to submit the data.
+	 * @param {Event} event
+	 * @protected
+	 */
+	_addToCart() {
+
+		var that = this;
+
+        var _quantity = this.quantity;
+        var ddmFormValues = "[]";
+
+        var productContent = this._getProductContent();
+
+        if (productContent) {
+            ddmFormValues = JSON.stringify(productContent.getFormValues());
+            this.cpInstanceId = productContent.getCPInstanceId();
+        }
+
+        var quantityNode = document.querySelector('#' + this.quantityInputId )
+
+        if (quantityNode) {
+            _quantity = quantityNode.value;
+        }
+
+		let formData = new FormData();
+
+		formData.append(this.portletNamespace + 'cpDefinitionId', this.cpDefinitionId);
+		formData.append(this.portletNamespace + 'cpInstanceId', this.cpInstanceId);
+		formData.append(this.portletNamespace + 'ddmFormValues', ddmFormValues);
+		formData.append(this.portletNamespace + 'quantity', _quantity);
+        formData.append(this.portletNamespace + 'type', this.cartType);
+
+		fetch(this.uri, {
+			body: formData,
+			credentials: 'include',
+			method: 'post'
+		})
+		.then(response => response.json())
+		.then((jsonresponse) => {
+
+			if (jsonresponse.success) {
+				Liferay.fire('commerce:productAddedToCart', jsonresponse);
+			}
+			else {
+				var validatorErrors = jsonresponse.validatorErrors;
+
+				if (validatorErrors) {
+
+					validatorErrors.forEach(
+						function(validatorError) {
+							that._showNotification(validatorError.message, 'danger');
+						}
+					);
+				}
+				else {
+					that._showNotification(response.error, 'danger');
+				}
+			}
+		});
+	}
+
+    _getProductContent(){
+        return Liferay.component(this.productContentId);
+    }
+
+    _handleClick(){
+
+	    var that = this;
+
+        var productContent = this._getProductContent();
+
+		if(productContent){
+            productContent.validateProduct(
+                function(hasError) {
+                    if (!hasError) {
+                        that._addToCart();
+                    }
+                }
+            );
+        }
+        else {
+            this._addToCart();
+        }
+	}
+
+	_showNotification(messageKey, type){
+		new Liferay.Notification(
+			{
+				message: Liferay.Language.get(messageKey),
+				render: true,
+				title: '',
+				type: type
+			}
+		);
+	}
+};
+
+/**
+ * State definition.
+ * @ignore
+ * @static
+ * @type {!Object}
+ */
+AddToCart.STATE = {
+    /**
+     * The type of the cart.
+     * @instance
+     * @memberof AddToCart
+     * @type {?number}
+     * @default undefined
+     */
+    cartType: Config.string(),
+
+	/**
+	 * CPDefinitionId.
+	 * @instance
+	 * @memberof AddToCart
+	 * @type {?number}
+	 * @default undefined
+	 */
+	cpDefinitionId: Config.string(),
+
+	/**
+	 * CPInstanceId.
+	 * @instance
+	 * @memberof AddToCart
+	 * @type {?number}
+	 * @default undefined
+	 */
+	cpInstanceId: Config.string(),
+
+	/**
+	 * CSS classes to be applied to the element.
+	 * @instance
+	 * @memberof AddToCart
+	 * @type {?string}
+	 * @default undefined
+	 */
+	elementClasses: Config.string(),
+
+    /**
+     * Id of the html input to get the quantity.
+     * @instance
+     * @memberof AddToCart
+     * @type {?string}
+     * @default undefined
+     */
+    quantityInputId: Config.string(),
+
+
+    /**
+     * Defaukt quantity to add to cart.
+     * @instance
+     * @memberof AddToCart
+     * @type {?string}
+     * @default undefined
+     */
+    quantity: Config.string(),
+
+	/**
+	 * Component id.
+	 * @instance
+	 * @memberof AddToCart
+	 * @type {String}
+	 */
+	id: Config.string().required(),
+
+	/**
+	 * Text to display inside the add to cart button.
+	 * @instance
+	 * @memberof AddToCart
+	 * @type {String}
+	 */
+	label: Config.string().required(),
+
+	/**
+	 * Portlet's namespace
+	 * @instance
+	 * @memberof AddToCart
+	 * @type {String}
+	 */
+	portletNamespace: Config.string().required(),
+
+    /**
+     * Product content id
+     * @instance
+     * @memberof AddToCart
+     * @type {String}
+     */
+    productContentId: Config.string().required(),
+
+	/**
+	 * Uri to add a cart item.
+	 * @instance
+	 * @memberof AddToCart
+	 * @type {String}
+	 */
+	uri: Config.string().required()
+};
+
+// Register component
+Soy.register(AddToCart, templates);
+
+export default AddToCart;
