087c5bede6554394007970ef64f45601d0e80a65
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-02-03 19:09:38 +0100

    LPS-77685 Uses portlet request to get parameters

--- modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/ui/JournalScheduleFormNavigatorEntry.java
+++ modules/apps/web-experience/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/ui/JournalScheduleFormNavigatorEntry.java
@@ -21,10 +21,13 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.util.PropsValues;
 
+import javax.portlet.PortletRequest;
+
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -53,10 +56,13 @@ public class JournalScheduleFormNavigatorEntry
 		ServiceContext serviceContext =
 			ServiceContextThreadLocal.getServiceContext();
 
-		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+		HttpServletRequest request = serviceContext.getRequest();
+
+		PortletRequest portletRequest = (PortletRequest)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_REQUEST);
 
 		long classNameId = BeanParamUtil.getLong(
-			article, themeDisplay.getRequest(), "classNameId");
+			article, portletRequest, "classNameId");
 
 		if (classNameId > JournalArticleConstants.CLASSNAME_ID_DEFAULT) {
 			return false;
