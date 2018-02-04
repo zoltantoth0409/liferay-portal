06eb7522a323d6c89263df7292d61cdd0d09a187
Marco Leo <marco.leo@liferay.com>
2018-02-03 22:17:01 -0800

    COMMERCE-235 Fix organization indexer

--- modules/dxp/apps/commerce/commerce-organization-service/src/main/java/com/liferay/commerce/organization/internal/search/OrganizationIndexerPostProcessor.java
+++ modules/dxp/apps/commerce/commerce-organization-service/src/main/java/com/liferay/commerce/organization/internal/search/OrganizationIndexerPostProcessor.java
@@ -21,10 +21,14 @@ import com.liferay.portal.kernel.search.IndexerPostProcessor;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
 import com.liferay.portal.kernel.search.filter.PrefixFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -58,12 +62,41 @@ public class OrganizationIndexerPostProcessor extends BaseIndexerPostProcessor {
 				BooleanClauseOccur.MUST);
 		}
 
+		List<String> organizationTreePaths = (List<String>)params.get(
+			"organizationTreePaths");
+
+		if (ListUtil.isNotEmpty(organizationTreePaths)) {
+			BooleanFilter treePathBooleanFilter = new BooleanFilter();
+
+			for (String organizationTreePath : organizationTreePaths) {
+				treePathBooleanFilter.add(
+					new PrefixFilter(Field.TREE_PATH, organizationTreePath),
+					BooleanClauseOccur.SHOULD);
+			}
+
+			booleanFilter.add(treePathBooleanFilter, BooleanClauseOccur.MUST);
+		}
+
 		String organizationType = MapUtil.getString(params, Field.TYPE);
 
 		if (Validator.isNotNull(treePath)) {
 			booleanFilter.add(
 				new PrefixFilter(Field.TYPE, organizationType),
 				BooleanClauseOccur.MUST);
+		}
+
+		List<Long> excludedOrganizationIds = (List<Long>)params.get(
+			"organizationIds");
+
+		if (ListUtil.isNotEmpty(excludedOrganizationIds)) {
+			TermsFilter termsFilter = new TermsFilter("organizationId");
+
+			termsFilter.addValues(
+				ArrayUtil.toStringArray(
+					excludedOrganizationIds.toArray(
+						new Long[excludedOrganizationIds.size()])));
+
+			booleanFilter.add(termsFilter, BooleanClauseOccur.MUST);
 		}
 	}
 
