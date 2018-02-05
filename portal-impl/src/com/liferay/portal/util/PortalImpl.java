f8af0f3a6b4d49eb5b49b35b5f5fc05a06cac1d7
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-02-05 19:16:28 +0100

    LPS-75491 Use default plid if layout private

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -1753,8 +1753,11 @@ public class PortalImpl implements Portal {
 			PortletURL createAccountURL = null;
 
 			if (layout.isPrivateLayout()) {
+				long defaultPlid = LayoutLocalServiceUtil.getDefaultPlid(
+					layout.getGroupId(), false);
+
 				createAccountURL = PortletURLFactoryUtil.create(
-					request, PortletKeys.LOGIN, null,
+					request, PortletKeys.LOGIN, defaultPlid,
 					PortletRequest.RENDER_PHASE);
 			}
 			else {
