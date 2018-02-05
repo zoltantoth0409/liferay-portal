e40aaf75ac059d730a365a5220afb33503541181
antonio-ortega <60252917@liferay.com>
2018-02-05 13:03:30 +0100

    LPS-77767 Reverting wrong commit introduced in BPR-14271 (LPS-76511)

--- modules/apps/foundation/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/Details.java
+++ modules/apps/foundation/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/Details.java
@@ -42,7 +42,7 @@ public @interface Details {
 	@Meta.AD(deflt = "false", name = "explain-resolutions", required = false)
 	public boolean explainResolutions();
 
-	@Meta.AD(deflt = "false", name = "expose-global", required = false)
+	@Meta.AD(deflt = "true", name = "expose-global", required = false)
 	public boolean exposeGlobal();
 
 }
