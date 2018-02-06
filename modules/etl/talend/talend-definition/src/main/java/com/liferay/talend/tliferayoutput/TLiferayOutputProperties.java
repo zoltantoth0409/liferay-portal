5a280a0e129c3f1cded1981d213d42833c87261d
Zoltán Takács <zoltan.takacs@liferay.com>
2018-02-06 16:33:31 +0100

    COMMERCE-299 Define the output component UI and behavior

--- modules/etl/talend/talend-definition/src/main/java/com/liferay/talend/tliferayoutput/TLiferayOutputProperties.java
+++ modules/etl/talend/talend-definition/src/main/java/com/liferay/talend/tliferayoutput/TLiferayOutputProperties.java
@@ -15,14 +15,31 @@
 package com.liferay.talend.tliferayoutput;
 
 import com.liferay.talend.connection.LiferayConnectionResourceBaseProperties;
+import com.liferay.talend.resource.LiferayResourceProperties;
+import com.liferay.talend.utils.CommonUtils;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.avro.Schema;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.talend.components.api.component.Connector;
+import org.talend.components.api.component.ISchemaListener;
 import org.talend.components.api.component.PropertyPathConnector;
+import org.talend.components.common.SchemaProperties;
+import org.talend.daikon.avro.AvroUtils;
+import org.talend.daikon.avro.SchemaConstants;
+import org.talend.daikon.properties.ValidationResult;
 import org.talend.daikon.properties.presentation.Form;
+import org.talend.daikon.properties.presentation.Widget;
+import org.talend.daikon.properties.property.Property;
+import org.talend.daikon.properties.property.PropertyFactory;
 
 /**
  * @author Zoltán Takács
@@ -34,11 +51,105 @@ public class TLiferayOutputProperties
 		super(name);
 	}
 
+	public void afterSupportedOperations() {
+		_updateOutputSchemas();
+
+		refreshLayout(getForm(Form.MAIN));
+		refreshLayout(getForm(Form.REFERENCE));
+	}
+
+	@Override
+	public void refreshLayout(Form form) {
+		super.refreshLayout(form);
+	}
+
 	@Override
 	public void setupLayout() {
 		super.setupLayout();
 
 		Form mainForm = getForm(Form.MAIN);
+
+		Widget supportedOperationsWidget = Widget.widget(supportedOperations);
+
+		supportedOperationsWidget.setWidgetType(Widget.ENUMERATION_WIDGET_TYPE);
+
+		mainForm.addRow(supportedOperationsWidget);
+	}
+
+	@Override
+	public void setupProperties() {
+		super.setupProperties();
+
+		resource = new ResourcePropertiesHelper("resource");
+
+		resource.connection = connection;
+		resource.setupProperties();
+		resource.setSchemaListener(
+			new ISchemaListener() {
+
+				@Override
+				public void afterSchema() {
+					_updateOutputSchemas();
+				}
+
+			});
+
+		supportedOperations.setValue(null);
+	}
+
+	public SchemaProperties schemaReject = new SchemaProperties("schemaReject");
+	public Property<List<String>> supportedOperations =
+		PropertyFactory.newStringList("supportedOperations");
+
+	/**
+	 * Have to use an explicit class to get the override of afterResourceURL(),
+	 * an anonymous class cannot be public and thus cannot be called via
+	 * Talend's reflection mechanism.
+	 */
+	public class ResourcePropertiesHelper extends LiferayResourceProperties {
+
+		public ResourcePropertiesHelper(String name) {
+			super(name);
+		}
+
+		@Override
+		public ValidationResult afterResourceURL() throws Exception {
+			setupSchema();
+
+			List<String> list = new ArrayList<>();
+
+			list.add("CREATE");
+			list.add("UPDATE");
+			list.add("DELETE");
+			list.add("UPSERT");
+
+			supportedOperations.setPossibleValues(list);
+
+			refreshLayout(getForm(Form.MAIN));
+			refreshLayout(getForm(Form.REFERENCE));
+
+			return ValidationResult.OK;
+		}
+
+		public void setupSchema() {
+			main.schema.setValue(SchemaProperties.EMPTY_SCHEMA);
+
+			Schema.Field docIdField = new Schema.Field(
+				"docId", AvroUtils._string(), null, (Object)null,
+				Schema.Field.Order.ASCENDING);
+
+			docIdField.addProp(SchemaConstants.TALEND_IS_LOCKED, "true");
+
+			List<Schema.Field> fields = new ArrayList<>();
+
+			fields.add(docIdField);
+
+			Schema initialSchema = Schema.createRecord(
+				"liferay", null, null, false, fields);
+
+			main.schema.setValue(initialSchema);
+		}
+
 	}
 
 	@Override
@@ -48,7 +159,7 @@ public class TLiferayOutputProperties
 		Set<PropertyPathConnector> connectors = new HashSet<>();
 
 		if (!outputConnectors) {
-			connectors.add(flowConnector);
+			connectors.add(mainConnector);
 			connectors.add(rejectConnector);
 
 			return connectors;
@@ -57,9 +168,80 @@ public class TLiferayOutputProperties
 		return Collections.<PropertyPathConnector>emptySet();
 	}
 
-	protected transient PropertyPathConnector flowConnector =
-		new PropertyPathConnector(Connector.MAIN_NAME, "schemaFlow");
 	protected transient PropertyPathConnector rejectConnector =
 		new PropertyPathConnector(Connector.REJECT_NAME, "schemaReject");
+
+	@SuppressWarnings("unused")
+	private List<String> _getSchemaFieldNames(Property<Schema> schemaProperty) {
+		Schema schema = schemaProperty.getValue();
+		List<String> fieldNames = new ArrayList<>();
+
+		for (Schema.Field field : schema.getFields()) {
+			fieldNames.add(field.name());
+		}
+
+		return fieldNames;
+	}
+
+	private void _updateOutputSchemas() {
+		Schema inputSchema = resource.main.schema.getValue();
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Update output schemas");
+		}
+
+		final List<Schema.Field> additionalMainFields = new ArrayList<>();
+
+		Schema mainOutputSchema = CommonUtils.newSchema(
+			inputSchema, "output", additionalMainFields);
+
+		resource.main.schema.setValue(mainOutputSchema);
+
+		final List<Schema.Field> additionalRejectFields = new ArrayList<>();
+
+		Schema.Field field = new Schema.Field(
+			_FIELD_ERROR_CODE, Schema.create(Schema.Type.STRING), null,
+			(Object)null);
+
+		field.addProp(SchemaConstants.TALEND_IS_LOCKED, "false");
+		field.addProp(SchemaConstants.TALEND_FIELD_GENERATED, "true");
+		field.addProp(SchemaConstants.TALEND_COLUMN_DB_LENGTH, "255");
+
+		additionalRejectFields.add(field);
+
+		field = new Schema.Field(
+			_FIELD_ERROR_FIELDS, Schema.create(Schema.Type.STRING), null,
+			(Object)null);
+
+		field.addProp(SchemaConstants.TALEND_IS_LOCKED, "false");
+		field.addProp(SchemaConstants.TALEND_FIELD_GENERATED, "true");
+		field.addProp(SchemaConstants.TALEND_COLUMN_DB_LENGTH, "255");
+
+		additionalRejectFields.add(field);
+
+		field = new Schema.Field(
+			_FIELD_ERROR_MESSAGE, Schema.create(Schema.Type.STRING), null,
+			(Object)null);
+
+		field.addProp(SchemaConstants.TALEND_IS_LOCKED, "false");
+		field.addProp(SchemaConstants.TALEND_FIELD_GENERATED, "true");
+		field.addProp(SchemaConstants.TALEND_COLUMN_DB_LENGTH, "255");
+
+		additionalRejectFields.add(field);
+
+		Schema rejectSchema = CommonUtils.newSchema(
+			inputSchema, "rejectOutput", additionalRejectFields);
+
+		schemaReject.schema.setValue(rejectSchema);
+	}
+
+	private static final String _FIELD_ERROR_CODE = "errorCode";
+
+	private static final String _FIELD_ERROR_FIELDS = "errorFields";
+
+	private static final String _FIELD_ERROR_MESSAGE = "errorMessage";
+
+	private static final Logger _log = LoggerFactory.getLogger(
+		TLiferayOutputProperties.class);
 
 }
---
+++ modules/etl/talend/talend-definition/src/main/java/com/liferay/talend/utils/CommonUtils.java
@@ -0,0 +1,222 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talend.utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.avro.Schema;
+import org.apache.avro.Schema.Field;
+
+import org.talend.components.api.component.Connector;
+import org.talend.components.api.properties.ComponentProperties;
+import org.talend.daikon.avro.SchemaConstants;
+
+/**
+ * @author Zoltán Takács
+ */
+public class CommonUtils {
+
+	/**
+	 * Gets the main schema from the input connector of output components
+	 *
+	 * @param  properties
+	 * @return Schema
+	 */
+	public static Schema getMainSchemaFromInputConnector(
+		ComponentProperties properties) {
+
+		Set<? extends Connector> inputConnectors =
+			properties.getPossibleConnectors(false);
+
+		if (inputConnectors == null) {
+			return null;
+		}
+
+		for (Connector connector : inputConnectors) {
+			if (Connector.MAIN_NAME.equals(connector.getName())) {
+				return properties.getSchema(connector, false);
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Gets main schema from the out connector of input components
+	 *
+	 * @param  properties
+	 * @return Schema
+	 */
+	public static Schema getMainSchemaFromOutputConnector(
+		ComponentProperties properties) {
+
+		return getOutputSchema(properties);
+	}
+
+	/**
+	 * Gets the output schema from the properties
+	 *
+	 * @param  properties
+	 * @return Schema
+	 */
+	public static Schema getOutputSchema(ComponentProperties properties) {
+		Set<? extends Connector> outputConnectors =
+			properties.getPossibleConnectors(true);
+
+		if (outputConnectors == null) {
+			return null;
+		}
+
+		for (Connector connector : outputConnectors) {
+			if (Connector.MAIN_NAME.equals(connector.getName())) {
+				return properties.getSchema(connector, true);
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Gets the reject schema from the given properties
+	 *
+	 * @param properties
+	 * @return
+	 */
+	public static Schema getRejectSchema(ComponentProperties properties) {
+		Set<? extends Connector> outputConnectors =
+			properties.getPossibleConnectors(true);
+
+		if (outputConnectors == null) {
+			return null;
+		}
+
+		for (Connector connector : outputConnectors) {
+			if (Connector.REJECT_NAME.equals(connector.getName())) {
+				return properties.getSchema(connector, true);
+			}
+		}
+
+		return null;
+	}
+
+	public static Schema mergeRuntimeSchemaWithDesignSchemaDynamic(
+		Schema designSchema, Schema runtimeSchema) {
+
+		List<Field> designFields = designSchema.getFields();
+		Set<String> designFieldSet = new HashSet<>();
+
+		for (Field designField : designFields) {
+			String name = designField.getProp(
+				SchemaConstants.TALEND_COLUMN_DB_COLUMN_NAME);
+
+			designFieldSet.add(name);
+		}
+
+		List<Schema.Field> dynamicFields = new ArrayList<>();
+
+		for (Field runtimeField : runtimeSchema.getFields()) {
+			String name = runtimeField.getProp(
+				SchemaConstants.TALEND_COLUMN_DB_COLUMN_NAME);
+
+			if (!designFieldSet.contains(name)) {
+				dynamicFields.add(runtimeField);
+			}
+		}
+
+		dynamicFields = _cloneFieldsAndResetPosition(dynamicFields);
+
+		int dynamicPosition = Integer.valueOf(
+			designSchema.getProp("di.dynamic.column.position"));
+
+		return newSchema(
+			designSchema, designSchema.getName(), dynamicFields,
+			dynamicPosition);
+	}
+
+	/**
+	 * Clones the source schema with a new name and add some additional fields
+	 *
+	 * @param  metadataSchema
+	 * @param  newSchemaName
+	 * @param  moreFields
+	 * @return Schema
+	 */
+	public static Schema newSchema(
+		Schema metadataSchema, String newSchemaName,
+		List<Schema.Field> moreFields) {
+
+		List<Field> fields = metadataSchema.getFields();
+
+		return newSchema(
+			metadataSchema, newSchemaName, moreFields, fields.size());
+	}
+
+	public static Schema newSchema(
+		Schema metadataSchema, String newSchemaName,
+		List<Schema.Field> moreFields, int insertPoint) {
+
+		Schema newSchema = Schema.createRecord(
+			newSchemaName, metadataSchema.getDoc(),
+			metadataSchema.getNamespace(), metadataSchema.isError());
+
+		List<Field> fields = metadataSchema.getFields();
+
+		List<Schema.Field> copyFieldList = _cloneFieldsAndResetPosition(fields);
+
+		copyFieldList.addAll(insertPoint, moreFields);
+
+		newSchema.setFields(copyFieldList);
+
+		Map<String, Object> objectProperties = metadataSchema.getObjectProps();
+
+		for (Map.Entry<String, Object> entry : objectProperties.entrySet()) {
+			newSchema.addProp(entry.getKey(), entry.getValue());
+		}
+
+		return newSchema;
+	}
+
+	private static List<Schema.Field> _cloneFieldsAndResetPosition(
+		List<Field> fields) {
+
+		List<Schema.Field> copyFieldList = new ArrayList<>();
+
+		for (Schema.Field schemaEntry : fields) {
+			Schema.Field field = new Schema.Field(
+				schemaEntry.name(), schemaEntry.schema(), schemaEntry.doc(),
+				schemaEntry.defaultVal(), schemaEntry.order());
+
+			Map<String, Object> objectProperties = schemaEntry.getObjectProps();
+
+			for (Map.Entry<String, Object> entry :
+					objectProperties.entrySet()) {
+
+				field.addProp(entry.getKey(), entry.getValue());
+			}
+
+			copyFieldList.add(field);
+		}
+
+		return copyFieldList;
+	}
+
+	private CommonUtils() {
+	}
+
+}
