9d3d9efe7f83081de5ff66a973e2ffa68bd7d446
tomwang2011 <tong.wang@liferay.com>
2018-02-06 10:02:55 -0800

    LPS-75010 Apply petra string StringPool to util-bridges

--- util-bridges/src/com/liferay/util/bridges/bsf/BaseBSFPortlet.java
+++ util-bridges/src/com/liferay/util/bridges/bsf/BaseBSFPortlet.java
@@ -14,11 +14,11 @@
 
 package com.liferay.util.bridges.bsf;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.IOException;
