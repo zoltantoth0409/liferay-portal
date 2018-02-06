a7193724f4b32f1d8afe87a10d47deb2f554258c
drewbrokke <drew.brokke@liferay.com>
2018-02-06 15:16:38 -0800

    LPS-77646 users-admin-api - adds BaseUserActionContributor.java

---
+++ modules/apps/foundation/users-admin/users-admin-api/src/main/java/com/liferay/users/admin/user/action/contributor/BaseUserActionContributor.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.user.action.contributor;
+
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletRequest;
+
+/**
+ * @author Drew Brokke
+ */
+public abstract class BaseUserActionContributor
+	implements UserActionContributor {
+
+	public ResourceBundle getResourceBundle(Locale locale) {
+		return PortalUtil.getResourceBundle(locale);
+	}
+
+	protected Locale getLocale(PortletRequest portletRequest) {
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		return themeDisplay.getLocale();
+	}
+
+}
