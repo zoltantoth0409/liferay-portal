facb8119c8d689e5091e977e030c5da2ef2efad3
Peter Shin <peter.shin@liferay.com>
2018-02-06 12:03:29 -0600

    LPS-77840 Expose NodeDefaultsPlugin as a real Gradle plugin

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/NodeDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/NodeDefaultsPlugin.java
@@ -12,9 +12,8 @@
  * details.
  */
 
-package com.liferay.gradle.plugins.internal;
+package com.liferay.gradle.plugins;
 
-import com.liferay.gradle.plugins.BaseDefaultsPlugin;
 import com.liferay.gradle.plugins.internal.util.GradleUtil;
 import com.liferay.gradle.plugins.node.NodeExtension;
 import com.liferay.gradle.plugins.node.NodePlugin;
@@ -25,7 +24,6 @@ import com.liferay.gradle.plugins.node.tasks.PublishNodeModuleTask;
 import com.liferay.gradle.util.Validator;
 
 import org.gradle.api.Action;
-import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskContainer;
 
@@ -33,8 +31,6 @@ import org.gradle.api.tasks.TaskContainer;
  * @author Andrea Di Giorgi
  */
 public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
-
-	public static final Plugin<Project> INSTANCE = new NodeDefaultsPlugin();
 
 	@Override
 	protected void configureDefaults(Project project, NodePlugin nodePlugin) {
@@ -47,9 +43,6 @@ public class NodeDefaultsPlugin extends BaseDefaultsPlugin<NodePlugin> {
 	@Override
 	protected Class<NodePlugin> getPluginClass() {
 		return NodePlugin.class;
-	}
-
-	private NodeDefaultsPlugin() {
 	}
 
 	private void _configureNode(Project project) {
