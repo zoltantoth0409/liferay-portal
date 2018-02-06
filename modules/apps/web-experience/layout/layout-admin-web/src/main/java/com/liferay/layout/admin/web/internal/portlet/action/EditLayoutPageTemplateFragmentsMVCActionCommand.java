49248cd49899cd3c3e61d3dc9c969f00ab0bf85b
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-02-06 16:13:01 +0100

    LPS-76234 Update action command

--- modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutPageTemplateFragmentsMVCActionCommand.java
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutPageTemplateFragmentsMVCActionCommand.java
@@ -54,12 +54,14 @@ public class EditLayoutPageTemplateFragmentsMVCActionCommand
 			actionRequest, "layoutPageTemplateEntryId");
 		long[] fragmentIds = ParamUtil.getLongValues(
 			actionRequest, "fragmentIds");
+		String editableValues = ParamUtil.getString(actionRequest, "editable");
 
 		ServiceContext serviceContext = ServiceContextFactory.getInstance(
 			actionRequest);
 
 		_layoutPageTemplateEntryService.updateLayoutPageTemplateEntry(
-			layoutPageTemplateEntryId, fragmentIds, null, serviceContext);
+			layoutPageTemplateEntryId, fragmentIds, editableValues,
+			serviceContext);
 
 		hideDefaultSuccessMessage(actionRequest);
 
