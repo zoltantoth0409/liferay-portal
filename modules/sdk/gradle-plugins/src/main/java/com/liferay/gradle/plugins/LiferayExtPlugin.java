460b61efb6f61588541765515e8f4fe1b6753ca6
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2018-02-06 10:41:09 -0800

    LPS-77352 Rewrite plugin

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/LiferayExtPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/LiferayExtPlugin.java
@@ -14,332 +14,349 @@
 
 package com.liferay.gradle.plugins;
 
+import com.liferay.gradle.plugins.internal.util.GradleUtil;
 import com.liferay.gradle.plugins.tasks.BuildExtInfoTask;
-import com.liferay.gradle.util.GradleUtil;
 
 import groovy.lang.Closure;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.concurrent.Callable;
 
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.DependencySet;
 import org.gradle.api.file.CopySpec;
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileTreeElement;
 import org.gradle.api.file.SourceDirectorySet;
-import org.gradle.api.plugins.BasePlugin;
-import org.gradle.api.plugins.PluginContainer;
 import org.gradle.api.plugins.WarPlugin;
 import org.gradle.api.plugins.WarPluginConvention;
-import org.gradle.api.tasks.Copy;
+import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.SourceSet;
-import org.gradle.api.tasks.SourceSetOutput;
 import org.gradle.api.tasks.Sync;
+import org.gradle.api.tasks.bundling.Jar;
 import org.gradle.api.tasks.bundling.War;
-import org.gradle.jvm.tasks.Jar;
 import org.gradle.util.GUtil;
 
 /**
  * @author David Truong
+ * @author Andrea Di Giorgi
  */
 public class LiferayExtPlugin implements Plugin<Project> {
 
+	public static final String BUILD_EXT_INFO_BASE_DIR_TASK_NAME =
+		"buildExtInfoBaseDir";
+
 	public static final String BUILD_EXT_INFO_TASK_NAME = "buildExtInfo";
 
-	public static final String CONFIGURATION_NAME = "ext";
+	public static final String EXT_IMPL_SOURCESET_NAME = "extImpl";
 
-	public static final String COPY_PORTAL_DEPENDENCIES_TASK_NAME =
-		"copyPortalDependencies";
+	public static final String EXT_KERNEL_SOURCESET_NAME = "extKernel";
+
+	public static final String EXT_UTIL_BRIDGES_SOURCESET_NAME =
+		"extUtilBridges";
+
+	public static final String EXT_UTIL_JAVA_SOURCESET_NAME = "extUtilJava";
+
+	public static final String EXT_UTIL_TAGLIB_SOURCESET_NAME = "extUtilTaglib";
 
 	@Override
-	public void apply(final Project project) {
-		Configuration extConfiguration = _addConfigurationExt(project);
+	public void apply(Project project) {
+		_applyPlugins(project);
 
-		PluginContainer pluginContainer = project.getPlugins();
-
-		if (!pluginContainer.hasPlugin(WarPlugin.class)) {
-			GradleUtil.applyPlugin(project, WarPlugin.class);
-		}
-
-		Copy copy = GradleUtil.addTask(
-			project, COPY_PORTAL_DEPENDENCIES_TASK_NAME, Copy.class);
-
-		War war = (War)GradleUtil.getTask(project, "war");
+		War war = (War)GradleUtil.getTask(project, WarPlugin.WAR_TASK_NAME);
 
 		WarPluginConvention warPluginConvention = GradleUtil.getConvention(
 			project, WarPluginConvention.class);
 
-		final File webInfDir = new File(
-			warPluginConvention.getWebAppDir(), "WEB-INF");
+		Configuration portalConfiguration = GradleUtil.getConfiguration(
+			project, LiferayBasePlugin.PORTAL_CONFIGURATION_NAME);
 
-		FileCollection extJars = project.files(extConfiguration);
+		Jar extKernelJar = _addSourceSet(
+			war, warPluginConvention, EXT_KERNEL_SOURCESET_NAME,
+			portalConfiguration);
 
-		Jar jarExtKernel = _configureExtJar(
-			war, "ext-kernel", webInfDir, extJars);
+		FileCollection compileClasspath = portalConfiguration.plus(
+			project.files(extKernelJar));
 
-		extJars = extJars.plus(project.files(jarExtKernel));
+		Jar utilBridgesJar = _addSourceSet(
+			war, warPluginConvention, EXT_UTIL_BRIDGES_SOURCESET_NAME,
+			compileClasspath);
 
-		Jar jarExtUtilBridges = _configureExtUtilJar(
-			war, "ext-util-bridges", webInfDir, extJars, copy);
+		compileClasspath = compileClasspath.plus(project.files(utilBridgesJar));
 
-		extJars = extJars.plus(project.files(jarExtUtilBridges));
+		Jar utilJavaJar = _addSourceSet(
+			war, warPluginConvention, EXT_UTIL_JAVA_SOURCESET_NAME,
+			compileClasspath);
 
-		Jar jarExtUtilJava = _configureExtUtilJar(
-			war, "ext-util-java", webInfDir, extJars, copy);
+		compileClasspath = compileClasspath.plus(project.files(utilJavaJar));
 
-		extJars = extJars.plus(project.files(jarExtUtilJava));
+		Jar utilTaglibJar = _addSourceSet(
+			war, warPluginConvention, EXT_UTIL_TAGLIB_SOURCESET_NAME,
+			compileClasspath);
 
-		Jar jarExtUtilTaglib = _configureExtUtilJar(
-			war, "ext-util-taglib", webInfDir, extJars, copy);
+		compileClasspath = compileClasspath.plus(project.files(utilTaglibJar));
 
-		extJars = extJars.plus(project.files(jarExtUtilTaglib));
+		Jar extImplJar = _addSourceSet(
+			war, warPluginConvention, EXT_IMPL_SOURCESET_NAME,
+			compileClasspath);
 
-		Jar jarExtImpl = _configureExtImplJar(
-			war, "ext-impl", webInfDir, extJars, copy);
+		Sync buildExtInfoBaseDirTask = _addTaskBuildExtInfoBaseDir(war);
 
-		extJars = extJars.plus(project.files(jarExtImpl));
+		BuildExtInfoTask buildExtInfoTask = _addTaskBuildExtInfo(
+			buildExtInfoBaseDirTask, war, portalConfiguration);
 
-		_addTaskBuildExtInfo(
-			project, extJars, war, webInfDir, jarExtImpl, jarExtKernel,
-			jarExtUtilBridges, jarExtUtilJava, jarExtUtilTaglib);
+		_configureTaskBuildExtInfoBaseDir(
+			buildExtInfoBaseDirTask, buildExtInfoTask);
+
+		_configureTaskExtImplJar(
+			extImplJar, utilBridgesJar, utilJavaJar, utilTaglibJar);
+		_configureTaskWar(war, buildExtInfoTask);
 	}
 
-	private Configuration _addConfigurationExt(final Project project) {
-		Configuration configuration = GradleUtil.addConfiguration(
-			project, CONFIGURATION_NAME);
+	private Jar _addSourceSet(
+		War war, final WarPluginConvention warPluginConvention, String name,
+		FileCollection compileClasspath) {
 
-		configuration.defaultDependencies(
-			new Action<DependencySet>() {
+		// Source set
+
+		Project project = war.getProject();
+
+		SourceSet sourceSet = GradleUtil.addSourceSet(project, name);
+
+		sourceSet.setCompileClasspath(compileClasspath);
+
+		// Jar task
+
+		final Jar jar = GradleUtil.addTask(
+			project, sourceSet.getJarTaskName(), Jar.class);
+
+		jar.from(sourceSet.getOutput());
+
+		jar.setAppendix(GUtil.toWords(name, '-'));
+
+		jar.setDescription(
+			"Assembles a jar archive containing the " + jar.getAppendix() +
+				" classes.");
+
+		// Source directories
+
+		Callable<File> srcDirCallable = new Callable<File>() {
+
+			@Override
+			public File call() throws Exception {
+				return new File(
+					warPluginConvention.getWebAppDir(),
+					"WEB-INF/" + jar.getAppendix() + "/src");
+			}
+
+		};
+
+		SourceDirectorySet javaSourceDirectorySet = sourceSet.getJava();
+
+		javaSourceDirectorySet.srcDir(srcDirCallable);
+
+		SourceDirectorySet resourcesDirectorySet = sourceSet.getResources();
+
+		resourcesDirectorySet.srcDir(srcDirCallable);
+
+		// War task
+
+		CopySpec copySpec = war.getWebInf();
+
+		copySpec.from(jar);
+
+		copySpec.into(
+			new Callable<String>() {
 
 				@Override
-				public void execute(DependencySet dependencySet) {
-					_addDependenciesExt(project);
+				public String call() throws Exception {
+					return jar.getAppendix();
 				}
 
 			});
 
-		configuration.setDescription("Configures Portal Classloader for Ext.");
-		configuration.setVisible(false);
+		copySpec.rename(
+			new Closure<String>(project) {
 
-		return configuration;
-	}
+				@SuppressWarnings("unused")
+				public String doCall(String fileName) {
+					return jar.getAppendix() + "." + jar.getExtension();
+				}
 
-	private void _addDependenciesExt(Project project) {
-		Class<?> clazz = getClass();
-
-		try (InputStream in = clazz.getResourceAsStream(
-				"dependencies/dependencies.txt");
-			BufferedReader bufferedReader =
-				new BufferedReader(new InputStreamReader(in))) {
-
-			String line = null;
-
-			while ((line = bufferedReader.readLine()) != null) {
-				String[] artifact = line.split(":");
-
-				GradleUtil.addDependency(
-					project, CONFIGURATION_NAME, artifact[0], artifact[1],
-					artifact[2], false);
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-
-			System.err.println("Could not add configuration ext");
-		}
-	}
-
-	private Jar _addJarTask(Project project, String name, SourceSet sourceSet) {
-		Jar jar = GradleUtil.addTask(
-			project, GUtil.toLowerCamelCase("jar " + name), Jar.class);
-
-		jar.from(sourceSet.getOutput());
-		jar.setArchiveName(name + ".jar");
+			});
 
 		return jar;
 	}
 
-	private Copy _addPortalDeployDependencies(
-		final Project project, Copy copy, Jar jar) {
-
-		copy.from(
-			jar,
-			new Closure<Void>(project) {
-
-				@SuppressWarnings("unused")
-				public void doCall(CopySpec copySpec) {
-					copySpec.rename(
-						new Closure<String>(project) {
-
-							public String doCall(String fileName) {
-								return fileName.replaceAll(
-									"ext", "ext-" + project.getName());
-							}
-
-						});
-				}
-
-			});
-
-		return copy;
-	}
-
-	private SourceSet _addSourceSet(
-		Project project, String name, File srcDir,
-		FileCollection fileCollection) {
-
-		SourceSet sourceSet = GradleUtil.addSourceSet(project, name);
-
-		Iterable<File> srcDirs = Collections.singleton(srcDir);
-
-		sourceSet.setCompileClasspath(fileCollection);
-
-		SourceDirectorySet javaSourceDirectorySet = sourceSet.getJava();
-
-		javaSourceDirectorySet.setSrcDirs(srcDirs);
-
-		SourceDirectorySet resourcesSourceDirectorySet =
-			sourceSet.getResources();
-
-		resourcesSourceDirectorySet.setSrcDirs(srcDirs);
-
-		return sourceSet;
-	}
-
 	private BuildExtInfoTask _addTaskBuildExtInfo(
-		Project project, FileCollection classpath, War war, File webInfDir,
-		Jar... jars) {
+		final Sync buildExtInfoBaseDirTask, final War war,
+		FileCollection classpath) {
+
+		Project project = buildExtInfoBaseDirTask.getProject();
 
 		final BuildExtInfoTask buildExtInfoTask = GradleUtil.addTask(
 			project, BUILD_EXT_INFO_TASK_NAME, BuildExtInfoTask.class);
 
-		buildExtInfoTask.setDescription("Build Ext Info file.");
-		buildExtInfoTask.setGroup(BasePlugin.BUILD_GROUP);
+		buildExtInfoTask.dependsOn(buildExtInfoBaseDirTask);
+
+		buildExtInfoTask.setBaseDir(
+			new Callable<File>() {
+
+				@Override
+				public File call() throws Exception {
+					return new File(
+						buildExtInfoBaseDirTask.getDestinationDir(), "WEB-INF");
+				}
+
+			});
+
 		buildExtInfoTask.setClasspath(classpath);
+		buildExtInfoTask.setDescription(
+			"Generates the ext information xml file.");
 
-		File destinationDir = buildExtInfoTask.getTemporaryDir();
+		buildExtInfoTask.setOutputDir(
+			new Callable<File>() {
 
-		Sync sync = _copyTempWebapp(project, webInfDir, destinationDir, jars);
+				@Override
+				public File call() throws Exception {
+					return buildExtInfoTask.getTemporaryDir();
+				}
 
-		buildExtInfoTask.dependsOn(sync);
+			});
 
-		List<String> args = new ArrayList<>();
+		buildExtInfoTask.setServletContextName(
+			new Callable<String>() {
 
-		args.add(destinationDir.getPath());
-		args.add(destinationDir.getPath());
-		args.add(project.getName());
+				@Override
+				public String call() throws Exception {
+					return war.getBaseName() + "-" + war.getAppendix();
+				}
 
-		buildExtInfoTask.setArgs(args);
-
-		CopySpec copySpec = war.getWebInf();
-
-		copySpec.from(buildExtInfoTask);
+			});
 
 		return buildExtInfoTask;
 	}
 
-	private Copy _configureCopyPortalDeployDepedenciesTask(
-		SourceSet sourceSet, Copy copy) {
+	private Sync _addTaskBuildExtInfoBaseDir(War war) {
+		final Project project = war.getProject();
 
-		SourceSetOutput sourceSetOutput = sourceSet.getOutput();
+		Sync sync = GradleUtil.addTask(
+			project, BUILD_EXT_INFO_BASE_DIR_TASK_NAME, Sync.class);
 
-		copy.into(
-			new File(
-				sourceSetOutput.getClassesDir(),
-				"com/liferay/portal/deploy/dependencies"));
+		sync.into(
+			new Callable<File>() {
 
-		return copy;
-	}
+				@Override
+				public File call() throws Exception {
+					return new File(project.getBuildDir(), "build-ext-info");
+				}
 
-	private Jar _configureExtImplJar(
-		War war, String name, File webInfDir, FileCollection fileCollection,
-		Copy copy) {
+			});
 
-		Project project = war.getProject();
-
-		SourceSet sourceSet = _addSourceSet(
-			project, name, new File(webInfDir, name + "/src"), fileCollection);
-
-		_configureCopyPortalDeployDepedenciesTask(sourceSet, copy);
-
-		Jar jar = _addJarTask(project, name, sourceSet);
-
-		_copyToWebInf(war, jar, name);
-
-		jar.dependsOn(copy);
-
-		return jar;
-	}
-
-	private Jar _configureExtJar(
-		War war, String name, File webInfDir, FileCollection fileCollection) {
-
-		Project project = war.getProject();
-
-		SourceSet sourceSet = _addSourceSet(
-			project, name, new File(webInfDir, name + "/src"), fileCollection);
-
-		Jar jar = _addJarTask(project, name, sourceSet);
-
-		_copyToWebInf(war, jar, name);
-
-		return jar;
-	}
-
-	private Jar _configureExtUtilJar(
-		War war, String name, File webInfDir, FileCollection fileCollection,
-		Copy copy) {
-
-		Jar jar = _configureExtJar(war, name, webInfDir, fileCollection);
-
-		_addPortalDeployDependencies(war.getProject(), copy, jar);
-
-		return jar;
-	}
-
-	private Sync _copyTempWebapp(
-		Project project, File webInfDir, File destinationDir, Jar... jars) {
-
-		Sync sync = GradleUtil.addTask(project, "syncWebapp", Sync.class);
-
-		sync.exclude("**/.touch");
-		sync.from(webInfDir);
-		sync.into(destinationDir);
-
-		for (final Jar jar : jars) {
-			sync.from(
-				jar,
-				new Closure<Void>(project) {
-
-					@SuppressWarnings("unused")
-					public void doCall(CopySpec copySpec) {
-						String fileName = jar.getArchiveName();
-
-						fileName = fileName.substring(0, fileName.length() - 4);
-
-						copySpec.into(fileName);
-					}
-
-				});
-		}
+		sync.setDescription(
+			"Copies the exploded war archive into a temporary directory.");
+		sync.with(war);
 
 		return sync;
 	}
 
-	private void _copyToWebInf(War war, Jar jar, String name) {
+	private void _applyPlugins(Project project) {
+		GradleUtil.applyPlugin(project, LiferayBasePlugin.class);
+		GradleUtil.applyPlugin(project, WarPlugin.class);
+	}
+
+	private void _configureTaskBuildExtInfoBaseDir(
+		Sync buildExtInfoBaseDirTask, final BuildExtInfoTask buildExtInfoTask) {
+
+		buildExtInfoBaseDirTask.exclude(
+			new Spec<FileTreeElement>() {
+
+				@Override
+				public boolean isSatisfiedBy(FileTreeElement fileTreeElement) {
+					File outputFile = buildExtInfoTask.getOutputFile();
+
+					String outputFileName = outputFile.getName();
+
+					if (outputFileName.equals(fileTreeElement.getPath())) {
+						return true;
+					}
+
+					return false;
+				}
+
+			});
+	}
+
+	private void _configureTaskExtImplJar(
+		final Jar extImplJar, final Jar... jars) {
+
+		final Project project = extImplJar.getProject();
+
+		for (final Jar jar : jars) {
+			extImplJar.into(
+				"com/liferay/portal/deploy/dependencies",
+				new Closure<Void>(project) {
+
+					@SuppressWarnings("unused")
+					public void doCall(CopySpec copySpec) {
+						copySpec.from(jar);
+						copySpec.rename(
+							new PortalDeployDependencyRenameClosure(jar));
+					}
+
+				});
+		}
+	}
+
+	private void _configureTaskWar(
+		War war, final BuildExtInfoTask buildExtInfoTask) {
+
+		war.setAppendix("ext");
+
 		CopySpec copySpec = war.getWebInf();
 
-		copySpec.from(jar);
-		copySpec.into(name);
+		war.dependsOn(buildExtInfoTask);
+
+		copySpec.from(
+			new Callable<File>() {
+
+				@Override
+				public File call() throws Exception {
+					return buildExtInfoTask.getOutputFile();
+				}
+
+			});
+	}
+
+	private static class PortalDeployDependencyRenameClosure
+		extends Closure<String> {
+
+		public PortalDeployDependencyRenameClosure(Jar jar) {
+			super(jar.getProject());
+
+			_jar = jar;
+		}
+
+		@SuppressWarnings("unused")
+		public String doCall(String fileName) {
+			Project project = _jar.getProject();
+
+			StringBuilder sb = new StringBuilder();
+
+			sb.append("ext-");
+			sb.append(project.getName());
+			sb.append('-');
+			sb.append(_jar.getAppendix());
+			sb.append('.');
+			sb.append(_jar.getExtension());
+
+			return sb.toString();
+		}
+
+		private final Jar _jar;
+
 	}
 
 }
