ee184324f43e6ce365f8b0e117b6b4165ef16ded
drewbrokke <drew.brokke@liferay.com>
2018-02-06 12:00:48 -0800

    LPS-77646 users-admin-web - ViewMVCRenderCommand.java - tracks the UserActionContributors

--- modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/portlet/action/ViewMVCRenderCommand.java
+++ modules/apps/foundation/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/portlet/action/ViewMVCRenderCommand.java
@@ -16,11 +16,20 @@ package com.liferay.users.admin.web.portlet.action;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.users.admin.constants.UsersAdminPortletKeys;
+import com.liferay.users.admin.user.action.contributor.UserActionContributor;
+import com.liferay.users.admin.web.internal.constants.UsersAdminWebKeys;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Pei-Jung Lan
@@ -40,7 +49,32 @@ public class ViewMVCRenderCommand implements MVCRenderCommand {
 	public String render(
 		RenderRequest renderRequest, RenderResponse renderResponse) {
 
+		renderRequest.setAttribute(
+			UsersAdminWebKeys.USER_ACTION_CONTRIBUTORS,
+			_userActionContributors.toArray(new UserActionContributor[0]));
+
 		return "/view.jsp";
 	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		unbind = "unsetUserActionContributor"
+	)
+	protected void setUserActionContributor(
+		UserActionContributor userActionContributor) {
+
+		_userActionContributors.add(userActionContributor);
+	}
+
+	protected void unsetUserActionContributor(
+		UserActionContributor userActionContributor) {
+
+		_userActionContributors.remove(userActionContributor);
+	}
+
+	private final List<UserActionContributor> _userActionContributors =
+		new CopyOnWriteArrayList<>();
 
 }
