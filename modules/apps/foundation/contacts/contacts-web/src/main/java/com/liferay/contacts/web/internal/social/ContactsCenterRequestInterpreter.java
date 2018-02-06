5b1c9cd4eb1af1c7163a12df55b1578781103d29
hai.yu <8219754@liferay.com>
2018-02-06 10:09:35 +0800

    LPS-76937 Solve compile

--- modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/internal/social/ContactsCenterRequestInterpreter.java
+++ modules/apps/foundation/contacts/contacts-web/src/main/java/com/liferay/contacts/web/internal/social/ContactsCenterRequestInterpreter.java
@@ -14,6 +14,7 @@
 
 package com.liferay.contacts.web.internal.social;
 
+import com.liferay.contacts.web.internal.constants.ContactsPortletKeys;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -25,18 +26,20 @@ import com.liferay.social.kernel.model.BaseSocialRequestInterpreter;
 import com.liferay.social.kernel.model.SocialRelationConstants;
 import com.liferay.social.kernel.model.SocialRequest;
 import com.liferay.social.kernel.model.SocialRequestFeedEntry;
+import com.liferay.social.kernel.model.SocialRequestInterpreter;
 import com.liferay.social.kernel.service.SocialActivityLocalService;
 import com.liferay.social.kernel.service.SocialRelationLocalService;
 
+import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Hai Yu
  */
 @Component(
-		property = {"javax.portlet.name=" + ContactsPortletKeys.CONTACTS_CENTER},
-		service = SocialRequestInterpreter.class
-	)
+	property = {"javax.portlet.name=" + ContactsPortletKeys.CONTACTS_CENTER},
+	service = SocialRequestInterpreter.class
+)
 public class ContactsCenterRequestInterpreter
 	extends BaseSocialRequestInterpreter {
 
