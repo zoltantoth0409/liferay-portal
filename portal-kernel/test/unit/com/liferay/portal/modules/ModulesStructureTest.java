4a7968e03a500291a78c242aecebcf9ea867f1d6
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2018-02-06 16:20:37 -0800

    LPS-77145 This file doesn't exist anymore

--- portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
+++ portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
@@ -1449,9 +1449,6 @@ public class ModulesStructureTest {
 	private static boolean _masterBranch;
 	private static Path _modulesDirPath;
 	private static final Set<String> _nonEmptyMarkerFileNames =
-		SetUtil.fromList(
-			Arrays.asList(
-				".lfrbuild-lowest-major-version",
-				".lfrbuild-poshi-runner-resources"));
+		Collections.singleton(".lfrbuild-lowest-major-version");
 
 }
