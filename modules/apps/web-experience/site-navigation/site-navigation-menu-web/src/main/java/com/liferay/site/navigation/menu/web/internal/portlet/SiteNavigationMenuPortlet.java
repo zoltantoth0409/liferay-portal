4de6cdf6d7584ec2ae9031fed12b2d2a51e10182
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-02-08 16:22:40 +0100

    LPS-76925 Makes site navigation portlet allowed from fragment editor

--- modules/apps/web-experience/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/portlet/SiteNavigationMenuPortlet.java
+++ modules/apps/web-experience/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/portlet/SiteNavigationMenuPortlet.java
@@ -35,6 +35,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
+		"com.liferay.fragment.entry.processor.portlet.alias=nav",
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-navigation",
 		"com.liferay.portlet.display-category=category.cms",
