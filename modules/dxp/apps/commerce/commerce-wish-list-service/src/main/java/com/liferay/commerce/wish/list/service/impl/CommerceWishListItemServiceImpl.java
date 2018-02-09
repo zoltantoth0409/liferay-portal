f182b46540fafedf5fdcbae38e71abf4e71da3a8
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-02-09 11:55:14 -0800

    COMMERCE-2 Wish list remote services

--- modules/dxp/apps/commerce/commerce-wish-list-service/src/main/java/com/liferay/commerce/wish/list/service/impl/CommerceWishListItemServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-wish-list-service/src/main/java/com/liferay/commerce/wish/list/service/impl/CommerceWishListItemServiceImpl.java
@@ -14,11 +14,99 @@
 
 package com.liferay.commerce.wish.list.service.impl;
 
+import com.liferay.commerce.wish.list.model.CommerceWishList;
+import com.liferay.commerce.wish.list.model.CommerceWishListItem;
 import com.liferay.commerce.wish.list.service.base.CommerceWishListItemServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
 
 /**
  * @author Andrea Di Giorgi
  */
 public class CommerceWishListItemServiceImpl
 	extends CommerceWishListItemServiceBaseImpl {
+
+	@Override
+	public CommerceWishListItem addCommerceWishListItem(
+			long commerceWishListId, long cpDefinitionId, long cpInstanceId,
+			String json, ServiceContext serviceContext)
+		throws PortalException {
+
+		_commerceWishListModelResourcePermission.check(
+			getPermissionChecker(), commerceWishListId, ActionKeys.UPDATE);
+
+		return commerceWishListItemLocalService.addCommerceWishListItem(
+			commerceWishListId, cpDefinitionId, cpInstanceId, json,
+			serviceContext);
+	}
+
+	@Override
+	public void deleteCommerceWishListItem(long commerceWishListItemId)
+		throws PortalException {
+
+		CommerceWishListItem commerceWishListItem =
+			commerceWishListItemLocalService.getCommerceWishListItem(
+				commerceWishListItemId);
+
+		_commerceWishListModelResourcePermission.check(
+			getPermissionChecker(),
+			commerceWishListItem.getCommerceWishListId(), ActionKeys.UPDATE);
+
+		commerceWishListItemLocalService.deleteCommerceWishListItem(
+			commerceWishListItem);
+	}
+
+	@Override
+	public CommerceWishListItem getCommerceWishListItem(
+			long commerceWishListItemId)
+		throws PortalException {
+
+		CommerceWishListItem commerceWishListItem =
+			commerceWishListItemLocalService.getCommerceWishListItem(
+				commerceWishListItemId);
+
+		_commerceWishListModelResourcePermission.check(
+			getPermissionChecker(),
+			commerceWishListItem.getCommerceWishListId(), ActionKeys.VIEW);
+
+		return commerceWishListItem;
+	}
+
+	@Override
+	public List<CommerceWishListItem> getCommerceWishListItems(
+			long commerceWishListId, int start, int end,
+			OrderByComparator<CommerceWishListItem> orderByComparator)
+		throws PortalException {
+
+		_commerceWishListModelResourcePermission.check(
+			getPermissionChecker(), commerceWishListId, ActionKeys.VIEW);
+
+		return commerceWishListItemLocalService.getCommerceWishListItems(
+			commerceWishListId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceWishListItemsCount(long commerceWishListId)
+		throws PortalException {
+
+		_commerceWishListModelResourcePermission.check(
+			getPermissionChecker(), commerceWishListId, ActionKeys.VIEW);
+
+		return commerceWishListItemLocalService.getCommerceWishListItemsCount(
+			commerceWishListId);
+	}
+
+	private static volatile ModelResourcePermission<CommerceWishList>
+		_commerceWishListModelResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				CommerceWishListItemServiceImpl.class,
+				"_commerceWishListModelResourcePermission",
+				CommerceWishList.class);
+
 }
--- modules/dxp/apps/commerce/commerce-wish-list-service/src/main/java/com/liferay/commerce/wish/list/service/impl/CommerceWishListServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-wish-list-service/src/main/java/com/liferay/commerce/wish/list/service/impl/CommerceWishListServiceImpl.java
@@ -14,11 +14,143 @@
 
 package com.liferay.commerce.wish.list.service.impl;
 
+import com.liferay.commerce.wish.list.constants.CommerceWishListActionKeys;
+import com.liferay.commerce.wish.list.model.CommerceWishList;
 import com.liferay.commerce.wish.list.service.base.CommerceWishListServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
 
 /**
  * @author Andrea Di Giorgi
  */
 public class CommerceWishListServiceImpl
 	extends CommerceWishListServiceBaseImpl {
+
+	@Override
+	public CommerceWishList addCommerceWishList(
+			String name, boolean defaultWishList, ServiceContext serviceContext)
+		throws PortalException {
+
+		if (getUserId() != serviceContext.getUserId()) {
+			checkManagePermission(serviceContext.getScopeGroupId());
+		}
+
+		return commerceWishListLocalService.addCommerceWishList(
+			name, defaultWishList, serviceContext);
+	}
+
+	@Override
+	public void deleteCommerceWishList(long commerceWishListId)
+		throws PortalException {
+
+		_commerceWishListModelResourcePermission.check(
+			getPermissionChecker(), commerceWishListId, ActionKeys.DELETE);
+
+		commerceWishListLocalService.deleteCommerceWishList(commerceWishListId);
+	}
+
+	@Override
+	public CommerceWishList getCommerceWishList(long commerceWishListId)
+		throws PortalException {
+
+		_commerceWishListModelResourcePermission.check(
+			getPermissionChecker(), commerceWishListId, ActionKeys.VIEW);
+
+		return commerceWishListLocalService.getCommerceWishList(
+			commerceWishListId);
+	}
+
+	@Override
+	public List<CommerceWishList> getCommerceWishLists(
+			long groupId, int start, int end,
+			OrderByComparator<CommerceWishList> orderByComparator)
+		throws PortalException {
+
+		checkManagePermission(groupId);
+
+		return commerceWishListLocalService.getCommerceWishLists(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public List<CommerceWishList> getCommerceWishLists(
+			long groupId, long userId, int start, int end,
+			OrderByComparator<CommerceWishList> orderByComparator)
+		throws PortalException {
+
+		if (getUserId() != userId) {
+			checkManagePermission(groupId);
+		}
+
+		return commerceWishListLocalService.getCommerceWishLists(
+			groupId, userId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceWishListsCount(long groupId) throws PortalException {
+		checkManagePermission(groupId);
+
+		return commerceWishListLocalService.getCommerceWishListsCount(groupId);
+	}
+
+	@Override
+	public int getCommerceWishListsCount(long groupId, long userId)
+		throws PortalException {
+
+		if (getUserId() != userId) {
+			checkManagePermission(groupId);
+		}
+
+		return commerceWishListLocalService.getCommerceWishListsCount(groupId);
+	}
+
+	@Override
+	public CommerceWishList getDefaultCommerceWishList(
+			long groupId, long userId)
+		throws PortalException {
+
+		if (getUserId() != userId) {
+			checkManagePermission(groupId);
+		}
+
+		return commerceWishListLocalService.getDefaultCommerceWishList(
+			groupId, userId);
+	}
+
+	@Override
+	public CommerceWishList updateCommerceWishList(
+			long commerceWishListId, String name, boolean defaultWishList)
+		throws PortalException {
+
+		_commerceWishListModelResourcePermission.check(
+			getPermissionChecker(), commerceWishListId, ActionKeys.UPDATE);
+
+		return commerceWishListLocalService.updateCommerceWishList(
+			commerceWishListId, name, defaultWishList);
+	}
+
+	protected void checkManagePermission(long groupId) throws PortalException {
+		PortletResourcePermission portletResourcePermission =
+			_commerceWishListModelResourcePermission.
+				getPortletResourcePermission();
+
+		portletResourcePermission.check(
+			getPermissionChecker(), groupId,
+			CommerceWishListActionKeys.MANAGE_COMMERCE_WISH_LISTS);
+	}
+
+	private static volatile ModelResourcePermission<CommerceWishList>
+		_commerceWishListModelResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				CommerceWishListServiceImpl.class,
+				"_commerceWishListModelResourcePermission",
+				CommerceWishList.class);
+
 }
