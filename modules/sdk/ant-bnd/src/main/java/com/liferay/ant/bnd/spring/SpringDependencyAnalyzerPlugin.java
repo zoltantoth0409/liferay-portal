1a3d8ded049bd8e397cfbc1b6f24b392e06d6eb5
Alberto Chaparro <alberto.chaparro@liferay.com>
2018-02-09 11:18:54 +0100

    LPS-76926 Allow compatibility with micro versions of requireSchemaVersion. Also allow to define a range of requireSchemaVersions. Filter to state OK to do not activate the module when an upgrade is in progress

--- modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/spring/SpringDependencyAnalyzerPlugin.java
+++ modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/spring/SpringDependencyAnalyzerPlugin.java
@@ -25,8 +25,9 @@ import aQute.bnd.osgi.Jar;
 import aQute.bnd.osgi.Resource;
 import aQute.bnd.osgi.WriteResource;
 import aQute.bnd.service.AnalyzerPlugin;
-
+import aQute.bnd.version.Version;
 import aQute.bnd.version.VersionRange;
+
 import aQute.lib.io.IO;
 
 import java.io.OutputStream;
@@ -85,27 +86,36 @@ public class SpringDependencyAnalyzerPlugin implements AnalyzerPlugin {
 		return false;
 	}
 
-	protected String getReleaseInfo(Analyzer analyzer) {
+	protected String getReleaseInfo(Analyzer analyzer) throws RuntimeException {
 		String property = analyzer.getProperty("Liferay-Require-SchemaVersion");
 
 		if (property == null) {
 			return "";
 		}
 
-		String schemaVersionFilter =
-			"(release.schema.version=" + property + ")";
+		VersionRange versionRange;
 
-		if (VersionRange.isVersionRange(property)) {
-			VersionRange versionRange =
-				VersionRange.parseVersionRange(property);
+		if (Version.isVersion(property)) {
+			Version version = Version.parseVersion(property);
 
-			schemaVersionFilter = versionRange.toFilter();
-
-			schemaVersionFilter = schemaVersionFilter.replaceAll(
-				"version", "release.schema.version");
+			versionRange = new VersionRange(
+				true, new Version(version.getMajor(), version.getMinor(), 0),
+				new Version(version.getMajor(), version.getMinor() + 1, 0),
+				false);
+		}
+		else if (VersionRange.isVersionRange(property)) {
+			versionRange = VersionRange.parseVersionRange(property);
+		}
+		else {
+			throw new RuntimeException(
+				"Invalid format for Liferay-Require-SchemaVersion. Use a " +
+					"version with syntax <major>.<minor>.<micro>[.qualifier] " +
+						"or a range of versions");
 		}
 
-		StringBuffer sb = new StringBuffer(6);
+		String versionRangeFilter = versionRange.toFilter();
+
+		StringBuffer sb = new StringBuffer(8);
 
 		sb.append("com.liferay.portal.kernel.model.Release ");
 		sb.append("(&(release.bundle.symbolic.name=");
@@ -115,11 +125,16 @@ public class SpringDependencyAnalyzerPlugin implements AnalyzerPlugin {
 		sb.append(entry.getKey());
 
 		sb.append(")");
-		sb.append(schemaVersionFilter);
-		sb.append(")");
+		sb.append(
+			versionRangeFilter.replaceAll("version", "release.schema.version"));
+		sb.append("(release.state=");
+		sb.append(_STATE_GOOD);
+		sb.append("))");
 
 		return sb.toString();
 	}
+
+	private static final int _STATE_GOOD = 0;
 
 	private static class ContextDependencyWriter extends WriteResource {
 
