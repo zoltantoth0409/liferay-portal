5d6120deac336c06c5bc6be9810424a3e9de420e
Brian Chan <brian.chan@liferay.com>
2018-02-12 10:47:23 -0800

    LPS-75010 Apply petra string StringPool to util-taglib

--- util-taglib/src/com/liferay/taglib/util/AssetCategoryUtil.java
+++ util-taglib/src/com/liferay/taglib/util/AssetCategoryUtil.java
@@ -20,7 +20,7 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
