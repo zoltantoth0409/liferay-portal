919781adc89e00e3c29352f75c068c398d8d03d9
Roberto Díaz <roberto.diaz@liferay.com>
2018-02-12 10:50:43 +0100

    LPS-75728 Use the same logic as used in master

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_5/UpgradeExpando.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_5/UpgradeExpando.java
@@ -14,12 +14,7 @@
 
 package com.liferay.portal.upgrade.v7_0_5;
 
-import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.StringBundler;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 /**
  * @author Cristina González
@@ -27,30 +22,9 @@ import java.sql.ResultSet;
 public class UpgradeExpando extends UpgradeProcess {
 
 	protected void deleteOrphanExpandoRow() throws Exception {
-		StringBundler sb = new StringBundler(3);
-
-		sb.append("select rowId_ from ExpandoRow where rowId_ not in (select ");
-		sb.append("distinct ExpandoRow.rowId_ from  ExpandoRow inner join ");
-		sb.append("ExpandoValue on ExpandoRow.rowId_ = ExpandoValue.rowId_)");
-
-		try (PreparedStatement ps1 =
-				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
-					connection, "delete from ExpandoRow where rowId_ = ?");
-			PreparedStatement ps2 = connection.prepareStatement(
-				sb.toString())) {
-
-			try (ResultSet rs = ps2.executeQuery()) {
-				while (rs.next()) {
-					long rowId = rs.getInt(1);
-
-					ps1.setLong(1, rowId);
-
-					ps1.addBatch();
-				}
-
-				ps1.executeBatch();
-			}
-		}
+		runSQL(
+			"delete from ExpandoRow where rowId_ not in (select rowId_ from " +
+				"ExpandoValue)");
 	}
 
 	@Override
