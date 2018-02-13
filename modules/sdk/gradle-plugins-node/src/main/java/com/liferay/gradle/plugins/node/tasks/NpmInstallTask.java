44b19b9ecdeb59b05f7e5c95892be22196d2e991
Peter Shin <peter.shin@liferay.com>
2018-02-13 12:50:45 -0800

    LPS-77996

--- modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/NpmInstallTask.java
+++ modules/sdk/gradle-plugins-node/src/main/java/com/liferay/gradle/plugins/node/tasks/NpmInstallTask.java
@@ -34,6 +34,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -102,6 +103,10 @@ public class NpmInstallTask extends ExecuteNpmTask {
 		return GradleUtil.toFile(getProject(), _nodeModulesCacheDir);
 	}
 
+	public File getNodeModulesDigestFile() {
+		return GradleUtil.toFile(getProject(), _nodeModulesDigestFile);
+	}
+
 	public File getNodeModulesDir() {
 		Project project = getProject();
 
@@ -138,6 +143,10 @@ public class NpmInstallTask extends ExecuteNpmTask {
 		boolean nodeModulesCacheNativeSync) {
 
 		_nodeModulesCacheNativeSync = nodeModulesCacheNativeSync;
+	}
+
+	public void setNodeModulesDigestFile(Object nodeModulesDigestFile) {
+		_nodeModulesDigestFile = nodeModulesDigestFile;
 	}
 
 	public void setRemoveShrinkwrappedUrls(Object removeShrinkwrappedUrls) {
@@ -178,7 +187,12 @@ public class NpmInstallTask extends ExecuteNpmTask {
 					logger.info("Cache for {} is disabled", this);
 				}
 
-				_npmInstall(reset);
+				if (_isCheckDigest()) {
+					_npmInstallCheckDigest(this, reset);
+				}
+				else {
+					_npmInstall(reset);
+				}
 			}
 		}
 		finally {
@@ -345,6 +359,31 @@ public class NpmInstallTask extends ExecuteNpmTask {
 		}
 	}
 
+	private static synchronized void _npmInstallCheckDigest(
+			NpmInstallTask npmInstallTask, boolean reset)
+		throws Exception {
+
+		String digest = _getNodeModulesCacheDigest(npmInstallTask);
+
+		byte[] digestBytes = digest.getBytes(StandardCharsets.UTF_8);
+
+		File nodeModulesDigestFile = npmInstallTask.getNodeModulesDigestFile();
+
+		if (nodeModulesDigestFile.exists()) {
+			byte[] bytes = Files.readAllBytes(nodeModulesDigestFile.toPath());
+
+			if (!Arrays.equals(bytes, digestBytes)) {
+				File nodeModulesDir = npmInstallTask.getNodeModulesDir();
+
+				nodeModulesDir.delete();
+			}
+		}
+
+		npmInstallTask._npmInstall(reset);
+
+		Files.write(nodeModulesDigestFile.toPath(), digestBytes);
+	}
+
 	private static void _removeBinDirLinks(
 			final Logger logger, File nodeModulesDir)
 		throws IOException {
@@ -397,6 +436,21 @@ public class NpmInstallTask extends ExecuteNpmTask {
 
 		if (!pluginContainer.hasPlugin("com.liferay.cache") &&
 			(getNodeModulesCacheDir() != null)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isCheckDigest() {
+		Project project = getProject();
+
+		PluginContainer pluginContainer = project.getPlugins();
+
+		if (!pluginContainer.hasPlugin("com.liferay.cache") &&
+			(getNodeModulesCacheDir() == null) &&
+			(getNodeModulesDigestFile() != null)) {
 
 			return true;
 		}
@@ -460,6 +514,7 @@ public class NpmInstallTask extends ExecuteNpmTask {
 
 	private Object _nodeModulesCacheDir;
 	private boolean _nodeModulesCacheNativeSync = true;
+	private Object _nodeModulesDigestFile;
 	private Object _removeShrinkwrappedUrls;
 
 }
