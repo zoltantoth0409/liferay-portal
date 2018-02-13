5866d3a5e97a6e4cbb98fdaabca0b496a4b5aa03
André de Oliveira <andre.oliveira@liferay.com>
2018-02-13 15:47:23 -0800

    LPS-77358 portal-search-elasticsearch6-api: Rename packages after git mv
    https://issues.liferay.com/browse/LPS-77358

--- modules/apps/foundation/portal-search-elasticsearch6/portal-search-elasticsearch6-api/src/main/java/com/liferay/portal/search/elasticsearch6/settings/IndexSettingsContributor.java
+++ modules/apps/foundation/portal-search-elasticsearch6/portal-search-elasticsearch6-api/src/main/java/com/liferay/portal/search/elasticsearch6/settings/IndexSettingsContributor.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.search.elasticsearch.settings;
+package com.liferay.portal.search.elasticsearch6.settings;
 
 import aQute.bnd.annotation.ProviderType;
 
