04a88b070b4be3c9f607e986418f62e9430fe209
Zoltán Takács <zoltan.takacs@liferay.com>
2018-02-13 19:11:46 +0100

    COMMERCE-299 Add Apio Architect Forms representation

---
+++ modules/etl/talend/talend-runtime/src/main/java/com/liferay/talend/runtime/apio/jsonld/ApioForm.java
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talend.runtime.apio.jsonld;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import com.liferay.talend.runtime.apio.form.Property;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Represent the Apio Architect Forms
+ *
+ * @author Zoltán Takács
+ */
+public class ApioForm extends ApioBaseResponse {
+
+	public ApioForm(JsonNode responseJsonNode) throws IOException {
+		super(responseJsonNode);
+
+		_validateForm();
+	}
+
+	/**
+	 * Parses the supportedProperty JsonNode of the form
+	 *
+	 * @return description of the Form or empty string if not present in the
+	 *         String
+	 */
+	public String getDescription() {
+		JsonNode jsonNode = responseJsonNode.path(ApioConstants.DESCRIPTION);
+
+		return jsonNode.asText();
+	}
+
+	/**
+	 * Determines the supported properties of the resource collection and
+	 * retruns them in a List
+	 *
+	 * @return <code>List</code> of <code>Operation</code>, empty List otherwise
+	 */
+	public List<Property> getSupportedProperties() {
+		JsonNode supportedPropertiesJsonNode = getSupportedPropertiesJsonNode();
+
+		if (!supportedPropertiesJsonNode.isArray() ||
+			(supportedPropertiesJsonNode.size() == 0)) {
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Unable to fetch the resource's supported properties");
+			}
+
+			return Collections.<Property>emptyList();
+		}
+
+		List<Property> supportedProperties = new ArrayList<>();
+
+		for (final JsonNode jsonNode : supportedPropertiesJsonNode) {
+			JsonNode typeJsonNode = jsonNode.path(ApioConstants.TYPE);
+
+			String type = typeJsonNode.asText();
+
+			if (!type.equals(HydraConstants.SUPPORTED_PROPERTY)) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(
+						String.format("Skipping unexpected field: %s", type),
+						type);
+				}
+
+				continue;
+			}
+
+			JsonNode propertyNameJsonNode = jsonNode.path(
+				ApioConstants.PROPERTY);
+			JsonNode readableJsonNode = jsonNode.path(ApioConstants.READABLE);
+			JsonNode requiredJsonNode = jsonNode.path(ApioConstants.REQUIRED);
+			JsonNode writeableJsonNode = jsonNode.path(ApioConstants.WRITEABLE);
+
+			try {
+				Property property = new Property(
+					propertyNameJsonNode.asText(), requiredJsonNode.asBoolean(),
+					readableJsonNode.asBoolean(),
+					writeableJsonNode.asBoolean());
+
+				supportedProperties.add(property);
+			}
+			catch (IllegalArgumentException iae) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(
+						String.format(
+							"Unsupported property: %s", iae.getMessage()),
+						iae);
+				}
+			}
+		}
+
+		return Collections.unmodifiableList(supportedProperties);
+	}
+
+	public JsonNode getSupportedPropertiesJsonNode() {
+		return findJsonNode(ApioConstants.SUPPORTED_PROPERTY);
+	}
+
+	/**
+	 * Parses the supportedProperty JsonNode of the form
+	 *
+	 * @return title of the Form or empty string if not present in the
+	 *         String
+	 */
+	public String getTitle() {
+		JsonNode jsonNode = responseJsonNode.path(ApioConstants.TITLE);
+
+		return jsonNode.asText();
+	}
+
+	private void _validateForm() throws IOException {
+		if (!HydraConstants.CLASS.equals(getTypeNode().asText())) {
+			throw new IOException("The given resource is not a from");
+		}
+	}
+
+	private static final Logger _log = LoggerFactory.getLogger(ApioForm.class);
+
+}
