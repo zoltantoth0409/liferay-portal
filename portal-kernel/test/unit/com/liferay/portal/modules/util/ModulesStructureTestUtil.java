c448095714e0389f70ac8bcb0c40c849a247ef04
tomwang2011 <tong.wang@liferay.com>
2018-02-13 12:11:36 -0800

    LPS-75010 Apply petra string StringPool to portal-kernel

--- portal-kernel/test/unit/com/liferay/portal/modules/util/ModulesStructureTestUtil.java
+++ portal-kernel/test/unit/com/liferay/portal/modules/util/ModulesStructureTestUtil.java
@@ -17,11 +17,11 @@ package com.liferay.portal.modules.util;
 import aQute.bnd.osgi.Constants;
 
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.File;
