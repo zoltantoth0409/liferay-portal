75133fe8bfecabaf914d505c0b808179ce76209a
Jorge Díaz <jorge.diaz@liferay.com>
2018-02-14 11:59:44 +0100

    LPS-72496 7.0.x only - re-add JournalServiceVerifyProcess.verifyTree() method as deprecated to avoid changing modules version

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/verify/JournalServiceVerifyProcess.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/verify/JournalServiceVerifyProcess.java
@@ -75,6 +75,7 @@ import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.Node;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.upgrade.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.verify.VerifyLayout;
 import com.liferay.portal.verify.VerifyProcess;
 import com.liferay.portal.verify.VerifyResourcePermissions;
@@ -976,6 +977,19 @@ public class JournalServiceVerifyProcess extends VerifyLayout {
 			_verifyResourcePermissions.verify(
 				new JournalArticleVerifiableModel());
 			_verifyResourcePermissions.verify(new JournalFeedVerifiableModel());
+		}
+	}
+
+	/**
+	 * @deprecated As of 3.24.4
+	 */
+	protected void verifyTree() throws Exception {
+		try (LoggingTimer loggingTimer = new LoggingTimer()) {
+			long[] companyIds = PortalInstances.getCompanyIdsBySQL();
+
+			for (long companyId : companyIds) {
+				_journalFolderLocalService.rebuildTree(companyId);
+			}
 		}
 	}
 
