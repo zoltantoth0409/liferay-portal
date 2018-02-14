40dc14b9f8f5f47a3be390aad1f3a7f556851ccf
Vendel Toreki <vendel.toreki@liferay.com>
2018-02-14 16:32:48 +0100

    LPS-78006 Add new Filter for WebProxy POST requests of type x-www-form-urlencoded to keep the Request InputStream accessible for PortletBridgeServlet by caching it

---
+++ modules/apps/foundation/web-proxy/web-proxy-web/src/main/java/com/liferay/web/proxy/web/internal/servlet/filter/WebProxyServletFilter.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.web.proxy.web.internal.servlet.filter;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.BaseFilter;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.web.proxy.web.internal.servlet.request.WebProxyServletRequest;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Daniel Couso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"before-filter=Valid Host Name Filter", "dispatcher=FORWARD",
+		"dispatcher=REQUEST", "servlet-context-name=",
+		"servlet-filter-name=Web Proxy Filter",
+		"url-pattern=/o/web-proxy-web/pbhs/*"
+	},
+	service = Filter.class
+)
+public class WebProxyServletFilter extends BaseFilter {
+
+	@Override
+	public boolean isFilterEnabled(
+		HttpServletRequest request, HttpServletResponse response) {
+
+		String contentType = request.getHeader(HttpHeaders.CONTENT_TYPE);
+
+		if ((contentType != null) &&
+			contentType.startsWith(
+				ContentTypes.APPLICATION_X_WWW_FORM_URLENCODED)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	protected Log getLog() {
+		return _log;
+	}
+
+	@Override
+	protected void processFilter(
+			HttpServletRequest request, HttpServletResponse response,
+			FilterChain filterChain)
+		throws Exception {
+
+		WebProxyServletRequest webProxyServletRequest =
+			new WebProxyServletRequest(request);
+
+		processFilter(
+			WebProxyServletFilter.class.getName(), webProxyServletRequest,
+			response, filterChain);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		WebProxyServletFilter.class);
+
+}
