ee76c3bc46ae95d92b92cc719b1e367c0f53a647
Máté Thurzó <mate.thurzo@liferay.com>
2018-02-15 09:43:31 +0100

    LPS-77889 Add new publish model menu item tag

---
+++ modules/apps/web-experience/export-import/export-import-changeset-taglib/src/main/java/com/liferay/exportimport/changeset/taglib/servlet/taglib/PublishModelMenuItemTag.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.changeset.taglib.servlet.taglib;
+
+import com.liferay.exportimport.changeset.Changeset;
+import com.liferay.exportimport.changeset.ChangesetManager;
+import com.liferay.exportimport.changeset.ChangesetManagerUtil;
+import com.liferay.exportimport.changeset.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Mate Thurzo
+ */
+public class PublishModelMenuItemTag extends IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		Changeset.Builder builder = Changeset.create();
+
+		Changeset changeset = builder.addStagedModel(
+			() -> _stagedModel
+		).build();
+
+		ChangesetManager changesetManager =
+			ChangesetManagerUtil.getChangesetManager();
+
+		changesetManager.addChangeset(changeset);
+
+		return EVAL_BODY_INCLUDE;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	public void setStagedModel(StagedModel stagedModel) {
+		_stagedModel = stagedModel;
+	}
+
+	@Override
+	protected void cleanUp() {
+		_changesetUuid = StringPool.BLANK;
+		_stagedModel = null;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-export-import-changeset:publish-model-menu-item:" +
+				"changesetUuid",
+			_changesetUuid);
+	}
+
+	private static final String _PAGE = "/publish_model_menu_item/page.jsp";
+
+	private String _changesetUuid = StringPool.BLANK;
+	private StagedModel _stagedModel;
+
+}
