cdc832a9a3b08d9dfee17fae73a1e55e0ec3d8f6
Peter Borkuti <peter.borkuti@liferay.com>
2018-02-15 13:33:00 +0100

    LPS-78051 process-duration tag for staging publish processes screen

---
+++ modules/apps/web-experience/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/servlet/taglib/ProcessDurationTag.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.taglib.servlet.taglib;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.backgroundtask.BackgroundTask;
+import com.liferay.staging.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Peter Borkuti
+ */
+@ProviderType
+public class ProcessDurationTag extends IncludeTag {
+
+	public void setBackgroundTask(BackgroundTask backgroundTask) {
+		_backgroundTask = backgroundTask;
+	}
+
+	public void setListView(boolean listView) {
+		_listView = listView;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	@Override
+	protected void cleanUp() {
+		_backgroundTask = null;
+		_listView = false;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-staging:process-duration:backgroundTask", _backgroundTask);
+		request.setAttribute(
+			"liferay-staging:process-duration:listView", _listView);
+	}
+
+	private static final String _PAGE = "/process_duration/page.jsp";
+
+	private BackgroundTask _backgroundTask;
+	private boolean _listView;
+
+}
---
+++ modules/apps/web-experience/staging/staging-taglib/src/main/resources/META-INF/resources/process_duration/init.jsp
@@ -0,0 +1,35 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+BackgroundTask backgroundTask = (BackgroundTask)request.getAttribute("liferay-staging:process-duration:backgroundTask");
+
+boolean listView = GetterUtil.getBoolean(request.getAttribute("liferay-staging:process-duration:listView"));
+
+Date endDate = backgroundTask.getCompletionDate();
+
+Date startDate = backgroundTask.getCreateDate();
+
+long duration = 0;
+
+if ((endDate != null) && (startDate != null) && (startDate.getTime() < endDate.getTime())) {
+	duration = endDate.getTime() - startDate.getTime();
+}
+
+String timeDescription = LanguageUtil.getTimeDescription(themeDisplay.getLocale(), duration, true);
+%>
