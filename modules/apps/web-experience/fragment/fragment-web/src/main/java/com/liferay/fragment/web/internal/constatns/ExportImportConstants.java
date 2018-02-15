890168c1cc11b6fe3c5b508d727cf5292d3979f6
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-02-15 17:50:25 +0100

    LPS-77816 Adds new constant for the temp folder

---
+++ modules/apps/web-experience/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/constatns/ExportImportConstants.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.constatns;
+
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.model.FragmentEntry;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class ExportImportConstants {
+
+	public static final String FRAGMENT_COLLECTION_TEMP_FOLDER_NAME =
+		FragmentCollection.class.getName();
+
+	public static final String FRAGMENT_ENTRY_TEMP_FOLDER_NAME =
+		FragmentEntry.class.getName();
+
+}
