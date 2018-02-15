a389c90ca56c5fec083974d01b71b22fd7d24fcd
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2018-02-15 08:29:32 +0100

    LPS-78007 Log reduced in case a SQL exception is thrown when updating a table. Fallback applied successfully informed.

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/UpgradeProcess.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/UpgradeProcess.java
@@ -437,7 +437,9 @@ public abstract class UpgradeProcess
 			}
 			catch (SQLException sqle) {
 				if (_log.isWarnEnabled()) {
-					_log.warn("Fallback to recreating the table", sqle);
+					_log.warn(
+						"Applying fallback consisting on recreating the " +
+							"table due to: " + sqle.getMessage());
 				}
 
 				Field tableColumnsField = tableClass.getField("TABLE_COLUMNS");
@@ -450,6 +452,12 @@ public abstract class UpgradeProcess
 					tableName, (Object[][])tableColumnsField.get(null),
 					(String)tableSQLCreateField.get(null),
 					(String[])tableSQLAddIndexesField.get(null));
+
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						"Fallback applied successfully. Table upgraded " +
+							"correctly.");
+				}
 			}
 		}
 	}
