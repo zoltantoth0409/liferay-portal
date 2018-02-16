9ca794d95f8a140c4486ddc79a9befa3112c3599
Adolfo Pérez <adolfo.perez@liferay.com>
2018-02-16 14:09:21 +0100

    LPS-78091 Create base class to remove duplication

---
+++ modules/apps/collaboration/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/upload/BaseMBUploadFileEntryHandler.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.web.internal.upload;
+
+import com.liferay.message.boards.constants.MBMessageConstants;
+import com.liferay.message.boards.service.MBMessageService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.TempFileEntryUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.upload.UploadFileEntryHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Adolfo Pérez
+ */
+public abstract class BaseMBUploadFileEntryHandler
+	implements UploadFileEntryHandler {
+
+	@Override
+	public FileEntry upload(UploadPortletRequest uploadPortletRequest)
+		throws IOException, PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)uploadPortletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		long categoryId = ParamUtil.getLong(uploadPortletRequest, "categoryId");
+
+		try (InputStream inputStream = getFileAsStream(uploadPortletRequest)) {
+			String tempFileName = TempFileEntryUtil.getTempFileName(
+				getFileName(uploadPortletRequest));
+
+			MBMessageService mbMessageService = getMBMessageService();
+
+			return mbMessageService.addTempAttachment(
+				themeDisplay.getScopeGroupId(), categoryId,
+				MBMessageConstants.TEMP_FOLDER_NAME, tempFileName, inputStream,
+				getContentType(uploadPortletRequest));
+		}
+	}
+
+	protected String getContentType(UploadPortletRequest uploadPortletRequest) {
+		return uploadPortletRequest.getContentType(getParameterName());
+	}
+
+	protected InputStream getFileAsStream(
+			UploadPortletRequest uploadPortletRequest)
+		throws IOException {
+
+		return uploadPortletRequest.getFileAsStream(getParameterName());
+	}
+
+	protected String getFileName(UploadPortletRequest uploadPortletRequest) {
+		return uploadPortletRequest.getFileName(getParameterName());
+	}
+
+	protected abstract MBMessageService getMBMessageService();
+
+	protected abstract String getParameterName();
+
+}
--- modules/apps/collaboration/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/upload/TempAttachmentMBUploadFileEntryHandler.java
+++ modules/apps/collaboration/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/upload/TempAttachmentMBUploadFileEntryHandler.java
@@ -14,19 +14,7 @@
 
 package com.liferay.message.boards.web.internal.upload;
 
-import com.liferay.message.boards.constants.MBMessageConstants;
 import com.liferay.message.boards.service.MBMessageService;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.repository.model.FileEntry;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.upload.UploadPortletRequest;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.TempFileEntryUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.upload.UploadFileEntryHandler;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -36,34 +24,17 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(service = TempAttachmentMBUploadFileEntryHandler.class)
 public class TempAttachmentMBUploadFileEntryHandler
-	implements UploadFileEntryHandler {
+	extends BaseMBUploadFileEntryHandler {
 
 	@Override
-	public FileEntry upload(UploadPortletRequest uploadPortletRequest)
-		throws IOException, PortalException {
-
-		ThemeDisplay themeDisplay =
-			(ThemeDisplay)uploadPortletRequest.getAttribute(
-				WebKeys.THEME_DISPLAY);
-
-		long categoryId = ParamUtil.getLong(uploadPortletRequest, "categoryId");
-		String fileName = uploadPortletRequest.getFileName(_PARAMETER_NAME);
-		String contentType = uploadPortletRequest.getContentType(
-			_PARAMETER_NAME);
-
-		try (InputStream inputStream =
-				uploadPortletRequest.getFileAsStream(_PARAMETER_NAME)) {
-
-			String tempFileName = TempFileEntryUtil.getTempFileName(fileName);
-
-			return _mbMessageService.addTempAttachment(
-				themeDisplay.getScopeGroupId(), categoryId,
-				MBMessageConstants.TEMP_FOLDER_NAME, tempFileName, inputStream,
-				contentType);
-		}
+	protected MBMessageService getMBMessageService() {
+		return _mbMessageService;
 	}
 
-	private static final String _PARAMETER_NAME = "file";
+	@Override
+	protected String getParameterName() {
+		return "file";
+	}
 
 	@Reference
 	private MBMessageService _mbMessageService;
