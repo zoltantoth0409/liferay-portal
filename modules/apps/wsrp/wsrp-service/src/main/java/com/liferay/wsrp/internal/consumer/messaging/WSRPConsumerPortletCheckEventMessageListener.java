5f00f522b90a04dd1fdf6740a7f4bfe7d4862401
Shinn Lok <shinn.lok@liferay.com>
2018-02-15 18:05:36 -0800

    LPS-77944 Unimplemented scheduler

--- modules/apps/wsrp/wsrp-service/src/main/java/com/liferay/wsrp/internal/consumer/messaging/WSRPConsumerPortletCheckEventMessageListener.java
+++ modules/apps/wsrp/wsrp-service/src/main/java/com/liferay/wsrp/internal/consumer/messaging/WSRPConsumerPortletCheckEventMessageListener.java
@@ -14,13 +14,25 @@
 
 package com.liferay.wsrp.internal.consumer.messaging;
 
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.SchedulerEntry;
+import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.Trigger;
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.wsrp.configuration.WSRPGroupServiceConfiguration;
 import com.liferay.wsrp.service.WSRPConsumerPortletLocalService;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -28,16 +40,46 @@ import org.osgi.service.component.annotations.Reference;
  * @author Peter Fellwock
  */
 @Component(
-	immediate = true, property = {"destination.name=" + DestinationNames.WSRP},
+	configurationPid = "com.liferay.twitter.configuration.TwitterConfiguration",
+	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true,
+	property = {"destination.name=" + DestinationNames.WSRP},
 	service = MessageListener.class
 )
 public class WSRPConsumerPortletCheckEventMessageListener
 	extends BaseMessageListener {
 
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		WSRPGroupServiceConfiguration wsrpGroupServiceConfiguration =
+			ConfigurableUtil.createConfigurable(
+				WSRPGroupServiceConfiguration.class, properties);
+
+		Class<?> clazz = getClass();
+
+		String className = clazz.getName();
+
+		Trigger trigger = _triggerFactory.createTrigger(
+			className, className, null, null,
+			wsrpGroupServiceConfiguration.failedConsumersCheckInterval(),
+			TimeUnit.SECOND);
+
+		SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
+			className, trigger);
+
+		_schedulerEngineHelper.register(
+			this, schedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
+	}
+
 	@Override
 	protected void doReceive(Message message) throws Exception {
 		_wsrpConsumerPortletLocalService.initFailedWSRPConsumerPortlets();
 	}
+
+	@Reference
+	private SchedulerEngineHelper _schedulerEngineHelper;
+
+	@Reference
+	private TriggerFactory _triggerFactory;
 
 	@Reference
 	private WSRPConsumerPortletLocalService _wsrpConsumerPortletLocalService;
