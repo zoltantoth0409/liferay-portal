46ae24160a106817328fbe1a02622f0a5ecbc218
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2018-02-16 16:06:27 -0800

    LPS-77343 Remove unused method

--- modules/sdk/gradle-plugins-target-platform/src/main/java/com/liferay/gradle/plugins/target/platform/internal/util/GradleUtil.java
+++ modules/sdk/gradle-plugins-target-platform/src/main/java/com/liferay/gradle/plugins/target/platform/internal/util/GradleUtil.java
@@ -14,17 +14,6 @@
 
 package com.liferay.gradle.plugins.target.platform.internal.util;
 
-import java.io.File;
-
-import java.net.URI;
-import java.net.URL;
-import java.net.URLClassLoader;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.Callable;
-
 /**
  * @author Gregory Amerson
  */
@@ -42,37 +31,6 @@ public class GradleUtil extends com.liferay.gradle.util.GradleUtil {
 		}
 
 		return false;
-	}
-
-	public static <T> T withClasspath(
-			Iterable<File> classpath, Callable<T> callable)
-		throws Exception {
-
-		Thread currentThread = Thread.currentThread();
-
-		URLClassLoader contextURLClassLoader =
-			(URLClassLoader)currentThread.getContextClassLoader();
-
-		List<URL> urls = new ArrayList<>();
-
-		for (File file : classpath) {
-			URI uri = file.toURI();
-
-			urls.add(uri.toURL());
-		}
-
-		Collections.addAll(urls, contextURLClassLoader.getURLs());
-
-		try (URLClassLoader urlClassLoader = new URLClassLoader(
-				urls.toArray(new URL[urls.size()]), null)) {
-
-			currentThread.setContextClassLoader(urlClassLoader);
-
-			return callable.call();
-		}
-		finally {
-			currentThread.setContextClassLoader(contextURLClassLoader);
-		}
 	}
 
 }
