4d6afacb79cb30a27f322636bfa2b66f0161cec9
Preston-Crary <preston.crary@liferay.com>
2018-02-20 15:40:42 -0800

    LPS-77585 Move verify to internal.

--- modules/apps/shopping/shopping-service/src/main/java/com/liferay/shopping/internal/verify/ShoppingServiceVerifyProcess.java
+++ modules/apps/shopping/shopping-service/src/main/java/com/liferay/shopping/internal/verify/ShoppingServiceVerifyProcess.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.shopping.verify;
+package com.liferay.shopping.internal.verify;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
