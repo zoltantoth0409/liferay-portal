79d25f26bd1e71bb001973d6829d58199ab3ece0
Lino Alves <lino.alves@liferay.com>
2018-02-20 18:51:38 -0300

    LPS-78136 adding portletResourcePermission to DDMForm

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/security/permission/resource/DDMFormPortletResourcePermissionRegistrar.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.internal.security.permission.resource;
+
+import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
+import com.liferay.exportimport.kernel.staging.permission.StagingPermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.StagedPortletPermissionLogic;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lino Alves
+ */
+@Component(immediate = true)
+public class DDMFormPortletResourcePermissionRegistrar {
+
+	public static final String RESOURCE_NAME =
+		"com.liferay.dynamic.data.mapping";
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("resource.name", RESOURCE_NAME);
+
+		_serviceRegistration = bundleContext.registerService(
+			PortletResourcePermission.class,
+			PortletResourcePermissionFactory.create(
+				RESOURCE_NAME,
+				new StagedPortletPermissionLogic(
+					_stagingPermission,
+					DDMPortletKeys.DYNAMIC_DATA_MAPPING_FORM_ADMIN)),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	private ServiceRegistration<PortletResourcePermission> _serviceRegistration;
+
+	@Reference
+	private StagingPermission _stagingPermission;
+
+}
