90078c8199507c50703c14d616e45d57f9e93e4d
Rafael Praxedes <rafael.praxedes@liferay.com>
2018-02-20 16:15:40 -0300

    LPS-77251 Use the save method from workflowDefinitionManager instead

--- modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/action/AddKaleoDefinitionVersionMVCActionCommand.java
+++ modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/action/AddKaleoDefinitionVersionMVCActionCommand.java
@@ -14,37 +14,16 @@
 
 package com.liferay.portal.workflow.kaleo.designer.web.internal.portlet.action;
 
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.portlet.LiferayPortletURL;
-import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
-import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.ServiceContextFactory;
-import com.liferay.portal.kernel.servlet.SessionErrors;
-import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.ResourceBundleLoader;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.kernel.uuid.PortalUUID;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.portal.kernel.workflow.WorkflowDefinitionManager;
-import com.liferay.portal.kernel.workflow.WorkflowException;
-import com.liferay.portal.workflow.kaleo.definition.Definition;
-import com.liferay.portal.workflow.kaleo.definition.parser.WorkflowModelParser;
+import com.liferay.portal.kernel.workflow.WorkflowDefinition;
 import com.liferay.portal.workflow.kaleo.designer.web.constants.KaleoDesignerPortletKeys;
 import com.liferay.portal.workflow.kaleo.designer.web.internal.constants.KaleoDesignerWebKeys;
 import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
-import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
-import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
 
 import java.util.Locale;
 import java.util.Map;
@@ -52,11 +31,8 @@ import java.util.ResourceBundle;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletException;
-import javax.portlet.PortletRequest;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Inácio Nery
@@ -77,58 +53,22 @@ public class AddKaleoDefinitionVersionMVCActionCommand
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
-		String content = ParamUtil.getString(actionRequest, "content");
-
-		Definition definition = null;
-
-		try {
-			definition = workflowModelParser.parse(content);
-		}
-		catch (Exception e) {
-			definition = new Definition(
-				StringPool.BLANK, StringPool.BLANK, content, 0);
-		}
-
-		String name = ParamUtil.getString(actionRequest, "name");
-
-		String definitionName = getDefinitionName(definition, name);
-
-		Map<Locale, String> titleMap = LocalizationUtil.getLocalizationMap(
-			actionRequest, "title");
-
-		String title = titleMap.get(LocaleUtil.getDefault());
-
-		String definitionDescription = getDefinitionDescription(
-			actionRequest, definition, title);
-
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
+		Map<Locale, String> titleMap = LocalizationUtil.getLocalizationMap(
+			actionRequest, "title");
+		String name = ParamUtil.getString(actionRequest, "name");
+		String content = ParamUtil.getString(actionRequest, "content");
+
+		WorkflowDefinition workflowDefinition =
+			workflowDefinitionManager.saveWorkflowDefinition(
+				themeDisplay.getCompanyId(), themeDisplay.getUserId(),
+				getTitle(titleMap), name, content.getBytes());
+
 		KaleoDefinitionVersion kaleoDefinitionVersion =
-			kaleoDefinitionVersionLocalService.
-				fetchLatestKaleoDefinitionVersion(
-					themeDisplay.getCompanyId(), definitionName, null);
-
-		ServiceContext serviceContext = ServiceContextFactory.getInstance(
-			KaleoDefinitionVersion.class.getName(), actionRequest);
-
-		serviceContext.setAttribute("status", WorkflowConstants.STATUS_DRAFT);
-
-		if (kaleoDefinitionVersion == null) {
-			kaleoDefinitionVersion =
-				kaleoDefinitionVersionLocalService.addKaleoDefinitionVersion(
-					definitionName, getTitle(titleMap), definitionDescription,
-					definition.getContent(), "0.1", serviceContext);
-		}
-		else {
-			String version = getNextVersion(
-				kaleoDefinitionVersion.getVersion());
-
-			kaleoDefinitionVersion =
-				kaleoDefinitionVersionLocalService.addKaleoDefinitionVersion(
-					definitionName, getTitle(titleMap), definitionDescription,
-					definition.getContent(), version, serviceContext);
-		}
+			kaleoDefinitionVersionLocalService.getLatestKaleoDefinitionVersion(
+				themeDisplay.getCompanyId(), workflowDefinition.getName());
 
 		actionRequest.setAttribute(
 			KaleoDesignerWebKeys.KALEO_DRAFT_DEFINITION,
