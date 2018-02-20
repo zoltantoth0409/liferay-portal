cdada71091e8537e47d13fe8f9bb17ce9defd4ff
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-02-20 08:43:18 +0100

    LPS-78082 Adding API to define social bookmark components

---
+++ modules/apps/collaboration/social/social-bookmarks-api/src/main/java/com/liferay/social/bookmarks/SocialBookmark.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.social.bookmarks;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Alejandro Tardín
+ */
+public interface SocialBookmark {
+
+	public String getName(Locale locale);
+
+	public String getPostUrl(String title, String url);
+
+	public String getType();
+
+	public void render(
+			String target, String title, String url, HttpServletRequest request,
+			HttpServletResponse response)
+		throws IOException, ServletException;
+
+}
