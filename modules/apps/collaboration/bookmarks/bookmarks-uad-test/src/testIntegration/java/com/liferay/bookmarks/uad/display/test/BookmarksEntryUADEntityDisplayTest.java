c58af92abc2405ab8a753c910286b3ef70c44086
Noah Sherrill <noah.sherrill@liferay.com>
2018-02-21 11:19:38 -0800

    LPS-77938 Create tests for UADEntityDisplay implementations

---
+++ modules/apps/collaboration/bookmarks/bookmarks-uad-test/src/testIntegration/java/com/liferay/bookmarks/uad/display/test/BookmarksEntryUADEntityDisplayTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bookmarks.uad.display.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.bookmarks.model.BookmarksEntry;
+import com.liferay.bookmarks.uad.constants.BookmarksUADConstants;
+import com.liferay.bookmarks.uad.test.BookmarksEntryUADEntityTestHelper;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.user.associated.data.test.util.BaseUADEntityDisplayTestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Noah Sherrill
+ */
+@RunWith(Arquillian.class)
+public class BookmarksEntryUADEntityDisplayTest
+	extends BaseUADEntityDisplayTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Override
+	protected BaseModel<?> addBaseModel(long userId) throws Exception {
+		BookmarksEntry bookmarksEntry =
+			_bookmarksEntryUADEntityTestHelper.addBookmarksEntry(userId);
+
+		_bookmarksEntries.add(bookmarksEntry);
+
+		return bookmarksEntry;
+	}
+
+	@Override
+	protected String getEntityTypeDescription() {
+		return "A link to another page or website";
+	}
+
+	@Override
+	protected String getUADRegistryKey() {
+		return BookmarksUADConstants.CLASS_NAME_BOOKMARKS_ENTRY;
+	}
+
+	@DeleteAfterTestRun
+	private final List<BookmarksEntry> _bookmarksEntries = new ArrayList<>();
+
+	@Inject
+	private BookmarksEntryUADEntityTestHelper
+		_bookmarksEntryUADEntityTestHelper;
+
+}
