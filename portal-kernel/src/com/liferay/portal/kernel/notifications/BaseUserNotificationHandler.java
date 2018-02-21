a45be3f44404ee24664133ab0ffb7104fdb0b92d
ricardocousolr <ricardo.couso@liferay.com>
2018-02-21 09:51:50 +0100

    LPS-77880 Pass argument applicable in new user notification feed entries

--- portal-kernel/src/com/liferay/portal/kernel/notifications/BaseUserNotificationHandler.java
+++ portal-kernel/src/com/liferay/portal/kernel/notifications/BaseUserNotificationHandler.java
@@ -79,7 +79,7 @@ public abstract class BaseUserNotificationHandler
 					});
 
 				userNotificationFeedEntry = new UserNotificationFeedEntry(
-					false, body, StringPool.BLANK);
+					false, body, StringPool.BLANK, false);
 			}
 
 			return userNotificationFeedEntry;
@@ -144,7 +144,11 @@ public abstract class BaseUserNotificationHandler
 
 		String link = getLink(userNotificationEvent, serviceContext);
 
-		return new UserNotificationFeedEntry(isActionable(), body, link);
+		boolean applicable = isApplicable(
+			userNotificationEvent, serviceContext);
+
+		return new UserNotificationFeedEntry(
+			isActionable(), body, link, applicable);
 	}
 
 	protected String getBody(
