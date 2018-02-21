524f2586f546e4a6d0905f2acff5e392df9bdf97
Lino Alves <lino.alves@liferay.com>
2018-02-21 17:43:14 -0300

    LPS-78136 adding model resource permission registar

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/security/permission/resource/DDMFormInstanceModelResourcePermissionRegistrar.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.internal.security.permission.resource;
+
+import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalService;
+import com.liferay.exportimport.kernel.staging.permission.StagingPermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.StagedModelPermissionLogic;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lino Alves
+ */
+@Component(immediate = true)
+public class DDMFormInstanceModelResourcePermissionRegistrar {
+
+	public static final String RESOURCE_NAME =
+		"com.liferay.dynamic.data.mapping";
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("model.class.name", DDMFormInstance.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				DDMFormInstance.class, DDMFormInstance::getFormInstanceId,
+				_formInstanceLocalService::getDDMFormInstance,
+				_portletResourcePermission,
+				(modelResourcePermission, consumer) -> consumer.accept(
+					new StagedModelPermissionLogic<>(
+						_stagingPermission,
+						DDMPortletKeys.DYNAMIC_DATA_MAPPING_FORM_ADMIN,
+						DDMFormInstance::getFormInstanceId))),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference(unbind = "-")
+	protected void setDDMFormInstanceLocalService(
+		DDMFormInstanceLocalService formInstanceLocalService) {
+
+		_formInstanceLocalService = formInstanceLocalService;
+	}
+
+	private DDMFormInstanceLocalService _formInstanceLocalService;
+
+	@Reference(target = "(resource.name=" + RESOURCE_NAME + ")")
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+	@Reference
+	private StagingPermission _stagingPermission;
+
+}
