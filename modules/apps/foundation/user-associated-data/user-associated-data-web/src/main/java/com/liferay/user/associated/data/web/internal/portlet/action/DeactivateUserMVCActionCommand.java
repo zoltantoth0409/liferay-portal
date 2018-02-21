33f1394b20b40d1ea5602f1d2c0b31682c6f5e2d
William Newbury <william.newbury@liferay.com>
2018-02-21 12:01:58 -0800

    LPS-77754 Reactivate group when deactivating the user for the purposes of gdpr

--- modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/action/DeactivateUserMVCActionCommand.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/action/DeactivateUserMVCActionCommand.java
@@ -14,9 +14,13 @@
 
 package com.liferay.user.associated.data.web.internal.portlet.action;
 
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.service.UserService;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -51,10 +55,24 @@ public class DeactivateUserMVCActionCommand extends BaseMVCActionCommand {
 		_userService.updateStatus(
 			selUserId, WorkflowConstants.STATUS_INACTIVE, new ServiceContext());
 
+		User selUser = _userLocalService.getUserById(selUserId);
+
+		Group group = selUser.getGroup();
+
+		group.setActive(true);
+
+		_groupLocalService.updateGroup(group);
+
 		String redirect = ParamUtil.getString(actionRequest, "redirect");
 
 		sendRedirect(actionRequest, actionResponse, redirect);
 	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 	@Reference
 	private UserService _userService;
