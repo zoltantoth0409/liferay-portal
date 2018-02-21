661a942789a7b8ff4bac800243dd50042f28289d
Preston-Crary <preston.crary@liferay.com>
2018-02-20 18:04:43 -0800

    LPS-77585 Move remaining methods to where they are used.

--- modules/apps/shopping/shopping-service/src/main/java/com/liferay/shopping/service/impl/ShoppingOrderLocalServiceImpl.java
+++ modules/apps/shopping/shopping-service/src/main/java/com/liferay/shopping/service/impl/ShoppingOrderLocalServiceImpl.java
@@ -23,11 +23,16 @@ import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
 import com.liferay.portal.kernel.settings.LocalizedValuesMap;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.kernel.util.CalendarUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.PwdGenerator;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.SubscriptionSender;
+import com.liferay.portal.kernel.util.TimeZoneUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.spring.extender.service.ServiceReference;
 import com.liferay.shopping.configuration.ShoppingGroupServiceOverriddenConfiguration;
@@ -65,11 +70,11 @@ import com.liferay.shopping.model.ShoppingOrderConstants;
 import com.liferay.shopping.model.ShoppingOrderItem;
 import com.liferay.shopping.model.impl.ShoppingCartItemImpl;
 import com.liferay.shopping.service.base.ShoppingOrderLocalServiceBaseImpl;
-import com.liferay.shopping.util.CreditCard;
 import com.liferay.shopping.util.ShoppingUtil;
 import com.liferay.shopping.util.comparator.OrderDateComparator;
 import com.liferay.subscription.service.SubscriptionLocalService;
 
+import java.util.Calendar;
 import java.util.Currency;
 import java.util.List;
 import java.util.Map;
@@ -799,10 +804,10 @@ public class ShoppingOrderLocalServiceImpl
 			else if (Validator.isNull(ccType)) {
 				throw new CCTypeException();
 			}
-			else if (!CreditCard.isValidNumber(ccNumber, ccType)) {
+			else if (!_isValidNumber(ccNumber, ccType)) {
 				throw new CCNumberException();
 			}
-			else if (!CreditCard.isValidExpirationDate(ccExpMonth, ccExpYear)) {
+			else if (!_isValidExpirationDate(ccExpMonth, ccExpYear)) {
 				throw new CCExpirationException();
 			}
 		}
@@ -813,6 +818,72 @@ public class ShoppingOrderLocalServiceImpl
 
 	@ServiceReference(type = SubscriptionLocalService.class)
 	protected SubscriptionLocalService subscriptionLocalService;
+
+	private static boolean _isValidExpirationDate(
+		int expirationMonth, int expirationYear) {
+
+		Calendar calendar = CalendarFactoryUtil.getCalendar(
+			TimeZoneUtil.getDefault(), LocaleUtil.getDefault());
+
+		if (CalendarUtil.isFuture(expirationMonth, expirationYear)) {
+			return true;
+		}
+		else if ((expirationMonth == calendar.get(Calendar.MONTH)) &&
+				 (expirationYear == calendar.get(Calendar.YEAR))) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private static boolean _isValidNumber(String number, String type) {
+		number = StringUtil.extractDigits(number);
+
+		if (type.equals("visa")) {
+			if (!number.startsWith("4")) {
+				return false;
+			}
+
+			if ((number.length() != 13) && (number.length() != 16)) {
+				return false;
+			}
+		}
+		else if (type.equals("mastercard")) {
+			if (!number.startsWith("51") && !number.startsWith("52") &&
+				!number.startsWith("53") && !number.startsWith("54") &&
+				!number.startsWith("55")) {
+
+				return false;
+			}
+
+			if (number.length() != 16) {
+				return false;
+			}
+		}
+		else if (type.equals("discover")) {
+			if (!number.startsWith("6011")) {
+				return false;
+			}
+
+			if (number.length() != 16) {
+				return false;
+			}
+		}
+		else if (type.equals("amex")) {
+			if (!number.startsWith("34") && !number.startsWith("35") &&
+				!number.startsWith("36") && !number.startsWith("37")) {
+
+				return false;
+			}
+
+			if (number.length() != 15) {
+				return false;
+			}
+		}
+
+		return Validator.isLUHN(number);
+	}
 
 	private ShoppingGroupServiceOverriddenConfiguration
 			_getShoppingGroupServiceOverriddenConfiguration(long groupId)
