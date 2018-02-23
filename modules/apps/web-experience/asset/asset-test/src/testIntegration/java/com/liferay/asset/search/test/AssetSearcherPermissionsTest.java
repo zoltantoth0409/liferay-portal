64486432774f50c827182391adb0ee4a08b935b3
Eric Yan <eric.yan@liferay.com>
2018-02-23 10:52:39 -0800

    LPS-78131 asset-test: rename class to further clarify the specific feature being tested
    https://issues.liferay.com/browse/LPS-78131

--- modules/apps/web-experience/asset/asset-test/src/testIntegration/java/com/liferay/asset/search/test/AssetSearcherPermissionsTest.java
+++ modules/apps/web-experience/asset/asset-test/src/testIntegration/java/com/liferay/asset/search/test/AssetSearcherPermissionsTest.java
@@ -38,7 +38,7 @@ import org.junit.runner.RunWith;
  * @author André de Oliveira
  */
 @RunWith(Arquillian.class)
-public class AssetSearcherTest {
+public class AssetSearcherPermissionsTest {
 
 	@ClassRule
 	@Rule
