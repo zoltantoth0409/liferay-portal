684e3b6b02e2d3fee1af249e236b04cf2e2ffc93
Chema Balsas <jose.balsas@liferay.com>
2018-02-26 15:47:15 +0100

    LPS-77926 Allows iframes in blogs

--- modules/apps/collaboration/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsContentEditorConfigContributor.java
+++ modules/apps/collaboration/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsContentEditorConfigContributor.java
@@ -71,7 +71,7 @@ public class BlogsContentEditorConfigContributor
 
 		sb.append("a[*](*); ");
 		sb.append(getAllowedContentText());
-		sb.append(" div(*); img[*] {height, width}; ");
+		sb.append(" div(*); iframe[*]; img[*] {height, width}; ");
 		sb.append(getAllowedContentLists());
 		sb.append(" p {text-align}; ");
 		sb.append(getAllowedContentTable());
--- modules/apps/foundation/portal-security/portal-security-antisamy/src/main/java/com/liferay/portal/security/antisamy/configuration/AntiSamyConfiguration.java
+++ modules/apps/foundation/portal-security/portal-security-antisamy/src/main/java/com/liferay/portal/security/antisamy/configuration/AntiSamyConfiguration.java
@@ -41,7 +41,7 @@ public interface AntiSamyConfiguration {
 	public String[] blacklist();
 
 	@Meta.AD(
-		deflt = "com.liferay.fragment.model.FragmentEntry|com.liferay.journal.model.JournalArticle",
+		deflt = "com.liferay.blogs.model.BlogsEntry|com.liferay.fragment.model.FragmentEntry|com.liferay.journal.model.JournalArticle",
 		name = "whitelist", required = false
 	)
 	public String[] whitelist();
