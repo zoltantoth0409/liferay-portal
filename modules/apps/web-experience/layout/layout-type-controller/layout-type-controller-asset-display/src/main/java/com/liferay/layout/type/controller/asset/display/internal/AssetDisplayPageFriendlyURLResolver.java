c40cfae75cf472cffa54057797875bb918192656
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-02-27 15:06:59 +0100

    LPS-78408 Create resolver for asset display pages

---
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-asset-display/src/main/java/com/liferay/layout/type/controller/asset/display/internal/AssetDisplayPageFriendlyURLResolver.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.type.controller.asset.display.internal;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.LayoutFriendlyURLComposite;
+import com.liferay.portal.kernel.portlet.FriendlyURLResolver;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(service = FriendlyURLResolver.class)
+public class AssetDisplayPageFriendlyURLResolver
+	implements FriendlyURLResolver {
+
+	@Override
+	public String getActualURL(
+			long companyId, long groupId, boolean privateLayout,
+			String mainPath, String friendlyURL, Map<String, String[]> params,
+			Map<String, Object> requestContext)
+		throws PortalException {
+
+		return null;
+	}
+
+	@Override
+	public LayoutFriendlyURLComposite getLayoutFriendlyURLComposite(
+			long companyId, long groupId, boolean privateLayout,
+			String friendlyURL, Map<String, String[]> params,
+			Map<String, Object> requestContext)
+		throws PortalException {
+
+		return null;
+	}
+
+	@Override
+	public String getURLSeparator() {
+		return "/_/";
+	}
+
+	@Reference
+	private Portal _portal;
+
+}
