f538ecfb496ab8630cab522d8c295ff12f83dbb5
pavel-savinov <pavel.savinov@liferay.com>
2018-02-27 12:22:03 +0100

    LPS-78279 Show configuration page and process selected fragment entry creating fragment entry link associated with the portlet preferences

---
+++ modules/apps/web-experience/fragment/fragment-display-web/src/main/java/com/liferay/fragment/display/web/internal/portlet/action/FragmentEntryDisplayConfigurationAction.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.display.web.internal.portlet.action;
+
+import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.fragment.display.web.internal.display.context.FragmentEntryDisplayContext;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.fragment.service.FragmentEntryLocalService;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + FragmentPortletKeys.FRAGMENT_DISPLAY},
+	service = ConfigurationAction.class
+)
+public class FragmentEntryDisplayConfigurationAction
+	extends DefaultConfigurationAction {
+
+	@Override
+	public void include(
+			PortletConfig portletConfig, HttpServletRequest request,
+			HttpServletResponse response)
+		throws Exception {
+
+		PortletRequest portletRequest = (PortletRequest)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_REQUEST);
+
+		PortletResponse portletResponse = (PortletResponse)request.getAttribute(
+			JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+		PortletPreferences portletPreferences = portletRequest.getPreferences();
+
+		FragmentEntryDisplayContext fragmentEntryDisplayContext =
+			new FragmentEntryDisplayContext(
+				portletRequest, portletResponse, portletPreferences,
+				_fragmentEntryLinkLocalService);
+
+		request.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT, fragmentEntryDisplayContext);
+
+		super.include(portletConfig, request, response);
+	}
+
+	@Override
+	public void processAction(
+			PortletConfig portletConfig, ActionRequest actionRequest,
+			ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		PortletPreferences portletPreferences = actionRequest.getPreferences();
+
+		long fragmentEntryId = ParamUtil.getLong(
+			actionRequest, "fragmentEntryId");
+
+		FragmentEntry fragmentEntry =
+			_fragmentEntryLocalService.getFragmentEntry(fragmentEntryId);
+
+		com.liferay.portal.kernel.model.PortletPreferences preferences =
+			_portletPreferencesLocalService.getPortletPreferences(
+				themeDisplay.getPlid(), PortletKeys.PREFS_OWNER_TYPE_LAYOUT,
+				themeDisplay.getPlid(), portletDisplay.getPortletResource());
+
+		long classNameId = _portal.getClassNameId(
+			com.liferay.portal.kernel.model.PortletPreferences.class);
+
+		_fragmentEntryLinkLocalService.
+			deleteLayoutPageTemplateEntryFragmentEntryLinks(
+				themeDisplay.getScopeGroupId(), classNameId,
+				preferences.getPortletPreferencesId());
+
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				themeDisplay.getScopeGroupId(), fragmentEntryId, classNameId,
+				preferences.getPortletPreferencesId(), fragmentEntry.getCss(),
+				fragmentEntry.getHtml(), fragmentEntry.getJs(),
+				StringPool.BLANK, 0);
+
+		portletPreferences.setValue(
+			"fragmentEntryLinkId",
+			String.valueOf(fragmentEntryLink.getFragmentEntryLinkId()));
+
+		portletPreferences.store();
+
+		super.processAction(portletConfig, actionRequest, actionResponse);
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.fragment.display.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Reference
+	private FragmentEntryLocalService _fragmentEntryLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private PortletPreferencesLocalService _portletPreferencesLocalService;
+
+}
---
+++ modules/apps/web-experience/fragment/fragment-display-web/src/main/resources/META-INF/resources/configuration.jsp
@@ -0,0 +1,48 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+String redirect = ParamUtil.getString(request, "redirect");
+%>
+
+<liferay-portlet:actionURL portletConfiguration="<%= true %>" var="configurationActionURL" />
+<liferay-portlet:renderURL portletConfiguration="<%= true %>" var="configurationRenderURL" />
+
+<aui:form action="<%= configurationActionURL %>" method="post" name="fm">
+	<aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= Constants.UPDATE %>" />
+	<aui:input name="redirect" type="hidden" value="<%= configurationRenderURL %>" />
+	<aui:input name="preferences--fragmentEntryLinkId--" type="hidden" value="<%= fragmentEntryDisplayContext.getFragmentEntryLinkId() %>" />
+
+	<div class="portlet-configuration-body-content">
+		<div class="container-fluid-1280">
+			<aui:fieldset-group markupView="lexicon">
+				<aui:fieldset>
+					<div id="<portlet:namespace />fragmentEntryPreview">
+						<liferay-util:include page="/fragment_entry.jsp" servletContext="<%= application %>" />
+					</div>
+				</aui:fieldset>
+			</aui:fieldset-group>
+		</div>
+	</div>
+
+	<aui:button-row>
+		<aui:button name="saveButton" type="submit" />
+
+		<aui:button href="<%= redirect %>" type="cancel" />
+	</aui:button-row>
+</aui:form>
