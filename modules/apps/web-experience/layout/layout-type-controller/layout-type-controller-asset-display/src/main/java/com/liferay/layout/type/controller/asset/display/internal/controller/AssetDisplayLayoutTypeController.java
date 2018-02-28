9d0019947e7bccf22b67561e9ea14bb06c791409
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-02-28 14:59:57 +0100

    LPS-78178 Add controller and it's empty views to the module

---
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-asset-display/src/main/java/com/liferay/layout/type/controller/asset/display/internal/controller/AssetDisplayLayoutTypeController.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.type.controller.asset.display.internal.controller;
+
+import com.liferay.layout.type.controller.asset.display.internal.constants.AssetDisplayLayoutTypeControllerConstants;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutTypeController;
+import com.liferay.portal.kernel.model.impl.BaseLayoutTypeControllerImpl;
+import com.liferay.taglib.servlet.PipingServletResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {"layout.type=" + AssetDisplayLayoutTypeControllerConstants.LAYOUT_TYPE_ASSET_DISPLAY},
+	service = LayoutTypeController.class
+)
+public class AssetDisplayLayoutTypeController
+	extends BaseLayoutTypeControllerImpl {
+
+	@Override
+	public String getType() {
+		return LayoutConstants.TYPE_PORTLET;
+	}
+
+	@Override
+	public String getURL() {
+		return _URL;
+	}
+
+	@Override
+	public boolean isBrowsable() {
+		return false;
+	}
+
+	@Override
+	public boolean isFirstPageable() {
+		return false;
+	}
+
+	@Override
+	public boolean isFullPageDisplayable() {
+		return true;
+	}
+
+	@Override
+	public boolean isInstanceable() {
+		return false;
+	}
+
+	@Override
+	public boolean isParentable() {
+		return false;
+	}
+
+	@Override
+	public boolean isSitemapable() {
+		return false;
+	}
+
+	@Override
+	public boolean isURLFriendliable() {
+		return true;
+	}
+
+	@Override
+	protected ServletResponse createServletResponse(
+		HttpServletResponse response, UnsyncStringWriter unsyncStringWriter) {
+
+		return new PipingServletResponse(response, unsyncStringWriter);
+	}
+
+	@Override
+	protected String getEditPage() {
+		return _EDIT_PAGE;
+	}
+
+	@Override
+	protected String getViewPage() {
+		return _VIEW_PAGE;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.layout.type.controller.asset.display)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+	private static final String _EDIT_PAGE = "/layout/edit/asset_display.jsp";
+
+	private static final String _URL =
+		"${liferay:mainPath}/portal/layout?p_l_id=${liferay:plid}" +
+			"&p_v_l_s_g_id=${liferay:pvlsgid}";
+
+	private static final String _VIEW_PAGE = "/layout/view/asset_display.jsp";
+
+}
---
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-asset-display/src/main/java/com/liferay/layout/type/controller/asset/display/internal/model/AssetDisplayLayoutTypeAccessPolicy.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.type.controller.asset.display.internal.model;
+
+import com.liferay.layout.type.controller.asset.display.internal.constants.AssetDisplayLayoutTypeControllerConstants;
+import com.liferay.portal.kernel.model.LayoutTypeAccessPolicy;
+import com.liferay.portal.kernel.model.impl.DefaultLayoutTypeAccessPolicyImpl;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {"layout.type=" + AssetDisplayLayoutTypeControllerConstants.LAYOUT_TYPE_ASSET_DISPLAY},
+	service = LayoutTypeAccessPolicy.class
+)
+public class AssetDisplayLayoutTypeAccessPolicy
+	extends DefaultLayoutTypeAccessPolicyImpl {
+}
