f725230fae9e95ac996b2b55f2e42a92091adbb9
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-02-28 15:02:37 +0100

    LPS-78178 Obtain all the fragment entry links linked to the page template entry and add them to the request

--- modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-asset-display/src/main/java/com/liferay/layout/type/controller/asset/display/internal/controller/AssetDisplayLayoutTypeController.java
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-asset-display/src/main/java/com/liferay/layout/type/controller/asset/display/internal/controller/AssetDisplayLayoutTypeController.java
@@ -14,15 +14,26 @@
 
 package com.liferay.layout.type.controller.asset.display.internal.controller;
 
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.type.controller.asset.display.internal.constants.AssetDisplayLayoutTypeControllerConstants;
+import com.liferay.layout.type.controller.asset.display.internal.constants.AssetDisplayLayoutTypeControllerWebKeys;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.LayoutTypeController;
 import com.liferay.portal.kernel.model.impl.BaseLayoutTypeControllerImpl;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.taglib.servlet.PipingServletResponse;
+
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
@@ -47,6 +58,30 @@ public class AssetDisplayLayoutTypeController
 	@Override
 	public String getURL() {
 		return _URL;
+	}
+
+	@Override
+	public boolean includeLayoutContent(
+			HttpServletRequest request, HttpServletResponse response,
+			Layout layout)
+		throws Exception {
+
+		long layoutPageTemplateEntryId = ParamUtil.getLong(
+			request, "layoutPageTemplateEntryId");
+
+		List<FragmentEntryLink> fragmentEntryLinks =
+			_fragmentEntryLinkLocalService.getFragmentEntryLinks(
+				layout.getGroupId(),
+				_portal.getClassNameId(LayoutPageTemplateEntry.class.getName()),
+				layoutPageTemplateEntryId);
+
+		request.setAttribute(
+			AssetDisplayLayoutTypeControllerWebKeys.LAYOUT_FRAGMENTS,
+			fragmentEntryLinks);
+
+		request.setAttribute(WebKeys.PORTLET_DECORATE, Boolean.FALSE);
+
+		return super.includeLayoutContent(request, response, layout);
 	}
 
 	@Override
@@ -116,5 +151,11 @@ public class AssetDisplayLayoutTypeController
 			"&p_v_l_s_g_id=${liferay:pvlsgid}";
 
 	private static final String _VIEW_PAGE = "/layout/view/asset_display.jsp";
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Reference
+	private Portal _portal;
 
 }
