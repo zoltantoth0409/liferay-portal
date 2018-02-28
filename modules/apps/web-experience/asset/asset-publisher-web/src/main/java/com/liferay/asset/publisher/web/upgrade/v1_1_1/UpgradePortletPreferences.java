a4bbd7b4d49b8be884a81237a93185aec62d47fa
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2018-02-28 13:57:09 +0100

    LPS-78284 Added step 1.1.1 to Asset Publisher upgrade proccess in order to update the package names of asset entry types that can be configured"

---
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/upgrade/v1_1_1/UpgradePortletPreferences.java
@@ -0,0 +1,144 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.publisher.web.upgrade.v1_1_1;
+
+import com.liferay.asset.publisher.constants.AssetPublisherPortletKeys;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.upgrade.BaseUpgradePortletPreferences;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.xml.SAXReader;
+
+import javax.portlet.PortletPreferences;
+
+/**
+ * @author Cristina Rodriguez
+ */
+public class UpgradePortletPreferences extends BaseUpgradePortletPreferences {
+
+	public UpgradePortletPreferences(SAXReader saxReader) {
+		_saxReader = saxReader;
+	}
+
+	@Override
+	protected String[] getPortletIds() {
+		return new String[] {
+			AssetPublisherPortletKeys.ASSET_PUBLISHER + "_INSTANCE_%"
+		};
+	}
+
+	@Override
+	protected String upgradePreferences(
+			long companyId, long ownerId, int ownerType, long plid,
+			String portletId, String xml)
+		throws Exception {
+
+		PortletPreferences portletPreferences =
+			PortletPreferencesFactoryUtil.fromXML(
+				companyId, ownerId, ownerType, plid, portletId, xml);
+
+		String[] assetEntryXmls = portletPreferences.getValues(
+			"assetEntryXml", new String[0]);
+
+		if (ArrayUtil.isNotEmpty(assetEntryXmls)) {
+			upgradeTypes(assetEntryXmls);
+
+			portletPreferences.setValues("assetEntryXml", assetEntryXmls);
+		}
+
+		return PortletPreferencesFactoryUtil.toXML(portletPreferences);
+	}
+
+	protected void upgradeTypes(String[] assetEntryXmls) throws Exception {
+		for (int i = 0; i < assetEntryXmls.length; i++) {
+			String assetEntry = assetEntryXmls[i];
+
+			Document document = _saxReader.read(assetEntry);
+
+			Element rootElement = document.getRootElement();
+
+			Element assetEntryTypeElement = rootElement.element(
+				"asset-entry-type");
+
+			if (assetEntryTypeElement == null) {
+				continue;
+			}
+
+			String assetEntryType = assetEntryTypeElement.getText();
+
+			String newAssetEntryType = assetEntryType;
+
+			for (String[] classNames : _CLASS_NAMES) {
+				newAssetEntryType = StringUtil.replace(
+					newAssetEntryType, classNames[0], classNames[1]);
+			}
+
+			if (!assetEntryType.equals(newAssetEntryType)) {
+				rootElement.remove(assetEntryTypeElement);
+
+				assetEntryTypeElement.setText(newAssetEntryType);
+				rootElement.add(assetEntryTypeElement);
+				document.setRootElement(rootElement);
+
+				assetEntryXmls[i] = document.formattedString(StringPool.BLANK);
+			}
+		}
+	}
+
+	private static final String[][] _CLASS_NAMES = {
+		{
+			"com.liferay.portlet.bookmarks.model.BookmarksEntry",
+			"com.liferay.bookmarks.model.BookmarksEntry"
+		},
+		{
+			"com.liferay.portlet.bookmarks.model.BookmarksFolder",
+			"com.liferay.bookmarks.model.BookmarksFolder"
+		},
+		{
+			"com.liferay.portlet.documentlibrary.model.DLFileEntry",
+			"com.liferay.document.library.kernel.model.DLFileEntry"
+		},
+		{
+			"com.liferay.portlet.documentlibrary.model.DLFolder",
+			"com.liferay.document.library.kernel.model.DLFolder"
+		},
+		{
+			"com.liferay.portlet.journal.model.JournalArticle",
+			"com.liferay.journal.model.JournalArticle"
+		},
+		{
+			"com.liferay.portlet.journal.model.JournalFolder",
+			"com.liferay.journal.model.JournalFolder"
+		},
+		{
+			"com.liferay.portlet.messageboards.model.MBMessage",
+			"com.liferay.message.boards.model.MBMessage"
+		},
+		{
+			"com.liferay.portlet.wiki.model.WikiPage",
+			"com.liferay.wiki.model.WikiPage"
+		},
+		{
+			"com.liferay.portlet.blogs.model.BlogsEntry",
+			"com.liferay.blogs.model.BlogsEntry"
+		}
+	};
+
+	private final SAXReader _saxReader;
+
+}
