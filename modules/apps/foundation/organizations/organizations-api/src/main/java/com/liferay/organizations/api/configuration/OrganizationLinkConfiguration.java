7d7c0f85428d53df8abd2527672f45e85c4ae38a
drewbrokke <drew.brokke@liferay.com>
2018-02-28 16:40:47 -0800

    LPS-78399 organizations-api - adds new OrganizationLinkConfiguration interface

---
+++ modules/apps/foundation/organizations/organizations-api/src/main/java/com/liferay/organizations/api/configuration/OrganizationLinkConfiguration.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.organizations.api.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Drew Brokke
+ */
+@ExtendedObjectClassDefinition(category = "foundation")
+@Meta.OCD(
+	id = "com.liferay.organizations.api.configuration.OrganizationLinkConfiguration",
+	localization = "content/Language",
+	name = "organization-link-configuration-name"
+)
+public interface OrganizationLinkConfiguration {
+
+	@Meta.AD(
+		deflt = "false", description = "organization-link-enabled-description",
+		name = "enabled", required = false
+	)
+	public boolean enabled();
+
+	@Meta.AD(
+		deflt = "", description = "organization-link-types-description",
+		name = "organization-link-types", required = false
+	)
+	public String[] linkTypes();
+
+}
