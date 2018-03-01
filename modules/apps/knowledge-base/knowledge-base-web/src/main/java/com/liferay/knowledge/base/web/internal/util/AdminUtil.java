b3ea3f30445d15ef280c481da589fb3bd4d2beca
Lance Ji <zheyi.ji@liferay.com>
2018-03-01 10:35:55 -0800

    LPS-77791 Add internal util class for web

---
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/util/AdminUtil.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.web.internal.util;
+
+import com.liferay.knowledge.base.util.AdminUtilHelper;
+import com.liferay.portal.kernel.diff.DiffVersionsInfo;
+
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lance Ji
+ */
+public class AdminUtil {
+
+	public static DiffVersionsInfo getDiffVersionsInfo(
+		long groupId, long kbArticleResourcePrimKey, int sourceVersion,
+		int targetVersion) {
+
+		return _adminUtilHelper.getDiffVersionsInfo(
+			groupId, kbArticleResourcePrimKey, sourceVersion, targetVersion);
+	}
+
+	public static String[] unescapeSections(String sections) {
+		return _adminUtilHelper.unescapeSections(sections);
+	}
+
+	@Reference(unbind = "-")
+	protected void setAdminUtilHelper(AdminUtilHelper adminUtilHelper) {
+		_adminUtilHelper = adminUtilHelper;
+	}
+
+	private static AdminUtilHelper _adminUtilHelper;
+
+}
