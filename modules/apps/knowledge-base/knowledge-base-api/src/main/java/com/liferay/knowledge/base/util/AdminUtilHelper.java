37b6d12c90bbebacd32a46676459f3f2b807ec8b
Lance Ji <zheyi.ji@liferay.com>
2018-03-01 09:55:45 -0800

    LPS-77791 Convert util methods into API

---
+++ modules/apps/knowledge-base/knowledge-base-api/src/main/java/com/liferay/knowledge/base/util/AdminUtilHelper.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.util;
+
+import com.liferay.portal.kernel.diff.DiffVersionsInfo;
+
+/**
+ * @author Lance Ji
+ */
+public interface AdminUtilHelper {
+
+	public String[] escapeSections(String[] sections);
+
+	public DiffVersionsInfo getDiffVersionsInfo(
+		long groupId, long kbArticleResourcePrimKey, int sourceVersion,
+		int targetVersion);
+
+	public String getKBArticleDiff(
+			long resourcePrimKey, int sourceVersion, int targetVersion,
+			String param)
+		throws Exception;
+
+	public String[] unescapeSections(String sections);
+
+}
