eef26f47ad268829376688072f4e6498e70a6b0a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-03-02 10:07:04 +0100

    LPS-76859 Render journal content portlet header

--- modules/apps/web-experience/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/servlet/taglib/JournalContentPortletHeaderDynamicInclude.java
+++ modules/apps/web-experience/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/servlet/taglib/JournalContentPortletHeaderDynamicInclude.java
@@ -14,10 +14,20 @@
 
 package com.liferay.journal.content.web.internal.servlet.taglib;
 
+import com.liferay.journal.constants.JournalContentPortletKeys;
+import com.liferay.journal.content.web.internal.constants.JournalContentWebKeys;
+import com.liferay.journal.content.web.internal.display.context.JournalContentDisplayContext;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
 import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -33,13 +43,51 @@ public class JournalContentPortletHeaderDynamicInclude
 
 	@Override
 	public void include(
-		HttpServletRequest request, HttpServletResponse response,
-		String key) {
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		JournalContentDisplayContext journalContentDisplayContext =
+			(JournalContentDisplayContext)request.getAttribute(
+				JournalContentWebKeys.JOURNAL_CONTENT_DISPLAY_CONTEXT);
+
+		try {
+			if (!journalContentDisplayContext.isShowArticle()) {
+				return;
+			}
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe);
+			}
+
+			return;
+		}
+
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher(_JSP_PATH);
+
+		try {
+			requestDispatcher.include(request, response);
+		}
+		catch (ServletException se) {
+			_log.error("Unable to include JSP " + _JSP_PATH, se);
+
+			throw new IOException("Unable to include JSP " + _JSP_PATH, se);
+		}
 	}
 
 	@Override
 	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"portlet_header_" + JournalContentPortletKeys.JOURNAL_CONTENT);
 	}
+
+	private static final String _JSP_PATH =
+		"/portlet/header/portlet_header.jsp";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalContentPortletHeaderDynamicInclude.class);
 
 	@Reference(
 		target = "(osgi.web.symbolicname=com.liferay.journal.content.web)"
