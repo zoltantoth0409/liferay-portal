3330f744333c9dfc76b77a20e6db86696253904c
NemethNorbert <norbert.nemeth@liferay.com>
2018-03-05 12:21:30 +0100

    LPS-77158 Waiting for editor.instanceReady with setHTML and checking against the current locale`s value at initial render.

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -2751,11 +2751,22 @@ AUI.add(
 
 						var editor = instance.getEditor();
 
+						var editorComponentName = instance.getInputName() + 'Editor';
+
+						var localizationMap = instance.get('localizationMap');
+
 						if (isNode(editor)) {
 							TextHTMLField.superclass.setValue.apply(instance, arguments);
 						}
-						else {
+						else if(editor.instanceReady) {
 							editor.setHTML(value);
+						}
+						else {
+							Liferay.after(editorComponentName + ':registered', function() {
+								if(value === localizationMap[instance.get('displayLocale')]) {
+									editor.setHTML(value);
+								}
+							});
 						}
 					},
 
