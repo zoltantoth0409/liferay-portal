ff983ba0075ddbba1322279b9970f77d9e38ccb6
Brian Chan <brian.chan@liferay.com>
2018-03-05 21:27:03 -0800

    LPS-75717 Wordsmith

--- modules/apps/web-experience/staging/staging-impl/src/main/java/com/liferay/staging/internal/StagingGroupHelperImpl.java
+++ modules/apps/web-experience/staging/staging-impl/src/main/java/com/liferay/staging/internal/StagingGroupHelperImpl.java
@@ -129,7 +129,7 @@ public class StagingGroupHelperImpl implements StagingGroupHelper {
 		catch (PortalException pe) {
 			if (_log.isWarnEnabled()) {
 				_log.warn(
-					"Cannot retrieve remote live group: " + pe.getMessage());
+					"Unable to get remote live group: " + pe.getMessage());
 			}
 
 			return null;
