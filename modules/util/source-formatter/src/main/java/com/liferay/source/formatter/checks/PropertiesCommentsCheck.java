6488e47173f88ab6557e70df159eb834482c989d
Hugo Huijser <hugo.huijser@liferay.com>
2018-03-06 11:00:49 +0800

    LPS-74544 Move method getTitleCase to SourceUtil

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesCommentsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesCommentsCheck.java
@@ -14,11 +14,8 @@
 
 package com.liferay.source.formatter.checks;
 
-import com.liferay.petra.string.CharPool;
-import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.checks.util.SourceUtil;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -45,7 +42,8 @@ public class PropertiesCommentsCheck extends BaseFileCheck {
 
 			String comment = matcher.group(2);
 
-			String titleCaseComment = _getTitleCase(comment, _BRAND_NAMES);
+			String titleCaseComment = SourceUtil.getTitleCase(
+				comment, _BRAND_NAMES);
 
 			titleCaseComment = titleCaseComment.replaceAll(
 				"(?i)(\\A|\\s)sf(\\Z|\\s)", "$1Source Formatter$2");
@@ -59,85 +57,8 @@ public class PropertiesCommentsCheck extends BaseFileCheck {
 		return content;
 	}
 
-	private String _getTitleCase(String s, String[] exceptions) {
-		String[] words = s.split("\\s+");
-
-		if (ArrayUtil.isEmpty(words)) {
-			return s;
-		}
-
-		StringBundler sb = new StringBundler(words.length * 2);
-
-		outerLoop:
-		for (int i = 0; i < words.length; i++) {
-			String word = words[i];
-
-			if (Validator.isNull(word)) {
-				continue;
-			}
-
-			for (String exception : exceptions) {
-				if (StringUtil.equalsIgnoreCase(exception, word)) {
-					sb.append(exception);
-					sb.append(CharPool.SPACE);
-
-					continue outerLoop;
-				}
-			}
-
-			if ((i != 0) && (i != words.length)) {
-				String lowerCaseWord = StringUtil.toLowerCase(word);
-
-				if (ArrayUtil.contains(_ARTICLES, lowerCaseWord) ||
-					ArrayUtil.contains(_CONJUNCTIONS, lowerCaseWord) ||
-					ArrayUtil.contains(_PREPOSITIONS, lowerCaseWord)) {
-
-					sb.append(lowerCaseWord);
-					sb.append(CharPool.SPACE);
-
-					continue;
-				}
-			}
-
-			if (Character.isUpperCase(word.charAt(0))) {
-				sb.append(word);
-			}
-			else {
-				sb.append(StringUtil.upperCaseFirstLetter(word));
-			}
-
-			sb.append(CharPool.SPACE);
-		}
-
-		sb.setIndex(sb.index() - 1);
-
-		return sb.toString();
-	}
-
-	private static final String[] _ARTICLES = {"a", "an", "the"};
-
 	private static final String[] _BRAND_NAMES =
 		{"jQuery", "reCAPTCHA", "svg4everybody", "tc"};
-
-	private static final String[] _CONJUNCTIONS =
-		{"and", "but", "for", "nor", "or", "yet"};
-
-	private static final String[] _PREPOSITIONS = {
-		"a", "abaft", "aboard", "about", "above", "absent", "across", "afore",
-		"after", "against", "along", "alongside", "amid", "amidst", "among",
-		"amongst", "an", "apropos", "apud", "around", "as", "aside", "astride",
-		"at", "athwart", "atop", "barring", "before", "behind", "below",
-		"beneath", "beside", "besides", "between", "beyond", "but", "by",
-		"circa", "concerning", "despite", "down", "during", "except",
-		"excluding", "failing", "for", "from", "given", "in", "including",
-		"inside", "into", "lest", "mid", "midst", "modulo", "near", "next",
-		"notwithstanding", "of", "off", "on", "onto", "opposite", "out",
-		"outside", "over", "pace", "past", "per", "plus", "pro", "qua",
-		"regarding", "sans", "since", "through", "throughout", "thru",
-		"thruout", "till", "to", "toward", "towards", "under", "underneath",
-		"unlike", "until", "unto", "up", "upon", "v", "versus", "via", "vice",
-		"vs", "with", "within", "without", "worth"
-	};
 
 	private final Pattern _commentPattern = Pattern.compile(
 		"([^#]\\s*)?\\n\\s*#+\\s+(\\w[\\s\\w]+)(\\n\\s*#+.*[\\w]+.*)?$",
