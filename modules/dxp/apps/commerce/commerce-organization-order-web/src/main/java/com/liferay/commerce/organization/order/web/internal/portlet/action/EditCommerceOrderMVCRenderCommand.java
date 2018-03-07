a547d6ffb35961cb1aa12b0597dbd02bcbc40d33
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-03-07 10:56:47 -0800

    COMMERCE-229 Set an order as current when viewing it

--- modules/dxp/apps/commerce/commerce-organization-order-web/src/main/java/com/liferay/commerce/organization/order/web/internal/portlet/action/EditCommerceOrderMVCRenderCommand.java
+++ modules/dxp/apps/commerce/commerce-organization-order-web/src/main/java/com/liferay/commerce/organization/order/web/internal/portlet/action/EditCommerceOrderMVCRenderCommand.java
@@ -14,14 +14,23 @@
 
 package com.liferay.commerce.organization.order.web.internal.portlet.action;
 
+import com.liferay.commerce.exception.NoSuchOrderException;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.order.CommerceOrderHelper;
 import com.liferay.commerce.organization.order.web.internal.constants.CommerceOrganizationOrderPortletKeys;
+import com.liferay.commerce.organization.order.web.internal.display.context.CommerceOrganizationOrderDisplayContext;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Andrea Di Giorgi
@@ -40,7 +49,42 @@ public class EditCommerceOrderMVCRenderCommand implements MVCRenderCommand {
 			RenderRequest renderRequest, RenderResponse renderResponse)
 		throws PortletException {
 
-		return "/edit_order.jsp";
+		try {
+			CommerceOrganizationOrderDisplayContext
+				commerceOrganizationOrderDisplayContext =
+					(CommerceOrganizationOrderDisplayContext)
+						renderRequest.getAttribute(
+							WebKeys.PORTLET_DISPLAY_CONTEXT);
+
+			CommerceOrder commerceOrder =
+				commerceOrganizationOrderDisplayContext.getCommerceOrder();
+
+			if ((commerceOrder != null) && commerceOrder.isOpen()) {
+				_commerceOrderHelper.setCurrentCommerceOrder(
+					_portal.getHttpServletRequest(renderRequest),
+					commerceOrder);
+			}
+
+			return "/edit_order.jsp";
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchOrderException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return "/error.jsp";
+			}
+			else {
+				throw new PortletException(e);
+			}
+		}
 	}
+
+	@Reference
+	private CommerceOrderHelper _commerceOrderHelper;
+
+	@Reference
+	private Portal _portal;
 
 }
