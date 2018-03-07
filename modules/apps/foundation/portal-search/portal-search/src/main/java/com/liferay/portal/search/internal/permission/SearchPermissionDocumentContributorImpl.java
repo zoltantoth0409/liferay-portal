70ab1e7f56e484438b080a06bf3d2513b06468f6
Michael Han <michael.han@liferay.com>
2018-03-06 19:55:40 -0600

    LPS-78255 portal-search: implement permission document contributor
    https://issues.liferay.com/browse/LPS-78255

---
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/SearchPermissionDocumentContributorImpl.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.permission;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.NoSuchResourceException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.permission.SearchPermissionDocumentContributor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(
+	immediate = true, service = SearchPermissionDocumentContributor.class
+)
+public class SearchPermissionDocumentContributorImpl
+	implements SearchPermissionDocumentContributor {
+
+	@Override
+	public void addPermissionFields(long companyId, Document document) {
+		long groupId = GetterUtil.getLong(document.get(Field.GROUP_ID));
+
+		String className = document.get(Field.ENTRY_CLASS_NAME);
+		String classPK = document.get(Field.ENTRY_CLASS_PK);
+
+		if (Validator.isNull(className) && Validator.isNull(classPK)) {
+			className = document.get(Field.ROOT_ENTRY_CLASS_NAME);
+			classPK = document.get(Field.ROOT_ENTRY_CLASS_PK);
+		}
+
+		boolean relatedEntry = GetterUtil.getBoolean(
+			document.get(Field.RELATED_ENTRY));
+
+		if (relatedEntry) {
+			long classNameId = GetterUtil.getLong(
+				document.get(Field.CLASS_NAME_ID));
+
+			className = _portal.getClassName(classNameId);
+
+			classPK = document.get(Field.CLASS_PK);
+		}
+
+		addPermissionFields(
+			companyId, groupId, className, GetterUtil.getLong(classPK),
+			document);
+	}
+
+	@Override
+	public void addPermissionFields(
+		long companyId, long groupId, String className, long classPK,
+		Document document) {
+
+		Indexer<?> indexer = _indexerRegistry.nullSafeGetIndexer(className);
+
+		if (!indexer.isPermissionAware()) {
+			return;
+		}
+
+		String viewActionId = document.get(Field.VIEW_ACTION_ID);
+
+		if (Validator.isNull(viewActionId)) {
+			viewActionId = ActionKeys.VIEW;
+		}
+
+		_addPermissionFields(
+			companyId, groupId, className, classPK, viewActionId, document);
+	}
+
+	private void _addPermissionFields(
+		long companyId, long groupId, String className, long classPK,
+		String viewActionId, Document doc) {
+
+		try {
+			List<Role> roles = _resourcePermissionLocalService.getRoles(
+				companyId, className, ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(classPK), viewActionId);
+
+			if (roles.isEmpty()) {
+				return;
+			}
+
+			List<Long> roleIds = new ArrayList<>();
+			List<String> groupRoleIds = new ArrayList<>();
+
+			for (Role role : roles) {
+				if ((role.getType() == RoleConstants.TYPE_ORGANIZATION) ||
+					(role.getType() == RoleConstants.TYPE_SITE)) {
+
+					groupRoleIds.add(
+						groupId + StringPool.DASH + role.getRoleId());
+				}
+				else {
+					roleIds.add(role.getRoleId());
+				}
+			}
+
+			doc.addKeyword(
+				Field.ROLE_ID, roleIds.toArray(new Long[roleIds.size()]));
+			doc.addKeyword(
+				Field.GROUP_ROLE_ID,
+				groupRoleIds.toArray(new String[groupRoleIds.size()]));
+		}
+		catch (NoSuchResourceException nsre) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(nsre, nsre);
+			}
+		}
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					StringBundler.concat(
+						"Error retrieving permission fields for className = ",
+						className, ", classPK = ", String.valueOf(classPK)),
+					e);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SearchPermissionDocumentContributorImpl.class);
+
+	@Reference
+	private IndexerRegistry _indexerRegistry;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+}
