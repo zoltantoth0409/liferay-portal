d75c57494e905f9f525c8f9fc4f98baf3c126840
Gregory Amerson <gregory.amerson@liferay.com>
2018-03-07 14:05:03 -0600

    LPS-77343 add additional tests for documented features

---
+++ modules/sdk/gradle-plugins-target-platform/src/gradleTest/resolveBundles/src/main/java/foo/portlet/portlet/FooPortlet.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package foo.portlet.portlet;
+
+import com.google.common.base.Strings;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import foo.portlet.constants.FooPortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=foo-portlet Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + FooPortletKeys.FOO,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class FooPortlet extends MVCPortlet {
+
+	public FooPortlet() {
+		Strings.repeat("foo", 10);
+	}
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
