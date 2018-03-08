edf231f613be04db25ad7fe79bdbdd68dbc8f377
Matthew Tambara <matthew.tambara@liferay.com>
2018-03-08 15:11:51 -0800

    LPS-78472 Add language.properties

--- modules/apps/foundation/frontend-js/frontend-js-minifier/src/main/java/com/frontend/js/minifier/configuration/YahooJavaScriptMinifierConfiguration.java
+++ modules/apps/foundation/frontend-js/frontend-js-minifier/src/main/java/com/frontend/js/minifier/configuration/YahooJavaScriptMinifierConfiguration.java
@@ -20,26 +20,31 @@ import aQute.bnd.annotation.metatype.Meta;
  * @author Iván Zaera Avellón
  */
 @Meta.OCD(
-	id = "com.frontend.js.minifier.configuration.YahooJavaScriptMinifierConfiguration"
+	id = "com.frontend.js.minifier.configuration.YahooJavaScriptMinifierConfiguration",
+	localization = "content/Language", name = "yahoo-js-minifier-configuration"
 )
 public interface YahooJavaScriptMinifierConfiguration {
 
-	@Meta.AD(deflt = "1", required = false)
+	@Meta.AD(deflt = "1", name = "css-line-break", required = false)
 	public int cssLineBreak();
 
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(
+		deflt = "false", name = "js-disable-optimizations", required = false
+	)
 	public boolean jsDisableOptimizations();
 
-	@Meta.AD(deflt = "1", required = false)
+	@Meta.AD(deflt = "1", name = "js-line-break", required = false)
 	public int jsLineBreak();
 
-	@Meta.AD(deflt = "true", required = false)
+	@Meta.AD(deflt = "true", name = "js-munge", required = false)
 	public boolean jsMunge();
 
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(
+		deflt = "false", name = "js-preserve-all-semicolons", required = false
+	)
 	public boolean jsPreserveAllSemicolons();
 
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(deflt = "false", name = "js-verbose", required = false)
 	public boolean jsVerbose();
 
 }
