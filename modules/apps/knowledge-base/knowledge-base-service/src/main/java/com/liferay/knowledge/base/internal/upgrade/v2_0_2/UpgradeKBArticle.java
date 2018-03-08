bc2179ed3a90b91588073153f48d0cbb3de74764
Adolfo Pérez <adolfo.perez@liferay.com>
2018-03-08 16:11:53 +0100

    LPS-77851 SF

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/upgrade/v2_0_2/UpgradeKBArticle.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/upgrade/v2_0_2/UpgradeKBArticle.java
@@ -114,10 +114,12 @@ public class UpgradeKBArticle extends UpgradeProcess {
 
 		return _renameConflictingFriendlyURL(
 			StringBundler.concat(
-				"select kbArticle2.kbArticleId, kbArticle2.urlTitle from KBArticle kbArticle1 ",
-				"inner join KBArticle kbArticle2 on kbArticle1.groupId = kbArticle2.groupId and ",
-				"kbArticle1.kbFolderId = kbArticle2.kbFolderId and kbArticle1.urlTitle = ",
-				"kbArticle2.urlTitle where kbArticle1.kbArticleId < kbArticle2.kbArticleId"),
+				"select kbArticle2.kbArticleId, kbArticle2.urlTitle from ",
+				"KBArticle kbArticle1 inner join KBArticle kbArticle2 on ",
+				"kbArticle1.groupId = kbArticle2.groupId and ",
+				"kbArticle1.kbFolderId = kbArticle2.kbFolderId and ",
+				"kbArticle1.urlTitle = kbArticle2.urlTitle where ",
+				"kbArticle1.kbArticleId < kbArticle2.kbArticleId"),
 			"update KBArticle set urlTitle = ? where kbArticleId = ?");
 	}
 
@@ -126,10 +128,12 @@ public class UpgradeKBArticle extends UpgradeProcess {
 
 		return _renameConflictingFriendlyURL(
 			StringBundler.concat(
-				"select kbArticle2.kbFolderId, kbArticle2.urlTitle from KBFolder kbArticle1 inner ",
-				"join KBFolder kbArticle2 on kbArticle1.groupId = kbArticle2.groupId and ",
-				"kbArticle1.parentKBFolderId = kbArticle2.parentKBFolderId and kbArticle1.urlTitle ",
-				"= kbArticle2.urlTitle where kbArticle1.kbFolderId < kbArticle2.kbFolderId"),
+				"select kbArticle2.kbFolderId, kbArticle2.urlTitle from ",
+				"KBFolder kbArticle1 inner join KBFolder kbArticle2 on ",
+				"kbArticle1.groupId = kbArticle2.groupId and ",
+				"kbArticle1.parentKBFolderId = kbArticle2.parentKBFolderId ",
+				"and kbArticle1.urlTitle = kbArticle2.urlTitle where ",
+				"kbArticle1.kbFolderId < kbArticle2.kbFolderId"),
 			"update KBFolder set urlTitle = ? where kbFolderId = ?");
 	}
 
