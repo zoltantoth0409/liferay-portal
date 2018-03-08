ec9b3507c5fd2e3def1326358a427de92f6b1606
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-03-08 18:30:40 +0100

    LPS-78564 Adds new key for journal article id

---
+++ modules/apps/web-experience/journal/journal-analytics/src/main/java/com/liferay/journal/analytics/internal/contants/JournalWebKeys.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.analytics.internal.contants;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class JournalWebKeys {
+
+	public static final String JOURNAL_ARTICLE_ID = "JOURNAL_ARTICLE_ID";
+
+}
