10545a55a4949bb5df3f27b033c75601a7e604f8
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2018-03-08 13:24:47 +0100

    LPS-78284 Register step 1.0.1 and removed 1.1.1

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/upgrade/AssetPublisherWebUpgrade.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/upgrade/AssetPublisherWebUpgrade.java
@@ -45,14 +45,14 @@ public class AssetPublisherWebUpgrade implements UpgradeStepRegistrator {
 				_saxReader));
 
 		registry.register(
-			"com.liferay.asset.publisher.web", "1.0.0", "1.1.0",
-			new com.liferay.asset.publisher.web.upgrade.v1_1_0.
-				UpgradePortletPreferences());
+			"com.liferay.asset.publisher.web", "1.0.0", "1.0.1",
+			new com.liferay.asset.publisher.web.upgrade.v1_0_1.
+				UpgradePortletPreferences(_saxReader));
 
 		registry.register(
-			"com.liferay.asset.publisher.web", "1.1.0", "1.1.1",
-			new com.liferay.asset.publisher.web.upgrade.v1_1_1.
-				UpgradePortletPreferences(_saxReader));
+			"com.liferay.asset.publisher.web", "1.0.1", "1.1.0",
+			new com.liferay.asset.publisher.web.upgrade.v1_1_0.
+				UpgradePortletPreferences());
 	}
 
 	@Reference(unbind = "-")
