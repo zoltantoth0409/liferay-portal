93309c033a7250da5670d93aa4b31067b9f3e364
Máté Thurzó <mate.thurzo@liferay.com>
2018-03-08 18:31:49 +0100

    LPS-78247 Add new utility class

---
+++ modules/apps/web-experience/staging/staging-impl/src/main/java/com/liferay/staging/internal/activator/StagingImplBundleActivator.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.internal.activator;
+
+import com.liferay.staging.StagingGroupHelper;
+import com.liferay.staging.StagingGroupHelperUtil;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Mate Thurzo
+ */
+public class StagingImplBundleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		_serviceTracker =
+			new ServiceTracker<StagingGroupHelper, StagingGroupHelper>(
+				bundleContext, StagingGroupHelper.class.getName(), null) {
+
+				@Override
+				public StagingGroupHelper addingService(
+					ServiceReference<StagingGroupHelper> serviceReference) {
+
+					StagingGroupHelper stagingGroupHelper =
+						bundleContext.getService(serviceReference);
+
+					StagingGroupHelperUtil.setStagingGroupHelper(
+						stagingGroupHelper);
+
+					return stagingGroupHelper;
+				}
+
+			};
+
+		_serviceTracker.open();
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+		_serviceTracker.close();
+	}
+
+	private ServiceTracker<StagingGroupHelper, StagingGroupHelper>
+		_serviceTracker;
+
+}
