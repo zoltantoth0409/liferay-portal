423c712cee3d958dd2ffb80db0f3d5a87deed870
drewbrokke <drew.brokke@liferay.com>
2018-03-08 09:59:06 -0800

    LPS-78259 user-associated-data-web - moves logic back into ForgetPersonalSiteMVCActionCommand.java and extends from BaseTransactionalMVCActionCommand

--- modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/action/ForgetPersonalSiteMVCActionCommand.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/action/ForgetPersonalSiteMVCActionCommand.java
@@ -14,11 +14,19 @@
 
 package com.liferay.user.associated.data.web.internal.portlet.action;
 
-import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.RequiredUserException;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseTransactionalMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.user.associated.data.constants.UserAssociatedDataPortletKeys;
+
+import java.util.Locale;
+import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -37,21 +45,34 @@ import org.osgi.service.component.annotations.Reference;
 	},
 	service = MVCActionCommand.class
 )
-public class ForgetPersonalSiteMVCActionCommand extends BaseMVCActionCommand {
+public class ForgetPersonalSiteMVCActionCommand
+	extends BaseTransactionalMVCActionCommand {
 
 	@Override
-	protected void doProcessAction(
+	protected void doTransactionalCommand(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
 		long selUserId = ParamUtil.getLong(actionRequest, "selUserId");
 
-		_userLocalService.resetUserGroup(selUserId);
+		User selUser = _userLocalService.getUserById(selUserId);
 
-		String redirect = ParamUtil.getString(actionRequest, "redirect");
+		if (selUser.isDefaultUser()) {
+			throw new RequiredUserException();
+		}
 
-		sendRedirect(actionRequest, actionResponse, redirect);
+		_groupLocalService.deleteGroup(selUser.getGroup());
+
+		_groupLocalService.addGroup(
+			selUser.getUserId(), GroupConstants.DEFAULT_PARENT_GROUP_ID,
+			User.class.getName(), selUser.getUserId(),
+			GroupConstants.DEFAULT_LIVE_GROUP_ID, (Map<Locale, String>)null,
+			null, 0, true, GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION,
+			StringPool.SLASH + selUser.getScreenName(), false, true, null);
 	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
