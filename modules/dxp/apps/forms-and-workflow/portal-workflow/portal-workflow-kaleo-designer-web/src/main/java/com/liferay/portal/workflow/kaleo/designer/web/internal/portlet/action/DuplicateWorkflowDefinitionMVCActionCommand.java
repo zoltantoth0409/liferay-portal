6749be978c62c4f3b6ad6cf315c41524fa063583
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2018-03-09 15:52:46 -0300

    LPS-74085 Create an action to duplicate a workflow definition

---
+++ modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/action/DuplicateWorkflowDefinitionMVCActionCommand.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.designer.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowDefinition;
+import com.liferay.portal.workflow.kaleo.designer.web.constants.KaleoDesignerPortletKeys;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jeyvison Nascimento
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + KaleoDesignerPortletKeys.KALEO_DESIGNER,
+		"mvc.command.name=duplicateWorkflowDefinition"
+	},
+	service = MVCActionCommand.class
+)
+public class DuplicateWorkflowDefinitionMVCActionCommand
+	extends PublishKaleoDefinitionVersionMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String randomNamespace = ParamUtil.getString(
+			actionRequest, "randomNamespace");
+
+		String titleParameterName = randomNamespace + "title";
+
+		Map<Locale, String> titleMap = LocalizationUtil.getLocalizationMap(
+			actionRequest, titleParameterName);
+
+		String title = titleMap.get(LocaleUtil.getDefault());
+
+		if (titleMap.isEmpty() || Validator.isNull(title)) {
+			title = ParamUtil.getString(actionRequest, titleParameterName);
+
+			if (Validator.isNull(title)) {
+				title = ParamUtil.getString(
+					actionRequest, "defaultDuplicationTitle");
+			}
+		}
+
+		String name = ParamUtil.getString(actionRequest, "name");
+
+		String content = ParamUtil.getString(actionRequest, "content");
+
+		WorkflowDefinition workflowDefinition =
+			workflowDefinitionManager.deployWorkflowDefinition(
+				themeDisplay.getCompanyId(), themeDisplay.getUserId(), title,
+				name, content.getBytes());
+
+		KaleoDefinitionVersion kaleoDefinitionVersion =
+			kaleoDefinitionVersionLocalService.getLatestKaleoDefinitionVersion(
+				themeDisplay.getCompanyId(), workflowDefinition.getName());
+
+		setRedirectAttribute(actionRequest, kaleoDefinitionVersion);
+
+		sendRedirect(actionRequest, actionResponse);
+	}
+
+	@Override
+	protected String getSuccessMessage(
+		ActionRequest actionRequest, ResourceBundle resourceBundle) {
+
+		String duplicatedDefinitionName = ParamUtil.getString(
+			actionRequest, "duplicatedDefinitionTitle");
+
+		return LanguageUtil.format(
+			resourceBundle, "duplicated-from-x",
+			StringUtil.quote(duplicatedDefinitionName));
+	}
+
+}
