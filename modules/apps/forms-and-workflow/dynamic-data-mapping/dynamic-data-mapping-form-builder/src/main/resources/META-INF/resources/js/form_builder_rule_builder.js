a92f65899e558474a7e4c500d09c89e459978be0
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2018-03-09 14:23:26 -0300

    LPS-77284 Create method to handle the rule draft

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder_rule_builder.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder_rule_builder.js
@@ -80,6 +80,7 @@ AUI.add(
 
 						instance.on('rulesChange', A.bind(instance._onRulesChange, instance));
 						instance.on('*:saveRule', A.bind(instance._handleSaveRule, instance));
+						instance.on('*:saveRuleDraft', A.bind(instance._handleSaveRuleDraft, instance));
 						instance.on('*:cancelRule', A.bind(instance._handleCancelRule, instance));
 
 						instance._eventHandlers = [
@@ -520,6 +521,22 @@ AUI.add(
 						var instance = this;
 
 						A.one('.lfr-ddm-add-rule').addClass('hide');
+					},
+
+					_handleSaveRuleDraft: function(event) {
+						var instance = this;
+
+						var rule = {};
+
+						if (!event.actions && !event.conditions && event['logical-operator'] != '') {
+							rule = {
+								actions: event.actions,
+								conditions: event.conditions,
+								'logical-operator': event['logical-operator']
+							};
+						}
+
+						instance.set('ruleDraft', rule);
 					},
 
 					_onRulesChange: function(val) {
