87ebd6a1f60dfc6408e8fe9e518bbd3c0d337bfc
drewbrokke <drew.brokke@liferay.com>
2018-03-09 11:16:30 -0800

    LPS-78646 user-associated-data-web - adds DeleteApplicationUADEntitiesMVCActionCommand.java to delete all entities related to an application

---
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/action/DeleteApplicationUADEntitiesMVCActionCommand.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.associated.data.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.user.associated.data.anonymizer.UADEntityAnonymizer;
+import com.liferay.user.associated.data.constants.UserAssociatedDataPortletKeys;
+import com.liferay.user.associated.data.web.internal.util.UADApplicationSummaryHelper;
+
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + UserAssociatedDataPortletKeys.USER_ASSOCIATED_DATA,
+		"mvc.command.name=/user_associated_data/delete_application_user_associated_data_entities"
+	},
+	service = MVCActionCommand.class
+)
+public class DeleteApplicationUADEntitiesMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long selUserId = ParamUtil.getLong(actionRequest, "selUserId");
+		String uadEntitySetName = ParamUtil.getString(
+			actionRequest, "uadEntitySetName");
+
+		List<UADEntityAnonymizer> uadEntityAnonymizers =
+			_uadApplicationSummaryHelper.getApplicationUADEntityAnonymizers(
+				uadEntitySetName);
+
+		for (UADEntityAnonymizer uadEntityAnonymizer : uadEntityAnonymizers) {
+			uadEntityAnonymizer.deleteAll(selUserId);
+		}
+	}
+
+	@Reference
+	private UADApplicationSummaryHelper _uadApplicationSummaryHelper;
+
+}
