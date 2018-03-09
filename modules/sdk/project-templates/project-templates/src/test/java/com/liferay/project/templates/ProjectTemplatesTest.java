c51f878fc4f1a215a7a224841257e9dff6873f26
Gregory Amerson <gregory.amerson@liferay.com>
2018-03-09 10:41:42 -0600

    LPS-78457 fix glob

--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
@@ -113,7 +113,7 @@ public class ProjectTemplatesTest {
 		FileSystem fileSystem = FileSystems.getDefault();
 
 		_pathMatcherOutputFiles = fileSystem.getPathMatcher(
-			"glob:**/*.{jar,war}");
+			"glob:**/{build/libs,target}/*.{jar,war}");
 
 		_projectTemplateVersions = FileUtil.readProperties(
 			Paths.get("build", "project-template-versions.properties"));
@@ -1804,7 +1804,8 @@ public class ProjectTemplatesTest {
 		File mavenOutputDir = new File(mavenProjectDir, "target");
 
 		_buildProjects(
-			gradleProjectDir, mavenProjectDir, gradleOutputDir, mavenOutputDir);
+			gradleProjectDir, mavenProjectDir, gradleOutputDir, mavenOutputDir,
+			_GRADLE_TASK_PATH_BUILD);
 	}
 
 	private static void _buildProjects(
