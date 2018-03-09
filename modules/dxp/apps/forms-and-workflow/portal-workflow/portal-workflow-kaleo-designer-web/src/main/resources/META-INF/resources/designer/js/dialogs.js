8ed048a3a9cb141f2e18dab34b4c033ec96f88ed
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2018-03-09 15:58:43 -0300

    LPS-74085 Javascript to handle the duplication dialog and the duplication form submission

--- modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/dialogs.js
+++ modules/dxp/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/dialogs.js
@@ -1,7 +1,97 @@
 AUI.add(
 	'liferay-kaleo-designer-dialogs',
 	function(A) {
-		var KaleoDesignerDialogs = {};
+		var KaleoDesignerDialogs = {
+
+			confirmBeforeDuplicateDialog: function(event, actionUrl, title, randomId, portletNamespace) {
+
+				var instance = this;
+
+				var form = A.one('#' + portletNamespace + randomId + 'form');
+
+				if (form && !instance._forms[randomId]) {
+					instance._forms[randomId] = form;
+				}
+				else if (!form && instance._forms[randomId]) {
+					form = instance._forms[randomId];
+				}
+
+				if (form) {
+					form.setAttribute('action', actionUrl);
+					form.setAttribute('method', 'POST');
+				}
+
+				var duplicationDialog = instance._duplicationDialog;
+
+				if (duplicationDialog) {
+					duplicationDialog.destroy();
+				}
+
+				var dialog = Liferay.Util.Window.getWindow(
+					{
+						dialog: {
+							bodyContent: form,
+							height: 325,
+							toolbars: {
+								footer: [
+									{
+										cssClass: 'btn btn-secondary',
+										discardDefaultButtonCssClasses: true,
+										label: Liferay.Language.get('cancel'),
+										on: {
+											click: function() {
+												if (form) {
+													form.reset();
+												}
+
+												dialog.hide();
+											}
+										}
+									},
+									{
+										cssClass: 'btn btn-primary',
+										discardDefaultButtonCssClasses: true,
+										label: Liferay.Language.get('duplicate'),
+										on: {
+											click: function() {
+												if (form) {
+													submitForm(form);
+												}
+
+												dialog.hide();
+											}
+										}
+									}
+								],
+								header: [
+									{
+										cssClass: 'close',
+										discardDefaultButtonCssClasses: true,
+										labelHTML: '<svg class="lexicon-icon" focusable="false"><use data-href="' + Liferay.ThemeDisplay.getPathThemeImages() + '/lexicon/icons.svg#times" /><title>' + Liferay.Language.get('close') + '</title></svg>',
+										on: {
+											click: function(event) {
+												if (form) {
+													form.reset();
+												}
+
+												dialog.hide();
+											}
+										}
+									}
+								]
+							},
+							width: 500
+						},
+						title: title
+					}
+				);
+
+				instance._duplicationDialog = dialog;
+			},
+
+			_duplicationDialog: null,
+			_forms: {}
+		};
 
 		var openConfirmDeleteDialog = function(title, message, actionUrl) {
 			var dialog = Liferay.Util.Window.getWindow(
