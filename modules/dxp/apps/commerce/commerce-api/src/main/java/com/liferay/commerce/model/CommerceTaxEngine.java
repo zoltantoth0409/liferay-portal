bd85ec82b6e7334544126e26f205d28e3a8ed75e
Alessio Rendina <asilcomando@gmail.com>
2018-03-13 23:30:56 +0100

    COMMERCE-305 Added commerce tax engine model

---
+++ modules/dxp/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/model/CommerceTaxEngine.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.model;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.commerce.exception.CommerceTaxEngineException;
+import com.liferay.commerce.product.model.CPDefinition;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@ProviderType
+public interface CommerceTaxEngine {
+
+	public List<CommerceTaxRate> getCommerceTaxRates(
+			CPDefinition cpDefinition, CommerceAddress commerceAddress,
+			Locale locale)
+		throws CommerceTaxEngineException;
+
+	public String getDescription(Locale locale);
+
+	public String getName(Locale locale);
+
+	public boolean isPercentage(long groupId);
+
+}
