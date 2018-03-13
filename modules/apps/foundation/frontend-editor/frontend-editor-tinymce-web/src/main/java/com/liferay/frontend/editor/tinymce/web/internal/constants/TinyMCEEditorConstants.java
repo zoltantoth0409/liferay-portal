4e6806ab6464ea467a2d843e24167cc85c27ae50
codyhoag <cody.hoag@liferay.com>
2018-03-13 16:30:11 -0400

    LPS-78451 LRDOCS-5045 Edit Javadoc

--- modules/apps/foundation/frontend-editor/frontend-editor-tinymce-web/src/main/java/com/liferay/frontend/editor/tinymce/web/internal/constants/TinyMCEEditorConstants.java
+++ modules/apps/foundation/frontend-editor/frontend-editor-tinymce-web/src/main/java/com/liferay/frontend/editor/tinymce/web/internal/constants/TinyMCEEditorConstants.java
@@ -23,10 +23,12 @@ public class TinyMCEEditorConstants {
 	 * The namespace prefix added to the taglib attributes when they are put in
 	 * the {@link javax.servlet.http.HttpServletRequest}.
 	 *
+	 * <p>
 	 * Do not change this value unless you stop implementing the legacy
-	 * {@link com.liferay.portal.kernel.editor.Editor} interface and leave
-	 * {@link com.liferay.frontend.editor.api.EditorRenderer} alone, otherwise
+	 * {@link com.liferay.portal.kernel.editor.Editor} interface and leave the
+	 * {@link com.liferay.frontend.editor.api.EditorRenderer} alone; otherwise,
 	 * the former will fail because it hard codes the use of this namespace.
+	 * </p>
 	 */
 	public static final String ATTRIBUTE_NAMESPACE = "liferay-ui:input-editor";
 
