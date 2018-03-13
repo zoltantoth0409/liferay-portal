3a0315ebcf86fa2fdd8067d384940e29dec16dea
Alessio Rendina <asilcomando@gmail.com>
2018-03-14 00:05:49 +0100

    COMMERCE-305 SF

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPMeasurementUnitImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPMeasurementUnitImpl.java
@@ -17,22 +17,11 @@ package com.liferay.commerce.product.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the CPMeasurementUnit service. Represents a row in the &quot;CPMeasurementUnit&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.commerce.product.model.CPMeasurementUnit} interface.
- * </p>
- *
  * @author Marco Leo
  */
 @ProviderType
 public class CPMeasurementUnitImpl extends CPMeasurementUnitBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a cp measurement unit model instance should use the {@link com.liferay.commerce.product.model.CPMeasurementUnit} interface instead.
-	 */
 	public CPMeasurementUnitImpl() {
 	}
 
