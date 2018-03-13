35e20e40c2768270ce1e087e4580b6afb63a36af
Marco Leo <marco.leo@liferay.com>
2018-03-13 00:56:16 -0700

    LPS-77353 Add default Blank group initializer

---
+++ modules/apps/web-experience/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/initializer/BlankSiteGroupInitializer.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.admin.web.internal.initializer;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.site.exception.InitializationException;
+import com.liferay.site.initializer.GroupInitializer;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	immediate = true,
+	property = {"group.initializer.key=" + BlankSiteGroupInitializer.KEY}
+)
+public class BlankSiteGroupInitializer implements GroupInitializer {
+
+	public static final String KEY = "blank-site-group-initializer";
+
+	@Override
+	public String getDescription(Locale locale) {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return LanguageUtil.get(locale, "blank-site");
+	}
+
+	@Override
+	public String getThumbnailSrc() {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public void initialize(long groupId) throws InitializationException {
+	}
+
+	@Override
+	public boolean isActive(long companyId) {
+		return true;
+	}
+
+}
