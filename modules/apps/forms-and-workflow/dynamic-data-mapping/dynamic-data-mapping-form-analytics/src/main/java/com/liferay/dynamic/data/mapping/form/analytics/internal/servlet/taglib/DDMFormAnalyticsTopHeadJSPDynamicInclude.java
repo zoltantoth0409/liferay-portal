d16397908ab57a1f80ac6f99030693be079f66f5
Peter Shin <peter.shin@liferay.com>
2018-03-13 11:56:39 -0700

    LPS-74544 Auto SF

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-analytics/src/main/java/com/liferay/dynamic/data/mapping/form/analytics/internal/servlet/taglib/DDMFormAnalyticsTopHeadJSPDynamicInclude.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-analytics/src/main/java/com/liferay/dynamic/data/mapping/form/analytics/internal/servlet/taglib/DDMFormAnalyticsTopHeadJSPDynamicInclude.java
@@ -38,7 +38,8 @@ import org.osgi.service.component.annotations.Reference;
  * @author Leonardo Barros
  */
 @Component(immediate = true, service = DynamicInclude.class)
-public class DDMFormAnalyticsTopHeadJSPDynamicInclude extends BaseJSPDynamicInclude {
+public class DDMFormAnalyticsTopHeadJSPDynamicInclude
+	extends BaseJSPDynamicInclude {
 
 	@Override
 	public void include(
