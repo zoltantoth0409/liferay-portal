dbc307579b42b523211a6c9a4cdab3797fefae24
Alessio Rendina <asilcomando@gmail.com>
2018-03-14 00:11:20 +0100

    COMMERCE-305 Added base commerce tax fixed rate display context

---
+++ modules/dxp/apps/commerce/commerce-tax-engine-fixed-web/src/main/java/com/liferay/commerce/tax/engine/fixed/web/internal/display/context/BaseCommerceTaxFixedRateDisplayContext.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.tax.engine.fixed.web.internal.display.context;
+
+import com.liferay.commerce.constants.CommerceConstants;
+import com.liferay.commerce.constants.CommerceTaxScreenNavigationConstants;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.service.CommerceCurrencyService;
+import com.liferay.commerce.model.CommerceTaxMethod;
+import com.liferay.commerce.service.CommerceTaxMethodService;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.dao.search.RowChecker;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public abstract class BaseCommerceTaxFixedRateDisplayContext<T> {
+
+	public BaseCommerceTaxFixedRateDisplayContext(
+		CommerceCurrencyService commerceCurrencyService,
+		CommerceTaxMethodService commerceTaxMethodService,
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		this.commerceCurrencyService = commerceCurrencyService;
+		this.commerceTaxMethodService = commerceTaxMethodService;
+		this.renderRequest = renderRequest;
+		this.renderResponse = renderResponse;
+
+		_defaultOrderByCol = "create-date";
+		_defaultOrderByType = "desc";
+	}
+
+	public String getCommerceCurrencyCode() throws PortalException {
+		CommerceTaxMethod commerceTaxMethod = getCommerceTaxMethod();
+
+		if (commerceTaxMethod.isPercentage()) {
+			return StringPool.PERCENT;
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		CommerceCurrency commerceCurrency =
+			commerceCurrencyService.fetchPrimaryCommerceCurrency(
+				themeDisplay.getScopeGroupId());
+
+		if (commerceCurrency != null) {
+			return commerceCurrency.getCode();
+		}
+
+		return StringPool.BLANK;
+	}
+
+	public CommerceTaxMethod getCommerceTaxMethod() throws PortalException {
+		if (_commerceTaxMethod != null) {
+			return _commerceTaxMethod;
+		}
+
+		long commerceTaxMethodId = ParamUtil.getLong(
+			renderRequest, "commerceTaxMethodId");
+
+		if (commerceTaxMethodId > 0) {
+			_commerceTaxMethod = commerceTaxMethodService.getCommerceTaxMethod(
+				commerceTaxMethodId);
+		}
+
+		return _commerceTaxMethod;
+	}
+
+	public long getCommerceTaxMethodId() throws PortalException {
+		CommerceTaxMethod commerceTaxMethod = getCommerceTaxMethod();
+
+		if (commerceTaxMethod == null) {
+			return 0;
+		}
+
+		return commerceTaxMethod.getCommerceTaxMethodId();
+	}
+
+	public String getOrderByCol() {
+		return ParamUtil.getString(
+			renderRequest, SearchContainer.DEFAULT_ORDER_BY_COL_PARAM,
+			_defaultOrderByCol);
+	}
+
+	public String getOrderByType() {
+		return ParamUtil.getString(
+			renderRequest, SearchContainer.DEFAULT_ORDER_BY_TYPE_PARAM,
+			_defaultOrderByType);
+	}
+
+	public PortletURL getPortletURL() throws PortalException {
+		PortletURL portletURL = renderResponse.createRenderURL();
+
+		portletURL.setParameter(
+			"commerceAdminModuleKey",
+			CommerceConstants.TAXES_COMMERCE_ADMIN_MODULE_KEY);
+		portletURL.setParameter(
+			"mvcRenderCommandName", "editCommerceTaxMethod");
+		portletURL.setParameter(
+			"screenNavigationEntryKey", getSelectedScreenNavigationEntryKey());
+
+		CommerceTaxMethod commerceTaxMethod = getCommerceTaxMethod();
+
+		if (commerceTaxMethod != null) {
+			portletURL.setParameter(
+				"commerceTaxMethodId",
+				String.valueOf(commerceTaxMethod.getCommerceTaxMethodId()));
+		}
+
+		String engineKey = ParamUtil.getString(renderRequest, "engineKey");
+
+		if (Validator.isNotNull(engineKey)) {
+			portletURL.setParameter("engineKey", engineKey);
+		}
+
+		String delta = ParamUtil.getString(renderRequest, "delta");
+
+		if (Validator.isNotNull(delta)) {
+			portletURL.setParameter("delta", delta);
+		}
+
+		portletURL.setParameter("orderByCol", getOrderByCol());
+		portletURL.setParameter("orderByType", getOrderByType());
+
+		return portletURL;
+	}
+
+	public RowChecker getRowChecker() {
+		if (_rowChecker == null) {
+			_rowChecker = new EmptyOnClickRowChecker(renderResponse);
+		}
+
+		return _rowChecker;
+	}
+
+	public String getScreenNavigationEntryKey() {
+		return CommerceTaxScreenNavigationConstants.
+			ENTRY_KEY_COMMERCE_TAX_METHOD_DETAIL;
+	}
+
+	public abstract SearchContainer<T> getSearchContainer()
+		throws PortalException;
+
+	public void setDefaultOrderByCol(String defaultOrderByCol) {
+		_defaultOrderByCol = defaultOrderByCol;
+	}
+
+	public void setDefaultOrderByType(String defaultOrderByType) {
+		_defaultOrderByType = defaultOrderByType;
+	}
+
+	protected String getSelectedScreenNavigationEntryKey() {
+		return ParamUtil.getString(
+			renderRequest, "screenNavigationEntryKey",
+			getScreenNavigationEntryKey());
+	}
+
+	protected final CommerceCurrencyService commerceCurrencyService;
+	protected final CommerceTaxMethodService commerceTaxMethodService;
+	protected final RenderRequest renderRequest;
+	protected final RenderResponse renderResponse;
+	protected SearchContainer<T> searchContainer;
+
+	private CommerceTaxMethod _commerceTaxMethod;
+	private String _defaultOrderByCol;
+	private String _defaultOrderByType;
+	private RowChecker _rowChecker;
+
+}
