19305172aae4d5996aee77f4d09a5b8b54b7bc38
Marco Leo <marco.leo@liferay.com>
2018-03-13 23:48:18 -0700

    LPS-78826 Adding missing setters

--- modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ScreenNavigationTag.java
+++ modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ScreenNavigationTag.java
@@ -37,6 +37,7 @@ import javax.servlet.jsp.PageContext;
 
 /**
  * @author Eudaldo Alonso
+ * @author Marco Leo
  */
 public class ScreenNavigationTag extends IncludeTag {
 
@@ -72,8 +73,16 @@ public class ScreenNavigationTag extends IncludeTag {
 		return _context;
 	}
 
+	public void setContainerCssClass(String containerCssClass) {
+		_containerCssClass = containerCssClass;
+	}
+
 	public void setContext(Object context) {
 		_context = context;
+	}
+
+	public void setFullContainerCssClass(String fullContainerCssClass) {
+		_fullContainerCssClass = fullContainerCssClass;
 	}
 
 	public void setId(String id) {
@@ -86,6 +95,10 @@ public class ScreenNavigationTag extends IncludeTag {
 
 	public void setModelBean(Object modelBean) {
 		_modelBean = modelBean;
+	}
+
+	public void setNavCssClass(String navCssClass) {
+		_navCssClass = navCssClass;
 	}
 
 	@Override
