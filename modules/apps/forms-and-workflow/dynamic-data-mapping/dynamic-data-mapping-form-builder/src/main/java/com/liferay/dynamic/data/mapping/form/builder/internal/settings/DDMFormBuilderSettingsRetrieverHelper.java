442a55bfda93232e8b793ebc58200cee1db561c7
hai.yu <8219754@liferay.com>
2018-03-15 17:29:01 +0800

    LPS-78856 remove static and use same fix.

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/java/com/liferay/dynamic/data/mapping/form/builder/internal/settings/DDMFormBuilderSettingsRetrieverHelper.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/java/com/liferay/dynamic/data/mapping/form/builder/internal/settings/DDMFormBuilderSettingsRetrieverHelper.java
@@ -29,6 +29,7 @@ import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONSerializer;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.List;
@@ -158,12 +159,14 @@ public class DDMFormBuilderSettingsRetrieverHelper {
 		return jsonSerializer.serializeDeep(ddmFormRules);
 	}
 
-	protected static String getServletContextPath(Servlet servlet) {
+	protected String getServletContextPath(Servlet servlet) {
+		String proxyPath = _portal.getPathProxy();
+
 		ServletConfig servletConfig = servlet.getServletConfig();
 
 		ServletContext servletContext = servletConfig.getServletContext();
 
-		return servletContext.getContextPath();
+		return proxyPath.concat(servletContext.getContextPath());
 	}
 
 	@Reference(
@@ -208,6 +211,9 @@ public class DDMFormBuilderSettingsRetrieverHelper {
 
 	@Reference
 	private JSONFactory _jsonFactory;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference(
 		target = "(osgi.http.whiteboard.servlet.name=com.liferay.dynamic.data.mapping.form.builder.internal.servlet.RolesServlet)"
