6c74cbece3d16e108f6c9fa41afd92363399da72
William Newbury <william.newbury@liferay.com>
2018-03-15 11:32:46 -0700

    LPS-78823 Implement contacts entry test helper

--- modules/apps/foundation/contacts/contacts-uad-test/src/testIntegration/java/com/liferay/contacts/uad/test/EntryUADEntityTestHelper.java
+++ modules/apps/foundation/contacts/contacts-uad-test/src/testIntegration/java/com/liferay/contacts/uad/test/EntryUADEntityTestHelper.java
@@ -15,27 +15,26 @@
 package com.liferay.contacts.uad.test;
 
 import com.liferay.contacts.model.Entry;
-
-import org.junit.Assume;
+import com.liferay.contacts.service.EntryLocalService;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author William Newbury
  */
 @Component(immediate = true, service = EntryUADEntityTestHelper.class)
 public class EntryUADEntityTestHelper {
-	/**
-	 * Implement addEntry() to enable some UAD tests.
-	 *
-	 * <p>
-	 * Several UAD tests depend on creating one or more valid Entries with a specified user ID in order to execute correctly. Implement addEntry() such that it creates a valid Entry with the specified user ID value and returns it in order to enable the UAD tests that depend on it.
-	 * </p>
-	 *
-	 */
-	public Entry addEntry(long userId) throws Exception {
-		Assume.assumeTrue(false);
 
-		return null;
+	public Entry addEntry(long userId) throws Exception {
+		return _entryLocalService.addEntry(
+			userId, RandomTestUtil.randomString(),
+			RandomTestUtil.randomString() + "@liferay.com",
+			RandomTestUtil.randomString());
 	}
+
+	@Reference
+	private EntryLocalService _entryLocalService;
+
 }
