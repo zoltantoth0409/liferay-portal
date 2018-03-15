6feb53211718cad57339dc310c0a80fb3c55cae7
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2018-03-15 11:35:13 -0700

    LPS-77425 Add a portal-tools.properties for 7.0.x

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/util/PortalTools.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/util/PortalTools.java
@@ -21,6 +21,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.gradle.api.Project;
@@ -33,7 +35,49 @@ public class PortalTools {
 
 	public static final String GROUP = "com.liferay";
 
+	public static final String PORTAL_VERSION_PROPERTY_NAME = "portal.version";
+
 	public static String getVersion(Project project, String name) {
+		return _getVersion(project, name, _getPortalVersion(project));
+	}
+
+	protected static final String PORTAL_TOOLS_FILE_NAME =
+		"com/liferay/gradle/plugins/dependencies/portal-tools.properties";
+
+	private static String _getPortalVersion(Project project) {
+		String portalVersion = null;
+
+		for (String name : _PORTAL_VERSION_PROPERTY_NAMES) {
+			portalVersion = GradleUtil.getProperty(project, name, (String)null);
+
+			if (Validator.isNotNull(portalVersion)) {
+				break;
+			}
+		}
+
+		if (portalVersion != null) {
+			portalVersion = portalVersion.trim();
+			portalVersion = portalVersion.toLowerCase();
+
+			int pos = portalVersion.indexOf('-');
+
+			if (pos != -1) {
+				portalVersion = portalVersion.substring(0, pos);
+			}
+
+			if (portalVersion.isEmpty() || portalVersion.equals("latest") ||
+				portalVersion.equals("master")) {
+
+				portalVersion = null;
+			}
+		}
+
+		return portalVersion;
+	}
+
+	private static String _getVersion(
+		Project project, String name, String portalVersion) {
+
 		String key = name + ".version";
 
 		String version = GradleUtil.getProperty(project, key, (String)null);
@@ -61,21 +105,50 @@ public class PortalTools {
 			return version;
 		}
 
-		return _versions.getProperty(name);
+		Properties properties = _versionsMap.get(portalVersion);
+
+		return properties.getProperty(name);
 	}
 
-	protected static final String PORTAL_TOOLS_FILE_NAME =
-		"com/liferay/gradle/plugins/dependencies/portal-tools.properties";
+	private static Properties _populateVersionsMap(
+			ClassLoader classLoader, String portalVersion,
+			Properties defaultProperties)
+		throws IOException {
 
-	private static final Properties _versions = new Properties();
+		String fileName = PORTAL_TOOLS_FILE_NAME;
+
+		if (portalVersion != null) {
+			fileName =
+				fileName.substring(0, fileName.length() - 11) + "-" +
+					portalVersion + ".properties";
+		}
+
+		Properties properties = new Properties(defaultProperties);
+
+		try (InputStream inputStream = classLoader.getResourceAsStream(
+				fileName)) {
+
+			properties.load(inputStream);
+		}
+
+		_versionsMap.put(portalVersion, properties);
+
+		return properties;
+	}
+
+	private static final String[] _PORTAL_VERSION_PROPERTY_NAMES =
+		{"git.working.branch.name", PORTAL_VERSION_PROPERTY_NAME};
+
+	private static final Map<String, Properties> _versionsMap = new HashMap<>();
 
 	static {
 		ClassLoader classLoader = PortalTools.class.getClassLoader();
 
-		try (InputStream inputStream = classLoader.getResourceAsStream(
-				PORTAL_TOOLS_FILE_NAME)) {
+		try {
+			Properties properties = _populateVersionsMap(
+				classLoader, null, null);
 
-			_versions.load(inputStream);
+			_populateVersionsMap(classLoader, "7.0.x", properties);
 		}
 		catch (IOException ioe) {
 			throw new ExceptionInInitializerError(ioe);
