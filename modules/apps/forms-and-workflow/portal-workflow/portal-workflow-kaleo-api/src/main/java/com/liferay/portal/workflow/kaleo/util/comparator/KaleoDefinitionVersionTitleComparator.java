69442b0b5638614d55038ceebe24a37b41ca67dd
Lino Alves <lino.alves@liferay.com>
2018-03-16 11:58:29 -0300

    LPS-73927 compare title by locale

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/util/comparator/KaleoDefinitionVersionTitleComparator.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/util/comparator/KaleoDefinitionVersionTitleComparator.java
@@ -14,9 +14,15 @@
 
 package com.liferay.portal.workflow.kaleo.util.comparator;
 
+import com.liferay.portal.kernel.util.CollatorUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
+
+import java.text.Collator;
+
+import java.util.Locale;
 
 /**
  * @author Inácio Nery
@@ -30,6 +36,20 @@ public class KaleoDefinitionVersionTitleComparator
 
 	public KaleoDefinitionVersionTitleComparator(boolean ascending) {
 		_ascending = ascending;
+
+		_locale = LocaleUtil.getSiteDefault();
+
+		_collator = CollatorUtil.getInstance(_locale);
+	}
+
+	public KaleoDefinitionVersionTitleComparator(
+		boolean ascending, Locale locale) {
+
+		_ascending = ascending;
+
+		_locale = locale;
+
+		_collator = CollatorUtil.getInstance(_locale);
 	}
 
 	@Override
@@ -38,11 +58,12 @@ public class KaleoDefinitionVersionTitleComparator
 		KaleoDefinitionVersion kaleoDefinitionVersion2) {
 
 		String title1 = StringUtil.toLowerCase(
-			kaleoDefinitionVersion1.getTitle());
-		String title2 = StringUtil.toLowerCase(
-			kaleoDefinitionVersion2.getTitle());
+			kaleoDefinitionVersion1.getTitle(_locale));
 
-		int value = title1.compareTo(title2);
+		String title2 = StringUtil.toLowerCase(
+			kaleoDefinitionVersion2.getTitle(_locale));
+
+		int value = _collator.compare(title1, title2);
 
 		if (_ascending) {
 			return value;
@@ -81,5 +102,7 @@ public class KaleoDefinitionVersionTitleComparator
 	private static final String[] _ORDER_BY_FIELDS = {"title"};
 
 	private final boolean _ascending;
+	private final Collator _collator;
+	private Locale _locale;
 
 }
