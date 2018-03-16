b35a3974923a82b9b06d3470c86568b5befc80c0
Pedro Queiroz <pedro.queiroz@liferay.com>
2018-03-16 15:18:13 -0300

    LPS-78530 Add unit test

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/type/paragraph/internal/ParagraphDDMFormFieldTemplateContextContributorTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type.paragraph.internal;
+
+import com.google.template.soy.data.SanitizedContent;
+import com.google.template.soy.data.UnsafeSanitizedContentOrdainer;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
+import com.liferay.dynamic.data.mapping.type.BaseDDMFormFieldTypeSettingsTestCase;
+import com.liferay.portal.template.soy.utils.SoyHTMLSanitizer;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * @author Pedro Queiroz
+ */
+@RunWith(PowerMockRunner.class)
+public class ParagraphDDMFormFieldTemplateContextContributorTest
+	extends BaseDDMFormFieldTypeSettingsTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+		setUpSoyHTMLSanitizer();
+	}
+
+	@Test
+	public void testGetParameters() {
+		DDMFormField ddmFormField = new DDMFormField("field", "paragraph");
+
+		String text = "<b>This is a header</b>\n";
+
+		ddmFormField.setProperty("text", text);
+
+		Map parameters =
+			_paragraphDDMFormFieldTemplateContextContributor.getParameters(
+				ddmFormField, new DDMFormFieldRenderingContext());
+
+		SanitizedContent sanitizedContent = (SanitizedContent)parameters.get(
+			"text");
+
+		Assert.assertEquals(text, sanitizedContent.getContent());
+	}
+
+	protected void setUpSoyHTMLSanitizer() throws Exception {
+		field(
+			ParagraphDDMFormFieldTemplateContextContributor.class,
+			"_soyHTMLSanitizer"
+		).set(
+			_paragraphDDMFormFieldTemplateContextContributor,
+			(SoyHTMLSanitizer)value ->
+				UnsafeSanitizedContentOrdainer.ordainAsSafe(
+					value, SanitizedContent.ContentKind.HTML)
+		);
+	}
+
+	private final ParagraphDDMFormFieldTemplateContextContributor
+		_paragraphDDMFormFieldTemplateContextContributor =
+			new ParagraphDDMFormFieldTemplateContextContributor();
+
+}
