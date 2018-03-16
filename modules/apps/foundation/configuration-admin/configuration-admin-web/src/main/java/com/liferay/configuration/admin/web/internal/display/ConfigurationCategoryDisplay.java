9fbe8fb39cdfea998d2282febd30e7a172b7ff79
Jorge Ferrer <jorge.ferrer@liferay.com>
2018-03-15 20:55:16 -0700

    LPS-75377 SF

--- modules/apps/foundation/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/display/ConfigurationCategoryDisplay.java
+++ modules/apps/foundation/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/display/ConfigurationCategoryDisplay.java
@@ -27,16 +27,16 @@ public class ConfigurationCategoryDisplay {
 		_configurationCategory = configurationCategory;
 	}
 
-	public String getCategorySection() {
-		return _configurationCategory.getCategorySection();
-	}
-
 	public String getCategoryIcon() {
 		return _configurationCategory.getCategoryIcon();
 	}
 
 	public String getCategoryKey() {
 		return _configurationCategory.getCategoryKey();
+	}
+
+	public String getCategorySection() {
+		return _configurationCategory.getCategorySection();
 	}
 
 	private final ConfigurationCategory _configurationCategory;
