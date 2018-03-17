89471669cf8376a310c7c691bc2fb071311f260f
Brian Chan <brian.chan@liferay.com>
2018-03-17 13:51:39 -0700

    LPS-77556 Add to deprecated list?

--- portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProperties.java
@@ -1911,6 +1911,7 @@ public class VerifyProperties extends VerifyProcess {
 		"sites.form.update.main", "sites.form.update.miscellaneous",
 		"sites.form.update.seo", "staging.lock.enabled",
 		"social.activity.sets.bundling.enabled", "social.activity.sets.enabled",
+		"social.bookmark.display.styles",
 		"table.mapper.cache.mapping.table.names", "tck.url",
 		"user.groups.indexer.enabled", "users.form.add.identification",
 		"users.indexer.enabled", "users.form.add.main",
