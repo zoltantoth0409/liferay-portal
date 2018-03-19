ea4a61fc6405f1f70180cc30b2319007094af0b7
Jonathan McCann <jonathan.mccann@liferay.com>
2018-03-19 08:20:14 -0700

    LPS-78936 When there is no rating for the asset set the average score to zero

--- portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetEntryFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetEntryFinderImpl.java
@@ -524,7 +524,8 @@ public class AssetEntryFinderImpl
 			sb.append("TEMP_TABLE.entryId = AssetEntry.entryId ORDER BY ");
 
 			if (orderByCol1.equals("ratings")) {
-				sb.append("TEMP_TABLE.averageScore");
+				sb.append("CASE WHEN TEMP_TABLE.averageScore IS NULL THEN 0 ");
+				sb.append("ELSE TEMP_TABLE.averageScore END");
 			}
 			else {
 				sb.append("AssetEntry.");
@@ -538,7 +539,9 @@ public class AssetEntryFinderImpl
 				!orderByCol1.equals(orderByCol2)) {
 
 				if (orderByCol2.equals("ratings")) {
-					sb.append(", TEMP_TABLE.averageScore");
+					sb.append(
+						", CASE WHEN TEMP_TABLE.averageScore IS NULL THEN 0 ");
+					sb.append("ELSE TEMP_TABLE.averageScore END");
 				}
 				else {
 					sb.append(", AssetEntry.");
