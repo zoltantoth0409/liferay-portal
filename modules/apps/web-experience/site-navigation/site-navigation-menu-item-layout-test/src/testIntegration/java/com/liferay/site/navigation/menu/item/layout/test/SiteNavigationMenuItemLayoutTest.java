9083cc75e00db347bfb8c65baeb581c30cdeb1cf
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-03-19 14:52:56 +0000

    LPS-78915 Adds tests to check to expected behaviour:
    1) There same menu "A" marked as Primary Navigation and Auto ("Add new pages to this menu" checkbox is checked).
    If user checks the checkbox "Add page to the Primary Navigation" page is added to the menu "A".
    If user doesn't check the checkbox "Add page to the Primary Navigation" page is not added to the menu "A".
    
    2) There is a Primary Navigation menu "A" and another menu "B" with the "Add new pages to this menu" checkbox checked.
    If user checks the checkbox "Add page to the Primary Navigation" page is added to the menu "A" and to the menu "B".
    If user doesn't check the checkbox "Add page to the Primary Navigation" page is not added to the menu "A", but added to the menu "B".

---
+++ modules/apps/web-experience/site-navigation/site-navigation-menu-item-layout-test/src/testIntegration/java/com/liferay/site/navigation/menu/item/layout/test/SiteNavigationMenuItemLayoutTest.java
@@ -0,0 +1,197 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.menu.item.layout.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.service.LayoutServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.site.navigation.constants.SiteNavigationConstants;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalServiceUtil;
+import com.liferay.site.navigation.service.SiteNavigationMenuLocalServiceUtil;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+public class SiteNavigationMenuItemLayoutTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testAddToPrimaryMenuFalseToMenu() throws PortalException {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		SiteNavigationMenuLocalServiceUtil.addSiteNavigationMenu(
+			TestPropsValues.getUserId(), _group.getGroupId(), "Auto Menu",
+			SiteNavigationConstants.TYPE_DEFAULT, true, serviceContext);
+
+		SiteNavigationMenuLocalServiceUtil.addSiteNavigationMenu(
+			TestPropsValues.getUserId(), _group.getGroupId(), "Primary Menu",
+			SiteNavigationConstants.TYPE_PRIMARY, false, serviceContext);
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.getSiteDefault(), "welcome");
+
+		UnicodeProperties typeSettingsProperties = new UnicodeProperties();
+
+		typeSettingsProperties.put(
+			"addToPrimaryMenu", Boolean.FALSE.toString());
+
+		LayoutServiceUtil.addLayout(
+			_group.getGroupId(), false, 0, nameMap, new HashMap<>(),
+			new HashMap<>(), new HashMap<>(), new HashMap<>(),
+			LayoutConstants.TYPE_PORTLET, typeSettingsProperties.toString(),
+			false, new HashMap<>(), serviceContext);
+
+		Assert.assertEquals(
+			1,
+			SiteNavigationMenuItemLocalServiceUtil.
+				getSiteNavigationMenuItemsCount());
+	}
+
+	@Test
+	public void testAddToPrimaryMenuFalseToPrimaryMenu()
+		throws PortalException {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		SiteNavigationMenuLocalServiceUtil.addSiteNavigationMenu(
+			TestPropsValues.getUserId(), _group.getGroupId(), "Primary Menu",
+			SiteNavigationConstants.TYPE_PRIMARY, true, serviceContext);
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.getSiteDefault(), "welcome");
+
+		UnicodeProperties typeSettingsProperties = new UnicodeProperties();
+
+		typeSettingsProperties.put(
+			"addToPrimaryMenu", Boolean.FALSE.toString());
+
+		LayoutServiceUtil.addLayout(
+			_group.getGroupId(), false, 0, nameMap, new HashMap<>(),
+			new HashMap<>(), new HashMap<>(), new HashMap<>(),
+			LayoutConstants.TYPE_PORTLET, typeSettingsProperties.toString(),
+			false, new HashMap<>(), serviceContext);
+
+		Assert.assertEquals(
+			0,
+			SiteNavigationMenuItemLocalServiceUtil.
+				getSiteNavigationMenuItemsCount());
+	}
+
+	@Test
+	public void testAddToPrimaryMenuTrueToMenu() throws PortalException {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		SiteNavigationMenuLocalServiceUtil.addSiteNavigationMenu(
+			TestPropsValues.getUserId(), _group.getGroupId(), "Auto Menu",
+			SiteNavigationConstants.TYPE_DEFAULT, true, serviceContext);
+
+		SiteNavigationMenuLocalServiceUtil.addSiteNavigationMenu(
+			TestPropsValues.getUserId(), _group.getGroupId(), "Primary Menu",
+			SiteNavigationConstants.TYPE_PRIMARY, false, serviceContext);
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.getSiteDefault(), "welcome");
+
+		UnicodeProperties typeSettingsProperties = new UnicodeProperties();
+
+		typeSettingsProperties.put("addToPrimaryMenu", Boolean.TRUE.toString());
+
+		LayoutServiceUtil.addLayout(
+			_group.getGroupId(), false, 0, nameMap, new HashMap<>(),
+			new HashMap<>(), new HashMap<>(), new HashMap<>(),
+			LayoutConstants.TYPE_PORTLET, typeSettingsProperties.toString(),
+			false, new HashMap<>(), serviceContext);
+
+		Assert.assertEquals(
+			2,
+			SiteNavigationMenuItemLocalServiceUtil.
+				getSiteNavigationMenuItemsCount());
+	}
+
+	@Test
+	public void testAddToPrimaryMenuTrueToPrimaryMenu() throws PortalException {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		SiteNavigationMenuLocalServiceUtil.addSiteNavigationMenu(
+			TestPropsValues.getUserId(), _group.getGroupId(), "Primary Menu",
+			SiteNavigationConstants.TYPE_PRIMARY, true, serviceContext);
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.getSiteDefault(), "welcome");
+
+		UnicodeProperties typeSettingsProperties = new UnicodeProperties();
+
+		typeSettingsProperties.put("addToPrimaryMenu", Boolean.TRUE.toString());
+
+		LayoutServiceUtil.addLayout(
+			_group.getGroupId(), false, 0, nameMap, new HashMap<>(),
+			new HashMap<>(), new HashMap<>(), new HashMap<>(),
+			LayoutConstants.TYPE_PORTLET, typeSettingsProperties.toString(),
+			false, new HashMap<>(), serviceContext);
+
+		Assert.assertEquals(
+			1,
+			SiteNavigationMenuItemLocalServiceUtil.
+				getSiteNavigationMenuItemsCount());
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
