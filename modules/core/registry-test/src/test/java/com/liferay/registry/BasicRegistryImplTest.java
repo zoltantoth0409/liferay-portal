31dca68ed1367f908055bbb790848b742396fde8
Preston-Crary <preston.crary@liferay.com>
2018-03-21 15:30:26 -0700

    LPS-79020 Create test to demonstrate the problem.

--- modules/core/registry-test/src/test/java/com/liferay/registry/BasicRegistryImplTest.java
+++ modules/core/registry-test/src/test/java/com/liferay/registry/BasicRegistryImplTest.java
@@ -16,6 +16,8 @@ package com.liferay.registry;
 
 import java.util.Collections;
 import java.util.EventListener;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Assert;
@@ -42,6 +44,46 @@ public class BasicRegistryImplTest {
 		serviceTracker.open();
 
 		Assert.assertNull(serviceTracker.getService());
+	}
+
+	@Test
+	public void testServiceRanking() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		Foo foo1 = new Foo();
+
+		Map<String, Object> properties = new HashMap<>();
+
+		properties.put("service.ranking", 1);
+
+		ServiceRegistration<Foo> serviceRegistration1 =
+			registry.registerService(Foo.class, foo1, properties);
+
+		ServiceReference<Foo> serviceReference = registry.getServiceReference(
+			Foo.class);
+
+		Assert.assertSame(foo1, registry.getService(serviceReference));
+
+		Foo foo2 = new Foo();
+
+		properties = new HashMap<>();
+
+		properties.put("service.ranking", 2);
+
+		ServiceRegistration<Foo> serviceRegistration2 =
+			registry.registerService(Foo.class, foo2, properties);
+
+		serviceReference = registry.getServiceReference(Foo.class);
+
+		Assert.assertSame(foo2, registry.getService(serviceReference));
+
+		serviceRegistration2.unregister();
+
+		serviceReference = registry.getServiceReference(Foo.class);
+
+		Assert.assertSame(foo1, registry.getService(serviceReference));
+
+		serviceRegistration1.unregister();
 	}
 
 	@Test
