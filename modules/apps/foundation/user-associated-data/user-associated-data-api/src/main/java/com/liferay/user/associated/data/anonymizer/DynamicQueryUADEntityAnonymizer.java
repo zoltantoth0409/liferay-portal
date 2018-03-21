38a34298f89ed59ec1f0ffa4dbb35517e6fadf2d
drewbrokke <drew.brokke@liferay.com>
2018-03-21 14:45:44 -0700

    LPS-78886 user-associated-data-api - creates DynamicQueryUADEntityAnonymizer.java to replace the BaseUADEntityAnonymizer.java

---
+++ modules/apps/foundation/user-associated-data/user-associated-data-api/src/main/java/com/liferay/user/associated/data/anonymizer/DynamicQueryUADEntityAnonymizer.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.associated.data.anonymizer;
+
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.user.associated.data.util.UADDynamicQueryHelper;
+
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author William Newbury
+ */
+public abstract class DynamicQueryUADEntityAnonymizer<T extends BaseModel>
+	implements UADEntityAnonymizer<T> {
+
+	@Override
+	public void autoAnonymizeAll(long userId) throws PortalException {
+		ActionableDynamicQuery actionableDynamicQuery =
+			_getActionableDynamicQuery(userId);
+
+		actionableDynamicQuery.setPerformActionMethod(
+			(T entity) -> autoAnonymize(entity, userId));
+
+		actionableDynamicQuery.performActions();
+	}
+
+	@Override
+	public void deleteAll(long userId) throws PortalException {
+		ActionableDynamicQuery actionableDynamicQuery =
+			_getActionableDynamicQuery(userId);
+
+		actionableDynamicQuery.setPerformActionMethod(
+			(T entity) -> delete(entity));
+
+		actionableDynamicQuery.performActions();
+	}
+
+	protected abstract ActionableDynamicQuery doGetActionableDynamicQuery();
+
+	protected abstract String[] doGetUserIdFieldNames();
+
+	@Reference
+	protected UADDynamicQueryHelper uadDynamicQueryHelper;
+
+	private ActionableDynamicQuery _getActionableDynamicQuery(long userId) {
+		return uadDynamicQueryHelper.addActionableDynamicQueryCriteria(
+			doGetActionableDynamicQuery(), doGetUserIdFieldNames(), userId);
+	}
+
+}
