b2156bd64b8f59ac080a31c7d47625b28ecc1257
drewbrokke <drew.brokke@liferay.com>
2018-03-21 14:47:08 -0700

    LPS-78886 user-associated-data-api - creates DynamicQueryUADEntityExporter.java to replace the BaseUADEntityExporter.java

---
+++ modules/apps/foundation/user-associated-data/user-associated-data-api/src/main/java/com/liferay/user/associated/data/exporter/DynamicQueryUADEntityExporter.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.associated.data.exporter;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.petra.xml.XMLUtil;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.SystemProperties;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.zip.ZipWriter;
+import com.liferay.portal.kernel.zip.ZipWriterFactoryUtil;
+import com.liferay.user.associated.data.exception.UADEntityExporterException;
+import com.liferay.user.associated.data.util.UADDynamicQueryHelper;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author William Newbury
+ */
+public abstract class DynamicQueryUADEntityExporter<T extends BaseModel>
+	implements UADEntityExporter<T> {
+
+	@Override
+	public long count(long userId) throws PortalException {
+		return _getActionableDynamicQuery(userId).performCount();
+	}
+
+	@Override
+	public byte[] export(T entity) throws PortalException {
+		String xml = entity.toXmlString();
+
+		xml = formatXML(xml);
+
+		try {
+			return xml.getBytes(StringPool.UTF8);
+		}
+		catch (UnsupportedEncodingException uee) {
+			throw new UADEntityExporterException(uee);
+		}
+	}
+
+	@Override
+	public File exportAll(long userId) throws PortalException {
+		ActionableDynamicQuery actionableDynamicQuery =
+			_getActionableDynamicQuery(userId);
+
+		AtomicReference<ZipWriter> zipWriterAtomicReference =
+			new AtomicReference<>();
+
+		actionableDynamicQuery.setPerformActionMethod(
+			(ActionableDynamicQuery.PerformActionMethod<T>)entity -> {
+				byte[] data = export(entity);
+
+				try {
+					zipWriterAtomicReference.compareAndSet(
+						null, getZipWriter(userId, entity.getModelClassName()));
+
+					ZipWriter zipWriter = zipWriterAtomicReference.get();
+
+					zipWriter.addEntry(
+						entity.getPrimaryKeyObj() + ".xml", data);
+				}
+				catch (IOException ioe) {
+					throw new PortalException(ioe);
+				}
+			});
+
+		actionableDynamicQuery.performActions();
+
+		ZipWriter zipWriter = zipWriterAtomicReference.get();
+
+		return zipWriter.getFile();
+	}
+
+	protected File createFolder(long userId) {
+		StringBundler sb = new StringBundler(3);
+
+		sb.append(SystemProperties.get(SystemProperties.TMP_DIR));
+		sb.append("/liferay/uad/");
+		sb.append(userId);
+
+		File file = new File(sb.toString());
+
+		file.mkdirs();
+
+		return file;
+	}
+
+	protected abstract ActionableDynamicQuery doGetActionableDynamicQuery();
+
+	protected abstract String[] doGetUserIdFieldNames();
+
+	protected String formatXML(String xml) {
+		return XMLUtil.formatXML(xml);
+	}
+
+	protected ZipWriter getZipWriter(long userId, String modelClassName) {
+		File file = createFolder(userId);
+
+		StringBundler sb = new StringBundler(6);
+
+		sb.append(file.getAbsolutePath());
+		sb.append(StringPool.SLASH);
+		sb.append(modelClassName);
+		sb.append(StringPool.UNDERLINE);
+		sb.append(Time.getShortTimestamp());
+		sb.append(".zip");
+
+		return ZipWriterFactoryUtil.getZipWriter(new File(sb.toString()));
+	}
+
+	@Reference
+	protected UADDynamicQueryHelper uadDynamicQueryHelper;
+
+	private ActionableDynamicQuery _getActionableDynamicQuery(long userId) {
+		return uadDynamicQueryHelper.addActionableDynamicQueryCriteria(
+			doGetActionableDynamicQuery(), doGetUserIdFieldNames(), userId);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DynamicQueryUADEntityExporter.class);
+
+}
