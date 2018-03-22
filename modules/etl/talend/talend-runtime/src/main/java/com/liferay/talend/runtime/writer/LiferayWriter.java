be10c6a4876d69783c9af3f846d1fe6b67c2f491
Zoltán Takács <zoltan.takacs@liferay.com>
2018-03-22 18:23:08 +0100

    COMMERCE-345 More debug information

--- modules/etl/talend/talend-runtime/src/main/java/com/liferay/talend/runtime/writer/LiferayWriter.java
+++ modules/etl/talend/talend-runtime/src/main/java/com/liferay/talend/runtime/writer/LiferayWriter.java
@@ -108,7 +108,9 @@ public class LiferayWriter
 				_runtimeContainer, singleResourceUri.toASCIIString());
 		}
 		catch (IOException ioe) {
-			_log.error("Unable to delete the resource: ", ioe);
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to delete the resource: ", ioe);
+			}
 
 			throw ioe;
 		}
@@ -125,7 +127,9 @@ public class LiferayWriter
 				_runtimeContainer, resourceURL, apioForm);
 		}
 		catch (IOException ioe) {
-			_log.error("Unable to insert the resource: ", ioe);
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to insert the resource: ", ioe);
+			}
 
 			throw ioe;
 		}
@@ -151,8 +155,8 @@ public class LiferayWriter
 				_runtimeContainer, singleResourceUri.toASCIIString(), apioForm);
 		}
 		catch (IOException ioe) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Unable to update the resource: " + ioe.getMessage());
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to update the resource: ", ioe);
 			}
 
 			throw ioe;
@@ -183,6 +187,17 @@ public class LiferayWriter
 	public void write(Object indexedRecordDatum) throws IOException {
 		if ((indexedRecordDatum == null) ||
 			!(indexedRecordDatum instanceof IndexedRecord)) {
+
+			if (_log.isDebugEnabled()) {
+				if (indexedRecordDatum != null) {
+					_log.debug(
+						"Unable to process incoming data row: " +
+							indexedRecordDatum.toString());
+				}
+				else {
+					_log.debug("Data row is null, skipping it.");
+				}
+			}
 
 			return;
 		}
