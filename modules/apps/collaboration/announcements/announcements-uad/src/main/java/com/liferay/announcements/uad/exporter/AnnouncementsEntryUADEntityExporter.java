db92b12e16aa02b763721ac11135e4942fa4366d
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-03-23 16:23:06 -0700

    LPS-75611 Implement export for AnnouncementsEntry

--- modules/apps/collaboration/announcements/announcements-uad/src/main/java/com/liferay/announcements/uad/exporter/AnnouncementsEntryUADEntityExporter.java
+++ modules/apps/collaboration/announcements/announcements-uad/src/main/java/com/liferay/announcements/uad/exporter/AnnouncementsEntryUADEntityExporter.java
@@ -14,25 +14,24 @@
 
 package com.liferay.announcements.uad.exporter;
 
-import com.liferay.announcements.constants.AnnouncementsPortletKeys;
 import com.liferay.announcements.kernel.model.AnnouncementsEntry;
+import com.liferay.announcements.kernel.service.AnnouncementsEntryLocalService;
 import com.liferay.announcements.uad.constants.AnnouncementsUADConstants;
 import com.liferay.announcements.uad.entity.AnnouncementsEntryUADEntity;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
-import com.liferay.portal.kernel.repository.model.Folder;
-import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.zip.ZipWriter;
 import com.liferay.user.associated.data.aggregator.UADEntityAggregator;
 import com.liferay.user.associated.data.entity.UADEntity;
 import com.liferay.user.associated.data.exception.UADEntityException;
 import com.liferay.user.associated.data.exception.UADEntityExporterException;
 import com.liferay.user.associated.data.exporter.BaseUADEntityExporter;
 import com.liferay.user.associated.data.exporter.UADEntityExporter;
+import com.liferay.user.associated.data.util.UADDynamicQueryHelper;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import org.osgi.service.component.annotations.Component;
@@ -49,26 +48,16 @@ import org.osgi.service.component.annotations.Reference;
 public class AnnouncementsEntryUADEntityExporter extends BaseUADEntityExporter {
 
 	@Override
-	public void export(UADEntity uadEntity) throws PortalException {
+	public byte[] export(UADEntity uadEntity) throws PortalException {
 		AnnouncementsEntry announcementsEntry = _getAnnouncementsEntry(
 			uadEntity);
 
-		String json = getJSON(announcementsEntry);
+		String xml = announcementsEntry.toXmlString();
 
-		Folder folder = getFolder(
-			announcementsEntry.getCompanyId(),
-			AnnouncementsPortletKeys.ANNOUNCEMENTS, _FOLDER_NAME);
+		xml = formatXML(xml);
 
 		try {
-			InputStream is = new UnsyncByteArrayInputStream(
-				json.getBytes(StringPool.UTF8));
-
-			PortletFileRepositoryUtil.addPortletFileEntry(
-				folder.getGroupId(), announcementsEntry.getUserId(),
-				Group.class.getName(), folder.getGroupId(),
-				AnnouncementsPortletKeys.ANNOUNCEMENTS, folder.getFolderId(),
-				is, uadEntity.getUADEntityId() + ".json",
-				ContentTypes.APPLICATION_JSON, false);
+			return xml.getBytes(StringPool.UTF8);
 		}
 		catch (UnsupportedEncodingException uee) {
 			throw new UADEntityExporterException(uee);
@@ -76,8 +65,57 @@ public class AnnouncementsEntryUADEntityExporter extends BaseUADEntityExporter {
 	}
 
 	@Override
+	public File exportAll(long userId) throws PortalException {
+		ActionableDynamicQuery actionableDynamicQuery =
+			_getActionableDynamicQuery(userId);
+
+		ZipWriter zipWriter = getZipWriter(userId);
+
+		actionableDynamicQuery.setPerformActionMethod(
+			new ActionableDynamicQuery.
+				PerformActionMethod<AnnouncementsEntry>() {
+
+				@Override
+				public void performAction(AnnouncementsEntry announcementsEntry)
+					throws PortalException {
+
+					AnnouncementsEntryUADEntity announcementsEntryUADEntity =
+						_getAnnouncementsEntryUADEntity(
+							userId, announcementsEntry);
+
+					byte[] data = export(announcementsEntryUADEntity);
+
+					try {
+						zipWriter.addEntry(
+							announcementsEntry.getEntryId() + ".xml", data);
+					}
+					catch (IOException ioe) {
+						throw new PortalException(ioe);
+					}
+				}
+
+			});
+
+		actionableDynamicQuery.performActions();
+
+		return zipWriter.getFile();
+	}
+
+	@Override
+	protected String getEntityName() {
+		return AnnouncementsEntry.class.getName();
+	}
+
+	@Override
 	protected UADEntityAggregator getUADEntityAggregator() {
 		return _uadEntityAggregator;
+	}
+
+	private ActionableDynamicQuery _getActionableDynamicQuery(long userId) {
+		return _uadDynamicQueryHelper.addActionableDynamicQueryCriteria(
+			_announcementsEntryLocalService.getActionableDynamicQuery(),
+			AnnouncementsUADConstants.USER_ID_FIELD_NAMES_ANNOUNCEMENTS_ENTRY,
+			userId);
 	}
 
 	private AnnouncementsEntry _getAnnouncementsEntry(UADEntity uadEntity)
@@ -91,13 +129,25 @@ public class AnnouncementsEntryUADEntityExporter extends BaseUADEntityExporter {
 		return announcementsEntryUADEntity.getAnnouncementsEntry();
 	}
 
+	private AnnouncementsEntryUADEntity _getAnnouncementsEntryUADEntity(
+		long userId, AnnouncementsEntry announcementsEntry) {
+
+		return new AnnouncementsEntryUADEntity(
+			userId, String.valueOf(announcementsEntry.getEntryId()),
+			announcementsEntry);
+	}
+
 	private void _validate(UADEntity uadEntity) throws PortalException {
 		if (!(uadEntity instanceof AnnouncementsEntryUADEntity)) {
 			throw new UADEntityException();
 		}
 	}
 
-	private static final String _FOLDER_NAME = "UADExport";
+	@Reference
+	private AnnouncementsEntryLocalService _announcementsEntryLocalService;
+
+	@Reference
+	private UADDynamicQueryHelper _uadDynamicQueryHelper;
 
 	@Reference(
 		target = "(model.class.name=" + AnnouncementsUADConstants.CLASS_NAME_ANNOUNCEMENTS_ENTRY + ")"
