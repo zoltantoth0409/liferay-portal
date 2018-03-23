b63442c46ad628bc83dee2a76e15646947452688
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-03-23 17:53:21 +0100

    LPS-79060 ButtonRowTag - Copy button-row tag, no logic changes

---
+++ modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ButtonRowTag.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.aui.base.BaseButtonRowTag;
+import com.liferay.taglib.util.InlineUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspWriter;
+
+/**
+ * @author Julio Camarero
+ * @author Jorge Ferrer
+ * @author Brian Wing Shun Chan
+ */
+public class ButtonRowTag extends BaseButtonRowTag {
+
+	@Override
+	protected boolean isCleanUpSetAttributes() {
+		return _CLEAN_UP_SET_ATTRIBUTES;
+	}
+
+	@Override
+	protected int processEndTag() throws Exception {
+		JspWriter jspWriter = pageContext.getOut();
+
+		jspWriter.write("</div>");
+
+		return EVAL_PAGE;
+	}
+
+	@Override
+	protected int processStartTag() throws Exception {
+		JspWriter jspWriter = pageContext.getOut();
+
+		jspWriter.write("<div class=\"button-holder ");
+
+		String cssClass = getCssClass();
+
+		if (cssClass != null) {
+			jspWriter.write(cssClass);
+		}
+
+		jspWriter.write("\" ");
+
+		String id = getId();
+
+		if (id != null) {
+			jspWriter.write("id=\"");
+			jspWriter.write(id);
+			jspWriter.write("\" ");
+		}
+
+		jspWriter.write(
+			InlineUtil.buildDynamicAttributes(getDynamicAttributes()));
+
+		jspWriter.write(">");
+
+		return EVAL_BODY_INCLUDE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (themeDisplay.isStatePopUp()) {
+			String cssClass = "dialog-footer";
+
+			if (getCssClass() != null) {
+				cssClass = cssClass + StringPool.SPACE + getCssClass();
+			}
+
+			setCssClass(cssClass);
+		}
+
+		super.setAttributes(request);
+	}
+
+	private static final boolean _CLEAN_UP_SET_ATTRIBUTES = true;
+
+}
---
+++ modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/base/BaseButtonRowTag.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib.base;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+
+/**
+ * @author Eduardo Lundgren
+ * @author Bruno Basto
+ * @author Nathan Cavanaugh
+ * @author Julio Camarero
+ * @generated
+ */
+public abstract class BaseButtonRowTag extends com.liferay.taglib.util.IncludeTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		return super.doStartTag();
+	}
+
+	public String getCssClass() {
+		return _cssClass;
+	}
+
+	public String getId() {
+		return _id;
+	}
+
+	public void setCssClass(String cssClass) {
+		_cssClass = cssClass;
+	}
+
+	public void setId(String id) {
+		_id = id;
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_cssClass = null;
+		_id = null;
+	}
+
+	@Override
+	protected String getStartPage() {
+		return _START_PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute("aui:button-row:cssClass", _cssClass);
+		request.setAttribute("aui:button-row:id", _id);
+	}
+
+	protected static final String _ATTRIBUTE_NAMESPACE = "aui:button-row:";
+
+	private static final String _START_PAGE =
+		"/html/taglib/aui/button_row/start.jsp";
+
+	private String _cssClass = null;
+	private String _id = null;
+
+}
