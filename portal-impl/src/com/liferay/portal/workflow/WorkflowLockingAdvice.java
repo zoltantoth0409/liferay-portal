aba9f47a65128cba432caa95afb0e7287a0911a9
tina.tian <tina.tian@liferay.com>
2018-03-23 14:41:31 -0700

    LPS-79121 Remove WorkflowLockingAdvice spring aop config, wrap it as a proxy manually instead

--- portal-impl/src/com/liferay/portal/workflow/WorkflowLockingAdvice.java
+++ portal-impl/src/com/liferay/portal/workflow/WorkflowLockingAdvice.java
@@ -16,14 +16,17 @@ package com.liferay.portal.workflow;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.lock.LockManagerUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.workflow.WorkflowDefinition;
+import com.liferay.portal.kernel.workflow.WorkflowDefinitionManager;
 import com.liferay.portal.kernel.workflow.WorkflowException;
+import com.liferay.portal.kernel.workflow.WorkflowInstanceManager;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
 /**
  * @author Shuyang Zhou
@@ -31,67 +34,26 @@ import org.aspectj.lang.Signature;
  */
 public class WorkflowLockingAdvice {
 
-	public Object invoke(ProceedingJoinPoint proceedingJoinPoint)
-		throws Throwable {
+	public static WorkflowDefinitionManager
+		createWorkflowDefinitionManagerProxy(
+			WorkflowDefinitionManager workflowDefinitionManager) {
 
-		Signature signature = proceedingJoinPoint.getSignature();
-
-		String methodName = signature.getName();
-
-		Object[] arguments = proceedingJoinPoint.getArgs();
-
-		if (methodName.equals(_START_WORKFLOW_INSTANCE_METHOD_NAME)) {
-			String workflowDefinitionName = (String)arguments[3];
-			Integer workflowDefinitionVersion = (Integer)arguments[4];
-
-			String className = WorkflowDefinition.class.getName();
-			String key = _encodeKey(
-				workflowDefinitionName, workflowDefinitionVersion);
-
-			if (LockManagerUtil.isLocked(className, key)) {
-				throw new WorkflowException(
-					StringBundler.concat(
-						"Workflow definition name ", workflowDefinitionName,
-						" and version ",
-						String.valueOf(workflowDefinitionVersion),
-						" is being undeployed"));
-			}
-
-			return proceedingJoinPoint.proceed();
-		}
-		else if (!methodName.equals(
-					_UNDEPLOY_WORKFLOW_DEFINITION_METHOD_NAME)) {
-
-			return proceedingJoinPoint.proceed();
-		}
-
-		long userId = (Long)arguments[1];
-		String name = (String)arguments[2];
-		Integer version = (Integer)arguments[3];
-
-		String className = WorkflowDefinition.class.getName();
-		String key = _encodeKey(name, version);
-
-		if (LockManagerUtil.isLocked(className, key)) {
-			throw new WorkflowException(
-				StringBundler.concat(
-					"Workflow definition name ", name, " and version ",
-					String.valueOf(version), " is being undeployed"));
-		}
-
-		try {
-			LockManagerUtil.lock(
-				userId, className, key, String.valueOf(userId), false,
-				Time.HOUR);
-
-			return proceedingJoinPoint.proceed();
-		}
-		finally {
-			LockManagerUtil.unlock(className, key);
-		}
+		return (WorkflowDefinitionManager)ProxyUtil.newProxyInstance(
+			WorkflowLockingAdvice.class.getClassLoader(),
+			new Class<?>[] {WorkflowDefinitionManager.class},
+			new WorkflowLockingInvocationHandler(workflowDefinitionManager));
 	}
 
-	private String _encodeKey(String name, int version) {
+	public static WorkflowInstanceManager createWorkflowInstanceManagerProxy(
+		WorkflowInstanceManager workflowInstanceManager) {
+
+		return (WorkflowInstanceManager)ProxyUtil.newProxyInstance(
+			WorkflowLockingAdvice.class.getClassLoader(),
+			new Class<?>[] {WorkflowInstanceManager.class},
+			new WorkflowLockingInvocationHandler(workflowInstanceManager));
+	}
+
+	private static String _encodeKey(String name, int version) {
 		return name.concat(StringPool.POUND).concat(
 			StringUtil.toHexString(version));
 	}
@@ -101,5 +63,73 @@ public class WorkflowLockingAdvice {
 
 	private static final String _UNDEPLOY_WORKFLOW_DEFINITION_METHOD_NAME =
 		"undeployWorkflowDefinition";
+
+	private static class WorkflowLockingInvocationHandler
+		implements InvocationHandler {
+
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] arguments)
+			throws Throwable {
+
+			String methodName = method.getName();
+
+			if (methodName.equals(_START_WORKFLOW_INSTANCE_METHOD_NAME)) {
+				String workflowDefinitionName = (String)arguments[3];
+				Integer workflowDefinitionVersion = (Integer)arguments[4];
+
+				String className = WorkflowDefinition.class.getName();
+				String key = _encodeKey(
+					workflowDefinitionName, workflowDefinitionVersion);
+
+				if (LockManagerUtil.isLocked(className, key)) {
+					throw new WorkflowException(
+						StringBundler.concat(
+							"Workflow definition name ", workflowDefinitionName,
+							" and version ",
+							String.valueOf(workflowDefinitionVersion),
+							" is being undeployed"));
+				}
+
+				return method.invoke(_targetObject, arguments);
+			}
+			else if (!methodName.equals(
+						_UNDEPLOY_WORKFLOW_DEFINITION_METHOD_NAME)) {
+
+				return method.invoke(_targetObject, arguments);
+			}
+
+			long userId = (Long)arguments[1];
+			String name = (String)arguments[2];
+			Integer version = (Integer)arguments[3];
+
+			String className = WorkflowDefinition.class.getName();
+			String key = _encodeKey(name, version);
+
+			if (LockManagerUtil.isLocked(className, key)) {
+				throw new WorkflowException(
+					StringBundler.concat(
+						"Workflow definition name ", name, " and version ",
+						String.valueOf(version), " is being undeployed"));
+			}
+
+			try {
+				LockManagerUtil.lock(
+					userId, className, key, String.valueOf(userId), false,
+					Time.HOUR);
+
+				return method.invoke(_targetObject, arguments);
+			}
+			finally {
+				LockManagerUtil.unlock(className, key);
+			}
+		}
+
+		private WorkflowLockingInvocationHandler(Object targetObject) {
+			_targetObject = targetObject;
+		}
+
+		private final Object _targetObject;
+
+	}
 
 }
