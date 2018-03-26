c709d27d41a78d5e2f03a0b69baf84b4e117c41b
tomwang2011 <tong.wang@liferay.com>
2018-03-26 13:40:09 -0700

    LPS-79108 Update schema version

--- modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/portlet/AMPortlet.java
+++ modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/portlet/AMPortlet.java
@@ -52,7 +52,7 @@ import org.osgi.service.component.annotations.Reference;
 public class AMPortlet extends MVCPortlet {
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.adaptive.media.web)(release.schema.version=1.0.0))",
+		target = "(&(release.bundle.symbolic.name=com.liferay.adaptive.media.web)(release.schema.version=1.0.1))",
 		unbind = "-"
 	)
 	protected void setRelease(Release release) {
