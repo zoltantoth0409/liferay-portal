dc7e6eb9b50926f2b911e364b088ba8903c20b2c
Marco Leo <marco.leo@liferay.com>
2018-03-26 12:46:51 +0200

    COMMERCE-354 Use iso code as key

--- modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/starter/ItalyCommerceRegionsStarter.java
+++ modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/starter/ItalyCommerceRegionsStarter.java
@@ -33,7 +33,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	immediate = true,
-	property = "commerce.region.starter.key=italyRegionsStarter",
+	property = "commerce.region.starter.key=" + ItalyCommerceRegionsStarter.ITALY_NUMERIC_ISO_CODE,
 	service = CommerceRegionsStarter.class
 )
 public class ItalyCommerceRegionsStarter implements CommerceRegionsStarter {
