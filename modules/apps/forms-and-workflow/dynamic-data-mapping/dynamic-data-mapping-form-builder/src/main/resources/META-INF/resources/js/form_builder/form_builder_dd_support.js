e2d74074798e225f3d6d0e3f1adb0bbaf87bcb1b
Rafael Praxedes <rafael.praxedes@liferay.com>
2018-03-26 14:55:27 -0300

    LPS-78836 Rename var

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder/form_builder_dd_support.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder/form_builder_dd_support.js
@@ -374,15 +374,15 @@ AUI.add(
 				var instance = this;
 
 				if (activeLayout.get('rows').length == 1 && instance._verifyEmptyForm(activeLayout.get('rows')[0]) && !A.one('#lfr-initial-col-message')) {
-					var colNode = activeLayout.get('rows')[0].get('cols')[0].get('node');
+					var columnNode = activeLayout.get('rows')[0].get('cols')[0].get('node');
 					var columnMessageNode = A.Node.create('<div/>');
 
 					columnMessageNode.text(Liferay.Language.get('drag-from-sidebar-and-drop-here'));
 					columnMessageNode.setAttribute('id', 'lfr-initial-col-message');
 
-					colNode.get('parentNode').all('.layout-builder-add-col-draggable').remove();
-					colNode.addClass('lfr-initial-col');
-					colNode.append(columnMessageNode);
+					columnNode.get('parentNode').all('.layout-builder-add-col-draggable').remove();
+					columnNode.addClass('lfr-initial-col');
+					columnNode.append(columnMessageNode);
 				}
 			},
 
