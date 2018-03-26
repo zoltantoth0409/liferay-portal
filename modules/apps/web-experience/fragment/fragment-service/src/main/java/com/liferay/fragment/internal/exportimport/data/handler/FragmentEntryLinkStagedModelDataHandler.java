3396c71eb8b7411e0a0fcb4a20ff3ab28c3a6be9
pavel-savinov <pavel.savinov@liferay.com>
2018-03-26 16:51:09 +0200

    LPS-79619 Add data handler and staged model repository for the Fragment Entry Link entity

---
+++ modules/apps/web-experience/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/exportimport/data/handler/FragmentEntryLinkStagedModelDataHandler.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.internal.exportimport.data.handler;
+
+import com.liferay.exportimport.data.handler.base.BaseStagedModelDataHandler;
+import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.xml.Element;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(immediate = true, service = StagedModelDataHandler.class)
+public class FragmentEntryLinkStagedModelDataHandler
+	extends BaseStagedModelDataHandler<FragmentEntryLink> {
+
+	public static final String[] CLASS_NAMES =
+		{FragmentEntryLink.class.getName()};
+
+	@Override
+	public String[] getClassNames() {
+		return CLASS_NAMES;
+	}
+
+	@Override
+	protected void doExportStagedModel(
+			PortletDataContext portletDataContext,
+			FragmentEntryLink fragmentEntryLink)
+		throws Exception {
+
+		FragmentEntryLink originalFragmentEntryLink =
+			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+				fragmentEntryLink.getOriginalFragmentEntryLinkId());
+
+		if (originalFragmentEntryLink != null) {
+			StagedModelDataHandlerUtil.exportReferenceStagedModel(
+				portletDataContext, fragmentEntryLink,
+				originalFragmentEntryLink,
+				PortletDataContext.REFERENCE_TYPE_PARENT);
+		}
+
+		Element fragmentEntryLinkElement =
+			portletDataContext.getExportDataElement(fragmentEntryLink);
+
+		portletDataContext.addClassedModel(
+			fragmentEntryLinkElement,
+			ExportImportPathUtil.getModelPath(fragmentEntryLink),
+			fragmentEntryLink);
+	}
+
+	@Override
+	protected void doImportMissingReference(
+			PortletDataContext portletDataContext, String uuid, long groupId,
+			long fragmentEntryLinkId)
+		throws Exception {
+
+		FragmentEntryLink existingFragmentEntryLink = fetchMissingReference(
+			uuid, groupId);
+
+		if (existingFragmentEntryLink == null) {
+			return;
+		}
+
+		Map<Long, Long> fragmentEntryLinkIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				FragmentEntryLink.class);
+
+		fragmentEntryLinkIds.put(
+			fragmentEntryLinkId,
+			existingFragmentEntryLink.getFragmentEntryLinkId());
+	}
+
+	@Override
+	protected void doImportStagedModel(
+			PortletDataContext portletDataContext,
+			FragmentEntryLink fragmentEntryLink)
+		throws Exception {
+
+		Map<Long, Long> originalFragmentEntryLinkIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				FragmentEntryLink.class);
+
+		long originalFragmentEntryLinkId = MapUtil.getLong(
+			originalFragmentEntryLinkIds,
+			fragmentEntryLink.getOriginalFragmentEntryLinkId(),
+			fragmentEntryLink.getOriginalFragmentEntryLinkId());
+
+		FragmentEntryLink importedFragmentEntryLink =
+			(FragmentEntryLink)fragmentEntryLink.clone();
+
+		importedFragmentEntryLink.setGroupId(
+			portletDataContext.getScopeGroupId());
+		importedFragmentEntryLink.setOriginalFragmentEntryLinkId(
+			originalFragmentEntryLinkId);
+
+		FragmentEntryLink existingFragmentEntryLink =
+			_stagedModelRepository.fetchStagedModelByUuidAndGroupId(
+				fragmentEntryLink.getUuid(),
+				portletDataContext.getScopeGroupId());
+
+		if ((existingFragmentEntryLink == null) ||
+			!portletDataContext.isDataStrategyMirror()) {
+
+			importedFragmentEntryLink = _stagedModelRepository.addStagedModel(
+				portletDataContext, importedFragmentEntryLink);
+		}
+		else {
+			importedFragmentEntryLink.setFragmentEntryLinkId(
+				existingFragmentEntryLink.getFragmentEntryLinkId());
+
+			importedFragmentEntryLink =
+				_stagedModelRepository.updateStagedModel(
+					portletDataContext, importedFragmentEntryLink);
+		}
+
+		portletDataContext.importClassedModel(
+			fragmentEntryLink, importedFragmentEntryLink);
+	}
+
+	@Override
+	protected StagedModelRepository<FragmentEntryLink>
+		getStagedModelRepository() {
+
+		return _stagedModelRepository;
+	}
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.fragment.model.FragmentEntryLink)",
+		unbind = "-"
+	)
+	private StagedModelRepository<FragmentEntryLink> _stagedModelRepository;
+
+}
---
+++ modules/apps/web-experience/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/exportimport/staged/model/repository/FragmentEntryLinkStagedModelRepository.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.internal.exportimport.staged.model.repository;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.StagedModelModifiedDateComparator;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepositoryHelper;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.fragment.model.FragmentEntryLink",
+	service = StagedModelRepository.class
+)
+public class FragmentEntryLinkStagedModelRepository
+	implements StagedModelRepository<FragmentEntryLink> {
+
+	@Override
+	public FragmentEntryLink addStagedModel(
+			PortletDataContext portletDataContext,
+			FragmentEntryLink fragmentEntryLink)
+		throws PortalException {
+
+		long userId = portletDataContext.getUserId(
+			fragmentEntryLink.getUserUuid());
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			fragmentEntryLink);
+
+		if (portletDataContext.isDataStrategyMirror()) {
+			serviceContext.setUuid(fragmentEntryLink.getUuid());
+		}
+
+		return _fragmentEntryLinkLocalService.addFragmentEntryLink(
+			userId, fragmentEntryLink.getGroupId(),
+			fragmentEntryLink.getOriginalFragmentEntryLinkId(),
+			fragmentEntryLink.getFragmentEntryId(),
+			fragmentEntryLink.getClassNameId(), fragmentEntryLink.getClassPK(),
+			fragmentEntryLink.getCss(), fragmentEntryLink.getHtml(),
+			fragmentEntryLink.getJs(), fragmentEntryLink.getEditableValues(),
+			fragmentEntryLink.getPosition(), serviceContext);
+	}
+
+	@Override
+	public void deleteStagedModel(FragmentEntryLink fragmentEntryLink)
+		throws PortalException {
+
+		_fragmentEntryLinkLocalService.deleteFragmentEntryLink(
+			fragmentEntryLink);
+	}
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException {
+
+		FragmentEntryLink fragmentEntry = fetchStagedModelByUuidAndGroupId(
+			uuid, groupId);
+
+		if (fragmentEntry != null) {
+			deleteStagedModel(fragmentEntry);
+		}
+	}
+
+	@Override
+	public void deleteStagedModels(PortletDataContext portletDataContext)
+		throws PortalException {
+
+		_fragmentEntryLinkLocalService.deleteFragmentEntryLinks(
+			portletDataContext.getScopeGroupId());
+	}
+
+	@Override
+	public FragmentEntryLink fetchMissingReference(String uuid, long groupId) {
+		return (FragmentEntryLink)_stagedModelRepositoryHelper.
+			fetchMissingReference(uuid, groupId, this);
+	}
+
+	@Override
+	public FragmentEntryLink fetchStagedModelByUuidAndGroupId(
+		String uuid, long groupId) {
+
+		return _fragmentEntryLinkLocalService.
+			fetchFragmentEntryLinkByUuidAndGroupId(uuid, groupId);
+	}
+
+	@Override
+	public List<FragmentEntryLink> fetchStagedModelsByUuidAndCompanyId(
+		String uuid, long companyId) {
+
+		return _fragmentEntryLinkLocalService.
+			getFragmentEntryLinksByUuidAndCompanyId(
+				uuid, companyId, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				new StagedModelModifiedDateComparator<FragmentEntryLink>());
+	}
+
+	@Override
+	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
+		PortletDataContext portletDataContext) {
+
+		return _fragmentEntryLinkLocalService.getExportActionableDynamicQuery(
+			portletDataContext);
+	}
+
+	@Override
+	public FragmentEntryLink saveStagedModel(
+			FragmentEntryLink fragmentEntryLink)
+		throws PortalException {
+
+		return _fragmentEntryLinkLocalService.updateFragmentEntryLink(
+			fragmentEntryLink);
+	}
+
+	@Override
+	public FragmentEntryLink updateStagedModel(
+			PortletDataContext portletDataContext,
+			FragmentEntryLink fragmentEntryLink)
+		throws PortalException {
+
+		long userId = portletDataContext.getUserId(
+			fragmentEntryLink.getUserUuid());
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			fragmentEntryLink);
+
+		return _fragmentEntryLinkLocalService.updateFragmentEntryLink(
+			userId, fragmentEntryLink.getFragmentEntryLinkId(),
+			fragmentEntryLink.getOriginalFragmentEntryLinkId(),
+			fragmentEntryLink.getFragmentEntryId(),
+			fragmentEntryLink.getClassNameId(), fragmentEntryLink.getClassPK(),
+			fragmentEntryLink.getCss(), fragmentEntryLink.getHtml(),
+			fragmentEntryLink.getJs(), fragmentEntryLink.getEditableValues(),
+			fragmentEntryLink.getPosition(), serviceContext);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		FragmentEntryLinkStagedModelRepository.class);
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Reference
+	private StagedModelRepositoryHelper _stagedModelRepositoryHelper;
+
+}
