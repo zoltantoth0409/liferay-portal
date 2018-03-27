a81111ef290004063b505fdc8cad0e28913b52b1
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-03-27 09:12:13 +0200

    LPS-79189 Check for all required attributes of the element

--- modules/apps/web-experience/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/EditableFragmentEntryProcessor.java
+++ modules/apps/web-experience/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/EditableFragmentEntryProcessor.java
@@ -18,6 +18,7 @@ import com.liferay.fragment.entry.processor.editable.parser.EditableElementParse
 import com.liferay.fragment.exception.FragmentEntryContentException;
 import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.processor.FragmentEntryProcessor;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -160,19 +161,25 @@ public class EditableFragmentEntryProcessor implements FragmentEntryProcessor {
 		Document document = _getDocument(html);
 
 		for (Element element : document.getElementsByTag("lfr-editable")) {
-			if (element.hasAttr("id")) {
-				continue;
+			for (String attribute : _REQUIRED_ATTRIBUTES) {
+				if (element.hasAttr(attribute)) {
+					continue;
+				}
+
+				ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+					"content.Language", getClass());
+
+				throw new FragmentEntryContentException(
+					LanguageUtil.format(
+						resourceBundle,
+						"you-must-define-all-require-attributes-x-for-each-" +
+							"editable-element",
+						String.join(StringPool.COMMA, _REQUIRED_ATTRIBUTES)));
 			}
-
-			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-				"content.Language", getClass());
-
-			throw new FragmentEntryContentException(
-				LanguageUtil.get(
-					resourceBundle,
-					"you-must-define-an-unique-id-for-each-editable-element"));
 		}
 	}
+
+	private static final String[] _REQUIRED_ATTRIBUTES = {"id", "type"};
 
 	private final Map<String, EditableElementParser> _editableElementParsers =
 		new HashMap<>();
