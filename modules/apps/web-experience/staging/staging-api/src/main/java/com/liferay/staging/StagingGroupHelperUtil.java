2368bc99ef3d2d32ef15e0c3c430e7aa6fde3517
Zoltán Császi <zoltan.csaszi@liferay.com>
2018-03-27 15:49:07 +0200

    LPS-79257 Adding StagingGroupHelperUtil for devs to easily access features

---
+++ modules/apps/web-experience/staging/staging-api/src/main/java/com/liferay/staging/StagingGroupHelperUtil.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging;
+
+import aQute.bnd.annotation.ProviderType;
+
+/**
+ * @author Mate Thurzo
+ */
+@ProviderType
+public class StagingGroupHelperUtil {
+
+	public static StagingGroupHelper getStagingGroupHelper() {
+		if (_stagingGroupHelper != null) {
+			return _stagingGroupHelper;
+		}
+
+		throw new NullPointerException("StagingGroupHelper is null");
+	}
+
+	public static void setStagingGroupHelper(
+		StagingGroupHelper stagingGroupHelper) {
+
+		if (_stagingGroupHelper != null) {
+			stagingGroupHelper = _stagingGroupHelper;
+
+			return;
+		}
+
+		_stagingGroupHelper = stagingGroupHelper;
+	}
+
+	private static StagingGroupHelper _stagingGroupHelper;
+
+}
