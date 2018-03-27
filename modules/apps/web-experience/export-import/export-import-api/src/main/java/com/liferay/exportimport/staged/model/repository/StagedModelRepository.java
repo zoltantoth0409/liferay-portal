9eb610fe2b6786106919e1bcfdd001955e55fe3f
akosthurzo <akos.thurzo@liferay.com>
2018-03-27 15:07:19 +0200

    LRDOCS-5102 doc

--- modules/apps/web-experience/export-import/export-import-api/src/main/java/com/liferay/exportimport/staged/model/repository/StagedModelRepository.java
+++ modules/apps/web-experience/export-import/export-import-api/src/main/java/com/liferay/exportimport/staged/model/repository/StagedModelRepository.java
@@ -25,8 +25,32 @@ import java.util.List;
 /**
  * @author Daniel Kocsis
  */
+
+/**
+ * StagedModelRepository provides an interface for CRUD operations for a given staged model.
+ * The goal is simplify the StagedModelDataHandlers by removing these CRUD operations and the code logic related to these operations.
+ *
+ * The get and fetch methods are typically used during an export process to query/find staged models.
+ *
+ * The staged model parameter can be either
+ * - the old staged model that was serialized at export time and deserialized at import time, meaning that it is more like a POJO and its key attributes (primary key and foreign keys) should not be used during the CRUD operations (more on that later)
+ * - or the new staged model that is from the portal that is being called right now, meaning that the staged model can be used as a regular entity.
+ * @param <T> is the type of the staged model on which the StagedModelRepository operates.
+ */
 public interface StagedModelRepository<T extends StagedModel> {
 
+	/**
+	 * Persists a new staged model.
+	 * @param portletDataContext
+	 * @param stagedModel is the old staged model which is being copied.
+	 *                    Primary Key attribute should not be used.
+	 *                    Foreign Keys should be set before calling this method.
+	 *                    Example: a BookmarksEntry is being imported. It has a groupId and a folderId as foreign keys which are required by the local service.
+	 *                    These keys should be set on the stagedModel before calling the the addStagedModel method because addStagedModel will forward these values to the service method like this:
+	 *                    _bookmarksEntryLocalService.addEntry(userId, bookmarksEntry.getGroupId(), bookmarksEntry.getFolderId(), bookmarksEntry.getName(), bookmarksEntry.getUrl(), bookmarksEntry.getDescription(), serviceContext);
+	 * @return
+	 * @throws PortalException
+	 */
 	public T addStagedModel(
 			PortletDataContext portletDataContext, T stagedModel)
 		throws PortalException;
@@ -37,9 +61,20 @@ public interface StagedModelRepository<T extends StagedModel> {
 
 	public void deleteStagedModel(T stagedModel) throws PortalException;
 
+	/**
+	 * Deletes all the staged models this repository handles. Usually deletes the data from the scope group of portlet data context.
+	 * @param portletDataContext
+	 * @throws PortalException
+	 */
 	public void deleteStagedModels(PortletDataContext portletDataContext)
 		throws PortalException;
 
+	/**
+	 * If possible, use return (T)_stagedModelRepositoryHelper.fetchMissingReference(uuid, groupId, this); as the implementation.
+	 * @param uuid
+	 * @param groupId
+	 * @return
+	 */
 	public default T fetchMissingReference(String uuid, long groupId) {
 		return null;
 	}
@@ -49,9 +84,19 @@ public interface StagedModelRepository<T extends StagedModel> {
 	public List<T> fetchStagedModelsByUuidAndCompanyId(
 		String uuid, long companyId);
 
+	/**
+	 * If possible, use return _yourEntityLocalService.getExportActionableDynamicQuery(portletDataContext); as the implementation.
+	 * @param portletDataContext
+	 * @return
+	 */
 	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
 		PortletDataContext portletDataContext);
 
+	/**
+	 * @param portletDataContext
+	 * @param stagedModel is the old staged model which is being copied.
+	 * @throws PortletDataException
+	 */
 	public default void restoreStagedModel(
 			PortletDataContext portletDataContext, T stagedModel)
 		throws PortletDataException {
@@ -59,6 +104,12 @@ public interface StagedModelRepository<T extends StagedModel> {
 
 	public T saveStagedModel(T stagedModel) throws PortalException;
 
+	/**
+	 * @param portletDataContext
+	 * @param stagedModel is the new staged model that is from the portal being called right now.
+	 * @return
+	 * @throws PortalException
+	 */
 	public T updateStagedModel(
 			PortletDataContext portletDataContext, T stagedModel)
 		throws PortalException;
