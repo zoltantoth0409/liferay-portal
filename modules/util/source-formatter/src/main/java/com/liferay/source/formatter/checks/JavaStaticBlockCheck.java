0ee9787fd7318d40d896499b8b703b03d668d9b7
Peter Shin <peter.shin@liferay.com>
2018-03-29 16:01:08 -0700

    LPS-79282

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaStaticBlockCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaStaticBlockCheck.java
@@ -17,6 +17,7 @@ package com.liferay.source.formatter.checks;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaMethod;
 import com.liferay.source.formatter.parser.JavaStaticBlock;
 import com.liferay.source.formatter.parser.JavaTerm;
 import com.liferay.source.formatter.parser.JavaVariable;
@@ -123,6 +124,19 @@ public class JavaStaticBlockCheck extends BaseJavaTermCheck {
 	private JavaTerm _getLastReferencedJavaTerm(
 		String staticBlockContent, List<JavaTerm> childJavaTerms) {
 
+		boolean javaMethodCall = false;
+
+		for (int i = childJavaTerms.size() - 1; i >= 0; i--) {
+			JavaTerm javaTerm = childJavaTerms.get(i);
+
+			if (javaTerm.isStatic() && (javaTerm instanceof JavaMethod) &&
+				staticBlockContent.matches(
+					"[\\s\\S]*\\s" + javaTerm.getName() + "\\([\\s\\S]*")) {
+
+				javaMethodCall = true;
+			}
+		}
+
 		for (int i = childJavaTerms.size() - 1; i >= 0; i--) {
 			JavaTerm javaTerm = childJavaTerms.get(i);
 
@@ -132,7 +146,11 @@ public class JavaStaticBlockCheck extends BaseJavaTermCheck {
 				staticBlockContent.matches(
 					"[\\s\\S]*\\W" + javaTerm.getName() + "\\W[\\s\\S]*")) {
 
-				return javaTerm;
+				if ((javaTerm instanceof JavaClass) ||
+					(javaTerm instanceof JavaVariable) && !javaMethodCall) {
+
+					return javaTerm;
+				}
 			}
 		}
 
