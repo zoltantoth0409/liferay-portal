d703dc3fd534d0dc29f049cf23569a584312546a
Joshua Cords <joshua.cords@liferay.com>
2018-04-02 14:49:26 -0700

    LPS-79311 Change WARN to ERROR, SF

--- modules/apps/static/portal-osgi-web/portal-osgi-web-wab-extender/src/main/java/com/liferay/portal/osgi/web/wab/extender/internal/event/EventUtil.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-wab-extender/src/main/java/com/liferay/portal/osgi/web/wab/extender/internal/event/EventUtil.java
@@ -129,10 +129,11 @@ public class EventUtil
 
 				properties.put("collision.bundles", collidedBundleIds);
 
-				_log.warn("Bundles " + collidedBundleNames +
-				" have the same Web-ContextPath. This can lead to" +
-				" unexpected behavior when the bundles are deployed" +
-				" to the same layout");
+				_log.error(
+					"Bundles " + collidedBundleNames +
+						" have the same Web-ContextPath. This can lead to " +
+							"unexpected behavior when the bundles are " +
+								"deployed to the same layout");
 			}
 		}
 
