4cdba1ae8517c016f470213c200d23b577351a52
akosthurzo <akos.thurzo@liferay.com>
2018-04-03 12:52:51 +0200

    LRDOCS-5102 more doc

--- modules/apps/web-experience/export-import/export-import-api/src/main/java/com/liferay/exportimport/staged/model/repository/StagedModelRepository.java
+++ modules/apps/web-experience/export-import/export-import-api/src/main/java/com/liferay/exportimport/staged/model/repository/StagedModelRepository.java
@@ -48,7 +48,7 @@ public interface StagedModelRepository<T extends StagedModel> {
 	 *                    Example: a BookmarksEntry is being imported. It has a groupId and a folderId as foreign keys which are required by the local service.
 	 *                    These keys should be set on the stagedModel before calling the the addStagedModel method because addStagedModel will forward these values to the service method like this:
 	 *                    _bookmarksEntryLocalService.addEntry(userId, bookmarksEntry.getGroupId(), bookmarksEntry.getFolderId(), bookmarksEntry.getName(), bookmarksEntry.getUrl(), bookmarksEntry.getDescription(), serviceContext);
-	 * @return
+	 * @return returns the newly persisted staged model.
 	 * @throws PortalException
 	 */
 	public T addStagedModel(
@@ -70,10 +70,13 @@ public interface StagedModelRepository<T extends StagedModel> {
 		throws PortalException;
 
 	/**
+	 * fetchMissingReference is used when the model (that is handled by this repository) needs to be fetched as a missing reference.
 	 * If possible, use return (T)_stagedModelRepositoryHelper.fetchMissingReference(uuid, groupId, this); as the implementation.
+	 * It is not just a regular fetchStagedModelByUuidAndGroupId (although the paramaters are the same), but it has a set of rules where to find a missing reference
+	 * (e.g. first in the importing site, then in its parent sites, then in the global site)
 	 * @param uuid
-	 * @param groupId
-	 * @return
+	 * @param groupId of the importing group
+	 * @return returns the missing reference entity if it is found or null if it is not found
 	 */
 	public default T fetchMissingReference(String uuid, long groupId) {
 		return null;
@@ -85,16 +88,18 @@ public interface StagedModelRepository<T extends StagedModel> {
 		String uuid, long companyId);
 
 	/**
+	 * getExportActionableDynamicQuery is a wrapper for the same method in the service builder generated local service.
 	 * If possible, use return _yourEntityLocalService.getExportActionableDynamicQuery(portletDataContext); as the implementation.
 	 * @param portletDataContext
-	 * @return
+	 * @return returns an ActionableDynamicQuery which queries the entities from the database based on modified date, status, etc. depending on the parameters in portletDataContext.
+	 * The performActionMethod of the ActionableDynamicQuery is an API call (StagedModelDataHandlerUtil.exportStagedModel()) to export each entity that is returned.
 	 */
 	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
 		PortletDataContext portletDataContext);
 
 	/**
 	 * @param portletDataContext
-	 * @param stagedModel is the old staged model which is being copied.
+	 * @param stagedModel is the old staged model. It needs to be fetched from the current portal and restored from trash.
 	 * @throws PortletDataException
 	 */
 	public default void restoreStagedModel(
@@ -107,7 +112,7 @@ public interface StagedModelRepository<T extends StagedModel> {
 	/**
 	 * @param portletDataContext
 	 * @param stagedModel is the new staged model that is from the portal being called right now.
-	 * @return
+	 * @return returns the updated staged model
 	 * @throws PortalException
 	 */
 	public T updateStagedModel(
