9f8b0fa85d9fc218594fd32a01a11e8345236c88
yichenroy <yi-chen.tsai@liferay.com>
2018-04-03 17:18:48 -0700

    LRQA-39804 Get relevant PQL for functional tests

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/FunctionalBatchTestClassGroup.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/FunctionalBatchTestClassGroup.java
@@ -14,16 +14,81 @@
 
 package com.liferay.jenkins.results.parser;
 
+import java.io.File;
+import java.io.IOException;
+
+import java.util.List;
+import java.util.Properties;
+
 /**
  * @author Yi-Chen Tsai
  */
 public class FunctionalBatchTestClassGroup extends BatchTestClassGroup {
+
+	public String getRelevantFunctionalTestQuery() {
+		return _relevantTestBatchRunPropertyQuery;
+	}
 
 	protected FunctionalBatchTestClassGroup(
 		String batchName, GitWorkingDirectory gitWorkingDirectory,
 		String testSuiteName) {
 
 		super(batchName, gitWorkingDirectory, testSuiteName);
+
+		_setRelevantTestbatchRunpropertyQuery();
 	}
+
+	private void _setRelevantTestbatchRunpropertyQuery() {
+		PortalGitWorkingDirectory portalGitWorkingDirectory =
+			(PortalGitWorkingDirectory)gitWorkingDirectory;
+
+		List<File> modifiedModuleDirs = null;
+
+		try {
+			modifiedModuleDirs =
+				portalGitWorkingDirectory.getModifiedModuleDirsList();
+		}
+		catch (IOException ioe) {
+			File workingDirectory = gitWorkingDirectory.getWorkingDirectory();
+
+			throw new RuntimeException(
+				JenkinsResultsParserUtil.combine(
+					"Unable to get module directories in ",
+					workingDirectory.getPath()),
+				ioe);
+		}
+
+		for (File modifiedModuleDir : modifiedModuleDirs) {
+			File modifiedModuleTestProperties = new File(
+				modifiedModuleDir, "test.properties");
+
+			if (!modifiedModuleTestProperties.exists()) {
+				continue;
+			}
+
+			Properties testProperties = JenkinsResultsParserUtil.getProperties(
+				modifiedModuleTestProperties);
+
+			String testBatchRunProperty = testProperties.getProperty(
+				JenkinsResultsParserUtil.combine(
+					"test.batch.run.property.query[", batchName, "][",
+					testSuiteName, "]"));
+
+			if (testBatchRunProperty != null) {
+				if (_relevantTestBatchRunPropertyQuery == null) {
+					_relevantTestBatchRunPropertyQuery =
+						JenkinsResultsParserUtil.combine(
+							"(", testBatchRunProperty, ")");
+				}
+				else {
+					_relevantTestBatchRunPropertyQuery +=
+						JenkinsResultsParserUtil.combine(
+							" OR (", testBatchRunProperty, ")");
+				}
+			}
+		}
+	}
+
+	private String _relevantTestBatchRunPropertyQuery;
 
 }
