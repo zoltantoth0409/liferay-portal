acd576dad512e9b857dac2da1d034062719d6662
Carlos Sierra <carlos.sierra@liferay.com>
2018-04-04 12:29:19 +0200

    LPS-79376 Support custom token retriever registration

--- modules/apps/foundation/portal-security-sso/portal-security-sso-token-impl/src/main/java/com/liferay/portal/security/sso/token/internal/auto/login/TokenAutoLogin.java
+++ modules/apps/foundation/portal-security-sso/portal-security-sso-token-impl/src/main/java/com/liferay/portal/security/sso/token/internal/auto/login/TokenAutoLogin.java
@@ -14,6 +14,9 @@
 
 package com.liferay.portal.security.sso.token.internal.auto.login;
 
+import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
@@ -37,18 +40,15 @@ import com.liferay.portal.security.sso.token.security.auth.TokenLocation;
 import com.liferay.portal.security.sso.token.security.auth.TokenRetriever;
 import com.liferay.portal.util.PropsValues;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * Participates in every unauthenticated HTTP request to Liferay Portal.
@@ -67,6 +67,21 @@ import org.osgi.service.component.annotations.ReferencePolicyOption;
 	service = AutoLogin.class
 )
 public class TokenAutoLogin extends BaseAutoLogin {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_tokenRetrievers = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, TokenRetriever.class, null,
+			ServiceReferenceMapperFactory.create(
+				bundleContext,
+				(tokenRetriever, emitter) -> emitter.emit(
+					tokenRetriever.getTokenLocation())));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_tokenRetrievers.close();
+	}
 
 	@Override
 	protected String[] doLogin(
@@ -90,7 +105,8 @@ public class TokenAutoLogin extends BaseAutoLogin {
 		TokenLocation tokenLocation =
 			tokenCompanyServiceSettings.tokenLocation();
 
-		TokenRetriever tokenRetriever = _tokenRetrievers.get(tokenLocation);
+		TokenRetriever tokenRetriever = _tokenRetrievers.getService(
+			tokenLocation);
 
 		if (tokenRetriever == null) {
 			if (_log.isWarnEnabled()) {
@@ -202,15 +218,6 @@ public class TokenAutoLogin extends BaseAutoLogin {
 		_configurationProvider = configurationProvider;
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.AT_LEAST_ONE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void setTokenRetriever(TokenRetriever tokenRetriever) {
-		_tokenRetrievers.put(tokenRetriever.getTokenLocation(), tokenRetriever);
-	}
-
 	@Reference(unbind = "-")
 	protected void setUserImporter(UserImporter userImporter) {
 		_userImporter = userImporter;
@@ -221,10 +228,6 @@ public class TokenAutoLogin extends BaseAutoLogin {
 		_userLocalService = userLocalService;
 	}
 
-	protected void unsetTokenRetriever(TokenRetriever tokenRetriever) {
-		_tokenRetrievers.remove(tokenRetriever.getTokenLocation());
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(TokenAutoLogin.class);
 
 	private ConfigurationProvider _configurationProvider;
@@ -232,8 +235,7 @@ public class TokenAutoLogin extends BaseAutoLogin {
 	@Reference
 	private Portal _portal;
 
-	private final Map<TokenLocation, TokenRetriever> _tokenRetrievers =
-		new ConcurrentHashMap<>();
+	private ServiceTrackerMap<TokenLocation, TokenRetriever> _tokenRetrievers;
 	private UserImporter _userImporter;
 	private UserLocalService _userLocalService;
 
