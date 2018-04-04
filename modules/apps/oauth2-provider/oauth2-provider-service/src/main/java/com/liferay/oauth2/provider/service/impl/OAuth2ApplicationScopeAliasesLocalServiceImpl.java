70a2b33c56c06fe088fb0b69365230f6497ea2ed
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-04-04 16:50:27 +0200

    OAUTH2-119 OAuth2ApplicationScopeAliases local service impl

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationScopeAliasesLocalServiceImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationScopeAliasesLocalServiceImpl.java
@@ -14,29 +14,131 @@
 
 package com.liferay.oauth2.provider.service.impl;
 
+import com.liferay.oauth2.provider.model.OAuth2ApplicationScopeAliases;
+import com.liferay.oauth2.provider.model.OAuth2ScopeGrant;
+import com.liferay.oauth2.provider.scope.liferay.LiferayOAuth2Scope;
+import com.liferay.oauth2.provider.scope.liferay.ScopeLocator;
 import com.liferay.oauth2.provider.service.base.OAuth2ApplicationScopeAliasesLocalServiceBaseImpl;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.osgi.framework.Bundle;
 
 /**
- * The implementation of the o auth2 application scope aliases local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.oauth2.provider.service.OAuth2ApplicationScopeAliasesLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see OAuth2ApplicationScopeAliasesLocalServiceBaseImpl
- * @see com.liferay.oauth2.provider.service.OAuth2ApplicationScopeAliasesLocalServiceUtil
  */
 public class OAuth2ApplicationScopeAliasesLocalServiceImpl
 	extends OAuth2ApplicationScopeAliasesLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.oauth2.provider.service.OAuth2ApplicationScopeAliasesLocalServiceUtil} to access the o auth2 application scope aliases local service.
-	 */
+	@Override
+	public OAuth2ApplicationScopeAliases addOAuth2ApplicationScopeAliases(
+			long companyId, long userId, String userName,
+			long oAuth2ApplicationId, List<String> scopeAliasesList)
+		throws PortalException {
+
+		long oAuth2ApplicationScopeAliasesId = counterLocalService.increment(
+			OAuth2ApplicationScopeAliases.class.getName());
+
+		OAuth2ApplicationScopeAliases oAuth2ApplicationScopeAliases =
+			createOAuth2ApplicationScopeAliases(
+				oAuth2ApplicationScopeAliasesId);
+
+		oAuth2ApplicationScopeAliases.setCompanyId(companyId);
+		oAuth2ApplicationScopeAliases.setUserId(userId);
+		oAuth2ApplicationScopeAliases.setUserName(userName);
+		oAuth2ApplicationScopeAliases.setCreateDate(new Date());
+		oAuth2ApplicationScopeAliases.setOAuth2ApplicationId(
+			oAuth2ApplicationId);
+
+		oAuth2ApplicationScopeAliases.setScopeAliasesList(scopeAliasesList);
+
+		oAuth2ApplicationScopeAliases =
+			oAuth2ApplicationScopeAliasesPersistence.update(
+				oAuth2ApplicationScopeAliases);
+
+		// Scope Grants
+
+		createScopeGrants(
+			companyId, oAuth2ApplicationScopeAliasesId, scopeAliasesList);
+
+		return oAuth2ApplicationScopeAliases;
+	}
+
+	@Override
+	public OAuth2ApplicationScopeAliases deleteOAuth2ApplicationScopeAliases(
+			long oAuth2ApplicationScopeAliasesId)
+		throws PortalException {
+
+		Collection<OAuth2ScopeGrant> oAuth2ScopeGrants =
+			oAuth2ScopeGrantLocalService.getOAuth2ScopeGrants(
+				oAuth2ApplicationScopeAliasesId, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS, null);
+
+		for (OAuth2ScopeGrant oAuth2ScopeGrant : oAuth2ScopeGrants) {
+			oAuth2ScopeGrantLocalService.deleteOAuth2ScopeGrant(
+				oAuth2ScopeGrant.getOAuth2ScopeGrantId());
+		}
+
+		return super.deleteOAuth2ApplicationScopeAliases(
+			oAuth2ApplicationScopeAliasesId);
+	}
+
+	@Override
+	public OAuth2ApplicationScopeAliases fetchOAuth2ApplicationScopeAliases(
+		long oAuth2ApplicationId, List<String> scopeAliasesList) {
+
+		String scopeAliases = StringUtil.merge(
+			scopeAliasesList, StringPool.SPACE);
+
+		return oAuth2ApplicationScopeAliasesPersistence.fetchByO_S(
+			oAuth2ApplicationId, scopeAliases);
+	}
+
+	@Override
+	public List<OAuth2ApplicationScopeAliases>
+		getOAuth2ApplicationScopeAliaseses(
+			long oAuth2ApplicationId, int start, int end,
+			OrderByComparator<OAuth2ApplicationScopeAliases>
+				orderByComparator) {
+
+		return oAuth2ApplicationScopeAliasesPersistence.
+			findByOAuth2ApplicationId(
+				oAuth2ApplicationId, start, end, orderByComparator);
+	}
+
+	protected void createScopeGrants(
+			long companyId, long oAuth2ApplicationScopeAliasesId,
+			List<String> scopeAliasesList)
+		throws PortalException {
+
+		Set<LiferayOAuth2Scope> liferayOAuth2Scopes = new HashSet<>();
+
+		for (String scopeAlias : scopeAliasesList) {
+			liferayOAuth2Scopes.addAll(
+				_scopeLocator.getLiferayOAuth2Scopes(companyId, scopeAlias));
+		}
+
+		for (LiferayOAuth2Scope liferayOAuth2Scope : liferayOAuth2Scopes) {
+			Bundle bundle = liferayOAuth2Scope.getBundle();
+
+			oAuth2ScopeGrantLocalService.createOAuth2ScopeGrant(
+				companyId, oAuth2ApplicationScopeAliasesId,
+				liferayOAuth2Scope.getApplicationName(),
+				bundle.getSymbolicName(), liferayOAuth2Scope.getScope());
+		}
+	}
+
+	@ServiceReference(type = ScopeLocator.class)
+	private ScopeLocator _scopeLocator;
 
 }
