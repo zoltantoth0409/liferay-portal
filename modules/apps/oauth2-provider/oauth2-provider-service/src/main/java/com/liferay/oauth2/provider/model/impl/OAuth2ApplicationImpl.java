d7b1eb7f4bb758cba74b8beb2317d9c9e1543f12
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-04-04 17:05:50 +0200

    OAUTH2-119 OAuth2Application impl

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/model/impl/OAuth2ApplicationImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/model/impl/OAuth2ApplicationImpl.java
@@ -16,13 +16,74 @@ package com.liferay.oauth2.provider.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.oauth2.provider.constants.GrantType;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class OAuth2ApplicationImpl extends OAuth2ApplicationBaseImpl {
 
-	public OAuth2ApplicationImpl() {
+	@Override
+	public List<GrantType> getAllowedGrantTypesList() {
+		Stream<String> stream = Arrays.stream(
+			StringUtil.split(getAllowedGrantTypes()));
+
+		List<GrantType> oAuth2ProviderGrantTypes = stream.map(
+			GrantType::valueOf
+		).collect(
+			Collectors.toList()
+		);
+
+		return oAuth2ProviderGrantTypes;
+	}
+
+	@Override
+	public List<String> getFeaturesList() {
+		return Arrays.asList(StringUtil.split(getFeatures()));
+	}
+
+	@Override
+	public List<String> getRedirectURIsList() {
+		return Arrays.asList(
+			StringUtil.split(getRedirectURIs(), StringPool.NEW_LINE));
+	}
+
+	@Override
+	public void setAllowedGrantTypesList(
+		List<GrantType> allowedGrantTypesList) {
+
+		Stream<GrantType> stream = allowedGrantTypesList.stream();
+
+		String allowedGrantTypes = stream.map(
+			GrantType::toString
+		).collect(
+			Collectors.joining(StringPool.COMMA)
+		);
+
+		setAllowedGrantTypes(allowedGrantTypes);
+	}
+
+	@Override
+	public void setFeaturesList(List<String> featuresList) {
+		String features = StringUtil.merge(featuresList);
+
+		setFeatures(features);
+	}
+
+	@Override
+	public void setRedirectURIsList(List<String> redirectURIsList) {
+		String redirectURIs = StringUtil.merge(
+			redirectURIsList, StringPool.NEW_LINE);
+
+		setRedirectURIs(redirectURIs);
 	}
 
 }
