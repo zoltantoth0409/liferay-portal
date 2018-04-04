a38391e39797dd923cf8607f91568147fbecaf4c
Carlos Lancha <carlos.lancha@liferay.com>
2018-04-04 15:53:16 +0200

    LPS-78357 Create DisplayContext for DL File Entry Type

---
+++ modules/apps/collaboration/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLFileEntryTypeDisplayContext.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.display.context;
+
+import com.liferay.document.library.kernel.service.DLFileEntryTypeServiceUtil;
+import com.liferay.document.library.web.internal.security.permission.resource.DLPermission;
+import com.liferay.portal.kernel.dao.search.DisplayTerms;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Carlos Lancha
+ */
+public class DLFileEntryTypeDisplayContext {
+
+	public DLFileEntryTypeDisplayContext(
+		RenderRequest renderRequest, RenderResponse renderResponse,
+		HttpServletRequest request) {
+
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+		_request = request;
+	}
+
+	public String getCreationURL() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (DLPermission.contains(themeDisplay.getPermissionChecker(),
+			themeDisplay.getScopeGroupId(), ActionKeys.ADD_DOCUMENT_TYPE)) {
+			PortletURL creationURL = _renderResponse.createRenderURL();
+
+			creationURL.setParameter(
+				"mvcPath", "/document_library/edit_file_entry_type.jsp");
+			creationURL.setParameter(
+				"redirect", PortalUtil.getCurrentURL(_request));
+
+			return creationURL.toString();
+		}
+
+		return null;
+	}
+
+	public SearchContainer getSearchContainer() throws PortalException {
+		if (_searchContainer != null) {
+			return _searchContainer;
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PortletURL portletURL = _renderResponse.createRenderURL();
+
+		portletURL.setParameter("mvcRenderCommandName",
+			"/document_library/view_file_entry_types");
+
+		SearchContainer searchContainer = new SearchContainer(_renderRequest, new DisplayTerms(_request),
+			new DisplayTerms(_request), SearchContainer.DEFAULT_CUR_PARAM,
+			SearchContainer.DEFAULT_DELTA, portletURL, null,
+			LanguageUtil.get(_request, "there-are-no-results"));
+
+		DisplayTerms searchTerms = searchContainer.getSearchTerms();
+
+		boolean includeBasicFileEntryType = ParamUtil.getBoolean(_renderRequest, "includeBasicFileEntryType");
+
+		int total = DLFileEntryTypeServiceUtil.searchCount(
+			themeDisplay.getCompanyId(),
+			PortalUtil.getCurrentAndAncestorSiteGroupIds(themeDisplay.getScopeGroupId()),
+			searchTerms.getKeywords(), includeBasicFileEntryType);
+
+		searchContainer.setTotal(total);
+
+		searchContainer.setResults(
+			DLFileEntryTypeServiceUtil.search(
+				themeDisplay.getCompanyId(),
+				PortalUtil.getCurrentAndAncestorSiteGroupIds(themeDisplay.getScopeGroupId()),
+				searchTerms.getKeywords(), includeBasicFileEntryType,
+				searchContainer.getStart(), searchContainer.getEnd(),
+				searchContainer.getOrderByComparator()));
+
+		_searchContainer = searchContainer;
+
+		return _searchContainer;
+	}
+
+	public String getSearchURL() {
+		PortletURL searchURL = _renderResponse.createRenderURL();
+
+		searchURL.setParameter("mvcRenderCommandName", "/document_library/view_file_entry_types");
+
+		return searchURL.toString();
+	}
+
+	public int getTotal() throws PortalException {
+		SearchContainer searchContainer = getSearchContainer();
+
+		return searchContainer.getTotal();
+	}
+
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+	private final HttpServletRequest _request;
+	private SearchContainer _searchContainer;
+
+}
