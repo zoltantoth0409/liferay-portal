7bbd78911e3ea28ea60c3c250dea99657f8f902e
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-04-04 17:20:49 +0200

    OAUTH2-119 OAuth2 remote services impl

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationServiceImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationServiceImpl.java
@@ -14,11 +14,197 @@
 
 package com.liferay.oauth2.provider.service.impl;
 
+import com.liferay.oauth2.provider.constants.GrantType;
+import com.liferay.oauth2.provider.constants.OAuth2ProviderActionKeys;
+import com.liferay.oauth2.provider.model.OAuth2Application;
 import com.liferay.oauth2.provider.service.base.OAuth2ApplicationServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMode;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionHelper;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.io.InputStream;
+
+import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
  */
+@JSONWebService(mode = JSONWebServiceMode.IGNORE)
 public class OAuth2ApplicationServiceImpl
 	extends OAuth2ApplicationServiceBaseImpl {
+
+	@Override
+	public OAuth2Application addOAuth2Application(
+			List<GrantType> allowedGrantTypesList, String clientId,
+			int clientProfile, String clientSecret, String description,
+			List<String> featuresList, String homePageURL, long iconFileEntryId,
+			String name, String privacyPolicyURL, List<String> redirectURIsList,
+			List<String> scopeAliasesList, ServiceContext serviceContext)
+		throws PortalException {
+
+		ModelResourcePermissionHelper.check(
+			_oAuth2ApplicationModelResourcePermission, getPermissionChecker(),
+			0, 0, OAuth2ProviderActionKeys.ACTION_ADD_APPLICATION);
+
+		User user = getUser();
+
+		return oAuth2ApplicationLocalService.addOAuth2Application(
+			user.getCompanyId(), user.getUserId(), user.getFullName(),
+			allowedGrantTypesList, clientId, clientProfile, clientSecret,
+			description, featuresList, homePageURL, iconFileEntryId, name,
+			privacyPolicyURL, redirectURIsList, scopeAliasesList,
+			serviceContext);
+	}
+
+	@Override
+	public OAuth2Application deleteOAuth2Application(long oAuth2ApplicationId)
+		throws PortalException {
+
+		OAuth2Application oAuth2Application =
+			oAuth2ApplicationLocalService.getOAuth2Application(
+				oAuth2ApplicationId);
+
+		_oAuth2ApplicationModelResourcePermission.check(
+			getPermissionChecker(), oAuth2Application, ActionKeys.DELETE);
+
+		oAuth2Application =
+			oAuth2ApplicationLocalService.deleteOAuth2Application(
+				oAuth2ApplicationId);
+
+		return oAuth2Application;
+	}
+
+	@Override
+	public OAuth2Application fetchOAuth2Application(
+			long companyId, String clientId)
+		throws PortalException {
+
+		OAuth2Application oAuth2Application =
+			oAuth2ApplicationLocalService.fetchOAuth2Application(
+				companyId, clientId);
+
+		if (oAuth2Application != null) {
+			_oAuth2ApplicationModelResourcePermission.check(
+				getPermissionChecker(), oAuth2Application, ActionKeys.VIEW);
+		}
+
+		return oAuth2Application;
+	}
+
+	@Override
+	public OAuth2Application getOAuth2Application(long oAuth2ApplicationId)
+		throws PortalException {
+
+		OAuth2Application oAuth2Application =
+			oAuth2ApplicationLocalService.getOAuth2Application(
+				oAuth2ApplicationId);
+
+		_oAuth2ApplicationModelResourcePermission.check(
+			getPermissionChecker(), oAuth2Application, ActionKeys.VIEW);
+
+		return oAuth2Application;
+	}
+
+	@Override
+	public OAuth2Application getOAuth2Application(
+			long companyId, String clientId)
+		throws PortalException {
+
+		OAuth2Application oAuth2Application =
+			oAuth2ApplicationLocalService.getOAuth2Application(
+				companyId, clientId);
+
+		_oAuth2ApplicationModelResourcePermission.check(
+			getPermissionChecker(), oAuth2Application, ActionKeys.VIEW);
+
+		return oAuth2Application;
+	}
+
+	@Override
+	public List<OAuth2Application> getOAuth2Applications(
+		long companyId, int start, int end,
+		OrderByComparator<OAuth2Application> orderByComparator) {
+
+		return oAuth2ApplicationPersistence.filterFindByC(
+			companyId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getOAuth2ApplicationsCount(long companyId) {
+		return oAuth2ApplicationPersistence.filterCountByC(companyId);
+	}
+
+	@Override
+	public OAuth2Application updateIcon(
+			long oAuth2ApplicationId, InputStream inputStream)
+		throws PortalException {
+
+		OAuth2Application oAuth2Application =
+			oAuth2ApplicationLocalService.getOAuth2Application(
+				oAuth2ApplicationId);
+
+		_oAuth2ApplicationModelResourcePermission.check(
+			getPermissionChecker(), oAuth2Application, ActionKeys.UPDATE);
+
+		return oAuth2ApplicationLocalService.updateIcon(
+			oAuth2ApplicationId, inputStream);
+	}
+
+	@Override
+	public OAuth2Application updateOAuth2Application(
+			long oAuth2ApplicationId, List<GrantType> allowedGrantTypesList,
+			String clientId, int clientProfile, String clientSecret,
+			String description, List<String> featuresList, String homePageURL,
+			long iconFileEntryId, String name, String privacyPolicyURL,
+			List<String> redirectURIsList, long auth2ApplicationScopeAliasesId,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		OAuth2Application oAuth2Application =
+			oAuth2ApplicationLocalService.getOAuth2Application(
+				oAuth2ApplicationId);
+
+		_oAuth2ApplicationModelResourcePermission.check(
+			getPermissionChecker(), oAuth2Application, ActionKeys.UPDATE);
+
+		return oAuth2ApplicationLocalService.updateOAuth2Application(
+			oAuth2ApplicationId, allowedGrantTypesList, clientId, clientProfile,
+			clientSecret, description, featuresList, homePageURL,
+			iconFileEntryId, name, privacyPolicyURL, redirectURIsList,
+			auth2ApplicationScopeAliasesId, serviceContext);
+	}
+
+	@Override
+	public OAuth2Application updateScopeAliases(
+			long oAuth2ApplicationId, List<String> scopeAliasesList)
+		throws PortalException {
+
+		OAuth2Application oAuth2Application =
+			oAuth2ApplicationLocalService.getOAuth2Application(
+				oAuth2ApplicationId);
+
+		_oAuth2ApplicationModelResourcePermission.check(
+			getPermissionChecker(), oAuth2Application, ActionKeys.UPDATE);
+
+		User user = getUser();
+
+		return oAuth2ApplicationLocalService.updateScopeAliases(
+			user.getUserId(), user.getFullName(), oAuth2ApplicationId,
+			scopeAliasesList);
+	}
+
+	private static volatile ModelResourcePermission<OAuth2Application>
+		_oAuth2ApplicationModelResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				OAuth2ApplicationServiceImpl.class,
+				"_oAuth2ApplicationModelResourcePermission",
+				OAuth2Application.class);
+
 }
--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2AuthorizationServiceImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2AuthorizationServiceImpl.java
@@ -14,11 +14,94 @@
 
 package com.liferay.oauth2.provider.service.impl;
 
+import com.liferay.oauth2.provider.constants.OAuth2ProviderActionKeys;
+import com.liferay.oauth2.provider.model.OAuth2Application;
+import com.liferay.oauth2.provider.model.OAuth2Authorization;
 import com.liferay.oauth2.provider.service.base.OAuth2AuthorizationServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMode;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
  */
+@JSONWebService(mode = JSONWebServiceMode.IGNORE)
 public class OAuth2AuthorizationServiceImpl
 	extends OAuth2AuthorizationServiceBaseImpl {
+
+	public List<OAuth2Authorization> getApplicationOAuth2Authorizations(
+			long oAuth2ApplicationId, int start, int end,
+			OrderByComparator<OAuth2Authorization> orderByComparator)
+		throws PortalException {
+
+		_oAuth2ApplicationModelResourcePermission.check(
+			getPermissionChecker(), oAuth2ApplicationId, ActionKeys.VIEW);
+
+		return oAuth2AuthorizationLocalService.getOAuth2Authorizations(
+			oAuth2ApplicationId, start, end, orderByComparator);
+	}
+
+	public int getApplicationOAuth2AuthorizationsCount(long oAuth2ApplicationId)
+		throws PortalException {
+
+		_oAuth2ApplicationModelResourcePermission.check(
+			getPermissionChecker(), oAuth2ApplicationId, ActionKeys.VIEW);
+
+		return oAuth2AuthorizationLocalService.getOAuth2AuthorizationsCount(
+			oAuth2ApplicationId);
+	}
+
+	public List<OAuth2Authorization> getUserOAuth2Authorizations(
+			int start, int end,
+			OrderByComparator<OAuth2Authorization> orderByComparator)
+		throws PortalException {
+
+		User user = getUser();
+
+		return oAuth2AuthorizationLocalService.getUserOAuth2Authorizations(
+			user.getUserId(), start, end, orderByComparator);
+	}
+
+	public int getUserOAuth2AuthorizationsCount() throws PortalException {
+		User user = getUser();
+
+		return oAuth2AuthorizationLocalService.getUserOAuth2AuthorizationsCount(
+			user.getUserId());
+	}
+
+	@Override
+	public void revokeOAuth2Authorization(long oAuth2AuthorizationId)
+		throws PortalException {
+
+		OAuth2Authorization oAuth2Authorization =
+			oAuth2AuthorizationLocalService.getOAuth2Authorization(
+				oAuth2AuthorizationId);
+
+		User user = getUser();
+
+		if (user.getUserId() != oAuth2Authorization.getUserId()) {
+			_oAuth2ApplicationModelResourcePermission.check(
+				getPermissionChecker(),
+				oAuth2Authorization.getOAuth2ApplicationId(),
+				OAuth2ProviderActionKeys.ACTION_REVOKE_TOKEN);
+		}
+
+		oAuth2AuthorizationLocalService.deleteOAuth2Authorization(
+			oAuth2AuthorizationId);
+	}
+
+	private static volatile ModelResourcePermission<OAuth2Application>
+		_oAuth2ApplicationModelResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				OAuth2AuthorizationServiceImpl.class,
+				"_oAuth2ApplicationModelResourcePermission",
+				OAuth2Application.class);
+
 }
