d78d71b61deb4a17d555255a2afdffda5d6ae863
Brian Chan <brian.chan@liferay.com>
2018-04-05 14:50:16 -0700

    OAUTH2-119 SF

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationLocalServiceImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationLocalServiceImpl.java
@@ -83,11 +83,8 @@ public class OAuth2ApplicationLocalServiceImpl
 		}
 
 		clientId = StringUtil.trim(clientId);
-
 		homePageURL = StringUtil.trim(homePageURL);
-
 		name = StringUtil.trim(name);
-
 		privacyPolicyURL = StringUtil.trim(privacyPolicyURL);
 
 		if (redirectURIsList == null) {
