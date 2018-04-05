bcdd64196dd20c2b06e1fd8323560530e005b015
Alessio Rendina <asilcomando@gmail.com>
2018-04-05 18:58:12 +0200

    COMMERCE-220 Added new commerce wish list constant

--- modules/dxp/apps/commerce/commerce-wish-list-api/src/main/java/com/liferay/commerce/wish/list/constants/CommerceWishListConstants.java
+++ modules/dxp/apps/commerce/commerce-wish-list-api/src/main/java/com/liferay/commerce/wish/list/constants/CommerceWishListConstants.java
@@ -21,4 +21,7 @@ public class CommerceWishListConstants {
 
 	public static final String RESOURCE_NAME = "com.liferay.commerce.wish.list";
 
+	public static final String WISH_LIST_SERVICE_NAME =
+		"com.liferay.commerce.wish.list.service.name";
+
 }
