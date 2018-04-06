a17e17ba0a71913cd021761baeeb9886d59af8e9
drewbrokke <drew.brokke@liferay.com>
2018-04-06 12:28:23 -0700

    LPS-79385 contacts - autogenerated

--- modules/apps/foundation/contacts/contacts-uad-test/src/testIntegration/java/com/liferay/contacts/uad/anonymizer/test/EntryUADAnonymizerTest.java
+++ modules/apps/foundation/contacts/contacts-uad-test/src/testIntegration/java/com/liferay/contacts/uad/anonymizer/test/EntryUADAnonymizerTest.java
@@ -32,6 +32,7 @@ import com.liferay.user.associated.data.aggregator.UADAggregator;
 import com.liferay.user.associated.data.anonymizer.UADAnonymizer;
 import com.liferay.user.associated.data.test.util.BaseUADAnonymizerTestCase;
 
+import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Rule;
 
@@ -99,6 +100,11 @@ public class EntryUADAnonymizerTest extends BaseUADAnonymizerTestCase {
 		}
 
 		return false;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_entryUADEntityTestHelper.cleanUpDependencies(_entries);
 	}
 
 	@DeleteAfterTestRun
