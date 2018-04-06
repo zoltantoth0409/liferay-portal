85950a8d188a9e19df9354cb24962f5637856fb8
Eduardo Garcia <eduardo.garcia@liferay.com>
2018-04-06 13:12:48 +0200

    LPS-79327 Add segments API classes

---
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/service/SegmentService.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.segments.model.Segment;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Models a service to retrieve segments.
+ *
+ * @author Eduardo Garcia
+ */
+@ProviderType
+public interface SegmentService<T extends Segment> {
+
+	public Collection<T> getSegments(long groupId);
+
+	public boolean matches(long userId, T segment, Map<String, Object> context);
+
+}
