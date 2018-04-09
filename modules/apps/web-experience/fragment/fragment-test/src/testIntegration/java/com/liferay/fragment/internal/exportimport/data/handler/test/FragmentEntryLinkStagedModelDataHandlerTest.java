0f5083f2cccfaa9f72ce310e0c8317723ab369fe
pavel-savinov <pavel.savinov@liferay.com>
2018-04-09 19:52:22 +0200

    LPS-79619 Add integration tests for fragment entry link staged model data handler

---
+++ modules/apps/web-experience/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/exportimport/data/handler/test/FragmentEntryLinkStagedModelDataHandlerTest.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.internal.exportimport.data.handler.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.exportimport.test.util.lar.BaseStagedModelDataHandlerTestCase;
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.util.FragmentTestUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Pavel Savinov
+ */
+@RunWith(Arquillian.class)
+public class FragmentEntryLinkStagedModelDataHandlerTest
+	extends BaseStagedModelDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Override
+	protected StagedModel addStagedModel(
+			Group group,
+			Map<String, List<StagedModel>> dependentStagedModelsMap)
+		throws Exception {
+
+		FragmentCollection fragmentCollection =
+			FragmentTestUtil.addFragmentCollection(
+				group.getGroupId(), RandomTestUtil.randomString(64));
+
+		FragmentEntry fragmentEntry = FragmentTestUtil.addFragmentEntry(
+			fragmentCollection.getFragmentCollectionId());
+
+		return FragmentTestUtil.addFragmentEntryLink(
+			fragmentEntry, 0, PortalUtil.getClassNameId(Layout.class),
+			group.getDefaultPublicPlid());
+	}
+
+	@Override
+	protected StagedModel getStagedModel(String uuid, Group group) {
+		return FragmentTestUtil.fetchFragmentEntryLink(
+			uuid, group.getGroupId());
+	}
+
+	@Override
+	protected Class<? extends StagedModel> getStagedModelClass() {
+		return FragmentEntryLink.class;
+	}
+
+}
---
+++ modules/apps/web-experience/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/util/FragmentTestUtil.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.util;
+
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentCollectionLocalServiceUtil;
+import com.liferay.fragment.service.FragmentEntryLinkLocalServiceUtil;
+import com.liferay.fragment.service.FragmentEntryLocalServiceUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+/**
+ * @author Pavel Savinov
+ */
+public class FragmentTestUtil {
+
+	public static FragmentCollection addFragmentCollection(
+			long groupId, String name)
+		throws PortalException {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(groupId);
+
+		return FragmentCollectionLocalServiceUtil.addFragmentCollection(
+			TestPropsValues.getUserId(), groupId, name, StringPool.BLANK,
+			serviceContext);
+	}
+
+	public static FragmentEntry addFragmentEntry(long fragmentCollectionId)
+		throws PortalException {
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionLocalServiceUtil.getFragmentCollection(
+				fragmentCollectionId);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				fragmentCollection.getGroupId());
+
+		return FragmentEntryLocalServiceUtil.addFragmentEntry(
+			TestPropsValues.getUserId(), serviceContext.getScopeGroupId(),
+			fragmentCollectionId, StringPool.BLANK,
+			RandomTestUtil.randomString(64), StringPool.BLANK, "<div></div>",
+			StringPool.BLANK, WorkflowConstants.STATUS_APPROVED,
+			serviceContext);
+	}
+
+	public static FragmentEntryLink addFragmentEntryLink(
+			FragmentEntry fragmentEntry, long originalFragmentEntryLinkId,
+			long classNameId, long classPK)
+		throws PortalException {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				fragmentEntry.getGroupId());
+
+		return FragmentEntryLinkLocalServiceUtil.addFragmentEntryLink(
+			TestPropsValues.getUserId(), serviceContext.getScopeGroupId(),
+			originalFragmentEntryLinkId, fragmentEntry.getFragmentEntryId(),
+			classNameId, classPK, fragmentEntry.getCss(),
+			fragmentEntry.getHtml(), fragmentEntry.getJs(), StringPool.BLANK, 1,
+			serviceContext);
+	}
+
+	public static FragmentEntryLink fetchFragmentEntryLink(
+		String uuid, long groupId) {
+
+		return FragmentEntryLinkLocalServiceUtil.
+			fetchFragmentEntryLinkByUuidAndGroupId(uuid, groupId);
+	}
+
+}
