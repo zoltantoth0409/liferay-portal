522d14083677c5fca48bfc1715784ab6c3cf4053
Michael Prigge <michael.prigge@liferay.com>
2018-04-10 14:16:19 -0700

    LPS-73350 Set predefined value to "dataType" property of Paragraph Field

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-paragraph/src/main/java/com/liferay/dynamic/data/mapping/type/paragraph/internal/ParagraphDDMFormFieldTypeSettings.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-paragraph/src/main/java/com/liferay/dynamic/data/mapping/type/paragraph/internal/ParagraphDDMFormFieldTypeSettings.java
@@ -68,6 +68,11 @@ public interface ParagraphDDMFormFieldTypeSettings
 	extends DefaultDDMFormFieldTypeSettings {
 
 	@DDMFormField(
+		predefinedValue = "", required = true, visibilityExpression = "FALSE"
+	)
+	public String dataType();
+
+	@DDMFormField(
 		label = "%title", properties = "placeholder=%enter-a-title",
 		required = true, type = "key-value"
 	)
