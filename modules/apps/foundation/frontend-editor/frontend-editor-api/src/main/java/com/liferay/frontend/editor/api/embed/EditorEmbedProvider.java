48b08c3f7685944131f2d7b02a9076179b9556f8
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-04-10 11:37:01 +0200

    LPS-78622 javadoc

--- modules/apps/foundation/frontend-editor/frontend-editor-api/src/main/java/com/liferay/frontend/editor/api/embed/EditorEmbedProvider.java
+++ modules/apps/foundation/frontend-editor/frontend-editor-api/src/main/java/com/liferay/frontend/editor/api/embed/EditorEmbedProvider.java
@@ -15,14 +15,57 @@
 package com.liferay.frontend.editor.api.embed;
 
 /**
+ * Provides an interface for supporting embed providers for the editors.
+ *
+ * <p>
+ * Implementations of this class must be OSGi components that are registered in
+ * the OSGi Registry.
+ * </p>
+ *
+ * <p>
+ * The embed providers can optionally be categorized by using the
+ * <code>type</code> OSGi property with any of the values defined in
+ * {@link EditorEmbedProviderTypeConstants}. By default the provider will be
+ * categorized as {@link EditorEmbedProviderTypeConstants.UNKNOWN} unless
+ * specified otherwise.
+ * </p>
+ *
  * @author Sergio González
  */
 public interface EditorEmbedProvider {
 
+	/**
+	 * Returns the embed provider's ID
+	 *
+	 * @return the ID of the embed provider
+	 */
 	public String getId();
 
+	/**
+	 * Returns an array with the the url schemes for the embed provider.
+	 *
+	 * The url scheme describes which urls of the provider have an embedded
+	 * representation. Url schemes are defined using a regular expression that
+	 * indicates whether a url matches with the provider or not.
+	 *
+	 * @return the url schemes for the embed provider
+	 */
 	public String[] getSchemas();
 
+	/**
+	 * Returns the template that will be used by the editor to embed the
+	 * content.
+	 *
+	 * The template accepts the variable <code>{embedId}</code> that represents
+	 * the content id that should be embedded. This value is obtained from the
+	 * url scheme regular expressions.
+	 *
+	 * This template is tipically an iframe to the provider that will display
+	 * the content.
+	 *
+	 * @return the template that will be used by the editor to embedded the
+	 *         content
+	 */
 	public String getTpl();
 
 }
