b3b6d4c922673232b002e2cc5cde94e2bd89615d
Lance Ji <zheyi.ji@liferay.com>
2018-04-10 16:35:05 -0700

    LPS-79328 Move ConfigurationBeanDeclaration and ConfigurationPidMapping out of social-activity-api

--- modules/apps/collaboration/social/social-activity-web/src/main/java/com/liferay/social/activity/web/internal/configuration/definition/SocialActivityGroupServiceConfigurationBeanDeclaration.java
+++ modules/apps/collaboration/social/social-activity-web/src/main/java/com/liferay/social/activity/web/internal/configuration/definition/SocialActivityGroupServiceConfigurationBeanDeclaration.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.social.activity.configuration.definition;
+package com.liferay.social.activity.web.internal.configuration.definition;
 
 import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaration;
 import com.liferay.social.activity.configuration.SocialActivityGroupServiceConfiguration;
