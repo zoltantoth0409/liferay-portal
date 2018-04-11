1f8f2a5a0ecf6df10bb8423c9250c992385c03e6
Alessio Rendina <asilcomando@gmail.com>
2018-04-11 11:52:11 +0200

    COMMERCE-305 Moved and renamed action key

--- modules/dxp/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/constants/CommerceActionKeys.java
+++ modules/dxp/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/constants/CommerceActionKeys.java
@@ -43,9 +43,6 @@ public class CommerceActionKeys {
 	public static final String MANAGE_COMMERCE_SHIPPING_METHODS =
 		"MANAGE_COMMERCE_SHIPPING_METHODS";
 
-	public static final String MANAGE_COMMERCE_TAX_CATEGORIES =
-		"MANAGE_COMMERCE_TAX_CATEGORIES";
-
 	public static final String MANAGE_COMMERCE_TAX_METHODS =
 		"MANAGE_COMMERCE_TAX_METHODS";
 
