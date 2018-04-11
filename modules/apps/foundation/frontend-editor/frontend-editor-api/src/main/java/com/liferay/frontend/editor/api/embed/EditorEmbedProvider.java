0a4fff924464c15ef3052086b36a06aae0736627
codyhoag <cody.hoag@liferay.com>
2018-04-11 08:50:20 -0400

    LPS-78622 LRDOCS-5115 Edit Javadoc

--- modules/apps/foundation/frontend-editor/frontend-editor-api/src/main/java/com/liferay/frontend/editor/api/embed/EditorEmbedProvider.java
+++ modules/apps/foundation/frontend-editor/frontend-editor-api/src/main/java/com/liferay/frontend/editor/api/embed/EditorEmbedProvider.java
@@ -15,19 +15,15 @@
 package com.liferay.frontend.editor.api.embed;
 
 /**
- * Provides an interface for supporting embed providers for the editors.
- *
- * <p>
+ * Provides an interface for supporting embed providers for editors.
  * Implementations of this class must be OSGi components that are registered in
  * the OSGi Registry.
- * </p>
  *
  * <p>
  * The embed providers can optionally be categorized by using the
- * <code>type</code> OSGi property with any of the values defined in
- * {@link EditorEmbedProviderTypeConstants}. By default, the provider is
- * categorized as {@link EditorEmbedProviderTypeConstants.UNKNOWN}, unless
- * specified otherwise.
+ * <code>type</code> OSGi property with any of the values defined in {@link
+ * EditorEmbedProviderTypeConstants}. By default, the provider is categorized as
+ * {@link EditorEmbedProviderTypeConstants#UNKNOWN}, unless specified otherwise.
  * </p>
  *
  * @author Sergio González
@@ -35,36 +31,38 @@ package com.liferay.frontend.editor.api.embed;
 public interface EditorEmbedProvider {
 
 	/**
-	 * Returns the embed provider's ID
+	 * Returns the embed provider's ID.
 	 *
-	 * @return the ID of the embed provider
+	 * @return the embed provider's ID
 	 */
 	public String getId();
 
 	/**
 	 * Returns the template that is used by the editor to embed the content.
-	 *
-	 * The template accepts the <code>{embedId}</code> that represents the ID of
-	 * the content to embed. This value is obtained from the URL scheme regular
-	 * expressions.
-	 *
-	 * This template is usually an iframe to the provider that displays the
+	 * This template is usually an iFrame to the provider that displays the
 	 * content.
+	 *
+	 * <p>
+	 * The template accepts the <code>embedId</code> that represents the ID of
+	 * the content to embed. This value is obtained from URL scheme regular
+	 * expressions.
+	 * </p>
 	 *
 	 * @return the template that is used by the editor to embed the content
 	 */
 	public String getTpl();
 
 	/**
-	 * Returns an array with the URL schemes for the embed provider.
+	 * Returns an array with the URL schemes for the embed provider. The URL
+	 * scheme describes which URLs of the provider have an embedded
+	 * representation. URL schemes are defined using a JavaScript regular
+	 * expression that indicates whether a URL matches the provider.
 	 *
-	 * The URL scheme describes which URLs of the provider have an embedded
-	 * representation. Url schemes are defined using a JavaScript Regular
-	 * Expression that indicates whether a URL matches with the provider or not.
-	 *
-	 * Every URL scheme should contain a single matching group. This match is used
-	 * to replace the <code>{embedId}</code> placeholder from the provided
+	 * <p>
+	 * Every URL scheme should contain a single matching group. This match is
+	 * used to replace the <code>embedId</code> placeholder from the provided
 	 * template.
+	 * </p>
 	 *
 	 * @return the URL schemes for the embed provider
 	 */
