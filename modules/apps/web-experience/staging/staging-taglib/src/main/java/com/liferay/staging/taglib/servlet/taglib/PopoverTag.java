8310c38dc096e68e55754e0c866c9dd4eb1df5da
Peter Borkuti <peter.borkuti@liferay.com>
2018-04-12 13:11:02 +0200

    LPS-79439 popover tag for staging

---
+++ modules/apps/web-experience/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/servlet/taglib/PopoverTag.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.taglib.servlet.taglib;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.staging.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.taglib.util.IncludeTag;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Peter Borkuti
+ */
+@ProviderType
+public class PopoverTag extends IncludeTag {
+
+	public void setId(String id) {
+		_id = id;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	public void setText(String text) {
+		_text = text;
+	}
+
+	public void setTitle(String title) {
+		_title = title;
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_id = StringPool.BLANK;
+		_text = StringPool.BLANK;
+		_title = StringPool.BLANK;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute("liferay-staging:popover:id", _id);
+		request.setAttribute("liferay-staging:popover:text", _text);
+		request.setAttribute("liferay-staging:popover:title", _title);
+	}
+
+	private static final String _PAGE = "/popover/page.jsp";
+
+	private String _id = StringPool.BLANK;
+	private String _text = StringPool.BLANK;
+	private String _title = StringPool.BLANK;
+
+}
---
+++ modules/apps/web-experience/staging/staging-taglib/src/main/resources/META-INF/resources/popover/page.jsp
@@ -0,0 +1,63 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/popover/init.jsp" %>
+
+<c:if test="<%= Validator.isNotNull(text) %>">
+	<span class="staging-taglib-popover" id="<%= domId %>">
+		<span class="staging-taglib-popover-icon-holder">
+			<clay:icon
+				symbol="question-circle-full"
+			/>
+		</span>
+
+		<div class="bs-popover-right popover">
+			<div class="arrow"></div>
+			<div class="inline-scroller">
+				<div class="popover-header"><%= title %></div>
+				<div class="popover-body">
+					<p><%= text %></p>
+				</div>
+			</div>
+		</div>
+	</span>
+
+	<aui:script use="aui-base">
+		A.ready("aui-base", function(A)
+			{
+				var popoverNode = A.one("#<%= domId %>");
+
+				var popover = popoverNode.one(".popover");
+
+				var arrow = popover.one(".arrow");
+
+				var dx = arrow.width();
+				var dy = arrow.height() / 2;
+
+				var iconHolderNode = popoverNode.one(".staging-taglib-popover-icon-holder");
+
+				iconHolderNode.on("hover", function alignPopoverToIcon(event)
+					{
+						if ("visible" !== popover.get('visibility')) {
+							popover.setXY([iconHolderNode.getX() + iconHolderNode.width() + dx, iconHolderNode.getY() - dy]);
+						}
+					}
+				);
+
+			}
+		);
+	</aui:script>
+</c:if>
