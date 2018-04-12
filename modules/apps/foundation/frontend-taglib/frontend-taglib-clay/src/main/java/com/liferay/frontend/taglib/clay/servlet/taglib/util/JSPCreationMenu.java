4ebda7c255d13807421c0d5faade9d1acb618e04
Carlos Lancha <carlos.lancha@liferay.com>
2018-04-12 12:06:50 +0200

    LPS-78240 Create JSPCreationMenu

---
+++ modules/apps/foundation/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/util/JSPCreationMenu.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.servlet.taglib.util;
+
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Carlos Lancha
+ */
+public class JSPCreationMenu extends CreationMenu {
+
+	public JSPCreationMenu(PageContext pageContext) {
+		super((HttpServletRequest)pageContext.getRequest());
+
+		renderResponse = (RenderResponse)pageContext.findAttribute(
+			"renderResponse");
+		request = (HttpServletRequest)pageContext.getRequest();
+	}
+
+	protected RenderResponse renderResponse;
+	protected HttpServletRequest request;
+
+}
