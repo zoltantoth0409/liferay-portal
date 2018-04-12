d8c02d5ce51ad1126f72955d85d5d568f4098fe4
Eduardo Garcia <eduardo.garcia@liferay.com>
2018-04-12 15:33:01 +0200

    LPS-79327 Extend tests

--- modules/apps/meris/meris-asset-category-demo-test/src/testIntegration/java/com/liferay/meris/asset/category/demo/test/AssetCategoryMerisSegmentManagerTest.java
+++ modules/apps/meris/meris-asset-category-demo-test/src/testIntegration/java/com/liferay/meris/asset/category/demo/test/AssetCategoryMerisSegmentManagerTest.java
@@ -21,15 +21,16 @@ import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.asset.kernel.service.AssetCategoryLocalServiceUtil;
 import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
 import com.liferay.asset.kernel.service.AssetVocabularyLocalServiceUtil;
+import com.liferay.meris.MerisRule;
 import com.liferay.meris.MerisSegment;
 import com.liferay.meris.MerisSegmentManager;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
@@ -37,9 +38,11 @@ import com.liferay.portal.service.test.ServiceTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
-import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -71,12 +74,17 @@ public class AssetCategoryMerisSegmentManagerTest {
 		AssetVocabulary assetVocabulary =
 			AssetVocabularyLocalServiceUtil.addVocabulary(
 				TestPropsValues.getUserId(), serviceContext.getScopeGroupId(),
-				_ASSET_VOCABULARY_NAME, serviceContext);
+				"Sports Fan", serviceContext);
 
-		AssetCategory assetCategory = AssetCategoryLocalServiceUtil.addCategory(
-			TestPropsValues.getUserId(), serviceContext.getScopeGroupId(),
-			RandomTestUtil.randomString(), assetVocabulary.getVocabularyId(),
-			serviceContext);
+		AssetCategory soccerAssetCategory =
+			AssetCategoryLocalServiceUtil.addCategory(
+				TestPropsValues.getUserId(), serviceContext.getScopeGroupId(),
+				"Soccer", assetVocabulary.getVocabularyId(), serviceContext);
+
+		AssetCategory tennisAssetCategory =
+			AssetCategoryLocalServiceUtil.addCategory(
+				TestPropsValues.getUserId(), serviceContext.getScopeGroupId(),
+				"Tennis", assetVocabulary.getVocabularyId(), serviceContext);
 
 		_user = UserTestUtil.addUser();
 
@@ -84,33 +92,99 @@ public class AssetCategoryMerisSegmentManagerTest {
 			_user.getModelClassName(), _user.getUserId());
 
 		AssetCategoryLocalServiceUtil.addAssetEntryAssetCategory(
-			assetEntry.getEntryId(), assetCategory.getCategoryId());
+			assetEntry.getEntryId(), soccerAssetCategory.getCategoryId());
+
+		AssetCategoryLocalServiceUtil.addAssetEntryAssetCategory(
+			assetEntry.getEntryId(), tennisAssetCategory.getCategoryId());
+
+		_merisSegmentId = Objects.toString(assetVocabulary.getVocabularyId());
+
+		_merisScopeId = Objects.toString(_group.getGroupId());
+
+		_merisProfileId = Objects.toString(_user.getUserId());
 	}
 
 	@Test
-	public void testMatches() throws Exception {
-		Collection<MerisSegment> merisSegments =
-			_merisSegmentManager.getMerisSegments(_group.getGroupId());
-
-		Assert.assertFalse("Meris segments is empty", merisSegments.isEmpty());
-
-		Iterator<MerisSegment> iterator = merisSegments.iterator();
-
-		MerisSegment merisSegment = iterator.next();
-
-		Assert.assertTrue(
-			"User does not match the meris segment",
-			_merisSegmentManager.matches(
-				_user.getUserId(), merisSegment, new HashMap<>()));
+	public void testGetMerisProfile() {
+		Assert.assertNotNull(
+			"Meris profile was not found",
+			_merisSegmentManager.getMerisProfile(_merisProfileId));
 	}
 
-	private static final String _ASSET_VOCABULARY_NAME = "Segments";
+	@Test
+	public void testGetMerisProfileMerisSegments() throws Exception {
+		Comparator<MerisSegment> merisSegmentNameComparator =
+			Comparator.comparing(s -> s.getName(Locale.getDefault()));
+
+		List<MerisSegment> merisSegments =
+			_merisSegmentManager.getMerisSegments(
+				_merisScopeId, _merisProfileId, _merisSegmentId,
+				new HashMap<>(), 0, 1, merisSegmentNameComparator);
+
+		Assert.assertFalse(
+			"Meris profile does not contain any meris segment",
+			merisSegments.isEmpty());
+	}
+
+	@Test
+	public void testGetMerisProfiles() throws Exception {
+		List merisProfiles = _merisSegmentManager.getMerisProfiles(
+			_merisSegmentId, new HashMap<>(), QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS, null);
+
+		Assert.assertFalse(
+			"Meris segment does not contain any meris profile",
+			merisProfiles.isEmpty());
+	}
+
+	@Test
+	public void testGetMerisSegment() {
+		MerisSegment merisSegment = _merisSegmentManager.getMerisSegment(
+			_merisSegmentId);
+
+		Assert.assertNotNull("Meris segment was not found", merisSegment);
+
+		Comparator<MerisRule> merisRuleNameComparator = Comparator.comparing(
+			s -> s.getName(Locale.getDefault()));
+
+		List<MerisRule> merisRules = merisSegment.getMerisRules(
+			0, 1, merisRuleNameComparator);
+
+		Assert.assertFalse(
+			"Meris segment does not contain any meris rule",
+			merisRules.isEmpty());
+	}
+
+	@Test
+	public void testGetMerisSegments() {
+		Comparator<MerisSegment> merisSegmentNameComparator =
+			Comparator.comparing(s -> s.getName(Locale.getDefault()));
+
+		List<MerisSegment> merisSegments =
+			_merisSegmentManager.getMerisSegments(
+				_merisScopeId, 0, 1, merisSegmentNameComparator);
+
+		Assert.assertFalse(
+			"No meris segments were found", merisSegments.isEmpty());
+	}
+
+	@Test
+	public void testMatches() {
+		Assert.assertTrue(
+			"Meris profile does not match the meris segment",
+			_merisSegmentManager.matches(
+				_merisProfileId, _merisSegmentId, new HashMap<>()));
+	}
 
 	@Inject
 	private static MerisSegmentManager _merisSegmentManager;
 
 	@DeleteAfterTestRun
 	private Group _group;
+
+	private String _merisProfileId;
+	private String _merisScopeId;
+	private String _merisSegmentId;
 
 	@DeleteAfterTestRun
 	private User _user;
