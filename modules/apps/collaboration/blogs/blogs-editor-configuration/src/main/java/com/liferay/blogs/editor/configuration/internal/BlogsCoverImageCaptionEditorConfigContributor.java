687ddf30fe0c0d1816dcfbcb9e5d0c7f46f7499f
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-04-13 14:41:23 +0200

    LPS-79621 Remove unused code

--- modules/apps/collaboration/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsCoverImageCaptionEditorConfigContributor.java
+++ modules/apps/collaboration/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsCoverImageCaptionEditorConfigContributor.java
@@ -17,8 +17,6 @@ package com.liferay.blogs.editor.configuration.internal;
 import com.liferay.blogs.constants.BlogsPortletKeys;
 import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
 import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
-import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -49,52 +47,6 @@ public class BlogsCoverImageCaptionEditorConfigContributor
 
 		jsonObject.put("allowedContent", "a");
 		jsonObject.put("disallowedContent", "br");
-	}
-
-	protected JSONObject getToolbarsJSONObject() {
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-
-		jsonObject.put("styles", getToolbarsStylesJSONObject());
-
-		return jsonObject;
-	}
-
-	protected JSONObject getToolbarsStylesJSONObject() {
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-
-		jsonObject.put("selections", getToolbarStylesSelectionsJSONArray());
-		jsonObject.put("tabIndex", 1);
-
-		return jsonObject;
-	}
-
-	protected JSONArray getToolbarStylesSelectionsJSONArray() {
-		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
-
-		jsonArray.put(getToolbarStylesSelectionsLinkJSONObject());
-		jsonArray.put(getToolbarStylesSelectionsTextJSONObject());
-
-		return jsonArray;
-	}
-
-	protected JSONObject getToolbarStylesSelectionsLinkJSONObject() {
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-
-		jsonObject.put("buttons", toJSONArray("['linkEdit']"));
-		jsonObject.put("name", "link");
-		jsonObject.put("test", "AlloyEditor.SelectionTest.link");
-
-		return jsonObject;
-	}
-
-	protected JSONObject getToolbarStylesSelectionsTextJSONObject() {
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
-
-		jsonObject.put("buttons", toJSONArray("['link']"));
-		jsonObject.put("name", "text");
-		jsonObject.put("test", "AlloyEditor.SelectionTest.text");
-
-		return jsonObject;
 	}
 
 }
