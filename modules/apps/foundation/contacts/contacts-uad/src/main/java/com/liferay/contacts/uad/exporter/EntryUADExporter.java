1612202f0985e63dc8ae8b06468f64f2f79d0032
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-04-12 17:02:13 -0700

    LPS-77639 contacts-uad - autogenerated

---
+++ modules/apps/foundation/contacts/contacts-uad/src/main/java/com/liferay/contacts/uad/exporter/EntryUADExporter.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contacts.uad.exporter;
+
+import com.liferay.contacts.model.Entry;
+import com.liferay.contacts.service.EntryLocalService;
+import com.liferay.contacts.uad.constants.ContactsUADConstants;
+
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+
+import com.liferay.user.associated.data.exporter.DynamicQueryUADExporter;
+import com.liferay.user.associated.data.exporter.UADExporter;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+@Component(immediate = true, property =  {
+	"model.class.name=" + ContactsUADConstants.CLASS_NAME_ENTRY}, service = UADExporter.class)
+public class EntryUADExporter extends DynamicQueryUADExporter<Entry> {
+	@Override
+	protected ActionableDynamicQuery doGetActionableDynamicQuery() {
+		return _entryLocalService.getActionableDynamicQuery();
+	}
+
+	@Override
+	protected String[] doGetUserIdFieldNames() {
+		return ContactsUADConstants.USER_ID_FIELD_NAMES_ENTRY;
+	}
+
+	@Reference
+	private EntryLocalService _entryLocalService;
+}
