ff2dbaf8b404ac8f6781327490b50b1ddf42f82a
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2018-04-17 13:05:56 +0200

    LPS-67045 Typo

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-runtime-form-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/form/internal/DDMFormDefinitionRetriever.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-runtime-form-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/form/internal/DDMFormDefinitionRetriever.java
@@ -31,7 +31,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Michael C. Han
  */
 @Component(immediate = true, service = FormDefinitionRetriever.class)
-public class DDMFormDefintionRetriever implements FormDefinitionRetriever {
+public class DDMFormDefinitionRetriever implements FormDefinitionRetriever {
 
 	@Override
 	public String getFormDefinition(
