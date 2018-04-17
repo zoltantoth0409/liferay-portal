43fff17cbc18067d3d0c089de6f060ca5c8cbe94
Marco Leo <marco.leo@liferay.com>
2018-04-17 10:59:50 +0200

    COMMERCE-256 Add Service implementation

--- modules/dxp/apps/commerce/commerce-user-segment-service/src/main/java/com/liferay/commerce/user/segment/service/impl/CommerceUserSegmentEntryLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-user-segment-service/src/main/java/com/liferay/commerce/user/segment/service/impl/CommerceUserSegmentEntryLocalServiceImpl.java
@@ -14,11 +14,129 @@
 
 package com.liferay.commerce.user.segment.service.impl;
 
+import com.liferay.commerce.user.segment.model.CommerceUserSegmentEntry;
 import com.liferay.commerce.user.segment.service.base.CommerceUserSegmentEntryLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
  */
 public class CommerceUserSegmentEntryLocalServiceImpl
 	extends CommerceUserSegmentEntryLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CommerceUserSegmentEntry addCommerceUserSegmentEntry(
+			Map<Locale, String> nameMap, double priority, boolean active,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		// Commerce user segment entry
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		long commerceUserSegmentEntryId = counterLocalService.increment();
+
+		CommerceUserSegmentEntry commerceUserSegmentEntry =
+			commerceUserSegmentEntryPersistence.create(
+				commerceUserSegmentEntryId);
+
+		commerceUserSegmentEntry.setGroupId(groupId);
+		commerceUserSegmentEntry.setCompanyId(user.getCompanyId());
+		commerceUserSegmentEntry.setUserId(user.getUserId());
+		commerceUserSegmentEntry.setUserName(user.getFullName());
+		commerceUserSegmentEntry.setNameMap(nameMap);
+		commerceUserSegmentEntry.setPriority(priority);
+		commerceUserSegmentEntry.setActive(active);
+		commerceUserSegmentEntry.setExpandoBridgeAttributes(serviceContext);
+
+		commerceUserSegmentEntryPersistence.update(commerceUserSegmentEntry);
+
+		// Resources
+
+		resourceLocalService.addModelResources(
+			commerceUserSegmentEntry, serviceContext);
+
+		return commerceUserSegmentEntry;
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CommerceUserSegmentEntry deleteCommerceUserSegmentEntry(
+			CommerceUserSegmentEntry commerceUserSegmentEntry)
+		throws PortalException {
+
+		// Commerce user segment entry
+
+		commerceUserSegmentEntryPersistence.remove(commerceUserSegmentEntry);
+
+		// Resources
+
+		resourceLocalService.deleteResource(
+			commerceUserSegmentEntry, ResourceConstants.SCOPE_INDIVIDUAL);
+
+		// Expando
+
+		expandoRowLocalService.deleteRows(
+			commerceUserSegmentEntry.getCommerceUserSegmentEntryId());
+
+		return commerceUserSegmentEntry;
+	}
+
+	@Override
+	public CommerceUserSegmentEntry deleteCommerceUserSegmentEntry(
+			long commerceUserSegmentEntryId)
+		throws PortalException {
+
+		CommerceUserSegmentEntry commerceUserSegmentEntry =
+			commerceUserSegmentEntryPersistence.findByPrimaryKey(
+				commerceUserSegmentEntryId);
+
+		return commerceUserSegmentEntryLocalService.
+			deleteCommerceUserSegmentEntry(commerceUserSegmentEntry);
+	}
+
+	@Override
+	public List<CommerceUserSegmentEntry> getCommerceUserSegmentEntries(
+		long groupId, int start, int end) {
+
+		return commerceUserSegmentEntryPersistence.findByGroupId(
+			groupId, start, end);
+	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CommerceUserSegmentEntry updateCommerceUserSegmentEntry(
+			long commerceUserSegmentEntryId, Map<Locale, String> nameMap,
+			double priority, boolean active, ServiceContext serviceContext)
+		throws PortalException {
+
+		// Commerce user segment entry
+
+		CommerceUserSegmentEntry commerceUserSegmentEntry =
+			commerceUserSegmentEntryPersistence.findByPrimaryKey(
+				commerceUserSegmentEntryId);
+
+		commerceUserSegmentEntry.setNameMap(nameMap);
+		commerceUserSegmentEntry.setPriority(priority);
+		commerceUserSegmentEntry.setActive(active);
+		commerceUserSegmentEntry.setExpandoBridgeAttributes(serviceContext);
+
+		return commerceUserSegmentEntryPersistence.update(
+			commerceUserSegmentEntry);
+	}
+
 }
--- modules/dxp/apps/commerce/commerce-user-segment-service/src/main/java/com/liferay/commerce/user/segment/service/impl/CommerceUserSegmentEntryServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-user-segment-service/src/main/java/com/liferay/commerce/user/segment/service/impl/CommerceUserSegmentEntryServiceImpl.java
@@ -14,11 +14,95 @@
 
 package com.liferay.commerce.user.segment.service.impl;
 
+import com.liferay.commerce.user.segment.constants.CommerceUserSegmentsActionKeys;
+import com.liferay.commerce.user.segment.constants.CommerceUserSegmentsConstants;
+import com.liferay.commerce.user.segment.model.CommerceUserSegmentEntry;
 import com.liferay.commerce.user.segment.service.base.CommerceUserSegmentEntryServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * @author Marco Leo
  */
 public class CommerceUserSegmentEntryServiceImpl
 	extends CommerceUserSegmentEntryServiceBaseImpl {
+
+	@Override
+	public CommerceUserSegmentEntry addCommerceUserSegmentEntry(
+			Map<Locale, String> nameMap, double priority, boolean active,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			CommerceUserSegmentsActionKeys.
+				ADD_COMMERCE_USER_SEGMENTATION_ENTRY);
+
+		return commerceUserSegmentEntryLocalService.addCommerceUserSegmentEntry(
+			nameMap, priority, active, serviceContext);
+	}
+
+	@Override
+	public CommerceUserSegmentEntry deleteCommerceUserSegmentEntry(
+			long commerceUserSegmentEntryId)
+		throws PortalException {
+
+		_commerceUserSegmentEntryResourcePermission.check(
+			getPermissionChecker(), commerceUserSegmentEntryId,
+			ActionKeys.DELETE);
+
+		return commerceUserSegmentEntryLocalService.
+			deleteCommerceUserSegmentEntry(commerceUserSegmentEntryId);
+	}
+
+	@Override
+	public List<CommerceUserSegmentEntry> getCommerceUserSegmentEntries(
+			long groupId, int start, int end)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId, ActionKeys.VIEW);
+
+		return commerceUserSegmentEntryLocalService.
+			getCommerceUserSegmentEntries(groupId, start, end);
+	}
+
+	@Override
+	public CommerceUserSegmentEntry updateCommerceUserSegmentEntry(
+			long commerceUserSegmentEntryId, Map<Locale, String> nameMap,
+			double priority, boolean active, ServiceContext serviceContext)
+		throws PortalException {
+
+		_commerceUserSegmentEntryResourcePermission.check(
+			getPermissionChecker(), commerceUserSegmentEntryId,
+			ActionKeys.UPDATE);
+
+		return commerceUserSegmentEntryLocalService.
+			updateCommerceUserSegmentEntry(
+				commerceUserSegmentEntryId, nameMap, priority, active,
+				serviceContext);
+	}
+
+	private static volatile ModelResourcePermission<CommerceUserSegmentEntry>
+		_commerceUserSegmentEntryResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				CommerceUserSegmentEntryServiceImpl.class,
+				"_commerceUserSegmentEntryResourcePermission",
+				CommerceUserSegmentEntry.class);
+	private static volatile PortletResourcePermission
+		_portletResourcePermission =
+			PortletResourcePermissionFactory.getInstance(
+				CommerceUserSegmentEntryServiceImpl.class,
+				"_portletResourcePermission",
+				CommerceUserSegmentsConstants.RESOURCE_NAME);
+
 }
