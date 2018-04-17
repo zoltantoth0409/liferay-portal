47f4539e522311a43dc9bad1ed2f08fbad858c04
drewbrokke <drew.brokke@liferay.com>
2018-04-17 11:35:30 -0700

    LPS-79728 user-associated-data-web - UADApplicationSummaryHelper.java - separates search container results and total into two different stream instances

--- modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADApplicationSummaryHelper.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADApplicationSummaryHelper.java
@@ -33,6 +33,7 @@ import com.liferay.user.associated.data.web.internal.registry.UADRegistry;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -77,14 +78,18 @@ public class UADApplicationSummaryHelper {
 			ParamUtil.getString(
 				renderRequest, searchContainer.getOrderByTypeParam(), "asc"));
 
-		Stream<UADApplicationSummaryDisplay>
-			uadApplicationSummaryDisplayStream =
-				getUADApplicationSummaryDisplayStream(portletRequest, userId);
+		Predicate<UADApplicationSummaryDisplay> predicate = getPredicate(
+			ParamUtil.getString(renderRequest, "navigation", "all"));
 
-		List<UADApplicationSummaryDisplay> uadApplicationSummaryDisplays =
-			uadApplicationSummaryDisplayStream.filter(
-				getPredicate(
-					ParamUtil.getString(renderRequest, "navigation", "all"))
+		Supplier<Stream<UADApplicationSummaryDisplay>> streamSupplier =
+			() -> getUADApplicationSummaryDisplayStream(portletRequest, userId);
+
+		Stream<UADApplicationSummaryDisplay> summaryDisplayStream =
+			streamSupplier.get();
+
+		List<UADApplicationSummaryDisplay> results =
+			summaryDisplayStream.filter(
+				predicate
 			).sorted(
 				getComparator(
 					searchContainer.getOrderByCol(),
@@ -93,9 +98,15 @@ public class UADApplicationSummaryHelper {
 				Collectors.toList()
 			);
 
-		searchContainer.setResults(uadApplicationSummaryDisplays);
+		searchContainer.setResults(results);
 
-		searchContainer.setTotal(uadApplicationSummaryDisplays.size());
+		summaryDisplayStream = streamSupplier.get();
+
+		long total = summaryDisplayStream.filter(
+			predicate
+		).count();
+
+		searchContainer.setTotal((int)total);
 
 		return searchContainer;
 	}
