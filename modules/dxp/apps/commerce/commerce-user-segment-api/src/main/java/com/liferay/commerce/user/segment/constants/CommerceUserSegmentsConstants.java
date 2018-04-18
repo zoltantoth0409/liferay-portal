7939fb4da81fb23c4c9ac673ba79c7562143345e
Marco Leo <marco.leo@liferay.com>
2018-04-18 18:38:37 +0200

    COMMERCE-256 Add criterion constants

--- modules/dxp/apps/commerce/commerce-user-segment-api/src/main/java/com/liferay/commerce/user/segment/constants/CommerceUserSegmentsConstants.java
+++ modules/dxp/apps/commerce/commerce-user-segment-api/src/main/java/com/liferay/commerce/user/segment/constants/CommerceUserSegmentsConstants.java
@@ -19,6 +19,16 @@ package com.liferay.commerce.user.segment.constants;
  */
 public class CommerceUserSegmentsConstants {
 
+	public static final String CRITERION_TYPE_ORGANIZATION =
+		"CRITERION_TYPE_ORGANIZATION";
+
+	public static final String CRITERION_TYPE_ROLE = "CRITERION_TYPE_ROLE";
+
+	public static final String CRITERION_TYPE_USER = "CRITERION_TYPE_USER";
+
+	public static final String CRITERION_TYPE_USER_GROUP =
+		"CRITERION_TYPE_USER_GROUP";
+
 	public static final String RESOURCE_NAME =
 		"com.liferay.commerce.user.segment";
 
