216c05e83915b771cef1e464debee4637d068e09
drewbrokke <drew.brokke@liferay.com>
2018-04-18 15:52:33 -0700

    LPS-79386 bookmarks-uad-test - renames *Display class and references

--- modules/apps/collaboration/bookmarks/bookmarks-uad-test/src/testIntegration/java/com/liferay/bookmarks/uad/display/test/BookmarksEntryUADDisplayTest.java
+++ modules/apps/collaboration/bookmarks/bookmarks-uad-test/src/testIntegration/java/com/liferay/bookmarks/uad/display/test/BookmarksEntryUADDisplayTest.java
@@ -24,8 +24,8 @@ import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.user.associated.data.aggregator.UADAggregator;
-import com.liferay.user.associated.data.display.UADEntityDisplay;
-import com.liferay.user.associated.data.test.util.BaseUADEntityDisplayTestCase;
+import com.liferay.user.associated.data.display.UADDisplay;
+import com.liferay.user.associated.data.test.util.BaseUADDisplayTestCase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,8 +38,7 @@ import org.junit.runner.RunWith;
  * @author Noah Sherrill
  */
 @RunWith(Arquillian.class)
-public class BookmarksEntryUADEntityDisplayTest
-	extends BaseUADEntityDisplayTestCase {
+public class BookmarksEntryUADDisplayTest extends BaseUADDisplayTestCase {
 
 	@ClassRule
 	@Rule
@@ -72,8 +71,8 @@ public class BookmarksEntryUADEntityDisplayTest
 	}
 
 	@Override
-	protected UADEntityDisplay getUADEntityDisplay() {
-		return _uadEntityDisplay;
+	protected UADDisplay getUADDisplay() {
+		return _uadDisplay;
 	}
 
 	@DeleteAfterTestRun
@@ -91,6 +90,6 @@ public class BookmarksEntryUADEntityDisplayTest
 	@Inject(
 		filter = "model.class.name=" + BookmarksUADConstants.CLASS_NAME_BOOKMARKS_ENTRY
 	)
-	private UADEntityDisplay _uadEntityDisplay;
+	private UADDisplay _uadDisplay;
 
 }
