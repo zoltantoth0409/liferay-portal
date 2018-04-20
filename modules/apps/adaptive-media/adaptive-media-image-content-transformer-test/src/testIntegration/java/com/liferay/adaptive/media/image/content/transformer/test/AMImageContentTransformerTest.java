9bb77ca8f94afacd9ad0b248e4344d072976d3ef
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-04-20 10:19:47 +0200

    LPS-79815 Adding failing test

---
+++ modules/apps/adaptive-media/adaptive-media-image-content-transformer-test/src/testIntegration/java/com/liferay/adaptive/media/image/content/transformer/test/AMImageContentTransformerTest.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.image.content.transformer.test;
+
+import com.liferay.adaptive.media.content.transformer.ContentTransformerHandler;
+import com.liferay.adaptive.media.content.transformer.constants.ContentTransformerContentTypes;
+import com.liferay.adaptive.media.image.configuration.AMImageConfigurationEntry;
+import com.liferay.adaptive.media.image.configuration.AMImageConfigurationHelper;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.document.library.kernel.util.DLUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alejandro Tardín
+ */
+@RunWith(Arquillian.class)
+public class AMImageContentTransformerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		Map<String, String> properties = new HashMap<>();
+
+		properties.put("max-height", "600");
+		properties.put("max-width", "800");
+
+		_amImageConfigurationEntry =
+			_amImageConfigurationHelper.addAMImageConfigurationEntry(
+				_group.getCompanyId(), StringUtil.randomString(),
+				StringUtil.randomString(), StringUtil.randomString(),
+				properties);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_amImageConfigurationHelper.forceDeleteAMImageConfigurationEntry(
+			_group.getCompanyId(), _amImageConfigurationEntry.getUUID());
+	}
+
+	@Test
+	public void testTransformASingleImage() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group, TestPropsValues.getUserId());
+
+		FileEntry fileEntry = _addImageFileEntry(serviceContext);
+
+		String rawHTML = String.format(
+			"<img data-fileEntryId=\"%s\" src=\"%s\" />",
+			fileEntry.getFileEntryId(),
+			DLUtil.getPreviewURL(
+				fileEntry, fileEntry.getFileVersion(), null, StringPool.BLANK,
+				false, false));
+
+		String regex =
+			"<picture data-fileEntryId=\".+\">" +
+				"<source media=\"\\(max-width:.+px\\)\" srcset=\".+\" \\/>" +
+					"<source media=\"\\(max-width:.+px\\) and " +
+						"\\(min-width:.+px\\)\" srcset=\".+\" \\/>" +
+							"<img data-fileEntryId=\".+\" src=\".+\" \\/>" +
+								"<\\/picture>";
+
+		String transformedHTML = _contentTransformerHandler.transform(
+			ContentTransformerContentTypes.HTML, rawHTML);
+
+		Assert.assertTrue(transformedHTML.matches(regex));
+	}
+
+	private FileEntry _addImageFileEntry(ServiceContext serviceContext)
+		throws Exception {
+
+		return _dlAppLocalService.addFileEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString(), ContentTypes.IMAGE_JPEG,
+			FileUtil.getBytes(AMImageContentTransformerTest.class, "image.jpg"),
+			serviceContext);
+	}
+
+	private AMImageConfigurationEntry _amImageConfigurationEntry;
+
+	@Inject
+	private AMImageConfigurationHelper _amImageConfigurationHelper;
+
+	@Inject
+	private ContentTransformerHandler _contentTransformerHandler;
+
+	@Inject
+	private DLAppLocalService _dlAppLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
