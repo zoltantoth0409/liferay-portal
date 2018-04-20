a34199ba1fdd672c601a011878bce645711babed
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2018-04-20 13:55:59 +0100

    OAUTH2-124 Constants

---
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/constants/OAuth2ProviderRestEndpointConstants.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.rest.internal.endpoint.constants;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public class OAuth2ProviderRestEndpointConstants {
+
+	public static final String AUTHORIZATION_CODE_PKCE_GRANT =
+		"authorization_code_pkce";
+
+	public static final String PROPERTY_KEY_CLIENT_FEATURE_PREFIX = "FEATURE-";
+
+	public static final String PROPERTY_KEY_CLIENT_FEATURE_TOKEN_INTROSPECTION =
+		"token-introspection";
+
+	public static final String PROPERTY_KEY_CLIENT_FEATURES = "FEATURES";
+
+	public static final String PROPERTY_KEY_CLIENT_REMOTE_ADDR =
+		"CLIENT_REMOTE_ADDR";
+
+	public static final String PROPERTY_KEY_CLIENT_REMOTE_HOST =
+		"CLIENT_REMOTE_HOST";
+
+	public static final String PROPERTY_KEY_COMPANY_ID = "COMPANY_ID";
+
+	public static final String PROPERTY_KEY_OAUTH2_ENDPOINT_JAXRS_PROVIDER =
+		"oauth2.endpoint.jaxrs.provider";
+
+	public static final String PROPERTY_KEY_OAUTH2_ENDPOINT_JAXRS_RESOURCE =
+		"oauth2.endpoint.jaxrs.resource";
+
+}
