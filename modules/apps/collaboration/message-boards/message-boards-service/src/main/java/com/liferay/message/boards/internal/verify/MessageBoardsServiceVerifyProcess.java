fefa8a8189be627e4f3e64a44e43e70702ada945
Adolfo Pérez <adolfo.perez@liferay.com>
2018-04-20 12:58:26 +0200

    LPS-79492 Update references to schema version

--- modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/verify/MessageBoardsServiceVerifyProcess.java
+++ modules/apps/collaboration/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/verify/MessageBoardsServiceVerifyProcess.java
@@ -66,7 +66,7 @@ public class MessageBoardsServiceVerifyProcess extends VerifyProcess {
 	}
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.message.boards.service)(release.schema.version=1.0.1))",
+		target = "(&(release.bundle.symbolic.name=com.liferay.message.boards.service)(release.schema.version=1.1.0))",
 		unbind = "-"
 	)
 	protected void setRelease(Release release) {
