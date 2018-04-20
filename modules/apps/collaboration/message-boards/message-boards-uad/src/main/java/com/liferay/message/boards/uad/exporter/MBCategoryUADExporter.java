d2b3dc853b7f54867bf7ac137f4a3bb1f319acf9
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-04-19 17:09:47 -0700

    LPS-78666 message-boards-uad - autogenerated

--- modules/apps/collaboration/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/exporter/MBCategoryUADExporter.java
+++ modules/apps/collaboration/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/exporter/MBCategoryUADExporter.java
@@ -19,6 +19,7 @@ import com.liferay.message.boards.service.MBCategoryLocalService;
 import com.liferay.message.boards.uad.constants.MBUADConstants;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.util.StringBundler;
 
 import com.liferay.user.associated.data.exporter.DynamicQueryUADExporter;
 import com.liferay.user.associated.data.exporter.UADExporter;
@@ -41,6 +42,48 @@ public class MBCategoryUADExporter extends DynamicQueryUADExporter<MBCategory> {
 	@Override
 	protected String[] doGetUserIdFieldNames() {
 		return MBUADConstants.USER_ID_FIELD_NAMES_MB_CATEGORY;
+	}
+
+	@Override
+	protected String toXmlString(MBCategory mbCategory) {
+		StringBundler sb = new StringBundler(25);
+
+		sb.append("<model><model-name>");
+		sb.append("com.liferay.message.boards.model.MBCategory");
+		sb.append("</model-name>");
+
+		sb.append(
+			"<column><column-name>categoryId</column-name><column-value><![CDATA[");
+		sb.append(mbCategory.getCategoryId());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>statusByUserId</column-name><column-value><![CDATA[");
+		sb.append(mbCategory.getStatusByUserId());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>statusByUserName</column-name><column-value><![CDATA[");
+		sb.append(mbCategory.getStatusByUserName());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>userId</column-name><column-value><![CDATA[");
+		sb.append(mbCategory.getUserId());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>userName</column-name><column-value><![CDATA[");
+		sb.append(mbCategory.getUserName());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>name</column-name><column-value><![CDATA[");
+		sb.append(mbCategory.getName());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>description</column-name><column-value><![CDATA[");
+		sb.append(mbCategory.getDescription());
+		sb.append("]]></column-value></column>");
+
+		sb.append("</model>");
+
+		return sb.toString();
 	}
 
 	@Reference
--- modules/apps/collaboration/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/exporter/MBMessageUADExporter.java
+++ modules/apps/collaboration/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/exporter/MBMessageUADExporter.java
@@ -19,6 +19,7 @@ import com.liferay.message.boards.service.MBMessageLocalService;
 import com.liferay.message.boards.uad.constants.MBUADConstants;
 
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.util.StringBundler;
 
 import com.liferay.user.associated.data.exporter.DynamicQueryUADExporter;
 import com.liferay.user.associated.data.exporter.UADExporter;
@@ -41,6 +42,48 @@ public class MBMessageUADExporter extends DynamicQueryUADExporter<MBMessage> {
 	@Override
 	protected String[] doGetUserIdFieldNames() {
 		return MBUADConstants.USER_ID_FIELD_NAMES_MB_MESSAGE;
+	}
+
+	@Override
+	protected String toXmlString(MBMessage mbMessage) {
+		StringBundler sb = new StringBundler(25);
+
+		sb.append("<model><model-name>");
+		sb.append("com.liferay.message.boards.model.MBMessage");
+		sb.append("</model-name>");
+
+		sb.append(
+			"<column><column-name>messageId</column-name><column-value><![CDATA[");
+		sb.append(mbMessage.getMessageId());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>statusByUserId</column-name><column-value><![CDATA[");
+		sb.append(mbMessage.getStatusByUserId());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>statusByUserName</column-name><column-value><![CDATA[");
+		sb.append(mbMessage.getStatusByUserName());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>userId</column-name><column-value><![CDATA[");
+		sb.append(mbMessage.getUserId());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>userName</column-name><column-value><![CDATA[");
+		sb.append(mbMessage.getUserName());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>subject</column-name><column-value><![CDATA[");
+		sb.append(mbMessage.getSubject());
+		sb.append("]]></column-value></column>");
+		sb.append(
+			"<column><column-name>body</column-name><column-value><![CDATA[");
+		sb.append(mbMessage.getBody());
+		sb.append("]]></column-value></column>");
+
+		sb.append("</model>");
+
+		return sb.toString();
 	}
 
 	@Reference
