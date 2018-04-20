e10d4424ff0ee8a8fa8e958530e6a1872d5c9689
Alberto Chaparro <alberto.chaparro@liferay.com>
2018-04-20 15:06:58 +0200

    LPS-79466 Layout was added to be verified in LPS-38364. Sample data has to be created using the API, in that way resource permissions are created

--- portal-impl/src/com/liferay/portal/verify/model/LayoutVerifiableModel.java
+++ portal-impl/src/com/liferay/portal/verify/model/LayoutVerifiableModel.java
@@ -14,26 +14,18 @@
 
 package com.liferay.portal.verify.model;
 
-import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.verify.model.VerifiableAuditedModel;
-import com.liferay.portal.kernel.verify.model.VerifiableResourcedModel;
 import com.liferay.portal.kernel.verify.model.VerifiableUUIDModel;
 
 /**
  * @author Miguel Pastor
  */
 public class LayoutVerifiableModel
-	implements VerifiableAuditedModel, VerifiableResourcedModel,
-			   VerifiableUUIDModel {
+	implements VerifiableAuditedModel,VerifiableUUIDModel {
 
 	@Override
 	public String getJoinByTableName() {
 		return null;
-	}
-
-	@Override
-	public String getModelName() {
-		return Layout.class.getName();
 	}
 
 	@Override
@@ -54,11 +46,6 @@ public class LayoutVerifiableModel
 	@Override
 	public String getTableName() {
 		return "Layout";
-	}
-
-	@Override
-	public String getUserIdColumnName() {
-		return "userId";
 	}
 
 	@Override
