c088196bce5b9fb8f148baa1973862472d77ef9a
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-04-20 18:06:34 -0700

    LRQA-39789 Avoid unnecessary GitHub API calls when object already exists

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CommitFactory.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CommitFactory.java
@@ -16,6 +16,8 @@ package com.liferay.jenkins.results.parser;
 
 import java.io.IOException;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,9 +33,11 @@ public class CommitFactory {
 		String repositoryName = pullRequest.getRepositoryName();
 		String sha = pullRequest.getSenderSHA();
 
-		String commitURL = JenkinsResultsParserUtil.combine(
-			"https://api.github.com/repos/", gitHubUserName, "/",
-			repositoryName, "/commits/", sha);
+		String commitURL = _getCommitURL(gitHubUserName, repositoryName, sha);
+
+		if (_commits.containsKey(commitURL)) {
+			return _commits.get(commitURL);
+		}
 
 		try {
 			JSONObject jsonObject = JenkinsResultsParserUtil.toJSONObject(
@@ -65,7 +69,21 @@ public class CommitFactory {
 		String repositoryName = gitWorkingDirectory.getRepositoryName();
 		String sha = matcher.group("sha");
 
+		String commitURL = _getCommitURL(gitHubUserName, repositoryName, sha);
+
+		if (_commits.containsKey(commitURL)) {
+			return _commits.get(commitURL);
+		}
+
 		return _newCommit(gitHubUserName, message, repositoryName, sha);
+	}
+
+	private static String _getCommitURL(
+		String gitHubUserName, String repositoryName, String sha) {
+
+		return JenkinsResultsParserUtil.combine(
+			"https://api.github.com/repos/", gitHubUserName, "/",
+			repositoryName, "/commits/", sha);
 	}
 
 	private static Commit _newCommit(
@@ -78,10 +96,16 @@ public class CommitFactory {
 			type = Commit.Type.LEGACY_ARCHIVE;
 		}
 
-		return new BaseCommit(
+		Commit commit = new BaseCommit(
 			gitHubUserName, message, repositoryName, sha, type);
+
+		_commits.put(
+			_getCommitURL(gitHubUserName, repositoryName, sha), commit);
+
+		return commit;
 	}
 
+	private static final Map<String, Commit> _commits = new HashMap<>();
 	private static final Pattern _pattern = Pattern.compile(
 		"(?<sha>[0-9a-f]{40}) (?<message>.*)");
 
