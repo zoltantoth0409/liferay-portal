d596147e9d17cc9a809208584124df7849ad0c60
arthurchan35 <arthurchan35@gmail.com>
2018-04-23 13:28:06 +0800

    LPS-79348 change back to long.max_val since it's not neccesary because adding to indexin engine with a document will later be handled, so that the date will be truncate to less than 10000 year

--- modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/contributor/document/AssetEntryDocumentContributor.java
+++ modules/apps/foundation/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/contributor/document/AssetEntryDocumentContributor.java
@@ -25,7 +25,6 @@ import com.liferay.portal.kernel.search.DocumentContributor;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Date;
@@ -73,9 +72,7 @@ public class AssetEntryDocumentContributor implements DocumentContributor {
 				Field.EXPIRATION_DATE, assetEntry.getExpirationDate());
 		}
 		else {
-			document.addDate(
-				Field.EXPIRATION_DATE,
-				new Date(System.currentTimeMillis() + (Time.YEAR * 1000)));
+			document.addDate(Field.EXPIRATION_DATE, new Date(Long.MAX_VALUE));
 		}
 
 		if (!document.hasField(Field.MODIFIED_DATE)) {
