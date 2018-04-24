1282d69baff4611441d723970b587c236f24a61d
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2018-04-24 10:36:01 +0100

    OAUTH2-124 Access token service & registrator

---
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/LiferayAccessTokenService.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.rest.internal.endpoint.access.token;
+
+import com.liferay.oauth2.provider.rest.internal.endpoint.constants.OAuth2ProviderRestEndpointConstants;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.cxf.jaxrs.ext.MessageContext;
+import org.apache.cxf.rs.security.oauth2.common.Client;
+import org.apache.cxf.rs.security.oauth2.services.AccessTokenService;
+
+/**
+ * @author Tomas Polesovsky
+ */
+@Path("/token")
+public class LiferayAccessTokenService extends AccessTokenService {
+
+	@Override
+	protected Client authenticateClientIfNeeded(
+		MultivaluedMap<String, String> params) {
+
+		Client client = super.authenticateClientIfNeeded(params);
+
+		Map<String, String> clientProperties = client.getProperties();
+
+		MessageContext messageContext = getMessageContext();
+
+		HttpServletRequest httpServletRequest =
+			messageContext.getHttpServletRequest();
+
+		String remoteAddr = httpServletRequest.getRemoteAddr();
+
+		String remoteHost = httpServletRequest.getRemoteHost();
+
+		try {
+			InetAddress inetAddress = InetAddress.getByName(remoteAddr);
+
+			remoteHost = inetAddress.getCanonicalHostName();
+		}
+		catch (UnknownHostException uhe) {
+		}
+
+		clientProperties.put(
+			OAuth2ProviderRestEndpointConstants.PROPERTY_KEY_CLIENT_REMOTE_ADDR,
+			remoteAddr);
+		clientProperties.put(
+			OAuth2ProviderRestEndpointConstants.PROPERTY_KEY_CLIENT_REMOTE_HOST,
+			remoteHost);
+
+		return client;
+	}
+
+}
---
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/LiferayAccessTokenServiceRegistrator.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.rest.internal.endpoint.access.token;
+
+import com.liferay.oauth2.provider.rest.internal.endpoint.constants.OAuth2ProviderRestEndpointConstants;
+import com.liferay.oauth2.provider.rest.internal.endpoint.liferay.LiferayOAuthDataProvider;
+import com.liferay.portal.kernel.util.MapUtil;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.cxf.rs.security.oauth2.provider.AccessTokenGrantHandler;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author Tomas Polesovsky
+ */
+@Component(
+	immediate = true,
+	property = {
+		"block.unsecure.requests=true", "can.support.public.clients=true",
+		"enabled=true"
+	}
+)
+public class LiferayAccessTokenServiceRegistrator {
+
+	@Activate
+	public void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		if (!MapUtil.getBoolean(properties, "enabled", true)) {
+			return;
+		}
+
+		LiferayAccessTokenService liferayAccessTokenService =
+			new LiferayAccessTokenService();
+
+		liferayAccessTokenService.setBlockUnsecureRequests(
+			MapUtil.getBoolean(properties, "block.unsecure.requests", true));
+		liferayAccessTokenService.setCanSupportPublicClients(
+			MapUtil.getBoolean(properties, "allow.public.clients", true));
+		liferayAccessTokenService.setDataProvider(_liferayOAuthDataProvider);
+		liferayAccessTokenService.setGrantHandlers(_accessTokenGrantHandlers);
+
+		Dictionary<String, Object> endpointProperties = new Hashtable<>();
+
+		endpointProperties.put(
+			OAuth2ProviderRestEndpointConstants.
+				PROPERTY_KEY_OAUTH2_ENDPOINT_JAXRS_RESOURCE,
+			true);
+
+		_serviceRegistration = bundleContext.registerService(
+			Object.class, liferayAccessTokenService, endpointProperties);
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.AT_LEAST_ONE,
+		policyOption = ReferencePolicyOption.GREEDY,
+		unbind = "removeAccessTokenGrantHandler"
+	)
+	public void addAccessTokenGrantHandler(
+		AccessTokenGrantHandler accessTokenGrantHandler) {
+
+		_accessTokenGrantHandlers.add(accessTokenGrantHandler);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		if (_serviceRegistration != null) {
+			_serviceRegistration.unregister();
+		}
+	}
+
+	public void removeAccessTokenGrantHandler(
+		AccessTokenGrantHandler accessTokenGrantHandler) {
+
+		_accessTokenGrantHandlers.remove(accessTokenGrantHandler);
+	}
+
+	private final List<AccessTokenGrantHandler> _accessTokenGrantHandlers =
+		new ArrayList<>();
+
+	@Reference
+	private LiferayOAuthDataProvider _liferayOAuthDataProvider;
+
+	private ServiceRegistration<Object> _serviceRegistration;
+
+}
