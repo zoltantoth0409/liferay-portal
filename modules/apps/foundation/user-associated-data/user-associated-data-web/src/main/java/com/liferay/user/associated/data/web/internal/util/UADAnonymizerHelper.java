63e94ea37f31c0de6ba56cb2c368c4c70c45e034
drewbrokke <drew.brokke@liferay.com>
2018-04-24 16:42:32 -0700

    LPS-80125 user-associated-data-web - moves UADAnonymizerHelper to the web module

--- modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADAnonymizerHelper.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADAnonymizerHelper.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.user.associated.data.util;
+package com.liferay.user.associated.data.web.internal.util;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
