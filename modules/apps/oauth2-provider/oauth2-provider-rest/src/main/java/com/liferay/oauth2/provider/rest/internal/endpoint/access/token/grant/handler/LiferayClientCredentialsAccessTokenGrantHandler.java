cd062b6383651baf3e3d7efc9c60f9e75719cf30
Carlos Sierra <carlos.sierra@liferay.com>
2018-04-25 18:08:19 +0200

    OAUTH2-124 No need to be immediate

--- modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/grant/handler/LiferayClientCredentialsAccessTokenGrantHandler.java
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/access/token/grant/handler/LiferayClientCredentialsAccessTokenGrantHandler.java
@@ -36,7 +36,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	configurationPid = "com.liferay.oauth2.provider.configuration.OAuth2ProviderConfiguration",
-	immediate = true, service = AccessTokenGrantHandler.class
+	service = AccessTokenGrantHandler.class
 )
 public class LiferayClientCredentialsAccessTokenGrantHandler
 	extends BaseAccessTokenGrantHandler {
