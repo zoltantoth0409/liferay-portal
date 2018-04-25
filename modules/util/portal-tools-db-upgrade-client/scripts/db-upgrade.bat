93c3d5f6ce3414652510b9adc9e5c5ddcdddc484
Jorge Díaz <jorge.diaz@liferay.com>
2018-04-25 20:34:24 +0200

    LPS-77354 Add db-upgrade.bat and db-upgrade.sh scripts

---
+++ modules/util/portal-tools-db-upgrade-client/scripts/db-upgrade.bat
@@ -0,0 +1,11 @@
+@echo off
+
+pushd "%~dp0"
+
+path %PATH%;%JAVA_HOME%\bin\
+
+java -jar com.liferay.portal.tools.db.upgrade.client.jar %*
+
+popd
+
+@echo on
---
+++ modules/util/portal-tools-db-upgrade-client/scripts/db-upgrade.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xv
+if [ -e /proc/$$/fd/255 ]; then
+	scriptpath=`readlink /proc/$$/fd/255 2>/dev/null`
+fi
+
+if [ ! -n "$scriptpath" ]; then
+	scriptpath="$0"
+fi
+
+cd "$(dirname "$scriptpath")"
+
+java -jar com.liferay.portal.tools.db.upgrade.client.jar "$@"
