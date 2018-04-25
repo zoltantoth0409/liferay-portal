8932c1755d5021bf0707425c7de612731c844c8f
Alessio Rendina <asilcomando@gmail.com>
2018-04-25 19:53:47 +0200

    COMMERCE-304 Added commerce discount rule local and remote service implementation

--- modules/dxp/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/service/impl/CommerceDiscountRuleLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/service/impl/CommerceDiscountRuleLocalServiceImpl.java
@@ -14,11 +14,67 @@
 
 package com.liferay.commerce.discount.service.impl;
 
+import com.liferay.commerce.discount.model.CommerceDiscountRule;
 import com.liferay.commerce.discount.service.base.CommerceDiscountRuleLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 /**
  * @author Marco Leo
+ * @author Alessio Antonio Rendina
  */
 public class CommerceDiscountRuleLocalServiceImpl
 	extends CommerceDiscountRuleLocalServiceBaseImpl {
+
+	@Override
+	public CommerceDiscountRule addCommerceDiscountRule(
+			long commerceDiscountId, String type, String typeSettings,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		long commerceDiscountRuleId = counterLocalService.increment();
+
+		CommerceDiscountRule commerceDiscountRule =
+			commerceDiscountRulePersistence.create(commerceDiscountRuleId);
+
+		commerceDiscountRule.setGroupId(groupId);
+		commerceDiscountRule.setCompanyId(user.getCompanyId());
+		commerceDiscountRule.setUserId(user.getUserId());
+		commerceDiscountRule.setUserName(user.getFullName());
+		commerceDiscountRule.setCommerceDiscountId(commerceDiscountId);
+		commerceDiscountRule.setType(type);
+		commerceDiscountRule.setTypeSettings(typeSettings);
+
+		commerceDiscountRulePersistence.update(commerceDiscountRule);
+
+		return commerceDiscountRule;
+	}
+
+	@Override
+	public void deleteCommerceDiscountRules(long commerceDiscountId) {
+		commerceDiscountRulePersistence.removeBycommerceDiscountId(
+			commerceDiscountId);
+	}
+
+	@Override
+	public CommerceDiscountRule updateCommerceDiscountRule(
+			long commerceDiscountRuleId, String type, String typeSettings)
+		throws PortalException {
+
+		CommerceDiscountRule commerceDiscountRule =
+			commerceDiscountRulePersistence.findByPrimaryKey(
+				commerceDiscountRuleId);
+
+		commerceDiscountRule.setType(type);
+		commerceDiscountRule.setTypeSettings(typeSettings);
+
+		commerceDiscountRulePersistence.update(commerceDiscountRule);
+
+		return commerceDiscountRule;
+	}
+
 }
--- modules/dxp/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/service/impl/CommerceDiscountRuleServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/service/impl/CommerceDiscountRuleServiceImpl.java
@@ -14,11 +14,75 @@
 
 package com.liferay.commerce.discount.service.impl;
 
+import com.liferay.commerce.discount.constants.CommerceDiscountActionKeys;
+import com.liferay.commerce.discount.model.CommerceDiscount;
+import com.liferay.commerce.discount.model.CommerceDiscountRule;
 import com.liferay.commerce.discount.service.base.CommerceDiscountRuleServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 /**
  * @author Marco Leo
+ * @author Alessio Antonio Rendina
  */
 public class CommerceDiscountRuleServiceImpl
 	extends CommerceDiscountRuleServiceBaseImpl {
+
+	@Override
+	public CommerceDiscountRule addCommerceDiscountRule(
+			long commerceDiscountId, String type, String typeSettings,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		_commerceDiscountResourcePermission.check(
+			getPermissionChecker(), commerceDiscountId,
+			CommerceDiscountActionKeys.MANAGE_COMMERCE_DISCOUNT_RULES);
+
+		return commerceDiscountRuleLocalService.addCommerceDiscountRule(
+			commerceDiscountId, type, typeSettings, serviceContext);
+	}
+
+	@Override
+	public void deleteCommerceDiscountRule(long commerceDiscountRuleId)
+		throws PortalException {
+
+		CommerceDiscountRule commerceDiscountRule =
+			commerceDiscountRuleLocalService.getCommerceDiscountRule(
+				commerceDiscountRuleId);
+
+		_commerceDiscountResourcePermission.check(
+			getPermissionChecker(),
+			commerceDiscountRule.getCommerceDiscountId(),
+			CommerceDiscountActionKeys.MANAGE_COMMERCE_DISCOUNT_RULES);
+
+		commerceDiscountRuleLocalService.deleteCommerceDiscountRule(
+			commerceDiscountRule);
+	}
+
+	@Override
+	public CommerceDiscountRule updateCommerceDiscountRule(
+			long commerceDiscountRuleId, String type, String typeSettings)
+		throws PortalException {
+
+		CommerceDiscountRule commerceDiscountRule =
+			commerceDiscountRuleLocalService.getCommerceDiscountRule(
+				commerceDiscountRuleId);
+
+		_commerceDiscountResourcePermission.check(
+			getPermissionChecker(),
+			commerceDiscountRule.getCommerceDiscountId(),
+			CommerceDiscountActionKeys.MANAGE_COMMERCE_DISCOUNT_RULES);
+
+		return commerceDiscountRuleLocalService.updateCommerceDiscountRule(
+			commerceDiscountRuleId, type, typeSettings);
+	}
+
+	private static volatile ModelResourcePermission<CommerceDiscount>
+		_commerceDiscountResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				CommerceDiscountRuleServiceImpl.class,
+				"_commerceDiscountResourcePermission", CommerceDiscount.class);
+
 }
