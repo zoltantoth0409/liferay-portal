4c267ec71238d3767bca71a3ca1fb70a9f740fca
shuyangzhou <shuyang.zhou@liferay.com>
2018-04-26 10:00:08 -0700

    LPS-79753 Deprecated kernel AutoDeleteFileInputStream

--- portal-kernel/src/com/liferay/portal/kernel/io/AutoDeleteFileInputStream.java
+++ portal-kernel/src/com/liferay/portal/kernel/io/AutoDeleteFileInputStream.java
@@ -21,6 +21,8 @@ import java.io.IOException;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of 7.0.0, replaced by {@link com.liferay.petra.io.
+ * 		   AutoDeleteFileInputStream}
  */
 public class AutoDeleteFileInputStream extends FileInputStream {
 
