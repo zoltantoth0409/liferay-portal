d2d8c86d53225b42857aedd1d27134319931b90d
Hugo Huijser <hugo.huijser@liferay.com>
2018-04-26 15:18:03 +0800

    LPS-77425 Strip fully qualified class names first so we can see which imports we need and update imports variable when the content is changed so the other transformations do not squash the change. (reverted from commit 3269a0b15b6fae98cadb6cdc1cb724ce15b93965)

--- portal-impl/src/com/liferay/portal/tools/JavaImportsFormatter.java
+++ portal-impl/src/com/liferay/portal/tools/JavaImportsFormatter.java
@@ -63,22 +63,20 @@ public class JavaImportsFormatter extends BaseImportsFormatter {
 			return content;
 		}
 
-		String newContent = ToolsUtil.stripFullyQualifiedClassNames(
-			content, imports, packagePath);
-
-		if (!content.equals(newContent)) {
-			imports = getImports(newContent);
-		}
-
 		String newImports = stripUnusedImports(
-			imports, newContent, packagePath, className, "\\*");
+			imports, content, packagePath, className, "\\*");
 
 		newImports = sortAndGroupImports(newImports);
 
-		newContent = StringUtil.replaceFirst(newContent, imports, newImports);
+		if (!imports.equals(newImports)) {
+			content = StringUtil.replaceFirst(content, imports, newImports);
+		}
 
-		return newContent.replaceFirst(
+		content = content.replaceFirst(
 			"(?m)^[ \t]*(package .*;)\\s*^[ \t]*import", "$1\n\nimport");
+
+		return ToolsUtil.stripFullyQualifiedClassNames(
+			content, newImports, packagePath);
 	}
 
 	private static final Pattern _importsPattern = Pattern.compile(
