385adda6afc60073076b02aeb62364e829c181cf
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-04-26 16:35:14 -0700

    LPS-80123 blogs-uad - autogenerated

--- modules/apps/collaboration/blogs/blogs-uad/src/main/java/com/liferay/blogs/uad/exporter/BlogsEntryUADExporter.java
+++ modules/apps/collaboration/blogs/blogs-uad/src/main/java/com/liferay/blogs/uad/exporter/BlogsEntryUADExporter.java
@@ -14,18 +14,11 @@
 
 package com.liferay.blogs.uad.exporter;
 
-import com.liferay.blogs.model.BlogsEntry;
-import com.liferay.blogs.service.BlogsEntryLocalService;
 import com.liferay.blogs.uad.constants.BlogsUADConstants;
 
-import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
-import com.liferay.portal.kernel.util.StringBundler;
-
-import com.liferay.user.associated.data.exporter.DynamicQueryUADExporter;
 import com.liferay.user.associated.data.exporter.UADExporter;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -33,79 +26,5 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(immediate = true, property =  {
 	"model.class.name=" + BlogsUADConstants.CLASS_NAME_BLOGS_ENTRY}, service = UADExporter.class)
-public class BlogsEntryUADExporter extends DynamicQueryUADExporter<BlogsEntry> {
-	@Override
-	protected ActionableDynamicQuery doGetActionableDynamicQuery() {
-		return _blogsEntryLocalService.getActionableDynamicQuery();
-	}
-
-	@Override
-	protected String[] doGetUserIdFieldNames() {
-		return BlogsUADConstants.USER_ID_FIELD_NAMES_BLOGS_ENTRY;
-	}
-
-	@Override
-	protected String toXmlString(BlogsEntry blogsEntry) {
-		StringBundler sb = new StringBundler(40);
-
-		sb.append("<model><model-name>");
-		sb.append("com.liferay.blogs.model.BlogsEntry");
-		sb.append("</model-name>");
-
-		sb.append(
-			"<column><column-name>entryId</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getEntryId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>statusByUserId</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getStatusByUserId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>statusByUserName</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getStatusByUserName());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>userId</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getUserId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>userName</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getUserName());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>title</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getTitle());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>subtitle</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getSubtitle());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>urlTitle</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getUrlTitle());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>description</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getDescription());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>content</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getContent());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>smallImage</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getSmallImage());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>smallImageId</column-name><column-value><![CDATA[");
-		sb.append(blogsEntry.getSmallImageId());
-		sb.append("]]></column-value></column>");
-
-		sb.append("</model>");
-
-		return sb.toString();
-	}
-
-	@Reference
-	private BlogsEntryLocalService _blogsEntryLocalService;
+public class BlogsEntryUADExporter extends BaseBlogsEntryUADExporter {
 }
