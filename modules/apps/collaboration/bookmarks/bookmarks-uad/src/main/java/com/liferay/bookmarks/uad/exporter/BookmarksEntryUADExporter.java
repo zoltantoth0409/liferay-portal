462b0f262dd8a90c9216a5d17e5349933e18c3c0
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-04-26 14:55:24 -0700

    LPS-80123 (bookmarks-uad) Add base implementation class for UADExporter

--- modules/apps/collaboration/bookmarks/bookmarks-uad/src/main/java/com/liferay/bookmarks/uad/exporter/BookmarksEntryUADExporter.java
+++ modules/apps/collaboration/bookmarks/bookmarks-uad/src/main/java/com/liferay/bookmarks/uad/exporter/BookmarksEntryUADExporter.java
@@ -14,16 +14,10 @@
 
 package com.liferay.bookmarks.uad.exporter;
 
-import com.liferay.bookmarks.model.BookmarksEntry;
-import com.liferay.bookmarks.service.BookmarksEntryLocalService;
 import com.liferay.bookmarks.uad.constants.BookmarksUADConstants;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
-import com.liferay.user.associated.data.exporter.DynamicQueryUADExporter;
 import com.liferay.user.associated.data.exporter.UADExporter;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Noah Sherrill
@@ -33,73 +27,5 @@ import org.osgi.service.component.annotations.Reference;
 	property = "model.class.name=" + BookmarksUADConstants.CLASS_NAME_BOOKMARKS_ENTRY,
 	service = UADExporter.class
 )
-public class BookmarksEntryUADExporter
-	extends DynamicQueryUADExporter<BookmarksEntry> {
-
-	@Override
-	protected ActionableDynamicQuery doGetActionableDynamicQuery() {
-		return _bookmarksEntryLocalService.getActionableDynamicQuery();
-	}
-
-	@Override
-	protected String[] doGetUserIdFieldNames() {
-		return BookmarksUADConstants.USER_ID_FIELD_NAMES_BOOKMARKS_ENTRY;
-	}
-
-	@Override
-	protected String toXmlString(BookmarksEntry bookmarksEntry) {
-		StringBundler sb = new StringBundler(36);
-
-		sb.append("<model><model-name>");
-		sb.append("com.liferay.bookmarks.model.BookmarksEntry");
-		sb.append("</model-name>");
-
-		sb.append("<column><column-name>entryId</column-name>");
-		sb.append("<column-value><![CDATA[");
-		sb.append(bookmarksEntry.getEntryId());
-		sb.append("]]></column-value></column>");
-
-		sb.append("<column><column-name>userId</column-name>");
-		sb.append("<column-value><![CDATA[");
-		sb.append(bookmarksEntry.getUserId());
-		sb.append("]]></column-value></column>");
-
-		sb.append("<column><column-name>userName</column-name>");
-		sb.append("<column-value><![CDATA[");
-		sb.append(bookmarksEntry.getUserName());
-		sb.append("]]></column-value></column>");
-
-		sb.append("<column><column-name>statusByUserId</column-name>");
-		sb.append("<column-value><![CDATA[");
-		sb.append(bookmarksEntry.getStatusByUserId());
-		sb.append("]]></column-value></column>");
-
-		sb.append("<column><column-name>statusByUserName</column-name>");
-		sb.append("<column-value><![CDATA[");
-		sb.append(bookmarksEntry.getStatusByUserName());
-		sb.append("]]></column-value></column>");
-
-		sb.append("<column><column-name>name</column-name>");
-		sb.append("<column-value><![CDATA[");
-		sb.append(bookmarksEntry.getName());
-		sb.append("]]></column-value></column>");
-
-		sb.append("<column><column-name>url</column-name>");
-		sb.append("<column-value><![CDATA[");
-		sb.append(bookmarksEntry.getUrl());
-		sb.append("]]></column-value></column>");
-
-		sb.append("<column><column-name>description</column-name>");
-		sb.append("<column-value><![CDATA[");
-		sb.append(bookmarksEntry.getDescription());
-		sb.append("]]></column-value></column>");
-
-		sb.append("</model>");
-
-		return sb.toString();
-	}
-
-	@Reference
-	private BookmarksEntryLocalService _bookmarksEntryLocalService;
-
+public class BookmarksEntryUADExporter extends BaseBookmarksEntryUADExporter {
 }
