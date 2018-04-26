b2c5fc6f06864d77dcfd33f3e7a86b2eac620e84
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-04-26 16:36:52 -0700

    LPS-80123 wiki-uad - autogenerated

--- modules/apps/collaboration/wiki/wiki-uad/src/main/java/com/liferay/wiki/uad/exporter/WikiNodeUADExporter.java
+++ modules/apps/collaboration/wiki/wiki-uad/src/main/java/com/liferay/wiki/uad/exporter/WikiNodeUADExporter.java
@@ -14,18 +14,11 @@
 
 package com.liferay.wiki.uad.exporter;
 
-import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
-import com.liferay.portal.kernel.util.StringBundler;
-
-import com.liferay.user.associated.data.exporter.DynamicQueryUADExporter;
 import com.liferay.user.associated.data.exporter.UADExporter;
 
-import com.liferay.wiki.model.WikiNode;
-import com.liferay.wiki.service.WikiNodeLocalService;
 import com.liferay.wiki.uad.constants.WikiUADConstants;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -33,59 +26,5 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(immediate = true, property =  {
 	"model.class.name=" + WikiUADConstants.CLASS_NAME_WIKI_NODE}, service = UADExporter.class)
-public class WikiNodeUADExporter extends DynamicQueryUADExporter<WikiNode> {
-	@Override
-	protected ActionableDynamicQuery doGetActionableDynamicQuery() {
-		return _wikiNodeLocalService.getActionableDynamicQuery();
-	}
-
-	@Override
-	protected String[] doGetUserIdFieldNames() {
-		return WikiUADConstants.USER_ID_FIELD_NAMES_WIKI_NODE;
-	}
-
-	@Override
-	protected String toXmlString(WikiNode wikiNode) {
-		StringBundler sb = new StringBundler(25);
-
-		sb.append("<model><model-name>");
-		sb.append("com.liferay.wiki.model.WikiNode");
-		sb.append("</model-name>");
-
-		sb.append(
-			"<column><column-name>nodeId</column-name><column-value><![CDATA[");
-		sb.append(wikiNode.getNodeId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>statusByUserId</column-name><column-value><![CDATA[");
-		sb.append(wikiNode.getStatusByUserId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>statusByUserName</column-name><column-value><![CDATA[");
-		sb.append(wikiNode.getStatusByUserName());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>userId</column-name><column-value><![CDATA[");
-		sb.append(wikiNode.getUserId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>userName</column-name><column-value><![CDATA[");
-		sb.append(wikiNode.getUserName());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>name</column-name><column-value><![CDATA[");
-		sb.append(wikiNode.getName());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>description</column-name><column-value><![CDATA[");
-		sb.append(wikiNode.getDescription());
-		sb.append("]]></column-value></column>");
-
-		sb.append("</model>");
-
-		return sb.toString();
-	}
-
-	@Reference
-	private WikiNodeLocalService _wikiNodeLocalService;
+public class WikiNodeUADExporter extends BaseWikiNodeUADExporter {
 }
--- modules/apps/collaboration/wiki/wiki-uad/src/main/java/com/liferay/wiki/uad/exporter/WikiPageUADExporter.java
+++ modules/apps/collaboration/wiki/wiki-uad/src/main/java/com/liferay/wiki/uad/exporter/WikiPageUADExporter.java
@@ -14,18 +14,11 @@
 
 package com.liferay.wiki.uad.exporter;
 
-import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
-import com.liferay.portal.kernel.util.StringBundler;
-
-import com.liferay.user.associated.data.exporter.DynamicQueryUADExporter;
 import com.liferay.user.associated.data.exporter.UADExporter;
 
-import com.liferay.wiki.model.WikiPage;
-import com.liferay.wiki.service.WikiPageLocalService;
 import com.liferay.wiki.uad.constants.WikiUADConstants;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -33,63 +26,5 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(immediate = true, property =  {
 	"model.class.name=" + WikiUADConstants.CLASS_NAME_WIKI_PAGE}, service = UADExporter.class)
-public class WikiPageUADExporter extends DynamicQueryUADExporter<WikiPage> {
-	@Override
-	protected ActionableDynamicQuery doGetActionableDynamicQuery() {
-		return _wikiPageLocalService.getActionableDynamicQuery();
-	}
-
-	@Override
-	protected String[] doGetUserIdFieldNames() {
-		return WikiUADConstants.USER_ID_FIELD_NAMES_WIKI_PAGE;
-	}
-
-	@Override
-	protected String toXmlString(WikiPage wikiPage) {
-		StringBundler sb = new StringBundler(28);
-
-		sb.append("<model><model-name>");
-		sb.append("com.liferay.wiki.model.WikiPage");
-		sb.append("</model-name>");
-
-		sb.append(
-			"<column><column-name>pageId</column-name><column-value><![CDATA[");
-		sb.append(wikiPage.getPageId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>statusByUserId</column-name><column-value><![CDATA[");
-		sb.append(wikiPage.getStatusByUserId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>statusByUserName</column-name><column-value><![CDATA[");
-		sb.append(wikiPage.getStatusByUserName());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>userId</column-name><column-value><![CDATA[");
-		sb.append(wikiPage.getUserId());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>userName</column-name><column-value><![CDATA[");
-		sb.append(wikiPage.getUserName());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>title</column-name><column-value><![CDATA[");
-		sb.append(wikiPage.getTitle());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>content</column-name><column-value><![CDATA[");
-		sb.append(wikiPage.getContent());
-		sb.append("]]></column-value></column>");
-		sb.append(
-			"<column><column-name>summary</column-name><column-value><![CDATA[");
-		sb.append(wikiPage.getSummary());
-		sb.append("]]></column-value></column>");
-
-		sb.append("</model>");
-
-		return sb.toString();
-	}
-
-	@Reference
-	private WikiPageLocalService _wikiPageLocalService;
+public class WikiPageUADExporter extends BaseWikiPageUADExporter {
 }
