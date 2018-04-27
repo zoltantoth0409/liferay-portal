738b91383074c8b499a3bcb84cc879515ed71ac9
Alberto Chaparro <alberto.chaparro@liferay.com>
2018-04-27 15:28:54 +0200

    LPS-79881 Not longer needed

--- portal-impl/src/com/liferay/portal/verify/VerifyProcessSuite.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyProcessSuite.java
@@ -23,8 +23,6 @@ public class VerifyProcessSuite extends VerifyProcess {
 	protected void doVerify() throws Exception {
 		verify(new VerifyProperties());
 
-		verify(new VerifyUUID());
-
 		verify(new VerifyPermission());
 		verify(new VerifyGroup());
 		verify(new VerifyRole());
