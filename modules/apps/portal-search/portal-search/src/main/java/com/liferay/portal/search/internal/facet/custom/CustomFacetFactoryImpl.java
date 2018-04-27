7e0af71f66d03b470335b4a1a9831b59dea25cd4
weicao <wade.cao@liferay.com>
2018-04-27 11:13:31 -0700

    LPS-80216 portal-search: Add CustomFacetFactoryImpl implementation
    https://issues.liferay.com/browse/LPS-80216
    
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>
    Co-authored-by: weicao <wade.cao@liferay.com>

---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/facet/custom/CustomFacetFactoryImpl.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.facet.custom;
+
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.facet.util.FacetFactory;
+import com.liferay.portal.search.facet.Facet;
+import com.liferay.portal.search.facet.custom.CustomFacetFactory;
+import com.liferay.portal.search.internal.facet.FacetImpl;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Wade Cao
+ */
+@Component(
+	immediate = true, service = {CustomFacetFactory.class, FacetFactory.class}
+)
+public class CustomFacetFactoryImpl implements CustomFacetFactory {
+
+	@Override
+	public String getFacetClassName() {
+		return CustomFacetFactory.class.getName();
+	}
+
+	@Override
+	public Facet newInstance(SearchContext searchContext) {
+		return new FacetImpl(null, searchContext);
+	}
+
+}
