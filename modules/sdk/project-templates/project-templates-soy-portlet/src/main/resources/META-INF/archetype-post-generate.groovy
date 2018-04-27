ac84e624ba1ef7cc6c2d67744f385f461ea918b4
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2018-04-27 16:48:23 -0700

    LPS-79496 SF

--- modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/META-INF/archetype-post-generate.groovy
+++ modules/sdk/project-templates/project-templates-soy-portlet/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -15,6 +15,7 @@
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+
 import java.util.List
 
 Path projectPath = Paths.get(request.outputDirectory, request.artifactId)
