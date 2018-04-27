1640a13a43477c058d416c1e0ccacba792e2a1e2
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-04-27 11:02:00 -0700

    LPS-80123 (contacts-uad) Remove @generated annotaiton from UADExporter

--- modules/apps/foundation/contacts/contacts-uad/src/main/java/com/liferay/contacts/uad/exporter/EntryUADExporter.java
+++ modules/apps/foundation/contacts/contacts-uad/src/main/java/com/liferay/contacts/uad/exporter/EntryUADExporter.java
@@ -15,16 +15,17 @@
 package com.liferay.contacts.uad.exporter;
 
 import com.liferay.contacts.uad.constants.ContactsUADConstants;
-
 import com.liferay.user.associated.data.exporter.UADExporter;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Brian Wing Shun Chan
- * @generated
  */
-@Component(immediate = true, property =  {
-	"model.class.name=" + ContactsUADConstants.CLASS_NAME_ENTRY}, service = UADExporter.class)
+@Component(
+	immediate = true,
+	property = "model.class.name=" + ContactsUADConstants.CLASS_NAME_ENTRY,
+	service = UADExporter.class
+)
 public class EntryUADExporter extends BaseEntryUADExporter {
 }
