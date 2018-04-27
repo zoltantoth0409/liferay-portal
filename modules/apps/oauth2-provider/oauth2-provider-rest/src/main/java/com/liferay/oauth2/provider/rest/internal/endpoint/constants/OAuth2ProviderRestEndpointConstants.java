6d6ce105f2b89fbe482a4ef41e53550edbb6f153
Brian Chan <brian.chan@liferay.com>
2018-04-26 21:49:09 -0700

    OAUTH2-124 Hugo SF

--- modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/constants/OAuth2ProviderRestEndpointConstants.java
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/constants/OAuth2ProviderRestEndpointConstants.java
@@ -19,7 +19,9 @@ package com.liferay.oauth2.provider.rest.internal.endpoint.constants;
  */
 public class OAuth2ProviderRestEndpointConstants {
 
-	/* Embraces CXF OAuthConstants naming + RFC grant_type value formats */
+	/**
+	 * Embraces CXF OAuthConstants naming + RFC grant_type value formats.
+	 */
 	public static final String AUTHORIZATION_CODE_PKCE_GRANT =
 		"authorization_code_pkce";
 
