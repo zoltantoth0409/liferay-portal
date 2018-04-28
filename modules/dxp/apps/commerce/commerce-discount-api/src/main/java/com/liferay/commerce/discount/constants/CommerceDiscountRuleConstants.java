abaf1fe85dad8699444ef323672b3bcfa97547f8
Alessio Rendina <asilcomando@gmail.com>
2018-04-28 19:20:36 +0200

    COMMERCE-304 Added new constants

---
+++ modules/dxp/apps/commerce/commerce-discount-api/src/main/java/com/liferay/commerce/discount/constants/CommerceDiscountRuleConstants.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.discount.constants;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceDiscountRuleConstants {
+
+	public static final String TYPE_CART_TOTAL = "cart-total";
+
+	public static final String TYPE_HAD_PURCHASED_ALL_OF_THESE_PRODUCTS =
+		"had-purchased-all-of-these-products";
+
+	public static final String TYPE_HAD_PURCHASED_ONE_OF_THESE_PRODUCTS =
+		"had-purchased-one-of-these-products";
+
+	public static final String TYPE_HAD_SPENT_AMOUNT = "had-spent-amount";
+
+	public static final String TYPE_HAS_ALL_OF_THESE_PRODUCTS =
+		"has-all-of-these-products";
+
+	public static final String TYPE_HAS_ONE_OF_THESE_PRODUCTS =
+		"has-one-of-these-products";
+
+}
---
+++ modules/dxp/apps/commerce/commerce-discount-api/src/main/java/com/liferay/commerce/discount/constants/CommerceDiscountTargetConstants.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.discount.constants;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceDiscountTargetConstants {
+
+	public static final String TYPE_CATEGORIES = "categories";
+
+	public static final String TYPE_PRODUCT = "product";
+
+	public static final String TYPE_SHIPMENT = "shipment";
+
+	public static final String TYPE_SUBTOTAL = "subtotal";
+
+	public static final String TYPE_TOTAL = "total";
+
+}
