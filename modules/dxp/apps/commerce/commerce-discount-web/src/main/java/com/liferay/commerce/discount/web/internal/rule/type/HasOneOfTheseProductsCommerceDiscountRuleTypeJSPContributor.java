db8114fa311ada748cf78a73bdf551461e1ef2e4
Alessio Rendina <asilcomando@gmail.com>
2018-04-28 19:34:59 +0200

    COMMERCE-304 Added commerce discount rule type jsp contributor implementations

---
+++ modules/dxp/apps/commerce/commerce-discount-web/src/main/java/com/liferay/commerce/discount/web/internal/rule/type/HasOneOfTheseProductsCommerceDiscountRuleTypeJSPContributor.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.discount.web.internal.rule.type;
+
+import com.liferay.commerce.discount.constants.CommerceDiscountRuleConstants;
+import com.liferay.commerce.discount.rule.type.CommerceDiscountRuleTypeJSPContributor;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = "commerce.discount.rule.type.jsp.contributor.key=" + CommerceDiscountRuleConstants.TYPE_HAS_ONE_OF_THESE_PRODUCTS
+)
+public class HasOneOfTheseProductsCommerceDiscountRuleTypeJSPContributor
+	implements CommerceDiscountRuleTypeJSPContributor {
+
+	public void render(
+			long commerceDiscountId, long commerceDiscountRuleId,
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/contributor/products.jsp");
+	}
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.discount.web)"
+	)
+	private ServletContext _servletContext;
+
+}
---
+++ modules/dxp/apps/commerce/commerce-discount-web/src/main/resources/META-INF/resources/contributor/spent_amount.jsp
@@ -0,0 +1,23 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+CommerceDiscountRuleDisplayContext commerceDiscountRuleDisplayContext = (CommerceDiscountRuleDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+%>
+
+<aui:input label="spent-amount" name="typeSettings" suffix="<%= commerceDiscountRuleDisplayContext.getDefaultCommerceCurrencyCode() %>" type="text" value="<%= commerceDiscountRuleDisplayContext.getTypeSettings() %>" />
