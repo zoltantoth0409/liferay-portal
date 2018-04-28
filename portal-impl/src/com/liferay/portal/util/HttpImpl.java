47f8f73a29734f47cd372aef56cd20eecaf1d785
shuyangzhou <shuyang.zhou@liferay.com>
2018-04-27 20:14:10 -0700

    Revert "LPS-79982 use length of encoded URL when shortening URLs"
    This reverts commit 9ce67658cfec798c42e136e630117f8155d11ae4.

--- portal-impl/src/com/liferay/portal/util/HttpImpl.java
+++ portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -2077,8 +2077,6 @@ public class HttpImpl implements Http {
 
 			String redirect = redirectParam.substring(pos + 1);
 
-			int encodedRedirectLength = redirect.length();
-
 			try {
 				redirect = URLCodec.decodeURL(redirect, StringPool.UTF8);
 			}
@@ -2099,7 +2097,7 @@ public class HttpImpl implements Http {
 			redirect = URLCodec.encodeURL(
 				_shortenURL(redirect, currentLength + newLength));
 
-			newLength += encodedRedirectLength;
+			newLength += redirect.length();
 
 			if ((currentLength + newLength) > URL_MAXIMUM_LENGTH) {
 				sb.setIndex(sb.index() - 2);
