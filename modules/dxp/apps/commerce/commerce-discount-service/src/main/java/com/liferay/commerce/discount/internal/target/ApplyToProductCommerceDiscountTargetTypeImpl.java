846b444af330f11cb30699d7dd9b69ff19922c3c
Alessio Rendina <asilcomando@gmail.com>
2018-04-28 19:23:12 +0200

    COMMERCE-304 Added commerce discount target type implementations

---
+++ modules/dxp/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/internal/target/ApplyToProductCommerceDiscountTargetTypeImpl.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.discount.internal.target;
+
+import com.liferay.commerce.discount.constants.CommerceDiscountTargetConstants;
+import com.liferay.commerce.discount.target.CommerceDiscountTargetType;
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"commerce.discount.target.type.key=" + CommerceDiscountTargetConstants.TYPE_PRODUCT,
+		"commerce.discount.target.type.order:Integer=20"
+	},
+	service = CommerceDiscountTargetType.class
+)
+public class ApplyToProductCommerceDiscountTargetTypeImpl
+	implements CommerceDiscountTargetType {
+
+	@Override
+	public String getKey() {
+		return CommerceDiscountTargetConstants.TYPE_PRODUCT;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "apply-to-product");
+	}
+
+}
