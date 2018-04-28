9d707e88534cc137decf8380661a1cde7cecb789
Jorge Ferrer <jorge.ferrer@liferay.com>
2018-04-28 10:18:58 +0200

    LPS-80214 Introduce ConfigurationScreen to represent a custom screen that is rendered within System Settings (while keeping the navigation)

---
+++ modules/apps/foundation/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/display/ConfigurationScreen.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.display;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Jorge Ferrer
+ */
+public interface ConfigurationScreen {
+
+	public String getCategoryKey();
+
+	public String getKey();
+
+	public String getName(Locale locale);
+
+	public String getScope();
+
+	public void render(HttpServletRequest request, HttpServletResponse response)
+		throws IOException;
+
+}
