107e9effdfb60ce1b88fc102acd604933d1e418a
Alessio Rendina <asilcomando@gmail.com>
2018-04-28 19:32:48 +0200

    COMMERCE-304 Added new screen navigation entries

---
+++ modules/dxp/apps/commerce/commerce-discount-web/src/main/java/com/liferay/commerce/discount/web/internal/servlet/taglib/ui/CommerceDiscountCategoriesScreenNavigationEntry.java
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.discount.web.internal.servlet.taglib.ui;
+
+import com.liferay.commerce.currency.service.CommerceCurrencyService;
+import com.liferay.commerce.discount.constants.CommerceDiscountTargetConstants;
+import com.liferay.commerce.discount.model.CommerceDiscount;
+import com.liferay.commerce.discount.service.CommerceDiscountRelService;
+import com.liferay.commerce.discount.service.CommerceDiscountService;
+import com.liferay.commerce.discount.service.CommerceDiscountUserSegmentRelService;
+import com.liferay.commerce.discount.target.CommerceDiscountTargetTypeRegistry;
+import com.liferay.commerce.discount.web.internal.display.context.CommerceDiscountRelDisplayContext;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationCategory;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	property = {
+		"screen.navigation.category.order:Integer=30",
+		"screen.navigation.entry.order:Integer=10"
+	},
+	service = {ScreenNavigationCategory.class, ScreenNavigationEntry.class}
+)
+public class CommerceDiscountCategoriesScreenNavigationEntry
+	implements ScreenNavigationCategory,
+			   ScreenNavigationEntry<CommerceDiscount> {
+
+	@Override
+	public String getCategoryKey() {
+		return CommerceDiscountScreenNavigationConstants.
+			CATEGORY_KEY_COMMERCE_DISCOUNT_CATEGORIES;
+	}
+
+	@Override
+	public String getEntryKey() {
+		return CommerceDiscountScreenNavigationConstants.
+			ENTRY_KEY_COMMERCE_DISCOUNT_CATEGORIES;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(
+			resourceBundle,
+			CommerceDiscountScreenNavigationConstants.
+				ENTRY_KEY_COMMERCE_DISCOUNT_CATEGORIES);
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return CommerceDiscountScreenNavigationConstants.
+			SCREEN_NAVIGATION_KEY_COMMERCE_DISCOUNT_GENERAL;
+	}
+
+	@Override
+	public boolean isVisible(User user, CommerceDiscount commerceDiscount) {
+		if (commerceDiscount == null) {
+			return false;
+		}
+
+		String target = commerceDiscount.getTarget();
+
+		if (!target.equals(CommerceDiscountTargetConstants.TYPE_CATEGORIES)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		CommerceDiscountRelDisplayContext commerceDiscountRelDisplayContext =
+			new CommerceDiscountRelDisplayContext(
+				_commerceCurrencyService, _commerceDiscountRelService,
+				_commerceDiscountService, _commerceDiscountTargetTypeRegistry,
+				_commerceDiscountUserSegmentRelService, _cpDefinitionService,
+				httpServletRequest, _itemSelector);
+
+		httpServletRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT, commerceDiscountRelDisplayContext);
+
+		_jspRenderer.renderJSP(
+			_servletContext, httpServletRequest, httpServletResponse,
+			"/discount/categories.jsp");
+	}
+
+	@Reference
+	private CommerceCurrencyService _commerceCurrencyService;
+
+	@Reference
+	private CommerceDiscountRelService _commerceDiscountRelService;
+
+	@Reference
+	private CommerceDiscountService _commerceDiscountService;
+
+	@Reference
+	private CommerceDiscountTargetTypeRegistry
+		_commerceDiscountTargetTypeRegistry;
+
+	@Reference
+	private CommerceDiscountUserSegmentRelService
+		_commerceDiscountUserSegmentRelService;
+
+	@Reference
+	private CPDefinitionService _cpDefinitionService;
+
+	@Reference
+	private ItemSelector _itemSelector;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.discount.web)"
+	)
+	private ServletContext _servletContext;
+
+}
