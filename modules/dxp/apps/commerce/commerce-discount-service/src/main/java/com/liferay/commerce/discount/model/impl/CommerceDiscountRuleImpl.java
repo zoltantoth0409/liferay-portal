c12156b05af9beb7511167832752669f1848e136
Alessio Rendina <asilcomando@gmail.com>
2018-04-28 19:16:34 +0200

    COMMERCE-304 Added commerce discount rule implementation

--- modules/dxp/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/model/impl/CommerceDiscountRuleImpl.java
+++ modules/dxp/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/model/impl/CommerceDiscountRuleImpl.java
@@ -16,13 +16,62 @@ package com.liferay.commerce.discount.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import java.io.IOException;
+
 /**
  * @author Marco Leo
+ * @author Alessio Antonio Rendina
  */
 @ProviderType
 public class CommerceDiscountRuleImpl extends CommerceDiscountRuleBaseImpl {
 
 	public CommerceDiscountRuleImpl() {
 	}
+
+	@Override
+	public UnicodeProperties getSettingsProperties() {
+		if (_settingsProperties == null) {
+			_settingsProperties = new UnicodeProperties(true);
+
+			try {
+				_settingsProperties.load(super.getTypeSettings());
+			}
+			catch (IOException ioe) {
+				_log.error(ioe, ioe);
+			}
+		}
+
+		return _settingsProperties;
+	}
+
+	@Override
+	public String getSettingsProperty(String key) {
+		UnicodeProperties settingsProperties = getSettingsProperties();
+
+		return settingsProperties.getProperty(key);
+	}
+
+	@Override
+	public void setSettingsProperties(UnicodeProperties settingsProperties) {
+		_settingsProperties = settingsProperties;
+
+		super.setTypeSettings(settingsProperties.toString());
+	}
+
+	@Override
+	public void setTypeSettings(String settings) {
+		_settingsProperties = null;
+
+		super.setTypeSettings(settings);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceDiscountRuleImpl.class);
+
+	private UnicodeProperties _settingsProperties;
 
 }
