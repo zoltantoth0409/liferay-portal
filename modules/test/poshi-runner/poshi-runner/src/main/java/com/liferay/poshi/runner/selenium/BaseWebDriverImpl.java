8b75d68a60f9ac6feee8667ba72e8347959018ee
Kenji Heigel <kenji.heigel@liferay.com>
2018-04-30 12:36:21 -0700

    LRQA-40455 Create a getText using WebDriver's JavascriptExecutor

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseWebDriverImpl.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseWebDriverImpl.java
@@ -1698,6 +1698,40 @@ public abstract class BaseWebDriverImpl implements LiferaySelenium, WebDriver {
 		executeJavaScriptEvent(locator, "MouseEvent", "click");
 	}
 
+	public String javaScriptGetText(String locator, String timeout)
+		throws Exception {
+
+		if (locator.contains("x:")) {
+			return getHtmlNodeText(locator);
+		}
+
+		WebElement webElement = getWebElement(locator, timeout);
+
+		if (webElement == null) {
+			throw new Exception(
+				"Element is not present at \"" + locator + "\"");
+		}
+
+		WrapsDriver wrapsDriver = (WrapsDriver)webElement;
+
+		WebDriver wrappedWebDriver = wrapsDriver.getWrappedDriver();
+
+		JavascriptExecutor javascriptExecutor =
+			(JavascriptExecutor)wrappedWebDriver;
+
+		StringBuilder sb = new StringBuilder(2);
+
+		sb.append("var element = arguments[0];");
+		sb.append("return element.innerText;");
+
+		String text = (String)javascriptExecutor.executeScript(
+			sb.toString(), webElement);
+
+		text = text.trim();
+
+		return text.replace("\n", " ");
+	}
+
 	@Override
 	public void javaScriptMouseDown(String locator) {
 		executeJavaScriptEvent(locator, "MouseEvent", "mousedown");
