12b7a47dced53d179ba8dffae1f291f14bd0c3f3
Kenji Heigel <kenji.heigel@liferay.com>
2018-04-30 13:29:23 -0700

    LRQA-40446 Add MultilinePoshiComment class

---
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/MultilinePoshiComment.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.elements;
+
+import org.dom4j.Comment;
+
+/**
+ * @author Kenji Heigel
+ */
+public class MultilinePoshiComment extends PoshiComment {
+
+	@Override
+	public PoshiComment clone(Comment comment) {
+		return null;
+	}
+
+	@Override
+	public PoshiComment clone(String readableSyntax) {
+		return null;
+	}
+
+	@Override
+	public boolean isReadableSyntaxComment(String readableSyntax) {
+		return false;
+	}
+
+	@Override
+	public void parseReadableSyntax(String readableSyntax) {
+	}
+
+	@Override
+	public String toReadableSyntax() {
+		return null;
+	}
+
+}
