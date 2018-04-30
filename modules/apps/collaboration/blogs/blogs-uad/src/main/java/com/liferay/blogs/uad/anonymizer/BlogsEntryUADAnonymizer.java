027ecbdbc655b49194cae36429de281202978702
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-04-30 14:13:42 -0700

    LPS-80122 blogs-uad - autogenerated

--- modules/apps/collaboration/blogs/blogs-uad/src/main/java/com/liferay/blogs/uad/anonymizer/BlogsEntryUADAnonymizer.java
+++ modules/apps/collaboration/blogs/blogs-uad/src/main/java/com/liferay/blogs/uad/anonymizer/BlogsEntryUADAnonymizer.java
@@ -14,67 +14,18 @@
 
 package com.liferay.blogs.uad.anonymizer;
 
-import com.liferay.blogs.model.BlogsEntry;
-import com.liferay.blogs.service.BlogsEntryLocalService;
 import com.liferay.blogs.uad.constants.BlogsUADConstants;
-
-import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.User;
-
-import com.liferay.user.associated.data.anonymizer.DynamicQueryUADAnonymizer;
 import com.liferay.user.associated.data.anonymizer.UADAnonymizer;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
- * @generated
  */
-@Component(immediate = true, property =  {
-	"model.class.name=" + BlogsUADConstants.CLASS_NAME_BLOGS_ENTRY}, service = UADAnonymizer.class)
-public class BlogsEntryUADAnonymizer extends DynamicQueryUADAnonymizer<BlogsEntry> {
-	@Override
-	public void autoAnonymize(BlogsEntry blogsEntry, long userId,
-		User anonymousUser) throws PortalException {
-		if (blogsEntry.getUserId() == userId) {
-			blogsEntry.setUserId(anonymousUser.getUserId());
-			blogsEntry.setUserName(anonymousUser.getFullName());
-		}
-
-		if (blogsEntry.getStatusByUserId() == userId) {
-			blogsEntry.setStatusByUserId(anonymousUser.getUserId());
-			blogsEntry.setStatusByUserName(anonymousUser.getFullName());
-		}
-
-		_blogsEntryLocalService.updateBlogsEntry(blogsEntry);
-	}
-
-	@Override
-	public void delete(BlogsEntry blogsEntry) throws PortalException {
-		_blogsEntryLocalService.deleteBlogsEntry(blogsEntry);
-	}
-
-	@Override
-	public List<String> getNonanonymizableFieldNames() {
-		return Arrays.asList("title", "subtitle", "urlTitle", "description",
-			"content", "smallImage", "smallImageId");
-	}
-
-	@Override
-	protected ActionableDynamicQuery doGetActionableDynamicQuery() {
-		return _blogsEntryLocalService.getActionableDynamicQuery();
-	}
-
-	@Override
-	protected String[] doGetUserIdFieldNames() {
-		return BlogsUADConstants.USER_ID_FIELD_NAMES_BLOGS_ENTRY;
-	}
-
-	@Reference
-	private BlogsEntryLocalService _blogsEntryLocalService;
+@Component(
+	immediate = true,
+	property = "model.class.name=" + BlogsUADConstants.CLASS_NAME_BLOGS_ENTRY,
+	service = UADAnonymizer.class
+)
+public class BlogsEntryUADAnonymizer extends BaseBlogsEntryUADAnonymizer {
 }
