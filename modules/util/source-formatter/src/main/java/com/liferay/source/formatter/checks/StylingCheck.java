f9858c14c1876d248f4e9cc0a191dfba264d27da
Hugo Huijser <hugo.huijser@liferay.com>
2018-04-30 12:34:14 +0800

    LPS-79679 Make sure we only change method calls with single parameter

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/StylingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/StylingCheck.java
@@ -14,7 +14,13 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Hugo Huijser
@@ -35,8 +41,7 @@ public abstract class StylingCheck extends BaseFileCheck {
 		content = _formatStyling(
 			content, "String.valueOf(true)", "Boolean.TRUE.toString()");
 
-		content = _formatStyling(
-			content, "Objects.toString(", "String.valueOf(");
+		content = _formatToStringMethodCall(content, "Objects");
 
 		return content;
 	}
@@ -68,6 +73,32 @@ public abstract class StylingCheck extends BaseFileCheck {
 					content, incorrectStyling, correctStyling);
 			}
 		}
+	}
+
+	private String _formatToStringMethodCall(String content, String className) {
+		Pattern pattern = Pattern.compile(
+			StringBundler.concat("\\W", className, "\\.toString\\((.*?)\\);\n"),
+			Pattern.DOTALL);
+
+		Matcher matcher = pattern.matcher(content);
+
+		while (matcher.find()) {
+			int pos = matcher.start();
+
+			if (!isJavaSource(content, pos)) {
+				continue;
+			}
+
+			List<String> parameterList = JavaSourceUtil.getParameterList(
+				matcher.group());
+
+			if (parameterList.size() == 1) {
+				return StringUtil.replaceFirst(
+					content, className + ".toString(", "String.valueOf(", pos);
+			}
+		}
+
+		return content;
 	}
 
 }
