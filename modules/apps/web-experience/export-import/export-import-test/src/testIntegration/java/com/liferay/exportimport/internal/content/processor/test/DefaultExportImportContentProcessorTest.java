867f0d94416dbfb2ca359e00315b2317b20d8548
ChrisKian <christopher.kian@liferay.com>
2018-05-01 15:24:09 -0700

    LPS-80345 Fix tests to use an actual staged group.  Update the expected results for the tests.

--- modules/apps/web-experience/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/internal/content/processor/test/DefaultExportImportContentProcessorTest.java
+++ modules/apps/web-experience/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/internal/content/processor/test/DefaultExportImportContentProcessorTest.java
@@ -148,7 +148,10 @@ public class DefaultExportImportContentProcessorTest {
 
 		_externalGroup = GroupTestUtil.addGroup();
 		_liveGroup = GroupTestUtil.addGroup();
-		_stagingGroup = GroupTestUtil.addGroup();
+
+		GroupTestUtil.enableLocalStaging(_liveGroup);
+
+		_stagingGroup = _liveGroup.getStagingGroup();
 
 		ServiceContext serviceContext =
 			ServiceContextTestUtil.getServiceContext(
@@ -413,8 +416,11 @@ public class DefaultExportImportContentProcessorTest {
 		Assert.assertTrue(
 			content,
 			content.contains(
-				"@data_handler_group_friendly_url@@" +
-					_stagingGroup.getFriendlyURL() + "@"));
+				"@data_handler_group_friendly_url@@" + _liveGroup.getUuid() +
+					"@"));
+		Assert.assertFalse(content, content.contains(_stagingGroup.getUuid()));
+		Assert.assertFalse(
+			content, content.contains(_stagingGroup.getFriendlyURL()));
 		Assert.assertTrue(
 			content, content.contains("@data_handler_path_context@/en@"));
 		Assert.assertFalse(
@@ -483,8 +489,11 @@ public class DefaultExportImportContentProcessorTest {
 		Assert.assertTrue(
 			content,
 			content.contains(
-				"@data_handler_group_friendly_url@@" +
-					_stagingGroup.getFriendlyURL() + "@"));
+				"@data_handler_group_friendly_url@@" + _liveGroup.getUuid() +
+					"@"));
+		Assert.assertFalse(content, content.contains(_stagingGroup.getUuid()));
+		Assert.assertFalse(
+			content, content.contains(_stagingGroup.getFriendlyURL()));
 		Assert.assertFalse(content, content.contains("/en/en"));
 
 		setFinalStaticField(
@@ -1215,10 +1224,7 @@ public class DefaultExportImportContentProcessorTest {
 	private PortletDataContext _portletDataContextExport;
 	private PortletDataContext _portletDataContextImport;
 	private StagedModel _referrerStagedModel;
-
-	@DeleteAfterTestRun
 	private Group _stagingGroup;
-
 	private Layout _stagingPrivateLayout;
 	private Layout _stagingPublicLayout;
 
