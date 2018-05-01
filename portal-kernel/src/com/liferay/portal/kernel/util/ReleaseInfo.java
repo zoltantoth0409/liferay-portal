c605d20f15d1400d603e9bf26e642b08e2909221
Chas Austin <charles.austin@liferay.com>
2018-05-01 11:35:03 -0700

    7.1 CE Alpha 2 Release Info

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -274,7 +274,7 @@ public class ReleaseInfo {
 
 	private static final String _CODE_NAME = "Judson";
 
-	private static final String _DATE = "April 6, 2018";
+	private static final String _DATE = "May 4, 2018";
 
 	private static final String _NAME = "Liferay Community Edition Portal";
 
@@ -290,7 +290,7 @@ public class ReleaseInfo {
 
 	private static final String _VERSION = "7.1.0";
 
-	private static final String _VERSION_DISPLAY_NAME = "7.1.0 CE Alpha";
+	private static final String _VERSION_DISPLAY_NAME = "7.1.0 CE Alpha 2";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
