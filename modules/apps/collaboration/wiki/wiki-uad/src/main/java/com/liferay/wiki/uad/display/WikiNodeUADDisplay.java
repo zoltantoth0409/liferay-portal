002942531c871b83e5d2c55cafea837d3d477be3
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-05-01 15:39:46 -0700

    LPS-80124 (wiki-uad) Remove UADDisplayHelper and move custom implementations to UADDisplay

--- modules/apps/collaboration/wiki/wiki-uad/src/main/java/com/liferay/wiki/uad/display/WikiNodeUADDisplay.java
+++ modules/apps/collaboration/wiki/wiki-uad/src/main/java/com/liferay/wiki/uad/display/WikiNodeUADDisplay.java
@@ -14,16 +14,49 @@
 
 package com.liferay.wiki.uad.display;
 
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.user.associated.data.display.UADDisplay;
-
+import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.model.WikiNode;
 import com.liferay.wiki.uad.constants.WikiUADConstants;
 
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  */
-@Component(immediate = true, property =  {
-	"model.class.name=" + WikiUADConstants.CLASS_NAME_WIKI_NODE}, service = UADDisplay.class)
+@Component(
+	immediate = true,
+	property = "model.class.name=" + WikiUADConstants.CLASS_NAME_WIKI_NODE,
+	service = UADDisplay.class
+)
 public class WikiNodeUADDisplay extends BaseWikiNodeUADDisplay {
+
+	@Override
+	public String getEditURL(
+			WikiNode wikiNode, LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws Exception {
+
+		PortletURL portletURL = liferayPortletResponse.createLiferayPortletURL(
+			portal.getControlPanelPlid(liferayPortletRequest),
+			WikiPortletKeys.WIKI_ADMIN, PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcRenderCommandName", "/wiki/edit_node");
+		portletURL.setParameter(
+			"redirect", portal.getCurrentURL(liferayPortletRequest));
+		portletURL.setParameter("nodeId", String.valueOf(wikiNode.getNodeId()));
+
+		return portletURL.toString();
+	}
+
+	@Reference
+	protected Portal portal;
+
 }
--- modules/apps/collaboration/wiki/wiki-uad/src/main/java/com/liferay/wiki/uad/display/WikiPageUADDisplay.java
+++ modules/apps/collaboration/wiki/wiki-uad/src/main/java/com/liferay/wiki/uad/display/WikiPageUADDisplay.java
@@ -14,16 +14,50 @@
 
 package com.liferay.wiki.uad.display;
 
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.user.associated.data.display.UADDisplay;
-
+import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.model.WikiPage;
 import com.liferay.wiki.uad.constants.WikiUADConstants;
 
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  */
-@Component(immediate = true, property =  {
-	"model.class.name=" + WikiUADConstants.CLASS_NAME_WIKI_PAGE}, service = UADDisplay.class)
+@Component(
+	immediate = true,
+	property = "model.class.name=" + WikiUADConstants.CLASS_NAME_WIKI_PAGE,
+	service = UADDisplay.class
+)
 public class WikiPageUADDisplay extends BaseWikiPageUADDisplay {
+
+	@Override
+	public String getEditURL(
+			WikiPage wikiPage, LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws Exception {
+
+		PortletURL portletURL = liferayPortletResponse.createLiferayPortletURL(
+			portal.getControlPanelPlid(liferayPortletRequest),
+			WikiPortletKeys.WIKI, PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcRenderCommandName", "/wiki/edit_page");
+		portletURL.setParameter(
+			"redirect", portal.getCurrentURL(liferayPortletRequest));
+		portletURL.setParameter("nodeId", String.valueOf(wikiPage.getNodeId()));
+		portletURL.setParameter("title", String.valueOf(wikiPage.getTitle()));
+
+		return portletURL.toString();
+	}
+
+	@Reference
+	protected Portal portal;
+
 }
