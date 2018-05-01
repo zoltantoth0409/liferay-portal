bf6f995b22e0a865287a2d3db56d3a0939aab0fa
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-05-01 15:35:04 -0700

    LPS-80124 (message-boards-uad) Remove UADDisplayHelper and move custom implementations to UADDisplay

--- modules/apps/collaboration/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/display/MBCategoryUADDisplay.java
+++ modules/apps/collaboration/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/display/MBCategoryUADDisplay.java
@@ -14,16 +14,50 @@
 
 package com.liferay.message.boards.uad.display;
 
+import com.liferay.message.boards.constants.MBPortletKeys;
+import com.liferay.message.boards.model.MBCategory;
 import com.liferay.message.boards.uad.constants.MBUADConstants;
-
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.user.associated.data.display.UADDisplay;
 
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  */
-@Component(immediate = true, property =  {
-	"model.class.name=" + MBUADConstants.CLASS_NAME_MB_CATEGORY}, service = UADDisplay.class)
+@Component(
+	immediate = true,
+	property = "model.class.name=" + MBUADConstants.CLASS_NAME_MB_CATEGORY,
+	service = UADDisplay.class
+)
 public class MBCategoryUADDisplay extends BaseMBCategoryUADDisplay {
+
+	@Override
+	public String getEditURL(
+			MBCategory mbCategory, LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws Exception {
+
+		PortletURL portletURL = liferayPortletResponse.createLiferayPortletURL(
+			portal.getControlPanelPlid(liferayPortletRequest),
+			MBPortletKeys.MESSAGE_BOARDS, PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcRenderCommandName", "/wiki/edit_category");
+		portletURL.setParameter(
+			"redirect", portal.getCurrentURL(liferayPortletRequest));
+		portletURL.setParameter(
+			"mbCategoryId", String.valueOf(mbCategory.getCategoryId()));
+
+		return portletURL.toString();
+	}
+
+	@Reference
+	protected Portal portal;
+
 }
--- modules/apps/collaboration/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/display/MBMessageUADDisplay.java
+++ modules/apps/collaboration/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/display/MBMessageUADDisplay.java
@@ -14,16 +14,50 @@
 
 package com.liferay.message.boards.uad.display;
 
+import com.liferay.message.boards.constants.MBPortletKeys;
+import com.liferay.message.boards.model.MBMessage;
 import com.liferay.message.boards.uad.constants.MBUADConstants;
-
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.user.associated.data.display.UADDisplay;
 
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  */
-@Component(immediate = true, property =  {
-	"model.class.name=" + MBUADConstants.CLASS_NAME_MB_MESSAGE}, service = UADDisplay.class)
+@Component(
+	immediate = true,
+	property = "model.class.name=" + MBUADConstants.CLASS_NAME_MB_MESSAGE,
+	service = UADDisplay.class
+)
 public class MBMessageUADDisplay extends BaseMBMessageUADDisplay {
+
+	@Override
+	public String getEditURL(
+			MBMessage mbMessage, LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws Exception {
+
+		PortletURL portletURL = liferayPortletResponse.createLiferayPortletURL(
+			portal.getControlPanelPlid(liferayPortletRequest),
+			MBPortletKeys.MESSAGE_BOARDS, PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcRenderCommandName", "/wiki/edit_message");
+		portletURL.setParameter(
+			"redirect", portal.getCurrentURL(liferayPortletRequest));
+		portletURL.setParameter(
+			"messageId", String.valueOf(mbMessage.getMessageId()));
+
+		return portletURL.toString();
+	}
+
+	@Reference
+	protected Portal portal;
+
 }
