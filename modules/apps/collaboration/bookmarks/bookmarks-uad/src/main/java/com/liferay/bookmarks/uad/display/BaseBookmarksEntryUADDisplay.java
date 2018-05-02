d878c91a243e2f4b43ec76553f3e001db25cda8d
drewbrokke <drew.brokke@liferay.com>
2018-05-02 08:42:49 -0700

    LPS-80386 bookmarks-uad - BaseBookmarksEntryUADDisplay.java - updates base display to implement the additional methods

--- modules/apps/collaboration/bookmarks/bookmarks-uad/src/main/java/com/liferay/bookmarks/uad/display/BaseBookmarksEntryUADDisplay.java
+++ modules/apps/collaboration/bookmarks/bookmarks-uad/src/main/java/com/liferay/bookmarks/uad/display/BaseBookmarksEntryUADDisplay.java
@@ -15,10 +15,18 @@
 package com.liferay.bookmarks.uad.display;
 
 import com.liferay.bookmarks.model.BookmarksEntry;
+import com.liferay.bookmarks.service.BookmarksEntryLocalService;
 import com.liferay.bookmarks.uad.constants.BookmarksUADConstants;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.user.associated.data.display.BaseModelUADDisplay;
 
+import java.io.Serializable;
+
+import java.util.List;
 import java.util.Locale;
+
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * Provides the base implementation for the bookmarks entry UAD display.
@@ -34,20 +42,56 @@ import java.util.Locale;
 public abstract class BaseBookmarksEntryUADDisplay
 	extends BaseModelUADDisplay<BookmarksEntry> {
 
+	@Override
+	public BookmarksEntry get(Serializable primaryKey) throws PortalException {
+		return bookmarksEntryLocalService.getBookmarksEntry(
+			Long.valueOf(primaryKey.toString()));
+	}
+
+	@Override
 	public String getApplicationName() {
 		return BookmarksUADConstants.APPLICATION_NAME;
 	}
 
+	@Override
 	public String[] getDisplayFieldNames() {
 		return new String[] {"name", "description", "url"};
 	}
 
+	@Override
 	public String getKey() {
 		return BookmarksUADConstants.CLASS_NAME_BOOKMARKS_ENTRY;
 	}
 
+	@Override
 	public String getTypeName(Locale locale) {
 		return "BookmarksEntry";
 	}
+
+	@Override
+	protected long doCount(DynamicQuery dynamicQuery) {
+		return bookmarksEntryLocalService.dynamicQueryCount(dynamicQuery);
+	}
+
+	@Override
+	protected DynamicQuery doGetDynamicQuery() {
+		return bookmarksEntryLocalService.dynamicQuery();
+	}
+
+	@Override
+	protected List<BookmarksEntry> doGetRange(
+		DynamicQuery dynamicQuery, int start, int end) {
+
+		return bookmarksEntryLocalService.dynamicQuery(
+			dynamicQuery, start, end);
+	}
+
+	@Override
+	protected String[] doGetUserIdFieldNames() {
+		return BookmarksUADConstants.USER_ID_FIELD_NAMES_BOOKMARKS_ENTRY;
+	}
+
+	@Reference
+	protected BookmarksEntryLocalService bookmarksEntryLocalService;
 
 }
