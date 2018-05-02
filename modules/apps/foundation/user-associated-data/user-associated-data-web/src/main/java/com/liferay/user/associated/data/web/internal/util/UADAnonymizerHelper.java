c96d0c01435ae2b1aec64286c633d0dd37b20ee8
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-05-02 16:49:16 -0700

    LPS-80419 Dynamically create an anonymous user when trying to anonymize a user and no anonymous user is found for the company

--- modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADAnonymizerHelper.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADAnonymizerHelper.java
@@ -14,16 +14,28 @@
 
 package com.liferay.user.associated.data.web.internal.util;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.user.associated.data.web.internal.configuration.AnonymousUserConfiguration;
 import com.liferay.user.associated.data.web.internal.configuration.AnonymousUserConfigurationRetriever;
 
+import java.util.Calendar;
+import java.util.Dictionary;
+import java.util.Locale;
 import java.util.Optional;
 
 import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -56,6 +68,54 @@ public class UADAnonymizerHelper {
 		}
 	}
 
+	private User _createAnonymousUser(long companyId) throws Exception {
+		long creatorUserId = 0;
+
+		String password = StringUtil.randomString();
+
+		boolean autoPassword = false;
+		String password1 = password;
+		String password2 = password;
+		boolean autoScreenName = false;
+		long facebookId = 0;
+		String openId = StringPool.BLANK;
+		Locale locale = LocaleThreadLocal.getDefaultLocale();
+		String firstName = "Anonymous";
+		String middleName = StringPool.BLANK;
+		String lastName = "Anonymous";
+		long prefixId = 0;
+		long suffixId = 0;
+		boolean male = true;
+		int birthdayMonth = Calendar.JANUARY;
+		int birthdayDay = 1;
+		int birthdayYear = 1970;
+		String jobTitle = StringPool.BLANK;
+		long[] groupIds = null;
+		long[] organizationIds = null;
+		long[] roleIds = null;
+		long[] userGroupIds = null;
+		boolean sendEmail = false;
+		ServiceContext serviceContext = null;
+
+		String screenName = firstName + String.valueOf(companyId);
+
+		Company company = _companyLocalService.getCompany(companyId);
+
+		String emailAddress = StringBundler.concat(
+			screenName, StringPool.AT, company.getMx());
+
+		User anonymousUser = _userLocalService.addUserWithWorkflow(
+			creatorUserId, companyId, autoPassword, password1, password2,
+			autoScreenName, screenName, emailAddress, facebookId, openId,
+			locale, firstName, middleName, lastName, prefixId, suffixId, male,
+			birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds,
+			organizationIds, roleIds, userGroupIds, sendEmail, serviceContext);
+
+		_registerAnonymousUser(companyId, anonymousUser.getUserId());
+
+		return anonymousUser;
+	}
+
 	private User _getAnonymousUser(long companyId) throws Exception {
 		Optional<Configuration> configurationOptional =
 			_anonymousUserConfigurationRetriever.getOptional(companyId);
@@ -70,15 +130,37 @@ public class UADAnonymizerHelper {
 		).map(
 			_userLocalService::fetchUser
 		).orElse(
-			_userLocalService.getDefaultUser(companyId)
+			_createAnonymousUser(companyId)
 		);
 
 		return anonymousUser;
 	}
 
+	private void _registerAnonymousUser(long companyId, long userId)
+		throws Exception {
+
+		Configuration anonymousUserConfiguration =
+			_configurationAdmin.createFactoryConfiguration(
+				AnonymousUserConfiguration.class.getName(),
+				StringPool.QUESTION);
+
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("companyId", companyId);
+		properties.put("userId", userId);
+
+		anonymousUserConfiguration.update(properties);
+	}
+
 	@Reference
 	private AnonymousUserConfigurationRetriever
 		_anonymousUserConfigurationRetriever;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private ConfigurationAdmin _configurationAdmin;
 
 	@Reference
 	private UserLocalService _userLocalService;
