d0b71825cc283bf98a4601b390f813a2ab59c2c6
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2018-05-02 16:59:46 -0300

    LPS-80303 Apply Clay classes to alert float on the top of screen

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/admin/js/form_portlet.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/admin/js/form_portlet.js
@@ -1020,6 +1020,14 @@ AUI.add(
 							alert.render('.management-bar-default .container-fluid-1280');
 						}
 
+						var alertBoundingBox = alert.get('boundingBox');
+
+						var alertParent = alertBoundingBox.get('parentNode');
+
+						alertBoundingBox.setStyle('wordWrap', 'break-word');
+
+						alertParent.addClass('alert-notifications alert-notifications-fixed');
+
 						alert.show();
 
 						instance.set('alert', alert);
