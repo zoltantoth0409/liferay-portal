24df71cb1e7493da1289bab85a121bea8fab4267
Matthew Tambara <matthew.tambara@liferay.com>
2018-05-03 14:53:09 -0700

    LPS-80978 Create pre-generated test byte array

---
+++ portal-test/src/com/liferay/portal/kernel/test/util/TestDataConstants.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.test.util;
+
+/**
+ * @author Matthew Tambara
+ */
+public class TestDataConstants {
+
+	public static final byte[] TEST_BYTE_ARRAY = {
+		(byte)-16, (byte)-96, (byte)-97, (byte)33, (byte)-36, (byte)-46,
+		(byte)-91, (byte)127
+	};
+
+}
