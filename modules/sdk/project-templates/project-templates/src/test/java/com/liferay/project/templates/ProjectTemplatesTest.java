61a2e23c34b1960f4cbb5befefa18a5d608dd659
Lawrence Lee <lawrence.lee@liferay.com>
2018-05-04 10:28:25 -0500

    LPS-80472 Add NPM CACHE DIR for Project Templates

--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
@@ -2292,7 +2292,7 @@ public class ProjectTemplatesTest {
 
 		GradleRunner gradleRunner = GradleRunner.create();
 
-		List<String> arguments = new ArrayList<>(taskPaths.length + 3);
+		List<String> arguments = new ArrayList<>(taskPaths.length + 5);
 
 		arguments.add("--stacktrace");
 
@@ -2302,6 +2302,14 @@ public class ProjectTemplatesTest {
 		if (Validator.isNotNull(httpProxyHost) && (httpProxyPort > 0)) {
 			arguments.add("-Dhttp.proxyHost=" + httpProxyHost);
 			arguments.add("-Dhttp.proxyPort=" + httpProxyPort);
+		}
+
+		if (Validator.isNotNull(System.getenv("JENKINS_HOME"))) {
+			arguments.add(
+				"-Dnodejs.npm.ci.registry=" + _NODEJS_NPM_CI_REGISTRY);
+			arguments.add(
+				"-Dnodejs.npm.ci.sass.binary.site=" +
+					_NODEJS_NPM_CI_SASS_BINARY_SITE);
 		}
 
 		for (String taskPath : taskPaths) {
@@ -2330,11 +2338,19 @@ public class ProjectTemplatesTest {
 	private static void _executeMaven(File projectDir, String... args)
 		throws Exception {
 
-		String[] completeArgs = new String[args.length + 1];
+		String[] completeArgs = new String[args.length + 3];
 
 		completeArgs[0] = "--update-snapshots";
 
-		System.arraycopy(args, 0, completeArgs, 1, args.length);
+		if (Validator.isNotNull(System.getenv("JENKINS_HOME"))) {
+			completeArgs[1] =
+				"-Dnodejs.npm.ci.registry=" + _NODEJS_NPM_CI_REGISTRY;
+			completeArgs[2] =
+				"-Dnodejs.npm.ci.sass.binary.site=" +
+					_NODEJS_NPM_CI_SASS_BINARY_SITE;
+		}
+
+		System.arraycopy(args, 0, completeArgs, 3, args.length);
 
 		MavenExecutor.Result result = mavenExecutor.execute(projectDir, args);
 
@@ -3305,6 +3321,12 @@ public class ProjectTemplatesTest {
 		"mvnw", "mvnw.cmd", ".mvn/wrapper/maven-wrapper.jar",
 		".mvn/wrapper/maven-wrapper.properties"
 	};
+
+	private static final String _NODEJS_NPM_CI_REGISTRY = System.getProperty(
+		"nodejs.npm.ci.registry");
+
+	private static final String _NODEJS_NPM_CI_SASS_BINARY_SITE =
+		System.getProperty("nodejs.npm.ci.sass.binary.site");
 
 	private static final String _OUTPUT_FILENAME_GLOB_REGEX = "*.{jar,war}";
 
