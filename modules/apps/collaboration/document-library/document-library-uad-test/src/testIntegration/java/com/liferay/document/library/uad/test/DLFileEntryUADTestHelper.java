27fbb800f45b7691bee6268cbc9ccd0e26933ca6
William Newbury <william.newbury@liferay.com>
2018-05-04 12:03:09 -0700

    LPS-79339 Document Library UAD test helpers

---
+++ modules/apps/collaboration/document-library/document-library-uad-test/src/testIntegration/java/com/liferay/document/library/uad/test/DLFileEntryUADTestHelper.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.uad.test;
+
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileEntryTypeConstants;
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.document.library.kernel.service.DLFolderLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.test.randomizerbumpers.TikaSafeRandomizerBumper;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author William Newbury
+ */
+@Component(immediate = true, service = DLFileEntryUADTestHelper.class)
+public class DLFileEntryUADTestHelper {
+
+	public DLFileEntry addDLFileEntry(long userId) throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				TestPropsValues.getGroupId());
+
+		DLFolder dlFolder = _dlFolderLocalService.addFolder(
+			userId, TestPropsValues.getGroupId(), TestPropsValues.getGroupId(),
+			false, 0L, RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), false, serviceContext);
+
+		byte[] bytes = RandomTestUtil.randomBytes(
+			TikaSafeRandomizerBumper.INSTANCE);
+
+		InputStream is = new ByteArrayInputStream(bytes);
+
+		return _dlFileEntryLocalService.addFileEntry(
+			userId, dlFolder.getGroupId(), dlFolder.getRepositoryId(),
+			dlFolder.getFolderId(), RandomTestUtil.randomString(),
+			ContentTypes.TEXT_PLAIN, RandomTestUtil.randomString(),
+			StringPool.BLANK, StringPool.BLANK,
+			DLFileEntryTypeConstants.FILE_ENTRY_TYPE_ID_BASIC_DOCUMENT, null,
+			null, is, bytes.length, serviceContext);
+	}
+
+	public void cleanUpDependencies(List<DLFileEntry> dlFileEntries)
+		throws Exception {
+
+		for (DLFileEntry dlFileEntry : dlFileEntries) {
+			_dlFolderLocalService.deleteFolder(dlFileEntry.getFolderId());
+		}
+	}
+
+	@Reference
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Reference
+	private DLFolderLocalService _dlFolderLocalService;
+
+}
---
+++ modules/apps/collaboration/document-library/document-library-uad-test/src/testIntegration/java/com/liferay/document/library/uad/test/DLFolderUADTestHelper.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.uad.test;
+
+import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.kernel.service.DLFolderLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author William Newbury
+ */
+@Component(immediate = true, service = DLFolderUADTestHelper.class)
+public class DLFolderUADTestHelper {
+
+	public DLFolder addDLFolder(long userId) throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				TestPropsValues.getGroupId());
+
+		return _dlFolderLocalService.addFolder(
+			userId, TestPropsValues.getGroupId(), TestPropsValues.getGroupId(),
+			false, 0L, RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), false, serviceContext);
+	}
+
+	public DLFolder addDLFolderWithStatusByUserId(
+			long userId, long statusByUserId)
+		throws Exception {
+
+		DLFolder dlFolder = addDLFolder(userId);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				TestPropsValues.getGroupId());
+
+		return _dlFolderLocalService.updateStatus(
+			statusByUserId, dlFolder.getFolderId(),
+			WorkflowConstants.STATUS_DRAFT, null, serviceContext);
+	}
+
+	public void cleanUpDependencies(List<DLFolder> dlFolders) throws Exception {
+	}
+
+	@Reference
+	private DLFolderLocalService _dlFolderLocalService;
+
+}
