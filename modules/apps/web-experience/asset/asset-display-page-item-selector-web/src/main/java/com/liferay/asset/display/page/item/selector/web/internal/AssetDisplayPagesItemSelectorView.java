ba095d16938752cc0bc5f1f8189a079024a01657
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-05-07 13:28:52 +0100

    LPS-79703 Create item selector view and inject required classes to the display context and render the view

---
+++ modules/apps/web-experience/asset/asset-display-page-item-selector-web/src/main/java/com/liferay/asset/display/page/item/selector/web/internal/AssetDisplayPagesItemSelectorView.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.display.page.item.selector.web.internal;
+
+import com.liferay.asset.display.contributor.AssetDisplayContributorTracker;
+import com.liferay.asset.display.page.item.selector.criterion.AssetDisplayPageSelectorCriterion;
+import com.liferay.asset.display.page.item.selector.web.internal.constants.AssetDisplayPageItemSelectorWebKeys;
+import com.liferay.asset.display.page.item.selector.web.internal.display.context.AssetDisplayPagesItemSelectorViewDisplayContext;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = ItemSelectorView.class)
+public class AssetDisplayPagesItemSelectorView
+	implements ItemSelectorView<AssetDisplayPageSelectorCriterion> {
+
+	@Override
+	public Class<AssetDisplayPageSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return AssetDisplayPageSelectorCriterion.class;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		ResourceBundle resourceBundle =
+			_resourceBundleLoader.loadResourceBundle(locale);
+
+		return ResourceBundleUtil.getString(resourceBundle, "display-pages");
+	}
+
+	@Override
+	public boolean isVisible(ThemeDisplay themeDisplay) {
+		return true;
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest request, ServletResponse response,
+			AssetDisplayPageSelectorCriterion assetDisplayPageSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+
+		AssetDisplayPagesItemSelectorViewDisplayContext
+			assetDisplayPagesItemSelectorViewDisplayContext =
+				new AssetDisplayPagesItemSelectorViewDisplayContext(
+					httpServletRequest, _assetDisplayContributorTracker,
+					assetDisplayPageSelectorCriterion, itemSelectedEventName,
+					portletURL);
+
+		request.setAttribute(
+			AssetDisplayPageItemSelectorWebKeys.
+				ASSET_DISPLAY_PAGES_ITEM_SELECTOR_VIEW_DISPLAY_CONTEXT,
+			assetDisplayPagesItemSelectorViewDisplayContext);
+
+		ServletContext servletContext = _servletContext;
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher("/display_pages.jsp");
+
+		requestDispatcher.include(request, response);
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.asset.display.page.item.selector.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.unmodifiableList(
+			ListUtil.fromArray(
+				new ItemSelectorReturnType[] {
+					new UUIDItemSelectorReturnType()
+				}));
+
+	@Reference
+	private AssetDisplayContributorTracker _assetDisplayContributorTracker;
+
+	@Reference(
+		target = "(bundle.symbolic.name=com.liferay.asset.display.page.item.selector.web)",
+		unbind = "-"
+	)
+	private ResourceBundleLoader _resourceBundleLoader;
+
+	private ServletContext _servletContext;
+
+}
