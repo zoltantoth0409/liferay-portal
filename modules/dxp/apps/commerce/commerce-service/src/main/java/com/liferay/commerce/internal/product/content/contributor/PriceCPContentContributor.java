e81397a1fe71999f2e01ac0594c0b1d4745bb1eb
Marco Leo <marco.leo@liferay.com>
2018-05-07 15:55:25 +0200

    COMMERCE-304 Add new price contributor

---
+++ modules/dxp/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/product/content/contributor/PriceCPContentContributor.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.product.content.contributor;
+
+import com.liferay.commerce.constants.CommerceWebKeys;
+import com.liferay.commerce.context.CommerceContext;
+import com.liferay.commerce.currency.model.CommerceMoney;
+import com.liferay.commerce.inventory.CPDefinitionInventoryEngine;
+import com.liferay.commerce.inventory.CPDefinitionInventoryEngineRegistry;
+import com.liferay.commerce.model.CPDefinitionInventory;
+import com.liferay.commerce.price.CommercePriceCalculation;
+import com.liferay.commerce.product.constants.CPContentContributorConstants;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.util.CPContentContributor;
+import com.liferay.commerce.product.util.CPContentContributorRegistry;
+import com.liferay.commerce.service.CPDefinitionInventoryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	immediate = true,
+	property = "commerce.product.content.contributor.name=" + CPContentContributorConstants.AVAILABILITY_NAME,
+	service = CPContentContributor.class
+)
+public class PriceCPContentContributor implements CPContentContributor {
+
+	@Override
+	public String getName() {
+		return CPContentContributorConstants.AVAILABILITY_NAME;
+	}
+
+	@Override
+	public JSONObject getValue(
+			CPInstance cpInstance, HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		CommerceContext commerceContext =
+			(CommerceContext)httpServletRequest.getAttribute(
+				CommerceWebKeys.COMMERCE_CONTEXT);
+
+		JSONObject jsonObject = _jsonFactory.createJSONObject();
+
+		if (cpInstance == null) {
+			return jsonObject;
+		}
+
+		CPDefinitionInventory cpDefinitionInventory =
+			_cpDefinitionInventoryLocalService.
+				fetchCPDefinitionInventoryByCPDefinitionId(
+					cpInstance.getCPDefinitionId());
+
+		CPDefinitionInventoryEngine cpDefinitionInventoryEngine =
+			_cpDefinitionInventoryEngineRegistry.getCPDefinitionInventoryEngine(
+				cpDefinitionInventory);
+
+		CommerceMoney commerceMoney = _commercePriceCalculation.getFinalPrice(
+			cpInstance.getCPInstanceId(),
+			cpDefinitionInventoryEngine.getMinOrderQuantity(cpInstance), true,
+			true, commerceContext);
+
+		jsonObject.put("price", commerceMoney.toString());
+
+		return jsonObject;
+	}
+
+	@Reference
+	private CommercePriceCalculation _commercePriceCalculation;
+
+	@Reference
+	private CPContentContributorRegistry _cpContentContributorRegistry;
+
+	@Reference
+	private CPDefinitionInventoryEngineRegistry
+		_cpDefinitionInventoryEngineRegistry;
+
+	@Reference
+	private CPDefinitionInventoryLocalService
+		_cpDefinitionInventoryLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}
