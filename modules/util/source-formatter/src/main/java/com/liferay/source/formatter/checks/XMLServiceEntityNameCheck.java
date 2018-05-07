394a32ebb192ab77e0752722945ad0e914601353
Hugo Huijser <hugo.huijser@liferay.com>
2018-05-07 09:42:48 +0800

    LPS-80520 Entity name should not be the same as package name

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/XMLServiceEntityNameCheck.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.checks.util.SourceUtil;
+
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+/**
+ * @author Hugo Huijser
+ */
+public class XMLServiceEntityNameCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (!fileName.endsWith("/service.xml")) {
+			return content;
+		}
+
+		Document document = SourceUtil.readXML(content);
+
+		Element rootElement = document.getRootElement();
+
+		String packagePathName = rootElement.attributeValue("package-path");
+
+		if (!packagePathName.startsWith("com.liferay.")) {
+			return content;
+		}
+
+		String trimmedPackage = StringUtil.removeChar(
+			packagePathName, CharPool.PERIOD);
+
+		for (Element entityElement :
+				(List<Element>)rootElement.elements("entity")) {
+
+			String entityName = entityElement.attributeValue("name");
+
+			if (trimmedPackage.endsWith(StringUtil.lowerCase(entityName))) {
+				addMessage(
+					fileName,
+					StringBundler.concat(
+						"Do not use entity '", entityName,
+						"' when package is '", packagePathName, "'"));
+			}
+		}
+
+		return content;
+	}
+
+}
