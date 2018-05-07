7c77c2781687e108a0358dc847ff01ca48528493
Máté Thurzó <mate.thurzo@liferay.com>
2018-05-07 21:18:52 +0200

    LPS-78421 Minor updates to asset error messages

--- modules/apps/web-experience/asset/asset-categories-navigation-web/src/main/java/com/liferay/asset/categories/navigation/web/internal/exportimport/portlet/preferences/processor/AssetCategoriesNavigationPortletPreferencesProcessor.java
+++ modules/apps/web-experience/asset/asset-categories-navigation-web/src/main/java/com/liferay/asset/categories/navigation/web/internal/exportimport/portlet/preferences/processor/AssetCategoriesNavigationPortletPreferencesProcessor.java
@@ -82,7 +82,9 @@ public class AssetCategoriesNavigationPortletPreferencesProcessor
 		}
 		catch (Exception e) {
 			throw new PortletDataException(
-				"Unable to update portlet preferences during export", e);
+				"Unable to update asset categories navigation portlet " +
+					"preferences during export",
+				e);
 		}
 	}
 
@@ -98,7 +100,9 @@ public class AssetCategoriesNavigationPortletPreferencesProcessor
 		}
 		catch (Exception e) {
 			throw new PortletDataException(
-				"Unable to update portlet preferences during import", e);
+				"Unable to update asset categories navigation portlet " +
+					"preferences during import",
+				e);
 		}
 	}
 
