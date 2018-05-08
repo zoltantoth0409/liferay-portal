a94b78383b905bb991bd2612a6beda85286e8965
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-05-08 09:30:33 +0200

    LPS-80527 Apply to journal

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/asset/display/contributor/JournalArticleAssetDisplayContributor.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/asset/display/contributor/JournalArticleAssetDisplayContributor.java
@@ -16,17 +16,26 @@ package com.liferay.journal.web.internal.asset.display.contributor;
 
 import com.liferay.asset.display.contributor.AssetDisplayContributor;
 import com.liferay.asset.display.contributor.BaseAssetDisplayContributor;
+import com.liferay.dynamic.data.mapping.kernel.DDMFormFieldValue;
+import com.liferay.dynamic.data.mapping.kernel.DDMFormValues;
+import com.liferay.dynamic.data.mapping.kernel.Value;
+import com.liferay.dynamic.data.mapping.util.FieldsToDDMFormValuesConverter;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.util.JournalContent;
+import com.liferay.journal.util.JournalConverter;
+import com.liferay.journal.web.asset.JournalArticleDDMFormValuesReader;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
-import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ResourceBundleLoader;
 import com.liferay.portal.kernel.util.ResourceBundleLoaderUtil;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -47,6 +56,49 @@ public class JournalArticleAssetDisplayContributor
 	@Override
 	protected String[] getAssetEntryModelFields() {
 		return new String[] {"content"};
+	}
+
+	@Override
+	protected Map<String, Object> getClassTypeValues(
+		JournalArticle article, Locale locale) {
+
+		Map<String, Object> classTypeValues = new HashMap<>();
+
+		JournalArticleDDMFormValuesReader journalArticleDDMFormValuesReader =
+			new JournalArticleDDMFormValuesReader(article);
+
+		journalArticleDDMFormValuesReader.setFieldsToDDMFormValuesConverter(
+			_fieldsToDDMFormValuesConverter);
+		journalArticleDDMFormValuesReader.setJournalConverter(
+			_journalConverter);
+
+		try {
+			DDMFormValues ddmFormValues =
+				journalArticleDDMFormValuesReader.getDDMFormValues();
+
+			Map<String, List<DDMFormFieldValue>> ddmFormFieldsValuesMap =
+				ddmFormValues.getDDMFormFieldValuesMap();
+
+			for (Map.Entry<String, List<DDMFormFieldValue>>
+					entry: ddmFormFieldsValuesMap.entrySet()) {
+
+				List<DDMFormFieldValue> ddmFormFieldValues = entry.getValue();
+
+				DDMFormFieldValue ddmFormFieldValue0 = ddmFormFieldValues.get(
+					0);
+
+				Value value = ddmFormFieldValue0.getValue();
+
+				classTypeValues.put(entry.getKey(), value.getString(locale));
+			}
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+		}
+
+		return classTypeValues;
 	}
 
 	@Override
@@ -76,7 +128,16 @@ public class JournalArticleAssetDisplayContributor
 			ResourceBundleLoaderUtil.getPortalResourceBundleLoader());
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalArticleAssetDisplayContributor.class);
+
+	@Reference
+	private FieldsToDDMFormValuesConverter _fieldsToDDMFormValuesConverter;
+
 	@Reference
 	private JournalContent _journalContent;
+
+	@Reference
+	private JournalConverter _journalConverter;
 
 }
