dde6f547e9961725e8817c3e87abe6ccc820916d
Zoltán Takács <zoltan.takacs@liferay.com>
2018-05-08 15:58:50 +0200

    COMMERCE-421 Helper method for adding new Price Lists

---
+++ modules/dxp/apps/commerce/commerce-data-integration/commerce-data-integration-apio-impl/src/main/java/com/liferay/commerce/data/integration/price/list/apio/internal/util/PriceListHelper.java
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.data.integration.price.list.apio.internal.util;
+
+import com.liferay.commerce.currency.exception.NoSuchCurrencyException;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.service.CommerceCurrencyService;
+import com.liferay.commerce.price.list.model.CommercePriceList;
+import com.liferay.commerce.price.list.service.CommercePriceListService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Zoltán Takács
+ */
+@Component(immediate = true, service = PriceListHelper.class)
+public class PriceListHelper {
+
+	public CommercePriceList addCommercePriceList(
+			long groupId, String currency, String name, Double priority,
+			Boolean neverExpire)
+
+		throws PortalException {
+
+		long commerceCurrencyId = _getCommerceCurrencyId(groupId, currency);
+
+		if (neverExpire == null) {
+			neverExpire = Boolean.TRUE;
+		}
+
+		if (priority == null) {
+			priority = 0D;
+		}
+
+		ServiceContext serviceContext = getServiceContext(groupId);
+
+		Calendar displayCalendar = CalendarFactoryUtil.getCalendar(
+			serviceContext.getTimeZone());
+
+		int displayDateMonth = displayCalendar.get(Calendar.MONTH);
+		int displayDateDay = displayCalendar.get(Calendar.DAY_OF_MONTH);
+		int displayDateYear = displayCalendar.get(Calendar.YEAR);
+		int displayDateHour = displayCalendar.get(Calendar.HOUR);
+		int displayDateMinute = displayCalendar.get(Calendar.MINUTE);
+		int displayDateAmPm = displayCalendar.get(Calendar.AM_PM);
+
+		if (displayDateAmPm == Calendar.PM) {
+			displayDateHour += 12;
+		}
+
+		Calendar expirationCalendar = CalendarFactoryUtil.getCalendar(
+			serviceContext.getTimeZone());
+
+		expirationCalendar.add(Calendar.MONTH, 1);
+
+		int expirationDateMonth = expirationCalendar.get(Calendar.MONTH);
+		int expirationDateDay = expirationCalendar.get(Calendar.DAY_OF_MONTH);
+		int expirationDateYear = expirationCalendar.get(Calendar.YEAR);
+		int expirationDateHour = expirationCalendar.get(Calendar.HOUR);
+		int expirationDateMinute = expirationCalendar.get(Calendar.MINUTE);
+		int expirationDateAmPm = expirationCalendar.get(Calendar.AM_PM);
+
+		if (expirationDateAmPm == Calendar.PM) {
+			expirationDateHour += 12;
+		}
+
+		return _commercePriceListService.addCommercePriceList(
+			commerceCurrencyId, name, priority, displayDateMonth,
+			displayDateDay, displayDateYear, displayDateHour, displayDateMinute,
+			expirationDateMonth, expirationDateDay, expirationDateYear,
+			expirationDateHour, expirationDateMinute, neverExpire,
+			serviceContext);
+	}
+
+	/**
+	 * Compose the ServiceContext object which needed for the operation on the
+	 * resource.
+	 *
+	 * @param  groupId
+	 * @return ServiceContext
+	 * @throws PortalException
+	 */
+	public ServiceContext getServiceContext(long groupId)
+		throws PortalException {
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext == null) {
+			serviceContext = new ServiceContext();
+		}
+
+		User user = _userService.getUserById(PrincipalThreadLocal.getUserId());
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setCompanyId(user.getCompanyId());
+		serviceContext.setScopeGroupId(groupId);
+		serviceContext.setTimeZone(user.getTimeZone());
+		serviceContext.setUserId(user.getUserId());
+
+		return serviceContext;
+	}
+
+	private long _getCommerceCurrencyId(Long groupId, String currencyCode)
+		throws NoSuchCurrencyException {
+
+		List<CommerceCurrency> commerceCurrencyList =
+			_commerceCurrencyService.getCommerceCurrencies(
+				groupId, true, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		Stream<CommerceCurrency> stream = commerceCurrencyList.stream();
+
+		CommerceCurrency commerceCurrency = stream.filter(
+			currency -> currencyCode.equals(currency.getCode())
+		).findFirst(
+		).orElseThrow(
+			() -> new NoSuchCurrencyException(
+				String.format(
+					"Unable to find currency with code: %s.", currencyCode))
+		);
+
+		return commerceCurrency.getCommerceCurrencyId();
+	}
+
+	@Reference
+	private CommerceCurrencyService _commerceCurrencyService;
+
+	@Reference
+	private CommercePriceListService _commercePriceListService;
+
+	@Reference
+	private UserService _userService;
+
+}
