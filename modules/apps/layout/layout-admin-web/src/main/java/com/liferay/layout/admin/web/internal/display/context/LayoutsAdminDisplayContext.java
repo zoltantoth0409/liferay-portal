86497feed5bae94bbb2ed37a9f40bee38f1feacc
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-05-08 10:53:28 +0100

    LPS-80228 SF

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
@@ -751,7 +751,6 @@ public class LayoutsAdminDisplayContext {
 
 		editLayoutURL.setParameter("mvcPath", "/edit_content_layout.jsp");
 		editLayoutURL.setParameter("backURL", _themeDisplay.getURLCurrent());
-
 		editLayoutURL.setParameter(
 			"groupId", String.valueOf(layout.getGroupId()));
 		editLayoutURL.setParameter("selPlid", String.valueOf(layout.getPlid()));
