016a467f520f8b005213373111be6d71660a5b87
drewbrokke <drew.brokke@liferay.com>
2018-05-09 16:36:33 -0700

    LPS-80666 user-associated-data-web - adds UADLanguageUtil.java to automatically retrieve the application names when given a bundle context and a locale

---
+++ modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/util/UADLanguageUtil.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.associated.data.web.internal.util;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleLoaderUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * @author Drew Brokke
+ */
+public class UADLanguageUtil {
+
+	public static String getApplicationName(
+		String applicationKey, Locale locale) {
+
+		ResourceBundleLoader resourceBundleLoader =
+			ResourceBundleLoaderUtil.
+				getResourceBundleLoaderByBundleSymbolicName(applicationKey);
+
+		if (resourceBundleLoader == null) {
+			return applicationKey;
+		}
+
+		ResourceBundle resourceBundle = resourceBundleLoader.loadResourceBundle(
+			locale);
+
+		return LanguageUtil.get(
+			resourceBundle, "application.name." + applicationKey,
+			applicationKey);
+	}
+
+}
