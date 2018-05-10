6de887b1ffddaa512e93058234e1e64fedb7791b
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2018-05-10 18:52:40 -0300

    Revert "LPS-80303 Apply Clay classes to alert float on the top of screen"
    This reverts commit d0b71825cc283bf98a4601b390f813a2ab59c2c6.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/admin/js/form_portlet.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/admin/js/form_portlet.js
@@ -1020,14 +1020,6 @@ AUI.add(
 							alert.render('.management-bar-default .container-fluid-1280');
 						}
 
-						var alertBoundingBox = alert.get('boundingBox');
-
-						var alertParent = alertBoundingBox.get('parentNode');
-
-						alertBoundingBox.setStyle('wordWrap', 'break-word');
-
-						alertParent.addClass('alert-notifications alert-notifications-fixed');
-
 						alert.show();
 
 						instance.set('alert', alert);
