95da79e95398dd338680148ac7b95fd0c7a7e4ba
Hugo Huijser <hugo.huijser@liferay.com>
2018-05-10 14:39:49 +0800

    LPS-80631 Split up into two classes

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaImportsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaImportsCheck.java
@@ -14,14 +14,10 @@
 
 package com.liferay.source.formatter.checks;
 
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.tools.ImportsFormatter;
 import com.liferay.portal.tools.JavaImportsFormatter;
 import com.liferay.source.formatter.checks.util.JavaSourceUtil;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Hugo Huijser
@@ -40,46 +36,7 @@ public class JavaImportsCheck extends BaseFileCheck {
 
 		content = importsFormatter.format(content, packageName, className);
 
-		content = StringUtil.replace(content, ";\n/**", ";\n\n/**");
-
-		Matcher matcher = _importMethodPattern.matcher(content);
-
-		while (matcher.find()) {
-			StringBundler sb = new StringBundler(5);
-
-			sb.append("Do not import method '");
-			sb.append(matcher.group(1));
-			sb.append("', import class '");
-			sb.append(matcher.group(2));
-			sb.append("' instead");
-
-			addMessage(
-				fileName, sb.toString(), "imports.markdown",
-				getLineCount(content, matcher.end()));
-		}
-
-		matcher = _importConstantPattern.matcher(content);
-
-		while (matcher.find()) {
-			StringBundler sb = new StringBundler(5);
-
-			sb.append("Do not import constant '");
-			sb.append(matcher.group(1));
-			sb.append("', import class '");
-			sb.append(matcher.group(2));
-			sb.append("' instead or use Fully Qualified Name");
-
-			addMessage(
-				fileName, sb.toString(), "imports.markdown",
-				getLineCount(content, matcher.end()));
-		}
-
-		return content;
+		return StringUtil.replace(content, ";\n/**", ";\n\n/**");
 	}
-
-	private final Pattern _importConstantPattern = Pattern.compile(
-		"\nimport static ((.*)\\.[A-Z_]*);");
-	private final Pattern _importMethodPattern = Pattern.compile(
-		"\nimport static ((.*\\.(Assert|(Power)?Mockito))\\.[a-z]\\w*);");
 
 }
---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaStaticImportsCheck.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.kernel.util.StringBundler;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaStaticImportsCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		Matcher matcher = _importMethodPattern.matcher(content);
+
+		while (matcher.find()) {
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("Do not import method '");
+			sb.append(matcher.group(1));
+			sb.append("', import class '");
+			sb.append(matcher.group(2));
+			sb.append("' instead");
+
+			addMessage(
+				fileName, sb.toString(), "imports.markdown",
+				getLineCount(content, matcher.end()));
+		}
+
+		matcher = _importConstantPattern.matcher(content);
+
+		while (matcher.find()) {
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("Do not import constant '");
+			sb.append(matcher.group(1));
+			sb.append("', import class '");
+			sb.append(matcher.group(2));
+			sb.append("' instead or use Fully Qualified Name");
+
+			addMessage(
+				fileName, sb.toString(), "imports.markdown",
+				getLineCount(content, matcher.end()));
+		}
+
+		return content;
+	}
+
+	private final Pattern _importConstantPattern = Pattern.compile(
+		"\nimport static ((.*)\\.[A-Z_]*);");
+	private final Pattern _importMethodPattern = Pattern.compile(
+		"\nimport static ((.*\\.(Assert|(Power)?Mockito))\\.[a-z]\\w*);");
+
+}
