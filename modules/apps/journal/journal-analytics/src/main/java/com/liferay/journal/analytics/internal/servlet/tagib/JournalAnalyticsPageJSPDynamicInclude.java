baf70685c262edb3f98a3c78862c5204fd3bf976
Lily.Chi <lily.chi@liferay.com>
2018-05-10 15:43:27 +0800

    Revert "LPS-80652 Renaming jsp"
    This reverts commit 1858b2f84b8b8f36b674d5d05a96c7b8aa91fe34.

--- modules/apps/journal/journal-analytics/src/main/java/com/liferay/journal/analytics/internal/servlet/tagib/JournalAnalyticsPageJSPDynamicInclude.java
+++ modules/apps/journal/journal-analytics/src/main/java/com/liferay/journal/analytics/internal/servlet/tagib/JournalAnalyticsPageJSPDynamicInclude.java
@@ -65,7 +65,7 @@ public class JournalAnalyticsPageJSPDynamicInclude
 
 	@Override
 	protected String getJspPath() {
-		return "/dynamic_include/page.jsp";
+		return "/dynamic_include/view.jsp";
 	}
 
 	@Override
