631b04f2f54a339aa55f19495e451c980601a4c0
spencerwoo <spencer.woo@liferay.com>
2018-05-10 10:40:02 -0700

    LPS-79505 Allow alignment styles

--- modules/apps/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsContentEditorConfigContributor.java
+++ modules/apps/blogs/blogs-editor-configuration/src/main/java/com/liferay/blogs/editor/configuration/internal/BlogsContentEditorConfigContributor.java
@@ -72,7 +72,7 @@ public class BlogsContentEditorConfigContributor
 
 		sb.append("a[*](*); ");
 		sb.append(getAllowedContentText());
-		sb.append(" div[*](*); iframe[*](*); img[*] {height, width}; ");
+		sb.append(" div[*](*); iframe[*](*); img[*] {display, float, height, margin-left, margin-right, width}; ");
 		sb.append(getAllowedContentLists());
 		sb.append(" p {text-align}; ");
 		sb.append(getAllowedContentTable());
