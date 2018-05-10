a5af91c2fb4cd91dd46013cb2dafbd2dc5f04a92
Zoltán Takács <zoltan.takacs@liferay.com>
2018-05-10 17:14:54 +0200

    COMMERCE-442 Add new Constant class for Price Entry fields

---
+++ modules/dxp/apps/commerce/commerce-data-integration/commerce-data-integration-apio-api/src/main/java/com/liferay/commerce/data/integration/apio/constants/PriceEntryFieldConstants.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.data.integration.apio.constants;
+
+/**
+ * @author Zoltán Takács
+ */
+public interface PriceEntryFieldConstants {
+
+	public static final String DATE_CREATED = "dateCreated";
+
+	public static final String DATE_MODIFIED = "dateModified";
+
+	public static final String NAME = "name";
+
+	public static final String PRICE = "price";
+
+	public static final String PROMO_PRICE = "promoPrice";
+
+	public static final String SKU = "sku";
+
+}
