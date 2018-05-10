aa66e433e87798a935ef47e4956dce85b6ced752
yichenroy <yi-chen.tsai@liferay.com>
2018-05-09 18:08:47 -0700

    LRQA-40666 Add PoshiProseFile class

---
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/prose/PoshiProseFile.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.prose;
+
+import com.liferay.poshi.runner.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Yi-Chen Tsai
+ */
+public class PoshiProseFile {
+
+	public PoshiProseFile(String fileName, String fileContent)
+		throws Exception {
+
+		_fileName = fileName;
+		_fileContent = fileContent;
+
+		List<String> poshiProseScenarioStrings = StringUtil.splitByKeys(
+			_fileContent, PoshiProseScenario.PROSE_SCENARIO_KEYWORDS);
+
+		for (String poshiProseScenarioString : poshiProseScenarioStrings) {
+			_poshiProseScenarios.add(
+				new PoshiProseScenario(poshiProseScenarioString));
+		}
+	}
+
+	public String getFileContent() {
+		return _fileContent;
+	}
+
+	public String getFileName() {
+		return _fileName;
+	}
+
+	private final String _fileContent;
+	private final String _fileName;
+	private final List<PoshiProseScenario> _poshiProseScenarios =
+		new ArrayList<>();
+
+}
