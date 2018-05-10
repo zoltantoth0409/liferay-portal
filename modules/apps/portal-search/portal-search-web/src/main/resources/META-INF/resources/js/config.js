f2fd4c51f7d8fecaa3af4a2448177d5f2f73ade7
André de Oliveira <andre.oliveira@liferay.com>
2018-05-09 18:54:42 -0700

    LPS-74277 portal-search-web: config.js - add modified_facet.js to project
    https://issues.liferay.com/browse/LPS-74277
    
    Co-authored-by: Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

--- modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/config.js
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/config.js
@@ -2,6 +2,17 @@
 	AUI().applyConfig(
 		{
 			groups: {
+				modified_facet: {
+					base: MODULE_PATH + '/js/',
+					combine: Liferay.AUI.getCombine(),
+					modules: {
+						'liferay-search-modified-facet': {
+							path: 'modified_facet.js',
+							requires: ['liferay-search-facet-util']
+						}
+					},
+					root: MODULE_PATH + '/js/'
+				},
 				search: {
 					base: MODULE_PATH + '/js/',
 					combine: Liferay.AUI.getCombine(),
