bf7681aef6f71fd6612f0b77d04796f82bd2a1e9
yichenroy <yi-chen.tsai@liferay.com>
2018-05-10 18:14:26 -0700

    LRQA-40666 Store variable values into a map for PoshiProseStatement

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/prose/PoshiProseStatement.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/prose/PoshiProseStatement.java
@@ -14,8 +14,11 @@
 
 package com.liferay.poshi.runner.prose;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -23,13 +26,63 @@ import java.util.regex.Pattern;
  */
 public class PoshiProseStatement {
 
-	public PoshiProseStatement(String proseStatement) {
+	public PoshiProseStatement(String proseStatement) throws Exception {
 		_proseStatement = proseStatement;
+
+		_poshiProseMatcher = PoshiProseMatcher.getPoshiProseMatcher(
+			_proseStatement.replaceAll(_varValuePattern.pattern(), "\"\""));
+
+		List<String> parameterNames = _poshiProseMatcher.getParameterNames();
+
+		List<String> parameterValues = new ArrayList<>();
+
+		Matcher matcher = _varValuePattern.matcher(_proseStatement);
+
+		while (matcher.find()) {
+			parameterValues.add(matcher.group(1));
+		}
+
+		for (int i = 0; i < parameterNames.size(); i++) {
+			String parameterName = parameterNames.get(i);
+			String parameterValue = parameterValues.get(i);
+
+			if (!_varValueMap.containsKey(parameterName)) {
+				_varValueMap.put(parameterName, parameterValue);
+
+				continue;
+			}
+
+			String varValue = _varValueMap.get(parameterName);
+
+			if (varValue.equals(parameterValue)) {
+				StringBuilder sb = new StringBuilder();
+
+				sb.append("Duplicate variable value assignment: ${");
+				sb.append(parameterName);
+				sb.append("} already has a value of ");
+				sb.append(varValue);
+				sb.append("\nProse statement: ");
+				sb.append(_proseStatement);
+				sb.append("\nMatching macro prose statement: ");
+				sb.append(_poshiProseMatcher.getPoshiProse());
+
+				throw new RuntimeException(sb.toString());
+			}
+		}
+	}
+
+	public String getProseStatement() {
+		return _proseStatement;
+	}
+
+	public Map<String, String> getVarValueMap() {
+		return _varValueMap;
 	}
 
 	private static final Pattern _varValuePattern = Pattern.compile(
 		"\"(.*?)\"");
 
+	private final PoshiProseMatcher _poshiProseMatcher;
 	private final String _proseStatement;
 	private final Map<String, String> _varValueMap = new HashMap<>();
 
