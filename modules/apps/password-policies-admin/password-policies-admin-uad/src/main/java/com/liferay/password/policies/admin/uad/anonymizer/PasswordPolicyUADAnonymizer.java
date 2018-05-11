1dd1b6c19bcbc54aefe47bef821c0d35fb2744fd
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-05-11 10:23:34 -0700

    LPS-80761 (password-policies-admin-uad) Auto SF

--- modules/apps/password-policies-admin/password-policies-admin-uad/src/main/java/com/liferay/password/policies/admin/uad/anonymizer/PasswordPolicyUADAnonymizer.java
+++ modules/apps/password-policies-admin/password-policies-admin-uad/src/main/java/com/liferay/password/policies/admin/uad/anonymizer/PasswordPolicyUADAnonymizer.java
@@ -22,5 +22,6 @@ import org.osgi.service.component.annotations.Component;
  * @author Brian Wing Shun Chan
  */
 @Component(immediate = true, service = UADAnonymizer.class)
-public class PasswordPolicyUADAnonymizer extends BasePasswordPolicyUADAnonymizer {
+public class PasswordPolicyUADAnonymizer
+	extends BasePasswordPolicyUADAnonymizer {
 }
