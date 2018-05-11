740baff7ebac83907511c0edb041529857451802
Bruno Basto <bruno.basto@liferay.com>
2018-05-11 17:29:09 -0300

    LPS-80826 Adds tracking of Web Contents to the Analytics Client

--- modules/apps/analytics/analytics-client-js/src/plugins/defaults.js
+++ modules/apps/analytics/analytics-client-js/src/plugins/defaults.js
@@ -3,6 +3,7 @@ import forms from './forms';
 import resolution from './resolution';
 import scrolling from './scrolling';
 import timing from './timing';
+import webContents from './web-contents';
 
-export {blogs, forms, resolution, scrolling, timing};
-export default [blogs, forms, resolution, scrolling, timing];
+export {blogs, forms, resolution, scrolling, timing, webContents};
+export default [blogs, forms, resolution, scrolling, timing, webContents];
---
+++ modules/apps/analytics/analytics-client-js/src/plugins/web-contents.js
@@ -0,0 +1,100 @@
+import {onReady} from '../utils/events.js';
+import {getClosestAssetElement} from '../utils/assets';
+
+const applicationId = 'WebContents';
+
+/**
+ * Returns analytics payload with WebContent information.
+ * @param {object} webContent The webContent DOM element
+ * @return {object} The payload with webContent information
+ */
+function getWebContentPayload(webContent) {
+	return {articleId: webContent.dataset.analyticsAssetId};
+}
+
+/**
+ * Wether a WebContent is trackable or not.
+ * @param {object} element The WebContent DOM element
+ * @return {boolean} True if the element is trackable.
+ */
+function isTrackableWebContent(element) {
+	return element && 'analyticsAssetId' in element.dataset;
+}
+
+/**
+ * Sends information when user clicks on a Web Content.
+ * @param {object} The Analytics client instance
+ */
+function trackWebContentClicked(analytics) {
+	const onClick = ({target}) => {
+		const webContentElement = getClosestAssetElement(target, 'web-content');
+
+		if (!isTrackableWebContent(webContentElement)) {
+			return;
+		}
+
+		const tagName = target.tagName.toLowerCase();
+
+		const payload = {
+			...getWebContentPayload(webContentElement),
+			tagName,
+		};
+
+		if (tagName === 'a') {
+			payload.href = target.href;
+			payload.text = target.innerText;
+		} else if (tagName === 'img') {
+			payload.src = target.src;
+		}
+
+		analytics.send('webContentClicked', applicationId, payload);
+	};
+
+	document.addEventListener('click', onClick);
+
+	return () => document.removeEventListener('click', onClick);
+}
+
+/**
+ * Sends information when user scrolls on a WebContent.
+ * @param {object} The Analytics client instance
+ */
+function trackWebContentViewed(analytics) {
+	const stopTrackingOnReady = onReady(() => {
+		Array.prototype.slice
+			.call(
+				document.querySelectorAll(
+					'[data-analytics-asset-type="web-content"]'
+				)
+			)
+			.filter(element => isTrackableWebContent(element))
+			.forEach(element => {
+				let payload = getWebContentPayload(element);
+				const title = element.dataset.analyticsAssetTitle;
+
+				if (title) {
+					payload = {title, ...payload};
+				}
+
+				analytics.send('webContentViewed', applicationId, payload);
+			});
+	});
+	return () => stopTrackingOnReady();
+}
+
+/**
+ * Plugin function that registers listeners for Web Content events
+ * @param {object} analytics The Analytics client
+ */
+function webContent(analytics) {
+	const stopTrackingWebContentClicked = trackWebContentClicked(analytics);
+	const stopTrackingWebContentViewed = trackWebContentViewed(analytics);
+
+	return () => {
+		stopTrackingWebContentClicked();
+		stopTrackingWebContentViewed();
+	};
+}
+
+export {webContent};
+export default webContent;
