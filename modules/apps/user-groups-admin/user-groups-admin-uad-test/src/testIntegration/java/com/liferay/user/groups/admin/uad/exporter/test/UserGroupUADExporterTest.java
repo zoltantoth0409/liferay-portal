37f4504de8f45a7d94a803b262d85386a955da35
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-05-11 10:57:34 -0700

    LPS-80763 User service builder to generate base UserGroup uad implementations

---
+++ modules/apps/user-groups-admin/user-groups-admin-uad-test/src/testIntegration/java/com/liferay/user/groups/admin/uad/exporter/test/UserGroupUADExporterTest.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.groups.admin.uad.exporter.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+
+import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import com.liferay.user.associated.data.exporter.UADExporter;
+import com.liferay.user.associated.data.test.util.BaseUADExporterTestCase;
+import com.liferay.user.groups.admin.uad.test.UserGroupUADTestHelper;
+
+import org.junit.After;
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+@RunWith(Arquillian.class)
+public class UserGroupUADExporterTest extends BaseUADExporterTestCase<UserGroup> {
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule = new LiferayIntegrationTestRule();
+
+	@After
+	public void tearDown() throws Exception {
+		_userGroupUADTestHelper.cleanUpDependencies(_userGroups);
+	}
+
+	@Override
+	protected UserGroup addBaseModel(long userId) throws Exception {
+		UserGroup userGroup = _userGroupUADTestHelper.addUserGroup(userId);
+
+		_userGroups.add(userGroup);
+
+		return userGroup;
+	}
+
+	@Override
+	protected String getPrimaryKeyName() {
+		return "userGroupId";
+	}
+
+	@Override
+	protected UADExporter getUADExporter() {
+		return _uadExporter;
+	}
+
+	@DeleteAfterTestRun
+	private final List<UserGroup> _userGroups = new ArrayList<UserGroup>();
+	@Inject
+	private UserGroupUADTestHelper _userGroupUADTestHelper;
+	@Inject(filter = "component.name=*.UserGroupUADExporter")
+	private UADExporter _uadExporter;
+}
---
+++ modules/apps/user-groups-admin/user-groups-admin-uad-test/src/testIntegration/java/com/liferay/user/groups/admin/uad/test/UserGroupUADTestHelper.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.groups.admin.uad.test;
+
+import com.liferay.portal.kernel.model.UserGroup;
+
+import org.junit.Assume;
+
+import org.osgi.service.component.annotations.Component;
+
+import java.util.List;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(immediate = true, service = UserGroupUADTestHelper.class)
+public class UserGroupUADTestHelper {
+	/**
+	 * Implement addUserGroup() to enable some UAD tests.
+	 *
+	 * <p>
+	 * Several UAD tests depend on creating one or more valid UserGroups with a specified user ID in order to execute correctly. Implement addUserGroup() such that it creates a valid UserGroup with the specified user ID value and returns it in order to enable the UAD tests that depend on it.
+	 * </p>
+	 */
+	public UserGroup addUserGroup(long userId) throws Exception {
+		Assume.assumeTrue(false);
+
+		return null;
+	}
+
+	/**
+	 * Implement cleanUpDependencies(List<UserGroup> userGroups) if tests require additional tear down logic.
+	 *
+	 * <p>
+	 * Several UAD tests depend on creating one or more valid UserGroups with specified user ID and status by user ID in order to execute correctly. Implement cleanUpDependencies(List<UserGroup> userGroups) such that any additional objects created during the construction of userGroups are safely removed.
+	 * </p>
+	 */
+	public void cleanUpDependencies(List<UserGroup> userGroups)
+		throws Exception {
+	}
+}
