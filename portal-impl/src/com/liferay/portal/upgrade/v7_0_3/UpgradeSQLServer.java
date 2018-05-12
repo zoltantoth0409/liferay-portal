963d764e87a6f39948e717a3ddce6ec8d403153e
Preston-Crary <preston.crary@liferay.com>
2018-05-11 17:20:11 -0700

    LPS-78583 Use DatabaseMetaData rather than system tables. We only want to upgrade stuff in our database.

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeSQLServer.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_3/UpgradeSQLServer.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.upgrade.v7_0_3;
 
 import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.log.Log;
@@ -22,8 +23,9 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
 
-import java.sql.PreparedStatement;
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -33,43 +35,61 @@ import java.sql.SQLException;
 public class UpgradeSQLServer extends UpgradeProcess {
 
 	protected void alterNVarcharColumns() throws Exception {
-		StringBundler sb = new StringBundler(3);
+		DatabaseMetaData databaseMetaData = connection.getMetaData();
+		DBInspector dbInspector = new DBInspector(connection);
 
-		sb.append("select table_name, column_name from ");
-		sb.append("INFORMATION_SCHEMA.COLUMNS where DATA_TYPE = 'nvarchar' ");
-		sb.append("and character_maximum_length = 2000");
+		String catalog = dbInspector.getCatalog();
+		String schema = dbInspector.getSchema();
 
 		try (LoggingTimer loggingTimer = new LoggingTimer();
-			PreparedStatement ps = connection.prepareStatement(sb.toString());
-			ResultSet rs = ps.executeQuery()) {
+			ResultSet tableRS = databaseMetaData.getTables(
+				catalog, schema, null, new String[] {"TABLE"})) {
 
-			while (rs.next()) {
-				String tableName = rs.getString(1);
-				String columnName = rs.getString(2);
+			while (tableRS.next()) {
+				String tableName = tableRS.getString("TABLE_NAME");
 
-				try {
-					runSQL(
-						StringBundler.concat(
-							"alter table ", tableName, " alter column ",
-							columnName, " nvarchar(4000)"));
-				}
-				catch (SQLException sqle) {
-					if (sqle.getErrorCode() == 1441) {
-						if (_log.isWarnEnabled()) {
-							sb = new StringBundler(6);
+				try (ResultSet columnRS = databaseMetaData.getColumns(
+						catalog, schema, tableName, null)) {
 
-							sb.append("Unable to alter length of column ");
-							sb.append(columnName);
-							sb.append(" for table ");
-							sb.append(tableName);
-							sb.append(" because it contains values larger ");
-							sb.append("than the new column length");
+					while (columnRS.next()) {
+						int size = columnRS.getInt("COLUMN_SIZE");
 
-							_log.warn(sb.toString());
+						if ((size == 2000) &&
+							StringUtil.equalsIgnoreCase(
+								"nvarchar", columnRS.getString("TYPE_NAME"))) {
+
+							String columnName = columnRS.getString(
+								"COLUMN_NAME");
+
+							try {
+								runSQL(
+									StringBundler.concat(
+										"alter table ", tableName,
+										" alter column ", columnName,
+										" nvarchar(4000)"));
+							}
+							catch (SQLException sqle) {
+								if (sqle.getErrorCode() == 1441) {
+									if (_log.isWarnEnabled()) {
+										StringBundler sb = new StringBundler(8);
+
+										sb.append("Unable to alter length of ");
+										sb.append("column ");
+										sb.append(columnName);
+										sb.append(" for table ");
+										sb.append(tableName);
+										sb.append(" because it contains ");
+										sb.append("values larger than the ");
+										sb.append("new column length");
+
+										_log.warn(sb.toString());
+									}
+								}
+								else {
+									throw sqle;
+								}
+							}
 						}
-					}
-					else {
-						throw sqle;
 					}
 				}
 			}
