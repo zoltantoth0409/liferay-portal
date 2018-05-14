f8ab6e0c7eaaa9fcc3a82e3a6c0e3e7433bdeefe
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-05-14 16:36:16 +0200

    LPS-76663 Update the thread whenever the root message is updated. This will ensure that the modifiedDate of the thread is kept updated

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBMessageLocalServiceImpl.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBMessageLocalServiceImpl.java
@@ -2757,8 +2757,10 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 			updatePriorities(thread.getThreadId(), priority);
 		}
 
-		if (message.isRoot() && !Objects.equals(subject, oldSubject)) {
-			thread.setTitle(subject);
+		if (message.isRoot()) {
+			if (!Objects.equals(subject, oldSubject)) {
+				thread.setTitle(subject);
+			}
 
 			mbThreadLocalService.updateMBThread(thread);
 		}
