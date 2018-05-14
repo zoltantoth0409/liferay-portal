48b64c64979b1298f5b12750e6a978fe6e0ee95b
Marcellus Tavares <marcellus.tavares@liferay.com>
2018-05-14 16:57:10 -0300

    LPS-80756 Add Group type Settings Constant

--- portal-kernel/src/com/liferay/portal/kernel/model/GroupConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/GroupConstants.java
@@ -51,6 +51,9 @@ public class GroupConstants {
 	public static final String TYPE_SETTINGS_KEY_INHERIT_LOCALES =
 		"inheritLocales";
 
+	public static final String TYPE_SETTINGS_KEY_LIFERAY_ANALYTICS_KEY =
+		"liferayAnalyticsKey";
+
 	public static final int TYPE_SITE_OPEN = 1;
 
 	public static final String TYPE_SITE_OPEN_LABEL = "open";
