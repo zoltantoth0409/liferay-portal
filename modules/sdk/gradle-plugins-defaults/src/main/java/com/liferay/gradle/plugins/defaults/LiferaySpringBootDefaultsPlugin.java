4ce673082ad538351ef1384b61a0e737c31d10fd
Peter Shin <peter.shin@liferay.com>
2018-05-15 15:05:57 -0700

    LPS-80944 Add plugin

---
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferaySpringBootDefaultsPlugin.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.gradle.plugins.defaults;
+
+import com.liferay.gradle.plugins.SourceFormatterDefaultsPlugin;
+import com.liferay.gradle.plugins.defaults.internal.util.GradleUtil;
+import com.liferay.gradle.plugins.source.formatter.SourceFormatterPlugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.plugins.BasePlugin;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.plugins.ide.eclipse.EclipsePlugin;
+import org.gradle.plugins.ide.idea.IdeaPlugin;
+
+import org.springframework.boot.gradle.plugin.SpringBootPlugin;
+
+/**
+ * @author Peter Shin
+ */
+public class LiferaySpringBootDefaultsPlugin implements Plugin<Project> {
+
+	public static final String RUN_TASK_NAME = "run";
+
+	@Override
+	public void apply(Project project) {
+		_applyPlugins(project);
+
+		_configureProject(project);
+
+		_addTaskRun(project);
+	}
+
+	private Task _addTaskRun(Project project) {
+		Task runTask = project.task(RUN_TASK_NAME);
+
+		runTask.dependsOn(GradleUtil.getTask(project, "bootRun"));
+		runTask.setDescription("Runs Spring Boot 'bootRun'.");
+		runTask.setGroup(BasePlugin.BUILD_GROUP);
+
+		return runTask;
+	}
+
+	private void _applyPlugins(Project project) {
+		GradleUtil.applyPlugin(project, EclipsePlugin.class);
+		GradleUtil.applyPlugin(project, IdeaPlugin.class);
+		GradleUtil.applyPlugin(project, JavaPlugin.class);
+		GradleUtil.applyPlugin(project, SourceFormatterDefaultsPlugin.class);
+		GradleUtil.applyPlugin(project, SourceFormatterPlugin.class);
+		GradleUtil.applyPlugin(project, SpringBootPlugin.class);
+	}
+
+	private void _configureProject(Project project) {
+		String group = GradleUtil.getGradlePropertiesValue(
+			project, "project.group", _GROUP);
+
+		project.setGroup(group);
+	}
+
+	private static final String _GROUP = "com.liferay.plugins";
+
+}
