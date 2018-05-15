a6d92a4b02ee7ac16808974299d53ac8dcd284fc
drewbrokke <drew.brokke@liferay.com>
2018-05-15 12:02:56 -0700

    LPS-80920 layout-uad-test - autogenerated

--- modules/apps/layout/layout-uad-test/src/testIntegration/java/com/liferay/layout/uad/exporter/test/LayoutSetBranchUADExporterTest.java
+++ modules/apps/layout/layout-uad-test/src/testIntegration/java/com/liferay/layout/uad/exporter/test/LayoutSetBranchUADExporterTest.java
@@ -38,7 +38,6 @@ import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
- * @generated
  */
 @RunWith(Arquillian.class)
 public class LayoutSetBranchUADExporterTest extends BaseUADExporterTestCase<LayoutSetBranch> {
