339c78a1e4c7743d3bb86e5fc301d71bf35941d1
drewbrokke <drew.brokke@liferay.com>
2018-05-15 12:06:32 -0700

    LPS-80920 site-teams-uad-test - auto SF

--- modules/apps/site/site-teams-uad-test/src/testIntegration/java/com/liferay/site/teams/uad/anonymizer/test/TeamUADAnonymizerTest.java
+++ modules/apps/site/site-teams-uad-test/src/testIntegration/java/com/liferay/site/teams/uad/anonymizer/test/TeamUADAnonymizerTest.java
@@ -15,7 +15,6 @@
 package com.liferay.site.teams.uad.anonymizer.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-
 import com.liferay.portal.kernel.model.Team;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.TeamLocalService;
@@ -23,29 +22,28 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-
 import com.liferay.site.teams.uad.test.TeamUADTestHelper;
-
 import com.liferay.user.associated.data.anonymizer.UADAnonymizer;
 import com.liferay.user.associated.data.test.util.BaseUADAnonymizerTestCase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Rule;
-
 import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
  */
 @RunWith(Arquillian.class)
 public class TeamUADAnonymizerTest extends BaseUADAnonymizerTestCase<Team> {
+
 	@ClassRule
 	@Rule
-	public static final AggregateTestRule aggregateTestRule = new LiferayIntegrationTestRule();
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
 
 	@After
 	public void tearDown() throws Exception {
@@ -60,6 +58,7 @@ public class TeamUADAnonymizerTest extends BaseUADAnonymizerTestCase<Team> {
 	@Override
 	protected Team addBaseModel(long userId, boolean deleteAfterTestRun)
 		throws Exception {
+
 		Team team = _teamUADTestHelper.addTeam(userId);
 
 		if (deleteAfterTestRun) {
@@ -70,8 +69,7 @@ public class TeamUADAnonymizerTest extends BaseUADAnonymizerTestCase<Team> {
 	}
 
 	@Override
-	protected void deleteBaseModels(List<Team> baseModels)
-		throws Exception {
+	protected void deleteBaseModels(List<Team> baseModels) throws Exception {
 		_teamUADTestHelper.cleanUpDependencies(baseModels);
 	}
 
@@ -83,12 +81,14 @@ public class TeamUADAnonymizerTest extends BaseUADAnonymizerTestCase<Team> {
 	@Override
 	protected boolean isBaseModelAutoAnonymized(long baseModelPK, User user)
 		throws Exception {
+
 		Team team = _teamLocalService.getTeam(baseModelPK);
 
 		String userName = team.getUserName();
 
 		if ((team.getUserId() != user.getUserId()) &&
-				!userName.equals(user.getFullName())) {
+			!userName.equals(user.getFullName())) {
+
 			return true;
 		}
 
@@ -104,12 +104,16 @@ public class TeamUADAnonymizerTest extends BaseUADAnonymizerTestCase<Team> {
 		return false;
 	}
 
-	@DeleteAfterTestRun
-	private final List<Team> _teams = new ArrayList<Team>();
 	@Inject
 	private TeamLocalService _teamLocalService;
+
+	@DeleteAfterTestRun
+	private final List<Team> _teams = new ArrayList<>();
+
 	@Inject
 	private TeamUADTestHelper _teamUADTestHelper;
+
 	@Inject(filter = "component.name=*.TeamUADAnonymizer")
 	private UADAnonymizer _uadAnonymizer;
+
 }
