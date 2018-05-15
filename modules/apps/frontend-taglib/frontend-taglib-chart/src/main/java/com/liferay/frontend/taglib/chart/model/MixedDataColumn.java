58202d4d9e68a703cc0586e76b9da9ad32f87b00
codyhoag <cody.hoag@liferay.com>
2018-05-14 21:12:05 -0400

    LPS-80532 LRDOCS-5291 Edit Javadoc

--- modules/apps/frontend-taglib/frontend-taglib-chart/src/main/java/com/liferay/frontend/taglib/chart/model/MixedDataColumn.java
+++ modules/apps/frontend-taglib/frontend-taglib-chart/src/main/java/com/liferay/frontend/taglib/chart/model/MixedDataColumn.java
@@ -23,10 +23,11 @@ import java.util.List;
 public class MixedDataColumn extends Column {
 
 	/**
-	 * Creates a new MixedDataColumn.
+	 * Creates a new mixed data column.
 	 *
-	 * @param id the ID of the column
-	 * @param values the values for the column. Each value can be an instance, array, or {@link Collection} of {@link Number}s.
+	 * @param id the column's ID
+	 * @param values the column's values. Each value can be an instance, array,
+	 *        or {@link Collection} of {@link Number}s.
 	 */
 	public MixedDataColumn(String id, Collection<?> values) {
 		super(id);
@@ -35,10 +36,11 @@ public class MixedDataColumn extends Column {
 	}
 
 	/**
-	 * Creates a new MixedDataColumn.
+	 * Creates a new mixed data column.
 	 *
-	 * @param id the ID of the column
-	 * @param values the values for the column. Each value can be an instance or array of {@link Number}s.
+	 * @param id the column's ID
+	 * @param values the column's values. Each value can be an instance or array
+	 *        of {@link Number}s.
 	 */
 	public MixedDataColumn(String id, Object... values) {
 		super(id);
@@ -47,7 +49,7 @@ public class MixedDataColumn extends Column {
 	}
 
 	/**
-	 * Adds a value to this column.
+	 * Adds a value to the column.
 	 *
 	 * @param value an array of {@link Number}s for the column values
 	 */
@@ -73,9 +75,9 @@ public class MixedDataColumn extends Column {
 	}
 
 	/**
-	 * Adds one or several values to this column.
+	 * Adds one or several values to the column.
 	 *
-	 * @param values a {@link Collection} of {@link Number}s for the column values
+	 * @param values the {@link Number}s for the column values
 	 */
 	public void addValues(Collection<?> values) {
 		for (Object value : values) {
@@ -84,7 +86,7 @@ public class MixedDataColumn extends Column {
 	}
 
 	/**
-	 * Adds one or several values to this column.
+	 * Adds one or several values to the column.
 	 *
 	 * @param values an array of {@link Number}s for the column values
 	 */
