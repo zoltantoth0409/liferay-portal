340abe8738a81266cfdb2121b96f9e4eba7b3ea5
drewbrokke <drew.brokke@liferay.com>
2018-05-15 12:03:20 -0700

    LPS-80920 roles-uad-test - autogenerated

--- modules/apps/roles/roles-uad-test/src/testIntegration/java/com/liferay/roles/uad/exporter/test/RoleUADExporterTest.java
+++ modules/apps/roles/roles-uad-test/src/testIntegration/java/com/liferay/roles/uad/exporter/test/RoleUADExporterTest.java
@@ -38,7 +38,6 @@ import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
- * @generated
  */
 @RunWith(Arquillian.class)
 public class RoleUADExporterTest extends BaseUADExporterTestCase<Role> {
