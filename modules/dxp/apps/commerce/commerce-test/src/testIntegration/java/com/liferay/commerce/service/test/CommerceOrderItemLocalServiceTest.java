443d7eb6f0e6fdf3ad67122505c9a80358126ccd
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-05-15 20:14:06 +0200

    COMMERCE-397 refactoring of CommerceOrderItemLocalServiceTest

--- modules/dxp/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/service/test/CommerceOrderItemLocalServiceTest.java
+++ modules/dxp/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/service/test/CommerceOrderItemLocalServiceTest.java
@@ -12,31 +12,26 @@
  * details.
  */
 
-package com.liferay.commerce.product.test;
+package com.liferay.commerce.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.commerce.context.CommerceContext;
 import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.currency.service.CommerceCurrencyLocalService;
 import com.liferay.commerce.exception.CommerceOrderValidatorException;
-import com.liferay.commerce.model.CommerceCountry;
+import com.liferay.commerce.internal.test.util.CommerceTestUtil;
 import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.model.CommerceOrderItem;
-import com.liferay.commerce.model.CommerceRegion;
 import com.liferay.commerce.model.CommerceWarehouse;
 import com.liferay.commerce.price.list.model.CommercePriceList;
 import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.model.CPInstanceConstants;
 import com.liferay.commerce.product.model.CPRule;
-import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.CPInstanceLocalService;
 import com.liferay.commerce.product.test.util.CPTestUtil;
-import com.liferay.commerce.service.CommerceCountryLocalService;
 import com.liferay.commerce.service.CommerceOrderItemLocalService;
 import com.liferay.commerce.service.CommerceOrderLocalService;
-import com.liferay.commerce.service.CommerceRegionLocalService;
-import com.liferay.commerce.service.CommerceWarehouseItemLocalService;
-import com.liferay.commerce.service.CommerceWarehouseLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Organization;
@@ -45,7 +40,6 @@ import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -80,14 +74,74 @@ public class CommerceOrderItemLocalServiceTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
-
 		_user = UserTestUtil.addUser();
 	}
 
-	@Test(expected = CommerceOrderValidatorException.class)
-	public void testAddNotPublishedProductToOrder() throws Exception {
+	@Test
+	public void testAddCommerceOrderItem() throws Exception {
 		frutillaRule.scenario(
-			"Add a SKU (cpInstance) to an order"
+			"Add a SKU (product instance) to an order"
+		).given(
+			"A group"
+		).and(
+			"A user"
+		).and(
+			"A published SKU"
+		).when(
+			"There is availability for the SKU"
+		).then(
+			"I should be able to add the SKU to an order"
+		);
+
+		CPInstance cpInstance = CPTestUtil.addCPInstance(_group.getGroupId());
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		CommerceWarehouse commerceWarehouse =
+			CommerceTestUtil.addCommerceWarehouse(_group.getGroupId());
+
+		CommerceTestUtil.addCommerceWarehouseItem(
+			commerceWarehouse, cpInstance.getCPInstanceId(), 2);
+
+		CommerceOrder commerceOrder =
+			_commerceOrderLocalService.addUserCommerceOrder(
+				_group.getGroupId(), _user.getUserId());
+
+		CommerceCurrency commerceCurrency =
+			_commerceCurrencyLocalService.fetchPrimaryCommerceCurrency(
+				_group.getGroupId());
+
+		Assert.assertNotNull(commerceCurrency);
+
+		CommerceContext commerceContext = new TestCommerceContext(
+			commerceCurrency);
+
+		CommerceOrderItem commerceOrderItem =
+			_commerceOrderItemLocalService.addCommerceOrderItem(
+				commerceOrder.getCommerceOrderId(),
+				cpInstance.getCPInstanceId(), 1, 0, null, new BigDecimal(3),
+				commerceContext, serviceContext);
+
+		List<CommerceOrderItem> commerceOrderItems =
+			commerceOrder.getCommerceOrderItems();
+
+		Assert.assertEquals(
+			commerceOrderItems.toString(), 1, commerceOrderItems.size());
+
+		CommerceOrderItem actualCommerceOrderItem = commerceOrderItems.get(0);
+
+		Assert.assertEquals(
+			commerceOrderItem.getCommerceOrderItemId(),
+			actualCommerceOrderItem.getCommerceOrderItemId());
+	}
+
+	@Test(expected = CommerceOrderValidatorException.class)
+	public void testAddCommerceOrderItemWithDraftCPDefinition()
+		throws Exception {
+
+		frutillaRule.scenario(
+			"Add a SKU (product instance) to an order"
 		).given(
 			"A group"
 		).and(
@@ -97,41 +151,24 @@ public class CommerceOrderItemLocalServiceTest {
 		).when(
 			"There is availability for the SKU"
 		).then(
-			"I'm able to add the SKU to an order"
+			"I should be able to add the SKU to an order"
 		);
 
-		CPInstance cpInstance = CPTestUtil.addCPInstance(_group.getGroupId());
+		CPDefinition cpDefinition = CPTestUtil.addCPDefinition(
+			_group.getGroupId(), false, true,
+			WorkflowConstants.ACTION_SAVE_DRAFT);
 
 		ServiceContext serviceContext =
 			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
 
-		CPDefinition cpDefinition = cpInstance.getCPDefinition();
-
-		_cpDefinitionLocalService.updateStatus(
-			_user.getUserId(), cpDefinition.getCPDefinitionId(),
-			WorkflowConstants.STATUS_DRAFT, serviceContext, null);
-
-		CommerceCountry commerceCountry =
-			_commerceCountryLocalService.fetchCommerceCountry(
-				serviceContext.getScopeGroupId(), 380);
-
-		CommerceRegion commerceRegion =
-			_commerceRegionLocalService.getCommerceRegion(
-				commerceCountry.getCommerceCountryId(), "VE");
-
 		CommerceWarehouse commerceWarehouse =
-			_commerceWarehouseLocalService.addCommerceWarehouse(
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				true, RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				commerceRegion.getCommerceRegionId(),
-				commerceCountry.getCommerceCountryId(), 45.4386111, 12.3266667,
-				serviceContext);
+			CommerceTestUtil.addCommerceWarehouse(_group.getGroupId());
 
-		_commerceWarehouseItemLocalService.addCommerceWarehouseItem(
-			commerceWarehouse.getCommerceWarehouseId(),
-			cpInstance.getCPInstanceId(), 2, serviceContext);
+		CPInstance cpInstance = _cpInstanceLocalService.getCPInstance(
+			cpDefinition.getCPDefinitionId(), CPInstanceConstants.DEFAULT_SKU);
+
+		CommerceTestUtil.addCommerceWarehouseItem(
+			commerceWarehouse, cpInstance.getCPInstanceId(), 2);
 
 		CommerceOrder commerceOrder =
 			_commerceOrderLocalService.addUserCommerceOrder(
@@ -140,6 +177,8 @@ public class CommerceOrderItemLocalServiceTest {
 		CommerceCurrency commerceCurrency =
 			_commerceCurrencyLocalService.fetchPrimaryCommerceCurrency(
 				_group.getGroupId());
+
+		Assert.assertNotNull(commerceCurrency);
 
 		CommerceContext commerceContext = new TestCommerceContext(
 			commerceCurrency);
@@ -150,9 +189,9 @@ public class CommerceOrderItemLocalServiceTest {
 	}
 
 	@Test(expected = CommerceOrderValidatorException.class)
-	public void testAddNotPublishedSKUToOrder() throws Exception {
+	public void testAddCommerceOrderItemWithDraftCPInstance() throws Exception {
 		frutillaRule.scenario(
-			"Add a SKU (cpInstance) to an order"
+			"Add a SKU (product instance) to an order"
 		).given(
 			"A group"
 		).and(
@@ -162,7 +201,7 @@ public class CommerceOrderItemLocalServiceTest {
 		).when(
 			"There is availability for the SKU"
 		).then(
-			"I'm not able to add the SKU to an order"
+			"I should not be able to add the SKU to an order"
 		);
 
 		CPInstance cpInstance = CPTestUtil.addCPInstance(_group.getGroupId());
@@ -174,27 +213,11 @@ public class CommerceOrderItemLocalServiceTest {
 			_user.getUserId(), cpInstance.getCPInstanceId(),
 			WorkflowConstants.STATUS_DRAFT, serviceContext, null);
 
-		CommerceCountry commerceCountry =
-			_commerceCountryLocalService.fetchCommerceCountry(
-				serviceContext.getScopeGroupId(), 380);
-
-		CommerceRegion commerceRegion =
-			_commerceRegionLocalService.getCommerceRegion(
-				commerceCountry.getCommerceCountryId(), "VE");
-
 		CommerceWarehouse commerceWarehouse =
-			_commerceWarehouseLocalService.addCommerceWarehouse(
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				true, RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				commerceRegion.getCommerceRegionId(),
-				commerceCountry.getCommerceCountryId(), 45.4386111, 12.3266667,
-				serviceContext);
+			CommerceTestUtil.addCommerceWarehouse(_group.getGroupId());
 
-		_commerceWarehouseItemLocalService.addCommerceWarehouseItem(
-			commerceWarehouse.getCommerceWarehouseId(),
-			cpInstance.getCPInstanceId(), 2, serviceContext);
+		CommerceTestUtil.addCommerceWarehouseItem(
+			commerceWarehouse, cpInstance.getCPInstanceId(), 2);
 
 		CommerceOrder commerceOrder =
 			_commerceOrderLocalService.addUserCommerceOrder(
@@ -203,6 +226,8 @@ public class CommerceOrderItemLocalServiceTest {
 		CommerceCurrency commerceCurrency =
 			_commerceCurrencyLocalService.fetchPrimaryCommerceCurrency(
 				_group.getGroupId());
+
+		Assert.assertNotNull(commerceCurrency);
 
 		CommerceContext commerceContext = new TestCommerceContext(
 			commerceCurrency);
@@ -212,88 +237,7 @@ public class CommerceOrderItemLocalServiceTest {
 			0, null, new BigDecimal(3), commerceContext, serviceContext);
 	}
 
-	@Test
-	public void testAddSKUToOrder() throws Exception {
-		frutillaRule.scenario(
-			"Add a SKU (cpInstance) to an order"
-		).given(
-			"A group"
-		).and(
-			"A user"
-		).and(
-			"A published SKU"
-		).when(
-			"There is availability for the SKU"
-		).then(
-			"I'm able to add the SKU to an order"
-		);
-
-		CPInstance cpInstance = CPTestUtil.addCPInstance(_group.getGroupId());
-
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
-
-		CommerceCountry commerceCountry =
-			_commerceCountryLocalService.fetchCommerceCountry(
-				serviceContext.getScopeGroupId(), 380);
-
-		CommerceRegion commerceRegion =
-			_commerceRegionLocalService.getCommerceRegion(
-				commerceCountry.getCommerceCountryId(), "VE");
-
-		CommerceWarehouse commerceWarehouse =
-			_commerceWarehouseLocalService.addCommerceWarehouse(
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				true, RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-				commerceRegion.getCommerceRegionId(),
-				commerceCountry.getCommerceCountryId(), 45.4386111, 12.3266667,
-				serviceContext);
-
-		_commerceWarehouseItemLocalService.addCommerceWarehouseItem(
-			commerceWarehouse.getCommerceWarehouseId(),
-			cpInstance.getCPInstanceId(), 2, serviceContext);
-
-		CommerceOrder commerceOrder =
-			_commerceOrderLocalService.addUserCommerceOrder(
-				_group.getGroupId(), _user.getUserId());
-
-		CommerceCurrency commerceCurrency =
-			_commerceCurrencyLocalService.fetchPrimaryCommerceCurrency(
-				_group.getGroupId());
-
-		CommerceContext commerceContext = new TestCommerceContext(
-			commerceCurrency);
-
-		CommerceOrderItem commerceOrderItem =
-			_commerceOrderItemLocalService.addCommerceOrderItem(
-				commerceOrder.getCommerceOrderId(),
-				cpInstance.getCPInstanceId(), 1, 0, null, new BigDecimal(3),
-				commerceContext, serviceContext);
-
-		int end = _commerceOrderItemLocalService.getCommerceOrderItemsCount(
-			commerceOrder.getCommerceOrderId());
-
-		List<CommerceOrderItem> commerceOrderItems =
-			_commerceOrderItemLocalService.getCommerceOrderItems(
-				commerceOrder.getCommerceOrderId(), 0, end);
-
-		Assert.assertEquals(
-			commerceOrderItems.toString(), 1, commerceOrderItems.size());
-
-		CommerceOrderItem commerceOrderItemRetrieved = commerceOrderItems.get(
-			0);
-
-		Assert.assertEquals(
-			commerceOrderItem.getCommerceOrderItemId(),
-			commerceOrderItemRetrieved.getCommerceOrderItemId());
-	}
-
 	public FrutillaRule frutillaRule = new FrutillaRule();
-
-	@Inject
-	private CommerceCountryLocalService _commerceCountryLocalService;
 
 	@Inject
 	private CommerceCurrencyLocalService _commerceCurrencyLocalService;
@@ -303,19 +247,6 @@ public class CommerceOrderItemLocalServiceTest {
 
 	@Inject
 	private CommerceOrderLocalService _commerceOrderLocalService;
-
-	@Inject
-	private CommerceRegionLocalService _commerceRegionLocalService;
-
-	@Inject
-	private CommerceWarehouseItemLocalService
-		_commerceWarehouseItemLocalService;
-
-	@Inject
-	private CommerceWarehouseLocalService _commerceWarehouseLocalService;
-
-	@Inject
-	private CPDefinitionLocalService _cpDefinitionLocalService;
 
 	@Inject
 	private CPInstanceLocalService _cpInstanceLocalService;
