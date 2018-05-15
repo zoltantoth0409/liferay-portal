951cf51862132ef2ee1cd857d3dc261e30118c14
drewbrokke <drew.brokke@liferay.com>
2018-05-15 09:36:50 -0700

    LPS-80920 document-library-uad - DLFileEntryUADExporter.java - includes the actual file from DLFileEntry in the exported zip

--- modules/apps/document-library/document-library-uad/src/main/java/com/liferay/document/library/uad/exporter/DLFileEntryUADExporter.java
+++ modules/apps/document-library/document-library-uad/src/main/java/com/liferay/document/library/uad/exporter/DLFileEntryUADExporter.java
@@ -14,13 +14,32 @@
 
 package com.liferay.document.library.uad.exporter;
 
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.zip.ZipWriter;
 import com.liferay.user.associated.data.exporter.UADExporter;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Brian Wing Shun Chan
+ * @author Drew Brokke
  */
 @Component(immediate = true, service = UADExporter.class)
 public class DLFileEntryUADExporter extends BaseDLFileEntryUADExporter {
+
+	@Override
+	protected void writeToZip(DLFileEntry dlFileEntry, ZipWriter zipWriter)
+		throws Exception {
+
+		String dlFileEntryFileName = StringBundler.concat(
+			String.valueOf(dlFileEntry.getPrimaryKeyObj()), ".",
+			dlFileEntry.getExtension());
+
+		zipWriter.addEntry(dlFileEntryFileName, dlFileEntry.getContentStream());
+
+		zipWriter.addEntry(
+			dlFileEntry.getPrimaryKeyObj() + "-meta.xml", export(dlFileEntry));
+	}
+
 }
