26cc8c01fef5ef364665d7a00e3d9cbc5f286f2a
shuyangzhou <shuyang.zhou@liferay.com>
2018-05-15 15:52:18 -0700

    LPS-80896 SF

--- portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
+++ portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.setup;
 
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.events.EventsProcessorUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.dao.jdbc.DataSourceFactoryUtil;
@@ -31,7 +32,7 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PropertiesParamUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
@@ -187,15 +188,12 @@ public class SetupWizardUtil {
 			String value = entry.getValue();
 
 			if (Validator.isNull(value)) {
-				unicodeProperties.setProperty(
-					entry.getKey(), StringPool.IS_NULL);
+				unicodeProperties.setProperty(entry.getKey(), _NULL_HOLDER);
 			}
 		}
 
-		String encodedPropertiesString = unicodeProperties.toString();
-
-		return encodedPropertiesString.replace(
-			StringPool.IS_NULL, StringPool.BLANK);
+		return StringUtil.replace(
+			unicodeProperties.toString(), _NULL_HOLDER, StringPool.BLANK);
 	}
 
 	private static boolean _isDatabaseConfigured(
@@ -392,6 +390,8 @@ public class SetupWizardUtil {
 
 		return false;
 	}
+
+	private static final String _NULL_HOLDER = "NULL_HOLDER";
 
 	private static final String _PROPERTIES_PREFIX = "properties--";
 
