2db3affec7776ea8e325fcd4685b0295324d8582
Pei-Jung Lan <pei-jung.lan@liferay.com>
2018-05-15 11:15:01 -0700

    LPS-80926 (roles-uad-test) Remove JavaDoc from *UADTestHelper

--- modules/apps/roles/roles-uad-test/src/testIntegration/java/com/liferay/roles/uad/test/RoleUADTestHelper.java
+++ modules/apps/roles/roles-uad-test/src/testIntegration/java/com/liferay/roles/uad/test/RoleUADTestHelper.java
@@ -32,13 +32,6 @@ import org.osgi.service.component.annotations.Reference;
 @Component(immediate = true, service = RoleUADTestHelper.class)
 public class RoleUADTestHelper {
 
-	/**
-	 * Implement addRole() to enable some UAD tests.
-	 *
-	 * <p>
-	 * Several UAD tests depend on creating one or more valid Roles with a specified user ID in order to execute correctly. Implement addRole() such that it creates a valid Role with the specified user ID value and returns it in order to enable the UAD tests that depend on it.
-	 * </p>
-	 */
 	public Role addRole(long userId) throws Exception {
 		String name = RandomTestUtil.randomString(
 			NumericStringRandomizerBumper.INSTANCE,
@@ -49,13 +42,6 @@ public class RoleUADTestHelper {
 			null);
 	}
 
-	/**
-	 * Implement cleanUpDependencies(List<Role> roles) if tests require additional tear down logic.
-	 *
-	 * <p>
-	 * Several UAD tests depend on creating one or more valid Roles with specified user ID and status by user ID in order to execute correctly. Implement cleanUpDependencies(List<Role> roles) such that any additional objects created during the construction of roles are safely removed.
-	 * </p>
-	 */
 	public void cleanUpDependencies(List<Role> roles) throws Exception {
 	}
 
