cb01f7ac01b427486536a8a15b15351788e47329
Alejandro Hernández <info@alejandrohdezma.com>
2018-05-16 12:07:31 +0200

    LPS-74270 SF

--- modules/apps/headless-apio/blog/blog-apio-impl/src/main/java/com/liferay/blog/apio/internal/resource/CommentNestedCollectionRouter.java
+++ modules/apps/headless-apio/blog/blog-apio-impl/src/main/java/com/liferay/blog/apio/internal/resource/CommentNestedCollectionRouter.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.blog.apio.internal.resource;
 
 import com.liferay.apio.architect.pagination.PageItems;
@@ -26,37 +27,42 @@ import com.liferay.portal.kernel.comment.Comment;
 import com.liferay.portal.kernel.comment.CommentManager;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * Provides the information necessary to expose the <a
  * href="http://schema.org/Comment">Comment</a> resources contained inside a <a
  * href="http://schema.org/BlogPosting">BlogPosting</a> through a web API. The
  * resources are mapped from the internal model {@link Comment} and
- * {@link com.liferay.blogs.model.BlogsEntry}.
+ * {@link BlogsEntry}.
  *
  * @author Eduardo Perez
  * @review
  */
 @Component(immediate = true)
-public class CommentNestedCollectionRouter implements NestedCollectionRouter<Comment, CommentIdentifier, Long, BlogPostingIdentifier>{
+public class CommentNestedCollectionRouter implements NestedCollectionRouter
+	<Comment, CommentIdentifier, Long, BlogPostingIdentifier> {
+
 	@Override
 	public NestedCollectionRoutes<Comment, Long> collectionRoutes(
 		NestedCollectionRoutes.Builder<Comment, Long> builder) {
+
 		return builder.addGetter(
 			this::_getPageItems
 		).build();
 	}
 
 	private PageItems<Comment> _getPageItems(
-		Pagination pagination, Long blogEntryId) throws PortalException {
+			Pagination pagination, Long blogEntryId)
+		throws PortalException {
 
-		BlogsEntry blogEntry = _blogsEntryLocalService.getEntry(blogEntryId);
+		BlogsEntry blogsEntry = _blogsEntryLocalService.getEntry(blogEntryId);
 
-		ClassNameClassPK classNameClassPK = ClassNameClassPK.create(blogEntry);
+		ClassNameClassPK classNameClassPK = ClassNameClassPK.create(blogsEntry);
 
 		List<Comment> comments = _commentManager.getRootComments(
 			classNameClassPK.getClassName(), classNameClassPK.getClassPK(),
@@ -70,8 +76,9 @@ public class CommentNestedCollectionRouter implements NestedCollectionRouter<Com
 	}
 
 	@Reference
-	CommentManager _commentManager;
+	private BlogsEntryLocalService _blogsEntryLocalService;
 
 	@Reference
-	BlogsEntryLocalService _blogsEntryLocalService;
+	private CommentManager _commentManager;
+
 }
