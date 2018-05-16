e8a2c4d5e499457eb2df59c335fdcdd10d9decab
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-05-16 19:47:24 +0200

    LPS-80706 SF

--- portal-kernel/src/com/liferay/portal/kernel/portlet/BasePortletLayoutFinder.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/BasePortletLayoutFinder.java
@@ -93,7 +93,7 @@ public abstract class BasePortletLayoutFinder implements PortletLayoutFinder {
 		if ((plidAndPortletId == null) &&
 			(scopeGroup.isSite() ||
 			 SitesUtil.isUserGroupLayoutSetViewable(
-				themeDisplay.getPermissionChecker(), scopeGroup))) {
+				 themeDisplay.getPermissionChecker(), scopeGroup))) {
 
 			plidAndPortletId = fetchPlidAndPortletId(
 				themeDisplay.getPermissionChecker(),
