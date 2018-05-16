1a6a721d54e29da8dc07191b6f0b7e284ce0b383
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-05-16 17:11:28 +0200

    COMMERCE-397 test for guest user segment entry

--- modules/dxp/apps/commerce/commerce-user-segment-test/src/testIntegration/java/com/liferay/commerce/user/segment/service/test/CommerceUserSegmentEntryLocalServiceTest.java
+++ modules/dxp/apps/commerce/commerce-user-segment-test/src/testIntegration/java/com/liferay/commerce/user/segment/service/test/CommerceUserSegmentEntryLocalServiceTest.java
@@ -17,17 +17,23 @@ package com.liferay.commerce.user.segment.service.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.commerce.user.segment.exception.CommerceUserSegmentEntrySystemException;
 import com.liferay.commerce.user.segment.model.CommerceUserSegmentEntry;
+import com.liferay.commerce.user.segment.model.CommerceUserSegmentEntryConstants;
 import com.liferay.commerce.user.segment.service.CommerceUserSegmentEntryLocalService;
 import com.liferay.commerce.user.segment.test.util.CommerceUserSegmentEntryTestUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.util.Locale;
+
 import org.frutilla.FrutillaRule;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -36,6 +42,7 @@ import org.junit.runner.RunWith;
 
 /**
  * @author Andrea Di Giorgi
+ * @author Luca Pellizzon
  */
 @RunWith(Arquillian.class)
 public class CommerceUserSegmentEntryLocalServiceTest {
@@ -70,6 +77,43 @@ public class CommerceUserSegmentEntryLocalServiceTest {
 			commerceUserSegmentEntry.getCommerceUserSegmentEntryId());
 	}
 
+	@Test
+	public void testGetGuestUserCommerceUserSegmentEntry() throws Exception {
+		frutillaRule.scenario(
+			"Retrieve user segments for default user (guest)"
+		).given(
+			"The guest user"
+		).when(
+			"I try to retrieve available user segments"
+		).then(
+			"The result should be the default user segment"
+		);
+
+		User guestUser = _userLocalService.getDefaultUser(
+			_group.getCompanyId());
+
+		long[] commerceUserSegmentEntryIDs =
+			_commerceUserSegmentEntryLocalService.
+				getCommerceUserSegmentEntryIds(
+					_group.getGroupId(), 0, guestUser.getUserId());
+
+		Assert.assertEquals(
+			commerceUserSegmentEntryIDs.toString(), 1,
+			commerceUserSegmentEntryIDs.length);
+
+		CommerceUserSegmentEntry commerceUserSegmentEntry =
+			_commerceUserSegmentEntryLocalService.getCommerceUserSegmentEntry(
+				commerceUserSegmentEntryIDs[0]);
+
+		Assert.assertEquals(
+			commerceUserSegmentEntry.toString(), true,
+			commerceUserSegmentEntry.isSystem());
+
+		Assert.assertEquals(
+			CommerceUserSegmentEntryConstants.KEY_GUEST,
+			commerceUserSegmentEntry.getName(Locale.US));
+	}
+
 	@Rule
 	public final FrutillaRule frutillaRule = new FrutillaRule();
 
@@ -79,5 +123,8 @@ public class CommerceUserSegmentEntryLocalServiceTest {
 
 	@DeleteAfterTestRun
 	private Group _group;
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 }
