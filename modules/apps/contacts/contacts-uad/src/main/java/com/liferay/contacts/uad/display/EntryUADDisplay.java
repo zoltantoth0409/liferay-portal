80c9c710faf44cbe94ef6a7088a0977ea41c89d8
drewbrokke <drew.brokke@liferay.com>
2018-05-16 15:34:37 -0700

    LPS-80985 contacts-uad - removes getEditURL override

--- modules/apps/contacts/contacts-uad/src/main/java/com/liferay/contacts/uad/display/EntryUADDisplay.java
+++ modules/apps/contacts/contacts-uad/src/main/java/com/liferay/contacts/uad/display/EntryUADDisplay.java
@@ -14,44 +14,13 @@
 
 package com.liferay.contacts.uad.display;
 
-import com.liferay.contacts.constants.ContactsPortletKeys;
-import com.liferay.contacts.model.Entry;
-import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
-import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.user.associated.data.display.UADDisplay;
 
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletURL;
-
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  */
 @Component(immediate = true, service = UADDisplay.class)
 public class EntryUADDisplay extends BaseEntryUADDisplay {
-
-	@Override
-	public String getEditURL(
-			Entry entry, LiferayPortletRequest liferayPortletRequest,
-			LiferayPortletResponse liferayPortletResponse)
-		throws Exception {
-
-		PortletURL portletURL = liferayPortletResponse.createLiferayPortletURL(
-			portal.getControlPanelPlid(liferayPortletRequest),
-			ContactsPortletKeys.CONCTACTS_CENTER, PortletRequest.RENDER_PHASE);
-
-		portletURL.setParameter("mvcPath", "/contacts_center/edit_entry");
-		portletURL.setParameter(
-			"redirect", portal.getCurrentURL(liferayPortletRequest));
-		portletURL.setParameter("entryId", String.valueOf(entry.getEntryId()));
-
-		return portletURL.toString();
-	}
-
-	@Reference
-	protected Portal portal;
-
 }
