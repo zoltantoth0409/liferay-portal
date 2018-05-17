a2b401b582352ea3a4714bbc0255f5cc8aaacdb7
Pablo Molina <contact@pablomolina.me>
2018-05-17 12:17:28 +0200

    LPS-80647 Update FragmentEditableField properties and emitted events

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/FragmentEditableField.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/FragmentEditableField.es.js
@@ -2,11 +2,21 @@ import Component from 'metal-component';
 import {Config} from 'metal-state';
 import Soy from 'metal-soy';
 
-import EditableImageFragmentProcessor from './fragment_processors/EditableImageFragmentProcessor.es';
-import EditableTextFragmentProcessor from './fragment_processors/EditableTextFragmentProcessor.es';
+import {showImageEditing} from './fragment_processors/EditableImageFragmentProcessor.es';
+import {createTextEditor} from './fragment_processors/EditableTextFragmentProcessor.es';
 import templates from './FragmentEditableField.soy';
 
 class FragmentEditableField extends Component {
+
+	/**
+	 * @inheritDoc
+	 * @review
+	 */
+
+	created() {
+		this._handleImageSelected = this._handleImageSelected.bind(this);
+		this._handleTextEditorChanged = this._handleTextEditorChanged.bind(this);
+	}
 
 	/**
 	 * Handle editable click event
@@ -28,11 +38,36 @@ class FragmentEditableField extends Component {
 
 	_handleEditButtonClick() {
 		if (this.type === 'image') {
-			EditableImageFragmentProcessor.showImageEditing();
+			showImageEditing(
+				this.refs.editable,
+				this.portletNamespace,
+				this.imageSelectorURL,
+				this.fragmentEntryLinkId,
+				this._handleImageSelected
+			);
 		}
 		else {
-			EditableTextFragmentProcessor.createTextEditor();
+			createTextEditor(
+				this.refs.editable,
+				this.defaultEditorConfiguration,
+				this.portletNamespace,
+				this.fragmentEntryLinkId,
+				this._handleTextEditorChanged
+			);
 		}
+	}
+
+	/**
+	 * Handle image editor select event
+	 * @param {string} imageUrl
+	 * @private
+	 */
+
+	_handleImageSelected(imageUrl) {
+		this.emit(
+			'imageSelected',
+			{imageUrl}
+		);
 	}
 
 	/**
@@ -43,7 +78,20 @@ class FragmentEditableField extends Component {
 	_handleMapButtonClick() {
 		this.emit(
 			'mapButtonClicked',
-			{editableId: this.id}
+			{editableId: this.editableId}
+		);
+	}
+
+	/**
+	 * Handle text editor change event
+	 * @param {string} newValue
+	 * @private
+	 */
+
+	_handleTextEditorChanged(newValue) {
+		this.emit(
+			'textEditorChanged',
+			{newValue}
 		);
 	}
 }
@@ -69,6 +117,39 @@ FragmentEditableField.STATE = {
 	content: Config.setter(content => Soy.toIncDom(content || '')),
 
 	/**
+	 * Default configuration for AlloyEditor instances.
+	 * @default {}
+	 * @instance
+	 * @memberOf FragmentEditableField
+	 * @review
+	 * @type {object}
+	 */
+
+	defaultEditorConfiguration: Config.object().value({}),
+
+	/**
+	 * FragmentEntryLink id
+	 * @default undefined
+	 * @instance
+	 * @memberOf FragmentEditableField
+	 * @review
+	 * @type {!string}
+	 */
+
+	fragmentEntryLinkId: Config.string().required(),
+
+	/**
+	 * Image selector url
+	 * @default undefined
+	 * @instance
+	 * @memberOf FragmentEditableField
+	 * @review
+	 * @type {!string}
+	 */
+
+	imageSelectorURL: Config.string().required(),
+
+	/**
 	 * Editable ID
 	 * @default undefined
 	 * @instance
@@ -77,7 +158,18 @@ FragmentEditableField.STATE = {
 	 * @type {!string}
 	 */
 
-	id: Config.string().required(),
+	editableId: Config.string().required(),
+
+	/**
+	 * Portlet namespace
+	 * @default undefined
+	 * @instance
+	 * @memberOf FragmentEditableField
+	 * @review
+	 * @type {!string}
+	 */
+
+	portletNamespace: Config.string().required(),
 
 	/**
 	 * Editable type
