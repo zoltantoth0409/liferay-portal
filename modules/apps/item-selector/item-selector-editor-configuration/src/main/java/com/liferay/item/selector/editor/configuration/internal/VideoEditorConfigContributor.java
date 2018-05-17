1fb200f8170d1d8efc60006b60f61fd9d9ce6947
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-05-17 15:54:20 +0200

    LPS-81164 Enable those audio and video config contributor only when xuggler is enabled

--- modules/apps/item-selector/item-selector-editor-configuration/src/main/java/com/liferay/item/selector/editor/configuration/internal/VideoEditorConfigContributor.java
+++ modules/apps/item-selector/item-selector-editor-configuration/src/main/java/com/liferay/item/selector/editor/configuration/internal/VideoEditorConfigContributor.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.xuggler.XugglerUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +45,10 @@ public class VideoEditorConfigContributor extends BaseEditorConfigContributor {
 		JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes,
 		ThemeDisplay themeDisplay,
 		RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+
+		if (!XugglerUtil.isEnabled()) {
+			return;
+		}
 
 		List<ItemSelectorCriterion> itemSelectorCriteria = new ArrayList<>();
 
