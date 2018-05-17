82aaba14c4fc4f6df6a133be6847ed92979c8f2e
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-05-17 11:01:26 -0700

    COMMERCE-X Hide orders portlet

--- modules/dxp/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/CommerceOrderPortlet.java
+++ modules/dxp/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/CommerceOrderPortlet.java
@@ -51,7 +51,7 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-commerce-order",
-		"com.liferay.portlet.display-category=hidden",
+		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.layout-cacheable=true",
 		"com.liferay.portlet.preferences-owned-by-group=true",
