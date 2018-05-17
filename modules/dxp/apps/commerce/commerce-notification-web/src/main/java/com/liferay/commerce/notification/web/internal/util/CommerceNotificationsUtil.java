a9e82f5f1d764ca177a4157d7e8dff721bdc8d78
Alessio Rendina <asilcomando@gmail.com>
2018-05-17 12:39:41 +0200

    COMMERCE-447 Added commerce notifications util class

---
+++ modules/dxp/apps/commerce/commerce-notification-web/src/main/java/com/liferay/commerce/notification/web/internal/util/CommerceNotificationsUtil.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.notification.web.internal.util;
+
+import com.liferay.commerce.notification.model.CommerceNotificationTemplate;
+import com.liferay.commerce.notification.util.comparator.CommerceNotificationTemplateCreateDateComparator;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceNotificationsUtil {
+
+	public static OrderByComparator<CommerceNotificationTemplate>
+		getCommerceNotificationTemplateOrderByComparator(
+			String orderByCol, String orderByType) {
+
+		boolean orderByAsc = false;
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<CommerceNotificationTemplate> orderByComparator =
+			null;
+
+		if (orderByCol.equals("create-date")) {
+			orderByComparator =
+				new CommerceNotificationTemplateCreateDateComparator(
+					orderByAsc);
+		}
+
+		return orderByComparator;
+	}
+
+}
