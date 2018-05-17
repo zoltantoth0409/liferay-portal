95c658b16c7baaf0aa55b417a544bef32444ed14
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-05-17 14:55:43 +0200

    OAUTH2-130 Create ApplicationDescriptorLocator to be consistent with ScopeDescriptorLocator

---
+++ modules/apps/oauth2-provider/oauth2-provider-scope-impl/src/main/java/com/liferay/oauth2/provider/scope/internal/liferay/ApplicationDescriptorLocatorImpl.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.scope.internal.liferay;
+
+import com.liferay.oauth2.provider.scope.internal.constants.OAuth2ProviderScopeConstants;
+import com.liferay.oauth2.provider.scope.liferay.ApplicationDescriptorLocator;
+import com.liferay.oauth2.provider.scope.spi.application.descriptor.ApplicationDescriptor;
+import com.liferay.oauth2.provider.scope.spi.scope.descriptor.ScopeDescriptor;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+
+/**
+ * @author Tomas Polesovsky
+ */
+@Component
+public class ApplicationDescriptorLocatorImpl
+	implements ApplicationDescriptorLocator {
+
+	@Override
+	public ApplicationDescriptor getApplicationDescriptor(
+		String applicationName) {
+
+		ApplicationDescriptor applicationDescriptor =
+			_applicationDescriptorServiceTrackerMap.getService(applicationName);
+
+		if (applicationDescriptor == null) {
+			return _defaultApplicationDescriptor;
+		}
+
+		return applicationDescriptor;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_applicationDescriptorServiceTrackerMap =
+			ServiceTrackerMapFactory.openSingleValueMap(
+				bundleContext, ApplicationDescriptor.class,
+				OAuth2ProviderScopeConstants.OSGI_JAXRS_NAME);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_applicationDescriptorServiceTrackerMap.close();
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		target = "(default=true)"
+	)
+	private ApplicationDescriptor _defaultApplicationDescriptor;
+
+	private ServiceTrackerMap<String, ApplicationDescriptor>
+		_applicationDescriptorServiceTrackerMap;
+
+}
