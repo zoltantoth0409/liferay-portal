668d64d63f30378585e6b5f292fcf25604c8d41d
akosthurzo <akos.thurzo@liferay.com>
2018-05-18 09:08:58 +0200

    LPS-81695 Delete changeset collection when a group is deleted (todo: move this code to disableStaging once it is modularized)

---
+++ modules/apps/staging/staging-impl/src/main/java/com/liferay/staging/internal/model/listener/GroupModelListener.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.staging.internal.model.listener;
+
+import com.liferay.changeset.model.ChangesetCollection;
+import com.liferay.changeset.service.ChangesetCollectionLocalService;
+import com.liferay.exportimport.kernel.staging.StagingConstants;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Akos Thurzo
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class GroupModelListener extends BaseModelListener<Group> {
+
+	@Override
+	public void onBeforeRemove(Group group) throws ModelListenerException {
+		ChangesetCollection changesetCollection =
+			_changesetCollectionLocalService.fetchChangesetCollection(
+				group.getGroupId(),
+				StagingConstants.RANGE_FROM_LAST_PUBLISH_DATE_CHANGESET_NAME);
+
+		if (changesetCollection == null) {
+			return;
+		}
+
+		try {
+			_changesetCollectionLocalService.deleteChangesetCollection(
+				changesetCollection.getChangesetCollectionId());
+		}
+		catch (PortalException pe) {
+			_log.error(pe);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		GroupModelListener.class);
+
+	@Reference
+	private ChangesetCollectionLocalService _changesetCollectionLocalService;
+
+}
