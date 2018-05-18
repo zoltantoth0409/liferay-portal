4104f86b1be8a5d7f740c50fde18afd23687e592
codyhoag <cody.hoag@liferay.com>
2018-05-18 09:28:43 -0400

    LPS-78940 LRDOCS-5300 Edit Javadoc

--- portal-kernel/src/com/liferay/portal/kernel/service/version/VersionService.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/version/VersionService.java
@@ -21,168 +21,175 @@ import com.liferay.portal.kernel.model.version.VersionedModel;
 import java.util.List;
 
 /**
+ * Provides a way to view versions, create new drafts, publish drafts, and roll
+ * back versions of versioned models.
+ *
  * @author Preston Crary
- * @see VersionServiceListener
+ * @see    VersionServiceListener
  */
 public interface VersionService
 	<E extends VersionedModel<V>, V extends VersionModel<E>> {
 
 	/**
-	 * Checks out the previous version for the versionedModel and saves it as a
-	 * draft. Notifies any {@link VersionServiceListener} that have been
-	 * registered.
+	 * Checks out the previous version of the versioned model and saves it as a
+	 * draft. Any {@link VersionServiceListener} that has been registered is
+	 * also notified. An {@link IllegalArgumentException} is thrown if the
+	 * versioned model already has a draft.
 	 *
-	 * @param publishedVersionedModel the published versioned model
-	 * @param version the version
-	 * @return the draft {@link VersionModel} being checkout out
-	 * @throws IllegalArgumentException if the versioned model already has a
-	 * draft
+	 * @param  publishedVersionedModel the published versioned model
+	 * @param  version the version
+	 * @return the draft {@link VersionModel} being checked out
 	 * @throws PortalException if the version does not exist
 	 */
 	public E checkout(E publishedVersionedModel, int version)
 		throws PortalException;
 
 	/**
-	 * Creates a new draft {@link VersionedModel}. Does not add the versioned
-	 * model to the database.
+	 * Creates a new draft {@link VersionedModel}. The versioned model is not
+	 * added to the database.
 	 *
 	 * @return the new draft versioned model
 	 */
 	public E create();
 
 	/**
-	 * Deletes the {@link VersionedModel}. Related draft if it exists and
-	 * versions are also deleted. Notifies any {@link VersionServiceListener}
-	 * that have been registered.
+	 * Deletes the {@link VersionedModel}. Related drafts and versions are also
+	 * deleted. Any {@link VersionServiceListener} that has been registered is
+	 * notified. An {@link IllegalArgumentException} is thrown if the versioned
+	 * model is a draft. Drafts can be deleted by calling {@link
+	 * #deleteDraft(VersionedModel)}.
 	 *
-	 * @param publishedVersionedModel the published versioned model
-	 * @throws IllegalArgumentException if the versioned model is a draft,
-	 *         delete drafts by calling {@link #deleteDraft(VersionedModel)}
+	 * @param  publishedVersionedModel the published versioned model
+	 * @return the versioned model that is deleted
 	 * @throws PortalException if a portal exception occurred
-	 * @return the versioned model that was deleted
 	 */
 	public E delete(E publishedVersionedModel) throws PortalException;
 
 	/**
-	 * Deletes the {@link VersionedModel} if it is a draft. Notifies any
-	 * {@link VersionServiceListener} that have been registered.
+	 * Deletes the {@link VersionedModel} if it is a draft. Any {@link
+	 * VersionServiceListener} that has been registered is notified. An {@link
+	 * IllegalArgumentException} is thrown if the versioned model is not a
+	 * draft. Published versioned models can be deleted by calling {@link
+	 * #delete(VersionedModel)}.
 	 *
-	 * @param draftVersionedModel the draft versioned model
-	 * @throws IllegalArgumentException if the versioned model is not a draft,
-	 *         delete published versioned models by calling {@link #delete(
-	 *         VersionedModel)}
+	 * @param  draftVersionedModel the draft versioned model
+	 * @return the versioned model that is deleted
 	 * @throws PortalException if a portal exception occurred
-	 * @return the versioned model that was deleted
 	 */
 	public E deleteDraft(E draftVersionedModel) throws PortalException;
 
 	/**
-	 * Deletes the {@link VersionModel} if it is not the latest version.
-	 * Notifies any {@link VersionServiceListener} that have been registered.
+	 * Deletes the {@link VersionModel} if it is not the latest version. Any
+	 * {@link VersionServiceListener} that has been registered is notified. An
+	 * {@link IllegalArgumentException} is thrown if the versioned model is the
+	 * latest version.
 	 *
-	 * @param versionModel the version model
-	 * @throws IllegalArgumentException if the version model is the latest
-	 *         version
+	 * @param  versionModel the version model
+	 * @return the version model that is deleted
 	 * @throws PortalException if a portal exception occurred
-	 * @return the version model that was deleted
 	 */
 	public V deleteVersion(V versionModel) throws PortalException;
 
 	/**
-	 * Fetches the draft if it exists, otherwise returns <code>null</code>.
+	 * Fetches the draft if it exists; otherwise, returns <code>null</code>.
 	 *
-	 * @param versionedModel the draft or published versioned model
-	 * @return <code>null</code> or the draft versioned model if it exists
+	 * @param  versionedModel the draft or published versioned model
+	 * @return the draft versioned model if it exists; otherwise
+	 *         <code>null</code>
 	 */
 	public E fetchDraft(E versionedModel);
 
 	/**
-	 * Fetches the draft if it exists, otherwise returns <code>null</code>.
+	 * Fetches the draft if it exists; otherwise, returns <code>null</code>.
 	 *
-	 * @param primaryKey of the draft or published versioned model
-	 * @return <code>null</code> or the draft versioned model if it exists
+	 * @param  primaryKey the draft or published versioned model's primary key
+	 * @return the draft versioned model if it exists; otherwise
+	 *         <code>null</code>
 	 */
 	public E fetchDraft(long primaryKey);
 
 	/**
-	 * Fetches the latest version model if it exists, otherwise returns
-	 * <code>null</code> if the versioned model has never been published.
+	 * Fetches the latest version model if it exists. If the versioned model has
+	 * never been published, <code>null</code> is returned.
 	 *
-	 * @param versionedModel the versioned model
-	 * @return <code>null</code> or the latest version model if it exists
+	 * @param  versionedModel the versioned model
+	 * @return the latest version model if it exists; otherwise
+	 *         <code>null</code>
 	 */
 	public V fetchLatestVersion(E versionedModel);
 
 	/**
-	 * Fetches the published versioned model if it exists, otherwise returns
-	 * <code>null</code> if the versioned model has never been published.
+	 * Fetches the published versioned model if it exists. If the versioned
+	 * model has never been published, <code>null</code> is returned.
 	 *
-	 * @param versionedModel the versioned model
-	 * @return <code>null</code> or the published versioned model if it exists
+	 * @param  versionedModel the versioned model
+	 * @return the published versioned model if it exists; otherwise
+	 *         <code>null</code>
 	 */
 	public E fetchPublished(E versionedModel);
 
 	/**
-	 * Fetches the published versioned model if it exists, otherwise returns
+	 * Fetches the published versioned model if it exists; otherwise, returns
 	 * <code>null</code>.
 	 *
-	 * @param versionedModel the versioned model
-	 * @return <code>null</code> or the published versioned model if it exists
+	 * @param  primaryKey the published versioned model's primary key
+	 * @return the published versioned model if it exists; otherwise
+	 *         <code>null</code>
 	 */
 	public E fetchPublished(long primaryKey);
 
 	/**
-	 * If the versioned model is a draft it is returned, otherwise fetches the
-	 * draft and lazily creates a new draft if it does not exist adding it to
-	 * the database. If a draft is lazily created any
-	 * {@link VersionServiceListener} that have been registered are notified.
+	 * Returns the versioned model draft. If the draft does not exist, it is
+	 * lazily created, adding it to the database. If a draft is lazily created,
+	 * any {@link VersionServiceListener} that has been registered is notified.
 	 *
-	 * @param versionedModel the versioned model
+	 * @param  versionedModel the versioned model
 	 * @return the versioned model
 	 * @throws PortalException if a portal exception occurred
 	 */
 	public E getDraft(E versionedModel) throws PortalException;
 
 	/**
-	 * Lazily creates a new draft adding it to the database if it does not exist
-	 * otherwise the existing versioned model already exists it is returned.
-	 * If a draft is lazily created any {@link VersionServiceListener} that
-	 * have been registered are notified.
+	 * Returns the versioned model draft with the primary key. If it does not
+	 * exist, a new draft is lazily created and added to the database. If a
+	 * draft is lazily created, any {@link VersionServiceListener} that has been
+	 * registered is notified.
 	 *
-	 * @param primaryKey of the draft or published versioned model
+	 * @param  primaryKey of the draft or published versioned model
 	 * @return the versioned model
-	 * @throws PortalException no draft or published versioned model exists with
-	 *         the primary key
+	 * @throws PortalException if no draft or published versioned model exists
+	 *         with the primary key
 	 */
 	public E getDraft(long primaryKey) throws PortalException;
 
 	/**
-	 * Gets the {@link VersionModel} for the {@link VersionedModel} with the
-	 * given version.
+	 * Returns the {@link VersionModel} for the {@link VersionedModel} matching
+	 * the version.
 	 *
-	 * @param versionedModel the versioned model
-	 * @param version the version
+	 * @param  versionedModel the versioned model
+	 * @param  version the version
 	 * @return the version model
 	 * @throws PortalException if the version does not exist
 	 */
 	public V getVersion(E versionedModel, int version) throws PortalException;
 
 	/**
-	 * Gets the list of {@link VersionModel} for the {@link VersionedModel}.
-	 * Sorted by highest to lowest version.
+	 * Returns the {@link VersionModel}s for the {@link VersionedModel}. They
+	 * are sorted in descending order.
 	 *
-	 * @param versionedModel the versioned model
+	 * @param  versionedModel the versioned model
 	 * @return the version models
 	 */
 	public List<V> getVersions(E versionedModel);
 
 	/**
 	 * Publishes the draft and creates a new version model for the new version.
-	 * Notifies any {@link VersionServiceListener} that have been registered.
+	 * Any {@link VersionServiceListener} that has been registered is notified.
+	 * An {@link IllegalArgumentException} is thrown if the versioned model is
+	 * not a draft.
 	 *
-	 * @param draftVersionedModel the draft version model
+	 * @param  draftVersionedModel the draft version model
 	 * @return the published versioned model
-	 * @throws IllegalArgumentException if the versionedModel is not a draft
 	 * @throws PortalException if a portal exception occurred
 	 */
 	public E publishDraft(E draftVersionedModel) throws PortalException;
@@ -190,7 +197,7 @@ public interface VersionService
 	/**
 	 * Registers the {@link VersionServiceListener}.
 	 *
-	 * @param versionServiceListener to be registered
+	 * @param versionServiceListener the version service listener to register
 	 */
 	public void registerListener(
 		VersionServiceListener<E, V> versionServiceListener);
@@ -198,18 +205,18 @@ public interface VersionService
 	/**
 	 * Unregisters the {@link VersionServiceListener}.
 	 *
-	 * @param versionServiceListener to be unregistered
+	 * @param versionServiceListener the version service listener to unregister
 	 */
 	public void unregisterListener(
 		VersionServiceListener<E, V> versionServiceListener);
 
 	/**
-	 * Updates the draft. Notifies any {@link VersionServiceListener} that have
-	 * been registered.
+	 * Updates the draft. Any {@link VersionServiceListener} that has been
+	 * registered is notified. An {@link IllegalArgumentException} is thrown if
+	 * the versioned model is not a draft.
 	 *
-	 * @param draftVersionedModel the draft version model
+	 * @param  draftVersionedModel the draft version model
 	 * @return the draft versioned model
-	 * @throws IllegalArgumentException if the versioned model is not a draft
 	 * @throws PortalException if a portal exception occurred
 	 */
 	public E updateDraft(E draftVersionedModel) throws PortalException;
