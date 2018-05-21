670bdd102a8665c2e555763e72bdd36d58cd88e4
Ibai Ruiz <ibai.ruiz@liferay.com>
2018-05-21 16:27:23 +0200

    LPS-79038 Source formatting

--- modules/apps/headless-apio/tag/tag-apio-impl/src/main/java/com/liferay/tag/apio/internal/router/BlogPostingTagNestedCollectionRouter.java
+++ modules/apps/headless-apio/tag/tag-apio-impl/src/main/java/com/liferay/tag/apio/internal/router/BlogPostingTagNestedCollectionRouter.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.tag.apio.internal.router;
 
 import com.liferay.apio.architect.pagination.PageItems;
@@ -15,6 +29,16 @@ import org.osgi.service.component.annotations.Reference;
 
 import java.util.List;
 
+/**
+ * Provides the information necessary to expose the <a
+ * href="http://schema.org/Tag">Tag</a> resources contained inside a <a
+ * href="http://schema.org/BlogPosting">BlogPosting</a> through a web API. The
+ * resources are mapped from the internal model {@link AssetTag} and {@code
+ * BlogsEntry}.
+ *
+ * @author Ibai Ruiz
+ * @review
+ */
 @Component(immediate = true)
 public class BlogPostingTagNestedCollectionRouter implements
 	NestedCollectionRouter<AssetTag, TagIdentifier, Long, BlogPostingIdentifier>
@@ -33,6 +57,7 @@ public class BlogPostingTagNestedCollectionRouter implements
 		throws PortalException {
 
 		List<AssetTag> tags = _assetTagService.getTags(BlogsEntry.class.getName(), blogEntryId);
+
 		int count = tags.size();
 
 		return new PageItems<>(tags, count);
