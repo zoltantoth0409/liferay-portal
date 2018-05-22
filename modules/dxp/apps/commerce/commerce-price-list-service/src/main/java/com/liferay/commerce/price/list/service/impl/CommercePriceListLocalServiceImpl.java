71b4e26c1ec8853459a987e6c2c6330b10ec2f0b
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-05-22 16:26:07 +0200

    COMMERCE-X fix priority SORT for PriceList

--- modules/dxp/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/service/impl/CommercePriceListLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/service/impl/CommercePriceListLocalServiceImpl.java
@@ -480,7 +480,7 @@ public class CommercePriceListLocalServiceImpl
 		queryConfig.setScoreEnabled(false);
 
 		Sort sort = SortFactoryUtil.create(
-			Field.PRIORITY + "_Number_sortable", false);
+			Field.PRIORITY + "_Number_sortable", true);
 
 		searchContext.setSorts(sort);
 
