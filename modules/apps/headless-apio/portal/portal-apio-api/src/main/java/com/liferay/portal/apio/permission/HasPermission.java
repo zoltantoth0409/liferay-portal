af6262d64d5eaadfab644bd89f1981eb879a80d9
Alejandro Hernández <info@alejandrohdezma.com>
2018-05-22 14:55:20 +0200

    LPS-81326 Refactor HasPermission interface so its valid for every model

--- modules/apps/headless-apio/portal/portal-apio-api/src/main/java/com/liferay/portal/apio/permission/HasPermission.java
+++ modules/apps/headless-apio/portal/portal-apio-api/src/main/java/com/liferay/portal/apio/permission/HasPermission.java
@@ -15,11 +15,6 @@
 package com.liferay.portal.apio.permission;
 
 import com.liferay.apio.architect.credentials.Credentials;
-import com.liferay.apio.architect.functional.Try;
-import com.liferay.portal.kernel.model.ClassedModel;
-import com.liferay.portal.kernel.security.permission.PermissionChecker;
-
-import java.util.function.BiFunction;
 
 /**
  * Instances of this class contains the information necessary to calculate
@@ -28,9 +23,8 @@ import java.util.function.BiFunction;
  * <p>
  * The different methods that this interface exposes are intended to be used
  * only in the {@code Routes.Builder} methods that need to check permissions.
- * And its signature applies exactly so they can be used as either method
- * references, or in a "semantic" form ({@code
- * _hasPermission.forDeleting(BlogsEntry.class)}).
+ * And its signature applies exactly so they can be used as method references
+ * {@code _hasPermission::forAdding}.
  * </p>
  *
  * @author Alejandro Hernández
@@ -40,116 +34,84 @@ import java.util.function.BiFunction;
 public interface HasPermission {
 
 	/**
-	 * Returns a {@code BiFunction} that can be used in an {@code
-	 * Routes.Builder#addCreator} method to check if the current {@code User}
-	 * has permission to perform that action.
+	 * Returns {@code true} if the current {@code User} has permission to
+	 * perform that action.
 	 *
 	 * <p>
 	 * This method is intended to be used for providing a function to check
-	 * permissions in {@code Routes.Builder#addCreator} methods.
-	 * </p>
-	 *
-	 * @param  clazz the class of entries being added
-	 * @return the {@code BiFunction} used to check the permissions
-	 * @review
-	 */
-	public BiFunction<Credentials, Long, Boolean> forAddingEntries(
-		Class<?> clazz);
-
-	/**
-	 * Returns {@code true} if the current {@code User} can add a root {@code
-	 * JournalArticle}.
-	 *
-	 * <p>
-	 * This method is intended to be used for providing a function to check
-	 * permissions in {@code Routes.Builder#addCreator} methods.
+	 * permissions in {@link
+	 * com.liferay.apio.architect.routes.CollectionRoutes.Builder#addCreator}
+	 * methods.
 	 * </p>
 	 *
 	 * @param  credentials the current request credentials
-	 * @param  groupId the group ID
-	 * @return {@code true} if the current user can add a root {@code
-	 *         JournalArticle}; {@code false} otherwise
-	 * @review
-	 */
-	public Boolean forAddingRootJournalArticle(
-		Credentials credentials, long groupId);
-
-	/**
-	 * Returns {@code true} if the current {@code User} can add an {@code User}.
-	 *
-	 * <p>
-	 * This method is intended to be used for providing a function to check
-	 * permissions in {@code Routes.Builder#addCreator} methods.
-	 * </p>
-	 *
-	 * @param  credentials the current request credentials
-	 * @return {@code true} if the current user can add an {@code User}; {@code
+	 * @return {@code true} if the current user can perform the action; {@code
 	 *         false} otherwise
 	 * @review
 	 */
-	public Boolean forAddingUsers(Credentials credentials);
+	public default Boolean forAdding(Credentials credentials) {
+		return false;
+	}
 
 	/**
-	 * Returns a {@code BiFunction} that can be used in an {@code
-	 * Routes.Builder#addRemover} method to check if the current {@code User}
-	 * has permission to perform that action.
+	 * Returns {@code true} if the current {@code User} has permission to
+	 * perform that action.
 	 *
 	 * <p>
 	 * This method is intended to be used for providing a function to check
-	 * permissions in {@code Routes.Builder#addRemover} methods.
-	 * </p>
-	 *
-	 * @param  clazz the class of elements being deleted
-	 * @return the {@code BiFunction} used to check the permissions
-	 * @review
-	 */
-	public BiFunction<Credentials, Long, Boolean> forDeleting(
-		Class<? extends ClassedModel> clazz);
-
-	/**
-	 * Returns {@code true} if the current {@code User} can delete a {@code
-	 * Layout} with the provided {@code plid}.
-	 *
-	 * <p>
-	 * This method is intended to be used for providing a function to check
-	 * permissions in {@code Routes.Builder#addRemover} methods.
+	 * permissions in {@link
+	 * com.liferay.apio.architect.routes.NestedCollectionRoutes.Builder#addCreator}
+	 * methods.
 	 * </p>
 	 *
 	 * @param  credentials the current request credentials
-	 * @param  plid the layout being deleted
-	 * @return {@code true} if the current user can delete the {@code Layout};
-	 *         {@code false} otherwise
+	 * @param  id the ID of the parent model
+	 * @return {@code true} if the current user can perform the action; {@code
+	 *         false} otherwise
 	 * @review
 	 */
-	public Boolean forDeletingLayouts(Credentials credentials, Long plid);
+	public default Boolean forAdding(Credentials credentials, Long id) {
+		return false;
+	}
 
 	/**
-	 * Returns a {@code BiFunction} that can be used in an {@code
-	 * Routes.Builder#addUpdater} method to check if the current {@code User}
-	 * has permission to perform that action.
+	 * Returns {@code true} if the current {@code User} has permission to
+	 * perform that action.
 	 *
 	 * <p>
 	 * This method is intended to be used for providing a function to check
-	 * permissions in {@code Routes.Builder#addUpdater} methods.
+	 * permissions in {@link
+	 * com.liferay.apio.architect.routes.ItemRoutes.Builder#addRemover} methods.
 	 * </p>
 	 *
-	 * @param  clazz the class of elements being updated
-	 * @return the {@code BiFunction} used to check the permissions
+	 * @param  credentials the current request credentials
+	 * @param  id the ID of the model
+	 * @return {@code true} if the current user can perform the action; {@code
+	 *         false} otherwise
 	 * @review
 	 */
-	public BiFunction<Credentials, Long, Boolean> forUpdating(
-		Class<? extends ClassedModel> clazz);
+	public default Boolean forDeleting(Credentials credentials, Long id) {
+		return false;
+	}
 
 	/**
-	 * Returns a {@code PermissionChecker} for the current request credentials,
-	 * if they are valid, returns a {@code Failure} with the error otherwise.
+	 * Returns {@code true} if the current {@code User} has permission to
+	 * perform that action.
+	 *
+	 * <p>
+	 * This method is intended to be used for providing a function to check
+	 * permissions in {@link
+	 * com.liferay.apio.architect.routes.ItemRoutes.Builder#addUpdater} methods.
+	 * </p>
 	 *
 	 * @param  credentials the current request credentials
-	 * @return the {@code PermissionChecker} for the current request, if valid;
-	 *         a {@code Failure} with the error otherwise
+	 * @param  id the ID of the model
+	 * @return {@code true} if the current user can perform the action; {@code
+	 *         false} otherwise
 	 * @review
 	 */
-	public Try<PermissionChecker> getPermissionCheckerTry(
-		Credentials credentials);
+	public default Boolean forUpdating(Credentials credentials, Long id) {
+		return false;
+	}
 
 }
