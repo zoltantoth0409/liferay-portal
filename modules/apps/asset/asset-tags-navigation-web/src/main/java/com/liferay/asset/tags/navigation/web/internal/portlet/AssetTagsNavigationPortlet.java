997fe0adf6859ccfcc5c21e921297f4a3be55303
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-05-22 15:06:19 +0200

    LPS-80971 AutoSF

--- modules/apps/asset/asset-tags-navigation-web/src/main/java/com/liferay/asset/tags/navigation/web/internal/portlet/AssetTagsNavigationPortlet.java
+++ modules/apps/asset/asset-tags-navigation-web/src/main/java/com/liferay/asset/tags/navigation/web/internal/portlet/AssetTagsNavigationPortlet.java
@@ -45,8 +45,8 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.name=" + AssetTagsNavigationPortletKeys.ASSET_TAGS_NAVIGATION,
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=guest,power-user,user",
-		"javax.portlet.supported-public-render-parameter=tag",
 		"javax.portlet.supported-public-render-parameter=resetCur",
+		"javax.portlet.supported-public-render-parameter=tag",
 		"javax.portlet.supports.mime-type=text/html"
 	},
 	service = Portlet.class
