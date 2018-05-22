50d9937ef63381a18ce1ae40fb7fdcc26139e165
drewbrokke <drew.brokke@liferay.com>
2018-05-22 11:46:24 -0700

    LPS-81056 user-associated-data-web - creates SearchContainerManagementToolbarDisplayContext.java

---
+++ modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/display/context/SearchContainerManagementToolbarDisplayContext.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.associated.data.web.internal.display.context;
+
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+
+/**
+ * @author Drew Brokke
+ */
+public class SearchContainerManagementToolbarDisplayContext
+	extends BaseManagementToolbarDisplayContext {
+
+	public SearchContainerManagementToolbarDisplayContext(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		SearchContainer searchContainer) {
+
+		super(liferayPortletRequest, liferayPortletResponse);
+
+		this.searchContainer = searchContainer;
+	}
+
+	@Override
+	public Boolean getDisabled() {
+		if (getItemsTotal() == 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public int getItemsTotal() {
+		return searchContainer.getTotal();
+	}
+
+	@Override
+	public String getSearchContainerId() {
+		return searchContainer.getId(request, getNamespace());
+	}
+
+	@Override
+	protected String getOrderByCol() {
+		return searchContainer.getOrderByCol();
+	}
+
+	@Override
+	protected String getOrderByColParam() {
+		return searchContainer.getOrderByColParam();
+	}
+
+	@Override
+	protected String getOrderByType() {
+		return searchContainer.getOrderByType();
+	}
+
+	@Override
+	protected String getOrderByTypeParam() {
+		return searchContainer.getOrderByTypeParam();
+	}
+
+	protected SearchContainer searchContainer;
+
+}
