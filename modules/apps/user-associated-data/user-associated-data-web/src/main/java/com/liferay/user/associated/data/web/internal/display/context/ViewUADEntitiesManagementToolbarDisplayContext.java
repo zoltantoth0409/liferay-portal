2e83b6beb356daaac8658e46901b7ee3ecbc5f85
drewbrokke <drew.brokke@liferay.com>
2018-05-22 11:46:40 -0700

    LPS-81056 user-associated-data-web - creates ViewUADEntitiesManagementToolbarDisplayContext.java

---
+++ modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/display/context/ViewUADEntitiesManagementToolbarDisplayContext.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.user.associated.data.web.internal.display.context;
+
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemList;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.user.associated.data.web.internal.display.UADEntity;
+
+import java.util.List;
+
+/**
+ * @author Drew Brokke
+ */
+public class ViewUADEntitiesManagementToolbarDisplayContext
+	extends SearchContainerManagementToolbarDisplayContext {
+
+	public ViewUADEntitiesManagementToolbarDisplayContext(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		SearchContainer<UADEntity> searchContainer) {
+
+		super(liferayPortletRequest, liferayPortletResponse, searchContainer);
+	}
+
+	@Override
+	public List<DropdownItem> getActionDropdownItems() {
+		return new DropdownItemList() {
+			{
+				add(
+					dropdownItem -> {
+						dropdownItem.setHref(
+							StringBundler.concat(
+								"javascript:", getNamespace(),
+								"doAnonymizeMultiple();"));
+						dropdownItem.setLabel(
+							LanguageUtil.get(request, "anonymize"));
+					});
+				add(
+					dropdownItem -> {
+						dropdownItem.setHref(
+							StringBundler.concat(
+								"javascript:", getNamespace(),
+								"doDeleteMultiple();"));
+						dropdownItem.setLabel(
+							LanguageUtil.get(request, "delete"));
+					});
+			}
+
+		};
+	}
+
+	@Override
+	public String getInfoPanelId() {
+		return "infoPanelId";
+	}
+
+}
