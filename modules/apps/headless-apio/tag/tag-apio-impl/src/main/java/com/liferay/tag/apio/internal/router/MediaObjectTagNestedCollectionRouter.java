c3ec54584432182c15a91fbf5ce9ba6d47d0d219
Ibai Ruiz <ibai.ruiz@liferay.com>
2018-05-22 17:09:29 +0200

    LPS-79038 Add paginated service method calls on remote interfaces

--- modules/apps/headless-apio/tag/tag-apio-impl/src/main/java/com/liferay/tag/apio/internal/router/MediaObjectTagNestedCollectionRouter.java
+++ modules/apps/headless-apio/tag/tag-apio-impl/src/main/java/com/liferay/tag/apio/internal/router/MediaObjectTagNestedCollectionRouter.java
@@ -21,7 +21,6 @@ import com.liferay.apio.architect.routes.NestedCollectionRoutes;
 import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.asset.kernel.service.AssetTagService;
 import com.liferay.document.library.kernel.model.DLFileEntry;
-import com.liferay.document.library.kernel.service.DLFileEntryService;
 import com.liferay.media.object.apio.architect.identifier.FileEntryIdentifier;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.tag.apio.identifier.TagIdentifier;
@@ -43,11 +42,11 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(immediate = true)
 public class MediaObjectTagNestedCollectionRouter implements
-	NestedCollectionRouter<AssetTag, TagIdentifier, Long, FileEntryIdentifier> {
+	NestedCollectionRouter<AssetTag, Long, TagIdentifier, Long, FileEntryIdentifier> {
 
 	@Override
-	public NestedCollectionRoutes<AssetTag, Long> collectionRoutes(
-		NestedCollectionRoutes.Builder<AssetTag, Long> builder) {
+	public NestedCollectionRoutes<AssetTag, Long, Long> collectionRoutes(
+		NestedCollectionRoutes.Builder<AssetTag, Long, Long> builder) {
 
 		return builder.addGetter(
 			this::_getPageItems
@@ -59,17 +58,16 @@ public class MediaObjectTagNestedCollectionRouter implements
 		throws PortalException {
 
 		List<AssetTag> tags = _assetTagService.getTags(
-			DLFileEntry.class.getName(), fileEntryId);
+			DLFileEntry.class.getName(), fileEntryId,
+			pagination.getStartPosition(), pagination.getEndPosition());
 
-		int count = tags.size();
+		int count = _assetTagService.getTagsCount(
+			DLFileEntry.class.getName(), fileEntryId);
 
 		return new PageItems<>(tags, count);
 	}
 
 	@Reference
 	private AssetTagService _assetTagService;
-
-	@Reference
-	private DLFileEntryService _dlFileEntryService;
 
 }
