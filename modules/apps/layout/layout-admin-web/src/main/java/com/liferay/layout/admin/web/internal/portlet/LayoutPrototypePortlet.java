0e4ca9add03574f256cac8fb029876d8e28f4918
pavel-savinov <pavel.savinov@liferay.com>
2018-05-23 11:46:02 +0200

    LPS-80877 Add empty layout prototype portlet since we have portlet data handler related to it

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/LayoutPrototypePortlet.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.portlet;
+
+import com.liferay.layout.prototype.constants.LayoutPrototypePortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.hidden",
+		"javax.portlet.name=" + LayoutPrototypePortletKeys.LAYOUT_PROTOTYPE
+	},
+	service = Portlet.class
+)
+public class LayoutPrototypePortlet extends MVCPortlet {
+}
