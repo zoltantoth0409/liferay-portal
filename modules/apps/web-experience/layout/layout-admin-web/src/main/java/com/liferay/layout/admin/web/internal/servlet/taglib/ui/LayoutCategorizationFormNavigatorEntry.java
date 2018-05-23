95f1e19fa3420e75c91d76728eaeb38d1f9c6d73
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-05-23 10:41:16 +0200

    LPS-81161 Uses fetch method

--- modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/ui/LayoutCategorizationFormNavigatorEntry.java
+++ modules/apps/web-experience/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/ui/LayoutCategorizationFormNavigatorEntry.java
@@ -14,13 +14,12 @@
 
 package com.liferay.layout.admin.web.internal.servlet.taglib.ui;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.GroupService;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorConstants;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
 
@@ -51,17 +50,10 @@ public class LayoutCategorizationFormNavigatorEntry
 
 	@Override
 	public boolean isVisible(User user, Layout layout) {
-		try {
-			Group group = _groupService.getGroup(layout.getGroupId());
+		Group group = _groupLocalService.fetchGroup(layout.getGroupId());
 
-			if ((group != null) && group.isLayoutPrototype()) {
-				return false;
-			}
-		}
-		catch (PortalException pe) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(pe);
-			}
+		if ((group != null) && group.isLayoutPrototype()) {
+			return false;
 		}
 
 		return super.isVisible(user, layout);
@@ -85,6 +77,6 @@ public class LayoutCategorizationFormNavigatorEntry
 		LayoutCategorizationFormNavigatorEntry.class);
 
 	@Reference
-	private GroupService _groupService;
+	private GroupLocalService _groupLocalService;
 
 }
