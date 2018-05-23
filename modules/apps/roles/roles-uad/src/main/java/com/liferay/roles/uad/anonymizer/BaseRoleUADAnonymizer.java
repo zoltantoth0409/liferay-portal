a355804620443e30891c0665a0cbbf9729347754
drewbrokke <drew.brokke@liferay.com>
2018-05-23 10:22:59 -0700

    LPS-81404 roles-uad - removes 'getNonanonymizableFieldNames' from UADAnonymizer

--- modules/apps/roles/roles-uad/src/main/java/com/liferay/roles/uad/anonymizer/BaseRoleUADAnonymizer.java
+++ modules/apps/roles/roles-uad/src/main/java/com/liferay/roles/uad/anonymizer/BaseRoleUADAnonymizer.java
@@ -26,9 +26,6 @@ import com.liferay.user.associated.data.anonymizer.DynamicQueryUADAnonymizer;
 
 import org.osgi.service.component.annotations.Reference;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Provides the base implementation for the role UAD anonymizer.
  *
@@ -56,11 +53,6 @@ public abstract class BaseRoleUADAnonymizer extends DynamicQueryUADAnonymizer<Ro
 	@Override
 	public void delete(Role role) throws PortalException {
 		roleLocalService.deleteRole(role);
-	}
-
-	@Override
-	public List<String> getNonanonymizableFieldNames() {
-		return Arrays.asList();
 	}
 
 	@Override
