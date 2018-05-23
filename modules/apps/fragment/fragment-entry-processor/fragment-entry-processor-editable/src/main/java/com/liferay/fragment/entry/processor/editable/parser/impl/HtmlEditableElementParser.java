25d1b0d38747abfb46ebfc976c3f7584b6001768
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-05-23 11:22:54 +0200

    LPS-79694 Copy text parser to html parser, and change type accordingly

---
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/parser/impl/HtmlEditableElementParser.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.entry.processor.editable.parser.impl;
+
+import com.liferay.fragment.entry.processor.editable.parser.EditableElementParser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, property = "type=html",
+	service = EditableElementParser.class
+)
+public class HtmlEditableElementParser implements EditableElementParser {
+
+	@Override
+	public String getValue(Element element) {
+		return element.html();
+	}
+
+	@Override
+	public void replace(Element element, String value) {
+		Document document = Jsoup.parseBodyFragment(value);
+
+		Document.OutputSettings outputSettings = new Document.OutputSettings();
+
+		outputSettings.prettyPrint(false);
+
+		document.outputSettings(outputSettings);
+
+		element.html(document.html());
+	}
+
+}
