e3f585777401b5707541b802e69f01bdd364a12b
Brian Chan <brian.chan@liferay.com>
2018-05-23 14:06:36 -0700

    Revert "LPS-81369 Hide Social Activity, Social Group Statistics, Social User Statistics portlets"
    This reverts commit b27d0d79ed81dfa34bad0c0f7ff6d664773b6fbd.

--- modules/apps/social/social-activity-web/src/main/java/com/liferay/social/activity/web/internal/application/list/SocialActivityPanelApp.java
+++ modules/apps/social/social-activity-web/src/main/java/com/liferay/social/activity/web/internal/application/list/SocialActivityPanelApp.java
@@ -30,7 +30,7 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"panel.app.order:Integer=200",
-		"panel.category.key=" + PanelCategoryKeys.HIDDEN
+		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONFIGURATION
 	},
 	service = PanelApp.class
 )
