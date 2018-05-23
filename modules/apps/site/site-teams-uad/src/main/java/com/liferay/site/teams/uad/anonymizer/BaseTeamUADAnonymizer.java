771050d460f6332223bcee8e40831231b718dc31
drewbrokke <drew.brokke@liferay.com>
2018-05-23 10:23:01 -0700

    LPS-81404 site-teams-uad - removes 'getNonanonymizableFieldNames' from UADAnonymizer

--- modules/apps/site/site-teams-uad/src/main/java/com/liferay/site/teams/uad/anonymizer/BaseTeamUADAnonymizer.java
+++ modules/apps/site/site-teams-uad/src/main/java/com/liferay/site/teams/uad/anonymizer/BaseTeamUADAnonymizer.java
@@ -26,9 +26,6 @@ import com.liferay.user.associated.data.anonymizer.DynamicQueryUADAnonymizer;
 
 import org.osgi.service.component.annotations.Reference;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Provides the base implementation for the team UAD anonymizer.
  *
@@ -56,11 +53,6 @@ public abstract class BaseTeamUADAnonymizer extends DynamicQueryUADAnonymizer<Te
 	@Override
 	public void delete(Team team) throws PortalException {
 		teamLocalService.deleteTeam(team);
-	}
-
-	@Override
-	public List<String> getNonanonymizableFieldNames() {
-		return Arrays.asList();
 	}
 
 	@Override
