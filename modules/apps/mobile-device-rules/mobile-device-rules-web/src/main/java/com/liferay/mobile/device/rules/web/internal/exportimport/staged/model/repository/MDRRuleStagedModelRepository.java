865ba05a4820ef82b96eec2704f4775bca48a4be
akosthurzo <akos.thurzo@liferay.com>
2018-05-23 14:31:19 +0200

    LPS-81201 MDR

---
+++ modules/apps/mobile-device-rules/mobile-device-rules-web/src/main/java/com/liferay/mobile/device/rules/web/internal/exportimport/staged/model/repository/MDRRuleStagedModelRepository.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.device.rules.web.internal.exportimport.staged.model.repository;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.PortletDataException;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.mobile.device.rules.model.MDRRule;
+import com.liferay.mobile.device.rules.service.MDRRuleLocalService;
+import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Akos Thurzo
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.mobile.device.rules.model.MDRRule",
+	service = StagedModelRepository.class
+)
+public class MDRRuleStagedModelRepository
+	implements StagedModelRepository<MDRRule> {
+
+	@Override
+	public MDRRule addStagedModel(
+			PortletDataContext portletDataContext, MDRRule rule)
+		throws PortalException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void deleteStagedModel(MDRRule rule) throws PortalException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void deleteStagedModels(PortletDataContext portletDataContext)
+		throws PortalException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public MDRRule fetchMissingReference(String uuid, long groupId) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public MDRRule fetchStagedModelByUuidAndGroupId(String uuid, long groupId) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<MDRRule> fetchStagedModelsByUuidAndCompanyId(
+		String uuid, long companyId) {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
+		PortletDataContext portletDataContext) {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public MDRRule getStagedModel(long ruleId) throws PortalException {
+		return _mdrRuleLocalService.getMDRRule(ruleId);
+	}
+
+	@Override
+	public void restoreStagedModel(
+			PortletDataContext portletDataContext, MDRRule rule)
+		throws PortletDataException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public MDRRule saveStagedModel(MDRRule rule) throws PortalException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public MDRRule updateStagedModel(
+			PortletDataContext portletDataContext, MDRRule rule)
+		throws PortalException {
+
+		throw new UnsupportedOperationException();
+	}
+
+	@Reference
+	private MDRRuleLocalService _mdrRuleLocalService;
+
+}
