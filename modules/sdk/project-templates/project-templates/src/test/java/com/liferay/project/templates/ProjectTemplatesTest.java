94b7988ce0b3b60f33a89bc1a5c8ad1ae5e2a0fe
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2018-05-25 14:38:11 -0700

    LPS-80472 Use less generic name

--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
@@ -131,7 +131,7 @@ public class ProjectTemplatesTest {
 
 		XPath xPath = xPathFactory.newXPath();
 
-		_xPathExpression = xPath.compile(
+		_pomXmlNpmInstallXPathExpression = xPath.compile(
 			"//id[contains(text(),'npm-install')]/parent::*");
 	}
 
@@ -2325,7 +2325,7 @@ public class ProjectTemplatesTest {
 
 		Document document = documentBuilder.parse(pomXmlFile);
 
-		NodeList nodeList = (NodeList)_xPathExpression.evaluate(
+		NodeList nodeList = (NodeList)_pomXmlNpmInstallXPathExpression.evaluate(
 			document, XPathConstants.NODESET);
 
 		Node executionNode = nodeList.item(0);
@@ -3453,7 +3453,7 @@ public class ProjectTemplatesTest {
 		"test.debug.bundle.diffs");
 
 	private static URI _gradleDistribution;
+	private static XPathExpression _pomXmlNpmInstallXPathExpression;
 	private static Properties _projectTemplateVersions;
-	private static XPathExpression _xPathExpression;
 
 }
