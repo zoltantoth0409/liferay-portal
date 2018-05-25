57b173493304df3bd3bcebb8c77e490a70bb66ee
Jesse Rao <jesse.rao@liferay.com>
2018-05-25 12:34:10 -0400

    LPS-74319 Apply proposed solution: https://github.com/matethurzo/liferay-portal/pull/1758:

--- modules/apps/mobile-device-rules/mobile-device-rules-web/src/main/java/com/liferay/mobile/device/rules/web/internal/portlet/MDRPortlet.java
+++ modules/apps/mobile-device-rules/mobile-device-rules-web/src/main/java/com/liferay/mobile/device/rules/web/internal/portlet/MDRPortlet.java
@@ -17,17 +17,8 @@ package com.liferay.mobile.device.rules.web.internal.portlet;
 import com.liferay.mobile.device.rules.constants.MDRPortletKeys;
 import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.util.GetterUtil;
-
-import java.io.IOException;
 
 import javax.portlet.Portlet;
-import javax.portlet.PortletContext;
-import javax.portlet.PortletException;
-import javax.portlet.PortletRequestDispatcher;
-import javax.portlet.PortletSession;
-import javax.portlet.ResourceRequest;
-import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -60,30 +51,6 @@ import org.osgi.service.component.annotations.Reference;
 	service = Portlet.class
 )
 public class MDRPortlet extends MVCPortlet {
-
-	@Override
-	public void serveResource(
-			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
-		throws IOException, PortletException {
-
-		String resourceID = GetterUtil.getString(
-			resourceRequest.getResourceID());
-
-		if (resourceID.equals("getMobileDeviceRules")) {
-			PortletSession portletSession = resourceRequest.getPortletSession();
-
-			PortletContext portletContext = portletSession.getPortletContext();
-
-			PortletRequestDispatcher portletRequestDispatcher =
-				portletContext.getRequestDispatcher(
-					"/layout/mobile_device_rules_rule_group_instances.jsp");
-
-			portletRequestDispatcher.include(resourceRequest, resourceResponse);
-		}
-		else {
-			super.serveResource(resourceRequest, resourceResponse);
-		}
-	}
 
 	@Reference(
 		target = "(&(release.bundle.symbolic.name=com.liferay.mobile.device.rules.web)(release.schema.version=1.0.2))",
---
+++ modules/apps/mobile-device-rules/mobile-device-rules-web/src/main/java/com/liferay/mobile/device/rules/web/internal/portlet/action/RuleGroupInstancesMVCResourceCommand.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.device.rules.web.internal.portlet.action;
+
+import com.liferay.mobile.device.rules.constants.MDRPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortletKeys;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + MDRPortletKeys.MOBILE_DEVICE_RULES,
+		"javax.portlet.name=" + PortletKeys.GROUP_PAGES,
+		"mvc.command.name=/mobile_device_rules/rule_group_instances"
+	},
+	service = MVCResourceCommand.class
+)
+public class RuleGroupInstancesMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+		ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher(_RULE_GROUP_INSTANCES_JSP);
+
+		requestDispatcher.include(
+			_portal.getHttpServletRequest(resourceRequest),
+			_portal.getHttpServletResponse(resourceResponse));
+	}
+
+	private static final String _RULE_GROUP_INSTANCES_JSP =
+		"/layout/mobile_device_rules_rule_group_instances.jsp";
+
+	@Reference
+	private Portal _portal;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.mobile.device.rules.web)",
+		unbind = "-"
+	)
+	private ServletContext _servletContext;
+
+}
