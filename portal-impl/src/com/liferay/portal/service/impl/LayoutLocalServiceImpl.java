dbc592a063b6d282af7c2d3825f253251d062d3e
codyhoag <cody.hoag@liferay.com>
2018-05-25 14:03:59 -0400

    LPS-80159 LRDOCS-5330 Readd Javadoc method description

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -427,6 +427,13 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 			friendlyURLMap, serviceContext);
 	}
 
+	/**
+	 * Deletes the layout, its child layouts, and its associated resources.
+	 *
+	 * @param  layout the layout
+	 * @return the deleted layout
+	 * @throws PortalException if a portal exception occurred
+	 */
 	@Override
 	public Layout deleteLayout(Layout layout) throws PortalException {
 		layoutLocalService.deleteLayout(layout, true, new ServiceContext());
