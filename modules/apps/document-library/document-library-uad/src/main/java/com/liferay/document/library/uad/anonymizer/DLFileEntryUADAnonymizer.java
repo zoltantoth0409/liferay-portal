ecb2d45fd84662378e55362df32940cc97043d54
drewbrokke <drew.brokke@liferay.com>
2018-05-25 17:03:00 -0700

    LPS-81220 document-library-uad - DLFileEntryUADAnonymizer - anonymizes all associated DLFileVersions

--- modules/apps/document-library/document-library-uad/src/main/java/com/liferay/document/library/uad/anonymizer/DLFileEntryUADAnonymizer.java
+++ modules/apps/document-library/document-library-uad/src/main/java/com/liferay/document/library/uad/anonymizer/DLFileEntryUADAnonymizer.java
@@ -14,13 +14,63 @@
 
 package com.liferay.document.library.uad.anonymizer;
 
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.service.DLFileVersionLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.user.associated.data.anonymizer.UADAnonymizer;
 
+import java.util.List;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  */
 @Component(immediate = true, service = UADAnonymizer.class)
 public class DLFileEntryUADAnonymizer extends BaseDLFileEntryUADAnonymizer {
+
+	@Override
+	public void autoAnonymize(
+			DLFileEntry dlFileEntry, long userId, User anonymousUser)
+		throws PortalException {
+
+		List<DLFileVersion> dlFileVersions = dlFileEntry.getFileVersions(
+			WorkflowConstants.STATUS_ANY);
+
+		for (DLFileVersion dlFileVersion : dlFileVersions) {
+			_anonymizeDLFileVersion(
+				dlFileVersion, userId, anonymousUser.getUserId(),
+				anonymousUser.getFullName());
+		}
+
+		super.autoAnonymize(dlFileEntry, userId, anonymousUser);
+	}
+
+	private void _anonymizeDLFileVersion(
+		DLFileVersion dlFileVersion, long userId, long anonymousUserId,
+		String anonymousUserFullName) {
+
+		if (userId == dlFileVersion.getUserId()) {
+			dlFileVersion.setUserId(anonymousUserId);
+			dlFileVersion.setUserName(anonymousUserFullName);
+		}
+
+		if (userId == dlFileVersion.getStatusByUserId()) {
+			dlFileVersion.setStatusByUserId(anonymousUserId);
+			dlFileVersion.setStatusByUserName(anonymousUserFullName);
+		}
+
+		_dlFileVersionLocalService.updateDLFileVersion(dlFileVersion);
+	}
+
+	private static final String[] _DL_FILE_VERSION_USER_ID_FIELD_NAMES =
+		{"statusByUserId", "userId"};
+
+	@Reference
+	private DLFileVersionLocalService _dlFileVersionLocalService;
+
 }
