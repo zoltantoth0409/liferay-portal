f19736f0e35f98787c471cca0b84491ca3503c5a
Pablo Molina <contact@pablomolina.me>
2018-05-29 15:45:49 +0200

    LPS-80145 Use event proxy inside FragmentsEditorSidebar

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/sidebar/FragmentsEditorSidebar.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/sidebar/FragmentsEditorSidebar.es.js
@@ -1,5 +1,6 @@
 import 'frontend-taglib/contextual_sidebar/ContextualSidebar.es';
 import Component from 'metal-component';
+import {EventEmitterProxy} from 'metal-events';
 import Soy from 'metal-soy';
 
 import './SidebarAddedFragments.es';
@@ -13,6 +14,62 @@ import templates from './FragmentsEditorSidebar.soy';
  */
 
 class FragmentsEditorSidebar extends Component {
+
+	/**
+	 * @inheritDoc
+	 * @review
+	 */
+
+	dispose() {
+		this._disposeTabEventProxy();
+	}
+
+	/**
+	 * @inheritDoc
+	 * @review
+	 */
+
+	rendered() {
+		if (
+			this.refs.contextualSidebar &&
+			this.refs.contextualSidebar.refs.sidebarTab
+		) {
+			this._updateTabEventProxy(
+				this.refs.contextualSidebar.refs.sidebarTab
+			);
+		}
+	}
+
+	/**
+	 * Update or create the _tabEventProxy to propagate all events emitted
+	 * from the given tab.
+	 * @param {EventEmitter} sidebarTab
+	 * @private
+	 * @review
+	 */
+
+	_updateTabEventProxy(sidebarTab) {
+		if (!this._tabEventProxy) {
+			this._tabEventProxy = new EventEmitterProxy(sidebarTab, this);
+		}
+		else {
+			this._tabEventProxy.setOriginEmitter(sidebarTab);
+		}
+	}
+
+	/**
+	 * Dispose the existing _tabEventProxy, if any.
+	 * @private
+	 * @review
+	 */
+
+	_disposeTabEventProxy() {
+		if (this._tabEventProxy) {
+			this._tabEventProxy.dispose();
+
+			this._tabEventProxy = null;
+		}
+	}
 }
 
 /**
