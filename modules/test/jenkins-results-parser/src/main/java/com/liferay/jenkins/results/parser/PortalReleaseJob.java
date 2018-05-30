d2d53a6d20ff0714105c6cdf916d194b12b47bee
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-05-30 13:11:38 -0700

    LRQA-41190 Create new "PortalReleaseJob" class to handle portal release test class groups

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PortalReleaseJob.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class PortalReleaseJob
+	extends BaseJob implements PortalTestClassJob, SmokeDependentJob {
+
+	public PortalReleaseJob(String jobName, String portalBranchName) {
+		super(jobName);
+
+		_portalBranchName = portalBranchName;
+
+		try {
+			_jenkinsGitWorkingDirectory =
+				JenkinsResultsParserUtil.getJenkinsGitWorkingDirectory();
+
+			_portalGitWorkingDirectory =
+				JenkinsResultsParserUtil.getPortalGitWorkingDirectory(
+					portalBranchName);
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(
+				"Could not create a git working directory", ioe);
+		}
+
+		jobProperties = JenkinsResultsParserUtil.getProperties(
+			new File(
+				_jenkinsGitWorkingDirectory.getWorkingDirectory(),
+				"commands/dependencies/test-portal-release.properties"));
+
+		jobProperties.putAll(
+			JenkinsResultsParserUtil.getProperties(
+				new File(
+					_portalGitWorkingDirectory.getWorkingDirectory(),
+					"test.properties")));
+	}
+
+	@Override
+	public Set<String> getBatchNames() {
+		String testBatchNames = JenkinsResultsParserUtil.getProperty(
+			jobProperties, "test.batch.names[" + _portalBranchName + "]");
+
+		return getSetFromString(testBatchNames);
+	}
+
+	@Override
+	public Set<String> getDistTypes() {
+		return Collections.emptySet();
+	}
+
+	@Override
+	public PortalGitWorkingDirectory getPortalGitWorkingDirectory() {
+		return _portalGitWorkingDirectory;
+	}
+
+	@Override
+	public Set<String> getSmokeBatchNames() {
+		String testBatchNames = JenkinsResultsParserUtil.getProperty(
+			jobProperties, "test.batch.names.smoke[" + _portalBranchName + "]");
+
+		return getSetFromString(testBatchNames);
+	}
+
+	private final GitWorkingDirectory _jenkinsGitWorkingDirectory;
+	private final String _portalBranchName;
+	private final PortalGitWorkingDirectory _portalGitWorkingDirectory;
+
+}
