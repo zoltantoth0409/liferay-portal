7b307c19f5c293ccf16c00c8628ec86eb17df35c
Roberto Díaz <roberto.diaz@liferay.com>
2018-05-30 11:39:18 +0200

    LPS-81743 Create missing components to migrate attachments from one store to another

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/convert/document/library/JournalDLStoreConvertProcess.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.convert.document.library;
+
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.portal.convert.documentlibrary.DLStoreConvertProcess;
+import com.liferay.portal.convert.documentlibrary.DLStoreConverter;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry;
+import com.liferay.portal.util.MaintenanceUtil;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(service = DLStoreConvertProcess.class)
+public class JournalDLStoreConvertProcess implements DLStoreConvertProcess {
+
+	@Override
+	public void migrate(final DLStoreConverter dlStoreConverter)
+		throws PortalException {
+
+		int count = _journalArticleLocalService.getJournalArticlesCount();
+
+		MaintenanceUtil.appendStatus(
+			"Migrating journal attachments in " + count + " messages");
+
+		ActionableDynamicQuery actionableDynamicQuery =
+			_journalArticleLocalService.getActionableDynamicQuery();
+
+		actionableDynamicQuery.setPerformActionMethod(
+			new ActionableDynamicQuery.PerformActionMethod<JournalArticle>() {
+
+				@Override
+				public void performAction(JournalArticle article)
+					throws PortalException {
+
+					for (FileEntry fileEntry : article.getImagesFileEntries()) {
+						DLFileEntry dlFileEntry =
+							(DLFileEntry)fileEntry.getModel();
+
+						dlStoreConverter.migrateDLFileEntry(
+							article.getCompanyId(),
+							DLFolderConstants.getDataRepositoryId(
+								dlFileEntry.getRepositoryId(),
+								dlFileEntry.getFolderId()),
+							new LiferayFileEntry(dlFileEntry));
+					}
+				}
+
+			});
+
+		actionableDynamicQuery.performActions();
+	}
+
+	@Reference(unbind = "-")
+	public void setJournalArticleLocalService(
+		JournalArticleLocalService journalArticleLocalService) {
+
+		_journalArticleLocalService = journalArticleLocalService;
+	}
+
+	private JournalArticleLocalService _journalArticleLocalService;
+
+}
