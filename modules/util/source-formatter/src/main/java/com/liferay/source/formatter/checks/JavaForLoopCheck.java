bc090f407c6118a83b794247997ac76f133debc6
Hugo Huijser <hugo.huijser@liferay.com>
2018-05-30 14:17:15 +0800

    LPS-81555 Improve logic in JavaForLoopCheck

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaForLoopCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaForLoopCheck.java
@@ -20,7 +20,6 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.tools.ToolsUtil;
 import com.liferay.source.formatter.parser.JavaTerm;
 
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,9 +34,12 @@ public class JavaForLoopCheck extends BaseJavaTermCheck {
 		String fileContent) {
 
 		String javaTermContent = _formatForLoop(
-			javaTerm.getContent(), _arrayPattern);
+			javaTerm.getContent(), fileName, fileContent,
+			_COLLECTION_TYPE_ARRAY, javaTerm.getLineNumber());
 
-		return _formatForLoop(javaTermContent, _listPattern);
+		return _formatForLoop(
+			javaTermContent, fileName, fileContent, _COLLECTION_TYPE_LIST,
+			javaTerm.getLineNumber());
 	}
 
 	@Override
@@ -45,69 +47,202 @@ public class JavaForLoopCheck extends BaseJavaTermCheck {
 		return new String[] {JAVA_CONSTRUCTOR, JAVA_METHOD};
 	}
 
-	private String _formatForLoop(String javaTermContent, Pattern pattern) {
-		Matcher matcher = pattern.matcher(javaTermContent);
+	private String _formatForLoop(
+		String javaTermContent, String fileName, String fileContent, int type,
+		int lineNumber) {
 
-		while (matcher.find()) {
-			String collectionVariableName = matcher.group(3);
-			String countVariableName = matcher.group(1);
+		Pattern pattern1 = _getForLoopPattern(type);
 
-			if (!Objects.equals(collectionVariableName, matcher.group(7)) ||
-				!Objects.equals(countVariableName, matcher.group(2)) ||
-				!Objects.equals(countVariableName, matcher.group(4)) ||
-				!Objects.equals(countVariableName, matcher.group(8))) {
+		Matcher matcher1 = pattern1.matcher(javaTermContent);
+
+		while (matcher1.find()) {
+			String countVarName = matcher1.group(1);
+
+			if (!countVarName.equals(matcher1.group(2)) ||
+				!countVarName.equals(matcher1.group(4))) {
 
 				continue;
 			}
 
-			int x = matcher.end();
+			String insideLoopContent = _getInsideLoopContent(
+				javaTermContent, matcher1.end());
 
-			while (true) {
-				x = javaTermContent.indexOf("}", x + 1);
-
-				if (x == -1) {
-					return javaTermContent;
-				}
-
-				if (ToolsUtil.isInsideQuotes(javaTermContent, x)) {
-					continue;
-				}
-
-				String s = javaTermContent.substring(matcher.end(), x);
-
-				if (getLevel(s, "{", "}") != 0) {
-					continue;
-				}
-
-				if (s.matches("(?s).*\\W" + countVariableName + "\\W.*")) {
-					break;
-				}
-
-				StringBundler sb = new StringBundler(7);
-
-				sb.append("\tfor (");
-				sb.append(matcher.group(5));
-				sb.append(StringPool.SPACE);
-				sb.append(matcher.group(6));
-				sb.append(" : ");
-				sb.append(collectionVariableName);
-				sb.append(") {\n");
-
-				return StringUtil.replaceFirst(
-					javaTermContent, matcher.group(), sb.toString(),
-					matcher.start());
+			if (insideLoopContent == null) {
+				continue;
 			}
+
+			String collectionVarName = matcher1.group(3);
+
+			if (type == _COLLECTION_TYPE_LIST) {
+				Pattern pattern2 = Pattern.compile(
+					">\\s+" + collectionVarName + "\\W");
+
+				Matcher matcher2 = pattern2.matcher(fileContent);
+
+				if (!matcher2.find()) {
+					continue;
+				}
+			}
+
+			if (insideLoopContent.matches(
+					"(?s).*\\." + collectionVarName + "\\W.*")) {
+
+				continue;
+			}
+
+			int varNameCount = _getOccurenceCount(
+				insideLoopContent, "\\W" + countVarName + "\\W");
+
+			if (varNameCount == 0) {
+				continue;
+			}
+
+			int retrieveFromCollectionCount = _getOccurenceCount(
+				insideLoopContent,
+				_getRetrieveFromCollectionRegex(
+					countVarName, collectionVarName, type));
+
+			if (varNameCount != retrieveFromCollectionCount) {
+				continue;
+			}
+
+			if (type == _COLLECTION_TYPE_ARRAY) {
+				int reassignVarCount = _getOccurenceCount(
+					insideLoopContent,
+					StringBundler.concat(
+						"\\W", collectionVarName, "\\[", countVarName,
+						"\\](\\s+[+-]?\\=\\s|--|\\+\\+)"));
+
+				if (reassignVarCount > 0) {
+					continue;
+				}
+			}
+
+			Pattern pattern2 = Pattern.compile(
+				_getAssignVarRegex(countVarName, collectionVarName, type));
+
+			Matcher matcher2 = pattern2.matcher(insideLoopContent);
+
+			if (!matcher2.find() || (varNameCount > 1)) {
+				addMessage(
+					fileName, "Use Enhanced For-Loop",
+					lineNumber - 1 +
+						getLineNumber(javaTermContent, matcher1.start()));
+
+				continue;
+			}
+
+			StringBundler sb = new StringBundler(7);
+
+			sb.append("\tfor (");
+			sb.append(matcher2.group(1));
+			sb.append(StringPool.SPACE);
+			sb.append(matcher2.group(2));
+			sb.append(" : ");
+			sb.append(collectionVarName);
+			sb.append(") {\n");
+
+			javaTermContent = StringUtil.replaceFirst(
+				javaTermContent, matcher1.group(), sb.toString(),
+				matcher1.start());
+
+			javaTermContent = StringUtil.replaceFirst(
+				javaTermContent, matcher2.group(), StringPool.BLANK,
+				matcher1.start());
+
+			return javaTermContent;
 		}
 
 		return javaTermContent;
 	}
 
+	private String _getAssignVarRegex(
+		String countVarName, String collectionVarName, int type) {
+
+		if (type == _COLLECTION_TYPE_ARRAY) {
+			return StringBundler.concat(
+				"\t+(\\w[\\w\\s\\[\\]<>,\\.\\?]*)\\s+(\\w+)\\s+=\\s+",
+				collectionVarName, "\\[", countVarName, "\\];\n");
+		}
+
+		return StringBundler.concat(
+			"\t+(\\w[\\w\\s\\[\\]<>,\\.\\?]*)\\s+(\\w+)\\s+=\\s+",
+			collectionVarName, "\\.get\\(", countVarName, "\\);\n");
+	}
+
+	private Pattern _getForLoopPattern(int type) {
+		if (type == _COLLECTION_TYPE_ARRAY) {
+			return _arrayPattern;
+		}
+
+		return _listPattern;
+	}
+
+	private String _getInsideLoopContent(String s, int x) {
+		int y = x;
+
+		while (true) {
+			y = s.indexOf("}", y + 1);
+
+			if (y == -1) {
+				return null;
+			}
+
+			if (ToolsUtil.isInsideQuotes(s, y)) {
+				continue;
+			}
+
+			String insideLoopContent = s.substring(x, y);
+
+			if (getLevel(insideLoopContent, "{", "}") == 0) {
+				return insideLoopContent;
+			}
+		}
+	}
+
+	private int _getOccurenceCount(
+		String s, String regex, String... arguments) {
+
+		for (int i = 0; i < arguments.length; i++) {
+			String argument = arguments[i];
+
+			regex = StringUtil.replace(regex, "{" + i + "}", argument);
+		}
+
+		int count = 0;
+
+		Pattern pattern = Pattern.compile(regex);
+
+		Matcher matcher = pattern.matcher(s);
+
+		while (matcher.find()) {
+			count++;
+		}
+
+		return count;
+	}
+
+	private String _getRetrieveFromCollectionRegex(
+		String countVarName, String collectionVarName, int type) {
+
+		if (type == _COLLECTION_TYPE_ARRAY) {
+			return StringBundler.concat(
+				"\\W", collectionVarName, "\\[", countVarName, "\\]");
+		}
+
+		return StringBundler.concat(
+			"\\W", collectionVarName, "\\.get\\(", countVarName, "\\)");
+	}
+
+	private static final int _COLLECTION_TYPE_ARRAY = 0;
+
+	private static final int _COLLECTION_TYPE_LIST = 1;
+
 	private final Pattern _arrayPattern = Pattern.compile(
 		"\tfor \\(int (\\w+) = 0;\\s+(\\w+) < (\\w+)\\.length;\\s+(\\w+)\\+" +
-			"\\+\\) \\{\n\\s+([\\w\\[\\]]+) (\\w+) =\\s+(\\w+)\\[(\\w+)\\];\n");
+			"\\+\\) \\{\n");
 	private final Pattern _listPattern = Pattern.compile(
 		"\tfor \\(int (\\w+) = 0;\\s+(\\w+) < (\\w+)\\.size\\(\\);\\s+(\\w+)" +
-			"\\+\\+\\) \\{\n\\s+([\\w<>]+) (\\w+) =\\s+(\\w+)\\.get\\(" +
-				"(\\w+)\\);\n");
+			"\\+\\+\\) \\{\n");
 
 }
