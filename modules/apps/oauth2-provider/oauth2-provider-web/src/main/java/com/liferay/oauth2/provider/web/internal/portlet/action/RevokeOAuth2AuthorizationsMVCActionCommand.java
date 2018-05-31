9a0c8adc0db1d8d6e3ecfff1fd7ea8341d44232d
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2018-05-31 14:48:48 +0100

    OAUTH2-122 Connected Applications portlet MVC commands (one shared with admin portlet)

---
+++ modules/apps/oauth2-provider/oauth2-provider-web/src/main/java/com/liferay/oauth2/provider/web/internal/portlet/action/RevokeOAuth2AuthorizationsMVCActionCommand.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.web.internal.portlet.action;
+
+import com.liferay.oauth2.provider.service.OAuth2AuthorizationService;
+import com.liferay.oauth2.provider.web.internal.constants.OAuth2ProviderPortletKeys;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tomas Polesovsky
+ * @author Stian Sigvartsen
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + OAuth2ProviderPortletKeys.OAUTH2_ADMIN,
+		"javax.portlet.name=" + OAuth2ProviderPortletKeys.OAUTH2_CONNECTED_APPLICATIONS,
+		"mvc.command.name=/admin/revoke_oauth2_authorizations",
+		"mvc.command.name=/connected_applications/revoke_oauth2_authorizations"
+	}
+)
+public class RevokeOAuth2AuthorizationsMVCActionCommand
+	implements MVCActionCommand {
+
+	@Override
+	public boolean processAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws PortletException {
+
+		long[] oAuth2AuthorizationIds = StringUtil.split(
+			ParamUtil.getString(actionRequest, "oAuth2AuthorizationIds"), 0L);
+
+		try {
+			for (long oAuth2AuthorizationId : oAuth2AuthorizationIds) {
+				_oAuth2AuthorizationService.revokeOAuth2Authorization(
+					oAuth2AuthorizationId);
+			}
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe);
+			}
+
+			SessionErrors.add(actionRequest, pe.getClass());
+		}
+
+		String backURL = ParamUtil.get(
+			actionRequest, "backURL", StringPool.BLANK);
+
+		if (Validator.isNotNull(backURL)) {
+			actionResponse.setRenderParameter("redirect", backURL);
+		}
+
+		return true;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		RevokeOAuth2AuthorizationsMVCActionCommand.class);
+
+	@Reference
+	private OAuth2AuthorizationService _oAuth2AuthorizationService;
+
+}
---
+++ modules/apps/oauth2-provider/oauth2-provider-web/src/main/java/com/liferay/oauth2/provider/web/internal/portlet/action/ViewConnectedApplicationsMVCRenderCommand.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.web.internal.portlet.action;
+
+import com.liferay.oauth2.provider.model.OAuth2ApplicationScopeAliases;
+import com.liferay.oauth2.provider.model.OAuth2Authorization;
+import com.liferay.oauth2.provider.scope.liferay.ApplicationDescriptorLocator;
+import com.liferay.oauth2.provider.scope.liferay.ScopeDescriptorLocator;
+import com.liferay.oauth2.provider.scope.liferay.ScopeLocator;
+import com.liferay.oauth2.provider.service.OAuth2ApplicationScopeAliasesLocalService;
+import com.liferay.oauth2.provider.service.OAuth2ApplicationService;
+import com.liferay.oauth2.provider.service.OAuth2AuthorizationLocalService;
+import com.liferay.oauth2.provider.web.internal.AssignableScopes;
+import com.liferay.oauth2.provider.web.internal.constants.OAuth2ProviderPortletKeys;
+import com.liferay.oauth2.provider.web.internal.constants.OAuth2ProviderWebKeys;
+import com.liferay.oauth2.provider.web.internal.display.context.OAuth2ConnectedApplicationsPortletDisplayContext;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tomas Polesovsky
+ * @author Stian Sigvartsen
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + OAuth2ProviderPortletKeys.OAUTH2_CONNECTED_APPLICATIONS,
+		"mvc.command.name=/", "mvc.command.name=/connected_applications/view"
+	}
+)
+public class ViewConnectedApplicationsMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		List<OAuth2Authorization> userOAuth2Authorizations =
+			_oAuth2AuthorizationLocalService.getUserOAuth2Authorizations(
+				themeDisplay.getUserId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				null);
+
+		long oAuth2AuthorizationId = ParamUtil.getLong(
+			renderRequest, "oAuth2AuthorizationId");
+
+		if (!ListUtil.exists(
+				userOAuth2Authorizations,
+				o -> o.getOAuth2AuthorizationId() == oAuth2AuthorizationId)) {
+
+			OAuth2ConnectedApplicationsPortletDisplayContext
+				oAuth2ConnectedApplicationsPortletDisplayContext =
+					new OAuth2ConnectedApplicationsPortletDisplayContext(
+						renderRequest);
+
+			renderRequest.setAttribute(
+				OAuth2ProviderWebKeys.
+					OAUTH2_CONNECTED_APPLICATIONS_PORTLET_DISPLAY_CONTEXT,
+				oAuth2ConnectedApplicationsPortletDisplayContext);
+
+			return "/connected_applications/view.jsp";
+		}
+
+		long oAuth2ApplicationId = ParamUtil.getLong(
+			renderRequest, "oAuth2ApplicationId");
+
+		List<OAuth2ApplicationScopeAliases> oAuth2ApplicationScopeAliaseses =
+			_oAuth2ApplicationScopeAliasesLocalService.
+				getOAuth2ApplicationScopeAliaseses(
+					oAuth2ApplicationId, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+					null);
+
+		Set<String> scopeAliases = new HashSet<>();
+
+		for (OAuth2ApplicationScopeAliases oAuth2ApplicationScopeAliases :
+				oAuth2ApplicationScopeAliaseses) {
+
+			scopeAliases.addAll(
+				oAuth2ApplicationScopeAliases.getScopeAliasesList());
+		}
+
+		AssignableScopes assignableScopes = new AssignableScopes(
+			_applicationDescriptorLocator, themeDisplay.getLocale(),
+			_scopeDescriptorLocator);
+
+		for (String scopeAlias : scopeAliases) {
+			assignableScopes.addLiferayOAuth2Scopes(
+				_scopeLocator.getLiferayOAuth2Scopes(
+					themeDisplay.getCompanyId(), scopeAlias));
+		}
+
+		OAuth2ConnectedApplicationsPortletDisplayContext
+			oAuth2ConnectedApplicationsPortletDisplayContext =
+				new OAuth2ConnectedApplicationsPortletDisplayContext(
+					assignableScopes, renderRequest, _oAuth2ApplicationService);
+
+		renderRequest.setAttribute(
+			OAuth2ProviderWebKeys.
+				OAUTH2_CONNECTED_APPLICATIONS_PORTLET_DISPLAY_CONTEXT,
+			oAuth2ConnectedApplicationsPortletDisplayContext);
+
+		return "/connected_applications/view_application.jsp";
+	}
+
+	@Reference
+	private ApplicationDescriptorLocator _applicationDescriptorLocator;
+
+	@Reference
+	private OAuth2ApplicationScopeAliasesLocalService
+		_oAuth2ApplicationScopeAliasesLocalService;
+
+	@Reference
+	private OAuth2ApplicationService _oAuth2ApplicationService;
+
+	@Reference
+	private OAuth2AuthorizationLocalService _oAuth2AuthorizationLocalService;
+
+	@Reference
+	private ScopeDescriptorLocator _scopeDescriptorLocator;
+
+	@Reference
+	private ScopeLocator _scopeLocator;
+
+}
