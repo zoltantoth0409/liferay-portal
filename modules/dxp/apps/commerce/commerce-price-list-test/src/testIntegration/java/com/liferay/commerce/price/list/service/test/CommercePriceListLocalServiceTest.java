b46ca03b31ceb0d8c874a254bf506ac7a9a35d82
Zoltán Takács <zoltan.takacs@liferay.com>
2018-05-31 15:49:16 +0200

    COMMERCE-442 Add more test for 'upsertCommercePriceList'

--- modules/dxp/apps/commerce/commerce-price-list-test/src/testIntegration/java/com/liferay/commerce/price/list/service/test/CommercePriceListLocalServiceTest.java
+++ modules/dxp/apps/commerce/commerce-price-list-test/src/testIntegration/java/com/liferay/commerce/price/list/service/test/CommercePriceListLocalServiceTest.java
@@ -17,9 +17,12 @@ package com.liferay.commerce.price.list.service.test;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.currency.exception.NoSuchCurrencyException;
+import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.price.list.model.CommercePriceList;
 import com.liferay.commerce.price.list.service.CommercePriceListLocalService;
 import com.liferay.commerce.price.list.test.util.CommercePriceListTestUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -57,7 +60,74 @@ public class CommercePriceListLocalServiceTest {
 	}
 
 	@Test
-	public void testUpsertCommercePriceList() throws Exception {
+	public void testAddCommercePriceList1() throws Exception {
+		frutillaRule.scenario(
+			"Adding a new Price List"
+		).given(
+			"A site (group)"
+		).and(
+			"A currency code expressed with 3-letter ISO 4217 format"
+		).and(
+			"The name of the new list"
+		).when(
+			"The name of the Price Lists"
+		).and(
+			"The currency code is checked against the input data"
+		).then(
+			"The result should be a new Price List on the given site"
+		);
+
+		Currency currency = Currency.getInstance(Locale.US);
+		String name = RandomTestUtil.randomString();
+
+		CommercePriceList commercePriceList =
+			CommercePriceListTestUtil.addCommercePriceList(
+				_group.getGroupId(), currency.getCurrencyCode(), name,
+				RandomTestUtil.randomDouble(), true, null, null, null);
+
+		_assertPriceListAttributes(currency, name, commercePriceList);
+	}
+
+	@Test
+	public void testAddCommercePriceList2() throws Exception {
+		frutillaRule.scenario(
+			"Adding a new Price List with an external reference code"
+		).given(
+			"A site (group)"
+		).and(
+			"A currency code expressed with 3-letter ISO 4217 format"
+		).and(
+			"The name of the new list"
+		).and(
+			"The external reference code (externalReferenceCode)"
+		).when(
+			"The name of the Price Lists"
+		).and(
+			"The currency code"
+		).and(
+			"The external reference code are checked against the input data"
+		).then(
+			"The result should be a new Price List on the given site"
+		);
+
+		Currency currency = Currency.getInstance(Locale.US);
+		String externalReferenceCode = RandomTestUtil.randomString();
+		String name = RandomTestUtil.randomString();
+
+		CommercePriceList commercePriceList =
+			CommercePriceListTestUtil.addCommercePriceList(
+				_group.getGroupId(), currency.getCurrencyCode(), name,
+				RandomTestUtil.randomDouble(), true, null, null,
+				externalReferenceCode);
+
+		_assertPriceListAttributes(currency, name, commercePriceList);
+		Assert.assertThat(
+			externalReferenceCode,
+			equalTo(commercePriceList.getExternalReferenceCode()));
+	}
+
+	@Test
+	public void testUpsertCommercePriceList1() throws Exception {
 		frutillaRule.scenario(
 			"Adding a new Price List"
 		).given(
@@ -79,14 +149,144 @@ public class CommercePriceListLocalServiceTest {
 
 		CommercePriceList commercePriceList =
 			CommercePriceListTestUtil.upsertCommercePriceList(
-				_group.getGroupId(), 0L, currency.getCurrencyCode(), name, 0D,
-				true, null, null, null);
+				_group.getGroupId(), 0L, currency.getCurrencyCode(), name,
+				RandomTestUtil.randomDouble(), true, null, null, null);
 
-		Assert.assertThat(name, equalTo(commercePriceList.getName()));
+		_assertPriceListAttributes(currency, name, commercePriceList);
+	}
+
+	@Test
+	public void testUpsertCommercePriceList2() throws Exception {
+		frutillaRule.scenario(
+			"Update an existing Price List"
+		).given(
+			"A site (group)"
+		).and(
+			"A currency code expressed with 3-letter ISO 4217 format"
+		).and(
+			"The name of the new list"
+		).when(
+			"The external reference code (externalReferenceCode) is used"
+		).and(
+			"(commercePriceListId) is not used in the method invocation"
+		).then(
+			"The result should be the updated Price List with the given " +
+				"(externalReferenceCode) on the given site"
+		);
+
+		Currency currency = Currency.getInstance(Locale.US);
+		String externalReferenceCode = RandomTestUtil.randomString();
+		String name = RandomTestUtil.randomString();
+
+		CommercePriceListTestUtil.addCommercePriceList(
+			_group.getGroupId(), currency.getCurrencyCode(), name,
+			RandomTestUtil.randomDouble(), true, null, null,
+			externalReferenceCode);
+
+		Currency updatedCurrency = Currency.getInstance(Locale.UK);
+		String updatedName = RandomTestUtil.randomString();
+
+		CommercePriceListTestUtil.upsertCommercePriceList(
+			_group.getGroupId(), 0L, updatedCurrency.getCurrencyCode(),
+			updatedName, RandomTestUtil.randomDouble(), true, null, null,
+			externalReferenceCode);
+
+		CommercePriceList commercePriceList =
+			_commercePriceListLocalService.fetchByExternalReferenceCode(
+				externalReferenceCode);
+
+		_assertPriceListAttributes(
+			updatedCurrency, updatedName, commercePriceList);
+	}
+
+	@Test
+	public void testUpsertCommercePriceList3() throws Exception {
+		frutillaRule.scenario(
+			"Update an existing Price List"
+		).given(
+			"A site (group)"
+		).and(
+			"A currency code expressed with 3-letter ISO 4217 format"
+		).and(
+			"The name of the new list"
+		).when(
+			"The external reference code (externalReferenceCode) is not used"
+		).and(
+			"(commercePriceListId) is used in the method invocation"
+		).then(
+			"The result should be the updated Price List with the given " +
+				"(commercePriceListId) on the given site"
+		);
+
+		Currency currency = Currency.getInstance(Locale.US);
+		String name = RandomTestUtil.randomString();
+
+		CommercePriceList commercePriceList =
+			CommercePriceListTestUtil.addCommercePriceList(
+				_group.getGroupId(), currency.getCurrencyCode(), name,
+				RandomTestUtil.randomDouble(), true, null, null, null);
+
+		long commercePriceListId = commercePriceList.getCommercePriceListId();
+
+		Currency updatedCurrency = Currency.getInstance(Locale.UK);
+		String updatedName = RandomTestUtil.randomString();
+
+		CommercePriceListTestUtil.upsertCommercePriceList(
+			_group.getGroupId(), commercePriceListId,
+			updatedCurrency.getCurrencyCode(), updatedName,
+			RandomTestUtil.randomDouble(), true, null, null, null);
+
+		CommercePriceList updatedCommercePriceList =
+			_commercePriceListLocalService.getCommercePriceList(
+				commercePriceListId);
+
+		_assertPriceListAttributes(
+			updatedCurrency, updatedName, updatedCommercePriceList);
+	}
+
+	@Test(expected = NoSuchCurrencyException.class)
+	public void testUpsertCommercePriceList4() throws Exception {
+		frutillaRule.scenario(
+			"Adding a new Price List"
+		).given(
+			"A site (group)"
+		).and(
+			"A non-existent currency code on the site"
+		).and(
+			"The name of the new list"
+		).when(
+			"The external reference code (externalReferenceCode)"
+		).and(
+			"(commercePriceListId) is not used in the method invocation"
+		).then(
+			"CommercePriceListCurrencyException should be thrown"
+		);
+
+		Currency currency = Currency.getInstance("KGS");
+		String name = RandomTestUtil.randomString();
+
+		CommercePriceListTestUtil.upsertCommercePriceList(
+			_group.getGroupId(), 0L, currency.getCurrencyCode(), name,
+			RandomTestUtil.randomDouble(), true, null, null, null);
 	}
 
 	@Rule
 	public final FrutillaRule frutillaRule = new FrutillaRule();
+
+	private static void _assertPriceListAttributes(
+			Currency currency, String name, CommercePriceList commercePriceList)
+		throws PortalException {
+
+		CommerceCurrency commerceCurrency =
+			commercePriceList.getCommerceCurrency();
+
+		String currencyCode = commerceCurrency.getCode();
+
+		Assert.assertThat(
+			currency, equalTo(Currency.getInstance(currencyCode)));
+
+		Assert.assertThat(name, equalTo(commercePriceList.getName()));
+	}
 
 	@Inject
 	private CommercePriceListLocalService _commercePriceListLocalService;
--- modules/dxp/apps/commerce/commerce-price-list-test/src/testIntegration/java/com/liferay/commerce/price/list/test/util/CommercePriceListTestUtil.java
+++ modules/dxp/apps/commerce/commerce-price-list-test/src/testIntegration/java/com/liferay/commerce/price/list/test/util/CommercePriceListTestUtil.java
@@ -33,6 +33,74 @@ import java.util.Date;
  */
 public class CommercePriceListTestUtil {
 
+	public static CommercePriceList addCommercePriceList(
+			Long groupId, String currency, String name, Double priority,
+			Boolean neverExpire, Date displayDate, Date expirationDate,
+			String externalReferenceCode)
+		throws Exception {
+
+		long commerceCurrencyId = _getCommerceCurrencyId(groupId, currency);
+
+		if (neverExpire == null) {
+			neverExpire = Boolean.TRUE;
+		}
+
+		if (priority == null) {
+			priority = 0D;
+		}
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(groupId);
+
+		User user = UserLocalServiceUtil.getUserById(
+			serviceContext.getUserId());
+
+		Calendar displayCalendar = CalendarFactoryUtil.getCalendar(
+			user.getTimeZone());
+
+		if (displayDate != null) {
+			displayCalendar = _convertDateToCalendar(displayDate);
+		}
+
+		int displayDateMonth = displayCalendar.get(Calendar.MONTH);
+		int displayDateDay = displayCalendar.get(Calendar.DAY_OF_MONTH);
+		int displayDateYear = displayCalendar.get(Calendar.YEAR);
+		int displayDateHour = displayCalendar.get(Calendar.HOUR);
+		int displayDateMinute = displayCalendar.get(Calendar.MINUTE);
+		int displayDateAmPm = displayCalendar.get(Calendar.AM_PM);
+
+		if (displayDateAmPm == Calendar.PM) {
+			displayDateHour += 12;
+		}
+
+		Calendar expirationCalendar = CalendarFactoryUtil.getCalendar(
+			user.getTimeZone());
+
+		expirationCalendar.add(Calendar.MONTH, 1);
+
+		if (expirationDate != null) {
+			expirationCalendar = _convertDateToCalendar(expirationDate);
+		}
+
+		int expirationDateMonth = expirationCalendar.get(Calendar.MONTH);
+		int expirationDateDay = expirationCalendar.get(Calendar.DAY_OF_MONTH);
+		int expirationDateYear = expirationCalendar.get(Calendar.YEAR);
+		int expirationDateHour = expirationCalendar.get(Calendar.HOUR);
+		int expirationDateMinute = expirationCalendar.get(Calendar.MINUTE);
+		int expirationDateAmPm = expirationCalendar.get(Calendar.AM_PM);
+
+		if (expirationDateAmPm == Calendar.PM) {
+			expirationDateHour += 12;
+		}
+
+		return CommercePriceListLocalServiceUtil.addCommercePriceList(
+			commerceCurrencyId, name, priority, displayDateMonth,
+			displayDateDay, displayDateYear, displayDateHour, displayDateMinute,
+			expirationDateMonth, expirationDateDay, expirationDateYear,
+			expirationDateHour, expirationDateMinute, externalReferenceCode,
+			neverExpire, serviceContext);
+	}
+
 	public static CommercePriceList upsertCommercePriceList(
 			Long groupId, Long commercePriceListId, String currency,
 			String name, Double priority, Boolean neverExpire, Date displayDate,
