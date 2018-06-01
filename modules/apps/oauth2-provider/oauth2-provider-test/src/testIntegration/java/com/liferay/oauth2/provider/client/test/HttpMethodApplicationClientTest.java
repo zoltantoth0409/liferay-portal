f9bd94e92c52afa9a4c627340a8ad621c24f437d
Carlos Sierra <carlos.sierra@liferay.com>
2018-06-01 21:07:49 +0200

    Revert "OAUTH2-131 Temporarily ignore tests"
    This reverts commit 33871ae2917adecae0085292a807e9bb770a8529.

--- modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/HttpMethodApplicationClientTest.java
+++ modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/HttpMethodApplicationClientTest.java
@@ -37,14 +37,12 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Carlos Sierra Andrés
  */
-@Ignore
 @RunAsClient
 @RunWith(Arquillian.class)
 public class HttpMethodApplicationClientTest extends BaseClientTestCase {
