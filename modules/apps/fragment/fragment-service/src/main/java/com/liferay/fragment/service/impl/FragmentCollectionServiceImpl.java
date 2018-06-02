4447ab00bba31370767f92757e3620c68fe673dc
Brian Chan <brian.chan@liferay.com>
2018-06-02 11:43:54 -0700

    LPS-81796 Fix compile error now that the filter find by methods are gone, Eudaldo, you need to fix this. Thx

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentCollectionServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentCollectionServiceImpl.java
@@ -119,15 +119,14 @@ public class FragmentCollectionServiceImpl
 
 	@Override
 	public List<FragmentCollection> getFragmentCollections(long groupId) {
-		return fragmentCollectionPersistence.filterFindByGroupId(groupId);
+		return fragmentCollectionPersistence.findByGroupId(groupId);
 	}
 
 	@Override
 	public List<FragmentCollection> getFragmentCollections(
 		long groupId, int start, int end) {
 
-		return fragmentCollectionPersistence.filterFindByGroupId(
-			groupId, start, end);
+		return fragmentCollectionPersistence.findByGroupId(groupId, start, end);
 	}
 
 	@Override
@@ -135,7 +134,7 @@ public class FragmentCollectionServiceImpl
 		long groupId, int start, int end,
 		OrderByComparator<FragmentCollection> orderByComparator) {
 
-		return fragmentCollectionPersistence.filterFindByGroupId(
+		return fragmentCollectionPersistence.findByGroupId(
 			groupId, start, end, orderByComparator);
 	}
 
@@ -144,19 +143,19 @@ public class FragmentCollectionServiceImpl
 		long groupId, String name, int start, int end,
 		OrderByComparator<FragmentCollection> orderByComparator) {
 
-		return fragmentCollectionPersistence.filterFindByG_LikeN(
+		return fragmentCollectionPersistence.findByG_LikeN(
 			groupId, _customSQL.keywords(name, WildcardMode.SURROUND)[0], start,
 			end, orderByComparator);
 	}
 
 	@Override
 	public int getFragmentCollectionsCount(long groupId) {
-		return fragmentCollectionPersistence.filterCountByGroupId(groupId);
+		return fragmentCollectionPersistence.countByGroupId(groupId);
 	}
 
 	@Override
 	public int getFragmentCollectionsCount(long groupId, String name) {
-		return fragmentCollectionPersistence.filterCountByG_LikeN(
+		return fragmentCollectionPersistence.countByG_LikeN(
 			groupId, _customSQL.keywords(name, WildcardMode.SURROUND)[0]);
 	}
 
