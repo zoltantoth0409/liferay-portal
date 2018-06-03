1f2c28ebc6e853cc854adeca8db42b65e3c83599
Chema Balsas <jose.balsas@liferay.com>
2018-06-03 13:23:29 +0200

    LPS-81714 Creates pattern to open toast notifications

---
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/toast/commands/OpenToast.es.js
@@ -0,0 +1,57 @@
+import dom from 'metal-dom';
+import {ClayToast} from 'clay-alert';
+
+/**
+ * Function that implements the Toast pattern, which allows to present feedback
+ * to user actions as a toast message in the lower left corner of the page
+ *
+ * @param {string} message The message to show in the toast notification
+ * @param {string} title The title associated with the message
+ * @param {string} type The type of notification to show. It can be one of the
+ * following: 'danger', 'info', 'success', 'warning'
+ * @review
+ */
+
+function openToast(
+	{
+		message = '',
+		title = Liferay.Language.get('success'),
+		type = 'success'
+	}
+) {
+	var alertContainer = document.getElementById('alertContainer');
+
+	if (!alertContainer) {
+		alertContainer = document.createElement('div');
+		alertContainer.id = 'alertContainer';
+
+		dom.addClasses(alertContainer, 'alert-notifications alert-notifications-fixed');
+		dom.enterDocument(alertContainer);
+	}
+	else {
+		dom.removeChildren(alertContainer);
+	}
+
+	const clayToast = new ClayToast(
+		{
+			autoClose: true,
+			destroyOnHide: true,
+			message: message,
+			spritemap: themeDisplay.getPathThemeImages() + '/lexicon/icons.svg',
+			style: type,
+			title: title
+		},
+		alertContainer
+	);
+
+	dom.removeClasses(clayToast.element, 'show');
+
+	requestAnimationFrame(
+		function() {
+			dom.addClasses(clayToast.element, 'show');
+		}
+	);
+}
+
+export {openToast};
+export default openToast;
