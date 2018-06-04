9521f26c90c44228346a3c81cf8fdaaee93b376c
Lance Ji <zheyi.ji@liferay.com>
2018-06-04 11:45:27 -0700

    Revert "LPS-80295 Fix test value"
    This reverts commit ced3791171f855fb7fd8c1e047e013dab9f4ea02.

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/exportimport/data/handler/test/ManifestSummaryTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/exportimport/data/handler/test/ManifestSummaryTest.java
@@ -170,7 +170,7 @@ public class ManifestSummaryTest
 			manifestSummary.getModelAdditionCounters();
 
 		Assert.assertEquals(
-			modelAdditionCounters.toString(), 5, modelAdditionCounters.size());
+			modelAdditionCounters.toString(), 4, modelAdditionCounters.size());
 
 		Assert.assertEquals(
 			1,
@@ -223,7 +223,7 @@ public class ManifestSummaryTest
 			manifestSummary.getModelAdditionCounters();
 
 		Assert.assertEquals(
-			modelAdditionCounters.toString(), 5, modelAdditionCounters.size());
+			modelAdditionCounters.toString(), 4, modelAdditionCounters.size());
 
 		Assert.assertEquals(
 			1,
