2234745abc1a00cdf1430d1ee1834e9bfe479827
Lily.Chi <lily.chi@liferay.com>
2018-06-04 17:51:07 +0800

    Revert "LPS-74551 portal-search-web: Search Bar: Set pre-filter with SearchContext groupIds"
    This reverts commit 5db6c01405f582652949ee751dc6475d31cb9d7d.

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/shared/search/SearchBarPortletSharedSearchContributor.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/shared/search/SearchBarPortletSharedSearchContributor.java
@@ -14,11 +14,14 @@
 
 package com.liferay.portal.search.web.internal.search.bar.portlet.shared.search;
 
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.generic.BooleanClauseImpl;
+import com.liferay.portal.kernel.search.generic.TermQueryImpl;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.search.web.internal.display.context.Keywords;
 import com.liferay.portal.search.web.internal.display.context.SearchScope;
-import com.liferay.portal.search.web.internal.display.context.SearchScopePreference;
 import com.liferay.portal.search.web.internal.search.bar.constants.SearchBarPortletKeys;
 import com.liferay.portal.search.web.internal.search.bar.portlet.SearchBarPortletPreferences;
 import com.liferay.portal.search.web.internal.search.bar.portlet.SearchBarPortletPreferencesImpl;
@@ -57,28 +60,17 @@ public class SearchBarPortletSharedSearchContributor
 		SearchBarPortletPreferences searchBarPortletPreferences,
 		PortletSharedSearchSettings portletSharedSearchSettings) {
 
-		SearchScope searchScope = getSearchScope(
+		Optional<Long> groupIdOptional = getThisSiteGroupId(
 			searchBarPortletPreferences, portletSharedSearchSettings);
 
-		if (searchScope != SearchScope.THIS_SITE) {
-			return;
-		}
-
-		SearchContext searchContext =
-			portletSharedSearchSettings.getSearchContext();
-
-		searchContext.setGroupIds(
-			new long[] {getScopeGroupId(portletSharedSearchSettings)});
-	}
-
-	protected SearchScope getDefaultSearchScope() {
-		SearchBarPortletPreferences searchBarPortletPreferences =
-			new SearchBarPortletPreferencesImpl(Optional.empty());
-
-		SearchScopePreference searchScopePreference =
-			searchBarPortletPreferences.getSearchScopePreference();
-
-		return searchScopePreference.getSearchScope();
+		groupIdOptional.ifPresent(
+			groupId -> {
+				portletSharedSearchSettings.addCondition(
+					new BooleanClauseImpl(
+						new TermQueryImpl(
+							Field.GROUP_ID, String.valueOf(groupId)),
+						BooleanClauseOccur.MUST));
+			});
 	}
 
 	protected long getScopeGroupId(
@@ -90,28 +82,34 @@ public class SearchBarPortletSharedSearchContributor
 		return themeDisplay.getScopeGroupId();
 	}
 
-	protected SearchScope getSearchScope(
+	protected Optional<SearchScope> getSearchScope(
 		SearchBarPortletPreferences searchBarPortletPreferences,
 		PortletSharedSearchSettings portletSharedSearchSettings) {
 
-		SearchScopePreference searchScopePreference =
-			searchBarPortletPreferences.getSearchScopePreference();
-
-		if (searchScopePreference !=
-				SearchScopePreference.LET_THE_USER_CHOOSE) {
-
-			return searchScopePreference.getSearchScope();
-		}
+		String parameterName =
+			searchBarPortletPreferences.getScopeParameterName();
 
 		Optional<String> parameterValueOptional =
-			portletSharedSearchSettings.getParameter(
-				searchBarPortletPreferences.getScopeParameterName());
+			portletSharedSearchSettings.getParameter(parameterName);
 
-		return parameterValueOptional.map(
-			SearchScope::getSearchScope
-		).orElseGet(
-			this::getDefaultSearchScope
-		);
+		Optional<SearchScope> searchScopeOptional = parameterValueOptional.map(
+			SearchScope::getSearchScope);
+
+		return searchScopeOptional;
+	}
+
+	protected Optional<Long> getThisSiteGroupId(
+		SearchBarPortletPreferences searchBarPortletPreferences,
+		PortletSharedSearchSettings portletSharedSearchSettings) {
+
+		Optional<SearchScope> searchScopeOptional = getSearchScope(
+			searchBarPortletPreferences, portletSharedSearchSettings);
+
+		Optional<SearchScope> thisSiteSearchScopeOptional =
+			searchScopeOptional.filter(SearchScope.THIS_SITE::equals);
+
+		return thisSiteSearchScopeOptional.map(
+			searchScope -> getScopeGroupId(portletSharedSearchSettings));
 	}
 
 	protected boolean isLuceneSyntax(
