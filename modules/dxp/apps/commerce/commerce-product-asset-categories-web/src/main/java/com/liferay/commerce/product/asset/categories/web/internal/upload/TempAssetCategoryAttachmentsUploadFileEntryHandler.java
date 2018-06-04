c367a8db87a52bd9622ea373dadb7f67327bf6ba
Alessio Rendina <asilcomando@gmail.com>
2018-06-04 13:02:20 +0200

    COMMERCE-X Merged upload handlers

--- modules/dxp/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/upload/TempAssetCategoryAttachmentsUploadFileEntryHandler.java
+++ modules/dxp/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/upload/TempAssetCategoryAttachmentsUploadFileEntryHandler.java
@@ -14,27 +14,83 @@
 
 package com.liferay.commerce.product.asset.categories.web.internal.upload;
 
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.service.AssetCategoryService;
+import com.liferay.commerce.product.definitions.web.configuration.AttachmentsConfiguration;
+import com.liferay.commerce.product.exception.CPAttachmentFileEntryNameException;
+import com.liferay.commerce.product.exception.CPAttachmentFileEntrySizeException;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.TempFileEntryUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.upload.UniqueFileNameProvider;
+import com.liferay.upload.UploadFileEntryHandler;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alessio Antonio Rendina
  */
-@Component(service = TempAssetCategoryAttachmentsUploadFileEntryHandler.class)
+@Component(
+	configurationPid = "com.liferay.commerce.product.definitions.web.configuration.AttachmentsConfiguration",
+	configurationPolicy = ConfigurationPolicy.OPTIONAL,
+	service = TempAssetCategoryAttachmentsUploadFileEntryHandler.class
+)
 public class TempAssetCategoryAttachmentsUploadFileEntryHandler
-	extends AssetCategoryAttachmentsUploadFileEntryHandler {
+	implements UploadFileEntryHandler {
 
 	@Override
+	public FileEntry upload(UploadPortletRequest uploadPortletRequest)
+		throws IOException, PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)uploadPortletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		long categoryId = ParamUtil.getLong(uploadPortletRequest, "categoryId");
+
+		AssetCategory assetCategory = assetCategoryService.fetchCategory(
+			categoryId);
+
+		String fileName = uploadPortletRequest.getFileName(_PARAMETER_NAME);
+		long size = uploadPortletRequest.getSize(_PARAMETER_NAME);
+
+		_validateFile(fileName, size);
+
+		String contentType = uploadPortletRequest.getContentType(
+			_PARAMETER_NAME);
+
+		try (InputStream inputStream =
+				uploadPortletRequest.getFileAsStream(_PARAMETER_NAME)) {
+
+			return addFileEntry(
+				assetCategory.getCategoryId(), fileName, contentType,
+				inputStream, themeDisplay);
+		}
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		_attachmentsConfiguration = ConfigurableUtil.createConfigurable(
+			AttachmentsConfiguration.class, properties);
+	}
+
 	protected FileEntry addFileEntry(
 			long categoryId, String fileName, String contentType,
 			InputStream inputStream, ThemeDisplay themeDisplay)
@@ -47,6 +103,9 @@ public class TempAssetCategoryAttachmentsUploadFileEntryHandler
 			themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
 			_TEMP_FOLDER_NAME, uniqueFileName, inputStream, contentType);
 	}
+
+	@Reference
+	protected AssetCategoryService assetCategoryService;
 
 	private boolean _exists(ThemeDisplay themeDisplay, String curFileName) {
 		try {
@@ -68,11 +127,40 @@ public class TempAssetCategoryAttachmentsUploadFileEntryHandler
 		}
 	}
 
+	private void _validateFile(String fileName, long size)
+		throws PortalException {
+
+		if ((_attachmentsConfiguration.imageMaxSize() > 0) &&
+			(size > _attachmentsConfiguration.imageMaxSize())) {
+
+			throw new CPAttachmentFileEntrySizeException();
+		}
+
+		String extension = FileUtil.getExtension(fileName);
+
+		String[] imageExtensions = _attachmentsConfiguration.imageExtensions();
+
+		for (String imageExtension : imageExtensions) {
+			if (StringPool.STAR.equals(imageExtension) ||
+				imageExtension.equals(StringPool.PERIOD + extension)) {
+
+				return;
+			}
+		}
+
+		throw new CPAttachmentFileEntryNameException(
+			"Invalid image for file name " + fileName);
+	}
+
+	private static final String _PARAMETER_NAME = "imageSelectorFileName";
+
 	private static final String _TEMP_FOLDER_NAME =
 		TempAssetCategoryAttachmentsUploadFileEntryHandler.class.getName();
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		TempAssetCategoryAttachmentsUploadFileEntryHandler.class);
+
+	private volatile AttachmentsConfiguration _attachmentsConfiguration;
 
 	@Reference
 	private UniqueFileNameProvider _uniqueFileNameProvider;
--- modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/upload/TempAttachmentsUploadFileEntryHandler.java
+++ modules/dxp/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/upload/TempAttachmentsUploadFileEntryHandler.java
@@ -14,27 +14,84 @@
 
 package com.liferay.commerce.product.definitions.web.internal.upload;
 
+import com.liferay.commerce.product.definitions.web.configuration.AttachmentsConfiguration;
+import com.liferay.commerce.product.exception.CPAttachmentFileEntryNameException;
+import com.liferay.commerce.product.exception.CPAttachmentFileEntrySizeException;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.TempFileEntryUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.upload.UniqueFileNameProvider;
+import com.liferay.upload.UploadFileEntryHandler;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marco Leo
  */
-@Component(service = TempAttachmentsUploadFileEntryHandler.class)
+@Component(
+	configurationPid = "com.liferay.commerce.product.definitions.web.configuration.AttachmentsConfiguration",
+	configurationPolicy = ConfigurationPolicy.OPTIONAL,
+	service = TempAttachmentsUploadFileEntryHandler.class
+)
 public class TempAttachmentsUploadFileEntryHandler
-	extends AttachmentsUploadFileEntryHandler {
+	implements UploadFileEntryHandler {
 
 	@Override
+	public FileEntry upload(UploadPortletRequest uploadPortletRequest)
+		throws IOException, PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)uploadPortletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		long cpDefinitionId = ParamUtil.getLong(
+			uploadPortletRequest, "cpDefinitionId");
+
+		CPDefinition cpDefinition = cpDefinitionService.getCPDefinition(
+			cpDefinitionId);
+
+		String fileName = uploadPortletRequest.getFileName(_PARAMETER_NAME);
+		long size = uploadPortletRequest.getSize(_PARAMETER_NAME);
+
+		_validateFile(fileName, size);
+
+		String contentType = uploadPortletRequest.getContentType(
+			_PARAMETER_NAME);
+
+		try (InputStream inputStream =
+				uploadPortletRequest.getFileAsStream(_PARAMETER_NAME)) {
+
+			return addFileEntry(
+				cpDefinition.getCPDefinitionId(), fileName, contentType,
+				inputStream, themeDisplay);
+		}
+	}
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		_attachmentsConfiguration = ConfigurableUtil.createConfigurable(
+			AttachmentsConfiguration.class, properties);
+	}
+
 	protected FileEntry addFileEntry(
 			long cpDefinitionId, String fileName, String contentType,
 			InputStream inputStream, ThemeDisplay themeDisplay)
@@ -47,6 +104,9 @@ public class TempAttachmentsUploadFileEntryHandler
 			themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
 			_TEMP_FOLDER_NAME, uniqueFileName, inputStream, contentType);
 	}
+
+	@Reference
+	protected CPDefinitionService cpDefinitionService;
 
 	private boolean _exists(ThemeDisplay themeDisplay, String curFileName) {
 		try {
@@ -68,11 +128,40 @@ public class TempAttachmentsUploadFileEntryHandler
 		}
 	}
 
+	private void _validateFile(String fileName, long size)
+		throws PortalException {
+
+		if ((_attachmentsConfiguration.imageMaxSize() > 0) &&
+			(size > _attachmentsConfiguration.imageMaxSize())) {
+
+			throw new CPAttachmentFileEntrySizeException();
+		}
+
+		String extension = FileUtil.getExtension(fileName);
+
+		String[] imageExtensions = _attachmentsConfiguration.imageExtensions();
+
+		for (String imageExtension : imageExtensions) {
+			if (StringPool.STAR.equals(imageExtension) ||
+				imageExtension.equals(StringPool.PERIOD + extension)) {
+
+				return;
+			}
+		}
+
+		throw new CPAttachmentFileEntryNameException(
+			"Invalid image for file name " + fileName);
+	}
+
+	private static final String _PARAMETER_NAME = "imageSelectorFileName";
+
 	private static final String _TEMP_FOLDER_NAME =
 		TempAttachmentsUploadFileEntryHandler.class.getName();
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		TempAttachmentsUploadFileEntryHandler.class);
+
+	private volatile AttachmentsConfiguration _attachmentsConfiguration;
 
 	@Reference
 	private UniqueFileNameProvider _uniqueFileNameProvider;
