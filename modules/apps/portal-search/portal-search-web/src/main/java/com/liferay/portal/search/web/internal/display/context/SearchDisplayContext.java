9eb54dd879a03eaebe617afd884aad2971a8cd8e
Lily.Chi <lily.chi@liferay.com>
2018-06-04 17:50:16 +0800

    Revert "LPS-74551 portal-search-web: Classic Search Portlet: Set pre-filter through SearchContext groupIds"
    This reverts commit ac888a66d6348ecb3c1b28c135777a2c6f1b33f4.

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/display/context/SearchDisplayContext.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/display/context/SearchDisplayContext.java
@@ -18,13 +18,17 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
 import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchContextFactory;
 import com.liferay.portal.kernel.search.facet.Facet;
 import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcherManager;
+import com.liferay.portal.kernel.search.generic.BooleanClauseImpl;
+import com.liferay.portal.kernel.search.generic.TermQueryImpl;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Html;
@@ -40,14 +44,10 @@ import com.liferay.portal.search.web.internal.facet.SearchFacetTracker;
 import com.liferay.portal.search.web.internal.portlet.SearchPortletSearchResultPreferences;
 import com.liferay.portal.search.web.internal.search.request.SearchRequestImpl;
 import com.liferay.portal.search.web.internal.search.request.SearchResponseImpl;
-import com.liferay.portal.search.web.internal.util.SearchOptionalUtil;
 import com.liferay.portal.search.web.search.request.SearchSettings;
-
-import java.io.Serializable;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -116,8 +116,6 @@ public class SearchDisplayContext {
 			_renderRequest, getPortletURL(), null, emptyResultMessage);
 
 		SearchContext searchContext = SearchContextFactory.getInstance(request);
-
-		_resetScope(searchContext);
 
 		boolean luceneSyntax = isUseAdvancedSearchSyntax();
 
@@ -546,12 +544,15 @@ public class SearchDisplayContext {
 	}
 
 	protected void filterByThisSite(SearchSettings searchSettings) {
-		SearchOptionalUtil.copy(
-			this::getThisSiteGroupId,
-			groupId -> {
-				SearchContext searchContext = searchSettings.getSearchContext();
+		Optional<Long> groupIdOptional = getThisSiteGroupId();
 
-				searchContext.setGroupIds(new long[] {groupId});
+		groupIdOptional.ifPresent(
+			groupId -> {
+				searchSettings.addCondition(
+					new BooleanClauseImpl<>(
+						new TermQueryImpl(
+							Field.GROUP_ID, String.valueOf(groupId)),
+						BooleanClauseOccur.MUST));
 			});
 	}
 
@@ -594,14 +595,6 @@ public class SearchDisplayContext {
 		}
 
 		return Optional.of(searchScopeGroupId);
-	}
-
-	private void _resetScope(SearchContext searchContext) {
-		searchContext.setGroupIds(null);
-
-		Map<String, Serializable> attributes = searchContext.getAttributes();
-
-		attributes.remove("groupId", "0");
 	}
 
 	private Integer _collatedSpellCheckResultDisplayThreshold;
