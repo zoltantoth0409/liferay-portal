74d49278764b49052fd290c473c335b506b06199
Lance Ji <zheyi.ji@liferay.com>
2018-06-04 11:50:25 -0700

    Revert "LPS-80295 Import all url titles, not only default"
    This reverts commit d5c669c5471951fe2375e663784d566380e2bf3a.

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/exportimport/staged/model/repository/FriendlyURLEntryStagedModelRepository.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/exportimport/staged/model/repository/FriendlyURLEntryStagedModelRepository.java
@@ -55,8 +55,8 @@ public class FriendlyURLEntryStagedModelRepository
 
 		return _friendlyURLEntryLocalService.addFriendlyURLEntry(
 			friendlyURLEntry.getGroupId(), friendlyURLEntry.getClassNameId(),
-			friendlyURLEntry.getClassPK(),
-			friendlyURLEntry.getLanguageIdToUrlTitleMap(), serviceContext);
+			friendlyURLEntry.getClassPK(), friendlyURLEntry.getUrlTitle(),
+			serviceContext);
 	}
 
 	@Override
