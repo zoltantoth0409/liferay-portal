b373f324e23b0120a1508dea09ed1ec2a8d2b97b
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-06-04 10:47:25 +0200

    LPS-79988 Use correct URL when there's no display page for the article

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7657,7 +7657,8 @@ public class JournalArticleLocalServiceImpl
 	}
 
 	protected String getURLViewInContext(
-		JournalArticle article, ServiceContext serviceContext) {
+		JournalArticle article, String portletId,
+		ServiceContext serviceContext) {
 
 		LiferayPortletRequest liferayPortletRequest =
 			serviceContext.getLiferayPortletRequest();
@@ -7669,6 +7670,9 @@ public class JournalArticleLocalServiceImpl
 		String urlViewInContext = StringPool.BLANK;
 
 		try {
+			String defaultArticleURL = PortalUtil.getControlPanelFullURL(
+				article.getGroupId(), portletId, null);
+
 			AssetRendererFactory<JournalArticle> assetRendererFactory =
 				AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClass(
 					JournalArticle.class);
@@ -7678,7 +7682,7 @@ public class JournalArticleLocalServiceImpl
 					article, AssetRendererFactory.TYPE_LATEST_APPROVED);
 
 			urlViewInContext = assetRenderer.getURLViewInContext(
-				liferayPortletRequest, null, serviceContext.getCurrentURL());
+				liferayPortletRequest, null, defaultArticleURL);
 		}
 		catch (Exception e) {
 			_log.error(e, e);
@@ -7745,8 +7749,8 @@ public class JournalArticleLocalServiceImpl
 		String portletId = PortletProviderUtil.getPortletId(
 			JournalArticle.class.getName(), PortletProvider.Action.EDIT);
 
-		String articleURL = PortalUtil.getControlPanelFullURL(
-			article.getGroupId(), portletId, null);
+		String articleURL = getURLViewInContext(
+			article, portletId, serviceContext);
 
 		if (Validator.isNull(articleURL)) {
 			return;
@@ -7756,8 +7760,6 @@ public class JournalArticleLocalServiceImpl
 			getJournalGroupServiceConfiguration(article.getGroupId());
 
 		String articleTitle = article.getTitle(serviceContext.getLanguageId());
-
-		articleURL = getURLViewInContext(article, serviceContext);
 
 		if (action.equals("add") &&
 			journalGroupServiceConfiguration.emailArticleAddedEnabled()) {
