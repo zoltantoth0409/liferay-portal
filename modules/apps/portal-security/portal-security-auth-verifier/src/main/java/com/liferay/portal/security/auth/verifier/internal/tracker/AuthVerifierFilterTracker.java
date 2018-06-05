77f8f01a342ee85eb759e6e3687d94f8a79a4fe0
Carlos Sierra <carlos.sierra@liferay.com>
2018-06-05 15:57:59 +0200

    LPS-81770 SF

--- modules/apps/portal-security/portal-security-auth-verifier/src/main/java/com/liferay/portal/security/auth/verifier/internal/tracker/AuthVerifierFilterTracker.java
+++ modules/apps/portal-security/portal-security-auth-verifier/src/main/java/com/liferay/portal/security/auth/verifier/internal/tracker/AuthVerifierFilterTracker.java
@@ -50,13 +50,13 @@ public class AuthVerifierFilterTracker {
 	protected void activate(BundleContext bundleContext) {
 		_bundleContext = bundleContext;
 
-		String filter = StringBundler.concat(
+		String filterString = StringBundler.concat(
 			"(&(com.liferay.auth.verifier.filter.enabled=true)(",
 			HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME, "=*)",
 			"(objectClass=", ServletContextHelper.class.getName(), "))");
 
 		_serviceTracker = ServiceTrackerFactory.open(
-			bundleContext, filter,
+			bundleContext, filterString,
 			new ServletContextHelperServiceTrackerCustomizer());
 	}
 
