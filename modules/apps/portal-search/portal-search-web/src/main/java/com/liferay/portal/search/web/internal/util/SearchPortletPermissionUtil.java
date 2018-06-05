b6768a41ec1a16737cea8f1b92dab87b9cdc2362
André de Oliveira <andre.oliveira@liferay.com>
2018-06-05 00:19:23 -0700

    LPS-81854 portal-search-web: hide sensitive portlets from guest users
    https://issues.liferay.com/browse/LPS-81854
    https://issues.liferay.com/browse/LPS-80940
    
    Co-authored-by: Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
    Co-authored-by: weicao <wade.cao@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/util/SearchPortletPermissionUtil.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.util;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.permission.PortletPermission;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.search.web.internal.display.context.PortletRequestThemeDisplaySupplier;
+import com.liferay.portal.search.web.internal.display.context.ThemeDisplaySupplier;
+
+import javax.portlet.RenderRequest;
+
+/**
+ * @author André de Oliveira
+ */
+public class SearchPortletPermissionUtil {
+
+	public static boolean containsConfiguration(
+		PortletPermission portletPermission, RenderRequest renderRequest,
+		Portal portal) {
+
+		ThemeDisplaySupplier themeDisplaySupplier =
+			new PortletRequestThemeDisplaySupplier(renderRequest);
+
+		ThemeDisplay themeDisplay = themeDisplaySupplier.getThemeDisplay();
+
+		try {
+			return portletPermission.contains(
+				themeDisplay.getPermissionChecker(), themeDisplay.getPlid(),
+				portal.getPortletId(renderRequest), ActionKeys.CONFIGURATION);
+		}
+		catch (PortalException pe) {
+			_log.error(pe);
+
+			return false;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SearchPortletPermissionUtil.class);
+
+}
