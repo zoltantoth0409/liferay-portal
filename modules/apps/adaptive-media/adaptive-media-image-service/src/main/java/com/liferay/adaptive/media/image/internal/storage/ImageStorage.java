d8e5a6b9cb8344da0220f638bad4fed2872d3560
Roberto Díaz <roberto.diaz@liferay.com>
2018-06-05 18:26:22 +0200

    LPS-81743 Store migration doesn't migrate Adaptive Media generated files

--- modules/apps/adaptive-media/adaptive-media-image-service/src/main/java/com/liferay/adaptive/media/image/internal/storage/ImageStorage.java
+++ modules/apps/adaptive-media/adaptive-media-image-service/src/main/java/com/liferay/adaptive/media/image/internal/storage/ImageStorage.java
@@ -15,6 +15,7 @@
 package com.liferay.adaptive.media.image.internal.storage;
 
 import com.liferay.adaptive.media.exception.AMRuntimeException;
+import com.liferay.adaptive.media.image.internal.util.AMImageUtil;
 import com.liferay.document.library.kernel.store.DLStoreUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.CompanyConstants;
@@ -33,23 +34,25 @@ public class ImageStorage {
 	public void delete(FileVersion fileVersion, String configurationUuid) {
 		DLStoreUtil.deleteDirectory(
 			fileVersion.getCompanyId(), CompanyConstants.SYSTEM,
-			getFileVersionPath(fileVersion, configurationUuid));
+			AMImageUtil.getFileVersionPath(fileVersion, configurationUuid));
 	}
 
 	public void delete(long companyId, String configurationUuid) {
 		DLStoreUtil.deleteDirectory(
 			companyId, CompanyConstants.SYSTEM,
-			getConfigurationEntryPath(configurationUuid));
+			AMImageUtil.getConfigurationEntryPath(configurationUuid));
 	}
 
 	public InputStream getContentStream(
 		FileVersion fileVersion, String configurationUuid) {
 
 		try {
-			String fileVersionPath = getFileVersionPath(
+			String fileVersionPath = AMImageUtil.getFileVersionPath(
 				fileVersion, configurationUuid);
 
-			return getFileAsStream(fileVersion.getCompanyId(), fileVersionPath);
+			return DLStoreUtil.getFileAsStream(
+				fileVersion.getCompanyId(), CompanyConstants.SYSTEM,
+				fileVersionPath);
 		}
 		catch (PortalException pe) {
 			throw new AMRuntimeException.IOException(pe);
@@ -61,7 +64,7 @@ public class ImageStorage {
 		InputStream inputStream) {
 
 		try {
-			String fileVersionPath = getFileVersionPath(
+			String fileVersionPath = AMImageUtil.getFileVersionPath(
 				fileVersion, configurationUuid);
 
 			DLStoreUtil.addFile(
@@ -71,26 +74,6 @@ public class ImageStorage {
 		catch (PortalException pe) {
 			throw new AMRuntimeException.IOException(pe);
 		}
-	}
-
-	protected String getConfigurationEntryPath(String configurationUuid) {
-		return String.format("adaptive/%s", configurationUuid);
-	}
-
-	protected InputStream getFileAsStream(long companyId, String path)
-		throws PortalException {
-
-		return DLStoreUtil.getFileAsStream(
-			companyId, CompanyConstants.SYSTEM, path);
-	}
-
-	protected String getFileVersionPath(
-		FileVersion fileVersion, String configurationUuid) {
-
-		return String.format(
-			"adaptive/%s/%d/%d/%d/%d/", configurationUuid,
-			fileVersion.getGroupId(), fileVersion.getRepositoryId(),
-			fileVersion.getFileEntryId(), fileVersion.getFileVersionId());
 	}
 
 }
---
+++ modules/apps/adaptive-media/adaptive-media-image-service/src/main/java/com/liferay/adaptive/media/image/internal/util/AMImageUtil.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.adaptive.media.image.internal.util;
+
+import com.liferay.portal.kernel.repository.model.FileVersion;
+
+/**
+ * @author Roberto Díaz
+ */
+public class AMImageUtil {
+
+	public static String getConfigurationEntryPath(String configurationUuid) {
+		return String.format("adaptive/%s", configurationUuid);
+	}
+
+	public static String getFileVersionPath(
+		FileVersion fileVersion, String configurationUuid) {
+
+		return String.format(
+			"adaptive/%s/%d/%d/%d/%d/", configurationUuid,
+			fileVersion.getGroupId(), fileVersion.getRepositoryId(),
+			fileVersion.getFileEntryId(), fileVersion.getFileVersionId());
+	}
+
+}
