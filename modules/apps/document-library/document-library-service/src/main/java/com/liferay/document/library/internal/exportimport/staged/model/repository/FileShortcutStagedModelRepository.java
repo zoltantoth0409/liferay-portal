f6ba8e5a0b401569e93bf0d6c149986b651d0dc4
akosthurzo <akos.thurzo@liferay.com>
2018-06-06 13:31:51 +0200

    LPS-82015 Register DL SMRepos to DL for now since it doesn't support multiple model.class.names yet

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/FileShortcutStagedModelRepository.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/FileShortcutStagedModelRepository.java
@@ -32,7 +32,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	immediate = true,
-	property = "model.class.name=com.liferay.portal.kernel.repository.model.FileShortcut",
+	property = "model.class.name=com.liferay.document.library.kernel.model.DLFileShortcut",
 	service = StagedModelRepository.class
 )
 public class FileShortcutStagedModelRepository
--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/FolderStagedModelRepository.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/staged/model/repository/FolderStagedModelRepository.java
@@ -32,7 +32,7 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	immediate = true,
-	property = "model.class.name=com.liferay.portal.kernel.repository.model.Folder",
+	property = "model.class.name=com.liferay.document.library.kernel.model.DLFolder",
 	service = StagedModelRepository.class
 )
 public class FolderStagedModelRepository
