7eaaef14e426fba19e59b332dcd16c15e6252a97
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-06-06 08:30:28 +0200

    COMMERCE-397 added shipping option to checkout test

--- modules/dxp/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/test/CommerceCheckoutTest.java
+++ modules/dxp/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/test/CommerceCheckoutTest.java
@@ -16,27 +16,22 @@ package com.liferay.commerce.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.commerce.internal.test.util.CommerceTestUtil;
-import com.liferay.commerce.model.CommerceAddress;
 import com.liferay.commerce.model.CommerceOrder;
-import com.liferay.commerce.model.CommercePaymentMethod;
-import com.liferay.commerce.model.CommerceShippingMethod;
-import com.liferay.commerce.model.CommerceWarehouse;
+import com.liferay.commerce.model.CommerceOrderItem;
 import com.liferay.commerce.product.model.CPInstance;
-import com.liferay.commerce.product.test.util.CPTestUtil;
 import com.liferay.commerce.service.CommerceOrderLocalService;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerTestRule;
 
 import java.math.BigDecimal;
+
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -59,7 +54,6 @@ public class CommerceCheckoutTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
-		_user = UserTestUtil.addUser();
 	}
 
 	@Test
@@ -67,54 +61,45 @@ public class CommerceCheckoutTest {
 		CommerceOrder commerceOrder = CommerceTestUtil.addUserCommerceOrder(
 			_group.getGroupId());
 
-		CPInstance cpInstance = CPTestUtil.addCPInstance(_group.getGroupId());
-
-		BigDecimal price = BigDecimal.valueOf(RandomTestUtil.randomDouble());
-
-		cpInstance.setPrice(price);
-
-		CommerceWarehouse commerceWarehouse =
-			CommerceTestUtil.addCommerceWarehouse(_group.getGroupId());
-
-		CommerceTestUtil.addCommerceWarehouseItem(
-			commerceWarehouse, cpInstance.getCPInstanceId(), 10);
-
-		CommerceTestUtil.addCommerceOrderItem(
-			commerceOrder.getCommerceOrderId(), cpInstance.getCPInstanceId(),
-			4);
-
-		CommerceAddress billingCommerceAddress =
-			CommerceTestUtil.addUserCommerceAddress(
-				_group.getGroupId(), _user.getUserId());
-
-		CommerceAddress shippingCommerceAddress =
-			CommerceTestUtil.addUserCommerceAddress(
-				_group.getGroupId(), _user.getUserId());
-
-		commerceOrder.setBillingAddressId(
-			billingCommerceAddress.getCommerceAddressId());
-
-		commerceOrder.setShippingAddressId(
-			shippingCommerceAddress.getCommerceAddressId());
-
-		CommercePaymentMethod commercePaymentMethod =
-			CommerceTestUtil.addCommercePaymentMethod(_group.getGroupId());
-
-		commerceOrder.setCommercePaymentMethodId(
-			commercePaymentMethod.getCommercePaymentMethodId());
-
-		CommerceShippingMethod commerceShippingMethod =
-			CommerceTestUtil.addCommerceShippingMethod(_group.getGroupId());
-
-		commerceOrder.setCommerceShippingMethodId(
-			commerceShippingMethod.getCommerceShippingMethodId());
-
-		_commerceOrderLocalService.updateCommerceOrder(commerceOrder);
+		CommerceTestUtil.addCheckoutDetailsToUserOrder(
+			commerceOrder, commerceOrder.getUserId());
 
 		commerceOrder = CommerceTestUtil.checkoutOrder(commerceOrder);
 
 		Assert.assertEquals(
 			WorkflowConstants.STATUS_APPROVED, commerceOrder.getStatus());
+
+		List<CommerceOrderItem> commerceOrderItems =
+			commerceOrder.getCommerceOrderItems();
+
+		BigDecimal expectedSubTotal = BigDecimal.ZERO;
+
+		for (CommerceOrderItem commerceOrderItem : commerceOrderItems) {
+			CPInstance cpInstance = commerceOrderItem.getCPInstance();
+
+			BigDecimal price = cpInstance.getPrice();
+
+			int quantity = commerceOrderItem.getQuantity();
+
+			BigDecimal totalItemPrice = price.multiply(
+				BigDecimal.valueOf(quantity));
+
+			expectedSubTotal = expectedSubTotal.add(totalItemPrice);
+		}
+
+		BigDecimal actualSubTotal = commerceOrder.getSubtotal();
+
+		Assert.assertEquals(
+			expectedSubTotal.doubleValue(), actualSubTotal.doubleValue(),
+			0.0001);
+
+		BigDecimal expectedTotal = expectedSubTotal.add(
+			commerceOrder.getShippingPrice());
+
+		BigDecimal actualTotal = commerceOrder.getTotal();
+
+		Assert.assertEquals(
+			expectedTotal.doubleValue(), actualTotal.doubleValue(), 0.0001);
 	}
 
 	@Inject
@@ -122,8 +107,5 @@ public class CommerceCheckoutTest {
 
 	@DeleteAfterTestRun
 	private Group _group;
-
-	@DeleteAfterTestRun
-	private User _user;
 
 }
