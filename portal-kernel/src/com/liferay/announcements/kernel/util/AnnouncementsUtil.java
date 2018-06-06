21074048dab529ad7a9f7d83021be38192625820
Lianne Louie <lianne.louie@liferay.com>
2018-06-06 14:55:24 -0700

    LPS-82061 - fix propositional logic taken from AnnouncementsEntryServiceImpl in faf58c32e568f13635ab782640f02a83b5bcbc28

--- portal-kernel/src/com/liferay/announcements/kernel/util/AnnouncementsUtil.java
+++ portal-kernel/src/com/liferay/announcements/kernel/util/AnnouncementsUtil.java
@@ -224,7 +224,7 @@ public class AnnouncementsUtil {
 
 				if (GroupPermissionUtil.contains(
 						themeDisplay.getPermissionChecker(), team.getGroupId(),
-						ActionKeys.MANAGE_ANNOUNCEMENTS) ||
+						ActionKeys.MANAGE_ANNOUNCEMENTS) &&
 					RolePermissionUtil.contains(
 						themeDisplay.getPermissionChecker(),
 						themeDisplay.getScopeGroupId(), role.getRoleId(),
