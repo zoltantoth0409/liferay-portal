96ff6581b732e1a4fb53a464df7b18a44dd941d7
Hugo Huijser <hugo.huijser@liferay.com>
2018-06-06 13:50:40 +0800

    LPS-81549 Fix logic. For milliseconds = 0, it returns "0 Secondss"

--- portal-kernel/src/com/liferay/portal/kernel/util/Time.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/Time.java
@@ -91,7 +91,7 @@ public class Time {
 
 		if (approximate) {
 			if (milliseconds <= 0) {
-				s = "0 Seconds";
+				s = "0 Second";
 			}
 			else if (milliseconds < MINUTE) {
 				x = (int)(milliseconds / SECOND);
