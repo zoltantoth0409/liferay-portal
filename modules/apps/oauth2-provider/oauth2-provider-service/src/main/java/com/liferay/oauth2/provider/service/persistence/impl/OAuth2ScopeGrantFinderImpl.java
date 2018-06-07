26748a13c19c7a7c805ab889a5251404ae7fa5b7
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-06-07 15:08:24 +0200

    OAUTH2-144 Use Type.TEXT where CLOB is unsupported

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/persistence/impl/OAuth2ScopeGrantFinderImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/persistence/impl/OAuth2ScopeGrantFinderImpl.java
@@ -18,6 +18,8 @@ import com.liferay.oauth2.provider.model.OAuth2ScopeGrant;
 import com.liferay.oauth2.provider.model.impl.OAuth2ScopeGrantImpl;
 import com.liferay.oauth2.provider.service.persistence.OAuth2ScopeGrantFinder;
 import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.orm.QueryPos;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.SQLQuery;
@@ -56,7 +58,13 @@ public class OAuth2ScopeGrantFinderImpl
 			QueryPos qPos = QueryPos.getInstance(q);
 
 			q.addEntity("OAuth2ScopeGrant", OAuth2ScopeGrantImpl.class);
-			q.addScalar("accessTokenContent", Type.MATERIALIZED_CLOB);
+
+			if (_supportsClob) {
+				q.addScalar("accessTokenContent", Type.MATERIALIZED_CLOB);
+			}
+			else {
+				q.addScalar("accessTokenContent", Type.TEXT);
+			}
 
 			qPos.add(companyId);
 			qPos.add(applicationName);
@@ -86,7 +94,15 @@ public class OAuth2ScopeGrantFinderImpl
 		}
 	}
 
+	protected void afterPropertiesSet() {
+		DB db = DBManagerUtil.getDB();
+
+		_supportsClob = db.isSupportsClob();
+	}
+
 	@ServiceReference(type = CustomSQL.class)
 	private CustomSQL _customSQL;
+
+	private boolean _supportsClob = true;
 
 }
