3ec8b093ed85f3c52807290555e6695c7828eb6a
Peter Shin <peter.shin@liferay.com>
2018-06-08 10:07:40 -0500

    LPS-77875 Update repository url

--- portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
+++ portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
@@ -1355,8 +1355,7 @@ public class ModulesStructureTest {
 		"systemProp.repository.private.username";
 
 	private static final String _REPOSITORY_URL =
-		"https://cdn.lfrs.sl/repository.liferay.com/nexus/content/groups" +
-			"/public";
+		"https://repository-cdn.liferay.com/nexus/content/groups/public";
 
 	private static final String _SOURCE_FORMATTER_IGNORE_FILE_NAME =
 		"source_formatter.ignore";
