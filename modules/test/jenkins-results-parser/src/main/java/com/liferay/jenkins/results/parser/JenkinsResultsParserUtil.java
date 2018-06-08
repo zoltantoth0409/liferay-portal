389b899f449a225a6ba00a04f505284992273e7d
pyoo47 <peter.yoo@liferay.com>
2018-06-08 12:48:49 -0700

    LRQA-41493 Rename merge() -> join() add delimiter parameter

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
@@ -1118,15 +1118,15 @@ public class JenkinsResultsParserUtil {
 		return false;
 	}
 
-	public static String merge(String... strings) {
+	public static String join(String delimiter, String... strings) {
 		StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < strings.length; i++) {
-			sb.append(strings[i]);
-
-			if (i < (strings.length - 1)) {
-				sb.append(",");
+		for (String string : strings) {
+			if (sb.length() > 0) {
+				sb.append(delimiter);
 			}
+
+			sb.append(string);
 		}
 
 		return sb.toString();
