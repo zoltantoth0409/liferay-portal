5c475a358492c2148631e2f14ab7375c0afcc9fa
yichenroy <yi-chen.tsai@liferay.com>
2018-06-08 15:15:54 -0700

    LRQA-41058 Add test to echo table using xml macro and poshi script macro

--- modules/test/poshi-runner/poshi-runner/src/test/java/com/liferay/poshi/runner/var/type/TableTest.java
+++ modules/test/poshi-runner/poshi-runner/src/test/java/com/liferay/poshi/runner/var/type/TableTest.java
@@ -14,7 +14,16 @@
 
 package com.liferay.poshi.runner.var.type;
 
+import com.liferay.poshi.runner.PoshiRunner;
+import com.liferay.poshi.runner.PoshiRunnerContext;
+import com.liferay.poshi.runner.selenium.SeleniumUtil;
 import com.liferay.poshi.runner.util.TableUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -22,6 +31,8 @@ import java.util.List;
 import java.util.Map;
 
 import junit.framework.TestCase;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -32,9 +43,48 @@ import org.junit.Test;
  */
 public class TableTest extends TestCase {
 
+	public TableTest() throws Exception {
+	}
+
 	@Before
 	public void setUp() throws Exception {
 		_rawDataList = TableUtil.getRawDataListFromString(_RAW_DATA_STRING);
+
+		_testBaseDir = new File(_TEST_BASE_DIR_NAME);
+
+		if (!_testBaseDir.exists()) {
+			throw new RuntimeException(
+				"Test directory does not exist: " + _TEST_BASE_DIR_NAME);
+		}
+
+		String[] poshiFileNames = ArrayUtils.addAll(
+			PoshiRunnerContext.POSHI_SUPPORT_FILE_INCLUDES,
+			PoshiRunnerContext.POSHI_TEST_FILE_INCLUDES);
+
+		PoshiRunnerContext.readFiles(poshiFileNames, _TEST_BASE_DIR_NAME);
+	}
+
+	@Test
+	public void testEchoTablesPoshiScript() throws Exception {
+		PoshiRunner pr = new PoshiRunner(
+			"LocalFile.TableTest#echoTablePoshiScript");
+
+		pr.setUp();
+
+		pr.test();
+
+		SeleniumUtil.stopSelenium();
+	}
+
+	@Test
+	public void testEchoTablesXML() throws Exception {
+		PoshiRunner pr = new PoshiRunner("LocalFile.TableTest#echoTableXML");
+
+		pr.setUp();
+
+		pr.test();
+
+		SeleniumUtil.stopSelenium();
 	}
 
 	@Test
@@ -124,9 +174,21 @@ public class TableTest extends TestCase {
 		Assert.assertEquals(expected, actual);
 	}
 
+	protected String read(File file) throws IOException {
+		return new String(Files.readAllBytes(Paths.get(file.toURI())));
+	}
+
+	protected String read(File dir, String fileName) throws IOException {
+		return read(new File(dir, fileName));
+	}
+
 	private static final String _RAW_DATA_STRING =
 		"| 1 | 2 |\n| 3 | 4 |\n| 5 | 6 |\n| 7 | 8 |";
 
+	private static final String _TEST_BASE_DIR_NAME =
+		"src/test/resources/com/liferay/poshi/runner/dependencies/var/type/";
+
 	private List<List<String>> _rawDataList;
+	private File _testBaseDir;
 
 }
