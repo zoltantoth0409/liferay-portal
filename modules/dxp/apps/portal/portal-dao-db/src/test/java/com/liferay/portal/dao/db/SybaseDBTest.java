ea3bc47a9cb7fa143417dd268aede501f9763bca
jpince <jason.pince@liferay.com>
2018-06-08 15:11:47 -0700

    Revert "OAUTH2-144 Temporarily ignore test"
    This reverts commit 826fd50862ef3e1398cbeb77c4c36ef245689571.

--- modules/dxp/apps/portal/portal-dao-db/src/test/java/com/liferay/portal/dao/db/SybaseDBTest.java
+++ modules/dxp/apps/portal/portal-dao-db/src/test/java/com/liferay/portal/dao/db/SybaseDBTest.java
@@ -19,13 +19,11 @@ import com.liferay.portal.kernel.dao.db.DB;
 import java.io.IOException;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * @author Miguel Pastor
  */
-@Ignore
 public class SybaseDBTest extends BaseDBTestCase {
 
 	@Test
