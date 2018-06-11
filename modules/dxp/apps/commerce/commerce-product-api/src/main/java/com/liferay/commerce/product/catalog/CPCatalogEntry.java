b0b50d17ca6c08bec2c5b0bcfdcd1ef8dd6d69a1
Marco Leo <marco.leo@liferay.com>
2018-06-11 20:28:58 +0200

    COMMERCE-468 Add Catalog Entry will be a pojo to rapresent the product in catalog browsing

---
+++ modules/dxp/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/catalog/CPCatalogEntry.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.catalog;
+
+/**
+ * @author Marco Leo
+ */
+public class CPCatalogEntry {
+
+	public long getCPDefinitionId() {
+		return _cpDefinitionId;
+	}
+
+	public String getDefaultImageFileUrl() {
+		return _defaultImageFileUrl;
+	}
+
+	public String getDescription() {
+		return _description;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String getShortDescription() {
+		return _shortDescription;
+	}
+
+	public String getSku() {
+		return _sku;
+	}
+
+	public String getUrl() {
+		return _url;
+	}
+
+	public void setCPDefinitionId(long cpDefinitionId) {
+		_cpDefinitionId = cpDefinitionId;
+	}
+
+	public void setDefaultImageFileUrl(String defaultImageFileUrl) {
+		_defaultImageFileUrl = defaultImageFileUrl;
+	}
+
+	public void setDescription(String description) {
+		_description = description;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		_shortDescription = shortDescription;
+	}
+
+	public void setSku(String sku) {
+		_sku = sku;
+	}
+
+	public void setUrl(String url) {
+		_url = url;
+	}
+
+	private long _cpDefinitionId;
+	private String _defaultImageFileUrl;
+	private String _description;
+	private String _name;
+	private String _shortDescription;
+	private String _sku;
+	private String _url;
+
+}
