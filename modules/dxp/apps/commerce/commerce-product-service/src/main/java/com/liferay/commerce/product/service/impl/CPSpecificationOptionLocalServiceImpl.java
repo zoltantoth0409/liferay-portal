56fbbf53e19d8382b18817dc0b2c96fb18820194
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-06-11 16:55:34 -0700

    COMMERCE-427 Fix product specification option permissions

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPSpecificationOptionLocalServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPSpecificationOptionLocalServiceImpl.java
@@ -40,6 +40,7 @@ import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.Serializable;
@@ -153,6 +154,20 @@ public class CPSpecificationOptionLocalServiceImpl
 		long groupId, String key) {
 
 		return cpSpecificationOptionPersistence.fetchByG_K(groupId, key);
+	}
+
+	@Override
+	public List<CPSpecificationOption> getCPSpecificationOptions(
+		long groupId, int start, int end,
+		OrderByComparator<CPSpecificationOption> orderByComparator) {
+
+		return cpSpecificationOptionPersistence.findByGroupId(
+			groupId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCPSpecificationOptionsCount(long groupId) {
+		return cpSpecificationOptionPersistence.countByGroupId(groupId);
 	}
 
 	@Override
--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPSpecificationOptionServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPSpecificationOptionServiceImpl.java
@@ -16,16 +16,11 @@ package com.liferay.commerce.product.service.impl;
 
 import com.liferay.commerce.product.constants.CPActionKeys;
 import com.liferay.commerce.product.constants.CPConstants;
-import com.liferay.commerce.product.constants.CPOptionCategoryConstants;
-import com.liferay.commerce.product.model.CPOptionCategory;
 import com.liferay.commerce.product.model.CPSpecificationOption;
 import com.liferay.commerce.product.service.base.CPSpecificationOptionServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.search.Sort;
-import com.liferay.portal.kernel.security.permission.ActionKeys;
-import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
-import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -52,13 +47,6 @@ public class CPSpecificationOptionServiceImpl
 			getPermissionChecker(), serviceContext.getScopeGroupId(),
 			CPActionKeys.ADD_COMMERCE_PRODUCT_SPECIFICATION_OPTION);
 
-		if (cpOptionCategoryId !=
-				CPOptionCategoryConstants.DEFAULT_CP_OPTION_CATEGORY_ID) {
-
-			_cpOptionCategoryModelResourcePermission.check(
-				getPermissionChecker(), cpOptionCategoryId, ActionKeys.VIEW);
-		}
-
 		return cpSpecificationOptionLocalService.addCPSpecificationOption(
 			cpOptionCategoryId, titleMap, descriptionMap, facetable, key,
 			serviceContext);
@@ -68,20 +56,22 @@ public class CPSpecificationOptionServiceImpl
 	public void deleteCPSpecificationOption(long cpSpecificationOptionId)
 		throws PortalException {
 
-		_cpSpecificationOptionModelResourcePermission.check(
-			getPermissionChecker(), cpSpecificationOptionId, ActionKeys.DELETE);
+		CPSpecificationOption cpSpecificationOption =
+			cpSpecificationOptionLocalService.getCPSpecificationOption(
+				cpSpecificationOptionId);
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), cpSpecificationOption.getGroupId(),
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_SPECIFICATION_OPTIONS);
 
 		cpSpecificationOptionLocalService.deleteCPSpecificationOption(
-			cpSpecificationOptionId);
+			cpSpecificationOption);
 	}
 
 	@Override
 	public CPSpecificationOption fetchCPSpecificationOption(
 			long cpSpecificationOptionId)
 		throws PortalException {
-
-		_cpSpecificationOptionModelResourcePermission.check(
-			getPermissionChecker(), cpSpecificationOptionId, ActionKeys.VIEW);
 
 		return cpSpecificationOptionLocalService.fetchCPSpecificationOption(
 			cpSpecificationOptionId);
@@ -92,25 +82,14 @@ public class CPSpecificationOptionServiceImpl
 			long groupId, String key)
 		throws PortalException {
 
-		CPSpecificationOption cpSpecificationOption =
-			cpSpecificationOptionLocalService.fetchCPSpecificationOption(
-				groupId, key);
-
-		if (cpSpecificationOption != null) {
-			_cpSpecificationOptionModelResourcePermission.check(
-				getPermissionChecker(), cpSpecificationOption, ActionKeys.VIEW);
-		}
-
-		return cpSpecificationOption;
+		return cpSpecificationOptionLocalService.fetchCPSpecificationOption(
+			groupId, key);
 	}
 
 	@Override
 	public CPSpecificationOption getCPSpecificationOption(
 			long cpSpecificationOptionId)
 		throws PortalException {
-
-		_cpSpecificationOptionModelResourcePermission.check(
-			getPermissionChecker(), cpSpecificationOptionId, ActionKeys.VIEW);
 
 		return cpSpecificationOptionLocalService.getCPSpecificationOption(
 			cpSpecificationOptionId);
@@ -122,13 +101,14 @@ public class CPSpecificationOptionServiceImpl
 			OrderByComparator<CPSpecificationOption> orderByComparator)
 		throws PortalException {
 
-		return cpSpecificationOptionPersistence.filterFindByGroupId(
+		return cpSpecificationOptionLocalService.getCPSpecificationOptions(
 			groupId, start, end, orderByComparator);
 	}
 
 	@Override
 	public int getCPSpecificationOptionsCount(long groupId) {
-		return cpSpecificationOptionPersistence.filterCountByGroupId(groupId);
+		return cpSpecificationOptionLocalService.getCPSpecificationOptionsCount(
+			groupId);
 	}
 
 	@Override
@@ -149,33 +129,20 @@ public class CPSpecificationOptionServiceImpl
 			boolean facetable, String key, ServiceContext serviceContext)
 		throws PortalException {
 
-		_cpSpecificationOptionModelResourcePermission.check(
-			getPermissionChecker(), cpSpecificationOptionId, ActionKeys.UPDATE);
+		CPSpecificationOption cpSpecificationOption =
+			cpSpecificationOptionLocalService.getCPSpecificationOption(
+				cpSpecificationOptionId);
 
-		if (cpOptionCategoryId !=
-				CPOptionCategoryConstants.DEFAULT_CP_OPTION_CATEGORY_ID) {
-
-			_cpOptionCategoryModelResourcePermission.check(
-				getPermissionChecker(), cpOptionCategoryId, ActionKeys.VIEW);
-		}
+		_portletResourcePermission.check(
+			getPermissionChecker(), cpSpecificationOption.getGroupId(),
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_SPECIFICATION_OPTIONS);
 
 		return cpSpecificationOptionLocalService.updateCPSpecificationOption(
-			cpSpecificationOptionId, cpOptionCategoryId, titleMap,
-			descriptionMap, facetable, key, serviceContext);
+			cpSpecificationOption.getCPSpecificationOptionId(),
+			cpOptionCategoryId, titleMap, descriptionMap, facetable, key,
+			serviceContext);
 	}
 
-	private static volatile ModelResourcePermission<CPOptionCategory>
-		_cpOptionCategoryModelResourcePermission =
-			ModelResourcePermissionFactory.getInstance(
-				CPSpecificationOptionServiceImpl.class,
-				"_cpOptionCategoryModelResourcePermission",
-				CPOptionCategory.class);
-	private static volatile ModelResourcePermission<CPSpecificationOption>
-		_cpSpecificationOptionModelResourcePermission =
-			ModelResourcePermissionFactory.getInstance(
-				CPSpecificationOptionServiceImpl.class,
-				"_cpSpecificationOptionModelResourcePermission",
-				CPSpecificationOption.class);
 	private static volatile PortletResourcePermission
 		_portletResourcePermission =
 			PortletResourcePermissionFactory.getInstance(
