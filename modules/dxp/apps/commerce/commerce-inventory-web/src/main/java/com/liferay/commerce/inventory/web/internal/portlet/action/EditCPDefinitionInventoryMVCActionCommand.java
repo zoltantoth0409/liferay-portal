61064bfc748e667c2ccd24cdf29e1ab48bd8f1ea
Andrea Di Giorgi <adigiorgi@outlook.com>
2018-06-11 15:48:10 -0700

    COMMERCE-X Auto SF

--- modules/dxp/apps/commerce/commerce-inventory-web/src/main/java/com/liferay/commerce/inventory/web/internal/portlet/action/EditCPDefinitionInventoryMVCActionCommand.java
+++ modules/dxp/apps/commerce/commerce-inventory-web/src/main/java/com/liferay/commerce/inventory/web/internal/portlet/action/EditCPDefinitionInventoryMVCActionCommand.java
@@ -19,7 +19,6 @@ import com.liferay.commerce.model.CPDefinitionInventory;
 import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.commerce.service.CPDAvailabilityEstimateService;
 import com.liferay.commerce.service.CPDefinitionInventoryService;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
