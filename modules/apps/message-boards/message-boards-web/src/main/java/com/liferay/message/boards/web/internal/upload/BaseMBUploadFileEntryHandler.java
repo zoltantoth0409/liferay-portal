3b63dd78d72ea1f07e6536430d0e66fbd83bf1a2
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-06-11 18:12:55 +0200

    LPS-82079 Inheriting references

--- modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/upload/BaseMBUploadFileEntryHandler.java
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/upload/BaseMBUploadFileEntryHandler.java
@@ -28,6 +28,8 @@ import com.liferay.upload.UploadFileEntryHandler;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Adolfo Pérez
  */
@@ -47,8 +49,6 @@ public abstract class BaseMBUploadFileEntryHandler
 		try (InputStream inputStream = getFileAsStream(uploadPortletRequest)) {
 			String tempFileName = TempFileEntryUtil.getTempFileName(
 				getFileName(uploadPortletRequest));
-
-			MBMessageService mbMessageService = getMBMessageService();
 
 			return mbMessageService.addTempAttachment(
 				themeDisplay.getScopeGroupId(), categoryId,
@@ -72,8 +72,9 @@ public abstract class BaseMBUploadFileEntryHandler
 		return uploadPortletRequest.getFileName(getParameterName());
 	}
 
-	protected abstract MBMessageService getMBMessageService();
-
 	protected abstract String getParameterName();
+
+	@Reference
+	protected MBMessageService mbMessageService;
 
 }
