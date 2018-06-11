f4c9cd25b5cb592d879fe8d00ec0a5febf3790b5
Brian Chan <brian.chan@liferay.com>
2018-06-11 15:46:00 -0700

    LPS-81651 SF

--- util-taglib/src/com/liferay/taglib/portlet/DefineObjectsTag3.java
+++ util-taglib/src/com/liferay/taglib/portlet/DefineObjectsTag3.java
@@ -20,6 +20,7 @@ import com.liferay.taglib.aui.AUIUtil;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 import javax.portlet.ActionRequest;
