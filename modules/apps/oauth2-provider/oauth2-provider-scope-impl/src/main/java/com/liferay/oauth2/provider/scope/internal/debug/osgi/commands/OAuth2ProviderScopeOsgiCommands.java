644865502141eb2ada5b8969e6aab0b01561c0a6
Carlos Sierra <carlos.sierra@liferay.com>
2018-06-12 15:33:27 +0200

    OAUTH2-218 Gogo command to list scopes

---
+++ modules/apps/oauth2-provider/oauth2-provider-scope-impl/src/main/java/com/liferay/oauth2/provider/scope/internal/debug/osgi/commands/OAuth2ProviderScopeOsgiCommands.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.scope.internal.debug.osgi.commands;
+
+import com.liferay.oauth2.provider.scope.liferay.LiferayOAuth2Scope;
+import com.liferay.oauth2.provider.scope.liferay.ScopeLocator;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(
+	immediate = true,
+	property = {
+		"osgi.command.function=listScopes", "osgi.command.scope=oauth2"
+	},
+	service = OAuth2ProviderScopeOsgiCommands.class
+)
+public class OAuth2ProviderScopeOsgiCommands {
+
+	public void listScopes() {
+		listScopes(_portal.getDefaultCompanyId());
+	}
+
+	public void listScopes(long companyId) {
+		Collection<String> scopeAliases = ListUtil.sort(
+			new ArrayList<>(_scopeLocator.getScopeAliases(companyId)));
+
+		for (String alias : scopeAliases) {
+			System.out.println();
+
+			System.out.println(alias);
+
+			Collection<LiferayOAuth2Scope> liferayOAuth2Scopes = ListUtil.sort(
+				new ArrayList<>(
+					_scopeLocator.getLiferayOAuth2Scopes(companyId, alias)),
+				Comparator.comparing(LiferayOAuth2Scope::getScope));
+
+			for (LiferayOAuth2Scope liferayOAuth2Scope : liferayOAuth2Scopes) {
+				System.out.println(
+					StringBundler.concat(
+						"    ", liferayOAuth2Scope.getScope(), " (",
+						liferayOAuth2Scope.getApplicationName(), ")"));
+			}
+		}
+
+		System.out.println();
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private ScopeLocator _scopeLocator;
+
+}
