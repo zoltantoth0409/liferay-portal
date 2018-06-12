d445e26f39030652a212181a6547cbf47222919e
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-06-12 12:32:40 +0200

    LPS-82079 Using the minimum file size available
    Before this the value for documents and media would always override that
    from upload servlet request.

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLValidatorImpl.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLValidatorImpl.java
@@ -72,13 +72,19 @@ public final class DLValidatorImpl implements DLValidator {
 
 	@Override
 	public long getMaxAllowableSize() {
-		long fileMaxSize = _dlConfiguration.fileMaxSize();
+		long dlFileMaxSize = _dlConfiguration.fileMaxSize();
+		long uploadServletRequestFileMaxSize =
+			_uploadServletRequestConfigurationHelper.getMaxSize();
 
-		if (fileMaxSize == 0) {
-			fileMaxSize = _uploadServletRequestConfigurationHelper.getMaxSize();
+		if (dlFileMaxSize == 0) {
+			return uploadServletRequestFileMaxSize;
 		}
 
-		return fileMaxSize;
+		if (uploadServletRequestFileMaxSize == 0) {
+			return dlFileMaxSize;
+		}
+
+		return Math.min(dlFileMaxSize, uploadServletRequestFileMaxSize);
 	}
 
 	@Override
