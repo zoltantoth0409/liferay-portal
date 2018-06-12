08353d5d5a845b1001b79cdcc30c355d7e6e5fc9
Pedro Queiroz <pedro.queiroz@liferay.com>
2018-06-11 21:48:37 -0300

    LPS-82171 Add Unit Tests

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-test/src/test/java/com/liferay/dynamic/data/lists/internal/upgrade/v1_2_0/UpgradeDDLRecordSetTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.internal.upgrade.v1_2_0;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Pedro Queiroz
+ */
+public class UpgradeDDLRecordSetTest {
+
+	@Before
+	public void setUp() {
+		setUpUpgradeDDLRecordSetSettings();
+	}
+
+	@Test
+	public void testActionIdsConversion1() {
+		Assert.assertEquals(31L, _upgradeDDLRecordSet.getNewActionIds(31L));
+	}
+
+	@Test
+	public void testActionIdsConversion2() {
+		Assert.assertEquals(8L, _upgradeDDLRecordSet.getNewActionIds(16L));
+	}
+
+	@Test
+	public void testActionIdsConversion3() {
+		Assert.assertEquals(16L, _upgradeDDLRecordSet.getNewActionIds(8L));
+	}
+
+	@Test
+	public void testActionIdsConversion4() {
+		Assert.assertEquals(15L, _upgradeDDLRecordSet.getNewActionIds(23L));
+	}
+
+	protected void setUpUpgradeDDLRecordSetSettings() {
+		_upgradeDDLRecordSet = new UpgradeDDLRecordSet(
+			null, null, null, null, null);
+	}
+
+	private UpgradeDDLRecordSet _upgradeDDLRecordSet;
+
+}
