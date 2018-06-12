369ceda0cebf58bd9bf5a7d120f2f4e9fe398428
shuyangzhou <shuyang.zhou@liferay.com>
2018-06-12 14:24:52 -0700

    LPS-79940 SF

--- modules/apps/product-navigation/product-navigation-control-menu-api/src/main/java/com/liferay/product/navigation/control/menu/util/ProductNavigationControlMenuCategoryRegistry.java
+++ modules/apps/product-navigation/product-navigation-control-menu-api/src/main/java/com/liferay/product/navigation/control/menu/util/ProductNavigationControlMenuCategoryRegistry.java
@@ -32,6 +32,6 @@ public interface ProductNavigationControlMenuCategoryRegistry {
 	public List<ProductNavigationControlMenuCategory>
 		getProductNavigationControlMenuCategories(
 			String productNavigationControlMenuCategoryKey,
-			final HttpServletRequest request);
+			HttpServletRequest request);
 
 }
--- modules/apps/product-navigation/product-navigation-control-menu-api/src/main/java/com/liferay/product/navigation/control/menu/util/ProductNavigationControlMenuEntryRegistry.java
+++ modules/apps/product-navigation/product-navigation-control-menu-api/src/main/java/com/liferay/product/navigation/control/menu/util/ProductNavigationControlMenuEntryRegistry.java
@@ -35,6 +35,6 @@ public interface ProductNavigationControlMenuEntryRegistry {
 		getProductNavigationControlMenuEntries(
 			ProductNavigationControlMenuCategory
 				productNavigationControlMenuCategory,
-			final HttpServletRequest request);
+			HttpServletRequest request);
 
 }
