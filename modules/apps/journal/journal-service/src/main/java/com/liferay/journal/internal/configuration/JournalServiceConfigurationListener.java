16979bb77ac659ccde4349bdba3f3f1af614e744
Lance Ji <zheyi.ji@liferay.com>
2018-06-12 10:20:59 -0700

    LPS-80213 Move impl of ConfigurationModelListener in journal to service bundle

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/configuration/JournalServiceConfigurationListener.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/configuration/JournalServiceConfigurationListener.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.journal.configuration;
+package com.liferay.journal.internal.configuration;
 
 import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
 import com.liferay.portal.servlet.filters.cache.CacheUtil;
