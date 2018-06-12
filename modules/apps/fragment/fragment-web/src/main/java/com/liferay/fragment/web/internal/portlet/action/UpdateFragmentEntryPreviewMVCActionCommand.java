764d33775986f97ed639a232cd912287f0fd63c5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-06-12 13:54:06 +0200

    LPS-81675 Adds new action to update the fragment entry preview

---
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/UpdateFragmentEntryPreviewMVCActionCommand.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.portlet.action;
+
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.service.FragmentEntryService;
+import com.liferay.portal.kernel.model.Repository;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.TempFileEntryUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + FragmentPortletKeys.FRAGMENT,
+		"mvc.command.name=/fragment/update_fragment_entry_preview"
+	},
+	service = MVCActionCommand.class
+)
+public class UpdateFragmentEntryPreviewMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long fragmentEntryId = ParamUtil.getLong(
+			actionRequest, "fragmentEntryId");
+		long fileEntryId = ParamUtil.getLong(actionRequest, "fileEntryId");
+
+		FileEntry fileEntry = _dlAppLocalService.getFileEntry(fileEntryId);
+
+		FileEntry tempFileEntry = fileEntry;
+
+		Repository repository =
+			PortletFileRepositoryUtil.fetchPortletRepository(
+				themeDisplay.getScopeGroupId(), FragmentPortletKeys.FRAGMENT);
+
+		String fileName =
+			fragmentEntryId + "_preview." + fileEntry.getExtension();
+
+		FileEntry oldFileEntry =
+			PortletFileRepositoryUtil.fetchPortletFileEntry(
+				themeDisplay.getScopeGroupId(), repository.getDlFolderId(),
+				fileName);
+
+		if (oldFileEntry != null) {
+			PortletFileRepositoryUtil.deletePortletFileEntry(
+				oldFileEntry.getFileEntryId());
+		}
+
+		fileEntry = PortletFileRepositoryUtil.addPortletFileEntry(
+			themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
+			FragmentEntry.class.getName(), fragmentEntryId,
+			FragmentPortletKeys.FRAGMENT, repository.getDlFolderId(),
+			fileEntry.getContentStream(), fileName, fileEntry.getMimeType(),
+			false);
+
+		_fragmentEntryService.updateFragmentEntry(
+			fragmentEntryId, fileEntry.getFileEntryId());
+
+		TempFileEntryUtil.deleteTempFileEntry(tempFileEntry.getFileEntryId());
+	}
+
+	@Reference
+	private DLAppLocalService _dlAppLocalService;
+
+	@Reference
+	private FragmentEntryService _fragmentEntryService;
+
+}
