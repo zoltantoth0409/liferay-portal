c1cfa4ac71e2e585c898728dfe49945d766c78a9
Peter Shin <peter.shin@liferay.com>
2018-06-12 09:02:38 -0700

    LPS-79679 Fix compile error

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/display/context/DDMDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/display/context/DDMDisplayContext.java
@@ -871,7 +871,7 @@ public class DDMDisplayContext {
 
 		boolean showAncestorScopes = showAncestorScopes();
 
-		if (showAncestorScopes != null) {
+		if (showAncestorScopes) {
 			portletURL.setParameter(
 				"showAncestorScopes", String.valueOf(showAncestorScopes));
 		}
