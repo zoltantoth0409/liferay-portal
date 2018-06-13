63c0601e5154ad33768c17bf99188c87acf00800
Matthew Tambara <matthew.tambara@liferay.com>
2018-06-13 15:54:31 -0700

    LPS-82414 Move LPKGArtifactInstaller to lpkg deployer

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/LPKGArtifactInstaller.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/LPKGArtifactInstaller.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.marketplace.deployer.internal;
+package com.liferay.portal.lpkg.deployer.internal;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.concurrent.DefaultNoticeableFuture;
