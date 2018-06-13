9067a81f5cf69a8aac53307b61f3de7a4ede9a8c
Hugo Huijser <hugo.huijser@liferay.com>
2018-06-13 09:48:44 +0800

    LPS-82343 No need to specify language key when it's already available in portal-impl

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesLanguageKeysCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesLanguageKeysCheck.java
@@ -14,17 +14,22 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.util.StringUtil;
 
+import java.io.InputStream;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author Peter Shin
+ * @author Hugo Huijser
  */
 public class PropertiesLanguageKeysCheck extends BaseFileCheck {
 
@@ -42,6 +47,11 @@ public class PropertiesLanguageKeysCheck extends BaseFileCheck {
 			return content;
 		}
 
+		StringBundler sb = new StringBundler();
+
+		Properties portalImplLanguageProperties =
+			_getPortalImplLanguageProperties(absolutePath);
+
 		try (UnsyncBufferedReader unsyncBufferedReader =
 				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
 
@@ -51,27 +61,70 @@ public class PropertiesLanguageKeysCheck extends BaseFileCheck {
 				String[] array = line.split("=", 2);
 
 				if (array.length < 2) {
+					sb.append(line);
+					sb.append("\n");
+
 					continue;
 				}
 
 				String key = array[0];
-
-				if (_isAllowedLanguageKey(key)) {
-					continue;
-				}
-
 				String value = array[1];
 
-				if (value.matches("(?s).*<([a-zA-Z0-9]+)[^>]*>.*?<\\/\\1>.*")) {
+				if (portalImplLanguageProperties != null) {
+					String portalImplValue =
+						portalImplLanguageProperties.getProperty(key);
+
+					if (value.equals(portalImplValue)) {
+						continue;
+					}
+				}
+
+				if (!_isAllowedLanguageKey(key) &&
+					value.matches("(?s).*<([a-zA-Z0-9]+)[^>]*>.*?<\\/\\1>.*")) {
+
 					addMessage(
 						fileName, "Remove HTML markup for '" + key + "'",
 						"language_keys.markdown",
 						getLineNumber(content, content.indexOf(line)));
 				}
+
+				sb.append(line);
+				sb.append("\n");
 			}
 		}
 
+		content = sb.toString();
+
+		if (content.endsWith("\n")) {
+			content = content.substring(0, content.length() - 1);
+		}
+
 		return content;
+	}
+
+	private Properties _getPortalImplLanguageProperties(String absolutePath)
+		throws Exception {
+
+		if (absolutePath.endsWith(_PORTAL_IMPL_LANGUAGE_PROPERTIES_FILE_NAME)) {
+			return null;
+		}
+
+		if (_portalImplLanguageProperties != null) {
+			return _portalImplLanguageProperties;
+		}
+
+		Properties portalImplLanguageProperties = new Properties();
+
+		InputStream inputStream = getPortalInputStream(
+			_PORTAL_IMPL_LANGUAGE_PROPERTIES_FILE_NAME);
+
+		if (inputStream != null) {
+			portalImplLanguageProperties.load(inputStream);
+		}
+
+		_portalImplLanguageProperties = portalImplLanguageProperties;
+
+		return _portalImplLanguageProperties;
 	}
 
 	private boolean _isAllowedLanguageKey(String key) {
@@ -86,6 +139,10 @@ public class PropertiesLanguageKeysCheck extends BaseFileCheck {
 		return false;
 	}
 
+	private static final String _PORTAL_IMPL_LANGUAGE_PROPERTIES_FILE_NAME =
+		"portal-impl/src/content/Language.properties";
+
 	private final List<String> _allowedLanguageKeys = new ArrayList<>();
+	private Properties _portalImplLanguageProperties;
 
 }
