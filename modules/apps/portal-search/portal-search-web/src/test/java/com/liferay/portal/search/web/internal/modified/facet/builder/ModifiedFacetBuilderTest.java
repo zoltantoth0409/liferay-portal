094f2e1ac181a9f6344d9e6f26b4be445b0517c2
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2018-06-14 20:16:39 -0300

    LPS-82478 portal-search-web: Add unit test to replicate the bug
    https://issues.liferay.com/browse/LPS-82478

--- modules/apps/portal-search/portal-search-web/src/test/java/com/liferay/portal/search/web/internal/modified/facet/builder/ModifiedFacetBuilderTest.java
+++ modules/apps/portal-search/portal-search-web/src/test/java/com/liferay/portal/search/web/internal/modified/facet/builder/ModifiedFacetBuilderTest.java
@@ -18,6 +18,7 @@ import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.facet.config.FacetConfiguration;
 import com.liferay.portal.kernel.search.facet.util.RangeParserUtil;
@@ -53,18 +54,7 @@ public class ModifiedFacetBuilderTest {
 		dateFormatFactory = new DateFormatFactoryImpl();
 		filterBuilders = new FilterBuildersImpl();
 		jsonFactory = new JSONFactoryImpl();
-	}
-
-	@Test
-	public void testBoundedRange() {
-		ModifiedFacetBuilder modifiedFacetBuilder =
-			createModifiedFacetBuilder();
-
-		modifiedFacetBuilder.setCustomRangeFrom("20180131");
-		modifiedFacetBuilder.setCustomRangeTo("20180228");
-
-		assertRange(
-			"20180131000000", "20180228235959", modifiedFacetBuilder.build());
+		searchContext = new SearchContext();
 	}
 
 	@Test
@@ -82,6 +72,31 @@ public class ModifiedFacetBuilderTest {
 
 		assertRange(
 			"20180228151923", "20180301151923", modifiedFacetBuilder.build());
+	}
+
+	@Test
+	public void testCustomRange() {
+		ModifiedFacetBuilder modifiedFacetBuilder =
+			createModifiedFacetBuilder();
+
+		modifiedFacetBuilder.setCustomRangeFrom("20180131");
+		modifiedFacetBuilder.setCustomRangeTo("20180228");
+
+		assertRange(
+			"20180131000000", "20180228235959", modifiedFacetBuilder.build());
+	}
+
+	@Test
+	public void testCustomRangeSetsSearchContextAttribute() {
+		ModifiedFacetBuilder modifiedFacetBuilder =
+			createModifiedFacetBuilder();
+
+		modifiedFacetBuilder.setCustomRangeFrom("20180131");
+		modifiedFacetBuilder.setCustomRangeTo("20180228");
+
+		modifiedFacetBuilder.build();
+
+		assertRange("20180131000000", "20180228235959", searchContext);
 	}
 
 	@Test
@@ -129,7 +144,18 @@ public class ModifiedFacetBuilderTest {
 	}
 
 	protected void assertRange(String from, String to, Facet facet) {
-		List<String> calendars = getRangeBounds(facet);
+		assertRange(from, to, facet.getSelections()[0]);
+	}
+
+	protected void assertRange(
+		String from, String to, SearchContext searchContext) {
+
+		assertRange(
+			from, to, (String)searchContext.getAttribute(Field.MODIFIED_DATE));
+	}
+
+	protected void assertRange(String from, String to, String range) {
+		List<String> calendars = getRangeBounds(range);
 
 		Assert.assertEquals(from, calendars.get(0));
 		Assert.assertEquals(to, calendars.get(1));
@@ -176,8 +202,6 @@ public class ModifiedFacetBuilderTest {
 	}
 
 	protected ModifiedFacetBuilder createModifiedFacetBuilder() {
-		SearchContext searchContext = new SearchContext();
-
 		ModifiedFacetFactory modifiedFacetFactory = createModifiedFacetFactory(
 			searchContext);
 
@@ -215,9 +239,7 @@ public class ModifiedFacetBuilderTest {
 		return jsonArray;
 	}
 
-	protected List<String> getRangeBounds(Facet facet) {
-		String range = facet.getSelections()[0];
-
+	protected List<String> getRangeBounds(String range) {
 		String[] dateStrings = RangeParserUtil.parserRange(range);
 
 		return Arrays.asList(dateStrings);
@@ -227,5 +249,6 @@ public class ModifiedFacetBuilderTest {
 	protected DateFormatFactory dateFormatFactory;
 	protected FilterBuilders filterBuilders;
 	protected JSONFactory jsonFactory;
+	protected SearchContext searchContext;
 
 }
