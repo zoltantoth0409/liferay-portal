116f8831dd876469ecbf418f1a13a74281b8c89e
arthurchan35 <arthurchan35@gmail.com>
2018-06-14 09:36:02 +0800

    LPS-79348 SF

--- modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/request/SearchRequestImpl.java
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/request/SearchRequestImpl.java
@@ -104,7 +104,7 @@ public class SearchRequestImpl implements SearchRequest {
 	protected SearchContext buildSearchContext() {
 		SearchContext searchContext = _searchContextBuilder.getSearchContext();
 
-		searchContext.setAttribute("filterExpired", true);
+		searchContext.setAttribute("filterExpired", Boolean.TRUE);
 		searchContext.setAttribute("paginationType", "more");
 
 		addScopeFacet(searchContext);
