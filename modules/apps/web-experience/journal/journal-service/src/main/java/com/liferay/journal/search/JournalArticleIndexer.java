f90afacb69c80c488916c770e6e7b8e8e51d25b0
arthurchan35 <arthurchan35@gmail.com>
2018-06-14 09:30:23 +0800

    LPS-79348 boolean != long

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/search/JournalArticleIndexer.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/search/JournalArticleIndexer.java
@@ -222,7 +222,7 @@ public class JournalArticleIndexer
 		boolean showNonindexable = GetterUtil.getBoolean(
 			searchContext.getAttribute("showNonindexable"));
 
-		boolean filterExpired = GetterUtil.getLong(
+		boolean filterExpired = GetterUtil.getBoolean(
 			searchContext.getAttribute("filterExpired"));
 
 		if (latest && !relatedClassName && !showNonindexable) {
