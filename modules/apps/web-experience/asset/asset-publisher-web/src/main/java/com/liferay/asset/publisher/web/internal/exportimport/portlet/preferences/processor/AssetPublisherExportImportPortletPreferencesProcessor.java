2e00d53ef3d59a9c884da37c1dfc61f8d4c132b2
Máté Thurzó <mate.thurzo@liferay.com>
2018-06-15 11:01:34 +0200

    LPS-82355 Asset publish should publish the configuration related entities

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/exportimport/portlet/preferences/processor/AssetPublisherExportImportPortletPreferencesProcessor.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/exportimport/portlet/preferences/processor/AssetPublisherExportImportPortletPreferencesProcessor.java
@@ -356,9 +356,8 @@ public class AssetPublisherExportImportPortletPreferencesProcessor
 				uuid = assetCategory.getUuid();
 				groupId = assetCategory.getGroupId();
 
-				portletDataContext.addReferenceElement(
-					portlet, rootElement, assetCategory,
-					PortletDataContext.REFERENCE_TYPE_DEPENDENCY, true);
+				StagedModelDataHandlerUtil.exportReferenceStagedModel(
+					portletDataContext, portlet.getPortletId(), assetCategory);
 			}
 		}
 		else if (className.equals(AssetVocabulary.class.getName())) {
@@ -370,9 +369,9 @@ public class AssetPublisherExportImportPortletPreferencesProcessor
 				uuid = assetVocabulary.getUuid();
 				groupId = assetVocabulary.getGroupId();
 
-				portletDataContext.addReferenceElement(
-					portlet, rootElement, assetVocabulary,
-					PortletDataContext.REFERENCE_TYPE_DEPENDENCY, true);
+				StagedModelDataHandlerUtil.exportReferenceStagedModel(
+					portletDataContext, portlet.getPortletId(),
+					assetVocabulary);
 			}
 		}
 		else if (className.equals(DDMStructure.class.getName())) {
@@ -383,9 +382,8 @@ public class AssetPublisherExportImportPortletPreferencesProcessor
 				uuid = ddmStructure.getUuid();
 				groupId = ddmStructure.getGroupId();
 
-				portletDataContext.addReferenceElement(
-					portlet, rootElement, ddmStructure,
-					PortletDataContext.REFERENCE_TYPE_DEPENDENCY, true);
+				StagedModelDataHandlerUtil.exportReferenceStagedModel(
+					portletDataContext, portlet.getPortletId(), ddmStructure);
 			}
 		}
 		else if (className.equals(DLFileEntryType.class.getName())) {
@@ -396,9 +394,9 @@ public class AssetPublisherExportImportPortletPreferencesProcessor
 				uuid = dlFileEntryType.getUuid();
 				groupId = dlFileEntryType.getGroupId();
 
-				portletDataContext.addReferenceElement(
-					portlet, rootElement, dlFileEntryType,
-					PortletDataContext.REFERENCE_TYPE_DEPENDENCY, true);
+				StagedModelDataHandlerUtil.exportReferenceStagedModel(
+					portletDataContext, portlet.getPortletId(),
+					dlFileEntryType);
 			}
 		}
 		else if (className.equals(Organization.class.getName())) {
