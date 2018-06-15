8d227ae740a962ec0a6919be9235940f24bc9649
Zoltán Császi <zoltan.csaszi@liferay.com>
2018-06-15 14:55:26 +0200

    LPS-82289 JournalArticle modelListener handles JournalArticleResources as well

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/model/listener/JournalArticleStagingModelListener.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/model/listener/JournalArticleStagingModelListener.java
@@ -14,11 +14,31 @@
 
 package com.liferay.journal.internal.model.listener;
 
+import com.liferay.changeset.model.ChangesetCollection;
+import com.liferay.changeset.model.ChangesetEntry;
+import com.liferay.changeset.service.ChangesetCollectionLocalService;
+import com.liferay.changeset.service.ChangesetEntryLocalService;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerRegistryUtil;
+import com.liferay.exportimport.kernel.staging.Staging;
+import com.liferay.exportimport.kernel.staging.StagingConstants;
 import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.model.JournalArticleResource;
+import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.staging.model.listener.StagingModelListener;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -35,6 +55,8 @@ public class JournalArticleStagingModelListener
 		throws ModelListenerException {
 
 		_stagingModelListener.onAfterCreate(journalArticle);
+
+		_addResourceEntityToChangeset(journalArticle);
 	}
 
 	@Override
@@ -42,6 +64,29 @@ public class JournalArticleStagingModelListener
 		throws ModelListenerException {
 
 		_stagingModelListener.onAfterRemove(journalArticle);
+
+		long articleClassNameId = _classNameLocalService.getClassNameId(
+			journalArticle.getModelClass());
+
+		try {
+			ChangesetCollection changesetCollection =
+				_changesetCollectionLocalService.fetchOrAddChangesetCollection(
+					journalArticle.getGroupId(),
+					StagingConstants.
+						RANGE_FROM_LAST_PUBLISH_DATE_CHANGESET_NAME);
+
+			long articleResourceClassNameId =
+				_classNameLocalService.getClassNameId(
+					JournalArticleResource.class);
+
+			_deleteUnnecessaryResourceEntity(
+				changesetCollection, articleClassNameId,
+				journalArticle.getResourcePrimKey(),
+				articleResourceClassNameId);
+		}
+		catch (PortalException pe) {
+			_log.error(pe.getMessage(), pe);
+		}
 	}
 
 	@Override
@@ -49,7 +94,108 @@ public class JournalArticleStagingModelListener
 		throws ModelListenerException {
 
 		_stagingModelListener.onAfterUpdate(journalArticle);
+
+		_addResourceEntityToChangeset(journalArticle);
 	}
+
+	private void _addResourceEntityToChangeset(JournalArticle journalArticle) {
+		try {
+			ChangesetCollection changesetCollection =
+				_changesetCollectionLocalService.fetchOrAddChangesetCollection(
+					journalArticle.getGroupId(),
+					StagingConstants.
+						RANGE_FROM_LAST_PUBLISH_DATE_CHANGESET_NAME);
+
+			long articleClassNameId = _classNameLocalService.getClassNameId(
+				journalArticle.getModelClass());
+
+			ChangesetEntry changesetEntry =
+				_changesetEntryLocalService.fetchChangesetEntry(
+					changesetCollection.getChangesetCollectionId(),
+					articleClassNameId, journalArticle.getId());
+
+			JournalArticleResource journalArticleResource =
+				journalArticle.getArticleResource();
+
+			long articleResourceClassNameId =
+				_classNameLocalService.getClassNameId(
+					journalArticleResource.getModelClass());
+
+			_deleteUnnecessaryResourceEntity(
+				changesetCollection, articleClassNameId,
+				journalArticleResource.getResourcePrimKey(),
+				articleResourceClassNameId);
+
+			if (changesetEntry == null) {
+				return;
+			}
+
+			if (journalArticle.getStatus() !=
+					WorkflowConstants.STATUS_IN_TRASH) {
+
+				_changesetEntryLocalService.fetchOrAddChangesetEntry(
+					changesetCollection.getChangesetCollectionId(),
+					articleResourceClassNameId,
+					journalArticleResource.getPrimaryKey());
+			}
+		}
+		catch (PortalException pe) {
+			_log.error(pe.getMessage(), pe);
+		}
+	}
+
+	private void _deleteUnnecessaryResourceEntity(
+		ChangesetCollection changesetCollection, long articleClassNameId,
+		long journalArticleResourcePrimKey, long articleResourceClassNameId) {
+
+		List<JournalArticle> articlesVersions =
+			_journalArticleLocalService.getArticlesByResourcePrimKey(
+				journalArticleResourcePrimKey);
+
+		Set<Long> classPKs = new HashSet<>();
+
+		StagedModelDataHandler stagedModelDataHandler =
+			StagedModelDataHandlerRegistryUtil.getStagedModelDataHandler(
+				JournalArticle.class.getName());
+
+		for (JournalArticle articleVersion : articlesVersions) {
+			if (ArrayUtil.contains(
+					stagedModelDataHandler.getExportableStatuses(),
+					articleVersion.getStatus())) {
+
+				classPKs.add(articleVersion.getId());
+			}
+		}
+
+		long journalChangetEntriesCount =
+			_changesetEntryLocalService.getChangesetEntriesCount(
+				changesetCollection.getChangesetCollectionId(),
+				articleClassNameId, classPKs);
+
+		if (journalChangetEntriesCount == 0) {
+			_changesetEntryLocalService.deleteEntry(
+				changesetCollection.getChangesetCollectionId(),
+				articleResourceClassNameId, journalArticleResourcePrimKey);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalArticleStagingModelListener.class);
+
+	@Reference
+	private ChangesetCollectionLocalService _changesetCollectionLocalService;
+
+	@Reference
+	private ChangesetEntryLocalService _changesetEntryLocalService;
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private JournalArticleLocalService _journalArticleLocalService;
+
+	@Reference
+	private Staging _staging;
 
 	@Reference
 	private StagingModelListener<JournalArticle> _stagingModelListener;
