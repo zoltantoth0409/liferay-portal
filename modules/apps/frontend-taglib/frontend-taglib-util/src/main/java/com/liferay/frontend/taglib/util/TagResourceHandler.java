4a476be26a02455752e536f5f517301db30e6e75
Iván Zaera Avellón <ivan.zaera@liferay.com>
2018-06-15 13:26:43 +0200

    LPS-82401 Create a taglib utility to write resources to the HTML stream

---
+++ modules/apps/frontend-taglib/frontend-taglib-util/src/main/java/com/liferay/frontend/taglib/util/TagResourceHandler.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.util;
+
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.frontend.taglib.util.internal.NPMResolverRef;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.util.OutputData;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.util.Dictionary;
+import java.util.EnumMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+public class TagResourceHandler {
+
+	public TagResourceHandler(Class<?> tagClass, TagAccessor tagAccessor) {
+		_tagAccessor = tagAccessor;
+
+		_log = LogFactoryUtil.getLog(tagClass);
+
+		_bundle = FrameworkUtil.getBundle(tagClass);
+
+		Dictionary<String, String> headers = _bundle.getHeaders(
+			StringPool.BLANK);
+
+		_webContextPath = headers.get("Web-ContextPath");
+	}
+
+	public void outputBundleStyleSheet(String bundleCssPath) {
+		StringBundler sb = new StringBundler(6);
+
+		sb.append("<link data-senna-track=\"temporary\" href=\"");
+		sb.append(PortalUtil.getPathModule());
+		sb.append(_webContextPath);
+		sb.append(StringPool.SLASH);
+		sb.append(bundleCssPath);
+		sb.append("\" rel=\"stylesheet\">");
+
+		outputResource(Position.TOP, sb.toString());
+	}
+
+	public void outputNPMResource(String npmResourcePath) {
+		try (NPMResolverRef npmResolverRef = new NPMResolverRef(_tagAccessor)) {
+			NPMResolver npmResolver = npmResolverRef.getNPMResolver();
+
+			String resourcePath = npmResolver.resolveModuleName(
+				npmResourcePath);
+
+			URL url = _bundle.getEntry(
+				"META-INF/resources/node_modules/" + resourcePath);
+
+			outputResource(Position.BOTTOM, StringUtil.read(url.openStream()));
+		}
+		catch (Exception e) {
+			_log.error("Unable to output npm resource " + npmResourcePath, e);
+		}
+	}
+
+	public void outputNPMStyleSheet(String npmCssPath) {
+		try (NPMResolverRef npmResolverRef = new NPMResolverRef(_tagAccessor)) {
+			NPMResolver npmResolver = npmResolverRef.getNPMResolver();
+
+			String cssPath = npmResolver.resolveModuleName(npmCssPath);
+
+			StringBundler sb = new StringBundler(6);
+
+			sb.append("<link href=\"");
+			sb.append(PortalUtil.getPathModule());
+			sb.append(_webContextPath);
+			sb.append("/node_modules/");
+			sb.append(cssPath);
+			sb.append("\" rel=\"stylesheet\">");
+
+			outputResource(Position.TOP, sb.toString());
+		}
+		catch (Exception e) {
+			_log.error("Unable to output npm style sheet " + npmCssPath, e);
+		}
+	}
+
+	public void outputResource(Position position, String html) {
+		HttpServletRequest request = _getRequest();
+
+		boolean xPjax = GetterUtil.getBoolean(request.getHeader("X-PJAX"));
+
+		ThemeDisplay themeDisplay = _getThemeDisplay();
+
+		if (themeDisplay.isIsolated() || themeDisplay.isLifecycleResource() ||
+			themeDisplay.isStateExclusive() || xPjax) {
+
+			try {
+				PageContext pageContext = _tagAccessor.getPageContext();
+
+				JspWriter jspWriter = pageContext.getOut();
+
+				jspWriter.write(html);
+			}
+			catch (IOException ioe) {
+				_log.error("Unable to output resource", ioe);
+			}
+		}
+		else {
+			OutputData outputData = _getOutputData();
+
+			Long key = _nextKey.getAndIncrement();
+
+			outputData.setDataSB(
+				key.toString(), _webKeysMap.get(position),
+				new StringBundler(html));
+		}
+	}
+
+	public enum Position {
+
+		BOTTOM, TOP
+
+	}
+
+	private OutputData _getOutputData() {
+		HttpServletRequest request = _getRequest();
+
+		OutputData outputData = (OutputData)request.getAttribute(
+			WebKeys.OUTPUT_DATA);
+
+		if (outputData == null) {
+			outputData = new OutputData();
+
+			request.setAttribute(WebKeys.OUTPUT_DATA, outputData);
+		}
+
+		return outputData;
+	}
+
+	private HttpServletRequest _getRequest() {
+		return _tagAccessor.getRequest();
+	}
+
+	private ThemeDisplay _getThemeDisplay() {
+		ServletRequest servletRequest = _getRequest();
+
+		return (ThemeDisplay)servletRequest.getAttribute(WebKeys.THEME_DISPLAY);
+	}
+
+	private static final EnumMap<Position, String> _webKeysMap = new EnumMap(
+		Position.class);
+
+	static {
+		_webKeysMap.put(Position.BOTTOM, WebKeys.PAGE_BODY_BOTTOM);
+		_webKeysMap.put(Position.TOP, WebKeys.PAGE_BODY_TOP);
+	}
+
+	private final Bundle _bundle;
+	private final Log _log;
+	private final AtomicLong _nextKey = new AtomicLong();
+	private final TagAccessor _tagAccessor;
+	private final String _webContextPath;
+
+}
