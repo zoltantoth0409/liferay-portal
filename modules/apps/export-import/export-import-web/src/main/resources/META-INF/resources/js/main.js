4e60d6650ac59c83c4ce5735d9221911b3fefbcf
Peter Borkuti <peter.borkuti@liferay.com>
2018-06-15 12:19:01 +0200

    LPS-75511 References are selected for the not selected contents at editing export templates - same code as for LPS-74465

--- modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/export-import/export-import-web/src/main/resources/META-INF/resources/js/main.js
@@ -1043,11 +1043,22 @@ AUI.add(
 
 						var inputs = contentNode.all('.field');
 
+						var portletDataNode = instance.byId('PORTLET_DATA_' + portletId);
+
+						var portletChecked = portletDataNode.attr('checked');
+
 						var selectedContent = [];
 
 						inputs.each(
 							function(item, index, collection) {
-								var checked = item.attr(STR_CHECKED);
+								var checked = false;
+
+								if (portletChecked) {
+									checked = item.attr(STR_CHECKED);
+								}
+								else {
+									item.attr(STR_CHECKED, false);
+								}
 
 								if (checked) {
 									selectedContent.push(item.attr('data-name'));
@@ -1056,7 +1067,7 @@ AUI.add(
 						);
 
 						if (selectedContent.length === 0) {
-							instance.byId('PORTLET_DATA_' + portletId).attr('checked', false);
+							portletDataNode.attr('checked', false);
 
 							instance.byId('showChangeContent_' + portletId).hide();
 						}
