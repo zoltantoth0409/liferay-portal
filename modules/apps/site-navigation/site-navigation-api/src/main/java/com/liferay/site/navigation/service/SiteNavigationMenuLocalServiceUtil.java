de301c2bed91789ad0c8fdf9b0089b167db4b1cb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-06-18 14:25:18 +0200

    LPS-82570 Auto

--- modules/apps/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuLocalServiceUtil.java
+++ modules/apps/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuLocalServiceUtil.java
@@ -42,12 +42,11 @@ public class SiteNavigationMenuLocalServiceUtil {
 	 *
 	 * Never modify this class directly. Add custom service methods to {@link com.liferay.site.navigation.service.impl.SiteNavigationMenuLocalServiceImpl} and rerun ServiceBuilder to regenerate this class.
 	 */
-	public static com.liferay.site.navigation.model.SiteNavigationMenu addDefaultSiteNavigationMenu(
-		long userId, long groupId,
+	public static void addDefaultSiteNavigationMenu(long userId, long groupId,
 		com.liferay.portal.kernel.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {
-		return getService()
-				   .addDefaultSiteNavigationMenu(userId, groupId, serviceContext);
+		getService()
+			.addDefaultSiteNavigationMenu(userId, groupId, serviceContext);
 	}
 
 	public static com.liferay.site.navigation.model.SiteNavigationMenu addSiteNavigationMenu(
