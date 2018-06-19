d4d103183515c35428d1fb0f249cea4379478983
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-06-19 09:14:01 +0200

    LPS-82540 SF

--- modules/apps/site/site-api/src/main/java/com/liferay/site/util/GroupSearchProvider.java
+++ modules/apps/site/site-api/src/main/java/com/liferay/site/util/GroupSearchProvider.java
@@ -72,9 +72,9 @@ public class GroupSearchProvider {
 		if (!searchTerms.hasSearchTerms() &&
 			isFilterManageableGroups(portletRequest) && (parentGroupId <= 0)) {
 
-			List<Group> groups = getAllGroups(portletRequest);
-
-			groups.sort(groupSearch.getOrderByComparator());
+			List<Group> groups = ListUtil.sort(
+				getAllGroups(portletRequest),
+				groupSearch.getOrderByComparator());
 
 			groupSearch.setTotal(groups.size());
 
