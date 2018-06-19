4f1108452e7398f2b89db04a17cd7d3e5374d988
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-06-19 12:25:37 +0200

    LPS-82572 We only want to show fragments in staging

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/application/list/FragmentPanelApp.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/application/list/FragmentPanelApp.java
@@ -18,7 +18,10 @@ import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -39,6 +42,17 @@ public class FragmentPanelApp extends BasePanelApp {
 	@Override
 	public String getPortletId() {
 		return FragmentPortletKeys.FRAGMENT;
+	}
+
+	@Override
+	public boolean isShow(PermissionChecker permissionChecker, Group group)
+		throws PortalException {
+
+		if (group.isStaged() && !group.isStagingGroup()) {
+			return false;
+		}
+
+		return super.isShow(permissionChecker, group);
 	}
 
 	@Override
