61ac7617f23fa5648183b37eedc9807dd9f93b0e
Hugo Huijser <hugo.huijser@liferay.com>
2018-06-19 18:01:20 +0800

    LPS-82420 Add check to SF

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/FrameworkBundleCheck.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.liferay.source.formatter.checkstyle.util.DetailASTUtil;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+
+/**
+ * @author Hugo Huijser
+ */
+public class FrameworkBundleCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.CTOR_DEF, TokenTypes.METHOD_DEF};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		List<String> importNames = DetailASTUtil.getImportNames(detailAST);
+
+		if (!importNames.contains("org.osgi.framework.Bundle")) {
+			return;
+		}
+
+		List<DetailAST> methodCallASTList = DetailASTUtil.getMethodCalls(
+			detailAST, "getHeaders");
+
+		for (DetailAST methodCallAST : methodCallASTList) {
+			DetailAST elistAST = methodCallAST.findFirstToken(TokenTypes.ELIST);
+
+			List<DetailAST> exprASTList = DetailASTUtil.getAllChildTokens(
+				elistAST, false, TokenTypes.EXPR);
+
+			if (!exprASTList.isEmpty()) {
+				continue;
+			}
+
+			String variableName = DetailASTUtil.getVariableName(methodCallAST);
+
+			String variableTypeName = DetailASTUtil.getVariableTypeName(
+				methodCallAST, variableName, false);
+
+			if (variableTypeName.equals("Bundle")) {
+				log(methodCallAST.getLineNo(), _MSG_USE_BUNDLE_GET_HEADERS);
+			}
+		}
+	}
+
+	private static final String _MSG_USE_BUNDLE_GET_HEADERS =
+		"bundle.get.headers.use";
+
+}
