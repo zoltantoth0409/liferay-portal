252b3af2453cba3b43ac4abc8a30b41e33d6943b
Pablo Molina <contact@pablomolina.me>
2018-06-19 10:10:02 +0200

    LPS-82593 Avoid using findIndex method

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/fragment_entry_link/FragmentEntryLinkList.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/fragment_entry_link/FragmentEntryLinkList.es.js
@@ -1,4 +1,5 @@
 import Component from 'metal-component';
+import {Config} from 'metal-state';
 import Soy from 'metal-soy';
 
 import './FragmentEntryLink.es';
@@ -20,10 +21,13 @@ class FragmentEntryLinkList extends Component {
 	focusFragmentEntryLink(fragmentEntryLinkId) {
 		requestAnimationFrame(
 			() => {
-				const index = this.fragmentEntryLinks.findIndex(
-					_fragmentEntryLink => {
-						return _fragmentEntryLink.fragmentEntryLinkId === fragmentEntryLinkId;
-					}
+				const index = this.fragmentEntryLinks.indexOf(
+					this.fragmentEntryLinks.find(
+						fragmentEntryLink => (
+							fragmentEntryLink.fragmentEntryLinkId ===
+							fragmentEntryLinkId
+						)
+					)
 				);
 
 				const fragmentEntryLinkElement = this.refs.fragmentEntryLinks.querySelectorAll(
@@ -86,7 +90,40 @@ class FragmentEntryLinkList extends Component {
  * @type {!Object}
  */
 
-FragmentEntryLinkList.STATE = {};
+FragmentEntryLinkList.STATE = {
+
+	/**
+	 * List of fragment instances being used, the order
+	 * of the elements in this array defines their position.
+	 * @default []
+	 * @instance
+	 * @memberOf FragmentEntryLinkList
+	 * @review
+	 * @type {Array<{
+	 *   config: Object,
+	 *   content: string,
+	 *   editableValues: Object,
+	 *   fragmentEntryId: !string,
+	 *   fragmentEntryLinkId: !string,
+	 *   name: !string,
+	 *   position: !number
+	 * }>}
+	 */
+
+	fragmentEntryLinks: Config.arrayOf(
+		Config.shapeOf(
+			{
+				config: Config.object().value({}),
+				content: Config.any().value(''),
+				editableValues: Config.object().value({}),
+				fragmentEntryId: Config.string().required(),
+				fragmentEntryLinkId: Config.string().required(),
+				name: Config.string().required(),
+				position: Config.number().required()
+			}
+		)
+	).value([])
+};
 
 Soy.register(FragmentEntryLinkList, templates);
 
