b7c1fced97336d8ae9c987e0e6a4ccf56bf64fff
Brian Chan <brian.chan@liferay.com>
2018-06-19 23:59:32 -0700

    LPS-82433 Consistency with other uses in this same class, otherwise I would default to your way

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetTagServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetTagServiceImpl.java
@@ -72,10 +72,10 @@ public class AssetTagServiceImpl extends AssetTagServiceBaseImpl {
 	@Override
 	public void deleteTags(long[] tagIds) throws PortalException {
 		for (long tagId : tagIds) {
-			AssetTag assetTag = assetTagLocalService.getTag(tagId);
+			AssetTag tag = assetTagLocalService.getTag(tagId);
 
 			AssetTagsPermission.check(
-				getPermissionChecker(), assetTag.getGroupId(),
+				getPermissionChecker(), tag.getGroupId(),
 				ActionKeys.MANAGE_TAG);
 
 			assetTagLocalService.deleteTag(tagId);
@@ -222,11 +222,10 @@ public class AssetTagServiceImpl extends AssetTagServiceBaseImpl {
 
 	@Override
 	public void mergeTags(long fromTagId, long toTagId) throws PortalException {
-		AssetTag assetTag = assetTagLocalService.getTag(fromTagId);
+		AssetTag tag = assetTagLocalService.getTag(fromTagId);
 
 		AssetTagsPermission.check(
-			getPermissionChecker(), assetTag.getGroupId(),
-			ActionKeys.MANAGE_TAG);
+			getPermissionChecker(), tag.getGroupId(), ActionKeys.MANAGE_TAG);
 
 		assetTagLocalService.mergeTags(fromTagId, toTagId);
 	}
@@ -257,11 +256,10 @@ public class AssetTagServiceImpl extends AssetTagServiceBaseImpl {
 			long tagId, String name, ServiceContext serviceContext)
 		throws PortalException {
 
-		AssetTag assetTag = assetTagLocalService.getTag(tagId);
+		AssetTag tag = assetTagLocalService.getTag(tagId);
 
 		AssetTagsPermission.check(
-			getPermissionChecker(), assetTag.getGroupId(),
-			ActionKeys.MANAGE_TAG);
+			getPermissionChecker(), tag.getGroupId(), ActionKeys.MANAGE_TAG);
 
 		return assetTagLocalService.updateTag(
 			getUserId(), tagId, name, serviceContext);
