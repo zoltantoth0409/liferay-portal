bb7793f20de61dabff7ad7f266d09a3a65a9d50f
Joshua Cords <joshua.cords@liferay.com>
2018-06-20 08:38:58 -0700

    LPS-81718 It's only used in asset, so move this to it's own bundle

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -2230,8 +2230,6 @@ public interface PropsKeys {
 
 	public static final String STAGING_LIVE_GROUP_REMOTE_STAGING_ENABLED = "staging.live.group.remote.staging.enabled";
 
-	public static final String STAGING_PORTLET_MERGE_TAG_NAMES = "staging.portlet.merge.tag.names";
-
 	public static final String STAGING_REMOTE_TRANSFER_BUFFER_SIZE = "staging.remote.transfer.buffer.size";
 
 	public static final String STRIP_CSS_SASS_ENABLED = "strip.css.sass.enabled";
