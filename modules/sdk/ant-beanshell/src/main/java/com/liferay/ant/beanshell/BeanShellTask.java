f51c922f7e3955f4e4b733c4223e864dc306d54a
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-06-19 17:14:11 -0700

    LRQA-41672 Add classpathref to the beanshell tasks

--- modules/sdk/ant-beanshell/src/main/java/com/liferay/ant/beanshell/BeanShellTask.java
+++ modules/sdk/ant-beanshell/src/main/java/com/liferay/ant/beanshell/BeanShellTask.java
@@ -20,8 +20,10 @@ import bsh.Interpreter;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.tools.ant.AntClassLoader;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Path;
 
 /**
  * @author Peter Yoo
@@ -34,11 +36,24 @@ public class BeanShellTask extends Task {
 
 	@Override
 	public void execute() throws BuildException {
-		Interpreter interpreter = new Interpreter();
-
 		Class<?> clazz = getClass();
 
-		interpreter.setClassLoader(clazz.getClassLoader());
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		if (_classpathRef != null) {
+			Path path = project.getReference(_classpathRef);
+
+			if (path == null) {
+				throw new BuildException(
+					"Invalid beanshell classpathref: " + _classpathRef);
+			}
+
+			classLoader = new AntClassLoader(getProject(), path);
+		}
+
+		Interpreter interpreter = new Interpreter();
+
+		interpreter.setClassLoader(classLoader);
 
 		try {
 			if (_mapId != null) {
@@ -52,6 +67,10 @@ public class BeanShellTask extends Task {
 		catch (EvalError ee) {
 			throw new BuildException(ee);
 		}
+	}
+
+	public void setClasspathRef(String classpathRef) {
+		_classpathRef = classpathRef;
 	}
 
 	public void setMapId(String mapId) {
@@ -73,6 +92,7 @@ public class BeanShellTask extends Task {
 	private static final Map<String, Map<String, Object>> _maps =
 		new HashMap<String, Map<String, Object>>();
 
+	private String _classpathRef;
 	private String _mapId;
 	private String _text;
 
