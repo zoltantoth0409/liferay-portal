842ebedd9e4e385399119ca2c8857b064b7a624e
Gregory Amerson <gregory.amerson@liferay.com>
2018-06-21 12:08:50 -0500

    LPS-82491 don't add source set, java plugin does that

--- modules/sdk/gradle-plugins-target-platform/src/main/java/com/liferay/gradle/plugins/target/platform/TargetPlatformIDEPlugin.java
+++ modules/sdk/gradle-plugins-target-platform/src/main/java/com/liferay/gradle/plugins/target/platform/TargetPlatformIDEPlugin.java
@@ -35,13 +35,10 @@ import java.util.Set;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.UnknownDomainObjectException;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.artifacts.DependencySet;
-import org.gradle.api.file.FileCollection;
 import org.gradle.api.plugins.JavaBasePlugin;
-import org.gradle.api.tasks.SourceSet;
 import org.gradle.plugins.ide.eclipse.EclipsePlugin;
 import org.gradle.plugins.ide.eclipse.model.EclipseClasspath;
 import org.gradle.plugins.ide.eclipse.model.EclipseModel;
@@ -262,21 +259,6 @@ public class TargetPlatformIDEPlugin implements Plugin<Project> {
 		scopes.put(generatedIdeaScope.name(), providedScope);
 
 		ideaModule.setScopes(scopes);
-
-		SourceSet mainSourceSet = null;
-
-		try {
-			mainSourceSet = GradleUtil.getSourceSet(
-				project, SourceSet.MAIN_SOURCE_SET_NAME);
-		}
-		catch (UnknownDomainObjectException udoe) {
-			mainSourceSet = GradleUtil.addSourceSet(
-				project, SourceSet.MAIN_SOURCE_SET_NAME);
-		}
-
-		FileCollection compileClasspath = mainSourceSet.getCompileClasspath();
-
-		compileClasspath.plus(targetPlatformIDEConfiguration);
 	}
 
 }
