d161858f7abf3b56b34d2373b7048969529ee6bc
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-06-21 11:35:06 +0200

    LPS-82444 Staging Handlers for asset display pages

---
+++ modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/internal/exportimport/data/handler/AssetDisplayPageStagedModelDataHandler.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.display.page.internal.exportimport.data.handler;
+
+import com.liferay.asset.display.page.model.AssetDisplayPageEntry;
+import com.liferay.exportimport.data.handler.base.BaseStagedModelDataHandler;
+import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.portal.kernel.xml.Element;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = StagedModelDataHandler.class)
+public class AssetDisplayPageStagedModelDataHandler
+	extends BaseStagedModelDataHandler<AssetDisplayPageEntry> {
+
+	public static final String[] CLASS_NAMES =
+		{AssetDisplayPageEntry.class.getName()};
+
+	@Override
+	public String[] getClassNames() {
+		return CLASS_NAMES;
+	}
+
+	@Override
+	protected void doExportStagedModel(
+			PortletDataContext portletDataContext,
+			AssetDisplayPageEntry assetDisplayPageEntry)
+		throws Exception {
+
+		Element assetDisplayPageElement =
+			portletDataContext.getExportDataElement(assetDisplayPageEntry);
+
+		portletDataContext.addClassedModel(
+			assetDisplayPageElement,
+			ExportImportPathUtil.getModelPath(assetDisplayPageEntry),
+			assetDisplayPageEntry);
+	}
+
+	@Override
+	protected void doImportStagedModel(
+			PortletDataContext portletDataContext,
+			AssetDisplayPageEntry assetDisplayPageEntry)
+		throws Exception {
+
+		AssetDisplayPageEntry importedAssetDisplayPageEntry =
+			(AssetDisplayPageEntry)assetDisplayPageEntry.clone();
+
+		importedAssetDisplayPageEntry.setGroupId(
+			portletDataContext.getScopeGroupId());
+
+		AssetDisplayPageEntry existingAssetDisplayPageEntry =
+			_stagedModelRepository.fetchStagedModelByUuidAndGroupId(
+				assetDisplayPageEntry.getUuid(),
+				portletDataContext.getScopeGroupId());
+
+		if ((existingAssetDisplayPageEntry == null) ||
+			!portletDataContext.isDataStrategyMirror()) {
+
+			importedAssetDisplayPageEntry =
+				_stagedModelRepository.addStagedModel(
+					portletDataContext, importedAssetDisplayPageEntry);
+		}
+		else {
+			importedAssetDisplayPageEntry.setAssetDisplayPageEntryId(
+				existingAssetDisplayPageEntry.getAssetDisplayPageEntryId());
+			importedAssetDisplayPageEntry.setClassPK(
+				existingAssetDisplayPageEntry.getClassPK());
+			importedAssetDisplayPageEntry.setLayoutPageTemplateEntryId(
+				existingAssetDisplayPageEntry.getLayoutPageTemplateEntryId());
+
+			importedAssetDisplayPageEntry =
+				_stagedModelRepository.updateStagedModel(
+					portletDataContext, importedAssetDisplayPageEntry);
+		}
+
+		portletDataContext.importClassedModel(
+			assetDisplayPageEntry, importedAssetDisplayPageEntry);
+	}
+
+	@Override
+	protected StagedModelRepository<AssetDisplayPageEntry>
+		getStagedModelRepository() {
+
+		return _stagedModelRepository;
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.asset.display.page.model.AssetDisplayPageEntry)",
+		unbind = "-"
+	)
+	private StagedModelRepository<AssetDisplayPageEntry> _stagedModelRepository;
+
+}
---
+++ modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/internal/exportimport/staged/model/repository/AssetDisplayPageStagedModelRepository.java
@@ -0,0 +1,161 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.display.page.internal.exportimport.staged.model.repository;
+
+import com.liferay.asset.display.page.model.AssetDisplayPageEntry;
+import com.liferay.asset.display.page.service.AssetDisplayPageEntryLocalService;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.StagedModelModifiedDateComparator;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepositoryHelper;
+import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.asset.display.page.model.AssetDisplayPageEntry",
+	service = StagedModelRepository.class
+)
+public class AssetDisplayPageStagedModelRepository
+	implements StagedModelRepository<AssetDisplayPageEntry> {
+
+	@Override
+	public AssetDisplayPageEntry addStagedModel(
+			PortletDataContext portletDataContext,
+			AssetDisplayPageEntry assetDisplayPageEntry)
+		throws PortalException {
+
+		long userId = portletDataContext.getUserId(
+			assetDisplayPageEntry.getUserUuid());
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			assetDisplayPageEntry);
+
+		if (portletDataContext.isDataStrategyMirror()) {
+			serviceContext.setUuid(assetDisplayPageEntry.getUuid());
+		}
+
+		return _assetDisplayPageEntryLocalService.addAssetDisplayPageEntry(
+			userId, serviceContext.getScopeGroupId(),
+			assetDisplayPageEntry.getClassNameId(),
+			assetDisplayPageEntry.getClassPK(),
+			assetDisplayPageEntry.getLayoutPageTemplateEntryId(),
+			assetDisplayPageEntry.getType(), serviceContext);
+	}
+
+	@Override
+	public void deleteStagedModel(AssetDisplayPageEntry assetDisplayPageEntry)
+		throws PortalException {
+
+		_assetDisplayPageEntryLocalService.deleteAssetDisplayPageEntry(
+			assetDisplayPageEntry);
+	}
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException {
+
+		AssetDisplayPageEntry assetDisplayPageEntry =
+			fetchStagedModelByUuidAndGroupId(uuid, groupId);
+
+		if (assetDisplayPageEntry != null) {
+			deleteStagedModel(assetDisplayPageEntry);
+		}
+	}
+
+	@Override
+	public void deleteStagedModels(PortletDataContext portletDataContext)
+		throws PortalException {
+	}
+
+	@Override
+	public AssetDisplayPageEntry fetchMissingReference(
+		String uuid, long groupId) {
+
+		return (AssetDisplayPageEntry)_stagedModelRepositoryHelper.
+			fetchMissingReference(uuid, groupId, this);
+	}
+
+	@Override
+	public AssetDisplayPageEntry fetchStagedModelByUuidAndGroupId(
+		String uuid, long groupId) {
+
+		return _assetDisplayPageEntryLocalService.
+			fetchAssetDisplayPageEntryByUuidAndGroupId(uuid, groupId);
+	}
+
+	@Override
+	public List<AssetDisplayPageEntry> fetchStagedModelsByUuidAndCompanyId(
+		String uuid, long companyId) {
+
+		return _assetDisplayPageEntryLocalService.
+			getAssetDisplayPageEntriesByUuidAndCompanyId(
+				uuid, companyId, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				new StagedModelModifiedDateComparator<>());
+	}
+
+	@Override
+	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
+		PortletDataContext portletDataContext) {
+
+		return _assetDisplayPageEntryLocalService.
+			getExportActionableDynamicQuery(portletDataContext);
+	}
+
+	@Override
+	public AssetDisplayPageEntry getStagedModel(long id)
+		throws PortalException {
+
+		return _assetDisplayPageEntryLocalService.getAssetDisplayPageEntry(id);
+	}
+
+	@Override
+	public AssetDisplayPageEntry saveStagedModel(
+			AssetDisplayPageEntry assetDisplayPageEntry)
+		throws PortalException {
+
+		return _assetDisplayPageEntryLocalService.updateAssetDisplayPageEntry(
+			assetDisplayPageEntry);
+	}
+
+	@Override
+	public AssetDisplayPageEntry updateStagedModel(
+			PortletDataContext portletDataContext,
+			AssetDisplayPageEntry assetDisplayPageEntry)
+		throws PortalException {
+
+		return _assetDisplayPageEntryLocalService.updateAssetDisplayPageEntry(
+			assetDisplayPageEntry);
+	}
+
+	@Reference
+	private AssetDisplayPageEntryLocalService
+		_assetDisplayPageEntryLocalService;
+
+	@Reference
+	private StagedModelRepositoryHelper _stagedModelRepositoryHelper;
+
+}
