89f3e6f40e0587122ebb922d76c2a75f12375c6a
Pablo Molina <contact@pablomolina.me>
2018-06-22 10:55:27 +0200

    LPS-82779 Add Object.entries polyfill

--- modules/apps/frontend-compatibility/frontend-compatibility-ie/src/main/java/com/liferay/frontend/compatibility/ie/internal/servlet/taglib/IETopHeadDynamicInclude.java
+++ modules/apps/frontend-compatibility/frontend-compatibility-ie/src/main/java/com/liferay/frontend/compatibility/ie/internal/servlet/taglib/IETopHeadDynamicInclude.java
@@ -72,7 +72,7 @@ public class IETopHeadDynamicInclude extends BaseDynamicInclude {
 
 	private static final String[] _FILE_NAMES = {
 		"array.find.js", "array.from.js", "fetch.js", "object.assign.js",
-		"object.values.js", "url.search.params.js"
+		"object.entries.js", "object.values.js", "url.search.params.js"
 	};
 
 	@Reference
