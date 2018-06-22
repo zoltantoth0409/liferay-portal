1a238e09d8b7376da25e8367760d9d5f88dcc028
Dylan Rebelak <dylan.rebelak@liferay.com>
2018-06-22 14:53:24 -0500

    COMMERCE-API Remove unused constants

--- modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/search/CPInstanceIndexer.java
+++ modules/dxp/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/search/CPInstanceIndexer.java
@@ -69,11 +69,6 @@ public class CPInstanceIndexer extends BaseIndexer<CPInstance> {
 	public static final String FIELD_EXTERNAL_REFERENCE_CODE =
 		"externalReferenceCode";
 
-	public static final String FIELD_GTIN = "gtin";
-
-	public static final String FIELD_MANUFACTURER_PART_NUMBER =
-		"manufacturerPartNumber";
-
 	public static final String FIELD_PURCHASABLE = "purchasable";
 
 	public static final String FIELD_SKU = "sku";
