6117730b67d4fb61fb570b3adc99728d75c979c4
Bruno Basto <bruno.basto@liferay.com>
2018-06-25 16:05:49 -0300

    LPS-82418 Make sure to send Analytics events for Document Library field

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/field_events_support.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/field_events_support.js
@@ -69,8 +69,48 @@ AUI.add(
 			_bindEvents: function() {
 				var instance = this;
 
-				instance.bindInputEvent('blur', instance._onInputBlur);
+				instance.bindInputEvent('blur', instance._onInputBlur, true);
+				instance.bindInputEvent('focus', instance._onInputFocus, true);
 				instance.bindInputEvent(['input', 'change'], instance._onValueChange);
+			},
+
+			_fireBlurEvent: function() {
+				var instance = this;
+
+				var root = instance.getRoot();
+
+				if (root) {
+					var now = new Date();
+
+					Liferay.fire(
+						'ddmFieldBlur',
+						{
+							fieldName: instance.get('name'),
+							focusDuration: (now - (instance.get('fieldFocusDate') || now)),
+							formId: root.getFormId(),
+							page: root.getCurrentPage() - 1
+						}
+					);
+				}
+			},
+
+			_fireFocusEvent: function() {
+				var instance = this;
+
+				var root = instance.getRoot();
+
+				if (root) {
+					instance.set('fieldFocusDate', new Date());
+
+					Liferay.fire(
+						'ddmFieldFocus',
+						{
+							fieldName: instance.get('name'),
+							formId: root.getFormId(),
+							page: root.getCurrentPage() - 1
+						}
+					);
+				}
 			},
 
 			_fireStartedFillingEvent: function() {
@@ -82,10 +122,14 @@ AUI.add(
 					var root = instance.getRoot();
 
 					if (root) {
-						Liferay.fire("ddmFieldStartedFilling", {
-							fieldName: instance.get("fieldName"),
-							formId: root.getFormId()
-						});
+						Liferay.fire(
+							'ddmFieldStartedFilling',
+							{
+								fieldName: instance.get('fieldName'),
+								formId: root.getFormId(),
+								page: root.getCurrentPage() || 1
+							}
+						);
 					}
 				}
 			},
@@ -100,6 +144,22 @@ AUI.add(
 						field: instance
 					}
 				);
+
+				instance._fireBlurEvent();
+			},
+
+			_onInputFocus: function(event) {
+				var instance = this;
+
+				instance.fire(
+					'focus',
+					{
+						domEvent: event,
+						field: instance
+					}
+				);
+
+				instance._fireFocusEvent();
 			},
 
 			_onValueChange: function(event) {
