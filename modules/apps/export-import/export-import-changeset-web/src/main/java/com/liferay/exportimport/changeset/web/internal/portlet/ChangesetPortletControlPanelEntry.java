d969c0cd16275a407205f774cfb1f345ddc67882
Máté Thurzó <mate.thurzo@liferay.com>
2018-06-25 14:13:56 +0200

    LPS-82564 Even though portlet is hidden create the control panel entry to be able to control access to the portlet's features

---
+++ modules/apps/export-import/export-import-changeset-web/src/main/java/com/liferay/exportimport/changeset/web/internal/portlet/ChangesetPortletControlPanelEntry.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.changeset.web.internal.portlet;
+
+import com.liferay.exportimport.changeset.constants.ChangesetPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.BaseControlPanelEntry;
+import com.liferay.portal.kernel.portlet.ControlPanelEntry;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Mate Thurzo
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + ChangesetPortletKeys.CHANGESET,
+	service = ControlPanelEntry.class
+)
+public class ChangesetPortletControlPanelEntry extends BaseControlPanelEntry {
+
+	@Override
+	protected boolean hasAccessPermissionExplicitlyGranted(
+			PermissionChecker permissionChecker, Group group, Portlet portlet)
+		throws PortalException {
+
+		if (GroupPermissionUtil.contains(
+				permissionChecker, group,
+				ActionKeys.EXPORT_IMPORT_PORTLET_INFO)) {
+
+			return true;
+		}
+
+		return super.hasAccessPermissionExplicitlyGranted(
+			permissionChecker, group, portlet);
+	}
+
+}
