0f175b89927168b86a402dfd6e32538584f0d156
Bruno Basto <bruno.basto@liferay.com>
2018-06-25 15:29:26 -0300

    LPS-75815 Create bundle for forms analytics

---
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-form-analytics/src/main/java/com/liferay/dynamic/data/mapping/form/analytics/internal/servlet/taglib/DDMFormAnalyticsTopHeadJSPDynamicInclude.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.analytics.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseJSPDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.aui.ScriptData;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Leonardo Barros
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class DDMFormAnalyticsTopHeadJSPDynamicInclude
+	extends BaseJSPDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		ScriptData scriptData = new ScriptData();
+
+		scriptData.append(
+			null, _TMPL_CONTENT, null, ScriptData.ModulesType.AUI);
+
+		scriptData.writeTo(response.getWriter());
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"/html/common/themes/top_head.jsp#post");
+	}
+
+	@Override
+	protected String getJspPath() {
+		return null;
+	}
+
+	@Override
+	protected Log getLog() {
+		return _log;
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.dynamic.data.mapping.form.analytics)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final String _TMPL_CONTENT = StringUtil.read(
+		DDMFormAnalyticsTopHeadJSPDynamicInclude.class,
+		"/META-INF/resources/form_analytics.tmpl");
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMFormAnalyticsTopHeadJSPDynamicInclude.class);
+
+}
