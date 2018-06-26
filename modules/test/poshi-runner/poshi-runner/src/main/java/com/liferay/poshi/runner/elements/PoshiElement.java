6a4f6548dcec2e2c14000f3e5707ce1ae4bde4eb
Kenji Heigel <kenji.heigel@liferay.com>
2018-06-26 12:30:51 -0700

    LRQA-41899 Balance check for single quotes

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/PoshiElement.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/PoshiElement.java
@@ -435,7 +435,7 @@ public abstract class PoshiElement
 					continue;
 				}
 
-				if (topCodeBoundary == '\"') {
+				if ((topCodeBoundary == '\"') || (topCodeBoundary == '\'')) {
 					continue;
 				}
 			}
@@ -742,6 +742,7 @@ public abstract class PoshiElement
 	private static final Pattern _varInvocationAssignmentStatementPattern;
 
 	static {
+		_codeBoundariesMap.put('\'', '\'');
 		_codeBoundariesMap.put('\"', '\"');
 		_codeBoundariesMap.put('(', ')');
 		_codeBoundariesMap.put('{', '}');
