43675149f4207c863c8a2e8e20efddd496b641aa
jpince <jason.pince@liferay.com>
2018-06-27 14:34:25 -0700

    LRQA-41941 Temporarily ignore TokenExpeditionTest.java

--- modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/TokenExpeditionTest.java
+++ modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/TokenExpeditionTest.java
@@ -36,12 +36,14 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Carlos Sierra Andrés
  */
+@Ignore
 @RunAsClient
 @RunWith(Arquillian.class)
 public class TokenExpeditionTest extends BaseClientTestCase {
