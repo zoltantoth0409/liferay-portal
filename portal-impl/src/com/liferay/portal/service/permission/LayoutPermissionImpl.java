9642f60cbceaee6f38132524bfa18288701cbd9b
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-06-27 10:50:16 +0200

    LPS-82952 Limit the check only to VIEW action as was originally intended

--- portal-impl/src/com/liferay/portal/service/permission/LayoutPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/LayoutPermissionImpl.java
@@ -344,6 +344,14 @@ public class LayoutPermissionImpl
 
 		Group group = GroupLocalServiceUtil.getGroup(layout.getGroupId());
 
+		if (group.isControlPanel() && layout.isTypeControlPanel()) {
+			if (!permissionChecker.isSignedIn()) {
+				return false;
+			}
+
+			return true;
+		}
+
 		// Inactive sites are not viewable
 
 		if (!GroupLocalServiceUtil.isLiveGroupActive(group)) {
@@ -517,14 +525,6 @@ public class LayoutPermissionImpl
 			if (!layoutTypeController.isCheckLayoutViewPermission()) {
 				return true;
 			}
-		}
-
-		if (layout.isTypeControlPanel()) {
-			if (!permissionChecker.isSignedIn()) {
-				return false;
-			}
-
-			return true;
 		}
 
 		if (actionId.equals(ActionKeys.CUSTOMIZE) &&
