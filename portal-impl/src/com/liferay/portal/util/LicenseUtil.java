56ba68edea004384c913c5e4a0a1acc4c80f6c3f
shuyangzhou <shuyang.zhou@liferay.com>
2018-06-27 14:41:55 -0700

    LPS-83029 Base on the usage, failure of initializing _symmetricKey will lead to NPEs at runtime, so basically LicenseUtil should not be considered as initialized without it.

--- portal-impl/src/com/liferay/portal/util/LicenseUtil.java
+++ portal-impl/src/com/liferay/portal/util/LicenseUtil.java
@@ -722,7 +722,7 @@ public class LicenseUtil {
 				encryptedSymmetricKey);
 		}
 		catch (Exception e) {
-			_log.error(e, e);
+			throw new ExceptionInInitializerError(e);
 		}
 
 		_ipAddresses = _getIPAddresses();
