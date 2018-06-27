385f1befd6ee5f862c9373d90551519897d9e1f8
Chema Balsas <jose.balsas@liferay.com>
2018-06-27 18:15:18 +0200

    LPS-83719 Updates logo

--- modules/apps/frontend-theme-porygon/frontend-theme-porygon-site-initializer/src/main/java/com/liferay/frontend/theme/porygon/site/initializer/internal/PorygonSiteInitializer.java
+++ modules/apps/frontend-theme-porygon/frontend-theme-porygon-site-initializer/src/main/java/com/liferay/frontend/theme/porygon/site/initializer/internal/PorygonSiteInitializer.java
@@ -15,13 +15,34 @@
 package com.liferay.frontend.theme.porygon.site.initializer.internal;
 
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutSetLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.site.exception.InitializationException;
 import com.liferay.site.initializer.SiteInitializer;
+
+import java.io.InputStream;
+
+import java.net.URL;
 
 import java.util.Locale;
 
 import javax.servlet.ServletContext;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -58,6 +79,16 @@ public class PorygonSiteInitializer implements SiteInitializer {
 
 	@Override
 	public void initialize(long groupId) throws InitializationException {
+		try {
+			ServiceContext serviceContext = _createServiceContext(groupId);
+
+			_updateLogo(serviceContext);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new InitializationException(e);
+		}
 	}
 
 	@Override
@@ -65,7 +96,65 @@ public class PorygonSiteInitializer implements SiteInitializer {
 		return true;
 	}
 
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	private ServiceContext _createServiceContext(long groupId)
+		throws PortalException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+
+		Group group = _groupLocalService.getGroup(groupId);
+
+		serviceContext.setCompanyId(group.getCompanyId());
+
+		User user = _userLocalService.getUser(PrincipalThreadLocal.getUserId());
+
+		Locale locale = LocaleUtil.getSiteDefault();
+
+		serviceContext.setLanguageId(LanguageUtil.getLanguageId(locale));
+
+		serviceContext.setScopeGroupId(groupId);
+		serviceContext.setUserId(user.getUserId());
+		serviceContext.setTimeZone(user.getTimeZone());
+
+		return serviceContext;
+	}
+
+	private void _updateLogo(ServiceContext serviceContext) throws Exception {
+		URL url = _bundle.getEntry(_PATH + "/images/logo.png");
+
+		byte[] bytes = null;
+
+		try (InputStream is = url.openStream()) {
+			bytes = FileUtil.getBytes(is);
+		}
+
+		_layoutSetLocalService.updateLogo(
+			serviceContext.getScopeGroupId(), false, true, bytes);
+	}
+
+	private static final String _PATH =
+		"com/liferay/frontend/theme/porygon/site/initializer/internal" +
+			"/dependencies";
+
 	private static final String _THEME_NAME = "Porygon";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PorygonSiteInitializer.class);
+
+	private Bundle _bundle;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private LayoutSetLocalService _layoutSetLocalService;
 
 	@Reference
 	private Portal _portal;
@@ -74,5 +163,8 @@ public class PorygonSiteInitializer implements SiteInitializer {
 		target = "(osgi.web.symbolicname=com.liferay.frontend.theme.porygon.site.initializer)"
 	)
 	private ServletContext _servletContext;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
