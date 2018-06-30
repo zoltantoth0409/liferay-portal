1462a1203b2159c46ef1c248826b945c81e0c1ba
André de Oliveira <andre.oliveira@liferay.com>
2018-06-29 22:22:07 -0700

    LPS-80820 portal-search-web: FolderSearcher: list of known folder classes (New Indexers fail "indexer instanceof FolderIndexer")
    - FolderSearcher moved from portal-kernel to portal-search-web
    
    https://issues.liferay.com/browse/LPS-80820

---
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/FolderSearcher.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.facet.display.context;
+
+import com.liferay.portal.kernel.search.BaseSearcher;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+/**
+ * @author Eduardo Garcia
+ * @author André de Oliveira
+ */
+public class FolderSearcher extends BaseSearcher {
+
+	public FolderSearcher() {
+		setDefaultSelectedFieldNames(Field.TITLE, Field.UID);
+		setFilterSearch(true);
+		setPermissionAware(true);
+	}
+
+	@Override
+	public String[] getSearchClassNames() {
+		return _FOLDER_CLASS_NAMES;
+	}
+
+	@Override
+	protected BooleanQuery createFullQuery(
+			BooleanFilter booleanFilter, SearchContext searchContext)
+		throws Exception {
+
+		TermsFilter termsFilter = new TermsFilter(Field.ENTRY_CLASS_PK);
+
+		termsFilter.addValues(
+			ArrayUtil.toStringArray(searchContext.getFolderIds()));
+
+		booleanFilter.add(termsFilter, BooleanClauseOccur.MUST);
+
+		return super.createFullQuery(booleanFilter, searchContext);
+	}
+
+	private static final String[] _FOLDER_CLASS_NAMES = {
+		"com.liferay.bookmarks.model.BookmarksFolder",
+		"com.liferay.document.library.kernel.model.DLFolder",
+		"com.liferay.journal.model.JournalFolder"
+	};
+
+}
