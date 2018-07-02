82ffd43609ad7e31f2826c77e1f3e2266adce607
Hugo Huijser <hugo.huijser@liferay.com>
2018-07-02 11:33:58 +0800

    LPS-82828 Manually change files that are skipped by SF

--- portal-impl/src/com/liferay/portal/service/ServiceContextAdvice.java
+++ portal-impl/src/com/liferay/portal/service/ServiceContextAdvice.java
@@ -58,7 +58,7 @@ public class ServiceContextAdvice extends ChainableMethodAdvice {
 	}
 
 	/**
-	 * @deprecated As of Judson, with no direct replacement
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
 	 */
 	@Deprecated
 	protected boolean hasServiceContextParameter(Method method) {
@@ -74,7 +74,7 @@ public class ServiceContextAdvice extends ChainableMethodAdvice {
 	}
 
 	/**
-	 * @deprecated As of Judson, with no direct replacement
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
 	 */
 	@Deprecated
 	protected boolean pushServiceContext(MethodInvocation methodInvocation) {
