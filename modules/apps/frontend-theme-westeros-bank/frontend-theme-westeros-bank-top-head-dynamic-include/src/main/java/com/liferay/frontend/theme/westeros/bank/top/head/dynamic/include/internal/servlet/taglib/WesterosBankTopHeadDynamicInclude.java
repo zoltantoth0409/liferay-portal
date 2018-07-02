bc62e72aaefdd2f23945b8514df0f9e53c1ce5bb
Chema Balsas <jose.balsas@liferay.com>
2018-07-02 13:37:13 +0200

    LPS-81238 Includes westeros bank fragments_editor css for improved fidelity when editing sites

---
+++ modules/apps/frontend-theme-westeros-bank/frontend-theme-westeros-bank-top-head-dynamic-include/src/main/java/com/liferay/frontend/theme/westeros/bank/top/head/dynamic/include/internal/servlet/taglib/WesterosBankTopHeadDynamicInclude.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.theme.westeros.bank.top.head.dynamic.include.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResourcesUtil;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Chema Balsas
+ */
+@Component(immediate = true)
+public class WesterosBankTopHeadDynamicInclude implements DynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		long themeLastModified = PortalWebResourcesUtil.getLastModified(
+			PortalWebResourceConstants.RESOURCE_TYPE_THEME_CONTRIBUTOR);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String portalCDNURL = themeDisplay.getCDNBaseURL();
+
+		String resourceURL = "/o/westeros-bank-theme/css/fragments_editor.css";
+
+		String staticResourceURL = _portal.getStaticResourceURL(
+			request,
+			portalCDNURL.concat(_portal.getPathProxy()).concat(resourceURL),
+            themeLastModified);
+
+        PrintWriter printWriter = response.getWriter();
+
+		printWriter.write("<link data-senna-track=\"permanent\" href=\"");
+		printWriter.write(staticResourceURL);
+		printWriter.write("\" rel=\"stylesheet\" type = \"text/css\" />\n");
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"/html/common/themes/top_head.jsp#post");
+	}
+
+	@Reference private Portal _portal;
+
+}
