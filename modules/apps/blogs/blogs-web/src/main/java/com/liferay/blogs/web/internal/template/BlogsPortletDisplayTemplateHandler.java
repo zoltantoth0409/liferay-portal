1b9ae20b72652b02732ce8a43d2084b534b94e20
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-07-03 16:31:49 +0200

    LPS-82646 Update schema version in template handler

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/template/BlogsPortletDisplayTemplateHandler.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/template/BlogsPortletDisplayTemplateHandler.java
@@ -152,7 +152,7 @@ public class BlogsPortletDisplayTemplateHandler
 	private Portal _portal;
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.blogs.service)(release.schema.version=1.1.0))"
+		target = "(&(release.bundle.symbolic.name=com.liferay.blogs.service)(release.schema.version=1.1.1))"
 	)
 	private Release _release;
 
