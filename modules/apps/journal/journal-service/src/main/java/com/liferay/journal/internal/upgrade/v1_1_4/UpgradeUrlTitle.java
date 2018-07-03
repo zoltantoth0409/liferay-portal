21d6d952a166eb1e9fabe54fcf2f904e974b7666
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-07-03 14:37:27 +0200

    LPS-82646 Journal - Upgrade process for urlTitle column length

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v1_1_4/UpgradeUrlTitle.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.upgrade.v1_1_4;
+
+import com.liferay.journal.internal.upgrade.v1_1_4.util.JournalArticleTable;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class UpgradeUrlTitle extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		alter(
+			JournalArticleTable.class,
+			new AlterColumnType("urlTitle", "VARCHAR(255) null"));
+	}
+
+}
