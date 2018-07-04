872fc4e3ff582e6b9de5bfa33440fcbfbda54251
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-07-04 10:18:10 +0200

    LPS-80971 Create base class, since the logic for tags cloud and tags navigation is the same (also the mapping). Use code from tags navigation without any other changes

---
+++ modules/apps/asset/asset-tags-navigation-web/src/main/java/com/liferay/asset/tags/navigation/web/internal/portlet/route/BaseAssetTagsFriendlyURLMapper.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.navigation.web.internal.portlet.route;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.portlet.DefaultFriendlyURLMapper;
+import com.liferay.portal.kernel.portlet.LiferayPortletURL;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class BaseAssetTagsFriendlyURLMapper extends DefaultFriendlyURLMapper {
+
+	@Override
+	public String buildPath(LiferayPortletURL liferayPortletURL) {
+		Map<String, String> routeParameters = new HashMap<>();
+
+		buildRouteParameters(liferayPortletURL, routeParameters);
+
+		if (routeParameters.get("resetCur") == null) {
+			routeParameters.put("resetCur", Boolean.TRUE.toString());
+		}
+
+		String friendlyURLPath = router.parametersToUrl(routeParameters);
+
+		if (Validator.isNull(friendlyURLPath)) {
+			return null;
+		}
+
+		addParametersIncludedInPath(liferayPortletURL, routeParameters);
+
+		friendlyURLPath = StringPool.SLASH.concat(getMapping()).concat(
+			friendlyURLPath);
+
+		return friendlyURLPath;
+	}
+
+	@Override
+	public String getMapping() {
+		return _MAPPING;
+	}
+
+	private static final String _MAPPING = "tag";
+
+}
