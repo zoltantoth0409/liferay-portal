b640f80cd70adc4931dddeb8185304506834f45d
Preston-Crary <preston.crary@liferay.com>
2018-07-03 18:44:39 -0700

    LPS-83344 Only create one bundle tracker.

--- modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
+++ modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -1422,11 +1422,15 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 		frameworkStartLevel.setStartLevel(
 			PropsValues.MODULE_FRAMEWORK_BEGINNING_START_LEVEL);
 
-		for (final Bundle bundle : bundles.values()) {
-			if (_isFragmentBundle(bundle)) {
-				continue;
-			}
+		final Set<Bundle> bundleSet = new HashSet<>();
 
+		for (Bundle bundle : bundles.values()) {
+			if (!_isFragmentBundle(bundle)) {
+				bundleSet.add(bundle);
+			}
+		}
+
+		if (!bundleSet.isEmpty()) {
 			final CountDownLatch countDownLatch = new CountDownLatch(1);
 
 			BundleTracker<Void> bundleTracker = new BundleTracker<Void>(
@@ -1436,10 +1440,12 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 				public Void addingBundle(
 					Bundle trackedBundle, BundleEvent bundleEvent) {
 
-					if (trackedBundle == bundle) {
-						countDownLatch.countDown();
+					if (bundleSet.remove(trackedBundle)) {
+						if (bundleSet.isEmpty()) {
+							countDownLatch.countDown();
 
-						close();
+							close();
+						}
 					}
 
 					return null;
