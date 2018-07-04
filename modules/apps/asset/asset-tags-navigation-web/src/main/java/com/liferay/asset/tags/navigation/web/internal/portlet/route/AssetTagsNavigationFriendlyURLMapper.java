ae12c43dbb33f8ffca0081942f596ac518333c63
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-07-04 10:18:21 +0200

    LPS-80971 Use base class

--- modules/apps/asset/asset-tags-navigation-web/src/main/java/com/liferay/asset/tags/navigation/web/internal/portlet/route/AssetTagsNavigationFriendlyURLMapper.java
+++ modules/apps/asset/asset-tags-navigation-web/src/main/java/com/liferay/asset/tags/navigation/web/internal/portlet/route/AssetTagsNavigationFriendlyURLMapper.java
@@ -15,15 +15,7 @@
 package com.liferay.asset.tags.navigation.web.internal.portlet.route;
 
 import com.liferay.asset.tags.navigation.constants.AssetTagsNavigationPortletKeys;
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.portlet.DefaultFriendlyURLMapper;
 import com.liferay.portal.kernel.portlet.FriendlyURLMapper;
-
-import com.liferay.portal.kernel.portlet.LiferayPortletURL;
-import com.liferay.portal.kernel.util.Validator;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -38,37 +30,5 @@ import org.osgi.service.component.annotations.Component;
 	service = FriendlyURLMapper.class
 )
 public class AssetTagsNavigationFriendlyURLMapper
-	extends DefaultFriendlyURLMapper {
-
-	@Override
-	public String buildPath(LiferayPortletURL liferayPortletURL) {
-		Map<String, String> routeParameters = new HashMap<>();
-
-		buildRouteParameters(liferayPortletURL, routeParameters);
-
-		if (routeParameters.get("resetCur") == null) {
-			routeParameters.put("resetCur", Boolean.TRUE.toString());
-		}
-
-		String friendlyURLPath = router.parametersToUrl(routeParameters);
-
-		if (Validator.isNull(friendlyURLPath)) {
-			return null;
-		}
-
-		addParametersIncludedInPath(liferayPortletURL, routeParameters);
-
-		friendlyURLPath = StringPool.SLASH.concat(getMapping()).concat(
-			friendlyURLPath);
-
-		return friendlyURLPath;
-	}
-
-	@Override
-	public String getMapping() {
-		return _MAPPING;
-	}
-
-	private static final String _MAPPING = "tag";
-
+	extends BaseAssetTagsFriendlyURLMapper {
 }
