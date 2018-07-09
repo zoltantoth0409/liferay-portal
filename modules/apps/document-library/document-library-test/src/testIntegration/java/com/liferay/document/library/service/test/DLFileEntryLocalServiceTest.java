71d68c2fb4c2575543dc2d760f476cd88276b46a
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-07-09 12:01:42 +0200

    LPS-82717 Test that the filename is updated when the document is updated keeping the file version label

--- modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/service/test/DLFileEntryLocalServiceTest.java
+++ modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/service/test/DLFileEntryLocalServiceTest.java
@@ -58,6 +58,7 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.repository.LocalRepository;
 import com.liferay.portal.kernel.repository.RepositoryProviderUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -348,6 +349,40 @@ public class DLFileEntryLocalServiceTest {
 			StringPool.BLANK, StringPool.BLANK,
 			DLFileEntryTypeConstants.FILE_ENTRY_TYPE_ID_BASIC_DOCUMENT,
 			ddmFormValuesMap, null, inputStream, 0, serviceContext);
+	}
+
+	@Test
+	public void testFileNameUpdatedWhenUpdatingAFileEntryKeepingFileVersionLabel()
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		FileEntry fileEntry = DLAppLocalServiceUtil.addFileEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, StringPool.BLANK,
+			ContentTypes.TEXT_PLAIN, "FE1.exe", RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), (byte[])null, serviceContext);
+
+		Assert.assertEquals("FE1.exe", fileEntry.getFileName());
+
+		FileVersion fileVersion = fileEntry.getFileVersion();
+
+		Assert.assertEquals("FE1.exe", fileVersion.getFileName());
+
+		fileEntry = DLAppLocalServiceUtil.updateFileEntry(
+			TestPropsValues.getUserId(), fileEntry.getFileEntryId(), "FE2.txt",
+			ContentTypes.TEXT_PLAIN, "FE1.exe", fileEntry.getDescription(),
+			RandomTestUtil.randomString(), false,
+			RandomTestUtil.randomBytes(TikaSafeRandomizerBumper.INSTANCE),
+			serviceContext);
+
+		Assert.assertEquals("FE1.exe.txt", fileEntry.getFileName());
+
+		fileVersion = fileEntry.getFileVersion();
+
+		Assert.assertEquals("FE1.exe.txt", fileVersion.getFileName());
 	}
 
 	@Test
