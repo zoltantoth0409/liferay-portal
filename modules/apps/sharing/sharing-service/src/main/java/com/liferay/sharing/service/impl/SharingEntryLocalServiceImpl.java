412a071ef20d688adad757a20f42f8bf8cd751ef
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-07-10 19:02:43 +0200

    LPS-83339 SharingEntry service to keep track of users sharing content and actions

--- modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/service/impl/SharingEntryLocalServiceImpl.java
+++ modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/service/impl/SharingEntryLocalServiceImpl.java
@@ -14,29 +14,161 @@
 
 package com.liferay.sharing.service.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+import com.liferay.sharing.constants.SharingEntryActionKey;
+import com.liferay.sharing.exception.InvalidSharingEntryActionKeyException;
+import com.liferay.sharing.exception.InvalidSharingEntryUserException;
+import com.liferay.sharing.model.SharingEntry;
 import com.liferay.sharing.service.base.SharingEntryLocalServiceBaseImpl;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
- * The implementation of the sharing entry local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.sharing.service.SharingEntryLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see SharingEntryLocalServiceBaseImpl
- * @see com.liferay.sharing.service.SharingEntryLocalServiceUtil
+ * @author Sergio González
  */
 public class SharingEntryLocalServiceImpl
 	extends SharingEntryLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.sharing.service.SharingEntryLocalServiceUtil} to access the sharing entry local service.
-	 */
+	public SharingEntry addSharingEntry(
+			long fromUserId, long toUserId, String className, long classPK,
+			long groupId,
+			Collection<SharingEntryActionKey> sharingEntryActionKeys,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		_validateSharingEntryActionKeys(sharingEntryActionKeys);
+
+		_validateUsers(fromUserId, toUserId);
+
+		long sharingEntryId = counterLocalService.increment();
+
+		SharingEntry sharingEntry = sharingEntryPersistence.create(
+			sharingEntryId);
+
+		sharingEntry.setUuid(serviceContext.getUuid());
+
+		Group group = _groupLocalService.getGroup(groupId);
+
+		sharingEntry.setCompanyId(group.getCompanyId());
+
+		sharingEntry.setGroupId(groupId);
+		sharingEntry.setFromUserId(fromUserId);
+		sharingEntry.setToUserId(toUserId);
+		sharingEntry.setClassName(className);
+		sharingEntry.setClassPK(classPK);
+
+		Stream<SharingEntryActionKey> sharingEntryActionKeyStream =
+			sharingEntryActionKeys.stream();
+
+		sharingEntryActionKeyStream.map(
+			SharingEntryActionKey::getBitwiseVaue
+		).reduce(
+			(bitwiseValue1, bitwiseValue2) -> bitwiseValue1 | bitwiseValue2
+		).ifPresent(
+			actionIds -> sharingEntry.setActionIds(actionIds)
+		);
+
+		return sharingEntryPersistence.update(sharingEntry);
+	}
+
+	public int countSharingEntriesFromUserId(long fromUserId) {
+		return sharingEntryPersistence.countByFromUserId(fromUserId);
+	}
+
+	public int countSharingEntriesToUserId(long toUserId) {
+		return sharingEntryPersistence.countByToUserId(toUserId);
+	}
+
+	public SharingEntry deleteSharingEntry(
+			long toUserId, String className, long classPK)
+		throws PortalException {
+
+		SharingEntry sharingEntry = sharingEntryPersistence.findByTU_CN_PK(
+			toUserId, className, classPK);
+
+		return sharingEntryPersistence.remove(sharingEntry);
+	}
+
+	public List<SharingEntry> getSharingEntries(
+		String className, long classPK) {
+
+		return sharingEntryPersistence.findByCN_PK(className, classPK);
+	}
+
+	public List<SharingEntry> getSharingEntriesFromUserId(long fromUserId) {
+		return sharingEntryPersistence.findByFromUserId(fromUserId);
+	}
+
+	public List<SharingEntry> getSharingEntriesToUserId(long toUserId) {
+		return sharingEntryPersistence.findByToUserId(toUserId);
+	}
+
+	public List<SharingEntry> getSharingEntriesToUserId(
+		long toUserId, String className) {
+
+		return sharingEntryPersistence.findByTU_CN(toUserId, className);
+	}
+
+	public SharingEntry getSharingEntry(
+			long toUserId, String className, long classPK)
+		throws PortalException {
+
+		return sharingEntryPersistence.findByTU_CN_PK(
+			toUserId, className, classPK);
+	}
+
+	public boolean hasSharingPermission(
+		long toUserId, String className, long classPK,
+		SharingEntryActionKey sharingEntryActionKey) {
+
+		SharingEntry sharingEntry = sharingEntryPersistence.fetchByTU_CN_PK(
+			toUserId, className, classPK);
+
+		if (sharingEntry == null) {
+			return false;
+		}
+
+		long actionIds = sharingEntry.getActionIds();
+
+		if ((actionIds & sharingEntryActionKey.getBitwiseVaue()) != 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private void _validateSharingEntryActionKeys(
+			Collection<SharingEntryActionKey> sharedEntryActionKeys)
+		throws InvalidSharingEntryActionKeyException {
+
+		if (sharedEntryActionKeys.isEmpty()) {
+			throw new InvalidSharingEntryActionKeyException();
+		}
+
+		for (SharingEntryActionKey curSharingEntryActionKey :
+				sharedEntryActionKeys) {
+
+			if (curSharingEntryActionKey == null) {
+				throw new InvalidSharingEntryActionKeyException();
+			}
+		}
+	}
+
+	private void _validateUsers(long fromUserId, long toUserId)
+		throws InvalidSharingEntryUserException {
+
+		if (fromUserId == toUserId) {
+			throw new InvalidSharingEntryUserException();
+		}
+	}
+
+	@ServiceReference(type = GroupLocalService.class)
+	private GroupLocalService _groupLocalService;
 
 }
