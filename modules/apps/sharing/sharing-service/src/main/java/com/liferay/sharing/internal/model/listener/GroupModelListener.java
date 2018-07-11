110b23d8d014bd2c33c235757bd524fbb4503723
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-07-11 14:45:55 +0200

    LPS-83374 When deleting a group delete every sharing entry related to that group

---
+++ modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/internal/model/listener/GroupModelListener.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.internal.model.listener;
+
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.sharing.service.SharingEntryLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class GroupModelListener extends BaseModelListener<Group> {
+
+	@Override
+	public void onBeforeRemove(Group group) {
+		_sharingEntryLocalService.deleteGroupSharingEntries(group.getGroupId());
+	}
+
+	@Reference
+	private SharingEntryLocalService _sharingEntryLocalService;
+
+}
