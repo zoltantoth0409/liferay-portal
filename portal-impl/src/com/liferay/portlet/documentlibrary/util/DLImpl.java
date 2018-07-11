ddb1f66656462de41f55879c94a965e9d9093d63
Roberto Díaz <roberto.diaz@liferay.com>
2018-07-11 16:34:03 +0200

    LPS-76893 Create a new method in order to handle all the cases

--- portal-impl/src/com/liferay/portlet/documentlibrary/util/DLImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/util/DLImpl.java
@@ -463,7 +463,7 @@ public class DLImpl implements DL {
 	}
 
 	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
+	 * @deprecated As of Judson, (7.1.x), with no direct replacement
 	 */
 	@Deprecated
 	@Override
@@ -508,26 +508,33 @@ public class DLImpl implements DL {
 
 	@Override
 	public String getImagePreviewURL(
-			FileEntry fileEntry, FileVersion fileVersion,
-			ThemeDisplay themeDisplay)
-		throws Exception {
+		FileEntry fileEntry, FileVersion fileVersion,
+		ThemeDisplay themeDisplay) {
 
-		String previewQueryString = null;
+		return getImagePreviewURL(fileEntry, fileVersion, themeDisplay, null,
+			true, true);
+	}
+
+	@Override
+	public String getImagePreviewURL(
+		FileEntry fileEntry, FileVersion fileVersion, ThemeDisplay themeDisplay,
+		String queryString, boolean appendVersion, boolean absoluteURL) {
 
 		if (ImageProcessorUtil.isSupported(fileVersion.getMimeType())) {
-			previewQueryString = "&imagePreview=1";
+			queryString.concat("&imagePreview=1");
 		}
 		else if (PropsValues.DL_FILE_ENTRY_PREVIEW_ENABLED) {
 			if (PDFProcessorUtil.hasImages(fileVersion)) {
-				previewQueryString = "&previewFileIndex=1";
+				queryString.concat("&previewFileIndex=1");
 			}
 			else if (VideoProcessorUtil.hasVideo(fileVersion)) {
-				previewQueryString = "&videoThumbnail=1";
+				queryString.concat("&videoThumbnail=1");
 			}
 		}
 
 		return getImageSrc(
-			fileEntry, fileVersion, themeDisplay, previewQueryString);
+			fileEntry, fileVersion, themeDisplay, queryString, appendVersion,
+			absoluteURL);
 	}
 
 	@Override
@@ -700,7 +707,7 @@ public class DLImpl implements DL {
 	}
 
 	/**
-	 * @deprecated As of Wilberforce (7.0.x), replaced by {@link
+	 * @deprecated As of Wilberforce, (7.0.x), replaced by {@link
 	 *             #getThumbnailSrc(FileEntry, ThemeDisplay)}
 	 */
 	@Deprecated
@@ -715,7 +722,7 @@ public class DLImpl implements DL {
 	}
 
 	/**
-	 * @deprecated As of Wilberforce (7.0.x), replaced by {@link
+	 * @deprecated As of Wilberforce, (7.0.x), replaced by {@link
 	 *             #getThumbnailSrc(FileEntry, FileVersion, ThemeDisplay)}
 	 */
 	@Deprecated
@@ -995,7 +1002,7 @@ public class DLImpl implements DL {
 	}
 
 	/**
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
+	 * @deprecated As of Judson, (7.1.x), replaced by {@link
 	 *             com.liferay.document.library.web.internal.util.
 	 *             DLSubscriptionUtil#isSubscribedToFileEntryType(long, long,
 	 *             long, long)}
@@ -1017,7 +1024,7 @@ public class DLImpl implements DL {
 	}
 
 	/**
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
+	 * @deprecated As of Judson, (7.1.x), replaced by {@link
 	 *             com.liferay.document.library.web.internal.util.
 	 *             DLSubscriptionUtil#isSubscribedToFolder(long, long, long,
 	 *             long)}
@@ -1032,7 +1039,7 @@ public class DLImpl implements DL {
 	}
 
 	/**
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
+	 * @deprecated As of Judson, (7.1.x), replaced by {@link
 	 *             com.liferay.document.library.web.internal.util.
 	 *             DLSubscriptionUtil#isSubscribedToFolder(long, long, long,
 	 *             long, boolean)}
@@ -1182,15 +1189,23 @@ public class DLImpl implements DL {
 	}
 
 	protected String getImageSrc(
-			FileEntry fileEntry, FileVersion fileVersion,
-			ThemeDisplay themeDisplay, String queryString)
-		throws Exception {
+		FileEntry fileEntry, FileVersion fileVersion, ThemeDisplay themeDisplay,
+		String queryString) {
+
+		return getImageSrc(fileEntry, fileVersion, themeDisplay, queryString,
+			true, true);
+	}
+
+	protected String getImageSrc(
+		FileEntry fileEntry, FileVersion fileVersion, ThemeDisplay themeDisplay,
+		String queryString, boolean appendVersion, boolean absoluteURL) {
 
 		String thumbnailSrc = StringPool.BLANK;
 
 		if (Validator.isNotNull(queryString)) {
 			thumbnailSrc = getPreviewURL(
-				fileEntry, fileVersion, themeDisplay, queryString, true, true);
+				fileEntry, fileVersion, themeDisplay, queryString,
+				appendVersion, absoluteURL);
 		}
 
 		return thumbnailSrc;
