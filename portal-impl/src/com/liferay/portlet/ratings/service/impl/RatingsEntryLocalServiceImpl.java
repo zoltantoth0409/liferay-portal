2a3bc19991206886dca492614293a4e214ef59a8
Jonathan McCann <jonathan.mccann@liferay.com>
2018-07-12 14:41:10 -0700

    LPS-79819 If we are deleting a rating add a revoke social activity

--- portal-impl/src/com/liferay/portlet/ratings/service/impl/RatingsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/ratings/service/impl/RatingsEntryLocalServiceImpl.java
@@ -89,6 +89,21 @@ public class RatingsEntryLocalServiceImpl
 		stats.setAverageScore(averageScore);
 
 		ratingsStatsPersistence.update(stats);
+
+		// Social
+
+		AssetEntry assetEntry = assetEntryLocalService.fetchEntry(
+			className, classPK);
+
+		if (assetEntry != null) {
+			JSONObject extraDataJSONObject = JSONFactoryUtil.createJSONObject();
+
+			extraDataJSONObject.put("title", assetEntry.getTitle());
+
+			SocialActivityManagerUtil.addActivity(
+				userId, assetEntry, SocialActivityConstants.TYPE_REVOKE_VOTE,
+				extraDataJSONObject.toString(), 0);
+		}
 	}
 
 	@Override
