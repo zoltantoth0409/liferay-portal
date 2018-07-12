945f7f60179258f2bcd6ecdaf6cd4664fb5e0072
Rafael Praxedes <rafael.praxedes@liferay.com>
2018-07-12 13:37:16 -0300

    LPS-81340 Implement rowCheckers for formInstance and FieldSet entities

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/search/FormInstanceRowChecker.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.search;
+
+import com.liferay.dynamic.data.mapping.form.web.internal.security.permission.resource.DDMFormInstancePermission;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+
+import javax.portlet.PortletResponse;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class FormInstanceRowChecker extends EmptyOnClickRowChecker {
+
+	public FormInstanceRowChecker(PortletResponse portletResponse) {
+		super(portletResponse);
+	}
+
+	@Override
+	public boolean isDisabled(Object obj) {
+		DDMFormInstance formInstance = (DDMFormInstance)obj;
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		try {
+			if (!DDMFormInstancePermission.contains(
+					permissionChecker, formInstance, ActionKeys.DELETE)) {
+
+				return true;
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		return super.isDisabled(obj);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		FormInstanceRowChecker.class);
+
+}
