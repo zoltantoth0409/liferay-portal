6a01452faba4128a3d570fd93f9e2ea2906bf8b9
drewbrokke <drew.brokke@liferay.com>
2018-07-13 12:13:41 -0500

    LPS-79656 users-admin-web - checks for visible custom fields before rendering the section

---
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/CustomFieldsUtil.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.web.internal;
+
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.expando.kernel.model.ExpandoColumnConstants;
+import com.liferay.expando.kernel.util.ExpandoBridgeFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import java.util.Enumeration;
+
+/**
+ * @author Drew Brokke
+ */
+public class CustomFieldsUtil {
+
+	public static boolean hasVisibleCustomFields(
+		long companyId, Class<?> clazz) {
+
+		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
+			companyId, clazz.getName());
+
+		Enumeration<String> attributeNames = expandoBridge.getAttributeNames();
+
+		while (attributeNames.hasMoreElements()) {
+			String attributeName = attributeNames.nextElement();
+
+			UnicodeProperties properties = expandoBridge.getAttributeProperties(
+				attributeName);
+
+			if (!GetterUtil.getBoolean(
+					properties.get(ExpandoColumnConstants.PROPERTY_HIDDEN))) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+}
