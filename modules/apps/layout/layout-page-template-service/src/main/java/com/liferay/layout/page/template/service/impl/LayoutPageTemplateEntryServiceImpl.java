9fcdf2e47b2210ddaacecbefa425e80df40be995
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-07-16 14:42:14 +0200

    LPS-83477 Deprecate unused methods

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateEntryServiceImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateEntryServiceImpl.java
@@ -140,6 +140,10 @@ public class LayoutPageTemplateEntryServiceImpl
 		return layoutPageTemplateEntry;
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
+	 */
+	@Deprecated
 	@Override
 	public int getLayoutPageTemplateCollectionsCount(
 		long groupId, long layoutPageTemplateCollectionId) {
@@ -149,6 +153,10 @@ public class LayoutPageTemplateEntryServiceImpl
 			WorkflowConstants.STATUS_ANY);
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
+	 */
+	@Deprecated
 	@Override
 	public int getLayoutPageTemplateCollectionsCount(
 		long groupId, long layoutPageTemplateCollectionId, int status) {
@@ -162,6 +170,10 @@ public class LayoutPageTemplateEntryServiceImpl
 			groupId, layoutPageTemplateCollectionId, status);
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
+	 */
+	@Deprecated
 	@Override
 	public int getLayoutPageTemplateCollectionsCount(
 		long groupId, long layoutPageTemplateCollectionId, String name) {
@@ -171,6 +183,10 @@ public class LayoutPageTemplateEntryServiceImpl
 			WorkflowConstants.STATUS_ANY);
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
+	 */
+	@Deprecated
 	@Override
 	public int getLayoutPageTemplateCollectionsCount(
 		long groupId, long layoutPageTemplateCollectionId, String name,
