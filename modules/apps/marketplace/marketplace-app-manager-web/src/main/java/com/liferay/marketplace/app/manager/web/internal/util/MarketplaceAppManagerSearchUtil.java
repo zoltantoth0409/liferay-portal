a108c90b0aa3d3ca35fd7cee2f493ea1383cea8f
drewbrokke <drew.brokke@liferay.com>
2018-07-16 11:55:54 -0500

    LPS-86444 marketplace-app-manager-web - removes quotes from keywords string. Before, the quotes were preventing the terms from matching at all, so a quoted search would literally exclude the desired terms from the search results.  This way, the desired terms will at least be included, even if we don't actually do a literal search for the quoted string.

--- modules/apps/marketplace/marketplace-app-manager-web/src/main/java/com/liferay/marketplace/app/manager/web/internal/util/MarketplaceAppManagerSearchUtil.java
+++ modules/apps/marketplace/marketplace-app-manager-web/src/main/java/com/liferay/marketplace/app/manager/web/internal/util/MarketplaceAppManagerSearchUtil.java
@@ -16,7 +16,6 @@ package com.liferay.marketplace.app.manager.web.internal.util;
 
 import com.liferay.marketplace.app.manager.web.internal.constants.BundleConstants;
 import com.liferay.marketplace.app.manager.web.internal.constants.BundleStateConstants;
-import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -79,7 +78,12 @@ public class MarketplaceAppManagerSearchUtil {
 	}
 
 	protected static String getKeywordsRegex(String keywords) {
-		keywords = StringUtil.replace(keywords, CharPool.SPACE, CharPool.PIPE);
+		keywords = StringUtil.replace(
+			keywords,
+			new String[] {
+				StringPool.SPACE, StringPool.APOSTROPHE, StringPool.QUOTE
+			},
+			new String[] {StringPool.PIPE, StringPool.BLANK, StringPool.BLANK});
 
 		return StringPool.OPEN_PARENTHESIS + keywords +
 			StringPool.CLOSE_PARENTHESIS;
