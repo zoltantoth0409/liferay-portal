96e0d988c908a6d43999b1babe49009588ef8bb7
Neil Griffin <neil.griffin.scm@gmail.com>
2018-07-16 18:55:33 -0400

    LPS-81861 Portlet 3.0: Refactor portlet request parameter handling and portlet URL generation (ActionRequest)

--- portal-impl/src/com/liferay/portlet/internal/ActionRequestImpl.java
+++ portal-impl/src/com/liferay/portlet/internal/ActionRequestImpl.java
@@ -14,10 +14,24 @@
 
 package com.liferay.portlet.internal;
 
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.InvokerPortlet;
 import com.liferay.portal.kernel.portlet.LiferayActionRequest;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 import javax.portlet.ActionParameters;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletPreferences;
 import javax.portlet.PortletRequest;
+import javax.portlet.RenderParameters;
+import javax.portlet.WindowState;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Brian Wing Shun Chan
@@ -28,12 +42,66 @@ public class ActionRequestImpl
 
 	@Override
 	public ActionParameters getActionParameters() {
-		throw new UnsupportedOperationException();
+		return _actionParameters;
 	}
 
 	@Override
 	public String getLifecycle() {
 		return PortletRequest.ACTION_PHASE;
 	}
+
+	@Override
+	public void init(
+		HttpServletRequest request, Portlet portlet,
+		InvokerPortlet invokerPortlet, PortletContext portletContext,
+		WindowState windowState, PortletMode portletMode,
+		PortletPreferences preferences, long plid) {
+
+		super.init(
+			request, portlet, invokerPortlet, portletContext, windowState,
+			portletMode, preferences, plid);
+
+		Map<String, String[]> actionParameterMap = new LinkedHashMap<>();
+		Map<String, String[]> parameterMap = getParameterMap();
+		String portletNamespace = PortalUtil.getPortletNamespace(
+			getPortletName());
+		Map<String, String[]> servletRequestParameterMap =
+			request.getParameterMap();
+		RenderParameters renderParameters = getRenderParameters();
+
+		Set<String> renderParameterNames = renderParameters.getNames();
+
+		for (Map.Entry<String, String[]> mapEntry : parameterMap.entrySet()) {
+			String name = mapEntry.getKey();
+
+			// If the parameter name is not a public/private render parameter,
+			// then it is an action parameter. Also, if the parameter name is
+			// prefixed with the portlet namespace in the original request, then
+			// it is to be regarded as an action parameter (even if it has the
+			// same name as a public render parameter). See: TCK
+			// V3PortletParametersTests_SPEC11_3_getNames
+
+			if (!renderParameterNames.contains(name)) {
+				actionParameterMap.put(name, mapEntry.getValue());
+			}
+			else {
+				String namespacedParameter = portletNamespace + name;
+
+				if (renderParameterNames.contains(name) &&
+					servletRequestParameterMap.containsKey(
+						namespacedParameter)) {
+
+					actionParameterMap.put(
+						name,
+						servletRequestParameterMap.get(namespacedParameter));
+				}
+			}
+		}
+
+		_actionParameters = new ActionParametersImpl(
+			actionParameterMap, portletNamespace);
+	}
+
+	private ActionParameters _actionParameters;
 
 }
