2c540e6a637c11905cb6dde8d08585bcaef97148
Gergely Mathe <gergely.mathe@liferay.com>
2018-07-16 14:13:36 +0200

    LPS-79312 Do the same with Blogs

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/exportimport/data/handler/BlogsPortletDataHandler.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/exportimport/data/handler/BlogsPortletDataHandler.java
@@ -14,25 +14,16 @@
 
 package com.liferay.blogs.web.internal.exportimport.data.handler;
 
-import com.liferay.blogs.constants.BlogsConstants;
 import com.liferay.blogs.constants.BlogsPortletKeys;
 import com.liferay.blogs.model.BlogsEntry;
-import com.liferay.blogs.service.BlogsEntryLocalService;
-import com.liferay.blogs.service.BlogsStatsUserLocalService;
 import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
-import com.liferay.exportimport.kernel.lar.ExportImportDateUtil;
+import com.liferay.exportimport.kernel.lar.DataLevel;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataHandler;
 import com.liferay.exportimport.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.exportimport.kernel.lar.PortletDataHandlerControl;
-import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.exportimport.kernel.lar.StagedModelType;
-import com.liferay.exportimport.kernel.staging.Staging;
-import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
-import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.util.PropsValues;
-
-import java.util.List;
 
 import javax.portlet.PortletPreferences;
 
@@ -45,40 +36,54 @@ import org.osgi.service.component.annotations.Reference;
  * @author Raymond Augé
  * @author Juan Fernández
  * @author Zsolt Berentey
+ * @author Gergely Mathe
  */
 @Component(
-	property = {
-		"javax.portlet.name=" + BlogsPortletKeys.BLOGS,
-		"javax.portlet.name=" + BlogsPortletKeys.BLOGS_ADMIN
-	},
+	property = "javax.portlet.name=" + BlogsPortletKeys.BLOGS,
 	service = PortletDataHandler.class
 )
 public class BlogsPortletDataHandler extends BasePortletDataHandler {
 
+	/**
+	 * @deprecated As of Judson (7.1.x), replaced by {@link
+	 *             BlogsAdminPortletDataHandler#NAMESPACE}
+	 */
+	@Deprecated
 	public static final String NAMESPACE = "blogs";
 
+	/**
+	 * @deprecated As of Judson (7.1.x), replaced by {@link
+	 *             BlogsAdminPortletDataHandler#SCHEMA_VERSION}
+	 */
+	@Deprecated
 	public static final String SCHEMA_VERSION = "1.0.0";
 
 	@Override
+	public String getNamespace() {
+		return _blogsAdminPortletDataHandler.getNamespace();
+	}
+
+	@Override
 	public String getSchemaVersion() {
-		return SCHEMA_VERSION;
+		return _blogsAdminPortletDataHandler.getSchemaVersion();
 	}
 
 	@Override
 	public String getServiceName() {
-		return BlogsConstants.SERVICE_NAME;
+		return _blogsAdminPortletDataHandler.getServiceName();
 	}
 
 	@Activate
 	protected void activate() {
+		setDataLevel(DataLevel.PORTLET_INSTANCE);
 		setDeletionSystemEventStagedModelTypes(
 			new StagedModelType(BlogsEntry.class));
 		setExportControls(
 			new PortletDataHandlerBoolean(
-				NAMESPACE, "entries", true, false,
+				getNamespace(), "entries", true, false,
 				new PortletDataHandlerControl[] {
 					new PortletDataHandlerBoolean(
-						NAMESPACE, "referenced-content")
+						getNamespace(), "referenced-content")
 				},
 				BlogsEntry.class.getName()));
 		setPublishToLiveByDefault(PropsValues.BLOGS_PUBLISH_TO_LIVE_BY_DEFAULT);
@@ -91,19 +96,8 @@ public class BlogsPortletDataHandler extends BasePortletDataHandler {
 			PortletPreferences portletPreferences)
 		throws Exception {
 
-		if (portletDataContext.addPrimaryKey(
-				BlogsPortletDataHandler.class, "deleteData")) {
-
-			return portletPreferences;
-		}
-
-		_blogsEntryLocalService.deleteEntries(
-			portletDataContext.getScopeGroupId());
-
-		_blogsStatsUserLocalService.deleteStatsUserByGroupId(
-			portletDataContext.getScopeGroupId());
-
-		return portletPreferences;
+		return _blogsAdminPortletDataHandler.doDeleteData(
+			portletDataContext, portletId, portletPreferences);
 	}
 
 	@Override
@@ -112,24 +106,8 @@ public class BlogsPortletDataHandler extends BasePortletDataHandler {
 			PortletPreferences portletPreferences)
 		throws Exception {
 
-		Element rootElement = addExportDataRootElement(portletDataContext);
-
-		if (!portletDataContext.getBooleanParameter(NAMESPACE, "entries")) {
-			return getExportDataRootElementString(rootElement);
-		}
-
-		portletDataContext.addPortletPermissions(BlogsConstants.RESOURCE_NAME);
-
-		rootElement.addAttribute(
-			"group-id", String.valueOf(portletDataContext.getScopeGroupId()));
-
-		ActionableDynamicQuery actionableDynamicQuery =
-			_blogsEntryLocalService.getExportActionableDynamicQuery(
-				portletDataContext);
-
-		actionableDynamicQuery.performActions();
-
-		return getExportDataRootElementString(rootElement);
+		return _blogsAdminPortletDataHandler.doExportData(
+			portletDataContext, portletId, portletPreferences);
 	}
 
 	@Override
@@ -138,24 +116,8 @@ public class BlogsPortletDataHandler extends BasePortletDataHandler {
 			PortletPreferences portletPreferences, String data)
 		throws Exception {
 
-		if (!portletDataContext.getBooleanParameter(NAMESPACE, "entries")) {
-			return null;
-		}
-
-		portletDataContext.importPortletPermissions(
-			BlogsConstants.RESOURCE_NAME);
-
-		Element entriesElement = portletDataContext.getImportDataGroupElement(
-			BlogsEntry.class);
-
-		List<Element> entryElements = entriesElement.elements();
-
-		for (Element entryElement : entryElements) {
-			StagedModelDataHandlerUtil.importStagedModel(
-				portletDataContext, entryElement);
-		}
-
-		return null;
+		return _blogsAdminPortletDataHandler.doImportData(
+			portletDataContext, portletId, portletPreferences, data);
 	}
 
 	@Override
@@ -164,43 +126,11 @@ public class BlogsPortletDataHandler extends BasePortletDataHandler {
 			PortletPreferences portletPreferences)
 		throws Exception {
 
-		if (ExportImportDateUtil.isRangeFromLastPublishDate(
-				portletDataContext)) {
-
-			_staging.populateLastPublishDateCounts(
-				portletDataContext,
-				new StagedModelType[] {
-					new StagedModelType(BlogsEntry.class.getName())
-				});
-
-			return;
-		}
-
-		ActionableDynamicQuery actionableDynamicQuery =
-			_blogsEntryLocalService.getExportActionableDynamicQuery(
-				portletDataContext);
-
-		actionableDynamicQuery.performCount();
+		_blogsAdminPortletDataHandler.doPrepareManifestSummary(
+			portletDataContext, portletPreferences);
 	}
-
-	@Reference(unbind = "-")
-	protected void setBlogsEntryLocalService(
-		BlogsEntryLocalService blogsEntryLocalService) {
-
-		_blogsEntryLocalService = blogsEntryLocalService;
-	}
-
-	@Reference(unbind = "-")
-	protected void setBlogsStatsUserLocalService(
-		BlogsStatsUserLocalService blogsStatsUserLocalService) {
-
-		_blogsStatsUserLocalService = blogsStatsUserLocalService;
-	}
-
-	private BlogsEntryLocalService _blogsEntryLocalService;
-	private BlogsStatsUserLocalService _blogsStatsUserLocalService;
 
 	@Reference
-	private Staging _staging;
+	private BlogsAdminPortletDataHandler _blogsAdminPortletDataHandler;
 
 }
