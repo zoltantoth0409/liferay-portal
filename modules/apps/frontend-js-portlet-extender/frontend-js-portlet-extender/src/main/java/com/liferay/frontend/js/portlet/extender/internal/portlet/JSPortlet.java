67b4a1f311505ba72c74af4dc805beff5420504b
Brian Chan <brian.chan@liferay.com>
2018-07-17 10:20:12 -0700

    LPS-83285 This may be overkill.. but it's consistent

--- modules/apps/frontend-js-portlet-extender/frontend-js-portlet-extender/src/main/java/com/liferay/frontend/js/portlet/extender/internal/portlet/JSPortlet.java
+++ modules/apps/frontend-js-portlet-extender/frontend-js-portlet-extender/src/main/java/com/liferay/frontend/js/portlet/extender/internal/portlet/JSPortlet.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.frontend.js.portlet.extender.internal;
+package com.liferay.frontend.js.portlet.extender.internal.portlet;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
