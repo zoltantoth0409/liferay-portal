bd857a1d98e2e5c4659527a5745a94836f1fa28e
Fortunato Maldonado <fortunato.maldonado@liferay.com>
2018-07-17 15:18:16 -0700

    LPS-83248 Added break lines after an image is uploaded to blogs

--- modules/apps/adaptive-media/adaptive-media-image-js-web/src/main/resources/META-INF/resources/ckeditor/plugins/adaptivemedia/plugin.js
+++ modules/apps/adaptive-media/adaptive-media-image-js-web/src/main/resources/META-INF/resources/ckeditor/plugins/adaptivemedia/plugin.js
@@ -118,7 +118,12 @@
 					el = instance._getImgElement(imageSrc, selectedItem);
 				}
 
+				var firstLineBreak = CKEDITOR.dom.element.createFromHtml('<br>');
+				var secondLineBreak = CKEDITOR.dom.element.createFromHtml('<br>');
+
 				editor.insertElement(el);
+				editor.insertElement(firstLineBreak);
+				editor.insertElement(secondLineBreak);
 
 				editor.setData(editor.getData());
 			}
