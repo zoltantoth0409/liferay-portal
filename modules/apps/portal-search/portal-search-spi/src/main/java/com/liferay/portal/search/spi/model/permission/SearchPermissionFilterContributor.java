8cb3170216908df8f6b20e1a55d5cf5215222cf1
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-07-18 14:45:49 +0200

    LPS-84845 portal-search-spi: Introduce extension points for permission fields and filters (for new sharing logic to be injected)
    https://issues.liferay.com/browse/LPS-84845
    
    Co-authored-by: Sergio Gonzalez <sergio.gonzalez@liferay.com>
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/SearchPermissionFilterContributor.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.spi.model.permission;
+
+import aQute.bnd.annotation.ConsumerType;
+
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+/**
+ * @author Sergio González
+ */
+@ConsumerType
+@FunctionalInterface
+public interface SearchPermissionFilterContributor {
+
+	public void contribute(
+		BooleanFilter booleanFilter, long companyId, long[] groupIds,
+		long userId, PermissionChecker permissionChecker, String className);
+
+}
