7414e1e225c0275a849e7935c0e1cf72520fd2b0
Jonathan McCann <jonathan.mccann@liferay.com>
2018-07-18 10:00:49 -0700

    LPS-79225 Change Xuggler ERROR level message to WARN when visiting Server Administration's External Services tab

--- portal-impl/src/com/liferay/portal/xuggler/XugglerImpl.java
+++ portal-impl/src/com/liferay/portal/xuggler/XugglerImpl.java
@@ -115,7 +115,7 @@ public class XugglerImpl implements Xuggler {
 	}
 
 	protected void informAdministrator(String errorMessage) {
-		if (!_informAdministrator) {
+		if (!_informAdministrator || !_log.isWarnEnabled()) {
 			return;
 		}
 
@@ -131,7 +131,7 @@ public class XugglerImpl implements Xuggler {
 		sb.append("/external-services. Error message is: ");
 		sb.append(errorMessage);
 
-		_log.error(sb.toString());
+		_log.warn(sb.toString());
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(XugglerImpl.class);
