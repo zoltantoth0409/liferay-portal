f2caf6f0a6121210d4896204995ca3a718ce6ee2
Peter Shin <peter.shin@liferay.com>
2018-07-18 15:15:08 -0700

    LPS-74544 Revert 4e61dd8

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLWhitespaceCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLWhitespaceCheck.java
@@ -17,11 +17,8 @@ package com.liferay.source.formatter.checks;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.source.formatter.checks.util.YMLSourceUtil;
 
 import java.io.IOException;
-
-import java.util.List;
 
 /**
  * @author Hugo Huijser
@@ -36,68 +33,7 @@ public class YMLWhitespaceCheck extends WhitespaceCheck {
 		content = StringUtil.replace(
 			content, CharPool.TAB, StringPool.FOUR_SPACES);
 
-		content = _checkDefinitions(content, StringPool.BLANK, 0);
-
 		return super.doProcess(fileName, absolutePath, content);
-	}
-
-	private String _checkDefinitions(String content, String indent, int level) {
-		List<String> definitions = YMLSourceUtil.getDefinitions(
-			content, indent);
-
-		for (String definition : definitions) {
-			String expectedIndent = StringPool.BLANK;
-
-			for (int i = 0; i < level; i++) {
-				expectedIndent = expectedIndent + StringPool.FOUR_SPACES;
-			}
-
-			if (!indent.equals(expectedIndent)) {
-				content = StringUtil.replaceFirst(
-					content, definition,
-					expectedIndent + StringUtil.trimLeading(definition));
-			}
-
-			String nestedDefinitionIndent =
-				YMLSourceUtil.getNestedDefinitionIndent(definition);
-
-			if (!nestedDefinitionIndent.equals(StringPool.BLANK)) {
-				List<String> nestedDefinitions = YMLSourceUtil.getDefinitions(
-					definition, nestedDefinitionIndent);
-				String newDefinition = _checkDefinitions(
-					definition, nestedDefinitionIndent, level + 1);
-
-				if (newDefinition.equals(definition) &&
-					nestedDefinitions.isEmpty()) {
-
-					String[] lines = StringUtil.splitLines(newDefinition);
-
-					if (lines.length > 1) {
-						expectedIndent =
-							expectedIndent + StringPool.FOUR_SPACES;
-
-						for (int i = 1; i < lines.length; i++) {
-							String line = lines[i];
-
-							String expectedLine =
-								expectedIndent + StringUtil.trimLeading(line);
-
-							if (!line.equals(expectedLine)) {
-								newDefinition = StringUtil.replaceFirst(
-									newDefinition, line, expectedLine);
-							}
-						}
-					}
-				}
-
-				if (!newDefinition.equals(definition)) {
-					content = StringUtil.replaceFirst(
-						content, definition, newDefinition);
-				}
-			}
-		}
-
-		return content;
 	}
 
 }
