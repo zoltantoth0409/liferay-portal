4b0eea8ed745a08fa50f6ec9c5895617d8b35819
Peter Shin <peter.shin@liferay.com>
2018-07-18 15:16:31 -0700

    LPS-74544 Fix whitespace in yml files

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLWhitespaceCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLWhitespaceCheck.java
@@ -17,8 +17,13 @@ package com.liferay.source.formatter.checks;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.checks.util.YMLSourceUtil;
 
 import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Hugo Huijser
@@ -33,7 +38,108 @@ public class YMLWhitespaceCheck extends WhitespaceCheck {
 		content = StringUtil.replace(
 			content, CharPool.TAB, StringPool.FOUR_SPACES);
 
+		content = _formatDefinitions(content, StringPool.BLANK, 0);
+
 		return super.doProcess(fileName, absolutePath, content);
+	}
+
+	private String _formatDefinition(
+		String definition, String indent, int level,
+		boolean hasNestedDefinitions) {
+
+		String expectedIndent = StringPool.BLANK;
+
+		for (int j = 0; j < level; j++) {
+			expectedIndent = expectedIndent + StringPool.FOUR_SPACES;
+		}
+
+		String newDefinition = definition;
+
+		if (!expectedIndent.equals(indent)) {
+			newDefinition = expectedIndent + StringUtil.trimLeading(definition);
+		}
+
+		if (hasNestedDefinitions) {
+			return newDefinition;
+		}
+
+		String[] lines = StringUtil.splitLines(newDefinition);
+
+		if (lines.length <= 1) {
+			return newDefinition;
+		}
+
+		String previousIndent = StringPool.BLANK;
+
+		for (int j = 1; j < lines.length; j++) {
+			String line = lines[j];
+
+			if (Validator.isNull(line)) {
+				continue;
+			}
+
+			String curIndent = line.replaceAll("^(\\s+).+", "$1");
+
+			if (curIndent.equals(line)) {
+				curIndent = StringPool.BLANK;
+			}
+
+			if (curIndent.length() > previousIndent.length()) {
+				expectedIndent = expectedIndent + StringPool.FOUR_SPACES;
+			}
+			else if (curIndent.length() < previousIndent.length()) {
+				expectedIndent = StringUtil.replaceFirst(
+					expectedIndent, StringPool.FOUR_SPACES, StringPool.BLANK);
+			}
+
+			if (!curIndent.equals(expectedIndent)) {
+				String s = expectedIndent + StringUtil.trimLeading(line);
+
+				newDefinition = StringUtil.replaceFirst(newDefinition, line, s);
+			}
+
+			previousIndent = curIndent;
+		}
+
+		return newDefinition;
+	}
+
+	private String _formatDefinitions(
+		String content, String indent, int level) {
+
+		List<String> definitions = YMLSourceUtil.getDefinitions(
+			content, indent);
+
+		for (String definition : definitions) {
+			String nestedDefinitionIndent =
+				YMLSourceUtil.getNestedDefinitionIndent(definition);
+
+			List<String> nestedDefinitions = Collections.emptyList();
+
+			if (!nestedDefinitionIndent.equals(StringPool.BLANK)) {
+				nestedDefinitions = YMLSourceUtil.getDefinitions(
+					content, nestedDefinitionIndent);
+
+				String newDefinition = _formatDefinitions(
+					definition, nestedDefinitionIndent, level + 1);
+
+				if (!newDefinition.equals(definition)) {
+					content = StringUtil.replaceFirst(
+						content, definition, newDefinition);
+				}
+			}
+
+			String newDefinition = _formatDefinition(
+				definition, nestedDefinitionIndent, level,
+				!nestedDefinitions.isEmpty());
+
+			if (!newDefinition.equals(definition)) {
+				content = StringUtil.replaceFirst(
+					content, definition, newDefinition);
+			}
+		}
+
+		return content;
 	}
 
 }
