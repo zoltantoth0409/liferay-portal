b8e66facc328d6e426239513c69bcf18e197416d
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-07-18 10:36:09 +0200

    LPS-83549 Delete related display page when deleting an asset

---
+++ modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/internal/model/listener/AssetEntryModelListener.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.display.page.internal.model.listener;
+
+import com.liferay.asset.display.page.model.AssetDisplayPageEntry;
+import com.liferay.asset.display.page.service.AssetDisplayPageEntryLocalService;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class AssetEntryModelListener extends BaseModelListener<AssetEntry> {
+
+	@Override
+	public void onBeforeRemove(AssetEntry assetEntry)
+		throws ModelListenerException {
+
+		AssetDisplayPageEntry assetDisplayPageEntry =
+			_assetDisplayPageEntryLocalService.fetchAssetDisplayPageEntry(
+				assetEntry.getGroupId(), assetEntry.getClassNameId(),
+				assetEntry.getClassPK());
+
+		if (assetDisplayPageEntry != null) {
+			_assetDisplayPageEntryLocalService.deleteAssetDisplayPageEntry(
+				assetDisplayPageEntry);
+		}
+	}
+
+	@Reference
+	private AssetDisplayPageEntryLocalService
+		_assetDisplayPageEntryLocalService;
+
+}
