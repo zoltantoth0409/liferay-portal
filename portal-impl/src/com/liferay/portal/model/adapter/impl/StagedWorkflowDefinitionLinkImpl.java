9b3ee22ea54fd2b1ef0ef0137e9ac14c63d6c65b
Máté Thurzó <mate.thurzo@liferay.com>
2018-07-19 17:46:10 +0200

    LPS-83326 Keep it lean, no need to have the different attributes

--- portal-impl/src/com/liferay/portal/model/adapter/impl/StagedWorkflowDefinitionLinkImpl.java
+++ portal-impl/src/com/liferay/portal/model/adapter/impl/StagedWorkflowDefinitionLinkImpl.java
@@ -15,13 +15,9 @@
 package com.liferay.portal.model.adapter.impl;
 
 import com.liferay.exportimport.kernel.lar.StagedModelType;
-import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.model.WorkflowDefinitionLink;
 import com.liferay.portal.kernel.model.adapter.StagedWorkflowDefinitionLink;
-import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
 import com.liferay.portal.model.impl.WorkflowDefinitionLinkImpl;
-
-import java.util.Date;
 
 /**
  * @author Zoltan Csaszi
@@ -33,54 +29,11 @@ public class StagedWorkflowDefinitionLinkImpl
 		WorkflowDefinitionLink workflowDefinitionLink) {
 
 		_workflowDefinitionLink = workflowDefinitionLink;
-
-		_workflowDefinitionLinkId =
-			workflowDefinitionLink.getWorkflowDefinitionLinkId();
-		_groupId = workflowDefinitionLink.getGroupId();
-		_classNameId = workflowDefinitionLink.getClassNameId();
-		_classPK = workflowDefinitionLink.getClassPK();
-		_companyId = workflowDefinitionLink.getCompanyId();
-		_createDate = workflowDefinitionLink.getCreateDate();
-		_modifiedDate = workflowDefinitionLink.getModifiedDate();
-		_typePK = workflowDefinitionLink.getTypePK();
-		_userId = workflowDefinitionLink.getUserId();
-		_userName = workflowDefinitionLink.getUserName();
-		_workflowDefinitionName =
-			workflowDefinitionLink.getWorkflowDefinitionName();
-		_workflowDefinitionVersion =
-			workflowDefinitionLink.getWorkflowDefinitionVersion();
 	}
 
 	@Override
 	public Object clone() {
 		return new StagedWorkflowDefinitionLinkImpl(_workflowDefinitionLink);
-	}
-
-	@Override
-	public String getClassName() {
-		ClassName className = ClassNameLocalServiceUtil.fetchClassName(
-			_classNameId);
-
-		if (className != null) {
-			return className.getClassName();
-		}
-
-		return null;
-	}
-
-	@Override
-	public long getClassNameId() {
-		return _classNameId;
-	}
-
-	@Override
-	public long getClassPK() {
-		return _classPK;
-	}
-
-	@Override
-	public long getCompanyId() {
-		return _companyId;
 	}
 
 	@Override
@@ -94,32 +47,10 @@ public class StagedWorkflowDefinitionLinkImpl
 	}
 
 	@Override
-	public String getWorkflowDefinitionName() {
-		return _workflowDefinitionName;
-	}
-
-	@Override
-	public int getWorkflowDefinitionVersion() {
-		return _workflowDefinitionVersion;
-	}
-
-	@Override
 	public void setUuid(String uuid) {
 		throw new UnsupportedOperationException();
 	}
 
-	private final long _classNameId;
-	private final long _classPK;
-	private final long _companyId;
-	private final Date _createDate;
-	private final long _groupId;
-	private final Date _modifiedDate;
-	private final long _typePK;
-	private final long _userId;
-	private final String _userName;
 	private final WorkflowDefinitionLink _workflowDefinitionLink;
-	private final long _workflowDefinitionLinkId;
-	private final String _workflowDefinitionName;
-	private final int _workflowDefinitionVersion;
 
 }
