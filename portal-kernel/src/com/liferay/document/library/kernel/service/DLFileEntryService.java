e7b94d35511fe37b49d831e1de3a9a0a9b471685
Adolfo Pérez <adolfo.perez@liferay.com>
2018-07-19 09:20:23 +0200

    LPS-82101 Autogenerated

--- portal-kernel/src/com/liferay/document/library/kernel/service/DLFileEntryService.java
+++ portal-kernel/src/com/liferay/document/library/kernel/service/DLFileEntryService.java
@@ -215,6 +215,10 @@ public interface DLFileEntryService extends BaseService {
 	public boolean isFileEntryCheckedOut(long fileEntryId)
 		throws PortalException;
 
+	/**
+	* @deprecated As of Judson (7.1.x), with no direct replacement
+	*/
+	@Deprecated
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public boolean isKeepFileVersionLabel(long fileEntryId,
 		boolean majorVersion, ServiceContext serviceContext)
