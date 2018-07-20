f53aa51d3272acec6356732c8736fd38ada22d46
Kenji Heigel <kenji.heigel@liferay.com>
2018-07-19 18:05:49 -0700

    LRQA-42411 Change this back to the original

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerVariablesUtil.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunnerVariablesUtil.java
@@ -180,10 +180,6 @@ public class PoshiRunnerVariablesUtil {
 		Matcher matcher = _pattern.matcher(token);
 
 		if (matcher.matches() && _executeMap.containsKey(matcher.group(1))) {
-			if (_commandMap.containsKey(matcher.group(1))) {
-				return getValueFromCommandMap(matcher.group(1));
-			}
-
 			return getValueFromExecuteMap(matcher.group(1));
 		}
 
@@ -191,10 +187,6 @@ public class PoshiRunnerVariablesUtil {
 
 		while (matcher.find() && _executeMap.containsKey(matcher.group(1))) {
 			String varValue = getStringFromExecuteMap(matcher.group(1));
-
-			if (_commandMap.containsKey(matcher.group(1))) {
-				varValue = getStringFromCommandMap(matcher.group(1));
-			}
 
 			token = StringUtil.replace(token, matcher.group(), varValue);
 		}
