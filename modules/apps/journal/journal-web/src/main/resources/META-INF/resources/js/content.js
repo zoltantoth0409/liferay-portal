761fa543339c25a7c170e87945c8a5e565ddfc87
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-07-20 10:39:54 +0200

    LPS-81251 Removes unused code

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/content.js
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/content.js
@@ -5,12 +5,6 @@ AUI.add(
 
 		var STR_CLICK = 'click';
 
-		var STR_DDM = 'ddm';
-
-		var STR_SELECT_STRUCTURE = 'selectStructure';
-
-		var STR_SELECT_TEMPLATE = 'selectTemplate';
-
 		var STR_STRINGS = 'strings';
 
 		var STR_URLS = 'urls';
@@ -20,11 +14,6 @@ AUI.add(
 		var JournalContent = A.Component.create(
 			{
 				ATTRS: {
-					ddm: {
-						validator: Lang.isObject,
-						value: {}
-					},
-
 					editStructure: {
 						setter: A.one
 					},
@@ -33,18 +22,9 @@ AUI.add(
 						setter: A.one
 					},
 
-					selectStructure: {
-						setter: A.one
-					},
-
-					selectTemplate: {
-						setter: A.one
-					},
-
 					strings: {
 						validator: Lang.isObject,
 						value: {
-							draft: Liferay.Language.get('draft'),
 							editStructure: Liferay.Language.get('editing-the-current-structure-deletes-all-unsaved-content'),
 							editTemplate: Liferay.Language.get('editing-the-current-template-deletes-all-unsaved-content'),
 							structures: Liferay.Language.get('structures'),
@@ -98,22 +78,6 @@ AUI.add(
 							);
 						}
 
-						var selectTemplate = instance.get(STR_SELECT_TEMPLATE);
-
-						if (selectTemplate) {
-							eventHandles.push(
-								selectTemplate.on(STR_CLICK, instance._openDDMTemplateSelector, instance)
-							);
-						}
-
-						var selectStructure = instance.get(STR_SELECT_STRUCTURE);
-
-						if (selectStructure) {
-							eventHandles.push(
-								selectStructure.on(STR_CLICK, instance._openDDMStructureSelector, instance)
-							);
-						}
-
 						instance._eventHandles = eventHandles;
 					},
 
@@ -153,96 +117,6 @@ AUI.add(
 								}
 							);
 						}
-					},
-
-					_getPrincipalForm: function(formName) {
-						var instance = this;
-
-						return instance.one('form[name=' + instance.ns(formName || 'fm1') + ']');
-					},
-
-					_openDDMStructureSelector: function() {
-						var instance = this;
-
-						var ddm = instance.get(STR_DDM);
-
-						var strings = instance.get(STR_STRINGS);
-
-						Liferay.Util.openDDMPortlet(
-							{
-								basePortletURL: ddm.basePortletURL,
-								classPK: ddm.classPK,
-								dialog: {
-									destroyOnHide: true
-								},
-								eventName: instance.ns(STR_SELECT_STRUCTURE),
-								groupId: ddm.groupId,
-								mvcPath: '/select_structure.jsp',
-								navigationStartsOn: 'SELECT_STRUCTURE',
-								refererPortletName: ddm.refererPortletName,
-								searchRestriction: ddm.searchRestriction,
-								searchRestrictionClassNameId: ddm.searchRestrictionClassNameId,
-								searchRestrictionClassPK: ddm.searchRestrictionClassPK,
-								showAncestorScopes: true,
-								showCacheableInput: true,
-								title: strings.structures
-							},
-							function(event) {
-								var form = instance._getPrincipalForm();
-
-								var ddmStructureId = instance.one('#ddmStructureId');
-
-								if (ddmStructureId.val() != event.ddmstructureid) {
-									ddmStructureId.val(event.ddmstructureid);
-
-									instance.one('#changeStructure').val(true);
-
-									instance.one('#ddmStructureKey').val(event.ddmstructurekey);
-
-									instance.one('#ddmTemplateKey').val('');
-
-									submitForm(form, null, false, false);
-								}
-							}
-						);
-					},
-
-					_openDDMTemplateSelector: function() {
-						var instance = this;
-
-						var ddm = instance.get(STR_DDM);
-
-						var strings = instance.get(STR_STRINGS);
-
-						Liferay.Util.openDDMPortlet(
-							{
-								basePortletURL: ddm.basePortletURL,
-								classNameId: ddm.classNameId,
-								classPK: ddm.classPK,
-								dialog: {
-									destroyOnHide: true
-								},
-								eventName: instance.ns(STR_SELECT_TEMPLATE),
-								groupId: ddm.groupId,
-								mvcPath: '/select_template.jsp',
-								navigationStartsOn: 'SELECT_TEMPLATE',
-								refererPortletName: ddm.refererPortletName,
-								resourceClassNameId: ddm.resourceClassNameId,
-								showAncestorScopes: true,
-								showCacheableInput: true,
-								templateId: ddm.templateId,
-								title: strings.templates
-							},
-							function(event) {
-								var form = instance._getPrincipalForm();
-
-								var ddmTemplateId = instance.one('#ddmTemplateId');
-
-								ddmTemplateId.val(event.ddmtemplateid);
-
-								submitForm(form, null, false, false);
-							}
-						);
 					}
 				}
 			}
