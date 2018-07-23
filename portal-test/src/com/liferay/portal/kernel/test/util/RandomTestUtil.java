a708eb98d6656225bfb64baec93c4980790e967c
Matthew Tambara <matthew.tambara@liferay.com>
2018-07-23 10:12:00 -0700

    LPS-80978 Deprecate old methods

--- portal-test/src/com/liferay/portal/kernel/test/util/RandomTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/RandomTestUtil.java
@@ -56,6 +56,11 @@ public class RandomTestUtil {
 		return _random.nextBoolean();
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), replaced by {@link
+	 * TestDataConstants.TEST_BYTE_ARRAY}
+	 */
+	@Deprecated
 	@SafeVarargs
 	public static byte[] randomBytes(
 		int size, RandomizerBumper<byte[]>... randomizerBumpers) {
@@ -85,6 +90,11 @@ public class RandomTestUtil {
 				" tries"));
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), replaced by {@link
+	 * TestDataConstants.TEST_BYTE_ARRAY}
+	 */
+	@Deprecated
 	@SafeVarargs
 	public static byte[] randomBytes(
 		RandomizerBumper<byte[]>... randomizerBumpers) {
@@ -106,6 +116,10 @@ public class RandomTestUtil {
 		}
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
+	 */
+	@Deprecated
 	@SafeVarargs
 	public static InputStream randomInputStream(
 		RandomizerBumper<byte[]>... randomizerBumpers) {
