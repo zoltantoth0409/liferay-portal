48477d99da8d8c4079f0ffaaaa7437022305e1f4
Sarai Díaz García <dgarciasarai@gmail.com>
2018-07-24 17:32:01 +0200

    LPS-82711 Add related collection tasks in role API

--- modules/apps/headless-apio/role/role-apio-impl/src/main/java/com/liferay/role/apio/internal/resource/RoleCollectionResource.java
+++ modules/apps/headless-apio/role/role-apio-impl/src/main/java/com/liferay/role/apio/internal/resource/RoleCollectionResource.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.RoleConstants;
 import com.liferay.portal.kernel.service.RoleService;
 import com.liferay.role.apio.identifier.RoleIdentifier;
+import com.liferay.workflow.apio.architect.identifier.WorkflowTaskIdentifier;
 
 import java.util.List;
 
@@ -83,6 +84,8 @@ public class RoleCollectionResource
 			"creator", PersonIdentifier.class, Role::getUserId
 		).addLocalizedStringByLocale(
 			"description", Role::getDescription
+		).addRelatedCollection(
+			"tasks", WorkflowTaskIdentifier.class
 		).addString(
 			"name", Role::getName
 		).addString(
