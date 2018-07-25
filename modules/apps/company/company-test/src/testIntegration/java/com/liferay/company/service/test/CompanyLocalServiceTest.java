6681ab6b25f7e9dfe57270cd0f4aaf387576c10a
jpince <jason.pince@liferay.com>
2018-07-25 11:49:45 -0700

    LPS-83072 Temporarily ignore test

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
@@ -91,6 +91,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -205,6 +206,7 @@ public class CompanyLocalServiceTest {
 			group);
 	}
 
+	@Ignore
 	@Test
 	public void testAddAndDeleteCompanyWithCompanyGroupStaging()
 		throws Exception {
