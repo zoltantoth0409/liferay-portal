8b73a8cc37820e50865589095d1f74b4b83b8b0a
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-07-25 17:06:14 -0700

    LRQA-41905 Add base git branch objects and factory

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/LocalGitBranch.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class LocalGitBranch extends BaseGitBranch {
+
+	public GitWorkingDirectory getGitWorkingDirectory() {
+		LocalRepository localRepository = getLocalRepository();
+
+		return localRepository.getGitWorkingDirectory();
+	}
+
+	public LocalRepository getLocalRepository() {
+		return _localRepository;
+	}
+
+	@Override
+	public String toString() {
+		LocalRepository localRepository = getLocalRepository();
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(localRepository.getDirectory());
+		sb.append(" (");
+		sb.append(getName());
+		sb.append(" - ");
+		sb.append(getSHA());
+		sb.append(")");
+
+		return sb.toString();
+	}
+
+	protected LocalGitBranch(
+		LocalRepository localRepository, String name, String sha) {
+
+		super(name, sha);
+
+		_localRepository = localRepository;
+	}
+
+	private final LocalRepository _localRepository;
+
+}
---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/RemoteGitBranch.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class RemoteGitBranch extends BaseGitBranch {
+
+	public RemoteRepository getRemoteRepository() {
+		return _remoteRepository;
+	}
+
+	public String getRepositoryName() {
+		return _remoteRepository.getName();
+	}
+
+	public String getRepositoryUsername() {
+		return _remoteRepository.getUsername();
+	}
+
+	@Override
+	public String toString() {
+		RemoteRepository remoteRepository = getRemoteRepository();
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(remoteRepository.getRemoteURL());
+		sb.append(" (");
+		sb.append(getName());
+		sb.append(" - ");
+		sb.append(getSHA());
+		sb.append(")");
+
+		return sb.toString();
+	}
+
+	protected RemoteGitBranch(
+		RemoteRepository remoteRepository, String name, String sha) {
+
+		super(name, sha);
+
+		_remoteRepository = remoteRepository;
+	}
+
+	private final RemoteRepository _remoteRepository;
+
+}
