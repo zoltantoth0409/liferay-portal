a04a7b2a017d8b5d901aff21f81b364ad572bd3b
pavel-savinov <pavel.savinov@liferay.com>
2018-07-30 14:43:04 +0200

    LPS-83455 Add integration tests for the primary navigation update and deletion

---
+++ modules/apps/site-navigation/site-navigation-test/src/testIntegration/java/com/liferay/site/navigation/service/test/SiteNavigationMenuServiceTest.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.site.navigation.constants.SiteNavigationConstants;
+import com.liferay.site.navigation.exception.RequiredPrimaryNavigationException;
+import com.liferay.site.navigation.model.SiteNavigationMenu;
+import com.liferay.site.navigation.service.SiteNavigationMenuLocalServiceUtil;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Pavel Savinov
+ */
+@RunWith(Arquillian.class)
+public class SiteNavigationMenuServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		_siteNavigationMenu =
+			SiteNavigationMenuLocalServiceUtil.addSiteNavigationMenu(
+				TestPropsValues.getUserId(), _group.getGroupId(), "Menu",
+				serviceContext);
+	}
+
+	@Test(expected = RequiredPrimaryNavigationException.class)
+	public void testChangePrimaryNavigationMenuFunction() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		SiteNavigationMenuLocalServiceUtil.updateSiteNavigationMenu(
+			TestPropsValues.getUserId(),
+			_siteNavigationMenu.getSiteNavigationMenuId(),
+			SiteNavigationConstants.TYPE_SECONDARY, false, serviceContext);
+	}
+
+	@Test(expected = RequiredPrimaryNavigationException.class)
+	public void testDeletePrimaryNavigationMenu() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		SiteNavigationMenuLocalServiceUtil.addSiteNavigationMenu(
+			TestPropsValues.getUserId(), _group.getGroupId(), "Secondary",
+			SiteNavigationConstants.TYPE_SECONDARY, false, serviceContext);
+
+		SiteNavigationMenuLocalServiceUtil.deleteSiteNavigationMenu(
+			_siteNavigationMenu);
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	private SiteNavigationMenu _siteNavigationMenu;
+
+}
