d07c5affe8ebc12f3d47056f08f356b46e71394f
Máté Thurzó <mate.thurzo@liferay.com>
2018-07-30 11:04:12 +0200

    LPS-83848 SF

--- modules/apps/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/servlet/taglib/CheckboxTag.java
+++ modules/apps/staging/staging-taglib/src/main/java/com/liferay/staging/taglib/servlet/taglib/CheckboxTag.java
@@ -20,8 +20,6 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.staging.taglib.internal.servlet.ServletContextUtil;
 import com.liferay.staging.taglib.servlet.taglib.base.BaseCssTag;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.PageContext;
