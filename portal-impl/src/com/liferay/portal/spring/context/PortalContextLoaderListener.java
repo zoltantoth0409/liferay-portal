a0417b8d9fad1d9edb7f4444beea11a6f76287ad
shuyangzhou <shuyang.zhou@liferay.com>
2018-07-30 10:09:23 -0700

    LPS-83988 Remove SecurityManagerUtil usages

--- portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
+++ portal-impl/src/com/liferay/portal/spring/context/PortalContextLoaderListener.java
@@ -58,7 +58,6 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.module.framework.ModuleFrameworkUtilAdapter;
-import com.liferay.portal.security.lang.SecurityManagerUtil;
 import com.liferay.portal.servlet.PortalSessionListener;
 import com.liferay.portal.spring.aop.DynamicProxyCreator;
 import com.liferay.portal.spring.bean.BeanReferenceRefreshUtil;
@@ -155,32 +154,26 @@ public class PortalContextLoaderListener extends ContextLoaderListener {
 
 		closeDataSource("liferayDataSourceImpl");
 
+		super.contextDestroyed(servletContextEvent);
+
 		try {
-			super.contextDestroyed(servletContextEvent);
-
-			try {
-				ModuleFrameworkUtilAdapter.stopRuntime();
-			}
-			catch (Exception e) {
-				_log.error(e, e);
-			}
-
-			try {
-				ModuleFrameworkUtilAdapter.stopFramework(
-					PropsValues.MODULE_FRAMEWORK_STOP_WAIT_TIMEOUT);
-			}
-			catch (Exception e) {
-				_log.error(e, e);
-			}
-
-			ModuleFrameworkUtilAdapter.unregisterContext(
-				_arrayApplicationContext);
-
-			_arrayApplicationContext.close();
+			ModuleFrameworkUtilAdapter.stopRuntime();
 		}
-		finally {
-			SecurityManagerUtil.destroy();
+		catch (Exception e) {
+			_log.error(e, e);
 		}
+
+		try {
+			ModuleFrameworkUtilAdapter.stopFramework(
+				PropsValues.MODULE_FRAMEWORK_STOP_WAIT_TIMEOUT);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		ModuleFrameworkUtilAdapter.unregisterContext(_arrayApplicationContext);
+
+		_arrayApplicationContext.close();
 	}
 
 	@Override
