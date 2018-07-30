dab7c8e2d49ad4e16d126c114e22f03e46e013f1
Máté Thurzó <mate.thurzo@liferay.com>
2018-07-30 10:37:24 +0200

    LPS-83849 SF

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/content/processor/JournalFeedReferencesExportImportContentProcessor.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/content/processor/JournalFeedReferencesExportImportContentProcessor.java
@@ -291,9 +291,9 @@ public class JournalFeedReferencesExportImportContentProcessor
 					groupId, className, classPK);
 			}
 
-			String exportedReference =
-				Portal.FRIENDLY_URL_SEPARATOR + "[$journalfeed-reference=" +
-					path + "$]";
+			String exportedReference = StringBundler.concat(
+				Portal.FRIENDLY_URL_SEPARATOR, "[$journalfeed-reference=", path,
+				"$]");
 
 			if (!content.contains(exportedReference)) {
 				continue;
