fde614723df844202359e752c5b08134320270dc
tina.tian <tina.tian@liferay.com>
2018-07-30 15:29:43 -0700

    LPS-83807 Auto SF

--- modules/apps/portal-cache/portal-cache-api/src/test/java/com/liferay/portal/cache/io/SerializableObjectWrapperTest.java
+++ modules/apps/portal-cache/portal-cache-api/src/test/java/com/liferay/portal/cache/io/SerializableObjectWrapperTest.java
@@ -14,9 +14,9 @@
 
 package com.liferay.portal.cache.io;
 
-import com.liferay.petra.lang.ClassLoaderPool;
 import com.liferay.petra.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.petra.io.unsync.UnsyncByteArrayOutputStream;
+import com.liferay.petra.lang.ClassLoaderPool;
 import com.liferay.portal.kernel.test.CaptureHandler;
 import com.liferay.portal.kernel.test.JDKLoggerTestUtil;
 import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
