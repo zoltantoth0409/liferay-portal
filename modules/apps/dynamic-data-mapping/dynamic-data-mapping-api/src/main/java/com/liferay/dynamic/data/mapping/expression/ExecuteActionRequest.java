98c283c455fea340701f9f6fc520d5dc2597c2b5
Leonardo Barros <leonardo.barros@liferay.com>
2018-08-01 10:12:31 -0300

    LPS-81609 Add expression action handler

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/expression/ExecuteActionRequest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.expression;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @author Leonardo Barros
+ */
+public final class ExecuteActionRequest {
+
+	public String getAction() {
+		return _action;
+	}
+
+	public <T> Optional<T> getParameter(String name) {
+		if (!_parameters.containsKey(name)) {
+			return Optional.empty();
+		}
+
+		return Optional.of((T)_parameters.get(name));
+	}
+
+	public static class Builder {
+
+		public static Builder newBuilder(String action) {
+			return new Builder(action);
+		}
+
+		public ExecuteActionRequest build() {
+			return _executeActionRequest;
+		}
+
+		public Builder withParameter(String name, Object value) {
+			_executeActionRequest._parameters.put(name, value);
+
+			return this;
+		}
+
+		private Builder(String action) {
+			_executeActionRequest._action = action;
+		}
+
+		private final ExecuteActionRequest _executeActionRequest =
+			new ExecuteActionRequest();
+
+	}
+
+	private ExecuteActionRequest() {
+	}
+
+	private String _action;
+	private Map<String, Object> _parameters = new HashMap<>();
+
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/expression/ExecuteActionResponse.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.expression;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @author Leonardo Barros
+ */
+public final class ExecuteActionResponse {
+
+	public <T> Optional<T> getOutput(String name) {
+		if (!_output.containsKey(name)) {
+			return Optional.empty();
+		}
+
+		return Optional.of((T)_output.get(name));
+	}
+
+	public static class Builder {
+
+		public static Builder newBuilder(boolean success) {
+			return new Builder(success);
+		}
+
+		public ExecuteActionResponse build() {
+			return _executeActionResponse;
+		}
+
+		public Builder withOutput(String name, Object value) {
+			_executeActionResponse._output.put(name, value);
+
+			return this;
+		}
+
+		private Builder(boolean success) {
+			_executeActionResponse._success = success;
+		}
+
+		private final ExecuteActionResponse _executeActionResponse =
+			new ExecuteActionResponse();
+
+	}
+
+	private ExecuteActionResponse() {
+	}
+
+	private Map<String, Object> _output = new HashMap<>();
+	private boolean _success;
+
+}
