f133b9647c6a9ee6d70cd5ee4c96d060e5ad3c55
pavel-savinov <pavel.savinov@liferay.com>
2018-08-03 11:40:21 +0200

    LPS-82480 Use body html since Jsoup text method unescapes HTML entities

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/parser/impl/TextEditableElementParser.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/parser/impl/TextEditableElementParser.java
@@ -53,7 +53,9 @@ public class TextEditableElementParser implements EditableElementParser {
 
 		document.outputSettings(outputSettings);
 
-		element.html(document.text());
+		Element body = document.body();
+
+		element.html(body.html());
 	}
 
 	private static final String _TMPL_VALIDATE_TEXT_FIELD = StringUtil.read(
