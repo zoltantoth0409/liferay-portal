b32cca1fb7059e6f54e019e84c32e67712dc356f
Peter Shin <peter.shin@liferay.com>
2018-08-03 15:40:26 -0700

    LPS-84094 Read properties from gradle-ext.properties

--- modules/sdk/gradle-plugins-wedeploy/src/main/java/com/liferay/gradle/plugins/wedeploy/WeDeployDataPlugin.java
+++ modules/sdk/gradle-plugins-wedeploy/src/main/java/com/liferay/gradle/plugins/wedeploy/WeDeployDataPlugin.java
@@ -22,11 +22,13 @@ import groovy.json.JsonSlurper;
 import java.io.File;
 
 import java.util.Map;
+import java.util.Properties;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.BasePlugin;
 import org.gradle.api.tasks.Exec;
+import org.gradle.util.GUtil;
 
 /**
  * @author Andrea Di Giorgi
@@ -111,13 +113,29 @@ public class WeDeployDataPlugin implements Plugin<Project> {
 	}
 
 	private String _getProperty(Project project, String suffix) {
-		Map<String, ?> properties = project.getProperties();
+		File rootDir = project.getRootDir();
 
-		for (Map.Entry<String, ?> entry : properties.entrySet()) {
-			String key = entry.getKey();
+		File propertiesFile = new File(rootDir, "gradle.properties");
 
-			if (key.endsWith(suffix)) {
-				return GradleUtil.toString(entry.getValue());
+		if (!propertiesFile.exists()) {
+			return null;
+		}
+
+		Properties properties = GUtil.loadProperties(propertiesFile);
+
+		propertiesFile = new File(rootDir, "gradle-ext.properties");
+
+		if (propertiesFile.exists()) {
+			Properties extProperties = GUtil.loadProperties(propertiesFile);
+
+			properties.putAll(extProperties);
+		}
+
+		for (Object key : properties.keySet()) {
+			String s = (String)key;
+
+			if (s.endsWith(suffix)) {
+				return GradleUtil.toString(properties.get(key));
 			}
 		}
 
