53b8fd07d838184d4e804de0753bf66a703dadbb
Alberto Chaparro <alberto.chaparro@liferay.com>
2018-08-07 14:06:32 +0200

    LPS-68619 Simplify for 7.x UpgradePortletPreferences since we can get the companyId value from the PortletPreferences table

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUpgradePortletPreferences.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUpgradePortletPreferences.java
@@ -252,10 +252,11 @@ public abstract class BaseUpgradePortletPreferences extends UpgradeProcess {
 
 	protected void updatePortletPreferences() throws Exception {
 		try (LoggingTimer loggingTimer = new LoggingTimer()) {
-			StringBundler sb = new StringBundler(4);
+			StringBundler sb = new StringBundler(5);
 
-			sb.append("select portletPreferencesId, ownerId, ownerType, ");
-			sb.append("plid, portletId, preferences from PortletPreferences");
+			sb.append("select portletPreferencesId, companyId, ownerId, ");
+			sb.append("ownerType, plid, portletId, preferences from ");
+			sb.append("PortletPreferences");
 
 			String whereClause = getUpdatePortletPreferencesWhereClause();
 
@@ -279,57 +280,12 @@ public abstract class BaseUpgradePortletPreferences extends UpgradeProcess {
 				ResultSet rs = ps1.executeQuery()) {
 
 				while (rs.next()) {
+					long portletPreferencesId = rs.getLong(
+						"portletPreferencesId");
+					long companyId = rs.getLong("companyId");
 					long ownerId = rs.getLong("ownerId");
 					int ownerType = rs.getInt("ownerType");
 					long plid = rs.getLong("plid");
-
-					long companyId = 0;
-
-					if (ownerType == PortletKeys.PREFS_OWNER_TYPE_ARCHIVED) {
-						companyId = getCompanyId(
-							"select companyId from PortletItem where " +
-								"portletItemId = ?",
-							ownerId);
-					}
-					else if (ownerType ==
-								 PortletKeys.PREFS_OWNER_TYPE_COMPANY) {
-
-						companyId = ownerId;
-					}
-					else if (ownerType == PortletKeys.PREFS_OWNER_TYPE_GROUP) {
-						Object[] group = getGroup(ownerId);
-
-						if (group != null) {
-							companyId = (Long)group[1];
-						}
-					}
-					else if (ownerType == PortletKeys.PREFS_OWNER_TYPE_LAYOUT) {
-						Object[] layout = getLayout(plid);
-
-						if (layout != null) {
-							companyId = (Long)layout[1];
-						}
-					}
-					else if (ownerType ==
-								 PortletKeys.PREFS_OWNER_TYPE_ORGANIZATION) {
-
-						companyId = getCompanyId(
-							"select companyId from Organization_ where " +
-								"organizationId = ?",
-							ownerId);
-					}
-					else if (ownerType == PortletKeys.PREFS_OWNER_TYPE_USER) {
-						companyId = getCompanyId(
-							"select companyId from User_ where userId = ?",
-							ownerId);
-					}
-					else {
-						throw new UnsupportedOperationException(
-							"Unsupported owner type " + ownerType);
-					}
-
-					long portletPreferencesId = rs.getLong(
-						"portletPreferencesId");
 
 					if (companyId > 0) {
 						String portletId = rs.getString("portletId");
