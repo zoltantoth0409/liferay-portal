f9b7023a68610956dcd3bb0b647f9a1fc2b19219
Hugo Huijser <hugo.huijser@liferay.com>
2018-08-09 14:11:43 +0800

    LPS-84039 Deprecate class

--- portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetCategoryPropertyFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetCategoryPropertyFinderImpl.java
@@ -30,9 +30,12 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * @author Brian Wing Shun Chan
- * @author Jorge Ferrer
+ * @author     Brian Wing Shun Chan
+ * @author     Jorge Ferrer
+ * @deprecated As of Judson (7.1.x), replaced by {@link
+ *             com.liferay.asset.category.property.service.persistence.impl.AssetCategoryPropertyFinderImpl}
  */
+@Deprecated
 public class AssetCategoryPropertyFinderImpl
 	extends AssetCategoryPropertyFinderBaseImpl
 	implements AssetCategoryPropertyFinder {
