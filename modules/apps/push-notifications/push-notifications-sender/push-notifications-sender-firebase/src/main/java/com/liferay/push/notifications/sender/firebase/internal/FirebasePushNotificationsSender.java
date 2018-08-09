b7f28b8dffbc4a635f94ca342ebea688c8150cbf
Roberto Díaz <roberto.diaz@liferay.com>
2018-08-09 15:28:24 +0200

    LPS-84294 Use the title as the body

--- modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-firebase/src/main/java/com/liferay/push/notifications/sender/firebase/internal/FirebasePushNotificationsSender.java
+++ modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-firebase/src/main/java/com/liferay/push/notifications/sender/firebase/internal/FirebasePushNotificationsSender.java
@@ -137,6 +137,37 @@ public class FirebasePushNotificationsSender
 				payloadJSONObject.getInt(PushNotificationsConstants.KEY_BADGE));
 		}
 
+		String title = payloadJSONObject.getString(
+			PushNotificationsConstants.KEY_TITLE);
+
+		if (Validator.isNotNull(title)) {
+			builder.title(title);
+		}
+
+		String titleLocalizedKey = payloadJSONObject.getString(
+			PushNotificationsConstants.KEY_TITLE_LOCALIZED);
+
+		if (Validator.isNotNull(titleLocalizedKey)) {
+			builder.titleLocalizationKey(titleLocalizedKey);
+		}
+
+		JSONArray titleLocalizedArgumentsJSONArray =
+			payloadJSONObject.getJSONArray(
+				PushNotificationsConstants.KEY_TITLE_LOCALIZED_ARGUMENTS);
+
+		if (titleLocalizedArgumentsJSONArray != null) {
+			List<String> localizedArguments = new ArrayList<>();
+
+			for (int i = 0; i < titleLocalizedArgumentsJSONArray.length();
+					i++) {
+
+				localizedArguments.add(
+					titleLocalizedArgumentsJSONArray.getString(i));
+			}
+
+			builder.titleLocalizationArguments(localizedArguments);
+		}
+
 		String body = payloadJSONObject.getString(
 			PushNotificationsConstants.KEY_BODY);
 
