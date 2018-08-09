794878197ea1af4a5610d8ad24eb347177164fcc
Brian Chan <brian.chan@liferay.com>
2018-08-09 08:22:23 -0700

    LPS-84285 Sort by builder.*

--- modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-apple/src/main/java/com/liferay/push/notifications/sender/apple/internal/ApplePushNotificationsSender.java
+++ modules/apps/push-notifications/push-notifications-sender/push-notifications-sender-apple/src/main/java/com/liferay/push/notifications/sender/apple/internal/ApplePushNotificationsSender.java
@@ -128,6 +128,13 @@ public class ApplePushNotificationsSender implements PushNotificationsSender {
 	protected String buildPayload(JSONObject payloadJSONObject) {
 		PayloadBuilder builder = PayloadBuilder.newPayload();
 
+		String title = payloadJSONObject.getString(
+			PushNotificationsConstants.KEY_TITLE);
+
+		if (Validator.isNotNull(title)) {
+			builder.alertTitle(title);
+		}
+
 		if (payloadJSONObject.has(PushNotificationsConstants.KEY_BADGE)) {
 			builder.badge(
 				payloadJSONObject.getInt(PushNotificationsConstants.KEY_BADGE));
@@ -174,13 +181,6 @@ public class ApplePushNotificationsSender implements PushNotificationsSender {
 
 		if (Validator.isNotNull(sound)) {
 			builder.sound(sound);
-		}
-
-		String title = payloadJSONObject.getString(
-			PushNotificationsConstants.KEY_TITLE);
-
-		if (Validator.isNotNull(title)) {
-			builder.alertTitle(title);
 		}
 
 		JSONArray titleLocalizedArgumentsJSONArray =
