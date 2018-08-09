9369a902ed72c363912515431e2e25f873f26562
shuyangzhou <shuyang.zhou@liferay.com>
2018-08-09 09:12:30 -0700

    LPS-84105 Collect all the failures, don't just give up on the first one.

--- modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
+++ modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -89,6 +89,7 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -1463,7 +1464,15 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 		executorService.shutdown();
 
 		for (Future<Void> future : futures) {
-			future.get();
+			try {
+				future.get();
+			}
+			catch (ExecutionException ee) {
+				throwableCollector.collect(ee.getCause());
+			}
+			catch (InterruptedException ie) {
+				throwableCollector.collect(ie);
+			}
 		}
 
 		throwableCollector.rethrow();
