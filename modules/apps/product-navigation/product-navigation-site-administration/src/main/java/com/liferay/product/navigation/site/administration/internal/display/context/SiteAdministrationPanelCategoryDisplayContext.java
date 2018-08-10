10730b17147e07b91a50bcc26380e43aa100f29c
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-08-10 12:01:53 +0200

    LPS-82977 We don't want to show the link to the site if only hidden pages (or no pages) are present. This now works because we establish another priority for the hidden layouts in the service

--- modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/internal/display/context/SiteAdministrationPanelCategoryDisplayContext.java
+++ modules/apps/product-navigation/product-navigation-site-administration/src/main/java/com/liferay/product/navigation/site/administration/internal/display/context/SiteAdministrationPanelCategoryDisplayContext.java
@@ -30,9 +30,11 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -352,7 +354,18 @@ public class SiteAdministrationPanelCategoryDisplayContext {
 	public boolean isDisplaySiteLink() {
 		Group group = getGroup();
 
-		if (group.hasPrivateLayouts() || group.hasPublicLayouts()) {
+		Layout layout = LayoutLocalServiceUtil.fetchFirstLayout(
+			group.getGroupId(), false,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
+
+		if ((layout != null) && !layout.isHidden()) {
+			return true;
+		}
+
+		layout = LayoutLocalServiceUtil.fetchFirstLayout(
+			group.getGroupId(), true, LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
+
+		if ((layout != null) && !layout.isHidden()) {
 			return true;
 		}
 
