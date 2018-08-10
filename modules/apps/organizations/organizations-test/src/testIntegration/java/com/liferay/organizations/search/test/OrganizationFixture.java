ff4cb5de345e5cf0dc3eab544fe690485bdfd87b
Igor Fabiano Nazar <igor.nazar@liferay.com>
2018-08-10 14:14:31 -0300

    LPS-87831 organizations-test: Add integration test to check the indexed fields and reindexing for the Organization.
    https://issues.liferay.com/browse/LPS-87831

---
+++ modules/apps/organizations/organizations-test/src/testIntegration/java/com/liferay/organizations/search/test/OrganizationFixture.java
@@ -0,0 +1,183 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.organizations.search.test;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Country;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ListTypeConstants;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.OrganizationConstants;
+import com.liferay.portal.kernel.model.Region;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CountryService;
+import com.liferay.portal.kernel.service.OrganizationService;
+import com.liferay.portal.kernel.service.RegionService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.service.test.ServiceTestUtil;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+/**
+ * @author Igor Fabiano Nazar
+ * @author Luan Maoski
+ */
+public class OrganizationFixture {
+
+	public OrganizationFixture(
+		OrganizationService organizationService, CountryService countryService,
+		RegionService regionService) {
+
+		_organizationService = organizationService;
+		_countryService = countryService;
+		_regionService = regionService;
+	}
+
+	public Organization createOrganization(String organizationName)
+		throws Exception {
+
+		return createOrganization(organizationName, "united-states", "Alabama");
+	}
+
+	public Organization createOrganization(
+			String organizationName, Map<String, Serializable> expandoValues)
+		throws Exception {
+
+		return createOrganization(
+			organizationName, "united-states", "Alabama", expandoValues);
+	}
+
+	public Organization createOrganization(
+			String organizationName, String countryName, String regionName)
+		throws Exception, PortalException {
+
+		return createOrganization(
+			organizationName, countryName, regionName, null);
+	}
+
+	public Organization createOrganization(
+			String organizationName, String countryName, String regionName,
+			Map<String, Serializable> expando)
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), getUserId());
+
+		Country country = _countryService.getCountryByName(countryName);
+
+		Region region = _getRegion(regionName, country);
+
+		if (expando != null) {
+			serviceContext.setExpandoBridgeAttributes(expando);
+		}
+
+		Organization organization = _organizationService.addOrganization(
+			(long)OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID,
+			organizationName, OrganizationConstants.TYPE_ORGANIZATION,
+			region.getRegionId(), country.getCountryId(),
+			ListTypeConstants.ORGANIZATION_STATUS_DEFAULT,
+			RandomTestUtil.randomString(), RandomTestUtil.randomBoolean(),
+			serviceContext);
+
+		_organizatons.add(organization);
+
+		return organization;
+	}
+
+	public String getCountryNames(Organization organization) {
+		Country country = _countryService.fetchCountry(
+			organization.getCountryId());
+
+		Set<String> countryNames = new HashSet<>();
+
+		for (Locale locale : LanguageUtil.getAvailableLocales()) {
+			String countryName = country.getName(locale);
+
+			countryName = StringUtil.toLowerCase(countryName);
+
+			countryNames.add(countryName);
+		}
+
+		return _joinCountryNames(countryNames);
+	}
+
+	public List<Organization> getOrganizations() {
+		return _organizatons;
+	}
+
+	public void setGroup(Group group) {
+		_group = group;
+	}
+
+	public void setUp() throws Exception {
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+
+		CompanyThreadLocal.setCompanyId(TestPropsValues.getCompanyId());
+	}
+
+	protected long getUserId() throws Exception {
+		return TestPropsValues.getUserId();
+	}
+
+	private Region _getRegion(String regionName, Country country) {
+		List<Region> regions = _regionService.getRegions(
+			country.getCountryId());
+
+		Stream<Region> stream = regions.stream();
+
+		Optional<Region> regionOptional = stream.filter(
+			line -> StringUtil.equalsIgnoreCase(regionName, line.getName())
+		).findFirst();
+
+		return regionOptional.get();
+	}
+
+	private String _joinCountryNames(Set<String> countryNames) {
+		String separator = ", ";
+		StringBuffer sb = new StringBuffer();
+
+		for (String string : countryNames) {
+			if (sb.length() != 0) {
+				sb.append(separator);
+			}
+
+			sb.append(string);
+		}
+
+		return "[" + sb.toString() + "]";
+	}
+
+	private final CountryService _countryService;
+	private Group _group;
+	private final OrganizationService _organizationService;
+	private final List<Organization> _organizatons = new ArrayList<>();
+	private final RegionService _regionService;
+
+}
---
+++ modules/apps/organizations/organizations-test/src/testIntegration/java/com/liferay/organizations/search/test/OrganizationIndexerIndexedFieldsTest.java
@@ -0,0 +1,306 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.organizations.search.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.expando.kernel.model.ExpandoColumn;
+import com.liferay.expando.kernel.model.ExpandoColumnConstants;
+import com.liferay.expando.kernel.model.ExpandoTable;
+import com.liferay.expando.kernel.service.ExpandoColumnLocalService;
+import com.liferay.expando.kernel.service.ExpandoTableLocalService;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.Region;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchEngineHelper;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.CountryService;
+import com.liferay.portal.kernel.service.OrganizationService;
+import com.liferay.portal.kernel.service.RegionService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.search.test.util.ExpandoTableSearchFixture;
+import com.liferay.portal.search.test.util.FieldValuesAssert;
+import com.liferay.portal.search.test.util.IndexedFieldsFixture;
+import com.liferay.portal.search.test.util.IndexerFixture;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+import com.liferay.users.admin.test.util.search.UserSearchFixture;
+
+import java.io.Serializable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Igor Fabiano Nazar
+ * @author Luan Maoski
+ */
+@RunWith(Arquillian.class)
+public class OrganizationIndexerIndexedFieldsTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE,
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		setUpExpandoTableSearchFixture();
+		setUpIndexedFieldsFixture();
+		setUpOrganizationIndexerFixture();
+
+		setUpUserSearchFixture();
+
+		setUpOrganizationFixture();
+	}
+
+	@Test
+	public void testIndexedFields() throws Exception {
+		Organization organization = organizationFixture.createOrganization(
+			"abcd efgh");
+
+		String searchTerm = "abcd";
+
+		Document document = organizationIndexerFixture.searchOnlyOne(
+			searchTerm);
+
+		indexedFieldsFixture.postProcessDocument(document);
+
+		Map<String, String> expected = _expectedFieldValues(organization);
+
+		FieldValuesAssert.assertFieldValues(expected, document, searchTerm);
+	}
+
+	@Test
+	public void testIndexedFieldsWithExpando() throws Exception {
+		String expandoColumnName = "expandoColumnName";
+		String expandoColumnObs = "expandoColumnObs";
+
+		expandoTableSearchFixture.addExpandoColumn(
+			Organization.class, ExpandoColumnConstants.INDEX_TYPE_KEYWORD,
+			expandoColumnObs, expandoColumnName);
+
+		Map<String, Serializable> expandoValues = new HashMap<>();
+
+		expandoValues.put(expandoColumnName, "Software Developer");
+		expandoValues.put(expandoColumnObs, "Software Engineer");
+
+		Organization organization = organizationFixture.createOrganization(
+			"My Organization", expandoValues);
+
+		String searchTerm = "Developer";
+
+		Document document = organizationIndexerFixture.searchOnlyOne(
+			searchTerm);
+
+		indexedFieldsFixture.postProcessDocument(document);
+
+		Map<String, String> expected = _expectedFieldValuesWithExpando(
+			organization);
+
+		FieldValuesAssert.assertFieldValues(expected, document, searchTerm);
+	}
+
+	protected void setUpExpandoTableSearchFixture() {
+		expandoTableSearchFixture = new ExpandoTableSearchFixture(
+			classNameLocalService, expandoColumnLocalService,
+			expandoTableLocalService);
+
+		_expandoColumns = expandoTableSearchFixture.getExpandoColumns();
+		_expandoTables = expandoTableSearchFixture.getExpandoTables();
+	}
+
+	protected void setUpIndexedFieldsFixture() {
+		indexedFieldsFixture = new IndexedFieldsFixture(
+			resourcePermissionLocalService, searchEngineHelper);
+	}
+
+	protected void setUpOrganizationFixture() throws Exception {
+		organizationFixture = new OrganizationFixture(
+			organizationService, countryService, regionService);
+
+		organizationFixture.setUp();
+
+		organizationFixture.setGroup(group);
+
+		_organizations = organizationFixture.getOrganizations();
+	}
+
+	protected void setUpOrganizationIndexerFixture() {
+		organizationIndexerFixture = new IndexerFixture<>(Organization.class);
+	}
+
+	protected void setUpUserSearchFixture() throws Exception {
+		userSearchFixture = new UserSearchFixture();
+
+		userSearchFixture.setUp();
+
+		_groups = userSearchFixture.getGroups();
+		_users = userSearchFixture.getUsers();
+
+		group = userSearchFixture.addGroup();
+	}
+
+	@Inject
+	protected ClassNameLocalService classNameLocalService;
+
+	@Inject
+	protected CountryService countryService;
+
+	@Inject
+	protected ExpandoColumnLocalService expandoColumnLocalService;
+
+	@Inject
+	protected ExpandoTableLocalService expandoTableLocalService;
+
+	protected ExpandoTableSearchFixture expandoTableSearchFixture;
+	protected Group group;
+	protected IndexedFieldsFixture indexedFieldsFixture;
+	protected OrganizationFixture organizationFixture;
+	protected IndexerFixture<Organization> organizationIndexerFixture;
+
+	@Inject
+	protected OrganizationService organizationService;
+
+	@Inject
+	protected RegionService regionService;
+
+	@Inject
+	protected ResourcePermissionLocalService resourcePermissionLocalService;
+
+	@Inject
+	protected SearchEngineHelper searchEngineHelper;
+
+	protected UserSearchFixture userSearchFixture;
+
+	private Map<String, String> _expectedFieldValues(Organization organization)
+		throws Exception {
+
+		Map<String, String> map = new HashMap<>();
+
+		String countryName = organizationFixture.getCountryNames(organization);
+
+		Region region = regionService.getRegion(organization.getRegionId());
+
+		String regionName = StringUtil.toLowerCase(region.getName());
+
+		map.put("country", countryName);
+		map.put("region", regionName);
+		map.put(
+			Field.NAME + "_sortable",
+			StringUtil.toLowerCase(organization.getName()));
+		map.put(
+			Field.ENTRY_CLASS_PK,
+			String.valueOf(organization.getOrganizationId()));
+		map.put(
+			"nameTreePath_String_sortable",
+			StringUtil.toLowerCase(organization.getName()));
+		map.put(Field.ENTRY_CLASS_NAME, Organization.class.getName());
+		map.put(
+			Field.USER_NAME,
+			StringUtil.toLowerCase(organization.getUserName()));
+		map.put(
+			"parentOrganizationId",
+			String.valueOf(organization.getParentOrganizationId()));
+		map.put(
+			Field.ORGANIZATION_ID,
+			String.valueOf(organization.getOrganizationId()));
+		map.put(Field.USER_ID, String.valueOf(organization.getUserId()));
+		map.put(Field.COMPANY_ID, String.valueOf(organization.getCompanyId()));
+		map.put("nameTreePath", organization.getName());
+		map.put(Field.NAME, organization.getName());
+		map.put(Field.TREE_PATH, organization.getTreePath());
+
+		indexedFieldsFixture.populateUID(
+			Organization.class.getName(), organization.getOrganizationId(),
+			map);
+
+		map.put(Field.TYPE, organization.getType());
+
+		_populateDates(organization, map);
+		_populateRoles(organization, map);
+
+		return map;
+	}
+
+	private Map<String, String> _expectedFieldValuesWithExpando(
+			Organization organization)
+		throws Exception {
+
+		Map<String, String> expectedFieldValues = _expectedFieldValues(
+			organization);
+
+		expectedFieldValues.put(
+			"expando__keyword__custom_fields__expandoColumnName",
+			"Software Developer");
+		expectedFieldValues.put(
+			"expando__keyword__custom_fields__expandoColumnObs",
+			"Software Engineer");
+
+		return expectedFieldValues;
+	}
+
+	private void _populateDates(
+		Organization organization, Map<String, String> map) {
+
+		indexedFieldsFixture.populateDate(
+			Field.CREATE_DATE, organization.getCreateDate(), map);
+		indexedFieldsFixture.populateDate(
+			Field.MODIFIED_DATE, organization.getModifiedDate(), map);
+	}
+
+	private void _populateRoles(
+			Organization organization, Map<String, String> map)
+		throws Exception {
+
+		indexedFieldsFixture.populateRoleIdFields(
+			organization.getCompanyId(), Organization.class.getName(),
+			organization.getOrganizationId(), organization.getGroupId(), null,
+			map);
+	}
+
+	@DeleteAfterTestRun
+	private List<ExpandoColumn> _expandoColumns;
+
+	@DeleteAfterTestRun
+	private List<ExpandoTable> _expandoTables;
+
+	@DeleteAfterTestRun
+	private List<Group> _groups;
+
+	@DeleteAfterTestRun
+	private List<Organization> _organizations;
+
+	@DeleteAfterTestRun
+	private List<User> _users;
+
+}
