edea7ee3669e86d353007aaeace7cbda7a0cc7e5
jpince <jason.pince@liferay.com>
2018-08-13 13:03:38 -0700

    LRQA-42916 Temporarily Ignore Failing Test

--- modules/apps/message-boards/message-boards-test/src/testIntegration/java/com/liferay/message/boards/lar/test/MBPortletDataHandlerTest.java
+++ modules/apps/message-boards/message-boards-test/src/testIntegration/java/com/liferay/message/boards/lar/test/MBPortletDataHandlerTest.java
@@ -52,6 +52,7 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -59,6 +60,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Zsolt Berentey
  */
+@Ignore
 @RunWith(Arquillian.class)
 public class MBPortletDataHandlerTest extends BasePortletDataHandlerTestCase {
 
