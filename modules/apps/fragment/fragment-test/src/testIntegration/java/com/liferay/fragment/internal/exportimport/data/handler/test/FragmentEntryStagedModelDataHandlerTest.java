5d995b7ada1080c57ad342d5327a855918ee5e74
kyle-miho <kyle.miho@liferay.com>
2018-08-14 16:27:10 -0700

    LPS-84391 Add integration test cover fragment entry changes not automatically propagating to fragment entries

---
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/exportimport/data/handler/test/FragmentEntryStagedModelDataHandlerTest.java
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.internal.exportimport.data.handler.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
+import com.liferay.exportimport.test.util.lar.BaseStagedModelDataHandlerTestCase;
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalServiceUtil;
+import com.liferay.fragment.service.FragmentEntryLocalServiceUtil;
+import com.liferay.fragment.util.FragmentTestUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.util.test.LayoutTestUtil;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Kyle Miho
+ */
+@RunWith(Arquillian.class)
+public class FragmentEntryStagedModelDataHandlerTest
+	extends BaseStagedModelDataHandlerTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+
+		LayoutTestUtil.addLayout(stagingGroup);
+	}
+
+	@Test
+	public void testUpdateFragmentEntryWithFragmentEntryLink()
+		throws Exception {
+
+		Map<String, List<StagedModel>> dependentStagedModelsMap =
+			addDependentStagedModelsMap(stagingGroup);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				stagingGroup.getGroupId(), TestPropsValues.getUserId());
+
+		StagedModel stagedModel = addStagedModel(
+			stagingGroup, dependentStagedModelsMap);
+
+		FragmentEntry fragmentEntry = (FragmentEntry)stagedModel;
+
+		FragmentEntryLink fragmentEntryLink =
+			FragmentEntryLinkLocalServiceUtil.addFragmentEntryLink(
+				TestPropsValues.getUserId(), stagingGroup.getGroupId(), 0,
+				fragmentEntry.getFragmentEntryId(),
+				PortalUtil.getClassNameId(Layout.class),
+				stagingGroup.getDefaultPublicPlid(), fragmentEntry.getCss(),
+				fragmentEntry.getHtml(), fragmentEntry.getJs(), "", 0,
+				serviceContext);
+
+		stagedModel = FragmentEntryLocalServiceUtil.updateFragmentEntry(
+			TestPropsValues.getUserId(), fragmentEntry.getFragmentEntryId(),
+			fragmentEntry.getName(), "css", "html", "js", 0);
+
+		try {
+			exportImportStagedModel(stagedModel);
+		}
+		finally {
+			ExportImportThreadLocal.setPortletImportInProcess(false);
+		}
+
+		StagedModel importedStagedModel = getStagedModel(
+			stagedModel.getUuid(), liveGroup);
+
+		FragmentEntry importedFragmentEntry =
+			(FragmentEntry)importedStagedModel;
+
+		Assert.assertNotNull(importedStagedModel);
+
+		Assert.assertNotEquals(
+			fragmentEntryLink.getCss(), importedFragmentEntry.getCss());
+		Assert.assertNotEquals(
+			fragmentEntryLink.getHtml(), importedFragmentEntry.getHtml());
+		Assert.assertNotEquals(
+			fragmentEntryLink.getJs(), importedFragmentEntry.getJs());
+		validateImportedStagedModel(stagedModel, importedStagedModel);
+	}
+
+	@Override
+	protected StagedModel addStagedModel(
+			Group group,
+			Map<String, List<StagedModel>> dependentStagedModelsMap)
+		throws Exception {
+
+		FragmentCollection fragmentCollection =
+			FragmentTestUtil.addFragmentCollection(group.getGroupId());
+
+		return FragmentTestUtil.addFragmentEntry(
+			fragmentCollection.getFragmentCollectionId());
+	}
+
+	@Override
+	protected StagedModel getStagedModel(String uuid, Group group) {
+		return FragmentEntryLocalServiceUtil.fetchFragmentEntryByUuidAndGroupId(
+			uuid, group.getGroupId());
+	}
+
+	@Override
+	protected Class<? extends StagedModel> getStagedModelClass() {
+		return FragmentEntry.class;
+	}
+
+	@Override
+	protected void validateImportedStagedModel(
+			StagedModel stagedModel, StagedModel importedStagedModel)
+		throws Exception {
+
+		FragmentEntry fragmentEntry = (FragmentEntry)stagedModel;
+		FragmentEntry importedFragmentEntry =
+			(FragmentEntry)importedStagedModel;
+
+		Assert.assertEquals(
+			importedFragmentEntry.getHtml(), fragmentEntry.getHtml());
+		Assert.assertEquals(
+			importedFragmentEntry.getCss(), fragmentEntry.getCss());
+		Assert.assertEquals(
+			importedFragmentEntry.getJs(), fragmentEntry.getJs());
+	}
+
+}
