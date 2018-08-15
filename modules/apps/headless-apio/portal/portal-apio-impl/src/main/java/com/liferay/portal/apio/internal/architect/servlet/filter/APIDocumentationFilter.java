63e37868af9b696aef485d2569f06ec9f054bd2b
Brian Chan <brian.chan@liferay.com>
2018-08-15 10:22:04 -0700

    APIO-162 put it in the right package, type 'git grep "extends BaseFilter"'

--- modules/apps/headless-apio/portal/portal-apio-impl/src/main/java/com/liferay/portal/apio/internal/architect/servlet/filter/APIDocumentationFilter.java
+++ modules/apps/headless-apio/portal/portal-apio-impl/src/main/java/com/liferay/portal/apio/internal/architect/servlet/filter/APIDocumentationFilter.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.apio.internal.architect.filter;
+package com.liferay.portal.apio.internal.architect.servlet.filter;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
