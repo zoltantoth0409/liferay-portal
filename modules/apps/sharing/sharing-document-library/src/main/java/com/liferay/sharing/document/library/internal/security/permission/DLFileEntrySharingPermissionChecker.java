e9af0dbf4ce5032cd5fedbe109350efb95543952
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-08-16 23:43:33 +0200

    LPS-84523 Inmediately initialize the component

--- modules/apps/sharing/sharing-document-library/src/main/java/com/liferay/sharing/document/library/internal/security/permission/DLFileEntrySharingPermissionChecker.java
+++ modules/apps/sharing/sharing-document-library/src/main/java/com/liferay/sharing/document/library/internal/security/permission/DLFileEntrySharingPermissionChecker.java
@@ -33,6 +33,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Sergio González
  */
 @Component(
+	immediate = true,
 	property = "model.class.name=com.liferay.portal.kernel.repository.model.DLFileEntry",
 	service = SharingPermissionChecker.class
 )
