946f262cfa33c487c5252d49ec7abd259d22d6cf
Leslie Wong <leslie.wong@liferay.com>
2018-08-15 17:44:05 -0700

    LRQA-42325 PoshiRunner should only worry about test running/initialization

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunner.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/PoshiRunner.java
@@ -217,21 +217,9 @@ public class PoshiRunner {
 			String namespacedClassCommandName)
 		throws Exception {
 
-		String className =
-			PoshiRunnerGetterUtil.getClassNameFromNamespacedClassCommandName(
-				namespacedClassCommandName);
 		String namespace =
 			PoshiRunnerGetterUtil.getNamespaceFromNamespacedClassCommandName(
 				namespacedClassCommandName);
-
-		List<Element> varElements = PoshiRunnerContext.getRootVarElements(
-			"test-case", className, namespace);
-
-		for (Element varElement : varElements) {
-			PoshiRunnerExecutor.runRootVarElement(varElement, false);
-		}
-
-		PoshiRunnerVariablesUtil.pushCommandMap();
 
 		String classCommandName =
 			PoshiRunnerGetterUtil.
@@ -245,7 +233,8 @@ public class PoshiRunner {
 			PoshiRunnerStackTraceUtil.startStackTrace(
 				namespacedClassCommandName, "test-case");
 
-			PoshiRunnerExecutor.parseElement(commandElement);
+			PoshiRunnerExecutor.runTestCaseCommandElement(
+				commandElement, namespacedClassCommandName);
 
 			PoshiRunnerStackTraceUtil.emptyStackTrace();
 		}
