2dc218f86382f3e3e64924d757a373bccde87b75
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-08-17 10:55:29 +0200

    LPS-84562 Create dummy view

---
+++ modules/apps/sharing/sharing-web/src/main/resources/META-INF/resources/Sharing.es.js
@@ -0,0 +1,9 @@
+import templates from './Sharing.soy';
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+
+class Sharing extends Component {}
+
+Soy.register(Sharing, templates);
+
+export default Sharing;
