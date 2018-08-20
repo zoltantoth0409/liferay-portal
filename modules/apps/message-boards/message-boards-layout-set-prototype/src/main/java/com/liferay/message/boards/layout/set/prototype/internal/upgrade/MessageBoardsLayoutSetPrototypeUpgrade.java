a44d406a363d48e5e0cb9b40e4f13cc707e46e62
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2018-08-20 15:30:19 +0200

    LPS-84602 ensure upgrade gets executed in next execution.

--- modules/apps/message-boards/message-boards-layout-set-prototype/src/main/java/com/liferay/message/boards/layout/set/prototype/internal/upgrade/MessageBoardsLayoutSetPrototypeUpgrade.java
+++ modules/apps/message-boards/message-boards-layout-set-prototype/src/main/java/com/liferay/message/boards/layout/set/prototype/internal/upgrade/MessageBoardsLayoutSetPrototypeUpgrade.java
@@ -15,6 +15,7 @@
 package com.liferay.message.boards.layout.set.prototype.internal.upgrade;
 
 import com.liferay.message.boards.layout.set.prototype.internal.upgrade.v1_0_0.UpgradeLocalizedColumn;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
@@ -28,7 +29,8 @@ public class MessageBoardsLayoutSetPrototypeUpgrade
 
 	@Override
 	public void register(Registry registry) {
-		registry.register("0.0.0", "1.0.0", new UpgradeLocalizedColumn());
+		registry.register("0.0.0", "1.0.0", new DummyUpgradeStep());
+		registry.register("1.0.0", "1.0.1", new UpgradeLocalizedColumn());
 	}
 
 }
