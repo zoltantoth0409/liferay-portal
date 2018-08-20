6a2e698d175ee72a5ae6c032d3f302360dab7c15
Jonathan McCann <jonathan.mccann@liferay.com>
2018-08-20 07:32:09 -0700

    LPS-84481 Escaping description

--- modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/asset/BlogsEntryAssetRenderer.java
+++ modules/apps/collaboration/blogs/blogs-web/src/main/java/com/liferay/blogs/web/asset/BlogsEntryAssetRenderer.java
@@ -137,7 +137,7 @@ public class BlogsEntryAssetRenderer
 				AssetUtil.ASSET_ENTRY_ABSTRACT_LENGTH);
 		}
 
-		String summary = _entry.getDescription();
+		String summary = HtmlUtil.escape(_entry.getDescription());
 
 		if (Validator.isNull(summary)) {
 			summary = HtmlUtil.stripHtml(
