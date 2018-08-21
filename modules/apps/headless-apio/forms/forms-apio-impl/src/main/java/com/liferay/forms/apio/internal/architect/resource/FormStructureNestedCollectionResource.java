c9dbdd734e4f669596fb6903a5fcb5b23d78cf11
Javier Gamarra <nhpatt@gmail.com>
2018-08-21 08:39:44 +0200

    LPS-84597 move customization to helper class

--- modules/apps/headless-apio/forms/forms-apio-impl/src/main/java/com/liferay/forms/apio/internal/architect/resource/FormStructureNestedCollectionResource.java
+++ modules/apps/headless-apio/forms/forms-apio-impl/src/main/java/com/liferay/forms/apio/internal/architect/resource/FormStructureNestedCollectionResource.java
@@ -14,30 +14,20 @@
 
 package com.liferay.forms.apio.internal.architect.resource;
 
-import static com.liferay.forms.apio.internal.util.LocalizedValueUtil.getLocalizedString;
-
-import com.liferay.apio.architect.functional.Try;
 import com.liferay.apio.architect.pagination.PageItems;
 import com.liferay.apio.architect.pagination.Pagination;
-import com.liferay.apio.architect.representor.NestedRepresentor;
 import com.liferay.apio.architect.representor.Representor;
 import com.liferay.apio.architect.resource.NestedCollectionResource;
 import com.liferay.apio.architect.routes.ItemRoutes;
 import com.liferay.apio.architect.routes.NestedCollectionRoutes;
 import com.liferay.content.space.apio.architect.identifier.ContentSpaceIdentifier;
 import com.liferay.dynamic.data.lists.model.DDLRecordSet;
-import com.liferay.dynamic.data.mapping.model.DDMForm;
-import com.liferay.dynamic.data.mapping.model.DDMFormSuccessPageSettings;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
-import com.liferay.dynamic.data.mapping.model.DDMStructureVersion;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.forms.apio.architect.identifier.StructureIdentifier;
 import com.liferay.forms.apio.internal.helper.FormStructureRepresentorBuilderHelper;
-import com.liferay.person.apio.architect.identifier.PersonIdentifier;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.service.ClassNameService;
-import com.liferay.structure.apio.architect.util.StructureRepresentorUtil;
 
 import java.util.List;
 
@@ -58,22 +48,6 @@ public class FormStructureNestedCollectionResource
 		<DDMStructure, Long, StructureIdentifier, Long,
 			ContentSpaceIdentifier> {
 
-	public static DDMFormSuccessPageSettings getSuccessPage(
-		DDMStructure ddmStructure) {
-
-		DDMForm ddmForm = ddmStructure.getDDMForm();
-
-		return ddmForm.getDDMFormSuccessPageSettings();
-	}
-
-	public static DDMStructureVersion getVersion(DDMStructure ddmStructure) {
-		return Try.fromFallible(
-			ddmStructure::getStructureVersion
-		).orElse(
-			null
-		);
-	}
-
 	@Override
 	public NestedCollectionRoutes<DDMStructure, Long, Long> collectionRoutes(
 		NestedCollectionRoutes.Builder<DDMStructure, Long, Long> builder) {
@@ -90,7 +64,8 @@ public class FormStructureNestedCollectionResource
 	public ItemRoutes<DDMStructure, Long> itemRoutes(
 		ItemRoutes.Builder<DDMStructure, Long> builder) {
 
-		return builder.addGetter(this::_getItem).build();
+		return builder.addGetter(
+			_ddmStructureLocalService::getStructure).build();
 	}
 
 	@Override
@@ -101,57 +76,7 @@ public class FormStructureNestedCollectionResource
 			_formStructureRepresentorBuilderHelper.buildDDMStructureFirstStep(
 				builder);
 
-		Representor.FirstStep<DDMStructure> bidirectionalModelStepBuilder =
-			ddmStructureFirstStep.addBidirectionalModel(
-				"contentSpace", "formStructures", ContentSpaceIdentifier.class,
-				DDMStructure::getGroupId);
-
-		bidirectionalModelStepBuilder.addNested(
-			"version", FormStructureNestedCollectionResource::getVersion,
-			FormStructureNestedCollectionResource::_buildVersion
-		).addNested(
-			"successPage",
-			FormStructureNestedCollectionResource::getSuccessPage,
-			FormStructureNestedCollectionResource::_buildSuccessPageSettings
-		).addNestedList(
-			"fields", _structureRepresentorUtil::getPages,
-			ddmFormFieldBuilder ->
-				_formStructureRepresentorBuilderHelper.buildFormPages(
-					ddmFormFieldBuilder).build()
-		);
-
-		return bidirectionalModelStepBuilder.build();
-	}
-
-	private static NestedRepresentor<DDMFormSuccessPageSettings>
-		_buildSuccessPageSettings(
-			NestedRepresentor.Builder<DDMFormSuccessPageSettings> builder) {
-
-		return builder.types(
-			"FormSuccessPageSettings"
-		).addBoolean(
-			"isEnabled", DDMFormSuccessPageSettings::isEnabled
-		).addLocalizedStringByLocale(
-			"headline", getLocalizedString(DDMFormSuccessPageSettings::getTitle)
-		).addLocalizedStringByLocale(
-			"text", getLocalizedString(DDMFormSuccessPageSettings::getBody)
-		).build();
-	}
-
-	private static NestedRepresentor<DDMStructureVersion> _buildVersion(
-		NestedRepresentor.Builder<DDMStructureVersion> nestedBuilder) {
-
-		return nestedBuilder.types(
-			"StructureVersion"
-		).addLinkedModel(
-			"creator", PersonIdentifier.class, DDMStructureVersion::getUserId
-		).addString(
-			"name", DDMStructureVersion::getVersion
-		).build();
-	}
-
-	private DDMStructure _getItem(Long structureId) throws PortalException {
-		return _ddmStructureLocalService.getStructure(structureId);
+		return ddmStructureFirstStep.build();
 	}
 
 	private PageItems<DDMStructure> _getPageItems(
@@ -180,8 +105,5 @@ public class FormStructureNestedCollectionResource
 	@Reference
 	private FormStructureRepresentorBuilderHelper
 		_formStructureRepresentorBuilderHelper;
-
-	@Reference
-	private StructureRepresentorUtil _structureRepresentorUtil;
 
 }
