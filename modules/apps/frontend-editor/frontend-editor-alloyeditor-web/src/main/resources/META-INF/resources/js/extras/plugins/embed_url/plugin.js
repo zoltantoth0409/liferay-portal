21c0a9924a6a229f597e681042f3521383df703b
pavel-savinov <pavel.savinov@liferay.com>
2018-08-21 09:11:30 +0200

    LPS-84222 Check if styles attribute is empty before parsing a JSON object from it

--- modules/apps/frontend-editor/frontend-editor-alloyeditor-web/src/main/resources/META-INF/resources/js/extras/plugins/embed_url/plugin.js
+++ modules/apps/frontend-editor/frontend-editor-alloyeditor-web/src/main/resources/META-INF/resources/js/extras/plugins/embed_url/plugin.js
@@ -379,7 +379,9 @@ if (!CKEDITOR.plugins.get('embedurl')) {
 
 							// Sync dimensions and alignment with editor wrapper
 
-							let styles = JSON.parse(instance.element.getAttribute('data-styles'));
+							const stylesJSON = instance.element.getAttribute('data-styles');
+
+							let styles = stylesJSON ? JSON.parse(stylesJSON) : null;
 
 							if (!styles) {
 								const iframe = instance.wrapper.findOne('iframe');
