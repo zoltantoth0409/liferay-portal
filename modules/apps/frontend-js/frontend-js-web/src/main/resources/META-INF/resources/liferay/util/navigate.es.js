dafbec79649be40d2ad7521e5575de6c5a1cb65c
Chema Balsas <jose.balsas@liferay.com>
2018-08-22 17:26:58 +0200

    LPS-84600 Adds support for lifecycle events and updates usage in openSimpleInputModal module

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/navigate.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/navigate.es.js
@@ -3,12 +3,22 @@
  * request through the SPA engine. If not, it will simple change the document
  * location.
  * @param {!string} url Destination URL to navigate
+ * @param {?object} listeners Object with key-value pairs with callbacks for
+ * specific page lifecycle events
  * @review
  */
 
-export default function(url) {
+export default function(url, listeners) {
 	if (Liferay.SPA) {
 		Liferay.SPA.app.navigate(url);
+
+		if (listeners) {
+			Object.keys(listeners).forEach(
+				key => {
+					Liferay.once(key, listeners[key]);
+				}
+			);
+		}
 	}
 	else {
 		window.location.href = url;
