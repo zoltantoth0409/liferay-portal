62c0cf7c984b0f08e05bd79cf34331fea0302217
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-08-23 18:08:46 +0200

    LPS-84521 Fix CalEvent Importer

--- modules/apps/calevent/calevent-importer/src/main/java/com/liferay/calevent/importer/internal/CalEventImporter.java
+++ modules/apps/calevent/calevent-importer/src/main/java/com/liferay/calevent/importer/internal/CalEventImporter.java
@@ -35,6 +35,7 @@ import com.liferay.calendar.recurrence.RecurrenceSerializer;
 import com.liferay.calendar.recurrence.Weekday;
 import com.liferay.calendar.service.CalendarBookingLocalService;
 import com.liferay.calendar.service.CalendarResourceLocalService;
+import com.liferay.calevent.importer.internal.json.jabsorb.serializer.CalEventImporterLiferayJSONDeserializationWhitelist;
 import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.message.boards.kernel.model.MBDiscussion;
 import com.liferay.message.boards.kernel.model.MBMessage;
@@ -1454,6 +1455,11 @@ public class CalEventImporter {
 	private AssetVocabularyLocalService _assetVocabularyLocalService;
 	private CalendarBookingLocalService _calendarBookingLocalService;
 	private CalendarResourceLocalService _calendarResourceLocalService;
+
+	@Reference
+	private CalEventImporterLiferayJSONDeserializationWhitelist
+		_calEventImporterLiferayJSONDeserializationWhitelist;
+
 	private ClassNameLocalService _classNameLocalService;
 	private CounterLocalService _counterLocalService;
 	private GroupLocalService _groupLocalService;
