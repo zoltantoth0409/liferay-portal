b6415b182763cfc891ffb4e57ac272771df9ee1a
Peter Shin <peter.shin@liferay.com>
2018-08-23 15:51:30 -0700

    LPS-74544 Apply

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BNDSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/BNDSourceProcessor.java
@@ -14,19 +14,13 @@
 
 package com.liferay.source.formatter;
 
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.source.formatter.checks.util.BNDSourceUtil;
 import com.liferay.source.formatter.util.FileUtil;
+import com.liferay.source.formatter.util.ModulesPropertiesUtil;
 
 import java.io.File;
 import java.io.IOException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
  * @author Hugo Huijser
@@ -44,25 +38,22 @@ public class BNDSourceProcessor extends BaseSourceProcessor {
 	}
 
 	@Override
-	protected File format(
-			File file, String fileName, String absolutePath, String content)
-		throws Exception {
-
-		file = super.format(file, fileName, absolutePath, content);
-
-		_processPortalModulesBundleVersion(absolutePath, content);
-
-		return file;
-	}
-
-	@Override
 	protected void postFormat() throws IOException {
-		_processPortalModulesBundleVersions();
-	}
+		if (!_checkModulesPropertiesFile()) {
+			return;
+		}
 
-	@Override
-	protected void preFormat() throws IOException {
-		_initPortalModulesVersionsMap();
+		File modulesPropertiesFile = new File(
+			getPortalDir(), "modules/modules.properties");
+
+		String newContent = ModulesPropertiesUtil.getContent(getPortalDir());
+		String oldContent = FileUtil.read(modulesPropertiesFile);
+
+		if (!oldContent.equals(newContent)) {
+			FileUtil.write(modulesPropertiesFile, newContent);
+
+			System.out.println("Updated 'modules.properties'");
+		}
 	}
 
 	private boolean _checkModulesPropertiesFile() {
@@ -81,127 +72,6 @@ public class BNDSourceProcessor extends BaseSourceProcessor {
 		return true;
 	}
 
-	private String _getBundleSymbolicName(String absolutePath, String content) {
-		if (absolutePath.endsWith("/portal-impl/bnd.bnd")) {
-			return "com.liferay.portal.impl";
-		}
-		else if (absolutePath.endsWith("/portal-kernel/bnd.bnd")) {
-			return "com.liferay.portal.kernel";
-		}
-		else if (absolutePath.endsWith("/portal-test-integration/bnd.bnd")) {
-			return "com.liferay.portal.test.integration";
-		}
-		else if (absolutePath.endsWith("/portal-test/bnd.bnd")) {
-			return "com.liferay.portal.test";
-		}
-		else if (absolutePath.endsWith("/portal-support-tomcat/bnd.bnd")) {
-			return "com.liferay.support.tomcat";
-		}
-		else if (absolutePath.endsWith("/util-bridges/bnd.bnd")) {
-			return "com.liferay.util.bridges";
-		}
-		else if (absolutePath.endsWith("/util-java/bnd.bnd")) {
-			return "com.liferay.util.java";
-		}
-		else if (absolutePath.endsWith("/util-slf4j/bnd.bnd")) {
-			return "com.liferay.util.slf4j";
-		}
-		else if (absolutePath.endsWith("/util-taglib/bnd.bnd")) {
-			return "com.liferay.util.taglib";
-		}
-
-		return BNDSourceUtil.getDefinitionValue(content, "Bundle-SymbolicName");
-	}
-
-	private void _initPortalModulesVersionsMap() throws IOException {
-		_portalModulesBundleVersionsMap = new TreeMap<>();
-
-		if (!_checkModulesPropertiesFile()) {
-			return;
-		}
-
-		File modulesPropertiesFile = new File(
-			getPortalDir(), "modules/modules.properties");
-
-		if (!modulesPropertiesFile.exists()) {
-			return;
-		}
-
-		String content = FileUtil.read(modulesPropertiesFile);
-
-		for (String line : StringUtil.splitLines(content)) {
-			String[] array = StringUtil.split(line, StringPool.EQUAL);
-
-			if (array.length == 2) {
-				_portalModulesBundleVersionsMap.put(array[0], array[1]);
-			}
-		}
-	}
-
-	private synchronized void _processPortalModulesBundleVersion(
-		String absolutePath, String content) {
-
-		if (!_checkModulesPropertiesFile()) {
-			return;
-		}
-
-		if (!absolutePath.endsWith("/bnd.bnd") ||
-			absolutePath.contains("/private/") ||
-			absolutePath.contains("/test/") ||
-			absolutePath.contains("/test-integration/")) {
-
-			return;
-		}
-
-		String bundleSymbolicName = _getBundleSymbolicName(
-			absolutePath, content);
-
-		if (Validator.isNull(bundleSymbolicName)) {
-			return;
-		}
-
-		if (!bundleSymbolicName.startsWith("com.liferay.")) {
-			return;
-		}
-
-		String bundleVersion = BNDSourceUtil.getDefinitionValue(
-			content, "Bundle-Version");
-
-		if (Validator.isNull(bundleVersion)) {
-			return;
-		}
-
-		_portalModulesBundleVersionsMap.put(bundleSymbolicName, bundleVersion);
-	}
-
-	private void _processPortalModulesBundleVersions() throws IOException {
-		if (!_checkModulesPropertiesFile()) {
-			return;
-		}
-
-		List<String> modulesProperties = new ArrayList<>();
-
-		for (Map.Entry<String, String> entry :
-				_portalModulesBundleVersionsMap.entrySet()) {
-
-			modulesProperties.add(entry.getKey() + "=" + entry.getValue());
-		}
-
-		File modulesPropertiesFile = new File(
-			getPortalDir(), "modules/modules.properties");
-
-		String newContent = StringUtil.merge(modulesProperties, "\n");
-		String oldContent = FileUtil.read(modulesPropertiesFile);
-
-		if (!newContent.equals(oldContent)) {
-			FileUtil.write(modulesPropertiesFile, newContent);
-
-			System.out.println("Updated 'modules.properties'");
-		}
-	}
-
 	private static final String[] _INCLUDES = {"**/*.bnd"};
-
-	private Map<String, String> _portalModulesBundleVersionsMap;
 
 }
