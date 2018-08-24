bfe08cb0f733a6b73b98f0e31b0b9626160837d1
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2018-08-24 14:59:07 -0300

    LPS-84279 Create test to check if sidebar opens when add field button gets clicked

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/pages/builder/__tests__/Builder.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/pages/builder/__tests__/Builder.es.js
@@ -1,7 +1,9 @@
 import Builder from '../Builder.es';
+import {dom as MetalTestUtil} from 'metal-dom';
 
 let component;
 const spritemap = 'icons.svg';
+let addButton;
 
 describe(
 	'Builder',
@@ -9,6 +11,10 @@ describe(
 		beforeEach(
 			() => {
 				jest.useFakeTimers();
+
+				MetalTestUtil.enterDocument('<button id="addFieldButton"></button>');
+
+				addButton = document.querySelector('#addFieldButton');
 
 				component = new Builder(
 					{
@@ -20,6 +26,7 @@ describe(
 
 		afterEach(
 			() => {
+				MetalTestUtil.exitDocument(addButton);
 				if (component) {
 					component.dispose();
 				}
@@ -177,6 +184,17 @@ describe(
 				jest.runAllTimers();
 
 				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should open sidebar when addField button is gets clicked',
+			() => {
+				MetalTestUtil.triggerEvent(addButton, 'click', {});
+
+				const sidebar = document.querySelector('.sidebar-container.show');
+
+				expect(sidebar).not.toBe(null);
 			}
 		);
 	}
