0c86d75f9a31dffe8e50a059958bb427d91436ff
Clóvis Neto <cloves13@hotmail.com>
2018-08-24 14:47:50 -0300

    LPS-84275 Put the page structure in a config file, in order to reuse it in the components page state

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/util/config.es.js
@@ -0,0 +1,22 @@
+import {Config} from 'metal-state';
+
+export const pageStructure = Config.shapeOf(
+	{
+		description: Config.string(),
+		rows: Config.arrayOf(
+			Config.shapeOf(
+				{
+					columns: Config.arrayOf(
+						Config.shapeOf(
+							{
+								fields: Config.array(),
+								size: Config.number(),
+							}
+						)
+					),
+				}
+			)
+		),
+		title: Config.string(),
+	}
+);
