3b072603f1c4560e2c72bfada4d9efe41183f3bc
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-08-24 14:26:31 +0200

    LPS-84907 Adding custom html vertical card

---
+++ modules/apps/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/HtmlVerticalCardTag.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alejandro Tardín
+ */
+public class HtmlVerticalCardTag extends VerticalCardTag {
+
+	public void setHtml(String html) {
+		_html = html;
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_html = null;
+	}
+
+	@Override
+	protected String getPage() {
+		return "/card/html_vertical_card/page.jsp";
+	}
+
+	@Override
+	protected boolean isCleanUpSetAttributes() {
+		return true;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		super.setAttributes(request);
+
+		request.setAttribute("liferay-frontend:card:html", _html);
+	}
+
+	private String _html;
+
+}
