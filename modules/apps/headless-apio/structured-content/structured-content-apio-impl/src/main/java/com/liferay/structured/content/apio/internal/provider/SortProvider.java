d06bfa94843da6b488ae94e82b180ddeed45f598
Cristina González Castellano <cristina.gonzalez@liferay.com>
2018-08-24 15:52:50 +0200

    LPS-84717 New Sort provider that reads the sort param from the URL

---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-impl/src/main/java/com/liferay/structured/content/apio/internal/provider/SortProvider.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.internal.provider;
+
+import com.liferay.apio.architect.provider.Provider;
+import com.liferay.structured.content.apio.architect.sort.Sort;
+import com.liferay.structured.content.apio.architect.sort.SortParser;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ */
+@Component
+public class SortProvider implements Provider<Sort> {
+
+	@Override
+	public Sort createContext(HttpServletRequest httpServletRequest) {
+		String sortParam = httpServletRequest.getParameter("sort");
+
+		return new Sort(_sortParser.parse(sortParam));
+	}
+
+	@Reference
+	private SortParser _sortParser;
+
+}
