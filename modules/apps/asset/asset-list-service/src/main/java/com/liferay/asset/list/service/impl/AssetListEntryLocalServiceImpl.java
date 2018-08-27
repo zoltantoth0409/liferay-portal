f9fcafeb4bf90b47ebbad4843e9dd8714387afa3
pavel-savinov <pavel.savinov@liferay.com>
2018-08-27 14:22:22 +0200

    LPS-84376 Uuid doesn't exist

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/service/impl/AssetListEntryLocalServiceImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/service/impl/AssetListEntryLocalServiceImpl.java
@@ -48,7 +48,6 @@ public class AssetListEntryLocalServiceImpl
 		AssetListEntry assetListEntry = assetListEntryPersistence.create(
 			assetListEntryId);
 
-		assetListEntry.setUuid(serviceContext.getUuid());
 		assetListEntry.setGroupId(groupId);
 		assetListEntry.setCompanyId(user.getCompanyId());
 		assetListEntry.setUserId(user.getUserId());
