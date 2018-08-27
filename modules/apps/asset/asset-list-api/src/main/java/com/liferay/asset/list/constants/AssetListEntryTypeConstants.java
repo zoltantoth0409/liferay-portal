29b2cf994876396eccaf1e7c57bc652b7ef20750
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-08-27 16:26:22 +0200

    LPS-84804 Define the types of asset lists that can be handled

---
+++ modules/apps/asset/asset-list-api/src/main/java/com/liferay/asset/list/constants/AssetListEntryTypeConstants.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.constants;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class AssetListEntryTypeConstants {
+
+	public static final int TYPE_DYNAMIC = 0;
+
+	public static final String TYPE_DYNAMIC_LABEL = "dynamic";
+
+	public static final int TYPE_MANUAL = 1;
+
+	public static final String TYPE_MANUAL_LABEL = "manual";
+
+	public static String getTypeLabel(int type) {
+		if (type == TYPE_DYNAMIC) {
+			return TYPE_DYNAMIC_LABEL;
+		}
+		else if (type == TYPE_MANUAL) {
+			return TYPE_MANUAL_LABEL;
+		}
+		else {
+			return null;
+		}
+	}
+
+}
