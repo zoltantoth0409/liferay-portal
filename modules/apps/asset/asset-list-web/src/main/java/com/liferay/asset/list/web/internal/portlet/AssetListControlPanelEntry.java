571aa2b85ef1734abdf2028f7d1e4a60436f882c
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-08-27 13:47:51 +0200

    LPS-84804 Show Asset List entry in Control Panel

---
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/AssetListControlPanelEntry.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.web.internal.portlet;
+
+import com.liferay.asset.list.constants.AssetListPortletKeys;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.BaseControlPanelEntry;
+import com.liferay.portal.kernel.portlet.ControlPanelEntry;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + AssetListPortletKeys.ASSET_LIST,
+	service = ControlPanelEntry.class
+)
+public class AssetListControlPanelEntry extends BaseControlPanelEntry {
+
+	@Override
+	protected boolean hasAccessPermissionDenied(
+			PermissionChecker permissionChecker, Group group, Portlet portlet)
+		throws Exception {
+
+		return super.hasAccessPermissionDenied(
+			permissionChecker, group, portlet);
+	}
+
+}
