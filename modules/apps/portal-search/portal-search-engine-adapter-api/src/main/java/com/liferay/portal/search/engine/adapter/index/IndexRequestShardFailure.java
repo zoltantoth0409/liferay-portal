89b6c6a7f6bb4d3019a98865ce94beb82b13f248
Michael Han <michael.han@liferay.com>
2018-08-27 18:50:34 -0500

    LPS-82007 flag as provider types

--- modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/index/IndexRequestShardFailure.java
+++ modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/index/IndexRequestShardFailure.java
@@ -14,9 +14,12 @@
 
 package com.liferay.portal.search.engine.adapter.index;
 
+import aQute.bnd.annotation.ProviderType;
+
 /**
  * @author Michael C. Han
  */
+@ProviderType
 public class IndexRequestShardFailure {
 
 	public String getIndex() {
