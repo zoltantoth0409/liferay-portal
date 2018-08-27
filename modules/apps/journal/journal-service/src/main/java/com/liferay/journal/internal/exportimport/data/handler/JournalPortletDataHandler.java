655d4e033df7d34c2b27fcd0da61f71495ba7b28
Máté Thurzó <mate.thurzo@liferay.com>
2018-08-27 14:12:27 +0200

    LPS-84380 Add friendly URL processing to relevant data handlers

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalPortletDataHandler.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalPortletDataHandler.java
@@ -34,6 +34,7 @@ import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.exportimport.kernel.lar.StagedModelType;
 import com.liferay.exportimport.kernel.staging.Staging;
 import com.liferay.exportimport.kernel.staging.StagingConstants;
+import com.liferay.friendly.url.model.FriendlyURLEntry;
 import com.liferay.journal.configuration.JournalServiceConfiguration;
 import com.liferay.journal.constants.JournalConstants;
 import com.liferay.journal.constants.JournalPortletKeys;
@@ -384,6 +385,20 @@ public class JournalPortletDataHandler extends BasePortletDataHandler {
 			}
 
 			_journalContent.clearCache();
+
+			// Friendy URLs
+
+			Element friendlyURLEntriesElement =
+				portletDataContext.getImportDataGroupElement(
+					FriendlyURLEntry.class);
+
+			List<Element> friendlyURLEntryElements =
+				friendlyURLEntriesElement.elements();
+
+			for (Element friendlyURLEntryElement : friendlyURLEntryElements) {
+				StagedModelDataHandlerUtil.importStagedModel(
+					portletDataContext, friendlyURLEntryElement);
+			}
 		}
 
 		return portletPreferences;
