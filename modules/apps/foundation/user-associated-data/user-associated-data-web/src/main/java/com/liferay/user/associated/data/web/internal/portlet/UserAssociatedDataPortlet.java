ff7d2a3ac8ea56146f6e6c54a0f160a97f54fe27
drewbrokke <drew.brokke@liferay.com>
2018-08-28 11:37:38 -0700

    LPS-84247 user-associated-data-web - registers the main.css file within the portlet context

--- modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/UserAssociatedDataPortlet.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/portlet/UserAssociatedDataPortlet.java
@@ -29,6 +29,7 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.footer-portlet-javascript=/js/main.js",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
