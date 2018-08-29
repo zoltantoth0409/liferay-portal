2277598e443459e94fb8cc50f048e3485842b703
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-08-29 20:01:47 +0200

    LPS-84859 We are not using it now, so we'll re-add later on, when we are indeed using it

--- modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/AssetListPortlet.java
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/AssetListPortlet.java
@@ -29,7 +29,6 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-asset-list-web",
 		"com.liferay.portlet.display-category=category.hidden",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
