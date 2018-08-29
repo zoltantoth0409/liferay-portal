dd2f4cb899dfbd7bba24291882dc67b03bea8144
Chema Balsas <jose.balsas@liferay.com>
2018-08-29 12:42:21 +0200

    LPS-84777 Exposes objectToFormData method as a global Util

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
@@ -1,6 +1,8 @@
 import portlet from './portlet/portlet.es';
 import navigate from './util/navigate.es';
+import objectToFormData from './util/object_to_form_data.es';
 
 Liferay.Util.navigate = navigate;
+Liferay.Util.objectToFormData = objectToFormData;
 
 export {portlet};
