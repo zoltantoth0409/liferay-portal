366204547d0f1baf32e218255f8f53aadebb4fea
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-08-29 17:22:21 -0700

    LRQA-42243 Add BuildData interface for new class hierachy

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BuildData.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.File;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+/**
+ * @author Michael Hashimoto
+ */
+public interface BuildData {
+
+	public Integer getBuildNumber();
+
+	public String getBuildURL();
+
+	public String getCohortName();
+
+	public List<String> getDistNodes();
+
+	public String getDistPath();
+
+	public String getHostname();
+
+	public String getJenkinsGitHubURL();
+
+	public String getJobName();
+
+	public String getMasterHostname();
+
+	public String getRunID();
+
+	public File getWorkspaceDir();
+
+	public JSONObject toJSONObject();
+
+}
