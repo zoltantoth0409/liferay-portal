50f976ccf49d941c428264290565b03357b750f1
Julio Camarero <julio.camarero@liferay.com>
2018-08-30 14:20:39 +0200

    LPS-83359 create Filter and Filter Parser API

---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-api/src/main/java/com/liferay/structured/content/apio/architect/filter/FilterParser.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.architect.filter;
+
+import com.liferay.structured.content.apio.architect.filter.expression.Expression;
+import com.liferay.structured.content.apio.architect.filter.expression.ExpressionVisitException;
+
+/**
+ * <code>FilterParser</code> transforms a String containing an oData filter in a
+ * manageable expression {@link Expression}.
+ *
+ * @author David Arques
+ * @review
+ */
+public interface FilterParser {
+
+	/**
+	 * Returns a {@link Expression} obtained from a String.
+	 *
+	 * @param  filterString - the filterString
+	 * @return The OData Expression
+	 * @review
+	 */
+	public Expression parse(String filterString)
+		throws ExpressionVisitException;
+
+}
