0d0e4acc2248da2c5ebf2b23e36e3d8a3f0c9031
Julio Camarero <julio.camarero@liferay.com>
2018-08-30 14:20:39 +0200

    LPS-83359 create Filter and Filter Parser API

---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-api/src/main/java/com/liferay/structured/content/apio/architect/filter/Filter.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.architect.filter;
+
+import com.liferay.structured.content.apio.architect.filter.expression.Expression;
+
+/**
+ * Represents a Filter for retrieving structured content by different fields. A
+ * OData {@link Expression} is needed to create a new instance. This
+ * instantiation is performed in the FilterProvider.
+ *
+ * @author Cristina González
+ * @author David Arques
+ * @review
+ */
+public class Filter {
+
+	/**
+	 * Creates a new {@link Filter} a given {@link Expression}
+	 *
+	 * @param  expression -  an OData Expression
+	 * @review
+	 */
+	public Filter(Expression expression) {
+		if (expression == null) {
+			throw new IllegalArgumentException("Expression is null");
+		}
+
+		_expression = expression;
+	}
+
+	/**
+	 * Returns the OData Expression.
+	 *
+	 * @return the filter expression
+	 * @review
+	 */
+	public Expression getExpression() {
+		return _expression;
+	}
+
+	private final Expression _expression;
+
+}
