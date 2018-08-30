f179ee8d569e184c56c0ba2807a2491653d8faa7
Ruben Pulido <ruben.pulido@liferay.com>
2018-08-30 14:25:26 +0200

    LPS-83359 Add tests

---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-impl/src/test/java/com/liferay/structured/content/apio/internal/architect/filter/FilterParserTest.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.internal.architect.filter;
+
+import com.liferay.structured.content.apio.architect.filter.expression.Binary;
+import com.liferay.structured.content.apio.architect.filter.expression.Expression;
+import com.liferay.structured.content.apio.architect.filter.expression.ExpressionVisitException;
+
+import org.assertj.core.api.AbstractThrowableAssert;
+import org.assertj.core.api.Assertions;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author David Arques
+ */
+public class FilterParserTest {
+
+	@Before
+	public void setUp() {
+		_filterParser = new FilterParserImpl();
+
+		_filterParser.activate();
+	}
+
+	@Test
+	public void testParseNonexistingField() {
+		String filterString = "(nonExistingField eq 'value')";
+
+		AbstractThrowableAssert exception = Assertions.assertThatThrownBy(
+			() -> _filterParser.parse(filterString)
+		).isInstanceOf(
+			InvalidFilterException.class
+		);
+
+		exception.hasMessageStartingWith(
+			String.format(
+				"Invalid query computed from filter '%s': 'Unknown property.'",
+				filterString));
+	}
+
+	@Test
+	public void testParseWithEmptyFilter() {
+		AbstractThrowableAssert exception = Assertions.assertThatThrownBy(
+			() -> _filterParser.parse("")
+		).isInstanceOf(
+			InvalidFilterException.class
+		);
+
+		exception.hasMessage("Filter is empty");
+	}
+
+	@Test
+	public void testParseWithNoSingleQuotes() {
+		String filter = "(title eq title1)";
+
+		AbstractThrowableAssert exception = Assertions.assertThatThrownBy(
+			() -> _filterParser.parse(filter)
+		).isInstanceOf(
+			InvalidFilterException.class
+		);
+
+		exception.hasMessageStartingWith(
+			String.format(
+				"Invalid query computed from filter '%s': 'Unknown property.'",
+				filter));
+	}
+
+	@Test
+	public void testParseWithNullFilter() {
+		AbstractThrowableAssert exception = Assertions.assertThatThrownBy(
+			() -> _filterParser.parse(null)
+		).isInstanceOf(
+			InvalidFilterException.class
+		);
+
+		exception.hasMessage("Filter is empty");
+	}
+
+	@Test
+	public void testParseWithSingleQuotes() throws ExpressionVisitException {
+		Expression expression = _filterParser.parse("title eq 'title1'");
+
+		Assert.assertNotNull(expression);
+
+		Binary binary = (Binary)expression;
+
+		Assert.assertEquals(Binary.Operation.EQ, binary.getOperation());
+		Assert.assertEquals("[title]", binary.getLeftOperand().toString());
+		Assert.assertEquals("'title1'", binary.getRightOperand().toString());
+	}
+
+	@Test
+	public void testParseWithSingleQuotesAndParentheses()
+		throws ExpressionVisitException {
+
+		Expression expression = _filterParser.parse("(title eq 'title1')");
+
+		Assert.assertNotNull(expression);
+
+		Binary binary = (Binary)expression;
+
+		Assert.assertEquals(Binary.Operation.EQ, binary.getOperation());
+		Assert.assertEquals("[title]", binary.getLeftOperand().toString());
+		Assert.assertEquals("'title1'", binary.getRightOperand().toString());
+	}
+
+	private FilterParserImpl _filterParser;
+
+}
---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-impl/src/test/java/com/liferay/structured/content/apio/internal/provider/StructuredContentEdmProviderTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.internal.provider;
+
+import com.liferay.structured.content.apio.internal.architect.filter.StructuredContentEdmProvider;
+
+import java.util.List;
+
+import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind;
+import org.apache.olingo.commons.api.edm.provider.CsdlEntityType;
+import org.apache.olingo.commons.api.edm.provider.CsdlProperty;
+import org.apache.olingo.commons.api.edm.provider.CsdlSchema;
+import org.apache.olingo.commons.api.ex.ODataException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author David Arques
+ */
+public class StructuredContentEdmProviderTest {
+
+	@Test
+	public void testGetSingleEntityTypeName() {
+		StructuredContentEdmProvider structuredContentEdmProvider =
+			new StructuredContentEdmProvider();
+
+		Assert.assertEquals(
+			"StructuredContent",
+			structuredContentEdmProvider.getSingleEntityTypeName());
+	}
+
+	@Test
+	public void testModelCreation() throws ODataException {
+		StructuredContentEdmProvider structuredContentEdmProvider =
+			new StructuredContentEdmProvider();
+
+		List<CsdlSchema> schemas = structuredContentEdmProvider.getSchemas();
+
+		Assert.assertEquals(schemas.toString(), 1, schemas.size());
+
+		CsdlSchema schema = schemas.get(0);
+
+		Assert.assertEquals("HypermediaRestApis", schema.getNamespace());
+
+		List<CsdlEntityType> entityTypes = schema.getEntityTypes();
+
+		Assert.assertEquals(entityTypes.toString(), 1, entityTypes.size());
+
+		CsdlEntityType entityType = entityTypes.get(0);
+
+		Assert.assertEquals("StructuredContent", entityType.getName());
+
+		CsdlProperty titleProperty = entityType.getProperty("title");
+
+		Assert.assertNotNull(titleProperty);
+		Assert.assertEquals(
+			EdmPrimitiveTypeKind.String.getFullQualifiedName(),
+			titleProperty.getTypeAsFQNObject());
+	}
+
+}
