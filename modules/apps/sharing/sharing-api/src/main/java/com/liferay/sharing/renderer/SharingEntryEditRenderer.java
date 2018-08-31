4fb794307a360ae6b1ba115e4aa483ff7a5f17de
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-08-31 10:35:57 +0200

    LPS-85191 Create API to list view and edit shared assets

---
+++ modules/apps/sharing/sharing-api/src/main/java/com/liferay/sharing/renderer/SharingEntryEditRenderer.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.renderer;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+
+import javax.portlet.PortletURL;
+
+/**
+ * @author Sergio González
+ */
+public interface SharingEntryEditRenderer<T> {
+
+	public PortletURL getURLEdit(
+			T entry, LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse)
+		throws PortalException;
+
+}
