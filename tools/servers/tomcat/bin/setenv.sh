efe4025ad6e432a16666d9352820431a5d3918d1
shuyangzhou <shuyang.zhou@liferay.com>
2018-08-31 16:47:08 -0700

    LPS-85007 Optimize default setenv.sh for developer (still assuming 32bit jvm)

--- tools/servers/tomcat/bin/setenv.sh
+++ tools/servers/tomcat/bin/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxMetaspaceSize=512m"
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -XX:NewSize=1536m -XX:MaxNewSize=1536m -XX:SurvivorRatio=7 -Xms2560m -Xmx2560m -XX:MetaspaceSize=384m -XX:MaxMetaspaceSize=384m"
