b15f914233e7103bdece870860593e80af2dee7d
pavel-savinov <pavel.savinov@liferay.com>
2018-08-31 11:40:39 +0200

    LPS-84973 Common methods for asset list screen navigation entries

---
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/servlet/taglib/ui/navigation/entry/BaseAssetListScreenNavigationEntry.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.web.internal.servlet.taglib.ui.navigation.entry;
+
+import com.liferay.asset.list.constants.AssetListFormConstants;
+import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.AggregateResourceBundle;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.io.IOException;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+public abstract class BaseAssetListScreenNavigationEntry
+	implements ScreenNavigationEntry<AssetListEntry> {
+
+	public abstract String getJspPath();
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(getResourceBundle(locale), getEntryKey());
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return AssetListFormConstants.SCREEN_NAVIGATION_KEY_ASSET_LIST;
+	}
+
+	@Override
+	public void render(HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		if (getJspPath() == null) {
+			return;
+		}
+
+		jspRenderer.renderJSP(request, response, getJspPath());
+	}
+
+	protected ResourceBundle getResourceBundle(Locale locale) {
+		ResourceBundle bundleResourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return new AggregateResourceBundle(
+			bundleResourceBundle, PortalUtil.getResourceBundle(locale));
+	}
+
+	@Reference
+	protected JSPRenderer jspRenderer;
+
+}
