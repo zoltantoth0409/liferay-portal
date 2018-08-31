71d3721b327cedea9d263aea57582a77fbf8df70
dsanz <daniel.sanz@liferay.com>
2018-08-31 14:44:24 +0200

    LPS-84577 add component with convenience methods to access the menu display style configuration

---
+++ modules/apps/web-experience/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/configuration/JournalContentConfigurationUtil.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.content.web.configuration;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Daniel Sanz
+ */
+@Component(immediate=true)
+@Deprecated
+public class JournalContentConfigurationUtil {
+	public String getMenuStyle() {
+		try {
+			JournalContentConfiguration journalContentConfiguration =
+				_configurationProvider.getSystemConfiguration(
+					JournalContentConfiguration.class);
+
+			String menuStyle = journalContentConfiguration.menuStyle();
+
+			return menuStyle;
+		}
+		catch (ConfigurationException ce) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(ce, ce);
+			}
+
+			return null;
+		}
+	}
+
+	public boolean isSeparateMenus() {
+		String menuStyle = getMenuStyle();
+
+		if ("separate-menus".equals(menuStyle)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean isSingleMenuContent() {
+		String menuStyle = getMenuStyle();
+
+		if ("single-menu-content".equals(menuStyle)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean isSingleMenuWidget() {
+		String menuStyle = getMenuStyle();
+
+		if ("single-menu-widget".equals(menuStyle)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	private ConfigurationProvider _configurationProvider;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalContentConfigurationUtil.class);
+
+	@Reference(unbind = "-")
+	protected void setConfigurationProvider(
+		ConfigurationProvider configurationProvider) {
+
+		_configurationProvider = configurationProvider;
+	}
+}
