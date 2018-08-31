e8dcf3dc84b72411d92f3b73a82c87280841df3f
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-08-31 12:04:11 -0700

    LRQA-42243 Propagate the JenkinsJSONObjectFile to dist nodes

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TopLevelBuildRunner.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TopLevelBuildRunner.java
@@ -14,6 +14,8 @@
 
 package com.liferay.jenkins.results.parser;
 
+import java.io.File;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,9 +29,53 @@ public abstract class TopLevelBuildRunner<T extends TopLevelBuildData>
 		return _batchNames;
 	}
 
+	@Override
+	public void run() {
+		super.run();
+
+		propagateDistFilesToDistNodes();
+	}
+
 	protected TopLevelBuildRunner(T topLevelBuildData) {
 		super(topLevelBuildData);
 	}
+
+	protected String[] getDistFileNames() {
+		return new String[] {BuildData.JENKINS_DATA_FILE_NAME};
+	}
+
+	protected void propagateDistFilesToDistNodes() {
+		if (!JenkinsResultsParserUtil.isCINode()) {
+			return;
+		}
+
+		writeJenkinsJSONObjectToFile();
+
+		BuildData buildData = getBuildData();
+
+		File workspaceDir = buildData.getWorkspaceDir();
+
+		FilePropagator filePropagator = new FilePropagator(
+			getDistFileNames(),
+			JenkinsResultsParserUtil.combine(
+				buildData.getHostname(), ":", workspaceDir.toString()),
+			buildData.getDistPath(), buildData.getDistNodes());
+
+		filePropagator.setCleanUpCommand(_FILE_PROPAGATOR_CLEAN_UP_COMMAND);
+
+		filePropagator.start(_FILE_PROPAGATOR_THREAD_COUNT);
+	}
+
+	private static final String _FILE_PROPAGATOR_CLEAN_UP_COMMAND =
+		JenkinsResultsParserUtil.combine(
+			"find ", BuildData.DIST_ROOT_PATH,
+			"/*/* -maxdepth 1 -type d -mmin +",
+			String.valueOf(TopLevelBuildRunner._FILE_PROPAGATOR_EXPIRATION),
+			" -exec rm -frv {} \\;");
+
+	private static final int _FILE_PROPAGATOR_EXPIRATION = 180;
+
+	private static final int _FILE_PROPAGATOR_THREAD_COUNT = 1;
 
 	private final List<String> _batchNames = new ArrayList<>();
 
