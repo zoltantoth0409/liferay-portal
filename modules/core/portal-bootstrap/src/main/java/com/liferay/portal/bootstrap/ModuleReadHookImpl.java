2e80ab8ce0697752639d261d67529967a5434f48
Matthew Tambara <matthew.tambara@liferay.com>
2018-08-31 17:14:19 -0700

    LPS-85003 Copy actual jar to osgi/state folder if bundleFile is not present

---
+++ modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleReadHookImpl.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.bootstrap;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.util.PropsValues;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.URL;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipFile;
+
+import org.eclipse.osgi.container.ModuleReadHook;
+
+/**
+ * @author Matthew Tambara
+ */
+public class ModuleReadHookImpl implements ModuleReadHook {
+
+	@Override
+	public void process(long bundleId, String location) {
+		Path path = Paths.get(
+			PropsValues.LIFERAY_HOME, "osgi/state/org.eclipse.osgi",
+			String.valueOf(bundleId), "0/bundleFile");
+
+		File bundleFile = path.toFile();
+
+		if ((bundleId == 0) || location.startsWith("webbundle") ||
+			bundleFile.exists()) {
+
+			return;
+		}
+
+		try {
+			if (location.startsWith("file")) {
+				URL url = new URL(location);
+
+				File file = new File(url.getPath());
+
+				Files.copy(new FileInputStream(file), path);
+			}
+			else {
+				Matcher matcher = _pattern.matcher(location);
+
+				if (matcher.matches()) {
+					try (ZipFile zipFile = new ZipFile(matcher.group(2));
+						InputStream inputStream = zipFile.getInputStream(
+							zipFile.getEntry(matcher.group(1)))) {
+
+						Files.copy(inputStream, path);
+					}
+				}
+				else {
+					if (_log.isWarnEnabled()) {
+						_log.warn("No bundle found for location " + location);
+					}
+				}
+			}
+		}
+		catch (IOException ioe) {
+			StringBundler sb = new StringBundler(4);
+
+			sb.append("Unable to copy from ");
+			sb.append(location);
+			sb.append(" to ");
+			sb.append(path.toString());
+
+			_log.error(sb.toString(), ioe);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ModuleReadHookImpl.class);
+
+	private static final Pattern _pattern = Pattern.compile(
+		"(.*\\.jar).*lpkgPath=([^&]*)");
+
+}
