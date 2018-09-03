588d81fda5a91b7bc9d5b7d9dd3891465faf028e
pavel-savinov <pavel.savinov@liferay.com>
2018-09-03 17:26:21 +0200

    LPS-85014 Process dynamic asset list filter query rules

--- modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/action/EditAssetListEntrySettingsMVCActionCommand.java
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/action/EditAssetListEntrySettingsMVCActionCommand.java
@@ -14,13 +14,22 @@
 
 package com.liferay.asset.list.web.internal.portlet.action;
 
+import com.liferay.asset.kernel.exception.DuplicateQueryRuleException;
+import com.liferay.asset.kernel.model.AssetQueryRule;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.asset.list.constants.AssetListPortletKeys;
 import com.liferay.asset.list.service.AssetListEntryService;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PropertiesParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -54,11 +63,97 @@ public class EditAssetListEntrySettingsMVCActionCommand
 			PropertiesParamUtil.getProperties(
 				actionRequest, "TypeSettingsProperties--");
 
+		updateQueryLogic(actionRequest, typeSettingsProperties);
+
 		_assetListEntryService.updateAssetListEntrySettings(
 			assetListEntryId, typeSettingsProperties.toString());
 	}
 
+	protected AssetQueryRule getQueryRule(
+		ActionRequest actionRequest, int index) {
+
+		boolean contains = ParamUtil.getBoolean(
+			actionRequest, "queryContains" + index);
+		boolean andOperator = ParamUtil.getBoolean(
+			actionRequest, "queryAndOperator" + index);
+
+		String name = ParamUtil.getString(actionRequest, "queryName" + index);
+
+		String[] values = null;
+
+		if (name.equals("assetTags")) {
+			values = StringUtil.split(
+				ParamUtil.getString(actionRequest, "queryTagNames" + index));
+		}
+		else {
+			values = StringUtil.split(
+				ParamUtil.getString(actionRequest, "queryCategoryIds" + index));
+		}
+
+		return new AssetQueryRule(contains, andOperator, name, values);
+	}
+
+	protected void updateQueryLogic(
+			ActionRequest actionRequest, UnicodeProperties properties)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long userId = themeDisplay.getUserId();
+		long groupId = themeDisplay.getSiteGroupId();
+
+		int[] queryRulesIndexes = StringUtil.split(
+			ParamUtil.getString(actionRequest, "queryLogicIndexes"), 0);
+
+		int i = 0;
+
+		List<AssetQueryRule> queryRules = new ArrayList<>();
+
+		for (int queryRulesIndex : queryRulesIndexes) {
+			AssetQueryRule queryRule = getQueryRule(
+				actionRequest, queryRulesIndex);
+
+			validateQueryRule(userId, groupId, queryRules, queryRule);
+
+			queryRules.add(queryRule);
+
+			properties.put(
+				"queryContains" + i, String.valueOf(queryRule.isContains()));
+			properties.put(
+				"queryAndOperator" + i,
+				String.valueOf(queryRule.isAndOperator()));
+			properties.put("queryName" + i, queryRule.getName());
+			properties.put(
+				"queryValues" + i, StringUtil.merge(queryRule.getValues()));
+
+			i++;
+		}
+	}
+
+	protected void validateQueryRule(
+			long userId, long groupId, List<AssetQueryRule> queryRules,
+			AssetQueryRule queryRule)
+		throws Exception {
+
+		String name = queryRule.getName();
+
+		if (name.equals("assetTags")) {
+			_assetTagLocalService.checkTags(
+				userId, groupId, queryRule.getValues());
+		}
+
+		if (queryRules.contains(queryRule)) {
+			throw new DuplicateQueryRuleException(
+				queryRule.isContains(), queryRule.isAndOperator(),
+				queryRule.getName());
+		}
+	}
+
 	@Reference
 	private AssetListEntryService _assetListEntryService;
+
+	@Reference
+	private AssetTagLocalService _assetTagLocalService;
 
 }
