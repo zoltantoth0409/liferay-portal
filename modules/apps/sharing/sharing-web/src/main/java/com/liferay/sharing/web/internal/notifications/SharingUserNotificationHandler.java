1d7a59171097e50ca9cf334441b6345349f2a44c
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-09-04 15:28:07 +0200

    LPS-83045 Customizing notification body

--- modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/notifications/SharingUserNotificationHandler.java
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/notifications/SharingUserNotificationHandler.java
@@ -14,11 +14,30 @@
 
 package com.liferay.sharing.web.internal.notifications;
 
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserNotificationEvent;
 import com.liferay.portal.kernel.notifications.BaseModelUserNotificationHandler;
 import com.liferay.portal.kernel.notifications.UserNotificationHandler;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.sharing.constants.SharingEntryActionKey;
+import com.liferay.sharing.model.SharingEntry;
+import com.liferay.sharing.service.SharingEntryLocalService;
 import com.liferay.sharing.web.internal.constants.SharingPortletKeys;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alejandro Tardín
@@ -34,5 +53,124 @@ public class SharingUserNotificationHandler
 	public SharingUserNotificationHandler() {
 		setPortletId(SharingPortletKeys.SHARING);
 	}
+
+	@Override
+	protected String getBody(
+			UserNotificationEvent userNotificationEvent,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			userNotificationEvent.getPayload());
+
+		long sharingEntryId = jsonObject.getLong("classPK");
+
+		SharingEntry sharingEntry = _sharingEntryLocalService.fetchSharingEntry(
+			sharingEntryId);
+
+		ResourceBundle resourceBundle =
+			_resourceBundleLoader.loadResourceBundle(
+				serviceContext.getLocale());
+
+		return ResourceBundleUtil.getString(
+			resourceBundle, "x-has-shared-x-with-you-for-x",
+			_getUserNameLink(sharingEntry.getFromUserId(), serviceContext),
+			_getSharedObjectLink(sharingEntry, serviceContext),
+			_getActionDescriptiveName(
+				sharingEntry, serviceContext.getLocale()));
+	}
+
+	private String _getActionDescriptiveName(
+		SharingEntry sharingEntry, Locale locale) {
+
+		ResourceBundle resourceBundle =
+			_resourceBundleLoader.loadResourceBundle(locale);
+
+		if (_hasAction(sharingEntry, SharingEntryActionKey.UPDATE)) {
+			return ResourceBundleUtil.getString(resourceBundle, "editing");
+		}
+		else if (_hasAction(
+					 sharingEntry, SharingEntryActionKey.ADD_DISCUSSION)) {
+
+			return ResourceBundleUtil.getString(resourceBundle, "commenting");
+		}
+		else if (_hasAction(sharingEntry, SharingEntryActionKey.VIEW)) {
+			return ResourceBundleUtil.getString(resourceBundle, "viewing");
+		}
+		else {
+			return ResourceBundleUtil.getString(resourceBundle, "nothing");
+		}
+	}
+
+	private String _getSharedObjectLink(
+			SharingEntry sharingEntry, ServiceContext serviceContext)
+		throws Exception {
+
+		AssetRenderer assetRenderer = getAssetRenderer(
+			sharingEntry.getClassName(), sharingEntry.getClassPK());
+
+		if (assetRenderer != null) {
+			return StringBundler.concat(
+				"<a href=\"",
+				assetRenderer.getURLViewInContext(
+					serviceContext.getLiferayPortletRequest(),
+					serviceContext.getLiferayPortletResponse(), null),
+				"\">", assetRenderer.getTitle(serviceContext.getLocale()),
+				"</a>");
+		}
+		else {
+			ResourceBundle resourceBundle =
+				_resourceBundleLoader.loadResourceBundle(
+					serviceContext.getLocale());
+
+			return ResourceBundleUtil.getString(resourceBundle, "something");
+		}
+	}
+
+	private String _getUserNameLink(
+		long userId, ServiceContext serviceContext) {
+
+		try {
+			if (userId <= 0) {
+				return StringPool.BLANK;
+			}
+
+			User user = _userLocalService.getUserById(userId);
+
+			String userName = user.getFullName();
+
+			String userDisplayURL = user.getDisplayURL(
+				serviceContext.getThemeDisplay());
+
+			return StringBundler.concat(
+				"<a href=\"", userDisplayURL, "\">", HtmlUtil.escape(userName),
+				"</a>");
+		}
+		catch (Exception e) {
+			return StringPool.BLANK;
+		}
+	}
+
+	private boolean _hasAction(
+		SharingEntry sharingEntry,
+		SharingEntryActionKey sharingEntryActionKey) {
+
+		long bitwiseVaue = sharingEntryActionKey.getBitwiseVaue();
+
+		if ((bitwiseVaue & sharingEntry.getActionIds()) != 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference(target = "(bundle.symbolic.name=com.liferay.sharing.web)")
+	private ResourceBundleLoader _resourceBundleLoader;
+
+	@Reference
+	private SharingEntryLocalService _sharingEntryLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
