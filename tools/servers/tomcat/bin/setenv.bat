1baf9bed0ab0233643e0a634546da70f2b178e4b
tina.tian <tina.tian@liferay.com>
2018-09-04 10:17:37 -0700

    LPS-85007 Update setenv.bat to make it consistent with setenv.sh

--- tools/servers/tomcat/bin/setenv.bat
+++ tools/servers/tomcat/bin/setenv.bat
@@ -6,4 +6,4 @@ if exist "%CATALINA_HOME%/jre@java.version@/win" (
 	set "JRE_HOME=%CATALINA_HOME%/jre@java.version@/win"
 )
 
-set "CATALINA_OPTS=%CATALINA_OPTS% -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxMetaspaceSize=512m"
+set "CATALINA_OPTS=%CATALINA_OPTS% -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -XX:NewSize=1536m -XX:MaxNewSize=1536m -XX:SurvivorRatio=7 -Xms2560m -Xmx2560m -XX:MetaspaceSize=384m -XX:MaxMetaspaceSize=384m"
