84fe21da0f20099b699459fa5a91785449215ef8
rubenpulido <ruben.pulido@liferay.com>
2018-09-04 15:05:02 +0200

    LPS-85050 Refactor existing Sort logic to use Index instead of Services

--- modules/apps/headless-apio/structured-content/structured-content-apio-impl/src/main/java/com/liferay/structured/content/apio/internal/architect/resource/StructuredContentNestedCollectionResource.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-impl/src/main/java/com/liferay/structured/content/apio/internal/architect/resource/StructuredContentNestedCollectionResource.java
@@ -43,7 +43,7 @@ import com.liferay.journal.model.JournalArticleConstants;
 import com.liferay.journal.model.JournalArticleDisplay;
 import com.liferay.journal.service.JournalArticleService;
 import com.liferay.journal.util.JournalContent;
-import com.liferay.journal.util.comparator.ArticleTitleComparator;
+import com.liferay.journal.util.JournalHelper;
 import com.liferay.media.object.apio.architect.identifier.MediaObjectIdentifier;
 import com.liferay.person.apio.architect.identifier.PersonIdentifier;
 import com.liferay.portal.apio.identifier.ClassNameClassPK;
@@ -52,12 +52,17 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.QueryConfig;
+import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.structure.apio.architect.identifier.ContentStructureIdentifier;
 import com.liferay.structured.content.apio.architect.filter.Filter;
@@ -78,6 +83,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -274,6 +281,40 @@ public class StructuredContentNestedCollectionResource
 			journalArticle.getResourcePrimKey());
 	}
 
+	private SearchContext _createSearchContext(
+		long companyId, long groupId, Locale locale, Sort sort, int start,
+		int end) {
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setAttribute(
+			Field.CLASS_NAME_ID, JournalArticleConstants.CLASSNAME_ID_DEFAULT);
+		searchContext.setAttribute("head", Boolean.TRUE);
+		searchContext.setAttribute(
+			Field.STATUS, WorkflowConstants.STATUS_APPROVED);
+		searchContext.setCompanyId(companyId);
+		searchContext.setEnd(end);
+		searchContext.setGroupIds(new long[] {groupId});
+		searchContext.setStart(start);
+
+		List<com.liferay.portal.kernel.search.Sort> sorts = _getSorts(
+			sort.getSortFields(), locale);
+
+		if (!sorts.isEmpty()) {
+			searchContext.setSorts(
+				sorts.toArray(new com.liferay.portal.kernel.search.Sort[0]));
+		}
+
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		queryConfig.setHighlightEnabled(false);
+		queryConfig.setScoreEnabled(false);
+		queryConfig.setSelectedFieldNames(
+			Field.ARTICLE_ID, Field.SCOPE_GROUP_ID);
+
+		return searchContext;
+	}
+
 	private void _deleteJournalArticle(long journalArticleId)
 		throws PortalException {
 
@@ -363,25 +404,6 @@ public class StructuredContentNestedCollectionResource
 		);
 	}
 
-	private OrderByComparator<JournalArticle>
-		_getJournalArticleOrderByComparator(List<SortField> sortFields) {
-
-		OrderByComparator<JournalArticle> orderByComparator = null;
-
-		for (SortField sortField : sortFields) {
-			String fieldName = sortField.getFieldName();
-
-			if (fieldName.equals("title")) {
-				orderByComparator = new ArticleTitleComparator(
-					sortField.isAscending());
-
-				break;
-			}
-		}
-
-		return orderByComparator;
-	}
-
 	private Long _getJournalArticleStructureId(
 		JournalArticleWrapper journalArticleWrapper) {
 
@@ -446,22 +468,23 @@ public class StructuredContentNestedCollectionResource
 	}
 
 	private PageItems<JournalArticleWrapper> _getPageItems(
-		Pagination pagination, long contentSpaceId, ThemeDisplay themeDisplay,
-		Filter filter, Sort sort) {
+			Pagination pagination, long contentSpaceId, ThemeDisplay themeDisplay,
+			Filter filter, Sort sort)
+		throws PortalException {
+
+		Indexer<JournalArticle> indexer = _indexerRegistry.nullSafeGetIndexer(
+			JournalArticle.class);
 
 		Map<String, Object> filterFieldsMap = getFilterFieldsMap(filter);
-		OrderByComparator<JournalArticle> orderByComparator =
-			_getJournalArticleOrderByComparator(sort.getSortFields());
+
+		Hits hits = indexer.search(
+			_createSearchContext(
+				themeDisplay.getCompanyId(), contentSpaceId,
+				themeDisplay.getLocale(), sort, pagination.getStartPosition(),
+				pagination.getEndPosition()));
 
 		List<JournalArticleWrapper> journalArticleWrappers = Stream.of(
-			_journalArticleService.search(
-				themeDisplay.getCompanyId(), contentSpaceId,
-				Collections.emptyList(),
-				JournalArticleConstants.CLASSNAME_ID_DEFAULT, null, null,
-				(String)filterFieldsMap.get("title"), null, null, new String[0],
-				new String[0], null, null, WorkflowConstants.STATUS_APPROVED,
-				null, true, pagination.getStartPosition(),
-				pagination.getEndPosition(), orderByComparator)
+			_journalHelper.getArticles(hits)
 		).flatMap(
 			List::stream
 		).map(
@@ -471,15 +494,7 @@ public class StructuredContentNestedCollectionResource
 			Collectors.toList()
 		);
 
-		int count = _journalArticleService.searchCount(
-			themeDisplay.getCompanyId(), contentSpaceId,
-			Collections.emptyList(),
-			JournalArticleConstants.CLASSNAME_ID_DEFAULT, null, null,
-			(String)filterFieldsMap.get("title"), null, null, new String[0],
-			new String[0], null, null, WorkflowConstants.STATUS_APPROVED, null,
-			true);
-
-		return new PageItems<>(journalArticleWrappers, count);
+		return new PageItems<>(journalArticleWrappers, hits.getLength());
 	}
 
 	private String _getRenderedContent(
@@ -515,6 +530,42 @@ public class StructuredContentNestedCollectionResource
 				ddmTemplate::getName,
 				locale -> _getRenderedContent(
 					journalArticleWrapper, ddmTemplate, locale))
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	private Optional<com.liferay.portal.kernel.search.Sort> _getSortOptional(
+		SortField sortField, Locale locale) {
+
+		String fieldName = null;
+
+		if (Objects.equals(sortField.getFieldName(), "title")) {
+			fieldName = Field.getSortableFieldName(
+				"localized_title_".concat(LocaleUtil.toLanguageId(locale)));
+		}
+		else {
+			return Optional.empty();
+		}
+
+		return Optional.of(
+			new com.liferay.portal.kernel.search.Sort(
+				fieldName, !sortField.isAscending()));
+	}
+
+	private List<com.liferay.portal.kernel.search.Sort> _getSorts(
+		List<SortField> sortFields, Locale locale) {
+
+		Stream<SortField> stream = sortFields.stream();
+
+		return stream.map(
+			sortField -> _getSortOptional(sortField, locale)
+		).flatMap(
+			sortFieldOptional -> sortFieldOptional.map(
+				Stream::of
+			).orElseGet(
+				Stream::empty
+			)
 		).collect(
 			Collectors.toList()
 		);
@@ -574,10 +625,16 @@ public class StructuredContentNestedCollectionResource
 	private HasPermission<Long> _hasPermission;
 
 	@Reference
+	private IndexerRegistry _indexerRegistry;
+
+	@Reference
 	private JournalArticleService _journalArticleService;
 
 	@Reference
 	private JournalContent _journalContent;
+
+	@Reference
+	private JournalHelper _journalHelper;
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;
