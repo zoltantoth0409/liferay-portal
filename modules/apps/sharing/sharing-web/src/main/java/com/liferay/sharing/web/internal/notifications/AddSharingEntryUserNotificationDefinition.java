fe96364fb9011485319abefc31f8f98e9ad930ee
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-09-04 13:20:11 +0200

    LPS-83045 Adding add entry notification definition

---
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/notifications/AddSharingEntryUserNotificationDefinition.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.web.internal.notifications;
+
+import com.liferay.portal.kernel.model.UserNotificationDeliveryConstants;
+import com.liferay.portal.kernel.notifications.UserNotificationDefinition;
+import com.liferay.portal.kernel.notifications.UserNotificationDeliveryType;
+import com.liferay.sharing.web.internal.constants.SharingPortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + SharingPortletKeys.SHARING,
+	service = UserNotificationDefinition.class
+)
+public class AddSharingEntryUserNotificationDefinition
+	extends UserNotificationDefinition {
+
+	public AddSharingEntryUserNotificationDefinition() {
+		super(
+			SharingPortletKeys.SHARING, 0,
+			UserNotificationDefinition.NOTIFICATION_TYPE_ADD_ENTRY,
+			"receive-a-notification-when-someone-shares-content-with-you");
+
+		addUserNotificationDeliveryType(
+			new UserNotificationDeliveryType(
+				"website", UserNotificationDeliveryConstants.TYPE_WEBSITE, true,
+				true));
+	}
+
+}
