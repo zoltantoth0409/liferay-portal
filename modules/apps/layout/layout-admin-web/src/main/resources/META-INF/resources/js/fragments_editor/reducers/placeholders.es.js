c6f486887e5fafb7602ca5e6fe4f2223324bf69c
Pablo Molina <contact@pablomolina.me>
2018-09-05 11:26:02 +0200

    LPS-84068 Create placeholders reducer

---
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/reducers/placeholders.es.js
@@ -0,0 +1,39 @@
+import {CLEAR_DRAG_TARGET, UPDATE_DRAG_TARGET} from '../actions/actions.es';
+
+/**
+ * Available drag positions
+ * @review
+ * @type {!object}
+ */
+
+const DRAG_POSITIONS = {
+	bottom: 'drag-bottom',
+	top: 'drag-top'
+};
+
+/**
+ * @param {!MetalStore} state
+ * @param {!string} actionType
+ * @param {!object} payload
+ * @param {!string} payload.hoveredFragmentEntryLinkBorder
+ * @param {!string} payload.hoveredFragmentEntryLinkId
+ * @return {MetalStore}
+ * @review
+ */
+
+function updateDragTargetReducer(state, actionType, payload) {
+	let nextState = Object.assign({}, state);
+
+	if (actionType === CLEAR_DRAG_TARGET) {
+		nextState.hoveredFragmentEntryLinkBorder = null;
+		nextState.hoveredFragmentEntryLinkId = null;
+	}
+	else if (actionType === UPDATE_DRAG_TARGET) {
+		nextState.hoveredFragmentEntryLinkBorder = payload.hoveredFragmentEntryLinkBorder;
+		nextState.hoveredFragmentEntryLinkId = payload.hoveredFragmentEntryLinkId;
+	}
+
+	return nextState;
+}
+
+export {DRAG_POSITIONS, updateDragTargetReducer};
