b6f60a4f0d3cf775a60e1f76f6b80480802fbdfd
Pablo Molina <contact@pablomolina.me>
2018-09-05 11:26:35 +0200

    LPS-84068 Create store actions

---
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/actions/actions.es.js
@@ -0,0 +1,65 @@
+/**
+ * Add fragment entry link action type
+ * @review
+ * @type {string}
+ */
+
+const ADD_FRAGMENT_ENTRY_LINK = 'addFragmentEntryLink';
+
+/**
+ * Clear drag target action type
+ * @review
+ * @type {string}
+ */
+
+const CLEAR_DRAG_TARGET = 'clearDragTarget';
+
+/**
+ * Remove fragment entry link action type
+ * @review
+ * @type {string}
+ */
+
+const REMOVE_FRAGMENT_ENTRY_LINK = 'removeFragmentEntryLink';
+
+/**
+ * Update drag target action type
+ * @review
+ * @type {string}
+ */
+
+const UPDATE_DRAG_TARGET = 'updateDragTarget';
+
+/**
+ * Update last save date action type
+ * @review
+ * @type {string}
+ */
+
+const UPDATE_LAST_SAVE_DATE = 'updateLastSaveDate';
+
+/**
+ * Update saving changes status action type
+ * @review
+ * @type {string}
+ */
+
+const UPDATE_SAVING_CHANGES_STATUS = 'updateSavingChangesStatus';
+
+/**
+ * Update translation status action type
+ * @review
+ * @type {string}
+ */
+
+const UPDATE_TRANSLATION_STATUS = 'updateTranslationStatus';
+
+export {
+	ADD_FRAGMENT_ENTRY_LINK,
+	CLEAR_DRAG_TARGET,
+	REMOVE_FRAGMENT_ENTRY_LINK,
+	UPDATE_DRAG_TARGET,
+	UPDATE_LAST_SAVE_DATE,
+	UPDATE_SAVING_CHANGES_STATUS,
+	UPDATE_TRANSLATION_STATUS
+};
