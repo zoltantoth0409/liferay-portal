b5a1134880998f534811993e332e916e1d1b142d
Javier Gamarra <nhpatt@gmail.com>
2018-09-06 12:42:47 +0200

    LPS-84597 move to child resources

--- modules/apps/headless-apio/structure/structure-apio-impl/src/main/java/com/liferay/structure/apio/internal/architect/resource/ContentStructureNestedCollectionResource.java
+++ modules/apps/headless-apio/structure/structure-apio-impl/src/main/java/com/liferay/structure/apio/internal/architect/resource/ContentStructureNestedCollectionResource.java
@@ -16,11 +16,13 @@ package com.liferay.structure.apio.internal.architect.resource;
 
 import com.liferay.apio.architect.pagination.PageItems;
 import com.liferay.apio.architect.pagination.Pagination;
+import com.liferay.apio.architect.representor.NestedRepresentor;
 import com.liferay.apio.architect.representor.Representor;
 import com.liferay.apio.architect.resource.NestedCollectionResource;
 import com.liferay.apio.architect.routes.ItemRoutes;
 import com.liferay.apio.architect.routes.NestedCollectionRoutes;
 import com.liferay.content.space.apio.architect.identifier.ContentSpaceIdentifier;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.journal.model.JournalArticle;
@@ -28,6 +30,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.service.ClassNameService;
 import com.liferay.structure.apio.architect.identifier.ContentStructureIdentifier;
+import com.liferay.structure.apio.architect.model.FormLayoutPage;
 import com.liferay.structure.apio.architect.util.StructureRepresentorBuilderHelper;
 
 import java.util.List;
@@ -80,12 +83,38 @@ public class ContentStructureNestedCollectionResource
 			_structureRepresentorBuilderHelper.buildDDMStructureFirstStep(
 				builder);
 
+		ddmStructureFirstStep.addNestedList(
+			"formPages", _structureRepresentorBuilderHelper::getFormLayoutPages,
+			nestedBuilder -> _buildFormLayoutPage(nestedBuilder).build());
+
 		Representor.FirstStep<DDMStructure> bidirectionalModelStep =
 			ddmStructureFirstStep.addBidirectionalModel(
 				"contentSpace", "contentStructures",
 				ContentSpaceIdentifier.class, DDMStructure::getGroupId);
 
 		return bidirectionalModelStep.build();
+	}
+
+	private NestedRepresentor.FirstStep<DDMFormField> _buildDDMFormFields(
+		NestedRepresentor.Builder<DDMFormField> ddmFormFieldBuilder) {
+
+		return _structureRepresentorBuilderHelper.buildDDMFormFieldFirstStep(
+			ddmFormFieldBuilder);
+	}
+
+	private NestedRepresentor.FirstStep<FormLayoutPage> _buildFormLayoutPage(
+		NestedRepresentor.Builder<FormLayoutPage> builder) {
+
+		NestedRepresentor.FirstStep<FormLayoutPage> formLayoutPageFirstStep =
+			_structureRepresentorBuilderHelper.buildFormLayoutPageFirstStep(
+				builder);
+
+		formLayoutPageFirstStep.addNestedList(
+			"fields", FormLayoutPage::getFields,
+			ddmFormFieldBuilder -> _buildDDMFormFields(
+				ddmFormFieldBuilder).build());
+
+		return formLayoutPageFirstStep;
 	}
 
 	private DDMStructure _getItem(Long structureId) throws PortalException {
