ee3d8544bdf7c6fd3b8b6630e21dc5688e730800
David Arques <david.arques@liferay.com>
2018-09-06 12:43:31 +0200

    LPS-83359 Add Filter Expression Visitor

---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-impl/src/main/java/com/liferay/structured/content/apio/internal/architect/filter/ExpressionVisitorImpl.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.internal.architect.filter;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.structured.content.apio.architect.filter.expression.BinaryExpression;
+import com.liferay.structured.content.apio.architect.filter.expression.ExpressionVisitor;
+import com.liferay.structured.content.apio.architect.filter.expression.LiteralExpression;
+import com.liferay.structured.content.apio.architect.filter.expression.MemberExpression;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Julio Camarero
+ */
+public class ExpressionVisitorImpl implements ExpressionVisitor<Object> {
+
+	@Override
+	public Object visitBinaryExpressionOperation(
+		BinaryExpression.Operation operation, Object leftBinaryOperationObject,
+		Object rightBinaryOperationObject) {
+
+		if (operation == BinaryExpression.Operation.EQ) {
+			return _createMap(
+				(String)leftBinaryOperationObject, rightBinaryOperationObject);
+		}
+		else {
+			throw new InvalidFilterException(
+				"Unsupported method visitBinaryExpressionOperation with " +
+					"operation" + operation);
+		}
+	}
+
+	@Override
+	public Object visitLiteralExpression(LiteralExpression literalExpression) {
+		if (LiteralExpression.Type.STRING.equals(literalExpression.getType())) {
+			return _normalizeLiteral(literalExpression.getText());
+		}
+
+		return literalExpression.getText();
+	}
+
+	@Override
+	public Object visitMemberExpression(MemberExpression memberExpression) {
+		List<String> resourcePath = memberExpression.getResourcePath();
+
+		return String.valueOf(resourcePath.get(0));
+	}
+
+	private Map<String, Object> _createMap(
+		String leftBinaryOperationObject, Object rightBinaryOperationObject) {
+
+		return new HashMap<String, Object>() {
+			{
+				put(leftBinaryOperationObject, rightBinaryOperationObject);
+			}
+		};
+	}
+
+	private Object _normalizeLiteral(String literal) {
+		return StringUtil.replace(
+			StringUtil.unquote(literal), StringPool.DOUBLE_APOSTROPHE,
+			StringPool.APOSTROPHE);
+	}
+
+}
