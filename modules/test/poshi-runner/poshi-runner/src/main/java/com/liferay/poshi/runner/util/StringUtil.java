6d2b28b9d30e4331809cb6a8754432de7c4294c4
pyoo47 <peter.yoo@liferay.com>
2018-09-06 09:04:27 -0700

    LRQA-43407 Account for the possibility that s only contains new lines

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/util/StringUtil.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/util/StringUtil.java
@@ -141,7 +141,7 @@ public class StringUtil {
 	}
 
 	public static int countStartingNewLines(String s) {
-		String[] snippets = s.split(System.getProperty("line.separator"));
+		String[] snippets = s.split(System.getProperty("line.separator"), -1);
 
 		if (snippets.length == 1) {
 			return 0;
