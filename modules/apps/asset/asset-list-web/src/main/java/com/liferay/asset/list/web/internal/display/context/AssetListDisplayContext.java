9151879247ead4b008c8a5ad40874ceac08223f8
pavel-savinov <pavel.savinov@liferay.com>
2018-09-06 14:00:16 +0200

    LPS-85140 Create default asset publisher portlet preferences for the asset list preview

--- modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/display/context/AssetListDisplayContext.java
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/display/context/AssetListDisplayContext.java
@@ -23,6 +23,7 @@ import com.liferay.asset.list.model.AssetListEntry;
 import com.liferay.asset.list.service.AssetListEntryServiceUtil;
 import com.liferay.asset.list.web.internal.security.permission.resource.AssetListPermission;
 import com.liferay.asset.list.web.util.AssetListPortletUtil;
+import com.liferay.asset.publisher.constants.AssetPublisherPortletKeys;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemList;
@@ -49,6 +50,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.portlet.PortletPreferences;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
@@ -447,6 +449,21 @@ public class AssetListDisplayContext {
 			Objects.equals(getOrderByType(), "asc") ? "desc" : "asc");
 
 		return sortingURL.toString();
+	}
+
+	public String getViewContentPreferences() throws Exception {
+		PortletPreferences portletPreferences =
+			PortletPreferencesFactoryUtil.getPortletPreferences(
+				_request, AssetPublisherPortletKeys.ASSET_PUBLISHER);
+
+		portletPreferences.setValue(
+			"assetListEntryId", String.valueOf(getAssetListEntryId()));
+		portletPreferences.setValue(
+			"emailAssetEntryAddedEnabled", Boolean.FALSE.toString());
+		portletPreferences.setValue("paginationType", "none");
+		portletPreferences.setValue("selectionStyle", "asset-list");
+
+		return PortletPreferencesFactoryUtil.toXML(portletPreferences);
 	}
 
 	public boolean isShowAddAssetListEntryAction() {
