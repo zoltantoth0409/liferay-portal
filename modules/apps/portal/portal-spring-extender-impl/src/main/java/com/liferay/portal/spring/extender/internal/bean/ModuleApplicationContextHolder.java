f048b51a78b226cfcee373e434a6dce90cc8cf31
Preston-Crary <preston.crary@liferay.com>
2018-09-07 14:24:31 -0700

    LPS-85212 Allow javax.sql.DataSource to be published into the service's BundleContext to override the portal's DataSource.

---
+++ modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/bean/ModuleApplicationContextHolder.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.spring.extender.internal.bean;
+
+import com.liferay.portal.spring.extender.internal.context.ModuleApplicationContext;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * @author Preston Crary
+ */
+public class ModuleApplicationContextHolder implements ApplicationContextAware {
+
+	public ModuleApplicationContext getModuleApplicationContext() {
+		return _moduleApplicationContext;
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		_moduleApplicationContext =
+			(ModuleApplicationContext)applicationContext;
+	}
+
+	private ModuleApplicationContext _moduleApplicationContext;
+
+}
