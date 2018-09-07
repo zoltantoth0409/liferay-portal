60e2b5eef08d3db49d83c68869779d71880a9558
Alberto Chaparro <alberto.chaparro@liferay.com>
2018-09-07 19:45:17 +0200

    LPS-85049 SF

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/template/JournalTemplateHandler.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/template/JournalTemplateHandler.java
@@ -28,7 +28,6 @@ import com.liferay.journal.util.JournalContent;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.template.TemplateHandler;
 import com.liferay.portal.kernel.template.TemplateVariableCodeHandler;
 import com.liferay.portal.kernel.template.TemplateVariableGroup;
