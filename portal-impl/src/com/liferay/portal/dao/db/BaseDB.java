c4af6a38938f17cb1115f34252f9abf9e911ab05
Brian Chan <brian.chan@liferay.com>
2018-09-11 10:16:52 -0700

    LPS-85206 Change some logging to debug

--- portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
+++ portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -511,12 +511,12 @@ public abstract class BaseDB implements DB {
 	public void setSupportsStringCaseSensitiveQuery(
 		boolean supportsStringCaseSensitiveQuery) {
 
-		if (_log.isInfoEnabled()) {
+		if (_log.isDebugEnabled()) {
 			if (supportsStringCaseSensitiveQuery) {
-				_log.info("Database supports case sensitive queries");
+				_log.debug("Database supports case sensitive queries");
 			}
 			else {
-				_log.info("Database does not support case sensitive queries");
+				_log.debug("Database does not support case sensitive queries");
 			}
 		}
 
