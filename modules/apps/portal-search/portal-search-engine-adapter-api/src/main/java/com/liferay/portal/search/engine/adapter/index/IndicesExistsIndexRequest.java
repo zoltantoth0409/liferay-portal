e073e10071373d1510129b836fba4291fb8ffed6
Michael Han <michael.han@liferay.com>
2018-09-11 00:31:23 -0500

    LPS-85065 add new index operations

---
+++ modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/index/IndicesExistsIndexRequest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.engine.adapter.index;
+
+import aQute.bnd.annotation.ProviderType;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public class IndicesExistsIndexRequest
+	implements IndexRequest<IndicesExistsIndexResponse> {
+
+	public IndicesExistsIndexRequest(String... indexNames) {
+		_indexNames = indexNames;
+	}
+
+	@Override
+	public IndicesExistsIndexResponse accept(
+		IndexRequestExecutor indexRequestExecutor) {
+
+		return indexRequestExecutor.executeIndexRequest(this);
+	}
+
+	@Override
+	public String[] getIndexNames() {
+		return _indexNames;
+	}
+
+	/**
+	 * @return
+	 * @deprecated As of Judson (7.1.x), with no direct replacement. This method
+	 *             should not be in the parent interface.  Only certain
+	 *             IndexRequests work with mappings.
+	 */
+	@Deprecated
+	@Override
+	public String getMappingName() {
+		throw new UnsupportedOperationException();
+	}
+
+	private final String[] _indexNames;
+
+}
