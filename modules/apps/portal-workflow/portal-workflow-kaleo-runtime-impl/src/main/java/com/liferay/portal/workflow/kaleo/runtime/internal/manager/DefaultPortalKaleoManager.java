e98d23efc00ffa09234cb74a7ebed0efe13ab921
Seiphon <871358932@qq.com>
2018-09-12 17:14:38 +0800

    LPS-85263 When initialize the bundle, set the title of workflow as xml rather than string

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/manager/DefaultPortalKaleoManager.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/manager/DefaultPortalKaleoManager.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.workflow.kaleo.runtime.internal.manager;
 
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -32,6 +33,7 @@ import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalService;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.workflow.WorkflowDefinition;
 import com.liferay.portal.kernel.workflow.WorkflowDefinitionManager;
 import com.liferay.portal.kernel.workflow.comparator.WorkflowComparatorFactory;
@@ -136,6 +138,15 @@ public class DefaultPortalKaleoManager
 
 			serviceContext.setCompanyId(companyId);
 
+			Locale defaultLocale = LocaleUtil.getDefault();
+
+			String languageId = LocaleUtil.toLanguageId(defaultLocale);
+
+			String localizedTitle = StringPool.BLANK;
+
+			localizedTitle = LocalizationUtil.updateLocalization(
+				localizedTitle, "Title", definitionName, languageId);
+
 			int kaleoDefinitionsCount =
 				kaleoDefinitionLocalService.getKaleoDefinitionsCount(
 					definitionName, serviceContext);
@@ -165,7 +176,7 @@ public class DefaultPortalKaleoManager
 
 			_workflowDefinitionManager.deployWorkflowDefinition(
 				serviceContext.getCompanyId(), defaultUser.getUserId(),
-				definitionName, definitionName, FileUtil.getBytes(inputStream));
+				localizedTitle, definitionName, FileUtil.getBytes(inputStream));
 		}
 	}
 
