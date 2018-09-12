56fc832d8c9848db338550951938eccb92452e6f
Dante Wang <dante.wang@liferay.com>
2018-09-12 17:18:01 +0800

    LPS-84130 Create ServletContextClassLoaderPool for servlet context name - class loader mapping

---
+++ portal-kernel/src/com/liferay/portal/kernel/util/ServletContextClassLoaderPool.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Shuyang Zhou
+ */
+public class ServletContextClassLoaderPool {
+
+	public static ClassLoader getClassLoader(String contextName) {
+		if (contextName == null) {
+			return null;
+		}
+
+		return _classLoaders.get(contextName);
+	}
+
+	public static String getContextName(ClassLoader classLoader) {
+		if (classLoader == null) {
+			return null;
+		}
+
+		return _contextNames.get(classLoader);
+	}
+
+	public static void register(String contextName, ClassLoader classLoader) {
+		_classLoaders.put(contextName, classLoader);
+		_contextNames.put(classLoader, contextName);
+	}
+
+	public static void unregister(ClassLoader classLoader) {
+		String contextName = _contextNames.remove(classLoader);
+
+		if (contextName != null) {
+			_classLoaders.remove(contextName);
+		}
+	}
+
+	public static void unregister(String contextName) {
+		ClassLoader classLoader = _classLoaders.remove(contextName);
+
+		if (classLoader != null) {
+			_contextNames.remove(classLoader);
+		}
+	}
+
+	private static final Map<String, ClassLoader> _classLoaders =
+		new ConcurrentHashMap<>();
+	private static final Map<ClassLoader, String> _contextNames =
+		new ConcurrentHashMap<>();
+
+}
