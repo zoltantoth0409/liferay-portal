01ba6229d721909af4dcd66d162a93336eb880ca
iiventura <ireneven@ucm.es>
2018-09-13 12:59:29 +0200

    LPS-83829 Created Liferay.Util.unescape function

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
@@ -1,8 +1,10 @@
 import portlet from './portlet/portlet.es';
 import navigate from './util/navigate.es';
 import objectToFormData from './util/object_to_form_data.es';
+import unescape from './util/unescape.es';
 
 Liferay.Util.navigate = navigate;
 Liferay.Util.objectToFormData = objectToFormData;
+Liferay.Util.unescape = unescape;
 
 export {portlet};
