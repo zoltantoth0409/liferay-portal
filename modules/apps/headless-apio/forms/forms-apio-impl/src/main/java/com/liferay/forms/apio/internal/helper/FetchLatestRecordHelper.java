e02ed6e8f9535db775d1f46e114c4815da04a40f
Paulo Cruz <paulo.cruz@liferay.com>
2018-09-13 14:42:14 -0300

    LPS-85033 Use Try.fromFallible on FetchLatestRecordHelper

--- modules/apps/headless-apio/forms/forms-apio-impl/src/main/java/com/liferay/forms/apio/internal/helper/FetchLatestRecordHelper.java
+++ modules/apps/headless-apio/forms/forms-apio-impl/src/main/java/com/liferay/forms/apio/internal/helper/FetchLatestRecordHelper.java
@@ -14,12 +14,12 @@
 
 package com.liferay.forms.apio.internal.helper;
 
+import com.liferay.apio.architect.functional.Try;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
 import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecordVersion;
 import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordVersionService;
 import com.liferay.portal.apio.user.CurrentUser;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import org.osgi.service.component.annotations.Component;
@@ -32,18 +32,20 @@ import org.osgi.service.component.annotations.Reference;
 public class FetchLatestRecordHelper {
 
 	public DDMFormInstanceRecord fetchLatestDraftRecord(
-			DDMFormInstance ddmFormInstance, CurrentUser currentUser)
-		throws PortalException {
+		DDMFormInstance ddmFormInstance, CurrentUser currentUser) {
 
-		DDMFormInstanceRecordVersion ddmFormInstanceRecordVersion =
-			_ddmFormInstanceRecordVersionService.
+		return Try.fromFallible(
+			() -> _ddmFormInstanceRecordVersionService.
 				fetchLatestFormInstanceRecordVersion(
 					currentUser.getUserId(),
 					ddmFormInstance.getFormInstanceId(),
 					ddmFormInstance.getVersion(),
-					WorkflowConstants.STATUS_DRAFT);
-
-		return ddmFormInstanceRecordVersion.getFormInstanceRecord();
+					WorkflowConstants.STATUS_DRAFT)
+		).map(
+			DDMFormInstanceRecordVersion::getFormInstanceRecord
+		).orElse(
+			null
+		);
 	}
 
 	@Reference
