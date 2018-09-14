9d95504d0a48fda32c9c7e43ac05724a7daeca86
Kenji Heigel <kenji.heigel@liferay.com>
2018-09-14 10:35:35 -0700

    LRQA-43550 Clean up unused properties. Some are obsolete, and some are only necessary in the portal ant layer

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/util/PropsValues.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/util/PropsValues.java
@@ -33,59 +33,11 @@ public class PropsValues {
 	public static final String BROWSER_VERSION = PropsUtil.get(
 		"browser.version");
 
-	public static final String CLUSTER_NODE_1 = PropsUtil.get("cluster.node1");
-
-	public static final String CLUSTER_NODE_2 = PropsUtil.get("cluster.node2");
-
-	public static final String COMPONENT_NAMES = PropsUtil.get(
-		"component.names");
-
-	public static final String EMAIL_ADDRESS_1 = PropsUtil.get(
-		"email.address.1");
-
-	public static final String EMAIL_ADDRESS_2 = PropsUtil.get(
-		"email.address.2");
-
-	public static final String EMAIL_ADDRESS_3 = PropsUtil.get(
-		"email.address.3");
-
-	public static final String EMAIL_ADDRESS_4 = PropsUtil.get(
-		"email.address.4");
-
-	public static final String EMAIL_ADDRESS_5 = PropsUtil.get(
-		"email.address.5");
-
-	public static final String EMAIL_PASSWORD_1 = PropsUtil.get(
-		"email.password.1");
-
-	public static final String EMAIL_PASSWORD_2 = PropsUtil.get(
-		"email.password.2");
-
-	public static final String EMAIL_PASSWORD_3 = PropsUtil.get(
-		"email.password.3");
-
-	public static final String EMAIL_PASSWORD_4 = PropsUtil.get(
-		"email.password.4");
-
-	public static final String EMAIL_PASSWORD_5 = PropsUtil.get(
-		"email.password.5");
-
-	public static final String[] FIXED_ISSUES = StringUtil.split(
-		PropsUtil.get("fixed.issues"));
-
 	public static final int GET_LOCATION_MAX_RETRIES = GetterUtil.getInteger(
 		PropsUtil.get("get.location.max.retries"));
 
 	public static final int GET_LOCATION_TIMEOUT = GetterUtil.getInteger(
 		PropsUtil.get("get.location.timeout"));
-
-	public static final String GOOGLE_API_KEY = PropsUtil.get("google.api.key");
-
-	public static final String GOOGLE_CLIENT_ID = PropsUtil.get(
-		"google.client.id");
-
-	public static final String GOOGLE_CLIENT_SECRET = PropsUtil.get(
-		"google.client.secret");
 
 	public static final String IGNORE_ERRORS = PropsUtil.get("ignore.errors");
 
@@ -94,8 +46,6 @@ public class PropsValues {
 
 	public static final String IGNORE_ERRORS_FILE_NAME = PropsUtil.get(
 		"ignore.errors.file.name");
-
-	public static final String LIFERAY_HOME = PropsUtil.get("liferay.home");
 
 	public static final String LIFERAY_PORTAL_BRANCH = PropsUtil.get(
 		"liferay.portal.branch");
@@ -123,15 +73,10 @@ public class PropsValues {
 	public static final String PRINT_JAVA_PROCESS_ON_FAIL = PropsUtil.get(
 		"print.java.process.on.fail");
 
-	public static final String PRODUCT_NAMES = PropsUtil.get("product.names");
-
 	public static final String PROJECT_DIR = PropsUtil.get("project.dir");
 
 	public static final boolean SAVE_SCREENSHOT = GetterUtil.getBoolean(
 		PropsUtil.get("save.screenshot"));
-
-	public static final boolean SAVE_SOURCE = GetterUtil.getBoolean(
-		PropsUtil.get("save.source"));
 
 	public static final boolean SAVE_WEB_PAGE = GetterUtil.getBoolean(
 		PropsUtil.get("save.web.page"));
@@ -148,19 +93,11 @@ public class PropsValues {
 	public static final String SELENIUM_GECKO_DRIVER_EXECUTABLE = PropsUtil.get(
 		"selenium.gecko.driver.executable");
 
-	public static final String SELENIUM_HOST = PropsUtil.get("selenium.host");
-
 	public static final String SELENIUM_IE_DRIVER_EXECUTABLE = PropsUtil.get(
 		"selenium.ie.driver.executable");
 
-	public static final String SELENIUM_IMPLEMENTATION = PropsUtil.get(
-		"selenium.implementation");
-
 	public static final boolean SELENIUM_LOGGER_ENABLED = GetterUtil.getBoolean(
 		PropsUtil.get("selenium.logger.enabled"));
-
-	public static final int SELENIUM_PORT = GetterUtil.getInteger(
-		PropsUtil.get("selenium.port"));
 
 	public static final boolean SELENIUM_REMOTE_DRIVER_ENABLED =
 		GetterUtil.getBoolean(PropsUtil.get("selenium.remote.driver.enabled"));
@@ -173,9 +110,6 @@ public class PropsValues {
 
 	public static final boolean TCAT_ENABLED = GetterUtil.getBoolean(
 		PropsUtil.get("tcat.enabled"));
-
-	public static final boolean TEAR_DOWN_BEFORE_TEST = GetterUtil.getBoolean(
-		PropsUtil.get("tear.down.before.test"));
 
 	public static final boolean TEST_ASSERT_CONSOLE_ERRORS =
 		GetterUtil.getBoolean(PropsUtil.get("test.assert.console.errors"));
@@ -225,9 +159,6 @@ public class PropsValues {
 	public static final String[] TEST_CSV_REPORT_PROPERTY_NAMES =
 		StringUtil.split(PropsUtil.get("test.csv.report.property.names"));
 
-	public static final boolean TEST_DATABASE_MINIMAL = GetterUtil.getBoolean(
-		PropsUtil.get("test.database.minimal"));
-
 	public static final String TEST_DEPENDENCIES_DIR_NAME = PropsUtil.get(
 		"test.dependencies.dir.name");
 
@@ -235,15 +166,6 @@ public class PropsValues {
 		PropsUtil.get("test.include.dir.names"));
 
 	public static final String TEST_NAME = PropsUtil.get("test.name");
-
-	public static final String TEST_NAME_SKIP_PORTAL_INSTANCE = PropsUtil.get(
-		"test.name.skip.portal.instance");
-
-	public static final boolean TEST_PAUSE_ON_FAILURE = GetterUtil.getBoolean(
-		PropsUtil.get("test.pause.on.failure"));
-
-	public static final boolean TEST_PORTAL_INSTANCE = GetterUtil.getBoolean(
-		PropsUtil.get("test.portal.instance"));
 
 	public static final String TEST_POSHI_WARNINGS_FILE_NAME = PropsUtil.get(
 		"test.poshi.warnings.file.name");
@@ -263,21 +185,10 @@ public class PropsValues {
 	public static final String[] TEST_SUBREPO_DIRS = StringUtil.split(
 		PropsUtil.get("test.subrepo.dirs"));
 
-	public static final boolean TESTING_CLASS_METHOD = GetterUtil.getBoolean(
-		PropsUtil.get("testing.class.method"));
-
-	public static final boolean THEME_CONTAINS_RESOURCES =
-		GetterUtil.getBoolean(PropsUtil.get("theme.contains.resources"));
-
-	public static final String[] THEME_IDS = StringUtil.split(
-		PropsUtil.get("theme.ids"));
-
 	public static final int TIMEOUT_EXPLICIT_WAIT = GetterUtil.getInteger(
 		PropsUtil.get("timeout.explicit.wait"));
 
 	public static final int TIMEOUT_IMPLICIT_WAIT = GetterUtil.getInteger(
 		PropsUtil.get("timeout.implicit.wait"));
-
-	public static final String VM_HOST = PropsUtil.get("vm.host");
 
 }
