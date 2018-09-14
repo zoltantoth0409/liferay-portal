4663f7c6fe47662d72335295b9bbdfcda8bd3861
Hugo Huijser <hugo.huijser@liferay.com>
2018-09-14 10:03:08 +0800

    LPS-84119 Fix missing leading space in for-statement

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaWhitespaceCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaWhitespaceCheck.java
@@ -23,6 +23,9 @@ import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.IOException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Hugo Huijser
  */
@@ -35,7 +38,62 @@ public class JavaWhitespaceCheck extends WhitespaceCheck {
 
 		content = _formatWhitespace(fileName, content);
 
+		content = _formatForStatement(content);
+
 		content = StringUtil.replace(content, "\n\n\n", "\n\n");
+
+		return content;
+	}
+
+	private String _fixMissingLeadingSpace(
+		String content, String statement, String line) {
+
+		String leadingWhitespace = _getLeadingWhitespace(line);
+
+		if (leadingWhitespace.contains(StringPool.SPACE)) {
+			return content;
+		}
+
+		String newLine = StringUtil.replaceFirst(
+			line, leadingWhitespace, leadingWhitespace + " ");
+
+		String newStatement = StringUtil.replaceFirst(statement, line, newLine);
+
+		return StringUtil.replaceFirst(content, statement, newStatement);
+	}
+
+	private String _formatForStatement(String content) {
+		Matcher matcher = _forStatementPattern.matcher(content);
+
+		while (matcher.find()) {
+			String statement = _getStatement(
+				content, matcher.start(), "(", ")");
+
+			if (statement == null) {
+				continue;
+			}
+
+			int x = statement.indexOf(";\n");
+
+			if (x == -1) {
+				continue;
+			}
+
+			for (int i = getLineNumber(statement, x) + 1;; i++) {
+				String line = getLine(statement, i);
+
+				if (line == null) {
+					break;
+				}
+
+				String newContent = _fixMissingLeadingSpace(
+					content, statement, line);
+
+				if (!newContent.equals(content)) {
+					return newContent;
+				}
+			}
+		}
 
 		return content;
 	}
@@ -123,5 +181,42 @@ public class JavaWhitespaceCheck extends WhitespaceCheck {
 
 		return content;
 	}
+
+	private String _getLeadingWhitespace(String s) {
+		StringBundler sb = new StringBundler();
+
+		for (char c : s.toCharArray()) {
+			if (Character.isWhitespace(c)) {
+				sb.append(c);
+			}
+			else {
+				return sb.toString();
+			}
+		}
+
+		return sb.toString();
+	}
+
+	private String _getStatement(
+		String content, int start, String openChar, String closingChar) {
+
+		int x = start;
+
+		while (true) {
+			x = content.indexOf(closingChar, x + 1);
+
+			if (x == -1) {
+				return null;
+			}
+
+			String statement = content.substring(start, x + 1);
+
+			if (getLevel(statement, openChar, closingChar) == 0) {
+				return statement;
+			}
+		}
+	}
+
+	private final Pattern _forStatementPattern = Pattern.compile("\tfor \\(");
 
 }
