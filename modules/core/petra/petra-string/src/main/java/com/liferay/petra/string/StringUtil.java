db200804f1f7d777e9696c09fd305994b9c3240a
shuyangzhou <shuyang.zhou@liferay.com>
2018-09-14 17:12:53 -0700

    LPS-86037 Introduce com.liferay.petra.string.StringUtil.merge()

--- modules/core/petra/petra-string/src/main/java/com/liferay/petra/string/StringUtil.java
+++ modules/core/petra/petra-string/src/main/java/com/liferay/petra/string/StringUtil.java
@@ -15,8 +15,11 @@
 package com.liferay.petra.string;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.RandomAccess;
+import java.util.function.Function;
 
 /**
  * The String utility class.
@@ -28,6 +31,324 @@ import java.util.List;
  * @author Hugo Huijser
  */
 public class StringUtil {
+
+	public static String merge(boolean[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return String.valueOf(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static String merge(byte[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return String.valueOf(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static String merge(char[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return String.valueOf(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static <T> String merge(
+		Collection<T> col, Function<T, String> toStringFunction,
+		String delimiter) {
+
+		if ((col instanceof List) && (col instanceof RandomAccess)) {
+			return merge(
+				(List<T> & RandomAccess)col, toStringFunction, delimiter);
+		}
+
+		if (col == null) {
+			return null;
+		}
+
+		if (col.isEmpty()) {
+			return StringPool.BLANK;
+		}
+
+		StringBundler sb = new StringBundler(2 * col.size());
+
+		for (T t : col) {
+			sb.append(toStringFunction.apply(t));
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static <T> String merge(Collection<T> col, String delimiter) {
+		return merge(col, String::valueOf, delimiter);
+	}
+
+	public static String merge(double[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return String.valueOf(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static String merge(float[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return String.valueOf(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static String merge(int[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return String.valueOf(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static <T, L extends List<T> & RandomAccess> String merge(
+		L list, Function<T, String> toStringFunction, String delimiter) {
+
+		if (list == null) {
+			return null;
+		}
+
+		if (list.isEmpty()) {
+			return StringPool.BLANK;
+		}
+
+		StringBundler sb = new StringBundler(2 * list.size());
+
+		for (int i = 0; i < list.size(); i++) {
+			sb.append(toStringFunction.apply(list.get(i)));
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static <T, L extends List<T> & RandomAccess> String merge(
+		L list, String delimiter) {
+
+		return merge(list, String::valueOf, delimiter);
+	}
+
+	public static String merge(long[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return String.valueOf(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static String merge(short[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return String.valueOf(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static String merge(String[] array, String delimiter) {
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return array[0];
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(array[i]);
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static <T> String merge(
+		T[] array, Function<T, String> toStringFunction, String delimiter) {
+
+		if (array == null) {
+			return null;
+		}
+
+		if (array.length == 0) {
+			return StringPool.BLANK;
+		}
+
+		if (array.length == 1) {
+			return toStringFunction.apply(array[0]);
+		}
+
+		StringBundler sb = new StringBundler(2 * array.length);
+
+		for (int i = 0; i < array.length; i++) {
+			sb.append(toStringFunction.apply(array[i]));
+			sb.append(delimiter);
+		}
+
+		sb.setIndex(sb.index() - 1);
+
+		return sb.toString();
+	}
+
+	public static <T> String merge(T[] array, String delimiter) {
+		return merge(array, String::valueOf, delimiter);
+	}
 
 	public static List<String> split(String s) {
 		return split(s, CharPool.COMMA);
