4886bdd167702dd585dc0168f68eec85a344b1d6
shuyangzhou <shuyang.zhou@liferay.com>
2018-09-16 13:53:41 -0700

    LPS-85206 Check for info enabled

--- modules/core/portal-equinox-log-bridge/src/main/java/com/liferay/portal/equinox/log/bridge/internal/BundleStartStopLogger.java
+++ modules/core/portal-equinox-log-bridge/src/main/java/com/liferay/portal/equinox/log/bridge/internal/BundleStartStopLogger.java
@@ -34,11 +34,13 @@ public class BundleStartStopLogger implements SynchronousBundleListener {
 	@Override
 	public void bundleChanged(BundleEvent bundleEvent) {
 		if (_portalStarted.get()) {
-			if (bundleEvent.getType() == BundleEvent.STARTED) {
-				_log.info("STARTED {}", bundleEvent.getBundle());
-			}
-			else if (bundleEvent.getType() == BundleEvent.STOPPED) {
-				_log.info("STOPPED {}", bundleEvent.getBundle());
+			if (_log.isInfoEnabled()) {
+				if (bundleEvent.getType() == BundleEvent.STARTED) {
+					_log.info("STARTED {}", bundleEvent.getBundle());
+				}
+				else if (bundleEvent.getType() == BundleEvent.STOPPED) {
+					_log.info("STOPPED {}", bundleEvent.getBundle());
+				}
 			}
 		}
 		else {
