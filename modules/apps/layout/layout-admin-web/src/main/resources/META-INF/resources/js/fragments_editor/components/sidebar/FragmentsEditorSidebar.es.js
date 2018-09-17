f83cc0c09536f14fb444474fc07035517a574aed
Pablo Molina <contact@pablomolina.me>
2018-09-17 16:39:09 +0200

    LPS-85501 Remove unnecesary logic from FragmentsEditorSidebar

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/FragmentsEditorSidebar.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/FragmentsEditorSidebar.es.js
@@ -1,31 +1,12 @@
 import 'frontend-taglib/contextual_sidebar/ContextualSidebar.es';
 import Component from 'metal-component';
 import {Config} from 'metal-state';
-import {EventEmitterProxy} from 'metal-events';
 import Soy from 'metal-soy';
 
-import './SidebarAddedFragments.es';
-import './SidebarAvailableFragments.es';
-import './SidebarMapping.es';
+import './FragmentsEditorSidebarContent.es';
 import {HIDE_SIDEBAR} from '../../actions/actions.es';
 import {Store} from '../../store/store.es';
 import templates from './FragmentsEditorSidebar.soy';
-
-/**
- * Added tab ID
- * @review
- * @type {!string}
- */
-
-const ADDED_TAB_ID = 'added';
-
-/**
- * Default selected tab
- * @review
- * @type {!string}
- */
-
-const DEFAULT_TAB_ID = 'available';
 
 /**
  * FragmentsEditorSidebar
@@ -35,103 +16,12 @@ const DEFAULT_TAB_ID = 'available';
 class FragmentsEditorSidebar extends Component {
 
 	/**
-	 * @inheritDoc
-	 * @review
-	 */
-
-	created() {
-		const addedTab = this.sidebarTabs.find(tab => tab.id === ADDED_TAB_ID);
-
-		if (addedTab) {
-			this._addedTabEnabled = addedTab.enabled;
-		}
-	}
-
-	/**
-	 * @inheritDoc
-	 * @review
-	 */
-
-	dispose() {
-		this._disposeTabEventProxy();
-	}
-
-	/**
-	 * @inheritDoc
-	 * @review
-	 */
-
-	prepareStateForRender(state) {
-		return Object.assign(
-			{},
-			state,
-			{
-				sidebarTabs: state.sidebarTabs.map(
-					sidebarTab => {
-						return sidebarTab.id !== ADDED_TAB_ID ?
-							sidebarTab :
-							Object.assign(
-								{},
-								sidebarTab,
-								{enabled: state._addedTabEnabled}
-							);
-					}
-				)
-			}
-		);
-	}
-
-	/**
-	 * @inheritDoc
-	 * @review
-	 */
-
-	rendered() {
-		if (
-			this.refs.contextualSidebar &&
-			this.refs.contextualSidebar.refs.sidebarTab
-		) {
-			this._updateTabEventProxy(
-				this.refs.contextualSidebar.refs.sidebarTab
-			);
-		}
-	}
-
-	/**
 	 * Disallow setting element display to none
 	 * @inheritDoc
 	 * @review
 	 */
 
 	syncVisible() {}
-
-	/**
-	 * Enable or disable added tab depending on we have fragments or not
-	 * @private
-	 * @review
-	 */
-
-	toggleAddedTab(enabled) {
-		this._addedTabEnabled = !!enabled;
-
-		if (!enabled) {
-			this._selectedTab = DEFAULT_TAB_ID;
-		}
-	}
-
-	/**
-	 * Dispose the existing _tabEventProxy, if any.
-	 * @private
-	 * @review
-	 */
-
-	_disposeTabEventProxy() {
-		if (this._tabEventProxy) {
-			this._tabEventProxy.dispose();
-
-			this._tabEventProxy = null;
-		}
-	}
 
 	/**
 	 * @private
@@ -150,36 +40,6 @@ class FragmentsEditorSidebar extends Component {
 	_handleHideSidebarButtonClick() {
 		this.store.dispatchAction(HIDE_SIDEBAR);
 	}
-
-	/**
-	 * Updates _selectedTab according to the clicked element
-	 * @param {!Event} event
-	 * @private
-	 * @review
-	 */
-
-	_handleTabClick(event) {
-		this._selectedTab = event.delegateTarget.dataset.tabId ||
-			DEFAULT_TAB_ID;
-	}
-
-	/**
-	 *
-	 * Update or create the _tabEventProxy to propagate all events emitted
-	 * from the given tab.
-	 * @param {EventEmitter} sidebarTab
-	 * @private
-	 * @review
-	 */
-
-	_updateTabEventProxy(sidebarTab) {
-		if (!this._tabEventProxy) {
-			this._tabEventProxy = new EventEmitterProxy(sidebarTab, this);
-		}
-		else {
-			this._tabEventProxy.setOriginEmitter(sidebarTab);
-		}
-	}
 }
 
 /**
@@ -190,85 +50,6 @@ class FragmentsEditorSidebar extends Component {
  */
 
 FragmentsEditorSidebar.STATE = {
-
-	/**
-	 * CSS class for the fragments drop target.
-	 * @default undefined
-	 * @instance
-	 * @memberOf FragmentsEditor
-	 * @review
-	 * @type {!string}
-	 */
-
-	dropTargetClass: Config.string(),
-
-	/**
-	 * Tabs being shown in sidebar
-	 * @default undefined
-	 * @instance
-	 * @memberOf FragmentsEditorSidebar
-	 * @review
-	 * @type {!Array<{
-	 *   enabled: bool,
-	 * 	 id: string,
-	 * 	 label: string
-	 * }>}
-	 */
-
-	sidebarTabs: Config.arrayOf(
-		Config.shapeOf(
-			{
-				enabled: Config.bool().required(),
-				id: Config.string().required(),
-				label: Config.string().required()
-			}
-		)
-	).required(),
-
-	/**
-	 * Whether to show added tab or not
-	 * @default null
-	 * @instance
-	 * @memberOf FragmentsEditorSidebar
-	 * @private
-	 * @review
-	 * @type {bool}
-	 */
-
-	_addedTabEnabled: Config
-		.bool()
-		.internal()
-		.value(null),
-
-	/**
-	 * Tab selected inside sidebar
-	 * @default DEFAULT_TAB_ID
-	 * @instance
-	 * @memberOf FragmentsEditorSidebar
-	 * @private
-	 * @review
-	 * @type {string}
-	 */
-
-	_selectedTab: Config
-		.string()
-		.internal()
-		.value(DEFAULT_TAB_ID),
-
-	/**
-	 * Event proxy used for propagating tabs events
-	 * @default null
-	 * @instance
-	 * @memberOf FragmentsEditorSidebar
-	 * @private
-	 * @review
-	 * @type {object|null}
-	 */
-
-	_tabEventProxy: Config
-		.object()
-		.internal()
-		.value(null),
 
 	/**
 	 * Store instance
