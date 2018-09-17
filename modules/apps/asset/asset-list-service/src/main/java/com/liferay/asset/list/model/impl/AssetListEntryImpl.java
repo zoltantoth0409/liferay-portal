cf87d87cd1c8b4d2a60b01653c675e72d0736a7b
pavel-savinov <pavel.savinov@liferay.com>
2018-09-17 10:27:09 +0200

    LPS-85141 Use start and end parameters to be able to limit asset entries list size

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/model/impl/AssetListEntryImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/model/impl/AssetListEntryImpl.java
@@ -24,23 +24,19 @@ import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
 import com.liferay.asset.kernel.service.AssetTagLocalServiceUtil;
 import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
 import com.liferay.asset.list.constants.AssetListEntryTypeConstants;
-import com.liferay.petra.string.CharPool;
+import com.liferay.asset.list.model.AssetListEntryAssetEntryRel;
+import com.liferay.asset.list.service.AssetListEntryAssetEntryRelLocalServiceUtil;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
-import com.liferay.portal.kernel.xml.Document;
-import com.liferay.portal.kernel.xml.DocumentException;
-import com.liferay.portal.kernel.xml.Node;
-import com.liferay.portal.kernel.xml.SAXReaderUtil;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -56,23 +52,13 @@ public class AssetListEntryImpl extends AssetListEntryBaseImpl {
 	}
 
 	public List<AssetEntry> getAssetEntries() {
-		UnicodeProperties properties = new UnicodeProperties(true);
+		if (Objects.equals(
+				getType(), AssetListEntryTypeConstants.TYPE_MANUAL)) {
 
-		properties.fastLoad(getTypeSettings());
+			return _getManualAssetEntries(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		}
 
-		List<String> assetEntryXmls = Arrays.asList(
-			StringUtil.split(
-				properties.getProperty("assetEntryXml"), CharPool.COMMA));
-
-		Stream<String> stream = assetEntryXmls.stream();
-
-		return stream.map(
-			assetEntryXml -> _getAssetEntry(assetEntryXml)
-		).filter(
-			assetEntry -> assetEntry != null
-		).collect(
-			Collectors.toList()
-		);
+		return _getDynamicAssetEntries(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
 	}
 
 	public AssetEntryQuery getAssetEntryQuery(Layout layout) {
@@ -231,31 +217,6 @@ public class AssetListEntryImpl extends AssetListEntryBaseImpl {
 				new Long[assetCategoryIdsList.size()]));
 	}
 
-	private AssetEntry _getAssetEntry(String assetEntryXml) {
-		try {
-			Document document = SAXReaderUtil.read(assetEntryXml);
-
-			Node assetEntryUuidNode = document.selectSingleNode(
-				"//asset-entry-uuid");
-
-			Node assetEntryGroupIdNode = document.selectSingleNode(
-				"//asset-entry-group-id");
-
-			String assetEntryUuid = assetEntryUuidNode.getStringValue();
-
-			long groupId = GetterUtil.getLong(
-				assetEntryGroupIdNode.getStringValue());
-
-			return AssetEntryLocalServiceUtil.fetchEntry(
-				groupId, assetEntryUuid);
-		}
-		catch (DocumentException de) {
-			_log.error("Unable to parse asset entry XML");
-
-			return null;
-		}
-	}
-
 	private long[] _getClassNameIds(
 		UnicodeProperties properties, long[] availableClassNameIds) {
 
@@ -281,6 +242,28 @@ public class AssetListEntryImpl extends AssetListEntryBaseImpl {
 		}
 
 		return availableClassNameIds;
+	}
+
+	private List<AssetEntry> _getDynamicAssetEntries(int start, int end) {
+		return Collections.emptyList();
+	}
+
+	private List<AssetEntry> _getManualAssetEntries(int start, int end) {
+		List<AssetListEntryAssetEntryRel> assetListEntryAssetEntryRels =
+			AssetListEntryAssetEntryRelLocalServiceUtil.
+				getAssetListEntryAssetEntryRels(
+					getAssetListEntryId(), start, end);
+
+		Stream<AssetListEntryAssetEntryRel> stream =
+			assetListEntryAssetEntryRels.stream();
+
+		return stream.map(
+			assetListEntryAssetEntryRel ->
+				AssetEntryLocalServiceUtil.fetchEntry(
+					assetListEntryAssetEntryRel.getAssetEntryId())
+		).collect(
+			Collectors.toList()
+		);
 	}
 
 	private void _setCategoriesAndTags(
@@ -389,8 +372,5 @@ public class AssetListEntryImpl extends AssetListEntryBaseImpl {
 
 		assetEntryQuery.setNotAnyTagIds(notAnyAssetTagIds);
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		AssetListEntryImpl.class);
 
 }
