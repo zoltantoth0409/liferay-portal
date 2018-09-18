b0bf36c29779859197e144fede55bde484635260
Clóvis Neto <cloves13@hotmail.com>
2018-09-18 17:07:36 -0300

    LPS-84277 Creates success page component

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/components/SuccessPage/SuccessPage.es.js
@@ -0,0 +1,44 @@
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import {Config} from 'metal-state';
+
+import templates from './SucessPage.soy.js';
+import {setValue} from '../../util/i18n.es';
+
+class SucessPage extends Component {
+	static STATE = {
+		/**
+		 * @instance
+		 * @memberof SucessPage
+		 * @type {?string}
+		 */
+		contentLabel: Config.string().value(Liferay.Language.get('content')),
+
+		/**
+		 * @instance
+		 * @memberof SucessPage
+		 * @type {?object}
+		 */
+		successPageSettings: Config.object(),
+
+		/**
+		 * @instance
+		 * @memberof SucessPage
+		 * @type {?string}
+		 */
+		titleLabel: Config.string().value(Liferay.Language.get('title'))
+	}
+
+	_handleSuccessPageSettingsChanged(event) {
+		const {delegateTarget} = event;
+		const {dataset: {setting}, value} = delegateTarget;
+		const {successPageSettings} = this;
+		const language = themeDisplay.getLanguageId();
+
+		this.emit('successPageChanged', setValue(successPageSettings, language, setting, value));
+	}
+}
+
+Soy.register(SucessPage, templates);
+
+export default SucessPage;
