39449e51b63cfd590df48411860cce67777cf803
Clóvis Neto <cloves13@hotmail.com>
2018-09-18 16:43:47 -0300

    LPS-84277 Create setValue method to internationalizate an object value

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/util/i18n.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/util/i18n.es.js
@@ -1,15 +1,19 @@
 import {capitalize} from './strings.es';
 
+export const setValue = (obj, languageId, prop, val) => {
+	if (!obj[prop]) {
+		obj[prop] = {}
+	}
+
+	obj[prop][languageId] = val;
+
+	return obj;
+};
+
 export const setLocalizedValue = (obj, languageId, prop, val) => {
 	obj[prop] = val;
 
 	const localizedProperty = `localized${capitalize(prop)}`;
 
-	if (!obj[localizedProperty]) {
-		obj[localizedProperty] = {};
-	}
-
-	obj[localizedProperty][languageId] = val;
-
-	return obj;
+	return setValue(obj, languageId, localizedProperty, val);
 };
