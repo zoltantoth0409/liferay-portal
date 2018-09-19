05f44cd08b457903f77a805bc1611aa866bfb0f7
Hugo Huijser <hugo.huijser@liferay.com>
2018-09-19 08:26:48 +0800

    LPS-85296 Auto SF

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/service/permission/DDLDDMPermissionSupport.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/service/permission/DDLDDMPermissionSupport.java
@@ -25,8 +25,7 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(
 	immediate = true,
-	property =
-		{"model.class.name=com.liferay.dynamic.data.lists.model.DDLRecordSet"},
+	property = "model.class.name=com.liferay.dynamic.data.lists.model.DDLRecordSet",
 	service = {
 		DDMStructurePermissionSupport.class, DDMTemplatePermissionSupport.class
 	}
