340422536146813ffd640e78c1ef7dc0e2582444
Roberto Díaz <roberto.diaz@liferay.com>
2018-09-19 09:39:56 +0200

    LPS-85419 Create a new tool to handle the upgrade in the bundles
    (cherry picked from commit 4e8b8123dfd7b77eb245cc7cb3ff416847d8f102)

---
+++ modules/apps/comment/comment-api/src/main/java/com/liferay/comment/upgrade/UpgradeDiscussionSubscriptionClassName.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.comment.upgrade;
+
+import com.liferay.message.boards.model.MBDiscussion;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.subscription.model.Subscription;
+import com.liferay.subscription.service.SubscriptionLocalService;
+
+import java.util.List;
+
+/**
+ * @author Roberto Díaz
+ */
+public class UpgradeDiscussionSubscriptionClassName extends UpgradeProcess {
+
+	public UpgradeDiscussionSubscriptionClassName(
+		SubscriptionLocalService subscriptionLocalService, String className,
+		DeletionMode deletionMode) {
+
+		_subscriptionLocalService = subscriptionLocalService;
+		_className = className;
+		_deletionMode = deletionMode;
+	}
+
+	public enum DeletionMode {
+
+		ADD_NEW, DELETE_OLD
+
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addSubscriptions();
+
+		if (_deletionMode == DeletionMode.DELETE_OLD) {
+			_deleteSubscriptions();
+		}
+	}
+
+	private void _addSubscriptions() throws PortalException {
+		String newSubscriptionClassName =
+			MBDiscussion.class.getName() + StringPool.UNDERLINE + _className;
+
+		if (_subscriptionLocalService.getSubscriptionsCount(
+				newSubscriptionClassName) > 0) {
+
+			return;
+		}
+
+		List<Subscription> subscriptions =
+			_subscriptionLocalService.getSubscriptions(_className);
+
+		for (Subscription subscription : subscriptions) {
+			_subscriptionLocalService.addSubscription(
+				subscription.getUserId(), subscription.getGroupId(),
+				newSubscriptionClassName, subscription.getClassPK());
+		}
+	}
+
+	private void _deleteSubscriptions() throws PortalException {
+		List<Subscription> subscriptions =
+			_subscriptionLocalService.getSubscriptions(_className);
+
+		for (Subscription subscription : subscriptions) {
+			_subscriptionLocalService.deleteSubscription(
+				subscription.getSubscriptionId());
+		}
+	}
+
+	private final String _className;
+	private final DeletionMode _deletionMode;
+	private final SubscriptionLocalService _subscriptionLocalService;
+
+}
