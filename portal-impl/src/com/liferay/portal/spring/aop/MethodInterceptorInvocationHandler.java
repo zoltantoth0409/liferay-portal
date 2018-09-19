e05dcd1b8dc2ccbfdde6b3036f2b7635ba4a9b0e
Preston-Crary <preston.crary@liferay.com>
2018-09-19 13:58:54 -0700

    LPS-85613 Deprecate MethodInterceptorInvocationHandler, it has no usages.

--- portal-impl/src/com/liferay/portal/spring/aop/MethodInterceptorInvocationHandler.java
+++ portal-impl/src/com/liferay/portal/spring/aop/MethodInterceptorInvocationHandler.java
@@ -23,7 +23,9 @@ import org.aopalliance.intercept.MethodInterceptor;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of Judson (7.1.x), with no direct replacement
  */
+@Deprecated
 public class MethodInterceptorInvocationHandler implements InvocationHandler {
 
 	public MethodInterceptorInvocationHandler(
