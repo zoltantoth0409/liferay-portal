b5908bbfeb223ded0aecc7710565122bef2c13a5
shuyangzhou <shuyang.zhou@liferay.com>
2018-09-20 11:02:27 -0700

    LPS-85596 No more ZipFile usemmap in jdk 11

--- portal-impl/src/com/liferay/portal/util/InitUtil.java
+++ portal-impl/src/com/liferay/portal/util/InitUtil.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.security.xml.SecureXMLFactoryProviderUtil;
 import com.liferay.portal.kernel.util.BasePortalLifecycle;
 import com.liferay.portal.kernel.util.ClassLoaderUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.JavaDetector;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OSDetector;
@@ -80,7 +81,7 @@ public class InitUtil {
 		}
 
 		try {
-			if (!OSDetector.isWindows()) {
+			if (!OSDetector.isWindows() && !JavaDetector.isJDK11()) {
 				Field field = ReflectionUtil.getDeclaredField(
 					ZipFile.class, "usemmap");
 
