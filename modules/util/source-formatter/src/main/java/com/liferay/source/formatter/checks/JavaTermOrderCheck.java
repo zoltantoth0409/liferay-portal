8ad28939c7a7ac1b3d3e67e49bda4d80d6347cf3
Hugo Huijser <hugo.huijser@liferay.com>
2018-09-21 14:24:05 +0800

    LPS-84119 Skip ordering javaterm without access modifier

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaTermOrderCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaTermOrderCheck.java
@@ -22,6 +22,7 @@ import com.liferay.source.formatter.parser.comparator.JavaTermComparator;
 import java.io.IOException;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -83,7 +84,11 @@ public class JavaTermOrderCheck extends BaseJavaTermCheck {
 		JavaTerm previousJavaTerm = null;
 
 		for (JavaTerm javaTerm : childJavaTerms) {
-			if (javaTerm.isJavaStaticBlock()) {
+			if (javaTerm.isJavaStaticBlock() ||
+				Objects.equals(
+					javaTerm.getAccessModifier(),
+					JavaTerm.ACCESS_MODIFIER_DEFAULT)) {
+
 				continue;
 			}
 
