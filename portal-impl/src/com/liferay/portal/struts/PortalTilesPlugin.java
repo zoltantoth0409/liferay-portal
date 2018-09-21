0a04b24a5cfc7bd3c556862294a9d26df9cc6e0e
shuyangzhou <shuyang.zhou@liferay.com>
2018-09-21 15:32:31 -0700

    LPS-85722 Remove ComponentDefinitionsFactoryWrapper

--- portal-impl/src/com/liferay/portal/struts/PortalTilesPlugin.java
+++ portal-impl/src/com/liferay/portal/struts/PortalTilesPlugin.java
@@ -14,18 +14,25 @@
 
 package com.liferay.portal.struts;
 
+import com.liferay.petra.string.StringPool;
+
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 
 import org.apache.struts.action.ActionServlet;
 import org.apache.struts.action.PlugIn;
 import org.apache.struts.config.ModuleConfig;
 import org.apache.struts.config.PlugInConfig;
+import org.apache.struts.tiles.ComponentDefinition;
 import org.apache.struts.tiles.DefinitionsFactory;
 import org.apache.struts.tiles.DefinitionsFactoryConfig;
 import org.apache.struts.tiles.DefinitionsFactoryException;
+import org.apache.struts.tiles.NoSuchDefinitionException;
 import org.apache.struts.tiles.TilesUtilImpl;
-import org.apache.struts.tiles.definition.ComponentDefinitionsFactoryWrapper;
+import org.apache.struts.tiles.xmlDefinition.FactorySet;
 import org.apache.struts.tiles.xmlDefinition.I18nFactorySet;
 
 /**
@@ -41,29 +48,69 @@ public class PortalTilesPlugin implements PlugIn {
 	public void init(ActionServlet servlet, ModuleConfig moduleConfig)
 		throws ServletException {
 
+		ServletContext servletContext = servlet.getServletContext();
+
 		try {
-			DefinitionsFactory definitionsFactory =
-				new ComponentDefinitionsFactoryWrapper(new I18nFactorySet());
-
-			ServletContext servletContext = servlet.getServletContext();
-
-			definitionsFactory.init(_factoryConfig, servletContext);
-
 			servletContext.setAttribute(
-				TilesUtilImpl.DEFINITIONS_FACTORY, definitionsFactory);
+				TilesUtilImpl.DEFINITIONS_FACTORY,
+				new DefinitionsFactoryAdaptor(
+					new I18nFactorySet(servletContext, _properties)));
 		}
 		catch (DefinitionsFactoryException dfe) {
 			throw new ServletException(dfe);
 		}
 	}
 
-	public void setCurrentPlugInConfigObject(PlugInConfig plugInConfig)
-		throws ReflectiveOperationException {
+	public void setCurrentPlugInConfigObject(PlugInConfig plugInConfig) {
+		_properties = plugInConfig.getProperties();
 
-		_factoryConfig.populate(plugInConfig.getProperties());
+		_properties.put(
+			DefinitionsFactoryConfig.PARSER_VALIDATE_PARAMETER_NAME,
+			StringPool.TRUE);
 	}
 
-	private final DefinitionsFactoryConfig _factoryConfig =
-		new DefinitionsFactoryConfig();
+	private Map<String, String> _properties;
+
+	private static class DefinitionsFactoryAdaptor
+		implements DefinitionsFactory {
+
+		@Override
+		public void destroy() {
+		}
+
+		@Override
+		public DefinitionsFactoryConfig getConfig() {
+			return null;
+		}
+
+		@Override
+		public ComponentDefinition getDefinition(
+				String name, ServletRequest servletRequest,
+				ServletContext servletContext)
+			throws DefinitionsFactoryException, NoSuchDefinitionException {
+
+			return _factorySet.getDefinition(
+				name, servletRequest, servletContext);
+		}
+
+		@Override
+		public void init(
+			DefinitionsFactoryConfig definitionsFactoryConfig,
+			ServletContext servletContext) {
+		}
+
+		@Override
+		public void setConfig(
+			DefinitionsFactoryConfig definitionsFactoryConfig,
+			ServletContext servletContext) {
+		}
+
+		private DefinitionsFactoryAdaptor(FactorySet factorySet) {
+			_factorySet = factorySet;
+		}
+
+		private final FactorySet _factorySet;
+
+	}
 
 }
