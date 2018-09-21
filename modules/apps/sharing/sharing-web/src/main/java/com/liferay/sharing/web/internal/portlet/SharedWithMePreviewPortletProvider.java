5451d7453e414632c50eaa5752669bf8c7a5d0fd
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-09-21 15:00:46 +0200

    LPS-85625 Previewing sharing entries with the shared with me portlet

---
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/portlet/SharedWithMePreviewPortletProvider.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.web.internal.portlet;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
+import com.liferay.portal.kernel.portlet.PreviewPortletProvider;
+import com.liferay.sharing.web.internal.constants.SharingPortletKeys;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.sharing.model.SharingEntry",
+	service = PreviewPortletProvider.class
+)
+public class SharedWithMePreviewPortletProvider
+	extends BasePortletProvider implements PreviewPortletProvider {
+
+	@Override
+	public String getPortletName() {
+		return SharingPortletKeys.SHARED_WITH_ME;
+	}
+
+	@Override
+	public PortletURL getPortletURL(HttpServletRequest request, Group group)
+		throws PortalException {
+
+		PortletURL portletURL = super.getPortletURL(request, group);
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "/shared_with_me/view_sharing_entry");
+
+		return portletURL;
+	}
+
+}
