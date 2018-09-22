7fc61aa746fbeb402974d02cb803031055023cfa
Kenji Heigel <kenji.heigel@liferay.com>
2018-09-21 18:28:16 -0700

    LRQA-42321 Add other directories to the modified list

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/FunctionalBatchTestClassGroup.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/FunctionalBatchTestClassGroup.java
@@ -80,6 +80,9 @@ public class FunctionalBatchTestClassGroup extends BatchTestClassGroup {
 				ioe);
 		}
 
+		modifiedDirsList.addAll(
+			portalGitWorkingDirectory.getModifiedNonmoduleDirsList());
+
 		StringBuilder sb = new StringBuilder();
 
 		for (File modifiedDir : modifiedDirsList) {
