feac620df8dd7a124d402c946e6bc9e2463a72c0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-09-22 12:00:09 +0100

    LPS-85576 Copy code from asset publisher

--- modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/AssetListPortlet.java
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/AssetListPortlet.java
@@ -17,9 +17,32 @@ package com.liferay.asset.list.web.internal.portlet;
 import com.liferay.asset.list.constants.AssetListPortletKeys;
 import com.liferay.asset.list.exception.AssetListEntryTitleException;
 import com.liferay.asset.list.exception.DuplicateAssetListEntryTitleException;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.storage.Field;
+import com.liferay.dynamic.data.mapping.storage.Fields;
+import com.liferay.dynamic.data.mapping.util.DDMUtil;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import java.text.DateFormat;
+
+import java.util.Date;
 
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -47,6 +70,111 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class AssetListPortlet extends MVCPortlet {
+
+	public void getFieldValue(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws PortletException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		try {
+			ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				resourceRequest);
+
+			long structureId = ParamUtil.getLong(
+				resourceRequest, "structureId");
+
+			Fields fields = (Fields)serviceContext.getAttribute(
+				Fields.class.getName() + structureId);
+
+			if (fields == null) {
+				String fieldsNamespace = ParamUtil.getString(
+					resourceRequest, "fieldsNamespace");
+
+				fields = DDMUtil.getFields(
+					structureId, fieldsNamespace, serviceContext);
+			}
+
+			String fieldName = ParamUtil.getString(resourceRequest, "name");
+
+			Field field = fields.get(fieldName);
+
+			Serializable fieldValue = field.getValue(
+				themeDisplay.getLocale(), 0);
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			if (fieldValue != null) {
+				jsonObject.put("success", true);
+			}
+			else {
+				jsonObject.put("success", false);
+
+				writeJSON(resourceRequest, resourceResponse, jsonObject);
+
+				return;
+			}
+
+			DDMStructure ddmStructure = field.getDDMStructure();
+
+			String type = ddmStructure.getFieldType(fieldName);
+
+			Serializable displayValue = DDMUtil.getDisplayFieldValue(
+				themeDisplay, fieldValue, type);
+
+			jsonObject.put("displayValue", String.valueOf(displayValue));
+
+			if (fieldValue instanceof Boolean) {
+				jsonObject.put("value", (Boolean)fieldValue);
+			}
+			else if (fieldValue instanceof Date) {
+				DateFormat dateFormat =
+					DateFormatFactoryUtil.getSimpleDateFormat(
+						"yyyyMM ddHHmmss");
+
+				jsonObject.put("value", dateFormat.format(fieldValue));
+			}
+			else if (fieldValue instanceof Double) {
+				jsonObject.put("value", (Double)fieldValue);
+			}
+			else if (fieldValue instanceof Float) {
+				jsonObject.put("value", (Float)fieldValue);
+			}
+			else if (fieldValue instanceof Integer) {
+				jsonObject.put("value", (Integer)fieldValue);
+			}
+			else if (fieldValue instanceof Number) {
+				jsonObject.put("value", String.valueOf(fieldValue));
+			}
+			else {
+				jsonObject.put("value", (String)fieldValue);
+			}
+
+			writeJSON(resourceRequest, resourceResponse, jsonObject);
+		}
+		catch (Exception e) {
+			throw new PortletException(e);
+		}
+	}
+
+	@Override
+	public void serveResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws IOException, PortletException {
+
+		String resourceID = GetterUtil.getString(
+			resourceRequest.getResourceID());
+
+		if (resourceID.equals("getFieldValue")) {
+			getFieldValue(resourceRequest, resourceResponse);
+		}
+		else {
+			super.serveResource(resourceRequest, resourceResponse);
+		}
+
+		super.serveResource(resourceRequest, resourceResponse);
+	}
 
 	@Override
 	protected boolean isSessionErrorException(Throwable cause) {
