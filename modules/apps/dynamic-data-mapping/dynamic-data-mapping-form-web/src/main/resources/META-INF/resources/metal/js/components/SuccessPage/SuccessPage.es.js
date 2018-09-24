c089e3c78358ca3e5d270a211c9fe04142b060ae
Bruno Basto <bruno.basto@liferay.com>
2018-09-24 14:45:55 -0300

    LPS-84277 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/components/SuccessPage/SuccessPage.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/components/SuccessPage/SuccessPage.es.js
@@ -1,9 +1,8 @@
+import {Config} from 'metal-state';
+import {setValue} from '../../util/i18n.es';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
-import {Config} from 'metal-state';
-
 import templates from './SucessPage.soy.js';
-import {setValue} from '../../util/i18n.es';
 
 class SucessPage extends Component {
 	static STATE = {
