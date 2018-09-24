ec2049b829c9b2afe6bf015a5cf3a733c8ae2352
Jorge Díaz <jorge.diaz@liferay.com>
2018-09-24 16:37:24 +0200

    LPS-85621 New check to avoid executing a new upgrade process when it is already in execution.
    Note: this code was copied from catalina.sh of tomcat 9.0.10

--- modules/util/portal-tools-db-upgrade-client/scripts/db_upgrade.sh
+++ modules/util/portal-tools-db-upgrade-client/scripts/db_upgrade.sh
@@ -15,4 +15,52 @@ fi
 
 cd "$(dirname "${DB_UPGRADE_PATH}")"
 
+#Check running process
+DB_UPGRADE_PID=db_upgrade.pid
+
+if [ -f "$DB_UPGRADE_PID" ]; then
+	if [ -s "$DB_UPGRADE_PID" ]; then
+		echo "Existing PID file found during start."
+		if [ -r "$DB_UPGRADE_PID" ]; then
+			PID=`cat "$DB_UPGRADE_PID"`
+			ps -p $PID >/dev/null 2>&1
+			if [ $? -eq 0 ] ; then
+				echo "Upgrade client appears to still be running with PID $PID. Start aborted."
+				echo ""
+				echo "If the following process is not the upgrade client process, remove the PID file and try again:"
+				ps -f -p $PID
+				exit 1
+			else
+				echo "Removing/clearing stale PID file."
+				rm -f "$DB_UPGRADE_PID" >/dev/null 2>&1
+				if [ $? != 0 ]; then
+					if [ -w "$DB_UPGRADE_PID" ]; then
+						cat /dev/null > "$DB_UPGRADE_PID"
+					else
+						echo "Unable to remove or clear stale PID file. Start aborted."
+						exit 1
+					fi
+				fi
+			fi
+		else
+			echo "Unable to read PID file. Start aborted."
+			exit 1
+		fi
+	else
+		rm -f "$DB_UPGRADE_PID" >/dev/null 2>&1
+		if [ $? != 0 ]; then
+			if [ ! -w "$DB_UPGRADE_PID" ]; then
+			echo "Unable to remove or write to empty PID file. Start aborted."
+			exit 1
+			fi
+		fi
+	fi
+fi
+
+echo $$ > $DB_UPGRADE_PID
+
+#Execute upgrade client
 java -jar com.liferay.portal.tools.db.upgrade.client.jar "$@"
+
+#Cleanup
+rm $DB_UPGRADE_PID
