bc49751ab63c7e6476886b6fcd254ef06c639038
Frank Fu <frank.fu@liferay.com>
2018-09-24 15:50:31 -0700

    LPS-82411 Announcements uses the delete icon instead of the trash icon

--- modules/apps/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/display/context/AnnouncementsAdminViewManagementToolbarDisplayContext.java
+++ modules/apps/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/display/context/AnnouncementsAdminViewManagementToolbarDisplayContext.java
@@ -66,7 +66,7 @@ public class AnnouncementsAdminViewManagementToolbarDisplayContext {
 				add(
 					dropdownItem -> {
 						dropdownItem.putData("action", "deleteEntries");
-						dropdownItem.setIcon("trash");
+						dropdownItem.setIcon("times");
 						dropdownItem.setLabel(
 							LanguageUtil.get(_request, "delete"));
 						dropdownItem.setQuickAction(true);
