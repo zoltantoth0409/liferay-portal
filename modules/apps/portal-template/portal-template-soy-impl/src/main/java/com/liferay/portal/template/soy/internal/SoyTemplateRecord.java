7e9fe7bfa2337f6a00603782859da2b535deaa31
Rafael Praxedes <rafael.praxedes@liferay.com>
2018-09-24 14:55:48 -0300

    LPS-94054 partial backport of LPS-81687: Add support to BigDecimal to SoyTemplateRecord
    Original commit: "LPS-81687 Add support to BigDecimal to SoyTemplateRecord" 1fa75e868537c6e3eb337ce7c80112670de9dc02

--- modules/apps/portal-template/portal-template-soy-impl/src/main/java/com/liferay/portal/template/soy/internal/SoyTemplateRecord.java
+++ modules/apps/portal-template/portal-template-soy-impl/src/main/java/com/liferay/portal/template/soy/internal/SoyTemplateRecord.java
@@ -40,6 +40,9 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
@@ -269,6 +272,12 @@ public class SoyTemplateRecord extends SoyAbstractValue implements SoyRecord {
 		}
 		else if (object instanceof String) {
 			return StringData.forValue((String)object);
+		}
+		else if (object instanceof BigDecimal) {
+			return StringData.forValue(object.toString());
+		}
+		else if (object instanceof BigInteger) {
+			return StringData.forValue(object.toString());
 		}
 
 		SoyMapData soyMapData = new SoyMapData();
