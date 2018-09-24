a0c465d2c6bc7d4c11965eaee2581988b3465430
shuyangzhou <shuyang.zhou@liferay.com>
2018-09-24 14:32:32 -0700

    LPS-85591 Order matters

--- modules/apps/static/portal-osgi-web/portal-osgi-web-wab-extender/src/main/java/com/liferay/portal/osgi/web/wab/extender/internal/WabBundleProcessor.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-wab-extender/src/main/java/com/liferay/portal/osgi/web/wab/extender/internal/WabBundleProcessor.java
@@ -52,6 +52,7 @@ import java.util.EventListener;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -200,7 +201,7 @@ public class WabBundleProcessor {
 
 			scanTLDsForListeners(webXMLDefinition, servletContext);
 
-			Set<ListenerDefinition> listenerDefinitions = new HashSet<>();
+			Set<ListenerDefinition> listenerDefinitions = new LinkedHashSet<>();
 
 			listenerDefinitions.addAll(
 				modifiableServletContext.getListenerDefinitions());
