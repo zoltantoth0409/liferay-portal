a040ebbe5110fc57abb982d8ddca86e4cf25d983
Daniel Couso <daniel.couso@liferay.com>
2018-09-25 15:27:22 +0200

    LPS-85203 Add necessary methods to SiteNavigationMenuItemImpl

--- modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/model/impl/SiteNavigationMenuItemImpl.java
+++ modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/model/impl/SiteNavigationMenuItemImpl.java
@@ -16,6 +16,14 @@ package com.liferay.site.navigation.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalServiceUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author Brian Wing Shun Chan
  */
@@ -23,6 +31,56 @@ import aQute.bnd.annotation.ProviderType;
 public class SiteNavigationMenuItemImpl extends SiteNavigationMenuItemBaseImpl {
 
 	public SiteNavigationMenuItemImpl() {
+	}
+
+	/**
+	 * Returns all parent site navigation menu items of the current site
+	 * navigation menu item. The list is retrieved recursively with the direct
+	 * parent site navigation menu item listed first, and most distant parent
+	 * listed last.
+	 *
+	 * @return the current site navigation menu item's list of parent site
+	 * navigation menu items
+	 */
+	@Override
+	public List<SiteNavigationMenuItem> getAncestors() throws PortalException {
+		List<SiteNavigationMenuItem> siteNavigationMenuItems =
+			Collections.emptyList();
+
+		SiteNavigationMenuItem siteNavigationMenuItem = this;
+
+		while (!siteNavigationMenuItem.isRootSiteNavigationMenuItem()) {
+			siteNavigationMenuItem =
+				SiteNavigationMenuItemLocalServiceUtil.
+					getSiteNavigationMenuItem(
+						siteNavigationMenuItem.
+							getParentSiteNavigationMenuItemId());
+
+			if (siteNavigationMenuItems.isEmpty()) {
+				siteNavigationMenuItems = new ArrayList<>();
+			}
+
+			siteNavigationMenuItems.add(siteNavigationMenuItem);
+		}
+
+		return siteNavigationMenuItems;
+	}
+
+	/**
+	 * Returns <code>true</code> if the current site navigation menu item is
+	 * the root site navigation menu item.
+	 *
+	 * @return <code>true</code> if the current site navigation menu item is
+	 * the root site navigation menu item;
+	 *         <code>false</code> otherwise
+	 */
+	@Override
+	public boolean isRootSiteNavigationMenuItem() {
+		if (getParentSiteNavigationMenuItemId() == 0) {
+			return true;
+		}
+
+		return false;
 	}
 
 }
