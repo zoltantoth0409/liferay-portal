6771bdcaf6157a1a9ee916a331eed85be4cc774e
Cristina González Castellano <cristina.gonzalez@liferay.com>
2018-09-26 17:22:28 +0200

    LPS-85787 Move to right test

---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-impl/src/test/java/com/liferay/structured/content/apio/internal/architect/filter/StructuredContentEntityModelTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.internal.architect.filter;
+
+import com.liferay.structured.content.apio.architect.entity.EntityModel;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Cristina González
+ */
+public class StructuredContentEntityModelTest {
+
+	@Test
+	public void testGetName() {
+		EntityModel entityModel = new StructuredContentEntityModel();
+
+		Assert.assertEquals("StructuredContent", entityModel.getName());
+	}
+
+}
