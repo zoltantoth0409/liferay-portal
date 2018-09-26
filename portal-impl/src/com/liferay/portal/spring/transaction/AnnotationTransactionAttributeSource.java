a271d40b5805a426b7181eb3adea4d5468e39fcc
Preston-Crary <preston.crary@liferay.com>
2018-09-26 15:40:59 -0700

    LPS-85772 Deprecate TransactionAttributeSource field in TransactionInterceptor and inline it's usages into TransactionInterceptor.

--- portal-impl/src/com/liferay/portal/spring/transaction/AnnotationTransactionAttributeSource.java
+++ portal-impl/src/com/liferay/portal/spring/transaction/AnnotationTransactionAttributeSource.java
@@ -29,10 +29,15 @@ import org.springframework.transaction.interceptor.TransactionAttributeSource;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of Judson (7.1.x), with no direct replacement
  */
+@Deprecated
 public class AnnotationTransactionAttributeSource
 	implements TransactionAttributeSource {
 
+	/**
+	 * @see TransactionInterceptor#getTransactionAttribute(MethodInvocation)
+	 */
 	@Override
 	public TransactionAttribute getTransactionAttribute(
 		Method method, Class<?> targetClass) {
