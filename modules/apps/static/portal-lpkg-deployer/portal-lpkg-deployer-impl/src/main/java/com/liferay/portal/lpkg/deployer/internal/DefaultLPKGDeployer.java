b629681512d3bcc0466aabd2e0d88ea87b32f049
Brian Chan <brian.chan@liferay.com>
2018-09-27 15:20:15 -0700

    LPS-85713 rename

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/DefaultLPKGDeployer.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/DefaultLPKGDeployer.java
@@ -477,14 +477,14 @@ public class DefaultLPKGDeployer implements LPKGDeployer {
 		}
 	}
 
-	private boolean _isValid(String string) {
-		int index = string.lastIndexOf(CharPool.DASH);
+	private boolean _isValid(String pathName) {
+		int index = pathName.lastIndexOf(CharPool.DASH);
 
 		if (index == -1) {
 			return true;
 		}
 
-		String version = string.substring(index + 1, string.length() - 4);
+		String version = pathName.substring(index + 1, pathName.length() - 4);
 
 		int count = StringUtil.count(version, CharPool.PERIOD);
 
