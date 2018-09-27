0c32a8e3aa22fde50185184eaaa36c4ad877049b
tina.tian <tina.tian@liferay.com>
2018-09-27 15:33:10 -0700

    LPS-84138 Deprecated SingleVMPoolUtil and MultiVMPoolUtil

--- portal-kernel/src/com/liferay/portal/kernel/cache/SingleVMPoolUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/cache/SingleVMPoolUtil.java
@@ -22,7 +22,9 @@ import java.io.Serializable;
 /**
  * @author Brian Wing Shun Chan
  * @author Michael Young
+ * @deprecated As of Judson (7.1.x), with no direct replacement
  */
+@Deprecated
 @OSGiBeanProperties(service = SingleVMPoolUtil.class)
 public class SingleVMPoolUtil {
 
