0d0326c1465169581cb691a4fa3e842f56847c0f
pavel-savinov <pavel.savinov@liferay.com>
2018-09-27 09:12:28 +0200

    LPS-85711 Use navigate instead of window.location to support SPA

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
@@ -1806,7 +1806,7 @@
 				var redirect = event.redirect;
 
 				if (redirect) {
-					openingWindow.location = redirect;
+					openingWindow.Liferay.Util.navigate(redirect);
 				}
 				else {
 					var refresh = event.refresh;
