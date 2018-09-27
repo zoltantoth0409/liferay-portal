f20203b309f41e13ee129b71196f834ec45e2b29
Preston-Crary <preston.crary@liferay.com>
2018-09-26 18:00:36 -0700

    LPS-85775 Remove register or unregister calls and deprecate ServiceBeanAopCacheManagerUtil.

--- portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAopCacheManagerUtil.java
+++ portal-impl/src/com/liferay/portal/spring/aop/ServiceBeanAopCacheManagerUtil.java
@@ -19,7 +19,9 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of Judson (7.1.x), with no direct replacement
  */
+@Deprecated
 public class ServiceBeanAopCacheManagerUtil {
 
 	public static void registerServiceBeanAopCacheManager(
