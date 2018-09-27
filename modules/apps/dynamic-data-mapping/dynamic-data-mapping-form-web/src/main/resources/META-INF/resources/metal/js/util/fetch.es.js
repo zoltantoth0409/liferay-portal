9a279513af767bda51617f0520ccb0752618029a
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2018-09-27 14:29:38 -0300

    LPS-85359 Add convertToSearchparams

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/util/fetch.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/util/fetch.es.js
@@ -1,4 +1,5 @@
 import 'url-search-params-polyfill';
+
 const defaultHeaders = {
 	'Accept': 'application/json',
 	'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
@@ -29,3 +30,24 @@ export const makeFetch = ({url, body, headers = defaultHeaders, method = 'POST'}
 			}
 		);
 };
+
+export const convertToSearchParams = (body) => {
+	let searchParams = new URLSearchParams();
+
+	if (body instanceof HTMLFormElement) {
+		const formData = new FormData(body);
+
+		formData.forEach((value, key) => searchParams.set(key, value));
+	}
+	else if (body instanceof FormData) {
+		body.forEach((value, key) => searchParams.set(key, value));
+	}
+	else if (typeof body === 'object') {
+		Object.entries(body).forEach(([key, value]) => searchParams.append(key, value));
+	}
+	else {
+		searchParams = body;
+	}
+
+	return searchParams;
+}
