191dfd3e89528e3655e718294d4a096fe4237794
Preston-Crary <preston.crary@liferay.com>
2018-10-01 12:15:55 -0700

    LPS-85203 Inline, no need to add to the API for only one usage. If there's more usages that's a different story.

--- modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/model/impl/SiteNavigationMenuItemImpl.java
+++ modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/model/impl/SiteNavigationMenuItemImpl.java
@@ -16,13 +16,6 @@ package com.liferay.site.navigation.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.site.navigation.model.SiteNavigationMenuItem;
-import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalServiceUtil;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author Brian Wing Shun Chan
  */
@@ -30,52 +23,6 @@ import java.util.List;
 public class SiteNavigationMenuItemImpl extends SiteNavigationMenuItemBaseImpl {
 
 	public SiteNavigationMenuItemImpl() {
-	}
-
-	/**
-	 * Returns all parent site navigation menu items of the current site
-	 * navigation menu item. The list is retrieved recursively with the direct
-	 * parent site navigation menu item listed first, and most distant parent
-	 * listed last.
-	 *
-	 * @return the current site navigation menu item's list of parent site
-	 * navigation menu items
-	 */
-	@Override
-	public List<SiteNavigationMenuItem> getAncestors() throws PortalException {
-		List<SiteNavigationMenuItem> siteNavigationMenuItems =
-			new ArrayList<>();
-
-		SiteNavigationMenuItem siteNavigationMenuItem = this;
-
-		while (!siteNavigationMenuItem.isRootSiteNavigationMenuItem()) {
-			siteNavigationMenuItem =
-				SiteNavigationMenuItemLocalServiceUtil.
-					getSiteNavigationMenuItem(
-						siteNavigationMenuItem.
-							getParentSiteNavigationMenuItemId());
-
-			siteNavigationMenuItems.add(siteNavigationMenuItem);
-		}
-
-		return siteNavigationMenuItems;
-	}
-
-	/**
-	 * Returns <code>true</code> if the current site navigation menu item is
-	 * the root site navigation menu item.
-	 *
-	 * @return <code>true</code> if the current site navigation menu item is
-	 * the root site navigation menu item;
-	 *         <code>false</code> otherwise
-	 */
-	@Override
-	public boolean isRootSiteNavigationMenuItem() {
-		if (getParentSiteNavigationMenuItemId() == 0) {
-			return true;
-		}
-
-		return false;
 	}
 
 }
