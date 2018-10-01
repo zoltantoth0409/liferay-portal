d398a431ba795d8afb981e4fdf80ee2dfa4274b4
Julien Castelain <julien.castelain@liferay.com>
2018-10-01 16:46:36 +0200

    LPS-83819 SF

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
@@ -1,9 +1,11 @@
+import escape from './util/escape.es';
 import groupBy from './util/group_by.es';
 import portlet from './portlet/portlet.es';
 import navigate from './util/navigate.es';
 import objectToFormData from './util/object_to_form_data.es';
 import unescape from './util/unescape.es';
 
+Liferay.Util.escape = escape;
 Liferay.Util.groupBy = groupBy;
 Liferay.Util.navigate = navigate;
 Liferay.Util.objectToFormData = objectToFormData;
