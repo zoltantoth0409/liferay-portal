6777a3593f45718e1152e70a9da966374b729ef6
Brian Chan <brian.chan@liferay.com>
2018-10-01 21:52:21 -0700

    LPS-85887 regen

--- portal-kernel/src/com/liferay/portal/kernel/service/GroupLocalService.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/GroupLocalService.java
@@ -421,7 +421,7 @@ public interface GroupLocalService extends BaseLocalService,
 	* Returns the number of active or inactive groups associated with the company.
 	*
 	* @param companyId the primary key of the company
-	* @param active whether to return only active groups, or only inactive
+	* @param active whether to count only active groups, or only inactive
 	groups
 	* @return the number of active or inactive groups associated with the company
 	* @review
