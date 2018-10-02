7991ced60001918f67959d89204232b15a6b5d6e
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2018-10-02 16:59:21 -0300

    LPS-84665 portal-search-api: `assetCategoryTitles*` fields not wildcard query anymore
    https://issues.liferay.com/browse/LPS-84665
    https://issues.liferay.com/browse/LPS-84666
    
    Co-authored-by: Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
    Co-authored-by: weicao <wade.cao@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/configuration/QueryPreProcessConfiguration.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/configuration/QueryPreProcessConfiguration.java
@@ -32,7 +32,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 public interface QueryPreProcessConfiguration {
 
 	@Meta.AD(
-		deflt = "assetCategoryTitles?(_.+)?|emailAddress|license|path|screenName|tag|treePath|userName",
+		deflt = "emailAddress|license|path|screenName|tag|treePath|userName",
 		name = "field-name-patterns", required = false
 	)
 	public String[] fieldNamePatterns();
