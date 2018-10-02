da2b3143fe123a44d9edfa52d81b3c9878698ca6
Brian Chan <brian.chan@liferay.com>
2018-10-02 13:42:44 -0700

    LPS-85886 SF

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/impl/SegmentsEntryRelServiceImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/impl/SegmentsEntryRelServiceImpl.java
@@ -30,18 +30,7 @@ import com.liferay.segments.service.base.SegmentsEntryRelServiceBaseImpl;
 import java.util.List;
 
 /**
- * The implementation of the segments entry rel remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.segments.service.SegmentsEntryRelService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
  * @author Eduardo Garcia
- * @see SegmentsEntryRelServiceBaseImpl
- * @see com.liferay.segments.service.SegmentsEntryRelServiceUtil
  */
 public class SegmentsEntryRelServiceImpl
 	extends SegmentsEntryRelServiceBaseImpl {
