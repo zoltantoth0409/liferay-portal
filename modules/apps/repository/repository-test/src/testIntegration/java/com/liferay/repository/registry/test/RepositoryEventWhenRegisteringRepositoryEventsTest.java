889c160d1b92bbdf5c5eb6b5fcf9b3ff30e66859
tomwang2011 <tong.wang@liferay.com>
2018-10-03 15:54:22 -0700

    LPS-85088 Expand RepositoryEventTest into individual test classes

---
+++ modules/apps/repository/repository-test/src/testIntegration/java/com/liferay/repository/registry/test/RepositoryEventWhenRegisteringRepositoryEventsTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.repository.registry.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.repository.event.RepositoryEventType;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
+import com.liferay.portal.repository.registry.DefaultRepositoryEventRegistry;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Adolfo Pérez
+ */
+@RunWith(Arquillian.class)
+public class RepositoryEventWhenRegisteringRepositoryEventsTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule
+		liferayIntegrationTestRule = new LiferayIntegrationTestRule();
+
+	@Test
+	public void shouldAcceptAnyNonnullListener() {
+		_repositoryEventRegistry.registerRepositoryEventListener(
+			RepositoryEventType.Add.class, FileEntry.class,
+			new RepositoryEventTest.NoOpRepositoryEventListener<RepositoryEventType.Add, FileEntry>());
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void shouldFailOnNullListener() {
+		_repositoryEventRegistry.registerRepositoryEventListener(
+			RepositoryEventType.Add.class, FileEntry.class, null);
+	}
+
+	private final RepositoryEventRegistry _repositoryEventRegistry =
+		new DefaultRepositoryEventRegistry(null);
+
+}
---
+++ modules/apps/repository/repository-test/src/testIntegration/java/com/liferay/repository/registry/test/RepositoryEventWhenTriggeringEventsTest.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.repository.registry.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.repository.event.RepositoryEventType;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.repository.registry.DefaultRepositoryEventRegistry;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Adolfo Pérez
+ */
+@RunWith(Arquillian.class)
+public class RepositoryEventWhenTriggeringEventsTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule
+		liferayIntegrationTestRule = new LiferayIntegrationTestRule();
+
+	@Test
+	public void shouldExecuteAllMatchingListeners() throws Exception {
+		AtomicInteger count = new AtomicInteger();
+
+		for (int i = 0; i < 3; i++) {
+			registerCounterRepositoryEventListener(
+				_defaultRepositoryEventRegistry,
+				RepositoryEventType.Add.class, FileEntry.class, count);
+		}
+
+		_defaultRepositoryEventRegistry.trigger(
+			RepositoryEventType.Add.class, FileEntry.class, null);
+
+		Assert.assertEquals(3, count.get());
+	}
+
+	@Test
+	public void shouldExecuteListenerExactlyOncePerEvent()
+		throws Exception {
+
+		AtomicInteger count = registerCounterRepositoryEventListener(
+			_defaultRepositoryEventRegistry, RepositoryEventType.Add.class,
+			FileEntry.class);
+
+		int randomInt = Math.abs(RandomTestUtil.nextInt());
+
+		for (int i = 0; i < randomInt; i++) {
+			_defaultRepositoryEventRegistry.trigger(
+				RepositoryEventType.Add.class, FileEntry.class, null);
+		}
+
+		Assert.assertEquals(randomInt, count.get());
+	}
+
+	@Test
+	public void shouldExecuteOnlyMatchingListeners() throws Exception {
+		AtomicInteger count = registerCounterRepositoryEventListener(
+			_defaultRepositoryEventRegistry, RepositoryEventType.Add.class,
+			FileEntry.class);
+
+		_defaultRepositoryEventRegistry.registerRepositoryEventListener(
+			RepositoryEventType.Update.class, FileEntry.class,
+			new RepositoryEventTest.AlwaysFailingRepositoryEventListener<RepositoryEventType.Update, FileEntry>());
+
+		_defaultRepositoryEventRegistry.trigger(
+			RepositoryEventType.Add.class, FileEntry.class, null);
+
+		Assert.assertEquals(1, count.get());
+	}
+
+	private final DefaultRepositoryEventRegistry
+		_defaultRepositoryEventRegistry =
+		new DefaultRepositoryEventRegistry(null);
+
+}
