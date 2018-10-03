eb653bf66123a568d6228574ed8d467149139d7e
IstvanD <istvan.dezsi@lifeay.com>
2018-10-03 17:57:31 +0200

    LPS-85733 Adding test case to verify converted Media: tags

--- modules/apps/collaboration/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/service/test/WikiNodeLocalServiceTest.java
+++ modules/apps/collaboration/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/service/test/WikiNodeLocalServiceTest.java
@@ -15,9 +15,16 @@
 package com.liferay.wiki.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.ProgressTracker;
 import com.liferay.portal.kernel.util.ProgressTrackerThreadLocal;
@@ -76,6 +83,73 @@ public class WikiNodeLocalServiceTest {
 			_node.getNodeId(), "Liferay");
 
 		Assert.assertNotNull(importedPage);
+	}
+
+	@Test
+	public void testMediaLinks() throws Exception {
+		_node = WikiTestUtil.addNode(TestPropsValues.getGroupId());
+
+		ProgressTracker progressTracker = new ProgressTracker(
+			StringUtil.randomString());
+
+		ProgressTrackerThreadLocal.setProgressTracker(progressTracker);
+
+		byte[] pagesBytes = FileUtil.getBytes(
+			getClass(),
+			"/com/liferay/wiki/service/test/dependencies/media_link_test.xml");
+
+		InputStream pagesInputStream = new ByteArrayInputStream(pagesBytes);
+
+		byte[] filesBytes = FileUtil.getBytes(
+			getClass(),
+			"/com/liferay/wiki/service/test/dependencies/media_link_test.zip");
+
+		InputStream filesInputStream = new ByteArrayInputStream(filesBytes);
+
+		WikiNodeLocalServiceUtil.importPages(
+			TestPropsValues.getUserId(), _node.getNodeId(), "MediaWiki",
+			new InputStream[] {pagesInputStream, null, filesInputStream},
+			Collections.<String, String[]>emptyMap());
+
+		WikiPage importedPage = WikiPageLocalServiceUtil.fetchPage(
+			_node.getNodeId(), "Media link migration test");
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		Company company = CompanyLocalServiceUtil.getCompany(
+			_node.getCompanyId());
+
+		String portalURL = company.getPortalURL(_node.getGroupId());
+
+		themeDisplay.setPortalURL(portalURL);
+
+		WikiPage sharedImagesPage = WikiPageLocalServiceUtil.fetchPage(
+			_node.getNodeId(), "SharedImages");
+
+		long sharedImagesPageAttachmentsFolderId =
+			sharedImagesPage.getAttachmentsFolderId();
+
+		String testFileName = "media_link_test.docx";
+
+		String linkLabel = "Download link";
+
+		FileEntry attachmentFileEntry =
+			PortletFileRepositoryUtil.getPortletFileEntry(
+				_node.getGroupId(), sharedImagesPageAttachmentsFolderId,
+				testFileName);
+
+		String attachmentFileEntryURL =
+			PortletFileRepositoryUtil.getPortletFileEntryURL(
+				themeDisplay, attachmentFileEntry, StringPool.BLANK);
+
+		String linkTag = StringBundler.concat(
+			"[[", attachmentFileEntryURL, StringPool.PIPE, linkLabel, "]]");
+
+		String expectedContent = StringBundler.concat(
+			"<<TableOfContents>>", StringPool.NEW_LINE, StringPool.NEW_LINE,
+			linkTag);
+
+		Assert.assertEquals(expectedContent, importedPage.getContent());
 	}
 
 	@DeleteAfterTestRun
