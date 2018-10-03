8132fa8a65cdda2037ba3deb638a340f854bacf3
pavel-savinov <pavel.savinov@liferay.com>
2018-10-03 14:30:40 +0200

    LPS-85932 Action command to add asset list scope group

---
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/portlet/action/AddScopeGroupMVCActionCommand.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.web.internal.portlet.action;
+
+import com.liferay.asset.list.constants.AssetListPortletKeys;
+import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.asset.list.service.AssetListEntryService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AssetListPortletKeys.ASSET_LIST,
+		"mvc.command.name=/asset_list/add_scope_group"
+	},
+	service = MVCActionCommand.class
+)
+public class AddScopeGroupMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long assetListEntryId = ParamUtil.getLong(
+			actionRequest, "assetListEntryId");
+
+		long groupId = ParamUtil.getLong(actionRequest, "groupId");
+
+		AssetListEntry assetListEntry =
+			_assetListEntryService.fetchAssetListEntry(assetListEntryId);
+
+		if (assetListEntry != null) {
+			UnicodeProperties properties = new UnicodeProperties(true);
+
+			properties.fastLoad(assetListEntry.getTypeSettings());
+
+			long[] groupIds = GetterUtil.getLongValues(
+				StringUtil.split(properties.getProperty("groupIds")));
+
+			if (ArrayUtil.isEmpty(groupIds)) {
+				ArrayUtil.append(groupIds, assetListEntry.getGroupId());
+			}
+
+			if (!ArrayUtil.contains(groupIds, groupId)) {
+				groupIds = ArrayUtil.append(groupIds, groupId);
+			}
+
+			properties.setProperty("groupIds", StringUtil.merge(groupIds));
+
+			_assetListEntryService.updateAssetListEntrySettings(
+				assetListEntryId, properties.toString());
+		}
+	}
+
+	@Reference
+	private AssetListEntryService _assetListEntryService;
+
+}
