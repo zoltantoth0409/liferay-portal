be66c54b42cf11b3bdebb4f28650e1345a011540
tomwang2011 <tong.wang@liferay.com>
2018-10-03 15:54:22 -0700

    LPS-85088 Expand RepositoryEventTest into individual test classes

--- modules/apps/repository/repository-test/src/testIntegration/java/com/liferay/repository/registry/test/RepositoryEventTest.java
+++ modules/apps/repository/repository-test/src/testIntegration/java/com/liferay/repository/registry/test/RepositoryEventTest.java
@@ -14,21 +14,12 @@
 
 package com.liferay.repository.registry.test;
 
-import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.repository.event.RepositoryEventListener;
 import com.liferay.portal.kernel.repository.event.RepositoryEventType;
-import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.repository.registry.DefaultRepositoryEventRegistry;
-import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.junit.Assert;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
@@ -37,98 +28,6 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Enclosed.class)
 public class RepositoryEventTest {
-
-	@RunWith(Arquillian.class)
-	public static final class WhenRegisteringRepositoryEvents {
-
-		@ClassRule
-		@Rule
-		public static final LiferayIntegrationTestRule
-			liferayIntegrationTestRule = new LiferayIntegrationTestRule();
-
-		@Test
-		public void shouldAcceptAnyNonnullListener() {
-			_repositoryEventRegistry.registerRepositoryEventListener(
-				RepositoryEventType.Add.class, FileEntry.class,
-				new NoOpRepositoryEventListener
-					<RepositoryEventType.Add, FileEntry>());
-		}
-
-		@Test(expected = NullPointerException.class)
-		public void shouldFailOnNullListener() {
-			_repositoryEventRegistry.registerRepositoryEventListener(
-				RepositoryEventType.Add.class, FileEntry.class, null);
-		}
-
-		private final RepositoryEventRegistry _repositoryEventRegistry =
-			new DefaultRepositoryEventRegistry(null);
-
-	}
-
-	@RunWith(Arquillian.class)
-	public static final class WhenTriggeringEvents {
-
-		@ClassRule
-		@Rule
-		public static final LiferayIntegrationTestRule
-			liferayIntegrationTestRule = new LiferayIntegrationTestRule();
-
-		@Test
-		public void shouldExecuteAllMatchingListeners() throws Exception {
-			AtomicInteger count = new AtomicInteger();
-
-			for (int i = 0; i < 3; i++) {
-				registerCounterRepositoryEventListener(
-					_defaultRepositoryEventRegistry,
-					RepositoryEventType.Add.class, FileEntry.class, count);
-			}
-
-			_defaultRepositoryEventRegistry.trigger(
-				RepositoryEventType.Add.class, FileEntry.class, null);
-
-			Assert.assertEquals(3, count.get());
-		}
-
-		@Test
-		public void shouldExecuteListenerExactlyOncePerEvent()
-			throws Exception {
-
-			AtomicInteger count = registerCounterRepositoryEventListener(
-				_defaultRepositoryEventRegistry, RepositoryEventType.Add.class,
-				FileEntry.class);
-
-			int randomInt = Math.abs(RandomTestUtil.nextInt());
-
-			for (int i = 0; i < randomInt; i++) {
-				_defaultRepositoryEventRegistry.trigger(
-					RepositoryEventType.Add.class, FileEntry.class, null);
-			}
-
-			Assert.assertEquals(randomInt, count.get());
-		}
-
-		@Test
-		public void shouldExecuteOnlyMatchingListeners() throws Exception {
-			AtomicInteger count = registerCounterRepositoryEventListener(
-				_defaultRepositoryEventRegistry, RepositoryEventType.Add.class,
-				FileEntry.class);
-
-			_defaultRepositoryEventRegistry.registerRepositoryEventListener(
-				RepositoryEventType.Update.class, FileEntry.class,
-				new AlwaysFailingRepositoryEventListener
-					<RepositoryEventType.Update, FileEntry>());
-
-			_defaultRepositoryEventRegistry.trigger(
-				RepositoryEventType.Add.class, FileEntry.class, null);
-
-			Assert.assertEquals(1, count.get());
-		}
-
-		private final DefaultRepositoryEventRegistry
-			_defaultRepositoryEventRegistry =
-				new DefaultRepositoryEventRegistry(null);
-
-	}
 
 	protected static <S extends RepositoryEventType, T>
 		AtomicInteger registerCounterRepositoryEventListener(
