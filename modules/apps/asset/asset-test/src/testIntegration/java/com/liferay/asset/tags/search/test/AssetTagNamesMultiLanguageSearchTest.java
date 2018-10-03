5c4f5d017809527713e3178e1bf5becca7406a81
weicao <wade.cao@liferay.com>
2018-10-03 10:57:11 -0700

    LPS-84665 asset-test: AssetTagNamesMultiLanguageSearchTest
    https://issues.liferay.com/browse/LPS-84665
    https://issues.liferay.com/browse/LPS-84666
    
    Co-authored-by: Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
    Co-authored-by: weicao <wade.cao@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/tags/search/test/AssetTagNamesMultiLanguageSearchTest.java
@@ -0,0 +1,278 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.search.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.document.library.test.util.search.FileEntryBlueprint;
+import com.liferay.document.library.test.util.search.FileEntrySearchFixture;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcher;
+import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcherManager;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
+import com.liferay.portal.search.facet.tag.AssetTagNamesFacetFactory;
+import com.liferay.portal.search.test.util.DocumentsAssert;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.users.admin.test.util.search.GroupBlueprint;
+import com.liferay.users.admin.test.util.search.UserSearchFixture;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Wade Cao
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class AssetTagNamesMultiLanguageSearchTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		WorkflowThreadLocal.setEnabled(false);
+
+		_fileEntrySearchFixture = new FileEntrySearchFixture(dlAppLocalService);
+
+		_fileEntrySearchFixture.setUp();
+
+		_userSearchFixture = new UserSearchFixture();
+
+		_userSearchFixture.setUp();
+
+		_assetTags = _userSearchFixture.getAssetTags();
+		_groups = _userSearchFixture.getGroups();
+		_users = _userSearchFixture.getUsers();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_userSearchFixture.tearDown();
+		_fileEntrySearchFixture.tearDown();
+	}
+
+	@Test
+	public void testChineseTags() throws Exception {
+		Locale locale = LocaleUtil.CHINA;
+		String title1 = "title should not match";
+		String tag = "你好";
+
+		Group group = _userSearchFixture.addGroup(
+			new GroupBlueprint() {
+				{
+					defaultLocale = locale;
+				}
+			});
+
+		_fileEntrySearchFixture.addFileEntry(
+			new FileEntryBlueprint() {
+				{
+					assetTagNames = new String[] {tag};
+					fileName = title1;
+					groupId = group.getGroupId();
+					title = title1;
+					userId = getAdminUserId(group);
+				}
+			});
+
+		assertSearch(tag, locale);
+	}
+
+	@Test
+	public void testEnglishTags() throws Exception {
+		Locale locale = Locale.US;
+		String title1 = "title should not match";
+		String tag = "searchtag";
+
+		Group group = _userSearchFixture.addGroup(
+			new GroupBlueprint() {
+				{
+					defaultLocale = locale;
+				}
+			});
+
+		_fileEntrySearchFixture.addFileEntry(
+			new FileEntryBlueprint() {
+				{
+					assetTagNames = new String[] {tag};
+					fileName = title1;
+					groupId = group.getGroupId();
+					title = title1;
+					userId = getAdminUserId(group);
+				}
+			});
+
+		assertSearch(tag, locale);
+	}
+
+	@Test
+	public void testJapaneseTags() throws Exception {
+		Locale locale = LocaleUtil.JAPAN;
+		String title1 = "first title should not match";
+		String title2 = "second title should not match";
+		String tag1 = "東京";
+		String tag2 = "出前京丁";
+
+		Group group = _userSearchFixture.addGroup(
+			new GroupBlueprint() {
+				{
+					defaultLocale = locale;
+				}
+			});
+
+		_fileEntrySearchFixture.addFileEntry(
+			new FileEntryBlueprint() {
+				{
+					assetTagNames = new String[] {tag1};
+					fileName = title1;
+					groupId = group.getGroupId();
+					title = title1;
+					userId = getAdminUserId(group);
+				}
+			});
+
+		_fileEntrySearchFixture.addFileEntry(
+			new FileEntryBlueprint() {
+				{
+					assetTagNames = new String[] {tag2};
+					fileName = title2;
+					groupId = group.getGroupId();
+					title = title2;
+					userId = getAdminUserId(group);
+				}
+			});
+
+		assertSearch(tag1, locale);
+		assertSearch(tag2, locale);
+	}
+
+	protected void assertDLFileEntryIndexer(String tagName, Locale locale)
+		throws Exception {
+
+		Indexer<DLFileEntry> indexer = indexerRegistry.getIndexer(
+			DLFileEntry.class);
+
+		SearchContext searchContext = getSearchContext(tagName, locale);
+
+		Hits hits = indexer.search(searchContext);
+
+		assertHits(tagName, hits, searchContext);
+	}
+
+	protected void assertFacetedSearcher(String tagName, Locale locale)
+		throws Exception {
+
+		FacetedSearcher facetedSearcher =
+			facetedSearcherManager.createFacetedSearcher();
+
+		SearchContext searchContext = getSearchContext(tagName, locale);
+
+		Hits hits = facetedSearcher.search(searchContext);
+
+		assertHits(tagName, hits, searchContext);
+	}
+
+	protected void assertHits(
+		String tagName, Hits hits, SearchContext searchContext) {
+
+		DocumentsAssert.assertValuesIgnoreRelevance(
+			(String)searchContext.getAttribute("queryString"), hits.getDocs(),
+			Field.ASSET_TAG_NAMES, Arrays.asList(tagName));
+	}
+
+	protected void assertSearch(String tagName, Locale locale)
+		throws Exception {
+
+		assertDLFileEntryIndexer(tagName, locale);
+		assertFacetedSearcher(tagName, locale);
+	}
+
+	protected long getAdminUserId(Group group) {
+		try {
+			User user = UserTestUtil.getAdminUser(group.getCompanyId());
+
+			return user.getUserId();
+		}
+		catch (PortalException pe) {
+			throw new RuntimeException(pe);
+		}
+	}
+
+	protected SearchContext getSearchContext(String keywords, Locale locale)
+		throws Exception {
+
+		SearchContext searchContext = _userSearchFixture.getSearchContext(
+			keywords);
+
+		searchContext.setLocale(locale);
+
+		return searchContext;
+	}
+
+	@Inject
+	protected static AssetTagNamesFacetFactory assetTagNamesFacetFactory;
+
+	@Inject
+	protected static DLAppLocalService dlAppLocalService;
+
+	@Inject
+	protected static FacetedSearcherManager facetedSearcherManager;
+
+	@Inject
+	protected static IndexerRegistry indexerRegistry;
+
+	@DeleteAfterTestRun
+	private List<AssetTag> _assetTags;
+
+	private FileEntrySearchFixture _fileEntrySearchFixture;
+
+	@DeleteAfterTestRun
+	private List<Group> _groups;
+
+	@DeleteAfterTestRun
+	private List<User> _users;
+
+	private UserSearchFixture _userSearchFixture;
+
+}
