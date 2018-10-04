93c391511d3bc6fae9439d7153b2a91340770317
hudakl <laszlo.hudak@liferay.com>
2018-10-04 14:42:42 +0200

    LPS-85890 Build Services

--- portal-kernel/src/com/liferay/message/boards/kernel/service/MBMessageLocalService.java
+++ portal-kernel/src/com/liferay/message/boards/kernel/service/MBMessageLocalService.java
@@ -385,6 +385,10 @@ public interface MBMessageLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public IndexableActionableDynamicQuery getIndexableActionableDynamicQuery();
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public MBMessage getLastThreadMessage(long threadId, int status)
+		throws PortalException;
+
 	/**
 	 * Returns the message-boards message with the primary key.
 	 *
