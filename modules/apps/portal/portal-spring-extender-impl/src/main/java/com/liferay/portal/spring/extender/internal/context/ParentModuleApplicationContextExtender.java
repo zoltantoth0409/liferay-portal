85fea3edf6f91fcea2109ddc29b05f3b6624006a
Preston-Crary <preston.crary@liferay.com>
2018-10-04 14:38:25 -0700

    LPS-86034 Remove unused features, cache documents and avoid class introspection for parent spring context in the spring extender. We're able to do this since we know exactly which features we need for the parent context.

--- modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/context/ParentModuleApplicationContextExtender.java
+++ modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/context/ParentModuleApplicationContextExtender.java
@@ -15,17 +15,28 @@
 package com.liferay.portal.spring.extender.internal.context;
 
 import com.liferay.osgi.felix.util.AbstractExtender;
+import com.liferay.petra.concurrent.ConcurrentReferenceValueHashMap;
+import com.liferay.petra.memory.FinalizeManager;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.InfrastructureUtil;
-import com.liferay.portal.spring.context.PortletBeanFactoryPostProcessor;
+import com.liferay.portal.kernel.util.TextFormatter;
+import com.liferay.portal.spring.bean.LiferayBeanFactory;
 import com.liferay.portal.spring.extender.internal.bean.ApplicationContextServicePublisherUtil;
 import com.liferay.portal.spring.extender.internal.bundle.CompositeResourceLoaderBundle;
 import com.liferay.portal.spring.extender.internal.classloader.BundleResolverClassLoader;
 
+import java.io.IOException;
+
+import java.lang.reflect.Method;
+
 import java.util.Dictionary;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.felix.utils.extender.Extension;
 
@@ -36,6 +47,29 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.config.BeanReference;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.TypedStringValue;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.ManagedProperties;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.xml.ResourceEntityResolver;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.Resource;
+
+import org.w3c.dom.Document;
+
+import org.xml.sax.InputSource;
 
 /**
  * @author Preston Crary
@@ -95,6 +129,273 @@ public class ParentModuleApplicationContextExtender extends AbstractExtender {
 	private static final Log _log = LogFactoryUtil.getLog(
 		ParentModuleApplicationContextExtender.class);
 
+	private static class CachingXmlBeanDefinitionReader
+		extends XmlBeanDefinitionReader {
+
+		@Override
+		protected Document doLoadDocument(
+				InputSource inputSource, Resource resource)
+			throws Exception {
+
+			Document document = _documents.get(resource.getFilename());
+
+			if (document == null) {
+				document = super.doLoadDocument(inputSource, resource);
+
+				_documents.put(resource.getFilename(), document);
+			}
+
+			return document;
+		}
+
+		private CachingXmlBeanDefinitionReader(
+			BeanDefinitionRegistry beanDefinitionRegistry) {
+
+			super(beanDefinitionRegistry);
+		}
+
+		private static Map<String, Document> _documents =
+			new ConcurrentReferenceValueHashMap<>(
+				FinalizeManager.SOFT_REFERENCE_FACTORY);
+
+	}
+
+	private static class ParentModuleApplicationContext
+		extends ModuleApplicationContext {
+
+		@Override
+		public void refresh() {
+			prepareRefresh();
+
+			ConfigurableListableBeanFactory configurableListableBeanFactory =
+				obtainFreshBeanFactory();
+
+			prepareBeanFactory(configurableListableBeanFactory);
+
+			try {
+				initMessageSource();
+
+				initApplicationEventMulticaster();
+
+				configurableListableBeanFactory.freezeConfiguration();
+
+				configurableListableBeanFactory.preInstantiateSingletons();
+
+				finishRefresh();
+			}
+			catch (BeansException be) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(be, be);
+				}
+
+				destroyBeans();
+
+				cancelRefresh(be);
+
+				throw be;
+			}
+		}
+
+		@Override
+		protected DefaultListableBeanFactory createBeanFactory() {
+			return new ParentModuleApplicationContextBeanFactory(
+				getInternalParentBeanFactory());
+		}
+
+		@Override
+		protected void loadBeanDefinitions(
+				DefaultListableBeanFactory defaultListableBeanFactory)
+			throws IOException {
+
+			XmlBeanDefinitionReader xmlBeanDefinitionReader =
+				new CachingXmlBeanDefinitionReader(defaultListableBeanFactory);
+
+			xmlBeanDefinitionReader.setEntityResolver(
+				new ResourceEntityResolver(this));
+			xmlBeanDefinitionReader.setEnvironment(getEnvironment());
+			xmlBeanDefinitionReader.setResourceLoader(this);
+
+			initBeanDefinitionReader(xmlBeanDefinitionReader);
+
+			loadBeanDefinitions(xmlBeanDefinitionReader);
+		}
+
+		private ParentModuleApplicationContext(
+			Bundle bundle, Bundle extenderBundle) {
+
+			super(
+				new CompositeResourceLoaderBundle(bundle, extenderBundle),
+				new BundleResolverClassLoader(bundle, extenderBundle),
+				_PARENT_CONFIG_LOCATIONS);
+		}
+
+	}
+
+	private static class ParentModuleApplicationContextBeanFactory
+		extends LiferayBeanFactory {
+
+		@Override
+		public void preInstantiateSingletons() {
+			for (String beanName : getBeanDefinitionNames()) {
+				RootBeanDefinition rootBeanDefinition =
+					getMergedLocalBeanDefinition(beanName);
+
+				if (!rootBeanDefinition.isAbstract() &&
+					rootBeanDefinition.isSingleton() &&
+					!rootBeanDefinition.isLazyInit() &&
+					!isFactoryBean(beanName)) {
+
+					getBean(beanName);
+				}
+			}
+		}
+
+		@Override
+		protected void applyPropertyValues(
+			String beanName, BeanDefinition beanDefinition,
+			BeanWrapper beanWrapper, PropertyValues propertyValues) {
+
+			try {
+				for (PropertyValue propertyValue :
+						propertyValues.getPropertyValues()) {
+
+					_applyPropertyValue(
+						beanName, beanDefinition, beanWrapper, propertyValue);
+				}
+			}
+			catch (ReflectiveOperationException roe) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(roe, roe);
+				}
+
+				throw new BeanCreationException(beanName, roe);
+			}
+		}
+
+		private ParentModuleApplicationContextBeanFactory(
+			BeanFactory parentBeanFactory) {
+
+			super(parentBeanFactory);
+		}
+
+		private void _applyPropertyValue(
+				String beanName, BeanDefinition beanDefinition,
+				BeanWrapper beanWrapper, PropertyValue propertyValue)
+			throws ReflectiveOperationException {
+
+			String name = propertyValue.getName();
+
+			Object value = _resolvePropertyValue(
+				beanName, beanDefinition, propertyValue.getValue());
+
+			String methodKey = beanName.concat(StringPool.DASH.concat(name));
+
+			Method method = _methods.get(methodKey);
+
+			if (method == null) {
+				String setterMethodName = "set".concat(
+					TextFormatter.format(name, TextFormatter.G));
+
+				Class<?> beanClass = beanWrapper.getWrappedClass();
+
+				for (Method currentMethod : beanClass.getMethods()) {
+					if (setterMethodName.equals(currentMethod.getName())) {
+						Class<?>[] parameterTypes =
+							currentMethod.getParameterTypes();
+
+						if ((parameterTypes.length == 1) &&
+							parameterTypes[0].isAssignableFrom(
+								value.getClass())) {
+
+							method = currentMethod;
+
+							_methods.put(methodKey, currentMethod);
+
+							break;
+						}
+					}
+				}
+			}
+
+			if (method == null) {
+				throw new BeanCreationException(
+					StringBundler.concat(
+						"Could not find setter for {bean=", beanName,
+						", property=", name, "}"));
+			}
+
+			method.invoke(beanWrapper.getWrappedInstance(), value);
+		}
+
+		private Object _resolvePropertyValue(
+			String beanName, BeanDefinition beanDefinition, Object value) {
+
+			if (value instanceof BeanReference) {
+				BeanReference beanReference = (BeanReference)value;
+
+				value = getBean(beanReference.getBeanName());
+			}
+			else if (value instanceof BeanDefinitionHolder) {
+				BeanDefinitionHolder beanDefinitionHolder =
+					(BeanDefinitionHolder)value;
+
+				RootBeanDefinition mergedBeanDefinition =
+					getMergedBeanDefinition(
+						beanDefinitionHolder.getBeanName(),
+						beanDefinitionHolder.getBeanDefinition(),
+						beanDefinition);
+
+				String innerBeanName =
+					mergedBeanDefinition.getBeanClassName() + "#0";
+
+				registerContainedBean(beanName, innerBeanName);
+
+				value = createBean(innerBeanName, mergedBeanDefinition, null);
+			}
+			else if (value instanceof ManagedProperties) {
+				ManagedProperties managedProperties = (ManagedProperties)value;
+
+				Properties properties = new Properties();
+
+				for (Map.Entry<Object, Object> propEntry :
+						managedProperties.entrySet()) {
+
+					Object propKey = propEntry.getKey();
+					Object propValue = propEntry.getValue();
+
+					if (propKey instanceof TypedStringValue) {
+						TypedStringValue typedStringValue =
+							(TypedStringValue)propKey;
+
+						propKey = typedStringValue.getValue();
+					}
+
+					if (propValue instanceof TypedStringValue) {
+						TypedStringValue typedStringValue =
+							(TypedStringValue)propValue;
+
+						propValue = typedStringValue.getValue();
+					}
+
+					properties.put(propKey, propValue);
+				}
+
+				value = properties;
+			}
+			else if (value instanceof TypedStringValue) {
+				TypedStringValue typedStringValue = (TypedStringValue)value;
+
+				value = typedStringValue.getValue();
+			}
+
+			return value;
+		}
+
+		private static final Map<String, Method> _methods =
+			new ConcurrentHashMap<>();
+
+	}
+
 	private class ParentModuleApplicationContextExtension implements Extension {
 
 		@Override
@@ -113,26 +414,8 @@ public class ParentModuleApplicationContextExtender extends AbstractExtender {
 
 			Bundle extenderBundle = extenderBundleContext.getBundle();
 
-			Bundle compositeResourceLoaderBundle =
-				new CompositeResourceLoaderBundle(_bundle, extenderBundle);
-
-			ClassLoader classLoader = new BundleResolverClassLoader(
-				_bundle, extenderBundle);
-
 			ModuleApplicationContext moduleApplicationContext =
-				new ModuleApplicationContext(
-					compositeResourceLoaderBundle, classLoader,
-					_PARENT_CONFIG_LOCATIONS);
-
-			moduleApplicationContext.addBeanFactoryPostProcessor(
-				new PortletBeanFactoryPostProcessor() {
-
-					@Override
-					protected ClassLoader getClassLoader() {
-						return classLoader;
-					}
-
-				});
+				new ParentModuleApplicationContext(_bundle, extenderBundle);
 
 			moduleApplicationContext.refresh();
 
