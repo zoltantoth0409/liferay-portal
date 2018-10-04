0080576c978bb23b1e7052d8431521acd0c4f4ba
pavel-savinov <pavel.savinov@liferay.com>
2018-10-04 13:17:47 +0200

    LPS-85783 Override get methods since there could be developers who are using them

--- modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/service/AssetEntryAssetCategoryRelAssetCategoryLocalServiceWrapper.java
+++ modules/apps/asset/asset-categories-service/src/main/java/com/liferay/asset/categories/internal/service/AssetEntryAssetCategoryRelAssetCategoryLocalServiceWrapper.java
@@ -142,6 +142,17 @@ public class AssetEntryAssetCategoryRelAssetCategoryLocalServiceWrapper
 	}
 
 	@Override
+	public List<AssetCategory> getAssetEntryAssetCategories(long entryId) {
+		return _getAssetCategoriesByEntryId(entryId);
+	}
+
+	@Override
+	public int getAssetEntryAssetCategoriesCount(long entryId) {
+		return _assetEntryAssetCategoryRelLocalService.
+			getAssetEntryAssetCategoryRelsCount(entryId);
+	}
+
+	@Override
 	public List<AssetCategory> getCategories(long classNameId, long classPK) {
 		AssetEntry entry = _assetEntryLocalService.fetchEntry(
 			classNameId, classPK);
