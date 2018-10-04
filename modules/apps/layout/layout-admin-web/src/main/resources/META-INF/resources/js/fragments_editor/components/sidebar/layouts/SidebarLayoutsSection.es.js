e5bc71f7ecc461e67816adb6c17887dadb42f9a2
Pablo Molina <contact@pablomolina.me>
2018-10-04 18:04:40 +0200

    LPS-86004 Include sample layout previews on sidebar

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/layouts/SidebarLayoutsSection.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/layouts/SidebarLayoutsSection.es.js
@@ -1,4 +1,5 @@
 import Component from 'metal-component';
+import {Config} from 'metal-state';
 import Soy from 'metal-soy';
 
 import templates from './SidebarLayoutsSection.soy';
@@ -16,7 +17,31 @@ class SidebarLayoutsSection extends Component {}
  * @type {!Object}
  */
 
-SidebarLayoutsSection.STATE = {};
+SidebarLayoutsSection.STATE = {
+
+	/**
+	 * List of layouts to be shown
+	 * @default []
+	 * @memberOf SidebarLayoutsSection
+	 * @private
+	 * @review
+	 * @type {Array}
+	 */
+
+	_layouts: Config.arrayOf(
+		Config.shapeOf(
+			{
+				columns: Config.arrayOf(Config.string())
+			}
+		)
+	).value(
+		[
+			{columns: ['3', '6', '3']},
+			{columns: ['6', '6']},
+			{columns: ['4', '8']}
+		]
+	)
+};
 
 Soy.register(SidebarLayoutsSection, templates);
 
