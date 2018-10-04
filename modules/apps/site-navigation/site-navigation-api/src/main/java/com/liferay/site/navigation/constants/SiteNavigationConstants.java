73b77fc1d7abde600fa9c8e56c9508a3e0b5870e
Brian Chan <brian.chan@liferay.com>
2018-10-05 01:05:10 +0200

    LPS-80611 More accurate

--- modules/apps/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/constants/SiteNavigationConstants.java
+++ modules/apps/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/constants/SiteNavigationConstants.java
@@ -28,7 +28,7 @@ public class SiteNavigationConstants {
 	public static final int TYPE_PRIMARY = 1;
 
 	/**
-	 * @deprecated As of Wilberforce (7.0.x), with no direct replacement
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
 	 */
 	@Deprecated
 	public static final int TYPE_PRIVATE = 4;
