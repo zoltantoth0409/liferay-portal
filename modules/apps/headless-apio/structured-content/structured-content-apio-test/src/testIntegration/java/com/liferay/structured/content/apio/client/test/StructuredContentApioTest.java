dc2fcb4b5aac6fa62eac4065a2d8de2aa7828172
rubenpulido <ruben.pulido@liferay.com>
2018-10-04 13:21:49 +0200

    LPS-85735 Create site for tests using BundleActivator

--- modules/apps/headless-apio/structured-content/structured-content-apio-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTest.java
@@ -16,10 +16,13 @@ package com.liferay.structured.content.apio.client.test;
 
 import com.jayway.jsonpath.JsonPath;
 
+import com.liferay.oauth2.provider.test.util.OAuth2ProviderTestUtil;
 import com.liferay.petra.json.web.service.client.JSONWebServiceClient;
 import com.liferay.petra.json.web.service.client.JSONWebServiceInvocationException;
 import com.liferay.petra.json.web.service.client.JSONWebServiceTransportException;
 import com.liferay.petra.json.web.service.client.internal.JSONWebServiceClientImpl;
+
+import java.io.File;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,9 +30,11 @@ import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -42,6 +47,18 @@ import org.junit.runner.RunWith;
 @RunAsClient
 @RunWith(Arquillian.class)
 public class StructuredContentApioTest {
+
+	@Deployment
+	public static Archive<?> getDeployment() throws Exception {
+		ClassLoader classLoader =
+			StructuredContentApioTest.class.getClassLoader();
+
+		URL url = classLoader.getResource("bnd.bnd");
+
+		return OAuth2ProviderTestUtil.getArchive(
+			StructuredContentApioTestBundleActivator.class,
+			new File(url.toURI()));
+	}
 
 	@Before
 	public void setUp() throws MalformedURLException {
@@ -66,8 +83,9 @@ public class StructuredContentApioTest {
 				JsonPath.read(
 					_get(_rootEndpointURL.toExternalForm()),
 					"$._links.content-space.href")),
-			"$._embedded.ContentSpace[?(@.name == 'Liferay')]._links." +
-				"structuredContents.href");
+			"$._embedded.ContentSpace[?(@.name == '" +
+				StructuredContentApioTestBundleActivator.SITE_NAME +
+					"')]._links.structuredContents.href");
 
 		Assert.assertNotNull(hrefs.get(0));
 	}
@@ -79,8 +97,9 @@ public class StructuredContentApioTest {
 				JsonPath.read(
 					_get(_rootEndpointURL.toExternalForm()),
 					"$._links.content-space.href")),
-			"$._embedded.ContentSpace[?(@.name == 'Liferay')]._links." +
-				"structuredContents.href");
+			"$._embedded.ContentSpace[?(@.name == '" +
+				StructuredContentApioTestBundleActivator.SITE_NAME +
+					"')]._links.structuredContents.href");
 
 		String href = JsonPath.read(_get(hrefs.get(0)), "$._links.self.href");
 
---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-test/src/testIntegration/java/com/liferay/structured/content/apio/client/test/StructuredContentApioTestBundleActivator.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.client.test;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Ruben Pulido
+ */
+public class StructuredContentApioTestBundleActivator
+	implements BundleActivator {
+
+	public static final String SITE_NAME =
+		StructuredContentApioTestBundleActivator.class.getSimpleName() + "Site";
+
+	@Override
+	public void start(BundleContext bundleContext) {
+		_autoCloseables = new ArrayList<>();
+
+		try {
+			_prepareTest();
+		}
+		catch (Exception e) {
+			_cleanUp();
+
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) {
+		_cleanUp();
+	}
+
+	private void _cleanUp() {
+		ListIterator<AutoCloseable> listIterator = _autoCloseables.listIterator(
+			_autoCloseables.size());
+
+		while (listIterator.hasPrevious()) {
+			AutoCloseable previousAutoCloseable = listIterator.previous();
+
+			try {
+				previousAutoCloseable.close();
+			}
+			catch (Exception e) {
+				_log.error(e, e);
+			}
+		}
+	}
+
+	private void _prepareTest() throws Exception {
+		User user = UserTestUtil.getAdminUser(TestPropsValues.getCompanyId());
+
+		Map<Locale, String> nameMap = new HashMap<Locale, String>() {
+			{
+				put(LocaleUtil.getDefault(), SITE_NAME);
+			}
+		};
+
+		Group group = GroupLocalServiceUtil.addGroup(
+			user.getUserId(), GroupConstants.DEFAULT_PARENT_GROUP_ID, null, 0,
+			GroupConstants.DEFAULT_LIVE_GROUP_ID, nameMap, nameMap,
+			GroupConstants.TYPE_SITE_OPEN, true,
+			GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION,
+			StringPool.SLASH + FriendlyURLNormalizerUtil.normalize(SITE_NAME),
+			true, true, ServiceContextTestUtil.getServiceContext());
+
+		_autoCloseables.add(() -> GroupLocalServiceUtil.deleteGroup(group));
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		StructuredContentApioTestBundleActivator.class);
+
+	private ArrayList<AutoCloseable> _autoCloseables;
+
+}
