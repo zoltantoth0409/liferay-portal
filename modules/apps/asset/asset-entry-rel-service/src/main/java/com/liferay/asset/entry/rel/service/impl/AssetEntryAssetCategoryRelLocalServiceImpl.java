4094d700ce472b700d2eec9c6066a0cbc3168a4b
pavel-savinov <pavel.savinov@liferay.com>
2018-10-04 13:10:21 +0200

    LPS-85783 Implement helper methods to override "has" methods in the categories service wrappers

--- modules/apps/asset/asset-entry-rel-service/src/main/java/com/liferay/asset/entry/rel/service/impl/AssetEntryAssetCategoryRelLocalServiceImpl.java
+++ modules/apps/asset/asset-entry-rel-service/src/main/java/com/liferay/asset/entry/rel/service/impl/AssetEntryAssetCategoryRelLocalServiceImpl.java
@@ -84,6 +84,14 @@ public class AssetEntryAssetCategoryRelLocalServiceImpl
 	}
 
 	@Override
+	public AssetEntryAssetCategoryRel fetchAssetEntryAssetCategoryRel(
+		long assetEntryId, long assetCategoryId) {
+
+		return assetEntryAssetCategoryRelPersistence.fetchByA_A(
+			assetEntryId, assetCategoryId);
+	}
+
+	@Override
 	public List<AssetEntryAssetCategoryRel>
 		getAssetEntryAssetCategoryRelsByAssetCategoryId(long assetCategoryId) {
 
@@ -96,6 +104,12 @@ public class AssetEntryAssetCategoryRelLocalServiceImpl
 		getAssetEntryAssetCategoryRelsByAssetEntryId(long assetEntryId) {
 
 		return assetEntryAssetCategoryRelPersistence.findByAssetEntryId(
+			assetEntryId);
+	}
+
+	@Override
+	public int getAssetEntryAssetCategoryRelsCount(long assetEntryId) {
+		return assetEntryAssetCategoryRelPersistence.countByAssetEntryId(
 			assetEntryId);
 	}
 
