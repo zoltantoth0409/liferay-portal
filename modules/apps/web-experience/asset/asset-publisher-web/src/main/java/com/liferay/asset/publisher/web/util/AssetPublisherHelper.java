c9a90c266666a7425d331d9d7ad01c57690cdd05
balazssk <balazs.safrany-kovalik@liferay.com>
2018-10-05 16:05:32 +0200

    LPS-85945 Replace portletId in viewUrl if Asset Publisher Urls are linked to another Page

--- modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherHelper.java
+++ modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherHelper.java
@@ -17,17 +17,25 @@ package com.liferay.asset.publisher.web.util;
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.model.AssetRenderer;
 import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutTypePortletConstants;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.Objects;
 
+import javax.portlet.PortletPreferences;
 import javax.portlet.PortletURL;
 
 /**
@@ -135,7 +143,67 @@ public class AssetPublisherHelper {
 			viewURL = viewFullContentURL.toString();
 		}
 
+		viewURL = _replacePortletIdIfLinkedToAnotherLayout(
+			liferayPortletRequest, viewURL);
+
 		return viewURL;
+	}
+
+	private static String _replacePortletIdIfLinkedToAnotherLayout(
+		LiferayPortletRequest liferayPortletRequest, String viewUrl) {
+
+		Layout layout = LayoutLocalServiceUtil.fetchLayout(
+			liferayPortletRequest.getPlid());
+
+		PortletPreferences portletPreferences =
+			liferayPortletRequest.getPreferences();
+
+		String portletSetupLinkToLayoutUuid = portletPreferences.getValue(
+			"portletSetupLinkToLayoutUuid", StringPool.BLANK);
+
+		if ((layout != null) &&
+			Validator.isNotNull(portletSetupLinkToLayoutUuid)) {
+
+			Layout linkedLayout =
+				LayoutLocalServiceUtil.fetchLayoutByUuidAndGroupId(
+					portletSetupLinkToLayoutUuid, layout.getGroupId(),
+					layout.isPrivateLayout());
+
+			String newPortletId = linkedLayout.getTypeSettingsProperty(
+				LayoutTypePortletConstants.DEFAULT_ASSET_PUBLISHER_PORTLET_ID,
+				StringPool.BLANK);
+
+			if (Validator.isNotNull(newPortletId)) {
+				String oldPortletName = liferayPortletRequest.getPortletName();
+
+				viewUrl = StringUtil.replace(
+					viewUrl, oldPortletName, newPortletId);
+
+				String newId = newPortletId.split("_INSTANCE_")[1];
+				String oldId = oldPortletName.split("_INSTANCE_")[1];
+
+				viewUrl = StringUtil.replace(
+					viewUrl, StringPool.SLASH + oldId + StringPool.SLASH,
+					StringPool.SLASH + newId + StringPool.SLASH);
+
+				Portlet oldPortlet = PortletLocalServiceUtil.getPortletById(
+					oldPortletName);
+
+				String oldPortletMapping = oldPortlet.getFriendlyURLMapping();
+
+				Portlet newPortlet = PortletLocalServiceUtil.getPortletById(
+					newPortletId);
+
+				String newPortletMapping = newPortlet.getFriendlyURLMapping();
+
+				viewUrl = StringUtil.replace(
+					viewUrl,
+					StringPool.SLASH + oldPortletMapping + StringPool.SLASH,
+					StringPool.SLASH + newPortletMapping + StringPool.SLASH);
+			}
+		}
+
+		return viewUrl;
 	}
 
 }
