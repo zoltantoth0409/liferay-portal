59873dc63c96d31686e949fb98eb48dbd9974bbc
Julien Castelain <julien.castelain@liferay.com>
2018-10-05 12:29:07 +0200

    LPS-83825 Add Liferay.Util.isEqual function

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
@@ -1,5 +1,6 @@
 import escape from './util/escape.es';
 import groupBy from './util/group_by.es';
+import isEqual from './util/is_equal.es';
 import portlet from './portlet/portlet.es';
 import navigate from './util/navigate.es';
 import objectToFormData from './util/object_to_form_data.es';
@@ -7,6 +8,7 @@ import unescape from './util/unescape.es';
 
 Liferay.Util.escape = escape;
 Liferay.Util.groupBy = groupBy;
+Liferay.Util.isEqual = isEqual;
 Liferay.Util.navigate = navigate;
 Liferay.Util.objectToFormData = objectToFormData;
 Liferay.Util.openToast = (...args) => {
