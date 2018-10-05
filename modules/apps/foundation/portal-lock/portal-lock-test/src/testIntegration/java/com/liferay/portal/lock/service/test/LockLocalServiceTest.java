027bedc6b413130a944293f157ec0e648e8c09f1
Matthew Tambara <matthew.tambara@liferay.com>
2018-10-05 10:32:50 -0700

    LRQA-41120 Futureproof expected log

--- modules/apps/foundation/portal-lock/portal-lock-test/src/testIntegration/java/com/liferay/portal/lock/service/test/LockLocalServiceTest.java
+++ modules/apps/foundation/portal-lock/portal-lock-test/src/testIntegration/java/com/liferay/portal/lock/service/test/LockLocalServiceTest.java
@@ -71,8 +71,8 @@ public class LockLocalServiceTest {
 			),
 			@ExpectedLog(
 				expectedDBType = ExpectedDBType.DB2,
-				expectedLog = "[jcc][t4][102][10040][4.16.53] Batch failure.",
-				expectedType = ExpectedType.PREFIX
+				expectedLog = "Batch failure.",
+				expectedType = ExpectedType.CONTAINS
 			),
 			@ExpectedLog(
 				expectedDBType = ExpectedDBType.HYPERSONIC,
