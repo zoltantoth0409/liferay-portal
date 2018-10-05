4b0c9837390812bf9e9a49da6b467cc7c93822c8
Brian Chan <brian.chan@liferay.com>
2018-10-05 13:41:43 -0700

    Revert "LPS-85726 As used"
    This reverts commit fbb6ae416eb7beca11f8fed575a8123922e779be.

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/lar/PortletDataContextImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/lar/PortletDataContextImpl.java
@@ -3090,12 +3090,6 @@ public class PortletDataContextImpl implements PortletDataContext {
 						newPrimaryKey))) {
 
 				try {
-					PermissionChecker permissionChecker =
-						PermissionThreadLocal.getPermissionChecker();
-
-					long importedClassPK = GetterUtil.getLong(
-						classedModel.getPrimaryKeyObj());
-
 					String referrerUuid =
 						stagedGroupedWorkflowDefinitionLinkElement.
 							attributeValue("uuid");
@@ -3114,6 +3108,12 @@ public class PortletDataContextImpl implements PortletDataContext {
 
 						typePK = ddmStructureIds.getOrDefault(typePK, typePK);
 					}
+
+					long importedClassPK = GetterUtil.getLong(
+						classedModel.getPrimaryKeyObj());
+
+					PermissionChecker permissionChecker =
+						PermissionThreadLocal.getPermissionChecker();
 
 					WorkflowDefinitionLinkLocalServiceUtil.
 						addWorkflowDefinitionLink(
