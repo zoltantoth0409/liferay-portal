ad06e01c466da25792801275fbd38087a6c9295d
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-10-05 13:19:51 +0200

    LPS-85928 Remove asset lists for a group upon deletion

---
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/model/listener/GroupModelListener.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.internal.model.listener;
+
+import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.asset.list.service.AssetListEntryLocalService;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class GroupModelListener extends BaseModelListener<Group> {
+
+	@Override
+	public void onBeforeRemove(Group group) throws ModelListenerException {
+		try {
+			List<AssetListEntry> assetListEntries =
+				_assetListEntryLocalService.getAssetListEntries(
+					group.getGroupId());
+
+			for (AssetListEntry assetListEntry : assetListEntries) {
+				_assetListEntryLocalService.deleteAssetListEntry(
+					assetListEntry);
+			}
+		}
+		catch (Exception e) {
+			throw new ModelListenerException(e);
+		}
+	}
+
+	@Reference
+	private AssetListEntryLocalService _assetListEntryLocalService;
+
+}
