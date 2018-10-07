1905906662473139e235b8533eac8daabd443e54
André de Oliveira <andre.oliveira@liferay.com>
2018-10-06 19:05:55 -0700

    LPS-84665 portal-search-elasticsearch6-impl: Tests: `assetTagNames*` fields always language aware now, so test them as such
    - Word stemming makes recall wider
    - Stopwords wholly out of tokenization
    
    https://issues.liferay.com/browse/LPS-84665
    https://issues.liferay.com/browse/LPS-84666

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/mappings/AssetTagNamesFieldQueryBuilderTest.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/mappings/AssetTagNamesFieldQueryBuilderTest.java
@@ -21,6 +21,9 @@ import com.liferay.portal.search.test.util.indexing.BaseIndexingTestCase;
 import com.liferay.portal.search.test.util.indexing.IndexingFixture;
 import com.liferay.portal.search.test.util.mappings.BaseAssetTagNamesFieldQueryBuilderTestCase;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 /**
@@ -33,35 +36,50 @@ public class AssetTagNamesFieldQueryBuilderTest
 	public void testMultiwordPhrasePrefixesElasticsearch() throws Exception {
 		addDocument("Name Tags");
 		addDocument("Names Tab");
-		addDocument("Tag Names");
 		addDocument("Tabs Names Tags");
+		addDocument("Tag Names");
 
-		assertSearch("\"name ta*\"", 1);
-		assertSearch("\"name tag*\"", 1);
-		assertSearch("\"name tags*\"", 1);
-		assertSearch("\"names ta*\"", 2);
-		assertSearch("\"names tab*\"", 1);
-		assertSearch("\"names tag*\"", 1);
-		assertSearch("\"names tags*\"", 1);
-		assertSearch("\"tabs name*\"", 1);
-		assertSearch("\"tabs names ta*\"", 1);
-		assertSearch("\"tabs names tag*\"", 1);
-		assertSearch("\"tabs names tags*\"", 1);
-		assertSearch("\"tabs names*\"", 1);
-		assertSearch("\"tag na*\"", 1);
-		assertSearch("\"tag name*\"", 1);
-		assertSearch("\"tag names*\"", 1);
+		List<String> results1 = Arrays.asList(
+			"Name Tags", "Names Tab", "Tabs Names Tags");
 
-		assertSearchNoHits("\"name tab*\"");
-		assertSearchNoHits("\"name tabs*\"");
-		assertSearchNoHits("\"names tabs*\"");
-		assertSearchNoHits("\"tab na*\"");
-		assertSearchNoHits("\"tab names*\"");
+		assertSearch("\"name ta*\"", results1);
+		assertSearch("\"names ta*\"", results1);
+
+		List<String> results2 = Arrays.asList("Name Tags", "Tabs Names Tags");
+
+		assertSearch("\"name tag*\"", results2);
+		assertSearch("\"name tags*\"", results2);
+		assertSearch("\"names tag*\"", results2);
+		assertSearch("\"names tags*\"", results2);
+
+		List<String> results3 = Arrays.asList("Names Tab");
+
+		assertSearch("\"name tab*\"", results3);
+		assertSearch("\"name tabs*\"", results3);
+		assertSearch("\"names tab*\"", results3);
+		assertSearch("\"names tabs*\"", results3);
+
+		List<String> results4 = Arrays.asList("Tabs Names Tags");
+
+		assertSearch("\"tab na*\"", results4);
+		assertSearch("\"tab names*\"", results4);
+		assertSearch("\"tabs name*\"", results4);
+		assertSearch("\"tabs names ta*\"", results4);
+		assertSearch("\"tabs names tag*\"", results4);
+		assertSearch("\"tabs names tags*\"", results4);
+		assertSearch("\"tabs names*\"", results4);
+		assertSearch("\"tabs name ta*\"", results4);
+
+		List<String> results5 = Arrays.asList("Tag Names");
+
+		assertSearch("\"tag na*\"", results5);
+		assertSearch("\"tag name*\"", results5);
+		assertSearch("\"tag names*\"", results5);
+		assertSearch("\"tags names*\"", results5);
+
 		assertSearchNoHits("\"tabs na ta*\"");
-		assertSearchNoHits("\"tabs name ta*\"");
 		assertSearchNoHits("\"tags na ta*\"");
 		assertSearchNoHits("\"tags names tabs*\"");
-		assertSearchNoHits("\"tags names*\"");
 		assertSearchNoHits("\"zz na*\"");
 		assertSearchNoHits("\"zz name*\"");
 		assertSearchNoHits("\"zz names*\"");
