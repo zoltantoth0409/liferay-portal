d8f938294e2864021853ed7b4c7e613e544139e0
pavel-savinov <pavel.savinov@liferay.com>
2018-10-08 10:14:58 +0200

    LPS-86093 Use languageId from the request if present and themedisplay value by default

--- modules/apps/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
+++ modules/apps/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
@@ -199,6 +199,8 @@ public class JournalContentDisplayContext {
 
 		String viewMode = ParamUtil.getString(
 			_portletRequest, "viewMode", null);
+		String languageId = ParamUtil.getString(
+			_portletRequest, "languageId", themeDisplay.getLanguageId());
 		int page = ParamUtil.getInteger(_portletRequest, "page", 1);
 
 		if (article.isApproved()) {
@@ -212,8 +214,8 @@ public class JournalContentDisplayContext {
 
 			_articleDisplay = journalContent.getDisplay(
 				article.getGroupId(), article.getArticleId(),
-				article.getVersion(), getDDMTemplateKey(), viewMode,
-				themeDisplay.getLanguageId(), page,
+				article.getVersion(), getDDMTemplateKey(), viewMode, languageId,
+				page,
 				new PortletRequestModel(_portletRequest, _portletResponse),
 				themeDisplay);
 		}
@@ -221,8 +223,8 @@ public class JournalContentDisplayContext {
 			try {
 				_articleDisplay =
 					JournalArticleLocalServiceUtil.getArticleDisplay(
-						article, getDDMTemplateKey(), viewMode,
-						themeDisplay.getLanguageId(), page,
+						article, getDDMTemplateKey(), viewMode, languageId,
+						page,
 						new PortletRequestModel(
 							_portletRequest, _portletResponse),
 						themeDisplay);
