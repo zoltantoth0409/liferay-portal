fe1413146b5f196bb43d0ebe5809c118906c17fa
David Arques <davidarchena@gmail.com>
2018-10-09 18:59:30 +0200

    LPS-85743 Add SegmentsEntryServiceTest

---
+++ modules/apps/segments/segments-test/src/testIntegration/java/com/liferay/segments/service/test/SegmentsEntryServiceTest.java
@@ -0,0 +1,430 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.context.ContextUserReplace;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.service.test.ServiceTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+import com.liferay.segments.constants.SegmentsActionKeys;
+import com.liferay.segments.model.SegmentsEntry;
+import com.liferay.segments.service.SegmentsEntryService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author David Arques
+ */
+@RunWith(Arquillian.class)
+public class SegmentsEntryServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_company = CompanyTestUtil.addCompany();
+
+		_companyAdminUser = UserTestUtil.addCompanyAdminUser(_company);
+
+		_group = GroupTestUtil.addGroup(
+			_company.getCompanyId(), _companyAdminUser.getUserId(),
+			GroupConstants.DEFAULT_PARENT_GROUP_ID);
+
+		_groupUser = UserTestUtil.addGroupUser(
+			_group, RoleConstants.POWER_USER);
+
+		ServiceTestUtil.setUser(_companyAdminUser);
+	}
+
+	@Test
+	public void testAddSegmentsEntryWithManageSegmentsEntriesPermission()
+		throws Exception {
+
+		Role siteMemberRole = RoleLocalServiceUtil.getRole(
+			_company.getCompanyId(), RoleConstants.SITE_MEMBER);
+
+		ResourcePermissionLocalServiceUtil.addResourcePermission(
+			_company.getCompanyId(), "com.liferay.segments",
+			ResourceConstants.SCOPE_GROUP, String.valueOf(_group.getGroupId()),
+			siteMemberRole.getRoleId(),
+			SegmentsActionKeys.MANAGE_SEGMENTS_ENTRIES);
+
+		PermissionChecker permissionChecker =
+			PermissionCheckerFactoryUtil.create(_groupUser);
+
+		try (ContextUserReplace contextUserReplace =
+				new ContextUserReplace(_groupUser, permissionChecker)) {
+
+			_addSegmentsEntry(
+				ServiceContextTestUtil.getServiceContext(
+					_group, _groupUser.getUserId()));
+		}
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testAddSegmentsEntryWithoutManageSegmentsEntriesPermission()
+		throws Exception {
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			_addSegmentsEntry(
+				ServiceContextTestUtil.getServiceContext(
+					_group, _groupUser.getUserId()));
+		}
+	}
+
+	@Test
+	public void testDeleteSegmentsEntryWithDeletePermission() throws Exception {
+		SegmentsEntry segmentsEntry = _addSegmentsEntry(
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId()));
+
+		Role siteMemberRole = RoleLocalServiceUtil.getRole(
+			_company.getCompanyId(), RoleConstants.SITE_MEMBER);
+
+		ResourcePermissionLocalServiceUtil.addResourcePermission(
+			_group.getCompanyId(), "com.liferay.segments.model.SegmentsEntry",
+			ResourceConstants.SCOPE_GROUP, String.valueOf(_group.getGroupId()),
+			siteMemberRole.getRoleId(), ActionKeys.DELETE);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			_segmentsEntryService.deleteSegmentsEntry(
+				segmentsEntry.getSegmentsEntryId());
+		}
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testDeleteSegmentsEntryWithoutDeletePermission()
+		throws Exception {
+
+		SegmentsEntry segmentsEntry = _addSegmentsEntry(
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId()));
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			_segmentsEntryService.deleteSegmentsEntry(
+				segmentsEntry.getSegmentsEntryId());
+		}
+	}
+
+	@Test
+	public void testGetSegmentsEntriesCountWithoutViewPermission()
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId());
+
+		SegmentsEntry segmentsEntry = _addSegmentsEntry(serviceContext);
+		_addSegmentsEntry(serviceContext);
+		_addSegmentsEntry(serviceContext);
+
+		List<Role> roles = RoleLocalServiceUtil.getRoles(
+			_company.getCompanyId());
+
+		for (Role role : roles) {
+			if (RoleConstants.OWNER.equals(role.getName())) {
+				continue;
+			}
+
+			ResourcePermissionLocalServiceUtil.removeResourcePermission(
+				_company.getCompanyId(),
+				"com.liferay.segments.model.SegmentsEntry",
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(segmentsEntry.getSegmentsEntryId()),
+				role.getRoleId(), ActionKeys.VIEW);
+		}
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			Assert.assertEquals(
+				2,
+				_segmentsEntryService.getSegmentsEntriesCount(
+					_group.getGroupId()));
+		}
+	}
+
+	@Test
+	public void testGetSegmentsEntriesCountWithViewPermission()
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId());
+
+		_addSegmentsEntry(serviceContext);
+		_addSegmentsEntry(serviceContext);
+		_addSegmentsEntry(serviceContext);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			Assert.assertEquals(
+				3,
+				_segmentsEntryService.getSegmentsEntriesCount(
+					_group.getGroupId()));
+		}
+	}
+
+	@Test
+	public void testGetSegmentsEntriesWithoutViewPermission() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId());
+
+		SegmentsEntry segmentsEntry1 = _addSegmentsEntry(serviceContext);
+
+		SegmentsEntry segmentsEntry2 = _addSegmentsEntry(serviceContext);
+
+		SegmentsEntry segmentsEntry3 = _addSegmentsEntry(serviceContext);
+
+		for (Role role :
+				RoleLocalServiceUtil.getRoles(_company.getCompanyId())) {
+
+			if (RoleConstants.OWNER.equals(role.getName())) {
+				continue;
+			}
+
+			ResourcePermissionLocalServiceUtil.removeResourcePermission(
+				_company.getCompanyId(),
+				"com.liferay.segments.model.SegmentsEntry",
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(segmentsEntry2.getSegmentsEntryId()),
+				role.getRoleId(), ActionKeys.VIEW);
+		}
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			List<SegmentsEntry> segmentsEntries =
+				_segmentsEntryService.getSegmentsEntries(
+					_group.getGroupId(), 0, 100, null);
+
+			Assert.assertEquals(
+				segmentsEntries.toString(), 2, segmentsEntries.size());
+
+			Assert.assertTrue(segmentsEntries.contains(segmentsEntry3));
+			Assert.assertTrue(segmentsEntries.contains(segmentsEntry1));
+		}
+	}
+
+	@Test
+	public void testGetSegmentsEntriesWithViewPermission() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId());
+
+		SegmentsEntry segmentsEntry1 = _addSegmentsEntry(serviceContext);
+
+		SegmentsEntry segmentsEntry2 = _addSegmentsEntry(serviceContext);
+
+		SegmentsEntry segmentsEntry3 = _addSegmentsEntry(serviceContext);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			List<SegmentsEntry> segmentsEntries =
+				_segmentsEntryService.getSegmentsEntries(
+					_group.getGroupId(), 0, 100, null);
+
+			Assert.assertEquals(
+				segmentsEntries.toString(), 3, segmentsEntries.size());
+
+			Assert.assertTrue(segmentsEntries.contains(segmentsEntry3));
+			Assert.assertTrue(segmentsEntries.contains(segmentsEntry2));
+			Assert.assertTrue(segmentsEntries.contains(segmentsEntry1));
+		}
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testGetSegmentsEntryWithoutViewPermission() throws Exception {
+		SegmentsEntry segmentsEntry = _addSegmentsEntry(
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId()));
+
+		List<Role> roles = RoleLocalServiceUtil.getRoles(
+			_company.getCompanyId());
+
+		for (Role role : roles) {
+			if (RoleConstants.OWNER.equals(role.getName())) {
+				continue;
+			}
+
+			ResourcePermissionLocalServiceUtil.removeResourcePermission(
+				_company.getCompanyId(),
+				"com.liferay.segments.model.SegmentsEntry",
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(segmentsEntry.getSegmentsEntryId()),
+				role.getRoleId(), ActionKeys.VIEW);
+		}
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			_segmentsEntryService.getSegmentsEntry(
+				segmentsEntry.getSegmentsEntryId());
+		}
+	}
+
+	@Test
+	public void testGetSegmentsEntryWithViewPermission() throws Exception {
+		SegmentsEntry segmentsEntry = _addSegmentsEntry(
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId()));
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			_segmentsEntryService.getSegmentsEntry(
+				segmentsEntry.getSegmentsEntryId());
+		}
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testUpdateSegmentsEntryWithoutUpdatePermission()
+		throws Exception {
+
+		SegmentsEntry segmentsEntry = _addSegmentsEntry(
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId()));
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			_updateSegmentsEntry(
+				segmentsEntry.getSegmentsEntryId(),
+				ServiceContextTestUtil.getServiceContext(
+					_group, _groupUser.getUserId()));
+		}
+	}
+
+	@Test
+	public void testUpdateSegmentsEntryWithUpdatePermission() throws Exception {
+		SegmentsEntry entry = _addSegmentsEntry(
+			ServiceContextTestUtil.getServiceContext(
+				_group, _companyAdminUser.getUserId()));
+
+		Role siteMemberRole = RoleLocalServiceUtil.getRole(
+			_company.getCompanyId(), RoleConstants.SITE_MEMBER);
+
+		ResourcePermissionLocalServiceUtil.addResourcePermission(
+			_group.getCompanyId(), "com.liferay.segments.model.SegmentsEntry",
+			ResourceConstants.SCOPE_GROUP, String.valueOf(_group.getGroupId()),
+			siteMemberRole.getRoleId(), ActionKeys.UPDATE);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_groupUser, PermissionCheckerFactoryUtil.create(_groupUser))) {
+
+			_updateSegmentsEntry(
+				entry.getSegmentsEntryId(),
+				ServiceContextTestUtil.getServiceContext(
+					_group, _groupUser.getUserId()));
+		}
+	}
+
+	private SegmentsEntry _addSegmentsEntry(ServiceContext serviceContext)
+		throws PortalException {
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.getDefault(), RandomTestUtil.randomString());
+
+		Map<Locale, String> descriptionMap = new HashMap<>();
+
+		descriptionMap.put(
+			LocaleUtil.getDefault(), RandomTestUtil.randomString());
+
+		return _segmentsEntryService.addSegmentsEntry(
+			nameMap, descriptionMap, true, RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			serviceContext);
+	}
+
+	private SegmentsEntry _updateSegmentsEntry(
+			long segmentsEntryId, ServiceContext serviceContext)
+		throws PortalException {
+
+		Map<Locale, String> nameMap = new HashMap<>();
+
+		nameMap.put(LocaleUtil.getDefault(), RandomTestUtil.randomString());
+
+		Map<Locale, String> descriptionMap = new HashMap<>();
+
+		descriptionMap.put(
+			LocaleUtil.getDefault(), RandomTestUtil.randomString());
+
+		return _segmentsEntryService.updateSegmentsEntry(
+			segmentsEntryId, nameMap, descriptionMap, true,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			serviceContext);
+	}
+
+	@DeleteAfterTestRun
+	private Company _company;
+
+	private User _companyAdminUser;
+	private Group _group;
+	private User _groupUser;
+
+	@Inject
+	private SegmentsEntryService _segmentsEntryService;
+
+}
