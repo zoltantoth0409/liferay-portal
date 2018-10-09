18a04e31cec627cc44a32de4ea98807cfe02bbb6
Julien Castelain <julien.castelain@liferay.com>
2018-10-09 12:51:01 +0200

    LPS-83825 Imports from lodash directly

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
@@ -1,14 +1,26 @@
-import escape from './util/escape.es';
-import groupBy from './util/group_by.es';
+import escape from 'lodash.escape';
+import groupBy from 'lodash.groupby';
+import isEqual from 'lodash.isequal';
 import portlet from './portlet/portlet.es';
 import navigate from './util/navigate.es';
 import objectToFormData from './util/object_to_form_data.es';
-import unescape from './util/unescape.es';
+import unescape from 'lodash.unescape';
 
 Liferay.Util.escape = escape;
 Liferay.Util.groupBy = groupBy;
+Liferay.Util.isEqual = isEqual;
 Liferay.Util.navigate = navigate;
 Liferay.Util.objectToFormData = objectToFormData;
+
+Liferay.Util.openToast = (...args) => {
+	Liferay.Loader.require(
+		'frontend-js-web/liferay/toast/commands/OpenToast.es',
+		commands => {
+			commands.openToast(...args);
+		}
+	);
+};
+
 Liferay.Util.unescape = unescape;
 
 export {portlet};
