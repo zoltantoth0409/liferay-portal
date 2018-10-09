171dcc73d50f7c8ab0090275e6f227838cd4a706
jpince <jason.pince@liferay.com>
2018-10-09 10:31:37 -0700

    LRQA-43984 Temporarily Ignore Test

--- modules/apps/staging/staging-test/src/testIntegration/java/com/liferay/staging/test/StagingGroupHelperTest.java
+++ modules/apps/staging/staging-test/src/testIntegration/java/com/liferay/staging/test/StagingGroupHelperTest.java
@@ -49,6 +49,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -56,6 +57,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Akos Thurzo
  */
+@Ignore
 @RunWith(Arquillian.class)
 @Sync(cleanTransaction = true)
 public class StagingGroupHelperTest {
