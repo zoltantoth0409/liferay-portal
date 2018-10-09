a3def0d32a94476caf255d2c7fb29c3bbe596b15
jpince <jason.pince@liferay.com>
2018-10-09 10:35:59 -0700

    LRQA-43984 Temporarily Ignore Tests

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/system/event/test/SystemEventTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/system/event/test/SystemEventTest.java
@@ -57,6 +57,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -243,6 +244,7 @@ public class SystemEventTest {
 		Assert.assertNull(systemEvent);
 	}
 
+	@Ignore
 	@Test
 	public void testRemoteStaging1() throws Exception {
 		setPortalProperty("STAGING_LIVE_GROUP_REMOTE_STAGING_ENABLED", false);
@@ -257,6 +259,7 @@ public class SystemEventTest {
 		Assert.assertNull(systemEvent);
 	}
 
+	@Ignore
 	@Test
 	public void testRemoteStaging2() throws Exception {
 		setPortalProperty("STAGING_LIVE_GROUP_REMOTE_STAGING_ENABLED", true);
