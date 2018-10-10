cdfc1b9ab037242f9bc5c40384918e47d3880589
Brian Chan <brian.chan@liferay.com>
2018-10-10 15:06:53 -0700

    OAUTH2-227 fix baseline

--- modules/apps/oauth2-provider/oauth2-provider-api/src/main/java/com/liferay/oauth2/provider/configuration/OAuth2ProviderConfiguration.java
+++ modules/apps/oauth2-provider/oauth2-provider-api/src/main/java/com/liferay/oauth2/provider/configuration/OAuth2ProviderConfiguration.java
@@ -14,6 +14,7 @@
 
 package com.liferay.oauth2.provider.configuration;
 
+import aQute.bnd.annotation.ProviderType;
 import aQute.bnd.annotation.metatype.Meta;
 
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
@@ -29,6 +30,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 	localization = "content/Language",
 	name = "oauth2-provider-configuration-name"
 )
+@ProviderType
 public interface OAuth2ProviderConfiguration {
 
 	@Meta.AD(
