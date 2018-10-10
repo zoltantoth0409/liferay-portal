de9ee7dc1c3345cff84865595becece22a630930
Brian Chan <brian.chan@liferay.com>
2018-10-09 21:32:05 -0700

    Revert "LPS-84119 Simplify"
    This reverts commit d0fde5a05bf7d4f50f835e0c95d3bdb41b0bb47c.

--- modules/apps/opensocial/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/gadget/portlet/BaseGadgetPortlet.java
+++ modules/apps/opensocial/opensocial-portlet/docroot/WEB-INF/src/com/liferay/opensocial/gadget/portlet/BaseGadgetPortlet.java
@@ -139,7 +139,9 @@ public abstract class BaseGadgetPortlet extends MVCPortlet {
 		}
 
 		GadgetSpec gadgetSpec = ShindigUtil.getGadgetSpec(gadget.getUrl());
+
 		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
 		Portlet portlet = PortletLocalServiceUtil.getPortletById(
 			themeDisplay.getCompanyId(), portletDisplay.getId());
 
