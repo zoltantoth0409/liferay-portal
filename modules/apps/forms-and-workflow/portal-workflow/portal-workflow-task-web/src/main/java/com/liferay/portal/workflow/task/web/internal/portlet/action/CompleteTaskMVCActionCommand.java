03571203bc7e58b87e59d6bca672ca3e1ca0d46e
Joshua Cords <joshua.cords@liferay.com>
2018-10-10 11:18:25 -0700

    LPS-85410 Update workflowContext to save the approver's id

--- modules/apps/forms-and-workflow/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/portlet/action/CompleteTaskMVCActionCommand.java
+++ modules/apps/forms-and-workflow/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/portlet/action/CompleteTaskMVCActionCommand.java
@@ -14,12 +14,21 @@
 
 package com.liferay.portal.workflow.task.web.internal.portlet.action;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.workflow.WorkflowInstance;
+import com.liferay.portal.kernel.workflow.WorkflowInstanceManagerUtil;
+import com.liferay.portal.kernel.workflow.WorkflowTask;
 import com.liferay.portal.kernel.workflow.WorkflowTaskManagerUtil;
+
+import java.io.Serializable;
+
+import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -55,9 +64,31 @@ public class CompleteTaskMVCActionCommand
 			actionRequest, "transitionName");
 		String comment = ParamUtil.getString(actionRequest, "comment");
 
+		long userId = themeDisplay.getUserId();
+
+		Map<String, Serializable> workflowContext = _getWorkflowContext(
+			themeDisplay.getCompanyId(), workflowTaskId);
+
+		workflowContext.put(
+			WorkflowConstants.CONTEXT_USER_ID, String.valueOf(userId));
+
 		WorkflowTaskManagerUtil.completeWorkflowTask(
-			themeDisplay.getCompanyId(), themeDisplay.getUserId(),
-			workflowTaskId, transitionName, comment, null);
+			themeDisplay.getCompanyId(), userId, workflowTaskId, transitionName,
+			comment, workflowContext);
+	}
+
+	private Map<String, Serializable> _getWorkflowContext(
+			long companyId, long workflowTaskId)
+		throws PortalException {
+
+		WorkflowTask workflowTask = WorkflowTaskManagerUtil.getWorkflowTask(
+			companyId, workflowTaskId);
+
+		WorkflowInstance workflowInstance =
+			WorkflowInstanceManagerUtil.getWorkflowInstance(
+				companyId, workflowTask.getWorkflowInstanceId());
+
+		return workflowInstance.getWorkflowContext();
 	}
 
 }
