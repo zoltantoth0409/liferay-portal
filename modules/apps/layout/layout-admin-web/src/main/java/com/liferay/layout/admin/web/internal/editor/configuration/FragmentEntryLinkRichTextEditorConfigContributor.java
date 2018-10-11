c62c0072ae1e943fccdff6c254c6fb7ed456cf50
pavel-savinov <pavel.savinov@liferay.com>
2018-10-11 10:02:58 +0200

    LPS-86320 Add headings and other predefined format options, copy from AlloyEditorConfigContributor

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/editor/configuration/FragmentEntryLinkRichTextEditorConfigContributor.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/editor/configuration/FragmentEntryLinkRichTextEditorConfigContributor.java
@@ -28,17 +28,26 @@ import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 import javax.portlet.PortletURL;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Pavel Savinov
@@ -82,7 +91,8 @@ public class FragmentEntryLinkRichTextEditorConfigContributor
 		jsonObject.put("filebrowserImageBrowseUrl", itemSelectorURL.toString());
 
 		jsonObject.put("removePlugins", getRemovePluginsLists());
-		jsonObject.put("toolbars", getToolbarsJSONObject());
+		jsonObject.put(
+			"toolbars", getToolbarsJSONObject(themeDisplay.getLocale()));
 	}
 
 	protected String getAllowedContentLists() {
@@ -125,7 +135,110 @@ public class FragmentEntryLinkRichTextEditorConfigContributor
 			"resize,tabletools,toolbar,ae_embed";
 	}
 
-	protected JSONObject getToolbarsJSONObject() {
+	protected JSONObject getStyleFormatJSONObject(
+		String styleFormatName, String element, String cssClass, int type) {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		jsonObject.put("name", styleFormatName);
+		jsonObject.put("style", getStyleJSONObject(element, cssClass, type));
+
+		return jsonObject;
+	}
+
+	protected JSONArray getStyleFormatsJSONArray(Locale locale) {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		ResourceBundle resourceBundle = null;
+
+		try {
+			resourceBundle = _resourceBundleLoader.loadResourceBundle(locale);
+		}
+		catch (MissingResourceException mre) {
+			resourceBundle = ResourceBundleUtil.EMPTY_RESOURCE_BUNDLE;
+		}
+
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "normal"), "p", null, 1));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.format(resourceBundle, "heading-x", "1"), "h1",
+				null, 1));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.format(resourceBundle, "heading-x", "2"), "h2",
+				null, 1));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.format(resourceBundle, "heading-x", "3"), "h3",
+				null, 1));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.format(resourceBundle, "heading-x", "4"), "h4",
+				null, 1));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "preformatted-text"), "pre",
+				null, 1));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "cited-work"), "cite", null,
+				2));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "computer-code"), "code", null,
+				2));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "info-message"), "div",
+				"portlet-msg-info", 1));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "alert-message"), "div",
+				"portlet-msg-alert", 1));
+		jsonArray.put(
+			getStyleFormatJSONObject(
+				LanguageUtil.get(resourceBundle, "error-message"), "div",
+				"portlet-msg-error", 1));
+
+		return jsonArray;
+	}
+
+	protected JSONObject getStyleFormatsJSONObject(Locale locale) {
+		JSONObject stylesJSONObject = JSONFactoryUtil.createJSONObject();
+
+		stylesJSONObject.put("styles", getStyleFormatsJSONArray(locale));
+
+		JSONObject styleFormatsJSONObject = JSONFactoryUtil.createJSONObject();
+
+		styleFormatsJSONObject.put("cfg", stylesJSONObject);
+		styleFormatsJSONObject.put("name", "styles");
+
+		return styleFormatsJSONObject;
+	}
+
+	protected JSONObject getStyleJSONObject(
+		String element, String cssClass, int type) {
+
+		JSONObject styleJSONObject = JSONFactoryUtil.createJSONObject();
+
+		if (Validator.isNotNull(cssClass)) {
+			JSONObject attributesJSONObject =
+				JSONFactoryUtil.createJSONObject();
+
+			attributesJSONObject.put("class", cssClass);
+
+			styleJSONObject.put("attributes", attributesJSONObject);
+		}
+
+		styleJSONObject.put("element", element);
+		styleJSONObject.put("type", type);
+
+		return styleJSONObject;
+	}
+
+	protected JSONObject getToolbarsJSONObject(Locale locale) {
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
 		JSONObject toolbarJSONObject = JSONFactoryUtil.createJSONObject();
@@ -135,25 +248,26 @@ public class FragmentEntryLinkRichTextEditorConfigContributor
 
 		jsonObject.put("add", toolbarJSONObject);
 
-		jsonObject.put("styles", getToolbarsStylesJSONObject());
+		jsonObject.put("styles", getToolbarsStylesJSONObject(locale));
 
 		return jsonObject;
 	}
 
-	protected JSONObject getToolbarsStylesJSONObject() {
+	protected JSONObject getToolbarsStylesJSONObject(Locale locale) {
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
-		jsonObject.put("selections", getToolbarsStylesSelectionsJSONArray());
+		jsonObject.put(
+			"selections", getToolbarsStylesSelectionsJSONArray(locale));
 		jsonObject.put("tabIndex", 1);
 
 		return jsonObject;
 	}
 
-	protected JSONArray getToolbarsStylesSelectionsJSONArray() {
+	protected JSONArray getToolbarsStylesSelectionsJSONArray(Locale locale) {
 		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
 
 		jsonArray.put(getToolbarsStylesSelectionsLinkJSONObject());
-		jsonArray.put(getToolbarsStylesSelectionsTextJSONObject());
+		jsonArray.put(getToolbarsStylesSelectionsTextJSONObject(locale));
 
 		return jsonArray;
 	}
@@ -168,11 +282,14 @@ public class FragmentEntryLinkRichTextEditorConfigContributor
 		return jsonObject;
 	}
 
-	protected JSONObject getToolbarsStylesSelectionsTextJSONObject() {
+	protected JSONObject getToolbarsStylesSelectionsTextJSONObject(
+		Locale locale) {
+
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
 		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
 
+		jsonArray.put(getStyleFormatsJSONObject(locale));
 		jsonArray.put("bold");
 		jsonArray.put("italic");
 		jsonArray.put("underline");
@@ -205,5 +322,12 @@ public class FragmentEntryLinkRichTextEditorConfigContributor
 
 	@Reference
 	private ItemSelector _itemSelector;
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(bundle.symbolic.name=com.liferay.frontend.editor.lang)"
+	)
+	private volatile ResourceBundleLoader _resourceBundleLoader;
 
 }
