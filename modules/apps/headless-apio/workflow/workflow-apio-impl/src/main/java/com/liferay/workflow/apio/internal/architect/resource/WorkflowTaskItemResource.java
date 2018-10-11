d1976b75419720472a9b143ff4f9b7a3e5f0c0f2
Victor Galan <victor.galan@liferay.com>
2018-10-11 12:37:34 +0200

    LPS-83417 Remove unused

--- modules/apps/headless-apio/workflow/workflow-apio-impl/src/main/java/com/liferay/workflow/apio/internal/architect/resource/WorkflowTaskItemResource.java
+++ modules/apps/headless-apio/workflow/workflow-apio-impl/src/main/java/com/liferay/workflow/apio/internal/architect/resource/WorkflowTaskItemResource.java
@@ -21,7 +21,6 @@ import com.liferay.apio.architect.routes.ItemRoutes;
 import com.liferay.blog.apio.architect.identifier.BlogPostingIdentifier;
 import com.liferay.comment.apio.architect.identifier.CommentIdentifier;
 import com.liferay.media.object.apio.architect.identifier.MediaObjectIdentifier;
-import com.liferay.person.apio.architect.identifier.MyUserAccountIdentifier;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
