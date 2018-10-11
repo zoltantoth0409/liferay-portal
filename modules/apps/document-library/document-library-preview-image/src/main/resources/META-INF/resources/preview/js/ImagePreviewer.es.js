230f6d7dd8154f5eb6e0ad70fe1edb86c45f9977
Jorge González <jorge.gonzalez@liferay.com>
2018-10-11 16:58:46 +0200

    LPS-86417 Implement zoom in/out

--- modules/apps/document-library/document-library-preview-image/src/main/resources/META-INF/resources/preview/js/ImagePreviewer.es.js
+++ modules/apps/document-library/document-library-preview-image/src/main/resources/META-INF/resources/preview/js/ImagePreviewer.es.js
@@ -4,6 +4,21 @@ import Soy from 'metal-soy';
 
 import templates from './ImagePreviewer.soy';
 
+const ZOOM_LEVELS = [
+	0.1,
+	0.2,
+	0.3,
+	0.4,
+	0.5,
+	0.6,
+	0.7,
+	0.8,
+	0.9,
+	1,
+];
+
+const ZOOM_LEVELS_REVERSED = ZOOM_LEVELS.slice().reverse();
+
 /**
  * ImagePreviewer
  * @review
@@ -18,10 +33,36 @@ class ImagePreviewer extends Component {
 		this._setZoomNumber(previewImage.width);
 	}
 
+	_handleZoom(event) {
+		const value = event.delegateTarget.value;
+
+		let zoomValue;
+
+		switch (value) {
+			case 'in':
+				zoomValue = ZOOM_LEVELS
+					.find((zoom) => zoom > this.zoomActual);
+				break;
+			case 'out':
+				zoomValue = ZOOM_LEVELS_REVERSED
+					.find((zoom) => zoom < this.zoomActual);
+				break;
+		}
+
+		this._setZoom(zoomValue);
+	}
+
+	_setZoom(zoomNumber) {
+		if (typeof zoomNumber == 'undefined') return;
+
+		this.imageWidth = this.previewImageNaturalWidth * zoomNumber;
+		this.imageHeight = this.previewImageNaturalHeight * zoomNumber;
+
+		this._setZoomNumber(this.imageWidth);
+	}
+
 	_setZoomNumber(width) {
-		this.zoomActual = Math.round(
-			(width / this.previewImageNaturalWidth) * 100
-		);
+		this.zoomActual = width / this.previewImageNaturalWidth;
 	}
 }
 
@@ -33,7 +74,9 @@ class ImagePreviewer extends Component {
  */
 
 ImagePreviewer.STATE = {
+	imageHeight: Config.number(),
 	imageURL: Config.string().required(),
+	imageWidth: Config.number(),
 	zoomActual: Config.number(),
 };
 
