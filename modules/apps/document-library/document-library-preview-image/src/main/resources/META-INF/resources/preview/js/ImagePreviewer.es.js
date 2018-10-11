0adc696fc6a80dcd890c36de0ddcb43298cea976
Jorge González <jorge.gonzalez@liferay.com>
2018-10-11 09:54:26 +0200

    LPS-86417 Add some markup and calculate initalZoom

--- modules/apps/document-library/document-library-preview-image/src/main/resources/META-INF/resources/preview/js/ImagePreviewer.es.js
+++ modules/apps/document-library/document-library-preview-image/src/main/resources/META-INF/resources/preview/js/ImagePreviewer.es.js
@@ -1,7 +1,5 @@
 import Component from 'metal-component';
-
 import {Config} from 'metal-state';
-
 import Soy from 'metal-soy';
 
 import templates from './ImagePreviewer.soy';
@@ -12,7 +10,19 @@ import templates from './ImagePreviewer.soy';
  */
 
 class ImagePreviewer extends Component {
+	attached() {
+		const previewImage = this.refs.previewImage;
+		this.previewImageNaturalWidth = previewImage.naturalWidth;
+		this.previewImageNaturalHeight = previewImage.naturalHeight;
 
+		this._setZoomNumber(previewImage.width);
+	}
+
+	_setZoomNumber(width) {
+		this.zoomActual = Math.round(
+			(width / this.previewImageNaturalWidth) * 100
+		);
+	}
 }
 
 /**
@@ -23,7 +33,8 @@ class ImagePreviewer extends Component {
  */
 
 ImagePreviewer.STATE = {
-	imageURL: Config.string().required()
+	imageURL: Config.string().required(),
+	zoomActual: Config.number(),
 };
 
 Soy.register(ImagePreviewer, templates);
