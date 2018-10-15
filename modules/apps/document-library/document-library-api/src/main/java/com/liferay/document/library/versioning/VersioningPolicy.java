142106849f1b55e2143d6701141974fee1e316bc
Adolfo Pérez <adolfo.perez@liferay.com>
2018-10-15 16:38:44 +0200

    LPS-86390 Add versioning policies to D&M module
    These look very similar to the VersioningStrategy, but there's a big
    difference:
    
    - VersioningStrategy controls the versioning process as a whole (it is
      an all or nothing approach), and it can be overriden (but not
      accumulated).
    
    - VersioningPolicy is an accumulative rule or filter; developers can
      add as many as they want, and they'll be chained together. The first
      "rule" to match, will be applied.

---
+++ modules/apps/document-library/document-library-api/src/main/java/com/liferay/document/library/versioning/VersioningPolicy.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.versioning;
+
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.model.DLVersionNumberIncrease;
+
+import java.util.Optional;
+
+/**
+ * @author Adolfo Pérez
+ */
+public interface VersioningPolicy {
+
+	public Optional<DLVersionNumberIncrease> computeDLVersionNumberIncrease(
+		DLFileVersion previousDLFileVersion, DLFileVersion nextDLFileVersion);
+
+}
---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/versioning/VersioningPolicyVersioningStrategy.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.versioning;
+
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.model.DLVersionNumberIncrease;
+import com.liferay.document.library.kernel.versioning.VersioningStrategy;
+import com.liferay.document.library.versioning.VersioningPolicy;
+import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
+import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
+
+import java.util.Optional;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(immediate = true, service = VersioningStrategy.class)
+public class VersioningPolicyVersioningStrategy implements VersioningStrategy {
+
+	@Override
+	public DLVersionNumberIncrease computeDLVersionNumberIncrease(
+		DLFileVersion previousDLFileVersion, DLFileVersion nextDLFileVersion) {
+
+		for (VersioningPolicy versioningPolicy : _serviceTrackerList) {
+			Optional<DLVersionNumberIncrease> dlVersionNumberIncreaseOptional =
+				versioningPolicy.computeDLVersionNumberIncrease(
+					previousDLFileVersion, nextDLFileVersion);
+
+			if (dlVersionNumberIncreaseOptional.isPresent()) {
+				return dlVersionNumberIncreaseOptional.get();
+			}
+		}
+
+		return DLVersionNumberIncrease.NONE;
+	}
+
+	@Override
+	public boolean isOverridable() {
+		return true;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerList = ServiceTrackerListFactory.open(
+			bundleContext, VersioningPolicy.class);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerList.close();
+	}
+
+	private ServiceTrackerList<VersioningPolicy, VersioningPolicy>
+		_serviceTrackerList;
+
+}
