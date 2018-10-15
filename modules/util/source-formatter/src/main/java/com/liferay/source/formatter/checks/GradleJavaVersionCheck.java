cd9ce02da0577bac98891ec7993ba4350e4f5677
Peter Shin <peter.shin@liferay.com>
2018-10-15 16:23:20 -0700

    LPS-86408 The default Java version is 1.8 for OSGi projects (see 298e44a1)

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/GradleJavaVersionCheck.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Peter Shin
+ */
+public class GradleJavaVersionCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws IOException {
+
+		if (!absolutePath.endsWith("/build.gradle")) {
+			return content;
+		}
+
+		if (!_hasBNDFile(absolutePath)) {
+			return content;
+		}
+
+		StringBundler sb = new StringBundler();
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				String[] array = line.split(StringPool.EQUAL, 2);
+
+				if (array.length == 2) {
+					String key = array[0].trim();
+					String value = array[1].trim();
+
+					if (key.equals("sourceCompatibility") ||
+						key.equals("targetCompatibility")) {
+
+						String strippedValue = StringUtil.removeChars(
+							value, CharPool.APOSTROPHE, CharPool.QUOTE);
+
+						if (strippedValue.equals("1.8")) {
+							continue;
+						}
+					}
+				}
+
+				sb.append(line);
+				sb.append("\n");
+			}
+		}
+
+		if (sb.length() > 0) {
+			sb.setIndex(sb.index() - 1);
+		}
+
+		return sb.toString();
+	}
+
+	private boolean _hasBNDFile(String absolutePath) {
+		int pos = absolutePath.lastIndexOf(StringPool.SLASH);
+
+		File file = new File(absolutePath.substring(0, pos + 1) + "bnd.bnd");
+
+		return file.exists();
+	}
+
+}
