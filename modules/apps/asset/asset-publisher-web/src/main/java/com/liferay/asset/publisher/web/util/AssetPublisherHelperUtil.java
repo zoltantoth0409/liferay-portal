0878181db0e2236e8a63d51b6374347f878f64a8
Daniel Couso <daniel.couso@liferay.com>
2018-10-15 10:40:49 +0200

    LPS-86179 Mark this class as deprecated

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherHelperUtil.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherHelperUtil.java
@@ -22,7 +22,9 @@ import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @author  Jürgen Kappler
+ * @deprecated As of Judson (7.1.x), with no direct replacement
  */
+@Deprecated
 public class AssetPublisherHelperUtil {
 
 	public static AssetPublisherHelper getAssetPublisherHelper() {
