55b8db5e0bbebb40845b6384bed4093ede6d0c7e
Shinn Lok <shinn.lok@liferay.com>
2018-10-15 18:09:25 -0700

    LPS-86412 Copy from com.liferay.oauth2.provider.web.internal.display.context.OAuth2AdminPortletDisplayContext

---
+++ modules/apps/analytics/analytics-oauth2/src/main/java/com/liferay/analytics/oauth2/internal/OAuth2Activator.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.oauth2.internal;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.io.BigEndianCodec;
+import com.liferay.portal.kernel.security.SecureRandomUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Shinn Lok
+ */
+@Component(immediate = true, service = {})
+public class OAuth2Activator {
+
+	private static String _generateRandomId() {
+		String randomSecret = _generateRandomSecret();
+
+		return StringUtil.replace(randomSecret, "secret-", "id-");
+	}
+
+	private static String _generateRandomSecret() {
+		int size = 16;
+
+		int count = (int)Math.ceil((double)size / 8);
+
+		byte[] buffer = new byte[count * 8];
+
+		for (int i = 0; i < count; i++) {
+			BigEndianCodec.putLong(buffer, i * 8, SecureRandomUtil.nextLong());
+		}
+
+		StringBundler sb = new StringBundler(size);
+
+		for (int i = 0; i < size; i++) {
+			sb.append(Integer.toHexString(0xFF & buffer[i]));
+		}
+
+		Matcher matcher = _baseIdPattern.matcher(sb.toString());
+
+		return matcher.replaceFirst("secret-$1-$2-$3-$4-$5");
+	}
+
+	private static final Pattern _baseIdPattern = Pattern.compile(
+		"(.{8})(.{4})(.{4})(.{4})(.*)");
+
+}
