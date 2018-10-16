1d55c3c1cfbc5b91308970200f274a3958320c46
Brian Chan <brian.chan@liferay.com>
2018-10-16 12:52:13 -0700

    Revert "LPS-86412 Move to a separate dir so we can have a different app.bnd since analytics (part of foundation) cannot depend on oauth2"
    This reverts commit a4792f837589edb229675b18adb952aaab9c1b22.

---
+++ modules/apps/analytics/analytics-web/src/main/java/com/liferay/analytics/web/internal/servlet/taglib/AnalyticsTopHeadDynamicInclude.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.web.internal.servlet.taglib;
+
+import com.liferay.asset.util.AssetHelper;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eduardo Garcia
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class AnalyticsTopHeadDynamicInclude extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		_portal.setPageKeywords(
+			_assetHelper.getAssetKeywords(
+				Layout.class.getName(), themeDisplay.getPlid()),
+			request);
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register("/html/common/themes/top_head.jsp#pre");
+	}
+
+	@Reference
+	private AssetHelper _assetHelper;
+
+	@Reference
+	private Portal _portal;
+
+}
