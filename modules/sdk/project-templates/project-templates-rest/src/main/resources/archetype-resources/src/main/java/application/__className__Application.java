91d9b58b88d05fc822d8b9f19918c85beefd1547
Lawrence Lee <lawrence.lee@liferay.com>
2018-10-19 16:29:22 -0500

    LRDOCS-4360 Fix conditionals

--- modules/sdk/project-templates/project-templates-rest/src/main/resources/archetype-resources/src/main/java/application/__className__Application.java
+++ modules/sdk/project-templates/project-templates-rest/src/main/resources/archetype-resources/src/main/java/application/__className__Application.java
@@ -3,7 +3,9 @@ package ${package}.application;
 import java.util.Collections;
 import java.util.Set;
 
+#if (${liferayVersion.startsWith("7.0")})
 import javax.ws.rs.ApplicationPath;
+#end
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,18 +14,24 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Application;
 
 import org.osgi.service.component.annotations.Component;
+#if (${liferayVersion.startsWith("7.1")})
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+#end
 
 /**
  * @author ${author}
  */
+#if (${liferayVersion.startsWith("7.0")})
+@ApplicationPath("/greetings")
+@Component(immediate = true, #else
 @Component(
 	property = {
 		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/greetings",
 		JaxrsWhiteboardConstants.JAX_RS_NAME + "=Greetings.Rest"
 	},
-	service = Application.class)
-
+#end
+	service = Application.class
+)
 public class ${className}Application extends Application {
 
 	public Set<Object> getSingletons() {
