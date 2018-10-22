b891bd4aa7a766ebf3355bc8c376679e6c8fcc75
arthurchan35 <arthurchan35@gmail.com>
2018-10-22 11:39:58 +0800

    Revert "LPS-80931 do the same for elasticsearch in portal-search module, only in 70 source, which has been removed in master"
    This reverts commit 5e84924715b961e7b8d7e18fc20fdd400a0dd08a.

--- modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/filter/ElasticsearchFilterTranslator.java
+++ modules/apps/foundation/portal-search/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/filter/ElasticsearchFilterTranslator.java
@@ -30,7 +30,6 @@ import com.liferay.portal.kernel.search.filter.QueryFilter;
 import com.liferay.portal.kernel.search.filter.RangeTermFilter;
 import com.liferay.portal.kernel.search.filter.TermFilter;
 import com.liferay.portal.kernel.search.filter.TermsFilter;
-import com.liferay.portal.search.filter.DateRangeFilter;
 import com.liferay.portal.search.filter.FilterVisitor;
 import com.liferay.portal.search.filter.TermsSetFilter;
 
@@ -57,11 +56,6 @@ public class ElasticsearchFilterTranslator
 	@Override
 	public QueryBuilder visit(BooleanFilter booleanFilter) {
 		return booleanFilterTranslator.translate(booleanFilter, this);
-	}
-
-	@Override
-	public QueryBuilder visit(DateRangeFilter dateRangeFilter) {
-		return dateRangeFilterTranslator.translate(dateRangeFilter);
 	}
 
 	@Override
@@ -132,9 +126,6 @@ public class ElasticsearchFilterTranslator
 
 	@Reference
 	protected BooleanFilterTranslator booleanFilterTranslator;
-
-	@Reference
-	protected DateRangeFilterTranslator dateRangeFilterTranslator;
 
 	@Reference
 	protected DateRangeTermFilterTranslator dateRangeTermFilterTranslator;
