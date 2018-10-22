4dc8f4b7390805fe750d58dfc7950df6093f3981
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-10-22 15:37:31 +0200

    LPS-86554 Field Contributor for journal article last editor name

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/asset/display/contributor/field/JournalArticleLastEditorNameAssetDisplayContributorField.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.asset.display.contributor.field;
+
+import com.liferay.asset.display.contributor.AssetDisplayContributorField;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	property = "model.class.name=com.liferay.journal.model.JournalArticle",
+	service = AssetDisplayContributorField.class
+)
+public class JournalArticleLastEditorNameAssetDisplayContributorField
+	implements AssetDisplayContributorField<JournalArticle> {
+
+	@Override
+	public String getKey() {
+		return "lastEditorName";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, "com.liferay.journal.lang");
+
+		return LanguageUtil.get(resourceBundle, "last-editor-name");
+	}
+
+	@Override
+	public String getType() {
+		return "text";
+	}
+
+	@Override
+	public String getValue(JournalArticle article, Locale locale) {
+		long userId = article.getUserId();
+
+		User user = _userLocalService.fetchUser(userId);
+
+		if (user != null) {
+			return user.getFullName();
+		}
+
+		return StringPool.BLANK;
+	}
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
