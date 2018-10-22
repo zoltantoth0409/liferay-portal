d7a73de6ceb26537e274858261e2998a2b84d57d
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-10-22 15:00:14 +0200

    LPS-86556 Remove "service" from the property

--- modules/apps/portal-security-sso/portal-security-sso-token-impl/src/main/java/com/liferay/portal/security/sso/token/internal/events/RedirectLogoutProcessor.java
+++ modules/apps/portal-security-sso/portal-security-sso-token-impl/src/main/java/com/liferay/portal/security/sso/token/internal/events/RedirectLogoutProcessor.java
@@ -32,7 +32,7 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(
 	immediate = true,
-	property = "logout.processor.service = LogoutProcessor.class, type=" + LogoutProcessorType.REDIRECT
+	property = "logout.processor.type=" + LogoutProcessorType.REDIRECT
 )
 public class RedirectLogoutProcessor implements LogoutProcessor {
 
