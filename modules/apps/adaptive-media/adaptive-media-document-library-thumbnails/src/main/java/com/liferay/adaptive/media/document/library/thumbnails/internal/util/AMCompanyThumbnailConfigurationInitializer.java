4876f42a7e53aa019f824aa4428b45012df2eda5
Jonathan McCann <jonathan.mccann@liferay.com>
2018-10-22 08:44:35 -0700

    LPS-86356 Also confirm that this configuration does not already exist

--- modules/apps/adaptive-media/adaptive-media-document-library-thumbnails/src/main/java/com/liferay/adaptive/media/document/library/thumbnails/internal/util/AMCompanyThumbnailConfigurationInitializer.java
+++ modules/apps/adaptive-media/adaptive-media-document-library-thumbnails/src/main/java/com/liferay/adaptive/media/document/library/thumbnails/internal/util/AMCompanyThumbnailConfigurationInitializer.java
@@ -148,15 +148,19 @@ public class AMCompanyThumbnailConfigurationInitializer {
 		String name = String.format(
 			"%s %dx%d", "Thumbnail", maxWidth, maxHeight);
 
+		String uuid = _normalize(name);
+
 		Map<String, String> properties = new HashMap<>();
 
 		properties.put("max-height", String.valueOf(maxHeight));
 		properties.put("max-width", String.valueOf(maxWidth));
 
-		_amImageConfigurationHelper.addAMImageConfigurationEntry(
-			company.getCompanyId(), name,
-			"This image resolution was automatically added.", _normalize(name),
-			properties);
+		if (!_configurationExists(company.getCompanyId(), name, uuid)) {
+			_amImageConfigurationHelper.addAMImageConfigurationEntry(
+				company.getCompanyId(), name,
+				"This image resolution was automatically added.", uuid,
+				properties);
+		}
 	}
 
 	private String _normalize(String str) {
