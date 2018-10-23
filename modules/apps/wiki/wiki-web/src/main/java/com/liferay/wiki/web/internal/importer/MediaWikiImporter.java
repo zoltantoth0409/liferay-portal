ce5de8d0f972d252a033747ebfc12d6937a30cc4
Adolfo Pérez <adolfo.perez@liferay.com>
2018-10-23 15:58:07 +0200

    LPS-85733 Don't use ThemeDisplay

--- modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/importer/MediaWikiImporter.java
+++ modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/importer/MediaWikiImporter.java
@@ -762,9 +762,7 @@ public class MediaWikiImporter implements WikiImporter {
 			Company company = _companyLocalService.getCompany(
 				node.getCompanyId());
 
-			ThemeDisplay themeDisplay = new ThemeDisplay();
-
-			themeDisplay.setPortalURL(company.getPortalURL(node.getGroupId()));
+			String portalURL = company.getPortalURL(node.getGroupId());
 
 			WikiPage sharedImagesPage = _wikiPageLocalService.getPage(
 				node.getNodeId(), SHARED_IMAGES_TITLE);
@@ -795,7 +793,7 @@ public class MediaWikiImporter implements WikiImporter {
 
 				String attachmentFileEntryURL =
 					PortletFileRepositoryUtil.getPortletFileEntryURL(
-						themeDisplay, attachmentFileEntry, StringPool.BLANK);
+						null, attachmentFileEntry, StringPool.BLANK);
 
 				String linkLabel = matcher.group(3);
 
@@ -804,7 +802,7 @@ public class MediaWikiImporter implements WikiImporter {
 				}
 
 				String linkTag = StringBundler.concat(
-					"[[", attachmentFileEntryURL, linkLabel, "]]");
+					"[[", portalURL, attachmentFileEntryURL, linkLabel, "]]");
 
 				sb.replace(
 					matcher.start(0) + offset,
