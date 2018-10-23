cb646c548b11cbcf059bd73b631dabfbd7085a96
Adolfo Pérez <adolfo.perez@liferay.com>
2018-10-23 16:39:33 +0200

    LPS-85733 Auto SF

--- modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/importer/impl/mediawiki/MediaWikiImporter.java
+++ modules/apps/collaboration/wiki/wiki-service/src/main/java/com/liferay/wiki/importer/impl/mediawiki/MediaWikiImporter.java
@@ -31,7 +31,6 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.ObjectValuePair;
@@ -772,8 +771,7 @@ public class MediaWikiImporter implements WikiImporter {
 				FileEntry attachmentFileEntry =
 					PortletFileRepositoryUtil.fetchPortletFileEntry(
 						node.getGroupId(),
-						sharedImagesPage.getAttachmentsFolderId(),
-						fileName);
+						sharedImagesPage.getAttachmentsFolderId(), fileName);
 
 				if (attachmentFileEntry == null) {
 					matcher.appendReplacement(sb, mediaLinkTag);
