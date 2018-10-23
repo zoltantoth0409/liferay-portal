eaf1109f0c7a3e59df49d2edbbb08091d19a67ca
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-10-23 11:18:24 -0700

    LRQA-38970 Copy methods that we need to override to generate a new Jenkins Report

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitBisectToolBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitBisectToolBuild.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
@@ -14,10 +14,26 @@
 
 package com.liferay.jenkins.results.parser;
 
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+
+import org.json.JSONObject;
+
 /**
  * @author Michael Hashimoto
  */
 public class GitBisectToolBuild extends TopLevelBuild {
+
+	@Override
+	public Element getJenkinsReportElement() {
+		return Dom4JUtil.getNewElement(
+			"html", null, getJenkinsReportHeadElement(),
+			getJenkinsReportBodyElement());
+	}
 
 	protected GitBisectToolBuild(String url) {
 		this(url, null);
@@ -25,6 +41,158 @@ public class GitBisectToolBuild extends TopLevelBuild {
 
 	protected GitBisectToolBuild(String url, TopLevelBuild topLevelBuild) {
 		super(url, topLevelBuild);
+	}
+
+	@Override
+	protected Element getJenkinsReportBodyElement() {
+		String buildURL = getBuildURL();
+
+		Element headingElement = Dom4JUtil.getNewElement(
+			"h1", null, "Jenkins report for ",
+			Dom4JUtil.getNewAnchorElement(buildURL, buildURL));
+
+		Element subheadingElement = null;
+
+		JSONObject jobJSONObject = getBuildJSONObject();
+
+		String description = jobJSONObject.optString("description");
+
+		if (!description.isEmpty()) {
+			subheadingElement = Dom4JUtil.getNewElement("h2");
+
+			try {
+				Dom4JUtil.addRawXMLToElement(subheadingElement, description);
+			}
+			catch (DocumentException de) {
+				throw new RuntimeException(
+					"Unable to parse description HTML " + description, de);
+			}
+		}
+
+		return Dom4JUtil.getNewElement(
+			"body", null, headingElement, subheadingElement,
+			getJenkinsReportSummaryElement(), getJenkinsReportTimelineElement(),
+			getJenkinsReportTopLevelTableElement(),
+			getJenkinsReportDownstreamElement());
+	}
+
+	@Override
+	protected Element getJenkinsReportTableColumnHeadersElement() {
+		Element nameElement = Dom4JUtil.getNewElement("th", null, "Name");
+
+		Element consoleElement = Dom4JUtil.getNewElement("th", null, "Console");
+
+		Element testReportElement = Dom4JUtil.getNewElement(
+			"th", null, "Test Report");
+
+		Element startTimeElement = Dom4JUtil.getNewElement(
+			"th", null, "Start Time");
+
+		Element buildTimeElement = Dom4JUtil.getNewElement(
+			"th", null, "Build Time");
+
+		Element statusElement = Dom4JUtil.getNewElement("th", null, "Status");
+
+		Element resultElement = Dom4JUtil.getNewElement("th", null, "Result");
+
+		Element tableColumnHeaderElement = Dom4JUtil.getNewElement("tr");
+
+		Dom4JUtil.addToElement(
+			tableColumnHeaderElement, nameElement, consoleElement,
+			testReportElement, startTimeElement, buildTimeElement,
+			statusElement, resultElement);
+
+		return tableColumnHeaderElement;
+	}
+
+	protected Element getJenkinsReportTableRowElement() {
+		String cellElementTagName =
+			getJenkinsReportBuildInfoCellElementTagName();
+
+		Element buildInfoElement = Dom4JUtil.getNewElement(
+			"tr", null,
+			Dom4JUtil.getNewElement(
+				cellElementTagName, null,
+				Dom4JUtil.getNewAnchorElement(
+					getBuildURL(), null, getDisplayName())),
+			Dom4JUtil.getNewElement(
+				cellElementTagName, null,
+				Dom4JUtil.getNewAnchorElement(
+					getBuildURL() + "console", null, "Console")),
+			Dom4JUtil.getNewElement(
+				cellElementTagName, null,
+				Dom4JUtil.getNewAnchorElement(
+					getBuildURL() + "testReport", "Test Report")));
+
+		getStartTime();
+
+		if (startTime == null) {
+			Dom4JUtil.addToElement(
+				buildInfoElement,
+				Dom4JUtil.getNewElement(
+					cellElementTagName, null, "",
+					getJenkinsReportTimeZoneName()));
+		}
+		else {
+			Dom4JUtil.addToElement(
+				buildInfoElement,
+				Dom4JUtil.getNewElement(
+					cellElementTagName, null,
+					toJenkinsReportDateString(
+						new Date(startTime), getJenkinsReportTimeZoneName())));
+		}
+
+		Dom4JUtil.addToElement(
+			buildInfoElement,
+			Dom4JUtil.getNewElement(
+				cellElementTagName, null,
+				JenkinsResultsParserUtil.toDurationString(getDuration())));
+
+		String status = getStatus();
+
+		if (status != null) {
+			status = StringUtils.upperCase(status);
+		}
+		else {
+			status = "";
+		}
+
+		Dom4JUtil.getNewElement(cellElementTagName, buildInfoElement, status);
+
+		String result = getResult();
+
+		if (result == null) {
+			result = "";
+		}
+
+		Dom4JUtil.getNewElement(cellElementTagName, buildInfoElement, result);
+
+		return buildInfoElement;
+	}
+
+	@Override
+	protected Element getJenkinsReportTopLevelTableElement() {
+		Element topLevelTableElement = Dom4JUtil.getNewElement("table");
+
+		String result = getResult();
+
+		if (result != null) {
+			Dom4JUtil.getNewElement(
+				"caption", topLevelTableElement, "Top Level Build - ",
+				Dom4JUtil.getNewElement("strong", null, getResult()));
+		}
+		else {
+			Dom4JUtil.getNewElement(
+				"caption", topLevelTableElement, "Top Level Build - ",
+				Dom4JUtil.getNewElement(
+					"strong", null, StringUtils.upperCase(getStatus())));
+		}
+
+		Dom4JUtil.addToElement(
+			topLevelTableElement, getJenkinsReportTableColumnHeadersElement(),
+			getJenkinsReportTableRowElement());
+
+		return topLevelTableElement;
 	}
 
 }
