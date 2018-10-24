c25a5d32f618a3fd10c3efddd0cb48fdb0456675
ricardocousolr <ricardo.couso@liferay.com>
2018-10-24 14:39:41 +0200

    LPS-70394 Add osgi commands to update indexes by bundle id or symbolic name or do all

---
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/index/updater/osgi/commands/IndexUpdaterOSGiCommands.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.internal.index.updater.osgi.commands;
+
+import com.liferay.portal.upgrade.internal.index.updater.IndexUpdater;
+
+import org.apache.felix.service.command.Descriptor;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ricardo Couso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"osgi.command.function=updateIndexes",
+		"osgi.command.function=updateIndexesAll", "osgi.command.scope=upgrade"
+	},
+	service = IndexUpdaterOSGiCommands.class
+)
+public class IndexUpdaterOSGiCommands {
+
+	@Activate
+	public void activate(final BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+		_indexUpdater = new IndexUpdater();
+	}
+
+	@Descriptor("Update database indexes for a specific module via bundle id")
+	public String updateIndexes(long bundleId) {
+		Bundle bundle = _bundleContext.getBundle(bundleId);
+
+		if (bundle == null) {
+			return "Module with id " + bundleId + " does not exist";
+		}
+
+		if (_indexUpdater.hasIndexes(bundle)) {
+			_indexUpdater.updateIndexes(bundle);
+
+			return "Completed update of indexes for module with id " + bundleId;
+		}
+
+		return "Module with id " + bundleId +
+			" has no indexes associated with it";
+	}
+
+	@Descriptor(
+		"Update database indexes for specific a module via symbolic name"
+	)
+	public String updateIndexes(String bundleSymbolicName) {
+		Bundle bundle = _indexUpdater.getBundle(bundleSymbolicName);
+
+		if (bundle == null) {
+			return "Module with symbolic name " + bundleSymbolicName +
+				" does not exist";
+		}
+
+		if (_indexUpdater.hasIndexes(bundle)) {
+			_indexUpdater.updateIndexes(bundle);
+
+			return "Completed update of indexes for module " +
+				bundleSymbolicName;
+		}
+
+		return "Module " + bundleSymbolicName +
+			" has no indexes associated with it";
+	}
+
+	@Descriptor("Update database indexes for all modules")
+	public String updateIndexesAll() {
+		_indexUpdater.updateIndexesAll();
+
+		return "Completed update of indexes for all modules";
+	}
+
+	private BundleContext _bundleContext;
+	private IndexUpdater _indexUpdater;
+
+}
