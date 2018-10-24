3d765f9e1f76636b415f95f5427ffced4ab76baf
Adolfo Pérez <adolfo.perez@liferay.com>
2018-10-24 16:47:13 +0200

    LPS-86390 Add versioning policies for content and metadata

---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/versioning/ContentVersioningPolicy.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.versioning;
+
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.model.DLVersionNumberIncrease;
+import com.liferay.document.library.kernel.service.DLFileVersionLocalService;
+import com.liferay.document.library.versioning.VersioningPolicy;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.DigesterUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.Optional;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true, property = "service.ranking:Integer=100",
+	service = VersioningPolicy.class
+)
+public class ContentVersioningPolicy implements VersioningPolicy {
+
+	@Override
+	public Optional<DLVersionNumberIncrease> computeDLVersionNumberIncrease(
+		DLFileVersion previousDLFileVersion, DLFileVersion nextDLFileVersion) {
+
+		long previousSize = previousDLFileVersion.getSize();
+		long nextSize = nextDLFileVersion.getSize();
+
+		if ((previousSize == 0) && (nextSize >= 0)) {
+			return Optional.empty();
+		}
+
+		if (previousSize != nextSize) {
+			return Optional.of(DLVersionNumberIncrease.MAJOR);
+		}
+
+		String previousChecksum = _computeChecksum(previousDLFileVersion);
+
+		if (previousChecksum == null) {
+			return Optional.empty();
+		}
+
+		String nextChecksum = _computeChecksum(nextDLFileVersion);
+
+		if ((nextChecksum == null) || previousChecksum.equals(nextChecksum)) {
+			return Optional.empty();
+		}
+
+		return Optional.of(DLVersionNumberIncrease.MAJOR);
+	}
+
+	private String _computeChecksum(DLFileVersion dlFileVersion) {
+		if (Validator.isNotNull(dlFileVersion.getChecksum())) {
+			return dlFileVersion.getChecksum();
+		}
+
+		try (InputStream is = dlFileVersion.getContentStream(false)) {
+			dlFileVersion.setChecksum(DigesterUtil.digestBase64(is));
+
+			_dlFileVersionLocalService.updateDLFileVersion(dlFileVersion);
+
+			return dlFileVersion.getChecksum();
+		}
+		catch (IOException | PortalException e) {
+			_log.warn(e, e);
+
+			return null;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ContentVersioningPolicy.class);
+
+	@Reference
+	private DLFileVersionLocalService _dlFileVersionLocalService;
+
+}
---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/versioning/MetadataVersioningPolicy.java
@@ -0,0 +1,153 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.versioning;
+
+import com.liferay.document.library.kernel.model.DLFileEntryMetadata;
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.model.DLVersionNumberIncrease;
+import com.liferay.document.library.kernel.service.DLFileEntryMetadataLocalService;
+import com.liferay.document.library.versioning.VersioningPolicy;
+import com.liferay.dynamic.data.mapping.kernel.DDMFormValues;
+import com.liferay.dynamic.data.mapping.kernel.DDMStructure;
+import com.liferay.dynamic.data.mapping.kernel.StorageEngineManagerUtil;
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.io.Serializable;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(immediate = true, service = VersioningPolicy.class)
+public class MetadataVersioningPolicy implements VersioningPolicy {
+
+	@Override
+	public Optional<DLVersionNumberIncrease> computeDLVersionNumberIncrease(
+		DLFileVersion previousDLFileVersion, DLFileVersion nextDLFileVersion) {
+
+		if (!Objects.equals(
+				previousDLFileVersion.getTitle(),
+				nextDLFileVersion.getTitle())) {
+
+			return Optional.of(DLVersionNumberIncrease.MINOR);
+		}
+
+		if (!Objects.equals(
+				previousDLFileVersion.getDescription(),
+				nextDLFileVersion.getDescription())) {
+
+			return Optional.of(DLVersionNumberIncrease.MINOR);
+		}
+
+		if (previousDLFileVersion.getFileEntryTypeId() !=
+				nextDLFileVersion.getFileEntryTypeId()) {
+
+			return Optional.of(DLVersionNumberIncrease.MINOR);
+		}
+
+		if (_isDLFileEntryTypeUpdated(
+				previousDLFileVersion, nextDLFileVersion)) {
+
+			return Optional.of(DLVersionNumberIncrease.MINOR);
+		}
+
+		if (_isExpandoUpdated(previousDLFileVersion, nextDLFileVersion)) {
+			return Optional.of(DLVersionNumberIncrease.MINOR);
+		}
+
+		return Optional.empty();
+	}
+
+	private boolean _isDLFileEntryTypeUpdated(
+		DLFileVersion previousDLFileVersion, DLFileVersion nextDLFileVersion) {
+
+		try {
+			DLFileEntryType dlFileEntryType =
+				previousDLFileVersion.getDLFileEntryType();
+
+			for (DDMStructure ddmStructure :
+					dlFileEntryType.getDDMStructures()) {
+
+				DLFileEntryMetadata previousFileEntryMetadata =
+					_dlFileEntryMetadataLocalService.fetchFileEntryMetadata(
+						ddmStructure.getStructureId(),
+						previousDLFileVersion.getFileVersionId());
+
+				if (previousFileEntryMetadata == null) {
+					return true;
+				}
+
+				DLFileEntryMetadata nextFileEntryMetadata =
+					_dlFileEntryMetadataLocalService.getFileEntryMetadata(
+						ddmStructure.getStructureId(),
+						nextDLFileVersion.getFileVersionId());
+
+				DDMFormValues previousDDMFormValues =
+					StorageEngineManagerUtil.getDDMFormValues(
+						previousFileEntryMetadata.getDDMStorageId());
+				DDMFormValues nextDDMFormValues =
+					StorageEngineManagerUtil.getDDMFormValues(
+						nextFileEntryMetadata.getDDMStorageId());
+
+				if (!previousDDMFormValues.equals(nextDDMFormValues)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+		catch (PortalException pe) {
+			_log.warn(pe, pe);
+
+			return false;
+		}
+	}
+
+	private boolean _isExpandoUpdated(
+		DLFileVersion previousDLFileVersion, DLFileVersion nextDLFileVersion) {
+
+		ExpandoBridge previousExpandoBridge =
+			previousDLFileVersion.getExpandoBridge();
+		ExpandoBridge nextExpandoBridge = nextDLFileVersion.getExpandoBridge();
+
+		Map<String, Serializable> previousAttributes =
+			previousExpandoBridge.getAttributes();
+		Map<String, Serializable> nextAttributes =
+			nextExpandoBridge.getAttributes();
+
+		if (!previousAttributes.equals(nextAttributes)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		MetadataVersioningPolicy.class);
+
+	@Reference
+	private DLFileEntryMetadataLocalService _dlFileEntryMetadataLocalService;
+
+}
