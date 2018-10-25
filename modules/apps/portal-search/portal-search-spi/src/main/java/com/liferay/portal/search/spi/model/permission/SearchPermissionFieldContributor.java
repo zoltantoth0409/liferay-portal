7cceaa862de820c845701bebec4a43685d0ce7c1
Russell Bohl <russell.bohl@liferay.com>
2018-10-25 17:24:27 -0400

    build.gradle auto SF

--- modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/SearchPermissionFieldContributor.java
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/SearchPermissionFieldContributor.java
@@ -20,8 +20,13 @@ import com.liferay.portal.kernel.search.Document;
 
 /**
  * Indexes new permission checking fields in a search document. These fields can
+<<<<<<< HEAD
  * be matched when returning search results via a corresponding {@link
  * SearchPermissionFilterContributor}.
+=======
+ * be matched when returning search results via a corresponding
+ * {@link SearchPermissionFilterContributor}.
+>>>>>>> 179f129... LPS-84966, review editing
  *
  * <p>
  * Register implementations of this interface as OSGi components using the
