b1368ad431b67cb73de818e1c79ce3c67198750e
Eduardo Garcia <eduardo.garcia@liferay.com>
2018-10-24 18:19:56 -0700

    LPS-86437 Add provider class to obtain related segments and entities.
    This class will be used to reindex segment information in entities.

---
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/provider/SegmentsEntryProvider.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.provider;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.segments.model.SegmentsEntry;
+
+import java.util.List;
+
+/**
+ * Provides methods for obtaining {@link SegmentsEntry} objects and their
+ * related entities.
+ *
+ * @author Eduardo Garcia
+ * @review
+ */
+public interface SegmentsEntryProvider {
+
+	/**
+	 * Returns the list of active {@link SegmentsEntry} objects related with the
+	 * entity.
+	 *
+	 * @param  groupId the primary key of the segment's group
+	 * @param  className the class name of the entity
+	 * @param  classPK the primary key of the entity
+	 * @return the list of active {@link SegmentsEntry} objects related to the
+	 *         entity
+	 * @throws PortalException
+	 * @review
+	 */
+	public List<SegmentsEntry> getSegmentsEntries(
+			long groupId, String className, long classPK)
+		throws PortalException;
+
+	/**
+	 * Returns the primary keys of the entities related with the segment.
+	 *
+	 * @param  segmentsEntryId the ID of the segment
+	 * @return the primary keys of the entities related with the segment
+	 * @throws PortalException
+	 * @review
+	 */
+	public long[] getSegmentsEntryClassPKs(long segmentsEntryId)
+		throws PortalException;
+
+}
---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/util/SegmentsEntryProviderImpl.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.util;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.segments.model.SegmentsEntry;
+import com.liferay.segments.model.SegmentsEntryRel;
+import com.liferay.segments.odata.retriever.ODataRetriever;
+import com.liferay.segments.provider.SegmentsEntryProvider;
+import com.liferay.segments.service.SegmentsEntryLocalService;
+import com.liferay.segments.service.SegmentsEntryRelLocalService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eduardo Garcia
+ */
+@Component(immediate = true, service = SegmentsEntryProvider.class)
+public class SegmentsEntryProviderImpl implements SegmentsEntryProvider {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, ODataRetriever.class, "model.class.name");
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	@Override
+	public List<SegmentsEntry> getSegmentsEntries(
+			long groupId, String className, long classPK)
+		throws PortalException {
+
+		List<SegmentsEntry> segmentsEntries =
+			_segmentsEntryLocalService.getSegmentsEntries(
+				groupId, true, className, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				null);
+
+		if (segmentsEntries.isEmpty()) {
+			return Collections.emptyList();
+		}
+
+		ODataRetriever oDataRetriever = _serviceTrackerMap.getService(
+			className);
+
+		List<SegmentsEntry> allSegmentsEntries = new ArrayList();
+
+		for (SegmentsEntry segmentsEntry : segmentsEntries) {
+			if (Validator.isNotNull(segmentsEntry.getCriteria()) &&
+				(oDataRetriever != null)) {
+
+				String filterString = String.format(
+					"(%s) and (classPK eq '%s')", segmentsEntry.getCriteria(),
+					classPK);
+
+				if (oDataRetriever.getResultsCount(
+						segmentsEntry.getCompanyId(), filterString,
+						Locale.getDefault()) == 0) {
+
+					continue;
+				}
+			}
+			else if (!_segmentsEntryRelLocalService.hasSegmentsEntryRel(
+						segmentsEntry.getSegmentsEntryId(),
+						_portal.getClassNameId(className), classPK)) {
+
+				continue;
+			}
+
+			allSegmentsEntries.add(segmentsEntry);
+		}
+
+		return allSegmentsEntries;
+	}
+
+	@Override
+	public long[] getSegmentsEntryClassPKs(long segmentsEntryId)
+		throws PortalException {
+
+		SegmentsEntry segmentsEntry =
+			_segmentsEntryLocalService.fetchSegmentsEntry(segmentsEntryId);
+
+		if (segmentsEntry == null) {
+			return new long[0];
+		}
+
+		if (Validator.isNotNull(segmentsEntry.getCriteria())) {
+			ODataRetriever oDataRetriever = _serviceTrackerMap.getService(
+				segmentsEntry.getType());
+
+			List<BaseModel<?>> results = oDataRetriever.getResults(
+				segmentsEntry.getCompanyId(), segmentsEntry.getCriteria(),
+				Locale.getDefault(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+			Stream<BaseModel<?>> stream = results.stream();
+
+			return stream.mapToLong(
+				baseModel -> (Long)baseModel.getPrimaryKeyObj()
+			).toArray();
+		}
+		else {
+			List<SegmentsEntryRel> segmentsEntryRels =
+				_segmentsEntryRelLocalService.getSegmentsEntryRels(
+					segmentsEntryId);
+
+			Stream<SegmentsEntryRel> stream = segmentsEntryRels.stream();
+
+			return stream.mapToLong(
+				SegmentsEntryRel::getClassPK
+			).toArray();
+		}
+	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private SegmentsEntryLocalService _segmentsEntryLocalService;
+
+	@Reference
+	private SegmentsEntryRelLocalService _segmentsEntryRelLocalService;
+
+	private ServiceTrackerMap<String, ODataRetriever> _serviceTrackerMap;
+
+}
