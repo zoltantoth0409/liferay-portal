0397da87d4353a608b65e82ad2527873ea5df9c5
Brian Chan <brian.chan@liferay.com>
2018-10-25 10:51:14 -0700

    LPS-85726 as used

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/lar/PortletDataContextImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/lar/PortletDataContextImpl.java
@@ -3089,6 +3089,9 @@ public class PortletDataContextImpl implements PortletDataContext {
 						newPrimaryKey))) {
 
 				try {
+					long importedClassPK = GetterUtil.getLong(
+						classedModel.getPrimaryKeyObj());
+
 					String referrerUuid =
 						stagedGroupedWorkflowDefinitionLinkElement.
 							attributeValue("uuid");
@@ -3107,9 +3110,6 @@ public class PortletDataContextImpl implements PortletDataContext {
 
 						typePK = ddmPrimaryKeys.getOrDefault(typePK, typePK);
 					}
-
-					long importedClassPK = GetterUtil.getLong(
-						classedModel.getPrimaryKeyObj());
 
 					PermissionChecker permissionChecker =
 						PermissionThreadLocal.getPermissionChecker();
