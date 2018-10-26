9b6e23ddc0d729f56bdfd9a3cb7467dc53fd93b0
Minhchau Dang <minhchau.dang@liferay.com>
2018-10-25 17:09:12 -0700

    LPS-86410 Make sure to delete the corresponding prototype

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateEntryLocalServiceImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateEntryLocalServiceImpl.java
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.LayoutPrototypeLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
@@ -211,6 +212,15 @@ public class LayoutPageTemplateEntryLocalServiceImpl
 		}
 
 		layoutPageTemplateEntryPersistence.remove(layoutPageTemplateEntry);
+
+		// Prototype
+
+		long layoutPrototypeId = layoutPageTemplateEntry.getLayoutPrototypeId();
+
+		if (layoutPrototypeId > 0) {
+			_layoutPrototypeLocalService.deleteLayoutPrototype(
+				layoutPrototypeId);
+		}
 
 		// Resources
 
@@ -581,5 +591,8 @@ public class LayoutPageTemplateEntryLocalServiceImpl
 
 	@ServiceReference(type = FragmentEntryLinkLocalService.class)
 	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@ServiceReference(type = LayoutPrototypeLocalService.class)
+	private LayoutPrototypeLocalService _layoutPrototypeLocalService;
 
 }
