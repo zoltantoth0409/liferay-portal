7d3a4144aab22c8b9833fe24a903b19170b66474
rubenpulido <ruben.pulido@liferay.com>
2018-10-26 14:31:21 +0200

    LPS-85235 Fix broken test

--- modules/apps/headless-apio/structured-content/structured-content-apio-test/src/testIntegration/java/com/liferay/structured/content/apio/internal/architect/resource/test/StructuredContentNestedCollectionResourceFilteringTest.java
+++ modules/apps/headless-apio/structured-content/structured-content-apio-test/src/testIntegration/java/com/liferay/structured/content/apio/internal/architect/resource/test/StructuredContentNestedCollectionResourceFilteringTest.java
@@ -989,7 +989,7 @@ public class StructuredContentNestedCollectionResourceFilteringTest
 			new Filter(
 				filterParser.parse(
 					StringBundler.concat(
-						"(values/s", _ddmStructure.getStructureId(),
+						"(values/_", _ddmStructure.getStructureId(),
 						StringPool.UNDERLINE, "TextFieldName eq ",
 						"'textfield')"))),
 			Sort.emptySort());
