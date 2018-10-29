13b3849aa0322e0c8c715f873e4c351bac39d4ce
Michael Han <michael.han@liferay.com>
2018-10-29 08:36:17 -0500

    LPS-86537 rely upon BooleanQueryTranslator to handle filters for boolean queries

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/query/BooleanQueryTranslatorImpl.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/query/BooleanQueryTranslatorImpl.java
@@ -56,6 +56,12 @@ public class BooleanQueryTranslatorImpl implements BooleanQueryTranslator {
 			return boolQueryBuilder;
 		}
 
+		//
+		// LPS-86537 The following conversion is present for backwards
+		// compatibility with how Liferay's Indexer frameworks handles queries.
+		// Ideally, we do not wrap the BooleanQuery with another BooleanQuery
+		//
+
 		BoolQueryBuilder wrapperBoolQueryBuilder = QueryBuilders.boolQuery();
 
 		wrapperBoolQueryBuilder.must(boolQueryBuilder);
