1bf8386c833d7039f94d89505618d262f385ada9
Alberto Chaparro <alberto.chaparro@liferay.com>
2018-10-29 10:05:14 +0100

    LPS-86460 New databases don't need to execute any upgrade step

--- modules/apps/flags/flags-web/src/main/java/com/liferay/flags/web/internal/upgrade/PageFlagsWebUpgrade.java
+++ modules/apps/flags/flags-web/src/main/java/com/liferay/flags/web/internal/upgrade/PageFlagsWebUpgrade.java
@@ -32,7 +32,7 @@ public class PageFlagsWebUpgrade implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
-		registry.register("0.0.0", "1.0.0", new DummyUpgradeStep());
+		registry.register("0.0.0", "1.0.1", new DummyUpgradeStep());
 
 		registry.register("0.0.1", "1.0.0", new UpgradePortletId());
 
