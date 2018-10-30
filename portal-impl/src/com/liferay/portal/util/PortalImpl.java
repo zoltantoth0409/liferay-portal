60c5f78527f893590050bb1a1403a46058ee7bca
Alan Huang <alan.huang@liferay.com>
2018-10-30 18:23:42 +0800

    LPS-86545 When impersonating a user, no need to add doAsUserId if the url is a external url

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -21,6 +21,7 @@ import com.liferay.expando.kernel.exception.ValueDataException;
 import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.expando.kernel.model.ExpandoColumnConstants;
 import com.liferay.exportimport.kernel.staging.StagingUtil;
+import com.liferay.layouts.admin.kernel.model.LayoutTypePortletConstants;
 import com.liferay.petra.encryptor.Encryptor;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
@@ -3301,7 +3302,16 @@ public class PortalImpl implements Portal {
 		}
 
 		if (layout.isTypeURL()) {
-			return getLayoutActualURL(layout);
+			String portalURL = getPortalURL(layout, themeDisplay);
+
+			String url = layout.getTypeSettingsProperty(
+				LayoutTypePortletConstants.URL);
+
+			if (!url.startsWith(StringPool.SLASH) &&
+				!url.startsWith(portalURL)) {
+
+				return getLayoutActualURL(layout);
+			}
 		}
 
 		String layoutFriendlyURL = getLayoutFriendlyURL(layout, themeDisplay);
