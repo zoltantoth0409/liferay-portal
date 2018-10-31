c6d426215bf696e5a1a925b1fd8b315c1b9aa4b6
tina.tian <tina.tian@liferay.com>
2018-10-31 10:55:00 -0700

    LPS-86815 Deprecate unused method

--- portal-kernel/src/com/liferay/portal/kernel/portlet/PortletClassLoaderUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/PortletClassLoaderUtil.java
@@ -31,6 +31,10 @@ public class PortletClassLoaderUtil {
 		return ClassLoaderPool.getClassLoader(getServletContextName());
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
+	 */
+	@Deprecated
 	public static ClassLoader getClassLoader(String portletId) {
 		PortletBag portletBag = PortletBagPool.get(portletId);
 
