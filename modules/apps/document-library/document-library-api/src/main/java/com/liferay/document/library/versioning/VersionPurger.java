cef6d7774b9f476765745d7d8864d7df05142a8c
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-11-02 14:01:01 +0100

    LPS-84799 Adding interface for version purging

---
+++ modules/apps/document-library/document-library-api/src/main/java/com/liferay/document/library/versioning/VersionPurger.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.versioning;
+
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+
+import java.util.Collection;
+
+/**
+ * @author Alejandro Tardín
+ */
+public interface VersionPurger {
+
+	public Collection<FileVersion> getFileVersionsToPurge(FileEntry fileEntry);
+
+	public interface VersionPurgedListener {
+
+		public void versionPurged(FileVersion fileVersion);
+
+	}
+
+}
