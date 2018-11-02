362fda78aa4da2482ef88d4e95ec33eb997073f9
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-11-02 13:06:14 +0100

    LPS-86871 Uses correct url

--- modules/apps/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
+++ modules/apps/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
@@ -47,8 +47,6 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
-import com.liferay.portal.kernel.portlet.PortletProvider;
-import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.portlet.PortletRequestModel;
 import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
@@ -657,66 +655,42 @@ public class JournalContentDisplayContext {
 		}
 	}
 
-	public String getURLEditTemplate() {
-		try {
-			DDMTemplate ddmTemplate = getDDMTemplate();
+	public String getURLEditTemplate() throws Exception {
+		DDMTemplate ddmTemplate = getDDMTemplate();
 
-			if (ddmTemplate == null) {
-				return StringPool.BLANK;
-			}
-
-			ThemeDisplay themeDisplay =
-				(ThemeDisplay)_portletRequest.getAttribute(
-					WebKeys.THEME_DISPLAY);
-
-			PortletURL portletURL = PortletURLFactoryUtil.create(
-				_portletRequest,
-				PortletProviderUtil.getPortletId(
-					DDMTemplate.class.getName(), PortletProvider.Action.EDIT),
-				PortletRequest.RENDER_PHASE);
-
-			portletURL.setParameter(
-				"hideDefaultSuccessMessage", Boolean.TRUE.toString());
-			portletURL.setParameter("mvcPath", "/edit_template.jsp");
-			portletURL.setParameter("navigationStartsOn", "SELECT_TEMPLATE");
-
-			PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
-
-			PortletURL redirectURL = PortletURLFactoryUtil.create(
-				_portletRequest, portletDisplay.getId(),
-				PortletRequest.RENDER_PHASE);
-
-			redirectURL.setParameter(
-				"mvcPath", "/update_journal_article_redirect.jsp");
-			redirectURL.setParameter(
-				"referringPortletResource", portletDisplay.getId());
-			redirectURL.setWindowState(LiferayWindowState.POP_UP);
-
-			portletURL.setParameter("redirect", redirectURL.toString());
-
-			portletURL.setParameter("showBackURL", Boolean.FALSE.toString());
-			portletURL.setParameter(
-				"showCacheableInput", Boolean.TRUE.toString());
-			portletURL.setParameter(
-				"groupId", String.valueOf(ddmTemplate.getGroupId()));
-			portletURL.setParameter(
-				"refererPortletName",
-				PortletProviderUtil.getPortletId(
-					JournalArticle.class.getName(),
-					PortletProvider.Action.EDIT));
-			portletURL.setParameter(
-				"templateId", String.valueOf(ddmTemplate.getTemplateId()));
-			portletURL.setParameter("showHeader", Boolean.FALSE.toString());
-			portletURL.setPortletMode(PortletMode.VIEW);
-			portletURL.setWindowState(LiferayWindowState.POP_UP);
-
-			return portletURL.toString();
-		}
-		catch (Exception e) {
-			_log.error("Unable to get edit template URL", e);
-
+		if (ddmTemplate == null) {
 			return StringPool.BLANK;
 		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)_portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		PortletURL portletURL = PortletURLFactoryUtil.create(
+			_portletRequest, JournalPortletKeys.JOURNAL,
+			PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter("mvcPath", "/edit_ddm_template.jsp");
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		PortletURL redirectURL = PortletURLFactoryUtil.create(
+			_portletRequest, portletDisplay.getId(),
+			PortletRequest.RENDER_PHASE);
+
+		redirectURL.setParameter(
+			"mvcPath", "/update_journal_article_redirect.jsp");
+		redirectURL.setParameter(
+			"referringPortletResource", portletDisplay.getId());
+		redirectURL.setWindowState(LiferayWindowState.POP_UP);
+
+		portletURL.setParameter("redirect", redirectURL.toString());
+
+		portletURL.setParameter(
+			"ddmTemplateId", String.valueOf(ddmTemplate.getTemplateId()));
+		portletURL.setPortletMode(PortletMode.VIEW);
+		portletURL.setWindowState(LiferayWindowState.POP_UP);
+
+		return portletURL.toString();
 	}
 
 	public boolean hasRestorePermission() throws PortalException {
