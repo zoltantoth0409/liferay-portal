5a6c64b9f716f8be6557add78c082bb90eab24a7
jpince <jason.pince@liferay.com>
2018-11-02 14:14:03 -0700

    Revert "LPS-86291 Satisfy the dependency with the current range for the porlet's schema version"
    This reverts commit ca0448e2259a6e6eaaa8855c9ec2bf79e050118b.

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/verify/KaleoDesignerWebVerifyProcess.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/verify/KaleoDesignerWebVerifyProcess.java
@@ -109,7 +109,7 @@ public class KaleoDesignerWebVerifyProcess extends VerifyProcess {
 		_kaleoDefinitionVersionLocalService;
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.portal.workflow.kaleo.designer.web)(&(release.schema.version>=2.0.0)(!(release.schema.version<2.1.0))))"
+		target = "(&(release.bundle.symbolic.name=com.liferay.portal.workflow.kaleo.designer.web)(release.schema.version=1.0.2))"
 	)
 	private Release _release;
 
