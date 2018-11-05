8493d375c974e97e2c241bdb493bdfd4b67b2bb0
Kenji Heigel <kenji.heigel@liferay.com>
2018-11-05 14:58:37 -0800

    LRQA-44079 Add new exception for unbalanced code

---
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/script/UnbalancedCodeException.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.poshi.runner.script;
+
+/**
+ * @author Kenji Heigel
+ */
+public class UnbalancedCodeException extends Exception {
+
+	public UnbalancedCodeException(String msg, int index, String code) {
+		super(msg);
+
+		_processLine(index, code);
+	}
+
+	public String getErrorPositionString() {
+		return _errorPositionString;
+	}
+
+	public int getLineNumber() {
+		return _lineNumber;
+	}
+
+	private static String _getLine(int lineNumber, String code) {
+		String[] lines = code.split("\n");
+
+		return lines[lineNumber - 1];
+	}
+
+	private void _processLine(int index, String code) {
+		int lineNumber = 1;
+
+		int newLineIndex = -1;
+
+		for (int i = 0; i < index; i++) {
+			if (code.charAt(i) == '\n') {
+				lineNumber++;
+
+				newLineIndex = i;
+			}
+		}
+
+		_lineNumber = lineNumber;
+
+		int column = 1;
+
+		for (int i = newLineIndex + 1; i < index; i++) {
+			if (code.charAt(i) == '\t') {
+				column += 4;
+
+				continue;
+			}
+
+			column++;
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(_getLine(lineNumber, code));
+		sb.append("\n");
+
+		for (int i = 1; i < column; i++) {
+			sb.append(" ");
+		}
+
+		sb.append("^");
+
+		_errorPositionString = sb.toString();
+	}
+
+	private String _errorPositionString;
+	private int _lineNumber;
+
+}
