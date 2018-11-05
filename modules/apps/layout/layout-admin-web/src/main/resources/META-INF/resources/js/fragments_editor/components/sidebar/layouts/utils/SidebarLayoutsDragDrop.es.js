75e74432ff68ef8eb206055b04f12046a173d258
Sandro Chinea <sandrochinea@gmail.com>
2018-11-05 15:19:38 +0000

    LPS-86007 Added handlers for drag, drop and leave target events

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/layouts/utils/SidebarLayoutsDragDrop.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/layouts/utils/SidebarLayoutsDragDrop.es.js
@@ -1,5 +1,8 @@
+import position from 'metal-position';
 import {Drag, DragDrop} from 'metal-drag-drop';
 import State from 'metal-state';
+
+import {DRAG_POSITIONS} from '../../../../reducers/placeholders.es';
 
 /**
  * SidebarLayoutsDragDrop
@@ -25,6 +28,67 @@ class SidebarLayoutsDragDrop extends State {
 	}
 
 	/**
+	 * Callback that is executed when a layout is being dragged.
+	 * @param {object} data
+	 * @param {MouseEvent} data.originalEvent
+	 * @private
+	 * @review
+	 */
+	_handleDrag(data) {
+		const targetItem = data.target;
+
+		if (targetItem && 'layoutSection' in targetItem.dataset) {
+			const mouseY = data.originalEvent.clientY;
+			const sectionRow = targetItem.querySelector('[data-row-id]');
+			const targetItemRegion = position.getRegion(targetItem);
+
+			let nearestBorder = DRAG_POSITIONS.bottom;
+
+			if (Math.abs(mouseY - targetItemRegion.top) <= Math.abs(mouseY - targetItemRegion.bottom)) {
+				nearestBorder = DRAG_POSITIONS.top;
+			}
+
+			this.emit(
+				'dragLayout',
+				{
+					hoveredSectionBorder: nearestBorder,
+					hoveredSectionId: sectionRow.dataset.rowId
+				}
+			);
+		}
+	}
+
+	/**
+	 * Callback that is executed when a drag target is leaved.
+	 * @private
+	 * @review
+	 */
+	_handleDragEnd() {
+		this.emit('leaveLayoutTarget');
+	}
+
+	/**
+	 * Callback that is executed when a layout is dropped.
+	 * @param {!object} data
+	 * @param {!HTMLElement} data.source
+	 * @param {!MouseEvent} event
+	 * @private
+	 * @review
+	 */
+	_handleDrop(data, event) {
+		event.preventDefault();
+
+		if (data.target) {
+			this.emit(
+				'dropLayout',
+				{
+					layoutIndex: data.source.dataset.layoutIndex
+				}
+			);
+		}
+	}
+
+	/**
 	 * @private
 	 * @review
 	 */
@@ -40,6 +104,21 @@ class SidebarLayoutsDragDrop extends State {
 				sources: '.layouts-drag-section',
 				targets: '.layouts-drop-target'
 			}
+		);
+
+		this._dragDrop.on(
+			DragDrop.Events.DRAG,
+			this._handleDrag.bind(this)
+		);
+
+		this._dragDrop.on(
+			DragDrop.Events.END,
+			this._handleDrop.bind(this)
+		);
+
+		this._dragDrop.on(
+			DragDrop.Events.TARGET_LEAVE,
+			this._handleDragEnd.bind(this)
 		);
 	}
 
