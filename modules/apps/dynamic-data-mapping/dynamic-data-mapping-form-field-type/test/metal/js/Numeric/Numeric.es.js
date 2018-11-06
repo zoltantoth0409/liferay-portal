eec365749c947f558a7805d2e3444980f4a18ef6
Clóvis Neto <cloves13@hotmail.com>
2018-11-06 14:31:24 -0300

    LPS-84341 Create numeric field tests

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/metal/js/Numeric/Numeric.es.js
@@ -0,0 +1,249 @@
+import Numeric from 'source/Numeric/Numeric.es';
+import {dom as MetalTestUtil} from 'metal-dom';
+
+let component;
+const spritemap = 'icons.svg';
+
+const defaultNumericConfig = {
+	name: 'numericField',
+	spritemap
+};
+
+describe(
+	'Field Numeric',
+	() => {
+		beforeEach(
+			() => {
+				jest.useFakeTimers();
+			}
+		);
+
+		afterEach(
+			() => {
+				if (component) {
+					component.dispose();
+				}
+			}
+		);
+
+		it(
+			'should render the default markup',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						readOnly: false
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should not be readOnly',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						readOnly: false
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a helptext',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						tip: 'Type something'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have an id',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						id: 'ID'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a label',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						label: 'label'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a placeholder',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						placeholder: 'Placeholder'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should not be required',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						required: false
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should render Label if showLabel is true',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						label: 'Numeric Field',
+						showLabel: true
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a spritemap',
+			() => {
+				component = new Numeric(defaultNumericConfig);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a value',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						value: '123'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a key',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						key: 'key'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should emit a field edit event on field value change',
+			() => {
+				const handleFieldEdited = jest.fn();
+
+				const events = {fieldEdited: handleFieldEdited};
+
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						events
+					}
+				);
+
+				MetalTestUtil.triggerEvent(
+					component.element.querySelector('input'),
+					'input',
+					{}
+				);
+
+				expect(handleFieldEdited).toHaveBeenCalled();
+			}
+		);
+
+		it(
+			'should propagate the field edit event',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig,
+						key: 'input'
+					}
+				);
+
+				const spy = jest.spyOn(component, 'emit');
+
+				MetalTestUtil.triggerEvent(
+					component.element.querySelector('input'),
+					'input',
+					{}
+				);
+
+				expect(spy).toHaveBeenCalled();
+				expect(spy).toHaveBeenCalledWith('fieldEdited', expect.any(Object));
+			}
+		);
+
+		it(
+			'should change the mask type',
+			() => {
+				component = new Numeric(
+					{
+						...defaultNumericConfig
+					}
+				);
+
+				jest.runAllTimers();
+
+				component.setState(
+					{
+						dataType: 'double'
+					}
+				);
+
+				jest.runAllTimers();
+
+				expect(component.dataType).toBe('double');
+			}
+		);
+	}
+);
