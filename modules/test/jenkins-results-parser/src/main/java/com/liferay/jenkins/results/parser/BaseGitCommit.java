7d5c39b91d992a3934d04731553fac4efaf566c4
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-11-08 11:25:34 -0800

    LRQA-44666 Create a new GitCommit class hierarchy to handle Local & Remote commits differently

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseGitCommit.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import org.json.JSONObject;
+
+/**
+ * @author Michael Hashimoto
+ */
+public abstract class BaseGitCommit implements GitCommit {
+
+	@Override
+	public boolean equals(Object object) {
+		if (object.hashCode() == hashCode()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public String getAbbreviatedSHA() {
+		return _sha.substring(0, 7);
+	}
+
+	@Override
+	public String getGitRepositoryName() {
+		return _gitRepositoryName;
+	}
+
+	@Override
+	public String getMessage() {
+		return _message;
+	}
+
+	@Override
+	public String getSHA() {
+		return _sha;
+	}
+
+	@Override
+	public GitCommit.Type getType() {
+		return _type;
+	}
+
+	@Override
+	public int hashCode() {
+		JSONObject jsonObject = toJSONObject();
+
+		String json = jsonObject.toString();
+
+		return json.hashCode();
+	}
+
+	@Override
+	public JSONObject toJSONObject() {
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("message", _message);
+		jsonObject.put("sha", _sha);
+
+		return jsonObject;
+	}
+
+	protected BaseGitCommit(
+		String gitRepositoryName, String message, String sha,
+		GitCommit.Type type) {
+
+		_gitRepositoryName = gitRepositoryName;
+		_message = message;
+		_sha = sha;
+		_type = type;
+	}
+
+	private final String _gitRepositoryName;
+	private final String _message;
+	private final String _sha;
+	private final GitCommit.Type _type;
+
+}
---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitHubRemoteGitCommit.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.json.JSONObject;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class GitHubRemoteGitCommit
+	extends BaseGitCommit implements RemoteGitCommit {
+
+	@Override
+	public String getGitHubCommitURL() {
+		return JenkinsResultsParserUtil.combine(
+			"https://github.com/", _gitHubUsername, "/", getGitRepositoryName(),
+			"/commit/", getSHA());
+	}
+
+	@Override
+	public void setStatus(
+		RemoteGitCommit.Status status, String context, String description,
+		String targetURL) {
+
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("state", StringUtils.lowerCase(status.toString()));
+
+		if (context != null) {
+			jsonObject.put("context", context);
+		}
+
+		if (description != null) {
+			jsonObject.put("description", description);
+		}
+
+		if ((targetURL != null) && targetURL.matches("https?\\:\\/\\/.*")) {
+			jsonObject.put("target_url", targetURL);
+		}
+
+		try {
+			JenkinsResultsParserUtil.toJSONObject(
+				getGitHubStatusURL(), jsonObject.toString());
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(ioe);
+		}
+	}
+
+	protected GitHubRemoteGitCommit(
+		String gitHubUsername, String gitRepositoryName, String message,
+		String sha, Type type) {
+
+		super(gitRepositoryName, message, sha, type);
+
+		_gitHubUsername = gitHubUsername;
+	}
+
+	protected String getGitHubStatusURL() {
+		return JenkinsResultsParserUtil.getGitHubApiUrl(
+			getGitRepositoryName(), _gitHubUsername, "statuses/" + getSHA());
+	}
+
+	private final String _gitHubUsername;
+
+}
