273b34698166e6c467010c4680f744d6070de09f
Preston-Crary <preston.crary@liferay.com>
2018-11-08 15:34:30 -0800

    LPS-86914 Fix race conditions with ConfigurationAdmin and store tests.

--- modules/apps/portal-store/portal-store-file-system-test/src/testIntegration/java/com/liferay/portal/store/file/system/test/AdvancedFileSystemStoreTest.java
+++ modules/apps/portal-store/portal-store-file-system-test/src/testIntegration/java/com/liferay/portal/store/file/system/test/AdvancedFileSystemStoreTest.java
@@ -16,14 +16,27 @@ package com.liferay.portal.store.file.system.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.document.library.kernel.store.Store;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portlet.documentlibrary.store.test.BaseStoreTestCase;
 
+import java.util.Dictionary;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 
 /**
  * @author Vilmos Papp
@@ -36,10 +49,45 @@ public class AdvancedFileSystemStoreTest extends BaseStoreTestCase {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_rootDir =
+			_props.get(PropsKeys.LIFERAY_HOME) +
+				"/test/store/advanced_file_system";
+
+		_configuration = _configurationAdmin.getConfiguration(
+			"com.liferay.portal.store.file.system.configuration." +
+				"AdvancedFileSystemStoreConfiguration",
+			StringPool.QUESTION);
+
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("rootDir", _rootDir);
+
+		ConfigurationTestUtil.saveConfiguration(_configuration, properties);
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		ConfigurationTestUtil.deleteConfiguration(_configuration);
+
+		FileUtil.deltree(_rootDir);
+	}
+
 	@Override
 	protected Store getStore() {
 		return _store;
 	}
+
+	private static Configuration _configuration;
+
+	@Inject
+	private static ConfigurationAdmin _configurationAdmin;
+
+	@Inject
+	private static Props _props;
+
+	private static String _rootDir;
 
 	@Inject(
 		filter = "(&(service.ranking=0)(store.type=com.liferay.portal.store.file.system.AdvancedFileSystemStore))",
