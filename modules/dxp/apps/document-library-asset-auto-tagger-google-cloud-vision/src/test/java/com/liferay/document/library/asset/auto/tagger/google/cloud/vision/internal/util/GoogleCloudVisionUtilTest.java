04875bbbd6c31accae88155b8dee90c912f5175d
Alejandro Tardín <alejandro.tardin@liferay.com>
2018-11-12 15:48:10 +0100

    LPS-87179 Adding test to ensure we know if this breaks again

---
+++ modules/dxp/apps/document-library-asset-auto-tagger-google-cloud-vision/src/test/java/com/liferay/document/library/asset/auto/tagger/google/cloud/vision/internal/util/GoogleCloudVisionUtilTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.document.library.asset.auto.tagger.google.cloud.vision.internal.util;
+
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.Base64;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.util.FileImpl;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * @author Alejandro Tardín
+ */
+public class GoogleCloudVisionUtilTest {
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+
+		FileUtil fileUtil = new FileUtil();
+
+		fileUtil.setFile(new FileImpl());
+
+		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
+
+		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
+	}
+
+	@Test
+	public void testGetAnnotateImagePayload() throws Exception {
+		Mockito.when(
+			_fileEntry.getFileVersion()
+		).thenReturn(
+			_fileVersion
+		);
+
+		String randomString = RandomTestUtil.randomString();
+
+		Mockito.when(
+			_fileVersion.getContentStream(false)
+		).thenReturn(
+			new ByteArrayInputStream(randomString.getBytes())
+		);
+
+		Assert.assertEquals(
+			StringBundler.concat(
+				"{\"requests\":[{\"features\":",
+				"[{\"type\":\"LABEL_DETECTION\"}],\"image\":{\"content\":\"",
+				Base64.encode(randomString.getBytes()), "\"}}]}"),
+			GoogleCloudVisionUtil.getAnnotateImagePayload(_fileEntry));
+	}
+
+	@Mock
+	private FileEntry _fileEntry;
+
+	@Mock
+	private FileVersion _fileVersion;
+
+}
