ca2bde14eb3c626dbd56d9ffedaa4c73151e6395
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2018-11-12 10:30:08 +0100

    LPS-86798 site-admin-web - SiteAdminPortlet.java - reads the 'inherit Locales' property to determine which locales should be saved for the site

--- modules/apps/web-experience/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
+++ modules/apps/web-experience/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
@@ -37,6 +37,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.RemoteOptionsException;
 import com.liferay.portal.kernel.exception.RequiredGroupException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
@@ -79,6 +80,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -804,6 +806,15 @@ public class SiteAdminPortlet extends MVCPortlet {
 		UnicodeProperties formTypeSettingsProperties =
 			PropertiesParamUtil.getProperties(
 				actionRequest, "TypeSettingsProperties--");
+
+		if (GetterUtil.getBoolean(
+				formTypeSettingsProperties.getProperty("inheritLocales"))) {
+
+			formTypeSettingsProperties.setProperty(
+			PropsKeys.LOCALES,
+			StringUtil.merge(
+				LocaleUtil.toLanguageIds(LanguageUtil.getAvailableLocales())));
+		}
 
 		typeSettingsProperties.putAll(formTypeSettingsProperties);
 
