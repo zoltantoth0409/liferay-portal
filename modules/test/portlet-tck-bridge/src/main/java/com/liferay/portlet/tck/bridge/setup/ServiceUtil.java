5b28e1eb919f250d9e2fe6ec34e8f2e23f93bdfd
Kyle Stiemann <stiemannkj1@gmail.com>
2018-11-12 10:58:33 -0500

    LRQA-33560 remove dead code

--- modules/test/portlet-tck-bridge/src/main/java/com/liferay/portlet/tck/bridge/setup/ServiceUtil.java
+++ modules/test/portlet-tck-bridge/src/main/java/com/liferay/portlet/tck/bridge/setup/ServiceUtil.java
@@ -14,20 +14,15 @@
 
 package com.liferay.portlet.tck.bridge.setup;
 
-import com.liferay.portal.kernel.exception.NoSuchUserException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-
-import java.util.Locale;
 
 /**
  * @author Vernon Singleton
@@ -67,64 +62,5 @@ public class ServiceUtil {
 			userId, groupId, privateLayout, parentLayoutId, name, title,
 			description, type, hidden, friendlyURL, serviceContext);
 	}
-
-	public static User addUser(
-			long creatorUserId, long companyId, String firstName,
-			String lastName)
-		throws Exception {
-
-		boolean autoPassword = false;
-		String password1 = "test";
-
-		boolean autoScreenName = false;
-
-		String screenName =
-			StringUtil.toLowerCase(firstName) + "." +
-				StringUtil.toLowerCase(lastName);
-
-		String emailAddress = screenName + "@liferay.com";
-
-		long facebookId = 0L;
-		String openId = "";
-		Locale locale = Locale.ENGLISH;
-		String middleName = "";
-		int prefixId = 0;
-		int suffixId = 0;
-		boolean male = true;
-		int birthdayMonth = 1;
-		int birthdayDay = 1;
-		int birthdayYear = 1970;
-		String jobTitle = "";
-		long[] groupIds = new long[0];
-		long[] organizationIds = new long[0];
-		long[] roleIds = new long[0];
-		long[] userGroupIds = new long[0];
-		boolean sendEmail = false;
-		ServiceContext serviceContext = new ServiceContext();
-
-		User user;
-
-		try {
-			user = UserLocalServiceUtil.getUserByScreenName(
-				companyId, screenName);
-		}
-		catch (NoSuchUserException nsue) {
-			user = UserLocalServiceUtil.addUser(
-				creatorUserId, companyId, autoPassword, password1, password1,
-				autoScreenName, screenName, emailAddress, facebookId, openId,
-				locale, firstName, middleName, lastName, prefixId, suffixId,
-				male, birthdayMonth, birthdayDay, birthdayYear, jobTitle,
-				groupIds, organizationIds, roleIds, userGroupIds, sendEmail,
-				serviceContext);
-
-			if (_log.isInfoEnabled()) {
-				_log.info("Added user: " + screenName);
-			}
-		}
-
-		return user;
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(ServiceUtil.class);
 
 }
