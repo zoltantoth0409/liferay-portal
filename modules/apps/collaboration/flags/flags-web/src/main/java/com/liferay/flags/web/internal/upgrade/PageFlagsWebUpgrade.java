9506ed7761c04c5e2778bcce9ceb8e0c9e6b56fa
ajsampang67 <ajsampang67@gmail.com>
2018-11-13 11:58:26 -0600

    LPS-86460 update db schema with PID changes

--- modules/apps/collaboration/flags/flags-web/src/main/java/com/liferay/flags/web/internal/upgrade/PageFlagsWebUpgrade.java
+++ modules/apps/collaboration/flags/flags-web/src/main/java/com/liferay/flags/web/internal/upgrade/PageFlagsWebUpgrade.java
@@ -14,11 +14,13 @@
 
 package com.liferay.flags.web.internal.upgrade;
 
+import com.liferay.flags.configuration.FlagsGroupServiceConfiguration;
 import com.liferay.flags.web.internal.upgrade.v1_0_0.UpgradePortletId;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.configuration.persistence.upgrade.ConfigurationUpgradeStepFactory;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
-
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Raymond Augé
@@ -30,10 +32,16 @@ public class PageFlagsWebUpgrade implements UpgradeStepRegistrator {
 	@Override
 	public void register(Registry registry) {
 		registry.register(
-			"com.liferay.flags.web", "0.0.0", "1.0.0", new DummyUpgradeStep());
+			"com.liferay.flags.web", "0.0.1", "1.0.0", new UpgradePortletId());
 
 		registry.register(
-			"com.liferay.flags.web", "0.0.1", "1.0.0", new UpgradePortletId());
-	}
+			"com.liferay.flags.web", "1.0.0", "1.0.1",
+			_configurationUpgradeStepFactory.createUpgradeStep(
+				"com.liferay.flags.configuration.FlagsConfiguration",
+				FlagsGroupServiceConfiguration.class.getName()));
+		}
+
+	@Reference
+	private ConfigurationUpgradeStepFactory _configurationUpgradeStepFactory;
 
 }
