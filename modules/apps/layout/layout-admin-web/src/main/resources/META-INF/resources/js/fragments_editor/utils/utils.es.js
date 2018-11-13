8ad18abc9d57a8c3b17d7839b0453c235fcfee00
Sandro Chinea <sandrochinea@gmail.com>
2018-11-13 10:47:00 +0000

    LPS-86902 Added functions to get column from columnId and from fragmentEntryLinkId

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/utils/utils.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/utils/utils.es.js
@@ -1,4 +1,42 @@
 /**
+ * Returns the column with the given columnId
+ * @param {Object} structure
+ * @param {string} columnId
+ * @return {Object}
+ */
+function getColumn(structure, columnId) {
+	return structure
+		.map(
+			section => section.columns.find(
+				_column => _column.columnId === columnId
+			)
+		)
+		.filter(column => column)
+		.find(column => column);
+}
+
+/**
+ * Returns the column that contains the fragment
+ * with the given fragmentEntryLinkId.
+ *
+ * @param {Array} structure
+ * @param {string} fragmentEntryLinkId
+ * @return {Object}
+ */
+function getFragmentColumn(structure, fragmentEntryLinkId) {
+	return structure
+		.map(
+			section => section.columns.find(
+				_column => _column.fragmentEntryLinkIds.find(
+					fragmentId => fragmentId === fragmentEntryLinkId
+				)
+			)
+		)
+		.filter(column => column)
+		.find(column => column);
+}
+
+/**
  * Returns the row index of a given fragmentEntryLinkId.
  * -1 if it is not present.
  *
@@ -91,6 +129,8 @@ function updateLayoutData(
 }
 
 export {
+	getColumn,
+	getFragmentColumn,
 	getFragmentRowIndex,
 	setIn,
 	updateLayoutData
