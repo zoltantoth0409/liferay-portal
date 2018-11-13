8318a828e60f3b132ca28200b9046aee963ff462
Hugo Huijser <hugo.huijser@liferay.com>
2018-11-13 15:26:25 +0800

    LPS-86806 Add implementation of getString for JavaMethodCall

--- modules/util/portal-tools-java-parser/src/main/java/com/liferay/portal/tools/java/parser/JavaMethodCall.java
+++ modules/util/portal-tools-java-parser/src/main/java/com/liferay/portal/tools/java/parser/JavaMethodCall.java
@@ -14,7 +14,10 @@
 
 package com.liferay.portal.tools.java.parser;
 
+import com.liferay.portal.kernel.util.StringBundler;
+
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Hugo Huijser
@@ -23,6 +26,49 @@ public class JavaMethodCall extends JavaExpression {
 
 	public JavaMethodCall(String methodName) {
 		_methodName = new JavaSimpleValue(methodName);
+	}
+
+	@Override
+	public String getString(
+		String indent, String prefix, String suffix, int maxLineLength,
+		boolean forceLineBreak) {
+
+		StringBundler sb = new StringBundler();
+
+		sb.append(indent);
+		sb.append(prefix);
+
+		if (_genericJavaTypes != null) {
+			append(sb, _genericJavaTypes, indent, "<", ">", maxLineLength);
+		}
+
+		if (_parameterValueJavaExpressions.isEmpty()) {
+			append(
+				sb, _methodName, indent, "", "()" + suffix, maxLineLength,
+				false);
+		}
+		else {
+			if (Objects.equals(
+					append(
+						sb, _methodName, indent, "", "(", maxLineLength, false),
+					APPENDED_NEW_LINE)) {
+
+				indent += "\t";
+			}
+
+			if (forceLineBreak) {
+				appendNewLine(
+					sb, _parameterValueJavaExpressions, indent + "\t", "",
+					")" + suffix, maxLineLength);
+			}
+			else {
+				append(
+					sb, _parameterValueJavaExpressions, indent, "",
+					")" + suffix, maxLineLength);
+			}
+		}
+
+		return sb.toString();
 	}
 
 	public void setGenericJavaTypes(List<JavaType> genericJavaTypes) {
