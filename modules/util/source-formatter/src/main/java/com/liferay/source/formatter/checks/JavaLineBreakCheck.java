d4f6f0d2500e48730c9d8ba7e83373516a09f966
Hugo Huijser <hugo.huijser@liferay.com>
2018-11-13 14:29:59 +0800

    LPS-84119 Fix false positives

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaLineBreakCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaLineBreakCheck.java
@@ -266,7 +266,7 @@ public class JavaLineBreakCheck extends LineBreakCheck {
 		if (line.endsWith(" throws") ||
 			((previousLine.endsWith(StringPool.COMMA) ||
 			  previousLine.endsWith(StringPool.OPEN_PARENTHESIS)) &&
-			 line.contains(" throws ") &&
+			 strippedQuotesLine.contains(" throws ") &&
 			 (line.endsWith(StringPool.OPEN_CURLY_BRACE) ||
 			  line.endsWith(StringPool.SEMICOLON)))) {
 
@@ -276,7 +276,7 @@ public class JavaLineBreakCheck extends LineBreakCheck {
 		}
 
 		if (line.endsWith(StringPool.PERIOD) &&
-			line.contains(StringPool.EQUAL)) {
+			strippedQuotesLine.contains(StringPool.EQUAL)) {
 
 			addMessage(
 				fileName, "There should be a line break after '='", lineNumber);
