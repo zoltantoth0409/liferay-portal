92dea84823caddd66afc12ac5bbca5303566d951
drewbrokke <drew.brokke@liferay.com>
2018-11-14 15:00:46 -0800

    LPS-86822 user-associated-data-api - annotates UADDisplay

--- modules/apps/foundation/user-associated-data/user-associated-data-api/src/main/java/com/liferay/user/associated/data/display/UADDisplay.java
+++ modules/apps/foundation/user-associated-data/user-associated-data-api/src/main/java/com/liferay/user/associated/data/display/UADDisplay.java
@@ -25,20 +25,61 @@ import java.util.Locale;
 import java.util.Map;
 
 /**
+ * Provides the necessary methods to count, retrieve, and display information
+ * about entities of type {@code T} related to a user. Can also provide a URL to
+ * allow an admin to edit the an entity.
+ *
  * @author William Newbury
  */
 public interface UADDisplay<T> extends UADComponent<T> {
 
+	/**
+	 * Returns a count of the number of entities of type {@code T} associated
+	 * with the given userId.
+	 *
+	 * @param userId the userId whose data to count
+	 * @return the number of entities associated with the userId
+	 */
 	public long count(long userId);
 
+	/**
+	 * Retrieves an entity of type {@code T}
+	 *
+	 * @param primaryKey the primaryKey of the entity to retrieve
+	 * @return an entity of type {@code T}
+	 * @throws Exception
+	 */
 	public T get(Serializable primaryKey) throws Exception;
 
+	/**
+	 * Returns an array of strings defining the field names to be used as table
+	 * column headers when displaying a list of entities of type {@code T}
+	 *
+	 * @return an array of field names used for column headers
+	 */
 	public default String[] getColumnFieldNames() {
 		return getDisplayFieldNames();
 	}
 
+	/**
+	 * Returns an array of strings defining the names of fields to display
+	 * when showing details about an entity of type {@code T}
+	 *
+	 * @return an array of field names identifying which information to display
+	 */
 	public String[] getDisplayFieldNames();
 
+	/**
+	 * Returns a string URL that allows the admin user to edit the entity. If
+	 * {@code null} is returned then no edit option will be presented to the
+	 * admin user.
+	 *
+	 * @param t the entity of type {@code T}
+	 * @param liferayPortletRequest the current portlet request
+	 * @param liferayPortletResponse the current portlet response
+	 * @return a string URL, or {@code null}
+	 * @throws Exception
+	 */
 	public default String getEditURL(
 			T t, LiferayPortletRequest liferayPortletRequest,
 			LiferayPortletResponse liferayPortletResponse)
@@ -47,12 +88,41 @@ public interface UADDisplay<T> extends UADComponent<T> {
 		return null;
 	}
 
+	/**
+	 * Returns a map of field names and values to be displayed in the UI.
+	 *
+	 * @param t the entity of type {@code T}
+	 * @param fieldNames the field names for getting values from the entity
+	 * @return a map of values to be displayed in the UI
+	 */
 	public Map<String, Object> getFieldValues(T t, String[] fieldNames);
 
+	/**
+	 * Returns the value representing the primary key of the entity of type
+	 * {@code T}
+	 *
+	 * @param t the entity to retrieve the primary key for
+	 * @return the primary key of the entity
+	 */
 	public Serializable getPrimaryKey(T t);
 
+	/**
+	 * Returns a paginated list of entities of type {@code T} associated with a
+	 * user.
+	 *
+	 * @param userId the userId whose data to get
+	 * @param start the starting index of the result set, for pagination
+	 * @param end the ending index of the result set, for pagination
+	 * @return a paginated list of entities related to the userId
+	 */
 	public List<T> getRange(long userId, int start, int end);
 
+	/**
+	 * Returns a localized string representing type {@code T}.
+	 *
+	 * @param locale the current locale
+	 * @return a localized string representing type {@code T}
+	 */
 	public String getTypeName(Locale locale);
 
 }
