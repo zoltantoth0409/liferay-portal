e562ecd0d020fd9dc010830b03e5c4f8b6b724cf
drewbrokke <drew.brokke@liferay.com>
2018-11-14 14:15:47 -0800

    LPS-86822 user-associated-data-api - annotates UADExporter

--- modules/apps/user-associated-data/user-associated-data-api/src/main/java/com/liferay/user/associated/data/exporter/UADExporter.java
+++ modules/apps/user-associated-data/user-associated-data-api/src/main/java/com/liferay/user/associated/data/exporter/UADExporter.java
@@ -22,15 +22,41 @@ import com.liferay.user.associated.data.component.UADComponent;
 import java.io.File;
 
 /**
+ * Handles converting the entities of type {@code T} related to a user into a
+ * format that can be written to a file and downloaded.
+ *
  * @author William Newbury
  */
 @ProviderType
 public interface UADExporter<T> extends UADComponent<T> {
 
+	/**
+	 * Returns a count of the number of entities of type {@code T} associated
+	 * with the given userId.
+	 *
+	 * @param userId the userId whose data to count
+	 * @return the number of entities associated with the userId
+	 */
 	public long count(long userId) throws PortalException;
 
+	/**
+	 * Returns a byte array representing the entity, ready to be written to a
+	 * file.
+	 *
+	 * @param t the entity of type {@code T} to convert into a byte array
+	 * @return a byte array representing the given entity
+	 * @throws PortalException
+	 */
 	public byte[] export(T t) throws PortalException;
 
+	/**
+	 * Returns a file object containing the data from all entities of type
+	 * {@code T} related to a user.
+	 *
+	 * @param userId whose data to export
+	 * @return a {@link File} object containing the exported data
+	 * @throws PortalException
+	 */
 	public File exportAll(long userId) throws PortalException;
 
 }
