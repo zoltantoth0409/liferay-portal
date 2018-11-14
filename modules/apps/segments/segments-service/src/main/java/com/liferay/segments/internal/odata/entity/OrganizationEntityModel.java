dc8d4f352bd8b56b65a6b107e12be29d530d4cef
David Arques <davidarchena@gmail.com>
2018-11-14 23:35:34 +0100

    LPS-87319 Use complex-type with new property 'customField'

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/odata/entity/OrganizationEntityModel.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/odata/entity/OrganizationEntityModel.java
@@ -15,17 +15,17 @@
 package com.liferay.segments.internal.odata.entity;
 
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.odata.entity.ComplexEntityField;
 import com.liferay.portal.odata.entity.DateEntityField;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.odata.entity.StringEntityField;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import org.osgi.service.component.annotations.Component;
 
 /**
  * Provides the entity data model from the Indexed Entity (Organization).
@@ -33,14 +33,34 @@ import org.osgi.service.component.annotations.Component;
  * @author David Arques
  * @review
  */
-@Component(
-	immediate = true,
-	property = "entity.model.name=" + OrganizationEntityModel.NAME,
-	service = EntityModel.class
-)
 public class OrganizationEntityModel implements EntityModel {
 
 	public static final String NAME = "Organization";
+
+	public OrganizationEntityModel(List<EntityField> customEntityFields) {
+		_entityFieldsMap = Stream.of(
+			new ComplexEntityField("customField", customEntityFields),
+			new DateEntityField(
+				"dateModified",
+				locale -> Field.getSortableFieldName(Field.MODIFIED_DATE),
+				locale -> Field.MODIFIED_DATE),
+			new StringEntityField("classPK", locale -> Field.ORGANIZATION_ID),
+			new StringEntityField("companyId", locale -> Field.COMPANY_ID),
+			new StringEntityField(
+				"name", locale -> Field.getSortableFieldName(Field.NAME)),
+			new StringEntityField(
+				"nameTreePath",
+				locale -> Field.getSortableFieldName("nameTreePath_String")),
+			new StringEntityField(
+				"organizationId", locale -> Field.ORGANIZATION_ID),
+			new StringEntityField(
+				"parentOrganizationId", locale -> "parentOrganizationId"),
+			new StringEntityField("treePath", locale -> Field.TREE_PATH),
+			new StringEntityField("type", locale -> Field.TYPE)
+		).collect(
+			Collectors.toMap(EntityField::getName, Function.identity())
+		);
+	}
 
 	@Override
 	public Map<String, EntityField> getEntityFieldsMap() {
@@ -52,26 +72,6 @@ public class OrganizationEntityModel implements EntityModel {
 		return NAME;
 	}
 
-	private static final Map<String, EntityField> _entityFieldsMap = Stream.of(
-		new DateEntityField(
-			"dateModified",
-			locale -> Field.getSortableFieldName(Field.MODIFIED_DATE),
-			locale -> Field.MODIFIED_DATE),
-		new StringEntityField("classPK", locale -> Field.ORGANIZATION_ID),
-		new StringEntityField("companyId", locale -> Field.COMPANY_ID),
-		new StringEntityField(
-			"name", locale -> Field.getSortableFieldName(Field.NAME)),
-		new StringEntityField(
-			"nameTreePath",
-			locale -> Field.getSortableFieldName("nameTreePath_String")),
-		new StringEntityField(
-			"organizationId", locale -> Field.ORGANIZATION_ID),
-		new StringEntityField(
-			"parentOrganizationId", locale -> "parentOrganizationId"),
-		new StringEntityField("treePath", locale -> Field.TREE_PATH),
-		new StringEntityField("type", locale -> Field.TYPE)
-	).collect(
-		Collectors.toMap(EntityField::getName, Function.identity())
-	);
+	private final Map<String, EntityField> _entityFieldsMap;
 
 }
