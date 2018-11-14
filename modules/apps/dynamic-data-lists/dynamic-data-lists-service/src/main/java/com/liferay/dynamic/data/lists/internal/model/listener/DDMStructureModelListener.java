e887b8d7264a50d5390a643f55aef87165c71f01
Joshua Cords <joshua.cords@liferay.com>
2018-11-13 16:12:59 -0800

    LPS-87175 Keep old nameMap from failing validateName during staging

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/model/listener/DDMStructureModelListener.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/model/listener/DDMStructureModelListener.java
@@ -17,6 +17,7 @@ package com.liferay.dynamic.data.lists.internal.model.listener;
 import com.liferay.dynamic.data.lists.model.DDLRecordSet;
 import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalService;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
@@ -26,6 +27,10 @@ import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -79,10 +84,25 @@ public class DDMStructureModelListener extends BaseModelListener<DDMStructure> {
 
 				serviceContext.setUserId(defaultUserId);
 
+				Locale siteLocale = null;
+
+				if (ExportImportThreadLocal.isImportInProcess()) {
+					siteLocale = LocaleThreadLocal.getSiteDefaultLocale();
+
+					Locale stagingLocale = LocaleUtil.fromLanguageId(
+						ddmStructure.getDefaultLanguageId());
+
+					LocaleThreadLocal.setSiteDefaultLocale(stagingLocale);
+				}
+
 				_ddlRecordSetLocalService.updateRecordSet(
 					recordSet.getRecordSetId(), ddmStructure.getStructureId(),
 					recordSet.getNameMap(), recordSet.getDescriptionMap(),
 					recordSet.getMinDisplayRows(), serviceContext);
+
+				if (ExportImportThreadLocal.isImportInProcess()) {
+					LocaleThreadLocal.setSiteDefaultLocale(siteLocale);
+				}
 			});
 
 		return actionableDynamicQuery;
