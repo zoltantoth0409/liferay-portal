b6fe292b69f6220340d61af4dc498e11c180c2b9
jpince <jason.pince@liferay.com>
2018-11-15 15:40:57 -0800

    Revert "LPS-84817 SF"
    This reverts commit 292c02a585355110c19db7d595b1f638324f6ea1.

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -4648,6 +4648,7 @@ public class PortalImpl implements Portal {
 			WebKeys.PORTLET_ID);
 
 		PortletConfig portletConfig = PortletConfigFactoryUtil.get(portletId);
+
 		Locale locale = portletRequest.getLocale();
 
 		String portletTitle = _getPortletTitle(
