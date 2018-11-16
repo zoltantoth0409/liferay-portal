9af7edd231d2e27b19917e1db65f8e5975550cdf
shuyangzhou <shuyang.zhou@liferay.com>
2018-11-16 15:18:11 -0800

    LPS-86406 Bring RetryAdvice back to internal

--- portal-impl/src/com/liferay/portal/spring/aop/AopConfigurableApplicationContextConfigurator.java
+++ portal-impl/src/com/liferay/portal/spring/aop/AopConfigurableApplicationContextConfigurator.java
@@ -24,8 +24,8 @@ import com.liferay.portal.search.IndexableAdvice;
 import com.liferay.portal.security.access.control.AccessControlAdvice;
 import com.liferay.portal.service.ServiceContextAdvice;
 import com.liferay.portal.spring.context.ConfigurableApplicationContextConfigurator;
-
 import com.liferay.portal.systemevent.SystemEventAdvice;
+
 import java.util.Map;
 
 import org.aopalliance.intercept.MethodInterceptor;
@@ -133,10 +133,14 @@ public class AopConfigurableApplicationContextConfigurator
 				configurableListableBeanFactory.getBean(
 					"serviceAdvice", MethodInterceptor.class);
 
+			RetryAdvice retryAdvice = new RetryAdvice();
+
+			retryAdvice.setNextMethodInterceptor(methodInterceptor);
+
 			ServiceContextAdvice serviceContextAdvice =
 				new ServiceContextAdvice();
 
-			serviceContextAdvice.setNextMethodInterceptor(methodInterceptor);
+			serviceContextAdvice.setNextMethodInterceptor(retryAdvice);
 
 			SystemEventAdvice systemEventAdvice = new SystemEventAdvice();
 
