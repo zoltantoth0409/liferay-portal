d98bc7f56375a1491d9799d518ca839f24fb5d3a
Istvan Sajtos <istvan.sajtos@liferay.com>
2018-11-16 18:27:09 +0100

    LPS-86740 Avoid creating big intermediate list

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/search/MBMessageIndexer.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/search/MBMessageIndexer.java
@@ -65,10 +65,8 @@ import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
@@ -383,23 +381,19 @@ public class MBMessageIndexer
 		MBMessage message = mbMessageLocalService.getMessage(classPK);
 
 		if (message.isRoot()) {
-			List<MBMessage> messages = new LinkedList<>();
+			for (MBMessage curMessage :
+					mbMessageLocalService.getThreadMessages(
+						message.getThreadId(),
+						WorkflowConstants.STATUS_APPROVED)) {
 
-			Optional.ofNullable(
-				mbMessageLocalService.getThreadMessages(
-					message.getThreadId(), WorkflowConstants.STATUS_APPROVED)
-			).ifPresent(
-				messages::addAll
-			);
+				reindex(curMessage);
+			}
 
-			Optional.ofNullable(
-				mbMessageLocalService.getThreadMessages(
-					message.getThreadId(), WorkflowConstants.STATUS_IN_TRASH)
-			).ifPresent(
-				messages::addAll
-			);
+			for (MBMessage curMessage :
+					mbMessageLocalService.getThreadMessages(
+						message.getThreadId(),
+						WorkflowConstants.STATUS_IN_TRASH)) {
 
-			for (MBMessage curMessage : messages) {
 				reindex(curMessage);
 			}
 		}
