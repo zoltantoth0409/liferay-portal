9a20ec9fe66a0329c8a9eb950714a8dab22796cf
Clóvis Neto <cloves13@hotmail.com>
2018-11-16 15:38:50 -0300

    LPS-87121 Add test coverage on Editor field

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/metal/js/Editor/Editor.es.js
@@ -0,0 +1,182 @@
+import Editor from 'source/Editor/Editor.es';
+
+let component;
+const spritemap = 'icons.svg';
+
+const defaultEditorConfig = {
+	name: 'textField',
+	spritemap
+};
+
+describe(
+	'Field Editor',
+	() => {
+		afterEach(
+			() => {
+				if (component) {
+					component.dispose();
+				}
+			}
+		);
+
+		it(
+			'should be readOnly',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						readOnly: true
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a helptext',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						tip: 'Type something'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have an id',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						id: 'ID'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a label',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						label: 'label'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a placeholder',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						placeholder: 'Placeholder'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should not be required',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						required: false
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should render Label if showLabel is true',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						label: 'text',
+						showLabel: true
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a spritemap',
+			() => {
+				component = new Editor(defaultEditorConfig);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a value',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						value: 'value'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a key',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig,
+						key: 'key'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should emit a change value when onChangeEditor method is triggered',
+			() => {
+				component = new Editor(
+					{
+						...defaultEditorConfig
+					}
+				);
+				const event = {};
+				const spy = jest.spyOn(component, 'emit');
+
+				component._onChangeEditor(event);
+
+				expect(spy).toHaveBeenCalledWith(
+					'fieldEdited',
+					{
+						fieldInstance: component,
+						originalEvent: event,
+						value: ''
+					}
+				);
+			}
+		);
+	}
+);
