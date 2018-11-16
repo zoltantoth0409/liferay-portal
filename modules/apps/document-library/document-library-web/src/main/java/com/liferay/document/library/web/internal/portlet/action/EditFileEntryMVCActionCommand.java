8211cc86fcc39401e6ebd257d6124fbcf7de9b5f
Brian Chan <brian.chan@liferay.com>
2018-11-16 11:50:35 -0800

    LPS-86907 SF

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/EditFileEntryMVCActionCommand.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/EditFileEntryMVCActionCommand.java
@@ -887,6 +887,7 @@ public class EditFileEntryMVCActionCommand extends BaseMVCActionCommand {
 
 		boolean majorVersion = ParamUtil.getBoolean(
 			uploadPortletRequest, "majorVersion");
+
 		boolean updateVersionDetails = ParamUtil.getBoolean(
 			uploadPortletRequest, "updateVersionDetails");
 
