8e1b082edbd421219a5de370cf893079a558b3c2
Carlos Sierra <carlos.sierra@liferay.com>
2018-11-16 17:21:18 +0100

    LPS-87113 No need for immediate

--- modules/apps/portal-security-audit/portal-security-audit-storage-service/src/main/java/com/liferay/portal/security/audit/storage/internal/upgrade/AuditStorageServiceUpgrade.java
+++ modules/apps/portal-security-audit/portal-security-audit-storage-service/src/main/java/com/liferay/portal/security/audit/storage/internal/upgrade/AuditStorageServiceUpgrade.java
@@ -22,7 +22,7 @@ import org.osgi.service.component.annotations.Component;
 /**
  * @author Samuel Ziemer
  */
-@Component(immediate = true, service = UpgradeStepRegistrator.class)
+@Component(service = UpgradeStepRegistrator.class)
 public class AuditStorageServiceUpgrade implements UpgradeStepRegistrator {
 
 	@Override
