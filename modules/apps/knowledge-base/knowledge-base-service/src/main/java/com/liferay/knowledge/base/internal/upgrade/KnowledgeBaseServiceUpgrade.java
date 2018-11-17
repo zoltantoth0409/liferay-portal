44a796fa39a0859be9ab6769408a6b73d64b20d4
Brian Chan <brian.chan@liferay.com>
2018-11-16 21:40:04 -0800

    LPS-87113 Hugo SF, apply the same changes in this ticket (see previous commits) to KB. Hugo, perhaps we can scan for more? Thx.

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/upgrade/KnowledgeBaseServiceUpgrade.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/upgrade/KnowledgeBaseServiceUpgrade.java
@@ -26,7 +26,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Adolfo Pérez
  */
-@Component(immediate = true, service = UpgradeStepRegistrator.class)
+@Component(service = UpgradeStepRegistrator.class)
 public class KnowledgeBaseServiceUpgrade implements UpgradeStepRegistrator {
 
 	@Override
