e3a4c1ea9b13fefda490a9ded187fe612bfd9ad6
Francisco Dias <francisco.dias@liferay.com>
2018-11-20 16:04:26 -0300

    LPS-84098 Install browsersync and insert files to watch files in develop mode

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/reloader.js
@@ -0,0 +1,79 @@
+(
+	function(bs) {
+		let resetModules = function(moduleName) {
+			Object.keys(Liferay.MODULES).forEach(
+				function(currentModule) {
+					let moduleCache = Liferay.MODULES[currentModule];
+
+					if (moduleCache.name.indexOf('.soy') > -1) {
+						return;
+					}
+
+					if (moduleCache.name.indexOf(moduleName) > -1) {
+						delete moduleCache.implementation;
+						delete moduleCache.pendingImplementation;
+
+						moduleCache.requested = false;
+					}
+				}
+			);
+		};
+
+		bs.socket.on(
+			'building',
+			function(data) {
+				new Liferay.Notification(
+					{
+						delay: {
+							hide: 5000,
+							show: 0,
+						},
+						message: 'Building ' + data.fileName,
+						type: 'info',
+					}
+				).render();
+			}
+		);
+		bs.socket.on(
+			'error',
+			function(data) {
+				new Liferay.Notification(
+					{
+						message: data.error,
+						title: data.fileName,
+						type: 'danger',
+					}
+				).render();
+			}
+		);
+		bs.socket.on(
+			'done',
+			function(data) {
+				new Liferay.Notification(
+					{
+						delay: {
+							hide: 5000,
+							show: 0,
+						},
+						message: 'Reloaded ' + data.fileName,
+						type: 'success',
+					}
+				).render();
+
+				if (data.fileName.endsWith('.soy')) {
+					return false;
+				}
+				else if (data.fileName.endsWith('.soy.js')) {
+					window.location.reload();
+				}
+				else {
+					resetModules(data.moduleName);
+
+					if (Liferay.Forms.App) {
+						Liferay.Forms.App.reset();
+					}
+				}
+			}
+		);
+	}
+)(___browserSync___);
