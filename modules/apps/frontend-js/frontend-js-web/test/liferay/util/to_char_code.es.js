3f7dd2c53a3c083b926e93fcaf9f96f5cec02e6e
Julien Castelain <julien.castelain@liferay.com>
2018-11-20 11:33:06 +0100

    LPS-87254 Remove lodash usage from frontend-js-aui-web

---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/to_char_code.es.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import toCharCode from '../../../src/main/resources/META-INF/resources/liferay/util/to_char_code.es';
+
+describe(
+	'Liferay.Util.toCharCode',
+	() => {
+
+		it(
+			'should return string',
+			() => {
+				const result = toCharCode('liferay');
+
+				expect(typeof result).toBe('string');
+				expect(result).toMatchSnapshot();
+			}
+		);
+
+	}
+);
