584349f6d695976f565dc9786eb47d7940a6536b
Francisco Dias <francisco.dias@liferay.com>
2018-11-20 16:40:08 -0300

    LPS-84098 Create Validation test

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/metal/js/Validation/Validation.es.js
@@ -0,0 +1,265 @@
+import Validation from 'source/Validation/Validation.es';
+import {dom as MetalTestUtil} from 'metal-dom';
+
+let component;
+const spritemap = 'icons.svg';
+
+describe('Validation', () => {
+	afterEach(() => {
+		if (component) {
+			component.dispose();
+		}
+	});
+
+	it('should render checkbox to enable Validation', () => {
+		component = new Validation({
+			dataType: 'string',
+			name: 'validation',
+			spritemap,
+		});
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should enable validation after click on toogle', () => {
+		jest.useFakeTimers();
+
+		component = new Validation({
+			dataType: 'string',
+			name: 'validation',
+			spritemap,
+		});
+
+		MetalTestUtil.triggerEvent(
+			component.element.querySelector('input[type=checkbox]'),
+			'change',
+			{}
+		);
+
+		jest.runAllTimers();
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render parameter field with TextField element', () => {
+		jest.useFakeTimers();
+
+		component = new Validation({
+			dataType: 'string',
+			enableValidation: true,
+			name: 'validation',
+			spritemap,
+		});
+
+		component.handleValidationValue({value: ['notContains']});
+
+		jest.runAllTimers();
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render parameter field with Numeric element', () => {
+		jest.useFakeTimers();
+
+		component = new Validation({
+			dataType: 'numeric',
+			enableValidation: true,
+			name: 'validation',
+			spritemap,
+		});
+
+		component.handleValidationValue({value: ['lt']});
+
+		jest.runAllTimers();
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render parameter field with Text element and then with Numeric after update dataType', () => {
+		jest.useFakeTimers();
+
+		component = new Validation({
+			dataType: 'string',
+			enableValidation: true,
+			name: 'validation',
+			spritemap,
+			validation: {},
+		});
+
+		component.handleValidationValue({value: ['notContains']});
+
+		jest.runAllTimers();
+
+		expect(component).toMatchSnapshot();
+
+		component.dataType = 'numeric';
+
+		jest.runAllTimers();
+
+		component.handleValidationValue({value: ['lt']});
+
+		jest.runAllTimers();
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should not render parameter field if validation is isUrl', () => {
+		jest.useFakeTimers();
+
+		component = new Validation({
+			dataType: 'string',
+			enableValidation: true,
+			name: 'validation',
+			spritemap,
+		});
+
+		component.handleValidationValue({value: ['url']});
+
+		jest.runAllTimers();
+
+		expect(component).toMatchSnapshot();
+	});
+	it('should not render parameter field if validation is isEmailAddress', () => {
+		jest.useFakeTimers();
+
+		component = new Validation({
+			dataType: 'string',
+			enableValidation: true,
+			name: 'validation',
+			spritemap,
+		});
+
+		component.handleValidationValue({value: ['email']});
+
+		jest.runAllTimers();
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should render filled fields when value is filled', () => {
+		jest.useFakeTimers();
+
+		component = new Validation({
+			dataType: 'string',
+			name: 'validation',
+			value: {
+				expression: 'NOT(contains(textfield, "forbiddenWord"))',
+				errorMessage: 'Error message',
+			},
+			spritemap,
+		});
+
+		jest.runAllTimers();
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should update parameter message and emit a field edited event', () => {
+		jest.useFakeTimers();
+
+		const handleFieldEdited = jest.fn();
+		const events = {fieldEdited: handleFieldEdited};
+
+		component = new Validation({
+			dataType: 'string',
+			enableValidation: true,
+			events,
+			name: 'validation',
+			value: {
+				expression: 'NOT(contains(textfield, "forbiddenWord"))',
+				errorMessage: 'Error message',
+			},
+			focusedField: {
+				fieldName: 'textfield',
+			},
+			spritemap,
+		});
+
+		jest.runAllTimers();
+
+		const parameterComponent = component.element.querySelector(
+			'input[name="validation_parameterMessage"]'
+		);
+
+		parameterComponent.value = 'new Parameter Message';
+
+		MetalTestUtil.triggerEvent(parameterComponent, 'input', {});
+
+		jest.runAllTimers();
+
+		expect(handleFieldEdited).toHaveBeenCalled();
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should update error message and emit a field edited event', () => {
+		jest.useFakeTimers();
+
+		const handleFieldEdited = jest.fn();
+		const events = {fieldEdited: handleFieldEdited};
+
+		component = new Validation({
+			dataType: 'string',
+			enableValidation: true,
+			events,
+			name: 'validation',
+			value: {
+				expression: 'NOT(contains(textfield, "forbiddenWord"))',
+				errorMessage: 'Error message',
+			},
+			focusedField: {
+				fieldName: 'textfield',
+			},
+			spritemap,
+		});
+
+		jest.runAllTimers();
+
+		const errorComponent = component.element.querySelector(
+			'input[name="validation_errorMessage"]'
+		);
+
+		errorComponent.value = 'new Error Message';
+
+		MetalTestUtil.triggerEvent(errorComponent, 'input', {});
+
+		jest.runAllTimers();
+
+		expect(handleFieldEdited).toHaveBeenCalled();
+		expect(component).toMatchSnapshot();
+	});
+
+	it('should propagate the field edit event', () => {
+		jest.useFakeTimers();
+
+		component = new Validation({
+			dataType: 'string',
+			enableValidation: true,
+			name: 'validation',
+			value: {
+				expression: 'NOT(contains(textfield, "forbiddenWord"))',
+				errorMessage: 'Error message',
+			},
+			focusedField: {
+				fieldName: 'textfield',
+			},
+			spritemap,
+		});
+
+		const spy = jest.spyOn(component, 'emit');
+
+		jest.runAllTimers();
+
+		const errorComponent = component.element.querySelector(
+			'input[name="validation_errorMessage"]'
+		);
+
+		errorComponent.value = 'new Error Message';
+
+		MetalTestUtil.triggerEvent(errorComponent, 'input', {});
+
+		jest.runAllTimers();
+
+		expect(spy).toHaveBeenCalled();
+		expect(spy).toHaveBeenCalledWith('fieldEdited', expect.any(Object));
+	});
+});
