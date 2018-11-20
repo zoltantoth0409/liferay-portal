9920b19dcaa1dc88234b0db6ae0574b6ed9f4e4a
Francisco Dias <francisco.dias@liferay.com>
2018-11-20 16:09:25 -0300

    LPS-84098 Create strings and validations util

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/metal/util/strings.es.js
@@ -0,0 +1,43 @@
+export const capitalize = text => {
+	return text.charAt(0).toUpperCase() + text.slice(1);
+};
+
+const SPLIT_REGEX = /({\d+})/g;
+const SPLIT_WORDS_REGEX = /({\w+})/g;
+
+export function sub(langKey, args) {
+	const keyArray = langKey.split(SPLIT_REGEX).filter(val => val.length !== 0);
+
+	for (let i = 0; i < args.length; i++) {
+		const arg = args[i];
+
+		const indexKey = `{${i}}`;
+
+		let argIndex = keyArray.indexOf(indexKey);
+
+		while (argIndex >= 0) {
+			keyArray.splice(argIndex, 1, arg);
+
+			argIndex = keyArray.indexOf(indexKey);
+		}
+	}
+
+	return keyArray.join('');
+}
+
+export function subWords(langKey, args) {
+	const keyArray = langKey.split(SPLIT_WORDS_REGEX).filter(val => val.length !== 0);
+
+	for (const arg in args) {
+		const indexKey = `{${arg}}`;
+		let argIndex = keyArray.indexOf(indexKey);
+
+		while (argIndex >= 0) {
+			keyArray.splice(argIndex, 1, args[arg]);
+
+			argIndex = keyArray.indexOf(indexKey);
+		}
+	}
+
+	return keyArray.join('');
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/metal/util/validations.es.js
@@ -0,0 +1,79 @@
+export const VALIDATIONS = {
+	numeric: [
+		{
+			label: Liferay.Language.get('is-greater-than-or-equal-to'),
+			name: 'lt',
+			parameterMessage: Liferay.Language.get('number-placeholder'),
+			regex: /^(.+)\<(\d+\.?\d*)$/,
+			template: '{name}<{parameter}',
+		},
+		{
+			label: Liferay.Language.get('is-greater-than'),
+			name: 'lteq',
+			parameterMessage: Liferay.Language.get('number-placeholder'),
+			regex: /^(.+)\<\=(\d+\.?\d*)$/,
+			template: '{name}<={parameter}',
+		},
+		{
+			label: Liferay.Language.get('is-not-equal-to'),
+			name: 'eq',
+			parameterMessage: Liferay.Language.get('number-placeholder'),
+			regex: /^(.+)\=\=(\d+\.?\d*)$/,
+			template: '{name}=={parameter}',
+		},
+		{
+			label: Liferay.Language.get('is-less-than-or-equal-to'),
+			name: 'gt',
+			parameterMessage: Liferay.Language.get('number-placeholder'),
+			regex: /^(.+)\>(\d+\.?\d*)$/,
+			template: '{name}>{parameter}',
+		},
+		{
+			label: Liferay.Language.get('is-less-than'),
+			name: 'gteq',
+			parameterMessage: Liferay.Language.get('number-placeholder'),
+			regex: /^(.+)\>\=(\d+\.?\d*)$/,
+			template: '{name}>={parameter}',
+		},
+
+	],
+	string: [
+		{
+			label: Liferay.Language.get('contains'),
+			name: 'notContains',
+			parameterMessage: Liferay.Language.get('text'),
+			regex: /^NOT\(contains\((.+), "(.+)"\)\)$/,
+			template: 'NOT(contains({name}, "{parameter}"))',
+		},
+		{
+			label: Liferay.Language.get('does-not-contain'),
+			name: 'contains',
+			parameterMessage: Liferay.Language.get('text'),
+			regex: /^contains\((.+), "(.+)"\)$/,
+			template: 'contains({name}, "{parameter}")',
+		},
+		{
+			label: Liferay.Language.get('is-not-url'),
+			name: 'url',
+			parameterMessage: '',
+			regex: /^isURL\((.+)\)$/,
+			template: 'isURL({name})',
+		},
+		{
+			label: Liferay.Language.get('is-not-email'),
+			name: 'email',
+			parameterMessage: '',
+			regex: /^isEmailAddress\((.+)\)$/,
+			template: 'isEmailAddress({name})',
+		},
+		{
+			label: Liferay.Language.get('does-not-match'),
+			name: 'regularExpression',
+			parameterMessage: Liferay.Language.get('regular-expression'),
+			regex: /^match\((.+), "(.*)"\)$/,
+			template: 'match({name}, "{parameter}")',
+		},
+	],
+};
+
+export default VALIDATIONS;
