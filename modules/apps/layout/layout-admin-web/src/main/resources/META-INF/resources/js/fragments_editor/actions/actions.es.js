332cfb6e92e346d3ba0f9d39b120793eb1a0e7d2
Pablo Molina <contact@pablomolina.me>
2018-11-21 12:36:19 +0100

    LPS-86007 Simplify actions.es.js

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/actions/actions.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/actions/actions.es.js
@@ -1,173 +1,19 @@
-/**
- * Add fragment entry link action type
- * @review
- * @type {string}
- */
-
-const ADD_FRAGMENT_ENTRY_LINK = 'addFragmentEntryLink';
-
-/**
- * Add section action type
- * @review
- * @type {string}
- */
-
-const ADD_SECTION = 'addSection';
-
-/**
- * Change languageId action type
- * @review
- * @type {string}
- */
-
-const CHANGE_LANGUAGE_ID = 'languageId';
-
-/**
- * Clear drag target action type
- * @review
- * @type {string}
- */
-
-const CLEAR_DRAG_TARGET = 'clearDragTarget';
-
-/**
- * Hide mapping type dialog
- * @review
- * @type {string}
- */
-
-const HIDE_MAPPING_TYPE_DIALOG = 'hideMappingTypeDialog';
-
-/**
- * Hide Fragments editor sidebar
- * @review
- * @type {string}
- */
-
-const HIDE_SIDEBAR = 'hideSidebar';
-
-/**
- * Move fragment entry link action type
- * @review
- * @type {string}
- */
-
-const MOVE_FRAGMENT_ENTRY_LINK = 'moveFragmentEntryLink';
-
-/**
- * Open asset type dialog
- * @review
- * @type {string}
- */
-
-const OPEN_ASSET_TYPE_DIALOG = 'openAssetTypeDialog';
-
-/**
- * Open mapping fields dialog
- * @review
- * @type {string}
- */
-
-const OPEN_MAPPING_FIELDS_DIALOG = 'openMappingFieldsDialog';
-
-/**
- * Remove fragment entry link action type
- * @review
- * @type {string}
- */
-
-const REMOVE_FRAGMENT_ENTRY_LINK = 'removeFragmentEntryLink';
-
-/**
- * Select mappeable field
- * @review
- * @type {string}
- */
-
-const SELECT_MAPPEABLE_FIELD = 'selectMappeableField';
-
-/**
- * Toggle Fragments editor sidebar
- * @review
- * @type {string}
- */
-
-const TOGGLE_SIDEBAR = 'toggleSidebar';
-
-/**
- * Select mappeable type
- * @review
- * @type {string}
- */
-
-const SELECT_MAPPEABLE_TYPE = 'selectMappeableType';
-
-/**
- * Update drag target action type
- * @review
- * @type {string}
- */
-
-const UPDATE_DRAG_TARGET = 'updateDragTarget';
-
-/**
- * Update editable value action type
- * @review
- * @type {string}
- */
-
-const UPDATE_EDITABLE_VALUE = 'updateEditableValue';
-
-/**
- * Update highlight mapping status action type
- * @review
- * @type {string}
- */
-
-const UPDATE_HIGHLIGHT_MAPPING_STATUS = 'updateHighlightMappingStatus';
-
-/**
- * Update last save date action type
- * @review
- * @type {string}
- */
-
-const UPDATE_LAST_SAVE_DATE = 'updateLastSaveDate';
-
-/**
- * Update saving changes status action type
- * @review
- * @type {string}
- */
-
-const UPDATE_SAVING_CHANGES_STATUS = 'updateSavingChangesStatus';
-
-/**
- * Update translation status action type
- * @review
- * @type {string}
- */
-
-const UPDATE_TRANSLATION_STATUS = 'updateTranslationStatus';
-
-export {
-	ADD_FRAGMENT_ENTRY_LINK,
-	ADD_SECTION,
-	CHANGE_LANGUAGE_ID,
-	CLEAR_DRAG_TARGET,
-	HIDE_SIDEBAR,
-	MOVE_FRAGMENT_ENTRY_LINK,
-	HIDE_MAPPING_TYPE_DIALOG,
-	OPEN_ASSET_TYPE_DIALOG,
-	OPEN_MAPPING_FIELDS_DIALOG,
-	REMOVE_FRAGMENT_ENTRY_LINK,
-	TOGGLE_SIDEBAR,
-	SELECT_MAPPEABLE_FIELD,
-	SELECT_MAPPEABLE_TYPE,
-	UPDATE_DRAG_TARGET,
-	UPDATE_EDITABLE_VALUE,
-	UPDATE_HIGHLIGHT_MAPPING_STATUS,
-	UPDATE_LAST_SAVE_DATE,
-	UPDATE_SAVING_CHANGES_STATUS,
-	UPDATE_TRANSLATION_STATUS
-};
+export const ADD_FRAGMENT_ENTRY_LINK = 'addFragmentEntryLink';
+export const ADD_SECTION = 'addSection';
+export const CHANGE_LANGUAGE_ID = 'languageId';
+export const CLEAR_DRAG_TARGET = 'clearDragTarget';
+export const HIDE_MAPPING_TYPE_DIALOG = 'hideMappingTypeDialog';
+export const HIDE_SIDEBAR = 'hideSidebar';
+export const MOVE_FRAGMENT_ENTRY_LINK = 'moveFragmentEntryLink';
+export const OPEN_ASSET_TYPE_DIALOG = 'openAssetTypeDialog';
+export const OPEN_MAPPING_FIELDS_DIALOG = 'openMappingFieldsDialog';
+export const REMOVE_FRAGMENT_ENTRY_LINK = 'removeFragmentEntryLink';
+export const SELECT_MAPPEABLE_FIELD = 'selectMappeableField';
+export const TOGGLE_SIDEBAR = 'toggleSidebar';
+export const SELECT_MAPPEABLE_TYPE = 'selectMappeableType';
+export const UPDATE_DRAG_TARGET = 'updateDragTarget';
+export const UPDATE_EDITABLE_VALUE = 'updateEditableValue';
+export const UPDATE_HIGHLIGHT_MAPPING_STATUS = 'updateHighlightMappingStatus';
+export const UPDATE_LAST_SAVE_DATE = 'updateLastSaveDate';
+export const UPDATE_SAVING_CHANGES_STATUS = 'updateSavingChangesStatus';
+export const UPDATE_TRANSLATION_STATUS = 'updateTranslationStatus';
