669cfd50692f736024cb51f40fe78c84f99262c8
shuyangzhou <shuyang.zhou@liferay.com>
2018-11-20 19:16:31 -0800

    LPS-86406 Use a hack replacement of LiferayBeanFactory to detach from com.liferay.portal.spring.bean package

--- modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/context/ModuleApplicationContext.java
+++ modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/context/ModuleApplicationContext.java
@@ -14,8 +14,6 @@
 
 package com.liferay.portal.spring.extender.internal.context;
 
-import com.liferay.portal.spring.bean.LiferayBeanFactory;
-
 import java.net.URL;
 
 import java.util.ArrayList;
@@ -25,7 +23,10 @@ import java.util.List;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -65,7 +66,51 @@ public class ModuleApplicationContext extends ClassPathXmlApplicationContext {
 
 	@Override
 	protected DefaultListableBeanFactory createBeanFactory() {
-		return new LiferayBeanFactory(getInternalParentBeanFactory());
+		return new DefaultListableBeanFactory(getInternalParentBeanFactory()) {
+
+			@Override
+			protected Object getEarlyBeanReference(
+				String beanName, RootBeanDefinition rootBeanDefinition,
+				Object bean) {
+
+				if ((bean == null) || rootBeanDefinition.isSynthetic()) {
+					return bean;
+				}
+
+				Object exposedObject = bean;
+
+				for (BeanPostProcessor beanPostProcessor :
+						getBeanPostProcessors()) {
+
+					if (!(beanPostProcessor instanceof
+							SmartInstantiationAwareBeanPostProcessor)) {
+
+						continue;
+					}
+
+					SmartInstantiationAwareBeanPostProcessor
+						smartInstantiationAwareBeanPostProcessor =
+							(SmartInstantiationAwareBeanPostProcessor)
+								beanPostProcessor;
+
+					exposedObject =
+						smartInstantiationAwareBeanPostProcessor.
+							getEarlyBeanReference(exposedObject, beanName);
+
+					if (exposedObject == null) {
+						return exposedObject;
+					}
+				}
+
+				return exposedObject;
+			}
+
+			@Override
+			protected boolean hasInstantiationAwareBeanPostProcessors() {
+				return false;
+			}
+
+		};
 	}
 
 	protected final Bundle bundle;
