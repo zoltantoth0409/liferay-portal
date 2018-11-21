ef614b86764c6a118a145c0b227fa68d287d71f4
marcelabc <mbc3@cin.ufpe.br>
2018-11-21 11:01:51 -0300

    LPS-87089 Apply new model indexer to DDLRecordSet

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/search/spi/model/index/contributor/DDLRecordSetModelIndexerWriterContributor.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.internal.search.spi.model.index.contributor;
+
+import com.liferay.dynamic.data.lists.internal.search.util.DDLRecordBatchReindexer;
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalService;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcela Cunha
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.dynamic.data.lists.model.DDLRecordSet",
+	service = ModelIndexerWriterContributor.class
+)
+public class DDLRecordSetModelIndexerWriterContributor
+	implements ModelIndexerWriterContributor<DDLRecordSet> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(DDLRecordSet recordSet) -> {
+				batchIndexingActionable.addDocuments(
+					modelIndexerWriterDocumentHelper.getDocument(recordSet));
+			});
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				recordSetLocalService.getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(DDLRecordSet recordSet) {
+		return recordSet.getCompanyId();
+	}
+
+	@Override
+	public void modelIndexed(DDLRecordSet recordSet) {
+		recordBatchReindexer.reindex(
+			recordSet.getRecordSetId(), recordSet.getCompanyId());
+	}
+
+	@Reference
+	protected DynamicQueryBatchIndexingActionableFactory
+		dynamicQueryBatchIndexingActionableFactory;
+
+	@Reference
+	protected DDLRecordBatchReindexer recordBatchReindexer;
+
+	@Reference
+	protected DDLRecordSetLocalService recordSetLocalService;
+
+}
---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/search/util/DDLRecordBatchReindexerImpl.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.internal.search.util;
+
+import com.liferay.dynamic.data.lists.model.DDLRecord;
+import com.liferay.dynamic.data.lists.service.DDLRecordLocalService;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.indexer.IndexerDocumentBuilder;
+import com.liferay.portal.search.indexer.IndexerWriter;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcela Cunha
+ */
+@Component(immediate = true, service = DDLRecordBatchReindexer.class)
+public class DDLRecordBatchReindexerImpl implements DDLRecordBatchReindexer {
+
+	@Override
+	public void reindex(long recordId, long companyId) {
+		BatchIndexingActionable batchIndexingActionable =
+			indexerWriter.getBatchIndexingActionable();
+
+		batchIndexingActionable.setAddCriteriaMethod(
+			dynamicQuery -> {
+				Property recordIdPropery = PropertyFactoryUtil.forName(
+					"recordId");
+
+				dynamicQuery.add(recordIdPropery.eq(recordId));
+			});
+		batchIndexingActionable.setCompanyId(companyId);
+		batchIndexingActionable.setPerformActionMethod(
+			(DDLRecord record) -> {
+				Document document = indexerDocumentBuilder.getDocument(record);
+
+				batchIndexingActionable.addDocuments(document);
+			});
+
+		batchIndexingActionable.performActions();
+	}
+
+	@Reference
+	protected DDLRecordLocalService formInstanceRecordLocalService;
+
+	@Reference(
+		target = "(indexer.class.name=com.liferay.dynamic.data.lists.model.DDLRecord)"
+	)
+	protected IndexerDocumentBuilder indexerDocumentBuilder;
+
+	@Reference(
+		target = "(indexer.class.name=com.liferay.dynamic.data.lists.model.DDLRecord)"
+	)
+	protected IndexerWriter<DDLRecord> indexerWriter;
+
+}
