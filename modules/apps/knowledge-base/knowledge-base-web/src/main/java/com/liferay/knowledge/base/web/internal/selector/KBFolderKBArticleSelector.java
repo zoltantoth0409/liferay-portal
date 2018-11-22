ef9ecf270800c25897fed6567bc123202289c420
David Zhang <david.zhang@liferay.com>
2018-11-22 11:13:36 +0800

    LPS-87425 Apply the new method in order to transfer "status" field

--- modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/selector/KBFolderKBArticleSelector.java
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/selector/KBFolderKBArticleSelector.java
@@ -188,7 +188,8 @@ public class KBFolderKBArticleSelector implements KBArticleSelector {
 		}
 
 		KBArticle kbArticle = _kbArticleService.fetchFirstChildKBArticle(
-			groupId, kbFolder.getKbFolderId());
+			groupId, kbFolder.getKbFolderId(),
+			WorkflowConstants.STATUS_APPROVED);
 
 		return new KBArticleSelection(kbArticle, true);
 	}
