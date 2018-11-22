79c343a237f9e7e9ec2de9dd4fe5b07f2fb675ec
Jorge González <jorge.gonzalez@liferay.com>
2018-11-22 16:17:26 +0100

    LPS-85934 Add new PageInput and handles

--- modules/apps/document-library/document-library-preview-pdf/src/main/resources/META-INF/resources/preview/js/PdfPreviewer.es.js
+++ modules/apps/document-library/document-library-preview-pdf/src/main/resources/META-INF/resources/preview/js/PdfPreviewer.es.js
@@ -5,11 +5,26 @@ import Soy from 'metal-soy';
 import templates from './PdfPreviewer.soy';
 
 /**
+ * Valid list of keycodes
+ * Includes backspace, tab, enter, escape, arrows, delete and numbers
+ * @type {Array<number>}
+ */
+const validKeyCodes = [8, 9, 13, 37, 38, 39, 40, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+
+/**
  * Component that create an pdf preview
  * @review
  */
 class PdfPreviewer extends Component {
 
+	/**
+	 * @inheritDoc
+	 */
+	rendered() {
+		if (this.showPageInput) {
+			this.refs.pageInput.focus();
+		}
+	}
 
 	/**
 	 * @inheritDoc
@@ -20,37 +35,68 @@ class PdfPreviewer extends Component {
 		this.nextPageDisabled = currentPage === this.totalPages;
 	}
 
-	_askPageNumber() {
-		const response = parseInt(prompt(), 0);
+	/**
+	 * Set the current page if is valid page
+	 * @param {number|string} page
+	 * @private
+	 * @review
+	 */
+	_setPage(page) {
+		const pageNumber = Number.parseInt(page, 10);
 
-		return Number.isInteger(response) ?
-			this._normalizePage(response) :
-			null
-		;
+		if (pageNumber) {
+			this.currentPage = Math.min(
+				Math.max(1, pageNumber),
+				this.totalPages
+			);
+		}
 	}
 
-	_normalizePage(page) {
-		return Math.min(
-			Math.max(1, page),
-			this.totalPages
-		);
+	/**
+	 * Event handler executed in pageInput blur
+	 * Save the current value
+	 * @param {!Event} event
+	 * @private
+	 * @review
+	 */
+	_handleBlurPageInput(event) {
+		this._setPage(event.delegateTarget.value);
+		this.showPageInput = false;
 	}
 
-	_handleClickGoToPage(event) {
+	/**
+	 * Prevents from introducing non digits in input field.
+	 * And map certain acctions to escape enter (save) or (cancel)
+	 * @param {KeyboardEvent} event The keyboard event.
+	 * @private
+	 * @review
+	 */
+	_handleKeyDownPageInput(event) {
+		const code = event.keyCode || event.charCode;
+
+		if (code === 13) {
+			this._setPage(event.delegateTarget.value);
+			this.showPageInput = false;
+		}
+		else if (code === 27) {
+			this.showPageInput = false;
+		}
+ 		else if (validKeyCodes.indexOf(code) == -1) {
+			event.preventDefault();
+		}
+	}
+
+	_handleClickToolbar(event) {
 		const action = event.currentTarget.value;
 
 		if (action === 'next') {
-			this.currentPage = this.currentPage + 1;
+			this._setPage(this.currentPage + 1);
 		}
 		else if (action === 'previous') {
-			this.currentPage = this.currentPage - 1;
+			this._setPage(this.currentPage - 1);
 		}
-		else if (action === 'ask') {
-			const page = this._askPageNumber();
-
-			if (page) {
-				this.currentPage = page;
-			}
+		else if (action === 'go') {
+			this.showPageInput = true;
 		}
 	}
 }
@@ -86,6 +132,12 @@ PdfPreviewer.STATE = {
 	 * @type {Boolean}
 	 */
 	previousPageDisabled: Config.bool(),
+
+	/**
+	 * Flag that indicate if 'pageInput' is visible.
+	 * @type {Boolean}
+	 */
+	showPageInput: Config.bool(),
 
 	/**
 	 * Path to icon images.
