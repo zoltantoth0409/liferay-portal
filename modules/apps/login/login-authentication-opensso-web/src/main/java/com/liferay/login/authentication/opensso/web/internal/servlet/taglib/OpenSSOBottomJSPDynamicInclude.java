1957db116f31001baa476ad8549abed9ace00374
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2018-11-23 11:01:05 +0000

    LPS-86003 New OpenSSO web module to render floating error messages when a policy is violated or other user creation issue occurs

---
+++ modules/apps/login/login-authentication-opensso-web/src/main/java/com/liferay/login/authentication/opensso/web/internal/servlet/taglib/OpenSSOBottomJSPDynamicInclude.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.login.authentication.opensso.web.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.exception.ContactNameException;
+import com.liferay.portal.kernel.exception.UserEmailAddressException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.sso.OpenSSO;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.servlet.taglib.BaseJSPDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.sso.opensso.StrangersNotAllowedException;
+import com.liferay.portal.security.sso.opensso.configuration.OpenSSOConfiguration;
+import com.liferay.portal.security.sso.opensso.constants.OpenSSOConstants;
+import com.liferay.portal.security.sso.opensso.constants.OpenSSOWebKeys;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Stian Sigvartsen
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class OpenSSOBottomJSPDynamicInclude extends BaseJSPDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		long companyId = _portal.getCompanyId(request);
+
+		try {
+			OpenSSOConfiguration openSSOConfiguration = getOpenSSOConfiguration(
+				companyId);
+
+			if (!openSSOConfiguration.enabled()) {
+				return;
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			return;
+		}
+
+		HttpServletRequest originalHttpServletRequest =
+			_portal.getOriginalServletRequest(request);
+
+		String error = (String)originalHttpServletRequest.getAttribute(
+			OpenSSOWebKeys.OPEN_SSO_ERROR);
+
+		if (Validator.isBlank(error)) {
+			return;
+		}
+
+		originalHttpServletRequest.removeAttribute(
+			OpenSSOWebKeys.OPEN_SSO_ERROR);
+
+		if (ArrayUtil.contains(_ERRORS, error)) {
+			SessionMessages.add(request, error);
+		}
+
+		super.include(request, response, key);
+	}
+
+	@Override
+	public void register(
+		DynamicInclude.DynamicIncludeRegistry dynamicIncludeRegistry) {
+
+		dynamicIncludeRegistry.register("/html/common/themes/bottom.jsp#post");
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/dynamic_include/com.liferay.portal/opensso_error.jsp";
+	}
+
+	@Override
+	protected Log getLog() {
+		return _log;
+	}
+
+	protected OpenSSOConfiguration getOpenSSOConfiguration(long companyId)
+		throws Exception {
+
+		return _configurationProvider.getConfiguration(
+			OpenSSOConfiguration.class,
+			new CompanyServiceSettingsLocator(
+				companyId, OpenSSOConstants.SERVICE_NAME));
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.login.authentication.opensso.web)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	private static final String[] _ERRORS = {
+		ContactNameException.class.getSimpleName(),
+		PrincipalException.MustBeAuthenticated.class.getSimpleName(),
+		StrangersNotAllowedException.class.getSimpleName(),
+		UserEmailAddressException.MustNotUseCompanyMx.class.getSimpleName()
+
+	};
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		OpenSSOBottomJSPDynamicInclude.class);
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private OpenSSO _openSSO;
+
+	@Reference
+	private Portal _portal;
+
+}
---
+++ modules/apps/login/login-authentication-opensso-web/src/main/resources/META-INF/resources/dynamic_include/com.liferay.portal/opensso_error.jsp
@@ -0,0 +1,65 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/dynamic_include/init.jsp" %>
+
+<%
+String openSSOSubjectScreenName = (String)request.getAttribute("open.sso.subject.screenName");
+%>
+
+<liferay-util:buffer
+	var="msg"
+>
+	<liferay-ui:message arguments='<%= "<strong>" + HtmlUtil.escape(openSSOSubjectScreenName) + "</strong>" %>' key="your-user-x-could-not-be-logged-in" />
+
+	<c:choose>
+		<c:when test='<%= SessionMessages.contains(request, "MustNotUseCompanyMx") %>'>
+			<liferay-ui:message key="the-email-address-associated-with-your-opensso-account-cannot-be-used-to-register-a-new-user-because-its-email-domain-is-reserved" />
+		</c:when>
+		<c:when test='<%= SessionMessages.contains(request, "StrangersNotAllowedException") %>'>
+			<liferay-ui:message key="only-known-users-are-allowed-to-sign-in-using-opensso" />
+		</c:when>
+		<c:when test='<%= SessionMessages.contains(request, "ContactNameException") %>'>
+			<liferay-ui:message key="your-contact-name-is-incomplete-or-invalid" />
+		</c:when>
+	</c:choose>
+
+	<a href="<%= themeDisplay.getURLSignOut() %>"><liferay-ui:message arguments='<%= "<strong>" + HtmlUtil.escapeAttribute(openSSOSubjectScreenName) + "</strong>" %>' key="not-x" /></a>
+</liferay-util:buffer>
+
+<script type="text/javascript">
+	// <![CDATA[
+	AUI().use(
+		'liferay-notification',
+		function(A) {
+			new Liferay.Notification(
+				{
+					closeable: true,
+					delay: {
+						hide: 10000,
+						show: 0
+					},
+					duration: 500,
+					message: '<%= HtmlUtil.escapeJS(msg) %>',
+					render: true,
+					title: '<liferay-ui:message key="warning" />',
+					type: 'warning'
+				}
+			).render('body');
+		}
+	);
+	// ]]>
+</script>
