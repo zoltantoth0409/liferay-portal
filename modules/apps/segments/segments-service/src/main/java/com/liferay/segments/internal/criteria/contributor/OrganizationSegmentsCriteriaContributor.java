4ab6366cb9c36a16da2633c2163bad8306421c2b
Eduardo Garcia <eduardo.garcia@liferay.com>
2018-11-23 15:14:23 +0100

    LPS-87498 Add separate contribution for organization entity model

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/criteria/contributor/OrganizationSegmentsCriteriaContributor.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.criteria.contributor;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.segments.criteria.Criteria;
+import com.liferay.segments.criteria.contributor.SegmentsCriteriaContributor;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * @author Eduardo Garcia
+ */
+@Component(
+	immediate = true,
+	property = {
+		"segments.criteria.contributor.key=" + OrganizationSegmentsCriteriaContributor.KEY,
+		"segments.criteria.contributor.model.class.name=com.liferay.portal.kernel.model.Organization",
+		"segments.criteria.contributor.priority:Integer=50"
+	},
+	service = SegmentsCriteriaContributor.class
+)
+public class OrganizationSegmentsCriteriaContributor
+	implements SegmentsCriteriaContributor {
+
+	public static final String KEY = "organization";
+
+	@Override
+	public void contribute(
+		Criteria criteria, String filterString,
+		Criteria.Conjunction conjunction) {
+
+		criteria.addCriterion(getKey(), filterString, conjunction);
+		criteria.addFilter(filterString, conjunction);
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, getKey());
+	}
+
+}
