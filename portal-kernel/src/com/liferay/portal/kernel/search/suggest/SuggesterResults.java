7aab5a337656dae9b4020c9bd119beb03e28a88c
Michael Han <michael.han@liferay.com>
2018-11-26 21:33:33 -0600

    LPS-87915 portal-kernel: Deprecate
    https://issues.liferay.com/browse/LPS-87915

--- portal-kernel/src/com/liferay/portal/kernel/search/suggest/SuggesterResults.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/suggest/SuggesterResults.java
@@ -21,7 +21,11 @@ import java.util.Map;
 
 /**
  * @author Michael C. Han
+ * @deprecated As of Mueller (7.2.x), moved to {@link
+ *             com.liferay.portal.search.engine.adapter.search.
+ *             SuggestSearchResult}
  */
+@Deprecated
 public class SuggesterResults {
 
 	public void addSuggesterResult(SuggesterResult suggesterResult) {
