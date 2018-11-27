4d852ca7eb52958e2a53ef105302d974eba8e77a
Alan Huang <alan.huang@liferay.com>
2018-11-27 17:44:30 +0800

    LPS-87843 Directly and simply return the url if the url is an external url

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -3314,7 +3314,7 @@ public class PortalImpl implements Portal {
 			if (Validator.isNotNull(url) && !url.startsWith(StringPool.SLASH) &&
 				!url.startsWith(getPortalURL(layout, themeDisplay))) {
 
-				return getLayoutActualURL(layout);
+				return url;
 			}
 		}
 
