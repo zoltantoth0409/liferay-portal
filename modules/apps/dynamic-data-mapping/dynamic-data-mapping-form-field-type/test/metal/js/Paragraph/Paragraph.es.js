87990556536de2fb7e23df0670f3fc1099ab5407
Clóvis Neto <cloves13@hotmail.com>
2018-11-27 16:05:16 -0300

    LPS-85811 Create tests for paragraph field type

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/metal/js/Paragraph/Paragraph.es.js
@@ -0,0 +1,158 @@
+import Paragraph from 'source/Paragraph/Paragraph.es';
+
+let component;
+const spritemap = 'icons.svg';
+
+const defaultParagraphConfig = {
+	name: 'textField',
+	spritemap
+};
+
+describe(
+	'Field Paragraph',
+	() => {
+		afterEach(
+			() => {
+				if (component) {
+					component.dispose();
+				}
+			}
+		);
+
+		it(
+			'should be readOnly',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						readOnly: true
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a helptext',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						tip: 'Type something'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have an id',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						id: 'ID'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a label',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						label: 'label'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a placeholder',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						placeholder: 'Placeholder'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should not be required',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						required: false
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should render Label if showLabel is true',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						label: 'text',
+						showLabel: true
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a spritemap',
+			() => {
+				component = new Paragraph(defaultParagraphConfig);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a value',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						value: 'value'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should have a key',
+			() => {
+				component = new Paragraph(
+					{
+						...defaultParagraphConfig,
+						key: 'key'
+					}
+				);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+	}
+);
