889b3ef12bee99146f86ea411c2421b3b1b9c6e3
Raymond Auge <raymond.auge@liferay.com>
2018-11-28 18:38:33 -0500

    LPS-89601 Exceptions thrown when WABs quickly added/removed

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-context-helper-impl/src/main/java/com/liferay/portal/osgi/web/servlet/context/helper/internal/ServletContextHelperRegistrationImpl.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-context-helper-impl/src/main/java/com/liferay/portal/osgi/web/servlet/context/helper/internal/ServletContextHelperRegistrationImpl.java
@@ -116,18 +116,60 @@ public class ServletContextHelperRegistrationImpl
 
 	@Override
 	public void close() {
-		_servletContextRegistration.unregister();
+		try {
+			_servletContextRegistration.unregister();
+		}
+		catch (IllegalStateException ise) {
 
-		_servletContextHelperServiceRegistration.unregister();
+			// ignore since the service has been unregistered
 
-		_servletContextListenerServiceRegistration.unregister();
+		}
 
-		_defaultServletServiceRegistration.unregister();
+		try {
+			_servletContextHelperServiceRegistration.unregister();
+		}
+		catch (IllegalStateException ise) {
 
-		_jspServletServiceRegistration.unregister();
+			// ignore since the service has been unregistered
+
+		}
+
+		try {
+			_servletContextListenerServiceRegistration.unregister();
+		}
+		catch (IllegalStateException ise) {
+
+			// ignore since the service has been unregistered
+
+		}
+
+		try {
+			_defaultServletServiceRegistration.unregister();
+		}
+		catch (IllegalStateException ise) {
+
+			// ignore since the service has been unregistered
+
+		}
+
+		try {
+			_jspServletServiceRegistration.unregister();
+		}
+		catch (IllegalStateException ise) {
+
+			// ignore since the service has been unregistered
+
+		}
 
 		if (_portletServletServiceRegistration != null) {
-			_portletServletServiceRegistration.unregister();
+			try {
+				_portletServletServiceRegistration.unregister();
+			}
+			catch (IllegalStateException ise) {
+
+				// ignore since the service has been unregistered
+
+			}
 		}
 	}
 
