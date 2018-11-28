ca2ff59935b8f6c5c2f70cbf1e2553493fe54c60
rubenpulido <ruben.pulido@liferay.com>
2018-11-28 12:12:00 +0100

    LPS-85972 Create new Expression interfaces

---
+++ modules/apps/portal-odata/portal-odata-api/src/main/java/com/liferay/portal/odata/filter/expression/LambdaFunctionExpression.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.odata.filter.expression;
+
+/**
+ * Represents a lambda function expression in the expression tree
+ *
+ * @author Ruben Pulido
+ * @review
+ */
+public interface LambdaFunctionExpression extends Expression {
+
+	/**
+	 * Returns the child expression
+	 *
+	 * @return The child expression
+	 * @review
+	 */
+	public Expression getExpression();
+
+	/**
+	 * Returns the type of the lambda function expression
+	 *
+	 * @return The type
+	 * @review
+	 */
+	public Type getType();
+
+	/**
+	 * @return The name of the lambda variable
+	 * @review
+	 */
+	public String getVariableName();
+
+	public static enum Type {
+
+		ANY
+
+	}
+
+}
---
+++ modules/apps/portal-odata/portal-odata-api/src/main/java/com/liferay/portal/odata/filter/expression/LambdaVariableExpression.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.odata.filter.expression;
+
+/**
+ * Represents a lambda variable expression in the expression tree
+ *
+ * @author Ruben Pulido
+ * @review
+ */
+public interface LambdaVariableExpression extends Expression {
+
+	/**
+	 * Returns the name of the lambda variable
+	 *
+	 * @return The name of the lambda variable
+	 * @review
+	 */
+	public String getVariableName();
+
+}
