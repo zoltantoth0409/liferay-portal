0e4cec81636b69c320f76f702eef14b514bc1f7d
Inácio Nery <inacio.nery@liferay.com>
2018-11-28 17:44:49 -0300

    LPS-87614 Add Workflow Index Constants

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-reports/src/main/java/com/liferay/portal/workflow/reports/internal/constants/WorkflowIndexConstants.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.reports.internal.constants;
+
+/**
+ * @author Inácio Nery
+ */
+public class WorkflowIndexConstants {
+
+	public static final String INDEX_NAME_WORKFLOW_EVENTS = "workflow-events";
+
+	public static final String INDEX_NAME_WORKFLOW_INSTANCES =
+		"workflow-instances";
+
+	public static final String INDEX_NAME_WORKFLOW_PROCESSES =
+		"workflow-processes";
+
+	public static final String INDEX_NAME_WORKFLOW_TASKS = "workflow-tasks";
+
+	public static final String TYPE_MAPPING_FILE_NAME_WORKFLOW_EVENTS =
+		"/META-INF/mappings/workflow-events-type-mappings.json";
+
+	public static final String TYPE_MAPPING_FILE_NAME_WORKFLOW_INSTANCES =
+		"/META-INF/mappings/workflow-instances-type-mappings.json";
+
+	public static final String TYPE_MAPPING_FILE_NAME_WORKFLOW_PROCESSES =
+		"/META-INF/mappings/workflow-processes-type-mappings.json";
+
+	public static final String TYPE_MAPPING_FILE_NAME_WORKFLOW_TASKS =
+		"/META-INF/mappings/workflow-tasks-type-mappings.json";
+
+	public static final String TYPE_MAPPING_NAME_WORKFLOW_EVENTS =
+		"WorkflowEventsType";
+
+	public static final String TYPE_MAPPING_NAME_WORKFLOW_INSTANCES =
+		"WorkflowInstancesType";
+
+	public static final String TYPE_MAPPING_NAME_WORKFLOW_PROCESSES =
+		"WorkflowProcessesType";
+
+	public static final String TYPE_MAPPING_NAME_WORKFLOW_TASKS =
+		"WorkflowTasksType";
+
+}
