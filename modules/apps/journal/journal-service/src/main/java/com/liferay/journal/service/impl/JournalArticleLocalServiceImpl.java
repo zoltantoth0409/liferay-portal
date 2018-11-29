e167cc96d6e2fcc3e77ca4af200664b8470dc9fc
Laszlo Pap <laszlo.pap@liferay.com>
2018-11-29 13:28:04 +0100

    LPS-87950 JournalArticleLocalService getOldestArticle gives back the newest version

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -3402,7 +3402,7 @@ public class JournalArticleLocalServiceImpl
 		throws PortalException {
 
 		return getFirstArticle(
-			groupId, articleId, status, new ArticleVersionComparator(false));
+			groupId, articleId, status, new ArticleVersionComparator(true));
 	}
 
 	/**
