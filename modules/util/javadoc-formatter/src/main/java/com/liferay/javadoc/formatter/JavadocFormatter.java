6c357db2e7e93b6cbf6be2c03ce1a7cd5407d6fd
Hugo Huijser <hugo.huijser@liferay.com>
2018-11-29 14:51:45 +0800

    LPS-87936 Do not capitalize 'Id' in {@link Id}

--- modules/util/javadoc-formatter/src/main/java/com/liferay/javadoc/formatter/JavadocFormatter.java
+++ modules/util/javadoc-formatter/src/main/java/com/liferay/javadoc/formatter/JavadocFormatter.java
@@ -1170,7 +1170,7 @@ public class JavadocFormatter {
 
 		// Capitalize ID
 
-		text = text.replaceAll("(?i) id(s)?\\b", " ID$1");
+		text = text.replaceAll("(?i)(?<!@link) id(s)?\\b", " ID$1");
 
 		// Wrap special constants in code tags
 
