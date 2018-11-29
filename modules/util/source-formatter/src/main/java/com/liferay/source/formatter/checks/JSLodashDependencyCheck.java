87328b5fec481680939295cbb3af46a1514caf40
Hugo Huijser <hugo.huijser@liferay.com>
2018-11-29 11:06:58 +0800

    LPS-86149 Do not use 'AUI._' so we can remove lodash dependency

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JSLodashDependencyCheck.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JSLodashDependencyCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		int x = -1;
+
+		while (true) {
+			x = content.indexOf("AUI._", x + 1);
+
+			if (x == -1) {
+				return content;
+			}
+
+			addMessage(
+				fileName, "Do not use 'AUI._'", getLineNumber(content, x));
+		}
+	}
+
+}
