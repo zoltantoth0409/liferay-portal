2e3a29df98f688d613d5b7fee8c5a01031982c81
Kevin Tan <kevin.tan@liferay.com>
2018-12-04 16:13:33 +0100

    LPS-87016 Separate odata conversion functions

---
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/utils/constants.es.js
@@ -0,0 +1,27 @@
+export const AND = 'and';
+
+export const BOOLEAN = 'boolean';
+
+export const CONTAINS = 'contains';
+
+export const DATE = 'data';
+
+export const EQ = 'eq';
+
+export const GE = 'ge';
+
+export const GROUP = 'group';
+
+export const GT = 'gt';
+
+export const LE = 'le';
+
+export const LT = 'lt';
+
+export const NE = 'ne';
+
+export const NUMBER = 'number';
+
+export const OR = 'or';
+
+export const STRING = 'string';
