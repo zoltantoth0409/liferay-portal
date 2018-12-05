39498c43e43e263944e71953e4f5dd4591a37542
Hugo Huijser <hugo.huijser@liferay.com>
2018-12-05 13:32:46 +0800

    LPS-87844 Add check to SourceFormatter

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JSPServiceUtilCheck.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JSPServiceUtilCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isModulesCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		Matcher matcher = _serviceUtilPattern.matcher(content);
+
+		if (matcher.find()) {
+			addMessage(
+				fileName, "Do not use *ServiceUtil in module jsp",
+				getLineNumber(content, matcher.start()));
+		}
+
+		return content;
+	}
+
+	private static final Pattern _serviceUtilPattern = Pattern.compile(
+		"\\b[A-Z]\\w+ServiceUtil\\.");
+
+}
