dce47c1cca532a5c6b050ea38483344839a8d11a
yichenroy <yi-chen.tsai@liferay.com>
2018-12-04 17:17:13 -0800

    LRQA-45336 Add failure message generator for gradle task failure

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/failure/message/generator/GradleTaskFailureMessageGenerator.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser.failure.message.generator;
+
+import com.liferay.jenkins.results.parser.Build;
+
+import org.dom4j.Element;
+
+/**
+ * @author Yi-Chen Tsai
+ */
+public class GradleTaskFailureMessageGenerator
+	extends BaseFailureMessageGenerator {
+
+	@Override
+	public Element getMessageElement(Build build) {
+		String consoleText = build.getConsoleText();
+
+		if (!consoleText.contains(_TOKEN_WHAT_WENT_WRONG)) {
+			return null;
+		}
+
+		int start = consoleText.lastIndexOf(_TOKEN_WHAT_WENT_WRONG);
+
+		start = consoleText.lastIndexOf("\n", start);
+
+		int end = Math.min(
+			start + MAX_CONSOLE_TEXT_SNIPPET_LENGTH, consoleText.length() - 1);
+
+		end = consoleText.lastIndexOf("\n", end);
+
+		return getConsoleTextSnippetElement(consoleText, false, start, end);
+	}
+
+	private static final String _TOKEN_WHAT_WENT_WRONG = "* What went wrong:";
+
+}
