6efedb4cbd13d6ec1d3b780bd5376a359a0d4f3f
Bryan Engler <bryan.engler@liferay.com>
2018-12-05 11:00:57 -0800

    LPS-88213 portal-search-elasticsearch6-impl: Remove zero_terms_query from the string (used for debugging), not compatible with earlier versions of kibana
    https://issues.liferay.com/browse/LPS-88213

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/search/engine/adapter/search/SearchSearchRequestExecutorImpl.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/search/engine/adapter/search/SearchSearchRequestExecutorImpl.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.search.elasticsearch6.internal.search.engine.adapter.search;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.search.elasticsearch6.internal.connection.ElasticsearchConnectionManager;
 import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
 import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
@@ -51,12 +53,19 @@ public class SearchSearchRequestExecutorImpl
 
 		String searchRequestBuilderString = searchRequestBuilder.toString();
 
+		searchRequestBuilderString = StringUtil.replace(
+			searchRequestBuilderString, ZERO_TERMS_QUERY_STRING,
+			StringPool.BLANK);
+
 		searchSearchResponseAssembler.assemble(
 			searchResponse, searchSearchResponse, searchSearchRequest,
 			searchRequestBuilderString);
 
 		return searchSearchResponse;
 	}
+
+	protected static final String ZERO_TERMS_QUERY_STRING =
+		",\"zero_terms_query\":\"NONE\"";
 
 	@Reference
 	protected ElasticsearchConnectionManager elasticsearchConnectionManager;
