e69d7ec05a474163e1197e65a3e4155c8ee1ef56
David Arques <davidarchena@gmail.com>
2018-12-05 12:19:28 +0100

    LPS-87431 Add AsahClient API

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/asah/client/AsahClient.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.asah.client;
+
+import com.liferay.segments.internal.asah.client.model.IndividualSegment;
+import com.liferay.segments.internal.asah.client.model.Results;
+import com.liferay.segments.internal.asah.client.util.OrderByField;
+
+import java.util.List;
+
+/**
+ * @author Shinn Lok
+ * @author David Arques
+ */
+public interface AsahClient {
+
+	/**
+	 * Returns active individual segments with members
+	 *
+	 * @param cur - Current page (one-based numbering)
+	 * @param delta - Page size
+	 * @param orderByFields - Sort fields
+	 * @return <code>{@link Results} of {@link IndividualSegment}</code>
+	 *
+	 * @review
+	 */
+	public Results<IndividualSegment> getIndividualSegments(
+		int cur, int delta, List<OrderByField> orderByFields);
+
+}
