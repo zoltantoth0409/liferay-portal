3643058b7c1f2f2251e378d299d50f8283d61d96
Hugo Huijser <hugo.huijser@liferay.com>
2018-12-06 11:31:12 +0800

    LPS-88171 Make check more generic so we can apply to any annotation

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaSpringFrameworkAnnotationsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaSpringFrameworkAnnotationsCheck.java
@@ -14,15 +14,23 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.tools.ToolsUtil;
 import com.liferay.source.formatter.parser.JavaClass;
 import com.liferay.source.formatter.parser.JavaTerm;
 
 import java.io.IOException;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Peter Shin
+ * @author Hugo Huijser
  */
 public class JavaSpringFrameworkAnnotationsCheck extends JavaAnnotationsCheck {
 
@@ -32,10 +40,6 @@ public class JavaSpringFrameworkAnnotationsCheck extends JavaAnnotationsCheck {
 			String fileContent)
 		throws IOException {
 
-		if (!fileContent.contains("org.springframework.web.bind.annotation.")) {
-			return javaTerm.getContent();
-		}
-
 		return formatAnnotations(fileName, (JavaClass)javaTerm, false);
 	}
 
@@ -44,36 +48,124 @@ public class JavaSpringFrameworkAnnotationsCheck extends JavaAnnotationsCheck {
 		String fileName, JavaClass javaClass, String annotation,
 		String indent) {
 
-		if (!annotation.contains("@DeleteMapping") &&
-			!annotation.contains("@GetMapping") &&
-			!annotation.contains("@PatchMapping") &&
-			!annotation.contains("@PostMapping") &&
-			!annotation.contains("@PutMapping")) {
+		annotation = _formatDefaultAttribute(annotation);
 
-			return annotation;
-		}
+		annotation = _formatDefaultValue(
+			annotation, javaClass, "org.springframework.web.bind.annotation",
+			"DeleteMapping");
+		annotation = _formatDefaultValue(
+			annotation, javaClass, "org.springframework.web.bind.annotation",
+			"GetMapping");
+		annotation = _formatDefaultValue(
+			annotation, javaClass, "org.springframework.web.bind.annotation",
+			"PatchMapping");
+		annotation = _formatDefaultValue(
+			annotation, javaClass, "org.springframework.web.bind.annotation",
+			"PostMapping");
+		annotation = _formatDefaultValue(
+			annotation, javaClass, "org.springframework.web.bind.annotation",
+			"PutMapping");
 
-		if (StringUtils.countMatches(annotation, "\"") != 2) {
-			return annotation;
-		}
-
-		if (annotation.contains("(\"\")")) {
-			return annotation.replace("(\"\")", "");
-		}
-
-		if (StringUtils.countMatches(annotation, "=") != 1) {
-			return annotation;
-		}
-
-		int x = annotation.indexOf("(value =");
-
-		int y = annotation.indexOf("\"", x);
-
-		if ((x == -1) || (y == -1)) {
-			return annotation;
-		}
-
-		return annotation.substring(0, x + 1).concat(annotation.substring(y));
+		return annotation;
 	}
+
+	private String _formatDefaultAttribute(String annotation) {
+		Matcher matcher = _valueAttributePattern.matcher(annotation);
+
+		if (!matcher.find()) {
+			return annotation;
+		}
+
+		int x = matcher.end();
+
+		while (true) {
+			x = annotation.indexOf(CharPool.COMMA, x + 1);
+
+			if (x == -1) {
+				break;
+			}
+
+			if (!ToolsUtil.isInsideQuotes(annotation, x) &&
+				(getLevel(annotation.substring(matcher.end(), x), "{", "}") ==
+					0)) {
+
+				return annotation;
+			}
+		}
+
+		return matcher.replaceFirst("$1");
+	}
+
+	private String _formatDefaultValue(
+		String annotation, JavaClass javaClass, String packageName,
+		String annotationName) {
+
+		return _formatDefaultValue(
+			annotation, javaClass, packageName, annotationName, null, "\"\"");
+	}
+
+	private String _formatDefaultValue(
+		String annotation, JavaClass javaClass, String packageName,
+		String annotationName, String attributeName,
+		String defaultAttributeValue) {
+
+		List<String> imports = javaClass.getImports();
+
+		String importName = null;
+
+		int pos = annotationName.indexOf(CharPool.PERIOD);
+
+		if (pos == -1) {
+			importName = packageName + StringPool.PERIOD + annotationName;
+		}
+		else {
+			importName =
+				packageName + StringPool.PERIOD +
+					annotationName.substring(0, pos);
+		}
+
+		if (!imports.contains(importName)) {
+			return annotation;
+		}
+
+		if (attributeName == null) {
+			Pattern pattern = Pattern.compile(
+				StringBundler.concat(
+					"(@", annotationName, ")\\(\\s*", defaultAttributeValue,
+					"\\s*\\)"));
+
+			Matcher matcher = pattern.matcher(annotation);
+
+			if (matcher.find()) {
+				return matcher.replaceFirst("$1");
+			}
+
+			return annotation;
+		}
+
+		Pattern pattern = Pattern.compile(
+			StringBundler.concat(
+				"(\\(|,)\\s*", attributeName, " = ", defaultAttributeValue,
+				"\\s*(\\)|,)"));
+
+		Matcher matcher = pattern.matcher(annotation);
+
+		if (!matcher.find()) {
+			return annotation;
+		}
+
+		if (Objects.equals(matcher.group(1), StringPool.COMMA)) {
+			return matcher.replaceFirst("$2");
+		}
+
+		if (Objects.equals(matcher.group(2), StringPool.COMMA)) {
+			return matcher.replaceFirst("$1");
+		}
+
+		return matcher.replaceFirst("");
+	}
+
+	private static final Pattern _valueAttributePattern = Pattern.compile(
+		"^(\\s*@[\\w.]+\\(\\s*)(value = )");
 
 }
