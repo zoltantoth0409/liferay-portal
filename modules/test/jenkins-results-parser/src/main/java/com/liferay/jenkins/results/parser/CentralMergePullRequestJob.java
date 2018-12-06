50042c74a448a9cf28dcd7ef7c39d2ce96e53f44
Michael Hashimoto <michael.hashimoto@liferay.com>
2018-12-05 17:24:14 -0800

    LRQA-45208 Remove deprecated interface GitSubrepositoryDependentJob

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CentralMergePullRequestJob.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CentralMergePullRequestJob.java
@@ -14,73 +14,13 @@
 
 package com.liferay.jenkins.results.parser;
 
-import java.io.File;
-import java.io.IOException;
-
-import java.util.List;
-import java.util.Properties;
-
 /**
  * @author Leslie Wong
  */
-public class CentralMergePullRequestJob
-	extends PortalAcceptancePullRequestJob
-	implements GitSubrepositoryDependentJob {
+public class CentralMergePullRequestJob extends PortalAcceptancePullRequestJob {
 
 	public CentralMergePullRequestJob(String url) {
 		super(url, "relevant");
-
-		_gitSubrepositoryName = _getGitSubrepositoryName(gitWorkingDirectory);
-
-		jobPropertiesFiles.add(
-			new File(getGitSubrepositoryWorkingDirectory(), "test.properties"));
 	}
-
-	@Override
-	public File getGitSubrepositoryWorkingDirectory() {
-		Properties buildProperties = null;
-
-		try {
-			buildProperties = JenkinsResultsParserUtil.getBuildProperties();
-		}
-		catch (IOException ioe) {
-			throw new RuntimeException("Unable to get build properties", ioe);
-		}
-
-		String gitSubrepositoryDir = JenkinsResultsParserUtil.combine(
-			buildProperties.getProperty("base.repository.dir"), "/",
-			_gitSubrepositoryName);
-
-		return new File(gitSubrepositoryDir);
-	}
-
-	private static String _getGitSubrepositoryName(
-		GitWorkingDirectory gitWorkingDirectory) {
-
-		List<File> currentBranchModifiedFiles =
-			gitWorkingDirectory.getModifiedFilesList();
-
-		File modifiedFile = currentBranchModifiedFiles.get(0);
-
-		File moduleDir = modifiedFile.getParentFile();
-
-		File gitrepoFile = new File(moduleDir, ".gitrepo");
-
-		Properties properties = JenkinsResultsParserUtil.getProperties(
-			gitrepoFile);
-
-		String gitSubrepositoryRemote = properties.getProperty("remote");
-
-		String gitSubrepositoryName = gitSubrepositoryRemote.replaceAll(
-			".*(com-liferay-[^\\.]+)\\.git", "$1");
-
-		if (!gitSubrepositoryName.contains("-private")) {
-			gitSubrepositoryName = gitSubrepositoryName + "-private";
-		}
-
-		return gitSubrepositoryName;
-	}
-
-	private final String _gitSubrepositoryName;
 
 }
