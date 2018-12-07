210f65b2230623ac5ba677789e5dcbfcf264698b
Máté Thurzó <mate.thurzo@liferay.com>
2018-12-07 15:51:11 +0100

    LPS-87698 remove engine bundles

--- modules/apps/change-tracking/change-tracking-engine-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,5 +0,0 @@
-create index IX_264F41B7 on CTEEntry (resourcePrimKey);
-
-create index IX_8745ABC on Collections_Entries (companyId);
-create index IX_D43D5A11 on Collections_Entries (cteCollectionId);
-create index IX_DF400899 on Collections_Entries (cteEntryId);
