384bb39a2dde47b1ea4c370aa19e49a31ae1dc4e
pavel-savinov <pavel.savinov@liferay.com>
2018-12-07 12:53:43 +0100

    LPS-88251 Add getAssetObject method with version support, to be able to render different content for different object version

--- portal-kernel/src/com/liferay/asset/kernel/model/AssetRenderer.java
+++ portal-kernel/src/com/liferay/asset/kernel/model/AssetRenderer.java
@@ -54,6 +54,10 @@ public interface AssetRenderer<T> extends Renderer {
 
 	public T getAssetObject();
 
+	public default T getAssetObject(long versionClassPK) {
+		return getAssetObject();
+	}
+
 	public AssetRendererFactory<T> getAssetRendererFactory();
 
 	public int getAssetRendererType();
