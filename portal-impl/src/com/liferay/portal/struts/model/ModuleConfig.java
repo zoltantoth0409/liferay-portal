efb133f3e0a4e11afe0a10bbaf461ab8402f5b73
shuyangzhou <shuyang.zhou@liferay.com>
2018-12-06 20:27:05 -0800

    LPS-88361 Merge StrutsActionRegistryUtil into ModuleConfig

--- portal-impl/src/com/liferay/portal/struts/model/ModuleConfig.java
+++ portal-impl/src/com/liferay/portal/struts/model/ModuleConfig.java
@@ -14,8 +14,15 @@
 
 package com.liferay.portal.struts.model;
 
+import com.liferay.portal.kernel.struts.StrutsAction;
 import com.liferay.portal.struts.ActionAdapter;
-import com.liferay.portal.struts.StrutsActionRegistryUtil;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+import com.liferay.registry.ServiceTrackerCustomizer;
+import com.liferay.registry.collections.ServiceTrackerMap;
+import com.liferay.registry.collections.ServiceTrackerMapFactory;
+import com.liferay.registry.collections.ServiceTrackerMapFactoryUtil;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,14 +45,69 @@ public class ModuleConfig {
 	}
 
 	public ActionMapping getActionMapping(String path) {
-		ActionAdapter actionAdapter = (ActionAdapter)
-			StrutsActionRegistryUtil.getAction(path);
+		ActionAdapter actionAdapter = _getActionAdaptor(path);
 
 		if (actionAdapter != null) {
 			return new ActionMapping(this, null, path, actionAdapter);
 		}
 
 		return _actionMappings.get(path);
+	}
+
+	private static ActionAdapter _getActionAdaptor(String path) {
+		ActionAdapter actionAdapter = _actionAdaptors.getService(path);
+
+		if (actionAdapter != null) {
+			return actionAdapter;
+		}
+
+		for (String key : _actionAdaptors.keySet()) {
+			if (path.startsWith(key)) {
+				return _actionAdaptors.getService(key);
+			}
+		}
+
+		return null;
+	}
+
+	private static final ServiceTrackerMap<String, ActionAdapter>
+		_actionAdaptors;
+
+	static {
+		ServiceTrackerMapFactory serviceTrackerMapFactory =
+			ServiceTrackerMapFactoryUtil.getServiceTrackerMapFactory();
+
+		_actionAdaptors = serviceTrackerMapFactory.openSingleValueMap(
+			StrutsAction.class, "path",
+			new ServiceTrackerCustomizer<StrutsAction, ActionAdapter>() {
+
+				@Override
+				public ActionAdapter addingService(
+					ServiceReference<StrutsAction> serviceReference) {
+
+					Registry registry = RegistryUtil.getRegistry();
+
+					return new ActionAdapter(
+						registry.getService(serviceReference));
+				}
+
+				@Override
+				public void modifiedService(
+					ServiceReference<StrutsAction> serviceReference,
+					ActionAdapter actionAdapter) {
+				}
+
+				@Override
+				public void removedService(
+					ServiceReference<StrutsAction> serviceReference,
+					ActionAdapter actionAdapter) {
+
+					Registry registry = RegistryUtil.getRegistry();
+
+					registry.ungetService(serviceReference);
+				}
+
+			});
 	}
 
 	private final Map<String, ActionForward> _actionForwards = new HashMap<>();
