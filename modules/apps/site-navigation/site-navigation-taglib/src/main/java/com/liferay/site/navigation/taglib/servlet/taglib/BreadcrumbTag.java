64460119433f8e94f2b7fad7c96d3896fdfafe67
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-12-10 17:17:04 +0100

    LPS-78733 Creates new taglib to allow users show breadcrumb entries

---
+++ modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/BreadcrumbTag.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.taglib.servlet.taglib;
+
+import com.liferay.portal.kernel.servlet.taglib.ui.BreadcrumbEntry;
+import com.liferay.site.navigation.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.taglib.util.IncludeTag;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class BreadcrumbTag extends IncludeTag {
+
+	public void setBreadcrumbEntries(List<BreadcrumbEntry> breadcrumbEntries) {
+		_breadcrumbEntries = breadcrumbEntries;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_breadcrumbEntries = null;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest request) {
+		request.setAttribute(
+			"liferay-site-navigation:breadcrumb:breadcrumbEntries",
+			_breadcrumbEntries);
+	}
+
+	private static final String _PAGE = "/breadcrumb/page.jsp";
+
+	private List<BreadcrumbEntry> _breadcrumbEntries;
+
+}
---
+++ modules/apps/site-navigation/site-navigation-taglib/src/main/resources/META-INF/resources/breadcrumb/page.jsp
@@ -0,0 +1,49 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+List<BreadcrumbEntry> breadcrumbEntries = (List<BreadcrumbEntry>)request.getAttribute("liferay-site-navigation:breadcrumb:breadcrumbEntries");
+%>
+
+<ol class="breadcrumb">
+
+	<%
+	for (int i = 0; i < breadcrumbEntries.size(); i++) {
+		BreadcrumbEntry breadcrumbEntry = breadcrumbEntries.get(i);
+	%>
+
+		<c:choose>
+			<c:when test="<%= i < (breadcrumbEntries.size() - 1) %>">
+				<li class="breadcrumb-item">
+					<a class="breadcrumb-link" href="<%= breadcrumbEntry.getURL() %>">
+						<span class="breadcrumb-text-truncate"><%= HtmlUtil.escape(breadcrumbEntry.getTitle()) %></span>
+					</a>
+				</li>
+			</c:when>
+			<c:otherwise>
+				<li class="active breadcrumb-item">
+					<span class="breadcrumb-text-truncate"><%= HtmlUtil.escape(breadcrumbEntry.getTitle()) %></span>
+				</li>
+			</c:otherwise>
+		</c:choose>
+
+	<%
+	}
+	%>
+
+</ol>
