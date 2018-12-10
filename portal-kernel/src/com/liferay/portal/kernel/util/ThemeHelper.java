cf705b2c804ed28827e51588e76887cbd68e328f
shuyangzhou <shuyang.zhou@liferay.com>
2018-12-10 10:58:39 -0800

    LPS-87064 Class level deprecation is enough

--- portal-kernel/src/com/liferay/portal/kernel/util/ThemeHelper.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ThemeHelper.java
@@ -36,16 +36,8 @@ public class ThemeHelper {
 
 	public static final String TEMPLATE_EXTENSION_FTL = "ftl";
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
 	public static final String TEMPLATE_EXTENSION_JSP = "jsp";
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
 	public static final String TEMPLATE_EXTENSION_VM = "vm";
 
 	public static String getResourcePath(
