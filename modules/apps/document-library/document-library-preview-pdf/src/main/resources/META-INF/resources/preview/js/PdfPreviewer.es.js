77a0c11cccafac9d3034ec5e8fc05fe9b5d91a75
Jorge González <jorge.gonzalez@liferay.com>
2018-12-11 13:17:54 +0100

    LPS-87432 Add UI loader and enhance _loadClosePages

--- modules/apps/document-library/document-library-preview-pdf/src/main/resources/META-INF/resources/preview/js/PdfPreviewer.es.js
+++ modules/apps/document-library/document-library-preview-pdf/src/main/resources/META-INF/resources/preview/js/PdfPreviewer.es.js
@@ -1,7 +1,8 @@
 import {Config} from 'metal-state';
+import {debounce} from 'metal-debounce';
 import Component from 'metal-component';
+import imagePromise from 'image-promise';
 import Soy from 'metal-soy';
-import loadImage from 'image-promise';
 
 import templates from './PdfPreviewer.soy';
 
@@ -17,10 +18,27 @@ const KEY_CODE_ESC = 27;
 const VALID_KEY_CODES = [8, 9, 37, 38, 39, 40, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
 
 /**
+ * Milisecons between goToPage calls
+ * @type {number}
+ */
+const WAIT_BETWEEN_GO_TO_PAGE = 250;
+
+/**
  * Component that create an pdf preview
  * @review
  */
 class PdfPreviewer extends Component {
+
+	/**
+	 * @inheritDoc
+	 */
+	attached() {
+		this._goToPageDebounced = debounce(
+			this._goToPage.bind(this),
+			WAIT_BETWEEN_GO_TO_PAGE
+		);
+		this._goToPageDebounced(this.currentPage);
+	}
 
 	/**
 	 * @inheritDoc
@@ -38,30 +56,60 @@ class PdfPreviewer extends Component {
 		this.refs.imageContainer.scrollTop = 0;
 		this.previousPageDisabled = currentPage === 1;
 		this.nextPageDisabled = currentPage === this.totalPages;
-
-		this._fetchClosePages(currentPage);
 	}
 
 	/**
-	 * Fetch close pages of the current one
+	 * Load close pages of the current one
 	 * @param {number|string} currentPage - the current page
 	 * @param {number} [numberOfPages=2] - number of load pages (before and after)
 	 * @private
 	 * @review
 	 */
-	_fetchClosePages(currentPage, numberOfPages = 2) {
-		const pagesUrlToLoad = [];
-
+	_loadClosePages(currentPage, numberOfPages = 2) {
 		for (let i = 1; i <= numberOfPages; i++) {
 			if (currentPage + i <= this.totalPages) {
-				pagesUrlToLoad.push(this.baseImageURL + (currentPage + i));
+				this._loadPage(currentPage + i);
 			}
 			if (currentPage - i > 1) {
-				pagesUrlToLoad.push(this.baseImageURL + (currentPage - i));
+				this._loadPage(currentPage - i);
 			}
 		}
+	}
 
-		loadImage(pagesUrlToLoad);
+	/**
+	 * Trigger a promise to load the image
+	 * @param {number} pageToLoad
+	 * @return {Promise} A promise to be resolved when the image is loaded
+	 * @private
+	 * @review
+	 */
+	_loadPage(pageToLoad) {
+		let pagePromise = this._loadedPages[pageToLoad];
+
+		if (!pagePromise) {
+			pagePromise = imagePromise(`${this.baseImageURL}${pageToLoad}`);
+			this._loadedPages[pageToLoad] = pagePromise;
+		}
+
+		return pagePromise;
+	}
+
+	/**
+	 * Show page when it's completely loaded
+	 * and load the closest pages
+	 * @param {number} page
+	 * @private
+	 * @review
+	 */
+	_goToPage(page) {
+		this._loadPage(page).then(
+			() => {
+				if (this.currentPage === page) {
+					this.currentPageLoading = false;
+					this._loadClosePages(page);
+				}
+			}
+		);
 	}
 
 	/**
@@ -120,7 +168,7 @@ class PdfPreviewer extends Component {
 	}
 
 	/**
-	 * Set the current page if is valid page
+	 * Set the current page if is valid page and show loader
 	 * @param {number|string} page
 	 * @private
 	 * @review
@@ -129,10 +177,15 @@ class PdfPreviewer extends Component {
 		const pageNumber = Number.parseInt(page, 10);
 
 		if (pageNumber) {
-			this.currentPage = Math.min(
+			const currentPage = Math.min(
 				Math.max(1, pageNumber),
 				this.totalPages
 			);
+
+			this.currentPage = currentPage;
+			this.currentPageLoading = true;
+
+			this._goToPageDebounced(currentPage);
 		}
 	}
 }
@@ -156,6 +209,12 @@ PdfPreviewer.STATE = {
 	 * @type {Number}
 	 */
 	currentPage: Config.number().required(),
+
+	/**
+	 * Flag that indicate if currentPgae is loading.
+	 * @type {Boolean}
+	 */
+	currentPageLoading: Config.bool(),
 
 	/**
 	 * Flag that indicate if 'next page' is disabled.
@@ -185,7 +244,14 @@ PdfPreviewer.STATE = {
 	 * Pdf pages lenght
 	 * @type {Number}
 	 */
-	totalPages: Config.number().required()
+	totalPages: Config.number().required(),
+
+	/**
+	 * Object that store promises in the page key
+	 * @default {}
+	 * @type {Object}
+	 */
+	_loadedPages: Config.object().internal().value({})
 };
 
 Soy.register(PdfPreviewer, templates);
