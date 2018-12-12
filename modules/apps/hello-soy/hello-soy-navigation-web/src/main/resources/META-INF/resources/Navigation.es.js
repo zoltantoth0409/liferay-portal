77d49b97b674cfd232e2a1c3236f205cf9469b46
Diego Nascimento <diego.nascimento@liferay.com>
2018-12-12 14:30:37 -0300

    LPS-86123 Missing import correction on hello-soy-navigation-web

--- modules/apps/hello-soy/hello-soy-navigation-web/src/main/resources/META-INF/resources/Navigation.es.js
+++ modules/apps/hello-soy/hello-soy-navigation-web/src/main/resources/META-INF/resources/Navigation.es.js
@@ -1,7 +1,7 @@
-import Component from 'metal-component/src/Component';
+import Component from 'metal-component';
 import Footer from 'hello-soy-web/Footer.soy';
 import Header from 'hello-soy-web/Header.soy';
-import Soy from 'metal-soy/src/Soy';
+import Soy from 'metal-soy';
 import templates from './Navigation.soy';
 
 class Navigation extends Component {}
