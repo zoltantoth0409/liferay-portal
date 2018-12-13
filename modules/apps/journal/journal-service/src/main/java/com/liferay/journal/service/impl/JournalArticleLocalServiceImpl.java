b56a20bad985903ac6fb716345b4898a5027ae5c
Joshua Cords <joshua.cords@liferay.com>
2018-12-13 13:25:08 -0800

    LPS-88424 Lower interval

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7100,6 +7100,8 @@ public class JournalArticleLocalServiceImpl
 		actionableDynamicQuery.setTransactionConfig(
 			DefaultActionableDynamicQuery.REQUIRES_NEW_TRANSACTION_CONFIG);
 
+		actionableDynamicQuery.setInterval(1000);
+
 		actionableDynamicQuery.performActions();
 
 		if (_previousCheckDate == null) {
