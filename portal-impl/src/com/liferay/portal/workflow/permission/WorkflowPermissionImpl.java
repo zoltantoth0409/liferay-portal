dd474e4a1f1a6c998bb6086cd9789773f9c4d8ac
Inácio Nery <inacio.nery@liferay.com>
2018-12-13 17:42:57 -0300

    LPS-87393 If the user isn't the owner, deny access

--- portal-impl/src/com/liferay/portal/workflow/permission/WorkflowPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/workflow/permission/WorkflowPermissionImpl.java
@@ -79,6 +79,13 @@ public class WorkflowPermissionImpl implements WorkflowPermission {
 				permissionChecker, workflowInstance);
 
 			if (!hasPermission && actionId.equals(ActionKeys.VIEW)) {
+				if (!permissionChecker.hasOwnerPermission(
+						permissionChecker.getCompanyId(), className, classPK,
+						permissionChecker.getUserId(), actionId)) {
+
+					return false;
+				}
+
 				return null;
 			}
 
