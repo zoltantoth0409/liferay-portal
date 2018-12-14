49f316387b1b1a2cf2b1ccab2695db8b2c8204df
Jorge González <jorge.gonzalez@liferay.com>
2018-12-14 13:14:05 +0100

    LPS-85940 Generate markup with soy, add state validation and scss

--- modules/apps/document-library/document-library-preview-video/src/main/resources/META-INF/resources/preview/js/VideoPreviewer.es.js
+++ modules/apps/document-library/document-library-preview-video/src/main/resources/META-INF/resources/preview/js/VideoPreviewer.es.js
@@ -1,3 +1,4 @@
+import {Config} from 'metal-state';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 
@@ -7,8 +8,7 @@ import templates from './VideoPreviewer.soy';
  * Component that create an video player
  * @review
  */
-class VideoPreviewer extends Component {
-}
+class VideoPreviewer extends Component {}
 
 /**
  * State definition.
@@ -17,6 +17,31 @@ class VideoPreviewer extends Component {
  * @type {!Object}
  */
 VideoPreviewer.STATE = {
+
+	/**
+	 * List of of video sources
+	 * @instance
+	 * @memberof VideoPreviewer
+	 * @review
+	 * @type {!Array<object>}
+	 */
+	videoURLs: Config.arrayOf(
+		Config.shapeOf(
+			{
+				type: Config.string().required(),
+				url: Config.string().required()
+			}
+		)
+	).required(),
+
+	/**
+	 * The "poster" attribute of the <video> element
+	 * @instance
+	 * @memberof VideoPreviewer
+	 * @review
+	 * @type {String}
+	 */
+	videoThumbnailURL: Config.string()
 };
 
 Soy.register(VideoPreviewer, templates);
