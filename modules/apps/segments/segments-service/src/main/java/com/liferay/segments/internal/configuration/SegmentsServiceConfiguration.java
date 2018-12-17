a17290eb012b5a88fe4887659b3b1c63dbec08a6
David Arques <davidarchena@gmail.com>
2018-12-17 11:22:41 +0100

    LPS-88262 Add Segments Service Configuration

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/configuration/SegmentsServiceConfiguration.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.configuration;
+
+import aQute.bnd.annotation.ProviderType;
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author David Arques
+ */
+@ExtendedObjectClassDefinition(category = "segments")
+@Meta.OCD(
+	id = "com.liferay.segments.internal.configuration.SegmentsServiceConfiguration",
+	localization = "content/Language",
+	name = "segments-service-configuration-name"
+)
+@ProviderType
+public interface SegmentsServiceConfiguration {
+
+	@Meta.AD(deflt = "1", name = "check-interval", required = false)
+	public int checkInterval();
+
+}
