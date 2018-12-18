a39bbf369af7b5dcfc499341469a45f0df9fe358
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-12-18 13:24:33 +0100

    LPS-88440 Implements processor, replace images that contains "../resources/" for the corresponding image in the repository

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-resources/src/main/java/com/liferay/fragment/entry/processor/resources/ResourcesFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-resources/src/main/java/com/liferay/fragment/entry/processor/resources/ResourcesFragmentEntryProcessor.java
@@ -14,12 +14,26 @@
 
 package com.liferay.fragment.entry.processor.resources;
 
+import com.liferay.document.library.kernel.util.DLUtil;
+import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.fragment.model.FragmentEntry;
 import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.processor.FragmentEntryProcessor;
+import com.liferay.fragment.service.FragmentEntryService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Repository;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
 
 import java.util.Locale;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
@@ -32,8 +46,75 @@ public class ResourcesFragmentEntryProcessor implements FragmentEntryProcessor {
 
 	@Override
 	public String processFragmentEntryLinkHTML(
-		FragmentEntryLink fragmentEntryLink, String html, String mode,
-		Locale locale) {
+			FragmentEntryLink fragmentEntryLink, String html, String mode,
+			Locale locale)
+		throws PortalException {
+
+		FragmentEntry fragmentEntry = _fragmentEntryService.fetchFragmentEntry(
+			fragmentEntryLink.getFragmentEntryId());
+
+		if (fragmentEntry == null) {
+			return html;
+		}
+
+		Repository repository =
+			PortletFileRepositoryUtil.fetchPortletRepository(
+				fragmentEntry.getGroupId(), FragmentPortletKeys.FRAGMENT);
+
+		if (repository == null) {
+			return html;
+		}
+
+		Folder folder = null;
+
+		try {
+			folder = PortletFileRepositoryUtil.getPortletFolder(
+				repository.getRepositoryId(), repository.getDlFolderId(),
+				String.valueOf(fragmentEntry.getFragmentCollectionId()));
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Unable to get fragment collection repository folder for " +
+						fragmentEntry.getFragmentCollectionId(),
+					pe);
+			}
+
+			return html;
+		}
+
+		while (html.contains(_RESOURCES_PATH)) {
+			int index = html.indexOf(_RESOURCES_PATH);
+
+			String delimiter = html.substring(index - 1, index);
+
+			if (Objects.equals(delimiter, StringPool.OPEN_PARENTHESIS)) {
+				delimiter = StringPool.CLOSE_PARENTHESIS;
+			}
+
+			int lastIndex = html.indexOf(delimiter, index);
+
+			if (lastIndex < 0) {
+				break;
+			}
+
+			String fileName = html.substring(
+				index + _RESOURCES_PATH.length(), lastIndex);
+
+			FileEntry fileEntry =
+				PortletFileRepositoryUtil.fetchPortletFileEntry(
+					fragmentEntry.getGroupId(), folder.getFolderId(), fileName);
+
+			String fileEntryURL = StringPool.BLANK;
+
+			if (fileEntry != null) {
+				fileEntryURL = DLUtil.getPreviewURL(
+					fileEntry, fileEntry.getFileVersion(), null,
+					StringPool.BLANK, false, false);
+			}
+
+			html = html.replaceAll(_RESOURCES_PATH + fileName, fileEntryURL);
+		}
 
 		return html;
 	}
@@ -41,5 +122,13 @@ public class ResourcesFragmentEntryProcessor implements FragmentEntryProcessor {
 	@Override
 	public void validateFragmentEntryHTML(String html) {
 	}
+
+	private static final String _RESOURCES_PATH = "../../resources/";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ResourcesFragmentEntryProcessor.class);
+
+	@Reference
+	private FragmentEntryService _fragmentEntryService;
 
 }
