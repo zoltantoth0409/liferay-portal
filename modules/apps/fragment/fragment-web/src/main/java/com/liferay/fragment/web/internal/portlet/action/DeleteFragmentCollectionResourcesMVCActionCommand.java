ddb6c47fa47b75b87d06571df1e5625ebd0e8a99
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-12-19 17:32:27 +0100

    LPS-88567 Adds action to delete resources

---
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/DeleteFragmentCollectionResourcesMVCActionCommand.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.portlet.action;
+
+import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.fragment.exception.RequiredFragmentEntryException;
+import com.liferay.fragment.service.FragmentCollectionService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + FragmentPortletKeys.FRAGMENT,
+		"mvc.command.name=/fragment/delete_fragment_collection_resources"
+	},
+	service = MVCActionCommand.class
+)
+public class DeleteFragmentCollectionResourcesMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long[] deleteFileEntryIds = null;
+
+		long fileEntryId = ParamUtil.getLong(actionRequest, "fileEntryId");
+
+		if (fileEntryId > 0) {
+			deleteFileEntryIds = new long[] {fileEntryId};
+		}
+		else {
+			deleteFileEntryIds = ParamUtil.getLongValues(
+				actionRequest, "rowIds");
+		}
+
+		try {
+			for (long curFileEntryId : deleteFileEntryIds) {
+				PortletFileRepositoryUtil.deletePortletFileEntry(
+					curFileEntryId);
+			}
+		}
+		catch (RequiredFragmentEntryException rfee) {
+			SessionErrors.add(actionRequest, rfee.getClass());
+
+			hideDefaultErrorMessage(actionRequest);
+
+			sendRedirect(actionRequest, actionResponse);
+		}
+	}
+
+	@Reference
+	private FragmentCollectionService _fragmentCollectionService;
+
+}
