79160f7f2f23eda7128512256930587c550a8433
Kevin Tan <kevin.tan@liferay.com>
2018-12-20 16:41:50 +0100

    LPS-88003 Add tests

---
+++ modules/apps/segments/segments-web/test/js/components/segment_edit/SegmentEdit.es.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SegmentEdit from 'components/segment_edit/SegmentEdit.es';
+import {cleanup, render} from 'react-testing-library';
+
+describe(
+	'SegmentEdit',
+	() => {
+		afterEach(cleanup);
+
+		it(
+			'should render',
+			() => {
+				const {asFragment} = render(
+					<SegmentEdit />
+				);
+
+				expect(asFragment()).toMatchSnapshot();
+			}
+		);
+	}
+);
