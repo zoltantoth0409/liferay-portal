fbd3b9d11023d305d274549f17b1426a3ba48c71
alexandre araujo <alexandre.araujo@liferay.com>
2018-12-20 15:51:15 -0300

    LPS-88146 Remove disable cache comment

--- modules/dxp/apps/portal-workflow/portal-workflow-reports-web/src/main/resources/META-INF/resources/js/components/__tests__/App.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-reports-web/src/main/resources/META-INF/resources/js/components/__tests__/App.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import App from '../App';
 import renderer from 'react-test-renderer';
--- modules/dxp/apps/portal-workflow/portal-workflow-reports-web/src/main/resources/META-INF/resources/js/components/process-list/__tests__/ProcessListCard.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-reports-web/src/main/resources/META-INF/resources/js/components/process-list/__tests__/ProcessListCard.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ProcessListCard from '../ProcessListCard';
 import renderer from 'react-test-renderer';
