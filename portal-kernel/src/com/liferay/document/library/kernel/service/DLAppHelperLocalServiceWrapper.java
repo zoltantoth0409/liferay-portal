6ec987909edc220ad8151e907852bfd38b78f7c1
Jonathan McCann <jonathan.mccann@liferay.com>
2018-12-20 11:51:44 -0800

    LPS-88484 Build service

--- portal-kernel/src/com/liferay/document/library/kernel/service/DLAppHelperLocalServiceWrapper.java
+++ portal-kernel/src/com/liferay/document/library/kernel/service/DLAppHelperLocalServiceWrapper.java
@@ -229,6 +229,12 @@ public class DLAppHelperLocalServiceWrapper implements DLAppHelperLocalService,
 	}
 
 	@Override
+	public void reindex(java.util.List<Long> dlFileEntryIds)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		_dlAppHelperLocalService.reindex(dlFileEntryIds);
+	}
+
+	@Override
 	public void restoreDependentsFromTrash(
 		com.liferay.document.library.kernel.model.DLFolder dlFolder)
 		throws com.liferay.portal.kernel.exception.PortalException {
