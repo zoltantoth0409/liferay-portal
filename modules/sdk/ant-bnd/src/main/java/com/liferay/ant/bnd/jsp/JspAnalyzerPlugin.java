50442eb7ebae183637f5bd0e4017fc8aa352fd71
shuyangzhou <shuyang.zhou@liferay.com>
2018-12-20 15:44:20 -0800

    Revert "LPS-88382 As we provide an implementation for http://java.sun.com/jsp/jstl/core it should not be blacklisted and it must be added as a requirement."
    This reverts commit 0682999f5ce2d1aa26349188ad3415eda7d04495.

--- modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/jsp/JspAnalyzerPlugin.java
+++ modules/sdk/ant-bnd/src/main/java/com/liferay/ant/bnd/jsp/JspAnalyzerPlugin.java
@@ -527,7 +527,7 @@ public class JspAnalyzerPlugin implements AnalyzerPlugin {
 	}
 
 	private static final String[] _JSTL_CORE_URIS = {
-		"http://java.sun.com/jsp/jstl/fmt",
+		"http://java.sun.com/jsp/jstl/core", "http://java.sun.com/jsp/jstl/fmt",
 		"http://java.sun.com/jsp/jstl/functions",
 		"http://java.sun.com/jsp/jstl/sql", "http://java.sun.com/jsp/jstl/xml"
 	};
