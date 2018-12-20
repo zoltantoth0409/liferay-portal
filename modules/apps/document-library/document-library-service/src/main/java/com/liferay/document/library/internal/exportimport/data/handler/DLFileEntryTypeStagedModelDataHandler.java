e02adb1f4d42f79704c9cfcd66c39a71f9f65422
Jorge Díaz <jorge.diaz@liferay.com>
2018-12-20 12:18:28 +0100

    LPS-88571 The DLFileEntryType with key = BASIC-DOCUMENT has companyId=0, groupId=0 and fileEntryTypeId=0 so in case group doesn't exists we should execute fetchExistingFileEntryType (old code before LPS-54392)

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/data/handler/DLFileEntryTypeStagedModelDataHandler.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/exportimport/data/handler/DLFileEntryTypeStagedModelDataHandler.java
@@ -392,7 +392,8 @@ public class DLFileEntryTypeStagedModelDataHandler
 		Group group = _groupLocalService.fetchGroup(groupId);
 
 		if (group == null) {
-			return null;
+			return fetchExistingFileEntryType(
+				uuid, groupId, fileEntryTypeKey, preloaded);
 		}
 
 		long companyId = group.getCompanyId();
