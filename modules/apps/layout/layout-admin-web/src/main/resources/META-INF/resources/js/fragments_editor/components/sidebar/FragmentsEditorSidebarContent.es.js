1fd92b8220c14e1a848d16a5e14010c6aa32b594
Sandro Chinea <sandrochinea@gmail.com>
2018-12-20 12:36:37 +0100

    LPS-87758 Added logic to hide sidebar content on buttons click

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/FragmentsEditorSidebarContent.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/FragmentsEditorSidebarContent.es.js
@@ -7,6 +7,9 @@ import './fragments/SidebarSectionsPanel.es';
 import './layouts/SidebarLayoutsPanel.es';
 import './mapping/SidebarMappingPanel.es';
 import './structure/SidebarStructurePanel.es';
+import {HIDE_SIDEBAR, TOGGLE_SIDEBAR} from '../../actions/actions.es';
+import state from '../../store/state.es';
+import {Store} from '../../store/store.es';
 import templates from './FragmentsEditorSidebarContent.soy';
 
 /**
@@ -24,8 +27,39 @@ class FragmentsEditorSidebarContent extends Component {
 	_handlePanelButtonClick(event) {
 		const data = event.delegateTarget.dataset;
 
-		this._panelId = data.panelId;
-		this._sidebarTitle = data.sidebarTitle;
+		if (!this.fragmentsEditorSidebarVisible) {
+			this.store.dispatchAction(TOGGLE_SIDEBAR);
+			this._setActivePanel(data.panelId, data.sidebarTitle);
+		}
+		else if (this._panelId === data.panelId) {
+			this._hideSidebar();
+		}
+		else {
+			this._setActivePanel(data.panelId, data.sidebarTitle);
+		}
+	}
+
+	/**
+	 * Hides sidebar
+	 * @private
+	 * @review
+	 */
+	_hideSidebar() {
+		this.store.dispatchAction(HIDE_SIDEBAR);
+		this._panelId = '';
+		this._sidebarTitle = '';
+	}
+
+	/**
+	 * Set as active the panel with the given panelId and also set sidebar title
+	 * @param {string} panelId
+	 * @param {string} title
+	 * @private
+	 * @review
+	 */
+	_setActivePanel(panelId, title) {
+		this._panelId = panelId;
+		this._sidebarTitle = title;
 	}
 }
 
@@ -36,6 +70,26 @@ class FragmentsEditorSidebarContent extends Component {
  * @type {!Object}
  */
 FragmentsEditorSidebarContent.STATE = {
+
+	/**
+	 * Determines if sidebar should show or not
+	 * @default true
+	 * @memberOf FragmentsEditorSidebarContent
+	 * @private
+	 * @review
+	 * @type {boolean}
+	 */
+	fragmentsEditorSidebarVisible: state.fragmentsEditorSidebarVisible,
+
+	/**
+	 * Store instance
+	 * @default undefined
+	 * @instance
+	 * @memberOf SidebarAvailableElements
+	 * @review
+	 * @type {Store}
+	 */
+	store: Config.instanceOf(Store),
 
 	/**
 	 * Sidebar active panel ID
