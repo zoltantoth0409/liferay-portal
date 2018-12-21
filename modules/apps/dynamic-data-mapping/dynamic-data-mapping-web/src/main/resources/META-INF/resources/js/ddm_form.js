669dc9f17d798ededbc5711031edc855a9606feb
spencerwoo <spencer.woo@liferay.com>
2018-12-21 09:46:35 -0800

    LPS-88560 guard groupIdNode against null

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -1365,7 +1365,7 @@ AUI.add(
 
 						var groupIdNode = A.one('#' + this.get('portletNamespace') + 'groupId');
 
-						var groupId = groupIdNode.getAttribute('value') || themeDisplay.getScopeGroupId();
+						var groupId = (groupIdNode && groupIdNode.getAttribute('value')) || themeDisplay.getScopeGroupId();
 
 						url.setParameter('eventName', 'selectContent');
 						url.setParameter('groupId', groupId);
@@ -1517,7 +1517,7 @@ AUI.add(
 
 							var groupIdNode = A.one('#' + this.get('portletNamespace') + 'groupId');
 
-							var groupId = groupIdNode.getAttribute('value') || themeDisplay.getScopeGroupId();
+							var groupId = (groupIdNode && groupIdNode.getAttribute('value')) || themeDisplay.getScopeGroupId();
 
 							var layoutsRoot = {
 								groupId: groupId,
@@ -1943,7 +1943,7 @@ AUI.add(
 
 						var groupIdNode = A.one('#' + this.get('portletNamespace') + 'groupId');
 
-						var groupId = groupIdNode.getAttribute('value') || themeDisplay.getScopeGroupId();
+						var groupId = (groupIdNode && groupIdNode.getAttribute('value')) || themeDisplay.getScopeGroupId();
 
 						var parentLayoutId = instance._currentParentLayoutId;
 
@@ -2184,7 +2184,7 @@ AUI.add(
 
 						var groupIdNode = A.one('#' + this.get('portletNamespace') + 'groupId');
 
-						var groupId = groupIdNode.getAttribute('value') || themeDisplay.getScopeGroupId();
+						var groupId = (groupIdNode && groupIdNode.getAttribute('value')) || themeDisplay.getScopeGroupId();
 
 						if (selectedLayout && selectedLayout.layoutId) {
 							instance._requestSiblingLayouts(
