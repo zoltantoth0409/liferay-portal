ceedcf729bc6732824be0e40d0dff80db2b75c4f
Pablo Molina <contact@pablomolina.me>
2018-12-21 14:53:44 +0100

    LPS-87758 Make sidebar show/hide in sync with product menu

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/FragmentsEditorSidebar.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/FragmentsEditorSidebar.es.js
@@ -14,6 +14,36 @@ import templates from './FragmentsEditorSidebar.soy';
 class FragmentsEditorSidebar extends Component {
 
 	/**
+	 * @inheritDoc
+	 * @review
+	 */
+	created() {
+		this._productMenuToggle = $('.product-menu-toggle');
+		this._productMenu = $(this._productMenuToggle.data('target'));
+		this._handleHide = this._handleHide.bind(this);
+
+		this._productMenu.on('openStart.lexicon.sidenav', this._handleHide);
+	}
+
+	/**
+	 * @inheritDoc
+	 * @review
+	 */
+	disposed() {
+		this._productMenu.off('openStart.lexicon.sidenav', this._handleHide);
+	}
+
+	/**
+	 * @inheritDoc
+	 * @review
+	 */
+	rendered() {
+		if (this.fragmentsEditorSidebarVisible) {
+			this._productMenuToggle.sideNavigation('hide');
+		}
+	}
+
+	/**
 	 * @private
 	 * @review
 	 */
@@ -38,7 +68,27 @@ FragmentsEditorSidebar.STATE = {
 	 * @review
 	 * @type {Store}
 	 */
-	store: Config.instanceOf(Store)
+	store: Config.instanceOf(Store),
+
+	/**
+	 * Synced ProductMenu sidebar.
+	 * @default undefined
+	 * @instance
+	 * @memberOf FragmentsEditorSidebar
+	 * @review
+	 * @type {object}
+	 */
+	_productMenu: Config.internal(),
+
+	/**
+	 * Synced ProductMenu toggle button.
+	 * @default undefined
+	 * @instance
+	 * @memberOf FragmentsEditorSidebar
+	 * @review
+	 * @type {object}
+	 */
+	_productMenuToggle: Config.internal()
 };
 
 Soy.register(FragmentsEditorSidebar, templates);
