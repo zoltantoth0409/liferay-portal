78065d23b17e6c667664a93bc606d203100ec1c5
Brian Chan <brian.chan@liferay.com>
2018-12-25 09:58:05 -0800

    LPS-88499 Move, no changes

--- modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/internal/upgrade/v2_1_0/UpgradeAssetDisplayLayout.java
+++ modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/internal/upgrade/v2_1_0/UpgradeAssetDisplayLayout.java
@@ -58,52 +58,10 @@ public class UpgradeAssetDisplayLayout extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		_upgradeSchema();
+		alter(
+			AssetDisplayPageEntryTable.class,
+			new AlterTableAddColumn("plid LONG"));
 
-		_upgradePlid();
-	}
-
-	private long _getPlid(
-			AssetEntry assetEntry, long userId, long groupId,
-			long layoutPageTemplateEntryId, ServiceContext serviceContext)
-		throws PortalException {
-
-		LayoutPageTemplateEntry layoutPageTemplateEntry = Optional.ofNullable(
-			_layoutPageTemplateEntryService.fetchLayoutPageTemplateEntry(
-				layoutPageTemplateEntryId)
-		).orElse(
-			_layoutPageTemplateEntryService.fetchDefaultLayoutPageTemplateEntry(
-				groupId, assetEntry.getClassNameId(),
-				assetEntry.getClassTypeId())
-		);
-
-		if (layoutPageTemplateEntry.getPlid() > 0) {
-			return layoutPageTemplateEntry.getPlid();
-		}
-
-		UnicodeProperties typeSettingsProperties = new UnicodeProperties();
-
-		typeSettingsProperties.put("visible", Boolean.FALSE.toString());
-
-		serviceContext.setAttribute(
-			"layout.instanceable.allowed", Boolean.TRUE);
-
-		Layout layout = _layoutLocalService.addLayout(
-			userId, groupId, false, 0, assetEntry.getTitleMap(),
-			assetEntry.getTitleMap(), assetEntry.getDescriptionMap(), null,
-			null, LayoutConstants.LAYOUT_TYPE_ASSET_DISPLAY,
-			typeSettingsProperties.toString(), true, new HashMap<>(),
-			serviceContext);
-
-		layoutPageTemplateEntry.setPlid(layout.getPlid());
-
-		_layoutPageTemplateEntryLocalService.updateLayoutPageTemplateEntry(
-			layoutPageTemplateEntry);
-
-		return layout.getPlid();
-	}
-
-	private void _upgradePlid() throws Exception {
 		StringBundler sb = new StringBundler(3);
 
 		sb.append("select assetDisplayPageEntryId, userId, groupId, ");
@@ -154,10 +112,44 @@ public class UpgradeAssetDisplayLayout extends UpgradeProcess {
 		}
 	}
 
-	private void _upgradeSchema() throws Exception {
-		alter(
-			AssetDisplayPageEntryTable.class,
-			new AlterTableAddColumn("plid LONG"));
+	private long _getPlid(
+			AssetEntry assetEntry, long userId, long groupId,
+			long layoutPageTemplateEntryId, ServiceContext serviceContext)
+		throws PortalException {
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry = Optional.ofNullable(
+			_layoutPageTemplateEntryService.fetchLayoutPageTemplateEntry(
+				layoutPageTemplateEntryId)
+		).orElse(
+			_layoutPageTemplateEntryService.fetchDefaultLayoutPageTemplateEntry(
+				groupId, assetEntry.getClassNameId(),
+				assetEntry.getClassTypeId())
+		);
+
+		if (layoutPageTemplateEntry.getPlid() > 0) {
+			return layoutPageTemplateEntry.getPlid();
+		}
+
+		UnicodeProperties typeSettingsProperties = new UnicodeProperties();
+
+		typeSettingsProperties.put("visible", Boolean.FALSE.toString());
+
+		serviceContext.setAttribute(
+			"layout.instanceable.allowed", Boolean.TRUE);
+
+		Layout layout = _layoutLocalService.addLayout(
+			userId, groupId, false, 0, assetEntry.getTitleMap(),
+			assetEntry.getTitleMap(), assetEntry.getDescriptionMap(), null,
+			null, LayoutConstants.LAYOUT_TYPE_ASSET_DISPLAY,
+			typeSettingsProperties.toString(), true, new HashMap<>(),
+			serviceContext);
+
+		layoutPageTemplateEntry.setPlid(layout.getPlid());
+
+		_layoutPageTemplateEntryLocalService.updateLayoutPageTemplateEntry(
+			layoutPageTemplateEntry);
+
+		return layout.getPlid();
 	}
 
 	private final AssetEntryLocalService _assetEntryLocalService;
