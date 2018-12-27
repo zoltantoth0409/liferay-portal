a60338ab0753571f2ef5102779791e87dcd82c44
rubenpulido <ruben.pulido@liferay.com>
2018-12-27 20:19:57 +0100

    LPS-88714 Document API client tests

---
+++ modules/apps/headless-apio/media-object/media-object-apio-client-test/src/testIntegration/java/com/liferay/media/object/apio/client/test/internal/activator/MediaObjectTestActivator.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.media.object.apio.client.test.internal.activator;
+
+import com.liferay.portal.apio.test.util.AuthConfigurationTestUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.test.rule.callback.PermissionCheckerTestCallback;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Ruben Pulido
+ */
+public class MediaObjectTestActivator implements BundleActivator {
+
+	public static final String CONTENT_SPACE_NAME =
+		MediaObjectTestActivator.class.getSimpleName() + "ContentSpaceName";
+
+	@Override
+	public void start(BundleContext bundleContext) {
+		_createDemoData(bundleContext);
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) {
+		_cleanUp();
+	}
+
+	private void _cleanUp() {
+		try {
+			GroupTestUtil.deleteGroup(_group);
+
+			_permissionCheckerTestCallback.afterMethod(null, null, null);
+		}
+		catch (Throwable t) {
+			_log.error(t, t);
+		}
+	}
+
+	private void _createDemoData(BundleContext bundleContext) {
+		try {
+			_permissionCheckerTestCallback.beforeMethod(null, null);
+
+			_group = GroupTestUtil.addGroup(
+				GroupConstants.DEFAULT_PARENT_GROUP_ID, CONTENT_SPACE_NAME,
+				new ServiceContext());
+
+			AuthConfigurationTestUtil.deployOAuthConfiguration(bundleContext);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		MediaObjectTestActivator.class);
+
+	private static final PermissionCheckerTestCallback
+		_permissionCheckerTestCallback = PermissionCheckerTestCallback.INSTANCE;
+
+	private Group _group;
+
+}
