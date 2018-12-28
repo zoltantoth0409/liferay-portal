99be7e01d151fe2a57c1b02676920fdf9da75339
Cristina González Castellano <cristina.gonzalez@liferay.com>
2018-12-28 12:50:56 +0100

    LPS-88614 Refactor tests also.

--- modules/apps/headless-apio/content-space/content-space-apio-test/src/testIntegration/java/com.liferay.content.space.apio.internal.architect.resource.test/ContentSpaceActionRouterTest.java
+++ modules/apps/headless-apio/content-space/content-space-apio-test/src/testIntegration/java/com.liferay.content.space.apio.internal.architect.resource.test/ContentSpaceActionRouterTest.java
@@ -16,8 +16,9 @@ package com.liferay.content.space.apio.internal.architect.resource.test;
 
 import com.liferay.apio.architect.pagination.PageItems;
 import com.liferay.apio.architect.pagination.Pagination;
-import com.liferay.apio.architect.resource.CollectionResource;
+import com.liferay.apio.architect.router.ActionRouter;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.content.space.apio.architect.model.ContentSpace;
 import com.liferay.portal.apio.test.util.PaginationRequest;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
@@ -27,6 +28,7 @@ import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerTestRule;
@@ -34,6 +36,9 @@ import com.liferay.portal.test.rule.PermissionCheckerTestRule;
 import java.lang.reflect.Method;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -45,7 +50,7 @@ import org.junit.runner.RunWith;
  * @author Julio Camarero
  */
 @RunWith(Arquillian.class)
-public class ContentSpaceCollectionResourceTest {
+public class ContentSpaceActionRouterTest {
 
 	@ClassRule
 	@Rule
@@ -55,19 +60,46 @@ public class ContentSpaceCollectionResourceTest {
 			PermissionCheckerTestRule.INSTANCE);
 
 	@Test
-	public void testGetActiveGroup() throws Throwable {
+	public void testGetContentSpace() throws Throwable {
+		Group group = GroupTestUtil.addGroup();
+
+		try {
+			ContentSpace contentSpace = getContentSpace(group.getGroupId());
+
+			Assert.assertNotNull(contentSpace);
+
+			Locale locale = LocaleUtil.fromLanguageId(
+				group.getDefaultLanguageId());
+
+			Assert.assertEquals(
+				group.getName(locale), contentSpace.getName(locale));
+
+			Assert.assertEquals(
+				group.getDescription(locale),
+				contentSpace.getDescription(locale));
+		}
+		finally {
+			_groupLocalService.deleteGroup(group);
+		}
+	}
+
+	@Test
+	public void testGetPageItemsWithActiveGroup() throws Throwable {
 		Group group = GroupTestUtil.addGroup();
 
 		try {
 			Company company = _companyLocalService.getCompany(
 				group.getCompanyId());
 
-			PageItems<Group> groupPageItems = _getPageItems(
+			PageItems<ContentSpace> contentSpacePageItems = getPageItems(
 				PaginationRequest.of(50, 1), company);
 
-			List<Group> groups = (List<Group>)groupPageItems.getItems();
+			ContentSpace contentSpace = _findContentSpace(
+				(List<ContentSpace>)
+					contentSpacePageItems.getItems(),
+				group.getGroupId());
 
-			Assert.assertTrue(groups.contains(group));
+			Assert.assertNotNull(contentSpace);
 		}
 		finally {
 			_groupLocalService.deleteGroup(group);
@@ -75,23 +107,7 @@ public class ContentSpaceCollectionResourceTest {
 	}
 
 	@Test
-	public void testGetGroup() throws Throwable {
-		Group group = GroupTestUtil.addGroup();
-
-		try {
-			Group finalGroup = _getGroup(group.getGroupId());
-
-			Assert.assertNotNull(finalGroup);
-
-			Assert.assertEquals(group.getName(), finalGroup.getName());
-		}
-		finally {
-			_groupLocalService.deleteGroup(group);
-		}
-	}
-
-	@Test
-	public void testGetInactiveGroup() throws Throwable {
+	public void testGetPageItemsWithInactiveGroup() throws Throwable {
 		Group group = GroupTestUtil.addGroup();
 
 		try {
@@ -100,16 +116,45 @@ public class ContentSpaceCollectionResourceTest {
 			Company company = _companyLocalService.getCompany(
 				group.getCompanyId());
 
-			PageItems<Group> groupPageItems = _getPageItems(
+			PageItems<ContentSpace> contentSpacePageItems = getPageItems(
 				PaginationRequest.of(50, 1), company);
 
-			List<Group> groups = (List<Group>)groupPageItems.getItems();
+			ContentSpace contentSpace = _findContentSpace(
+				(List<ContentSpace>)
+					contentSpacePageItems.getItems(),
+				group.getGroupId());
 
-			Assert.assertFalse(groups.contains(group));
+			Assert.assertNull(contentSpace);
 		}
 		finally {
 			_groupLocalService.deleteGroup(group);
 		}
+	}
+
+	protected ContentSpace getContentSpace(long contentSpaceId)
+		throws Exception {
+
+		Class<? extends ActionRouter> clazz = _actionRouter.getClass();
+
+		Method method = clazz.getDeclaredMethod("getContentSpace", long.class);
+
+		method.setAccessible(true);
+
+		return (ContentSpace)method.invoke(_actionRouter, contentSpaceId);
+	}
+
+	protected PageItems<ContentSpace> getPageItems(
+			Pagination pagination, Company company)
+		throws Exception {
+
+		Class<? extends ActionRouter> clazz = _actionRouter.getClass();
+
+		Method method = clazz.getDeclaredMethod(
+			"getPageItems", Pagination.class, Company.class);
+
+		method.setAccessible(true);
+
+		return (PageItems)method.invoke(_actionRouter, pagination, company);
 	}
 
 	private Group _deactivateGroup(Group group) throws PortalException {
@@ -121,37 +166,23 @@ public class ContentSpaceCollectionResourceTest {
 			new ServiceContext());
 	}
 
-	private Group _getGroup(long groupId) throws Exception {
-		Class<? extends CollectionResource> clazz =
-			_collectionResource.getClass();
+	private ContentSpace _findContentSpace(
+		List<ContentSpace> contentSpaces, long groupId) {
 
-		Method method = clazz.getDeclaredMethod("_getGroup", long.class);
+		Stream<ContentSpace> stream = contentSpaces.stream();
 
-		method.setAccessible(true);
-
-		return (Group)method.invoke(_collectionResource, groupId);
-	}
-
-	private PageItems<Group> _getPageItems(
-			Pagination pagination, Company company)
-		throws Exception {
-
-		Class<? extends CollectionResource> clazz =
-			_collectionResource.getClass();
-
-		Method method = clazz.getDeclaredMethod(
-			"_getPageItems", Pagination.class, Company.class);
-
-		method.setAccessible(true);
-
-		return (PageItems)method.invoke(
-			_collectionResource, pagination, company);
+		return stream.filter(
+			contentSpace -> Objects.equals(contentSpace.getId(), groupId)
+		).findAny(
+		).orElse(
+			null
+		);
 	}
 
 	@Inject(
-		filter = "component.name=com.liferay.content.space.apio.internal.architect.resource.ContentSpaceCollectionResource"
+		filter = "component.name=com.liferay.content.space.apio.internal.architect.resource.ContentSpaceActionRouter"
 	)
-	private CollectionResource _collectionResource;
+	private ActionRouter _actionRouter;
 
 	@Inject
 	private CompanyLocalService _companyLocalService;
