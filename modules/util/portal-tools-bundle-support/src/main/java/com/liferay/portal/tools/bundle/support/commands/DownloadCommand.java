97e782edf18be8bbc2e49be6b6ddcb573f8bb1d8
David Truong <david.truong@liferay.com>
2018-12-30 23:29:08 -0600

    LPS-86705 separate download parts into it's own command

---
+++ modules/util/portal-tools-bundle-support/src/main/java/com/liferay/portal/tools/bundle/support/commands/DownloadCommand.java
@@ -0,0 +1,202 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.bundle.support.commands;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+import com.liferay.portal.tools.bundle.support.constants.BundleSupportConstants;
+import com.liferay.portal.tools.bundle.support.internal.util.FileUtil;
+import com.liferay.portal.tools.bundle.support.internal.util.HttpUtil;
+import com.liferay.portal.tools.bundle.support.util.StreamLogger;
+
+import java.io.File;
+
+import java.net.URI;
+import java.net.URL;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Arrays;
+
+/**
+ * @author David Truong
+ * @author Andrea Di Giorgi
+ */
+@Parameters(commandDescription = "Download a file.", commandNames = "download")
+public class DownloadCommand extends BaseCommand implements StreamLogger {
+
+	@Override
+	public void execute() throws Exception {
+		Path cacheDirPath = null;
+
+		if (_cacheDir != null) {
+			cacheDirPath = _cacheDir.toPath();
+		}
+
+		URI uri = _url.toURI();
+
+		if ("file".equals(uri.getScheme())) {
+			_downloadPath = Paths.get(uri);
+		}
+		else if (_token) {
+			String tokenContent = FileUtil.read(_tokenFile);
+
+			tokenContent = tokenContent.trim();
+
+			_downloadPath = HttpUtil.downloadFile(
+				uri, tokenContent, cacheDirPath, this);
+		}
+		else {
+			_downloadPath = HttpUtil.downloadFile(
+				uri, _userName, _password, cacheDirPath, this);
+		}
+	}
+
+	public File getCacheDir() {
+		return _cacheDir;
+	}
+
+	public Path getDownloadPath() {
+		return _downloadPath;
+	}
+
+	public String getPassword() {
+		return _password;
+	}
+
+	public File getTokenFile() {
+		return _tokenFile;
+	}
+
+	public URL getUrl() {
+		return _url;
+	}
+
+	public String getUserName() {
+		return _userName;
+	}
+
+	public boolean isToken() {
+		return _token;
+	}
+
+	@Override
+	public void onCompleted() {
+		System.out.println();
+	}
+
+	@Override
+	public void onProgress(long completed, long length) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(FileUtil.getFileLength(completed));
+
+		if (length > 0) {
+			sb.append('/');
+			sb.append(FileUtil.getFileLength(length));
+		}
+
+		sb.append(" downloaded");
+
+		onProgress(sb.toString());
+	}
+
+	@Override
+	public void onStarted() {
+		onStarted("Download " + _url);
+	}
+
+	public void setCacheDir(File cacheDir) {
+		_cacheDir = cacheDir;
+	}
+
+	public void setPassword(String password) {
+		_password = password;
+	}
+
+	public void setToken(boolean token) {
+		_token = token;
+	}
+
+	public void setTokenFile(File tokenFile) {
+		_tokenFile = tokenFile;
+	}
+
+	public void setUrl(URL url) {
+		_url = url;
+	}
+
+	public void setUserName(String userName) {
+		_userName = userName;
+	}
+
+	protected void onProgress(String message) {
+		char[] chars = new char[80];
+
+		System.arraycopy(message.toCharArray(), 0, chars, 0, message.length());
+
+		Arrays.fill(chars, message.length(), chars.length - 2, ' ');
+
+		chars[chars.length - 1] = '\r';
+
+		System.out.print(chars);
+	}
+
+	protected void onStarted(String message) {
+		System.out.println(message);
+	}
+
+	@Parameter(
+		description = "The directory where to cache the downloaded bundles.",
+		names = "--cache-dir"
+	)
+	private File _cacheDir = new File(
+		System.getProperty("user.home"),
+		BundleSupportConstants.DEFAULT_BUNDLE_CACHE_DIR_NAME);
+
+	private Path _downloadPath;
+
+	@Parameter(
+		description = "The password if your URL requires authentication.",
+		names = {"-p", "--password"}, password = true
+	)
+	private String _password;
+
+	@Parameter(
+		description = "Use token authentication.", names = {"-t", "--token"}
+	)
+	private boolean _token;
+
+	@Parameter(
+		description = "The file where your liferay.com download token is stored.",
+		names = "--token-file"
+	)
+	private File _tokenFile = BundleSupportConstants.DEFAULT_TOKEN_FILE;
+
+	@Parameter(
+		description = "The URL of the Liferay Bundle to expand.",
+		names = "--url"
+	)
+	private URL _url = BundleSupportConstants.DEFAULT_BUNDLE_URL_OBJECT;
+
+	@Parameter(
+		description = "The user name if your URL requires authentication.",
+		names = {"-u", "--username", "--user-name"}
+	)
+	private String _userName;
+
+}
--- modules/util/portal-tools-bundle-support/src/main/java/com/liferay/portal/tools/bundle/support/commands/InitBundleCommand.java
+++ modules/util/portal-tools-bundle-support/src/main/java/com/liferay/portal/tools/bundle/support/commands/InitBundleCommand.java
@@ -19,25 +19,18 @@ import com.beust.jcommander.Parameters;
 
 import com.liferay.portal.tools.bundle.support.constants.BundleSupportConstants;
 import com.liferay.portal.tools.bundle.support.internal.util.FileUtil;
-import com.liferay.portal.tools.bundle.support.internal.util.HttpUtil;
-import com.liferay.portal.tools.bundle.support.util.StreamLogger;
 
 import java.io.File;
 import java.io.IOException;
 
-import java.net.URI;
-import java.net.URL;
-
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
 
-import java.util.Arrays;
 import java.util.Set;
 
 /**
@@ -48,47 +41,21 @@ import java.util.Set;
 	commandDescription = "Download and expand a new bundle.",
 	commandNames = "initBundle"
 )
-public class InitBundleCommand extends BaseCommand implements StreamLogger {
+public class InitBundleCommand extends DownloadCommand {
 
 	@Override
 	public void execute() throws Exception {
 		_deleteBundle();
 
-		Path cacheDirPath = null;
-
-		if (_cacheDir != null) {
-			cacheDirPath = _cacheDir.toPath();
-		}
-
-		Path path;
-
-		URI uri = _url.toURI();
-
-		if ("file".equals(uri.getScheme())) {
-			path = Paths.get(uri);
-		}
-		else if (_token) {
-			String tokenContent = FileUtil.read(_tokenFile);
-
-			tokenContent = tokenContent.trim();
-
-			path = HttpUtil.downloadFile(uri, tokenContent, cacheDirPath, this);
-		}
-		else {
-			path = HttpUtil.downloadFile(
-				uri, _userName, _password, cacheDirPath, this);
-		}
+		super.execute();
 
 		File liferayHomeDir = getLiferayHomeDir();
 
-		FileUtil.unpack(path, liferayHomeDir.toPath(), _stripComponents);
+		FileUtil.unpack(
+			getDownloadPath(), liferayHomeDir.toPath(), _stripComponents);
 
 		_copyConfigs();
 		_fixPosixFilePermissions();
-	}
-
-	public File getCacheDir() {
-		return _cacheDir;
 	}
 
 	public File getConfigsDir() {
@@ -99,58 +66,8 @@ public class InitBundleCommand extends BaseCommand implements StreamLogger {
 		return _environment;
 	}
 
-	public String getPassword() {
-		return _password;
-	}
-
 	public int getStripComponents() {
 		return _stripComponents;
-	}
-
-	public File getTokenFile() {
-		return _tokenFile;
-	}
-
-	public URL getUrl() {
-		return _url;
-	}
-
-	public String getUserName() {
-		return _userName;
-	}
-
-	public boolean isToken() {
-		return _token;
-	}
-
-	@Override
-	public void onCompleted() {
-		System.out.println();
-	}
-
-	@Override
-	public void onProgress(long completed, long length) {
-		StringBuilder sb = new StringBuilder();
-
-		sb.append(FileUtil.getFileLength(completed));
-
-		if (length > 0) {
-			sb.append('/');
-			sb.append(FileUtil.getFileLength(length));
-		}
-
-		sb.append(" downloaded");
-
-		onProgress(sb.toString());
-	}
-
-	@Override
-	public void onStarted() {
-		onStarted("Download " + _url);
-	}
-
-	public void setCacheDir(File cacheDir) {
-		_cacheDir = cacheDir;
 	}
 
 	public void setConfigsDir(File configsDir) {
@@ -161,44 +78,8 @@ public class InitBundleCommand extends BaseCommand implements StreamLogger {
 		_environment = environment;
 	}
 
-	public void setPassword(String password) {
-		_password = password;
-	}
-
 	public void setStripComponents(int stripComponents) {
 		_stripComponents = stripComponents;
-	}
-
-	public void setToken(boolean token) {
-		_token = token;
-	}
-
-	public void setTokenFile(File tokenFile) {
-		_tokenFile = tokenFile;
-	}
-
-	public void setUrl(URL url) {
-		_url = url;
-	}
-
-	public void setUserName(String userName) {
-		_userName = userName;
-	}
-
-	protected void onProgress(String message) {
-		char[] chars = new char[80];
-
-		System.arraycopy(message.toCharArray(), 0, chars, 0, message.length());
-
-		Arrays.fill(chars, message.length(), chars.length - 2, ' ');
-
-		chars[chars.length - 1] = '\r';
-
-		System.out.print(chars);
-	}
-
-	protected void onStarted(String message) {
-		System.out.println(message);
 	}
 
 	private void _copyConfigs() throws IOException {
@@ -267,14 +148,6 @@ public class InitBundleCommand extends BaseCommand implements StreamLogger {
 		PosixFilePermissions.fromString("rwxr-x---");
 
 	@Parameter(
-		description = "The directory where to cache the downloaded bundles.",
-		names = "--cache-dir"
-	)
-	private File _cacheDir = new File(
-		System.getProperty("user.home"),
-		BundleSupportConstants.DEFAULT_BUNDLE_CACHE_DIR_NAME);
-
-	@Parameter(
 		description = "The directory that contains the configuration files.",
 		names = "--configs"
 	)
@@ -287,39 +160,10 @@ public class InitBundleCommand extends BaseCommand implements StreamLogger {
 	private String _environment = BundleSupportConstants.DEFAULT_ENVIRONMENT;
 
 	@Parameter(
-		description = "The password if your URL requires authentication.",
-		names = {"-p", "--password"}, password = true
-	)
-	private String _password;
-
-	@Parameter(
 		description = "The number of directories to strip when expanding your bundle.",
 		names = "--strip-components"
 	)
 	private int _stripComponents =
 		BundleSupportConstants.DEFAULT_STRIP_COMPONENTS;
-
-	@Parameter(
-		description = "Use token authentication.", names = {"-t", "--token"}
-	)
-	private boolean _token;
-
-	@Parameter(
-		description = "The file where your liferay.com download token is stored.",
-		names = "--token-file"
-	)
-	private File _tokenFile = BundleSupportConstants.DEFAULT_TOKEN_FILE;
-
-	@Parameter(
-		description = "The URL of the Liferay Bundle to expand.",
-		names = "--url"
-	)
-	private URL _url = BundleSupportConstants.DEFAULT_BUNDLE_URL_OBJECT;
-
-	@Parameter(
-		description = "The user name if your URL requires authentication.",
-		names = {"-u", "--username", "--user-name"}
-	)
-	private String _userName;
 
 }
