6678ab174729ac581b76e734676fa724c4d1dc30
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-12-31 09:33:51 -0300

    LPS-88735 Access ClassName value directly

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -4705,7 +4705,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 		ClassName className = ClassNameServiceHttp.fetchByClassNameId(
 			httpPrincipal, group.getClassNameId());
 
-		if (Objects.equals(className.getClassName(), Company.class.getName())) {
+		if (Objects.equals(className.getValue(), Company.class.getName())) {
 			return true;
 		}
 
