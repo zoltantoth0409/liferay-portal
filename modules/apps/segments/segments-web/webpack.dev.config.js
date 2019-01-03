86caaa3684c098e5554c3578b41a52acbaa75af6
Kevin Tan <kevin.tan@liferay.com>
2019-01-03 14:13:36 -0800

    LPS-88575 Remove trailing commas

--- modules/apps/segments/segments-web/webpack.dev.config.js
+++ modules/apps/segments/segments-web/webpack.dev.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
 	devServer: {
@@ -9,13 +9,13 @@ module.exports = {
 		openPage: 'o/segments-web/',
 		port: 9000,
 		proxy: {
-			'**': 'http://0.0.0.0:8080',
+			'**': 'http://0.0.0.0:8080'
 		},
-		publicPath: '/o/segments-web/',
+		publicPath: '/o/segments-web/'
 	},
 	entry: {
-		'ODataParser': path.resolve(__dirname,'src','main','resources','META-INF','resources','js','libs','ODataParser.es.js'),
-		'index.dev': path.resolve(__dirname,'src','main','resources','META-INF','resources','js','index.dev.js'),
+		'ODataParser': path.resolve(__dirname, 'src', 'main', 'resources', 'META-INF', 'resources', 'js', 'libs', 'ODataParser.es.js'),
+		'index.dev': path.resolve(__dirname, 'src', 'main', 'resources', 'META-INF', 'resources', 'js', 'index.dev.js')
 	},
 	mode: 'development',
 	module: {
@@ -25,34 +25,34 @@ module.exports = {
 				test: /\.(js|jsx)$/,
 				use: [
 					{
-						loader: 'babel-loader',
+						loader: 'babel-loader'
 					},
-					'liferay-lang-key-dev-loader',
-				],
+					'liferay-lang-key-dev-loader'
+				]
 			},
 			{
 				test: /\.(scss|css)$/,
 				use: [
 					{loader: 'style-loader'},
 					{loader: 'css-loader'},
-					{loader: 'sass-loader'},
-				],
-			},
-		],
+					{loader: 'sass-loader'}
+				]
+			}
+		]
 	},
 	output: {
 		filename: `[name].js`,
 		library: 'oDataParser',
 		libraryTarget: 'window',
-		path: path.resolve(__dirname,'dev-build'),
+		path: path.resolve(__dirname,'dev-build')
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			appMountId: 'app',
-			template: require('html-webpack-template'),
-		}),
+			template: require('html-webpack-template')
+		})
 	],
 	resolve: {
-		extensions: ['.js', '.jsx', '.svg'],
-	},
+		extensions: ['.js', '.jsx', '.svg']
+	}
 };
