c237fa679c5cdc5eea45bec71e8d123bc4700a36
Alberto Chaparro <alberto.chaparro@liferay.com>
2019-01-03 16:46:35 +0100

    LPS-88381 Upgrade from 6.1.x is not longer supported. Classes associated to this upgrade path has been removed

--- sql/update-6.1.1-6.2.0-expando.sql
+++
@@ -1,5 +0,0 @@
-alter table ExpandoRow add modifiedDate DATE null;
-
-COMMIT_TRANSACTION;
-
-update ExpandoRow set modifiedDate = CURRENT_TIMESTAMP;
--- sql/update-6.1.1-6.2.0-user.sql
+++
@@ -1,5 +0,0 @@
-alter table User_ add ldapServerId LONG;
-
-COMMIT_TRANSACTION;
-
-update User_ set ldapServerId = -1;
