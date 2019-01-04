9916ea63412b6906ce26fe38bd1c8a38514bc78a
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-01-04 15:17:46 +0100

    LPS-88809 Add a context criteria contributor

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/criteria/contributor/ContextSegmentsCriteriaContributor.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.criteria.contributor;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.segments.criteria.Criteria;
+import com.liferay.segments.criteria.Field;
+import com.liferay.segments.criteria.contributor.SegmentsCriteriaContributor;
+import com.liferay.segments.internal.odata.entity.ContextEntityModel;
+import com.liferay.segments.internal.odata.entity.EntityModelFieldMapper;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author Eduardo García
+ */
+@Component(
+	immediate = true,
+	property = {
+		"segments.criteria.contributor.key=" + ContextSegmentsCriteriaContributor.KEY,
+		"segments.criteria.contributor.model.class.name=*",
+		"segments.criteria.contributor.priority:Integer=1"
+	},
+	service = SegmentsCriteriaContributor.class
+)
+public class ContextSegmentsCriteriaContributor
+	implements SegmentsCriteriaContributor {
+
+	public static final String KEY = "context";
+
+	@Override
+	public List<Field> getFields(Locale locale) {
+		return _entityModelFieldMapper.getFields(_entityModel, locale);
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, getKey());
+	}
+
+	@Override
+	public Criteria.Type getType() {
+		return Criteria.Type.CONTEXT;
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MANDATORY,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(entity.model.name=" + ContextEntityModel.NAME + ")"
+	)
+	private volatile EntityModel _entityModel;
+
+	@Reference
+	private EntityModelFieldMapper _entityModelFieldMapper;
+
+}
