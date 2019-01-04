c339933b2b3a46e3140ea22e37e1197b9dd55a87
tina.tian <tina.tian@liferay.com>
2019-01-04 15:43:33 -0800

    LPS-88547 Deprecate unused class

--- portal-kernel/src/com/liferay/portal/kernel/cache/configurator/PortalCacheConfiguratorSettings.java
+++ portal-kernel/src/com/liferay/portal/kernel/cache/configurator/PortalCacheConfiguratorSettings.java
@@ -16,7 +16,9 @@ package com.liferay.portal.kernel.cache.configurator;
 
 /**
  * @author Tina Tian
+ * @deprecated As of Mueller (7.2.x), with no direct replacement
  */
+@Deprecated
 public class PortalCacheConfiguratorSettings {
 
 	public PortalCacheConfiguratorSettings(
