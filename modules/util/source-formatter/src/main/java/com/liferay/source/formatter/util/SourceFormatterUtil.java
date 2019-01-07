8fbb86d5f6f1d159350f8a00d36d5c51fc83d476
Hugo Huijser <hugo.huijser@liferay.com>
2019-01-07 09:23:59 +0800

    LPS-87469 Add property to exclude a specific properties file when retrieving a property value from source-formatter.properties

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/util/SourceFormatterUtil.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/util/SourceFormatterUtil.java
@@ -45,6 +45,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -270,9 +271,22 @@ public class SourceFormatterUtil {
 	public static String getPropertyValue(
 		String propertyName, Map<String, Properties> propertiesMap) {
 
+		return getPropertyValue(propertyName, propertiesMap, null);
+	}
+
+	public static String getPropertyValue(
+		String propertyName, Map<String, Properties> propertiesMap,
+		String excludedPropertiesFileLocation) {
+
 		StringBundler sb = new StringBundler(propertiesMap.size() * 2);
 
 		for (Map.Entry<String, Properties> entry : propertiesMap.entrySet()) {
+			if (Objects.equals(
+					entry.getKey(), excludedPropertiesFileLocation)) {
+
+				continue;
+			}
+
 			Properties properties = entry.getValue();
 
 			String value = properties.getProperty(propertyName);
