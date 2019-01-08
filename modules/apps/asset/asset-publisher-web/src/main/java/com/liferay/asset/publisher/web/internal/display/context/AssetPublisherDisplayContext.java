89c363a5f746777848cc5600b311189600a9ee9d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-01-08 16:11:21 +0100

    LPS-88432 Clean code

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
@@ -52,7 +52,6 @@ import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
@@ -116,11 +115,10 @@ public class AssetPublisherDisplayContext {
 	};
 
 	public AssetPublisherDisplayContext(
-			AssetPublisherCustomizer assetPublisherCustomizer,
-            AssetPublisherHelper assetPublisherHelper,
-			PortletRequest portletRequest, PortletResponse portletResponse,
-			PortletPreferences portletPreferences)
-		throws ConfigurationException {
+        AssetPublisherCustomizer assetPublisherCustomizer,
+        AssetPublisherHelper assetPublisherHelper,
+        PortletRequest portletRequest, PortletResponse portletResponse,
+        PortletPreferences portletPreferences) {
 
 		_assetPublisherCustomizer = assetPublisherCustomizer;
 		_assetPublisherHelper = assetPublisherHelper;
@@ -199,7 +197,7 @@ public class AssetPublisherDisplayContext {
 		return _allAssetCategoryIds;
 	}
 
-	public String[] getAllAssetTagNames() throws Exception {
+	public String[] getAllAssetTagNames() {
 		if (_allAssetTagNames != null) {
 			return _allAssetTagNames;
 		}
@@ -321,12 +319,6 @@ public class AssetPublisherDisplayContext {
 		portletURL.setWindowState(LiferayWindowState.POP_UP);
 
 		return portletURL.toString();
-	}
-
-	public AssetPublisherPortletInstanceConfiguration
-		getAssetPublisherPortletInstanceConfiguration() {
-
-		return _assetPublisherPortletInstanceConfiguration;
 	}
 
 	public Map<String, Serializable> getAttributes() {
@@ -997,7 +989,7 @@ public class AssetPublisherDisplayContext {
 		return _userId;
 	}
 
-	public String getVocabularyIds() throws Exception {
+	public String getVocabularyIds() {
 		List<AssetVocabulary> vocabularies =
 			AssetVocabularyServiceUtil.getGroupsVocabularies(getGroupIds());
 
@@ -1092,7 +1084,7 @@ public class AssetPublisherDisplayContext {
 		return _enableComments;
 	}
 
-	public Boolean isEnableConversions() throws Exception {
+	public Boolean isEnableConversions() {
 		if (_enableConversions != null) {
 			return _enableConversions;
 		}
@@ -1222,10 +1214,6 @@ public class AssetPublisherDisplayContext {
 		return DocumentConversionUtil.isEnabled();
 	}
 
-	public boolean isOrderingAndGroupingEnabled() {
-		return _assetPublisherCustomizer.isOrderingAndGroupingEnabled(_request);
-	}
-
 	public boolean isOrderingByTitleEnabled() {
 		return _assetPublisherCustomizer.isOrderingByTitleEnabled(_request);
 	}
@@ -1233,7 +1221,11 @@ public class AssetPublisherDisplayContext {
 	public boolean isPaginationTypeNone() {
 		String paginationType = getPaginationType();
 
-		return paginationType.equals(PAGINATION_TYPE_NONE);
+		if (Objects.equals(paginationType, PAGINATION_TYPE_NONE)) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public boolean isPaginationTypeSelected(String paginationType) {
@@ -1261,10 +1253,6 @@ public class AssetPublisherDisplayContext {
 		String selectionStyle = getSelectionStyle();
 
 		return selectionStyle.equals("dynamic");
-	}
-
-	public boolean isSelectionStyleEnabled() {
-		return _assetPublisherCustomizer.isSelectionStyleEnabled(_request);
 	}
 
 	public boolean isSelectionStyleManual() throws PortalException {
@@ -1394,16 +1382,6 @@ public class AssetPublisherDisplayContext {
 		return _showOnlyLayoutAssets;
 	}
 
-	public boolean isShowScopeSelector() {
-		String rootPortletId = getRootPortletId();
-
-		if (rootPortletId.equals(AssetPublisherPortletKeys.RELATED_ASSETS)) {
-			return false;
-		}
-
-		return true;
-	}
-
 	public boolean isShowSubtypeFieldsFilter() {
 		return _assetPublisherCustomizer.isShowSubtypeFieldsFilter(_request);
 	}
@@ -1446,10 +1424,6 @@ public class AssetPublisherDisplayContext {
 
 	public void setSelectionStyle(String selectionStyle) {
 		_selectionStyle = selectionStyle;
-	}
-
-	public void setShowContextLink(Boolean showContextLink) {
-		_showContextLink = showContextLink;
 	}
 
 	protected void configureSubtypeFieldFilter(
@@ -1588,7 +1562,7 @@ public class AssetPublisherDisplayContext {
 	private Boolean _anyAssetType;
 	private final AssetEntryActionRegistry _assetEntryActionRegistry;
 	private AssetEntryQuery _assetEntryQuery;
-	private AssetHelper _assetHelper;
+	private final AssetHelper _assetHelper;
 	private String _assetLinkBehavior;
 	private AssetListEntry _assetListEntry;
 	private final AssetPublisherCustomizer _assetPublisherCustomizer;
