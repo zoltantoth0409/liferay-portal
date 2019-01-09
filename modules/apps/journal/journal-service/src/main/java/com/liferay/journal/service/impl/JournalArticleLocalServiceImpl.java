a7e7e46b329e5e7df5d9babf6b63d6e95d3996c1
Brian Chan <brian.chan@liferay.com>
2019-01-08 21:58:13 -0800

    LPS-88424 SF

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -6918,7 +6918,7 @@ public class JournalArticleLocalServiceImpl
 
 		long checkInterval = getArticleCheckInterval();
 
-		final ActionableDynamicQuery actionableDynamicQuery =
+		ActionableDynamicQuery actionableDynamicQuery =
 			getActionableDynamicQuery();
 
 		actionableDynamicQuery.setAddCriteriaMethod(
