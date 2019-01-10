2aa065e50662fded33f59efdb8d74113ab550362
Bryan Engler <bryan.engler@liferay.com>
2019-01-10 10:26:05 -0800

    LPS-86743 portal-search-solr7-impl: Search methods
    https://issues.liferay.com/browse/LPS-86743

--- modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/search/engine/adapter/search/CountSearchRequestExecutorImpl.java
+++ modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/search/engine/adapter/search/CountSearchRequestExecutorImpl.java
@@ -14,10 +14,22 @@
 
 package com.liferay.portal.search.solr7.internal.search.engine.adapter.search;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.search.engine.adapter.search.CountSearchRequest;
 import com.liferay.portal.search.engine.adapter.search.CountSearchResponse;
+import com.liferay.portal.search.solr7.internal.connection.SolrClientManager;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Bryan Engler
@@ -28,7 +40,85 @@ public class CountSearchRequestExecutorImpl
 
 	@Override
 	public CountSearchResponse execute(CountSearchRequest countSearchRequest) {
-		throw new UnsupportedOperationException();
+		SolrQuery solrQuery = new SolrQuery();
+
+		_baseSolrQueryAssembler.assemble(solrQuery, countSearchRequest);
+
+		solrQuery.setRows(0);
+
+		String requestString = solrQuery.toString();
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Search query " + requestString);
+		}
+
+		QueryResponse queryResponse = getQueryResponse(
+			new QueryRequest(solrQuery), countSearchRequest.getIndexNames());
+
+		CountSearchResponse countSearchResponse = new CountSearchResponse();
+
+		_baseSearchResponseAssembler.assemble(
+			countSearchResponse, solrQuery, queryResponse, countSearchRequest);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				StringBundler.concat(
+					"The search engine processed ",
+					countSearchResponse.getSearchRequestString(), " in ",
+					countSearchResponse.getExecutionTime() + " ms"));
+		}
+
+		SolrDocumentList solrDocumentList = queryResponse.getResults();
+
+		countSearchResponse.setCount(solrDocumentList.getNumFound());
+
+		return countSearchResponse;
 	}
+
+	protected QueryResponse getQueryResponse(
+		QueryRequest queryRequest, String[] indexNames) {
+
+		try {
+			queryRequest.setMethod(SolrRequest.METHOD.POST);
+
+			return queryRequest.process(
+				_solrClientManager.getSolrClient(), indexNames[0]);
+		}
+		catch (Exception e) {
+			if (e instanceof SolrException) {
+				SolrException se = (SolrException)e;
+
+				throw se;
+			}
+
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setBaseSearchResponseAssembler(
+		BaseSearchResponseAssembler baseSearchResponseAssembler) {
+
+		_baseSearchResponseAssembler = baseSearchResponseAssembler;
+	}
+
+	@Reference(unbind = "-")
+	protected void setBaseSolrQueryAssembler(
+		BaseSolrQueryAssembler baseSolrQueryAssembler) {
+
+		_baseSolrQueryAssembler = baseSolrQueryAssembler;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSolrClientManager(SolrClientManager solrClientManager) {
+		_solrClientManager = solrClientManager;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CountSearchRequestExecutorImpl.class);
+
+	private BaseSearchResponseAssembler _baseSearchResponseAssembler;
+	private BaseSolrQueryAssembler _baseSolrQueryAssembler;
+	private SolrClientManager _solrClientManager;
 
 }
--- modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/search/engine/adapter/search/SearchSearchRequestExecutorImpl.java
+++ modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/search/engine/adapter/search/SearchSearchRequestExecutorImpl.java
@@ -14,10 +14,25 @@
 
 package com.liferay.portal.search.solr7.internal.search.engine.adapter.search;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.URLCodec;
 import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
 import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.solr7.internal.connection.SolrClientManager;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Bryan Engler
@@ -30,7 +45,92 @@ public class SearchSearchRequestExecutorImpl
 	public SearchSearchResponse execute(
 		SearchSearchRequest searchSearchRequest) {
 
-		throw new UnsupportedOperationException();
+		SolrQuery solrQuery = new SolrQuery();
+
+		_searchSolrQueryAssembler.assemble(solrQuery, searchSearchRequest);
+
+		String requestString = solrQuery.toString();
+
+		String debugRequestString = _getDebugRequestString(requestString);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Search query: " + debugRequestString);
+		}
+
+		QueryResponse queryResponse = getQueryResponse(
+			new QueryRequest(solrQuery), searchSearchRequest.getIndexNames());
+
+		SearchSearchResponse searchSearchResponse = new SearchSearchResponse();
+
+		_searchSearchResponseAssembler.assemble(
+			searchSearchResponse, solrQuery, queryResponse,
+			searchSearchRequest);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				StringBundler.concat(
+					"The search engine processed ",
+					searchSearchResponse.getSearchRequestString(), " in ",
+					searchSearchResponse.getExecutionTime() + " ms"));
+		}
+
+		return searchSearchResponse;
 	}
+
+	protected QueryResponse getQueryResponse(
+		QueryRequest queryRequest, String[] indexNames) {
+
+		try {
+			queryRequest.setMethod(SolrRequest.METHOD.POST);
+
+			return queryRequest.process(
+				_solrClientManager.getSolrClient(), indexNames[0]);
+		}
+		catch (Exception e) {
+			if (e instanceof SolrException) {
+				SolrException se = (SolrException)e;
+
+				throw se;
+			}
+
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Reference(unbind = "-")
+	protected void setSearchSearchResponseAssembler(
+		SearchSearchResponseAssembler searchSearchResponseAssembler) {
+
+		_searchSearchResponseAssembler = searchSearchResponseAssembler;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSearchSolrQueryAssembler(
+		SearchSolrQueryAssembler searchSolrQueryAssembler) {
+
+		_searchSolrQueryAssembler = searchSolrQueryAssembler;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSolrClientManager(SolrClientManager solrClientManager) {
+		_solrClientManager = solrClientManager;
+	}
+
+	private String _getDebugRequestString(String requestString) {
+		requestString = URLCodec.decodeURL(requestString);
+
+		requestString = StringUtil.replace(
+			requestString, CharPool.AMPERSAND,
+			StringPool.NEW_LINE + StringPool.AMPERSAND);
+
+		return requestString;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SearchSearchRequestExecutorImpl.class);
+
+	private SearchSearchResponseAssembler _searchSearchResponseAssembler;
+	private SearchSolrQueryAssembler _searchSolrQueryAssembler;
+	private SolrClientManager _solrClientManager;
 
 }
