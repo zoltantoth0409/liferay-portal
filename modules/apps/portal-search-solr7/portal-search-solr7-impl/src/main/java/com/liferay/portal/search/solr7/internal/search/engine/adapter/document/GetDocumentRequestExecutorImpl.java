64732bd656adbe65f50685202939cb48708570c1
Bryan Engler <bryan.engler@liferay.com>
2019-01-10 10:46:44 -0800

    LPS-86743 portal-search-solr7-impl: Document methods
    https://issues.liferay.com/browse/LPS-86743

--- modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/search/engine/adapter/document/GetDocumentRequestExecutorImpl.java
+++ modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/search/engine/adapter/document/GetDocumentRequestExecutorImpl.java
@@ -14,10 +14,26 @@
 
 package com.liferay.portal.search.solr7.internal.search.engine.adapter.document;
 
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.search.document.DocumentBuilder;
+import com.liferay.portal.search.document.DocumentBuilderFactory;
+import com.liferay.portal.search.engine.adapter.document.BulkableDocumentRequestTranslator;
 import com.liferay.portal.search.engine.adapter.document.GetDocumentRequest;
 import com.liferay.portal.search.engine.adapter.document.GetDocumentResponse;
+import com.liferay.portal.search.solr7.internal.connection.SolrClientManager;
+import com.liferay.portal.search.solr7.internal.document.DocumentFieldsTranslator;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Bryan Engler
@@ -28,7 +44,76 @@ public class GetDocumentRequestExecutorImpl
 
 	@Override
 	public GetDocumentResponse execute(GetDocumentRequest getDocumentRequest) {
-		throw new UnsupportedOperationException();
+		QueryRequest queryRequest =
+			_bulkableDocumentRequestTranslator.translate(getDocumentRequest);
+
+		try {
+			QueryResponse queryResponse = queryRequest.process(
+				_solrClientManager.getSolrClient(),
+				getDocumentRequest.getIndexName());
+
+			SolrDocumentList solrDocumentList = queryResponse.getResults();
+
+			if (solrDocumentList.isEmpty()) {
+				return new GetDocumentResponse(false);
+			}
+
+			SolrDocument solrDocument = solrDocumentList.get(0);
+
+			GetDocumentResponse getDocumentResponse = new GetDocumentResponse(
+				true);
+
+			Map<String, Collection<Object>> fieldValuesMap =
+				solrDocument.getFieldValuesMap();
+
+			getDocumentResponse.setSource(fieldValuesMap.toString());
+
+			DocumentBuilder documentBuilder = _documentBuilderFactory.builder();
+
+			DocumentFieldsTranslator.translate(documentBuilder, solrDocument);
+
+			getDocumentResponse.setDocument(documentBuilder.build());
+
+			getDocumentResponse.setVersion(
+				GetterUtil.getLong(solrDocument.getFieldValue(_VERSION_FIELD)));
+
+			return getDocumentResponse;
+		}
+		catch (Exception e) {
+			if (e instanceof SolrException) {
+				SolrException se = (SolrException)e;
+
+				throw se;
+			}
+
+			throw new RuntimeException(e);
+		}
 	}
+
+	@Reference(target = "(search.engine.impl=Solr)", unbind = "-")
+	protected void setBulkableDocumentRequestTranslator(
+		BulkableDocumentRequestTranslator bulkableDocumentRequestTranslator) {
+
+		_bulkableDocumentRequestTranslator = bulkableDocumentRequestTranslator;
+	}
+
+	@Reference(unbind = "-")
+	protected void setDocumentBuilderFactory(
+		DocumentBuilderFactory documentBuilderFactory) {
+
+		_documentBuilderFactory = documentBuilderFactory;
+	}
+
+	@Reference(unbind = "-")
+	protected void setSolrClientManager(SolrClientManager solrClientManager) {
+		_solrClientManager = solrClientManager;
+	}
+
+	private static final String _VERSION_FIELD = "_version_";
+
+	private BulkableDocumentRequestTranslator
+		_bulkableDocumentRequestTranslator;
+	private DocumentBuilderFactory _documentBuilderFactory;
+	private SolrClientManager _solrClientManager;
 
 }
--- modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/util/LogUtil.java
+++ modules/apps/portal-search-solr7/portal-search-solr7-impl/src/main/java/com/liferay/portal/search/solr7/internal/util/LogUtil.java
@@ -17,31 +17,40 @@ package com.liferay.portal.search.solr7.internal.util;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.log.Log;
 
-import org.apache.solr.client.solrj.response.SolrResponseBase;
+import org.apache.solr.client.solrj.SolrResponse;
+import org.apache.solr.common.SolrException;
 
 /**
  * @author Michael C. Han
  */
 public class LogUtil {
 
-	public static void logSolrResponseBase(
-		Log log, SolrResponseBase solrResponseBase) {
+	public static void logSolrException(Log log, SolrException solrException) {
+		if (log.isWarnEnabled()) {
+			StringBundler sb = new StringBundler(5);
 
-		if (log.isInfoEnabled()) {
-			return;
+			sb.append("{class=");
+			sb.append(solrException.getClass());
+			sb.append(", message=");
+			sb.append(solrException.getMessage());
+			sb.append("}");
+
+			log.warn(sb);
 		}
+	}
 
-		StringBundler sb = new StringBundler(7);
+	public static void logSolrResponse(Log log, SolrResponse solrResponse) {
+		if (log.isInfoEnabled()) {
+			StringBundler sb = new StringBundler(5);
 
-		sb.append("{elapsedTime=");
-		sb.append(solrResponseBase.getElapsedTime());
-		sb.append(", requestURL=");
-		sb.append(solrResponseBase.getRequestUrl());
-		sb.append(", response=");
-		sb.append(solrResponseBase);
-		sb.append("}");
+			sb.append("{elapsedTime=");
+			sb.append(solrResponse.getElapsedTime());
+			sb.append(", response=");
+			sb.append(solrResponse);
+			sb.append("}");
 
-		log.info(sb);
+			log.info(sb);
+		}
 	}
 
 }
