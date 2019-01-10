e4f5c2456c5737eb45644b5ac276bb7223342804
Brian Chan <brian.chan@liferay.com>
2019-01-10 14:17:39 -0800

    LPS-88698 move to proper dir

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/asset/util/JournalArticleAssetEntryUsageChecker.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/asset/util/JournalArticleAssetEntryUsageChecker.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.journal.internal.util;
+package com.liferay.journal.internal.asset.util;
 
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.model.AssetEntryUsage;
