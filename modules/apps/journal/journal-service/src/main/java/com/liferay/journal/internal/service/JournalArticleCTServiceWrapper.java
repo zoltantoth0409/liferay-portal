e1b26728138ff711fc5a72b604044965f9991366
Gergely Mathe <gergely.mathe@liferay.com>
2019-01-10 13:10:59 +0100

    LPS-88848 Create JournalArticleCTServiceWrapper with add and update operations

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/service/JournalArticleCTServiceWrapper.java
@@ -0,0 +1,379 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.service;
+
+import com.liferay.change.tracking.CTManager;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.journal.service.JournalArticleLocalServiceWrapper;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.io.File;
+import java.io.Serializable;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gergely Mathe
+ */
+@Component(immediate = true, service = ServiceWrapper.class)
+public class JournalArticleCTServiceWrapper
+	extends JournalArticleLocalServiceWrapper {
+
+	public JournalArticleCTServiceWrapper() {
+		super(null);
+	}
+
+	public JournalArticleCTServiceWrapper(
+		JournalArticleLocalService journalArticleLocalService) {
+
+		super(journalArticleLocalService);
+	}
+
+	@Override
+	public JournalArticle addArticle(
+			long userId, long groupId, long folderId, long classNameId,
+			long classPK, String articleId, boolean autoArticleId,
+			double version, Map<Locale, String> titleMap,
+			Map<Locale, String> descriptionMap,
+			Map<Locale, String> friendlyURLMap, String content,
+			String ddmStructureKey, String ddmTemplateKey, String layoutUuid,
+			int displayDateMonth, int displayDateDay, int displayDateYear,
+			int displayDateHour, int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, int reviewDateMonth, int reviewDateDay,
+			int reviewDateYear, int reviewDateHour, int reviewDateMinute,
+			boolean neverReview, boolean indexable, boolean smallImage,
+			String smallImageURL, File smallImageFile,
+			Map<String, byte[]> images, String articleURL,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.addArticle(
+			userId, groupId, folderId, classNameId, classPK, articleId,
+			autoArticleId, version, titleMap, descriptionMap, friendlyURLMap,
+			content, ddmStructureKey, ddmTemplateKey, layoutUuid,
+			displayDateMonth, displayDateDay, displayDateYear, displayDateHour,
+			displayDateMinute, expirationDateMonth, expirationDateDay,
+			expirationDateYear, expirationDateHour, expirationDateMinute,
+			neverExpire, reviewDateMonth, reviewDateDay, reviewDateYear,
+			reviewDateHour, reviewDateMinute, neverReview, indexable,
+			smallImage, smallImageURL, smallImageFile, images, articleURL,
+			serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle addArticle(
+			long userId, long groupId, long folderId, long classNameId,
+			long classPK, String articleId, boolean autoArticleId,
+			double version, Map<Locale, String> titleMap,
+			Map<Locale, String> descriptionMap, String content,
+			String ddmStructureKey, String ddmTemplateKey, String layoutUuid,
+			int displayDateMonth, int displayDateDay, int displayDateYear,
+			int displayDateHour, int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, int reviewDateMonth, int reviewDateDay,
+			int reviewDateYear, int reviewDateHour, int reviewDateMinute,
+			boolean neverReview, boolean indexable, boolean smallImage,
+			String smallImageURL, File smallImageFile,
+			Map<String, byte[]> images, String articleURL,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.addArticle(
+			userId, groupId, folderId, classNameId, classPK, articleId,
+			autoArticleId, version, titleMap, descriptionMap, content,
+			ddmStructureKey, ddmTemplateKey, layoutUuid, displayDateMonth,
+			displayDateDay, displayDateYear, displayDateHour, displayDateMinute,
+			expirationDateMonth, expirationDateDay, expirationDateYear,
+			expirationDateHour, expirationDateMinute, neverExpire,
+			reviewDateMonth, reviewDateDay, reviewDateYear, reviewDateHour,
+			reviewDateMinute, neverReview, indexable, smallImage, smallImageURL,
+			smallImageFile, images, articleURL, serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle addArticle(
+			long userId, long groupId, long folderId,
+			Map<Locale, String> titleMap, Map<Locale, String> descriptionMap,
+			String content, String ddmStructureKey, String ddmTemplateKey,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.addArticle(
+			userId, groupId, folderId, titleMap, descriptionMap, content,
+			ddmStructureKey, ddmTemplateKey, serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateArticle(
+			long userId, long groupId, long folderId, String articleId,
+			double version, Map<Locale, String> titleMap,
+			Map<Locale, String> descriptionMap,
+			Map<Locale, String> friendlyURLMap, String content,
+			String ddmStructureKey, String ddmTemplateKey, String layoutUuid,
+			int displayDateMonth, int displayDateDay, int displayDateYear,
+			int displayDateHour, int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, int reviewDateMonth, int reviewDateDay,
+			int reviewDateYear, int reviewDateHour, int reviewDateMinute,
+			boolean neverReview, boolean indexable, boolean smallImage,
+			String smallImageURL, File smallImageFile,
+			Map<String, byte[]> images, String articleURL,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateArticle(
+			userId, groupId, folderId, articleId, version, titleMap,
+			descriptionMap, friendlyURLMap, content, ddmStructureKey,
+			ddmTemplateKey, layoutUuid, displayDateMonth, displayDateDay,
+			displayDateYear, displayDateHour, displayDateMinute,
+			expirationDateMonth, expirationDateDay, expirationDateYear,
+			expirationDateHour, expirationDateMinute, neverExpire,
+			reviewDateMonth, reviewDateDay, reviewDateYear, reviewDateHour,
+			reviewDateMinute, neverReview, indexable, smallImage, smallImageURL,
+			smallImageFile, images, articleURL, serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateArticle(
+			long userId, long groupId, long folderId, String articleId,
+			double version, Map<Locale, String> titleMap,
+			Map<Locale, String> descriptionMap, String content,
+			String layoutUuid, ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateArticle(
+			userId, groupId, folderId, articleId, version, titleMap,
+			descriptionMap, content, layoutUuid, serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateArticle(
+			long userId, long groupId, long folderId, String articleId,
+			double version, Map<Locale, String> titleMap,
+			Map<Locale, String> descriptionMap, String content,
+			String ddmStructureKey, String ddmTemplateKey, String layoutUuid,
+			int displayDateMonth, int displayDateDay, int displayDateYear,
+			int displayDateHour, int displayDateMinute, int expirationDateMonth,
+			int expirationDateDay, int expirationDateYear,
+			int expirationDateHour, int expirationDateMinute,
+			boolean neverExpire, int reviewDateMonth, int reviewDateDay,
+			int reviewDateYear, int reviewDateHour, int reviewDateMinute,
+			boolean neverReview, boolean indexable, boolean smallImage,
+			String smallImageURL, File smallImageFile,
+			Map<String, byte[]> images, String articleURL,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateArticle(
+			userId, groupId, folderId, articleId, version, titleMap,
+			descriptionMap, content, ddmStructureKey, ddmTemplateKey,
+			layoutUuid, displayDateMonth, displayDateDay, displayDateYear,
+			displayDateHour, displayDateMinute, expirationDateMonth,
+			expirationDateDay, expirationDateYear, expirationDateHour,
+			expirationDateMinute, neverExpire, reviewDateMonth, reviewDateDay,
+			reviewDateYear, reviewDateHour, reviewDateMinute, neverReview,
+			indexable, smallImage, smallImageURL, smallImageFile, images,
+			articleURL, serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateArticle(
+			long userId, long groupId, long folderId, String articleId,
+			double version, String content, ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateArticle(
+			userId, groupId, folderId, articleId, version, content,
+			serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateArticle(long id, String urlTitle)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateArticle(id, urlTitle);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateArticleTranslation(
+			long groupId, String articleId, double version, Locale locale,
+			String title, String description, String content,
+			Map<String, byte[]> images, ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateArticleTranslation(
+			groupId, articleId, version, locale, title, description, content,
+			images, serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateContent(
+			long groupId, String articleId, double version, String content)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateContent(
+			groupId, articleId, version, content);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateStatus(
+			long userId, JournalArticle article, int status, String articleURL,
+			ServiceContext serviceContext,
+			Map<String, Serializable> workflowContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateStatus(
+			userId, article, status, articleURL, serviceContext,
+			workflowContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateStatus(
+			long userId, long classPK, int status,
+			Map<String, Serializable> workflowContext,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateStatus(
+			userId, classPK, status, workflowContext, serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Override
+	public JournalArticle updateStatus(
+			long userId, long groupId, String articleId, double version,
+			int status, String articleURL,
+			Map<String, Serializable> workflowContext,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		JournalArticle journalArticle = super.updateStatus(
+			userId, groupId, articleId, version, status, articleURL,
+			workflowContext, serviceContext);
+
+		_ctManager.registerModelChange(
+			PrincipalThreadLocal.getUserId(),
+			_portal.getClassNameId(JournalArticle.class.getName()),
+			journalArticle.getId(), journalArticle.getResourcePrimKey());
+
+		return journalArticle;
+	}
+
+	@Reference
+	private CTManager _ctManager;
+
+	@Reference
+	private JournalArticleLocalService _journalArticleLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
