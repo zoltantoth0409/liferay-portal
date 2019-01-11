43a791aa1dd5cea1d75a7415cc19571cd14a5bef
Pablo Molina <contact@pablomolina.me>
2019-01-11 13:57:23 +0100

    LPS-88718 Add spacing panel to FloatingToolbar

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/floating_toolbar/FloatingToolbar.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/floating_toolbar/FloatingToolbar.es.js
@@ -1,15 +1,92 @@
 import Component from 'metal-component';
+import {Config} from 'metal-state';
 import Soy from 'metal-soy';
 
+import './spacing/FloatingToolbarSpacingPanel.es';
+import getConnectedComponent from '../../store/ConnectedComponent.es';
 import templates from './FloatingToolbar.soy';
+
+/**
+ * List of available panels
+ * @review
+ * @type {object[]}
+ */
+const FLOATING_TOOLBAR_PANELS = [
+	{
+		icon: 'table',
+		panelId: 'spacing',
+		title: Liferay.Language.get('spacing')
+	}
+];
 
 /**
  * FloatingToolbar
  */
 class FloatingToolbar extends Component {
+
+	/**
+	 * Handle button click
+	 * @param {MouseEvent} event Click event
+	 */
+	_handleButtonClick(event) {
+		const {panelId = null} = event.delegateTarget.dataset;
+
+		this._selectedPanelId = panelId;
+	}
+
 }
 
-Soy.register(FloatingToolbar, templates);
+/**
+ * State definition.
+ * @review
+ * @static
+ * @type {!Object}
+ */
+FloatingToolbar.STATE = {
 
-export {FloatingToolbar};
-export default FloatingToolbar;
+	/**
+	 * List of available panels.
+	 * @default FLOATING_TOOLBAR_PANELS
+	 * @instance
+	 * @memberOf FloatingToolbar
+	 * @private
+	 * @review
+	 * @type {object[]}
+	 */
+	_panels: Config
+		.arrayOf(
+			Config.shapeOf(
+				{
+					icon: Config.string().required(),
+					panelId: Config.string().required(),
+					title: Config.string().required()
+				}
+			)
+		)
+		.internal()
+		.value(FLOATING_TOOLBAR_PANELS),
+
+	/**
+	 * Selected panel ID.
+	 * @default null
+	 * @instance
+	 * @memberOf FloatingToolbar
+	 * @private
+	 * @review
+	 * @type {string|null}
+	 */
+	_selectedPanelId: Config
+		.oneOf(FLOATING_TOOLBAR_PANELS.map(panel => panel.panelId))
+		.internal()
+		.value(null)
+};
+
+const ConnectedFloatingToolbar = getConnectedComponent(
+	FloatingToolbar,
+	['spritemap']
+);
+
+Soy.register(ConnectedFloatingToolbar, templates);
+
+export {ConnectedFloatingToolbar, FloatingToolbar};
+export default ConnectedFloatingToolbar;
---
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/floating_toolbar/spacing/FloatingToolbarSpacingPanel.es.js
@@ -0,0 +1,15 @@
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+
+import templates from './FloatingToolbarSpacingPanel.soy';
+
+/**
+ * FloatingToolbarSpacingPanel
+ */
+class FloatingToolbarSpacingPanel extends Component {
+}
+
+Soy.register(FloatingToolbarSpacingPanel, templates);
+
+export {FloatingToolbarSpacingPanel};
+export default FloatingToolbarSpacingPanel;
