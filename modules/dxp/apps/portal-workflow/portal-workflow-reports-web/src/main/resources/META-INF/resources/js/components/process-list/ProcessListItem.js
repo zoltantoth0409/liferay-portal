7b903eea64d6c3241b194cb377e7a9b24acc5529
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-01-11 10:45:48 -0300

    LPS-86638|LPS-88670 List Item render

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-reports-web/src/main/resources/META-INF/resources/js/components/process-list/ProcessListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class ProcessListItem extends React.Component {
+	render() {
+		const {onTime, instancesCount, overdue, processName} = this.props;
+
+		return (
+			<tr>
+				<td className="text-semi-bold">{processName}</td>
+				<td>
+					{instancesCount > 1
+						? `${instancesCount} items`
+						: `${instancesCount} item`}
+				</td>
+				<td>{onTime}</td>
+				<td>{overdue}</td>
+			</tr>
+		);
+	}
+}
