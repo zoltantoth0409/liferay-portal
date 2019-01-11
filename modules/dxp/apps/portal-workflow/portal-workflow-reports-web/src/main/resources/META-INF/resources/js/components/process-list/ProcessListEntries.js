287612a4466e2af74d170451b32993a5d115083d
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-01-11 10:44:38 -0300

    LPS-86638|LPS-88670 List of entries

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-reports-web/src/main/resources/META-INF/resources/js/components/process-list/ProcessListEntries.js
@@ -0,0 +1,50 @@
+import autobind from 'autobind-decorator';
+import Icon from '../../libs/Icon';
+import ProcessListEntryItem from './ProcessListEntryItem';
+import React from 'react';
+
+export default class ProcessListEntries extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedEntry: props.selectedEntry
+		};
+	}
+
+	@autobind
+	setEntry(entry) {
+		const {onSelectEntry} = this.props;
+		this.setState({selectedEntry: entry});
+		onSelectEntry(entry);
+	}
+
+	render() {
+		const {entries} = this.props;
+		const {selectedEntry} = this.state;
+
+		return (
+			<div className="dropdown pagination-items-per-page">
+				<a
+					aria-expanded="false"
+					aria-haspopup="true"
+					className="dropdown-toggle"
+					data-toggle="dropdown"
+					href="#1"
+					role="button"
+				>
+					{`${selectedEntry} entries`}
+					<Icon iconName="caret-double-l" />
+				</a>
+				<div className="dropdown-menu dropdown-menu-top">
+					{entries.map((entry, index) => (
+						<ProcessListEntryItem
+							entry={entry}
+							key={`${index}_${entry}`}
+							onChangeEntry={this.setEntry}
+						/>
+					))}
+				</div>
+			</div>
+		);
+	}
+}
