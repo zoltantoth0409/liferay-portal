25e4b979a7e33cbe768a42dc68e407db9227404a
shuyangzhou <shuyang.zhou@liferay.com>
2019-01-11 10:49:35 -0800

    LPS-89139 Revert LPS-77110 1fe38a052f2e9e5f4365e158d6979a59d01f0b70, this broke SB generated code's FQDN simplification.

--- portal-kernel/src/com/liferay/portal/kernel/util/ClassUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ClassUtil.java
@@ -94,12 +94,6 @@ public class ClassUtil {
 
 		while (st.nextToken() != StreamTokenizer.TT_EOF) {
 			if (st.ttype == StreamTokenizer.TT_WORD) {
-				Matcher matcher = _fullyQualifiedNamePattern.matcher(st.sval);
-
-				if (matcher.find()) {
-					continue;
-				}
-
 				int firstIndex = st.sval.indexOf('.');
 
 				if (firstIndex >= 0) {
@@ -400,7 +394,5 @@ public class ClassUtil {
 		"@(\\w+)\\.?(\\w*)$");
 	private static final Pattern _annotationParametersPattern = Pattern.compile(
 		"@(\\w+)\\.?(\\w*)\\({0,1}\\{{0,1}([^)}]+)\\}{0,1}\\){0,1}");
-	private static final Pattern _fullyQualifiedNamePattern = Pattern.compile(
-		"^([a-z]\\w*\\.){2,}([A-Z]\\w*)(\\.|\\Z)");
 
 }
