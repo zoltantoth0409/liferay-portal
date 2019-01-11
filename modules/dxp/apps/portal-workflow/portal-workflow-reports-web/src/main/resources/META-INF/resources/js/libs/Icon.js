2d446b7a970e70b04623ba0e10ba57e82f37b710
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-01-11 10:57:35 -0300

    LPS-86638|LPS-88670 Generic icon component

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-reports-web/src/main/resources/META-INF/resources/js/libs/Icon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Icon extends React.Component {
+	render() {
+		const {iconName} = this.props;
+
+		return (
+			<svg
+				className={`lexicon-icon lexicon-icon-${iconName}`}
+				focusable="false"
+				role="presentation"
+			>
+				<use
+					href={`${Liferay.ThemeDisplay.getPathThemeImages()}/lexicon/icons.svg#${iconName}`}
+				/>
+			</svg>
+		);
+	}
+}
