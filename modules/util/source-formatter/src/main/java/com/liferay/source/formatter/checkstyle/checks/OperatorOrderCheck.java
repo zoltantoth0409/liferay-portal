d8c3c13672df9f4a20f3ff0d7e0e7a97e53b31e3
Hugo Huijser <hugo.huijser@liferay.com>
2019-01-14 14:06:00 +0800

    LPS-84119 Simplify

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/OperatorOrderCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/OperatorOrderCheck.java
@@ -35,54 +35,43 @@ public class OperatorOrderCheck extends BaseCheck {
 	@Override
 	protected void doVisitToken(DetailAST detailAST) {
 		DetailAST firstChildDetailAST = detailAST.getFirstChild();
-		String message = "";
 
 		if (!ArrayUtil.contains(
-				_UNARY_MINUS_OR_UNARY_PLUS, firstChildDetailAST.getType())) {
-
-			if (!ArrayUtil.contains(
-					_LITERAL_OR_NUM_TYPES, firstChildDetailAST.getType())) {
-
-				return;
-			}
-		}
-		else {
-			DetailAST nameDetailAST = firstChildDetailAST.getFirstChild();
-
-			message = firstChildDetailAST.getText() + nameDetailAST.getText();
-		}
-
-		DetailAST secondChildDetailAST = firstChildDetailAST.getNextSibling();
-
-		if (ArrayUtil.contains(
-				_UNARY_MINUS_OR_UNARY_PLUS, secondChildDetailAST.getType())) {
+				_LITERAL_OR_NUM_TYPES, firstChildDetailAST.getType())) {
 
 			return;
 		}
 
+		DetailAST secondChildDetailAST = firstChildDetailAST.getNextSibling();
+
 		if (!ArrayUtil.contains(
 				_LITERAL_OR_NUM_TYPES, secondChildDetailAST.getType())) {
 
-			if (message.isEmpty()) {
-				message = firstChildDetailAST.getText();
-			}
-
 			log(
 				firstChildDetailAST, _MSG_LITERAL_OR_NUM_LEFT_ARGUMENT,
-				message);
+				_getStringValue(firstChildDetailAST));
 		}
+	}
+
+	private String _getStringValue(DetailAST detailAST) {
+		if ((detailAST.getType() == TokenTypes.UNARY_MINUS) ||
+			(detailAST.getType() == TokenTypes.UNARY_PLUS)) {
+
+			DetailAST firstChildDetailAST = detailAST.getFirstChild();
+
+			return detailAST.getText() + firstChildDetailAST.getText();
+		}
+
+		return detailAST.getText();
 	}
 
 	private static final int[] _LITERAL_OR_NUM_TYPES = {
 		TokenTypes.NUM_DOUBLE, TokenTypes.NUM_FLOAT, TokenTypes.NUM_INT,
-		TokenTypes.NUM_LONG, TokenTypes.STRING_LITERAL
+		TokenTypes.NUM_LONG, TokenTypes.STRING_LITERAL, TokenTypes.UNARY_MINUS,
+		TokenTypes.UNARY_PLUS
 	};
 
 	private static final String _MSG_LITERAL_OR_NUM_LEFT_ARGUMENT =
 		"left.argument.literal.or.num";
-
-	private static final int[] _UNARY_MINUS_OR_UNARY_PLUS = {
-		TokenTypes.UNARY_MINUS, TokenTypes.UNARY_PLUS
-	};
 
 }
