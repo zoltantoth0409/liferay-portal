a9e20280b682dcbbcffedc07699657a502749ee4
Samuel Tran <samuel.tran@liferay.com>
2019-01-14 08:42:20 -0800

    LPS-88922 portal-kernel - TextFormatter.java - Add bytes to formatStorageSize()

--- portal-kernel/src/com/liferay/portal/kernel/util/TextFormatter.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/TextFormatter.java
@@ -198,9 +198,13 @@ public class TextFormatter {
 	}
 
 	public static String formatStorageSize(double size, Locale locale) {
-		String suffix = _STORAGE_SIZE_SUFFIX_KB;
+		String suffix = _STORAGE_SIZE_SUFFIX_B;
 
-		size = size / _STORAGE_SIZE_DENOMINATOR;
+		if (size >= _STORAGE_SIZE_DENOMINATOR) {
+			suffix = _STORAGE_SIZE_SUFFIX_KB;
+
+			size = size / _STORAGE_SIZE_DENOMINATOR;
+		}
 
 		if (size >= _STORAGE_SIZE_DENOMINATOR) {
 			suffix = _STORAGE_SIZE_SUFFIX_MB;
@@ -216,7 +220,9 @@ public class TextFormatter {
 
 		NumberFormat numberFormat = NumberFormat.getInstance(locale);
 
-		if (suffix.equals(_STORAGE_SIZE_SUFFIX_KB)) {
+		if (suffix.equals(_STORAGE_SIZE_SUFFIX_B) ||
+			suffix.equals(_STORAGE_SIZE_SUFFIX_KB)) {
+
 			numberFormat.setMaximumFractionDigits(0);
 		}
 		else {
@@ -419,6 +425,8 @@ public class TextFormatter {
 	}
 
 	private static final double _STORAGE_SIZE_DENOMINATOR = 1024.0;
+
+	private static final String _STORAGE_SIZE_SUFFIX_B = "B";
 
 	private static final String _STORAGE_SIZE_SUFFIX_GB = "GB";
 
