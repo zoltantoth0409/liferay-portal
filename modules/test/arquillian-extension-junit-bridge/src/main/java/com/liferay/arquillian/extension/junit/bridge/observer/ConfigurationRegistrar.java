be950fe29a2fc2e4d121569258504848484e1a60
Matthew Tambara <matthew.tambara@liferay.com>
2019-01-15 10:39:46 -0800

    LPS-89221 This just needs to not be null to avoid NPE in org.jboss.arquillian.container.impl.client.container.ContainerRegistryCreator

--- modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/observer/ConfigurationRegistrar.java
+++ modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/observer/ConfigurationRegistrar.java
@@ -15,18 +15,14 @@
 package com.liferay.arquillian.extension.junit.bridge.observer;
 
 import java.io.IOException;
-import java.io.InputStream;
-
-import java.util.Properties;
 
 import org.jboss.arquillian.config.descriptor.api.ArquillianDescriptor;
-import org.jboss.arquillian.config.impl.extension.PropertiesParser;
+import org.jboss.arquillian.config.descriptor.impl.ArquillianDescriptorImpl;
 import org.jboss.arquillian.core.api.InstanceProducer;
 import org.jboss.arquillian.core.api.annotation.ApplicationScoped;
 import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.core.api.annotation.Observes;
 import org.jboss.arquillian.core.api.event.ManagerStarted;
-import org.jboss.shrinkwrap.descriptor.api.Descriptors;
 
 /**
  * @author Matthew Tambara
@@ -36,30 +32,7 @@ public class ConfigurationRegistrar {
 	public void loadConfiguration(@Observes ManagerStarted event)
 		throws IOException {
 
-		ArquillianDescriptor arquillianDescriptor = Descriptors.create(
-			ArquillianDescriptor.class);
-
-		final PropertiesParser propertiesParser = new PropertiesParser();
-
-		ClassLoader classLoader = JUnitBridgeObserver.class.getClassLoader();
-
-		try (InputStream inputStream = classLoader.getResourceAsStream(
-				"arquillian.properties")) {
-
-			Properties properties = new Properties();
-
-			properties.load(inputStream);
-
-			propertiesParser.addProperties(arquillianDescriptor, properties);
-		}
-
-		Properties envProperties = new Properties();
-
-		envProperties.putAll(System.getenv());
-
-		propertiesParser.addProperties(arquillianDescriptor, envProperties);
-
-		_arquillianDescriptor.set(arquillianDescriptor);
+		_arquillianDescriptor.set(new ArquillianDescriptorImpl(null));
 	}
 
 	@ApplicationScoped
