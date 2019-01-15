5fe024b7c0478fa26df26367e6be24f6c9c2583b
rubenpulido <ruben.pulido@liferay.com>
2019-01-15 16:15:54 +0100

    LPS-88715 Extract MediaObjectImpl to its own class

--- modules/apps/headless-apio/media-object/media-object-apio-test/src/testIntegration/java/com/liferay/media/object/apio/internal/architect/resource/test/DefaultMediaObjectNestedCollectionResourceTest.java
+++ modules/apps/headless-apio/media-object/media-object-apio-test/src/testIntegration/java/com/liferay/media/object/apio/internal/architect/resource/test/DefaultMediaObjectNestedCollectionResourceTest.java
@@ -20,8 +20,8 @@ import com.liferay.adaptive.media.image.processor.AMImageProcessor;
 import com.liferay.apio.architect.file.BinaryFile;
 import com.liferay.apio.architect.pagination.PageItems;
 import com.liferay.apio.architect.resource.NestedCollectionResource;
-import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.media.object.apio.architect.model.MediaObject;
+import com.liferay.media.object.apio.internal.architect.resource.test.model.MediaObjectImpl;
 import com.liferay.portal.apio.test.util.PaginationRequest;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.repository.model.FileEntry;
@@ -54,6 +54,7 @@ import org.apache.commons.io.IOUtils;
 
 import org.awaitility.Awaitility;
 
+import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -327,67 +328,5 @@ public class DefaultMediaObjectNestedCollectionResourceTest
 
 	@DeleteAfterTestRun
 	private Group _group;
-
-	private static class MediaObjectImpl implements MediaObject {
-
-		@Override
-		public BinaryFile getBinaryFile() {
-			return _binaryFile;
-		}
-
-		@Override
-		public List<Long> getCategories() {
-			return _categories;
-		}
-
-		@Override
-		public String getDescription() {
-			return _description;
-		}
-
-		@Override
-		public List<String> getKeywords() {
-			return _keywords;
-		}
-
-		@Override
-		public String getTitle() {
-			return _title;
-		}
-
-		private MediaObjectImpl(
-			BinaryFile binaryFile, String title, String description,
-			List<String> keywords, List<Long> categories) {
-
-			_binaryFile = binaryFile;
-			_title = title;
-			_description = description;
-			_keywords = Optional.ofNullable(
-				keywords
-			).map(
-				List::stream
-			).orElseGet(
-				Stream::empty
-			).collect(
-				Collectors.toList()
-			);
-			_categories = Optional.ofNullable(
-				categories
-			).map(
-				List::stream
-			).orElseGet(
-				Stream::empty
-			).collect(
-				Collectors.toList()
-			);
-		}
-
-		private final BinaryFile _binaryFile;
-		private final List<Long> _categories;
-		private final String _description;
-		private final List<String> _keywords;
-		private final String _title;
-
-	}
 
 }
