53c07d31ea726245972e548aab62cf7f448e6ea7
Pablo Molina <contact@pablomolina.me>
2019-01-15 14:05:12 +0100

    LPS-88718 Include spacing options

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/floating_toolbar/spacing/FloatingToolbarSpacingPanel.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/floating_toolbar/spacing/FloatingToolbarSpacingPanel.es.js
@@ -1,13 +1,96 @@
 import Component from 'metal-component';
-import Soy from 'metal-soy';
+import Soy, {Config} from 'metal-soy';
 
+import './FloatingToolbarSpacingPanelDelegateTemplate.soy';
+import {CONTAINER_TYPES, ITEM_CONFIG_KEYS} from '../../../utils/constants';
+import {UPDATE_LAST_SAVE_DATE, UPDATE_SAVING_CHANGES_STATUS, UPDATE_SECTION_CONFIG, UPDATE_TRANSLATION_STATUS} from '../../../actions/actions.es';
 import templates from './FloatingToolbarSpacingPanel.soy';
 
 /**
  * FloatingToolbarSpacingPanel
  */
 class FloatingToolbarSpacingPanel extends Component {
+
+	/**
+	 * Handle container option change
+	 * @param {Event} event
+	 */
+	_handleContainerOptionChange(event) {
+		this.store
+			.dispatchAction(
+				UPDATE_SAVING_CHANGES_STATUS,
+				{
+					savingChanges: true
+				}
+			)
+			.dispatchAction(
+				UPDATE_SECTION_CONFIG,
+				{
+					config: {
+						[ITEM_CONFIG_KEYS.containerType]: event.delegateTarget.value
+					},
+					sectionId: this.itemId
+				}
+			)
+			.dispatchAction(
+				UPDATE_TRANSLATION_STATUS
+			)
+			.dispatchAction(
+				UPDATE_LAST_SAVE_DATE,
+				{
+					lastSaveDate: new Date()
+				}
+			)
+			.dispatchAction(
+				UPDATE_SAVING_CHANGES_STATUS,
+				{
+					savingChanges: false
+				}
+			);
+	}
+
 }
+
+/**
+ * State definition.
+ * @review
+ * @static
+ * @type {!Object}
+ */
+FloatingToolbarSpacingPanel.STATE = {
+
+	/**
+	 * @default undefined
+	 * @memberof FloatingToolbarSpacingPanel
+	 * @review
+	 * @type {object}
+	 */
+	store: Config
+		.object()
+		.value(null),
+
+	/**
+	 * @default undefined
+	 * @memberof FloatingToolbarSpacingPanel
+	 * @review
+	 * @type {!string}
+	 */
+	itemId: Config
+		.string()
+		.required(),
+
+	/**
+	 * @default CONTAINER_TYPES
+	 * @memberOf FloatingToolbarSpacingPanel
+	 * @private
+	 * @review
+	 * @type {object[]}
+	 */
+	_containerTypes: Config
+		.array()
+		.internal()
+		.value(Object.values(CONTAINER_TYPES))
+};
 
 Soy.register(FloatingToolbarSpacingPanel, templates);
 
