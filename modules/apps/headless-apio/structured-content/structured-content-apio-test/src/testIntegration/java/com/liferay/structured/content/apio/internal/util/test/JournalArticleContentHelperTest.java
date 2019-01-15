5e10fbabcd629fbb4bef4729003c04103726f041
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-01-15 21:59:18 +0100

    LPS-89152 Add test.

---
+++ modules/apps/headless-apio/structured-content/structured-content-apio-test/src/testIntegration/java/com/liferay/structured/content/apio/internal/util/test/JournalArticleContentHelperTest.java
@@ -0,0 +1,534 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.structured.content.apio.internal.util.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeResponse;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerTracker;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.DDMStructureConstants;
+import com.liferay.dynamic.data.mapping.storage.Field;
+import com.liferay.dynamic.data.mapping.storage.Fields;
+import com.liferay.dynamic.data.mapping.storage.StorageType;
+import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestHelper;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.model.JournalFolderConstants;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.journal.util.JournalConverter;
+import com.liferay.portal.apio.test.util.FileTestUtil;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+import com.liferay.registry.Filter;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceTracker;
+import com.liferay.structured.content.apio.architect.model.StructuredContentLocation;
+import com.liferay.structured.content.apio.architect.model.StructuredContentValue;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Cristina González
+ */
+@RunWith(Arquillian.class)
+public class JournalArticleContentHelperTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		Registry registry = RegistryUtil.getRegistry();
+
+		Filter filter = registry.getFilter(
+			"(objectClass=com.liferay.structured.content.apio.internal.util." +
+				"JournalArticleContentHelper)");
+
+		_serviceTracker = registry.trackServices(filter);
+
+		_serviceTracker.open();
+
+		_group = GroupTestUtil.addGroup();
+
+		_ddmStructureTestHelper = new DDMStructureTestHelper(
+			PortalUtil.getClassNameId(JournalArticle.class), _group);
+	}
+
+	@After
+	public void tearDown() {
+		_serviceTracker.close();
+	}
+
+	@Test
+	public void testCreateJournalArticleContentWithMediaObjectStructuredContentValue()
+		throws Throwable {
+
+		DDMStructure ddmStructure = _ddmStructureTestHelper.addStructure(
+			PortalUtil.getClassNameId(JournalArticle.class),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			deserialize(
+				_ddmFormDeserializerTracker,
+				FileTestUtil.readFile(
+					"test-journal-media-object-field-structure.json",
+					JournalArticleContentHelperTest.class)),
+			StorageType.JSON.getValue(), DDMStructureConstants.TYPE_DEFAULT);
+
+		FileEntry fileEntry = _dlAppLocalService.addFileEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString() + ".jpg", ContentTypes.IMAGE_JPEG,
+			FileUtil.getBytes(
+				JournalArticleContentHelperTest.class, "image.jpg"),
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId()));
+
+		StructuredContentValue structuredContentValue =
+			getDocumentStructuredContentValue(
+				"MyDocumentsAndMedia", fileEntry.getFileEntryId(),
+				RandomTestUtil.randomString());
+
+		String content = createJournalArticleContent(
+			LocaleUtil.US,
+			Collections.singletonMap(
+				LocaleUtil.US,
+				Collections.singletonList(structuredContentValue)),
+			ddmStructure);
+
+		Fields fields = _journalConverter.getDDMFields(ddmStructure, content);
+
+		Assert.assertEquals(
+			ddmStructure.getStructureId(), fields.getDDMStructureId());
+
+		Assert.assertEquals(LocaleUtil.US, fields.getDefaultLocale());
+		Assert.assertEquals(
+			fields.getAvailableLocales(), Collections.singleton(LocaleUtil.US));
+
+		Field field = fields.get(structuredContentValue.getName());
+
+		Assert.assertNotNull(field);
+		Assert.assertEquals(structuredContentValue.getName(), field.getName());
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			(String)field.getValue());
+
+		Assert.assertEquals(
+			fileEntry.getFileEntryId(), jsonObject.getLong("fileEntryId"));
+		Assert.assertEquals(
+			fileEntry.getGroupId(), jsonObject.getLong("groupId"));
+		Assert.assertEquals(fileEntry.getFileName(), jsonObject.get("name"));
+		Assert.assertEquals(
+			fileEntry.getPrimaryKey(), jsonObject.getLong("resourcePrimKey"));
+		Assert.assertEquals(fileEntry.getFileName(), jsonObject.get("title"));
+		Assert.assertEquals("document", jsonObject.get("type"));
+		Assert.assertEquals(fileEntry.getUuid(), jsonObject.get("uuid"));
+		Assert.assertEquals(
+			structuredContentValue.getValue(), jsonObject.get("alt"));
+	}
+
+	@Test
+	public void testCreateJournalArticleContentWithStructuredContentLocationStructuredContentValue()
+		throws Throwable {
+
+		DDMStructure ddmStructure = _ddmStructureTestHelper.addStructure(
+			PortalUtil.getClassNameId(JournalArticle.class),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			deserialize(
+				_ddmFormDeserializerTracker,
+				FileTestUtil.readFile(
+					"test-journal-geolocalization-field-structure.json",
+					JournalArticleContentHelperTest.class)),
+			StorageType.JSON.getValue(), DDMStructureConstants.TYPE_DEFAULT);
+
+		StructuredContentValue structuredContentValue =
+			getStructuredContentLocationStructuredContentValue(
+				"MyGeolocation", 12.0, -13.7);
+
+		String content = createJournalArticleContent(
+			LocaleUtil.US,
+			Collections.singletonMap(
+				LocaleUtil.US,
+				Collections.singletonList(structuredContentValue)),
+			ddmStructure);
+
+		Fields fields = _journalConverter.getDDMFields(ddmStructure, content);
+
+		Assert.assertEquals(
+			ddmStructure.getStructureId(), fields.getDDMStructureId());
+
+		Assert.assertEquals(LocaleUtil.US, fields.getDefaultLocale());
+		Assert.assertEquals(
+			fields.getAvailableLocales(), Collections.singleton(LocaleUtil.US));
+
+		Field field = fields.get(structuredContentValue.getName());
+
+		Assert.assertNotNull(field);
+		Assert.assertEquals(structuredContentValue.getName(), field.getName());
+
+		StructuredContentLocation structuredContentLocation =
+			structuredContentValue.getStructuredContentLocation();
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			(String)field.getValue());
+
+		Assert.assertEquals(
+			structuredContentLocation.getLatitude(),
+			jsonObject.getDouble("latitude"), 0);
+
+		Assert.assertEquals(
+			structuredContentLocation.getLongitude(),
+			jsonObject.getDouble("longitude"), 0);
+	}
+
+	@Test
+	public void testCreateJournalArticleContentWithTextStructuredContentValue()
+		throws Throwable {
+
+		DDMStructure ddmStructure = _ddmStructureTestHelper.addStructure(
+			PortalUtil.getClassNameId(JournalArticle.class),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			deserialize(
+				_ddmFormDeserializerTracker,
+				FileTestUtil.readFile(
+					"test-journal-text-field-structure.json",
+					JournalArticleContentHelperTest.class)),
+			StorageType.JSON.getValue(), DDMStructureConstants.TYPE_DEFAULT);
+
+		StructuredContentValue structuredContentValue =
+			getTextStructuredContentValue("MyText", "My text");
+
+		String content = createJournalArticleContent(
+			LocaleUtil.US,
+			Collections.singletonMap(
+				LocaleUtil.US,
+				Collections.singletonList(structuredContentValue)),
+			ddmStructure);
+
+		Fields fields = _journalConverter.getDDMFields(ddmStructure, content);
+
+		Assert.assertEquals(
+			ddmStructure.getStructureId(), fields.getDDMStructureId());
+
+		Assert.assertEquals(fields.getDefaultLocale(), LocaleUtil.US);
+
+		Assert.assertEquals(
+			fields.getAvailableLocales(), Collections.singleton(LocaleUtil.US));
+
+		Field field = fields.get(structuredContentValue.getName());
+
+		Assert.assertNotNull(field);
+		Assert.assertEquals(structuredContentValue.getName(), field.getName());
+		Assert.assertEquals(
+			structuredContentValue.getValue(), field.getValue());
+		Assert.assertEquals(
+			structuredContentValue.getValue(), field.getValue(LocaleUtil.US));
+	}
+
+	@Test
+	public void testCreateJournalArticleContentWitStructuredContentStructuredContentValue()
+		throws Throwable {
+
+		DDMStructure ddmStructure = _ddmStructureTestHelper.addStructure(
+			PortalUtil.getClassNameId(JournalArticle.class),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			deserialize(
+				_ddmFormDeserializerTracker,
+				FileTestUtil.readFile(
+					"test-journal-structured-content-field-structure.json",
+					JournalArticleContentHelperTest.class)),
+			StorageType.JSON.getValue(), DDMStructureConstants.TYPE_DEFAULT);
+
+		JournalArticle journalArticle = JournalTestUtil.addArticle(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		StructuredContentValue structuredContentValue =
+			getStructureStructuredContentValue(
+				"MyJournalArticle", journalArticle.getPrimaryKey());
+
+		String content = createJournalArticleContent(
+			LocaleUtil.US,
+			Collections.singletonMap(
+				LocaleUtil.US,
+				Collections.singletonList(structuredContentValue)),
+			ddmStructure);
+
+		Fields fields = _journalConverter.getDDMFields(ddmStructure, content);
+
+		Assert.assertEquals(
+			ddmStructure.getStructureId(), fields.getDDMStructureId());
+
+		Assert.assertEquals(LocaleUtil.US, fields.getDefaultLocale());
+		Assert.assertEquals(
+			Collections.singleton(LocaleUtil.US), fields.getAvailableLocales());
+
+		Field field = fields.get(structuredContentValue.getName());
+
+		Assert.assertNotNull(field);
+		Assert.assertEquals(structuredContentValue.getName(), field.getName());
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			(String)field.getValue());
+
+		Assert.assertEquals(
+			JournalArticle.class.getName(), jsonObject.getString("className"));
+		Assert.assertEquals(
+			journalArticle.getResourcePrimKey(), jsonObject.getLong("classPK"));
+		Assert.assertEquals(
+			journalArticle.getTitle(), jsonObject.getString("title"));
+	}
+
+	protected String createJournalArticleContent(
+			Locale defaultLocale,
+			Map<Locale, List<? extends StructuredContentValue>>
+				structuredContentValuesMap,
+			DDMStructure ddmStructure)
+		throws Throwable {
+
+		Object service = _serviceTracker.getService();
+
+		Class<?> clazz = service.getClass();
+
+		Method method = clazz.getDeclaredMethod(
+			"createJournalArticleContent", Locale.class, Map.class,
+			DDMStructure.class);
+
+		method.setAccessible(true);
+
+		try {
+			return (String)method.invoke(
+				service, defaultLocale, structuredContentValuesMap,
+				ddmStructure);
+		}
+		catch (InvocationTargetException ite) {
+			throw ite.getTargetException();
+		}
+	}
+
+	protected DDMForm deserialize(
+		DDMFormDeserializerTracker ddmFormDeserializerTracker, String content) {
+
+		DDMFormDeserializer ddmFormDeserializer =
+			ddmFormDeserializerTracker.getDDMFormDeserializer("json");
+
+		DDMFormDeserializerDeserializeRequest.Builder builder =
+			DDMFormDeserializerDeserializeRequest.Builder.newBuilder(content);
+
+		DDMFormDeserializerDeserializeResponse
+			ddmFormDeserializerDeserializeResponse =
+				ddmFormDeserializer.deserialize(builder.build());
+
+		return ddmFormDeserializerDeserializeResponse.getDDMForm();
+	}
+
+	protected StructuredContentValue getDocumentStructuredContentValue(
+		String name, Long document, String value) {
+
+		return new StructuredContentValue() {
+
+			@Override
+			public Long getDocument() {
+				return document;
+			}
+
+			@Override
+			public String getName() {
+				return name;
+			}
+
+			@Override
+			public Long getStructuredContentId() {
+				return null;
+			}
+
+			@Override
+			public StructuredContentLocation getStructuredContentLocation() {
+				return null;
+			}
+
+			@Override
+			public String getValue() {
+				return value;
+			}
+
+		};
+	}
+
+	protected StructuredContentValue
+		getStructuredContentLocationStructuredContentValue(
+			String name, Double latitude, Double longitude) {
+
+		return new StructuredContentValue() {
+
+			@Override
+			public Long getDocument() {
+				return null;
+			}
+
+			@Override
+			public String getName() {
+				return name;
+			}
+
+			@Override
+			public Long getStructuredContentId() {
+				return null;
+			}
+
+			@Override
+			public StructuredContentLocation getStructuredContentLocation() {
+				return new StructuredContentLocation() {
+
+					@Override
+					public Double getLatitude() {
+						return latitude;
+					}
+
+					@Override
+					public Double getLongitude() {
+						return longitude;
+					}
+
+				};
+			}
+
+			@Override
+			public String getValue() {
+				return null;
+			}
+
+		};
+	}
+
+	protected StructuredContentValue getStructureStructuredContentValue(
+		String name, Long structuredContentId) {
+
+		return new StructuredContentValue() {
+
+			@Override
+			public Long getDocument() {
+				return null;
+			}
+
+			@Override
+			public String getName() {
+				return name;
+			}
+
+			@Override
+			public Long getStructuredContentId() {
+				return structuredContentId;
+			}
+
+			@Override
+			public StructuredContentLocation getStructuredContentLocation() {
+				return null;
+			}
+
+			@Override
+			public String getValue() {
+				return null;
+			}
+
+		};
+	}
+
+	protected StructuredContentValue getTextStructuredContentValue(
+		String name, String value) {
+
+		return new StructuredContentValue() {
+
+			@Override
+			public Long getDocument() {
+				return null;
+			}
+
+			@Override
+			public String getName() {
+				return name;
+			}
+
+			@Override
+			public Long getStructuredContentId() {
+				return null;
+			}
+
+			@Override
+			public StructuredContentLocation getStructuredContentLocation() {
+				return null;
+			}
+
+			@Override
+			public String getValue() {
+				return value;
+			}
+
+		};
+	}
+
+	private static ServiceTracker<Object, Object> _serviceTracker;
+
+	@Inject
+	private DDMFormDeserializerTracker _ddmFormDeserializerTracker;
+
+	private DDMStructureTestHelper _ddmStructureTestHelper;
+
+	@Inject
+	private DLAppLocalService _dlAppLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private JournalConverter _journalConverter;
+
+}
