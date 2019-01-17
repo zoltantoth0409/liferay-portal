4bd1497cbb17bdfca7a5469a6a0c8a6f4d81b0b7
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-01-17 14:38:39 +0100

    LPS-89176 Add column in upgrade process

--- sql/update-7.1.0-7.2.0.sql
+++ sql/update-7.1.0-7.2.0.sql
@@ -1,3 +1,8 @@
 alter table Layout add parentPlid LONG;
 alter table Layout add leftPlid LONG;
 alter table Layout add rightPlid LONG;
+alter table Layout add system BOOLEAN;
+
+COMMIT_TRANSACTION;
+
+update Layout set system = FALSE;
