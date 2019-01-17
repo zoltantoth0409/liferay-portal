014b31cd25b935b8eb9f5aa6fbcfa851cecc21af
Sandro Chinea <sandrochinea@gmail.com>
2019-01-17 17:33:49 +0100

    LPS-88626 Added remove element button

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/structure/SidebarStructurePanel.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/components/sidebar/structure/SidebarStructurePanel.es.js
@@ -60,38 +60,12 @@ class SidebarStructurePanel extends Component {
 	}
 
 	/**
-	 * Callback executed when the fragment remove button is clicked.
-	 * It emits a 'fragmentRemoveButtonClick' event with the fragment index.
-	 * @param {{itemId: !string}} event
+	 * Callback executed when the element remove button is clicked.
+	 * @param {object} event
 	 * @private
 	 * @review
 	 */
-	_handleFragmentRemoveButtonClick(event) {
-		this.store
-			.dispatchAction(
-				UPDATE_SAVING_CHANGES_STATUS,
-				{
-					savingChanges: true
-				}
-			)
-			.dispatchAction(
-				REMOVE_FRAGMENT_ENTRY_LINK,
-				{
-					fragmentEntryLinkId: event.itemId
-				}
-			)
-			.dispatchAction(
-				UPDATE_LAST_SAVE_DATE,
-				{
-					lastSaveDate: new Date()
-				}
-			)
-			.dispatchAction(
-				UPDATE_SAVING_CHANGES_STATUS,
-				{
-					savingChanges: false
-				}
-			);
+	_handleElementRemoveButtonClick(event) {
 	}
 
 }
