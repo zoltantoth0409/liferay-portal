78f2588fb03b07a22a634b40ddd3a674eb559e77
Brian Chan <brian.chan@liferay.com>
2019-01-17 07:13:19 -0800

    LPS-89235

---
+++ modules/apps/headless/headless-content-space/headless-content-space-api/src/main/java/com/liferay/headless/content/space/dto/ContentSpace.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.content.space.dto;
+
+import com.liferay.apio.architect.annotation.Id;
+import com.liferay.apio.architect.annotation.Vocabulary;
+import com.liferay.apio.architect.identifier.Identifier;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author Javier Gamarra
+ * @author Cristina González
+ * @generated
+ */
+@Vocabulary.Type("ContentSpace")
+public interface ContentSpace extends Identifier<Long> {
+
+	@Vocabulary.Field("availableLanguages")
+	public List<String> getAvailableLanguages();
+
+	@Vocabulary.Field("description")
+	public String getDescription();
+
+	@Id
+	public long getId();
+
+	@Vocabulary.Field("name")
+	public String getName(Locale locale);
+
+}
---
+++ modules/apps/headless/headless-person/headless-person-api/src/main/java/com/liferay/headless/person/dto/Person.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.person.dto;
+
+import com.liferay.apio.architect.annotation.Id;
+import com.liferay.apio.architect.annotation.Vocabulary;
+import com.liferay.apio.architect.identifier.Identifier;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+@Vocabulary.Type("Person")
+public interface Person extends Identifier<Long> {
+
+	@Vocabulary.Field("familyName")
+	public String getFamilyName();
+
+	@Id
+	public long getId();
+
+}
