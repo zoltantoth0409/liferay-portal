426737a60b6a77353380113cd5cbdaf8eabdf5ee
Máté Thurzó <mate.thurzo@liferay.com>
2019-01-17 10:28:28 +0100

    LPS-89172 Rename package to util (Partial backport)

---
+++ modules/apps/portal-template/portal-template-soy-api/src/main/java/com/liferay/portal/template/soy/util/SoyContext.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.template.soy.util;
+
+import java.util.Map;
+
+/**
+ * @author Bruno Basto
+ */
+public interface SoyContext extends Map<String, Object> {
+
+	public void clearInjectedData();
+
+	/**
+	 * Put an HTML parameter in the SoyContext container. This is the same as
+	 * calling {@link SoyContext#put(Object, Object)} with a
+	 * {@link com.liferay.portal.template.soy.data.SoyHTMLData} value.
+	 * @param key
+	 * @param value
+	 * @see com.liferay.portal.template.soy.data.SoyHTMLData
+	 * @review
+	 */
+	public void putHTML(String key, String value);
+
+	public void putInjectedData(String key, Object value);
+
+	public void removeInjectedData(String key);
+
+}
---
+++ modules/apps/portal-template/portal-template-soy-api/src/main/java/com/liferay/portal/template/soy/util/SoyTemplateResourcesProvider.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.template.soy.util;
+
+import com.liferay.portal.kernel.template.TemplateResource;
+
+import java.util.List;
+
+import org.osgi.framework.Bundle;
+
+/**
+ * @author Miguel Pastor
+ */
+public interface SoyTemplateResourcesProvider {
+
+	public List<TemplateResource> getAllTemplateResources();
+
+	public List<TemplateResource> getBundleTemplateResources(
+		Bundle bundle, String templatePath);
+
+}
