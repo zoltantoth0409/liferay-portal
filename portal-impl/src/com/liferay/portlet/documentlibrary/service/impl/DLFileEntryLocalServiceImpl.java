2c0d50b83c4a4fff47d3bb812d03f90c2a8f0e29
Adolfo Pérez <adolfo.perez@liferay.com>
2019-01-17 15:11:37 +0100

    LPS-88762 SF - Inline variable
    The condition is so simple that introducing a variable is not worth
    it.

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -2485,14 +2485,8 @@ public class DLFileEntryLocalServiceImpl
 			autoCheckIn = true;
 		}
 
-		boolean updateDLStore = false;
-
-		if ((file != null) || (is != null)) {
-			updateDLStore = true;
-		}
-
 		if (autoCheckIn) {
-			if (updateDLStore) {
+			if ((file != null) || (is != null)) {
 				dlFileEntry = _checkOutDLFileEntryModel(
 					userId, fileEntryId, fileEntryTypeId, serviceContext);
 			}
@@ -2568,7 +2562,7 @@ public class DLFileEntryLocalServiceImpl
 
 			// File
 
-			if (updateDLStore) {
+			if ((file != null) || (is != null)) {
 				DLStoreUtil.deleteFile(
 					user.getCompanyId(), dlFileEntry.getDataRepositoryId(),
 					dlFileEntry.getName(), version);
