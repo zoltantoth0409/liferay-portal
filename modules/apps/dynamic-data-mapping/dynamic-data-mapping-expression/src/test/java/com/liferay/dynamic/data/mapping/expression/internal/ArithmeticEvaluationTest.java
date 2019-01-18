6dbcf74f013132c8b596cea2a6cb74be8dccbaf4
Hugo Huijser <hugo.huijser@liferay.com>
2019-01-18 09:12:40 +0800

    LPS-86806 SF, avoid chaining

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-expression/src/test/java/com/liferay/dynamic/data/mapping/expression/internal/ArithmeticEvaluationTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-expression/src/test/java/com/liferay/dynamic/data/mapping/expression/internal/ArithmeticEvaluationTest.java
@@ -26,27 +26,27 @@ public class ArithmeticEvaluationTest {
 
 	@Test
 	public void testAdditionExpression() throws Exception {
-		Assert.assertEquals(2, evaluate("1 + 1").intValue());
-		Assert.assertEquals(4L, evaluate("2 + 2").longValue());
-		Assert.assertEquals(3.5F, evaluate("2.5 + 1").floatValue(), .1);
-		Assert.assertEquals(.5D, evaluate("-2 + 2.5").doubleValue(), .1);
+		Assert.assertEquals(2, _evaluateInt("1 + 1"));
+		Assert.assertEquals(4L, _evaluateLong("2 + 2"));
+		Assert.assertEquals(3.5F, _evaluateFloat("2.5 + 1"), .1);
+		Assert.assertEquals(.5D, _evaluateDouble("-2 + 2.5"), .1);
 	}
 
 	@Test
 	public void testCombinedDecimalExpression() throws Exception {
 		int expected = -((1 + 3) - 4 * (2 - (2 + (4 - 5)) * (5 - 2) * 5) + 1);
 
-		Number actual = evaluate(
+		int actual = _evaluateInt(
 			"-((1 + 3) - 4 * (2 - (2 + (4 - 5)) * (5 - 2) * 5) + 1)");
 
-		Assert.assertEquals(expected, actual.intValue());
+		Assert.assertEquals(expected, actual);
 
 		expected = 1 * 2 - 5 * 4 - 3 - 5 * 2 * 5 - 5 + 7 - 10 - 4 * 3 - 2 + 1;
 
-		actual = evaluate(
+		actual = _evaluateInt(
 			"1 * 2 - 5 * 4 - 3 - 5 * 2 * 5 - 5 + 7 - 10 - 4 * 3 - 2 + 1");
 
-		Assert.assertEquals(expected, actual.intValue());
+		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
@@ -54,65 +54,65 @@ public class ArithmeticEvaluationTest {
 		double expected =
 			-(-2.5 + 4 * (2.1 + ((11 + 3.2) - 2) * (.5 + 6) - (2.6 * 1.1 - 4)));
 
-		Number actual = evaluate(
+		double actual = _evaluateDouble(
 			"-(-2.5 + 4 * (2.1 + ((11 + 3.2) - 2) * (.5 + 6) - (2.6 * 1.1 - " +
 				"4)))");
 
-		Assert.assertEquals(expected, actual.doubleValue(), .1);
+		Assert.assertEquals(expected, actual, .1);
 	}
 
 	@Test
 	public void testDecimalLiteral() throws Exception {
-		Assert.assertEquals(42, evaluate("42").intValue());
-		Assert.assertEquals(10000000000L, evaluate("10000000000").longValue());
+		Assert.assertEquals(42, _evaluateInt("42"));
+		Assert.assertEquals(10000000000L, _evaluateLong("10000000000"));
 	}
 
 	@Test
 	public void testDivisionExpression() throws Exception {
-		Assert.assertEquals(2, evaluate("4 / 2").intValue());
-		Assert.assertEquals(4L, evaluate("12 / 3").longValue());
-		Assert.assertEquals(7.5F, evaluate("15 / 2").floatValue(), .1);
-		Assert.assertEquals(8.5D, evaluate("17 / 2").doubleValue(), .1);
+		Assert.assertEquals(2, _evaluateInt("4 / 2"));
+		Assert.assertEquals(4L, _evaluateLong("12 / 3"));
+		Assert.assertEquals(7.5F, _evaluateFloat("15 / 2"), .1);
+		Assert.assertEquals(8.5D, _evaluateDouble("17 / 2"), .1);
 	}
 
 	@Test
 	public void testFloatingPointLiteral() throws Exception {
-		Assert.assertEquals(42.5F, evaluate("42.5").floatValue(), .1);
+		Assert.assertEquals(42.5F, _evaluateFloat("42.5"), .1);
 		Assert.assertEquals(
-			10000000000.5D, evaluate("10000000000.5").doubleValue(), .1);
+			10000000000.5D, _evaluateDouble("10000000000.5"), .1);
 	}
 
 	@Test
 	public void testMinusExpression() throws Exception {
-		Assert.assertEquals(-1, evaluate("-1").intValue());
-		Assert.assertEquals(1, evaluate("--1").intValue());
-		Assert.assertEquals(-.5F, evaluate("-.5").floatValue(), .1);
-		Assert.assertEquals(5.5D, evaluate("--5.5").doubleValue(), .1);
+		Assert.assertEquals(-1, _evaluateInt("-1"));
+		Assert.assertEquals(1, _evaluateInt("--1"));
+		Assert.assertEquals(-.5F, _evaluateFloat("-.5"), .1);
+		Assert.assertEquals(5.5D, _evaluateDouble("--5.5"), .1);
 	}
 
 	@Test
 	public void testMultiplicationExpression() throws Exception {
-		Assert.assertEquals(2, evaluate("2 * 1").intValue());
-		Assert.assertEquals(8L, evaluate("4 * 2").longValue());
-		Assert.assertEquals(5F, evaluate("2.5 * 2").floatValue(), .1);
-		Assert.assertEquals(7D, evaluate("2 * 3.5").doubleValue(), .1);
+		Assert.assertEquals(2, _evaluateInt("2 * 1"));
+		Assert.assertEquals(8L, _evaluateLong("4 * 2"));
+		Assert.assertEquals(5F, _evaluateFloat("2.5 * 2"), .1);
+		Assert.assertEquals(7D, _evaluateDouble("2 * 3.5"), .1);
 	}
 
 	@Test
 	public void testScientificNotation() throws Exception {
-		Assert.assertEquals(100000L, evaluate("1e5").longValue());
-		Assert.assertEquals(100000000L, evaluate("1E8").longValue());
-		Assert.assertEquals(123, evaluate(".123e+3").longValue());
-		Assert.assertEquals(.2D, evaluate("2e-1").doubleValue(), .01);
-		Assert.assertEquals(.123, evaluate("123E-3").doubleValue(), .001);
+		Assert.assertEquals(100000L, _evaluateLong("1e5"));
+		Assert.assertEquals(100000000L, _evaluateLong("1E8"));
+		Assert.assertEquals(123, _evaluateLong(".123e+3"));
+		Assert.assertEquals(.2D, _evaluateDouble("2e-1"), .01);
+		Assert.assertEquals(.123, _evaluateDouble("123E-3"), .001);
 	}
 
 	@Test
 	public void testSubtractionExpression() throws Exception {
-		Assert.assertEquals(1, evaluate("2 - 1").intValue());
-		Assert.assertEquals(2L, evaluate("4 - 2").longValue());
-		Assert.assertEquals(.5F, evaluate("2.5 - 2").floatValue(), .1);
-		Assert.assertEquals(-.5D, evaluate("2 - 2.5").doubleValue(), .1);
+		Assert.assertEquals(1, _evaluateInt("2 - 1"));
+		Assert.assertEquals(2L, _evaluateLong("4 - 2"));
+		Assert.assertEquals(.5F, _evaluateFloat("2.5 - 2"), .1);
+		Assert.assertEquals(-.5D, _evaluateDouble("2 - 2.5"), .1);
 	}
 
 	protected Number evaluate(String expressionString) throws Exception {
@@ -120,6 +120,30 @@ public class ArithmeticEvaluationTest {
 			expressionString, Number.class);
 
 		return ddmExpression.evaluate();
+	}
+
+	private double _evaluateDouble(String expressionString) throws Exception {
+		Number number = evaluate(expressionString);
+
+		return number.doubleValue();
+	}
+
+	private float _evaluateFloat(String expressionString) throws Exception {
+		Number number = evaluate(expressionString);
+
+		return number.floatValue();
+	}
+
+	private int _evaluateInt(String expressionString) throws Exception {
+		Number number = evaluate(expressionString);
+
+		return number.intValue();
+	}
+
+	private long _evaluateLong(String expressionString) throws Exception {
+		Number number = evaluate(expressionString);
+
+		return number.longValue();
 	}
 
 }
