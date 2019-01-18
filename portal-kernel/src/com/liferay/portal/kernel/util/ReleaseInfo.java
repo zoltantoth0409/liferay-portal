8681ed710e01a9c3cf81e908ff69a6359005aec4
Chas Austin <charles.austin@liferay.com>
2019-01-18 13:53:06 -0800

    LRQA-46220 ReleaseInfo - Alpha 1

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -108,7 +108,7 @@ public class ReleaseInfo {
 
 	private static final String _CODE_NAME = "Mueller";
 
-	private static final String _DATE = "January 7, 2019";
+	private static final String _DATE = "January 31, 2019";
 
 	private static final String _NAME = "Liferay Community Edition Portal";
 
@@ -124,7 +124,7 @@ public class ReleaseInfo {
 
 	private static final String _VERSION = "7.2.0";
 
-	private static final String _VERSION_DISPLAY_NAME = "7.2.0 CE M1";
+	private static final String _VERSION_DISPLAY_NAME = "7.2.0 CE A1";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
