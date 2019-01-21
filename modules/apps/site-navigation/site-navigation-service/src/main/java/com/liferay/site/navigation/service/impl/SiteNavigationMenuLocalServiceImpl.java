7b2f82316a5b02288ef5fdc3cc20e0110542db27
balazssk <balazs.safrany-kovalik@liferay.com>
2019-01-21 16:04:39 +0100

    LPS-89358 Override entity-parametrized method to trigger system event advice correctly

--- modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/service/impl/SiteNavigationMenuLocalServiceImpl.java
+++ modules/apps/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/service/impl/SiteNavigationMenuLocalServiceImpl.java
@@ -22,8 +22,12 @@ import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.spring.extender.service.ServiceReference;
@@ -144,10 +148,13 @@ public class SiteNavigationMenuLocalServiceImpl
 		SiteNavigationMenu siteNavigationMenu = getSiteNavigationMenu(
 			siteNavigationMenuId);
 
-		return deleteSiteNavigationMenu(siteNavigationMenu);
+		return siteNavigationMenuLocalService.deleteSiteNavigationMenu(
+			siteNavigationMenu);
 	}
 
+	@Indexable(type = IndexableType.DELETE)
 	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public SiteNavigationMenu deleteSiteNavigationMenu(
 			SiteNavigationMenu siteNavigationMenu)
 		throws PortalException {
