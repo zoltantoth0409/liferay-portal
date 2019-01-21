e04416339b5304f22b9390990d2048e86a8a2b42
Pablo Molina <contact@pablomolina.me>
2019-01-21 16:59:25 +0100

    LPS-89006 Reuse existing functions in portlets.es.js

--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/reducers/portlets.es.js
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/fragments_editor/reducers/portlets.es.js
@@ -1,7 +1,7 @@
 import {ADD_PORTLET} from '../actions/actions.es';
-import {add, setIn, updateIn, updateLayoutData} from '../utils/FragmentsEditorUpdateUtils.es';
-import {FRAGMENTS_EDITOR_ITEM_TYPES} from '../utils/constants';
-import {getColumn, getWidgetPath} from '../utils/FragmentsEditorGetUtils.es';
+import {addFragment, getFragmentEntryLinkContent} from './fragments.es';
+import {getWidgetPath} from '../utils/FragmentsEditorGetUtils.es';
+import {setIn, updateLayoutData} from '../utils/FragmentsEditorUpdateUtils.es';
 
 /**
  * @param {!object} state
@@ -22,38 +22,38 @@ function addPortletReducer(state, actionType, payload) {
 				let nextData = null;
 
 				_addPortlet(
-					state.addPortletURL,
+					nextState.addPortletURL,
 					payload.portletId,
-					state.classNameId,
-					state.classPK,
-					state.portletNamespace
+					nextState.classNameId,
+					nextState.classPK,
+					nextState.portletNamespace
 				)
 					.then(
 						response => {
 							fragmentEntryLink = response;
 
-							nextData = _addPortletElement(
+							nextData = addFragment(
 								fragmentEntryLink.fragmentEntryLinkId,
-								state.dropTargetBorder,
-								state.dropTargetItemId,
-								state.dropTargetItemType,
-								state.layoutData
+								nextState.dropTargetBorder,
+								nextState.dropTargetItemId,
+								nextState.dropTargetItemType,
+								nextState.layoutData
 							);
 
 							return updateLayoutData(
-								state.updateLayoutPageTemplateDataURL,
-								state.portletNamespace,
-								state.classNameId,
-								state.classPK,
+								nextState.updateLayoutPageTemplateDataURL,
+								nextState.portletNamespace,
+								nextState.classNameId,
+								nextState.classPK,
 								nextData
 							);
 						}
 					)
 					.then(
-						() => _getFragmentEntryLinkContent(
-							state.renderFragmentEntryURL,
+						() => getFragmentEntryLinkContent(
+							nextState.renderFragmentEntryURL,
 							fragmentEntryLink,
-							state.portletNamespace
+							nextState.portletNamespace
 						)
 					)
 					.then(
@@ -72,7 +72,7 @@ function addPortletReducer(state, actionType, payload) {
 							);
 
 							if (!payload.instanceable) {
-								const widgetPath = getWidgetPath(state.widgets, payload.portletId);
+								const widgetPath = getWidgetPath(nextState.widgets, payload.portletId);
 
 								nextState = setIn(
 									nextState,
@@ -152,135 +152,6 @@ function _addPortlet(
 					fragmentEntryLinkId: response.fragmentEntryLinkId,
 					name: response.name
 				};
-			}
-		);
-}
-
-/**
- * Adds a portlet fragment at the corresponding container in the layout
- * @param {string} fragmentEntryLinkId
- * @param {string} dropTargetBorder
- * @param {string} dropTargetItemId
- * @param {string} dropTargetItemType
- * @param {object} layoutData
- * @private
- * @return {object}
- * @review
- */
-function _addPortletElement(
-	fragmentEntryLinkId,
-	dropTargetBorder,
-	dropTargetItemId,
-	dropTargetItemType,
-	layoutData
-) {
-	let nextData = layoutData;
-
-	if (dropTargetItemType === FRAGMENTS_EDITOR_ITEM_TYPES.column) {
-		const fragmentColumn = getColumn(
-			layoutData.structure,
-			dropTargetItemId
-		);
-
-		nextData = _addPortletToColumn(
-			layoutData,
-			fragmentEntryLinkId,
-			dropTargetItemId,
-			fragmentColumn.fragmentEntryLinkIds.length
-		);
-	}
-
-	return nextData;
-}
-
-/**
- * Returns a new layoutData with the given portlet element inserted
- * into a given column at the given position
- *
- * @param {object} layoutData
- * @param {string} fragmentEntryLinkId
- * @param {string} targetColumnId
- * @param {number} position
- * @return {object}
- */
-function _addPortletToColumn(
-	layoutData,
-	fragmentEntryLinkId,
-	targetColumnId,
-	position
-) {
-	const {structure} = layoutData;
-
-	const column = getColumn(structure, targetColumnId);
-	const section = structure.find(
-		_section => _section.columns.find(
-			_column => column === _column
-		)
-	);
-
-	const columnIndex = section.columns.indexOf(column);
-	const sectionIndex = structure.indexOf(section);
-
-	return updateIn(
-		layoutData,
-		[
-			'structure',
-			sectionIndex,
-			'columns',
-			columnIndex,
-			'fragmentEntryLinkIds'
-		],
-		fragmentEntryLinkIds => add(
-			fragmentEntryLinkIds,
-			fragmentEntryLinkId,
-			position
-		)
-	);
-}
-
-/**
- * @param {string} renderFragmentEntryURL
- * @param {{fragmentEntryLinkId: string}} fragmentEntryLink
- * @param {string} portletNamespace
- * @return {Promise<object>}
- * @review
- */
-function _getFragmentEntryLinkContent(
-	renderFragmentEntryURL,
-	fragmentEntryLink,
-	portletNamespace
-) {
-	const formData = new FormData();
-
-	formData.append(
-		`${portletNamespace}fragmentEntryLinkId`,
-		fragmentEntryLink.fragmentEntryLinkId
-	);
-
-	return fetch(
-		renderFragmentEntryURL,
-		{
-			body: formData,
-			credentials: 'include',
-			method: 'POST'
-		}
-	)
-		.then(
-			response => response.json()
-		)
-		.then(
-			response => {
-				if (!response.content) {
-					throw new Error();
-				}
-
-				return Object.assign(
-					{},
-					fragmentEntryLink,
-					{
-						content: response.content
-					}
-				);
 			}
 		);
 }
