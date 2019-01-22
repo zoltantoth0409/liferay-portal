c4757db31e2a6b490a48ea73300f2f9ed08989a6
Hugo Huijser <hugo.huijser@liferay.com>
2019-01-22 12:13:10 +0800

    LPS-86806 Remove logic to allow specific long lines in *Table.java

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaLongLinesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaLongLinesCheck.java
@@ -52,10 +52,7 @@ public class JavaLongLinesCheck extends BaseFileCheck {
 
 				if (line.startsWith("import ") || line.startsWith("package ") ||
 					line.startsWith(StringPool.SPACE) ||
-					line.matches("\\s*\\*.*") ||
-					(fileName.endsWith("Table.java") &&
-					 (line.contains("final String TABLE_") ||
-					  line.contains("\"create index ")))) {
+					line.matches("\\s*\\*.*")) {
 
 					continue;
 				}
