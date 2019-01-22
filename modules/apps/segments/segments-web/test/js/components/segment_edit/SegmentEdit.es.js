080ca586a591d4cb146d8f74404d7634d97e916a
Kevin Tan <kevin.tan@liferay.com>
2019-01-22 10:46:12 -0800

    LPS-89268 Update tests

--- modules/apps/segments/segments-web/test/js/components/segment_edit/SegmentEdit.es.js
+++ modules/apps/segments/segments-web/test/js/components/segment_edit/SegmentEdit.es.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import SegmentEdit from 'components/segment_edit/SegmentEdit.es';
+import {SOURCES} from 'utils/constants.es';
 import {cleanup, render} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+const SOURCE_ICON_TESTID = 'source-icon';
 
 describe(
 	'SegmentEdit',
@@ -17,6 +21,42 @@ describe(
 				);
 
 				expect(asFragment()).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should render with an analytics cloud icon',
+			() => {
+				const {icon, name} = SOURCES.ASAH_FARO_BACKEND;
+
+				const {getByTestId} = render(
+					<SegmentEdit
+						locale="en_US"
+						source={name}
+					/>
+				);
+
+				const image = getByTestId(SOURCE_ICON_TESTID);
+
+				expect(image).toHaveAttribute('src', icon);
+			}
+		);
+
+		it(
+			'should render with a dxp icon',
+			() => {
+				const {icon, name} = SOURCES.DEFAULT;
+
+				const {getByTestId} = render(
+					<SegmentEdit
+						locale="en_US"
+						source={name}
+					/>
+				);
+
+				const image = getByTestId(SOURCE_ICON_TESTID);
+
+				expect(image).toHaveAttribute('src', icon);
 			}
 		);
 	}
