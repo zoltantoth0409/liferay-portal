9012015f90a729ae6349cc28a2d3d93ba10c7871
Máté Thurzó <mate.thurzo@liferay.com>
2019-01-23 18:14:13 +0100

    LPS-89270 Extend display context with code for UI pieces

--- modules/apps/change-tracking/change-tracking-change-lists-web/src/main/java/com/liferay/change/tracking/change/lists/web/internal/display/context/ChangeListsDisplayContext.java
+++ modules/apps/change-tracking/change-tracking-change-lists-web/src/main/java/com/liferay/change/tracking/change/lists/web/internal/display/context/ChangeListsDisplayContext.java
@@ -14,15 +14,25 @@
 
 package com.liferay.change.tracking.change.lists.web.internal.display.context;
 
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemList;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.ViewTypeItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.ViewTypeItemList;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.template.soy.util.SoyContext;
 import com.liferay.portal.template.soy.util.SoyContextFactoryUtil;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
+import javax.portlet.PortletURL;
 import javax.portlet.RenderResponse;
 
 import javax.servlet.http.HttpServletRequest;
@@ -81,7 +91,209 @@ public class ChangeListsDisplayContext {
 		return soyContext;
 	}
 
+	public CreationMenu getCreationMenu() {
+		CreationMenu creationMenu = new CreationMenu();
+
+		creationMenu.addDropdownItem(
+			dropdownItem -> {
+				dropdownItem.setHref(
+					_renderResponse.createRenderURL(), "mvcRenderCommandName",
+					"/change_lists/add_change_tracking_collection");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "add-change-list"));
+			});
+
+		return creationMenu;
+	}
+
+	public List<DropdownItem> getFilterDropdownItems() {
+		return new DropdownItemList() {
+			{
+				addGroup(
+					dropdownGroupItem -> {
+						dropdownGroupItem.setDropdownItems(
+							_getFilterUserDropdownItems());
+						dropdownGroupItem.setLabel(
+							LanguageUtil.get(
+								_httpServletRequest, "filter-by-user"));
+					});
+
+				addGroup(
+					dropdownGroupItem -> {
+						dropdownGroupItem.setDropdownItems(
+							_getFilterStatusDropdownItems());
+						dropdownGroupItem.setLabel(
+							LanguageUtil.get(
+								_httpServletRequest, "filter-by-status"));
+					});
+
+				addGroup(
+					dropdownGroupItem -> {
+						dropdownGroupItem.setDropdownItems(
+							_getOrderByDropdownItems());
+						dropdownGroupItem.setLabel(
+							LanguageUtil.get(_httpServletRequest, "order-by"));
+					});
+			}
+		};
+	}
+
+	public String getOrderByType() {
+		if (_orderByType != null) {
+			return _orderByType;
+		}
+
+		_orderByType = ParamUtil.getString(
+			_httpServletRequest, "orderByType", "asc");
+
+		return _orderByType;
+	}
+
+	public String getSortingURL() {
+		PortletURL sortingURL = _getPortletURL();
+
+		sortingURL.setParameter(
+			"orderByType",
+			Objects.equals(getOrderByType(), "asc") ? "desc" : "asc");
+
+		return sortingURL.toString();
+	}
+
+	public List<ViewTypeItem> getViewTypeItems() {
+		return new ViewTypeItemList(_getPortletURL(), _getDisplayStyle()) {
+			{
+				addCardViewTypeItem();
+				addTableViewTypeItem();
+			}
+		};
+	}
+
+	private String _getDisplayStyle() {
+		if (_displayStyle != null) {
+			return _displayStyle;
+		}
+
+		_displayStyle = ParamUtil.getString(
+			_httpServletRequest, "displayStyle", "list");
+
+		return _displayStyle;
+	}
+
+	private String _getFilterByStatus() {
+		if (_filterByStatus != null) {
+			return _filterByStatus;
+		}
+
+		_filterByStatus = ParamUtil.getString(
+			_httpServletRequest, "status", "all");
+
+		return _filterByStatus;
+	}
+
+	private List<DropdownItem> _getFilterStatusDropdownItems() {
+		return new DropdownItemList() {
+			{
+				add(
+					dropdownItem -> {
+						dropdownItem.setActive(
+							Objects.equals(_getFilterByStatus(), "all"));
+						dropdownItem.setHref(_getPortletURL(), "status", "all");
+						dropdownItem.setLabel(
+							LanguageUtil.get(_httpServletRequest, "all"));
+					});
+				add(
+					dropdownItem -> {
+						dropdownItem.setActive(
+							Objects.equals(_getFilterByStatus(), "active"));
+						dropdownItem.setHref(
+							_getPortletURL(), "status", "active");
+						dropdownItem.setLabel(
+							LanguageUtil.get(_httpServletRequest, "active"));
+					});
+			}
+		};
+	}
+
+	private List<DropdownItem> _getFilterUserDropdownItems() {
+		return new DropdownItemList() {
+			{
+				add(
+					dropdownItem -> {
+						dropdownItem.setHref(_getPortletURL());
+						dropdownItem.setLabel(
+							LanguageUtil.get(_httpServletRequest, "all"));
+					});
+			}
+		};
+	}
+
+	private String _getOrderByCol() {
+		if (_orderByCol != null) {
+			return _orderByCol;
+		}
+
+		_orderByCol = ParamUtil.getString(
+			_httpServletRequest, "orderByCol", "create-date");
+
+		return _orderByCol;
+	}
+
+	private List<DropdownItem> _getOrderByDropdownItems() {
+		return new DropdownItemList() {
+			{
+				add(
+					dropdownItem -> {
+						dropdownItem.setActive(
+							Objects.equals(_getOrderByCol(), "modified-date"));
+						dropdownItem.setHref(
+							_getPortletURL(), "orderByCol", "modified-date");
+						dropdownItem.setLabel(
+							LanguageUtil.get(
+								_httpServletRequest, "modified-date"));
+					});
+				add(
+					dropdownItem -> {
+						dropdownItem.setActive(
+							Objects.equals(_getOrderByCol(), "name"));
+						dropdownItem.setHref(
+							_getPortletURL(), "orderByCol", "name");
+						dropdownItem.setLabel(
+							LanguageUtil.get(_httpServletRequest, "name"));
+					});
+			}
+		};
+	}
+
+	private PortletURL _getPortletURL() {
+		PortletURL portletURL = _renderResponse.createRenderURL();
+
+		String displayStyle = ParamUtil.getString(
+			_httpServletRequest, "displayStyle");
+
+		if (Validator.isNotNull(displayStyle)) {
+			portletURL.setParameter("displayStyle", _getDisplayStyle());
+		}
+
+		String orderByCol = _getOrderByCol();
+
+		if (Validator.isNotNull(orderByCol)) {
+			portletURL.setParameter("orderByCol", orderByCol);
+		}
+
+		String orderByType = getOrderByType();
+
+		if (Validator.isNotNull(orderByType)) {
+			portletURL.setParameter("orderByType", orderByType);
+		}
+
+		return portletURL;
+	}
+
+	private String _displayStyle;
+	private String _filterByStatus;
 	private final HttpServletRequest _httpServletRequest;
+	private String _orderByCol;
+	private String _orderByType;
 	private final RenderResponse _renderResponse;
 	private final ThemeDisplay _themeDisplay;
 
