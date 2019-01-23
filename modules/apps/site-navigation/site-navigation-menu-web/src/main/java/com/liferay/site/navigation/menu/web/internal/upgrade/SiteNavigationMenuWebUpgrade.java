9387f48668e9d8d1070d095001854c74a7071d23
balazssk <balazs.safrany-kovalik@liferay.com>
2019-01-23 10:37:26 +0100

    LPS-88997 Rename to better reflect super class

--- modules/apps/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/upgrade/SiteNavigationMenuWebUpgrade.java
+++ modules/apps/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/upgrade/SiteNavigationMenuWebUpgrade.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.site.navigation.menu.web.internal.upgrade.v1_0_0.UpgradePortletId;
 import com.liferay.site.navigation.menu.web.internal.upgrade.v1_0_0.UpgradePortletPreferences;
+import com.liferay.site.navigation.menu.web.internal.upgrade.v1_0_1.RenameUpgradePortletPreferences;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -36,9 +37,7 @@ public class SiteNavigationMenuWebUpgrade implements UpgradeStepRegistrator {
 			new UpgradePortletPreferences());
 
 		registry.register(
-			"1.0.0", "1.0.1",
-			new com.liferay.site.navigation.menu.web.internal.upgrade.v1_0_1.
-				UpgradePortletPreferences());
+			"1.0.0", "1.0.1", new RenameUpgradePortletPreferences());
 	}
 
 }
