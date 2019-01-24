3af2066620ede4374ae6c9a9c71eae4bf1e5ab7f
Peter Shin <peter.shin@liferay.com>
2019-01-23 22:09:56 -0800

    LPS-89415 Abstract out FreeMarker logic

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/RESTBuilder.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/RESTBuilder.java
@@ -15,22 +15,18 @@
 package com.liferay.portal.tools.rest.builder;
 
 import com.liferay.portal.tools.ArgumentsUtil;
+import com.liferay.portal.tools.rest.builder.internal.freemarker.FreeMarker;
+import com.liferay.portal.tools.rest.builder.internal.freemarker.FreeMarkerConstants;
 import com.liferay.portal.tools.rest.builder.internal.util.FileUtil;
 import com.liferay.portal.tools.rest.builder.internal.yaml.Components;
 import com.liferay.portal.tools.rest.builder.internal.yaml.Configuration;
 import com.liferay.portal.tools.rest.builder.internal.yaml.Schema;
-
-import freemarker.cache.ClassTemplateLoader;
-
-import freemarker.template.DefaultObjectWrapperBuilder;
-import freemarker.template.Template;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -127,7 +123,7 @@ public class RESTBuilder {
 		context.put("name", name);
 		context.put("schema", schema);
 
-		return _processTemplate(
+		return _freeMarker.processTemplate(
 			"com/liferay/portal/tools/rest/builder/dependencies/model.ftl",
 			context);
 	}
@@ -148,44 +144,6 @@ public class RESTBuilder {
 		return new File(sb.toString());
 	}
 
-	private String _processTemplate(String name, Map<String, Object> context)
-		throws Exception {
-
-		freemarker.template.Configuration configuration = _configuration;
-
-		if (configuration == null) {
-			_configuration = new freemarker.template.Configuration(
-				freemarker.template.Configuration.getVersion());
-
-			_configuration.setNumberFormat("computer");
-
-			DefaultObjectWrapperBuilder defaultObjectWrapperBuilder =
-				new DefaultObjectWrapperBuilder(
-					freemarker.template.Configuration.getVersion());
-
-			_configuration.setObjectWrapper(
-				defaultObjectWrapperBuilder.build());
-
-			_configuration.setTemplateLoader(
-				new ClassTemplateLoader(RESTBuilder.class, "/"));
-			_configuration.setTemplateUpdateDelayMilliseconds(Long.MAX_VALUE);
-
-			configuration = _configuration;
-		}
-
-		Template template = configuration.getTemplate(name);
-
-		StringWriter stringWriter = new StringWriter();
-
-		template.process(context, stringWriter);
-
-		StringBuffer stringBuffer = stringWriter.getBuffer();
-
-		String content = stringBuffer.toString();
-
-		return content.replace("\r", "");
-	}
-
-	private static freemarker.template.Configuration _configuration;
+	private static final FreeMarker _freeMarker = new FreeMarker();
 
 }
---
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/FreeMarker.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.rest.builder.internal.freemarker;
+
+import freemarker.cache.ClassTemplateLoader;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapperBuilder;
+import freemarker.template.Template;
+
+import java.io.StringWriter;
+
+import java.util.Map;
+
+/**
+ * @author Peter Shin
+ */
+public class FreeMarker {
+
+	public FreeMarker() {
+		_configuration.setNumberFormat("computer");
+
+		DefaultObjectWrapperBuilder defaultObjectWrapperBuilder =
+			new DefaultObjectWrapperBuilder(Configuration.getVersion());
+
+		_configuration.setObjectWrapper(defaultObjectWrapperBuilder.build());
+
+		ClassTemplateLoader classTemplateLoader = new ClassTemplateLoader(
+			FreeMarker.class, "/");
+
+		_configuration.setTemplateLoader(classTemplateLoader);
+
+		_configuration.setTemplateUpdateDelayMilliseconds(Long.MAX_VALUE);
+	}
+
+	public String processTemplate(String name, Map<String, Object> context)
+		throws Exception {
+
+		Template template = _configuration.getTemplate(name);
+
+		StringWriter stringWriter = new StringWriter();
+
+		template.process(context, stringWriter);
+
+		StringBuffer stringBuffer = stringWriter.getBuffer();
+
+		return stringBuffer.toString();
+	}
+
+	private static final Configuration _configuration = new Configuration(
+		Configuration.getVersion());
+
+}
