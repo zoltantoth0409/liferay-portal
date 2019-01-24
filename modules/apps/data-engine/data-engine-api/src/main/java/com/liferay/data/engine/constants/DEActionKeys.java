c108798972cb91c1e04ebacecc622f1f3b47cab3
Leonardo Barros <leonardo.barros@liferay.com>
2019-01-24 10:59:47 -0300

    LPS-89131 Add constants

--- modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/constants/DEActionKeys.java
+++ modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/constants/DEActionKeys.java
@@ -29,6 +29,10 @@ public class DEActionKeys {
 
 	public static final String DEFINE_PERMISSIONS_ACTION = "DEFINE_PERMISSIONS";
 
+	public static final String DELETE_DATA_RECORD = "DELETE_DATA_RECORD";
+
 	public static final String UPDATE_DATA_RECORD = "UPDATE_DATA_RECORD";
+
+	public static final String VIEW_DATA_RECORD = "VIEW_DATA_RECORD";
 
 }
