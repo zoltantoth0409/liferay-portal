64b4309b28a62c3bd02067989d3b37911ca14d10
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-01-24 12:03:53 +0100

    LPS-89483 Adds new module for a fragment collection contributor of fragments of section content type with its contributor

---
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-content-section/src/main/java/com/liferay/fragment/collection/contributor/content/section/ContentSectionFragmentCollectionContributor.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.collection.contributor.content.section;
+
+import com.liferay.fragment.contributor.BaseFragmentCollectionContributor;
+import com.liferay.fragment.contributor.FragmentCollectionContributor;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(service = FragmentCollectionContributor.class)
+public class ContentSectionFragmentCollectionContributor
+	extends BaseFragmentCollectionContributor {
+
+	@Override
+	public String getFragmentCollectionKey() {
+		return "CONTENT_SECTION";
+	}
+
+	@Activate
+	protected void activate() {
+		readAndCheckFragmentCollectionStructure();
+	}
+
+	@Override
+	protected Class getResourceClass() {
+		return ContentSectionFragmentCollectionContributor.class;
+	}
+
+}
