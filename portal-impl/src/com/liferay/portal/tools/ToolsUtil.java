d7b329c41400d4e7dbb8dc690bd7e0a50d558d13
Hugo Huijser <hugo.huijser@liferay.com>
2019-01-24 11:49:06 +0800

    LPS-88665 Add logic to deal with Jalopy hack to ToolsUtil

--- portal-impl/src/com/liferay/portal/tools/ToolsUtil.java
+++ portal-impl/src/com/liferay/portal/tools/ToolsUtil.java
@@ -455,6 +455,26 @@ public class ToolsUtil {
 
 		// Beautify
 
+		String jalopyIgnoreStart = "/** Jalopy ignore start */";
+
+		String jalopyIgnoreEnd = "/** Jalopy ignore end */\n";
+
+		int start = content.indexOf(jalopyIgnoreStart);
+
+		String jalopyIgnoreBody = null;
+
+		if (start != -1) {
+			start += jalopyIgnoreStart.length();
+
+			int end = content.indexOf(jalopyIgnoreEnd);
+
+			if (end != -1) {
+				jalopyIgnoreBody = content.substring(start, end);
+
+				content = content.substring(0, start) + content.substring(end);
+			}
+		}
+
 		StringBuffer sb = new StringBuffer();
 
 		Jalopy jalopy = new Jalopy();
@@ -532,6 +552,18 @@ public class ToolsUtil {
 		boolean formatSuccess = jalopy.format();
 
 		String newContent = sb.toString();
+
+		if (jalopyIgnoreBody != null) {
+			start = newContent.indexOf(jalopyIgnoreStart);
+
+			start = newContent.lastIndexOf('\n', start);
+
+			int end = newContent.indexOf(jalopyIgnoreEnd);
+
+			newContent =
+				newContent.substring(0, start) + jalopyIgnoreBody +
+					newContent.substring(end + jalopyIgnoreEnd.length());
+		}
 
 		// Remove double blank lines after the package or last import
 
