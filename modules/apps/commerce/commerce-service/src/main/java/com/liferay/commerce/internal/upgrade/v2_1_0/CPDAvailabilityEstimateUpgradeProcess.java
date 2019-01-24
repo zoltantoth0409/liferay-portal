ethanbustad/com-liferay-commerce

COMMERCE-704 commerce-service Upgrade process

commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v1_2_0/CPDAvailabilityEstimateUpgradeProcess.java
@@ -1,0 +1,167 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.upgrade.v1_2_0;
+
+import com.liferay.commerce.model.impl.CPDAvailabilityEstimateModelImpl;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CPInstanceLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+
+import java.lang.Override;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Alec Sloan
+ */
+public class CPDAvailabilityEstimateUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addColumn(
+			CPDAvailabilityEstimateModelImpl.class,
+			CPDAvailabilityEstimateModelImpl.TABLE_NAME, "CProductId", "LONG");
+
+		_addIndexes(CPDAvailabilityEstimateModelImpl.TABLE_NAME);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Statement s = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update CPDAvailabilityEstimate set CProductId = ? where " +
+					"CPDefinitionId = ?");
+
+			s = connection.createStatement();
+
+			rs = s.executeQuery("select * from CPDAvailabilityEstimate");
+
+			while (rs.next()) {
+				ps.setLong(1, rs.getLong("CPDefinitionId"));
+				ps.setLong(2, rs.getLong("CPDefinitionId"));
+
+				ps.execute();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+			DataAccess.cleanUp(s, rs);
+		}
+
+		runSQL("drop index IX_86A2368F on CPDAvailabilityEstimate");
+
+		runSQL(
+			"alter table CPDAvailabilityEstimate drop column CPDefinitionId");
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private void _addIndexes(String tableName) throws Exception {
+		Class<?> clazz = getClass();
+
+		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
+			clazz.getClassLoader(), tableName);
+
+		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
+			IndexMetadata indexMetadata = indexSQL.getValue();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Adding index %s to table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+
+			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
+				runSQL(indexMetadata.getCreateSQL(null));
+			}
+			else if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already exists on table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+		}
+	}
+
+	private boolean _tableHasIndex(String tableName, String indexName)
+		throws Exception {
+
+		ResultSet rs = null;
+
+		try {
+			DatabaseMetaData metadata = connection.getMetaData();
+
+			rs = metadata.getIndexInfo(null, null, tableName, false, false);
+
+			while (rs.next()) {
+				String curIndexName = rs.getString("index_name");
+
+				if (Objects.equals(indexName, curIndexName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(rs);
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDAvailabilityEstimateUpgradeProcess.class);
+
+}

commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v1_2_0/CPDefinitionInventoryUpgradeProcess.java
@@ -1,0 +1,166 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.upgrade.v1_2_0;
+
+import com.liferay.commerce.model.impl.CPDefinitionInventoryModelImpl;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CPInstanceLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+
+import java.lang.Override;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Alec Sloan
+ */
+public class CPDefinitionInventoryUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addColumn(
+			CPDefinitionInventoryModelImpl.class,
+			CPDefinitionInventoryModelImpl.TABLE_NAME, "CProductId", "LONG");
+
+		_addIndexes(CPDefinitionInventoryModelImpl.TABLE_NAME);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Statement s = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update CPDefinitionInventory set CProductId = ? where " +
+					"CPDefinitionId = ?");
+
+			s = connection.createStatement();
+
+			rs = s.executeQuery("select * from CPDefinitionInventory");
+
+			while (rs.next()) {
+				ps.setLong(1, rs.getLong("CPDefinitionId"));
+				ps.setLong(2, rs.getLong("CPDefinitionId"));
+
+				ps.execute();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+			DataAccess.cleanUp(s, rs);
+		}
+
+		runSQL("drop index IX_34D62DF1 on CPDefinitionInventory");
+
+		runSQL("alter table CPDefinitionInventory drop column CPDefinitionId");
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private void _addIndexes(String tableName) throws Exception {
+		Class<?> clazz = getClass();
+
+		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
+			clazz.getClassLoader(), tableName);
+
+		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
+			IndexMetadata indexMetadata = indexSQL.getValue();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Adding index %s to table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+
+			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
+				runSQL(indexMetadata.getCreateSQL(null));
+			}
+			else if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already exists on table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+		}
+	}
+
+	private boolean _tableHasIndex(String tableName, String indexName)
+		throws Exception {
+
+		ResultSet rs = null;
+
+		try {
+			DatabaseMetaData metadata = connection.getMetaData();
+
+			rs = metadata.getIndexInfo(null, null, tableName, false, false);
+
+			while (rs.next()) {
+				String curIndexName = rs.getString("index_name");
+
+				if (Objects.equals(indexName, curIndexName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(rs);
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDefinitionInventoryUpgradeProcess.class);
+
+}

commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v1_2_0/CommerceOrderItemUpgradeProcess.java
@@ -1,0 +1,183 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.upgrade.v1_2_0;
+
+import com.liferay.commerce.model.impl.CommerceOrderItemModelImpl;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CPInstanceLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+
+import java.lang.Override;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Alec Sloan
+ */
+public class CommerceOrderItemUpgradeProcess extends UpgradeProcess {
+
+	public CommerceOrderItemUpgradeProcess(
+		CPDefinitionLocalService cpDefinitionLocalService,
+		CPInstanceLocalService cpInstanceLocalService) {
+			_cpDefinitionLocalService = cpDefinitionLocalService;
+			_cpInstanceLocalService = cpInstanceLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+
+		_addColumn(
+			CommerceOrderItemModelImpl.class,
+			CommerceOrderItemModelImpl.TABLE_NAME, "CProductId", "LONG");
+
+		_addIndexes(CommerceOrderItemModelImpl.TABLE_NAME);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Statement s = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update CommerceOrderItem set CProductId = ? where " +
+					"CPInstanceId = ?");
+
+			s = connection.createStatement();
+
+			rs = s.executeQuery("select * from CommerceOrderItem");
+
+			while (rs.next()) {
+				long instanceId = rs.getLong("CPInstanceId");
+
+				CPInstance cpInstance = _cpInstanceLocalService.getCPInstance(
+					instanceId);
+
+				CPDefinition cpDefinition =
+					_cpDefinitionLocalService.getCPDefinition(
+						cpInstance.getCPDefinitionId());
+
+				ps.setLong(1, cpDefinition.getCProductId());
+
+				ps.setLong(2, instanceId);
+
+				ps.execute();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+			DataAccess.cleanUp(s, rs);
+		}
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private void _addIndexes(String tableName) throws Exception {
+		Class<?> clazz = getClass();
+
+		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
+			clazz.getClassLoader(), tableName);
+
+		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
+			IndexMetadata indexMetadata = indexSQL.getValue();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Adding index %s to table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+
+			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
+				runSQL(indexMetadata.getCreateSQL(null));
+			}
+			else if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already exists on table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+		}
+	}
+
+	private boolean _tableHasIndex(String tableName, String indexName)
+		throws Exception {
+
+		ResultSet rs = null;
+
+		try {
+			DatabaseMetaData metadata = connection.getMetaData();
+
+			rs = metadata.getIndexInfo(null, null, tableName, false, false);
+
+			while (rs.next()) {
+				String curIndexName = rs.getString("index_name");
+
+				if (Objects.equals(indexName, curIndexName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(rs);
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceOrderItemUpgradeProcess.class);
+
+	private CPDefinitionLocalService _cpDefinitionLocalService;
+	private CPInstanceLocalService _cpInstanceLocalService;
+
+}

zoltantoth0409/liferay-portal

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v2_1_0/CPDAvailabilityEstimateUpgradeProcess.java
@@ -1,0 +1,161 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.upgrade.v2_1_0;
+
+import com.liferay.commerce.model.impl.CPDAvailabilityEstimateModelImpl;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Alec Sloan
+ */
+public class CPDAvailabilityEstimateUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addColumn(
+			CPDAvailabilityEstimateModelImpl.class,
+			CPDAvailabilityEstimateModelImpl.TABLE_NAME, "CProductId", "LONG");
+
+		_addIndexes(CPDAvailabilityEstimateModelImpl.TABLE_NAME);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Statement s = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update CPDAvailabilityEstimate set CProductId = ? where " +
+					"CPDefinitionId = ?");
+
+			s = connection.createStatement();
+
+			rs = s.executeQuery("select * from CPDAvailabilityEstimate");
+
+			while (rs.next()) {
+				ps.setLong(1, rs.getLong("CPDefinitionId"));
+				ps.setLong(2, rs.getLong("CPDefinitionId"));
+
+				ps.execute();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+			DataAccess.cleanUp(s, rs);
+		}
+
+		runSQL("drop index IX_86A2368F on CPDAvailabilityEstimate");
+
+		runSQL(
+			"alter table CPDAvailabilityEstimate drop column CPDefinitionId");
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private void _addIndexes(String tableName) throws Exception {
+		Class<?> clazz = getClass();
+
+		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
+			clazz.getClassLoader(), tableName);
+
+		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
+			IndexMetadata indexMetadata = indexSQL.getValue();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Adding index %s to table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+
+			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
+				runSQL(indexMetadata.getCreateSQL(null));
+			}
+			else if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already exists on table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+		}
+	}
+
+	private boolean _tableHasIndex(String tableName, String indexName)
+		throws Exception {
+
+		ResultSet rs = null;
+
+		try {
+			DatabaseMetaData metadata = connection.getMetaData();
+
+			rs = metadata.getIndexInfo(null, null, tableName, false, false);
+
+			while (rs.next()) {
+				String curIndexName = rs.getString("index_name");
+
+				if (Objects.equals(indexName, curIndexName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(rs);
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDAvailabilityEstimateUpgradeProcess.class);
+
+}

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v2_1_0/CPDefinitionInventoryUpgradeProcess.java
@@ -1,0 +1,160 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.upgrade.v2_1_0;
+
+import com.liferay.commerce.model.impl.CPDefinitionInventoryModelImpl;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Alec Sloan
+ */
+public class CPDefinitionInventoryUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addColumn(
+			CPDefinitionInventoryModelImpl.class,
+			CPDefinitionInventoryModelImpl.TABLE_NAME, "CProductId", "LONG");
+
+		_addIndexes(CPDefinitionInventoryModelImpl.TABLE_NAME);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Statement s = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update CPDefinitionInventory set CProductId = ? where " +
+					"CPDefinitionId = ?");
+
+			s = connection.createStatement();
+
+			rs = s.executeQuery("select * from CPDefinitionInventory");
+
+			while (rs.next()) {
+				ps.setLong(1, rs.getLong("CPDefinitionId"));
+				ps.setLong(2, rs.getLong("CPDefinitionId"));
+
+				ps.execute();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+			DataAccess.cleanUp(s, rs);
+		}
+
+		runSQL("drop index IX_34D62DF1 on CPDefinitionInventory");
+
+		runSQL("alter table CPDefinitionInventory drop column CPDefinitionId");
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private void _addIndexes(String tableName) throws Exception {
+		Class<?> clazz = getClass();
+
+		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
+			clazz.getClassLoader(), tableName);
+
+		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
+			IndexMetadata indexMetadata = indexSQL.getValue();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Adding index %s to table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+
+			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
+				runSQL(indexMetadata.getCreateSQL(null));
+			}
+			else if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already exists on table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+		}
+	}
+
+	private boolean _tableHasIndex(String tableName, String indexName)
+		throws Exception {
+
+		ResultSet rs = null;
+
+		try {
+			DatabaseMetaData metadata = connection.getMetaData();
+
+			rs = metadata.getIndexInfo(null, null, tableName, false, false);
+
+			while (rs.next()) {
+				String curIndexName = rs.getString("index_name");
+
+				if (Objects.equals(indexName, curIndexName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(rs);
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDefinitionInventoryUpgradeProcess.class);
+
+}

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v2_1_0/CommerceOrderItemUpgradeProcess.java
@@ -1,0 +1,181 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.upgrade.v2_1_0;
+
+import com.liferay.commerce.model.impl.CommerceOrderItemModelImpl;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CPInstanceLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Alec Sloan
+ */
+public class CommerceOrderItemUpgradeProcess extends UpgradeProcess {
+
+	public CommerceOrderItemUpgradeProcess(
+		CPDefinitionLocalService cpDefinitionLocalService,
+		CPInstanceLocalService cpInstanceLocalService) {
+
+		_cpDefinitionLocalService = cpDefinitionLocalService;
+		_cpInstanceLocalService = cpInstanceLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addColumn(
+			CommerceOrderItemModelImpl.class,
+			CommerceOrderItemModelImpl.TABLE_NAME, "CProductId", "LONG");
+
+		_addIndexes(CommerceOrderItemModelImpl.TABLE_NAME);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Statement s = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update CommerceOrderItem set CProductId = ? where " +
+					"CPInstanceId = ?");
+
+			s = connection.createStatement();
+
+			rs = s.executeQuery("select * from CommerceOrderItem");
+
+			while (rs.next()) {
+				long instanceId = rs.getLong("CPInstanceId");
+
+				CPInstance cpInstance = _cpInstanceLocalService.getCPInstance(
+					instanceId);
+
+				CPDefinition cpDefinition =
+					_cpDefinitionLocalService.getCPDefinition(
+						cpInstance.getCPDefinitionId());
+
+				ps.setLong(1, cpDefinition.getCProductId());
+
+				ps.setLong(2, instanceId);
+
+				ps.execute();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+			DataAccess.cleanUp(s, rs);
+		}
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private void _addIndexes(String tableName) throws Exception {
+		Class<?> clazz = getClass();
+
+		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
+			clazz.getClassLoader(), tableName);
+
+		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
+			IndexMetadata indexMetadata = indexSQL.getValue();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Adding index %s to table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+
+			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
+				runSQL(indexMetadata.getCreateSQL(null));
+			}
+			else if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already exists on table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+		}
+	}
+
+	private boolean _tableHasIndex(String tableName, String indexName)
+		throws Exception {
+
+		ResultSet rs = null;
+
+		try {
+			DatabaseMetaData metadata = connection.getMetaData();
+
+			rs = metadata.getIndexInfo(null, null, tableName, false, false);
+
+			while (rs.next()) {
+				String curIndexName = rs.getString("index_name");
+
+				if (Objects.equals(indexName, curIndexName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(rs);
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceOrderItemUpgradeProcess.class);
+
+	private final CPDefinitionLocalService _cpDefinitionLocalService;
+	private final CPInstanceLocalService _cpInstanceLocalService;
+
+}