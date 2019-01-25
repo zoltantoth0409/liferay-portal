ed83015f3d249b67da0b78715a5ac45bc3722caf
Matthew Tambara <matthew.tambara@liferay.com>
2019-01-24 16:24:59 -0800

    LPS-89565 Inline JMXDeployableContainer

--- modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/container/remote/LiferayRemoteDeployableContainer.java
+++ modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/container/remote/LiferayRemoteDeployableContainer.java
@@ -14,36 +14,84 @@
 
 package com.liferay.arquillian.extension.junit.bridge.container.remote;
 
+import java.io.IOException;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.URL;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import javax.management.MBeanServerConnection;
+import javax.management.MBeanServerInvocationHandler;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.TabularData;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
 
-import org.jboss.arquillian.container.osgi.jmx.JMXDeployableContainer;
+import org.jboss.arquillian.container.osgi.jmx.http.SimpleHTTPServer;
+import org.jboss.arquillian.container.spi.client.container.DeployableContainer;
 import org.jboss.arquillian.container.spi.client.container.DeploymentException;
 import org.jboss.arquillian.container.spi.client.container.LifecycleException;
+import org.jboss.arquillian.container.spi.client.protocol.ProtocolDescription;
 import org.jboss.arquillian.container.spi.client.protocol.metadata.HTTPContext;
+import org.jboss.arquillian.container.spi.client.protocol.metadata.JMXContext;
 import org.jboss.arquillian.container.spi.client.protocol.metadata.ProtocolMetaData;
+import org.jboss.arquillian.container.spi.context.annotation.ContainerScoped;
+import org.jboss.arquillian.core.api.InstanceProducer;
+import org.jboss.arquillian.core.api.annotation.Inject;
+import org.jboss.osgi.spi.BundleInfo;
+import org.jboss.osgi.vfs.AbstractVFS;
+import org.jboss.osgi.vfs.VFSUtils;
+import org.jboss.osgi.vfs.VirtualFile;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.descriptor.api.Descriptor;
 
+import org.osgi.framework.BundleException;
 import org.osgi.jmx.framework.BundleStateMBean;
 import org.osgi.jmx.framework.FrameworkMBean;
-import org.osgi.jmx.framework.ServiceStateMBean;
 
 /**
  * @author Preston Crary
  */
 public class LiferayRemoteDeployableContainer
-	<T extends LiferayRemoteContainerConfiguration>
-		extends JMXDeployableContainer<T> {
+	implements DeployableContainer<LiferayRemoteContainerConfiguration> {
 
 	@Override
 	public ProtocolMetaData deploy(Archive<?> archive)
 		throws DeploymentException {
 
-		ProtocolMetaData protocolMetaData = super.deploy(archive);
+		try {
+			BundleHandle bundleHandle = _installBundle(archive);
+
+			_deployedBundles.put(archive.getName(), bundleHandle);
+		}
+		catch (Exception e) {
+			throw new DeploymentException(
+				"Cannot deploy: " + archive.getName(), e);
+		}
+
+		MBeanServerConnection mBeanServerConnection =
+			_mbeanServerConnectionInstance.get();
+
+		ProtocolMetaData protocolMetaData = new ProtocolMetaData();
+
+		protocolMetaData.addContext(new JMXContext(mBeanServerConnection));
 
 		protocolMetaData.addContext(
 			new HTTPContext(
@@ -54,46 +102,333 @@ public class LiferayRemoteDeployableContainer
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
-	public Class<T> getConfigurationClass() {
-		return (Class<T>)LiferayRemoteContainerConfiguration.class;
+	public void deploy(Descriptor descriptor) throws DeploymentException {
 	}
 
 	@Override
-	public void setup(T configuration) {
-		_liferayRemoteContainerConfiguration = configuration;
+	public Class<LiferayRemoteContainerConfiguration> getConfigurationClass() {
+		return LiferayRemoteContainerConfiguration.class;
+	}
 
-		super.setup(configuration);
+	@Override
+	public ProtocolDescription getDefaultProtocol() {
+		return new ProtocolDescription("jmx-osgi");
+	}
+
+	@Override
+	public void setup(LiferayRemoteContainerConfiguration configuration) {
+		_liferayRemoteContainerConfiguration = configuration;
 	}
 
 	@Override
 	public void start() throws LifecycleException {
 		try {
-			MBeanServerConnection mBeanServer = getMBeanServerConnection(
+			MBeanServerConnection mBeanServer = _getMBeanServerConnection(
 				30, TimeUnit.SECONDS);
 
-			mbeanServerInstance.set(mBeanServer);
+			_mbeanServerConnectionInstance.set(mBeanServer);
 
-			frameworkMBean = getMBeanProxy(
+			_frameworkMBean = _getMBeanProxy(
 				mBeanServer, _frameworkObjectName, FrameworkMBean.class, 30,
 				TimeUnit.SECONDS);
 
-			bundleStateMBean = getMBeanProxy(
+			_bundleStateMBean = _getMBeanProxy(
 				mBeanServer, _bundleStateObjectName, BundleStateMBean.class, 30,
 				TimeUnit.SECONDS);
-
-			serviceStateMBean = getMBeanProxy(
-				mBeanServer, _serviceStateObjectName, ServiceStateMBean.class,
-				30, TimeUnit.SECONDS);
 		}
 		catch (TimeoutException te) {
 			throw new LifecycleException("JMX timeout", te);
 		}
 	}
 
+	public void startBundle(String symbolicName, String version)
+		throws Exception {
+
+		BundleHandle bundleHandle = _getBundle(symbolicName, version);
+
+		if (bundleHandle == null) {
+			throw new IllegalStateException(
+				"Bundle '" + symbolicName + ":" + version + "' was not found");
+		}
+
+		_frameworkMBean.startBundle(bundleHandle.getBundleId());
+	}
+
+	@Override
+	public void stop() throws LifecycleException {
+	}
+
+	@Override
+	public void undeploy(Archive<?> archive) throws DeploymentException {
+		BundleHandle bundleHandle = _deployedBundles.remove(archive.getName());
+
+		if (bundleHandle == null) {
+			return;
+		}
+
+		String bundleState = null;
+
+		try {
+			long bundleId = bundleHandle.getBundleId();
+
+			CompositeData compositeData = _bundleStateMBean.getBundle(bundleId);
+
+			if (compositeData != null) {
+				bundleState = (String)compositeData.get(BundleStateMBean.STATE);
+			}
+		}
+		catch (IOException ioe) {
+			return;
+		}
+
+		if ((bundleState != null) &&
+			!bundleState.equals(BundleStateMBean.UNINSTALLED)) {
+
+			try {
+				long bundleId = bundleHandle.getBundleId();
+
+				_frameworkMBean.uninstallBundle(bundleId);
+			}
+			catch (IOException ioe) {
+			}
+		}
+	}
+
+	@Override
+	public void undeploy(Descriptor desc) throws DeploymentException {
+	}
+
+	private BundleHandle _getBundle(String symbolicName, String version)
+		throws Exception {
+
+		TabularData listBundles = _bundleStateMBean.listBundles();
+
+		Collection values = listBundles.values();
+
+		Iterator<?> iterator = values.iterator();
+
+		while (iterator.hasNext()) {
+			CompositeData bundleType = (CompositeData)iterator.next();
+
+			String mBeanSymbolicName = (String)bundleType.get(
+				BundleStateMBean.SYMBOLIC_NAME);
+
+			String mBeanVersion = (String)bundleType.get(
+				BundleStateMBean.VERSION);
+
+			if (symbolicName.equals(mBeanSymbolicName) &&
+				version.equals(mBeanVersion)) {
+
+				return new BundleHandle(
+					(Long)bundleType.get(BundleStateMBean.IDENTIFIER),
+					symbolicName);
+			}
+		}
+
+		return null;
+	}
+
+	private <T> T _getMBeanProxy(
+			MBeanServerConnection mbeanServer, ObjectName objectName,
+			Class<T> type, final long timeout, final TimeUnit unit)
+		throws TimeoutException {
+
+		Callable<T> callable = new Callable<T>() {
+
+			@Override
+			public T call() throws Exception {
+				long timeoutMillis =
+					System.currentTimeMillis() + unit.toMillis(timeout);
+
+				while (System.currentTimeMillis() < timeoutMillis) {
+					Set<ObjectName> names = mbeanServer.queryNames(
+						objectName, null);
+
+					if (names.size() == 1) {
+						ObjectName instanceName = names.iterator().next();
+
+						return MBeanServerInvocationHandler.newProxyInstance(
+							mbeanServer, instanceName, type, false);
+					}
+
+					Thread.sleep(500);
+				}
+
+				throw new TimeoutException();
+			}
+
+		};
+
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+
+		Future<T> future = executor.submit(callable);
+
+		try {
+			return future.get(timeout, unit);
+		}
+		catch (TimeoutException te) {
+			throw te;
+		}
+		catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	private MBeanServerConnection _getMBeanServerConnection()
+		throws IOException {
+
+		String[] credentials = {
+			_liferayRemoteContainerConfiguration.getJmxUsername(),
+			_liferayRemoteContainerConfiguration.getJmxPassword()
+		};
+
+		Map<String, String[]> env = Collections.singletonMap(
+			JMXConnector.CREDENTIALS, credentials);
+
+		JMXServiceURL jmxServiceURL = new JMXServiceURL(
+			_liferayRemoteContainerConfiguration.getJmxServiceURL());
+
+		JMXConnector connector = JMXConnectorFactory.connect(
+			jmxServiceURL, env);
+
+		return connector.getMBeanServerConnection();
+	}
+
+	private MBeanServerConnection _getMBeanServerConnection(
+			long timeout, TimeUnit unit)
+		throws TimeoutException {
+
+		Callable<MBeanServerConnection> callable =
+			new Callable<MBeanServerConnection>() {
+
+				@Override
+				public MBeanServerConnection call() throws Exception {
+					Exception lastException = null;
+					long timeoutMillis =
+						System.currentTimeMillis() + unit.toMillis(timeout);
+
+					while (System.currentTimeMillis() < timeoutMillis) {
+						try {
+							return _getMBeanServerConnection();
+						}
+						catch (Exception e) {
+							lastException = e;
+							Thread.sleep(500);
+						}
+					}
+
+					TimeoutException timeoutException = new TimeoutException();
+
+					timeoutException.initCause(lastException);
+
+					throw timeoutException;
+				}
+
+			};
+
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+
+		Future<MBeanServerConnection> future = executor.submit(callable);
+
+		try {
+			return future.get(timeout, unit);
+		}
+		catch (TimeoutException te) {
+			throw te;
+		}
+		catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	private BundleHandle _installBundle(Archive<?> archive) throws Exception {
+		VirtualFile virtualFile = _toVirtualFile(archive);
+
+		try {
+			return _installBundle(archive.getName(), virtualFile);
+		}
+		finally {
+			VFSUtils.safeClose(virtualFile);
+		}
+	}
+
+	private BundleHandle _installBundle(String location, URL streamURL)
+		throws BundleException, IOException {
+
+		URL serverUrl = streamURL;
+
+		SimpleHTTPServer server = null;
+
+		if (!_isLocalHost(_liferayRemoteContainerConfiguration)) {
+			server = new SimpleHTTPServer();
+
+			serverUrl = server.serve(streamURL);
+
+			server.start();
+		}
+
+		try {
+			long bundleId = _frameworkMBean.installBundleFromURL(
+				location, serverUrl.toExternalForm());
+
+			String symbolicName = _bundleStateMBean.getSymbolicName(bundleId);
+
+			return new BundleHandle(bundleId, symbolicName);
+		}
+		finally {
+			if (server != null) {
+				server.shutdown();
+			}
+		}
+	}
+
+	private BundleHandle _installBundle(
+			String location, VirtualFile virtualFile)
+		throws Exception {
+
+		BundleInfo info = BundleInfo.createBundleInfo(virtualFile);
+
+		VirtualFile root = info.getRoot();
+
+		URL streamURL = root.getStreamURL();
+
+		return _installBundle(location, streamURL);
+	}
+
+	private boolean _isLocalHost(
+		LiferayRemoteContainerConfiguration
+			liferayRemoteContainerConfiguration) {
+
+		try {
+			JMXServiceURL serviceURL = new JMXServiceURL(
+				liferayRemoteContainerConfiguration.getJmxServiceURL());
+
+			InetAddress addr = InetAddress.getByName(serviceURL.getHost());
+
+			if (addr.isAnyLocalAddress() || addr.isLoopbackAddress()) {
+				return true;
+			}
+
+			if (NetworkInterface.getByInetAddress(addr) != null) {
+				return true;
+			}
+
+			return false;
+		}
+		catch (IOException ioe) {
+			return false;
+		}
+	}
+
+	private VirtualFile _toVirtualFile(Archive<?> archive) throws IOException {
+		ZipExporter exporter = archive.as(ZipExporter.class);
+
+		return AbstractVFS.toVirtualFile(
+			archive.getName(), exporter.exportAsInputStream());
+	}
+
 	private static final ObjectName _bundleStateObjectName;
 	private static final ObjectName _frameworkObjectName;
-	private static final ObjectName _serviceStateObjectName;
 
 	static {
 		try {
@@ -101,16 +436,46 @@ public class LiferayRemoteDeployableContainer
 				"osgi.core:type=bundleState,*");
 
 			_frameworkObjectName = new ObjectName("osgi.core:type=framework,*");
-
-			_serviceStateObjectName = new ObjectName(
-				"osgi.core:type=serviceState,*");
 		}
 		catch (MalformedObjectNameException mone) {
 			throw new ExceptionInInitializerError(mone);
 		}
 	}
 
+	private BundleStateMBean _bundleStateMBean;
+	private final Map<String, BundleHandle> _deployedBundles = new HashMap<>();
+	private FrameworkMBean _frameworkMBean;
 	private LiferayRemoteContainerConfiguration
 		_liferayRemoteContainerConfiguration;
+
+	@ContainerScoped
+	@Inject
+	private InstanceProducer<MBeanServerConnection>
+		_mbeanServerConnectionInstance;
+
+	private class BundleHandle {
+
+		public BundleHandle(long bundleId, String symbolicName) {
+			_bundleId = bundleId;
+			_symbolicName = symbolicName;
+		}
+
+		public long getBundleId() {
+			return _bundleId;
+		}
+
+		public String getSymbolicName() {
+			return _symbolicName;
+		}
+
+		@Override
+		public String toString() {
+			return "[" + _bundleId + "]" + _symbolicName;
+		}
+
+		private final long _bundleId;
+		private final String _symbolicName;
+
+	}
 
 }
