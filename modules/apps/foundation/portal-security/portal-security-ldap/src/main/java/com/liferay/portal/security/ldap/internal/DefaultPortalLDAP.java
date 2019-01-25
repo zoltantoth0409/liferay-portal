d89ebc31beafd78996a34c05029ef1f93977eab7
Joshua Cords <joshua.cords@liferay.com>
2019-01-25 09:17:26 -0800

    LPS-89295 Remove dependence on the default LDAPSettings service (DefaultLDAPSettings) to trim whitespace values (different to empty string)

--- modules/apps/foundation/portal-security/portal-security-ldap/src/main/java/com/liferay/portal/security/ldap/internal/DefaultPortalLDAP.java
+++ modules/apps/foundation/portal-security/portal-security-ldap/src/main/java/com/liferay/portal/security/ldap/internal/DefaultPortalLDAP.java
@@ -39,7 +39,6 @@ import com.liferay.portal.security.ldap.configuration.SystemLDAPConfiguration;
 import com.liferay.portal.security.ldap.validator.LDAPFilterValidator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
@@ -677,7 +676,7 @@ public class DefaultPortalLDAP implements PortalLDAP {
 
 		Collection<Object> values = userMappings.values();
 
-		values.removeAll(Arrays.asList(""));
+		values.removeIf(o -> Validator.isNull(o));
 
 		String[] mappedUserAttributeIds = ArrayUtil.toStringArray(
 			values.toArray(new Object[userMappings.size()]));
