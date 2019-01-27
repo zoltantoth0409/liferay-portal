49c74c724d9bc477a9f3ba149c5550decf3b604f
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-01-27 15:48:41 +0100

    LPS-89423 Implement customizer for language field

---
+++ modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/field/customizer/LanguageSegmentsFieldCustomizer.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.web.internal.field.customizer;
+
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.segments.field.Field;
+import com.liferay.segments.field.customizer.SegmentsFieldCustomizer;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eduardo García
+ */
+@Component(
+	immediate = true,
+	property = {
+		"segments.field.customizer.entity.name=Context",
+		"segments.field.customizer.key=" + LanguageSegmentsFieldCustomizer.KEY,
+		"segments.field.customizer.priority:Integer=50"
+	},
+	service = SegmentsFieldCustomizer.class
+)
+public class LanguageSegmentsFieldCustomizer
+	implements SegmentsFieldCustomizer {
+
+	public static final String KEY = "language";
+
+	@Override
+	public List<String> getFieldNames() {
+		return _fieldNames;
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public List<Field.Option> getOptions(Locale locale) {
+		Set<Locale> availableLocales = LanguageUtil.getAvailableLocales();
+
+		Stream<Locale> stream = availableLocales.stream();
+
+		return stream.map(
+			availableLocale -> new Field.Option(
+				availableLocale.getDisplayLanguage(locale),
+				LanguageUtil.getLanguageId(availableLocale))
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	private static final List<String> _fieldNames = ListUtil.fromArray(
+		new String[] {"languageId"});
+
+}
