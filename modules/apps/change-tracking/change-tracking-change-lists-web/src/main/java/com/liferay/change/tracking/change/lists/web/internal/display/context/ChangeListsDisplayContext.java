ab4b9ed5fbd1db7e585350dc2256be152b1d3cf5
Roland Pákai <roland.pakai@liferay.com>
2019-01-28 12:03:11 +0100

    LPS-89340 Remove translations from soy and from java class and use msg

--- modules/apps/change-tracking/change-tracking-change-lists-web/src/main/java/com/liferay/change/tracking/change/lists/web/internal/display/context/ChangeListsDisplayContext.java
+++ modules/apps/change-tracking/change-tracking-change-lists-web/src/main/java/com/liferay/change/tracking/change/lists/web/internal/display/context/ChangeListsDisplayContext.java
@@ -60,27 +60,6 @@ public class ChangeListsDisplayContext {
 			"spritemap",
 			_themeDisplay.getPathThemeImages() + "/lexicon/icons.svg");
 
-		// Translations
-
-		Map<String, String> translations = new HashMap<>();
-
-		translations.put(
-			"description",
-			LanguageUtil.get(_httpServletRequest, "description"));
-		translations.put(
-			"production-view",
-			LanguageUtil.get(_httpServletRequest, "production-view"));
-		translations.put(
-			"published-by",
-			LanguageUtil.get(_httpServletRequest, "published-by"));
-		translations.put(
-			"published-change-list",
-			LanguageUtil.get(_httpServletRequest, "published-change-list"));
-
-		soyContext.put("translations", translations);
-
-		// URLs
-
 		soyContext.put(
 			"urlProductionCollection",
 			_themeDisplay.getPortalURL() +
