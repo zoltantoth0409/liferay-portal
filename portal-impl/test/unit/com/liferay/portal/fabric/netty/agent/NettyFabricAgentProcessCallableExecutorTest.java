d9d0f9de1a6154b41da2af0357846e24f06ccbbf
shuyangzhou <shuyang.zhou@liferay.com>
2019-01-28 14:56:35 -0800

    LPS-88841 Better API usage

--- portal-impl/test/unit/com/liferay/portal/fabric/netty/agent/NettyFabricAgentProcessCallableExecutorTest.java
+++ portal-impl/test/unit/com/liferay/portal/fabric/netty/agent/NettyFabricAgentProcessCallableExecutorTest.java
@@ -67,8 +67,8 @@ public class NettyFabricAgentProcessCallableExecutorTest {
 
 				});
 
-		embeddedChannel.writeInbound((Object)(embeddedChannel.readOutbound()));
-		embeddedChannel.writeInbound((Object)(embeddedChannel.readOutbound()));
+		embeddedChannel.writeOneInbound(embeddedChannel.readOutbound());
+		embeddedChannel.writeOneInbound(embeddedChannel.readOutbound());
 
 		Assert.assertEquals(StringPool.BLANK, noticeableFuture.get());
 
@@ -84,8 +84,8 @@ public class NettyFabricAgentProcessCallableExecutorTest {
 
 			});
 
-		embeddedChannel.writeInbound((Object)(embeddedChannel.readOutbound()));
-		embeddedChannel.writeInbound((Object)(embeddedChannel.readOutbound()));
+		embeddedChannel.writeOneInbound(embeddedChannel.readOutbound());
+		embeddedChannel.writeOneInbound(embeddedChannel.readOutbound());
 
 		try {
 			noticeableFuture.get();
--- portal-impl/test/unit/com/liferay/portal/fabric/netty/worker/NettyFabricWorkerStubTest.java
+++ portal-impl/test/unit/com/liferay/portal/fabric/netty/worker/NettyFabricWorkerStubTest.java
@@ -324,8 +324,8 @@ public class NettyFabricWorkerStubTest {
 		NoticeableFuture<String> noticeableFuture = nettyFabricWorkerStub.write(
 			new ReturnProcessCallable<String>(result));
 
-		embeddedChannel.writeInbound((Object)(embeddedChannel.readOutbound()));
-		embeddedChannel.writeInbound((Object)(embeddedChannel.readOutbound()));
+		embeddedChannel.writeOneInbound(embeddedChannel.readOutbound());
+		embeddedChannel.writeOneInbound(embeddedChannel.readOutbound());
 
 		Assert.assertEquals(result, noticeableFuture.get());
 	}
