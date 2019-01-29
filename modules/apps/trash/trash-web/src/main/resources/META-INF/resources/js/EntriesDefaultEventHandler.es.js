b2b666e520ea91f5b05fed62442224413044b53b
Pavel Savinov <pavel.savinov@liferay.com>
2019-01-29 10:41:44 +0100

    LPS-89572 Select restore target container in the pop-up

--- modules/apps/trash/trash-web/src/main/resources/META-INF/resources/js/EntriesDefaultEventHandler.es.js
+++ modules/apps/trash/trash-web/src/main/resources/META-INF/resources/js/EntriesDefaultEventHandler.es.js
@@ -11,8 +11,54 @@ class EntriesDefaultEventHandler extends PortletBase {
 	}
 
 	restoreEntry(itemData) {
-		if (itemData.move) {
-			Liferay.Util.navigate(itemData.restoreURL);
+		const instance = this;
+
+		if (itemData.move && itemData.move !== 'false') {
+			Liferay.Util.selectEntity(
+				{
+					dialog: {
+						constrain: true,
+						destroyOnHide: true,
+						modal: true,
+						width: 1024
+					},
+					eventName: this.ns('selectContainer'),
+					id: this.ns('selectContainer'),
+					title: Liferay.Language.get('warning'),
+					uri: itemData.restoreURL
+				},
+				function(event) {
+					const selectContainerForm = document.getElementById(`${instance.namespace}selectContainerForm`);
+
+					if (selectContainerForm) {
+						const className = selectContainerForm.querySelector(`#${instance.namespace}className`);
+
+						if (className) {
+							className.setAttribute('value', event.classname);
+						}
+
+						const classPK = selectContainerForm.querySelector(`#${instance.namespace}classPK`);
+
+						if (classPK) {
+							classPK.setAttribute('value', event.classpk);
+						}
+
+						const containerModelId = selectContainerForm.querySelector(`#${instance.namespace}containerModelId`);
+
+						if (containerModelId) {
+							containerModelId.setAttribute('value', event.containermodelid);
+						}
+
+						const redirect = selectContainerForm.querySelector(`#${instance.namespace}redirect`);
+
+						if (redirect) {
+							redirect.setAttribute('value', event.redirect);
+						}
+
+						submitForm(selectContainerForm);
+					}
+				}
+			);
 		}
 		else {
 			submitForm(this.one('#fm'), itemData.restoreURL);
