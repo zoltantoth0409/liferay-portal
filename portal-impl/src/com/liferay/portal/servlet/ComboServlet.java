b189c493229083c08483d2c5d3d8d94015c8897f
Norbert Nemeth <31600235+NemethNorbert@users.noreply.github.com>
2019-01-29 16:25:07 +0100

    LPS-89615 Update ComboServlet with .min.js and .min.css suffix

--- portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
@@ -334,8 +334,11 @@ public class ComboServlet extends HttpServlet {
 			String stringFileContent = objectValuePair.getKey();
 
 			if (!StringUtil.endsWith(resourcePath, _CSS_MINIFIED_SUFFIX) &&
+				!StringUtil.endsWith(resourcePath, _CSS_MINIFIED_DOT_SUFFIX) &&
 				!StringUtil.endsWith(
-					resourcePath, _JAVASCRIPT_MINIFIED_SUFFIX)) {
+					resourcePath, _JAVASCRIPT_MINIFIED_SUFFIX) &&
+				!StringUtil.endsWith(
+					resourcePath, _JAVASCRIPT_MINIFIED_DOT_SUFFIX)) {
 
 				if (minifierType.equals("css")) {
 					try {
@@ -472,10 +475,14 @@ public class ComboServlet extends HttpServlet {
 
 	private static final String _CSS_EXTENSION = "css";
 
+	private static final String _CSS_MINIFIED_DOT_SUFFIX = ".min.css";
+
 	private static final String _CSS_MINIFIED_SUFFIX = "-min.css";
 
 	private static final FileContentBag _EMPTY_FILE_CONTENT_BAG =
 		new FileContentBag(new byte[0], 0);
+
+	private static final String _JAVASCRIPT_MINIFIED_DOT_SUFFIX = ".min.js";
 
 	private static final String _JAVASCRIPT_MINIFIED_SUFFIX = "-min.js";
 
