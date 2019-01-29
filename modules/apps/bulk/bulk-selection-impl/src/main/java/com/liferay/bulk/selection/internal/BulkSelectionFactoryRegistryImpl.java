760fe8889f5cb0d71632b5a739993c1ef07f4d14
Adolfo Pérez <adolfo.perez@liferay.com>
2019-01-29 15:36:13 +0100

    LPS-88397 Keep track of factories by class name

---
+++ modules/apps/bulk/bulk-selection-impl/src/main/java/com/liferay/bulk/selection/internal/BulkSelectionFactoryRegistryImpl.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bulk.selection.internal;
+
+import com.liferay.bulk.selection.BulkSelectionFactory;
+import com.liferay.bulk.selection.BulkSelectionFactoryRegistry;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(immediate = true, service = BulkSelectionFactoryRegistry.class)
+public class BulkSelectionFactoryRegistryImpl
+	implements BulkSelectionFactoryRegistry {
+
+	@Override
+	public <T> BulkSelectionFactory<T> getBulkSelectionFactory(long classNameId)
+		throws PortalException {
+
+		ClassName className = _classNameLocalService.getClassName(classNameId);
+
+		return getBulkSelectionFactory(className.getClassName());
+	}
+
+	@Override
+	public <T> BulkSelectionFactory<T> getBulkSelectionFactory(
+		String className) {
+
+		return _serviceTrackerMap.getService(className);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, BulkSelectionFactory.class, "model.class.name");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	private ServiceTrackerMap<String, BulkSelectionFactory> _serviceTrackerMap;
+
+}
