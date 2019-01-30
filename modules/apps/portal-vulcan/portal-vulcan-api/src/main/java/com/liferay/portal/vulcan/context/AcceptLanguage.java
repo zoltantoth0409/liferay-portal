c4462e525f081706a1041ee924401ba9468044fb
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-01-30 17:40:58 +0100

    LPS-89901 New AcceptLanguage object

---
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/context/AcceptLanguage.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.context;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Represents information about the requested languages.
+ *
+ * @author Alejandro Hernández
+ * @review
+ */
+public interface AcceptLanguage {
+
+	/**
+	 * Returns the {@code List} of the request's preferred {@code Locale}, in
+	 * decreasing order.
+	 *
+	 * <p>
+	 * The list values start with the first locale added on the {@code
+	 * Accept-Language} header and continue with the rest of the header.
+	 * </p>
+	 *
+	 * <p>
+	 * If the request doesn't have an {@code Accept-Language} header, this
+	 * method returns an {@code Stream} containing the default locale for the
+	 * server.
+	 * </p>
+	 *
+	 * @return a {@code List} of the request's preferred {@code Locale}, if
+	 *         the {@code Accept-Language} header is present; otherwise returns
+	 *         a {@code List} containing the server's default locale
+	 * @review
+	 */
+	public default List<Locale> getLocales() {
+		return Collections.singletonList(getPreferredLocale());
+	}
+
+	/**
+	 * Returns the first {@code Locale} added on the {@code Accept-Language}
+	 * header. If the request doesn't have an {@code Accept-Language} header,
+	 * this method returns the server's default locale.
+	 *
+	 * @return the request's first {@code Locale}, if the {@code
+	 *         Accept-Language} header is present; otherwise returns the {@code
+	 *         Enumeration} containing the default locale for the server
+	 */
+	public Locale getPreferredLocale();
+
+}
