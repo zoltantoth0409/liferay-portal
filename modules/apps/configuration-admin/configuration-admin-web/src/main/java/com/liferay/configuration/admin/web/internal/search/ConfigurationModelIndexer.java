7022a8f4e84293685b4fc54646c183bc395742ab
drewbrokke <drew.brokke@liferay.com>
2019-01-30 13:39:01 -0800

    Revert "LPS-88275 Clear index before reindexing, since LDAP configurations append UID to configuration name and there is no way of retrieving them after they are removed from the portal"
    This reverts commit da29b2c70dda6d36ebe6c703c4b72d3ee76be27d.

--- modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/search/ConfigurationModelIndexer.java
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/search/ConfigurationModelIndexer.java
@@ -287,10 +287,6 @@ public class ConfigurationModelIndexer extends BaseIndexer<ConfigurationModel> {
 		Map<String, ConfigurationModel> configurationModels =
 			_configurationModelRetriever.getConfigurationModels();
 
-		_indexWriterHelper.deleteEntityDocuments(
-			getSearchEngineId(), CompanyConstants.SYSTEM, getClassName(),
-			isCommitImmediately());
-
 		for (ConfigurationModel configurationModel :
 				configurationModels.values()) {
 
