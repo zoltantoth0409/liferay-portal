e1014fb14c7414431b95860f01ce68a09f74b9b5
Brian Chan <brian.chan@liferay.com>
2019-01-29 16:22:45 -0800

    Revert "LPS-89289 Cannot share a field with the old Log4JUtil since the compat has been removed."
    This reverts commit 8a1d6007dee023d5fbdeb85e96623ca1106ffb26.

--- modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/Log4JUtil.java
+++ modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/Log4JUtil.java
@@ -14,12 +14,14 @@
 
 package com.liferay.petra.log4j;
 
+import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.log.LogFactory;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.ServerDetector;
@@ -29,13 +31,14 @@ import com.liferay.portal.kernel.util.StringUtil;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.lang.reflect.Field;
+
 import java.net.URL;
 
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
@@ -143,7 +146,7 @@ public class Log4JUtil {
 	}
 
 	public static Map<String, String> getCustomLogSettings() {
-		return new HashMap<>(_customLogSettings);
+		return new HashMap<>(_getCustomLogSettings());
 	}
 
 	public static String getOriginalLevel(String className) {
@@ -199,7 +202,9 @@ public class Log4JUtil {
 		jdkLogger.setLevel(_getJdkLevel(priority));
 
 		if (custom) {
-			_customLogSettings.put(name, priority);
+			Map<String, String> customLogSettings = _getCustomLogSettings();
+
+			customLogSettings.put(name, priority);
 		}
 	}
 
@@ -225,6 +230,23 @@ public class Log4JUtil {
 		StreamUtil.transfer(inputStream, unsyncByteArrayOutputStream, -1, true);
 
 		return unsyncByteArrayOutputStream.toByteArray();
+	}
+
+	private static Map<String, String> _getCustomLogSettings() {
+		ClassLoader classLoader = PortalClassLoaderUtil.getClassLoader();
+
+		try {
+			Class<?> clazz = classLoader.loadClass(
+				"com.liferay.util.log4j.Log4JUtil");
+
+			Field field = ReflectionUtil.getDeclaredField(
+				clazz, "_customLogSettings");
+
+			return (Map<String, String>)field.get(null);
+		}
+		catch (Exception e) {
+			return ReflectionUtil.throwException(e);
+		}
 	}
 
 	private static java.util.logging.Level _getJdkLevel(String priority) {
@@ -312,8 +334,6 @@ public class Log4JUtil {
 
 	private static final Logger _logger = Logger.getRootLogger();
 
-	private static final Map<String, String> _customLogSettings =
-		new ConcurrentHashMap<>();
 	private static String _liferayHome;
 
 }
