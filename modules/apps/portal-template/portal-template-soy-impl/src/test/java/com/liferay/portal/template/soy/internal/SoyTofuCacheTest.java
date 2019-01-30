e8782e8b7a843eb4174a957df2f590b28fa3578b
Lance Ji <zheyi.ji@liferay.com>
2019-01-29 17:23:01 -0800

    LPS-89879 Inline

--- modules/apps/portal-template/portal-template-soy-impl/src/test/java/com/liferay/portal/template/soy/internal/SoyTofuCacheTest.java
+++ modules/apps/portal-template/portal-template-soy-impl/src/test/java/com/liferay/portal/template/soy/internal/SoyTofuCacheTest.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.template.soy.internal;
 
-import com.google.template.soy.SoyFileSet;
 import com.google.template.soy.tofu.SoyTofu;
 
 import com.liferay.portal.kernel.template.TemplateResource;
@@ -52,11 +51,9 @@ public class SoyTofuCacheTest {
 			_soyTestHelper.getTemplateResources(
 				Arrays.asList("simple.soy", "context.soy"));
 
-		SoyFileSet soyFileSet = _soyTestHelper.getSoyFileSet(templateResources);
-
-		SoyTofu soyTofu = ProxyFactory.newDummyInstance(SoyTofu.class);
-
-		_soyTofuCacheHandler.add(templateResources, soyFileSet, soyTofu);
+		_soyTofuCacheHandler.add(
+			templateResources, _soyTestHelper.getSoyFileSet(templateResources),
+			ProxyFactory.newDummyInstance(SoyTofu.class));
 
 		Assert.assertNotNull(_soyTofuCacheHandler.get(templateResources));
 	}
@@ -67,16 +64,14 @@ public class SoyTofuCacheTest {
 			_soyTestHelper.getTemplateResources(
 				Arrays.asList("simple.soy", "context.soy"));
 
-		SoyFileSet soyFileSet = _soyTestHelper.getSoyFileSet(templateResources);
+		_soyTofuCacheHandler.add(
+			templateResources, _soyTestHelper.getSoyFileSet(templateResources),
+			ProxyFactory.newDummyInstance(SoyTofu.class));
 
-		SoyTofu soyTofu = ProxyFactory.newDummyInstance(SoyTofu.class);
-
-		_soyTofuCacheHandler.add(templateResources, soyFileSet, soyTofu);
-
-		List<TemplateResource> templateResourcesA =
-			_soyTestHelper.getTemplateResources(Arrays.asList("context.soy"));
-
-		Assert.assertNull(_soyTofuCacheHandler.get(templateResourcesA));
+		Assert.assertNull(
+			_soyTofuCacheHandler.get(
+				_soyTestHelper.getTemplateResources(
+					Arrays.asList("context.soy"))));
 	}
 
 	@Test
@@ -86,12 +81,10 @@ public class SoyTofuCacheTest {
 				Arrays.asList(
 					"simple.soy", "context.soy", "multi-context.soy"));
 
-		SoyFileSet soyFileSet = _soyTestHelper.getSoyFileSet(
-			cachedTemplateResources);
-
-		SoyTofu soyTofu = ProxyFactory.newDummyInstance(SoyTofu.class);
-
-		_soyTofuCacheHandler.add(cachedTemplateResources, soyFileSet, soyTofu);
+		_soyTofuCacheHandler.add(
+			cachedTemplateResources,
+			_soyTestHelper.getSoyFileSet(cachedTemplateResources),
+			ProxyFactory.newDummyInstance(SoyTofu.class));
 
 		List<TemplateResource> templateResources =
 			_soyTestHelper.getTemplateResources(Arrays.asList("context.soy"));
@@ -106,13 +99,10 @@ public class SoyTofuCacheTest {
 		List<TemplateResource> cachedTemplateResourcesA =
 			_soyTestHelper.getTemplateResources(Arrays.asList("simple.soy"));
 
-		SoyFileSet soyFileSetA = _soyTestHelper.getSoyFileSet(
-			cachedTemplateResourcesA);
-
-		SoyTofu soyTofuA = ProxyFactory.newDummyInstance(SoyTofu.class);
-
 		_soyTofuCacheHandler.add(
-			cachedTemplateResourcesA, soyFileSetA, soyTofuA);
+			cachedTemplateResourcesA,
+			_soyTestHelper.getSoyFileSet(cachedTemplateResourcesA),
+			ProxyFactory.newDummyInstance(SoyTofu.class));
 
 		Assert.assertNotNull(
 			_soyTofuCacheHandler.get(cachedTemplateResourcesA));
@@ -121,18 +111,13 @@ public class SoyTofuCacheTest {
 			_soyTestHelper.getTemplateResources(
 				Arrays.asList("context.soy", "multi-context.soy"));
 
-		SoyFileSet soyFileSetB = _soyTestHelper.getSoyFileSet(
-			cachedTemplateResourcesA);
-
-		SoyTofu soyTofuB = ProxyFactory.newDummyInstance(SoyTofu.class);
-
 		_soyTofuCacheHandler.add(
-			cachedTemplateResourcesB, soyFileSetB, soyTofuB);
+			cachedTemplateResourcesB,
+			_soyTestHelper.getSoyFileSet(cachedTemplateResourcesA),
+			ProxyFactory.newDummyInstance(SoyTofu.class));
 
-		List<TemplateResource> templateResources =
-			_soyTestHelper.getTemplateResources(Arrays.asList("context.soy"));
-
-		_soyTofuCacheHandler.removeIfAny(templateResources);
+		_soyTofuCacheHandler.removeIfAny(
+			_soyTestHelper.getTemplateResources(Arrays.asList("context.soy")));
 
 		Assert.assertNull(_soyTofuCacheHandler.get(cachedTemplateResourcesB));
 		Assert.assertNotNull(
