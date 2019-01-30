a507d6d812258cd78ec8762874103f6239d3baf1
Bryan Engler <bryan.engler@liferay.com>
2019-01-30 10:28:34 -0800

    LPS-89914 portal-search-test-util: StatsRequest and StatsResponse with cardinality
    https://issues.liferay.com/browse/LPS-89914
    
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

--- modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/stats/BaseStatisticsTestCase.java
+++ modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/stats/BaseStatisticsTestCase.java
@@ -14,19 +14,21 @@
 
 package com.liferay.portal.search.test.util.stats;
 
-import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.Hits;
-import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.Stats;
 import com.liferay.portal.kernel.search.StatsResults;
-import com.liferay.portal.search.test.util.IdempotentRetryAssert;
+import com.liferay.portal.search.internal.stats.StatsRequestBuilderFactoryImpl;
+import com.liferay.portal.search.internal.stats.StatsResponseBuilderImpl;
+import com.liferay.portal.search.stats.StatsRequest;
+import com.liferay.portal.search.stats.StatsRequestBuilder;
+import com.liferay.portal.search.stats.StatsRequestBuilderFactory;
+import com.liferay.portal.search.stats.StatsResponse;
+import com.liferay.portal.search.stats.StatsResponseBuilder;
 import com.liferay.portal.search.test.util.indexing.BaseIndexingTestCase;
 import com.liferay.portal.search.test.util.indexing.DocumentCreationHelpers;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -37,47 +39,253 @@ import org.junit.Test;
 public abstract class BaseStatisticsTestCase extends BaseIndexingTestCase {
 
 	@Test
-	public void testGetStats() throws Exception {
+	public void testStats() throws Exception {
 		addDocuments(31);
 
-		IdempotentRetryAssert.retryAssert(
-			3, TimeUnit.SECONDS,
-			new Callable<Void>() {
+		String field = STAT_SORTABLE_FIELD;
 
-				@Override
-				public Void call() throws Exception {
-					assertStats();
+		Stats stats = new Stats() {
+			{
+				setCount(true);
+				setField(field);
+				setMax(true);
+				setMean(true);
+				setMin(true);
+				setSum(true);
+				setSumOfSquares(true);
+			}
+		};
 
-					return null;
+		StatsResults expectedStatsResults = new StatsResults(field) {
+			{
+				setCount(31);
+				setMax(31);
+				setMean(16);
+				setMin(1);
+				setSum(496);
+				setSumOfSquares(10416);
+			}
+		};
+
+		assertSearch(
+			indexingTestHelper -> {
+				indexingTestHelper.define(
+					searchContext -> searchContext.addStats(stats));
+
+				indexingTestHelper.search();
+
+				indexingTestHelper.verify(
+					hits -> {
+						Map<String, StatsResults> statsResultsMap =
+							hits.getStatsResults();
+
+						Assert.assertNotNull(statsResultsMap);
+
+						StatsResults statsResults = statsResultsMap.get(field);
+
+						Assert.assertNotNull(statsResults);
+
+						Assert.assertEquals(
+							toString(expectedStatsResults),
+							toString(statsResults));
+					});
+			});
+	}
+
+	@Test
+	public void testStatsAgainstSearchCount() throws Exception {
+		addDocuments(31);
+
+		String field = STAT_SORTABLE_FIELD;
+
+		Stats stats = new Stats() {
+			{
+				setCount(true);
+				setField(field);
+			}
+		};
+
+		assertSearch(
+			indexingTestHelper -> {
+				indexingTestHelper.define(
+					searchContext -> searchContext.addStats(stats));
+
+				indexingTestHelper.searchCount();
+
+				indexingTestHelper.verify(hits -> Assert.assertNull(hits));
+
+				indexingTestHelper.verifyResponse(
+					searchResponse -> {
+						Map<String, StatsResponse> map =
+							searchResponse.getStatsResponseMap();
+
+						Assert.assertTrue(map.isEmpty());
+					});
+			});
+	}
+
+	@Test
+	public void testStatsRequest() throws Exception {
+		addDocuments(31);
+
+		String field = STAT_SORTABLE_FIELD;
+
+		StatsRequestBuilder statsRequestBuilder =
+			_statsRequestBuilderFactory.getStatsRequestBuilder();
+
+		StatsRequest statsRequest = statsRequestBuilder.cardinality(
+			true
+		).count(
+			true
+		).field(
+			field
+		).max(
+			true
+		).mean(
+			true
+		).min(
+			true
+		).missing(
+			true
+		).sum(
+			true
+		).sumOfSquares(
+			true
+		).build();
+
+		StatsResponseBuilder statsResponseBuilder =
+			new StatsResponseBuilderImpl();
+
+		StatsResponse expectedStatsResponse = statsResponseBuilder.cardinality(
+			31
+		).count(
+			31
+		).field(
+			field
+		).max(
+			31
+		).mean(
+			16
+		).min(
+			1
+		).sum(
+			496
+		).sumOfSquares(
+			10416
+		).build();
+
+		assertSearch(
+			indexingTestHelper -> {
+				indexingTestHelper.defineRequest(
+					searchRequest -> searchRequest.statsRequests(statsRequest));
+
+				indexingTestHelper.search();
+
+				indexingTestHelper.verifyResponse(
+					searchResponse -> {
+						Map<String, StatsResponse> map =
+							searchResponse.getStatsResponseMap();
+
+						Assert.assertNotNull(map);
+
+						StatsResponse statsResponse = map.get(field);
+
+						Assert.assertNotNull(statsResponse);
+
+						Assert.assertEquals(
+							toString(expectedStatsResponse),
+							toString(statsResponse));
+					});
+			});
+	}
+
+	@Test
+	public void testStatsRequestAgainstSearchCount() throws Exception {
+		addDocuments(31);
+
+		String field = STAT_SORTABLE_FIELD;
+
+		StatsRequestBuilder statsRequestBuilder =
+			_statsRequestBuilderFactory.getStatsRequestBuilder();
+
+		StatsRequest statsRequest = statsRequestBuilder.cardinality(
+			true
+		).field(
+			field
+		).build();
+
+		StatsResponseBuilder statsResponseBuilder =
+			new StatsResponseBuilderImpl();
+
+		StatsResponse expectedStatsResponse = statsResponseBuilder.cardinality(
+			31
+		).field(
+			field
+		).build();
+
+		assertSearch(
+			indexingTestHelper -> {
+				indexingTestHelper.defineRequest(
+					searchRequestBuilder -> searchRequestBuilder.statsRequests(
+						statsRequest));
+
+				indexingTestHelper.searchCount();
+
+				indexingTestHelper.verifyResponse(
+					searchResponse -> {
+						Map<String, StatsResponse> map =
+							searchResponse.getStatsResponseMap();
+
+						Assert.assertNotNull(map);
+
+						StatsResponse statsResponse = map.get(field);
+
+						Assert.assertNotNull(statsResponse);
+
+						Assert.assertEquals(
+							toString(expectedStatsResponse),
+							toString(statsResponse));
+					});
+			});
+	}
+
+	protected static String toString(StatsResponse statsResponse) {
+		return String.valueOf(
+			new LinkedHashMap<String, Object>() {
+				{
+					put("cardinality", statsResponse.getCardinality());
+					put("count", statsResponse.getCount());
+					put("field", statsResponse.getField());
+					put("max", statsResponse.getMax());
+					put("mean", statsResponse.getMean());
+					put("min", statsResponse.getMin());
+					put("missing", statsResponse.getMissing());
+					put(
+						"standardDeviation",
+						statsResponse.getStandardDeviation());
+					put("sum", statsResponse.getSum());
+					put("sumOfSquares", statsResponse.getSumOfSquares());
 				}
-
 			});
 	}
 
 	protected static String toString(StatsResults statsResults) {
-		StringBundler sb = new StringBundler(19);
-
-		sb.append("{count=");
-		sb.append(statsResults.getCount());
-		sb.append(", field=");
-		sb.append(statsResults.getField());
-		sb.append(", max=");
-		sb.append(statsResults.getMax());
-		sb.append(", mean=");
-		sb.append(statsResults.getMean());
-		sb.append(", min=");
-		sb.append(statsResults.getMin());
-		sb.append(", missing=");
-		sb.append(statsResults.getMissing());
-		sb.append(", standardDeviation=");
-		sb.append(statsResults.getStandardDeviation());
-		sb.append(", sum=");
-		sb.append(statsResults.getSum());
-		sb.append(", sumOfSquares=");
-		sb.append(statsResults.getSumOfSquares());
-		sb.append("}");
-
-		return sb.toString();
+		return String.valueOf(
+			new LinkedHashMap<String, Object>() {
+				{
+					put("count", statsResults.getCount());
+					put("field", statsResults.getField());
+					put("max", statsResults.getMax());
+					put("mean", statsResults.getMean());
+					put("min", statsResults.getMin());
+					put("missing", statsResults.getMissing());
+					put(
+						"standardDeviation",
+						statsResults.getStandardDeviation());
+					put("sum", statsResults.getSum());
+					put("sumOfSquares", statsResults.getSumOfSquares());
+				}
+			});
 	}
 
 	protected void addDocuments(int count) throws Exception {
@@ -88,49 +296,12 @@ public abstract class BaseStatisticsTestCase extends BaseIndexingTestCase {
 		}
 	}
 
-	protected void assertStats() throws Exception {
-		String field = STAT_SORTABLE_FIELD;
-
-		SearchContext searchContext = createSearchContext();
-
-		Stats stats = new Stats();
-
-		stats.setCount(true);
-		stats.setField(field);
-		stats.setMax(true);
-		stats.setMean(true);
-		stats.setMin(true);
-		stats.setSum(true);
-		stats.setSumOfSquares(true);
-
-		searchContext.addStats(stats);
-
-		Hits hits = search(searchContext);
-
-		Map<String, StatsResults> statsResultsMap = hits.getStatsResults();
-
-		Assert.assertNotNull(statsResultsMap);
-
-		StatsResults statsResults = statsResultsMap.get(field);
-
-		Assert.assertNotNull(statsResults);
-
-		StatsResults expectedStatsResults = new StatsResults(field);
-
-		expectedStatsResults.setCount(31);
-		expectedStatsResults.setMax(31);
-		expectedStatsResults.setMean(16);
-		expectedStatsResults.setMin(1);
-		expectedStatsResults.setSum(496);
-		expectedStatsResults.setSumOfSquares(10416);
-
-		Assert.assertEquals(
-			toString(expectedStatsResults), toString(statsResults));
-	}
-
 	protected static final String STAT_FIELD = Field.PRIORITY;
 
 	protected static final String STAT_SORTABLE_FIELD =
 		STAT_FIELD + "_Number_sortable";
+
+	private final StatsRequestBuilderFactory _statsRequestBuilderFactory =
+		new StatsRequestBuilderFactoryImpl();
 
 }
