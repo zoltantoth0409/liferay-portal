93d781c36635e07748899059d2926fe19ef1a522
Pavel Savinov <pavel.savinov@liferay.com>
2019-01-30 12:17:03 +0100

    LPS-89154 Implement fragments editor display context provider

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContextProvider.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.display.context;
+
+import com.liferay.layout.content.page.editor.constants.ContentPageEditorWebKeys;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
+
+import java.util.Objects;
+
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true, service = ContentPageEditorDisplayContextProvider.class
+)
+public class ContentPageEditorDisplayContextProvider {
+
+	public ContentPageEditorDisplayContext getContentPageEditorDisplayContext(
+		HttpServletRequest httpServletRequest) {
+
+		RenderResponse renderResponse =
+			(RenderResponse)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_RESPONSE);
+
+		String className = GetterUtil.getString(
+			httpServletRequest.getAttribute(
+				ContentPageEditorWebKeys.CLASS_NAME));
+
+		long classPK = GetterUtil.getLong(
+			httpServletRequest.getAttribute(ContentPageEditorWebKeys.CLASS_PK));
+
+		if (className == Layout.class.getName()) {
+			return new ContentPageLayoutEditorDisplayContext(
+				httpServletRequest, renderResponse, className, classPK);
+		}
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			_layoutPageTemplateEntryLocalService.fetchLayoutPageTemplateEntry(
+				classPK);
+
+		boolean showMapping = false;
+
+		if ((layoutPageTemplateEntry != null) &&
+			(layoutPageTemplateEntry.getType() ==
+				LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE)) {
+
+			showMapping = true;
+		}
+
+		return new ContentPageEditorLayoutPageTemplateDisplayContext(
+			httpServletRequest, renderResponse, className, classPK,
+			showMapping);
+	}
+
+	@Reference
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
+
+}
