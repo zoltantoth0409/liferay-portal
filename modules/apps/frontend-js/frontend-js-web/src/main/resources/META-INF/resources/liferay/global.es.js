a3ac4eaa8cfd13ee15727ce06f93424a952fbf51
Brian Chiu <brian.chiu@liferay.com>
2019-01-31 14:05:37 -0800

    Revert "LPS-89481 frontend-js-web - Add form utility functions, with intention to replace jQuery fm plugin"
    This reverts commit 738076f8bc5f3c063becfc0eb52646e2e03b4439.

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/global.es.js
@@ -8,15 +8,12 @@ import {
 	initComponentCache
 } from './component.es';
 import escape from 'lodash.escape';
-import getFormElement from './util/form/get_form_element.es';
 import groupBy from 'lodash.groupby';
 import isEqual from 'lodash.isequal';
 import portlet from './portlet/portlet.es';
 import navigate from './util/navigate.es';
 import ns from './util/ns.es';
 import objectToFormData from './util/object_to_form_data.es';
-import postForm from './util/form/post_form.es';
-import setFormValues from './util/form/set_form_values.es';
 import toCharCode from './util/to_char_code.es';
 import unescape from 'lodash.unescape';
 
@@ -29,14 +26,11 @@ Liferay.getComponentCache = getComponentCache;
 Liferay.initComponentCache = initComponentCache;
 
 Liferay.Util.escape = escape;
-Liferay.Util.getFormElement = getFormElement;
 Liferay.Util.groupBy = groupBy;
 Liferay.Util.isEqual = isEqual;
 Liferay.Util.navigate = navigate;
 Liferay.Util.ns = ns;
 Liferay.Util.objectToFormData = objectToFormData;
-Liferay.Util.postForm = postForm;
-Liferay.Util.setFormValues = setFormValues;
 Liferay.Util.toCharCode = toCharCode;
 
 Liferay.Util.openToast = (...args) => {
