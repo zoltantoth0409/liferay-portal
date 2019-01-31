a974e8e040b5b9f676795c441ddddf9515e3da1b
Brian Chan <brian.chan@liferay.com>
2019-01-30 22:18:56 -0800

    LPS-89415 not used

--- modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/ContentStructureResourceImpl.java
+++ modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/ContentStructureResourceImpl.java
@@ -25,7 +25,6 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.ClassNameService;
 import com.liferay.portal.kernel.service.CompanyService;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.vulcan.context.Pagination;
@@ -94,8 +93,5 @@ public class ContentStructureResourceImpl implements ContentStructureResource {
 
 	@Reference
 	private DDMStructureService _ddmStructureService;
-
-	@Reference
-	private Portal _portal;
 
 }
