2a3ed1fb4b213fb001dbe5a93cd6db30c41e0fe1
Máté Thurzó <mate.thurzo@liferay.com>
2019-01-31 09:12:53 +0100

    LPS-89406 Create REST endpoint for processes

---
+++ modules/apps/change-tracking/change-tracking-rest/src/main/java/com/liferay/change/tracking/rest/internal/model/process/CTProcessModel.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.rest.internal.model.process;
+
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.rest.internal.model.collection.CTCollectionModel;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlElement;
+
+/**
+ * @author Máté Thurzó
+ */
+public class CTProcessModel {
+
+	public static final CTProcessModel EMPTY_CT_PROCESS_MODEL =
+		new CTProcessModel();
+
+	public static CTProcessModel.Builder forCompany(long companyId) {
+		return new Builder(companyId);
+	}
+
+	@XmlElement
+	public long getCompanyId() {
+		return _companyId;
+	}
+
+	@XmlElement
+	public CTCollectionModel getCTCollection() {
+		return _ctCollection;
+	}
+
+	@XmlElement
+	public Date getDate() {
+		return _date;
+	}
+
+	@XmlElement
+	public String getUserInitials() {
+		return _userInitials;
+	}
+
+	@XmlElement
+	public String getUserName() {
+		return _userName;
+	}
+
+	@XmlElement
+	public String getUserPortraitURL() {
+		return _userPortraitURL;
+	}
+
+	public static class Builder {
+
+		public Builder(long companyId) {
+			_ctProcessModel = new CTProcessModel();
+
+			_ctProcessModel._companyId = companyId;
+		}
+
+		public CTProcessModel build() {
+			return _ctProcessModel;
+		}
+
+		public Builder setCTCollection(CTCollection ctCollection) {
+			CTCollectionModel.Builder builder =
+				CTCollectionModel.forCTCollection(ctCollection);
+
+			_ctProcessModel._ctCollection = builder.build();
+
+			return this;
+		}
+
+		public Builder setDate(Date date) {
+			_ctProcessModel._date = date;
+
+			return this;
+		}
+
+		public Builder setUserInitials(String userInitials) {
+			_ctProcessModel._userInitials = userInitials;
+
+			return this;
+		}
+
+		public Builder setUserName(String userName) {
+			_ctProcessModel._userName = userName;
+
+			return this;
+		}
+
+		public Builder setUserPortraitURL(String userPortraitURL) {
+			_ctProcessModel._userPortraitURL = userPortraitURL;
+
+			return this;
+		}
+
+		private final CTProcessModel _ctProcessModel;
+
+	}
+
+	private CTProcessModel() {
+	}
+
+	private long _companyId;
+	private CTCollectionModel _ctCollection;
+	private Date _date;
+	private String _userInitials;
+	private String _userName;
+	private String _userPortraitURL;
+
+}
---
+++ modules/apps/change-tracking/change-tracking-rest/src/main/java/com/liferay/change/tracking/rest/internal/resource/CTProcessResource.java
@@ -0,0 +1,176 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.rest.internal.resource;
+
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.model.CTProcess;
+import com.liferay.change.tracking.rest.internal.model.process.CTProcessModel;
+import com.liferay.change.tracking.rest.internal.util.CTJaxRsUtil;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTProcessLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserConstants;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Máté Thurzó
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=change-tracking-application)",
+		"osgi.jaxrs.resource=true"
+	},
+	scope = ServiceScope.PROTOTYPE, service = CTProcessResource.class
+)
+@Path("/processes")
+public class CTProcessResource {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<CTProcessModel> getCtProcessModels(
+			@QueryParam("companyId") long companyId,
+			@QueryParam("published") boolean published)
+		throws PortalException {
+
+		if (published) {
+			CTProcessModel ctProcessModel = _getPublishedCTProcessModel(
+				companyId);
+
+			return Collections.singletonList(ctProcessModel);
+		}
+
+		List<CTProcess> ctProcesses = _ctProcessLocalService.getCTProcesses(
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Stream<CTProcess> stream = ctProcesses.stream();
+
+		return stream.filter(
+			ctProcess -> ctProcess.getCompanyId() == companyId
+		).map(
+			this::_getCTProcessModel
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	private CTProcessModel _getCTProcessModel(CTProcess ctProcess) {
+		CTCollection ctCollection = _ctCollectionLocalService.fetchCTCollection(
+			ctProcess.getCtCollectionId());
+
+		User user = _userLocalService.fetchUser(ctProcess.getUserId());
+
+		Optional<User> userOptional = Optional.ofNullable(user);
+
+		CTProcessModel.Builder builder = CTProcessModel.forCompany(
+			ctProcess.getCompanyId());
+
+		return builder.setCTCollection(
+			ctCollection
+		).setDate(
+			ctProcess.getCreateDate()
+		).setUserInitials(
+			userOptional.map(
+				User::getInitials
+			).orElse(
+				StringPool.BLANK
+			)
+		).setUserName(
+			userOptional.map(
+				User::getFullName
+			).orElse(
+				StringPool.BLANK
+			)
+		).setUserPortraitURL(
+			userOptional.map(
+				this::_getPortraitURL
+			).orElse(
+				StringPool.BLANK
+			)
+		).build();
+	}
+
+	private String _getPortraitURL(User user) {
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)_httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		if (themeDisplay == null) {
+			return StringPool.BLANK;
+		}
+
+		if (user != null) {
+			return user.fetchPortraitURL(themeDisplay);
+		}
+
+		return UserConstants.getPortraitURL(
+			themeDisplay.getPathImage(), true, 0, StringPool.BLANK);
+	}
+
+	private CTProcessModel _getPublishedCTProcessModel(long companyId)
+		throws PortalException {
+
+		CTJaxRsUtil.checkCompany(companyId);
+
+		CTProcess ctProcess = _ctProcessLocalService.fetchLatestCTProcess(
+			companyId);
+
+		if (ctProcess == null) {
+			return CTProcessModel.EMPTY_CT_PROCESS_MODEL;
+		}
+
+		return _getCTProcessModel(ctProcess);
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private CTCollectionLocalService _ctCollectionLocalService;
+
+	@Reference
+	private CTProcessLocalService _ctProcessLocalService;
+
+	@Context
+	private HttpServletRequest _httpServletRequest;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
