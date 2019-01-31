a1518b6a789e193ccb1c2f619143b03a0e3a660e
Brian Chan <brian.chan@liferay.com>
2019-01-31 14:13:20 -0800

    LPS-89415 Move to the correct resource

--- modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/ContentStructureResourceImpl.java
+++ modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/ContentStructureResourceImpl.java
@@ -14,30 +14,9 @@
 
 package com.liferay.headless.web.experience.internal.resource;
 
-import com.liferay.dynamic.data.mapping.model.DDMStructure;
-import com.liferay.dynamic.data.mapping.service.DDMStructureService;
-import com.liferay.headless.web.experience.dto.Comment;
-import com.liferay.headless.web.experience.dto.ContentStructure;
-import com.liferay.headless.web.experience.dto.StructuredContent;
 import com.liferay.headless.web.experience.resource.ContentStructureResource;
-import com.liferay.journal.model.JournalArticle;
-import com.liferay.portal.kernel.model.ClassName;
-import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.service.ClassNameService;
-import com.liferay.portal.kernel.service.CompanyService;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.PropsUtil;
-import com.liferay.portal.vulcan.context.AcceptLanguage;
-import com.liferay.portal.vulcan.context.Pagination;
-import com.liferay.portal.vulcan.dto.Page;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 
@@ -51,67 +30,6 @@ import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 	},
 	scope = ServiceScope.PROTOTYPE, service = ContentStructureResource.class
 )
-public class ContentStructureResourceImpl implements ContentStructureResource {
-
-	@Override
-	public Page<StructuredContent> getContentSpaceStructuredContentsPage(
-			Integer parentId, String filter, String sort,
-			AcceptLanguage acceptLanguage, Pagination pagination)
-		throws Exception {
-
-		Company company = _companyService.getCompanyByWebId(
-			PropsUtil.get(PropsKeys.COMPANY_DEFAULT_WEB_ID));
-
-		Group group = company.getGroup();
-
-		ClassName className = _classNameService.fetchClassName(
-			JournalArticle.class.getName());
-
-		List<DDMStructure> ddmStructures = _ddmStructureService.getStructures(
-			company.getCompanyId(), new long[] {group.getGroupId()},
-			className.getClassNameId(), pagination.getStartPosition(),
-			pagination.getEndPosition(), null);
-
-		List<ContentStructure> contentStructures = new ArrayList<>(
-			ddmStructures.size());
-
-		for (DDMStructure ddmStructure : ddmStructures) {
-			ContentStructure contentStructure = new ContentStructure();
-
-			// TODO Do not cast to int
-
-			contentStructure.setId((int)ddmStructure.getStructureId());
-
-			contentStructures.add(contentStructure);
-		}
-
-		int count = _ddmStructureService.getStructuresCount(
-			group.getGroupId(), new long[] {className.getClassNameId()},
-			className.getClassNameId());
-
-		return new Page(contentStructures, count);
-	}
-
-	@Override
-	public ContentStructure getContentStructure(Integer id) throws Exception {
-		return new ContentStructure();
-	}
-
-	@Override
-	public Page<Comment> getStructuredContentsCommentPage(
-			StructuredContent parentId, Pagination pagination)
-		throws Exception {
-
-		return new Page(Collections.emptyList(), 0);
-	}
-
-	@Reference
-	private ClassNameService _classNameService;
-
-	@Reference
-	private CompanyService _companyService;
-
-	@Reference
-	private DDMStructureService _ddmStructureService;
-
+public class ContentStructureResourceImpl
+	extends BaseContentStructureResourceImpl {
 }
