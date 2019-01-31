9946a304443fa0ab9918ba3f1091b73d0c3636f0
David Arques <david.arques@liferay.com>
2019-01-31 16:49:11 +0100

    LPS-89662 Add test

---
+++ modules/apps/segments/segments-test/src/testIntegration/java/com/liferay/segments/internal/instance/lifecycle/test/AddDefaultSegmentsEntryPortalInstanceLifecycleListenerTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.instance.lifecycle.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+import com.liferay.segments.constants.SegmentsConstants;
+import com.liferay.segments.model.SegmentsEntry;
+import com.liferay.segments.service.SegmentsEntryLocalService;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author David Arques
+ */
+@RunWith(Arquillian.class)
+public class AddDefaultSegmentsEntryPortalInstanceLifecycleListenerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE);
+
+	@Test
+	public void testAddDefaultSegmentsEntryWhenAddingNewCompany()
+		throws Exception {
+
+		_company = CompanyTestUtil.addCompany();
+
+		List<SegmentsEntry> segmentsEntries =
+			_segmentsEntryLocalService.getSegmentsEntries(
+				_company.getGroupId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				null);
+
+		Assert.assertEquals(
+			segmentsEntries.toString(), 1, segmentsEntries.size());
+
+		SegmentsEntry segmentsEntry = segmentsEntries.get(0);
+
+		Assert.assertEquals(
+			SegmentsConstants.KEY_DEFAULT, segmentsEntry.getKey());
+	}
+
+	@DeleteAfterTestRun
+	private Company _company;
+
+	@Inject
+	private SegmentsEntryLocalService _segmentsEntryLocalService;
+
+}
