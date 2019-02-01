0d760e83c0776be7fd07a875a755d7508bd17cf6
Brian Chan <brian.chan@liferay.com>
2019-01-31 17:45:58 -0800

    LPS-89415 Readd logic that was lost to ContentStructureResourceImpl because of a1518b6a789e193ccb1c2f619143b03a0e3a660e

--- modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/StructuredContentResourceImpl.java
+++ modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/StructuredContentResourceImpl.java
@@ -14,9 +14,41 @@
 
 package com.liferay.headless.web.experience.internal.resource;
 
+import com.liferay.headless.web.experience.dto.StructuredContent;
 import com.liferay.headless.web.experience.resource.StructuredContentResource;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.model.JournalArticleConstants;
+import com.liferay.journal.util.JournalHelper;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.IndexSearcherHelperUtil;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.Query;
+import com.liferay.portal.kernel.search.QueryConfig;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.search.SearchResultPermissionFilter;
+import com.liferay.portal.kernel.search.SearchResultPermissionFilterFactory;
+import com.liferay.portal.kernel.search.SearchResultPermissionFilterSearcher;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.vulcan.context.AcceptLanguage;
+import com.liferay.portal.vulcan.context.Pagination;
+import com.liferay.portal.vulcan.dto.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,4 +60,120 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class StructuredContentResourceImpl
 	extends BaseStructuredContentResourceImpl {
+
+	@Override
+	public Page<StructuredContent> getContentSpaceStructuredContentsPage(
+			Integer parentId, String filter, String sort,
+			AcceptLanguage acceptLanguage, Pagination pagination)
+		throws Exception {
+
+		// TODO setId should not require a cast to (int)
+
+		Hits hits = _getHits(pagination);
+
+		List<StructuredContent> structuredContents = Stream.of(
+			_journalHelper.getArticles(hits)
+		).flatMap(
+			List::stream
+		).map(
+			journalArticle -> {
+				StructuredContent structuredContent = new StructuredContent();
+
+				structuredContent.setId(
+					(int)journalArticle.getResourcePrimKey());
+
+				return structuredContent;
+			}
+		).collect(
+			Collectors.toList()
+		);
+
+		return new Page(structuredContents, hits.getLength());
+	}
+
+	private SearchContext _createSearchContext(
+		long companyId, long groupId, int start, int end) {
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setAttribute(
+			Field.CLASS_NAME_ID, JournalArticleConstants.CLASSNAME_ID_DEFAULT);
+		searchContext.setAttribute(
+			Field.STATUS, WorkflowConstants.STATUS_APPROVED);
+		searchContext.setAttribute("head", Boolean.TRUE);
+		searchContext.setCompanyId(companyId);
+		searchContext.setEnd(end);
+		searchContext.setGroupIds(new long[] {groupId});
+		searchContext.setStart(start);
+
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		queryConfig.setHighlightEnabled(false);
+		queryConfig.setScoreEnabled(false);
+		queryConfig.setSelectedFieldNames(
+			Field.ARTICLE_ID, Field.SCOPE_GROUP_ID);
+
+		return searchContext;
+	}
+
+	private Hits _getHits(Pagination pagination) throws Exception {
+		Company company = _companyService.getCompanyByWebId(
+			PropsUtil.get(PropsKeys.COMPANY_DEFAULT_WEB_ID));
+
+		Group group = company.getGroup();
+
+		SearchContext searchContext = _createSearchContext(
+			company.getCompanyId(), group.getGroupId(),
+			pagination.getStartPosition(), pagination.getEndPosition());
+
+		Query query = _getQuery(searchContext);
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (permissionChecker == null) {
+			return IndexSearcherHelperUtil.search(searchContext, query);
+		}
+
+		if (searchContext.getUserId() == 0) {
+			searchContext.setUserId(permissionChecker.getUserId());
+		}
+
+		SearchResultPermissionFilter searchResultPermissionFilter =
+			_searchResultPermissionFilterFactory.create(
+				new SearchResultPermissionFilterSearcher() {
+
+					public Hits search(SearchContext searchContext)
+						throws SearchException {
+
+						return IndexSearcherHelperUtil.search(
+							searchContext, query);
+					}
+
+				},
+				permissionChecker);
+
+		return searchResultPermissionFilter.search(searchContext);
+	}
+
+	private Query _getQuery(SearchContext searchContext) throws Exception {
+		Indexer<JournalArticle> indexer = _indexerRegistry.nullSafeGetIndexer(
+			JournalArticle.class);
+
+		return indexer.getFullQuery(searchContext);
+	}
+
+	@Reference
+	private CompanyService _companyService;
+
+	@Reference
+	private IndexerRegistry _indexerRegistry;
+
+	@Reference
+	private JournalHelper _journalHelper;
+
+	@Reference
+	private SearchResultPermissionFilterFactory
+		_searchResultPermissionFilterFactory;
+
 }
