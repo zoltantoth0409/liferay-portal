4141d59d32c3a002d533b45d78ff7857089cb0ed
Leonardo Barros <leonardo.barros@liferay.com>
2019-02-01 15:58:00 -0300

    LPS-89907 add constants

---
+++ modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/constants/DEDataDefinitionRuleConstants.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.constants;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DEDataDefinitionRuleConstants {
+
+	public static final String DECIMAL_LITERAL_RULE = "decimalLiteral";
+
+	public static final String EMAIL_ADDRESS_RULE = "emailAddress";
+
+	public static final String EMPTY_RULE = "empty";
+
+	public static final String EXPRESSION_PARAMETER = "expression";
+
+	public static final String INTEGER_LITERAL_RULE = "integerLiteral";
+
+	public static final String INVALID_EMAIL_ADDRESS_ERROR =
+		"invalid-email-address";
+
+	public static final String INVALID_URL_ERROR = "invalid-url";
+
+	public static final String MATCH_EXPRESSION_RULE = "matchExpression";
+
+	public static final String REQUIRED_RULE = "required";
+
+	public static final String URL_RULE = "url";
+
+	public static final String VALIDATION_RULE_TYPE = "validation";
+
+	public static final String VALUE_MUST_BE_DECIMAL_ERROR =
+		"value-must-be-a-decimal-value";
+
+	public static final String VALUE_MUST_BE_INTEGER_ERROR =
+		"value-must-be-an-integer-value";
+
+	public static final String VALUE_MUST_MATCH_EXPRESSION_ERROR =
+		"value-must-match-expression";
+
+	public static final String VALUE_MUST_NOT_BE_EMPTY_ERROR =
+		"value-must-not-be-empty";
+
+}
