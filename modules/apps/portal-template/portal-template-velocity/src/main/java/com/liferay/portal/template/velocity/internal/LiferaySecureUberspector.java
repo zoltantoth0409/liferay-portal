8fc85e4232e7b5fa2a6556d5d83821719204b582
Marta Medio <marta.medio@liferay.com>
2019-02-01 17:08:58 +0100

    LPS-64815 Process Velocity configuration and throw exception if template try to use a forbidden property

--- modules/apps/portal-template/portal-template-velocity/src/main/java/com/liferay/portal/template/velocity/internal/LiferaySecureUberspector.java
+++ modules/apps/portal-template/portal-template-velocity/src/main/java/com/liferay/portal/template/velocity/internal/LiferaySecureUberspector.java
@@ -22,6 +22,8 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.util.PortalImpl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -77,6 +79,13 @@ public class LiferaySecureUberspector extends SecureUberspector {
 					cnfe);
 			}
 		}
+
+		String[] restrictedClassProperties = (String[])extendedProperties.get(
+			"liferay." + RuntimeConstants.INTROSPECTOR_RESTRICT_CLASSES +
+				".properties");
+
+		_restrictedClassProperties = _getRestrictedClassPropertiesMap(
+			restrictedClassProperties);
 
 		String[] restrictedPackageNames = extendedProperties.getStringArray(
 			RuntimeConstants.INTROSPECTOR_RESTRICT_PACKAGES);
@@ -154,12 +163,63 @@ public class LiferaySecureUberspector extends SecureUberspector {
 		}
 	}
 
+	private void _checkClassPropertyIsRestricted(
+		Class clazz, String methodName) {
+
+		String className = clazz.getName();
+
+		if (_restrictedClassProperties.containsKey(className)) {
+			List<String> properties = _restrictedClassProperties.get(className);
+
+			if (methodName.startsWith("get")) {
+				String property = methodName.substring(3);
+
+				if (properties.contains(property)) {
+					throw new IllegalArgumentException(
+						StringBundler.concat(
+							"Forbbiden access to property ", property, " of ",
+							clazz.getName()));
+				}
+			}
+		}
+	}
+
+	private Map<String, List<String>> _getRestrictedClassPropertiesMap(
+		String[] restrictedClassProperties) {
+
+		Map<String, List<String>> restrictedClassPropertiesMap =
+			new HashMap<>();
+
+		if (restrictedClassProperties != null) {
+			for (String restrictedClassProperty : restrictedClassProperties) {
+				String className = StringUtil.extractFirst(
+					restrictedClassProperty.trim(), StringPool.EQUAL);
+				String propertyName = StringUtil.extractLast(
+					restrictedClassProperty.trim(), StringPool.EQUAL);
+
+				if (restrictedClassPropertiesMap.containsKey(className)) {
+					List<String> properties = restrictedClassPropertiesMap.get(
+						className);
+
+					properties.add(propertyName);
+				}
+				else {
+					restrictedClassPropertiesMap.put(
+						className.trim(), Arrays.asList(propertyName));
+				}
+			}
+		}
+
+		return restrictedClassPropertiesMap;
+	}
+
 	private static final ClassRestrictionInformation _nullInstance =
 		new ClassRestrictionInformation(null);
 
 	private final Map<String, ClassRestrictionInformation>
 		_classRestrictionInformations = new ConcurrentHashMap<>();
 	private List<Class<?>> _restrictedClasses;
+	private Map<String, List<String>> _restrictedClassProperties;
 	private List<String> _restrictedPackageNames;
 	private RuntimeServices _runtimeServices;
 
@@ -200,6 +260,8 @@ public class LiferaySecureUberspector extends SecureUberspector {
 			}
 
 			_checkClassIsRestricted(clazz);
+
+			_checkClassPropertyIsRestricted(clazz, methodName);
 
 			return true;
 		}
