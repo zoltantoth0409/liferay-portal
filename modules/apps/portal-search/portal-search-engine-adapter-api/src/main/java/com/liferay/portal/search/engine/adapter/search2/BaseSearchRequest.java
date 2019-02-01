d9a82b6586299edbb822f8e00da31675b92145f0
Michael Han <michael.han@liferay.com>
2019-02-01 17:47:43 -0600

    LPS-89499 portal-search-engine-adapter-api: update to use varargs and set default result size to 10
    https://issues.liferay.com/browse/LPS-89499
    
    Co-authored-by: Michael Han <michael.han@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

--- modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/search2/BaseSearchRequest.java
+++ modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/search2/BaseSearchRequest.java
@@ -124,7 +124,7 @@ public abstract class BaseSearchRequest {
 		_includeResponseString = includeResponseString;
 	}
 
-	public void setIndexNames(String[] indexNames) {
+	public void setIndexNames(String... indexNames) {
 		_indexNames = indexNames;
 	}
 
--- modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/search2/SearchSearchRequest.java
+++ modules/apps/portal-search/portal-search-engine-adapter-api/src/main/java/com/liferay/portal/search/engine/adapter/search2/SearchSearchRequest.java
@@ -34,6 +34,8 @@ import java.util.Map;
 public class SearchSearchRequest
 	extends BaseSearchRequest implements SearchRequest<SearchSearchResponse> {
 
+	public static final int DEFAULT_SEARCH_RESULT_SIZE = 10;
+
 	@Override
 	public SearchSearchResponse accept(
 		SearchRequestExecutor searchRequestExecutor) {
@@ -141,7 +143,7 @@ public class SearchSearchRequest
 	private String _preference;
 	private boolean _scoreEnabled;
 	private String[] _selectedFieldNames;
-	private int _size;
+	private int _size = DEFAULT_SEARCH_RESULT_SIZE;
 	private final List<Sort> _sorts = new ArrayList<>();
 	private int _start;
 	private Boolean _version;
