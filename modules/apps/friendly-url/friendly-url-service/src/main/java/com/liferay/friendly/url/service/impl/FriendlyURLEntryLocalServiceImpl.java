c7142ef1bffa452d9600956f47dd1f6e0c5557b8
Brian Chan <brian.chan@liferay.com>
2019-02-01 12:04:58 -0800

    LPS-89116 SF

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLEntryLocalServiceImpl.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/service/impl/FriendlyURLEntryLocalServiceImpl.java
@@ -349,10 +349,8 @@ public class FriendlyURLEntryLocalServiceImpl
 				Math.min(
 					maxLength - suffix.length(), normalizedUrlTitle.length()));
 
-			String decodedUrlTitle = HttpUtil.decodePath(prefix + suffix);
-
 			curUrlTitle = FriendlyURLNormalizerUtil.normalizeWithEncoding(
-				decodedUrlTitle);
+				HttpUtil.decodePath(prefix + suffix));
 		}
 
 		return curUrlTitle;
@@ -451,10 +449,9 @@ public class FriendlyURLEntryLocalServiceImpl
 		int maxLength = ModelHintsUtil.getMaxLength(
 			FriendlyURLEntryLocalization.class.getName(), "urlTitle");
 
-		String decodedUrlTitle = HttpUtil.decodePath(urlTitle);
-
 		String normalizedUrlTitle =
-			FriendlyURLNormalizerUtil.normalizeWithEncoding(decodedUrlTitle);
+			FriendlyURLNormalizerUtil.normalizeWithEncoding(
+				HttpUtil.decodePath(urlTitle));
 
 		if (normalizedUrlTitle.length() > maxLength) {
 			throw new FriendlyURLLengthException(
