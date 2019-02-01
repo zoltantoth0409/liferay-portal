791e416c75b688a5bd256c09b616358d7156b9dd
tina.tian <tina.tian@liferay.com>
2019-01-31 16:17:32 -0800

    LPS-89456 Add upgrade process - dynamic-data-mapping-service

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/DDMServiceUpgrade.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/DDMServiceUpgrade.java
@@ -43,6 +43,7 @@ import com.liferay.dynamic.data.mapping.internal.upgrade.v3_0_0.util.DDMStructur
 import com.liferay.dynamic.data.mapping.internal.upgrade.v3_0_0.util.DDMStructureVersionTable;
 import com.liferay.dynamic.data.mapping.internal.upgrade.v3_0_0.util.DDMTemplateTable;
 import com.liferay.dynamic.data.mapping.internal.upgrade.v3_0_0.util.DDMTemplateVersionTable;
+import com.liferay.dynamic.data.mapping.internal.upgrade.v4_0_0.UpgradeBadColumnNames;
 import com.liferay.dynamic.data.mapping.io.DDMFormDeserializer;
 import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerTracker;
 import com.liferay.dynamic.data.mapping.io.DDMFormLayoutSerializer;
@@ -217,6 +218,8 @@ public class DDMServiceUpgrade implements UpgradeStepRegistrator {
 			"3.0.0", "3.0.1",
 			new com.liferay.dynamic.data.mapping.internal.upgrade.v3_0_1.
 				UpgradeDDMFormParagraphFields(_jsonFactory));
+
+		registry.register("3.0.1", "4.0.0", new UpgradeBadColumnNames());
 	}
 
 	protected DDMFormDeserializer getDDMFormJSONDeserializer() {
