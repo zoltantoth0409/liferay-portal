aa96a384b05e4b6893f46d203e75958f408f2a40
Victor Galan <victor.galan@liferay.com>
2019-02-04 10:33:59 +0000

    LPS-89723 Add Get for blogPostings

--- modules/apps/headless/headless-collaboration/headless-collaboration-impl/src/main/java/com/liferay/headless/collaboration/internal/resource/BlogPostingResourceImpl.java
+++ modules/apps/headless/headless-collaboration/headless-collaboration-impl/src/main/java/com/liferay/headless/collaboration/internal/resource/BlogPostingResourceImpl.java
@@ -14,17 +14,66 @@
 
 package com.liferay.headless.collaboration.internal.resource;
 
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryService;
+import com.liferay.headless.collaboration.dto.BlogPosting;
 import com.liferay.headless.collaboration.resource.BlogPostingResource;
-
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.vulcan.context.Pagination;
+import com.liferay.portal.vulcan.dto.Page;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ServiceScope;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Javier Gamarra
  */
 @Component(
 	properties = "OSGI-INF/blog-posting.properties",
-	scope = ServiceScope.PROTOTYPE, service = BlogPostingResource.class
+	service = BlogPostingResource.class
 )
 public class BlogPostingResourceImpl extends BaseBlogPostingResourceImpl {
+
+	@Override
+	public Page<BlogPosting> getContentSpaceBlogPostingPage(
+		Long parentId, Pagination pagination) {
+
+		List<BlogsEntry> blogsEntries = _blogsEntryService.getGroupEntries(
+			parentId, WorkflowConstants.STATUS_APPROVED,
+			pagination.getStartPosition(), pagination.getEndPosition());
+
+		Stream<BlogsEntry> stream = blogsEntries.stream();
+
+		List<BlogPosting> blogPostings = stream.map(
+			this::_toBlogPosting
+		).collect(
+			Collectors.toList()
+		);
+
+		int count = _blogsEntryService.getGroupEntriesCount(
+			parentId, WorkflowConstants.STATUS_APPROVED);
+
+		return new Page<>(blogPostings, count);
+	}
+
+	private BlogPosting _toBlogPosting(BlogsEntry blogsEntry) {
+		BlogPosting blogPosting = new BlogPosting();
+
+		blogPosting.setAlternativeHeadline(blogsEntry.getSubtitle());
+		blogPosting.setArticleBody(blogsEntry.getContent());
+		blogPosting.setCaption(blogsEntry.getCoverImageCaption());
+		blogPosting.setDescription(blogsEntry.getDescription());
+		blogPosting.setFriendlyUrlPath(blogsEntry.getUrlTitle());
+		blogPosting.setHeadline(blogsEntry.getTitle());
+		blogPosting.setId(blogsEntry.getEntryId());
+
+		return blogPosting;
+	}
+
+	@Reference
+	private BlogsEntryService _blogsEntryService;
+
 }
