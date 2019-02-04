18b775147489bf42d8805de49996854964c61553
Adolfo Pérez <adolfo.perez@liferay.com>
2019-02-04 14:18:14 +0100

    LPS-90075 Create group configuration

---
+++ modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/internal/configuration/SharingGroupConfiguration.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Adolfo Pérez
+ */
+@ExtendedObjectClassDefinition(
+	category = "sharing", generateUI = false,
+	scope = ExtendedObjectClassDefinition.Scope.GROUP
+)
+@Meta.OCD(
+	id = "com.liferay.sharing.internal.configuration.SharingGroupConfiguration",
+	localization = "content/Language", name = "sharing-group-configuration-name"
+)
+public interface SharingGroupConfiguration {
+
+	/**
+	 * Enables sharing.
+	 * @review
+	 */
+	@Meta.AD(deflt = "true", name = "enabled", required = false)
+	public boolean enabled();
+
+}
