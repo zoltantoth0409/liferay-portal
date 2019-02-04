c89fda4ac301953fcb47a38c2885580052c78e5a
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-02-04 10:12:58 +0100

    LPS-89951 Also add message for layout name exception

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddLayoutPrototypeMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddLayoutPrototypeMVCActionCommand.java
@@ -19,6 +19,7 @@ import com.liferay.layout.page.template.exception.DuplicateLayoutPageTemplateEnt
 import com.liferay.layout.page.template.exception.LayoutPageTemplateEntryNameException;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.portal.kernel.exception.LayoutNameException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -141,7 +142,9 @@ public class AddLayoutPrototypeMVCActionCommand extends BaseMVCActionCommand {
 
 			String errorMessage = "an-unexpected-error-occurred";
 
-			if (t instanceof LayoutPageTemplateEntryNameException) {
+			if (t instanceof LayoutNameException ||
+				t instanceof LayoutPageTemplateEntryNameException) {
+
 				errorMessage = "please-enter-a-valid-name";
 			}
 			else if (t instanceof
