146656cb047d9456e4c6e3066c9d2069917bfbd9
Pavel Savinov <pavel.savinov@liferay.com>
2019-02-05 09:23:55 +0100

    LPS-89483 Replace with iterator

--- modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/contributor/BaseFragmentCollectionContributor.java
+++ modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/contributor/BaseFragmentCollectionContributor.java
@@ -31,6 +31,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -86,9 +87,11 @@ public abstract class BaseFragmentCollectionContributor
 
 				_fragmentEntries = new HashMap<>();
 
-				for (int i = 0; i < jsonArray.length(); i++) {
+				Iterator<String> iterator = jsonArray.iterator();
+
+				while (iterator.hasNext()) {
 					FragmentEntry fragmentEntry = _getFragmentEntry(
-						jsonArray.getString(i));
+						iterator.next());
 
 					List<FragmentEntry> fragmentEntryList =
 						_fragmentEntries.getOrDefault(
