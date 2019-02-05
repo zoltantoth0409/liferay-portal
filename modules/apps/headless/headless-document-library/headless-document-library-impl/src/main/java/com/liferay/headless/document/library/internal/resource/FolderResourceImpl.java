20fcd4f1b9f24d9d83385787d16a56d9866b037b
rubenpulido <ruben.pulido@liferay.com>
2019-02-05 13:12:40 +0100

    LPS-89710 CRUD implementation

--- modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/FolderResourceImpl.java
+++ modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/FolderResourceImpl.java
@@ -21,10 +21,13 @@ import com.liferay.headless.document.library.dto.Folder;
 import com.liferay.headless.document.library.resource.FolderResource;
 import com.liferay.portal.kernel.exception.NoSuchGroupException;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.vulcan.context.Pagination;
 import com.liferay.portal.vulcan.dto.Page;
 
 import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.NotFoundException;
 
 import org.osgi.service.component.annotations.Component;
@@ -42,38 +45,129 @@ public class FolderResourceImpl extends BaseFolderResourceImpl {
 
 	@Override
 	public Page<Folder> getDocumentsRepositoryFolderPage(
-			Long parentId, Pagination pagination)
-		throws Exception {
+		Long parentId, Pagination pagination) {
 
 		return _getFolderPage(
 			parentId, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, pagination);
 	}
 
 	@Override
+	public Folder getFolder(Long id) {
+		return _toFolder(_getFolder(id));
+	}
+
+	@Override
 	public Page<Folder> getFolderFolderPage(
-			Long parentId, Pagination pagination)
-		throws Exception {
+		Long parentId, Pagination pagination) {
 
+		try {
+			com.liferay.portal.kernel.repository.model.Folder parentFolder =
+				_dlAppService.getFolder(parentId);
+
+			return _getFolderPage(
+				parentFolder.getGroupId(), parentFolder.getFolderId(),
+				pagination);
+		}
+		catch (NoSuchFolderException nsfe) {
+			throw new NotFoundException(nsfe);
+		}
+		catch (PrincipalException pe) {
+			throw new NotAuthorizedException(pe);
+		}
+		catch (PortalException pe) {
+			throw new InternalServerErrorException(pe);
+		}
+	}
+
+	@Override
+	public Folder postDocumentsRepositoryFolder(Long parentId, Folder folder) {
+		return _addFolder(parentId, 0L, folder);
+	}
+
+	@Override
+	public Folder postFolderFolder(Long parentId, Folder folder) {
 		com.liferay.portal.kernel.repository.model.Folder parentFolder =
-			_dlAppService.getFolder(parentId);
+			_getFolder(parentId);
 
-		return _getFolderPage(
-			parentFolder.getGroupId(), parentFolder.getFolderId(),
-			pagination);
+		return _addFolder(
+			parentFolder.getGroupId(), parentFolder.getFolderId(), folder);
+	}
+
+	@Override
+	public Folder putFolder(Long id, Folder folder) {
+		try {
+			return _toFolder(
+				_dlAppService.updateFolder(
+					id, folder.getName(), folder.getDescription(),
+					new ServiceContext()));
+		}
+		catch (NoSuchFolderException nsfe) {
+			throw new NotFoundException(nsfe);
+		}
+		catch (PrincipalException pe) {
+			throw new NotAuthorizedException(pe);
+		}
+		catch (PortalException pe) {
+			throw new InternalServerErrorException(pe);
+		}
+	}
+
+	private Folder _addFolder(
+		Long parentId, Long parentFolderId, Folder folder) {
+
+		try {
+			return _toFolder(
+				_dlAppService.addFolder(
+					parentId, parentFolderId, folder.getName(),
+					folder.getDescription(), new ServiceContext()));
+		}
+		catch (NoSuchGroupException nsge) {
+			throw new NotFoundException(nsge);
+		}
+		catch (PrincipalException pe) {
+			throw new NotAuthorizedException(pe);
+		}
+		catch (PortalException pe) {
+			throw new InternalServerErrorException(pe);
+		}
+	}
+
+	private com.liferay.portal.kernel.repository.model.Folder _getFolder(
+		Long id) {
+
+		try {
+			return _dlAppService.getFolder(id);
+		}
+		catch (NoSuchFolderException nsfe) {
+			throw new NotFoundException(nsfe);
+		}
+		catch (PortalException pe) {
+			throw new InternalServerErrorException(pe);
+		}
 	}
 
 	private Page<Folder> _getFolderPage(
-			Long groupId, Long parentFolderId, Pagination pagination)
-		throws Exception {
+		Long groupId, Long parentFolderId, Pagination pagination) {
 
-		return Page.of(
-			transform(
-				_dlAppService.getFolders(
-					groupId, parentFolderId, pagination.getStartPosition(),
-					pagination.getEndPosition(), null),
-				this::_toFolder),
-			pagination,
-			_dlAppService.getFoldersCount(groupId, parentFolderId));
+		try {
+			return Page.of(
+				transform(
+					_dlAppService.getFolders(
+						groupId, parentFolderId, pagination.getStartPosition(),
+						pagination.getEndPosition(), null),
+					this::_toFolder),
+				pagination,
+				_dlAppService.getFoldersCount(groupId, parentFolderId));
+		}
+		catch (NoSuchGroupException nsge) {
+			throw new NotFoundException(nsge);
+		}
+		catch (PrincipalException pe) {
+			throw new NotAuthorizedException(pe);
+		}
+		catch (PortalException pe) {
+			throw new InternalServerErrorException(pe);
+		}
 	}
 
 	private Folder _toFolder(
