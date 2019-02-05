682478cca2b4b2fea5b83d00e67cab5f2b92e1f9
Pavel Savinov <pavel.savinov@liferay.com>
2019-02-05 11:15:37 +0100

    LPS-89617 Set edit mode parameter for the render fragment entry link url

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -53,6 +53,8 @@ import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.service.PortletPreferencesLocalServiceUtil;
 import com.liferay.portal.kernel.service.permission.PortletPermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -249,7 +251,8 @@ public class ContentPageEditorDisplayContext {
 
 		actionURL.setParameter(ActionRequest.ACTION_NAME, action);
 
-		return actionURL.toString();
+		return HttpUtil.addParameter(
+			actionURL.toString(), "p_l_mode", Constants.EDIT);
 	}
 
 	protected SoyContext getFragmentEntrySoyContext(
