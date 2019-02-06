1a8c0c4140ea5985be4b9e49a4ccda3486a4ee83
Gergely Mathe <gergely.mathe@liferay.com>
2019-02-06 10:51:48 +0100

    LPS-89404 Avoid NPE

--- modules/apps/journal/journal-change-tracking/src/main/java/com/liferay/journal/change/tracking/internal/service/CTJournalArticleLocalServiceWrapper.java
+++ modules/apps/journal/journal-change-tracking/src/main/java/com/liferay/journal/change/tracking/internal/service/CTJournalArticleLocalServiceWrapper.java
@@ -1377,6 +1377,10 @@ public class CTJournalArticleLocalServiceWrapper
 	}
 
 	private boolean _hasChange(JournalArticle journalArticle) {
+		if (journalArticle == null) {
+			return false;
+		}
+
 		Optional<CTEntry> ctEntryOptional =
 			_ctManager.getModelChangeCTEntryOptional(
 				PrincipalThreadLocal.getUserId(),
