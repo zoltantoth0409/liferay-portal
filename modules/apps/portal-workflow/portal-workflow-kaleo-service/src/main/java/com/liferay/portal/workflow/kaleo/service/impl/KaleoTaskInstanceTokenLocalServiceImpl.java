177d50173b9643fa04d2a574a59c979e2c84da92
Inacio Nery <inacionery@gmail.com>
2019-02-06 14:43:39 -0300

    LPS-90082 Add kaleoInstanceId in query to find KaleoTaskInstanceTokens

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoTaskInstanceTokenLocalServiceImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoTaskInstanceTokenLocalServiceImpl.java
@@ -421,6 +421,7 @@ public class KaleoTaskInstanceTokenLocalServiceImpl
 		kaleoTaskInstanceTokenQuery.setAssigneeClassPK(
 			serviceContext.getUserId());
 		kaleoTaskInstanceTokenQuery.setCompleted(completed);
+		kaleoTaskInstanceTokenQuery.setKaleoInstanceId(kaleoInstanceId);
 
 		return kaleoTaskInstanceTokenFinder.countKaleoTaskInstanceTokens(
 			kaleoTaskInstanceTokenQuery);
