6d75b9bb5bf6a23981d573df5ed4e8c93c86a0d9
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-02-07 14:15:10 +0100

    LPS-90233 Adding new change tracking entry resource skeleton

---
+++ modules/apps/change-tracking/change-tracking-rest/src/main/java/com/liferay/change/tracking/rest/internal/resource/CTEntryResource.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.rest.internal.resource;
+
+import javax.ws.rs.Path;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Daniel Kocsis
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=change-tracking-application)",
+		"osgi.jaxrs.resource=true"
+	},
+	scope = ServiceScope.PROTOTYPE, service = CTEntryResource.class
+)
+@Path("/collections/{collectionId}/entries")
+public class CTEntryResource {
+}
