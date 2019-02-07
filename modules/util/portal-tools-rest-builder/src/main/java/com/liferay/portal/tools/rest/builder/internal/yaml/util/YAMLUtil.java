f12f12ea84bb166fe16d7289f2285c18ccf843f2
Preston-Crary <preston.crary@liferay.com>
2019-02-07 11:48:33 -0800

    LPS-90074 Name the vars instead of commenting them.

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/yaml/util/YAMLUtil.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/yaml/util/YAMLUtil.java
@@ -40,14 +40,13 @@ public class YAMLUtil {
 	public static ConfigYAML loadConfigYAML(String yamlString) {
 		List<TypeDescription> typeDescriptions = new ArrayList<>();
 
-		// Security
+		TypeDescription securityTypeDescription = new TypeDescription(
+			Security.class);
 
-		TypeDescription typeDescription = new TypeDescription(Security.class);
-
-		typeDescription.substituteProperty(
+		securityTypeDescription.substituteProperty(
 			"oAuth2", String.class, "getOAuth2", "setOAuth2");
 
-		typeDescriptions.add(typeDescription);
+		typeDescriptions.add(securityTypeDescription);
 
 		TypeDescription[] typeDescriptionsArray = typeDescriptions.toArray(
 			new TypeDescription[typeDescriptions.size()]);
@@ -58,66 +57,63 @@ public class YAMLUtil {
 	public static OpenAPIYAML loadOpenAPIYAML(String yamlString) {
 		List<TypeDescription> typeDescriptions = new ArrayList<>();
 
-		// Items
+		TypeDescription itemTypeDescription = new TypeDescription(Items.class);
 
-		TypeDescription typeDescription = new TypeDescription(Items.class);
-
-		typeDescription.substituteProperty(
+		itemTypeDescription.substituteProperty(
 			"$ref", String.class, "getReference", "setReference");
 
-		typeDescriptions.add(typeDescription);
+		typeDescriptions.add(itemTypeDescription);
 
-		// Open API YAML
+		TypeDescription openAPIYAMLTypeDescription = new TypeDescription(
+			OpenAPIYAML.class);
 
-		typeDescription = new TypeDescription(OpenAPIYAML.class);
-
-		typeDescription.substituteProperty(
+		openAPIYAMLTypeDescription.substituteProperty(
 			"paths", Map.class, "getPathItems", "setPathItems");
 
-		typeDescription.addPropertyParameters(
+		openAPIYAMLTypeDescription.addPropertyParameters(
 			"paths", String.class, PathItem.class);
 
-		typeDescriptions.add(typeDescription);
+		typeDescriptions.add(openAPIYAMLTypeDescription);
 
-		// Parameter
+		TypeDescription parameterTypeDescription = new TypeDescription(
+			Parameter.class);
 
-		typeDescription = new TypeDescription(Parameter.class);
-
-		typeDescription.substituteProperty(
+		parameterTypeDescription.substituteProperty(
 			"$ref", String.class, "getReference", "setReference");
 
-		typeDescriptions.add(typeDescription);
+		typeDescriptions.add(parameterTypeDescription);
 
 		// Schema
 
-		typeDescription = new TypeDescription(Schema.class);
+		TypeDescription schemaTypeDescription = new TypeDescription(
+			Schema.class);
 
-		typeDescription.substituteProperty(
+		schemaTypeDescription.substituteProperty(
 			"$ref", String.class, "getReference", "setReference");
 
-		typeDescription.substituteProperty(
+		schemaTypeDescription.substituteProperty(
 			"allOf", List.class, "getAllOfSchemas", "setAllOfSchemas");
 
-		typeDescription.addPropertyParameters("allOf", Schema.class);
+		schemaTypeDescription.addPropertyParameters("allOf", Schema.class);
 
-		typeDescription.substituteProperty(
+		schemaTypeDescription.substituteProperty(
 			"anyOf", List.class, "getAnyOfSchemas", "setAnyOfSchemas");
 
-		typeDescription.addPropertyParameters("anyOf", Schema.class);
+		schemaTypeDescription.addPropertyParameters("anyOf", Schema.class);
 
-		typeDescription.substituteProperty(
+		schemaTypeDescription.substituteProperty(
 			"oneOf", List.class, "getOneOfSchemas", "setOneOfSchemas");
 
-		typeDescription.addPropertyParameters("oneOf", Schema.class);
+		schemaTypeDescription.addPropertyParameters("oneOf", Schema.class);
 
-		typeDescription.substituteProperty(
+		schemaTypeDescription.substituteProperty(
 			"properties", Map.class, "getPropertySchemas",
 			"setPropertySchemas");
 
-		typeDescription.addPropertyParameters(
+		schemaTypeDescription.addPropertyParameters(
 			"properties", String.class, Schema.class);
 
-		typeDescriptions.add(typeDescription);
+		typeDescriptions.add(schemaTypeDescription);
 
 		TypeDescription[] typeDescriptionsArray = typeDescriptions.toArray(
 			new TypeDescription[typeDescriptions.size()]);
