947cafd0ee82d7ef4c7a62a07a96c5927475bde9
Diana Lin <diana.lin@liferay.com>
2019-02-07 15:09:48 -0800

    LPS-60134 SF

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
@@ -774,7 +774,9 @@ public class StagedLayoutSetStagedModelDataHandler
 
 		Set<Long> parentPlids = new HashSet<>();
 
-		for (long plid : layoutPriorities.keySet()) {
+		Set<Long> updatedPlids = layoutPriorities.keySet();
+
+		for (long plid : updatedPlids) {
 			Layout layout = _layoutLocalService.fetchLayout(plid);
 
 			layout.setPriority(layoutPriorities.get(plid));
@@ -789,9 +791,7 @@ public class StagedLayoutSetStagedModelDataHandler
 				portletDataContext.getGroupId(), privateLayout, parentPlid);
 
 			for (Layout layout : siblingLayouts) {
-				Set<Long> plids = layoutPriorities.keySet();
-
-				if (!plids.contains(layout.getPlid())) {
+				if (!updatedPlids.contains(layout.getPlid())) {
 					if (siblingLayoutHasSamePriority(layout)) {
 						do {
 							int priority = layout.getPriority();
