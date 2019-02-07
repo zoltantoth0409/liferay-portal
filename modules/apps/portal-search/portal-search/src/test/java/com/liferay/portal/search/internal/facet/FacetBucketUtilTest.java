7081899e6d7dd94a05926e959d69a9cfc4674739
Bryan Engler <bryan.engler@liferay.com>
2019-02-07 12:33:04 -0800

    LPS-90187 portal-search: Add unit test to replicate the bug
    https://issues.liferay.com/browse/LPS-90187

--- modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/facet/FacetBucketUtilTest.java
+++ modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/facet/FacetBucketUtilTest.java
@@ -30,6 +30,16 @@ import org.junit.Test;
 public class FacetBucketUtilTest {
 
 	@Test
+	public void testFacetImpl() {
+		Field field = new Field(_FIELD_NAME, new String[] {"foo", "bar"});
+
+		Facet facet = new FacetImpl(_FIELD_NAME, null);
+
+		Assert.assertTrue(FacetBucketUtil.isFieldInBucket(field, "foo", facet));
+		Assert.assertTrue(FacetBucketUtil.isFieldInBucket(field, "bar", facet));
+	}
+
+	@Test
 	public void testMultiValueFacet() {
 		Field field = new Field(_FIELD_NAME, new String[] {"foo", "bar"});
 
