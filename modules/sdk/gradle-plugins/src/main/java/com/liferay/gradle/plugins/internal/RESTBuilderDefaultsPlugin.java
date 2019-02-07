4f4e7a8d848f8559c4d64836109d42f2aae307f3
Peter Shin <peter.shin@liferay.com>
2019-02-07 08:59:24 -0800

    LPS-89415 Copy yaml files to META-INF/yaml

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/RESTBuilderDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/RESTBuilderDefaultsPlugin.java
@@ -15,10 +15,17 @@
 package com.liferay.gradle.plugins.internal;
 
 import com.liferay.gradle.plugins.BasePortalToolDefaultsPlugin;
+import com.liferay.gradle.plugins.internal.util.GradleUtil;
+import com.liferay.gradle.plugins.rest.builder.BuildRESTTask;
 import com.liferay.gradle.plugins.rest.builder.RESTBuilderPlugin;
+
+import groovy.lang.Closure;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.file.CopySpec;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.tasks.Copy;
 
 /**
  * @author Peter Shin
@@ -28,6 +35,18 @@ public class RESTBuilderDefaultsPlugin
 
 	public static final Plugin<Project> INSTANCE =
 		new RESTBuilderDefaultsPlugin();
+
+	@Override
+	protected void configureDefaults(
+		final Project project, RESTBuilderPlugin restBuilderPlugin) {
+
+		super.configureDefaults(project, restBuilderPlugin);
+
+		BuildRESTTask buildRESTTask = (BuildRESTTask)GradleUtil.getTask(
+			project, RESTBuilderPlugin.BUILD_REST_TASK_NAME);
+
+		_configureTaskProcessResources(buildRESTTask);
+	}
 
 	@Override
 	protected Class<RESTBuilderPlugin> getPluginClass() {
@@ -45,6 +64,25 @@ public class RESTBuilderDefaultsPlugin
 	}
 
 	private RESTBuilderDefaultsPlugin() {
+	}
+
+	private void _configureTaskProcessResources(BuildRESTTask buildRESTTask) {
+		final Project project = buildRESTTask.getProject();
+
+		Copy copy = (Copy)GradleUtil.getTask(
+			project, JavaPlugin.PROCESS_RESOURCES_TASK_NAME);
+
+		copy.into(
+			"META-INF/yaml",
+			new Closure<Void>(copy) {
+
+				@SuppressWarnings("unused")
+				public void doCall(CopySpec copySpec) {
+					copySpec.from(project.getProjectDir());
+					copySpec.include("*.yaml");
+				}
+
+			});
 	}
 
 	private static final String _PORTAL_TOOL_NAME =
