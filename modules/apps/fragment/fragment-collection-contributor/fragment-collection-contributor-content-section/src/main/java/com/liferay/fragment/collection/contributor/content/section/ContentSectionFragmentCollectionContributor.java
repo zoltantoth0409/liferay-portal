036cdbf1bc7ed57cc2c576a8d53d95b184d9cf20
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-02-08 08:58:11 +0100

    LPS-90232 Apply changes to contributors

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-content-section/src/main/java/com/liferay/fragment/collection/contributor/content/section/ContentSectionFragmentCollectionContributor.java
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-content-section/src/main/java/com/liferay/fragment/collection/contributor/content/section/ContentSectionFragmentCollectionContributor.java
@@ -17,7 +17,10 @@ package com.liferay.fragment.collection.contributor.content.section;
 import com.liferay.fragment.contributor.BaseFragmentCollectionContributor;
 import com.liferay.fragment.contributor.FragmentCollectionContributor;
 
+import javax.servlet.ServletContext;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Jürgen Kappler
@@ -30,5 +33,15 @@ public class ContentSectionFragmentCollectionContributor
 	public String getFragmentCollectionKey() {
 		return "CONTENT_SECTION";
 	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.fragment.collection.contributor.content.section)"
+	)
+	private ServletContext _servletContext;
 
 }
