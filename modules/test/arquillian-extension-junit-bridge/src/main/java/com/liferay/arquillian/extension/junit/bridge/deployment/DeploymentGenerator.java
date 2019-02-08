9e1283c910566c4e2252d72c3f6ff3dbec2fb330
Matthew Tambara <matthew.tambara@liferay.com>
2019-02-07 16:59:48 -0800

    LPS-90208 Don't need to validate

--- modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/deployment/DeploymentGenerator.java
+++ modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/deployment/DeploymentGenerator.java
@@ -19,14 +19,10 @@ import java.util.Collection;
 import java.util.List;
 
 import org.jboss.arquillian.container.spi.Container;
-import org.jboss.arquillian.container.spi.ContainerRegistry;
+import org.jboss.arquillian.container.spi.client.container.DeployableContainer;
 import org.jboss.arquillian.container.spi.client.deployment.Deployment;
 import org.jboss.arquillian.container.spi.client.deployment.DeploymentDescription;
 import org.jboss.arquillian.container.spi.client.deployment.DeploymentScenario;
-import org.jboss.arquillian.container.spi.client.deployment.TargetDescription;
-import org.jboss.arquillian.container.spi.client.protocol.ProtocolDescription;
-import org.jboss.arquillian.container.test.api.TargetsContainer;
-import org.jboss.arquillian.container.test.impl.client.deployment.ValidationException;
 import org.jboss.arquillian.container.test.impl.client.deployment.event.GenerateDeployment;
 import org.jboss.arquillian.container.test.impl.domain.ProtocolDefinition;
 import org.jboss.arquillian.container.test.impl.domain.ProtocolRegistry;
@@ -64,8 +60,6 @@ public class DeploymentGenerator {
 
 			deploymentScenario.addDeployment(deploymentDescription);
 		}
-
-		_validate(deploymentScenario);
 
 		_createTestableDeployments(deploymentScenario, event.getTestClass());
 
@@ -189,104 +183,6 @@ public class DeploymentGenerator {
 		}
 
 		return archives;
-	}
-
-	private void _throwNoContainerFound(TargetDescription targetDescription) {
-		throw new ValidationException(
-			"DeploymentScenario contains a target (" +
-				targetDescription.getName() + ") not matching any defined " +
-					"Container in the registry.\nPlease include at least 1 " +
-						"Deployable Container on your Classpath.");
-	}
-
-	private void _throwNoMatchFound(
-		ContainerRegistry containerRegistry,
-		TargetDescription targetDescription) {
-
-		throw new ValidationException("DeploymentScenario contains a target (" + targetDescription.getName()
-			+ ") not matching any defined Container in the registry.\n" + "Possible causes are: None of the "
-			+ containerRegistry.getContainers().size() + " Containers are marked as default or you have defined a " + "@"
-			+ org.jboss.arquillian.container.test.api.Deployment.class.getSimpleName() + " with a @"
-			+ TargetsContainer.class.getSimpleName() + " of value (" + targetDescription.getName() + ") that "
-			+ "does not match any found/configured Containers (" + _toString(containerRegistry) +
-			"), see arquillian.xml container@qualifier");
-	}
-
-	private void _throwTargetNotFoundValidationException(
-		ContainerRegistry containerRegistry,
-		TargetDescription targetDescription) {
-
-		List<Container> containers = containerRegistry.getContainers();
-
-		if (containers.isEmpty()) {
-			_throwNoContainerFound(targetDescription);
-		}
-
-		_throwNoMatchFound(containerRegistry, targetDescription);
-	}
-
-	private String _toString(ContainerRegistry containerRegistry) {
-		StringBuilder sb = new StringBuilder();
-
-		for (Container container : containerRegistry.getContainers()) {
-			sb.append(container.getName());
-			sb.append(".");
-		}
-
-		if (sb.length() > 0) {
-			sb.deleteCharAt(sb.length() - 1);
-		}
-
-		return sb.toString();
-	}
-
-	private void _validate(DeploymentScenario deploymentScenario) {
-		ContainerRegistry containerRegistry = _containerRegistryInstance.get();
-
-		for (TargetDescription target : deploymentScenario.targets()) {
-			Container container = containerRegistry.getContainer(target);
-
-			if (container == null) {
-				_throwTargetNotFoundValidationException(
-					containerRegistry, target);
-			}
-		}
-
-		for (Deployment deployment : deploymentScenario.deployments()) {
-			Container container = containerRegistry.getContainer(
-				deployment.getDescription().getTarget());
-
-			if ("custom".equalsIgnoreCase(
-					container.getContainerConfiguration().getMode())) {
-
-				if (deployment.getDescription().managed()) {
-					throw new ValidationException(
-							"Deployment " + deployment.getDescription().getName() + " is targeted against container " +
-							container.getName() + ". This container is set to mode custom which can not handle managed deployments. " +
-							"Please verify the @" + TargetsContainer.class.getName() + " annotation or container@mode in arquillian.xml");
-				}
-			}
-		}
-
-		ProtocolRegistry protocolRegistry = _protocolRegistryInstance.get();
-
-		for (ProtocolDescription protocolDescription :
-				deploymentScenario.protocols()) {
-
-			if (ProtocolDescription.DEFAULT.equals(protocolDescription)) {
-				continue;
-			}
-
-			ProtocolDefinition protocol = protocolRegistry.getProtocol(
-				protocolDescription);
-
-			if (protocol == null) {
-				throw new ValidationException(
-					DeploymentScenario.class.getSimpleName() + " contains " +
-						"protocols not maching any defined Protocol in the " +
-							"registry. " + protocolDescription.getName());
-			}
-		}
 	}
 
 	@Inject
