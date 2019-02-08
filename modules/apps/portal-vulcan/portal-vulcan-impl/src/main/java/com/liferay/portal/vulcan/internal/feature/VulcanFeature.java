5332af9bb5d834ad670aca26ba469abfef93bb7b
Alejandro Hernández <info@alejandrohdezma.com>
2019-02-08 14:14:09 +0100

    LPS-90226 Provide all the extensions in VulcanFeature

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/feature/VulcanFeature.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/feature/VulcanFeature.java
@@ -14,10 +14,22 @@
 
 package com.liferay.portal.vulcan.internal.feature;
 
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.vulcan.context.AcceptLanguage;
+import com.liferay.portal.vulcan.internal.context.provider.PaginationContextProvider;
+import com.liferay.portal.vulcan.internal.exception.mapper.NoSuchModelExceptionMapper;
+import com.liferay.portal.vulcan.internal.exception.mapper.PortalExceptionMapper;
+import com.liferay.portal.vulcan.internal.exception.mapper.PrincipalExceptionMapper;
+import com.liferay.portal.vulcan.internal.jaxrs.json.JSONMessageBodyReader;
+import com.liferay.portal.vulcan.internal.jaxrs.json.JSONMessageBodyWriter;
+
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 
+import org.apache.cxf.jaxrs.ext.ContextProvider;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
 
@@ -40,7 +52,27 @@ public class VulcanFeature implements Feature {
 
 	@Override
 	public boolean configure(FeatureContext featureContext) {
+		featureContext.register(JSONMessageBodyReader.class);
+		featureContext.register(JSONMessageBodyWriter.class);
+		featureContext.register(NoSuchModelExceptionMapper.class);
+		featureContext.register(PaginationContextProvider.class);
+		featureContext.register(PortalExceptionMapper.class);
+		featureContext.register(PrincipalExceptionMapper.class);
+
+		featureContext.register(_acceptLanguageContextProvider);
+		featureContext.register(_companyContextProvider);
+
 		return false;
 	}
+
+	@Reference(
+		target = "(osgi.jaxrs.name=Liferay.Vulcan.AcceptLanguageContextProvider)"
+	)
+	private ContextProvider<AcceptLanguage> _acceptLanguageContextProvider;
+
+	@Reference(
+		target = "(osgi.jaxrs.name=Liferay.Vulcan.CompanyContextProvider)"
+	)
+	private ContextProvider<Company> _companyContextProvider;
 
 }
