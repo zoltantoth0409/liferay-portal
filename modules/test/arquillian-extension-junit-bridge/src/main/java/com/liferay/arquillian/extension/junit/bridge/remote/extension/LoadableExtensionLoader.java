0a3836ad337a689a4946cfac57baadafafe01740
Matthew Tambara <matthew.tambara@liferay.com>
2019-02-11 15:51:29 -0800

    LPS-90519 Only have one LoadableExtension

--- modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/remote/extension/LoadableExtensionLoader.java
+++ modules/test/arquillian-extension-junit-bridge/src/main/java/com/liferay/arquillian/extension/junit/bridge/remote/extension/LoadableExtensionLoader.java
@@ -14,9 +14,7 @@
 
 package com.liferay.arquillian.extension.junit.bridge.remote.extension;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
+import com.liferay.arquillian.extension.junit.bridge.LiferayArquillianJUnitBridgeExtension;
 
 import org.jboss.arquillian.core.api.Injector;
 import org.jboss.arquillian.core.api.Instance;
@@ -24,10 +22,7 @@ import org.jboss.arquillian.core.api.InstanceProducer;
 import org.jboss.arquillian.core.api.annotation.ApplicationScoped;
 import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.core.api.annotation.Observes;
-import org.jboss.arquillian.core.impl.loadable.JavaSPIExtensionLoader;
-import org.jboss.arquillian.core.impl.loadable.SecurityActions;
 import org.jboss.arquillian.core.impl.loadable.ServiceRegistry;
-import org.jboss.arquillian.core.spi.ExtensionLoader;
 import org.jboss.arquillian.core.spi.LoadableExtension;
 import org.jboss.arquillian.core.spi.ServiceLoader;
 import org.jboss.arquillian.core.spi.context.Context;
@@ -38,121 +33,61 @@ import org.jboss.arquillian.core.spi.event.ManagerProcessing;
  */
 public class LoadableExtensionLoader {
 
-	public LoadableExtensionLoader() {
-		this(new JavaSPIExtensionLoader());
-	}
-
 	public void load(@Observes final ManagerProcessing managerProcessing) {
-		ExtensionLoader extensionLoader = _locateExtensionLoader();
+		LoadableExtension loadableExtension =
+			new LiferayArquillianJUnitBridgeExtension();
 
 		ServiceRegistry serviceRegistry = new ServiceRegistry(
 			_injectorInstance.get());
 
-		Collection<LoadableExtension> loadableExtensions =
-			extensionLoader.load();
+		loadableExtension.register(
+			new LoadableExtension.ExtensionBuilder() {
 
-		for (LoadableExtension loadableExtension : loadableExtensions) {
-			loadableExtension.register(
-				new LoadableExtension.ExtensionBuilder() {
+				@Override
+				public LoadableExtension.ExtensionBuilder context(
+					Class<? extends Context> context) {
 
-					@Override
-					public LoadableExtension.ExtensionBuilder context(
-						Class<? extends Context> context) {
+					managerProcessing.context(context);
 
-						managerProcessing.context(context);
+					return this;
+				}
 
-						return this;
-					}
+				@Override
+				public LoadableExtension.ExtensionBuilder observer(
+					Class<?> handler) {
 
-					@Override
-					public LoadableExtension.ExtensionBuilder observer(
-						Class<?> handler) {
+					managerProcessing.observer(handler);
 
-						managerProcessing.observer(handler);
+					return this;
+				}
 
-						return this;
-					}
+				@Override
+				public <T> LoadableExtension.ExtensionBuilder override(
+					Class<T> service, Class<? extends T> oldServiceImpl,
+					Class<? extends T> newServiceImpl) {
 
-					@Override
-					public <T> LoadableExtension.ExtensionBuilder override(
-						Class<T> service, Class<? extends T> oldServiceImpl,
-						Class<? extends T> newServiceImpl) {
+					serviceRegistry.overrideService(
+						service, oldServiceImpl, newServiceImpl);
 
-						serviceRegistry.overrideService(
-							service, oldServiceImpl, newServiceImpl);
+					return this;
+				}
 
-						return this;
-					}
+				@Override
+				public <T> LoadableExtension.ExtensionBuilder service(
+					Class<T> service, Class<? extends T> impl) {
 
-					@Override
-					public <T> LoadableExtension.ExtensionBuilder service(
-						Class<T> service, Class<? extends T> impl) {
+					serviceRegistry.addService(service, impl);
 
-						serviceRegistry.addService(service, impl);
+					return this;
+				}
 
-						return this;
-					}
-
-				});
-		}
+			});
 
 		_serviceLoaderInstanceProducer.set(serviceRegistry.getServiceLoader());
 	}
 
-	private LoadableExtensionLoader(
-		JavaSPIExtensionLoader javaSPIExtensionLoader) {
-
-		_javaSPIExtensionLoader = javaSPIExtensionLoader;
-	}
-
-	private ExtensionLoader _locateExtensionLoader() {
-		Collection<ExtensionLoader> loaders = Collections.emptyList();
-
-		if (SecurityActions.getThreadContextClassLoader() != null) {
-			loaders = _javaSPIExtensionLoader.all(
-				SecurityActions.getThreadContextClassLoader(),
-				ExtensionLoader.class);
-		}
-
-		if (loaders.isEmpty()) {
-			loaders = _javaSPIExtensionLoader.all(
-				LoadableExtensionLoader.class.getClassLoader(),
-				ExtensionLoader.class);
-		}
-
-		if (loaders.size() > 1) {
-			throw new IllegalArgumentException(
-				"Multiple ExtensionLoader's found on classpath: " +
-					_toString(loaders));
-		}
-
-		if (loaders.size() == 1) {
-			Iterator<ExtensionLoader> iterator = loaders.iterator();
-
-			return iterator.next();
-		}
-
-		return _javaSPIExtensionLoader;
-	}
-
-	private String _toString(Collection<ExtensionLoader> extensionLoaders) {
-		StringBuilder sb = new StringBuilder();
-
-		for (ExtensionLoader loader : extensionLoaders) {
-			Class<?> clazz = loader.getClass();
-
-			sb.append(clazz.getName());
-
-			sb.append(", ");
-		}
-
-		return sb.toString();
-	}
-
 	@Inject
 	private Instance<Injector> _injectorInstance;
-
-	private JavaSPIExtensionLoader _javaSPIExtensionLoader;
 
 	@ApplicationScoped
 	@Inject
