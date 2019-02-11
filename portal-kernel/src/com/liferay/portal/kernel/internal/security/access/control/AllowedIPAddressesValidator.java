41a285e97f323cb04213717b519034ce430a5406
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2019-02-11 09:46:45 +0100

    LPS-90155 Create classes to validate according to ranges.

---
+++ portal-kernel/src/com/liferay/portal/kernel/internal/security/access/control/AllowedIPAddressesValidator.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.internal.security.access.control;
+
+/**
+* @author Carlos Sierra
+* @author Mariano Álvaro Sáiz
+*/
+public interface AllowedIPAddressesValidator {
+
+	public byte[] getEmptyNetmask();
+
+	public boolean isAllowedIPAddress(String ipAddress);
+
+	public boolean isSameProtocol(byte[] ipAddressBytes);
+
+	public boolean isValidCIDRNetmask(int cidr);
+
+	public boolean isValidDotNotationNetmask(String dotNotationNetmask);
+
+}
