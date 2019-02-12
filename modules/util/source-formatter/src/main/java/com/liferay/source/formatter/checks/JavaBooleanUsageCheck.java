fd96cb39054ac26d65fdfdf0d17073b56dc0d64d
Hugo Huijser <hugo.huijser@liferay.com>
2019-02-12 19:31:29 +0800

    LPS-84119 Fix IndexOutOfBoundsException

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaBooleanUsageCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaBooleanUsageCheck.java
@@ -14,13 +14,12 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.tools.ToolsUtil;
 import com.liferay.source.formatter.checks.util.JavaSourceUtil;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author Hugo Huijser
@@ -37,24 +36,47 @@ public class JavaBooleanUsageCheck extends BaseFileCheck {
 	}
 
 	private String _fixIncorrectBooleanUse(String content, String methodName) {
-		Pattern pattern = Pattern.compile(
-			"\\." + methodName + "\\((.*?)\\);\n", Pattern.DOTALL);
+		int x = -1;
 
-		Matcher matcher = pattern.matcher(content);
+		while (true) {
+			x = content.indexOf("." + methodName + "(", x + 1);
 
-		while (matcher.find()) {
-			if (ToolsUtil.isInsideQuotes(content, matcher.start())) {
+			if (x == -1) {
+				return content;
+			}
+
+			if (ToolsUtil.isInsideQuotes(content, x)) {
 				continue;
 			}
 
-			String match = matcher.group();
+			String methodCall = null;
 
-			if (match.contains("\t//")) {
+			int y = x;
+
+			while (true) {
+				y = content.indexOf(")", y + 1);
+
+				if (y == -1) {
+					return content;
+				}
+
+				methodCall = content.substring(x, y + 1);
+
+				if (!ToolsUtil.isInsideQuotes(content, y) &&
+					(getLevel(methodCall) == 0)) {
+
+					break;
+				}
+			}
+
+			if (methodCall.contains("\t//") ||
+				(content.charAt(y + 1) != CharPool.SEMICOLON)) {
+
 				continue;
 			}
 
 			List<String> parametersList = JavaSourceUtil.getParameterList(
-				match);
+				methodCall);
 
 			if (parametersList.size() != 2) {
 				continue;
@@ -66,14 +88,12 @@ public class JavaBooleanUsageCheck extends BaseFileCheck {
 				secondParameterName.equals("true")) {
 
 				String replacement = StringUtil.replaceLast(
-					match, secondParameterName,
+					methodCall, secondParameterName,
 					"Boolean." + StringUtil.toUpperCase(secondParameterName));
 
-				return StringUtil.replace(content, match, replacement);
+				return StringUtil.replace(content, methodCall, replacement);
 			}
 		}
-
-		return content;
 	}
 
 }
