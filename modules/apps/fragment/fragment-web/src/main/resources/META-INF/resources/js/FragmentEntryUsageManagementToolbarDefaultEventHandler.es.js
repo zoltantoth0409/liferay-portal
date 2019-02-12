0065a963b00ed6210bbb5b742db23ec1d81b61df
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-02-12 17:44:26 +0100

    LPS-90063 Display context and default event handler for the management tool that is used by fragment entry usages

---
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/FragmentEntryUsageManagementToolbarDefaultEventHandler.es.js
@@ -0,0 +1,27 @@
+import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
+import {Config} from 'metal-state';
+
+class FragmentEntryUsageManagementToolbarDefaultEventHandler extends PortletBase {
+	callAction(event) {
+		var itemData = event.data.item.data;
+
+		if (itemData && itemData.action && this[itemData.action]) {
+			this[itemData.action](itemData);
+		}
+	}
+
+	propagate() {
+		submitForm(this.one('#fm'));
+	}
+
+
+	handleActionItemClicked(event) {
+		this.callAction(event);
+	}
+}
+
+FragmentEntryUsageManagementToolbarDefaultEventHandler.STATE = {
+	namespace: Config.string()
+};
+
+export default FragmentEntryUsageManagementToolbarDefaultEventHandler;
