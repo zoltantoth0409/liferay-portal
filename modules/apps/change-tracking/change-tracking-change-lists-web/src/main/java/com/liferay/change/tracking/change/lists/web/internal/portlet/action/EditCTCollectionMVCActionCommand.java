861936a6af3e33da63c85b05452ec35a55a15363
jpince <jason.pince@liferay.com>
2019-02-12 10:40:09 -0800

    LPS-89547 Auto SF

--- modules/apps/change-tracking/change-tracking-change-lists-web/src/main/java/com/liferay/change/tracking/change/lists/web/internal/portlet/action/EditCTCollectionMVCActionCommand.java
+++ modules/apps/change-tracking/change-tracking-change-lists-web/src/main/java/com/liferay/change/tracking/change/lists/web/internal/portlet/action/EditCTCollectionMVCActionCommand.java
@@ -55,7 +55,6 @@ public class EditCTCollectionMVCActionCommand extends BaseMVCActionCommand {
 			WebKeys.THEME_DISPLAY);
 
 		long ctCollectionId = ParamUtil.getLong(
-
 		actionRequest, "ctCollectionId");
 		String description = ParamUtil.getString(actionRequest, "description");
 		String name = ParamUtil.getString(actionRequest, "name");
