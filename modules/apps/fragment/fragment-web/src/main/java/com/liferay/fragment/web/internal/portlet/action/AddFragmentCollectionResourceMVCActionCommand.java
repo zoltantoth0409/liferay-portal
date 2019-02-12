58d23ad4c71ffae1e412dc0576c79969e2584a0d
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-02-12 12:38:14 +0100

    LPS-90063 Action command and file entry handler for uploading resources

---
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/AddFragmentCollectionResourceMVCActionCommand.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.portlet.action;
+
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.service.FragmentCollectionService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.TempFileEntryUtil;
+import com.liferay.upload.UniqueFileNameProvider;
+
+import java.io.InputStream;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + FragmentPortletKeys.FRAGMENT,
+		"mvc.command.name=/fragment/add_fragment_collection_resource"
+	},
+	service = MVCActionCommand.class
+)
+public class AddFragmentCollectionResourceMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long fragmentCollectionId = ParamUtil.getLong(
+			actionRequest, "fragmentCollectionId");
+
+		FragmentCollection fragmentCollection =
+			_fragmentCollectionService.fetchFragmentCollection(
+				fragmentCollectionId);
+
+		if (fragmentCollection == null) {
+			return;
+		}
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			actionRequest);
+
+		long fileEntryId = ParamUtil.getLong(actionRequest, "fileEntryId");
+
+		FileEntry fileEntry = _dlAppLocalService.getFileEntry(fileEntryId);
+
+		FileEntry tempFileEntry = fileEntry;
+
+		byte[] bytes = null;
+
+		try (InputStream is = tempFileEntry.getContentStream()) {
+			bytes = FileUtil.getBytes(is);
+		}
+
+		String uniqueFileName = _uniqueFileNameProvider.provide(
+			fileEntry.getFileName(),
+			curFileName -> _exists(
+				serviceContext, fragmentCollection, curFileName));
+
+		_dlAppLocalService.addFileEntry(
+			serviceContext.getUserId(), serviceContext.getScopeGroupId(),
+			fragmentCollection.getResourcesFolderId(), uniqueFileName, null,
+			uniqueFileName, StringPool.BLANK, StringPool.BLANK, bytes,
+			serviceContext);
+
+		TempFileEntryUtil.deleteTempFileEntry(tempFileEntry.getFileEntryId());
+
+		String redirect = ParamUtil.getString(actionRequest, "redirect");
+
+		sendRedirect(actionRequest, actionResponse, redirect);
+	}
+
+	private boolean _exists(
+		ServiceContext serviceContext, FragmentCollection fragmentCollection,
+		String curFileName) {
+
+		try {
+			if (_dlAppLocalService.getFileEntry(
+					serviceContext.getScopeGroupId(),
+					fragmentCollection.getResourcesFolderId(),
+					curFileName) != null) {
+
+				return true;
+			}
+
+			return false;
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+
+			return false;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AddFragmentCollectionResourceMVCActionCommand.class);
+
+	@Reference
+	private DLAppLocalService _dlAppLocalService;
+
+	@Reference
+	private FragmentCollectionService _fragmentCollectionService;
+
+	@Reference
+	private UniqueFileNameProvider _uniqueFileNameProvider;
+
+}
