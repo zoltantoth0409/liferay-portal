e744201fe532d1e683ba99ad0e86e1dab9d1b651
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-02-12 14:04:08 +0100

    LPS-90063 Dropdown items provider and event handler for vertical card item actions

---
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/servlet/taglib/util/FragmentCollectionResourceActionDropdownItemsProvider.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.servlet.taglib.util;
+
+import com.liferay.fragment.constants.FragmentActionKeys;
+import com.liferay.fragment.web.internal.security.permission.resource.FragmentPermission;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemList;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class FragmentCollectionResourceActionDropdownItemsProvider {
+
+	public FragmentCollectionResourceActionDropdownItemsProvider(
+		FileEntry fileEntry, RenderRequest renderRequest,
+		RenderResponse renderResponse) {
+
+		_fileEntry = fileEntry;
+		_renderResponse = renderResponse;
+
+		_request = PortalUtil.getHttpServletRequest(renderRequest);
+
+		_themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	public List<DropdownItem> getActionDropdownItems() {
+		return new DropdownItemList() {
+			{
+				if (FragmentPermission.contains(
+						_themeDisplay.getPermissionChecker(),
+						_themeDisplay.getScopeGroupId(),
+						FragmentActionKeys.MANAGE_FRAGMENT_ENTRIES)) {
+
+					add(_getDeleteFragmentCollectionResourceActionConsumer());
+				}
+			}
+		};
+	}
+
+	private Consumer<DropdownItem>
+		_getDeleteFragmentCollectionResourceActionConsumer() {
+
+		PortletURL deleteFragmentCollectionResourceURL =
+			_renderResponse.createActionURL();
+
+		deleteFragmentCollectionResourceURL.setParameter(
+			ActionRequest.ACTION_NAME,
+			"/fragment/delete_fragment_collection_resources");
+
+		deleteFragmentCollectionResourceURL.setParameter(
+			"redirect", _themeDisplay.getURLCurrent());
+		deleteFragmentCollectionResourceURL.setParameter(
+			"fileEntryId", String.valueOf(_fileEntry.getFileEntryId()));
+
+		return dropdownItem -> {
+			dropdownItem.putData("action", "deleteFragmentCollectionResource");
+			dropdownItem.putData(
+				"deleteFragmentCollectionResourceURL",
+				deleteFragmentCollectionResourceURL.toString());
+			dropdownItem.setLabel(LanguageUtil.get(_request, "delete"));
+		};
+	}
+
+	private final FileEntry _fileEntry;
+	private final RenderResponse _renderResponse;
+	private final HttpServletRequest _request;
+	private final ThemeDisplay _themeDisplay;
+
+}
---
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/FragmentCollectionResourceDropdownDefaultEventHandler.es.js
@@ -0,0 +1,25 @@
+import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
+import {Config} from 'metal-state';
+
+class FragmentCollectionResourceDropdownDefaultEventHandler extends PortletBase {
+	handleItemClicked(event) {
+		const itemData = event.data.item.data;
+
+		if (itemData && itemData.action && this[itemData.action]) {
+			this[itemData.action](itemData);
+		}
+	}
+
+	deleteFragmentCollectionResource(itemData) {
+		if (confirm(Liferay.Language.get('are-you-sure-you-want-to-delete-this'))) {
+			submitForm(document.hrefFm, itemData.deleteFragmentCollectionResourceURL);
+			this._send();
+		}
+	}
+}
+
+FragmentCollectionResourceDropdownDefaultEventHandler.STATE = {
+	namespace: Config.string()
+};
+
+export default FragmentCollectionResourceDropdownDefaultEventHandler;
