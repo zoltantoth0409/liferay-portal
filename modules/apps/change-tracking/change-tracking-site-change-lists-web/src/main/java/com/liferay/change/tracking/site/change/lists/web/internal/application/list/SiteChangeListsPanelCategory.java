2a4dfbfc6e3909de3ec6dcabce774041c150829b
Máté Thurzó <mate.thurzo@liferay.com>
2019-02-12 13:00:59 +0100

    LPS-89493 Reorder to be above publishing

--- modules/apps/change-tracking/change-tracking-site-change-lists-web/src/main/java/com/liferay/change/tracking/site/change/lists/web/internal/application/list/SiteChangeListsPanelCategory.java
+++ modules/apps/change-tracking/change-tracking-site-change-lists-web/src/main/java/com/liferay/change/tracking/site/change/lists/web/internal/application/list/SiteChangeListsPanelCategory.java
@@ -33,7 +33,7 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION,
-		"panel.category.order:Integer=400"
+		"panel.category.order:Integer=700"
 	},
 	service = PanelCategory.class
 )
