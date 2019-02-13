677c4448f4b1f27eb03ac2a759248a0172c1d8c0
Javier Gamarra <nhpatt@gmail.com>
2019-02-14 00:53:31 +0100

    LPS-89752 migrate comment API for web experience

--- modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/v1_0/CommentResourceImpl.java
+++ modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/v1_0/CommentResourceImpl.java
@@ -14,9 +14,26 @@
 
 package com.liferay.headless.web.experience.internal.resource.v1_0;
 
+import com.liferay.headless.web.experience.dto.v1_0.Comment;
+import com.liferay.headless.web.experience.internal.dto.v1_0.CommentUtil;
 import com.liferay.headless.web.experience.resource.v1_0.CommentResource;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleService;
+import com.liferay.portal.kernel.comment.CommentManager;
+import com.liferay.portal.kernel.comment.DiscussionPermission;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.Collections;
+
+import javax.ws.rs.NotFoundException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +44,102 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = CommentResource.class
 )
 public class CommentResourceImpl extends BaseCommentResourceImpl {
+
+	@Override
+	public Comment getComment(Long commentId) throws Exception {
+		com.liferay.portal.kernel.comment.Comment comment =
+			_commentManager.fetchComment(commentId);
+
+		_checkViewPermission(
+			comment, PermissionThreadLocal.getPermissionChecker());
+
+		return CommentUtil.toComment(comment);
+	}
+
+	@Override
+	public Page<Comment> getCommentCommentsPage(
+			Long commentId, Pagination pagination)
+		throws Exception {
+
+		_checkViewPermission(
+			_commentManager.fetchComment(commentId),
+			PermissionThreadLocal.getPermissionChecker());
+
+		return Page.of(
+			transform(
+				_commentManager.getChildComments(
+					commentId, WorkflowConstants.STATUS_APPROVED,
+					pagination.getStartPosition(), pagination.getEndPosition()),
+				CommentUtil::toComment),
+			pagination,
+			_commentManager.getChildCommentsCount(
+				commentId, WorkflowConstants.STATUS_APPROVED));
+	}
+
+	@Override
+	public Page<Comment> getStructuredContentCommentsPage(
+			Long structuredContentId, Pagination pagination)
+		throws Exception {
+
+		JournalArticle journalArticle = _journalArticleService.getLatestArticle(
+			structuredContentId);
+
+		int count = _commentManager.getRootCommentsCount(
+			journalArticle.getModelClassName(), structuredContentId,
+			WorkflowConstants.STATUS_APPROVED);
+
+		if (count == 0) {
+			return Page.of(Collections.emptyList());
+		}
+
+		_checkViewPermission(
+			PermissionThreadLocal.getPermissionChecker(),
+			journalArticle.getGroupId(), journalArticle.getModelClassName(),
+			structuredContentId);
+
+		return Page.of(
+			transform(
+				_commentManager.getRootComments(
+					journalArticle.getModelClassName(), structuredContentId,
+					WorkflowConstants.STATUS_APPROVED,
+					pagination.getStartPosition(), pagination.getEndPosition()),
+				CommentUtil::toComment),
+			pagination, count);
+	}
+
+	private void _checkViewPermission(
+			com.liferay.portal.kernel.comment.Comment comment,
+			PermissionChecker permissionChecker)
+		throws PortalException {
+
+		if (comment == null) {
+			throw new NotFoundException();
+		}
+
+		DiscussionPermission discussionPermission =
+			_commentManager.getDiscussionPermission(permissionChecker);
+
+		discussionPermission.checkViewPermission(
+			permissionChecker.getCompanyId(), comment.getGroupId(),
+			comment.getClassName(), comment.getClassPK());
+	}
+
+	private void _checkViewPermission(
+			PermissionChecker permissionChecker, long groupId, String className,
+			long classPK)
+		throws PortalException {
+
+		DiscussionPermission discussionPermission =
+			_commentManager.getDiscussionPermission(permissionChecker);
+
+		discussionPermission.checkViewPermission(
+			permissionChecker.getCompanyId(), groupId, className, classPK);
+	}
+
+	@Reference
+	private CommentManager _commentManager;
+
+	@Reference
+	private JournalArticleService _journalArticleService;
+
 }
