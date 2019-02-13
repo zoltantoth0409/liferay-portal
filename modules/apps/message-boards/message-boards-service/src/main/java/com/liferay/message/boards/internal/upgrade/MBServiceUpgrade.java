ea1068a7f11be2f13f1f2ef0e632a75b6d1cb3c5
tina.tian <tina.tian@liferay.com>
2019-02-13 08:57:03 -0800

    Revert "LPS-89456 Add upgrade process - message-boards-service"
    This reverts commit 335b3285ad7122d9fa424ebad560af10e09488f5.

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/upgrade/MBServiceUpgrade.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/upgrade/MBServiceUpgrade.java
@@ -25,7 +25,6 @@ import com.liferay.message.boards.internal.upgrade.v2_0_0.util.MBMessageTable;
 import com.liferay.message.boards.internal.upgrade.v2_0_0.util.MBStatsUserTable;
 import com.liferay.message.boards.internal.upgrade.v2_0_0.util.MBThreadFlagTable;
 import com.liferay.message.boards.internal.upgrade.v2_0_0.util.MBThreadTable;
-import com.liferay.message.boards.internal.upgrade.v3_0_0.UpgradeBadColumnNames;
 import com.liferay.portal.kernel.service.ResourceActionLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
@@ -64,8 +63,6 @@ public class MBServiceUpgrade implements UpgradeStepRegistrator {
 					MBMessageTable.class, MBStatsUserTable.class,
 					MBThreadFlagTable.class, MBThreadTable.class
 				}));
-
-		registry.register("2.0.0", "3.0.0", new UpgradeBadColumnNames());
 	}
 
 	@Reference
