570719460b2142aa51c5055b2ff88120aa646779
tina.tian <tina.tian@liferay.com>
2019-02-13 09:13:52 -0800

    Revert "LPS-89456 Update schema version dependency for private module"
    This reverts commit 812e822b6bffea38fadff1a4f4f18f41858faa13.

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/upgrade/KaleoDesignerWebUpgrade.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/upgrade/KaleoDesignerWebUpgrade.java
@@ -71,7 +71,7 @@ public class KaleoDesignerWebUpgrade implements UpgradeStepRegistrator {
 		_kaleoDefinitionVersionLocalService;
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.portal.workflow.kaleo.service)(&(release.schema.version>=3.0.0)(!(release.schema.version>=3.1.0))))"
+		target = "(&(release.bundle.symbolic.name=com.liferay.portal.workflow.kaleo.service)(&(release.schema.version>=2.0.0)(!(release.schema.version>=2.1.0))))"
 	)
 	private Release _release;
 
