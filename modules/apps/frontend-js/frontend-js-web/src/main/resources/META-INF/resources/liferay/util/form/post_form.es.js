91c7723bee5bfabd915d749d94d2f174d1a9a1a1
Marko Čikoš <marko.cikos@liferay.com>
2019-02-13 16:41:24 +0100

    LPS-90334 frontend-js-web - Add capability to have an optional url parameter in postForm

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/form/post_form.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/form/post_form.es.js
@@ -1,5 +1,6 @@
 import dom from 'metal-dom';
 import {
+	isDef,
 	isObject,
 	isString
 } from 'metal';
@@ -8,24 +9,38 @@ import setFormValues from './set_form_values.es';
 /**
  * Submits the form, with optional setting of form elements.
  * @param {!Element|!string} form The form DOM element or the selector
- * @param {!string} url An action
- * @param {Object=} data Form data to set before submission, an object
- * containing form element names and values
+ * @param {Object=} options An object containing optional settings:
+ * - `url` : a string containing form action url
+ * - `data` : an object containing form elements keys and values, to be set
+ * before submission
  * @review
  */
 
-export default function postForm(form, url, data) {
-	if (isString(url)) {
-		form = dom.toElement(form);
+export default function postForm(form, options) {
+	form = dom.toElement(form);
 
-		if (form && form.nodeName === 'FORM') {
-			form.setAttribute('method', 'post');
+	if (form && form.nodeName === 'FORM') {
+		form.setAttribute('method', 'post');
+
+		if (isObject(options)) {
+			const {data, url} = options;
 
 			if (isObject(data)) {
 				setFormValues(form, data);
 			}
+			else {
+				return;
+			}
 
-			submitForm(form, url);
+			if (!isDef(url)) {
+				submitForm(form);
+			}
+			else if (isString(url)) {
+				submitForm(form, url);
+			}
+		}
+		else {
+			submitForm(form);
 		}
 	}
 }
