4bdc95a250b56ef6f1d10223714acd22dd8c547a
tina.tian <tina.tian@liferay.com>
2019-02-13 08:56:04 -0800

    Revert "LPS-89456 Add upgrade process - oauth2-provider-service"
    This reverts commit b90126fa65e4aa7a9a3b24cf90fccedcda4e7749.

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/internal/upgrade/OAuth2ServiceUpgrade.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/internal/upgrade/OAuth2ServiceUpgrade.java
@@ -15,7 +15,6 @@
 package com.liferay.oauth2.provider.internal.upgrade;
 
 import com.liferay.oauth2.provider.internal.upgrade.v1_1_0.UpgradeOAuth2ScopeGrant;
-import com.liferay.oauth2.provider.internal.upgrade.v2_0_0.UpgradeBadColumnNames;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
@@ -29,8 +28,6 @@ public class OAuth2ServiceUpgrade implements UpgradeStepRegistrator {
 	@Override
 	public void register(Registry registry) {
 		registry.register("1.0.0", "1.1.0", new UpgradeOAuth2ScopeGrant());
-
-		registry.register("1.1.0", "2.0.0", new UpgradeBadColumnNames());
 	}
 
 }
