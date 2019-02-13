fbb420514ec769f3bc311629bf88e33fb6bdf1d7
tina.tian <tina.tian@liferay.com>
2019-02-13 08:51:57 -0800

    Revert "LPS-89456 Check if current table is already updated to avoid false warning."
    This reverts commit 2b9499e93e4ce088192805d90e58e0271e971385.

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUpgradeBadColumnNames.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUpgradeBadColumnNames.java
@@ -56,19 +56,6 @@ public abstract class BaseUpgradeBadColumnNames extends UpgradeProcess {
 			columnNames.length);
 
 		for (String columnName : columnNames) {
-			String newColumnName = columnName.concat(StringPool.UNDERLINE);
-
-			try (ResultSet columnResultSet = databaseMetaData.getColumns(
-					dbInspector.getCatalog(), dbInspector.getSchema(),
-					tableName,
-					dbInspector.normalizeName(
-						newColumnName, databaseMetaData))) {
-
-				if (columnResultSet.next()) {
-					continue;
-				}
-			}
-
 			try (ResultSet columnResultSet = databaseMetaData.getColumns(
 					dbInspector.getCatalog(), dbInspector.getSchema(),
 					tableName,
@@ -86,7 +73,8 @@ public abstract class BaseUpgradeBadColumnNames extends UpgradeProcess {
 				}
 			}
 
-			String columnSQL = columnSQLs.get(newColumnName);
+			String columnSQL = columnSQLs.get(
+				columnName.concat(StringPool.UNDERLINE));
 
 			if (Validator.isNull(columnSQL)) {
 				if (_log.isWarnEnabled()) {
