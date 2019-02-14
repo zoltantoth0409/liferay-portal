9af8a53e69ecffd64b14bf243026b3a08906bda5
Victor Galan <victor.galan@liferay.com>
2019-02-15 00:59:55 +0100

    LPS-89820 Add vocabulary list with filter and sorting

--- modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/v1_0/VocabularyResourceImpl.java
+++ modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/v1_0/VocabularyResourceImpl.java
@@ -14,9 +14,36 @@
 
 package com.liferay.headless.foundation.internal.resource.v1_0;
 
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetVocabularyService;
+import com.liferay.headless.foundation.dto.v1_0.Vocabulary;
+import com.liferay.headless.foundation.internal.odata.entity.v1_0.VocabularyEntityModel;
 import com.liferay.headless.foundation.resource.v1_0.VocabularyResource;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.SearchResultPermissionFilterFactory;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.service.ClassNameService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.resource.EntityModelResource;
+import com.liferay.portal.vulcan.util.SearchUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -26,5 +53,91 @@ import org.osgi.service.component.annotations.ServiceScope;
 	properties = "OSGI-INF/liferay/rest/v1_0/vocabulary.properties",
 	scope = ServiceScope.PROTOTYPE, service = VocabularyResource.class
 )
-public class VocabularyResourceImpl extends BaseVocabularyResourceImpl {
+public class VocabularyResourceImpl
+	extends BaseVocabularyResourceImpl implements EntityModelResource {
+
+	@Override
+	public Page<Vocabulary> getContentSpaceVocabulariesPage(
+			Long contentSpaceId, Filter filter, Pagination pagination,
+			Sort[] sorts)
+		throws Exception {
+
+		List<AssetVocabulary> assetVocabularies = new ArrayList<>();
+
+		ClassName className = _classNameService.fetchClassName(
+			AssetVocabulary.class.getName());
+
+		Hits hits = SearchUtil.getHits(
+			filter, _indexerRegistry.nullSafeGetIndexer(AssetVocabulary.class),
+			pagination,
+			booleanQuery -> {
+			},
+			queryConfig -> {
+				queryConfig.setSelectedFieldNames(Field.ASSET_VOCABULARY_ID);
+			},
+			searchContext -> {
+				searchContext.setAttribute(
+					Field.CLASS_NAME_ID, className.getClassNameId());
+				searchContext.setAttribute("head", Boolean.TRUE);
+				searchContext.setCompanyId(company.getCompanyId());
+				searchContext.setGroupIds(new long[] {contentSpaceId});
+			},
+			_searchResultPermissionFilterFactory, sorts);
+
+		for (Document document : hits.getDocs()) {
+			AssetVocabulary vocabulary = _assetVocabularyService.getVocabulary(
+				GetterUtil.getLong(document.get(Field.ASSET_VOCABULARY_ID)));
+
+			assetVocabularies.add(vocabulary);
+		}
+
+		return Page.of(
+			transform(assetVocabularies, this::_toVocabulary), pagination,
+			assetVocabularies.size());
+	}
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap) {
+		return _vocabularyEntityModel;
+	}
+
+	private Vocabulary _toVocabulary(AssetVocabulary assetVocabulary) {
+		return new Vocabulary() {
+			{
+				setAvailableLanguages(
+					assetVocabulary.getAvailableLanguageIds());
+				setContentSpace(assetVocabulary.getGroupId());
+				setDateCreated(assetVocabulary.getCreateDate());
+				setDateModified(assetVocabulary.getModifiedDate());
+				setDescription(
+					assetVocabulary.getDescription(
+						acceptLanguage.getPreferredLocale()));
+				setId(assetVocabulary.getVocabularyId());
+				setName(
+					assetVocabulary.getTitle(
+						acceptLanguage.getPreferredLocale()));
+				setVocabularyCategoriesIds(
+					ListUtil.toArray(
+						assetVocabulary.getCategories(),
+						AssetCategory.CATEGORY_ID_ACCESSOR));
+			}
+		};
+	}
+
+	private static final VocabularyEntityModel _vocabularyEntityModel =
+		new VocabularyEntityModel();
+
+	@Reference
+	private AssetVocabularyService _assetVocabularyService;
+
+	@Reference
+	private ClassNameService _classNameService;
+
+	@Reference
+	private IndexerRegistry _indexerRegistry;
+
+	@Reference
+	private SearchResultPermissionFilterFactory
+		_searchResultPermissionFilterFactory;
+
 }
