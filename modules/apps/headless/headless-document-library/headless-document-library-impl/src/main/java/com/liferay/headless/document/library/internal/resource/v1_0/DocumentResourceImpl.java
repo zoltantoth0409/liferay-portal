0c0c59b634401e12fffe280c7251a46e9b1243f3
Alejandro Hernández <info@alejandrohdezma.com>
2019-02-14 16:28:06 +0100

    LPS-89718 Add Document#getAdaptedMedias

--- modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/v1_0/DocumentResourceImpl.java
+++ modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/v1_0/DocumentResourceImpl.java
@@ -14,6 +14,13 @@
 
 package com.liferay.headless.document.library.internal.resource.v1_0;
 
+import com.liferay.adaptive.media.AMAttribute;
+import com.liferay.adaptive.media.AdaptiveMedia;
+import com.liferay.adaptive.media.image.finder.AMImageFinder;
+import com.liferay.adaptive.media.image.finder.AMImageQueryBuilder;
+import com.liferay.adaptive.media.image.mime.type.AMImageMimeTypeProvider;
+import com.liferay.adaptive.media.image.processor.AMImageAttribute;
+import com.liferay.adaptive.media.image.processor.AMImageProcessor;
 import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
@@ -21,17 +28,25 @@ import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.document.library.util.DLURLHelper;
+import com.liferay.headless.document.library.dto.v1_0.AdaptedMedia;
 import com.liferay.headless.document.library.dto.v1_0.Creator;
 import com.liferay.headless.document.library.dto.v1_0.Document;
 import com.liferay.headless.document.library.internal.dto.v1_0.CreatorUtil;
 import com.liferay.headless.document.library.resource.v1_0.DocumentResource;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.kernel.service.UserService;
 import com.liferay.portal.kernel.util.ListUtil;
 
+import java.net.URI;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import javax.ws.rs.InternalServerErrorException;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -55,6 +70,41 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 		return _toDocument(fileEntry, fileEntry.getFileVersion(), user);
 	}
 
+	private AdaptedMedia[] _getAdaptiveMedias(FileEntry fileEntry) {
+		String mimeType = fileEntry.getMimeType();
+
+		if (!_amImageMimeTypeProvider.isMimeTypeSupported(mimeType)) {
+			return new AdaptedMedia[0];
+		}
+
+		try {
+			Stream<AdaptiveMedia<AMImageProcessor>> adaptiveMediaStream =
+				_amImageFinder.getAdaptiveMediaStream(
+					builder -> builder.forFileEntry(
+						fileEntry
+					).withConfigurationStatus(
+						AMImageQueryBuilder.ConfigurationStatus.ANY
+					).done());
+
+			return adaptiveMediaStream.map(
+				this::_toAdaptedMedia
+			).toArray(
+				AdaptedMedia[]::new
+			);
+		}
+		catch (PortalException pe) {
+			throw new InternalServerErrorException(pe);
+		}
+	}
+
+	private <T, S> T _getAdaptiveMediaValue(
+		AdaptiveMedia<S> adaptiveMedia, AMAttribute<S, T> amAttribute) {
+
+		Optional<T> valueOptional = adaptiveMedia.getValueOptional(amAttribute);
+
+		return valueOptional.orElse(null);
+	}
+
 	private String[] _getAssetTagNames(FileEntry fileEntry) {
 		List<AssetTag> assetTags = _assetTagLocalService.getTags(
 			DLFileEntry.class.getName(), fileEntry.getFileEntryId());
@@ -71,6 +121,34 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 			assetCategories, AssetCategory.CATEGORY_ID_ACCESSOR);
 	}
 
+	private AdaptedMedia _toAdaptedMedia(
+		AdaptiveMedia<AMImageProcessor> adaptiveMedia) {
+
+		URI contentURI = adaptiveMedia.getURI();
+
+		Integer height = _getAdaptiveMediaValue(
+			adaptiveMedia, AMImageAttribute.AM_IMAGE_ATTRIBUTE_HEIGHT);
+
+		String resolutionName = _getAdaptiveMediaValue(
+			adaptiveMedia, AMAttribute.getConfigurationUuidAMAttribute());
+
+		Long sizeInBytes = _getAdaptiveMediaValue(
+			adaptiveMedia, AMAttribute.getContentLengthAMAttribute());
+
+		Integer width = _getAdaptiveMediaValue(
+			adaptiveMedia, AMImageAttribute.AM_IMAGE_ATTRIBUTE_WIDTH);
+
+		return new AdaptedMedia() {
+			{
+				setContentUrl(contentURI.toString());
+				setHeight(height);
+				setResolutionName(resolutionName);
+				setSizeInBytes(sizeInBytes);
+				setWidth(width);
+			}
+		};
+	}
+
 	private Document _toDocument(
 		FileEntry fileEntry, FileVersion fileVersion, User user) {
 
@@ -83,8 +161,11 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 
 		Creator creator = CreatorUtil.toCreator(user);
 
+		AdaptedMedia[] adaptiveMedias = _getAdaptiveMedias(fileEntry);
+
 		return new Document() {
 			{
+				setAdaptedMedia(adaptiveMedias);
 				setCategory(categoryIds);
 				setContentUrl(previewURL);
 				setCreator(creator);
@@ -101,6 +182,12 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 			}
 		};
 	}
+
+	@Reference
+	private AMImageFinder _amImageFinder;
+
+	@Reference
+	private AMImageMimeTypeProvider _amImageMimeTypeProvider;
 
 	@Reference
 	private AssetCategoryLocalService _assetCategoryLocalService;
