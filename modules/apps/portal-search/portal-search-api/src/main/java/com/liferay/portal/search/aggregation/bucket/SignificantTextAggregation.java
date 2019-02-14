fec216c391a87a837bdc26b40a68f6c5f71aadff
André de Oliveira <andre.oliveira@liferay.com>
2019-02-13 20:01:10 -0800

    LPS-89499 portal-search-api: add Query based aggregations
    https://issues.liferay.com/browse/LPS-89499
    
    Co-authored-by: Michael Han <michael.han@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/aggregation/bucket/SignificantTextAggregation.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.aggregation.bucket;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.search.aggregation.FieldAggregation;
+import com.liferay.portal.search.query.Query;
+import com.liferay.portal.search.significance.SignificanceHeuristic;
+
+import java.util.List;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public interface SignificantTextAggregation extends FieldAggregation {
+
+	public Query getBackgroundFilterQuery();
+
+	public BucketCountThresholds getBucketCountThresholds();
+
+	public String getExecutionHint();
+
+	public Boolean getFilterDuplicateText();
+
+	public IncludeExcludeClause getIncludeExcludeClause();
+
+	public Long getMinDocCount();
+
+	public Long getShardMinDocCount();
+
+	public Integer getShardSize();
+
+	public SignificanceHeuristic getSignificanceHeuristic();
+
+	public Integer getSize();
+
+	public List<String> getSourceFields();
+
+}
