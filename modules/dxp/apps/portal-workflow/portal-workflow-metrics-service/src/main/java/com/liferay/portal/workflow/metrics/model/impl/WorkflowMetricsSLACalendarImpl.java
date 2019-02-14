eb484297bdd1736558ce0cdbfd0f1bab2d014a7f
Brian Chan <brian.chan@liferay.com>
2019-02-13 19:32:44 -0800

    LPS-89014 SF

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/model/impl/WorkflowMetricsSLACalendarImpl.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/model/impl/WorkflowMetricsSLACalendarImpl.java
@@ -17,23 +17,12 @@ package com.liferay.portal.workflow.metrics.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the WorkflowMetricsSLACalendar service. Represents a row in the &quot;WorkflowMetricsSLACalendar&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.portal.workflow.metrics.model.WorkflowMetricsSLACalendar} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class WorkflowMetricsSLACalendarImpl
 	extends WorkflowMetricsSLACalendarBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a workflow metrics sla calendar model instance should use the {@link com.liferay.portal.workflow.metrics.model.WorkflowMetricsSLACalendar} interface instead.
-	 */
 	public WorkflowMetricsSLACalendarImpl() {
 	}
 
--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/model/impl/WorkflowMetricsSLADefinitionImpl.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/model/impl/WorkflowMetricsSLADefinitionImpl.java
@@ -17,23 +17,12 @@ package com.liferay.portal.workflow.metrics.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the WorkflowMetricsSLADefinition service. Represents a row in the &quot;WorkflowMetricsSLADefinition&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.portal.workflow.metrics.model.WorkflowMetricsSLADefinition} interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 @ProviderType
 public class WorkflowMetricsSLADefinitionImpl
 	extends WorkflowMetricsSLADefinitionBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a workflow metrics sla definition model instance should use the {@link com.liferay.portal.workflow.metrics.model.WorkflowMetricsSLADefinition} interface instead.
-	 */
 	public WorkflowMetricsSLADefinitionImpl() {
 	}
 
