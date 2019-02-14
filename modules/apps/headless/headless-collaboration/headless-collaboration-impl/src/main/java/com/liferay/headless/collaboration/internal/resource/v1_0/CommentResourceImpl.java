05a755b8e88051761a0043f876616b324c9f4ade
Javier Gamarra <nhpatt@gmail.com>
2019-02-14 18:05:44 +0100

    LPS-89752 migrate Comments API

--- modules/apps/headless/headless-collaboration/headless-collaboration-impl/src/main/java/com/liferay/headless/collaboration/internal/resource/v1_0/CommentResourceImpl.java
+++ modules/apps/headless/headless-collaboration/headless-collaboration-impl/src/main/java/com/liferay/headless/collaboration/internal/resource/v1_0/CommentResourceImpl.java
@@ -14,9 +14,25 @@
 
 package com.liferay.headless.collaboration.internal.resource.v1_0;
 
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryService;
+import com.liferay.headless.collaboration.dto.v1_0.Comment;
+import com.liferay.headless.collaboration.internal.dto.v1_0.CommentUtil;
 import com.liferay.headless.collaboration.resource.v1_0.CommentResource;
+import com.liferay.portal.kernel.comment.CommentManager;
+import com.liferay.portal.kernel.comment.DiscussionPermission;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.Collections;
+
+import javax.ws.rs.NotFoundException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +43,101 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = CommentResource.class
 )
 public class CommentResourceImpl extends BaseCommentResourceImpl {
+
+	@Override
+	public Page<Comment> getBlogPostingCommentsPage(
+			Long blogPostingId, Pagination pagination)
+		throws Exception {
+
+		BlogsEntry blogsEntry = _blogsEntryService.getEntry(blogPostingId);
+
+		int count = _commentManager.getRootCommentsCount(
+			blogsEntry.getModelClassName(), blogPostingId,
+			WorkflowConstants.STATUS_APPROVED);
+
+		if (count == 0) {
+			return Page.of(Collections.emptyList());
+		}
+
+		_checkViewPermission(
+			blogsEntry.getGroupId(), blogsEntry.getModelClassName(),
+			blogPostingId);
+
+		return Page.of(
+			transform(
+				_commentManager.getRootComments(
+					blogsEntry.getModelClassName(), blogPostingId,
+					WorkflowConstants.STATUS_APPROVED,
+					pagination.getStartPosition(), pagination.getEndPosition()),
+				CommentUtil::toComment),
+			pagination, count);
+	}
+
+	@Override
+	public Comment getComment(Long commentId) throws Exception {
+		com.liferay.portal.kernel.comment.Comment comment =
+			_commentManager.fetchComment(commentId);
+
+		_checkViewPermission(comment);
+
+		return CommentUtil.toComment(comment);
+	}
+
+	@Override
+	public Page<Comment> getCommentCommentsPage(
+			Long commentId, Pagination pagination)
+		throws Exception {
+
+		_checkViewPermission(_commentManager.fetchComment(commentId));
+
+		return Page.of(
+			transform(
+				_commentManager.getChildComments(
+					commentId, WorkflowConstants.STATUS_APPROVED,
+					pagination.getStartPosition(), pagination.getEndPosition()),
+				CommentUtil::toComment),
+			pagination,
+			_commentManager.getChildCommentsCount(
+				commentId, WorkflowConstants.STATUS_APPROVED));
+	}
+
+	private void _checkViewPermission(
+			com.liferay.portal.kernel.comment.Comment comment)
+		throws Exception {
+
+		if (comment == null) {
+			throw new NotFoundException();
+		}
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		DiscussionPermission discussionPermission =
+			_commentManager.getDiscussionPermission(permissionChecker);
+
+		discussionPermission.checkViewPermission(
+			permissionChecker.getCompanyId(), comment.getGroupId(),
+			comment.getClassName(), comment.getClassPK());
+	}
+
+	private void _checkViewPermission(
+			long groupId, String className, long classPK)
+		throws Exception {
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		DiscussionPermission discussionPermission =
+			_commentManager.getDiscussionPermission(permissionChecker);
+
+		discussionPermission.checkViewPermission(
+			permissionChecker.getCompanyId(), groupId, className, classPK);
+	}
+
+	@Reference
+	private BlogsEntryService _blogsEntryService;
+
+	@Reference
+	private CommentManager _commentManager;
+
 }
--- modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/v1_0/CommentResourceImpl.java
+++ modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/v1_0/CommentResourceImpl.java
@@ -14,9 +14,23 @@
 
 package com.liferay.headless.document.library.internal.resource.v1_0;
 
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.service.DLFileEntryService;
+import com.liferay.headless.document.library.dto.v1_0.Comment;
+import com.liferay.headless.document.library.internal.dto.v1_0.CommentUtil;
 import com.liferay.headless.document.library.resource.v1_0.CommentResource;
+import com.liferay.portal.kernel.comment.CommentManager;
+import com.liferay.portal.kernel.comment.DiscussionPermission;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.Collections;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +41,54 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = CommentResource.class
 )
 public class CommentResourceImpl extends BaseCommentResourceImpl {
+
+	@Override
+	public Page<Comment> getDocumentCommentsPage(
+			Long fileEntryId, Pagination pagination)
+		throws Exception {
+
+		DLFileEntry dlFileEntry = _dlFileEntryService.getFileEntry(fileEntryId);
+
+		int count = _commentManager.getRootCommentsCount(
+			dlFileEntry.getModelClassName(), fileEntryId,
+			WorkflowConstants.STATUS_APPROVED);
+
+		if (count == 0) {
+			return Page.of(Collections.emptyList());
+		}
+
+		_checkViewPermission(
+			dlFileEntry.getGroupId(), dlFileEntry.getModelClassName(),
+			fileEntryId);
+
+		return Page.of(
+			transform(
+				_commentManager.getRootComments(
+					dlFileEntry.getModelClassName(), fileEntryId,
+					WorkflowConstants.STATUS_APPROVED,
+					pagination.getStartPosition(), pagination.getEndPosition()),
+				CommentUtil::toComment),
+			pagination, count);
+	}
+
+	private void _checkViewPermission(
+			long groupId, String className, long classPK)
+		throws Exception {
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		DiscussionPermission discussionPermission =
+			_commentManager.getDiscussionPermission(permissionChecker);
+
+		discussionPermission.checkViewPermission(
+			permissionChecker.getCompanyId(), groupId, className, classPK);
+	}
+
+	@Reference
+	private CommentManager _commentManager;
+
+	@Reference
+	private DLFileEntryService _dlFileEntryService;
+
 }
