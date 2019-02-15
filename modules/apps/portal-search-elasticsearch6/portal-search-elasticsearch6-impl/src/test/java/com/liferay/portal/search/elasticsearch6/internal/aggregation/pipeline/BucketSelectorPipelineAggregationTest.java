6a0c8d983e050b265eadbed34c15653347c4a494
André de Oliveira <andre.oliveira@liferay.com>
2019-02-14 19:40:43 -0800

    LPS-89499 portal-search-elasticsearch6-impl: Test: add Script based aggregations
    https://issues.liferay.com/browse/LPS-89499
    
    Co-authored-by: Michael Han <michael.han@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/aggregation/pipeline/BucketSelectorPipelineAggregationTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch6.internal.aggregation.pipeline;
+
+import com.liferay.portal.search.elasticsearch6.internal.ElasticsearchIndexingFixture;
+import com.liferay.portal.search.elasticsearch6.internal.connection.ElasticsearchFixture;
+import com.liferay.portal.search.test.util.aggregation.pipeline.BaseBucketSelectorPipelineAggregationTestCase;
+import com.liferay.portal.search.test.util.indexing.BaseIndexingTestCase;
+import com.liferay.portal.search.test.util.indexing.IndexingFixture;
+
+/**
+ * @author Michael C. Han
+ */
+public class BucketSelectorPipelineAggregationTest
+	extends BaseBucketSelectorPipelineAggregationTestCase {
+
+	@Override
+	protected IndexingFixture createIndexingFixture() throws Exception {
+		return new ElasticsearchIndexingFixture() {
+			{
+				setCompanyId(BaseIndexingTestCase.COMPANY_ID);
+				setElasticsearchFixture(new ElasticsearchFixture(getClass()));
+				setLiferayMappingsAddedToIndex(true);
+			}
+		};
+	}
+
+}
