b382807de51116a719df420e5f8b57a906c08965
Pei-Jung Lan <pei-jung.lan@liferay.com>
2019-02-14 16:22:32 -0800

    LPS-90317 LPS-89521 Add test

---
+++ portal-impl/test/integration/com/liferay/portal/service/user/UserServiceWhenUpdatingUserWithSiteRoleTest.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service.user;
+
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserGroupRole;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserGroupRoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserServiceUtil;
+import com.liferay.portal.kernel.service.persistence.UserGroupRolePK;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.OrganizationTestUtil;
+import com.liferay.portal.kernel.test.util.RoleTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerTestRule;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public class UserServiceWhenUpdatingUserWithSiteRoleTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerTestRule.INSTANCE);
+
+	@Test
+	public void testShouldAssignSiteRoleForInheritedSite() throws Exception {
+		_organization = OrganizationTestUtil.addOrganization();
+
+		_group = GroupTestUtil.addGroup();
+
+		OrganizationLocalServiceUtil.addGroupOrganization(
+			_group.getGroupId(), _organization);
+
+		_user = UserTestUtil.addUser();
+
+		OrganizationLocalServiceUtil.addUserOrganization(
+			_user.getUserId(), _organization);
+
+		_role = RoleTestUtil.addRole(RoleConstants.TYPE_SITE);
+
+		UserGroupRolePK userGroupRolePK = new UserGroupRolePK(
+			_user.getUserId(), _group.getGroupId(), _role.getRoleId());
+
+		UserGroupRole userGroupRole =
+			UserGroupRoleLocalServiceUtil.createUserGroupRole(userGroupRolePK);
+
+		_user = _updateUser(_user, Collections.singletonList(userGroupRole));
+
+		Assert.assertTrue(
+			UserGroupRoleLocalServiceUtil.hasUserGroupRole(
+				_user.getUserId(), _group.getGroupId(), _role.getRoleId()));
+	}
+
+	private User _updateUser(User user, List<UserGroupRole> userGroupRoles)
+		throws Exception {
+
+		Contact contact = user.getContact();
+
+		Calendar birthdayCal = CalendarFactoryUtil.getCalendar();
+
+		birthdayCal.setTime(contact.getBirthday());
+
+		int birthdayMonth = birthdayCal.get(Calendar.MONTH);
+		int birthdayDay = birthdayCal.get(Calendar.DATE);
+		int birthdayYear = birthdayCal.get(Calendar.YEAR);
+
+		return UserServiceUtil.updateUser(
+			user.getUserId(), user.getPassword(), null, null,
+			user.isPasswordReset(), null, null, user.getScreenName(),
+			user.getEmailAddress(), user.getFacebookId(), user.getOpenId(),
+			user.getLanguageId(), user.getTimeZoneId(), user.getGreeting(),
+			user.getComments(), user.getFirstName(), user.getMiddleName(),
+			user.getLastName(), contact.getPrefixId(), contact.getSuffixId(),
+			user.isMale(), birthdayMonth, birthdayDay, birthdayYear,
+			contact.getSmsSn(), contact.getFacebookSn(), contact.getJabberSn(),
+			contact.getSkypeSn(), contact.getTwitterSn(), user.getJobTitle(),
+			null, null, null, userGroupRoles, null, new ServiceContext());
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@DeleteAfterTestRun
+	private Organization _organization;
+
+	@DeleteAfterTestRun
+	private Role _role;
+
+	@DeleteAfterTestRun
+	private User _user;
+
+}
