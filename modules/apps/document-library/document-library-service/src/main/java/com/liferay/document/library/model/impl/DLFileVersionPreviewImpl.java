b76f649fca95c7d235350c0aeb655e7bed1f1e3c
Adolfo Pérez <adolfo.perez@liferay.com>
2019-02-15 12:59:39 +0100

    LPS-86455 SF

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/model/impl/DLFileVersionPreviewImpl.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/model/impl/DLFileVersionPreviewImpl.java
@@ -17,22 +17,12 @@ package com.liferay.document.library.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the DLFileVersionPreview service. Represents a row in the &quot;DLFileVersionPreview&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.document.library.model.DLFileVersionPreview<code> interface.
- * </p>
- *
- * @author Brian Wing Shun Chan
+ * @author Roberto Díaz
+ * @author Adolfo Pérez
  */
 @ProviderType
 public class DLFileVersionPreviewImpl extends DLFileVersionPreviewBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a dl file version preview model instance should use the {@link com.liferay.document.library.model.DLFileVersionPreview} interface instead.
-	 */
 	public DLFileVersionPreviewImpl() {
 	}
 
