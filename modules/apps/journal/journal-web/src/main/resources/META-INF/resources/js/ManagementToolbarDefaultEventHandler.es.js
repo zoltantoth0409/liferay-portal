f69165939cac243bf9ea3db9eea34a4ef736e92b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-02-15 13:00:11 +0100

    LPS-89898 SF

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/ManagementToolbarDefaultEventHandler.es.js
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/ManagementToolbarDefaultEventHandler.es.js
@@ -20,13 +20,13 @@ class ManagementToolbarDefaultEventHandler extends PortletBase {
 	}
 
 	deleteEntries() {
-		let message = 'are-you-sure-you-want-to-delete-the-selected-entries';
+		let message = Liferay.Language.get('are-you-sure-you-want-to-delete-the-selected-entries');
 
 		if (this.trashEnabled) {
-			message = 'are-you-sure-you-want-to-move-the-selected-entries-to-the-recycle-bin';
+			message = Liferay.Language.get('are-you-sure-you-want-to-move-the-selected-entries-to-the-recycle-bin');
 		}
 
-		if (confirm(Liferay.Language.get(message))) {
+		if (confirm(message)) {
 			Liferay.fire(
 				this.ns('editEntry'),
 				{
