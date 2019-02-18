3389e98ab6b2afe8bc457bafb33a13f671f4e974
Pavel Savinov <pavel.savinov@liferay.com>
2019-02-18 14:24:44 +0100

    LPS-89252 Save shortened localized description to avoid field overflow

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v1_1_0/UpgradeJournalArticleLocalizedValues.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v1_1_0/UpgradeJournalArticleLocalizedValues.java
@@ -150,6 +150,8 @@ public class UpgradeJournalArticleLocalizedValues extends UpgradeProcess {
 						if (localizedDescription != safeLocalizedDescription) {
 							_log(articleId, "description");
 						}
+
+						localizedDescription = safeLocalizedDescription;
 					}
 
 					ps2.setLong(1, _increment());
