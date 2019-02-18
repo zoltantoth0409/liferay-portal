26f72e7a4d6e3032d21c1f3311e8aec1ed19edd9
Máté Thurzó <mate.thurzo@liferay.com>
2019-02-18 21:39:55 +0100

    LPS-90577 SF

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/CTEngineManagerImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/CTEngineManagerImpl.java
@@ -53,7 +53,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
