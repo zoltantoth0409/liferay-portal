f9fbc0e80121403a3c0c2280b998ee028e16820a
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-02-19 10:42:29 +0100

    LPS-90784 Adding autocomplete resource

---
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/portlet/action/SharingUserAutocompleteMVCResourceCommand.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.dao.orm.WildcardMode;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.util.comparator.UserScreenNameComparator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.sharing.constants.SharingPortletKeys;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + SharingPortletKeys.SHARING,
+		"mvc.command.name=/sharing/users"
+	},
+	service = MVCResourceCommand.class
+)
+public class SharingUserAutocompleteMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		HttpServletRequest request = _portal.getHttpServletRequest(
+			resourceRequest);
+
+		JSONArray usersJSONArray = getUsersJSONArray(request);
+
+		HttpServletResponse response = _portal.getHttpServletResponse(
+			resourceResponse);
+
+		response.setContentType(ContentTypes.APPLICATION_JSON);
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse, usersJSONArray);
+	}
+
+	protected JSONArray getUsersJSONArray(HttpServletRequest request) {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		for (User user : _getUsers(request, themeDisplay)) {
+			if (user.isDefaultUser() ||
+				(themeDisplay.getUserId() == user.getUserId())) {
+
+				continue;
+			}
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			jsonObject.put("emailAddress", user.getEmailAddress());
+			jsonObject.put("fullName", user.getFullName());
+
+			jsonArray.put(jsonObject);
+		}
+
+		return jsonArray;
+	}
+
+	private List<User> _getUsers(
+		HttpServletRequest request, ThemeDisplay themeDisplay) {
+
+		String query = ParamUtil.getString(request, "query");
+
+		LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+
+		params.put("wildcardMode", WildcardMode.TRAILING);
+
+		return _userLocalService.search(
+			themeDisplay.getCompanyId(), query,
+			WorkflowConstants.STATUS_APPROVED, params, 0, 20,
+			new UserScreenNameComparator());
+	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
