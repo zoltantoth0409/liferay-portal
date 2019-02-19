6160e79a36a50540966f99c13aece63ea0cbd0cc
Jose A. Jimenez <jose.jimenez@liferay.com>
2019-02-19 12:06:27 +0100

    LPS-90801 WCD should not display articles displayed in other portlets / taglibs, the problem is introduced by the JournalArticleAssetRenderer that was created for the search portlet but can be introduced using it from other portlets (asset-publisher, template ussing AssetDisplayTag, etc.)

--- modules/apps/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
+++ modules/apps/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/display/context/JournalContentDisplayContext.java
@@ -161,13 +161,6 @@ public class JournalContentDisplayContext {
 			return _article;
 		}
 
-		_article = (JournalArticle)_portletRequest.getAttribute(
-			WebKeys.JOURNAL_ARTICLE);
-
-		if (_article != null) {
-			return _article;
-		}
-
 		long articleResourcePrimKey = ParamUtil.getLong(
 			_portletRequest, "articleResourcePrimKey");
 
@@ -192,13 +185,6 @@ public class JournalContentDisplayContext {
 	}
 
 	public JournalArticleDisplay getArticleDisplay() throws PortalException {
-		if (_articleDisplay != null) {
-			return _articleDisplay;
-		}
-
-		_articleDisplay = (JournalArticleDisplay)_portletRequest.getAttribute(
-			WebKeys.JOURNAL_ARTICLE_DISPLAY);
-
 		if (_articleDisplay != null) {
 			return _articleDisplay;
 		}
