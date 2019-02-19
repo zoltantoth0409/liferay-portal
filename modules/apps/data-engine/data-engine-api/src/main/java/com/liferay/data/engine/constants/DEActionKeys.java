16fc146738fd815cb79680c60cccf3ea6a076ee7
Leonardo Barros <leonardo.barros@liferay.com>
2019-02-19 10:39:06 -0300

    LPS-90707 add constant

--- modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/constants/DEActionKeys.java
+++ modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/constants/DEActionKeys.java
@@ -31,6 +31,8 @@ public class DEActionKeys {
 
 	public static final String DELETE_DATA_RECORD = "DELETE_DATA_RECORD";
 
+	public static final String EXPORT_DATA_RECORDS = "EXPORT_DATA_RECORDS";
+
 	public static final String UPDATE_DATA_RECORD = "UPDATE_DATA_RECORD";
 
 	public static final String VIEW_DATA_RECORD = "VIEW_DATA_RECORD";
