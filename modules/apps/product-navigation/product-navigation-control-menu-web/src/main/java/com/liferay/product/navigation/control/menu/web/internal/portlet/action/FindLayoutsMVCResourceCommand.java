d4b8876d769367487e94f86a7df66de121580a54
Pavel Savinov <pavel.savinov@liferay.com>
2019-02-19 11:09:36 +0100

    LPS-90363 Resource command to be able to find layouts by keywords

---
+++ modules/apps/product-navigation/product-navigation-control-menu-web/src/main/java/com/liferay/product/navigation/control/menu/web/internal/portlet/action/FindLayoutsMVCResourceCommand.java
@@ -0,0 +1,144 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.product.navigation.control.menu.web.internal.portlet.action;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.servlet.ServletResponseUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.product.navigation.control.menu.web.internal.constants.ProductNavigationControlMenuPortletKeys;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + ProductNavigationControlMenuPortletKeys.PRODUCT_NAVIGATION_CONTROL_MENU,
+		"mvc.command.name=/control_menu/find_layouts"
+	},
+	service = MVCResourceCommand.class
+)
+public class FindLayoutsMVCResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		String keywords = ParamUtil.getString(resourceRequest, "keywords");
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		HttpServletResponse response = _portal.getHttpServletResponse(
+			resourceResponse);
+
+		response.setContentType(ContentTypes.APPLICATION_JSON);
+
+		if (Validator.isNull(keywords)) {
+			jsonObject.put("layouts", JSONFactoryUtil.createJSONArray());
+			jsonObject.put("totalCount", 0);
+
+			ServletResponseUtil.write(response, jsonObject.toString());
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		List<Layout> layouts = new ArrayList<>();
+
+		layouts.addAll(
+			_layoutLocalService.getLayouts(
+				themeDisplay.getScopeGroupId(), false, keywords,
+				new String[] {"portlet", "content"}, 0, 10, null));
+
+		layouts.addAll(
+			_layoutLocalService.getLayouts(
+				themeDisplay.getScopeGroupId(), true, keywords,
+				new String[] {"portlet", "content"}, 0, 10, null));
+
+		int totalCount = _layoutLocalService.getLayoutsCount(
+			themeDisplay.getScopeGroup(), false, keywords,
+			new String[] {"portlet", "content"});
+
+		totalCount += _layoutLocalService.getLayoutsCount(
+			themeDisplay.getScopeGroup(), true, keywords,
+			new String[] {"portlet", "content"});
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		for (Layout layout : layouts) {
+			JSONObject layoutJSONObject = JSONFactoryUtil.createJSONObject();
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append(layout.getName(themeDisplay.getLocale()));
+			sb.append(StringPool.SPACE);
+			sb.append(StringPool.OPEN_PARENTHESIS);
+
+			if (layout.isPrivateLayout()) {
+				sb.append(
+					LanguageUtil.get(themeDisplay.getLocale(), "private"));
+			}
+			else {
+				sb.append(LanguageUtil.get(themeDisplay.getLocale(), "public"));
+			}
+
+			sb.append(StringPool.CLOSE_PARENTHESIS);
+
+			layoutJSONObject.put("name", sb.toString());
+			layoutJSONObject.put(
+				"url", _portal.getLayoutFullURL(layout, themeDisplay));
+
+			jsonArray.put(layoutJSONObject);
+		}
+
+		jsonObject.put("layouts", jsonArray);
+		jsonObject.put("totalCount", totalCount);
+
+		ServletResponseUtil.write(response, jsonObject.toString());
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
