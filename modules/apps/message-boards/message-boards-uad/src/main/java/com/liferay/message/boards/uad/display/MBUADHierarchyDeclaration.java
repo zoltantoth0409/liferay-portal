0048462b4d57f00da33aa66f8d70075604438b8b
Samuel Tran <samuel.tran@liferay.com>
2019-02-19 11:38:12 -0800

    LPS-90860 message-boards-uad - Implement MBUADHierarchyDeclaration

---
+++ modules/apps/message-boards/message-boards-uad/src/main/java/com/liferay/message/boards/uad/display/MBUADHierarchyDeclaration.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.uad.display;
+
+import com.liferay.user.associated.data.display.UADDisplay;
+import com.liferay.user.associated.data.display.UADHierarchyDeclaration;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Samuel Trong Tran
+ */
+@Component(
+	immediate = true,
+	service = {MBUADHierarchyDeclaration.class, UADHierarchyDeclaration.class}
+)
+public class MBUADHierarchyDeclaration implements UADHierarchyDeclaration {
+
+	@Override
+	public UADDisplay<?>[] getContainerUADDisplays() {
+		return new UADDisplay[] {_mbCategoryUADDisplay, _mbThreadUADDisplay};
+	}
+
+	@Override
+	public UADDisplay<?>[] getNoncontainerUADDisplays() {
+		return new UADDisplay[] {_mbMessageUADDisplay};
+	}
+
+	@Reference
+	private MBCategoryUADDisplay _mbCategoryUADDisplay;
+
+	@Reference
+	private MBMessageUADDisplay _mbMessageUADDisplay;
+
+	@Reference
+	private MBThreadUADDisplay _mbThreadUADDisplay;
+
+}
