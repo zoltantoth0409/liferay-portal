3d9e0c15a94d5047bb36586aecb5b1d75567ade0
Marcelo Mello <marcelo.mello@liferay.com>
2019-02-20 18:19:54 -0300

    LPS-90943 Create DataRecordCollection search service request/response

---
+++ modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/service/DEDataRecordCollectionSearchRequest.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.service;
+
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+
+/**
+ * @author Marcelo Mello
+ */
+public class DEDataRecordCollectionSearchRequest {
+
+	public long getCompanyId() {
+		return _companyId;
+	}
+
+	public int getEnd() {
+		return _end;
+	}
+
+	public long getGroupId() {
+		return _groupId;
+	}
+
+	public String getKeywords() {
+		return _keywords;
+	}
+
+	public long getStart() {
+		return _start;
+	}
+
+	public static final class Builder {
+
+		public DEDataRecordCollectionSearchRequest build() {
+			return _deDataRecordCollectionSearchRequest;
+		}
+
+		public Builder endingAt(int end) {
+			_deDataRecordCollectionSearchRequest._end = end;
+
+			return this;
+		}
+
+		public Builder havingKeywords(String keywords) {
+			_deDataRecordCollectionSearchRequest._keywords = keywords;
+
+			return this;
+		}
+
+		public Builder inCompany(long companyId) {
+			_deDataRecordCollectionSearchRequest._companyId = companyId;
+
+			return this;
+		}
+
+		public Builder inGroup(long groupId) {
+			_deDataRecordCollectionSearchRequest._groupId = groupId;
+
+			return this;
+		}
+
+		public Builder startingAt(int start) {
+			_deDataRecordCollectionSearchRequest._start = start;
+
+			return this;
+		}
+
+		private final DEDataRecordCollectionSearchRequest
+			_deDataRecordCollectionSearchRequest =
+				new DEDataRecordCollectionSearchRequest();
+	}
+
+	private DEDataRecordCollectionSearchRequest() {
+	}
+
+	private long _companyId;
+	private int _end = QueryUtil.ALL_POS;
+	private long _groupId;
+	private String _keywords;
+	private long _start = QueryUtil.ALL_POS;
+
+}
---
+++ modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/service/DEDataRecordCollectionSearchResponse.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.service;
+
+import com.liferay.data.engine.model.DEDataRecordCollection;
+
+import java.util.List;
+
+/**
+ * @author Marcelo Mello
+ */
+public class DEDataRecordCollectionSearchResponse {
+
+	public List<DEDataRecordCollection> getDeDataRecordCollections() {
+		return _deDataRecordCollections;
+	}
+
+	public static final class Builder {
+
+		public static Builder newBuilder(
+			List<DEDataRecordCollection> deDataRecordCollections) {
+
+			return new Builder(deDataRecordCollections);
+		}
+
+		public static DEDataRecordCollectionSearchResponse of(
+			List<DEDataRecordCollection> deDataRecordCollections) {
+
+			return newBuilder(
+				deDataRecordCollections
+			).build();
+		}
+
+		public DEDataRecordCollectionSearchResponse build() {
+			return _deDataRecordCollectionSearchResponse;
+		}
+
+		private Builder(List<DEDataRecordCollection> deDataRecordCollections) {
+			_deDataRecordCollectionSearchResponse._deDataRecordCollections =
+				deDataRecordCollections;
+		}
+
+		private final DEDataRecordCollectionSearchResponse
+			_deDataRecordCollectionSearchResponse =
+			new DEDataRecordCollectionSearchResponse();
+	}
+
+	private DEDataRecordCollectionSearchResponse() {
+	}
+
+	private List<DEDataRecordCollection> _deDataRecordCollections;
+}
