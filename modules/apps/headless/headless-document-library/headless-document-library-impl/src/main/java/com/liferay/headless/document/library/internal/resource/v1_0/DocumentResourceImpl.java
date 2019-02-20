6d8111725c9c04f769d41f2c2fbe2e709310884d
Sarai Díaz García <dgarciasarai@gmail.com>
2019-02-20 12:58:44 +0100

    LPS-89702 Add filter and sort in document lists

--- modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/v1_0/DocumentResourceImpl.java
+++ modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/v1_0/DocumentResourceImpl.java
@@ -35,17 +35,39 @@ import com.liferay.headless.document.library.internal.dto.v1_0.AdaptedImagesImpl
 import com.liferay.headless.document.library.internal.dto.v1_0.CategoriesImpl;
 import com.liferay.headless.document.library.internal.dto.v1_0.DocumentImpl;
 import com.liferay.headless.document.library.internal.dto.v1_0.util.CreatorUtil;
+import com.liferay.headless.document.library.internal.odata.entity.v1_0.DocumentEntityModel;
 import com.liferay.headless.document.library.resource.v1_0.DocumentResource;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.FileVersion;
-import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.IndexerRegistry;
+import com.liferay.portal.kernel.search.SearchResultPermissionFilterFactory;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.search.filter.TermFilter;
+import com.liferay.portal.kernel.service.ClassNameService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.resource.EntityModelResource;
+import com.liferay.portal.vulcan.util.SearchUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -58,7 +80,8 @@ import org.osgi.service.component.annotations.ServiceScope;
 	properties = "OSGI-INF/liferay/rest/v1_0/document.properties",
 	scope = ServiceScope.PROTOTYPE, service = DocumentResource.class
 )
-public class DocumentResourceImpl extends BaseDocumentResourceImpl {
+public class DocumentResourceImpl
+	extends BaseDocumentResourceImpl implements EntityModelResource {
 
 	@Override
 	public boolean deleteDocument(Long documentId) throws Exception {
@@ -68,12 +91,56 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 	}
 
 	@Override
+	public Page<Document> getContentSpaceDocumentsPage(
+			Long contentSpaceId, Filter filter, Pagination pagination,
+			Sort[] sorts)
+		throws Exception {
+
+		return _getDocumentsPage(
+			booleanQuery -> {
+				if (contentSpaceId != null) {
+					BooleanFilter booleanFilter =
+						booleanQuery.getPreBooleanFilter();
+
+					booleanFilter.add(
+						new TermFilter(
+							Field.GROUP_ID, String.valueOf(contentSpaceId)),
+						BooleanClauseOccur.MUST);
+				}
+			},
+			filter, pagination, sorts);
+	}
+
+	@Override
 	public Document getDocument(Long documentId) throws Exception {
 		FileEntry fileEntry = _dlAppService.getFileEntry(documentId);
 
-		return _toDocument(
-			fileEntry, fileEntry.getFileVersion(),
-			_userService.getUserById(fileEntry.getUserId()));
+		return _toDocument(fileEntry);
+	}
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap) {
+		return _vocabularyEntityModel;
+	}
+
+	@Override
+	public Page<Document> getFolderDocumentsPage(
+			Long folderId, Filter filter, Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		return _getDocumentsPage(
+			booleanQuery -> {
+				if (folderId != null) {
+					BooleanFilter booleanFilter =
+						booleanQuery.getPreBooleanFilter();
+
+					booleanFilter.add(
+						new TermFilter(
+							Field.FOLDER_ID, String.valueOf(folderId)),
+						BooleanClauseOccur.MUST);
+				}
+			},
+			filter, pagination, sorts);
 	}
 
 	private AdaptedImages[] _getAdaptiveMedias(FileEntry fileEntry)
@@ -126,6 +193,40 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 		);
 	}
 
+	private Page<Document> _getDocumentsPage(
+			Consumer<BooleanQuery> booleanQueryConsumer, Filter filter,
+			Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		List<FileEntry> fileEntries = new ArrayList<>();
+
+		Hits hits = SearchUtil.getHits(
+			filter, _indexerRegistry.nullSafeGetIndexer(DLFileEntry.class),
+			pagination, booleanQueryConsumer,
+			queryConfig -> {
+				queryConfig.setSelectedFieldNames(Field.ENTRY_CLASS_PK);
+			},
+			searchContext -> {
+				searchContext.setCompanyId(contextCompany.getCompanyId());
+			},
+			_searchResultPermissionFilterFactory, sorts);
+
+		for (com.liferay.portal.kernel.search.Document document :
+				hits.getDocs()) {
+
+			long fileEntryId = GetterUtil.getLong(
+				document.get(Field.ENTRY_CLASS_PK));
+
+			FileEntry fileEntry = _dlAppService.getFileEntry(fileEntryId);
+
+			fileEntries.add(fileEntry);
+		}
+
+		return Page.of(
+			transform(fileEntries, this::_toDocument), pagination,
+			fileEntries.size());
+	}
+
 	private <T, S> T _getValue(
 		AdaptiveMedia<S> adaptiveMedia, AMAttribute<S, T> amAttribute) {
 
@@ -153,6 +254,12 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 		};
 	}
 
+	private Document _toDocument(FileEntry fileEntry) throws Exception {
+		return _toDocument(
+			fileEntry, fileEntry.getFileVersion(),
+			_userLocalService.getUserById(fileEntry.getUserId()));
+	}
+
 	private Document _toDocument(
 			FileEntry fileEntry, FileVersion fileVersion, User user)
 		throws Exception {
@@ -178,6 +285,9 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 		};
 	}
 
+	private static final DocumentEntityModel _vocabularyEntityModel =
+		new DocumentEntityModel();
+
 	@Reference
 	private AMImageFinder _amImageFinder;
 
@@ -191,15 +301,25 @@ public class DocumentResourceImpl extends BaseDocumentResourceImpl {
 	private AssetTagLocalService _assetTagLocalService;
 
 	@Reference
+	private ClassNameService _classNameService;
+
+	@Reference
 	private DLAppService _dlAppService;
 
 	@Reference
 	private DLURLHelper _dlURLHelper;
 
 	@Reference
+	private IndexerRegistry _indexerRegistry;
+
+	@Reference
 	private Portal _portal;
 
 	@Reference
-	private UserService _userService;
+	private SearchResultPermissionFilterFactory
+		_searchResultPermissionFilterFactory;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
