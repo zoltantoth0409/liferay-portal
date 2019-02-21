3a0b2d141188ae609989ccbc53634b3d1dc4039d
Marcelo Mello <marcelo.mello@liferay.com>
2019-02-21 10:19:27 -0300

    LPS-90943 Add DataRecordCollection search executor

---
+++ modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/internal/executor/DEDataRecordCollectionSearchExecutor.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.internal.executor;
+
+import com.liferay.data.engine.model.DEDataRecordCollection;
+import com.liferay.data.engine.service.DEDataRecordCollectionSearchRequest;
+import com.liferay.data.engine.service.DEDataRecordCollectionSearchResponse;
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.dynamic.data.lists.model.DDLRecordSetConstants;
+import com.liferay.dynamic.data.lists.service.DDLRecordSetLocalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Marcelo Mello
+ */
+public class DEDataRecordCollectionSearchExecutor {
+
+	public DEDataRecordCollectionSearchExecutor(
+		DEDataEngineRequestExecutor deDataEngineRequestExecutor,
+		DDLRecordSetLocalService ddlRecordSetLocalService) {
+
+		_deDataEngineRequestExecutor = deDataEngineRequestExecutor;
+		_ddlRecordSetLocalService = ddlRecordSetLocalService;
+	}
+
+	public DEDataRecordCollectionSearchResponse execute(
+		DEDataRecordCollectionSearchRequest
+			deDataRecordCollectionSearchRequest) throws Exception {
+
+		List<DEDataRecordCollection> deDataRecordCollections =
+			new ArrayList<>();
+
+		List<DDLRecordSet> ddlRecordSets = _ddlRecordSetLocalService.search(
+			deDataRecordCollectionSearchRequest.getCompanyId(),
+			deDataRecordCollectionSearchRequest.getGroupId(),
+			deDataRecordCollectionSearchRequest.getKeywords(),
+			DDLRecordSetConstants.SCOPE_DYNAMIC_DATA_LISTS,
+			deDataRecordCollectionSearchRequest.getStart(),
+			deDataRecordCollectionSearchRequest.getEnd(), null);
+
+		for (DDLRecordSet ddlRecordSet : ddlRecordSets) {
+			deDataRecordCollections.add(
+				_deDataEngineRequestExecutor.map(ddlRecordSet));
+		}
+
+		return DEDataRecordCollectionSearchResponse.Builder.of(
+			deDataRecordCollections);
+	}
+
+	private final DDLRecordSetLocalService _ddlRecordSetLocalService;
+	private final DEDataEngineRequestExecutor _deDataEngineRequestExecutor;
+
+}
