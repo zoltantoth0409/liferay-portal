a9eba87ada9c4a0ee3b95daf2430aa6870c51537
rubenpulido <ruben.pulido@liferay.com>
2019-02-21 12:08:19 +0100

    LPS-90415 Fix tests

--- modules/apps/headless/headless-document-library/headless-document-library-test/src/testIntegration/java/com/liferay/headless/document/library/resource/v1_0/test/FolderResourceTest.java
+++ modules/apps/headless/headless-document-library/headless-document-library-test/src/testIntegration/java/com/liferay/headless/document/library/resource/v1_0/test/FolderResourceTest.java
@@ -17,17 +17,16 @@ package com.liferay.headless.document.library.resource.v1_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.headless.document.library.dto.v1_0.Folder;
 
+import java.util.Date;
 import java.util.Objects;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class FolderResourceTest extends BaseFolderResourceTestCase {
 
@@ -35,12 +34,12 @@ public class FolderResourceTest extends BaseFolderResourceTestCase {
 	public void testDeleteFolder() throws Exception {
 		Folder randomFolder = randomFolder();
 
-		invokePostContentSpaceFolder(testGroup.getGroupId(), randomFolder);
+		Folder postFolder = invokePostContentSpaceFolder(
+			testGroup.getGroupId(), randomFolder);
 
-		assertResponseCode(
-			202, invokeDeleteFolderResponse(randomFolder.getId()));
+		assertResponseCode(200, invokeDeleteFolderResponse(postFolder.getId()));
 
-		assertResponseCode(404, invokeGetFolderResponse(randomFolder.getId()));
+		assertResponseCode(404, invokeGetFolderResponse(postFolder.getId()));
 	}
 
 	@Test
@@ -51,6 +50,7 @@ public class FolderResourceTest extends BaseFolderResourceTestCase {
 		Folder getFolder = invokeGetFolder(postFolder.getId());
 
 		assertEquals(postFolder, getFolder);
+		assertValid(getFolder);
 	}
 
 	@Test
@@ -61,6 +61,7 @@ public class FolderResourceTest extends BaseFolderResourceTestCase {
 			testGroup.getGroupId(), randomFolder);
 
 		assertEquals(randomFolder, postFolder);
+		assertValid(postFolder);
 	}
 
 	@Test
@@ -74,6 +75,7 @@ public class FolderResourceTest extends BaseFolderResourceTestCase {
 			postContentSpaceFolder.getId(), randomFolder);
 
 		assertEquals(randomFolder, postFolderFolder);
+		assertValid(postFolderFolder);
 	}
 
 	@Test
@@ -86,23 +88,52 @@ public class FolderResourceTest extends BaseFolderResourceTestCase {
 		Folder putFolder = invokePutFolder(postFolder.getId(), randomFolder);
 
 		assertEquals(randomFolder, putFolder);
+		assertValid(putFolder);
 
 		Folder getFolder = invokeGetFolder(putFolder.getId());
 
 		assertEquals(randomFolder, getFolder);
+		assertValid(getFolder);
 	}
 
 	protected void assertEquals(Folder folder1, Folder folder2) {
 		boolean equals = false;
 
 		if (Objects.equals(
-				folder1.getDateCreated(), folder2.getDateCreated()) &&
-			Objects.equals(folder1.getId(), folder2.getId())) {
+				folder1.getDescription(), folder2.getDescription()) &&
+			Objects.equals(folder1.getName(), folder2.getName())) {
 
 			equals = true;
 		}
 
 		Assert.assertTrue(equals);
+	}
+
+	protected void assertValid(Folder folder) {
+		boolean valid = false;
+
+		if (!Objects.isNull(folder.getDateCreated()) &&
+			!Objects.isNull(folder.getDateModified()) &&
+			!Objects.isNull(folder.getId())) {
+
+			valid = true;
+		}
+
+		Assert.assertTrue(valid);
+	}
+
+	@Override
+	protected Folder randomFolder() {
+		Folder folder = super.randomFolder();
+
+		folder.setDateCreated((Date)null);
+		folder.setDateModified((Date)null);
+		folder.setHasDocuments((Boolean)null);
+		folder.setHasFolders((Boolean)null);
+		folder.setId((Long)null);
+		folder.setRepositoryId((Long)null);
+
+		return folder;
 	}
 
 }
