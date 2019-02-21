bef997c7e8708295dd50efd0eb2a7e9c84c68c70
David Arques <david.arques@liferay.com>
2019-02-21 13:19:22 +0100

    LPS-90436 Add SegmentsExperienceLocalServiceImpl

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/impl/SegmentsExperienceLocalServiceImpl.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.service.impl;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.segments.model.SegmentsExperience;
+import com.liferay.segments.service.base.SegmentsExperienceLocalServiceBaseImpl;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * The implementation of the segments experience local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.segments.service.SegmentsExperienceLocalService</code> interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author David Arques
+ * @see SegmentsExperienceLocalServiceBaseImpl
+ */
+public class SegmentsExperienceLocalServiceImpl
+	extends SegmentsExperienceLocalServiceBaseImpl {
+
+	@Override
+	public SegmentsExperience addSegmentsExperience(
+			long segmentsEntryId, long classNameId, long classPK,
+			Map<Locale, String> nameMap, int priority, boolean active,
+			ServiceContext serviceContext)
+
+		throws PortalException {
+
+		// Segments experience
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+
+		long groupId = serviceContext.getScopeGroupId();
+
+		long segmentsExperienceId = counterLocalService.increment();
+
+		SegmentsExperience segmentsExperience =
+			segmentsExperiencePersistence.create(segmentsExperienceId);
+
+		segmentsExperience.setGroupId(groupId);
+		segmentsExperience.setCompanyId(user.getCompanyId());
+		segmentsExperience.setUserId(user.getUserId());
+		segmentsExperience.setUserName(user.getFullName());
+		segmentsExperience.setCreateDate(
+			serviceContext.getCreateDate(new Date()));
+		segmentsExperience.setModifiedDate(
+			serviceContext.getModifiedDate(new Date()));
+		segmentsExperience.setSegmentsEntryId(segmentsEntryId);
+		segmentsExperience.setClassNameId(classNameId);
+		segmentsExperience.setClassPK(classPK);
+		segmentsExperience.setNameMap(nameMap);
+		segmentsExperience.setPriority(priority);
+		segmentsExperience.setActive(active);
+
+		segmentsExperiencePersistence.update(segmentsExperience);
+
+		// Resources
+
+		resourceLocalService.addModelResources(
+			segmentsExperience, serviceContext);
+
+		return segmentsExperience;
+	}
+
+	@Override
+	public SegmentsExperience deleteSegmentsExperience(
+			long segmentsExperienceId)
+		throws PortalException {
+
+		SegmentsExperience segmentsExperience =
+			segmentsExperiencePersistence.findByPrimaryKey(
+				segmentsExperienceId);
+
+		return segmentsExperienceLocalService.deleteSegmentsExperience(
+			segmentsExperience);
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public SegmentsExperience deleteSegmentsExperience(
+			SegmentsExperience segmentsExperience)
+		throws PortalException {
+
+		// Segments experience
+
+		segmentsExperiencePersistence.remove(segmentsExperience);
+
+		// Resources
+
+		resourceLocalService.deleteResource(
+			segmentsExperience, ResourceConstants.SCOPE_INDIVIDUAL);
+
+		return segmentsExperience;
+	}
+
+	@Override
+	public void deleteSegmentsExperiences(long groupId) throws PortalException {
+		List<SegmentsExperience> segmentsExperiences =
+			segmentsExperiencePersistence.findByGroupId(groupId);
+
+		for (SegmentsExperience segmentsExperience : segmentsExperiences) {
+			segmentsExperienceLocalService.deleteSegmentsExperience(
+				segmentsExperience.getSegmentsExperienceId());
+		}
+	}
+
+	@Override
+	public void deleteSegmentsExperiencesBySegmentsEntryId(long segmentsEntryId)
+		throws PortalException {
+
+		List<SegmentsExperience> segmentsExperiences =
+			segmentsExperiencePersistence.findBySegmentsEntryId(
+				segmentsEntryId);
+
+		for (SegmentsExperience segmentsExperience : segmentsExperiences) {
+			segmentsExperienceLocalService.deleteSegmentsExperience(
+				segmentsExperience.getSegmentsExperienceId());
+		}
+	}
+
+	@Override
+	public SegmentsExperience fetchSegmentsExperience(long segmentsExperienceId)
+	{
+
+		return segmentsExperiencePersistence.fetchByPrimaryKey(
+			segmentsExperienceId);
+	}
+
+	@Override
+	public SegmentsExperience getSegmentsExperience(long segmentsExperienceId)
+		throws PortalException {
+
+		return segmentsExperiencePersistence.findByPrimaryKey(
+			segmentsExperienceId);
+	}
+
+	@Override
+	public List<SegmentsExperience> getSegmentsExperiences(
+		long groupId, long classNameId, long classPK, boolean active, int start,
+		int end, OrderByComparator<SegmentsExperience> orderByComparator) {
+
+		return segmentsExperiencePersistence.findByG_C_C_A(
+			groupId, classNameId, classPK, active, start, end,
+			orderByComparator);
+	}
+
+	@Override
+	public int getSegmentsExperiencesCount(long groupId) {
+		return segmentsExperiencePersistence.countByGroupId(groupId);
+	}
+
+	@Override
+	public int getSegmentsExperiencesCount(
+		long groupId, long classNameId, long classPK, boolean active) {
+
+		return segmentsExperiencePersistence.countByG_C_C_A(
+			groupId, classNameId, classPK, active);
+	}
+
+	@Override
+	public SegmentsExperience updateSegmentsExperience(
+			long segmentsExperienceId, long segmentsEntryId,
+			Map<Locale, String> nameMap, int priority, boolean active)
+		throws PortalException {
+
+		SegmentsExperience segmentsExperience =
+			segmentsExperiencePersistence.findByPrimaryKey(
+				segmentsExperienceId);
+
+		segmentsExperience.setSegmentsEntryId(segmentsEntryId);
+		segmentsExperience.setNameMap(nameMap);
+		segmentsExperience.setPriority(priority);
+		segmentsExperience.setActive(active);
+
+		return segmentsExperiencePersistence.update(segmentsExperience);
+	}
+
+}
