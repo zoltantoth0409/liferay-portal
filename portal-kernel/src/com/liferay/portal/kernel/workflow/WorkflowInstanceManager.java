4b1df7d6eeec9f99b71dbbefad36221c3e0e6988
Inácio Nery <inacio.nery@liferay.com>
2019-02-21 10:12:43 -0300

    LPS-84450 Deprecated old operations and add new ones as default to prevent bumper

--- portal-kernel/src/com/liferay/portal/kernel/workflow/WorkflowInstanceManager.java
+++ portal-kernel/src/com/liferay/portal/kernel/workflow/WorkflowInstanceManager.java
@@ -76,29 +76,46 @@ public interface WorkflowInstanceManager {
 			int end, OrderByComparator<WorkflowInstance> orderByComparator)
 		throws WorkflowException;
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), replaced by {@link #search(long, Long,
+	 *             String, String, String, String, String, Boolean, int, int,
+	 *             OrderByComparator)}
+	 */
+	@Deprecated
 	public List<WorkflowInstance> search(
 			long companyId, Long userId, String assetType, String nodeName,
 			String kaleoDefinitionName, Boolean completed, int start, int end,
 			OrderByComparator<WorkflowInstance> orderByComparator)
 		throws WorkflowException;
 
-	public List<WorkflowInstance> search(
-			long companyId, Long userId, String assetDescription,
-			String assetTitle, String assetType, String nodeName,
+	public default List<WorkflowInstance> search(
+			long companyId, Long userId, String assetClassName,
+			String assetTitle, String assetDescription, String nodeName,
 			String kaleoDefinitionName, Boolean completed, int start, int end,
 			OrderByComparator<WorkflowInstance> orderByComparator)
-		throws WorkflowException;
+		throws WorkflowException {
 
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * @deprecated As of Mueller (7.2.x), replaced by {@link #searchCount(long,
+	 *             Long, String, String, String, String, String, Boolean)}
+	 */
+	@Deprecated
 	public int searchCount(
 			long companyId, Long userId, String assetType, String nodeName,
 			String kaleoDefinitionName, Boolean completed)
 		throws WorkflowException;
 
-	public int searchCount(
-			long companyId, Long userId, String assetDescription,
-			String assetTitle, String assetType, String nodeName,
+	public default int searchCount(
+			long companyId, Long userId, String assetClassName,
+			String assetTitle, String assetDescription, String nodeName,
 			String kaleoDefinitionName, Boolean completed)
-		throws WorkflowException;
+		throws WorkflowException {
+
+		throw new UnsupportedOperationException();
+	}
 
 	public WorkflowInstance signalWorkflowInstance(
 			long companyId, long userId, long workflowInstanceId,
