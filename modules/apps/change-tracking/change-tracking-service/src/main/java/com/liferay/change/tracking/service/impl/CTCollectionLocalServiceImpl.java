3f68cc304b9ea51dde698f06f2cf10ad86316c5c
Zoltán Császi <zoltan.csaszi@liferay.com>
2019-02-22 16:34:44 +0100

    LPS-90809 server-side validating name length

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTCollectionLocalServiceImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/impl/CTCollectionLocalServiceImpl.java
@@ -21,6 +21,7 @@ import com.liferay.change.tracking.model.CTProcess;
 import com.liferay.change.tracking.service.base.CTCollectionLocalServiceBaseImpl;
 import com.liferay.portal.kernel.dao.orm.QueryDefinition;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.Validator;
@@ -155,6 +156,13 @@ public class CTCollectionLocalServiceImpl
 	private void _validate(String name) throws CTCollectionNameException {
 		if (Validator.isNull(name)) {
 			throw new CTCollectionNameException();
+		}
+
+		int nameMaxLength = ModelHintsUtil.getMaxLength(
+			CTCollection.class.getName(), "name");
+
+		if (name.length() > nameMaxLength) {
+			throw new CTCollectionNameException("Name is too long");
 		}
 	}
 
