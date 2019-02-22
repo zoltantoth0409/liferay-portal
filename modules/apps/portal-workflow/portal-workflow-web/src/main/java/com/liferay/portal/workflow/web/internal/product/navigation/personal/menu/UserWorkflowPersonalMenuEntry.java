e9b66686eed4666fdc9db6fda73423571b3d3b4e
Pei-Jung Lan <pei-jung.lan@liferay.com>
2019-02-21 18:12:05 -0800

    LPS-90257 (portal-workflow-web) Register personal menu entry

---
+++ modules/apps/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/product/navigation/personal/menu/UserWorkflowPersonalMenuEntry.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.web.internal.product.navigation.personal.menu;
+
+import com.liferay.portal.workflow.web.internal.constants.WorkflowPortletKeys;
+import com.liferay.product.navigation.personal.menu.BasePersonalMenuEntry;
+import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"product.navigation.personal.menu.entry.order:Integer=400",
+		"product.navigation.personal.menu.group:Integer=200"
+	},
+	service = PersonalMenuEntry.class
+)
+public class UserWorkflowPersonalMenuEntry extends BasePersonalMenuEntry {
+
+	@Override
+	protected String getPortletId() {
+		return WorkflowPortletKeys.USER_WORKFLOW;
+	}
+
+}
