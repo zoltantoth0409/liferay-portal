27df83a8ea13ded9846ba768d12aa4f6fad8755a
kyle-miho <kyle.miho@liferay.com>
2019-02-21 16:41:54 -0800

    LPS-90741 Auto

--- portal-impl/src/com/liferay/portal/service/http/LayoutServiceSoap.java
+++ portal-impl/src/com/liferay/portal/service/http/LayoutServiceSoap.java
@@ -79,7 +79,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param parentLayoutId the primary key of the parent layout (optionally
+	* @param parentLayoutId the layout ID of the parent layout (optionally
 	{@link LayoutConstants#DEFAULT_PARENT_LAYOUT_ID})
 	* @param localeNamesMap the layout's locales and localized names
 	* @param localeTitlesMap the layout's locales and localized titles
@@ -158,7 +158,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param parentLayoutId the primary key of the parent layout (optionally
+	* @param parentLayoutId the layout ID of the parent layout (optionally
 	{@link LayoutConstants#DEFAULT_PARENT_LAYOUT_ID})
 	* @param name the layout's locales and localized names
 	* @param title the layout's locales and localized titles
@@ -206,7 +206,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
+	* @param layoutId the layout ID of the layout
 	* @param serviceContext the service context to be applied
 	* @throws PortalException if a portal exception occurred
 	*/
@@ -435,7 +435,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
+	* @param layoutId the layout ID of the layout
 	* @param languageId the primary key of the language. For more information
 	See {@link Locale}.
 	* @return the layout's name
@@ -704,7 +704,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param parentLayoutId the primary key of the parent layout
+	* @param parentLayoutId the layout ID of the parent layout
 	* @param layoutIds the primary keys of the layouts
 	* @param serviceContext the service context to be applied
 	* @throws PortalException if a portal exception occurred
@@ -790,8 +790,8 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
-	* @param parentLayoutId the primary key of the layout's new parent layout
+	* @param layoutId the layout ID of the layout
+	* @param parentLayoutId the layout ID of the layout's new parent layout
 	* @param localeNamesMap the layout's locales and localized names
 	* @param localeTitlesMap the layout's locales and localized titles
 	* @param descriptionMap the locales and localized descriptions to merge
@@ -860,7 +860,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
+	* @param layoutId the layout ID of the layout
 	* @param typeSettings the settings to load the unicode properties object.
 	See {@link com.liferay.portal.kernel.util.UnicodeProperties
 	#fastLoad(String)}.
@@ -888,7 +888,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
+	* @param layoutId the layout ID of the layout
 	* @param themeId the primary key of the layout's new theme
 	* @param colorSchemeId the primary key of the layout's new color scheme
 	* @param css the layout's new CSS
@@ -917,7 +917,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
+	* @param layoutId the layout ID of the layout
 	* @param name the layout's new name
 	* @param languageId the primary key of the language. For more information
 	see {@link Locale}.
@@ -971,8 +971,8 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
-	* @param parentLayoutId the primary key to be assigned to the parent
+	* @param layoutId the layout ID of the layout
+	* @param parentLayoutId the layout ID to be assigned to the parent
 	layout
 	* @return the matching layout
 	* @throws PortalException if a portal exception occurred
@@ -1049,7 +1049,7 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
+	* @param layoutId the layout ID of the layout
 	* @param priority the layout's new priority
 	* @return the updated layout
 	* @throws PortalException if a portal exception occurred
@@ -1077,9 +1077,9 @@ public class LayoutServiceSoap {
 	*
 	* @param groupId the primary key of the group
 	* @param privateLayout whether the layout is private to the group
-	* @param layoutId the primary key of the layout
-	* @param nextLayoutId the primary key of the next layout
-	* @param previousLayoutId the primary key of the previous layout
+	* @param layoutId the layout ID of the layout
+	* @param nextLayoutId the layout ID of the next layout
+	* @param previousLayoutId the layout ID of the previous layout
 	* @return the updated layout
 	* @throws PortalException if a portal exception occurred
 	*/
