1e5cba4ec79983da2b966a0236146644e30f5fdd
Pei-Jung Lan <pei-jung.lan@liferay.com>
2019-02-21 18:30:23 -0800

    LPS-90257 (my-subscriptions-web) Register personal menu entry

---
+++ modules/apps/my-subscriptions/my-subscriptions-web/src/main/java/com/liferay/my/subscriptions/web/internal/product/navigation/personal/menu/MySubscriptionsPersonalMenuEntry.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.my.subscriptions.web.internal.product.navigation.personal.menu;
+
+import com.liferay.my.subscriptions.web.internal.constants.MySubscriptionsPortletKeys;
+import com.liferay.product.navigation.personal.menu.BasePersonalMenuEntry;
+import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"product.navigation.personal.menu.entry.order:Integer=500",
+		"product.navigation.personal.menu.group:Integer=300"
+	},
+	service = PersonalMenuEntry.class
+)
+public class MySubscriptionsPersonalMenuEntry extends BasePersonalMenuEntry {
+
+	@Override
+	protected String getPortletId() {
+		return MySubscriptionsPortletKeys.MY_SUBSCRIPTIONS;
+	}
+
+}
