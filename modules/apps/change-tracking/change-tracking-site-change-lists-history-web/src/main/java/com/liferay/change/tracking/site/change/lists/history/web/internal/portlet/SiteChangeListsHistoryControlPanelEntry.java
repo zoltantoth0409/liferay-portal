b1ede55a5e3eb66452ab6650470982e0b000b743
Zoltán Császi <zoltan.csaszi@liferay.com>
2019-02-22 11:43:24 +0100

    LPS-90992 site change lists menu item is not visible in site or page template scope

---
+++ modules/apps/change-tracking/change-tracking-site-change-lists-history-web/src/main/java/com/liferay/change/tracking/site/change/lists/history/web/internal/portlet/SiteChangeListsHistoryControlPanelEntry.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.site.change.lists.history.web.internal.portlet;
+
+import com.liferay.change.tracking.constants.CTPortletKeys;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.BaseControlPanelEntry;
+import com.liferay.portal.kernel.portlet.ControlPanelEntry;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Zoltan Csaszi
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + CTPortletKeys.SITE_CHANGE_LISTS_HISTORY,
+	service = ControlPanelEntry.class
+)
+public class SiteChangeListsHistoryControlPanelEntry
+	extends BaseControlPanelEntry {
+
+	@Override
+	protected boolean hasAccessPermissionDenied(
+			PermissionChecker permissionChecker, Group group, Portlet portlet)
+		throws Exception {
+
+		if (group.isLayoutPrototype() || group.isLayoutSetPrototype()) {
+			return true;
+		}
+
+		return super.hasAccessPermissionDenied(
+			permissionChecker, group, portlet);
+	}
+
+}
