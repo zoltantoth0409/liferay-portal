78cdd20e7ed698642f3db832a4dae5d3cf20c33e
gregory-bretall <gregory.bretall@liferay.com>
2019-02-22 14:14:59 -0800

    LPS-90506 Source formatting

--- portal-kernel/src/com/liferay/portal/kernel/util/DateUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/DateUtil.java
@@ -95,13 +95,6 @@ public class DateUtil {
 		return dateFormat.format(date);
 	}
 
-	public static int getYear(Date date) {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(date);
-
-		return cal.get(Calendar.YEAR);
-	}
-
 	public static int getDaysBetween(Date date1, Date date2) {
 		return getDaysBetween(date1, date2, null);
 	}
@@ -217,6 +210,14 @@ public class DateUtil {
 
 		return DateFormatFactoryUtil.getSimpleDateFormat(
 			pattern, TimeZoneUtil.getTimeZone(StringPool.UTC));
+	}
+
+	public static int getYear(Date date) {
+		Calendar cal = Calendar.getInstance();
+
+		cal.setTime(date);
+
+		return cal.get(Calendar.YEAR);
 	}
 
 	public static boolean isFormatAmPm(Locale locale) {
