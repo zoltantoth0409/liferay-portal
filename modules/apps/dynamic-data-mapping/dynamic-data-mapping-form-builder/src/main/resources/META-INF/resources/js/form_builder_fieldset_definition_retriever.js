f263723dc1ede82edce7e3fa8c39e24d6cb72225
ChrisKian <christopher.kian@liferay.com>
2019-02-22 17:24:45 -0800

    LPS-90515 use the site default language when parsing fields

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder_fieldset_definition_retriever.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/form_builder_fieldset_definition_retriever.js
@@ -23,7 +23,7 @@ AUI.add(
 
 								var payload = {
 									ddmStructureId: fieldSet.get('id'),
-									languageId: themeDisplay.getLanguageId(),
+									languageId: themeDisplay.getDefaultLanguageId(),
 									portletNamespace: Liferay.DDM.Settings.portletNamespace,
 									scopeGroupId: themeDisplay.getScopeGroupId()
 								};
