9e70a75f89e7578fdba9353dc3e5aad607cc2fac
David Arques <david.arques@liferay.com>
2019-02-25 16:54:18 +0100

    LPS-91056 Delete related experience

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/model/listener/LayoutModelListener.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.model.listener;
+
+import com.liferay.layout.constants.LayoutConstants;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
+
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Arques
+ */
+@Component(service = ModelListener.class)
+public class LayoutModelListener extends BaseModelListener<Layout> {
+
+	@Override
+	public void onAfterRemove(Layout layout) throws ModelListenerException {
+		if (!_isContentLayout(layout)) {
+			return;
+		}
+
+		try {
+			_segmentsExperienceLocalService.deleteSegmentsExperiences(
+				layout.getGroupId(),
+				_classNameLocalService.getClassNameId(Layout.class),
+				layout.getPrimaryKey());
+		}
+		catch (Exception e) {
+			throw new ModelListenerException(e);
+		}
+	}
+
+	private boolean _isContentLayout(Layout layout) {
+		if (Objects.equals(
+				layout.getType(), LayoutConstants.LAYOUT_TYPE_CONTENT)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+}
