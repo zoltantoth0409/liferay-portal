f678ff465c4271e534c782804e4b016f3ca19d82
Brian Chan <brian.chan@liferay.com>
2019-02-25 13:33:31 -0800

    LPS-90505 Just once?

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/template/TemplateHandlerRegistryImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/template/TemplateHandlerRegistryImpl.java
@@ -241,6 +241,8 @@ public class TemplateHandlerRegistryImpl implements TemplateHandlerRegistry {
 
 			ServiceContext serviceContext = new ServiceContext();
 
+			serviceContext.setAddGuestPermissions(true);
+
 			Group group = _groupLocalService.getCompanyGroup(
 				company.getCompanyId());
 
@@ -309,8 +311,6 @@ public class TemplateHandlerRegistryImpl implements TemplateHandlerRegistry {
 
 				boolean cacheable = GetterUtil.getBoolean(
 					templateElement.elementText("cacheable"));
-
-				serviceContext.setAddGuestPermissions(true);
 
 				if (ddmTemplate == null) {
 					_ddmTemplateManager.addTemplate(
