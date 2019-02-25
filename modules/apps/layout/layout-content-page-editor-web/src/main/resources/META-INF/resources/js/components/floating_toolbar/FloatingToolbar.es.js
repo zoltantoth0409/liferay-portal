fdc33e407348a5406b9409633c0fbf7c270af6eb
Sandro Chinea <sandrochinea@gmail.com>
2019-02-25 17:35:41 +0100

    LPS-90911 Added method to clear active item on floating toolbar focusout

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/FloatingToolbar.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/FloatingToolbar.es.js
@@ -3,7 +3,9 @@ import Soy from 'metal-soy';
 import {Align} from 'metal-position';
 import {Config} from 'metal-state';
 
+import {CLEAR_ACTIVE_ITEM} from '../../actions/actions.es';
 import getConnectedComponent from '../../store/ConnectedComponent.es';
+import {shouldClearFocus} from '../../utils/FragmentsEditorUpdateUtils.es';
 import templates from './FloatingToolbar.soy';
 
 /**
@@ -54,6 +56,18 @@ class FloatingToolbar extends Component {
 		);
 
 		return selectedPanelId;
+	}
+
+	_handleFloatingToolbarFocusOut() {
+		requestAnimationFrame(
+			() => {
+				if (shouldClearFocus(this.anchorElement)) {
+					this.store.dispatchAction(
+						CLEAR_ACTIVE_ITEM
+					);
+				}
+			}
+		);
 	}
 
 	/**
