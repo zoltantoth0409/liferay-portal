0eaffdc1c5e70db813cf93f38a9119d451cb9ca2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-02-26 09:17:08 +0100

    LPS-91001 Creates new asset list provider interfaces to allow contribute an asset list as code

---
+++ modules/apps/asset/asset-list-api/src/main/java/com/liferay/asset/list/provider/AssetListProvider.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.provider;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public interface AssetListProvider {
+
+	public List<AssetEntry> getAssetEntries(PortletRequest portletRequest);
+
+	public List<AssetEntry> getAssetEntries(
+		PortletRequest portletRequest, int start, int end);
+
+	public int getAssetEntriesCount(PortletRequest portletRequest);
+
+	public String getLabel(Locale locale);
+
+	public default boolean isViewable(PortletRequest portletRequest) {
+		return true;
+	}
+
+}
