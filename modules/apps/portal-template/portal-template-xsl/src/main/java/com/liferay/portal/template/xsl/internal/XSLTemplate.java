06c9f9ea3c9975d7fba9ff3ac477ec3b01ca1950
tina.tian <tina.tian@liferay.com>
2019-02-25 21:22:50 -0800

    LPS-89354 Avoid Warning: encoding "UTF-8" not supported, using UTF-8. This is caused by a properties file from xalan.jar can not be found by current context classloader.

--- modules/apps/portal-template/portal-template-xsl/src/main/java/com/liferay/portal/template/xsl/internal/XSLTemplate.java
+++ modules/apps/portal-template/portal-template-xsl/src/main/java/com/liferay/portal/template/xsl/internal/XSLTemplate.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.template.Template;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.template.TemplateException;
 import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.util.AggregateClassLoader;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.template.TemplateContextHelper;
@@ -76,9 +77,11 @@ public class XSLTemplate implements Template {
 
 		Class<?> transformerFactoryClass = TransformerFactoryImpl.class;
 
+		_transformerFactoryClassLoader =
+			transformerFactoryClass.getClassLoader();
+
 		_transformerFactory = TransformerFactory.newInstance(
-			transformerFactoryClass.getName(),
-			transformerFactoryClass.getClassLoader());
+			transformerFactoryClass.getName(), _transformerFactoryClassLoader);
 
 		try {
 			_transformerFactory.setFeature(
@@ -202,6 +205,14 @@ public class XSLTemplate implements Template {
 
 	@Override
 	public void processTemplate(Writer writer) throws TemplateException {
+		Thread currentThread = Thread.currentThread();
+
+		ClassLoader contextClassLoader = currentThread.getContextClassLoader();
+
+		currentThread.setContextClassLoader(
+			AggregateClassLoader.getAggregateClassLoader(
+				contextClassLoader, _transformerFactoryClassLoader));
+
 		try {
 			doProcessTemplate(writer);
 		}
@@ -243,6 +254,9 @@ public class XSLTemplate implements Template {
 						_errorTemplateResource.getTemplateId(),
 					e2);
 			}
+		}
+		finally {
+			currentThread.setContextClassLoader(contextClassLoader);
 		}
 	}
 
@@ -304,6 +318,7 @@ public class XSLTemplate implements Template {
 	private final boolean _preventLocalConnections;
 	private final TemplateContextHelper _templateContextHelper;
 	private final TransformerFactory _transformerFactory;
+	private final ClassLoader _transformerFactoryClassLoader;
 	private StreamSource _xmlStreamSource;
 	private final XSLTemplateResource _xslTemplateResource;
 
