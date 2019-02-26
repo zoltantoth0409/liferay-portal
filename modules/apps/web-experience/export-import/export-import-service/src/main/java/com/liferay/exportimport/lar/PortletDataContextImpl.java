e85c64a73158e0643c84ac7ec852924201b1b4ff
Michael Prigge <michael.prigge@liferay.com>
2019-02-26 15:21:20 -0800

    LPS-83326 Rename method

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/lar/PortletDataContextImpl.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/lar/PortletDataContextImpl.java
@@ -2678,6 +2678,16 @@ public class PortletDataContextImpl implements PortletDataContext {
 		}
 	}
 
+	private long _getOldPrimaryKey(Map<Long, Long> map, long value) {
+		for (Map.Entry<Long, Long> entry : map.entrySet()) {
+			if (entry.getValue() == value) {
+				return entry.getKey();
+			}
+		}
+
+		return 0;
+	}
+
 	private String _getPortletXmlPath() {
 		if (_exportDataRootElement == null) {
 			return StringPool.BLANK;
@@ -2698,16 +2708,6 @@ public class PortletDataContextImpl implements PortletDataContext {
 		}
 
 		return parentElement.attributeValue("self-path");
-	}
-
-	private long _getKeyFromMap(Map<Long, Long> map, long value) {
-		for (Map.Entry<Long, Long> entry : map.entrySet()) {
-			if (entry.getValue() == value) {
-				return entry.getKey();
-			}
-		}
-
-		return 0;
 	}
 
 	private void _importWorkflowDefinitionLink(ClassedModel classedModel)
@@ -2734,7 +2734,7 @@ public class PortletDataContextImpl implements PortletDataContext {
 			long newPrimaryKey = GetterUtil.getLong(
 				classedModel.getPrimaryKeyObj());
 
-			long oldPrimaryKey = _getKeyFromMap(primaryKeys, newPrimaryKey);
+			long oldPrimaryKey = _getOldPrimaryKey(primaryKeys, newPrimaryKey);
 
 			if (!referrerClassName.equals(className) ||
 				(referrerClassPK != oldPrimaryKey)) {
