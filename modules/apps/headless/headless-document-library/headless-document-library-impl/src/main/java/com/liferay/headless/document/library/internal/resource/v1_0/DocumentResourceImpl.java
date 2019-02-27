bfc894f5345e27ea9d98c6373202a7190f20d1be
Alejandro Hernández <info@alejandrohdezma.com>
2019-02-27 17:33:18 +0100

    LPS-89732 Fix bug in DocumentResourceImpl

--- modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/v1_0/DocumentResourceImpl.java
+++ modules/apps/headless/headless-document-library/headless-document-library-impl/src/main/java/com/liferay/headless/document/library/internal/resource/v1_0/DocumentResourceImpl.java
@@ -178,7 +178,7 @@ public class DocumentResourceImpl
 		);
 
 		String description = optional.map(
-			Document::getTitle
+			Document::getDescription
 		).orElseGet(
 			existingFileEntry::getDescription
 		);
