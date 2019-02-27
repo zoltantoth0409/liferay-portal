9da14460e35a780f0b6499a0173e0e7eff635e8f
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2019-02-27 22:20:52 +0100

    LPS-91331 Escape HTML tags in JSON

--- portal-kernel/src/com/liferay/portal/kernel/portlet/LiferayPortlet.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/LiferayPortlet.java
@@ -653,7 +653,7 @@ public class LiferayPortlet extends GenericPortlet {
 
 		response.setContentType(getJSONContentType(portletRequest));
 
-		ServletResponseUtil.write(response, jsonObj.toString());
+		ServletResponseUtil.write(response, _toXSSSafeJSON(jsonObj.toString()));
 
 		response.flushBuffer();
 	}
@@ -665,7 +665,8 @@ public class LiferayPortlet extends GenericPortlet {
 
 		mimeResponse.setContentType(getJSONContentType(portletRequest));
 
-		PortletResponseUtil.write(mimeResponse, jsonObj.toString());
+		PortletResponseUtil.write(
+			mimeResponse, _toXSSSafeJSON(jsonObj.toString()));
 
 		mimeResponse.flushBuffer();
 	}
@@ -673,6 +674,10 @@ public class LiferayPortlet extends GenericPortlet {
 	protected boolean addProcessActionSuccessMessage;
 	protected boolean alwaysSendRedirect;
 	protected Set<String> validPaths;
+
+	private String _toXSSSafeJSON(String json) {
+		return StringUtil.replace(json, CharPool.LESS_THAN, "\\u003c");
+	}
 
 	private static final String[] _IGNORED_SESSION_MESSAGE_SUFFIXES = {
 		SessionMessages.KEY_SUFFIX_DELETE_SUCCESS_DATA,
