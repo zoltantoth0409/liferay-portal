59690f56d4c4f2188a81f3cbd18bd19d2ece2e40
David Arques <david.arques@liferay.com>
2019-02-27 17:45:26 +0100

    LPS-91058 Regiter model resource permission

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/security/permission/resource/SegmentsExperienceModelResourcePermissionRegistrar.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.security.permission.resource;
+
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.segments.constants.SegmentsConstants;
+import com.liferay.segments.model.SegmentsExperience;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Arques
+ */
+@Component(immediate = true, service = {})
+public class SegmentsExperienceModelResourcePermissionRegistrar {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("model.class.name", SegmentsExperience.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				SegmentsExperience.class,
+				SegmentsExperience::getSegmentsExperienceId,
+				_segmentsExperienceLocalService::getSegmentsExperience,
+				_portletResourcePermission,
+				(modelResourcePermission, consumer) -> {
+				}),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference(
+		target = "(resource.name=" + SegmentsConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	@Reference
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+}
