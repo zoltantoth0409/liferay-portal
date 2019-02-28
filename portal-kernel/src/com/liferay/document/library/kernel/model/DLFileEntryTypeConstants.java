79e0723655c95b33277685f23f24f3f84286a60f
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-02-28 15:27:01 +0100

    LPS-83172 Deprecating

--- portal-kernel/src/com/liferay/document/library/kernel/model/DLFileEntryTypeConstants.java
+++ portal-kernel/src/com/liferay/document/library/kernel/model/DLFileEntryTypeConstants.java
@@ -32,25 +32,53 @@ public class DLFileEntryTypeConstants {
 	public static final String FILE_ENTRY_TYPE_KEY_IG_IMAGE =
 		"IMAGE GALLERY IMAGE";
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String FILE_ENTRY_TYPE_KEY_MARKETING_BANNER =
 		"MARKETING BANNER";
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String FILE_ENTRY_TYPE_KEY_ONLINE_TRAINING =
 		"ONLINE TRAINING";
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String FILE_ENTRY_TYPE_KEY_SALES_PRESENTATION =
 		"SALES PRESENTATION";
 
 	public static final String NAME_BASIC_DOCUMENT = "basic-document";
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String NAME_CONTRACT = "contract";
 
 	public static final String NAME_IG_IMAGE = "image-gallery-image";
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String NAME_MARKETING_BANNER = "marketing-banner";
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String NAME_ONLINE_TRAINING = "online-training";
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String NAME_SALES_PRESENTATION = "sales-presentation";
 
 }
