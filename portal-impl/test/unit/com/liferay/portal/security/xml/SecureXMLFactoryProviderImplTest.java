926f44bc449e8b5ccfbe04da55d8150db846dfbe
Hugo Huijser <hugo.huijser@liferay.com>
2019-02-28 21:00:45 +0800

    LPS-86806 Avoid chaining

--- portal-impl/test/unit/com/liferay/portal/security/xml/SecureXMLFactoryProviderImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/security/xml/SecureXMLFactoryProviderImplTest.java
@@ -29,6 +29,7 @@ import java.net.ConnectException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -123,9 +124,11 @@ public class SecureXMLFactoryProviderImplTest {
 
 			@Override
 			public void run(String xml) throws Exception {
+				XMLInputFactory xmlInputFactory =
+					_secureXMLFactoryProvider.newXMLInputFactory();
+
 				XMLEventReader xmlEventReader =
-					_secureXMLFactoryProvider.newXMLInputFactory().
-						createXMLEventReader(new StringReader(xml));
+					xmlInputFactory.createXMLEventReader(new StringReader(xml));
 
 				while (xmlEventReader.hasNext()) {
 					xmlEventReader.next();
