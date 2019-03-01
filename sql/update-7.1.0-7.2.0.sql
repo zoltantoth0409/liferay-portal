3b46cba6bb4e91eda20b411abd46f119e4282336
Pavel Savinov <pavel.savinov@liferay.com>
2019-03-01 08:34:40 +0100

    LPS-91286 Add missing columns to the upgrade sql file

--- sql/update-7.1.0-7.2.0.sql
+++ sql/update-7.1.0-7.2.0.sql
@@ -3,6 +3,10 @@ alter table Layout add leftPlid LONG;
 alter table Layout add rightPlid LONG;
 alter table Layout add system BOOLEAN;
 
+alter table Layout add classNameId LONG;
+alter table Layout add classPK LONG;
+alter table Layout add publishDate DATE null;
+
 COMMIT_TRANSACTION;
 
 update Layout set system = FALSE;
