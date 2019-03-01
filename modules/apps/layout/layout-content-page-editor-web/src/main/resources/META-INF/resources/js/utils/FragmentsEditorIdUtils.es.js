9b7c67fe873ed2724bc5b49338501f97a9939c52
Sandro Chinea <sandrochinea@gmail.com>
2019-03-01 12:12:27 +0100

    LPS-89681 Added functions to manage ids in new IdUtils file and tests for them

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/FragmentsEditorIdUtils.es.js
@@ -0,0 +1,61 @@
+import {setIn} from './FragmentsEditorUpdateUtils.es';
+
+/**
+ * Adds encoded id to an object and creating a copy of the original one
+ * @param {!object} obj Original object
+ * @param {!array} ids Array with id fields to be encoded
+ * @private
+ */
+function _addEncodedId(obj, ids) {
+	let idObj = {};
+
+	ids.forEach(
+		id => {
+			idObj[id] = obj[id];
+		}
+	);
+
+	return setIn(
+		obj,
+		['encodedId'],
+		_encodeId(idObj)
+	);
+}
+
+/**
+ * Encode an id object and returns encoded id
+ * @param {!obj} obj
+ */
+const _encodeId = obj => btoa(JSON.stringify(obj));
+
+/**
+ * Decode an id and returns original object
+ * @param {!string} str
+ */
+const decodeId = str => JSON.parse(atob(str));
+
+/**
+ * @param {!object} asset Asset object
+ * @return {object} Asset object with encoded id added
+ */
+function encodeAssetId(asset) {
+	if (!(asset && typeof asset === 'object')) {
+		throw new TypeError('Expect input to be an object');
+	}
+	else if (!(asset.assetEntryClassNameId && asset.assetEntryClassPK)) {
+		throw new Error('Expect input to be an asset');
+	}
+
+	return _addEncodedId(
+		asset,
+		[
+			'assetEntryClassNameId',
+			'assetEntryClassPK'
+		]
+	);
+}
+
+export {
+	decodeId,
+	encodeAssetId
+};
---
+++ modules/apps/layout/layout-content-page-editor-web/test/js/utils/FragmentsEditorUpdateUtils.test.js
@@ -0,0 +1,74 @@
+import {decodeId, encodeAssetId} from "../../../src/main/resources/META-INF/resources/js/utils/FragmentsEditorIdUtils.es";
+
+describe(
+	'FragmentsEditorIdUtils', 
+	() => {
+		describe(
+			'#decodeId',
+			() => {
+				it(
+					'should decode a given base64 encoded object',
+					() => {
+						const obj = {a: 'a', b: 2};
+						const str = btoa(JSON.stringify(obj));
+
+						expect(decodeId(str)).toEqual(obj);
+					}
+				);
+
+				it(
+					'should throw an error when string is not base64 encoded',
+					() => {
+						const str = 'not base64 encoded';
+
+						expect(() => decodeId(str)).toThrow();
+					}
+				);
+
+				it(
+					'should throw an error when string is not an object',
+					() => {
+						const str = btoa('not an object');
+
+						expect(() => decodeId(str)).toThrow();
+					}
+				);
+			}
+		);
+
+		describe(
+			'#encodeAssetId',
+			() => {
+				it(
+					'should add an encoded id to a given asset object',
+					() => {
+						const asset = {
+							assetEntryClassNameId: 11111,
+							assetEntryClassPK: 22222,
+							assetEntryTitle: 'My asset'
+						};
+
+						expect(encodeAssetId(asset)).toMatchSnapshot();
+					}
+				);
+
+				it(
+					'should throw an error when input is not an object',
+					() => {
+						const input = 'not an object';
+
+						expect(() => encodeAssetId(input)).toThrowError();
+					}
+				);
+
+				it(
+					'should throw an error when input is not an asset',
+					() => {
+						const input = {a: 'a', b: 2};
+						expect(() => encodeAssetId(input)).toThrowError();
+					}
+				);
+			}
+		);
+	}
+);
