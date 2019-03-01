31ced8f6ab0e6b195b3d05650160d9654a33c892
alee8888 <albert.lee@liferay.com>
2019-02-28 17:00:21 -0800

    LPS-90541 Add Custom Field manage portlet provider service

---
+++ modules/apps/expando/expando-web/src/main/java/com/liferay/expando/web/internal/portlet/ExpandoManagePortletProvider.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.expando.web.internal.portlet;
+
+import com.liferay.expando.constants.ExpandoPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
+import com.liferay.portal.kernel.portlet.ManagePortletProvider;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Albert Lee
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.expando.kernel.model.ExpandoColumn",
+	service = ManagePortletProvider.class
+)
+public class ExpandoManagePortletProvider
+	extends BasePortletProvider implements ManagePortletProvider {
+
+	@Override
+	public String getPortletName() {
+		return ExpandoPortletKeys.EXPANDO;
+	}
+
+	@Override
+	public PortletURL getPortletURL(HttpServletRequest request)
+		throws PortalException {
+
+		PortletURL portletURL = super.getPortletURL(request);
+
+		portletURL.setParameter("mvcPath", "/view_attributes.jsp");
+
+		return portletURL;
+	}
+
+}
