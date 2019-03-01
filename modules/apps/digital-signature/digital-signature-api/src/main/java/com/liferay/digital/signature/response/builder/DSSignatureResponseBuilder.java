d8ca67dcac36edd586555d315104d923d6e6d117
Michael Han <michael.han@liferay.com>
2019-03-01 12:39:58 -0600

    LPS-90590 digital-signature-api: add response API

---
+++ modules/apps/digital-signature/digital-signature-api/src/main/java/com/liferay/digital/signature/response/builder/DSSignatureResponseBuilder.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.digital.signature.response.builder;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.digital.signature.DSSignaturePackageStatus;
+import com.liferay.digital.signature.response.DSSignatureResponse;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public interface DSSignatureResponseBuilder {
+
+	public DSSignatureResponse getDSSignatureResponse();
+
+	public DSSignatureResponseBuilder setDSSignatureRequestStatus(
+		DSSignaturePackageStatus dsSignaturePackageStatus);
+
+	public DSSignatureResponseBuilder setErrorCode(String errorCode);
+
+	public DSSignatureResponseBuilder setErrorDebugMessage(
+		String errorDebugMessage);
+
+	public DSSignatureResponseBuilder setErrorMessage(String errorMessage);
+
+	public DSSignatureResponseBuilder setExternalReferenceId(
+		String externalReferenceId);
+
+	public DSSignatureResponseBuilder setExternalReferenceURI(
+		String externalReferenceURI);
+
+	public DSSignatureResponseBuilder setTimestamp(ZonedDateTime timestamp);
+
+}
