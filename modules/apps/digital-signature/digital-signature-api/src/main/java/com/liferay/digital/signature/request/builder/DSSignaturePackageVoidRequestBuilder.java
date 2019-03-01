4a7f7aabe5744c3557b603a2f24a406b52cf09cc
Michael Han <michael.han@liferay.com>
2019-03-01 12:39:41 -0600

    LPS-90590 digital-signature-api: add request builders

---
+++ modules/apps/digital-signature/digital-signature-api/src/main/java/com/liferay/digital/signature/request/builder/DSSignaturePackageVoidRequestBuilder.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.digital.signature.request.builder;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.digital.signature.request.DSSignaturePackageVoidRequest;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public interface DSSignaturePackageVoidRequestBuilder {
+
+	public DSSignaturePackageVoidRequest getDSSignaturePackageVoidRequest();
+
+	public DSSignaturePackageVoidRequestBuilder setDSSignaturePackageId(
+		String dsSignaturePackageId);
+
+	public DSSignaturePackageVoidRequestBuilder setExternalReferenceId(
+		String externalReferenceId);
+
+	public DSSignaturePackageVoidRequestBuilder setVoidReason(
+		String voidReason);
+
+}
---
+++ modules/apps/digital-signature/digital-signature-api/src/main/java/com/liferay/digital/signature/request/builder/DSSignatureRequestBuildersFactory.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.digital.signature.request.builder;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.digital.signature.model.DSSessionId;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public interface DSSignatureRequestBuildersFactory {
+
+	public DSSignaturePackageCreateRequestBuilder
+		createDSSignaturePackageCreateRequestBuilder(DSSessionId dsSessionId);
+
+	public DSSignaturePackageVoidRequestBuilder
+		createDSSignaturePackageVoidRequestBuilder(DSSessionId dsSessionId);
+
+}
