e54822a498659e85e77343e74eaeedc71bcb4ac2
Julien Castelain <julien.castelain@liferay.com>
2019-03-01 13:20:01 +0100

    LPS-90610 Include a poyfill for Array.prototype.includes

--- modules/apps/frontend-compatibility/frontend-compatibility-ie/src/main/java/com/liferay/frontend/compatibility/ie/internal/servlet/taglib/IETopHeadDynamicInclude.java
+++ modules/apps/frontend-compatibility/frontend-compatibility-ie/src/main/java/com/liferay/frontend/compatibility/ie/internal/servlet/taglib/IETopHeadDynamicInclude.java
@@ -71,8 +71,8 @@ public class IETopHeadDynamicInclude extends BaseDynamicInclude {
 	}
 
 	private static final String[] _FILE_NAMES = {
-		"array.fill.js", "array.find.js", "array.from.js", "fetch.js",
-		"formdata.js", "object.assign.js", "object.entries.js",
+		"array.fill.js", "array.find.js", "array.from.js", "array.includes.js",
+		"fetch.js", "formdata.js", "object.assign.js", "object.entries.js",
 		"object.values.js", "string.endswith.js", "url.search.params.js"
 	};
 
