9d6b71f6ca6bd366b79a080350efe385ec9bd1a6
Sergio Alonso Manzanedo <sergio.alonso@liferay.com>
2019-03-01 12:29:50 +0100

    LPS-91363 Using WARN level instead of ERROR for the exception because that inconsistency is allowed by product.   Only print data related to data inconsistency.  An

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v1_1_5/UpgradeContentImages.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v1_1_5/UpgradeContentImages.java
@@ -174,7 +174,14 @@ public class UpgradeContentImages extends UpgradeProcess {
 				fileEntryId);
 		}
 		catch (PortalException pe) {
-			_log.error("Unable to get file entry " + fileEntryId, pe);
+			if (_log.isWarnEnabled()) {
+				_log.warn("Unable to get file entry " + fileEntryId);
+			}
+
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to get file entry " + fileEntryId, pe);
+			}
+
 		}
 
 		return fileEntry;
@@ -197,12 +204,27 @@ public class UpgradeContentImages extends UpgradeProcess {
 				groupId, folderId, id);
 		}
 		catch (PortalException pe) {
-			_log.error(
-				StringBundler.concat(
-					"Unable to get file entry with group ID ",
-					String.valueOf(groupId), ", folder ID ",
-					String.valueOf(folderId), ", and file name ", id),
-				pe);
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					StringBundler.concat(
+						"Unable to get file entry with group ID ",
+						String.valueOf(groupId), ", folder ID ",
+						String.valueOf(folderId), ", and file name ", id,
+						" for resourcePrimKey ",
+						String.valueOf(resourcePrimKey))
+				);
+			}
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					StringBundler.concat(
+						"Unable to get file entry with group ID ",
+						String.valueOf(groupId), ", folder ID ",
+						String.valueOf(folderId), ", and file name ", id,
+						" for resourcePrimKey ",
+						String.valueOf(resourcePrimKey)),
+					pe);
+			}
 		}
 
 		return fileEntry;
