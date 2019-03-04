3cc65d582a6d4063b5ed9dbd48fa0a279bcf48cd
Javier Gamarra <nhpatt@gmail.com>
2019-03-04 20:00:42 +0100

    LPS-91104 add documentation resource in vulcan

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/resource/DocumentationResourceImpl.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.resource;
+
+import static io.swagger.v3.jaxrs2.integration.ServletConfigContextUtils.getContextIdFromServletConfig;
+
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.vulcan.resource.DocumentationResource;
+
+import io.swagger.v3.core.util.Json;
+import io.swagger.v3.core.util.Yaml;
+import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
+import io.swagger.v3.oas.integration.api.OpenAPIConfiguration;
+import io.swagger.v3.oas.integration.api.OpenApiContext;
+import io.swagger.v3.oas.integration.api.OpenApiScanner;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.ServletConfig;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Javier Gamarra
+ */
+@Component(service = DocumentationResource.class)
+public class DocumentationResourceImpl implements DocumentationResource {
+
+	@Override
+	public Response getOpenApi(
+			Set<Class<?>> resourceClasses, HttpHeaders headers,
+			ServletConfig servletConfig, Application app, UriInfo uriInfo,
+			String type)
+		throws Exception {
+
+		String contextId = getContextIdFromServletConfig(servletConfig);
+
+		OpenApiContext openApiContext =
+			new JaxrsOpenApiContextBuilder().servletConfig(
+				servletConfig
+			).application(
+				app
+			).ctxId(
+				contextId
+			).buildContext(
+				true
+			);
+
+		openApiContext.setOpenApiScanner(
+			new OpenApiScanner() {
+
+				@Override
+				public Set<Class<?>> classes() {
+					return resourceClasses;
+				}
+
+				@Override
+				public Map<String, Object> resources() {
+					return new HashMap<>();
+				}
+
+				@Override
+				public void setConfiguration(
+					OpenAPIConfiguration openAPIConfiguration) {
+				}
+
+			});
+
+		OpenAPI openAPI = openApiContext.read();
+
+		if (openAPI == null) {
+			return Response.status(
+				404
+			).build();
+		}
+
+		if (StringUtil.equalsIgnoreCase("yaml", type)) {
+			return Response.status(
+				Response.Status.OK
+			).entity(
+				Yaml.pretty(openAPI)
+			).type(
+				"application/yaml"
+			).build();
+		}
+
+		return Response.status(
+			Response.Status.OK
+		).entity(
+			Json.pretty(openAPI)
+		).type(
+			MediaType.APPLICATION_JSON_TYPE
+		).build();
+	}
+
+}
