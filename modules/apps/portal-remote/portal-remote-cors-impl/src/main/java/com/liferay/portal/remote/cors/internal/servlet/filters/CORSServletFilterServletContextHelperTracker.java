aab49aeb31028fc7a3cef19b6bd10aa8941ba8c0
Carlos Sierra <carlos.sierra@liferay.com>
2019-03-04 17:20:12 +0100

    LPS-91514 Tracks web contexts and publishes filter for matching CORS configurations

---
+++ modules/apps/portal-remote/portal-remote-cors-impl/src/main/java/com/liferay/portal/remote/cors/internal/servlet/filters/CORSServletFilterServletContextHelperTracker.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.remote.cors.internal.servlet.filters;
+
+import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.remote.cors.configuration.WebContextCORSConfiguration;
+import com.liferay.portal.remote.cors.internal.CorsSupport;
+
+import java.util.Dictionary;
+import java.util.Map;
+
+import javax.servlet.Filter;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.http.context.ServletContextHelper;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+
+/**
+ * @author Carlos Sierra Andrés
+ * @author Marta Medio
+ */
+@Component(
+	configurationPid = "com.liferay.portal.remote.cors.configuration.WebContextCORSConfiguration",
+	configurationPolicy = ConfigurationPolicy.REQUIRE, immediate = true,
+	service = {}
+)
+public class CORSServletFilterServletContextHelperTracker {
+
+	@Activate
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		_bundleContext = bundleContext;
+
+		WebContextCORSConfiguration webContextCORSConfiguration =
+			ConfigurableUtil.createConfigurable(
+				WebContextCORSConfiguration.class, properties);
+
+		_corsHeaders = CorsSupport.buildCorsHeaders(
+			webContextCORSConfiguration.headers());
+		_filterMappingUrlPatterns =
+			webContextCORSConfiguration.filterMappingURLPatterns();
+		_serviceTracker = ServiceTrackerFactory.open(
+			_bundleContext,
+			StringBundler.concat(
+				"(&",
+				webContextCORSConfiguration.servletContextHelperSelectFilter(),
+				"(", HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME,
+				"=*)(objectClass=", ServletContextHelper.class.getName(), "))"),
+			new ServletContextHelperServiceTrackerCustomizer());
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTracker.close();
+	}
+
+	private BundleContext _bundleContext;
+	private Map<String, String> _corsHeaders;
+	private String[] _filterMappingUrlPatterns;
+	private ServiceTracker<ServletContextHelper, ServiceRegistration<?>>
+		_serviceTracker;
+
+	private class ServletContextHelperServiceTrackerCustomizer
+		implements ServiceTrackerCustomizer
+			<ServletContextHelper, ServiceRegistration<?>> {
+
+		@Override
+		public ServiceRegistration<?> addingService(
+			ServiceReference<ServletContextHelper> serviceReference) {
+
+			CORSServletFilter corsServletFilter = new CORSServletFilter();
+
+			corsServletFilter.setCorsHeaders(_corsHeaders);
+
+			return _bundleContext.registerService(
+				Filter.class, corsServletFilter,
+				_buildProperties(serviceReference));
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<ServletContextHelper> serviceReference,
+			ServiceRegistration<?> serviceRegistration) {
+
+			serviceRegistration.setProperties(
+				_buildProperties(serviceReference));
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<ServletContextHelper> serviceReference,
+			ServiceRegistration<?> serviceRegistration) {
+
+			try {
+				serviceRegistration.unregister();
+			}
+			catch (Exception e) {
+			}
+		}
+
+		private Dictionary<String, Object> _buildProperties(
+			ServiceReference<ServletContextHelper> serviceReference) {
+
+			Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+			properties.put(Constants.SERVICE_RANKING, -1);
+			properties.put(
+				HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT,
+				GetterUtil.getString(
+					serviceReference.getProperty(
+						HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME)));
+			properties.put(
+				HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_NAME,
+				CORSServletFilter.class.getName());
+
+			if (ArrayUtil.isEmpty(_filterMappingUrlPatterns) ||
+				ArrayUtil.contains(
+					_filterMappingUrlPatterns, StringPool.STAR)) {
+
+				properties.put(
+					HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_SERVLET,
+					"cxf-servlet");
+			}
+			else {
+				properties.put(
+					HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN,
+					_filterMappingUrlPatterns);
+			}
+
+			return properties;
+		}
+
+	}
+
+}
