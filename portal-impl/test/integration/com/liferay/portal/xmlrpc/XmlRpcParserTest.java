2444a5d662f559a379ec5ad3e67402b98cc5d8e8
Hugo Huijser <hugo.huijser@liferay.com>
2019-03-04 08:42:44 +0800

    LPS-91420 Add exclusion

--- portal-impl/test/integration/com/liferay/portal/xmlrpc/XmlRpcParserTest.java
+++ portal-impl/test/integration/com/liferay/portal/xmlrpc/XmlRpcParserTest.java
@@ -132,6 +132,8 @@ public class XmlRpcParserTest {
 		}
 	}
 
+	// Skip JavaParser
+
 	private static final String[] _FAULT_RESPONSES = {
 		StringBundler.concat(
 			"<?xml version=\"1.0\"?>",
@@ -171,6 +173,8 @@ public class XmlRpcParserTest {
 			"</methodResponse>")
 	};
 
+	// Skip JavaParser
+
 	private static final String[] _NON_PARAMETERIZED_METHODS = {
 		StringBundler.concat(
 			"<?xml version=\"1.0\"?>",
@@ -186,6 +190,8 @@ public class XmlRpcParserTest {
 			"</methodCall>")
 	};
 
+	// Skip JavaParser
+
 	private static final String _PARAMETERIZED_METHOD =
 		StringBundler.concat(
 			"<?xml version=\"1.0\"?>",
@@ -197,6 +203,8 @@ public class XmlRpcParserTest {
 			"<param><value><string>world</string></value></param>",
 			"</params>",
 			"</methodCall>");
+
+	// Skip JavaParser
 
 	private static final String[] _SUCCESS_RESPONSES = {
 		StringBundler.concat(
