623db0b5107e977665cfd45a5ecd35b5bad0b6b7
Máté Thurzó <mate.thurzo@liferay.com>
2019-03-04 16:43:58 +0100

    LPS-91517 Add new control menu category and related menu entry

---
+++ modules/apps/change-tracking/change-tracking-change-lists-indicator-web/src/main/java/com/liferay/change/tracking/change/lists/indicator/web/internal/control/menu/CTIndicatorProductNavigationControlMenuEntry.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.change.lists.indicator.web.internal.control.menu;
+
+import com.liferay.change.tracking.CTEngineManager;
+import com.liferay.change.tracking.constants.CTProductNavigationControlMenuCategoryKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.product.navigation.control.menu.BaseJSPProductNavigationControlMenuEntry;
+import com.liferay.product.navigation.control.menu.ProductNavigationControlMenuEntry;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Máté Thurzó
+ */
+@Component(
+	immediate = true,
+	property = {
+		"product.navigation.control.menu.category.key=" + CTProductNavigationControlMenuCategoryKeys.CHANGE_TRACKING,
+		"product.navigation.control.menu.entry.order:Integer=100"
+	},
+	service = ProductNavigationControlMenuEntry.class
+)
+public class CTIndicatorProductNavigationControlMenuEntry
+	extends BaseJSPProductNavigationControlMenuEntry
+	implements ProductNavigationControlMenuEntry {
+
+	@Override
+	public String getIconJspPath() {
+		return "/view.jsp";
+	}
+
+	@Override
+	public boolean isShow(HttpServletRequest request) throws PortalException {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		return _ctEngineManager.isChangeTrackingEnabled(
+			themeDisplay.getCompanyId());
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.change.tracking.change.lists.indicator.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	@Reference
+	private CTEngineManager _ctEngineManager;
+
+}
