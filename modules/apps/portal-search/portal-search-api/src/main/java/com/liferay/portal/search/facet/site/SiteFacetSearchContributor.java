7e18c61fd1699d047e9cf59f06b82b1b722ba476
André de Oliveira <andre.oliveira@liferay.com>
2019-03-04 00:42:09 -0800

    LPS-91481 portal-search-api: Facet Search Contributors, for Searcher
    https://issues.liferay.com/browse/LPS-91481

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/facet/site/SiteFacetSearchContributor.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.facet.site;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+
+import java.util.function.Consumer;
+
+/**
+ * @author André de Oliveira
+ */
+@ProviderType
+public interface SiteFacetSearchContributor {
+
+	public void contribute(
+		SearchRequestBuilder searchRequestBuilder,
+		Consumer<SiteFacetBuilder> siteFacetBuilderConsumer);
+
+	@ProviderType
+	public interface SiteFacetBuilder {
+
+		public SiteFacetBuilder aggregationName(String aggregationName);
+
+		public SiteFacetBuilder frequencyThreshold(int frequencyThreshold);
+
+		public SiteFacetBuilder maxTerms(int maxTerms);
+
+		public SiteFacetBuilder selectedGroupIds(String... selectedGroupIds);
+
+	}
+
+}
---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/facet/type/TypeFacetSearchContributor.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.facet.type;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+
+import java.util.function.Consumer;
+
+/**
+ * @author André de Oliveira
+ */
+public interface TypeFacetSearchContributor {
+
+	public void contribute(
+		SearchRequestBuilder searchRequestBuilder,
+		Consumer<TypeFacetBuilder> typeFacetBuilderConsumer);
+
+	@ProviderType
+	public interface TypeFacetBuilder {
+
+		public TypeFacetBuilder aggregationName(String aggregationName);
+
+		public TypeFacetBuilder frequencyThreshold(int frequencyThreshold);
+
+		public TypeFacetBuilder selectedEntryClassNames(
+			String... selectedEntryClassNames);
+
+	}
+
+}
