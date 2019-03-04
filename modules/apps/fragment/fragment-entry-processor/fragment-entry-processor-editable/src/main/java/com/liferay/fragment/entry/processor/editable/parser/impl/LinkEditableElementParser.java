01daa8faf836fed9537de8e51c458e8f0a7389f6
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-03-04 20:22:38 +0100

    LPS-91217 New editable element processor of type link

---
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/parser/impl/LinkEditableElementParser.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.entry.processor.editable.parser.impl;
+
+import com.liferay.fragment.entry.processor.editable.EditableFragmentEntryProcessor;
+import com.liferay.fragment.entry.processor.editable.parser.EditableElementParser;
+import com.liferay.fragment.entry.processor.editable.parser.util.EditableElementParserUtil;
+import com.liferay.fragment.exception.FragmentEntryContentException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.jsoup.nodes.Element;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true, property = "type=link",
+	service = EditableElementParser.class
+)
+public class LinkEditableElementParser implements EditableElementParser {
+
+	@Override
+	public String getFieldTemplate() {
+		return _TMPL_LINK_FIELD_TEMPLATE;
+	}
+
+	@Override
+	public String getValue(Element element) {
+		List<Element> elements = element.getElementsByTag("a");
+
+		Element replaceableElement = elements.get(0);
+
+		return replaceableElement.html();
+	}
+
+	@Override
+	public void replace(Element element, String value) {
+		replace(element, value, null);
+	}
+
+	@Override
+	public void replace(
+		Element element, String value, JSONObject configJSONObject) {
+
+		List<Element> elements = element.getElementsByTag("a");
+
+		Element replaceableElement = elements.get(0);
+
+		Element bodyElement = EditableElementParserUtil.getDocumentBody(value);
+
+		if (configJSONObject == null) {
+			replaceableElement.html(bodyElement.html());
+
+			return;
+		}
+
+		EditableElementParserUtil.addAttribute(
+			replaceableElement, configJSONObject, "target", "target");
+
+		EditableElementParserUtil.addClass(
+			replaceableElement, configJSONObject, "btn btn-", "buttonType");
+
+		replaceableElement.html(bodyElement.html());
+	}
+
+	@Override
+	public void validate(Element element) throws FragmentEntryContentException {
+		List<Element> elements = element.getElementsByTag("a");
+
+		if (elements.size() != 1) {
+			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+				"content.Language", getClass());
+
+			throw new FragmentEntryContentException(
+				LanguageUtil.format(
+					resourceBundle,
+					"each-editable-image-element-must-contain-an-a-tag",
+					new Object[] {"<em>", "</em>"}, false));
+		}
+	}
+
+	private static final String _TMPL_LINK_FIELD_TEMPLATE = StringUtil.read(
+		EditableFragmentEntryProcessor.class,
+		"/META-INF/resources/fragment/entry/processor/editable" +
+			"/link_field_template.tmpl");
+
+}
