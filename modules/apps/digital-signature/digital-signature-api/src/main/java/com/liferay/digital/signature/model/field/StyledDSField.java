7faa120afa3097a1e324c32300eda00f7fbabae4
Michael Han <michael.han@liferay.com>
2019-03-04 23:18:27 -0600

    LPS-90590 digital-signature-api: rename DS*Field* to *DSField*

---
+++ modules/apps/digital-signature/digital-signature-api/src/main/java/com/liferay/digital/signature/model/field/StyledDSField.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.digital.signature.model.field;
+
+import aQute.bnd.annotation.ProviderType;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public interface StyledDSField<T extends DSField> extends DSField<T> {
+
+	public Boolean getBold();
+
+	public String getFont();
+
+	public String getFontColor();
+
+	public String getFontSize();
+
+	public Integer getHeight();
+
+	public Boolean getItalic();
+
+	public Boolean getUnderline();
+
+	public Integer getWidth();
+
+}
---
+++ modules/apps/digital-signature/digital-signature-api/src/main/java/com/liferay/digital/signature/model/field/builder/RadioGroupDSFieldBuilder.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.digital.signature.model.field.builder;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.digital.signature.model.field.RadioDSField;
+import com.liferay.digital.signature.model.field.RadioGroupDSField;
+
+/**
+ * @author Michael C. Han
+ */
+@ProviderType
+public interface RadioGroupDSFieldBuilder
+	extends UserEntryDSFieldBuilder<RadioGroupDSField> {
+
+	public RadioGroupDSFieldBuilder addDSRadioField(RadioDSField radioDSField);
+
+	public RadioGroupDSFieldBuilder addDSRadioFields(
+		RadioDSField... radioDSField);
+
+	public RadioGroupDSFieldBuilder setGroupName(String groupName);
+
+}
