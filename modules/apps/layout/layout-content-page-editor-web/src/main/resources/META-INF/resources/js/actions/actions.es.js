68dc344ec8228ee5cb1d35c50c559b9105989cba
Pablo Molina <contact@pablomolina.me>
2019-03-05 12:40:51 +0100

    LPS-90642 Group actions by associated reducers

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/actions/actions.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/actions/actions.es.js
@@ -1,34 +1,38 @@
+export const HIDE_MAPPING_TYPE_DIALOG = 'hideMappingTypeDialog';
+export const OPEN_ASSET_TYPE_DIALOG = 'openAssetTypeDialog';
+export const OPEN_MAPPING_FIELDS_DIALOG = 'openMappingFieldsDialog';
+export const SELECT_MAPPEABLE_FIELD = 'selectMappeableField';
+export const SELECT_MAPPEABLE_TYPE = 'selectMappeableType';
+
+export const HIDE_SIDEBAR = 'hideSidebar';
+export const TOGGLE_SIDEBAR = 'toggleSidebar';
+
+export const UPDATE_LAST_SAVE_DATE = 'updateLastSaveDate';
+export const UPDATE_SAVING_CHANGES_STATUS = 'updateSavingChangesStatus';
+
+export const CHANGE_LANGUAGE_ID = 'languageId';
+export const SELECT_EXPERIENCE = 'selectExperience';
+export const UPDATE_TRANSLATION_STATUS = 'updateTranslationStatus';
+
+export const CREATE_EXPERIENCE = 'createExperience';
+export const END_CREATE_EXPERIENCE = 'endCreateExperience';
+export const START_CREATE_EXPERIENCE = 'startCreateExperience';
+
+export const ADD_SECTION = 'addSection';
+export const MOVE_SECTION = 'moveSection';
+export const REMOVE_SECTION = 'removeSection';
+export const UPDATE_SECTION_CONFIG = 'updateSectionConfig';
+
 export const ADD_FRAGMENT_ENTRY_LINK = 'addFragmentEntryLink';
 export const ADD_PORTLET = 'addPortlet';
-export const ADD_SECTION = 'addSection';
-export const CHANGE_LANGUAGE_ID = 'languageId';
-export const CHANGE_SEGMENT_ID = 'segmentId';
+export const MOVE_FRAGMENT_ENTRY_LINK = 'moveFragmentEntryLink';
+export const REMOVE_FRAGMENT_ENTRY_LINK = 'removeFragmentEntryLink';
+export const UPDATE_CONFIG_ATTRIBUTES = 'updateConfigAttributes';
+export const UPDATE_EDITABLE_VALUE = 'updateEditableValue';
+
 export const CLEAR_ACTIVE_ITEM = 'clearActiveItem';
 export const CLEAR_DROP_TARGET = 'clearDragTarget';
 export const CLEAR_HOVERED_ITEM = 'clearHoveredItem';
-export const HIDE_MAPPING_TYPE_DIALOG = 'hideMappingTypeDialog';
-export const HIDE_SIDEBAR = 'hideSidebar';
-export const MOVE_FRAGMENT_ENTRY_LINK = 'moveFragmentEntryLink';
-export const MOVE_SECTION = 'moveSection';
-export const OPEN_ASSET_TYPE_DIALOG = 'openAssetTypeDialog';
-export const OPEN_MAPPING_FIELDS_DIALOG = 'openMappingFieldsDialog';
-export const REMOVE_FRAGMENT_ENTRY_LINK = 'removeFragmentEntryLink';
-export const REMOVE_SECTION = 'removeSection';
-export const SELECT_MAPPEABLE_FIELD = 'selectMappeableField';
-export const SELECT_MAPPEABLE_TYPE = 'selectMappeableType';
-export const TOGGLE_SIDEBAR = 'toggleSidebar';
 export const UPDATE_ACTIVE_ITEM = 'updateActiveItem';
-export const UPDATE_CONFIG_ATTRIBUTES = 'updateConfigAttributes';
 export const UPDATE_DROP_TARGET = 'updateDragTarget';
-export const UPDATE_EDITABLE_VALUE = 'updateEditableValue';
-export const UPDATE_HIGHLIGHT_MAPPING_STATUS = 'updateHighlightMappingStatus';
 export const UPDATE_HOVERED_ITEM = 'updateHoveredItem';
-export const UPDATE_LAST_SAVE_DATE = 'updateLastSaveDate';
-export const UPDATE_SAVING_CHANGES_STATUS = 'updateSavingChangesStatus';
-export const UPDATE_SECTION_CONFIG = 'updateSectionConfig';
-export const UPDATE_TRANSLATION_STATUS = 'updateTranslationStatus';
-
-export const CREATE_EXPERIENCE = 'CREATE_EXPERIENCE';
-export const END_CREATE_EXPERIENCE = 'END_CREATE_EXPERIENCE';
-export const SELECT_EXPERIENCE = 'SELECT_EXPERIENCE';
-export const START_CREATE_EXPERIENCE = 'START_CREATE_EXPERIENCE';
