9c199ef39f5ad35451a6f10571143485f27015e6
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-03-05 21:05:01 +0100

    LPS-91288 Helper class to create a basic compatible structure based on a list of fragment entry links

---
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/util/LayoutPageTemplateStructureHelperUtil.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.util;
+
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+
+import java.util.List;
+
+/**
+ * @author Jürgen
+ */
+public class LayoutPageTemplateStructureHelperUtil {
+	public static JSONObject generateContentLayoutStructure(List<FragmentEntryLink> fragmentEntryLinks) {
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		JSONArray structureJSONArray = JSONFactoryUtil.createJSONArray();
+
+		for (int i = 0; i < fragmentEntryLinks.size(); i++) {
+			FragmentEntryLink fragmentEntryLink = fragmentEntryLinks.get(i);
+
+			JSONObject structureJSONObject = JSONFactoryUtil.createJSONObject();
+
+			JSONArray columnJSONArray = JSONFactoryUtil.createJSONArray();
+
+			JSONObject columnJSONObject = JSONFactoryUtil.createJSONObject();
+
+			columnJSONObject.put("columnId", String.valueOf(i));
+
+			JSONArray fragmentEntryLinksJSONArray =
+				JSONFactoryUtil.createJSONArray();
+
+			fragmentEntryLinksJSONArray.put(
+				fragmentEntryLink.getFragmentEntryLinkId());
+
+			columnJSONObject.put(
+				"fragmentEntryLinkIds", fragmentEntryLinksJSONArray);
+
+			columnJSONObject.put("size", StringPool.BLANK);
+
+			columnJSONArray.put(columnJSONObject);
+
+			structureJSONObject.put("columns", columnJSONArray);
+
+			structureJSONObject.put("rowId", String.valueOf(i));
+
+			structureJSONArray.put(structureJSONObject);
+		}
+
+		jsonObject.put("config", JSONFactoryUtil.createJSONObject());
+		jsonObject.put(
+			"nextColumnId", String.valueOf(fragmentEntryLinks.size()));
+		jsonObject.put("nextRowId", String.valueOf(fragmentEntryLinks.size()));
+
+		if (!fragmentEntryLinks.isEmpty()) {
+			jsonObject.put(
+				"rowId", String.valueOf(fragmentEntryLinks.size() - 1));
+		}
+
+		jsonObject.put("structure", structureJSONArray);
+
+		return jsonObject;
+	}
+}
