e2e172561f17f9410ec1e36b189f44e7cbd8963a
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-03-05 20:16:30 +0100

    LPS-91288 We have to take the content structure into account when copying the layout

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutCopyHelperImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutCopyHelperImpl.java
@@ -16,7 +16,12 @@ package com.liferay.layout.internal.util;
 
 import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
 import com.liferay.layout.util.LayoutCopyHelper;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.PortletPreferences;
 import com.liferay.portal.kernel.service.LayoutLocalService;
@@ -26,10 +31,14 @@ import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.sites.kernel.util.Sites;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -59,15 +68,32 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 			Layout sourceLayout, Layout targetLayout)
 		throws Exception {
 
-		List<FragmentEntryLink> fragmentEntryLinks =
-			_fragmentEntryLinkLocalService.getFragmentEntryLinks(
-				sourceLayout.getGroupId(), _portal.getClassNameId(Layout.class),
-				sourceLayout.getPlid());
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				fetchLayoutPageTemplateStructure(
+					sourceLayout.getGroupId(),
+					_portal.getClassNameId(Layout.class),
+					sourceLayout.getPlid());
 
-		_fragmentEntryLinkLocalService.
-			deleteLayoutPageTemplateEntryFragmentEntryLinks(
-				targetLayout.getGroupId(), _portal.getClassNameId(Layout.class),
-				targetLayout.getPlid());
+		if (layoutPageTemplateStructure == null) {
+			return;
+		}
+
+		String data = layoutPageTemplateStructure.getData();
+
+		if (Validator.isNull(data)) {
+			return;
+		}
+
+		JSONObject dataJSONObject = JSONFactoryUtil.createJSONObject(data);
+
+		JSONArray structureJSONArray = dataJSONObject.getJSONArray("structure");
+
+		if (structureJSONArray == null) {
+			return;
+		}
+
+		long classNameId = _portal.getClassNameId(Layout.class);
 
 		ServiceContext serviceContext = Optional.ofNullable(
 			ServiceContextThreadLocal.getServiceContext()
@@ -75,15 +101,84 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 			new ServiceContext()
 		);
 
-		for (FragmentEntryLink fragmentEntryLink : fragmentEntryLinks) {
-			_fragmentEntryLinkLocalService.addFragmentEntryLink(
-				targetLayout.getUserId(), targetLayout.getGroupId(), 0,
-				fragmentEntryLink.getFragmentEntryId(),
-				_portal.getClassNameId(Layout.class), targetLayout.getPlid(),
-				fragmentEntryLink.getCss(), fragmentEntryLink.getHtml(),
-				fragmentEntryLink.getJs(),
-				fragmentEntryLink.getEditableValues(),
-				fragmentEntryLink.getPosition(), serviceContext);
+		List<FragmentEntryLink> fragmentEntryLinks =
+			_fragmentEntryLinkLocalService.getFragmentEntryLinks(
+				sourceLayout.getGroupId(), classNameId, sourceLayout.getPlid());
+
+		Stream<FragmentEntryLink> stream = fragmentEntryLinks.stream();
+
+		Map<Long, FragmentEntryLink> fragmentEntryLinkMap = stream.collect(
+			Collectors.toMap(
+				FragmentEntryLink::getFragmentEntryLinkId,
+				fragmentEntryLink -> fragmentEntryLink));
+
+		_fragmentEntryLinkLocalService.
+			deleteLayoutPageTemplateEntryFragmentEntryLinks(
+				targetLayout.getGroupId(), classNameId, targetLayout.getPlid());
+
+		for (int i = 0; i < structureJSONArray.length(); i++) {
+			JSONObject rowJSONObject = structureJSONArray.getJSONObject(i);
+
+			JSONArray columnsJSONArray = rowJSONObject.getJSONArray("columns");
+
+			for (int j = 0; j < columnsJSONArray.length(); j++) {
+				JSONObject columnJSONObject = columnsJSONArray.getJSONObject(j);
+
+				JSONArray fragmentEntryLinkIdsJSONArray =
+					columnJSONObject.getJSONArray("fragmentEntryLinkIds");
+
+				JSONArray newFragmentEntryLinkIdsJSONArray =
+					JSONFactoryUtil.createJSONArray();
+
+				for (int k = 0; k < fragmentEntryLinkIdsJSONArray.length();
+					 k++) {
+
+					FragmentEntryLink fragmentEntryLink =
+						fragmentEntryLinkMap.get(
+							fragmentEntryLinkIdsJSONArray.getLong(k));
+
+					if (fragmentEntryLink == null) {
+						continue;
+					}
+
+					FragmentEntryLink newFragmentEntryLink =
+						_fragmentEntryLinkLocalService.addFragmentEntryLink(
+							targetLayout.getUserId(), targetLayout.getGroupId(),
+							0, fragmentEntryLink.getFragmentEntryId(),
+							classNameId, targetLayout.getPlid(),
+							fragmentEntryLink.getCss(),
+							fragmentEntryLink.getHtml(),
+							fragmentEntryLink.getJs(),
+							fragmentEntryLink.getEditableValues(),
+							fragmentEntryLink.getPosition(), serviceContext);
+
+					newFragmentEntryLinkIdsJSONArray.put(
+						newFragmentEntryLink.getFragmentEntryLinkId());
+				}
+
+				columnJSONObject.put(
+					"fragmentEntryLinkIds", newFragmentEntryLinkIdsJSONArray);
+			}
+		}
+
+		LayoutPageTemplateStructure targetLayoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				fetchLayoutPageTemplateStructure(
+					targetLayout.getGroupId(), classNameId,
+					targetLayout.getPlid());
+
+		if (targetLayoutPageTemplateStructure != null) {
+			_layoutPageTemplateStructureLocalService.
+				updateLayoutPageTemplateStructure(
+					targetLayout.getGroupId(), classNameId,
+					targetLayout.getPlid(), dataJSONObject.toString());
+		}
+		else {
+			_layoutPageTemplateStructureLocalService.
+				addLayoutPageTemplateStructure(
+					targetLayout.getUserId(), targetLayout.getGroupId(),
+					classNameId, targetLayout.getPlid(),
+					dataJSONObject.toString(), serviceContext);
 		}
 	}
 
@@ -127,6 +222,10 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
 
 	@Reference
 	private Portal _portal;
