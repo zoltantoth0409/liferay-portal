c34fa26851d0a5ff188943e9d715447fb10e6f0e
Peter Shin <peter.shin@liferay.com>
2019-03-05 18:41:36 -0800

    LPS-89415 Use JavaParser#parse

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/util/FileUtil.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/util/FileUtil.java
@@ -16,8 +16,6 @@ package com.liferay.portal.tools.rest.builder.internal.util;
 
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.tools.java.parser.JavaParser;
-import com.liferay.source.formatter.SourceFormatter;
-import com.liferay.source.formatter.SourceFormatterArgs;
 
 import java.io.File;
 import java.io.FileFilter;
@@ -31,7 +29,6 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -150,20 +147,6 @@ public class FileUtil {
 		if (StringUtil.endsWith(file.getName(), ".java")) {
 			JavaParser.parse(file, 80);
 		}
-
-		SourceFormatterArgs sourceFormatterArgs = new SourceFormatterArgs();
-
-		sourceFormatterArgs.setFileNames(
-			Collections.singletonList(file.getCanonicalPath()));
-		sourceFormatterArgs.setIncludeGeneratedFiles(true);
-		sourceFormatterArgs.setPrintErrors(false);
-		sourceFormatterArgs.setSkipCheckNames(
-			Collections.singletonList("JavaOSGiReferenceCheck"));
-
-		SourceFormatter sourceFormatter = new SourceFormatter(
-			sourceFormatterArgs);
-
-		sourceFormatter.format();
 
 		return read(file);
 	}
