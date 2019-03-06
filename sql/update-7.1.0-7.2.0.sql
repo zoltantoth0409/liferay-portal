aa240dc85c31cc26a1f6b3fc338eb3fa3d2acbf8
Brian Chan <brian.chan@liferay.com>
2019-03-06 11:27:33 -0800

    LPS-91286 match service.xml

--- sql/update-7.1.0-7.2.0.sql
+++ sql/update-7.1.0-7.2.0.sql
@@ -1,10 +1,9 @@
 alter table Layout add parentPlid LONG;
 alter table Layout add leftPlid LONG;
 alter table Layout add rightPlid LONG;
-alter table Layout add system BOOLEAN;
-
 alter table Layout add classNameId LONG;
 alter table Layout add classPK LONG;
+alter table Layout add system BOOLEAN;
 alter table Layout add publishDate DATE null;
 
 COMMIT_TRANSACTION;
