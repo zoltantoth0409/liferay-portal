7da6a1866066f7504a8ea9654815bd09cd23161e
Roland Pákai <roland.pakai@liferay.com>
2019-03-06 11:10:32 +0100

    LPS-91426 Fetch and set activeChangeListName and tooltipContentText

--- modules/apps/change-tracking/change-tracking-change-lists-indicator-web/src/main/resources/META-INF/resources/js/ChangeListsIndicator.es.js
+++ modules/apps/change-tracking/change-tracking-change-lists-indicator-web/src/main/resources/META-INF/resources/js/ChangeListsIndicator.es.js
@@ -3,6 +3,7 @@ import ClayIcon from 'clay-icon';
 import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
 import Soy from 'metal-soy';
 import {Config} from 'metal-state';
+import {openToast} from 'frontend-js-web/liferay/toast/commands/OpenToast.es';
 
 import templates from './ChangeListsIndicator.soy';
 
@@ -13,6 +14,48 @@ import templates from './ChangeListsIndicator.soy';
 class ChangeListsIndicator extends PortletBase {
 
 	created() {
+		let urlActiveCollection = this.urlCollectionsBase + '?type=active&userId=' + Liferay.ThemeDisplay.getUserId();
+		
+		this._getDataRequest(
+			urlActiveCollection,
+			response => {
+				if (response) {
+					this.activeChangeListName = response[0].name;
+				}
+			}
+		);
+	}
+	
+	_getDataRequest(url, callback) {	
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+		
+		let type = 'GET';
+
+		let init = {
+			credentials: 'include',
+			headers,
+			method: type
+		};
+
+		fetch(url, init)
+			.then(response => response.json())
+			.then(response => callback(response))
+			.catch(
+				(error) => {
+					const message = typeof error === 'string' ?
+						error :
+						Liferay.Language.get('an-error-occured-while-saving-configuration');
+
+					openToast(
+						{
+							message,
+							title: Liferay.Language.get('error'),
+							type: 'danger'
+						}
+					);
+				}
+			);
 	}
 }
 
@@ -25,6 +68,17 @@ class ChangeListsIndicator extends PortletBase {
 ChangeListsIndicator.STATE = {
 	
 	/**
+	 * Name of the active change list.
+	 * @default
+	 * @instance
+	 * @memberOf ChangeListsIndicator
+	 * @review
+	 * @type {!string}
+	 */
+	
+	activeChangeListName: Config.string().value(''),
+	
+	/**
 	 * Path of the available icons.
 	 * @default undefined
 	 * @instance
@@ -33,7 +87,18 @@ ChangeListsIndicator.STATE = {
 	 * @type {!string}
 	 */
 
-	spritemap: Config.string().required()
+	spritemap: Config.string().required(),
+	
+	/**
+	 * BBase REST API URL to collection resource
+	 * @default
+	 * @instance
+	 * @memberOf ChangeListsIndicator
+	 * @review
+	 * @type {string}
+	 */
+
+	urlCollectionsBase: Config.string()
 
 };
 
