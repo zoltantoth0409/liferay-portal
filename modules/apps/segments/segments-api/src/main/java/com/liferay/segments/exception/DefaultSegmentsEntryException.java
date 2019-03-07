075e89273f21d9f72c6e4fe5c0e5b5d5ef023740
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-03-07 16:22:23 +0100

    LPS-91705 Add exception for default segments entry

---
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/exception/DefaultSegmentsEntryException.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.exception;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Eduardo García
+ */
+@ProviderType
+public class DefaultSegmentsEntryException extends PortalException {
+
+	public DefaultSegmentsEntryException() {
+	}
+
+	public DefaultSegmentsEntryException(String msg) {
+		super(msg);
+	}
+
+	public DefaultSegmentsEntryException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public DefaultSegmentsEntryException(Throwable cause) {
+		super(cause);
+	}
+
+	public static class MustNotDeleteDefaultSegmentsEntry
+		extends DefaultSegmentsEntryException {
+
+		public MustNotDeleteDefaultSegmentsEntry(long segmentsEntryId) {
+			super(
+				String.format(
+					"Segments entry %s cannot be deleted because it is the " +
+						"default experience",
+					segmentsEntryId));
+		}
+
+	}
+
+	public static class MustNotUpdateDefaultSegmentsEntry
+		extends DefaultSegmentsEntryException {
+
+		public MustNotUpdateDefaultSegmentsEntry(long segmentsEntryId) {
+			super(
+				String.format(
+					"Segments entry %s cannot be updated because it is the " +
+						"default experience",
+					segmentsEntryId));
+		}
+
+	}
+
+}
