a929a2cef2b858bb86c3e6aebefa15a7b86a10fd
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-03-07 13:06:30 +0100

    LPS-91705 Add exception.
    Same pattern as for DDMTemplates

---
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/exception/RequiredSegmentsEntryException.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.exception;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Eduardo García
+ */
+@ProviderType
+public class RequiredSegmentsEntryException extends PortalException {
+
+	public RequiredSegmentsEntryException() {
+	}
+
+	public RequiredSegmentsEntryException(String msg) {
+		super(msg);
+	}
+
+	public RequiredSegmentsEntryException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public RequiredSegmentsEntryException(Throwable cause) {
+		super(cause);
+	}
+
+	public static class
+		MustNotDeleteSegmentsEntryReferencedBySegmentsExperiences
+			extends RequiredSegmentsEntryException {
+
+		public MustNotDeleteSegmentsEntryReferencedBySegmentsExperiences(
+			long segmentsEntryId) {
+
+			super(
+				String.format(
+					"Segments entry %s cannot be deleted because it is " +
+						"referenced by one or more segments experiences",
+					segmentsEntryId));
+		}
+
+	}
+
+}
