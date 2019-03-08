272ef0aa09b8cd356f19e66f4f11f862dffb9e69
Kenji Heigel <kenji.heigel@liferay.com>
2019-03-08 13:38:43 -0800

    LRCI-48 Use the default method from the interface

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/PoshiElement.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/elements/PoshiElement.java
@@ -73,7 +73,7 @@ public abstract class PoshiElement
 	}
 
 	public int getDefaultPoshiScriptLineNumber() {
-		return getPoshiScriptLineNumber();
+		return PoshiNode.super.getPoshiScriptLineNumber();
 	}
 
 	public String getPoshiLogDescriptor() {
