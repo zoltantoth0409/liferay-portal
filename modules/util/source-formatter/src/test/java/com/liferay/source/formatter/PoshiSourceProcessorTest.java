f32bed5aa21f4857731070ff2ab9262b4fe01e59
Alan Huang <alan.huang@liferay.com>
2019-03-08 20:10:45 +0800

    LPS-93513 Add testcase

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/PoshiSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/PoshiSourceProcessorTest.java
@@ -22,6 +22,14 @@ import org.junit.Test;
 public class PoshiSourceProcessorTest extends BaseSourceProcessorTestCase {
 
 	@Test
+	public void testDuplicatedParameters() throws Exception {
+		test(
+			"PoshiDuplicatedParameters.testmacro",
+			new String[] {"Parameter 'userScreenName' is already used"},
+			new Integer[] {4});
+	}
+
+	@Test
 	public void testIncorrectAnnotationsOrder() throws Exception {
 		test("IncorrectAnnotationsOrder.testmacro");
 	}
@@ -37,6 +45,11 @@ public class PoshiSourceProcessorTest extends BaseSourceProcessorTestCase {
 				"There should be a line break after ';'"
 			},
 			new Integer[] {2, 3, 17, 18});
+	}
+
+	@Test
+	public void testIncorrectParameterOrder() throws Exception {
+		test("PoshiIncorrectParameterOrder.testmacro");
 	}
 
 	@Test
