2b1272fcb5d21e4a24f1b601bc657a418b53aa42
Brian Chan <brian.chan@liferay.com>
2019-03-07 23:25:07 -0800

    LPS-91760 Tests don't work yet (and are ignored), but the idea is that you implement just a few methods and it tests everything for you.

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v1_0/test/DataDefinitionResourceTest.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v1_0/test/DataDefinitionResourceTest.java
@@ -15,7 +15,11 @@
 package com.liferay.data.engine.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.data.engine.rest.dto.v1_0.DataDefinition;
 
+import java.util.Objects;
+
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
@@ -26,4 +30,81 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class DataDefinitionResourceTest
 	extends BaseDataDefinitionResourceTestCase {
+
+	protected void assertValid(DataDefinition dataDefinition) {
+		boolean valid = false;
+
+		if ((dataDefinition.getDateCreated() != null) &&
+			(dataDefinition.getDateModified() != null) &&
+			Objects.equals(
+				dataDefinition.getGroupId(), testGroup.getGroupId()) &&
+			(dataDefinition.getId() != null)) {
+
+			valid = true;
+		}
+
+		Assert.assertTrue(valid);
+	}
+
+	@Override
+	protected boolean equals(
+		DataDefinition dataDefinition1, DataDefinition dataDefinition2) {
+
+		if (Objects.equals(
+				dataDefinition1.getDateCreated(),
+				dataDefinition2.getDateCreated()) &&
+			Objects.equals(dataDefinition1.getId(), dataDefinition2.getId())) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	protected DataDefinition randomDataDefinition() {
+		return new DataDefinition() {
+			{
+			}
+		};
+	}
+
+	protected DataDefinition testDeleteDataDefinition_addDataDefinition()
+		throws Exception {
+
+		return invokePostDataDefinition(
+			testGroup.getGroupId(), randomDataDefinition());
+	}
+
+	protected DataDefinition testGetDataDefinition_addDataDefinition()
+		throws Exception {
+
+		return invokePostDataDefinition(
+			testGroup.getGroupId(), randomDataDefinition());
+	}
+
+	protected DataDefinition testGetDataDefinitionsPage_addDataDefinition(
+			Long groupId, DataDefinition dataDefinition)
+		throws Exception {
+
+		return invokePostDataDefinition(groupId, dataDefinition);
+	}
+
+	protected Long testGetDataDefinitionsPage_getGroupId() throws Exception {
+		return testGroup.getGroupId();
+	}
+
+	protected DataDefinition testPostDataDefinition_addDataDefinition(
+			DataDefinition dataDefinition)
+		throws Exception {
+
+		return invokePostDataDefinition(testGroup.getGroupId(), dataDefinition);
+	}
+
+	protected DataDefinition testPutDataDefinition_addDataDefinition()
+		throws Exception {
+
+		return invokePostDataDefinition(
+			testGroup.getGroupId(), randomDataDefinition());
+	}
+
 }
