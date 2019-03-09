7119576465a7f7acb725c58ee74241a7d09bf345
Javier Gamarra <nhpatt@gmail.com>
2019-03-09 15:07:06 +0100

    LPS-89813 webUrl API

--- modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/v1_0/WebUrlResourceImpl.java
+++ modules/apps/headless/headless-foundation/headless-foundation-impl/src/main/java/com/liferay/headless/foundation/internal/resource/v1_0/WebUrlResourceImpl.java
@@ -14,9 +14,25 @@
 
 package com.liferay.headless.foundation.internal.resource.v1_0;
 
+import com.liferay.headless.foundation.dto.v1_0.WebUrl;
 import com.liferay.headless.foundation.resource.v1_0.WebUrlResource;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.ListType;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.Website;
+import com.liferay.portal.kernel.service.OrganizationService;
+import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.service.WebsiteService;
+import com.liferay.portal.vulcan.identifier.ClassNameClassPK;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +43,60 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = WebUrlResource.class
 )
 public class WebUrlResourceImpl extends BaseWebUrlResourceImpl {
+
+	@Override
+	public WebUrl getWebUrl(Long webUrlId) throws Exception {
+		return _toWebUrl(_websiteService.getWebsite(webUrlId));
+	}
+
+	@Override
+	public Page<WebUrl> getWebUrlsByClassNameClassPK(
+			ClassNameClassPK classNameClassPK, Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			transform(_getWebsites(classNameClassPK), this::_toWebUrl));
+	}
+
+	private List<Website> _getWebsites(ClassNameClassPK classNameClassPK)
+		throws PortalException {
+
+		String className = classNameClassPK.getClassName();
+
+		if (className.equals(Organization.class.getName())) {
+			Organization organization = _organizationService.getOrganization(
+				classNameClassPK.getClassPK());
+
+			return _websiteService.getWebsites(
+				organization.getModelClassName(),
+				organization.getOrganizationId());
+		}
+
+		User user = _userService.getUserById(classNameClassPK.getClassPK());
+
+		return _websiteService.getWebsites(
+			Contact.class.getName(), user.getContactId());
+	}
+
+	private WebUrl _toWebUrl(Website website) throws PortalException {
+		ListType listType = website.getType();
+
+		return new WebUrl() {
+			{
+				id = website.getWebsiteId();
+				url = website.getUrl();
+				urlType = listType.getName();
+			}
+		};
+	}
+
+	@Reference
+	private OrganizationService _organizationService;
+
+	@Reference
+	private UserService _userService;
+
+	@Reference
+	private WebsiteService _websiteService;
+
 }
