9a13fc559846252407b7466b6ab6fabb4ab3a3e8
Javier Gamarra <nhpatt@gmail.com>
2019-03-10 18:19:13 +0100

    LPS-91858 port API

--- modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/WorkflowLogResourceImpl.java
+++ modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/WorkflowLogResourceImpl.java
@@ -14,9 +14,21 @@
 
 package com.liferay.headless.workflow.internal.resource.v1_0;
 
+import com.liferay.headless.workflow.dto.v1_0.WorkflowLog;
+import com.liferay.headless.workflow.internal.dto.v1_0.util.CreatorUtil;
 import com.liferay.headless.workflow.resource.v1_0.WorkflowLogResource;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.workflow.WorkflowLogManager;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.workflow.kaleo.KaleoWorkflowModelConverter;
+import com.liferay.portal.workflow.kaleo.definition.util.KaleoLogUtil;
+import com.liferay.portal.workflow.kaleo.service.KaleoLogLocalService;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +39,70 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = WorkflowLogResource.class
 )
 public class WorkflowLogResourceImpl extends BaseWorkflowLogResourceImpl {
+
+	@Override
+	public WorkflowLog getWorkflowLog(Long workflowLogId) throws Exception {
+		return _toWorkflowLog(
+			_kaleoWorkflowModelConverter.toWorkflowLog(
+				_kaleoLogLocalService.getKaleoLog(workflowLogId)));
+	}
+
+	@Override
+	public Page<WorkflowLog> getWorkflowTaskWorkflowLogsPage(
+			Long workflowTaskId, Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			transform(
+				_workflowLogManager.getWorkflowLogsByWorkflowTask(
+					contextCompany.getCompanyId(), workflowTaskId, null,
+					pagination.getStartPosition(), pagination.getEndPosition(),
+					null),
+				this::_toWorkflowLog),
+			pagination,
+			_workflowLogManager.getWorkflowLogCountByWorkflowTask(
+				contextCompany.getCompanyId(), workflowTaskId, null));
+	}
+
+	private WorkflowLog _toWorkflowLog(
+			com.liferay.portal.kernel.workflow.WorkflowLog workflowLog)
+		throws PortalException {
+
+		return new WorkflowLog() {
+			{
+				auditPerson = CreatorUtil.toCreator(
+					_portal,
+					_userLocalService.getUser(workflowLog.getAuditUserId()));
+				commentLog = workflowLog.getComment();
+				dateCreated = workflowLog.getCreateDate();
+				id = workflowLog.getWorkflowLogId();
+				person = CreatorUtil.toCreator(
+					_portal,
+					_userLocalService.getUser(workflowLog.getUserId()));
+				previousPerson = CreatorUtil.toCreator(
+					_portal,
+					_userLocalService.getUser(workflowLog.getPreviousUserId()));
+				previousState = workflowLog.getPreviousState();
+				state = workflowLog.getState();
+				taskId = workflowLog.getWorkflowTaskId();
+				type = KaleoLogUtil.convert(workflowLog.getType());
+			}
+		};
+	}
+
+	@Reference
+	private KaleoLogLocalService _kaleoLogLocalService;
+
+	@Reference
+	private KaleoWorkflowModelConverter _kaleoWorkflowModelConverter;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+	@Reference
+	private WorkflowLogManager _workflowLogManager;
+
 }
--- modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/WorkflowTaskResourceImpl.java
+++ modules/apps/headless/headless-workflow/headless-workflow-impl/src/main/java/com/liferay/headless/workflow/internal/resource/v1_0/WorkflowTaskResourceImpl.java
@@ -14,9 +14,33 @@
 
 package com.liferay.headless.workflow.internal.resource.v1_0;
 
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.headless.workflow.dto.v1_0.ChangeDescription;
+import com.liferay.headless.workflow.dto.v1_0.ObjectReviewed;
+import com.liferay.headless.workflow.dto.v1_0.WorkflowTask;
+import com.liferay.headless.workflow.dto.v1_0.WorkflowTaskAssignToMe;
+import com.liferay.headless.workflow.dto.v1_0.WorkflowTaskAssignToUser;
 import com.liferay.headless.workflow.resource.v1_0.WorkflowTaskResource;
+import com.liferay.message.boards.model.MBDiscussion;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.workflow.WorkflowTaskManager;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.io.Serializable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.core.Context;
+
+import javax.xml.bind.ValidationException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +51,245 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = WorkflowTaskResource.class
 )
 public class WorkflowTaskResourceImpl extends BaseWorkflowTaskResourceImpl {
+
+	@Override
+	public Page<WorkflowTask> getRoleWorkflowTasksPage(
+			Long roleId, Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			transform(
+				_workflowTaskManager.getWorkflowTasksByRole(
+					contextCompany.getCompanyId(), roleId, null,
+					pagination.getStartPosition(), pagination.getEndPosition(),
+					null),
+				this::_toWorkflowTask),
+			pagination,
+			_workflowTaskManager.getWorkflowTaskCountByRole(
+				contextCompany.getCompanyId(), roleId, null));
+	}
+
+	@Override
+	public WorkflowTask getWorkflowTask(Long workflowTaskId) throws Exception {
+		return _toWorkflowTask(
+			_workflowTaskManager.getWorkflowTask(
+				contextCompany.getCompanyId(), workflowTaskId));
+	}
+
+	@Override
+	public Page<WorkflowTask> getWorkflowTasksByWorkflowTask(
+			String workflowTaskId, Pagination pagination)
+		throws Exception {
+
+		if (workflowTaskId.equals(WorkflowTaskType.TO_ME.getName())) {
+			return Page.of(
+				transform(
+					_workflowTaskManager.getWorkflowTasksByUser(
+						contextCompany.getCompanyId(), _user.getUserId(), null,
+						pagination.getStartPosition(),
+						pagination.getEndPosition(), null),
+					this::_toWorkflowTask),
+				pagination,
+				_workflowTaskManager.getWorkflowTaskCountByUser(
+					contextCompany.getCompanyId(), _user.getUserId(), null));
+		}
+		else if (workflowTaskId.equals(
+					WorkflowTaskType.TO_MY_ROLES.getName())) {
+
+			return Page.of(
+				transform(
+					_workflowTaskManager.getWorkflowTasksByUserRoles(
+						contextCompany.getCompanyId(), _user.getUserId(), null,
+						pagination.getStartPosition(),
+						pagination.getEndPosition(), null),
+					this::_toWorkflowTask),
+				pagination,
+				_workflowTaskManager.getWorkflowTaskCountByUserRoles(
+					contextCompany.getCompanyId(), _user.getUserId(), null));
+		}
+
+		return null;
+	}
+
+	@Override
+	public WorkflowTask postWorkflowTaskAssignToMe(
+			Long workflowTaskId, WorkflowTaskAssignToMe workflowTaskAssignToMe)
+		throws Exception {
+
+		return _toWorkflowTask(
+			_workflowTaskManager.assignWorkflowTaskToUser(
+				_user.getCompanyId(), _user.getUserId(), workflowTaskId,
+				_user.getUserId(), workflowTaskAssignToMe.getComment(),
+				workflowTaskAssignToMe.getDueDate(), null));
+	}
+
+	@Override
+	public WorkflowTask postWorkflowTaskAssignToUser(
+			Long workflowTaskId,
+			WorkflowTaskAssignToUser workflowTaskAssignToUser)
+		throws Exception {
+
+		long assigneeId = workflowTaskAssignToUser.getAssignee();
+
+		return _toWorkflowTask(
+			_workflowTaskManager.assignWorkflowTaskToUser(
+				contextCompany.getCompanyId(), _user.getUserId(),
+				workflowTaskId, assigneeId, "", null, null));
+	}
+
+	@Override
+	public WorkflowTask postWorkflowTaskChangeTransition(
+			Long workflowTaskId, ChangeDescription changeDescription)
+		throws Exception {
+
+		String transition = changeDescription.getTransition();
+
+		List<String> transitionsNames = _getTaskTransitionsNames(
+			_workflowTaskManager.getWorkflowTask(
+				_user.getCompanyId(), workflowTaskId));
+
+		if (!transitionsNames.contains(transition)) {
+			throw new ValidationException("Invalid transition: " + transition);
+		}
+
+		return _toWorkflowTask(
+			_workflowTaskManager.completeWorkflowTask(
+				_user.getCompanyId(), _user.getUserId(), workflowTaskId,
+				transition, "", null));
+	}
+
+	@Override
+	public WorkflowTask postWorkflowTaskUpdateDueDate(
+			Long workflowTaskId, WorkflowTaskAssignToMe workflowTaskAssignToMe)
+		throws Exception {
+
+		return _toWorkflowTask(
+			_workflowTaskManager.updateDueDate(
+				_user.getCompanyId(), _user.getUserId(), workflowTaskId,
+				workflowTaskAssignToMe.getComment(),
+				workflowTaskAssignToMe.getDueDate()));
+	}
+
+	public enum WorkflowTaskType {
+
+		TO_ME("assigned-to-me"), TO_MY_ROLES("assigned-to-my-roles");
+
+		public String getName() {
+			return _name;
+		}
+
+		private WorkflowTaskType(String name) {
+			_name = name;
+		}
+
+		private final String _name;
+
+	}
+
+	private String _getResourceType(
+		Map<String, Serializable> optionalAttributes) {
+
+		Map<String, String> map = new HashMap<String, String>() {
+			{
+				put(BlogsEntry.class.getName(), "BlogPosting");
+				put(MBDiscussion.class.getName(), "Comment");
+			}
+		};
+
+		String type = map.get(optionalAttributes.get("entryClassName"));
+
+		if (type == null) {
+			return null;
+		}
+
+		return type;
+	}
+
+	private String _getResourceURL(
+		Map<String, Serializable> optionalAttributes) {
+
+		Map<String, String> map = new HashMap<String, String>() {
+			{
+				put(BlogsEntry.class.getName(), "blog-posting");
+				put(MBDiscussion.class.getName(), "comment");
+			}
+		};
+
+		String entryClassName = map.get(
+			optionalAttributes.get("entryClassName"));
+
+		if (entryClassName == null) {
+			return null;
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("p/");
+		sb.append(entryClassName);
+		sb.append("/");
+
+		String entryClassPK = (String)optionalAttributes.get("entryClassPK");
+
+		sb.append(entryClassPK);
+
+		return sb.toString();
+	}
+
+	private List<String> _getTaskTransitionsNames(
+			com.liferay.portal.kernel.workflow.WorkflowTask workflowTask)
+		throws PortalException {
+
+		User user = _userLocalService.getUserById(
+			workflowTask.getAssigneeUserId());
+
+		return _workflowTaskManager.getNextTransitionNames(
+			user.getCompanyId(), workflowTask.getAssigneeUserId(),
+			workflowTask.getWorkflowTaskId());
+	}
+
+	private ObjectReviewed _toObjectReviewed(
+		Map<String, Serializable> optionalAttributes) {
+
+		return new ObjectReviewed() {
+			{
+				identifier = _getResourceURL(optionalAttributes);
+				resourceType = _getResourceType(optionalAttributes);
+			}
+		};
+	}
+
+	private WorkflowTask _toWorkflowTask(
+			com.liferay.portal.kernel.workflow.WorkflowTask workflowTask)
+		throws PortalException {
+
+		return new WorkflowTask() {
+			{
+				completed = workflowTask.isCompleted();
+				dateCompleted = workflowTask.getCompletionDate();
+				dateCreated = workflowTask.getCreateDate();
+				definitionName = workflowTask.getWorkflowDefinitionName();
+				description = workflowTask.getDescription();
+				dueDate = workflowTask.getDueDate();
+				id = workflowTask.getWorkflowTaskId();
+				name = workflowTask.getName();
+				objectReviewed = _toObjectReviewed(
+					workflowTask.getOptionalAttributes());
+				transitions = _getTaskTransitionsNames(
+					workflowTask
+				).toArray(
+					new String[0]
+				);
+			}
+		};
+	}
+
+	@Context
+	private User _user;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+	@Reference
+	private WorkflowTaskManager _workflowTaskManager;
+
 }
