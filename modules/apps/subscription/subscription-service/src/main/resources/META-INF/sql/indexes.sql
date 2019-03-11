48b8ee7bdfde894aab8cd4f45f08f88b2a3590e6
Roberto Díaz <roberto.diaz@liferay.com>
2019-03-11 13:40:37 +0100

    LPS-85419 Autogenerated (for 7.1 version)

--- modules/apps/subscription/subscription-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/subscription/subscription-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,3 +1,4 @@
+create index IX_4410586B on Subscription (classNameId);
 create index IX_786D171A on Subscription (companyId, classNameId, classPK);
 create unique index IX_2E1A92D4 on Subscription (companyId, userId, classNameId, classPK);
 create index IX_1290B81 on Subscription (groupId, userId);
