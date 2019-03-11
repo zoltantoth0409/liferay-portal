3cf549de3c5e1ef65597d639caecaf483745e374
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2019-03-11 14:06:59 +0100

    LPS-90290 Test the default SAP no longer applies

--- modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/SAPClientTest.java
+++ modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/SAPClientTest.java
@@ -65,6 +65,13 @@ public class SAPClientTest extends BaseClientTestCase {
 
 		Assert.assertEquals(false, builder.get(Boolean.class));
 
+		webTarget = getWebTarget("CUSTOM_SAP/AUTHORIZED_OAUTH2_SAP");
+
+		builder = authorize(
+			webTarget.request(), getToken("oauthTestApplication"));
+
+		Assert.assertEquals(false, builder.get(Boolean.class));
+
 		webTarget = getWebTarget("CUSTOM_SAP/CUSTOM_SAP");
 
 		builder = authorize(
