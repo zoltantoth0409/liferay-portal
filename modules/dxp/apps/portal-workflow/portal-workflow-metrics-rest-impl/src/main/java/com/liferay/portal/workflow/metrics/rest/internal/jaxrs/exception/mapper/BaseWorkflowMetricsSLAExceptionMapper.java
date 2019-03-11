9fcc5dca772c7ed1a2e967c93a9eb0181503fce5
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-03-11 13:38:26 -0300

    LPS-89014 | LPS-91445 Add exception mapper to each sla definition expections

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/jaxrs/exception/mapper/BaseWorkflowMetricsSLAExceptionMapper.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.workflow.metrics.rest.dto.v1_0.GenericError;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rafael Praxedes
+ */
+public abstract class BaseWorkflowMetricsSLAExceptionMapper
+	<T extends PortalException>
+		implements ExceptionMapper<T> {
+
+	public String getFieldName() {
+		return StringPool.BLANK;
+	}
+
+	public abstract String getKey();
+
+	@Override
+	public Response toResponse(T portalException) {
+		return Response.status(
+			Response.Status.BAD_REQUEST
+		).entity(
+			new GenericError() {
+				{
+					fieldName =
+						BaseWorkflowMetricsSLAExceptionMapper.this.
+							getFieldName();
+					message =
+						BaseWorkflowMetricsSLAExceptionMapper.this.getMessage();
+				}
+			}
+		).build();
+	}
+
+	protected String getMessage() {
+		return language.get(
+			contextAcceptLanguage.getPreferredLocale(), getKey());
+	}
+
+	@Context
+	protected AcceptLanguage contextAcceptLanguage;
+
+	@Reference
+	protected Language language;
+
+}
---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/jaxrs/exception/mapper/WorkflowMetricsSLADefinitionDuplicateNameExceptionMapper.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.portal.workflow.metrics.exception.WorkflowMetricsSLADefinitionDuplicateNameException;
+
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=portal-workflow-metrics-rest-application)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=portal-workflow-metrics-rest-application.v1_0.WorkflowMetricsSLADefinitionDuplicateNameExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class WorkflowMetricsSLADefinitionDuplicateNameExceptionMapper
+	extends BaseWorkflowMetricsSLAExceptionMapper
+		<WorkflowMetricsSLADefinitionDuplicateNameException> {
+
+	@Override
+	public String getFieldName() {
+		return "name";
+	}
+
+	@Override
+	public String getKey() {
+		return "an-sla-with-the-same-name-already-exists";
+	}
+
+}
