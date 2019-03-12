b3be3074e579ae78a6df3fdee40cd93fc63d8102
Hugo Huijser <hugo.huijser@liferay.com>
2019-03-12 15:25:00 +0800

    LPS-84119 Use kernel.util.StringBundler

--- modules/apps/collaboration/document-library/document-library-repository-cmis-api/src/main/java/com/liferay/document/library/repository/cmis/search/CMISNotExpression.java
+++ modules/apps/collaboration/document-library/document-library-repository-cmis-api/src/main/java/com/liferay/document/library/repository/cmis/search/CMISNotExpression.java
@@ -14,7 +14,7 @@
 
 package com.liferay.document.library.repository.cmis.search;
 
-import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.StringBundler;
 
 /**
  * @author Mika Koivisto
