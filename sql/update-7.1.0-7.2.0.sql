a7eb6ad84c49240e274afac92e2548d81ec548be
Máté Thurzó <mate.thurzo@liferay.com>
2019-03-12 14:01:13 +0100

    LPS-89210 Index statements are not neede

--- sql/update-7.1.0-7.2.0.sql
+++ sql/update-7.1.0-7.2.0.sql
@@ -68,20 +68,9 @@ from LayoutSetVersion;
 alter table LayoutSet add head BOOLEAN;
 alter table LayoutSet add headId LONG;
 
-drop index IX_48550691 on LayoutSet;
-
 COMMIT_TRANSACTION;
 
 update LayoutSet set headId = -1 * layoutSetId, head = TRUE;
-
-create unique index IX_AAF037A5 on LayoutSet (groupId, privateLayout, head);
-create unique index IX_28B7204D on LayoutSet (headId);
-
-create unique index IX_7CDFE94F on LayoutSetVersion (groupId, privateLayout, version);
-create index IX_1F8F53E4 on LayoutSetVersion (groupId, version);
-create unique index IX_D88D5E2B on LayoutSetVersion (layoutSetId, version);
-create index IX_61B651E9 on LayoutSetVersion (layoutSetPrototypeUuid[$COLUMN_LENGTH:75$], version);
-create index IX_1D356397 on LayoutSetVersion (privateLayout, logoId, version);
 
 create table LayoutVersion (
 	layoutVersionId LONG not null primary key,
@@ -180,30 +169,3 @@ insert into Counter (name, currentId)
 	from LayoutVersion;
 
 COMMIT_TRANSACTION;
-
-create unique index IX_C1143B45 on Layout (groupId, privateLayout, friendlyURL[$COLUMN_LENGTH:255$], head);
-create unique index IX_D2DE1750 on Layout (groupId, privateLayout, layoutId, head);
-create unique index IX_9D3FD85F on Layout (headId);
-create unique index IX_92B7E1CB on Layout (uuid_[$COLUMN_LENGTH:75$], groupId, privateLayout, head);
-
-create index IX_FBF6F939 on LayoutVersion (companyId, layoutPrototypeUuid[$COLUMN_LENGTH:75$], version);
-create index IX_DEFEF354 on LayoutVersion (companyId, version);
-create index IX_D0513E86 on LayoutVersion (groupId, leftPlid, rightPlid, privateLayout, version);
-create unique index IX_6A5941DB on LayoutVersion (groupId, privateLayout, friendlyURL[$COLUMN_LENGTH:255$], version);
-create unique index IX_B816A2A8 on LayoutVersion (groupId, privateLayout, layoutId, version);
-create index IX_1A490C46 on LayoutVersion (groupId, privateLayout, parentLayoutId, priority, version);
-create index IX_23BEB05E on LayoutVersion (groupId, privateLayout, parentLayoutId, version);
-create index IX_C32CE133 on LayoutVersion (groupId, privateLayout, sourcePrototypeLayoutUuid[$COLUMN_LENGTH:75$], version);
-create index IX_7E7D225A on LayoutVersion (groupId, privateLayout, type_[$COLUMN_LENGTH:75$], version);
-create index IX_B9B3D961 on LayoutVersion (groupId, privateLayout, version);
-create index IX_B3248089 on LayoutVersion (groupId, type_[$COLUMN_LENGTH:75$], version);
-create index IX_F8CF8212 on LayoutVersion (groupId, version);
-create index IX_182CF60F on LayoutVersion (iconImageId, version);
-create index IX_B8044279 on LayoutVersion (layoutPrototypeUuid[$COLUMN_LENGTH:75$], version);
-create index IX_1A7E559F on LayoutVersion (parentPlid, version);
-create unique index IX_F852DF29 on LayoutVersion (plid, version);
-create index IX_C5652CE4 on LayoutVersion (privateLayout, iconImageId, version);
-create index IX_45E580A0 on LayoutVersion (sourcePrototypeLayoutUuid[$COLUMN_LENGTH:75$], version);
-create index IX_E66E67A0 on LayoutVersion (uuid_[$COLUMN_LENGTH:75$], companyId, version);
-create unique index IX_BD8C095 on LayoutVersion (uuid_[$COLUMN_LENGTH:75$], groupId, privateLayout, version);
-create index IX_82FCA548 on LayoutVersion (uuid_[$COLUMN_LENGTH:75$], version);
