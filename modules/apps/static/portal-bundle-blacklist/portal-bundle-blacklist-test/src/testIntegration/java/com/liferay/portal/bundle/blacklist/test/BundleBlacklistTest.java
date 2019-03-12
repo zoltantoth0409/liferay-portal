77c7c00e0c52c703e1709f8e637190d7206e8278
Matthew Tambara <matthew.tambara@liferay.com>
2019-03-12 14:07:46 -0700

    LPS-90703 Stop ignoring test

--- modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist-test/src/testIntegration/java/com/liferay/portal/bundle/blacklist/test/BundleBlacklistTest.java
+++ modules/apps/static/portal-bundle-blacklist/portal-bundle-blacklist-test/src/testIntegration/java/com/liferay/portal/bundle/blacklist/test/BundleBlacklistTest.java
@@ -40,7 +40,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -59,7 +58,6 @@ import org.osgi.util.tracker.BundleTracker;
 /**
  * @author Matthew Tambara
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class BundleBlacklistTest {
 
