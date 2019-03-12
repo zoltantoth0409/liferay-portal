f7ffb354ac5357f9d652dba44a462873705525df
Dante Wang <dante.wang@liferay.com>
2019-03-12 14:57:31 +0800

    LPS-92088 BaseIndexerGetFullQueryTest does not need mock *IndexerRegistry which is leftover after being moved from unit to integration

--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/test/BaseIndexerGetFullQueryTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/test/BaseIndexerGetFullQueryTest.java
@@ -19,27 +19,12 @@ import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.portal.kernel.search.BaseIndexer;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerRegistry;
-import com.liferay.portal.kernel.search.RelatedEntryIndexer;
-import com.liferay.portal.kernel.search.RelatedEntryIndexerRegistry;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.search.test.TestIndexerRegistry;
-import com.liferay.registry.Registry;
-import com.liferay.registry.RegistryUtil;
-import com.liferay.registry.ServiceReference;
-import com.liferay.registry.ServiceTracker;
-import com.liferay.registry.ServiceTrackerCustomizer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
@@ -56,9 +41,7 @@ import org.junit.runner.RunWith;
 public class BaseIndexerGetFullQueryTest {
 
 	@Before
-	public void setUp() throws Exception {
-		setUpIndexerRegistry();
-
+	public void setUp() {
 		_indexer = new TestIndexer();
 	}
 
@@ -98,16 +81,6 @@ public class BaseIndexerGetFullQueryTest {
 
 		Assert.assertArrayEquals(
 			expectedEntryClassNames, actualEntryClassNames);
-	}
-
-	protected void setUpIndexerRegistry() {
-		Registry registry = RegistryUtil.getRegistry();
-
-		registry.registerService(
-			IndexerRegistry.class, new TestIndexerRegistry());
-		registry.registerService(
-			RelatedEntryIndexerRegistry.class,
-			new TestRelatedEntryIndexerRegistry());
 	}
 
 	private static final String _CLASS_NAME = RandomTestUtil.randomString();
@@ -151,131 +124,6 @@ public class BaseIndexerGetFullQueryTest {
 
 		@Override
 		protected void doReindex(String[] ids) throws Exception {
-		}
-
-	}
-
-	private class TestRelatedEntryIndexerRegistry
-		implements RelatedEntryIndexerRegistry {
-
-		public TestRelatedEntryIndexerRegistry() {
-			Registry registry = RegistryUtil.getRegistry();
-
-			_serviceTracker = registry.trackServices(
-				RelatedEntryIndexer.class,
-				new RelatedEntryIndexerServiceTrackerCustomizer());
-
-			_serviceTracker.open();
-		}
-
-		public void destroy() {
-			if (_serviceTracker != null) {
-				_serviceTracker.close();
-			}
-
-			_serviceTracker = null;
-		}
-
-		@Override
-		public List<RelatedEntryIndexer> getRelatedEntryIndexers() {
-			List<RelatedEntryIndexer> relatedEntryIndexers = new ArrayList<>();
-
-			for (List<RelatedEntryIndexer> currentRelatedEntryIndexers :
-					_relatedEntryIndexers.values()) {
-
-				relatedEntryIndexers.addAll(currentRelatedEntryIndexers);
-			}
-
-			return relatedEntryIndexers;
-		}
-
-		@Override
-		public List<RelatedEntryIndexer> getRelatedEntryIndexers(Class clazz) {
-			return getRelatedEntryIndexers(clazz.getName());
-		}
-
-		@Override
-		public List<RelatedEntryIndexer> getRelatedEntryIndexers(
-			String className) {
-
-			List<RelatedEntryIndexer> relatedEntryIndexers =
-				_relatedEntryIndexers.get(className);
-
-			if (relatedEntryIndexers == null) {
-				relatedEntryIndexers = Collections.emptyList();
-			}
-
-			return relatedEntryIndexers;
-		}
-
-		private final Map<String, List<RelatedEntryIndexer>>
-			_relatedEntryIndexers = new ConcurrentHashMap<>();
-		private ServiceTracker<RelatedEntryIndexer, RelatedEntryIndexer>
-			_serviceTracker;
-
-		private class RelatedEntryIndexerServiceTrackerCustomizer
-			implements ServiceTrackerCustomizer
-				<RelatedEntryIndexer, RelatedEntryIndexer> {
-
-			@Override
-			public RelatedEntryIndexer addingService(
-				ServiceReference<RelatedEntryIndexer> serviceReference) {
-
-				Registry registry = RegistryUtil.getRegistry();
-
-				RelatedEntryIndexer relatedEntryIndexer = registry.getService(
-					serviceReference);
-
-				String relatedEntryIndexerClassName =
-					(String)serviceReference.getProperty(
-						"related.entry.indexer.class.name");
-
-				if (Validator.isNull(relatedEntryIndexerClassName)) {
-					throw new IllegalStateException(
-						"Service must contain a " +
-							"related.entry.indexer.class.name");
-				}
-
-				List<RelatedEntryIndexer> relatedEntryIndexers =
-					_relatedEntryIndexers.get(relatedEntryIndexerClassName);
-
-				if (relatedEntryIndexers == null) {
-					relatedEntryIndexers = new ArrayList<>();
-
-					_relatedEntryIndexers.put(
-						relatedEntryIndexerClassName, relatedEntryIndexers);
-				}
-
-				relatedEntryIndexers.add(relatedEntryIndexer);
-
-				return relatedEntryIndexer;
-			}
-
-			@Override
-			public void modifiedService(
-				ServiceReference<RelatedEntryIndexer> serviceReference,
-				RelatedEntryIndexer relatedEntryIndexer) {
-			}
-
-			@Override
-			public void removedService(
-				ServiceReference<RelatedEntryIndexer> serviceReference,
-				RelatedEntryIndexer relatedEntryIndexer) {
-
-				Registry registry = RegistryUtil.getRegistry();
-
-				registry.ungetService(serviceReference);
-
-				String relatedEntryIndexerClassName =
-					(String)serviceReference.getProperty(
-						"related.entry.indexer.class.name");
-
-				List<RelatedEntryIndexer> relatedEntryIndexers =
-					_relatedEntryIndexers.get(relatedEntryIndexerClassName);
-
-				relatedEntryIndexers.remove(relatedEntryIndexer);
-			}
-
 		}
 
 	}
