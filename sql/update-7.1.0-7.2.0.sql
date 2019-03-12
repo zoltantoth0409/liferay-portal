c7151a6586d661b5ccb1b11f0130d2fd96562b7f
Brian Chan <brian.chan@liferay.com>
2019-03-11 22:13:33 -0700

    LPS-89210 Sort according to service.xml, and remove bad 'system', see we already have 'system_'

--- sql/update-7.1.0-7.2.0.sql
+++ sql/update-7.1.0-7.2.0.sql
@@ -1,12 +1,11 @@
+alter table Layout add headId LONG;
+alter table Layout add head BOOLEAN;
 alter table Layout add parentPlid LONG;
 alter table Layout add leftPlid LONG;
 alter table Layout add rightPlid LONG;
-alter table Layout add system_ BOOLEAN;
-alter table Layout add headId LONG;
-alter table Layout add head BOOLEAN;
 alter table Layout add classNameId LONG;
 alter table Layout add classPK LONG;
-alter table Layout add system BOOLEAN;
+alter table Layout add system_ BOOLEAN;
 alter table Layout add publishDate DATE null;
 
 COMMIT_TRANSACTION;
