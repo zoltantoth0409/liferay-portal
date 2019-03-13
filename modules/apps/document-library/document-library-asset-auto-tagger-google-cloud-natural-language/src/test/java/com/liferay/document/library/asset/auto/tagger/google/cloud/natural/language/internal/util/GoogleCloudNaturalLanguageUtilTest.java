19b1f1b66d6f1ee34990e49b9466c9c16aa91517
Alicia Garcia <alicia.garcia@liferay.com>
2019-03-13 18:36:32 +0100

    LPS-82637 GoogleCloudNaturalLanguageUtil test

---
+++ modules/apps/document-library/document-library-asset-auto-tagger-google-cloud-natural-language/src/test/java/com/liferay/document/library/asset/auto/tagger/google/cloud/natural/language/internal/util/GoogleCloudNaturalLanguageUtilTest.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.asset.auto.tagger.google.cloud.natural.language.internal.util;
+
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.Base64;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.util.FileImpl;
+
+import java.io.ByteArrayInputStream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * @author Alicia García
+ */
+public class GoogleCloudNaturalLanguageUtilTest {
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+
+		FileUtil fileUtil = new FileUtil();
+
+		fileUtil.setFile(new FileImpl());
+
+		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
+
+		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
+	}
+
+	@Test
+	public void testGetAnnotateImagePayload() throws Exception {
+		Mockito.when(
+			_fileEntry.getFileVersion()
+		).thenReturn(
+			_fileVersion
+		);
+
+		String randomString = RandomTestUtil.randomString();
+
+		Mockito.when(
+			_fileVersion.getContentStream(false)
+		).thenReturn(
+			new ByteArrayInputStream(randomString.getBytes())
+		);
+
+		List<String> actual =
+			GoogleCloudNaturalLanguageUtil.splitTextToMaxSizeCall(
+				_fileVersion, 5000);
+
+		List<String> expected = new ArrayList<>();
+
+		expected.add(
+			StringBundler.concat(
+				"{\"document\":{\"type\":\"PLAIN_TEXT\",\"content\":\"",
+				Base64.encode(randomString.getBytes()), "\"}}"));
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Mock
+	private FileEntry _fileEntry;
+
+	@Mock
+	private FileVersion _fileVersion;
+
+}
