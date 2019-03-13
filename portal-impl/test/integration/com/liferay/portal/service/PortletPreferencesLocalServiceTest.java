f1c1f4ad41fc2ea20ab05eccc35b1542950b821b
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2019-03-13 15:26:34 +0100

    LPS-91113 Fix tests. Deploy portlet as method is now taking it to account if portlet is deployed or not.

--- portal-impl/test/integration/com/liferay/portal/service/PortletPreferencesLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/PortletPreferencesLocalServiceTest.java
@@ -14,11 +14,13 @@
 
 package com.liferay.portal.service;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.deploy.hot.ServiceBag;
 import com.liferay.portal.kernel.bean.ClassLoaderBeanHandler;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.PortletApp;
 import com.liferay.portal.kernel.model.PortletPreferences;
 import com.liferay.portal.kernel.model.PortletPreferencesIds;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
@@ -33,6 +35,7 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.util.ClassLoaderUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.model.impl.PortletAppImpl;
 import com.liferay.portal.service.util.test.PortletPreferencesImplTestUtil;
 import com.liferay.portal.service.util.test.PortletPreferencesTestUtil;
 import com.liferay.portal.spring.aop.ServiceBeanAopCacheManagerUtil;
@@ -46,6 +49,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -76,6 +80,13 @@ public class PortletPreferencesLocalServiceTest {
 
 		_portlet = PortletLocalServiceUtil.getPortletById(
 			_layout.getCompanyId(), String.valueOf(_PORTLET_ID));
+	}
+
+	@After
+	public void tearDown() {
+		_portlet.setPortletApp(_DUMMY_PORTLET_APP);
+
+		PortletLocalServiceUtil.destroyPortlet(_portlet);
 	}
 
 	@Test
@@ -834,6 +845,8 @@ public class PortletPreferencesLocalServiceTest {
 		PortletPreferencesTestUtil.addLayoutPortletPreferences(
 			_layout, _portlet);
 
+		PortletLocalServiceUtil.deployPortlet(_portlet);
+
 		Assert.assertEquals(
 			1,
 			PortletPreferencesLocalServiceUtil.getPortletPreferencesCount(
@@ -1181,6 +1194,8 @@ public class PortletPreferencesLocalServiceTest {
 		PortletPreferencesTestUtil.addLayoutPortletPreferences(
 			_layout, _portlet);
 
+		PortletLocalServiceUtil.deployPortlet(_portlet);
+
 		Assert.assertEquals(
 			1,
 			PortletPreferencesLocalServiceUtil.getPortletPreferencesCount(
@@ -1524,6 +1539,9 @@ public class PortletPreferencesLocalServiceTest {
 		ServiceBeanAopCacheManagerUtil.reset();
 	}
 
+	private static final PortletApp _DUMMY_PORTLET_APP = new PortletAppImpl(
+		StringPool.CONTENT);
+
 	private static final String[] _MULTIPLE_VALUES = {"value1", "value2"};
 
 	private static final String _NAME = "name";
@@ -1538,7 +1556,10 @@ public class PortletPreferencesLocalServiceTest {
 	private final List<Group> _groups = new ArrayList<>();
 
 	private Layout _layout;
+
+	@DeleteAfterTestRun
 	private Portlet _portlet;
+
 	private ServiceBag<PortletPreferencesLocalService> _serviceBag;
 
 	private static class TestPortletPreferencesLocalServiceWrapper
