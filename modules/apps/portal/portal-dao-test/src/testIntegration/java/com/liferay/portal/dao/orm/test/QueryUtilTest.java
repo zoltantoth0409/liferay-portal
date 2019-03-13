1796d860b85801eb618ed55477a95aa02208234e
hai.yu <8219754@liferay.com>
2019-03-13 18:21:01 +0800

    LPS-92527 Move class of com.liferay.portal.kernel.dao.orm package to module

--- modules/apps/portal/portal-dao-test/src/testIntegration/java/com/liferay/portal/dao/orm/test/QueryUtilTest.java
+++ modules/apps/portal/portal-dao-test/src/testIntegration/java/com/liferay/portal/dao/orm/test/QueryUtilTest.java
@@ -12,10 +12,16 @@
  * details.
  */
 
-package com.liferay.portal.kernel.dao.orm;
+package com.liferay.portal.dao.orm.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.dao.orm.SessionFactory;
+import com.liferay.portal.kernel.dao.orm.Type;
 import com.liferay.portal.kernel.service.persistence.UserUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -32,10 +38,12 @@ import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Sampsa Sohlman
  */
+@RunWith(Arquillian.class)
 public class QueryUtilTest {
 
 	@ClassRule
