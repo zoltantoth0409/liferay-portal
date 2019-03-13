03e2937f86efc2487e10a21f135098541a93e846
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-03-13 14:05:43 +0100

    LPS-91941 Add a utility class to normalize strings as valid OData identifiers

---
+++ modules/apps/portal-odata/portal-odata-api/src/test/java/com/liferay/portal/odata/util/NormalizerTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.odata.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Eduardo García
+ */
+public class NormalizerTest {
+
+	@Test
+	public void testNormalizeIdentifierForNotNullValue() {
+		String identifier = Normalizer.normalizeIdentifier(
+			"Aaa Ááá Üüü B'bb (Ccc) Ñññ d_d _[]+-./&ªº!|\"·$=?¿¡`^*¨´Ç};:-");
+
+		Assert.assertEquals("Aaa_Ááá_Üüü_Bbb_Ccc_Ñññ_d_d__ªºÇ", identifier);
+	}
+
+}
