68cebd949b20291b2d59332116f604b2b5894974
Brian Chan <brian.chan@liferay.com>
2019-03-14 13:10:19 -0700

    LPS-91949 Sort

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/search/spi/model/index/contributor/KaleoTaskInstanceTokenModelDocumentContributor.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/search/spi/model/index/contributor/KaleoTaskInstanceTokenModelDocumentContributor.java
@@ -85,16 +85,16 @@ public class KaleoTaskInstanceTokenModelDocumentContributor
 			KaleoTaskInstanceTokenField.ASSIGNEE_GROUP_IDS,
 			assigneeGroupIds.toArray(new Long[assigneeGroupIds.size()]));
 		document.addKeyword(
+			KaleoTaskInstanceTokenField.CLASS_NAME,
+			kaleoTaskInstanceToken.getClassName());
+		document.addKeyword(
+			Field.CLASS_PK, kaleoTaskInstanceToken.getClassPK());
+		document.addKeyword(
 			KaleoTaskInstanceTokenField.COMPLETED,
 			kaleoTaskInstanceToken.isCompleted());
 		document.addDate(
 			KaleoTaskInstanceTokenField.COMPLETION_DATE,
 			kaleoTaskInstanceToken.getCompletionDate());
-		document.addKeyword(
-			KaleoTaskInstanceTokenField.CLASS_NAME,
-			kaleoTaskInstanceToken.getClassName());
-		document.addKeyword(
-			Field.CLASS_PK, kaleoTaskInstanceToken.getClassPK());
 		document.addDate(
 			KaleoTaskInstanceTokenField.DUE_DATE,
 			kaleoTaskInstanceToken.getDueDate());
