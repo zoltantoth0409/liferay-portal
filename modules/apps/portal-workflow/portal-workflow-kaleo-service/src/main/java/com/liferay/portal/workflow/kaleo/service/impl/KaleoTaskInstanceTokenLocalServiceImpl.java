ba4a14e0a7628a5b075d7e3d95e85fc5ad7e0b4f
Brian Chan <brian.chan@liferay.com>
2019-03-14 13:06:38 -0700

    LPS-91949 Rename

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoTaskInstanceTokenLocalServiceImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoTaskInstanceTokenLocalServiceImpl.java
@@ -792,10 +792,10 @@ public class KaleoTaskInstanceTokenLocalServiceImpl
 
 		return stream.map(
 			orderByCol -> {
-				String fieldName = _fieldNameOrderByColMap.getOrDefault(
+				String fieldName = _fieldNameOrderByCols.getOrDefault(
 					orderByCol, orderByCol);
 
-				int sortType = _fieldNameSortTypeMap.getOrDefault(
+				int sortType = _fieldNameSortTypes.getOrDefault(
 					fieldName, Sort.STRING_TYPE);
 
 				return new Sort(
@@ -809,7 +809,7 @@ public class KaleoTaskInstanceTokenLocalServiceImpl
 	private static final Log _log = LogFactoryUtil.getLog(
 		KaleoTaskInstanceTokenLocalServiceImpl.class);
 
-	private static final Map<String, String> _fieldNameOrderByColMap =
+	private static final Map<String, String> _fieldNameOrderByCols =
 		new HashMap<String, String>() {
 			{
 				put("completed", KaleoTaskInstanceTokenField.COMPLETED);
@@ -827,7 +827,7 @@ public class KaleoTaskInstanceTokenLocalServiceImpl
 				put("userId", Field.USER_ID);
 			}
 		};
-	private static final Map<String, Integer> _fieldNameSortTypeMap =
+	private static final Map<String, Integer> _fieldNameSortTypes =
 		new HashMap<String, Integer>() {
 			{
 				put(Field.CREATE_DATE, Sort.LONG_TYPE);
