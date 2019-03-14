b042aa56d69fb3e678c0ced6faf44b5e26bce035
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-03-14 14:49:13 -0300

    LPS-88573|LPS-89590 Add ListView component.

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/empty-state/LoadingState.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * LoadingState.
+ * @extends React.Component
+ */
+export default class LoadingState extends React.Component {
+	render() {
+		return (
+			<span aria-hidden="true" className="loading-animation">
+			</span>
+		);
+	}
+}
---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/list/ListView.js
@@ -0,0 +1,46 @@
+import EmptyContent from '../EmptyContent';
+import LoadingState from '../empty-state/LoadingState';
+import React from 'react';
+
+/**
+ * ListView.
+ * @extends React.Component
+ */
+export default class ListView extends React.Component {
+	render() {
+		const {children, emptyMessageText, emptyTitleText, isFetching, isLoading, isSearching} = this.props;
+
+		const emptySearchRender = secondaryRender =>
+			isSearching ? (
+				<EmptyContent
+					message={emptyMessageText}
+					type="not-found"
+				/>
+			) : (
+				secondaryRender
+			);
+
+		const emptyContentRender = secondaryRender =>
+			isFetching ? (
+				<EmptyContent
+					message={emptyMessageText}
+					title={emptyTitleText}
+				/>
+			) : (
+				secondaryRender
+			);
+
+		const emptyLoadingState = secondaryRender =>
+			isLoading ? (
+				<LoadingState />
+			) : (
+				secondaryRender
+			);
+
+		return (
+			<div>
+				{emptySearchRender(emptyContentRender(emptyLoadingState(children)))}
+			</div>
+		);
+	}
+}
