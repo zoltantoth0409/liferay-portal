02c8fc164675a315c838de87cb9be7d66c4a5ac5
Paulo Cruz <paulo.cruz@liferay.com>
2019-03-15 14:49:57 -0300

    LPS-89014 | LPS-89911 Create a wrapper to handle requests with json body

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/rest/fetch.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/rest/fetch.js
@@ -1,21 +1,40 @@
+const getResponseBody = response => {
+	const contentType = response.headers.get('content-type');
+
+	if (contentType && contentType.includes('application/json')) {
+		return response.json();
+	}
+
+	return response.text().then(error => ({
+		message: error
+	}));
+};
+
 /**
  * @module fetch
- * @description  Basic alias of fetch client to perform consults in portal rest API.
+ * @description Basic alias of fetch client to perform consults in portal rest API.
  * @example
  * import fetch from '@/shared/rest/fetch';
  * fetch('/process').then(res => console.log(res));
- * */
+ */
 const restClient = (url, config = {}) => {
 	if (url) {
 		url = `/o/portal-workflow-metrics/v1.0${url}`;
 	}
 
-	return fetch(url, config).then(res => {
-		if (!res.ok) {
-			throw res;
-		}
-
-		return res.json();
+	return new Promise((resolve, reject) => {
+		fetch(url, config).then(response => {
+			if (response && response.ok) {
+				getResponseBody(response)
+					.then(resolve)
+					.catch(reject);
+			}
+			else {
+				getResponseBody(response)
+					.then(reject)
+					.catch(reject);
+			}
+		});
 	});
 };
 
