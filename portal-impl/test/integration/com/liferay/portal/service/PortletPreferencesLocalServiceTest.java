4ac39d93e4ad22076972d9b146eef2f3d323cb49
Brian Chan <brian.chan@liferay.com>
2019-03-17 14:52:21 -0700

    LPS-91113 SF

--- portal-impl/test/integration/com/liferay/portal/service/PortletPreferencesLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/PortletPreferencesLocalServiceTest.java
@@ -81,7 +81,7 @@ public class PortletPreferencesLocalServiceTest {
 
 	@After
 	public void tearDown() {
-		_portlet.setPortletApp(_DUMMY_PORTLET_APP);
+		_portlet.setPortletApp(_portletApp);
 
 		PortletLocalServiceUtil.destroyPortlet(_portlet);
 	}
@@ -1530,7 +1530,7 @@ public class PortletPreferencesLocalServiceTest {
 		_serviceBag.replace();
 	}
 
-	private static final PortletApp _DUMMY_PORTLET_APP = new PortletAppImpl(
+	private static final PortletApp _portletApp = new PortletAppImpl(
 		StringPool.CONTENT);
 
 	private static final String[] _MULTIPLE_VALUES = {"value1", "value2"};
