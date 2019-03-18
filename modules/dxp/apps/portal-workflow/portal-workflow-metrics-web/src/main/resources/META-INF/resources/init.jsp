92fbb638f8674af831e929b45a45720d172b60a1
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-03-18 10:50:33 -0300

    LPS-88573|LPS-89590 Add global pagination properties.

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/init.jsp
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/init.jsp
@@ -20,6 +20,8 @@
 taglib uri="http://liferay.com/tld/frontend" prefix="liferay-frontend" %><%@
 taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
 
+<%@ page import="com.liferay.portal.util.PropsValues" %>
+
 <liferay-frontend:defineObjects />
 
 <liferay-theme:defineObjects />
--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/index.js
@@ -2,9 +2,14 @@ import AppComponent from './components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default function(elementId) {
+export default function(elementId, defaultDelta, deltas, maxPages) {
 	ReactDOM.render(
-		<AppComponent companyId={Liferay.ThemeDisplay.getCompanyId()} />,
+		<AppComponent
+			companyId={Liferay.ThemeDisplay.getCompanyId()}
+			defaultDelta={defaultDelta}
+			deltas={deltas}
+			maxPages={maxPages}
+		/>,
 		document.getElementById(elementId)
 	);
 }
