5d3a6500be9748884919808ace52241d2b75fd36
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2019-03-18 17:52:08 -0300

    LPS-91824 Create data storage

---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/dto/v1_0/util/DataRecordValueUtil.java
@@ -0,0 +1,180 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.dto.v1_0.util;
+
+import com.liferay.data.engine.rest.dto.v1_0.DataDefinition;
+import com.liferay.data.engine.rest.dto.v1_0.DataDefinitionField;
+import com.liferay.data.engine.rest.dto.v1_0.DataRecordValue;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * @author Jeyvison Nascimento
+ */
+public class DataRecordValueUtil {
+
+	public static DataRecordValue[] toDataRecordValues(
+			DataDefinition dataDefinition, String json)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(json);
+
+		List<DataRecordValue> dataRecordValues = new ArrayList<>();
+
+		for (DataDefinitionField dataDefinitionField :
+				dataDefinition.getDataDefinitionFields()) {
+
+			String name = dataDefinitionField.getName();
+
+			if (!jsonObject.has(name)) {
+				continue;
+			}
+
+			DataRecordValue dataRecordValue = new DataRecordValue() {
+				{
+					key = name;
+
+					if (dataDefinitionField.getLocalizable()) {
+						JSONObject dataRecordValueJSONObject =
+							jsonObject.getJSONObject(name);
+
+						Iterable<String> iterable =
+							dataRecordValueJSONObject::keys;
+
+						Map<String, Object> localizedValues = new HashMap<>();
+
+						StreamSupport.stream(
+							iterable.spliterator(), false
+						).forEach(
+							key -> localizedValues.put(
+								key, dataRecordValueJSONObject.get(key))
+						);
+
+						value = localizedValues;
+					}
+					else if (dataDefinitionField.getRepeatable()) {
+						value = JSONUtil.toObjectArray(
+							jsonObject.getJSONArray(name));
+					}
+					else {
+						value = jsonObject.get(name);
+					}
+				}
+			};
+
+			dataRecordValues.add(dataRecordValue);
+		}
+
+		return dataRecordValues.toArray(
+			new DataRecordValue[dataRecordValues.size()]);
+	}
+
+	public static String toJSON(
+			DataDefinition dataDefinition, DataRecordValue[] dataRecordValues)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		_addDEDataDefinitionFieldValue(
+			jsonObject, dataDefinition, dataRecordValues);
+
+		return jsonObject.toString();
+	}
+
+	private static void _addDEDataDefinitionFieldValue(
+			JSONObject jsonObject, DataDefinition dataDefinition,
+			DataRecordValue[] dataRecordValues)
+		throws Exception {
+
+		Map<String, Object> dataRecordValuesMap = _toDataRecordValuesMap(
+			dataRecordValues);
+
+		Map<String, DataDefinitionField> dataDefinitionFields = Stream.of(
+			dataDefinition.getDataDefinitionFields()
+		).collect(
+			Collectors.toMap(field -> field.getName(), Function.identity())
+		);
+
+		for (Map.Entry<String, DataDefinitionField> entry :
+				dataDefinitionFields.entrySet()) {
+
+			if (!dataRecordValuesMap.containsKey(entry.getKey())) {
+				continue;
+			}
+
+			DataDefinitionField dataDefinitionField = entry.getValue();
+
+			if (dataDefinitionField.getLocalizable()) {
+				JSONObject localizedJSONObject =
+					JSONFactoryUtil.createJSONObject();
+
+				_addLocalizedValues(
+					localizedJSONObject,
+					(Map<String, Object>)dataRecordValuesMap.get(
+						dataDefinitionField.getName()));
+
+				jsonObject.put(entry.getKey(), localizedJSONObject);
+			}
+			else if (dataDefinitionField.getRepeatable()) {
+				jsonObject.put(
+					entry.getKey(),
+					JSONUtil.toJSONArray(
+						(Object[])dataRecordValuesMap.get(entry.getKey()),
+						object -> object));
+			}
+			else {
+				jsonObject.put(
+					entry.getKey(), dataRecordValuesMap.get(entry.getKey()));
+			}
+		}
+	}
+
+	private static void _addLocalizedValues(
+		JSONObject jsonObject, Map<String, Object> values) {
+
+		for (Map.Entry<String, Object> entry : values.entrySet()) {
+			jsonObject.put(
+				entry.getKey(),
+				GetterUtil.get(entry.getValue(), StringPool.BLANK));
+		}
+	}
+
+	private static Map<String, Object> _toDataRecordValuesMap(
+			DataRecordValue[] dataRecordValues)
+		throws Exception {
+
+		Map<String, Object> dataRecordValuesMap = new HashMap<>();
+
+		for (DataRecordValue dataRecordValue : dataRecordValues) {
+			dataRecordValuesMap.put(
+				dataRecordValue.getKey(), dataRecordValue.getValue());
+		}
+
+		return dataRecordValuesMap;
+	}
+
+}
---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/storage/DataStorage.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.storage;
+
+import com.liferay.data.engine.rest.dto.v1_0.DataRecord;
+import com.liferay.data.engine.rest.dto.v1_0.DataRecordCollection;
+import com.liferay.data.engine.rest.dto.v1_0.DataRecordValue;
+import com.liferay.data.engine.rest.internal.dto.v1_0.util.DataRecordValueUtil;
+import com.liferay.data.engine.rest.resource.v1_0.DataDefinitionResource;
+import com.liferay.data.engine.rest.resource.v1_0.DataRecordCollectionResource;
+import com.liferay.dynamic.data.mapping.model.DDMContent;
+import com.liferay.dynamic.data.mapping.service.DDMContentLocalService;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+/**
+ * @author Jeyvison Nascimento
+ */
+public class DataStorage {
+
+	public DataStorage(
+		DataRecordCollectionResource dataRecordCollectionResource,
+		DataDefinitionResource dataDefinitionResource,
+		DDMContentLocalService ddmContentLocalService) {
+
+		_dataRecordCollectionResource = dataRecordCollectionResource;
+		_dataDefinitionResource = dataDefinitionResource;
+		_ddmContentLocalService = ddmContentLocalService;
+	}
+
+	public long delete(long dataStorageId) throws Exception {
+		DDMContent ddmContent = _ddmContentLocalService.fetchDDMContent(
+			dataStorageId);
+
+		if (ddmContent != null) {
+			_ddmContentLocalService.deleteDDMContent(ddmContent);
+		}
+
+		return dataStorageId;
+	}
+
+	public DataRecordValue[] get(long dataDefinitionId, long dataStorageId)
+		throws Exception {
+
+		DDMContent ddmContent = _ddmContentLocalService.getContent(
+			dataStorageId);
+
+		return DataRecordValueUtil.toDataRecordValues(
+			_dataDefinitionResource.getDataDefinition(dataDefinitionId),
+			ddmContent.getData());
+	}
+
+	public long save(long groupId, DataRecord dataRecord) throws Exception {
+		DataRecordCollection dataRecordCollection =
+			_dataRecordCollectionResource.getDataRecordCollection(
+				dataRecord.getDataRecordCollectionId());
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setScopeGroupId(groupId);
+		serviceContext.setUserId(PrincipalThreadLocal.getUserId());
+
+		DDMContent ddmContent = _ddmContentLocalService.addContent(
+			PrincipalThreadLocal.getUserId(), groupId,
+			DataRecord.class.getName(), null,
+			DataRecordValueUtil.toJSON(
+				_dataDefinitionResource.getDataDefinition(
+					dataRecordCollection.getDataDefinitionId()),
+				dataRecord.getDataRecordValues()),
+			serviceContext);
+
+		return ddmContent.getPrimaryKey();
+	}
+
+	private final DataDefinitionResource _dataDefinitionResource;
+	private final DataRecordCollectionResource _dataRecordCollectionResource;
+	private final DDMContentLocalService _ddmContentLocalService;
+
+}
