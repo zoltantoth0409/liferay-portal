2b6a01c29b512c2cc1006d4a3bfceaf352549fff
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2019-03-18 10:10:47 -0700

    LPS-92256 Fetch instead of find and check for null. This is necessary for databases that did upgrade on 7.0 without the fix for upgradeSocial

--- portal-impl/src/com/liferay/portlet/social/service/impl/SocialActivitySetLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/social/service/impl/SocialActivitySetLocalServiceImpl.java
@@ -72,7 +72,11 @@ public class SocialActivitySetLocalServiceImpl
 		}
 
 		SocialActivitySet activitySet =
-			socialActivitySetPersistence.findByPrimaryKey(activitySetId);
+			socialActivitySetPersistence.fetchByPrimaryKey(activitySetId);
+
+		if (activitySet == null) {
+			return;
+		}
 
 		if (activitySet.getActivityCount() == 1) {
 			socialActivitySetPersistence.remove(activitySetId);
