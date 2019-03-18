172e25beeac920beee7aee37fe5b7a807ca1ffe3
Brian Chan <brian.chan@liferay.com>
2019-03-18 10:27:41 -0700

    LPS-91000 Typo

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/spacing/FloatingToolbarSpacingPanel.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/spacing/FloatingToolbarSpacingPanel.es.js
@@ -134,7 +134,7 @@ class FloatingToolbarSpacingPanel extends Component {
 				}
 			}
 
-			if (showConfirmation && !confirm(Liferay.Language.get('reducing-the-number-of-columns-will-lose-the-content-added-to-the-deleted-columns-are-you-sure-you-want-to-proced'))) {
+			if (showConfirmation && !confirm(Liferay.Language.get('reducing-the-number-of-columns-will-lose-the-content-added-to-the-deleted-columns-are-you-sure-you-want-to-proceed'))) {
 				event.preventDefault();
 				event.delegateTarget.querySelector(`option[value="${prevValue}"]`).selected = true;
 				return false;
