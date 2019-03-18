91718bef8d869b8b52f068d8011b708e72076cd1
David Arques <davidarchena@gmail.com>
2019-03-18 18:29:51 +0100

    LPS-92231 Create default experience only with published content pages

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/model/listener/LayoutModelListener.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/model/listener/LayoutModelListener.java
@@ -15,11 +15,13 @@
 package com.liferay.segments.internal.model.listener;
 
 import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.segments.exception.DefaultSegmentsExperienceException;
 import com.liferay.segments.service.SegmentsExperienceLocalService;
 
@@ -36,20 +38,25 @@ public class LayoutModelListener extends BaseModelListener<Layout> {
 
 	@Override
 	public void onAfterCreate(Layout layout) throws ModelListenerException {
-		if (!_isContentLayout(layout)) {
-			return;
-		}
+		try {
+			if (!_isPublishedContentLayout(layout)) {
+				return;
+			}
 
-		_addDefaultSegmentsExperience(layout);
+			_addDefaultSegmentsExperience(layout);
+		}
+		catch (PortalException pe) {
+			throw new ModelListenerException(pe);
+		}
 	}
 
 	@Override
 	public void onAfterRemove(Layout layout) throws ModelListenerException {
-		if (!_isContentLayout(layout)) {
-			return;
-		}
-
 		try {
+			if (!_isPublishedContentLayout(layout)) {
+				return;
+			}
+
 			_segmentsExperienceLocalService.deleteSegmentsExperiences(
 				layout.getGroupId(),
 				_classNameLocalService.getClassNameId(Layout.class),
@@ -62,11 +69,11 @@ public class LayoutModelListener extends BaseModelListener<Layout> {
 
 	@Override
 	public void onAfterUpdate(Layout layout) throws ModelListenerException {
-		if (!_isContentLayout(layout)) {
-			return;
-		}
-
 		try {
+			if (!_isPublishedContentLayout(layout)) {
+				return;
+			}
+
 			_segmentsExperienceLocalService.getDefaultSegmentsExperience(
 				layout.getGroupId(),
 				_classNameLocalService.getClassNameId(Layout.class),
@@ -92,16 +99,33 @@ public class LayoutModelListener extends BaseModelListener<Layout> {
 		}
 	}
 
-	private boolean _isContentLayout(Layout layout) {
-		if (Objects.equals(layout.getType(), LayoutConstants.TYPE_CONTENT)) {
+	private boolean _isPublishedContentLayout(Layout layout)
+		throws PortalException {
+
+		if (!Objects.equals(layout.getType(), LayoutConstants.TYPE_CONTENT)) {
+			return false;
+		}
+
+		if (layout.getClassNameId() != _classNameLocalService.getClassNameId(
+				Layout.class)) {
+
 			return true;
 		}
 
-		return false;
+		Layout draftLayout = _layoutLocalService.getLayout(layout.getClassPK());
+
+		if (draftLayout != null) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
 
 	@Reference
 	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
