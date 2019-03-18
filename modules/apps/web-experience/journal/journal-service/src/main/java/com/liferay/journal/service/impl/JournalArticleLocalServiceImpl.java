4c10c7405719987b03962816ddffa1cf28d36834
Michael Prigge <michael.prigge@liferay.com>
2019-03-18 15:57:51 -0700

    LPS-92084 partial backport of LPS-84119: Need SF changes before applying unique branch changes.
    Original commit: "LPS-84119 SF, remove unnecessary else statements" 48231f4a948a65786be8dd22622ed1f363bef5b9

--- modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/web-experience/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -3418,18 +3418,17 @@ public class JournalArticleLocalServiceImpl
 			if ((article == null) || articleId.equals(article.getArticleId())) {
 				break;
 			}
-			else {
-				String suffix = StringPool.DASH + i;
 
-				String prefix = urlTitle;
+			String suffix = StringPool.DASH + i;
 
-				if (urlTitle.length() > suffix.length()) {
-					prefix = urlTitle.substring(
-						0, urlTitle.length() - suffix.length());
-				}
+			String prefix = urlTitle;
 
-				urlTitle = prefix + suffix;
+			if (urlTitle.length() > suffix.length()) {
+				prefix = urlTitle.substring(
+					0, urlTitle.length() - suffix.length());
 			}
+
+			urlTitle = prefix + suffix;
 		}
 
 		return urlTitle;
