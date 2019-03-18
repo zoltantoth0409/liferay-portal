505df02f537b9d34c24126e11420a0cc5860a8a0
Preston-Crary <preston.crary@liferay.com>
2019-03-18 11:09:16 -0700

    LPS-90402 Remove from cache on destroy.

--- portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -363,6 +363,10 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 
 			_portletApps.remove(servletContextName);
 
+			ServletContext servletContext = portletApp.getServletContext();
+
+			_propertiesConfigurations.remove(servletContext.getClassLoader());
+
 			ExtraPortletAppConfigRegistry.unregisterExtraPortletAppConfig(
 				servletContextName);
 		}
