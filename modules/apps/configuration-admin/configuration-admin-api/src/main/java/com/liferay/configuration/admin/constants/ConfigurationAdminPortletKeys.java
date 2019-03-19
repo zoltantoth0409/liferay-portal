788a7a5105474ca659e84c511b031fdaf0080986
drewbrokke <drew.brokke@liferay.com>
2019-03-19 11:13:38 -0700

    LPS-90254 configuration-admin-api - renames new portlet to "Instance Settings"

--- modules/apps/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/constants/ConfigurationAdminPortletKeys.java
+++ modules/apps/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/constants/ConfigurationAdminPortletKeys.java
@@ -19,10 +19,10 @@ package com.liferay.configuration.admin.constants;
  */
 public class ConfigurationAdminPortletKeys {
 
+	public static final String INSTANCE_SETTINGS =
+		"com_liferay_configuration_admin_web_portlet_InstanceSettingsPortlet";
+
 	public static final String SYSTEM_SETTINGS =
 		"com_liferay_configuration_admin_web_portlet_SystemSettingsPortlet";
-
-	public static final String COMPANY_SETTINGS =
-		"com_liferay_configuration_admin_web_portlet_CompanySettingsPortlet";
 
 }
