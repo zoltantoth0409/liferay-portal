16f09750dcc8ab8ef59c292432bbe72abaca5218
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-03-19 16:36:56 +0100

    LPS-92320 Adding new exception classes

---
+++ modules/apps/change-tracking/change-tracking-api/src/main/java/com/liferay/change/tracking/exception/CTEntryCollisionException.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.exception;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class CTEntryCollisionException extends CTException {
+
+	public CTEntryCollisionException(long companyId, long ctEntryId) {
+		super(
+			companyId,
+			"Unable to publish change entry " + ctEntryId +
+				" because it is colliding with other changes");
+
+		_ctEntryId = ctEntryId;
+	}
+
+	public long getCtEntryId() {
+		return _ctEntryId;
+	}
+
+	private final long _ctEntryId;
+
+}
---
+++ modules/apps/change-tracking/change-tracking-api/src/main/java/com/liferay/change/tracking/exception/CTProcessException.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.exception;
+
+/**
+ * @author Daniel Kocsis
+ */
+public class CTProcessException extends CTException {
+
+	public CTProcessException(long companyId, long ctProcessId, String msg) {
+		super(companyId, msg);
+
+		_ctProcessId = ctProcessId;
+	}
+
+	public CTProcessException(
+		long companyId, long ctProcessId, String msg, Throwable cause) {
+
+		super(companyId, msg, cause);
+
+		_ctProcessId = ctProcessId;
+	}
+
+	public long getCtProcessId() {
+		return _ctProcessId;
+	}
+
+	private final long _ctProcessId;
+
+}
