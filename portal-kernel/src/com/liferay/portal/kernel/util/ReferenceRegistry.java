4ce2ad6716c09c95ccffacf27dee689439b9f5ff
Preston-Crary <preston.crary@liferay.com>
2019-03-19 15:04:54 -0700

    LPS-90402 Deprecate and empty out ReferenceRegistry to avoid leaks, we don't support portal-impl reloading.

--- portal-kernel/src/com/liferay/portal/kernel/util/ReferenceRegistry.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReferenceRegistry.java
@@ -14,101 +14,40 @@
 
 package com.liferay.portal.kernel.util;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-
 import java.lang.reflect.Field;
-
-import java.util.Collections;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of Mueller (7.2.x), with no direct replacement
  */
+@Deprecated
 public class ReferenceRegistry {
 
 	public static void registerReference(
 		Class<?> clazz, Object object, String fieldName) {
-
-		try {
-			ReferenceEntry referenceEntry = _pacl.getReferenceEntry(
-				clazz, object, fieldName);
-
-			_referenceEntries.add(referenceEntry);
-		}
-		catch (SecurityException se) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					StringBundler.concat(
-						"Not allowed to get field ", fieldName, " for ",
-						String.valueOf(clazz)));
-			}
-		}
-		catch (Exception e) {
-			_log.error(
-				StringBundler.concat(
-					"Unable to get field ", fieldName, " for ",
-					String.valueOf(clazz)));
-		}
 	}
 
 	public static void registerReference(Class<?> clazz, String fieldName) {
-		registerReference(clazz, null, fieldName);
 	}
 
 	public static void registerReference(Field field) {
-		ReferenceEntry referenceEntry = new ReferenceEntry(field);
-
-		_referenceEntries.add(referenceEntry);
 	}
 
 	public static void registerReference(Object object, Field field) {
-		ReferenceEntry referenceEntry = new ReferenceEntry(object, field);
-
-		_referenceEntries.add(referenceEntry);
 	}
 
 	public static void releaseReferences() {
-		for (ReferenceEntry referenceEntry : _referenceEntries) {
-			try {
-				referenceEntry.setValue(null);
-			}
-			catch (Exception e) {
-				_log.error(
-					"Failed to release reference for " + referenceEntry, e);
-			}
-		}
-
-		_referenceEntries.clear();
 	}
 
+	/**
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
+	 */
+	@Deprecated
 	public interface PACL {
 
 		public ReferenceEntry getReferenceEntry(
 				Class<?> clazz, Object object, String fieldName)
 			throws NoSuchFieldException, SecurityException;
-
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		ReferenceRegistry.class);
-
-	private static final PACL _pacl = new NoPACL();
-	private static final Set<ReferenceEntry> _referenceEntries =
-		Collections.newSetFromMap(new ConcurrentHashMap<>());
-
-	private static class NoPACL implements PACL {
-
-		@Override
-		public ReferenceEntry getReferenceEntry(
-				Class<?> clazz, Object object, String fieldName)
-			throws NoSuchFieldException, SecurityException {
-
-			Field field = clazz.getDeclaredField(fieldName);
-
-			return new ReferenceEntry(object, field);
-		}
 
 	}
 
