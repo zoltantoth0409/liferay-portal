fd1a9ebf4199c2245c69ee97d6043a192eae68e6
Pavel Savinov <pavel.savinov@liferay.com>
2019-03-19 12:27:37 +0100

    LPS-92312 Send redirect to the draft page edit mode when user tries to edit a published instance

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/ContentPageEditorPortlet.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/ContentPageEditorPortlet.java
@@ -18,8 +18,15 @@ import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortlet
 import com.liferay.layout.content.page.editor.web.internal.constants.ContentPageEditorWebKeys;
 import com.liferay.layout.content.page.editor.web.internal.display.context.ContentPageEditorDisplayContext;
 import com.liferay.layout.content.page.editor.web.internal.display.context.ContentPageEditorDisplayContextProvider;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
 
@@ -29,6 +36,7 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -81,12 +89,44 @@ public class ContentPageEditorPortlet extends MVCPortlet {
 				contentPageEditorDisplayContext);
 		}
 
-		super.doDispatch(renderRequest, renderResponse);
+		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Layout layout = themeDisplay.getLayout();
+
+		Layout draftLayout = _layoutLocalService.fetchLayout(
+			_portal.getClassNameId(Layout.class), layout.getPlid());
+
+		if (draftLayout != null) {
+			HttpServletResponse httpServletResponse =
+				_portal.getHttpServletResponse(renderResponse);
+
+			try {
+				String layoutFullURL = _portal.getLayoutFullURL(
+					draftLayout, themeDisplay);
+
+				httpServletResponse.sendRedirect(
+					_http.addParameter(
+						layoutFullURL, "p_l_mode", Constants.EDIT));
+			}
+			catch (PortalException pe) {
+				throw new PortletException(pe);
+			}
+		}
+		else {
+			super.doDispatch(renderRequest, renderResponse);
+		}
 	}
 
 	@Reference
 	private ContentPageEditorDisplayContextProvider
 		_contentPageEditorDisplayContextProvider;
+
+	@Reference
+	private Http _http;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
 
 	@Reference
 	private Portal _portal;
