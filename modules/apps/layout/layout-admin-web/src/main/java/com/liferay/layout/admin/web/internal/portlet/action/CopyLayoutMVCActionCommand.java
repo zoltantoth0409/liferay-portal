065ad057f803444c5673f72b54e2093b91eedfc3
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-03-19 15:36:52 +0100

    LPS-92312 Fix copy layout by using the layout copy helper (so as to not have duplicate code and always be up to date)

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/CopyLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/CopyLayoutMVCActionCommand.java
@@ -14,10 +14,8 @@
 
 package com.liferay.layout.admin.web.internal.portlet.action;
 
-import com.liferay.fragment.model.FragmentEntryLink;
-import com.liferay.fragment.processor.PortletRegistry;
-import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
+import com.liferay.layout.util.LayoutCopyHelper;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -25,39 +23,30 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.LayoutTypePortlet;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
-import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.LayoutService;
-import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.PropertiesParamUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.sites.kernel.util.SitesUtil;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletPreferences;
 import javax.portlet.PortletURL;
 
 import org.osgi.service.component.annotations.Component;
@@ -116,9 +105,6 @@ public class CopyLayoutMVCActionCommand extends BaseMVCActionCommand {
 			Layout copyLayout = _layoutLocalService.fetchLayout(
 				groupId, privateLayout, layoutId);
 
-			LayoutTypePortlet copyLayoutTypePortlet =
-				(LayoutTypePortlet)copyLayout.getLayoutType();
-
 			UnicodeProperties copyTypeSettingsProperties =
 				copyLayout.getTypeSettingsProperties();
 
@@ -131,42 +117,7 @@ public class CopyLayoutMVCActionCommand extends BaseMVCActionCommand {
 				copyTypeSettingsProperties.toString(), false, new HashMap<>(),
 				serviceContext);
 
-			LayoutTypePortlet layoutTypePortlet =
-				(LayoutTypePortlet)layout.getLayoutType();
-
-			layoutTypePortlet.setLayoutTemplateId(
-				themeDisplay.getUserId(),
-				copyLayoutTypePortlet.getLayoutTemplateId());
-
-			_layoutService.updateLayout(
-				groupId, privateLayout, layout.getLayoutId(),
-				layout.getTypeSettings());
-
-			com.liferay.portlet.sites.action.ActionUtil.copyPreferences(
-				actionRequest, layout, copyLayout);
-
-			SitesUtil.copyLookAndFeel(layout, copyLayout);
-
-			long liveGroupId = ParamUtil.getLong(actionRequest, "liveGroupId");
-			long stagingGroupId = ParamUtil.getLong(
-				actionRequest, "stagingGroupId");
-
-			_actionUtil.updateLookAndFeel(
-				actionRequest, themeDisplay.getCompanyId(), liveGroupId,
-				stagingGroupId, privateLayout, layout.getLayoutId(),
-				layout.getTypeSettingsProperties());
-
-			List<FragmentEntryLink> fragmentEntryLinks =
-				_fragmentEntryLinkLocalService.getFragmentEntryLinks(
-					copyLayout.getGroupId(),
-					_portal.getClassNameId(Layout.class), copyLayout.getPlid());
-
-			if (ListUtil.isNotEmpty(fragmentEntryLinks)) {
-				for (FragmentEntryLink fragmentEntryLink : fragmentEntryLinks) {
-					_copyFragmentEntryLink(
-						layout.getPlid(), fragmentEntryLink, serviceContext);
-				}
-			}
+			_layoutCopyHelper.copyLayout(copyLayout, layout);
 
 			LiferayPortletResponse liferayPortletResponse =
 				_portal.getLiferayPortletResponse(actionResponse);
@@ -200,59 +151,11 @@ public class CopyLayoutMVCActionCommand extends BaseMVCActionCommand {
 		}
 	}
 
-	private void _copyFragmentEntryLink(
-			long newPlid, FragmentEntryLink fragmentEntryLink,
-			ServiceContext serviceContext)
-		throws PortalException {
-
-		FragmentEntryLink newFragmentEntryLink =
-			_fragmentEntryLinkLocalService.addFragmentEntryLink(
-				serviceContext.getUserId(), serviceContext.getScopeGroupId(), 0,
-				fragmentEntryLink.getFragmentEntryId(),
-				_portal.getClassNameId(Layout.class), newPlid,
-				fragmentEntryLink.getCss(), fragmentEntryLink.getHtml(),
-				fragmentEntryLink.getJs(),
-				fragmentEntryLink.getEditableValues(),
-				fragmentEntryLink.getPosition(), serviceContext);
-
-		List<String> portletIds =
-			_portletRegistry.getFragmentEntryLinkPortletIds(fragmentEntryLink);
-
-		for (String portletId : portletIds) {
-			long defaultPlid = _portal.getControlPanelPlid(
-				serviceContext.getCompanyId());
-
-			PortletPreferences portletPreferences =
-				_portletPreferencesLocalService.fetchPreferences(
-					serviceContext.getCompanyId(),
-					PortletKeys.PREFS_OWNER_ID_DEFAULT,
-					PortletKeys.PREFS_OWNER_TYPE_LAYOUT, defaultPlid,
-					portletId);
-
-			if (portletPreferences == null) {
-				continue;
-			}
-
-			String newPortletId = StringUtil.replace(
-				portletId, fragmentEntryLink.getNamespace(),
-				newFragmentEntryLink.getNamespace());
-
-			_portletPreferencesLocalService.addPortletPreferences(
-				serviceContext.getCompanyId(),
-				PortletKeys.PREFS_OWNER_ID_DEFAULT,
-				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, defaultPlid, newPortletId,
-				null, PortletPreferencesFactoryUtil.toXML(portletPreferences));
-		}
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		CopyLayoutMVCActionCommand.class);
 
 	@Reference
-	private ActionUtil _actionUtil;
-
-	@Reference
-	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+	private LayoutCopyHelper _layoutCopyHelper;
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;
@@ -262,11 +165,5 @@ public class CopyLayoutMVCActionCommand extends BaseMVCActionCommand {
 
 	@Reference
 	private Portal _portal;
-
-	@Reference
-	private PortletPreferencesLocalService _portletPreferencesLocalService;
-
-	@Reference
-	private PortletRegistry _portletRegistry;
 
 }
