14bd4f1e017349966cf9c8919879ebcbb09d915a
Chema Balsas <jose.balsas@liferay.com>
2019-03-20 15:42:01 +0100

    LPS-90449 Avoids complete serialization of Layout model which might cause concurrent database access

--- modules/apps/hello-soy/hello-soy-web/src/main/java/com/liferay/hello/soy/web/internal/portlet/action/HelloSoyViewMVCRenderCommand.java
+++ modules/apps/hello-soy/hello-soy-web/src/main/java/com/liferay/hello/soy/web/internal/portlet/action/HelloSoyViewMVCRenderCommand.java
@@ -14,11 +14,18 @@
 
 package com.liferay.hello.soy.web.internal.portlet.action;
 
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 import com.liferay.portal.kernel.template.Template;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ReleaseInfo;
 import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
@@ -49,7 +56,22 @@ public class HelloSoyViewMVCRenderCommand implements MVCRenderCommand {
 		ThemeDisplay themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		template.put("layouts", themeDisplay.getLayouts());
+		List<Layout> layouts = themeDisplay.getLayouts();
+
+		Stream<Layout> layoutStream = layouts.stream();
+
+		List<Map> layoutMaps = layoutStream.map(
+			layout -> new HashMap<String, String>() {
+				{
+					put("friendlyURL", layout.getFriendlyURL());
+					put("nameCurrentValue", layout.getNameCurrentValue());
+				}
+			}
+		).collect(
+			Collectors.toList()
+		);
+
+		template.put("layouts", layoutMaps);
 
 		PortletURL navigationURL = renderResponse.createRenderURL();
 
