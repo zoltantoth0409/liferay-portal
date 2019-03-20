zoltantoth0409/liferay-portal

commit 2c34186546e14d3bb3b7d46230081634c89e16b9

LPS-91940 Print JVM OPTS in the output, that also it is being persisted in the log file

modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
@@ -171,12 +171,15 @@ public class UpgradeClient {
 		commands.add("-cp");
 		commands.add(_getBootstrapClassPath());
 
-		Collections.addAll(commands, _jvmOpts.split(" "));
+		String jvmOptsCommands = _jvmOpts.concat(
+			" -Dexternal-properties=portal-upgrade.properties " +
+				"-Dserver.detector.server.id=" +
+					_appServer.getServerDetectorServerId());
 
-		commands.add("-Dexternal-properties=portal-upgrade.properties");
-		commands.add(
-			"-Dserver.detector.server.id=" +
-				_appServer.getServerDetectorServerId());
+		System.out.println("JVM arguments: " + jvmOptsCommands);
+
+		Collections.addAll(commands, jvmOptsCommands.split(" "));
+
 		commands.add(DBUpgraderLauncher.class.getName());
 
 		processBuilder.command(commands);

commit 22a403774b93d1383145ef550b1e51608065f8cb

LPS-92259 Extra space causes a broken classpath after changes in 2c34186546e14d3bb3b7d46230081634c89e16b9. This was already fixed in 7.1 and master by 5fe630271408999c0cd304d8456e533f165b5dc1

modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
@@ -81,7 +81,7 @@ public class UpgradeClient {
 			else {
 				jvmOpts =
 					"-Dfile.encoding=UTF8 -Duser.country=US " +
-						"-Duser.language=en -Duser.timezone=GMT -Xmx2048m ";
+						"-Duser.language=en -Duser.timezone=GMT -Xmx2048m";
 			}
 
 			File logFile = null;