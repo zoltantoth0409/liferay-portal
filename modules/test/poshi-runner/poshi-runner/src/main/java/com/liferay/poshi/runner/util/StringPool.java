348fc1caa5aec470804332c8bf3f322b8216396c
Kenji Heigel <kenji.heigel@liferay.com>
2019-03-20 15:08:59 -0700

    LPS-84119 New SF: Avoid chaining

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/util/StringPool.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/util/StringPool.java
@@ -59,8 +59,7 @@ public class StringPool {
 
 	public static final String DASH = "-";
 
-	public static final String DEFAULT_CHARSET_NAME =
-		Charset.defaultCharset().name();
+	public static final String DEFAULT_CHARSET_NAME;
 
 	public static final String DOLLAR = "$";
 
@@ -204,6 +203,10 @@ public class StringPool {
 		for (int i = 0; i < 128; i++) {
 			ASCII_TABLE[i] = String.valueOf((char)i);
 		}
+
+		Charset charset = Charset.defaultCharset();
+
+		DEFAULT_CHARSET_NAME = charset.name();
 	}
 
 }
