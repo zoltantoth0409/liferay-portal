dd5e9bfbf42ec4294b2d9d515b1a1222de562a5b
Pavel Savinov <pavel.savinov@liferay.com>
2019-03-23 12:31:20 +0100

    LPS-91733 Include theme-specific CSS only in case when current scope group is actually using the theme

--- modules/apps/frontend-theme-fjord/frontend-theme-fjord-site-initializer/src/main/java/com/liferay/frontend/theme/fjord/site/initializer/internal/servlet/taglib/FjordTopHeadDynamicInclude.java
+++ modules/apps/frontend-theme-fjord/frontend-theme-fjord-site-initializer/src/main/java/com/liferay/frontend/theme/fjord/site/initializer/internal/servlet/taglib/FjordTopHeadDynamicInclude.java
@@ -16,7 +16,9 @@ package com.liferay.frontend.theme.fjord.site.initializer.internal.servlet.tagli
 
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.Theme;
 import com.liferay.portal.kernel.service.ThemeLocalService;
 import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
@@ -52,10 +54,14 @@ public class FjordTopHeadDynamicInclude implements DynamicInclude {
 		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		Theme theme = _themeLocalService.getTheme(
-			themeDisplay.getCompanyId(), _THEME_ID);
+		Group scopeGroup = themeDisplay.getScopeGroup();
 
-		if (!Objects.equals(theme.getThemeId(), _THEME_ID)) {
+		LayoutSet publicLayoutSet = scopeGroup.getPublicLayoutSet();
+		LayoutSet privateLayoutSet = scopeGroup.getPrivateLayoutSet();
+
+		if (!Objects.equals(publicLayoutSet.getThemeId(), _THEME_ID) &&
+			!Objects.equals(privateLayoutSet.getThemeId(), _THEME_ID)) {
+
 			return;
 		}
 
@@ -70,6 +76,9 @@ public class FjordTopHeadDynamicInclude implements DynamicInclude {
 
 			return;
 		}
+
+		Theme theme = _themeLocalService.getTheme(
+			themeDisplay.getCompanyId(), _THEME_ID);
 
 		PrintWriter printWriter = response.getWriter();
 
