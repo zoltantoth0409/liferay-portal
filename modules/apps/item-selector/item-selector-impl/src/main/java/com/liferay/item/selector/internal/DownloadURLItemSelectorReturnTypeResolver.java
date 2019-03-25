16b93af6374a5b7347eef69727c401a679dfd9e0
Pavel Savinov <pavel.savinov@liferay.com>
2019-03-25 12:17:06 +0100

    LPS-91550 Resolve download url item selector return type value as download url of the file entry

---
+++ modules/apps/item-selector/item-selector-impl/src/main/java/com/liferay/item/selector/internal/DownloadURLItemSelectorReturnTypeResolver.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.item.selector.internal;
+
+import com.liferay.document.library.util.DLURLHelper;
+import com.liferay.item.selector.ItemSelectorReturnTypeResolver;
+import com.liferay.item.selector.criteria.DownloadURLItemSelectorReturnType;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true, property = "service.ranking:Integer=100",
+	service = ItemSelectorReturnTypeResolver.class
+)
+public class DownloadURLItemSelectorReturnTypeResolver
+	implements ItemSelectorReturnTypeResolver
+		<DownloadURLItemSelectorReturnType, FileEntry> {
+
+	@Override
+	public Class<DownloadURLItemSelectorReturnType>
+		getItemSelectorReturnTypeClass() {
+
+		return DownloadURLItemSelectorReturnType.class;
+	}
+
+	@Override
+	public Class<FileEntry> getModelClass() {
+		return FileEntry.class;
+	}
+
+	@Override
+	public String getValue(FileEntry fileEntry, ThemeDisplay themeDisplay)
+		throws Exception {
+
+		return _dlurlHelper.getDownloadURL(
+			fileEntry, fileEntry.getFileVersion(), themeDisplay,
+			StringPool.BLANK);
+	}
+
+	@Reference
+	private DLURLHelper _dlurlHelper;
+
+}
