f015de2026f21a73c4d053d0bf4f8497bd5aa4b0
Pei-Jung Lan <pei-jung.lan@liferay.com>
2019-03-25 11:07:23 -0700

    LPS-90257 Set add-default-resource to true to allow the portlet to be accessible on any page

--- modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/portlet/PersonalMenuPortlet.java
+++ modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/portlet/PersonalMenuPortlet.java
@@ -27,6 +27,7 @@ import org.osgi.service.component.annotations.Component;
 @Component(
 	immediate = true,
 	property = {
+		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
