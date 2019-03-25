ec096eaec96905e7b3355fc9b9132c497aa9886e
Paulo Cruz <paulo.cruz@liferay.com>
2019-03-25 16:06:37 -0300

    LPS-89014 | LPS-92333 Create router wrappers to handle back navigation

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/router/routerWrapper.js
@@ -0,0 +1,52 @@
+import {Link, Redirect, withRouter} from 'react-router-dom';
+import queryString from 'query-string';
+import React from 'react';
+
+class BackLinkWrapper extends React.Component {
+	render() {
+		return (
+			<Link
+				children={this.props.children}
+				className={this.props.className}
+				to={getBackPathFromProps(this.props)}
+			/>
+		);
+	}
+}
+
+class BackRedirectWrapper extends React.Component {
+	render() {
+		return <Redirect to={getBackPathFromProps(this.props)} />;
+	}
+}
+
+class ChildLinkWrapper extends React.Component {
+	render() {
+		const backPath = this.props.location.pathname + this.props.location.search;
+
+		return (
+			<Link
+				children={this.props.children}
+				className={this.props.className}
+				to={{
+					pathname: this.props.to,
+					search: queryString.stringify({backPath})
+				}}
+			/>
+		);
+	}
+}
+
+function getBackPathFromProps(props) {
+	const {
+		location: {search}
+	} = props;
+
+	return queryString.parse(search).backPath;
+}
+
+const BackLink = withRouter(BackLinkWrapper);
+const BackRedirect = withRouter(BackRedirectWrapper);
+const ChildLink = withRouter(ChildLinkWrapper);
+
+export {BackLink, BackRedirect, ChildLink};
