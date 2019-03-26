57c4ad1a25172c94c625928284b4da0b9a5857b1
Marcellus Tavares <marcellus.tavares@liferay.com>
2019-03-26 15:07:59 -0300

    LPS-92741 Not needed anymore

--- modules/apps/analytics/analytics-client-js/src/utils/hash.js
+++ modules/apps/analytics/analytics-client-js/src/utils/hash.js
@@ -1,11 +1,8 @@
 import objectHash from 'object-hash';
 
-const ignoredProps = ['screenWidth', 'screenHeight', 'devicePixelRatio'];
-
 const hash = value =>
 	objectHash(value, {
 		algorithm: 'md5',
-		excludeKeys: key => ignoredProps.indexOf(key) !== -1,
 		unorderedObjects: true,
 	});
 
