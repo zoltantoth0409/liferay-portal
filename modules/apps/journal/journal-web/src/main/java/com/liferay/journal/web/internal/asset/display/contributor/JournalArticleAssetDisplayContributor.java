f19b8bc5b92eda0a9f3f0f7561401a70e3fa1f26
Pavel Savinov <pavel.savinov@liferay.com>
2019-03-26 09:33:01 +0100

    LPS-92697 Support rendering of web content templates as asset display fields

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/asset/display/contributor/JournalArticleAssetDisplayContributor.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/asset/display/contributor/JournalArticleAssetDisplayContributor.java
@@ -15,14 +15,20 @@
 package com.liferay.journal.web.internal.asset.display.contributor;
 
 import com.liferay.asset.display.contributor.AssetDisplayContributor;
+import com.liferay.asset.display.contributor.AssetDisplayField;
 import com.liferay.asset.display.contributor.BaseAssetDisplayContributor;
+import com.liferay.asset.display.contributor.util.ContentAccessor;
 import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.document.library.util.DLURLHelper;
 import com.liferay.dynamic.data.mapping.kernel.DDMFormFieldValue;
 import com.liferay.dynamic.data.mapping.kernel.DDMFormValues;
 import com.liferay.dynamic.data.mapping.kernel.Value;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.dynamic.data.mapping.util.FieldsToDDMFormValuesConverter;
 import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.util.JournalContent;
 import com.liferay.journal.util.JournalConverter;
 import com.liferay.journal.web.asset.JournalArticleDDMFormValuesReader;
 import com.liferay.petra.string.StringPool;
@@ -34,7 +40,10 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.sanitizer.Sanitizer;
 import com.liferay.portal.kernel.sanitizer.SanitizerUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.HashMap;
@@ -42,6 +51,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -61,6 +72,36 @@ public class JournalArticleAssetDisplayContributor
 	@Override
 	public String getClassName() {
 		return JournalArticle.class.getName();
+	}
+
+	@Override
+	public List<AssetDisplayField> getClassTypeFields(
+			long classTypeId, Locale locale)
+		throws PortalException {
+
+		List<AssetDisplayField> classTypesFields = super.getClassTypeFields(
+			classTypeId, locale);
+
+		DDMStructure ddmStructure = _ddmStructureLocalService.fetchDDMStructure(
+			classTypeId);
+
+		List<DDMTemplate> ddmTemplates = ddmStructure.getTemplates();
+
+		Stream<DDMTemplate> stream = ddmTemplates.stream();
+
+		List<AssetDisplayField> assetDisplayFields = stream.map(
+			ddmTemplate -> new AssetDisplayField(
+				_DDM_TEMPLATE + ddmTemplate.getTemplateKey(),
+				ddmTemplate.getName(locale) + StringPool.SPACE +
+					StringPool.STAR,
+				"text")
+		).collect(
+			Collectors.toList()
+		);
+
+		classTypesFields.addAll(assetDisplayFields);
+
+		return classTypesFields;
 	}
 
 	@Override
@@ -109,6 +150,17 @@ public class JournalArticleAssetDisplayContributor
 
 				classTypeValues.put(entry.getKey(), fieldValue);
 			}
+
+			DDMStructure ddmStructure = article.getDDMStructure();
+
+			List<DDMTemplate> ddmTemplates = ddmStructure.getTemplates();
+
+			ddmTemplates.forEach(
+				ddmTemplate -> classTypeValues.put(
+					_DDM_TEMPLATE + ddmTemplate.getTemplateKey(),
+					new DDMTemplateContentAccessor(
+						article, ddmTemplate,
+						LocaleUtil.toLanguageId(locale))));
 		}
 		catch (PortalException pe) {
 			if (_log.isDebugEnabled()) {
@@ -145,8 +197,13 @@ public class JournalArticleAssetDisplayContributor
 		return StringPool.BLANK;
 	}
 
+	private static final String _DDM_TEMPLATE = "ddmTemplate_";
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		JournalArticleAssetDisplayContributor.class);
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
 
 	@Reference
 	private DLAppService _dlAppService;
@@ -158,6 +215,33 @@ public class JournalArticleAssetDisplayContributor
 	private FieldsToDDMFormValuesConverter _fieldsToDDMFormValuesConverter;
 
 	@Reference
+	private JournalContent _journalContent;
+
+	@Reference
 	private JournalConverter _journalConverter;
+
+	private class DDMTemplateContentAccessor implements ContentAccessor {
+
+		public DDMTemplateContentAccessor(
+			JournalArticle article, DDMTemplate ddmTemplate,
+			String languageId) {
+
+			_article = article;
+			_ddmTemplate = ddmTemplate;
+			_languageId = languageId;
+		}
+
+		public String getContent() {
+			return _journalContent.getContent(
+				_article.getGroupId(), _article.getArticleId(),
+				_ddmTemplate.getTemplateKey(), Constants.VIEW, _languageId,
+				(ThemeDisplay)null);
+		}
+
+		private final JournalArticle _article;
+		private final DDMTemplate _ddmTemplate;
+		private final String _languageId;
+
+	}
 
 }
