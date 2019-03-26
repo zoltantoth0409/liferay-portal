8b4968ecbe34150782e8d5ffcef5cf9df1c45603
Pablo Molina <contact@pablomolina.me>
2019-03-26 18:28:49 +0100

    LPS-91728 Add FragmentsEditor focusIn and mouseOver listeners

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/FragmentsEditor.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/FragmentsEditor.es.js
@@ -1,4 +1,5 @@
 import Component from 'metal-component';
+import dom from 'metal-dom';
 import Soy from 'metal-soy';
 import {Config} from 'metal-state';
 
@@ -7,6 +8,7 @@ import './components/mapping_type/SelectMappingTypeDialog.es';
 import './components/fragment_entry_link/FragmentEntryLinkList.es';
 import './components/sidebar/FragmentsEditorSidebar.es';
 import './components/toolbar/FragmentsEditorToolbar.es';
+import {CLEAR_ACTIVE_ITEM, CLEAR_HOVERED_ITEM, UPDATE_ACTIVE_ITEM, UPDATE_HOVERED_ITEM} from './actions/actions.es';
 import {INITIAL_STATE} from './store/state.es';
 import {Store} from './store/store.es';
 import templates from './FragmentsEditor.soy';
@@ -16,6 +18,121 @@ import templates from './FragmentsEditor.soy';
  * @review
  */
 class FragmentsEditor extends Component {
+
+	/**
+	 * @param {MouseEvent} event
+	 * @return {{fragmentsEditorItemId: string|null, fragmentsEditorItemType: string|null}}
+	 * @private
+	 * @review
+	 */
+	static _getItemTarget(event) {
+		let {fragmentsEditorItemId = null, fragmentsEditorItemType = null} = event.target.dataset || {};
+
+		if (!fragmentsEditorItemId || !fragmentsEditorItemType) {
+			const parent = dom.closest(event.target, '[data-fragments-editor-item-id]');
+
+			if (parent) {
+				fragmentsEditorItemId = parent.dataset.fragmentsEditorItemId;
+				fragmentsEditorItemType = parent.dataset.fragmentsEditorItemType;
+			}
+		}
+
+		return {
+			fragmentsEditorItemId,
+			fragmentsEditorItemType
+		};
+	}
+
+	/**
+	 * @inheritdoc
+	 * @review
+	 */
+	created() {
+		this._handleDocumentClick = this._handleDocumentClick.bind(this);
+		this._handleDocumentFocus = this._handleDocumentFocus.bind(this);
+		this._handleDocumentMouseOver = this._handleDocumentMouseOver.bind(this);
+
+		document.addEventListener('click', this._handleDocumentClick, true);
+		document.addEventListener('focusin', this._handleDocumentFocus);
+		document.addEventListener('mouseover', this._handleDocumentMouseOver);
+	}
+
+	/**
+	 * @inheritdoc
+	 * @review
+	 */
+	disposed() {
+		document.removeEventListener('click', this._handleDocumentClick);
+		document.removeEventListener('focusin', this._handleDocumentFocus);
+		document.removeEventListener('mouseover', this._handleDocumentMouseOver);
+	}
+
+	/**
+	 * @param {MouseEvent} event
+	 * @private
+	 * @review
+	 */
+	_handleDocumentClick(event) {
+		this._updateActiveItem(event);
+	}
+
+	/**
+	 * @param {FocusEvent} event
+	 * @private
+	 * @review
+	 */
+	_handleDocumentFocus(event) {
+		this._updateActiveItem(event);
+	}
+
+	/**
+	 * @param {MouseEvent} event
+	 * @private
+	 * @review
+	 */
+	_handleDocumentMouseOver(event) {
+		const {fragmentsEditorItemId, fragmentsEditorItemType} = FragmentsEditor._getItemTarget(event);
+
+		if (fragmentsEditorItemId && fragmentsEditorItemType) {
+			this.store.dispatchAction(
+				UPDATE_HOVERED_ITEM,
+				{
+					hoveredItemId: fragmentsEditorItemId,
+					hoveredItemType: fragmentsEditorItemType
+				}
+			);
+		}
+		else {
+			this.store.dispatchAction(
+				CLEAR_HOVERED_ITEM
+			);
+		}
+	}
+
+	/**
+	 * @param {Event} event
+	 * @private
+	 * @review
+	 */
+	_updateActiveItem(event) {
+		const {fragmentsEditorItemId, fragmentsEditorItemType} = FragmentsEditor._getItemTarget(event);
+
+		if (fragmentsEditorItemId && fragmentsEditorItemType) {
+			this.store.dispatchAction(
+				UPDATE_ACTIVE_ITEM,
+				{
+					activeItemId: fragmentsEditorItemId,
+					activeItemType: fragmentsEditorItemType
+				}
+			);
+		}
+		else {
+			this.store.dispatchAction(
+				CLEAR_ACTIVE_ITEM
+			);
+		}
+	}
+
 }
 
 /**
@@ -26,6 +143,7 @@ class FragmentsEditor extends Component {
  */
 FragmentsEditor.STATE = Object.assign(
 	{
+
 		/**
 		 * Store instance
 		 * @default undefined
@@ -34,7 +152,8 @@ FragmentsEditor.STATE = Object.assign(
 		 * @review
 		 * @type {Store}
 		 */
-		store: Config.instanceOf(Store),
+		store: Config.instanceOf(Store)
+
 	},
 	INITIAL_STATE
 );
