8dda5a784752d627599a8508dfd04261f9394845
Greg Hurrell <greg.hurrell@liferay.com>
2019-03-26 17:10:29 +0100

    LPS-92609 Fix remaining errors in dynamic-data-mapping-form-web tests
    Seems we need a custom processor to make jest transform these ES6 source
    files that we are grabbing from a sibling directory (which in itself is
    probably not a great idea).
    
    Fixes two errors of the sort:
    
        SyntaxError: Unexpected token export
    
    when we try to import ES6 files like
    'dynamic-data-mapping-form-builder/metal/js/util/dom.es'.
    
    With this, however, all the tests are green.

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/jest-preprocessor.js
@@ -0,0 +1,7 @@
+const babelJest = require('babel-jest');
+const fs = require('fs');
+
+const configString = fs.readFileSync('./.babelrc', 'utf8').toString();
+const config = JSON.parse(configString);
+
+module.exports = babelJest.createTransformer(config)
