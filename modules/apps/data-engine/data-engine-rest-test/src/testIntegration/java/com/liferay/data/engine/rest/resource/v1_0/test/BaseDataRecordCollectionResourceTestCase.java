d2ea73c582a58c37fec244879fd1fe9395055cb6
Brian Chan <brian.chan@liferay.com>
2019-03-26 11:19:15 -0700

    LPS-91816 regen with latest rest builder

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v1_0/test/BaseDataRecordCollectionResourceTestCase.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v1_0/test/BaseDataRecordCollectionResourceTestCase.java
@@ -1039,7 +1039,7 @@ public abstract class BaseDataRecordCollectionResourceTestCase {
 
 		for (int i = 0; i < values.length; i++) {
 			template = template.replaceFirst(
-				"\\{.*\\}", String.valueOf(values[i]));
+				"\\{.*?\\}", String.valueOf(values[i]));
 		}
 
 		return template;
