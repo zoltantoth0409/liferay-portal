f6e9346f90e6d63ab2fa260c2af67b71f9f7be9a
Chema Balsas <jose.balsas@liferay.com>
2019-03-27 13:15:38 +0100

    LPS-92609 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/jest-preprocessor.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/jest-preprocessor.js
@@ -4,4 +4,4 @@ const fs = require('fs');
 const configString = fs.readFileSync('./.babelrc', 'utf8').toString();
 const config = JSON.parse(configString);
 
-module.exports = babelJest.createTransformer(config)
+module.exports = babelJest.createTransformer(config);
