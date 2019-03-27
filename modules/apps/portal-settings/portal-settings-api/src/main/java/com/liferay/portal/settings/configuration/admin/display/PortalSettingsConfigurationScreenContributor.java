993139fb85f47a5bd6f81f17eb610925dfdd93dc
drewbrokke <drew.brokke@liferay.com>
2019-03-27 16:45:30 -0700

    LPS-90255 portal-settings-api - adds PortalSettingsConfigurationScreenContributor interface to unify adding ConfigurationScreens to the new Instance Settings

---
+++ modules/apps/portal-settings/portal-settings-api/src/main/java/com/liferay/portal/settings/configuration/admin/display/PortalSettingsConfigurationScreenContributor.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.settings.configuration.admin.display;
+
+import java.util.Locale;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Drew Brokke
+ */
+public interface PortalSettingsConfigurationScreenContributor {
+
+	public String getCategoryKey();
+
+	public default String getDeleteMVCActionCommandName() {
+		return null;
+	}
+
+	public String getJspPath();
+
+	public String getKey();
+
+	public String getName(Locale locale);
+
+	public String getSaveMVCActionCommandName();
+
+	public ServletContext getServletContext();
+
+	public default String getTestButtonLabel(Locale locale) {
+		return null;
+	}
+
+	public default String getTestButtonOnClick(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		return null;
+	}
+
+	public default void setAttributes(
+		HttpServletRequest request, HttpServletResponse response) {
+	}
+
+}
