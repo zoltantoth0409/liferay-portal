e829d0b38af303b07dc67829d715f10c4df6eceb
Preston-Crary <preston.crary@liferay.com>
2019-03-27 15:22:39 -0700

    LPS-92677 Build Services.

--- modules/apps/change-tracking/change-tracking-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/change-tracking/change-tracking-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,11 +1,9 @@
 create unique index IX_A0CFE092 on CTCollection (companyId, name[$COLUMN_LENGTH:75$]);
 
 create index IX_15C20AD4 on CTCollection_CTEntryAggregate (companyId);
-create index IX_ACB81B32 on CTCollection_CTEntryAggregate (ctCollectionId);
 create index IX_90D19701 on CTCollection_CTEntryAggregate (ctEntryAggregateId);
 
 create index IX_AFC3725E on CTCollections_CTEntries (companyId);
-create index IX_D9FBD9E8 on CTCollections_CTEntries (ctCollectionId);
 create index IX_6EA8BE62 on CTCollections_CTEntries (ctEntryId);
 
 create unique index IX_88A1512E on CTEntry (modelClassNameId, modelClassPK);
@@ -14,7 +12,6 @@ create index IX_49B471E1 on CTEntryAggregate (ownerCTEntryId);
 
 create index IX_5C08EBAD on CTEntryAggregates_CTEntries (companyId);
 create index IX_4E879A48 on CTEntryAggregates_CTEntries (ctEntryAggregateId);
-create index IX_1AEE37B1 on CTEntryAggregates_CTEntries (ctEntryId);
 
 create index IX_7523B0A4 on CTProcess (companyId);
 create index IX_B4859762 on CTProcess (ctCollectionId);
