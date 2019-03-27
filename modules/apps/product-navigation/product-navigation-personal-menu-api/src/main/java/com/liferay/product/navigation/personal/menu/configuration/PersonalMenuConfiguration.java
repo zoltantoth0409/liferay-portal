10da0c22eb21b87ebfd3369c76f60bfd3f13c96e
Samuel Tran <samuel.tran@liferay.com>
2019-03-26 17:01:51 -0700

    LPS-92519 product-navigation - Add configuration for showing the personal menu in the control menu

---
+++ modules/apps/product-navigation/product-navigation-personal-menu-api/src/main/java/com/liferay/product/navigation/personal/menu/configuration/PersonalMenuConfiguration.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.product.navigation.personal.menu.configuration;
+
+import aQute.bnd.annotation.ProviderType;
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Samuel Trong Tran
+ */
+@ExtendedObjectClassDefinition(
+	category = "users", scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.product.navigation.personal.menu.configuration.PersonalMenuConfiguration",
+	localization = "content/Language", name = "personal-menu-configuration-name"
+)
+@ProviderType
+public interface PersonalMenuConfiguration {
+
+	@Meta.AD(
+		deflt = "false",
+		description = "show-the-personal-menu-in-the-control-menu-help",
+		name = "show-the-personal-menu-in-the-control-menu", required = false
+	)
+	public boolean showThePersonalMenuInTheControlMenu();
+
+}
