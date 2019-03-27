e2b758a58e2b4d86d9790616d43012b64e9015d3
Alan Huang <alan.huang@liferay.com>
2019-03-27 11:12:42 +0800

    LPS-84119 Add specific ordering

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLDefinitionOrderCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLDefinitionOrderCheck.java
@@ -22,6 +22,8 @@ import com.liferay.source.formatter.checks.util.YMLSourceUtil;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Hugo Huijser
@@ -42,7 +44,7 @@ public class YMLDefinitionOrderCheck extends BaseFileCheck {
 		List<String> definitions = YMLSourceUtil.getDefinitions(
 			content, indent);
 
-		if (definitions.size() == 1 && !content.contains("\n")) {
+		if ((definitions.size() == 1) && !content.contains("\n")) {
 			return content;
 		}
 
@@ -53,8 +55,33 @@ public class YMLDefinitionOrderCheck extends BaseFileCheck {
 			new Comparator<String>() {
 
 				@Override
-				public int compare(String arg0, String arg1) {
-					return arg0.split("\n")[0].compareTo(arg1.split("\n")[0]);
+				public int compare(String definition1, String definition2) {
+					String[] definition1Lines = StringUtil.splitLines(
+						definition1);
+					String[] definition2Lines = StringUtil.splitLines(
+						definition2);
+
+					if (definition1Lines[0].equals("- ") &&
+						definition2Lines[0].equals("- ")) {
+
+						String trimmedDefinitin1Line =
+							definition1Lines[1].trim();
+						String trimmedDefinitin2Line =
+							definition2Lines[1].trim();
+
+						if (trimmedDefinitin1Line.equals("in: query") &&
+							trimmedDefinitin1Line.equals(
+								trimmedDefinitin2Line)) {
+
+							return _sortSpecificDefinitions(
+								definition1, definition2, "name");
+						}
+
+						return definition1Lines[1].compareTo(
+							definition2Lines[1]);
+					}
+
+					return definition1Lines[0].compareTo(definition2Lines[0]);
 				}
 
 			});
@@ -94,6 +121,31 @@ public class YMLDefinitionOrderCheck extends BaseFileCheck {
 		}
 
 		return content;
+	}
+
+	private int _sortSpecificDefinitions(
+		String definition1, String definition2, String key) {
+
+		Pattern pattern = Pattern.compile(
+			"^ *" + key + ": *(\\w*)(\n|\\Z)", Pattern.MULTILINE);
+
+		String value1 = StringPool.BLANK;
+
+		Matcher matcher = pattern.matcher(definition1);
+
+		if (matcher.find()) {
+			value1 = matcher.group(1);
+		}
+
+		String value2 = StringPool.BLANK;
+
+		matcher = pattern.matcher(definition2);
+
+		if (matcher.find()) {
+			value2 = matcher.group(1);
+		}
+
+		return value1.compareTo(value2);
 	}
 
 }
