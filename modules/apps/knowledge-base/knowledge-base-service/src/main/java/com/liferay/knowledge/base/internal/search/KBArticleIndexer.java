03e2e0b5f771924047c30887f4cdde5bccb80e22
Brian Chan <brian.chan@liferay.com>
2019-03-27 09:07:16 -0700

    LPS-92384 SF

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/search/KBArticleIndexer.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/search/KBArticleIndexer.java
@@ -132,9 +132,8 @@ public class KBArticleIndexer extends BaseIndexer<KBArticle> {
 		document.addText(
 			Field.CONTENT, HtmlUtil.extractText(kbArticle.getContent()));
 		document.addText(Field.DESCRIPTION, kbArticle.getDescription());
-		document.addText(Field.TITLE, kbArticle.getTitle());
-
 		document.addKeyword(Field.FOLDER_ID, kbArticle.getKbFolderId());
+		document.addText(Field.TITLE, kbArticle.getTitle());
 		document.addKeyword("folderNames", getKBFolderNames(kbArticle));
 		document.addKeyword(
 			"parentMessageId", kbArticle.getParentResourcePrimKey());
