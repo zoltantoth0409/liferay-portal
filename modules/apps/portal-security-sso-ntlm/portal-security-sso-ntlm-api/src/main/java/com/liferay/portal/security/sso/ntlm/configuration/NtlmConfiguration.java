457221d838bf3a0eca98ef3d6f005717adac2d19
drewbrokke <drew.brokke@liferay.com>
2019-03-27 12:55:03 -0700

    LPS-90255 portal-security-sso-ntlm-api - makes NtlmConfiguration system-scoped so that its entry does not show up in Instance Settings

--- modules/apps/portal-security-sso-ntlm/portal-security-sso-ntlm-api/src/main/java/com/liferay/portal/security/sso/ntlm/configuration/NtlmConfiguration.java
+++ modules/apps/portal-security-sso-ntlm/portal-security-sso-ntlm-api/src/main/java/com/liferay/portal/security/sso/ntlm/configuration/NtlmConfiguration.java
@@ -29,9 +29,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
  *
  * @author Michael C. Han
  */
-@ExtendedObjectClassDefinition(
-	category = "sso", scope = ExtendedObjectClassDefinition.Scope.COMPANY
-)
+@ExtendedObjectClassDefinition(category = "sso")
 @Meta.OCD(
 	id = "com.liferay.portal.security.sso.ntlm.configuration.NtlmConfiguration",
 	localization = "content/Language", name = "ntlm-configuration-name"
