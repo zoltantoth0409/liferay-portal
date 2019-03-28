179740ce4dc535c33926f2c7209ba73b3e45e1fc
Victor Galan <victor.galan@liferay.com>
2019-03-28 18:36:57 +0100

    LPS-87529 Create content list API using the AssetEntryToDTOConverter to avoid duplicating code
    Co-authored-by: ruben-pulido <ruben.pulido@liferay.com>

--- modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/v1_0/ContentListElementResourceImpl.java
+++ modules/apps/headless/headless-web-experience/headless-web-experience-impl/src/main/java/com/liferay/headless/web/experience/internal/resource/v1_0/ContentListElementResourceImpl.java
@@ -14,9 +14,26 @@
 
 package com.liferay.headless.web.experience.internal.resource.v1_0;
 
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.asset.list.service.AssetListEntryService;
+import com.liferay.headless.common.spi.osgi.AssetEntryToDTOConverter;
+import com.liferay.headless.web.experience.dto.v1_0.ContentListElement;
+import com.liferay.headless.web.experience.internal.registry.AssetEntryToDTOConverterRegistry;
 import com.liferay.headless.web.experience.resource.v1_0.ContentListElementResource;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.segments.model.SegmentsEntry;
+import com.liferay.segments.service.SegmentsEntryLocalService;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.core.Context;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,4 +45,63 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class ContentListElementResourceImpl
 	extends BaseContentListElementResourceImpl {
+
+	@Override
+	public Page<ContentListElement> getContentListContentListElementsPage(
+			Long contentListId, Pagination pagination)
+		throws Exception {
+
+		AssetListEntry assetListEntry =
+			_assetListEntryService.fetchAssetListEntry(contentListId);
+
+		SegmentsEntry segmentsEntry =
+			_segmentsEntryLocalService.getDefaultSegmentsEntry(
+				assetListEntry.getGroupId());
+
+		long segmentsEntryId = segmentsEntry.getSegmentsEntryId();
+
+		return Page.of(
+			transform(
+				assetListEntry.getAssetEntries(
+					segmentsEntryId, pagination.getStartPosition(),
+					pagination.getEndPosition()),
+				this::_toAssetListElement),
+			pagination, assetListEntry.getAssetEntriesCount(segmentsEntryId));
+	}
+
+	private ContentListElement _toAssetListElement(AssetEntry assetEntry) {
+		String className = assetEntry.getClassName();
+
+		Optional<AssetEntryToDTOConverter> assetEntryToDTOConverterOptional =
+			_assetEntryToDTOConverterRegistry.getAssetEntryToDTOConverter(
+				className);
+
+		return new ContentListElement() {
+			{
+				contentType = className;
+				content = assetEntryToDTOConverterOptional.map(
+					assetEntryToDTOConverter -> assetEntryToDTOConverter.toDTO(
+						assetEntry, contextAcceptLanguage, contextUriInfo)
+				).orElse(
+					null
+				);
+				order = assetEntry.getPriority();
+				title = assetEntry.getTitle(
+					contextAcceptLanguage.getPreferredLocale());
+			}
+		};
+	}
+
+	@Reference
+	private AssetEntryToDTOConverterRegistry _assetEntryToDTOConverterRegistry;
+
+	@Reference
+	private AssetListEntryService _assetListEntryService;
+
+	@Context
+	private HttpServletRequest _contextHttpServletRequest;
+
+	@Reference
+	private SegmentsEntryLocalService _segmentsEntryLocalService;
+
 }
