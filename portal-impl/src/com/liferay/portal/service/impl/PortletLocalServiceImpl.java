760e7bbc21f68ec08c6b4c48335848704f77b0de
Preston-Crary <preston.crary@liferay.com>
2019-03-28 10:15:21 -0700

    LPS-90402 Avoid ConfigurationNotFoundException.

--- portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -2734,6 +2734,10 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 
 		ClassLoader classLoader = servletContext.getClassLoader();
 
+		if (classLoader.getResource(propertyFileName + ".properties") == null) {
+			return null;
+		}
+
 		return ConfigurationFactoryUtil.getConfiguration(
 			classLoader, propertyFileName);
 	}
