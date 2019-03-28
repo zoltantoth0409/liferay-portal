08cc2dd2d85636a197c61d539e8f1005d2d5670d
Kenji Heigel <kenji.heigel@liferay.com>
2019-03-28 16:12:53 -0700

    LRCI-93 Add singleton for using kubernetes API client

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/kubernetes/LiferayKubernetesApi.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser.kubernetes;
+
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.AppsV1Api;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.util.Config;
+
+import java.io.IOException;
+
+/**
+ * @author Kenji Heigel
+ */
+public class LiferayKubernetesApi {
+
+	public static AppsV1Api apps;
+	public static CoreV1Api core;
+
+	public static LiferayKubernetesApi getInstance() {
+		if (_liferayKubernetesApi == null) {
+			_liferayKubernetesApi = new LiferayKubernetesApi();
+		}
+
+		return _liferayKubernetesApi;
+	}
+
+	private static final ApiClient _apiClient;
+	private static LiferayKubernetesApi _liferayKubernetesApi;
+
+	static {
+		try {
+			_apiClient = Config.defaultClient();
+
+			Configuration.setDefaultApiClient(_apiClient);
+
+			_apiClient.setDebugging(true);
+
+			apps = new AppsV1Api(_apiClient);
+			core = new CoreV1Api(_apiClient);
+		}
+		catch (IOException ioe) {
+			throw new ExceptionInInitializerError(ioe);
+		}
+	}
+
+}
