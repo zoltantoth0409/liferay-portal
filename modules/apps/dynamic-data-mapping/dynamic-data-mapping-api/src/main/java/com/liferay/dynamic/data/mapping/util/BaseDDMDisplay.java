a4ab2d10bbd4a94f6d6032782902b5d1d2b08789
Vendel Toreki <vendel.toreki@liferay.com>
2019-03-29 21:43:46 +0100

    LPS-92039 Show Add button for ADTs in staging and live, when the site is staged but the ADTs aren't

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/BaseDDMDisplay.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/BaseDDMDisplay.java
@@ -407,7 +407,9 @@ public abstract class BaseDDMDisplay implements DDMDisplay {
 			return false;
 		}
 
-		if (!scopeGroup.hasLocalOrRemoteStagingGroup()) {
+		if (!scopeGroup.hasLocalOrRemoteStagingGroup() ||
+			!scopeGroup.isStagedPortlet(portletId)) {
+
 			return true;
 		}
 
