2ba72a6b498d00ff3d09b5ac234ce60ef75bd263
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-03-29 11:41:40 -0300

    LPS-89015 | LPS-92498 Add nodeResource implementation

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/resource/v1_0/NodeResourceImpl.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/resource/v1_0/NodeResourceImpl.java
@@ -14,9 +14,25 @@
 
 package com.liferay.portal.workflow.metrics.rest.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.engine.adapter.search.SearchRequestExecutor;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.hits.SearchHit;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.workflow.metrics.rest.dto.v1_0.Node;
 import com.liferay.portal.workflow.metrics.rest.resource.v1_0.NodeResource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +43,59 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = NodeResource.class
 )
 public class NodeResourceImpl extends BaseNodeResourceImpl {
+
+	@Override
+	public Page<Node> getProcessNodesPage(Long processId) throws Exception {
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
+
+		searchSearchRequest.setIndexNames("workflow-metrics-nodes");
+		searchSearchRequest.setQuery(
+			new BooleanQueryImpl() {
+				{
+					setPreBooleanFilter(
+						new BooleanFilter() {
+							{
+								addRequiredTerm(
+									"companyId", contextCompany.getCompanyId());
+								addRequiredTerm("deleted", false);
+								addRequiredTerm("processId", processId);
+							}
+						});
+				}
+			});
+
+		SearchSearchResponse searchSearchResponse =
+			_searchRequestExecutor.executeSearchRequest(searchSearchRequest);
+
+		SearchHits searchHits = searchSearchResponse.getSearchHits();
+
+		return Page.of(
+			Stream.of(
+				searchHits.getSearchHits()
+			).flatMap(
+				List::stream
+			).map(
+				SearchHit::getDocument
+			).map(
+				this::_toNode
+			).collect(
+				Collectors.toList()
+			));
+	}
+
+	private Node _toNode(Document document) {
+		return new Node() {
+			{
+				id = document.getLong("nodeId");
+				initial = GetterUtil.getBoolean(document.getValue("initial"));
+				name = document.getString("name");
+				terminal = GetterUtil.getBoolean(document.getValue("terminal"));
+				type = document.getString("type");
+			}
+		};
+	}
+
+	@Reference
+	private SearchRequestExecutor _searchRequestExecutor;
+
 }
