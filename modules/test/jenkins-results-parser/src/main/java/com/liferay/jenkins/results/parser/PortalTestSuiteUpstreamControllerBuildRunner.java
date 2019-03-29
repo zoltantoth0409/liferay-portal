731427d6ae3891ac8896d158feb338e0f5630700
Michael Hashimoto <michael.hashimoto@liferay.com>
2019-03-29 14:01:16 -0700

    LRCI-108 Only keep builds that have a new SHA

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PortalTestSuiteUpstreamControllerBuildRunner.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PortalTestSuiteUpstreamControllerBuildRunner.java
@@ -14,15 +14,39 @@
 
 package com.liferay.jenkins.results.parser;
 
+import java.io.IOException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * @author Michael Hashimoto
  */
 public class PortalTestSuiteUpstreamControllerBuildRunner
-	extends BaseBuildRunner<BuildData, Workspace> {
+	<S extends PortalTopLevelBuildData>
+		extends BaseBuildRunner<S, Workspace> {
 
-	protected PortalTestSuiteUpstreamControllerBuildRunner(
-		BuildData buildData) {
+	@Override
+	public void run() {
+		S buildData = getBuildData();
 
+		String previousPortalBranchSHA = _getPreviousPortalBranchSHA();
+
+		if (!previousPortalBranchSHA.equals(buildData.getPortalBranchSHA())) {
+			updateBuildDescription();
+
+			keepLogs(true);
+		}
+	}
+
+	@Override
+	public void tearDown() {
+	}
+
+	protected PortalTestSuiteUpstreamControllerBuildRunner(S buildData) {
 		super(buildData);
 	}
 
@@ -30,5 +54,56 @@ public class PortalTestSuiteUpstreamControllerBuildRunner
 	protected void initWorkspace() {
 		setWorkspace(WorkspaceFactory.newSimpleWorkspace());
 	}
+
+	@Override
+	protected void updateBuildDescription() {
+		S buildData = getBuildData();
+
+		buildData.setBuildDescription(
+			JenkinsResultsParserUtil.combine(
+				"<strong>GIT ID</strong> - ", buildData.getPortalBranchSHA()));
+
+		super.updateBuildDescription();
+	}
+
+	private String _getPreviousPortalBranchSHA() {
+		BuildData buildData = getBuildData();
+
+		try {
+			JSONObject jsonObject = JenkinsResultsParserUtil.toJSONObject(
+				JenkinsResultsParserUtil.getLocalURL(buildData.getJobURL()) +
+					"api/json");
+
+			JSONArray buildsJSONArray = jsonObject.getJSONArray("builds");
+
+			for (int i = 0; i < buildsJSONArray.length(); i++) {
+				JSONObject buildJSONObject = buildsJSONArray.getJSONObject(i);
+
+				JSONObject previousBuildJSONObject =
+					JenkinsResultsParserUtil.toJSONObject(
+						JenkinsResultsParserUtil.getLocalURL(
+							buildJSONObject.getString("url") + "api/json"));
+
+				String description = previousBuildJSONObject.optString(
+					"description", "");
+
+				Matcher matcher = _pattern.matcher(description);
+
+				if (!matcher.find()) {
+					continue;
+				}
+
+				return matcher.group("sha");
+			}
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(ioe);
+		}
+
+		return "";
+	}
+
+	private static final Pattern _pattern = Pattern.compile(
+		"<strong>GIT ID<\\/strong> - (?<sha>[0-9a-f]{7,40})");
 
 }
