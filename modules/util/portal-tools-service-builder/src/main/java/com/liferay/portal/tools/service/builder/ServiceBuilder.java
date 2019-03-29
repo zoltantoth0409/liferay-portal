c397a0cf8539f66c528193e16f54e89e315648a1
shuyangzhou <shuyang.zhou@liferay.com>
2019-03-28 17:02:28 -0700

    LPS-92677 Break on first mismatch

--- modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -2111,6 +2111,8 @@ public class ServiceBuilder {
 				for (int i = 0; i < columnNames.length; i++) {
 					if (!columnNames[i].equals(pkEntityColumnDBNames.get(i))) {
 						redundant = false;
+
+						break;
 					}
 				}
 
