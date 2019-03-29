8815e8a4c304f3bfbe4b5bad725b3d8bc221f838
Ivica Cardic <ivica.cardic@liferay.com>
2019-03-29 06:26:39 +0100

    COMMERCE-1217 - portal-vulcan-impl - Add XmlMapperContextResolver

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/context/resolver/XmlMapperContextResolver.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.jaxrs.context.resolver;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author Ivica Cardic
+ */
+@Provider
+public class XmlMapperContextResolver implements ContextResolver<XmlMapper> {
+
+	@Override
+	public XmlMapper getContext(Class<?> clazz) {
+		XmlMapper xmlMapper = new XmlMapper();
+
+		xmlMapper.configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+		xmlMapper.setDateFormat(new ISO8601DateFormat());
+		xmlMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+		return xmlMapper;
+	}
+
+}
