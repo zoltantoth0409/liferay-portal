20cca7360883445e9f45f93102714ef184163511
Inácio Nery <inacio.nery@liferay.com>
2019-03-29 16:02:15 -0300

    LPS-92245 IndexExecutor must be run in serial to avoid concurrent update issues

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/executor/WorkflowMetricsPortalExecutorConfig.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.internal.executor;
+
+import com.liferay.petra.concurrent.ThreadPoolHandlerAdapter;
+import com.liferay.petra.executor.PortalExecutorConfig;
+import com.liferay.petra.lang.CentralizedThreadLocal;
+import com.liferay.portal.kernel.util.NamedThreadFactory;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
+import com.liferay.portal.workflow.metrics.internal.search.index.IndexExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Inácio Nery
+ */
+@Component(immediate = true, service = PortalExecutorConfig.class)
+public class WorkflowMetricsPortalExecutorConfig extends PortalExecutorConfig {
+
+	public WorkflowMetricsPortalExecutorConfig() {
+		super(
+			IndexExecutor.class.getName(), 1, 1, 60, TimeUnit.SECONDS,
+			Integer.MAX_VALUE,
+			new NamedThreadFactory(
+				IndexExecutor.class.getName(), Thread.NORM_PRIORITY,
+				PortalClassLoaderUtil.getClassLoader()),
+			new ThreadPoolExecutor.AbortPolicy(),
+			new ThreadPoolHandlerAdapter() {
+
+				@Override
+				public void afterExecute(
+					Runnable runnable, Throwable throwable) {
+
+					CentralizedThreadLocal.clearShortLivedThreadLocals();
+				}
+
+			});
+	}
+
+}
