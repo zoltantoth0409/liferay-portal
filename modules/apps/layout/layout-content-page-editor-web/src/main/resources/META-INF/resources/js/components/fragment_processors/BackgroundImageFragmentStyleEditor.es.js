6a544e19bcb2d0eca97650f9c3c1f26d00c776fe
Chema Balsas <jose.balsas@liferay.com>
2019-03-29 12:13:14 +0100

    LPS-92401 Supports DownloadURL return type

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_processors/BackgroundImageFragmentStyleEditor.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_processors/BackgroundImageFragmentStyleEditor.es.js
@@ -2,6 +2,7 @@
  * Possible types that can be returned by the image selector
  */
 const RETURN_TYPES = {
+	downloadURL: 'com.liferay.item.selector.criteria.DownloadURLItemSelectorReturnType',
 	fileEntryItemSelector: 'com.liferay.item.selector.criteria.FileEntryItemSelectorReturnType',
 	url: 'URL'
 };
@@ -25,7 +26,7 @@ function _handleImageEditorChange(
 		const {returnType} = selectedItem;
 		let url = '';
 
-		if (returnType === RETURN_TYPES.url) {
+		if (returnType === RETURN_TYPES.url || RETURN_TYPES.downloadURL) {
 			url = selectedItem.value;
 		}
 		else if (returnType === RETURN_TYPES.fileEntryItemSelector) {
