8dea57e98fa764c983ffd7b88d32df94865424d4
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2019-03-29 18:48:35 -0300

    LPS-93069 Create method to handle a new field set into Form builder and update the fieldNames

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/metal/js/components/LayoutProvider/handlers/fieldSetAddedHandler.es.js
@@ -0,0 +1,48 @@
+import {generateFieldName} from '../util/fields.es';
+import {PagesVisitor} from '../../../util/visitors.es';
+
+const handleFieldSetAdded = (props, state, event) => {
+	const {fieldSetPage, target} = event;
+	const {pages} = state;
+	const {pageIndex, rowIndex} = target;
+
+	const visitor = new PagesVisitor(fieldSetPage);
+
+	const newFieldsetPages = visitor.mapFields(
+		field => {
+			const name = generateFieldName(pages, field.fieldName);
+
+			const settingsContextVisitor = new PagesVisitor(field.settingsContext.pages);
+
+			return {
+				...field,
+				fieldName: name,
+				settingsContext: {
+					...field.settingsContext,
+					pages: settingsContextVisitor.mapFields(
+						settingsContextField => {
+							if (settingsContextField.fieldName === 'name') {
+								settingsContextField = {
+									...settingsContextField,
+									value: name
+								};
+							}
+
+							return settingsContextField;
+						}
+					)
+				}
+			};
+		}
+	);
+
+	const rows = newFieldsetPages[0].rows;
+
+	for (let i = rows.length - 1; i >= 0; i--) {
+		pages[pageIndex].rows.splice(rowIndex, 0, rows[i]);
+	}
+
+	return {pages};
+};
+
+export default handleFieldSetAdded;
