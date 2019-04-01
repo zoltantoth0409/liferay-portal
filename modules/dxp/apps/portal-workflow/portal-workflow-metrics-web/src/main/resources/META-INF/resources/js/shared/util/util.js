2ed8fed1bc7bd46d1a2e0aabb0012b7fa5d298ee
Francisco Dias <francisco.dias@liferay.com>
2019-04-01 13:50:49 -0300

    LPS-88968 | LPS-92421 Create util functions

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/util/util.js
@@ -0,0 +1,22 @@
+/**
+ * Returns the percent number passing as
+ * parameter the current number and total number.
+ * @param {number} number1
+ * @param {number} number2
+ * @returns {number}
+ */
+const getPercentage = (number1, number2) => {
+	const result = (number1 / number2) * 100;
+
+	return isValidNumber(result) ? result : 0;
+};
+
+/**
+ * Return true if number is valid
+ * @param {number} number
+ */
+const isValidNumber = number => {
+	return !isNaN(number) && number !== Infinity ? true : false;
+};
+
+export { getPercentage, isValidNumber };
