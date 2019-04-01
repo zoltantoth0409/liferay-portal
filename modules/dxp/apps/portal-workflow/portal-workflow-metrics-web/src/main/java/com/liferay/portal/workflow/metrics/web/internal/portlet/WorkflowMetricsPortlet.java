38b48ebeaa0989e103ed7ef81d10d90df8aa1b82
Inácio Nery <inacio.nery@liferay.com>
2019-04-01 12:55:40 -0300

    LPS-93054 Rename to Metrics

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/java/com/liferay/portal/workflow/metrics/web/internal/portlet/WorkflowMetricsPortlet.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/java/com/liferay/portal/workflow/metrics/web/internal/portlet/WorkflowMetricsPortlet.java
@@ -37,7 +37,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
 		"com.liferay.portlet.use-default-template=true",
-		"javax.portlet.display-name=Reports",
+		"javax.portlet.display-name=Metrics",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/META-INF/resources/",
 		"javax.portlet.init-param.view-template=/view.jsp",
