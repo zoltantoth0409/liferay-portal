893a0c9dba78b57e4e85faa9668e2fe14fc44c0e
tina.tian <tina.tian@liferay.com>
2019-04-01 10:19:49 -0700

    LPS-92088 SF, rename package to make it consistent with IndexerRegistryImpl

--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/internal/test/IndexerRegistryImplTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/internal/test/IndexerRegistryImplTest.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.search.indexer.test;
+package com.liferay.portal.search.internal.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.search.BaseIndexer;
