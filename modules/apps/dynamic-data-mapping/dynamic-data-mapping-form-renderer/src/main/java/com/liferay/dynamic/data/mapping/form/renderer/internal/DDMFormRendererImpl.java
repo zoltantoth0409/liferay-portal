e7f2e2e653f55c72a2671ca62258a87928eee4a0
Leonardo Barros <leonardo.barros@liferay.com>
2019-04-01 11:22:25 -0300

    LPS-93027 Use SoyRendererComponent API to render the Form

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormRendererImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormRendererImpl.java
@@ -14,8 +14,8 @@
 
 package com.liferay.dynamic.data.mapping.form.renderer.internal;
 
-import com.liferay.dynamic.data.mapping.form.field.type.BaseDDMFormFieldRenderer;
-import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldRenderer;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeServicesTracker;
 import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderer;
 import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderingContext;
 import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderingException;
@@ -24,47 +24,39 @@ import com.liferay.dynamic.data.mapping.form.renderer.internal.servlet.taglib.DD
 import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormLayout;
 import com.liferay.dynamic.data.mapping.util.DDM;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
 import com.liferay.portal.kernel.json.JSONFactory;
-import com.liferay.portal.kernel.json.JSONSerializer;
 import com.liferay.portal.kernel.servlet.taglib.DynamicIncludeUtil;
 import com.liferay.portal.kernel.template.Template;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.template.TemplateException;
-import com.liferay.portal.kernel.template.TemplateManagerUtil;
-import com.liferay.portal.kernel.template.TemplateResource;
-import com.liferay.portal.kernel.template.URLTemplateResource;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.template.soy.SoyTemplateResourceFactory;
-import com.liferay.portal.template.soy.util.SoyRawData;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.template.soy.renderer.ComponentDescriptor;
+import com.liferay.portal.template.soy.renderer.SoyComponentRenderer;
 
 import java.io.Writer;
 
-import java.net.URL;
-
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.CopyOnWriteArrayList;
 
-import org.osgi.service.component.annotations.Activate;
+import javax.servlet.http.HttpServletRequest;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Marcellus Tavares
  */
-@Component(
-	immediate = true, property = "templatePath=/META-INF/resources/form.soy",
-	service = DDMFormRenderer.class
-)
+@Component(immediate = true, service = DDMFormRenderer.class)
 public class DDMFormRendererImpl implements DDMFormRenderer {
 
 	@Override
@@ -79,8 +71,8 @@ public class DDMFormRendererImpl implements DDMFormRenderer {
 		catch (DDMFormRenderingException ddmfre) {
 			throw ddmfre;
 		}
-		catch (PortalException pe) {
-			throw new DDMFormRenderingException(pe);
+		catch (Exception e) {
+			throw new DDMFormRenderingException(e);
 		}
 	}
 
@@ -97,58 +89,39 @@ public class DDMFormRendererImpl implements DDMFormRenderer {
 		catch (DDMFormRenderingException ddmfre) {
 			throw ddmfre;
 		}
-		catch (PortalException pe) {
-			throw new DDMFormRenderingException(pe);
-		}
-	}
-
-	@Activate
-	protected void activate(Map<String, Object> properties) {
-		String templatePath = MapUtil.getString(properties, "templatePath");
-
-		TemplateResource formTemplateResource = getFormTemplateResource(
-			templatePath);
-
-		_templateResources.add(formTemplateResource);
-	}
-
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void addDDMFormFieldRenderer(
-		DDMFormFieldRenderer ddmFormFieldRenderer) {
-
-		TemplateResource templateResource = getTemplateResource(
-			ddmFormFieldRenderer);
-
-		if (templateResource != null) {
-			_templateResources.add(templateResource);
+		catch (Exception e) {
+			throw new DDMFormRenderingException(e);
 		}
 	}
 
 	protected String doRender(
 			DDMForm ddmForm, DDMFormLayout ddmFormLayout,
 			DDMFormRenderingContext ddmFormRenderingContext)
-		throws PortalException {
+		throws Exception {
 
-		Template template = TemplateManagerUtil.getTemplate(
-			TemplateConstants.LANG_TYPE_SOY,
-			_soyTemplateResourceFactory.createSoyTemplateResource(
-				_templateResources),
-			false);
+		Set<String> dependencies = new HashSet<>();
 
-		populateCommonContext(
-			template, ddmForm, ddmFormLayout, ddmFormRenderingContext);
+		List<DDMFormFieldType> ddmFormFieldTypes =
+			_ddmFormFieldTypeServicesTracker.getDDMFormFieldTypes();
 
-		SoyRawData soyRawData = (SoyRawData)template.get("templateNamespace");
+		for (DDMFormFieldType ddmFormFieldType : ddmFormFieldTypes) {
+			String moduleName = ddmFormFieldType.getModuleName();
 
-		String templateNamespace = (String)soyRawData.getValue();
+			if (Validator.isNotNull(moduleName)) {
+				dependencies.add(_npmResolver.resolveModuleName(moduleName));
+			}
+		}
 
-		String html = render(template, templateNamespace);
+		ComponentDescriptor componentDescriptor = new ComponentDescriptor(
+			_TEMPLATE_NAMESPACE, _npmResolver.resolveModuleName(_MODULE_NAME),
+			ddmFormRenderingContext.getContainerId(), dependencies);
 
-		String javaScript = render(template, "ddm.form_renderer_js");
+		Writer writer = new UnsyncStringWriter();
+
+		_soyComponentRenderer.renderSoyComponent(
+			ddmFormRenderingContext.getHttpServletRequest(), writer,
+			componentDescriptor,
+			getContext(ddmForm, ddmFormLayout, ddmFormRenderingContext));
 
 		DynamicIncludeUtil.include(
 			ddmFormRenderingContext.getHttpServletRequest(),
@@ -160,7 +133,36 @@ public class DDMFormRendererImpl implements DDMFormRenderer {
 			ddmFormRenderingContext.getHttpServletResponse(),
 			DDMFormRenderer.class.getName() + "#formRendered", true);
 
-		return html.concat(javaScript);
+		return writer.toString();
+	}
+
+	protected Map<String, Object> getContext(
+			DDMForm ddmForm, DDMFormLayout ddmFormLayout,
+			DDMFormRenderingContext ddmFormRenderingContext)
+		throws PortalException {
+
+		Map<String, Object> ddmFormTemplateContext =
+			_ddmFormTemplateContextFactory.create(
+				ddmForm, ddmFormLayout, ddmFormRenderingContext);
+
+		ddmFormTemplateContext.put("editable", false);
+
+		ddmFormTemplateContext.remove("fieldTypes");
+
+		HttpServletRequest httpServletRequest =
+			ddmFormRenderingContext.getHttpServletRequest();
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		String pathThemeImages = themeDisplay.getPathThemeImages();
+
+		String spriteMap = pathThemeImages.concat("/clay/icons.svg");
+
+		ddmFormTemplateContext.put("spritemap", spriteMap);
+
+		return ddmFormTemplateContext;
 	}
 
 	protected String getDefaultLanguageId(
@@ -180,86 +182,6 @@ public class DDMFormRendererImpl implements DDMFormRenderer {
 		return LocaleUtil.toLanguageId(ddmForm.getDefaultLocale());
 	}
 
-	protected TemplateResource getFormTemplateResource(String templatePath) {
-		Class<?> clazz = getClass();
-
-		ClassLoader classLoader = clazz.getClassLoader();
-
-		URL templateURL = classLoader.getResource(templatePath);
-
-		return new URLTemplateResource(templateURL.getPath(), templateURL);
-	}
-
-	protected TemplateResource getTemplateResource(
-		DDMFormFieldRenderer ddmFormFieldRenderer) {
-
-		if (ddmFormFieldRenderer instanceof BaseDDMFormFieldRenderer) {
-			BaseDDMFormFieldRenderer baseDDMFormFieldRenderer =
-				(BaseDDMFormFieldRenderer)ddmFormFieldRenderer;
-
-			return baseDDMFormFieldRenderer.getTemplateResource();
-		}
-
-		return null;
-	}
-
-	protected TemplateResource getTemplateResource(String templatePath) {
-		Class<?> clazz = getClass();
-
-		ClassLoader classLoader = clazz.getClassLoader();
-
-		URL templateURL = classLoader.getResource(templatePath);
-
-		return new URLTemplateResource(templateURL.getPath(), templateURL);
-	}
-
-	protected void populateCommonContext(
-			Template template, DDMForm ddmForm, DDMFormLayout ddmFormLayout,
-			DDMFormRenderingContext ddmFormRenderingContext)
-		throws PortalException {
-
-		Map<String, Object> ddmFormTemplateContext =
-			_ddmFormTemplateContextFactory.create(
-				ddmForm, ddmFormLayout, ddmFormRenderingContext);
-
-		for (Map.Entry<String, Object> entry :
-				ddmFormTemplateContext.entrySet()) {
-
-			SoyRawData soyRawData = new SoyRawData() {
-
-				@Override
-				public Object getValue() {
-					return entry.getValue();
-				}
-
-			};
-
-			template.put(entry.getKey(), soyRawData);
-		}
-
-		ddmFormTemplateContext.remove("fieldTypes");
-
-		JSONSerializer jsonSerializer = _jsonFactory.createJSONSerializer();
-
-		template.put(
-			"context", jsonSerializer.serializeDeep(ddmFormTemplateContext));
-
-		template.put(
-			"defaultLanguageId",
-			getDefaultLanguageId(ddmForm, ddmFormRenderingContext));
-	}
-
-	protected void removeDDMFormFieldRenderer(
-		DDMFormFieldRenderer ddmFormFieldRenderer) {
-
-		TemplateResource templateResource = getTemplateResource(
-			ddmFormFieldRenderer);
-
-		if (templateResource != null) {
-			_templateResources.remove(templateResource);
-		}
-	}
-
 	protected String render(Template template, String namespace)
 		throws TemplateException {
 
@@ -273,8 +195,17 @@ public class DDMFormRendererImpl implements DDMFormRenderer {
 		return writer.toString();
 	}
 
+	private static final String _MODULE_NAME =
+		"dynamic-data-mapping-form-builder/metal/js/components/Form" +
+			"/FormRenderer.es";
+
+	private static final String _TEMPLATE_NAMESPACE = "FormRenderer.render";
+
 	@Reference
 	private DDM _ddm;
+
+	@Reference
+	private DDMFormFieldTypeServicesTracker _ddmFormFieldTypeServicesTracker;
 
 	@Reference
 	private DDMFormTemplateContextFactory _ddmFormTemplateContextFactory;
@@ -283,12 +214,12 @@ public class DDMFormRendererImpl implements DDMFormRenderer {
 	private JSONFactory _jsonFactory;
 
 	@Reference
+	private NPMResolver _npmResolver;
+
+	@Reference
 	private Portal _portal;
 
 	@Reference
-	private SoyTemplateResourceFactory _soyTemplateResourceFactory;
-
-	private final List<TemplateResource> _templateResources =
-		new CopyOnWriteArrayList<>();
+	private SoyComponentRenderer _soyComponentRenderer;
 
 }
