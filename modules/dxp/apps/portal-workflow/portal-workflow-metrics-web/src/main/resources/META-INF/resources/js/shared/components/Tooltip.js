6610fb5530c7528d3b20ffa21c22d9c0e107db9b
Francisco Dias <francisco.dias@liferay.com>
2019-04-01 13:48:47 -0300

    LPS-88968 | LPS-92421 New Tooltip component

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/Tooltip.js
@@ -0,0 +1,77 @@
+import autobind from 'autobind-decorator';
+import getCN from 'classnames';
+import React from 'react';
+
+const CLASSNAME = 'workflow-tooltip';
+
+/**
+ * Tooltip
+ * @class
+ * @memberof TooltipBase component
+ */
+
+export default class Tooltip extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			displayTooltip: false
+		};
+	}
+	@autobind
+	hideTooltip() {
+		this.setState({ displayTooltip: false });
+	}
+
+	@autobind
+	showTooltip() {
+		this.setState({ displayTooltip: true });
+	}
+
+	render() {
+		const { message, position = 'top', width } = this.props;
+		const { displayTooltip } = this.state;
+
+		return (
+			<span className={CLASSNAME} onMouseLeave={this.hideTooltip}>
+				{displayTooltip && (
+					<TooltipBase
+						position={position}
+						title={message}
+						visible={displayTooltip}
+						width={width}
+					/>
+				)}
+				<span className="tooltip-trigger" onMouseOver={this.showTooltip}>
+					{this.props.children}
+				</span>
+			</span>
+		);
+	}
+}
+
+/**
+ * Tooltip Base
+ * @class
+ */
+export class TooltipBase extends React.Component {
+	render() {
+		const { position, title, visible, width } = this.props;
+		const classes = getCN('tooltip', {
+			[`clay-tooltip-${position}`]: position,
+			[`workflow-tooltip-${position}`]: position,
+			['show']: visible
+		});
+
+		const classesArrow = getCN('arrow', 'workflow-tooltip-arrow');
+
+		return (
+			<div className={classes} style={{ width: `${width}px` }}>
+				{position !== 'none' && <div className={classesArrow} />}
+				<div className="tooltip-inner">
+					<div>{title}</div>
+				</div>
+			</div>
+		);
+	}
+}
---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/__tests__/Tooltip.js
@@ -0,0 +1,84 @@
+import Tooltip, {TooltipBase} from '../Tooltip';
+import React from 'react';
+
+describe('Tooltip', () => {
+	let component;
+
+	const props = {
+		message: 'Tooltip Message',
+		position: 'top',
+		width: '200'
+	};
+
+	afterEach(() => {
+		if(component) {
+			component.unmount();
+		}
+	});
+
+	it('Should render component without tooltip markup', () => {
+		component = shallow(<Tooltip {...props}>{'Target'}</Tooltip>);
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('Should render component with tooltip markup', () => {
+		component = shallow(<Tooltip {...props}>{'Target'}</Tooltip>);
+		const instance = component.instance();
+
+		instance.showTooltip();
+		expect(component).toMatchSnapshot();
+	});
+
+		
+	it('Should render component with tooltip after mouse over', () => {
+		component = shallow(<Tooltip {...props} position={'right'}>{'Target'}</Tooltip>);
+		component.find('.tooltip-trigger').simulate('mouseover');
+
+		expect(component).toMatchSnapshot();
+	});
+	
+	it('Should render component without tooltip after mouse leave', () => {
+		component = shallow(<Tooltip {...props} position={'right'}>{'Target'}</Tooltip>);
+		component.find('.tooltip-trigger').simulate('mouseover');
+		component.find('.workflow-tooltip').simulate('mouseleave');
+
+		expect(component).toMatchSnapshot();
+	});
+
+	it('Should render component with tooltip with top position', () => {
+		component = shallow(<Tooltip {...props}>{'Target'}</Tooltip>);
+		const instance = component.instance();
+
+		instance.showTooltip();
+
+		expect(component.find(TooltipBase).render()).toMatchSnapshot();
+	});
+
+	it('Should render component with tooltip with bottom position', () => {
+		jest.useFakeTimers();
+		
+		component = shallow(<Tooltip {...props} position={'bottom'}>{'Target'}</Tooltip>);
+		const instance = component.instance();
+
+		instance.showTooltip();
+		expect(component.find(TooltipBase).render()).toMatchSnapshot();
+	});
+
+	it('Should render component with tooltip with right position', () => {
+		component = shallow(<Tooltip {...props} position={'right'}>{'Target'}</Tooltip>);
+		const instance = component.instance();
+
+		instance.showTooltip();
+		expect(component.find(TooltipBase).render()).toMatchSnapshot();
+	});
+
+	it('Should render component with tooltip with left position', () => {
+		component = shallow(<Tooltip {...props} position={'right'}>{'Target'}</Tooltip>);
+		const instance = component.instance();
+
+		instance.showTooltip();
+		expect(component.find(TooltipBase).render()).toMatchSnapshot();
+	});
+
+});
