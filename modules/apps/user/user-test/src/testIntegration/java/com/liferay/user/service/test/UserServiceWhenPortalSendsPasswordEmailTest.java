f864899b7ce848a85c695ba80694a84369b15c93
hai.yu <8219754@liferay.com>
2019-04-02 11:17:39 +0800

    LPS-93516 Move test files to module and use the current module's classloader to get the test files.

--- modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/service/test/UserServiceWhenPortalSendsPasswordEmailTest.java
+++ modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/service/test/UserServiceWhenPortalSendsPasswordEmailTest.java
@@ -19,21 +19,27 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.UserServiceUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.Localization;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.test.mail.MailServiceTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.SynchronousMailTestRule;
 import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.portal.util.PropsUtil;
 
 import javax.portlet.PortletPreferences;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -53,8 +59,49 @@ public class UserServiceWhenPortalSendsPasswordEmailTest {
 		new AggregateTestRule(
 			new LiferayIntegrationTestRule(), SynchronousMailTestRule.INSTANCE);
 
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		PropsUtil.set(
+			PropsKeys.ADMIN_EMAIL_PASSWORD_RESET_SUBJECT,
+			"com/liferay/user/service/test/dependencies" +
+				"/email_password_reset_subject.tmpl");
+		PropsUtil.set(
+			PropsKeys.ADMIN_EMAIL_PASSWORD_RESET_BODY,
+			"com/liferay/user/service/test/dependencies" +
+				"/email_password_reset_body.tmpl");
+		PropsUtil.set(
+			PropsKeys.ADMIN_EMAIL_PASSWORD_SENT_SUBJECT,
+			"com/liferay/user/service/test/dependencies" +
+				"/email_password_sent_subject.tmpl");
+		PropsUtil.set(
+			PropsKeys.ADMIN_EMAIL_PASSWORD_SENT_BODY,
+			"com/liferay/user/service/test/dependencies" +
+				"/email_password_sent_body.tmpl");
+	}
+
 	@Before
 	public void setUp() throws Exception {
+		_localization = LocalizationUtil.getLocalization();
+
+		ReflectionTestUtil.setFieldValue(
+			LocalizationUtil.class, "_localization",
+			ProxyUtil.newProxyInstance(
+				Localization.class.getClassLoader(),
+				new Class<?>[] {Localization.class},
+				(proxy, method, args) -> {
+					if ("getLocalizationMap".equals(method.getName()) &&
+						(args.length == 3)) {
+
+						return _localization.getLocalizationMap(
+							(PortletPreferences)args[0], (String)args[1],
+							(String)args[2], PropsUtil.get((String)args[2]),
+							UserServiceWhenPortalSendsPasswordEmailTest.class.
+								getClassLoader());
+					}
+
+					throw new UnsupportedOperationException();
+				}));
+
 		_user = UserTestUtil.addUser();
 
 		ServiceContext serviceContext =
@@ -66,6 +113,9 @@ public class UserServiceWhenPortalSendsPasswordEmailTest {
 
 	@After
 	public void tearDown() throws Exception {
+		ReflectionTestUtil.setFieldValue(
+			LocalizationUtil.class, "_localization", _localization);
+
 		ServiceContextThreadLocal.popServiceContext();
 	}
 
@@ -260,6 +310,8 @@ public class UserServiceWhenPortalSendsPasswordEmailTest {
 
 		portletPreferences.store();
 	}
+
+	private Localization _localization;
 
 	@DeleteAfterTestRun
 	private User _user;
