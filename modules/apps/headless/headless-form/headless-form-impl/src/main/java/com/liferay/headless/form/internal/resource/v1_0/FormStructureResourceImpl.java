8a32faa4b282d6450b0354574d3147a28d6cd3f8
Victor Oliveira <victorlaertedoliveira@gmail.com>
2019-04-02 16:31:22 -0300

    LPS-91859 Add FormStructure Resource

--- modules/apps/headless/headless-form/headless-form-impl/src/main/java/com/liferay/headless/form/internal/resource/v1_0/FormStructureResourceImpl.java
+++ modules/apps/headless/headless-form/headless-form-impl/src/main/java/com/liferay/headless/form/internal/resource/v1_0/FormStructureResourceImpl.java
@@ -14,17 +14,73 @@
 
 package com.liferay.headless.form.internal.resource.v1_0;
 
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.headless.form.dto.v1_0.FormStructure;
+import com.liferay.headless.form.internal.dto.v1_0.util.StructureUtil;
 import com.liferay.headless.form.resource.v1_0.FormStructureResource;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.service.ClassNameService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
  * @author Javier Gamarra
+ * @author Victor Oliveira
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v1_0/form-structure.properties",
 	scope = ServiceScope.PROTOTYPE, service = FormStructureResource.class
 )
 public class FormStructureResourceImpl extends BaseFormStructureResourceImpl {
+
+	@Override
+	public Page<FormStructure> getContentSpaceFormStructuresPage(
+			Long contentSpaceId, Pagination pagination)
+		throws Exception {
+
+		ClassName className = _classNameService.fetchClassName(
+			DDMFormInstance.class.getName());
+
+		return Page.of(
+			transform(
+				_ddmStructureLocalService.getStructures(
+					contentSpaceId, className.getClassNameId(),
+					pagination.getStartPosition(), pagination.getEndPosition(),
+					null),
+				ddmStructure -> StructureUtil.toFormStructure(
+					ddmStructure, _portal, _userLocalService,
+					contextAcceptLanguage.getPreferredLocale())),
+			pagination,
+			_ddmStructureLocalService.getStructuresCount(
+				contentSpaceId, className.getClassNameId()));
+	}
+
+	@Override
+	public FormStructure getFormStructure(Long formStructureId)
+		throws Exception {
+
+		return StructureUtil.toFormStructure(
+			_ddmStructureLocalService.getStructure(formStructureId), _portal,
+			_userLocalService, contextAcceptLanguage.getPreferredLocale());
+	}
+
+	@Reference
+	private ClassNameService _classNameService;
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
