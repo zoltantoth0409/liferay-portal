19eb0489ea64520573fb8c6a6192279013a8e6b3
Victor Galan <victor.galan@liferay.com>
2019-04-02 22:13:36 +0200

    LPS-93228 Add exception mappers for blog posting

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/EntryContentExceptionMapper.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.jaxrs.exception.mapper;
+
+import com.liferay.blogs.exception.EntryContentException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Converts any {@code EntryContentException} to a {@code 400} error.
+ *
+ * @author Víctor Galán
+ * @review
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Collaboration.EntryContentExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class EntryContentExceptionMapper
+	implements ExceptionMapper<EntryContentException> {
+
+	@Override
+	public Response toResponse(EntryContentException ece) {
+		String message = ece.getMessage();
+
+		return Response.status(
+			400
+		).type(
+			MediaType.TEXT_PLAIN
+		).entity(
+			message.replace("Content", "Article body")
+		).build();
+	}
+
+}
---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/EntryURLTitleExceptionMapper.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.jaxrs.exception.mapper;
+
+import com.liferay.blogs.exception.EntryUrlTitleException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Converts any {@code EntryUrlTitleException} to a {@code 400} error.
+ *
+ * @author Víctor Galán
+ * @review
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Collaboration.EntryUrlTitleExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class EntryURLTitleExceptionMapper
+	implements ExceptionMapper<EntryUrlTitleException> {
+
+	@Override
+	public Response toResponse(EntryUrlTitleException eute) {
+		String message = eute.getMessage();
+
+		return Response.status(
+			400
+		).type(
+			MediaType.TEXT_PLAIN
+		).entity(
+			message.replace("URL title", "FriendlyURLPath")
+		).build();
+	}
+
+}
