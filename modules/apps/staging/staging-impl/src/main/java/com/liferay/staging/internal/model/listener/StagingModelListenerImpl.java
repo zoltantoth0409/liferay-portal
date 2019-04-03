97d8d835e60381f5eeb84572bdc5ca67bb6da7c7
Máté Thurzó <mate.thurzo@liferay.com>
2019-04-03 10:20:38 +0200

    LPS-92663 Fix staged model listener

--- modules/apps/staging/staging-impl/src/main/java/com/liferay/staging/internal/model/listener/StagingModelListenerImpl.java
+++ modules/apps/staging/staging-impl/src/main/java/com/liferay/staging/internal/model/listener/StagingModelListenerImpl.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.version.VersionedModel;
 import com.liferay.staging.model.listener.StagingModelListener;
 
 import org.osgi.service.component.annotations.Component;
@@ -34,6 +35,10 @@ public class StagingModelListenerImpl<T extends BaseModel<T>>
 
 	@Override
 	public void onAfterCreate(T model) throws ModelListenerException {
+		if (!_checkVersionedModel(model)) {
+			return;
+		}
+
 		try {
 			_staging.addModelToChangesetCollection(model);
 		}
@@ -44,6 +49,10 @@ public class StagingModelListenerImpl<T extends BaseModel<T>>
 
 	@Override
 	public void onAfterRemove(T model) throws ModelListenerException {
+		if (!_checkVersionedModel(model)) {
+			return;
+		}
+
 		try {
 			_staging.removeModelFromChangesetCollection(model);
 		}
@@ -54,12 +63,33 @@ public class StagingModelListenerImpl<T extends BaseModel<T>>
 
 	@Override
 	public void onAfterUpdate(T model) throws ModelListenerException {
+		if (!_checkVersionedModel(model)) {
+			return;
+		}
+
 		try {
 			_staging.addModelToChangesetCollection(model);
 		}
 		catch (PortalException pe) {
 			_log.error("Unable to add updated model to the changeset", pe);
 		}
+	}
+
+	private boolean _checkVersionedModel(T model) {
+		boolean checkedModel = false;
+
+		if (model instanceof VersionedModel) {
+			VersionedModel versionedModel = (VersionedModel)model;
+
+			if (versionedModel.isHead()) {
+				checkedModel = true;
+			}
+		}
+		else {
+			checkedModel = true;
+		}
+
+		return checkedModel;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
