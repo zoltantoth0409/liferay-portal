02514b93b348d194bd2ba1afe1e674af6e97eb02
Brian Chan <brian.chan@liferay.com>
2019-04-03 12:00:42 -0700

    LPS-92519 Hugo SF

--- modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/configuration/PersonalMenuConfigurationTrackerImpl.java
+++ modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/configuration/PersonalMenuConfigurationTrackerImpl.java
@@ -43,7 +43,7 @@ import org.osgi.service.component.annotations.Modified;
 	}
 )
 public class PersonalMenuConfigurationTrackerImpl
-	implements PersonalMenuConfigurationTracker, ManagedServiceFactory {
+	implements ManagedServiceFactory, PersonalMenuConfigurationTracker {
 
 	@Activate
 	@Modified
