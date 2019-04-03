5a1e5ee08b3951aa01c512debe90744df924741a
Victor Galan <victor.galan@liferay.com>
2019-04-03 17:32:37 +0200

    LPS-93299 Add exception mappers for KnowledgeBase

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/DuplicateKBFolderNameExceptionMapper.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.jaxrs.exception.mapper;
+
+import com.liferay.knowledge.base.exception.DuplicateKBFolderNameException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Converts any {@code DuplicateKBFolderNameException} to a {@code 422}
+ * error.
+ *
+ * @author Alejandro Hernández
+ * @review
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Delivery.DuplicateKBFolderNameExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class DuplicateKBFolderNameExceptionMapper
+	implements ExceptionMapper<DuplicateKBFolderNameException> {
+
+	@Override
+	public Response toResponse(DuplicateKBFolderNameException dkbfne) {
+		return Response.status(
+			409
+		).type(
+			MediaType.TEXT_PLAIN
+		).entity(
+			dkbfne.getMessage()
+		).build();
+	}
+
+}
---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/InvalidKBFolderNameExceptionMapper.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.jaxrs.exception.mapper;
+
+import com.liferay.knowledge.base.exception.InvalidKBFolderNameException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Converts any {@code InvalidKBFolderNameException} to a {@code 409} error.
+ *
+ * @author Víctor Galán
+ * @review
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Delivery.InvalidKBFolderNameExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class InvalidKBFolderNameExceptionMapper
+	implements ExceptionMapper<InvalidKBFolderNameException> {
+
+	@Override
+	public Response toResponse(InvalidKBFolderNameException ikbfne) {
+		return Response.status(
+			409
+		).type(
+			MediaType.TEXT_PLAIN
+		).entity(
+			ikbfne.getMessage()
+		).build();
+	}
+
+}
