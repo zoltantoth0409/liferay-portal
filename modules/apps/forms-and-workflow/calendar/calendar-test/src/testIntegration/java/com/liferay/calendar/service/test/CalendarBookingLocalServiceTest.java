774f17c65cff11b23b7a8e5488bf6780403500b7
Brian I. Kim <il.kim@liferay.com>
2019-04-03 11:32:10 -0700

    LPS-91093 Add integration test

--- modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/service/test/CalendarBookingLocalServiceTest.java
+++ modules/apps/forms-and-workflow/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/service/test/CalendarBookingLocalServiceTest.java
@@ -829,6 +829,37 @@ public class CalendarBookingLocalServiceTest {
 	}
 
 	@Test
+	public void testInviteUserCalendarWithWorkflowShouldNotifieInviteCalendarBookingOnlyAfterApprovedAndPublished()
+		throws Exception {
+
+		_group = GroupTestUtil.addGroup();
+
+		CalendarWorkflowTestUtil.activateWorkflow(_group);
+
+		_user = UserTestUtil.addUser();
+
+		Calendar invitedCalendar = CalendarTestUtil.addCalendar(_user);
+
+		Calendar invitingCalendar = CalendarTestUtil.getDefaultCalendar(_group);
+
+		CalendarBooking calendarBooking =
+			CalendarBookingTestUtil.addMasterCalendarBookingWithWorkflow(
+				invitingCalendar, invitedCalendar,
+				WorkflowConstants.ACTION_PUBLISH);
+
+		String mailMessageSubject =
+			"Calendar: Event Notification for " + StringPool.QUOTE +
+				calendarBooking.getTitle(LocaleUtil.getDefault()) +
+					StringPool.QUOTE;
+
+		assertMailSubjectCount(mailMessageSubject, 0);
+
+		CalendarWorkflowTestUtil.completeWorkflow(_group);
+
+		assertMailSubjectCount(mailMessageSubject, 1);
+	}
+
+	@Test
 	public void testInviteUserResourceCalendar() throws Exception {
 		ServiceContext serviceContext = createServiceContext();
 
