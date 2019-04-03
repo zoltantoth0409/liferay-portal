133d02268d56798a6cbc90849b32035f1459bc65
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-04-03 16:20:30 +0200

    LPS-91295 Support OpenNLP for Journal.

---
+++ modules/apps/journal/journal-asset-auto-tagger-opennlp/src/main/java/com/liferay/journal/asset/auto/tagger/opennlp/internal/OpenNLPDocumentAssetAutoTagProvider.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.asset.auto.tagger.opennlp.internal;
+
+import com.liferay.asset.auto.tagger.AssetAutoTagProvider;
+import com.liferay.asset.auto.tagger.opennlp.api.OpenNLPDocumentAssetAutoTagger;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.storage.Fields;
+import com.liferay.dynamic.data.mapping.util.DDMIndexer;
+import com.liferay.dynamic.data.mapping.util.FieldsToDDMFormValuesConverter;
+import com.liferay.journal.asset.auto.tagger.opennlp.internal.configuration.OpenNPLDocumentAssetAutoTagProviderCompanyConfiguration;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.util.JournalConverter;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ * @author Alicia García
+ */
+@Component(
+	property = "model.class.name=com.liferay.journal.model.JournalArticle",
+	service = AssetAutoTagProvider.class
+)
+public class OpenNLPDocumentAssetAutoTagProvider
+	implements AssetAutoTagProvider<JournalArticle> {
+
+	@Override
+	public Collection<String> getTagNames(JournalArticle journalArticle) {
+		try {
+			return _getTagNames(journalArticle);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			return Collections.emptyList();
+		}
+	}
+
+	protected String extractDDMContent(JournalArticle journalArticle) {
+		DDMStructure ddmStructure = _ddmStructureLocalService.fetchStructure(
+			_portal.getSiteGroupId(journalArticle.getGroupId()),
+			_portal.getClassNameId(JournalArticle.class),
+			journalArticle.getDDMStructureKey(), true);
+
+		if (ddmStructure == null) {
+			return StringPool.BLANK;
+		}
+
+		DDMFormValues ddmFormValues = null;
+
+		try {
+			Fields fields = _journalConverter.getDDMFields(
+				ddmStructure, journalArticle.getDocument());
+
+			ddmFormValues = _fieldsToDDMFormValuesConverter.convert(
+				ddmStructure, fields);
+		}
+		catch (Exception e) {
+			return StringPool.BLANK;
+		}
+
+		if (ddmFormValues == null) {
+			return StringPool.BLANK;
+		}
+
+		return _ddmIndexer.extractIndexableAttributes(
+			ddmStructure, ddmFormValues,
+			LocaleUtil.fromLanguageId(journalArticle.getDefaultLanguageId()));
+	}
+
+	private Collection<String> _getTagNames(JournalArticle journalArticle)
+		throws Exception {
+
+		return _openNLPDocumentAssetAutoTagger.getTagNames(
+			_configurationProvider.getCompanyConfiguration(
+				OpenNPLDocumentAssetAutoTagProviderCompanyConfiguration.class,
+				journalArticle.getCompanyId()),
+			extractDDMContent(journalArticle), ContentTypes.TEXT_PLAIN);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		OpenNLPDocumentAssetAutoTagProvider.class);
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private DDMIndexer _ddmIndexer;
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+	@Reference
+	private FieldsToDDMFormValuesConverter _fieldsToDDMFormValuesConverter;
+
+	@Reference
+	private JournalConverter _journalConverter;
+
+	@Reference
+	private OpenNLPDocumentAssetAutoTagger _openNLPDocumentAssetAutoTagger;
+
+	@Reference
+	private Portal _portal;
+
+}
---
+++ modules/apps/journal/journal-asset-auto-tagger-opennlp/src/main/java/com/liferay/journal/asset/auto/tagger/opennlp/internal/configuration/OpenNPLDocumentAssetAutoTagProviderCompanyConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.asset.auto.tagger.opennlp.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.asset.auto.tagger.opennlp.api.configuration.OpenNLPDocumentAssetAutoTagCompanyConfiguration;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Cristina González
+ */
+@ExtendedObjectClassDefinition(
+	category = "web-content",
+	scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	description = "opennlp-auto-tag-provider-configuration-description",
+	id = "com.liferay.journal.asset.auto.tagger.opennlp.internal.configuration.OpenNPLDocumentAssetAutoTagProviderCompanyConfiguration",
+	localization = "content/Language",
+	name = "opennlp-auto-tag-provider-configuration-name"
+)
+public interface OpenNPLDocumentAssetAutoTagProviderCompanyConfiguration
+	extends OpenNLPDocumentAssetAutoTagCompanyConfiguration {
+
+	@Meta.AD(deflt = "false", name = "enabled", required = false)
+	public boolean enabled();
+
+	@Meta.AD(
+		deflt = "0.1", description = "confidence-threshold-description",
+		name = "confidence-threshold", required = false
+	)
+	public float confidenceThreshold();
+
+}
