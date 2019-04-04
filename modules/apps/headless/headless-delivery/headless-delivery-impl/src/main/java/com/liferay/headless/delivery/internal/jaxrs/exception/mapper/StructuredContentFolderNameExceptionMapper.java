bb1303907dad9faabf93e90ff704980650c748be
Victor Galan <victor.galan@liferay.com>
2019-04-04 23:23:36 +0200

    LPS-93270 Add patch and required for StructuredContentFolder

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/StructuredContentFolderNameExceptionMapper.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.jaxrs.exception.mapper;
+
+import com.liferay.document.library.kernel.exception.FolderNameException;
+import com.liferay.portal.kernel.util.StringUtil;
+import org.osgi.service.component.annotations.Component;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+/**
+ * Converts any {@code FolderNameException} to a {@code 400} error.
+ *
+ * @author Víctor Galán
+ * @review
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Delivery.FolderNameExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class StructuredContentFolderNameExceptionMapper
+	implements ExceptionMapper<FolderNameException> {
+
+	@Override
+	public Response toResponse(FolderNameException fne) {
+		return Response.status(
+			400
+		).type(
+			MediaType.TEXT_PLAIN
+		).entity(
+			StringUtil.replace(fne.getMessage(), "Folder", "Document folder")
+		).build();
+	}
+
+}
