5b2ae3732d96f7f0dec6d35cb4de99f9d389c248
Julien Castelain <julien.castelain@liferay.com>
2019-04-04 15:16:55 +0200

    LPS-93377 Update CKEditor to v4.11.3

--- modules/apps/frontend-editor/frontend-editor-alloyeditor-web/src/main/java/com/liferay/frontend/editor/alloyeditor/web/internal/editor/configuration/BaseAlloyEditorConfigContributor.java
+++ modules/apps/frontend-editor/frontend-editor-alloyeditor-web/src/main/java/com/liferay/frontend/editor/alloyeditor/web/internal/editor/configuration/BaseAlloyEditorConfigContributor.java
@@ -79,8 +79,8 @@ public abstract class BaseAlloyEditorConfigContributor
 
 		jsonObject.put(
 			"removePlugins",
-			"contextmenu,elementspath,image,link,liststyle,resize,tabletools," +
-				"toolbar");
+			"contextmenu,elementspath,floatingspace,image,link,liststyle,resize," +
+				"tabletools,toolbar");
 
 		String namespace = GetterUtil.getString(
 			inputEditorTaglibAttributes.get(
