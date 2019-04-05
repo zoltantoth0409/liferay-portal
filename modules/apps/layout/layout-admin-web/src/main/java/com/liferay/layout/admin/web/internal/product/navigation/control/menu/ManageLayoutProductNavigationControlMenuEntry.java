3598f207d2cc785d81aa1bd4634e611b7748a496
Pavel Savinov <pavel.savinov@liferay.com>
2019-04-05 12:17:27 +0200

    LPS-93444 Make page configuration icon visible for page templates and display pages

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/product/navigation/control/menu/ManageLayoutProductNavigationControlMenuEntry.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/product/navigation/control/menu/ManageLayoutProductNavigationControlMenuEntry.java
@@ -15,8 +15,6 @@
 package com.liferay.layout.admin.web.internal.product.navigation.control.menu;
 
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
-import com.liferay.layout.content.page.editor.constants.ContentPageEditorWebKeys;
-import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.Language;
@@ -41,7 +39,6 @@ import java.io.Writer;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 import javax.portlet.PortletRequest;
@@ -170,15 +167,6 @@ public class ManageLayoutProductNavigationControlMenuEntry
 
 		if (!(themeDisplay.isShowLayoutTemplatesIcon() ||
 			  themeDisplay.isShowPageSettingsIcon())) {
-
-			return false;
-		}
-
-		String className = (String)request.getAttribute(
-			ContentPageEditorWebKeys.CLASS_NAME);
-
-		if (Objects.equals(
-				className, LayoutPageTemplateEntry.class.getName())) {
 
 			return false;
 		}
