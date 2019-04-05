2b5980a540f48125cae1a252dea64c79edeb73a3
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-04-05 10:42:09 +0200

    LPS-93441 Creating auto tag provider availability controller
    After a chat with @drewbrokke we decided to use a single component to
    avoid repeating the same code in 5 different places. For 7.3 the API
    will be improved in a way we can avoid using internals from here.

---
+++ modules/apps/asset/asset-auto-tagger-service/src/main/java/com/liferay/asset/auto/tagger/internal/configuration/display/AssetAutoTagProviderConfigurationAvailabilityController.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.auto.tagger.internal.configuration.display;
+
+import com.liferay.asset.auto.tagger.configuration.AssetAutoTaggerConfiguration;
+import com.liferay.asset.auto.tagger.configuration.AssetAutoTaggerConfigurationFactory;
+import com.liferay.configuration.admin.display.ConfigurationAvailabilityController;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+
+import java.io.Serializable;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = {
+		"configuration.pid=com.liferay.document.library.asset.auto.tagger.google.cloud.natural.language.internal.configuration.GCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration",
+		"configuration.pid=com.liferay.document.library.asset.auto.tagger.google.cloud.vision.internal.configuration.GCloudVisionAssetAutoTagProviderCompanyConfiguration",
+		"configuration.pid=com.liferay.document.library.asset.auto.tagger.microsoft.cognitive.services.internal.configuration.MSCognitiveServicesAssetAutoTagProviderCompanyConfiguration",
+		"configuration.pid=com.liferay.document.library.asset.auto.tagger.opennlp.internal.configuration.OpenNLPDocumentAssetAutoTagProviderCompanyConfiguration",
+		"configuration.pid=com.liferay.document.library.asset.auto.tagger.tensorflow.internal.configuration.TensorFlowImageAssetAutoTagProviderCompanyConfiguration"
+	},
+	service = ConfigurationAvailabilityController.class
+)
+public class AssetAutoTagProviderConfigurationAvailabilityController
+	implements ConfigurationAvailabilityController {
+
+	@Override
+	public boolean isVisible(
+		ExtendedObjectClassDefinition.Scope scope, Serializable scopePK) {
+
+		try {
+			AssetAutoTaggerConfiguration assetAutoTaggerConfiguration =
+				_getAssetAutoTaggerConfiguration(scope, scopePK);
+
+			if (ExtendedObjectClassDefinition.Scope.SYSTEM.equals(scope)) {
+				return assetAutoTaggerConfiguration.isEnabled();
+			}
+
+			return assetAutoTaggerConfiguration.isAvailable();
+		}
+		catch (PortalException pe) {
+			_log.error(pe, pe);
+
+			return false;
+		}
+	}
+
+	private AssetAutoTaggerConfiguration _getAssetAutoTaggerConfiguration(
+			ExtendedObjectClassDefinition.Scope scope, Serializable scopePK)
+		throws PortalException {
+
+		if (ExtendedObjectClassDefinition.Scope.SYSTEM.equals(scope)) {
+			return _assetAutoTaggerConfigurationFactory.
+				getSystemAssetAutoTaggerConfiguration();
+		}
+
+		if (ExtendedObjectClassDefinition.Scope.COMPANY.equals(scope)) {
+			return _assetAutoTaggerConfigurationFactory.
+				getCompanyAssetAutoTaggerConfiguration(
+					_companyLocalService.getCompany((long)scopePK));
+		}
+
+		throw new IllegalArgumentException("Unrecognized scope: " + scope);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AssetAutoTagProviderConfigurationAvailabilityController.class);
+
+	@Reference
+	private AssetAutoTaggerConfigurationFactory
+		_assetAutoTaggerConfigurationFactory;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+}
