9ba07b27119523d607a16184bd6049ff05f15d4e
Brian Chan <brian.chan@liferay.com>
2019-04-05 09:16:02 -0700

    LPS-92478 avoid this type of chaining

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
@@ -448,7 +448,9 @@ public class DDMFormDisplayContext {
 	protected String createCaptchaResourceURL() {
 		ResourceURL resourceURL = _renderResponse.createResourceURL();
 
-		((LiferayPortletURL)resourceURL).setCopyCurrentRenderParameters(false);
+		LiferayPortletURL liferayPortletURL = (LiferayPortletURL)resourceURL;
+
+		liferayPortletURL.setCopyCurrentRenderParameters(false);
 
 		resourceURL.setResourceID("captcha");
 
