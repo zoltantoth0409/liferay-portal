949cf10339961f1bf7c5a4ce85b364c74b176b8a
Matthew Tambara <matthew.tambara@liferay.com>
2019-04-05 14:17:53 -0700

    LPS-93697 Extract to StatementWrapper

--- portal-test/src/com/liferay/portal/kernel/test/rule/BaseTestRule.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/BaseTestRule.java
@@ -14,14 +14,8 @@
 
 package com.liferay.portal.kernel.test.rule;
 
-import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.callback.TestCallback;
 
-import org.junit.internal.runners.statements.ExpectException;
-import org.junit.internal.runners.statements.FailOnTimeout;
-import org.junit.internal.runners.statements.InvokeMethod;
-import org.junit.internal.runners.statements.RunAfters;
-import org.junit.internal.runners.statements.RunBefores;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -76,31 +70,6 @@ public class BaseTestRule<C, M> implements TestRule {
 			}
 
 		};
-	}
-
-	protected Object inspectTarget(Statement statement) {
-		while (statement instanceof StatementWrapper) {
-			StatementWrapper statementWrapper = (StatementWrapper)statement;
-
-			statement = statementWrapper.getStatement();
-		}
-
-		if (statement instanceof InvokeMethod ||
-			statement instanceof RunAfters || statement instanceof RunBefores) {
-
-			return ReflectionTestUtil.getFieldValue(statement, "target");
-		}
-		else if (statement instanceof ExpectException) {
-			return inspectTarget(
-				ReflectionTestUtil.<Statement>getFieldValue(statement, "next"));
-		}
-		else if (statement instanceof FailOnTimeout) {
-			return inspectTarget(
-				ReflectionTestUtil.<Statement>getFieldValue(
-					statement, "originalStatement"));
-		}
-
-		throw new IllegalStateException("Unknow statement " + statement);
 	}
 
 	private final TestCallback<C, M> _testCallback;
--- portal-test/src/com/liferay/portal/kernel/test/rule/StatementWrapper.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/StatementWrapper.java
@@ -14,6 +14,13 @@
 
 package com.liferay.portal.kernel.test.rule;
 
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.internal.runners.statements.FailOnTimeout;
+import org.junit.internal.runners.statements.InvokeMethod;
+import org.junit.internal.runners.statements.RunAfters;
+import org.junit.internal.runners.statements.RunBefores;
 import org.junit.runners.model.Statement;
 
 /**
@@ -27,6 +34,31 @@ public abstract class StatementWrapper extends Statement {
 
 	public Statement getStatement() {
 		return statement;
+	}
+
+	public Object inspectTarget(Statement statement) {
+		while (statement instanceof StatementWrapper) {
+			StatementWrapper statementWrapper = (StatementWrapper)statement;
+
+			statement = statementWrapper.getStatement();
+		}
+
+		if (statement instanceof InvokeMethod ||
+			statement instanceof RunAfters || statement instanceof RunBefores) {
+
+			return ReflectionTestUtil.getFieldValue(statement, "target");
+		}
+		else if (statement instanceof ExpectException) {
+			return inspectTarget(
+				ReflectionTestUtil.<Statement>getFieldValue(statement, "next"));
+		}
+		else if (statement instanceof FailOnTimeout) {
+			return inspectTarget(
+				ReflectionTestUtil.<Statement>getFieldValue(
+					statement, "originalStatement"));
+		}
+
+		throw new IllegalStateException("Unknow statement " + statement);
 	}
 
 	protected final Statement statement;
