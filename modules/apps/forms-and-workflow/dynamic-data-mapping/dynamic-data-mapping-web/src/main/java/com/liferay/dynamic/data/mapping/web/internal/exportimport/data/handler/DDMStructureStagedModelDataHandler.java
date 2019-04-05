a30ca3185d347957df5b96f707b0963575e1bc04
JavierMoral <javier.moral@liferay.com>
2019-04-05 10:29:38 +0200

    LPS-91735 Consider case when ther's no value for companyGroupId in map

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/DDMStructureStagedModelDataHandler.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/DDMStructureStagedModelDataHandler.java
@@ -339,8 +339,11 @@ public class DDMStructureStagedModelDataHandler
 				(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
 					Group.class);
 
-			if (groupId != groupIds.get(
-					portletDataContext.getCompanyGroupId())) {
+			Long companyGroupIdValue = groupIds.get(
+				portletDataContext.getCompanyGroupId());
+
+			if ((companyGroupIdValue == null) ||
+				(groupId != companyGroupIdValue)) {
 
 				groupId = portletDataContext.getCompanyGroupId();
 			}
