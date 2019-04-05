6eb148660aa305f4def1acdb63b94c31530e54e3
David Arques <davidarchena@gmail.com>
2019-04-05 17:30:03 +0200

    LPS-93534 Add delete segments experience command

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageLayoutEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageLayoutEditorDisplayContext.java
@@ -73,6 +73,10 @@ public class ContentPageLayoutEditorDisplayContext
 			"defaultSegmentsExperienceId",
 			String.valueOf(SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT)
 		).put(
+			"deleteSegmentsExperienceURL",
+			getFragmentEntryActionURL(
+				"/content_layout/delete_segments_experience")
+		).put(
 			"layoutDataList", _getLayoutDataListSoyContext()
 		).put(
 			"sidebarPanels", getSidebarPanelSoyContexts(false)
@@ -104,6 +108,10 @@ public class ContentPageLayoutEditorDisplayContext
 		).put(
 			"defaultSegmentsExperienceId",
 			String.valueOf(SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT)
+		).put(
+			"deleteSegmentsExperienceURL",
+			getFragmentEntryActionURL(
+				"/content_layout/delete_segments_experience")
 		).put(
 			"layoutDataList", _getLayoutDataListSoyContext()
 		);
---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/DeleteSegmentsExperienceMVCActionCommand.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.portlet.action;
+
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionConfig;
+import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.segments.constants.SegmentsConstants;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
+
+import java.util.concurrent.Callable;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Arques
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + ContentPageEditorPortletKeys.CONTENT_PAGE_EDITOR_PORTLET,
+		"mvc.command.name=/content_layout/delete_segments_experience"
+	},
+	service = MVCActionCommand.class
+)
+public class DeleteSegmentsExperienceMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	protected void deleteSegmentsExperience(ActionRequest actionRequest)
+		throws PortalException {
+
+		long segmentsExperienceId = ParamUtil.getLong(
+			actionRequest, "segmentsExperienceId",
+			SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT);
+
+		if (segmentsExperienceId !=
+				SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT) {
+
+			_segmentsExperienceLocalService.deleteSegmentsExperience(
+				segmentsExperienceId);
+		}
+
+		String fragmentEntryLinkIdsString = ParamUtil.getString(
+			actionRequest, "fragmentEntryLinkIds");
+
+		if (Validator.isNotNull(fragmentEntryLinkIdsString)) {
+			long[] toFragmentEntryLinkIds = JSONUtil.toLongArray(
+				JSONFactoryUtil.createJSONArray(fragmentEntryLinkIdsString));
+
+			_fragmentEntryLinkLocalService.deleteFragmentEntryLinks(
+				toFragmentEntryLinkIds);
+		}
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		Callable<Void> callable = new DeleteSegmentsExperienceCallable(
+			actionRequest);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		try {
+			TransactionInvokerUtil.invoke(_transactionConfig, callable);
+		}
+		catch (Throwable t) {
+			_log.error(t, t);
+
+			jsonObject.put(
+				"error",
+				LanguageUtil.get(
+					themeDisplay.getRequest(), "an-unexpected-error-occurred"));
+		}
+
+		hideDefaultSuccessMessage(actionRequest);
+
+		JSONPortletResponseUtil.writeJSON(
+			actionRequest, actionResponse, jsonObject);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DeleteSegmentsExperienceMVCActionCommand.class);
+
+	private static final TransactionConfig _transactionConfig =
+		TransactionConfig.Factory.create(
+			Propagation.REQUIRED, new Class<?>[] {Exception.class});
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Reference
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+	private class DeleteSegmentsExperienceCallable implements Callable<Void> {
+
+		@Override
+		public Void call() throws Exception {
+			deleteSegmentsExperience(_actionRequest);
+
+			return null;
+		}
+
+		private DeleteSegmentsExperienceCallable(ActionRequest actionRequest) {
+			_actionRequest = actionRequest;
+		}
+
+		private final ActionRequest _actionRequest;
+
+	}
+
+}
