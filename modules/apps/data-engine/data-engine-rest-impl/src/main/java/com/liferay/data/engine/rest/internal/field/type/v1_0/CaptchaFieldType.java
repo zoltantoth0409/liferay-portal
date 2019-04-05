d785da479542b27c63f98f07a02c62973a028a36
Brian Chan <brian.chan@liferay.com>
2019-04-04 20:54:04 -0700

    LPS-93221 Auto SF

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/CaptchaFieldType.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/CaptchaFieldType.java
@@ -29,7 +29,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.jsp.PageContext;
 
 /**
  * @author Marcela Cunha
