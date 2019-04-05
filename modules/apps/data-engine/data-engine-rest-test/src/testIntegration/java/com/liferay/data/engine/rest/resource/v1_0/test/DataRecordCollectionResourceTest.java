b9161df8bb7aa9497848d676abe4affa80bc8c57
Gabriel Albuquerque <gwas@cin.ufpe.br>
2019-04-05 12:31:12 -0300

    LPS-93321 Adjust post and get test methods for Data Record Collection

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v1_0/test/DataRecordCollectionResourceTest.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v1_0/test/DataRecordCollectionResourceTest.java
@@ -15,15 +15,126 @@
 package com.liferay.data.engine.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.data.engine.rest.dto.v1_0.DataRecordCollection;
+import com.liferay.data.engine.rest.dto.v1_0.LocalizedValue;
+import com.liferay.data.engine.rest.resource.v1_0.test.util.DataDefinitionTestUtil;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 
+import java.util.Arrays;
+
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 /**
- * @author Jeyvison Nascimento
+ * @author Gabriel Albuquerque
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class DataRecordCollectionResourceTest
 	extends BaseDataRecordCollectionResourceTestCase {
+
+	@Ignore
+	@Override
+	public void testDeleteDataRecordCollection() throws Exception {
+	}
+
+	@Ignore
+	@Override
+	public void testGetDataDefinitionDataRecordCollectionsPage()
+		throws Exception {
+	}
+
+	@Ignore
+	@Override
+	public void testGetDataDefinitionDataRecordCollectionsPageWithPagination()
+		throws Exception {
+	}
+
+	@Ignore
+	@Override
+	public void testGetSiteDataRecordCollectionsPage() throws Exception {
+	}
+
+	@Ignore
+	@Override
+	public void testGetSiteDataRecordCollectionsPageWithPagination()
+		throws Exception {
+	}
+
+	@Ignore
+	@Override
+	public void testPutDataRecordCollection() throws Exception {
+	}
+
+	protected void assertValid(DataRecordCollection dataRecordCollection) {
+		boolean valid = false;
+
+		if ((dataRecordCollection.getDataDefinitionId() != null) &&
+			(dataRecordCollection.getId() != null) &&
+			(dataRecordCollection.getName() != null)) {
+
+			valid = true;
+		}
+
+		Assert.assertTrue(valid);
+	}
+
+	@Override
+	protected boolean equals(
+		DataRecordCollection dataRecordCollection1,
+		DataRecordCollection dataRecordCollection2) {
+
+		return Arrays.equals(
+			dataRecordCollection1.getName(), dataRecordCollection2.getName());
+	}
+
+	@Override
+	protected DataRecordCollection randomDataRecordCollection() {
+		try {
+			DDMStructure ddmStructure = DataDefinitionTestUtil.addDDMStructure(
+				testGroup);
+
+			return new DataRecordCollection() {
+				{
+					dataDefinitionId = ddmStructure.getStructureId();
+					id = RandomTestUtil.randomLong();
+					name = new LocalizedValue[] {
+						new LocalizedValue() {
+							{
+								key = "en_US";
+								value = RandomTestUtil.randomString();
+							}
+						}
+					};
+				}
+			};
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	protected DataRecordCollection
+			testGetDataRecordCollection_addDataRecordCollection()
+		throws Exception {
+
+		DataRecordCollection dataRecordCollection =
+			randomDataRecordCollection();
+
+		return invokePostDataDefinitionDataRecordCollection(
+			dataRecordCollection.getDataDefinitionId(), dataRecordCollection);
+	}
+
+	@Override
+	protected DataRecordCollection
+			testPostDataDefinitionDataRecordCollection_addDataRecordCollection(
+				DataRecordCollection dataRecordCollection)
+		throws Exception {
+
+		return invokePostDataDefinitionDataRecordCollection(
+			dataRecordCollection.getDataDefinitionId(), dataRecordCollection);
+	}
+
 }
