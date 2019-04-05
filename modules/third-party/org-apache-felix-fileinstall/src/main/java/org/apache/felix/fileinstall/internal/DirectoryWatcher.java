1cbec7d356d7dda66e679ed3e803f4e120e00b0e
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2019-04-05 17:43:51 +0200

    LPS-93457 Adapt LPS-73562 to 3.6.4

--- modules/third-party/org-apache-felix-fileinstall/src/main/java/org/apache/felix/fileinstall/internal/DirectoryWatcher.java
+++ modules/third-party/org-apache-felix-fileinstall/src/main/java/org/apache/felix/fileinstall/internal/DirectoryWatcher.java
@@ -1035,6 +1035,11 @@ public class DirectoryWatcher extends Thread implements BundleListener
             Util.log(context, Logger.LOG_INFO, "Installing bundle " + sn
                     + " / " + v, null);
             Bundle b = context.installBundle(bundleLocation, is);
+
+            if (b.getState() == Bundle.UNINSTALLED) {
+                return b;
+            }
+
             Util.storeChecksum(b, checksum, context);
             modified.set(true);
 
@@ -1496,3 +1501,4 @@ public class DirectoryWatcher extends Thread implements BundleListener
     }
 
 }
+/* @generated */
