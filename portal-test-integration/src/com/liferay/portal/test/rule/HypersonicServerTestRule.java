f82e9dc76dcd9c19d69af285b188a0e0a229da2b
Matthew Tambara <matthew.tambara@liferay.com>
2019-04-05 16:43:37 -0700

    LPS-93697 Convert HypersonicServerTestCallback to TestRule

--- portal-test-integration/src/com/liferay/portal/test/rule/HypersonicServerTestRule.java
+++ portal-test-integration/src/com/liferay/portal/test/rule/HypersonicServerTestRule.java
@@ -14,30 +14,139 @@
 
 package com.liferay.portal.test.rule;
 
+import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.CharPool;
-import com.liferay.portal.kernel.test.rule.BaseTestRule;
-import com.liferay.portal.kernel.test.rule.callback.BaseTestCallback;
+import com.liferay.portal.kernel.io.unsync.UnsyncPrintWriter;
+import com.liferay.portal.kernel.test.rule.ClassTestRule;
 import com.liferay.portal.kernel.util.Props;
-import com.liferay.portal.test.rule.callback.HypersonicServerTestCallback;
 import com.liferay.portal.util.PropsImpl;
+import com.liferay.portal.util.PropsValues;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.hsqldb.jdbc.JDBCDriver;
 import org.hsqldb.server.Server;
+import org.hsqldb.server.ServerConstants;
+
+import org.junit.runner.Description;
 
 /**
  * @author William Newbury
  * @author Shuyang Zhou
  */
-public class HypersonicServerTestRule extends BaseTestRule<Server, Object> {
+public class HypersonicServerTestRule extends ClassTestRule<Server> {
+
+	public static final String DATABASE_URL_BASE =
+		"jdbc:hsqldb:hsql://localhost/";
 
 	public static final HypersonicServerTestRule INSTANCE;
 
-	public HypersonicServerTestRule() {
-		super(_getTestCallback());
+	@Override
+	public void afterClass(Description description, Server server)
+		throws Exception {
+
+		try (Connection connection = DriverManager.getConnection(
+				DATABASE_URL_BASE + _DATABASE_NAME, "sa", "");
+			Statement statement = connection.createStatement()) {
+
+			statement.execute("SHUTDOWN COMPACT");
+		}
+
+		server.stop();
+
+		deleteFolder(Paths.get(_HYPERSONIC_TEMP_DIR_NAME));
+	}
+
+	@Override
+	public Server beforeClass(Description description) throws Exception {
+		final CountDownLatch startCountDownLatch = new CountDownLatch(1);
+
+		Server server = new Server() {
+
+			@Override
+			public int stop() {
+				try (PrintWriter logPrintWriter = getLogWriter();
+					PrintWriter errPrintWriter = getErrWriter()) {
+
+					int state = super.stop();
+
+					if (!_shutdownCountDownLatch.await(1, TimeUnit.MINUTES)) {
+						throw new IllegalStateException(
+							"Unable to shut down Hypersonic " + _DATABASE_NAME);
+					}
+
+					return state;
+				}
+				catch (InterruptedException ie) {
+					return ReflectionUtil.throwException(ie);
+				}
+			}
+
+			@Override
+			protected synchronized void setState(int state) {
+				super.setState(state);
+
+				if (state == ServerConstants.SERVER_STATE_ONLINE) {
+					startCountDownLatch.countDown();
+				}
+				else if (state == ServerConstants.SERVER_STATE_SHUTDOWN) {
+					_shutdownCountDownLatch.countDown();
+				}
+			}
+
+			private final CountDownLatch _shutdownCountDownLatch =
+				new CountDownLatch(1);
+
+		};
+
+		try (Connection connection = DriverManager.getConnection(
+				PropsValues.JDBC_DEFAULT_URL, "sa", "");
+			Statement statement = connection.createStatement()) {
+
+			statement.execute(
+				"BACKUP DATABASE TO '" + _HYPERSONIC_TEMP_DIR_NAME +
+					"' BLOCKING AS FILES");
+		}
+
+		server.setErrWriter(
+			new UnsyncPrintWriter(
+				new File(
+					_HYPERSONIC_TEMP_DIR_NAME, _DATABASE_NAME + ".err.log")));
+		server.setLogWriter(
+			new UnsyncPrintWriter(
+				new File(
+					_HYPERSONIC_TEMP_DIR_NAME, _DATABASE_NAME + ".std.log")));
+
+		server.setDatabaseName(0, _DATABASE_NAME);
+		server.setDatabasePath(0, _HYPERSONIC_TEMP_DIR_NAME + _DATABASE_NAME);
+
+		server.start();
+
+		if (!startCountDownLatch.await(1, TimeUnit.MINUTES)) {
+			throw new IllegalStateException(
+				"Unable to start up Hypersonic " + _DATABASE_NAME);
+		}
+
+		return server;
 	}
 
 	public List<String> getJdbcProperties() {
@@ -51,12 +160,67 @@ public class HypersonicServerTestRule extends BaseTestRule<Server, Object> {
 		return Collections.emptyList();
 	}
 
-	private static BaseTestCallback<Server, Object> _getTestCallback() {
-		if (_HYPERSONIC) {
-			return new HypersonicServerTestCallback(_DATABASE_NAME);
+	protected void copyFile(
+			String fileName, Path fromFolderPath, Path toFolderPath)
+		throws IOException {
+
+		Path filePath = fromFolderPath.resolve(fileName);
+
+		if (Files.exists(filePath)) {
+			Files.createDirectories(toFolderPath);
+
+			Files.copy(filePath, toFolderPath.resolve(fileName));
+		}
+	}
+
+	@Override
+	protected org.junit.runners.model.Statement createClassStatement(
+		org.junit.runners.model.Statement statement, Description description) {
+
+		if (!_HYPERSONIC) {
+			return statement;
 		}
 
-		return new BaseTestCallback<>();
+		return super.createClassStatement(statement, description);
+	}
+
+	protected void deleteFolder(Path folderPath) throws IOException {
+		if (!Files.exists(folderPath)) {
+			return;
+		}
+
+		Files.walkFileTree(
+			folderPath,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult postVisitDirectory(
+						Path dirPath, IOException ioe)
+					throws IOException {
+
+					if (ioe != null) {
+						throw ioe;
+					}
+
+					Files.delete(dirPath);
+
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(
+						Path filePath, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					Files.delete(filePath);
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+	}
+
+	private HypersonicServerTestRule() {
 	}
 
 	private static final String _DATABASE_NAME;
@@ -64,6 +228,9 @@ public class HypersonicServerTestRule extends BaseTestRule<Server, Object> {
 	private static final String _DATABASE_URL;
 
 	private static final boolean _HYPERSONIC;
+
+	private static final String _HYPERSONIC_TEMP_DIR_NAME =
+		PropsValues.LIFERAY_HOME + "/data/hypersonic_temp/";
 
 	static {
 		Props props = new PropsImpl();
@@ -92,8 +259,7 @@ public class HypersonicServerTestRule extends BaseTestRule<Server, Object> {
 
 			_DATABASE_NAME = databaseName;
 
-			_DATABASE_URL =
-				HypersonicServerTestCallback.DATABASE_URL_BASE + _DATABASE_NAME;
+			_DATABASE_URL = DATABASE_URL_BASE + _DATABASE_NAME;
 		}
 		else {
 			_DATABASE_NAME = null;
