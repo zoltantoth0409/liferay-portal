92eec889a192804285888a087a9ece2ef9f4f93f
Julien Castelain <julien.castelain@liferay.com>
2019-04-05 11:40:31 +0200

    LPS-93263 Add Array.prototype.findIndex polyfill for IE11

--- modules/apps/frontend-compatibility/frontend-compatibility-ie/src/main/java/com/liferay/frontend/compatibility/ie/internal/servlet/taglib/IETopHeadDynamicInclude.java
+++ modules/apps/frontend-compatibility/frontend-compatibility-ie/src/main/java/com/liferay/frontend/compatibility/ie/internal/servlet/taglib/IETopHeadDynamicInclude.java
@@ -81,10 +81,11 @@ public class IETopHeadDynamicInclude extends BaseDynamicInclude {
 	}
 
 	private static final String[] _FILE_NAMES = {
-		"/array.fill.js", "/array.find.js", "/array.from.js",
-		"/array.includes.js", "/array.of.js", "/fetch.js", "/formdata.js",
-		"/object.assign.js", "/object.entries.js", "/object.values.js",
-		"/promise.js", "/string.endswith.js", "/url.search.params.js"
+		"/array.fill.js", "/array.find.js", "/array.findindex.js",
+		"/array.from.js", "/array.includes.js", "/array.of.js", "/fetch.js",
+		"/formdata.js", "/object.assign.js", "/object.entries.js",
+		"/object.values.js", "/promise.js", "/string.endswith.js",
+		"/url.search.params.js"
 	};
 
 	@Reference
