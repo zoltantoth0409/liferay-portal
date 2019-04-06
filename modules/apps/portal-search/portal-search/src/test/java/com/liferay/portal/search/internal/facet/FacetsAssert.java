3be8b3a76d95be24746bae3ddbf5b9a52932fef7
André de Oliveira <andre.oliveira@liferay.com>
2019-04-05 18:09:59 -0700

    LPS-92937 portal-search: Test: Facet buckets should stay in descending order of frequency, even after decrements by post search permission checker
    https://issues.liferay.com/browse/LPS-92937

---
+++ modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/facet/FacetsAssert.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.facet;
+
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.facet.Facet;
+import com.liferay.portal.kernel.search.facet.collector.FacetCollector;
+import com.liferay.portal.kernel.search.facet.collector.TermCollector;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Assert;
+
+/**
+ * @author André de Oliveira
+ */
+public class FacetsAssert {
+
+	public static void assertFrequencies(
+		String message, Facet facet, String expected) {
+
+		FacetCollector facetCollector = facet.getFacetCollector();
+
+		List<TermCollector> termCollectors = facetCollector.getTermCollectors();
+
+		Assert.assertNotNull(termCollectors);
+
+		Stream<TermCollector> stream = termCollectors.stream();
+
+		Assert.assertEquals(
+			message, expected,
+			stream.map(
+				FacetsAssert::toString
+			).collect(
+				Collectors.toList()
+			).toString());
+	}
+
+	public static void assertFrequencies(
+		String facetName, SearchContext searchContext, List<String> expected) {
+
+		String message = (String)searchContext.getAttribute("queryString");
+
+		Facet facet = searchContext.getFacet(facetName);
+
+		assertFrequencies(message, facet, expected.toString());
+	}
+
+	protected static String toString(TermCollector termCollector) {
+		return termCollector.getTerm() + "=" + termCollector.getFrequency();
+	}
+
+}
