35120406b7b3887647c876bf782185de0d69cc09
Javier Gamarra <nhpatt@gmail.com>
2019-04-08 23:43:57 +0000

    LPS-91222 comment test

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/CommentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/CommentResourceTest.java
@@ -15,14 +15,221 @@
 package com.liferay.headless.delivery.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.headless.delivery.dto.v1_0.Comment;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portlet.documentlibrary.util.test.DLAppTestUtil;
 
-import org.junit.Ignore;
+import java.util.Objects;
+
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class CommentResourceTest extends BaseCommentResourceTestCase {
+
+	@Override
+	protected void assertValid(Comment comment) {
+		boolean valid = false;
+
+		if ((comment.getDateCreated() != null) &&
+			(comment.getDateModified() != null) && (comment.getId() != null) &&
+			(comment.getText() != null)) {
+
+			valid = true;
+		}
+
+		Assert.assertTrue(valid);
+	}
+
+	@Override
+	protected boolean equals(Comment comment1, Comment comment2) {
+		if (Objects.equals(_formatHTML(comment1), _formatHTML(comment2))) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	protected Comment testDeleteComment_addComment() throws Exception {
+		BlogsEntry blogsEntry = _addBlogsEntry();
+
+		return invokePostBlogPostingComment(
+			blogsEntry.getEntryId(), randomComment());
+	}
+
+	@Override
+	protected Comment testGetBlogPostingCommentsPage_addComment(
+			Long blogPostingId, Comment comment)
+		throws Exception {
+
+		return invokePostBlogPostingComment(blogPostingId, comment);
+	}
+
+	@Override
+	protected Long testGetBlogPostingCommentsPage_getBlogPostingId()
+		throws Exception {
+
+		BlogsEntry blogsEntry = _addBlogsEntry();
+
+		return blogsEntry.getEntryId();
+	}
+
+	@Override
+	protected Comment testGetComment_addComment() throws Exception {
+		BlogsEntry blogsEntry = _addBlogsEntry();
+
+		return invokePostBlogPostingComment(
+			blogsEntry.getEntryId(), randomComment());
+	}
+
+	@Override
+	protected Comment testGetCommentCommentsPage_addComment(
+			Long parentCommentId, Comment comment)
+		throws Exception {
+
+		return invokePostCommentComment(parentCommentId, comment);
+	}
+
+	@Override
+	protected Long testGetCommentCommentsPage_getParentCommentId()
+		throws Exception {
+
+		BlogsEntry blogsEntry = _addBlogsEntry();
+
+		Comment comment = invokePostBlogPostingComment(
+			blogsEntry.getEntryId(), randomComment());
+
+		return comment.getId();
+	}
+
+	@Override
+	protected Comment testGetDocumentCommentsPage_addComment(
+			Long documentId, Comment comment)
+		throws Exception {
+
+		return invokePostDocumentComment(documentId, comment);
+	}
+
+	@Override
+	protected Long testGetDocumentCommentsPage_getDocumentId()
+		throws Exception {
+
+		FileEntry fileEntry = _addFileEntry();
+
+		return fileEntry.getFileEntryId();
+	}
+
+	@Override
+	protected Comment testGetStructuredContentCommentsPage_addComment(
+			Long structuredContentId, Comment comment)
+		throws Exception {
+
+		return invokePostStructuredContentComment(structuredContentId, comment);
+	}
+
+	@Override
+	protected Long testGetStructuredContentCommentsPage_getStructuredContentId()
+		throws Exception {
+
+		JournalArticle journalArticle = _addJournalArticle();
+
+		return journalArticle.getResourcePrimKey();
+	}
+
+	@Override
+	protected Comment testPostBlogPostingComment_addComment(Comment comment)
+		throws Exception {
+
+		BlogsEntry blogsEntry = _addBlogsEntry();
+
+		return invokePostBlogPostingComment(blogsEntry.getEntryId(), comment);
+	}
+
+	@Override
+	protected Comment testPostCommentComment_addComment(Comment comment)
+		throws Exception {
+
+		BlogsEntry blogsEntry = _addBlogsEntry();
+
+		Comment parentComment = invokePostBlogPostingComment(
+			blogsEntry.getEntryId(), randomComment());
+
+		return invokePostCommentComment(parentComment.getId(), comment);
+	}
+
+	@Override
+	protected Comment testPostDocumentComment_addComment(Comment comment)
+		throws Exception {
+
+		FileEntry fileEntry = _addFileEntry();
+
+		return invokePostDocumentComment(fileEntry.getFileEntryId(), comment);
+	}
+
+	@Override
+	protected Comment testPostStructuredContentComment_addComment(
+			Comment comment)
+		throws Exception {
+
+		JournalArticle journalArticle = _addJournalArticle();
+
+		return invokePostStructuredContentComment(
+			journalArticle.getResourcePrimKey(), comment);
+	}
+
+	@Override
+	protected Comment testPutComment_addComment() throws Exception {
+		BlogsEntry blogsEntry = _addBlogsEntry();
+
+		Comment comment = invokePostBlogPostingComment(
+			blogsEntry.getEntryId(), randomComment());
+
+		return invokePostCommentComment(comment.getId(), randomComment());
+	}
+
+	private BlogsEntry _addBlogsEntry() throws PortalException {
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setScopeGroupId(testGroup.getGroupId());
+
+		return BlogsEntryLocalServiceUtil.addEntry(
+			UserLocalServiceUtil.getDefaultUserId(testGroup.getCompanyId()),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			serviceContext);
+	}
+
+	private FileEntry _addFileEntry() throws Exception {
+		return DLAppTestUtil.addFileEntryWithWorkflow(
+			UserLocalServiceUtil.getDefaultUserId(testGroup.getCompanyId()),
+			testGroup.getGroupId(), 0, RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), true, new ServiceContext());
+	}
+
+	private JournalArticle _addJournalArticle() throws Exception {
+		return JournalTestUtil.addArticle(testGroup.getGroupId(), 0);
+	}
+
+	private String _formatHTML(Comment comment) {
+		String text = comment.getText();
+
+		if (!text.startsWith("<p>")) {
+			return StringBundler.concat("<p>", text, "</p>");
+		}
+
+		return text;
+	}
+
 }
