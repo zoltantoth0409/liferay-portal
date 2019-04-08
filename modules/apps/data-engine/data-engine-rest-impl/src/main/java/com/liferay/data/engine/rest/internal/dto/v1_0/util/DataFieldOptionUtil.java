f1a69f4b1e87a976e011f85e6214b60a654d7a12
marcelabc <mbc3@cin.ufpe.br>
2019-04-08 15:49:18 -0300

    LPS-93229 Create DataFieldOption class and its util

---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/dto/v1_0/util/DataFieldOptionUtil.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.dto.v1_0.util;
+
+import com.liferay.data.engine.rest.internal.field.type.v1_0.DataFieldOption;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Marcela Cunha
+ */
+public class DataFieldOptionUtil {
+
+	public static List<DataFieldOption> getOptions(
+		List<DataFieldOption> dataFieldOptions, String languageId) {
+
+		Stream<DataFieldOption> stream = dataFieldOptions.stream();
+
+		return stream.map(
+			dataFieldOption -> new DataFieldOption(
+				dataFieldOption.getLabel(languageId), languageId,
+				dataFieldOption.getValue())
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	public static List<DataFieldOption> toDataFieldOptions(
+		JSONObject jsonObject) {
+
+		List<DataFieldOption> dataFieldOptions = new ArrayList<>();
+
+		Iterator<String> keys = jsonObject.keys();
+
+		while (keys.hasNext()) {
+			String key = keys.next();
+
+			Map<String, String> localizationMap =
+				LocalizedValueUtil.toLocalizationMap(
+					jsonObject.getJSONObject(key));
+
+			DataFieldOption dataFieldOption = new DataFieldOption(
+				localizationMap, key);
+
+			dataFieldOptions.add(dataFieldOption);
+		}
+
+		return dataFieldOptions;
+	}
+
+	public static JSONObject toJSONObject(
+			List<DataFieldOption> dataFieldOptions)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		for (DataFieldOption dataFieldOption : dataFieldOptions) {
+			Map<String, String> labels = dataFieldOption.getLabels();
+
+			JSONObject labelJSONObject = JSONFactoryUtil.createJSONObject();
+
+			for (Map.Entry<String, String> entry : labels.entrySet()) {
+				labelJSONObject.put(entry.getKey(), entry.getValue());
+			}
+
+			jsonObject.put(dataFieldOption.getValue(), labelJSONObject);
+		}
+
+		return jsonObject;
+	}
+
+}
---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/DataFieldOption.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.field.type.v1_0;
+
+import com.liferay.portal.kernel.util.MapUtil;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Marcela Cunha
+ */
+public class DataFieldOption {
+
+	public DataFieldOption(Map<String, String> labels, String value) {
+		_labels = labels;
+		_value = value;
+	}
+
+	public DataFieldOption(String label, String languageId, String value) {
+		_labels.put(languageId, label);
+		_value = value;
+	}
+
+	public String getLabel(String languageId) {
+		return MapUtil.getString(_labels, languageId);
+	}
+
+	public Map<String, String> getLabels() {
+		return Collections.unmodifiableMap(_labels);
+	}
+
+	public String getValue() {
+		return _value;
+	}
+
+	private Map<String, String> _labels = new HashMap<>();
+	private final String _value;
+
+}
