fa4ed77b2e04066e1069e6c5041d871c29d432b6
Dante Wang <dante.wang@liferay.com>
2019-04-08 13:40:47 +0800

    LPS-85997 Also update the version in db-upgrade-client

--- modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/AppServer.java
+++ modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/AppServer.java
@@ -52,7 +52,7 @@ public class AppServer {
 
 	public static AppServer getTomcatAppServer() {
 		return new AppServer(
-			"../../tomcat-9.0.10", "/bin", "/lib", "/webapps/ROOT", "tomcat");
+			"../../tomcat-9.0.17", "/bin", "/lib", "/webapps/ROOT", "tomcat");
 	}
 
 	public static AppServer getWebLogicAppServer() {
