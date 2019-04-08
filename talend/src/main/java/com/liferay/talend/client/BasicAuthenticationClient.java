b939742f400c3ad586eecb924d5dc793c9e73aa7
Igor Beslic <igor.beslic@liferay.com>
2019-04-08 10:12:09 +0200

    COMMERCE-1265 talend - input component - http client - basic authentication - will be renamed and reused later when additional code is added to component

---
+++ talend/src/main/java/com/liferay/talend/client/BasicAuthenticationClient.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talend.client;
+
+import org.talend.sdk.component.api.service.http.Header;
+import org.talend.sdk.component.api.service.http.HttpClient;
+import org.talend.sdk.component.api.service.http.Request;
+import org.talend.sdk.component.api.service.http.Response;
+
+import javax.json.JsonObject;
+
+/**
+ * @author Igor Beslic
+ */
+public interface BasicAuthenticationClient extends HttpClient {
+
+	@Request(path = "/c/portal/login", method = "POST")
+	public Response<JsonObject> checkRequest(
+		@Header("Authorization") String auth,
+		@Header("Content-Type") String contentType);
+
+}
