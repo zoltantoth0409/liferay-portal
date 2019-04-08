2cd29a73b31b27bc207aaa6f9be1fb1f25f4a2f2
Inácio Nery <inacio.nery@liferay.com>
2019-04-08 09:28:52 -0300

    LPS-88573 Add JS tests

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-list/__tests__/ResultsBar.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-list/__tests__/ResultsBar.js
@@ -3,6 +3,29 @@ import renderer from 'react-test-renderer';
 import ResultsBar from '../ResultsBar';
 import { MockRouter as Router } from '../../../test/mock/MockRouter';
 
+test('Should render component with one item', () => {
+	const component = renderer.create(
+		<Router>
+			<ResultsBar
+				match={{
+					params: {
+						page: 1,
+						pageSize: 5,
+						search: 'test',
+						sort: encodeURIComponent('title:asc')
+					},
+					path: '/processes/:pageSize/:page/:sort/:search?'
+				}}
+				totalCount={1}
+			/>
+		</Router>
+	);
+
+	const tree = component.toJSON();
+
+	expect(tree).toMatchSnapshot();
+});
+
 test('Should render component with one list item', () => {
 	const component = renderer.create(
 		<Router>
--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/pagination/__tests__/PageSizeEntries.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/pagination/__tests__/PageSizeEntries.js
@@ -1,5 +1,14 @@
 import PageSizeEntries from '../PageSizeEntries';
 import React from 'react';
+import { MockRouter as Router } from '../../../../test/mock/MockRouter';
+
+test('Should change page size', () => {
+	const component = shallow(
+		<PageSizeEntries pageSizeEntries={[10, 20, 30, 40]} selectedPageSize={30} />
+	);
+
+	expect(component).toMatchSnapshot();
+});
 
 test('Should render component', () => {
 	const component = shallow(
@@ -9,9 +18,11 @@ test('Should render component', () => {
 	expect(component).toMatchSnapshot();
 });
 
-test('Should change page size', () => {
-	const component = shallow(
-		<PageSizeEntries pageSizeEntries={[10, 20, 30, 40]} selectedPageSize={30} />
+test('Should render with default deltas', () => {
+	const component = mount(
+		<Router>
+			<PageSizeEntries selectedPageSize={30} />
+		</Router>
 	);
 
 	expect(component).toMatchSnapshot();
