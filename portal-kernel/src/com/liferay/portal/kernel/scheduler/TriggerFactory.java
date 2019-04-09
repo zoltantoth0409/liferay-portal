b43baa32a4b7b950b95bdf4f043a8eb9fb9cc653
Alec-Shay <Alec.Shay@liferay.com>
2019-04-09 11:09:27 -0700

    LPS-93315 Add API to allow for non-global time zones

--- portal-kernel/src/com/liferay/portal/kernel/scheduler/TriggerFactory.java
+++ portal-kernel/src/com/liferay/portal/kernel/scheduler/TriggerFactory.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.kernel.scheduler;
 
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.osgi.annotation.versioning.ProviderType;
 
@@ -31,6 +32,10 @@ public interface TriggerFactory {
 	public Trigger createTrigger(
 		String jobName, String groupName, Date startDate, Date endDate,
 		String cronExpression);
+
+	public Trigger createTrigger(
+		String jobName, String groupName, Date startDate, Date endDate,
+		TimeZone timeZone, String cronExpression);
 
 	public Trigger createTrigger(Trigger trigger, Date startDate, Date endDate);
 
