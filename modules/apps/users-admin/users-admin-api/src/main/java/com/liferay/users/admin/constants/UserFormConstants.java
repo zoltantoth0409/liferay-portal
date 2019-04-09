408ecebeaa8322499971e30796497fb4871d24ea
Pei-Jung Lan <pei-jung.lan@liferay.com>
2019-04-09 15:23:29 -0700

    LPS-93670 Deprecate instead of remove

---
+++ modules/apps/users-admin/users-admin-api/src/main/java/com/liferay/users/admin/constants/UserFormConstants.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.constants;
+
+/**
+ * @author Pei-Jung Lan
+ * @deprecated As of Mueller (7.2.x), replaced by {@link UserScreenNavigationEntryConstants}
+ */
+@Deprecated
+public class UserFormConstants {
+
+	public static final String CATEGORY_KEY_CONTACT = "contact";
+
+	public static final String CATEGORY_KEY_GENERAL = "general";
+
+	public static final String CATEGORY_KEY_PREFERENCES = "preferences";
+
+	public static final String ENTRY_KEY_ADDRESSES = "addresses";
+
+	public static final String ENTRY_KEY_ALERTS_AND_ANNOUNCEMENTS_DELIVERY =
+		"alerts-and-announcements-delivery";
+
+	public static final String ENTRY_KEY_CONTACT_INFORMATION =
+		"contact-information";
+
+	public static final String ENTRY_KEY_DISPLAY_SETTINGS = "display-settings";
+
+	public static final String ENTRY_KEY_INFORMATION = "information";
+
+	public static final String ENTRY_KEY_MEMBERSHIPS = "memberships";
+
+	public static final String ENTRY_KEY_ORGANIZATIONS = "organizations";
+
+	public static final String ENTRY_KEY_PASSWORD = "password";
+
+	public static final String ENTRY_KEY_PROFILE_AND_DASHBOARD =
+		"profile-and-dashboard";
+
+	public static final String ENTRY_KEY_ROLES = "roles";
+
+	public static final String SCREEN_NAVIGATION_KEY_ORGANIZATIONS =
+		"edit.organization.form";
+
+	public static final String SCREEN_NAVIGATION_KEY_USERS = "edit.user.form";
+
+}
