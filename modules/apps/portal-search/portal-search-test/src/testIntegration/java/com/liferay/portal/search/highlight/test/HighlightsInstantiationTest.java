cf2c20e26ba5cd23dbb878a83dc19515b7ac403c
André de Oliveira <andre.oliveira@liferay.com>
2019-04-08 18:09:42 -0700

    LPS-91259 portal-search-test: Test: Highlight: decouple interfaces from implementation
    https://issues.liferay.com/browse/LPS-91259
    
    Co-authored-by: weicao <wade.cao@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/highlight/test/HighlightsInstantiationTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.highlight.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.search.highlight.Highlights;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author André de Oliveira
+ */
+@RunWith(Arquillian.class)
+public class HighlightsInstantiationTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testBuilders() {
+		Assert.assertNotNull(
+			_highlights.builder(
+			).addFieldConfig(
+				_highlights.fieldConfigBuilder(
+				).field(
+					"field"
+				).build()
+			).build());
+	}
+
+	@Test
+	public void testFactories() {
+		Assert.assertNotNull(
+			_highlights.highlight(_highlights.fieldConfig("field")));
+	}
+
+	@Inject
+	private static Highlights _highlights;
+
+}
