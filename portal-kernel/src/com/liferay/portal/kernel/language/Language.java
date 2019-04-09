ad46343b1c63773d202c0b9446c9657424bd2497
Jose A. Jimenez <jose.jimenez@liferay.com>
2019-04-09 12:02:47 +0200

    LPS-93353 Add a default implementation to make easier the backport/semver

--- portal-kernel/src/com/liferay/portal/kernel/language/Language.java
+++ portal-kernel/src/com/liferay/portal/kernel/language/Language.java
@@ -132,7 +132,9 @@ public interface Language {
 
 	public String getLanguageId(PortletRequest portletRequest);
 
-	public long getLastModified();
+	public default long getLastModified() {
+		return System.currentTimeMillis();
+	}
 
 	public Locale getLocale(long groupId, String languageCode);
 
