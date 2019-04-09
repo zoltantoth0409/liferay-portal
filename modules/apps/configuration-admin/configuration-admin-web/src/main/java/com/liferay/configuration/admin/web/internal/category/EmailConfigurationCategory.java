2d14ae2f7b96e98c69629642ae587d3abd8b4978
drewbrokke <drew.brokke@liferay.com>
2019-04-09 15:49:53 -0700

    LPS-93427 configuration-admin-web - adds the "Email" configuration category

---
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/category/EmailConfigurationCategory.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.internal.category;
+
+import com.liferay.configuration.admin.category.ConfigurationCategory;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(service = ConfigurationCategory.class)
+public class EmailConfigurationCategory implements ConfigurationCategory {
+
+	@Override
+	public String getCategoryIcon() {
+		return _CATEGORY_ICON;
+	}
+
+	@Override
+	public String getCategoryKey() {
+		return _CATEGORY_KEY;
+	}
+
+	@Override
+	public String getCategorySection() {
+		return _CATEGORY_SECTION;
+	}
+
+	private static final String _CATEGORY_ICON = "envelope-closed";
+
+	private static final String _CATEGORY_KEY = "email";
+
+	private static final String _CATEGORY_SECTION = "platform";
+
+}
