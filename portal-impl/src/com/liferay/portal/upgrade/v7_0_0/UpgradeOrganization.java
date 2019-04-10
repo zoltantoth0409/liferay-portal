33d5d392ce3668d12031a1048e41fceec9ec8d6d
Brian Chan <brian.chan@liferay.com>
2019-04-10 12:28:42 -0700

    LPS-81837 SF

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeOrganization.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeOrganization.java
@@ -30,6 +30,7 @@ public class UpgradeOrganization extends UpgradeProcess {
 	@Override
 	protected void doUpgrade() throws Exception {
 		alter(OrganizationTable.class, new AlterColumnType("statusId", "LONG"));
+
 		upgradeOrganizationLogoId();
 	}
 
