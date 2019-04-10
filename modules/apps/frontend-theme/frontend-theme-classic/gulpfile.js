240c442476fe29ca6cbf2d3e12b4b2d4c73d1a68
Chema Balsas <jose.balsas@liferay.com>
2019-04-10 14:00:30 +0200

    LPS-93637 Bundles and uses font-awesome and glyphicons fallbacks in classic theme

--- modules/apps/frontend-theme/frontend-theme-classic/gulpfile.js
+++ modules/apps/frontend-theme/frontend-theme-classic/gulpfile.js
@@ -1,9 +1,25 @@
 'use strict';
 
-var gulp = require('gulp');
-var liferayThemeTasks = require('liferay-theme-tasks');
+const fs = require('fs-extra');
+const gulp = require('gulp');
+const path = require('path');
+
+const liferayFontAwesome = path.dirname(require.resolve('liferay-font-awesome/package.json'));
+const liferayThemeTasks = require('liferay-theme-tasks');
 
 liferayThemeTasks.registerTasks(
 	{
-		gulp: gulp
-	});
+		gulp: gulp,
+		hookFn: function(gulp) {
+			gulp.hook(
+				'before:build:war',
+				function(done) {
+					fs.copy(
+						path.join(liferayFontAwesome, 'font'),
+						path.join('build', 'font')
+					).then(done).catch(done);
+				}
+			);
+		}
+	}
+);
