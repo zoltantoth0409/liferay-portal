6b6112afc1cfef30e7f11da9b256b99c00e58005
Pablo Molina <contact@pablomolina.me>
2019-04-10 08:33:57 +0200

    LPS-90397 Add FragmentsEditorDragDrop

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/utils/FragmentsEditorDragDrop.es.js
@@ -0,0 +1,73 @@
+import {AOP} from 'metal-aop';
+import {Drag, DragDrop} from 'metal-drag-drop';
+import {Position} from 'metal-position';
+
+let _getRegionInterceptor = null;
+
+/**
+ * Stops intercepting metal-position getRegion calls
+ * @private
+ * @review
+ * @see {_interceptGetRegion}
+ */
+function _stopInterceptGetRegion() {
+	if (_getRegionInterceptor) {
+		_getRegionInterceptor.detach();
+
+		_getRegionInterceptor = null;
+	}
+}
+
+/**
+ * Uses metal-aop to intercept metal-position getRegion calls
+ * and take into account bootstrap's negative margins.
+ * @private
+ * @review
+ * @see {_stopInterceptGetRegion}
+ */
+function _interceptGetRegion() {
+	_getRegionInterceptor = _getRegionInterceptor || AOP.after(
+		target => {
+			let newReturnVal;
+
+			if ((target instanceof HTMLElement) &&
+				target.classList.contains('col') &&
+				target.parentElement &&
+				target.parentElement.classList.contains('row')) {
+
+				const currentRetVal = Object.assign({}, AOP.currentRetVal);
+
+				const parentComputedStyle = window.getComputedStyle(target.parentElement);
+
+				const negativeLeftMargin = Math.min(parseInt(parentComputedStyle.marginLeft, 10) || 0, 0);
+				const negativeRightMargin = Math.min(parseInt(parentComputedStyle.marginRight, 10) || 0, 0);
+
+				currentRetVal.width = currentRetVal.width + negativeLeftMargin + negativeRightMargin;
+				currentRetVal.left -= negativeLeftMargin;
+				currentRetVal.right += negativeRightMargin;
+
+				newReturnVal = AOP.alterReturn(currentRetVal);
+			}
+
+			return newReturnVal;
+		},
+		Position,
+		'getRegion'
+	);
+}
+
+/**
+ * @param {object} dragDropOptions
+ * @review
+ */
+function initializeDragDrop(dragDropOptions) {
+	const dragDrop = new DragDrop(dragDropOptions);
+
+	dragDrop.on('dispose', _stopInterceptGetRegion);
+	dragDrop.on(Drag.Events.START, _interceptGetRegion);
+	dragDrop.on(DragDrop.Events.END, _stopInterceptGetRegion);
+
+	return dragDrop;
+}
+
+export {initializeDragDrop};
