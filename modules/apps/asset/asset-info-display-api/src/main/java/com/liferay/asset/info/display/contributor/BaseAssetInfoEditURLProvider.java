d77bfd849232ccd52050c3c9921c73e1e4df3fc6
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-04-10 15:40:28 +0200

    LPS-92603 New interface and implementation so that models can contribute their edit URL when being previewed in a info display page. While we are at it, also create the tracker to get them by className

---
+++ modules/apps/asset/asset-info-display-api/src/main/java/com/liferay/asset/info/display/contributor/BaseAssetInfoEditURLProvider.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.info.display.contributor;
+
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.info.display.contributor.InfoEditURLProvider;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class BaseAssetInfoEditURLProvider
+	implements InfoEditURLProvider<AssetEntry> {
+
+	@Override
+	public String getURL(AssetEntry assetEntry, HttpServletRequest request)
+		throws Exception {
+
+		if (assetEntry == null) {
+			return StringPool.BLANK;
+		}
+
+		AssetRendererFactory assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				assetEntry.getClassName());
+
+		if (assetRendererFactory == null) {
+			return StringPool.BLANK;
+		}
+
+		AssetRenderer assetRenderer = assetRendererFactory.getAssetRenderer(
+			assetEntry.getClassPK());
+
+		if (assetRenderer == null) {
+			return StringPool.BLANK;
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (!assetRenderer.hasEditPermission(
+				themeDisplay.getPermissionChecker())) {
+
+			return StringPool.BLANK;
+		}
+
+		PortletURL editAssetEntryURL = assetRenderer.getURLEdit(
+			request, LiferayWindowState.NORMAL, themeDisplay.getURLCurrent());
+
+		if (editAssetEntryURL == null) {
+			return StringPool.BLANK;
+		}
+
+		editAssetEntryURL.setParameter(
+			"portletResource", assetRendererFactory.getPortletId());
+
+		return editAssetEntryURL.toString();
+	}
+
+}
