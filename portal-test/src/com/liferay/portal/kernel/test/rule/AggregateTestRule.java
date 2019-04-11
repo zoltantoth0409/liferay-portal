fdc8cbdbe74b80d3ecb12d3007fe742f93944937
Matthew Tambara <matthew.tambara@liferay.com>
2019-04-11 09:49:41 -0700

    LPS-93697 Remove deprecated test rule

--- portal-test/src/com/liferay/portal/kernel/test/rule/AggregateTestRule.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/AggregateTestRule.java
@@ -61,7 +61,6 @@ public class AggregateTestRule implements TestRule {
 	private static final String[] _ORDERED_RULE_CLASS_NAMES = {
 		TimeoutTestRule.class.getName(), HeapDumpTestRule.class.getName(),
 		CodeCoverageAssertor.class.getName(), NewEnvTestRule.class.getName(),
-		"com.liferay.portal.test.rule.PortalExecutorManagerTestRule",
 		AssumeTestRule.class.getName(),
 		"com.liferay.portal.test.rule.LiferayIntegrationTestRule",
 		"com.liferay.portal.test.rule.HypersonicServerTestRule",
