d61d69fda442b47ced847e5c5b9b564d31e531ee
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-04-11 14:19:59 +0200

    LPS-92541 Adding filtering

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-change-tracking/src/main/java/com/liferay/dynamic/data/mapping/change/tracking/internal/service/CTDDMStructureVersionLocalServiceWrapper.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-change-tracking/src/main/java/com/liferay/dynamic/data/mapping/change/tracking/internal/service/CTDDMStructureVersionLocalServiceWrapper.java
@@ -14,14 +14,20 @@
 
 package com.liferay.dynamic.data.mapping.change.tracking.internal.service;
 
+import com.liferay.change.tracking.CTEngineManager;
 import com.liferay.change.tracking.CTManager;
 import com.liferay.change.tracking.model.CTEntry;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.model.DDMStructureVersion;
 import com.liferay.dynamic.data.mapping.service.DDMStructureVersionLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMStructureVersionLocalServiceWrapper;
+import com.liferay.journal.model.JournalArticle;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.util.Portal;
 
 import java.util.Optional;
 
@@ -49,30 +55,75 @@ public class CTDDMStructureVersionLocalServiceWrapper
 	public DDMStructureVersion getLatestStructureVersion(long structureId)
 		throws PortalException {
 
+		DDMStructureVersion latestStructureVersion =
+			super.getLatestStructureVersion(structureId);
+
+		if (!_isChangeTrackingEnabled(latestStructureVersion)) {
+			return latestStructureVersion;
+		}
+
 		Optional<CTEntry> ctEntryOptional =
 			_ctManager.getLatestModelChangeCTEntryOptional(
 				PrincipalThreadLocal.getUserId(), structureId);
 
-		if (ctEntryOptional.isPresent()) {
-			Optional<DDMStructureVersion> ddmStructureVersionOptional =
-				ctEntryOptional.map(
-					CTEntry::getModelClassPK
-				).map(
-					_ddmStructureVersionLocalService::fetchDDMStructureVersion
-				);
+		return ctEntryOptional.map(
+			CTEntry::getModelClassPK
+		).map(
+			_ddmStructureVersionLocalService::fetchDDMStructureVersion
+		).orElse(
+			latestStructureVersion
+		);
+	}
 
-			if (ddmStructureVersionOptional.isPresent()) {
-				return ddmStructureVersionOptional.get();
-			}
+	private boolean _isChangeTrackingEnabled(
+		DDMStructureVersion ddmStructureVersion) {
+
+		if (!_ctEngineManager.isChangeTrackingEnabled(
+				ddmStructureVersion.getCompanyId())) {
+
+			return false;
 		}
 
-		return super.getLatestStructureVersion(structureId);
+		DDMStructure ddmStructure = null;
+
+		try {
+			ddmStructure = ddmStructureVersion.getStructure();
+		}
+		catch (PortalException pe) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to find structure with id " +
+						ddmStructureVersion.getStructureId(),
+					pe);
+			}
+
+			return false;
+		}
+
+		long journalClassNameId = _portal.getClassNameId(JournalArticle.class);
+
+		if ((ddmStructure.getClassNameId() != journalClassNameId) ||
+			!"BASIC-WEB-CONTENT".equals(ddmStructure.getStructureKey())) {
+
+			return false;
+		}
+
+		return true;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CTDDMStructureVersionLocalServiceWrapper.class);
+
+	@Reference
+	private CTEngineManager _ctEngineManager;
 
 	@Reference
 	private CTManager _ctManager;
 
 	@Reference
 	private DDMStructureVersionLocalService _ddmStructureVersionLocalService;
+
+	@Reference
+	private Portal _portal;
 
 }
