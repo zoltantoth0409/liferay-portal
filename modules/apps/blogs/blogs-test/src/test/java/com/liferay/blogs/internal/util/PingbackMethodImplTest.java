d2aaeb9d3d8ed30c5ee50191b6d35486f16409fe
Brian Chan <brian.chan@liferay.com>
2019-04-10 20:20:08 -0700

    LPS-92948 Sort

--- modules/apps/blogs/blogs-test/src/test/java/com/liferay/blogs/internal/util/PingbackMethodImplTest.java
+++ modules/apps/blogs/blogs-test/src/test/java/com/liferay/blogs/internal/util/PingbackMethodImplTest.java
@@ -401,21 +401,16 @@ public class PingbackMethodImplTest extends PowerMockito {
 		PingbackMethodImpl pingbackMethodImpl = new PingbackMethodImpl();
 
 		ReflectionTestUtil.setFieldValue(
-			pingbackMethodImpl, "_commentManager", _commentManager);
-
-		ReflectionTestUtil.setFieldValue(
-			pingbackMethodImpl, "_http", HttpUtil.getHttp());
-
-		ReflectionTestUtil.setFieldValue(
-			pingbackMethodImpl, "_portal", PortalUtil.getPortal());
-
-		ReflectionTestUtil.setFieldValue(
 			pingbackMethodImpl, "_blogsEntryLocalService",
 			_blogsEntryLocalService);
-
+		ReflectionTestUtil.setFieldValue(
+			pingbackMethodImpl, "_commentManager", _commentManager);
+		ReflectionTestUtil.setFieldValue(
+			pingbackMethodImpl, "_http", HttpUtil.getHttp());
+		ReflectionTestUtil.setFieldValue(
+			pingbackMethodImpl, "_portal", PortalUtil.getPortal());
 		ReflectionTestUtil.setFieldValue(
 			pingbackMethodImpl, "_portletLocalService", _portletLocalService);
-
 		ReflectionTestUtil.setFieldValue(
 			pingbackMethodImpl, "_userLocalService", _userLocalService);
 
