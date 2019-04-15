90ae4c364bb821aa23a1502b07d00e93773f7a18
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-04-15 09:28:10 +0200

    LPS-94014 Fix condition

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/soy/SelectLayoutTag.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/soy/SelectLayoutTag.java
@@ -214,7 +214,7 @@ public class SelectLayoutTag extends ComponentRendererTag {
 
 		for (Layout layout : layouts) {
 			if ((layout.isHidden() && !_isShowHiddenLayouts()) ||
-				(!Objects.equals(
+				(Objects.equals(
 					layout.getType(), LayoutConstants.TYPE_CONTENT) &&
 				 Objects.equals(
 					 layout.getCreateDate(), layout.getPublishDate())) ||
