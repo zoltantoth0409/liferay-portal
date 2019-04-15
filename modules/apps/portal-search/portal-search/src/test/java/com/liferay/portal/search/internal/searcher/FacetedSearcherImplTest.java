440935de4471eb781749396b68f1a6504664eb48
André de Oliveira <andre.oliveira@liferay.com>
2019-04-15 01:33:48 -0700

    LPS-84696 portal-search: Test: accept federated searches
    - IndexSearcherHelper with lambda friendly unchecked exceptions only
    
    https://issues.liferay.com/browse/LPS-84696

--- modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/searcher/FacetedSearcherImplTest.java
+++ modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/searcher/FacetedSearcherImplTest.java
@@ -17,7 +17,6 @@ package com.liferay.portal.search.internal.searcher;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.search.ExpandoQueryContributor;
 import com.liferay.portal.kernel.search.Hits;
-import com.liferay.portal.kernel.search.IndexSearcherHelper;
 import com.liferay.portal.kernel.search.IndexerRegistry;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchEngineHelper;
