d85c7dd7146b8b02af338ad64d59a56bc2c8bef1
Kimberly Chau <kimberly.chau@liferay.com>
2019-04-17 10:47:24 -0700

    LPS-92354 Add check to see if the compiled code contains errors, if true, return the code
    (cherry picked from commit 4503f75918279ff7c5e208f2536ad1ede9e34650)

--- modules/apps/frontend-js/frontend-js-minifier/src/main/java/com/frontend/js/minifier/GoogleJavaScriptMinifier.java
+++ modules/apps/frontend-js/frontend-js-minifier/src/main/java/com/frontend/js/minifier/GoogleJavaScriptMinifier.java
@@ -70,6 +70,10 @@ public class GoogleJavaScriptMinifier implements JavaScriptMinifier {
 				SourceFile.fromCode("extern", StringPool.BLANK), sourceFile,
 				compilerOptions);
 
+			if (compiler.hasErrors()) {
+				return content;
+			}
+
 			return compiler.toSource();
 		}
 		finally {
