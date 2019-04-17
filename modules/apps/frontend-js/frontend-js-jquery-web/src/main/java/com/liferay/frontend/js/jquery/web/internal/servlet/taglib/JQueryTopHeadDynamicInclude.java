5de54567a6d43e6bae7e48451673136f30cdeebf
Brian Chan <brian.chan@liferay.com>
2019-04-17 11:48:58 -0700

    LPS-94222 Optimize for readability over performance (unless we measure it and it makes a real difference)

--- modules/apps/frontend-js/frontend-js-jquery-web/src/main/java/com/liferay/frontend/js/jquery/web/internal/servlet/taglib/JQueryTopHeadDynamicInclude.java
+++ modules/apps/frontend-js/frontend-js-jquery-web/src/main/java/com/liferay/frontend/js/jquery/web/internal/servlet/taglib/JQueryTopHeadDynamicInclude.java
@@ -74,8 +74,8 @@ public class JQueryTopHeadDynamicInclude extends BaseDynamicInclude {
 			sb.append("<script data-senna-track=\"permanent\" src=\"");
 			sb.append(
 				_portal.getStaticResourceURL(
-					request, _comboContextPath, "minifierType=js",
-					_lastModified));
+					request, _portal.getPathContext() + "/combo",
+					"minifierType=js", _lastModified));
 
 			for (String fileName : _FILE_NAMES) {
 				sb.append("&");
@@ -106,15 +106,6 @@ public class JQueryTopHeadDynamicInclude extends BaseDynamicInclude {
 		dynamicIncludeRegistry.register("/html/common/themes/top_head.jsp#pre");
 	}
 
-	@Reference(unbind = "-")
-	public void setPortal(Portal portal) {
-		String pathContext = portal.getPathContext();
-
-		_comboContextPath = pathContext.concat("/combo");
-
-		_portal = portal;
-	}
-
 	@Activate
 	@Modified
 	protected void activate(
@@ -138,9 +129,10 @@ public class JQueryTopHeadDynamicInclude extends BaseDynamicInclude {
 	private AbsolutePortalURLBuilderFactory _absolutePortalURLBuilderFactory;
 
 	private BundleContext _bundleContext;
-	private String _comboContextPath;
 	private volatile JSJQueryConfiguration _jsJQueryConfiguration;
 	private long _lastModified;
+
+	@Reference
 	private Portal _portal;
 
 }
