25a0fead6b6e2ed541ac536d8c74eb2f7ae2f437
tina.tian <tina.tian@liferay.com>
2019-04-17 17:33:46 -0700

    LPS-91534 Remove dead code, portalCache won't be null in any case

--- modules/apps/portal-cache/portal-cache-multiple/src/main/java/com/liferay/portal/cache/multiple/internal/cluster/link/messaging/ClusterLinkPortalCacheClusterListener.java
+++ modules/apps/portal-cache/portal-cache-multiple/src/main/java/com/liferay/portal/cache/multiple/internal/cluster/link/messaging/ClusterLinkPortalCacheClusterListener.java
@@ -74,10 +74,6 @@ public class ClusterLinkPortalCacheClusterListener extends BaseMessageListener {
 				portalCacheManager.getPortalCache(
 					portalCacheClusterEvent.getPortalCacheName());
 
-		if (portalCache == null) {
-			return;
-		}
-
 		PortalCacheClusterEventType portalCacheClusterEventType =
 			portalCacheClusterEvent.getEventType();
 
