d125d53d316baccc7f8e19b492deece22422d251
Brian Chan <brian.chan@liferay.com>
2019-04-22 10:22:14 -0700

    LPS-91966 Hugo SF, please apply all uses of setDesiredItemSelectorReturnTypes. I gave you many samples 2.) Return ItemSelectorCriterion where possible, and not XxxItemSelectorCriterion

--- modules/apps/wiki/wiki-editor-configuration/src/main/java/com/liferay/wiki/editor/configuration/internal/BaseWikiLinksCKEditorConfigContributor.java
+++ modules/apps/wiki/wiki-editor-configuration/src/main/java/com/liferay/wiki/editor/configuration/internal/BaseWikiLinksCKEditorConfigContributor.java
@@ -93,37 +93,27 @@ public abstract class BaseWikiLinksCKEditorConfigContributor
 
 	protected abstract ItemSelectorReturnType getItemSelectorReturnType();
 
-	protected WikiAttachmentItemSelectorCriterion
+	protected ItemSelectorCriterion
 		getWikiAttachmentItemSelectorCriterion(long wikiPageResourcePrimKey) {
 
-		WikiAttachmentItemSelectorCriterion itemSelectorCriterion =
+		ItemSelectorCriterion itemSelectorCriterion =
 			new WikiAttachmentItemSelectorCriterion(wikiPageResourcePrimKey);
 
-		List<ItemSelectorReturnType> desiredItemSelectorReturnTypes =
-			new ArrayList<>();
-
-		desiredItemSelectorReturnTypes.add(new URLItemSelectorReturnType());
-
 		itemSelectorCriterion.setDesiredItemSelectorReturnTypes(
-			desiredItemSelectorReturnTypes);
+			new URLItemSelectorReturnType());
 
 		return itemSelectorCriterion;
 	}
 
-	protected WikiPageItemSelectorCriterion getWikiPageItemSelectorCriterion(
+	protected ItemSelectorCriterion getWikiPageItemSelectorCriterion(
 		long nodeId) {
 
-		WikiPageItemSelectorCriterion itemSelectorCriterion =
+		ItemSelectorCriterion itemSelectorCriterion =
 			new WikiPageItemSelectorCriterion(
 				nodeId, WorkflowConstants.STATUS_APPROVED);
 
-		List<ItemSelectorReturnType> desiredItemSelectorReturnTypes =
-			new ArrayList<>();
-
-		desiredItemSelectorReturnTypes.add(getItemSelectorReturnType());
-
 		itemSelectorCriterion.setDesiredItemSelectorReturnTypes(
-			desiredItemSelectorReturnTypes);
+			getItemSelectorReturnType());
 
 		return itemSelectorCriterion;
 	}
