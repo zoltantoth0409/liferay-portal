57225def6982ad20db3721f81584099b3c8befb5
Gabriel Albuquerque <gwas@cin.ufpe.br>
2019-04-22 18:19:33 -0300

    LPS-94505 Create Validation Field Type

---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/ValidationFieldType.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.field.type.v1_0;
+
+import com.liferay.data.engine.rest.dto.v1_0.DataDefinitionField;
+import com.liferay.data.engine.rest.internal.field.type.v1_0.util.CustomPropertyUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.template.soy.data.SoyDataFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+public class ValidationFieldType extends BaseFieldType {
+
+	public ValidationFieldType(
+		DataDefinitionField dataDefinitionField,
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse,
+		SoyDataFactory soyDataFactory) {
+
+		super(
+			dataDefinitionField, httpServletRequest, httpServletResponse,
+			soyDataFactory);
+	}
+
+	@Override
+	protected void addContext(Map<String, Object> context) {
+		context.put("value", _getValue());
+	}
+
+	private Map<String, String> _getValue() {
+		Map<String, String> value = new HashMap<>();
+
+		String valueString = CustomPropertyUtil.getString(
+			dataDefinitionField.getCustomProperties(), "value");
+
+		if (Validator.isNotNull(valueString)) {
+			try {
+				JSONObject valueJSONObject = JSONFactoryUtil.createJSONObject(
+					valueString);
+
+				value.put(
+					"errorMessage", valueJSONObject.getString("errorMessage"));
+				value.put(
+					"expression", valueJSONObject.getString("expression"));
+			}
+			catch (JSONException jsone) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(jsone, jsone);
+				}
+			}
+		}
+		else {
+			value.put("errorMessage", StringPool.BLANK);
+			value.put("expression", StringPool.BLANK);
+		}
+
+		return value;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ValidationFieldType.class);
+
+}
