212eaf90849e8262510588249fdb9901861de3a3
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-04-22 10:55:44 +0200

    LPS-86700 Moves code to jsps

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/configuration/icon/ViewJournalSourcePortletConfigurationIcon.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/configuration/icon/ViewJournalSourcePortletConfigurationIcon.java
@@ -18,24 +18,21 @@ import com.liferay.journal.constants.JournalPortletKeys;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.journal.web.internal.portlet.action.ActionUtil;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.BaseJSPPortletConfigurationIcon;
 import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
-import com.liferay.portal.kernel.theme.PortletDisplay;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.AggregateResourceBundle;
-import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.io.IOException;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -52,41 +49,17 @@ import org.osgi.service.component.annotations.Reference;
 	service = PortletConfigurationIcon.class
 )
 public class ViewJournalSourcePortletConfigurationIcon
-	extends BasePortletConfigurationIcon {
+	extends BaseJSPPortletConfigurationIcon {
+
+	@Override
+	public String getJspPath() {
+		return "/configuration/icon/view_source_icon.jsp";
+	}
 
 	@Override
 	public String getMessage(PortletRequest portletRequest) {
 		return LanguageUtil.get(
 			getResourceBundle(getLocale(portletRequest)), "view-source");
-	}
-
-	@Override
-	public String getOnClick(
-		PortletRequest portletRequest, PortletResponse portletResponse) {
-
-		JournalArticle article = null;
-
-		try {
-			article = ActionUtil.getArticle(portletRequest);
-		}
-		catch (Exception e) {
-		}
-
-		if (article == null) {
-			return StringPool.BLANK;
-		}
-
-		return "var sourceModal = " + getWindowJS(portletRequest, article) +
-			" return false;";
-	}
-
-	@Override
-	public ResourceBundle getResourceBundle(Locale locale) {
-		ResourceBundle classResourceBundle = ResourceBundleUtil.getBundle(
-			"content.Language", locale, getClass());
-
-		return new AggregateResourceBundle(
-			classResourceBundle, _portal.getResourceBundle(locale));
 	}
 
 	@Override
@@ -102,11 +75,28 @@ public class ViewJournalSourcePortletConfigurationIcon
 	}
 
 	@Override
+	public boolean include(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		try {
+			PortletRequest portletRequest =
+				(PortletRequest)request.getAttribute(
+					JavaConstants.JAVAX_PORTLET_REQUEST);
+
+			request.setAttribute(
+				WebKeys.JOURNAL_ARTICLE, _getArticle(portletRequest));
+		}
+		catch (Exception e) {
+		}
+
+		return super.include(request, response);
+	}
+
+	@Override
 	public boolean isShow(PortletRequest portletRequest) {
 		try {
-			JournalArticle article = ActionUtil.getArticle(portletRequest);
-
-			if (article != null) {
+			if (_getArticle(portletRequest) != null) {
 				return true;
 			}
 		}
@@ -117,54 +107,26 @@ public class ViewJournalSourcePortletConfigurationIcon
 	}
 
 	@Override
-	public boolean isToolTip() {
-		return false;
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.journal.web)", unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
 	}
 
-	protected String getDialogJS(JournalArticle article) {
-		StringBundler sb = new StringBundler(6);
+	private JournalArticle _getArticle(PortletRequest portletRequest)
+		throws Exception {
 
-		sb.append("{bodyContent: '<pre>");
-		sb.append(HtmlUtil.escapeJS(HtmlUtil.escape(article.getContent())));
-		sb.append("</pre>', modal: true, toolbars: {footer: ");
-		sb.append("[{label:Liferay.Language.get('close'), on: {click: ");
-		sb.append("function(event) {event.domEvent.preventDefault(); ");
-		sb.append("sourceModal.hide();}}}]}}");
+		if (_article != null) {
+			return _article;
+		}
 
-		return sb.toString();
+		_article = ActionUtil.getArticle(portletRequest);
+
+		return _article;
 	}
 
-	protected String getWindowJS(
-		PortletRequest portletRequest, JournalArticle article) {
-
-		StringBundler sb = new StringBundler(14);
-
-		sb.append("Liferay.Util.Window.getWindow({bodyCssClass: ");
-		sb.append("'dialog-with-footer', destroyOnHide: true, dialog: ");
-		sb.append(getDialogJS(article));
-		sb.append(", id: '");
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
-
-		sb.append(HtmlUtil.escape(portletDisplay.getNamespace()));
-
-		sb.append("viewSource', namespace: '");
-		sb.append(portletDisplay.getNamespace());
-		sb.append("', portlet: '#p_p_id_");
-		sb.append(portletDisplay.getId());
-		sb.append("_', portletId: '");
-		sb.append(portletDisplay.getId());
-		sb.append("', title: '");
-		sb.append(
-			HtmlUtil.escapeJS(
-				HtmlUtil.escape(article.getTitle(themeDisplay.getLocale()))));
-		sb.append("'});");
-
-		return sb.toString();
-	}
+	private JournalArticle _article;
 
 	@Reference
 	private JournalArticleLocalService _journalArticleLocalService;
---
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/configuration/icon/view_source_icon.jsp
@@ -0,0 +1,52 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<liferay-ui:icon
+	id="viewResourcesButton"
+	message="view-source"
+	url="javascript:;"
+/>
+
+<%
+JournalArticle article = (JournalArticle)request.getAttribute(WebKeys.JOURNAL_ARTICLE);
+%>
+
+<script>
+	var viewResourcesButton = document.getElementById('<portlet:namespace />viewResourcesButton');
+
+	if (viewResourcesButton) {
+		viewResourcesButton.addEventListener(
+			'click',
+			function(event) {
+				Liferay.Util.openWindow(
+					{
+						dialog: {
+							destroyOnHide: true,
+							modal: true
+						},
+						dialogIframe: {
+							bodyCssClass: 'dialog-with-footer'
+						},
+						title: '<%= HtmlUtil.escapeJS(HtmlUtil.escape(article.getTitle(themeDisplay.getLocale()))) %>',
+						uri: '<portlet:renderURL windowState="<%= LiferayWindowState.POP_UP.toString() %>"><portlet:param name="mvcPath" value="/configuration/icon/view_source.jsp" /><portlet:param name="redirect" value="<%= currentURL %>" /><portlet:param name="groupId" value="<%= String.valueOf(article.getGroupId()) %>" /><portlet:param name="articleId" value="<%= article.getArticleId() %>" /></portlet:renderURL>'
+					}
+				);
+			}
+		);
+	}
+</script>
