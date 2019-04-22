fbc9759db4c0f6fd45788e4dde5d8522b62a9e90
Vagner Bernal de Castro <vagner.castro@gs.liferay.com>
2019-04-22 17:56:29 -0300

    LPS-94506 blogs-test: Create BlogsEntryMultiLanguageSearchTest

---
+++ modules/apps/blogs/blogs-test/src/testIntegration/java/com/liferay/blogs/search/test/BlogsEntryMultiLanguageSearchTest.java
@@ -0,0 +1,179 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.search.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.search.test.util.FieldValuesAssert;
+import com.liferay.portal.search.test.util.IndexerFixture;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.users.admin.test.util.search.UserSearchFixture;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Vagner B.C
+ */
+@RunWith(Arquillian.class)
+public class BlogsEntryMultiLanguageSearchTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE,
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		setUpUserSearchFixture();
+
+		setUpBlogsEntryFixture();
+
+		setUpBlogsEntryIndexerFixture();
+
+		_defaultLocale = LocaleThreadLocal.getDefaultLocale();
+	}
+
+	@After
+	public void tearDown() {
+		LocaleThreadLocal.setDefaultLocale(_defaultLocale);
+	}
+
+	@Test
+	public void testChineseTitle() throws Exception {
+		_testLocaleKeywords(LocaleUtil.CHINA, "你好");
+	}
+
+	@Test
+	public void testEnglishTitle() throws Exception {
+		_testLocaleKeywords(LocaleUtil.US, "title");
+	}
+
+	@Test
+	public void testJapaneseTitle() throws Exception {
+		_testLocaleKeywords(LocaleUtil.JAPAN, "東京");
+	}
+
+	protected void assertFieldValues(
+		String prefix, Locale locale, Map<String, String> map,
+		String searchTerm) {
+
+		Document document = blogsEntryIndexerFixture.searchOnlyOne(
+			searchTerm, locale);
+
+		FieldValuesAssert.assertFieldValues(map, prefix, document, searchTerm);
+	}
+
+	protected void setTestLocale(Locale locale) throws Exception {
+		blogsEntryFixture.updateDisplaySettings(locale);
+
+		LocaleThreadLocal.setDefaultLocale(locale);
+	}
+
+	protected void setUpBlogsEntryFixture() {
+		blogsEntryFixture = new BlogsEntryFixture(_group);
+
+		_blogsEntries = blogsEntryFixture.getBlogsEntries();
+	}
+
+	protected void setUpBlogsEntryIndexerFixture() {
+		blogsEntryIndexerFixture = new IndexerFixture<>(BlogsEntry.class);
+	}
+
+	protected void setUpUserSearchFixture() throws Exception {
+		userSearchFixture = new UserSearchFixture();
+
+		userSearchFixture.setUp();
+
+		_group = userSearchFixture.addGroup();
+
+		_groups = userSearchFixture.getGroups();
+
+		_users = userSearchFixture.getUsers();
+	}
+
+	protected BlogsEntryFixture blogsEntryFixture;
+	protected IndexerFixture<BlogsEntry> blogsEntryIndexerFixture;
+	protected UserSearchFixture userSearchFixture;
+
+	private Map<String, String> _getMapResult(BlogsEntry blogsEntry) {
+		HashMap<String, String> map = new HashMap<>();
+
+		String title = blogsEntry.getTitle();
+
+		map.put(Field.TITLE, title);
+		map.put(Field.getSortableFieldName(Field.TITLE), title);
+
+		for (Locale locale :
+				LanguageUtil.getAvailableLocales(blogsEntry.getGroupId())) {
+
+			String languageId = LocaleUtil.toLanguageId(locale);
+
+			map.put(
+				LocalizationUtil.getLocalizedName(Field.TITLE, languageId),
+				title);
+		}
+
+		return map;
+	}
+
+	private void _testLocaleKeywords(Locale locale, String keywords)
+		throws Exception {
+
+		setTestLocale(locale);
+
+		BlogsEntry blogsEntry = blogsEntryFixture.createBlogsEntry(keywords);
+
+		assertFieldValues(
+			Field.TITLE, locale, _getMapResult(blogsEntry), keywords);
+	}
+
+	@DeleteAfterTestRun
+	private List<BlogsEntry> _blogsEntries;
+
+	private Locale _defaultLocale;
+	private Group _group;
+
+	@DeleteAfterTestRun
+	private List<Group> _groups;
+
+	@DeleteAfterTestRun
+	private List<User> _users;
+
+}
