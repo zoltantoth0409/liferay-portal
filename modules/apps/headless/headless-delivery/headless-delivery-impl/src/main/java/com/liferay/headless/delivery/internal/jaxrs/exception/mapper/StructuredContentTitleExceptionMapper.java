8bb58f5ce74231ea2b3bbb1ecec5168157f4463a
Victor Galan <victor.galan@liferay.com>
2019-04-23 17:29:25 +0200

    LPS-94557 Add exception mapper for StructuredContentTitleExceptionMapper

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/StructuredContentTitleExceptionMapper.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.jaxrs.exception.mapper;
+
+import com.liferay.journal.exception.ArticleTitleException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Converts any {@code ArticleTitleException} to a {@code 400} error.
+ *
+ * @author Víctor Galán
+ * @review
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Delivery.StructuredContentTitleExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class StructuredContentTitleExceptionMapper
+	implements ExceptionMapper<ArticleTitleException> {
+
+	@Override
+	public Response toResponse(ArticleTitleException ate) {
+		return Response.status(
+			400
+		).type(
+			MediaType.TEXT_PLAIN
+		).entity(
+			ate.getMessage()
+		).build();
+	}
+
+}
