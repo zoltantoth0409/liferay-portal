2577f42a102eaabf7d4594ae0f90e45827ff2209
Iván Zaera Avellón <izaera@gmail.com>
2019-04-23 12:34:13 +0200

    LPS-83949 Make frontend-js-polyfill-babel-web register only under the old loader

--- modules/apps/frontend-js/frontend-js-polyfill-babel-web/src/main/resources/META-INF/resources/config.js
+++ modules/apps/frontend-js/frontend-js-polyfill-babel-web/src/main/resources/META-INF/resources/config.js
@@ -1,8 +1,10 @@
-Liferay.Loader.addModule(
-	{
-		dependencies: [],
-		exports: '_babelPolyfill',
-		name: 'polyfill-babel',
-		path: MODULE_PATH + '/browser-polyfill.min.js'
-	}
-);
+if (Liferay.Loader.version().indexOf('3.') === 0) {
+	Liferay.Loader.addModule(
+		{
+			dependencies: [],
+			exports: '_babelPolyfill',
+			name: 'polyfill-babel',
+			path: MODULE_PATH + '/browser-polyfill.min.js'
+		}
+	);
+}
