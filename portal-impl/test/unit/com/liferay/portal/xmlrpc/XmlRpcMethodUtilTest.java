aaed69c14ecfc2533c2a22d646da6524a6e20fc4
Leon Chi <leon.chi@liferay.com>
2019-04-23 08:50:50 +0800

    LPS-94205 Make XmlRpcMethodUtil unit test

---
+++ portal-impl/test/unit/com/liferay/portal/xmlrpc/XmlRpcMethodUtilTest.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.xmlrpc;
+
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.xmlrpc.Method;
+import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceRegistration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Leon Chi
+ */
+public class XmlRpcMethodUtilTest {
+
+	@Test
+	public void testNoReturn() {
+		Method xmlRpcMethod = (Method)ProxyUtil.newProxyInstance(
+			Method.class.getClassLoader(), new Class<?>[] {Method.class},
+			(proxy, method, args) -> {
+				if ("getToken".equals(method.getName())) {
+					return _TOKEN;
+				}
+
+				if ("getMethodName".equals(method.getName())) {
+					return _METHOD_NAME;
+				}
+
+				return null;
+			});
+
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		ServiceRegistration<Method> serviceRegistration =
+			registry.registerService(Method.class, xmlRpcMethod);
+
+		try {
+			Assert.assertSame(
+				xmlRpcMethod, XmlRpcMethodUtil.getMethod(_TOKEN, _METHOD_NAME));
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
+	}
+
+	private static final String _METHOD_NAME = "METHOD_NAME";
+
+	private static final String _TOKEN = "TOKEN";
+
+}
