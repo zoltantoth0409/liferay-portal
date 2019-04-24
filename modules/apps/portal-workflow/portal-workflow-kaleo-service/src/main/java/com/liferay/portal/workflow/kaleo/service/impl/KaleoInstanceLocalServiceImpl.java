3ba6f002009cc39a2215ecf74b7281d50c314905
Gergely Száz <gergely.szaz@liferay.com>
2019-04-24 19:31:30 +0200

    Revert "LPS-91949 Fix getSortsFromComparator operation"
    This reverts commit 0d7a3a623d2e5c86badf6069f2e5b3b2c31a4665.

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoInstanceLocalServiceImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoInstanceLocalServiceImpl.java
@@ -27,7 +27,6 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Document;
-import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
@@ -36,6 +35,7 @@ import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.workflow.kaleo.exception.NoSuchInstanceException;
@@ -50,7 +50,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -548,15 +547,14 @@ public class KaleoInstanceLocalServiceImpl
 			orderByComparator.getOrderByFields());
 
 		return stream.map(
-			orderByCol -> {
-				String fieldName = _fieldNameOrderByColMap.getOrDefault(
-					orderByCol, orderByCol);
+			field -> {
+				if (StringUtil.endsWith(field, "date")) {
+					return new Sort(
+						field, Sort.LONG_TYPE,
+						!orderByComparator.isAscending());
+				}
 
-				int sortType = _fieldNameSortTypeMap.getOrDefault(
-					fieldName, Sort.STRING_TYPE);
-
-				return new Sort(
-					fieldName, sortType, !orderByComparator.isAscending());
+				return new Sort(field, !orderByComparator.isAscending());
 			}
 		).toArray(
 			Sort[]::new
@@ -565,27 +563,5 @@ public class KaleoInstanceLocalServiceImpl
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		KaleoInstanceLocalServiceImpl.class);
-
-	private static final Map<String, String> _fieldNameOrderByColMap =
-		new HashMap<String, String>() {
-			{
-				put("completed", KaleoInstanceTokenField.COMPLETED);
-				put("completionDate", KaleoInstanceTokenField.COMPLETION_DATE);
-				put("createDate", Field.CREATE_DATE);
-				put(
-					"kaleoInstanceId",
-					KaleoInstanceTokenField.KALEO_INSTANCE_ID);
-				put("modifiedDate", Field.MODIFIED_DATE);
-				put("state", KaleoInstanceTokenField.CURRENT_KALEO_NODE_NAME);
-			}
-		};
-	private static final Map<String, Integer> _fieldNameSortTypeMap =
-		new HashMap<String, Integer>() {
-			{
-				put(Field.CREATE_DATE, Sort.LONG_TYPE);
-				put(Field.MODIFIED_DATE, Sort.LONG_TYPE);
-				put(KaleoInstanceTokenField.COMPLETION_DATE, Sort.LONG_TYPE);
-			}
-		};
 
 }
