ec3567f1defea47abbd245c944105d8f57be9404
Pavel Savinov <pavel.savinov@liferay.com>
2019-04-24 12:01:29 +0200

    LPS-94497 Mark new layout as published setting appropriate publish date

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddSimpleLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddSimpleLayoutMVCActionCommand.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.LayoutTypePortlet;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.LayoutService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
@@ -34,6 +35,7 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PropertiesParamUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -119,6 +121,15 @@ public class AddSimpleLayoutMVCActionCommand
 				stagingGroupId, privateLayout, layout.getLayoutId(),
 				layout.getTypeSettingsProperties());
 
+			Layout draftLayout = _layoutLocalService.fetchLayout(
+				_portal.getClassNameId(Layout.class), layout.getPlid());
+
+			if (draftLayout != null) {
+				_layoutLocalService.updateLayout(
+					groupId, privateLayout, layout.getLayoutId(),
+					draftLayout.getModifiedDate());
+			}
+
 			MultiSessionMessages.add(actionRequest, "layoutAdded", layout);
 
 			String redirectURL = getRedirectURL(
@@ -157,6 +168,12 @@ public class AddSimpleLayoutMVCActionCommand
 	private LayoutExceptionRequestHandler _layoutExceptionRequestHandler;
 
 	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
 	private LayoutService _layoutService;
+
+	@Reference
+	private Portal _portal;
 
 }
