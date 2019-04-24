23a3d1a42e755ffd3c2957d41f657e11b8bd719a
Inácio Nery <inacio.nery@liferay.com>
2019-04-24 11:19:04 -0300

    LPS-86652 | LPS-94375 Add to the Instance index the asset title, asset type and user name

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/InstanceWorkflowMetricsIndexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/InstanceWorkflowMetricsIndexer.java
@@ -14,13 +14,23 @@
 
 package com.liferay.portal.workflow.metrics.internal.search.index;
 
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.DocumentImpl;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.workflow.kaleo.model.KaleoDefinition;
 import com.liferay.portal.workflow.kaleo.model.KaleoInstance;
 import com.liferay.portal.workflow.kaleo.service.KaleoInstanceLocalService;
@@ -28,6 +38,9 @@ import com.liferay.portal.workflow.kaleo.service.KaleoInstanceLocalService;
 import java.time.Duration;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -47,6 +60,24 @@ public class InstanceWorkflowMetricsIndexer extends BaseWorkflowMetricsIndexer {
 				kaleoInstance.getCompanyId(),
 				kaleoInstance.getKaleoDefinitionVersionId(),
 				kaleoInstance.getKaleoInstanceId()));
+
+		AssetEntry assetEntry = _getAssetEntry(kaleoInstance);
+
+		if (assetEntry != null) {
+			document.addLocalizedKeyword(
+				"assetTitle",
+				LocalizationUtil.populateLocalizationMap(
+					assetEntry.getTitleMap(), assetEntry.getDefaultLanguageId(),
+					assetEntry.getGroupId()),
+				false, true);
+
+			document.addLocalizedKeyword(
+				"assetType",
+				_createAssetTypeLocalizationMap(
+					assetEntry.getClassName(), assetEntry.getGroupId()),
+				false, true);
+		}
+
 		document.addKeyword("className", kaleoInstance.getClassName());
 		document.addKeyword("classPK", kaleoInstance.getClassPK());
 		document.addKeyword("companyId", kaleoInstance.getCompanyId());
@@ -84,6 +115,7 @@ public class InstanceWorkflowMetricsIndexer extends BaseWorkflowMetricsIndexer {
 		}
 
 		document.addKeyword("userId", kaleoInstance.getUserId());
+		document.addKeyword("userName", kaleoInstance.getUserName());
 		document.addKeyword(
 			"version",
 			StringBundler.concat(
@@ -126,6 +158,55 @@ public class InstanceWorkflowMetricsIndexer extends BaseWorkflowMetricsIndexer {
 
 		actionableDynamicQuery.performActions();
 	}
+
+	private Map<Locale, String> _createAssetTypeLocalizationMap(
+		String className, long groupId) {
+
+		Map<Locale, String> localizationMap = new HashMap<>();
+
+		for (Locale availableLocale :
+				LanguageUtil.getAvailableLocales(groupId)) {
+
+			localizationMap.put(
+				availableLocale,
+				ResourceActionsUtil.getModelResource(
+					availableLocale, className));
+		}
+
+		return localizationMap;
+	}
+
+	private AssetEntry _getAssetEntry(KaleoInstance kaleoInstance) {
+		try {
+			AssetRendererFactory<?> assetRendererFactory =
+				_getAssetRendererFactory(kaleoInstance.getClassName());
+
+			AssetRenderer<?> assetRenderer =
+				assetRendererFactory.getAssetRenderer(
+					kaleoInstance.getClassPK());
+
+			return _assetEntryLocalService.getEntry(
+				assetRenderer.getClassName(), assetRenderer.getClassPK());
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+		}
+
+		return null;
+	}
+
+	private AssetRendererFactory<?> _getAssetRendererFactory(String className) {
+		return AssetRendererFactoryRegistryUtil.
+			getAssetRendererFactoryByClassName(className);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		InstanceWorkflowMetricsIndexer.class);
+
+	@Reference
+	private AssetEntryLocalService _assetEntryLocalService;
 
 	@Reference
 	private KaleoInstanceLocalService _kaleoInstanceLocalService;
