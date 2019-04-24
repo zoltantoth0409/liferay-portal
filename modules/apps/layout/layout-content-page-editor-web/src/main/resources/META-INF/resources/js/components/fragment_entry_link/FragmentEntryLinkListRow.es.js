c606c98d977047ad6f481ce783deab89932a08e3
Pablo Molina <contact@pablomolina.me>
2019-04-24 12:12:00 +0200

    LPS-94476 Do not move rows if direction is null

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_entry_link/FragmentEntryLinkListRow.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_entry_link/FragmentEntryLinkListRow.es.js
@@ -342,20 +342,22 @@ class FragmentEntryLinkListRow extends Component {
 	/**
 	 * Callback executed when a key is pressed on the focused row.
 	 * @private
-	 * @param {Event} event
+	 * @param {KeyboardEvent} event
 	 */
 	_handleRowKeyUp(event) {
 		if (this.activeItemId === this.rowId &&
 			this.activeItemType === FRAGMENTS_EDITOR_ITEM_TYPES.row) {
 
-			const direction = getItemMoveDirection(event.which);
+			const direction = getItemMoveDirection(event.keyCode);
 
-			moveRow(
-				direction,
-				getRowIndex(this.layoutData.structure, this.rowId),
-				this.store,
-				this.layoutData.structure
-			);
+			if (direction) {
+				moveRow(
+					direction,
+					getRowIndex(this.layoutData.structure, this.rowId),
+					this.store,
+					this.layoutData.structure
+				);
+			}
 		}
 	}
 
