4db14a05b0ad47c27be578f09a9246fbbd955c49
Andrés Fulla <andres.fulla@liferay.com>
2019-04-24 11:03:23 +0200

    LPS-88629 Localize input component

--- modules/apps/segments/segments-web/src/main/resources/META-INF/resources/init.jsp
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/init.jsp
@@ -29,6 +29,9 @@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 
 <%@ page import="com.liferay.petra.string.StringPool" %><%@
 page import="com.liferay.portal.kernel.dao.search.ResultRow" %><%@
+page import="com.liferay.portal.kernel.json.JSONFactoryUtil" %><%@
+page import="com.liferay.portal.kernel.json.JSONObject" %><%@
+page import="com.liferay.portal.kernel.json.JSONSerializer" %><%@
 page import="com.liferay.portal.kernel.language.LanguageUtil" %><%@
 page import="com.liferay.portal.kernel.language.UnicodeLanguageUtil" %><%@
 page import="com.liferay.portal.kernel.portlet.LiferayWindowState" %><%@
---
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/title_editor/LocalizedDropdown.es.js
@@ -0,0 +1,173 @@
+import ClayIcon from '../shared/ClayIcon.es';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class LocalizedDropdown extends React.Component {
+	static defaultProps = {
+		initialLang: PropTypes.string,
+		initialOpen: PropTypes.bool,
+		availableLanguages: PropTypes.object
+	}
+
+	constructor(props) {
+		super(props);
+		const {initialOpen, initialLang} = props;
+		this.state = {
+			open: initialOpen,
+			currentLangKey: keyLangToLanguageTag(initialLang),
+			currentLangTag: keyLangToLanguageTag(initialLang, false)
+		};
+	}
+	_handleButtonClick = () => {
+		this.setState(
+			prevState => ({
+				open: !prevState.open
+			})
+		);
+	}
+
+	_handleButtonBlur = e => {
+		if (e.nativeEvent.explicitOriginalTarget &&
+			e.nativeEvent.explicitOriginalTarget === e.nativeEvent.originalTarget) {
+			return;
+		}
+
+		if (this.state.open) {
+			this.timer = setTimeout(() => {
+				this.setState(() => ({
+					open: false
+				})
+				);
+			}, 200);
+		}
+	}
+
+	_changeLanguage = langKey => {
+		this.setState(
+			{
+				currentLangKey: keyLangToLanguageTag(langKey),
+				currentLangTag: keyLangToLanguageTag(langKey, false),
+				open: false
+			},
+			() => this.props.onLanguageChange(langKey)
+		);
+	}
+
+	_handleItemFocus = () => {
+		clearTimeout(this.timer);
+	}
+
+	_handleLanguageClick = langKey => () => this._changeLanguage(langKey)
+
+	_handleLanguageKeyboard = langKey => e => {
+		if (e.keyCode === 13) {
+			this._changeLanguage(langKey);
+		}
+	}
+
+	render() {
+		const {
+			open,
+			currentLangKey,
+			currentLangTag
+		} = this.state;
+		const {
+			availableLanguages,
+			defaultLang
+		} = this.props;
+
+		return (<div className={`dropdown postion-relative lfr-icon-menu ${open ? 'open' : ''}`}>
+			<button
+				aria-expanded="false"
+				aria-haspopup="true"
+				className="btn btn-monospaced btn-secondary dropdown-toggle"
+				onBlur={this._handleButtonBlur}
+				onClick={this._handleButtonClick}
+				role="button"
+				title=""
+				type="button"
+			>
+				<span className="inline-item">
+					<ClayIcon iconName={currentLangKey} />
+				</span>
+				<span className="btn-section">{currentLangTag}</span>
+			</button>
+			{
+				open &&
+				<ul
+					className="dropdown-menu d-block"
+					role="menu"
+				>
+					{
+						availableLanguages.map(entry => {
+							const {key, hasValue} = entry;
+
+							return (<li
+								key={key}
+								onBlur={this._handleButtonBlur}
+								onClick={this._handleLanguageClick(key)}
+								onFocus={this._handleItemFocus}
+								onKeyDown={this._handleLanguageKeyboard(key)}
+								role="presentation"
+							>
+								<a
+									className="dropdown-item palette-item lfr-icon-item taglib-icon"
+									role="menuitem"
+									tabIndex="0"
+									target="_self"
+								>
+									<span className="inline-item inline-item-before">
+										<ClayIcon iconName={keyLangToLanguageTag(key)} />
+									</span>
+									<span
+										className="taglib-text-icon"
+									>
+										{
+											keyLangToLanguageTag(
+												key,
+												false
+											)
+										}
+										{
+											defaultLang === key &&
+											<span className="ml-1 label label-info">
+												{Liferay.Language.get('default-value')}
+											</span>
+										}
+										{
+											defaultLang !== key && (
+												hasValue ?
+													<span className="ml-1 label label-success">
+														{Liferay.Language.get('translated')}
+													</span> :
+													<span className="ml-1 label label-warning">
+														{Liferay.Language.get('untranslated')}
+													</span>
+											)
+										}
+									</span>
+								</a>
+							</li>);
+						})
+					}
+
+				</ul>}
+		</div>);
+	}
+}
+
+/**
+ * Helper to deal with the differnce in language keys for
+ * human reading, svg consumption and keys
+ *
+ * @param {string} [keyLang='']
+ * @param {boolean} [lowercase=true]
+ * @returns
+ */
+function keyLangToLanguageTag(keyLang = '', lowercase = true) {
+	let langTag = keyLang.replace(/_/g, '-');
+	if (lowercase) {langTag = langTag.toLowerCase();}
+	return langTag;
+};
+
+export default LocalizedDropdown;
