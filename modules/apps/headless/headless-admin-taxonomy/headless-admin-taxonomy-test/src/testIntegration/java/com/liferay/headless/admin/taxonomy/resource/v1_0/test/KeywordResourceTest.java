f2de5db34d6af10e7633e8a60240521b87724968
Brian Chan <brian.chan@liferay.com>
2019-04-24 14:45:03 -0700

    LPS-92618 revert my temp WIP code

--- modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-test/src/testIntegration/java/com/liferay/headless/admin/taxonomy/resource/v1_0/test/KeywordResourceTest.java
+++ modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-test/src/testIntegration/java/com/liferay/headless/admin/taxonomy/resource/v1_0/test/KeywordResourceTest.java
@@ -17,33 +17,14 @@ package com.liferay.headless.admin.taxonomy.resource.v1_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.headless.admin.taxonomy.client.dto.v1_0.Keyword;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.headless.admin.taxonomy.client.http.HttpInvoker;
+
 import org.junit.runner.RunWith;
-import org.junit.Test;
 
 /**
  * @author Javier Gamarra
  */
 @RunWith(Arquillian.class)
 public class KeywordResourceTest extends BaseKeywordResourceTestCase {
-
-	@Test
-	public void testSomething() throws Exception {
-		HttpInvoker httpInvoker = HttpInvoker.newHttpInvoker();
-
-		httpInvoker.contentType("application/json");
-		httpInvoker.httpMethod(HttpInvoker.HttpMethod.GET);
-		httpInvoker.parameter("page", "1");
-		httpInvoker.parameter("pageSize", "2");
-		httpInvoker.path("http://localhost:8080/o/headless-admin-taxonomy/v1.0/sites/{siteId}/keywords", testGroup.getGroupId());
-		httpInvoker.userNameAndPassword("test@liferay.com", "test");
-
-		HttpInvoker.HttpResponse httpResponse = httpInvoker.invoke();
-
-		System.out.println("## HTTP Response content: " + httpResponse.getContent());
-		System.out.println("## HTTP Response message: " + httpResponse.getMessage());
-		System.out.println("## HTTP Response status: " + httpResponse.getStatus());
-	}
 
 	@Override
 	protected String[] getAdditionalAssertFieldNames() {
