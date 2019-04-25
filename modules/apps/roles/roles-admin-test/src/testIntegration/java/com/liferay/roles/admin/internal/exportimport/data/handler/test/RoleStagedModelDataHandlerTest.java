3c1a996c4c0d29443a8d47d99f05af02c01327e6
shuyangzhou <shuyang.zhou@liferay.com>
2019-04-25 14:19:42 -0700

    LPS-93809 Apply to roles-admin-test

--- modules/apps/roles/roles-admin-test/src/testIntegration/java/com/liferay/roles/admin/internal/exportimport/data/handler/test/RoleStagedModelDataHandlerTest.java
+++ modules/apps/roles/roles-admin-test/src/testIntegration/java/com/liferay/roles/admin/internal/exportimport/data/handler/test/RoleStagedModelDataHandlerTest.java
@@ -16,6 +16,7 @@ package com.liferay.roles.admin.internal.exportimport.data.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.exportimport.test.util.lar.BaseStagedModelDataHandlerTestCase;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.RoleConstants;
@@ -56,14 +57,11 @@ public class RoleStagedModelDataHandlerTest
 	}
 
 	@Override
-	protected StagedModel getStagedModel(String uuid, Group group) {
-		try {
-			return RoleLocalServiceUtil.fetchRoleByUuidAndCompanyId(
-				uuid, group.getCompanyId());
-		}
-		catch (Exception e) {
-			return null;
-		}
+	protected StagedModel getStagedModel(String uuid, Group group)
+		throws PortalException {
+
+		return RoleLocalServiceUtil.getRoleByUuidAndCompanyId(
+			uuid, group.getCompanyId());
 	}
 
 	@Override
