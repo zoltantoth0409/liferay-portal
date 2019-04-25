2954e6f4970333c24a301890b43525835fcf4bf4
shuyangzhou <shuyang.zhou@liferay.com>
2019-04-25 14:08:24 -0700

    LPS-93809 Apply to layout-prototype-test

--- modules/apps/layout/layout-prototype-test/src/testIntegration/java/com/liferay/layout/prototype/internal/exportimport/data/handler/test/LayoutPrototypeStagedModelDataHandlerTest.java
+++ modules/apps/layout/layout-prototype-test/src/testIntegration/java/com/liferay/layout/prototype/internal/exportimport/data/handler/test/LayoutPrototypeStagedModelDataHandlerTest.java
@@ -17,6 +17,7 @@ package com.liferay.layout.prototype.internal.exportimport.data.handler.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
 import com.liferay.exportimport.test.util.lar.BaseStagedModelDataHandlerTestCase;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutFriendlyURL;
@@ -128,15 +129,11 @@ public class LayoutPrototypeStagedModelDataHandlerTest
 	}
 
 	@Override
-	protected StagedModel getStagedModel(String uuid, Group group) {
-		try {
-			return LayoutPrototypeLocalServiceUtil.
-				fetchLayoutPrototypeByUuidAndCompanyId(
-					uuid, group.getCompanyId());
-		}
-		catch (Exception e) {
-			return null;
-		}
+	protected StagedModel getStagedModel(String uuid, Group group)
+		throws PortalException {
+
+		return LayoutPrototypeLocalServiceUtil.
+			getLayoutPrototypeByUuidAndCompanyId(uuid, group.getCompanyId());
 	}
 
 	@Override
