9dd77e18c26dba197632e058a7ecc2184a293964
shuyangzhou <shuyang.zhou@liferay.com>
2019-04-25 14:26:27 -0700

    LPS-93809 Apply to wiki-test

--- modules/apps/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/internal/exportimport/data/handler/test/WikiNodeStagedModelDataHandlerTest.java
+++ modules/apps/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/internal/exportimport/data/handler/test/WikiNodeStagedModelDataHandlerTest.java
@@ -16,6 +16,7 @@ package com.liferay.wiki.internal.exportimport.data.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.exportimport.test.util.lar.BaseStagedModelDataHandlerTestCase;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.StagedModel;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -63,14 +64,11 @@ public class WikiNodeStagedModelDataHandlerTest
 	}
 
 	@Override
-	protected StagedModel getStagedModel(String uuid, Group group) {
-		try {
-			return WikiNodeLocalServiceUtil.getWikiNodeByUuidAndGroupId(
-				uuid, group.getGroupId());
-		}
-		catch (Exception e) {
-			return null;
-		}
+	protected StagedModel getStagedModel(String uuid, Group group)
+		throws PortalException {
+
+		return WikiNodeLocalServiceUtil.getWikiNodeByUuidAndGroupId(
+			uuid, group.getGroupId());
 	}
 
 	@Override
