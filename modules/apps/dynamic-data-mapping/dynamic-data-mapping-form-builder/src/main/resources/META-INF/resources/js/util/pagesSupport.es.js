86bd0769c41a9a7dd96093decaf65a7e1efc879d
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2019-04-25 16:20:17 -0300

    LPS-94290 Create methods to support pages handling which are been used by RuleEditor and RuleList classes

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/util/pagesSupport.es.js
@@ -0,0 +1,47 @@
+import {PagesVisitor} from './visitors.es';
+import {sub} from './strings.es.js';
+
+export function pagesOptions(pages, maxPageIndex = 0) {
+	const pageOptions = [];
+
+	for (let pageIndex = maxPageIndex + 2; pageIndex <= pages.length; pageIndex++) {
+		let pageTitle = `${pageIndex} ${sub(Liferay.Language.get('untitled-page-x-of-x'), [pageIndex, pages.length])}`;
+
+		if (pages[pageIndex - 1].title) {
+			pageTitle = `${pageIndex} ${pages[pageIndex - 1].title}`;
+		}
+
+		pageOptions.push(
+			{
+				label: pageTitle,
+				name: pageIndex.toString(),
+				value: pageIndex.toString()
+			}
+		);
+	}
+
+	return pageOptions;
+}
+
+export function maxPageIndex(conditions, pages) {
+	const pageIndexes = [];
+	const visitor = new PagesVisitor(pages);
+
+	if (conditions.length && conditions[0].operands[0].value) {
+		conditions.forEach(
+			condition => {
+				visitor.mapFields(
+					(field, fieldIndex, columnIndex, rowIndex, pageIndex) => {
+						if (field.fieldName === condition.operands[0].value) {
+							pageIndexes.push(pageIndex);
+						}
+					}
+				);
+			}
+		);
+	}
+
+	const maxPageIndex = Math.max(...pageIndexes);
+
+	return isFinite(maxPageIndex) ? maxPageIndex : 0;
+}
