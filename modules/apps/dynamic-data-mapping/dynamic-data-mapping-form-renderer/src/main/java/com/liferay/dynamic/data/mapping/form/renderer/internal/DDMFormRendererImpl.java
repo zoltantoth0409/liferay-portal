a77f3b76b7bb42be89f779abd220b612bdc8d8d2
Iván Zaera Avellón <izaera@gmail.com>
2019-04-26 11:49:23 +0200

    LPS-94647 Load dynamic-data-mapping-form-renderer CSS only when needed

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormRendererImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormRendererImpl.java
@@ -151,6 +151,11 @@ public class DDMFormRendererImpl implements DDMFormRenderer {
 			ddmFormRenderingContext.getHttpServletResponse(),
 			DDMFormFieldTypesDynamicInclude.class.getName(), true);
 
+		DynamicIncludeUtil.include(
+			ddmFormRenderingContext.getHttpServletRequest(),
+			ddmFormRenderingContext.getHttpServletResponse(),
+			DDMFormRenderer.class.getName() + "#formRendered", true);
+
 		return html.concat(javaScript);
 	}
 
