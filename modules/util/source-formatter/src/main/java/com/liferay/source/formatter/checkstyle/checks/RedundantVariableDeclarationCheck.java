6266785dc91d7b14d1997ec6943a695534b5ea20
Hugo Huijser <hugo.huijser@liferay.com>
2019-04-26 08:42:57 +0800

    LPS-84119 Add check to SF. No need to declare variable if it's returned right after

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/RedundantVariableDeclarationCheck.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * @author Hugo Huijser
+ */
+public class RedundantVariableDeclarationCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.VARIABLE_DEF};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		DetailAST modifiersDetailAST = detailAST.findFirstToken(
+			TokenTypes.MODIFIERS);
+
+		if (modifiersDetailAST.branchContains(TokenTypes.ANNOTATION)) {
+			return;
+		}
+
+		DetailAST nextSiblingDetailAST = detailAST.getNextSibling();
+
+		if ((nextSiblingDetailAST == null) ||
+			(nextSiblingDetailAST.getType() != TokenTypes.SEMI)) {
+
+			return;
+		}
+
+		nextSiblingDetailAST = nextSiblingDetailAST.getNextSibling();
+
+		if ((nextSiblingDetailAST == null) ||
+			(nextSiblingDetailAST.getType() != TokenTypes.LITERAL_RETURN) ||
+			(nextSiblingDetailAST.getHiddenBefore() != null)) {
+
+			return;
+		}
+
+		DetailAST firstChildDetailAST = nextSiblingDetailAST.getFirstChild();
+
+		if (firstChildDetailAST.getType() != TokenTypes.EXPR) {
+			return;
+		}
+
+		firstChildDetailAST = firstChildDetailAST.getFirstChild();
+
+		if (firstChildDetailAST.getType() == TokenTypes.IDENT) {
+			DetailAST nameDetailAST = detailAST.findFirstToken(
+				TokenTypes.IDENT);
+
+			String variableName = nameDetailAST.getText();
+
+			if (variableName.equals(firstChildDetailAST.getText())) {
+				log(
+					detailAST, _MSG_REDUNDANT_VARIABLE_DECLARATION,
+					variableName);
+			}
+		}
+	}
+
+	private static final String _MSG_REDUNDANT_VARIABLE_DECLARATION =
+		"variable.declaration.redundant";
+
+}
