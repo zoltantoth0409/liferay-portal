35a4e74887f920fec204002731b402b48706fe2e
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-04-26 17:33:15 +0200

    LPS-94756 Fix localized text indexing

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/search/spi/model/index/contributor/SegmentsEntryModelDocumentContributor.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/search/spi/model/index/contributor/SegmentsEntryModelDocumentContributor.java
@@ -14,13 +14,21 @@
 
 package com.liferay.segments.internal.search.spi.model.index.contributor;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.search.localization.SearchLocalizationHelper;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 import com.liferay.segments.internal.search.SegmentsEntryField;
 import com.liferay.segments.model.SegmentsEntry;
 
+import java.util.Locale;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eduardo García
@@ -38,10 +46,39 @@ public class SegmentsEntryModelDocumentContributor
 		document.addLocalizedKeyword(
 			Field.DESCRIPTION, segmentsEntry.getDescriptionMap(), true);
 		document.addDate(Field.MODIFIED_DATE, segmentsEntry.getModifiedDate());
+
+		Locale siteDefaultLocale = _getSiteDefaultLocale(
+			segmentsEntry.getGroupId());
+
+		_searchLocalizationHelper.addLocalizedField(
+			document, Field.NAME, siteDefaultLocale,
+			segmentsEntry.getNameMap());
+
 		document.addLocalizedKeyword(
-			Field.NAME, segmentsEntry.getNameMap(), true);
+			"localized_name",
+			LocalizationUtil.populateLocalizationMap(
+				segmentsEntry.getNameMap(),
+				segmentsEntry.getDefaultLanguageId(),
+				segmentsEntry.getGroupId()),
+			true, true);
+
 		document.addKeyword(
 			SegmentsEntryField.ACTIVE, segmentsEntry.isActive());
 	}
+
+	private Locale _getSiteDefaultLocale(long groupId) {
+		try {
+			return _portal.getSiteDefaultLocale(groupId);
+		}
+		catch (PortalException pe) {
+			throw new SystemException(pe);
+		}
+	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private SearchLocalizationHelper _searchLocalizationHelper;
 
 }
