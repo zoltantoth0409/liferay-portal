a52d8ff6b47692e1c2c6c2c550b79ec4d52933f9
Brian Chan <brian.chan@liferay.com>
2019-04-26 09:20:33 -0700

    LPS-91668 not needed

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/clay/SelectGlobalTemplatesVerticalCard.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/clay/SelectGlobalTemplatesVerticalCard.java
@@ -86,7 +86,7 @@ public class SelectGlobalTemplatesVerticalCard implements VerticalCard {
 	@Override
 	public String getElementClasses() {
 		return "add-layout-action-option card-interactive " +
-			"card-interactive-primary card-type-template " + "template-card";
+			"card-interactive-primary card-type-template template-card";
 	}
 
 	@Override
