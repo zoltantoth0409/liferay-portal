39ea797da6d5b778037dd49f907c24c547fcad18
Zoltán Takács <zoltan.takacs@liferay.com>
2019-04-30 00:35:39 +0200

    COMMERCE-1348 Add necessary fields for splitting the result set from our endpoints

--- modules/etl/talend/src/main/java/com/liferay/talend/configuration/LiferayInputMapperConfiguration.java
+++ modules/etl/talend/src/main/java/com/liferay/talend/configuration/LiferayInputMapperConfiguration.java
@@ -19,6 +19,9 @@ import com.liferay.talend.dataset.InputDataSet;
 import java.io.Serializable;
 
 import org.talend.sdk.component.api.configuration.Option;
+import org.talend.sdk.component.api.configuration.constraint.Max;
+import org.talend.sdk.component.api.configuration.constraint.Min;
+import org.talend.sdk.component.api.configuration.ui.DefaultValue;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayouts;
 
@@ -34,11 +37,29 @@ import org.talend.sdk.component.api.configuration.ui.layout.GridLayouts;
 		),
 		@GridLayout(
 			names = GridLayout.FormType.ADVANCED,
-			value = {@GridLayout.Row("_timeout"), @GridLayout.Row("_batchSize")}
+			value = {
+				@GridLayout.Row("_timeout"), @GridLayout.Row("_batchSize"),
+				@GridLayout.Row("_sort")
+			}
 		)
 	}
 )
 public class LiferayInputMapperConfiguration implements Serializable {
+
+	public LiferayInputMapperConfiguration() {
+	}
+
+	public LiferayInputMapperConfiguration(
+		LiferayInputMapperConfiguration liferayInputMapperConfiguration) {
+
+		_batchSize = liferayInputMapperConfiguration._batchSize;
+		_inputDataSet = liferayInputMapperConfiguration._inputDataSet;
+		_page = liferayInputMapperConfiguration._page;
+		_pageSize = liferayInputMapperConfiguration._pageSize;
+		_segmentPages = liferayInputMapperConfiguration._segmentPages;
+		_sort = liferayInputMapperConfiguration._sort;
+		_timeout = liferayInputMapperConfiguration._timeout;
+	}
 
 	public int getBatchSize() {
 		return _batchSize;
@@ -46,6 +67,22 @@ public class LiferayInputMapperConfiguration implements Serializable {
 
 	public InputDataSet getInputDataSet() {
 		return _inputDataSet;
+	}
+
+	public int getPage() {
+		return _page;
+	}
+
+	public int getPageSize() {
+		return _pageSize;
+	}
+
+	public int getSegmentPages() {
+		return _segmentPages;
+	}
+
+	public String getSort() {
+		return _sort;
 	}
 
 	public int getTimeout() {
@@ -66,17 +103,51 @@ public class LiferayInputMapperConfiguration implements Serializable {
 		return this;
 	}
 
+	public LiferayInputMapperConfiguration setPage(int page) {
+		_page = page;
+
+		return this;
+	}
+
+	public LiferayInputMapperConfiguration setPageSize(int pageSize) {
+		_pageSize = pageSize;
+
+		return this;
+	}
+
+	public LiferayInputMapperConfiguration setSegmentPages(int segmentPages) {
+		_segmentPages = segmentPages;
+
+		return this;
+	}
+
+	public LiferayInputMapperConfiguration setSort(String sort) {
+		_sort = sort;
+
+		return this;
+	}
+
 	public LiferayInputMapperConfiguration setTimeout(int timeout) {
 		_timeout = timeout;
 
 		return this;
 	}
 
+	@Max(10000)
+	@Min(1)
 	@Option
 	private int _batchSize = 100;
 
 	@Option
 	private InputDataSet _inputDataSet;
+
+	private int _page = 1;
+	private int _pageSize = _batchSize;
+	private int _segmentPages = 1;
+
+	@DefaultValue("dateCreated")
+	@Option
+	private String _sort;
 
 	@Option
 	private int _timeout = 60000;
