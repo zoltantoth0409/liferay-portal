56a84f229f5712570bfce300adf7635c932f07d2
Zoltán Takács <zoltan.takacs@liferay.com>
2019-04-30 00:36:12 +0200

    COMMERCE-1348 Implement data splitting on the PartitionMapper

--- modules/etl/talend/src/main/java/com/liferay/talend/source/LiferayInputMapper.java
+++ modules/etl/talend/src/main/java/com/liferay/talend/source/LiferayInputMapper.java
@@ -14,15 +14,26 @@
 
 package com.liferay.talend.source;
 
-import static java.util.Collections.singletonList;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import com.liferay.talend.configuration.LiferayInputMapperConfiguration;
+import com.liferay.talend.data.store.GenericDataStore;
+import com.liferay.talend.dataset.InputDataSet;
 import com.liferay.talend.service.ConnectionService;
 import com.liferay.talend.service.LiferayService;
+import com.liferay.talend.util.JsonUtils;
 
 import java.io.Serializable;
 
+import java.net.URI;
+import java.net.URL;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.json.JsonObject;
+
+import javax.ws.rs.core.UriBuilder;
 
 import org.talend.sdk.component.api.component.Icon;
 import org.talend.sdk.component.api.component.Version;
@@ -65,12 +76,84 @@ public class LiferayInputMapper implements Serializable {
 
 	@Assessor
 	public long estimateSize() {
-		return 1L;
+		InputDataSet inputDataSet =
+			_liferayInputMapperConfiguration.getInputDataSet();
+
+		GenericDataStore genericDataStore = inputDataSet.getGenericDataStore();
+
+		URL openAPISpecURL = genericDataStore.getOpenAPISpecURL();
+
+		URL serverURL = ConnectionService.getServerURL(genericDataStore);
+
+		String appBasePathSegment =
+			_liferayService.extractJaxRSAppBasePathSegment(openAPISpecURL);
+
+		String endpoint = inputDataSet.getEndpoint();
+
+		UriBuilder uriBuilder = UriBuilder.fromPath(serverURL.toExternalForm());
+
+		URI resourceURI = uriBuilder.path(
+			appBasePathSegment
+		).path(
+			endpoint
+		).queryParam(
+			"page", 1
+		).queryParam(
+			"pageSize", 1
+		).build(
+			inputDataSet.getFirstPathParam(), inputDataSet.getSecondPathParam(),
+			inputDataSet.getThirdPathParam()
+		);
+
+		JsonObject responseJsonObject =
+			_connectionService.getResponseJsonObject(
+				inputDataSet, resourceURI.toString());
+
+		JsonNode responseJsonNode = JsonUtils.toJsonNode(responseJsonObject);
+
+		JsonNode totalCountJsonNode = responseJsonNode.path("totalCount");
+
+		return totalCountJsonNode.asLong();
 	}
 
 	@Split
-	public List<LiferayInputMapper> split(@PartitionSize final long bundles) {
-		return singletonList(this);
+	public List<LiferayInputMapper> split(
+		@PartitionSize final long segmentSize) {
+
+		int pageSize;
+
+		if (segmentSize > _liferayInputMapperConfiguration.getBatchSize()) {
+			pageSize = _liferayInputMapperConfiguration.getBatchSize();
+		}
+		else {
+			pageSize = (int)segmentSize;
+		}
+
+		long segments = Math.round(Math.ceil(estimateSize() / segmentSize));
+		long segmentPages = Math.round(Math.ceil(segmentSize / pageSize));
+
+		List<LiferayInputMapper> liferayInputMappers = new ArrayList<>();
+
+		for (int i = 0; i < segments; i++) {
+			LiferayInputMapperConfiguration liferayInputMapperConfiguration =
+				new LiferayInputMapperConfiguration(
+					_liferayInputMapperConfiguration);
+
+			int page = _liferayInputMapperConfiguration.getPage();
+
+			liferayInputMapperConfiguration.setPage(
+				(int)(page + (i * segmentPages)));
+
+			liferayInputMapperConfiguration.setPageSize(pageSize);
+			liferayInputMapperConfiguration.setSegmentPages((int)segmentPages);
+
+			liferayInputMappers.add(
+				new LiferayInputMapper(
+					_connectionService, _liferayService,
+					liferayInputMapperConfiguration, _recordBuilderFactory));
+		}
+
+		return liferayInputMappers;
 	}
 
 	private final ConnectionService _connectionService;
