97c45887b8173c15369f9541a1d2312789f5168c
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-04-29 15:12:27 +0200

    LPS-94751 Do not set head as true during upgrade process

--- sql/update-7.1.0-7.2.0.sql
+++ sql/update-7.1.0-7.2.0.sql
@@ -8,7 +8,7 @@ alter table Layout add publishDate DATE null;
 
 COMMIT_TRANSACTION;
 
-update Layout set headId = plid, head = TRUE, system_ = FALSE;
+update Layout set headId = plid, head = FALSE, system_ = FALSE;
 
 insert into Counter (name, currentId)
 	select
@@ -21,7 +21,7 @@ alter table LayoutSet add head BOOLEAN;
 
 COMMIT_TRANSACTION;
 
-update LayoutSet set headId = layoutSetId, head = TRUE;
+update LayoutSet set headId = layoutSetId, head = FALSE;
 
 insert into Counter (name, currentId)
 	select
