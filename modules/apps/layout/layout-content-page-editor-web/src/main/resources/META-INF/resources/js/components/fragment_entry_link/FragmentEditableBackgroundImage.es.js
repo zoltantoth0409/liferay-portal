fd292c56a6e443dc5c385087c78347d86dffdb8b
Sandro Chinea <sandrochinea@gmail.com>
2019-04-30 15:38:50 +0200

    LPS-95260 Added FragmentEditableBackgroundImage component

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_entry_link/FragmentEditableBackgroundImage.es.js
@@ -0,0 +1,133 @@
+import {Config} from 'metal-state';
+import Component from 'metal-component';
+import {Store} from '../../store/store.es';
+
+import FragmentEditableFieldTooltip from './FragmentEditableFieldTooltip.es';
+import {init} from '../fragment_processors/EditableBackgroundImageProcessor.es'
+
+/**
+ * FragmentEditableBackgroundImage
+ */
+class FragmentEditableBackgroundImage extends Component {
+
+	/**
+	 * Returns the list of buttons to be shown inside the tooltip.
+	 * @param {boolean} showMapping
+	 * @return {Array<{id: string, label: string}>}
+	 * @review
+	 */
+	static getButtons(showMapping) {
+		const buttons = [
+			{
+				icon: 'pencil',
+				id: 'select',
+				label: Liferay.Language.get('select-background')
+			}
+		];
+
+		if (showMapping) {
+			buttons.push(
+				{
+					icon: 'bolt',
+					id: 'map',
+					label: Liferay.Language.get('map-background')
+				}
+			);
+		}
+
+		return buttons;
+	}
+	
+	/**
+	 * @inheritDoc
+	 * @review
+	 */
+	created() {
+		this._handleClick = this._handleClick.bind(this);
+		this._handleOutsideTooltipClick = this._handleOutsideTooltipClick.bind(this);
+
+		this.element.addEventListener('click', this._handleClick);
+	}
+
+	/**
+	 * @inheritDoc
+	 * @review
+	 */
+	disposed() {
+		this.element.removeEventListener('click', this._handleClick);
+		this._disposeTooltip();
+	}
+
+	/**
+	 * @private
+	 * @review
+	 */
+	_disposeTooltip() {
+		if (this._tooltip) {
+			this._tooltip.dispose();
+			this._tooltip = null;
+		}
+	}
+
+	/**
+	 * @private
+	 * @review
+	 */
+	_handleClick() {
+		if (this._tooltip) {
+			this._disposeTooltip();
+		}
+		else {
+			this._tooltip = new FragmentEditableFieldTooltip(
+				{
+					alignElement: this.element,
+					buttons: FragmentEditableBackgroundImage.getButtons(
+						this.showMapping
+					),
+					store: this.store
+				}
+			);
+
+			this._tooltip.on('outsideTooltipClick', this._handleOutsideTooltipClick);
+		}
+	}
+
+	/**
+	 * @private
+	 * @review
+	 */
+	_handleOutsideTooltipClick() {
+		this._disposeTooltip();
+	}
+
+}
+
+/**
+ * State definition.
+ * @static
+ * @type {!Object}
+ */
+FragmentEditableBackgroundImage.STATE = {
+
+	/**
+	 * If <code>true</code>, the mapping is activated.
+	 * @default undefined
+	 * @instance
+	 * @memberOf FragmentEditableBackgroundImage
+	 * @type {!boolean}
+	 */
+	showMapping: Config.bool().required(),
+
+	/**
+	 * Store instance.
+	 * @default undefined
+	 * @instance
+	 * @memberOf FragmentEditableBackgroundImage
+	 * @type {Store}
+	 */
+	store: Config.instanceOf(Store)
+
+};
+
+export {FragmentEditableBackgroundImage};
+export default FragmentEditableBackgroundImage;
