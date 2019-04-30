026ff3bb43caa5678ec3e5213d885daa656f1dd3
Iván Zaera Avellón <izaera@gmail.com>
2019-04-30 16:45:04 +0200

    LPS-94781 Convert theme contributors into TopHeadDynamicIncludes

---
+++ modules/apps/product-navigation/product-navigation-control-menu-theme-contributor/src/main/java/com/liferay/product/navigation/control/menu/theme/contributor/internal/servlet/taglib/ProductNavigationControlMenuTopHeadDynamicInclude.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.product.navigation.control.menu.theme.contributor.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.url.builder.AbsolutePortalURLBuilder;
+import com.liferay.portal.url.builder.AbsolutePortalURLBuilderFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class ProductNavigationControlMenuTopHeadDynamicInclude
+	implements DynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest request, HttpServletResponse response,
+			String key)
+		throws IOException {
+
+		PrintWriter printWriter = response.getWriter();
+
+		StringBundler sb = new StringBundler(6);
+
+		AbsolutePortalURLBuilder absolutePortalURLBuilder =
+			_absolutePortalURLBuilderFactory.getAbsolutePortalURLBuilder(
+				request);
+
+		sb.append("<link data-senna-track=\"permanent\" href=\"");
+		sb.append(
+			absolutePortalURLBuilder.forModule(
+				_bundle, "/product_navigation_control_menu.css"
+			).build());
+		sb.append("\" rel=\"stylesheet\" type = \"text/css\" />\n");
+
+		sb.append("<script data-senna-track=\"permanent\" src=\"");
+		sb.append(
+			absolutePortalURLBuilder.forModule(
+				_bundle, "/product_navigation_control_menu.js"
+			).build());
+		sb.append("\" type=\"text/javascript\"></script>");
+
+		printWriter.println(sb.toString());
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"/html/common/themes/top_head.jsp#post");
+	}
+
+	@Activate
+	@Modified
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference
+	private AbsolutePortalURLBuilderFactory _absolutePortalURLBuilderFactory;
+
+	private Bundle _bundle;
+
+}
