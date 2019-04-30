d70f4e519a002b45e119d6e2be7fb333f64e600e
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-04-30 21:14:29 +0200

    LPS-88191 Add a contributor for request-based context

---
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/context/contributor/RequestContextContributor.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.context.contributor;
+
+import com.liferay.segments.context.Context;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Provides an interface for extending the {@link Context} with information
+ * from the request.
+ *
+ * @author Eduardo García
+ * @review
+ */
+public interface RequestContextContributor {
+
+	/**
+	 * Contributes additional information to the context.
+	 *
+	 * @param  context the context
+	 * @param  request the current request
+	 * @review
+	 */
+	public void contribute(Context context, HttpServletRequest request);
+
+}
