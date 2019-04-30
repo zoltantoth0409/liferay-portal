e05f0c8f7ef179f20eb2cbff6b76c78778de2808
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-04-30 16:32:09 +0200

    LPS-92159 Hiding virtual instance settings when disabled at system level

---
+++ modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/internal/configuration/display/SharingCompanyConfigurationVisibilityController.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.internal.configuration.display;
+
+import com.liferay.configuration.admin.display.ConfigurationVisibilityController;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.sharing.configuration.SharingConfiguration;
+import com.liferay.sharing.configuration.SharingConfigurationFactory;
+
+import java.io.Serializable;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = "configuration.pid=com.liferay.sharing.internal.configuration.SharingCompanyConfiguration",
+	service = ConfigurationVisibilityController.class
+)
+public class SharingCompanyConfigurationVisibilityController
+	implements ConfigurationVisibilityController {
+
+	@Override
+	public boolean isVisible(
+		ExtendedObjectClassDefinition.Scope scope, Serializable scopePK) {
+
+		SharingConfiguration sharingSystemConfiguration =
+			_sharingConfigurationFactory.getSystemSharingConfiguration();
+
+		return sharingSystemConfiguration.isEnabled();
+	}
+
+	@Reference
+	private SharingConfigurationFactory _sharingConfigurationFactory;
+
+}
