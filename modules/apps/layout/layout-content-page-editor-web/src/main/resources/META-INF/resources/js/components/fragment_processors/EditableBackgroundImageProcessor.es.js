200bcd3a31eece8645bf529b40ebd4f2fc0be154
Sandro Chinea <sandrochinea@gmail.com>
2019-04-30 10:59:38 +0200

    LPS-95260 Added EditableBackgroundImageProcessor

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_processors/EditableBackgroundImageProcessor.es.js
@@ -0,0 +1,48 @@
+import {openImageSelector} from '../../utils/FragmentsEditorDialogUtils';
+
+/**
+ * Do nothing, as LiferayItemSelectorDialog is automatically
+ * destroyed on hide.
+ * @review
+ */
+function destroy() {
+}
+
+/**
+ * Show the image selector dialog and calls the given callback when an
+ * image is selected.
+ * @param {function} callback
+ * @param {string} imageSelectorURL
+ * @param {string} portletNamespace
+ * @review
+ */
+function init(
+	callback,
+	imageSelectorURL,
+	portletNamespace
+) {
+	openImageSelector(
+		{
+			callback,
+			imageSelectorURL,
+			portletNamespace
+		}
+	);
+}
+
+/**
+ * @param {HTMLElement} element
+ * @param {string} [backgroundImageURL='']
+ */
+function render(element, backgroundImageURL = '') {
+	element.style.backgroundImage = backgroundImageURL ?
+		`url("${backgroundImageURL}")` :
+		'';
+	element.style.backgroundSize = 'cover';
+}
+
+export default {
+	destroy,
+	init,
+	render
+};
