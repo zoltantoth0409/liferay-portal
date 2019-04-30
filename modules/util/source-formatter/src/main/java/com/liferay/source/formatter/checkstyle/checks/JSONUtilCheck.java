47d6d4d1382202543af7e96702696d7f234f0826
Hugo Huijser <hugo.huijser@liferay.com>
2019-04-30 11:10:20 +0800

    LPS-84119 Add JSONUtilCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/JSONUtilCheck.java
@@ -0,0 +1,204 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.liferay.source.formatter.checkstyle.util.DetailASTUtil;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FullIdent;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JSONUtilCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.ASSIGN};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		DetailAST parentDetailAST = detailAST.getParent();
+
+		if ((parentDetailAST.getType() != TokenTypes.EXPR) &&
+			(parentDetailAST.getType() != TokenTypes.VARIABLE_DEF)) {
+
+			return;
+		}
+
+		DetailAST nextSiblingDetailAST = parentDetailAST.getNextSibling();
+
+		if ((nextSiblingDetailAST == null) ||
+			(nextSiblingDetailAST.getType() != TokenTypes.SEMI)) {
+
+			return;
+		}
+
+		DetailAST methodCallDetailAST = _getMethodCallDetailAST(
+			detailAST, parentDetailAST);
+
+		if (methodCallDetailAST == null) {
+			return;
+		}
+
+		DetailAST elistDetailAST = methodCallDetailAST.findFirstToken(
+			TokenTypes.ELIST);
+
+		if (elistDetailAST.getChildCount() > 0) {
+			return;
+		}
+
+		DetailAST firstChildDetailAST = methodCallDetailAST.getFirstChild();
+
+		FullIdent fullIdent = FullIdent.createFullIdent(firstChildDetailAST);
+
+		String methodName = fullIdent.getText();
+
+		if (!methodName.equals("JSONFactoryUtil.createJSONArray") &&
+			!methodName.equals("JSONFactoryUtil.createJSONObject")) {
+
+			return;
+		}
+
+		String variableName = _getVariableName(detailAST, parentDetailAST);
+
+		if (variableName == null) {
+			return;
+		}
+
+		while (true) {
+			nextSiblingDetailAST = nextSiblingDetailAST.getNextSibling();
+
+			if (nextSiblingDetailAST == null) {
+				return;
+			}
+
+			int lineNumber = _getMethodCallLineNumber(
+				nextSiblingDetailAST, variableName, "put");
+
+			if (lineNumber != -1) {
+				log(lineNumber, _MSG_USE_JSON_UTIL_PUT);
+			}
+
+			if (_containsVariableName(nextSiblingDetailAST, variableName)) {
+				return;
+			}
+		}
+	}
+
+	private boolean _containsVariableName(
+		DetailAST detailAST, String variableName) {
+
+		List<DetailAST> identDetailASTList = DetailASTUtil.getAllChildTokens(
+			detailAST, true, TokenTypes.IDENT);
+
+		for (DetailAST identDetailAST : identDetailASTList) {
+			if (variableName.equals(identDetailAST.getText())) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private DetailAST _getMethodCallDetailAST(
+		DetailAST assignDetailAST, DetailAST parentDetailAST) {
+
+		DetailAST firstChildDetailAST = assignDetailAST.getFirstChild();
+
+		DetailAST assignValueDetailAST = null;
+
+		if (parentDetailAST.getType() == TokenTypes.EXPR) {
+			assignValueDetailAST = firstChildDetailAST.getNextSibling();
+		}
+		else {
+			assignValueDetailAST = firstChildDetailAST.getFirstChild();
+		}
+
+		if ((assignValueDetailAST != null) &&
+			(assignValueDetailAST.getType() == TokenTypes.METHOD_CALL)) {
+
+			return assignValueDetailAST;
+		}
+
+		return null;
+	}
+
+	private int _getMethodCallLineNumber(
+		DetailAST detailAST, String variableName, String methodName) {
+
+		if (detailAST.getType() != TokenTypes.EXPR) {
+			return -1;
+		}
+
+		DetailAST firstChildDetailAST = detailAST.getFirstChild();
+
+		while (true) {
+			if ((firstChildDetailAST == null) ||
+				(firstChildDetailAST.getType() != TokenTypes.METHOD_CALL)) {
+
+				return -1;
+			}
+
+			firstChildDetailAST = firstChildDetailAST.getFirstChild();
+
+			FullIdent fullIdent = FullIdent.createFullIdent(
+				firstChildDetailAST);
+
+			if (Objects.equals(
+					fullIdent.getText(), variableName + "." + methodName)) {
+
+				return fullIdent.getLineNo();
+			}
+
+			if (firstChildDetailAST.getType() != TokenTypes.DOT) {
+				return -1;
+			}
+
+			firstChildDetailAST = firstChildDetailAST.getFirstChild();
+		}
+	}
+
+	private String _getVariableName(
+		DetailAST assignDetailAST, DetailAST parentDetailAST) {
+
+		if (parentDetailAST.getType() == TokenTypes.EXPR) {
+			DetailAST nameDetailAST = assignDetailAST.getFirstChild();
+
+			if (nameDetailAST.getType() == TokenTypes.IDENT) {
+				return nameDetailAST.getText();
+			}
+
+			return null;
+		}
+
+		DetailAST nameDetailAST = parentDetailAST.findFirstToken(
+			TokenTypes.IDENT);
+
+		if (nameDetailAST != null) {
+			return nameDetailAST.getText();
+		}
+
+		return null;
+	}
+
+	private static final String _MSG_USE_JSON_UTIL_PUT = "json.util.put.use";
+
+}
