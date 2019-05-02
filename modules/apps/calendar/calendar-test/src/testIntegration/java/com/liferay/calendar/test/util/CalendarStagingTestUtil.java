43e78e2b34fb90cd661fc6afbc71587771efefab
Hugo Huijser <hugo.huijser@liferay.com>
2019-05-02 13:11:06 +0800

    Revert "LPS-84119 SF"
    This reverts commit 1190984d153bb2a924468207f5a62b927244d80c.

--- modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CalendarStagingTestUtil.java
+++ modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CalendarStagingTestUtil.java
@@ -34,7 +34,6 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -99,18 +98,14 @@ public class CalendarStagingTestUtil {
 
 		Group stagingGroup = liveGroup.getStagingGroup();
 
-		List<String> portletIds = null;
-
-		if (enableCalendarStaging) {
-			portletIds = Arrays.asList(CalendarPortletKeys.CALENDAR);
-		}
-
 		Map<String, String[]> parameters =
 			ExportImportConfigurationParameterMapFactoryUtil.buildParameterMap(
 				PortletDataHandlerKeys.DATA_STRATEGY_MIRROR_OVERWRITE, true,
 				false, true, false, false, true, true, true, true, false, null,
-				true, false, portletIds, false, null,
-				ExportImportDateUtil.RANGE_ALL, true, true,
+				true, false,
+				enableCalendarStaging ?
+				Arrays.asList(CalendarPortletKeys.CALENDAR) : null,
+				false, null, ExportImportDateUtil.RANGE_ALL, true, true,
 				UserIdStrategy.CURRENT_USER_ID);
 
 		StagingUtil.publishLayouts(
