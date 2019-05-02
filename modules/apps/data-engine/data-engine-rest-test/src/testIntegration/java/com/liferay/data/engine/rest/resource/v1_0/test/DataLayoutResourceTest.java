c1a096b4401800a6737a6f103867f50b703477d2
Brian Chan <brian.chan@liferay.com>
2019-05-02 13:30:46 -0700

    LPS-93317 Can we use this instead? Please test

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v1_0/test/DataLayoutResourceTest.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v1_0/test/DataLayoutResourceTest.java
@@ -96,7 +96,7 @@ public class DataLayoutResourceTest extends BaseDataLayoutResourceTestCase {
 	protected Long testGetSiteDataLayoutPage_getIrrelevantSiteId()
 		throws Exception {
 
-		return null;
+		return irrelevantGroup.getGroupId();
 	}
 
 	@Override
