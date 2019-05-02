3c6432c050fa5bc19a8a7db0c0ee4a0d8baf4753
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-05-02 15:43:39 -0300

    LPS-89082|LPS-94927 Add blocked message to "pause" constant.

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/sla/Constants.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/sla/Constants.js
@@ -3,6 +3,7 @@ const DAYS = 'days';
 const DURATION = 'duration';
 const HOURS = 'hours';
 const NAME = 'name';
+const PAUSE_NODE_KEYS = 'pauseNodeKeys';
 const START_NODE_KEYS = 'startNodeKeys';
 const STOP_NODE_KEYS = 'stopNodeKeys';
 const REQUEST_ORIGIN_TYPE_FETCH = 'REQUEST_ORIGIN_TYPE_FETCH';
@@ -13,6 +14,7 @@ export {
 	DURATION,
 	HOURS,
 	NAME,
+	PAUSE_NODE_KEYS,
 	START_NODE_KEYS,
 	STOP_NODE_KEYS,
 	REQUEST_ORIGIN_TYPE_FETCH
