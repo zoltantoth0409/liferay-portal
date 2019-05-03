80afad5fe917033423b130a2220a7156ff3ebb6e
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-05-03 14:08:32 +0200

    LPS-95059 Whitelist segments preview action in live site

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/security/permission/resource/SegmentsPortletResourcePermissionRegistrar.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/security/permission/resource/SegmentsPortletResourcePermissionRegistrar.java
@@ -15,10 +15,13 @@
 package com.liferay.segments.internal.security.permission.resource;
 
 import com.liferay.exportimport.kernel.staging.permission.StagingPermission;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
-import com.liferay.portal.kernel.security.permission.resource.StagedPortletPermissionLogic;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionLogic;
 import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.segments.constants.SegmentsActionKeys;
 import com.liferay.segments.constants.SegmentsConstants;
 import com.liferay.segments.constants.SegmentsPortletKeys;
 
@@ -61,5 +64,39 @@ public class SegmentsPortletResourcePermissionRegistrar {
 
 	@Reference
 	private StagingPermission _stagingPermission;
+
+	private static class StagedPortletPermissionLogic
+		implements PortletResourcePermissionLogic {
+
+		@Override
+		public Boolean contains(
+			PermissionChecker permissionChecker, String name, Group group,
+			String actionId) {
+
+			if (SegmentsActionKeys.SIMULATE_SEGMENTS_ENTRIES.equals(actionId)) {
+				return null;
+			}
+
+			long groupId = 0;
+
+			if (group != null) {
+				groupId = group.getGroupId();
+			}
+
+			return _stagingPermission.hasPermission(
+				permissionChecker, group, name, groupId, _portletId, actionId);
+		}
+
+		private StagedPortletPermissionLogic(
+			StagingPermission stagingPermission, String portletId) {
+
+			_stagingPermission = stagingPermission;
+			_portletId = portletId;
+		}
+
+		private final String _portletId;
+		private final StagingPermission _stagingPermission;
+
+	}
 
 }
