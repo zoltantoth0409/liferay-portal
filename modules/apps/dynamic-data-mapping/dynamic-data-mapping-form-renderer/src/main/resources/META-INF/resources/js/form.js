50fbcaf1a69208d44d8bdc71955effd6fe602c61
Renato Rego <renato.rego@liferay.com>
2019-05-03 14:39:45 -0300

    LPS-94720 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/form.js
@@ -17,10 +17,6 @@ AUI.add(
 						value: ''
 					},
 
-					languageId: {
-						value: themeDisplay.getLanguageId()
-					},
-					
 					defaultLanguageId: {
 						value: themeDisplay.getDefaultLanguageId()
 					},
@@ -225,11 +221,11 @@ AUI.add(
 						var languageId = instance._getURLParameter(portletNamespace, 'languageId');
 
 						if (!languageId) {
-							languageId = instance.get('languageId');	
-						}
+							languageId = themeDisplay.getLanguageId();
 
-						if (!languageId) {
-							languageId = instance.get('defaultLanguageId');	
+							if (!languageId) {
+								languageId = instance.get('defaultLanguageId');
+							}
 						}
 
 						return languageId;
