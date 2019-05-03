a885805617a38e750ad97f7f77f6a847b7a83515
Rafael Praxedes Gomes <rafaprax28@gmail.com>
2019-05-03 15:09:28 -0300

    LPS-89082 | LPS-93691 Map the new timeframe validation exception

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/jaxrs/exception/mapper/SLAPauseNodeKeysExceptionMapper.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.portal.workflow.metrics.exception.WorkflowMetricsSLADefinitionPauseNodeKeysException;
+
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Portal.Workflow.Metrics.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Portal.Workflow.Metrics.REST.SLAPauseNodeKeysExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class SLAPauseNodeKeysExceptionMapper
+	extends BaseSLAExceptionMapper
+		<WorkflowMetricsSLADefinitionPauseNodeKeysException> {
+
+	@Override
+	public String getFieldName() {
+		return "pauseNodeKeys";
+	}
+
+	@Override
+	public String getKey() {
+		return "the-pause-node-field-is-not-valid";
+	}
+
+}
