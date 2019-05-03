278090fcf0016f59f1b842853a6d76e89d526a41
Preston-Crary <preston.crary@liferay.com>
2019-05-03 09:20:00 -0700

    LPS-93662 Replace broken magic number code with the working API for this to fix random test failures.

--- modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/BaseTestPreparatorBundleActivator.java
+++ modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/BaseTestPreparatorBundleActivator.java
@@ -21,9 +21,8 @@ import com.liferay.oauth2.provider.scope.spi.prefix.handler.PrefixHandler;
 import com.liferay.oauth2.provider.scope.spi.prefix.handler.PrefixHandlerFactory;
 import com.liferay.oauth2.provider.scope.spi.scope.mapper.ScopeMapper;
 import com.liferay.oauth2.provider.service.OAuth2ApplicationLocalService;
-import com.liferay.osgi.util.ServiceTrackerFactory;
-import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.ConfigurationTemporarySwapper;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -33,10 +32,8 @@ import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.security.service.access.policy.model.SAPEntry;
 import com.liferay.portal.security.service.access.policy.service.SAPEntryLocalService;
 
@@ -61,16 +58,11 @@ import javax.ws.rs.core.Application;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
-import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
-import org.osgi.service.cm.ManagedService;
 import org.osgi.service.cm.ManagedServiceFactory;
-import org.osgi.service.jaxrs.runtime.JaxrsServiceRuntime;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Carlos Sierra Andrés
@@ -331,132 +323,6 @@ public abstract class BaseTestPreparatorBundleActivator
 		}
 	}
 
-	protected void deleteConfiguration(
-		BundleContext bundleContext, String pid) {
-
-		CountDownLatch countDownLatch = new CountDownLatch(1);
-
-		Dictionary<String, Object> properties = new HashMapDictionary<>();
-
-		properties.put(Constants.SERVICE_PID, pid);
-
-		ServiceRegistration<ManagedService> serviceRegistration =
-			bundleContext.registerService(
-				ManagedService.class,
-				incomingProperties -> {
-					if (Validator.isNull(incomingProperties)) {
-						countDownLatch.countDown();
-					}
-				},
-				properties);
-
-		try {
-			ServiceReference<ConfigurationAdmin> serviceReference =
-				bundleContext.getServiceReference(ConfigurationAdmin.class);
-
-			ConfigurationAdmin configurationAdmin = bundleContext.getService(
-				serviceReference);
-
-			try {
-				Configuration configuration =
-					configurationAdmin.getConfiguration(
-						pid, StringPool.QUESTION);
-
-				configuration.delete();
-
-				countDownLatch.await(5, TimeUnit.MINUTES);
-			}
-			catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-			finally {
-				bundleContext.ungetService(serviceReference);
-			}
-		}
-		finally {
-			serviceRegistration.unregister();
-		}
-	}
-
-	protected void executeAndWaitForReadiness(Runnable runnable) {
-		int expectedModificationsCount = 23;
-
-		CountDownLatch countDownLatch = new CountDownLatch(
-			expectedModificationsCount);
-
-		ServiceTracker<JaxrsServiceRuntime, Object> serviceTracker =
-			ServiceTrackerFactory.open(
-				bundleContext, JaxrsServiceRuntime.class,
-				new ServiceTrackerCustomizer<JaxrsServiceRuntime, Object>() {
-
-					@Override
-					public Object addingService(
-						ServiceReference<JaxrsServiceRuntime> reference) {
-
-						return reference;
-					}
-
-					@Override
-					public void modifiedService(
-						ServiceReference<JaxrsServiceRuntime> reference,
-						Object service) {
-
-						countDownLatch.countDown();
-					}
-
-					@Override
-					public void removedService(
-						ServiceReference<JaxrsServiceRuntime> reference,
-						Object service) {
-					}
-
-				});
-
-		runnable.run();
-
-		try {
-			if (!countDownLatch.await(10, TimeUnit.MINUTES)) {
-				throw new IllegalStateException(
-					StringBundler.concat(
-						"Expected modifications count ",
-						expectedModificationsCount, " did not match ",
-						countDownLatch.getCount()));
-			}
-		}
-		catch (InterruptedException ie) {
-			throw new RuntimeException(ie);
-		}
-		finally {
-			serviceTracker.close();
-		}
-	}
-
-	protected Configuration getConfiguration(
-			BundleContext bundleContext, String pid)
-		throws InvalidSyntaxException, IOException {
-
-		ServiceReference<ConfigurationAdmin> serviceReference =
-			bundleContext.getServiceReference(ConfigurationAdmin.class);
-
-		ConfigurationAdmin configurationAdmin = bundleContext.getService(
-			serviceReference);
-
-		try {
-			Configuration[] configurations =
-				configurationAdmin.listConfigurations(
-					"(" + Constants.SERVICE_PID + "=" + pid + ")");
-
-			if (ArrayUtil.isEmpty(configurations)) {
-				return null;
-			}
-
-			return configurations[0];
-		}
-		finally {
-			bundleContext.ungetService(serviceReference);
-		}
-	}
-
 	protected boolean isIncluded(
 		Dictionary<String, ?> properties1, Dictionary<String, ?> properties2) {
 
@@ -527,109 +393,13 @@ public abstract class BaseTestPreparatorBundleActivator
 		return serviceRegistration;
 	}
 
-	protected Configuration updateConfiguration(
-		BundleContext bundleContext, String servicePid,
-		Dictionary<String, ?> properties) {
-
-		CountDownLatch countDownLatch = new CountDownLatch(1);
-
-		Dictionary<String, Object> registrationProperties =
-			new HashMapDictionary<>();
-
-		registrationProperties.put(Constants.SERVICE_PID, servicePid);
-
-		ServiceRegistration<ManagedService> serviceRegistration =
-			bundleContext.registerService(
-				ManagedService.class,
-				updatedProperties -> {
-					if (updatedProperties == null) {
-						return;
-					}
-
-					if (isIncluded(properties, updatedProperties)) {
-						countDownLatch.countDown();
-					}
-				},
-				registrationProperties);
-
-		try {
-			ServiceReference<ConfigurationAdmin> serviceReference =
-				bundleContext.getServiceReference(ConfigurationAdmin.class);
-
-			ConfigurationAdmin configurationAdmin = bundleContext.getService(
-				serviceReference);
-
-			Configuration configuration = null;
-
-			try {
-				configuration = configurationAdmin.getConfiguration(
-					servicePid, StringPool.QUESTION);
-
-				configuration.update(properties);
-
-				countDownLatch.await(5, TimeUnit.MINUTES);
-
-				return configuration;
-			}
-			catch (IOException ioe) {
-				throw new RuntimeException(ioe);
-			}
-			catch (InterruptedException ie) {
-				try {
-					configuration.delete();
-				}
-				catch (IOException ioe) {
-					throw new RuntimeException(ioe);
-				}
-
-				throw new RuntimeException(ie);
-			}
-			finally {
-				bundleContext.ungetService(serviceReference);
-			}
-		}
-		finally {
-			serviceRegistration.unregister();
-		}
-	}
-
 	protected void updateOAuth2ProviderConfiguration(
-		Dictionary<String, Object> properties) {
+			Dictionary<String, Object> properties)
+		throws Exception {
 
-		executeAndWaitForReadiness(
-			() -> {
-				Runnable runnable = updateOrCreateConfiguration(
-					OAuth2ProviderConfiguration.class.getName(), properties);
-
-				autoCloseables.add(() -> executeAndWaitForReadiness(runnable));
-			});
-	}
-
-	protected Runnable updateOrCreateConfiguration(
-		String servicePid, Dictionary<String, ?> properties) {
-
-		Configuration configuration = null;
-
-		try {
-			configuration = getConfiguration(bundleContext, servicePid);
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-
-		if (configuration == null) {
-			updateConfiguration(bundleContext, servicePid, properties);
-
-			return () -> deleteConfiguration(bundleContext, servicePid);
-		}
-
-		Dictionary<String, Object> oldProperties =
-			configuration.getProperties();
-
-		updateConfiguration(bundleContext, servicePid, properties);
-
-		return () -> updateConfiguration(
-			bundleContext, servicePid, oldProperties);
+		autoCloseables.add(
+			new ConfigurationTemporarySwapper(
+				OAuth2ProviderConfiguration.class.getName(), properties));
 	}
 
 	protected ArrayList<AutoCloseable> autoCloseables;
