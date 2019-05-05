291aab4e1cfa3d5d6f8e06fb2036891f6969b3c7
Javier Gamarra <nhpatt@gmail.com>
2019-05-05 16:40:17 +0000

    LPS-95092 index parentMessageId, the id of the immediate parent of a KBArticle and the folderId

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/search/KBArticleIndexer.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/search/KBArticleIndexer.java
@@ -134,7 +134,10 @@ public class KBArticleIndexer extends BaseIndexer<KBArticle> {
 		document.addText(Field.DESCRIPTION, kbArticle.getDescription());
 		document.addText(Field.TITLE, kbArticle.getTitle());
 
+		document.addKeyword(Field.FOLDER_ID, kbArticle.getKbFolderId());
 		document.addKeyword("folderNames", getKBFolderNames(kbArticle));
+		document.addKeyword(
+			"parentMessageId", kbArticle.getParentResourcePrimKey());
 		document.addKeyword("titleKeyword", kbArticle.getTitle(), true);
 
 		return document;
