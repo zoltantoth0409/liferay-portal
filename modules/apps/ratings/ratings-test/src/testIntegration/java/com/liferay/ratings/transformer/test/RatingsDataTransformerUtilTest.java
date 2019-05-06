3f15373e61ee1cf9ae31c63a1ee94677ca9f2cf6
Leon Chi <leon.chi@liferay.com>
2019-05-06 08:24:59 +0800

    LPS-95165 Move RatingsDataTransformerUtilTest to module and adapt to arquillian test

--- modules/apps/ratings/ratings-test/src/testIntegration/java/com/liferay/ratings/transformer/test/RatingsDataTransformerUtilTest.java
+++ modules/apps/ratings/ratings-test/src/testIntegration/java/com/liferay/ratings/transformer/test/RatingsDataTransformerUtilTest.java
@@ -12,14 +12,15 @@
  * details.
  */
 
-package com.liferay.portlet.ratings.transformer;
+package com.liferay.ratings.transformer.test;
 
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.SyntheticBundleClassTestRule;
-import com.liferay.portal.util.test.AtomicState;
 import com.liferay.portlet.PortletPreferencesImpl;
+import com.liferay.ratings.kernel.transformer.RatingsDataTransformer;
 import com.liferay.ratings.kernel.transformer.RatingsDataTransformerUtil;
 
 import javax.portlet.PortletPreferences;
@@ -30,33 +31,54 @@ import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
- * @author Peter Fellwock
+ * @author Leon Chi
  */
+@RunWith(Arquillian.class)
 public class RatingsDataTransformerUtilTest {
 
 	@ClassRule
 	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			new SyntheticBundleClassTestRule(
-				"bundle.ratingsdatatransformerutil"));
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
 
 	@BeforeClass
 	public static void setUpClass() {
-		_atomicState = new AtomicState();
+		Bundle bundle = FrameworkUtil.getBundle(
+			RatingsDataTransformerUtilTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_serviceRegistration = bundleContext.registerService(
+			RatingsDataTransformer.class,
+			(RatingsDataTransformer)ProxyUtil.newProxyInstance(
+				RatingsDataTransformer.class.getClassLoader(),
+				new Class<?>[] {RatingsDataTransformer.class},
+				(proxy, method, args) -> {
+					if ("transformRatingsData".equals(method.getName())) {
+						_calledTransformRatingsData = true;
+					}
+
+					return null;
+				}),
+			MapUtil.singletonDictionary("service.ranking", Integer.MAX_VALUE));
 	}
 
 	@AfterClass
 	public static void tearDownClass() {
-		_atomicState.close();
+		_serviceRegistration.unregister();
 	}
 
 	@Test
 	public void testTransformCompanyRatingsData() throws Exception {
-		_atomicState.reset();
+		_calledTransformRatingsData = false;
 
 		PortletPreferences oldPortletPreferences = new PortletPreferencesImpl();
 
@@ -103,12 +125,12 @@ public class RatingsDataTransformerUtilTest {
 		RatingsDataTransformerUtil.transformCompanyRatingsData(
 			1, oldPortletPreferences, unicodeProperties);
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledTransformRatingsData);
 	}
 
 	@Test
 	public void testTransformGroupRatingsData() throws Exception {
-		_atomicState.reset();
+		_calledTransformRatingsData = false;
 
 		UnicodeProperties oldUnicodeProperties = new UnicodeProperties();
 
@@ -155,9 +177,11 @@ public class RatingsDataTransformerUtilTest {
 		RatingsDataTransformerUtil.transformGroupRatingsData(
 			1, oldUnicodeProperties, unicodeProperties);
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledTransformRatingsData);
 	}
 
-	private static AtomicState _atomicState;
+	private static boolean _calledTransformRatingsData;
+	private static ServiceRegistration<RatingsDataTransformer>
+		_serviceRegistration;
 
 }
