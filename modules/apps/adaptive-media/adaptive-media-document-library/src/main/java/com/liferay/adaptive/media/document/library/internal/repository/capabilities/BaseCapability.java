0ab33b30bc032380a54d577b699e4db2893243fd
Adolfo Pérez <adolfo.perez@liferay.com>
2019-05-06 16:03:46 +0200

    LPS-92696 Use safe input stream in Adaptive Media

--- modules/apps/adaptive-media/adaptive-media-document-library/src/main/java/com/liferay/adaptive/media/document/library/internal/repository/capabilities/BaseCapability.java
+++ modules/apps/adaptive-media/adaptive-media-document-library/src/main/java/com/liferay/adaptive/media/document/library/internal/repository/capabilities/BaseCapability.java
@@ -18,15 +18,20 @@ import com.liferay.adaptive.media.document.library.internal.util.AMCleanUpOnUpda
 import com.liferay.adaptive.media.image.service.AMImageEntryLocalService;
 import com.liferay.adaptive.media.processor.AMAsyncProcessor;
 import com.liferay.adaptive.media.processor.AMAsyncProcessorLocator;
+import com.liferay.document.library.security.io.InputStreamSanitizer;
 import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.capabilities.Capability;
 import com.liferay.portal.kernel.repository.event.RepositoryEventAware;
 import com.liferay.portal.kernel.repository.event.RepositoryEventType;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileEntryWrapper;
 import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.repository.model.FileVersionWrapper;
 import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.io.InputStream;
 
 import java.util.List;
 
@@ -58,6 +63,9 @@ public abstract class BaseCapability
 
 	@Reference
 	protected AMImageEntryLocalService amImageEntryLocalService;
+
+	@Reference
+	protected InputStreamSanitizer inputStreamSanitizer;
 
 	private void _deleteAdaptiveMedia(FileEntry fileEntry) {
 		if (ExportImportThreadLocal.isImportInProcess()) {
@@ -100,12 +108,86 @@ public abstract class BaseCapability
 				amAsyncProcessorLocator.locateForClass(FileVersion.class);
 
 			amAsyncProcessor.triggerProcess(
-				latestFileVersion,
+				_wrap(latestFileVersion),
 				String.valueOf(latestFileVersion.getFileVersionId()));
 		}
 		catch (PortalException pe) {
 			throw new RuntimeException(pe);
 		}
+	}
+
+	private FileVersion _wrap(FileVersion fileVersion) {
+		if (fileVersion == null) {
+			return null;
+		}
+
+		return new SafeFileVersion(fileVersion);
+	}
+
+	private class SafeFileEntry extends FileEntryWrapper {
+
+		public SafeFileEntry(FileEntry fileEntry) {
+			super(fileEntry);
+		}
+
+		@Override
+		public InputStream getContentStream() throws PortalException {
+			return inputStreamSanitizer.sanitize(super.getContentStream());
+		}
+
+		@Override
+		public InputStream getContentStream(String version)
+			throws PortalException {
+
+			return inputStreamSanitizer.sanitize(
+				super.getContentStream(version));
+		}
+
+		@Override
+		public FileVersion getFileVersion() throws PortalException {
+			return new SafeFileVersion(super.getFileVersion());
+		}
+
+		@Override
+		public FileVersion getFileVersion(String version)
+			throws PortalException {
+
+			return new SafeFileVersion(super.getFileVersion(version));
+		}
+
+		@Override
+		public FileVersion getLatestFileVersion() throws PortalException {
+			return new SafeFileVersion(super.getLatestFileVersion());
+		}
+
+		@Override
+		public FileVersion getLatestFileVersion(boolean trusted)
+			throws PortalException {
+
+			return new SafeFileVersion(super.getLatestFileVersion(trusted));
+		}
+
+	}
+
+	private class SafeFileVersion extends FileVersionWrapper {
+
+		public SafeFileVersion(FileVersion fileVersion) {
+			super(fileVersion);
+		}
+
+		@Override
+		public InputStream getContentStream(boolean incrementCounter)
+			throws PortalException {
+
+			return inputStreamSanitizer.sanitize(
+				super.getContentStream(incrementCounter));
+		}
+
+		@Override
+		public FileEntry getFileEntry() throws PortalException {
+			return new SafeFileEntry(super.getFileEntry());
+		}
+
 	}
 
 }
