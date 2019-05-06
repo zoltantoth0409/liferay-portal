562c4ecac3594f4fb6146cc48e614b62534bfb2c
Tamas Molnar <tamas.molnar@liferay.com>
2019-05-06 15:59:06 +0200

    LPS-93600 Layouts created in a Site which is connected to a Site Template need the "_PRIORITY_BUFFER" offset

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/StagedLayoutSetStagedModelDataHandler.java
@@ -60,6 +60,7 @@ import com.liferay.portal.kernel.util.ThemeFactoryUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.service.impl.LayoutLocalServiceHelper;
 import com.liferay.sites.kernel.util.Sites;
 import com.liferay.sites.kernel.util.SitesUtil;
 
@@ -612,6 +613,13 @@ public class StagedLayoutSetStagedModelDataHandler
 		}
 	}
 
+	@Reference(unbind = "-")
+	protected void setLayoutLocalServiceHelper(
+		LayoutLocalServiceHelper layoutLocalServiceHelper) {
+
+		_layoutLocalServiceHelper = layoutLocalServiceHelper;
+	}
+
 	protected StagedLayoutSet unwrapLayoutSetStagingHandler(
 		StagedLayoutSet stagedLayoutSet) {
 
@@ -747,6 +755,11 @@ public class StagedLayoutSetStagedModelDataHandler
 				int layoutPriority = GetterUtil.getInteger(
 					layoutElement.attributeValue("layout-priority"));
 
+				layoutPriority = _layoutLocalServiceHelper.getNextPriority(
+					layout.getGroupId(), layout.isPrivateLayout(),
+					layout.getParentLayoutId(),
+					layout.getSourcePrototypeLayoutUuid(), layoutPriority);
+
 				layoutPriorities.put(layout.getPlid(), layoutPriority);
 			}
 		}
@@ -873,6 +886,8 @@ public class StagedLayoutSetStagedModelDataHandler
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;
+
+	private LayoutLocalServiceHelper _layoutLocalServiceHelper;
 
 	@Reference
 	private LayoutSetBranchLocalService _layoutSetBranchLocalService;
