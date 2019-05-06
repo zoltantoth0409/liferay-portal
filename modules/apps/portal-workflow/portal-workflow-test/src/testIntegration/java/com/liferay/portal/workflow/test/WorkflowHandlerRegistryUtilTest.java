3f27f9443e75ef7bc165b34628571e90da6788c6
Leon Chi <leon.chi@liferay.com>
2019-05-06 16:06:23 +0800

    LPS-95165 Move WorkflowHandlerRegistryUtilTest to module and adapt to Arquillian test

---
+++ modules/apps/portal-workflow/portal-workflow-test/src/testIntegration/java/com/liferay/portal/workflow/test/WorkflowHandlerRegistryUtilTest.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.workflow.WorkflowHandler;
+import com.liferay.portal.kernel.workflow.WorkflowHandlerRegistryUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Leon Chi
+ */
+@RunWith(Arquillian.class)
+public class WorkflowHandlerRegistryUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() {
+		Bundle bundle = FrameworkUtil.getBundle(
+			WorkflowHandlerRegistryUtilTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_workflowHandler = (WorkflowHandler)ProxyUtil.newProxyInstance(
+			WorkflowHandler.class.getClassLoader(),
+			new Class<?>[] {WorkflowHandler.class},
+			(proxy, method, args) -> {
+				if ("getClassName".equals(method.getName())) {
+					return _CLASS_NAME;
+				}
+
+				if ("updateStatus".equals(method.getName())) {
+					_calledWorkflowHandler = true;
+				}
+
+				if ("isScopeable".equals(method.getName())) {
+					return false;
+				}
+
+				return null;
+			});
+
+		_serviceRegistration = bundleContext.registerService(
+			WorkflowHandler.class, _workflowHandler,
+			MapUtil.singletonDictionary("service.ranking", Integer.MAX_VALUE));
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_serviceRegistration.unregister();
+	}
+
+	@Test
+	public void testGetWorkflowHandler() {
+		WorkflowHandler<Object> workflowHandler =
+			WorkflowHandlerRegistryUtil.getWorkflowHandler(_CLASS_NAME);
+
+		Assert.assertSame(_workflowHandler, workflowHandler);
+	}
+
+	@Test
+	public void testGetWorkflowHandlers() {
+		List<WorkflowHandler<?>> workflowHandlers =
+			WorkflowHandlerRegistryUtil.getWorkflowHandlers();
+
+		Assert.assertTrue(
+			_CLASS_NAME + " not found in " + workflowHandlers,
+			workflowHandlers.removeIf(
+				workflowHandler -> _workflowHandler == workflowHandler));
+	}
+
+	@Test
+	public void testStartWorkflowInstance1() throws PortalException {
+		_calledWorkflowHandler = false;
+
+		WorkflowHandlerRegistryUtil.startWorkflowInstance(
+			1, 1, 1, _CLASS_NAME, 1, null, new ServiceContext(),
+			new HashMap<>());
+
+		Assert.assertTrue(_calledWorkflowHandler);
+	}
+
+	@Test
+	public void testStartWorkflowInstance2() throws PortalException {
+		_calledWorkflowHandler = false;
+
+		WorkflowHandlerRegistryUtil.startWorkflowInstance(
+			1, 1, 1, _CLASS_NAME, 1, null, new ServiceContext());
+
+		Assert.assertTrue(_calledWorkflowHandler);
+	}
+
+	private static final String _CLASS_NAME = "TestWorkflowHandler";
+
+	private static boolean _calledWorkflowHandler;
+	private static ServiceRegistration<WorkflowHandler> _serviceRegistration;
+	private static WorkflowHandler<?> _workflowHandler;
+
+}
