b03193ada01cebce5d6bb63082fcc7b55a62a05f
Adolfo Pérez <adolfo.perez@liferay.com>
2019-05-06 15:28:39 +0200

    LPS-92696 Create PNG aware input stream

---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/security/io/SafePNGInputStream.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.security.io;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import java.util.Arrays;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class SafePNGInputStream extends InputStream {
+
+	public SafePNGInputStream(BufferedInputStream bufferedInputStream) {
+		_bufferedInputStream = bufferedInputStream;
+	}
+
+	@Override
+	public int read() throws IOException {
+		if (_firstRun) {
+			_detectPNGSignature();
+
+			_firstRun = false;
+			_readForwardByteCount = _PNG_SIGNATURE.length;
+		}
+
+		if (!_png) {
+			return _bufferedInputStream.read();
+		}
+
+		if (_readForwardByteCount > 0) {
+			_readForwardByteCount--;
+
+			return _bufferedInputStream.read();
+		}
+
+		_bufferedInputStream.mark(_CHUNK_LENGTH_SIZE + _CHUNK_ID_SIZE);
+
+		long chunkLength = _readChunkLength();
+
+		if (chunkLength == -1) {
+			_bufferedInputStream.reset();
+
+			return _bufferedInputStream.read();
+		}
+
+		byte[] bytes = new byte[_CHUNK_ID_SIZE];
+
+		int n = _bufferedInputStream.read(bytes);
+
+		if ((n != _CHUNK_ID_SIZE) || !Arrays.equals(_ZTXT_CHUNK_ID, bytes)) {
+			_bufferedInputStream.reset();
+
+			_readForwardByteCount =
+				_CHUNK_LENGTH_SIZE + _CHUNK_ID_SIZE + chunkLength + _CRC_SIZE -
+					1;
+
+			return _bufferedInputStream.read();
+		}
+
+		_bufferedInputStream.skip(chunkLength + _CRC_SIZE);
+
+		return _bufferedInputStream.read();
+	}
+
+	private void _detectPNGSignature() throws IOException {
+		_bufferedInputStream.mark(_PNG_SIGNATURE.length);
+
+		byte[] bytes = new byte[_PNG_SIGNATURE.length];
+
+		int n = _bufferedInputStream.read(bytes);
+
+		if ((n == bytes.length) && Arrays.equals(_PNG_SIGNATURE, bytes)) {
+			_png = true;
+		}
+
+		_bufferedInputStream.reset();
+	}
+
+	private long _readChunkLength() throws IOException {
+		byte[] bytes = new byte[_CHUNK_LENGTH_SIZE * 2];
+
+		Arrays.fill(bytes, (byte)0);
+
+		int n = _bufferedInputStream.read(
+			bytes, bytes.length / 2, _CHUNK_LENGTH_SIZE);
+
+		if (n != _CHUNK_LENGTH_SIZE) {
+			return -1;
+		}
+
+		ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+
+		byteBuffer.order(ByteOrder.BIG_ENDIAN);
+
+		return byteBuffer.getLong();
+	}
+
+	private static final int _CHUNK_ID_SIZE = 4;
+
+	private static final int _CHUNK_LENGTH_SIZE = 4;
+
+	private static final int _CRC_SIZE = 4;
+
+	private static final byte[] _PNG_SIGNATURE = {
+		-119, 80, 78, 71, 13, 10, 26, 10
+	};
+
+	private static final byte[] _ZTXT_CHUNK_ID = {122, 84, 88, 116};
+
+	private final BufferedInputStream _bufferedInputStream;
+	private boolean _firstRun = true;
+	private boolean _png;
+	private long _readForwardByteCount;
+
+}
---
+++ modules/apps/document-library/document-library-service/src/test/java/com/liferay/document/library/internal/security/io/SafePNGInputStreamTest.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.security.io;
+
+import com.liferay.document.library.internal.util.InputStreamUtil;
+import com.liferay.petra.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class SafePNGInputStreamTest {
+
+	@Test
+	public void testChunksOtherThanZTXTArePreserved() throws Exception {
+		InputStream inputStream = _createInputStream(
+			_PNG_SIGNATURE, _MISC_CHUNK1);
+
+		byte[] bytes = new byte[_PNG_SIGNATURE.length + _MISC_CHUNK1.length];
+
+		inputStream.read(bytes);
+
+		Assert.assertArrayEquals(
+			ArrayUtil.append(_PNG_SIGNATURE, _MISC_CHUNK1), bytes);
+
+		Assert.assertEquals(-1, inputStream.read());
+	}
+
+	@Test
+	public void testEmptyInputStreamReturnsNoData() throws Exception {
+		InputStream inputStream = _createInputStream(new byte[0]);
+
+		Assert.assertEquals(-1, inputStream.read());
+	}
+
+	@Test
+	public void testPNGSignatureIsPreserved() throws Exception {
+		InputStream inputStream = _createInputStream(_PNG_SIGNATURE);
+
+		byte[] bytes = new byte[_PNG_SIGNATURE.length];
+
+		inputStream.read(bytes);
+
+		Assert.assertArrayEquals(_PNG_SIGNATURE, bytes);
+
+		Assert.assertEquals(-1, inputStream.read());
+	}
+
+	@Test
+	public void testRemainingChunksOrderIsPreserved() throws Exception {
+		InputStream inputStream = _createInputStream(
+			_PNG_SIGNATURE, _MISC_CHUNK1, _ZTXT_CHUNK, _MISC_CHUNK2);
+
+		byte[] bytes = new byte
+			[_PNG_SIGNATURE.length + _MISC_CHUNK1.length + _MISC_CHUNK2.length];
+
+		inputStream.read(bytes);
+
+		Assert.assertArrayEquals(
+			ArrayUtil.append(_PNG_SIGNATURE, _MISC_CHUNK1, _MISC_CHUNK2),
+			bytes);
+
+		Assert.assertEquals(-1, inputStream.read());
+	}
+
+	@Test
+	public void testStreamNotModifiedWhenNotAPNG() throws Exception {
+		InputStream inputStream = _createInputStream(_NOT_PNG_CONTENT_BYTES);
+
+		byte[] bytes = new byte[_NOT_PNG_CONTENT_BYTES.length];
+
+		inputStream.read(bytes);
+
+		Assert.assertArrayEquals(_NOT_PNG_CONTENT_BYTES, bytes);
+	}
+
+	@Test
+	public void testZTXTChunksAreFilteredOut() throws Exception {
+		InputStream inputStream = _createInputStream(
+			_PNG_SIGNATURE, _ZTXT_CHUNK);
+
+		byte[] bytes = new byte[_PNG_SIGNATURE.length];
+
+		inputStream.read(bytes);
+
+		Assert.assertArrayEquals(_PNG_SIGNATURE, bytes);
+
+		Assert.assertEquals(-1, inputStream.read());
+	}
+
+	private static InputStream _createInputStream(byte[]... bytes) {
+		return new SafePNGInputStream(
+			InputStreamUtil.toBufferedInputStream(
+				new UnsyncByteArrayInputStream(ArrayUtil.append(bytes))));
+	}
+
+	private static final byte[] _MISC_CHUNK1 = {
+		0, 0, 0, 4, 42, 84, 88, 116, 0, 0, 0, 0, 0, 0, 0, 0
+	};
+
+	private static final byte[] _MISC_CHUNK2 = {
+		0, 0, 0, 4, 43, 84, 88, 116, 0, 0, 0, 0, 0, 0, 0, 0
+	};
+
+	private static final byte[] _NOT_PNG_CONTENT_BYTES = "text".getBytes();
+
+	private static final byte[] _PNG_SIGNATURE = {
+		-119, 80, 78, 71, 13, 10, 26, 10
+	};
+
+	private static final byte[] _ZTXT_CHUNK = {
+		0, 0, 0, 4, 122, 84, 88, 116, 0, 0, 0, 0, 0, 0, 0, 0
+	};
+
+}
