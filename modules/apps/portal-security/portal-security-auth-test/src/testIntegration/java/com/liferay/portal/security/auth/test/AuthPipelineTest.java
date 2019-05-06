1366949deffc7c38d1ae4835578d26d04a628642
Leon Chi <leon.chi@liferay.com>
2019-05-06 16:28:33 +0800

    LPS-95165 Move AuthPipelineTest to module and adapt to arquillian test

--- modules/apps/portal-security/portal-security-auth-test/src/testIntegration/java/com/liferay/portal/security/auth/test/AuthPipelineTest.java
+++ modules/apps/portal-security/portal-security-auth-test/src/testIntegration/java/com/liferay/portal/security/auth/test/AuthPipelineTest.java
@@ -12,81 +12,126 @@
  * details.
  */
 
-package com.liferay.portal.security.auth;
+package com.liferay.portal.security.auth.test;
 
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.security.auth.AuthException;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.security.auth.AuthFailure;
+import com.liferay.portal.kernel.security.auth.Authenticator;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.security.auth.AuthPipeline;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.SyntheticBundleClassTestRule;
-import com.liferay.portal.util.test.AtomicState;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Philip Jones
  */
+@RunWith(Arquillian.class)
 public class AuthPipelineTest {
 
 	@ClassRule
 	@Rule
-	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			new SyntheticBundleClassTestRule("bundle.authpipeline"));
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
 
 	@BeforeClass
 	public static void setUpClass() {
-		_atomicState = new AtomicState();
+		Bundle bundle = FrameworkUtil.getBundle(AuthPipelineTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		_authFailureServiceRegistration = bundleContext.registerService(
+			AuthFailure.class,
+			(AuthFailure)ProxyUtil.newProxyInstance(
+				AuthFailure.class.getClassLoader(),
+				new Class<?>[] {AuthFailure.class},
+				(proxy, method, args) -> {
+					_calledAuthFailure = true;
+
+					return null;
+				}),
+			new HashMapDictionary<String, Object>() {
+				{
+					put(
+						"key",
+						new String[] {"auth.failure", "auth.max.failures"});
+					put("service.ranking", Integer.MAX_VALUE);
+				}
+			});
+
+		_authenticatorServiceRegistration = bundleContext.registerService(
+			Authenticator.class,
+			(Authenticator)ProxyUtil.newProxyInstance(
+				Authenticator.class.getClassLoader(),
+				new Class<?>[] {Authenticator.class},
+				(proxy, method, args) -> {
+					_calledAuthenticator = true;
+
+					return Authenticator.SUCCESS;
+				}),
+			new HashMapDictionary<String, Object>() {
+				{
+					put("key", "auth.pipeline.pre");
+					put("service.ranking", Integer.MAX_VALUE);
+				}
+			});
 	}
 
 	@AfterClass
 	public static void tearDownClass() {
-		_atomicState.close();
+		_authFailureServiceRegistration.unregister();
+		_authenticatorServiceRegistration.unregister();
+	}
+
+	@Before
+	public void setUp() {
+		_calledAuthenticator = false;
 	}
 
 	@Test
 	public void testAuthenticateByEmailAddress() throws AuthException {
-		_atomicState.reset();
-
 		AuthPipeline.authenticateByEmailAddress(
 			"auth.pipeline.pre", 0, RandomTestUtil.randomString(),
 			RandomTestUtil.randomString(), null, null);
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledAuthenticator);
 	}
 
 	@Test
 	public void testAuthenticateByScreenName() throws AuthException {
-		_atomicState.reset();
-
 		AuthPipeline.authenticateByScreenName(
 			"auth.pipeline.pre", 0, RandomTestUtil.randomString(),
 			RandomTestUtil.randomString(), null, null);
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledAuthenticator);
 	}
 
 	@Test
 	public void testAuthenticateByUserId() throws AuthException {
-		_atomicState.reset();
-
 		AuthPipeline.authenticateByUserId(
 			"auth.pipeline.pre", 0, RandomTestUtil.randomLong(),
 			RandomTestUtil.randomString(), null, null);
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledAuthenticator);
 	}
 
 	@Test
 	public void testOnFailureByScreenName() {
-		_atomicState.reset();
-
 		try {
 			AuthPipeline.onFailureByScreenName(
 				"auth.failure", 0, RandomTestUtil.randomString(), null, null);
@@ -94,13 +139,11 @@ public class AuthPipelineTest {
 		catch (AuthException ae) {
 		}
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledAuthFailure);
 	}
 
 	@Test
 	public void testOnFailureByUserId() {
-		_atomicState.reset();
-
 		try {
 			AuthPipeline.onFailureByUserId(
 				"auth.failure", 0, RandomTestUtil.randomLong(), null, null);
@@ -108,13 +151,11 @@ public class AuthPipelineTest {
 		catch (AuthException ae) {
 		}
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledAuthFailure);
 	}
 
 	@Test
 	public void testOnMaxFailuresByEmailAddress() {
-		_atomicState.reset();
-
 		try {
 			AuthPipeline.onMaxFailuresByEmailAddress(
 				"auth.max.failures", 0, RandomTestUtil.randomString(), null,
@@ -123,13 +164,11 @@ public class AuthPipelineTest {
 		catch (AuthException ae) {
 		}
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledAuthFailure);
 	}
 
 	@Test
 	public void testOnMaxFailuresByScreenName() {
-		_atomicState.reset();
-
 		try {
 			AuthPipeline.onMaxFailuresByScreenName(
 				"auth.max.failures", 0, RandomTestUtil.randomString(), null,
@@ -138,13 +177,11 @@ public class AuthPipelineTest {
 		catch (AuthException ae) {
 		}
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledAuthFailure);
 	}
 
 	@Test
 	public void testOnMaxFailuresByUserId() {
-		_atomicState.reset();
-
 		try {
 			AuthPipeline.onMaxFailuresByUserId(
 				"auth.max.failures", 0, RandomTestUtil.randomLong(), null,
@@ -153,9 +190,14 @@ public class AuthPipelineTest {
 		catch (AuthException ae) {
 		}
 
-		Assert.assertTrue(_atomicState.isSet());
+		Assert.assertTrue(_calledAuthFailure);
 	}
 
-	private static AtomicState _atomicState;
+	private static ServiceRegistration<Authenticator>
+		_authenticatorServiceRegistration;
+	private static ServiceRegistration<AuthFailure>
+		_authFailureServiceRegistration;
+	private static boolean _calledAuthenticator;
+	private static boolean _calledAuthFailure;
 
 }
