d355187c04e67cf59d03a08b4256634087cefc02
Jonathan McCann <jonathan.mccann@liferay.com>
2019-05-06 14:27:35 -0700

    LPS-95151 Pass in the groupId of the Group or Organization that the user is attempting to add roles to so that the roles are appropriately filtered

--- modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/display/context/SelectRoleManagementToolbarDisplayContext.java
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/display/context/SelectRoleManagementToolbarDisplayContext.java
@@ -122,6 +122,13 @@ public class SelectRoleManagementToolbarDisplayContext {
 	public SearchContainer getRoleSearchContainer(boolean filterManageableRoles)
 		throws Exception {
 
+		return getRoleSearchContainer(filterManageableRoles, 0);
+	}
+
+	public SearchContainer getRoleSearchContainer(
+			boolean filterManageableRoles, long groupId)
+		throws Exception {
+
 		if (_roleSearch != null) {
 			return _roleSearch;
 		}
@@ -143,8 +150,14 @@ public class SelectRoleManagementToolbarDisplayContext {
 				new Integer[] {_roleType}, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
 				roleSearch.getOrderByComparator());
 
-			results = UsersAdminUtil.filterRoles(
-				themeDisplay.getPermissionChecker(), results);
+			if (groupId == 0) {
+				results = UsersAdminUtil.filterRoles(
+					themeDisplay.getPermissionChecker(), results);
+			}
+			else {
+				results = UsersAdminUtil.filterGroupRoles(
+					themeDisplay.getPermissionChecker(), groupId, results);
+			}
 
 			total = results.size();
 
