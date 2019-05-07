3970deca62dfef9b9159fb811e185f654cdb5ccd
Hugo Huijser <hugo.huijser@liferay.com>
2019-05-07 15:30:34 +0800

    LPS-95067 Add check to SF

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaServiceTrackerFactoryCheck.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.portal.tools.ToolsUtil;
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaServiceTrackerFactoryCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isPortalCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		Matcher matcher = _serviceTrackerFactoryOpenPattern.matcher(content);
+
+		while (matcher.find()) {
+			if (ToolsUtil.isInsideQuotes(content, matcher.end())) {
+				continue;
+			}
+
+			List<String> parameterList = JavaSourceUtil.getParameterList(
+				content.substring(matcher.start()));
+
+			if (parameterList.size() == 1) {
+				addMessage(
+					fileName,
+					"ServiceTrackerFactory.open(Class clazz) is deprecated " +
+						"since it leaks ServiceTrackers, see LPS-95067",
+					getLineNumber(content, matcher.start()));
+			}
+		}
+
+		return content;
+	}
+
+	private static final Pattern _serviceTrackerFactoryOpenPattern =
+		Pattern.compile("\\WServiceTrackerFactory\\.\\s*open\\(");
+
+}
