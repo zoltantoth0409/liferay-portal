bf808384c96928469eb576051eef2b28adc39c79
Vishal Reddy <vishal.reddy@liferay.com>
2019-05-07 08:44:20 -0500

    LPS-95156 Add assertions to unquote test

--- portal-kernel/test/unit/com/liferay/portal/kernel/util/StringUtilTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/util/StringUtilTest.java
@@ -1080,12 +1080,12 @@ public class StringUtilTest {
 
 	@Test
 	public void testUnquote() {
+		Assert.assertEquals("", StringUtil.unquote(""));
+		Assert.assertEquals("Hello World", StringUtil.unquote("'Hello World'"));
+		Assert.assertEquals("'Hello World", StringUtil.unquote("'Hello World"));
 		Assert.assertEquals(
 			"Hello World", StringUtil.unquote("\"Hello World\""));
-
-		// String with single character
-
-		Assert.assertEquals("\"", StringUtil.unquote("\""));
+		Assert.assertEquals("Hello World", StringUtil.unquote("Hello World"));
 	}
 
 	@Test
