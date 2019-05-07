a03ea76ac0eaeb5f889ddfddb3c2b8fdf7e3caac
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2019-05-07 15:10:53 -0700

    LPS-95219 Update typeSettings to use the new layout-template-id for 1_2_1 columns to what is used now for the 70/30 split on 1_2_1 layout templates

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/upgrade/v_1_0_3/UpgradeLayoutTemplateId.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.upgrade.v_1_0_3;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.StringBundler;
+
+/**
+ * @author Sam Ziemer
+ */
+public class UpgradeLayoutTemplateId extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		updateLayoutTemplateId();
+	}
+
+	protected void updateLayoutTemplateId() throws Exception {
+		String sql = StringBundler.concat(
+			"update Layout set typeSettings = REPLACE(typeSettings, ",
+			"'layout-template-id=1_2_1_columns', ",
+			"'layout-template-id=1_2_1_columns_ii') where typesettings like ",
+			"'%layout-template-id=1_2_1_columns%'");
+
+		runSQL(sql);
+	}
+
+}
