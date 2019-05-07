b849b683e0f53afa3d2ccd71f42e61bb9cae55d5
Peter Shin <peter.shin@liferay.com>
2019-05-07 15:21:43 -0700

    LPS-95133 Fix NPE

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/RESTBuilder.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/RESTBuilder.java
@@ -1171,6 +1171,10 @@ public class RESTBuilder {
 
 			Map<String, Schema> propertySchemas = schema.getPropertySchemas();
 
+			if (propertySchemas == null) {
+				continue;
+			}
+
 			for (Map.Entry<String, Schema> entry2 :
 					propertySchemas.entrySet()) {
 
