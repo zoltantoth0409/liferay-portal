6b2378b0045c1416b6185e3d924b1928ae326498
weicao <wade.cao@liferay.com>
2019-05-07 11:35:33 -0700

    LPS-90630 portal-search-api: Add TitleFieldQueryBuilderConfiguration interface
    https://issues.liferay.com/browse/LPS-90630

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/configuration/TitleFieldQueryBuilderConfiguration.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.configuration;
+
+import aQute.bnd.annotation.ProviderType;
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Wade Cao
+ */
+@ExtendedObjectClassDefinition(category = "search")
+@Meta.OCD(
+	id = "com.liferay.portal.search.configuration.TitleFieldQueryBuilderConfiguration",
+	localization = "content/Language",
+	name = "title-field-query-configuration-name"
+)
+@ProviderType
+public interface TitleFieldQueryBuilderConfiguration {
+
+	@Meta.AD(deflt = "2.0", name = "exact-match-boost", required = false)
+	public float exactMatchBoost();
+
+	@Meta.AD(deflt = "300", name = "max-expansions", required = false)
+	public int maxExpansions();
+
+}
