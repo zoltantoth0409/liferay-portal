189921bbc79bd09d71a66b0716125e79327c7415
Adolfo Pérez <adolfo.perez@liferay.com>
2019-05-08 14:42:00 +0200

    LPS-94316 Generate control panel URLs only when in control panel

---
+++ modules/apps/wiki/wiki-web/src/main/java/com/liferay/wiki/web/internal/portlet/WikiViewPortletProvider.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.web.internal.portlet;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.ViewPortletProvider;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.wiki.constants.WikiPortletKeys;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	immediate = true,
+	property = {
+		"model.class.name=com.liferay.wiki.model.WikiPage",
+		"service.ranking:Integer=100"
+	},
+	service = ViewPortletProvider.class
+)
+public class WikiViewPortletProvider
+	extends BasePortletProvider implements ViewPortletProvider {
+
+	@Override
+	public String getPortletName() {
+		return WikiPortletKeys.WIKI;
+	}
+
+	@Override
+	public PortletURL getPortletURL(
+			HttpServletRequest httpServletRequest, Group group)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		Layout layout = themeDisplay.getLayout();
+
+		if (layout.isTypeControlPanel()) {
+			return super.getPortletURL(httpServletRequest, group);
+		}
+
+		return PortletURLFactoryUtil.create(
+			httpServletRequest, getPortletName(), PortletRequest.RENDER_PHASE);
+	}
+
+}
