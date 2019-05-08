3842080d605047f568ee65f926b594ab40d59f6b
Andrés Fulla <andres.fulla@liferay.com>
2019-05-08 09:47:30 +0200

    LPS-89098 Reporting property not found

---
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/shared/ClayAlert.js
@@ -0,0 +1,74 @@
+import getCN from 'classnames';
+import React, {Component} from 'react';
+import ThemeContext from '../../ThemeContext.es';
+import {PropTypes} from 'prop-types';
+
+/**
+ * React implementation of Clay Alert
+ *
+ *
+ * @class ClayAlert
+ * @extends {Component}
+ */
+class ClayAlert extends Component {
+	static contextType = ThemeContext;
+
+	static propTypes = {
+		className: PropTypes.string,
+		iconName: PropTypes.string.isRequired
+	};
+
+	render() {
+		const {
+			className,
+			title,
+			message,
+			type
+		} = this.props;
+
+		const classes = getCN(
+			'alert',
+			type ? `alert-${type}` : 'alert-info',
+			{
+				[className]: className
+			}
+		);
+
+		const iconName = _getIconNameByAlertType(type);
+
+		return (
+			<div className={classes} role="alert">
+				<span class="alert-indicator">
+					<svg class="lexicon-icon lexicon-icon-check-circle-full" focusable="false" role="presentation">
+						<use href={`${this.context.spritemap}#${iconName}`}></use>
+					</svg>
+				</span>
+				{
+					title &&
+					<strong class="lead">{ title }:</strong>
+				}
+				{
+					message
+				}
+			</div>
+		);
+	}
+}
+
+export default ClayAlert;
+
+function _getIconNameByAlertType(alertType) {
+	let iconName = 'info-circle';
+	switch(alertType) {
+		case 'danger':
+			iconName = 'exclamation-full';
+			break;
+		case 'success':
+			iconName = 'lexicon-icon-outline';
+			break;
+		case 'warning':
+			iconName = 'warning-full';
+			break;
+	}
+	return iconName;
+}
