9e4394c12f0c8fe28eb2ebfb1937369eb9908e3f
Sarai Díaz García <dgarciasarai@gmail.com>
2019-05-08 16:59:53 +0200

    LPS-94575 Update tests

--- modules/apps/segments/segments-web/test/js/components/inputs/DateTimeInput.es.js
+++ modules/apps/segments/segments-web/test/js/components/inputs/DateTimeInput.es.js
@@ -1,7 +1,8 @@
+import dateFns from 'date-fns';
 import DateTimeInput from 'components/inputs/DateTimeInput.es';
 import React from 'react';
 import {cleanup, render} from 'react-testing-library';
-import {testControlledInput} from 'test/utils';
+import {testControlledDateInput} from 'test/utils';
 
 const DATE_INPUT_TESTID = 'date-input';
 
@@ -28,12 +29,46 @@ describe(
 
 				const element = getByTestId(DATE_INPUT_TESTID);
 
-				testControlledInput(
+				testControlledDateInput(
 					{
 						element,
-						mockFunc: mockOnChange,
+						mockOnChangeFunc: mockOnChange,
 						newValue: '2019-01-24',
-						newValueExpected: '2019-01-24T00:00:00.000Z',
+						newValueExpected: '2019-01-24',
+						newValueOnChange: '2019-01-24T00:00:00.000Z',
+						value: defaultNumberValue
+					}
+				);
+			}
+		);
+
+		it(
+			'should render now with wrong date',
+			() => {
+				const mockOnChange = jest.fn();
+
+				const defaultNumberValue = '2019-01-23';
+
+				const {asFragment, getByTestId} = render(
+					<DateTimeInput
+						onChange={mockOnChange}
+						value={defaultNumberValue}
+					/>
+				);
+
+				expect(asFragment()).toMatchSnapshot();
+
+				const element = getByTestId(DATE_INPUT_TESTID);
+
+				const date = dateFns.format(new Date(), 'YYYY-MM-DD');
+
+				testControlledDateInput(
+					{
+						element,
+						mockOnChangeFunc: mockOnChange,
+						newValue: '2019-01-XX',
+						newValueExpected: date,
+						newValueOnChange: dateFns.parse(date, 'YYYY-MM-DD').toISOString(),
 						value: defaultNumberValue
 					}
 				);
