bcf41513e3d2719d695de781af89832e9b3c1621
André de Oliveira <andre.oliveira@liferay.com>
2019-05-08 10:09:38 -0700

    LPS-95127 portal-search-test-util: Maximize isolation of documents created by different tests, by using test method name as filter criteria
    https://issues.liferay.com/browse/LPS-95127

--- modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/count/BaseCountTestCase.java
+++ modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/count/BaseCountTestCase.java
@@ -14,19 +14,14 @@
 
 package com.liferay.portal.search.test.util.count;
 
-import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.IndexSearcher;
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
 import com.liferay.portal.kernel.search.Query;
-import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.TermFilter;
 import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
-import com.liferay.portal.kernel.test.randomizerbumpers.UniqueStringRandomizerBumper;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.search.test.util.IdempotentRetryAssert;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.search.test.util.indexing.BaseIndexingTestCase;
 import com.liferay.portal.search.test.util.indexing.DocumentCreationHelpers;
-
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -47,76 +42,76 @@ public abstract class BaseCountTestCase extends BaseIndexingTestCase {
 		for (int i = 0; i < _TOTAL_DOCUMENTS; i++) {
 			addDocument(
 				DocumentCreationHelpers.singleText(
-					"test-field",
-					RandomTestUtil.randomString(
-						UniqueStringRandomizerBumper.INSTANCE)));
+					_FIELD, StringUtil.toLowerCase(testName.getMethodName())));
 		}
 	}
 
 	@Test
 	public void testAll() throws Exception {
-		_assertCount(createSearchContext());
+		assertSearch(
+			indexingTestHelper -> Assert.assertEquals(
+				_TOTAL_DOCUMENTS, indexingTestHelper.searchCount()));
 	}
 
 	@Test
 	public void testPaginationIsIgnored() throws Exception {
-		SearchContext searchContext = createSearchContext();
+		assertSearch(
+			indexingTestHelper -> {
+				indexingTestHelper.define(
+					searchContext -> {
+						int start = 5;
 
-		int start = 5;
+						searchContext.setEnd(start - 1);
+						searchContext.setStart(start);
+					});
 
-		searchContext.setEnd(start - 1);
-		searchContext.setStart(start);
-
-		_assertCount(searchContext);
+				Assert.assertEquals(
+					_TOTAL_DOCUMENTS, indexingTestHelper.searchCount());
+			});
 	}
 
 	@Test
 	public void testPostFilterWithoutMainQuery() throws Exception {
-		Query query = new BooleanQueryImpl();
+		assertSearch(
+			indexingTestHelper -> {
+				Query query = new BooleanQueryImpl();
 
-		query.setPostFilter(_createBooleanFilter());
+				query.setPostFilter(_createBooleanFilter());
 
-		_assertCount(query, createSearchContext());
+				indexingTestHelper.setQuery(query);
+
+				Assert.assertEquals(
+					_TOTAL_DOCUMENTS, indexingTestHelper.searchCount());
+			});
 	}
 
 	@Test
 	public void testPreFilterWithoutMainQuery() throws Exception {
-		Query query = new BooleanQueryImpl();
+		assertSearch(
+			indexingTestHelper -> {
+				Query query = new BooleanQueryImpl();
 
-		query.setPreBooleanFilter(_createBooleanFilter());
+				query.setPreBooleanFilter(_createBooleanFilter());
 
-		_assertCount(query, createSearchContext());
-	}
+				indexingTestHelper.setQuery(query);
 
-	private void _assertCount(Query query, SearchContext searchContext)
-		throws Exception {
-
-		IdempotentRetryAssert.retryAssert(
-			3, TimeUnit.SECONDS,
-			() -> {
-				IndexSearcher indexSearcher = getIndexSearcher();
-
-				long count = indexSearcher.searchCount(searchContext, query);
-
-				Assert.assertEquals(_TOTAL_DOCUMENTS, count);
-
-				return null;
+				Assert.assertEquals(
+					_TOTAL_DOCUMENTS, indexingTestHelper.searchCount());
 			});
-	}
-
-	private void _assertCount(SearchContext searchContext) throws Exception {
-		Query query = getDefaultQuery();
-
-		_assertCount(query, searchContext);
 	}
 
 	private BooleanFilter _createBooleanFilter() {
 		BooleanFilter booleanFilter = new BooleanFilter();
 
-		booleanFilter.addRequiredTerm(Field.GROUP_ID, GROUP_ID);
+		booleanFilter.add(
+			new TermFilter(
+				_FIELD, StringUtil.toLowerCase(testName.getMethodName())),
+			BooleanClauseOccur.MUST);
 
 		return booleanFilter;
 	}
+
+	private static final String _FIELD = "test-field";
 
 	private static final int _TOTAL_DOCUMENTS = 20;
 
