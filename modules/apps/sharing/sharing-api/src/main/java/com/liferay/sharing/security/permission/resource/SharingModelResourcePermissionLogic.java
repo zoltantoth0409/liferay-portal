0fc64186f9bb9ca1f167b4d70426386a68ec28ac
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-05-09 17:33:24 +0200

    LPS-86435 Exposing sharing permission logic to be used directly from the original model resource permission registrar

---
+++ modules/apps/sharing/sharing-api/src/main/java/com/liferay/sharing/security/permission/resource/SharingModelResourcePermissionLogic.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sharing.security.permission.resource;
+
+import com.liferay.portal.kernel.model.GroupedModel;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionLogic;
+
+/**
+ * @author Alejandro Tardín
+ */
+public interface SharingModelResourcePermissionLogic<T extends GroupedModel>
+	extends ModelResourcePermissionLogic<T> {
+}
