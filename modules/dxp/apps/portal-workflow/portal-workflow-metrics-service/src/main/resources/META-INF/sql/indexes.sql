b5b104297c1c2077b085258ed773c6a618b9ee71
Brian Chan <brian.chan@liferay.com>
2019-05-09 13:49:05 -0700

    LPS-89119 guys, we need to remove old tables manually (I deleted the sql files and regenerated)

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,10 +1,3 @@
-create index IX_A80FF93C on WorkflowMetricsSLACalendar (uuid_[$COLUMN_LENGTH:75$], companyId);
-create unique index IX_8A7D9EBE on WorkflowMetricsSLACalendar (uuid_[$COLUMN_LENGTH:75$], groupId);
-
-create index IX_E9612291 on WorkflowMetricsSLACondition (companyId, workflowMetricsSLADefinitionId);
-create index IX_3EE5A191 on WorkflowMetricsSLACondition (uuid_[$COLUMN_LENGTH:75$], companyId);
-create unique index IX_81DB5853 on WorkflowMetricsSLACondition (uuid_[$COLUMN_LENGTH:75$], groupId);
-
 create index IX_467A6470 on WorkflowMetricsSLADefinition (companyId, name[$COLUMN_LENGTH:4000$], processId);
 create index IX_C56FD8D2 on WorkflowMetricsSLADefinition (companyId, processId, processVersion[$COLUMN_LENGTH:75$], status);
 create index IX_6C52ED35 on WorkflowMetricsSLADefinition (companyId, processId, status);
--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,28 +1,3 @@
-create table WorkflowMetricsSLACalendar (
-	mvccVersion LONG default 0 not null,
-	uuid_ VARCHAR(75) null,
-	workflowMetricsSLACalendarId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null
-);
-
-create table WorkflowMetricsSLACondition (
-	mvccVersion LONG default 0 not null,
-	uuid_ VARCHAR(75) null,
-	workflowMetricsSLAConditionId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	workflowMetricsSLADefinitionId LONG
-);
-
 create table WorkflowMetricsSLADefinition (
 	mvccVersion LONG default 0 not null,
 	uuid_ VARCHAR(75) null,
