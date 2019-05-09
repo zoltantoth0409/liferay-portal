7e20991c05e66d565abe9fd86dc74fc6024654fb
Andrés Fulla <andres.fulla@liferay.com>
2019-05-09 16:51:25 +0200

    LPS-94575 Removing unnecessary argument

--- modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/inputs/DateInput.es.js
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/inputs/DateInput.es.js
@@ -19,7 +19,7 @@ class DateInput extends React.Component {
 		};
 	}
 
-	shouldComponentUpdate(nextProps, nextState) {
+	shouldComponentUpdate(nextProps) {
 		if (this.props.value != nextProps.value) {
 			this.setState({value: nextProps.value});
 		}
