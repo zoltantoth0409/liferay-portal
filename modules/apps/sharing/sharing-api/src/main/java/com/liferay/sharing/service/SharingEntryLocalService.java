33e0fc025362f3ac99e77cf9ffa7e8af66d3a85b
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-05-10 13:37:38 +0200

    LPS-95358 Generated

--- modules/apps/sharing/sharing-api/src/main/java/com/liferay/sharing/service/SharingEntryLocalService.java
+++ modules/apps/sharing/sharing-api/src/main/java/com/liferay/sharing/service/SharingEntryLocalService.java
@@ -622,7 +622,12 @@ public interface SharingEntryLocalService
 	 sharing entry actions are invalid (e.g., empty, don't contain
 	 {@code SharingEntryAction#VIEW}, or contain a {@code null}
 	 value), or if the expiration date is a past value
+	 * @deprecated As of Mueller (7.2.x), replaced by {@link
+	 SharingEntryLocalService#
+	 updateSharingEntry(
+	 long, long, Collection, boolean, Date, ServiceContext)}
 	 */
+	@Deprecated
 	public SharingEntry updateSharingEntry(
 			long sharingEntryId,
 			Collection<SharingEntryAction> sharingEntryActions,
