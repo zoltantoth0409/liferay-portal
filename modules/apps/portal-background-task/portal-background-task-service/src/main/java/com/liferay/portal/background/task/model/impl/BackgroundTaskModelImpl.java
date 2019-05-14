efb801ccf83094b589a4764af507ad393d3c9147
Lance Ji <zheyi.ji@liferay.com>
2019-05-13 17:11:17 -0700

    LPS-95408 Build service

--- modules/apps/portal-background-task/portal-background-task-service/src/main/java/com/liferay/portal/background/task/model/impl/BackgroundTaskModelImpl.java
+++ modules/apps/portal-background-task/portal-background-task-service/src/main/java/com/liferay/portal/background/task/model/impl/BackgroundTaskModelImpl.java
@@ -123,21 +123,6 @@ public class BackgroundTaskModelImpl
 
 	public static final String TX_MANAGER = "liferayTransactionManager";
 
-	public static final boolean ENTITY_CACHE_ENABLED = GetterUtil.getBoolean(
-		com.liferay.portal.background.task.service.util.ServiceProps.get(
-			"value.object.entity.cache.enabled.com.liferay.portal.background.task.model.BackgroundTask"),
-		true);
-
-	public static final boolean FINDER_CACHE_ENABLED = GetterUtil.getBoolean(
-		com.liferay.portal.background.task.service.util.ServiceProps.get(
-			"value.object.finder.cache.enabled.com.liferay.portal.background.task.model.BackgroundTask"),
-		true);
-
-	public static final boolean COLUMN_BITMASK_ENABLED = GetterUtil.getBoolean(
-		com.liferay.portal.background.task.service.util.ServiceProps.get(
-			"value.object.column.bitmask.enabled.com.liferay.portal.background.task.model.BackgroundTask"),
-		true);
-
 	public static final long COMPANYID_COLUMN_BITMASK = 1L;
 
 	public static final long COMPLETED_COLUMN_BITMASK = 2L;
@@ -151,6 +136,14 @@ public class BackgroundTaskModelImpl
 	public static final long TASKEXECUTORCLASSNAME_COLUMN_BITMASK = 32L;
 
 	public static final long CREATEDATE_COLUMN_BITMASK = 64L;
+
+	public static void setEntityCacheEnabled(boolean entityCacheEnabled) {
+		_entityCacheEnabled = entityCacheEnabled;
+	}
+
+	public static void setFinderCacheEnabled(boolean finderCacheEnabled) {
+		_finderCacheEnabled = finderCacheEnabled;
+	}
 
 	/**
 	 * Converts the soap model instance into a normal model instance.
@@ -207,10 +200,6 @@ public class BackgroundTaskModelImpl
 
 		return models;
 	}
-
-	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(
-		com.liferay.portal.background.task.service.util.ServiceProps.get(
-			"lock.expiration.time.com.liferay.portal.background.task.model.BackgroundTask"));
 
 	public BackgroundTaskModelImpl() {
 	}
@@ -793,12 +782,12 @@ public class BackgroundTaskModelImpl
 
 	@Override
 	public boolean isEntityCacheEnabled() {
-		return ENTITY_CACHE_ENABLED;
+		return _entityCacheEnabled;
 	}
 
 	@Override
 	public boolean isFinderCacheEnabled() {
-		return FINDER_CACHE_ENABLED;
+		return _finderCacheEnabled;
 	}
 
 	@Override
@@ -1001,6 +990,8 @@ public class BackgroundTaskModelImpl
 	private static final Class<?>[] _escapedModelInterfaces = new Class[] {
 		BackgroundTask.class, ModelWrapper.class
 	};
+	private static boolean _entityCacheEnabled;
+	private static boolean _finderCacheEnabled;
 
 	private long _mvccVersion;
 	private long _backgroundTaskId;
--- modules/apps/portal-background-task/portal-background-task-service/src/main/java/com/liferay/portal/background/task/service/persistence/impl/BackgroundTaskFinderBaseImpl.java
+++ modules/apps/portal-background-task/portal-background-task-service/src/main/java/com/liferay/portal/background/task/service/persistence/impl/BackgroundTaskFinderBaseImpl.java
@@ -16,41 +16,54 @@ package com.liferay.portal.background.task.service.persistence.impl;
 
 import com.liferay.portal.background.task.model.BackgroundTask;
 import com.liferay.portal.background.task.service.persistence.BackgroundTaskPersistence;
-import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portal.background.task.service.persistence.impl.constants.BackgroundTaskPersistenceConstants;
+import com.liferay.portal.kernel.configuration.Configuration;
+import com.liferay.portal.kernel.dao.orm.SessionFactory;
 import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
+
+import javax.sql.DataSource;
+
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
  * @generated
  */
-public class BackgroundTaskFinderBaseImpl
+public abstract class BackgroundTaskFinderBaseImpl
 	extends BasePersistenceImpl<BackgroundTask> {
 
 	public BackgroundTaskFinderBaseImpl() {
 		setModelClass(BackgroundTask.class);
 	}
 
-	/**
-	 * Returns the background task persistence.
-	 *
-	 * @return the background task persistence
-	 */
-	public BackgroundTaskPersistence getBackgroundTaskPersistence() {
-		return backgroundTaskPersistence;
+	@Override
+	@Reference(
+		target = BackgroundTaskPersistenceConstants.ORIGIN_BUNDLE_SYMBOLIC_NAME_FILTER,
+		unbind = "-"
+	)
+	public void setConfiguration(Configuration configuration) {
+		super.setConfiguration(configuration);
 	}
 
-	/**
-	 * Sets the background task persistence.
-	 *
-	 * @param backgroundTaskPersistence the background task persistence
-	 */
-	public void setBackgroundTaskPersistence(
-		BackgroundTaskPersistence backgroundTaskPersistence) {
-
-		this.backgroundTaskPersistence = backgroundTaskPersistence;
+	@Override
+	@Reference(
+		target = BackgroundTaskPersistenceConstants.ORIGIN_BUNDLE_SYMBOLIC_NAME_FILTER,
+		unbind = "-"
+	)
+	public void setDataSource(DataSource dataSource) {
+		super.setDataSource(dataSource);
 	}
 
-	@BeanReference(type = BackgroundTaskPersistence.class)
+	@Override
+	@Reference(
+		target = BackgroundTaskPersistenceConstants.ORIGIN_BUNDLE_SYMBOLIC_NAME_FILTER,
+		unbind = "-"
+	)
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		super.setSessionFactory(sessionFactory);
+	}
+
+	@Reference
 	protected BackgroundTaskPersistence backgroundTaskPersistence;
 
 }
