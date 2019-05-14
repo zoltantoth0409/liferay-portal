fc0574d7cb64e844e8c25a1876fecd2f52edd4db
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-05-14 15:21:02 -0300

    LPS-92053|LPS-95362 Add InstanceItemDetail component.

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-dashboard/instance-list/InstanceItemDetail.js
@@ -0,0 +1,289 @@
+import { AppContext } from '../../AppContext';
+import Icon from '../../../shared/components/Icon';
+import moment from 'moment';
+import React from 'react';
+
+class InstanceItemDetail extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		const { instanceId, processId } = nextProps;
+
+		if (instanceId) {
+			this.fetchData(instanceId, processId).then(data => this.setState(data));
+		}
+	}
+
+	fetchData(instanceId, processId) {
+		return this.context.client
+			.get(`/processes/${processId}/instances/${instanceId}`)
+			.then(({ data }) => data);
+	}
+
+	render() {
+		const { instanceId } = this.props;
+		const {
+			assetTitle,
+			assetType,
+			dateCreated,
+			slaResults = [],
+			slaStatus,
+			status,
+			taskNames = [],
+			userName
+		} = this.state;
+
+		const slaResolved = slaResults.filter(({ status }) => status === 'Stopped');
+		const slaOpen = slaResults.filter(({ status }) =>
+			['Running', 'Paused'].includes(status)
+		);
+
+		let currentStepText = Liferay.Language.get('current-step');
+
+		let styleName = 'text-danger';
+
+		const empty = slaResults.length === 0;
+		const overdue = slaStatus === 'Overdue';
+
+		if (
+			slaStatus === 'Completed' &&
+			slaResolved.length > 1 &&
+			slaOpen.length === 0
+		) {
+			currentStepText = Liferay.Language.get('end-date');
+		}
+
+		if (status === 'Pending' && slaStatus === 'OnTime') {
+			styleName = 'text-success';
+		}
+		else if (status === 'Completed' || empty) {
+			styleName = 'text-secondary';
+		}
+
+		return (
+			<div
+				aria-labelledby="instanceDetailModalLabel"
+				className="fade modal"
+				id="instanceDetailModal"
+				role="dialog"
+				style={{ display: 'none' }}
+				tabIndex="-1"
+			>
+				<div className="modal-dialog modal-lg">
+					<div className="modal-content">
+						<div className="modal-header">
+							<div
+								className="font-weight-medium modal-title"
+								id="instanceDetailModalLabel"
+							>
+								<span className={`modal-title-indicator ${styleName}`}>
+									{empty ? (
+										<InstanceItemDetail.EmptyIcon />
+									) : overdue ? (
+										<InstanceItemDetail.OverdueIcon />
+									) : (
+										<InstanceItemDetail.OnTimeIcon />
+									)}
+								</span>
+
+								{`${Liferay.Language.get('item')} #${instanceId}`}
+							</div>
+							<button
+								aria-labelledby="Close"
+								className="close"
+								data-dismiss="modal"
+								role="button"
+								type="button"
+							>
+								<Icon iconName="times" />
+							</button>
+						</div>
+						<div className="modal-body">
+							<InstanceItemDetail.SectionTitle>
+								{Liferay.Language.get('due-date-by-sla')}
+							</InstanceItemDetail.SectionTitle>
+
+							{slaResults.length === 0 && (
+								<p>
+									<span className="font-weight-medium text-muted">
+										{Liferay.Language.get('no-sla-records-for-this-item')}
+									</span>
+								</p>
+							)}
+
+							{!!slaOpen.length && (
+								<InstanceItemDetail.SectionSubTitle>
+									{`${Liferay.Language.get('open').toUpperCase()} (${
+										slaOpen.length
+									})`}
+								</InstanceItemDetail.SectionSubTitle>
+							)}
+
+							{slaOpen.map(item => (
+								<InstanceItemDetail.Item key={item.id} {...item} />
+							))}
+
+							{!!slaResolved.length && (
+								<InstanceItemDetail.SectionSubTitle>
+									{`${Liferay.Language.get('resolved').toUpperCase()} (${
+										slaResolved.length
+									})`}
+								</InstanceItemDetail.SectionSubTitle>
+							)}
+
+							{slaResolved.map(item => (
+								<InstanceItemDetail.Item key={item.id} {...item} />
+							))}
+
+							<InstanceItemDetail.SectionTitle className="mt-5">
+								{Liferay.Language.get('process-details')}
+							</InstanceItemDetail.SectionTitle>
+
+							<InstanceItemDetail.SectionAttribute
+								description={Liferay.Language.get('process-status')}
+								detail={status}
+							/>
+
+							<InstanceItemDetail.SectionAttribute
+								description={Liferay.Language.get('created-by')}
+								detail={userName}
+							/>
+
+							{!!dateCreated && (
+								<InstanceItemDetail.SectionAttribute
+									description={Liferay.Language.get('creation-date')}
+									detail={moment(dateCreated).format('MMM DD, LT')}
+								/>
+							)}
+
+							<InstanceItemDetail.SectionAttribute
+								description={Liferay.Language.get('asset-type')}
+								detail={assetType}
+							/>
+
+							<InstanceItemDetail.SectionAttribute
+								description={Liferay.Language.get('asset-title')}
+								detail={assetTitle}
+							/>
+
+							<InstanceItemDetail.SectionAttribute
+								description={currentStepText}
+								detail={taskNames.join(', ')}
+							/>
+
+							<a
+								className="btn btn-secondary btn-sm mb-1 font-weight-medium mt-3"
+								href={`/group/control_panel/manage/-/workflow_instance/view/${instanceId}`}
+								target="_blank"
+							>
+								{Liferay.Language.get('go-to-submission-page')}
+
+								<span className="inline-item inline-item-after">
+									<Icon iconName="shortcut" />
+								</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+InstanceItemDetail.Item = ({
+	dateOverdue,
+	name,
+	onTime,
+	remainingTime,
+	status
+}) => {
+	let bgColor = 'danger';
+	let iconClassName = 'text-danger';
+	let iconName = 'exclamation-circle';
+	let statusText = `(${Liferay.Language.get('resolved-overdue')})`;
+
+	if (onTime) {
+		bgColor = 'success';
+		iconClassName = 'text-success';
+		iconName = 'check-circle';
+	}
+
+	if (status === 'Stopped' && onTime) {
+		statusText = `(${Liferay.Language.get('resolved-on-time')})`;
+	}
+	else if (status === 'Running') {
+		const remainingTimePositive = onTime ? remainingTime : remainingTime * -1;
+
+		const durationText = moment
+			.utc(remainingTimePositive)
+			.format('d[d] hh[h] mm[min]');
+		let onTimeText = Liferay.Language.get('overdue');
+
+		if (onTime) {
+			onTimeText = Liferay.Language.get('left');
+		}
+		statusText = `${moment(dateOverdue).format(
+			'MMM DD, LT'
+		)} (${durationText} ${onTimeText})`;
+	}
+	else if (status === 'Paused') {
+		statusText = `(${Liferay.Language.get('sla-paused')})`;
+	}
+
+	return (
+		<p>
+			<span
+				className={`bg-${bgColor}-light sticker`}
+				style={{ height: '1.5rem', width: '1.5rem' }}
+			>
+				<span className="inline-item" style={{ fontSize: '10px' }}>
+					<Icon elementClasses={iconClassName} iconName={iconName} />
+				</span>
+			</span>
+
+			<span className="font-weight-medium small text-secondary">{` ${name} `}</span>
+
+			<span className="small">{statusText}</span>
+		</p>
+	);
+};
+
+InstanceItemDetail.SectionTitle = ({ children, className = '' }) => {
+	const classNames = `${className} font-weight-medium mb-4`;
+
+	return <h4 className={classNames}>{children}</h4>;
+};
+
+InstanceItemDetail.SectionSubTitle = ({ children }) => {
+	return (
+		<h5 className="font-weight-medium mb-4 mt-4 text-secondary">{children}</h5>
+	);
+};
+
+InstanceItemDetail.SectionAttribute = ({ description, detail }) => {
+	return (
+		<p className="row">
+			<span className="col-2 font-weight-medium small text-secondary">{`${description} `}</span>
+
+			<span className="col small">{detail}</span>
+		</p>
+	);
+};
+
+InstanceItemDetail.OverdueIcon = () => {
+	return <Icon iconName="exclamation-circle" />;
+};
+
+InstanceItemDetail.OnTimeIcon = () => {
+	return <Icon iconName="check-circle" />;
+};
+
+InstanceItemDetail.EmptyIcon = () => {
+	return <Icon iconName="hr" />;
+};
+
+InstanceItemDetail.contextType = AppContext;
+export default InstanceItemDetail;
