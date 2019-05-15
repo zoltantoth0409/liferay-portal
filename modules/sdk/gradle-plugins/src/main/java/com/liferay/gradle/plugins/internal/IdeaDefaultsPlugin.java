9cbef10a2c376838fd8d34db2023bfbfb8415f61
Peter Shin <peter.shin@liferay.com>
2019-05-15 12:47:43 -0700

    LPS-95442 Auto SF

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/IdeaDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/IdeaDefaultsPlugin.java
@@ -31,7 +31,6 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.XmlProvider;
-import org.gradle.api.file.FileCollection;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetOutput;
