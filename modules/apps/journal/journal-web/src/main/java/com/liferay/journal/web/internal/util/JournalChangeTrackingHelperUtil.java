21923a325e4fc5c1468de3e797f72dbeb49d7c6c
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-05-15 12:54:51 +0200

    LPS-95172 Re-using the same pattern we have for JournalHelper and create a utility class for easier invocation from non-osgi services

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/util/JournalChangeTrackingHelperUtil.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.util;
+
+import com.liferay.journal.util.JournalChangeTrackingHelper;
+import com.liferay.petra.string.StringPool;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Daniel Kocsis
+ */
+@Component(immediate = true, service = {})
+public class JournalChangeTrackingHelperUtil {
+
+	public static String getJournalArticleCTCollectionName(
+		long userId, long classPK) {
+
+		if (_journalChangeTrackingHelper == null) {
+			return StringPool.BLANK;
+		}
+
+		return _journalChangeTrackingHelper.getJournalArticleCTCollectionName(
+			userId, classPK);
+	}
+
+	public static boolean hasActiveCTCollection(long companyId, long userId) {
+		if (_journalChangeTrackingHelper == null) {
+			return false;
+		}
+
+		return _journalChangeTrackingHelper.hasActiveCTCollection(
+			companyId, userId);
+	}
+
+	public static boolean isJournalArticleInChangeList(
+		long userId, long classPK) {
+
+		if (_journalChangeTrackingHelper == null) {
+			return false;
+		}
+
+		return _journalChangeTrackingHelper.isJournalArticleInChangeList(
+			userId, classPK);
+	}
+
+	@Reference(unbind = "-")
+	public void setJournalChangeTrackingHelper(
+		JournalChangeTrackingHelper journalChangeTrackingHelper) {
+
+		_journalChangeTrackingHelper = journalChangeTrackingHelper;
+	}
+
+	private static JournalChangeTrackingHelper _journalChangeTrackingHelper;
+
+}
