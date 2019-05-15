dfcc95719269217b8a9fdee954ebaabee0e55dcd
Zoltán Takács <zoltan.takacs@liferay.com>
2019-05-15 12:23:16 +0200

    COMMERCE-1417 Update OSGiTLiferay Integration test and explicitly declare the version for Felix

--- modules/etl/talend/talend-definition/src/test/java/com/liferay/talend/OSGiTLiferayInputTestIT.java
+++ modules/etl/talend/talend-definition/src/test/java/com/liferay/talend/OSGiTLiferayInputTestIT.java
@@ -16,13 +16,16 @@ package com.liferay.talend;
 
 import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.linkBundle;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
 
 import org.junit.runner.RunWith;
 
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.OptionUtils;
 import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.options.MavenArtifactProvisionOption;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
@@ -37,15 +40,29 @@ public class OSGiTLiferayInputTestIT extends LiferayTestBase {
 
 	@Configuration
 	public Option[] config() {
-		Option compositeOption = composite(
-			ComponentsPaxExamOptions.getOptions());
+		Option[] options = ComponentsPaxExamOptions.getOptions();
+
+		Option[] updatedOptions = OptionUtils.remove(
+			MavenArtifactProvisionOption.class, options);
+
 		Option bundleOption1 = linkBundle(
 			"org.talend.components-components-common-bundle");
 		Option bundleOption2 = linkBundle(
 			"com.liferay-com.liferay.talend.definition");
+		Option apacheFelixOption = composite(
+			options(
+				mavenBundle(
+				).groupId(
+					"org.apache.felix"
+				).artifactId(
+					"org.apache.felix.scr"
+				).version(
+					"1.6.0"
+				)));
 
 		return options(
-			composite(compositeOption), bundleOption1, bundleOption2);
+			composite(updatedOptions), apacheFelixOption, bundleOption1,
+			bundleOption2);
 	}
 
 }
