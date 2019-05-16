44cca037eacd6d7b06005a4587c5798c2ad08563
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-05-16 17:32:51 -0300

    LPS-92053|LPS-95362 Fix Icon component.

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/Icon.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/Icon.js
@@ -2,8 +2,20 @@ import getCN from 'classnames';
 import React from 'react';
 
 export default class Icon extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			elementClasses: props.elementClasses || '',
+			iconName: props.iconName || ''
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.setState(nextProps);
+	}
+
 	render() {
-		const { elementClasses, iconName } = this.props;
+		const { elementClasses, iconName } = this.state;
 		const classes = getCN(
 			'lexicon-icon',
 			`lexicon-icon-${iconName}`,
