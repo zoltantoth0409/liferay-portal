31be448cf133aca5a69b0246f39836d3155e1192
Sandro Chinea <sandrochinea@gmail.com>
2019-05-16 14:45:37 +0100

    LPS-95178 Added functions to get Update Columns action

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/actions/updateRowColumns.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/actions/updateRowColumns.es.js
@@ -1,4 +1,80 @@
+import {getRowIndex} from '../utils/FragmentsEditorGetUtils.es';
+import {setIn} from '../utils/FragmentsEditorUpdateUtils.es';
+import {updatePageEditorLayoutData} from '../utils/FragmentsEditorFetchUtils.es';
 import {UPDATE_ROW_COLUMNS_ERROR, UPDATE_ROW_COLUMNS_LOADING, UPDATE_ROW_COLUMNS_SUCCESS} from './actions.es';
+
+/**
+ * @param {function} dispatch
+ * @param {number} rowIndex
+ * @review
+ */
+function updateRowColumns(
+	dispatch,
+	layoutData,
+	previousData,
+	segmentsExperienceId
+) {
+	updatePageEditorLayoutData(
+		layoutData,
+		segmentsExperienceId
+	).then(
+		() => {
+			dispatch(
+				updateRowColumnsSuccessAction()
+			);
+		}
+	).catch(
+		() => {
+			dispatch(
+				updateRowColumnsErrorAction(previousData)
+			);
+		}
+	);
+}
+
+/**
+ * @param {Array} columns
+ * @param {string} rowId
+ * @return {function}
+ * @review
+ */
+function updateRowColumnsAction(columns, rowId) {
+	return function(dispatch, getState) {
+		const state = getState();
+
+		const rowIndex = getRowIndex(
+			state.layoutData.structure,
+			rowId
+		);
+
+		const previousData = state.layoutData;
+
+		let nextData = previousData;
+
+		if (rowIndex !== -1) {
+			nextData = setIn(
+				previousData,
+				[
+					'structure',
+					rowIndex.toString(),
+					'columns'
+				],
+				columns
+			);
+		}
+
+		dispatch(
+			updateRowColumnsLoadingAction(nextData)
+		);
+
+		updateRowColumns(
+			dispatch,
+			nextData,
+			previousData,
+			state.segmentsExperienceId,
+		);
+	};
+}
 
 /**
  * @param {Array} layoutData
@@ -33,3 +109,7 @@ function updateRowColumnsSuccessAction() {
 		type: UPDATE_ROW_COLUMNS_SUCCESS
 	};
 }
+
+export {
+	updateRowColumnsAction
+};
