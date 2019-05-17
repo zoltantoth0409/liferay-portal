d84334ff85e7d31e26d452f73859c5c64a91cca6
oliveiraaraujo <alexandre.araujo@liferay.com>
2019-05-17 12:03:18 -0300

    LPS-92053|LPS-95745 Fix update icon.

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/Icon.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/components/Icon.js
@@ -2,32 +2,23 @@ import getCN from 'classnames';
 import React from 'react';
 
 export default class Icon extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			elementClasses: props.elementClasses || '',
-			iconName: props.iconName || ''
-		};
-	}
-
-	componentWillReceiveProps(nextProps) {
-		this.setState(nextProps);
-	}
-
 	render() {
-		const { elementClasses, iconName } = this.state;
+		const { elementClasses, iconName } = this.props;
 		const classes = getCN(
 			'lexicon-icon',
 			`lexicon-icon-${iconName}`,
 			elementClasses
 		);
 
+		const useTag = `<use xlink:href="${Liferay.ThemeDisplay.getPathThemeImages()}/lexicon/icons.svg#${iconName}" />`;
+
 		return (
-			<svg className={classes} focusable="false" role="presentation">
-				<use
-					href={`${Liferay.ThemeDisplay.getPathThemeImages()}/lexicon/icons.svg#${iconName}`}
-				/>
-			</svg>
+			<svg
+				className={classes}
+				dangerouslySetInnerHTML={{ __html: useTag }}
+				focusable="false"
+				role="presentation"
+			/>
 		);
 	}
 }
