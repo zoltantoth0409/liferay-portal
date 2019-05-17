5b4483797eb0ba76df4d789a254899b816586a68
Victor Galan <victor.galan@liferay.com>
2019-05-17 10:42:32 +0200

    LPS-95401 Add tooltip

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/toolbar/FragmentsEditorToolbar.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/toolbar/FragmentsEditorToolbar.es.js
@@ -6,12 +6,35 @@ import './SegmentsExperienceSelector.es';
 import getConnectedComponent from '../../store/ConnectedComponent.es';
 import templates from './FragmentsEditorToolbar.soy';
 import {TOGGLE_SIDEBAR} from '../../actions/actions.es';
+import {setIn} from '../../utils/FragmentsEditorUpdateUtils.es';
 
 /**
  * FragmentsEditorToolbar
  * @review
  */
 class FragmentsEditorToolbar extends Component {
+
+	prepareStateForRender(state) {
+		const {lastSaveDate, savingChanges} = state;
+
+		let lastSaveText = '';
+
+		if (savingChanges) {
+			lastSaveText = Liferay.Language.get('saving-changes');
+		}
+		else if (lastSaveDate) {
+			let placeholder = Liferay.Language.get('draft-saved-at-x');
+
+			lastSaveText = placeholder.replace('{0}', lastSaveDate);
+		}
+
+		const nextState = setIn(
+			state,
+			['_lastSaveText'],
+			lastSaveText);
+
+		return nextState;
+	}
 
 	/**
 	 * Handles discard draft form submit action.
