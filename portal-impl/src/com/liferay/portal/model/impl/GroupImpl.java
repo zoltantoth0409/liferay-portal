9d15b5d8ee713a8c0b65f9f830c17a071fd1d753
Tamas Molnar <tamas.molnar@liferay.com>
2019-05-20 17:45:28 +0200

    LPS-95262 Add null check

--- portal-impl/src/com/liferay/portal/model/impl/GroupImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/GroupImpl.java
@@ -1053,6 +1053,10 @@ public class GroupImpl extends GroupBaseImpl {
 				PortletDataHandler stagedPortletDataHandler =
 					stagedPortlet.getPortletDataHandlerInstance();
 
+				if (stagedPortletDataHandler == null) {
+					continue;
+				}
+
 				if (portletDataHandler.getServiceName().equals(
 						stagedPortletDataHandler.getServiceName())) {
 
