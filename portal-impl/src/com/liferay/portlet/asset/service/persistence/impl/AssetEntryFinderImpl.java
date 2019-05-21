b5280b624066d821b99a96897956ea5f67c0556a
Brian Chan <brian.chan@liferay.com>
2019-05-21 12:15:07 -0700

    LPS-95821 my bad

--- portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetEntryFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetEntryFinderImpl.java
@@ -399,7 +399,7 @@ public class AssetEntryFinderImpl
 				orderByCol1 = orderByColumn;
 			}
 
-			if (orderByColumn.equals(entryQuery.getOrderByCol1(2)) {
+			if (orderByColumn.equals(entryQuery.getOrderByCol1(2))) {
 				orderByCol2 = orderByColumn;
 			}
 		}
