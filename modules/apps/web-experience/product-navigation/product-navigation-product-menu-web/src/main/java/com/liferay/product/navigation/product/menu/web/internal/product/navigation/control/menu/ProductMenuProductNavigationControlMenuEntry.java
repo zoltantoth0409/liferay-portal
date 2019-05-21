50684c682a456baf7ecb8733e0cbde2dffc7f934
gregory-bretall <gregory.bretall@liferay.com>
2019-05-21 12:29:33 -0700

    LPS-95122 This is practically disabling BodyBottomTag's logic, then it is better to just not use it to avoid the unnecessary buffer creation and copying.

--- modules/apps/web-experience/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/product/navigation/control/menu/ProductMenuProductNavigationControlMenuEntry.java
+++ modules/apps/web-experience/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/product/navigation/control/menu/ProductMenuProductNavigationControlMenuEntry.java
@@ -24,9 +24,7 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ReflectionUtil;
-import com.liferay.portal.kernel.util.ServerDetector;
 import com.liferay.portal.kernel.util.SessionClicks;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -36,7 +34,7 @@ import com.liferay.product.navigation.control.menu.constants.ProductNavigationCo
 import com.liferay.product.navigation.product.menu.web.constants.ProductNavigationProductMenuPortletKeys;
 import com.liferay.product.navigation.product.menu.web.constants.ProductNavigationProductMenuWebKeys;
 import com.liferay.taglib.portletext.RuntimeTag;
-import com.liferay.taglib.util.BodyBottomTag;
+import com.liferay.taglib.servlet.PageContextFactoryUtil;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -52,7 +50,6 @@ import javax.portlet.WindowStateException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
 
@@ -88,39 +85,8 @@ public class ProductMenuProductNavigationControlMenuEntry
 			HttpServletRequest request, HttpServletResponse response)
 		throws IOException {
 
-		BodyBottomTag bodyBottomTag = new BodyBottomTag() {
-
-			@Override
-			public int doEndTag() throws JspException {
-				try {
-					JspWriter jspWriter = pageContext.getOut();
-
-					StringBundler sb = getBodyContentAsStringBundler();
-
-					jspWriter.write(sb.toString());
-
-					return EVAL_PAGE;
-				}
-				catch (Exception e) {
-					throw new JspException(e);
-				}
-				finally {
-					if (!ServerDetector.isResin()) {
-						cleanUp();
-					}
-				}
-			}
-
-		};
-		bodyBottomTag.setOutputKey("productMenu");
-
-		try {
-			bodyBottomTag.doBodyTag(
-				request, response, this::_processBodyBottomContent);
-		}
-		catch (JspException je) {
-			throw new IOException(je);
-		}
+		_processBodyBottomContent(
+			PageContextFactoryUtil.create(request, response));
 
 		return true;
 	}
