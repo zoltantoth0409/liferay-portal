1cb111b2c83d209c1f4ecf8f76e2105607a68ccf
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2019-05-22 14:06:39 +0200

    LPS-95429 Hide Browse button for images

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/itemselector/plugin.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/itemselector/plugin.js
@@ -162,8 +162,13 @@
 							instance._bindBrowseButton(event.editor, dialogDefinition, 'info', 'audioselector', 'url');
 						}
 						else if (dialogName === 'image') {
-							instance._bindBrowseButton(event.editor, dialogDefinition, 'info', 'imageselector', 'txtUrl');
 							instance._bindBrowseButton(event.editor, dialogDefinition, 'Link', 'linkselector', 'txtUrl');
+
+							dialogDefinition.getContents('info').remove('browse');
+
+							dialogDefinition.onLoad = function() {
+								this.getContentElement('info', 'txtUrl').getInputElement().setAttribute('readOnly', true);
+							};
 						}
 						else if (dialogName === 'video') {
 							instance._bindBrowseButton(event.editor, dialogDefinition, 'info', 'videoselector', 'poster');
