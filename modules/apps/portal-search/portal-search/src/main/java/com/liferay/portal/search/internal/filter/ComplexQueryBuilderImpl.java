ed658e50b497008c0837b5004ad85f237b8ff4bc
André de Oliveira <andre.oliveira@liferay.com>
2019-05-22 14:00:00 -0700

    LPS-95912 portal-search: Complex Query Parts with a blank value wrongly lead to an impossible condition and zero results
    https://issues.liferay.com/browse/LPS-95912

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/filter/ComplexQueryBuilderImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/filter/ComplexQueryBuilderImpl.java
@@ -65,7 +65,7 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 				Collectors.toMap(ComplexQueryPart::getName, Function.identity())
 			);
 
-		Build build = new Build(complexQueryPartsMap);
+		Build build = new Build(complexQueryPartsMap, _getRootBooleanQuery());
 
 		return build.build();
 	}
@@ -77,6 +77,14 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 		return this;
 	}
 
+	private BooleanQuery _getRootBooleanQuery() {
+		if (_booleanQuery != null) {
+			return _booleanQuery;
+		}
+
+		return _queries.booleanQuery();
+	}
+
 	private BooleanQuery _booleanQuery;
 	private final List<ComplexQueryPart> _complexQueryParts = new ArrayList<>();
 	private final Queries _queries;
@@ -84,8 +92,12 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 
 	private class Build {
 
-		public Build(Map<String, ComplexQueryPart> complexQueryPartsMap) {
+		public Build(
+			Map<String, ComplexQueryPart> complexQueryPartsMap,
+			BooleanQuery rootBooleanQuery) {
+
 			_complexQueryPartsMap = complexQueryPartsMap;
+			_rootBooleanQuery = rootBooleanQuery;
 		}
 
 		public Query build() {
@@ -156,6 +168,10 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 			}
 
 			if (Objects.equals(type, "fuzzy")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				return _queries.fuzzy(field, value);
 			}
 
@@ -168,10 +184,18 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 			}
 
 			if (Objects.equals(type, "match_phrase")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				return _queries.matchPhrase(field, value);
 			}
 
 			if (Objects.equals(type, "match_phrase_prefix")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				return _queries.matchPhrasePrefix(field, value);
 			}
 
@@ -185,10 +209,18 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 			}
 
 			if (Objects.equals(type, "prefix")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				return _queries.prefix(field, value);
 			}
 
 			if (Objects.equals(type, "query_string")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				StringQuery stringQuery = _queries.string(value);
 
 				if (!Validator.isBlank(field)) {
@@ -199,14 +231,26 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 			}
 
 			if (Objects.equals(type, "regexp")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				return _queries.regex(field, value);
 			}
 
 			if (Objects.equals(type, "script")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				return _queries.script(_scripts.script(value));
 			}
 
 			if (Objects.equals(type, "simple_query_string")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				SimpleStringQuery simpleStringQuery = _queries.simpleString(
 					value);
 
@@ -218,10 +262,18 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 			}
 
 			if (Objects.equals(type, "term")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				return _queries.term(field, value);
 			}
 
 			if (Objects.equals(type, "wildcard")) {
+				if (Validator.isBlank(value)) {
+					return null;
+				}
+
 				return _queries.wildcard(field, value);
 			}
 
@@ -250,7 +302,7 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 		}
 
 		protected BooleanQuery getRootBooleanQuery() {
-			return _booleanQuery;
+			return _rootBooleanQuery;
 		}
 
 		protected Query hydrate(ComplexQueryPart complexQueryPart) {
@@ -276,6 +328,7 @@ public class ComplexQueryBuilderImpl implements ComplexQueryBuilder {
 
 		private final Map<String, ComplexQueryPart> _complexQueryPartsMap;
 		private final Map<String, Query> _queriesMap = new HashMap<>();
+		private final BooleanQuery _rootBooleanQuery;
 
 	}
 
