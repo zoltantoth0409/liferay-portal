997b41b0479b93b757508952547da0f2ffe8f703
Samuel Tran <samuel.tran@liferay.com>
2019-05-22 16:27:03 -0700

    LPS-95910 portal-workflow-task-web - Include JS when embedded

---
+++ modules/apps/portal-workflow/portal-workflow-task-web/src/main/resources/META-INF/resources/js/config.js
@@ -0,0 +1,22 @@
+;(function() {
+	AUI().applyConfig(
+		{
+			groups: {
+				workflowtasks: {
+					base: MODULE_PATH + '/js/',
+					combine: Liferay.AUI.getCombine(),
+					filter: Liferay.AUI.getFilterConfig(),
+					modules: {
+						'liferay-workflow-tasks': {
+							path: 'main.js',
+							requires: [
+								'liferay-util-window'
+							]
+						}
+					},
+					root: MODULE_PATH + '/js/'
+				}
+			}
+		}
+	);
+})();
