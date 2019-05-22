addef338aff9042f478aff2146678020040feb8f
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-05-22 10:57:19 +0200

    LPS-95699 Validate name

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/impl/SegmentsEntryLocalServiceImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/service/impl/SegmentsEntryLocalServiceImpl.java
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.GroupThreadLocal;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -42,6 +43,7 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.segments.constants.SegmentsConstants;
 import com.liferay.segments.exception.RequiredSegmentsEntryException;
 import com.liferay.segments.exception.SegmentsEntryKeyException;
+import com.liferay.segments.exception.SegmentsEntryNameException;
 import com.liferay.segments.model.SegmentsEntry;
 import com.liferay.segments.service.base.SegmentsEntryLocalServiceBaseImpl;
 
@@ -82,6 +84,7 @@ public class SegmentsEntryLocalServiceImpl
 		}
 
 		validateKey(0, groupId, segmentsEntryKey);
+		validateName(nameMap);
 
 		long segmentsEntryId = counterLocalService.increment();
 
@@ -317,6 +320,8 @@ public class SegmentsEntryLocalServiceImpl
 		validateKey(
 			segmentsEntryId, segmentsEntry.getGroupId(), segmentsEntryKey);
 
+		validateName(nameMap);
+
 		segmentsEntry.setSegmentsEntryKey(segmentsEntryKey);
 
 		segmentsEntry.setNameMap(nameMap);
@@ -418,6 +423,16 @@ public class SegmentsEntryLocalServiceImpl
 			(segmentsEntry.getSegmentsEntryId() != segmentsEntryId)) {
 
 			throw new SegmentsEntryKeyException();
+		}
+	}
+
+	protected void validateName(Map<Locale, String> nameMap)
+		throws PortalException {
+
+		Locale locale = LocaleUtil.getDefault();
+
+		if (nameMap.isEmpty() || Validator.isNull(nameMap.get(locale))) {
+			throw new SegmentsEntryNameException();
 		}
 	}
 
