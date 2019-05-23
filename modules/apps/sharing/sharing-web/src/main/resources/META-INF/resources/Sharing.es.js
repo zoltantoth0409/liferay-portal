d67ee7e889dd36015fd6b0bf5794e5cb2a02846b
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2019-05-23 13:47:46 +0200

    LPS-96030 No need to override the datachange function

--- modules/apps/sharing/sharing-web/src/main/resources/META-INF/resources/Sharing.es.js
+++ modules/apps/sharing/sharing-web/src/main/resources/META-INF/resources/Sharing.es.js
@@ -46,30 +46,6 @@ class Sharing extends PortletBase {
 	}
 
 	/**
-	 * Disables filtering of results
-	 * @private
-	 * @review
-	 */
-	_handleDataChange(e) {
-		e.preventDefault();
-
-		if (e.data && e.target.refs.autocomplete._query) {
-			e.target.filteredItems = e.data.map(
-				(element, index) => ({
-					data: element,
-					index,
-					matches: [],
-					score: 0,
-					value: element
-				})
-			);
-		}
-		else {
-			e.target.filteredItems = [];
-		}
-	}
-
-	/**
 	 * Close the SharingDialog
 	 * @private
 	 * @review
