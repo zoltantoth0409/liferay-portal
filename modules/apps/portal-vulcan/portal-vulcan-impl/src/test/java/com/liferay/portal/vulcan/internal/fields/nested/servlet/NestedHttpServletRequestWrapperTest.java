1d8ace8699b63d058db0f00892e6fa7e52d63614
Ivica Cardic <ivica.cardic@liferay.com>
2019-05-23 15:14:17 +0200

    LPS-96040 - portal-vulcan-impl - Add tests

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/test/java/com/liferay/portal/vulcan/internal/fields/nested/servlet/NestedHttpServletRequestWrapperTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.fields.nested.servlet;
+
+import com.liferay.portal.vulcan.internal.util.request.MockHttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Ivica Cardic
+ */
+public class NestedHttpServletRequestWrapperTest {
+
+	@Test
+	public void testGetParameter() {
+		NestedFieldsHttpServletRequestWrapper
+			nestedFieldsHttpServletRequestWrapper =
+				new NestedFieldsHttpServletRequestWrapper(
+					"skus",
+					new MockHttpServletRequest(
+						"skus", "externalReferenceCode", "12345", "width",
+						"11"));
+
+		Assert.assertEquals(
+			"12345",
+			nestedFieldsHttpServletRequestWrapper.getParameter(
+				"externalReferenceCode"));
+		Assert.assertEquals(
+			"11", nestedFieldsHttpServletRequestWrapper.getParameter("width"));
+	}
+
+}
---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/test/java/com/liferay/portal/vulcan/internal/jaxrs/container/request/filter/NestedFieldsContainerRequestFilterTest.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.jaxrs.container.request.filter;
+
+import com.liferay.portal.vulcan.internal.fields.nested.NestedFieldsContext;
+import com.liferay.portal.vulcan.internal.fields.nested.NestedFieldsContextThreadLocal;
+
+import java.io.IOException;
+
+import java.util.List;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+/**
+ * @author Ivica Cardic
+ */
+public class NestedFieldsContainerRequestFilterTest {
+
+	@Test
+	public void testFilter() throws IOException {
+		NestedFieldsContainerRequestFilter nestedFieldsContainerRequestFilter =
+			new NestedFieldsContainerRequestFilter();
+
+		ContainerRequestContext context = Mockito.mock(
+			ContainerRequestContext.class);
+
+		UriInfo uriInfo = Mockito.mock(UriInfo.class);
+
+		Mockito.when(
+			context.getUriInfo()
+		).thenReturn(
+			uriInfo
+		);
+
+		MultivaluedMap<String, String> queryParameters =
+			new MultivaluedHashMap<>();
+
+		Mockito.when(
+			uriInfo.getQueryParameters()
+		).thenReturn(
+			queryParameters
+		);
+
+		nestedFieldsContainerRequestFilter.filter(context);
+
+		NestedFieldsContext nestedFieldsContext =
+			NestedFieldsContextThreadLocal.getNestedFieldsContext();
+
+		Assert.assertNull(nestedFieldsContext);
+
+		queryParameters.putSingle("nestedFields", "skus,productOptions");
+
+		nestedFieldsContainerRequestFilter.filter(context);
+
+		nestedFieldsContext =
+			NestedFieldsContextThreadLocal.getNestedFieldsContext();
+
+		List<String> fieldNames = nestedFieldsContext.getFieldNames();
+
+		Assert.assertEquals(fieldNames.toString(), 2, fieldNames.size());
+	}
+
+}
