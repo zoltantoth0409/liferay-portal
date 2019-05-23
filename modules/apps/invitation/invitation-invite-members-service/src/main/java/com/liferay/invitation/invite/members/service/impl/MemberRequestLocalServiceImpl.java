2f06d98acf44ae04664e33a1f9dae7a77a820455
Lance Ji <zheyi.ji@liferay.com>
2019-05-23 16:58:24 -0700

    LPS-96074 Manual changes

--- modules/apps/invitation/invitation-invite-members-service/src/main/java/com/liferay/invitation/invite/members/service/impl/MemberRequestLocalServiceImpl.java
+++ modules/apps/invitation/invitation-invite-members-service/src/main/java/com/liferay/invitation/invite/members/service/impl/MemberRequestLocalServiceImpl.java
@@ -22,7 +22,7 @@ import com.liferay.invitation.invite.members.service.base.MemberRequestLocalServ
 import com.liferay.mail.kernel.model.MailMessage;
 import com.liferay.mail.kernel.service.MailService;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.NoSuchUserException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -42,6 +42,7 @@ import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserNotificationEventLocalService;
 import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalService;
+import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.HttpUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PrefsPropsUtil;
@@ -56,10 +57,17 @@ import java.util.List;
 
 import javax.mail.internet.InternetAddress;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Ryan Park
  * @author Jonathan Lee
  */
+@Component(
+	property = "model.class.name=com.liferay.invitation.invite.members.model.MemberRequest",
+	service = AopService.class
+)
 public class MemberRequestLocalServiceImpl
 	extends MemberRequestLocalServiceBaseImpl {
 
@@ -296,7 +304,7 @@ public class MemberRequestLocalServiceImpl
 			createAccountURL = serviceContext.getPortalURL();
 		}
 
-		if (!workflowDefinitionLinkLocalService.hasWorkflowDefinitionLink(
+		if (!_workflowDefinitionLinkLocalService.hasWorkflowDefinitionLink(
 				memberRequest.getCompanyId(),
 				WorkflowConstants.DEFAULT_GROUP_ID, User.class.getName(), 0)) {
 
@@ -330,7 +338,7 @@ public class MemberRequestLocalServiceImpl
 		redirectURL = addParameterWithPortletNamespace(
 			redirectURL, "key", memberRequest.getKey());
 
-		return HttpUtil.addParameter(loginURL, "redirect", redirectURL);
+		return _http.addParameter(loginURL, "redirect", redirectURL);
 	}
 
 	protected String getRedirectURL(ServiceContext serviceContext) {
@@ -424,7 +432,7 @@ public class MemberRequestLocalServiceImpl
 		MailMessage mailMessage = new MailMessage(
 			from, to, subject, body, true);
 
-		mailService.sendEmail(mailMessage);
+		_mailService.sendEmail(mailMessage);
 	}
 
 	protected void sendNotificationEvent(MemberRequest memberRequest)
@@ -453,7 +461,7 @@ public class MemberRequestLocalServiceImpl
 			notificationEvent.setDeliveryType(
 				UserNotificationDeliveryConstants.TYPE_WEBSITE);
 
-			userNotificationEventLocalService.addUserNotificationEvent(
+			_userNotificationEventLocalService.addUserNotificationEvent(
 				memberRequest.getReceiverUserId(), true, notificationEvent);
 		}
 	}
@@ -471,18 +479,21 @@ public class MemberRequestLocalServiceImpl
 		}
 	}
 
-	@BeanReference(type = MailService.class)
-	protected MailService mailService;
-
-	@BeanReference(type = UserNotificationEventLocalService.class)
-	protected UserNotificationEventLocalService
-		userNotificationEventLocalService;
-
-	@BeanReference(type = WorkflowDefinitionLinkLocalService.class)
-	protected WorkflowDefinitionLinkLocalService
-		workflowDefinitionLinkLocalService;
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		MemberRequestLocalServiceImpl.class);
+
+	@Reference
+	private Http _http;
+
+	@Reference
+	private MailService _mailService;
+
+	@Reference
+	private UserNotificationEventLocalService
+		_userNotificationEventLocalService;
+
+	@Reference
+	private WorkflowDefinitionLinkLocalService
+		_workflowDefinitionLinkLocalService;
 
 }
