eb3e736344898296b66c44c65e31faf45bf8bcc4
Hugo Huijser <hugo.huijser@liferay.com>
2019-05-23 14:31:20 +0800

    LPS-84119 Pass the simple name

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/util/CheckstyleUtil.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/util/CheckstyleUtil.java
@@ -105,7 +105,7 @@ public class CheckstyleUtil {
 				continue;
 			}
 
-			checkNames.add(checkName);
+			checkNames.add(checkSimpleName);
 
 			DefaultConfiguration defaultConfiguration =
 				new DefaultConfiguration(checkName);
