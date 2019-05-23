947c0bfbdfabd2435274bc60f045ced2e4f5dc38
Ivica Cardic <ivica.cardic@liferay.com>
2019-05-23 16:37:59 +0200

    LPS-96040 - portal-vulcan-impl - Add logic for nested fields support

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/fields/nested/servlet/NestedFieldsHttpServletRequestWrapper.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.fields.nested.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * @author Ivica Cardic
+ */
+public class NestedFieldsHttpServletRequestWrapper
+	extends HttpServletRequestWrapper {
+
+	public NestedFieldsHttpServletRequestWrapper(
+		String fieldName, HttpServletRequest httpServletRequest) {
+
+		super(httpServletRequest);
+
+		_fieldName = fieldName;
+	}
+
+	@Override
+	public String getParameter(String name) {
+		return super.getParameter(getNestedFieldsParameterName(name));
+	}
+
+	protected String getNestedFieldsParameterName(String name) {
+		return _fieldName + "." + name;
+	}
+
+	private final String _fieldName;
+
+}
---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/container/request/filter/NestedFieldsContainerRequestFilter.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.jaxrs.container.request.filter;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.vulcan.internal.fields.nested.NestedFieldsContext;
+import com.liferay.portal.vulcan.internal.fields.nested.NestedFieldsContextThreadLocal;
+
+import java.io.IOException;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Provider;
+
+import org.apache.cxf.jaxrs.utils.JAXRSUtils;
+
+/**
+ * @author Ivica Cardic
+ */
+@Provider
+public class NestedFieldsContainerRequestFilter
+	implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext containerRequestContext)
+		throws IOException {
+
+		UriInfo uriInfo = containerRequestContext.getUriInfo();
+
+		MultivaluedMap<String, String> queryParameters =
+			uriInfo.getQueryParameters();
+
+		String nestedFields = queryParameters.getFirst("nestedFields");
+
+		if (Validator.isNotNull(nestedFields)) {
+			List<String> fieldNames = Arrays.asList(
+				nestedFields.split("\\s*,\\s*"));
+
+			MultivaluedMap<String, String> pathParameters =
+				uriInfo.getPathParameters();
+
+			NestedFieldsContextThreadLocal.setNestedFieldsContext(
+				new NestedFieldsContext(
+					fieldNames, JAXRSUtils.getCurrentMessage(), pathParameters,
+					queryParameters));
+		}
+	}
+
+}
