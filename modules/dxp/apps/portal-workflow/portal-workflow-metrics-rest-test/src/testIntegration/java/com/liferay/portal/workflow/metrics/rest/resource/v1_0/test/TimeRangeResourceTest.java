bcac21ca744fe8dfee83aed9734c5394a2fc59a5
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-05-24 10:07:04 -0300

    LPS-95809 Add tests to TimeRange Resource

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/TimeRangeResourceTest.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/TimeRangeResourceTest.java
@@ -15,7 +15,15 @@
 package com.liferay.portal.workflow.metrics.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.workflow.metrics.rest.client.dto.v1_0.TimeRange;
+import com.liferay.portal.workflow.metrics.rest.client.pagination.Page;
+import com.liferay.portal.workflow.metrics.rest.client.resource.v1_0.TimeRangeResource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -23,4 +31,76 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class TimeRangeResourceTest extends BaseTimeRangeResourceTestCase {
+
+	@Test
+	public void testGetTimeRangesPage() throws Exception {
+		Page<TimeRange> timeRangesPage = TimeRangeResource.getTimeRangesPage();
+
+		List<TimeRange> timeRanges1 =
+			(List<TimeRange>)timeRangesPage.getItems();
+
+		Assert.assertEquals(timeRanges1.toString(), 7, timeRanges1.size());
+
+		List<TimeRange> timeRanges2 = new ArrayList<TimeRange>() {
+			{
+				add(
+					new TimeRange() {
+						{
+							defaultTimeRange = false;
+							id = 0;
+						}
+					});
+				add(
+					new TimeRange() {
+						{
+							defaultTimeRange = false;
+							id = 1;
+						}
+					});
+				add(
+					new TimeRange() {
+						{
+							defaultTimeRange = false;
+							id = 7;
+						}
+					});
+				add(
+					new TimeRange() {
+						{
+							defaultTimeRange = true;
+							id = 30;
+						}
+					});
+				add(
+					new TimeRange() {
+						{
+							defaultTimeRange = false;
+							id = 90;
+						}
+					});
+				add(
+					new TimeRange() {
+						{
+							defaultTimeRange = false;
+							id = 180;
+						}
+					});
+				add(
+					new TimeRange() {
+						{
+							defaultTimeRange = false;
+							id = 365;
+						}
+					});
+			}
+		};
+
+		assertEquals(timeRanges1, timeRanges2);
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"defaultTimeRange", "id"};
+	}
+
 }
