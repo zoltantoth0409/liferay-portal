981b6ce1bc5e48f9c8059f12684ea591939e1084
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-05-24 15:52:22 +0200

    LPS-94854 This is an engine function not a regular API function

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/engine/CTEngineManagerImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/engine/CTEngineManagerImpl.java
@@ -297,6 +297,15 @@ public class CTEngineManagerImpl implements CTEngineManager {
 	}
 
 	@Override
+	public List<CTEntry> getCTEntries(
+		CTCollection ctCollection, String keywords,
+		QueryDefinition<CTEntry> queryDefinition) {
+
+		return _ctEntryLocalService.search(
+			ctCollection, keywords, queryDefinition);
+	}
+
+	@Override
 	public List<CTEntry> getCTEntries(long ctCollectionId) {
 		return _ctEntryLocalService.getCTCollectionCTEntries(ctCollectionId);
 	}
@@ -325,6 +334,15 @@ public class CTEngineManagerImpl implements CTEngineManager {
 		return (int)_ctEntryLocalService.searchCount(
 			ctCollection, groupIds, userIds, classNameIds, changeTypes,
 			collision, queryDefinition);
+	}
+
+	@Override
+	public int getCTEntriesCount(
+		CTCollection ctCollection, String keywords,
+		QueryDefinition<CTEntry> queryDefinition) {
+
+		return _ctEntryLocalService.searchCount(
+			ctCollection, keywords, queryDefinition);
 	}
 
 	@Override
