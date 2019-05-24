214d223b9cce7ce579ff32b603f87507c01e10df
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-05-24 07:59:03 +0200

    LPS-96083 Auto

--- modules/apps/asset/asset-display-page-api/src/main/java/com/liferay/asset/display/page/model/AssetDisplayPageEntrySoap.java
+++ modules/apps/asset/asset-display-page-api/src/main/java/com/liferay/asset/display/page/model/AssetDisplayPageEntrySoap.java
@@ -23,7 +23,7 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * This class is used by SOAP remote services.
+ * This class is used by SOAP remote services, specifically {@link com.liferay.asset.display.page.service.http.AssetDisplayPageEntryServiceSoap}.
  *
  * @author Brian Wing Shun Chan
  * @generated
--- modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/model/impl/AssetDisplayPageEntryModelImpl.java
+++ modules/apps/asset/asset-display-page-service/src/main/java/com/liferay/asset/display/page/model/impl/AssetDisplayPageEntryModelImpl.java
@@ -18,11 +18,13 @@ import aQute.bnd.annotation.ProviderType;
 
 import com.liferay.asset.display.page.model.AssetDisplayPageEntry;
 import com.liferay.asset.display.page.model.AssetDisplayPageEntryModel;
+import com.liferay.asset.display.page.model.AssetDisplayPageEntrySoap;
 import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.expando.kernel.util.ExpandoBridgeFactoryUtil;
 import com.liferay.exportimport.kernel.lar.StagedModelType;
 import com.liferay.portal.kernel.bean.AutoEscapeBeanHandler;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSON;
 import com.liferay.portal.kernel.model.CacheModel;
 import com.liferay.portal.kernel.model.ModelWrapper;
 import com.liferay.portal.kernel.model.User;
@@ -42,10 +44,12 @@ import java.lang.reflect.InvocationHandler;
 
 import java.sql.Types;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
@@ -61,6 +65,7 @@ import java.util.function.Function;
  * @see AssetDisplayPageEntryImpl
  * @generated
  */
+@JSON(strict = true)
 @ProviderType
 public class AssetDisplayPageEntryModelImpl
 	extends BaseModelImpl<AssetDisplayPageEntry>
@@ -150,6 +155,63 @@ public class AssetDisplayPageEntryModelImpl
 	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(
 		com.liferay.asset.display.page.service.util.ServiceProps.get(
 			"lock.expiration.time.com.liferay.asset.display.page.model.AssetDisplayPageEntry"));
+
+	/**
+	 * Converts the soap model instance into a normal model instance.
+	 *
+	 * @param soapModel the soap model instance to convert
+	 * @return the normal model instance
+	 */
+	public static AssetDisplayPageEntry toModel(
+		AssetDisplayPageEntrySoap soapModel) {
+
+		if (soapModel == null) {
+			return null;
+		}
+
+		AssetDisplayPageEntry model = new AssetDisplayPageEntryImpl();
+
+		model.setUuid(soapModel.getUuid());
+		model.setAssetDisplayPageEntryId(
+			soapModel.getAssetDisplayPageEntryId());
+		model.setGroupId(soapModel.getGroupId());
+		model.setCompanyId(soapModel.getCompanyId());
+		model.setUserId(soapModel.getUserId());
+		model.setUserName(soapModel.getUserName());
+		model.setCreateDate(soapModel.getCreateDate());
+		model.setModifiedDate(soapModel.getModifiedDate());
+		model.setClassNameId(soapModel.getClassNameId());
+		model.setClassPK(soapModel.getClassPK());
+		model.setLayoutPageTemplateEntryId(
+			soapModel.getLayoutPageTemplateEntryId());
+		model.setType(soapModel.getType());
+		model.setPlid(soapModel.getPlid());
+
+		return model;
+	}
+
+	/**
+	 * Converts the soap model instances into normal model instances.
+	 *
+	 * @param soapModels the soap model instances to convert
+	 * @return the normal model instances
+	 */
+	public static List<AssetDisplayPageEntry> toModels(
+		AssetDisplayPageEntrySoap[] soapModels) {
+
+		if (soapModels == null) {
+			return null;
+		}
+
+		List<AssetDisplayPageEntry> models =
+			new ArrayList<AssetDisplayPageEntry>(soapModels.length);
+
+		for (AssetDisplayPageEntrySoap soapModel : soapModels) {
+			models.add(toModel(soapModel));
+		}
+
+		return models;
+	}
 
 	public AssetDisplayPageEntryModelImpl() {
 	}
@@ -588,6 +650,7 @@ public class AssetDisplayPageEntryModelImpl
 			(Map)attributeSetterBiConsumers);
 	}
 
+	@JSON
 	@Override
 	public String getUuid() {
 		if (_uuid == null) {
@@ -613,6 +676,7 @@ public class AssetDisplayPageEntryModelImpl
 		return GetterUtil.getString(_originalUuid);
 	}
 
+	@JSON
 	@Override
 	public long getAssetDisplayPageEntryId() {
 		return _assetDisplayPageEntryId;
@@ -623,6 +687,7 @@ public class AssetDisplayPageEntryModelImpl
 		_assetDisplayPageEntryId = assetDisplayPageEntryId;
 	}
 
+	@JSON
 	@Override
 	public long getGroupId() {
 		return _groupId;
@@ -645,6 +710,7 @@ public class AssetDisplayPageEntryModelImpl
 		return _originalGroupId;
 	}
 
+	@JSON
 	@Override
 	public long getCompanyId() {
 		return _companyId;
@@ -667,6 +733,7 @@ public class AssetDisplayPageEntryModelImpl
 		return _originalCompanyId;
 	}
 
+	@JSON
 	@Override
 	public long getUserId() {
 		return _userId;
@@ -693,6 +760,7 @@ public class AssetDisplayPageEntryModelImpl
 	public void setUserUuid(String userUuid) {
 	}
 
+	@JSON
 	@Override
 	public String getUserName() {
 		if (_userName == null) {
@@ -708,6 +776,7 @@ public class AssetDisplayPageEntryModelImpl
 		_userName = userName;
 	}
 
+	@JSON
 	@Override
 	public Date getCreateDate() {
 		return _createDate;
@@ -718,6 +787,7 @@ public class AssetDisplayPageEntryModelImpl
 		_createDate = createDate;
 	}
 
+	@JSON
 	@Override
 	public Date getModifiedDate() {
 		return _modifiedDate;
@@ -754,6 +824,7 @@ public class AssetDisplayPageEntryModelImpl
 		setClassNameId(classNameId);
 	}
 
+	@JSON
 	@Override
 	public long getClassNameId() {
 		return _classNameId;
@@ -776,6 +847,7 @@ public class AssetDisplayPageEntryModelImpl
 		return _originalClassNameId;
 	}
 
+	@JSON
 	@Override
 	public long getClassPK() {
 		return _classPK;
@@ -798,6 +870,7 @@ public class AssetDisplayPageEntryModelImpl
 		return _originalClassPK;
 	}
 
+	@JSON
 	@Override
 	public long getLayoutPageTemplateEntryId() {
 		return _layoutPageTemplateEntryId;
@@ -820,6 +893,7 @@ public class AssetDisplayPageEntryModelImpl
 		return _originalLayoutPageTemplateEntryId;
 	}
 
+	@JSON
 	@Override
 	public int getType() {
 		return _type;
@@ -830,6 +904,7 @@ public class AssetDisplayPageEntryModelImpl
 		_type = type;
 	}
 
+	@JSON
 	@Override
 	public StagedModelType getStagedModelType() {
 		return new StagedModelType(
