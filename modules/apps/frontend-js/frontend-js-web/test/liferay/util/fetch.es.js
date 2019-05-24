509921d1eeb92a21b8ba0d85a42b07156f647381
Marko Čikoš <marko.cikos@liferay.com>
2019-05-24 15:24:09 +0200

    LPS-94712 frontend-js-web - Test fetch utility

---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/fetch.es.js
@@ -0,0 +1,78 @@
+'use strict';
+
+import fetch from '../../../src/main/resources/META-INF/resources/liferay/util/fetch.es';
+
+describe(
+	'Liferay.Util.fetch',
+	() => {
+		const sampleUrl = 'http://sampleurl.com';
+
+		it(
+			'should apply default settings if none are given',
+			() => {
+				global.fetch = jest.fn(
+					(resource, init) => {
+						expect(init).toEqual(
+							{
+								credentials: 'include'
+							}
+						);
+					}
+				);
+
+				fetch(sampleUrl);
+
+				global.fetch.mockRestore();
+			}
+		);
+
+		it(
+			'should override a default setting with given setting',
+			() => {
+				global.fetch = jest.fn(
+					(resource, init) => {
+						expect(init).toEqual(
+							{
+								credentials: 'omit'
+							}
+						);
+					}
+				);
+
+				fetch(
+					sampleUrl,
+					{
+						credentials: 'omit'
+					}
+				);
+
+				global.fetch.mockRestore();
+			}
+		);
+
+		it(
+			'should merge default settings with given different settings',
+			() => {
+				global.fetch = jest.fn(
+					(resource, init) => {
+						expect(init).toEqual(
+							{
+								credentials: 'include',
+								method: 'get'
+							}
+						);
+					}
+				);
+
+				fetch(
+					sampleUrl,
+					{
+						method: 'get'
+					}
+				);
+
+				global.fetch.mockRestore();
+			}
+		);
+	}
+);
