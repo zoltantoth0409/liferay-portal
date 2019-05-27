6f2be3719a5476410da5f6e6cfb5f6dce8eca070
Javier Gamarra <nhpatt@gmail.com>
2019-05-27 19:04:29 +0200

    LPS-94274 generate

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-client/src/main/java/com/liferay/headless/admin/workflow/client/serdes/v1_0/WorkflowTaskAssignToUserSerDes.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-client/src/main/java/com/liferay/headless/admin/workflow/client/serdes/v1_0/WorkflowTaskAssignToUserSerDes.java
@@ -156,7 +156,9 @@ public class WorkflowTaskAssignToUserSerDes {
 	private static String _escape(Object object) {
 		String string = String.valueOf(object);
 
-		return string.replaceAll("\"", "\\\\\"");
+		string = string.replaceAll("\"", "\\\\\"");
+
+		return string.replace("\\", "\\\\");
 	}
 
 	private static String _toJSON(Map<String, ?> map) {
