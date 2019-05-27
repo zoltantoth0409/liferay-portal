32065e869522ca833820cff3ff445639be291e3b
Alan Huang <alan.huang@liferay.com>
2019-05-27 10:49:10 +0800

    LPS-96091 Apply to HTML

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/HTMLWhitespaceCheck.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
+
+import java.io.IOException;
+
+/**
+ * @author Alan Huang
+ */
+public class HTMLWhitespaceCheck extends WhitespaceCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws IOException {
+
+		StringBundler sb = new StringBundler();
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				line = formatSelfClosingTags(line);
+
+				sb.append(line);
+
+				sb.append("\n");
+			}
+		}
+
+		return super.doProcess(fileName, absolutePath, sb.toString());
+	}
+
+}
