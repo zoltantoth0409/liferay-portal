32b3b3bf0c6ef28ecb4680cc5da2278d3984db15
Leon Chi <leon.chi@liferay.com>
2019-05-27 11:26:37 +0800

    LPS-95716 Move CleanUpPermissionsUtil and CleanUpPortletPreferencesUtil to module

--- modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/CleanUpPermissionsUtil.java
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/CleanUpPermissionsUtil.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portlet.admin.util;
+package com.liferay.server.admin.web.internal.portlet.action;
 
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
