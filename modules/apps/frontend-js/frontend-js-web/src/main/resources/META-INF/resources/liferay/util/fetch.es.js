feaf62bd5732e977bb5ef83cdf13872e14a37589
Marko Čikoš <marko.cikos@liferay.com>
2019-05-27 11:50:52 +0200

    LPS-94712 frontend-js-web - Implement fetch utility with default configuration

---
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/util/fetch.es.js
@@ -0,0 +1,22 @@
+const DEFAULT_INIT = {
+	credentials: 'include'
+};
+
+/**
+ * Fetches a resource. A thin wrapper around ES6 Fetch API, with standardized
+ * default configuration.
+ * @param {!string|!Request} resource The URL to the resource, or a Resource
+ * object.
+ * @param {Object=} init An optional object containing custom configuration.
+ * @return {Promise} A Promise that resolves to a Response object.
+ */
+
+export default function defaultFetch(resource, init = {}) {
+	return fetch(
+		resource,
+		{
+			...DEFAULT_INIT,
+			...init
+		}
+	);
+}
