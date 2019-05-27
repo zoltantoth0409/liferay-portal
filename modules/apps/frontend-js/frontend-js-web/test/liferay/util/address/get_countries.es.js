ebad1885caaa152fa0faaac84970b05c99cec272
Krešimir Čoko <kresimir.coko@liferay.com>
2019-05-27 13:04:24 +0200

    LPS-94694 frontend-js-web - Add tests for the Address utility

---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/address/get_countries.es.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import getCountries from '../../../../src/main/resources/META-INF/resources/liferay/util/address/get_countries.es';
+
+describe(
+	'Liferay.Address.getCountries',
+	() => {
+		it(
+			'should throw an error if the callback parameter is not a function',
+			() => {
+				expect(() => getCountries('')).toThrow('must be a function');
+			}
+		);
+	}
+);
