d9b19df6a9aa87848f1d0170133792491c5da5b4
Javier Gamarra <nhpatt@gmail.com>
2019-05-27 19:04:29 +0200

    LPS-94274 generate

--- modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/serdes/v1_0/KnowledgeBaseArticleSerDes.java
+++ modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/serdes/v1_0/KnowledgeBaseArticleSerDes.java
@@ -582,7 +582,9 @@ public class KnowledgeBaseArticleSerDes {
 	private static String _escape(Object object) {
 		String string = String.valueOf(object);
 
-		return string.replaceAll("\"", "\\\\\"");
+		string = string.replaceAll("\"", "\\\\\"");
+
+		return string.replace("\\", "\\\\");
 	}
 
 	private static String _toJSON(Map<String, ?> map) {
