36ee74cb7d206951b7136be34c01b1d310f2a993
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-05-27 17:08:21 +0200

    LPS-95667 Add SegmentsEntryProviderRegistry

---
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/provider/SegmentsEntryProviderRegistry.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.provider;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.segments.context.Context;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * Provides methods for retrieving segments entry providers defined by
+ * {@link SegmentsEntryProvider} implementations.
+ *
+ * @author Eduardo García
+ */
+@ProviderType
+public interface SegmentsEntryProviderRegistry {
+
+	public long[] getSegmentsEntryClassPKs(
+			long segmentsEntryId, int start, int end)
+		throws PortalException;
+
+	public int getSegmentsEntryClassPKsCount(long segmentsEntryId)
+		throws PortalException;
+
+	public long[] getSegmentsEntryIds(
+			long groupId, String className, long classPK, Context context)
+		throws PortalException;
+
+	public SegmentsEntryProvider getSegmentsEntryProvider(String source);
+
+}
