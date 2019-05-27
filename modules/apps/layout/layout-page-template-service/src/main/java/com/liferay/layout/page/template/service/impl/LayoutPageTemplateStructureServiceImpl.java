5464d2a2f4f5e380a0db2cfc2527aace843e4977
Pavel Savinov <pavel.savinov@liferay.com>
2019-05-27 10:55:27 +0200

    LPS-96046 Implement methods with permissions checking

---
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/service/impl/LayoutPageTemplateStructureServiceImpl.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.service.impl;
+
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.base.LayoutPageTemplateStructureServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.BaseModelPermissionCheckerUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+/**
+ * @author Pavel Savinov
+ */
+public class LayoutPageTemplateStructureServiceImpl
+	extends LayoutPageTemplateStructureServiceBaseImpl {
+
+	@Override
+	public LayoutPageTemplateStructure updateLayoutPageTemplateStructure(
+			long groupId, long classNameId, long classPK,
+			long segmentsExperienceId, String data)
+		throws PortalException {
+
+		Boolean containsPermission =
+			BaseModelPermissionCheckerUtil.containsBaseModelPermission(
+				getPermissionChecker(), groupId,
+				_portal.getClassName(classNameId), classPK, ActionKeys.UPDATE);
+
+		if (!containsPermission) {
+			throw new PrincipalException.MustHavePermission(
+				getUserId(), _portal.getClassName(classNameId), classPK,
+				ActionKeys.UPDATE);
+		}
+
+		return layoutPageTemplateStructureLocalService.
+			updateLayoutPageTemplateStructure(
+				groupId, classNameId, classPK, segmentsExperienceId, data);
+	}
+
+	@ServiceReference(type = Portal.class)
+	private Portal _portal;
+
+}
