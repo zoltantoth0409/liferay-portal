ecd18f24f3d5742adf9f7f18fa1f01a319cdec0b
Krešimir Čoko <kresimir.coko@liferay.com>
2019-05-27 13:04:24 +0200

    LPS-94694 frontend-js-web - Add tests for the Address utility

---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/address/get_regions.es.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import getRegions from '../../../../src/main/resources/META-INF/resources/liferay/util/address/get_regions.es';
+
+describe(
+	'Liferay.Address.getRegions',
+	() => {
+		it(
+			'should throw an error if the callback parameter is not a function',
+			() => {
+				expect(() => getRegions('')).toThrow('must be a function');
+			}
+		);
+
+		it(
+			'should throw an error if the selectKey parameter is not a string',
+			() => {
+				expect(() => getRegions(() => {}, {})).toThrow('must be a string');
+			}
+		)
+	}
+);
