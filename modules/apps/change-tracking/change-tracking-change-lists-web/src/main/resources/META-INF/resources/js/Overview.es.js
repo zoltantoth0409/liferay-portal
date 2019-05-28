28fe59706636e6ad2becb8721edef8e284a49e1b
Zoltán Császi <zoltan.csaszi@liferay.com>
2019-05-28 08:30:11 +0200

    LPS-92470 contentsAffected screen added to overview

--- modules/apps/change-tracking/change-tracking-change-lists-web/src/main/resources/META-INF/resources/js/Overview.es.js
+++ modules/apps/change-tracking/change-tracking-change-lists-web/src/main/resources/META-INF/resources/js/Overview.es.js
@@ -5,6 +5,7 @@ import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
 import {Config} from 'metal-state';
 import {openToast} from 'frontend-js-web/liferay/toast/commands/OpenToast.es';
 import {PublishChangeList} from './PublishChangeList.es';
+import {ContentsAffected} from './ContentsAffected.es';
 
 import templates from './Overview.soy';
 
@@ -237,6 +238,19 @@ class Overview extends PortletBase {
 			});
 	}
 
+	_handleClickAffected(event) {
+		event.preventDefault();
+		let entryId = event.target.getAttribute('data-entry-id');
+
+		new ContentsAffected(
+			{
+				entityNameTranslations: this.entityNameTranslations,
+				spritemap: themeDisplay.getPathThemeImages() + '/lexicon/icons.svg',
+				urlAffectedContents: this.urlCollectionsBase + '/' + this.activeCTCollectionId + '/entries/' + entryId + '/affecteds'
+			}
+		);
+	}
+
 	_handleClickPublish(event) {
 		new PublishChangeList({
 			changeListDescription: this.descriptionActiveChangeList,
@@ -366,9 +380,11 @@ class Overview extends PortletBase {
 			);
 
 			this.changeEntries.push({
+				affectedCTEntriesCount: changeEntry.affectedCTEntriesCount,
 				changeType: changeTypeStr,
 				conflict: changeEntry.collision,
 				contentType: entityNameTranslation.translation,
+				ctEntryId: changeEntry.ctEntryId,
 				lastEdited: new Intl.DateTimeFormat(
 					Liferay.ThemeDisplay.getBCP47LanguageId(),
 					{
@@ -675,6 +691,7 @@ Overview.STATE = {
 	 */
 	changeEntries: Config.arrayOf(
 		Config.shapeOf({
+			affectedCTEntriesCount: Config.number(),
 			changeType: Config.string(),
 			conflict: Config.bool(),
 			contentType: Config.string(),
