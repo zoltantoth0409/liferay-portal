d4a0c8d6d7663dde70c5cc6f48bd9b98dc06378b
Greg Hurrell <greg.hurrell@liferay.com>
2019-05-28 17:03:07 +0200

    LPS-95476 Remove remaining usages of `$(document)`

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/side_navigation.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/side_navigation.es.js
@@ -112,8 +112,6 @@
 		return null;
 	}
 
-	var $doc = $(document);
-
 	var listenerAdded = false;
 
 	// Make sure we only add one resize listener to the page,
@@ -224,7 +222,7 @@
 			options.breakpoint = toInt(options.breakpoint);
 			options.container = options.container || $toggler.data('target') || $toggler.attr('href');
 			options.gutter = toInt(options.gutter);
-			options.rtl = $doc.attr('dir') === 'rtl';
+			options.rtl = document.dir === 'rtl';
 			options.width = toInt(options.width);
 			options.widthOriginal = options.width;
 
