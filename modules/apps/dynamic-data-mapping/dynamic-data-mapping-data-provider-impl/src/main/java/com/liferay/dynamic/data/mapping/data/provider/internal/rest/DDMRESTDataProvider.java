f2c7d0dbf331cb47bf08a677693254e6ca241618
jpince <jason.pince@liferay.com>
2019-05-28 07:53:58 -0700

    LPS-96064 Brian Auto SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-impl/src/main/java/com/liferay/dynamic/data/mapping/data/provider/internal/rest/DDMRESTDataProvider.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-impl/src/main/java/com/liferay/dynamic/data/mapping/data/provider/internal/rest/DDMRESTDataProvider.java
@@ -297,9 +297,8 @@ public class DDMRESTDataProvider implements DDMDataProvider {
 			socketHttpConnectionProvider.useProxy(
 				ProxyInfo.httpProxy(proxyAddress, proxyPort, null, null));
 
-			HttpRequest proxiedHttpRequest =
-				httpRequest.withConnectionProvider(
-					socketHttpConnectionProvider);
+			HttpRequest proxiedHttpRequest = httpRequest.withConnectionProvider(
+				socketHttpConnectionProvider);
 
 			httpResponse = proxiedHttpRequest.send();
 		}
