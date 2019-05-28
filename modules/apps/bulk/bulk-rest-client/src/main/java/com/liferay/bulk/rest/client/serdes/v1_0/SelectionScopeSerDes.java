6dbb4d4a6d7ffa632055985589097d14cf7e1767
Brian Chan <brian.chan@liferay.com>
2019-05-27 21:18:26 -0700

    LPS-94274 regen bulk and data engine

--- modules/apps/bulk/bulk-rest-client/src/main/java/com/liferay/bulk/rest/client/serdes/v1_0/SelectionScopeSerDes.java
+++ modules/apps/bulk/bulk-rest-client/src/main/java/com/liferay/bulk/rest/client/serdes/v1_0/SelectionScopeSerDes.java
@@ -133,7 +133,9 @@ public class SelectionScopeSerDes {
 	private static String _escape(Object object) {
 		String string = String.valueOf(object);
 
-		return string.replaceAll("\"", "\\\\\"");
+		string = string.replaceAll("\"", "\\\\\"");
+
+		return string.replace("\\", "\\\\");
 	}
 
 	private static String _toJSON(Map<String, ?> map) {
--- modules/apps/data-engine/data-engine-rest-client/src/main/java/com/liferay/data/engine/rest/client/serdes/v1_0/DataRecordCollectionPermissionSerDes.java
+++ modules/apps/data-engine/data-engine-rest-client/src/main/java/com/liferay/data/engine/rest/client/serdes/v1_0/DataRecordCollectionPermissionSerDes.java
@@ -329,7 +329,9 @@ public class DataRecordCollectionPermissionSerDes {
 	private static String _escape(Object object) {
 		String string = String.valueOf(object);
 
-		return string.replaceAll("\"", "\\\\\"");
+		string = string.replaceAll("\"", "\\\\\"");
+
+		return string.replace("\\", "\\\\");
 	}
 
 	private static String _toJSON(Map<String, ?> map) {
