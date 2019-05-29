725936bb373e3dadec303e76ee8e6643bd4fba6b
shuyangzhou <shuyang.zhou@liferay.com>
2019-05-29 08:57:04 -0700

    LPS-96206 Make StringUtil.merge(Collection, String) behaving the same as StringUtil.merge(Object[], String)

--- portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java
@@ -1773,7 +1773,9 @@ public class StringUtil {
 			sb.append(delimiter);
 		}
 
-		sb.setIndex(sb.index() - 1);
+		if (!delimiter.isEmpty()) {
+			sb.setIndex(sb.index() - 1);
+		}
 
 		return sb.toString();
 	}
