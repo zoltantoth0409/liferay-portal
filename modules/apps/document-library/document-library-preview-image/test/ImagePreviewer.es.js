7200c5d58fb2c160707b24fac1697b84706f9cd1
Julien Castelain <julien.castelain@liferay.com>
2019-05-29 10:11:50 +0200

    LPS-96242 Fix test

--- modules/apps/document-library/document-library-preview-image/test/ImagePreviewer.es.js
+++ modules/apps/document-library/document-library-preview-image/test/ImagePreviewer.es.js
@@ -1,36 +1,24 @@
-
 import ImagePreviewer from '../src/main/resources/META-INF/resources/preview/js/ImagePreviewer.es';
-
-
-let imagePreviewer;
 
 describe(
 	'document-library-preview-image',
 	() => {
 
-		// it('debug', () => {
-		// 	console.log(process.env)
-		// 	console.log(document.body);
+		it(
+			'should render a creation image previwer',
+			() => {
 
-		// 	var d = document.createElement('div');
-		// 	d.innerHTML = 'hola';
-		// 	document.body.appendChild(d);
+				const imagePreviewer = new ImagePreviewer(
+					{
+						element: document.body,
+						imageURL: 'image.jpg',
+						spritemap: 'icons.svg'
+					},
+				);
 
-		// 	expect(true).toBe(true)
-		// });
-
-		it('should render a creation image previwer', () => {
-
-			const imagePreviewer = new ImagePreviewer(
-				{
-					spritemap: 'icons.svg',
-					imageURL: 'image.jpg',
-				},
-				document.body
-			);
-
-			expect(imagePreviewer).toMatchSnapshot();
-		});
+				expect(imagePreviewer).toMatchSnapshot();
+			}
+		);
 
 	}
 );
