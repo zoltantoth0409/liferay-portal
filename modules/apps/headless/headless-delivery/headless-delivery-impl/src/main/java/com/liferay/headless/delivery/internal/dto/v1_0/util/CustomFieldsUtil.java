966eec937ebb7f84058f08aa2c6962ecb5788fb5
Javier Gamarra <nhpatt@gmail.com>
2019-05-29 14:58:53 +0200

    LPS-94857 convert between object and customfield and string to serviceContext map

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/util/CustomFieldsUtil.java
@@ -0,0 +1,292 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.dto.v1_0.util;
+
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.expando.kernel.model.ExpandoColumnConstants;
+import com.liferay.expando.kernel.util.ExpandoBridgeFactoryUtil;
+import com.liferay.headless.delivery.dto.v1_0.CustomField;
+import com.liferay.headless.delivery.dto.v1_0.CustomValue;
+import com.liferay.headless.delivery.dto.v1_0.Geo;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import java.io.Serializable;
+
+import java.lang.reflect.Array;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Javier Gamarra
+ */
+public class CustomFieldsUtil {
+
+	public static CustomField[] toCustomFields(
+		long classPK, Class<?> clazz, long companyId, Locale locale) {
+
+		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
+			companyId, clazz.getName(), classPK);
+
+		Map<String, Serializable> attributes = expandoBridge.getAttributes();
+
+		Set<Map.Entry<String, Serializable>> entries = attributes.entrySet();
+
+		return entries.stream(
+		).filter(
+			entry -> {
+				UnicodeProperties unicodeProperties =
+					expandoBridge.getAttributeProperties(entry.getKey());
+
+				return !GetterUtil.getBoolean(
+					unicodeProperties.getProperty(
+						ExpandoColumnConstants.PROPERTY_HIDDEN));
+			}
+		).map(
+			entry -> _toCustomField(entry, expandoBridge, locale)
+		).toArray(
+			CustomField[]::new
+		);
+	}
+
+	public static Map<String, Serializable> toMap(
+		Class clazz, long companyId, CustomField[] customFields,
+		Locale locale) {
+
+		if (customFields == null) {
+			return null;
+		}
+
+		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
+			companyId, clazz.getName());
+
+		return Stream.of(
+			customFields
+		).collect(
+			Collectors.toMap(
+				CustomField::getName,
+				customField -> {
+					int attributeType = expandoBridge.getAttributeType(
+						customField.getName());
+
+					CustomValue customValue = customField.getCustomValue();
+
+					Object data = customValue.getData();
+
+					if (ExpandoColumnConstants.GEOLOCATION == attributeType) {
+						Geo geo = customValue.getGeo();
+
+						return JSONUtil.put(
+							"latitude", geo.getLatitude()
+						).put(
+							"longitude", geo.getLongitude()
+						).toString();
+					}
+					else if (ExpandoColumnConstants.STRING_LOCALIZED ==
+								attributeType) {
+
+						return (Serializable)Collections.singletonMap(
+							locale, data);
+					}
+					else if (ExpandoColumnConstants.DATE == attributeType) {
+						return _parseDate(String.valueOf(data));
+					}
+					else if (ExpandoColumnConstants.DOUBLE_ARRAY ==
+								attributeType) {
+
+						return _toDoubleArray((List)data);
+					}
+					else if (ExpandoColumnConstants.FLOAT_ARRAY ==
+								attributeType) {
+
+						return _toFloatArray((List)data);
+					}
+					else if (ExpandoColumnConstants.STRING_ARRAY ==
+								attributeType) {
+
+						return ((List)data).toArray(new String[0]);
+					}
+					else if (ExpandoColumnConstants.INTEGER_ARRAY ==
+								attributeType) {
+
+						return _toIntegerArray((List)data);
+					}
+					else if (ExpandoColumnConstants.LONG_ARRAY ==
+								attributeType) {
+
+						return _toLongArray((List)data);
+					}
+
+					return (Serializable)data;
+				})
+		);
+	}
+
+	private static Object _getValue(
+		int attributeType, Locale locale, Object value) {
+
+		if (ExpandoColumnConstants.STRING_LOCALIZED == attributeType) {
+			return ((Map<Locale, String>)value).get(locale);
+		}
+		else if (ExpandoColumnConstants.DATE == attributeType) {
+			return DateUtil.getDate(
+				(Date)value, "yyyy-MM-dd'T'HH:mm:ss'Z'", locale,
+				TimeZone.getTimeZone("UTC"));
+		}
+
+		return value;
+	}
+
+	private static boolean _isEmpty(Object value) {
+		if (value == null) {
+			return true;
+		}
+
+		Class<?> clazz = value.getClass();
+
+		if (clazz.isArray() && (Array.getLength(value) == 0)) {
+			return true;
+		}
+
+		if (value instanceof Map) {
+			Map<?, ?> map = (Map<?, ?>)value;
+
+			if (map.isEmpty()) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private static Serializable _parseDate(String data) {
+		DateFormat dateFormat = DateFormatFactoryUtil.getSimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+		try {
+			return dateFormat.parse(data);
+		}
+		catch (ParseException pe) {
+			throw new IllegalArgumentException(
+				"Unable to parse date from " + data, pe);
+		}
+	}
+
+	private static CustomField _toCustomField(
+		Map.Entry<String, Serializable> entry, ExpandoBridge expandoBridge,
+		Locale locale) {
+
+		String key = entry.getKey();
+
+		int attributeType = expandoBridge.getAttributeType(key);
+
+		if (ExpandoColumnConstants.GEOLOCATION == attributeType) {
+			return new CustomField() {
+				{
+					dataType = "Geolocation";
+					name = entry.getKey();
+
+					setCustomValue(
+						() -> {
+							JSONObject jsonObject =
+								JSONFactoryUtil.createJSONObject(
+									String.valueOf(entry.getValue()));
+
+							return new CustomValue() {
+								{
+									geo = new Geo() {
+										{
+											latitude = jsonObject.getDouble(
+												"latitude");
+											longitude = jsonObject.getDouble(
+												"longitude");
+										}
+									};
+								}
+							};
+						});
+				}
+			};
+		}
+
+		return new CustomField() {
+			{
+				customValue = new CustomValue() {
+					{
+						Object value = entry.getValue();
+
+						if (_isEmpty(entry.getValue())) {
+							value = expandoBridge.getAttributeDefault(key);
+						}
+
+						data = _getValue(attributeType, locale, value);
+					}
+				};
+				dataType = ExpandoColumnConstants.getDataType(attributeType);
+				name = entry.getKey();
+			}
+		};
+	}
+
+	private static Serializable _toDoubleArray(List<Double> doubles) {
+		return doubles.stream(
+		).mapToDouble(
+			Double::doubleValue
+		).toArray();
+	}
+
+	private static Serializable _toFloatArray(List<Double> doubles) {
+		float[] floats = new float[doubles.size()];
+
+		for (int i = 0; i < doubles.size(); i++) {
+			Double aDouble = doubles.get(i);
+
+			floats[i] = aDouble.floatValue();
+		}
+
+		return floats;
+	}
+
+	private static Serializable _toIntegerArray(List<Integer> integers) {
+		return integers.stream(
+		).mapToInt(
+			Integer::intValue
+		).toArray();
+	}
+
+	private static Serializable _toLongArray(List<Integer> integers) {
+		return integers.stream(
+		).mapToLong(
+			Integer::longValue
+		).toArray();
+	}
+
+}
