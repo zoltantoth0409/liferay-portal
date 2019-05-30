7d31a0627bd1eb4d697769760e3fc5bf3d027853
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-05-30 18:29:15 +0200

    LPS-96274 Adding resource command to retrieve the quick reply markup

---
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/portlet/action/GetEditMessageQuickMVCResourceCommand.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.web.internal.portlet.action;
+
+import com.liferay.message.boards.constants.MBPortletKeys;
+import com.liferay.message.boards.model.MBMessage;
+import com.liferay.message.boards.service.MBMessageService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + MBPortletKeys.MESSAGE_BOARDS,
+		"javax.portlet.name=" + MBPortletKeys.MESSAGE_BOARDS_ADMIN,
+		"mvc.command.name=/message_boards/get_edit_message_quick"
+	},
+	service = MVCResourceCommand.class
+)
+public class GetEditMessageQuickMVCResourceCommand
+	extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		MBMessage message = _mbMessageService.getMessage(
+			ParamUtil.getLong(resourceRequest, "messageId"));
+
+		resourceRequest.setAttribute(
+			"edit_message_quick_content.jsp-message", message);
+
+		PortletRequestDispatcher portletRequestDispatcher =
+			getPortletRequestDispatcher(
+				resourceRequest,
+				"/message_boards/edit_message_quick_resource.jsp");
+
+		portletRequestDispatcher.include(resourceRequest, resourceResponse);
+	}
+
+	@Reference
+	private MBMessageService _mbMessageService;
+
+}
