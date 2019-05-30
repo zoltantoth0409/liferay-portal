85f96f2cc14d40dad4b9d21fc175fa530762d1a4
shuyangzhou <shuyang.zhou@liferay.com>
2019-05-29 17:00:59 -0700

    LPS-96252 Deprecated NullSafeStringComparator

--- portal-kernel/src/com/liferay/portal/kernel/util/NullSafeStringComparator.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/NullSafeStringComparator.java
@@ -18,7 +18,10 @@ import java.util.Comparator;
 
 /**
  * @author Shuyang Zhou
+ /**
+ * @deprecated As of Mueller (7.2.x), with no direct replacement
  */
+@Deprecated
 public class NullSafeStringComparator implements Comparator<String> {
 
 	@Override
