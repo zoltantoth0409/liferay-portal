b11246486071fb4a189d8cf5ae9535697aa78aa1
Victor Galan <victor.galan@liferay.com>
2019-05-30 10:15:37 +0200

    LPS-96159 Do not set languageId in portletURL

--- modules/apps/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/soy/DiffVersionComparatorTag.java
+++ modules/apps/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/soy/DiffVersionComparatorTag.java
@@ -107,8 +107,6 @@ public class DiffVersionComparatorTag extends ComponentRendererTag {
 	@Override
 	public int doStartTag() {
 		if (Validator.isNotNull(_languageId)) {
-			_portletURL.setParameter("languageId", _languageId);
-
 			_resourceURL.setParameter("languageId", _languageId);
 		}
 
