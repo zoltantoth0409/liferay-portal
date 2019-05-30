cc5507fb5f1126822d4cfbab3ec2ea6ea9cd5523
tina.tian <tina.tian@liferay.com>
2019-05-30 10:54:07 -0700

    LPS-95571 Revert "LPS-95219 Since we need to process the whole table anyway we don't need the where clause"

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/upgrade/v_1_0_3/UpgradeLayoutTemplateId.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/upgrade/v_1_0_3/UpgradeLayoutTemplateId.java
@@ -31,7 +31,8 @@ public class UpgradeLayoutTemplateId extends UpgradeProcess {
 		String sql = StringBundler.concat(
 			"update Layout set typeSettings = REPLACE(typeSettings, ",
 			"'layout-template-id=1_2_1_columns', ",
-			"'layout-template-id=1_2_1_columns_ii')");
+			"'layout-template-id=1_2_1_columns_ii') where typesettings like ",
+			"'%layout-template-id=1_2_1_columns%'");
 
 		runSQL(sql);
 	}
