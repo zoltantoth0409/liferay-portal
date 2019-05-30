292eeb81d6a2ae3f5ce7259148f9542999b85a72
balazssk <balazs.safrany-kovalik@liferay.com>
2019-05-30 12:02:35 +0200

    LPS-95585 Updating logging

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/StagingImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/StagingImpl.java
@@ -2130,8 +2130,10 @@ public class StagingImpl implements Staging {
 					return true;
 				}
 			}
-			catch (PortalException e) {
-				_log.warn("Couldn't find Global group.");
+			catch (PortalException pe) {
+				if (_log.isWarnEnabled()) {
+					_log.warn("Global group not found", pe);
+				}
 			}
 
 			return false;
