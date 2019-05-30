b99c4693fa2cd062402e7f906db38f44c1cbbf84
Jorge García Jiménez <jorge.garcia@liferay.com>
2019-05-30 10:05:53 +0200

    LPS-96246 Changing page's friendly URL redirects to the old URL after save

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java
@@ -130,6 +130,7 @@ public class EditLayoutMVCActionCommand extends BaseMVCActionCommand {
 		}
 
 		String currentType = layout.getType();
+		String oldFriendlyURL = layout.getFriendlyURL();
 
 		if (StringUtil.equals(
 				currentType, LayoutConstants.TYPE_ASSET_DISPLAY)) {
@@ -232,7 +233,7 @@ public class EditLayoutMVCActionCommand extends BaseMVCActionCommand {
 
 		String redirect = ParamUtil.getString(actionRequest, "redirect");
 
-		if (Validator.isNull(redirect)) {
+		if (Validator.isNull(redirect) || redirect.endsWith(oldFriendlyURL)) {
 			redirect = _portal.getLayoutFullURL(layout, themeDisplay);
 		}
 
