fe7ac32ddc66a07cf610350cbf1292249354e037
Victor Galan <victor.galan@liferay.com>
2019-05-30 10:15:52 +0200

    LPS-96159 Set languageId in the targetURL

--- modules/apps/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/soy/DiffVersionComparatorTag.java
+++ modules/apps/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/soy/DiffVersionComparatorTag.java
@@ -77,6 +77,10 @@ public class DiffVersionComparatorTag extends ComponentRendererTag {
 
 		diffVersionJSONObject.put("sourceURL", sourceURL.toString());
 
+		if (Validator.isNotNull(_languageId)) {
+			targetURL.setParameter("languageId", _languageId);
+		}
+
 		targetURL.setParameter("targetVersion", diffVersionString);
 
 		diffVersionJSONObject.put("targetURL", targetURL.toString());
