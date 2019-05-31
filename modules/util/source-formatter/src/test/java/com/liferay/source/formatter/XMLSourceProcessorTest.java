db1ebe30d99813cace28c65a7a1a43860732cef1
Alan Huang <alan.huang@liferay.com>
2019-05-31 13:50:00 +0800

    LPS-84119 Add testcases

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/XMLSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/XMLSourceProcessorTest.java
@@ -22,6 +22,12 @@ import org.junit.Test;
 public class XMLSourceProcessorTest extends BaseSourceProcessorTestCase {
 
 	@Test
+	public void testIncorrectEmptyLines() throws Exception {
+		test("IncorrectEmptyLines1.testxml");
+		test("IncorrectEmptyLines2.testxml");
+	}
+
+	@Test
 	public void testIncorrectTabs() throws Exception {
 		test("IncorrectTabs1.testaction");
 		test("IncorrectTabs2.testaction");
