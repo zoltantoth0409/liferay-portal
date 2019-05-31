17ef8eb83d1db5f829ed1fb1923a9cdcd2abe1e3
Zoltán Takács <zoltan.takacs@liferay.com>
2019-05-31 15:47:02 +0200

    COMMERCE-1417 Drop TaCoKit component

--- modules/etl/talend/src/main/java/com/liferay/talend/package-info.java
+++
@@ -1,6 +0,0 @@
-@Components(categories = {"Business", "Cloud"}, family = "Liferay")
-@Icon(custom = "LiferayFamily", value = Icon.IconType.CUSTOM)
-package com.liferay.talend;
-
-import org.talend.sdk.component.api.component.Components;
-import org.talend.sdk.component.api.component.Icon;
