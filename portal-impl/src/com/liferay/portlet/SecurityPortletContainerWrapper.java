05c002e3254cf6c711928828d651e68e57f67d85
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2019-05-31 13:36:55 +0200

    LPS-64364 SF

--- portal-impl/src/com/liferay/portlet/SecurityPortletContainerWrapper.java
+++ portal-impl/src/com/liferay/portlet/SecurityPortletContainerWrapper.java
@@ -416,7 +416,8 @@ public class SecurityPortletContainerWrapper implements PortletContainer {
 
 			_log.warn(
 				String.format(
-					"User %s is not allowed to access URL %s and portlet %s: %s",
+					"User %s is not allowed to access URL %s and portlet %s: " +
+						"%s",
 					PortalUtil.getUserId(httpServletRequest), url,
 					portlet.getPortletId(), pe.getMessage()));
 		}
