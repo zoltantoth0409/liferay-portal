7d404cca3845ea9211b2c1d5f5a620410e3da9a1
Victor Ware <victor.ware@liferay.com>
2019-05-31 12:25:01 -0700

    Revert "LPS-96289 SF, simplify"
    This reverts commit 2ea4f7d9ab631cace8a8d31066a319d60154af37.

--- portal-kernel/src/com/liferay/portal/kernel/model/impl/DefaultLayoutTypeAccessPolicyImpl.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/impl/DefaultLayoutTypeAccessPolicyImpl.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.kernel.model.impl;
 
-import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;
@@ -87,12 +86,15 @@ public class DefaultLayoutTypeAccessPolicyImpl
 			}
 		}
 
+		String resourceName = portlet.getDisplayName();
+
+		resourceName = resourceName.concat(StringPool.SPACE);
+
+		resourceName = resourceName.concat(portlet.getPortletId());
+
 		throw new PrincipalException.MustHavePermission(
-			PortalUtil.getUserId(httpServletRequest),
-			StringBundler.concat(
-				portlet.getDisplayName(), StringPool.SPACE,
-				portlet.getPortletId()),
-			0, ActionKeys.ACCESS);
+			PortalUtil.getUserId(httpServletRequest), resourceName, 0,
+			ActionKeys.ACCESS);
 	}
 
 	@Override
