d93ffa4b2125351a3ad29862edf3efc54322030c
Zoltán Császi <zoltan.csaszi@liferay.com>
2019-06-03 09:51:26 +0200

    LPS-96123 null check added before applying lambdas

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/engine/CTEngineManagerImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/engine/CTEngineManagerImpl.java
@@ -738,8 +738,12 @@ public class CTEngineManagerImpl implements CTEngineManager {
 			V versionEntity,
 			Function<V, List<R>> versionEntityRelatedEntityFunction) {
 
-		List<R> relatedEntities = versionEntityRelatedEntityFunction.apply(
-			versionEntity);
+		List<R> relatedEntities = Collections.EMPTY_LIST;
+
+		if (versionEntity != null) {
+			relatedEntities = versionEntityRelatedEntityFunction.apply(
+				versionEntity);
+		}
 
 		Stream<R> relatedEntityStream = relatedEntities.stream();
 
