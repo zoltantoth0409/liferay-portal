10e6b8b26e330087519fc18802b0157c4f5d8ced
Brian Chan <brian.chan@liferay.com>
2019-06-03 22:35:33 -0700

    CEREBRO-2123 JSONUtil

--- portal-kernel/src/com/liferay/portal/kernel/json/JSONUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/json/JSONUtil.java
@@ -29,6 +29,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collector;
 
 /**
  * @author Brian Wing Shun Chan
@@ -65,6 +66,23 @@ public class JSONUtil {
 				collection.add((String)value);
 			}
 		}
+	}
+
+	public static JSONArray concat(JSONArray... jsonArrays) {
+		JSONArray newJSONArray = _createJSONArray();
+
+		for (JSONArray jsonArray : jsonArrays) {
+			for (int i = 0; i < jsonArray.length(); i++) {
+				newJSONArray.put(jsonArray.get(i));
+			}
+		}
+
+		return newJSONArray;
+	}
+
+	public static Collector<Object, JSONArray, JSONArray> createCollector() {
+		return Collector.of(
+			JSONUtil::_createJSONArray, JSONArray::put, JSONUtil::concat);
 	}
 
 	public static Object getValue(Object object, String... paths) {
