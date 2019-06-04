04c6eb9df5260edb865efc1c1445fe0dbbd3a293
Leon Chi <leon.chi@liferay.com>
2019-06-04 08:36:03 +0800

    LPS-96407 SF

--- modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/ratings/test/UserRatingsTest.java
+++ modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/ratings/test/UserRatingsTest.java
@@ -19,7 +19,6 @@ import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -37,7 +36,7 @@ public class UserRatingsTest extends BaseRatingsTestCase {
 
 	@ClassRule
 	@Rule
-	public static final AggregateTestRule aggregateTestRule =
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
 
 	@Override
