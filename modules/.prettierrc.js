e4b5b2e4d439953a1493449b56f14b405707692d
Greg Hurrell <greg.hurrell@liferay.com>
2019-06-04 14:01:07 +0200

    LPS-96415 Set up fallback Prettier config
    Because we want to make sure that the config is applied as consistently
    and as broadly as possible, this commit sets up a fallback that will
    make our standard Prettier formatting config available even in a new
    checkout (ie. one where we haven't done a build yet, or installed
    "liferay-npm-scripts").

--- modules/.prettierrc.js
+++ modules/.prettierrc.js
@@ -1,3 +1,36 @@
-const config = require('liferay-npm-scripts/src/config/prettier');
+/**
+ * This fallback config exists so that users who haven't installed the
+ * NPM modules yet (eg. in a pre-build, clean checkout) will still have
+ * standard Prettier config in their development environments.
+ */
+const FALLBACK_CONFIG = {
+	bracketSpacing: false,
+	endOfLine: 'lf',
+	jsxSingleQuote: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'none',
+	useTabs: true
+};
 
-module.exports = config;
+function getConfig() {
+	let config;
+
+	try {
+		config = require('liferay-npm-scripts/src/config/prettier');
+	} catch (error) {
+		console.log(`info: using fallback config in ${__filename}`);
+		return FALLBACK_CONFIG;
+	}
+
+	if (JSON.stringify(FALLBACK_CONFIG) !== JSON.stringify(config)) {
+		console.warn(
+			`warning: The fallback config in ${__filename} is out of sync ` +
+				'with the one in liferay-npm-scripts and should be updated'
+		);
+	}
+
+	return config;
+}
+
+module.exports = getConfig();
