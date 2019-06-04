7e360a730eacc2350202f50d8ce11d8d1a39fa82
Alan Huang <alan.huang@liferay.com>
2019-06-04 12:47:37 +0800

    LPS-84119 Add testcases

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/BNDSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/BNDSourceProcessorTest.java
@@ -14,12 +14,26 @@
 
 package com.liferay.source.formatter;
 
+import com.liferay.petra.string.StringBundler;
+
 import org.junit.Test;
 
 /**
  * @author Alan Huang
  */
 public class BNDSourceProcessorTest extends BaseSourceProcessorTestCase {
+
+	@Test
+	public void testFormatBndInstructions() throws Exception {
+		test("FormatBndInstructions1/app.testbnd");
+		test("FormatBndInstructions2/app.testbnd");
+		test(
+			"FormatBndInstructions3/app.testbnd",
+			StringBundler.concat(
+				"The 'Liferay-Releng-Suite' can be blank or one of the ",
+				"following values collaboration, forms-and-workflow, ",
+				"foundation, static, web-experience"));
+	}
 
 	@Test
 	public void testIncorrectBundleActivator() throws Exception {
