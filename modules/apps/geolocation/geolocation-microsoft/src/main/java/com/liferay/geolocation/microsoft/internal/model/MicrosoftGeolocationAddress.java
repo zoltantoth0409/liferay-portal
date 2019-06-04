cbac6217bbed33a78d0c24f56ef2de9321284440
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-06-04 14:34:19 +0200

    LPS-96098 Add Microsoft provider

---
+++ modules/apps/geolocation/geolocation-microsoft/src/main/java/com/liferay/geolocation/microsoft/internal/model/MicrosoftGeolocationAddress.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.geolocation.microsoft.internal.model;
+
+import com.liferay.geolocation.model.GeolocationAddress;
+
+/**
+ * @author Eduardo García
+ */
+public class MicrosoftGeolocationAddress implements GeolocationAddress {
+
+	@Override
+	public String getCity() {
+		return null;
+	}
+
+	@Override
+	public String getCountryCode() {
+		return null;
+	}
+
+	@Override
+	public String getRegionCode() {
+		return null;
+	}
+
+	@Override
+	public String getStreet() {
+		return null;
+	}
+
+	@Override
+	public String getZip() {
+		return null;
+	}
+
+}
