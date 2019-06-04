24fb1254acaf72c4b9246f108067a42d3ad1506f
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-06-04 08:03:42 +0200

    LPS-91959 Move closer

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/lar/DeletionSystemEventExporter.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/lar/DeletionSystemEventExporter.java
@@ -121,9 +121,6 @@ public class DeletionSystemEventExporter {
 			Property classNameIdProperty = PropertyFactoryUtil.forName(
 				"classNameId");
 
-			Property extraDataProperty = PropertyFactoryUtil.forName(
-				"extraData");
-
 			Property referrerClassNameIdProperty = PropertyFactoryUtil.forName(
 				"referrerClassNameId");
 
@@ -147,6 +144,9 @@ public class DeletionSystemEventExporter {
 				String className = stagedModelType.getClassName();
 
 				if (className.equals(Layout.class.getName())) {
+					Property extraDataProperty = PropertyFactoryUtil.forName(
+						"extraData");
+
 					boolean privateLayout = MapUtil.getBoolean(
 						portletDataContext.getParameterMap(), "privateLayout");
 
