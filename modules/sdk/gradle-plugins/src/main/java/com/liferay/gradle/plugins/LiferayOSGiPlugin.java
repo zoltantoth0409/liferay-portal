67c71a06b2d2fac43d3d2e97438ab35428257c6b
Peter Shin <peter.shin@liferay.com>
2019-06-04 12:18:19 -0700

    LPS-95442 Auto SF

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/LiferayOSGiPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/LiferayOSGiPlugin.java
@@ -84,7 +84,6 @@ import java.nio.charset.StandardCharsets;
 
 import java.util.Collections;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
