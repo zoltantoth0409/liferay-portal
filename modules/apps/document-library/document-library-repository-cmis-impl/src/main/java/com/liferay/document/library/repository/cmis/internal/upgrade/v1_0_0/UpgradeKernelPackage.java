647ab1a4eb3d075be71ecb93e929933ec2e6dd0c
Adolfo Pérez <adolfo.perez@liferay.com>
2019-06-04 14:38:30 +0200

    LPS-82804 Package was renamed

---
+++ modules/apps/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/upgrade/v1_0_0/UpgradeKernelPackage.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.repository.cmis.internal.upgrade.v1_0_0;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class UpgradeKernelPackage
+	extends com.liferay.portal.upgrade.v7_0_0.UpgradeKernelPackage {
+
+	@Override
+	protected String[][] getClassNames() {
+		return _CLASS_NAMES;
+	}
+
+	@Override
+	protected String[][] getResourceNames() {
+		return _RESOURCE_NAMES;
+	}
+
+	private static final String[][] _CLASS_NAMES = {
+		{
+			"com.liferay.portal.repository.cmis.",
+			"com.liferay.document.library.repository.cmis.internal."
+		}
+	};
+
+	private static final String[][] _RESOURCE_NAMES = {};
+
+}
