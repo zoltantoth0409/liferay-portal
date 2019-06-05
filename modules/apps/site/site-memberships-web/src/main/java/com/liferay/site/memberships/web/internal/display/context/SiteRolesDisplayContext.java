af620b4e37deb701ce5edd5a4e6d7682e12cda50
drewbrokke <drew.brokke@liferay.com>
2019-06-05 12:14:20 -0700

    LPS-95705 site-memberships-web - updates usages of RoleConstants

--- modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/internal/display/context/SiteRolesDisplayContext.java
+++ modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/internal/display/context/SiteRolesDisplayContext.java
@@ -20,7 +20,7 @@ import com.liferay.portal.kernel.dao.search.RowChecker;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Role;
-import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ListUtil;
