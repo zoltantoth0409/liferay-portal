877398ec000966732a4e9f25aa961a6a36e38cd6
Inácio Nery <inacio.nery@liferay.com>
2019-06-05 16:56:25 -0300

    LPS-95810 Add test to get custom calendar

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/CalendarResourceTest.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/CalendarResourceTest.java
@@ -15,21 +15,57 @@
 package com.liferay.portal.workflow.metrics.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.workflow.metrics.rest.client.dto.v1_0.Calendar;
 import com.liferay.portal.workflow.metrics.rest.client.pagination.Page;
 import com.liferay.portal.workflow.metrics.rest.client.resource.v1_0.CalendarResource;
+import com.liferay.portal.workflow.metrics.rest.resource.v1_0.test.helper.WorkflowMetricsRESTTestHelper;
+import com.liferay.portal.workflow.metrics.sla.calendar.WorkflowMetricsSLACalendar;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 import java.util.List;
+import java.util.Locale;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Rafael Praxedes
  */
 @RunWith(Arquillian.class)
 public class CalendarResourceTest extends BaseCalendarResourceTestCase {
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		BaseCalendarResourceTestCase.setUpClass();
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			WorkflowMetricsRESTTestHelper.class);
+
+		_bundleContext = bundle.getBundleContext();
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+
+		if (_serviceRegistration == null) {
+			return;
+		}
+
+		_serviceRegistration.unregister();
+	}
 
 	@Override
 	@Test
@@ -46,4 +82,65 @@ public class CalendarResourceTest extends BaseCalendarResourceTestCase {
 		Assert.assertEquals("default", calendar.getKey());
 		Assert.assertEquals("24/7", calendar.getTitle());
 	}
+
+	@Test
+	public void testGetCalendarsPageWithCustomCalendars() throws Exception {
+		_registerCustomWorkflowMetricsSLACalendar();
+
+		Page<Calendar> calendarsPage = CalendarResource.getCalendarsPage();
+
+		List<Calendar> calendars = (List<Calendar>)calendarsPage.getItems();
+
+		Assert.assertEquals(calendars.toString(), 2, calendars.size());
+
+		for (Calendar calendar : calendars) {
+			if (calendar.getDefaultCalendar()) {
+				continue;
+			}
+
+			Assert.assertEquals(false, calendar.getDefaultCalendar());
+			Assert.assertEquals("custom", calendar.getKey());
+			Assert.assertEquals("Custom", calendar.getTitle());
+		}
+	}
+
+	private void _registerCustomWorkflowMetricsSLACalendar() {
+		_serviceRegistration = _bundleContext.registerService(
+			WorkflowMetricsSLACalendar.class,
+			new WorkflowMetricsSLACalendar() {
+
+				@Override
+				public Duration getDuration(
+					LocalDateTime startLocalDateTime,
+					LocalDateTime endLocalDateTime) {
+
+					return Duration.ZERO;
+				}
+
+				@Override
+				public LocalDateTime getOverdueLocalDateTime(
+					LocalDateTime nowLocalDateTime,
+					Duration remainingDuration) {
+
+					return nowLocalDateTime;
+				}
+
+				@Override
+				public String getTitle(Locale locale) {
+					return "Custom";
+				}
+
+			},
+			new HashMapDictionary<String, String>() {
+				{
+					put("sla.calendar.key", "custom");
+				}
+			});
+	}
+
+	private static BundleContext _bundleContext;
+
+	private ServiceRegistration<WorkflowMetricsSLACalendar>
+		_serviceRegistration;
+
 }
