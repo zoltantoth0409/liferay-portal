cbe3ad3e046d78a5dbc363a61b76e7ac84c019f5
drewbrokke <drew.brokke@liferay.com>
2019-06-05 12:14:45 -0700

    LPS-95705 portal-test - updates usages of RoleConstants

--- portal-test/src/com/liferay/portal/kernel/test/util/RoleTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/RoleTestUtil.java
@@ -16,7 +16,7 @@ package com.liferay.portal.kernel.test.util;
 
 import com.liferay.portal.kernel.exception.NoSuchRoleException;
 import com.liferay.portal.kernel.model.Role;
-import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.test.randomizerbumpers.NumericStringRandomizerBumper;
