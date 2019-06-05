4beb7cb9096f84e1baaa8b43d221c8048d646ac4
David Arques <davidarchena@gmail.com>
2019-06-05 18:04:33 +0200

    LPS-95667 Add tests

---
+++ modules/apps/segments/segments-asah-connector/src/test/java/com/liferay/segments/asah/connector/internal/cache/SegmentsAsahCacheTest.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.asah.connector.internal.cache;
+
+import com.liferay.portal.kernel.cache.PortalCache;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author David Arques
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SegmentsAsahCacheTest {
+
+	@Before
+	public void setUp() {
+		ReflectionTestUtil.setFieldValue(
+			_segmentsAsahCache, "_portalCache", _portalCache);
+	}
+
+	@Test
+	public void testGetSegmentsEntryIds() {
+		String userId = RandomTestUtil.randomString();
+
+		long[] segmentsEntryIds = {
+			RandomTestUtil.randomLong(), RandomTestUtil.randomLong()
+		};
+
+		String cacheKey = _generateCacheKey(userId);
+
+		Mockito.when(
+			_portalCache.get(cacheKey)
+		).thenReturn(
+			segmentsEntryIds
+		);
+
+		Assert.assertArrayEquals(
+			segmentsEntryIds, _segmentsAsahCache.getSegmentsEntryIds(userId));
+
+		Mockito.verify(
+			_portalCache, Mockito.times(1)
+		).get(
+			Mockito.eq(cacheKey)
+		);
+	}
+
+	@Test
+	public void testPutSegmentsEntryIds() {
+		String userId = RandomTestUtil.randomString();
+
+		long[] segmentsEntryIds = {
+			RandomTestUtil.randomLong(), RandomTestUtil.randomLong()
+		};
+
+		String cacheKey = _generateCacheKey(userId);
+
+		_segmentsAsahCache.putSegmentsEntryIds(userId, segmentsEntryIds);
+
+		Mockito.verify(
+			_portalCache, Mockito.times(1)
+		).put(
+			cacheKey, segmentsEntryIds, 0
+		);
+	}
+
+	private String _generateCacheKey(String userId) {
+		return "segments-" + userId;
+	}
+
+	@Mock
+	private PortalCache<String, long[]> _portalCache;
+
+	private final SegmentsAsahCache _segmentsAsahCache =
+		new SegmentsAsahCache();
+
+}
