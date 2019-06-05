a104fbb92d848977e95b81065ba919cdeae4a239
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-06-05 16:56:23 -0300

    LPS-95810 Override testGetCalendarsPage

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/CalendarResourceTest.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-test/src/testIntegration/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/test/CalendarResourceTest.java
@@ -15,14 +15,35 @@
 package com.liferay.portal.workflow.metrics.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.workflow.metrics.rest.client.dto.v1_0.Calendar;
+import com.liferay.portal.workflow.metrics.rest.client.pagination.Page;
+import com.liferay.portal.workflow.metrics.rest.client.resource.v1_0.CalendarResource;
 
-import org.junit.Ignore;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Rafael Praxedes
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class CalendarResourceTest extends BaseCalendarResourceTestCase {
+
+	@Override
+	@Test
+	public void testGetCalendarsPage() throws Exception {
+		Page<Calendar> calendarsPage = CalendarResource.getCalendarsPage();
+
+		List<Calendar> calendars = (List<Calendar>)calendarsPage.getItems();
+
+		Assert.assertEquals(calendars.toString(), 1, calendars.size());
+
+		Calendar calendar = calendars.get(0);
+
+		Assert.assertEquals(true, calendar.getDefaultCalendar());
+		Assert.assertEquals("default", calendar.getKey());
+		Assert.assertEquals("24/7", calendar.getTitle());
+	}
 }
