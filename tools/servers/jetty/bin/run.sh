ce33fd2573d0b30591a3b5c40bc12eaf06899d9e
Victor Ware <victor.ware@liferay.com>
2019-06-06 10:02:36 -0700

    LPS-95636 Increase Metaspace to prevent out of memory on builds with all private modules

--- tools/servers/jetty/bin/run.sh
+++ tools/servers/jetty/bin/run.sh
@@ -6,6 +6,6 @@ then
 	exit
 fi
 
-export JAVA_OPTS="-Dfile.encoding=UTF8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms2560m -Xmx2560m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=512m -XX:MetaspaceSize=512m -XX:NewSize=1536m -XX:SurvivorRatio=7"
+export JAVA_OPTS="-Dfile.encoding=UTF8 -Djava.locale.providers=JRE,COMPAT,CLDR -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms2560m -Xmx2560m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=768m -XX:MetaspaceSize=768m -XX:NewSize=1536m -XX:SurvivorRatio=7"
 
 $JAVA_HOME/bin/java $JAVA_OPTS -jar ../start.jar
