471b5994f6bcc2267efa0d1767e413b82ed3752e
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-06-07 13:15:52 +0200

    LPS-96284 Add edit portlet provider for segments entries

---
+++ modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/portlet/SegmentsEntryEditPortletProvider.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.web.internal.portlet;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
+import com.liferay.portal.kernel.portlet.EditPortletProvider;
+import com.liferay.segments.constants.SegmentsPortletKeys;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eduardo García
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.segments.model.SegmentsEntry",
+	service = EditPortletProvider.class
+)
+public class SegmentsEntryEditPortletProvider
+	extends BasePortletProvider implements EditPortletProvider {
+
+	@Override
+	public String getPortletName() {
+		return SegmentsPortletKeys.SEGMENTS;
+	}
+
+	@Override
+	public PortletURL getPortletURL(HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		PortletURL portletURL = super.getPortletURL(httpServletRequest);
+
+		portletURL.setParameter("mvcRenderCommandName", "editSegmentsEntry");
+
+		return portletURL;
+	}
+
+}
