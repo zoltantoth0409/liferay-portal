4a6c1a30a1cbf76874512ff0788ef467caadb267
Lianne Louie <lianne.louie@liferay.com>
2019-06-06 17:15:21 -0700

    LPS-86455 - remove outdated entries in the tables and indexes files

--- modules/apps/document-library/document-library-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/document-library/document-library-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,8 +1,2 @@
-create unique index IX_18641746 on DLFileEntryPreview (fileEntryId, fileVersionId);
-create index IX_E04CB467 on DLFileEntryPreview (fileVersionId);
-
 create unique index IX_9B6A9A0 on DLFileVersionPreview (fileEntryId, fileVersionId);
 create index IX_E43957CD on DLFileVersionPreview (fileVersionId);
-
-create unique index IX_FBCCD118 on FileVersionPreview (fileEntryId, fileVersionId);
-create index IX_28E06D55 on FileVersionPreview (fileVersionId);
--- modules/apps/document-library/document-library-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/apps/document-library/document-library-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,21 +1,5 @@
-create table DLFileEntryPreview (
-	fileEntryPreviewId LONG not null primary key,
-	groupId LONG,
-	fileEntryId LONG,
-	fileVersionId LONG,
-	previewType INTEGER
-);
-
 create table DLFileVersionPreview (
 	dlFileVersionPreviewId LONG not null primary key,
-	groupId LONG,
-	fileEntryId LONG,
-	fileVersionId LONG,
-	previewStatus INTEGER
-);
-
-create table FileVersionPreview (
-	fileVersionPreviewId LONG not null primary key,
 	groupId LONG,
 	fileEntryId LONG,
 	fileVersionId LONG,
