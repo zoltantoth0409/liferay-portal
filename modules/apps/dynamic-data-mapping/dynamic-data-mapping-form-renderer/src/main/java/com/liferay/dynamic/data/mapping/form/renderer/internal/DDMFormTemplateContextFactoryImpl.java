81114b338bee167d4abbed8095e2c1a4cab9f6ca
Bruno Basto <bruno.basto@liferay.com>
2019-06-10 14:49:43 -0300

    LPS-93027 Make sure to honor form pagination mode

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormTemplateContextFactoryImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormTemplateContextFactoryImpl.java
@@ -133,6 +133,8 @@ public class DDMFormTemplateContextFactoryImpl
 		templateContext.put("pages", pages);
 
 		templateContext.put(
+			"paginationMode", ddmFormLayout.getPaginationMode());
+		templateContext.put(
 			"portletNamespace", ddmFormRenderingContext.getPortletNamespace());
 		templateContext.put("readOnly", ddmFormRenderingContext.isReadOnly());
 
