0f53eefd5c74d69492caf146d4e74e51494a447d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-06-10 17:01:46 +0200

    LPS-97269 Extract code

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/ui/BaseJournalFormNavigatorEntry.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/servlet/taglib/ui/BaseJournalFormNavigatorEntry.java
@@ -15,12 +15,21 @@
 package com.liferay.journal.web.internal.servlet.taglib.ui;
 
 import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.model.JournalArticleConstants;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.bean.BeanParamUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.servlet.taglib.ui.BaseJSPFormNavigatorEntry;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorConstants;
+import com.liferay.portal.kernel.util.JavaConstants;
 
 import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Eudaldo Alonso
@@ -41,6 +50,26 @@ public abstract class BaseJournalFormNavigatorEntry
 	@Override
 	public String getLabel(Locale locale) {
 		return LanguageUtil.get(locale, getKey());
+	}
+
+	protected boolean isEditDefaultValues(JournalArticle article) {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		HttpServletRequest httpServletRequest = serviceContext.getRequest();
+
+		PortletRequest portletRequest =
+			(PortletRequest)httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_REQUEST);
+
+		long classNameId = BeanParamUtil.getLong(
+			article, portletRequest, "classNameId");
+
+		if (classNameId > JournalArticleConstants.CLASSNAME_ID_DEFAULT) {
+			return true;
+		}
+
+		return false;
 	}
 
 }
