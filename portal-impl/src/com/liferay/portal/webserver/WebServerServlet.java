b7e7d5f3b26c6f9ddde150b118d0604605074fd2
Adolfo Pérez <adolfo.perez@liferay.com>
2019-06-10 14:20:47 +0200

    LPS-95290 This will never be null

--- portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java
+++ portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java
@@ -1490,10 +1490,6 @@ public class WebServerServlet extends HttpServlet {
 
 			FileEntry fileEntry = getFileEntry(pathArray);
 
-			if (fileEntry == null) {
-				throw new NoSuchFileEntryException();
-			}
-
 			String portletId = null;
 
 			Group group = GroupLocalServiceUtil.getGroup(
