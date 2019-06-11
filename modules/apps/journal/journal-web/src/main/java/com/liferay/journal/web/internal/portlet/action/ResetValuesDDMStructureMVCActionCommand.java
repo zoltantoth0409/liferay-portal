0bb60800e5809cd58a56b6aafaf054d328d311f3
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-06-11 16:38:29 +0200

    LPS-97269 Creates new action for reset the default values of a structure

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/ResetValuesDDMStructureMVCActionCommand.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.portlet.action;
+
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.service.JournalArticleService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + JournalPortletKeys.JOURNAL,
+		"mvc.command.name=/journal/reset_values_ddm_structure"
+	},
+	service = MVCActionCommand.class
+)
+public class ResetValuesDDMStructureMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long groupId = ParamUtil.getLong(actionRequest, "groupId");
+		String articleId = ParamUtil.getString(actionRequest, "articleId");
+		String ddmStructureKey = ParamUtil.getString(
+			actionRequest, "ddmStructureKey");
+
+		_journalArticleService.deleteArticleDefaultValues(
+			groupId, articleId, ddmStructureKey);
+	}
+
+	@Reference
+	private JournalArticleService _journalArticleService;
+
+	@Reference
+	private Portal _portal;
+
+}
