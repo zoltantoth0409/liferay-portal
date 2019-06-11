daca42776840c17dc0459303ff9e2176d68044ab
Daniel Kocsis <daniel.kocsis@liferay.com>
2019-06-11 18:37:28 +0200

    LPS-90809 Adding upgrade process

---
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/upgrade/ChangeTrackingServiceUpgrade.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.internal.upgrade;
+
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Daniel Kocsis
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class ChangeTrackingServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register("0.0.0", "1.0.0", new DummyUpgradeStep());
+
+		registry.register(
+			"1.0.0", "1.0.1",
+			new com.liferay.change.tracking.internal.upgrade.v1_0_1.
+				UpgradeCTCollection());
+	}
+
+}
