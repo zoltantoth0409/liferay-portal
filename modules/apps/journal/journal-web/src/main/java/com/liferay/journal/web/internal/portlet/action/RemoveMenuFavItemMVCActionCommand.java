5ef5936e5ab73315a64bf66774dfcd5e519a6d94
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-06-11 16:54:54 +0200

    LPS-96824 Extract RemoveMenuFavItem action to a new class

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/RemoveMenuFavItemMVCActionCommand.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.portlet.action;
+
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.web.util.JournalPortletUtil;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + JournalPortletKeys.JOURNAL,
+		"mvc.command.name=/journal/remove_menu_fav_item"
+	},
+	service = MVCActionCommand.class
+)
+public class RemoveMenuFavItemMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String ddmStructureKey = ParamUtil.getString(
+			actionRequest, "ddmStructureKey");
+
+		PortalPreferences portalPreferences =
+			PortletPreferencesFactoryUtil.getPortalPreferences(actionRequest);
+
+		String key = JournalPortletUtil.getAddMenuFavItemKey(
+			actionRequest, actionResponse);
+
+		String[] addMenuFavItems = portalPreferences.getValues(
+			JournalPortletKeys.JOURNAL, key);
+
+		portalPreferences.setValues(
+			JournalPortletKeys.JOURNAL, key,
+			ArrayUtil.remove(addMenuFavItems, ddmStructureKey));
+	}
+
+}
