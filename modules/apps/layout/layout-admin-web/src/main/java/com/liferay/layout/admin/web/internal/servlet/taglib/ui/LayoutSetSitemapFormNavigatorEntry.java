21525941f9097a6b2c0628ffa0b6296cbdb141e8
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-06-11 11:28:38 +0200

    LPS-70047 Don't show sitemap section when configuring private layouts

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/ui/LayoutSetSitemapFormNavigatorEntry.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/ui/LayoutSetSitemapFormNavigatorEntry.java
@@ -14,6 +14,8 @@
 
 package com.liferay.layout.admin.web.internal.servlet.taglib.ui;
 
+import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorConstants;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
 
@@ -40,6 +42,15 @@ public class LayoutSetSitemapFormNavigatorEntry
 	@Override
 	public String getKey() {
 		return "sitemap";
+	}
+
+	@Override
+	public boolean isVisible(User user, LayoutSet layoutSet) {
+		if (layoutSet.isPrivateLayout()) {
+			return false;
+		}
+
+		return super.isVisible(user, layoutSet);
 	}
 
 	@Override
