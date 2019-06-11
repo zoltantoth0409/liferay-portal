a3487fb2625e0c76daaacfd972ad5e224905a3dd
shuyangzhou <shuyang.zhou@liferay.com>
2019-06-10 21:46:25 -0700

    LPS-96675 No need of DS, move initialization to static block to defer it until Spring is asking for it.

--- modules/apps/static/portal-osgi-web/portal-osgi-web-wab-spring-bridge-api/src/main/java/org/eclipse/core/runtime/FileLocator.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-wab-spring-bridge-api/src/main/java/org/eclipse/core/runtime/FileLocator.java
@@ -21,14 +21,10 @@ import java.io.IOException;
 
 import java.net.URL;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 import org.eclipse.osgi.service.urlconversion.URLConverter;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * This is a dummy copy of what's needed to make spring annotation processing
@@ -36,55 +32,27 @@ import org.osgi.service.component.annotations.Deactivate;
  *
  * @author Raymond Augé
  */
-@Component(immediate = true, service = {})
 public class FileLocator {
 
 	public static URL resolve(URL url) throws IOException {
-		String protocol = url.getProtocol();
+		URLConverter converter = _serviceTrackerMap.getService(
+			url.getProtocol());
 
-		_lock.lock();
-
-		try {
-			if (_serviceTrackerMap == null) {
-				Bundle bundle = FrameworkUtil.getBundle(FileLocator.class);
-
-				_serviceTrackerMap =
-					ServiceTrackerMapFactory.openSingleValueMap(
-						bundle.getBundleContext(), URLConverter.class,
-						"protocol");
-			}
-
-			URLConverter converter = _serviceTrackerMap.getService(protocol);
-
-			if (converter == null) {
-				return url;
-			}
-
-			return converter.resolve(url);
+		if (converter == null) {
+			return url;
 		}
-		finally {
-			_lock.unlock();
-		}
+
+		return converter.resolve(url);
 	}
 
-	@Deactivate
-	protected void deactivate() {
-		_lock.lock();
-
-		try {
-			if (_serviceTrackerMap != null) {
-				_serviceTrackerMap.close();
-
-				_serviceTrackerMap = null;
-			}
-		}
-		finally {
-			_lock.unlock();
-		}
-	}
-
-	private static final ReentrantLock _lock = new ReentrantLock(true);
-	private static volatile ServiceTrackerMap<String, URLConverter>
+	private static final ServiceTrackerMap<String, URLConverter>
 		_serviceTrackerMap;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(FileLocator.class);
+
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundle.getBundleContext(), URLConverter.class, "protocol");
+	}
 
 }
