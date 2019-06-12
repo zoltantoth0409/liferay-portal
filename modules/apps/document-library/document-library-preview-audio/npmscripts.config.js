6a3fda53c65ae5754ecf6b36c0df7a65d5546586
Jorge González <jorge.gonzalez@liferay.com>
2019-06-12 15:04:02 +0200

    LPS-96683 SF

--- modules/apps/document-library/document-library-preview-audio/npmscripts.config.js
+++ modules/apps/document-library/document-library-preview-audio/npmscripts.config.js
@@ -2,12 +2,6 @@ const standardPreset = require('liferay-npm-scripts/src/presets/standard/index')
 
 module.exports = {
 	preset: 'liferay-npm-scripts/src/presets/standard/index',
-	format: [
-		...standardPreset.format,
-		'test/**/*.js',
-	],
-	lint: [
-		...standardPreset.lint,
-		'test/**/*.js',
-	]
+	format: [...standardPreset.format, 'test/**/*.js'],
+	lint: [...standardPreset.lint, 'test/**/*.js']
 };
