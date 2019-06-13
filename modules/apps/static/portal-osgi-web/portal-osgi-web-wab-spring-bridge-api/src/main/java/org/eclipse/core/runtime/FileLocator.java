97f2fd918fc535623bb48e086db1edbc19ea58b7
Brian Chan <brian.chan@liferay.com>
2019-06-13 11:02:53 -0700

    LPS-96675 deprecate

--- modules/apps/static/portal-osgi-web/portal-osgi-web-wab-spring-bridge-api/src/main/java/org/eclipse/core/runtime/FileLocator.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-wab-spring-bridge-api/src/main/java/org/eclipse/core/runtime/FileLocator.java
@@ -45,6 +45,13 @@ public class FileLocator {
 		return converter.resolve(url);
 	}
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no replacement
+	 */
+	@Deprecated
+	protected void deactivate() {
+	}
+
 	private static final ServiceTrackerMap<String, URLConverter>
 		_serviceTrackerMap;
 
