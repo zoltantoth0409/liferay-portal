cbb4ec06eb1784bf37ec514815d3394ea5023fda
Leonardo Barros <leonardo.barros@liferay.com>
2019-06-14 08:45:48 -0300

    LPS-96153 Add exception mapper

---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/jaxrs/exception/mapper/NoSuchStructureExceptionMapper.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.data.engine.rest.dto.v1_0.DataDefinition;
+import com.liferay.dynamic.data.mapping.exception.NoSuchStructureException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Leonardo Barros
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Data.Engine.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Data.Engine.REST.NoSuchStructureExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class NoSuchStructureExceptionMapper
+	implements ExceptionMapper<NoSuchStructureException> {
+
+	@Override
+	public Response toResponse(NoSuchStructureException exception) {
+		return Response.status(
+			Response.Status.NOT_FOUND
+		).entity(
+			new DataDefinition()
+		).build();
+	}
+
+}
