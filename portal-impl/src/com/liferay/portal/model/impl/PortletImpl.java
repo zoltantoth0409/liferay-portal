e6cee2468ef45fe14d57bff754ff1c6fea2ddfff
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-06-17 10:52:07 +0200

    LPS-96946 Avoid NPE

--- portal-impl/src/com/liferay/portal/model/impl/PortletImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/PortletImpl.java
@@ -701,6 +701,10 @@ public class PortletImpl extends PortletBaseImpl {
 	public ConfigurationAction getConfigurationActionInstance() {
 		PortletBag portletBag = PortletBagPool.get(getRootPortletId());
 
+		if (portletBag == null) {
+			return null;
+		}
+
 		List<ConfigurationAction> configurationActionInstances =
 			portletBag.getConfigurationActionInstances();
 
@@ -2232,6 +2236,10 @@ public class PortletImpl extends PortletBaseImpl {
 	@Override
 	public WebDAVStorage getWebDAVStorageInstance() {
 		PortletBag portletBag = PortletBagPool.get(getRootPortletId());
+
+		if (portletBag == null) {
+			return null;
+		}
 
 		List<WebDAVStorage> webDAVStorageInstances =
 			portletBag.getWebDAVStorageInstances();
