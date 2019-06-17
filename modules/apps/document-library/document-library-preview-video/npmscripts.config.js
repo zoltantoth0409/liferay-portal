fb8b6c89be2438775dfe8b1b52166d363d9e710d
Jorge González <jorge.gonzalez@liferay.com>
2019-06-17 14:58:26 +0200

    LPS-96683 Remove unnecessary config to fix/check the test code

--- modules/apps/document-library/document-library-preview-video/npmscripts.config.js
+++
@@ -1,7 +0,0 @@
-const standardPreset = require('liferay-npm-scripts/src/presets/standard/index');
-
-module.exports = {
-	preset: 'liferay-npm-scripts/src/presets/standard/index',
-	format: [...standardPreset.format, 'test/**/*.js'],
-	lint: [...standardPreset.lint, 'test/**/*.js']
-};
