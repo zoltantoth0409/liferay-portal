afc2e9e47ec04b33e2198a353d70b3ff1ec8bc20
Iván Zaera Avellón <izaera@gmx.es>
2019-06-18 13:08:02 +0200

    LPS-97016 Update Liferay.Language.get regex to tolerate line breaks

--- portal-impl/src/com/liferay/portal/language/LanguageImpl.java
+++ portal-impl/src/com/liferay/portal/language/LanguageImpl.java
@@ -1918,7 +1918,8 @@ public class LanguageImpl implements Language, Serializable {
 	private static PortalCache<Long, Serializable> _groupLocalesPortalCache;
 	private static volatile long _lastModified = System.currentTimeMillis();
 	private static final Pattern _pattern = Pattern.compile(
-		"Liferay\\.Language\\.get\\([\"']([^)]+)[\"']\\)");
+		"Liferay\\s*\\.\\s*Language\\s*\\.\\s*get\\s*\\(\\s*[\"']([^)]+)[\"']\\s*\\)",
+		Pattern.MULTILINE);
 
 	private static final Synchronizer<Long, Serializable> _removeSynchronizer =
 		new Synchronizer<Long, Serializable>() {
