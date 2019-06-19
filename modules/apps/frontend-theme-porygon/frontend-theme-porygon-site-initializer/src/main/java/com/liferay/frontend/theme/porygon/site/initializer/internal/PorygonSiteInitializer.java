6504e8ea99b1cc8fce3e40d7e71f09f48fc509bf
Pavel Savinov <pavel.savinov@liferay.com>
2019-06-19 16:12:29 +0200

    LPS-96866 Use complete method to create layout page template entry for the display page

--- modules/apps/frontend-theme-porygon/frontend-theme-porygon-site-initializer/src/main/java/com/liferay/frontend/theme/porygon/site/initializer/internal/PorygonSiteInitializer.java
+++ modules/apps/frontend-theme-porygon/frontend-theme-porygon-site-initializer/src/main/java/com/liferay/frontend/theme/porygon/site/initializer/internal/PorygonSiteInitializer.java
@@ -266,8 +266,8 @@ public class PorygonSiteInitializer implements SiteInitializer {
 				serviceContext.getUserId(), serviceContext.getScopeGroupId(), 0,
 				ddmStructure.getClassNameId(), ddmStructure.getStructureId(),
 				name, LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE,
-				true, 0, previewFileEntryId, WorkflowConstants.STATUS_APPROVED,
-				serviceContext);
+				true, 0, previewFileEntryId, 0,
+				WorkflowConstants.STATUS_APPROVED, serviceContext);
 
 		long[] fragmentEntryIds = ListUtil.toLongArray(
 			fragmentEntries, FragmentEntryModel::getFragmentEntryId);
