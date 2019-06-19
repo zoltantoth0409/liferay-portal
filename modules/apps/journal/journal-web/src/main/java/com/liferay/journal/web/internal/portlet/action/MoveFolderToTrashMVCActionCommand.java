b614669269586f5c5448b325af5143da6262cfcb
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-06-19 15:52:01 +0200

    LPS-96824 Extract deleteFolder and moveFolderToTrash to new classes

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/MoveFolderToTrashMVCActionCommand.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.portlet.action;
+
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.model.JournalFolder;
+import com.liferay.journal.service.JournalFolderService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + JournalPortletKeys.JOURNAL,
+		"mvc.command.name=/journal/move_folder_to_trash"
+	},
+	service = MVCActionCommand.class
+)
+public class MoveFolderToTrashMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long folderId = ParamUtil.getLong(actionRequest, "folderId");
+
+		JournalFolder folder = _journalFolderService.moveFolderToTrash(
+			folderId);
+
+		Map<String, Object> data = new HashMap<>();
+
+		data.put("trashedModels", Collections.singletonList(folder));
+
+		SessionMessages.add(
+			actionRequest,
+			_portal.getPortletId(actionRequest) +
+				SessionMessages.KEY_SUFFIX_DELETE_SUCCESS_DATA,
+			data);
+
+		hideDefaultSuccessMessage(actionRequest);
+	}
+
+	@Reference
+	private JournalFolderService _journalFolderService;
+
+	@Reference
+	private Portal _portal;
+
+}
