0ef9c97b3f9ac0aed693279445bf274d80209702
Leonardo Barros <leonardo.barros@liferay.com>
2019-06-19 10:50:48 -0300

    LPS-96186 Pass values to data layout renderer

--- modules/apps/data-engine/data-engine-taglib/src/main/java/com/liferay/data/engine/taglib/servlet/taglib/util/DataLayoutTaglibUtil.java
+++ modules/apps/data-engine/data-engine-taglib/src/main/java/com/liferay/data/engine/taglib/servlet/taglib/util/DataLayoutTaglibUtil.java
@@ -14,6 +14,8 @@
 
 package com.liferay.data.engine.taglib.servlet.taglib.util;
 
+import com.liferay.data.engine.rest.client.dto.v1_0.DataRecord;
+import com.liferay.data.engine.rest.client.resource.v1_0.DataRecordResource;
 import com.liferay.data.engine.spi.field.type.FieldType;
 import com.liferay.data.engine.spi.field.type.FieldTypeTracker;
 import com.liferay.data.engine.spi.renderer.DataLayoutRenderer;
@@ -46,6 +48,7 @@ import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -75,12 +78,14 @@ public class DataLayoutTaglibUtil {
 	}
 
 	public static String renderDataLayout(
-			Long dataLayoutId, HttpServletRequest httpServletRequest,
+			Long dataLayoutId, long dataRecordId,
+			HttpServletRequest httpServletRequest,
 			HttpServletResponse httpServletResponse)
 		throws Exception {
 
 		return _instance._dataLayoutRenderer.render(
-			dataLayoutId, httpServletRequest, httpServletResponse);
+			dataLayoutId, _getDataRecordValues(dataRecordId),
+			httpServletRequest, httpServletResponse);
 	}
 
 	public static String resolveFieldTypesModules() {
@@ -99,6 +104,21 @@ public class DataLayoutTaglibUtil {
 	@Deactivate
 	protected void deactivate() {
 		_instance = null;
+	}
+
+	private static Map<String, Object> _getDataRecordValues(long dataRecordId)
+		throws Exception {
+
+		if (dataRecordId == 0) {
+			return Collections.emptyMap();
+		}
+
+		DataRecordResource dataRecordResource = DataRecordResource.builder(
+		).build();
+
+		DataRecord dataRecord = dataRecordResource.getDataRecord(dataRecordId);
+
+		return dataRecord.getDataRecordValues();
 	}
 
 	private JSONObject _createFieldContext(
