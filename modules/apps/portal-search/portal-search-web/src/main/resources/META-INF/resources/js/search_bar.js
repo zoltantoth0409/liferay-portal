bef9451854ba9f011b050e9dd789b405c3f3fab3
Jorge Díaz <jorge.diaz@liferay.com>
2019-06-19 09:51:45 +0200

    LPS-95798 Partial backport of "LPS-96376 Update to liferay-npm-scripts v2.1.0 (prettier)" commit 7930ab36254ae02f3d40dcfc23d8ac3503867f2d only search_bar.js and search_bar_test.js changes.

--- modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/search_bar.js
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/search_bar.js
@@ -10,126 +10,136 @@ AUI.add(
 
 			instance.form.on('submit', A.bind(instance._onSubmit, instance));
 
-			var emptySearchInput = instance.form.one('.search-bar-empty-search-input');
+			var emptySearchInput = instance.form.one(
+				'.search-bar-empty-search-input'
+			);
 
 			if (emptySearchInput.val() === 'true') {
 				instance.emptySearchEnabled = true;
-			}
-			else {
+			} else {
 				instance.emptySearchEnabled = false;
 			}
 
-			instance.keywordsInput = instance.form.one('.search-bar-keywords-input');
+			instance.keywordsInput = instance.form.one(
+				'.search-bar-keywords-input'
+			);
 
-			instance.resetStartPage = instance.form.one('.search-bar-reset-start-page');
+			instance.resetStartPage = instance.form.one(
+				'.search-bar-reset-start-page'
+			);
 
-			instance.scopeSelect = instance.form.one('.search-bar-scope-select');
+			instance.scopeSelect = instance.form.one(
+				'.search-bar-scope-select'
+			);
 
 			var searchButton = instance.form.one('.search-bar-search-button');
 
 			searchButton.on('click', A.bind(instance._onClick, instance));
 		};
 
-		A.mix(
-			SearchBar.prototype,
-			{
-				getKeywords: function() {
-					var instance = this;
+		A.mix(SearchBar.prototype, {
+			getKeywords: function() {
+				var instance = this;
 
-					var keywords = instance.keywordsInput.val();
+				var keywords = instance.keywordsInput.val();
 
-					return keywords.replace(/^\s+|\s+$/, '');
-				},
+				return keywords.replace(/^\s+|\s+$/, '');
+			},
 
-				isSubmitEnabled: function() {
-					var instance = this;
+			isSubmitEnabled: function() {
+				var instance = this;
 
-					return (instance.getKeywords() !== '') || instance.emptySearchEnabled;
-				},
+				return (
+					instance.getKeywords() !== '' || instance.emptySearchEnabled
+				);
+			},
 
-				search: function() {
-					var instance = this;
+			search: function() {
+				var instance = this;
 
-					if (instance.isSubmitEnabled()) {
-						var searchURL = instance.form.get('action');
+				if (instance.isSubmitEnabled()) {
+					var searchURL = instance.form.get('action');
 
-						var queryString = instance.updateQueryString(document.location.search);
+					var queryString = instance.updateQueryString(
+						document.location.search
+					);
 
-						document.location.href = searchURL + queryString;
-					}
-				},
+					document.location.href = searchURL + queryString;
+				}
+			},
 
-				updateQueryString: function(queryString) {
-					var instance = this;
+			updateQueryString: function(queryString) {
+				var instance = this;
 
-					var hasQuestionMark = false;
+				var hasQuestionMark = false;
 
-					if (queryString[0] === '?') {
-						hasQuestionMark = true;
-					}
+				if (queryString[0] === '?') {
+					hasQuestionMark = true;
+				}
 
+				queryString = FacetUtil.updateQueryString(
+					instance.keywordsInput.get('name'),
+					[instance.getKeywords()],
+					queryString
+				);
+
+				if (instance.scopeSelect) {
 					queryString = FacetUtil.updateQueryString(
-						instance.keywordsInput.get('name'),
-						[instance.getKeywords()],
+						instance.scopeSelect.get('name'),
+						[instance.scopeSelect.val()],
 						queryString
 					);
+				}
 
-					if (instance.scopeSelect) {
-						queryString = FacetUtil.updateQueryString(
-							instance.scopeSelect.get('name'),
-							[instance.scopeSelect.val()],
-							queryString
-						);
-					}
+				if (hasQuestionMark) {
+					var parts = queryString.split('?');
 
-					if (hasQuestionMark) {
-						var parts = queryString.split('?');
+					queryString = parts[1];
 
-						queryString = parts[1];
+					hasQuestionMark = false;
+				}
 
-						hasQuestionMark = false;
-					}
+				var parameterArray = queryString.split('&');
 
-					var parameterArray = queryString.split('&');
+				parameterArray = FacetUtil.removeURLParameters(
+					'start',
+					parameterArray
+				);
 
-					parameterArray = FacetUtil.removeURLParameters(
-						'start',
-						parameterArray
+				if (instance.resetStartPage) {
+					var resetStartPageName = instance.resetStartPage.get(
+						'name'
 					);
 
-					if (instance.resetStartPage) {
-						var resetStartPageName = instance.resetStartPage.get('name');
-
-						parameterArray = FacetUtil.removeURLParameters(
-							resetStartPageName,
-							parameterArray
-						);
-					}
-
-					queryString = parameterArray.join('&');
-
-					if (!hasQuestionMark) {
-						queryString = '?' + queryString;
-					}
-
-					return queryString;
-				},
-
-				_onClick: function(event) {
-					var instance = this;
-
-					instance.search();
-				},
-
-				_onSubmit: function(event) {
-					var instance = this;
-
-					event.stopPropagation();
-
-					instance.search();
+					parameterArray = FacetUtil.removeURLParameters(
+						resetStartPageName,
+						parameterArray
+					);
 				}
+
+				queryString = parameterArray.join('&');
+
+				if (!hasQuestionMark) {
+					queryString = '?' + queryString;
+				}
+
+				return queryString;
+			},
+
+			_onClick: function(event) {
+				var instance = this;
+
+				instance.search();
+			},
+
+			_onSubmit: function(event) {
+				var instance = this;
+
+				event.stopPropagation();
+
+				instance.search();
 			}
-		);
+		});
 
 		Liferay.namespace('Search').SearchBar = SearchBar;
 	},
