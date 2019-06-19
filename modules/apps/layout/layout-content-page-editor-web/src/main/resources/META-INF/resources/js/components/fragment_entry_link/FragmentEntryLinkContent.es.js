56fd3aab96f3f177686a73fe986865651cec97f5
Paulo Cruz <paulo.cruz@liferay.com>
2019-06-19 11:01:00 -0300

    LPS-91917 Fix content scripts running twice

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_entry_link/FragmentEntryLinkContent.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_entry_link/FragmentEntryLinkContent.es.js
@@ -67,9 +67,9 @@ class FragmentEntryLinkContent extends Component {
 	/**
 	 * @inheritDoc
 	 */
-	rendered() {
+	rendered(firstRender) {
 		if (this.content) {
-			this._renderContent(this.content);
+			this._renderContent(this.content, firstRender);
 		}
 	}
 
@@ -256,11 +256,13 @@ class FragmentEntryLinkContent extends Component {
 	 * @param {string} content
 	 * @private
 	 */
-	_renderContent(content) {
+	_renderContent(content, firstRender) {
 		if (content && this.refs.content) {
 			this.refs.content.innerHTML = content;
 
-			globalEval.runScriptsInElement(this.refs.content);
+			if (firstRender) {
+				globalEval.runScriptsInElement(this.refs.content);
+			}
 
 			if (this.editableValues) {
 				this._createEditables();
