8fb05103f4703cc6c9add33cdfa13f9cce72b8da
spencerwoo <spencer.woo@liferay.com>
2019-06-20 14:45:38 -0700

    LPS-93973 doAsUserId should be preserved for redirects.

--- portal-impl/src/com/liferay/portal/servlet/FriendlyURLServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/FriendlyURLServlet.java
@@ -14,12 +14,15 @@
 
 package com.liferay.portal.servlet;
 
+import com.liferay.petra.encryptor.Encryptor;
+import com.liferay.petra.encryptor.EncryptorException;
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.exception.NoSuchGroupException;
 import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
@@ -65,6 +68,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Brian Wing Shun Chan
@@ -396,6 +400,23 @@ public class FriendlyURLServlet extends HttpServlet {
 			actualURL = StringUtil.removeSubstring(actualURL, portalURL);
 		}
 
+		long userId = PortalUtil.getUserId(request);
+
+		Company company = PortalUtil.getCompany(request);
+
+		if ((userId > 0) && _isImpersonated(request, userId)) {
+			try {
+				String encDoAsUserId = Encryptor.encrypt(
+					company.getKeyObj(), String.valueOf(userId));
+
+				actualURL = HttpUtil.setParameter(
+					actualURL, "doAsUserId", encDoAsUserId);
+			}
+			catch (EncryptorException ee) {
+				return new Redirect(actualURL);
+			}
+		}
+
 		return new Redirect(actualURL);
 	}
 
@@ -546,6 +567,18 @@ public class FriendlyURLServlet extends HttpServlet {
 		}
 
 		return false;
+	}
+
+	private boolean _isImpersonated(HttpServletRequest request, long userId) {
+		HttpSession session = request.getSession();
+
+		Long realUserId = (Long)session.getAttribute(WebKeys.USER_ID);
+
+		if (userId == realUserId) {
+			return false;
+		}
+
+		return true;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
