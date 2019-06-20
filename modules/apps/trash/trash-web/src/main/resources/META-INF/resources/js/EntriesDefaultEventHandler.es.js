19fdbc7a92cee3a1d38144932a7bf517035d969b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-06-20 21:27:17 +0200

    LPS-97080 Split method into restore and move

--- modules/apps/trash/trash-web/src/main/resources/META-INF/resources/js/EntriesDefaultEventHandler.es.js
+++ modules/apps/trash/trash-web/src/main/resources/META-INF/resources/js/EntriesDefaultEventHandler.es.js
@@ -15,71 +15,71 @@
 import {DefaultEventHandler} from 'frontend-js-web';
 
 class EntriesDefaultEventHandler extends DefaultEventHandler {
-	restoreEntry(itemData) {
+	moveEntry(itemData) {
 		const instance = this;
 
-		if (itemData.move && itemData.move !== 'false') {
-			Liferay.Util.selectEntity(
-				{
-					dialog: {
-						constrain: true,
-						destroyOnHide: true,
-						modal: true,
-						width: 1024
-					},
-					eventName: this.ns('selectContainer'),
-					id: this.ns('selectContainer'),
-					title: Liferay.Language.get('warning'),
-					uri: itemData.restoreURL
+		Liferay.Util.selectEntity(
+			{
+				dialog: {
+					constrain: true,
+					destroyOnHide: true,
+					modal: true,
+					width: 1024
 				},
-				function(event) {
-					const selectContainerForm = document.getElementById(
-						`${instance.namespace}selectContainerForm`
+				eventName: this.ns('selectContainer'),
+				id: this.ns('selectContainer'),
+				title: Liferay.Language.get('warning'),
+				uri: itemData.moveEntryURL
+			},
+			function(event) {
+				const selectContainerForm = document.getElementById(
+					`${instance.namespace}selectContainerForm`
+				);
+
+				if (selectContainerForm) {
+					const className = selectContainerForm.querySelector(
+						`#${instance.namespace}className`
 					);
 
-					if (selectContainerForm) {
-						const className = selectContainerForm.querySelector(
-							`#${instance.namespace}className`
-						);
-
-						if (className) {
-							className.setAttribute('value', event.classname);
-						}
-
-						const classPK = selectContainerForm.querySelector(
-							`#${instance.namespace}classPK`
-						);
-
-						if (classPK) {
-							classPK.setAttribute('value', event.classpk);
-						}
-
-						const containerModelId = selectContainerForm.querySelector(
-							`#${instance.namespace}containerModelId`
-						);
-
-						if (containerModelId) {
-							containerModelId.setAttribute(
-								'value',
-								event.containermodelid
-							);
-						}
-
-						const redirect = selectContainerForm.querySelector(
-							`#${instance.namespace}redirect`
-						);
-
-						if (redirect) {
-							redirect.setAttribute('value', event.redirect);
-						}
-
-						submitForm(selectContainerForm);
+					if (className) {
+						className.setAttribute('value', event.classname);
 					}
+
+					const classPK = selectContainerForm.querySelector(
+						`#${instance.namespace}classPK`
+					);
+
+					if (classPK) {
+						classPK.setAttribute('value', event.classpk);
+					}
+
+					const containerModelId = selectContainerForm.querySelector(
+						`#${instance.namespace}containerModelId`
+					);
+
+					if (containerModelId) {
+						containerModelId.setAttribute(
+							'value',
+							event.containermodelid
+						);
+					}
+
+					const redirect = selectContainerForm.querySelector(
+						`#${instance.namespace}redirect`
+					);
+
+					if (redirect) {
+						redirect.setAttribute('value', event.redirect);
+					}
+
+					submitForm(selectContainerForm);
 				}
-			);
-		} else {
-			submitForm(document.hrefFm, itemData.restoreURL);
-		}
+			}
+		);
+	}
+
+	restoreEntry(itemData) {
+		submitForm(document.hrefFm, itemData.restoreEntryURL);
 	}
 
 	deleteEntry(itemData) {
