48f02636359007bf409aadc9a6804d33ced5b371
shuyangzhou <shuyang.zhou@liferay.com>
2019-06-20 22:49:46 -0700

    LPS-97219 Move LockListenerRegistryUtil's logic to LockLocalServiceImpl

--- portal-kernel/src/com/liferay/portal/kernel/lock/LockListenerRegistryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/lock/LockListenerRegistryUtil.java
@@ -14,74 +14,16 @@
 
 package com.liferay.portal.kernel.lock;
 
-import com.liferay.registry.Registry;
-import com.liferay.registry.RegistryUtil;
-import com.liferay.registry.ServiceReference;
-import com.liferay.registry.ServiceTracker;
-import com.liferay.registry.ServiceTrackerCustomizer;
-
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Alexander Chow
  * @author Peter Fellwock
+ * @deprecated As of Mueller (7.2.x), with no direct replacement
  */
+@Deprecated
 public class LockListenerRegistryUtil {
 
 	public static LockListener getLockListener(String className) {
-		return _instance._lockListeners.get(className);
-	}
-
-	private LockListenerRegistryUtil() {
-		Registry registry = RegistryUtil.getRegistry();
-
-		_serviceTracker = registry.trackServices(
-			LockListener.class, new LockListenerServiceTrackerCustomizer());
-
-		_serviceTracker.open();
-	}
-
-	private static final LockListenerRegistryUtil _instance =
-		new LockListenerRegistryUtil();
-
-	private final Map<String, LockListener> _lockListeners = new HashMap<>();
-	private final ServiceTracker<?, LockListener> _serviceTracker;
-
-	private class LockListenerServiceTrackerCustomizer
-		implements ServiceTrackerCustomizer<LockListener, LockListener> {
-
-		@Override
-		public LockListener addingService(
-			ServiceReference<LockListener> serviceReference) {
-
-			Registry registry = RegistryUtil.getRegistry();
-
-			LockListener lockListener = registry.getService(serviceReference);
-
-			_lockListeners.put(lockListener.getClassName(), lockListener);
-
-			return lockListener;
-		}
-
-		@Override
-		public void modifiedService(
-			ServiceReference<LockListener> serviceReference,
-			LockListener lockListener) {
-		}
-
-		@Override
-		public void removedService(
-			ServiceReference<LockListener> serviceReference,
-			LockListener lockListener) {
-
-			Registry registry = RegistryUtil.getRegistry();
-
-			registry.ungetService(serviceReference);
-
-			_lockListeners.remove(lockListener.getClassName());
-		}
-
+		return null;
 	}
 
 }
