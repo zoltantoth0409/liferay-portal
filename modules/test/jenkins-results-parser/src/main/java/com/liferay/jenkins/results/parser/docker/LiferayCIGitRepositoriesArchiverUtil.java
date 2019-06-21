0f7f958ffaf4e1f641ba7d3abe687434e6f21c02
Michael Hashimoto <michael.hashimoto@liferay.com>
2019-06-20 19:32:09 -0700

    LRCI-382 Rename 'LiferayCIGitRepositoriesInitializerUtil' to 'LiferayCIGitRepositoriesArchiver"

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/docker/LiferayCIGitRepositoriesArchiverUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/docker/LiferayCIGitRepositoriesArchiverUtil.java
@@ -37,7 +37,7 @@ import java.util.Map;
  * @author Peter Yoo
  * @author Michael Hashimoto
  */
-public class LiferayCIGitRepositoriesInitializerUtil {
+public class LiferayCIGitRepositoriesArchiverUtil {
 
 	public static void init() {
 		JenkinsResultsParserUtil.regenerateSshIdRsa(
