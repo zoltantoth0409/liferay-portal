0a1fe78b8185b46991686a15bfcfcd7357d3ac80
Pavel Savinov <pavel.savinov@liferay.com>
2019-06-21 10:34:59 +0200

    LPS-96706 Get expando-based info display contributor fields by class name

---
+++ modules/apps/asset/asset-info-display-api/src/main/java/com/liferay/asset/info/display/contributor/util/CustomFieldsInfoDisplayContributorFieldUtil.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.info.display.contributor.util;
+
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.expando.kernel.util.ExpandoBridgeFactoryUtil;
+import com.liferay.info.display.contributor.field.InfoDisplayContributorField;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ * @author Pavel Savinov
+ */
+public class CustomFieldsInfoDisplayContributorFieldUtil {
+
+	public static List<InfoDisplayContributorField>
+		getInfoDisplayContributorFields(String className) {
+
+		List<InfoDisplayContributorField> infoDisplayContributorFields =
+			new ArrayList<>();
+
+		ExpandoBridge expandoBridge = ExpandoBridgeFactoryUtil.getExpandoBridge(
+			CompanyThreadLocal.getCompanyId(), className, 0L);
+
+		Enumeration<String> attributeNames = expandoBridge.getAttributeNames();
+
+		while (attributeNames.hasMoreElements()) {
+			String attributeName = attributeNames.nextElement();
+
+			infoDisplayContributorFields.add(
+				new CustomFieldInfoDisplayContributorField(
+					attributeName, expandoBridge));
+		}
+
+		return infoDisplayContributorFields;
+	}
+
+}
