daeb9553f6b0396f17b1b3f5e6b271132ac86da0
Sarai Díaz García <dgarciasarai@gmail.com>
2019-06-24 16:57:36 +0200

    LPS-97280 Add AssetListHelper

---
+++ modules/apps/asset/asset-list-api/src/main/java/com/liferay/asset/list/util/AssetListHelper.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.util;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
+import com.liferay.asset.list.model.AssetListEntry;
+
+import java.util.List;
+
+/**
+ * @author Sarai Díaz
+ */
+public interface AssetListHelper {
+
+	public List<AssetEntry> getAssetEntries(
+		AssetListEntry assetListEntry, long segmentsEntryId);
+
+	public List<AssetEntry> getAssetEntries(
+		AssetListEntry assetListEntry, long segmentsEntryId, int start,
+		int end);
+
+	public List<AssetEntry> getAssetEntries(
+		AssetListEntry assetListEntry, long[] segmentsEntryIds);
+
+	public List<AssetEntry> getAssetEntries(
+		AssetListEntry assetListEntry, long[] segmentsEntryIds, int start,
+		int end);
+
+	public int getAssetEntriesCount(
+		AssetListEntry assetListEntry, long segmentsEntryId);
+
+	public int getAssetEntriesCount(
+		AssetListEntry assetListEntry, long[] segmentsEntryIds);
+
+	public AssetEntryQuery getAssetEntryQuery(
+		AssetListEntry assetListEntry, long segmentsEntryId);
+
+	public AssetEntryQuery getAssetEntryQuery(
+		AssetListEntry assetListEntry, long[] segmentsEntryIds);
+
+}
