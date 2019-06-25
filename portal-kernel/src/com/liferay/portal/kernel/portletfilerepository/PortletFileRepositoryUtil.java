3fe69ab535fc5ffedc1f8607f92136534a4ba386
Adolfo Pérez <adolfo.perez@liferay.com>
2019-06-25 11:18:25 +0200

    LPS-97276 Keep backwards compatibility

--- portal-kernel/src/com/liferay/portal/kernel/portletfilerepository/PortletFileRepositoryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/portletfilerepository/PortletFileRepositoryUtil.java
@@ -362,6 +362,16 @@ public class PortletFileRepositoryUtil {
 			repositoryId, searchContext);
 	}
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
+	public void setPortletFileRepository(
+		PortletFileRepository portletFileRepository) {
+
+		_portletFileRepository = portletFileRepository;
+	}
+
 	private static volatile PortletFileRepository _portletFileRepository =
 		ServiceProxyFactory.newServiceTrackedInstance(
 			PortletFileRepository.class, PortletFileRepositoryUtil.class,
