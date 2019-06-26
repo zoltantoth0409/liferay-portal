6f6f4d6036246e0a8819e350ac7b4e224bc151e7
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2019-06-26 11:31:08 -0700

    LPS-96987 Seperate the create and populate into 2 statements to work with DB2 and SQLServer

--- portal-impl/src/com/liferay/portal/upgrade/v7_2_x/UpgradeLayoutParentPlid.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_2_x/UpgradeLayoutParentPlid.java
@@ -24,11 +24,14 @@ public class UpgradeLayoutParentPlid extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
+
+		runSQL("create table TEMP (plid integer, parentPlid integer)");
+
 		String sql = StringBundler.concat(
-			"create table TEMP as (select l1.plid as plid, l2.plid as ",
-			"parentPlid from Layout l1 left join Layout l2 on l1.groupId = ",
-			"l2.groupId and l1.privateLayout = l2.privateLayout and ",
-			"l1.parentLayoutId = l2.layoutId)");
+			"insert into TEMP select l1.plid as plid, l2.plid as parentPlid from " +
+			"Layout l1 left join Layout l2 on l1.groupId = l2.groupId and " +
+			"l1.privateLayout = l2.privateLayout and " +
+			"l1.parentLayoutId = l2.layoutId");
 
 		runSQL(sql);
 
