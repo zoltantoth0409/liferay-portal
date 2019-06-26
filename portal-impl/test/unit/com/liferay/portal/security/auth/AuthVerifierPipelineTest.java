cd03cbbc62b61f32bc3c02102fc19b3c18a1cc0a
Carlos Sierra <carlos.sierra@liferay.com>
2019-06-26 09:26:30 +0200

    LPS-97129 Mock UserLocalServiceUtil

---
+++ portal-impl/test/unit/com/liferay/portal/security/auth/AuthVerifierPipelineTest.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.AccessControlContext;
+import com.liferay.portal.kernel.security.auth.verifier.AuthVerifier;
+import com.liferay.portal.kernel.security.auth.verifier.AuthVerifierResult;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.registry.BasicRegistryImpl;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceRegistration;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Objects;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockServletContext;
+
+/**
+ * @author Peter Fellwock
+ */
+@PrepareForTest(UserLocalServiceUtil.class)
+@RunWith(PowerMockRunner.class)
+public class AuthVerifierPipelineTest {
+
+	@Test
+	public void testVerifyRequest() throws PortalException {
+		RegistryUtil.setRegistry(new BasicRegistryImpl());
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		User user = Mockito.mock(User.class);
+
+		Mockito.when(
+			user.isActive()
+		).thenReturn(
+			true
+		);
+
+		PowerMockito.mockStatic(UserLocalServiceUtil.class);
+
+		Mockito.when(
+			UserLocalServiceUtil.fetchUser(Mockito.anyLong())
+		).thenReturn(
+			user
+		);
+
+		AuthVerifierResult authVerifierResult = new AuthVerifierResult();
+
+		authVerifierResult.setSettings(new HashMap<>());
+		authVerifierResult.setState(AuthVerifierResult.State.SUCCESS);
+
+		ServiceRegistration<AuthVerifier> serviceRegistration =
+			registry.registerService(
+				AuthVerifier.class,
+				(AuthVerifier)ProxyUtil.newProxyInstance(
+					AuthVerifier.class.getClassLoader(),
+					new Class<?>[] {AuthVerifier.class},
+					(proxy, method, args) -> {
+						if (Objects.equals(method.getName(), "verify")) {
+							return authVerifierResult;
+						}
+
+						return null;
+					}),
+				Collections.singletonMap("urls.includes", _BASE_URL + "/*"));
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest(new MockServletContext());
+
+		mockHttpServletRequest.setRequestURI(_BASE_URL + "/Hello");
+
+		AccessControlContext accessControlContext = new AccessControlContext();
+
+		accessControlContext.setRequest(mockHttpServletRequest);
+
+		try {
+			Assert.assertSame(
+				authVerifierResult,
+				AuthVerifierPipeline.verifyRequest(accessControlContext));
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
+	}
+
+	private static final String _BASE_URL = "/TestAuthVerifier";
+
+}
