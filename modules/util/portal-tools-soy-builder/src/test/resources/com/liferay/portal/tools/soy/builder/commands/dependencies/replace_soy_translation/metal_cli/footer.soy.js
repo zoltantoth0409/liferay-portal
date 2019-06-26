fe9bf5c1570cbefeacfd2c87d5bf7835d8312cd9
Peter Shin <peter.shin@liferay.com>
2019-06-26 16:42:50 -0700

    Revert "LPS-96376 Update to liferay-npm-scripts v2.1.0 (prettier)"

--- modules/util/portal-tools-soy-builder/src/test/resources/com/liferay/portal/tools/soy/builder/commands/dependencies/replace_soy_translation/metal_cli/footer.soy.js
+++ modules/util/portal-tools-soy-builder/src/test/resources/com/liferay/portal/tools/soy/builder/commands/dependencies/replace_soy_translation/metal_cli/footer.soy.js
@@ -1,151 +1,124 @@
-Liferay.Loader.define(
-	'hello-soy-web@1.0.4/Footer.soy',
-	['exports', 'metal-component/src/Component', 'metal-soy/src/Soy'],
-	function(exports, _Component2, _Soy) {
-		'use strict';
+Liferay.Loader.define("hello-soy-web@1.0.4/Footer.soy", ['exports', 'metal-component/src/Component', 'metal-soy/src/Soy'], function (exports, _Component2, _Soy) {
+  'use strict';
 
-		Object.defineProperty(exports, '__esModule', {
-			value: true
-		});
-		exports.templates = exports.Footer = undefined;
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.templates = exports.Footer = undefined;
 
-		var _Component3 = _interopRequireDefault(_Component2);
+  var _Component3 = _interopRequireDefault(_Component2);
 
-		var _Soy2 = _interopRequireDefault(_Soy);
+  var _Soy2 = _interopRequireDefault(_Soy);
 
-		function _interopRequireDefault(obj) {
-			return obj && obj.__esModule
-				? obj
-				: {
-						default: obj
-				  };
-		}
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
 
-		function _classCallCheck(instance, Constructor) {
-			if (!(instance instanceof Constructor)) {
-				throw new TypeError('Cannot call a class as a function');
-			}
-		}
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
 
-		function _possibleConstructorReturn(self, call) {
-			if (!self) {
-				throw new ReferenceError(
-					"this hasn't been initialised - super() hasn't been called"
-				);
-			}
+  function _possibleConstructorReturn(self, call) {
+    if (!self) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
 
-			return call &&
-				(typeof call === 'object' || typeof call === 'function')
-				? call
-				: self;
-		}
+    return call && (typeof call === "object" || typeof call === "function") ? call : self;
+  }
 
-		function _inherits(subClass, superClass) {
-			if (typeof superClass !== 'function' && superClass !== null) {
-				throw new TypeError(
-					'Super expression must either be null or a function, not ' +
-						typeof superClass
-				);
-			}
+  function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+    }
 
-			subClass.prototype = Object.create(
-				superClass && superClass.prototype,
-				{
-					constructor: {
-						value: subClass,
-						enumerable: false,
-						writable: true,
-						configurable: true
-					}
-				}
-			);
-			if (superClass)
-				Object.setPrototypeOf
-					? Object.setPrototypeOf(subClass, superClass)
-					: (subClass.__proto__ = superClass);
-		}
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value: subClass,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+  }
 
-		var templates;
-		goog.loadModule(function(exports) {
-			// This file was automatically generated from Footer.soy.
-			// Please don't edit this file by hand.
+  var templates;
+  goog.loadModule(function (exports) {
 
-			/**
-			 * @fileoverview Templates in namespace Footer.
-			 * @public
-			 */
+    // This file was automatically generated from Footer.soy.
+    // Please don't edit this file by hand.
 
-			goog.module('Footer.incrementaldom');
+    /**
+     * @fileoverview Templates in namespace Footer.
+     * @public
+     */
 
-			/** @suppress {extraRequire} */
-			var soy = goog.require('soy');
-			/** @suppress {extraRequire} */
-			var soydata = goog.require('soydata');
-			/** @suppress {extraRequire} */
-			goog.require('goog.i18n.bidi');
-			/** @suppress {extraRequire} */
-			goog.require('goog.asserts');
-			var IncrementalDom = goog.require('incrementaldom');
-			var ie_open = IncrementalDom.elementOpen;
-			var ie_close = IncrementalDom.elementClose;
-			var ie_void = IncrementalDom.elementVoid;
-			var ie_open_start = IncrementalDom.elementOpenStart;
-			var ie_open_end = IncrementalDom.elementOpenEnd;
-			var itext = IncrementalDom.text;
-			var iattr = IncrementalDom.attr;
+    goog.module('Footer.incrementaldom');
 
-			/**
-			 * @param {Object<string, *>=} opt_data
-			 * @param {(null|undefined)=} opt_ignored
-			 * @param {Object<string, *>=} opt_ijData
-			 * @return {void}
-			 * @suppress {checkTypes}
-			 */
-			function $render(opt_data, opt_ignored, opt_ijData) {
-				/** @desc  */
-				var MSG_EXTERNAL_2986127772961742460 = goog.getMsg(
-					'this-portlet-was-written-using-soy-templates'
-				);
-				var msg_s136 = MSG_EXTERNAL_2986127772961742460;
-				ie_open('blockquote', null, null, 'class', 'blockquote-xs');
-				ie_open('small');
-				itext(msg_s136);
-				ie_close('small');
-				ie_close('blockquote');
-			}
-			exports.render = $render;
-			if (goog.DEBUG) {
-				$render.soyTemplateName = 'Footer.render';
-			}
+    /** @suppress {extraRequire} */
+    var soy = goog.require('soy');
+    /** @suppress {extraRequire} */
+    var soydata = goog.require('soydata');
+    /** @suppress {extraRequire} */
+    goog.require('goog.i18n.bidi');
+    /** @suppress {extraRequire} */
+    goog.require('goog.asserts');
+    var IncrementalDom = goog.require('incrementaldom');
+    var ie_open = IncrementalDom.elementOpen;
+    var ie_close = IncrementalDom.elementClose;
+    var ie_void = IncrementalDom.elementVoid;
+    var ie_open_start = IncrementalDom.elementOpenStart;
+    var ie_open_end = IncrementalDom.elementOpenEnd;
+    var itext = IncrementalDom.text;
+    var iattr = IncrementalDom.attr;
 
-			exports.render.params = [];
-			exports.render.types = {};
-			exports.templates = templates = exports;
-			return exports;
-		});
+    /**
+     * @param {Object<string, *>=} opt_data
+     * @param {(null|undefined)=} opt_ignored
+     * @param {Object<string, *>=} opt_ijData
+     * @return {void}
+     * @suppress {checkTypes}
+     */
+    function $render(opt_data, opt_ignored, opt_ijData) {
+      /** @desc  */
+      var MSG_EXTERNAL_2986127772961742460 = goog.getMsg('this-portlet-was-written-using-soy-templates');
+      var msg_s136 = MSG_EXTERNAL_2986127772961742460;
+      ie_open('blockquote', null, null, 'class', 'blockquote-xs');
+      ie_open('small');
+      itext(msg_s136);
+      ie_close('small');
+      ie_close('blockquote');
+    }
+    exports.render = $render;
+    if (goog.DEBUG) {
+      $render.soyTemplateName = 'Footer.render';
+    }
 
-		var Footer = (function(_Component) {
-			_inherits(Footer, _Component);
+    exports.render.params = [];
+    exports.render.types = {};
+    exports.templates = templates = exports;
+    return exports;
+  });
 
-			function Footer() {
-				_classCallCheck(this, Footer);
+  var Footer = function (_Component) {
+    _inherits(Footer, _Component);
 
-				return _possibleConstructorReturn(
-					this,
-					(Footer.__proto__ || Object.getPrototypeOf(Footer)).apply(
-						this,
-						arguments
-					)
-				);
-			}
+    function Footer() {
+      _classCallCheck(this, Footer);
 
-			return Footer;
-		})(_Component3.default);
+      return _possibleConstructorReturn(this, (Footer.__proto__ || Object.getPrototypeOf(Footer)).apply(this, arguments));
+    }
 
-		_Soy2.default.register(Footer, templates);
-		exports.Footer = Footer;
-		exports.templates = templates;
-		exports.default = templates;
-	}
-);
+    return Footer;
+  }(_Component3.default);
+
+  _Soy2.default.register(Footer, templates);
+  exports.Footer = Footer;
+  exports.templates = templates;
+  exports.default = templates;
+});
 //# sourceMappingURL=Footer.soy.js.map
