33eaae27f9d70c6d8a6c1b4393ec3ea425828ef5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-06-26 10:51:11 +0200

    LPS-96702 Uses groupId and plid instead of the json array

--- modules/apps/layout/layout-type-controller/layout-type-controller-display-page/src/main/java/com/liferay/layout/type/controller/display/page/internal/display/context/DisplayPageLayoutTypeControllerDisplayContext.java
+++ modules/apps/layout/layout-type-controller/layout-type-controller-display-page/src/main/java/com/liferay/layout/type/controller/display/page/internal/display/context/DisplayPageLayoutTypeControllerDisplayContext.java
@@ -26,22 +26,12 @@ import com.liferay.info.display.contributor.InfoDisplayContributorTracker;
 import com.liferay.info.display.contributor.InfoDisplayObjectProvider;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorWebKeys;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
-import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
-import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalServiceUtil;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryServiceUtil;
-import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.segments.constants.SegmentsConstants;
-import com.liferay.segments.constants.SegmentsWebKeys;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -171,57 +161,10 @@ public class DisplayPageLayoutTypeControllerDisplayContext {
 		return 0;
 	}
 
-	public long[] getSegmentExperienceIds() {
-		return GetterUtil.getLongValues(
-			_httpServletRequest.getAttribute(
-				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS),
-			new long[] {SegmentsConstants.SEGMENTS_EXPERIENCE_ID_DEFAULT});
-	}
-
-	public JSONArray getStructureJSONArray() throws PortalException {
-		if (_structureJSONArray != null) {
-			return _structureJSONArray;
-		}
-
-		InfoDisplayObjectProvider infoDisplayObjectProvider =
-			getInfoDisplayObjectProvider();
-
-		if (infoDisplayObjectProvider == null) {
-			return null;
-		}
-
-		long[] segmentsExperienceIds = getSegmentExperienceIds();
-
-		LayoutPageTemplateEntry layoutPageTemplateEntry =
-			LayoutPageTemplateEntryLocalServiceUtil.getLayoutPageTemplateEntry(
-				getLayoutPageTemplateEntryId());
-
-		LayoutPageTemplateStructure layoutPageTemplateStructure =
-			LayoutPageTemplateStructureLocalServiceUtil.
-				fetchLayoutPageTemplateStructure(
-					infoDisplayObjectProvider.getGroupId(),
-					PortalUtil.getClassNameId(Layout.class.getName()),
-					layoutPageTemplateEntry.getPlid(), true);
-
-		String data = layoutPageTemplateStructure.getData(
-			segmentsExperienceIds);
-
-		if (Validator.isNull(data)) {
-			return null;
-		}
-
-		JSONObject dataJSONObject = JSONFactoryUtil.createJSONObject(data);
-
-		_structureJSONArray = dataJSONObject.getJSONArray("structure");
-
-		return _structureJSONArray;
-	}
-
 	private final HttpServletRequest _httpServletRequest;
 	private final InfoDisplayContributor _infoDisplayContributor;
 	private Map<Long, Map<String, Object>> _infoDisplayFieldsValuesMap =
 		new HashMap<>();
 	private final InfoDisplayObjectProvider _infoDisplayObjectProvider;
-	private JSONArray _structureJSONArray;
 
 }
