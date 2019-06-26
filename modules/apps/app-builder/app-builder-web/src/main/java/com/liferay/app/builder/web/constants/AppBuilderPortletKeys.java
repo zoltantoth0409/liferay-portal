42ab4f275713f4a4130ce091bd4b6f81a80ac7dc
Brian Chan <brian.chan@liferay.com>
2019-06-26 14:18:50 -0700

    LPS-95949 match actual class

--- modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/constants/AppBuilderPortletKeys.java
+++ modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/constants/AppBuilderPortletKeys.java
@@ -20,7 +20,7 @@ package com.liferay.app.builder.web.constants;
 public class AppBuilderPortletKeys {
 
 	public static final String APP_BUILDER =
-		"com_liferay_app_builder_web_portlet_AppBuilderPortlet";
+		"com_liferay_app_builder_web_internal_portlet_AppBuilderPortlet";
 
 	public static final String CONTROL_PANEL_APP_BUILDER =
 		"control_panel.app_builder";
