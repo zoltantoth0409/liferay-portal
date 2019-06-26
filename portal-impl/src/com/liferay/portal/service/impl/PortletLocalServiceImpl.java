801338a5a2fbf67e898fb6c54667d3c15ebddadd
Brian Chan <brian.chan@liferay.com>
2019-06-26 11:23:15 -0700

    LPS-97175 sort

--- portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -578,8 +578,8 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 			}
 
 			portlet.setActive(true);
-			portlet.setUndeployedPortlet(true);
 			portlet.setReady(true);
+			portlet.setUndeployedPortlet(true);
 		}
 
 		return portlet;
