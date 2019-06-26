6a624e86d9926cd3bf50fbb903677784b70f0b85
shuyangzhou <shuyang.zhou@liferay.com>
2019-06-25 19:10:20 -0700

    LPS-97175 UndeployedPortlet should always be ready.

--- portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/PortletLocalServiceImpl.java
@@ -579,6 +579,7 @@ public class PortletLocalServiceImpl extends PortletLocalServiceBaseImpl {
 
 			portlet.setActive(true);
 			portlet.setUndeployedPortlet(true);
+			portlet.setReady(true);
 		}
 
 		return portlet;
