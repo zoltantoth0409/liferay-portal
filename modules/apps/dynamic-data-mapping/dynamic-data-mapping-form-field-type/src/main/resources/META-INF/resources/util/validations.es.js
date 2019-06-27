8287d35c0d2975d7032b1c6b4c4c701ab04016df
Bruno Basto <bruno.basto@liferay.com>
2019-06-27 15:50:08 -0300

    LPS-97490 General SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/util/validations.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/util/validations.es.js
@@ -20,35 +20,35 @@ export const VALIDATIONS = {
 			label: Liferay.Language.get('is-greater-than-or-equal-to'),
 			name: 'lt',
 			parameterMessage: Liferay.Language.get('number-placeholder'),
-			regex: /^(.+)\<(\d+\.?\d*)?$/,
+			regex: /^(.+)<(\d+\.?\d*)?$/,
 			template: '{name}<{parameter}'
 		},
 		{
 			label: Liferay.Language.get('is-greater-than'),
 			name: 'lteq',
 			parameterMessage: Liferay.Language.get('number-placeholder'),
-			regex: /^(.+)\<\=(\d+\.?\d*)?$/,
+			regex: /^(.+)<=(\d+\.?\d*)?$/,
 			template: '{name}<={parameter}'
 		},
 		{
 			label: Liferay.Language.get('is-not-equal-to'),
 			name: 'eq',
 			parameterMessage: Liferay.Language.get('number-placeholder'),
-			regex: /^(.+)\=\=(\d+\.?\d*)?$/,
+			regex: /^(.+)==(\d+\.?\d*)?$/,
 			template: '{name}=={parameter}'
 		},
 		{
 			label: Liferay.Language.get('is-less-than-or-equal-to'),
 			name: 'gt',
 			parameterMessage: Liferay.Language.get('number-placeholder'),
-			regex: /^(.+)\>(\d+\.?\d*)?$/,
+			regex: /^(.+)>(\d+\.?\d*)?$/,
 			template: '{name}>{parameter}'
 		},
 		{
 			label: Liferay.Language.get('is-less-than'),
 			name: 'gteq',
 			parameterMessage: Liferay.Language.get('number-placeholder'),
-			regex: /^(.+)\>\=(\d+\.?\d*)?$/,
+			regex: /^(.+)>=(\d+\.?\d*)?$/,
 			template: '{name}>={parameter}'
 		}
 	],
