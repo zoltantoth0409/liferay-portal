7f6482b97641b9413f9eb905a583f6fc33133768
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-06-27 12:59:54 +0200

    LPS-97534 Indicating the scope of auto tagging

--- modules/apps/document-library/document-library-asset-auto-tagger-tensorflow/src/main/java/com/liferay/document/library/asset/auto/tagger/tensorflow/internal/configuration/TensorFlowImageAssetAutoTagProviderCompanyConfiguration.java
+++ modules/apps/document-library/document-library-asset-auto-tagger-tensorflow/src/main/java/com/liferay/document/library/asset/auto/tagger/tensorflow/internal/configuration/TensorFlowImageAssetAutoTagProviderCompanyConfiguration.java
@@ -35,7 +35,10 @@ public interface TensorFlowImageAssetAutoTagProviderCompanyConfiguration {
 	/**
 	 * Enables auto tagging of images using a pre-trained tensorflow model.
 	 */
-	@Meta.AD(deflt = "false", name = "enabled", required = false)
+	@Meta.AD(
+		deflt = "false", description = "enabled-description", name = "enabled",
+		required = false
+	)
 	public boolean enabled();
 
 	/**
