1a70f6d55802d44cff8fcde96040b23fddf0c4a8
Marko Čikoš <marko.cikos@liferay.com>
2019-06-28 10:38:53 +0200

    LPS-96778 frontend-js-web - Tests for the new utility

---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/object_to_url_search_params.es.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+'use strict';
+
+import objectToURLSearchParams from '../../../src/main/resources/META-INF/resources/liferay/util/object_to_url_search_params.es';
+
+describe('Liferay.Util.objectToURLSearchParams', () => {
+	it('throws error if obj parameter is not an object', () => {
+		expect(() => objectToURLSearchParams('abc')).toThrow(
+			'must be an object'
+		);
+	});
+
+	it('converts given object into URLSearchParams', () => {
+		const obj = {
+			key1: 'value1',
+			key2: 123
+		};
+
+		const urlSearchParams = objectToURLSearchParams(obj);
+
+		expect(urlSearchParams.constructor.name).toEqual('URLSearchParams');
+
+		expect(urlSearchParams.get('key1')).toEqual('value1');
+		expect(urlSearchParams.get('key2')).toEqual('123');
+	});
+});
