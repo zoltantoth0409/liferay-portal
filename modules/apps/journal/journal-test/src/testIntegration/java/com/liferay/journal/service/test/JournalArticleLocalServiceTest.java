f203eb55b860260fed9d2dde5d59905ebbb78c92
jpince <jason.pince@liferay.com>
2019-06-28 13:41:45 -0700

    Revert "LPS-97269 Uses new method"
    This reverts commit ee2930c3ace72b06899e9a15f36b1759337f8d3a.

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/service/test/JournalArticleLocalServiceTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/service/test/JournalArticleLocalServiceTest.java
@@ -32,6 +32,7 @@ import com.liferay.journal.model.JournalFolderConstants;
 import com.liferay.journal.service.JournalArticleLocalServiceUtil;
 import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.ResourcePermission;
@@ -260,12 +261,15 @@ public class JournalArticleLocalServiceTest {
 		titleMap.put(LocaleUtil.US, "Test Article");
 
 		JournalArticle journalArticle =
-			JournalArticleLocalServiceUtil.addArticleDefaultValues(
+			JournalArticleLocalServiceUtil.addArticle(
 				serviceContext.getUserId(), serviceContext.getScopeGroupId(),
+				JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
 				ClassNameLocalServiceUtil.getClassNameId(DDMStructure.class),
-				ddmStructure.getStructureId(), titleMap, null, content,
-				ddmStructure.getStructureKey(), ddmTemplate.getTemplateKey(),
-				null, true, false, null, null, serviceContext);
+				ddmStructure.getStructureId(), StringPool.BLANK, true, 0,
+				titleMap, null, content, ddmStructure.getStructureKey(),
+				ddmTemplate.getTemplateKey(), null, 1, 1, 1965, 0, 0, 0, 0, 0,
+				0, 0, true, 0, 0, 0, 0, 0, true, true, false, null, null, null,
+				null, serviceContext);
 
 		DDMStructure actualDDMStrucure = journalArticle.getDDMStructure();
 
