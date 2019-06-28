9fc31e5ba1c309eef9a89cadb3e7e8add423bf5a
Zoltán Császi <zoltan.csaszi@liferay.com>
2019-06-28 10:28:56 +0200

    LPS-96583 getting a specific entry rest query implemented

--- modules/apps/change-tracking/change-tracking-rest-impl/src/main/java/com/liferay/change/tracking/rest/internal/resource/v1_0/EntryResourceImpl.java
+++ modules/apps/change-tracking/change-tracking-rest-impl/src/main/java/com/liferay/change/tracking/rest/internal/resource/v1_0/EntryResourceImpl.java
@@ -14,9 +14,16 @@
 
 package com.liferay.change.tracking.rest.internal.resource.v1_0;
 
+import com.liferay.change.tracking.definition.CTDefinitionRegistryUtil;
+import com.liferay.change.tracking.model.CTEntry;
+import com.liferay.change.tracking.rest.dto.v1_0.Entry;
 import com.liferay.change.tracking.rest.resource.v1_0.EntryResource;
+import com.liferay.change.tracking.service.CTEntryLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryDefinition;
+import com.liferay.portal.kernel.util.GetterUtil;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +34,56 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = EntryResource.class
 )
 public class EntryResourceImpl extends BaseEntryResourceImpl {
+
+	@Override
+	public Entry getEntry(Long entryId) throws Exception {
+		CTEntry ctEntry = _ctEntryLocalService.getCTEntry(
+			GetterUtil.getLong(entryId));
+
+		if (ctEntry == null) {
+			return new Entry();
+		}
+
+		return _toEntry(ctEntry);
+	}
+
+	private Entry _toEntry(CTEntry ctEntry) {
+		return new Entry() {
+			{
+				affectedByEntriesCount =
+					_ctEntryLocalService.getRelatedOwnerCTEntriesCount(
+						ctEntry.getCtEntryId(), new QueryDefinition<>());
+				changeType = ctEntry.getChangeType();
+				classNameId = ctEntry.getModelClassNameId();
+				classPK = ctEntry.getModelClassPK();
+				collision = ctEntry.isCollision();
+
+				contentType =
+					CTDefinitionRegistryUtil.
+						getVersionEntityContentTypeLanguageKey(
+							ctEntry.getModelClassNameId());
+
+				dateModified = ctEntry.getModifiedDate();
+				entryId = ctEntry.getCtEntryId();
+				key = ctEntry.getModelResourcePrimKey();
+
+				siteName = CTDefinitionRegistryUtil.getVersionEntitySiteName(
+					ctEntry.getModelClassNameId(), ctEntry.getModelClassPK());
+
+				title = CTDefinitionRegistryUtil.getVersionEntityTitle(
+					ctEntry.getModelClassNameId(), ctEntry.getModelClassPK());
+
+				userName = ctEntry.getUserName();
+
+				version = String.valueOf(
+					CTDefinitionRegistryUtil.getVersionEntityVersion(
+						ctEntry.getModelClassNameId(),
+						ctEntry.getModelClassPK()));
+			}
+		};
+	}
+
+	@Reference
+	private CTEntryLocalService _ctEntryLocalService;
+
 }
