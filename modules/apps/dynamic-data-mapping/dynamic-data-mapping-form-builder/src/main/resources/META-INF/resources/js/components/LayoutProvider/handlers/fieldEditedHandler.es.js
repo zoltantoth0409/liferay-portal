feb064ad5a7aa61a6f5e30a10e53c3937bfeecfd
Bruno Basto <bruno.basto@liferay.com>
2019-06-28 21:22:29 -0300

    LPS-97572 Detect changed option value and update referenced rules

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/handlers/fieldEditedHandler.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/handlers/fieldEditedHandler.es.js
@@ -14,19 +14,12 @@
 
 import * as FormSupport from 'dynamic-data-mapping-form-renderer/js/components/FormRenderer/FormSupport.es';
 import {updateFocusedField} from '../util/focusedField.es';
-import {updateRulesFieldName} from '../util/rules.es';
+import {updateRulesReferences} from '../util/rules.es';
 
 export const updatePages = (pages, oldFieldProperties, newFieldProperties) => {
 	const {fieldName} = oldFieldProperties;
 
 	return FormSupport.updateField(pages, fieldName, newFieldProperties);
-};
-
-export const updateRules = (rules, oldFieldProperties, newFieldProperties) => {
-	const {fieldName} = oldFieldProperties;
-	const newFieldName = newFieldProperties.fieldName;
-
-	return updateRulesFieldName(rules, fieldName, newFieldName);
 };
 
 export const updateField = (
@@ -48,7 +41,7 @@ export const updateField = (
 	return {
 		focusedField: updatedFocusedField,
 		pages: updatePages(pages, focusedField, updatedFocusedField),
-		rules: updateRules(rules, focusedField, updatedFocusedField)
+		rules: updateRulesReferences(rules, focusedField, updateFocusedField)
 	};
 };
 
