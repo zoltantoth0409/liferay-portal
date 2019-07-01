801ad99c3a8acfb4700cdab96bb8b12558e6fb9b
Greg Hurrell <greg.hurrell@liferay.com>
2019-07-01 12:44:57 +0200

    LPS-97643 Fix lints

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/ItemSelectorDialog.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/ItemSelectorDialog.es.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 import Component from 'metal-component';
 import {Config} from 'metal-state';
 
@@ -68,9 +82,9 @@ class ItemSelectorDialog extends Component {
 							}
 						}
 					],
-					zIndex: zIndex
+					zIndex
 				},
-				eventName: eventName,
+				eventName,
 				id: eventName,
 				stack: !zIndex,
 				title: this.title,
@@ -113,7 +127,6 @@ class ItemSelectorDialog extends Component {
  */
 
 ItemSelectorDialog.STATE = {
-
 	/**
 	 * Label for the Add button
 	 *
