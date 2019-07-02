c5e6b760d9dd7c8ac86714e063daceb00aefd56f
Renato Rego <renato.rego@liferay.com>
2019-07-02 14:51:54 -0300

    LPS-96983 Update the ID of the imported Data Providers referenced by Text fields.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/DDMStructureStagedModelDataHandler.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/exportimport/data/handler/DDMStructureStagedModelDataHandler.java
@@ -642,7 +642,9 @@ public class DDMStructureStagedModelDataHandler
 		for (DDMFormField ddmFormField : ddmFormFields) {
 			String ddmFormFieldType = ddmFormField.getType();
 
-			if (!ddmFormFieldType.equals(DDMFormFieldType.SELECT)) {
+			if (!ddmFormFieldType.equals(DDMFormFieldType.SELECT) &&
+				!ddmFormFieldType.equals(DDMFormFieldType.TEXT)) {
+
 				continue;
 			}
 
