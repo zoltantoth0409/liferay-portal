772501cb30906cd8e2012f440cdf86cff6eceeff
rubenpulido <ruben.pulido@liferay.com>
2019-07-03 13:42:21 +0200

    LPS-97758 Unit test for getConfigurationDefaultValuesJSONObject

---
+++ modules/apps/fragment/fragment-api/src/test/java/com/liferay/fragment/util/FragmentEntryConfigUtilTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.util;
+
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.util.FileImpl;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Rubén Pulido
+ */
+public class FragmentEntryConfigUtilTest {
+
+	@BeforeClass
+	public static void setUpClass() {
+		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
+
+		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
+
+		new FileUtil().setFile(new FileImpl());
+	}
+
+	@Test
+	public void testGetConfigurationDefaultValuesJSONObject() throws Exception {
+		JSONObject configurationDefaultValuesJSONObject =
+			FragmentEntryConfigUtil.getConfigurationDefaultValuesJSONObject(
+				_getFileContent("configuration.json"));
+
+		JSONObject expectedConfigurationDefaultValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_getFileContent("expected-configuration-default-values.json"));
+
+		Assert.assertEquals(
+			expectedConfigurationDefaultValuesJSONObject.toJSONString(),
+			configurationDefaultValuesJSONObject.toJSONString());
+	}
+
+	private String _getFileContent(String fileName) throws Exception {
+		return new String(
+			FileUtil.getBytes(getClass(), "dependencies/" + fileName));
+	}
+
+}
