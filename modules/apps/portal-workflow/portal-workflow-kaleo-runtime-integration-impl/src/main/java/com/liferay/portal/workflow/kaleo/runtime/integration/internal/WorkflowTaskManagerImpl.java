46fe0a310b0d4a3e404ce6ce60f33ff3a06a8cfc
IstvanD <istvan.dezsi@liferay.com>
2019-07-03 14:53:29 +0200

    LPS-97748 Add only active users

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowTaskManagerImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowTaskManagerImpl.java
@@ -968,7 +968,9 @@ public class WorkflowTaskManagerImpl implements WorkflowTaskManager {
 			for (UserGroupRole userGroupRole : userGroupRoles) {
 				User user = userGroupRole.getUser();
 
-				users.add(user);
+				if (user.isActive()) {
+					users.add(user);
+				}
 			}
 
 			List<UserGroupGroupRole> userGroupGroupRoles =
