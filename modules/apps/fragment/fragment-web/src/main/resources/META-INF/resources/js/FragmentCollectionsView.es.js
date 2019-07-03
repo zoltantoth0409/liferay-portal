dddac419f93c82045a95f5fb1a3e62423cbfb388
Pablo Molina <contact@pablomolina.me>
2019-07-03 13:36:51 +0200

    LPS-96711 Use new ItemSelector in FragmentCollectionsView

---
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/FragmentCollectionsView.es.js
@@ -0,0 +1,210 @@
+import {ItemSelectorDialog, PortletBase} from 'frontend-js-web';
+import {Config} from 'metal-state';
+
+/**
+ * @class FragmentCollectionsView
+ */
+class FragmentCollectionsView extends PortletBase {
+	/**
+	 * @inheritdoc
+	 * @review
+	 */
+	created() {
+		this._fragmentCollectionsFm =
+			document[this.ns('fragmentCollectionsFm')];
+		this._handleComponentReady = this._handleComponentReady.bind(this);
+
+		Liferay.componentReady(this.ns('actionsComponent')).then(
+			this._handleComponentReady
+		);
+
+		Liferay.componentReady(this.ns('emptyResultMessageComponent')).then(
+			this._handleComponentReady
+		);
+	}
+
+	/**
+	 * Handles a componentReady event and adds a click handler to the
+	 * given fragment.
+	 * @param {{ on: function }} component
+	 * @private
+	 * @review
+	 */
+	_handleComponentReady(component) {
+		const ACTIONS = {
+			deleteCollections: this._deleteCollections,
+			exportCollections: this._exportCollections,
+			openImportView: this._openImportView
+		};
+
+		component.on(['click', 'itemClicked'], (event, facade) => {
+			let itemData;
+
+			if (event.data && event.data.item) {
+				itemData = event.data.item.data;
+			} else if (!event.data && facade && facade.target) {
+				itemData = facade.target.data;
+			}
+
+			if (itemData && itemData.action && ACTIONS[itemData.action]) {
+				ACTIONS[itemData.action].call(this);
+			}
+		});
+	}
+
+	/**
+	 * Opens an item selector to remove selected collections
+	 * @private
+	 * @review
+	 */
+	_deleteCollections() {
+		this._openFragmentCollectionsItemSelector(
+			Liferay.Language.get('delete'),
+			Liferay.Language.get('delete-collection'),
+			selectedItems => {
+				if (
+					confirm(
+						Liferay.Language.get(
+							'are-you-sure-you-want-to-delete-the-selected-entries'
+						)
+					)
+				) {
+					selectedItems.forEach(item => {
+						this._fragmentCollectionsFm.appendChild(item);
+					});
+				}
+
+				submitForm(
+					this._fragmentCollectionsFm,
+					this.deleteFragmentCollectionFormAction
+				);
+			}
+		);
+	}
+
+	/**
+	 * Opens an item selector to export selected collections
+	 * @private
+	 * @review
+	 */
+	_exportCollections() {
+		this._openFragmentCollectionsItemSelector(
+			Liferay.Language.get('export'),
+			Liferay.Language.get('export-collection'),
+			selectedItems => {
+				selectedItems.forEach(item => {
+					this._fragmentCollectionsFm.append(item);
+				});
+
+				submitForm(
+					this._fragmentCollectionsFm,
+					this.exportFragmentCollectionFormAction
+				);
+			}
+		);
+	}
+
+	/**
+	 * Opens an item selector to select some fragment collections.
+	 * @param {string} dialogButtonLabel
+	 * @param {string} dialogTitle
+	 * @param {function} callback Callback executed when some items have been
+	 *  selected. They will be sent as parameters to this callback
+	 * @private
+	 * @review
+	 */
+	_openFragmentCollectionsItemSelector(
+		dialogButtonLabel,
+		dialogTitle,
+		callback
+	) {
+		const itemSelectorDialog = new ItemSelectorDialog({
+			buttonAddLabel: dialogButtonLabel,
+			eventName: this.ns('selectCollections'),
+			title: dialogTitle,
+			url: this.viewFragmentCollectionsUrl
+		});
+
+		itemSelectorDialog.on('selectedItemChange', event => {
+			if (event.selectedItem) {
+				callback(event.selectedItem);
+			}
+		});
+
+		itemSelectorDialog.open();
+	}
+
+	/**
+	 * Opens an iframe dialog with an fragment collection import form
+	 * @private
+	 * @review
+	 */
+	_openImportView() {
+		Liferay.Util.openWindow({
+			dialog: {
+				after: {
+					destroy: () => {
+						window.location.reload();
+					}
+				},
+				destroyOnHide: true
+			},
+			dialogIframe: {
+				bodyCssClass: 'dialog-with-footer'
+			},
+			id: this.ns('openImportView'),
+			title: Liferay.Language.get('import'),
+			uri: this.viewImportCollectionsUrl
+		});
+	}
+}
+
+FragmentCollectionsView.STATE = {
+	/**
+	 * Collections form used for updating backend
+	 * @default undefined
+	 * @instance
+	 * @memberof FragmentCollectionsView
+	 * @review
+	 * @type {HTMLFormElement|undefined}
+	 */
+	_fragmentCollectionsFm: Config.instanceOf(HTMLFormElement).internal(),
+
+	/**
+	 * @default undefined
+	 * @instance
+	 * @memberof FragmentCollectionsView
+	 * @review
+	 * @type {string}
+	 */
+	deleteFragmentCollectionFormAction: Config.string().required(),
+
+	/**
+	 * @default undefined
+	 * @instance
+	 * @memberof FragmentCollectionsView
+	 * @review
+	 * @type {string}
+	 */
+	exportFragmentCollectionFormAction: Config.string().required(),
+
+	/**
+	 * @default undefined
+	 * @instance
+	 * @memberof FragmentCollectionsView
+	 * @review
+	 * @type {string}
+	 */
+	viewFragmentCollectionsUrl: Config.string().required(),
+
+	/**
+	 * @default undefined
+	 * @instance
+	 * @memberof FragmentCollectionsView
+	 * @review
+	 * @type {string}
+	 */
+	viewImportCollectionsUrl: Config.string().required()
+};
+
+export default FragmentCollectionsView;
