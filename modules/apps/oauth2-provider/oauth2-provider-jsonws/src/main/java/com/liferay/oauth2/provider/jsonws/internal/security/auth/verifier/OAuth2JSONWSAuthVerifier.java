99ec62e78c0eed85b79fb912a23f7922c295efba
Carlos Sierra <carlos.sierra@liferay.com>
2019-07-03 17:34:02 +0200

    OAUTH2-254 Not used

--- modules/apps/oauth2-provider/oauth2-provider-jsonws/src/main/java/com/liferay/oauth2/provider/jsonws/internal/security/auth/verifier/OAuth2JSONWSAuthVerifier.java
+++ modules/apps/oauth2-provider/oauth2-provider-jsonws/src/main/java/com/liferay/oauth2/provider/jsonws/internal/security/auth/verifier/OAuth2JSONWSAuthVerifier.java
@@ -95,7 +95,7 @@ public class OAuth2JSONWSAuthVerifier implements AuthVerifier {
 
 		try {
 			BearerTokenProvider.AccessToken accessToken = getAccessToken(
-				oAuth2Authorization, oAuth2AuthorizationOAuth2ScopeGrants);
+				oAuth2Authorization);
 
 			if (accessToken == null) {
 				return authVerifierResult;
@@ -174,8 +174,7 @@ public class OAuth2JSONWSAuthVerifier implements AuthVerifier {
 	}
 
 	protected BearerTokenProvider.AccessToken getAccessToken(
-			OAuth2Authorization oAuth2Authorization,
-			List<OAuth2ScopeGrant> oAuth2AuthorizationOAuth2ScopeGrants)
+			OAuth2Authorization oAuth2Authorization)
 		throws PortalException {
 
 		if (oAuth2Authorization == null) {
