229f023f4d3ea2f827d8b070724331fb6154576d
arthurchan35 <arthurchan35@gmail.com>
2019-07-04 12:49:40 +0800

    LPS-97073 No need to test rootPortletId anymore, full portlet id is always supplied

--- portal-impl/src/com/liferay/portal/model/impl/LayoutTypePortletImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutTypePortletImpl.java
@@ -1937,12 +1937,7 @@ public class LayoutTypePortletImpl
 		String[] columnValues = StringUtil.split(columnValue);
 
 		for (String nonstaticPortletId : columnValues) {
-			String decodedNonStaticPortletName =
-				PortletIdCodec.decodePortletName(nonstaticPortletId);
-
-			if (nonstaticPortletId.equals(portletId) ||
-				decodedNonStaticPortletName.equals(portletId)) {
-
+			if (nonstaticPortletId.equals(portletId)) {
 				return true;
 			}
 		}
@@ -1955,12 +1950,7 @@ public class LayoutTypePortletImpl
 			PropsKeys.LAYOUT_STATIC_PORTLETS_START + columnId);
 
 		for (String staticPortletId : staticPortletIdsStart) {
-			String decodedStaticPortletName = PortletIdCodec.decodePortletName(
-				staticPortletId);
-
-			if (staticPortletId.equals(portletId) ||
-				decodedStaticPortletName.equals(portletId)) {
-
+			if (staticPortletId.equals(portletId)) {
 				return true;
 			}
 		}
@@ -1969,12 +1959,7 @@ public class LayoutTypePortletImpl
 			PropsKeys.LAYOUT_STATIC_PORTLETS_END + columnId);
 
 		for (String staticPortletId : staticPortletIdsEnd) {
-			String decodedStaticPortletName = PortletIdCodec.decodePortletName(
-				staticPortletId);
-
-			if (staticPortletId.equals(portletId) ||
-				decodedStaticPortletName.equals(portletId)) {
-
+			if (staticPortletId.equals(portletId)) {
 				return true;
 			}
 		}
