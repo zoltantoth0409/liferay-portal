0f35f55fc507c10e280cc269425574e6370b04a8
Bruno Farache <bruno.farache@liferay.com>
2019-07-05 22:48:14 -0300

    LPS-97776 Replace page size selector with dropdown

--- modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/components/search-container/PageSize.es.js
+++ modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/components/search-container/PageSize.es.js
@@ -12,29 +12,54 @@
  * details.
  */
 
-import {ClaySelectWithOption} from '@clayui/select';
-import React, {Fragment} from 'react';
+import ClayIcon from '@clayui/icon';
+import ClayDropDown, {Align} from '@clayui/drop-down';
+import React, {Fragment, useState} from 'react';
 import {sub} from '../../utils/lang.es';
+
+const {Item, ItemList} = ClayDropDown;
 
 export default function PageSize(props) {
 	const {itemsCount, onPageSizeChange, page, pageSize, totalCount} = props;
+	const [active, setActive] = useState(false);
 
-	const options = [5, 10, 20, 30, 50, 75].map(entry => ({
-		label: `${entry} ${Liferay.Language.get('entries')}`,
-		selected: `${pageSize}` === `${entry}`,
-		value: `${entry}`
-	}));
+	const options = [5, 10, 20, 30, 50, 75];
 
 	const firstEntry = pageSize * (page - 1) + 1;
 	const lastEntry = firstEntry + itemsCount - 1;
 
 	return (
 		<Fragment>
-			<ClaySelectWithOption
-				id='pageSize'
-				onChange={event => onPageSizeChange(event.target.value)}
-				options={options}
-			/>
+			<ClayDropDown
+				active={active}
+				alignmentPosition={Align.RightCenter}
+				className='pagination-items-per-page'
+				onActiveChange={newVal => setActive(newVal)}
+				trigger={
+					<button className='page-link' type='button'>
+						{`${pageSize} Entries`}
+						<ClayIcon
+							spritemap={`${Liferay.ThemeDisplay.getPathThemeImages()}/lexicon/icons.svg`}
+							symbol='caret-double-l'
+						/>
+					</button>
+				}
+			>
+				<ItemList>
+					{options.map((option, index) => (
+						<Item
+							key={index}
+							onClick={() => {
+								setActive(false);
+								onPageSizeChange(option);
+							}}
+						>
+							{option}
+						</Item>
+					))}
+				</ItemList>
+			</ClayDropDown>
+
 			{sub(Liferay.Language.get('showing-x-to-x-of-x-entries'), [
 				firstEntry,
 				lastEntry,
