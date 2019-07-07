56f8fc327c809072fe683e01ad2f7016911d4ad6
Javier Gamarra <nhpatt@gmail.com>
2019-07-07 08:59:23 +0200

    LPS-97900 add indexer call in all operations

--- modules/apps/wiki/wiki-api/src/main/java/com/liferay/wiki/service/WikiNodeLocalService.java
+++ modules/apps/wiki/wiki-api/src/main/java/com/liferay/wiki/service/WikiNodeLocalService.java
@@ -71,6 +71,7 @@ public interface WikiNodeLocalService
 	public WikiNode addDefaultNode(long userId, ServiceContext serviceContext)
 		throws PortalException;
 
+	@Indexable(type = IndexableType.REINDEX)
 	public WikiNode addNode(
 			long userId, String name, String description,
 			ServiceContext serviceContext)
