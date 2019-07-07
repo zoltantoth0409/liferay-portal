825b822c004d208cd4b89bd0f1e9575fb2377cb4
Javier Gamarra <nhpatt@gmail.com>
2019-07-07 09:00:30 +0200

    LPS-97900 return sensible message when inserting duplicated node

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/DuplicateNodeNameExceptionExceptionMapper.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.jaxrs.exception.mapper;
+
+import com.liferay.wiki.exception.DuplicateNodeNameException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Converts any {@code FolderNameException} to a {@code 400} error.
+ *
+ * @author Javier Gamarra
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Delivery.DuplicateNodeNameExceptionExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class DuplicateNodeNameExceptionExceptionMapper
+	implements ExceptionMapper<DuplicateNodeNameException> {
+
+	@Override
+	public Response toResponse(DuplicateNodeNameException dnne) {
+		return Response.status(
+			409
+		).type(
+			MediaType.TEXT_PLAIN
+		).entity(
+			"A wiki node already exists with the name " + dnne.getMessage()
+		).build();
+	}
+
+}
