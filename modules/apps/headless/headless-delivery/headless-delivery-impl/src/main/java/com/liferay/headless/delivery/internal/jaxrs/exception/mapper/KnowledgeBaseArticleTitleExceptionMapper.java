1c97bff6e14f30fee0def3c7a5e86a7f25c86fdc
Javier Gamarra <nhpatt@gmail.com>
2019-07-09 16:35:50 +0200

    LPS-97971 use consistent naming

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/KnowledgeBaseArticleTitleExceptionMapper.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/KnowledgeBaseArticleTitleExceptionMapper.java
@@ -31,7 +31,7 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
 		"osgi.jaxrs.extension=true",
-		"osgi.jaxrs.name=Liferay.Headless.Delivery.KBArticleTitleException"
+		"osgi.jaxrs.name=Liferay.Headless.Delivery.KnowledgeBaseArticleTitleExceptionMapper"
 	},
 	service = ExceptionMapper.class
 )
