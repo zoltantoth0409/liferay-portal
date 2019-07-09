9a5479bc4c0e0d5685cbc161126a67c50e341a80
Alicia Garcia <alicia.garcia@liferay.com>
2019-07-09 11:38:15 +0200

    LPS-96762 UpgradeMVCCVersion Test

---
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/util/test/UpgradeMVCCVersionTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.util.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.test.util.DBAssertionUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeMVCCVersion;
+import com.liferay.portal.kernel.util.StreamUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.sql.DatabaseMetaData;
+
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alicia García
+ */
+@RunWith(Arquillian.class)
+public class UpgradeMVCCVersionTest extends UpgradeMVCCVersion {
+
+	@Before
+	public void setUp() throws Exception {
+		connection = DataAccess.getConnection();
+		runSQL(UpgradeMVCCVersionTestTableClass.TABLE_SQL_CREATE);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		runSQL(UpgradeMVCCVersionTestTableClass.TABLE_SQL_DROP);
+
+		connection.close();
+	}
+
+	@Test
+	public void testDoUpgrade() throws Exception {
+		doUpgrade();
+
+		DBAssertionUtil.assertColumns(
+			UpgradeMVCCVersionTestTableClass.TABLE_NAME, "_id", "_userId",
+			"mvccversion");
+	}
+
+	@Test
+	public void testUpgradeMVCCVersion() throws Exception {
+		DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+		upgradeMVCCVersion(
+			databaseMetaData, UpgradeMVCCVersionTestTableClass.TABLE_NAME);
+
+		DBAssertionUtil.assertColumns(
+			UpgradeMVCCVersionTestTableClass.TABLE_NAME, "_id", "_userId",
+			"mvccVersion");
+	}
+
+	protected void addResource(String path, JarOutputStream jarOutputStream)
+		throws IOException {
+
+		addResource(path, path, jarOutputStream);
+	}
+
+	protected void addResource(
+			String resourcePath, String path, JarOutputStream jarOutputStream)
+		throws IOException {
+
+		jarOutputStream.putNextEntry(new JarEntry(path));
+
+		try (InputStream inputStream =
+				UpgradeMVCCVersionTest.class.getResourceAsStream(
+					resourcePath)) {
+
+			StreamUtil.transfer(inputStream, jarOutputStream, false);
+		}
+
+		jarOutputStream.closeEntry();
+	}
+
+}
