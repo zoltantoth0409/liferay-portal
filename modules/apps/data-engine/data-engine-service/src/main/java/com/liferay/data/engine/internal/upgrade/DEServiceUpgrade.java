f301c33da553e19ac3b8d500ebcc725cd71c2141
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2019-07-10 17:32:15 -0300

    LPS-97932 Create upgrade process

---
+++ modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/internal/upgrade/DEServiceUpgrade.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.internal.upgrade;
+
+import com.liferay.data.engine.internal.upgrade.v1_0_0.UpgradeSchema;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jeyvison Nascimento
+ */
+@Component(
+	immediate = true,
+	service = {DEServiceUpgrade.class, UpgradeStepRegistrator.class}
+)
+public class DEServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register("1.0.0", "1.1.0", new UpgradeSchema());
+	}
+
+}
---
+++ modules/apps/data-engine/data-engine-service/src/main/resources/com/liferay/data/engine/internal/upgrade/v1_0_0/dependencies/update.sql
@@ -0,0 +1,39 @@
+create table DEDataDefinitionFieldLink (
+	uuid_ VARCHAR(75) null,
+	deDataDefinitionFieldLinkId LONG not null primary key,
+	groupId LONG,
+	classNameId LONG,
+	classPK LONG,
+	DDMStructureId LONG,
+	fieldName LONG
+);
+
+create unique index IX_2CEE588F on DEDataDefinitionFieldLink (classNameId, classPK, ddmStructureId, fieldName);
+create unique index IX_AAE65DF2 on DEDataDefinitionFieldLink (uuid_[$COLUMN_LENGTH:75$], groupId);
+
+create table DEDataListView (
+	uuid_ VARCHAR(75) null,
+	deDataListViewId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	ddmStructureId LONG,
+	name STRING null,
+	deDataRecordQueryId LONG
+);
+
+create index IX_FA1639C7 on DEDataListView (groupId, companyId, ddmStructureId);
+create index IX_7113A88 on DEDataListView (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_3336C30A on DEDataListView (uuid_[$COLUMN_LENGTH:75$], groupId);
+
+create table DEDataRecordQuery (
+	uuid_ VARCHAR(75) null,
+	deDataRecordQueryId LONG not null primary key,
+	appliedFilters VARCHAR(75) null,
+	fieldNames VARCHAR(75) null
+);
+
+create index IX_CC933E82 on DEDataRecordQuery (uuid_[$COLUMN_LENGTH:75$]);
