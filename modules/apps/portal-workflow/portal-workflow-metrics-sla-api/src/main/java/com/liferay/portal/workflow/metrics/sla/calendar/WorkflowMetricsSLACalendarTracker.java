4a81b9a39d1809b75a083c8ea969269435ec42d4
Brian Chan <brian.chan@liferay.com>
2019-07-10 13:30:11 -0700

    LPS-98003 prevent split package.. 2 modules should never export the same package

---
+++ modules/apps/portal-workflow/portal-workflow-metrics-sla-api/src/main/java/com/liferay/portal/workflow/metrics/sla/calendar/WorkflowMetricsSLACalendarTracker.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.sla.calendar;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Rafael Praxedes
+ */
+public interface WorkflowMetricsSLACalendarTracker {
+
+	public WorkflowMetricsSLACalendar getWorkflowMetricsSLACalendar(String key);
+
+	public Map<String, String> getWorkflowMetricsSLACalendarTitles(
+		Locale locale);
+
+}
