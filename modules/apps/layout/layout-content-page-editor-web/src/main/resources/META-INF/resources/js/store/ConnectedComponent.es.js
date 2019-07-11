4e10eee8125ea2b3ac33271c707d0cdd784c86d9
Pablo Molina <contact@pablomolina.me>
2019-07-11 15:11:36 +0200

    LPS-98040 Remove forceUpdate

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/store/ConnectedComponent.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/store/ConnectedComponent.es.js
@@ -12,10 +12,12 @@
  * details.
  */
 
+import React, {useContext, useEffect, useState} from 'react';
 import {Config} from 'metal-state';
 
 import {connect, disconnect, Store} from './store.es';
 import INITIAL_STATE from './state.es';
+import StoreContext from './StoreContext.es';
 
 /**
  * HOC that returns a component that connects automatically
@@ -88,5 +90,48 @@ const getConnectedComponent = (Component, properties) => {
 	return ConnectedComponent;
 };
 
-export {getConnectedComponent};
+/**
+ * Second order function to produce a Connected Component Wrapper
+ * @param {Function} mapStateToProps - Recieves the state and returns mapped version of it ready for consumption by the Wrapped Component
+ * @param {Function} mapDispatchToProps - Recieves the dispatch and returns a set of component props that use it to call the modify the state tree
+ * @returns
+ */
+function getConnectedReactComponent(mapStateToProps, mapDispatchToProps) {
+	/**
+	 *
+	 * @param {React.Component} WrappedComponent - The Component to connect to the store
+	 * @returns {React.Component} - Wrapper Connected Component that propagates every store change mapped to the component
+	 */
+	return function _getConnectedWrapperComponent(WrappedComponent) {
+		return props => {
+			const store = useContext(StoreContext);
+
+			const [storeState, setStoreState] = useState(
+				store ? store.getState() : {}
+			);
+
+			useEffect(() => {
+				if (store) {
+					const handleStoreChange = () =>
+						setStoreState(store.getState());
+					const subscriber = store.on('change', handleStoreChange);
+
+					return () => subscriber.removeListener();
+				}
+			}, [store]);
+
+			return store ? (
+				<WrappedComponent
+					{...props}
+					{...mapStateToProps(storeState, props)}
+					{...mapDispatchToProps(store.dispatch, props)}
+				/>
+			) : (
+				<></>
+			);
+		};
+	};
+}
+
+export {getConnectedComponent, getConnectedReactComponent};
 export default getConnectedComponent;
