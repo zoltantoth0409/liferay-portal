d08acbf4b2f905d0ac94c7a0b9265118ff697687
Victor Galan <victor.galan@liferay.com>
2019-07-12 09:35:59 +0200

    LPS-96659 Check permission for assign roles and include the action

--- modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/internal/display/context/UsersManagementToolbarDisplayContext.java
+++ modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/internal/display/context/UsersManagementToolbarDisplayContext.java
@@ -21,6 +21,7 @@ import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemList;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItemList;
 import com.liferay.petra.string.StringPool;
+import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Role;
@@ -34,6 +35,7 @@ import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.WebKeys;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -155,6 +157,8 @@ public class UsersManagementToolbarDisplayContext
 	}
 
 	public String getAvailableActions(User user) throws PortalException {
+		List<String> availableActions = new ArrayList<>();
+
 		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
@@ -167,10 +171,18 @@ public class UsersManagementToolbarDisplayContext
 			!SiteMembershipPolicyUtil.isMembershipRequired(
 				user.getUserId(), _usersDisplayContext.getGroupId())) {
 
-			return "deleteSelectedUsers";
+			availableActions.add("deleteSelectedUsers");
 		}
 
-		return StringPool.BLANK;
+		if (GroupPermissionUtil.contains(
+				themeDisplay.getPermissionChecker(),
+				themeDisplay.getSiteGroupIdOrLiveGroupId(),
+				ActionKeys.ASSIGN_USER_ROLES)) {
+
+			availableActions.add("selectSiteRole");
+		}
+
+		return StringUtil.merge(availableActions, StringPool.COMMA);
 	}
 
 	@Override
