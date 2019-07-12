8249282a2a5dff524b43f8672edd2cdf31d3fe4f
Victor Galan <victor.galan@liferay.com>
2019-07-12 14:13:39 +0200

    LPS-98177 Convert Select field to component

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/fragment_configuration/field_types/SelectField.es.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import {Config} from 'metal-state';
+
+import './SelectFieldDelegateTemplate.soy';
+import templates from './SelectField.soy';
+
+/**
+ * SelectField
+ */
+class SelectField extends Component {
+	_handleSelectValueChanged(event) {
+		const targetElement = event.delegateTarget;
+
+		this.emit('fieldValueChanged', {
+			name: this.field.name,
+			value: targetElement.options[targetElement.selectedIndex].value
+		});
+	}
+}
+
+SelectField.STATE = {
+	field: Config.objectOf(
+		Config.shapeOf({
+			label: Config.string(),
+			name: Config.string(),
+			type: Config.string(),
+			typeOptions: Config.object()
+		})
+	)
+};
+
+Soy.register(SelectField, templates);
+
+export {SelectField};
+export default SelectField;
