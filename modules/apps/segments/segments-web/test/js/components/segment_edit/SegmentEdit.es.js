f95cc9c0a8ea776208d645a226d2ed6fde3195bf
Andrés Fulla <andres.fulla@liferay.com>
2019-07-12 10:01:06 +0200

    LPS-97655 Render utility for SegmentEdit tests

--- modules/apps/segments/segments-web/test/js/components/segment_edit/SegmentEdit.es.js
+++ modules/apps/segments/segments-web/test/js/components/segment_edit/SegmentEdit.es.js
@@ -20,23 +20,66 @@ import {SOURCES} from 'utils/constants.es';
 
 const SOURCE_ICON_TESTID = 'source-icon';
 
+const PROPERTY_GROUPS_BASIC = [
+	{
+		entityName: 'First Test Values Group',
+		name: 'First Test Values Group',
+		properties: [
+			{
+				label: 'Value',
+				name: 'value',
+				options: [],
+				selectEntity: null,
+				type: 'string'
+			}
+		],
+		propertyKey: 'first-test-values-group'
+	}
+];
+
+const DEFAULT_REDIRECT = '/test-url';
+
+const CONTRIBUTORS = [
+	{
+		conjunctionId: 'and',
+		conjunctionInputId: 'conjunction-input-1',
+		initialQuery: "(value eq 'value')",
+		inputId: 'input-id-for-backend-form',
+		propertyKey: 'first-test-values-group'
+	}
+];
+
+function _renderSegmentEditComponent({
+	source = undefined,
+	redirect = DEFAULT_REDIRECT,
+	hasUpdatePermission = undefined,
+	contributors = undefined,
+	showInEditMode = undefined
+} = {}) {
+	return render(
+		<SegmentEdit
+			availableLocales={{
+				en_US: ''
+			}}
+			contributors={contributors}
+			defaultLanguageId='en_US'
+			hasUpdatePermission={hasUpdatePermission}
+			initialSegmentName={{
+				en_US: 'Segment title'
+			}}
+			locale='en_US'
+			redirect={redirect}
+			showInEditMode={showInEditMode}
+			source={source}
+		/>
+	);
+}
+
 describe('SegmentEdit', () => {
 	afterEach(cleanup);
 
 	it('renders', () => {
-		const {asFragment} = render(
-			<SegmentEdit
-				availableLocales={{
-					en_US: ''
-				}}
-				defaultLanguageId="en_US"
-				initialSegmentName={{
-					en_US: 'Segment title'
-				}}
-				locale="en_US"
-				redirect="/test-url"
-			/>
-		);
+		const {asFragment} = _renderSegmentEditComponent();
 
 		expect(asFragment()).toMatchSnapshot();
 	});
@@ -44,20 +87,9 @@ describe('SegmentEdit', () => {
 	it('renders with an analytics cloud icon', () => {
 		const {icon, name} = SOURCES.ASAH_FARO_BACKEND;
 
-		const {getByTestId} = render(
-			<SegmentEdit
-				availableLocales={{
-					en_US: ''
-				}}
-				defaultLanguageId="en_US"
-				initialSegmentName={{
-					en_US: 'Segment title'
-				}}
-				locale="en_US"
-				redirect="/test-url"
-				source={name}
-			/>
-		);
+		const {getByTestId} = _renderSegmentEditComponent({
+			source: name
+		});
 
 		const image = getByTestId(SOURCE_ICON_TESTID);
 
@@ -67,20 +99,9 @@ describe('SegmentEdit', () => {
 	it('renders with a dxp icon', () => {
 		const {icon, name} = SOURCES.DEFAULT;
 
-		const {getByTestId} = render(
-			<SegmentEdit
-				availableLocales={{
-					en_US: ''
-				}}
-				defaultLanguageId="en_US"
-				initialSegmentName={{
-					en_US: 'Segment title'
-				}}
-				locale="en_US"
-				redirect="/test-url"
-				source={name}
-			/>
-		);
+		const {getByTestId} = _renderSegmentEditComponent({
+			source: name
+		});
 
 		const image = getByTestId(SOURCE_ICON_TESTID);
 
@@ -90,20 +111,9 @@ describe('SegmentEdit', () => {
 	it('renders with edit buttons if the user has update permissions', () => {
 		const hasUpdatePermission = true;
 
-		const {asFragment} = render(
-			<SegmentEdit
-				availableLocales={{
-					en_US: ''
-				}}
-				defaultLanguageId="en_US"
-				hasUpdatePermission={hasUpdatePermission}
-				initialSegmentName={{
-					en_US: 'Segment title'
-				}}
-				locale="en_US"
-				redirect="/test-url"
-			/>
-		);
+		const {asFragment} = _renderSegmentEditComponent({
+			hasUpdatePermission
+		});
 
 		expect(asFragment()).toMatchSnapshot();
 	});
@@ -111,130 +121,54 @@ describe('SegmentEdit', () => {
 	it('renders without edit buttons if the user does not have update permissions', () => {
 		const hasUpdatePermission = false;
 
-		const {asFragment} = render(
-			<SegmentEdit
-				availableLocales={{
-					en_US: ''
-				}}
-				defaultLanguageId="en_US"
-				hasUpdatePermission={hasUpdatePermission}
-				initialSegmentName={{
-					en_US: 'Segment title'
-				}}
-				locale="en_US"
-				redirect="/test-url"
-			/>
-		);
+		const {asFragment} = _renderSegmentEditComponent({
+			hasUpdatePermission
+		});
 
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it('renders with given values', () => {
-		const contributors = [
-			{
-				conjunctionId: 'and',
-				conjunctionInputId: 'conjunction-input-1',
-				initialQuery: "(value eq 'value')",
-				inputId: 'input-id-for-backend-form',
-				propertyKey: 'first-test-values-group'
-			}
-		];
 		const hasUpdatePermission = true;
-
-		const propertyGroups = [
-			{
-				entityName: 'First Test Values Group',
-				name: 'First Test Values Group',
-				properties: [
-					{
-						label: 'Value',
-						name: 'value',
-						options: [],
-						selectEntity: null,
-						type: 'string'
-					}
-				],
-				propertyKey: 'first-test-values-group'
-			}
-		];
 
 		const {asFragment, getByTestId} = render(
 			<SegmentEdit
 				availableLocales={{
 					en_US: ''
 				}}
-				contributors={contributors}
-				defaultLanguageId="en_US"
-				hasUpdatePermission={hasUpdatePermission}
-				initialSegmentName={{
-					en_US: 'Segment title'
-				}}
-				locale="en_US"
-				propertyGroups={propertyGroups}
-				redirect="/test-url"
-				showInEditMode={true}
-			/>
-		);
-
-		expect(getByTestId(contributors[0].inputId).value).toBe(
-			contributors[0].initialQuery
-		);
-
-		expect(asFragment()).toMatchSnapshot();
-	});
-
-	it('redirects when cancelling without any edition', () => {
-		const redirect = '/test-url';
-		const mockConfirm = jest.fn();
-		const mockNavigate = jest.fn();
-		window.Liferay.Util.navigate = mockNavigate;
-		window.confirm = mockConfirm;
-
-		const contributors = [
-			{
-				conjunctionId: 'and',
-				conjunctionInputId: 'conjunction-input-1',
-				initialQuery: "(value eq 'value')",
-				inputId: 'input-id-for-backend-form',
-				propertyKey: 'first-test-values-group'
-			}
-		];
-		const hasUpdatePermission = true;
-
-		const propertyGroups = [
-			{
-				entityName: 'First Test Values Group',
-				name: 'First Test Values Group',
-				properties: [
-					{
-						label: 'Value',
-						name: 'value',
-						options: [],
-						selectEntity: null,
-						type: 'string'
-					}
-				],
-				propertyKey: 'first-test-values-group'
-			}
-		];
-
-		const {getByText} = render(
-			<SegmentEdit
-				availableLocales={{
-					en_US: ''
-				}}
-				contributors={contributors}
+				contributors={CONTRIBUTORS}
 				defaultLanguageId='en_US'
 				hasUpdatePermission={hasUpdatePermission}
 				initialSegmentName={{
 					en_US: 'Segment title'
 				}}
 				locale='en_US'
-				propertyGroups={propertyGroups}
-				redirect={redirect}
+				propertyGroups={PROPERTY_GROUPS_BASIC}
+				redirect='/test-url'
 				showInEditMode={true}
 			/>
 		);
+
+		expect(getByTestId(CONTRIBUTORS[0].inputId).value).toBe(
+			CONTRIBUTORS[0].initialQuery
+		);
+
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('redirects when cancelling without any edition', () => {
+		const mockConfirm = jest.fn();
+		const mockNavigate = jest.fn();
+		window.Liferay.Util.navigate = mockNavigate;
+		window.confirm = mockConfirm;
+
+		const hasUpdatePermission = true;
+
+		const {getByText} = _renderSegmentEditComponent({
+			hasUpdatePermission,
+			propertyGroups: PROPERTY_GROUPS_BASIC,
+			contributors: CONTRIBUTORS
+		});
 
 		const cancelButton = getByText('cancel');
 
@@ -243,62 +177,23 @@ describe('SegmentEdit', () => {
 		fireEvent.click(cancelButton);
 
 		expect(mockNavigate).toHaveBeenCalledTimes(1);
-		expect(mockNavigate).toHaveBeenCalledWith(redirect);
+		expect(mockNavigate).toHaveBeenCalledWith(DEFAULT_REDIRECT);
 		expect(mockConfirm).toHaveBeenCalledTimes(0);
 	});
 
 	it('redirects when cancelling after title edition', done => {
-		const redirect = '/test-url';
 		const mockConfirm = jest.fn();
 		const mockNavigate = jest.fn();
 		window.Liferay.Util.navigate = mockNavigate;
 		window.confirm = mockConfirm;
 
-		const contributors = [
-			{
-				conjunctionId: 'and',
-				conjunctionInputId: 'conjunction-input-1',
-				initialQuery: "(value eq 'value')",
-				inputId: 'input-id-for-backend-form',
-				propertyKey: 'first-test-values-group'
-			}
-		];
 		const hasUpdatePermission = true;
 
-		const propertyGroups = [
-			{
-				entityName: 'First Test Values Group',
-				name: 'First Test Values Group',
-				properties: [
-					{
-						label: 'Value',
-						name: 'value',
-						options: [],
-						selectEntity: null,
-						type: 'string'
-					}
-				],
-				propertyKey: 'first-test-values-group'
-			}
-		];
-
-		const {getByTestId, getByText} = render(
-			<SegmentEdit
-				availableLocales={{
-					en_US: ''
-				}}
-				contributors={contributors}
-				defaultLanguageId='en_US'
-				hasUpdatePermission={hasUpdatePermission}
-				initialSegmentName={{
-					en_US: 'Segment title'
-				}}
-				locale='en_US'
-				propertyGroups={propertyGroups}
-				redirect={redirect}
-				showInEditMode={true}
-			/>
-		);
+		const {getByTestId, getByText} = _renderSegmentEditComponent({
+			hasUpdatePermission,
+			propertyGroups: PROPERTY_GROUPS_BASIC,
+			contributors: CONTRIBUTORS
+		});
 
 		const localizedInput = getByTestId('localized-main-input');
 		const cancelButton = getByText('cancel');
