a792b1bb128ea8abc6387ca12b188a9fdd3ffc42
Inácio Nery <inacio.nery@liferay.com>
2019-07-12 08:40:12 -0300

    LPS-97254 Add Reports ConfigurationCategory

---
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/category/ReportsConfigurationCategory.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.internal.category;
+
+import com.liferay.configuration.admin.category.ConfigurationCategory;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Inácio Nery
+ */
+@Component(service = ConfigurationCategory.class)
+public class ReportsConfigurationCategory implements ConfigurationCategory {
+
+	@Override
+	public String getCategoryIcon() {
+		return _CATEGORY_ICON;
+	}
+
+	@Override
+	public String getCategoryKey() {
+		return _CATEGORY_KEY;
+	}
+
+	@Override
+	public String getCategorySection() {
+		return _CATEGORY_SECTION;
+	}
+
+	private static final String _CATEGORY_ICON = "polls";
+
+	private static final String _CATEGORY_KEY = "reports";
+
+	private static final String _CATEGORY_SECTION = "content-and-data";
+
+}
