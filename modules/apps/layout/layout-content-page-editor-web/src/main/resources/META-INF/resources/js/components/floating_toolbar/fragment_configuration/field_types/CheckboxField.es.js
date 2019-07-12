f6a61713f58b16eef9d782b77ad3c2104c7288a4
Victor Galan <victor.galan@liferay.com>
2019-07-12 14:13:06 +0200

    LPS-98177 Convert Checkbox field to a component

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/fragment_configuration/field_types/CheckboxField.es.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import {Config} from 'metal-state';
+
+import './CheckboxFieldDelegateTemplate.soy';
+import templates from './CheckboxField.soy';
+
+/**
+ * CheckboxField
+ */
+class CheckboxField extends Component {
+	_handleCheckboxValueChanged(event) {
+		const targetElement = event.delegateTarget;
+
+		this.emit('fieldValueChanged', {
+			name: this.field.name,
+			value: `${targetElement.checked}`
+		});
+	}
+}
+
+CheckboxField.STATE = {
+	field: Config.objectOf(
+		Config.shapeOf({
+			label: Config.string(),
+			name: Config.string(),
+			type: Config.string(),
+			typeOptions: Config.object()
+		})
+	)
+};
+
+Soy.register(CheckboxField, templates);
+
+export {CheckboxField};
+export default CheckboxField;
