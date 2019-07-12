dfd7a2eda17c7beaf6ba69779f903f0f42123ff6
Sandro Chinea <sandrochinea@gmail.com>
2019-07-12 12:12:40 +0100

    LPS-97744 Take defaultConfigurationValues from response

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/reducers/fragments.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/reducers/fragments.es.js
@@ -630,6 +630,7 @@ function _addFragmentEntryLink(
 				config: {},
 				configuration: response.configuration,
 				content: '',
+				defaultConfigurationValues: response.defaultConfigurationValues,
 				editableValues: JSON.parse(response.editableValues),
 				fragmentEntryKey,
 				fragmentEntryLinkId: response.fragmentEntryLinkId,
