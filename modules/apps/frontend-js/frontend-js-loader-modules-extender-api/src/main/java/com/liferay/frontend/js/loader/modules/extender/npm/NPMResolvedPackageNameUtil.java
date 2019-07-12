0fd3c92be8622bf33823fefbf21876bea54521f5
shuyangzhou <shuyang.zhou@liferay.com>
2019-07-11 23:34:23 -0700

    LPS-98362 Lazily caculate npmResolvedPackageName

--- modules/apps/frontend-js/frontend-js-loader-modules-extender-api/src/main/java/com/liferay/frontend/js/loader/modules/extender/npm/NPMResolvedPackageNameUtil.java
+++ modules/apps/frontend-js/frontend-js-loader-modules-extender-api/src/main/java/com/liferay/frontend/js/loader/modules/extender/npm/NPMResolvedPackageNameUtil.java
@@ -14,14 +14,25 @@
 
 package com.liferay.frontend.js.loader.modules.extender.npm;
 
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.PortletBag;
 import com.liferay.portal.kernel.portlet.PortletBagPool;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.servlet.ServletContextClassLoaderPool;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+
+import java.net.URL;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleReference;
 
 /**
  * @author Iván Zaera Avellón
@@ -65,8 +76,32 @@ public class NPMResolvedPackageNameUtil {
 	 * @review
 	 */
 	public static String get(ServletContext servletContext) {
-		return (String)servletContext.getAttribute(
+		Object obj = servletContext.getAttribute(
 			NPMResolvedPackageNameUtil.class.getName());
+
+		if (obj instanceof String) {
+			return (String)obj;
+		}
+
+		if (obj == _NULL_HOLDER) {
+			return null;
+		}
+
+		String npmResolvedPackageName = _getNPMResolvedPackageName(
+			ServletContextClassLoaderPool.getClassLoader(
+				servletContext.getServletContextName()));
+
+		if (npmResolvedPackageName == null) {
+			servletContext.setAttribute(
+				NPMResolvedPackageNameUtil.class.getName(), _NULL_HOLDER);
+		}
+		else {
+			servletContext.setAttribute(
+				NPMResolvedPackageNameUtil.class.getName(),
+				npmResolvedPackageName);
+		}
+
+		return npmResolvedPackageName;
 	}
 
 	public static void set(
@@ -82,5 +117,51 @@ public class NPMResolvedPackageNameUtil {
 				NPMResolvedPackageNameUtil.class.getName());
 		}
 	}
+
+	private static String _getNPMResolvedPackageName(
+		Bundle bundle, NPMResolver npmResolver) {
+
+		URL url = bundle.getResource("META-INF/resources/package.json");
+
+		if (url == null) {
+			return null;
+		}
+
+		try {
+			String json = StringUtil.read(url.openStream());
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(json);
+
+			String name = jsonObject.getString("name");
+
+			return npmResolver.resolveModuleName(name);
+		}
+		catch (Exception e) {
+			_log.error(
+				"Unable to read META-INF/resources/package.json in " +
+					bundle.getSymbolicName(),
+				e);
+		}
+
+		return null;
+	}
+
+	private static String _getNPMResolvedPackageName(ClassLoader classLoader) {
+		if (!(classLoader instanceof BundleReference)) {
+			return null;
+		}
+
+		BundleReference bundleReference = (BundleReference)classLoader;
+
+		Bundle bundle = bundleReference.getBundle();
+
+		return _getNPMResolvedPackageName(
+			bundle, NPMResolverUtil.getNPMResolver(bundle));
+	}
+
+	private static final Object _NULL_HOLDER = new Object();
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		NPMResolvedPackageNameUtil.class);
 
 }
