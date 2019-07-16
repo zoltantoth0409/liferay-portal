8682598c4e64033611ad0e6ef2e6e1b7c73efda2
Igor Beslic <igor.beslic@liferay.com>
2019-07-16 21:17:37 +0200

    COMMERCE-1563 talend - component - common - shorten stacktrace elements and make possible to log on info level

--- modules/etl/talend/talend-common/src/main/java/com/liferay/talend/common/log/DebugUtils.java
+++ modules/etl/talend/talend-common/src/main/java/com/liferay/talend/common/log/DebugUtils.java
@@ -17,6 +17,8 @@ package com.liferay.talend.common.log;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,31 +26,55 @@ import org.slf4j.Logger;
 
 /**
  * @author Zoltán Takács
+ * @author Igor Beslic
  */
 public class DebugUtils {
 
-	public static void logCurrentStackTrace(Logger logger) {
+	public static void debugCurrentStackTrace(Logger logger) {
 		if (logger.isDebugEnabled()) {
-			Throwable throwable = new Throwable();
-
-			List<StackTraceElement> stackTraceElements = new ArrayList<>(
-				Arrays.asList(throwable.getStackTrace()));
-
-			Stream<StackTraceElement> stream = stackTraceElements.stream();
-
-			List<String> stackTraces = stream.map(
-				element -> element.toString()
-			).collect(
-				Collectors.toList()
-			);
-
 			logger.debug(
-				"Actual thread's stacktrace: " +
-					String.join(System.lineSeparator(), stackTraces));
+				"Actual thread's stacktrace: " + _getCurrentStackTrace());
 		}
+	}
+
+	public static void infoCurrentStackTrace(Logger logger) {
+		if (logger.isInfoEnabled()) {
+			logger.info(
+				"Actual thread's stacktrace: " + _getCurrentStackTrace());
+		}
+	}
+
+	private static String _getCurrentStackTrace() {
+		Throwable throwable = new Throwable();
+
+		List<StackTraceElement> stackTraceElements = new ArrayList<>(
+			Arrays.asList(throwable.getStackTrace()));
+
+		Stream<StackTraceElement> stream = stackTraceElements.stream();
+
+		List<String> stackTraces = stream.map(
+			element -> _toClassAndMethod(element.toString())
+		).collect(
+			Collectors.toList()
+		);
+
+		return String.join(System.lineSeparator(), stackTraces);
+	}
+
+	private static String _toClassAndMethod(String line) {
+		Matcher matcher = _stackLinePattern.matcher(line);
+
+		if (!matcher.matches()) {
+			return line;
+		}
+
+		return matcher.group(2) + matcher.group(1);
 	}
 
 	private DebugUtils() {
 	}
+
+	private static final Pattern _stackLinePattern = Pattern.compile(
+		".+(\\.\\w+)\\((.+)\\)");
 
 }
