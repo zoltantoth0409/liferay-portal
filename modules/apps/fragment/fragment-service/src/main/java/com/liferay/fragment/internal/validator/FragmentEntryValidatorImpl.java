db55c91fe833b42b32c116e6e0e2155ddceb609f
Ruben Pulido <ruben.pulido@liferay.com>
2019-07-16 12:10:55 +0200

    LPS-96689 Implement FragmentEntryValidatorImpl

---
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/validator/FragmentEntryValidatorImpl.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.internal.validator;
+
+import com.liferay.fragment.exception.FragmentEntryConfigurationException;
+import com.liferay.fragment.validator.FragmentEntryValidator;
+
+import java.io.InputStream;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rubén Pulido
+ */
+@Component(immediate = true, service = FragmentEntryValidator.class)
+public class FragmentEntryValidatorImpl implements FragmentEntryValidator {
+
+	@Override
+	public void validateConfiguration(String configuration)
+		throws FragmentEntryConfigurationException {
+
+		InputStream configurationJSONSchemaInputStream =
+			FragmentEntryValidatorImpl.class.getResourceAsStream(
+				"dependencies/configuration-json-schema.json");
+
+		try {
+			JSONObject jsonObject = new JSONObject(
+				new JSONTokener(configurationJSONSchemaInputStream));
+
+			Schema schema = SchemaLoader.load(jsonObject);
+
+			schema.validate(new JSONObject(configuration));
+		}
+		catch (ValidationException ve) {
+			throw new FragmentEntryConfigurationException(ve);
+		}
+	}
+
+}
