f241bebe3f707bee4f289bd3fb3f62fab2323ba3
Brian Chan <brian.chan@liferay.com>
2019-07-17 15:44:18 -0700

    LPS-97932 SF

--- modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/service/impl/DEDataRecordQueryLocalServiceImpl.java
+++ modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/service/impl/DEDataRecordQueryLocalServiceImpl.java
@@ -20,17 +20,7 @@ import com.liferay.portal.aop.AopService;
 import org.osgi.service.component.annotations.Component;
 
 /**
- * The implementation of the de data record query local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.data.engine.service.DEDataRecordQueryLocalService</code> interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see DEDataRecordQueryLocalServiceBaseImpl
  */
 @Component(
 	property = "model.class.name=com.liferay.data.engine.model.DEDataRecordQuery",
@@ -38,11 +28,4 @@ import org.osgi.service.component.annotations.Component;
 )
 public class DEDataRecordQueryLocalServiceImpl
 	extends DEDataRecordQueryLocalServiceBaseImpl {
-
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Use <code>com.liferay.data.engine.service.DEDataRecordQueryLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.data.engine.service.DEDataRecordQueryLocalServiceUtil</code>.
-	 */
-
 }
