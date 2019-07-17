44f3ed8fbcabe58c8c9501bc6e3c858b9fa26044
Bryan Engler <bryan.engler@liferay.com>
2019-07-17 10:38:04 -0700

    LPS-101267 portal-search-api: "properties" is an always present keyword field
    https://issues.liferay.com/browse/LPS-101267

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/configuration/QueryPreProcessConfiguration.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/configuration/QueryPreProcessConfiguration.java
@@ -33,7 +33,7 @@ import org.osgi.annotation.versioning.ProviderType;
 public interface QueryPreProcessConfiguration {
 
 	@Meta.AD(
-		deflt = "emailAddress|license|path|screenName|tag|treePath|userName",
+		deflt = "emailAddress|license|path|properties|screenName|tag|treePath|userName",
 		name = "field-name-patterns", required = false
 	)
 	public String[] fieldNamePatterns();
