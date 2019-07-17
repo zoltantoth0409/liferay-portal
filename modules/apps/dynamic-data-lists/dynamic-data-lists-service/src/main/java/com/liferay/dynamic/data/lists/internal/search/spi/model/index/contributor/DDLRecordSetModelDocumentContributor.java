b7876ed4f004c102511a2d6fec17c9d0eca62422
marcelabc <mbc3@cin.ufpe.br>
2019-07-17 19:28:03 -0300

    LPS-98083 Implement Search API for DDLRecordSet

---
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/search/spi/model/index/contributor/DDLRecordSetModelDocumentContributor.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.lists.internal.search.spi.model.index.contributor;
+
+import com.liferay.dynamic.data.lists.model.DDLRecordSet;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcela Cunha
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.dynamic.data.lists.model.DDLRecordSet",
+	service = ModelDocumentContributor.class
+)
+public class DDLRecordSetModelDocumentContributor
+	implements ModelDocumentContributor<DDLRecordSet> {
+
+	@Override
+	public void contribute(Document document, DDLRecordSet ddlRecordSet) {
+		document.addKeyword("DDMStructureId", ddlRecordSet.getDDMStructureId());
+		document.addKeyword("scope", ddlRecordSet.getScope());
+		document.addKeyword(Field.VERSION, ddlRecordSet.getVersion());
+
+		Locale defaultLocale = LocaleUtil.getSiteDefault();
+
+		String defaultLanguageId = LocaleUtil.toLanguageId(defaultLocale);
+
+		String[] descriptionLanguageIds = getLanguageIds(
+			defaultLanguageId, ddlRecordSet.getDescription());
+
+		for (String descriptionLanguageId : descriptionLanguageIds) {
+			document.addText(
+				LocalizationUtil.getLocalizedName(
+					Field.DESCRIPTION, descriptionLanguageId),
+				ddlRecordSet.getDescription(descriptionLanguageId));
+		}
+
+		String[] nameLanguageIds = getLanguageIds(
+			defaultLanguageId, ddlRecordSet.getName());
+
+		for (String nameLanguageId : nameLanguageIds) {
+			document.addText(
+				LocalizationUtil.getLocalizedName(Field.NAME, nameLanguageId),
+				ddlRecordSet.getName(nameLanguageId));
+		}
+	}
+
+	protected String[] getLanguageIds(
+		String defaultLanguageId, String content) {
+
+		String[] languageIds = LocalizationUtil.getAvailableLanguageIds(
+			content);
+
+		if (languageIds.length == 0) {
+			languageIds = new String[] {defaultLanguageId};
+		}
+
+		return languageIds;
+	}
+
+	@Reference
+	protected ClassNameLocalService classNameLocalService;
+
+}
