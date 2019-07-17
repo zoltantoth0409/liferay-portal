f2dfa9b43ad5c6d23e0bfa18911c19cc1d3a49f9
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-07-17 14:27:36 -0300

    LPS-98175 Use fetch instead

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoTaskInstanceTokenLocalServiceImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoTaskInstanceTokenLocalServiceImpl.java
@@ -63,6 +63,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
@@ -584,16 +585,12 @@ public class KaleoTaskInstanceTokenLocalServiceImpl
 				long kaleoTaskInstanceTokenId = GetterUtil.getLong(
 					document.get(Field.ENTRY_CLASS_PK));
 
-				try {
-					kaleoTaskInstanceTokens.add(
-						kaleoTaskInstanceTokenPersistence.findByPrimaryKey(
-							kaleoTaskInstanceTokenId));
-				}
-				catch (PortalException pe) {
-					if (_log.isWarnEnabled()) {
-						_log.warn(pe, pe);
-					}
-				}
+				Optional.ofNullable(
+					kaleoTaskInstanceTokenPersistence.fetchByPrimaryKey(
+						kaleoTaskInstanceTokenId)
+				).ifPresent(
+					kaleoTaskInstanceTokens::add
+				);
 			}
 
 			return kaleoTaskInstanceTokens;
