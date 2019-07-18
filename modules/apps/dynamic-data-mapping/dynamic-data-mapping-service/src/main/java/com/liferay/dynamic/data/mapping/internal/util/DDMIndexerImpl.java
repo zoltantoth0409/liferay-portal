70e375b03341428f750deeaee3f9dd65824a51a0
Jose Maria Muñoz <josemaria.munoz@liferay.com>
2019-07-18 13:53:14 +0200

    LPS-98394 Not indexing when type is none

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/util/DDMIndexerImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/util/DDMIndexerImpl.java
@@ -283,7 +283,7 @@ public class DDMIndexerImpl implements DDMIndexer {
 				String indexType = ddmStructure.getFieldProperty(
 					field.getName(), "indexType");
 
-				if (Validator.isNull(indexType)) {
+				if (Validator.isNull(indexType) || indexType.equals("none")) {
 					continue;
 				}
 
