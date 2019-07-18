7dbf9ce33fcb80d5f9b84ed520aa49883f975990
Sandro Chinea <sandrochinea@gmail.com>
2019-07-18 09:40:12 +0200

    LPS-97704 Added logic to FloatingToolbarDropdown component

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/common/FloatingToolbarDropdown.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/common/FloatingToolbarDropdown.es.js
@@ -12,6 +12,7 @@
  * details.
  */
 
+import {Align} from 'metal-position';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 import templates from './FloatingToolbarDropdown.soy';
@@ -20,7 +21,93 @@ import {Config} from 'metal-state';
 /**
  * FloatingToolbarDropdown
  */
-class FloatingToolbarDropdown extends Component {}
+class FloatingToolbarDropdown extends Component {
+	/**
+	 * @inheritdoc
+	 */
+	created() {
+		this._handleButtonClick = this._handleButtonClick.bind(this);
+		this._handleWindowMouseUp = this._handleWindowMouseUp.bind(this);
+		this._handleScroll = this._handleScroll.bind(this);
+
+		this._button = document.querySelector(`#${this.buttonId}`);
+		this._fragmentEntryLinkListWrapper = document.querySelector(
+			'.fragment-entry-link-list-wrapper'
+		);
+
+		this._button.addEventListener('click', this._handleButtonClick);
+
+		this._fragmentEntryLinkListWrapper.addEventListener(
+			'scroll',
+			this._handleScroll
+		);
+
+		document.body.addEventListener('scroll', this._handleScroll);
+
+		window.addEventListener('mouseup', this._handleWindowMouseUp);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	disposed() {
+		window.removeEventListener('mouseup', this._handleWindowMouseUp);
+
+		this._fragmentEntryLinkListWrapper.removeEventListener(
+			'scroll',
+			this._handleScroll
+		);
+
+		document.body.removeEventListener('scroll', this._handleScroll);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	rendered() {
+		if (!this._dropdown) {
+			this._dropdown = document.querySelector(`#${this.dropdownId}`);
+		}
+	}
+
+	/**
+	 * Align dropdown to button
+	 * @private
+	 * @review
+	 */
+	_alignDropdown() {
+		Align.align(this._dropdown, this._button, Align.LeftCenter, false);
+	}
+
+	/**
+	 * Handle button click
+	 * @private
+	 * @review
+	 */
+	_handleButtonClick() {
+		this._dropdown.classList.add('show');
+
+		this._alignDropdown();
+	}
+
+	/**
+	 * Handle FragmentEntryLinkListWrapper scroll
+	 * @private
+	 * @review
+	 */
+	_handleScroll() {
+		this._alignDropdown();
+	}
+
+	/**
+	 * Handle window mouseup event
+	 * @private
+	 * @review
+	 */
+	_handleWindowMouseUp() {
+		this._dropdown.classList.remove('show');
+	}
+}
 
 /**
  * State definition.
