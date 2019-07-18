f046717090ceb611e0a2335af7aee87da7569dd4
Pablo Molina <contact@pablomolina.me>
2019-07-18 15:19:57 +0200

    LPS-98335 Add FragmentComment component

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/sidebar/comments/FragmentComment.es.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/* eslint no-unused-vars: "warn" */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import UserIcon from '../../common/UserIcon.es';
+
+const FragmentComment = props => (
+	<article className="fragments-editor__fragment-comment small">
+		<div className="d-flex mb-2">
+			<UserIcon {...props.author} />
+
+			<div className="pl-2">
+				<p className="m-0 text-truncate">
+					<strong>{props.author.fullName}</strong>
+				</p>
+
+				<p className="m-0 text-secondary">{props.dateDescription}</p>
+			</div>
+		</div>
+
+		<p
+			className="text-secondary"
+			dangerouslySetInnerHTML={{__html: props.body}}
+		/>
+	</article>
+);
+
+FragmentComment.propTypes = {
+	author: PropTypes.shape({
+		fullName: PropTypes.string,
+		portraitURL: PropTypes.string
+	}),
+
+	body: PropTypes.string,
+	dateDescription: PropTypes.string
+};
+
+export {FragmentComment};
+export default FragmentComment;
