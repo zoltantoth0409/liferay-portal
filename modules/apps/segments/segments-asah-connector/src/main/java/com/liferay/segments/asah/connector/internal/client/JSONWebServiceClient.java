cffa97ab4ad56da1ffa10ebafd5d34ca68ee7aa4
Sarai Díaz García <dgarciasarai@gmail.com>
2019-07-19 11:56:47 +0200

    LPS-98407 Support PUT operation in client

--- modules/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/client/JSONWebServiceClient.java
+++ modules/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/client/JSONWebServiceClient.java
@@ -30,6 +30,9 @@ public interface JSONWebServiceClient {
 	public <T> T doPost(
 		Class<T> clazz, String url, T object, Map<String, String> headers);
 
+	public <T> T doPut(
+		Class<T> clazz, String url, T object, Map<String, String> headers);
+
 	public String getBaseURI();
 
 	public void setBaseURI(String baseURI);
