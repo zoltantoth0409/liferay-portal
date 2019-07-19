39f5b70092bd778b5dbff731b1f05932609f20a7
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-07-19 12:13:40 +0200

    LPS-98444 Adding the URL to the soy context

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -213,6 +213,10 @@ public class ContentPageEditorDisplayContext {
 			getFragmentEntryActionURL(
 				"/content_layout/add_fragment_entry_link_comment")
 		).put(
+			"deleteFragmentEntryLinkCommentURL",
+			getFragmentEntryActionURL(
+				"/content_layout/delete_fragment_entry_link_comment")
+		).put(
 			"editFragmentEntryLinkCommentURL",
 			getFragmentEntryActionURL(
 				"/content_layout/edit_fragment_entry_link_comment")
