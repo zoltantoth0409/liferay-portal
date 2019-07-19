abf11b6908dc7e0cf97e3df9ade8a5d88775d796
Pavel Savinov <pavel.savinov@liferay.com>
2019-07-19 16:56:29 +0200

    LPS-98031 Add mapper interface which allows to map editable element values

---
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/mapper/EditableElementMapper.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.entry.processor.editable.mapper;
+
+import com.liferay.fragment.processor.FragmentEntryProcessorContext;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+
+import org.jsoup.nodes.Element;
+
+/**
+ * @author Pavel Savinov
+ */
+public interface EditableElementMapper {
+
+	public void map(
+			Element element, JSONObject configJSONObject,
+			FragmentEntryProcessorContext fragmentEntryProcessorContext)
+		throws PortalException;
+
+}
