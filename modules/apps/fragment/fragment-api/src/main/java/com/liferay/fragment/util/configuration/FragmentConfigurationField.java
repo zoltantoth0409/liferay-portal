ab61db237043f7666df33abf91c6cb3d727a2fc4
Victor Galan <victor.galan@liferay.com>
2019-07-22 15:04:46 +0200

    LPS-98245 Parse configuration fields as a java object for easier use

---
+++ modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/util/configuration/FragmentConfigurationField.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.util.configuration;
+
+/**
+ * @author Víctor Galán
+ */
+public class FragmentConfigurationField {
+
+	public FragmentConfigurationField(
+		String name, String dataType, String defaultValue, String type) {
+
+		_name = name;
+		_dataType = dataType;
+		_defaultValue = defaultValue;
+		_type = type;
+	}
+
+	public String getDataType() {
+		return _dataType;
+	}
+
+	public String getDefaultValue() {
+		return _defaultValue;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String getType() {
+		return _type;
+	}
+
+	private final String _dataType;
+	private final String _defaultValue;
+	private final String _name;
+	private final String _type;
+
+}
