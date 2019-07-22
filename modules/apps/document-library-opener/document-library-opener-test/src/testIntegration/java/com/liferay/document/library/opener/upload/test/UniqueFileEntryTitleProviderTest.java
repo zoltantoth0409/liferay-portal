f316f915f223c204bc4d9c03c2059092835220e4
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-07-22 16:52:55 +0200

    LPS-98495 Add test.

---
+++ modules/apps/document-library-opener/document-library-opener-test/src/testIntegration/java/com/liferay/document/library/opener/upload/test/UniqueFileEntryTitleProviderTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.opener.upload.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.document.library.opener.upload.UniqueFileEntryTitleProvider;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Cristina González
+ */
+@RunWith(Arquillian.class)
+public class UniqueFileEntryTitleProviderTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_folder = _dlAppLocalService.addFolder(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+	}
+
+	@Test
+	public void testProvideWithLocale() throws PortalException {
+		String uniqueEntryFileName = _uniqueFileEntryTitleProvider.provide(
+			_group.getGroupId(), _folder.getFolderId(), LocaleUtil.US);
+
+		Assert.assertEquals("Untitled", uniqueEntryFileName);
+	}
+
+	@Test
+	public void testProvideWithName() throws PortalException {
+		String uniqueEntryFileName = _uniqueFileEntryTitleProvider.provide(
+			_group.getGroupId(), _folder.getFolderId(), "someTitle");
+
+		Assert.assertEquals("someTitle", uniqueEntryFileName);
+	}
+
+	@Inject
+	private DLAppLocalService _dlAppLocalService;
+
+	private Folder _folder;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private UniqueFileEntryTitleProvider _uniqueFileEntryTitleProvider;
+
+}
