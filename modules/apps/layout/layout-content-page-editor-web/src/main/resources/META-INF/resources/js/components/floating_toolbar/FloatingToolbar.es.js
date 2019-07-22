b22fa4d66284743ed602d19d48ff40ab5c114134
Pavel Savinov <pavel.savinov@liferay.com>
2019-07-22 10:32:10 +0200

    LPS-98031 Support different css classes for the floating toolbar buttons

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/FloatingToolbar.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/FloatingToolbar.es.js
@@ -385,6 +385,7 @@ FloatingToolbar.STATE = {
 	 */
 	buttons: Config.arrayOf(
 		Config.shapeOf({
+			cssClass: Config.string(),
 			icon: Config.string(),
 			id: Config.string(),
 			panelId: Config.string(),
