b7d21b44fd4cba74e49852df6b6d6741a46a367a
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-07-22 14:01:34 +0200

    LPS-98495 Use new implementation.

--- modules/apps/document-library-opener/document-library-opener-google-drive-web/src/main/java/com/liferay/document/library/opener/google/drive/web/internal/upload/UniqueFileEntryTitleProviderImpl.java
+++ modules/apps/document-library-opener/document-library-opener-google-drive-web/src/main/java/com/liferay/document/library/opener/google/drive/web/internal/upload/UniqueFileEntryTitleProviderImpl.java
@@ -14,19 +14,10 @@
 
 package com.liferay.document.library.opener.google.drive.web.internal.upload;
 
-import com.liferay.document.library.kernel.exception.NoSuchFileEntryException;
-import com.liferay.document.library.kernel.service.DLAppLocalService;
 import com.liferay.document.library.opener.google.drive.upload.UniqueFileEntryTitleProvider;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
-import com.liferay.upload.UniqueFileNameProvider;
 
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -42,56 +33,20 @@ public class UniqueFileEntryTitleProviderImpl
 	public String provide(long groupId, long folderId, Locale locale)
 		throws PortalException {
 
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-			locale, UniqueFileEntryTitleProviderImpl.class);
-
-		return _provide(
-			groupId, folderId, _language.get(resourceBundle, "untitled"));
+		return _uniqueFileEntryTitleProvider.provide(groupId, folderId, locale);
 	}
 
 	@Override
 	public String provide(long groupId, long folderId, String fileName)
 		throws PortalException {
 
-		return _provide(groupId, folderId, fileName);
+		return _uniqueFileEntryTitleProvider.provide(
+			groupId, folderId, fileName);
 	}
 
-	private boolean _exists(long groupId, long folderId, String fileName) {
-		try {
-			_dlAppLocalService.getFileEntry(groupId, folderId, fileName);
-
-			return true;
-		}
-		catch (NoSuchFileEntryException nsfee) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(nsfee, nsfee);
-			}
-		}
-		catch (PortalException pe) {
-			throw new SystemException(pe);
-		}
-
-		return false;
-	}
-
-	private String _provide(long groupId, long folderId, String fileName)
-		throws PortalException {
-
-		return _uniqueFileNameProvider.provide(
-			fileName,
-			generatedFileName -> _exists(groupId, folderId, generatedFileName));
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		UniqueFileEntryTitleProviderImpl.class);
-
 	@Reference
-	private DLAppLocalService _dlAppLocalService;
-
-	@Reference
-	private Language _language;
-
-	@Reference
-	private UniqueFileNameProvider _uniqueFileNameProvider;
+	private
+		com.liferay.document.library.opener.upload.UniqueFileEntryTitleProvider
+			_uniqueFileEntryTitleProvider;
 
 }
