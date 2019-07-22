83083ea1578474a2ffe3ab2e749e7438621f4f06
David Arques <davidarchena@gmail.com>
2019-07-22 14:44:41 +0200

    LPS-97199 Refactor to a non component class

--- modules/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/processor/AsahSegmentsExperimentProcessor.java
+++ modules/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/processor/AsahSegmentsExperimentProcessor.java
@@ -15,24 +15,45 @@
 package com.liferay.segments.asah.connector.internal.processor;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.segments.asah.connector.internal.client.AsahFaroBackendClient;
 import com.liferay.segments.asah.connector.internal.client.AsahFaroBackendClientFactory;
-import com.liferay.segments.asah.connector.internal.client.converter.ExperimentDTOConverter;
 import com.liferay.segments.asah.connector.internal.client.model.Experiment;
+import com.liferay.segments.asah.connector.internal.client.model.util.ExperimentUtil;
 import com.liferay.segments.model.SegmentsExperiment;
+import com.liferay.segments.service.SegmentsEntryLocalService;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
 import com.liferay.segments.service.SegmentsExperimentLocalService;
 
 import java.util.Optional;
-
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Sarai Díaz
  * @author David Arques
  */
-@Component(immediate = true, service = AsahSegmentsExperimentProcessor.class)
 public class AsahSegmentsExperimentProcessor {
+
+	public AsahSegmentsExperimentProcessor(
+		AsahFaroBackendClientFactory asahFaroBackendClientFactory,
+		CompanyLocalService companyLocalService,
+		GroupLocalService groupLocalService,
+		LayoutLocalService layoutLocalService, Portal portal,
+		SegmentsEntryLocalService segmentsEntryLocalService,
+		SegmentsExperienceLocalService segmentsExperienceLocalService,
+		SegmentsExperimentLocalService segmentsExperimentLocalService) {
+
+		_asahFaroBackendClientFactory = asahFaroBackendClientFactory;
+		_companyLocalService = companyLocalService;
+		_groupLocalService = groupLocalService;
+		_layoutLocalService = layoutLocalService;
+		_portal = portal;
+		_segmentsEntryLocalService = segmentsEntryLocalService;
+		_segmentsExperienceLocalService = segmentsExperienceLocalService;
+		_segmentsExperimentLocalService = segmentsExperimentLocalService;
+	}
 
 	public void processAddSegmentsExperiment(
 			SegmentsExperiment segmentsExperiment)
@@ -52,8 +73,11 @@ public class AsahSegmentsExperimentProcessor {
 		_asahFaroBackendClient = asahFaroBackendClientOptional.get();
 
 		Experiment experiment = _asahFaroBackendClient.addExperiment(
-			_experimentDTOConverter.toDTO(
-				segmentsExperiment, _asahFaroBackendClient.getDataSourceId()));
+			ExperimentUtil.toExperiment(
+				_companyLocalService, _asahFaroBackendClient.getDataSourceId(),
+				_groupLocalService, _layoutLocalService, _portal,
+				_segmentsEntryLocalService, _segmentsExperienceLocalService,
+				segmentsExperiment));
 
 		segmentsExperiment.setSegmentsExperimentKey(experiment.getId());
 
@@ -62,14 +86,15 @@ public class AsahSegmentsExperimentProcessor {
 	}
 
 	private AsahFaroBackendClient _asahFaroBackendClient;
-
-	@Reference
-	private AsahFaroBackendClientFactory _asahFaroBackendClientFactory;
-
-	@Reference
-	private ExperimentDTOConverter _experimentDTOConverter;
-
-	@Reference
-	private SegmentsExperimentLocalService _segmentsExperimentLocalService;
+	private final AsahFaroBackendClientFactory _asahFaroBackendClientFactory;
+	private final CompanyLocalService _companyLocalService;
+	private final GroupLocalService _groupLocalService;
+	private final LayoutLocalService _layoutLocalService;
+	private final Portal _portal;
+	private final SegmentsEntryLocalService _segmentsEntryLocalService;
+	private final SegmentsExperienceLocalService
+		_segmentsExperienceLocalService;
+	private final SegmentsExperimentLocalService
+		_segmentsExperimentLocalService;
 
 }
