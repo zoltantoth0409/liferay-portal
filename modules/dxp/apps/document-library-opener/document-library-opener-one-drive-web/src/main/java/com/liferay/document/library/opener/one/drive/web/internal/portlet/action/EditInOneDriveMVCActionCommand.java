8ddb183c6bf04eb9e5bad071a2fb939f8f18e15b
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-07-22 12:24:05 +0200

    LPS-83411 Add action edit_in_office365.

---
+++ modules/dxp/apps/document-library-opener/document-library-opener-one-drive-web/src/main/java/com/liferay/document/library/opener/one/drive/web/internal/portlet/action/EditInOneDriveMVCActionCommand.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.document.library.opener.one.drive.web.internal.portlet.action;
+
+import com.liferay.document.library.constants.DLPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Cristina González
+ */
+@Component(
+	property = {
+		"auth.token.ignore.mvc.action=true",
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"mvc.command.name=/document_library/edit_in_office365"
+	},
+	service = MVCActionCommand.class
+)
+public class EditInOneDriveMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+	}
+
+}
