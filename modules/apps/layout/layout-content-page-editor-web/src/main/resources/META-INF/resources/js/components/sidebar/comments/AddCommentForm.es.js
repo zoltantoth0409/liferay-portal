92a9a5937a7f7e4064f9cc9d59329694ed220e0f
Pablo Molina <contact@pablomolina.me>
2019-07-22 12:20:04 +0200

    LPS-98488 Move comment form UI to CommentForm

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/sidebar/comments/AddCommentForm.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/sidebar/comments/AddCommentForm.es.js
@@ -12,20 +12,15 @@
  * details.
  */
 
-import ClayButton from '@clayui/button';
 import PropTypes from 'prop-types';
 import React, {useState} from 'react';
 
 import {addFragmentEntryLinkComment} from '../../../utils/FragmentsEditorFetchUtils.es';
 import {addFragmentEntryLinkCommentAction} from '../../../actions/addFragmentEntryLinkComment.es';
-import Button from '../../common/Button.es';
+import CommentForm from './CommentForm.es';
 import {getConnectedReactComponent} from '../../../store/ConnectedComponent.es';
-import InvisibleFieldset from '../../common/InvisibleFieldset.es';
-import Textarea from '../../common/Textarea.es';
 
 const AddCommentForm = props => {
-	const newCommentId = `${props.portletNamespace}newComment`;
-
 	const [addingComment, setAddingComment] = useState(false);
 	const [showButtons, setShowButtons] = useState(false);
 	const [textareaContent, setTextareaContent] = useState('');
@@ -60,60 +55,26 @@ const AddCommentForm = props => {
 	};
 
 	return (
-		<form onFocus={_handleFormFocus}>
-			<InvisibleFieldset disabled={addingComment}>
-				<div className="form-group form-group-sm">
-					<label className="sr-only" htmlFor={newCommentId}>
-						{Liferay.Language.get('add-comment')}
-					</label>
-
-					<Textarea
-						id={newCommentId}
-						onChange={_handleTextareaChange}
-						placeholder={Liferay.Language.get(
-							'type-your-comment-here'
-						)}
-						value={textareaContent}
-					/>
-				</div>
-
-				{showButtons && (
-					<ClayButton.Group spaced>
-						<Button
-							disabled={!textareaContent}
-							displayType="primary"
-							loading={addingComment}
-							onClick={_handleCommentButtonClick}
-							small
-						>
-							{Liferay.Language.get('comment')}
-						</Button>
-
-						<Button
-							displayType="secondary"
-							onClick={_handleCancelButtonClick}
-							small
-							type="button"
-						>
-							{Liferay.Language.get('cancel')}
-						</Button>
-					</ClayButton.Group>
-				)}
-			</InvisibleFieldset>
-		</form>
+		<CommentForm
+			loading={addingComment}
+			onCancelButtonClick={_handleCancelButtonClick}
+			onFormFocus={_handleFormFocus}
+			onSubmitButtonClick={_handleCommentButtonClick}
+			onTextareaChange={_handleTextareaChange}
+			showButtons={showButtons}
+			submitButtonLabel={Liferay.Language.get('comment')}
+			textareaContent={textareaContent}
+		/>
 	);
 };
 
 AddCommentForm.propTypes = {
 	addComment: PropTypes.func,
-	fragmentEntryLinkId: PropTypes.string.isRequired,
-	portletNamespace: PropTypes.string
+	fragmentEntryLinkId: PropTypes.string.isRequired
 };
 
 const ConnectedAddCommentForm = getConnectedReactComponent(
-	state => ({
-		portletNamespace: state.portletNamespace
-	}),
+	() => ({}),
 	dispatch => ({
 		addComment: (fragmentEntryLinkId, comment) =>
 			dispatch(
