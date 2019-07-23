789d98c5c3e7f3f97fba108b218564cfc22825a2
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2019-07-23 10:43:32 -0300

    LPS-97933 add search

---
+++ modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/internal/search/spi/model/index/contributor/DEDataListViewModelIndexerWriterContributor.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.internal.search.spi.model.index.contributor;
+
+import com.liferay.data.engine.model.DEDataListView;
+import com.liferay.data.engine.service.DEDataListViewLocalService;
+import com.liferay.portal.search.batch.BatchIndexingActionable;
+import com.liferay.portal.search.batch.DynamicQueryBatchIndexingActionableFactory;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.index.contributor.helper.ModelIndexerWriterDocumentHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jeyvison Nascimento
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.data.engine.model.DEDataListView",
+	service = ModelIndexerWriterContributor.class
+)
+public class DEDataListViewModelIndexerWriterContributor
+	implements ModelIndexerWriterContributor<DEDataListView> {
+
+	@Override
+	public void customize(
+		BatchIndexingActionable batchIndexingActionable,
+		ModelIndexerWriterDocumentHelper modelIndexerWriterDocumentHelper) {
+
+		batchIndexingActionable.setPerformActionMethod(
+			(DEDataListView deDataListView) ->
+				batchIndexingActionable.addDocuments(
+					modelIndexerWriterDocumentHelper.getDocument(
+						deDataListView)));
+	}
+
+	@Override
+	public BatchIndexingActionable getBatchIndexingActionable() {
+		return _dynamicQueryBatchIndexingActionableFactory.
+			getBatchIndexingActionable(
+				_deDataListViewLocalService.
+					getIndexableActionableDynamicQuery());
+	}
+
+	@Override
+	public long getCompanyId(DEDataListView deDataListView) {
+		return deDataListView.getCompanyId();
+	}
+
+	@Reference
+	private DEDataListViewLocalService _deDataListViewLocalService;
+
+	@Reference
+	private DynamicQueryBatchIndexingActionableFactory
+		_dynamicQueryBatchIndexingActionableFactory;
+
+}
---
+++ modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/internal/search/spi/model/query/contributor/DEDataListViewKeywordQueryContributor.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.internal.search.spi.model.query.contributor;
+
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.query.QueryHelper;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jeyvison Nascimento
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.data.engine.model.DEDataListView",
+	service = KeywordQueryContributor.class
+)
+public class DEDataListViewKeywordQueryContributor
+	implements KeywordQueryContributor {
+
+	@Override
+	public void contribute(
+		String keywords, BooleanQuery booleanQuery,
+		KeywordQueryContributorHelper keywordQueryContributorHelper) {
+
+		SearchContext searchContext =
+			keywordQueryContributorHelper.getSearchContext();
+
+		if (Validator.isNull(searchContext.getAttribute(Field.NAME))) {
+			return;
+		}
+
+		searchContext.setAttribute(
+			LocalizationUtil.getLocalizedName(
+				Field.NAME, searchContext.getLanguageId()),
+			searchContext.getAttribute(Field.NAME));
+
+		queryHelper.addSearchLocalizedTerm(
+			booleanQuery, keywordQueryContributorHelper.getSearchContext(),
+			Field.NAME, false);
+	}
+
+	@Reference
+	protected QueryHelper queryHelper;
+
+}
