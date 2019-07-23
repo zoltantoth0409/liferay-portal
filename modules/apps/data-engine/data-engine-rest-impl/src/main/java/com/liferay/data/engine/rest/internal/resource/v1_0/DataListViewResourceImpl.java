e31b8fcf3c61c7690cec3abc33648bad3fe3f929
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2019-07-23 19:15:14 -0300

    LPS-97933 Create get endpoint

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v1_0/DataListViewResourceImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v1_0/DataListViewResourceImpl.java
@@ -14,9 +14,35 @@
 
 package com.liferay.data.engine.rest.internal.resource.v1_0;
 
+import com.liferay.data.engine.field.type.util.LocalizedValueUtil;
+import com.liferay.data.engine.model.DEDataListView;
+import com.liferay.data.engine.rest.dto.v1_0.DataListView;
 import com.liferay.data.engine.rest.resource.v1_0.DataListViewResource;
+import com.liferay.data.engine.service.DEDataListViewLocalService;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.util.SearchUtil;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.BadRequestException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +53,98 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = DataListViewResource.class
 )
 public class DataListViewResourceImpl extends BaseDataListViewResourceImpl {
+
+	@Override
+	public Page<DataListView> getDataDefinitionDataListViewsPage(
+			Long dataDefinitionId, String keywords, Pagination pagination,
+			Sort[] sorts)
+		throws Exception {
+
+		if (pagination.getPageSize() > 250) {
+			throw new BadRequestException(
+				LanguageUtil.format(
+					contextAcceptLanguage.getPreferredLocale(),
+					"page-size-is-greater-than-x", 250));
+		}
+
+		if (ArrayUtil.isEmpty(sorts)) {
+			sorts = new Sort[] {
+				new Sort(
+					Field.getSortableFieldName(Field.MODIFIED_DATE),
+					Sort.STRING_TYPE, true)
+			};
+		}
+
+		DDMStructure ddmStructure = _ddmStructureLocalService.getStructure(
+			dataDefinitionId);
+
+		return SearchUtil.search(
+			booleanQuery -> {
+			},
+			null, DEDataListView.class, keywords, pagination,
+			queryConfig -> queryConfig.setSelectedFieldNames(
+				Field.ENTRY_CLASS_PK),
+			searchContext -> {
+				searchContext.setAttribute("ddmStructureId", dataDefinitionId);
+				searchContext.setAttribute(Field.DESCRIPTION, keywords);
+				searchContext.setAttribute(Field.NAME, keywords);
+				searchContext.setCompanyId(contextCompany.getCompanyId());
+				searchContext.setGroupIds(
+					new long[] {ddmStructure.getGroupId()});
+			},
+			document -> _toDataListView(
+				_deDataListViewLocalService.getDEDataListView(
+					GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK)))),
+			sorts);
+	}
+
+	private DataListView _toDataListView(DEDataListView deDataListView)
+		throws Exception {
+
+		return new DataListView() {
+			{
+				appliedFilters = _toMap(deDataListView.getAppliedFilters());
+				dataDefinitionId = deDataListView.getDdmStructureId();
+				dateCreated = deDataListView.getCreateDate();
+				dateModified = deDataListView.getModifiedDate();
+				fieldNames = JSONUtil.toStringArray(
+					_jsonFactory.createJSONArray(
+						deDataListView.getFieldNames()));
+				id = deDataListView.getPrimaryKey();
+				name = LocalizedValueUtil.toStringObjectMap(
+					deDataListView.getNameMap());
+				siteId = deDataListView.getGroupId();
+				sortField = deDataListView.getSortField();
+				userId = deDataListView.getUserId();
+			}
+		};
+	}
+
+	private Map<String, Object> _toMap(String json) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(json);
+
+		Set<String> keySet = jsonObject.keySet();
+
+		Iterator<String> iterator = keySet.iterator();
+
+		while (iterator.hasNext()) {
+			String key = iterator.next();
+
+			map.put(key, jsonObject.get(key));
+		}
+
+		return map;
+	}
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+	@Reference
+	private DEDataListViewLocalService _deDataListViewLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
 }
