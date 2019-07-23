d5dfee2aeac1b8ff9c6bcd6615350a72d82c884f
Carlos Lancha <carlos.lancha@liferay.com>
2019-07-23 12:28:43 +0200

    LPS-98496 Update liferay-npm-scripts and liferay-font-awesome versions, remove no longer needed gulp tasks from themes.

--- modules/apps/frontend-theme/frontend-theme-classic/gulpfile.js
+++ modules/apps/frontend-theme/frontend-theme-classic/gulpfile.js
@@ -1,24 +1,9 @@
 'use strict';
 
-const fs = require('fs-extra');
 const gulp = require('gulp');
-const path = require('path');
 
-const liferayFontAwesome = path.dirname(
-	require.resolve('liferay-font-awesome/package.json')
-);
 const liferayThemeTasks = require('liferay-theme-tasks');
 
 liferayThemeTasks.registerTasks({
-	gulp: gulp,
-	hookFn: function(gulp) {
-		gulp.hook('before:build:war', function(done) {
-			fs.copy(
-				path.join(liferayFontAwesome, 'font'),
-				path.join('build', 'font')
-			)
-				.then(done)
-				.catch(done);
-		});
-	}
+	gulp: gulp
 });
