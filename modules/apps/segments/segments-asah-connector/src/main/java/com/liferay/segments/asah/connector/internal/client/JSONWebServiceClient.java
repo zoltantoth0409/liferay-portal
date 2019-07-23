f37dd0c30421d9127dd7695f970b07c63660de05
David Arques <davidarchena@gmail.com>
2019-07-23 16:14:32 +0200

    LPS-98592 Adapt PUT operation to return void as in AC

--- modules/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/client/JSONWebServiceClient.java
+++ modules/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/client/JSONWebServiceClient.java
@@ -30,8 +30,7 @@ public interface JSONWebServiceClient {
 	public <T> T doPost(
 		Class<T> clazz, String url, T object, Map<String, String> headers);
 
-	public <T> T doPut(
-		Class<T> clazz, String url, T object, Map<String, String> headers);
+	public <T> void doPut(String url, T object, Map<String, String> headers);
 
 	public String getBaseURI();
 
