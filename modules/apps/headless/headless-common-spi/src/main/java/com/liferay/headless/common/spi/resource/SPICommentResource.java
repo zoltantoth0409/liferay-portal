249c40330ab426ad87858a53b5ccd2d3168aedfc
Brian Chan <brian.chan@liferay.com>
2019-07-24 11:01:50 -0700

    LPS-98602 wordsmith

--- modules/apps/headless/headless-common-spi/src/main/java/com/liferay/headless/common/spi/resource/SPICommentResource.java
+++ modules/apps/headless/headless-common-spi/src/main/java/com/liferay/headless/common/spi/resource/SPICommentResource.java
@@ -77,7 +77,7 @@ public class SPICommentResource<T> {
 
 		if (comment == null) {
 			throw new NoSuchModelException(
-				"No Comment exists with the primary key " + commentId);
+				"No comment exists with comment ID " + commentId);
 		}
 
 		_checkViewPermission(comment);
