43e2b47b0c27dc88d2fa82bde3698cf13a4c7b60
hai.yu <8219754@liferay.com>
2019-07-24 17:57:52 +0800

    LPS-98626 Assign user created in sub test class to another field so that any user created can be removed

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/subscriptions/test/JournalSubscriptionLocalizedContentTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/subscriptions/test/JournalSubscriptionLocalizedContentTest.java
@@ -21,9 +21,11 @@ import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.service.JournalArticleLocalServiceUtil;
 import com.liferay.journal.service.JournalFolderLocalServiceUtil;
 import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.portal.kernel.service.PortletPreferencesLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
@@ -58,7 +60,7 @@ public class JournalSubscriptionLocalizedContentTest
 	public void setUp() throws Exception {
 		super.setUp();
 
-		user = UserTestUtil.addOmniAdminUser();
+		_user = UserTestUtil.addOmniAdminUser();
 	}
 
 	@Override
@@ -76,7 +78,7 @@ public class JournalSubscriptionLocalizedContentTest
 		throws Exception {
 
 		JournalFolderLocalServiceUtil.subscribe(
-			user.getUserId(), group.getGroupId(), containerModelId);
+			_user.getUserId(), group.getGroupId(), containerModelId);
 	}
 
 	@Override
@@ -130,5 +132,8 @@ public class JournalSubscriptionLocalizedContentTest
 
 		JournalTestUtil.updateArticleWithWorkflow(userId, article, true);
 	}
+
+	@DeleteAfterTestRun
+	private User _user;
 
 }
