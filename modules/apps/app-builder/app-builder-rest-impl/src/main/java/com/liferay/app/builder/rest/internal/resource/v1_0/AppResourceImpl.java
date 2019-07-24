5ea0034bd43b5afa354616ec2bd9e109a28c7e02
Gabriel Albuquerque <gwas@cin.ufpe.br>
2019-07-24 18:45:02 -0300

    LPS-97944 Implement post App method

--- modules/apps/app-builder/app-builder-rest-impl/src/main/java/com/liferay/app/builder/rest/internal/resource/v1_0/AppResourceImpl.java
+++ modules/apps/app-builder/app-builder-rest-impl/src/main/java/com/liferay/app/builder/rest/internal/resource/v1_0/AppResourceImpl.java
@@ -14,9 +14,21 @@
 
 package com.liferay.app.builder.rest.internal.resource.v1_0;
 
+import com.liferay.app.builder.model.AppBuilderApp;
+import com.liferay.app.builder.rest.dto.v1_0.App;
 import com.liferay.app.builder.rest.resource.v1_0.AppResource;
+import com.liferay.app.builder.service.AppBuilderAppLocalService;
+import com.liferay.data.engine.field.type.util.LocalizedValueUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +39,61 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = AppResource.class
 )
 public class AppResourceImpl extends BaseAppResourceImpl {
+
+	@Override
+	public App postDataDefinitionApp(Long dataDefinitionId, App app)
+		throws Exception {
+
+		return _toApp(
+			_appBuilderAppLocalService.addAppBuilderApp(
+				app.getSiteId(), contextCompany.getCompanyId(), app.getUserId(),
+				dataDefinitionId, app.getDataLayoutId(),
+				app.getDataListViewId(),
+				LocalizedValueUtil.toLocaleStringMap(app.getName()),
+				_toJSON(app.getSettings())));
+	}
+
+	private App _toApp(AppBuilderApp appBuilderApp) throws Exception {
+		return new App() {
+			{
+				dataDefinitionId = appBuilderApp.getDdmStructureId();
+				dataLayoutId = appBuilderApp.getDdmStructureLayoutId();
+				dataListViewId = appBuilderApp.getDeDataListViewId();
+				dateCreated = appBuilderApp.getCreateDate();
+				dateModified = appBuilderApp.getModifiedDate();
+				id = appBuilderApp.getAppBuilderAppId();
+				name = LocalizedValueUtil.toStringObjectMap(
+					appBuilderApp.getNameMap());
+				settings = _toSettings(appBuilderApp.getSettings());
+				siteId = appBuilderApp.getGroupId();
+				userId = appBuilderApp.getUserId();
+			}
+		};
+	}
+
+	private String _toJSON(Map<String, Object> settings) {
+		return JSONUtil.put(
+			"deploymentStatus", settings.get("deploymentStatus")
+		).put(
+			"deploymentTypes", settings.get("deploymentTypes")
+		).toString();
+	}
+
+	private Map<String, Object> _toSettings(String settings) throws Exception {
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(settings);
+
+		return new HashMap<String, Object>() {
+			{
+				put("deploymentStatus", jsonObject.get("deploymentStatus"));
+				put(
+					"deploymentTypes",
+					JSONUtil.toObjectList(
+						(JSONArray)jsonObject.get("deploymentTypes")));
+			}
+		};
+	}
+
+	@Reference
+	private AppBuilderAppLocalService _appBuilderAppLocalService;
+
 }
