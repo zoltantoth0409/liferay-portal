f556375a91724cd37e4a99761c5b3e72137c7647
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-07-25 19:45:21 +0200

    LPS-98042 Extract to util class

---
+++ portal-kernel/src/com/liferay/portal/kernel/service/util/BaseServiceHelperUtil.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.service.util;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.BaseServiceImpl;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class BaseServiceHelperUtil {
+
+	public static User getGuestOrUser() throws PortalException {
+		User user = getUser(getUserId());
+
+		return getGuestOrUser(user);
+	}
+
+	public static User getGuestOrUser(User user) throws PortalException {
+		try {
+			return getUser(user.getUserId());
+		}
+		catch (PrincipalException pe) {
+			try {
+				return UserLocalServiceUtil.getDefaultUser(
+					CompanyThreadLocal.getCompanyId());
+			}
+			catch (Exception e) {
+				throw pe;
+			}
+		}
+	}
+
+	public static User getUser(long userId) throws PortalException {
+		return UserLocalServiceUtil.getUserById(userId);
+	}
+
+	public static long getUserId() throws PrincipalException {
+		String name = PrincipalThreadLocal.getName();
+
+		if (Validator.isNull(name)) {
+			throw new PrincipalException("Principal is null");
+		}
+
+		for (String anonymousName : BaseServiceImpl.ANONYMOUS_NAMES) {
+			if (StringUtil.equalsIgnoreCase(name, anonymousName)) {
+				throw new PrincipalException(
+					"Principal cannot be " + anonymousName);
+			}
+		}
+
+		return GetterUtil.getLong(name);
+	}
+
+}
