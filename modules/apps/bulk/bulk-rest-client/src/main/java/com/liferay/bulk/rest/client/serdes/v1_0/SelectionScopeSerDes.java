5d2f0bf2b5514c272a81108871d7a79956daed9b
Brian Chan <brian.chan@liferay.com>
2019-07-25 16:13:31 -0700

    LPS-98787 regen

--- modules/apps/bulk/bulk-rest-client/src/main/java/com/liferay/bulk/rest/client/serdes/v1_0/SelectionScopeSerDes.java
+++ modules/apps/bulk/bulk-rest-client/src/main/java/com/liferay/bulk/rest/client/serdes/v1_0/SelectionScopeSerDes.java
@@ -154,7 +154,14 @@ public class SelectionScopeSerDes {
 			sb.append(entry.getKey());
 			sb.append("\":");
 			sb.append("\"");
-			sb.append(entry.getValue());
+
+			if (entry.getValue() instanceof String) {
+				sb.append(_escape(entry.getValue()));
+			}
+			else {
+				sb.append(entry.getValue());
+			}
+
 			sb.append("\"");
 
 			if (iterator.hasNext()) {
--- modules/apps/data-engine/data-engine-rest-client/src/main/java/com/liferay/data/engine/rest/client/serdes/v1_0/DataLayoutPermissionSerDes.java
+++ modules/apps/data-engine/data-engine-rest-client/src/main/java/com/liferay/data/engine/rest/client/serdes/v1_0/DataLayoutPermissionSerDes.java
@@ -227,7 +227,14 @@ public class DataLayoutPermissionSerDes {
 			sb.append(entry.getKey());
 			sb.append("\":");
 			sb.append("\"");
-			sb.append(entry.getValue());
+
+			if (entry.getValue() instanceof String) {
+				sb.append(_escape(entry.getValue()));
+			}
+			else {
+				sb.append(entry.getValue());
+			}
+
 			sb.append("\"");
 
 			if (iterator.hasNext()) {
