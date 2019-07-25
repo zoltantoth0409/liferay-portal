151a7ca13f40dea2628523fefcbaec82e36fe465
Inácio Nery <inacio.nery@liferay.com>
2019-07-25 15:34:24 -0300

    LPS-98776 Check if the user name has changed.

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/model/listener/UserModelListener.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/model/listener/UserModelListener.java
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.util.Portal;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -50,6 +51,12 @@ public class UserModelListener extends BaseModelListener<User> {
 				return;
 			}
 
+			String name = calendarResource.getName(LocaleUtil.getSiteDefault());
+
+			if (Objects.equals(name, user.getFullName())) {
+				return;
+			}
+			
 			Map<Locale, String> nameMap = new HashMap<>();
 
 			nameMap.put(LocaleUtil.getSiteDefault(), user.getFullName());
