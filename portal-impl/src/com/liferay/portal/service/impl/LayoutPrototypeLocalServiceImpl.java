c9c62f34ab7bad62b468eb38a305615c7e3cca24
Jose Maria Muñoz <josemaria.munoz@liferay.com>
2019-07-26 10:47:42 +0200

    LPS-97745 Adding error message in case of exception when trying to deleteLayoutPrototype

--- portal-impl/src/com/liferay/portal/service/impl/LayoutPrototypeLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutPrototypeLocalServiceImpl.java
@@ -18,6 +18,8 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.NoSuchLayoutPrototypeException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.RequiredLayoutPrototypeException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Layout;
@@ -32,6 +34,7 @@ import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.service.base.LayoutPrototypeLocalServiceBaseImpl;
 
 import java.util.Date;
@@ -149,6 +152,16 @@ public class LayoutPrototypeLocalServiceImpl
 			(layoutPersistence.countByC_L(
 				layoutPrototype.getCompanyId(), layoutPrototype.getUuid()) >
 					0)) {
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("Delete not performed because exists some Layout");
+			sb.append("with companyId ");
+			sb.append(layoutPrototype.getCompanyId());
+			sb.append(" and layoutPrototypeUuid ");
+			sb.append(layoutPrototype.getUuid());
+
+			_log.error(sb.toString());
 
 			throw new RequiredLayoutPrototypeException();
 		}
@@ -331,5 +344,8 @@ public class LayoutPrototypeLocalServiceImpl
 		return updateLayoutPrototype(
 			layoutPrototypeId, nameMap, descriptionMap, active, null);
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+			LayoutPrototypeLocalServiceImpl.class);
 
 }
