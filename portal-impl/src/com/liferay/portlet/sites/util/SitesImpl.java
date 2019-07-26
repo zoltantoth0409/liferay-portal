b36691b63748f68a4a4bd91da751e05e72b6a756
Minhchau <minhchau.dang@liferay.com>
2019-07-26 15:11:51 -0700

    LPS-95817 Call clearLocalCache and re-fetch

--- portal-impl/src/com/liferay/portlet/sites/util/SitesImpl.java
+++ portal-impl/src/com/liferay/portlet/sites/util/SitesImpl.java
@@ -29,6 +29,7 @@ import com.liferay.portal.events.EventsProcessorUtil;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTask;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskManagerUtil;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskThreadLocal;
+import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.RequiredLayoutException;
 import com.liferay.portal.kernel.language.LanguageUtil;
@@ -1330,6 +1331,11 @@ public class SitesImpl implements Sites {
 			return;
 		}
 
+		EntityCacheUtil.clearLocalCache();
+
+		layoutSet = LayoutSetLocalServiceUtil.fetchLayoutSet(
+			layoutSet.getLayoutSetId());
+
 		UnicodeProperties settingsProperties =
 			layoutSet.getSettingsProperties();
 
@@ -1733,6 +1739,10 @@ public class SitesImpl implements Sites {
 		if (owner == null) {
 			return;
 		}
+
+		EntityCacheUtil.clearLocalCache();
+
+		layout = LayoutLocalServiceUtil.fetchLayout(layout.getPlid());
 
 		try {
 			MergeLayoutPrototypesThreadLocal.setInProgress(true);
