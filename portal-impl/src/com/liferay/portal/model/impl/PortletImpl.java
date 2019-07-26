0a3da8ebb6a9acd2344cd49b7912e053fd371358
Eric Yan <eric.yan@liferay.com>
2019-07-25 17:33:06 -0700

    LPS-98789 portal-impl: Avoid NPE

--- portal-impl/src/com/liferay/portal/model/impl/PortletImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/PortletImpl.java
@@ -2127,6 +2127,10 @@ public class PortletImpl extends PortletBaseImpl {
 	public URLEncoder getURLEncoderInstance() {
 		PortletBag portletBag = PortletBagPool.get(getRootPortletId());
 
+		if (portletBag == null) {
+			return null;
+		}
+
 		List<URLEncoder> urlEncoderInstances =
 			portletBag.getURLEncoderInstances();
 
