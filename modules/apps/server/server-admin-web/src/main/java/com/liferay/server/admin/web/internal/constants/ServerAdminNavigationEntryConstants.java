9476c2a965ae568fed5db108f6a97230a11a074e
alee8888 <albert.lee@liferay.com>
2019-07-26 14:44:13 -0700

    LPS-78524 Create Server Admin Navigation Entry Constants class

---
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/constants/ServerAdminNavigationEntryConstants.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.server.admin.web.internal.constants;
+
+/**
+ * @author Albert Lee
+ */
+public class ServerAdminNavigationEntryConstants {
+
+	public static final String CATEGORY_KEY_PORTAL_PROPERTIES =
+		"portal-properties";
+
+	public static final String CATEGORY_KEY_SYSTEM_PROPERTIES =
+		"system-properties";
+
+	public static final String ENTRY_KEY_PORTAL_PROPERTIES =
+		"portal-properties";
+
+	public static final String ENTRY_KEY_SYSTEM_PROPERTIES =
+		"system-properties";
+
+	public static final String SCREEN_NAVIGATION_KEY_PROPERTIES =
+		"properties.form";
+
+}
