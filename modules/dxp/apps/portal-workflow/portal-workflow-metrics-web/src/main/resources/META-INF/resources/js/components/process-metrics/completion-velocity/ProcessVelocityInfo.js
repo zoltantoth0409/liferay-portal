30c9599c745ac1a57603796694099594af6f6251
Paulo Cruz <paulo.cruz@liferay.com>
2019-07-26 09:24:08 -0300

    LPS-98772 Fix velocity value format

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-metrics/completion-velocity/ProcessVelocityInfo.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-metrics/completion-velocity/ProcessVelocityInfo.js
@@ -1,17 +1,19 @@
 import React, {useContext} from 'react';
+import {formatNumber} from 'shared/util/numeral';
 import {VelocityDataContext} from './store/VelocityDataStore';
 import {VelocityUnitContext} from './store/VelocityUnitStore';
 
 function ProcessVelocityInfo() {
-	const {velocityData} = useContext(VelocityDataContext);
 	const {getSelectedVelocityUnit} = useContext(VelocityUnitContext);
+	const {velocityData = {}} = useContext(VelocityDataContext);
 
+	const formattedValue = formatNumber(velocityData.value, '0[.]00');
 	const selectedVelocityUnit = getSelectedVelocityUnit() || {};
 
 	return (
 		velocityData && (
 			<div className="pb-2">
-				<span className="velocity-value">{velocityData.value}</span>
+				<span className="velocity-value">{formattedValue}</span>
 				<span className="velocity-unit">
 					{selectedVelocityUnit.name}
 				</span>
