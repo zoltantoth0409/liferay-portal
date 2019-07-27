f16c39195dd1579a6c68f44971bd52f02bd73ce2
Javier Gamarra <nhpatt@gmail.com>
2019-07-27 15:27:59 +0000

    LPS-97900 implement attachment endpoints

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/WikiPageAttachmentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/WikiPageAttachmentResourceTest.java
@@ -15,15 +15,116 @@
 package com.liferay.headless.delivery.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.delivery.client.dto.v1_0.WikiPageAttachment;
+import com.liferay.headless.delivery.client.http.HttpInvoker;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.wiki.model.WikiNode;
+import com.liferay.wiki.model.WikiPage;
+import com.liferay.wiki.service.WikiNodeLocalServiceUtil;
+import com.liferay.wiki.service.WikiPageLocalServiceUtil;
 
-import org.junit.Ignore;
+import java.io.File;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class WikiPageAttachmentResourceTest
 	extends BaseWikiPageAttachmentResourceTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setScopeGroupId(testGroup.getGroupId());
+
+		long defaultUserId = UserLocalServiceUtil.getDefaultUserId(
+			testGroup.getCompanyId());
+
+		WikiNode wikiNode = WikiNodeLocalServiceUtil.addNode(
+			defaultUserId, RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), serviceContext);
+
+		_wikiPage = WikiPageLocalServiceUtil.addPage(
+			defaultUserId, wikiNode.getNodeId(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(), false,
+			serviceContext);
+	}
+
+	@Override
+	protected void assertValid(
+			WikiPageAttachment wikiPageAttachment,
+			Map<String, File> multipartFiles)
+		throws Exception {
+
+		Assert.assertEquals(
+			new String(FileUtil.getBytes(multipartFiles.get("file"))),
+			_read(
+				"http://localhost:8080" + wikiPageAttachment.getContentUrl()));
+	}
+
+	@Override
+	protected Map<String, File> getMultipartFiles() throws Exception {
+		Map<String, File> files = new HashMap<>();
+
+		String randomString = RandomTestUtil.randomString();
+
+		files.put("file", FileUtil.createTempFile(randomString.getBytes()));
+
+		return files;
+	}
+
+	@Override
+	protected WikiPageAttachment
+			testDeleteWikiPageAttachment_addWikiPageAttachment()
+		throws Exception {
+
+		return wikiPageAttachmentResource.postWikiPageWikiPageAttachment(
+			_wikiPage.getPageId(), randomWikiPageAttachment(),
+			getMultipartFiles());
+	}
+
+	@Override
+	protected WikiPageAttachment
+			testGetWikiPageAttachment_addWikiPageAttachment()
+		throws Exception {
+
+		return wikiPageAttachmentResource.postWikiPageWikiPageAttachment(
+			_wikiPage.getPageId(), randomWikiPageAttachment(),
+			getMultipartFiles());
+	}
+
+	@Override
+	protected Long testGetWikiPageWikiPageAttachmentsPage_getWikiPageId() {
+		return _wikiPage.getPageId();
+	}
+
+	private String _read(String url) throws Exception {
+		HttpInvoker httpInvoker = HttpInvoker.newHttpInvoker();
+
+		httpInvoker.httpMethod(HttpInvoker.HttpMethod.GET);
+		httpInvoker.path(url);
+		httpInvoker.userNameAndPassword("test@liferay.com:test");
+
+		HttpInvoker.HttpResponse httpResponse = httpInvoker.invoke();
+
+		return httpResponse.getContent();
+	}
+
+	private WikiPage _wikiPage;
+
 }
