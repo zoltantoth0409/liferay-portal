bd4bffdc77b3cf1e73c3ee041fb6ed561e418aa4
Javier Gamarra <nhpatt@gmail.com>
2019-07-27 17:14:16 +0200

    LPS-98869 use multipart parts if server has already parsed them

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/util/MultipartUtil.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.jaxrs.util;
+
+import javax.servlet.http.Part;
+
+import javax.ws.rs.core.HttpHeaders;
+
+/**
+ * @author Javier Gamarra
+ */
+public class MultipartUtil {
+
+	public static String getFileName(Part part) {
+		String header = part.getHeader(HttpHeaders.CONTENT_DISPOSITION);
+
+		if (header == null) {
+			return part.getName();
+		}
+
+		String string = "filename=\"";
+
+		int index = header.indexOf(string);
+
+		if (index == -1) {
+			return null;
+		}
+
+		return header.substring(index + string.length(), header.length() - 1);
+	}
+
+}
