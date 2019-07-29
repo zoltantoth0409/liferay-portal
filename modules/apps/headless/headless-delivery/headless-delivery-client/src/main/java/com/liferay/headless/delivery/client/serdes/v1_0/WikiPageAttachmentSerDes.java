7403788b8f81b519650e8fa76ea6daef2978d8a6
Javier Gamarra <nhpatt@gmail.com>
2019-07-29 22:28:38 +0000

    LPS-98931 generate

--- modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/serdes/v1_0/WikiPageAttachmentSerDes.java
+++ modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/serdes/v1_0/WikiPageAttachmentSerDes.java
@@ -228,16 +228,38 @@ public class WikiPageAttachmentSerDes {
 			sb.append("\"");
 			sb.append(entry.getKey());
 			sb.append("\":");
-			sb.append("\"");
 
-			if (entry.getValue() instanceof String) {
+			Object value = entry.getValue();
+
+			Class valueClass = value.getClass();
+
+			if (value instanceof String) {
+				sb.append("\"");
 				sb.append(_escape(entry.getValue()));
+				sb.append("\"");
+			}
+			else if (valueClass.isArray()) {
+				Object[] values = (Object[])value;
+
+				sb.append("[");
+
+				for (int i = 0; i < values.length; i++) {
+					sb.append("\"");
+					sb.append(_escape(values[i]));
+					sb.append("\"");
+
+					if ((i + 1) < values.length) {
+						sb.append(", ");
+					}
+				}
+
+				sb.append("]");
 			}
 			else {
+				sb.append("\"");
 				sb.append(entry.getValue());
+				sb.append("\"");
 			}
-
-			sb.append("\"");
 
 			if (iterator.hasNext()) {
 				sb.append(",");
