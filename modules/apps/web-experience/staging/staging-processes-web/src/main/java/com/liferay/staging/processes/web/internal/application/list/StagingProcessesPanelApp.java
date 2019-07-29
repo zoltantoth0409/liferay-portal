0651a49965ac02f41e34e2a9532bd3f76d6046b7
Matthew Chan <matthew.chan@liferay.com>
2019-07-29 09:42:11 -0700

    LPS-98054 staging to be removed from personal and usergroup sites

--- modules/apps/web-experience/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/application/list/StagingProcessesPanelApp.java
+++ modules/apps/web-experience/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/application/list/StagingProcessesPanelApp.java
@@ -17,7 +17,10 @@ package com.liferay.staging.processes.web.internal.application.list;
 import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.staging.constants.StagingProcessesPortletKeys;
 
 import org.osgi.service.component.annotations.Component;
@@ -39,6 +42,17 @@ public class StagingProcessesPanelApp extends BasePanelApp {
 	@Override
 	public String getPortletId() {
 		return StagingProcessesPortletKeys.STAGING_PROCESSES;
+	}
+
+	@Override
+	public boolean isShow(PermissionChecker permissionChecker, Group group)
+		throws PortalException {
+
+		if (group.isUser() || group.isUserGroup()) {
+			return false;
+		}
+
+		return super.isShow(permissionChecker, group);
 	}
 
 	@Override
