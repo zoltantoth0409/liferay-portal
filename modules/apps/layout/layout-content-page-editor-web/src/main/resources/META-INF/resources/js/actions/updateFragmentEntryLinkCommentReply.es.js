993dc8f4553279fc97d68f1d5b4c649a690b576a
Pablo Molina <contact@pablomolina.me>
2019-07-29 16:03:03 +0200

    LPS-98891 Add updateFragmentEntryLinkCommentReply action

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/actions/updateFragmentEntryLinkCommentReply.es.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {UPDATE_FRAGMENT_ENTRY_LINK_COMMENT_REPLY} from './actions.es';
+
+/**
+ * Adds/updates a given comment reply
+ * @param {string} fragmentEntryLinkId
+ * @param {string} parentCommentId
+ * @param {object} comment
+ */
+const updateFragmentEntryLinkCommentReplyAction = (
+	fragmentEntryLinkId,
+	parentCommentId,
+	comment
+) => ({
+	comment,
+	fragmentEntryLinkId,
+	parentCommentId,
+	type: UPDATE_FRAGMENT_ENTRY_LINK_COMMENT_REPLY
+});
+
+export {updateFragmentEntryLinkCommentReplyAction};
