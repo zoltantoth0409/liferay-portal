629422a7423a9aa0ba9f4ab522d420a79a26bb36
Victor Galan <victor.galan@liferay.com>
2019-07-29 14:45:00 +0200

    LPS-98893 Add MappedContent component

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/sidebar/page_content/MappedContent.es.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayDropDown from '@clayui/drop-down';
+import ClayIcon from '@clayui/icon';
+import ClayLabel from '@clayui/label';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const MappedContent = props => {
+	const {label, style} = props.status;
+
+	return (
+		<li className="fragments-editor__mapped-content">
+			<div className="d-flex py-3 pl-2 flex-column">
+				<strong className="list-group-title truncate-text">
+					{props.title}
+				</strong>
+
+				<span className="text-secondary small">{props.name}</span>
+
+				<span className="text-secondary small">{props.usageLabel}</span>
+
+				<ClayLabel
+					className="align-self-start mt-2"
+					displayType={style}
+				>
+					{label}
+				</ClayLabel>
+			</div>
+		</li>
+	);
+};
+
+MappedContent.propTypes = {
+	name: PropTypes.string.isRequired,
+	status: PropTypes.shape({
+		label: PropTypes.string,
+		style: PropTypes.string
+	}),
+	title: PropTypes.string.isRequired,
+	usageLabel: PropTypes.string.isRequired
+};
+
+export {MappedContent};
+export default MappedContent;
