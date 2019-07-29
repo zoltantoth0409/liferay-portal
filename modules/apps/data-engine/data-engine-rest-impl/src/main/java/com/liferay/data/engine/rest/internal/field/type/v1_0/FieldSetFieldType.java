321c7504d215ce8a0dfb18efd954a0f0113d8f4b
marcelabc <mbc3@cin.ufpe.br>
2019-07-29 15:41:31 -0300

    LPS-98468 Add methods to serialize/deserialize properties in FieldSetFieldType

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/FieldSetFieldType.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/field/type/v1_0/FieldSetFieldType.java
@@ -16,10 +16,11 @@ package com.liferay.data.engine.rest.internal.field.type.v1_0;
 
 import com.liferay.data.engine.field.type.BaseFieldType;
 import com.liferay.data.engine.field.type.FieldType;
+import com.liferay.data.engine.field.type.FieldTypeTracker;
 import com.liferay.data.engine.rest.internal.field.type.v1_0.util.CustomPropertiesUtil;
 import com.liferay.data.engine.spi.dto.SPIDataDefinitionField;
+import com.liferay.portal.kernel.json.JSONObject;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -43,8 +44,41 @@ import org.osgi.service.component.annotations.Component;
 public class FieldSetFieldType extends BaseFieldType {
 
 	@Override
+	public SPIDataDefinitionField deserialize(
+			FieldTypeTracker fieldTypeTracker, JSONObject jsonObject)
+		throws Exception {
+
+		SPIDataDefinitionField spiDataDefinitionField = super.deserialize(
+			fieldTypeTracker, jsonObject);
+
+		Map<String, Object> customProperties =
+			spiDataDefinitionField.getCustomProperties();
+
+		customProperties.put(
+			"orientation", jsonObject.getString("orientation", "horizontal"));
+
+		return spiDataDefinitionField;
+	}
+
+	@Override
 	public String getName() {
 		return "fieldset";
+	}
+
+	@Override
+	public JSONObject toJSONObject(
+			FieldTypeTracker fieldTypeTracker,
+			SPIDataDefinitionField spiDataDefinitionField)
+		throws Exception {
+
+		JSONObject jsonObject = super.toJSONObject(
+			fieldTypeTracker, spiDataDefinitionField);
+
+		return jsonObject.put(
+			"orientation",
+			CustomPropertiesUtil.getString(
+				spiDataDefinitionField.getCustomProperties(), "orientation",
+				"horizontal"));
 	}
 
 	@Override
