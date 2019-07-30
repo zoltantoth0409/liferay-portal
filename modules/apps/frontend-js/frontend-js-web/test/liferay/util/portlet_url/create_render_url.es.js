432cc7d4c4116a4a865dff445de6508297b49dfb
Krešimir Čoko <kresimir.coko@liferay.com>
2019-07-30 14:32:37 +0200

    LPS-94714 Mock Liferay.ThemeDisplay.getPortalURL in the other test files too LPS-94714 Add the same adjustment to the modern variant of the utility, adjust tests and add one test for the added behavior LPS-94714 Adjust to accept URLs without the starting part, and make parameter setting more robust by not assigning reserved parameters that are null LPS-94714 Add case sensitivity to RegExp check in old PortletURL utility LPS-94714 Bring back old getPortletNamespace utility and have it call the new one LPS-94714 Import, implementation and tests for new Portlet URL API LPS-94714 Reduce required modules in the old PortletURL API LPS-94714 Implementation and tests for the new getPortletNamespace utility LPS-94714 Rewrite the old toString method in PortletURL API to use the new API LPS-94714 Delete the old getPortletNamespace utility

---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/portlet_url/create_render_url.es.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+'use strict';
+
+import createRenderURL from '../../../../src/main/resources/META-INF/resources/liferay/util/portlet_url/create_render_url.es';
+
+describe('Liferay.Util.PortletURL.createRenderURL', () => {
+	it('returns a url with the p_p_lifecycle parameter set to 0', () => {
+		Liferay = {
+			ThemeDisplay: {
+				getPortalURL: jest.fn(() => 'http://localhost:8080')
+			}
+		};
+
+		expect(
+			createRenderURL(
+				'http://localhost:8080/group/control_panel/manage?p_p_id=foo'
+			)
+		).toEqual(
+			'http://localhost:8080/group/control_panel/manage?p_p_id=foo&p_p_lifecycle=0'
+		);
+	});
+});
---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/portlet_url/create_url.es.js
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+'use strict';
+
+import createURL from '../../../../src/main/resources/META-INF/resources/liferay/util/portlet_url/create_url.es';
+
+describe('Liferay.Util.PortletURL.createURL', () => {
+	afterEach(() => {
+		Liferay.ThemeDisplay.getPortalURL.mockRestore();
+	});
+
+	beforeEach(() => {
+		Liferay = {
+			ThemeDisplay: {
+				getPortalURL: jest.fn(() => 'http://localhost:8080')
+			}
+		};
+	});
+
+	it('throws an error if basePortletURL is not a string', () => {
+		expect(() => createURL({portlet: 'url'}, {foo: 'bar'})).toThrow(
+			'basePortletURL parameter must be a string'
+		);
+	});
+
+	it('throws an error if parameters is not an object', () => {
+		expect(() =>
+			createURL(
+				'http://localhost:8080/group/control_panel/manage',
+				'foo:bar'
+			)
+		).toThrow('parameters argument must be an object');
+	});
+
+	it('throws an error if parameters are provided but portlet ID is null', () => {
+		expect(() =>
+			createURL('http://localhost:8080/group/control_panel/manage', {
+				foo: 'bar'
+			})
+		).toThrow('Portlet ID must not be null if parameters are provided');
+	});
+
+	it('returns a string containing base url and parameters, where reserved parameters are not namespaced', () => {
+		expect(
+			createURL(
+				'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet',
+				{
+					doAsGroupId: 'fooBar',
+					foo: 'bar'
+				}
+			)
+		).toEqual(
+			'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet&doAsGroupId=fooBar&_com_liferay_roles_admin_web_portlet_RolesAdminPortlet_foo=bar'
+		);
+	});
+
+	it('overwrites a parameter in base url if the same parameter is set in the parameters argument', () => {
+		expect(
+			createURL(
+				'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet&doAsGroupId=fooBar',
+				{
+					doAsGroupId: 'barBaz',
+					foo: 'bar'
+				}
+			)
+		).toEqual(
+			'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet&doAsGroupId=barBaz&_com_liferay_roles_admin_web_portlet_RolesAdminPortlet_foo=bar'
+		);
+	});
+
+	it('prepends portal url if the provided base url does not have a proper beginning', () => {
+		expect(createURL('/group/control_panel/manage')).toEqual(
+			'http://localhost:8080/group/control_panel/manage'
+		);
+	});
+});
