b6eb036ae599f7c48346dcb4f07d52ccac606cee
Brian Chan <brian.chan@liferay.com>
2019-07-29 23:16:59 -0700

    LPS-98587 Sort

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -540,11 +540,11 @@ public class ContentPageEditorDisplayContext {
 			String className = PortalUtil.getClassName(classNameId);
 
 			soyContext.put(
-				"href", _getAssetBrowserURL(className)
-			).put(
 				"className", className
 			).put(
 				"classNameId", classNameId
+			).put(
+				"href", _getAssetBrowserURL(className)
 			).put(
 				"typeName",
 				ResourceActionsUtil.getModelResource(
