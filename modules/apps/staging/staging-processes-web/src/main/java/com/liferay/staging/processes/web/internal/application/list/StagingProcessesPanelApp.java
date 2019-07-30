d3fc06cb3e803d13a15dcbc40fb2cc43caf5b5f3
Tamas Molnar <tamas.molnar@liferay.com>
2019-07-30 10:35:54 +0200

    LPS-98054 We don't need to check twice

--- modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/application/list/StagingProcessesPanelApp.java
+++ modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/application/list/StagingProcessesPanelApp.java
@@ -17,10 +17,7 @@ package com.liferay.staging.processes.web.internal.application.list;
 import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
-import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.staging.constants.StagingProcessesPortletKeys;
 
 import org.osgi.service.component.annotations.Component;
@@ -42,17 +39,6 @@ public class StagingProcessesPanelApp extends BasePanelApp {
 	@Override
 	public String getPortletId() {
 		return StagingProcessesPortletKeys.STAGING_PROCESSES;
-	}
-
-	@Override
-	public boolean isShow(PermissionChecker permissionChecker, Group group)
-		throws PortalException {
-
-		if (group.isUser() || group.isUserGroup()) {
-			return false;
-		}
-
-		return super.isShow(permissionChecker, group);
 	}
 
 	@Override
