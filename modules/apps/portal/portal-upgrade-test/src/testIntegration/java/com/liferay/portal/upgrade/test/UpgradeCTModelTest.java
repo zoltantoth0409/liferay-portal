ed651f35a58231ebede839de1026e68f6689fe3d
Preston-Crary <preston.crary@liferay.com>
2019-07-30 18:50:12 -0700

    LPS-100265 General upgrade path for CTModels with test coverage.

---
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/UpgradeCTModelTest.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.upgrade.UpgradeCTModel;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Types;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Preston Crary
+ */
+@RunWith(Arquillian.class)
+public class UpgradeCTModelTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_db = DBManagerUtil.getDB();
+
+		_db.runSQL(
+			StringBundler.concat(
+				"create table UpgradeCTModelTest (mvccVersion LONG default 0 ",
+				"not null, uuid_ VARCHAR(75) null, upgradeCTModelId LONG not ",
+				"null primary key, companyId LONG, createDate DATE null, ",
+				"modifiedDate DATE null, name STRING null);"));
+
+		_db.runSQL(
+			"insert into UpgradeCTModelTest values (0, 'uuid', 1, 2, NULL, " +
+				"NULL, 'name');");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_db.runSQL(TestTableClass.TABLE_SQL_DROP);
+	}
+
+	@Test
+	public void testUpgrade() throws Exception {
+		UpgradeCTModel upgradeCTModel = new UpgradeCTModel(
+			TestTableClass.class);
+
+		upgradeCTModel.upgrade();
+
+		try (Connection connection = DataAccess.getConnection();
+			PreparedStatement ps = connection.prepareStatement(
+				"select * from UpgradeCTModelTest");
+			ResultSet rs = ps.executeQuery()) {
+
+			Assert.assertTrue(rs.next());
+
+			Assert.assertEquals(0, rs.getLong("mvccVersion"));
+			Assert.assertEquals("uuid", rs.getString("uuid_"));
+			Assert.assertEquals(1, rs.getLong("upgradeCTModelId"));
+			Assert.assertEquals(0, rs.getLong("ctCollectionId"));
+			Assert.assertEquals(2, rs.getLong("companyId"));
+			Assert.assertNull(rs.getTimestamp("createDate"));
+			Assert.assertNull(rs.getTimestamp("modifiedDate"));
+			Assert.assertEquals("name", rs.getString("name"));
+
+			Assert.assertFalse(rs.next());
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public static class TestTableClass {
+
+		public static final Object[][] TABLE_COLUMNS = {
+			{"mvccVersion", Types.BIGINT}, {"uuid_", Types.VARCHAR},
+			{"upgradeCTModelId", Types.BIGINT},
+			{"ctCollectionId", Types.BIGINT}, {"companyId", Types.BIGINT},
+			{"createDate", Types.TIMESTAMP}, {"modifiedDate", Types.TIMESTAMP},
+			{"name", Types.VARCHAR}
+		};
+
+		public static final String TABLE_NAME = "UpgradeCTModelTest";
+
+		public static final String[] TABLE_SQL_ADD_INDEXES = {
+			"create index IX_TEST on UpgradeCTModelTest " +
+				"(uuid_[$COLUMN_LENGTH:75$], companyId)"
+		};
+
+		public static final String TABLE_SQL_CREATE = StringBundler.concat(
+			"create table UpgradeCTModelTest (mvccVersion LONG default 0 not ",
+			"null, uuid_ VARCHAR(75) null, upgradeCTModelId LONG not null, ",
+			"ctCollectionId LONG not null, companyId LONG, createDate DATE ",
+			"null, modifiedDate DATE null, name STRING null, primary key ",
+			"(upgradeCTModelId, ctCollectionId))");
+
+		public static final String TABLE_SQL_DROP =
+			"drop table UpgradeCTModelTest";
+
+	}
+
+	private DB _db;
+
+}
---
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/UpgradeCTModel.java
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.upgrade;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.db.DBInspector;
+
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+
+import java.util.Collection;
+
+/**
+ * @author Preston Crary
+ */
+public class UpgradeCTModel extends UpgradeProcess {
+
+	public UpgradeCTModel(Class<?> tableClass) {
+		_tableClass = tableClass;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+		DBInspector dbInspector = new DBInspector(connection);
+
+		String tableName = dbInspector.normalizeName(
+			getTableName(_tableClass), databaseMetaData);
+
+		String primaryKeyColumnName = null;
+
+		try (ResultSet rs1 = databaseMetaData.getColumns(
+				dbInspector.getCatalog(), dbInspector.getSchema(), tableName,
+				dbInspector.normalizeName("ctCollectionId", databaseMetaData));
+			ResultSet rs2 = databaseMetaData.getPrimaryKeys(
+				dbInspector.getCatalog(), dbInspector.getSchema(), tableName)) {
+
+			if (rs1.next()) {
+				return;
+			}
+
+			if (rs2.next()) {
+				primaryKeyColumnName = rs2.getString("COLUMN_NAME");
+			}
+
+			if (rs2.next()) {
+				throw new UpgradeException(
+					"Single column primary key is required to upgrade " +
+						tableName);
+			}
+		}
+
+		if (primaryKeyColumnName == null) {
+			throw new UpgradeException(
+				"No primary key column found for " + tableName);
+		}
+
+		alter(
+			_tableClass, new AlterTableAddCTCollectionIdColumn(),
+			new DropPrimaryKeyAlterable(),
+			new AddPrimaryKeyAlterable(primaryKeyColumnName));
+	}
+
+	private final Class<?> _tableClass;
+
+	private static class AddPrimaryKeyAlterable
+		extends BasePrimaryKeyAlterable {
+
+		@Override
+		public String getSQL(String tableName) {
+			return StringBundler.concat(
+				"alter table ", tableName, " add primary key (",
+				_primaryKeyColumnName, ", ctCollectionId)");
+		}
+
+		private AddPrimaryKeyAlterable(String primaryKeyColumnName) {
+			_primaryKeyColumnName = primaryKeyColumnName;
+		}
+
+		private final String _primaryKeyColumnName;
+
+	}
+
+	private abstract static class BasePrimaryKeyAlterable implements Alterable {
+
+		/**
+		 * @deprecated As of Judson (7.1.x), with no direct replacement
+		 */
+		@Deprecated
+		@Override
+		public String getIndexedColumnName() {
+			return null;
+		}
+
+		@Override
+		public boolean shouldAddIndex(Collection<String> columnNames) {
+			return false;
+		}
+
+		@Override
+		public boolean shouldDropIndex(Collection<String> columnNames) {
+			return false;
+		}
+
+	}
+
+	private static class DropPrimaryKeyAlterable
+		extends BasePrimaryKeyAlterable {
+
+		@Override
+		public String getSQL(String tableName) {
+			return StringBundler.concat(
+				"alter table ", tableName, " drop primary key");
+		}
+
+	}
+
+	private class AlterTableAddCTCollectionIdColumn
+		extends AlterTableAddColumn {
+
+		@Override
+		public String getSQL(String tableName) {
+			return StringBundler.concat(
+				"alter table ", tableName,
+				" add ctCollectionId LONG default 0 not null");
+		}
+
+		private AlterTableAddCTCollectionIdColumn() {
+			super("ctCollectionId");
+		}
+
+	}
+
+}
