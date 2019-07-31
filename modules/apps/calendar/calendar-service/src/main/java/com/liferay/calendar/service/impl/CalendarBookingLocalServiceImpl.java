d96dfee79296f455515dbb9d62806054baa866e0
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-07-31 11:43:47 -0300

    LPS-98525 SF

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/service/impl/CalendarBookingLocalServiceImpl.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/service/impl/CalendarBookingLocalServiceImpl.java
@@ -1286,12 +1286,8 @@ public class CalendarBookingLocalServiceImpl
 		// Calendar booking
 
 		Calendar calendar = calendarPersistence.findByPrimaryKey(calendarId);
-
 		CalendarBooking calendarBooking =
 			calendarBookingPersistence.findByPrimaryKey(calendarBookingId);
-
-		long oldStartTime = calendarBooking.getStartTime();
-		long oldEndTime = calendarBooking.getEndTime();
 
 		if (isStagingCalendarBooking(calendarBooking) &&
 			(calendar.getGroupId() != calendarBooking.getGroupId())) {
@@ -1358,6 +1354,10 @@ public class CalendarBookingLocalServiceImpl
 			updatedDescriptionMap, serviceContext.getLocale());
 
 		calendarBooking.setLocation(location);
+
+		long oldStartTime = calendarBooking.getStartTime();
+		long oldEndTime = calendarBooking.getEndTime();
+
 		calendarBooking.setStartTime(startTimeJCalendar.getTimeInMillis());
 		calendarBooking.setEndTime(endTimeJCalendar.getTimeInMillis());
 		calendarBooking.setAllDay(allDay);
