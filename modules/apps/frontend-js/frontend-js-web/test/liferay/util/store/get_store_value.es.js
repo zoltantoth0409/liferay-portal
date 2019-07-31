281f7c1c15e52e6503809c86ccf546760ac3f942
mateomustapic <mateo.mustapic@liferay.com>
2019-07-31 13:34:03 +0200

    LPS-95863 Tests for Store utilities

---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/store/get_store_value.es.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+'use strict';
+
+import getStoreValue from '../../../../src/main/resources/META-INF/resources/liferay/util/store/get_store_value.es';
+
+describe('Liferay.Util.Store.getStoreValue', () => {
+	it('throws error if key parameter is not a string', () => {
+		expect(() => getStoreValue(0)).toThrow(
+			'Parameter key must be a string'
+		);
+	});
+
+	it('throws error if callback parameter is not a function', () => {
+		expect(() => getStoreValue('foo', 'bar')).toThrow(
+			'Parameter callback must be a function'
+		);
+	});
+
+	it('gets the value of a Store entry with the given key', () => {
+		let globalLiferay = global.Liferay;
+
+		global.Liferay = {
+			authToken: 'abcd',
+			ThemeDisplay: {
+				getPathMain: jest.fn(() => {
+					return 'http://sampleurl.com';
+				}),
+				getDoAsUserIdEncoded: jest.fn(() => {
+					return 'efgh';
+				})
+			}
+		};
+
+		const storedValue = 'fooValue';
+
+		global.fetch = jest.fn(resource => {
+			expect(resource).toEqual(
+				'http://sampleurl.com/portal/session_click?cmd=get&key=foo&p_auth=abcd&doAsUserId=efgh'
+			);
+
+			return Promise.resolve({
+				text: jest.fn(() => Promise.resolve(storedValue))
+			});
+		});
+
+		const callback = value => {
+			expect(value).toEqual(storedValue);
+		};
+
+		getStoreValue('foo', callback);
+
+		global.Liferay = globalLiferay;
+	});
+});
