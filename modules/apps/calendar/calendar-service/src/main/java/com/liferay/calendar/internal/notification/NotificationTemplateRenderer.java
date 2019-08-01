31f04187bee6da7cb147c4607f2bcbfd0ef6b3cc
IstvanD <istvan.dezsi@liferay.com>
2019-08-01 13:00:34 +0200

    LPS-99154 attributes should be present before referring them

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationTemplateRenderer.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationTemplateRenderer.java
@@ -67,6 +67,9 @@ public class NotificationTemplateRenderer {
 			NotificationTemplateContext notificationTemplateContext, int mode)
 		throws Exception {
 
+		Map<String, Serializable> attributes =
+			notificationTemplateContext.getAttributes();
+
 		String location = GetterUtil.getString(attributes.get("location"));
 		String title = GetterUtil.getString(attributes.get("title"));
 
@@ -74,9 +77,6 @@ public class NotificationTemplateRenderer {
 			location = HtmlUtil.escape(location);
 			title = HtmlUtil.escape(title);
 		}
-
-		Map<String, Serializable> attributes =
-			notificationTemplateContext.getAttributes();
 
 		return StringUtil.replace(
 			notificationTemplate,
