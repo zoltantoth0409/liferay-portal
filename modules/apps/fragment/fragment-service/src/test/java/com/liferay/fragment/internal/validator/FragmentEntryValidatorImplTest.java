161429fa20b07468a93f509ad668494ba94b20c9
rubenpulido <ruben.pulido@liferay.com>
2019-08-01 14:51:25 +0200

    LPS-99177 Test for valid select field with all attributes

--- modules/apps/fragment/fragment-service/src/test/java/com/liferay/fragment/internal/validator/FragmentEntryValidatorImplTest.java
+++ modules/apps/fragment/fragment-service/src/test/java/com/liferay/fragment/internal/validator/FragmentEntryValidatorImplTest.java
@@ -118,6 +118,14 @@ public class FragmentEntryValidatorImplTest {
 	}
 
 	@Test
+	public void testValidateConfigurationValidFieldSelectStringComplete()
+		throws Exception {
+
+		_fragmentEntryValidatorImpl.validateConfiguration(
+			_read("configuration-valid-field-select-string-complete.json"));
+	}
+
+	@Test
 	public void testValidateConfigurationValidRequired() throws Exception {
 		_fragmentEntryValidatorImpl.validateConfiguration(
 			_read("configuration-valid-required.json"));
