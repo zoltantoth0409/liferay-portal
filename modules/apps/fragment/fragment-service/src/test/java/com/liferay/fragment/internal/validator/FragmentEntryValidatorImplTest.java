265e815bfd7ebfc9ef988905a4fb7a3bca587d38
rubenpulido <ruben.pulido@liferay.com>
2019-08-01 14:54:19 +0200

    LPS-99177 Test for valid select field with only required attributes

--- modules/apps/fragment/fragment-service/src/test/java/com/liferay/fragment/internal/validator/FragmentEntryValidatorImplTest.java
+++ modules/apps/fragment/fragment-service/src/test/java/com/liferay/fragment/internal/validator/FragmentEntryValidatorImplTest.java
@@ -127,6 +127,14 @@ public class FragmentEntryValidatorImplTest {
 	}
 
 	@Test
+	public void testValidateConfigurationValidFieldSelectStringRequired()
+		throws Exception {
+
+		_fragmentEntryValidatorImpl.validateConfiguration(
+			_read("configuration-valid-field-select-string-required.json"));
+	}
+
+	@Test
 	public void testValidateConfigurationValidRequired() throws Exception {
 		_fragmentEntryValidatorImpl.validateConfiguration(
 			_read("configuration-valid-required.json"));
