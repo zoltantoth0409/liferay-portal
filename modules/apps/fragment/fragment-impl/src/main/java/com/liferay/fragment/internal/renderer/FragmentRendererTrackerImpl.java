f8eed109d1e040604da74bc9940c6779e0a54468
rubenpulido <ruben.pulido@liferay.com>
2019-08-01 11:26:33 +0200

    LPS-99068 Add private class FragmentRendererTrackerServiceTrackerCustomizer returning only FragmentRenders with valid configuration

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentRendererTrackerImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentRendererTrackerImpl.java
@@ -14,8 +14,16 @@
 
 package com.liferay.fragment.internal.renderer;
 
+import com.liferay.fragment.exception.FragmentEntryConfigurationException;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.renderer.DefaultFragmentRendererContext;
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.fragment.renderer.FragmentRendererTracker;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.fragment.validator.FragmentEntryValidator;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
@@ -26,10 +34,13 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Jorge Ferrer
@@ -79,5 +90,73 @@ public class FragmentRendererTrackerImpl implements FragmentRendererTracker {
 
 	private final Map<String, FragmentRenderer> _fragmentRenderers =
 		new ConcurrentHashMap<>();
+	private static final Log _log = LogFactoryUtil.getLog(
+		FragmentRendererTrackerImpl.class);
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Reference
+	private FragmentEntryValidator _fragmentEntryValidator;
+
+	private class FragmentRendererTrackerServiceTrackerCustomizer
+		implements ServiceTrackerCustomizer
+			<FragmentRenderer, FragmentRenderer> {
+
+		public FragmentRendererTrackerServiceTrackerCustomizer(
+			BundleContext bundleContext) {
+
+			_bundleContext = bundleContext;
+		}
+
+		@Override
+		public FragmentRenderer addingService(
+			ServiceReference<FragmentRenderer> serviceReference) {
+
+			FragmentRenderer fragmentRenderer = _bundleContext.getService(
+				serviceReference);
+
+			try {
+				FragmentEntryLink fragmentEntryLink =
+					_fragmentEntryLinkLocalService.createFragmentEntryLink(0);
+
+				_fragmentEntryValidator.validateConfiguration(
+					fragmentRenderer.getConfiguration(
+						new DefaultFragmentRendererContext(fragmentEntryLink)));
+
+				return fragmentRenderer;
+			}
+			catch (FragmentEntryConfigurationException fece) {
+				_log.error(
+					String.format(
+						"FragmentRenderer with collection key %s and label " +
+							"%s could not be registered due to invalid " +
+								"configuration.",
+						fragmentRenderer.getCollectionKey(),
+						fragmentRenderer.getLabel(
+							LocaleUtil.getMostRelevantLocale())),
+					fece);
+			}
+
+			return null;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<FragmentRenderer> serviceReference,
+			FragmentRenderer fragmentRenderer) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<FragmentRenderer> serviceReference,
+			FragmentRenderer fragmentRenderer) {
+
+			_bundleContext.ungetService(serviceReference);
+		}
+
+		private final BundleContext _bundleContext;
+
+	}
 
 }
