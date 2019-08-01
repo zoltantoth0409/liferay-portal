c0c5181fd3171cc2372d87585c81c8b349d06b90
rubenpulido <ruben.pulido@liferay.com>
2019-08-01 15:07:01 +0200

    LPS-99177 Test for invalid select field with missing defaultValue

--- modules/apps/fragment/fragment-service/src/test/java/com/liferay/fragment/internal/validator/FragmentEntryValidatorImplTest.java
+++ modules/apps/fragment/fragment-service/src/test/java/com/liferay/fragment/internal/validator/FragmentEntryValidatorImplTest.java
@@ -113,6 +113,21 @@ public class FragmentEntryValidatorImplTest {
 	}
 
 	@Test
+	public void testValidateConfigurationInvalidSelectFieldDefaultValueMissing()
+		throws Exception {
+
+		expectedException.expect(FragmentEntryConfigurationException.class);
+		expectedException.expectMessage(
+			"#: only 1 subschema matches out of 2\n#/fieldSets/0/fields/0: " +
+				"required key [defaultValue] not found");
+
+		_fragmentEntryValidatorImpl.validateConfiguration(
+			_read(
+				"configuration-invalid-field-select-defaultValue-missing." +
+					"json"));
+	}
+
+	@Test
 	public void testValidateConfigurationValidComplete() throws Exception {
 		_fragmentEntryValidatorImpl.validateConfiguration(
 			_read("configuration-valid-complete.json"));
