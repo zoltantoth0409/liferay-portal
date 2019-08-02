99fe5c8ba364e8f2dd26e2939b015a4f7daf0a47
Vendel Toreki <vendel.toreki@liferay.com>
2019-08-02 12:27:07 +0200

    LPS-95817 LAYOUT_PROTOTYPE_MERGE_LOCK_MAX_TIME is already converted to seconds in PropsValues

--- portal-impl/src/com/liferay/portlet/sites/util/SitesImpl.java
+++ portal-impl/src/com/liferay/portlet/sites/util/SitesImpl.java
@@ -99,7 +99,6 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.SystemProperties;
-import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -1318,7 +1317,7 @@ public class SitesImpl implements Sites {
 
 		String owner = _acquireLock(
 			LayoutSet.class.getName(), layoutSet.getLayoutSetId(),
-			Time.SECOND * PropsValues.LAYOUT_SET_PROTOTYPE_MERGE_LOCK_MAX_TIME);
+			PropsValues.LAYOUT_SET_PROTOTYPE_MERGE_LOCK_MAX_TIME);
 
 		if (owner == null) {
 			return;
@@ -1735,7 +1734,7 @@ public class SitesImpl implements Sites {
 
 		String owner = _acquireLock(
 			Layout.class.getName(), layout.getPlid(),
-			Time.SECOND * PropsValues.LAYOUT_PROTOTYPE_MERGE_LOCK_MAX_TIME);
+			PropsValues.LAYOUT_PROTOTYPE_MERGE_LOCK_MAX_TIME);
 
 		if (owner == null) {
 			return;
