9af7becd195085d4622568162f919386b883297e
Sarai Díaz García <dgarciasarai@gmail.com>
2019-08-02 10:23:33 +0200

    LPS-99220 Move experiences constants to new file

--- modules/apps/segments/segments-api/src/main/java/com/liferay/segments/constants/SegmentsExperienceConstants.java
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/constants/SegmentsExperienceConstants.java
@@ -21,27 +21,24 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
- * @author Eduardo García
+ * @author Sarai Díaz
  */
-public class SegmentsConstants {
+public class SegmentsExperienceConstants {
 
-	public static final String RESOURCE_NAME = "com.liferay.segments";
+	public static final long SEGMENTS_EXPERIENCE_ID_DEFAULT = 0;
 
-	public static final long SEGMENTS_ENTRY_ID_DEFAULT = 0;
+	public static final String SEGMENTS_EXPERIENCE_ID_PREFIX =
+		"segments-experience-id-";
 
-	public static final String SEGMENTS_ENTRY_KEY_DEFAULT = "DEFAULT";
+	public static final String SEGMENTS_EXPERIENCE_KEY_DEFAULT = "DEFAULT";
 
-	public static final String SERVICE_NAME = "com.liferay.segments";
+	public static final int SEGMENTS_EXPERIENCE_PRIORITY_DEFAULT = -1;
 
-	public static final String SOURCE_ASAH_FARO_BACKEND = "ASAH_FARO_BACKEND";
-
-	public static final String SOURCE_DEFAULT = "DEFAULT";
-
-	public static String getDefaultSegmentsEntryName(Locale locale) {
+	public static String getDefaultSegmentsExperienceName(Locale locale) {
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", locale, SegmentsConstants.class);
 
-		return LanguageUtil.get(resourceBundle, "default-segment-name");
+		return LanguageUtil.get(resourceBundle, "default-experience-name");
 	}
 
 }
