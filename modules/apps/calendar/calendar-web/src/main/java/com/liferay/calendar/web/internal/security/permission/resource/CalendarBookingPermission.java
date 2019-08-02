7d2e011d94e91e0cdf19381e52cbb5743644e724
Jonathan McCann <jonathan.mccann@liferay.com>
2019-08-02 09:05:08 -0700

    LPS-99238 Add new permission checking for CalendarBooking

---
+++ modules/apps/calendar/calendar-web/src/main/java/com/liferay/calendar/web/internal/security/permission/resource/CalendarBookingPermission.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.web.internal.security.permission.resource;
+
+import com.liferay.calendar.model.CalendarBooking;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jonathan McCann
+ */
+@Component(immediate = true, service = {})
+public class CalendarBookingPermission {
+
+	public static boolean contains(
+			PermissionChecker permissionChecker,
+			CalendarBooking calendarBooking, String actionId)
+		throws PortalException {
+
+		return _calendarBookingeModelResourcePermission.contains(
+			permissionChecker, calendarBooking, actionId);
+	}
+
+	public static boolean contains(
+			PermissionChecker permissionChecker, long calendarBookingId,
+			String actionId)
+		throws PortalException {
+
+		return _calendarBookingeModelResourcePermission.contains(
+			permissionChecker, calendarBookingId, actionId);
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.calendar.model.CalendarBooking)",
+		unbind = "-"
+	)
+	protected void setModelPermissionChecker(
+		ModelResourcePermission<CalendarBooking> modelResourcePermission) {
+
+		_calendarBookingeModelResourcePermission = modelResourcePermission;
+	}
+
+	private static ModelResourcePermission<CalendarBooking>
+		_calendarBookingeModelResourcePermission;
+
+}
