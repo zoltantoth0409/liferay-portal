8a26f20ac7cc5d1b8e05c006c99589adeaf6e4b8
Pablo Molina <contact@pablomolina.me>
2019-08-02 11:23:54 +0200

    LPS-98885 Rely on css attributes for selection
    Instead of adding inline listeners, which may fail when there are more
    kinds of elements inside the fragment entry link list, adding an
    user-select:none attribute to the list produces much less overhead.

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_processors/EditableTextFragmentProcessor.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/fragment_processors/EditableTextFragmentProcessor.es.js
@@ -34,7 +34,6 @@ function destroy() {
 		const editorData = _editor.get('nativeEditor').getData();
 
 		_editableElement.innerHTML = editorData;
-		_editableElement.setAttribute('onselectstart', 'return false;');
 
 		_editor.destroy();
 
@@ -171,8 +170,6 @@ function init(
 	_editorEventHandler.add(
 		nativeEditor.on('instanceReady', () => nativeEditor.focus())
 	);
-
-	_editableElement.setAttribute('onselectstart', '');
 }
 
 /**
