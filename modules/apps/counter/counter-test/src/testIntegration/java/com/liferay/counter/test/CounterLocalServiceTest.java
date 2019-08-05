af1207ea7f8aaf3c54bd93ef45565155d291e302
hai.yu <8219754@liferay.com>
2019-08-05 14:40:23 +0800

    LPS-96692 Move HypersonicServerClassTestRule to the module since it's only used by CounterLocalServiceTest

--- modules/apps/counter/counter-test/src/testIntegration/java/com/liferay/counter/test/CounterLocalServiceTest.java
+++ modules/apps/counter/counter-test/src/testIntegration/java/com/liferay/counter/test/CounterLocalServiceTest.java
@@ -30,7 +30,6 @@ import com.liferay.portal.kernel.test.rule.ClassTestRule;
 import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.test.rule.HypersonicServerClassTestRule;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.InitUtil;
