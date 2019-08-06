3cc69c16d2df757f38f09afcfb70b810e4882a4e
Pablo Molina <contact@pablomolina.me>
2019-08-06 17:49:04 +0200

    LPS-99353 Add ResolveButton component

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/sidebar/comments/ResolveButton.es.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Loader from '../../common/Loader.es';
+
+const ResolveButton = props => {
+	let icon = (
+		<span
+			className="lfr-portal-tooltip ml-1 text-lowercase"
+			data-title={Liferay.Language.get('resolve')}
+		>
+			<ClayIcon symbol="check-circle" />
+		</span>
+	);
+
+	if (props.loading) {
+		icon = <Loader />;
+	} else if (props.resolved) {
+		icon = (
+			<span
+				className="lfr-portal-tooltip ml-1 text-lowercase text-success"
+				data-title={Liferay.Language.get('unresolve')}
+			>
+				<ClayIcon symbol="check-circle-full" />
+			</span>
+		);
+	}
+
+	return (
+		<ClayButton
+			borderless
+			className="flex-shrink-0"
+			disabled={props.loading}
+			displayType="secondary"
+			monospaced
+			onClick={props.onClick}
+			outline
+			small
+		>
+			{icon}
+		</ClayButton>
+	);
+};
+
+ResolveButton.propTypes = {
+	loading: PropTypes.bool.isRequired,
+	onClick: PropTypes.func.isRequired,
+	resolved: PropTypes.bool.isRequired
+};
+
+export {ResolveButton};
+export default ResolveButton;
