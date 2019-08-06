471c9a8c7d1c7ca48e6cdf6100e5b545b7a2a1ba
Brian Chan <brian.chan@liferay.com>
2019-08-06 16:44:38 -0700

    LPS-98878 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormDisplayContext.java
@@ -203,7 +203,6 @@ public class DDMFormDisplayContext {
 		}
 
 		ddmFormRenderingContext.setShowSubmitButton(isShowSubmitButton());
-
 		ddmFormRenderingContext.setSubmitLabel(getSubmitLabel());
 
 		return _ddmFormRenderer.render(
