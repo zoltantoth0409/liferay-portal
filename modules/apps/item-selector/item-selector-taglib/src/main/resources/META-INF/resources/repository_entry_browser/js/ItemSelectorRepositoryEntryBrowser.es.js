c5628f7fadba8a604cfaefd3d79f3eab3171db49
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2019-08-06 13:21:32 +0200

    LPS-99058 Add logic when an item is selected from the file system

--- modules/apps/item-selector/item-selector-taglib/src/main/resources/META-INF/resources/repository_entry_browser/js/ItemSelectorRepositoryEntryBrowser.es.js
+++ modules/apps/item-selector/item-selector-taglib/src/main/resources/META-INF/resources/repository_entry_browser/js/ItemSelectorRepositoryEntryBrowser.es.js
@@ -12,18 +12,29 @@
  * details.
  */
 
-import Component from 'metal-component';
+import {EventHandler} from 'metal-events';
 import {Config} from 'metal-state';
+import {PortletBase} from 'frontend-js-web';
 
-class ItemSelectorRepositoryEntryBrowser extends Component {
+const sub = (str, obj) => str.replace(/\{([^}]+)\}/g, (_, m) => obj[m]);
+
+class ItemSelectorRepositoryEntryBrowser extends PortletBase {
+	/**
+	 * @inheritDoc
+	 */
+	created() {
+		this._eventHandler = new EventHandler();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
 	attached() {
-		console.log('attached');
-
 		AUI().use('liferay-item-selector-uploader,liferay-item-viewer', A => {
 			this._itemViewer = new A.LiferayItemViewer({
 				btnCloseCaption: this.closeCaption,
 				editItemURL: this.editItemURL,
-				links: '', //instance.all('.item-preview'),
+				links: '', //this.all('.item-preview'), TODO
 				uploadItemURL: this.uploadItemURL
 			});
 
@@ -38,18 +49,105 @@ class ItemSelectorRepositoryEntryBrowser extends Component {
 			});
 
 			this._bindEvents();
+			this._renderUI();
 		});
 	}
 
+	/**
+	 * @inheritDoc
+	 */
+	detached() {
+		super.detached();
+
+		this._itemViewer.destroy();
+		this._uploadItemViewer.destroy();
+		this._itemSelectorUploader.destroy();
+
+		this._eventHandler.removeAllListeners();
+	}
+
 	_bindEvents() {
-		console.log(this._itemViewer);
+		const inputFileNode = this.one('input[type="file"]');
+
+		if (inputFileNode) {
+			this._eventHandler.add(
+				inputFileNode.addEventListener('change', event => {
+					this._validateFile(event.target.files[0])
+				})
+			);
+		}
 	}
 
 	_convertMaxFileSize(maxFileSize) {
 		return parseInt(maxFileSize);
 	}
+
+	_renderUI() {
+		const rootNode = this.rootNode;
+
+		this._itemViewer.render(rootNode);
+		this._uploadItemViewer.render(rootNode);
+	}
+
+	_showError(message) {
+		console.log(message);
+		//TODO
+	}
+
+	_validateFile(file) {
+		let errorMessage = '';
+
+		const fileExtension = file.name
+			.split('.')
+			.pop()
+			.toLowerCase();
+
+		const validExtensions = this.validExtensions;
+
+		if (
+			validExtensions === '*' ||
+			validExtensions.indexOf(fileExtension) != -1
+		) {
+			const maxFileSize = this.maxFileSize;
+
+			if (file.size <= maxFileSize) {
+				console.log('file OK, go to preview');
+				//this._previewFile(file); TODO
+			} else {
+				errorMessage = sub(
+					Liferay.Language.get(
+						'please-enter-a-file-with-a-valid-file-size-no-larger-than-x'
+					),
+					[maxFileSize] //TODO format file size
+				);
+			}
+		} else {
+			errorMessage = sub(
+				Liferay.Language.get(
+					'please-enter-a-file-with-a-valid-extension-x'
+				),
+				[validExtensions]
+			);
+		}
+
+		if (errorMessage) {
+			var inputTypeFile = this.one('input[type="file"]');
+
+			if (inputTypeFile) {
+				inputTypeFile.value = '';
+			}
+
+			this._showError(errorMessage);
+		}
+	}
 }
 
+/**
+ * State definition.
+ *
+ * @static
+ * @type {!Object}
+ */
 ItemSelectorRepositoryEntryBrowser.STATE = {
 	closeCaption: Config.string(),
 
@@ -58,8 +156,6 @@ ItemSelectorRepositoryEntryBrowser.STATE = {
 	maxFileSize: Config.oneOfType([Config.number(), Config.string()])
 		.setter('_convertMaxFileSize')
 		.value(Liferay.PropsValues.UPLOAD_SERVLET_REQUEST_IMPL_MAX_SIZE),
-
-	rootNode: Config.string(),
 
 	uploadItemReturnType: Config.string(),
 
