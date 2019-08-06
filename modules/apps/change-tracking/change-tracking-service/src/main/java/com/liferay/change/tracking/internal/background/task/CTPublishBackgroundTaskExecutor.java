6144620825ec07ac6c5fd03a871972c85fe9cd3b
Preston-Crary <preston.crary@liferay.com>
2019-08-06 11:43:10 -0700

    LPS-99376 Clean up CTPublishBackgroundTaskExecutor hidden dependencies.

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/background/task/CTPublishBackgroundTaskExecutor.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/background/task/CTPublishBackgroundTaskExecutor.java
@@ -24,16 +24,16 @@ import com.liferay.change.tracking.internal.process.util.CTProcessMessageSenderU
 import com.liferay.change.tracking.internal.util.CTEntryCollisionUtil;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.model.CTEntry;
-import com.liferay.change.tracking.service.CTCollectionLocalServiceUtil;
-import com.liferay.change.tracking.service.CTEntryLocalServiceUtil;
-import com.liferay.change.tracking.service.CTProcessLocalServiceUtil;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTEntryLocalService;
+import com.liferay.change.tracking.service.CTProcessLocalService;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTask;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskConstants;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskExecutor;
-import com.liferay.portal.kernel.backgroundtask.BackgroundTaskManagerUtil;
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskManager;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskResult;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatus;
-import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatusRegistryUtil;
+import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatusRegistry;
 import com.liferay.portal.kernel.backgroundtask.BaseBackgroundTaskExecutor;
 import com.liferay.portal.kernel.backgroundtask.display.BackgroundTaskDisplay;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -41,7 +41,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
@@ -57,14 +57,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.osgi.framework.Bundle;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Zoltan Csaszi
  * @author Daniel Kocsis
  */
+@Component(
+	immediate = true,
+	property = "background.task.executor.class.name=com.liferay.change.tracking.internal.background.task.CTPublishBackgroundTaskExecutor",
+	service = BackgroundTaskExecutor.class
+)
 public class CTPublishBackgroundTaskExecutor
 	extends BaseBackgroundTaskExecutor {
 
@@ -72,22 +76,11 @@ public class CTPublishBackgroundTaskExecutor
 		setBackgroundTaskStatusMessageTranslator(
 			new CTPublishBackgroundTaskStatusMessageTranslator());
 		setIsolationLevel(BackgroundTaskConstants.ISOLATION_LEVEL_COMPANY);
-
-		Bundle bundle = FrameworkUtil.getBundle(
-			CTPublishBackgroundTaskExecutor.class);
-
-		ServiceTracker<CTEngineManager, CTEngineManager> serviceTracker =
-			new ServiceTracker<>(
-				bundle.getBundleContext(), CTEngineManager.class, null);
-
-		serviceTracker.open();
-
-		_ctEngineManager = serviceTracker.getService();
 	}
 
 	@Override
 	public BackgroundTaskExecutor clone() {
-		return new CTPublishBackgroundTaskExecutor();
+		return this;
 	}
 
 	@Override
@@ -141,7 +134,7 @@ public class CTPublishBackgroundTaskExecutor
 		throws IOException, PortalException {
 
 		BackgroundTaskStatus backgroundTaskStatus =
-			BackgroundTaskStatusRegistryUtil.getBackgroundTaskStatus(
+			_backgroundTaskStatusRegistry.getBackgroundTaskStatus(
 				backgroundTask.getBackgroundTaskId());
 
 		CTProcessLog ctProcessLog =
@@ -149,7 +142,7 @@ public class CTPublishBackgroundTaskExecutor
 
 		String ctProcessLogJSON = ctProcessLog.toString();
 
-		BackgroundTaskManagerUtil.addBackgroundTaskAttachment(
+		_backgroundTaskManager.addBackgroundTaskAttachment(
 			backgroundTask.getUserId(), backgroundTask.getBackgroundTaskId(),
 			"log", FileUtil.createTempFile(ctProcessLogJSON.getBytes()));
 	}
@@ -177,7 +170,7 @@ public class CTPublishBackgroundTaskExecutor
 		throws Exception {
 
 		CTProcessMessageSenderUtil.logCTProcessStarted(
-			CTProcessLocalServiceUtil.getCTProcess(ctProcessId));
+			_ctProcessLocalService.getCTProcess(ctProcessId));
 
 		List<CTEntry> ctEntries = _ctEngineManager.getCTEntries(ctCollectionId);
 
@@ -203,7 +196,7 @@ public class CTPublishBackgroundTaskExecutor
 			boolean ignoreCollision)
 		throws Exception {
 
-		User user = UserLocalServiceUtil.getUser(userId);
+		User user = _userLocalService.getUser(userId);
 
 		for (CTEntry ctEntry : ctEntries) {
 			_publishCTEntry(ctEntry, ignoreCollision);
@@ -221,7 +214,7 @@ public class CTPublishBackgroundTaskExecutor
 				"Unable to find change tracking collection " + ctCollectionId));
 
 		try {
-			CTCollectionLocalServiceUtil.updateStatus(
+			_ctCollectionLocalService.updateStatus(
 				userId, ctCollection, WorkflowConstants.STATUS_APPROVED,
 				new ServiceContext());
 		}
@@ -240,7 +233,7 @@ public class CTPublishBackgroundTaskExecutor
 
 		_checkExistingCollisions(ctEntry, ignoreCollision);
 
-		CTEntryLocalServiceUtil.updateStatus(
+		_ctEntryLocalService.updateStatus(
 			ctEntry.getCtEntryId(), WorkflowConstants.STATUS_APPROVED);
 
 		CTEntryCollisionUtil.checkCollidingCTEntries(ctEntry);
@@ -249,9 +242,29 @@ public class CTPublishBackgroundTaskExecutor
 	private static final Log _log = LogFactoryUtil.getLog(
 		CTPublishBackgroundTaskExecutor.class);
 
-	private final CTEngineManager _ctEngineManager;
-	private final TransactionConfig _transactionConfig =
+	private static final TransactionConfig _transactionConfig =
 		TransactionConfig.Factory.create(
 			Propagation.REQUIRED, new Class<?>[] {Exception.class});
+
+	@Reference
+	private BackgroundTaskManager _backgroundTaskManager;
+
+	@Reference
+	private BackgroundTaskStatusRegistry _backgroundTaskStatusRegistry;
+
+	@Reference
+	private CTCollectionLocalService _ctCollectionLocalService;
+
+	@Reference
+	private CTEngineManager _ctEngineManager;
+
+	@Reference
+	private CTEntryLocalService _ctEntryLocalService;
+
+	@Reference
+	private CTProcessLocalService _ctProcessLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
