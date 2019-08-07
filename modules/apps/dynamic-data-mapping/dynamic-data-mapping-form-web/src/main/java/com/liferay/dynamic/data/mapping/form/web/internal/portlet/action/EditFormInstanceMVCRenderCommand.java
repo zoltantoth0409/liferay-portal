e05322dee1bdccaca6a851d2b4b8d2848fdac752
Jorge Díaz <jorge.diaz@liferay.com>
2019-08-07 19:35:56 +0200

    LPS-98519 7.1.x only - Remove all references to new metal interface as it doesn't exist.
    Just removing everything inside "if (ddmFormWebConfiguration.enableExperimentalInterface()) {" and all its auxiliary methods

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/EditFormInstanceMVCRenderCommand.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/EditFormInstanceMVCRenderCommand.java
@@ -15,39 +15,12 @@
 package com.liferay.dynamic.data.mapping.form.web.internal.portlet.action;
 
 import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
-import com.liferay.dynamic.data.mapping.form.builder.context.DDMFormBuilderContextFactory;
-import com.liferay.dynamic.data.mapping.form.builder.context.DDMFormBuilderContextRequest;
-import com.liferay.dynamic.data.mapping.form.builder.context.DDMFormBuilderContextResponse;
-import com.liferay.dynamic.data.mapping.form.web.internal.configuration.DDMFormWebConfiguration;
-import com.liferay.dynamic.data.mapping.form.web.internal.configuration.activator.DDMFormWebConfigurationActivator;
-import com.liferay.dynamic.data.mapping.form.web.internal.display.context.DDMFormAdminDisplayContext;
-import com.liferay.dynamic.data.mapping.model.DDMForm;
-import com.liferay.dynamic.data.mapping.model.DDMStructure;
-import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
-import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.json.JSONFactory;
-import com.liferay.portal.kernel.json.JSONSerializer;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import java.util.Locale;
-import java.util.Optional;
 
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Bruno Basto
@@ -64,117 +37,7 @@ public class EditFormInstanceMVCRenderCommand implements MVCRenderCommand {
 	public String render(
 		RenderRequest renderRequest, RenderResponse renderResponse) {
 
-		DDMFormWebConfiguration ddmFormWebConfiguration =
-			_ddmFormWebConfigurationActivator.getDDMFormWebConfiguration();
-
-		if (ddmFormWebConfiguration.enableExperimentalInterface()) {
-			DDMFormAdminDisplayContext ddmFormAdminDisplayContext =
-				(DDMFormAdminDisplayContext)renderRequest.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
-
-			long ddmStructureId;
-
-			try {
-				ddmStructureId = ddmFormAdminDisplayContext.getDDMStructureId();
-
-				String serializedFormBuilderContext = getFormBuilderContext(
-					ddmStructureId,
-					PortalUtil.getHttpServletRequest(renderRequest));
-
-				renderRequest.setAttribute(
-					"serializedFormBuilderContext",
-					serializedFormBuilderContext);
-			} catch (PortalException e) {
-				e.printStackTrace();
-			}
-
-			renderRequest.setAttribute(
-				"mainRequire",
-				_npmResolver.resolveModuleName("dynamic-data-mapping-form-web") +
-					" as main");
-
-			return "/metal/edit_form_instance.jsp";
-		}
-
 		return "/admin/edit_form_instance.jsp";
 	}
-
-	protected String getFormBuilderContext(
-		long ddmStructureId, HttpServletRequest request) {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		String serializedFormBuilderContext = ParamUtil.getString(
-			request, "serializedFormBuilderContext");
-
-		if (Validator.isNotNull(serializedFormBuilderContext)) {
-			return serializedFormBuilderContext;
-		}
-
-		JSONSerializer jsonSerializer = _jsonFactory.createJSONSerializer();
-
-		Optional<DDMStructure> ddmStructureOptional = Optional.ofNullable(
-			_ddmStructureLocalService.fetchDDMStructure(ddmStructureId));
-
-		Locale locale = themeDisplay.getSiteDefaultLocale();
-
-		if (ddmStructureOptional.isPresent()) {
-			DDMStructure ddmStructure = ddmStructureOptional.get();
-
-			DDMForm ddmForm = ddmStructure.getDDMForm();
-
-			locale = ddmForm.getDefaultLocale();
-		}
-
-		DDMFormBuilderContextResponse formBuilderContextResponse =
-			_ddmFormBuilderContextFactory.create(
-				DDMFormBuilderContextRequest.with(
-					ddmStructureOptional, themeDisplay.getRequest(),
-					themeDisplay.getResponse(), locale, true));
-
-		return jsonSerializer.serializeDeep(
-			formBuilderContextResponse.getContext());
-	}
-
-	@Reference(unbind = "-")
-	protected void setDDMFormBuilderContextFactory(
-		DDMFormBuilderContextFactory ddmFormBuilderContextFactory) {
-
-		_ddmFormBuilderContextFactory = ddmFormBuilderContextFactory;
-	}
-
-	@Reference(unbind = "-")
-	protected void setDDMStructureLocalService(
-		DDMStructureLocalService ddmStructureLocalService) {
-
-		_ddmStructureLocalService = ddmStructureLocalService;
-	}
-
-	@Reference(unbind = "-")
-	protected void setJSONFactory(JSONFactory jsonFactory) {
-		_jsonFactory = jsonFactory;
-	}
-
-	protected void unsetDDMFormWebConfigurationActivator(
-		DDMFormWebConfigurationActivator ddmFormWebConfigurationActivator) {
-
-		_ddmFormWebConfigurationActivator = null;
-	}
-
-	private static DDMFormBuilderContextFactory _ddmFormBuilderContextFactory;
-	private static DDMStructureLocalService _ddmStructureLocalService;
-	private static JSONFactory _jsonFactory;
-
-	@Reference(
-		cardinality = ReferenceCardinality.OPTIONAL,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY,
-		unbind = "unsetDDMFormWebConfigurationActivator"
-	)
-	private volatile DDMFormWebConfigurationActivator
-		_ddmFormWebConfigurationActivator;
-
-	@Reference
-	private NPMResolver _npmResolver;
 
 }
