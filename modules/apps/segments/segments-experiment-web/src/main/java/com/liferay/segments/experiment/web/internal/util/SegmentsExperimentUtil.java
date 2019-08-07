ae8ff11f360c6d370f24f3b6f1ab4394b399a7d0
David Arques <davidarchena@gmail.com>
2019-08-07 13:24:34 +0200

    LPS-99083 Add converter util class

---
+++ modules/apps/segments/segments-experiment-web/src/main/java/com/liferay/segments/experiment/web/internal/util/SegmentsExperimentUtil.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.experiment.web.internal.util;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.segments.model.SegmentsExperiment;
+import com.liferay.segments.model.SegmentsExperimentRel;
+
+import java.util.Locale;
+
+/**
+ * @author David Arques
+ */
+public class SegmentsExperimentUtil {
+
+	public static JSONObject toSegmentsExperimentJSONObject(
+		SegmentsExperiment segmentsExperiment) {
+
+		if (segmentsExperiment == null) {
+			return null;
+		}
+
+		return JSONUtil.put(
+			"description", segmentsExperiment.getDescription()
+		).put(
+			"name", segmentsExperiment.getName()
+		).put(
+			"segmentsExperienceId",
+			String.valueOf(segmentsExperiment.getSegmentsExperienceId())
+		).put(
+			"segmentsExperimentId",
+			String.valueOf(segmentsExperiment.getSegmentsExperimentId())
+		);
+	}
+
+	public static JSONObject toSegmentsExperimentRelJSONObject(
+			SegmentsExperimentRel segmentsExperimentRel, Locale locale)
+		throws PortalException {
+
+		if (segmentsExperimentRel == null) {
+			return null;
+		}
+
+		return JSONUtil.put(
+			"name", segmentsExperimentRel.getSegmentsExperienceName(locale)
+		).put(
+			"segmentsExperienceId",
+			String.valueOf(segmentsExperimentRel.getSegmentsExperienceId())
+		).put(
+			"segmentsExperimentId",
+			String.valueOf(segmentsExperimentRel.getSegmentsExperimentId())
+		).put(
+			"segmentsExperimentRelId",
+			String.valueOf(segmentsExperimentRel.getSegmentsExperimentRelId())
+		);
+	}
+
+	private SegmentsExperimentUtil() {
+	}
+
+}
