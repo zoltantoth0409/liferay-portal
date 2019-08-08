657330f8064abafdb7569107454eb4bd72da9a42
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-08-08 09:41:35 +0200

    LPS-99375 Take into account experiences only when it is needed

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-freemarker/src/main/java/com/liferay/fragment/entry/processor/freemarker/FreeMarkerFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-freemarker/src/main/java/com/liferay/fragment/entry/processor/freemarker/FreeMarkerFragmentEntryProcessor.java
@@ -42,8 +42,10 @@ import com.liferay.portal.kernel.template.TemplateManagerUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.segments.constants.SegmentsExperienceConstants;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -244,9 +246,13 @@ public class FreeMarkerFragmentEntryProcessor
 			return configurationDefaultValuesJSONObject;
 		}
 
-		JSONObject configurationJSONObject =
-			FragmentEntryConfigUtil.getSegmentedConfigurationValues(
-				segmentsExperienceIds, configurationValuesJSONObject);
+		JSONObject configurationJSONObject = configurationValuesJSONObject;
+
+		if (_isPersonalizationSupported(configurationValuesJSONObject)) {
+			configurationJSONObject =
+				FragmentEntryConfigUtil.getSegmentedConfigurationValues(
+					segmentsExperienceIds, configurationValuesJSONObject);
+		}
 
 		List<FragmentConfigurationField> configurationFields =
 			FragmentEntryConfigUtil.getFragmentConfigurationFields(
@@ -289,6 +295,20 @@ public class FreeMarkerFragmentEntryProcessor
 		}
 
 		return message;
+	}
+
+	private boolean _isPersonalizationSupported(JSONObject jsonObject) {
+		Iterator<String> keys = jsonObject.keys();
+
+		while (keys.hasNext()) {
+			String key = keys.next();
+
+			if (key.startsWith(SegmentsExperienceConstants.ID_PREFIX)) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
