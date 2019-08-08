541e0c97687b61d44d71a771c5e1ae505938532a
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-08-08 12:33:30 +0200

    LPS-99404 Override method so as to let the user click on a fragment that is inherit and view it in read only mode

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/servlet/taglib/clay/InheritedFragmentEntryVerticalCard.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/servlet/taglib/clay/InheritedFragmentEntryVerticalCard.java
@@ -14,7 +14,9 @@
 
 package com.liferay.fragment.web.internal.servlet.taglib.clay;
 
+import com.liferay.fragment.constants.FragmentActionKeys;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.web.internal.security.permission.resource.FragmentPermission;
 import com.liferay.fragment.web.internal.servlet.taglib.util.InheritedFragmentEntryActionDropdownItemsProvider;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
 import com.liferay.portal.kernel.dao.search.RowChecker;
@@ -23,6 +25,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 import java.util.List;
 
+import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
@@ -60,6 +63,32 @@ public class InheritedFragmentEntryVerticalCard
 		}
 
 		return null;
+	}
+
+	@Override
+	public String getHref() {
+		if (!FragmentPermission.contains(
+				themeDisplay.getPermissionChecker(),
+				themeDisplay.getScopeGroupId(),
+				FragmentActionKeys.MANAGE_FRAGMENT_ENTRIES)) {
+
+			return null;
+		}
+
+		PortletURL editFragmentEntryURL = _renderResponse.createRenderURL();
+
+		editFragmentEntryURL.setParameter(
+			"mvcRenderCommandName", "/fragment/edit_fragment_entry");
+		editFragmentEntryURL.setParameter(
+			"redirect", themeDisplay.getURLCurrent());
+		editFragmentEntryURL.setParameter(
+			"fragmentCollectionId",
+			String.valueOf(fragmentEntry.getFragmentCollectionId()));
+		editFragmentEntryURL.setParameter(
+			"fragmentEntryId",
+			String.valueOf(fragmentEntry.getFragmentEntryId()));
+
+		return editFragmentEntryURL.toString();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
