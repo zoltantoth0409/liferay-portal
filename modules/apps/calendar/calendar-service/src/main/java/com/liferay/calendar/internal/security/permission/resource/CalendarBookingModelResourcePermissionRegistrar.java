e0fa8696ff429bbd6553e2795376bfc6b2d101ee
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-08-08 09:24:03 -0300

    LPS-99385 Extract logic to an inner class and delegate the permission check if the action is VIEW.

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/security/permission/resource/CalendarBookingModelResourcePermissionRegistrar.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/security/permission/resource/CalendarBookingModelResourcePermissionRegistrar.java
@@ -16,12 +16,16 @@ package com.liferay.calendar.internal.security.permission.resource;
 
 import com.liferay.calendar.constants.CalendarConstants;
 import com.liferay.calendar.constants.CalendarPortletKeys;
+import com.liferay.calendar.model.Calendar;
 import com.liferay.calendar.model.CalendarBooking;
 import com.liferay.calendar.service.CalendarBookingLocalService;
 import com.liferay.exportimport.kernel.staging.permission.StagingPermission;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionLogic;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.StagedModelPermissionLogic;
 import com.liferay.portal.kernel.util.HashMapDictionary;
@@ -55,14 +59,7 @@ public class CalendarBookingModelResourcePermissionRegistrar {
 				_portletResourcePermission,
 				(modelResourcePermission, consumer) -> {
 					consumer.accept(
-						(permissionChecker, name, calendarBooking, actionId) -> {
-							if (actionId.equals(ActionKeys.VIEW)) {
-								return true;
-							}
-
-							return null;
-						});
-
+						new CalendarBookingInheritanceModelResourcePermissionLogic());
 					consumer.accept(
 						new StagedModelPermissionLogic<>(
 							_stagingPermission, CalendarPortletKeys.CALENDAR,
@@ -80,6 +77,11 @@ public class CalendarBookingModelResourcePermissionRegistrar {
 	private CalendarBookingLocalService _calendarBookingLocalService;
 
 	@Reference(
+		target = "(model.class.name=com.liferay.calendar.model.Calendar)"
+	)
+	private ModelResourcePermission<Calendar> _calendarModelResourcePermission;
+
+	@Reference(
 		target = "(resource.name=" + CalendarConstants.RESOURCE_NAME + ")"
 	)
 	private PortletResourcePermission _portletResourcePermission;
@@ -88,5 +90,24 @@ public class CalendarBookingModelResourcePermissionRegistrar {
 
 	@Reference
 	private StagingPermission _stagingPermission;
+
+	private class CalendarBookingInheritanceModelResourcePermissionLogic
+		implements ModelResourcePermissionLogic<CalendarBooking> {
+
+		@Override
+		public Boolean contains(
+				PermissionChecker permissionChecker, String name,
+				CalendarBooking calendarBooking, String actionId)
+			throws PortalException {
+
+			if (!actionId.equals(ActionKeys.VIEW)) {
+				return null;
+			}
+
+			return _calendarModelResourcePermission.contains(
+				permissionChecker, calendarBooking.getCalendar(), actionId);
+		}
+
+	}
 
 }
