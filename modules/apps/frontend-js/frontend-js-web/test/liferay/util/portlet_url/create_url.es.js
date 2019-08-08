564263fb506564aab556ff9c5325699fa07b0682
Krešimir Čoko <kresimir.coko@liferay.com>
2019-08-08 09:59:44 +0200

    LPS-94714 Update tests to reflect the removal of converting the URL to String

--- modules/apps/frontend-js/frontend-js-web/test/liferay/util/portlet_url/create_url.es.js
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/portlet_url/create_url.es.js
@@ -52,36 +52,37 @@ describe('Liferay.Util.PortletURL.createURL', () => {
 		).toThrow('Portlet ID must not be null if parameters are provided');
 	});
 
-	it('returns a string containing base url and parameters, where reserved parameters are not namespaced', () => {
-		expect(
-			createURL(
-				'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet',
-				{
-					doAsGroupId: 'fooBar',
-					foo: 'bar'
-				}
-			)
-		).toEqual(
+	it('returns a URL object with a href parameter containing base url and parameters, where reserved parameters are not namespaced', () => {
+		const portletURL = createURL(
+			'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet',
+			{
+				doAsGroupId: 'fooBar',
+				foo: 'bar'
+			}
+		);
+
+		expect(portletURL.href).toEqual(
 			'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet&doAsGroupId=fooBar&_com_liferay_roles_admin_web_portlet_RolesAdminPortlet_foo=bar'
 		);
 	});
 
 	it('overwrites a parameter in base url if the same parameter is set in the parameters argument', () => {
-		expect(
-			createURL(
-				'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet&doAsGroupId=fooBar',
-				{
-					doAsGroupId: 'barBaz',
-					foo: 'bar'
-				}
-			)
-		).toEqual(
+		const portletURL = createURL(
+			'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet&doAsGroupId=fooBar',
+			{
+				doAsGroupId: 'barBaz',
+				foo: 'bar'
+			}
+		);
+		expect(portletURL.href).toEqual(
 			'http://localhost:8080/group/control_panel/manage?p_p_id=com_liferay_roles_admin_web_portlet_RolesAdminPortlet&doAsGroupId=barBaz&_com_liferay_roles_admin_web_portlet_RolesAdminPortlet_foo=bar'
 		);
 	});
 
 	it('prepends portal url if the provided base url does not have a proper beginning', () => {
-		expect(createURL('/group/control_panel/manage')).toEqual(
+		const portletURL = createURL('/group/control_panel/manage');
+
+		expect(portletURL.href).toEqual(
 			'http://localhost:8080/group/control_panel/manage'
 		);
 	});
