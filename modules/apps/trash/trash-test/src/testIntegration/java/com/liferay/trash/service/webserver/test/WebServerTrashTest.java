f0734c32adc6232a61e491417219fbe51274d681
hai.yu <8219754@liferay.com>
2019-08-08 14:47:38 +0800

    LPS-99222 SF, delete unclean data. The code was added by the commit (LPS-40597 183254865e4964f1cfb1ab3d9e54f4e176506ef5), but it is not used in the test for now. In addition, it doesn't been deleted after the test finishes.

--- modules/apps/trash/trash-test/src/testIntegration/java/com/liferay/trash/service/webserver/test/WebServerTrashTest.java
+++ modules/apps/trash/trash-test/src/testIntegration/java/com/liferay/trash/service/webserver/test/WebServerTrashTest.java
@@ -19,7 +19,6 @@ import com.liferay.document.library.kernel.service.DLAppLocalServiceUtil;
 import com.liferay.document.library.kernel.service.DLTrashServiceUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
@@ -45,7 +44,6 @@ import com.liferay.portal.kernel.webdav.methods.Method;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.webserver.test.util.BaseWebServerTestCase;
-import com.liferay.portlet.documentlibrary.constants.DLConstants;
 import com.liferay.trash.model.TrashEntry;
 
 import java.util.HashMap;
@@ -84,23 +82,6 @@ public class WebServerTrashTest extends BaseWebServerTestCase {
 			ResourceConstants.SCOPE_COMPANY,
 			String.valueOf(TestPropsValues.getCompanyId()),
 			ActionKeys.ACCESS_IN_CONTROL_PANEL);
-
-		RoleTestUtil.addResourcePermission(
-			RoleConstants.GUEST, DLConstants.RESOURCE_NAME,
-			ResourceConstants.SCOPE_GROUP_TEMPLATE,
-			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
-			ActionKeys.VIEW);
-	}
-
-	@Override
-	public void tearDown() throws Exception {
-		super.tearDown();
-
-		RoleTestUtil.removeResourcePermission(
-			RoleConstants.GUEST, DLConstants.RESOURCE_NAME,
-			ResourceConstants.SCOPE_GROUP_TEMPLATE,
-			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
-			ActionKeys.VIEW);
 	}
 
 	@Test
