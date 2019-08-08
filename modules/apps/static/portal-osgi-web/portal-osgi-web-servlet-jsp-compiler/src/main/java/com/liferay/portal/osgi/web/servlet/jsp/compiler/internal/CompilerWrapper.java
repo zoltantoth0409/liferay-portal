256ce07cb3aad77dba20c4666a642838642713b3
Brian Chan <brian.chan@liferay.com>
2019-08-08 16:26:42 -0700

    LPS-98937 linux and windows

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/CompilerWrapper.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/CompilerWrapper.java
@@ -123,7 +123,7 @@ public class CompilerWrapper extends Compiler {
 
 	private URL _getClassURL(String className) {
 		String classNamePath = className.replace(
-			CharPool.PERIOD, CharPool.BACK_SLASH);
+			CharPool.PERIOD, File.separatorChar);
 
 		classNamePath = classNamePath.concat(".class");
 
