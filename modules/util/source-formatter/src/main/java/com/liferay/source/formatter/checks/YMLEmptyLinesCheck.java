1fb505d34104d435bdac5f8da4e6ad0d756c2a63
Alan Huang <alan.huang@liferay.com>
2019-08-09 14:20:25 +0800

    LPS-99481 Fix YMLEmptyLinesCheck in order to avoid formatting for '|-' block

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLEmptyLinesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLEmptyLinesCheck.java
@@ -14,6 +14,12 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Alan Huang
  */
@@ -23,11 +29,48 @@ public class YMLEmptyLinesCheck extends BaseFileCheck {
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
 
-		content = content.replaceAll("(?<!\n)(\n---\n)", "\n$1");
-		content = content.replaceAll("(\n---\n)(?!\n)", "$1\n");
-		content = content.replaceAll("(?<!---)\n\n(?!---)", "\n");
+		String[] contentBlocks = new String[0];
+		Matcher matcher = _styleBlockPattern.matcher(content);
 
-		return content;
+		int lastEndPos = 0;
+
+		while (matcher.find()) {
+			contentBlocks = ArrayUtil.append(
+				contentBlocks, content.substring(lastEndPos, matcher.start()));
+			contentBlocks = ArrayUtil.append(
+				contentBlocks,
+				content.substring(matcher.start(), matcher.end()));
+
+			lastEndPos = matcher.end();
+		}
+
+		if (contentBlocks.length == 0) {
+			contentBlocks = ArrayUtil.append(contentBlocks, content);
+		}
+
+		for (int i = 0; i < contentBlocks.length; i += 2) {
+			contentBlocks[i] = contentBlocks[i].replaceAll(
+				"(?<!\n)(\n---\n)", "\n$1");
+			contentBlocks[i] = contentBlocks[i].replaceAll(
+				"(\n---\n)(?!\n)", "$1\n");
+			contentBlocks[i] = contentBlocks[i].replaceAll(
+				"(?<!---)\n\n(?!---)", "\n");
+		}
+
+		if (contentBlocks.length == 1) {
+			return contentBlocks[0];
+		}
+
+		StringBundler sb = new StringBundler(contentBlocks.length);
+
+		for (String s : contentBlocks) {
+			sb.append(s);
+		}
+
+		return sb.toString();
 	}
+
+	private static final Pattern _styleBlockPattern = Pattern.compile(
+		"(?<=\\|-\n)( +)(.*?(\n|\\Z))(\\1(.*?(\n|\\Z)))*", Pattern.DOTALL);
 
 }
