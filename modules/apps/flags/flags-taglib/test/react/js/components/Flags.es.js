f6ea42aa1df41491798011164f8b41e0242fc4bd
Jorge González <jorge.gonzalez@liferay.com>
2019-08-09 15:07:29 +0200

    LPS-98033 Add test for react component

---
+++ modules/apps/flags/flags-taglib/test/react/js/components/Flags.es.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, render} from '@testing-library/react';
+import Flags from '../../../../src/main/resources/META-INF/resources/flags/react/js/components/Flags.es';
+import React from 'react';
+
+function _renderFlagsComponent({
+	companyName = 'Liferay',
+	baseData = {},
+	onlyIcon = false,
+	pathTermsOfUse = '/',
+	reasons = {},
+	signedIn = false,
+	uri = '//'
+} = {}) {
+	return render(
+		<Flags
+			baseData={baseData}
+			companyName={companyName}
+			onlyIcon={onlyIcon}
+			pathTermsOfUse={pathTermsOfUse}
+			reasons={reasons}
+			signedIn={signedIn}
+			uri={uri}
+		/>
+	);
+}
+
+describe('Flags', () => {
+	afterEach(cleanup);
+
+	it('renders', () => {
+		const {asFragment} = _renderFlagsComponent();
+
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('renders with only icon visible (text visually hidden)', () => {
+		const {getByText} = _renderFlagsComponent({onlyIcon: true});
+
+		expect(getByText('report')).toHaveClass('sr-only');
+	});
+});
