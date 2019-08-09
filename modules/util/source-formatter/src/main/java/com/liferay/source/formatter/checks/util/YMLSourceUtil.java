86b4169f6c5aa73f0fba6a9420212a0138e9ef71
Alan Huang <alan.huang@liferay.com>
2019-08-09 18:29:04 +0800

    LPS-99481 All whitespace line doesn't mean empty line here

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/util/YMLSourceUtil.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/util/YMLSourceUtil.java
@@ -17,7 +17,6 @@ package com.liferay.source.formatter.checks.util;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +35,7 @@ public class YMLSourceUtil {
 		StringBundler sb = new StringBundler();
 
 		for (String line : lines) {
-			if (Validator.isNull(line)) {
+			if (line.length() == 0) {
 				sb.append("\n");
 
 				continue;
