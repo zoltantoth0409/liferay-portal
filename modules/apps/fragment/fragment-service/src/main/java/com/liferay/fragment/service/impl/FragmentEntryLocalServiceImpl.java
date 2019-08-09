09d79ddcd1552641b08bb5cea0130d85e0b58928
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-08-09 13:46:49 +0200

    LPS-99503 Logic to move a fragment resource to the fragment collection where the fragment is moved

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLocalServiceImpl.java
@@ -14,11 +14,13 @@
 
 package com.liferay.fragment.service.impl;
 
+import com.liferay.document.library.kernel.service.DLAppLocalService;
 import com.liferay.fragment.configuration.FragmentServiceConfiguration;
 import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.exception.DuplicateFragmentEntryKeyException;
 import com.liferay.fragment.exception.FragmentEntryNameException;
 import com.liferay.fragment.exception.RequiredFragmentEntryException;
+import com.liferay.fragment.model.FragmentCollection;
 import com.liferay.fragment.model.FragmentEntry;
 import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.processor.FragmentEntryProcessorRegistry;
@@ -36,14 +38,20 @@ import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.WildcardMode;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -51,9 +59,14 @@ import com.liferay.portal.kernel.util.TempFileEntryUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
+import java.io.InputStream;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -585,7 +598,13 @@ public class FragmentEntryLocalServiceImpl
 			return fragmentEntry;
 		}
 
+		long originalFragmentCollectionId =
+			fragmentEntry.getFragmentCollectionId();
+
 		fragmentEntry.setFragmentCollectionId(fragmentCollectionId);
+
+		_copyFragmentEntryResources(
+			fragmentEntry, originalFragmentCollectionId, fragmentCollectionId);
 
 		return fragmentEntryPersistence.update(fragmentEntry);
 	}
@@ -760,6 +779,88 @@ public class FragmentEntryLocalServiceImpl
 		}
 	}
 
+	private void _copyFragmentEntryResources(
+			FragmentEntry fragmentEntry, long sourceFragmentCollectionId,
+			long targetFragmentCollectionId)
+		throws PortalException {
+
+		if (sourceFragmentCollectionId == targetFragmentCollectionId) {
+			return;
+		}
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		List<FileEntry> fileEntries = _getFileEntries(
+			sourceFragmentCollectionId, fragmentEntry);
+
+		if ((serviceContext == null) || ListUtil.isEmpty(fileEntries)) {
+			return;
+		}
+
+		FragmentCollection targetFragmentCollection =
+			fragmentCollectionPersistence.fetchByPrimaryKey(
+				targetFragmentCollectionId);
+
+		try {
+			for (FileEntry fileEntry : fileEntries) {
+				FileEntry existingFileEntry =
+					PortletFileRepositoryUtil.fetchPortletFileEntry(
+						fragmentEntry.getGroupId(),
+						targetFragmentCollection.getResourcesFolderId(),
+						fileEntry.getFileName());
+
+				if (existingFileEntry == null) {
+					byte[] bytes = null;
+
+					try (InputStream is = fileEntry.getContentStream()) {
+						bytes = FileUtil.getBytes(is);
+					}
+
+					_dlAppLocalService.addFileEntry(
+						serviceContext.getUserId(),
+						serviceContext.getScopeGroupId(),
+						targetFragmentCollection.getResourcesFolderId(),
+						fileEntry.getFileName(), null, fileEntry.getFileName(),
+						StringPool.BLANK, StringPool.BLANK, bytes,
+						serviceContext);
+				}
+			}
+		}
+		catch (Exception e) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(e, e);
+			}
+		}
+	}
+
+	private List<FileEntry> _getFileEntries(
+			long fragmentCollectionId, FragmentEntry fragmentEntry)
+		throws PortalException {
+
+		List<FileEntry> fileEntries = new ArrayList<>();
+
+		FragmentCollection fragmentCollection =
+			fragmentCollectionPersistence.fetchByPrimaryKey(
+				fragmentCollectionId);
+
+		Matcher matcher = _pattern.matcher(fragmentEntry.getHtml());
+
+		while (matcher.find()) {
+			FileEntry fileEntry =
+				PortletFileRepositoryUtil.fetchPortletFileEntry(
+					fragmentEntry.getGroupId(),
+					fragmentCollection.getResourcesFolderId(),
+					matcher.group(1));
+
+			if (fileEntry != null) {
+				fileEntries.add(fileEntry);
+			}
+		}
+
+		return fileEntries;
+	}
+
 	private String _getFragmentEntryKey(String fragmentEntryKey) {
 		if (fragmentEntryKey != null) {
 			fragmentEntryKey = fragmentEntryKey.trim();
@@ -791,11 +892,20 @@ public class FragmentEntryLocalServiceImpl
 		actionableDynamicQuery.performActions();
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		FragmentEntryLocalServiceImpl.class);
+
+	private static final Pattern _pattern = Pattern.compile(
+		"\\[resources:(.+?)\\]");
+
 	@Reference
 	private ConfigurationProvider _configurationProvider;
 
 	@Reference
 	private CustomSQL _customSQL;
+
+	@Reference
+	private DLAppLocalService _dlAppLocalService;
 
 	@Reference
 	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
