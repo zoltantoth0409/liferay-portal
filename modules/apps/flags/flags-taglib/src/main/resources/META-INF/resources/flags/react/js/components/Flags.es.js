e185613eafb3a02d92e4a625756112a84eb46bec
Jorge González <jorge.gonzalez@liferay.com>
2019-08-09 15:06:37 +0200

    LPS-98033 Add react component

---
+++ modules/apps/flags/flags-taglib/src/main/resources/META-INF/resources/flags/react/js/components/Flags.es.js
@@ -0,0 +1,191 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {fetch, objectToFormData} from 'frontend-js-web';
+import ClayButton from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import {
+	OTHER_REASON_VALUE,
+	STATUS_ERROR,
+	STATUS_LOGIN,
+	STATUS_REPORT,
+	STATUS_SUCCESS
+} from '../constants.es';
+
+import ThemeContext from '../ThemeContext.es';
+import FlagsModal from './FlagsModal.es';
+
+class Flags extends React.PureComponent {
+	static contextType = ThemeContext;
+
+	static propTypes = {
+		baseData: PropTypes.object.isRequired,
+		companyName: PropTypes.string.isRequired,
+		disabled: PropTypes.bool,
+		forceLogin: PropTypes.bool,
+		message: PropTypes.string,
+		onlyIcon: PropTypes.bool,
+		pathTermsOfUse: PropTypes.string.isRequired,
+		reasons: PropTypes.object.isRequired,
+		signedIn: PropTypes.bool,
+		uri: PropTypes.string.isRequired
+	};
+
+	static defaultProps = {
+		disabled: false,
+		forceLogin: false,
+		onlyIcon: false,
+		message: Liferay.Language.get('report'),
+		signedIn: false
+	};
+
+	constructor(props) {
+		super(props);
+
+		const {forceLogin, reasons} = this.props;
+
+		this.state = {
+			isSending: false,
+			otherReason: '',
+			selectedReason: Object.keys(reasons)[0],
+			reportDialogOpen: false,
+			reporterEmailAddress: '',
+			status: forceLogin ? STATUS_LOGIN : STATUS_REPORT
+		};
+	}
+
+	getReason() {
+		const {selectedReason, otherReason} = this.state;
+
+		if (selectedReason === OTHER_REASON_VALUE) {
+			return otherReason || Liferay.Language.get('no-reason-specified');
+		}
+		return selectedReason;
+	}
+
+	handleClickClose = () => {
+		this.setState({reportDialogOpen: false});
+	};
+
+	handleClickShow = () => {
+		this.setState({reportDialogOpen: true});
+	};
+
+	handleInputChange = event => {
+		const target = event.target;
+		const value =
+			target.type === 'checkbox' ? target.checked : target.value.trim();
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		});
+	};
+
+	handleSubmitReport = event => {
+		event.preventDefault();
+
+		const {baseData, uri, signedIn} = this.props;
+		const {reporterEmailAddress} = this.state;
+		const {namespace} = this.context;
+
+		this.setState({isSending: true}, () => {
+			const formDataObj = {
+				...baseData,
+				[`${namespace}reason`]: this.getReason()
+			};
+
+			if (!signedIn) {
+				formDataObj[
+					`${namespace}reporterEmailAddress`
+				] = reporterEmailAddress;
+			}
+
+			fetch(uri, {
+				body: objectToFormData(formDataObj),
+				method: 'post'
+			})
+				.then(({status}) => {
+					if (status === Liferay.STATUS_CODE.OK) {
+						this.setState({status: STATUS_SUCCESS});
+					} else {
+						this.setState({status: STATUS_ERROR});
+					}
+				})
+				.catch(() => this.setState({status: STATUS_ERROR}));
+		});
+	};
+
+	render() {
+		const {
+			companyName,
+			disabled,
+			message,
+			onlyIcon,
+			pathTermsOfUse,
+			reasons,
+			signedIn
+		} = this.props;
+		const {isSending, reportDialogOpen, status} = this.state;
+		const {spritemap} = this.context;
+
+		return (
+			<>
+				<ClayButton
+					className={`btn-outline-borderless btn-outline-secondary ${
+						onlyIcon ? 'lfr-portal-tooltip' : ''
+					}`}
+					data-title={onlyIcon ? message : undefined}
+					disabled={disabled}
+					displayType="secondary"
+					monospaced={onlyIcon}
+					onClick={this.handleClickShow}
+					small
+				>
+					<span
+						className={
+							!onlyIcon
+								? 'inline-item inline-item-before'
+								: undefined
+						}
+					>
+						<ClayIcon spritemap={spritemap} symbol="flag-empty" />
+					</span>
+					<span className={onlyIcon ? 'sr-only' : undefined}>
+						{message}
+					</span>
+				</ClayButton>
+				{reportDialogOpen && (
+					<FlagsModal
+						companyName={companyName}
+						handleClose={this.handleClickClose}
+						handleInputChange={this.handleInputChange}
+						handleSubmit={this.handleSubmitReport}
+						isSending={isSending}
+						pathTermsOfUse={pathTermsOfUse}
+						reasons={reasons}
+						selectedReason={this.state.selectedReason}
+						signedIn={signedIn}
+						status={status}
+					/>
+				)}
+			</>
+		);
+	}
+}
+
+export default Flags;
---
+++ modules/apps/flags/flags-taglib/src/main/resources/META-INF/resources/flags/react/js/components/FlagsModal.es.js
@@ -0,0 +1,269 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayModal from '@clayui/modal';
+import PropTypes from 'prop-types';
+import React, {useContext} from 'react';
+
+import {sub} from '../utils.es';
+import {
+	OTHER_REASON_VALUE,
+	STATUS_ERROR,
+	STATUS_LOGIN,
+	STATUS_REPORT,
+	STATUS_SUCCESS
+} from '../constants.es';
+import ThemeContext from '../ThemeContext.es';
+
+const ModalContentForm = ({
+	handleClose,
+	handleInputChange,
+	handleSubmit,
+	isSending,
+	pathTermsOfUse,
+	selectedReason,
+	reasons,
+	signedIn
+}) => {
+	const {namespace} = useContext(ThemeContext);
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<ClayModal.Body>
+				<p>
+					{sub(
+						Liferay.Language.get(
+							'you-are-about-to-report-a-violation-of-our-x.-all-reports-are-strictly-confidential'
+						),
+						[
+							<a href={pathTermsOfUse} key={pathTermsOfUse}>
+								{Liferay.Language.get('terms-of-use')}
+							</a>
+						],
+						false
+					)}
+				</p>
+				<div className="form-group">
+					<label
+						className="control-label"
+						htmlFor={`${namespace}selectedReason`}
+					>
+						{Liferay.Language.get('reason-for-the-report')}
+					</label>
+					<select
+						className="form-control"
+						id={`${namespace}selectedReason`}
+						name="selectedReason"
+						onChange={handleInputChange}
+						value={selectedReason}
+					>
+						{Object.entries(reasons).map(([value, text]) => (
+							<option key={value} value={value}>
+								{text}
+							</option>
+						))}
+						<option value={OTHER_REASON_VALUE}>
+							{Liferay.Language.get('other-reason')}
+						</option>
+					</select>
+				</div>
+				{selectedReason === OTHER_REASON_VALUE && (
+					<div className="form-group">
+						<label
+							className="control-label"
+							htmlFor={`${namespace}otherReason`}
+						>
+							{Liferay.Language.get('other-reason')}
+						</label>
+						<input
+							autoFocus
+							className="form-control"
+							id={`${namespace}otherReason`}
+							name="otherReason"
+							onChange={handleInputChange}
+						/>
+					</div>
+				)}
+				{!signedIn && (
+					<div className="form-group">
+						<label
+							className="control-label"
+							htmlFor={`${namespace}otherRreporterEmailAddresseason`}
+						>
+							{Liferay.Language.get('email-address')}
+						</label>
+						<input
+							className="form-control"
+							id={`${namespace}otherRreporterEmailAddresseason`}
+							name="reporterEmailAddress"
+							onChange={handleInputChange}
+							type="email"
+						/>
+					</div>
+				)}
+			</ClayModal.Body>
+			<ClayModal.Footer
+				last={
+					<ClayButton.Group spaced>
+						<ClayButton
+							displayType="secondary"
+							onClick={handleClose}
+						>
+							{Liferay.Language.get('cancel')}
+						</ClayButton>
+						<ClayButton
+							disabled={isSending}
+							displayType="primary"
+							type="submit"
+						>
+							{Liferay.Language.get('report')}
+						</ClayButton>
+					</ClayButton.Group>
+				}
+			/>
+		</form>
+	);
+};
+
+const ModalBodySuccess = ({companyName}) => (
+	<ClayModal.Body>
+		<p>
+			<strong>{Liferay.Language.get('thank-you-for-your-report')}</strong>
+		</p>
+		<p>
+			{sub(
+				Liferay.Language.get(
+					'although-we-cannot-disclose-our-final-decision,-we-do-review-every-report-and-appreciate-your-effort-to-make-sure-x-is-a-safe-environment-for-everyone'
+				),
+				[companyName]
+			)}
+		</p>
+	</ClayModal.Body>
+);
+
+const ModalBodyError = () => (
+	<ClayModal.Body>
+		<p>
+			<strong>
+				{Liferay.Language.get(
+					'an-error-occurred-while-sending-the-report.-please-try-again-in-a-few-minutes'
+				)}
+			</strong>
+		</p>
+	</ClayModal.Body>
+);
+
+const ModalBodyLogin = () => (
+	<ClayModal.Body>
+		<p>
+			<strong>
+				{Liferay.Language.get(
+					'please-sign-in-to-flag-this-as-inappropriate'
+				)}
+			</strong>
+		</p>
+	</ClayModal.Body>
+);
+
+const ModalBody = ({companyName, status}) => {
+	switch (status) {
+		case STATUS_LOGIN:
+			return <ModalBodyLogin />;
+
+		case STATUS_SUCCESS:
+			return <ModalBodySuccess companyName={companyName} />;
+
+		case STATUS_ERROR:
+		default:
+			return <ModalBodyError />;
+	}
+};
+
+const FlagsModal = ({
+	companyName,
+	handleClose,
+	handleInputChange,
+	handleSubmit,
+	isSending,
+	pathTermsOfUse,
+	selectedReason,
+	reasons,
+	signedIn,
+	status
+}) => {
+	const {spritemap} = useContext(ThemeContext);
+
+	return (
+		<ClayModal onClose={handleClose} size="md" spritemap={spritemap}>
+			{onClose => (
+				<>
+					<ClayModal.Header>
+						{Liferay.Language.get('report-inappropriate-content')}
+					</ClayModal.Header>
+					{status === STATUS_REPORT ? (
+						<ModalContentForm
+							handleClose={onClose}
+							handleInputChange={handleInputChange}
+							handleSubmit={handleSubmit}
+							isSending={isSending}
+							pathTermsOfUse={pathTermsOfUse}
+							reasons={reasons}
+							selectedReason={selectedReason}
+							signedIn={signedIn}
+						/>
+					) : (
+						<>
+							<ModalBody
+								companyName={companyName}
+								handleClose={onClose}
+								status={status}
+							/>
+							<ClayModal.Footer
+								last={
+									<ClayButton
+										displayType="secondary"
+										onClick={onClose}
+									>
+										{Liferay.Language.get('close')}
+									</ClayButton>
+								}
+							/>
+						</>
+					)}
+				</>
+			)}
+		</ClayModal>
+	);
+};
+
+FlagsModal.propTypes = {
+	companyName: PropTypes.string.isRequired,
+	handleClose: PropTypes.func.isRequired,
+	handleInputChange: PropTypes.func.isRequired,
+	handleSubmit: PropTypes.func.isRequired,
+	isSending: PropTypes.bool.isRequired,
+	pathTermsOfUse: PropTypes.string.isRequired,
+	selectedReason: PropTypes.string.isRequired,
+	reasons: PropTypes.object.isRequired,
+	signedIn: PropTypes.bool.isRequired,
+	status: PropTypes.oneOf([
+		STATUS_ERROR,
+		STATUS_LOGIN,
+		STATUS_REPORT,
+		STATUS_SUCCESS
+	]).isRequired
+};
+
+export default FlagsModal;
