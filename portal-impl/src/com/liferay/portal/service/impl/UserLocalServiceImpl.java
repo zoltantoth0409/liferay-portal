752b8cfc48a6ab064fd16956657d1eaffaccc8b7
JavierMoral <javier.moral@liferay.com>
2019-08-09 13:00:20 +0200

    LPS-99556 set threadlocal to ensure that password gets exported to LDAP

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1007,6 +1007,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		if (Validator.isNotNull(password1)) {
 			user.setPassword(PasswordEncryptorUtil.encrypt(password1));
 			user.setPasswordUnencrypted(password1);
+
+			PasswordModificationThreadLocal.setPasswordModified(true);
+			PasswordModificationThreadLocal.setPasswordUnencrypted(password1);
 		}
 
 		user.setPasswordEncrypted(true);
