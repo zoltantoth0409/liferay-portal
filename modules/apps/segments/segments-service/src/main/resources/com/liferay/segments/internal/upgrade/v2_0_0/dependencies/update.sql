2e27e00839fd5c1e11a3cac614fd078fffcff25b
Brian Chan <brian.chan@liferay.com>
2019-08-09 11:38:38 -0700

    LPS-99331 manually sync update.sql (see missing index)

--- modules/apps/segments/segments-service/src/main/resources/com/liferay/segments/internal/upgrade/v2_0_0/dependencies/update.sql
+++ modules/apps/segments/segments-service/src/main/resources/com/liferay/segments/internal/upgrade/v2_0_0/dependencies/update.sql
@@ -18,8 +18,8 @@ create table SegmentsExperiment (
 	classPK LONG,
 	name VARCHAR(75) null,
 	description VARCHAR(75) null,
-	status INTEGER,
-	typeSettings TEXT null
+	typeSettings TEXT null,
+	status INTEGER
 );
 
 create index IX_C7EDCF7 on SegmentsExperiment (classNameId, classPK, segmentsExperienceId, status);
@@ -28,6 +28,7 @@ create index IX_8D0D6955 on SegmentsExperiment (groupId, segmentsExperienceId, c
 create unique index IX_72C2980B on SegmentsExperiment (groupId, segmentsExperimentKey[$COLUMN_LENGTH:75$]);
 create index IX_12B591F7 on SegmentsExperiment (segmentsExperienceId, classNameId, classPK, status);
 create index IX_2FF139A2 on SegmentsExperiment (segmentsExperienceId, status);
+create index IX_127B4FCF on SegmentsExperiment (segmentsExperimentKey[$COLUMN_LENGTH:75$]);
 create index IX_DECD8397 on SegmentsExperiment (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_284003D9 on SegmentsExperiment (uuid_[$COLUMN_LENGTH:75$], groupId);
 
