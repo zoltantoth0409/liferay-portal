fe5fd752bf3f051da24d5a025ca009e92f77b11f
Pablo Molina <pablo.molina@liferay.com>
2019-08-12 11:53:05 +0200

    LPS-99320 Remove AceEditor annotations

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/AceEditor.es.js
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/AceEditor.es.js
@@ -204,10 +204,8 @@ class AceEditor extends Component {
 	_overrideSetAnnotations(session) {
 		const setAnnotations = session.setAnnotations.bind(session);
 
-		session.setAnnotations = annotations => {
-			setAnnotations(
-				annotations.filter(annotation => annotation.type !== 'info')
-			);
+		session.setAnnotations = () => {
+			setAnnotations([]);
 		};
 	}
 }
