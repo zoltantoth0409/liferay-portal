f2114eae1c41abb291dd90524c82cc0302616279
Alan Huang <alan.huang@liferay.com>
2019-08-12 15:23:13 +0800

    LPS-99481 No need to change YMLEmptyLinesCheck for '|-' block

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLEmptyLinesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLEmptyLinesCheck.java
@@ -14,12 +14,6 @@
 
 package com.liferay.source.formatter.checks;
 
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.source.formatter.checks.util.YMLSourceUtil;
-
-import java.util.regex.Pattern;
-
 /**
  * @author Alan Huang
  */
@@ -29,40 +23,11 @@ public class YMLEmptyLinesCheck extends BaseFileCheck {
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
 
-		return _formatEmptyLines(content);
+		content = content.replaceAll("(?<!\n)(\n---\n)", "\n$1");
+		content = content.replaceAll("(\n---\n)(?!\n)", "$1\n");
+		content = content.replaceAll("(?<!---)\n\n(?!---)", "\n");
+
+		return content;
 	}
-
-	private String _formatEmptyLines(String content) {
-		String[] contentBlocks = YMLSourceUtil.getContentBlocks(
-			content, _styleBlockPattern);
-
-		StringBundler sb = new StringBundler(contentBlocks.length);
-
-		for (int i = 0; i < contentBlocks.length; i++) {
-			String s = contentBlocks[i];
-
-			if ((i % 2) != 0) {
-				sb.append(s);
-				sb.append(StringPool.NEW_LINE);
-
-				continue;
-			}
-
-			s = s.replaceAll("(?<!\n)(\n---\n)", "\n$1");
-			s = s.replaceAll("(\n---\n)(?!\n)", "$1\n");
-			s = s.replaceAll("(?<!---)\n\n(?!---)", "\n");
-
-			sb.append(s);
-
-			sb.append(StringPool.NEW_LINE);
-		}
-
-		sb.setIndex(sb.index() - 1);
-
-		return sb.toString();
-	}
-
-	private static final Pattern _styleBlockPattern = Pattern.compile(
-		"(?<=\\|-\n)( +)(.*)(\n\\1.*)*");
 
 }
