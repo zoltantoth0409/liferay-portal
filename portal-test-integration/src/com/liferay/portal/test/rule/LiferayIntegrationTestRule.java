801bc5322b36075a9ac685db3dc88e99c5d08d13
Matthew Tambara <matthew.tambara@liferay.com>
2019-08-12 16:07:06 -0700

    LPS-100344 Spring will be initialized already, don't need this anymore

--- portal-test-integration/src/com/liferay/portal/test/rule/LiferayIntegrationTestRule.java
+++ portal-test-integration/src/com/liferay/portal/test/rule/LiferayIntegrationTestRule.java
@@ -14,32 +14,16 @@
 
 package com.liferay.portal.test.rule;
 
-import com.liferay.petra.log4j.Log4JUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.ClassTestRule;
 import com.liferay.portal.kernel.test.rule.CompanyProviderClassTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRunMethodTestRule;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.rule.TimeoutTestRule;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.SystemProperties;
-import com.liferay.portal.spring.hibernate.DialectDetector;
-import com.liferay.portal.util.InitUtil;
-import com.liferay.portal.util.PortalClassPathUtil;
-import com.liferay.portal.util.PropsUtil;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import org.apache.log4j.Level;
-
 import org.junit.rules.TestRule;
-import org.junit.runner.Description;
-
-import org.springframework.mock.web.MockServletContext;
 
 /**
  * @author Shuyang Zhou
@@ -58,7 +42,6 @@ public class LiferayIntegrationTestRule extends AggregateTestRule {
 		}
 
 		testRules.add(LogAssertionTestRule.INSTANCE);
-		testRules.add(_springInitializationClassTestRule);
 		testRules.add(SybaseDumpTransactionLogTestRule.INSTANCE);
 		testRules.add(ClearThreadLocalClassTestRule.INSTANCE);
 		testRules.add(UniqueStringRandomizerBumperClassTestRule.INSTANCE);
@@ -70,58 +53,5 @@ public class LiferayIntegrationTestRule extends AggregateTestRule {
 
 		return testRules.toArray(new TestRule[0]);
 	}
-
-	private static final TestRule _springInitializationClassTestRule =
-		new ClassTestRule<Void>() {
-
-			@Override
-			protected void afterClass(Description description, Void v) {
-			}
-
-			@Override
-			protected Void beforeClass(Description description) {
-				if (!InitUtil.isInitialized()) {
-					List<String> configLocations = ListUtil.fromArray(
-						PropsUtil.getArray(PropsKeys.SPRING_CONFIGS));
-
-					boolean configureLog4j = false;
-
-					if (GetterUtil.getBoolean(
-							SystemProperties.get("log4j.configure.on.startup"),
-							true)) {
-
-						SystemProperties.set(
-							"log4j.configure.on.startup", "false");
-
-						configureLog4j = true;
-					}
-
-					Log4JUtil.setLevel(
-						DialectDetector.class.getName(), Level.INFO.toString(),
-						false);
-
-					PortalClassPathUtil.initializeClassPaths(
-						new MockServletContext());
-
-					InitUtil.initWithSpring(configLocations, true, true);
-
-					if (configureLog4j) {
-						Log4JUtil.configureLog4J(
-							InitUtil.class.getClassLoader());
-
-						LogAssertionTestRule.startAssert(
-							Collections.<ExpectedLogs>emptyList());
-					}
-
-					if (System.getProperty("external-properties") == null) {
-						System.setProperty(
-							"external-properties", "portal-test.properties");
-					}
-				}
-
-				return null;
-			}
-
-		};
 
 }
