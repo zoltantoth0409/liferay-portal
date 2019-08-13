cab88898f73008f9158dc6369fe8c1412f11fdb5
André de Oliveira <andre.oliveira@liferay.com>
2019-08-12 19:01:28 -0700

    LPS-99711 portal-search-admin-web: Search Panel Category
    https://issues.liferay.com/browse/LPS-99711

---
+++ modules/apps/portal-search/portal-search-admin-web/src/main/java/com/liferay/portal/search/admin/web/internal/application/list/SearchPanelCategory.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.admin.web.internal.application.list;
+
+import com.liferay.application.list.BasePanelCategory;
+import com.liferay.application.list.PanelCategory;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.search.web.application.list.constants.SearchPanelCategoryKeys;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author André de Oliveira
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.category.key=" + PanelCategoryKeys.CONTROL_PANEL,
+		"panel.category.order:Integer=900"
+	},
+	service = PanelCategory.class
+)
+public class SearchPanelCategory extends BasePanelCategory {
+
+	@Override
+	public String getKey() {
+		return SearchPanelCategoryKeys.CONTROL_PANEL_SEARCH;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "search");
+	}
+
+}
