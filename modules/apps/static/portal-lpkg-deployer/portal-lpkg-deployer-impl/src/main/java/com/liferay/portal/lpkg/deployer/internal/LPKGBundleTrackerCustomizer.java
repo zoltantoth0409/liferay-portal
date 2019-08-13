c5a22610cd56af32b481772534e3ef7e69decc69
Matthew Tambara <matthew.tambara@liferay.com>
2019-08-13 10:42:11 -0700

    Revert "RELEASE-1628 SF"
    This reverts commit 556aa2178cd7421bd9f395452d7566756b2f9de2.

--- modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/LPKGBundleTrackerCustomizer.java
+++ modules/apps/static/portal-lpkg-deployer/portal-lpkg-deployer-impl/src/main/java/com/liferay/portal/lpkg/deployer/internal/LPKGBundleTrackerCustomizer.java
@@ -879,7 +879,7 @@ public class LPKGBundleTrackerCustomizer
 		}
 		catch (IOException ioe) {
 			if (_log.isWarnEnabled()) {
-				_log.warn("Unable to extract JSPs from " + name, ioe);
+				_log.warn("Unable to extract jsps from " + name, ioe);
 			}
 		}
 	}
