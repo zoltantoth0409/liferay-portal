aca84f2d5f5c006e7a26c6de38ace1f78c873158
Hugo Huijser <hugo.huijser@liferay.com>
2019-08-13 14:03:06 -0700

    LPS-99657 Class that implements ConfigurationBeanDeclaration should be in package *.settings.definition

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/settings/definition/BlogsPortletInstanceConfigurationBeanDeclaration.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/settings/definition/BlogsPortletInstanceConfigurationBeanDeclaration.java
@@ -12,9 +12,9 @@
  * details.
  */
 
-package com.liferay.blogs.web.internal.configuration.definition;
+package com.liferay.blogs.web.internal.settings.definition;
 
-import com.liferay.blogs.configuration.BlogsGroupServiceConfiguration;
+import com.liferay.blogs.web.internal.configuration.BlogsPortletInstanceConfiguration;
 import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaration;
 
 import org.osgi.service.component.annotations.Component;
@@ -23,12 +23,12 @@ import org.osgi.service.component.annotations.Component;
  * @author Sergio González
  */
 @Component(service = ConfigurationBeanDeclaration.class)
-public class BlogsGroupServiceConfigurationBeanDeclaration
+public class BlogsPortletInstanceConfigurationBeanDeclaration
 	implements ConfigurationBeanDeclaration {
 
 	@Override
 	public Class<?> getConfigurationBeanClass() {
-		return BlogsGroupServiceConfiguration.class;
+		return BlogsPortletInstanceConfiguration.class;
 	}
 
 }
