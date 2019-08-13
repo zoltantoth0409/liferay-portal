080db5a9874a796543108b474defca6754e28245
Koor Gai <koor.gai@liferay.com>
2019-08-13 14:18:17 -0700

    LRQA-42977 Add test-app-portlet war file for 7.0.0

---
+++ portal-web/test/functional/com/liferay/portalweb/dependencies/test-app-portlet-7.0.0.war/WEB-INF/src/com/liferay/testhook/hook/upgrade/UpgradeProcess_1_1_0.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.testhook.hook.upgrade;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class UpgradeProcess_1_1_0 extends UpgradeProcess {
+
+	@Override
+	public int getThreshold() {
+		return 110;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		if (_log.isInfoEnabled()) {
+			_log.info("Running " + UpgradeProcess_1_1_0.class.getName());
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(UpgradeProcess_1_1_0.class);
+
+}
