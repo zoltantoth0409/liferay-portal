c8152d2889a5449855ea1d3d14a11c30c61b30b4
pei-jung <pei-jung.lan@liferay.com>
2019-08-12 17:06:19 -0700

    LPS-97616 Add action command for updating account status

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/UpdateAccountStatusMVCActionCommand.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.portlet.action;
+
+import com.liferay.account.constants.AccountsPortletKeys;
+import com.liferay.account.service.AccountEntryLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AccountsPortletKeys.ACCOUNTS_ADMIN,
+		"mvc.command.name=/account_admin/update_account_status"
+	},
+	service = MVCActionCommand.class
+)
+public class UpdateAccountStatusMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		long[] accountEntryIds = ParamUtil.getLongValues(
+			actionRequest, "accountEntryIds");
+
+		if (cmd.equals(Constants.DEACTIVATE)) {
+			_accountEntryLocalService.deactivateAccountEntries(accountEntryIds);
+		}
+		else if (cmd.equals(Constants.RESTORE)) {
+			_accountEntryLocalService.activateAccountEntries(accountEntryIds);
+		}
+	}
+
+	@Reference
+	private AccountEntryLocalService _accountEntryLocalService;
+
+}
