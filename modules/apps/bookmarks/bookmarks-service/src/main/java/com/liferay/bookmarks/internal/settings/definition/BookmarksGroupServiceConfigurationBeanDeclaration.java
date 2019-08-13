0c1a9123f25951641fc63ce38c2e661eee084b3f
Hugo Huijser <hugo.huijser@liferay.com>
2019-08-13 14:18:50 -0700

    LPS-99657 Class that implements ConfigurationBeanDeclaration should be in package *.settings.definition

--- modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/internal/settings/definition/BookmarksGroupServiceConfigurationBeanDeclaration.java
+++ modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/internal/settings/definition/BookmarksGroupServiceConfigurationBeanDeclaration.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.bookmarks.internal.configuration.definition;
+package com.liferay.bookmarks.internal.settings.definition;
 
 import com.liferay.bookmarks.configuration.BookmarksGroupServiceConfiguration;
 import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaration;
