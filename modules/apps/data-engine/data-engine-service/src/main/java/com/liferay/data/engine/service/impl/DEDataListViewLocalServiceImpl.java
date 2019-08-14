732a82c4ecb5c7f9471a67bc58d5767d811581a7
Gabriel Albuquerque <gwas@cin.ufpe.br>
2019-08-14 16:25:19 -0300

    LPS-99567 Create get and count DEDataListViews methods on service

--- modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/service/impl/DEDataListViewLocalServiceImpl.java
+++ modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/service/impl/DEDataListViewLocalServiceImpl.java
@@ -21,7 +21,9 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.OrderByComparator;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -66,6 +68,23 @@ public class DEDataListViewLocalServiceImpl
 		deDataListViewPersistence.update(deDataListView);
 
 		return deDataListView;
+	}
+
+	@Override
+	public List<DEDataListView> getDEDataListViews(
+		long groupId, long companyId, long ddmStructureId, int start, int end,
+		OrderByComparator<DEDataListView> orderByComparator) {
+
+		return deDataListViewPersistence.findByG_C_D(
+			groupId, companyId, ddmStructureId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getDEDataListViewsCount(
+		long groupId, long companyId, long ddmStructureId) {
+
+		return deDataListViewPersistence.countByG_C_D(
+			groupId, companyId, ddmStructureId);
 	}
 
 	@Reference
