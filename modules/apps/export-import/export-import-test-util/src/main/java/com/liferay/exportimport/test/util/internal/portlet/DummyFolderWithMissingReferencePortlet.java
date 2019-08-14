f0e987a4ceb4144a749c81af68e5e2f23d7d1484
Hugo Huijser <hugo.huijser@liferay.com>
2019-08-14 13:48:11 -0700

    LPS-99657 In the package, 'internal' should come first after the symbolic name of the bundle

--- modules/apps/export-import/export-import-test-util/src/main/java/com/liferay/exportimport/test/util/internal/portlet/DummyFolderWithMissingReferencePortlet.java
+++ modules/apps/export-import/export-import-test-util/src/main/java/com/liferay/exportimport/test/util/internal/portlet/DummyFolderWithMissingReferencePortlet.java
@@ -12,16 +12,14 @@
  * details.
  */
 
-package com.liferay.exportimport.test.util.web.internal.portlet;
+package com.liferay.exportimport.test.util.internal.portlet;
 
-import com.liferay.exportimport.kernel.lar.PortletDataHandler;
 import com.liferay.exportimport.test.util.constants.DummyFolderPortletKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Akos Thurzo
@@ -29,16 +27,10 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"javax.portlet.name=" + DummyFolderPortletKeys.DUMMY_FOLDER,
+		"javax.portlet.name=" + DummyFolderPortletKeys.DUMMY_FOLDER_WITH_MISSING_REFERENCE,
 		"javax.portlet.resource-bundle=content.Language"
 	},
-	service = {DummyFolderPortlet.class, Portlet.class}
+	service = {DummyFolderWithMissingReferencePortlet.class, Portlet.class}
 )
-public class DummyFolderPortlet extends MVCPortlet {
-
-	@Reference(
-		target = "(javax.portlet.name=" + DummyFolderPortletKeys.DUMMY_FOLDER + ")"
-	)
-	private PortletDataHandler _dummyFolderPortletDataHandler;
-
+public class DummyFolderWithMissingReferencePortlet extends MVCPortlet {
 }
