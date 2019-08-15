4e7b10bd67f1ba3d02f002e8958fd4d122b67cd2
Hugo Huijser <hugo.huijser@liferay.com>
2019-08-14 17:13:17 -0700

    LPS-99657 Class that implements PermissionUpdateHandler should be in package *.security.permission

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/security/permission/LayoutPermissionUpdateHandler.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/security/permission/LayoutPermissionUpdateHandler.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.layout.internal.service.permission;
+package com.liferay.layout.internal.security.permission;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
