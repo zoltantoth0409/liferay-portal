9b574869b4b3d25708864b2229ea3fb71249ae75
drewbrokke <drew.brokke@liferay.com>
2019-08-15 12:09:00 -0500

    LPS-99839 account-api - adds AccountRole and AccountRoleManager interfaces

---
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/role/AccountRoleManager.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.role;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Drew Brokke
+ */
+@ProviderType
+public interface AccountRoleManager {
+
+	public AccountRole addAccountRole(
+			long userId, String name, Map<Locale, String> titleMap,
+			Map<Locale, String> descriptionMap)
+		throws PortalException;
+
+	public List<AccountRole> getAccountRoles(long companyId);
+
+}
