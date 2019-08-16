179b6e263d049e123884cd05588a7e290ed28cc0
Javier Gamarra <nhpatt@gmail.com>
2019-08-16 06:54:29 +0000

    LPS-96698 enable more tests

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/graphql/v1_0/test/ContentStructureGraphQLTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/graphql/v1_0/test/ContentStructureGraphQLTest.java
@@ -15,15 +15,92 @@
 package com.liferay.headless.delivery.graphql.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeResponse;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerTracker;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.DDMStructureConstants;
+import com.liferay.dynamic.data.mapping.storage.StorageType;
+import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestHelper;
+import com.liferay.headless.delivery.client.dto.v1_0.ContentStructure;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
 
-import org.junit.Ignore;
+import java.io.InputStream;
+
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class ContentStructureGraphQLTest
 	extends BaseContentStructureGraphQLTestCase {
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"name"};
+	}
+
+	@Override
+	protected ContentStructure testContentStructure_addContentStructure()
+		throws Exception {
+
+		DDMStructureTestHelper ddmStructureTestHelper =
+			new DDMStructureTestHelper(
+				PortalUtil.getClassNameId(JournalArticle.class), testGroup);
+
+		return _toContentStructure(
+			ddmStructureTestHelper.addStructure(
+				PortalUtil.getClassNameId(JournalArticle.class),
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				_deserialize(_read("test-structured-content-structure.json")),
+				StorageType.JSON.getValue(),
+				DDMStructureConstants.TYPE_DEFAULT));
+	}
+
+	private DDMForm _deserialize(String content) {
+		DDMFormDeserializer ddmFormDeserializer =
+			_ddmFormDeserializerTracker.getDDMFormDeserializer("json");
+
+		DDMFormDeserializerDeserializeRequest.Builder builder =
+			DDMFormDeserializerDeserializeRequest.Builder.newBuilder(content);
+
+		DDMFormDeserializerDeserializeResponse
+			ddmFormDeserializerDeserializeResponse =
+				ddmFormDeserializer.deserialize(builder.build());
+
+		return ddmFormDeserializerDeserializeResponse.getDDMForm();
+	}
+
+	private String _read(String fileName) throws Exception {
+		Class<?> clazz = getClass();
+
+		InputStream inputStream = clazz.getResourceAsStream(
+			"dependencies/" + fileName);
+
+		return StringUtil.read(inputStream);
+	}
+
+	private ContentStructure _toContentStructure(DDMStructure structure) {
+		return new ContentStructure() {
+			{
+				dateCreated = structure.getCreateDate();
+				dateModified = structure.getModifiedDate();
+				id = structure.getStructureId();
+				name = structure.getName(LocaleUtil.getDefault());
+				siteId = structure.getGroupId();
+			}
+		};
+	}
+
+	@Inject
+	private DDMFormDeserializerTracker _ddmFormDeserializerTracker;
+
 }
