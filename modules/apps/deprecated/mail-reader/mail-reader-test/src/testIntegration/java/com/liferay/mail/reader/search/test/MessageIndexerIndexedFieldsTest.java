8a323bf732cad23557bcc4a55d3205fada3fced2
Brian Chan <brian.chan@liferay.com>
2019-08-17 09:53:41 -0700

    Revert "LPS-98200 Temporarily ignore mail-reader integration tests"
    This reverts commit b67c2464c01118239ecf63624ce2a9543897e9e3.

--- modules/apps/deprecated/mail-reader/mail-reader-test/src/testIntegration/java/com/liferay/mail/reader/search/test/MessageIndexerIndexedFieldsTest.java
+++ modules/apps/deprecated/mail-reader/mail-reader-test/src/testIntegration/java/com/liferay/mail/reader/search/test/MessageIndexerIndexedFieldsTest.java
@@ -43,7 +43,6 @@ import java.util.Map;
 
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -51,7 +50,6 @@ import org.junit.runner.RunWith;
 /**
  * @author Luan Maoski
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class MessageIndexerIndexedFieldsTest {
 
--- modules/apps/deprecated/mail-reader/mail-reader-test/src/testIntegration/java/com/liferay/mail/reader/service/persistence/test/AttachmentPersistenceTest.java
+++ modules/apps/deprecated/mail-reader/mail-reader-test/src/testIntegration/java/com/liferay/mail/reader/service/persistence/test/AttachmentPersistenceTest.java
@@ -49,7 +49,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -57,7 +56,6 @@ import org.junit.runner.RunWith;
 /**
  * @generated
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class AttachmentPersistenceTest {
 
