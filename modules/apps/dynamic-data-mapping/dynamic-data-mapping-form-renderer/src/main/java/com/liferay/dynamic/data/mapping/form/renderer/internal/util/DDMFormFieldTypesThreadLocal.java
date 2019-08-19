c0b4506336541d697b87e4125509388f08c49b2a
Leonardo Barros <leonardo.barros@liferay.com>
2019-08-19 13:40:37 -0300

    LPS-98857 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/util/DDMFormFieldTypesThreadLocal.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/util/DDMFormFieldTypesThreadLocal.java
@@ -19,41 +19,35 @@ import com.liferay.portal.kernel.cache.thread.local.ThreadLocalCache;
 import com.liferay.portal.kernel.cache.thread.local.ThreadLocalCacheManager;
 import com.liferay.portal.kernel.util.GetterUtil;
 
-import java.util.function.Supplier;
-
 /**
  * @author Bruno Basto
  */
 public class DDMFormFieldTypesThreadLocal {
 
 	public static boolean isFieldTypesRequested() {
-		ThreadLocalCache<Boolean> threadLocalCache =
-			_threadLocalCacheSupplier.get();
+		ThreadLocalCache<Boolean> threadLocalCache = _getThreadLocalCache();
 
 		return GetterUtil.getBoolean(
 			threadLocalCache.get(_FIELD_TYPES_REQUESTED));
 	}
 
 	public static void removeAll() {
-		ThreadLocalCache<Boolean> threadLocalCache =
-			_threadLocalCacheSupplier.get();
+		ThreadLocalCache<Boolean> threadLocalCache = _getThreadLocalCache();
 
 		threadLocalCache.removeAll();
 	}
 
 	public static void setFieldTypesRequested(boolean fieldTypesRequested) {
-		ThreadLocalCache<Boolean> threadLocalCache =
-			_threadLocalCacheSupplier.get();
+		ThreadLocalCache<Boolean> threadLocalCache = _getThreadLocalCache();
 
 		threadLocalCache.put(_FIELD_TYPES_REQUESTED, fieldTypesRequested);
 	}
 
-	private static final String _FIELD_TYPES_REQUESTED = "fieldTypesRequested";
+	private static ThreadLocalCache<Boolean> _getThreadLocalCache() {
+		return ThreadLocalCacheManager.getThreadLocalCache(
+			Lifecycle.REQUEST, DDMFormFieldTypesThreadLocal.class.getName());
+	}
 
-	private static final Supplier<ThreadLocalCache<Boolean>>
-		_threadLocalCacheSupplier =
-			() -> ThreadLocalCacheManager.getThreadLocalCache(
-				Lifecycle.REQUEST,
-				DDMFormFieldTypesThreadLocal.class.getName());
+	private static final String _FIELD_TYPES_REQUESTED = "fieldTypesRequested";
 
 }
