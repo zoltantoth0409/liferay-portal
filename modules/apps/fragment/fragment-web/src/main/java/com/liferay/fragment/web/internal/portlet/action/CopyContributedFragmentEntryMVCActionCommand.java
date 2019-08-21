de8962fbc3f2f9214a9e74f638e258b5dda1223b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-08-21 11:36:37 +0100

    LPS-100025 Adds the image preview when we are copying contributed fragments

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/CopyContributedFragmentEntryMVCActionCommand.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/CopyContributedFragmentEntryMVCActionCommand.java
@@ -16,23 +16,36 @@ package com.liferay.fragment.web.internal.portlet.action;
 
 import com.liferay.fragment.constants.FragmentPortletKeys;
 import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.model.FragmentCollection;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.service.FragmentCollectionService;
 import com.liferay.fragment.service.FragmentEntryService;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.io.InputStream;
+
+import java.net.URL;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -86,12 +99,25 @@ public class CopyContributedFragmentEntryMVCActionCommand
 				LanguageUtil.get(LocaleUtil.getMostRelevantLocale(), "copy"));
 			sb.append(StringPool.CLOSE_PARENTHESIS);
 
+			long previewFileEntryId = 0;
+
+			String imagePreviewURL = fragmentEntry.getImagePreviewURL(
+				themeDisplay);
+
+			if (Validator.isNotNull(imagePreviewURL)) {
+				previewFileEntryId = _getPreviewFileEntryId(
+					themeDisplay.getUserId(), themeDisplay.getScopeGroupId(),
+					fragmentCollectionId,
+					themeDisplay.getPortalURL() + imagePreviewURL);
+			}
+
 			_fragmentEntryService.addFragmentEntry(
 				themeDisplay.getScopeGroupId(), fragmentCollectionId,
 				StringPool.BLANK, sb.toString(), fragmentEntry.getCss(),
 				fragmentEntry.getHtml(), fragmentEntry.getJs(),
-				fragmentEntry.getConfiguration(), 0, fragmentEntry.getType(),
-				WorkflowConstants.STATUS_APPROVED, serviceContext);
+				fragmentEntry.getConfiguration(), previewFileEntryId,
+				fragmentEntry.getType(), WorkflowConstants.STATUS_APPROVED,
+				serviceContext);
 		}
 
 		LiferayPortletResponse liferayPortletResponse =
@@ -105,9 +131,51 @@ public class CopyContributedFragmentEntryMVCActionCommand
 		sendRedirect(actionRequest, actionResponse, redirectURL.toString());
 	}
 
+	private long _getPreviewFileEntryId(
+		long userId, long groupId, long fragmentCollectionId,
+		String imagePreviewURL) {
+
+		try {
+			FragmentCollection fragmentCollection =
+				_fragmentCollectionService.fetchFragmentCollection(
+					fragmentCollectionId);
+
+			URL url = new URL(imagePreviewURL);
+
+			byte[] bytes = null;
+
+			try (InputStream is = url.openStream()) {
+				bytes = FileUtil.getBytes(is);
+			}
+
+			String shortFileName = FileUtil.getShortFileName(imagePreviewURL);
+
+			FileEntry fileEntry = PortletFileRepositoryUtil.addPortletFileEntry(
+				groupId, userId, FragmentCollection.class.getName(),
+				fragmentCollectionId, FragmentPortletKeys.FRAGMENT,
+				fragmentCollection.getResourcesFolderId(), bytes, shortFileName,
+				MimeTypesUtil.getContentType(shortFileName), false);
+
+			return fileEntry.getFileEntryId();
+		}
+		catch (Exception e) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to get preview entry image URL", e);
+			}
+		}
+
+		return 0;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CopyContributedFragmentEntryMVCActionCommand.class);
+
 	@Reference
 	private FragmentCollectionContributorTracker
 		_fragmentCollectionContributorTracker;
+
+	@Reference
+	private FragmentCollectionService _fragmentCollectionService;
 
 	@Reference
 	private FragmentEntryService _fragmentEntryService;
