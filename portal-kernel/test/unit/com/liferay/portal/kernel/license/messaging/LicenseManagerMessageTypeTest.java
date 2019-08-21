ed16a96b09c88a270087d7d47e6e7dc94d66fb90
Igor Beslic <igor.beslic@liferay.com>
2019-08-21 22:44:06 +0200

    LPS-100148 portal-kernel - add test for licenseManagerMessageType enumeration

---
+++ portal-kernel/test/unit/com/liferay/portal/kernel/license/messaging/LicenseManagerMessageTypeTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.license.messaging;
+
+import com.liferay.portal.kernel.messaging.Message;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Igor Beslic
+ */
+public class LicenseManagerMessageTypeTest {
+
+	public void testCreateMessage() {
+		LicenseManagerMessageType validateLCSLicenseManagerMessageType =
+			LicenseManagerMessageType.VALIDATE_LCS;
+
+		Message message = validateLCSLicenseManagerMessageType.createMessage();
+
+		Object payload = message.getPayload();
+
+		Assert.assertTrue(
+			"message payload is string", payload instanceof String);
+
+		Assert.assertEquals("{\"type\": \"liferay/lcs_status\"}", payload);
+
+		message = validateLCSLicenseManagerMessageType.createMessage(
+			LCSPortletState.NO_SUBSCRIPTION);
+
+		payload = message.getPayload();
+
+		Assert.assertTrue(
+			"message payload is string", payload instanceof String);
+
+		Assert.assertEquals(
+			"{\"state\": 4, \"type\": \"liferay/lcs_status\"}", payload);
+	}
+
+}
