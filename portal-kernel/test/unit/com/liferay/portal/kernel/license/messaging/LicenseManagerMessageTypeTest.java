c71edff72468cbe7d0d0061d6701ec496cc5509e
Igor Beslic <igor.beslic@liferay.com>
2019-08-22 06:58:35 +0200

    LPS-100148 portal-kernel - TEST - FIX test

--- portal-kernel/test/unit/com/liferay/portal/kernel/license/messaging/LicenseManagerMessageTypeTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/license/messaging/LicenseManagerMessageTypeTest.java
@@ -17,24 +17,28 @@ package com.liferay.portal.kernel.license.messaging;
 import com.liferay.portal.kernel.messaging.Message;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author Igor Beslic
  */
 public class LicenseManagerMessageTypeTest {
 
+	@Test
 	public void testCreateMessage() {
 		LicenseManagerMessageType validateLCSLicenseManagerMessageType =
-			LicenseManagerMessageType.VALIDATE_LCS;
+			LicenseManagerMessageType.LCS_AVAILABLE;
 
 		Message message = validateLCSLicenseManagerMessageType.createMessage();
 
 		Object payload = message.getPayload();
 
+		Assert.assertEquals("liferay/lcs_status", message.getDestinationName());
+
 		Assert.assertTrue(
 			"message payload is string", payload instanceof String);
 
-		Assert.assertEquals("{\"type\": \"liferay/lcs_status\"}", payload);
+		Assert.assertEquals("{\"type\": \"LCS_AVAILABLE\"}", payload);
 
 		message = validateLCSLicenseManagerMessageType.createMessage(
 			LCSPortletState.NO_SUBSCRIPTION);
@@ -45,7 +49,7 @@ public class LicenseManagerMessageTypeTest {
 			"message payload is string", payload instanceof String);
 
 		Assert.assertEquals(
-			"{\"state\": 4, \"type\": \"liferay/lcs_status\"}", payload);
+			"{\"state\": 4, \"type\": \"LCS_AVAILABLE\"}", payload);
 	}
 
 }
