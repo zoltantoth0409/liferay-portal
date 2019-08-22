ccb102c1418b75a59e21e6b70eed632203682ead
David Arques <davidarchena@gmail.com>
2019-08-22 12:44:57 +0200

    LPS-99884 Add editable property to status

--- modules/apps/segments/segments-api/src/main/java/com/liferay/segments/constants/SegmentsExperimentConstants.java
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/constants/SegmentsExperimentConstants.java
@@ -75,8 +75,8 @@ public class SegmentsExperimentConstants {
 
 	public enum Status {
 
-		COMPLETED(STATUS_COMPLETED, "COMPLETED", "completed", true),
-		DRAFT(STATUS_DRAFT, "DRAFT", "draft", true) {
+		COMPLETED(STATUS_COMPLETED, "COMPLETED", "completed", false, true),
+		DRAFT(STATUS_DRAFT, "DRAFT", "draft", true, true) {
 
 			@Override
 			public Set<Status> validTransitions() {
@@ -86,7 +86,7 @@ public class SegmentsExperimentConstants {
 
 		},
 		FINISHED_NO_WINNER(
-			STATUS_FINISHED_NO_WINNER, "FINISHED_NO_WINNER", "no-winner",
+			STATUS_FINISHED_NO_WINNER, "FINISHED_NO_WINNER", "no-winner", false,
 			true) {
 
 			@Override
@@ -96,7 +96,7 @@ public class SegmentsExperimentConstants {
 
 		},
 		FINISHED_WINNER_DECLARED(
-			STATUS_FINISHED_WINNER, "FINISHED_WINNER", "winner", true) {
+			STATUS_FINISHED_WINNER, "FINISHED_WINNER", "winner", false, true) {
 
 			@Override
 			public Set<Status> validTransitions() {
@@ -104,7 +104,7 @@ public class SegmentsExperimentConstants {
 			}
 
 		},
-		PAUSED(STATUS_PAUSED, "PAUSED", "paused", true) {
+		PAUSED(STATUS_PAUSED, "PAUSED", "paused", false, true) {
 
 			@Override
 			public Set<Status> validTransitions() {
@@ -112,7 +112,7 @@ public class SegmentsExperimentConstants {
 			}
 
 		},
-		RUNNING(STATUS_RUNNING, "RUNNING", "running", true) {
+		RUNNING(STATUS_RUNNING, "RUNNING", "running", false, true) {
 
 			@Override
 			public Set<Status> validTransitions() {
@@ -124,7 +124,7 @@ public class SegmentsExperimentConstants {
 			}
 
 		},
-		SCHEDULED(STATUS_SCHEDULED, "SCHEDULED", "scheduled", true) {
+		SCHEDULED(STATUS_SCHEDULED, "SCHEDULED", "scheduled", false, true) {
 
 			@Override
 			public Set<Status> validTransitions() {
@@ -132,7 +132,7 @@ public class SegmentsExperimentConstants {
 			}
 
 		},
-		TERMINATED(STATUS_TERMINATED, "TERMINATED", "terminated", false);
+		TERMINATED(STATUS_TERMINATED, "TERMINATED", "terminated", false, true);
 
 		public static int[] exclusiveStates() {
 			Stream<Status> stream = Arrays.stream(Status.values());
@@ -212,6 +212,10 @@ public class SegmentsExperimentConstants {
 			return _value;
 		}
 
+		public boolean isEditable() {
+			return _editable;
+		}
+
 		public boolean isExclusive() {
 			return _exclusive;
 		}
@@ -226,14 +230,17 @@ public class SegmentsExperimentConstants {
 		}
 
 		private Status(
-			int value, String stringValue, String label, boolean exclusive) {
+			int value, String stringValue, String label, boolean editable,
+			boolean exclusive) {
 
 			_value = value;
 			_stringValue = stringValue;
 			_label = label;
+			_editable = editable;
 			_exclusive = exclusive;
 		}
 
+		private final boolean _editable;
 		private final boolean _exclusive;
 		private final String _label;
 		private final String _stringValue;
