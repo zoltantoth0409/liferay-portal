818638b4e0e258d1f05acb03524dc5c545b98cc5
Bruno Basto <bruno.basto@liferay.com>
2019-08-23 14:14:36 -0300

    LPS-100059 Add hook to facilitate animating content with a Sidebar on the side

---
+++ modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/hooks/useSidebarContent.es.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {useEffect} from 'react';
+
+export default (ref, isClosed) => {
+	useEffect(() => {
+		const {classList} = ref.current;
+
+		classList.add('app-builder-sidebar-content');
+
+		if (isClosed) {
+			classList.add('closed');
+		} else {
+			classList.remove('closed');
+		}
+
+		return () => classList.remove('app-builder-sidebar-content', 'closed');
+	}, [ref, isClosed]);
+};
