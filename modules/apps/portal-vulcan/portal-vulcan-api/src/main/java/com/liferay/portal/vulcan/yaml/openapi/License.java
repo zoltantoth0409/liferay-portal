84e93e338974f128450588a169822f667df9f7c7
Peter Shin <peter.shin@liferay.com>
2019-08-23 09:38:47 -0700

    LPS-100333 Add support for the license object in rest-openapi.yaml

--- modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/yaml/openapi/License.java
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/yaml/openapi/License.java
@@ -17,43 +17,25 @@ package com.liferay.portal.vulcan.yaml.openapi;
 /**
  * @author Peter Shin
  */
-public class Info {
+public class License {
 
-	public String getDescription() {
-		return _description;
+	public String getName() {
+		return _name;
 	}
 
-	public License getLicense() {
-		return _license;
+	public String getUrl() {
+		return _url;
 	}
 
-	public String getTitle() {
-		return _title;
+	public void setName(String name) {
+		_name = name;
 	}
 
-	public String getVersion() {
-		return _version;
+	public void setUrl(String url) {
+		_url = url;
 	}
 
-	public void setDescription(String description) {
-		_description = description;
-	}
-
-	public void setLicense(License license) {
-		_license = license;
-	}
-
-	public void setTitle(String title) {
-		_title = title;
-	}
-
-	public void setVersion(String version) {
-		_version = version;
-	}
-
-	private String _description;
-	private License _license;
-	private String _title;
-	private String _version;
+	private String _name;
+	private String _url;
 
 }
