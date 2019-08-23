e86f6a3c80f74a2d933a61910cbca0d40d55b15e
Minhchau <minhchau.dang@liferay.com>
2019-08-22 17:16:17 -0700

    LPS-100274 Apply to methods that use NIO (StreamUtil, FileChannel)

--- portal-kernel/src/com/liferay/portal/kernel/servlet/ServletResponseUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/ServletResponseUtil.java
@@ -358,6 +358,9 @@ public class ServletResponseUtil {
 					0, contentLength,
 					Channels.newChannel(httpServletResponse.getOutputStream()));
 			}
+			catch (IOException ioe) {
+				_checkSocketException(ioe);
+			}
 		}
 	}
 
@@ -395,7 +398,13 @@ public class ServletResponseUtil {
 
 		httpServletResponse.flushBuffer();
 
-		StreamUtil.transfer(inputStream, httpServletResponse.getOutputStream());
+		try {
+			StreamUtil.transfer(
+				inputStream, httpServletResponse.getOutputStream());
+		}
+		catch (IOException ioe) {
+			_checkSocketException(ioe);
+		}
 	}
 
 	public static void write(HttpServletResponse httpServletResponse, String s)
@@ -565,9 +574,14 @@ public class ServletResponseUtil {
 
 			byteArrayInputStream.skip(start);
 
-			StreamUtil.transfer(
-				byteArrayInputStream, outputStream, StreamUtil.BUFFER_SIZE,
-				false, length);
+			try {
+				StreamUtil.transfer(
+					byteArrayInputStream, outputStream, StreamUtil.BUFFER_SIZE,
+					false, length);
+			}
+			catch (IOException ioe) {
+				_checkSocketException(ioe);
+			}
 
 			return byteArrayInputStream;
 		}
@@ -576,8 +590,13 @@ public class ServletResponseUtil {
 
 			FileChannel fileChannel = fileInputStream.getChannel();
 
-			fileChannel.transferTo(
-				start, length, Channels.newChannel(outputStream));
+			try {
+				fileChannel.transferTo(
+					start, length, Channels.newChannel(outputStream));
+			}
+			catch (IOException ioe) {
+				_checkSocketException(ioe);
+			}
 
 			return fileInputStream;
 		}
@@ -587,17 +606,28 @@ public class ServletResponseUtil {
 
 			randomAccessInputStream.seek(start);
 
-			StreamUtil.transfer(
-				randomAccessInputStream, outputStream, StreamUtil.BUFFER_SIZE,
-				false, length);
+			try {
+				StreamUtil.transfer(
+					randomAccessInputStream, outputStream,
+					StreamUtil.BUFFER_SIZE, false, length);
+			}
+			catch (IOException ioe) {
+				_checkSocketException(ioe);
+			}
 
 			return randomAccessInputStream;
 		}
 
 		inputStream.skip(start);
 
-		StreamUtil.transfer(
-			inputStream, outputStream, StreamUtil.BUFFER_SIZE, false, length);
+		try {
+			StreamUtil.transfer(
+				inputStream, outputStream, StreamUtil.BUFFER_SIZE, false,
+				length);
+		}
+		catch (IOException ioe) {
+			_checkSocketException(ioe);
+		}
 
 		return inputStream;
 	}
