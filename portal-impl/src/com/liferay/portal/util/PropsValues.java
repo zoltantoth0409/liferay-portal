42c774940c5462c517b5c78d323a43de438abad2
Leonardo Barros <leonardo.barros@liferay.com>
2019-08-23 09:31:35 -0300

    LPS-98368 Lets use system settings instead

--- portal-impl/src/com/liferay/portal/util/PropsValues.java
+++ portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -749,10 +749,6 @@ public class PropsValues {
 	public static final String DATABASE_MYSQL_ENGINE = PropsUtil.get(
 		PropsKeys.DATABASE_MYSQL_ENGINE);
 
-	public static final boolean DDM_DATA_PROVIDER_ACCESS_LOCAL_NETWORK =
-		GetterUtil.getBoolean(
-			PropsUtil.get(PropsKeys.DDM_DATA_PROVIDER_ACCESS_LOCAL_NETWORK));
-
 	public static final String DEFAULT_ADMIN_EMAIL_ADDRESS_PREFIX =
 		PropsUtil.get(PropsKeys.DEFAULT_ADMIN_EMAIL_ADDRESS_PREFIX);
 
