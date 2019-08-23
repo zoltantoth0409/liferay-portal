dc6df095935a43a1c21b0b2c35aa999e92e9083d
Brian Chan <brian.chan@liferay.com>
2019-08-23 11:46:51 -0700

    LPS-99220 Epgarcia, can you update the javadocs to show what it was replaced by, I wanted to avoid a breaking change

--- modules/apps/segments/segments-api/src/main/java/com/liferay/segments/constants/SegmentsConstants.java
+++ modules/apps/segments/segments-api/src/main/java/com/liferay/segments/constants/SegmentsConstants.java
@@ -14,6 +14,12 @@
 
 package com.liferay.segments.constants;
 
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * @author Eduardo García
  */
@@ -21,6 +27,57 @@ public class SegmentsConstants {
 
 	public static final String RESOURCE_NAME = "com.liferay.segments";
 
+	/**
+	 * @deprecated As of Mueller (7.2.x)
+	 */
+	public static final long SEGMENTS_ENTRY_ID_DEFAULT = 0;
+
+	/**
+	 * @deprecated As of Mueller (7.2.x)
+	 */
+	public static final long SEGMENTS_EXPERIENCE_ID_DEFAULT = 0;
+
+	/**
+	 * @deprecated As of Mueller (7.2.x)
+	 */
+	public static final String SEGMENTS_EXPERIENCE_ID_PREFIX =
+		"segments-experience-id-";
+
+	/**
+	 * @deprecated As of Mueller (7.2.x)
+	 */
+	public static final int SEGMENTS_EXPERIENCE_PRIORITY_DEFAULT = -1;
+
 	public static final String SERVICE_NAME = "com.liferay.segments";
+
+	/**
+	 * @deprecated As of Mueller (7.2.x)
+	 */
+	public static final String SOURCE_ASAH_FARO_BACKEND = "ASAH_FARO_BACKEND";
+
+	/**
+	 * @deprecated As of Mueller (7.2.x)
+	 */
+	public static final String SOURCE_DEFAULT = "DEFAULT";
+
+	/**
+	 * @deprecated As of Mueller (7.2.x)
+	 */
+	public static String getDefaultSegmentsEntryName(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, SegmentsConstants.class);
+
+		return LanguageUtil.get(resourceBundle, "default-segment-name");
+	}
+
+	/**
+	 * @deprecated As of Mueller (7.2.x)
+	 */
+	public static String getDefaultSegmentsExperienceName(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, SegmentsConstants.class);
+
+		return LanguageUtil.get(resourceBundle, "default-experience-name");
+	}
 
 }
