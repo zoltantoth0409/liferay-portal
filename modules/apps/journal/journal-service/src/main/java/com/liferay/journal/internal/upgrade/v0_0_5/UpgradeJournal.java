c83638941b35c170423994e29006606602b2dfa2
Brian Chan <brian.chan@liferay.com>
2019-08-26 14:16:58 -0700

    LPS-99086 order

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v0_0_5/UpgradeJournal.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v0_0_5/UpgradeJournal.java
@@ -250,7 +250,7 @@ public class UpgradeJournal extends UpgradeProcess {
 		return false;
 	}
 
-	protected String convertStaticContentToDynamic(String content, long groupId)
+	protected String convertStaticContentToDynamic(long groupId, String content)
 		throws Exception {
 
 		Document document = SAXReaderUtil.read(content);
@@ -579,7 +579,7 @@ public class UpgradeJournal extends UpgradeProcess {
 				String ddmStructureKey = rs.getString("DDMStructureKey");
 
 				if (Validator.isNull(ddmStructureKey)) {
-					content = convertStaticContentToDynamic(content, groupId);
+					content = convertStaticContentToDynamic(groupId, content);
 
 					updateJournalArticle(id, name, name, content);
 
