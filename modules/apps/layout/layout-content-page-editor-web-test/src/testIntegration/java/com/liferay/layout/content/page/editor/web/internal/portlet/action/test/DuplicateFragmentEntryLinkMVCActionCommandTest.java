2038d41719e863ce587361053ce24c051d61147f
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-08-27 12:27:40 +0200

    LPS-100183 Use new class, and simplify. Remove unneeded code

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/DuplicateFragmentEntryLinkMVCActionCommandTest.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/DuplicateFragmentEntryLinkMVCActionCommandTest.java
@@ -24,6 +24,7 @@ import com.liferay.fragment.renderer.FragmentRendererContext;
 import com.liferay.fragment.service.FragmentCollectionLocalService;
 import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.fragment.service.FragmentEntryLocalService;
+import com.liferay.layout.content.page.editor.web.internal.portlet.action.test.util.MockLiferayPortletRequest;
 import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -31,14 +32,10 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.Portlet;
-import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
-import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.servlet.PortletServlet;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -48,7 +45,6 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -59,28 +55,12 @@ import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
 
-import java.io.IOException;
-
-import java.util.Collection;
-import java.util.Enumeration;
 import java.util.Locale;
-import java.util.Map;
 
-import javax.portlet.ActionParameters;
 import javax.portlet.ActionRequest;
-import javax.portlet.PortletConfig;
-import javax.portlet.PortletContext;
-import javax.portlet.PortletException;
-import javax.portlet.PortletMode;
-import javax.portlet.PortletResponse;
-import javax.portlet.RenderParameters;
-import javax.portlet.WindowState;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
-
-import javax.xml.namespace.QName;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -91,8 +71,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.portlet.MockActionRequest;
-import org.springframework.mock.web.portlet.MockPortletConfig;
 
 /**
  * @author Jürgen Kappler
@@ -140,7 +118,7 @@ public class DuplicateFragmentEntryLinkMVCActionCommandTest {
 				StringPool.BLANK, 0, testFragmentRenderer.getKey(),
 				serviceContext);
 
-		ActionRequest actionRequest = _getMockHttpServletRequest(
+		ActionRequest actionRequest = _getMockActionRequest(
 			originalFragmentEntryLink.getFragmentEntryLinkId());
 
 		JSONObject jsonObject = ReflectionTestUtil.invoke(
@@ -237,7 +215,7 @@ public class DuplicateFragmentEntryLinkMVCActionCommandTest {
 				StringUtil.randomString(), 0, StringUtil.randomString(),
 				serviceContext);
 
-		ActionRequest actionRequest = _getMockHttpServletRequest(
+		ActionRequest actionRequest = _getMockActionRequest(
 			originalFragmentEntryLink.getFragmentEntryLinkId());
 
 		JSONObject jsonObject = ReflectionTestUtil.invoke(
@@ -302,7 +280,7 @@ public class DuplicateFragmentEntryLinkMVCActionCommandTest {
 
 	@Test
 	public void testDuplicateInvalidFragmentEntryLink() throws Exception {
-		ActionRequest actionRequest = _getMockHttpServletRequest(
+		ActionRequest actionRequest = _getMockActionRequest(
 			RandomTestUtil.randomLong());
 
 		JSONObject jsonObject = ReflectionTestUtil.invoke(
@@ -318,20 +296,19 @@ public class DuplicateFragmentEntryLinkMVCActionCommandTest {
 			jsonObject.getString("error"));
 	}
 
-	private MockLiferayPortletRequest _getMockHttpServletRequest(
+	private MockLiferayPortletRequest _getMockActionRequest(
 			long fragmentEntryLinkId)
 		throws PortalException {
 
-		MockLiferayPortletRequest mockLiferayPortletRequest =
-			new MockLiferayPortletRequest();
+		MockLiferayPortletRequest mockActionRequest = new MockActionRequest();
 
-		mockLiferayPortletRequest.setAttribute(
+		mockActionRequest.setAttribute(
 			WebKeys.THEME_DISPLAY, _getThemeDisplay());
 
-		mockLiferayPortletRequest.addParameter(
+		mockActionRequest.addParameter(
 			"fragmentEntryLinkId", String.valueOf(fragmentEntryLinkId));
 
-		return mockLiferayPortletRequest;
+		return mockActionRequest;
 	}
 
 	private ThemeDisplay _getThemeDisplay() throws PortalException {
@@ -375,166 +352,12 @@ public class DuplicateFragmentEntryLinkMVCActionCommandTest {
 
 	private ServiceRegistration<FragmentRenderer> _serviceRegistration;
 
-	private static class MockLiferayPortletConfig
-		extends MockPortletConfig implements LiferayPortletConfig {
-
-		@Override
-		public Portlet getPortlet() {
-			return null;
-		}
-
-		@Override
-		public String getPortletId() {
-			return "ContentPageEditorPortlet";
-		}
-
-		@Override
-		public Enumeration<PortletMode> getPortletModes(String mimeType) {
-			return null;
-		}
-
-		@Override
-		public Map<String, QName> getPublicRenderParameterDefinitions() {
-			return null;
-		}
-
-		@Override
-		public Enumeration<WindowState> getWindowStates(String mimeType) {
-			return null;
-		}
-
-		@Override
-		public boolean isCopyRequestParameters() {
-			return false;
-		}
-
-		@Override
-		public boolean isWARFile() {
-			return false;
-		}
-
-	}
-
-	private static class MockLiferayPortletRequest
-		extends MockActionRequest implements LiferayPortletRequest {
-
-		@Override
-		public void addParameter(String name, String value) {
-			_mockHttpServletRequest.addParameter(name, value);
-
-			super.addParameter(name, value);
-		}
-
-		@Override
-		public void cleanUp() {
-		}
-
-		@Override
-		public Map<String, String[]> clearRenderParameters() {
-			return null;
-		}
-
-		@Override
-		public void defineObjects(
-			PortletConfig portletConfig, PortletResponse portletResponse) {
-		}
-
-		@Override
-		public ActionParameters getActionParameters() {
-			return null;
-		}
-
-		@Override
-		public Object getAttribute(String name) {
-			if (name.equals(JavaConstants.JAVAX_PORTLET_CONFIG)) {
-				return new MockLiferayPortletConfig();
-			}
-
-			if (name.equals(PortletServlet.PORTLET_SERVLET_REQUEST)) {
-				return _mockHttpServletRequest;
-			}
-
-			return super.getAttribute(name);
-		}
-
-		@Override
-		public long getContentLengthLong() {
-			return 0;
-		}
+	private static class MockActionRequest extends MockLiferayPortletRequest {
 
 		@Override
 		public HttpServletRequest getHttpServletRequest() {
-			return _mockHttpServletRequest;
+			return new MockHttpServletRequest();
 		}
-
-		@Override
-		public String getLifecycle() {
-			return null;
-		}
-
-		@Override
-		public HttpServletRequest getOriginalHttpServletRequest() {
-			return _mockHttpServletRequest;
-		}
-
-		@Override
-		public Part getPart(String name) throws IOException, PortletException {
-			return null;
-		}
-
-		@Override
-		public Collection<Part> getParts()
-			throws IOException, PortletException {
-
-			return null;
-		}
-
-		@Override
-		public long getPlid() {
-			return 0;
-		}
-
-		@Override
-		public Portlet getPortlet() {
-			return null;
-		}
-
-		@Override
-		public PortletContext getPortletContext() {
-			return null;
-		}
-
-		@Override
-		public String getPortletName() {
-			return null;
-		}
-
-		@Override
-		public HttpServletRequest getPortletRequestDispatcherRequest() {
-			return null;
-		}
-
-		@Override
-		public RenderParameters getRenderParameters() {
-			return null;
-		}
-
-		@Override
-		public String getUserAgent() {
-			return null;
-		}
-
-		@Override
-		public void invalidateSession() {
-		}
-
-		@Override
-		public void setPortletRequestDispatcherRequest(
-			HttpServletRequest httpServletRequest) {
-		}
-
-		private final MockHttpServletRequest _mockHttpServletRequest =
-			new MockHttpServletRequest();
 
 	}
 
