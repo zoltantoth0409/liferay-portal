9d7ee43a9867007f288eaf2b37b0a6197355484e
Pavel Savinov <pavel.savinov@liferay.com>
2019-08-27 11:25:59 +0200

    LPS-98957 Mark not supported type fields as disabled

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/GetContentMappingFieldsMVCResourceCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/GetContentMappingFieldsMVCResourceCommand.java
@@ -17,19 +17,25 @@ package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.asset.kernel.model.ClassTypeReader;
+import com.liferay.dynamic.data.mapping.model.DDMFormFieldType;
 import com.liferay.info.display.contributor.InfoDisplayField;
 import com.liferay.info.display.field.ClassTypesInfoDisplayFieldProvider;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.List;
@@ -81,10 +87,25 @@ public class GetContentMappingFieldsMVCResourceCommand
 				themeDisplay.getLocale());
 
 		for (InfoDisplayField infoDisplayField : infoDisplayFields) {
+			boolean disabled = !ArrayUtil.contains(
+				_SUPPORTED_DDM_TYPES, infoDisplayField.getType());
+
+			String label = infoDisplayField.getLabel();
+
+			if (disabled) {
+				label += StringUtil.appendParentheticalSuffix(
+					StringPool.SPACE,
+					LanguageUtil.get(
+						_portal.getHttpServletRequest(resourceRequest),
+						"not-supported"));
+			}
+
 			JSONObject jsonObject = JSONUtil.put(
+				"disabled", disabled
+			).put(
 				"key", infoDisplayField.getKey()
 			).put(
-				"label", infoDisplayField.getLabel()
+				"label", label
 			).put(
 				"type", infoDisplayField.getType()
 			);
@@ -96,8 +117,16 @@ public class GetContentMappingFieldsMVCResourceCommand
 			resourceRequest, resourceResponse, jsonArray);
 	}
 
+	private static final String[] _SUPPORTED_DDM_TYPES = {
+		DDMFormFieldType.IMAGE, DDMFormFieldType.TEXT,
+		DDMFormFieldType.TEXT_AREA, DDMFormFieldType.TEXT_HTML
+	};
+
 	@Reference
 	private ClassTypesInfoDisplayFieldProvider
 		_classTypesInfoDisplayFieldProvider;
+
+	@Reference
+	private Portal _portal;
 
 }
