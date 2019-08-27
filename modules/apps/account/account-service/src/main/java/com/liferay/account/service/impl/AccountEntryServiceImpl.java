cbf03e471b30a8b9afd982705c61c9555eec328b
alee8888 <albert.lee@liferay.com>
2019-08-27 14:13:56 -0700

    LPS-97651 For Account logo, we need logoBytes not the logoId

--- modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryServiceImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/service/impl/AccountEntryServiceImpl.java
@@ -48,14 +48,14 @@ public class AccountEntryServiceImpl extends AccountEntryServiceBaseImpl {
 	@Override
 	public AccountEntry addAccountEntry(
 			long userId, long parentAccountEntryId, String name,
-			String description, long logoId, int status)
+			String description, byte[] logoBytes, int status)
 		throws PortalException {
 
 		_portletResourcePermission.check(
 			getPermissionChecker(), null, AccountActionKeys.ADD_ACCOUNT_ENTRY);
 
 		return accountEntryLocalService.addAccountEntry(
-			userId, parentAccountEntryId, name, description, logoId, status);
+			userId, parentAccountEntryId, name, description, logoBytes, status);
 	}
 
 	@Override
