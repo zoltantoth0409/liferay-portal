7b7c248ece563e58d6f4a85f6b40285c10d74646
Brian Chan <brian.chan@liferay.com>
2019-08-26 21:17:52 -0700

    Revert "LPS-99992 SF"
    This reverts commit b47eb21a1058525ab4132ac55642720cc37584a5.

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v3_0_2/UpgradeUrlTitle.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v3_0_2/UpgradeUrlTitle.java
@@ -18,6 +18,7 @@ import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.LoggingTimer;
@@ -32,8 +33,10 @@ import java.sql.ResultSet;
 public class UpgradeUrlTitle extends UpgradeProcess {
 
 	public UpgradeUrlTitle(
+		ClassNameLocalService classNameLocalService,
 		FriendlyURLEntryLocalService friendlyURLEntryLocalService) {
 
+		_classNameLocalService = classNameLocalService;
 		_friendlyURLEntryLocalService = friendlyURLEntryLocalService;
 	}
 
@@ -50,8 +53,9 @@ public class UpgradeUrlTitle extends UpgradeProcess {
 			FriendlyURLNormalizerUtil.normalizeWithPeriodsAndSlashes(urlTitle);
 
 		return _friendlyURLEntryLocalService.getUniqueUrlTitle(
-			groupId, _JOURNAL_ARTICLE_CLASSNAMEID, resourcePrimKey,
-			normalizedURLTitle);
+			groupId,
+			_classNameLocalService.getClassNameId(JournalArticle.class),
+			resourcePrimKey, normalizedURLTitle);
 	}
 
 	protected void updateFriendlyURLEntryLocalization() throws Exception {
@@ -63,7 +67,7 @@ public class UpgradeUrlTitle extends UpgradeProcess {
 					"classNameId = ? and (urlTitle like ? or urlTitle like ",
 					"?)"))) {
 
-			ps1.setLong(1, _JOURNAL_ARTICLE_CLASSNAMEID);
+			ps1.setLong(1, PortalUtil.getClassNameId(JournalArticle.class));
 			ps1.setString(2, "%/%");
 			ps1.setString(3, "%.%");
 
@@ -110,6 +114,7 @@ public class UpgradeUrlTitle extends UpgradeProcess {
 				long groupId = rs.getLong(3);
 				String urlTitle = rs.getString(4);
 
+
 				try (PreparedStatement ps2 =
 						AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 							connection,
@@ -128,9 +133,7 @@ public class UpgradeUrlTitle extends UpgradeProcess {
 		}
 	}
 
-	private static final long _JOURNAL_ARTICLE_CLASSNAMEID =
-		PortalUtil.getClassNameId(JournalArticle.class);
-
+	private final ClassNameLocalService _classNameLocalService;
 	private final FriendlyURLEntryLocalService _friendlyURLEntryLocalService;
 
 }
