07da8b58ff2a1e1b7ffe860b9a72b8e5b9a3dd2a
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-08-29 09:16:49 +0200

    LPS-100298 Test that the user cannot select a display page for a journal article if no display page exists

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/info/display/contributor/field/test/JournalArticleDisplayPageURLDisplayContributorFieldTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/info/display/contributor/field/test/JournalArticleDisplayPageURLDisplayContributorFieldTest.java
@@ -15,15 +15,28 @@
 package com.liferay.journal.info.display.contributor.field.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.info.display.contributor.InfoDisplayContributor;
+import com.liferay.info.display.contributor.InfoDisplayContributorTracker;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -40,9 +53,35 @@ public class JournalArticleDisplayPageURLDisplayContributorFieldTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId());
+
+		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
+	}
+
+	@Test
+	public void testCannotSelectDisplayPageURLIfNoneExists() throws Exception {
+		InfoDisplayContributor<JournalArticle> infoDisplayContributor =
+			_infoDisplayContributorTracker.getInfoDisplayContributor(
+				JournalArticle.class.getName());
+
+		JournalArticle journalArticle = JournalTestUtil.addArticle(
+			_group.getGroupId(), 0);
+
+		Map<String, Object> fieldsValues =
+			infoDisplayContributor.getInfoDisplayFieldsValues(
+				journalArticle, LocaleUtil.ENGLISH);
+
+		Assert.assertNull(fieldsValues.get("displayPageURL"));
 	}
 
 	@DeleteAfterTestRun
 	private Group _group;
+
+	@Inject
+	private InfoDisplayContributorTracker _infoDisplayContributorTracker;
+
+	private ServiceContext _serviceContext;
 
 }
