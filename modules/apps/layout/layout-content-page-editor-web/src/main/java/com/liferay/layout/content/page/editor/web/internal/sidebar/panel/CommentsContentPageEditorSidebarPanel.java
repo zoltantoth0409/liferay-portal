a6cb80a79b3d3e0beb9dd807ee8bc71313793d75
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-08-30 13:06:30 +0200

    LPS-100838 Implements panels

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/sidebar/panel/CommentsContentPageEditorSidebarPanel.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.sidebar.panel;
+
+import com.liferay.layout.content.page.editor.sidebar.panel.ContentPageEditorSidebarPanel;
+import com.liferay.layout.content.page.editor.web.internal.configuration.ContentPageEditorConfiguration;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, property = "service.ranking:Integer=100",
+	service = ContentPageEditorSidebarPanel.class
+)
+public class CommentsContentPageEditorSidebarPanel
+	implements ContentPageEditorSidebarPanel {
+
+	@Override
+	public String getIcon() {
+		return "comments";
+	}
+
+	@Override
+	public String getId() {
+		return "comments";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "comments");
+	}
+
+	@Override
+	public boolean includeSeparator() {
+		return true;
+	}
+
+	@Override
+	public boolean isVisible(boolean pageIsDisplayPage) {
+		try {
+			ContentPageEditorConfiguration companyConfiguration =
+				_configurationProvider.getCompanyConfiguration(
+					ContentPageEditorConfiguration.class,
+					CompanyThreadLocal.getCompanyId());
+
+			return companyConfiguration.commentsEnabled();
+		}
+		catch (Exception e) {
+		}
+
+		return false;
+	}
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+}
---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/sidebar/panel/PageStructureContentPageEditorSidebarPanel.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.sidebar.panel;
+
+import com.liferay.layout.content.page.editor.sidebar.panel.ContentPageEditorSidebarPanel;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, property = "service.ranking:Integer=300",
+	service = ContentPageEditorSidebarPanel.class
+)
+public class PageStructureContentPageEditorSidebarPanel
+	implements ContentPageEditorSidebarPanel {
+
+	@Override
+	public String getIcon() {
+		return "pages-tree";
+	}
+
+	@Override
+	public String getId() {
+		return "page-structure";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "page-structure");
+	}
+
+}
