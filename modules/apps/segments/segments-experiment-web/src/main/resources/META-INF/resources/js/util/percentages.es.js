35163d47ff270ef594eab09773c2b0e6b7dcf3aa
Andrés Fulla <andres.fulla@liferay.com>
2019-08-30 13:01:49 +0200

    LPS-100639 Better parsing percentage and index

---
+++ modules/apps/segments/segments-experiment-web/src/main/resources/META-INF/resources/js/util/percentages.es.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/**
+ * Example: `0.22` => `"22%"`
+ */
+export function indexToPercentageString(index) {
+	return indexToPercentageNumber(index) + '%';
+}
+
+/**
+ * Example: `0.22` => `22`
+ */
+export function indexToPercentageNumber(index) {
+	return parseInt(index * 100, 10);
+}
+
+/**
+ * Example: `22` => `0.22`
+ */
+export function percentageNumberToIndex(percentageNumber) {
+	const fixedIndexString = parseFloat(percentageNumber / 100).toFixed(2);
+
+	return parseFloat(fixedIndexString);
+}
