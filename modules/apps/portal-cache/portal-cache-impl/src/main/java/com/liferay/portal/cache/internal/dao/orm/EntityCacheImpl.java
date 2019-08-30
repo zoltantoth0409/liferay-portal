2d5cf3aedbd670e48a1e6943b79b0f100c3809e5
shuyangzhou <shuyang.zhou@liferay.com>
2019-08-30 14:47:44 -0700

    LPS-100565 Change load to get, we should accept the fact that FinderCache and EntityCache could get out of sync even with the protection of Tractional cache, due to tranactional cache committing is not atomic. In case of FinderCache hit but EntityCache miss, we should make the entire cache lookup a miss.

--- modules/apps/portal-cache/portal-cache-impl/src/main/java/com/liferay/portal/cache/internal/dao/orm/EntityCacheImpl.java
+++ modules/apps/portal-cache/portal-cache-impl/src/main/java/com/liferay/portal/cache/internal/dao/orm/EntityCacheImpl.java
@@ -197,7 +197,7 @@ public class EntityCacheImpl
 			try {
 				session = sessionFactory.openSession();
 
-				return (Serializable)session.load(clazz, primaryKey);
+				return (Serializable)session.get(clazz, primaryKey);
 			}
 			finally {
 				sessionFactory.closeSession(session);
@@ -238,7 +238,7 @@ public class EntityCacheImpl
 				try {
 					session = sessionFactory.openSession();
 
-					loadResult = (Serializable)session.load(clazz, primaryKey);
+					loadResult = (Serializable)session.get(clazz, primaryKey);
 				}
 				finally {
 					if (loadResult == null) {
