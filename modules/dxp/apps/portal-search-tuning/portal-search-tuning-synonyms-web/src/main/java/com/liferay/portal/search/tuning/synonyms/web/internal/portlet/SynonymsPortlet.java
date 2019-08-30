fca8be4d4ee008efe8c3d550b4a675b1a1f9eacc
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-08-30 17:15:29 -0300

    LPS-99897 portal-search-tuning-synonyms-web: Restrict synonyms to omniadmins.
    https://issues.liferay.com/browse/LPS-99896

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/portlet/SynonymsPortlet.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/portlet/SynonymsPortlet.java
@@ -51,7 +51,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + SynonymsPortletKeys.SYNONYMS,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.security-role-ref=administrator",
 		"javax.portlet.supports.mime-type=text/html"
 	},
 	service = Portlet.class
