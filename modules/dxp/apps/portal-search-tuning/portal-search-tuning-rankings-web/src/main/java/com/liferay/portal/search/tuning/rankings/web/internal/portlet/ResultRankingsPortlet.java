40fe4ec5c4cf78ac82a05e2255746833a4813dfc
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-08-30 17:15:15 -0300

    LPS-99896 portal-search-tuning-rankings-web: Restrict rankings to omniadmins.
    https://issues.liferay.com/browse/LPS-99896

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/portlet/ResultRankingsPortlet.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/portlet/ResultRankingsPortlet.java
@@ -57,7 +57,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + ResultRankingsPortletKeys.RESULT_RANKINGS,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.security-role-ref=administrator",
 		"javax.portlet.supports.mime-type=text/html"
 	},
 	service = Portlet.class
