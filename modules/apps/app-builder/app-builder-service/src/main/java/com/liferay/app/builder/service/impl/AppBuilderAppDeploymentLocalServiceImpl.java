763acb56060c436775e3edc837dfee842117db10
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2019-09-01 19:32:00 -0300

    LPS-100419 Apply change to localServiceImpls

---
+++ modules/apps/app-builder/app-builder-service/src/main/java/com/liferay/app/builder/service/impl/AppBuilderAppDeploymentLocalServiceImpl.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.app.builder.service.impl;
+
+import com.liferay.app.builder.model.AppBuilderAppDeployment;
+import com.liferay.app.builder.service.base.AppBuilderAppDeploymentLocalServiceBaseImpl;
+import com.liferay.portal.aop.AopService;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(
+	property = "model.class.name=com.liferay.app.builder.model.AppBuilderAppDeployment",
+	service = AopService.class
+)
+public class AppBuilderAppDeploymentLocalServiceImpl
+	extends AppBuilderAppDeploymentLocalServiceBaseImpl {
+
+	@Override
+	public AppBuilderAppDeployment addAppBuilderAppDeployment(
+		long appBuilderAppId, String deploymentType, String settings) {
+
+		AppBuilderAppDeployment appBuilderAppDeployment =
+			appBuilderAppDeploymentPersistence.create(
+				counterLocalService.increment());
+
+		appBuilderAppDeployment.setAppBuilderAppId(appBuilderAppId);
+		appBuilderAppDeployment.setDeploymentType(deploymentType);
+		appBuilderAppDeployment.setSettings(settings);
+
+		return appBuilderAppDeploymentPersistence.update(
+			appBuilderAppDeployment);
+	}
+
+	@Override
+	public AppBuilderAppDeployment getAppBuilderAppDeployment(
+			long appBuilderAppId, String deploymentType)
+		throws Exception {
+
+		return appBuilderAppDeploymentPersistence.findByA_D(
+			appBuilderAppId, deploymentType);
+	}
+
+	@Override
+	public List<AppBuilderAppDeployment> getAppBuilderAppDeployments(
+		long appBuilderAppId) {
+
+		return appBuilderAppDeploymentPersistence.findByAppBuilderAppId(
+			appBuilderAppId);
+	}
+
+}
