1f9f552f4a92a75bf756d20ebd5ad6bbb8eb6cf4
Chema Balsas <jose.balsas@liferay.com>
2019-09-02 14:18:26 +0200

    LPS-95524 Adds react:component jsp tag to render react components from within jsps

---
+++ modules/apps/frontend-taglib/frontend-taglib-react/src/main/java/com/liferay/frontend/taglib/react/servlet/taglib/ComponentTag.java
@@ -0,0 +1,179 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.react.servlet.taglib;
+
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolvedPackageNameUtil;
+import com.liferay.frontend.taglib.react.internal.util.ReactRendererProvider;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.template.react.renderer.ComponentDescriptor;
+import com.liferay.portal.template.react.renderer.ReactRenderer;
+import com.liferay.taglib.util.ParamAndPropertyAncestorTagImpl;
+
+import java.util.HashSet;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+
+/**
+ * @author Chema Balsas
+ */
+public class ComponentTag extends ParamAndPropertyAncestorTagImpl {
+
+	@Override
+	public int doEndTag() throws JspException {
+		JspWriter jspWriter = pageContext.getOut();
+
+		Map<String, Object> data = getData();
+
+		try {
+			prepareData(data);
+
+			ComponentDescriptor componentDescriptor = new ComponentDescriptor(
+				getModule(), getComponentId(), new HashSet<>(),
+				isPositionInLine());
+
+			ReactRenderer reactRenderer =
+				ReactRendererProvider.getReactRenderer();
+
+			reactRenderer.renderReact(
+				request, jspWriter, componentDescriptor, data);
+		}
+		catch (Exception e) {
+			throw new JspException(e);
+		}
+		finally {
+			cleanUp();
+		}
+
+		return EVAL_PAGE;
+	}
+
+	@Override
+	public int doStartTag() {
+		return EVAL_BODY_INCLUDE;
+	}
+
+	public String getComponentId() {
+		return _componentId;
+	}
+
+	public String getModule() {
+		String namespace;
+
+		if (_setServletContext) {
+			namespace = NPMResolvedPackageNameUtil.get(servletContext);
+		}
+		else {
+			HttpServletRequest httpServletRequest =
+				(HttpServletRequest)pageContext.getRequest();
+
+			namespace = NPMResolvedPackageNameUtil.get(httpServletRequest);
+		}
+
+		return namespace + "/" + _module;
+	}
+
+	@Override
+	public void release() {
+		super.release();
+
+		_setServletContext = false;
+	}
+
+	public void setComponentId(String componentId) {
+		_componentId = componentId;
+	}
+
+	public void setData(Map<String, Object> data) {
+		_data = data;
+	}
+
+	public void setModule(String module) {
+		_module = module;
+	}
+
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+
+		_setServletContext = true;
+	}
+
+	protected void cleanUp() {
+		_componentId = null;
+		_data = null;
+		_module = null;
+	}
+
+	protected Map<String, Object> getData() {
+		return _data;
+	}
+
+	protected boolean isPositionInLine() {
+		Boolean positionInline = null;
+
+		String fragmentId = ParamUtil.getString(request, "p_f_id");
+
+		if (Validator.isNotNull(fragmentId)) {
+			positionInline = true;
+		}
+
+		if (positionInline == null) {
+			ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+			if (themeDisplay.isIsolated() ||
+				themeDisplay.isLifecycleResource() ||
+				themeDisplay.isStateExclusive()) {
+
+				positionInline = true;
+			}
+
+			PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+			String portletId = portletDisplay.getId();
+
+			if (Validator.isNotNull(portletId) &&
+				themeDisplay.isPortletEmbedded(
+					themeDisplay.getScopeGroupId(), themeDisplay.getLayout(),
+					portletId)) {
+
+				positionInline = true;
+			}
+		}
+
+		if (positionInline == null) {
+			positionInline = false;
+		}
+
+		return positionInline;
+	}
+
+	protected void prepareData(Map<String, Object> data) {
+	}
+
+	private String _componentId;
+	private Map<String, Object> _data;
+	private String _module;
+	private boolean _setServletContext;
+
+}
