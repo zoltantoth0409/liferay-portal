42ac148ea81f203f216cccf21783024b27333ad8
Gergely Mathe <gergely.mathe@liferay.com>
2019-09-02 11:29:02 +0200

    LPS-98892 We need to include the parent layouts separately

--- modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/display/context/ProcessSummaryDisplayContext.java
+++ modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/display/context/ProcessSummaryDisplayContext.java
@@ -70,15 +70,48 @@ public class ProcessSummaryDisplayContext {
 		for (long selectedLayoutId :
 				ArrayUtil.sortedUnique(selectedLayoutIds)) {
 
-			String pageName = _getPageName(
-				groupId, privateLayout, selectedLayoutId);
-
-			if (pageName != null) {
-				pageNames.add(pageName);
-			}
+			_addPageNames(groupId, privateLayout, selectedLayoutId, pageNames);
 		}
 
 		return pageNames;
+	}
+
+	private void _addPageNames(
+		long groupId, boolean privateLayout, long selectedLayoutId,
+		List<String> pageNames) {
+
+		Layout layout = LayoutLocalServiceUtil.fetchLayout(
+			groupId, privateLayout, selectedLayoutId);
+
+		if (layout == null) {
+			return;
+		}
+
+		StringBuilder sb = new StringBuilder(layout.getName());
+
+		while (layout.getParentLayoutId() !=
+					LayoutConstants.DEFAULT_PARENT_LAYOUT_ID) {
+
+			try {
+				layout = LayoutLocalServiceUtil.getParentLayout(layout);
+
+				_addPageNames(
+					groupId, privateLayout, layout.getLayoutId(), pageNames);
+
+				sb.insert(0, layout.getName() + StringPool.FORWARD_SLASH);
+			}
+			catch (PortalException pe) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(pe, pe);
+				}
+			}
+		}
+
+		String pageName = sb.toString();
+
+		if (!pageNames.contains(pageName)) {
+			pageNames.add(pageName);
+		}
 	}
 
 	private List<String> _getChildPageNames(
@@ -109,36 +142,6 @@ public class ProcessSummaryDisplayContext {
 		}
 
 		return pageNames;
-	}
-
-	private String _getPageName(
-		long groupId, boolean privateLayout, long selectedLayoutId) {
-
-		Layout layout = LayoutLocalServiceUtil.fetchLayout(
-			groupId, privateLayout, selectedLayoutId);
-
-		if (layout == null) {
-			return null;
-		}
-
-		StringBuilder sb = new StringBuilder(layout.getName());
-
-		while (layout.getParentLayoutId() !=
-					LayoutConstants.DEFAULT_PARENT_LAYOUT_ID) {
-
-			try {
-				layout = LayoutLocalServiceUtil.getParentLayout(layout);
-			}
-			catch (PortalException pe) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(pe, pe);
-				}
-			}
-
-			sb.insert(0, layout.getName() + StringPool.FORWARD_SLASH);
-		}
-
-		return sb.toString();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
