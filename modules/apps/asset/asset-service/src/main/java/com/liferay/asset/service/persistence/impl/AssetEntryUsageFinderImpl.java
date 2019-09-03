41514fbb5fbd012115cbcbe0b79912faf93a218c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-09-03 15:14:26 +0200

    LPS-100898 Adds new method to count the unique asset entry usages by assetEntryId

--- modules/apps/asset/asset-service/src/main/java/com/liferay/asset/service/persistence/impl/AssetEntryUsageFinderImpl.java
+++ modules/apps/asset/asset-service/src/main/java/com/liferay/asset/service/persistence/impl/AssetEntryUsageFinderImpl.java
@@ -15,8 +15,17 @@
 package com.liferay.asset.service.persistence.impl;
 
 import com.liferay.asset.service.persistence.AssetEntryUsageFinder;
+import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.dao.orm.Type;
+import com.liferay.portal.kernel.exception.SystemException;
+
+import java.util.Iterator;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
@@ -24,4 +33,48 @@ import org.osgi.service.component.annotations.Component;
 @Component(service = AssetEntryUsageFinder.class)
 public class AssetEntryUsageFinderImpl
 	extends AssetEntryUsageFinderBaseImpl implements AssetEntryUsageFinder {
+
+	public static final String COUNT_BY_ASSET_ENTRY_ID =
+		AssetEntryUsageFinder.class.getName() + ".countByAssetEntryId";
+
+	@Override
+	public int countByAssetEntryId(long assetEntryId) {
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = _customSQL.get(getClass(), COUNT_BY_ASSET_ENTRY_ID);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addScalar(COUNT_COLUMN_NAME, Type.LONG);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			qPos.add(assetEntryId);
+
+			Iterator<Long> itr = q.iterate();
+
+			if (itr.hasNext()) {
+				Long count = itr.next();
+
+				if (count != null) {
+					return count.intValue();
+				}
+			}
+
+			return 0;
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Reference
+	private CustomSQL _customSQL;
+
 }
