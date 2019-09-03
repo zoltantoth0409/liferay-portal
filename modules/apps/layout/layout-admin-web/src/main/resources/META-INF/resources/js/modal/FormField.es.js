c617b46b4d876b66f38929c9539bc02cea5806bb
Victor Galan <victor.galan@liferay.com>
2019-09-03 19:27:32 +0200

    LPS-100546 Add a formField component to reuse error handling

---
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/modal/FormField.es.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayIcon from '@clayui/icon';
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+
+const FormField = ({children, error, id, name}) => {
+	const hasError = error != null;
+
+	return (
+		<div
+			className={classNames({'form-group': true, 'has-error': hasError})}
+		>
+			<label htmlFor={id}>
+				{name}
+				<span className="reference-mark">
+					<ClayIcon symbol="asterisk" />
+				</span>
+			</label>
+
+			{children}
+
+			{hasError && (
+				<div className="form-feedback-item">
+					<span className="form-feedback-indicator mr-1">
+						<ClayIcon symbol="exclamation-full" />
+					</span>
+					{error}
+				</div>
+			)}
+		</div>
+	);
+};
+
+FormField.propTypes = {
+	error: PropTypes.string,
+	id: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired
+};
+
+export {FormField};
+export default FormField;
