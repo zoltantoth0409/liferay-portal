1dd4cafd483c7b743cbf59c98d47173caa2b6bfc
Sarai Díaz García <dgarciasarai@gmail.com>
2019-09-03 10:26:41 +0200

    LPS-99886 Apply new service method

--- modules/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/resource/v1_0/StatusResourceImpl.java
+++ modules/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/resource/v1_0/StatusResourceImpl.java
@@ -62,6 +62,7 @@ public class StatusResourceImpl extends BaseStatusResourceImpl {
 		return _toExperiment(
 			_segmentsExperimentService.updateSegmentsExperimentStatus(
 				String.valueOf(segmentsExperimentKey),
+				String.valueOf(status.getWinnerVariantId()),
 				optionalStatus.map(
 					SegmentsExperimentConstants.Status::getValue
 				).orElseThrow(
@@ -84,6 +85,8 @@ public class StatusResourceImpl extends BaseStatusResourceImpl {
 				name = segmentsExperiment.getName();
 				siteId = segmentsExperiment.getGroupId();
 				status = segmentsExperimentConstantsStatus.toString();
+				winnerVariantId =
+					segmentsExperiment.getWinnerSegmentsExperienceId();
 			}
 		};
 	}
