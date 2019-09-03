cf01dc942b99c170d6265f2fe0d19c156e166306
Sarai Díaz García <dgarciasarai@gmail.com>
2019-09-03 12:26:34 +0200

    LPS-100500 Run experiment implementation

--- modules/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/resource/v1_0/RunExperimentResourceImpl.java
+++ modules/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/resource/v1_0/RunExperimentResourceImpl.java
@@ -14,9 +14,23 @@
 
 package com.liferay.segments.asah.rest.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.segments.asah.rest.dto.v1_0.ExperimentVariant;
+import com.liferay.segments.asah.rest.dto.v1_0.RunExperiment;
 import com.liferay.segments.asah.rest.resource.v1_0.RunExperimentResource;
+import com.liferay.segments.constants.SegmentsExperimentConstants;
+import com.liferay.segments.model.SegmentsExperiment;
+import com.liferay.segments.model.SegmentsExperimentRel;
+import com.liferay.segments.service.SegmentsExperimentRelService;
+import com.liferay.segments.service.SegmentsExperimentService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +41,82 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = RunExperimentResource.class
 )
 public class RunExperimentResourceImpl extends BaseRunExperimentResourceImpl {
+
+	@Override
+	public RunExperiment postExperimentRun(
+			Long segmentsExperimentKey, RunExperiment runExperiment)
+		throws Exception {
+
+		ExperimentVariant[] experimentVariants = runExperiment.getVariants();
+
+		Map<String, Double> segmentsExperienceKeySplitMap = new HashMap<>();
+
+		for (ExperimentVariant experimentVariant : experimentVariants) {
+			segmentsExperienceKeySplitMap.put(
+				String.valueOf(experimentVariant.getId()),
+				experimentVariant.getTrafficSplit());
+		}
+
+		return _toRunExperiment(
+			_segmentsExperimentService.runSegmentsExperiment(
+				String.valueOf(segmentsExperimentKey),
+				runExperiment.getConfidenceLevel(),
+				segmentsExperienceKeySplitMap));
+	}
+
+	private ExperimentVariant _toExperimentVariant(
+		SegmentsExperimentRel segmentsExperimentRel) {
+
+		return new ExperimentVariant() {
+			{
+				id = segmentsExperimentRel.getSegmentsExperimentRelId();
+				trafficSplit = segmentsExperimentRel.getSplit();
+			}
+		};
+	}
+
+	private ExperimentVariant[] _toExperimentVariants(
+		List<SegmentsExperimentRel> segmentsExperimentRels) {
+
+		List<ExperimentVariant> experimentVariants = new ArrayList<>();
+
+		for (SegmentsExperimentRel segmentsExperimentRel :
+				segmentsExperimentRels) {
+
+			ExperimentVariant experimentVariant = _toExperimentVariant(
+				segmentsExperimentRel);
+
+			experimentVariants.add(experimentVariant);
+		}
+
+		return experimentVariants.toArray(new ExperimentVariant[0]);
+	}
+
+	private RunExperiment _toRunExperiment(
+			SegmentsExperiment segmentsExperiment)
+		throws PortalException {
+
+		SegmentsExperimentConstants.Status segmentsExperimentConstantsStatus =
+			SegmentsExperimentConstants.Status.valueOf(
+				segmentsExperiment.getStatus());
+
+		List<SegmentsExperimentRel> segmentsExperimentRels =
+			_segmentsExperimentRelService.getSegmentsExperimentRels(
+				segmentsExperiment.getSegmentsExperimentId());
+
+		return new RunExperiment() {
+			{
+				confidenceLevel = segmentsExperiment.getConfidenceLevel();
+				status = segmentsExperimentConstantsStatus.toString();
+				variants = _toExperimentVariants(segmentsExperimentRels);
+			}
+		};
+	}
+
+	@Reference
+	private SegmentsExperimentRelService _segmentsExperimentRelService;
+
+	@Reference
+	private SegmentsExperimentService _segmentsExperimentService;
+
 }
