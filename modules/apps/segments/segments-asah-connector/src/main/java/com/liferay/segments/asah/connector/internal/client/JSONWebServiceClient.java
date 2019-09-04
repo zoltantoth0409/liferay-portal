2211edac317c6a46d655f35e8a0da82bcf738c5f
Sarai Díaz García <dgarciasarai@gmail.com>
2019-09-04 16:54:20 +0200

    LPS-100653 Support DELETE operation in client

--- modules/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/client/JSONWebServiceClient.java
+++ modules/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/client/JSONWebServiceClient.java
@@ -23,6 +23,10 @@ import javax.ws.rs.core.MultivaluedMap;
  */
 public interface JSONWebServiceClient {
 
+	public String doDelete(
+		String url, Map<String, String> parameters,
+		Map<String, String> headers);
+
 	public String doGet(
 		String url, MultivaluedMap<String, Object> parameters,
 		Map<String, String> headers);
