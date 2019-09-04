ddaa7f2db7c7bddaa1f8d049f87d611ff5cbd808
rubenpulido <ruben.pulido@liferay.com>
2019-09-04 23:22:16 +0200

    LPS-101029 Test successfully adding structured content with structure with image field mapped to a valid base64 JPG image

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/AddStructuredContentMVCActionCommandTest.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/AddStructuredContentMVCActionCommandTest.java
@@ -144,6 +144,16 @@ public class AddStructuredContentMVCActionCommandTest {
 	}
 
 	@Test
+	public void testAddStructuredContentValidStructureWithFieldImageBase64Jpg()
+		throws Exception {
+
+		String fieldValue = _read("liferay_base_64_jpg.txt");
+
+		_testAddStructuredContentValidStructureWithFieldImage(
+			fieldValue, fieldValue.split("base64,")[1]);
+	}
+
+	@Test
 	public void testAddStructuredContentValidStructureWithFieldImageURLJpg()
 		throws Exception {
 
