1775e2327804c3c2370b9e397aa681741fc79762
pei-jung <pei-jung.lan@liferay.com>
2019-09-03 17:13:58 -0700

    LPS-97652 Implement model resource permission for account entry

---
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/security/permission/resource/AccountEntryModelResourcePermission.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.internal.security.permission.resource;
+
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.model.AccountEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.account.model.AccountEntry",
+	service = ModelResourcePermission.class
+)
+public class AccountEntryModelResourcePermission
+	implements ModelResourcePermission<AccountEntry> {
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, AccountEntry accountEntry,
+			String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, accountEntry, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, AccountEntry.class.getName(),
+				accountEntry.getAccountEntryId(), actionId);
+		}
+	}
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, long accountEntryId,
+			String actionId)
+		throws PortalException {
+
+		if (!contains(permissionChecker, accountEntryId, actionId)) {
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, AccountEntry.class.getName(), accountEntryId,
+				actionId);
+		}
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, AccountEntry accountEntry,
+			String actionId)
+		throws PortalException {
+
+		return permissionChecker.hasPermission(
+			null, AccountEntry.class.getName(),
+			accountEntry.getAccountEntryId(), actionId);
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, long accountEntryId,
+			String actionId)
+		throws PortalException {
+
+		return permissionChecker.hasPermission(
+			null, AccountEntry.class.getName(), accountEntryId, actionId);
+	}
+
+	@Override
+	public String getModelName() {
+		return AccountEntry.class.getName();
+	}
+
+	@Override
+	public PortletResourcePermission getPortletResourcePermission() {
+		return _portletResourcePermission;
+	}
+
+	@Reference(
+		target = "(resource.name=" + AccountConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+}
