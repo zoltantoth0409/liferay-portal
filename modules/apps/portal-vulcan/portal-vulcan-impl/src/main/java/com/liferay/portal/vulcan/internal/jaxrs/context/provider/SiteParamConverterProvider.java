578f2a08a1e4b9ccb43fcbc2c3ac35dd9a070137
Javier Gamarra <nhpatt@gmail.com>
2019-09-05 14:34:00 +0200

    LPS-100427 allow passing site keys as argument in siteId placeholders

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/context/provider/SiteParamConverterProvider.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.jaxrs.context.provider;
+
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author Javier Gamarra
+ */
+@Provider
+public class SiteParamConverterProvider
+	implements ParamConverter<Long>, ParamConverterProvider {
+
+	public SiteParamConverterProvider(GroupLocalService groupLocalService) {
+		_groupLocalService = groupLocalService;
+	}
+
+	@Override
+	public Long fromString(String parameter) {
+		MultivaluedMap<String, String> multivaluedMap =
+			_uriInfo.getPathParameters();
+
+		String siteId = multivaluedMap.getFirst("siteId");
+
+		if (siteId != null) {
+			Group group = _groupLocalService.fetchGroup(
+				_company.getCompanyId(), siteId);
+
+			if (group == null) {
+				group = _groupLocalService.fetchGroup(
+					GetterUtil.getLong(siteId));
+			}
+
+			if ((group != null) && group.isSite()) {
+				return group.getGroupId();
+			}
+		}
+
+		throw new NotFoundException(
+			"Unable to get a valid site with ID " + siteId);
+	}
+
+	@Override
+	public <T> ParamConverter<T> getConverter(
+		Class<T> clazz, Type type, Annotation[] annotations) {
+
+		if (Long.class.equals(clazz) && _hasSiteIdAnnotation(annotations)) {
+			return (ParamConverter<T>)this;
+		}
+
+		return null;
+	}
+
+	@Override
+	public String toString(Long parameter) {
+		return String.valueOf(parameter);
+	}
+
+	private boolean _hasSiteIdAnnotation(Annotation[] annotations) {
+		for (Annotation annotation : annotations) {
+			String annotationString = annotation.toString();
+
+			if (annotationString.equals(
+					"@javax.ws.rs.PathParam(value=siteId)")) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@Context
+	private Company _company;
+
+	private final GroupLocalService _groupLocalService;
+
+	@Context
+	private UriInfo _uriInfo;
+
+}
