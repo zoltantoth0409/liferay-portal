54cbc17f567eea049d4b3359729788b8a663a2f0
Istvan Sajtos <istvan.sajtos@liferay.com>
2019-09-06 14:07:20 +0200

    LPS-84440 portal-search-api: GroupBy request, response
    https://issues.liferay.com/browse/LPS-84440

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/groupby/GroupByResponse.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.groupby;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.search.Hits;
+
+import java.util.Map;
+
+/**
+ * @author Bryan Engler
+ * @author Michael C. Han
+ */
+@ProviderType
+public interface GroupByResponse {
+
+	public String getField();
+
+	public Hits getHits(String term);
+
+	public Map<String, Hits> getHitsMap();
+
+	public void putHits(String term, Hits hits);
+
+	public void setField(String field);
+
+}
