cfeb45454b74106632ed7e232a482214dcb78d34
Pavel Savinov <pavel.savinov@liferay.com>
2019-09-06 11:27:30 +0200

    LPS-97260 Create expando table for the test object

--- modules/apps/info/info-test/src/testIntegration/java/com/liferay/info/display/field/test/ExpandoInfoDisplayFieldProviderTest.java
+++ modules/apps/info/info-test/src/testIntegration/java/com/liferay/info/display/field/test/ExpandoInfoDisplayFieldProviderTest.java
@@ -15,6 +15,16 @@
 package com.liferay.info.display.field.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.expando.kernel.model.ExpandoTable;
+import com.liferay.expando.kernel.model.ExpandoTableConstants;
+import com.liferay.expando.kernel.service.ExpandoTableLocalService;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import org.junit.Before;
@@ -35,6 +45,24 @@ public class ExpandoInfoDisplayFieldProviderTest {
 
 	@Before
 	public void setUp() throws Exception {
+		_expandoTable = _expandoTableLocalService.addDefaultTable(
+			CompanyThreadLocal.getCompanyId(), User.class.getName());
+
+		_group = GroupTestUtil.addGroup();
+
+		_user = UserTestUtil.addUser();
 	}
+
+	@DeleteAfterTestRun
+	private ExpandoTable _expandoTable;
+
+	@Inject
+	private ExpandoTableLocalService _expandoTableLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@DeleteAfterTestRun
+	private User _user;
 
 }
