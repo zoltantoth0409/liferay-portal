a1024d308495dbe1de42a1b0eafcc8aca6b5aeaa
antonio-ortega <60252917@liferay.com>
2019-09-06 08:10:37 +0200

    LPS-99515 Content Reviewer roles should have permissions to manage workflows

--- portal-impl/src/com/liferay/portal/workflow/permission/WorkflowPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/workflow/permission/WorkflowPermissionImpl.java
@@ -54,9 +54,7 @@ public class WorkflowPermissionImpl implements WorkflowPermission {
 
 		long companyId = permissionChecker.getCompanyId();
 
-		if (permissionChecker.isCompanyAdmin() ||
-			permissionChecker.isGroupAdmin(groupId)) {
-
+		if (permissionChecker.isContentReviewer(companyId, groupId)) {
 			return Boolean.TRUE;
 		}
 
