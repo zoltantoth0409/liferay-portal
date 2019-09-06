ef33034e8c6a0e48f42aa67e4d7e20f682901fc1
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-09-06 10:25:50 +0200

    LPS-101131 Creates new interface, copy from FragmentEntryProcessorUtil

---
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-api/src/main/java/com/liferay/fragment/entry/processor/util/FragmentEntryProcessorHelper.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.entry.processor.util;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.fragment.processor.FragmentEntryProcessorContext;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@ProviderType
+public interface FragmentEntryProcessorHelper {
+
+	public String getEditableValue(
+		JSONObject jsonObject, Locale locale, long[] segmentsExperienceIds);
+
+	public Object getMappedValue(
+			JSONObject jsonObject,
+			Map<Long, Map<String, Object>> infoDisplaysFieldValues,
+			FragmentEntryProcessorContext fragmentEntryProcessorContext)
+		throws PortalException;
+
+	public Object getMappedValue(
+			JSONObject jsonObject,
+			Map<Long, Map<String, Object>> infoDisplaysFieldValues, String mode,
+			Locale locale, long previewClassPK, int previewType)
+		throws PortalException;
+
+	public boolean isAssetDisplayPage(String mode);
+
+	public boolean isMapped(JSONObject jsonObject);
+
+	public String processTemplate(
+			String html,
+			FragmentEntryProcessorContext fragmentEntryProcessorContext)
+		throws PortalException;
+
+}
