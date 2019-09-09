4a46eeea6f0ec0ba3a8f4dc6b1d0054333ba95de
Hugo Huijser <hugo.huijser@liferay.com>
2019-09-09 14:28:12 -0700

    LPS-101089 Improve logic to detect bad usage of @ProviderType

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaProviderTypeAnnotationCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaProviderTypeAnnotationCheck.java
@@ -14,12 +14,21 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.BNDSettings;
+import com.liferay.source.formatter.checks.util.BNDSourceUtil;
+import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaTerm;
+
+import java.io.IOException;
+
+import java.util.List;
 
 /**
  * @author Hugo Huijser
  */
-public class JavaProviderTypeAnnotationCheck extends BaseFileCheck {
+public class JavaProviderTypeAnnotationCheck extends BaseJavaTermCheck {
 
 	@Override
 	public boolean isLiferaySourceCheck() {
@@ -28,13 +37,57 @@ public class JavaProviderTypeAnnotationCheck extends BaseFileCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, String content) {
+			String fileName, String absolutePath, JavaTerm javaTerm,
+			String fileContent)
+		throws IOException {
 
-		if (!fileName.endsWith("ServiceImpl.java")) {
-			return content;
+		if (!javaTerm.hasAnnotation("ProviderType")) {
+			return javaTerm.getContent();
 		}
 
-		return StringUtil.removeSubstring(content, "@ProviderType\n");
+		JavaClass javaClass = (JavaClass)javaTerm;
+
+		if (!javaClass.isInterface() && !javaClass.isAbstract()) {
+			return StringUtil.replaceFirst(
+				javaTerm.getContent(), "@ProviderType\n", StringPool.BLANK);
+		}
+
+		if (javaClass.getParentJavaClass() != null) {
+			return javaTerm.getContent();
+		}
+
+		BNDSettings bndSettings = getBNDSettings(fileName);
+
+		List<String> exportPackages = BNDSourceUtil.getDefinitionValues(
+			bndSettings.getContent(), "Export-Package");
+
+		String packageName = javaClass.getPackageName();
+
+		if (exportPackages.contains(packageName)) {
+			return javaTerm.getContent();
+		}
+
+		int x = -1;
+
+		while (true) {
+			x = packageName.indexOf(StringPool.PERIOD, x + 1);
+
+			if (x == -1) {
+				return StringUtil.replaceFirst(
+					javaTerm.getContent(), "@ProviderType\n", StringPool.BLANK);
+			}
+
+			if (exportPackages.contains(
+					packageName.substring(0, x + 1) + StringPool.STAR)) {
+
+				return javaTerm.getContent();
+			}
+		}
+	}
+
+	@Override
+	protected String[] getCheckableJavaTermNames() {
+		return new String[] {JAVA_CLASS};
 	}
 
 }
