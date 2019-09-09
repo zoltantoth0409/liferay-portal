8f335dc794a1c6a54199009e7b0c45bee7c3e45e
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-09-09 16:37:55 +0200

    LPS-100540 New view to display the usages of a fragment per site

---
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/display/context/GroupFragmentEntryLinkDisplayContext.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.display.context;
+
+import com.liferay.fragment.constants.FragmentActionKeys;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalServiceUtil;
+import com.liferay.fragment.service.FragmentEntryLocalServiceUtil;
+import com.liferay.fragment.web.internal.security.permission.resource.FragmentPermission;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.util.comparator.GroupNameComparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class GroupFragmentEntryLinkDisplayContext {
+
+	public GroupFragmentEntryLinkDisplayContext(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+	}
+
+	public long getFragmentCollectionId() {
+		if (Validator.isNotNull(_fragmentCollectionId)) {
+			return _fragmentCollectionId;
+		}
+
+		_fragmentCollectionId = ParamUtil.getLong(
+			_renderRequest, "fragmentCollectionId");
+
+		return _fragmentCollectionId;
+	}
+
+	public FragmentEntry getFragmentEntry() throws PortalException {
+		if (_fragmentEntry != null) {
+			return _fragmentEntry;
+		}
+
+		_fragmentEntry = FragmentEntryLocalServiceUtil.getFragmentEntry(
+			getFragmentEntryId());
+
+		return _fragmentEntry;
+	}
+
+	public long getFragmentEntryId() {
+		if (Validator.isNotNull(_fragmentEntryId)) {
+			return _fragmentEntryId;
+		}
+
+		_fragmentEntryId = ParamUtil.getLong(_renderRequest, "fragmentEntryId");
+
+		return _fragmentEntryId;
+	}
+
+	public long getFragmentGroupUsage(Group group) {
+		Map<Group, Long> groupFragmentEntryUsages =
+			_getGroupFragmentEntryUsages();
+
+		return groupFragmentEntryUsages.get(group);
+	}
+
+	public String getRedirect() {
+		if (_redirect != null) {
+			return _redirect;
+		}
+
+		_redirect = ParamUtil.getString(_renderRequest, "redirect");
+
+		return _redirect;
+	}
+
+	public SearchContainer getSearchContainer() {
+		if (_searchContainer != null) {
+			return _searchContainer;
+		}
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)_renderRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		SearchContainer groupsSearchContainer = new SearchContainer(
+			_renderRequest, _renderResponse.createRenderURL(), null,
+			"there-are-no-fragment-usages");
+
+		groupsSearchContainer.setId("groups" + getFragmentCollectionId());
+
+		if (FragmentPermission.contains(
+				themeDisplay.getPermissionChecker(),
+				themeDisplay.getScopeGroupId(),
+				FragmentActionKeys.MANAGE_FRAGMENT_ENTRIES)) {
+
+			groupsSearchContainer.setRowChecker(
+				new EmptyOnClickRowChecker(_renderResponse));
+		}
+
+		Map<Group, Long> groupFragmentEntryUsages =
+			_getGroupFragmentEntryUsages();
+
+		List<Group> groups = new ArrayList<>(groupFragmentEntryUsages.keySet());
+
+		Collections.sort(groups, new GroupNameComparator(true));
+
+		groupsSearchContainer.setResults(groups);
+
+		groupsSearchContainer.setTotal(groupFragmentEntryUsages.size());
+
+		_searchContainer = groupsSearchContainer;
+
+		return _searchContainer;
+	}
+
+	private Map<Group, Long> _getGroupFragmentEntryUsages() {
+		if (_groupFragmentEntryUsages != null) {
+			return _groupFragmentEntryUsages;
+		}
+
+		List<FragmentEntryLink> fragmentEntryLinks =
+			FragmentEntryLinkLocalServiceUtil.
+				getFragmentEntryLinksByFragmentEntryId(getFragmentEntryId());
+
+		Stream<FragmentEntryLink> stream = fragmentEntryLinks.stream();
+
+		_groupFragmentEntryUsages = stream.collect(
+			Collectors.groupingBy(
+				fragmentEntryLink -> GroupLocalServiceUtil.fetchGroup(
+					fragmentEntryLink.getGroupId()),
+				Collectors.counting()));
+
+		return _groupFragmentEntryUsages;
+	}
+
+	private Long _fragmentCollectionId;
+	private FragmentEntry _fragmentEntry;
+	private Long _fragmentEntryId;
+	private Map<Group, Long> _groupFragmentEntryUsages;
+	private String _redirect;
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+	private SearchContainer _searchContainer;
+
+}
