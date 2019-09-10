498d4bc4901eb7f94fd947a46b494cfdefef166c
Gabriel Albuquerque <gwas@cin.ufpe.br>
2019-09-10 14:54:56 -0300

    LPS-101361 Implement add Data Definition Field Link

--- modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/service/impl/DEDataDefinitionFieldLinkLocalServiceImpl.java
+++ modules/apps/data-engine/data-engine-service/src/main/java/com/liferay/data/engine/service/impl/DEDataDefinitionFieldLinkLocalServiceImpl.java
@@ -14,23 +14,16 @@
 
 package com.liferay.data.engine.service.impl;
 
+import com.liferay.data.engine.model.DEDataDefinitionFieldLink;
 import com.liferay.data.engine.service.base.DEDataDefinitionFieldLinkLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * The implementation of the de data definition field link local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.data.engine.service.DEDataDefinitionFieldLinkLocalService</code> interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see DEDataDefinitionFieldLinkLocalServiceBaseImpl
  */
 @Component(
 	property = "model.class.name=com.liferay.data.engine.model.DEDataDefinitionFieldLink",
@@ -39,10 +32,24 @@ import org.osgi.service.component.annotations.Component;
 public class DEDataDefinitionFieldLinkLocalServiceImpl
 	extends DEDataDefinitionFieldLinkLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Use <code>com.liferay.data.engine.service.DEDataDefinitionFieldLinkLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.data.engine.service.DEDataDefinitionFieldLinkLocalServiceUtil</code>.
-	 */
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public DEDataDefinitionFieldLink addDEDataDefinitionFieldLink(
+		long groupId, long classNameId, long classPK, long ddmStructureId,
+		String fieldName) {
+
+		DEDataDefinitionFieldLink deDataDefinitionFieldLink =
+			deDataDefinitionFieldLinkPersistence.create(
+				counterLocalService.increment());
+
+		deDataDefinitionFieldLink.setGroupId(groupId);
+		deDataDefinitionFieldLink.setClassNameId(classNameId);
+		deDataDefinitionFieldLink.setClassPK(classPK);
+		deDataDefinitionFieldLink.setDdmStructureId(ddmStructureId);
+		deDataDefinitionFieldLink.setFieldName(fieldName);
+
+		return deDataDefinitionFieldLinkPersistence.update(
+			deDataDefinitionFieldLink);
+	}
 
 }
