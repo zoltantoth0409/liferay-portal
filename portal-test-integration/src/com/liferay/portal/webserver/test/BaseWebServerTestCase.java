d0381961fd871ea36c74bf68808d9cc7ff546a82
Matthew Tambara <matthew.tambara@liferay.com>
2019-09-10 16:23:40 -0700

    LPS-99222 Bring back and deprecate to avoid breaking change

---
+++ portal-test-integration/src/com/liferay/portal/webserver/test/BaseWebServerTestCase.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.webserver.test;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.webserver.WebServerServlet;
+import com.liferay.portlet.documentlibrary.service.test.BaseDLAppTestCase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.Servlet;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * @author Alexander Chow
+ * @deprecated As of Mueller (7.2.x), with no direct replacement
+ */
+@Deprecated
+public abstract class BaseWebServerTestCase extends BaseDLAppTestCase {
+
+	public MockHttpServletResponse service(
+			String method, String path, Map<String, String> headers,
+			Map<String, String> params, User user, byte[] data)
+		throws Exception {
+
+		if (headers == null) {
+			headers = new HashMap<>();
+		}
+
+		if (params == null) {
+			params = new HashMap<>();
+		}
+
+		if (user == null) {
+			user = TestPropsValues.getUser();
+		}
+
+		String requestURI = StringBundler.concat(
+			_CONTEXT_PATH, _SERVLET_PATH, _PATH_INFO_PREFACE, path);
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest(method, requestURI);
+
+		mockHttpServletRequest.setAttribute(WebKeys.USER, user);
+		mockHttpServletRequest.setContextPath(_CONTEXT_PATH);
+		mockHttpServletRequest.setParameters(params);
+		mockHttpServletRequest.setPathInfo(_PATH_INFO_PREFACE + path);
+		mockHttpServletRequest.setServletPath(_SERVLET_PATH);
+
+		if (data != null) {
+			mockHttpServletRequest.setContent(data);
+
+			String contentType = headers.remove(HttpHeaders.CONTENT_TYPE);
+
+			if (contentType != null) {
+				mockHttpServletRequest.setContentType(contentType);
+			}
+			else {
+				mockHttpServletRequest.setContentType(ContentTypes.TEXT_PLAIN);
+			}
+		}
+
+		for (Map.Entry<String, String> entry : headers.entrySet()) {
+			mockHttpServletRequest.addHeader(entry.getKey(), entry.getValue());
+		}
+
+		MockHttpServletResponse mockHttpServletResponse =
+			new MockHttpServletResponse();
+
+		mockHttpServletResponse.setCharacterEncoding(StringPool.UTF8);
+
+		Servlet httpServlet = getServlet();
+
+		httpServlet.service(mockHttpServletRequest, mockHttpServletResponse);
+
+		return mockHttpServletResponse;
+	}
+
+	protected Servlet getServlet() {
+		return new WebServerServlet();
+	}
+
+	private static final String _CONTEXT_PATH = "/documents";
+
+	private static final String _PATH_INFO_PREFACE = "";
+
+	private static final String _SERVLET_PATH = "";
+
+}
---
+++ portal-test-integration/src/com/liferay/portlet/documentlibrary/service/test/BaseDLAppTestCase.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.documentlibrary.service.test;
+
+import com.liferay.document.library.kernel.exception.NoSuchFolderException;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLAppServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.RoleTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portlet.documentlibrary.constants.DLConstants;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+/**
+ * @author Alexander Chow
+ * @deprecated As of Mueller (7.2.x), with no direct replacement
+ */
+@Deprecated
+public abstract class BaseDLAppTestCase {
+
+	public static final String CONTENT =
+		"Content: Enterprise. Open Source. For Life.";
+
+	@ClassRule
+	@Rule
+	public static final PermissionCheckerMethodTestRule
+		permissionCheckerTestRule = PermissionCheckerMethodTestRule.INSTANCE;
+
+	@Before
+	public void setUp() throws Exception {
+		_name = PrincipalThreadLocal.getName();
+
+		group = GroupTestUtil.addGroup();
+
+		targetGroup = GroupTestUtil.addGroup();
+
+		try {
+			DLAppServiceUtil.deleteFolder(
+				group.getGroupId(), DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+				"Test Folder");
+		}
+		catch (NoSuchFolderException nsfe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(nsfe, nsfe);
+			}
+		}
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				group.getGroupId(), TestPropsValues.getUserId());
+
+		parentFolder = DLAppServiceUtil.addFolder(
+			group.getGroupId(), DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			"Test Folder", RandomTestUtil.randomString(), serviceContext);
+
+		RoleTestUtil.addResourcePermission(
+			RoleConstants.GUEST, DLConstants.RESOURCE_NAME,
+			ResourceConstants.SCOPE_GROUP, String.valueOf(group.getGroupId()),
+			ActionKeys.VIEW);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		PrincipalThreadLocal.setName(_name);
+
+		RoleTestUtil.removeResourcePermission(
+			RoleConstants.GUEST, DLConstants.RESOURCE_NAME,
+			ResourceConstants.SCOPE_GROUP, String.valueOf(group.getGroupId()),
+			ActionKeys.VIEW);
+	}
+
+	@DeleteAfterTestRun
+	protected Group group;
+
+	protected Folder parentFolder;
+
+	@DeleteAfterTestRun
+	protected Group targetGroup;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BaseDLAppTestCase.class);
+
+	private String _name;
+
+}
