25a392bc8f4835bb330935341ec84e39c7ce7fb2
pei-jung <pei-jung.lan@liferay.com>
2019-09-11 12:23:15 -0700

    Revert "LPS-100463 My Workflow Task and User Workflow portlets must be rendered in the Control Panel URL"
    This reverts commit 83ac518979929664e72a59cef8b0c48bb7b30582.

--- modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/product/navigation/personal/menu/MyWorkflowTaskPersonalMenuEntry.java
+++ modules/apps/portal-workflow/portal-workflow-task-web/src/main/java/com/liferay/portal/workflow/task/web/internal/product/navigation/personal/menu/MyWorkflowTaskPersonalMenuEntry.java
@@ -14,19 +14,11 @@
 
 package com.liferay.portal.workflow.task.web.internal.product.navigation.personal.menu;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.product.navigation.personal.menu.BasePersonalMenuEntry;
 import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
 
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletURL;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Pei-Jung Lan
@@ -45,19 +37,5 @@ public class MyWorkflowTaskPersonalMenuEntry extends BasePersonalMenuEntry {
 	public String getPortletId() {
 		return PortletKeys.MY_WORKFLOW_TASK;
 	}
-
-	@Override
-	public String getPortletURL(HttpServletRequest httpServletRequest)
-		throws PortalException {
-
-		PortletURL portletURL = _portal.getControlPanelPortletURL(
-			httpServletRequest, PortletKeys.MY_WORKFLOW_TASK,
-			PortletRequest.RENDER_PHASE);
-
-		return portletURL.toString();
-	}
-
-	@Reference
-	private Portal _portal;
 
 }
--- modules/apps/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/product/navigation/personal/menu/UserWorkflowPersonalMenuEntry.java
+++ modules/apps/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/product/navigation/personal/menu/UserWorkflowPersonalMenuEntry.java
@@ -14,19 +14,11 @@
 
 package com.liferay.portal.workflow.web.internal.product.navigation.personal.menu;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.workflow.web.internal.constants.WorkflowPortletKeys;
 import com.liferay.product.navigation.personal.menu.BasePersonalMenuEntry;
 import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
 
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletURL;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Pei-Jung Lan
@@ -45,19 +37,5 @@ public class UserWorkflowPersonalMenuEntry extends BasePersonalMenuEntry {
 	public String getPortletId() {
 		return WorkflowPortletKeys.USER_WORKFLOW;
 	}
-
-	@Override
-	public String getPortletURL(HttpServletRequest httpServletRequest)
-		throws PortalException {
-
-		PortletURL portletURL = _portal.getControlPanelPortletURL(
-			httpServletRequest, WorkflowPortletKeys.USER_WORKFLOW,
-			PortletRequest.RENDER_PHASE);
-
-		return portletURL.toString();
-	}
-
-	@Reference
-	private Portal _portal;
 
 }
