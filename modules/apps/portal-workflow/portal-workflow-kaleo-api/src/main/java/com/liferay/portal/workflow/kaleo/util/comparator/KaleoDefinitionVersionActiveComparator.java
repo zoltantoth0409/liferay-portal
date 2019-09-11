3a122197a611e37e235332fc7f3dc2e522a4bcf9
Inácio Nery <inacio.nery@liferay.com>
2019-09-11 13:32:08 -0300

    LPS-101428 Add KaleoDefinitionVersionActiveComparator

---
+++ modules/apps/portal-workflow/portal-workflow-kaleo-api/src/main/java/com/liferay/portal/workflow/kaleo/util/comparator/KaleoDefinitionVersionActiveComparator.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.util.comparator;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinition;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
+
+/**
+ * @author Inácio Nery
+ */
+public class KaleoDefinitionVersionActiveComparator
+	extends OrderByComparator<KaleoDefinitionVersion> {
+
+	public KaleoDefinitionVersionActiveComparator() {
+		this(false);
+	}
+
+	public KaleoDefinitionVersionActiveComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	@Override
+	public int compare(
+		KaleoDefinitionVersion kaleoDefinitionVersion1,
+		KaleoDefinitionVersion kaleoDefinitionVersion2) {
+
+		int value = Boolean.compare(
+			_isActive(kaleoDefinitionVersion1),
+			_isActive(kaleoDefinitionVersion2));
+
+		if (_ascending) {
+			return value;
+		}
+
+		return -value;
+	}
+
+	@Override
+	public boolean isAscending() {
+		return _ascending;
+	}
+
+	private boolean _isActive(KaleoDefinitionVersion kaleoDefinitionVersion) {
+		try {
+			KaleoDefinition kaleoDefinition =
+				kaleoDefinitionVersion.getKaleoDefinition();
+
+			return kaleoDefinition.isActive();
+		}
+		catch (PortalException pe) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(pe, pe);
+			}
+
+			return false;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		KaleoDefinitionVersionActiveComparator.class);
+
+	private final boolean _ascending;
+
+}
