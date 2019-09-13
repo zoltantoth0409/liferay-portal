cec04966b53744cec61a14c596e499db085b0202
Alberto Chaparro <alberto.chaparro@liferay.com>
2019-09-13 16:30:17 -0700

    LPS-101450 Simplify the upgrade processes definition for the future

--- portal-impl/src/com/liferay/portal/upgrade/PortalUpgradeProcess.java
+++ portal-impl/src/com/liferay/portal/upgrade/PortalUpgradeProcess.java
@@ -169,6 +169,13 @@ public class PortalUpgradeProcess extends UpgradeProcess {
 		}
 	}
 
+	private static final Class<?>[] _PORTAL_UPGRADE_PROCESS_REGISTRIES = {
+		PortalUpgradeProcessRegistryImpl.class,
+		com.liferay.portal.upgrade.v7_2_x.PortalUpgradeProcessRegistryImpl.
+			class,
+		com.liferay.portal.upgrade.v7_3_x.PortalUpgradeProcessRegistryImpl.class
+	};
+
 	private static final Version _initialSchemaVersion = new Version(0, 1, 0);
 	private static final TreeMap<Version, UpgradeProcess> _upgradeProcesses =
 		new TreeMap<Version, UpgradeProcess>() {
@@ -178,25 +185,20 @@ public class PortalUpgradeProcess extends UpgradeProcess {
 		};
 
 	static {
-		PortalUpgradeProcessRegistry portalUpgradeProcessRegistry =
-			new PortalUpgradeProcessRegistryImpl();
+		try {
+			for (Class<?> portalUpgradeProcessRegistry :
+					_PORTAL_UPGRADE_PROCESS_REGISTRIES) {
 
-		portalUpgradeProcessRegistry.registerUpgradeProcesses(
-			_upgradeProcesses);
+				PortalUpgradeProcessRegistry registry =
+					(PortalUpgradeProcessRegistry)
+						portalUpgradeProcessRegistry.newInstance();
 
-		PortalUpgradeProcessRegistry v72xPortalUpgradeProcessRegistry =
-			new com.liferay.portal.upgrade.v7_2_x.
-				PortalUpgradeProcessRegistryImpl();
-
-		v72xPortalUpgradeProcessRegistry.registerUpgradeProcesses(
-			_upgradeProcesses);
-
-		PortalUpgradeProcessRegistry portalUpgradeProcessRegistry73x =
-			new com.liferay.portal.upgrade.v7_3_x.
-				PortalUpgradeProcessRegistryImpl();
-
-		portalUpgradeProcessRegistry73x.registerUpgradeProcesses(
-			_upgradeProcesses);
+				registry.registerUpgradeProcesses(_upgradeProcesses);
+			}
+		}
+		catch (ReflectiveOperationException roe) {
+			throw new ExceptionInInitializerError(roe);
+		}
 	}
 
 }
