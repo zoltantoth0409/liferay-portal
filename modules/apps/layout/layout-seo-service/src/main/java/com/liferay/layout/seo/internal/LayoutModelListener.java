189137e23403651dfdd2e079ea34796df66be7cc
Adolfo Pérez <adolfo.perez@liferay.com>
2019-09-16 16:46:06 +0200

    LPS-98354 Remove seo entries when layout is removed

---
+++ modules/apps/layout/layout-seo-service/src/main/java/com/liferay/layout/seo/internal/LayoutModelListener.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.seo.internal;
+
+import com.liferay.layout.seo.exception.NoSuchEntryException;
+import com.liferay.layout.seo.model.LayoutSEOEntry;
+import com.liferay.layout.seo.service.LayoutSEOEntryLocalService;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(service = ModelListener.class)
+public class LayoutModelListener extends BaseModelListener<Layout> {
+
+	@Override
+	public void onBeforeRemove(Layout layout) throws ModelListenerException {
+		try {
+			super.onBeforeRemove(layout);
+
+			LayoutSEOEntry layoutSEOEntry =
+				_layoutSEOEntryLocalService.fetchLayoutSEOEntry(
+					layout.getGroupId(), layout.isPrivateLayout(),
+					layout.getLayoutId());
+
+			if (layoutSEOEntry != null) {
+				_layoutSEOEntryLocalService.deleteLayoutSEOEntry(
+					layout.getGroupId(), layout.isPrivateLayout(),
+					layout.getLayoutId());
+			}
+		}
+		catch (NoSuchEntryException nsee) {
+			throw new ModelListenerException(nsee);
+		}
+	}
+
+	@Reference
+	private LayoutSEOEntryLocalService _layoutSEOEntryLocalService;
+
+}
