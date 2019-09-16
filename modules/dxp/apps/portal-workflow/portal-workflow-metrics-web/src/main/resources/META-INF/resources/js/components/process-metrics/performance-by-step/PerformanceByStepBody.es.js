74b39fe2ac8f43c6b13a82c61890c5ed1f1ae147
Paulo Cruz <paulo.cruz@liferay.com>
2019-09-16 11:28:25 -0300

    LPS-97667 | LPS-100868 Create performance by step component

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-metrics/performance-by-step/PerformanceByStepBody.es.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import React, {useContext, useEffect, useState} from 'react';
+import {AppContext} from '../../AppContext.es';
+import {ErrorContext} from '../../../shared/components/request/Error.es';
+import Icon from '../../../shared/components/Icon.es';
+import {LoadingContext} from '../../../shared/components/request/Loading.es';
+import Panel from '../../../shared/components/Panel.es';
+import PerformanceByStepCard from './PerformanceByStepCard.es';
+import Request from '../../../shared/components/request/Request.es';
+import {TimeRangeContext} from '../filter/store/TimeRangeStore.es';
+
+const Body = ({processId}) => {
+	const {client} = useContext(AppContext);
+	const {getSelectedTimeRange} = useContext(TimeRangeContext);
+	const {setError} = useContext(ErrorContext);
+	const {setLoading} = useContext(LoadingContext);
+	const [data, setData] = useState({});
+
+	const timeRange = getSelectedTimeRange();
+
+	useEffect(() => {
+		setLoading(true);
+
+		client
+			.get(getRequestUrl(processId, timeRange))
+			.then(({data}) => {
+				setData(data);
+			})
+			.catch(error => {
+				setError(error);
+			})
+			.then(() => {
+				setLoading(false);
+			});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [timeRange]);
+
+	return (
+		<Request.Success>
+			<Panel.Body>
+				<PerformanceByStepCard.Table items={data.items} />
+
+				<div className="mb-1 text-right">
+					<button className="border-0 btn btn-secondary btn-sm">
+						{Liferay.Language.get('view-all-steps') +
+							` (${data.totalCount})`}
+
+						<Icon iconName="caret-right-l" />
+					</button>
+				</div>
+			</Panel.Body>
+		</Request.Success>
+	);
+};
+
+const getRequestUrl = (processId, timeRange) => {
+	let requestUrl = `/processes/${processId}/tasks?completed=true`;
+
+	if (timeRange) {
+		const {dateEnd, dateStart} = timeRange;
+
+		requestUrl += `&dateEnd=${dateEnd.toISOString()}&dateStart=${dateStart.toISOString()}`;
+	}
+
+	requestUrl += '&page=0&pageSize=10&sort=durationAvg:asc';
+
+	return requestUrl;
+};
+
+export {Body};
---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/components/process-metrics/performance-by-step/PerformanceByStepTable.es.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {formatDuration} from '../../../shared/util/duration.es';
+import {formatNumber} from '../../../shared/util/numeral.es';
+import PerformanceByStepCard from './PerformanceByStepCard.es';
+import React from 'react';
+
+const getSLABreachedPercentage = (instanceCount, overdueInstanceCount) => {
+	let percentage = 0;
+
+	if (instanceCount) {
+		percentage = overdueInstanceCount / instanceCount;
+	}
+
+	return formatNumber(percentage, '0[.]00%');
+};
+
+const Table = ({items = []}) => (
+	<div className="mb-3 table-scrollable table-responsive">
+		<table className="table table-autofit table-heading-nowrap table-hover table-list">
+			<thead>
+				<tr>
+					<th style={{width: '60%'}}>
+						{Liferay.Language.get('step-name')}
+					</th>
+
+					<th className="text-right" style={{width: '20%'}}>
+						{`${Liferay.Language.get('sla-breached')} (%)`}
+					</th>
+
+					<th className="text-right" style={{width: '20%'}}>
+						{Liferay.Language.get('avg-completion-time')}
+					</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				{items.map((item, index) => (
+					<PerformanceByStepCard.Item {...item} key={index} />
+				))}
+			</tbody>
+		</table>
+	</div>
+);
+
+const Item = ({durationAvg, instanceCount, name, overdueInstanceCount}) => {
+	const formattedDuration = formatDuration(durationAvg);
+	const percentage = getSLABreachedPercentage(
+		instanceCount,
+		overdueInstanceCount
+	);
+
+	return (
+		<tr>
+			<td data-testId="stepName">{name}</td>
+
+			<td className="text-right" data-testId="slaBreached">
+				{`${overdueInstanceCount} (${percentage})`}
+			</td>
+
+			<td className="text-right" data-testId="avgCompletionTime">
+				{formattedDuration}
+			</td>
+		</tr>
+	);
+};
+
+export {Item, Table};
