4e9b151d853ffb97625a8114633678a3329720b3
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-09-17 14:04:20 +0200

    LPS-100762 Just like in update selection style, if we don't receive the parameter, treat it as dynamic

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/portlet/action/AssetPublisherConfigurationAction.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/portlet/action/AssetPublisherConfigurationAction.java
@@ -234,6 +234,10 @@ public class AssetPublisherConfigurationAction
 				String selectionStyle = getParameter(
 					actionRequest, "selectionStyle");
 
+				if (Validator.isNull(selectionStyle)) {
+					selectionStyle = "dynamic";
+				}
+
 				if (selectionStyle.equals("dynamic")) {
 					updateQueryLogic(actionRequest, preferences);
 				}
