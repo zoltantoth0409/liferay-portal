4470c5ef7295e108827671dd7cd3eec2634e2600
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-09-17 14:34:41 +0200

    LPS-101027 Skipping comments at top level
    They will be imported by the commented models in
    BaseStagedModelDataHandler. This way we avoid importing messages too
    early when the referenced model hasn't been imported yet.

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/exportimport/data/handler/MBDiscussionStagedModelDataHandler.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/exportimport/data/handler/MBDiscussionStagedModelDataHandler.java
@@ -19,6 +19,7 @@ import com.liferay.asset.kernel.service.AssetEntryLocalService;
 import com.liferay.exportimport.data.handler.base.BaseStagedModelDataHandler;
 import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.PortletDataException;
 import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
 import com.liferay.exportimport.kernel.lar.StagedModelModifiedDateComparator;
 import com.liferay.message.boards.model.MBDiscussion;
@@ -96,6 +97,22 @@ public class MBDiscussionStagedModelDataHandler
 		catch (Exception e) {
 			return discussion.getUuid();
 		}
+	}
+
+	@Override
+	public void importStagedModel(
+			PortletDataContext portletDataContext, MBDiscussion discussion)
+		throws PortletDataException {
+
+		Map<Long, Long> relatedClassPKs =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				discussion.getClassName());
+
+		if (!relatedClassPKs.containsKey(discussion.getClassPK())) {
+			return;
+		}
+
+		super.importStagedModel(portletDataContext, discussion);
 	}
 
 	@Override
