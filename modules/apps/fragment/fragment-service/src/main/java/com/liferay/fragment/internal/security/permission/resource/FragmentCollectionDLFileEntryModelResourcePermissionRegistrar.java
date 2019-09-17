7e9d0593eecfa27f8a79e1cbd259b8f8150afb75
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2019-09-17 17:04:49 +0200

    LPS-100777 Publish ModelResourcePermission to allow access to Fragment DLFileEntry resources

---
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/security/permission/resource/FragmentCollectionDLFileEntryModelResourcePermissionRegistrar.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.internal.security.permission.resource;
+
+import com.liferay.fragment.constants.FragmentActionKeys;
+import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.service.FragmentCollectionLocalService;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tomas Polesovsky
+ */
+@Component(immediate = true, service = {})
+public class FragmentCollectionDLFileEntryModelResourcePermissionRegistrar {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("model.class.name", FragmentCollection.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				FragmentCollection.class,
+				FragmentCollection::getFragmentCollectionId,
+				_fragmentCollectionLocalService::getFragmentCollection,
+				_portletResourcePermission,
+				(modelResourcePermission, consumer) -> consumer.accept(
+					(permissionChecker, name, model, actionId) -> {
+						if (actionId.equals(ActionKeys.VIEW)) {
+							return true;
+						}
+
+						if (_portletResourcePermission.contains(
+								permissionChecker, model.getGroupId(),
+								FragmentActionKeys.MANAGE_FRAGMENT_ENTRIES)) {
+
+							return true;
+						}
+
+						return null;
+					})),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference
+	private FragmentCollectionLocalService _fragmentCollectionLocalService;
+
+	@Reference(
+		target = "(resource.name=" + FragmentConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+}
