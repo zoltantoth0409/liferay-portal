b95d5c6ce07931867662ad21723db7ae491b2dd9
Adolfo Pérez <adolfo.perez@liferay.com>
2019-09-18 11:00:20 +0200

    LPS-101816 Remove announcements entries when removing company

---
+++ modules/apps/announcements/announcements-web/src/main/java/com/liferay/announcements/web/internal/model/listener/CompanyModelListener.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.announcements.web.internal.model.listener;
+
+import com.liferay.announcements.kernel.service.AnnouncementsEntryLocalService;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(service = ModelListener.class)
+public class CompanyModelListener extends BaseModelListener<Company> {
+
+	@Override
+	public void onBeforeRemove(Company company) throws ModelListenerException {
+		super.onBeforeRemove(company);
+
+		_announcementsEntryLocalService.deleteEntries(company.getCompanyId());
+	}
+
+	@Reference
+	private AnnouncementsEntryLocalService _announcementsEntryLocalService;
+
+}
