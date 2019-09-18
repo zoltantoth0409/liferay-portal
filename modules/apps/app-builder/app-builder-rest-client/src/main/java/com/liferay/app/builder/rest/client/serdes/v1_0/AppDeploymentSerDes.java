3fbca33949d19602fb2f1fff5c2eac27afdf44ad
Brian Chan <brian.chan@liferay.com>
2019-09-17 22:00:52 -0700

    LPS-100419 regen

--- modules/apps/app-builder/app-builder-rest-client/src/main/java/com/liferay/app/builder/rest/client/serdes/v1_0/AppDeploymentSerDes.java
+++ modules/apps/app-builder/app-builder-rest-client/src/main/java/com/liferay/app/builder/rest/client/serdes/v1_0/AppDeploymentSerDes.java
@@ -17,11 +17,11 @@ package com.liferay.app.builder.rest.client.serdes.v1_0;
 import com.liferay.app.builder.rest.client.dto.v1_0.AppDeployment;
 import com.liferay.app.builder.rest.client.json.BaseJSONParser;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.TreeMap;
 
 import javax.annotation.Generated;
 
@@ -96,7 +96,7 @@ public class AppDeploymentSerDes {
 			return null;
 		}
 
-		Map<String, String> map = new HashMap<>();
+		Map<String, String> map = new TreeMap<>();
 
 		if (appDeployment.getSettings() == null) {
 			map.put("settings", null);
