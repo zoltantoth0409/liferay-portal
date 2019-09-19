1d68e51965c8c97165c93e7c261642bb0de93eb8
Gabriel Albuquerque <gwas@cin.ufpe.br>
2019-09-19 14:10:37 -0300

    LPS-100953 Create AppDeployer tracker

---
+++ modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/deploy/AppDeployerTrackerImpl.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.app.builder.web.internal.deploy;
+
+import com.liferay.app.builder.deploy.AppDeployer;
+import com.liferay.app.builder.deploy.AppDeployerTracker;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Jeyvison Nascimento
+ * @author Gabriel Albuquerque
+ */
+@Component(immediate = true, service = AppDeployerTracker.class)
+public class AppDeployerTrackerImpl implements AppDeployerTracker {
+
+	@Override
+	public AppDeployer getAppDeployer(String type) {
+		return _serviceTrackerMap.getService(type);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, AppDeployer.class, "com.app.builder.deploy.type");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private ServiceTrackerMap<String, AppDeployer> _serviceTrackerMap;
+
+}
