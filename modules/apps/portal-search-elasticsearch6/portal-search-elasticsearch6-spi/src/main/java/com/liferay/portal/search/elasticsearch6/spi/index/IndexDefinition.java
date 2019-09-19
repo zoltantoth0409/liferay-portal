95287f56b85b72caa92d6b64899000f3a75d9077
Bryan Engler <bryan.engler@liferay.com>
2019-09-19 10:28:21 -0700

    LPS-101883 portal-search-elasticsearch6-spi: Deprecate IndexDefinition interface
    https://issues.liferay.com/browse/LPS-101883

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-spi/src/main/java/com/liferay/portal/search/elasticsearch6/spi/index/IndexDefinition.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-spi/src/main/java/com/liferay/portal/search/elasticsearch6/spi/index/IndexDefinition.java
@@ -15,8 +15,11 @@
 package com.liferay.portal.search.elasticsearch6.spi.index;
 
 /**
- * @author André de Oliveira
+ * @author     André de Oliveira
+ * @deprecated As of Athanasius (7.3.x), replaced by {@link
+ *             com.liferay.portal.search.spi.index.IndexDefinition}
  */
+@Deprecated
 public interface IndexDefinition {
 
 	public static final String PROPERTY_KEY_INDEX_NAME = "index.name";
