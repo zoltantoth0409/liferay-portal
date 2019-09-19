fb8338408ceb24d5e4d22df306df674e0d97b97d
Bryan Engler <bryan.engler@liferay.com>
2019-09-19 10:28:21 -0700

    LPS-101883 portal-search-spi: Move IndexDefinition interface to portal-search-spi
    https://issues.liferay.com/browse/LPS-101883

---
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/index/IndexDefinition.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.spi.index;
+
+/**
+ * @author André de Oliveira
+ */
+public interface IndexDefinition {
+
+	public static final String PROPERTY_KEY_INDEX_NAME = "index.name";
+
+	public static final String PROPERTY_KEY_INDEX_SETTINGS_RESOURCE_NAME =
+		"index.settings.resource.name";
+
+}
