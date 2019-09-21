2b992108f90a0100bcd874ad148a332fc17ba869
Brian Chan <brian.chan@liferay.com>
2019-09-21 11:48:06 -0700

    LPS-101196 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -4036,6 +4036,7 @@ AUI.add(
 					var currentLocale = repeatedField.get('displayLocale');
 
 					var localizations = Object.keys(totalLocalizations);
+
 					localizations.push(currentLocale);
 
 					localizations.forEach(function(localization) {
