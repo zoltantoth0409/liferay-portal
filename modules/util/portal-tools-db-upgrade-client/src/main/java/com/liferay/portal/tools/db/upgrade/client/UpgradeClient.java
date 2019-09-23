e0c316afa80f0d9cbd518b4a51e61611da32f837
Brian Chan <brian.chan@liferay.com>
2019-09-23 09:41:41 -0700

    Revert "LPS-101810 Do not print a white line if no command is sent"
    This reverts commit ab419b38a90660be5125948af65e51b89da542aa.

--- modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
+++ modules/util/portal-tools-db-upgrade-client/src/main/java/com/liferay/portal/tools/db/upgrade/client/UpgradeClient.java
@@ -406,10 +406,6 @@ public class UpgradeClient {
 			return false;
 		}
 
-		if (command.equals("")) {
-			return true;
-		}
-
 		String output = gogoShellClient.send(command);
 
 		int endOfFirstLineIndex = output.indexOf('\n');
