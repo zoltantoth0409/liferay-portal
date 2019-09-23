ab46a2ea529a403fe2ad00d236386281c40f0053
Preston-Crary <preston.crary@liferay.com>
2019-09-23 15:13:16 -0700

    LPS-102147 Make asset-service mvcc enabled.

---
+++ modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/upgrade/AssetServiceUpgrade.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.internal.upgrade;
+
+import com.liferay.portal.kernel.upgrade.UpgradeMVCCVersion;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Preston Crary
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class AssetServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"1.0.0", "1.1.0",
+			new UpgradeMVCCVersion() {
+
+				@Override
+				protected String[] getModuleTableNames() {
+					return new String[] {"AssetEntryUsage"};
+				}
+
+			});
+	}
+
+}
