f62dcf83cbe1ca1ded5ac369647c2faf49cf2f7b
pei-jung <pei-jung.lan@liferay.com>
2019-09-23 14:57:55 -0700

    LPS-101269 Get account users

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/dao/search/AccountUserDisplaySearchContainerFactory.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.dao.search;
+
+import com.liferay.account.admin.web.internal.display.AccountUserDisplay;
+import com.liferay.account.retriever.AccountUserRetriever;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.vulcan.util.TransformUtil;
+import com.liferay.portlet.usersadmin.search.UserSearch;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(service = {})
+public class AccountUserDisplaySearchContainerFactory {
+
+	public static SearchContainer create(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse, long accountEntryId) {
+
+		SearchContainer accountUserDisplaySearchContainer = new UserSearch(
+			liferayPortletRequest, liferayPortletResponse.createRenderURL());
+
+		accountUserDisplaySearchContainer.setEmptyResultsMessage(
+			"there-are-no-users-associated-to-this-account");
+		accountUserDisplaySearchContainer.setId("accountUsers");
+		accountUserDisplaySearchContainer.setRowChecker(
+			new EmptyOnClickRowChecker(liferayPortletResponse));
+
+		List<User> accountUsers = _accountUserRetriever.getAccountUsers(
+			accountEntryId);
+
+		accountUserDisplaySearchContainer.setTotal(accountUsers.size());
+
+		accountUsers = ListUtil.sort(
+			accountUsers,
+			accountUserDisplaySearchContainer.getOrderByComparator());
+
+		accountUsers = ListUtil.subList(
+			accountUsers, accountUserDisplaySearchContainer.getStart(),
+			accountUserDisplaySearchContainer.getEnd());
+
+		List<AccountUserDisplay> accountUserDisplays = TransformUtil.transform(
+			accountUsers, AccountUserDisplay::of);
+
+		accountUserDisplaySearchContainer.setResults(accountUserDisplays);
+
+		return accountUserDisplaySearchContainer;
+	}
+
+	@Reference(unbind = "-")
+	protected void setAccountUserRetriever(
+		AccountUserRetriever accountUserRetriever) {
+
+		_accountUserRetriever = accountUserRetriever;
+	}
+
+	private static AccountUserRetriever _accountUserRetriever;
+
+}
---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/display/AccountUserDisplay.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.display;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.User;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public class AccountUserDisplay {
+
+	public static AccountUserDisplay of(User user) {
+		if (user == null) {
+			return null;
+		}
+
+		return new AccountUserDisplay(user);
+	}
+
+	public String getAccountRoles() {
+		return _accountRoles;
+	}
+
+	public String getEmailAddress() {
+		return _emailAddress;
+	}
+
+	public String getJobTitle() {
+		return _jobTitle;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public long getUserId() {
+		return _userId;
+	}
+
+	private AccountUserDisplay(User user) {
+		_userId = user.getUserId();
+		_name = user.getFullName();
+		_emailAddress = user.getEmailAddress();
+		_jobTitle = user.getJobTitle();
+		_accountRoles = StringPool.BLANK;
+	}
+
+	private final String _accountRoles;
+	private final String _emailAddress;
+	private final String _jobTitle;
+	private final String _name;
+	private final long _userId;
+
+}
