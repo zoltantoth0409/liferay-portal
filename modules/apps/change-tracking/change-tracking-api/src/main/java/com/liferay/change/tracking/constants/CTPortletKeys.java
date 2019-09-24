80d81eae995533d5d7ca19bdf09d4c60ef3ea63c
Samuel Tran <samuel.tran@liferay.com>
2019-09-24 14:41:34 -0700

    LPS-102145 Remove unused portlet keys

--- modules/apps/change-tracking/change-tracking-api/src/main/java/com/liferay/change/tracking/constants/CTPortletKeys.java
+++ modules/apps/change-tracking/change-tracking-api/src/main/java/com/liferay/change/tracking/constants/CTPortletKeys.java
@@ -31,16 +31,4 @@ public class CTPortletKeys {
 		"com_liferay_change_tracking_change_lists_history_web_portlet_" +
 			"ChangeListsHistoryPortlet";
 
-	public static final String MY_CHANGE_LISTS =
-		"com_liferay_change_tracking_my_change_lists_web_portlet_" +
-			"MyChangeListsPortlet";
-
-	public static final String SITE_CHANGE_LISTS =
-		"com_liferay_change_tracking_site_change_lists_web_portlet_" +
-			"SiteChangeListsPortlet";
-
-	public static final String SITE_CHANGE_LISTS_HISTORY =
-		"com_liferay_change_tracking_site_change_lists_history_web_portlet_" +
-			"SiteChangeListsHistoryPortlet";
-
 }
