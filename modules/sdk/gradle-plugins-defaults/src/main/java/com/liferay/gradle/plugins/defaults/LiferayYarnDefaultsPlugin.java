04841569e6aa765131d89b3e3aa2b010e85e4743
Peter Shin <peter.shin@liferay.com>
2019-09-26 11:58:05 -0700

    LPS-100168 Add CI configuration logic

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayYarnDefaultsPlugin.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/LiferayYarnDefaultsPlugin.java
@@ -15,8 +15,11 @@
 package com.liferay.gradle.plugins.defaults;
 
 import com.liferay.gradle.plugins.LiferayYarnPlugin;
+import com.liferay.gradle.plugins.defaults.internal.LiferayCIPatcherPlugin;
+import com.liferay.gradle.plugins.defaults.internal.LiferayCIPlugin;
 import com.liferay.gradle.plugins.defaults.internal.NodeDefaultsPlugin;
 import com.liferay.gradle.plugins.defaults.internal.util.GradleUtil;
+import com.liferay.gradle.util.Validator;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
@@ -31,6 +34,32 @@ public class LiferayYarnDefaultsPlugin implements Plugin<Project> {
 		GradleUtil.applyPlugin(project, LiferayYarnPlugin.class);
 
 		NodeDefaultsPlugin.INSTANCE.apply(project);
+
+		if (_isRunningInCIEnvironment()) {
+			LiferayCIPlugin.INSTANCE.apply(project);
+		}
+
+		if (_isRunningInCIPatcherEnvironment()) {
+			LiferayCIPatcherPlugin.INSTANCE.apply(project);
+		}
+	}
+
+	private boolean _isRunningInCIEnvironment() {
+		if (Validator.isNotNull(System.getenv("JENKINS_HOME"))) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isRunningInCIPatcherEnvironment() {
+		if (Validator.isNotNull(
+				System.getenv("FIX_PACKS_RELEASE_ENVIRONMENT"))) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 }
