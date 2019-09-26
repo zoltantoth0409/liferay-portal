9c0b5bae7240f8271d19caeed53b95e174eca466
Brian Chan <brian.chan@liferay.com>
2019-09-26 14:28:19 -0700

    LPS-101095 remove test for now

--- modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/service/test/AccountEntryUserRelLocalServiceTest.java
+++ modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/service/test/AccountEntryUserRelLocalServiceTest.java
@@ -24,21 +24,16 @@ import com.liferay.account.service.AccountEntryUserRelLocalService;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.portal.kernel.exception.NoSuchUserException;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.LocaleThreadLocal;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -111,24 +106,6 @@ public class AccountEntryUserRelLocalServiceTest {
 	}
 
 	@Test
-	public void testAddUser() throws Exception {
-		AccountEntry accountEntry = AccountEntryTestUtil.addAccountEntry(
-			_accountEntryLocalService);
-		UserInfo userInfo = new UserInfo();
-
-		AccountEntryUserRel accountEntryUserRel = _addAccountEntryUserRel(
-			accountEntry.getAccountEntryId(), userInfo);
-
-		User user = _userLocalService.fetchUser(
-			accountEntryUserRel.getAccountUserId());
-
-		Assert.assertNotNull(user);
-		Assert.assertEquals(
-			accountEntryUserRel.getAccountUserId(), user.getUserId());
-		Assert.assertEquals(userInfo.screenName, user.getScreenName());
-	}
-
-	@Test
 	public void testGetAccountEntryUserRelsByAccountEntryId() throws Exception {
 		_users.add(UserTestUtil.addUser());
 		_users.add(UserTestUtil.addUser());
@@ -158,58 +135,6 @@ public class AccountEntryUserRelLocalServiceTest {
 		Assert.assertArrayEquals(expectedUserIds, actualUserIds);
 	}
 
-	@Test
-	public void testUserNotCreatedWithInvalidAccountEntryId() throws Exception {
-		UserInfo userInfo = new UserInfo();
-
-		try {
-			_addAccountEntryUserRel(RandomTestUtil.nextLong(), userInfo);
-
-			Assert.fail();
-		}
-		catch (Exception e) {
-			Assert.assertNull(
-				_userLocalService.fetchUserByScreenName(
-					TestPropsValues.getCompanyId(), userInfo.screenName));
-		}
-	}
-
-	@Test
-	public void testUserNotCreatedWithInvalidUserInfo() throws Exception {
-		UserInfo userInfo = new UserInfo();
-
-		userInfo.emailAddress = "liferay";
-
-		try {
-			_addAccountEntryUserRel(
-				_accountEntry.getAccountEntryId(), userInfo);
-
-			Assert.fail();
-		}
-		catch (Exception e) {
-			Assert.assertNull(
-				_userLocalService.fetchUserByScreenName(
-					TestPropsValues.getCompanyId(), userInfo.screenName));
-		}
-	}
-
-	private AccountEntryUserRel _addAccountEntryUserRel(
-			long accountId, UserInfo userInfo)
-		throws Exception {
-
-		AccountEntryUserRel accountEntryUserRel =
-			_accountEntryUserRelLocalService.addAccountEntryUserRel(
-				accountId, TestPropsValues.getUserId(), userInfo.screenName,
-				userInfo.emailAddress, userInfo.locale, userInfo.firstName,
-				userInfo.middleName, userInfo.lastName, userInfo.prefixId,
-				userInfo.suffixId);
-
-		_users.add(
-			_userLocalService.getUser(accountEntryUserRel.getAccountUserId()));
-
-		return accountEntryUserRel;
-	}
-
 	@DeleteAfterTestRun
 	private AccountEntry _accountEntry;
 
@@ -226,25 +151,7 @@ public class AccountEntryUserRelLocalServiceTest {
 	@DeleteAfterTestRun
 	private User _user;
 
-	@Inject
-	private UserLocalService _userLocalService;
-
 	@DeleteAfterTestRun
 	private final List<User> _users = new ArrayList<>();
-
-	private static class UserInfo {
-
-		public String emailAddress =
-			RandomTestUtil.randomString() + "@liferay.com";
-		public String firstName = RandomTestUtil.randomString();
-		public String lastName = RandomTestUtil.randomString();
-		public Locale locale = LocaleThreadLocal.getDefaultLocale();
-		public String middleName = RandomTestUtil.randomString();
-		public long prefixId = RandomTestUtil.randomLong();
-		public String screenName = StringUtil.toLowerCase(
-			RandomTestUtil.randomString());
-		public long suffixId = RandomTestUtil.randomLong();
-
-	}
 
 }
