9342e382ea0c301cbb4d1bdf6b2538ecdc6a2522
Raymond Augé <raymond.auge@liferay.com>
2019-09-27 16:33:33 -0400

    LPS-102441 Hide items found elsewhere in Site administration

---
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/display/SiteDefaultConfigurationVisibilityController.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.internal.display;
+
+import com.liferay.configuration.admin.display.ConfigurationVisibilityController;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+import java.io.Serializable;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	immediate = true,
+	property = {
+		"configuration.pid=com.liferay.asset.auto.tagger.internal.configuration.AssetAutoTaggerGroupConfiguration",
+		"configuration.pid=com.liferay.blogs.configuration.BlogsGroupServiceConfiguration",
+		"configuration.pid=com.liferay.bookmarks.configuration.BookmarksGroupServiceConfiguration",
+		"configuration.pid=com.liferay.comment.configuration.CommentGroupServiceConfiguration",
+		"configuration.pid=com.liferay.dynamic.data.mapping.configuration.DDMGroupServiceConfiguration",
+		"configuration.pid=com.liferay.dynamic.data.mapping.data.provider.configuration.DDMDataProviderConfiguration",
+		"configuration.pid=com.liferay.dynamic.data.mapping.form.web.internal.configuration.DDMFormWebConfiguration",
+		"configuration.pid=com.liferay.journal.configuration.JournalGroupServiceConfiguration",
+		"configuration.pid=com.liferay.knowledge.base.configuration.KBGroupServiceConfiguration",
+		"configuration.pid=com.liferay.sharing.internal.configuration.SharingGroupConfiguration",
+		"configuration.pid=com.liferay.social.activity.configuration.SocialActivityGroupServiceConfiguration",
+		"configuration.pid=com.liferay.wiki.configuration.WikiGroupServiceConfiguration"
+	},
+	service = ConfigurationVisibilityController.class
+)
+public class SiteDefaultConfigurationVisibilityController
+	implements ConfigurationVisibilityController {
+
+	@Override
+	public boolean isVisible(
+		ExtendedObjectClassDefinition.Scope scope, Serializable scopePK) {
+
+		return false;
+	}
+
+}
