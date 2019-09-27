14e53bf711681ec41e04e3acd889afbf2807eff3
Alec Sloan <Alec.sloan@Liferay.com>
2019-09-27 10:05:04 -0700

    COMMERCE-1502 Move 'Apps' panel so 'Commerce' can come before it

--- modules/apps/product-navigation/product-navigation-control-panel/src/main/java/com/liferay/product/navigation/control/panel/internal/application/list/AppsPanelCategory.java
+++ modules/apps/product-navigation/product-navigation-control-panel/src/main/java/com/liferay/product/navigation/control/panel/internal/application/list/AppsPanelCategory.java
@@ -30,7 +30,7 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"panel.category.key=" + PanelCategoryKeys.CONTROL_PANEL,
-		"panel.category.order:Integer=300"
+		"panel.category.order:Integer=350"
 	},
 	service = PanelCategory.class
 )
