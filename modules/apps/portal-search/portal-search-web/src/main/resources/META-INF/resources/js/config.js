1251f7e4f8d6cfa93ce4c38a40d5fd58d3d76c3d
Kevin Tan <kevin.tan@liferay.com>
2019-09-26 17:49:28 -0700

    LPS-102287 Moves js to liferay-search-modified-facet for better maintainability
    Since an AUI component already exists for the page, we should reuse the same variables being defined for the AUI component. This also helps to keep values synced.

--- modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/config.js
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/config.js
@@ -34,7 +34,10 @@
 					},
 					'liferay-search-modified-facet': {
 						path: 'modified_facet.js',
-						requires: ['liferay-search-facet-util']
+						requires: [
+							'aui-form-validator',
+							'liferay-search-facet-util'
+						]
 					},
 					'liferay-search-modified-facet-configuration': {
 						path: 'modified_facet_configuration.js',
