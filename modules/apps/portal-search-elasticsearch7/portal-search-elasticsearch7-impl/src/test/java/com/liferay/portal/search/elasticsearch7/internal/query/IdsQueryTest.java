367c6ed0432af79665cfdccc772d1de420ee61d9
Bryan Engler <bryan.engler@liferay.com>
2019-09-28 00:08:04 -0700

    LPS-101774 portal-search-elasticsearch7-impl: Reactivate IdsQueryTest
    https://issues.liferay.com/browse/LPS-101774

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/query/IdsQueryTest.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/query/IdsQueryTest.java
@@ -18,12 +18,9 @@ import com.liferay.portal.search.elasticsearch7.internal.LiferayElasticsearchInd
 import com.liferay.portal.search.test.util.indexing.IndexingFixture;
 import com.liferay.portal.search.test.util.query.BaseIdsQueryTestCase;
 
-import org.junit.Ignore;
-
 /**
  * @author Michael C. Han
  */
-@Ignore
 public class IdsQueryTest extends BaseIdsQueryTestCase {
 
 	@Override
