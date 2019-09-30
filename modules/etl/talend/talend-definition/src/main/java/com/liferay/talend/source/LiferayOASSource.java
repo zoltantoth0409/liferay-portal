cc748fac30f35e5f54a4ec8db0a61d1dcac85c06
Igor Beslic <igor.beslic@liferay.com>
2019-09-30 11:26:17 +0200

    COMMERCE-1950 talend - component - definition - remove runtime and add LiferayOASSource - this is class used by designer to provide designer classes access to Liferay OAS endpoints in design time

---
+++ modules/etl/talend/talend-definition/src/main/java/com/liferay/talend/source/LiferayOASSource.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talend.source;
+
+import com.liferay.talend.common.oas.OASSource;
+import org.talend.daikon.properties.ValidationResult;
+
+/**
+ * @author Igor Beslic
+ */
+public class LiferayOASSource {
+
+	public LiferayOASSource(
+		OASSource oasSource, ValidationResult validationResult) {
+
+		_oasSource = oasSource;
+		_validationResult = validationResult;
+	}
+
+	public boolean isValid() {
+		if (_validationResult.getStatus() == ValidationResult.Result.ERROR) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public OASSource getOASSource() {
+		return _oasSource;
+	}
+
+	public ValidationResult getValidationResult() {
+		return _validationResult;
+	}
+
+	private final OASSource _oasSource;
+	private final ValidationResult _validationResult;
+
+}
