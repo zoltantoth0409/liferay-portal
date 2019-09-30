6fb687336fa449c8e43ea798c35867c32a595333
Greg Hurrell <greg.hurrell@liferay.com>
2019-09-30 17:33:05 +0200

    LPS-102503 Remove liferay-theme-es2015-hook
    The Porygon theme is the only user of this module and we can rewrite the
    JS fairly trivially to work without it, which allows us to clean up some
    cruft from the top-level package.json and yarn.lock.
    
    We move the contents of top_search.es.js into main.js with as few
    changes as possible (just the necessary ones to drop the dependencies on
    the metal modules).

--- modules/apps/frontend-theme-porygon/frontend-theme-porygon/src/js/main.js
+++ modules/apps/frontend-theme-porygon/frontend-theme-porygon/src/js/main.js
@@ -21,9 +21,22 @@
 		}
 	});
 
-	Liferay.Loader.require('porygon-theme/js/top_search.es', function(
-		TopSearch
-	) {
-		new TopSearch.default();
-	});
+	var porygonSearch = document.querySelector('.porygon-search');
+	var porygonSearchButton = document.querySelector('.porygon-search-button');
+	var porygonSearchInput = document.querySelector(
+		'.porygon-search .search-portlet-keywords-input'
+	);
+
+	if (porygonSearch && porygonSearchButton && porygonSearchInput) {
+		porygonSearchButton.addEventListener('click', function(_event) {
+			porygonSearch.classList.toggle('active');
+			porygonSearchInput.focus();
+		});
+
+		porygonSearch.addEventListener('keydown', function(event) {
+			if (event.keyCode === 27) {
+				porygonSearch.classList.remove('active');
+			}
+		});
+	}
 })();
