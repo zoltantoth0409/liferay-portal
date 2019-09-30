8f160f6cd94f9e99c6a737ab8eeb3c813c70207e
Alan <alan.huang@liferay.com>
2019-09-30 14:09:14 +0800

    LPS-84119 Add empty line after/before comment

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PoshiEmptyLinesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PoshiEmptyLinesCheck.java
@@ -14,6 +14,12 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.checks.util.SourceUtil;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Alan Huang
  */
@@ -32,7 +38,59 @@ public class PoshiEmptyLinesCheck extends BaseFileCheck {
 		content = content.replaceFirst(
 			"(\n\t*[^@\\s].*\n)((\t@.+\n)*\t(function|macro|test) )", "$1\n$2");
 
+		content = _fixMissingEmptyLinesAroundComments(content);
+
 		return content;
 	}
+
+	private String _fixMissingEmptyLinesAroundComments(String content) {
+		int[] multiLineCommentsPositions = SourceUtil.getMultiLinePositions(
+			content, _multiLineCommentsPattern);
+		int[] multiLineStringPositions = SourceUtil.getMultiLinePositions(
+			content, _multiLineStringPattern);
+
+		Matcher matcher = _missingEmptyLineAfterCommentPattern.matcher(content);
+
+		int lineNumber;
+
+		if (matcher.find()) {
+			lineNumber = getLineNumber(content, matcher.start());
+
+			if (!SourceUtil.isInsideMultiLines(
+					lineNumber, multiLineCommentsPositions) &&
+				!SourceUtil.isInsideMultiLines(
+					lineNumber, multiLineStringPositions)) {
+
+				return StringUtil.replaceFirst(
+					content, "\n", "\n\n", matcher.start() + 1);
+			}
+		}
+
+		matcher = _missingEmptyLineBeforeCommentPattern.matcher(content);
+
+		if (matcher.find()) {
+			lineNumber = getLineNumber(content, matcher.start());
+
+			if (!SourceUtil.isInsideMultiLines(
+					lineNumber, multiLineCommentsPositions) &&
+				!SourceUtil.isInsideMultiLines(
+					lineNumber, multiLineStringPositions)) {
+
+				return StringUtil.replaceFirst(
+					content, "\n", "\n\n", matcher.start() + 1);
+			}
+		}
+
+		return content;
+	}
+
+	private static final Pattern _missingEmptyLineAfterCommentPattern =
+		Pattern.compile("\n\t*// .*\n[\t ]*(?!// )\\S");
+	private static final Pattern _missingEmptyLineBeforeCommentPattern =
+		Pattern.compile("\n[\t ]*(?!// )\\S.*\n\t*// ");
+	private static final Pattern _multiLineCommentsPattern = Pattern.compile(
+		"[ \t]/\\*.*?\\*/", Pattern.DOTALL);
+	private static final Pattern _multiLineStringPattern = Pattern.compile(
+		"'''.*?'''", Pattern.DOTALL);
 
 }
