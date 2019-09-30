1597dc721089c10f9dadab13bf98d3573ae3a60a
Iván Zaera Avellón <izaera@gmx.es>
2019-09-30 13:38:46 +0200

    LPS-102323 Save changes of JavaScript Loader makes the System Setting unavailable
    This error was caused because modifying the settings of the loader
    caused the NPMRegistry to reload and that caused a Loader call from the
    browser to fail.
    
    In this commit I'm avoiding the reload by providing a @Modified handler
    in the component and I'm also optimizing the Tracker reopen (this is not
    strictly necessary to fix the bug but should add stability and
    performance to the system).

--- modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/npm/NPMRegistryImpl.java
+++ modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/npm/NPMRegistryImpl.java
@@ -62,6 +62,7 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.util.tracker.BundleTracker;
 import org.osgi.util.tracker.BundleTrackerCustomizer;
@@ -263,8 +264,7 @@ public class NPMRegistryImpl implements NPMRegistry {
 
 	@Activate
 	protected void activate(
-			BundleContext bundleContext, Map<String, Object> properties)
-		throws Exception {
+		BundleContext bundleContext, Map<String, Object> properties) {
 
 		_bundleContext = bundleContext;
 
@@ -282,61 +282,9 @@ public class NPMRegistryImpl implements NPMRegistry {
 
 		_refreshJSModuleCaches(tracked.values());
 
-		Details details = ConfigurableUtil.createConfigurable(
-			Details.class, properties);
+		_lastApplyVersioning = null;
 
-		_serviceTracker = ServiceTrackerFactory.open(
-			bundleContext,
-			"(&(objectClass=" + ServletContext.class.getName() +
-				")(osgi.web.contextpath=*))",
-			new ServiceTrackerCustomizer
-				<ServletContext, JSConfigGeneratorPackage>() {
-
-				@Override
-				public JSConfigGeneratorPackage addingService(
-					ServiceReference<ServletContext> serviceReference) {
-
-					Bundle bundle = serviceReference.getBundle();
-
-					URL url = bundle.getEntry(Details.CONFIG_JSON);
-
-					if (url == null) {
-						return null;
-					}
-
-					JSConfigGeneratorPackage jsConfigGeneratorPackage =
-						new JSConfigGeneratorPackage(
-							details.applyVersioning(),
-							serviceReference.getBundle(),
-							(String)serviceReference.getProperty(
-								"osgi.web.contextpath"));
-
-					String jsConfigGeneratorPackageResolvedId =
-						jsConfigGeneratorPackage.getName() + StringPool.AT +
-							jsConfigGeneratorPackage.getVersion();
-
-					_partialMatchMap.put(
-						jsConfigGeneratorPackage.getName(),
-						jsConfigGeneratorPackageResolvedId);
-
-					return jsConfigGeneratorPackage;
-				}
-
-				@Override
-				public void modifiedService(
-					ServiceReference<ServletContext> serviceReference,
-					JSConfigGeneratorPackage jsConfigGeneratorPackage) {
-				}
-
-				@Override
-				public void removedService(
-					ServiceReference<ServletContext> serviceReference,
-					JSConfigGeneratorPackage jsConfigGeneratorPackage) {
-
-					_partialMatchMap.remove(jsConfigGeneratorPackage.getName());
-				}
-
-			});
+		modified(bundleContext, properties);
 	}
 
 	@Deactivate
@@ -344,6 +292,74 @@ public class NPMRegistryImpl implements NPMRegistry {
 		_serviceTracker.close();
 
 		_bundleTracker.close();
+	}
+
+	@Modified
+	protected void modified(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		Details details = ConfigurableUtil.createConfigurable(
+			Details.class, properties);
+
+		if ((_lastApplyVersioning == null) ||
+			(details.applyVersioning() != _lastApplyVersioning)) {
+
+			_lastApplyVersioning = details.applyVersioning();
+
+			_serviceTracker = ServiceTrackerFactory.open(
+				bundleContext,
+				"(&(objectClass=" + ServletContext.class.getName() +
+					")(osgi.web.contextpath=*))",
+				new ServiceTrackerCustomizer
+					<ServletContext, JSConfigGeneratorPackage>() {
+
+					@Override
+					public JSConfigGeneratorPackage addingService(
+						ServiceReference<ServletContext> serviceReference) {
+
+						Bundle bundle = serviceReference.getBundle();
+
+						URL url = bundle.getEntry(Details.CONFIG_JSON);
+
+						if (url == null) {
+							return null;
+						}
+
+						JSConfigGeneratorPackage jsConfigGeneratorPackage =
+							new JSConfigGeneratorPackage(
+								details.applyVersioning(),
+								serviceReference.getBundle(),
+								(String)serviceReference.getProperty(
+									"osgi.web.contextpath"));
+
+						String jsConfigGeneratorPackageResolvedId =
+							jsConfigGeneratorPackage.getName() + StringPool.AT +
+								jsConfigGeneratorPackage.getVersion();
+
+						_partialMatchMap.put(
+							jsConfigGeneratorPackage.getName(),
+							jsConfigGeneratorPackageResolvedId);
+
+						return jsConfigGeneratorPackage;
+					}
+
+					@Override
+					public void modifiedService(
+						ServiceReference<ServletContext> serviceReference,
+						JSConfigGeneratorPackage jsConfigGeneratorPackage) {
+					}
+
+					@Override
+					public void removedService(
+						ServiceReference<ServletContext> serviceReference,
+						JSConfigGeneratorPackage jsConfigGeneratorPackage) {
+
+						_partialMatchMap.remove(
+							jsConfigGeneratorPackage.getName());
+					}
+
+				});
+		}
 	}
 
 	private JSONObject _getPackageJSONObject(Bundle bundle) {
@@ -483,6 +499,7 @@ public class NPMRegistryImpl implements NPMRegistry {
 
 	private Map<String, JSPackage> _jsPackages = new HashMap<>();
 	private List<JSPackageVersion> _jsPackageVersions = new ArrayList<>();
+	private Boolean _lastApplyVersioning;
 	private final Map<String, String> _partialMatchMap =
 		new ConcurrentHashMap<>();
 	private Map<String, JSModule> _resolvedJSModules = new HashMap<>();
