8c6d719b4509220cefb9aedc65d36f47aa40c5c8
drewbrokke <drew.brokke@liferay.com>
2019-10-02 16:59:31 -0500

    LPS-101098 account-service - adds DocumentContributor and PreFilterContirbutor to account for accountEntryIds during indexing and searching

---
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/search/search/spi/model/query/contributor/UserModelPreFilterContributor.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.internal.search.search.spi.model.query.contributor;
+
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.portal.kernel.model.User",
+	service = ModelPreFilterContributor.class
+)
+public class UserModelPreFilterContributor
+	implements ModelPreFilterContributor {
+
+	@Override
+	public void contribute(
+		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
+		SearchContext searchContext) {
+
+		long[] accountEntryIds = (long[])searchContext.getAttribute(
+			"accountEntryIds");
+
+		if (ArrayUtil.isNotEmpty(accountEntryIds)) {
+			TermsFilter accountEntryTermsFilter = new TermsFilter(
+				"accountEntryIds");
+
+			accountEntryTermsFilter.addValues(
+				ArrayUtil.toStringArray(accountEntryIds));
+
+			booleanFilter.add(accountEntryTermsFilter, BooleanClauseOccur.MUST);
+		}
+	}
+
+}
---
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/search/spi/model/index/contributor/UserModelDocumentContributor.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.internal.search.spi.model.index.contributor;
+
+import com.liferay.account.model.AccountEntryUserRel;
+import com.liferay.account.service.AccountEntryUserRelLocalService;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.portal.kernel.model.User",
+	service = ModelDocumentContributor.class
+)
+public class UserModelDocumentContributor
+	implements ModelDocumentContributor<User> {
+
+	@Override
+	public void contribute(Document document, User user) {
+		try {
+			document.addKeyword("accountEntryIds", getAccountEntryIds(user));
+		}
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Unable to index user " + user.getUserId(), e);
+			}
+		}
+	}
+
+	protected long[] getAccountEntryIds(User user) throws Exception {
+		Set<Long> accountEntryIds = new HashSet<>();
+
+		DynamicQuery dynamicQuery =
+			accountEntryUserRelLocalService.dynamicQuery();
+
+		dynamicQuery.add(
+			RestrictionsFactoryUtil.eq("accountUserId", user.getUserId()));
+
+		List<AccountEntryUserRel> accountEntryUserRels =
+			accountEntryUserRelLocalService.dynamicQuery(dynamicQuery);
+
+		for (AccountEntryUserRel accountEntryUserRel : accountEntryUserRels) {
+			accountEntryIds.add(accountEntryUserRel.getAccountEntryId());
+		}
+
+		return ArrayUtil.toLongArray(accountEntryIds);
+	}
+
+	@Reference
+	protected AccountEntryUserRelLocalService accountEntryUserRelLocalService;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UserModelDocumentContributor.class);
+
+}
