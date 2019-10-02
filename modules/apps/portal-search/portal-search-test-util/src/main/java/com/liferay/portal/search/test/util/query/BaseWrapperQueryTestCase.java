085e0efe524c98c1444f264ff7d60d4744b6197e
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2019-10-01 21:12:58 -0300

    LPS-102620 portal-search-test-util: Create a base test case for wrapper queries.
    https://issues.liferay.com/browse/LPS-102620

---
+++ modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/query/BaseWrapperQueryTestCase.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.test.util.query;
+
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.hits.SearchHit;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.search.query.WrapperQuery;
+import com.liferay.portal.search.test.util.indexing.BaseIndexingTestCase;
+import com.liferay.portal.search.test.util.indexing.DocumentCreationHelpers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Adam Brandizzi
+ */
+public abstract class BaseWrapperQueryTestCase extends BaseIndexingTestCase {
+
+	@BeforeClass
+	public static void setUpClassJSONUtil() {
+		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
+
+		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
+	}
+
+	@Test
+	public void testWrapperQuery() {
+		addDocuments(
+			"java eclipse", "java liferay", "java liferay eclipse",
+			"C is the best language");
+
+		assertSearch(
+			"liferay uses java",
+			Arrays.asList(
+				"java liferay", "java liferay eclipse", "java eclipse"));
+	}
+
+	protected void addDocuments(String... values) {
+		addDocuments(
+			value -> DocumentCreationHelpers.singleText(_FIELD_NAME, value),
+			Arrays.asList(values));
+	}
+
+	protected void assertSearch(Object value, List<String> expectedValues) {
+		assertSearch(
+			indexingTestHelper -> {
+				String query = JSONUtil.put(
+					"match", JSONUtil.put(_FIELD_NAME, value)
+				).toString();
+
+				WrapperQuery wrapperQuery = queries.wrapper(query);
+
+				SearchSearchRequest searchSearchRequest =
+					new SearchSearchRequest();
+
+				searchSearchRequest.setIndexNames("_all");
+				searchSearchRequest.setQuery(wrapperQuery);
+				searchSearchRequest.setSize(30);
+
+				SearchEngineAdapter searchEngineAdapter =
+					getSearchEngineAdapter();
+
+				SearchSearchResponse searchSearchResponse =
+					searchEngineAdapter.execute(searchSearchRequest);
+
+				SearchHits searchHits = searchSearchResponse.getSearchHits();
+
+				Assert.assertEquals(
+					"Total hits", expectedValues.size(),
+					searchHits.getTotalHits());
+
+				List<SearchHit> searchHitsList = searchHits.getSearchHits();
+
+				Assert.assertEquals(
+					"Retrieved hits", expectedValues.size(),
+					searchHitsList.size());
+
+				List<String> actualValues = new ArrayList<>();
+
+				searchHitsList.forEach(
+					searchHit -> {
+						Document document = searchHit.getDocument();
+
+						actualValues.add(document.getString(_FIELD_NAME));
+					});
+
+				Assert.assertEquals(
+					"Retrieved hits ->" + actualValues,
+					expectedValues.toString(), actualValues.toString());
+			});
+	}
+
+	private static final String _FIELD_NAME = "content";
+
+}
