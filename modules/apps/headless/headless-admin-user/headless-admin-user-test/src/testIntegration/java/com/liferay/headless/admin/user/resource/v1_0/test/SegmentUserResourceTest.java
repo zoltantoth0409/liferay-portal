2d674977d9427084e3ffea7ecf412bb9c39bf9ae
Sarai Díaz García <dgarciasarai@gmail.com>
2019-10-02 19:11:21 +0200

    LPS-94731 Add tests for segments users headless API

--- modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/SegmentUserResourceTest.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/SegmentUserResourceTest.java
@@ -15,14 +15,172 @@
 package com.liferay.headless.admin.user.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.admin.user.client.dto.v1_0.SegmentUser;
+import com.liferay.headless.admin.user.client.pagination.Page;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserConstants;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.segments.constants.SegmentsEntryConstants;
+import com.liferay.segments.model.SegmentsEntry;
+import com.liferay.segments.service.SegmentsEntryLocalServiceUtil;
+import com.liferay.segments.test.util.SegmentsTestUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Javier Gamarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class SegmentUserResourceTest extends BaseSegmentUserResourceTestCase {
+
+	@Test
+	public void testGetSegmentUserAccountsEmptyPage() throws Exception {
+		_filterString = "(contains(emailAddress, 'invalid'))";
+
+		Page<SegmentUser> page = segmentUserResource.getSegmentUserAccountsPage(
+			testGetSegmentUserAccountsPage_getSegmentId(), null);
+
+		Assert.assertEquals(0, page.getTotalCount());
+	}
+
+	@Override
+	@Test
+	public void testGetSegmentUserAccountsPage() throws Exception {
+		Page<SegmentUser> page = segmentUserResource.getSegmentUserAccountsPage(
+			testGetSegmentUserAccountsPage_getSegmentId(), null);
+
+		Assert.assertEquals(1, page.getTotalCount());
+
+		Long segmentId = testGetSegmentUserAccountsPage_getSegmentId();
+
+		Long irrelevantSegmentId =
+			testGetSegmentUserAccountsPage_getIrrelevantSegmentId();
+
+		if (irrelevantSegmentId != null) {
+			SegmentUser irrelevantSegmentUser =
+				testGetSegmentUserAccountsPage_addSegmentUser(
+					irrelevantSegmentId, randomIrrelevantSegmentUser());
+
+			page = segmentUserResource.getSegmentUserAccountsPage(
+				irrelevantSegmentId, null);
+
+			Assert.assertEquals(1, page.getTotalCount());
+
+			assertEquals(
+				Arrays.asList(irrelevantSegmentUser),
+				(List<SegmentUser>)page.getItems());
+			assertValid(page);
+		}
+
+		testGetSegmentUserAccountsPage_addSegmentUser(
+			segmentId, randomSegmentUser());
+		testGetSegmentUserAccountsPage_addSegmentUser(
+			segmentId, randomSegmentUser());
+
+		page = segmentUserResource.getSegmentUserAccountsPage(segmentId, null);
+
+		Assert.assertEquals(3, page.getTotalCount());
+
+		assertValid(page);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetSegmentUserAccountsPageWithNonexistingSegmentId()
+		throws Exception {
+
+		segmentUserResource.getSegmentUserAccountsPage(
+			RandomTestUtil.randomLong(), null);
+	}
+
+	@Ignore
+	@Test
+	public void testGetSegmentUserAccountsPageWithPagination() {
+	}
+
+	@Override
+	protected SegmentUser randomSegmentUser() throws Exception {
+		return new SegmentUser() {
+			{
+				emailAddress = RandomTestUtil.randomString() + "@liferay.com";
+				id = RandomTestUtil.randomLong();
+				name = RandomTestUtil.randomString();
+			}
+		};
+	}
+
+	@Override
+	protected SegmentUser testGetSegmentUserAccountsPage_addSegmentUser(
+			Long segmentId, SegmentUser segmentUser)
+		throws Exception {
+
+		User user = UserLocalServiceUtil.addUser(
+			UserConstants.USER_ID_DEFAULT, PortalUtil.getDefaultCompanyId(),
+			true, null, null, true, "", segmentUser.getEmailAddress(), 0,
+			StringPool.BLANK, LocaleUtil.getDefault(), segmentUser.getName(),
+			StringPool.BLANK, segmentUser.getName(), 0, 0, true, 1, 1, 1970, "",
+			null, null, null, null, false, new ServiceContext());
+
+		_users.add(user);
+
+		return _toSegmentUser(user);
+	}
+
+	@Override
+	protected Long testGetSegmentUserAccountsPage_getSegmentId()
+		throws Exception {
+
+		SegmentsEntry segmentsEntry = SegmentsTestUtil.addSegmentsEntry(
+			testGroup.getGroupId(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(), "",
+			User.class.getName());
+
+		segmentsEntry.setActive(true);
+		segmentsEntry.setCriteria(
+			JSONUtil.put(
+				"criteria",
+				JSONUtil.put(
+					"user",
+					JSONUtil.put(
+						"conjunction", "and"
+					).put(
+						"filterString", _filterString
+					))
+			).toString());
+		segmentsEntry.setSource(SegmentsEntryConstants.SOURCE_DEFAULT);
+
+		SegmentsEntry segmentsEntry1 =
+			SegmentsEntryLocalServiceUtil.updateSegmentsEntry(segmentsEntry);
+
+		return segmentsEntry1.getSegmentsEntryId();
+	}
+
+	private SegmentUser _toSegmentUser(User user) {
+		return new SegmentUser() {
+			{
+				emailAddress = user.getEmailAddress();
+				id = user.getUserId();
+				name = user.getFullName();
+			}
+		};
+	}
+
+	private String _filterString = "(contains(emailAddress, 'liferay'))";
+
+	@DeleteAfterTestRun
+	private final List<User> _users = new ArrayList<>();
+
 }
