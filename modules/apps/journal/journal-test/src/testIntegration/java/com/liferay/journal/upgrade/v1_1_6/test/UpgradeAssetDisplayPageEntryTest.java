b0e8c952bc9e5a2a71d346c95418202ac8cc735a
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-10-03 08:36:38 +0200

    LPS-102603 Take into account the unique index in the table, since every DB is different and can handle it in different ways

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/upgrade/v1_1_6/test/UpgradeAssetDisplayPageEntryTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/upgrade/v1_1_6/test/UpgradeAssetDisplayPageEntryTest.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeStep;
@@ -155,7 +156,7 @@ public class UpgradeAssetDisplayPageEntryTest {
 	}
 
 	protected void addJournalArticle(
-			long resourcePrimKey, long groupId, long companyId,
+			long resourcePrimKey, long groupId, long companyId, double version,
 			String layoutUuid)
 		throws Exception {
 
@@ -164,8 +165,8 @@ public class UpgradeAssetDisplayPageEntryTest {
 		sb.append("insert into JournalArticle (uuid_, id_, resourcePrimKey, ");
 		sb.append("groupId, companyId, userId, userName, createDate, ");
 		sb.append("modifiedDate, folderId, classNameId, classPK, treePath, ");
-		sb.append("layoutUuid) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ");
-		sb.append("?, ?)");
+		sb.append("articleId, version, layoutUuid) values (?, ?, ?, ?, ?, ?, ");
+		sb.append("?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
 		String sql = sb.toString();
 
@@ -185,7 +186,9 @@ public class UpgradeAssetDisplayPageEntryTest {
 			ps.setLong(11, 0);
 			ps.setLong(12, 0);
 			ps.setString(13, "/");
-			ps.setString(14, layoutUuid);
+			ps.setString(14, RandomTestUtil.randomString());
+			ps.setDouble(15, version);
+			ps.setString(16, layoutUuid);
 
 			ps.executeUpdate();
 		}
@@ -239,18 +242,20 @@ public class UpgradeAssetDisplayPageEntryTest {
 			long resourcePrimKey)
 		throws Exception {
 
+		double version = 1.0;
+
 		if (multipleArticleVersions) {
 			addJournalArticle(
 				resourcePrimKey, group.getGroupId(), group.getCompanyId(),
-				null);
+				version++, null);
 
 			addJournalArticle(
 				resourcePrimKey, group.getGroupId(), group.getCompanyId(),
-				PortalUUIDUtil.generate());
+				version++, PortalUUIDUtil.generate());
 		}
 
 		addJournalArticle(
-			resourcePrimKey, group.getGroupId(), group.getCompanyId(),
+			resourcePrimKey, group.getGroupId(), group.getCompanyId(), version,
 			layoutUuid);
 	}
 
