b554bf29a2f422dae814cae8ebbe9a402a838bcc
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-10-03 15:50:05 +0200

    LPS-102820 Adds new portlet for page templates and empty views

---
+++ modules/apps/layout/layout-template-admin-web/src/main/java/com/liferay/layout/template/admin/web/internal/portlet/GroupPageTemplatesPortlet.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.template.admin.web.internal.portlet;
+
+import com.liferay.layout.template.admin.constants.LayoutTemplateAdminPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=portlet-layout-templates-admin",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.icon=/icons/group_pages.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.system=true",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Group Page Templates",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + LayoutTemplateAdminPortletKeys.GROUP_PAGE_TEMPLATES,
+		"javax.portlet.resource-bundle=content.Language"
+	},
+	service = Portlet.class
+)
+public class GroupPageTemplatesPortlet extends MVCPortlet {
+}
