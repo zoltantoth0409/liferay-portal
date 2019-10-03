557eaa85d11fbf940c2cb7536d04565d791df8b2
Sarai Díaz García <dgarciasarai@gmail.com>
2019-10-03 15:11:37 +0200

    LPS-102177 Add @review

--- modules/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/jaxrs/exception/mapper/ExperimentVariantTrafficSplitExceptionMapper.java
+++ modules/apps/segments/segments-asah-rest-impl/src/main/java/com/liferay/segments/asah/rest/internal/jaxrs/exception/mapper/ExperimentVariantTrafficSplitExceptionMapper.java
@@ -26,6 +26,7 @@ import org.osgi.service.component.annotations.Component;
  * Converts any {@code SegmentsExperimentRelSplitException} to a {@code 400} error.
  *
  * @author Sarai Díaz
+ * @review
  */
 @Component(
 	property = {
