18194c4013e8e20d0acc156368119575176cbe7f
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-10-03 12:28:23 +0200

    LPS-100589 Validate that we don't have spaces in the collection key

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentRendererTrackerImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentRendererTrackerImpl.java
@@ -24,6 +24,7 @@ import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.fragment.validator.FragmentEntryValidator;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
@@ -179,6 +180,12 @@ public class FragmentRendererTrackerImpl implements FragmentRendererTracker {
 					fragmentRenderer.getKey(),
 					FragmentRendererConstants.
 						FRAGMENT_ENTRY_FRAGMENT_RENDERER_KEY)) {
+
+				return false;
+			}
+
+			if (Validator.isNotNull(fragmentCollectionKey) &&
+				fragmentCollectionKey.contains(StringPool.SPACE)) {
 
 				return false;
 			}
