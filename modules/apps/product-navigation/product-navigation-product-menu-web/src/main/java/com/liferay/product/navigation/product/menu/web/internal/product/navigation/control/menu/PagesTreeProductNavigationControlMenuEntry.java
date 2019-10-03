d640bdee36844119a6cd94273997b60f90ebba71
Pavel Savinov <pavel.savinov@liferay.com>
2019-10-03 09:20:42 +0200

    LPS-102648 Add pages tree sidebar control menu icon

---
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/product/navigation/control/menu/PagesTreeProductNavigationControlMenuEntry.java
@@ -0,0 +1,223 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.product.navigation.product.menu.web.internal.product.navigation.control.menu;
+
+import com.liferay.application.list.PanelCategoryRegistry;
+import com.liferay.petra.reflect.ReflectionUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.SessionClicks;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.product.navigation.control.menu.BaseProductNavigationControlMenuEntry;
+import com.liferay.product.navigation.control.menu.ProductNavigationControlMenuEntry;
+import com.liferay.product.navigation.control.menu.constants.ProductNavigationControlMenuCategoryKeys;
+import com.liferay.product.navigation.product.menu.web.internal.constants.ProductNavigationProductMenuPortletKeys;
+import com.liferay.taglib.portletext.RuntimeTag;
+import com.liferay.taglib.servlet.PageContextFactoryUtil;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.WindowStateException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"product.navigation.control.menu.category.key=" + ProductNavigationControlMenuCategoryKeys.SITES,
+		"product.navigation.control.menu.entry.order:Integer=110"
+	},
+	service = ProductNavigationControlMenuEntry.class
+)
+public class PagesTreeProductNavigationControlMenuEntry
+	extends BaseProductNavigationControlMenuEntry {
+
+	@Override
+	public String getLabel(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public String getURL(HttpServletRequest httpServletRequest) {
+		return null;
+	}
+
+	@Override
+	public boolean includeBody(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		_processBodyBottomContent(
+			PageContextFactoryUtil.create(
+				httpServletRequest, httpServletResponse));
+
+		return true;
+	}
+
+	@Override
+	public boolean includeIcon(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		Map<String, String> values = new HashMap<>();
+
+		String portletNamespace = _portal.getPortletNamespace(
+			ProductNavigationProductMenuPortletKeys.
+				PRODUCT_NAVIGATION_PRODUCT_MENU);
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		values.put("pathThemeImages", themeDisplay.getPathThemeImages());
+
+		values.put("portletNamespace", portletNamespace);
+
+		values.put(
+			"title",
+			HtmlUtil.escape(
+				LanguageUtil.get(httpServletRequest, "find-a-page")));
+
+		String pagesTreeState = SessionClicks.get(
+			httpServletRequest,
+			"com.liferay.product.navigation.product.menu.web_pagesTreeState",
+			"closed");
+
+		if (Objects.equals(pagesTreeState, "open")) {
+			values.put("cssClass", "active");
+			values.put("dataURL", StringPool.BLANK);
+		}
+		else {
+			values.put("cssClass", StringPool.BLANK);
+
+			PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+			PortletURL portletURL = PortletURLFactoryUtil.create(
+				httpServletRequest,
+				ProductNavigationProductMenuPortletKeys.
+					PRODUCT_NAVIGATION_PRODUCT_MENU,
+				RenderRequest.RENDER_PHASE);
+
+			portletURL.setParameter("mvcPath", "/portlet/pages_tree.jsp");
+			portletURL.setParameter("selPpid", portletDisplay.getId());
+
+			try {
+				portletURL.setWindowState(LiferayWindowState.EXCLUSIVE);
+			}
+			catch (WindowStateException wse) {
+				ReflectionUtil.throwException(wse);
+			}
+
+			values.put("dataURL", "data-url='" + portletURL.toString() + "'");
+		}
+
+		Writer writer = httpServletResponse.getWriter();
+
+		writer.write(StringUtil.replace(_TMPL_CONTENT, "${", "}", values));
+
+		return true;
+	}
+
+	@Override
+	public boolean isShow(HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		return false;
+	}
+
+	private void _processBodyBottomContent(PageContext pageContext) {
+		try {
+			JspWriter jspWriter = pageContext.getOut();
+
+			jspWriter.write("<div class=\"");
+
+			HttpServletRequest httpServletRequest =
+				(HttpServletRequest)pageContext.getRequest();
+
+			String pagesTreeState = SessionClicks.get(
+				httpServletRequest,
+				"com.liferay.product.navigation.product.menu.web_" +
+					"pagesTreeState",
+				"closed");
+
+			jspWriter.write(pagesTreeState);
+
+			jspWriter.write(
+				" hidden-print lfr-product-menu-panel sidenav-fixed " +
+					"sidenav-menu-slider\" id=\"");
+
+			String portletNamespace = _portal.getPortletNamespace(
+				ProductNavigationProductMenuPortletKeys.
+					PRODUCT_NAVIGATION_PRODUCT_MENU);
+
+			jspWriter.write(portletNamespace);
+
+			jspWriter.write("pagesTreeSidenavSliderId\">");
+			jspWriter.write(
+				"<div class=\"pages-tree product-menu sidebar sidenav-menu\">");
+
+			RuntimeTag runtimeTag = new RuntimeTag();
+
+			runtimeTag.setPortletName(
+				ProductNavigationProductMenuPortletKeys.
+					PRODUCT_NAVIGATION_PRODUCT_MENU);
+
+			runtimeTag.doTag(pageContext);
+
+			jspWriter.write("</div></div>");
+		}
+		catch (Exception e) {
+			ReflectionUtil.throwException(e);
+		}
+	}
+
+	private static final String _TMPL_CONTENT = StringUtil.read(
+		PagesTreeProductNavigationControlMenuEntry.class,
+		"pages_tree_icon.tmpl");
+
+	@Reference
+	private PanelCategoryRegistry _panelCategoryRegistry;
+
+	@Reference
+	private Portal _portal;
+
+}
