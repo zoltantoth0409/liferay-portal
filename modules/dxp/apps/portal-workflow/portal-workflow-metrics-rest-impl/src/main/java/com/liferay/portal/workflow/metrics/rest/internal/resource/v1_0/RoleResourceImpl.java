42c6bcd96d5921471816498aaf827c5379f60495
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-10-03 09:46:20 -0300

    LPS-98518 | LPS-102669 Implement Role resource

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/resource/v1_0/RoleResourceImpl.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.rest.internal.resource.v1_0;
+
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.comparator.RoleNameComparator;
+import com.liferay.portal.search.aggregation.Aggregations;
+import com.liferay.portal.search.aggregation.bucket.Bucket;
+import com.liferay.portal.search.aggregation.bucket.TermsAggregation;
+import com.liferay.portal.search.aggregation.bucket.TermsAggregationResult;
+import com.liferay.portal.search.engine.adapter.search.SearchRequestExecutor;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
+import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.workflow.metrics.rest.dto.v1_0.Role;
+import com.liferay.portal.workflow.metrics.rest.resource.v1_0.RoleResource;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(
+	properties = "OSGI-INF/liferay/rest/v1_0/role.properties",
+	scope = ServiceScope.PROTOTYPE, service = RoleResource.class
+)
+public class RoleResourceImpl extends BaseRoleResourceImpl {
+
+	@Override
+	public Page<Role> getProcessRolesPage(Long processId) throws Exception {
+		return Page.of(transform(_getRoles(processId), this::_toRole));
+	}
+
+	private BooleanQuery _createTokensBooleanQuery(long processId) {
+		BooleanQuery booleanQuery = _queries.booleanQuery();
+
+		booleanQuery.addMustNotQueryClauses(_queries.term("tokenId", "0"));
+
+		return booleanQuery.addMustQueryClauses(
+			_queries.term("companyId", contextCompany.getCompanyId()),
+			_queries.term("completed", Boolean.FALSE),
+			_queries.term("deleted", Boolean.FALSE),
+			_queries.term("processId", processId));
+	}
+
+	private Set<Long> _getAssigneeIds(long processId) {
+		SearchSearchRequest searchSearchRequest = new SearchSearchRequest();
+
+		TermsAggregation termsAggregation = _aggregations.terms(
+			"assigneeId", "assigneeId");
+
+		termsAggregation.setSize(10000);
+
+		searchSearchRequest.addAggregation(termsAggregation);
+
+		searchSearchRequest.setIndexNames("workflow-metrics-tokens");
+
+		searchSearchRequest.setQuery(_createTokensBooleanQuery(processId));
+
+		return Stream.of(
+			_searchRequestExecutor.executeSearchRequest(searchSearchRequest)
+		).map(
+			SearchSearchResponse::getAggregationResultsMap
+		).map(
+			aggregationResultsMap ->
+				(TermsAggregationResult)aggregationResultsMap.get("assigneeId")
+		).map(
+			TermsAggregationResult::getBuckets
+		).flatMap(
+			Collection::stream
+		).map(
+			Bucket::getKey
+		).map(
+			GetterUtil::getLong
+		).collect(
+			Collectors.toSet()
+		);
+	}
+
+	private Set<com.liferay.portal.kernel.model.Role> _getRoles(Long processId)
+		throws PortalException {
+
+		Set<Long> assigneeIds = _getAssigneeIds(processId);
+
+		if (assigneeIds.isEmpty()) {
+			return Collections.emptySet();
+		}
+
+		Set<com.liferay.portal.kernel.model.Role> roles = new TreeSet<>(
+			new RoleNameComparator(true));
+
+		ActionableDynamicQuery actionableDynamicQuery =
+			_userLocalService.getActionableDynamicQuery();
+
+		actionableDynamicQuery.setAddCriteriaMethod(
+			dynamicQuery -> {
+				Property userIdProperty = PropertyFactoryUtil.forName("userId");
+
+				dynamicQuery.add(userIdProperty.in(assigneeIds));
+			});
+		actionableDynamicQuery.setPerformActionMethod(
+			(User user) -> roles.addAll(user.getRoles()));
+
+		actionableDynamicQuery.performActions();
+
+		return roles;
+	}
+
+	private Role _toRole(com.liferay.portal.kernel.model.Role role) {
+		return new Role() {
+			{
+				id = role.getRoleId();
+				name = role.getName();
+			}
+		};
+	}
+
+	@Reference
+	private Aggregations _aggregations;
+
+	@Reference
+	private Queries _queries;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+	@Reference
+	private SearchRequestExecutor _searchRequestExecutor;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
