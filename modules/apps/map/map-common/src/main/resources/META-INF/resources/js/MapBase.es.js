b78ead7121f7755c1a17a6c113bdd72c85943045
Roland Pákai <roland.pakai@liferay.com>
2019-10-03 19:24:13 +0200

    LPS-97413 Use querySelector instead of firstElementChild

--- modules/apps/map/map-common/src/main/resources/META-INF/resources/js/MapBase.es.js
+++ modules/apps/map/map-common/src/main/resources/META-INF/resources/js/MapBase.es.js
@@ -175,7 +175,7 @@ class MapBase extends State {
 		const customControls = {};
 
 		if (controls.indexOf(this.constructor.CONTROLS.HOME) !== -1) {
-			const homeControl = buildFragment(TPL_HOME_BUTTON).firstElementChild;
+			const homeControl = buildFragment(TPL_HOME_BUTTON).querySelector('.btn.btn-default.home-button');
 			customControls[this.constructor.CONTROLS.HOME] = homeControl;
 			this.addControl(homeControl, this.constructor.POSITION.RIGHT_BOTTOM);
 		}
@@ -184,7 +184,7 @@ class MapBase extends State {
 			controls.indexOf(this.constructor.CONTROLS.SEARCH) !== -1 &&
 			this.constructor.SearchImpl
 		) {
-			const searchControl = buildFragment(TPL_SEARCH_BOX).firstElementChild;
+			const searchControl = buildFragment(TPL_SEARCH_BOX).querySelector('div.col-md-6.search-controls');
 			customControls[
 				this.constructor.CONTROLS.SEARCH
 			] = new this.constructor.SearchImpl({
