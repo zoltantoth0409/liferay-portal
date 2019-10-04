a10f8472adf184c26b964cb9c3ef01c87742eb6d
Greg Hurrell <greg.hurrell@liferay.com>
2019-10-04 08:24:15 +0200

    LPS-102599 Update useThunk to further avoid running unmounted
    Makes the two changes I commented about here, plus a couple of others:
    
    https://github.com/wincent/liferay-portal/pull/67#commitcomment-35348144
    
    Specifically:
    
    - Add an additional layer of wrapping around the `dispatch()` call
      when working with thunks: this means that in addition to guarding
      against an action being dispatched when a thunk is called after
      unmounting, we also guard against an action being dispatched when a
      thunk schedules an asynchronous callback which fires after unmounting;
      eg:
    
          function simpleAction() {
            return dispatch => dispatch({type: 'simpleAction'});
          }
    
          function asyncAction(url) {
            return dispatch => {
              fetch(url)
                .then(response => dispatch({type: 'asyncResponse', response}))
                .catch(error => dispatch({type: 'asyncError', error}));
            };
          }
    
          // We won't fire if we're dismounted before the thunk is called:
          setTimeout(() => dispatch(simpleAction()), 10000);
    
          // We won't fire if we're mounted when the thunk is called but we
          // get dismounted before an asynchronous callback (scheduled by the
          // thunk) is called:
          dispatch(asyncAction(endpoint));
    
    - Stop passing in the closed-over store state, which was bound to be
      stale and probably not very useful.
    
    - Stop returning the value from the thunk as actions are in general
      "fire and forget".
    
    - Improve the accuracy of the comments.

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/hooks/useThunk.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/hooks/useThunk.js
@@ -19,18 +19,22 @@ const {useRef} = React;
 
 /**
  * "Middleware" hook intended to wrap `useReducer` that enables you to dispatch
- * thunks (ie. functions that return actions) as well as plain actions (ie.
+ * thunks (ie. functions that dispatch actions) as well as plain actions (ie.
  * objects).
  */
 export default function useThunk([state, dispatch]) {
 	const isMounted = useIsMounted();
 
-	// Use a ref to ensure `dispatch` is stable across renders, just like the
-	// `dispatch` that we're wrapping.
+	// Use a ref to ensure our `dispatch` is stable across renders, just
+	// like the React-provided `dispatch` that we're wrapping.
 	const thunkDispatch = useRef(action => {
 		if (isMounted()) {
 			if (typeof action === 'function') {
-				return action(dispatch, state);
+				action(payload => {
+					if (isMounted()) {
+						dispatch(payload);
+					}
+				});
 			} else {
 				dispatch(action);
 			}
