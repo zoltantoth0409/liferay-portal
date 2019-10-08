74360c0cb3c9df2287962eae2f2884ab8446ec6f
Kimberly Chau <kimberly.chau@liferay.com>
2019-10-08 08:55:40 -0700

    LPS-102368 Does not remove row when the index value is the same as the number of weeks

--- modules/apps/calendar/calendar-web/src/main/resources/META-INF/resources/js/scheduler.js
+++ modules/apps/calendar/calendar-web/src/main/resources/META-INF/resources/js/scheduler.js
@@ -855,7 +855,7 @@ AUI.add(
 					var weeks = DateMath.getWeeksInMonth(date, firstDayOfWeek);
 
 					A.each(instance.tableRows, function(item, index) {
-						if (index > weeks) {
+						if (index >= weeks) {
 							item.remove();
 						} else if (index < weeks && !item.parentElement) {
 							instance.tableRowContainer.appendChild(item);
