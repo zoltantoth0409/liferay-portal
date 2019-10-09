20853837b789f7786187a5e3d3a3f502f0131c71
rubenpulido <ruben.pulido@liferay.com>
2019-10-09 08:06:03 +0200

    LPS-102948 Test converting widget pages with layout template 1_2_columns_ii with no portlets

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/util/template/test/LayoutConverterTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/util/template/test/LayoutConverterTest.java
@@ -146,6 +146,11 @@ public class LayoutConverterTest {
 	}
 
 	@Test
+	public void testConvertOneTwoColumnsIINoPortlets() throws Exception {
+		_testConvertNoPortlets("1_2_columns_ii");
+	}
+
+	@Test
 	public void testConvertOneTwoColumnsIMultiplePortlets() throws Exception {
 		_testConvertOneTwoColumnsMultiplePortlets("1_2_columns_i");
 	}
