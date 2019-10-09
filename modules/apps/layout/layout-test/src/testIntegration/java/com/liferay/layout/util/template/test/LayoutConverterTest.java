21936e86849c4e4fae8ee004462aad5516c6fc30
rubenpulido <ruben.pulido@liferay.com>
2019-10-09 11:52:41 +0200

    LPS-102948 Test converting widget pages with layout template 1_3_2_columns with a single portlet per column

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/util/template/test/LayoutConverterTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/util/template/test/LayoutConverterTest.java
@@ -280,6 +280,68 @@ public class LayoutConverterTest {
 	}
 
 	@Test
+	public void testConvertOneThreeTwoColumnsSinglePortlet() throws Exception {
+		Map<String, String[]> portletIdsMap1 = new TreeMap<String, String[]>() {
+			{
+				put(
+					"column-1",
+					new String[] {
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet"
+					});
+			}
+		};
+		Map<String, String[]> portletIdsMap2 = new TreeMap<String, String[]>() {
+			{
+				put(
+					"column-1",
+					new String[] {
+						"com_liferay_hello_velocity_web_portlet_" +
+							"HelloVelocityPortlet"
+					});
+				put(
+					"column-2",
+					new String[] {
+						"com_liferay_hello_world_web_portlet_HelloWorldPortlet"
+					});
+				put(
+					"column-3",
+					new String[] {
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet"
+					});
+			}
+		};
+		Map<String, String[]> portletIdsMap3 = new TreeMap<String, String[]>() {
+			{
+				put(
+					"column-1",
+					new String[] {
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet"
+					});
+				put(
+					"column-2",
+					new String[] {
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet"
+					});
+			}
+		};
+
+		List<Map<String, String[]>> portletIdsMaps =
+			new ArrayList<Map<String, String[]>>() {
+				{
+					add(portletIdsMap1);
+					add(portletIdsMap2);
+					add(portletIdsMap3);
+				}
+			};
+
+		_testConvert("1_3_2_columns", portletIdsMaps);
+	}
+
+	@Test
 	public void testConvertOneTwoColumnsIIMultiplePortlets() throws Exception {
 		_testConvertOneTwoColumnsMultiplePortlets("1_2_columns_ii");
 	}
