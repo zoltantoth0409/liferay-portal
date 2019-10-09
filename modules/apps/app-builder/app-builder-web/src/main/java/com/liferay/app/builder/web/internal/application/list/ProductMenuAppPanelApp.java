f2627aac666b910693a015d6c838d8e758c8ea80
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-10-09 11:13:01 +0200

    LPS-101421 Fix null pointer, we need to override the method since we are creating the url with:
    PortletURL portletURL = PortalUtil.getControlPanelPortletURL(
    	httpServletRequest, getGroup(httpServletRequest), getPortletId(), 0,
    	0, PortletRequest.RENDER_PHASE);
    
    and the method getGroup are expecting to have a portlet and for ProductMenuAppPanelApp is null

--- modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/application/list/ProductMenuAppPanelApp.java
+++ modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/application/list/ProductMenuAppPanelApp.java
@@ -19,6 +19,12 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Jeyvison Nascimento
@@ -36,6 +42,23 @@ public class ProductMenuAppPanelApp extends BasePanelApp {
 	@Override
 	public String getPortletId() {
 		return _portletId;
+	}
+
+	@Override
+	public PortletURL getPortletURL(HttpServletRequest httpServletRequest) {
+		PortletURL portletURL = PortalUtil.getControlPanelPortletURL(
+			httpServletRequest, getPortletId(), PortletRequest.RENDER_PHASE);
+
+		Group group = groupProvider.getGroup(httpServletRequest);
+
+		if (group == null) {
+			return portletURL;
+		}
+
+		portletURL.setParameter(
+			"p_v_l_s_g_id", String.valueOf(group.getGroupId()));
+
+		return portletURL;
 	}
 
 	@Override
