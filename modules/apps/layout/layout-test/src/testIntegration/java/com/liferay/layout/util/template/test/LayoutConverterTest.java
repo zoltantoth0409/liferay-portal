6cc5d64e590ed5b2af2e0dc2934d0436f87e54ad
rubenpulido <ruben.pulido@liferay.com>
2019-10-09 14:57:46 +0200

    LPS-102948 Test converting widget pages with layout template 2_2_columns with multiple portlets per column

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/util/template/test/LayoutConverterTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/util/template/test/LayoutConverterTest.java
@@ -707,6 +707,59 @@ public class LayoutConverterTest {
 	}
 
 	@Test
+	public void testConvertTwoTwoColumnsMultiplePortlets() throws Exception {
+		Map<String, String[]> portletIdsMap1 = new TreeMap<String, String[]>() {
+			{
+				put(
+					"column-1",
+					new String[] {
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet",
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet"
+					});
+				put(
+					"column-2",
+					new String[] {
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet",
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet"
+					});
+			}
+		};
+		Map<String, String[]> portletIdsMap2 = new TreeMap<String, String[]>() {
+			{
+				put(
+					"column-1",
+					new String[] {
+						"com_liferay_hello_velocity_web_portlet_" +
+							"HelloVelocityPortlet",
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet"
+					});
+				put(
+					"column-2",
+					new String[] {
+						"com_liferay_hello_world_web_portlet_HelloWorldPortlet",
+						"com_liferay_chart_sample_web_portlet_" +
+							"ChartSamplePortlet"
+					});
+			}
+		};
+
+		List<Map<String, String[]>> portletIdsMaps =
+			new ArrayList<Map<String, String[]>>() {
+				{
+					add(portletIdsMap1);
+					add(portletIdsMap2);
+				}
+			};
+
+		_testConvert("2_2_columns", portletIdsMaps);
+	}
+
+	@Test
 	public void testConvertTwoTwoColumnsNoPortlets() throws Exception {
 		_testConvertNoPortlets("2_2_columns");
 	}
