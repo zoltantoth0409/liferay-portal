0335970342033fb07977259f41e6968d4684eab3
Hugo Huijser <hugo.huijser@liferay.com>
2019-10-09 12:24:20 -0700

    LPS-102451 Combine checks, no logical changes

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/StringMethodsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/StringMethodsCheck.java
@@ -14,6 +14,14 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.tools.ToolsUtil;
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+
+import java.lang.reflect.Field;
+
+import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -30,7 +38,10 @@ public class StringMethodsCheck extends BaseFileCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, String content) {
+			String fileName, String absolutePath, String content)
+		throws ReflectiveOperationException {
+
+		_checkStringUtilReplaceCalls(fileName, content);
 
 		if (isExcludedPath(RUN_OUTSIDE_PORTAL_EXCLUDES, absolutePath)) {
 			return content;
@@ -80,5 +91,64 @@ public class StringMethodsCheck extends BaseFileCheck {
 			}
 		}
 	}
+
+	private void _checkStringUtilReplaceCalls(String fileName, String content)
+		throws ReflectiveOperationException {
+
+		Matcher matcher = _stringUtilReplacePattern.matcher(content);
+
+		while (matcher.find()) {
+			if (ToolsUtil.isInsideQuotes(content, matcher.start())) {
+				continue;
+			}
+
+			List<String> parametersList = JavaSourceUtil.getParameterList(
+				content.substring(matcher.start()));
+
+			if (parametersList.size() != 3) {
+				continue;
+			}
+
+			String secondParameter = parametersList.get(1);
+
+			Matcher singleLengthMatcher = _singleLengthStringPattern.matcher(
+				secondParameter);
+
+			if (!singleLengthMatcher.find()) {
+				continue;
+			}
+
+			String fieldName = singleLengthMatcher.group(2);
+
+			if (fieldName != null) {
+				Field field = StringPool.class.getDeclaredField(fieldName);
+
+				String value = (String)field.get(null);
+
+				if (value.length() != 1) {
+					continue;
+				}
+			}
+
+			String method = matcher.group(1);
+
+			StringBundler sb = new StringBundler(5);
+
+			sb.append("Use StringUtil.");
+			sb.append(method);
+			sb.append("(String, char, char) or StringUtil.");
+			sb.append(method);
+			sb.append("(String, char, String) instead");
+
+			addMessage(
+				fileName, sb.toString(), "string_methods.markdown",
+				getLineNumber(content, matcher.start()));
+		}
+	}
+
+	private static final Pattern _singleLengthStringPattern = Pattern.compile(
+		"^(\".\"|StringPool\\.([A-Z_]+))$");
+	private static final Pattern _stringUtilReplacePattern = Pattern.compile(
+		"StringUtil\\.(replace(First|Last)?)\\(");
 
 }
