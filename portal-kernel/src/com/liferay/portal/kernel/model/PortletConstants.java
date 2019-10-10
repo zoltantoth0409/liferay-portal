887a104ed1e52fbb15cbc39153c23b9465bb1b1b
Dante Wang <dante.wang@liferay.com>
2019-10-10 16:53:58 +0800

    LPS-103251 Remove unused deprecated classes or methods before 7.2.x from com.liferay.portal.kernel.model

--- portal-kernel/src/com/liferay/portal/kernel/model/PortletConstants.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/PortletConstants.java
@@ -14,11 +14,6 @@
 
 package com.liferay.portal.kernel.model;
 
-import com.liferay.portal.kernel.portlet.PortletIdCodec;
-import com.liferay.portal.kernel.util.Validator;
-
-import java.util.Objects;
-
 /**
  * @author Brian Wing Shun Chan
  */
@@ -28,14 +23,6 @@ public class PortletConstants {
 	 * Default preferences.
 	 */
 	public static final String DEFAULT_PREFERENCES = "<portlet-preferences />";
-
-	/**
-	 * Facebook integration method for FBML.
-	 *
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static final String FACEBOOK_INTEGRATION_FBML = "fbml";
 
 	/**
 	 * Facebook integration method for IFrame.
@@ -65,156 +52,5 @@ public class PortletConstants {
 	 * War file separator.
 	 */
 	public static final String WAR_SEPARATOR = "_WAR_";
-
-	/**
-	 * Returns a properly assembled portlet ID from the parameters passed. If
-	 * the portlet ID contains an instance ID it will be properly retained. If
-	 * the portlet ID contains a user ID it will be replaced by the user ID
-	 * parameter.
-	 *
-	 * @param      portletId the portlet ID
-	 * @param      userId a user ID
-	 * @return     the properly assembled portlet ID
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static String assemblePortletId(String portletId, long userId) {
-		return PortletIdCodec.encode(
-			getRootPortletId(portletId), userId, getInstanceId(portletId));
-	}
-
-	/**
-	 * Returns a properly assembled portlet ID from the parameters passed. If
-	 * the portlet ID contains a user ID it will be replaced by the user ID
-	 * parameter. If the portlet ID contains an instance ID it will be replaced
-	 * by the instance ID parameter.
-	 *
-	 * @param      portletId the portlet ID
-	 * @param      userId the user ID
-	 * @param      instanceId an instance ID. If <code>null</code>, an instance
-	 *             ID is derived from the portlet ID.
-	 * @return     the properly assembled portlet ID
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static String assemblePortletId(
-		String portletId, long userId, String instanceId) {
-
-		if (Validator.isNull(instanceId)) {
-			instanceId = getInstanceId(portletId);
-		}
-
-		return PortletIdCodec.encode(
-			getRootPortletId(portletId), userId, instanceId);
-	}
-
-	/**
-	 * Returns a properly assembled portlet ID from the parameters passed. If
-	 * the portlet ID contains a user ID it will be properly retained. If the
-	 * portlet ID contains an instance ID it will be replaced by the instance ID
-	 * parameter.
-	 *
-	 * @param      portletId the portlet ID
-	 * @param      instanceId an instance ID
-	 * @return     the properly assembled portlet ID
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static String assemblePortletId(
-		String portletId, String instanceId) {
-
-		PortletIdCodec.validatePortletName(portletId);
-
-		return PortletIdCodec.encode(portletId, instanceId);
-	}
-
-	/**
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
-	 *             PortletIdCodec#generateInstanceId()}
-	 */
-	@Deprecated
-	public static String generateInstanceId() {
-		return PortletIdCodec.generateInstanceId();
-	}
-
-	/**
-	 * Returns the instance ID of the portlet.
-	 *
-	 * @param      portletId the portlet ID
-	 * @return     the instance ID of the portlet
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
-	 *             PortletIdCodec#decodeInstanceId(String)}
-	 */
-	@Deprecated
-	public static String getInstanceId(String portletId) {
-		return PortletIdCodec.decodeInstanceId(portletId);
-	}
-
-	/**
-	 * Returns the root portlet ID of the portlet.
-	 *
-	 * @param      portletId the portlet ID
-	 * @return     the root portlet ID of the portlet
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
-	 *             PortletIdCodec#decodePortletName(String)}
-	 */
-	@Deprecated
-	public static String getRootPortletId(String portletId) {
-		return PortletIdCodec.decodePortletName(portletId);
-	}
-
-	/**
-	 * Returns the user ID of the portlet. This only applies when the portlet is
-	 * added by a user to a page in customizable mode.
-	 *
-	 * @param      portletId the portlet ID
-	 * @return     the user ID of the portlet
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
-	 *             PortletIdCodec#decodeUserId(String)}
-	 */
-	@Deprecated
-	public static long getUserId(String portletId) {
-		return PortletIdCodec.decodeUserId(portletId);
-	}
-
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static boolean hasIdenticalRootPortletId(
-		String portletId1, String portletId2) {
-
-		return Objects.equals(
-			PortletIdCodec.decodePortletName(portletId1),
-			PortletIdCodec.decodePortletName(portletId2));
-	}
-
-	/**
-	 * Returns <code>true</code> if the portlet ID contains an instance ID.
-	 *
-	 * @param      portletId the portlet ID
-	 * @return     <code>true</code> if the portlet ID contains an instance ID;
-	 *             <code>false</code> otherwise
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
-	 *             PortletIdCodec#hasInstanceId(String)}
-	 */
-	@Deprecated
-	public static boolean hasInstanceId(String portletId) {
-		return PortletIdCodec.hasInstanceId(portletId);
-	}
-
-	/**
-	 * Returns <code>true</code> if the portlet ID contains a user ID.
-	 *
-	 * @param      portletId the portlet ID
-	 * @return     <code>true</code> if the portlet ID contains a user ID;
-	 *             <code>false</code> otherwise
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
-	 *             PortletIdCodec#hasUserId(String)}
-	 */
-	@Deprecated
-	public static boolean hasUserId(String portletId) {
-		return PortletIdCodec.hasUserId(portletId);
-	}
 
 }
