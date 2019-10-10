9415c829597f0cd6700fe1847d0e20062468824c
Samuel Tran <samuel.tran@liferay.com>
2019-10-09 17:01:08 -0700

    LPS-101084 Add ViewCountService in kernel

---
+++ portal-kernel/src/com/liferay/portal/kernel/service/view/count/ViewCountService.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.service.view.count;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Preston Crary
+ */
+@ProviderType
+public interface ViewCountService {
+
+	public void addViewCountEntry(
+		long companyId, long classNameId, long classPK);
+
+	public long getViewCount(long companyId, long classNameId, long classPK);
+
+	public void incrementViewCount(
+		long companyId, long classNameId, long classPK);
+
+	public void incrementViewCount(
+		long companyId, long classNameId, long classPK, int increment);
+
+	public void removeViewCount(long companyId, long classNameId, long classPK)
+		throws PortalException;
+
+}
---
+++ portal-kernel/src/com/liferay/portal/kernel/service/view/count/ViewCountServiceUtil.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.service.view.count;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.ServiceProxyFactory;
+
+/**
+ * @author Preston Crary
+ */
+public class ViewCountServiceUtil {
+
+	public static void addViewCountEntry(
+		long companyId, long classNameId, long classPK) {
+
+		_viewCountService.addViewCountEntry(companyId, classNameId, classPK);
+	}
+
+	public static long getViewCount(
+		long companyId, long classNameId, long classPK) {
+
+		return _viewCountService.getViewCount(companyId, classNameId, classPK);
+	}
+
+	public static void incrementViewCount(
+		long companyId, long classNameId, long classPK) {
+
+		_viewCountService.incrementViewCount(companyId, classNameId, classPK);
+	}
+
+	public static void incrementViewCount(
+		long companyId, long classNameId, long classPK, int increment) {
+
+		_viewCountService.incrementViewCount(
+			companyId, classNameId, classPK, increment);
+	}
+
+	public static void removeViewCount(
+			long companyId, long classNameId, long classPK)
+		throws PortalException {
+
+		_viewCountService.removeViewCount(companyId, classNameId, classPK);
+	}
+
+	private static volatile ViewCountService _viewCountService =
+		ServiceProxyFactory.newServiceTrackedInstance(
+			ViewCountService.class, ViewCountServiceUtil.class,
+			"_viewCountService", true);
+
+}
