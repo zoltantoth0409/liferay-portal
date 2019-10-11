60509dc0296e8fed9f51f4316c0a3ebd8d633548
Greg Hurrell <greg.hurrell@liferay.com>
2019-10-11 14:33:03 +0200

    LPS-102599 Make useLoad callback function stable
    Same as we did with useStateSafe in the last commit.

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/core/hooks/useLoad.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/core/hooks/useLoad.js
@@ -15,7 +15,7 @@
 import {useIsMounted} from 'frontend-js-react-web';
 import React from 'react';
 
-const {useRef} = React;
+const {useCallback, useRef} = React;
 
 /**
  * Provides a way to load a module on demand.
@@ -29,30 +29,33 @@ export default function useLoad() {
 
 	const isMounted = useIsMounted();
 
-	return function load(key, entryPoint) {
-		if (!modules.current.get(key)) {
-			modules.current.set(
-				key,
-				new Promise((resolve, reject) => {
-					Liferay.Loader.require(
-						[entryPoint],
-						Plugin => {
-							if (isMounted()) {
-								resolve(Plugin.default);
+	return useCallback(
+		(key, entryPoint) => {
+			if (!modules.current.get(key)) {
+				modules.current.set(
+					key,
+					new Promise((resolve, reject) => {
+						Liferay.Loader.require(
+							[entryPoint],
+							Plugin => {
+								if (isMounted()) {
+									resolve(Plugin.default);
+								}
+							},
+							error => {
+								if (isMounted()) {
+									// Reset to allow future retries.
+									modules.current.delete(key);
+									reject(error);
+								}
 							}
-						},
-						error => {
-							if (isMounted()) {
-								// Reset to allow future retries.
-								modules.current.delete(key);
-								reject(error);
-							}
-						}
-					);
-				})
-			);
-		}
+						);
+					})
+				);
+			}
 
-		return modules.current.get(key);
-	};
+			return modules.current.get(key);
+		},
+		[isMounted]
+	);
 }
