0999f0de6e9ca72f692dfea5267e11677521bf11
David Truong <david.truong@liferay.com>
2019-10-10 21:22:48 -0500

    LPS-103051 use jsass for sass compiler

---
+++ modules/util/sass-compiler-jsass/src/main/java/com/liferay/sass/compiler/jsass/internal/JSassCompiler.java
@@ -0,0 +1,252 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sass.compiler.jsass.internal;
+
+import com.liferay.sass.compiler.SassCompiler;
+
+import io.bit3.jsass.Compiler;
+import io.bit3.jsass.Options;
+import io.bit3.jsass.Output;
+import io.bit3.jsass.OutputStyle;
+import io.bit3.jsass.context.FileContext;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import java.nio.file.Files;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author David Truong
+ */
+public class JSassCompiler implements SassCompiler {
+
+	public JSassCompiler() {
+		this(_PRECISION_DEFAULT);
+	}
+
+	public JSassCompiler(int precision) {
+		this(precision, System.getProperty("java.io.tmpdir"));
+	}
+
+	public JSassCompiler(int precision, String tmpDirName) {
+		_precision = precision;
+		_tmpDirName = tmpDirName;
+	}
+
+	@Override
+	public String compileFile(String inputFileName, String includeDirName)
+		throws JSassCompilerException {
+
+		return compileFile(inputFileName, includeDirName, false, "");
+	}
+
+	@Override
+	public String compileFile(
+			String inputFileName, String includeDirName,
+			boolean generateSourceMap)
+		throws JSassCompilerException {
+
+		return compileFile(
+			inputFileName, includeDirName, generateSourceMap, "");
+	}
+
+	@Override
+	public String compileFile(
+			String inputFileName, String includeDirName,
+			boolean generateSourceMap, String sourceMapFileName)
+		throws JSassCompilerException {
+
+		try {
+			Options options = new Options();
+
+			List<File> includeDirNames = new ArrayList<>();
+
+			for (String fileName : includeDirName.split(File.pathSeparator)) {
+				File file = new File(fileName);
+
+				includeDirNames.add(file.getCanonicalFile());
+			}
+
+			File inputFile = new File(inputFileName);
+
+			includeDirNames.add(inputFile.getParentFile());
+
+			options.setIncludePaths(includeDirNames);
+			options.setPrecision(_precision);
+			options.setSourceComments(false);
+			options.setOutputStyle(OutputStyle.NESTED);
+
+			if (generateSourceMap) {
+				if ((sourceMapFileName == null) ||
+					sourceMapFileName.equals("")) {
+
+					sourceMapFileName =
+						getOutputFileName(inputFileName) + ".map";
+				}
+
+				File sourceMapFile = new File(sourceMapFileName);
+
+				options.setSourceMapContents(false);
+				options.setSourceMapEmbed(false);
+				options.setSourceMapFile(sourceMapFile.toURI());
+				options.setOmitSourceMapUrl(false);
+			}
+
+			FileContext fileContext = new FileContext(
+				inputFile.toURI(), null, options);
+
+			Compiler compiler = new Compiler();
+
+			Output output = compiler.compile(fileContext);
+
+			if (output == null) {
+				throw new JSassCompilerException("Null output");
+			}
+
+			if (generateSourceMap) {
+				try {
+					File sourceMapFile = new File(sourceMapFileName);
+
+					String sourceMapOutput = output.getSourceMap();
+
+					write(sourceMapFile, sourceMapOutput);
+				}
+				catch (Exception e) {
+					System.out.println("Unable to create source map");
+				}
+			}
+
+			return output.getCss();
+		}
+		catch (Exception e) {
+			throw new JSassCompilerException(e);
+		}
+	}
+
+	@Override
+	public String compileString(String input, String includeDirName)
+		throws JSassCompilerException {
+
+		return compileString(input, "", includeDirName, false);
+	}
+
+	@Override
+	public String compileString(
+			String input, String inputFileName, String includeDirName,
+			boolean generateSourceMap)
+		throws JSassCompilerException {
+
+		return compileString(
+			input, inputFileName, includeDirName, generateSourceMap, "");
+	}
+
+	@Override
+	public String compileString(
+			String input, String inputFileName, String includeDirName,
+			boolean generateSourceMap, String sourceMapFileName)
+		throws JSassCompilerException {
+
+		try {
+			if ((inputFileName == null) || inputFileName.equals("")) {
+				inputFileName = _tmpDirName + File.separator + "tmp.scss";
+
+				if (generateSourceMap) {
+					System.out.println("Source maps require a valid file name");
+
+					generateSourceMap = false;
+				}
+			}
+
+			int index = inputFileName.lastIndexOf(File.separatorChar);
+
+			if ((index == -1) && (File.separatorChar != '/')) {
+				index = inputFileName.lastIndexOf('/');
+			}
+
+			index += 1;
+
+			String dirName = inputFileName.substring(0, index);
+
+			String fileName = inputFileName.substring(index);
+
+			String outputFileName = getOutputFileName(fileName);
+
+			if ((sourceMapFileName == null) || sourceMapFileName.equals("")) {
+				sourceMapFileName = dirName + outputFileName + ".map";
+			}
+
+			File tempFile = new File(dirName, "tmp.scss");
+
+			tempFile.deleteOnExit();
+
+			write(tempFile, input);
+
+			String output = compileFile(
+				tempFile.getCanonicalPath(), includeDirName, generateSourceMap,
+				sourceMapFileName);
+
+			if (generateSourceMap) {
+				File sourceMapFile = new File(sourceMapFileName);
+
+				String sourceMapContent = new String(
+					Files.readAllBytes(sourceMapFile.toPath()));
+
+				sourceMapContent = sourceMapContent.replaceAll(
+					"tmp\\.scss", fileName);
+				sourceMapContent = sourceMapContent.replaceAll(
+					"tmp\\.css", outputFileName);
+
+				write(sourceMapFile, sourceMapContent);
+			}
+
+			return output;
+		}
+		catch (Throwable t) {
+			throw new JSassCompilerException(t);
+		}
+	}
+
+	protected String getOutputFileName(String fileName) {
+		return fileName.replaceAll("scss$", "css");
+	}
+
+	protected void write(File file, String string) throws IOException {
+		if (!file.exists()) {
+			File parentFile = file.getParentFile();
+
+			parentFile.mkdirs();
+
+			file.createNewFile();
+		}
+
+		try (Writer writer = new OutputStreamWriter(
+				new FileOutputStream(file, false), "UTF-8")) {
+
+			writer.write(string);
+		}
+	}
+
+	private static final int _PRECISION_DEFAULT = 5;
+
+	private final int _precision;
+	private final String _tmpDirName;
+
+}
---
+++ modules/util/sass-compiler-jsass/src/test/java/com/liferay/sass/compiler/jsass/internal/JSassCompilerTest.java
@@ -0,0 +1,240 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.sass.compiler.jsass.internal;
+
+import com.liferay.sass.compiler.SassCompiler;
+
+import java.io.File;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author David Truong
+ */
+public class JSassCompilerTest {
+
+	@Test
+	public void testBoxShadowTransparent() throws Exception {
+		SassCompiler sassCompiler = new JSassCompiler();
+
+		String expectedOutput =
+			"foo { box-shadow: 2px 4px 7px rgba(0, 0, 0, 0.5); }";
+		String actualOutput = sassCompiler.compileString(
+			"foo { box-shadow: 2px 4px 7px rgba(0, 0, 0, 0.5); }", "");
+
+		Assert.assertEquals(
+			stripNewLines(expectedOutput), stripNewLines(actualOutput));
+	}
+
+	@Test
+	public void testCompileFileClayCss() throws Exception {
+		SassCompiler sassCompiler = new JSassCompiler();
+
+		File sassSpecDir = new File(
+			"../sass-compiler-jni/src/test/resources/com/liferay/sass" +
+				"/compiler/jni/internal/dependencies/sass-spec");
+
+		for (File testDir : sassSpecDir.listFiles()) {
+			File inputFile = new File(testDir, "input.scss");
+
+			String dirName = testDir.getName();
+
+			if (!inputFile.exists() || dirName.endsWith("-4.0")) {
+				continue;
+			}
+
+			String actualOutput = sassCompiler.compileFile(
+				inputFile.getCanonicalPath(), "", false);
+
+			Assert.assertNotNull("Testing: " + dirName, actualOutput);
+
+			File expectedOutputFile = new File(testDir, "expected_output.css");
+
+			String expectedOutput = read(expectedOutputFile.toPath());
+
+			Assert.assertEquals(
+				"Testing: " + dirName, stripNewLines(expectedOutput),
+				stripNewLines(actualOutput));
+		}
+	}
+
+	@Test
+	public void testCompileFileSassVariableWithUnicode() throws Exception {
+		SassCompiler sassCompiler = new JSassCompiler();
+
+		File inputDir = new File(
+			"../sass-compiler-jni/src/test/resources/com/liferay/sass" +
+				"/compiler/jni/internal/dependencies/");
+
+		File inputFile = new File(inputDir, "/unicode/input.scss");
+
+		String actualOutput = sassCompiler.compileFile(
+			inputFile.getCanonicalPath(), "");
+
+		Assert.assertNotNull(actualOutput);
+
+		File expectedOutputFile = new File(
+			inputDir, "/unicode/expected_output.css");
+
+		String expectedOutput = read(expectedOutputFile.toPath());
+
+		Assert.assertEquals(
+			stripNewLines(expectedOutput), stripNewLines(actualOutput));
+	}
+
+	@Test
+	public void testCompileFileWithSourceMap() throws Exception {
+		SassCompiler sassCompiler = new JSassCompiler();
+
+		File inputDir = new File(
+			"../sass-compiler-jni/src/test/resources/com/liferay/sass" +
+				"/compiler/jni/internal/dependencies/sass-spec/14_imports");
+
+		File sourceMapFile = new File(inputDir, ".sass-cache/input.css.map");
+
+		sourceMapFile.deleteOnExit();
+
+		Assert.assertFalse(sourceMapFile.exists());
+
+		File inputFile = new File(inputDir, "input.scss");
+
+		String actualOutput = sassCompiler.compileFile(
+			inputFile.getCanonicalPath(), "", true,
+			sourceMapFile.getCanonicalPath());
+
+		Assert.assertNotNull(actualOutput);
+
+		Assert.assertTrue(sourceMapFile.exists());
+
+		File expectedOutputFile = new File(
+			"../sass-compiler-jni/src/test/resources/com/liferay/sass" +
+				"/compiler/jni/internal/dependencies/sourcemap",
+			"expected_output_custom_source_map.css");
+
+		String expectedOutput = read(expectedOutputFile.toPath());
+
+		Assert.assertEquals(
+			stripNewLines(expectedOutput), stripNewLines(actualOutput));
+	}
+
+	@Test
+	public void testCompileString() throws Exception {
+		SassCompiler sassCompiler = new JSassCompiler();
+
+		String expectedOutput = "foo { margin: 42px; }";
+		String actualOutput = sassCompiler.compileString(
+			"foo { margin: 21px * 2; }", "");
+
+		Assert.assertEquals(
+			stripNewLines(expectedOutput), stripNewLines(actualOutput));
+	}
+
+	@Test
+	public void testCompileStringSassVariableWithUnicode() throws Exception {
+		SassCompiler sassCompiler = new JSassCompiler();
+
+		File inputDir = new File(
+			"../sass-compiler-jni/src/test/resources/com/liferay/sass" +
+				"/compiler/jni/internal/dependencies/");
+
+		File inputFile = new File(inputDir, "/unicode/input.scss");
+
+		String input = read(inputFile.toPath());
+
+		String actualOutput = sassCompiler.compileString(input, "");
+
+		Assert.assertNotNull(actualOutput);
+
+		File expectedOutputFile = new File(
+			inputDir, "/unicode/expected_output.css");
+
+		String expectedOutput = read(expectedOutputFile.toPath());
+
+		Assert.assertEquals(
+			stripNewLines(expectedOutput), stripNewLines(actualOutput));
+	}
+
+	@Test
+	public void testCompileStringWithSourceMap() throws Exception {
+		SassCompiler sassCompiler = new JSassCompiler();
+
+		File inputDir = new File(
+			"../sass-compiler-jni/src/test/resources/com/liferay/sass" +
+				"/compiler/jni/internal/dependencies/sass-spec/14_imports");
+
+		File sourceMapFile = new File(inputDir, "input.css.map");
+
+		sourceMapFile.deleteOnExit();
+
+		Assert.assertFalse(sourceMapFile.exists());
+
+		File inputFile = new File(inputDir, "input.scss");
+
+		String input = read(inputFile.toPath());
+
+		String actualOutput = sassCompiler.compileString(
+			input, inputFile.getCanonicalPath(), "", true);
+
+		Assert.assertNotNull(actualOutput);
+
+		Assert.assertTrue(sourceMapFile.exists());
+
+		File expectedOutputFile = new File(
+			"../sass-compiler-jni/src/test/resources/com/liferay/sass" +
+				"/compiler/jni/internal/dependencies/sourcemap",
+			"expected_output.css");
+
+		String expectedOutput = read(expectedOutputFile.toPath());
+
+		Assert.assertEquals(
+			stripNewLines(expectedOutput), stripNewLines(actualOutput));
+	}
+
+	@Test
+	public void testSassPrecision() throws Exception {
+		SassCompiler sassCompiler = new JSassCompiler(10);
+
+		String expectedOutput = ".foo { line-height: 1.428571429; }";
+		String actualOutput = sassCompiler.compileString(
+			"$val: 1.428571429;.foo { line-height: $val; }", "");
+
+		Assert.assertEquals(
+			stripNewLines(expectedOutput), stripNewLines(actualOutput));
+
+		sassCompiler = new JSassCompiler(3);
+
+		expectedOutput = ".foo { line-height: 1.429; }";
+		actualOutput = sassCompiler.compileString(
+			"$val: 1.428571429;.foo { line-height: $val; }", "");
+
+		Assert.assertEquals(
+			stripNewLines(expectedOutput), stripNewLines(actualOutput));
+	}
+
+	protected String read(Path filePath) throws Exception {
+		return new String(Files.readAllBytes(filePath));
+	}
+
+	protected String stripNewLines(String string) {
+		string = string.replaceAll("\\n|\\r", "");
+
+		return string.replaceAll("\\s", "");
+	}
+
+}
