ca4255ce9d76fd143d8b583f07c7e4261ad096cf
Dante Wang <dante.wang@liferay.com>
2019-10-11 13:13:47 +0800

    LPS-103257 Remove unused pre-7.2.x deprecations from com.liferay.portal.kernel.search.hits

--- portal-kernel/src/com/liferay/portal/kernel/search/hits/HitsProcessorRegistryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/hits/HitsProcessorRegistryUtil.java
@@ -24,14 +24,6 @@ import com.liferay.portal.kernel.util.ServiceProxyFactory;
  */
 public class HitsProcessorRegistryUtil {
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static HitsProcessorRegistry getHitsProcessorRegistry() {
-		return _hitsProcessorRegistry;
-	}
-
 	public static boolean process(SearchContext searchContext, Hits hits)
 		throws SearchException {
 
