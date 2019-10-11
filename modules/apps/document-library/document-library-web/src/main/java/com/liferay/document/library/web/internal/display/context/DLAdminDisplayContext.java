157fa1b237c2b76cb3371b16d5a355b8722c8586
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2019-10-11 11:34:44 -0700

    LPS-103047 Add folderId to searchContext to get the correct filtered results

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLAdminDisplayContext.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLAdminDisplayContext.java
@@ -428,11 +428,18 @@ public class DLAdminDisplayContext {
 					_request.getLocale());
 			}
 
+			List<Long> folderIds = new ArrayList<>(1);
+
+			if (folderId != DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+				folderIds.add(folderId);
+			}
+
 			SearchContext searchContext = SearchContextFactory.getInstance(
 				_request);
 
 			searchContext.setAttribute("paginationType", "none");
 			searchContext.setEnd(dlSearchContainer.getEnd());
+			searchContext.setFolderIds(folderIds);
 
 			int type = Sort.STRING_TYPE;
 			String fieldName = orderByCol;
