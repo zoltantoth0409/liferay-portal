46c923bde69de7247cc71763fbc53de26c536f94
Leon Chi <leon.chi@liferay.com>
2019-10-12 16:09:18 +0800

    LPS-103202 Remove deprecated methods in class DataSourceFactoryImpl

--- portal-impl/src/com/liferay/portal/dao/jdbc/DataSourceFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/dao/jdbc/DataSourceFactoryImpl.java
@@ -223,16 +223,6 @@ public class DataSourceFactoryImpl implements DataSourceFactory {
 		return initDataSource(properties);
 	}
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public interface PACL {
-
-		public DataSource getDataSource(DataSource dataSource);
-
-	}
-
 	protected DataSource initDataSourceC3PO(Properties properties)
 		throws Exception {
 
