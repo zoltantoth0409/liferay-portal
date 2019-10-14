86500115c3709b9b6d8615d22e6fd224dbe3f87a
Sandro Chinea <sandrochinea@gmail.com>
2019-10-14 17:49:57 +0200

    LPS-102829 Added tests for SidebarWidgets

---
+++ modules/apps/layout/layout-content-page-editor-web/test/js/components/sidebar/widgets/SidebarWidgets.es.js
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {cleanup, render, fireEvent} from '@testing-library/react';
+import React from 'react';
+import SidebarWidgets from '../../../../../src/main/resources/META-INF/resources/js/components/sidebar/widgets/SidebarWidgets.es';
+import StateProvider from '../../../../../src/main/resources/META-INF/resources/js/store/StateProvider.es';
+import Store from '../../../../../src/main/resources/META-INF/resources/js/store/store.es';
+import StoreContext from '../../../../../src/main/resources/META-INF/resources/js/store/StoreContext.es';
+import userEvent from '@testing-library/user-event';
+
+const widgets = [
+	{
+		portlets: [
+			{
+				instanceable: true,
+				portletId: 'assetPublisher',
+				title: 'Asset Publisher',
+				used: false
+			},
+			{
+				instanceable: true,
+				portletId: 'DocumentsAndMedia',
+				title: 'Documents and Media',
+				used: false
+			},
+			{
+				instanceable: true,
+				portletId: 'NavigationMenu',
+				title: 'Navigation Menu',
+				used: false
+			}
+		],
+		title: 'Highlighted'
+	},
+	{
+		portlets: [
+			{
+				instanceable: false,
+				portletId: 'Blogs',
+				title: 'Blogs',
+				used: false
+			},
+			{
+				instanceable: false,
+				portletId: 'BlogsAggregator',
+				title: 'Blogs Aggregator',
+				used: false
+			}
+		],
+		title: 'Collaboration'
+	}
+];
+
+const store = new Store({widgets}, () => {});
+
+const RenderSidebarWidgets = () => {
+	return (
+		<StoreContext.Provider value={store}>
+			<StateProvider>
+				<SidebarWidgets />
+			</StateProvider>
+		</StoreContext.Provider>
+	);
+};
+
+describe('SidebarWidgets', () => {
+	afterEach(cleanup);
+
+	it('renders the categories collapsed', () => {
+		const {getByLabelText, getByText, queryByText} = render(
+			<RenderSidebarWidgets />
+		);
+
+		expect(getByText('Collaboration')).not.toBe(null);
+		expect(getByText('Highlighted')).not.toBe(null);
+		expect(getByLabelText('search-form')).not.toBe(null);
+
+		expect(queryByText('Asset Publisher')).toBe(null);
+	});
+
+	it('expands a category on click and closes it when clicking it again', () => {
+		const {getByText, queryByText} = render(<RenderSidebarWidgets />);
+		const highlightedCategory = getByText('Highlighted');
+
+		fireEvent.click(highlightedCategory);
+
+		expect(highlightedCategory.getAttribute('aria-expanded')).toBe('true');
+
+		expect(getByText('Asset Publisher')).not.toBe(null);
+
+		fireEvent.click(highlightedCategory);
+
+		expect(highlightedCategory.getAttribute('aria-expanded')).toBe('false');
+
+		expect(queryByText('Asset Publisher')).toBe(null);
+	});
+
+	it('finds a widget when you search it', () => {
+		const {getByLabelText, getByText, queryByText} = render(
+			<RenderSidebarWidgets />
+		);
+
+		userEvent.type(getByLabelText('search-form'), 'asset');
+
+		expect(getByText('Asset Publisher')).not.toBe(null);
+
+		expect(queryByText('Blogs')).toBe(null);
+	});
+
+	it('expands all categories when you type something in search form', () => {
+		const {getByLabelText, getByText} = render(<RenderSidebarWidgets />);
+
+		userEvent.type(getByLabelText('search-form'), 'a');
+
+		expect(getByText('Collaboration').getAttribute('aria-expanded')).toBe(
+			'true'
+		);
+		expect(getByText('Highlighted').getAttribute('aria-expanded')).toBe(
+			'true'
+		);
+	});
+});
