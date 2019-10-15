d44d5c8ee2b33b01a700a8c442c0be032beb662e
Pavel Savinov <pavel.savinov@liferay.com>
2019-10-15 17:09:20 +0200

    LPS-102651 Set pages tree state to closed when pages tree icon is hidden

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/product/navigation/control/menu/PagesTreeProductNavigationControlMenuEntry.java
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/java/com/liferay/product/navigation/product/menu/web/internal/product/navigation/control/menu/PagesTreeProductNavigationControlMenuEntry.java
@@ -20,6 +20,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.LiferayWindowState;
 import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.theme.PortletDisplay;
@@ -171,9 +172,23 @@ public class PagesTreeProductNavigationControlMenuEntry
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
 
+		Layout layout = themeDisplay.getLayout();
+
 		Group group = themeDisplay.getScopeGroup();
 
-		if (group.isCompany() || group.isControlPanel()) {
+		if (group.isCompany() || group.isControlPanel() ||
+			layout.isTypeControlPanel()) {
+
+			SessionClicks.put(
+				httpServletRequest,
+				"com.liferay.product.navigation.product.menu.web_" +
+					"pagesTreeState",
+				"closed");
+
+			httpServletRequest.setAttribute(
+				ProductNavigationProductMenuWebKeys.PANEL_NAME,
+				ProductNavigationProductMenuWebKeys.PRODUCT_MENU);
+
 			return false;
 		}
 
