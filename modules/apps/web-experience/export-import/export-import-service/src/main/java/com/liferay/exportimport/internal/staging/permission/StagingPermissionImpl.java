590ba393c7daa88e6759df9b1164389492070806
Kimberly Chau <kimberly.chau@liferay.com>
2019-10-15 11:25:03 -0700

    LPS-103282 Allow SUBSCRIBE action in live to proceed after proper permission check

--- modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/permission/StagingPermissionImpl.java
+++ modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/permission/StagingPermissionImpl.java
@@ -89,6 +89,7 @@ public class StagingPermissionImpl implements StagingPermission {
 			!actionId.equals(ActionKeys.CUSTOMIZE) &&
 			!actionId.equals(ActionKeys.DELETE) &&
 			!actionId.equals(ActionKeys.DELETE_DISCUSSION) &&
+			!actionId.equals(ActionKeys.SUBSCRIBE) &&
 			!actionId.equals(ActionKeys.UPDATE_DISCUSSION) &&
 			!actionId.equals(ActionKeys.VIEW) &&
 			group.hasLocalOrRemoteStagingGroup() &&
