368431704c8981c46496a0963f6736e6b3dc25e2
drewbrokke <drew.brokke@liferay.com>
2019-10-16 16:50:54 -0500

    LPS-103409 roles-admin-impl - implements RoleSegmentsEntryRetrieverImpl.java

---
+++ modules/apps/roles/roles-admin-impl/src/main/java/com/liferay/roles/admin/internal/segments/entry/RoleSegmentsEntryRetrieverImpl.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.roles.admin.internal.segments.entry;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.roles.admin.segments.entry.RoleSegmentsEntryRetriever;
+import com.liferay.segments.model.SegmentsEntry;
+import com.liferay.segments.service.SegmentsEntryLocalService;
+
+import java.util.LinkedHashMap;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(immediate = true, service = RoleSegmentsEntryRetriever.class)
+public class RoleSegmentsEntryRetrieverImpl
+	implements RoleSegmentsEntryRetriever {
+
+	@Override
+	public BaseModelSearchResult<SegmentsEntry> searchRoleSegmentsEntries(
+			long roleId, String keywords, int cur, int delta, String sortField,
+			boolean reverse)
+		throws PortalException {
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+
+		params.put("roleIds", new long[] {roleId});
+
+		return _segmentsEntryLocalService.searchSegmentsEntries(
+			serviceContext.getCompanyId(), serviceContext.getScopeGroupId(),
+			keywords, true, params, cur, cur + delta,
+			new Sort(sortField, reverse));
+	}
+
+	@Reference
+	private SegmentsEntryLocalService _segmentsEntryLocalService;
+
+}
