4fb2d3fa8275314e2243bb0c0fc9fb040966fbeb
Hugo Huijser <hugo.huijser@liferay.com>
2019-10-16 10:09:33 -0700

    LPS-84119 Auto SF

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/service/impl/DDLRecordLocalServiceImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/service/impl/DDLRecordLocalServiceImpl.java
@@ -1048,8 +1048,8 @@ public class DDLRecordLocalServiceImpl extends DDLRecordLocalServiceBaseImpl {
 					record.getVersion(), recordVersion.getVersion()) <= 0) {
 
 				record.setDDMStorageId(recordVersion.getDDMStorageId());
-				record.setVersion(recordVersion.getVersion());
 				record.setRecordSetId(recordVersion.getRecordSetId());
+				record.setVersion(recordVersion.getVersion());
 				record.setDisplayIndex(recordVersion.getDisplayIndex());
 				record.setVersion(recordVersion.getVersion());
 				record.setVersionUserId(recordVersion.getUserId());
