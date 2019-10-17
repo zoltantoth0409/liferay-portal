4a5bb6aa22c337a8b754f6cf07ec3c2a083bd6a0
Dennis Ju <dennis.ju@liferay.com>
2019-10-17 16:03:22 -0700

    SYNC-1816 Update sync-engine 3.4.7 dependencies. MVCC property removed; enabled by default since H2 1.4.

--- modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/service/persistence/BasePersistenceImpl.java
+++ modules/apps/sync/sync-engine/src/main/java/com/liferay/sync/engine/service/persistence/BasePersistenceImpl.java
@@ -184,7 +184,7 @@ public class BasePersistenceImpl<TT extends BaseModel, TID>
 
 		sb.append(PropsValues.SYNC_DATABASE_NAME);
 		sb.append(";AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE;");
-		sb.append("LOCK_TIMEOUT=120000;MVCC=TRUE;");
+		sb.append("LOCK_TIMEOUT=120000;");
 
 		_connectionSource = new JdbcPooledConnectionSource(sb.toString());
 
