66179eabcb4a76c39d278d17fbe033945a6f5957
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-10-17 18:12:22 +0200

    LPS-103351 Deleting rels

--- modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/model/listener/DepotEntryModelListener.java
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/model/listener/DepotEntryModelListener.java
@@ -15,6 +15,8 @@
 package com.liferay.depot.internal.model.listener;
 
 import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.model.DepotEntryGroupRel;
+import com.liferay.depot.service.DepotEntryGroupRelLocalService;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.BaseModelListener;
@@ -36,11 +38,22 @@ public class DepotEntryModelListener extends BaseModelListener<DepotEntry> {
 
 		try {
 			_groupLocalService.deleteGroup(depotEntry.getGroupId());
+
+			for (DepotEntryGroupRel depotEntryGroupRel :
+					_depotEntryGroupRelLocalService.getDepotEntryGroupRels(
+						depotEntry)) {
+
+				_depotEntryGroupRelLocalService.deleteDepotEntryGroupRel(
+					depotEntryGroupRel);
+			}
 		}
 		catch (PortalException pe) {
 			throw new ModelListenerException(pe);
 		}
 	}
+
+	@Reference
+	private DepotEntryGroupRelLocalService _depotEntryGroupRelLocalService;
 
 	@Reference
 	private GroupLocalService _groupLocalService;
