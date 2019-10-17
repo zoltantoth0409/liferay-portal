db81b6e4f170b873ccbf14638f164cb6c9438f4c
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2019-10-17 11:09:14 -0700

    LPS-102947 Move “resource permission name” logic from “SearchPermissionCheckerImpl” and “SearchPermissionDocumentContributorImpl” to “DDMSearchHelper”, “DDMStructureModelDocumentContributor” and “DDMTemplateModelDocumentContributor”
    - Document field “resourcePermissionName” introduced, solely used by DDM.
    - Document field “resourceClassNameId” discontinued, solely used by DDM.
    - SearchContext attribute “resourcePermissionName” introduced, solely used by DDM.
    - SearchContext attribute “resourceClassNameId” discontinued, solely used by DDM.
    - This field / attribute pair will transition to a Search extension point in the future.
    
    Co-authored-by: Rodrigo Paulino <rodrigo.paulino@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/search/spi/model/index/contributor/DDMStructureModelDocumentContributor.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/search/spi/model/index/contributor/DDMStructureModelDocumentContributor.java
@@ -16,6 +16,7 @@ package com.liferay.dynamic.data.mapping.internal.search.spi.model.index.contrib
 
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.model.DDMStructureVersion;
+import com.liferay.dynamic.data.mapping.security.permission.DDMPermissionSupport;
 import com.liferay.dynamic.data.mapping.service.DDMStructureVersionLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
@@ -61,6 +62,11 @@ public class DDMStructureModelDocumentContributor
 
 			document.addKeyword(Field.STATUS, structureVersion.getStatus());
 			document.addKeyword(Field.VERSION, structureVersion.getVersion());
+
+			document.addKeyword(
+				"resourcePermissionName",
+				_ddmPermissionSupport.getStructureModelResourceName(
+					ddmStructure.getClassNameId()));
 		}
 		catch (PortalException pe) {
 			if (_log.isDebugEnabled()) {
@@ -97,5 +103,8 @@ public class DDMStructureModelDocumentContributor
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DDMStructureModelDocumentContributor.class);
+
+	@Reference
+	private DDMPermissionSupport _ddmPermissionSupport;
 
 }
