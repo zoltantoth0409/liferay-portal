23b122be5accfecc9851aa516b9493a0c0b759f3
Peter Shin <peter.shin@liferay.com>
2019-10-17 15:07:17 -0700

    LPS-103170 Add properties for ReleaseInfo.java

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -106,15 +106,15 @@ public class ReleaseInfo {
 		return _VERSION;
 	}
 
-	private static final String _BUILD = "7201";
+	private static final String _BUILD = "@release.info.build@";
 
 	private static final int _BUILD_NUMBER = GetterUtil.getInteger(_BUILD);
 
 	private static final String _CODE_NAME = "Mueller";
 
-	private static final String _DATE = "November 5, 2019";
+	private static final String _DATE = "@release.info.date@";
 
-	private static final String _NAME = "Liferay Community Edition Portal";
+	private static final String _NAME = "@release.info.name@";
 
 	private static final int _PARENT_BUILD_NUMBER = _BUILD_NUMBER;
 
@@ -126,9 +126,10 @@ public class ReleaseInfo {
 
 	private static final String _VENDOR = "Liferay, Inc.";
 
-	private static final String _VERSION = "7.2.1";
+	private static final String _VERSION = "@release.info.version@";
 
-	private static final String _VERSION_DISPLAY_NAME = "7.2.1 CE GA2";
+	private static final String _VERSION_DISPLAY_NAME =
+		"@release.info.version.display.name@";
 
 	private static String _releaseInfo;
 	private static String _serverInfo;
