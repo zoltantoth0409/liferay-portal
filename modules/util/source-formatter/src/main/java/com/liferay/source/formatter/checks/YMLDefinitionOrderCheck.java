ea3bc5770588e48164d861d6beb5ece845748ea6
Alan <alan.huang@liferay.com>
2019-10-17 14:06:37 +0800

    LPS-84119 Don't sort the content of description

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLDefinitionOrderCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/YMLDefinitionOrderCheck.java
@@ -203,7 +203,9 @@ public class YMLDefinitionOrderCheck extends BaseFileCheck {
 		for (String definition : definitions) {
 			String[] lines = StringUtil.splitLines(definition);
 
-			if ((lines.length != 0) && lines[0].matches(" *.+: +.+")) {
+			if ((lines.length != 0) &&
+				lines[0].matches(" *(description:|.+: +.+)")) {
+
 				continue;
 			}
 
