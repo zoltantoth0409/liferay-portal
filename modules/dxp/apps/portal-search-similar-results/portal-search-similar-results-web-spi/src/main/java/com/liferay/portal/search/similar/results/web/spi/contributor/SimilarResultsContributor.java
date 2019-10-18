2e0d1fc8462225536eacaa13b77aeaf2728e7cd8
André de Oliveira <andre.oliveira@liferay.com>
2019-10-18 15:00:54 -0700

    LPS-98533 portal-search-similar-results-web-spi: Create module
    https://issues.liferay.com/browse/LPS-98533
    
    Co-authored-by: Kevin Tan <kevin.tan@liferay.com>
    Co-authored-by: weicao <wade.cao@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

---
+++ modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web-spi/src/main/java/com/liferay/portal/search/similar/results/web/spi/contributor/SimilarResultsContributor.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.search.similar.results.web.spi.contributor;
+
+import com.liferay.portal.search.similar.results.web.spi.contributor.helper.CriteriaBuilder;
+import com.liferay.portal.search.similar.results.web.spi.contributor.helper.CriteriaHelper;
+import com.liferay.portal.search.similar.results.web.spi.contributor.helper.DestinationBuilder;
+import com.liferay.portal.search.similar.results.web.spi.contributor.helper.DestinationHelper;
+import com.liferay.portal.search.similar.results.web.spi.contributor.helper.RouteBuilder;
+import com.liferay.portal.search.similar.results.web.spi.contributor.helper.RouteHelper;
+
+import org.osgi.annotation.versioning.ConsumerType;
+
+/**
+ * @author André de Oliveira
+ */
+@ConsumerType
+public interface SimilarResultsContributor {
+
+	public void detectRoute(RouteBuilder routeBuilder, RouteHelper routeHelper);
+
+	public void resolveCriteria(
+		CriteriaBuilder criteriaBuilder, CriteriaHelper criteriaHelper);
+
+	public void writeDestination(
+		DestinationBuilder destinationBuilder,
+		DestinationHelper destinationHelper);
+
+}
