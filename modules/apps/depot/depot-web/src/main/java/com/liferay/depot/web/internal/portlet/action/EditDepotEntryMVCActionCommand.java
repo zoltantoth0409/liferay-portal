c84fd34af9abd98246bd2330dee1bcf8bb6a0733
Alicia Garcia <alicia.garcia@liferay.com>
2019-10-18 08:52:48 +0200

    LPS-101158 add support to save available languages on repositories

--- modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/action/EditDepotEntryMVCActionCommand.java
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/action/EditDepotEntryMVCActionCommand.java
@@ -17,20 +17,35 @@ package com.liferay.depot.web.internal.portlet.action;
 import com.liferay.depot.model.DepotEntry;
 import com.liferay.depot.service.DepotEntryLocalService;
 import com.liferay.depot.web.internal.constants.DepotPortletKeys;
+import com.liferay.depot.web.internal.util.DepotEntryURLUtil;
+import com.liferay.portal.kernel.exception.GroupNameException;
+import com.liferay.portal.kernel.exception.LocaleException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.GroupService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PropertiesParamUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Locale;
 import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.RenderURL;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -57,7 +72,9 @@ public class EditDepotEntryMVCActionCommand extends BaseMVCActionCommand {
 		DepotEntry depotEntry = _depotEntryLocalService.getDepotEntry(
 			depotEntryId);
 
-		Group group = _groupService.getGroup(depotEntry.getGroupId());
+		long groupId = depotEntry.getGroupId();
+
+		Group group = _groupService.getGroup(groupId);
 
 		Map<Locale, String> nameMap = LocalizationUtil.getLocalizationMap(
 			actionRequest, "name", group.getNameMap());
@@ -68,8 +85,76 @@ public class EditDepotEntryMVCActionCommand extends BaseMVCActionCommand {
 		ServiceContext serviceContext = ServiceContextFactory.getInstance(
 			DepotEntry.class.getName(), actionRequest);
 
+		//languages
+		UnicodeProperties formTypeSettingsProperties =
+			PropertiesParamUtil.getProperties(
+				actionRequest, "TypeSettingsProperties--");
+
+		Locale defaultLocale = LocaleUtil.fromLanguageId(
+			formTypeSettingsProperties.getProperty("languageId"));
+
+		if (!group.isGuest()) {
+			_validateDefaultLocaleGroupName(nameMap, defaultLocale);
+		}
+
 		_depotEntryLocalService.updateDepotEntry(
 			depotEntryId, nameMap, descriptionMap, serviceContext);
+
+		UnicodeProperties typeSettingsProperties =
+			group.getTypeSettingsProperties();
+
+		boolean inheritLocales = GetterUtil.getBoolean(
+			typeSettingsProperties.getProperty("inheritLocales"));
+
+		if (formTypeSettingsProperties.containsKey("inheritLocales")) {
+			inheritLocales = GetterUtil.getBoolean(
+				formTypeSettingsProperties.getProperty("inheritLocales"));
+		}
+
+		if (inheritLocales) {
+			formTypeSettingsProperties.setProperty(
+				PropsKeys.LOCALES,
+				StringUtil.merge(
+					LocaleUtil.toLanguageIds(
+						LanguageUtil.getAvailableLocales())));
+		}
+
+		if (formTypeSettingsProperties.containsKey(PropsKeys.LOCALES) &&
+			Validator.isNull(
+				formTypeSettingsProperties.getProperty(PropsKeys.LOCALES))) {
+
+			throw new LocaleException(
+				LocaleException.TYPE_DEFAULT,
+				"Must have at least one valid locale for site " + groupId);
+		}
+
+		typeSettingsProperties.putAll(formTypeSettingsProperties);
+
+		try {
+			_groupService.updateGroup(
+				group.getGroupId(), typeSettingsProperties.toString());
+		}
+		catch (LocaleException le) {
+			SessionErrors.add(actionRequest, le.getClass(), le);
+			RenderURL editDepotEntryRenderURL =
+				DepotEntryURLUtil.getEditDepotEntryRenderURL(
+					depotEntryId,
+					ParamUtil.getString(actionRequest, "redirect"),
+					_portal.getLiferayPortletResponse(actionResponse));
+
+			sendRedirect(
+				actionRequest, actionResponse,
+				editDepotEntryRenderURL.toString());
+		}
+	}
+
+	private void _validateDefaultLocaleGroupName(
+			Map<Locale, String> nameMap, Locale defaultLocale)
+		throws PortalException {
+
+		if ((nameMap == null) || Validator.isNull(nameMap.get(defaultLocale))) {
+			throw new GroupNameException();
+		}
 	}
 
 	@Reference
@@ -77,5 +162,8 @@ public class EditDepotEntryMVCActionCommand extends BaseMVCActionCommand {
 
 	@Reference
 	private GroupService _groupService;
+
+	@Reference
+	private Portal _portal;
 
 }
