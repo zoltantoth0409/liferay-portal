9348b21e2bb3a43e7b58ab17e9a0273a26932662
Alicia Garcia <alicia.garcia@liferay.com>
2019-10-18 14:46:07 +0200

    LPS-101158 move to the service all the update

--- modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/action/EditDepotEntryMVCActionCommand.java
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/action/EditDepotEntryMVCActionCommand.java
@@ -18,10 +18,7 @@ import com.liferay.depot.model.DepotEntry;
 import com.liferay.depot.service.DepotEntryLocalService;
 import com.liferay.depot.web.internal.constants.DepotPortletKeys;
 import com.liferay.depot.web.internal.util.DepotEntryURLUtil;
-import com.liferay.portal.kernel.exception.GroupNameException;
 import com.liferay.portal.kernel.exception.LocaleException;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
@@ -29,16 +26,11 @@ import com.liferay.portal.kernel.service.GroupService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.servlet.SessionErrors;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PropertiesParamUtil;
-import com.liferay.portal.kernel.util.PropsKeys;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
-import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Locale;
 import java.util.Map;
@@ -72,9 +64,7 @@ public class EditDepotEntryMVCActionCommand extends BaseMVCActionCommand {
 		DepotEntry depotEntry = _depotEntryLocalService.getDepotEntry(
 			depotEntryId);
 
-		long groupId = depotEntry.getGroupId();
-
-		Group group = _groupService.getGroup(groupId);
+		Group group = _groupService.getGroup(depotEntry.getGroupId());
 
 		Map<Locale, String> nameMap = LocalizationUtil.getLocalizationMap(
 			actionRequest, "name", group.getNameMap());
@@ -89,47 +79,10 @@ public class EditDepotEntryMVCActionCommand extends BaseMVCActionCommand {
 			PropertiesParamUtil.getProperties(
 				actionRequest, "TypeSettingsProperties--");
 
-		Locale defaultLocale = LocaleUtil.fromLanguageId(
-			formTypeSettingsProperties.getProperty("languageId"));
-
-		_validateDefaultLocaleGroupName(nameMap, defaultLocale);
-
-		_depotEntryLocalService.updateDepotEntry(
-			depotEntryId, nameMap, descriptionMap, serviceContext);
-
-		UnicodeProperties typeSettingsProperties =
-			group.getTypeSettingsProperties();
-
-		boolean inheritLocales = GetterUtil.getBoolean(
-			typeSettingsProperties.getProperty("inheritLocales"));
-
-		if (formTypeSettingsProperties.containsKey("inheritLocales")) {
-			inheritLocales = GetterUtil.getBoolean(
-				formTypeSettingsProperties.getProperty("inheritLocales"));
-		}
-
-		if (inheritLocales) {
-			formTypeSettingsProperties.setProperty(
-				PropsKeys.LOCALES,
-				StringUtil.merge(
-					LocaleUtil.toLanguageIds(
-						LanguageUtil.getAvailableLocales())));
-		}
-
-		if (formTypeSettingsProperties.containsKey(PropsKeys.LOCALES) &&
-			Validator.isNull(
-				formTypeSettingsProperties.getProperty(PropsKeys.LOCALES))) {
-
-			throw new LocaleException(
-				LocaleException.TYPE_DEFAULT,
-				"Must have at least one valid locale for site " + groupId);
-		}
-
-		typeSettingsProperties.putAll(formTypeSettingsProperties);
-
 		try {
-			_groupService.updateGroup(
-				group.getGroupId(), typeSettingsProperties.toString());
+			_depotEntryLocalService.updateDepotEntry(
+				depotEntryId, formTypeSettingsProperties, nameMap,
+				descriptionMap, serviceContext);
 		}
 		catch (LocaleException le) {
 			SessionErrors.add(actionRequest, le.getClass(), le);
@@ -143,15 +96,6 @@ public class EditDepotEntryMVCActionCommand extends BaseMVCActionCommand {
 			sendRedirect(
 				actionRequest, actionResponse,
 				editDepotEntryRenderURL.toString());
-		}
-	}
-
-	private void _validateDefaultLocaleGroupName(
-			Map<Locale, String> nameMap, Locale defaultLocale)
-		throws PortalException {
-
-		if ((nameMap == null) || Validator.isNull(nameMap.get(defaultLocale))) {
-			throw new GroupNameException();
 		}
 	}
 
