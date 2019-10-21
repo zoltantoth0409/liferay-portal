fde3c2aed38a2e604575a34c09898bf5f2dba9d4
Hugo Huijser <hugo.huijser@liferay.com>
2019-10-21 11:02:56 -0700

    LPS-103170 Consolidate logic in BaseFileCheck

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesLiferayPluginPackageLiferayVersionsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesLiferayPluginPackageLiferayVersionsCheck.java
@@ -14,12 +14,9 @@
 
 package com.liferay.source.formatter.checks;
 
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
-import com.liferay.source.formatter.util.FileUtil;
 
-import java.io.File;
 import java.io.IOException;
 
 import java.util.regex.Matcher;
@@ -64,7 +61,7 @@ public class PropertiesLiferayPluginPackageLiferayVersionsCheck
 
 		boolean privateApp = isModulesApp(absolutePath, true);
 
-		String portalVersion = _getPortalVersion(privateApp);
+		String portalVersion = getPortalVersion(privateApp);
 
 		if (Validator.isNull(portalVersion)) {
 			return content;
@@ -75,147 +72,7 @@ public class PropertiesLiferayPluginPackageLiferayVersionsCheck
 			"liferay-versions=" + portalVersion + "+", matcher.start());
 	}
 
-	private String _getPortalVersion(boolean privateApp) throws IOException {
-		String portalVersion = _getPublicPortalVersion();
-
-		if (privateApp) {
-			portalVersion = _getPrivatePortalVersion();
-		}
-
-		if (Validator.isNull(portalVersion) || privateApp) {
-			return portalVersion;
-		}
-
-		Matcher matcher = _portalVersionPattern.matcher(portalVersion);
-
-		if (matcher.find()) {
-			portalVersion = matcher.group(1) + ".0";
-		}
-
-		return portalVersion;
-	}
-
-	private synchronized String _getPrivatePortalVersion() throws IOException {
-		if (_privatePortalVersion != null) {
-			return _privatePortalVersion;
-		}
-
-		_privatePortalVersion = StringPool.BLANK;
-
-		if (!isPortalSource()) {
-			return _privatePortalVersion;
-		}
-
-		File workingDirPropertiesFile = new File(
-			getPortalDir(), "working.dir.properties");
-
-		if (!workingDirPropertiesFile.exists()) {
-			return _privatePortalVersion;
-		}
-
-		String content = FileUtil.read(workingDirPropertiesFile);
-
-		Matcher matcher = _privateBranchNamePattern.matcher(content);
-
-		if (!matcher.find()) {
-			return _privatePortalVersion;
-		}
-
-		String privateBranchName = StringUtil.trim(matcher.group(1));
-
-		if (Validator.isNull(privateBranchName)) {
-			return _privatePortalVersion;
-		}
-
-		String s = Pattern.quote("lp.version[" + privateBranchName + "]=");
-
-		Pattern pattern = Pattern.compile(s + "(.*)");
-
-		matcher = pattern.matcher(content);
-
-		if (!matcher.find()) {
-			return _privatePortalVersion;
-		}
-
-		_privatePortalVersion = StringUtil.trim(matcher.group(1));
-
-		return _privatePortalVersion;
-	}
-
-	private synchronized String _getPublicPortalVersion() throws IOException {
-		if (_publicPortalVersion != null) {
-			return _publicPortalVersion;
-		}
-
-		_publicPortalVersion = StringPool.BLANK;
-
-		if (!isPortalSource()) {
-			return _publicPortalVersion;
-		}
-
-		File workingDirPropertiesFile = new File(
-			getPortalDir(), "working.dir.properties");
-
-		if (workingDirPropertiesFile.exists()) {
-			String content = FileUtil.read(workingDirPropertiesFile);
-
-			Matcher matcher = _privateBranchNamePattern.matcher(content);
-
-			if (matcher.find()) {
-				String privateBranchName = StringUtil.trim(matcher.group(1));
-
-				if (Validator.isNotNull(privateBranchName) &&
-					privateBranchName.endsWith("-private")) {
-
-					String branchName = StringUtil.replaceLast(
-						privateBranchName, "-private", StringPool.BLANK);
-
-					String s = Pattern.quote("lp.version[" + branchName + "]=");
-
-					Pattern pattern = Pattern.compile(s + "(.*)");
-
-					matcher = pattern.matcher(content);
-
-					if (matcher.find()) {
-						String match = matcher.group(1);
-
-						_publicPortalVersion = StringUtil.trim(match);
-					}
-				}
-			}
-		}
-		else {
-			File releaseInfoJavaFile = new File(
-				getPortalDir(), _PORTAL_KERNEL_RELEASE_INFO_JAVA_FILE_NAME);
-
-			if (releaseInfoJavaFile.exists()) {
-				String content = FileUtil.read(releaseInfoJavaFile);
-
-				Matcher matcher =
-					_portalKernelReleaseInfoVersionPattern.matcher(content);
-
-				if (matcher.find()) {
-					_publicPortalVersion = StringUtil.trim(matcher.group(1));
-				}
-			}
-		}
-
-		return _publicPortalVersion;
-	}
-
-	private static final String _PORTAL_KERNEL_RELEASE_INFO_JAVA_FILE_NAME =
-		"portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java";
-
 	private static final Pattern _liferayVersionsPattern = Pattern.compile(
 		"\nliferay-versions=(.*)\n");
-	private static final Pattern _portalKernelReleaseInfoVersionPattern =
-		Pattern.compile("private static final String _VERSION = \"(.*)\";");
-	private static final Pattern _portalVersionPattern = Pattern.compile(
-		"(\\w+\\.\\w+)\\.\\w+");
-	private static final Pattern _privateBranchNamePattern = Pattern.compile(
-		"private.branch.name=(.*)\n");
-
-	private String _privatePortalVersion;
-	private String _publicPortalVersion;
 
 }
--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/XMLLookAndFeelCompatibilityVersionCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/XMLLookAndFeelCompatibilityVersionCheck.java
@@ -19,14 +19,10 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.source.formatter.checks.util.SourceUtil;
-import com.liferay.source.formatter.util.FileUtil;
 
-import java.io.File;
 import java.io.IOException;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -64,7 +60,7 @@ public class XMLLookAndFeelCompatibilityVersionCheck extends BaseFileCheck {
 
 		boolean privateApp = isModulesApp(absolutePath, true);
 
-		String portalVersion = _getPortalVersion(privateApp);
+		String portalVersion = getPortalVersion(privateApp);
 
 		if (Validator.isNull(portalVersion)) {
 			return;
@@ -91,134 +87,6 @@ public class XMLLookAndFeelCompatibilityVersionCheck extends BaseFileCheck {
 		}
 	}
 
-	private String _getPortalVersion(boolean privateApp) throws IOException {
-		String portalVersion = _getPublicPortalVersion();
-
-		if (privateApp) {
-			portalVersion = _getPrivatePortalVersion();
-		}
-
-		if (Validator.isNull(portalVersion) || privateApp) {
-			return portalVersion;
-		}
-
-		Matcher matcher = _portalVersionPattern.matcher(portalVersion);
-
-		if (matcher.find()) {
-			portalVersion = matcher.group(1) + ".0";
-		}
-
-		return portalVersion;
-	}
-
-	private synchronized String _getPrivatePortalVersion() throws IOException {
-		if (_privatePortalVersion != null) {
-			return _privatePortalVersion;
-		}
-
-		_privatePortalVersion = StringPool.BLANK;
-
-		if (!isPortalSource()) {
-			return _privatePortalVersion;
-		}
-
-		File workingDirPropertiesFile = new File(
-			getPortalDir(), "working.dir.properties");
-
-		if (!workingDirPropertiesFile.exists()) {
-			return _privatePortalVersion;
-		}
-
-		String content = FileUtil.read(workingDirPropertiesFile);
-
-		Matcher matcher = _privateBranchNamePattern.matcher(content);
-
-		if (!matcher.find()) {
-			return _privatePortalVersion;
-		}
-
-		String privateBranchName = StringUtil.trim(matcher.group(1));
-
-		if (Validator.isNull(privateBranchName)) {
-			return _privatePortalVersion;
-		}
-
-		String s = Pattern.quote("lp.version[" + privateBranchName + "]=");
-
-		Pattern pattern = Pattern.compile(s + "(.*)");
-
-		matcher = pattern.matcher(content);
-
-		if (!matcher.find()) {
-			return _privatePortalVersion;
-		}
-
-		_privatePortalVersion = StringUtil.trim(matcher.group(1));
-
-		return _privatePortalVersion;
-	}
-
-	private synchronized String _getPublicPortalVersion() throws IOException {
-		if (_publicPortalVersion != null) {
-			return _publicPortalVersion;
-		}
-
-		_publicPortalVersion = StringPool.BLANK;
-
-		if (!isPortalSource()) {
-			return _publicPortalVersion;
-		}
-
-		File workingDirPropertiesFile = new File(
-			getPortalDir(), "working.dir.properties");
-
-		if (workingDirPropertiesFile.exists()) {
-			String content = FileUtil.read(workingDirPropertiesFile);
-
-			Matcher matcher = _privateBranchNamePattern.matcher(content);
-
-			if (matcher.find()) {
-				String privateBranchName = StringUtil.trim(matcher.group(1));
-
-				if (Validator.isNotNull(privateBranchName) &&
-					privateBranchName.endsWith("-private")) {
-
-					String branchName = StringUtil.replaceLast(
-						privateBranchName, "-private", StringPool.BLANK);
-
-					String s = Pattern.quote("lp.version[" + branchName + "]=");
-
-					Pattern pattern = Pattern.compile(s + "(.*)");
-
-					matcher = pattern.matcher(content);
-
-					if (matcher.find()) {
-						String match = matcher.group(1);
-
-						_publicPortalVersion = StringUtil.trim(match);
-					}
-				}
-			}
-		}
-		else {
-			File releaseInfoJavaFile = new File(
-				getPortalDir(), _PORTAL_KERNEL_RELEASE_INFO_JAVA_FILE_NAME);
-
-			if (releaseInfoJavaFile.exists()) {
-				String content = FileUtil.read(releaseInfoJavaFile);
-
-				Matcher matcher =
-					_portalKernelReleaseInfoVersionPattern.matcher(content);
-
-				if (matcher.find()) {
-					_publicPortalVersion = StringUtil.trim(matcher.group(1));
-				}
-			}
-		}
-
-		return _publicPortalVersion;
-	}
-
 	private boolean _hasPortalVersions(
 		Element compatibilityElement, String portalVersion) {
 
@@ -235,18 +103,5 @@ public class XMLLookAndFeelCompatibilityVersionCheck extends BaseFileCheck {
 
 		return false;
 	}
-
-	private static final String _PORTAL_KERNEL_RELEASE_INFO_JAVA_FILE_NAME =
-		"portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java";
-
-	private static final Pattern _portalKernelReleaseInfoVersionPattern =
-		Pattern.compile("private static final String _VERSION = \"(.*)\";");
-	private static final Pattern _portalVersionPattern = Pattern.compile(
-		"(\\w+\\.\\w+)\\.\\w+");
-	private static final Pattern _privateBranchNamePattern = Pattern.compile(
-		"private.branch.name=(.*)\n");
-
-	private String _privatePortalVersion;
-	private String _publicPortalVersion;
 
 }
