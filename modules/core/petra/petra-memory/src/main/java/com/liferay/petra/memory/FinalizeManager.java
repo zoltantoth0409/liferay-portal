b6cf1d67dc3e4943077780d0a51449bcfb951110
Leon Chi <leon.chi@liferay.com>
2019-10-21 14:43:43 +0800

    LPS-103549 Remove deprecated field from FinalizeManager

--- modules/core/petra/petra-memory/src/main/java/com/liferay/petra/memory/FinalizeManager.java
+++ modules/core/petra/petra-memory/src/main/java/com/liferay/petra/memory/FinalizeManager.java
@@ -68,12 +68,6 @@ public class FinalizeManager {
 
 		};
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	public static final boolean THREAD_ENABLED = true;
-
 	public static final ReferenceFactory WEAK_REFERENCE_FACTORY =
 		new ReferenceFactory() {
 
