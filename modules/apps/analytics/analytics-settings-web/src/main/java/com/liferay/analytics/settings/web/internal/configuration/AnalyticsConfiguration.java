b32be9123276c2bb1665d70bbb703e53fabb9be8
Marcellus Tavares <marcellus.tavares@liferay.com>
2019-10-21 13:05:32 -0700

    LRAC-4223 Add AnalyticsConfiguration and Pid mapping

---
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/configuration/AnalyticsConfiguration.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Marcellus Tavares
+ */
+@ExtendedObjectClassDefinition(
+	category = "analytics-cloud", generateUI = false,
+	scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.analytics.settings.web.internal.configuration.AnalyticsConfiguration",
+	localization = "content/Language", name = "analytics-configuration-name"
+)
+public interface AnalyticsConfiguration {
+
+	@Meta.AD(name = "token", required = false)
+	public String token();
+
+}
