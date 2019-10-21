6ecda78a74b0fd3bd0ff26354812fb0764768311
Marcellus Tavares <marcellus.tavares@liferay.com>
2019-10-21 13:07:14 -0700

    LRAC-4223 Implement EditWorkspaceConnectionMVCActionCommand

---
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/portlet/action/EditWorkspaceConnectionMVCActionCommand.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.web.internal.portlet.action;
+
+import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.ModifiableSettings;
+import com.liferay.portal.kernel.settings.Settings;
+import com.liferay.portal.kernel.settings.SettingsDescriptor;
+import com.liferay.portal.kernel.settings.SettingsException;
+import com.liferay.portal.kernel.settings.SettingsFactoryUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.MutableRenderParameters;
+import javax.portlet.ValidatorException;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + ConfigurationAdminPortletKeys.INSTANCE_SETTINGS,
+		"mvc.command.name=/analytics/edit_workspace_connection"
+	},
+	service = MVCActionCommand.class
+)
+public class EditWorkspaceConnectionMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	protected void checkPermissions(ThemeDisplay themeDisplay)
+		throws PrincipalException {
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (!permissionChecker.isCompanyAdmin(themeDisplay.getCompanyId())) {
+			throw new PrincipalException();
+		}
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+			checkPermissions(themeDisplay);
+
+			storeSettings(actionRequest, themeDisplay);
+		}
+		catch (PrincipalException pe) {
+			SessionErrors.add(actionRequest, pe.getClass());
+
+			MutableRenderParameters mutableRenderParameters =
+				actionResponse.getRenderParameters();
+
+			mutableRenderParameters.setValue("mvcPath", "/error.jsp");
+		}
+	}
+
+	protected void storeSettings(
+			ActionRequest actionRequest, ThemeDisplay themeDisplay)
+		throws IOException, SettingsException, ValidatorException {
+
+		Settings settings = SettingsFactoryUtil.getSettings(
+			new CompanyServiceSettingsLocator(
+				themeDisplay.getCompanyId(), "com.liferay.analytics"));
+
+		ModifiableSettings modifiableSettings =
+			settings.getModifiableSettings();
+
+		SettingsDescriptor settingsDescriptor =
+			SettingsFactoryUtil.getSettingsDescriptor("com.liferay.analytics");
+
+		for (String name : settingsDescriptor.getAllKeys()) {
+			String value = ParamUtil.getString(actionRequest, name);
+
+			String oldValue = settings.getValue(name, null);
+
+			if (!value.equals(oldValue)) {
+				modifiableSettings.setValue(name, value);
+			}
+		}
+
+		modifiableSettings.store();
+	}
+
+}
