9710d1d2d44181ea834784defa11fe1d4a34595b
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-10-21 13:59:54 +0200

    LPS-103205 New API MentionsStrategy

---
+++ modules/apps/mentions/mentions-api/src/main/java/com/liferay/mentions/strategy/MentionsStrategy.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mentions.strategy;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.User;
+
+import java.util.List;
+
+/**
+ * @author Cristina González
+ */
+public interface MentionsStrategy {
+
+	public List<User> getUsers(
+			long companyId, long user, String query, JSONObject jsonObject)
+		throws PortalException;
+
+}
