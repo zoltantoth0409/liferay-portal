5f6981fe8d818e5348d4a8688210fc17022986b3
shuyangzhou <shuyang.zhou@liferay.com>
2019-10-22 15:28:05 -0700

    LPS-103578 Apply usage

--- modules/apps/portal-store/portal-store-db/src/main/java/com/liferay/portal/store/db/DBStore.java
+++ modules/apps/portal-store/portal-store-db/src/main/java/com/liferay/portal/store/db/DBStore.java
@@ -16,40 +16,17 @@ package com.liferay.portal.store.db;
 
 import com.liferay.document.library.content.exception.NoSuchContentException;
 import com.liferay.document.library.content.model.DLContent;
-import com.liferay.document.library.content.model.DLContentDataBlobModel;
 import com.liferay.document.library.content.service.DLContentLocalService;
 import com.liferay.document.library.kernel.exception.NoSuchFileException;
 import com.liferay.document.library.kernel.store.Store;
 import com.liferay.document.library.kernel.util.DLUtil;
-import com.liferay.petra.io.AutoDeleteFileInputStream;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.dao.db.DB;
-import com.liferay.portal.kernel.dao.db.DBManagerUtil;
-import com.liferay.portal.kernel.dao.db.DBType;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.transaction.Propagation;
-import com.liferay.portal.kernel.transaction.TransactionConfig;
-import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
-import com.liferay.portal.kernel.util.FileUtil;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-
-import java.nio.channels.FileChannel;
-
-import java.sql.Blob;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Callable;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -69,60 +46,8 @@ public class DBStore implements Store {
 		long companyId, long repositoryId, String fileName, String versionLabel,
 		InputStream inputStream) {
 
-		long length = -1;
-
-		if (inputStream instanceof ByteArrayInputStream) {
-			ByteArrayInputStream byteArrayInputStream =
-				(ByteArrayInputStream)inputStream;
-
-			length = byteArrayInputStream.available();
-		}
-		else if (inputStream instanceof FileInputStream) {
-			FileInputStream fileInputStream = (FileInputStream)inputStream;
-
-			FileChannel fileChannel = fileInputStream.getChannel();
-
-			try {
-				length = fileChannel.size();
-			}
-			catch (IOException ioe) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Unable to detect file size from file channel", ioe);
-				}
-			}
-		}
-		else if (inputStream instanceof UnsyncByteArrayInputStream) {
-			UnsyncByteArrayInputStream unsyncByteArrayInputStream =
-				(UnsyncByteArrayInputStream)inputStream;
-
-			length = unsyncByteArrayInputStream.available();
-		}
-
-		if (length >= 0) {
-			_dlContentLocalService.addContent(
-				companyId, repositoryId, fileName, versionLabel, inputStream,
-				length);
-		}
-		else {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Unable to detect length from input stream. Reading " +
-						"entire input stream into memory as a last resort.");
-			}
-
-			byte[] bytes = null;
-
-			try {
-				bytes = FileUtil.getBytes(inputStream);
-			}
-			catch (IOException ioe) {
-				throw new SystemException(ioe);
-			}
-
-			_dlContentLocalService.addContent(
-				companyId, repositoryId, fileName, versionLabel, bytes);
-		}
+		_dlContentLocalService.addContent(
+			companyId, repositoryId, fileName, versionLabel, inputStream);
 	}
 
 	@Override
@@ -138,13 +63,8 @@ public class DBStore implements Store {
 		long companyId, long repositoryId, String fileName,
 		String versionLabel) {
 
-		try {
-			_dlContentLocalService.deleteContent(
-				companyId, repositoryId, fileName, versionLabel);
-		}
-		catch (PortalException pe) {
-			throw new SystemException(pe);
-		}
+		_dlContentLocalService.deleteContent(
+			companyId, repositoryId, fileName, versionLabel);
 	}
 
 	@Override
@@ -157,13 +77,12 @@ public class DBStore implements Store {
 			DLContent dlContent = _dlContentLocalService.getContent(
 				companyId, repositoryId, fileName, versionLabel);
 
-			return TransactionInvokerUtil.invoke(
-				_transactionConfig,
-				new GetBlobDataCallable(dlContent.getContentId()));
+			return _dlContentLocalService.openContentInputStream(
+				dlContent.getContentId());
 		}
-		catch (Throwable t) {
+		catch (NoSuchContentException nsce) {
 			throw new NoSuchFileException(
-				companyId, repositoryId, fileName, versionLabel, t);
+				companyId, repositoryId, fileName, versionLabel, nsce);
 		}
 	}
 
@@ -171,16 +90,9 @@ public class DBStore implements Store {
 	public String[] getFileNames(
 		long companyId, long repositoryId, String dirName) {
 
-		List<DLContent> dlContents = null;
-
-		if (dirName.isEmpty()) {
-			dlContents = _dlContentLocalService.getContents(
-				companyId, repositoryId);
-		}
-		else {
-			dlContents = _dlContentLocalService.getContentsByDirectory(
+		List<DLContent> dlContents =
+			_dlContentLocalService.getContentsByDirectory(
 				companyId, repositoryId, dirName);
-		}
 
 		String[] fileNames = new String[dlContents.size()];
 
@@ -246,58 +158,7 @@ public class DBStore implements Store {
 			companyId, repositoryId, fileName, versionLabel);
 	}
 
-	@Activate
-	protected void activate() {
-		DB db = DBManagerUtil.getDB();
-
-		if ((db.getDBType() == DBType.DB2) ||
-			(db.getDBType() == DBType.MYSQL) ||
-			(db.getDBType() == DBType.MARIADB) ||
-			(db.getDBType() == DBType.SYBASE)) {
-
-			_transactionConfig = TransactionConfig.Factory.create(
-				Propagation.SUPPORTS,
-				new Class<?>[] {PortalException.class, SystemException.class});
-		}
-		else {
-			_transactionConfig = TransactionConfig.Factory.create(
-				Propagation.REQUIRED,
-				new Class<?>[] {PortalException.class, SystemException.class});
-		}
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(DBStore.class);
-
 	@Reference
 	private DLContentLocalService _dlContentLocalService;
-
-	private TransactionConfig _transactionConfig;
-
-	private class GetBlobDataCallable implements Callable<InputStream> {
-
-		@Override
-		public InputStream call() throws Exception {
-			DLContentDataBlobModel dlContentDataBlobModel =
-				_dlContentLocalService.getDataBlobModel(_contentId);
-
-			Blob blob = dlContentDataBlobModel.getDataBlob();
-
-			InputStream inputStream = blob.getBinaryStream();
-
-			if (_transactionConfig.getPropagation() == Propagation.REQUIRED) {
-				inputStream = new AutoDeleteFileInputStream(
-					FileUtil.createTempFile(inputStream));
-			}
-
-			return inputStream;
-		}
-
-		private GetBlobDataCallable(long contentId) {
-			_contentId = contentId;
-		}
-
-		private final long _contentId;
-
-	}
 
 }
