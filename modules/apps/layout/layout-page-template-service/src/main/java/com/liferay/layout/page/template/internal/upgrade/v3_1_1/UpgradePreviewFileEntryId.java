7d490a04a1cd4ccbdc6325ba0a4684ff7a0f4aa0
Rubén Pulido <ruben.pulido@liferay.com>
2019-10-22 20:50:27 +0200

    LPS-103465 Insert rows into repository table

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/v3_1_1/UpgradePreviewFileEntryId.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/v3_1_1/UpgradePreviewFileEntryId.java
@@ -14,8 +14,19 @@
 
 package com.liferay.layout.page.template.internal.upgrade.v3_1_1;
 
+import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
+import com.liferay.layout.page.template.admin.constants.LayoutPageTemplateAdminPortletKeys;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
 
 /**
  * @author Rubén Pulido
@@ -27,6 +38,77 @@ public class UpgradePreviewFileEntryId extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() {
+		_insertRepository();
+	}
+
+	private void _insertRepository() {
+		StringBundler sb1 = new StringBundler(6);
+
+		sb1.append("select mvccVersion, groupId, companyId, userId,  ");
+		sb1.append("userName, classNameId, name, description, typeSettings, ");
+		sb1.append("dlFolderId, lastPublishDate from Repository where ");
+		sb1.append("portletId = '");
+		sb1.append(LayoutAdminPortletKeys.GROUP_PAGES);
+		sb1.append("'");
+
+		StringBundler sb2 = new StringBundler(6);
+
+		sb2.append("insert into Repository (mvccVersion, uuid_, ");
+		sb2.append("repositoryId, groupId, companyId, userId, userName, ");
+		sb2.append("createDate, modifiedDate, classNameId, name,  ");
+		sb2.append("description, portletId, typeSettings, dlFolderId,  ");
+		sb2.append("lastPublishDate) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,  ");
+		sb2.append("?, ?, ?, ?, ?, ?)");
+
+		try (Statement s = connection.createStatement();
+			ResultSet rs = s.executeQuery(sb1.toString());
+			PreparedStatement ps = AutoBatchPreparedStatementUtil.autoBatch(
+				connection.prepareStatement(sb2.toString()))) {
+
+			while (rs.next()) {
+				long mvccVersion = rs.getLong("mvccVersion");
+				long groupId = rs.getLong("groupId");
+				long companyId = rs.getLong("companyId");
+				long userId = rs.getLong("userId");
+				String userName = rs.getString("userName");
+				long classNameId = rs.getLong("classNameId");
+				String name = rs.getString("name");
+				String description = rs.getString("description");
+				String typeSettings = rs.getString("typeSettings");
+				long dlFolderId = rs.getLong("dlFolderId");
+				Timestamp lastPublishDate = rs.getTimestamp("lastPublishDate");
+
+				Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+				ps.setLong(1, mvccVersion);
+				ps.setString(2, PortalUUIDUtil.generate());
+				ps.setLong(3, increment());
+				ps.setLong(4, groupId);
+				ps.setLong(5, companyId);
+				ps.setLong(6, userId);
+				ps.setString(7, userName);
+				ps.setTimestamp(8, timestamp);
+				ps.setTimestamp(9, timestamp);
+				ps.setLong(10, classNameId);
+				ps.setString(11, name);
+				ps.setString(12, description);
+				ps.setString(
+					13,
+					LayoutPageTemplateAdminPortletKeys.LAYOUT_PAGE_TEMPLATES);
+				ps.setString(14, typeSettings);
+				ps.setLong(15, dlFolderId);
+				ps.setTimestamp(16, lastPublishDate);
+
+				ps.addBatch();
+			}
+
+			ps.executeBatch();
+		}
+		catch (Exception e) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(e, e);
+			}
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
