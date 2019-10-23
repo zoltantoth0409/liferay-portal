e59cfb81e2c4ed3b94100ab18d4c94b3798fd586
Hugo Huijser <hugo.huijser@liferay.com>
2019-10-23 15:02:38 -0700

    LPS-103252 Add line breaks

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/model/table/Field.java
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/model/table/Field.java
@@ -80,6 +80,7 @@ public class Field {
 		).put(
 			"contentRendererMap", _contentRendererMap
 		).build();
+
 		map.putAll(_customProperties);
 		map.put("fieldName", _fieldName);
 		map.put("fieldsMap", _fieldsMap);
--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/CopyLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/CopyLayoutMVCActionCommand.java
@@ -81,6 +81,7 @@ public class CopyLayoutMVCActionCommand extends BaseMVCActionCommand {
 		long groupId = ParamUtil.getLong(actionRequest, "groupId");
 		boolean privateLayout = ParamUtil.getBoolean(
 			actionRequest, "privateLayout");
+
 		String name = ParamUtil.getString(actionRequest, "name");
 
 		Map<Locale, String> nameMap = HashMapBuilder.put(
