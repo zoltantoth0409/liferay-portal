779ee468a03b8c19c74ba01c399501dda9694cdc
drewbrokke <drew.brokke@liferay.com>
2019-10-23 15:37:41 -0500

    LPS-103595 account-service - (AccountEntryImpl.java) adds methods to get the AccountEntry group and groupId

--- modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryImpl.java
@@ -14,6 +14,12 @@
 
 package com.liferay.account.model.impl;
 
+import com.liferay.account.model.AccountEntry;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+
 /**
  * @author Brian Wing Shun Chan
  */
@@ -21,5 +27,27 @@ public class AccountEntryImpl extends AccountEntryBaseImpl {
 
 	public AccountEntryImpl() {
 	}
+
+	public Group getAccountEntryGroup() {
+		return _getAccountEntryGroup();
+	}
+
+	public long getAccountEntryGroupId() {
+		Group group = _getAccountEntryGroup();
+
+		if (group == null) {
+			return GroupConstants.DEFAULT_LIVE_GROUP_ID;
+		}
+
+		return group.getGroupId();
+	}
+
+	private Group _getAccountEntryGroup() {
+		return GroupLocalServiceUtil.fetchGroup(
+			getCompanyId(), _accountEntryClassNameId, getAccountEntryId());
+	}
+
+	private long _accountEntryClassNameId =
+		ClassNameLocalServiceUtil.getClassNameId(AccountEntry.class);
 
 }
