1825fd04436188619f118038e82fbf01b2f65e2d
Igor Beslic <igor.beslic@liferay.com>
2019-10-23 13:45:01 +0200

    COMMERCE-2307 talend - component - common - Add DaikonUtil to handle repeatable daikon component related operations

---
+++ modules/etl/talend/talend-common/src/main/java/com/liferay/talend/common/daikon/DaikonUtil.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talend.common.daikon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.talend.daikon.NamedThing;
+import org.talend.daikon.SimpleNamedThing;
+
+/**
+ * @author Igor Beslic
+ */
+public class DaikonUtil {
+
+	public static List<NamedThing> toNamedThings(Set<String> names) {
+		List<NamedThing> namedThings = new ArrayList<>();
+
+		names.forEach(
+			name -> namedThings.add(new SimpleNamedThing(name, name)));
+
+		return namedThings;
+	}
+
+}
