7ffb48b6d889f53b714abde2fb2e5a14d9d5863b
pei-jung <pei-jung.lan@liferay.com>
2019-10-23 10:19:16 -0700

    LPS-63259 Escape attributes used in email message

--- modules/apps/collaboration/flags/flags-service/src/main/java/com/liferay/flags/internal/messaging/FlagsRequestMessageListener.java
+++ modules/apps/collaboration/flags/flags-service/src/main/java/com/liferay/flags/internal/messaging/FlagsRequestMessageListener.java
@@ -240,16 +240,13 @@ public class FlagsRequestMessageListener extends BaseMessageListener {
 		subscriptionSender.setBody(body);
 		subscriptionSender.setCompanyId(company.getCompanyId());
 		subscriptionSender.setContextAttributes(
-			"[$CONTENT_ID$]", contentId, "[$CONTENT_TYPE$]", contentType,
+			"[$CONTENT_ID$]", contentId, "[$CONTENT_TITLE$]", contentTitle,
+			"[$CONTENT_TYPE$]", contentType, "[$CONTENT_URL$]", contentURL,
 			"[$DATE$]", now.toString(), "[$REASON$]", reason,
 			"[$REPORTED_USER_ADDRESS$]", reportedEmailAddress,
 			"[$REPORTED_USER_NAME$]", reportedUserName, "[$REPORTED_USER_URL$]",
 			reportedUserURL, "[$REPORTER_USER_ADDRESS$]", reporterEmailAddress,
 			"[$REPORTER_USER_NAME$]", reporterUserName);
-		subscriptionSender.setContextAttribute(
-			"[$CONTENT_TITLE$]", contentTitle, false);
-		subscriptionSender.setContextAttribute(
-			"[$CONTENT_URL$]", contentURL, false);
 		subscriptionSender.setCreatorUserId(reporterUserId);
 		subscriptionSender.setFrom(fromAddress, fromName);
 		subscriptionSender.setHtmlFormat(true);
