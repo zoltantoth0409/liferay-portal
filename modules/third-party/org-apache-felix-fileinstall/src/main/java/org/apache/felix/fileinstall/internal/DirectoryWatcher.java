6d097ad0d19b9f4014f63b0fd4450d7978ea52d5
Matthew Tambara <matthew.tambara@liferay.com>
2019-10-25 13:43:49 -0700

    LPS-103581 Apply patch

--- modules/third-party/org-apache-felix-fileinstall/src/main/java/org/apache/felix/fileinstall/internal/DirectoryWatcher.java
+++ modules/third-party/org-apache-felix-fileinstall/src/main/java/org/apache/felix/fileinstall/internal/DirectoryWatcher.java
@@ -809,7 +809,7 @@ public class DirectoryWatcher extends Thread implements BundleListener
     private void initializeCurrentManagedBundles()
     {
         Bundle[] bundles = this.context.getBundles();
-        String watchedDirPath = watchedDirectory.toURI().normalize().getPath();
+        String watchedDirPath = watchedDirectory.toURI().normalize().toString();
         Map<File, Long> checksums = new HashMap<File, Long>();
         Pattern filePattern = filter == null || filter.isEmpty() ? null : Pattern.compile(filter);
         for (Bundle bundle : bundles) {
@@ -997,7 +997,9 @@ public class DirectoryWatcher extends Thread implements BundleListener
     {
 		Bundle bundle = context.getBundle(bundleLocation);
 
-		if (bundle != null) {
+		if ((bundle != null) &&
+			(Util.loadChecksum(bundle, context) != checksum)) {
+
 			bundle.update(is);
 
 			Util.storeChecksum(bundle, checksum, context);
