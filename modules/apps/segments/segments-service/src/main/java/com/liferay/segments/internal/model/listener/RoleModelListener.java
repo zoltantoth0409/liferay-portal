1ce8224c5392294cdbc46107b62627e36b7d75f4
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-10-28 19:19:40 +0100

    LPS-103409 Remove when role is removed

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/model/listener/RoleModelListener.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.model.listener;
+
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.segments.service.SegmentsEntryRoleLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eduardo García
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class RoleModelListener extends BaseModelListener<Role> {
+
+	@Override
+	public void onBeforeRemove(Role role) throws ModelListenerException {
+		try {
+			_segmentsEntryRoleLocalService.deleteSegmentsEntryRolesByRoleId(
+				role.getRoleId());
+		}
+		catch (Exception e) {
+			throw new ModelListenerException(e);
+		}
+	}
+
+	@Reference
+	private SegmentsEntryRoleLocalService _segmentsEntryRoleLocalService;
+
+}
