ca75228ced888983e7c29834ebbec141d8a3120c
Sarai Díaz García <dgarciasarai@gmail.com>
2019-10-29 17:32:13 +0100

    LPS-102889 Add tests for extracted logic

---
+++ modules/dxp/apps/segments/segments-asah-connector/src/test/java/com/liferay/segments/asah/connector/internal/client/model/util/DXPVariantSettingsUtilTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.segments.asah.connector.internal.client.model.util;
+
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.segments.asah.connector.internal.client.model.DXPVariantSettings;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author Sarai Díaz
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DXPVariantSettingsUtilTest {
+
+	@Test
+	public void testToDXPVariantSettingsWithControlVariant() {
+		String controlSegmentsExperienceKey = RandomTestUtil.randomString();
+
+		double split = RandomTestUtil.randomDouble();
+
+		DXPVariantSettings dxpVariantSettings =
+			DXPVariantSettingsUtil.toDXPVariantSettings(
+				controlSegmentsExperienceKey, controlSegmentsExperienceKey,
+				split);
+
+		Assert.assertEquals(
+			controlSegmentsExperienceKey, dxpVariantSettings.getDXPVariantId());
+		Assert.assertEquals(
+			split, dxpVariantSettings.getTrafficSplit() / 100, 0);
+		Assert.assertTrue(dxpVariantSettings.isControl());
+	}
+
+	@Test
+	public void testToDXPVariantSettingsWithNoncontrolVariant() {
+		String controlSegmentsExperienceKey = RandomTestUtil.randomString();
+
+		String segmentsExperienceKey = RandomTestUtil.randomString();
+
+		double split = RandomTestUtil.randomDouble();
+
+		DXPVariantSettings dxpVariantSettings =
+			DXPVariantSettingsUtil.toDXPVariantSettings(
+				controlSegmentsExperienceKey, segmentsExperienceKey, split);
+
+		Assert.assertEquals(
+			segmentsExperienceKey, dxpVariantSettings.getDXPVariantId());
+		Assert.assertEquals(
+			split, dxpVariantSettings.getTrafficSplit() / 100, 0);
+		Assert.assertFalse(dxpVariantSettings.isControl());
+	}
+
+}
---
+++ modules/dxp/apps/segments/segments-asah-connector/src/test/java/com/liferay/segments/asah/connector/internal/client/model/util/ExperimentSettingsUtilTest.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.segments.asah.connector.internal.client.model.util;
+
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.segments.asah.connector.internal.client.model.DXPVariantSettings;
+import com.liferay.segments.asah.connector.internal.client.model.ExperimentSettings;
+import com.liferay.segments.model.SegmentsExperiment;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author Sarai Díaz
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ExperimentSettingsUtilTest {
+
+	@Test
+	public void testToExperimentSettings() {
+		double confidenceLevel = RandomTestUtil.randomDouble();
+
+		Map<String, Double> segmentsExperienceKeySplitMap = new HashMap<>();
+
+		segmentsExperienceKeySplitMap.put(
+			RandomTestUtil.randomString(), RandomTestUtil.randomDouble());
+
+		SegmentsExperiment segmentsExperiment = Mockito.mock(
+			SegmentsExperiment.class);
+
+		Mockito.doReturn(
+			RandomTestUtil.randomString()
+		).when(
+			segmentsExperiment
+		).getSegmentsExperimentKey();
+
+		ExperimentSettings experimentSettings =
+			ExperimentSettingsUtil.toExperimentSettings(
+				confidenceLevel, segmentsExperienceKeySplitMap,
+				segmentsExperiment);
+
+		Assert.assertEquals(
+			confidenceLevel, experimentSettings.getConfidenceLevel(), 0);
+
+		List<DXPVariantSettings> dxpVariantsSettings =
+			experimentSettings.getDXPVariantsSettings();
+
+		Assert.assertEquals(
+			dxpVariantsSettings.toString(), 1, dxpVariantsSettings.size());
+
+		Map<String, DXPVariantSettings> dxpVariantsSettingsMap =
+			experimentSettings.getDXPVariantsSettingsMap();
+
+		Assert.assertEquals(
+			dxpVariantsSettingsMap.toString(), 1,
+			dxpVariantsSettingsMap.size());
+	}
+
+}
