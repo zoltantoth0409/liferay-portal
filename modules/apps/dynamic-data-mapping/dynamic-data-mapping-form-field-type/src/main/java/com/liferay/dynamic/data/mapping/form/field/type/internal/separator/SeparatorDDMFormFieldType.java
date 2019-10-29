e6a484185c1c3b4f24b5d3943803bbac3a5577f1
marcelabc <mbc3@cin.ufpe.br>
2019-10-29 10:57:25 -0300

    LPS-103629 Creation of a new field: Separator

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/separator/SeparatorDDMFormFieldType.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.separator;
+
+import com.liferay.dynamic.data.mapping.form.field.type.BaseDDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcela Cunha
+ */
+@Component(
+	immediate = true,
+	property = {
+		"ddm.form.field.type.data.domain=separator",
+		"ddm.form.field.type.description=separator-field-type-description",
+		"ddm.form.field.type.display.order:Integer=9",
+		"ddm.form.field.type.group=basic", "ddm.form.field.type.icon=separator",
+		"ddm.form.field.type.label=separator-field-type-label",
+		"ddm.form.field.type.name=separator"
+	},
+	service = DDMFormFieldType.class
+)
+public class SeparatorDDMFormFieldType extends BaseDDMFormFieldType {
+
+	@Override
+	public Class<? extends DDMFormFieldTypeSettings>
+		getDDMFormFieldTypeSettings() {
+
+		return SeparatorDDMFormFieldTypeSettings.class;
+	}
+
+	@Override
+	public String getModuleName() {
+		return "dynamic-data-mapping-form-field-type/Separator/Separator.es";
+	}
+
+	@Override
+	public String getName() {
+		return "separator";
+	}
+
+}
