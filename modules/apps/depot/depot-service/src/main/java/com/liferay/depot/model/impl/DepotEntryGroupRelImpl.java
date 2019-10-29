a7bb9a9d6f9754134d4c30350e52cdd6e5eec8fa
Brian Chan <brian.chan@liferay.com>
2019-10-28 20:50:26 -0700

    LPS-103623 SF

--- modules/apps/depot/depot-service/src/main/java/com/liferay/depot/model/impl/DepotEntryGroupRelImpl.java
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/model/impl/DepotEntryGroupRelImpl.java
@@ -15,21 +15,10 @@
 package com.liferay.depot.model.impl;
 
 /**
- * The extended model implementation for the DepotEntryGroupRel service. Represents a row in the &quot;DepotEntryGroupRel&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.depot.model.DepotEntryGroupRel</code> interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 public class DepotEntryGroupRelImpl extends DepotEntryGroupRelBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a depot entry group rel model instance should use the {@link com.liferay.depot.model.DepotEntryGroupRel} interface instead.
-	 */
 	public DepotEntryGroupRelImpl() {
 	}
 
