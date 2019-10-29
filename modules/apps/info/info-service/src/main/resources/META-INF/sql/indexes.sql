88749a2985465ab3cb1e19b6da897fd9f53013a9
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-10-29 23:09:59 +0100

    LPS-103402 Revert info service since we are going to use LayoutClassedModelUsage that makes more sense

--- modules/apps/info/info-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,7 +0,0 @@
-create unique index IX_A5521169 on InfoItemUsage (classNameId, classPK, containerKey[$COLUMN_LENGTH:75$], containerType, plid);
-create unique index IX_4A70EE8D on InfoItemUsage (classNameId, classPK, containerType, containerKey[$COLUMN_LENGTH:75$], plid);
-create index IX_550A3EBA on InfoItemUsage (classNameId, classPK, type_);
-create index IX_DCFFEB5E on InfoItemUsage (containerKey[$COLUMN_LENGTH:75$], containerType, plid);
-create index IX_821EC882 on InfoItemUsage (containerType, containerKey[$COLUMN_LENGTH:75$], plid);
-create index IX_B7FAB1 on InfoItemUsage (plid);
-create unique index IX_B80DFA5C on InfoItemUsage (uuid_[$COLUMN_LENGTH:75$], groupId);
