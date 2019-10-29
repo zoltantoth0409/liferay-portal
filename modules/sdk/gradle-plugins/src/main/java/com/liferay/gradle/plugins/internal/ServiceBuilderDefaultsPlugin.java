50c24bd1026e96a4149bf1b320485604599bf3ef
Alan <alan.huang@liferay.com>
2019-10-29 17:32:10 +0800

    LPS-103547 Move logic to ServiceBuilderDefaultsPlugin

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/ServiceBuilderDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/ServiceBuilderDefaultsPlugin.java
@@ -24,6 +24,7 @@ import com.liferay.gradle.plugins.internal.util.GradleUtil;
 import com.liferay.gradle.plugins.service.builder.BuildServiceTask;
 import com.liferay.gradle.plugins.service.builder.ServiceBuilderPlugin;
 import com.liferay.gradle.plugins.tasks.BuildDBTask;
+import com.liferay.gradle.util.FileUtil;
 
 import groovy.lang.Closure;
 
@@ -92,6 +93,44 @@ public class ServiceBuilderDefaultsPlugin
 				@Override
 				public void execute(BndBuilderPlugin bndBuilderPlugin) {
 					_configureTasksBuildServiceForBndBuilderPlugin(project);
+				}
+
+			});
+
+		project.afterEvaluate(
+			new Action<Project>() {
+
+				@Override
+				public void execute(Project project) {
+					File apiDir = buildServiceTask.getApiDir();
+
+					if (apiDir == null) {
+						return;
+					}
+
+					File apiProjectDir = GradleUtil.getRootDir(
+						project.file(apiDir), "bnd.bnd");
+
+					if (apiProjectDir != null) {
+						Project rootProject = project.getRootProject();
+
+						String relativePath = FileUtil.relativize(
+							apiProjectDir, rootProject.getProjectDir());
+
+						relativePath = relativePath.replace(
+							File.separatorChar, '/');
+
+						String apiProjectPath =
+							':' + relativePath.replace('/', ':');
+
+						Project apiProject = rootProject.findProject(
+							apiProjectPath);
+
+						if (apiProject != null) {
+							buildServiceTask.finalizedBy(
+								apiProjectPath + ":baseline");
+						}
+					}
 				}
 
 			});
