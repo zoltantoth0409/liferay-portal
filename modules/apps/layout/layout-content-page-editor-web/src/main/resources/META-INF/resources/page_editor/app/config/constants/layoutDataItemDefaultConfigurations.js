9fc0f4d59dbcb247c9fcca3489a4b56b00f08100
Pablo Molina <contact@pablomolina.me>
2019-10-30 15:05:22 +0100

    LPS-103146 Add layout data related constants
    Co-authored-by: Sandro Chinea <sandrochinea@gmail.com>

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/config/constants/layoutDataItemDefaultConfigurations.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {LAYOUT_DATA_ITEM_TYPES} from './layoutDataItemTypes';
+
+export const LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS = {
+	[LAYOUT_DATA_ITEM_TYPES.column]: {
+		size: 0
+	},
+
+	[LAYOUT_DATA_ITEM_TYPES.container]: {
+		backgroundColorCssClass: '',
+		backgroundImage: '',
+		paddingHorizontal: 3,
+		paddingVertical: 3,
+		type: 'fluid'
+	},
+
+	[LAYOUT_DATA_ITEM_TYPES.fragment]: {
+		fragmentEntryLinkId: ''
+	},
+
+	[LAYOUT_DATA_ITEM_TYPES.root]: {},
+
+	[LAYOUT_DATA_ITEM_TYPES.row]: {
+		gutters: true
+	}
+};
