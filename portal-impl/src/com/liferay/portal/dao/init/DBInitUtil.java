f178bc42dcb6aca97d2a1be477e8e8c3cb1b2aaa
Brian Chan <brian.chan@liferay.com>
2019-10-30 10:26:19 -0700

    Revert "LPS-103534 Remove very old upgrade logic."
    This reverts commit dba70d5c2bab66d3d51b055754d1b709584d5b50.

--- portal-impl/src/com/liferay/portal/dao/init/DBInitUtil.java
+++ portal-impl/src/com/liferay/portal/dao/init/DBInitUtil.java
@@ -80,6 +80,16 @@ public class DBInitUtil {
 				}
 			}
 
+			try {
+				db.runSQL(
+					connection, "alter table Release_ add state_ INTEGER");
+			}
+			catch (Exception e) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(e.getMessage());
+				}
+			}
+
 			if (_checkDefaultRelease(connection)) {
 				_setSupportsStringCaseSensitiveQuery(db, connection);
 
@@ -105,8 +115,8 @@ public class DBInitUtil {
 				StringBundler.concat(
 					"insert into Release_ (releaseId, createDate, ",
 					"modifiedDate, servletContextName, schemaVersion, ",
-					"buildNumber, verified, testString) values (",
-					ReleaseConstants.DEFAULT_ID, ", ?, ?, ?, ?, ?, ?, ?)"))) {
+					"buildNumber, verified) values (",
+					ReleaseConstants.DEFAULT_ID, ", ?, ?, ?, ?, ?, ?)"))) {
 
 			Date now = new Date(System.currentTimeMillis());
 
@@ -122,7 +132,6 @@ public class DBInitUtil {
 
 			ps.setInt(5, ReleaseInfo.getBuildNumber());
 			ps.setBoolean(6, false);
-			ps.setString(7, ReleaseConstants.TEST_STRING);
 
 			ps.executeUpdate();
 		}
@@ -170,8 +179,7 @@ public class DBInitUtil {
 	}
 
 	private static boolean _hasDefaultReleaseWithTestString(
-			Connection connection, String testString)
-		throws Exception {
+		Connection connection, String testString) {
 
 		try (PreparedStatement ps = connection.prepareStatement(
 				"select count(*) from Release_ where releaseId = ? and " +
@@ -184,6 +192,11 @@ public class DBInitUtil {
 				if (rs.next() && (rs.getInt(1) > 0)) {
 					return true;
 				}
+			}
+		}
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(e.getMessage());
 			}
 		}
 
@@ -203,14 +216,40 @@ public class DBInitUtil {
 	}
 
 	private static void _setSupportsStringCaseSensitiveQuery(
-			DB db, Connection connection)
-		throws Exception {
+		DB db, Connection connection) {
 
 		if (!_hasDefaultReleaseWithTestString(
 				connection, ReleaseConstants.TEST_STRING)) {
 
-			throw new SystemException(
-				"Release_ table was not initialized properly");
+			try {
+				db.runSQL(
+					connection,
+					"alter table Release_ add testString VARCHAR(1024) null");
+			}
+			catch (Exception e) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(e.getMessage());
+				}
+			}
+
+			try {
+				db.runSQL(
+					connection,
+					"update Release_ set testString = '" +
+						ReleaseConstants.TEST_STRING + "'");
+			}
+			catch (Exception e) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(e.getMessage());
+				}
+			}
+
+			if (!_hasDefaultReleaseWithTestString(
+					connection, ReleaseConstants.TEST_STRING)) {
+
+				throw new SystemException(
+					"Release_ table was not initialized properly");
+			}
 		}
 
 		if (_hasDefaultReleaseWithTestString(
