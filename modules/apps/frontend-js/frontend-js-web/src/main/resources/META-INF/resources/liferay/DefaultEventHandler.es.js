afbcd81935f3c3aa3ec4e43e5efb56d85a48489c
Chema Balsas <jose.balsas@liferay.com>
2019-10-30 16:15:21 +0100

    LPS-100125 Removes unnecessary even prevention

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/DefaultEventHandler.es.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/DefaultEventHandler.es.js
@@ -16,8 +16,6 @@ import PortletBase from './PortletBase.es';
 
 class DefaultEventHandler extends PortletBase {
 	callAction(event) {
-		event.preventDefault();
-
 		var itemData = event.data.item.data;
 
 		if (itemData && itemData.action && this[itemData.action]) {
