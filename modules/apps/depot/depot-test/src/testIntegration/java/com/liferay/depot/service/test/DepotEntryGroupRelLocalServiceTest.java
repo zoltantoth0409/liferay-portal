9da5b1e285a407cec7f156608c0325a71ab0dc4a
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-10-31 15:32:16 +0100

    LPS-103912 Add CRUD tests.

--- modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/service/test/DepotEntryGroupRelLocalServiceTest.java
+++ modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/service/test/DepotEntryGroupRelLocalServiceTest.java
@@ -15,24 +15,30 @@
 package com.liferay.depot.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.depot.exception.NoSuchEntryGroupRelException;
 import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.model.DepotEntryGroupRel;
 import com.liferay.depot.service.DepotEntryGroupRelLocalService;
 import com.liferay.depot.service.DepotEntryLocalService;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -49,34 +55,85 @@ public class DepotEntryGroupRelLocalServiceTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testAddDepotEntryGroupRel() throws Exception {
+		DepotEntry depotEntry = _addDepotEntry();
+
+		DepotEntryGroupRel depotEntryGroupRel =
+			_depotEntryGroupRelLocalService.addDepotEntryGroupRel(
+				depotEntry.getDepotEntryId(), _group.getGroupId());
+
+		Assert.assertNotNull(depotEntryGroupRel.getDepotEntryId());
+		Assert.assertEquals(
+			depotEntry.getDepotEntryId(), depotEntryGroupRel.getDepotEntryId());
+		Assert.assertEquals(
+			_group.getGroupId(), depotEntryGroupRel.getToGroupId());
+	}
+
+	@Test
+	public void testAddDepotEntryGroupRelWithARepeatedDepotEntryGroupRel()
+		throws Exception {
+
+		DepotEntry depotEntry = _addDepotEntry();
+
+		DepotEntryGroupRel originalDepotEntryGroupRel =
+			_depotEntryGroupRelLocalService.addDepotEntryGroupRel(
+				depotEntry.getDepotEntryId(), _group.getGroupId());
+
+		DepotEntryGroupRel finalDepotEntryGroupRel =
+			_depotEntryGroupRelLocalService.addDepotEntryGroupRel(
+				depotEntry.getDepotEntryId(), _group.getGroupId());
+
+		Assert.assertEquals(
+			originalDepotEntryGroupRel.getDepotEntryGroupRelId(),
+			finalDepotEntryGroupRel.getDepotEntryGroupRelId());
+		Assert.assertEquals(
+			originalDepotEntryGroupRel.getDepotEntryId(),
+			finalDepotEntryGroupRel.getDepotEntryId());
+		Assert.assertEquals(
+			originalDepotEntryGroupRel.getToGroupId(),
+			finalDepotEntryGroupRel.getToGroupId());
+	}
+
+	@Test
+	public void testDeleteDepotEntryGroupRel() throws Exception {
+		DepotEntry depotEntry = _addDepotEntry();
+
+		DepotEntryGroupRel depotEntryGroupRel =
+			_depotEntryGroupRelLocalService.addDepotEntryGroupRel(
+				depotEntry.getDepotEntryId(), _group.getGroupId());
+
+		_depotEntryGroupRelLocalService.deleteDepotEntryGroupRel(
+			depotEntryGroupRel.getDepotEntryGroupRelId());
+
+		_depotEntries.remove(depotEntry);
+
+		try {
+			_depotEntryGroupRelLocalService.getDepotEntryGroupRel(
+				depotEntryGroupRel.getDepotEntryGroupRelId());
+
+			Assert.fail();
+		}
+		catch (NoSuchEntryGroupRelException nsegre) {
+		}
+	}
+
 	@Test
 	public void testDeleteGroup() throws Exception {
-		DepotEntry depotEntry = _depotEntryLocalService.addDepotEntry(
-			new HashMap<Locale, String>() {
-				{
-					put(LocaleUtil.getDefault(), StringUtil.randomString());
-				}
-			},
-			new HashMap<Locale, String>() {
-				{
-					put(LocaleUtil.getDefault(), StringUtil.randomString());
-				}
-			},
-			ServiceContextTestUtil.getServiceContext());
+		DepotEntry depotEntry = _addDepotEntry();
 
 		Group group = _groupLocalService.addGroup(
 			TestPropsValues.getUserId(), GroupConstants.DEFAULT_PARENT_GROUP_ID,
 			null, 0, 0,
-			new HashMap<Locale, String>() {
-				{
-					put(LocaleUtil.getDefault(), StringUtil.randomString());
-				}
-			},
-			new HashMap<Locale, String>() {
-				{
-					put(LocaleUtil.getDefault(), StringUtil.randomString());
-				}
-			},
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
 			GroupConstants.TYPE_SITE_OPEN, true,
 			GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION, null, true, true,
 			ServiceContextTestUtil.getServiceContext());
@@ -98,11 +155,53 @@ public class DepotEntryGroupRelLocalServiceTest {
 			_depotEntryGroupRelLocalService.getDepotEntryGroupRelsCount());
 	}
 
+	@Test
+	public void testGetDepotEntryGroupRel() throws Exception {
+		DepotEntry depotEntry = _addDepotEntry();
+
+		DepotEntryGroupRel originalDepotEntryGroupRel =
+			_depotEntryGroupRelLocalService.addDepotEntryGroupRel(
+				depotEntry.getDepotEntryId(), _group.getGroupId());
+
+		DepotEntryGroupRel finalDepotEntryGroupRel =
+			_depotEntryGroupRelLocalService.getDepotEntryGroupRel(
+				originalDepotEntryGroupRel.getDepotEntryGroupRelId());
+
+		Assert.assertEquals(
+			originalDepotEntryGroupRel.getDepotEntryGroupRelId(),
+			finalDepotEntryGroupRel.getDepotEntryGroupRelId());
+		Assert.assertEquals(
+			originalDepotEntryGroupRel.getDepotEntryId(),
+			finalDepotEntryGroupRel.getDepotEntryId());
+		Assert.assertEquals(
+			originalDepotEntryGroupRel.getToGroupId(),
+			finalDepotEntryGroupRel.getToGroupId());
+	}
+
+	private DepotEntry _addDepotEntry() throws Exception {
+		DepotEntry depotEntry = _depotEntryLocalService.addDepotEntry(
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
+			ServiceContextTestUtil.getServiceContext());
+
+		_depotEntries.add(depotEntry);
+
+		return depotEntry;
+	}
+
+	@DeleteAfterTestRun
+	private final List<DepotEntry> _depotEntries = new ArrayList<>();
+
 	@Inject
 	private DepotEntryGroupRelLocalService _depotEntryGroupRelLocalService;
 
 	@Inject
 	private DepotEntryLocalService _depotEntryLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
 
 	@Inject
 	private GroupLocalService _groupLocalService;
