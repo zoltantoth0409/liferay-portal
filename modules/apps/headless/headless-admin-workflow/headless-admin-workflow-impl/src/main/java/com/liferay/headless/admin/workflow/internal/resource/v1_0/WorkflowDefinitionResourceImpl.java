21622d044cb44bf48bfa2382e1b23fb54f7feb76
Rafael Praxedes <rafael.praxedes@liferay.com>
2019-10-31 11:27:39 -0300

    LPS-102655 | LPS-103339 Implement added operations

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowDefinitionResourceImpl.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowDefinitionResourceImpl.java
@@ -14,9 +14,15 @@
 
 package com.liferay.headless.admin.workflow.internal.resource.v1_0;
 
+import com.liferay.headless.admin.workflow.dto.v1_0.WorkflowDefinition;
 import com.liferay.headless.admin.workflow.resource.v1_0.WorkflowDefinitionResource;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.WorkflowDefinitionManager;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,5 +34,117 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class WorkflowDefinitionResourceImpl
 	extends BaseWorkflowDefinitionResourceImpl {
+
+	@Override
+	public void deleteWorkflowDefinitionUndeploy(String name, String version)
+		throws Exception {
+
+		_workflowDefinitionManager.undeployWorkflowDefinition(
+			contextCompany.getCompanyId(), contextUser.getUserId(), name,
+			GetterUtil.getInteger(version));
+	}
+
+	@Override
+	public WorkflowDefinition getWorkflowDefinitionFindByName(String name)
+		throws Exception {
+
+		return _toWorkflowDefinition(
+			_workflowDefinitionManager.getLatestWorkflowDefinition(
+				contextCompany.getCompanyId(), name));
+	}
+
+	@Override
+	public Page<WorkflowDefinition> getWorkflowDefinitionsPage(
+			Pagination pagination)
+		throws Exception {
+
+		return Page.of(
+			transform(
+				_workflowDefinitionManager.getLatestWorkflowDefinitions(
+					contextCompany.getCompanyId(),
+					pagination.getStartPosition(), pagination.getEndPosition(),
+					null),
+				this::_toWorkflowDefinition),
+			pagination,
+			_workflowDefinitionManager.getLatestWorkflowDefinitionCount(
+				contextCompany.getCompanyId()));
+	}
+
+	public WorkflowDefinition postWorkflowDefinitionDeploy(
+			WorkflowDefinition workflowDefinition)
+		throws Exception {
+
+		String content = workflowDefinition.getContent();
+
+		return _toWorkflowDefinition(
+			_workflowDefinitionManager.deployWorkflowDefinition(
+				contextCompany.getCompanyId(), contextUser.getUserId(),
+				workflowDefinition.getTitle(), workflowDefinition.getName(),
+				content.getBytes()));
+	}
+
+	public WorkflowDefinition postWorkflowDefinitionSave(
+			WorkflowDefinition workflowDefinition)
+		throws Exception {
+
+		String content = workflowDefinition.getContent();
+
+		return _toWorkflowDefinition(
+			_workflowDefinitionManager.saveWorkflowDefinition(
+				contextCompany.getCompanyId(), contextUser.getUserId(),
+				workflowDefinition.getTitle(), workflowDefinition.getName(),
+				content.getBytes()));
+	}
+
+	@Override
+	public WorkflowDefinition postWorkflowDefinitionUpdateActive(
+			Boolean active, String name, String version)
+		throws Exception {
+
+		_workflowDefinitionManager.updateActive(
+			contextCompany.getCompanyId(), contextUser.getUserId(), name,
+			GetterUtil.getInteger(version), active);
+
+		return _toWorkflowDefinition(
+			_workflowDefinitionManager.getWorkflowDefinition(
+				contextCompany.getCompanyId(), name,
+				GetterUtil.getInteger(version)));
+	}
+
+	@Override
+	public WorkflowDefinition postWorkflowDefinitionUpdateTitle(
+			String name, String title, String version)
+		throws Exception {
+
+		_workflowDefinitionManager.updateTitle(
+			contextCompany.getCompanyId(), contextUser.getUserId(), name,
+			GetterUtil.getInteger(version), title);
+
+		return _toWorkflowDefinition(
+			_workflowDefinitionManager.getWorkflowDefinition(
+				contextCompany.getCompanyId(), name,
+				GetterUtil.getInteger(version)));
+	}
+
+	private WorkflowDefinition _toWorkflowDefinition(
+			com.liferay.portal.kernel.workflow.WorkflowDefinition
+				workflowDefinition)
+		throws Exception {
+
+		return new WorkflowDefinition() {
+			{
+				active = workflowDefinition.isActive();
+				content = workflowDefinition.getContent();
+				dateModified = workflowDefinition.getModifiedDate();
+				description = workflowDefinition.getDescription();
+				name = workflowDefinition.getName();
+				title = workflowDefinition.getTitle();
+				version = GetterUtil.getString(workflowDefinition.getVersion());
+			}
+		};
+	}
+
+	@Reference
+	private WorkflowDefinitionManager _workflowDefinitionManager;
 
 }
