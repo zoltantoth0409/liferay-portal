184dabb35a599f068d8cc7c185032e09e5ab4051
marcelabc <mbc3@cin.ufpe.br>
2019-11-01 16:21:04 -0300

    LPS-101072 Create util class to get the field properties

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/util/DDMFormFieldTypeUtil.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.util;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
+import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.Locale;
+
+/**
+ * @author Marcela Cunha
+ */
+public class DDMFormFieldTypeUtil {
+
+	public static String getPropertyValue(
+		DDMFormField ddmFormField, Locale locale, String propertyName) {
+
+		LocalizedValue value = (LocalizedValue)ddmFormField.getProperty(
+			propertyName);
+
+		return GetterUtil.getString(value.getString(locale));
+	}
+
+	public static String getPropertyValue(
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext,
+		String propertyName) {
+
+		return GetterUtil.getString(
+			String.valueOf(
+				ddmFormFieldRenderingContext.getProperty(propertyName)));
+	}
+
+}
