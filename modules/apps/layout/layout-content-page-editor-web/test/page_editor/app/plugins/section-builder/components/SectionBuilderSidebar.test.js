d5e697c270cd7b206ca833bb5884fe8fd87b37e6
Sandro Chinea <sandrochinea@gmail.com>
2019-11-03 18:04:36 +0000

    LPS-103907 Added tests for Widgets and SectionBuilder

---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/plugins/section-builder/components/SectionBuilderSidebar.test.js
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {cleanup, render, fireEvent} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import {StoreContext} from '../../../../../../src/main/resources/META-INF/resources/page_editor/app/store/index';
+import SectionBuilderSidebar from '../../../../../../src/main/resources/META-INF/resources/page_editor/plugins/section-builder/components/SectionBuilderSidebar';
+
+import '@testing-library/jest-dom/extend-expect';
+
+const elements = [
+	{
+		fragmentCollectionId: 'basicComponents',
+		fragmentEntries: [
+			{
+				fragmentEntryKey: 'basicComponentButton',
+				imagePreviewURL: 'buttonImageURL',
+				name: 'Button'
+			},
+			{
+				fragmentEntryKey: 'basicComponentCard',
+				imagePreviewURL: 'cardImageURL',
+				name: 'Card'
+			},
+			{
+				fragmentEntryKey: 'basicComponentHeading',
+				imagePreviewURL: 'headingImageURL',
+				name: 'Heading'
+			}
+		],
+		name: 'Basic Components'
+	}
+];
+
+const RenderSectionBuilderSidebar = () => {
+	return (
+		<StoreContext.Provider value={{elements}}>
+			<SectionBuilderSidebar />
+		</StoreContext.Provider>
+	);
+};
+
+describe('SectionBuilderSidebar', () => {
+	afterEach(cleanup);
+
+	it('renders the categories collapsed', () => {
+		const {getByLabelText, getByText, queryByText} = render(
+			<RenderSectionBuilderSidebar />
+		);
+
+		expect(getByText('Basic Components')).toBeInTheDocument();
+		expect(getByText('layouts')).toBeInTheDocument();
+		expect(getByLabelText('search-form')).toBeInTheDocument();
+
+		expect(queryByText('Button')).toBe(null);
+	});
+
+	it('expands a category on click and closes it when clicking it again', () => {
+		const {getByText, queryByText} = render(
+			<RenderSectionBuilderSidebar />
+		);
+		const basicComponentsCollapse = getByText('Basic Components');
+
+		fireEvent.click(basicComponentsCollapse);
+
+		expect(basicComponentsCollapse.getAttribute('aria-expanded')).toBe(
+			'true'
+		);
+
+		expect(getByText('Button'));
+
+		fireEvent.click(basicComponentsCollapse);
+
+		expect(basicComponentsCollapse.getAttribute('aria-expanded')).toBe(
+			'false'
+		);
+
+		expect(queryByText('Button')).toBe(null);
+	});
+
+	it('finds an element when you search it', () => {
+		const {getByLabelText, getByText, queryByText} = render(
+			<RenderSectionBuilderSidebar />
+		);
+		const searchFormInput = getByLabelText('search-form');
+
+		userEvent.type(searchFormInput, 'button');
+
+		expect(getByText('Button')).toBeInTheDocument();
+
+		expect(queryByText('Heading')).toBe(null);
+	});
+
+	it('expands all categories when you type something in search form and hides layouts', () => {
+		const {getByLabelText, getByText, queryByText} = render(
+			<RenderSectionBuilderSidebar />
+		);
+
+		userEvent.type(getByLabelText('search-form'), 'a');
+
+		expect(
+			getByText('Basic Components').getAttribute('aria-expanded')
+		).toBe('true');
+
+		expect(queryByText('layouts')).toBe(null);
+	});
+});
---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/plugins/widgets/components/WidgetsSidebar.test.js
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {cleanup, render, fireEvent} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import {StoreContext} from '../../../../../../src/main/resources/META-INF/resources/page_editor/app/store/index';
+import WidgetsSidebar from '../../../../../../src/main/resources/META-INF/resources/page_editor/plugins/widgets/components/WidgetsSidebar';
+
+import '@testing-library/jest-dom/extend-expect';
+
+const widgets = [
+	{
+		portlets: [
+			{
+				instanceable: true,
+				portletId: 'assetPublisher',
+				title: 'Asset Publisher',
+				used: false
+			},
+			{
+				instanceable: true,
+				portletId: 'DocumentsAndMedia',
+				title: 'Documents and Media',
+				used: false
+			},
+			{
+				instanceable: true,
+				portletId: 'NavigationMenu',
+				title: 'Navigation Menu',
+				used: false
+			}
+		],
+		title: 'Highlighted'
+	},
+	{
+		portlets: [
+			{
+				instanceable: false,
+				portletId: 'Blogs',
+				title: 'Blogs',
+				used: false
+			},
+			{
+				instanceable: false,
+				portletId: 'BlogsAggregator',
+				title: 'Blogs Aggregator',
+				used: false
+			}
+		],
+		title: 'Collaboration'
+	}
+];
+
+const RenderWidgetsSidebar = () => {
+	return (
+		<StoreContext.Provider value={{widgets}}>
+			<WidgetsSidebar />
+		</StoreContext.Provider>
+	);
+};
+
+describe('WidgetsSidebar', () => {
+	afterEach(cleanup);
+
+	it('renders the categories collapsed', () => {
+		const {getByLabelText, getByText, queryByText} = render(
+			<RenderWidgetsSidebar />
+		);
+
+		expect(getByText('Collaboration')).toBeInTheDocument();
+		expect(getByText('Highlighted')).toBeInTheDocument();
+		expect(getByLabelText('search-form')).toBeInTheDocument();
+
+		expect(queryByText('Asset Publisher')).toBe(null);
+	});
+
+	it('expands a category on click and closes it when clicking it again', () => {
+		const {getByText, queryByText} = render(<RenderWidgetsSidebar />);
+		const highlightedCategory = getByText('Highlighted');
+
+		fireEvent.click(highlightedCategory);
+
+		expect(highlightedCategory.getAttribute('aria-expanded')).toBe('true');
+
+		expect(getByText('Asset Publisher')).toBeInTheDocument();
+
+		fireEvent.click(highlightedCategory);
+
+		expect(highlightedCategory.getAttribute('aria-expanded')).toBe('false');
+
+		expect(queryByText('Asset Publisher')).toBe(null);
+	});
+
+	it('finds a widget when you search it', () => {
+		const {getByLabelText, getByText, queryByText} = render(
+			<RenderWidgetsSidebar />
+		);
+
+		userEvent.type(getByLabelText('search-form'), 'asset');
+
+		expect(getByText('Asset Publisher')).toBeInTheDocument();
+
+		expect(queryByText('Blogs')).toBe(null);
+	});
+
+	it('expands all categories when you type something in search form', () => {
+		const {getByLabelText, getByText} = render(<RenderWidgetsSidebar />);
+
+		userEvent.type(getByLabelText('search-form'), 'a');
+
+		expect(getByText('Collaboration').getAttribute('aria-expanded')).toBe(
+			'true'
+		);
+		expect(getByText('Highlighted').getAttribute('aria-expanded')).toBe(
+			'true'
+		);
+	});
+});
