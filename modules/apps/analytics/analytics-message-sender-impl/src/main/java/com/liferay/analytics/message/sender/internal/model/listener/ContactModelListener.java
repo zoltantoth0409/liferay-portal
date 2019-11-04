99a0af805fcd57e86b4b9ca522a3bfa5c44777da
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2019-11-04 09:42:37 -0800

    LRAC-4061 add listeners skeleton

---
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/ContactModelListener.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.message.sender.internal.model.listener;
+
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class ContactModelListener extends BaseModelListener<Contact> {
+
+	@Override
+	public void onAfterCreate(Contact contact) throws ModelListenerException {
+	}
+
+	@Override
+	public void onBeforeRemove(Contact contact) throws ModelListenerException {
+	}
+
+	@Override
+	public void onBeforeUpdate(Contact newContact)
+		throws ModelListenerException {
+	}
+
+}
---
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/OrganizationModelListener.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.message.sender.internal.model.listener;
+
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.model.Organization;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class OrganizationModelListener extends BaseModelListener<Organization> {
+
+	@Override
+	public void onAfterCreate(Organization organization)
+		throws ModelListenerException {
+	}
+
+	@Override
+	public void onBeforeRemove(Organization organization)
+		throws ModelListenerException {
+	}
+
+	@Override
+	public void onBeforeUpdate(Organization newOrganization)
+		throws ModelListenerException {
+	}
+
+}
