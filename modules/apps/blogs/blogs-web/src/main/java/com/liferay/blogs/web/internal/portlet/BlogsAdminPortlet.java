93fe45d07c12a0979a98bd3e1a7a411e7be91074
Adolfo Pérez <adolfo.perez@liferay.com>
2019-11-04 11:37:55 +0100

    LPS-103875 Add default resources

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/portlet/BlogsAdminPortlet.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/portlet/BlogsAdminPortlet.java
@@ -37,6 +37,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
+		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-blogs",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/blogs/css/main.css",
