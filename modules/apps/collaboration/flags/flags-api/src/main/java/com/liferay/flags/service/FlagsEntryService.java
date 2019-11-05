e7b60ed67a9f5f2f77dcc6d8f292b1e5f9d63b14
pei-jung <pei-jung.lan@liferay.com>
2019-11-05 15:15:58 -0800

    LPS-63259 Regen

--- modules/apps/collaboration/flags/flags-api/src/main/java/com/liferay/flags/service/FlagsEntryService.java
+++ modules/apps/collaboration/flags/flags-api/src/main/java/com/liferay/flags/service/FlagsEntryService.java
@@ -19,6 +19,7 @@ import aQute.bnd.annotation.ProviderType;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMode;
 import com.liferay.portal.kernel.security.access.control.AccessControlled;
 import com.liferay.portal.kernel.service.BaseService;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -56,6 +57,7 @@ public interface FlagsEntryService extends BaseService {
 	 *
 	 * Never modify or reference this interface directly. Always use {@link FlagsEntryServiceUtil} to access the flags entry remote service. Add custom service methods to <code>com.liferay.flags.service.impl.FlagsEntryServiceImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface.
 	 */
+	@JSONWebService(mode = JSONWebServiceMode.IGNORE)
 	public void addEntry(
 			String className, long classPK, String reporterEmailAddress,
 			long reportedUserId, String contentTitle, String contentURL,
