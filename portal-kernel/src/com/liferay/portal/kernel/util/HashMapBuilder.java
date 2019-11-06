bc8d75944c1782a473807fcf7e0964596c78db1e
Hugo Huijser <hugo.huijser@liferay.com>
2019-11-06 13:03:14 -0800

    LPS-103252 Add constructors/methods to support using lambda inside HashMapBuilder

--- portal-kernel/src/com/liferay/portal/kernel/util/HashMapBuilder.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/HashMapBuilder.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.kernel.util;
 
+import com.liferay.petra.function.UnsafeSupplier;
+
 import java.util.HashMap;
 
 /**
@@ -21,24 +23,134 @@ import java.util.HashMap;
  */
 public class HashMapBuilder<K, V> {
 
+	public static <K, V> HashMapWrapper<K, V> put(
+		K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+		return new HashMapWrapper<>(key, valueUnsafeSupplier);
+	}
+
 	public static <K, V> HashMapWrapper<K, V> put(K key, V value) {
 		return new HashMapWrapper<>(key, value);
 	}
 
+	public static <K, V> HashMapWrapper<K, V> put(
+		UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+		UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+		return new HashMapWrapper<>(keyUnsafeSupplier, valueUnsafeSupplier);
+	}
+
+	public static <K, V> HashMapWrapper<K, V> put(
+		UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+		return new HashMapWrapper<>(keyUnsafeSupplier, value);
+	}
+
 	public static final class HashMapWrapper<K, V> {
+
+		public HashMapWrapper(
+			K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			_put(key, valueUnsafeSupplier);
+		}
 
 		public HashMapWrapper(K key, V value) {
 			_hashMap.put(key, value);
+		}
+
+		public HashMapWrapper(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+			UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			_put(keyUnsafeSupplier, valueUnsafeSupplier);
+		}
+
+		public HashMapWrapper(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+			_put(keyUnsafeSupplier, value);
 		}
 
 		public HashMap<K, V> build() {
 			return _hashMap;
 		}
 
+		public HashMapWrapper<K, V> put(
+			K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			_put(key, valueUnsafeSupplier);
+
+			return this;
+		}
+
 		public HashMapWrapper<K, V> put(K key, V value) {
 			_hashMap.put(key, value);
 
 			return this;
+		}
+
+		public HashMapWrapper<K, V> put(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+			UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			_put(keyUnsafeSupplier, valueUnsafeSupplier);
+
+			return this;
+		}
+
+		public HashMapWrapper<K, V> put(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+			_put(keyUnsafeSupplier, value);
+
+			return this;
+		}
+
+		private void _put(
+			K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			try {
+				V value = valueUnsafeSupplier.get();
+
+				if (value != null) {
+					_hashMap.put(key, value);
+				}
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		private void _put(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+			UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			try {
+				K key = keyUnsafeSupplier.get();
+				V value = valueUnsafeSupplier.get();
+
+				if ((key != null) && (value != null)) {
+					_hashMap.put(key, value);
+				}
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		private void _put(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+			try {
+				K key = keyUnsafeSupplier.get();
+
+				if (key != null) {
+					_hashMap.put(key, value);
+				}
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
 		}
 
 		private final HashMap<K, V> _hashMap = new HashMap<>();
