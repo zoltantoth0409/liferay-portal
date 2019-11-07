dd03f4a8fe79548f222ee5bc80bf01f743eafda2
Pavel Savinov <pavel.savinov@liferay.com>
2019-11-07 08:32:28 +0100

    LPS-104292 Exclude hidden and system layouts from asset categories validation

---
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/validator/LayoutPageTemplateEntryAssetEntryValidatorExclusionRule.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.internal.validator;
+
+import com.liferay.asset.kernel.validator.AssetEntryValidatorExclusionRule;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	property = "model.class.name=com.liferay.portal.kernel.model.Layout",
+	service = AssetEntryValidatorExclusionRule.class
+)
+public class LayoutPageTemplateEntryAssetEntryValidatorExclusionRule
+	implements AssetEntryValidatorExclusionRule {
+
+	@Override
+	public boolean isValidationExcluded(
+		long groupId, String className, long classPK, long classTypePK,
+		long[] categoryIds, String[] tagNames) {
+
+		Layout layout = _layoutLocalService.fetchLayout(classPK);
+
+		if (layout.isHidden() && layout.isSystem()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+}
