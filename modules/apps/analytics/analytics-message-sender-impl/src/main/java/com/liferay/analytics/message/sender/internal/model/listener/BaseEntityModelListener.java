c9585143a307a571b195dda58be3b068ce410180
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2019-11-06 17:59:41 -0800

    LRAC-4061 construct the jsonObject instead

--- modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/BaseEntityModelListener.java
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/BaseEntityModelListener.java
@@ -20,7 +20,7 @@ import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
 import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONSerializer;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
@@ -32,8 +32,8 @@ import com.liferay.portal.kernel.service.UserLocalService;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Reference;
 
@@ -41,8 +41,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Rachael Koestartyo
  */
 public abstract class BaseEntityModelListener
-	<T extends BaseModel<T> & ShardedModel>
-		extends BaseModelListener<T> {
+	<T extends BaseModel<T> & ShardedModel> extends BaseModelListener<T> {
 
 	@Override
 	public void onAfterCreate(T model) throws ModelListenerException {
@@ -131,7 +130,7 @@ public abstract class BaseEntityModelListener
 	private void _send(
 		String eventType, List<String> includeAttributes, T model) {
 
-		String objectString = _serialize(includeAttributes, model);
+		JSONObject jsonObject = _serialize(includeAttributes, model);
 
 		try {
 			AnalyticsMessage.Builder analyticsMessageBuilder =
@@ -140,29 +139,34 @@ public abstract class BaseEntityModelListener
 					model.getModelClassName());
 
 			analyticsMessageBuilder.action(eventType);
-			analyticsMessageBuilder.object(
-				JSONFactoryUtil.createJSONObject(objectString));
+			analyticsMessageBuilder.object(jsonObject);
 
 			analyticsMessageSenderClient.send(
 				Collections.singletonList(analyticsMessageBuilder.build()));
 		}
 		catch (Exception e) {
 			if (_log.isInfoEnabled()) {
-				_log.info("Unable to send analytics message " + objectString);
+				_log.info(
+					"Unable to send analytics message " +
+						jsonObject.toString());
 			}
 		}
 	}
 
-	private String _serialize(List<String> includeAttributes, T model) {
-		JSONSerializer jsonSerializer = JSONFactoryUtil.createJSONSerializer();
+	private JSONObject _serialize(List<String> includeAttributes, T model) {
+		Map<String, Object> modelAttributes = model.getModelAttributes();
 
-		includeAttributes.add(getPrimaryKeyName());
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
-		Stream<String> stream = includeAttributes.stream();
+		for (String includeAttribute : includeAttributes) {
+			jsonObject.put(
+				includeAttribute, modelAttributes.get(includeAttribute));
+		}
 
-		jsonSerializer.include(stream.toArray(String[]::new));
+		jsonObject.put(
+			getPrimaryKeyName(), String.valueOf(model.getPrimaryKeyObj()));
 
-		return jsonSerializer.serialize(model);
+		return jsonObject;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
