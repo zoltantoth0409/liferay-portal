056e24a33ff165f4636bb00e629ebfd3b5734b01
Brian Chan <brian.chan@liferay.com>
2019-11-07 15:29:12 -0800

    LPS-104326 my bad... SF is right and I'm wrong

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/FragmentEntryProcessorEditableTest.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/FragmentEntryProcessorEditableTest.java
@@ -113,9 +113,9 @@ public class FragmentEntryProcessorEditableTest {
 
 		FragmentEntry fragmentEntry = _addFragmentEntry("fragment_entry.html");
 
+		fragmentEntryLink.setHtml(fragmentEntry.getHtml());
 		fragmentEntryLink.setEditableValues(
 			_getJsonFileAsString("fragment_entry_link_editable_values.json"));
-		fragmentEntryLink.setHtml(fragmentEntry.getHtml());
 
 		Assert.assertEquals(
 			_processedHTML,
@@ -132,10 +132,10 @@ public class FragmentEntryProcessorEditableTest {
 
 		FragmentEntry fragmentEntry = _addFragmentEntry("fragment_entry.html");
 
+		fragmentEntryLink.setHtml(fragmentEntry.getHtml());
 		fragmentEntryLink.setEditableValues(
 			_getJsonFileAsString(
 				"fragment_entry_link_editable_values_compatibility.json"));
-		fragmentEntryLink.setHtml(fragmentEntry.getHtml());
 
 		Assert.assertEquals(
 			_processedHTML,
