2534e735c7ad5bb0cb54281d11bc3e0ad646ef05
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2019-11-07 09:45:43 +0100

    LPS-101720 Service implementation

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-checker-email-otp-service/src/main/java/com/liferay/multi/factor/authentication/checker/email/otp/service/impl/EmailOTPEntryLocalServiceImpl.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-checker-email-otp-service/src/main/java/com/liferay/multi/factor/authentication/checker/email/otp/service/impl/EmailOTPEntryLocalServiceImpl.java
@@ -14,8 +14,15 @@
 
 package com.liferay.multi.factor.authentication.checker.email.otp.service.impl;
 
+import com.liferay.multi.factor.authentication.checker.email.otp.model.EmailOTPEntry;
 import com.liferay.multi.factor.authentication.checker.email.otp.service.base.EmailOTPEntryLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -39,9 +46,85 @@ import org.osgi.service.component.annotations.Component;
 public class EmailOTPEntryLocalServiceImpl
 	extends EmailOTPEntryLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Use <code>com.liferay.multi.factor.authentication.checker.email.otp.service.EmailOTPEntryLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.multi.factor.authentication.checker.email.otp.service.EmailOTPEntryLocalServiceUtil</code>.
-	 */
+	public EmailOTPEntry addEmailOTPEntry(long userId) throws PortalException {
+		EmailOTPEntry emailOTPEntry =
+			emailOTPEntryLocalService.fetchEntryByUserId(userId);
+
+		if (emailOTPEntry != null) {
+			throw new IllegalArgumentException(
+				"There is already one Email OTP Entry for user " + userId);
+		}
+
+		User user = userLocalService.getUserById(userId);
+
+		long entryId = counterLocalService.increment();
+
+		emailOTPEntry = emailOTPEntryPersistence.create(entryId);
+
+		emailOTPEntry.setCompanyId(user.getCompanyId());
+		emailOTPEntry.setUserId(userId);
+		emailOTPEntry.setUserName(user.getFullName());
+		emailOTPEntry.setCreateDate(new Date());
+
+		emailOTPEntryPersistence.update(emailOTPEntry);
+
+		return emailOTPEntry;
+	}
+
+	public EmailOTPEntry fetchEntryByUserId(long userId) {
+		return emailOTPEntryPersistence.fetchByUserId(userId);
+	}
+
+	public void resetFailedAttempts(long userId) {
+		EmailOTPEntry emailOTPEntry =
+			emailOTPEntryLocalService.fetchEntryByUserId(userId);
+
+		if (emailOTPEntry == null) {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					"Reset failed attempts on non existent user: " + userId);
+			}
+
+			return;
+		}
+
+		emailOTPEntry.setFailedAttempts(0);
+		emailOTPEntry.setLastFailDate(null);
+		emailOTPEntry.setLastFailIP(null);
+
+		emailOTPEntryPersistence.update(emailOTPEntry);
+	}
+
+	public void updateAttempts(long userId, String userIP, boolean success) {
+		EmailOTPEntry emailOTPEntry =
+			emailOTPEntryLocalService.fetchEntryByUserId(userId);
+
+		if (emailOTPEntry == null) {
+			if (_log.isInfoEnabled()) {
+				_log.info("Update attempts on non existent user: " + userId);
+			}
+
+			return;
+		}
+
+		if (success) {
+			emailOTPEntry.setFailedAttempts(0);
+			emailOTPEntry.setLastSuccessDate(new Date());
+			emailOTPEntry.setLastSuccessIP(userIP);
+			emailOTPEntry.setLastFailDate(null);
+			emailOTPEntry.setLastFailIP(null);
+		}
+		else {
+			emailOTPEntry.setFailedAttempts(
+				emailOTPEntry.getFailedAttempts() + 1);
+			emailOTPEntry.setLastFailDate(new Date());
+			emailOTPEntry.setLastFailIP(userIP);
+		}
+
+		emailOTPEntryPersistence.update(emailOTPEntry);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EmailOTPEntryLocalServiceImpl.class);
+
 }
