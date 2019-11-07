85be985a64837ae0050264f7ed08d2c91eb9b581
Rubén Pulido <ruben.pulido@liferay.com>
2019-11-07 22:44:56 +0100

    LPS-104183 Create LayoutDataItemTypesConstants

---
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/constants/LayoutDataItemTypesConstants.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.internal.constants;
+
+/**
+ * @author Rubén Pulido
+ */
+public class LayoutDataItemTypesConstants {
+
+	public static final String ITEM_TYPE_COLUMN = "column";
+
+	public static final String ITEM_TYPE_CONTAINER = "container";
+
+	public static final String ITEM_TYPE_FRAGMENT = "fragment";
+
+	public static final String ITEM_TYPE_ROOT = "root";
+
+	public static final String ITEM_TYPE_ROW = "row";
+
+}
