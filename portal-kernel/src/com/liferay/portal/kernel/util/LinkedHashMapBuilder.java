408007e95d0bf3361fc405d70e35dd314433997b
Hugo Huijser <hugo.huijser@liferay.com>
2019-11-07 21:33:40 -0800

    LPS-103252 Apply to other *MapBuilders

--- portal-kernel/src/com/liferay/portal/kernel/util/LinkedHashMapBuilder.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/LinkedHashMapBuilder.java
@@ -14,31 +14,125 @@
 
 package com.liferay.portal.kernel.util;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
 
 /**
  * @author Hugo Huijser
  */
-public class LinkedHashMapBuilder<K, V> {
+public class LinkedHashMapBuilder<K, V> extends BaseMapBuilder {
+
+	public static <K, V> LinkedHashMapWrapper<K, V> put(
+		Collection<? extends K> inputCollection,
+		UnsafeFunction<K, V, Exception> unsafeFunction) {
+
+		return new LinkedHashMapWrapper<>(inputCollection, unsafeFunction);
+	}
+
+	public static <K, V> LinkedHashMapWrapper<K, V> put(
+		K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+		return new LinkedHashMapWrapper<>(key, valueUnsafeSupplier);
+	}
 
 	public static <K, V> LinkedHashMapWrapper<K, V> put(K key, V value) {
 		return new LinkedHashMapWrapper<>(key, value);
 	}
 
-	public static final class LinkedHashMapWrapper<K, V> {
+	public static <K, V> LinkedHashMapWrapper<K, V> put(
+		UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+		UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+		return new LinkedHashMapWrapper<>(
+			keyUnsafeSupplier, valueUnsafeSupplier);
+	}
+
+	public static <K, V> LinkedHashMapWrapper<K, V> put(
+		UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+		return new LinkedHashMapWrapper<>(keyUnsafeSupplier, value);
+	}
+
+	public static final class LinkedHashMapWrapper<K, V>
+		extends BaseMapWrapper<K, V> {
+
+		public LinkedHashMapWrapper(
+			Collection<? extends K> inputCollection,
+			UnsafeFunction<K, V, Exception> unsafeFunction) {
+
+			doPut(inputCollection, unsafeFunction);
+		}
+
+		public LinkedHashMapWrapper(
+			K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			doPut(key, valueUnsafeSupplier);
+		}
 
 		public LinkedHashMapWrapper(K key, V value) {
 			_linkedHashMap.put(key, value);
+		}
+
+		public LinkedHashMapWrapper(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+			UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			doPut(keyUnsafeSupplier, valueUnsafeSupplier);
+		}
+
+		public LinkedHashMapWrapper(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+			doPut(keyUnsafeSupplier, value);
 		}
 
 		public LinkedHashMap<K, V> build() {
 			return _linkedHashMap;
 		}
 
+		public LinkedHashMapWrapper<K, V> put(
+			Collection<? extends K> inputCollection,
+			UnsafeFunction<K, V, Exception> unsafeFunction) {
+
+			doPut(inputCollection, unsafeFunction);
+
+			return this;
+		}
+
+		public LinkedHashMapWrapper<K, V> put(
+			K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			doPut(key, valueUnsafeSupplier);
+
+			return this;
+		}
+
 		public LinkedHashMapWrapper<K, V> put(K key, V value) {
 			_linkedHashMap.put(key, value);
 
 			return this;
+		}
+
+		public LinkedHashMapWrapper<K, V> put(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+			UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			doPut(keyUnsafeSupplier, valueUnsafeSupplier);
+
+			return this;
+		}
+
+		public LinkedHashMapWrapper<K, V> put(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+			doPut(keyUnsafeSupplier, value);
+
+			return this;
+		}
+
+		@Override
+		protected LinkedHashMap<K, V> getMap() {
+			return _linkedHashMap;
 		}
 
 		private final LinkedHashMap<K, V> _linkedHashMap =
--- portal-kernel/src/com/liferay/portal/kernel/util/TreeMapBuilder.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/TreeMapBuilder.java
@@ -14,31 +14,124 @@
 
 package com.liferay.portal.kernel.util;
 
+import java.util.Collection;
 import java.util.TreeMap;
 
 /**
  * @author Hugo Huijser
  */
-public class TreeMapBuilder<K, V> {
+public class TreeMapBuilder<K, V> extends BaseMapBuilder {
+
+	public static <K, V> TreeMapWrapper<K, V> put(
+		Collection<? extends K> inputCollection,
+		UnsafeFunction<K, V, Exception> unsafeFunction) {
+
+		return new TreeMapWrapper<>(inputCollection, unsafeFunction);
+	}
+
+	public static <K, V> TreeMapWrapper<K, V> put(
+		K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+		return new TreeMapWrapper<>(key, valueUnsafeSupplier);
+	}
 
 	public static <K, V> TreeMapWrapper<K, V> put(K key, V value) {
 		return new TreeMapWrapper<>(key, value);
 	}
 
-	public static final class TreeMapWrapper<K, V> {
+	public static <K, V> TreeMapWrapper<K, V> put(
+		UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+		UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+		return new TreeMapWrapper<>(keyUnsafeSupplier, valueUnsafeSupplier);
+	}
+
+	public static <K, V> TreeMapWrapper<K, V> put(
+		UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+		return new TreeMapWrapper<>(keyUnsafeSupplier, value);
+	}
+
+	public static final class TreeMapWrapper<K, V>
+		extends BaseMapWrapper<K, V> {
+
+		public TreeMapWrapper(
+			Collection<? extends K> inputCollection,
+			UnsafeFunction<K, V, Exception> unsafeFunction) {
+
+			doPut(inputCollection, unsafeFunction);
+		}
+
+		public TreeMapWrapper(
+			K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			doPut(key, valueUnsafeSupplier);
+		}
 
 		public TreeMapWrapper(K key, V value) {
 			_treeMap.put(key, value);
+		}
+
+		public TreeMapWrapper(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+			UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			doPut(keyUnsafeSupplier, valueUnsafeSupplier);
+		}
+
+		public TreeMapWrapper(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+			doPut(keyUnsafeSupplier, value);
 		}
 
 		public TreeMap<K, V> build() {
 			return _treeMap;
 		}
 
+		public TreeMapWrapper<K, V> put(
+			Collection<? extends K> inputCollection,
+			UnsafeFunction<K, V, Exception> unsafeFunction) {
+
+			doPut(inputCollection, unsafeFunction);
+
+			return this;
+		}
+
+		public TreeMapWrapper<K, V> put(
+			K key, UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			doPut(key, valueUnsafeSupplier);
+
+			return this;
+		}
+
 		public TreeMapWrapper<K, V> put(K key, V value) {
 			_treeMap.put(key, value);
 
 			return this;
+		}
+
+		public TreeMapWrapper<K, V> put(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier,
+			UnsafeSupplier<V, Exception> valueUnsafeSupplier) {
+
+			doPut(keyUnsafeSupplier, valueUnsafeSupplier);
+
+			return this;
+		}
+
+		public TreeMapWrapper<K, V> put(
+			UnsafeSupplier<K, Exception> keyUnsafeSupplier, V value) {
+
+			doPut(keyUnsafeSupplier, value);
+
+			return this;
+		}
+
+		@Override
+		protected TreeMap<K, V> getMap() {
+			return _treeMap;
 		}
 
 		private final TreeMap<K, V> _treeMap = new TreeMap<>();
