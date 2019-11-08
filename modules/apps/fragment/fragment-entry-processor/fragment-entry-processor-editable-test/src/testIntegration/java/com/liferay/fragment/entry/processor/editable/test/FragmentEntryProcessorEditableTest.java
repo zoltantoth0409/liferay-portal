d87758b7d6892896f75d21b920847169e685862f
Hugo Huijser <hugo.huijser@liferay.com>
2019-11-07 21:14:06 -0800

    LPS-104326 SF

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/FragmentEntryProcessorEditableTest.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/FragmentEntryProcessorEditableTest.java
@@ -114,6 +114,7 @@ public class FragmentEntryProcessorEditableTest {
 		FragmentEntry fragmentEntry = _addFragmentEntry("fragment_entry.html");
 
 		fragmentEntryLink.setHtml(fragmentEntry.getHtml());
+
 		fragmentEntryLink.setEditableValues(
 			_getJsonFileAsString("fragment_entry_link_editable_values.json"));
 
@@ -133,6 +134,7 @@ public class FragmentEntryProcessorEditableTest {
 		FragmentEntry fragmentEntry = _addFragmentEntry("fragment_entry.html");
 
 		fragmentEntryLink.setHtml(fragmentEntry.getHtml());
+
 		fragmentEntryLink.setEditableValues(
 			_getJsonFileAsString(
 				"fragment_entry_link_editable_values_compatibility.json"));
