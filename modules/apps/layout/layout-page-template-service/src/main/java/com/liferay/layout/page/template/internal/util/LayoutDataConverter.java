9d27ce00ad84a26034e7415969a253e3d0cde316
Rubén Pulido <ruben.pulido@liferay.com>
2019-11-08 23:54:06 +0100

    LPS-104183 Enhance convert method to be able to convert non empty data layout data

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/util/LayoutDataConverter.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/util/LayoutDataConverter.java
@@ -14,8 +14,10 @@
 
 package com.liferay.layout.page.template.internal.util;
 
+import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.layout.page.template.internal.constants.LayoutDataItemTypesConstants;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -50,10 +52,187 @@ public class LayoutDataConverter {
 			return data;
 		}
 
+		JSONObject itemsJSONObject = JSONFactoryUtil.createJSONObject();
+
+		JSONArray mainChildrenJSONArray = JSONFactoryUtil.createJSONArray();
+
 		UUID mainUUID = UUID.randomUUID();
 
+		JSONArray structureJSONArray = inputDataJSONObject.getJSONArray(
+			"structure");
+
+		for (int i = 0; i < structureJSONArray.length(); i++) {
+			JSONObject inputRowJSONObject = structureJSONArray.getJSONObject(i);
+
+			JSONArray columnsJSONArray = inputRowJSONObject.getJSONArray(
+				"columns");
+
+			String type = inputRowJSONObject.getString("type");
+
+			if (type.equals(String.valueOf(FragmentConstants.TYPE_COMPONENT))) {
+				UUID containerUUID = UUID.randomUUID();
+
+				mainChildrenJSONArray.put(containerUUID);
+
+				UUID rowUUID = UUID.randomUUID();
+
+				JSONArray rowChildrenJSONArray =
+					JSONFactoryUtil.createJSONArray();
+
+				for (int j = 0; j < columnsJSONArray.length(); j++) {
+					JSONObject inputColumnJSONObject =
+						columnsJSONArray.getJSONObject(j);
+
+					UUID columnUUID = UUID.randomUUID();
+
+					rowChildrenJSONArray.put(columnUUID.toString());
+
+					JSONArray columnChildrenJSONArray =
+						JSONFactoryUtil.createJSONArray();
+
+					JSONArray fragmentEntryLinksJSONArray =
+						inputColumnJSONObject.getJSONArray(
+							"fragmentEntryLinkIds");
+
+					for (int k = 0; k < fragmentEntryLinksJSONArray.length();
+						 k++) {
+
+						String fragmentEntryLinkId =
+							fragmentEntryLinksJSONArray.getString(k);
+
+						UUID fragmentUUID = UUID.randomUUID();
+
+						columnChildrenJSONArray.put(fragmentUUID.toString());
+
+						JSONObject fragmentJSONObject = JSONUtil.put(
+							"children", JSONFactoryUtil.createJSONArray()
+						).put(
+							"config",
+							JSONUtil.put(
+								"fragmentEntryLinkId", fragmentEntryLinkId)
+						).put(
+							"itemId", fragmentUUID.toString()
+						).put(
+							"parentId", columnUUID.toString()
+						).put(
+							"type",
+							LayoutDataItemTypesConstants.ITEM_TYPE_FRAGMENT
+						);
+
+						itemsJSONObject.put(
+							fragmentUUID.toString(), fragmentJSONObject);
+					}
+
+					JSONObject columnJSONObject = JSONUtil.put(
+						"children", columnChildrenJSONArray
+					).put(
+						"config",
+						JSONUtil.put(
+							"size",
+							Integer.valueOf(
+								inputColumnJSONObject.getString("size")))
+					).put(
+						"itemId", columnUUID.toString()
+					).put(
+						"parentId", rowUUID.toString()
+					).put(
+						"type", LayoutDataItemTypesConstants.ITEM_TYPE_COLUMN
+					);
+
+					itemsJSONObject.put(
+						columnUUID.toString(), columnJSONObject);
+				}
+
+				JSONObject inputRowConfigJSONObject =
+					inputRowJSONObject.getJSONObject("config");
+
+				JSONObject rowJSONObject = JSONUtil.put(
+					"children", rowChildrenJSONArray
+				).put(
+					"config",
+					JSONUtil.put(
+						"gutters", inputRowConfigJSONObject.get("columnSpacing")
+					).put(
+						"verticalAlign", "top"
+					)
+				).put(
+					"itemId", rowUUID.toString()
+				).put(
+					"parentId", containerUUID.toString()
+				).put(
+					"type", LayoutDataItemTypesConstants.ITEM_TYPE_ROW
+				);
+
+				itemsJSONObject.put(rowUUID.toString(), rowJSONObject);
+
+				JSONObject containerJSONObject = JSONUtil.put(
+					"children", JSONUtil.put(rowUUID.toString())
+				).put(
+					"config",
+					JSONUtil.put(
+						"backgroundColorCssClass",
+						inputRowConfigJSONObject.get("backgroundColorCssClass")
+					).put(
+						"backgroundImage",
+						inputRowConfigJSONObject.get("backgroundImage")
+					).put(
+						"paddingHorizontal",
+						Integer.valueOf(
+							inputRowConfigJSONObject.getString(
+								"paddingHorizontal"))
+					).put(
+						"paddingVertical",
+						Integer.valueOf(
+							inputRowConfigJSONObject.getString(
+								"paddingVertical"))
+					).put(
+						"type",
+						inputRowConfigJSONObject.getString(
+							"containerType", null)
+					)
+				).put(
+					"itemId", containerUUID.toString()
+				).put(
+					"parentId", mainUUID.toString()
+				).put(
+					"type", LayoutDataItemTypesConstants.ITEM_TYPE_CONTAINER
+				);
+
+				itemsJSONObject.put(
+					containerUUID.toString(), containerJSONObject);
+			}
+			else {
+				UUID fragmentUUID = UUID.randomUUID();
+
+				mainChildrenJSONArray.put(fragmentUUID.toString());
+
+				JSONObject columnJSONObject = columnsJSONArray.getJSONObject(0);
+
+				JSONArray fragmentEntryLinkIdsJSONArray =
+					columnJSONObject.getJSONArray("fragmentEntryLinkIds");
+
+				JSONObject fragmentJSONObject = JSONUtil.put(
+					"children", JSONFactoryUtil.createJSONArray()
+				).put(
+					"config",
+					JSONUtil.put(
+						"fragmentEntryLinkId",
+						fragmentEntryLinkIdsJSONArray.get(0))
+				).put(
+					"itemId", fragmentUUID.toString()
+				).put(
+					"parentId", mainUUID
+				).put(
+					"type", LayoutDataItemTypesConstants.ITEM_TYPE_FRAGMENT
+				);
+
+				itemsJSONObject.put(
+					fragmentUUID.toString(), fragmentJSONObject);
+			}
+		}
+
 		JSONObject mainJSONObject = JSONUtil.put(
-			"children", JSONFactoryUtil.createJSONArray()
+			"children", mainChildrenJSONArray
 		).put(
 			"config", JSONFactoryUtil.createJSONObject()
 		).put(
@@ -64,8 +243,7 @@ public class LayoutDataConverter {
 			"type", LayoutDataItemTypesConstants.ITEM_TYPE_ROOT
 		);
 
-		JSONObject itemsJSONObject = JSONUtil.put(
-			mainUUID.toString(), mainJSONObject);
+		itemsJSONObject.put(mainUUID.toString(), mainJSONObject);
 
 		JSONObject outputDataJSONObject = JSONUtil.put(
 			"items", itemsJSONObject
