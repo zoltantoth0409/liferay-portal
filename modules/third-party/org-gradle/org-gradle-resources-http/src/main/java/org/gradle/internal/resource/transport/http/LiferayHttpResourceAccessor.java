71f3524193c82c9ceadda821d0be1e228f9efe9c
Peter Shin <peter.shin@liferay.com>
2019-11-12 09:56:30 -0800

    LPS-100515 Fix deployDistribution

--- modules/third-party/org-gradle/org-gradle-resources-http/src/main/java/org/gradle/internal/resource/transport/http/LiferayHttpResourceAccessor.java
+++ modules/third-party/org-gradle/org-gradle-resources-http/src/main/java/org/gradle/internal/resource/transport/http/LiferayHttpResourceAccessor.java
@@ -314,8 +314,11 @@ public class LiferayHttpResourceAccessor extends HttpResourceAccessor {
 			HttpHeaders.CONTENT_LENGTH,
 			String.valueOf(byteArrayOutputStream.size()));
 
-		return new HttpResponseResource(
+		HttpClientResponse httpClientResponse = new HttpClientResponse(
 			HttpGet.METHOD_NAME, uri, closeableHttpResponse);
+
+		return new HttpResponseResource(
+			HttpGet.METHOD_NAME, uri, httpClientResponse);
 	}
 
 	private String _getModuleLatestVersion(
@@ -381,8 +384,11 @@ public class LiferayHttpResourceAccessor extends HttpResourceAccessor {
 			HttpHeaders.LAST_MODIFIED,
 			String.valueOf(cachedArtifactFile.lastModified()));
 
-		return new HttpResponseResource(
+		HttpClientResponse httpClientResponse = new HttpClientResponse(
 			HttpGet.METHOD_NAME, uri, closeableHttpResponse);
+
+		return new HttpResponseResource(
+			HttpGet.METHOD_NAME, uri, httpClientResponse);
 	}
 
 	private boolean _isForcedCacheEnabled() {
