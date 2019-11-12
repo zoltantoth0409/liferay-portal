da9b5d75b19403c5cd37d5a163d39b1504935b08
tina.tian <tina.tian@liferay.com>
2019-11-11 16:02:42 -0800

    LPS-104353 SF, no need to throw

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -2235,9 +2235,8 @@ public class DataFactory {
 	}
 
 	public FragmentEntryLinkModel newFragmentEntryLinkModel(
-			LayoutModel layoutModel, FragmentEntryModel fragmentEntryModel,
-			int position)
-		throws Exception {
+		LayoutModel layoutModel, FragmentEntryModel fragmentEntryModel,
+		int position) {
 
 		FragmentEntryLinkModel fragmentEntryLinkModel =
 			new FragmentEntryLinkModelImpl();
@@ -2266,9 +2265,8 @@ public class DataFactory {
 	}
 
 	public List<FragmentEntryLinkModel> newFragmentEntryLinkModels(
-			LayoutModel layoutModel,
-			Map<String, FragmentEntryModel> fragmentEntryModels)
-		throws Exception {
+		LayoutModel layoutModel,
+		Map<String, FragmentEntryModel> fragmentEntryModels) {
 
 		List<FragmentEntryLinkModel> fragmentEntryLinkModels =
 			new ArrayList<>();
