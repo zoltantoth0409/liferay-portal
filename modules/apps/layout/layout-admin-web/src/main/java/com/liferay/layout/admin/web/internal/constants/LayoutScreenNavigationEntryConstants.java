375ffc4e98c8982e9da1f61ef4ec46d35291ccac
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-11-13 16:44:47 +0100

    LPS-104633 Rendering the old form in the screen navigation

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/constants/LayoutScreenNavigationEntryConstants.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.constants;
+
+/**
+ * @author Alejandro Tardín
+ */
+public class LayoutScreenNavigationEntryConstants {
+
+	public static final String CATEGORY_KEY_GENERAL = "CATEGORY_KEY_GENERAL";
+
+	public static final String ENTRY_KEY_GENERAL = "general";
+
+	public static final String SCREEN_NAVIGATION_KEY_LAYOUT =
+		"SCREEN_NAVIGATION_KEY_LAYOUT";
+
+}
---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/servlet/taglib/LayoutGeneralScreenNavigationEntry.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.servlet.taglib;
+
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.layout.admin.web.internal.constants.LayoutScreenNavigationEntryConstants;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.util.AggregateResourceBundle;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.io.IOException;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(service = ScreenNavigationEntry.class)
+public class LayoutGeneralScreenNavigationEntry
+	implements ScreenNavigationEntry<Layout> {
+
+	@Override
+	public String getCategoryKey() {
+		return LayoutScreenNavigationEntryConstants.CATEGORY_KEY_GENERAL;
+	}
+
+	@Override
+	public String getEntryKey() {
+		return LayoutScreenNavigationEntryConstants.ENTRY_KEY_GENERAL;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(getResourceBundle(locale), getEntryKey());
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return LayoutScreenNavigationEntryConstants.
+			SCREEN_NAVIGATION_KEY_LAYOUT;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		_jspRenderer.renderJSP(
+			httpServletRequest, httpServletResponse,
+			"/edit_layout_general.jsp");
+	}
+
+	protected ResourceBundle getResourceBundle(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return new AggregateResourceBundle(
+			resourceBundle, _portal.getResourceBundle(locale));
+	}
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference
+	private Portal _portal;
+
+}
