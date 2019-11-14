f26fcb6f0cfaac6b83759a9b97eb732baca3dc01
Inácio Nery <inacio.nery@liferay.com>
2019-11-14 10:08:05 -0300

    LPS-104586 Run `ant format-javadoc`

--- modules/apps/change-tracking/change-tracking-store-service/src/main/java/com/liferay/change/tracking/store/model/impl/CTSContentImpl.java
+++ modules/apps/change-tracking/change-tracking-store-service/src/main/java/com/liferay/change/tracking/store/model/impl/CTSContentImpl.java
@@ -15,10 +15,15 @@
 package com.liferay.change.tracking.store.model.impl;
 
 /**
- * The extended model implementation for the CTSContent service. Represents a row in the &quot;CTSContent&quot; database table, with each column mapped to a property of this class.
+ * The extended model implementation for the CTSContent service. Represents a
+ * row in the &quot;CTSContent&quot; database table, with each column mapped to
+ * a property of this class.
  *
  * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.change.tracking.store.model.CTSContent</code> interface.
+ * Helper methods and all application logic should be put in this class.
+ * Whenever methods are added, rerun ServiceBuilder to copy their definitions
+ * into the <code>com.liferay.change.tracking.store.model.CTSContent</code>
+ * interface.
  * </p>
  *
  * @author Shuyang Zhou
@@ -28,7 +33,9 @@ public class CTSContentImpl extends CTSContentBaseImpl {
 	/**
 	 * NOTE FOR DEVELOPERS:
 	 *
-	 * Never reference this class directly. All methods that expect a cts content model instance should use the {@link com.liferay.change.tracking.store.model.CTSContent} interface instead.
+	 * Never reference this class directly. All methods that expect a cts
+	 * content model instance should use the {@link
+	 * com.liferay.change.tracking.store.model.CTSContent} interface instead.
 	 */
 	public CTSContentImpl() {
 	}
