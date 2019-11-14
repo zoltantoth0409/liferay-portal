e63787e9f65f6333e8e048cf43a5a04fddde281a
drewbrokke <drew.brokke@liferay.com>
2019-11-14 14:33:04 -0600

    LPS-99914 account-service - (AccountRoleConstants.java) adds default Account role names

---
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountRoleConstants.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.constants;
+
+/**
+ * @author Drew Brokke
+ */
+public class AccountRoleConstants {
+
+	public static final String REQUIRED_ROLE_NAME_ACCOUNT_ADMINISTRATOR =
+		"Account Administrator";
+
+	public static final String REQUIRED_ROLE_NAME_ACCOUNT_OWNER =
+		"Account Owner";
+
+	public static final String REQUIRED_ROLE_NAME_ACCOUNT_POWER_USER =
+		"Account Power User";
+
+	public static final String REQUIRED_ROLE_NAME_ACCOUNT_USER = "Account User";
+
+	public static final String[] REQUIRED_ROLE_NAMES = {
+		REQUIRED_ROLE_NAME_ACCOUNT_USER, REQUIRED_ROLE_NAME_ACCOUNT_POWER_USER,
+		REQUIRED_ROLE_NAME_ACCOUNT_OWNER,
+		REQUIRED_ROLE_NAME_ACCOUNT_ADMINISTRATOR
+	};
+
+}
