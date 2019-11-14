18c47892060da74bf1d5d080c8a0b030ef98865b
Alan <alan.huang@liferay.com>
2019-11-14 17:15:41 +0800

    LPS-84119 Add AttributeOrderCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/AttributeOrderCheck.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.liferay.source.formatter.checkstyle.util.DetailASTUtil;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.util.List;
+
+/**
+ * @author Alan Huang
+ */
+public class AttributeOrderCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {TokenTypes.INSTANCE_INIT};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		DetailAST parentDetailAST = detailAST.getParent();
+
+		if (parentDetailAST.getType() != TokenTypes.OBJBLOCK) {
+			return;
+		}
+
+		parentDetailAST = parentDetailAST.getParent();
+
+		if (parentDetailAST.getType() != TokenTypes.LITERAL_NEW) {
+			return;
+		}
+
+		DetailAST childDetailAST = detailAST.getFirstChild();
+
+		if (childDetailAST.getType() != TokenTypes.SLIST) {
+			return;
+		}
+
+		List<DetailAST> exprDetailASTList = DetailASTUtil.getAllChildTokens(
+			childDetailAST, false, TokenTypes.EXPR);
+
+		if (exprDetailASTList.size() < 2) {
+			return;
+		}
+
+		_checkAttributeOrder(exprDetailASTList);
+	}
+
+	private void _checkAttributeOrder(List<DetailAST> exprDetailASTList) {
+		String previousName = null;
+
+		for (DetailAST exprDetailAST : exprDetailASTList) {
+			DetailAST childDetailAST = exprDetailAST.getFirstChild();
+
+			if (childDetailAST.getType() != TokenTypes.ASSIGN) {
+				continue;
+			}
+
+			DetailAST nameDetailAST = childDetailAST.findFirstToken(
+				TokenTypes.IDENT);
+
+			String name = nameDetailAST.getText();
+
+			if ((previousName != null) &&
+				(previousName.compareToIgnoreCase(name) > 0)) {
+
+				log(
+					exprDetailAST, _MSG_ATTRIBUTE_INCORRECT_ORDER,
+					previousName, name);
+			}
+
+			previousName = name;
+		}
+	}
+
+	private static final String _MSG_ATTRIBUTE_INCORRECT_ORDER =
+		"attribute.incorrect.order";
+
+}
