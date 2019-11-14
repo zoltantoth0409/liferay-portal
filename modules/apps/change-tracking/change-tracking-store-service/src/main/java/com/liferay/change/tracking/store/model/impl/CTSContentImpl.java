2d5ec53c9afb5f066b2db3ecd3210734ca55a481
Brian Chan <brian.chan@liferay.com>
2019-11-14 08:45:59 -0800

    LPS-100518 SF

--- modules/apps/change-tracking/change-tracking-store-service/src/main/java/com/liferay/change/tracking/store/model/impl/CTSContentImpl.java
+++ modules/apps/change-tracking/change-tracking-store-service/src/main/java/com/liferay/change/tracking/store/model/impl/CTSContentImpl.java
@@ -15,28 +15,10 @@
 package com.liferay.change.tracking.store.model.impl;
 
 /**
- * The extended model implementation for the CTSContent service. Represents a
- * row in the &quot;CTSContent&quot; database table, with each column mapped to
- * a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class.
- * Whenever methods are added, rerun ServiceBuilder to copy their definitions
- * into the <code>com.liferay.change.tracking.store.model.CTSContent</code>
- * interface.
- * </p>
- *
  * @author Shuyang Zhou
  */
 public class CTSContentImpl extends CTSContentBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a cts
-	 * content model instance should use the {@link
-	 * com.liferay.change.tracking.store.model.CTSContent} interface instead.
-	 */
 	public CTSContentImpl() {
 	}
 
