692a1efee6d41b672273ca4243a20f17264dda5d
Sarai Díaz García <dgarciasarai@gmail.com>
2019-11-14 19:07:54 +0100

    LPS-95813 Implement methods to get all generated tests passing successfully

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/ContentSetElementResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/ContentSetElementResourceTest.java
@@ -15,8 +15,19 @@
 package com.liferay.headless.delivery.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-
+import com.liferay.asset.list.constants.AssetListEntryTypeConstants;
+import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.asset.list.service.AssetListEntryLocalServiceUtil;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
 import com.liferay.headless.delivery.client.dto.v1_0.ContentSetElement;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 /**
@@ -26,45 +37,84 @@ import org.junit.runner.RunWith;
 public class ContentSetElementResourceTest
 	extends BaseContentSetElementResourceTestCase {
 
+	@Before
 	@Override
-	protected String testGetSiteContentSetByUuidContentSetElementsPage_getUuid()
-		throws Exception {
-		return "uuid";
+	public void setUp() throws Exception {
+		super.setUp();
+
+		long userId = TestPropsValues.getUserId();
+
+		_serviceContext = new ServiceContext();
+
+		_serviceContext.setScopeGroupId(testGroup.getGroupId());
+		_serviceContext.setUserId(userId);
+
+		_assetListEntry = AssetListEntryLocalServiceUtil.addAssetListEntry(
+			userId, testGroup.getGroupId(), RandomTestUtil.randomString(),
+			AssetListEntryTypeConstants.TYPE_DYNAMIC, _serviceContext);
 	}
 
 	@Override
-	protected String testGetSiteContentSetByKeyContentSetElementsPage_getKey()
+	protected ContentSetElement
+			testGetContentSetContentSetElementsPage_addContentSetElement(
+				Long contentSetId, ContentSetElement contentSetElement)
 		throws Exception {
-		return "key";
+
+		return _toContentSetElement(_addBlogsEntry());
 	}
 
 	@Override
-	protected Long testGetContentSetContentSetElementsPage_getContentSetId()
-		throws Exception {
-		return 0L;
+	protected Long testGetContentSetContentSetElementsPage_getContentSetId() {
+		return _assetListEntry.getAssetListEntryId();
 	}
 
 	@Override
-	protected ContentSetElement testGetContentSetContentSetElementsPage_addContentSetElement(
-		Long contentSetId, ContentSetElement contentSetElement)
+	protected ContentSetElement
+			testGetSiteContentSetByKeyContentSetElementsPage_addContentSetElement(
+				Long siteId, String key, ContentSetElement contentSetElement)
 		throws Exception {
 
-		return new ContentSetElement();
+		return _toContentSetElement(_addBlogsEntry());
 	}
 
 	@Override
-	protected ContentSetElement testGetSiteContentSetByKeyContentSetElementsPage_addContentSetElement(
-		Long siteId, String key, ContentSetElement contentSetElement)
-		throws Exception {
-
-		return new ContentSetElement();
+	protected String testGetSiteContentSetByKeyContentSetElementsPage_getKey() {
+		return _assetListEntry.getAssetListEntryKey();
 	}
 
 	@Override
-	protected ContentSetElement testGetSiteContentSetByUuidContentSetElementsPage_addContentSetElement(
-		Long siteId, String uuid, ContentSetElement contentSetElement)
+	protected ContentSetElement
+			testGetSiteContentSetByUuidContentSetElementsPage_addContentSetElement(
+				Long siteId, String uuid, ContentSetElement contentSetElement)
 		throws Exception {
 
-		return new ContentSetElement();
+		return _toContentSetElement(_addBlogsEntry());
 	}
+
+	@Override
+	protected String
+		testGetSiteContentSetByUuidContentSetElementsPage_getUuid() {
+
+		return _assetListEntry.getUuid();
+	}
+
+	private BlogsEntry _addBlogsEntry() throws PortalException {
+		return BlogsEntryLocalServiceUtil.addEntry(
+			UserLocalServiceUtil.getDefaultUserId(testGroup.getCompanyId()),
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			_serviceContext);
+	}
+
+	private ContentSetElement _toContentSetElement(BlogsEntry blogsEntry) {
+		return new ContentSetElement() {
+			{
+				id = blogsEntry.getEntryId();
+				title = blogsEntry.getTitle();
+			}
+		};
+	}
+
+	private AssetListEntry _assetListEntry;
+	private ServiceContext _serviceContext;
+
 }
