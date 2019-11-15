1e6addf8cdbffa945819feeb007968b8f4b00674
Ivica Cardic <ivica.cardic@liferay.com>
2019-11-15 16:22:22 +0100

    COMMERCE-1067 - batch-engine-api - Rename BatchEngineTaskField to BatchEngineTaskFieldId, to have consuistent names as we already have NestedFieldId that has similar role

--- modules/apps/batch-engine/batch-engine-api/src/main/java/com/liferay/batch/engine/BatchEngineTaskFieldId.java
+++ modules/apps/batch-engine/batch-engine-api/src/main/java/com/liferay/batch/engine/BatchEngineTaskFieldId.java
@@ -24,7 +24,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
-public @interface BatchEngineTaskField {
+public @interface BatchEngineTaskFieldId {
 
 	public String value();
 
