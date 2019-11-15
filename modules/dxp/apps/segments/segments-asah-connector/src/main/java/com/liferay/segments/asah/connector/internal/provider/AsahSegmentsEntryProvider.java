22b351757a3930d1ef533db5f52e7479d8bd2d90
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-11-15 11:54:41 +0100

    LPS-104666 If user is signed in, retrieve membership from synchronized data

--- modules/dxp/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/provider/AsahSegmentsEntryProvider.java
+++ modules/dxp/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/provider/AsahSegmentsEntryProvider.java
@@ -14,6 +14,7 @@
 
 package com.liferay.segments.asah.connector.internal.provider;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -24,16 +25,20 @@ import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.segments.asah.connector.internal.cache.AsahSegmentsEntryCache;
 import com.liferay.segments.asah.connector.internal.constants.SegmentsAsahDestinationNames;
 import com.liferay.segments.asah.connector.internal.context.contributor.SegmentsAsahRequestContextContributor;
 import com.liferay.segments.constants.SegmentsEntryConstants;
 import com.liferay.segments.context.Context;
+import com.liferay.segments.model.SegmentsEntry;
 import com.liferay.segments.model.SegmentsEntryRel;
 import com.liferay.segments.provider.SegmentsEntryProvider;
+import com.liferay.segments.service.SegmentsEntryLocalService;
 import com.liferay.segments.service.SegmentsEntryRelLocalService;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -92,6 +97,36 @@ public class AsahSegmentsEntryProvider implements SegmentsEntryProvider {
 
 		if (context == null) {
 			return new long[0];
+		}
+
+		if (GetterUtil.getBoolean(context.get(Context.SIGNED_IN))) {
+			List<SegmentsEntry> segmentsEntries =
+				_segmentsEntryLocalService.getSegmentsEntries(
+					groupId, true,
+					SegmentsEntryConstants.SOURCE_ASAH_FARO_BACKEND, className,
+					QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+			if (segmentsEntries.isEmpty()) {
+				return new long[0];
+			}
+
+			Stream<SegmentsEntry> stream = segmentsEntries.stream();
+
+			return stream.filter(
+				segmentsEntry ->
+					_segmentsEntryRelLocalService.hasSegmentsEntryRel(
+						segmentsEntry.getSegmentsEntryId(),
+						_portal.getClassNameId(className), classPK)
+			).sorted(
+				(segmentsEntry1, segmentsEntry2) -> {
+					Date modifiedDate = segmentsEntry2.getModifiedDate();
+
+					return modifiedDate.compareTo(
+						segmentsEntry1.getModifiedDate());
+				}
+			).mapToLong(
+				SegmentsEntry::getSegmentsEntryId
+			).toArray();
 		}
 
 		String userId = GetterUtil.getString(
@@ -163,6 +198,12 @@ public class AsahSegmentsEntryProvider implements SegmentsEntryProvider {
 
 	@Reference
 	private MessageBus _messageBus;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private SegmentsEntryLocalService _segmentsEntryLocalService;
 
 	@Reference
 	private SegmentsEntryRelLocalService _segmentsEntryRelLocalService;
