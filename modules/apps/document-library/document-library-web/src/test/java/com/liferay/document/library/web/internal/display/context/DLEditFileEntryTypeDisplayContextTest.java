db68e91e9aa1ce275fa97ab67f1031768e0417b0
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-11-15 11:26:31 +0100

    LPS-104630 Add tests.

--- modules/apps/document-library/document-library-web/src/test/java/com/liferay/document/library/web/internal/display/context/DLEditFileEntryTypeDisplayContextTest.java
+++ modules/apps/document-library/document-library-web/src/test/java/com/liferay/document/library/web/internal/display/context/DLEditFileEntryTypeDisplayContextTest.java
@@ -14,22 +14,140 @@
 
 package com.liferay.document.library.web.internal.display.context;
 
+import com.liferay.document.library.web.internal.display.context.util.MockHttpServletRequestBuilder;
+import com.liferay.dynamic.data.mapping.kernel.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.util.DDM;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.bean.BeanPropertiesImpl;
+import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderRequest;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.util.PropsImpl;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import org.mockito.Mockito;
 
 /**
  * @author Cristina González
  */
 public class DLEditFileEntryTypeDisplayContextTest {
 
+	@Before
+	public void setUp() {
+		BeanPropertiesUtil beanPropertiesUtil = new BeanPropertiesUtil();
+
+		beanPropertiesUtil.setBeanProperties(new BeanPropertiesImpl());
+
+		_ddm = Mockito.mock(DDM.class);
+
+		_ddmStructureLocalService = Mockito.mock(
+			DDMStructureLocalService.class);
+
+		PropsUtil.setProps(new PropsImpl());
+	}
+
 	@Test
-	public void test() {
+	public void testGetAvailableFields() {
 		DLEditFileEntryTypeDisplayContext dlEditFileEntryTypeDisplayContext =
-			new DLEditFileEntryTypeDisplayContext(null, null, null, null);
+			new DLEditFileEntryTypeDisplayContext(
+				_ddm, _ddmStructureLocalService, null, null);
 
 		Assert.assertEquals(
 			"Liferay.FormBuilder.AVAILABLE_FIELDS.DDM_STRUCTURE",
 			dlEditFileEntryTypeDisplayContext.getAvailableFields());
+	}
+
+	@Test
+	public void testGetFieldsJSONArrayString() {
+		DLEditFileEntryTypeDisplayContext dlEditFileEntryTypeDisplayContext =
+			new DLEditFileEntryTypeDisplayContext(
+				_ddm, _ddmStructureLocalService,
+				new MockPortletRenderRequest(
+					new MockHttpServletRequestBuilder().withAttribute(
+						WebKeys.DOCUMENT_LIBRARY_DYNAMIC_DATA_MAPPING_STRUCTURE,
+						_getRandomDDMStructure()
+					).build()),
+				null);
+
+		Assert.assertEquals(
+			StringPool.BLANK,
+			dlEditFileEntryTypeDisplayContext.getFieldsJSONArrayString());
+	}
+
+	@Test
+	public void testGetStructure() {
+		DLEditFileEntryTypeDisplayContext dlEditFileEntryTypeDisplayContext =
+			new DLEditFileEntryTypeDisplayContext(
+				_ddm, _ddmStructureLocalService,
+				new MockPortletRenderRequest(
+					new MockHttpServletRequestBuilder().withAttribute(
+						WebKeys.DOCUMENT_LIBRARY_DYNAMIC_DATA_MAPPING_STRUCTURE,
+						_getRandomDDMStructure()
+					).build()),
+				null);
+
+		DDMStructure ddmStructure =
+			dlEditFileEntryTypeDisplayContext.getDDMStructure();
+
+		Assert.assertNotNull(ddmStructure);
+		Assert.assertNotNull(ddmStructure.getStructureId());
+	}
+
+	private DDMStructure _getRandomDDMStructure() {
+		DDMStructure ddmStructure = Mockito.mock(DDMStructure.class);
+
+		long randomLong = RandomTestUtil.randomLong();
+
+		Mockito.when(
+			ddmStructure.getStructureId()
+		).thenReturn(
+			randomLong
+		);
+
+		Mockito.when(
+			ddmStructure.getDescription()
+		).thenReturn(
+			"Description" + (randomLong + 1)
+		);
+
+		return ddmStructure;
+	}
+
+	private DDM _ddm;
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+	private class MockPortletRenderRequest
+		extends MockLiferayPortletRenderRequest {
+
+		public MockPortletRenderRequest(HttpServletRequest httpServletRequest) {
+			_httpServletRequest = httpServletRequest;
+		}
+
+		@Override
+		public Object getAttribute(String name) {
+			return _httpServletRequest.getAttribute(name);
+		}
+
+		@Override
+		public HttpServletRequest getHttpServletRequest() {
+			return _httpServletRequest;
+		}
+
+		@Override
+		public String getParameter(String name) {
+			return _httpServletRequest.getParameter(name);
+		}
+
+		private final HttpServletRequest _httpServletRequest;
+
 	}
 
 }
