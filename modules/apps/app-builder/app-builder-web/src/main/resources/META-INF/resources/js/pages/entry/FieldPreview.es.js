687e003c14cf14a79ca0f6c6a327c15d733f540a
Bruno Basto <bruno.basto@liferay.com>
2019-11-18 16:19:53 -0300

    LPS-104694 Adds preview for uploaded documents

--- modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/entry/FieldPreview.es.js
+++ modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/entry/FieldPreview.es.js
@@ -14,7 +14,8 @@
 
 import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
 import ClayIcon from '@clayui/icon';
-import React from 'react';
+import {ClayTooltipProvider} from '@clayui/tooltip';
+import React, {useEffect, useState} from 'react';
 
 const getDataDefinitionField = (dataDefinition, fieldName) => {
 	return dataDefinition.dataDefinitionFields.find(field => {
@@ -38,44 +39,107 @@ const getOptionLabel = (options, value) => {
 	}, value);
 };
 
-const documentRenderer = value => {
-	const {folderId, groupId, title} = JSON.parse(value);
+const createFileEntryPreviewURL = (groupId, fileEntryId) => {
+	const portletURL = Liferay.PortletURL.createURL(
+		themeDisplay.getLayoutRelativeControlPanelURL()
+	);
 
-	const getDocumentIcon = fileName => {
-		const extension = fileName.split('.').pop();
+	portletURL.setParameter('doAsGroupId', groupId);
+	portletURL.setParameter('fileEntryId', fileEntryId);
+	portletURL.setParameter(
+		'mvcRenderCommandName',
+		'/document_library/view_file_entry'
+	);
+	portletURL.setParameter('p_p_auth', Liferay.authToken);
+	portletURL.setParameter('refererGroupId', themeDisplay.getScopeGroupId());
 
-		if (extension === 'pdf') {
-			return 'document-pdf';
-		}
+	portletURL.setPortletId(Liferay.PortletKeys.DOCUMENT_LIBRARY);
+	portletURL.setPortletMode('view');
+	portletURL.setWindowState('pop_up');
 
-		if (extension === 'doc' || extension === 'txt') {
-			return 'document-text';
-		}
+	return portletURL.toString();
+};
 
-		return 'document-default';
+const getDocumentIcon = fileName => {
+	const extension = fileName.split('.').pop();
+
+	if (extension === 'pdf') {
+		return 'document-pdf';
+	}
+
+	if (extension === 'doc' || extension === 'txt') {
+		return 'document-text';
+	}
+
+	return 'document-default';
+};
+
+const DocumentRenderer = ({value = {}}) => {
+	let fileEntry = {};
+
+	if (typeof value === 'string' && value.startsWith('{')) {
+		fileEntry = JSON.parse(value);
+	}
+
+	const {fileEntryId, folderId, groupId, title = ''} = fileEntry;
+	const [previewURL, setPreviewURL] = useState('');
+
+	useEffect(() => {
+		AUI().use('liferay-portlet-url', () => {
+			setPreviewURL(createFileEntryPreviewURL(groupId, fileEntryId));
+		});
+	});
+
+	const onClickPreview = () => {
+		Liferay.Util.openWindow({
+			dialog: {
+				destroyOnHide: true,
+				modal: true
+			},
+			title: Liferay.Language.get('file-preview'),
+			uri: previewURL
+		});
 	};
 
-	const onClick = () => {
+	const onClickDownload = () => {
 		location.href = `${themeDisplay.getPathContext()}/documents/${groupId}/${folderId ||
 			'0'}/${encodeURIComponent(title)}?download=true`;
 	};
 
 	return (
-		<ClayButton.Group className="data-record-document-field">
-			<ClayButton displayType="secondary" onClick={onClick}>
-				<ClayIcon className="mr-2" symbol={getDocumentIcon(title)} />
-				{title}
-			</ClayButton>
-			<ClayButtonWithIcon
-				displayType="secondary"
-				onClick={onClick}
-				symbol="download"
-			/>
-		</ClayButton.Group>
+		<>
+			{fileEntryId && (
+				<ClayTooltipProvider>
+					<ClayButton.Group className="data-record-document-field">
+						<ClayButton
+							data-tooltip-align="bottom"
+							data-tooltip-delay="200"
+							displayType="secondary"
+							onClick={onClickPreview}
+							title={Liferay.Language.get('file-preview')}
+						>
+							<ClayIcon
+								className="mr-2"
+								symbol={getDocumentIcon(title)}
+							/>
+							{title}
+						</ClayButton>
+						<ClayButtonWithIcon
+							data-tooltip-align="bottom"
+							data-tooltip-delay="200"
+							displayType="secondary"
+							onClick={onClickDownload}
+							symbol="download"
+							title={Liferay.Language.get('download')}
+						/>
+					</ClayButton.Group>
+				</ClayTooltipProvider>
+			)}
+		</>
 	);
 };
 
-const optionsRenderer = (options, values) => (
+const OptionsRenderer = ({options, values}) => (
 	<ul>
 		{values.map((value, index) => (
 			<li key={index}>{getOptionLabel(options, value)}</li>
@@ -83,7 +147,7 @@ const optionsRenderer = (options, values) => (
 	</ul>
 );
 
-const stringRenderer = ({value}) => <p>{value}</p>;
+const StringRenderer = ({value}) => <p>{value}</p>;
 
 const getFieldValueRenderer = dataDefinitionField => {
 	const {customProperties, fieldType} = dataDefinitionField;
@@ -91,24 +155,42 @@ const getFieldValueRenderer = dataDefinitionField => {
 	if (fieldType === 'checkbox_multiple') {
 		const {options} = customProperties;
 
-		return ({value}) => optionsRenderer(options, value);
+		return ({value}) => (
+			<OptionsRenderer options={options} values={value} />
+		);
+	}
+
+	if (fieldType === 'document_library') {
+		return ({value}) => <DocumentRenderer value={value} />;
+	}
+
+	if (fieldType === 'grid') {
+		return ({value}) => <StringRenderer value={Object.keys(value)} />;
+	}
+
+	if (fieldType === 'radio') {
+		const {options} = customProperties;
+
+		return ({value}) => (
+			<StringRenderer value={getOptionLabel(options, value)} />
+		);
 	}
 
 	if (fieldType === 'select') {
 		const {multiple, options} = customProperties;
 
 		if (multiple) {
-			return ({value}) => optionsRenderer(options, value);
+			return ({value}) => (
+				<OptionsRenderer options={options} values={value} />
+			);
 		}
 
-		return ({value}) => stringRenderer({value: value[0] || ''});
+		return ({value}) => (
+			<StringRenderer value={getOptionLabel(options, value[0] || '')} />
+		);
 	}
 
-	if (fieldType === 'document_library') {
-		return ({value}) => documentRenderer(value);
-	}
-
-	return stringRenderer;
+	return ({value}) => <StringRenderer value={value} />;
 };
 
 export default ({dataDefinition, dataRecordValues, fieldName}) => {
