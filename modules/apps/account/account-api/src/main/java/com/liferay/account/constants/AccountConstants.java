070625c65d85765f46fd2d221fd3c29d89b88fb3
pei-jung <pei-jung.lan@liferay.com>
2019-11-18 16:57:12 -0800

    LPS-104594 Add new constant for ANY account entry

--- modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountConstants.java
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountConstants.java
@@ -19,6 +19,8 @@ package com.liferay.account.constants;
  */
 public class AccountConstants {
 
+	public static final long ACCOUNT_ENTRY_ID_ANY = -1;
+
 	public static final long ACCOUNT_ENTRY_ID_DEFAULT = 0;
 
 	public static final String RESOURCE_NAME = "com.liferay.account";
