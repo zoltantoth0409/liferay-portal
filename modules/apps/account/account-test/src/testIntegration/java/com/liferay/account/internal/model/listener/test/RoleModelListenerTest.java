7b1e6b55f67c3f80e936af7ebc439e2ebacc2594
drewbrokke <drew.brokke@liferay.com>
2019-11-19 14:50:56 -0600

    LPS-99914 account-test - adds tests for RoleModelListener

---
+++ modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/internal/model/listener/test/RoleModelListenerTest.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.internal.model.listener.test;
+
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.model.AccountRole;
+import com.liferay.account.service.AccountEntryLocalService;
+import com.liferay.account.service.AccountRoleLocalService;
+import com.liferay.account.service.test.AccountEntryTestUtil;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.exception.RequiredRoleException;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Drew Brokke
+ */
+@RunWith(Arquillian.class)
+public class RoleModelListenerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_company = CompanyTestUtil.addCompany();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (_companyLocalService.fetchCompany(_company.getCompanyId()) !=
+				null) {
+
+			_companyLocalService.deleteCompany(_company);
+		}
+	}
+
+	@Test(expected = ModelListenerException.class)
+	public void testDeleteRole() throws Exception {
+		_accountEntry = AccountEntryTestUtil.addAccountEntry(
+			_accountEntryLocalService, WorkflowConstants.STATUS_APPROVED);
+
+		_accountRole = _accountRoleLocalService.addAccountRole(
+			TestPropsValues.getUserId(), _accountEntry.getAccountEntryId(),
+			RandomTestUtil.randomString(), null, null);
+
+		try {
+			_roleLocalService.deleteRole(_accountRole.getRoleId());
+
+			Assert.fail(
+				"Should not be able to directly delete a role associated " +
+					"with an AccountRole");
+		}
+		catch (ModelListenerException mle) {
+			Throwable throwable = mle.getCause();
+
+			Assert.assertTrue(throwable instanceof RequiredRoleException);
+
+			String message = throwable.getMessage();
+
+			Assert.assertTrue(
+				message.contains("is required by an AccountRole"));
+
+			throw mle;
+		}
+	}
+
+	@DeleteAfterTestRun
+	private AccountEntry _accountEntry;
+
+	@Inject
+	private AccountEntryLocalService _accountEntryLocalService;
+
+	@DeleteAfterTestRun
+	private AccountRole _accountRole;
+
+	@Inject
+	private AccountRoleLocalService _accountRoleLocalService;
+
+	private Company _company;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@Inject
+	private RoleLocalService _roleLocalService;
+
+}
