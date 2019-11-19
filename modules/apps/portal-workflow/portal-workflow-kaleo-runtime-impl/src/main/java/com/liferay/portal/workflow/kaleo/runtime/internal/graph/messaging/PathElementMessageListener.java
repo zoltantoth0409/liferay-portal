192f15006491c9f1d1ef7ff0da4411c57cfd1e89
Preston-Crary <preston.crary@liferay.com>
2019-11-18 18:00:03 -0800

    LPS-104523 Walk the tree in a single thread to prevent concurrent updates to the same KaleoInstance row causing StaleObjectStateExceptions.

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/graph/messaging/PathElementMessageListener.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/graph/messaging/PathElementMessageListener.java
@@ -22,8 +22,11 @@ import com.liferay.portal.workflow.kaleo.runtime.constants.KaleoRuntimeDestinati
 import com.liferay.portal.workflow.kaleo.runtime.graph.GraphWalker;
 import com.liferay.portal.workflow.kaleo.runtime.graph.PathElement;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -40,21 +43,24 @@ public class PathElementMessageListener extends BaseMessageListener {
 
 	@Override
 	protected void doReceive(Message message) throws Exception {
-		PathElement pathElement = (PathElement)message.getPayload();
+		Queue<List<PathElement>> queue = new ArrayDeque<>();
 
-		List<PathElement> remainingPathElements = new ArrayList<>();
+		queue.add(Collections.singletonList((PathElement)message.getPayload()));
 
-		_graphWalker.follow(
-			pathElement.getStartNode(), pathElement.getTargetNode(),
-			remainingPathElements, pathElement.getExecutionContext());
+		while (!queue.isEmpty()) {
+			List<PathElement> pathElements = queue.remove();
 
-		for (PathElement remainingPathElement : remainingPathElements) {
-			message = new Message();
+			for (PathElement pathElement : pathElements) {
+				List<PathElement> remainingPathElements = new ArrayList<>();
 
-			message.setPayload(remainingPathElement);
+				_graphWalker.follow(
+					pathElement.getStartNode(), pathElement.getTargetNode(),
+					remainingPathElements, pathElement.getExecutionContext());
 
-			_messageBus.sendMessage(
-				KaleoRuntimeDestinationNames.KALEO_GRAPH_WALKER, message);
+				if (!remainingPathElements.isEmpty()) {
+					queue.add(remainingPathElements);
+				}
+			}
 		}
 	}
 
