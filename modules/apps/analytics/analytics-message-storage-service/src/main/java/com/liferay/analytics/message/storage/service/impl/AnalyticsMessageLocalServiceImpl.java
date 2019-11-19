0969b7b6b5ca6cc48dda23fab8fd6343c4a61fe9
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2019-11-19 11:21:36 -0800

    LRAC-4406 addAnalyticsMessage

--- modules/apps/analytics/analytics-message-storage-service/src/main/java/com/liferay/analytics/message/storage/service/impl/AnalyticsMessageLocalServiceImpl.java
+++ modules/apps/analytics/analytics-message-storage-service/src/main/java/com/liferay/analytics/message/storage/service/impl/AnalyticsMessageLocalServiceImpl.java
@@ -14,14 +14,26 @@
 
 package com.liferay.analytics.message.storage.service.impl;
 
+import com.liferay.analytics.message.storage.model.AnalyticsMessage;
 import com.liferay.analytics.message.storage.service.base.AnalyticsMessageLocalServiceBaseImpl;
+import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.dao.jdbc.OutputBlob;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * The implementation of the analytics message local service.
+ *
  * @author Brian Wing Shun Chan
+ * @author Rachael Koestartyo
  */
 @Component(
 	property = "model.class.name=com.liferay.analytics.message.storage.model.AnalyticsMessage",
@@ -29,4 +41,34 @@ import org.osgi.service.component.annotations.Component;
 )
 public class AnalyticsMessageLocalServiceImpl
 	extends AnalyticsMessageLocalServiceBaseImpl {
+
+	public AnalyticsMessage addAnalyticsMessage(
+			long companyId, long userId, byte[] body)
+		throws PortalException {
+
+		long analyticsMessageId = _counterLocalService.increment();
+
+		User user = _userLocalService.getUser(userId);
+
+		AnalyticsMessage analyticsMessage = analyticsMessagePersistence.create(
+			analyticsMessageId);
+
+		analyticsMessage.setCompanyId(companyId);
+		analyticsMessage.setUserId(userId);
+		analyticsMessage.setUserName(user.getFullName());
+		analyticsMessage.setCreateDate(new Date());
+		analyticsMessage.setBody(
+			new OutputBlob(new UnsyncByteArrayInputStream(body), body.length));
+
+		analyticsMessagePersistence.update(analyticsMessage);
+
+		return analyticsMessage;
+	}
+
+	@Reference
+	private CounterLocalService _counterLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
