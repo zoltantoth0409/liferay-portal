50306472bdacec4f3f239395a8f76f247f819850
drewbrokke <drew.brokke@liferay.com>
2019-11-19 14:51:33 -0600

    LPS-99914 account-test - adds tests for CompanyModelListener

---
+++ modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/internal/model/listener/test/CompanyModelListenerTest.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.internal.model.listener.test;
+
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.model.AccountRole;
+import com.liferay.account.service.AccountEntryLocalService;
+import com.liferay.account.service.AccountRoleLocalService;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Drew Brokke
+ */
+@RunWith(Arquillian.class)
+public class CompanyModelListenerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_company = CompanyTestUtil.addCompany();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if (_companyLocalService.fetchCompany(_company.getCompanyId()) !=
+				null) {
+
+			_companyLocalService.deleteCompany(_company);
+		}
+	}
+
+	@Test
+	public void testCleanUpAccountRoles() throws Exception {
+		User defaultUser = _company.getDefaultUser();
+
+		AccountEntry accountEntry = _accountEntryLocalService.addAccountEntry(
+			defaultUser.getUserId(), 0L, RandomTestUtil.randomString(50),
+			RandomTestUtil.randomString(50), null, null,
+			WorkflowConstants.STATUS_APPROVED);
+
+		AccountRole accountRole = _accountRoleLocalService.addAccountRole(
+			defaultUser.getUserId(), accountEntry.getAccountEntryId(),
+			RandomTestUtil.randomString(), null, null);
+
+		_companyLocalService.deleteCompany(_company);
+
+		Assert.assertNull(
+			_accountRoleLocalService.fetchAccountRole(
+				accountRole.getAccountRoleId()));
+	}
+
+	@Inject
+	private AccountEntryLocalService _accountEntryLocalService;
+
+	@Inject
+	private AccountRoleLocalService _accountRoleLocalService;
+
+	private Company _company;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+}
