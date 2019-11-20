27b8a9b9724af2319f01c38a50dd42443329c4f5
Leon Chi <leon.chi@liferay.com>
2019-11-20 13:46:52 +0800

    LPS-104613 Remove the deprecated method from AssetEntryLocalServiceImpl

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
@@ -883,31 +883,6 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 		return entry;
 	}
 
-	/**
-	 * @deprecated As of Judson (7.1.x), replaced by {@link #updateEntry(long,
-	 *             long, Date, Date, String, long, String, long, long[],
-	 *             String[], boolean, boolean, Date, Date, Date, Date, String,
-	 *             String, String, String, String, String, int, int, Double)}
-	 */
-	@Deprecated
-	@Override
-	public AssetEntry updateEntry(
-			long userId, long groupId, Date createDate, Date modifiedDate,
-			String className, long classPK, String classUuid, long classTypeId,
-			long[] categoryIds, String[] tagNames, boolean listable,
-			boolean visible, Date startDate, Date endDate, Date expirationDate,
-			String mimeType, String title, String description, String summary,
-			String url, String layoutUuid, int height, int width,
-			Double priority)
-		throws PortalException {
-
-		return assetEntryLocalService.updateEntry(
-			userId, groupId, createDate, modifiedDate, className, classPK,
-			classUuid, classTypeId, categoryIds, tagNames, true, visible,
-			startDate, endDate, null, expirationDate, mimeType, title,
-			description, summary, url, layoutUuid, height, width, priority);
-	}
-
 	@Override
 	public AssetEntry updateEntry(
 			long userId, long groupId, String className, long classPK,
