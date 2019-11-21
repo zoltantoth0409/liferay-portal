7254a70c8c84dfcfdc32ab4db97e8170e4be2ea6
Samuel Tran <samuel.tran@liferay.com>
2019-11-20 16:03:33 -0800

    LPS-101085 Fix comparator

--- modules/apps/knowledge-base/knowledge-base-api/src/main/java/com/liferay/knowledge/base/util/comparator/KBObjectsViewCountComparator.java
+++ modules/apps/knowledge-base/knowledge-base-api/src/main/java/com/liferay/knowledge/base/util/comparator/KBObjectsViewCountComparator.java
@@ -29,7 +29,7 @@ public class KBObjectsViewCountComparator<T> extends OrderByComparator<T> {
 	public static final String ORDER_BY_DESC =
 		"modelFolder DESC, viewCount DESC, title ASC";
 
-	public static final String[] ORDER_BY_FIELDS = {"viewCount, title"};
+	public static final String[] ORDER_BY_FIELDS = {"viewCount", "title"};
 
 	public KBObjectsViewCountComparator() {
 		this(false);
