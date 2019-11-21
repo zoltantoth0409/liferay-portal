f89e34d487c0a308deebf1af3e2d86c9d80e9627
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2019-11-20 17:13:13 -0800

    LRAC-4405 schedule to run every 1 hour

---
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/messaging/SendAnalyticsMessagesMessageListener.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.message.sender.internal.messaging;
+
+import com.liferay.analytics.message.sender.client.AnalyticsMessageSenderClient;
+import com.liferay.analytics.message.storage.model.AnalyticsMessage;
+import com.liferay.analytics.message.storage.service.AnalyticsMessageLocalService;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.SchedulerEntry;
+import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.Trigger;
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
+
+import java.sql.Blob;
+
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component(
+	immediate = true, service = SendAnalyticsMessagesMessageListener.class
+)
+public class SendAnalyticsMessagesMessageListener extends BaseMessageListener {
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		Class<?> clazz = getClass();
+
+		String className = clazz.getName();
+
+		Trigger trigger = _triggerFactory.createTrigger(
+			className, className, null, null, 1, TimeUnit.HOUR);
+
+		SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
+			className, trigger);
+
+		_schedulerEngineHelper.register(
+			this, schedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_schedulerEngineHelper.unregister(this);
+	}
+
+	@Override
+	protected void doReceive(Message message) throws Exception {
+		List<Long> companyIds = _analyticsMessageLocalService.getCompanyIds();
+
+		for (long companyId : companyIds) {
+			_process(companyId);
+		}
+	}
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+	}
+
+	private void _process(long companyId) throws Exception {
+		int count = _analyticsMessageLocalService.getAnalyticsMessagesCount(
+			companyId);
+
+		int pages = count / _BATCH_SIZE;
+
+		for (int i = 0; i <= pages; i++) {
+			int start = i * _BATCH_SIZE;
+
+			int end = start + _BATCH_SIZE - 1;
+
+			if (count < end) {
+				end = count;
+			}
+
+			JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+			List<AnalyticsMessage> analyticsMessages =
+				_analyticsMessageLocalService.getAnalyticsMessages(
+					companyId, start, end);
+
+			for (AnalyticsMessage analyticsMessage : analyticsMessages) {
+				Blob body = analyticsMessage.getBody();
+
+				byte[] bodyBytes = body.getBytes(1, (int)body.length());
+
+				JSONObject bodyJSONObject = JSONFactoryUtil.createJSONObject(
+					new String(bodyBytes));
+
+				jsonArray.put(bodyJSONObject);
+			}
+
+			if (jsonArray.length() > 0) {
+				_analyticsMessageSenderClient.send(
+					jsonArray.toString(), companyId);
+
+				_analyticsMessageLocalService.deleteAnalyticsMessages(
+					analyticsMessages);
+			}
+		}
+	}
+
+	private static final int _BATCH_SIZE = 100;
+
+	@Reference
+	private AnalyticsMessageLocalService _analyticsMessageLocalService;
+
+	@Reference
+	private AnalyticsMessageSenderClient _analyticsMessageSenderClient;
+
+	@Reference
+	private SchedulerEngineHelper _schedulerEngineHelper;
+
+	@Reference
+	private TriggerFactory _triggerFactory;
+
+}
