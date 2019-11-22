6e07fb3e77f3d9fb13b67a3d69da262924bc3433
Inácio Nery <inacio.nery@liferay.com>
2019-11-22 10:05:15 -0300

    LPS-104155 | LPS-104156 Add KaleoActionLocalServiceTest

---
+++ modules/apps/portal-workflow/portal-workflow-kaleo-test/src/testIntegration/java/com/liferay/portal/workflow/kaleo/service/test/KaleoActionLocalServiceTest.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.workflow.kaleo.model.KaleoAction;
+import com.liferay.portal.workflow.kaleo.model.KaleoInstance;
+import com.liferay.portal.workflow.kaleo.model.KaleoNode;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Inácio Nery
+ */
+@RunWith(Arquillian.class)
+public class KaleoActionLocalServiceTest extends BaseKaleoLocalServiceTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testGetKaleoActions() throws Exception {
+		long companyId = TestPropsValues.getCompanyId();
+
+		KaleoInstance kaleoInstance = addKaleoInstance();
+
+		KaleoNode kaleoNode = addKaleoNode(kaleoInstance);
+
+		KaleoAction kaleoAction = addKaleoAction(kaleoInstance, kaleoNode);
+
+		List<KaleoAction> kaleoActions =
+			kaleoActionLocalService.getKaleoActions(
+				companyId, KaleoNode.class.getName(),
+				kaleoNode.getKaleoNodeId());
+
+		Assert.assertEquals(kaleoActions.toString(), 1, kaleoActions.size());
+
+		Assert.assertEquals(kaleoAction, kaleoActions.get(0));
+
+		kaleoActions = kaleoActionLocalService.getKaleoActions(
+			companyId, KaleoNode.class.getName(), kaleoNode.getKaleoNodeId(),
+			"onAssignment");
+
+		Assert.assertEquals(kaleoActions.toString(), 1, kaleoActions.size());
+
+		Assert.assertEquals(kaleoAction, kaleoActions.get(0));
+	}
+
+}
