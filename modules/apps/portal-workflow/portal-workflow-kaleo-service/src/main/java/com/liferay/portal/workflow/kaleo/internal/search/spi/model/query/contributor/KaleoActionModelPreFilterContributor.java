724dd34a3fb644ca949bbb2f32e03fcb16e49aa4
Inácio Nery <inacio.nery@liferay.com>
2019-11-22 10:01:38 -0300

    LPS-104155 | LPS-104156 Implement the new indexing API for KaleoAction

---
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/search/spi/model/query/contributor/KaleoActionModelPreFilterContributor.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.internal.search.spi.model.query.contributor;
+
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Inácio Nery
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.portal.workflow.kaleo.model.KaleoAction",
+	service = ModelPreFilterContributor.class
+)
+public class KaleoActionModelPreFilterContributor
+	implements ModelPreFilterContributor {
+
+	@Override
+	public void contribute(
+		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
+		SearchContext searchContext) {
+
+		long executionType = GetterUtil.getLong(
+			searchContext.getAttribute("executionType"));
+
+		if (executionType > 0) {
+			booleanFilter.addRequiredTerm("executionType", executionType);
+		}
+
+		long kaleoClassPK = GetterUtil.getLong(
+			searchContext.getAttribute("kaleoClassPK"));
+
+		if (kaleoClassPK > 0) {
+			booleanFilter.addRequiredTerm("kaleoClassPK", kaleoClassPK);
+		}
+	}
+
+}
