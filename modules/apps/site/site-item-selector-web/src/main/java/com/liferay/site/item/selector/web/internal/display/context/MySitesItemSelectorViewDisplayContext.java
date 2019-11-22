9e20ca908f6d05f0c15e76df96a06d4a4998a0f5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-11-22 13:59:18 +0100

    LPS-104428 Uses method getPortletURL since this method clones the url instead of using the same url

--- modules/apps/site/site-item-selector-web/src/main/java/com/liferay/site/item/selector/web/internal/display/context/MySitesItemSelectorViewDisplayContext.java
+++ modules/apps/site/site-item-selector-web/src/main/java/com/liferay/site/item/selector/web/internal/display/context/MySitesItemSelectorViewDisplayContext.java
@@ -14,7 +14,6 @@
 
 package com.liferay.site.item.selector.web.internal.display.context;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -52,12 +51,13 @@ public class MySitesItemSelectorViewDisplayContext
 		_groupSearchProvider = groupSearchProvider;
 		_portletRequest = getPortletRequest();
 
-		addBreadcrumbEntries(portletURL);
+		addBreadcrumbEntries();
 	}
 
 	@Override
-	public GroupSearch getGroupSearch() throws PortalException {
-		return _groupSearchProvider.getGroupSearch(_portletRequest, portletURL);
+	public GroupSearch getGroupSearch() throws Exception {
+		return _groupSearchProvider.getGroupSearch(
+			_portletRequest, getPortletURL());
 	}
 
 	@Override
@@ -70,17 +70,20 @@ public class MySitesItemSelectorViewDisplayContext
 		return true;
 	}
 
-	protected void addBreadcrumbEntries(PortletURL portletURL) {
+	protected void addBreadcrumbEntries() {
 		Group group = getGroup();
 
 		if (group == null) {
 			return;
 		}
 
-		PortalUtil.addPortletBreadcrumbEntry(
-			request, LanguageUtil.get(request, "all"), portletURL.toString());
-
 		try {
+			PortletURL portletURL = getPortletURL();
+
+			PortalUtil.addPortletBreadcrumbEntry(
+				request, LanguageUtil.get(request, "all"),
+				portletURL.toString());
+
 			SitesUtil.addPortletBreadcrumbEntries(group, request, portletURL);
 		}
 		catch (Exception e) {
