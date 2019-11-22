d4cda5ae7dcbaae233fdc8a5faba3624fea4784f
Olivia Yu <olivia.yu@liferay.com>
2019-11-22 12:23:28 -0800

    LPS-104772 portal-search-tuning-rankings-web: Add testing for AddResultSearchBar

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/test/js/components/add_result/AddResultSearchBar.es.js
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/test/js/components/add_result/AddResultSearchBar.es.js
@@ -9,7 +9,7 @@
  * distribution rights of the Software.
  */
 
-import {render} from '@testing-library/react';
+import {fireEvent, render} from '@testing-library/react';
 import React from 'react';
 
 import AddResultSearchBar from '../../../../src/main/resources/META-INF/resources/js/components/add_result/AddResultSearchBar.es';
@@ -18,19 +18,56 @@ import '@testing-library/jest-dom/extend-expect';
 
 const TEST_TEXT = 'search query test';
 
+function renderTestAddResultSearchBar(props) {
+	return render(
+		<AddResultSearchBar
+			onSearchKeyDown={jest.fn()}
+			onSearchQueryChange={jest.fn()}
+			onSearchSubmit={jest.fn()}
+			searchQuery={TEST_TEXT}
+			{...props}
+		/>
+	);
+}
+
 describe('AddResult', () => {
 	it('shows a search input', () => {
-		const {getByPlaceholderText} = render(
-			<AddResultSearchBar
-				onSearchKeyDown={jest.fn()}
-				onSearchQueryChange={jest.fn()}
-				onSearchSubmit={jest.fn()}
-				searchQuery={TEST_TEXT}
-			/>
-		);
+		const {getByPlaceholderText} = renderTestAddResultSearchBar();
 
 		const input = getByPlaceholderText('search-the-engine');
 
 		expect(input.value).toEqual(TEST_TEXT);
+	});
+
+	it('calls the onSearchKeyDown function when a key is pressed in the search field', () => {
+		const onSearchKeyDown = jest.fn();
+
+		const {getByPlaceholderText} = renderTestAddResultSearchBar({
+			onSearchKeyDown
+		});
+
+		const input = getByPlaceholderText('search-the-engine');
+
+		fireEvent.change(input, {target: {value: ' '}});
+
+		fireEvent.keyDown(input, {key: 'Enter', keyCode: 13, which: 13});
+
+		expect(onSearchKeyDown.mock.calls.length).toBe(1);
+	});
+
+	it('calls the onSearchQueryChange function when the search field changes', () => {
+		const onSearchQueryChange = jest.fn();
+
+		const {getByPlaceholderText} = renderTestAddResultSearchBar({
+			onSearchQueryChange
+		});
+
+		const input = getByPlaceholderText('search-the-engine');
+
+		fireEvent.change(input, {target: {value: ' '}});
+
+		fireEvent.keyDown(input, {key: 'Enter', keyCode: 13, which: 13});
+
+		expect(onSearchQueryChange.mock.calls.length).toBe(1);
 	});
 });
