2e6ceac713bb7d22e9cde89708ae7cb5d674ee54
Alicia Garcia <alicia.garcia@liferay.com>
2019-11-25 15:38:30 +0100

    LPS-104840 ddm support for LayoutSEOEntry

---
+++ modules/apps/layout/layout-seo-service/src/main/java/com/liferay/layout/seo/internal/dynamic/data/mapping/util/LayoutDDMStructurePermissionSupport.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.seo.internal.dynamic.data.mapping.util;
+
+import com.liferay.dynamic.data.mapping.util.DDMStructurePermissionSupport;
+import com.liferay.layout.seo.model.LayoutSEOEntry;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alicia Garcia
+ */
+@Component(
+	property = "model.class.name=com.liferay.layout.seo.model.LayoutSEOEntry",
+	service = DDMStructurePermissionSupport.class
+)
+public class LayoutDDMStructurePermissionSupport
+	implements DDMStructurePermissionSupport {
+
+	@Override
+	public String getResourceName() {
+		return LayoutSEOEntry.class.getName();
+	}
+
+}
