6b968bffb0edc1421b2610277ad0217191546d54
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-11-25 22:13:54 +0100

    LPS-104627 Update data url

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/servlet/taglib/clay/SelectDisplayPageMasterLayoutVerticalCard.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/servlet/taglib/clay/SelectDisplayPageMasterLayoutVerticalCard.java
@@ -15,21 +15,18 @@
 package com.liferay.layout.page.template.admin.web.internal.servlet.taglib.clay;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.soy.VerticalCard;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.WebKeys;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Eudaldo Alonso
@@ -43,45 +40,30 @@ public class SelectDisplayPageMasterLayoutVerticalCard implements VerticalCard {
 		_layoutPageTemplateEntry = layoutPageTemplateEntry;
 		_renderResponse = renderResponse;
 
-		_httpServletRequest = PortalUtil.getHttpServletRequest(renderRequest);
 		_themeDisplay = (ThemeDisplay)renderRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 	}
 
 	@Override
 	public Map<String, String> getData() {
-		Map<String, String> data = new HashMap<>();
+		PortletURL addDisplayPageURL = _renderResponse.createActionURL();
 
-		String redirect = ParamUtil.getString(_httpServletRequest, "redirect");
+		addDisplayPageURL.setParameter(
+			ActionRequest.ACTION_NAME,
+			"/layout_page_template/add_display_page");
+		addDisplayPageURL.setParameter(
+			"backURL", _themeDisplay.getURLCurrent());
+		addDisplayPageURL.setParameter(
+			"type",
+			String.valueOf(
+				LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE));
+		addDisplayPageURL.setParameter(
+			"masterLayoutPlid",
+			String.valueOf(_layoutPageTemplateEntry.getPlid()));
 
-		long layoutPageTemplateCollectionId = ParamUtil.getLong(
-			_httpServletRequest, "layoutPageTemplateCollectionId");
-
-		try {
-			PortletURL addLayoutPageTemplateEntryURL =
-				_renderResponse.createActionURL();
-
-			addLayoutPageTemplateEntryURL.setParameter(
-				ActionRequest.ACTION_NAME,
-				"/layout_page_template/add_layout_page_template_entry");
-			addLayoutPageTemplateEntryURL.setParameter(
-				"redirect", _themeDisplay.getURLCurrent());
-			addLayoutPageTemplateEntryURL.setParameter("backURL", redirect);
-			addLayoutPageTemplateEntryURL.setParameter(
-				"layoutPageTemplateCollectionId",
-				String.valueOf(layoutPageTemplateCollectionId));
-			addLayoutPageTemplateEntryURL.setParameter(
-				"masterLayoutPlid",
-				String.valueOf(_layoutPageTemplateEntry.getPlid()));
-
-			data.put(
-				"add-layout-page-template-entry-url",
-				addLayoutPageTemplateEntryURL.toString());
-		}
-		catch (Exception e) {
-		}
-
-		return data;
+		return HashMapBuilder.put(
+			"add-display-page-url", addDisplayPageURL.toString()
+		).build();
 	}
 
 	@Override
@@ -110,7 +92,6 @@ public class SelectDisplayPageMasterLayoutVerticalCard implements VerticalCard {
 		return false;
 	}
 
-	private final HttpServletRequest _httpServletRequest;
 	private final LayoutPageTemplateEntry _layoutPageTemplateEntry;
 	private final RenderResponse _renderResponse;
 	private final ThemeDisplay _themeDisplay;
