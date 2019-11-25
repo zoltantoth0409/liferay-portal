4983d491008dd8eb8d8c1fa53520aba9f26a43c8
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2019-11-25 18:25:37 +0100

    LPS-101720 Email OTP configuration category

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-checker-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/checker/email/otp/web/internal/configuration/admin/category/MultiFactorAuthenticationConfigurationCategory.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.checker.email.otp.web.internal.configuration.admin.category;
+
+import com.liferay.configuration.admin.category.ConfigurationCategory;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(service = ConfigurationCategory.class)
+public class MultiFactorAuthenticationConfigurationCategory
+	implements ConfigurationCategory {
+
+	@Override
+	public String getCategoryIcon() {
+		return _CATEGORY_ICON;
+	}
+
+	@Override
+	public String getCategoryKey() {
+		return _CATEGORY_KEY;
+	}
+
+	@Override
+	public String getCategorySection() {
+		return _CATEGORY_SECTION;
+	}
+
+	private static final String _CATEGORY_ICON = "lock";
+
+	private static final String _CATEGORY_KEY = "multi-factor-authentication";
+
+	private static final String _CATEGORY_SECTION = "security";
+
+}
