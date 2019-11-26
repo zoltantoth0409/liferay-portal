bbe09fe8e3596fbcc2e2b340915da22ed9ac8148
Brian Chan <brian.chan@liferay.com>
2019-11-26 14:07:19 -0800

    LPS-102995 SF

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/model/impl/CTMessageImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/model/impl/CTMessageImpl.java
@@ -15,21 +15,10 @@
 package com.liferay.change.tracking.model.impl;
 
 /**
- * The extended model implementation for the CTMessage service. Represents a row in the &quot;CTMessage&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.change.tracking.model.CTMessage</code> interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 public class CTMessageImpl extends CTMessageBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a ct message model instance should use the {@link com.liferay.change.tracking.model.CTMessage} interface instead.
-	 */
 	public CTMessageImpl() {
 	}
 
