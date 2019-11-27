70a4c6dd13797625734aac27a4f4c9cea953c8c9
Brian Chan <brian.chan@liferay.com>
2019-11-26 18:59:31 -0800

    LPS-104630 Map is fine

--- modules/apps/document-library/document-library-web/src/test/java/com/liferay/document/library/web/internal/display/context/util/MockHttpServletRequestBuilder.java
+++ modules/apps/document-library/document-library-web/src/test/java/com/liferay/document/library/web/internal/display/context/util/MockHttpServletRequestBuilder.java
@@ -59,7 +59,7 @@ public class MockHttpServletRequestBuilder {
 		return this;
 	}
 
-	private HashMap<String, Object> _attributes = new HashMap<>();
-	private HashMap<String, String> _parameters = new HashMap<>();
+	private Map<String, Object> _attributes = new HashMap<>();
+	private Map<String, String> _parameters = new HashMap<>();
 
 }
