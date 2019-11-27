d439493aaa30a4a98f6467a8650bc4e0e6146934
Daniel Couso <daniel.couso@liferay.com>
2019-11-27 15:44:55 +0100

    LPS-82798 Remove Content Pages from branching

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/LayoutStagingImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/LayoutStagingImpl.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.LayoutRevision;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetBranch;
@@ -111,7 +112,11 @@ public class LayoutStagingImpl implements LayoutStaging {
 
 	@Override
 	public boolean isBranchingLayout(Layout layout) {
-		if ((layout == null) || layout.isSystem()) {
+		String layoutType = layout.getType();
+
+		if ((layout == null) || layout.isSystem() ||
+			LayoutConstants.TYPE_CONTENT.equals(layoutType)) {
+
 			return false;
 		}
 
