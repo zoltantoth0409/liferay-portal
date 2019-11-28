dab52ff8619352815469cd10bd51c18127631a3f
lily.chi <lily.chi@liferay.com>
2019-11-28 10:06:21 +0800

    LPS-104416 Add PropsValues and PropsKeys

---
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/PropsKeys.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.sample.sql.builder;
+
+/**
+ * @author Lily Chi
+ */
+public interface PropsKeys {
+
+	public static final String DB_TYPE = "sample.sql.db.type";
+
+	public static final String MAX_ASSET_CATEGORY_COUNT =
+		"sample.sql.max.asset.category.count";
+
+	public static final String MAX_ASSET_ENTRY_TO_ASSET_CATEGORY_COUNT =
+		"sample.sql.max.asset.entry.to.asset.category.count";
+
+	public static final String MAX_ASSET_ENTRY_TO_ASSET_TAG_COUNT =
+		"sample.sql.max.asset.entry.to.asset.tag.count";
+
+	public static final String MAX_ASSET_TAG_COUNT =
+		"sample.sql.max.asset.tag.count";
+
+	public static final String MAX_ASSET_VUCABULARY_COUNT =
+		"sample.sql.max.asset.vocabulary.count";
+
+	public static final String MAX_ASSETPUBLISHER_PAGE_COUNT =
+		"sample.sql.max.asset.publisher.page.count";
+
+	public static final String MAX_BLOGS_ENTRY_COMMENT_COUNT =
+		"sample.sql.max.blogs.entry.comment.count";
+
+	public static final String MAX_BLOGS_ENTRY_COUNT =
+		"sample.sql.max.blogs.entry.count";
+
+	public static final String MAX_COMMERCE_PRODUCT_COUNT =
+		"sample.sql.max.commerce.product.count";
+
+	public static final String MAX_COMMERCE_PRODUCT_DEFINITION_COUNT =
+		"sample.sql.max.commerce.product.definition.count";
+
+	public static final String MAX_COMMERCE_PRODUCT_INSTANCE_COUNT =
+		"sample.sql.max.commerce.product.instance.count";
+
+	public static final String MAX_CONTENT_LAYOUT_COUNT =
+		"sample.sql.max.content.layout.count";
+
+	public static final String MAX_DDL_CUSTOM_FIELD_COUNT =
+		"sample.sql.max.ddl.custom.field.count";
+
+	public static final String MAX_DDL_RECORD_COUNT =
+		"sample.sql.max.ddl.record.count";
+
+	public static final String MAX_DDL_RECORD_SET_COUNT =
+		"sample.sql.max.ddl.record.set.count";
+
+	public static final String MAX_DL_FILE_ENTRY_COUNT =
+		"sample.sql.max.dl.file.entry.count";
+
+	public static final String MAX_DL_FILE_ENTRY_SIZE =
+		"sample.sql.max.dl.file.entry.size";
+
+	public static final String MAX_DL_FOLDER_COUNT =
+		"sample.sql.max.dl.folder.count";
+
+	public static final String MAX_DL_FOLDER_DEPTH =
+		"sample.sql.max.dl.folder.depth";
+
+	public static final String MAX_GROUP_COUNT = "sample.sql.max.group.count";
+
+	public static final String MAX_JOURNAL_ARTICLE_COUNT =
+		"sample.sql.max.journal.article.count";
+
+	public static final String MAX_JOURNAL_ARTICLE_PAGE_COUNT =
+		"sample.sql.max.journal.article.page.count";
+
+	public static final String MAX_JOURNAL_ARTICLE_SIZE =
+		"sample.sql.max.journal.article.size";
+
+	public static final String MAX_JOURNAL_ARTICLE_VERSION_COUNT =
+		"sample.sql.max.journal.article.version.count";
+
+	public static final String MAX_MB_CATEGORY_COUNT =
+		"sample.sql.max.mb.category.count";
+
+	public static final String MAX_MB_MESSAGE_COUNT =
+		"sample.sql.max.mb.message.count";
+
+	public static final String MAX_MB_THREAD_COUNT =
+		"sample.sql.max.mb.thread.count";
+
+	public static final String MAX_USER_COUNT = "sample.sql.max.user.count";
+
+	public static final String MAX_USER_TO_GROUP_COUNT =
+		"sample.sql.max.user.to.group.count";
+
+	public static final String MAX_WIKI_NODE_COUNT =
+		"sample.sql.max.wiki.node.count";
+
+	public static final String MAX_WIKI_PAGE_COMMENT_COUNT =
+		"sample.sql.max.wiki.page.comment.count";
+
+	public static final String MAX_WIKI_PAGE_COUNT =
+		"sample.sql.max.wiki.page.count";
+
+	public static final String OPTIMIZE_BUFFER_SIZE =
+		"sample.sql.optimize.buffer.size";
+
+	public static final String OUTPUT_CSV_FILE_NAMES =
+		"sample.sql.output.csv.file.names";
+
+	public static final String OUTPUT_DIR = "sample.sql.output.dir";
+
+	public static final String OUTPUT_MERGE = "sample.sql.output.merge";
+
+	public static final String SCRIPT = "sample.sql.script";
+
+	public static final String VIRTUAL_HOST_NAME =
+		"sample.sql.virtual.hostname";
+
+}
---
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/PropsValues.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.sample.sql.builder;
+
+import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Lily Chi
+ */
+public class PropsValues {
+
+	public static final DBType DB_TYPE = DBType.valueOf(
+		StringUtil.toUpperCase(PropsUtil.get(PropsKeys.DB_TYPE)));
+
+	public static final int MAX_ASSET_CATEGORY_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_ASSET_CATEGORY_COUNT));
+
+	public static final int MAX_ASSET_ENTRY_TO_ASSET_CATEGORY_COUNT =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.MAX_ASSET_ENTRY_TO_ASSET_CATEGORY_COUNT));
+
+	public static final int MAX_ASSET_ENTRY_TO_ASSET_TAG_COUNT =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.MAX_ASSET_ENTRY_TO_ASSET_TAG_COUNT));
+
+	public static final int MAX_ASSET_TAG_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_ASSET_TAG_COUNT));
+
+	public static final int MAX_ASSET_VUCABULARY_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_ASSET_VUCABULARY_COUNT));
+
+	public static final int MAX_ASSETPUBLISHER_PAGE_COUNT =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.MAX_ASSETPUBLISHER_PAGE_COUNT));
+
+	public static final int MAX_BLOGS_ENTRY_COMMENT_COUNT =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.MAX_BLOGS_ENTRY_COMMENT_COUNT));
+
+	public static final int MAX_BLOGS_ENTRY_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_BLOGS_ENTRY_COUNT));
+
+	public static final int MAX_COMMERCE_PRODUCT_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_COMMERCE_PRODUCT_COUNT));
+
+	public static final int MAX_COMMERCE_PRODUCT_DEFINITION_COUNT =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.MAX_COMMERCE_PRODUCT_DEFINITION_COUNT));
+
+	public static final int MAX_COMMERCE_PRODUCT_INSTANCE_COUNT =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.MAX_COMMERCE_PRODUCT_INSTANCE_COUNT));
+
+	public static final int MAX_CONTENT_LAYOUT_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_CONTENT_LAYOUT_COUNT));
+
+	public static final int MAX_DDL_CUSTOM_FIELD_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_DDL_CUSTOM_FIELD_COUNT));
+
+	public static final int MAX_DDL_RECORD_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_DDL_RECORD_COUNT));
+
+	public static final int MAX_DDL_RECORD_SET_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_DDL_RECORD_SET_COUNT));
+
+	public static final int MAX_DL_FILE_ENTRY_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_DL_FILE_ENTRY_COUNT));
+
+	public static final int MAX_DL_FILE_ENTRY_SIZE = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_DL_FILE_ENTRY_SIZE));
+
+	public static final int MAX_DL_FOLDER_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_DL_FOLDER_COUNT));
+
+	public static final int MAX_DL_FOLDER_DEPTH = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_DL_FOLDER_DEPTH));
+
+	public static final int MAX_GROUP_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_GROUP_COUNT));
+
+	public static final int MAX_JOURNAL_ARTICLE_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_JOURNAL_ARTICLE_COUNT));
+
+	public static final int MAX_JOURNAL_ARTICLE_PAGE_COUNT =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.MAX_JOURNAL_ARTICLE_PAGE_COUNT));
+
+	public static final int MAX_JOURNAL_ARTICLE_SIZE = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_JOURNAL_ARTICLE_SIZE));
+
+	public static final int MAX_JOURNAL_ARTICLE_VERSION_COUNT =
+		GetterUtil.getInteger(
+			PropsUtil.get(PropsKeys.MAX_JOURNAL_ARTICLE_VERSION_COUNT));
+
+	public static final int MAX_MB_CATEGORY_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_MB_CATEGORY_COUNT));
+
+	public static final int MAX_MB_MESSAGE_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_MB_MESSAGE_COUNT));
+
+	public static final int MAX_MB_THREAD_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_MB_THREAD_COUNT));
+
+	public static final int MAX_USER_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_USER_COUNT));
+
+	public static final int MAX_USER_TO_GROUP_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_USER_TO_GROUP_COUNT));
+
+	public static final int MAX_WIKI_NODE_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_WIKI_NODE_COUNT));
+
+	public static final int MAX_WIKI_PAGE_COMMENT_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_WIKI_PAGE_COMMENT_COUNT));
+
+	public static final int MAX_WIKI_PAGE_COUNT = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.MAX_WIKI_PAGE_COUNT));
+
+	public static final int OPTIMIZE_BUFFER_SIZE = GetterUtil.getInteger(
+		PropsUtil.get(PropsKeys.OPTIMIZE_BUFFER_SIZE));
+
+	public static final String[] OUTPUT_CSV_FILE_NAMES = StringUtil.split(
+		PropsUtil.get(PropsKeys.OUTPUT_CSV_FILE_NAMES));
+
+	public static final String OUTPUT_DIR = PropsUtil.get(PropsKeys.OUTPUT_DIR);
+
+	public static final boolean OUTPUT_MERGE = GetterUtil.getBoolean(
+		PropsUtil.get(PropsKeys.OUTPUT_MERGE));
+
+	public static final String SCRIPT = PropsUtil.get(PropsKeys.SCRIPT);
+
+	public static final String VIRTUAL_HOST_NAME = PropsUtil.get(
+		PropsKeys.VIRTUAL_HOST_NAME);
+
+}
