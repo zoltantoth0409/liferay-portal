d43e85fec265ea0d4c8c48d3e0a09609f4907e51
Christopher Kian <christopher.kian@liferay.com>
2019-11-27 16:04:55 -0800

    LPS-105078 If portlet config/context has changed, regardless of whether or not it's undeployed,  update with the new values

--- portal-impl/src/com/liferay/portlet/internal/PortletConfigFactoryImpl.java
+++ portal-impl/src/com/liferay/portlet/internal/PortletConfigFactoryImpl.java
@@ -15,6 +15,7 @@
 package com.liferay.portlet.internal;
 
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
 import com.liferay.portal.kernel.portlet.PortletConfigFactory;
 import com.liferay.portal.kernel.portlet.PortletContextFactory;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
@@ -49,16 +50,12 @@ public class PortletConfigFactoryImpl implements PortletConfigFactory {
 			_pool.put(portlet.getRootPortletId(), portletConfigs);
 		}
 
-		PortletConfig portletConfig = null;
+		PortletConfig portletConfig = portletConfigs.get(
+			portlet.getPortletId());
 
-		if (portlet.isUndeployedPortlet()) {
-			portletConfigs.remove(portlet.getPortletId());
-		}
-		else {
-			portletConfig = portletConfigs.get(portlet.getPortletId());
-		}
+		if ((portletConfig == null) ||
+			!_isSamePortletDeployedStatus(portlet, portletConfig)) {
 
-		if (portletConfig == null) {
 			PortletContext portletContext = _portletContextFactory.create(
 				portlet, servletContext);
 
@@ -117,6 +114,24 @@ public class PortletConfigFactoryImpl implements PortletConfigFactory {
 		portletConfigs.put(portlet.getPortletId(), portletConfig);
 
 		return portletConfig;
+	}
+
+	private boolean _isSamePortletDeployedStatus(
+		Portlet portlet, PortletConfig portletConfig) {
+
+		LiferayPortletConfig liferayPortletConfig =
+			(LiferayPortletConfig)portletConfig;
+
+		Portlet existingPortlet = liferayPortletConfig.getPortlet();
+
+		if ((existingPortlet != null) &&
+			(portlet.isUndeployedPortlet() ==
+				existingPortlet.isUndeployedPortlet())) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	private final Map<String, Map<String, PortletConfig>> _pool;
