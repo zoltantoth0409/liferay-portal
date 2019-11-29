3805a36c2653835d08016b19fc2348ad7b60e11c
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-11-29 11:42:41 +0100

    LPS-105133 Searching in linked repos from the search bar

---
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/search/bar/portlet/shared/search/DepotSearchBarPortletSharedSearchContributor.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.depot.internal.search.bar.portlet.shared.search;
+
+import com.liferay.depot.model.DepotEntryGroupRel;
+import com.liferay.depot.model.DepotEntryGroupRelModel;
+import com.liferay.depot.model.DepotEntryModel;
+import com.liferay.depot.service.DepotEntryGroupRelLocalService;
+import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchContributor;
+import com.liferay.portal.search.web.portlet.shared.search.PortletSharedSearchSettings;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=com_liferay_portal_search_web_search_bar_portlet_SearchBarPortlet",
+		"service.ranking:Integer=100"
+	},
+	service = PortletSharedSearchContributor.class
+)
+public class DepotSearchBarPortletSharedSearchContributor
+	implements PortletSharedSearchContributor {
+
+	@Override
+	public void contribute(
+		PortletSharedSearchSettings portletSharedSearchSettings) {
+
+		_defaultSearchBarPortletSharedSearchContributor.contribute(
+			portletSharedSearchSettings);
+
+		SearchContext searchContext =
+			portletSharedSearchSettings.getSearchContext();
+
+		long[] groupIds = searchContext.getGroupIds();
+
+		if (ArrayUtil.isEmpty(groupIds)) {
+			return;
+		}
+
+		for (long groupId : groupIds) {
+			List<DepotEntryGroupRel> depotEntryGroupRels =
+				_depotEntryGroupRelLocalService.getDepotEntryGroupRels(
+					groupId, 0,
+					_depotEntryGroupRelLocalService.getDepotEntryGroupRelsCount(
+						groupId));
+
+			Stream<DepotEntryGroupRel> stream = depotEntryGroupRels.stream();
+
+			List<Long> depotEntryGroupIds = stream.map(
+				DepotEntryGroupRelModel::getDepotEntryId
+			).map(
+				_depotEntryLocalService::fetchDepotEntry
+			).map(
+				DepotEntryModel::getGroupId
+			).collect(
+				Collectors.toList()
+			);
+
+			searchContext.setGroupIds(
+				ArrayUtil.append(
+					searchContext.getGroupIds(),
+					ArrayUtil.toLongArray(depotEntryGroupIds)));
+		}
+	}
+
+	@Reference(
+		target = "(&(javax.portlet.name=com_liferay_portal_search_web_search_bar_portlet_SearchBarPortlet)(!(component.name=com.liferay.depot.internal.search.bar.portlet.shared.search.DepotSearchBarPortletSharedSearchContributor)))"
+	)
+	private PortletSharedSearchContributor
+		_defaultSearchBarPortletSharedSearchContributor;
+
+	@Reference
+	private DepotEntryGroupRelLocalService _depotEntryGroupRelLocalService;
+
+	@Reference
+	private DepotEntryLocalService _depotEntryLocalService;
+
+}
