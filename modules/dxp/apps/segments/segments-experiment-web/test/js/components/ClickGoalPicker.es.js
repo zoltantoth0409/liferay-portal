55bf21b94f479f070fef989a4cd0755e9fc8bc1f
Sarai Díaz García <dgarciasarai@gmail.com>
2019-11-29 14:14:24 +0100

    LPS-104574 Add tests

--- modules/dxp/apps/segments/segments-experiment-web/test/js/components/ClickGoalPicker.es.js
+++ modules/dxp/apps/segments/segments-experiment-web/test/js/components/ClickGoalPicker.es.js
@@ -11,10 +11,12 @@
 
 import '@testing-library/jest-dom/extend-expect';
 import {cleanup, render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
-import SegmentsExperimentsClickGoal from '../../../src/main/resources/META-INF/resources/js/components/ClickGoalPicker/ClickGoalPicker.es';
+import ClickGoalPicker from '../../../src/main/resources/META-INF/resources/js/components/ClickGoalPicker/ClickGoalPicker.es';
 import {segmentsExperiment} from '../fixtures.es';
+import renderApp from '../renderApp.es';
 
 describe('Segments Experiments with Click Goal', () => {
 	afterEach(cleanup);
@@ -23,22 +25,100 @@ describe('Segments Experiments with Click Goal', () => {
 		const experiment = {
 			...segmentsExperiment,
 			goal: {
+				label: 'Click',
 				value: 'click'
 			}
 		};
 
-		const {asFragment} = render(
-			<SegmentsExperimentsClickGoal segmentsExperiment={experiment} />
-		);
+		const {getByText} = renderApp({
+			initialSegmentsExperiment: segmentsExperiment
+		});
 
-		expect(asFragment().children.length).not.toBe(0);
+		getByText(experiment.name);
+		getByText('review-and-run-test');
 	});
 
-	test.todo('Actually make the one above a meaningful test');
+	it('User clicks in set element button, the button turns from default to active mode', () => {
+		const experiment = {
+			...segmentsExperiment,
+			goal: {
+				label: 'Click',
+				value: 'click'
+			}
+		};
 
-	test.todo(
-		'Set click target section appears when draft experiment has goal set to click'
-	);
+		const {getByText} = render(
+			<ClickGoalPicker segmentsExperiment={experiment} />
+		);
+
+		const setElementButton = getByText('set-element');
+		userEvent.click(setElementButton);
+
+		const clickGoalRoot = document.body.querySelector(
+			'div.lfr-segments-experiment-click-goal-root'
+		);
+		expect(clickGoalRoot).not.toBe(null);
+	});
+
+	it('The user can set a click target element in a draft experiment', () => {
+		const experiment = {
+			...segmentsExperiment,
+			goal: {
+				label: 'Click',
+				value: 'click'
+			}
+		};
+
+		const {getByText} = render(
+			<ClickGoalPicker segmentsExperiment={experiment} />
+		);
+
+		expect(experiment.status.label).toBe('Draft');
+		getByText(
+			'a-clickable-element-on-the-page-must-be-selected-to-be-measured'
+		);
+		const setElementButton = getByText('set-element');
+		expect(setElementButton.attributes['disabled']).toBe(undefined);
+	});
+
+	it('The user can edit a selected click target element in a draft experiment', () => {
+		const experiment = {
+			...segmentsExperiment,
+			goal: {
+				label: 'Click',
+				target: '#myButtonId',
+				value: 'click'
+			}
+		};
+
+		const {getByText} = render(
+			<ClickGoalPicker target={experiment.goal.target} />
+		);
+
+		expect(experiment.status.label).toBe('Draft');
+		const editElement = getByText('edit-element');
+		expect(editElement.attributes['disabled']).toBe(undefined);
+	});
+
+	it('When the user set element as click target, then the element is set as the click target and the id of the element as a link', () => {
+		const experiment = {
+			...segmentsExperiment,
+			goal: {
+				label: 'Click',
+				target: '#myButtonId',
+				value: 'click'
+			}
+		};
+
+		const {getByText} = render(
+			<ClickGoalPicker target={experiment.goal.target} />
+		);
+
+		const targetElement = getByText(experiment.goal.target);
+		expect(targetElement.attributes['href'].value).toBe(
+			experiment.goal.target
+		);
+	});
 
 	test.todo(
 		'All clickable elements highlighted when the user clicks on set element button'
@@ -51,19 +131,11 @@ describe('Segments Experiments with Click Goal', () => {
 	test.todo('Selectable as target elements show tooltips on hover');
 
 	test.todo(
-		'When the user set element as click target, then the element is set as the click target and the id of the element as a link'
-	);
-
-	test.todo(
 		'Cancel selection click target element proccess when clicking out of selection zone'
 	);
 
 	test.todo(
 		'When hovering over to invalid click target elements, the mouse is displayed in not-allowed mode'
-	);
-
-	test.todo(
-		'The user can edit a selected click target in a draft experiment'
 	);
 
 	test.todo('The user can remove a selected click target in a draft element');
