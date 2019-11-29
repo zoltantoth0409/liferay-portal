80adfe4cc4d78990ada197f2fd068829304f90b6
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-11-29 10:53:35 +0000

    LPS-101248 Creates a new object to represent the templates that can be renderer, with a label and a key. It is going to by used on InfoItemTemplatedRenderer to represent the templates that can be renderer

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/item/template/InfoItemRendererTemplate.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.item.template;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class InfoItemRendererTemplate {
+
+	public InfoItemRendererTemplate(String label, String templateKey) {
+		_label = label;
+		_templateKey = templateKey;
+	}
+
+	public String getLabel() {
+		return _label;
+	}
+
+	public String getTemplateKey() {
+		return _templateKey;
+	}
+
+	public void setLabel(String label) {
+		_label = label;
+	}
+
+	public void setTemplateKey(String templateKey) {
+		_templateKey = templateKey;
+	}
+
+	private String _label;
+	private String _templateKey;
+
+}
