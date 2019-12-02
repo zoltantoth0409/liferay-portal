c6406b508f03e65e2a793dd9f19acfc8c6bb5516
hai.yu <8219754@liferay.com>
2019-12-02 16:12:36 +0800

    LPS-95571 REPLACE function does not support TEXT data type in Sybase.

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/upgrade/v_1_0_3/UpgradeLayoutTemplateId.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/upgrade/v_1_0_3/UpgradeLayoutTemplateId.java
@@ -15,6 +15,8 @@
 package com.liferay.layout.admin.web.internal.upgrade.v_1_0_3;
 
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.dao.db.DBTypeToSQLMap;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 
 /**
@@ -28,12 +30,21 @@ public class UpgradeLayoutTemplateId extends UpgradeProcess {
 	}
 
 	protected void updateLayoutTemplateId() throws Exception {
-		String sql = StringBundler.concat(
-			"update Layout set typeSettings = REPLACE(typeSettings, ",
-			"'layout-template-id=1_2_1_columns\n', ",
-			"'layout-template-id=1_2_1_columns_i\n')");
+		DBTypeToSQLMap dbTypeToSQLMap = new DBTypeToSQLMap(
+			StringBundler.concat(
+				"update Layout set typeSettings = REPLACE(typeSettings, ",
+				"'layout-template-id=1_2_1_columns\n', ",
+				"'layout-template-id=1_2_1_columns_i\n')"));
 
-		runSQL(sql);
+		dbTypeToSQLMap.add(
+			DBType.SYBASE,
+			StringBundler.concat(
+				"update Layout set typeSettings = ",
+				"REPLACE(CAST_TEXT(typeSettings), ",
+				"'layout-template-id=1_2_1_columns\n', ",
+				"'layout-template-id=1_2_1_columns_i\n')"));
+
+		runSQL(dbTypeToSQLMap);
 	}
 
 }
