884f58b93de88140f564636885942764abd7be3b
tina.tian <tina.tian@liferay.com>
2019-12-03 10:14:28 -0800

    LPS-95571 Do CAST_TEXT for every type of database

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/upgrade/v_1_0_3/UpgradeLayoutTemplateId.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/upgrade/v_1_0_3/UpgradeLayoutTemplateId.java
@@ -15,8 +15,6 @@
 package com.liferay.layout.admin.web.internal.upgrade.v_1_0_3;
 
 import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.dao.db.DBType;
-import com.liferay.portal.kernel.dao.db.DBTypeToSQLMap;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 
 /**
@@ -30,21 +28,12 @@ public class UpgradeLayoutTemplateId extends UpgradeProcess {
 	}
 
 	protected void updateLayoutTemplateId() throws Exception {
-		DBTypeToSQLMap dbTypeToSQLMap = new DBTypeToSQLMap(
-			StringBundler.concat(
-				"update Layout set typeSettings = REPLACE(typeSettings, ",
-				"'layout-template-id=1_2_1_columns\n', ",
-				"'layout-template-id=1_2_1_columns_i\n')"));
+		String sql = StringBundler.concat(
+			"update Layout set typeSettings = REPLACE(CAST_TEXT(typeSettings)",
+			", 'layout-template-id=1_2_1_columns\n', ",
+			"'layout-template-id=1_2_1_columns_i\n')");
 
-		dbTypeToSQLMap.add(
-			DBType.SYBASE,
-			StringBundler.concat(
-				"update Layout set typeSettings = ",
-				"REPLACE(CAST_TEXT(typeSettings), ",
-				"'layout-template-id=1_2_1_columns\n', ",
-				"'layout-template-id=1_2_1_columns_i\n')"));
-
-		runSQL(dbTypeToSQLMap);
+		runSQL(sql);
 	}
 
 }
