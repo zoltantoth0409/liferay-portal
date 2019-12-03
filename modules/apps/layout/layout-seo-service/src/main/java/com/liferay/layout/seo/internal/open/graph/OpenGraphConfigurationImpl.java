243a4ad3febf0d4f457d084463e9bd3a42230984
Adolfo Pérez <adolfo.perez@liferay.com>
2019-12-03 17:34:59 +0100

    LPS-104242 Create config interface to centralize logic

---
+++ modules/apps/layout/layout-seo-service/src/main/java/com/liferay/layout/seo/internal/open/graph/OpenGraphConfigurationImpl.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.seo.internal.open.graph;
+
+import com.liferay.layout.seo.internal.configuration.LayoutSEOCompanyConfiguration;
+import com.liferay.layout.seo.model.LayoutSEOSite;
+import com.liferay.layout.seo.open.graph.OpenGraphConfiguration;
+import com.liferay.layout.seo.service.LayoutSEOSiteLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(service = OpenGraphConfiguration.class)
+public class OpenGraphConfigurationImpl implements OpenGraphConfiguration {
+
+	@Override
+	public boolean isOpenGraphEnabled(Group group) throws PortalException {
+		LayoutSEOCompanyConfiguration layoutSEOCompanyConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				LayoutSEOCompanyConfiguration.class, group.getCompanyId());
+
+		if (!layoutSEOCompanyConfiguration.enableOpenGraph()) {
+			return false;
+		}
+
+		LayoutSEOSite layoutSEOSite =
+			_layoutSEOSiteLocalService.fetchLayoutSEOSiteByGroupId(
+				group.getGroupId());
+
+		if ((layoutSEOSite != null) && layoutSEOSite.isOpenGraphEnabled()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private LayoutSEOSiteLocalService _layoutSEOSiteLocalService;
+
+}
