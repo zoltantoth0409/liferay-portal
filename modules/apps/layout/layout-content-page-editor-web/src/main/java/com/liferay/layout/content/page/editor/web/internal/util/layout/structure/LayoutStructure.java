c9c3695e4217519cb79b2605b42c3e35c19b9f87
Victor Galan <victor.galan@liferay.com>
2019-12-05 17:38:43 +0100

    LPS-105467 Adds an utility to handle new layout structure

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/layout/structure/LayoutStructure.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.util.layout.structure;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Víctor Galán
+ */
+public class LayoutStructure {
+
+	public LayoutStructure(
+		Map<String, Item> layoutStructureItems, Root layoutStructureRoot) {
+
+		_layoutStructureItems = layoutStructureItems;
+		_layoutStructureRoot = layoutStructureRoot;
+	}
+
+	public LayoutStructure addItem(
+		Item layoutStructureItem, String parentId, int position) {
+
+		_layoutStructureItems.put(
+			layoutStructureItem.getItemId(), layoutStructureItem);
+
+		if (parentId != null) {
+			Item parentLayoutStructureItem = _layoutStructureItems.get(
+				parentId);
+
+			List<String> children = parentLayoutStructureItem.getChildren();
+
+			children.add(position, layoutStructureItem.getItemId());
+		}
+
+		return this;
+	}
+
+	public JSONObject toJSONObject() {
+		JSONObject itemsJSONObject = JSONFactoryUtil.createJSONObject();
+
+		for (Map.Entry<String, Item> entry : _layoutStructureItems.entrySet()) {
+			Item layoutStructureItem = entry.getValue();
+
+			itemsJSONObject.put(
+				entry.getKey(),
+				JSONUtil.put(
+					"children", layoutStructureItem.getChildren()
+				).put(
+					"config", layoutStructureItem.getConfig()
+				).put(
+					"itemId", layoutStructureItem.getItemId()
+				).put(
+					"parentId", layoutStructureItem.getParentId()
+				).put(
+					"type", layoutStructureItem.getType()
+				));
+		}
+
+		return JSONUtil.put(
+			"items", itemsJSONObject
+		).put(
+			"rootItems", JSONUtil.put("main", _layoutStructureRoot.getMain())
+		).put(
+			"version", 1
+		);
+	}
+
+	public static class Item {
+
+		public static Item create(
+			JSONObject config, String itemId, String parentId, String type) {
+
+			return new Item(new ArrayList<>(), config, itemId, parentId, type);
+		}
+
+		public Item(
+			List<String> children, JSONObject config, String itemId,
+			String parentId, String type) {
+
+			_children = children;
+			_config = config;
+			_itemId = itemId;
+			_parentId = parentId;
+			_type = type;
+		}
+
+		public List<String> getChildren() {
+			return _children;
+		}
+
+		public JSONObject getConfig() {
+			return _config;
+		}
+
+		public String getItemId() {
+			return _itemId;
+		}
+
+		public String getParentId() {
+			return _parentId;
+		}
+
+		public String getType() {
+			return _type;
+		}
+
+		private final List<String> _children;
+		private final JSONObject _config;
+		private final String _itemId;
+		private final String _parentId;
+		private final String _type;
+
+	}
+
+	public static class Root {
+
+		public Root(String main) {
+			_main = main;
+		}
+
+		public String getMain() {
+			return _main;
+		}
+
+		private final String _main;
+
+	}
+
+	private final Map<String, Item> _layoutStructureItems;
+	private final Root _layoutStructureRoot;
+
+}
---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/layout/structure/LayoutStructureUtil.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.util.layout.structure;
+
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalServiceUtil;
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.segments.constants.SegmentsExperienceConstants;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+
+/**
+ * @author Víctor Galán
+ */
+public class LayoutStructureUtil {
+
+	public static JSONObject updateLayoutPageTemplateData(
+			ActionRequest actionRequest,
+			UnsafeConsumer<LayoutStructure, PortalException> unsafeConsumer)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long segmentsExperienceId = ParamUtil.getLong(
+			actionRequest, "segmentsExperienceId",
+			SegmentsExperienceConstants.ID_DEFAULT);
+		long classNameId = ParamUtil.getLong(actionRequest, "classNameId");
+		long classPK = ParamUtil.getLong(actionRequest, "classPK");
+
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			LayoutPageTemplateStructureLocalServiceUtil.
+				fetchLayoutPageTemplateStructure(
+					themeDisplay.getScopeGroupId(),
+					PortalUtil.getClassNameId(Layout.class.getName()),
+					themeDisplay.getPlid(), true);
+
+		LayoutStructure layoutStructure = _parse(
+			layoutPageTemplateStructure.getData(segmentsExperienceId));
+
+		unsafeConsumer.accept(layoutStructure);
+
+		JSONObject dataJSONObject = layoutStructure.toJSONObject();
+
+		LayoutPageTemplateStructureLocalServiceUtil.
+			updateLayoutPageTemplateStructure(
+				themeDisplay.getScopeGroupId(), classNameId, classPK,
+				segmentsExperienceId, dataJSONObject.toString());
+
+		return dataJSONObject;
+	}
+
+	private static LayoutStructure _parse(String layoutStructure)
+		throws JSONException {
+
+		JSONObject layoutStructureJSONObject = JSONFactoryUtil.createJSONObject(
+			layoutStructure);
+
+		JSONObject rootItems = layoutStructureJSONObject.getJSONObject(
+			"rootItems");
+
+		LayoutStructure.Root layoutStructureRoot = new LayoutStructure.Root(
+			rootItems.getString("main"));
+
+		JSONObject itemsJSONObject = layoutStructureJSONObject.getJSONObject(
+			"items");
+
+		Map<String, LayoutStructure.Item> itemMap = new HashMap<>(
+			itemsJSONObject.length());
+
+		for (String key : itemsJSONObject.keySet()) {
+			itemMap.put(
+				key,
+				_toLayoutStructureItem(itemsJSONObject.getJSONObject(key)));
+		}
+
+		return new LayoutStructure(itemMap, layoutStructureRoot);
+	}
+
+	private static LayoutStructure.Item _toLayoutStructureItem(
+		JSONObject jsonObject) {
+
+		JSONObject configJSONObject = jsonObject.getJSONObject("config");
+		String itemId = jsonObject.getString("itemId");
+		String parentId = jsonObject.getString("parentId");
+		String type = jsonObject.getString("type");
+
+		ArrayList<String> children = new ArrayList<>();
+
+		JSONUtil.addToStringCollection(
+			children, jsonObject.getJSONArray("children"));
+
+		return new LayoutStructure.Item(
+			children, configJSONObject, itemId, parentId, type);
+	}
+
+}
