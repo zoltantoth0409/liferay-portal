fd2240da10b4b764b7729bae32172cd7313538ab
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-12-05 16:14:59 +0100

    LPS-104378 Implementing a basic support controller

---
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/application/support/DepotApplicationSupportController.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.depot.web.internal.application.support;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(immediate = true, service = DepotApplicationSupportController.class)
+public class DepotApplicationSupportController {
+
+	public boolean isEnabeld(String portletId) {
+		if (_serviceTrackerMap.getService(portletId) != null) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, DepotApplicationSupportDescriptor.class, null,
+			(serviceReference, emitter) -> {
+				DepotApplicationSupportDescriptor
+					depotApplicationSupportDescriptor =
+						bundleContext.getService(serviceReference);
+
+				emitter.emit(depotApplicationSupportDescriptor.getPortletId());
+
+				bundleContext.ungetService(serviceReference);
+			});
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private ServiceTrackerMap<String, DepotApplicationSupportDescriptor>
+		_serviceTrackerMap;
+
+}
