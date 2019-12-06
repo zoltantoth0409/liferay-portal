5f802905d9f4ac7f60acdc2b5fe534f131c69274
Geyson Silva <geyson.silva@liferay.com>
2019-12-06 13:45:37 -0300

    LRAC-4546 Create UserGroup and Organization checker

---
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/search/OrganizationChecker.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.web.internal.search;
+
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.model.Organization;
+
+import java.util.Set;
+
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Geyson Silva
+ */
+public class OrganizationChecker extends EmptyOnClickRowChecker {
+
+	public OrganizationChecker(RenderResponse renderResponse, Set<String> ids) {
+		super(renderResponse);
+
+		setRowIds("organizationIds");
+
+		_ids = ids;
+	}
+
+	@Override
+	public boolean isChecked(Object obj) {
+		Organization organization = (Organization)obj;
+
+		return _ids.contains(String.valueOf(organization.getOrganizationId()));
+	}
+
+	private final Set<String> _ids;
+
+}
---
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/search/UserGroupChecker.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.web.internal.search;
+
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.model.UserGroup;
+
+import java.util.Set;
+
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Geyson Silva
+ */
+public class UserGroupChecker extends EmptyOnClickRowChecker {
+
+	public UserGroupChecker(RenderResponse renderResponse, Set<String> ids) {
+		super(renderResponse);
+
+		setRowIds("userGroupIds");
+
+		_ids = ids;
+	}
+
+	@Override
+	public boolean isChecked(Object obj) {
+		UserGroup userGroup = (UserGroup)obj;
+
+		return _ids.contains(String.valueOf(userGroup.getUserGroupId()));
+	}
+
+	private final Set<String> _ids;
+
+}
