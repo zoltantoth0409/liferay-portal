90d1578a1cb1d38333f3d1307d9326e5d588353a
Alberto Chaparro <alberto.chaparro@liferay.com>
2019-12-06 16:46:44 -0800

    LPS-105539 We forgot to remove this index in LPS-93595

--- modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/upgrade/AssetServiceUpgrade.java
+++ modules/apps/asset/asset-service/src/main/java/com/liferay/asset/internal/upgrade/AssetServiceUpgrade.java
@@ -15,6 +15,7 @@
 package com.liferay.asset.internal.upgrade;
 
 import com.liferay.asset.internal.upgrade.v2_0_0.UpgradeCompanyId;
+import com.liferay.asset.internal.upgrade.v2_0_1.UpgradeAssetEntryUsage;
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.portal.kernel.upgrade.UpgradeMVCCVersion;
 import com.liferay.portal.kernel.upgrade.UpgradeViewCount;
@@ -47,6 +48,8 @@ public class AssetServiceUpgrade implements UpgradeStepRegistrator {
 			"1.1.0", "2.0.0", new UpgradeCompanyId(),
 			new UpgradeViewCount(
 				"AssetEntry", AssetEntry.class, "entryId", "viewCount"));
+
+		registry.register("2.0.0", "2.0.1", new UpgradeAssetEntryUsage());
 	}
 
 	/**
--- modules/apps/asset/asset-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/asset/asset-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,5 +1,4 @@
 create unique index IX_E6C54E12 on AssetEntryUsage (assetEntryId, containerType, containerKey[$COLUMN_LENGTH:200$], plid);
-create unique index IX_71A0231C on AssetEntryUsage (assetEntryId, plid, containerType, containerKey[$COLUMN_LENGTH:200$]);
 create index IX_2816A7BF on AssetEntryUsage (assetEntryId, type_);
 create index IX_7DFFC5C3 on AssetEntryUsage (containerType, containerKey[$COLUMN_LENGTH:200$], plid);
 create index IX_8DA9ACD on AssetEntryUsage (plid, containerType, containerKey[$COLUMN_LENGTH:200$]);
