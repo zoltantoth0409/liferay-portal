f6218990223f806234b24e7ab8fc8f91198cc9f8
Andre Miranda <andre.miranda@liferay.com>
2019-12-10 18:05:12 -0300

    LRAC-4581 Notify AC when contacts are (un)selected

--- modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/portlet/action/EditSyncedContactsMVCActionCommand.java
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/portlet/action/EditSyncedContactsMVCActionCommand.java
@@ -15,12 +15,28 @@
 package com.liferay.analytics.settings.web.internal.portlet.action;
 
 import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.Dictionary;
 
 import javax.portlet.ActionRequest;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -39,8 +55,9 @@ public class EditSyncedContactsMVCActionCommand
 
 	@Override
 	protected void updateConfigurationProperties(
-		ActionRequest actionRequest,
-		Dictionary<String, Object> configurationProperties) {
+			ActionRequest actionRequest,
+			Dictionary<String, Object> configurationProperties)
+		throws Exception {
 
 		boolean syncAllContacts = ParamUtil.getBoolean(
 			actionRequest, "syncAllContacts");
@@ -57,6 +74,76 @@ public class EditSyncedContactsMVCActionCommand
 				"syncedOrganizationIds", syncedOrganizationIds);
 			configurationProperties.put(
 				"syncedUserGroupIds", syncedUserGroupIds);
+		}
+
+		_notifyAnalyticsCloud(
+			actionRequest, syncAllContacts, syncedOrganizationIds,
+			syncedUserGroupIds);
+	}
+
+	private void _notifyAnalyticsCloud(
+			ActionRequest actionRequest, boolean syncAllContacts,
+			String[] syncedOrganizationIds, String[] syncedUserGroupIds)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String liferayAnalyticsDataSourceId = PrefsPropsUtil.getString(
+			themeDisplay.getCompanyId(), "liferayAnalyticsDataSourceId");
+
+		String liferayAnalyticsFaroBackendSecuritySignature =
+			PrefsPropsUtil.getString(
+				themeDisplay.getCompanyId(),
+				"liferayAnalyticsFaroBackendSecuritySignature");
+
+		String liferayAnalyticsFaroBackendURL = PrefsPropsUtil.getString(
+			themeDisplay.getCompanyId(), "liferayAnalyticsFaroBackendURL");
+
+		if (Validator.isNull(liferayAnalyticsDataSourceId) ||
+			Validator.isNull(liferayAnalyticsFaroBackendSecuritySignature) ||
+			Validator.isNull(liferayAnalyticsFaroBackendURL)) {
+
+			return;
+		}
+
+		boolean contactsSelected = true;
+
+		if (!syncAllContacts && ArrayUtil.isEmpty(syncedOrganizationIds) &&
+			ArrayUtil.isEmpty(syncedUserGroupIds)) {
+
+			contactsSelected = false;
+		}
+
+		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+
+		try (CloseableHttpClient closeableHttpClient =
+				httpClientBuilder.build()) {
+
+			HttpPut httpPut = new HttpPut(
+				String.format(
+					"%s/api/1.0/data-sources/%s/details",
+					liferayAnalyticsFaroBackendURL,
+					liferayAnalyticsDataSourceId));
+
+			JSONObject bodyJSONObject = JSONUtil.put(
+				"contactsSelected", contactsSelected);
+
+			httpPut.setEntity(new StringEntity(bodyJSONObject.toString()));
+
+			httpPut.setHeader("Content-type", "application/json");
+			httpPut.setHeader(
+				"OSB-Asah-Faro-Backend-Security-Signature",
+				liferayAnalyticsFaroBackendSecuritySignature);
+
+			CloseableHttpResponse closeableHttpResponse =
+				closeableHttpClient.execute(httpPut);
+
+			StatusLine statusLine = closeableHttpResponse.getStatusLine();
+
+			if (statusLine.getStatusCode() != HttpStatus.SC_OK) {
+				throw new PortalException("Invalid token");
+			}
 		}
 	}
 
