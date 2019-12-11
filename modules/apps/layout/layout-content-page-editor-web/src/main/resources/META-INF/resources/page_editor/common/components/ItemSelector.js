05e17e6e7f13735bdbbd3a8a2bbf51311d0190f6
Sandro Chinea <sandrochinea@gmail.com>
2019-12-11 17:15:54 +0100

    LPS-105138 Added common ItemSelector component

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/ItemSelector.js
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import {ClayInput} from '@clayui/form';
+import ClayIcon from '@clayui/icon';
+import {ItemSelectorDialog} from 'frontend-js-web';
+import React, {useContext} from 'react';
+
+import {ConfigContext} from '../../app/config/index';
+
+export default function ItemSelector({label, onSelectItem, value}) {
+	const {infoItemSelectorURL, portletNamespace} = useContext(ConfigContext);
+
+	const openInfoItemSelector = (callback, destroyedCallback = null) => {
+		const itemSelectorDialog = new ItemSelectorDialog({
+			eventName: `${portletNamespace}selectInfoItem`,
+			singleSelect: true,
+			title: Liferay.Language.get('select'),
+			url: infoItemSelectorURL
+		});
+
+		itemSelectorDialog.on('selectedItemChange', event => {
+			const selectedItem = event.selectedItem;
+
+			if (selectedItem && selectedItem.value) {
+				const infoItem = JSON.parse(selectedItem.value);
+
+				callback({
+					className: infoItem.className,
+					classNameId: infoItem.classNameId,
+					classPK: infoItem.classPK,
+					title: infoItem.title
+				});
+			}
+		});
+
+		itemSelectorDialog.on('visibleChange', event => {
+			if (event.newVal === false && destroyedCallback) {
+				destroyedCallback();
+			}
+		});
+
+		itemSelectorDialog.open();
+	};
+
+	return (
+		<>
+			{label && <label htmlFor="itemSelectorInput">{label}</label>}
+
+			<div className="d-flex">
+				<ClayInput
+					className="form-control-sm mr-2"
+					id="itemSelectorInput"
+					readOnly
+					type="text"
+					value={value}
+				/>
+
+				<ClayButton.Group>
+					<ClayButton
+						displayType="secondary"
+						onClick={() => {
+							openInfoItemSelector(selectedInfoItem => {
+								onSelectItem(selectedInfoItem);
+							});
+						}}
+						small
+					>
+						<ClayIcon symbol="plus" />
+					</ClayButton>
+				</ClayButton.Group>
+			</div>
+		</>
+	);
+}
