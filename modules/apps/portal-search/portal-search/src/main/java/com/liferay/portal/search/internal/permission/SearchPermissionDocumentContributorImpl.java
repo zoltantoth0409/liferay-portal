063440b7a43f67e63c715b8a9e66ee0ee34fce76
Brian Chan <brian.chan@liferay.com>
2019-12-10 21:39:35 -0800

    Revert "LPS-102947 portal-search: Move “resource permission name” logic from “SearchPermissionCheckerImpl” and “SearchPermissionDocumentContributorImpl” to “DDMSearchHelper”, “DDMStructureModelDocumentContributor” and “DDMTemplateModelDocumentContributor”"
    This reverts commit 1fe04d08786065930cfd67ba57765f4d26eef9c2.

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/SearchPermissionDocumentContributorImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/SearchPermissionDocumentContributorImpl.java
@@ -19,6 +19,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.NoSuchResourceException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.role.RoleConstants;
@@ -27,6 +28,8 @@ import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistry;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.ResourceActions;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -135,11 +138,17 @@ public class SearchPermissionDocumentContributorImpl
 				document, className, classPK);
 		}
 
-		String permissionName = _getPermissionName(document, className);
-
 		try {
+			ClassName resourceClassName = _classNameLocalService.fetchClassName(
+				GetterUtil.getLong(document.get("resourceClassNameId")));
+
+			if (resourceClassName != null) {
+				className = _resourceActions.getCompositeModelName(
+					className, resourceClassName.getClassName());
+			}
+
 			List<Role> roles = _resourcePermissionLocalService.getRoles(
-				companyId, permissionName, ResourceConstants.SCOPE_INDIVIDUAL,
+				companyId, className, ResourceConstants.SCOPE_INDIVIDUAL,
 				String.valueOf(classPK), viewActionId);
 
 			if (roles.isEmpty()) {
@@ -175,30 +184,26 @@ public class SearchPermissionDocumentContributorImpl
 				_log.warn(
 					StringBundler.concat(
 						"Unable to get permission fields for class name ",
-						permissionName, " and class PK ", classPK),
+						className, " and class PK ", classPK),
 					e);
 			}
 		}
-	}
-
-	private String _getPermissionName(Document document, String defaultValue) {
-		String resourcePermissionName = document.get("resourcePermissionName");
-
-		if (Validator.isNull(document.get("resourcePermissionName"))) {
-			return defaultValue;
-		}
-
-		return resourcePermissionName;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		SearchPermissionDocumentContributorImpl.class);
 
 	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
 	private IndexerRegistry _indexerRegistry;
 
 	@Reference
 	private Portal _portal;
+
+	@Reference
+	private ResourceActions _resourceActions;
 
 	@Reference
 	private ResourcePermissionLocalService _resourcePermissionLocalService;
