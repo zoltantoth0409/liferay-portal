912146a9ef180e9503d129a0b2600014f9dd2d10
hudakl <laszlo.hudak@liferay.com>
2019-12-12 15:25:24 +0100

    LPS-104673 portal-search-api: Add new search context attribute
    https://issues.liferay.com/browse/LPS-104673

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/constants/SearchContextAttributes.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/constants/SearchContextAttributes.java
@@ -28,4 +28,6 @@ public class SearchContextAttributes {
 	public static final String ATTRIBUTE_KEY_LUCENE_SYNTAX =
 		"search.lucene.syntax";
 
+	public static final String ATTRIBUTE_KEY_SHOW_STAGED = "search.show.staged";
+
 }
