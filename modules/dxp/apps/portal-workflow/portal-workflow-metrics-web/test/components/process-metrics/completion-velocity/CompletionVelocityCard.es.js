a7552ca316be4a128cbc25a192d3ab8ed46a9e3c
Albino Júnior <albino.junior@liferay.com>
2019-12-12 11:26:17 -0300

    LPS-104318 | LPS-104854 Apply tests for completion velocity card

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/test/components/process-metrics/completion-velocity/CompletionVelocityCard.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/test/components/process-metrics/completion-velocity/CompletionVelocityCard.es.js
@@ -9,41 +9,120 @@
  * distribution rights of the Software.
  */
 
+import {render, findAllByTestId, findByTestId} from '@testing-library/react';
 import React from 'react';
 
 import CompletionVelocityCard from '../../../../src/main/resources/META-INF/resources/js/components/process-metrics/completion-velocity/CompletionVelocityCard.es';
-import {MockRouter as Router} from '../../../mock/MockRouter.es';
-import fetch from '../../../mock/fetch.es';
+import {MockRouter} from '../../../mock/MockRouter.es';
 
-test('Should render component', () => {
-	const props = {
-		processId: 12345,
-		query:
-			'?backPath=%2Fprocesses%2F20%2F1%2FoverdueInstanceC…eRange%5B0%5D=30&filters.velocityUnit%5B0%5D=Days'
-	};
+const {processId, query} = {
+	processId: 12345,
+	query:
+		'?filters.completionvelocityUnit%5B0%5D=Days&filters.completiontimeRange%5B0%5D=7'
+};
 
-	const data = {
-		items: [
-			{
-				instanceCount: 1,
-				name: 'Task Name',
-				onTimeInstanceCount: 1,
-				overdueInstanceCount: 0
-			}
-		],
-		totalCount: 1
-	};
+const data = {
+	histograms: [
+		{
+			key: '2019-12-03T00:00',
+			value: 0.0
+		},
+		{
+			key: '2019-12-04T00:00',
+			value: 0.0
+		},
+		{
+			key: '2019-12-05T00:00',
+			value: 0.0
+		},
+		{
+			key: '2019-12-06T00:00',
+			value: 0.0
+		},
+		{
+			key: '2019-12-07T00:00',
+			value: 0.0
+		},
+		{
+			key: '2019-12-08T00:00',
+			value: 0.8
+		},
+		{
+			key: '2019-12-09T00:00',
+			value: 0.0
+		}
+	],
+	value: 0.36
+};
 
-	const component = mount(
-		<Router client={fetch(data)}>
-			<CompletionVelocityCard {...props} />
-		</Router>
-	);
+const timeRangeData = {
+	items: [
+		{
+			dateEnd: '2019-12-09T00:00:00Z',
+			dateStart: '2019-12-03T00:00:00Z',
+			defaultTimeRange: false,
+			id: 7,
+			name: 'Last 7 Days'
+		},
+		{
+			dateEnd: '2019-12-09T00:00:00Z',
+			dateStart: '2019-11-10T00:00:00Z',
+			defaultTimeRange: true,
+			id: 30,
+			name: 'Last 30 Days'
+		}
+	],
+	totalCount: 2
+};
 
-	expect(
-		component
-			.find('.dashboard-panel-header .mr-2')
-			.html()
-			.includes(Liferay.Language.get('completion-velocity'))
-	).toEqual(true);
+describe('The completion velocity card component should', () => {
+	let getByTestId;
+
+	beforeAll(() => {
+		const clientMock = {
+			get: jest
+				.fn()
+				.mockResolvedValueOnce({data: timeRangeData})
+				.mockResolvedValue({data})
+		};
+
+		const renderResult = render(
+			<MockRouter client={clientMock} query={query}>
+				<CompletionVelocityCard routeParams={{processId}} />
+			</MockRouter>
+		);
+
+		getByTestId = renderResult.getByTestId;
+	});
+
+	test('Be rendered with time range filter', async () => {
+		const timeRangeFilter = getByTestId('timeRangeFilter');
+		const filterItems = await findAllByTestId(
+			timeRangeFilter,
+			'filterItem'
+		);
+		const activeItem = filterItems.find(item =>
+			item.className.includes('active')
+		);
+		const activeItemName = await findByTestId(activeItem, 'filterItemName');
+
+		expect(timeRangeFilter).not.toBeNull();
+		expect(activeItemName.innerHTML).toBe('Last 7 Days');
+	});
+
+	test('Be rendered with time range filter', async () => {
+		const velocityUnitFilter = await getByTestId('velocityUnitFilter');
+		const filterItems = await findAllByTestId(
+			velocityUnitFilter,
+			'filterItem'
+		);
+
+		const activeItem = filterItems.find(item =>
+			item.className.includes('active')
+		);
+		const activeItemName = await findByTestId(activeItem, 'filterItemName');
+
+		expect(velocityUnitFilter).not.toBeNull();
+		expect(activeItemName.innerHTML).toBe('inst-day');
+	});
 });
--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/test/components/process-metrics/completion-velocity/VelocityChart.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/test/components/process-metrics/completion-velocity/VelocityChart.es.js
@@ -15,270 +15,186 @@ import React from 'react';
 
 import {AppContext} from '../../../../src/main/resources/META-INF/resources/js/components/AppContext.es';
 import VelocityChart from '../../../../src/main/resources/META-INF/resources/js/components/process-metrics/completion-velocity/VelocityChart.es';
-import {VelocityDataContext} from '../../../../src/main/resources/META-INF/resources/js/components/process-metrics/completion-velocity/store/VelocityDataStore.es';
-import {TimeRangeContext} from '../../../../src/main/resources/META-INF/resources/js/components/process-metrics/filter/store/TimeRangeStore.es';
-import {VelocityUnitContext} from '../../../../src/main/resources/META-INF/resources/js/components/process-metrics/filter/store/VelocityUnitStore.es';
-import {ErrorContext} from '../../../../src/main/resources/META-INF/resources/js/shared/components/request/Error.es';
-import {LoadingContext} from '../../../../src/main/resources/META-INF/resources/js/shared/components/request/Loading.es';
-import {MockRouter as Router} from '../../../mock/MockRouter.es';
-import fetch from '../../../mock/fetch.es';
 
 import '@testing-library/jest-dom/extend-expect';
 
-test('Should render velocity chart', async () => {
-	const data = {
-		items: [
-			{
-				dateEnd: '2019-09-05T18:40:40Z',
-				dateStart: '2019-09-05T00:00:00Z',
-				defaultTimeRange: false,
-				id: 0,
-				name: 'Today'
-			},
-			{
-				dateEnd: '2019-09-04T23:59:59Z',
-				dateStart: '2019-09-04T00:00:00Z',
-				defaultTimeRange: false,
-				id: 1,
-				name: 'Yesterday'
-			},
-			{
-				dateEnd: '2019-09-05T18:40:40Z',
-				dateStart: '2019-08-30T00:00:00Z',
-				defaultTimeRange: false,
-				id: 7,
-				name: 'Last 7 Days'
-			},
-			{
-				dateEnd: '2019-09-05T18:40:40Z',
-				dateStart: '2019-08-07T00:00:00Z',
-				defaultTimeRange: true,
-				id: 30,
-				name: 'Last 30 Days'
-			},
-			{
-				dateEnd: '2019-09-05T18:40:40Z',
-				dateStart: '2019-06-08T00:00:00Z',
-				defaultTimeRange: false,
-				id: 90,
-				name: 'Last 90 Days'
-			},
-			{
-				dateEnd: '2019-09-05T18:40:40Z',
-				dateStart: '2019-03-10T00:00:00Z',
-				defaultTimeRange: false,
-				id: 180,
-				name: 'Last 180 Days'
-			},
-			{
-				dateEnd: '2019-09-05T18:40:40Z',
-				dateStart: '2018-09-05T00:00:00Z',
-				defaultTimeRange: false,
-				id: 365,
-				name: 'Last Year'
-			}
-		],
-		lastPage: 1,
-		page: 1,
-		pageSize: 7,
-		totalCount: 7
-	};
+describe('The velocity chart should', () => {
+	test('Be rendered in document', async () => {
+		const velocityData = {
+			histograms: [
+				{
+					key: '2019-12-03T00:00',
+					value: 0.0
+				},
+				{
+					key: '2019-12-04T00:00',
+					value: 0.0
+				},
+				{
+					key: '2019-12-05T00:00',
+					value: 0.0
+				},
+				{
+					key: '2019-12-06T00:00',
+					value: 0.0
+				},
+				{
+					key: '2019-12-07T00:00',
+					value: 0.2
+				},
+				{
+					key: '2019-12-08T00:00',
+					value: 0.8
+				},
+				{
+					key: '2019-12-09T00:00',
+					value: 0.0
+				}
+			],
+			value: 0.0
+		};
 
-	const velocityData = {
-		histograms: [
-			{
-				key: '2019-06-27T00:00',
-				value: 0.0
-			},
-			{
-				key: '2019-07-01T00:00',
-				value: 0.0
-			},
-			{
-				key: '2019-08-01T00:00',
-				value: 0.0
-			},
-			{
-				key: '2019-09-01T00:00',
-				value: 0.0
-			}
-		],
-		value: 0.0
-	};
+		const {getByTestId} = render(
+			<AppContext.Provider value={{isAmPm: true}}>
+				<VelocityChart
+					timeRange={{
+						dateEnd: '2019-12-09T00:00:00Z',
+						dateStart: '2019-12-03T00:00:00Z'
+					}}
+					velocityData={velocityData}
+					velocityUnit={{key: 'Days', name: 'Int / Day'}}
+				/>
+			</AppContext.Provider>
+		);
 
-	const AppContextState = {
-		client: fetch(data),
-		companyId: '12345',
-		defaultDelta: 20,
-		deltas: 0,
-		isAmPm: true,
-		maxPages: 10,
-		namespace: 'test',
-		setStatus() {},
-		setTitle() {},
-		status: null,
-		title: Liferay.Language.get('metrics')
-	};
+		const velocityChat = await waitForElement(() =>
+			getByTestId('velocity-chart')
+		);
 
-	const getSelectedTimeRange = () => true;
-
-	const Wrapper = () => (
-		<AppContext.Provider value={AppContextState}>
-			<Router client={fetch(data)}>
-				<ErrorContext.Provider value={{error: '', setError: () => {}}}>
-					<LoadingContext.Provider value={{setLoading: () => {}}}>
-						<TimeRangeContext.Provider
-							value={{
-								getSelectedTimeRange
-							}}
-						>
-							<VelocityUnitContext.Provider
-								value={{
-									getSelectedVelocityUnit: () => ({})
-								}}
-							>
-								<VelocityDataContext.Provider
-									value={{
-										velocityData
-									}}
-								>
-									<VelocityChart />
-								</VelocityDataContext.Provider>
-							</VelocityUnitContext.Provider>
-						</TimeRangeContext.Provider>
-					</LoadingContext.Provider>
-				</ErrorContext.Provider>
-			</Router>
-		</AppContext.Provider>
-	);
-
-	const {container, getByTestId} = render(<Wrapper></Wrapper>);
-
-	const element = await waitForElement(() => getByTestId('velocity-chart'), {
-		container
+		expect(velocityChat).toBeInTheDocument();
 	});
 
-	expect(element).toBeInTheDocument();
-});
+	describe('Be render tooltip', () => {
+		test('with valid dates', async () => {
+			const timeRange = {
+				active: true,
+				dateEnd: '2019-09-25T17:06:52.000Z',
+				dateStart: '2019-06-28T00:00:00.000Z',
+				defaultTimeRange: false,
+				description: 'Jun 28 - Sep 25',
+				id: 90,
+				key: '90',
+				name: 'Last 90 Days'
+			};
 
-test('Should render velocity chart tooltip', async () => {
-	const timeRange = {
-		active: true,
-		dateEnd: '2019-09-25T17:06:52.000Z',
-		dateStart: '2019-06-28T00:00:00.000Z',
-		defaultTimeRange: false,
-		description: 'Jun 28 - Sep 25',
-		id: 90,
-		key: '90',
-		name: 'Last 90 Days'
-	};
+			const dataPoints = [
+				{
+					id: 'data_1',
+					index: 5,
+					name: 'data_1',
+					value: 0,
+					x: new Date('2019-09-24T05:00:00.000Z')
+				}
+			];
 
-	const dataPoints = [
-		{
-			id: 'data_1',
-			index: 5,
-			name: 'data_1',
-			value: 0,
-			x: new Date('2019-09-24T05:00:00.000Z')
-		}
-	];
+			const text = VelocityChart.Tooltip(
+				true,
+				timeRange,
+				'Months',
+				'Inst / Month'
+			)(dataPoints);
 
-	const text = VelocityChart.Tooltip(
-		true,
-		timeRange,
-		'Months',
-		'Inst / Month'
-	)(dataPoints);
+			const textDefault = VelocityChart.Tooltip(
+				true,
+				timeRange,
+				'test',
+				'Inst / Month'
+			)(dataPoints);
 
-	const textDefault = VelocityChart.Tooltip(
-		true,
-		timeRange,
-		'test',
-		'Inst / Month'
-	)(dataPoints);
+			const textHours = VelocityChart.Tooltip(
+				true,
+				timeRange,
+				'Hours',
+				'Inst / Hours'
+			)(dataPoints);
 
-	const textHours = VelocityChart.Tooltip(
-		true,
-		timeRange,
-		'Hours',
-		'Inst / Hours'
-	)(dataPoints);
+			const textHoursAmPm = VelocityChart.Tooltip(
+				false,
+				timeRange,
+				'Hours',
+				'Inst / Hours'
+			)(dataPoints);
 
-	const textHoursAmPm = VelocityChart.Tooltip(
-		false,
-		timeRange,
-		'Hours',
-		'Inst / Hours'
-	)(dataPoints);
+			const textMonths = VelocityChart.Tooltip(
+				true,
+				timeRange,
+				'Months',
+				'Inst / Months'
+			)(dataPoints);
 
-	const textMonths = VelocityChart.Tooltip(
-		true,
-		timeRange,
-		'Months',
-		'Inst / Months'
-	)(dataPoints);
+			const textWeeks = VelocityChart.Tooltip(
+				true,
+				timeRange,
+				'Weeks',
+				'Inst / Weeks'
+			)(dataPoints);
 
-	const textWeeks = VelocityChart.Tooltip(
-		true,
-		timeRange,
-		'Weeks',
-		'Inst / Weeks'
-	)(dataPoints);
+			const textYears = VelocityChart.Tooltip(
+				true,
+				timeRange,
+				'Years',
+				'Inst / Years'
+			)(dataPoints);
 
-	const textYears = VelocityChart.Tooltip(
-		true,
-		timeRange,
-		'Years',
-		'Inst / Years'
-	)(dataPoints);
+			expect(textHours).toContain('0 Inst / Hours');
+			expect(textHoursAmPm).toContain('0 Inst / Hours');
+			expect(textMonths).toContain('0 Inst / Months');
+			expect(textWeeks).toContain('0 Inst / Weeks');
+			expect(textYears).toContain('0 Inst / Years');
+			expect(text).toContain('0 Inst / Month');
+			expect(textDefault).toContain('0 Inst / Month');
+		});
 
-	expect(textHours.includes('0 Inst / Hours')).toBe(true);
-	expect(textHoursAmPm.includes('0 Inst / Hours')).toBe(true);
-	expect(textMonths.includes('0 Inst / Months')).toBe(true);
-	expect(textWeeks.includes('0 Inst / Weeks')).toBe(true);
-	expect(textYears.includes('0 Inst / Years')).toBe(true);
-	expect(text.includes('0 Inst / Month')).toBe(true);
-	expect(textDefault.includes('0 Inst / Month')).toBe(true);
-});
+		test('with invalid date', async () => {
+			const timeRange = {
+				active: true,
+				dateEnd: '2019-09-25T17:06:52.000Z',
+				dateStart: '2019-06-28T00:00:00.000Z',
+				defaultTimeRange: false,
+				description: 'Jun 28 - Sep 25',
+				id: 90,
+				key: '90',
+				name: 'Last 90 Days'
+			};
 
-test('Should render velocity chart tooltip with invalid date', async () => {
-	const timeRange = {
-		active: true,
-		dateEnd: '2019-09-25T17:06:52.000Z',
-		dateStart: '2019-06-28T00:00:00.000Z',
-		defaultTimeRange: false,
-		description: 'Jun 28 - Sep 25',
-		id: 90,
-		key: '90',
-		name: 'Last 90 Days'
-	};
+			const dataPoints = [
+				{
+					id: 'data_1',
+					index: 5,
+					name: 'data_1',
+					value: 0,
+					x: new Date('1936-09-25T17:06:52.000Z')
+				}
+			];
 
-	const dataPoints = [
-		{
-			id: 'data_1',
-			index: 5,
-			name: 'data_1',
-			value: 0,
-			x: new Date('1936-09-25T17:06:52.000Z')
-		}
-	];
+			const textHours = VelocityChart.Tooltip(
+				true,
+				timeRange,
+				'Hours',
+				'Inst / Hours'
+			)(dataPoints);
 
-	const textHours = VelocityChart.Tooltip(
-		true,
-		timeRange,
-		'Hours',
-		'Inst / Hours'
-	)(dataPoints);
+			dataPoints[0].x = null;
 
-	dataPoints[0].x = null;
+			const textDefault = VelocityChart.Tooltip(
+				true,
+				timeRange,
+				'Hours',
+				'Inst / Hours'
+			)(dataPoints);
 
-	const textDefault = VelocityChart.Tooltip(
-		true,
-		timeRange,
-		'Hours',
-		'Inst / Hours'
-	)(dataPoints);
-
-	expect(textHours.includes('0 Inst / Hours')).toBe(true);
-	expect(textDefault.includes('0 Inst / Hours')).toBe(true);
+			expect(textHours).toContain('0 Inst / Hours');
+			expect(textDefault).toContain('0 Inst / Hours');
+		});
+	});
 });
