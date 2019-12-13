f9d420b0e72cc4f19d2044eed77ce00c10214217
Alberto Chaparro <alberto.chaparro@liferay.com>
2019-12-13 11:11:59 -0800

    LPS-105822 Add missing upgrade after LPS-104849

--- modules/apps/layout/layout-seo-service/src/main/java/com/liferay/layout/seo/internal/upgrade/LayoutSEOServiceUpgrade.java
+++ modules/apps/layout/layout-seo-service/src/main/java/com/liferay/layout/seo/internal/upgrade/LayoutSEOServiceUpgrade.java
@@ -15,6 +15,7 @@
 package com.liferay.layout.seo.internal.upgrade;
 
 import com.liferay.layout.seo.internal.upgrade.v2_0_0.UpgradeSEOEntry;
+import com.liferay.layout.seo.internal.upgrade.v2_1_0.UpgradeSchema;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
@@ -28,6 +29,8 @@ public class LayoutSEOServiceUpgrade implements UpgradeStepRegistrator {
 	@Override
 	public void register(Registry registry) {
 		registry.register("1.0.0", "2.0.0", new UpgradeSEOEntry());
+
+		registry.register("2.0.0", "2.1.0", new UpgradeSchema());
 	}
 
 }
---
+++ modules/apps/layout/layout-seo-service/src/main/resources/com/liferay/layout/seo/internal/upgrade/v2_1_0/dependencies/update.sql
@@ -0,0 +1,19 @@
+create table LayoutSEOSite (
+	mvccVersion LONG default 0 not null,
+	uuid_ VARCHAR(75) null,
+	layoutSEOSiteId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	openGraphEnabled BOOLEAN,
+	openGraphImageFileEntryId LONG
+);
+
+create unique index IX_6E43ACCA on LayoutSEOSite (groupId);
+create index IX_24696DD4 on LayoutSEOSite (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_C75B5956 on LayoutSEOSite (uuid_[$COLUMN_LENGTH:75$], groupId);
+
+COMMIT_TRANSACTION;
