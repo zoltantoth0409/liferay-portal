77f798c2a02474020f2df378d9e982e04b3df70b
Diego Nascimento <diego.nascimento@liferay.com>
2019-12-13 12:02:23 -0300

    LPS-105135 Creates a command function that retrieves information from a ItemSelectorDialog and return a callback with the title and url of the content

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/core/openImageSelector.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {ItemSelectorDialog} from 'frontend-js-web';
+
+export function openImageSelector(config, callback, destroyedCallback = null) {
+	const {imageSelectorURL, portletNamespace} = config;
+
+	const itemSelectorDialog = new ItemSelectorDialog({
+		eventName: `${portletNamespace}selectImage`,
+		singleSelect: true,
+		title: Liferay.Language.get('select'),
+		url: imageSelectorURL
+	});
+
+	itemSelectorDialog.on('selectedItemChange', event => {
+		const selectedItem = event.selectedItem || {};
+
+		const {returnType, value} = selectedItem;
+		const selectedImage = {};
+
+		if (returnType === 'URL') {
+			selectedImage.title = value;
+			selectedImage.url = value;
+		}
+
+		if (returnType) {
+			const fileEntry = JSON.parse(value);
+
+			selectedImage.title = fileEntry.title;
+			selectedImage.url = fileEntry.url;
+		}
+
+		if (selectedImage.url) {
+			callback(selectedImage);
+		}
+	});
+
+	itemSelectorDialog.on('visibleChange', () => {
+		if (destroyedCallback) {
+			destroyedCallback();
+		}
+	});
+
+	itemSelectorDialog.open();
+}
