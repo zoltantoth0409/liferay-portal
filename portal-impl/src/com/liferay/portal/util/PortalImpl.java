6c8e3fa38bc6edcbb187ee6c3aecd97d138c8d7e
Leon Chi <leon.chi@liferay.com>
2019-12-16 10:49:37 +0800

    LPS-57105 URL decode when needed

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -6408,6 +6408,10 @@ public class PortalImpl implements Portal {
 			return true;
 		}
 
+		if (resourceId.contains("%")) {
+			resourceId = HttpUtil.decodePath(resourceId);
+		}
+
 		Matcher matcher = _bannedResourceIdPattern.matcher(resourceId);
 
 		if (matcher.matches()) {
