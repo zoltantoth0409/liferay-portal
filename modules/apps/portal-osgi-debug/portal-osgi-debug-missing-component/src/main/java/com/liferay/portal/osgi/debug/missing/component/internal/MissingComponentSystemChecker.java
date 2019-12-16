ac8c0aae8a014e7e68db097a8109cabee0ebf26d
Matthew Tambara <matthew.tambara@liferay.com>
2019-12-16 14:58:08 -0800

    LPS-105900 Make new ci-only module for checking bundle blacklist manager status when service is unavailable

---
+++ modules/apps/portal-osgi-debug/portal-osgi-debug-missing-component/src/main/java/com/liferay/portal/osgi/debug/missing/component/internal/MissingComponentSystemChecker.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.osgi.debug.missing.component.internal;
+
+import com.liferay.portal.osgi.debug.SystemChecker;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.runtime.ServiceComponentRuntime;
+
+/**
+ * @author Matthew Tambara
+ */
+@Component(immediate = true, service = SystemChecker.class)
+public class MissingComponentSystemChecker implements SystemChecker {
+
+	@Override
+	public String check() {
+		return MissingComponentUtil.scan(
+			_bundleContext, _serviceComponentRuntime);
+	}
+
+	@Override
+	public String getName() {
+		return "Missing Component Scanner";
+	}
+
+	@Override
+	public String getOSGiCommand() {
+		return "ds:missing";
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+	}
+
+	private BundleContext _bundleContext;
+
+	@Reference
+	private ServiceComponentRuntime _serviceComponentRuntime;
+
+}
---
+++ modules/apps/portal-osgi-debug/portal-osgi-debug-missing-component/src/main/java/com/liferay/portal/osgi/debug/missing/component/internal/MissingComponentUtil.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.osgi.debug.missing.component.internal;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.bundle.blacklist.BundleBlacklistManager;
+
+import java.util.Objects;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.runtime.ServiceComponentRuntime;
+import org.osgi.service.component.runtime.dto.ComponentConfigurationDTO;
+import org.osgi.service.component.runtime.dto.ComponentDescriptionDTO;
+import org.osgi.service.component.runtime.dto.SatisfiedReferenceDTO;
+import org.osgi.service.component.runtime.dto.UnsatisfiedReferenceDTO;
+
+/**
+ * @author Matthew Tambara
+ */
+public class MissingComponentUtil {
+
+	public static String scan(
+		BundleContext bundleContext,
+		ServiceComponentRuntime serviceComponentRuntime) {
+
+		Bundle blacklistBundle = null;
+
+		for (Bundle bundle : bundleContext.getBundles()) {
+			if (Objects.equals(
+					bundle.getSymbolicName(),
+					"com.liferay.portal.bundle.blacklist.impl")) {
+
+				blacklistBundle = bundle;
+
+				break;
+			}
+		}
+
+		ServiceReference<BundleBlacklistManager> serviceReference =
+			bundleContext.getServiceReference(BundleBlacklistManager.class);
+
+		if (serviceReference != null) {
+			return null;
+		}
+
+		ComponentDescriptionDTO componentDescriptionDTO =
+			serviceComponentRuntime.getComponentDescriptionDTO(
+				blacklistBundle,
+				"com.liferay.portal.bundle.blacklist.internal." +
+					"BundleBlacklistManagerImpl");
+
+		StringBundler sb = new StringBundler();
+
+		sb.append("@@@@Name: ");
+		sb.append(componentDescriptionDTO.name);
+		sb.append("\n@@Instances:");
+
+		for (ComponentConfigurationDTO componentConfigurationDTO :
+				serviceComponentRuntime.getComponentConfigurationDTOs(
+					componentDescriptionDTO)) {
+
+			sb.append("\nid: ");
+			sb.append(componentConfigurationDTO.id);
+			sb.append("\nState: ");
+			sb.append(componentConfigurationDTO.state);
+			sb.append("\n Satisfied references: ");
+
+			for (SatisfiedReferenceDTO satisfiedReference :
+					componentConfigurationDTO.satisfiedReferences) {
+
+				sb.append("\n\tName: ");
+				sb.append(satisfiedReference.name);
+			}
+
+			sb.append("\nUnsatisfied References: ");
+
+			for (UnsatisfiedReferenceDTO unsatisfiedReference :
+					componentConfigurationDTO.unsatisfiedReferences) {
+
+				sb.append("\n\tName: ");
+				sb.append(unsatisfiedReference.name);
+			}
+		}
+
+		return sb.toString();
+	}
+
+}
