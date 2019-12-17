ff4b7a156aa904f196316e0fda12e1c3dcda2710
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-12-17 10:56:01 +0100

    LPS-105858 Auto SF

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
@@ -551,8 +551,8 @@ public class AssetPublisherDisplayContext {
 
 			if (Objects.equals(queryName, "assetTags")) {
 				String[] tagNames = StringUtil.split(
-						queryValues, StringPool.COMMA);
-				
+					queryValues, StringPool.COMMA);
+
 				tagNames = ParamUtil.getStringValues(
 					_httpServletRequest, "queryTagNames" + queryLogicIndex,
 					tagNames);
