8d17c6bc449def8f66065538b0f0fba7abb7cd54
Javier Gamarra <nhpatt@gmail.com>
2019-12-18 09:07:49 +0100

    LPS-105982 pass application path (retrieving it from OSGi properties is very verbose)

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/StructuredContentDTOConverter.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/StructuredContentDTOConverter.java
@@ -263,6 +263,7 @@ public class StructuredContentDTOConverter implements DTOConverter {
 			ddmTemplate -> new RenderedContent() {
 				{
 					renderedContentURL = JaxRsLinkUtil.getJaxRsLink(
+						"headless-delivery",
 						BaseStructuredContentResourceImpl.class,
 						"getStructuredContentRenderedContentTemplate",
 						uriInfoOptional.get(),
