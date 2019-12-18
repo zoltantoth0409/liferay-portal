5e4cae3ba0b7413c936f898c90b556d089aa0c41
ChrisKian <christopher.kian@liferay.com>
2019-12-17 17:54:37 -0800

    LPS-105994 Ensure user messageIDs are unique

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -6984,7 +6984,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 			mailMessage.setMessageId(
 				PortalUtil.getMailId(
-					company.getMx(), "user", toUser.getUserId()));
+					company.getMx(), "user", toUser.getUserId(),
+					System.currentTimeMillis()));
 
 			mailService.sendEmail(mailMessage);
 		}
