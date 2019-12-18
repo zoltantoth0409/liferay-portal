4ddd6bc483a9007ca63b47291ef1e260cfc8f64e
Pavel Savinov <pavel.savinov@liferay.com>
2019-12-18 14:27:25 +0100

    LPS-102200 Add method to check if current master page is being used by any content page or page template

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -431,6 +431,8 @@ public class ContentPageEditorDisplayContext {
 			"masterLayoutData",
 			JSONFactoryUtil.createJSONObject(_getMasterLayoutData())
 		).put(
+			"masterUsed", _isMasterUsed()
+		).put(
 			"pageContents",
 			ContentUtil.getPageContentsJSONArray(
 				themeDisplay.getPlid(), request)
@@ -496,6 +498,8 @@ public class ContentPageEditorDisplayContext {
 			"draft", modifiedDate.after(publishDate)
 		).put(
 			"lastSaveDate", StringPool.BLANK
+		).put(
+			"masterUsed", _isMasterUsed()
 		).put(
 			"pageType", String.valueOf(_getPageType())
 		).put(
@@ -1291,12 +1295,18 @@ public class ContentPageEditorDisplayContext {
 	}
 
 	private int _getPageType() throws PortalException {
+		if (_pageType != null) {
+			return _pageType;
+		}
+
 		Layout publishedLayout = _getPublishedLayout();
 
 		if (Objects.equals(
 				publishedLayout.getType(), LayoutConstants.TYPE_PORTLET)) {
 
-			return LayoutConverterTypeConstants.TYPE_CONVERSION;
+			_pageType = LayoutConverterTypeConstants.TYPE_CONVERSION;
+
+			return _pageType;
 		}
 
 		Layout layout = themeDisplay.getLayout();
@@ -1312,10 +1322,13 @@ public class ContentPageEditorDisplayContext {
 		}
 
 		if (layoutPageTemplateEntry == null) {
-			return LayoutPageTemplateEntryTypeConstants.TYPE_BASIC;
+			_pageType = LayoutPageTemplateEntryTypeConstants.TYPE_BASIC;
+		}
+		else {
+			_pageType = layoutPageTemplateEntry.getType();
 		}
 
-		return layoutPageTemplateEntry.getType();
+		return _pageType;
 	}
 
 	private String _getPortletCategoryTitle(PortletCategory portletCategory) {
@@ -1573,6 +1586,25 @@ public class ContentPageEditorDisplayContext {
 		return false;
 	}
 
+	private boolean _isMasterUsed() throws PortalException {
+		if (_getPageType() !=
+				LayoutPageTemplateEntryTypeConstants.TYPE_MASTER_LAYOUT) {
+
+			return false;
+		}
+
+		Layout layout = _getPublishedLayout();
+
+		int masterUsagesCount = LayoutLocalServiceUtil.getLayoutsCount(
+			themeDisplay.getScopeGroupId(), layout.getPlid());
+
+		if (masterUsagesCount > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
 	private boolean _isUsed(Portlet portlet, long plid) {
 		if (portlet.isInstanceable()) {
 			return false;
@@ -1615,6 +1647,7 @@ public class ContentPageEditorDisplayContext {
 	private ItemSelectorCriterion _imageItemSelectorCriterion;
 	private final ItemSelector _itemSelector;
 	private String _layoutData;
+	private Integer _pageType;
 	private Layout _publishedLayout;
 	private String _redirect;
 	private final RenderResponse _renderResponse;
