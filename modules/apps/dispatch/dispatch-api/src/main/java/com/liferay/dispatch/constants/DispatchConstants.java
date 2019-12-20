0e544bf4c710f3031fad33be88b4bacc70b5e864
Igor Beslic <igor.beslic@liferay.com>
2019-12-20 11:32:02 +0100

    COMMERCE-2475 dispatch-api - Reorganize - rename

--- modules/apps/dispatch/dispatch-api/src/main/java/com/liferay/dispatch/constants/DispatchConstants.java
+++ modules/apps/dispatch/dispatch-api/src/main/java/com/liferay/dispatch/constants/DispatchConstants.java
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- *
- *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 package com.liferay.dispatch.constants;
@@ -19,21 +19,16 @@ package com.liferay.dispatch.constants;
  */
 public class DispatchConstants {
 
-	public static final String CATEGORY_KEY_COMMERCE_DATA_INTEGRATION_DETAILS =
-		"details";
+	public static final String CATEGORY_KEY_DISPATCH_DETAILS = "details";
 
-	public static final String CATEGORY_KEY_COMMERCE_DATA_INTEGRATION_LOGS =
-		"logs";
+	public static final String CATEGORY_KEY_DISPATCH_LOGS = "logs";
 
-	public static final String
-		CATEGORY_KEY_COMMERCE_DATA_INTEGRATION_SCHEDULED_TASK =
-			"scheduled-task";
+	public static final String CATEGORY_KEY_DISPATCH_TASK = "dispatch-task";
 
 	public static final String EXECUTOR_DESTINATION_NAME =
-		"liferay/commerce/data_integration/executor";
+		"liferay/dispatch/executor";
 
-	public static final String
-		SCREEN_NAVIGATION_KEY_COMMERCE_DATA_INTEGRATION_GENERAL =
-			"commerce.data.integration.general";
+	public static final String SCREEN_NAVIGATION_KEY_DISPATCH_GENERAL =
+		"dispatch.general";
 
 }
