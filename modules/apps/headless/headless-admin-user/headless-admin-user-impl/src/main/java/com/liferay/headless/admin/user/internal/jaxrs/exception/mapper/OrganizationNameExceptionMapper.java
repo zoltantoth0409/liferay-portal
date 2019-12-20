0cbf0b7cc70ffded4fb5b2339967f7b896fa2549
drewbrokke <drew.brokke@liferay.com>
2019-12-19 16:12:10 -0800

    LPS-105616 headless-admin-user-impl - adds exception mappers to report organization and address exceptions in the response

---
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/jaxrs/exception/mapper/OrganizationNameExceptionMapper.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.user.internal.jaxrs.exception.mapper;
+
+import com.liferay.portal.kernel.exception.OrganizationNameException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.User)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.User.OrganizationNameException"
+	},
+	service = ExceptionMapper.class
+)
+public class OrganizationNameExceptionMapper
+	extends BaseExceptionMapper<OrganizationNameException> {
+
+	@Override
+	protected Problem getProblem(
+		OrganizationNameException organizationNameException) {
+
+		return new Problem(
+			Response.Status.BAD_REQUEST,
+			"A name is required to create an organization");
+	}
+
+}
