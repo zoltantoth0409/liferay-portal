620a2ddc5cd4b7b419c6cab2009912d42b47b29e
Eric Yan <eric.yan@liferay.com>
2019-12-20 17:09:42 -0800

    LPS-105899 org-apache-axis: Partial revert of "LPS-104001 Unified sorting for overloading methods."

--- modules/third-party/org-apache-axis/src/main/java/org/apache/axis/description/JavaServiceDesc.java
+++ modules/third-party/org-apache-axis/src/main/java/org/apache/axis/description/JavaServiceDesc.java
@@ -40,7 +40,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -532,7 +531,8 @@ public class JavaServiceDesc implements ServiceDesc {
                 {
                     Method meth1 = ((OperationDesc)o1).getMethod();
                     Method meth2 = ((OperationDesc)o2).getMethod();
-                    return meth2.toString().compareTo(meth1.toString());
+                    return (meth1.getParameterTypes().length -
+                                         meth2.getParameterTypes().length);
                 }
             });
 
@@ -641,13 +641,6 @@ public class JavaServiceDesc implements ServiceDesc {
         // Find the method.  We do this once for each Operation.
         
         Method[] methods = getMethods(implClass);
-
-		Arrays.sort(methods, new Comparator<Method>() {
-			@Override
-			public int compare(Method m1, Method m2) {
-				return m2.toString().compareTo(m1.toString());
-			}
-		});
         // A place to keep track of possible matches
         Method possibleMatch = null;
         
