ba5ca01d12b74b47708e7f57b4648294fba379ac
Javier Gamarra <nhpatt@gmail.com>
2019-12-22 23:42:10 +0100

    LPS-105228 add a new registry to be potentially used with all the DTOs (that can allow for extension)

---
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/dto/converter/DTOConverterRegistryImpl.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.internal.dto.converter;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Rubén Pulido
+ * @author Víctor Galán
+ */
+@Component(service = DTOConverterRegistry.class)
+public class DTOConverterRegistryImpl implements DTOConverterRegistry {
+
+	public DTOConverter getDTOConverter(String assetEntryClassName) {
+		return _serviceTrackerMap.getService(assetEntryClassName);
+	}
+
+	@Activate
+	protected void activate(final BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, DTOConverter.class, "(dto.class.name=*)",
+			(serviceReference, emitter) -> {
+				String assetEntryClassName =
+					(String)serviceReference.getProperty("dto.class.name");
+
+				emitter.emit(assetEntryClassName);
+			});
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private ServiceTrackerMap<String, DTOConverter> _serviceTrackerMap;
+
+}
