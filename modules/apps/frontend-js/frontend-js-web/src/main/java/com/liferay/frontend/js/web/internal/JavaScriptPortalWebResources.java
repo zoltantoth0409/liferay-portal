00a88f2438db058f4a501da313fb083223ef16e3
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2019-12-23 11:20:57 +0100

    LPS-104365 Javascript generated files can depend on ResourceBundle keys, not only on the bundle own modification, so the last modified time should take into account ResourceBundle bundles also.

--- modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/JavaScriptPortalWebResources.java
+++ modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/JavaScriptPortalWebResources.java
@@ -14,16 +14,27 @@
 
 package com.liferay.frontend.js.web.internal;
 
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
 import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.ServletContext;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
 /**
  * @author Peter Fellwock
@@ -38,7 +49,7 @@ public class JavaScriptPortalWebResources implements PortalWebResources {
 
 	@Override
 	public long getLastModified() {
-		return _bundle.getLastModified();
+		return _lastModified.get();
 	}
 
 	@Override
@@ -52,8 +63,37 @@ public class JavaScriptPortalWebResources implements PortalWebResources {
 	}
 
 	@Activate
-	protected void activate(BundleContext bundleContext) {
-		_bundle = bundleContext.getBundle();
+	protected void activate(BundleContext bundleContext) throws Exception {
+		_bundleContext = bundleContext;
+
+		Bundle bundle = _bundleContext.getBundle();
+
+		_lastModified = new AtomicLong(bundle.getLastModified());
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_bundleContext.removeServiceListener(_resourceBundleServiceListener);
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		target = ModuleServiceLifecycle.PORTAL_INITIALIZED
+	)
+	protected void setModuleServiceLifecycle(
+			ModuleServiceLifecycle moduleServiceLifecycle)
+		throws Exception {
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			JavaScriptPortalWebResources.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		bundleContext.addServiceListener(
+			_resourceBundleServiceListener,
+			"(&(!(javax.portlet.name=*))(language.id=*)(objectClass=" +
+				ResourceBundle.class.getName() + "))");
 	}
 
 	@Reference(
@@ -64,7 +104,32 @@ public class JavaScriptPortalWebResources implements PortalWebResources {
 		_servletContext = servletContext;
 	}
 
-	private Bundle _bundle;
+	protected void unsetModuleServiceLifecycle(
+		ModuleServiceLifecycle moduleServiceLifecycle) {
+
+		_bundleContext.removeServiceListener(_resourceBundleServiceListener);
+	}
+
+	private BundleContext _bundleContext;
+	private AtomicLong _lastModified;
+	private final ResourceBundleServiceListener _resourceBundleServiceListener =
+		new ResourceBundleServiceListener();
 	private ServletContext _servletContext;
+
+	private class ResourceBundleServiceListener implements ServiceListener {
+
+		@Override
+		public void serviceChanged(ServiceEvent serviceEvent) {
+			ServiceReference<?> serviceReference =
+				serviceEvent.getServiceReference();
+
+			Bundle bundle = serviceReference.getBundle();
+
+			_lastModified.getAndUpdate(
+				lastModified -> Math.max(
+					lastModified, bundle.getLastModified()));
+		}
+
+	}
 
 }
