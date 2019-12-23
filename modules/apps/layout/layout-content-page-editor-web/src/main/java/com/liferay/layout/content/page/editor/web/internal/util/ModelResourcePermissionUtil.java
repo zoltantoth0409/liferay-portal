b8eb67d1d8142f17d12fa57e8f45d8c82bcfc4a5
Rubén Pulido <ruben.pulido@liferay.com>
2019-12-23 11:53:32 +0100

    LPS-103454 Use ModelResourcePermission in Util class. Fix typo in class name.

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/ModelResourcePermissionUtil.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.util;
+
+import com.liferay.layout.util.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(immediate = true, service = {})
+public class ModelResourcePermissionUtil {
+
+	public static boolean contains(
+		PermissionChecker permissionChecker, String className, long classPK,
+		String actionId) {
+
+		return _modelResourcePermission.contains(
+			permissionChecker, className, classPK, actionId);
+	}
+
+	public boolean contains(
+		PermissionChecker permissionChecker, long plid, String actionId) {
+
+		return _modelResourcePermission.contains(
+			permissionChecker, plid, actionId);
+	}
+
+	@Reference(unbind = "-")
+	protected void setsModelResourcePermission(
+		ModelResourcePermission modelResourcePermission) {
+
+		_modelResourcePermission = modelResourcePermission;
+	}
+
+	private static ModelResourcePermission _modelResourcePermission;
+
+}
