28114b83b84776991bfc00252516c66d9984aef9
hudakl <laszlo.hudak@liferay.com>
2019-12-23 10:22:22 +0100

    LPS-103335 Author and copyright information

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/exportimport/portlet/preferences/processor/IGDisplayExportImportPortletPreferencesProcessorUtil.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.exportimport.portlet.preferences.processor;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.template.TemplateHandler;
+import com.liferay.portal.kernel.template.TemplateHandlerRegistryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import javax.portlet.PortletPreferences;
+
+/**
+ * @author Laszlo Hudak
+ */
+public class IGDisplayExportImportPortletPreferencesProcessorUtil {
+
+	public static String getDisplayStyle(
+		PortletPreferences portletPreferences) {
+
+		try {
+			TemplateHandler templateHandler =
+				TemplateHandlerRegistryUtil.getTemplateHandler(
+					FileEntry.class.getName());
+
+			if (templateHandler != null) {
+				return portletPreferences.getValue("displayStyle", null);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		return null;
+	}
+
+	public static long getDisplayStyleGroupId(
+		PortletPreferences portletPreferences) {
+
+		try {
+			TemplateHandler templateHandler =
+				TemplateHandlerRegistryUtil.getTemplateHandler(
+					FileEntry.class.getName());
+
+			if (templateHandler != null) {
+				return GetterUtil.getLong(
+					portletPreferences.getValue("displayStyleGroupId", null));
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+
+		return 0;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		IGDisplayExportImportPortletPreferencesProcessorUtil.class);
+
+}
---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/exportimport/portlet/preferences/processor/IGDisplayPortletDisplayTemplateExportCapability.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.exportimport.portlet.preferences.processor;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.portlet.display.template.constants.PortletDisplayTemplateConstants;
+import com.liferay.portlet.display.template.exportimport.portlet.preferences.processor.PortletDisplayTemplateRegister;
+
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Provides the implementation of the export capability for the Media Gallery
+ * portlet. This allows the display style and display style group ID to be
+ * provided based on the existence of the template handler.
+ *
+ * @author Laszlo Hudak
+ */
+@Component(
+	immediate = true,
+	property = {
+		"name=IGDisplayExportCapability",
+		"type=" + PortletDisplayTemplateConstants.DISPLAY_TEMPLATE_EXPORT
+	},
+	service = PortletDisplayTemplateRegister.class
+)
+public class IGDisplayPortletDisplayTemplateExportCapability
+	implements PortletDisplayTemplateRegister {
+
+	@Override
+	public String getDisplayStyle(
+		PortletDataContext portletDataContext, String portletId,
+		PortletPreferences portletPreferences) {
+
+		return IGDisplayExportImportPortletPreferencesProcessorUtil.
+			getDisplayStyle(portletPreferences);
+	}
+
+	@Override
+	public long getDisplayStyleGroupId(
+		PortletDataContext portletDataContext, String portletId,
+		PortletPreferences portletPreferences) {
+
+		return IGDisplayExportImportPortletPreferencesProcessorUtil.
+			getDisplayStyleGroupId(portletPreferences);
+	}
+
+}
