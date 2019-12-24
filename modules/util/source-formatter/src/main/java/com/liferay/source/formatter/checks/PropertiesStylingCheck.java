7e21724fd6777b7f04ba54d0b595b1731a2817af
Hugo Huijser <hugo.huijser@liferay.com>
2019-12-24 13:32:50 -0800

    LPS-105380 Add logic to sort sql in *.properties

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesStylingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesStylingCheck.java
@@ -14,8 +14,15 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.checks.util.SourceUtil;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Alan Huang
+ * @author Hugo Huijser
  */
 public class PropertiesStylingCheck extends BaseFileCheck {
 
@@ -26,7 +33,49 @@ public class PropertiesStylingCheck extends BaseFileCheck {
 		content = content.replaceAll(
 			"(\\A|\n)( *[\\w.-]+)(( +=)|(= +))(.*)(\\Z|\n)", "$1$2=$6$7");
 
+		Matcher matcher = _sqlPattern.matcher(content);
+
+		while (matcher.find()) {
+			int lineNumber = getLineNumber(content, matcher.start());
+
+			String nextLine = StringUtil.trim(
+				SourceUtil.getLine(content, lineNumber + 1));
+
+			String nextSQLValue = _getSQLValue(nextLine, matcher.group(1));
+
+			if (nextSQLValue == null) {
+				continue;
+			}
+
+			String sqlValue = matcher.group(2);
+
+			if (sqlValue.compareTo(nextSQLValue) > 0) {
+				content = StringUtil.replaceFirst(
+					content, nextSQLValue, sqlValue,
+					getLineStartPos(content, lineNumber + 1));
+
+				return StringUtil.replaceFirst(
+					content, sqlValue, nextSQLValue,
+					getLineStartPos(content, lineNumber));
+			}
+		}
+
 		return content;
 	}
+
+	private String _getSQLValue(String line, String columnName) {
+		Pattern pattern = Pattern.compile("^\\(" + columnName + " ~ (\".*\")");
+
+		Matcher matcher = pattern.matcher(line);
+
+		if (matcher.find()) {
+			return matcher.group(1);
+		}
+
+		return null;
+	}
+
+	private static final Pattern _sqlPattern = Pattern.compile(
+		"\\s\\((.*) ~ (\".*\")\\) OR ");
 
 }
