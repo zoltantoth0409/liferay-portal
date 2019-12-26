99c0a6368c6cf23bbe579f30977260e47605b48c
Sarai Díaz García <dgarciasarai@gmail.com>
2019-12-26 13:47:38 +0100

    Revert "LPS-106278 Fix tests"
    This reverts commit d58ec1cf

--- modules/dxp/apps/segments/segments-experiment-web/test/js/renderApp.es.js
+++ modules/dxp/apps/segments/segments-experiment-web/test/js/renderApp.es.js
@@ -22,7 +22,7 @@ export default function renderApp({
 	initialGoals = segmentsGoals,
 	initialExperimentHistory = [],
 	initialSegmentsExperiences = [],
-	initialSegmentsExperiment = {},
+	initialSegmentsExperiment,
 	initialSegmentsVariants = [],
 	APIService = {},
 	selectedSegmentsExperienceId,
