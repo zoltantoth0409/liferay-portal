7eca56f27ff4c56a607619b6a73a7b6a9656bb41
Victor Galan <victor.galan@liferay.com>
2019-12-27 10:20:29 +0100

    LPS-106349 Add test for DeleteItemReactMVCActionCommand

---
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/DeleteItemReactMVCActionCommandTest.java
@@ -0,0 +1,487 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.portlet.action.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentCollectionLocalServiceUtil;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.fragment.service.FragmentEntryLocalServiceUtil;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.segments.constants.SegmentsExperienceConstants;
+
+import java.io.InputStream;
+
+import javax.portlet.ActionRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Víctor Galán
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class DeleteItemReactMVCActionCommandTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_company = _companyLocalService.getCompany(_group.getCompanyId());
+
+		_layout = LayoutTestUtil.addLayout(_group);
+
+		_fragmentCollection = _addFragmentCollection(_group.getGroupId());
+
+		_fragmentEntry = _addFragmentEntry(
+			_fragmentCollection.getFragmentCollectionId());
+	}
+
+	@Test
+	public void testDeleteContainerItem() throws Exception {
+		_addLayoutPageTemplateStructure(
+			_read("layout_data_with_children.json"));
+
+		MockLiferayPortletActionRequest actionRequest = _getMockActionRequest();
+
+		actionRequest.addParameter("itemId", "newItemId");
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "deleteItemJSONObject",
+			new Class<?>[] {ActionRequest.class}, actionRequest);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			_layoutPageTemplateStructure.getData(
+				SegmentsExperienceConstants.ID_DEFAULT));
+
+		JSONObject itemsJSONObject = jsonObject.getJSONObject("items");
+
+		JSONObject rootJSONObject = itemsJSONObject.getJSONObject("root");
+
+		Assert.assertEquals(1, itemsJSONObject.length());
+
+		Assert.assertNotNull(rootJSONObject);
+
+		JSONArray childrenJSONArray = rootJSONObject.getJSONArray("children");
+
+		Assert.assertEquals(0, childrenJSONArray.length());
+	}
+
+	@Test
+	public void testDeleteFragmentEntryLinkItem() throws Exception {
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				_fragmentEntry.getFragmentEntryId(),
+				PortalUtil.getClassNameId(Layout.class), _layout.getPlid(),
+				"css value", "<div>HTML value</div>", "js value",
+				"{fieldSets: []}", StringPool.BLANK, StringPool.BLANK, 0, null,
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId()));
+
+		String layoutData = _read("layout_data_with_fragment_entry_link.json");
+
+		_addLayoutPageTemplateStructure(
+			StringUtil.replace(
+				layoutData, "<id>",
+				String.valueOf(fragmentEntryLink.getFragmentEntryLinkId())));
+
+		MockLiferayPortletActionRequest actionRequest = _getMockActionRequest();
+
+		actionRequest.addParameter("itemId", "newItemId");
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "deleteItemJSONObject",
+			new Class<?>[] {ActionRequest.class}, actionRequest);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			_layoutPageTemplateStructure.getData(
+				SegmentsExperienceConstants.ID_DEFAULT));
+
+		JSONObject itemsJSONObject = jsonObject.getJSONObject("items");
+
+		JSONObject rootJSONObject = itemsJSONObject.getJSONObject("root");
+
+		Assert.assertEquals(1, itemsJSONObject.length());
+
+		Assert.assertNotNull(rootJSONObject);
+
+		JSONArray childrenJSONArray = rootJSONObject.getJSONArray("children");
+
+		Assert.assertEquals(0, childrenJSONArray.length());
+
+		Assert.assertNull(
+			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+				fragmentEntryLink.getFragmentEntryLinkId()));
+	}
+
+	@Test
+	public void testDeleteItemWithNestedFragmentEntryLink() throws Exception {
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				_fragmentEntry.getFragmentEntryId(),
+				PortalUtil.getClassNameId(Layout.class), _layout.getPlid(),
+				"css value", "<div>HTML value</div>", "js value",
+				"{fieldSets: []}", StringPool.BLANK, StringPool.BLANK, 0, null,
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId()));
+
+		String layoutData = _read("layout_data_with_fragment_entry_link.json");
+
+		_addLayoutPageTemplateStructure(
+			StringUtil.replace(
+				layoutData, "<id>",
+				String.valueOf(fragmentEntryLink.getFragmentEntryLinkId())));
+
+		MockLiferayPortletActionRequest actionRequest = _getMockActionRequest();
+
+		actionRequest.addParameter("itemId", "newItemId");
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "deleteItemJSONObject",
+			new Class<?>[] {ActionRequest.class}, actionRequest);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			_layoutPageTemplateStructure.getData(
+				SegmentsExperienceConstants.ID_DEFAULT));
+
+		JSONObject itemsJSONObject = jsonObject.getJSONObject("items");
+
+		JSONObject rootJSONObject = itemsJSONObject.getJSONObject("root");
+
+		Assert.assertEquals(1, itemsJSONObject.length());
+
+		Assert.assertNotNull(rootJSONObject);
+
+		JSONArray childrenJSONArray = rootJSONObject.getJSONArray("children");
+
+		Assert.assertEquals(0, childrenJSONArray.length());
+
+		Assert.assertNull(
+			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+				fragmentEntryLink.getFragmentEntryLinkId()));
+	}
+
+	@Test
+	public void testDeleteItemWithSeveralNestedFragmentEntryLink()
+		throws Exception {
+
+		FragmentEntryLink fragmentEntryLink1 =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				_fragmentEntry.getFragmentEntryId(),
+				PortalUtil.getClassNameId(Layout.class), _layout.getPlid(),
+				"css value", "<div>HTML value</div>", "js value",
+				"{fieldSets: []}", StringPool.BLANK, StringPool.BLANK, 0, null,
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId()));
+
+		FragmentEntryLink fragmentEntryLink2 =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				_fragmentEntry.getFragmentEntryId(),
+				PortalUtil.getClassNameId(Layout.class), _layout.getPlid(),
+				"css value", "<div>HTML value</div>", "js value",
+				"{fieldSets: []}", StringPool.BLANK, StringPool.BLANK, 0, null,
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId()));
+
+		String layoutData = _read(
+			"layout_data_with_several_nested_fragment_entry_link.json");
+
+		layoutData = StringUtil.replace(
+			layoutData, "<id1>",
+			String.valueOf(fragmentEntryLink1.getFragmentEntryLinkId()));
+
+		_addLayoutPageTemplateStructure(
+			StringUtil.replace(
+				layoutData, "<id2>",
+				String.valueOf(fragmentEntryLink2.getFragmentEntryLinkId())));
+
+		MockLiferayPortletActionRequest actionRequest = _getMockActionRequest();
+
+		actionRequest.addParameter("itemId", "newItemId");
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "deleteItemJSONObject",
+			new Class<?>[] {ActionRequest.class}, actionRequest);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			_layoutPageTemplateStructure.getData(
+				SegmentsExperienceConstants.ID_DEFAULT));
+
+		JSONObject itemsJSONObject = jsonObject.getJSONObject("items");
+
+		JSONObject rootJSONObject = itemsJSONObject.getJSONObject("root");
+
+		Assert.assertEquals(1, itemsJSONObject.length());
+
+		Assert.assertNotNull(rootJSONObject);
+
+		JSONArray childrenJSONArray = rootJSONObject.getJSONArray("children");
+
+		Assert.assertEquals(0, childrenJSONArray.length());
+
+		Assert.assertNull(
+			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+				fragmentEntryLink1.getFragmentEntryLinkId()));
+		Assert.assertNull(
+			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+				fragmentEntryLink2.getFragmentEntryLinkId()));
+	}
+
+	@Test
+	public void testDeleteItemWithTwoLevelNesting() throws Exception {
+		FragmentEntryLink fragmentEntryLink1 =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				_fragmentEntry.getFragmentEntryId(),
+				PortalUtil.getClassNameId(Layout.class), _layout.getPlid(),
+				"css value", "<div>HTML value</div>", "js value",
+				"{fieldSets: []}", StringPool.BLANK, StringPool.BLANK, 0, null,
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId()));
+
+		FragmentEntryLink fragmentEntryLink2 =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				_fragmentEntry.getFragmentEntryId(),
+				PortalUtil.getClassNameId(Layout.class), _layout.getPlid(),
+				"css value", "<div>HTML value</div>", "js value",
+				"{fieldSets: []}", StringPool.BLANK, StringPool.BLANK, 0, null,
+				ServiceContextTestUtil.getServiceContext(
+					_group, TestPropsValues.getUserId()));
+
+		String layoutData = _read("layout_data_with_two_level_nesting.json");
+
+		layoutData = StringUtil.replace(
+			layoutData, "<id1>",
+			String.valueOf(fragmentEntryLink1.getFragmentEntryLinkId()));
+
+		_addLayoutPageTemplateStructure(
+			StringUtil.replace(
+				layoutData, "<id2>",
+				String.valueOf(fragmentEntryLink2.getFragmentEntryLinkId())));
+
+		MockLiferayPortletActionRequest actionRequest = _getMockActionRequest();
+
+		actionRequest.addParameter("itemId", "newItemId");
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "deleteItemJSONObject",
+			new Class<?>[] {ActionRequest.class}, actionRequest);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+			_layoutPageTemplateStructure.getData(
+				SegmentsExperienceConstants.ID_DEFAULT));
+
+		JSONObject itemsJSONObject = jsonObject.getJSONObject("items");
+
+		JSONObject rootJSONObject = itemsJSONObject.getJSONObject("root");
+
+		Assert.assertEquals(1, itemsJSONObject.length());
+
+		Assert.assertNotNull(rootJSONObject);
+
+		JSONArray childrenJSONArray = rootJSONObject.getJSONArray("children");
+
+		Assert.assertEquals(0, childrenJSONArray.length());
+
+		Assert.assertNull(
+			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+				fragmentEntryLink1.getFragmentEntryLinkId()));
+		Assert.assertNull(
+			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+				fragmentEntryLink2.getFragmentEntryLinkId()));
+	}
+
+	private FragmentCollection _addFragmentCollection(long groupId)
+		throws PortalException {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(groupId);
+
+		return FragmentCollectionLocalServiceUtil.addFragmentCollection(
+			TestPropsValues.getUserId(), groupId, RandomTestUtil.randomString(),
+			StringPool.BLANK, serviceContext);
+	}
+
+	private FragmentEntry _addFragmentEntry(long fragmentCollectionId)
+		throws PortalException {
+
+		FragmentCollection fragmentCollection =
+			FragmentCollectionLocalServiceUtil.getFragmentCollection(
+				fragmentCollectionId);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				fragmentCollection.getGroupId());
+
+		return FragmentEntryLocalServiceUtil.addFragmentEntry(
+			TestPropsValues.getUserId(), serviceContext.getScopeGroupId(),
+			fragmentCollectionId, StringPool.BLANK,
+			RandomTestUtil.randomString(), StringPool.BLANK, "<div>TEST</div>",
+			StringPool.BLANK, StringPool.BLANK, 0,
+			FragmentConstants.TYPE_COMPONENT, WorkflowConstants.STATUS_APPROVED,
+			serviceContext);
+	}
+
+	private void _addLayoutPageTemplateStructure(String layoutData)
+		throws Exception {
+
+		_layoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				addLayoutPageTemplateStructure(
+					TestPropsValues.getUserId(), _group.getGroupId(),
+					_portal.getClassNameId(Layout.class.getName()),
+					_layout.getPlid(), layoutData,
+					ServiceContextTestUtil.getServiceContext(
+						_group, TestPropsValues.getUserId()));
+	}
+
+	private MockActionRequest _getMockActionRequest() throws PortalException {
+		MockActionRequest mockActionRequest = new MockActionRequest();
+
+		mockActionRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay());
+
+		mockActionRequest.addParameter(
+			"classNameId",
+			String.valueOf(PortalUtil.getClassNameId(Layout.class.getName())));
+		mockActionRequest.addParameter(
+			"classPK", String.valueOf(_layout.getPlid()));
+		mockActionRequest.addParameter(
+			"groupId", String.valueOf(_group.getGroupId()));
+
+		return mockActionRequest;
+	}
+
+	private ThemeDisplay _getThemeDisplay() throws PortalException {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setCompany(_company);
+		themeDisplay.setLayout(_layout);
+		themeDisplay.setLayoutSet(_layout.getLayoutSet());
+		themeDisplay.setPermissionChecker(
+			PermissionThreadLocal.getPermissionChecker());
+		themeDisplay.setPlid(_layout.getPlid());
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		return themeDisplay;
+	}
+
+	private String _read(String fileName) throws Exception {
+		Class<?> clazz = getClass();
+
+		InputStream inputStream = clazz.getResourceAsStream(
+			"dependencies/" + fileName);
+
+		return StringUtil.read(inputStream);
+	}
+
+	private Company _company;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	private FragmentCollection _fragmentCollection;
+	private FragmentEntry _fragmentEntry;
+
+	@Inject
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	private Layout _layout;
+
+	@DeleteAfterTestRun
+	private LayoutPageTemplateStructure _layoutPageTemplateStructure;
+
+	@Inject
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
+	@Inject(filter = "mvc.command.name=/content_layout/delete_item_react")
+	private MVCActionCommand _mvcActionCommand;
+
+	@Inject
+	private Portal _portal;
+
+	private static class MockActionRequest
+		extends MockLiferayPortletActionRequest {
+
+		@Override
+		public HttpServletRequest getHttpServletRequest() {
+			return new MockHttpServletRequest();
+		}
+
+	}
+
+}
