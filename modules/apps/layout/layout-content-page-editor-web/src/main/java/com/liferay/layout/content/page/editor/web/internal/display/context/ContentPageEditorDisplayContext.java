00906e514cc9b3f9ee71bbc041ce4d64f941053f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-12-27 09:01:58 +0000

    LPS-102194 Don't show empty collections

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -886,14 +886,20 @@ public class ContentPageEditorDisplayContext {
 				continue;
 			}
 
+			List<SoyContext> fragmentEntriesSoyContext =
+				_getFragmentEntriesSoyContexts(fragmentEntries);
+
+			if (ListUtil.isEmpty(fragmentEntriesSoyContext)) {
+				continue;
+			}
+
 			SoyContext soyContext = SoyContextFactoryUtil.createSoyContext();
 
 			soyContext.put(
 				"fragmentCollectionId",
 				fragmentCollectionContributor.getFragmentCollectionKey()
 			).put(
-				"fragmentEntries",
-				_getFragmentEntriesSoyContexts(fragmentEntries)
+				"fragmentEntries", fragmentEntriesSoyContext
 			).put(
 				"name",
 				fragmentCollectionContributor.getName(themeDisplay.getLocale())
@@ -932,12 +938,18 @@ public class ContentPageEditorDisplayContext {
 
 			SoyContext soyContext = SoyContextFactoryUtil.createSoyContext();
 
+			List<SoyContext> fragmentEntriesSoyContext =
+				_getFragmentEntriesSoyContexts(fragmentEntries);
+
+			if (ListUtil.isEmpty(fragmentEntriesSoyContext)) {
+				continue;
+			}
+
 			soyContext.put(
 				"fragmentCollectionId",
 				fragmentCollection.getFragmentCollectionId()
 			).put(
-				"fragmentEntries",
-				_getFragmentEntriesSoyContexts(fragmentEntries)
+				"fragmentEntries", fragmentEntriesSoyContext
 			).put(
 				"name", fragmentCollection.getName()
 			);
