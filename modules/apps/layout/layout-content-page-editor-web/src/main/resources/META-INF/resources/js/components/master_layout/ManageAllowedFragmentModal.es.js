e8643ce26292ad80af07f1fcf01ce174d0b88cfd
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-12-27 10:36:10 +0000

    LPS-102194 Creates new modal

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/master_layout/ManageAllowedFragmentModal.es.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayModal from '@clayui/modal';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const ManageAllowedFragmentModal = ({
+	observer,
+	onClose,
+}) => {
+	return (
+		<ClayModal observer={observer} size="md">
+			<ClayModal.Header>{Liferay.Language.get('allowed-fragments')}</ClayModal.Header>
+
+			<ClayModal.Body>
+				<p>{Liferay.Language.get('specify-which-fragments-a-page-author-is-allowed-to-use-within-the-drop-zone-when-creating-a-page-from-this-master')}</p>
+			</ClayModal.Body>
+
+			<ClayModal.Footer
+				last={
+					<ClayButton.Group spaced>
+						<ClayButton displayType="secondary" onClick={onClose}>
+							{Liferay.Language.get('cancel')}
+						</ClayButton>
+
+						<ClayButton
+							displayType="primary"
+						>
+							{Liferay.Language.get('save')}
+						</ClayButton>
+					</ClayButton.Group>
+				}
+			></ClayModal.Footer>
+		</ClayModal>
+	);
+};
+
+ManageAllowedFragmentModal.propTypes = {
+	observer: PropTypes.object.isRequired,
+	onClose: PropTypes.func.isRequired
+};
+
+export default ManageAllowedFragmentModal;
