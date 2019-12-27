87b87ed784c14b97d5e5d9e5ef41eb2f46d73def
Victor Galan <victor.galan@liferay.com>
2019-12-27 16:30:32 +0100

    LPS-102194 Create Selector to select available fragments using a treeview

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/master_layout/AllowedFragmentSelector.es.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {Treeview} from 'frontend-js-components-web';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import useSelector from '../../store/hooks/useSelector.es';
+
+const toNodes = collections => {
+	return [
+		{
+			children: collections.map(collection => {
+				const children = collection.fragmentEntries.map(
+					fragmentEntry => ({
+						id: fragmentEntry.fragmentEntryKey,
+						name: fragmentEntry.name
+					})
+				);
+
+				return {
+					children,
+					id: collection.fragmentCollectionId,
+					name: collection.name
+				};
+			}),
+			id: 'lfr-all-fragments-id',
+			name: 'All Fragments'
+		}
+	];
+};
+
+const AllowedFragmentSelector = ({onSelectedFragment}) => {
+	const elements = useSelector(state => state.elements);
+
+	const nodes = toNodes(elements);
+
+	return (
+		<Treeview nodes={nodes} onSelectedNodesChange={onSelectedFragment} />
+	);
+};
+
+AllowedFragmentSelector.propTypes = {
+	onSelectedFragment: PropTypes.func.isRequired
+};
+
+export {AllowedFragmentSelector};
+export default AllowedFragmentSelector;
