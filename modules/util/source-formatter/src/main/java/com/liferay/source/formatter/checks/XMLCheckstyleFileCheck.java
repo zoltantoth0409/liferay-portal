3dcf3549b43825580f460928b9b3828623d53c38
Hugo Huijser <hugo.huijser@liferay.com>
2019-12-26 16:34:21 -0800

    LPS-106147 Add logic to format sourcechecks.xml and checkstyle.xml

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/XMLCheckstyleFileCheck.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.source.formatter.checks.comparator.ElementComparator;
+import com.liferay.source.formatter.checks.util.SourceUtil;
+
+import java.io.IOException;
+
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+
+/**
+ * @author Hugo Huijser
+ */
+public class XMLCheckstyleFileCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isLiferaySourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws DocumentException, IOException {
+
+		if (fileName.endsWith("/checkstyle.xml") ||
+			fileName.endsWith("/checkstyle-jsp.xml")) {
+
+			_checkCheckstyleXML(fileName, content);
+		}
+
+		return content;
+	}
+
+	private void _checkCheckstyleXML(String fileName, String content)
+		throws DocumentException, IOException {
+
+		Document document = SourceUtil.readXML(content);
+
+		_checkOrder(fileName, document.getRootElement());
+	}
+
+	private void _checkOrder(String fileName, Element element) {
+		checkElementOrder(
+			fileName, element, "module", null, new ElementComparator());
+
+		String moduleName = element.attributeValue("name");
+
+		checkElementOrder(
+			fileName, element, "message", moduleName,
+			new ElementComparator("key"));
+		checkElementOrder(
+			fileName, element, "property", moduleName, new ElementComparator());
+
+		for (Element moduleElement :
+				(List<Element>)element.elements("module")) {
+
+			_checkOrder(fileName, moduleElement);
+		}
+	}
+
+}
---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/XMLSourcechecksFileCheck.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.source.formatter.checks.comparator.ElementComparator;
+import com.liferay.source.formatter.checks.util.SourceUtil;
+
+import java.io.IOException;
+
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+
+/**
+ * @author Hugo Huijser
+ */
+public class XMLSourcechecksFileCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isLiferaySourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws DocumentException, IOException {
+
+		if (fileName.endsWith("/sourcechecks.xml")) {
+			_checkSourcechecksXML(fileName, content);
+		}
+
+		return content;
+	}
+
+	private void _checkSourcechecksXML(String fileName, String content)
+		throws DocumentException, IOException {
+
+		Document document = SourceUtil.readXML(content);
+
+		Element rootElement = document.getRootElement();
+
+		checkElementOrder(
+			fileName, rootElement, "source-processor", null,
+			new ElementComparator());
+
+		for (Element sourceProcessorElement :
+				(List<Element>)rootElement.elements("source-processor")) {
+
+			checkElementOrder(
+				fileName, sourceProcessorElement, "check",
+				sourceProcessorElement.attributeValue("name"),
+				new ElementComparator());
+
+			for (Element checkElement :
+					(List<Element>)sourceProcessorElement.elements("check")) {
+
+				checkElementOrder(
+					fileName, checkElement, "property",
+					checkElement.attributeValue("name"),
+					new ElementComparator());
+			}
+		}
+	}
+
+}
