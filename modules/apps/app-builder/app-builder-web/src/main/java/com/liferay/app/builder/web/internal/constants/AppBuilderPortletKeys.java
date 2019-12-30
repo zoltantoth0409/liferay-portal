a6c626556bb413d13915ea899f52010a1cb65bb6
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2019-12-30 15:15:27 -0300

    LPS-106424 Add MANAGE  permission to App Builder

--- modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/constants/AppBuilderPortletKeys.java
+++ modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/constants/AppBuilderPortletKeys.java
@@ -19,6 +19,9 @@ package com.liferay.app.builder.web.internal.constants;
  */
 public class AppBuilderPortletKeys {
 
+	public static final String APP_BUILDER_HIDDEN =
+		"com_liferay_app_builder_web_internal_portlet_AppBuilderHiddenPortlet";
+
 	public static final String CUSTOM_OBJECTS =
 		"com_liferay_app_builder_web_internal_portlet_CustomObjectsPortlet";
 
---
+++ modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/portlet/AppBuilderHiddenPortlet.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.app.builder.web.internal.portlet;
+
+import com.liferay.app.builder.constants.AppBuilderPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jeyvison Nascimento
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + AppBuilderPortletKeys.APP_BUILDER_HIDDEN,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user"
+	},
+	service = Portlet.class
+)
+public class AppBuilderHiddenPortlet extends MVCPortlet {
+}
