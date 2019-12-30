7129e7c78d864eff554b112654523b89256946c4
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-12-30 13:21:10 +0100

    LPS-105103 Index segments in user

---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/search/spi/model/index/contributor/UserModelDocumentContributor.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.search.spi.model.index.contributor;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+import com.liferay.segments.model.SegmentsEntryRel;
+import com.liferay.segments.service.SegmentsEntryRelLocalService;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eduardo García
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.portal.kernel.model.User",
+	service = ModelDocumentContributor.class
+)
+public class UserModelDocumentContributor
+	implements ModelDocumentContributor<User> {
+
+	@Override
+	public void contribute(Document document, User user) {
+		try {
+			long[] segmentsEntryIds = getSegmentsEntryIds(user);
+
+			if (ArrayUtil.isNotEmpty(segmentsEntryIds)) {
+				document.addKeyword("segmentsEntryIds", segmentsEntryIds);
+			}
+		}
+		catch (Exception e) {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Unable to index user " + user.getUserId(), e);
+			}
+		}
+	}
+
+	protected long[] getSegmentsEntryIds(User user) throws Exception {
+		List<SegmentsEntryRel> segmentsEntryRels =
+			_segmentsEntryRelLocalService.getSegmentsEntryRels(
+				_portal.getClassNameId(User.class), user.getUserId());
+
+		Stream<SegmentsEntryRel> stream = segmentsEntryRels.stream();
+
+		return stream.mapToLong(
+			SegmentsEntryRel::getSegmentsEntryId
+		).toArray();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UserModelDocumentContributor.class);
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private SegmentsEntryRelLocalService _segmentsEntryRelLocalService;
+
+}
---
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/search/spi/model/query/contributor/UserModelPreFilterContributor.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.search.spi.model.query.contributor;
+
+import com.liferay.portal.kernel.search.BooleanClauseOccur;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.TermsFilter;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eduardo García
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.portal.kernel.model.User",
+	service = ModelPreFilterContributor.class
+)
+public class UserModelPreFilterContributor
+	implements ModelPreFilterContributor {
+
+	@Override
+	public void contribute(
+		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
+		SearchContext searchContext) {
+
+		long[] segmentsEntryIds = (long[])searchContext.getAttribute(
+			"segmentsEntryIds");
+
+		if (ArrayUtil.isNotEmpty(segmentsEntryIds)) {
+			TermsFilter segmentsEntryIdsTermsFilter = new TermsFilter(
+				"segmentsEntryIds");
+
+			segmentsEntryIdsTermsFilter.addValues(
+				ArrayUtil.toStringArray(segmentsEntryIds));
+
+			booleanFilter.add(
+				segmentsEntryIdsTermsFilter, BooleanClauseOccur.MUST);
+		}
+	}
+
+}
