e13b4059c55cbed81fc83fce9b31cebdec3935f9
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-12-30 13:24:17 +0000

    LPS-106413 Implements url layout type controller

---
+++ modules/apps/layout/layout-type-controller/layout-type-controller-url/src/main/java/com/liferay/layout/type/controller/url/internal/controller/URLLayoutTypeController.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.type.controller.url.internal.controller;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutTypeController;
+import com.liferay.portal.kernel.model.impl.BaseLayoutTypeControllerImpl;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.servlet.PipingServletResponse;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, property = "layout.type=" + LayoutConstants.TYPE_URL,
+	service = LayoutTypeController.class
+)
+public class URLLayoutTypeController extends BaseLayoutTypeControllerImpl {
+
+	@Override
+	public String getURL() {
+		return _URL;
+	}
+
+	@Override
+	public String includeEditContent(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, Layout layout)
+		throws Exception {
+
+		httpServletRequest.setAttribute(WebKeys.SEL_LAYOUT, layout);
+
+		return super.includeEditContent(
+			httpServletRequest, httpServletResponse, layout);
+	}
+
+	@Override
+	public boolean isFirstPageable() {
+		return false;
+	}
+
+	@Override
+	public boolean isParentable() {
+		return false;
+	}
+
+	@Override
+	public boolean isSitemapable() {
+		return false;
+	}
+
+	@Override
+	public boolean isURLFriendliable() {
+		return true;
+	}
+
+	@Override
+	public boolean isWorkflowEnabled() {
+		return false;
+	}
+
+	@Override
+	protected ServletResponse createServletResponse(
+		HttpServletResponse httpServletResponse,
+		UnsyncStringWriter unsyncStringWriter) {
+
+		return new PipingServletResponse(
+			httpServletResponse, unsyncStringWriter);
+	}
+
+	@Override
+	protected String getEditPage() {
+		return _EDIT_PAGE;
+	}
+
+	@Override
+	protected String getViewPage() {
+		return StringPool.BLANK;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.layout.type.controller.url)",
+		unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+	private static final String _EDIT_PAGE = "/layout/edit/url.jsp";
+
+	private static final String _URL = "${url}";
+
+}
