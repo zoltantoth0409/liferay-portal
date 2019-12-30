258e5507424efd6ed917cb5383d8b431357e538c
Eduardo Garcia <eduardo.garcia@liferay.com>
2019-12-30 13:20:33 +0100

    LPS-105103 Contribute to model filter to preview members in segments with referred segments

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/criteria/contributor/SegmentsEntrySegmentsCriteriaContributor.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/criteria/contributor/SegmentsEntrySegmentsCriteriaContributor.java
@@ -49,6 +49,19 @@ public class SegmentsEntrySegmentsCriteriaContributor
 	public static final String KEY = "segments";
 
 	@Override
+	public void contribute(
+		Criteria criteria, String filterString,
+		Criteria.Conjunction conjunction) {
+
+		criteria.addCriterion(getKey(), getType(), filterString, conjunction);
+		criteria.addFilter(getType(), filterString, conjunction);
+
+		// Contribute to model filter to support preview
+
+		criteria.addFilter(Criteria.Type.MODEL, filterString, conjunction);
+	}
+
+	@Override
 	public EntityModel getEntityModel() {
 		return _entityModel;
 	}
