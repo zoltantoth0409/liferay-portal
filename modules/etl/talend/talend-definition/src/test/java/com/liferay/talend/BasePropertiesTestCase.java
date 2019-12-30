05131ba881aaea33d1ce83be1fca3f1ae67ea5f6
Igor Beslic <igor.beslic@liferay.com>
2019-12-30 17:29:59 +0100

    COMMERCE-2474 talend - component - definition - add tests - test Connector Links that point to schemaProperties (influences component connectivity at TOS job designer)

---
+++ modules/etl/talend/talend-definition/src/test/java/com/liferay/talend/BasePropertiesTestCase.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talend;
+
+import java.lang.reflect.Constructor;
+
+import org.apache.avro.Schema;
+
+import org.junit.Assert;
+
+import org.talend.components.api.properties.ComponentProperties;
+import org.talend.components.common.SchemaProperties;
+import org.talend.daikon.properties.property.Property;
+
+/**
+ * @author Igor Beslic
+ */
+public abstract class BasePropertiesTestCase {
+
+	protected void assertEquals(
+		ComponentProperties componentProperties,
+		String schemaPropertiesPropertyPath, Schema expected) {
+
+		SchemaProperties schemaProperties =
+			(SchemaProperties)componentProperties.getProperties(
+				schemaPropertiesPropertyPath);
+
+		Assert.assertNotNull(
+			String.format(
+				"Property path %s must identify not null schema properties " +
+					"in %s",
+				schemaPropertiesPropertyPath, componentProperties.getClass()),
+			schemaProperties);
+
+		Property<Schema> schemaProperty = schemaProperties.schema;
+
+		Assert.assertEquals(
+			String.format(
+				"Schema in schema properties with property path %s in %s",
+				schemaPropertiesPropertyPath, componentProperties.getClass()),
+			expected, schemaProperty.getValue());
+	}
+
+	protected ComponentProperties
+			getDefaultInitializedComponentPropertiesInstance(
+				Class propertyClass)
+		throws Exception {
+
+		Constructor<? extends ComponentProperties> constructor =
+			propertyClass.getConstructor(String.class);
+
+		ComponentProperties componentProperties = constructor.newInstance(
+			"Root");
+
+		componentProperties.init();
+
+		return componentProperties;
+	}
+
+}
---
+++ modules/etl/talend/talend-definition/src/test/java/com/liferay/talend/tliferaybatchfile/TLiferayBatchFilePropertiesTest.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.talend.tliferaybatchfile;
+
+import com.liferay.talend.BasePropertiesTestCase;
+import com.liferay.talend.common.schema.constants.BatchSchemaConstants;
+
+import java.util.Objects;
+
+import org.apache.avro.Schema;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.talend.components.api.component.Connector;
+import org.talend.components.api.component.PropertyPathConnector;
+import org.talend.components.api.properties.ComponentProperties;
+import org.talend.components.common.SchemaProperties;
+
+/**
+ * @author Igor Beslic
+ */
+public class TLiferayBatchFilePropertiesTest extends BasePropertiesTestCase {
+
+	@Test
+	public void testComponentConnectors() throws Exception {
+		TLiferayBatchFileDefinition tLiferayBatchFileDefinition =
+			new TLiferayBatchFileDefinition();
+
+		Class<? extends ComponentProperties> propertyClass =
+			tLiferayBatchFileDefinition.getPropertyClass();
+
+		Assert.assertEquals(
+			"Component properties implementation class",
+			TLiferayBatchFileProperties.class, propertyClass);
+
+		ComponentProperties componentProperties =
+			getDefaultInitializedComponentPropertiesInstance(propertyClass);
+
+		for (Connector outputConnector :
+				componentProperties.getPossibleConnectors(true)) {
+
+			PropertyPathConnector propertyPathConnector =
+				(PropertyPathConnector)outputConnector;
+
+			Schema expectedSchema = SchemaProperties.EMPTY_SCHEMA;
+
+			if (Objects.equals(
+					outputConnector.getName(), Connector.MAIN_NAME)) {
+
+				expectedSchema = BatchSchemaConstants.SCHEMA;
+			}
+
+			assertEquals(
+				componentProperties, propertyPathConnector.getPropertyPath(),
+				expectedSchema);
+		}
+
+		for (Connector inputConnector :
+				componentProperties.getPossibleConnectors(false)) {
+
+			PropertyPathConnector propertyPathConnector =
+				(PropertyPathConnector)inputConnector;
+
+			Schema expectedSchema = SchemaProperties.EMPTY_SCHEMA;
+
+			if (Objects.equals(inputConnector.getName(), Connector.MAIN_NAME)) {
+				expectedSchema = BatchSchemaConstants.SCHEMA;
+			}
+
+			assertEquals(
+				componentProperties, propertyPathConnector.getPropertyPath(),
+				expectedSchema);
+		}
+	}
+
+}
