be26dd12e40055c1c9ccba6f18c8e76a5a5b14d0
shuyangzhou <shuyang.zhou@liferay.com>
2020-01-02 14:49:40 -0800

    LPS-101965 For non-LiferayPortletResponse, wrap and cache the output by ourselves to work with surrounding filters.

--- modules/apps/adaptive-media/adaptive-media-blogs-web/src/main/java/com/liferay/adaptive/media/blogs/web/internal/portlet/filter/BlogsPortletFilter.java
+++ modules/apps/adaptive-media/adaptive-media-blogs-web/src/main/java/com/liferay/adaptive/media/blogs/web/internal/portlet/filter/BlogsPortletFilter.java
@@ -17,12 +17,18 @@ package com.liferay.adaptive.media.blogs.web.internal.portlet.filter;
 import com.liferay.adaptive.media.content.transformer.ContentTransformerHandler;
 import com.liferay.adaptive.media.content.transformer.constants.ContentTransformerContentTypes;
 import com.liferay.blogs.constants.BlogsPortletKeys;
+import com.liferay.portal.kernel.io.WriterOutputStream;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.servlet.BufferCacheServletResponse;
 import com.liferay.portal.kernel.servlet.ServletResponseUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.UnsyncPrintWriterPool;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
@@ -31,6 +37,7 @@ import javax.portlet.filter.FilterChain;
 import javax.portlet.filter.FilterConfig;
 import javax.portlet.filter.PortletFilter;
 import javax.portlet.filter.RenderFilter;
+import javax.portlet.filter.RenderResponseWrapper;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -65,6 +72,82 @@ public class BlogsPortletFilter implements RenderFilter {
 			return;
 		}
 
+		if (renderResponse instanceof LiferayPortletResponse) {
+			_processLiferayPortletResponse(
+				renderRequest, renderResponse, filterChain);
+
+			return;
+		}
+
+		UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
+
+		RenderResponseWrapper renderResponseWrapper = new RenderResponseWrapper(
+			renderResponse) {
+
+			@Override
+			public OutputStream getPortletOutputStream() throws IOException {
+				if (_calledGetWriter) {
+					throw new IllegalStateException(
+						"Unable to obtain OutputStream because Writer is " +
+							"already in use");
+				}
+
+				if (_outputStream != null) {
+					return _outputStream;
+				}
+
+				_outputStream = new WriterOutputStream(unsyncStringWriter);
+
+				_calledGetOutputStream = true;
+
+				return _outputStream;
+			}
+
+			@Override
+			public PrintWriter getWriter() throws IOException {
+				if (_calledGetOutputStream) {
+					throw new IllegalStateException(
+						"Unable to obtain Writer because OutputStream is " +
+							"already in use");
+				}
+
+				if (_printWriter != null) {
+					return _printWriter;
+				}
+
+				_printWriter = UnsyncPrintWriterPool.borrow(unsyncStringWriter);
+
+				_calledGetWriter = true;
+
+				return _printWriter;
+			}
+
+			private boolean _calledGetOutputStream;
+			private boolean _calledGetWriter;
+			private OutputStream _outputStream;
+			private PrintWriter _printWriter;
+
+		};
+
+		filterChain.doFilter(renderRequest, renderResponseWrapper);
+
+		PrintWriter printWriter = renderResponse.getWriter();
+
+		printWriter.write(
+			_contentTransformerHandler.transform(
+				ContentTransformerContentTypes.HTML,
+				unsyncStringWriter.toString()));
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) {
+	}
+
+	private void _processLiferayPortletResponse(
+			RenderRequest renderRequest, RenderResponse renderResponse,
+			FilterChain filterChain)
+		throws IOException, PortletException {
+
 		filterChain.doFilter(renderRequest, renderResponse);
 
 		HttpServletResponse httpServletResponse =
@@ -73,16 +156,11 @@ public class BlogsPortletFilter implements RenderFilter {
 		BufferCacheServletResponse bufferCacheServletResponse =
 			(BufferCacheServletResponse)httpServletResponse;
 
-		String content = bufferCacheServletResponse.getString();
-
-		String transformedContent = _contentTransformerHandler.transform(
-			ContentTransformerContentTypes.HTML, content);
-
-		ServletResponseUtil.write(httpServletResponse, transformedContent);
-	}
-
-	@Override
-	public void init(FilterConfig filterConfig) {
+		ServletResponseUtil.write(
+			httpServletResponse,
+			_contentTransformerHandler.transform(
+				ContentTransformerContentTypes.HTML,
+				bufferCacheServletResponse.getString()));
 	}
 
 	@Reference
