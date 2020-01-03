2818fde2195a3c7ec580765b82758f8b65cac2e9
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-01-03 09:53:09 -0800

    LRAC-4766 add destination names constant

---
+++ modules/apps/analytics/analytics-message-sender-api/src/main/java/com/liferay/analytics/message/sender/constants/AnalyticsMessageDestinationNames.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.message.sender.constants;
+
+/**
+ * @author Rachael Koestartyo
+ */
+public class AnalyticsMessageDestinationNames {
+
+	public static final String ADD_ANALYTICS_MESSAGES_PROCESSOR =
+		"liferay/add_analytics_messages_processor";
+
+}
