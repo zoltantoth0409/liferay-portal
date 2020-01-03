0b431b9303dcbe36c33e2b12beb975644e918835
Javier Gamarra <nhpatt@gmail.com>
2020-01-03 19:56:20 +0100

    LPS-102806 empty questions portlet

---
+++ modules/apps/questions/questions-web/src/main/java/com/liferay/questions/web/internal/portlet/QuestionsPortlet.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.questions.web.internal.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.questions.web.internal.constants.QuestionsPortletKeys;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Javier Gamarra
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-questions",
+		"com.liferay.portlet.display-category=category.collaboration",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=0",
+		"javax.portlet.display-name=Questions",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/META-INF/resources/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + QuestionsPortletKeys.QUESTIONS,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user"
+	},
+	service = Portlet.class
+)
+public class QuestionsPortlet extends MVCPortlet {
+}
---
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/App.es.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+
+import {AppContextProvider} from './AppContext.es';
+import NavigationBar from './pages/NavigationBar.es';
+import Keywords from './pages/keywords/Keywords.es';
+import NewQuestion from './pages/questions/NewQuestion.es';
+import Question from './pages/questions/Question.es';
+import Questions from './pages/questions/Questions.es';
+
+export default props => {
+	return (
+		<AppContextProvider {...props}>
+			<Router>
+				<div>
+					<NavigationBar />
+
+					<Switch>
+						<Route
+							exact
+							path="/"
+							render={props => <Questions {...props} />}
+						/>
+						<Route component={Questions} exact path="/questions" />
+						<Route
+							component={NewQuestion}
+							exact
+							path="/questions/new"
+						/>
+						<Route
+							component={Question}
+							path="/questions/:questionId"
+						/>
+						<Route component={Keywords} path="/keywords" />
+					</Switch>
+				</div>
+			</Router>
+		</AppContextProvider>
+	);
+};
