8e8570ce7ec552a9cf9cbbb2201a252ba7846630
Javier Gamarra <nhpatt@gmail.com>
2020-01-08 07:30:35 +0100

    LPS-102806 add questions list

---
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Questions.es.js
@@ -0,0 +1,144 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayLoadingIndicator from '@clayui/loading-indicator';
+import {ClayPaginationWithBasicItems} from '@clayui/pagination';
+import React, {useContext, useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+
+import {AppContext} from '../../AppContext.es';
+import KeywordsList from '../../components/KeywordList.es';
+import QuestionBadge from '../../components/QuestionsBadge.es';
+import UserAvatar from '../../components/UserAvatar.es';
+import {getThreads} from '../../utils/client.es';
+import {dateToInternationalHuman} from '../../utils/utils.es';
+
+export default () => {
+	const context = useContext(AppContext);
+
+	const [loading, setLoading] = useState(true);
+	const [page, setPage] = useState(1);
+	const [pageSize] = useState(5);
+	const [questions, setQuestions] = useState([]);
+
+	useEffect(() => {
+		getThreads({page, pageSize, siteKey: context.siteKey})
+			.then(data => setQuestions(data))
+			.then(() => setLoading(false));
+	}, [page, pageSize, context.siteKey]);
+
+	const hasValidAnswer = question =>
+		question.messageBoardMessages.items.filter(
+			message => message.showAsAnswer
+		).length > 0;
+
+	return (
+		<section>
+			{loading ? (
+				<ClayLoadingIndicator />
+			) : (
+				questions.items &&
+				questions.items.map(question => (
+					<div className={'question-row'} key={question.id}>
+						<div className="autofit-padded autofit-row">
+							<div className="autofit-col autofit-col-expand">
+								<div className="autofit-section">
+									<h2>
+										<Link to={'/questions/' + question.id}>
+											{question.headline}
+										</Link>
+									</h2>
+								</div>
+							</div>
+							<div className="autofit-col">
+								<p>
+									<QuestionBadge
+										symbol="caret-top"
+										value={
+											question.aggregateRating &&
+											question.aggregateRating.ratingCount
+										}
+									/>
+
+									<QuestionBadge
+										symbol="view"
+										value={question.viewCount}
+									/>
+
+									<QuestionBadge
+										className={
+											hasValidAnswer(question)
+												? 'question-accepted-badge'
+												: ''
+										}
+										symbol={
+											hasValidAnswer(question)
+												? 'check-circle-full'
+												: 'message'
+										}
+										value={
+											question.messageBoardMessages.items
+												.length
+										}
+									/>
+								</p>
+							</div>
+						</div>
+						<div className="autofit-padded autofit-row">
+							<div className="autofit-col autofit-col-expand">
+								<p className="text-truncate">
+									{question.articleBody}
+								</p>
+							</div>
+						</div>
+
+						<div className="autofit-padded autofit-row autofit-row-center">
+							<div className="autofit-col autofit-col-expand">
+								<div>
+									<UserAvatar user={question.creator} />
+									<span>
+										<strong>
+											{' ' + question.creator.name}
+										</strong>
+									</span>
+									<span>
+										{' - ' +
+											dateToInternationalHuman(
+												question.dateModified
+											)}
+									</span>
+								</div>
+							</div>
+							<div className="autofit-col">
+								<KeywordsList keywords={question.keywords} />
+							</div>
+						</div>
+					</div>
+				))
+			)}
+
+			{!!questions.totalCount &&
+				questions.totalCount > questions.pageSize && (
+					<ClayPaginationWithBasicItems
+						activePage={page}
+						ellipsisBuffer={2}
+						onPageChange={setPage}
+						totalPages={Math.ceil(
+							questions.totalCount / questions.pageSize
+						)}
+					/>
+				)}
+		</section>
+	);
+};
