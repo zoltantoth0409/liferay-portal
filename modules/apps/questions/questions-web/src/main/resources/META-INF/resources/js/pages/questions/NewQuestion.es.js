7cafea79341156b090687cb62345822940c10619
Javier Gamarra <nhpatt@gmail.com>
2020-01-08 07:30:48 +0100

    LPS-102806 add new question component

---
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/NewQuestion.es.js
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayForm, {ClayInput} from '@clayui/form';
+import React, {useContext, useState} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+
+import {AppContext} from '../../AppContext.es';
+import {createQuestion} from '../../utils/client.es';
+
+export default withRouter(({history}) => {
+	const context = useContext(AppContext);
+	const [articleBody, setArticleBody] = useState('');
+	const [headline, setHeadline] = useState('');
+	const [keywords, setKeywords] = useState('');
+
+	const submit = () =>
+		createQuestion(articleBody, headline, keywords, context.siteKey).then(
+			() => history.push('/')
+		);
+
+	return (
+		<>
+			<h1>{Liferay.Language.get('new-question')}</h1>
+
+			<ClayForm>
+				<ClayForm.Group className="form-group-sm">
+					<label htmlFor="basicInput">
+						{Liferay.Language.get('title')}
+					</label>
+					<ClayInput
+						onChange={event => setHeadline(event.target.value)}
+						placeholder={Liferay.Language.get(
+							'what-is-your-programming-question'
+						)}
+						required
+						type="text"
+						value={headline}
+					/>
+					<ClayForm.FeedbackGroup>
+						<ClayForm.FeedbackItem>
+							{Liferay.Language.get(
+								'be-specific-and-imagine-you-re-asking-a-question-to-another-developer'
+							)}
+							<span className="d-block">
+								{Liferay.Language.get(
+									'what-is-your-programming-question-be-specific'
+								)}
+							</span>
+						</ClayForm.FeedbackItem>
+					</ClayForm.FeedbackGroup>
+				</ClayForm.Group>
+				<ClayForm.Group className="form-group-sm">
+					<label htmlFor="basicInput">
+						{Liferay.Language.get('body')}
+					</label>
+					<textarea
+						className="form-control"
+						onChange={event => setArticleBody(event.target.value)}
+						placeholder={Liferay.Language.get('description')}
+						required
+						value={articleBody}
+					/>
+					<ClayForm.FeedbackGroup>
+						<ClayForm.FeedbackItem>
+							{Liferay.Language.get(
+								'include-all-the-information-someone-would-need-to-answer-your-question'
+							)}
+						</ClayForm.FeedbackItem>
+						<ClayForm.Text>{''}</ClayForm.Text>
+					</ClayForm.FeedbackGroup>
+				</ClayForm.Group>
+				<ClayForm.Group className="form-group-sm">
+					<label htmlFor="basicInput">
+						{Liferay.Language.get('keywords')}
+					</label>
+					<ClayInput
+						onChange={event => setKeywords(event.target.value)}
+						placeholder={Liferay.Language.get('add-your-keywords')}
+						type="text"
+						value={keywords}
+					/>
+					<ClayForm.FeedbackGroup>
+						<ClayForm.FeedbackItem>
+							{Liferay.Language.get(
+								'add-up-to-5-keywords-to-describe-what-your-question-is-about'
+							)}
+						</ClayForm.FeedbackItem>
+					</ClayForm.FeedbackGroup>
+				</ClayForm.Group>
+			</ClayForm>
+
+			<div className="sheet-footer">
+				<div className="btn-group-item">
+					<div className="btn-group-item">
+						<button
+							className="btn btn-primary"
+							disabled={!articleBody || !headline}
+							onClick={submit}
+						>
+							{Liferay.Language.get('post-your-question')}
+						</button>
+					</div>
+					<div className="btn-group-item">
+						<Link to={`/`}>
+							<button className="btn btn-secondary">
+								{Liferay.Language.get('cancel')}
+							</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+});
