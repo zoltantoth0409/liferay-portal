44b95a965bc8ba06a2c8ec56fbb066ada992ccd9
Inácio Nery <inacio.nery@liferay.com>
2020-01-09 16:02:05 -0300

    LPS-106152 When a token is updated, also update the SLA task with completion and assignment information.

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/TokenWorkflowMetricsIndexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/TokenWorkflowMetricsIndexer.java
@@ -18,6 +18,8 @@ import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.DocumentImpl;
@@ -30,9 +32,12 @@ import com.liferay.portal.workflow.kaleo.model.KaleoInstance;
 import com.liferay.portal.workflow.kaleo.model.KaleoTaskAssignmentInstance;
 import com.liferay.portal.workflow.kaleo.model.KaleoTaskInstanceToken;
 
+import java.text.ParseException;
+
 import java.time.Duration;
 
 import java.util.Date;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -139,38 +144,55 @@ public class TokenWorkflowMetricsIndexer extends BaseWorkflowMetricsIndexer {
 	public void updateDocument(Document document) {
 		super.updateDocument(document);
 
-		if (GetterUtil.getBoolean(document.get("completed"))) {
-			BooleanQuery booleanQuery = queries.booleanQuery();
+		BooleanQuery booleanQuery = queries.booleanQuery();
 
-			booleanQuery.addMustQueryClauses(
-				queries.term(
-					"companyId", GetterUtil.getLong(document.get("companyId"))),
-				queries.term(
-					"instanceId",
-					GetterUtil.getLong(document.get("instanceId"))));
+		booleanQuery.addMustQueryClauses(
+			queries.term(
+				"companyId", GetterUtil.getLong(document.get("companyId"))),
+			queries.term(
+				"instanceId", GetterUtil.getLong(document.get("instanceId"))),
+			queries.term(
+				"processId", GetterUtil.getLong(document.get("processId"))),
+			queries.term("taskId", GetterUtil.getLong(document.get("taskId"))),
+			queries.term(
+				"tokenId", GetterUtil.getLong(document.get("tokenId"))));
 
-			_slaInstanceResultWorkflowMetricsIndexer.updateDocuments(
-				documentImpl -> new DocumentImpl() {
-					{
-						addKeyword(
-							"status", WorkflowMetricsSLAStatus.EXPIRED.name());
-						addKeyword(
-							Field.UID, documentImpl.getString(Field.UID));
+		_slaTaskResultWorkflowMetricsIndexer.updateDocuments(
+			documentImpl -> new DocumentImpl() {
+				{
+					if (!Objects.isNull(document.get("assigneeId"))) {
+						addKeyword("assigneeId", document.get("assigneeId"));
 					}
-				},
-				booleanQuery);
 
-			_slaTaskResultWorkflowMetricsIndexer.updateDocuments(
-				documentImpl -> new DocumentImpl() {
-					{
-						addKeyword(
-							"status", WorkflowMetricsSLAStatus.EXPIRED.name());
-						addKeyword(
-							Field.UID, documentImpl.getString(Field.UID));
+					if (!Objects.isNull(document.get("completionDate"))) {
+						try {
+							addDateSortable(
+								"completionDate",
+								document.getDate("completionDate"));
+						}
+						catch (ParseException pe) {
+							if (_log.isDebugEnabled()) {
+								_log.debug(pe, pe);
+							}
+						}
 					}
-				},
-				booleanQuery);
-		}
+
+					if (!Objects.isNull(document.get("completionUserId"))) {
+						addKeyword(
+							"completionUserId",
+							document.get("completionUserId"));
+					}
+
+					if (!Objects.isNull(document.get("instanceCompleted"))) {
+						addKeyword(
+							"instanceCompleted",
+							document.get("instanceCompleted"));
+					}
+
+					addKeyword(Field.UID, documentImpl.getString(Field.UID));
+				}
+			},
+			booleanQuery);
 	}
 
 	@Override
@@ -203,6 +225,8 @@ public class TokenWorkflowMetricsIndexer extends BaseWorkflowMetricsIndexer {
 		actionableDynamicQuery.performActions();
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		TokenWorkflowMetricsIndexer.class);
 
 	@Reference
 	private SLATaskResultWorkflowMetricsIndexer
