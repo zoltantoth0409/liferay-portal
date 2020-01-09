6013aecfea84fdd468296ce00c5bd99eb638ab47
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-01-09 07:21:52 +0100

    LPS-106041 Adding API for described views

---
+++ modules/apps/item-selector/item-selector-api/src/main/java/com/liferay/item/selector/ItemSelectorViewDescriptor.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.item.selector;
+
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+
+/**
+ * @author Alejandro Tardín
+ */
+public interface ItemSelectorViewDescriptor<T> {
+
+	public ItemDescriptor getItemDescriptor(T t);
+
+	public default String[] getOrderByKeys() {
+		return null;
+	}
+
+	public ItemSelectorReturnType getReturnType();
+
+	public SearchContainer getSearchContainer();
+
+	public interface ItemDescriptor {
+
+		public String getIcon();
+
+		public String getImageURL();
+
+		public String getPayload();
+
+		public String getSubtitle();
+
+		public String getTitle();
+
+	}
+
+}
