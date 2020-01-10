af489fa3ceaab81f7df657c2ee2cb6da223df4d4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-01-10 11:06:51 +0100

    LPS-106053 ant format-javadoc

--- portal-impl/src/com/liferay/portal/model/impl/CompanyInfoImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/CompanyInfoImpl.java
@@ -15,10 +15,14 @@
 package com.liferay.portal.model.impl;
 
 /**
- * The extended model implementation for the CompanyInfo service. Represents a row in the &quot;CompanyInfo&quot; database table, with each column mapped to a property of this class.
+ * The extended model implementation for the CompanyInfo service. Represents a
+ * row in the &quot;CompanyInfo&quot; database table, with each column mapped to
+ * a property of this class.
  *
  * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.portal.kernel.model.CompanyInfo</code> interface.
+ * Helper methods and all application logic should be put in this class.
+ * Whenever methods are added, rerun ServiceBuilder to copy their definitions
+ * into the <code>com.liferay.portal.kernel.model.CompanyInfo</code> interface.
  * </p>
  *
  * @author Brian Wing Shun Chan
@@ -28,7 +32,9 @@ public class CompanyInfoImpl extends CompanyInfoBaseImpl {
 	/**
 	 * NOTE FOR DEVELOPERS:
 	 *
-	 * Never reference this class directly. All methods that expect a company info model instance should use the {@link com.liferay.portal.kernel.model.CompanyInfo} interface instead.
+	 * Never reference this class directly. All methods that expect a company
+	 * info model instance should use the {@link
+	 * com.liferay.portal.kernel.model.CompanyInfo} interface instead.
 	 */
 	public CompanyInfoImpl() {
 	}
--- portal-impl/src/com/liferay/portal/upgrade/v7_0_6/UpgradeResourceActions.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_6/UpgradeResourceActions.java
@@ -24,7 +24,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 /**
- * @author     Michael Bowerman
+ * @author Michael Bowerman
  */
 public class UpgradeResourceActions extends UpgradeProcess {
 
