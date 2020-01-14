e4a83735165554868d27263dbc8157b1a6bb2f85
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-01-14 11:47:24 +0100

    LPS-107058 Defining the ADD_REPOSITORY permission

---
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/resource/DepotPortletResourcePermissionRegistrar.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.depot.internal.resource;
+
+import com.liferay.depot.constants.DepotConstants;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(immediate = true, service = {})
+public class DepotPortletResourcePermissionRegistrar {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("resource.name", DepotConstants.RESOURCE_NAME);
+
+		_serviceRegistration = bundleContext.registerService(
+			PortletResourcePermission.class,
+			PortletResourcePermissionFactory.create(
+				DepotConstants.RESOURCE_NAME),
+			properties);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	private ServiceRegistration<PortletResourcePermission> _serviceRegistration;
+
+}
