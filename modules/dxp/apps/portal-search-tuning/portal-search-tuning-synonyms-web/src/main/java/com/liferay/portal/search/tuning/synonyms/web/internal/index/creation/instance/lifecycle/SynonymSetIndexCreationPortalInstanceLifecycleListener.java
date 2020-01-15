d96841cff6421b0be0d5e397c124455bf4a54cc1
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2020-01-15 20:13:45 -0300

    LPS-100272 portal-search-tuning-synonyms-web: Use a portal instance lifecycle listener to create missing synonym set indices.
    https://issues.liferay.com/browse/LPS-100272

---
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-synonyms-web/src/main/java/com/liferay/portal/search/tuning/synonyms/web/internal/index/creation/instance/lifecycle/SynonymSetIndexCreationPortalInstanceLifecycleListener.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.search.tuning.synonyms.web.internal.index.creation.instance.lifecycle;
+
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyService;
+import com.liferay.portal.search.index.IndexNameBuilder;
+import com.liferay.portal.search.tuning.synonyms.web.internal.index.SynonymSetIndexCreator;
+import com.liferay.portal.search.tuning.synonyms.web.internal.index.SynonymSetIndexReader;
+import com.liferay.portal.search.tuning.synonyms.web.internal.index.name.SynonymSetIndexName;
+import com.liferay.portal.search.tuning.synonyms.web.internal.index.name.SynonymSetIndexNameBuilder;
+import com.liferay.portal.search.tuning.synonyms.web.internal.synchronizer.FilterToIndexSynchronizer;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adam Brandizzi
+ */
+@Component(immediate = true, service = PortalInstanceLifecycleListener.class)
+public class SynonymSetIndexCreationPortalInstanceLifecycleListener
+	extends BasePortalInstanceLifecycleListener {
+
+	@Override
+	public void portalInstanceRegistered(Company company) throws Exception {
+		String indexName = _indexNameBuilder.getIndexName(
+			company.getCompanyId());
+
+		SynonymSetIndexName synonymSetIndexName =
+			_synonymSetIndexNameBuilder.getSynonymSetIndexName(indexName);
+
+		if (!_synonymSetIndexReader.isExists(synonymSetIndexName)) {
+			_synonymSetIndexCreator.create(synonymSetIndexName);
+
+			_filterToIndexSynchronizer.copyToIndex(indexName);
+		}
+	}
+
+	@Reference
+	private CompanyService _companyService;
+
+	@Reference
+	private FilterToIndexSynchronizer _filterToIndexSynchronizer;
+
+	@Reference
+	private IndexNameBuilder _indexNameBuilder;
+
+	@Reference
+	private SynonymSetIndexCreator _synonymSetIndexCreator;
+
+	@Reference
+	private SynonymSetIndexNameBuilder _synonymSetIndexNameBuilder;
+
+	@Reference
+	private SynonymSetIndexReader _synonymSetIndexReader;
+
+}
