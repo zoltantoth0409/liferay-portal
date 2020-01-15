9e85b746b67983347ce5faf6a6ce16b582fc6ae9
Brian Chan <brian.chan@liferay.com>
2020-01-15 14:08:17 -0800

    LPS-107052 SF

--- modules/apps/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/servlet/taglib/AssetTagsSelectorTag.java
+++ modules/apps/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/servlet/taglib/AssetTagsSelectorTag.java
@@ -220,15 +220,13 @@ public class AssetTagsSelectorTag extends IncludeTag {
 				return null;
 			}
 
-			portletURL.setParameter("eventName", getEventName());
-
 			if (_groupIds != null) {
 				portletURL.setParameter(
 					"groupIds", StringUtil.merge(_groupIds, StringPool.COMMA));
 			}
 
+			portletURL.setParameter("eventName", getEventName());
 			portletURL.setParameter("selectedTagNames", "{selectedTagNames}");
-
 			portletURL.setWindowState(LiferayWindowState.POP_UP);
 
 			return portletURL;
