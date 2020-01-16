7d97df0a9a5f33b8c47e073a558c38b763ff3e38
Victor Galan <victor.galan@liferay.com>
2020-01-16 10:56:02 +0100

    LPS-107108 Add MasterLayout, a component which controls the render of the master page

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/MasterLayout.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import classNames from 'classnames';
+import React, {useContext} from 'react';
+
+import {LAYOUT_DATA_ITEM_TYPES} from '../config/constants/layoutDataItemTypes';
+import {StoreContext} from '../store/index';
+import PageEditor from './PageEditor';
+import UnsafeHTML from './UnsafeHTML';
+import {Column, Container, Row} from './layout-data-items/index';
+
+const LAYOUT_DATA_ITEMS = {
+	[LAYOUT_DATA_ITEM_TYPES.column]: Column,
+	[LAYOUT_DATA_ITEM_TYPES.container]: Container,
+	[LAYOUT_DATA_ITEM_TYPES.dropZone]: DropZoneContainer,
+	[LAYOUT_DATA_ITEM_TYPES.fragment]: Fragment,
+	[LAYOUT_DATA_ITEM_TYPES.root]: Root,
+	[LAYOUT_DATA_ITEM_TYPES.row]: Row
+};
+
+export default function MasterPage() {
+	const {
+		fragmentEntryLinks,
+		masterLayoutData,
+		sidebarOpen,
+		sidebarPanelId
+	} = useContext(StoreContext);
+
+	const mainItem = masterLayoutData.items[masterLayoutData.rootItems.main];
+
+	return (
+		<div
+			className={classNames('master-page', 'master-page--with-sidebar', {
+				'master-page--with-sidebar-open': sidebarPanelId && sidebarOpen
+			})}
+		>
+			<MasterLayoutDataItem
+				fragmentEntryLinks={fragmentEntryLinks}
+				item={mainItem}
+				layoutData={masterLayoutData}
+			/>
+		</div>
+	);
+}
+
+function MasterLayoutDataItem({fragmentEntryLinks, item, layoutData}) {
+	const Component = LAYOUT_DATA_ITEMS[item.type];
+
+	if (!Component) {
+		return null;
+	}
+
+	return (
+		<Component
+			fragmentEntryLinks={fragmentEntryLinks}
+			item={item}
+			layoutData={layoutData}
+		>
+			{item.children.map(childId => {
+				return (
+					<MasterLayoutDataItem
+						fragmentEntryLinks={fragmentEntryLinks}
+						item={layoutData.items[childId]}
+						key={childId}
+						layoutData={layoutData}
+					/>
+				);
+			})}
+		</Component>
+	);
+}
+
+function DropZoneContainer() {
+	return <PageEditor withinMasterPage />;
+}
+
+function Root({children}) {
+	return <div>{children}</div>;
+}
+
+function Fragment({fragmentEntryLinks, item}) {
+	const fragmentEntryLink =
+		fragmentEntryLinks[item.config.fragmentEntryLinkId];
+
+	return <UnsafeHTML markup={fragmentEntryLink.content.value.content} />;
+}
