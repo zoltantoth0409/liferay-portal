70fd04a8f375648ce295bb3ed487ab440bbc25e9
Victor Galan <victor.galan@liferay.com>
2020-01-16 09:34:34 +0100

    LPS-107108 Export components

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/index.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/index.js
@@ -12,8 +12,11 @@
  * details.
  */
 
+export {default as Column} from './Column';
 export {default as ColumnWithControls} from './ColumnWithControls';
+export {default as Container} from './Container';
 export {default as ContainerWithControls} from './ContainerWithControls';
 export {default as FragmentWithControls} from './FragmentWithControls';
 export {default as Root} from './Root';
+export {default as Row} from './Row';
 export {default as RowWithControls} from './RowWithControls';
