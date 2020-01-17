24ee1d1b59dd3b714c5ef83d11e526e582ce6159
Adolfo Pérez <adolfo.perez@liferay.com>
2020-01-17 12:12:19 +0100

    LPS-107212 Copy logic of removed base class

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/security/permission/MBThreadPermission.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/security/permission/MBThreadPermission.java
@@ -14,9 +14,14 @@
 
 package com.liferay.message.boards.internal.security.permission;
 
+import com.liferay.message.boards.model.MBMessage;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.permission.BaseModelPermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -26,6 +31,29 @@ import org.osgi.service.component.annotations.Component;
 	property = "model.class.name=com.liferay.message.boards.model.MBThread",
 	service = BaseModelPermissionChecker.class
 )
-public class MBThreadPermission
-	extends MBMessagePermission implements BaseModelPermissionChecker {
+public class MBThreadPermission implements BaseModelPermissionChecker {
+
+	@Override
+	public void checkBaseModel(
+			PermissionChecker permissionChecker, long groupId, long messageId,
+			String actionId)
+		throws PortalException {
+
+		_messageModelResourcePermission.check(
+			permissionChecker, messageId, actionId);
+	}
+
+	@Reference(
+		target = "(model.class.name=com.liferay.message.boards.model.MBMessage)",
+		unbind = "-"
+	)
+	protected void setModelResourcePermission(
+		ModelResourcePermission<MBMessage> modelResourcePermission) {
+
+		_messageModelResourcePermission = modelResourcePermission;
+	}
+
+	private static ModelResourcePermission<MBMessage>
+		_messageModelResourcePermission;
+
 }
