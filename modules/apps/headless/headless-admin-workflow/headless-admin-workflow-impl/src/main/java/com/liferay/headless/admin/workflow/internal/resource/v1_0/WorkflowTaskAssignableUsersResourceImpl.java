855d059812148377e0eb023a691f8e8f7549eca9
Brian Chan <brian.chan@liferay.com>
2020-01-16 19:44:43 -0800

    LPS-102664 Move this out of regen commit

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowTaskAssignableUsersResourceImpl.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowTaskAssignableUsersResourceImpl.java
@@ -79,7 +79,7 @@ public class WorkflowTaskAssignableUsersResourceImpl
 						workflowTaskAssignableUsers.add(
 							_createWorkflowTaskAssignableUser(
 								commonPooledActors,
-								_COMMON_POOLED_ACTORS_TASK_ID));
+								_COMMON_ASSIGNABLE_USERS_TASK_ID));
 
 						return workflowTaskAssignableUsers.toArray(
 							new WorkflowTaskAssignableUser[0]);
@@ -103,7 +103,7 @@ public class WorkflowTaskAssignableUsersResourceImpl
 		return workflowTaskAssignableUser;
 	}
 
-	private static final Long _COMMON_POOLED_ACTORS_TASK_ID = 0L;
+	private static final Long _COMMON_ASSIGNABLE_USERS_TASK_ID = 0L;
 
 	@Reference
 	private Portal _portal;
