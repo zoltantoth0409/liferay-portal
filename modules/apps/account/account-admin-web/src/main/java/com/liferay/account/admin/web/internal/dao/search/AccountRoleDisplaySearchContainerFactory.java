3b9dd17450453eebe73d08efe8ce58cc3f4e2dd5
pei-jung <pei-jung.lan@liferay.com>
2020-01-20 12:35:14 -0800

    LPS-105883 Add search container factory for account roles

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/dao/search/AccountRoleDisplaySearchContainerFactory.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.dao.search;
+
+import com.liferay.account.admin.web.internal.display.AccountRoleDisplay;
+import com.liferay.account.model.AccountRole;
+import com.liferay.account.service.AccountRoleLocalServiceUtil;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.comparator.RoleNameComparator;
+import com.liferay.portal.vulcan.util.TransformUtil;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public class AccountRoleDisplaySearchContainerFactory {
+
+	public static SearchContainer create(
+		long accountEntryId, LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse) {
+
+		SearchContainer searchContainer = new SearchContainer(
+			liferayPortletRequest, liferayPortletResponse.createRenderURL(),
+			null, "there-are-no-roles");
+
+		searchContainer.setId("accountRoles");
+
+		searchContainer.setOrderByCol("name");
+
+		String orderByType = ParamUtil.getString(
+			liferayPortletRequest, "orderByType", "asc");
+
+		searchContainer.setOrderByType(orderByType);
+
+		String keywords = ParamUtil.getString(
+			liferayPortletRequest, "keywords");
+
+		BaseModelSearchResult<AccountRole> baseModelSearchResult =
+			AccountRoleLocalServiceUtil.searchAccountRoles(
+				accountEntryId, keywords, searchContainer.getStart(),
+				searchContainer.getEnd(),
+				new RoleNameComparator(orderByType.equals("asc")));
+
+		searchContainer.setTotal(baseModelSearchResult.getLength());
+		searchContainer.setResults(
+			TransformUtil.transform(
+				baseModelSearchResult.getBaseModels(), AccountRoleDisplay::of));
+
+		return searchContainer;
+	}
+
+}
---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/display/AccountRoleDisplay.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.display;
+
+import com.liferay.account.model.AccountRole;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+
+import java.util.Locale;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public class AccountRoleDisplay {
+
+	public static AccountRoleDisplay of(AccountRole accountRole)
+		throws Exception {
+
+		if (accountRole == null) {
+			return null;
+		}
+
+		return new AccountRoleDisplay(accountRole);
+	}
+
+	public String getDescription(Locale locale) {
+		return _role.getDescription(locale);
+	}
+
+	public String getName(Locale locale) throws Exception {
+		return _role.getTitle(locale);
+	}
+
+	public long getRoleId() {
+		return _role.getRoleId();
+	}
+
+	private AccountRoleDisplay(AccountRole accountRole) throws Exception {
+		_role = RoleLocalServiceUtil.getRole(accountRole.getRoleId());
+	}
+
+	private final Role _role;
+
+}
