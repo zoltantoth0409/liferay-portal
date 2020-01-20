97d6686c1c6baa1882572f1436d23bb268570cd0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-01-20 21:14:52 +0100

    LPS-106619 Add new method to LayoutImpl isTypeContent, like other methods in the class

--- portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/LayoutImpl.java
@@ -1161,6 +1161,18 @@ public class LayoutImpl extends LayoutBaseImpl {
 	}
 
 	@Override
+	public boolean isTypeContent() {
+		if (Objects.equals(getType(), LayoutConstants.TYPE_CONTENT) ||
+			Objects.equals(
+				_getLayoutTypeControllerType(), LayoutConstants.TYPE_CONTENT)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
 	public boolean isTypeControlPanel() {
 		if (Objects.equals(getType(), LayoutConstants.TYPE_CONTROL_PANEL) ||
 			Objects.equals(
