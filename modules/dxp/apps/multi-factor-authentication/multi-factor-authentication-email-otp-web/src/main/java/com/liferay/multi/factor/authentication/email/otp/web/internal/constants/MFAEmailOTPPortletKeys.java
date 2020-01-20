51d5a8c80c12066010efb98753c5dfc026737c95
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-01-20 12:57:19 +0100

    LPS-101720 add PortletKeys

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/constants/MFAEmailOTPPortletKeys.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.email.otp.web.internal.constants;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public class MFAEmailOTPPortletKeys {
+
+	public static final String MFA_EMAIL_OTP_VERIFY_PORTLET =
+		"com_liferay_multi_factor_authentication_portlet_web_internal_" +
+			"portlet_MFAEmailOTPVerifyPortlet";
+
+}
