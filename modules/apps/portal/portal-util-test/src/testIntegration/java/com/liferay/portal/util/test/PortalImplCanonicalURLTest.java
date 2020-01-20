49602c7cd1aa7f7921d8bb4e688a34dd5e90fe92
kyle.miho <kyle.miho@liferay.com>
2020-01-20 13:17:18 -0800

    LPS-106526 Add coverage

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplCanonicalURLTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplCanonicalURLTest.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -158,6 +159,34 @@ public class PortalImplCanonicalURLTest {
 
 	@Test
 	public void testCanonicalURLWithFriendlyURL() throws Exception {
+		String portalDomain = "localhost";
+
+		String completeURL = _generateURL(
+			portalDomain, "8080", StringPool.BLANK, _group.getFriendlyURL(),
+			Portal.FRIENDLY_URL_SEPARATOR + "content-name", false);
+
+		ThemeDisplay themeDisplay = _createThemeDisplay(
+			portalDomain, _group, 8080, false);
+
+		Assert.assertEquals(
+			completeURL,
+			_portal.getCanonicalURL(
+				_http.addParameter(
+					completeURL, "_ga",
+					"2.237928582.786466685.1515402734-1365236376"),
+				themeDisplay, _layout1, false, false));
+	}
+
+	@Test
+	public void testCanonicalURLWithFriendlyURLContainingLayoutID()
+		throws Exception {
+
+		_group.setFriendlyURL(
+			StringPool.SLASH + _layout1.getLayoutId() +
+				RandomTestUtil.randomString());
+
+		_groupLocalService.updateGroup(_group);
+
 		String portalDomain = "localhost";
 
 		String completeURL = _generateURL(
