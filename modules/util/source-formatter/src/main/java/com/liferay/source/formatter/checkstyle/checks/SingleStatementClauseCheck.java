b5be27af404669ca2171a37be18c78bc13dc52fb
Hugo Huijser <hugo.huijser@liferay.com>
2020-01-21 13:34:10 -0800

    LPS-105380 Enforce use of braces around clause of for/if/while statement

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/SingleStatementClauseCheck.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checkstyle.checks;
+
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * @author Hugo Huijser
+ */
+public class SingleStatementClauseCheck extends BaseCheck {
+
+	@Override
+	public int[] getDefaultTokens() {
+		return new int[] {
+			TokenTypes.LITERAL_FOR, TokenTypes.LITERAL_IF,
+			TokenTypes.LITERAL_WHILE
+		};
+	}
+
+	@Override
+	protected void doVisitToken(DetailAST detailAST) {
+		DetailAST lastChildDetailAST = detailAST.getLastChild();
+
+		if (lastChildDetailAST.getType() == TokenTypes.SEMI) {
+			log(detailAST, _MSG_MISSING_BRACES, detailAST.getText());
+		}
+	}
+
+	private static final String _MSG_MISSING_BRACES = "braces.missing";
+
+}
