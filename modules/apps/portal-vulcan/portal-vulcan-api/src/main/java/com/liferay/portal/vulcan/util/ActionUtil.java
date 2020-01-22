ccff02772d37f84f5de7cb1f01de08d76e4af1b5
Brian Chan <brian.chan@liferay.com>
2020-01-22 12:00:19 -0800

    LPS-98387 baseline

--- modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/util/ActionUtil.java
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/util/ActionUtil.java
@@ -55,35 +55,6 @@ public class ActionUtil {
 			groupedModel.getGroupId(), uriInfo);
 	}
 
-	/**
-	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
-	 *             #addAction(String, Class, GroupedModel, String, Object,
-	 *             UriInfo)}
-	 */
-	@Deprecated
-	public static Map<String, String> addAction(
-		String actionName, Class clazz, GroupedModel groupedModel,
-		String methodName, UriInfo uriInfo) {
-
-		return addAction(
-			actionName, clazz, groupedModel, methodName, null, uriInfo);
-	}
-
-	/**
-	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
-	 *             #addAction(String, Class, Long, String, String, Object,
-	 *             Long, UriInfo)}
-	 */
-	@Deprecated
-	public static Map<String, String> addAction(
-		String actionName, Class clazz, Long id, String methodName,
-		String permissionName, Long siteId, UriInfo uriInfo) {
-
-		return addAction(
-			actionName, clazz, id, methodName, permissionName, null, siteId,
-			uriInfo);
-	}
-
 	public static Map<String, String> addAction(
 		String actionName, Class clazz, Long id, String methodName,
 		String permissionName, Object object, Long siteId, UriInfo uriInfo) {
