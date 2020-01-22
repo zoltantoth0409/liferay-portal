57d6bb81714119f9e244dd3f08a675652e5e4684
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-01-22 14:16:03 +0100

    LPS-108020 Creates a new LayoutStructureItem class for each type of LayoutDataItemTypeConstants

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/layout/structure/FragmentLayoutStructureItem.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.util.layout.structure;
+
+import com.liferay.layout.util.constants.LayoutDataItemTypeConstants;
+
+import java.util.UUID;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class FragmentLayoutStructureItem extends LayoutStructureItem {
+
+	public FragmentLayoutStructureItem(String parentItemId) {
+		super(
+			String.valueOf(UUID.randomUUID()), parentItemId,
+			LayoutDataItemTypeConstants.TYPE_FRAGMENT);
+	}
+
+}
---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/layout/structure/RootLayoutStructureItem.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.util.layout.structure;
+
+import com.liferay.layout.util.constants.LayoutDataItemTypeConstants;
+import com.liferay.petra.string.StringPool;
+
+import java.util.UUID;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class RootLayoutStructureItem extends LayoutStructureItem {
+
+	public RootLayoutStructureItem() {
+		super(
+			String.valueOf(UUID.randomUUID()), StringPool.BLANK,
+			LayoutDataItemTypeConstants.TYPE_ROOT);
+	}
+
+}
