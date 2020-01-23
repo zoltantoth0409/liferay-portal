a88f27998f845d6c31da0e6ebb129282f2f8e836
Javier Gamarra <nhpatt@gmail.com>
2020-01-23 16:08:50 +0100

    LPS-105235 generate

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-client/src/main/java/com/liferay/portal/workflow/metrics/rest/client/permission/Permission.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-client/src/main/java/com/liferay/portal/workflow/metrics/rest/client/permission/Permission.java
@@ -35,19 +35,19 @@ public class Permission {
 	}
 
 	public String[] getActionIds() {
-		return _actionIds;
+		return actionIds;
 	}
 
 	public String getRoleName() {
-		return _roleName;
+		return roleName;
 	}
 
 	public void setActionIds(String[] actionIds) {
-		this._actionIds = actionIds;
+		this.actionIds = actionIds;
 	}
 
 	public void setRoleName(String roleName) {
-		this._roleName = roleName;
+		this.roleName = roleName;
 	}
 
 	@Override
@@ -56,15 +56,15 @@ public class Permission {
 
 		sb.append("{");
 
-		if (_actionIds != null) {
+		if (actionIds != null) {
 			sb.append("\"actionIds\": [");
 
-			for (int i = 0; i < _actionIds.length; i++) {
+			for (int i = 0; i < actionIds.length; i++) {
 				sb.append("\"");
-				sb.append(_actionIds[i]);
+				sb.append(actionIds[i]);
 				sb.append("\"");
 
-				if ((i + 1) < _actionIds.length) {
+				if ((i + 1) < actionIds.length) {
 					sb.append(", ");
 				}
 			}
@@ -72,13 +72,13 @@ public class Permission {
 			sb.append("]");
 		}
 
-		if (_roleName != null) {
+		if (roleName != null) {
 			if (sb.length() > 1) {
 				sb.append(", ");
 			}
 
 			sb.append("\"roleName\": \"");
-			sb.append(_roleName);
+			sb.append(roleName);
 			sb.append("\"");
 		}
 
@@ -87,8 +87,8 @@ public class Permission {
 		return sb.toString();
 	}
 
-	private String[] _actionIds;
-	private String _roleName;
+	protected String[] actionIds;
+	protected String roleName;
 
 	private static class PermissionJSONParser<T>
 		extends BaseJSONParser<Permission> {
