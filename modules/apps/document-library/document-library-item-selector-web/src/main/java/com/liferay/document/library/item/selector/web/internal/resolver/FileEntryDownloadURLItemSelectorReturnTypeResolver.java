dd499c1cefb1c4b22e5e4e4fc50ba4d7e688f36c
Adolfo Pérez <adolfo.perez@liferay.com>
2020-01-24 11:19:32 +0100

    LPS-107295 Don't append version to the other urls

--- modules/apps/document-library/document-library-item-selector-web/src/main/java/com/liferay/document/library/item/selector/web/internal/resolver/FileEntryDownloadURLItemSelectorReturnTypeResolver.java
+++ modules/apps/document-library/document-library-item-selector-web/src/main/java/com/liferay/document/library/item/selector/web/internal/resolver/FileEntryDownloadURLItemSelectorReturnTypeResolver.java
@@ -53,7 +53,7 @@ public class FileEntryDownloadURLItemSelectorReturnTypeResolver
 
 		return _dlURLHelper.getDownloadURL(
 			fileEntry, fileEntry.getFileVersion(), themeDisplay,
-			StringPool.BLANK, true, false);
+			StringPool.BLANK, false, false);
 	}
 
 	@Reference
