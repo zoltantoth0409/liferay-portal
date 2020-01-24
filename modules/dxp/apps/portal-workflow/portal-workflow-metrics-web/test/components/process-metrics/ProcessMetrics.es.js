30f63e3148da491ef08c989fc90efc9d0ce000d6
Gabriel Ramos <gabriel.ramos@liferay.com>
2020-01-24 15:59:55 -0300

    LPS-107622 Update tests of ProcessMetrics and remove snapshot tests

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/test/components/process-metrics/ProcessMetrics.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/test/components/process-metrics/ProcessMetrics.es.js
@@ -9,90 +9,81 @@
  * distribution rights of the Software.
  */
 
+import {cleanup, fireEvent, render} from '@testing-library/react';
 import React from 'react';
 
 import ProcessMetrics from '../../../src/main/resources/META-INF/resources/js/components/process-metrics/ProcessMetrics.es';
 import {MockRouter} from '../../mock/MockRouter.es';
-import fetch from '../../mock/fetch.es';
 import fetchFailure from '../../mock/fetchFailure.es';
 
-beforeAll(() => {
-	const vbody = document.createElement('div');
+import '@testing-library/jest-dom/extend-expect';
 
-	vbody.innerHTML = `
-		<div id="workflow">
-			<div class="user-control-group">
-				<div class="control-menu-icon"></div>
-			</div>
-		</div>
-	`;
-	document.body.appendChild(vbody);
+const client = {
+	get: jest
+		.fn()
+		.mockResolvedValueOnce({data: {totalCount: 1}})
+		.mockResolvedValueOnce({data: {totalCount: 0}})
+};
+
+describe('The ProcessMetrics component should', () => {
+	let getByTestId;
+
+	beforeAll(() => {
+		const component = render(
+			<MockRouter
+				client={client}
+				initialPath="/metrics/35315/dashboard"
+				path="/metrics/:processId"
+			>
+				<ProcessMetrics />
+			</MockRouter>
+		);
+		getByTestId = component.getByTestId;
+	});
+
+	test('Show SLA setup alert and new SLA creation alert when has neither SLA blocked nor SLA created for instances gave', () => {
+		const processMetricsDashBoard = getByTestId('processMetricsDashBoard');
+
+		expect(
+			processMetricsDashBoard.children[1].children[0]
+		).toHaveTextContent(
+			'fix-the-sla-configuration-to-resume-accurate-reporting'
+		);
+
+		expect(
+			processMetricsDashBoard.children[1].children[0]
+		).toHaveTextContent('set-up-slas');
+
+		expect(
+			processMetricsDashBoard.children[2].children[0]
+		).toHaveTextContent('no-slas-are-defined-for-this-process');
+
+		expect(
+			processMetricsDashBoard.children[2].children[0]
+		).toHaveTextContent('add-a-new-sla');
+	});
 });
 
-test('Should render component with completed tab activated', () => {
-	const component = mount(
-		<MockRouter
-			client={fetchFailure()}
-			initialPath="/metrics/35315"
-			path="/metrics/:processId"
-		>
-			<ProcessMetrics />
-		</MockRouter>
-	);
+describe('The ProcessMetrics component should', () => {
+	let getByTestId;
 
-	expect(component).toMatchSnapshot();
-});
+	beforeAll(() => {
+		cleanup();
+		const component = render(
+			<MockRouter
+				client={fetchFailure()}
+				initialPath="/metrics/35315"
+				path="/metrics/:processId"
+			>
+				<ProcessMetrics />
+			</MockRouter>
+		);
+		getByTestId = component.getByTestId;
+	});
 
-test('Should render component with default tab activated', () => {
-	const data = {
-		id: 35315,
-		onTimeInstanceCount: 1,
-		overdueInstanceCount: 0,
-		title: 'Single Approver',
-		totalCount: 1
-	};
-
-	const component = mount(
-		<MockRouter
-			client={fetch({data})}
-			initialPath="/metrics/35315/dashboard"
-			path="/metrics/:processId"
-		>
-			<ProcessMetrics />
-		</MockRouter>
-	);
-
-	expect(component).toMatchSnapshot();
-});
-
-test('Should render component with failure state', () => {
-	const component = mount(
-		<MockRouter
-			client={fetchFailure()}
-			initialPath="/metrics/35315/performance"
-			path="/metrics/:processId"
-		>
-			<ProcessMetrics />
-		</MockRouter>
-	);
-
-	expect(component).toMatchSnapshot();
-});
-
-test('Should render with blocked SLA', () => {
-	const component = mount(
-		<MockRouter
-			client={fetchFailure()}
-			initialPath="/metrics/35315/performance"
-			path="/metrics/:processId"
-		>
-			<ProcessMetrics />
-		</MockRouter>
-	);
-
-	const instance = component.find(ProcessMetrics).instance();
-
-	instance.setState({blockedSLACount: 1}, () => {
-		expect(component).toMatchSnapshot();
+	test('Active the tab by clicking it', () => {
+		const tabElements = getByTestId('tabElements');
+		fireEvent.click(tabElements.children[1].children[0]);
+		expect(tabElements.children[1].children[0]).toHaveClass('active');
 	});
 });
