fc7fd274f0da2a28eb4adcdfb7a74e30350e5c4b
drewbrokke <drew.brokke@liferay.com>
2020-01-29 13:19:20 -0600

    LPS-107709 account-rest-test - unignores test and implements necessary methods

--- modules/apps/account/account-rest-test/src/testIntegration/java/com/liferay/account/rest/resource/v1_0/test/AccountResourceTest.java
+++ modules/apps/account/account-rest-test/src/testIntegration/java/com/liferay/account/rest/resource/v1_0/test/AccountResourceTest.java
@@ -14,15 +14,88 @@
 
 package com.liferay.account.rest.resource.v1_0.test;
 
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.rest.client.dto.v1_0.Account;
+import com.liferay.account.service.AccountEntryLocalService;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
 
-import org.junit.Ignore;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.runner.RunWith;
 
 /**
  * @author Drew Brokke
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class AccountResourceTest extends BaseAccountResourceTestCase {
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"name"};
+	}
+
+	@Override
+	protected Account testGetAccount_addAccount() throws Exception {
+		return _addAccount();
+	}
+
+	@Override
+	protected Account testGraphQLAccount_addAccount() throws Exception {
+		return _addAccount();
+	}
+
+	private Account _addAccount() throws Exception {
+		return _toAccount(_addAccountEntry());
+	}
+
+	private AccountEntry _addAccountEntry() throws PortalException {
+		AccountEntry accountEntry = _accountEntryLocalService.addAccountEntry(
+			TestPropsValues.getUserId(),
+			AccountConstants.ACCOUNT_ENTRY_ID_DEFAULT,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(), null,
+			null, WorkflowConstants.STATUS_APPROVED);
+
+		_accountEntries.add(accountEntry);
+
+		return accountEntry;
+	}
+
+	private String[] _getDomainsArray(AccountEntry accountEntry) {
+		List<String> domains = StringUtil.split(accountEntry.getDomains());
+
+		return domains.toArray(new String[0]);
+	}
+
+	private Account _toAccount(AccountEntry accountEntry) throws Exception {
+		if (accountEntry == null) {
+			return null;
+		}
+
+		return new Account() {
+			{
+				setDescription(accountEntry.getDescription());
+				setDomains(_getDomainsArray(accountEntry));
+				setId(accountEntry.getAccountEntryId());
+				setName(accountEntry.getName());
+				setParentAccountId(accountEntry.getParentAccountEntryId());
+				setStatus(accountEntry.getStatus());
+			}
+		};
+	}
+
+	@DeleteAfterTestRun
+	private final List<AccountEntry> _accountEntries = new ArrayList<>();
+
+	@Inject
+	private AccountEntryLocalService _accountEntryLocalService;
+
 }
