6bc7cbb4662e0845645b55d4b14b840f8a11805e
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-01-29 14:58:01 +0100

    LPS-101720 Add LoginPortletKeys
    we are copying here this class in order to register ActionCommands to Login Portlet. Since LoginPortletKeys in internal.

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/constants/LoginPortletKeys.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.email.otp.web.internal.constants;
+
+/**
+ * @author Tomas Polesovsky
+ */
+public class LoginPortletKeys {
+
+	public static final String FAST_LOGIN =
+		"com_liferay_login_web_portlet_FastLoginPortlet";
+
+	public static final String LOGIN =
+		"com_liferay_login_web_portlet_LoginPortlet";
+
+}
