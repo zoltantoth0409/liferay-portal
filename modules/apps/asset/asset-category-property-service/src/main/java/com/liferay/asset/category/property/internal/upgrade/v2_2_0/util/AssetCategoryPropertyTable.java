00816cda8179cc4ebe971512a45f52022a555e2b
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-01-29 09:28:08 +0100

    LPS-107730 generated buildUpgradeTable

---
+++ modules/apps/asset/asset-category-property-service/src/main/java/com/liferay/asset/category/property/internal/upgrade/v2_2_0/util/AssetCategoryPropertyTable.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.category.property.internal.upgrade.v2_2_0.util;
+
+import java.sql.Types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author	  Brian Wing Shun Chan
+ * @generated
+ */
+public class AssetCategoryPropertyTable {
+
+	public static final String TABLE_NAME = "AssetCategoryProperty";
+
+	public static final Object[][] TABLE_COLUMNS = {
+		{"mvccVersion", Types.BIGINT}, {"categoryPropertyId", Types.BIGINT},
+		{"companyId", Types.BIGINT}, {"userId", Types.BIGINT},
+		{"userName", Types.VARCHAR}, {"createDate", Types.TIMESTAMP},
+		{"modifiedDate", Types.TIMESTAMP}, {"categoryId", Types.BIGINT},
+		{"key_", Types.VARCHAR}, {"value", Types.VARCHAR}
+	};
+
+	public static final Map<String, Integer> TABLE_COLUMNS_MAP =
+new HashMap<String, Integer>();
+
+static {
+TABLE_COLUMNS_MAP.put("mvccVersion", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("categoryPropertyId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("companyId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("userId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("userName", Types.VARCHAR);
+
+TABLE_COLUMNS_MAP.put("createDate", Types.TIMESTAMP);
+
+TABLE_COLUMNS_MAP.put("modifiedDate", Types.TIMESTAMP);
+
+TABLE_COLUMNS_MAP.put("categoryId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("key_", Types.VARCHAR);
+
+TABLE_COLUMNS_MAP.put("value", Types.VARCHAR);
+
+}
+	public static final String TABLE_SQL_CREATE =
+"create table AssetCategoryProperty (mvccVersion LONG default 0 not null,categoryPropertyId LONG not null primary key,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,categoryId LONG,key_ VARCHAR(255) null,value VARCHAR(255) null)";
+
+	public static final String TABLE_SQL_DROP =
+"drop table AssetCategoryProperty";
+
+	public static final String[] TABLE_SQL_ADD_INDEXES = {
+		"create unique index IX_DBD111AA on AssetCategoryProperty (categoryId, key_[$COLUMN_LENGTH:255$])",
+		"create index IX_52340033 on AssetCategoryProperty (companyId, key_[$COLUMN_LENGTH:255$])"
+	};
+
+}
