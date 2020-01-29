3a3dd3339c9ac38c3aa4812a02dc31f6cb5e2c4f
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-01-29 14:53:27 +0100

    LPS-101720 Not needed

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/constants/MFAEmailOTPWebKeys.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/constants/MFAEmailOTPWebKeys.java
@@ -21,9 +21,6 @@ public class MFAEmailOTPWebKeys {
 
 	public static final String MFA_EMAIL_OTP = "MFA_EMAIL_OTP";
 
-	public static final String MFA_EMAIL_OTP_BODY_LOCALIZED_VALUES_MAP =
-		"MFA_EMAIL_OTP_BODY_LOCALIZED_VALUES_MAP";
-
 	public static final String MFA_EMAIL_OTP_CHECKER = "MFA_EMAIL_OTP_CHECKER";
 
 	public static final String MFA_EMAIL_OTP_CONFIGURATION =
@@ -36,9 +33,6 @@ public class MFAEmailOTPWebKeys {
 
 	public static final String MFA_EMAIL_OTP_SET_AT_TIME =
 		"MFA_EMAIL_OTP_SET_AT_TIME";
-
-	public static final String MFA_EMAIL_OTP_SUBJECT_LOCALIZED_VALUES_MAP =
-		"MFA_EMAIL_OTP_SUBJECT_LOCALIZED_VALUES_MAP";
 
 	public static final String MFA_EMAIL_OTP_USER_ID = "MFA_EMAIL_OTP_USER_ID";
 
