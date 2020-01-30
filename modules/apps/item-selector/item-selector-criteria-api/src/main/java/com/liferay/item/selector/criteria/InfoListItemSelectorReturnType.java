0919a9b7350c827e74e897ae0f1b9736bcabf846
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-01-30 16:49:06 +0100

    LPS-108349 Adds new Return type for InfoList

---
+++ modules/apps/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/InfoListItemSelectorReturnType.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.item.selector.criteria;
+
+import com.liferay.item.selector.ItemSelectorReturnType;
+
+/**
+ * This return type should return the following information of a info item as a
+ * JSON object:
+ *
+ * <ul>
+ * <li>
+ * <code>className</code>: The class name of the selected info list
+ * </li>
+ * <li>
+ * <code>classPK</code>: The class pk of the selected info list
+ * </li>
+ * <li>
+ * <code>infoListProviderKey</code>: The key of the selected info list provider
+ * </li>
+ * <li>
+ * <code>title</code>: The title of the selected info list
+ * </li>
+ * </ul>
+ *
+ * @author Eudaldo Alonso
+ * @review
+ */
+public class InfoListItemSelectorReturnType implements ItemSelectorReturnType {
+}
