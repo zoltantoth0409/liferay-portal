600ec0ab734e874bc93c464093b3612087d78f62
Javier Gamarra <nhpatt@gmail.com>
2020-01-30 12:05:29 +0100

    LPS-102806 new edit answer component

---
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/answers/EditAnswer.es.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayForm, {ClayInput} from '@clayui/form';
+import React, {useEffect, useState} from 'react';
+import {withRouter} from 'react-router-dom';
+
+import {getMessage, updateMessage} from '../../utils/client.es';
+
+export default withRouter(
+	({
+		history,
+		match: {
+			params: {answerId}
+		}
+	}) => {
+		const [articleBody, setArticleBody] = useState('');
+
+		useEffect(() => {
+			getMessage(answerId).then(({articleBody}) =>
+				setArticleBody(articleBody)
+			);
+		}, [answerId]);
+
+		const submit = () => {
+			updateMessage(articleBody, answerId).then(() => history.goBack());
+		};
+
+		return (
+			<>
+				<h1>{Liferay.Language.get('edit-answer')}</h1>
+
+				<ClayForm>
+					<ClayForm.Group className="form-group-sm">
+						<label htmlFor="basicInput">
+							{Liferay.Language.get('answer')}
+						</label>
+						<ClayInput
+							onChange={event =>
+								setArticleBody(event.target.value)
+							}
+							placeholder={Liferay.Language.get(
+								'what-is-your-programming-question'
+							)}
+							required
+							type="text"
+							value={articleBody}
+						/>
+						<ClayForm.FeedbackGroup>
+							<ClayForm.FeedbackItem>
+								{Liferay.Language.get(
+									'include-all-the-information-someone-would-need-to-answer-your-question'
+								)}
+							</ClayForm.FeedbackItem>
+						</ClayForm.FeedbackGroup>
+					</ClayForm.Group>
+				</ClayForm>
+
+				<div className="sheet-footer">
+					<div className="btn-group-item">
+						<div className="btn-group-item">
+							<button
+								className="btn btn-primary"
+								disabled={!articleBody}
+								onClick={submit}
+							>
+								{Liferay.Language.get('update-your-answer')}
+							</button>
+						</div>
+						<div className="btn-group-item">
+							<button
+								className="btn btn-secondary"
+								onClick={() => history.goBack()}
+							>
+								{Liferay.Language.get('cancel')}
+							</button>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
+);
