3ea1d915cfd16b3137988dbe77bade063334c430
Diego Nascimento <diego.nascimento@liferay.com>
2020-01-31 16:36:31 -0300

    LPS-106487 Makes Columns resizable

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Column.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Column.js
@@ -12,28 +12,113 @@
  * details.
  */
 
+import ClayButton from '@clayui/button';
 import classNames from 'classnames';
-import React from 'react';
+import React, {useContext, useMemo} from 'react';
 
 import {LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS} from '../../config/constants/layoutDataItemDefaultConfigurations';
 import {LAYOUT_DATA_ITEM_TYPES} from '../../config/constants/layoutDataItemTypes';
+import {useSelector} from '../../store/index';
+import ResizingContext from './ResizingContext';
 
-const Column = React.forwardRef(({children, className, item}, ref) => {
-	const size =
-		item.config.size ||
-		LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS[LAYOUT_DATA_ITEM_TYPES.column]
-			.size;
+/**
+ * Retrieves necessary data from the current and next column.
+ *
+ * @param {!Object} options
+ * @param {!Object} options.item
+ * @param {!Object} options.layoutData
+ *
+ * @returns {!Object}
+ */
+function getColumnInfo({item, layoutData}) {
+	const rowColumns = layoutData.items[item.parentId].children;
+	const colIndex = rowColumns.indexOf(item.itemId);
+	const nextColumnIndex = colIndex + 1;
+	const currentColumn = item;
+	const currentColumnConfig = currentColumn.config;
+	const nextColumn = {...layoutData.items[rowColumns[nextColumnIndex]]};
+	const nextColumnConfig =
+		typeof nextColumn === 'object' && Object.keys(nextColumn).length
+			? nextColumn.config
+			: {};
 
-	return (
-		<div
-			className={classNames(className, 'col', {
-				[`col-${size}`]: size
-			})}
-			ref={ref}
-		>
-			{children}
-		</div>
-	);
-});
+	return {
+		colIndex,
+		currentColumn,
+		currentColumnConfig,
+		isLastColumn: rowColumns.indexOf(item.itemId) === rowColumns.length - 1,
+		nextColumn: nextColumn ? nextColumn : {},
+		nextColumnConfig: nextColumn ? nextColumnConfig : {},
+		nextColumnIndex: colIndex + 1,
+		rowColumns
+	};
+}
+
+const Column = React.forwardRef(
+	({children, className, item, ...props}, ref) => {
+		const {
+			config: {
+				size = LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS[
+					LAYOUT_DATA_ITEM_TYPES.column
+				].size
+			}
+		} = item;
+
+		const layoutData = useSelector(state => state.layoutData);
+
+		const {onResizeEnd, onResizeStart, onResizing} = useContext(
+			ResizingContext
+		);
+
+		const columnInfo = useMemo(() => getColumnInfo({item, layoutData}), [
+			item,
+			layoutData
+		]);
+
+		const onMouseMoveInWindow = event => {
+			event.preventDefault();
+
+			onResizing(event, columnInfo);
+		};
+
+		const onMouseDownInWindow = event => {
+			onResizeStart(event);
+
+			window.addEventListener('mouseup', onMouseUpInWindow);
+			window.addEventListener('mousemove', onMouseMoveInWindow);
+		};
+
+		const onMouseUpInWindow = event => {
+			onResizeEnd(event);
+
+			window.removeEventListener('mouseup', onMouseUpInWindow);
+			window.removeEventListener('mousemove', onMouseMoveInWindow);
+		};
+
+		const resizeHandler = (
+			<div>
+				{children}
+				<ClayButton
+					className="page-editor__col-resizer"
+					onMouseDown={onMouseDownInWindow}
+				/>
+			</div>
+		);
+
+		return (
+			<>
+				<div
+					className={classNames(className, 'col', {
+						[`col-${size}`]: size
+					})}
+					ref={ref}
+					{...props}
+				>
+					{!columnInfo.isLastColumn ? resizeHandler : children}
+				</div>
+			</>
+		);
+	}
+);
 
 export default Column;
