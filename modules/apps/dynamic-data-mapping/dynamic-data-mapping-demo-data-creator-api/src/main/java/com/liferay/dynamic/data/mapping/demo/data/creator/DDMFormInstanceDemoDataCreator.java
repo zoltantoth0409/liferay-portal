b0789a72c3d950f6c4b97a4b992e15587c7f5416
Inácio Nery <inacio.nery@liferay.com>
2020-01-31 11:39:27 -0300

    LPS-106125 Add Dynamic Data Mapping Demo Data Creator API

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-demo-data-creator-api/src/main/java/com/liferay/dynamic/data/mapping/demo/data/creator/DDMFormInstanceDemoDataCreator.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.demo.data.creator;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Date;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Inácio Nery
+ */
+@ProviderType
+public interface DDMFormInstanceDemoDataCreator {
+
+	public DDMFormInstance create(
+			long companyId, Date createDate, long groupId, long userId)
+		throws PortalException;
+
+	public void deleted() throws PortalException;
+
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-demo-data-creator-api/src/main/java/com/liferay/dynamic/data/mapping/demo/data/creator/DDMFormInstanceRecordDemoDataCreator.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.demo.data.creator;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Date;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Inácio Nery
+ */
+@ProviderType
+public interface DDMFormInstanceRecordDemoDataCreator {
+
+	public DDMFormInstanceRecord create(
+			long companyId, Date createDate, DDMFormInstance formInstance,
+			long groupId, long userId)
+		throws PortalException;
+
+	public void deleted() throws PortalException;
+
+}
