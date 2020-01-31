360c5f9c6592a25923a481dbd15acd52ce3e6ed0
Diego Nascimento <diego.nascimento@liferay.com>
2020-01-31 16:33:42 -0300

    LPS-106487 Pass otherProps from Controls to Column component for receiving `layoutData` information

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/ColumnWithControls.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/ColumnWithControls.js
@@ -12,20 +12,6 @@
  * details.
  */
 
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 import React from 'react';
 
 import {LAYOUT_DATA_ITEM_TYPES} from '../../config/constants/layoutDataItemTypes';
@@ -35,11 +21,16 @@ import TopperEmpty from '../TopperEmpty';
 import Column from './Column';
 
 const ColumnWithControls = React.forwardRef(
-	({children, item, layoutData}, ref) => {
+	({children, item, layoutData, ...otherProps}, ref) => {
 		const showLayoutItemTopper = useSelector(selectShowLayoutItemTopper);
 
 		const content = (
-			<Column className="page-editor__col" item={item} ref={ref}>
+			<Column
+				className="page-editor__col"
+				item={item}
+				ref={ref}
+				{...otherProps}
+			>
 				{children}
 			</Column>
 		);
