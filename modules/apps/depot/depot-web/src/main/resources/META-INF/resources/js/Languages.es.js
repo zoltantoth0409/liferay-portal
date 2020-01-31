cccb938147337fc8c7f02f51d1cfd18b1f2edd08
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2020-01-31 10:59:03 +0100

    LPS-103810 No hover if there are no actions available

--- modules/apps/depot/depot-web/src/main/resources/META-INF/resources/js/Languages.es.js
+++ modules/apps/depot/depot-web/src/main/resources/META-INF/resources/js/Languages.es.js
@@ -134,7 +134,7 @@ const Languages = ({
 
 	const LanguagesList = ({defaultLocaleId, locales, showActions = false}) => {
 		return (
-			<ClayTable borderless headVerticalAlignment="middle">
+			<ClayTable borderless headVerticalAlignment="middle" hover={showActions}>
 				<ClayTable.Head>
 					<ClayTable.Row>
 						<ClayTable.Cell expanded headingCell headingTitle>
