7243254fc07585950e87c64d0ed9fdeff00d45cb
Sarai Díaz García <dgarciasarai@gmail.com>
2020-01-31 12:08:04 +0100

    LPS-106825 Add TotalCount component to entry point

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/AnalyticsReportsApp.es.js
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/AnalyticsReportsApp.es.js
@@ -12,9 +12,28 @@
 import React from 'react';
 
 import BasicInformation from './components/BasicInformation.es';
+import TotalCount from './components/TotalCount.es';
+import APIService from './util/APIService.es';
 
-export default function({props}) {
+export default function({context, props}) {
+	const {endpoints, namespace, page} = context;
 	const {authorName, publishDate, title} = props;
+
+	const {getAnalyticsReportsTotalViewsURL} = endpoints;
+
+	const api = APIService({
+		endpoints: {
+			getAnalyticsReportsTotalViewsURL
+		},
+		namespace,
+		page
+	});
+
+	function _handleTotalViews() {
+		return api.getTotalViews().then(response => {
+			return response.analyticsReportsTotalViews;
+		});
+	}
 
 	return (
 		<div className="p-3">
@@ -22,6 +41,11 @@ export default function({props}) {
 				authorName={authorName}
 				publishDate={publishDate}
 				title={title}
+			/>
+
+			<TotalCount
+				dataProvider={_handleTotalViews}
+				label={Liferay.Util.sub(Liferay.Language.get('total-views'))}
 			/>
 		</div>
 	);
