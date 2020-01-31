d6368b5052a19445f95cce99b5f94c5873c45b00
Rubén Pulido <ruben.pulido@liferay.com>
2020-01-31 14:29:28 +0100

    LPS-107685 Create PageDefinitionConverterUtil

---
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/util/PageDefinitionConverterUtil.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.admin.web.internal.util;
+
+import com.liferay.headless.delivery.dto.v1_0.PageDefinition;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rubén Pulido
+ */
+@Component(immediate = true, service = PageDefinitionConverterUtil.class)
+public class PageDefinitionConverterUtil {
+
+	public PageDefinition toPageDefinition(long plid) {
+		Layout layout = _layoutLocalService.fetchLayout(plid);
+
+		return new PageDefinition() {
+			{
+				dateCreated = layout.getCreateDate();
+				dateModified = layout.getModifiedDate();
+			}
+		};
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+}
