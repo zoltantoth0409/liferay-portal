45b398a82c8e717215fc78a605ac16c259623ad4
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-01-31 11:19:27 +0100

    LPS-107877 Adding renderer provider interface

---
+++ modules/apps/item-selector/item-selector-api/src/main/java/com/liferay/item/selector/provider/ItemSelectorViewRendererProvider.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.item.selector.provider;
+
+import com.liferay.item.selector.ItemSelectorCriterion;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.ItemSelectorViewRenderer;
+
+import javax.portlet.PortletURL;
+
+/**
+ * @author Alejandro Tardín
+ */
+public interface ItemSelectorViewRendererProvider {
+
+	public ItemSelectorViewRenderer getItemSelectorViewRenderer(
+		ItemSelectorView<ItemSelectorCriterion> itemSelectorView,
+		ItemSelectorCriterion itemSelectorCriterion, PortletURL portletURL,
+		String itemSelectedEventName, boolean search);
+
+}
