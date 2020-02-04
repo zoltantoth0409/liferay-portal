efba795ca3db0066a1aa2e2467a834b0764d5fa8
Adolfo Pérez <adolfo.perez@liferay.com>
2020-02-04 18:21:07 +0100

    LPS-108094 Add tests

--- modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/service/test/FriendlyURLEntryLocalServiceTest.java
+++ modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/service/test/FriendlyURLEntryLocalServiceTest.java
@@ -21,6 +21,7 @@ import com.liferay.friendly.url.model.FriendlyURLEntry;
 import com.liferay.friendly.url.model.FriendlyURLEntryLocalization;
 import com.liferay.friendly.url.service.FriendlyURLEntryLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.model.User;
@@ -31,8 +32,11 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Collections;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -67,6 +71,39 @@ public class FriendlyURLEntryLocalServiceTest {
 		FriendlyURLEntryLocalServiceUtil.deleteGroupFriendlyURLEntries(
 			_group.getGroupId(),
 			ClassNameLocalServiceUtil.getClassNameId(User.class));
+	}
+
+	@Test
+	public void testAddFriendlyURLEntryKeepsOldLocalizedValues()
+		throws Exception {
+
+		long classNameId = ClassNameLocalServiceUtil.getClassNameId(User.class);
+
+		FriendlyURLEntryLocalServiceUtil.addFriendlyURLEntry(
+			_group.getGroupId(), classNameId, TestPropsValues.getUserId(),
+			Collections.singletonMap(
+				LanguageUtil.getLanguageId(LocaleUtil.ENGLISH), "url-title-en"),
+			_getServiceContext());
+
+		FriendlyURLEntryLocalServiceUtil.addFriendlyURLEntry(
+			_group.getGroupId(), classNameId, TestPropsValues.getUserId(),
+			Collections.singletonMap(
+				LanguageUtil.getLanguageId(LocaleUtil.CHINA), "url-title-zh"),
+			_getServiceContext());
+
+		FriendlyURLEntry finalFriendlyURL =
+			FriendlyURLEntryLocalServiceUtil.addFriendlyURLEntry(
+				_group.getGroupId(), classNameId, TestPropsValues.getUserId(),
+				"url-title-en", _getServiceContext());
+
+		Assert.assertEquals(
+			"url-title-en",
+			finalFriendlyURL.getUrlTitle(
+				LanguageUtil.getLanguageId(LocaleUtil.ENGLISH)));
+		Assert.assertEquals(
+			"url-title-zh",
+			finalFriendlyURL.getUrlTitle(
+				LanguageUtil.getLanguageId(LocaleUtil.CHINA)));
 	}
 
 	@Test
