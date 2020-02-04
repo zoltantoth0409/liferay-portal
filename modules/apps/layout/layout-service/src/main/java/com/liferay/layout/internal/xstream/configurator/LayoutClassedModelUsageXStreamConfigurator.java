734a5c0ac6c00b5761b27103e231e2fd47628e3f
Pavel Savinov <pavel.savinov@liferay.com>
2020-02-04 14:33:38 +0100

    LPS-107012 Add XStream configurator for layout classed model usages

---
+++ modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/xstream/configurator/LayoutClassedModelUsageXStreamConfigurator.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.internal.xstream.configurator;
+
+import com.liferay.exportimport.kernel.xstream.XStreamAlias;
+import com.liferay.exportimport.kernel.xstream.XStreamConverter;
+import com.liferay.exportimport.kernel.xstream.XStreamType;
+import com.liferay.layout.model.impl.LayoutClassedModelUsageImpl;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.xstream.configurator.XStreamConfigurator;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(immediate = true, service = XStreamConfigurator.class)
+public class LayoutClassedModelUsageXStreamConfigurator
+	implements XStreamConfigurator {
+
+	@Override
+	public List<XStreamType> getAllowedXStreamTypes() {
+		return null;
+	}
+
+	@Override
+	public List<XStreamAlias> getXStreamAliases() {
+		return ListUtil.fromArray(_xStreamAliases);
+	}
+
+	@Override
+	public List<XStreamConverter> getXStreamConverters() {
+		return null;
+	}
+
+	@Activate
+	protected void activate() {
+		_xStreamAliases = new XStreamAlias[] {
+			new XStreamAlias(
+				LayoutClassedModelUsageImpl.class, "LayoutClassedModelUsage")
+		};
+	}
+
+	private XStreamAlias[] _xStreamAliases;
+
+}
