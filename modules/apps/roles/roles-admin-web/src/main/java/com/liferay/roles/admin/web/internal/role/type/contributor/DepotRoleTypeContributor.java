bf5d6a7603afae2d2079ef191dacf32a8b3bab3e
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-02-04 16:06:19 +0100

    LPS-108012 New DepotRoleTypeContributor.

---
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/role/type/contributor/DepotRoleTypeContributor.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.roles.admin.web.internal.role.type.contributor;
+
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.util.PropsValues;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	immediate = true, property = "service.ranking:Integer=200",
+	service = RoleTypeContributor.class
+)
+public class DepotRoleTypeContributor implements RoleTypeContributor {
+
+	@Override
+	public String getIcon() {
+		return "globe";
+	}
+
+	@Override
+	public String getName() {
+		return "repository";
+	}
+
+	@Override
+	public String[] getSubtypes() {
+		return PropsValues.ROLES_SITE_SUBTYPES;
+	}
+
+	@Override
+	public String getTabTitle(Locale locale) {
+		return "repository-roles";
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return "repository-role";
+	}
+
+	@Override
+	public int getType() {
+		return RoleConstants.TYPE_DEPOT;
+	}
+
+	@Override
+	public boolean isAllowAssignMembers(Role role) {
+		return false;
+	}
+
+	@Override
+	public boolean isAllowDefinePermissions(Role role) {
+		return true;
+	}
+
+	@Override
+	public boolean isAllowDelete(Role role) {
+		return false;
+	}
+
+}
