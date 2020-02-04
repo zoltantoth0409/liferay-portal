b879ce6f386057115a57f6c4b7fbc43d7cb98bdd
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-02-04 10:37:45 +0100

    LPS-107685 Export DropZone and Root

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/PageDefinitionConverterUtil.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/PageDefinitionConverterUtil.java
@@ -25,9 +25,11 @@ import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocal
 import com.liferay.layout.page.template.util.LayoutDataConverter;
 import com.liferay.layout.util.structure.ColumnLayoutStructureItem;
 import com.liferay.layout.util.structure.ContainerLayoutStructureItem;
+import com.liferay.layout.util.structure.DropZoneLayoutStructureItem;
 import com.liferay.layout.util.structure.FragmentLayoutStructureItem;
 import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.util.structure.LayoutStructureItem;
+import com.liferay.layout.util.structure.RootLayoutStructureItem;
 import com.liferay.layout.util.structure.RowLayoutStructureItem;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -122,10 +124,26 @@ public class PageDefinitionConverterUtil {
 			};
 		}
 
+		if (layoutStructureItem instanceof DropZoneLayoutStructureItem) {
+			return new PageElement() {
+				{
+					type = PageElement.Type.DROP_ZONE;
+				}
+			};
+		}
+
 		if (layoutStructureItem instanceof FragmentLayoutStructureItem) {
 			return new PageElement() {
 				{
 					type = PageElement.Type.FRAGMENT;
+				}
+			};
+		}
+
+		if (layoutStructureItem instanceof RootLayoutStructureItem) {
+			return new PageElement() {
+				{
+					type = PageElement.Type.ROOT;
 				}
 			};
 		}
