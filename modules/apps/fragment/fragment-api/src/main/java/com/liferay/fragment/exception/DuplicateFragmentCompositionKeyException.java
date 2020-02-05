c3bf8d6cc158d8baa473439241d85f1b52a00986
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-02-05 18:35:51 +0100

    LPS-107990 Auto SF

--- modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/exception/DuplicateFragmentCompositionKeyException.java
+++ modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/exception/DuplicateFragmentCompositionKeyException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.fragment.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -27,7 +28,9 @@ public class DuplicateFragmentCompositionKeyException extends PortalException {
 		super(msg);
 	}
 
-	public DuplicateFragmentCompositionKeyException(String msg, Throwable cause) {
+	public DuplicateFragmentCompositionKeyException(
+		String msg, Throwable cause) {
+
 		super(msg, cause);
 	}
 
