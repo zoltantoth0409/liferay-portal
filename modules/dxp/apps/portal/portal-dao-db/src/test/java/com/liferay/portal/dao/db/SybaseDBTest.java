129c8944048e38d99a890a244346ad5aae7013d2
Matthew Tambara <matthew.tambara@liferay.com>
2020-02-05 10:50:00 -0800

    LPS-108159 Add test

--- modules/dxp/apps/portal/portal-dao-db/src/test/java/com/liferay/portal/dao/db/SybaseDBTest.java
+++ modules/dxp/apps/portal/portal-dao-db/src/test/java/com/liferay/portal/dao/db/SybaseDBTest.java
@@ -16,16 +16,56 @@ package com.liferay.portal.dao.db;
 
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.test.BaseDBTestCase;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.PropsTestUtil;
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 
 import java.io.IOException;
 
+import java.lang.reflect.Method;
+
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * @author Miguel Pastor
  */
 public class SybaseDBTest extends BaseDBTestCase {
+
+	@BeforeClass
+	public static void setUpClass() {
+		_props = PropsUtil.getProps();
+
+		PropsTestUtil.setProps(
+			PropsKeys.DATABASE_STRING_INDEX_MAX_LENGTH, "-1");
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		PropsUtil.setProps(_props);
+	}
+
+	@Test
+	public void testApplyMaxStringIndexLengthLimitation() throws Exception {
+		DB db = getDB();
+
+		Method method = ReflectionTestUtil.getMethod(
+			db.getClass(), "applyMaxStringIndexLengthLimitation", String.class);
+
+		Assert.assertEquals(
+			"create index 0 on Test (test);",
+			method.invoke(
+				db, "create index 0 on Test (test[$COLUMN_LENGTH:200$]);"));
+
+		Assert.assertEquals(
+			"",
+			method.invoke(
+				db, "create index 0 on Test (test[$COLUMN_LENGTH:1251$]);"));
+	}
 
 	@Test
 	public void testRewordRenameTable() throws IOException {
@@ -37,5 +77,7 @@ public class SybaseDBTest extends BaseDBTestCase {
 	protected DB getDB() {
 		return new SybaseDB(0, 0);
 	}
+
+	private static Props _props;
 
 }
