feb21383dbbeb1221b1afe59dab19203693d3bdd
pei-jung <pei-jung.lan@liferay.com>
2020-02-04 16:30:00 -0800

    LPS-105883 Implement add account role

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/EditAccountRoleMVCActionCommand.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.portlet.action;
+
+import com.liferay.account.constants.AccountPortletKeys;
+import com.liferay.account.model.AccountRole;
+import com.liferay.account.service.AccountRoleLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AccountPortletKeys.ACCOUNT_ENTRIES_ADMIN,
+		"mvc.command.name=/account_admin/edit_account_role"
+	},
+	service = MVCActionCommand.class
+)
+public class EditAccountRoleMVCActionCommand extends BaseMVCActionCommand {
+
+	protected AccountRole addAccountRole(ActionRequest actionRequest)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long accountEntryId = ParamUtil.getLong(
+			actionRequest, "accountEntryId");
+		String name = ParamUtil.getString(actionRequest, "name");
+		Map<Locale, String> titleMap = LocalizationUtil.getLocalizationMap(
+			actionRequest, "title");
+		Map<Locale, String> descriptionMap =
+			LocalizationUtil.getLocalizationMap(actionRequest, "description");
+
+		return _accountRoleLocalService.addAccountRole(
+			themeDisplay.getUserId(), accountEntryId, name, titleMap,
+			descriptionMap);
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		try {
+			String redirect = ParamUtil.getString(actionRequest, "redirect");
+
+			if (cmd.equals(Constants.ADD)) {
+				AccountRole accountRole = addAccountRole(actionRequest);
+
+				redirect = _http.setParameter(
+					redirect, actionResponse.getNamespace() + "accountRoleId",
+					accountRole.getAccountRoleId());
+			}
+
+			if (Validator.isNotNull(redirect)) {
+				sendRedirect(actionRequest, actionResponse, redirect);
+			}
+		}
+		catch (Exception exception) {
+			String mvcPath = "/account_entries_admin/edit_account_role.jsp";
+
+			if (exception instanceof PrincipalException) {
+				SessionErrors.add(actionRequest, exception.getClass());
+
+				mvcPath = "/account_entries_admin/error.jsp";
+			}
+			else {
+				SessionErrors.add(actionRequest, exception.getClass());
+			}
+
+			actionResponse.setRenderParameter("mvcPath", mvcPath);
+		}
+	}
+
+	@Reference
+	private AccountRoleLocalService _accountRoleLocalService;
+
+	@Reference
+	private Http _http;
+
+}
--- modules/apps/account/account-admin-web/src/main/resources/META-INF/resources/account_entries_admin/edit_account_role.jsp
+++ modules/apps/account/account-admin-web/src/main/resources/META-INF/resources/account_entries_admin/edit_account_role.jsp
@@ -15,3 +15,88 @@
 --%>
 
 <%@ include file="/init.jsp" %>
+
+<%
+long accountEntryId = ParamUtil.getLong(request, "accountEntryId");
+
+String backURL = ParamUtil.getString(request, "backURL");
+
+if (Validator.isNull(backURL)) {
+	PortletURL viewAccountRolesURL = renderResponse.createRenderURL();
+
+	viewAccountRolesURL.setParameter("mvcRenderCommandName", "/account_admin/edit_account_entry");
+	viewAccountRolesURL.setParameter("screenNavigationCategoryKey", AccountScreenNavigationEntryConstants.CATEGORY_KEY_ROLES);
+	viewAccountRolesURL.setParameter("accountEntryId", String.valueOf(accountEntryId));
+
+	backURL = viewAccountRolesURL.toString();
+}
+
+portletDisplay.setShowBackIcon(true);
+portletDisplay.setURLBack(backURL);
+
+renderResponse.setTitle(LanguageUtil.get(request, "add-new-role"));
+%>
+
+<portlet:actionURL name="/account_admin/edit_account_role" var="editAccountRoleURL" />
+
+<liferay-frontend:edit-form
+	action="<%= editAccountRoleURL %>"
+>
+	<liferay-frontend:edit-form-body>
+		<aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= Constants.ADD %>" />
+		<aui:input name="redirect" type="hidden" value="<%= currentURL %>" />
+		<aui:input name="accountEntryId" type="hidden" value="<%= String.valueOf(accountEntryId) %>" />
+
+		<aui:model-context model="<%= Role.class %>" />
+
+		<aui:input helpMessage="title-field-help" name="title" />
+		<aui:input name="description" />
+
+		<%
+		String nameLabel = LanguageUtil.get(request, "role-key");
+		%>
+
+		<liferay-ui:error exception="<%= RoleNameException.class %>">
+			<p>
+				<liferay-ui:message arguments="<%= new String[] {nameLabel, RoleConstants.getNameGeneralRestrictions(locale, PropsValues.ROLES_NAME_ALLOW_NUMERIC), RoleConstants.NAME_RESERVED_WORDS} %>" key="the-x-cannot-be-x-or-a-reserved-word-such-as-x" />
+			</p>
+
+			<p>
+				<liferay-ui:message arguments="<%= new String[] {nameLabel, RoleConstants.NAME_INVALID_CHARACTERS} %>" key="the-x-cannot-contain-the-following-invalid-characters-x" />
+			</p>
+		</liferay-ui:error>
+
+		<aui:input helpMessage="key-field-help" label="key" name="name" />
+	</liferay-frontend:edit-form-body>
+
+	<liferay-frontend:edit-form-footer>
+		<aui:button type="submit" />
+
+		<aui:button href="<%= backURL %>" type="cancel" />
+	</liferay-frontend:edit-form-footer>
+</liferay-frontend:edit-form>
+
+<aui:script require="frontend-js-web/liferay/debounce/debounce.es as debounceModule">
+	var form = document.getElementById('<portlet:namespace />fm');
+
+	if (form) {
+		var nameInput = form.querySelector('#<portlet:namespace />name');
+		var titleInput = form.querySelector('#<portlet:namespace />title');
+
+		if (nameInput && titleInput) {
+			var debounce = debounceModule.default;
+
+			var handleOnTitleInput = function(event) {
+				var value = event.target.value;
+
+				if (nameInput.hasAttribute('maxLength')) {
+					value = value.substring(0, nameInput.getAttribute('maxLength'));
+				}
+
+				nameInput.value = value;
+			};
+
+			titleInput.addEventListener('input', debounce(handleOnTitleInput, 200));
+		}
+	}
+</aui:script>
