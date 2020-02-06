5ddc96b273cc3021af619c70c352287954d01426
Rubén Pulido <ruben.pulido@liferay.com>
2020-02-06 14:38:43 +0100

    LPS-107685 Add references in FragmentDefinitionConverterUtil

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/FragmentDefinitionConverterUtil.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/FragmentDefinitionConverterUtil.java
@@ -14,10 +14,16 @@
 
 package com.liferay.layout.page.template.admin.web.internal.headless.delivery.dto.v1_0;
 
+import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.renderer.FragmentRendererTracker;
+import com.liferay.fragment.service.FragmentCollectionLocalService;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.fragment.service.FragmentEntryLocalService;
 import com.liferay.headless.delivery.dto.v1_0.FragmentDefinition;
 import com.liferay.layout.util.structure.FragmentLayoutStructureItem;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Rubén Pulido
@@ -35,5 +41,50 @@ public class FragmentDefinitionConverterUtil {
 			}
 		};
 	}
+
+	@Reference(unbind = "-")
+	protected void setFragmentCollectionContributorTracker(
+		FragmentCollectionContributorTracker
+			fragmentCollectionContributorTracker) {
+
+		_fragmentCollectionContributorTracker =
+			fragmentCollectionContributorTracker;
+	}
+
+	@Reference(unbind = "-")
+	protected void setFragmentCollectionLocalService(
+		FragmentCollectionLocalService fragmentCollectionLocalService) {
+
+		_fragmentCollectionLocalService = fragmentCollectionLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setFragmentEntryLinkLocalService(
+		FragmentEntryLinkLocalService fragmentEntryLinkLocalService) {
+
+		_fragmentEntryLinkLocalService = fragmentEntryLinkLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setFragmentEntryLocalService(
+		FragmentEntryLocalService fragmentEntryLocalService) {
+
+		_fragmentEntryLocalService = fragmentEntryLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setFragmentRendererTracker(
+		FragmentRendererTracker fragmentRendererTracker) {
+
+		_fragmentRendererTracker = fragmentRendererTracker;
+	}
+
+	private static FragmentCollectionContributorTracker
+		_fragmentCollectionContributorTracker;
+	private static FragmentCollectionLocalService
+		_fragmentCollectionLocalService;
+	private static FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+	private static FragmentEntryLocalService _fragmentEntryLocalService;
+	private static FragmentRendererTracker _fragmentRendererTracker;
 
 }
