395462ea290beeb34719c4e971db515ec6a23a9b
Pavel Savinov <pavel.savinov@liferay.com>
2020-02-06 09:21:28 +0100

    LPS-107012 Use base company id upgrade instead of setting company id manually

--- modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/upgrade/LayoutServiceUpgrade.java
+++ modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/upgrade/LayoutServiceUpgrade.java
@@ -19,7 +19,7 @@ import com.liferay.asset.service.AssetEntryUsageLocalService;
 import com.liferay.layout.internal.upgrade.v1_0_0.UpgradeLayout;
 import com.liferay.layout.internal.upgrade.v1_0_0.UpgradeLayoutClassedModelUsage;
 import com.liferay.layout.internal.upgrade.v1_0_0.UpgradeLayoutPermissions;
-import com.liferay.layout.internal.upgrade.v1_1_0.UpgradeSchema;
+import com.liferay.layout.internal.upgrade.v1_1_0.UpgradeCompanyId;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
@@ -41,7 +41,7 @@ public class LayoutServiceUpgrade implements UpgradeStepRegistrator {
 
 		registry.register("1.0.0", "1.0.1", new UpgradeLayout());
 
-		registry.register("1.0.1", "1.1.0", new UpgradeSchema());
+		registry.register("1.0.1", "1.1.0", new UpgradeCompanyId());
 	}
 
 	@Reference
