44794ed2fe67eb2d552f724323120be97c05f7ea
Jeyvison Nascimento <jeyvison.nascimento@liferay.com>
2020-02-06 15:17:28 -0300

    LPS-107710 Create test

--- modules/apps/dynamic-data-lists/dynamic-data-lists-test/src/testIntegration/java/com/liferay/dynamic/data/lists/service/test/DDLRecordServiceTest.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-test/src/testIntegration/java/com/liferay/dynamic/data/lists/service/test/DDLRecordServiceTest.java
@@ -43,7 +43,6 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
@@ -83,7 +82,9 @@ public class DDLRecordServiceTest {
 		new LiferayIntegrationTestRule();
 
 	@BeforeClass
-	public static void setUpClass() {
+	public static void setUpClass() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
@@ -97,12 +98,7 @@ public class DDLRecordServiceTest {
 
 	@Before
 	public void setUp() throws Exception {
-		_availableLocales = DDMFormTestUtil.createAvailableLocales(
-			LocaleUtil.US);
 		_defaultLocale = LocaleUtil.US;
-
-		_group = GroupTestUtil.addGroup();
-
 		_ddmStructureTestHelper = new DDMStructureTestHelper(
 			PortalUtil.getClassNameId(DDLRecordSet.class), _group);
 		_recordSetTestHelper = new DDLRecordSetTestHelper(_group);
@@ -440,6 +436,28 @@ public class DDLRecordServiceTest {
 		assertRecordFieldValue(record, "Phone", "123456");
 	}
 
+	@Test
+	public void testUpdateRecordIncreaseRecordVersion() throws Exception {
+		DDMForm ddmForm = createDDMForm();
+
+		ddmForm.addDDMFormField(createTextDDMFormField("Name", true, false));
+
+		DDLRecordSet recordSet = addRecordSet(ddmForm);
+
+		DDLRecordTestHelper recordTestHelper = new DDLRecordTestHelper(
+			_group, recordSet);
+
+		DDLRecord record = recordTestHelper.addRecord();
+
+		DDLRecord updatedRecord = DDLRecordLocalServiceUtil.updateRecord(
+			TestPropsValues.getUserId(), record.getRecordId(),
+			record.getDDMStorageId(),
+			DDLRecordTestUtil.getServiceContext(
+				WorkflowConstants.ACTION_PUBLISH));
+
+		Assert.assertNotEquals(record.getVersion(), updatedRecord.getVersion());
+	}
+
 	protected DDLRecordSet addRecordSet(DDMForm ddmForm) throws Exception {
 		return addRecordSet(ddmForm, StorageType.JSON.toString());
 	}
@@ -576,15 +594,11 @@ public class DDLRecordServiceTest {
 			serviceContext);
 	}
 
+	private static Group _group;
 	private static ServiceRegistration<StorageAdapter> _serviceRegistration;
 
-	private Set<Locale> _availableLocales;
 	private DDMStructureTestHelper _ddmStructureTestHelper;
 	private Locale _defaultLocale;
-
-	@DeleteAfterTestRun
-	private Group _group;
-
 	private DDLRecordSetTestHelper _recordSetTestHelper;
 
 }
