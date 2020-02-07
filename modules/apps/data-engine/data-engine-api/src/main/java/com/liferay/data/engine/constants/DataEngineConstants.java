04e096a75a12b34a1512df8b7d2c79c88edb9aa5
Leonardo Barros <leonardo.barros@liferay.com>
2020-02-07 09:32:16 -0300

    LPS-108364 deprecate

--- modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/constants/DataEngineConstants.java
+++ modules/apps/data-engine/data-engine-api/src/main/java/com/liferay/data/engine/constants/DataEngineConstants.java
@@ -16,6 +16,7 @@ package com.liferay.data.engine.constants;
 
 /**
  * @author Jeyvison Nascimento
+ * @deprecated As of Athanasius (7.3.x), with no direct replacement
  */
 public class DataEngineConstants {
 
