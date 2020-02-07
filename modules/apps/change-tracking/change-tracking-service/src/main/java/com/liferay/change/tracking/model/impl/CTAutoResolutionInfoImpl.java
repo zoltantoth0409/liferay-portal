b3f2581d283ba83c5f8a642910ee12a997048cf5
Preston-Crary <preston.crary@liferay.com>
2020-02-07 10:24:20 -0800

    LPS-105608 SF.

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/model/impl/CTAutoResolutionInfoImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/model/impl/CTAutoResolutionInfoImpl.java
@@ -15,22 +15,7 @@
 package com.liferay.change.tracking.model.impl;
 
 /**
- * The extended model implementation for the CTAutoResolutionInfo service. Represents a row in the &quot;CTAutoResolutionInfo&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.change.tracking.model.CTAutoResolutionInfo</code> interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 public class CTAutoResolutionInfoImpl extends CTAutoResolutionInfoBaseImpl {
-
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a ct auto resolution info model instance should use the {@link com.liferay.change.tracking.model.CTAutoResolutionInfo} interface instead.
-	 */
-	public CTAutoResolutionInfoImpl() {
-	}
-
 }
