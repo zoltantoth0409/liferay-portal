9fe5f8ccd904b100f12c491586ffd2c2c7b7b1e0
Alicia Garcia <alicia.garcia@liferay.com>
2020-02-07 12:48:28 +0100

    LPS-108260 user same method to know if sharing is available or not

--- modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/servlet/taglib/ui/SharingSitesFormNavigatorEntry.java
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/servlet/taglib/ui/SharingSitesFormNavigatorEntry.java
@@ -14,13 +14,9 @@
 
 package com.liferay.sharing.web.internal.servlet.taglib.ui;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.servlet.taglib.ui.BaseJSPFormNavigatorEntry;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorConstants;
 import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
@@ -91,22 +87,10 @@ public class SharingSitesFormNavigatorEntry
 
 	@Override
 	public boolean isVisible(User user, Group group) {
-		try {
-			if (group.isStagingGroup()) {
-				return false;
-			}
+		SharingConfiguration groupSharingConfiguration =
+			_sharingConfigurationFactory.getGroupSharingConfiguration(group);
 
-			SharingConfiguration companySharingConfiguration =
-				_sharingConfigurationFactory.getCompanySharingConfiguration(
-					_companyLocalService.getCompany(group.getCompanyId()));
-
-			return companySharingConfiguration.isEnabled();
-		}
-		catch (PortalException portalException) {
-			_log.error(portalException, portalException);
-
-			return false;
-		}
+		return groupSharingConfiguration.isAvailable();
 	}
 
 	@Override
@@ -121,12 +105,6 @@ public class SharingSitesFormNavigatorEntry
 	protected String getJspPath() {
 		return "/sites_admin/sharing.jsp";
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		SharingSitesFormNavigatorEntry.class);
-
-	@Reference
-	private CompanyLocalService _companyLocalService;
 
 	@Reference(target = "(bundle.symbolic.name=com.liferay.sharing.web)")
 	private ResourceBundleLoader _resourceBundleLoader;
