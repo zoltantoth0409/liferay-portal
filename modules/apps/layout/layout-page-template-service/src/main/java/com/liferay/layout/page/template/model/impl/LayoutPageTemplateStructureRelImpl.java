bb0702ea45485699ca16e7c5f44cd2ce6475eb3d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-02-09 00:12:01 +0100

    LPS-108539 Convert data to the new format when we get the data

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/model/impl/LayoutPageTemplateStructureRelImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/model/impl/LayoutPageTemplateStructureRelImpl.java
@@ -14,13 +14,42 @@
 
 package com.liferay.layout.page.template.model.impl;
 
+import com.liferay.layout.page.template.util.LayoutDataConverter;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
 /**
- * @author Brian Wing Shun Chan
+ * @author Eudaldo Alonso
  */
 public class LayoutPageTemplateStructureRelImpl
 	extends LayoutPageTemplateStructureRelBaseImpl {
 
-	public LayoutPageTemplateStructureRelImpl() {
+	@Override
+	public String getData() {
+		String data = super.getData();
+
+		try {
+			JSONObject dataJSONObject = JSONFactoryUtil.createJSONObject(data);
+
+			if (LayoutDataConverter.isLatestVersion(dataJSONObject)) {
+				return data;
+			}
+
+			return LayoutDataConverter.convert(data);
+		}
+		catch (JSONException jsonException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to parse JSON object", jsonException);
+			}
+		}
+
+		return data;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutPageTemplateStructureRelImpl.class);
 
 }
