22beb3ed2a6a588da5b4a4d85f8f56e10551cd27
tina.tian <tina.tian@liferay.com>
2020-02-07 21:31:39 -0800

    LPS-108310 portal-search-elasticsearch6-impl: CreateIndexRequestExecutorImpl should also respect IndexSettingsContributor

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/search/engine/adapter/index/CreateIndexRequestExecutorImpl.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/search/engine/adapter/index/CreateIndexRequestExecutorImpl.java
@@ -15,11 +15,13 @@
 package com.liferay.portal.search.elasticsearch6.internal.search.engine.adapter.index;
 
 import com.liferay.portal.search.elasticsearch6.internal.connection.ElasticsearchClientResolver;
+import com.liferay.portal.search.elasticsearch6.internal.settings.IndexSettingsContributorHelper;
 import com.liferay.portal.search.engine.adapter.index.CreateIndexRequest;
 import com.liferay.portal.search.engine.adapter.index.CreateIndexResponse;
 
 import org.elasticsearch.action.admin.indices.create.CreateIndexAction;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentType;
 
 import org.osgi.service.component.annotations.Component;
@@ -55,6 +57,18 @@ public class CreateIndexRequestExecutorImpl
 		createIndexRequestBuilder.setSource(
 			createIndexRequest.getSource(), XContentType.JSON);
 
+		org.elasticsearch.action.admin.indices.create.CreateIndexRequest
+			elasticsearchCreateIndexRequest =
+				createIndexRequestBuilder.request();
+
+		Settings.Builder builder = Settings.builder();
+
+		builder.put(elasticsearchCreateIndexRequest.settings());
+
+		_indexSettingsContributorHelper.loadIndexSettingsContributors(builder);
+
+		elasticsearchCreateIndexRequest.settings(builder);
+
 		return createIndexRequestBuilder;
 	}
 
@@ -66,5 +80,8 @@ public class CreateIndexRequestExecutorImpl
 	}
 
 	private ElasticsearchClientResolver _elasticsearchClientResolver;
+
+	@Reference
+	private IndexSettingsContributorHelper _indexSettingsContributorHelper;
 
 }
