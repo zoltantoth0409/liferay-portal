a341a8c0b3caa6245de77bab49fc9b8c571a8744
Pablo Molina <contact@pablomolina.me>
2020-02-10 09:26:42 +0100

    LPS-108332 Fix resolving comments button

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/comments/components/FragmentComment.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/comments/components/FragmentComment.js
@@ -80,7 +80,7 @@ export default function FragmentComment({
 			body,
 			commentId,
 			config,
-			fragmentEntryLinkId,
+			onNetworkStatus: dispatch,
 			resolved: !resolved
 		})
 			.then(comment => {
