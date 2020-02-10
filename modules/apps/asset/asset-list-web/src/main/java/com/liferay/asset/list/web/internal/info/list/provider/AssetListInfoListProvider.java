5a5b6582eb36228fa5a396f639ebe8c8958f66af
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-02-10 09:08:34 +0100

    LPS-108349 Adds new InfoListProvider for asset entries

---
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/info/list/provider/AssetListInfoListProvider.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.web.internal.info.list.provider;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.info.list.provider.InfoListProvider;
+import com.liferay.info.list.provider.InfoListProviderContext;
+import com.liferay.info.pagination.Pagination;
+import com.liferay.info.sort.Sort;
+import com.liferay.petra.string.StringPool;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(immediate = true, service = InfoListProvider.class)
+public class AssetListInfoListProvider implements InfoListProvider<AssetEntry> {
+
+	@Override
+	public List<AssetEntry> getInfoList(
+		InfoListProviderContext infoListProviderContext) {
+
+		return Collections.emptyList();
+	}
+
+	@Override
+	public List<AssetEntry> getInfoList(
+		InfoListProviderContext infoListProviderContext, Pagination pagination,
+		Sort sort) {
+
+		return Collections.emptyList();
+	}
+
+	@Override
+	public int getInfoListCount(
+		InfoListProviderContext infoListProviderContext) {
+
+		return 0;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return StringPool.BLANK;
+	}
+
+}
