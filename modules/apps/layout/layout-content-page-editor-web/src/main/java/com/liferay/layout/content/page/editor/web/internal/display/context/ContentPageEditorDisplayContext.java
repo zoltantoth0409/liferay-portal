8689e207c31d1ba5d36e324225b1203e023af001
Victor Galan <victor.galan@liferay.com>
2020-02-11 16:27:06 +0100

    LPS-106732 Remove unused

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -315,8 +315,6 @@ public class ContentPageEditorDisplayContext {
 			).put(
 				"languageDirection", _getLanguageDirection()
 			).put(
-				"lookAndFeelURL", _getLookAndFeelURL()
-			).put(
 				"mappingFieldsURL", editorSoyContext.get("mappingFieldsURL")
 			).put(
 				"masterUsed", editorSoyContext.get("masterUsed")
@@ -513,8 +511,6 @@ public class ContentPageEditorDisplayContext {
 				_portletRequest, "layoutConversionWarningMessages")
 		).put(
 			"layoutData", JSONFactoryUtil.createJSONObject(_getLayoutData())
-		).put(
-			"lookAndFeelURL", _getLookAndFeelURL()
 		).put(
 			"mappedInfoItems", _getMappedInfoItemsSoyContexts()
 		).put(
@@ -1359,33 +1355,6 @@ public class ContentPageEditorDisplayContext {
 			getSegmentsExperienceId());
 
 		return _layoutData;
-	}
-
-	private String _getLookAndFeelURL() {
-		PortletURL lookAndFeelURL = PortalUtil.getControlPanelPortletURL(
-			httpServletRequest, LayoutAdminPortletKeys.GROUP_PAGES,
-			PortletRequest.RENDER_PHASE);
-
-		lookAndFeelURL.setParameter(
-			"mvcRenderCommandName", "/layout/edit_layout");
-
-		lookAndFeelURL.setParameter(
-			"redirect",
-			ParamUtil.getString(
-				PortalUtil.getOriginalServletRequest(httpServletRequest),
-				"p_l_back_url"));
-		lookAndFeelURL.setParameter("backURL", themeDisplay.getURLCurrent());
-
-		Layout layout = themeDisplay.getLayout();
-
-		lookAndFeelURL.setParameter(
-			"groupId", String.valueOf(layout.getGroupId()));
-		lookAndFeelURL.setParameter(
-			"selPlid", String.valueOf(layout.getPlid()));
-		lookAndFeelURL.setParameter(
-			"privateLayout", String.valueOf(layout.isPrivateLayout()));
-
-		return lookAndFeelURL.toString();
 	}
 
 	private Set<SoyContext> _getMappedInfoItemsSoyContexts()
