d47a8671d7968988c4e3525a771a8aa1e778d00e
shuyangzhou <shuyang.zhou@liferay.com>
2020-02-11 10:28:16 -0800

    LPS-107255 Missed one usage

--- modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/info/display/contributor/test/FileEntryInfoDisplayContributorTest.java
+++ modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/info/display/contributor/test/FileEntryInfoDisplayContributorTest.java
@@ -36,6 +36,7 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -49,7 +50,6 @@ import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.TextFormatter;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -166,7 +166,7 @@ public class FileEntryInfoDisplayContributorTest {
 					infoDisplayFieldsValues.get("publishDate"));
 
 				Assert.assertEquals(
-					TextFormatter.formatStorageSize(
+					LanguageUtil.formatStorageSize(
 						fileEntry.getSize(), LocaleUtil.getDefault()),
 					infoDisplayFieldsValues.get("size"));
 				Assert.assertEquals(
