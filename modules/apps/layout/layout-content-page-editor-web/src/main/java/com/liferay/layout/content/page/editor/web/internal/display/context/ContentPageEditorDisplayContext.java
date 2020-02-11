61d2760437ec095e476f047596f03274b9cd3965
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-02-11 11:46:43 +0100

    LPS-108355 We can do a put instead a putHTML since we don't need to support soy

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -1228,7 +1228,7 @@ public class ContentPageEditorDisplayContext {
 
 				soyContext.put(
 					"configuration", configurationJSONObject
-				).putHTML(
+				).put(
 					"content", content
 				).put(
 					"defaultConfigurationValues",
