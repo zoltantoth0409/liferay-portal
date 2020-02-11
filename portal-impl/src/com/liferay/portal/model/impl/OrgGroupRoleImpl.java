8fcc5e1a029c6bc3bfb63e296591752778d10ca0
Preston-Crary <preston.crary@liferay.com>
2020-02-11 10:42:08 -0800

    LPS-108515 Deprecate OrgGroupRole since it's unused.

--- portal-impl/src/com/liferay/portal/model/impl/OrgGroupRoleImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/OrgGroupRoleImpl.java
@@ -21,7 +21,9 @@ import java.util.List;
 
 /**
  * @author Brian Wing Shun Chan
+ * @deprecated As of Athanasius (7.3.x), with no direct replacement
  */
+@Deprecated
 public class OrgGroupRoleImpl extends OrgGroupRoleBaseImpl {
 
 	@Override
