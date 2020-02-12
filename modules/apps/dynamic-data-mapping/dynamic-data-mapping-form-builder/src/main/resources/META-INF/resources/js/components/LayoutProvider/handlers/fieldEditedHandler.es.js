3fc6b0336c974d19056c2b30fbca2c5d336404ef
Bruno Basto <bruno.basto@liferay.com>
2020-02-12 14:51:28 -0300

    LPS-98077 Allow editing nested fields

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/handlers/fieldEditedHandler.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/handlers/fieldEditedHandler.es.js
@@ -12,34 +12,116 @@
  * details.
  */
 
-import {FormSupport} from 'dynamic-data-mapping-form-renderer';
+import {PagesVisitor} from 'dynamic-data-mapping-form-renderer/js/util/visitors.es';
 
-import {updateFocusedField} from '../util/focusedField.es';
 import {updateRulesReferences} from '../util/rules.es';
+import {
+	updateFocusedField,
+	updateSettingsContextProperty
+} from '../util/settingsContext.es';
 
-export const updatePages = (pages, oldFieldProperties, newFieldProperties) => {
-	const {fieldName} = oldFieldProperties;
+export const updatePages = (
+	editingLanguageId,
+	pages,
+	previousFieldName,
+	newFieldName,
+	propertyName,
+	propertyValue
+) => {
+	let parentFieldName;
+	const visitor = new PagesVisitor(pages);
 
-	return FormSupport.updateField(pages, fieldName, newFieldProperties);
+	let newPages = visitor.mapFields(
+		(field, fieldIndex, columnIndex, rowIndex, pageIndex, parentField) => {
+			if (field.fieldName === previousFieldName) {
+				if (parentField) {
+					parentFieldName = parentField.fieldName;
+				}
+
+				return {
+					...field,
+					fieldName: newFieldName,
+					name: newFieldName,
+					[propertyName]: propertyValue
+				};
+			}
+
+			return field;
+		},
+		true,
+		true
+	);
+
+	if (parentFieldName && previousFieldName !== newFieldName) {
+		visitor.setPages(newPages);
+
+		newPages = visitor.mapFields(
+			field => {
+				if (parentFieldName === field.fieldName) {
+					const visitor = new PagesVisitor([{rows: field.rows}]);
+
+					const layout = visitor.mapColumns(column => {
+						return {
+							...column,
+							fields: column.fields.map(fieldName => {
+								if (fieldName === previousFieldName) {
+									return newFieldName;
+								}
+
+								return fieldName;
+							})
+						};
+					});
+
+					const {rows} = layout[0];
+
+					return {
+						...field,
+						rows,
+						settingsContext: updateSettingsContextProperty(
+							editingLanguageId,
+							field.settingsContext,
+							'rows',
+							rows
+						)
+					};
+				}
+
+				return field;
+			},
+			true,
+			true
+		);
+	}
+
+	return newPages;
 };
 
-export const updateField = (props, state, fieldName, fieldValue) => {
+export const updateField = (props, state, propertyName, propertyValue) => {
+	const {editingLanguageId} = props;
 	const {focusedField, pages, rules} = state;
-	const updatedFocusedField = updateFocusedField(
+	const {fieldName: previousFieldName} = focusedField;
+	const newFocusedField = updateFocusedField(
 		props,
 		state,
-		fieldName,
-		fieldValue
+		propertyName,
+		propertyValue
+	);
+	const {fieldName: newFieldName} = newFocusedField;
+
+	const newPages = updatePages(
+		editingLanguageId,
+		pages,
+		previousFieldName,
+		newFieldName,
+		propertyName,
+		newFocusedField[propertyName]
 	);
 
 	return {
-		focusedField: updatedFocusedField,
-		pages: updatePages(pages, focusedField, updatedFocusedField),
-		rules: updateRulesReferences(
-			rules || [],
-			focusedField,
-			updatedFocusedField
-		),
+		focusedField: newFocusedField,
+		pages: newPages,
+		rules: updateRulesReferences(rules || [], focusedField, newFocusedField)
 	};
 };
 
