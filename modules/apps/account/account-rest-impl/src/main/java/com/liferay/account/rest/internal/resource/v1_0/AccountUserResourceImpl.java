6c4fe208b9edaa3fd187f16eef42fc1ac46b0416
drewbrokke <drew.brokke@liferay.com>
2020-02-12 16:43:39 -0600

    LPS-108399 account-rest-impl - implements POST method

--- modules/apps/account/account-rest-impl/src/main/java/com/liferay/account/rest/internal/resource/v1_0/AccountUserResourceImpl.java
+++ modules/apps/account/account-rest-impl/src/main/java/com/liferay/account/rest/internal/resource/v1_0/AccountUserResourceImpl.java
@@ -14,9 +14,21 @@
 
 package com.liferay.account.rest.internal.resource.v1_0;
 
+import com.liferay.account.model.AccountEntryUserRel;
+import com.liferay.account.rest.dto.v1_0.AccountUser;
 import com.liferay.account.rest.resource.v1_0.AccountUserResource;
+import com.liferay.account.service.AccountEntryUserRelLocalService;
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.ListType;
+import com.liferay.portal.kernel.model.ListTypeConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ListTypeLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.util.Optional;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +39,91 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = AccountUserResource.class
 )
 public class AccountUserResourceImpl extends BaseAccountUserResourceImpl {
+
+	@Override
+	public AccountUser postAccountUser(Long accountId, AccountUser accountUser)
+		throws Exception {
+
+		AccountEntryUserRel accountEntryUserRel =
+			_accountEntryUserRelLocalService.addAccountEntryUserRel(
+				accountId, contextUser.getUserId(), accountUser.getScreenName(),
+				accountUser.getEmailAddress(),
+				contextAcceptLanguage.getPreferredLocale(),
+				accountUser.getFirstName(), accountUser.getMiddleName(),
+				accountUser.getLastName(), _getPrefixId(accountUser),
+				_getSuffixId(accountUser));
+
+		return _toAccountUser(
+			_userLocalService.getUser(accountEntryUserRel.getAccountUserId()));
+	}
+
+	private long _getListTypeId(String value, String type) {
+		ListType listType = _listTypeLocalService.addListType(value, type);
+
+		return listType.getListTypeId();
+	}
+
+	private long _getPrefixId(AccountUser accountUser) {
+		return Optional.ofNullable(
+			accountUser.getPrefix()
+		).map(
+			prefix -> _getListTypeId(prefix, ListTypeConstants.CONTACT_PREFIX)
+		).orElse(
+			0L
+		);
+	}
+
+	private long _getSuffixId(AccountUser accountUser) {
+		return Optional.ofNullable(
+			accountUser.getSuffix()
+		).map(
+			prefix -> _getListTypeId(prefix, ListTypeConstants.CONTACT_SUFFIX)
+		).orElse(
+			0L
+		);
+	}
+
+	private AccountUser _toAccountUser(User user) throws Exception {
+		return new AccountUser() {
+			{
+				emailAddress = user.getEmailAddress();
+				firstName = user.getFirstName();
+				id = user.getUserId();
+				lastName = user.getLastName();
+				middleName = user.getMiddleName();
+
+				Contact contact = user.getContact();
+
+				long prefixId = contact.getPrefixId();
+
+				if (prefixId > 0) {
+					ListType prefixListType = _listTypeLocalService.getListType(
+						prefixId);
+
+					prefix = prefixListType.getName();
+				}
+
+				screenName = user.getScreenName();
+
+				long suffixId = contact.getSuffixId();
+
+				if (suffixId > 0) {
+					ListType suffixListType = _listTypeLocalService.getListType(
+						suffixId);
+
+					suffix = suffixListType.getName();
+				}
+			}
+		};
+	}
+
+	@Reference
+	private AccountEntryUserRelLocalService _accountEntryUserRelLocalService;
+
+	@Reference
+	private ListTypeLocalService _listTypeLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
