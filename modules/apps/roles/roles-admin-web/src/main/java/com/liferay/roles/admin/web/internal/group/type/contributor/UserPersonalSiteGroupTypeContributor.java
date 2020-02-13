1f66c44fc8b10d4a74e525980841814a6cbaf98c
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-02-13 16:48:45 +0100

    LPS-108574 Implementing existing group types

---
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/group/type/contributor/UserPersonalSiteGroupTypeContributor.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.roles.admin.web.internal.group.type.contributor;
+
+import com.liferay.portal.kernel.model.UserPersonalSite;
+import com.liferay.roles.admin.group.type.contributor.GroupTypeContributor;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(service = GroupTypeContributor.class)
+public class UserPersonalSiteGroupTypeContributor
+	implements GroupTypeContributor {
+
+	@Override
+	public String getClassName() {
+		return UserPersonalSite.class.getName();
+	}
+
+}
