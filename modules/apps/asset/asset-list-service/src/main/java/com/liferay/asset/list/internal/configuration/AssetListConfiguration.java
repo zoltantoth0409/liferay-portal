f82f8e703b7286d68d6b7651d392cae7cde6d0fd
ricardocousolr <ricardo.couso@liferay.com>
2020-02-13 09:56:10 +0100

    LPS-104108 Add a configuration to allow showing assets for all segments a user belongs to for manual and dynamic content sets

---
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/configuration/AssetListConfiguration.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.list.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Ricardo Couso
+ */
+@ExtendedObjectClassDefinition(category = "assets")
+@Meta.OCD(
+	id = "com.liferay.asset.list.internal.configuration.AssetListConfiguration",
+	localization = "content/Language", name = "asset-list-configuration-name"
+)
+public interface AssetListConfiguration {
+
+	/**
+	 * Set this to <code>true</code> to combine and display in an Asset
+	 * Publisher the assets from all personalized views/segments that the
+	 * viewing user belongs to for a dynamic content set.
+	 *
+	 * @return default display style.
+	 */
+	@Meta.AD(
+		deflt = "false",
+		description = "combine-assets-from-all-segments-in-asset-publisher-dynamic-description",
+		name = "combine-assets-from-all-segments-in-asset-publisher-dynamic",
+		required = false
+	)
+	public boolean combineAssetsFromAllSegmentsDynamic();
+
+	/**
+	 * Set this to <code>true</code> to combine and display in an Asset
+	 * Publisher the assets from all personalized views/segments that the
+	 * viewing user belongs to for a manual content set.
+	 *
+	 * @return default display style.
+	 */
+	@Meta.AD(
+		deflt = "false",
+		description = "combine-assets-from-all-segments-in-asset-publisher-manual-description",
+		name = "combine-assets-from-all-segments-in-asset-publisher-manual",
+		required = false
+	)
+	public boolean combineAssetsFromAllSegmentsManual();
+
+}
