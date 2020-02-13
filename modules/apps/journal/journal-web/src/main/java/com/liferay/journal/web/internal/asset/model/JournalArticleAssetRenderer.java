b2979df53dcd6852d25d08de899b5ae0ac849fbf
Attila Bakay <attila.bakay@liferay.com>
2020-02-13 13:27:32 +0100

    LPS-108651 Use themeDisplay instead of request's languageId

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/asset/model/JournalArticleAssetRenderer.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/asset/model/JournalArticleAssetRenderer.java
@@ -573,12 +573,20 @@ public class JournalArticleAssetRenderer
 		String viewMode = ParamUtil.getString(
 			httpServletRequest, "viewMode", Constants.VIEW);
 		String languageId = LanguageUtil.getLanguageId(httpServletRequest);
-		int articlePage = ParamUtil.getInteger(httpServletRequest, "page", 1);
-		PortletRequestModel portletRequestModel = getPortletRequestModel(
-			httpServletRequest, httpServletResponse);
+
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
+
+		if ((themeDisplay != null) &&
+			Validator.isNotNull(themeDisplay.getLanguageId())) {
+
+			languageId = themeDisplay.getLanguageId();
+		}
+
+		int articlePage = ParamUtil.getInteger(httpServletRequest, "page", 1);
+		PortletRequestModel portletRequestModel = getPortletRequestModel(
+			httpServletRequest, httpServletResponse);
 
 		if (!workflowAssetPreview && _article.isApproved()) {
 			return _journalContent.getDisplay(
