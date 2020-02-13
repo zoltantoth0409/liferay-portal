9b189323e3ad5c4d865731a675948bae856fda70
drewbrokke <drew.brokke@liferay.com>
2020-02-13 14:39:25 -0600

    LPS-108399 account-rest-test - implements test helper methods

--- modules/apps/account/account-rest-test/src/testIntegration/java/com/liferay/account/rest/resource/v1_0/test/AccountUserResourceTest.java
+++ modules/apps/account/account-rest-test/src/testIntegration/java/com/liferay/account/rest/resource/v1_0/test/AccountUserResourceTest.java
@@ -14,15 +14,117 @@
 
 package com.liferay.account.rest.resource.v1_0.test;
 
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.rest.client.dto.v1_0.AccountUser;
+import com.liferay.account.service.AccountEntryLocalService;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
 
-import org.junit.Ignore;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.runner.RunWith;
 
 /**
  * @author Drew Brokke
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class AccountUserResourceTest extends BaseAccountUserResourceTestCase {
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"screenName", "emailAddress"};
+	}
+
+	@Override
+	protected AccountUser randomAccountUser() {
+		return new AccountUser() {
+			{
+				emailAddress =
+					StringUtil.toLowerCase(RandomTestUtil.randomString()) +
+						"@liferay.com";
+				firstName = RandomTestUtil.randomString();
+				id = RandomTestUtil.randomLong();
+				lastName = RandomTestUtil.randomString();
+				middleName = RandomTestUtil.randomString();
+				prefix = RandomTestUtil.randomString();
+				screenName = StringUtil.toLowerCase(
+					RandomTestUtil.randomString());
+				suffix = RandomTestUtil.randomString();
+			}
+		};
+	}
+
+	@Override
+	protected AccountUser testGetAccountUsersPage_addAccountUser(
+			Long accountId, AccountUser accountUser)
+		throws Exception {
+
+		return _addAccountUser(accountId, accountUser);
+	}
+
+	@Override
+	protected Long testGetAccountUsersPage_getAccountId() throws Exception {
+		return _getAccountEntryId();
+	}
+
+	@Override
+	protected Long testGetAccountUsersPage_getIrrelevantAccountId()
+		throws Exception {
+
+		return _getAccountEntryId();
+	}
+
+	@Override
+	protected AccountUser testGraphQLAccountUser_addAccountUser()
+		throws Exception {
+
+		return _addAccountUser(_getAccountEntryId(), randomAccountUser());
+	}
+
+	@Override
+	protected AccountUser testPostAccountUser_addAccountUser(
+			AccountUser accountUser)
+		throws Exception {
+
+		return _addAccountUser(_getAccountEntryId(), accountUser);
+	}
+
+	private AccountEntry _addAccountEntry() throws PortalException {
+		AccountEntry accountEntry = _accountEntryLocalService.addAccountEntry(
+			TestPropsValues.getUserId(),
+			AccountConstants.ACCOUNT_ENTRY_ID_DEFAULT,
+			RandomTestUtil.randomString(20), RandomTestUtil.randomString(20),
+			null, null, WorkflowConstants.STATUS_APPROVED);
+
+		_accountEntries.add(accountEntry);
+
+		return accountEntry;
+	}
+
+	private AccountUser _addAccountUser(long accountId, AccountUser accountUser)
+		throws Exception {
+
+		return accountUserResource.postAccountUser(accountId, accountUser);
+	}
+
+	private Long _getAccountEntryId() throws Exception {
+		AccountEntry accountEntry = _addAccountEntry();
+
+		return accountEntry.getAccountEntryId();
+	}
+
+	@DeleteAfterTestRun
+	private final List<AccountEntry> _accountEntries = new ArrayList<>();
+
+	@Inject
+	private AccountEntryLocalService _accountEntryLocalService;
+
 }
