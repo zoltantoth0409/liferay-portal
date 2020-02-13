e99e9981c5a854f45570a8ad9b0c3ecc9c34adfa
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-02-13 12:24:31 +0100

    LPS-108719 Add a depot member role contributor, so all the users that have a userGroup with a Depot group have it.

---
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/security/permissions/contributor/DepotMemberRoleContributor.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.depot.internal.security.permissions.contributor;
+
+import com.liferay.depot.internal.constants.DepotRolesConstants;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.security.permission.UserBag;
+import com.liferay.portal.kernel.security.permission.contributor.RoleCollection;
+import com.liferay.portal.kernel.security.permission.contributor.RoleContributor;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ */
+@Component(service = RoleContributor.class)
+public class DepotMemberRoleContributor implements RoleContributor {
+
+	@Override
+	public void contribute(RoleCollection roleCollection) {
+		if (roleCollection.getGroupId() <= 0) {
+			return;
+		}
+
+		try {
+			Group group = _groupLocalService.getGroup(
+				roleCollection.getGroupId());
+
+			UserBag userBag = roleCollection.getUserBag();
+
+			if (Objects.equals(GroupConstants.TYPE_DEPOT, group.getType()) &&
+				(userBag.hasUserGroup(group) ||
+				 userBag.hasUserOrgGroup(group))) {
+
+				Role role = _roleLocalService.getRole(
+					group.getCompanyId(), DepotRolesConstants.DEPOT_MEMBER);
+
+				roleCollection.addRoleId(role.getRoleId());
+			}
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException, portalException);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DepotMemberRoleContributor.class);
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+}
