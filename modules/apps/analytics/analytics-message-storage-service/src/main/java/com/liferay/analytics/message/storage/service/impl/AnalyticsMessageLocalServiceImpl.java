52e47a01cdd83f60e3ec88b539746be7881af55e
Shinn Lok <shinn.lok@liferay.com>
2020-02-12 16:04:51 -0800

    LRAC-5185 Better fix?

--- modules/apps/analytics/analytics-message-storage-service/src/main/java/com/liferay/analytics/message/storage/service/impl/AnalyticsMessageLocalServiceImpl.java
+++ modules/apps/analytics/analytics-message-storage-service/src/main/java/com/liferay/analytics/message/storage/service/impl/AnalyticsMessageLocalServiceImpl.java
@@ -23,7 +23,6 @@ import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.util.OrderByComparator;
 
 import java.util.Date;
 import java.util.List;
@@ -81,30 +80,7 @@ public class AnalyticsMessageLocalServiceImpl
 		long companyId, int start, int end) {
 
 		return analyticsMessagePersistence.findByCompanyId(
-			companyId, start, end,
-			new OrderByComparator<AnalyticsMessage>() {
-
-				@Override
-				public int compare(
-					AnalyticsMessage analyticsMessage1,
-					AnalyticsMessage analyticsMessage2) {
-
-					return Long.compare(
-						analyticsMessage1.getAnalyticsMessageId(),
-						analyticsMessage2.getAnalyticsMessageId());
-				}
-
-				@Override
-				public String getOrderBy() {
-					return "AnalyticsMessage.analyticsMessageId ASC";
-				}
-
-				@Override
-				public String[] getOrderByFields() {
-					return new String[] {"analyticsMessageId"};
-				}
-
-			});
+			companyId, start, end);
 	}
 
 	@Override
