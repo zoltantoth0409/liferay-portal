7c138f9b04aaba8acb1de946945c71e389c11cb8
arthurchan35 <arthurchan35@gmail.com>
2020-02-14 16:32:38 +0800

    LPS-101720 Add system setting to switch on/off MFA

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/portlet/action/LoginMVCActionCommand.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/portlet/action/LoginMVCActionCommand.java
@@ -19,6 +19,7 @@ import com.liferay.multi.factor.authentication.email.otp.web.internal.checker.MF
 import com.liferay.multi.factor.authentication.email.otp.web.internal.configuration.MFAEmailOTPConfiguration;
 import com.liferay.multi.factor.authentication.email.otp.web.internal.constants.MFAEmailOTPPortletKeys;
 import com.liferay.multi.factor.authentication.email.otp.web.internal.constants.MFAEmailOTPWebKeys;
+import com.liferay.multi.factor.authentication.email.otp.web.internal.system.configuration.MFAEmailOTPSystemConfiguration;
 import com.liferay.petra.encryptor.Encryptor;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProviderUtil;
@@ -79,6 +80,16 @@ public class LoginMVCActionCommand extends BaseMVCActionCommand {
 	protected void doProcessAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
+
+		MFAEmailOTPSystemConfiguration mfaEmailOTPSystemConfiguration =
+			ConfigurationProviderUtil.getSystemConfiguration(
+				MFAEmailOTPSystemConfiguration.class);
+
+		if (!mfaEmailOTPSystemConfiguration.enabled()) {
+			_loginMVCActionCommand.processAction(actionRequest, actionResponse);
+
+			return;
+		}
 
 		MFAEmailOTPConfiguration mfaEmailOTPConfiguration =
 			ConfigurationProviderUtil.getCompanyConfiguration(
---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/email/otp/web/internal/settings/definition/MFAEmailOTPSystemConfigurationBeanDeclaration.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.email.otp.web.internal.settings.definition;
+
+import com.liferay.multi.factor.authentication.email.otp.web.internal.system.configuration.MFAEmailOTPSystemConfiguration;
+import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaration;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Arthur Chan
+ */
+@Component(service = ConfigurationBeanDeclaration.class)
+public class MFAEmailOTPSystemConfigurationBeanDeclaration
+	implements ConfigurationBeanDeclaration {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return MFAEmailOTPSystemConfiguration.class;
+	}
+
+}
