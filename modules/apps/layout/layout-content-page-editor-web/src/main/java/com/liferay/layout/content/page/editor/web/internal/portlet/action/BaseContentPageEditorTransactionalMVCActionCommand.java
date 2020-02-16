d003d9acd02e1dcbc196e5ba0d6ee182b48bbb90
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-02-16 21:03:13 +0100

    LPS-107834 Adds new method to process the exception, since we should return a JSONObject with the error

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/BaseContentPageEditorTransactionalMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/BaseContentPageEditorTransactionalMVCActionCommand.java
@@ -15,18 +15,23 @@
 package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.concurrent.Callable;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletException;
 
 /**
  * @author Eudaldo Alonso
@@ -49,11 +54,20 @@ public abstract class BaseContentPageEditorTransactionalMVCActionCommand
 				_transactionConfig, callable);
 		}
 		catch (Throwable t) {
-			if (t instanceof PortletException) {
-				throw (PortletException)t;
+			if (_log.isDebugEnabled()) {
+				_log.debug(t, t);
 			}
 
-			throw new PortletException(t);
+			Exception exception = null;
+
+			if (t instanceof Exception) {
+				exception = (Exception)t;
+			}
+			else {
+				exception = new Exception(t);
+			}
+
+			jsonObject = processException(actionRequest, exception);
 		}
 
 		hideDefaultSuccessMessage(actionRequest);
@@ -65,6 +79,21 @@ public abstract class BaseContentPageEditorTransactionalMVCActionCommand
 	protected abstract JSONObject doTransactionalCommand(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception;
+
+	protected JSONObject processException(
+		ActionRequest actionRequest, Exception exception) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		return JSONUtil.put(
+			"error",
+			LanguageUtil.get(
+				themeDisplay.getRequest(), "an-unexpected-error-occurred"));
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BaseContentPageEditorTransactionalMVCActionCommand.class);
 
 	private static final TransactionConfig _transactionConfig =
 		TransactionConfig.Factory.create(
