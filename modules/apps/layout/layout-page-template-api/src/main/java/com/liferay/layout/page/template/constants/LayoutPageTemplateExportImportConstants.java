0371f145a8570774fa59cef0acb490c719c67a78
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-02-17 15:31:36 +0100

    LPS-107756 New constants for import/export

---
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/constants/LayoutPageTemplateExportImportConstants.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.constants;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class LayoutPageTemplateExportImportConstants {
+
+	public static final String FILE_NAME_COLLECTION_CONFIG =
+		"page-template-collection.json";
+
+	public static final String FILE_NAME_PAGE_TEMPLATE_CONFIG =
+		"page-template.json";
+
+}
