f66fbf8c06b1263ee2c10a4b89f0d1bc86b899e0
Pavel Savinov <pavel.savinov@liferay.com>
2020-02-17 11:27:12 +0100

    LPS-99844 Adjust context contributor to use layout item selector URL

--- modules/apps/layout/layout-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/layout/dynamic/data/mapping/form/field/type/internal/LayoutDDMFormFieldTemplateContextContributor.java
+++ modules/apps/layout/layout-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/layout/dynamic/data/mapping/form/field/type/internal/LayoutDDMFormFieldTemplateContextContributor.java
@@ -19,27 +19,30 @@ import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
 import com.liferay.item.selector.ItemSelector;
-import com.liferay.item.selector.criteria.JournalArticleItemSelectorReturnType;
-import com.liferay.item.selector.criteria.info.item.criterion.InfoItemItemSelectorCriterion;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.layout.item.selector.criterion.LayoutItemSelectorCriterion;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
+
+import java.util.Map;
 
 import javax.portlet.PortletURL;
+
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Pavel Savinov
  */
 @Component(
-	immediate = true, property = "ddm.form.field.type.name=journal_article",
+	immediate = true, property = "ddm.form.field.type.name=link_to_layout",
 	service = {
 		DDMFormFieldTemplateContextContributor.class,
-		JournalArticleDDMFormFieldTemplateContextContributor.class
+		LayoutDDMFormFieldTemplateContextContributor.class
 	}
 )
 public class LayoutDDMFormFieldTemplateContextContributor
@@ -86,17 +89,20 @@ public class LayoutDDMFormFieldTemplateContextContributor
 			return null;
 		}
 
-		InfoItemItemSelectorCriterion infoItemItemSelectorCriterion =
-			new InfoItemItemSelectorCriterion();
+		LayoutItemSelectorCriterion layoutItemSelectorCriterion =
+			new LayoutItemSelectorCriterion();
 
-		infoItemItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
-			new JournalArticleItemSelectorReturnType());
+		layoutItemSelectorCriterion.setShowPrivatePages(true);
+		layoutItemSelectorCriterion.setShowPublicPages(true);
+
+		layoutItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
+			new UUIDItemSelectorReturnType());
 
 		PortletURL itemSelectorURL = _itemSelector.getItemSelectorURL(
 			RequestBackedPortletURLFactoryUtil.create(httpServletRequest),
 			ddmFormFieldRenderingContext.getPortletNamespace() +
 				"selectJournalArticle",
-			infoItemItemSelectorCriterion);
+			layoutItemSelectorCriterion);
 
 		return itemSelectorURL.toString();
 	}
