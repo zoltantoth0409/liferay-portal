e0c19079996735e9718b1167cfd557690cb418b9
Hugo Huijser <hugo.huijser@liferay.com>
2020-02-17 09:51:18 -0800

    LPS-105380 No need to declare abstract for methods in interface, since those are abstract by default

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaAbstractMethodCheck.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaTerm;
+
+/**
+ * @author Hugo Huijser
+ */
+public class JavaAbstractMethodCheck extends BaseJavaTermCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, JavaTerm javaTerm,
+		String fileContent) {
+
+		String content = javaTerm.getContent();
+
+		if (!javaTerm.isAbstract()) {
+			return content;
+		}
+
+		JavaClass javaClass = javaTerm.getParentJavaClass();
+
+		if (!javaClass.isInterface()) {
+			return content;
+		}
+
+		int x = content.indexOf("\t" + javaTerm.getAccessModifier());
+
+		if (x != -1) {
+			return StringUtil.replaceFirst(
+				content, "abstract", StringPool.BLANK, x);
+		}
+
+		return content;
+	}
+
+	@Override
+	protected String[] getCheckableJavaTermNames() {
+		return new String[] {JAVA_METHOD};
+	}
+
+}
