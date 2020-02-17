df10aab3fb6ffa8e9decc9a041b3fed1f5ad1607
Pavel Savinov <pavel.savinov@liferay.com>
2020-02-17 11:19:19 +0100

    LPS-99844 Copy from JournalArticleDDMFormFieldTemplateContextContributor

---
+++ modules/apps/layout/layout-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/layout/dynamic/data/mapping/form/field/type/internal/LayoutDDMFormFieldTemplateContextContributor.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.dynamic.data.mapping.form.field.type.internal;
+
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
+import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.item.selector.criteria.JournalArticleItemSelectorReturnType;
+import com.liferay.item.selector.criteria.info.item.criterion.InfoItemItemSelectorCriterion;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.portlet.PortletURL;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true, property = "ddm.form.field.type.name=journal_article",
+	service = {
+		DDMFormFieldTemplateContextContributor.class,
+		JournalArticleDDMFormFieldTemplateContextContributor.class
+	}
+)
+public class LayoutDDMFormFieldTemplateContextContributor
+	implements DDMFormFieldTemplateContextContributor {
+
+	@Override
+	public Map<String, Object> getParameters(
+		DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		LocalizedValue localizedValue =
+			(LocalizedValue)ddmFormField.getProperty("predefinedValue");
+
+		String predefinedValue = StringPool.BLANK;
+
+		if (localizedValue != null) {
+			predefinedValue = GetterUtil.getString(
+				localizedValue.getString(
+					ddmFormFieldRenderingContext.getLocale()));
+		}
+
+		return HashMapBuilder.<String, Object>put(
+			"itemSelectorURL",
+			getItemSelectorURL(
+				ddmFormFieldRenderingContext,
+				ddmFormFieldRenderingContext.getHttpServletRequest())
+		).put(
+			"portletNamespace",
+			ddmFormFieldRenderingContext.getPortletNamespace()
+		).put(
+			"predefinedValue", predefinedValue
+		).put(
+			"value",
+			GetterUtil.getString(
+				ddmFormFieldRenderingContext.getProperty("value"))
+		).build();
+	}
+
+	protected String getItemSelectorURL(
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext,
+		HttpServletRequest httpServletRequest) {
+
+		if (_itemSelector == null) {
+			return null;
+		}
+
+		InfoItemItemSelectorCriterion infoItemItemSelectorCriterion =
+			new InfoItemItemSelectorCriterion();
+
+		infoItemItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
+			new JournalArticleItemSelectorReturnType());
+
+		PortletURL itemSelectorURL = _itemSelector.getItemSelectorURL(
+			RequestBackedPortletURLFactoryUtil.create(httpServletRequest),
+			ddmFormFieldRenderingContext.getPortletNamespace() +
+				"selectJournalArticle",
+			infoItemItemSelectorCriterion);
+
+		return itemSelectorURL.toString();
+	}
+
+	@Reference
+	private ItemSelector _itemSelector;
+
+}
