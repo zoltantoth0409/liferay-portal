f68f9d24c95e51e3b5f7576367b62852bdecf37b
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-02-18 11:39:39 +0100

    LPS-108070 SF Update StringBundler

--- modules/dxp/apps/saml/saml-impl/src/main/java/com/liferay/saml/runtime/internal/servlet/filter/SamlSameSiteLaxCookiesFilter.java
+++ modules/dxp/apps/saml/saml-impl/src/main/java/com/liferay/saml/runtime/internal/servlet/filter/SamlSameSiteLaxCookiesFilter.java
@@ -106,7 +106,7 @@ public class SamlSameSiteLaxCookiesFilter extends BaseSamlPortalFilter {
 			return;
 		}
 
-		StringBundler sb = new StringBundler(8 + 5 * _PARAMS.length);
+		StringBundler sb = new StringBundler(7 + 5 * _PARAMS.length);
 
 		sb.append("<!DOCTYPE html>\n");
 		sb.append("<html><body onload=\"document.forms[0].submit()\">");
