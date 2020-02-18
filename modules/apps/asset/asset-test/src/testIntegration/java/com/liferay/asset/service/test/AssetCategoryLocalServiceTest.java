cd46e95edbfe912012749894fb7405b15d7adb09
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-02-18 15:50:46 +0100

    LPS-108880 Change to references instead of utils

--- modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/service/test/AssetCategoryLocalServiceTest.java
+++ modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/service/test/AssetCategoryLocalServiceTest.java
@@ -17,15 +17,15 @@ package com.liferay.asset.service.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetVocabulary;
-import com.liferay.asset.kernel.service.AssetCategoryLocalServiceUtil;
-import com.liferay.asset.kernel.service.AssetVocabularyLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.ListTypeConstants;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.OrganizationConstants;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
-import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.kernel.service.OrganizationLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.Locale;
@@ -84,12 +85,12 @@ public class AssetCategoryLocalServiceTest {
 				_group.getGroupId(), TestPropsValues.getUserId());
 
 		AssetVocabulary assetVocabulary =
-			AssetVocabularyLocalServiceUtil.addVocabulary(
+			_assetVocabularyLocalService.addVocabulary(
 				TestPropsValues.getUserId(), _group.getGroupId(),
 				RandomTestUtil.randomString(), titleMap, null, null,
 				serviceContext);
 
-		AssetCategory assetCategory = AssetCategoryLocalServiceUtil.addCategory(
+		AssetCategory assetCategory = _assetCategoryLocalService.addCategory(
 			TestPropsValues.getUserId(), _group.getGroupId(),
 			RandomTestUtil.randomString(), assetVocabulary.getVocabularyId(),
 			serviceContext);
@@ -97,7 +98,7 @@ public class AssetCategoryLocalServiceTest {
 		serviceContext.setAssetCategoryIds(
 			new long[] {assetCategory.getCategoryId()});
 
-		_organization = OrganizationLocalServiceUtil.addOrganization(
+		_organization = _organizationLocalService.addOrganization(
 			TestPropsValues.getUserId(),
 			OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID,
 			RandomTestUtil.randomString(),
@@ -117,8 +118,14 @@ public class AssetCategoryLocalServiceTest {
 
 		IndexerRegistryUtil.register(testAssetIndexer);
 
-		AssetCategoryLocalServiceUtil.deleteCategory(assetCategory, true);
+		_assetCategoryLocalService.deleteCategory(assetCategory, true);
 	}
+
+	@Inject
+	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Inject
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
 
 	@DeleteAfterTestRun
 	private Group _group;
@@ -127,5 +134,8 @@ public class AssetCategoryLocalServiceTest {
 	private Organization _organization;
 
 	private Indexer<Organization> _organizationIndexer;
+
+	@Inject
+	private OrganizationLocalService _organizationLocalService;
 
 }
