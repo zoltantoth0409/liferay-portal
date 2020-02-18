d3d6847010b7d3e25d9e7ad30e5f7347f09510ac
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2020-02-18 15:47:18 +0000

    LPS-105156 New custom SQL finder

---
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/persistence/impl/OAuth2AuthorizationFinderImpl.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.service.persistence.impl;
+
+import com.liferay.oauth2.provider.model.OAuth2Authorization;
+import com.liferay.oauth2.provider.model.impl.OAuth2AuthorizationImpl;
+import com.liferay.oauth2.provider.service.persistence.OAuth2AuthorizationFinder;
+import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.exception.SystemException;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Stian Sigvartsen
+ */
+@Component(service = OAuth2AuthorizationFinder.class)
+public class OAuth2AuthorizationFinderImpl
+	extends OAuth2AuthorizationFinderBaseImpl
+	implements OAuth2AuthorizationFinder {
+
+	public static final String FIND_BY_PURGEDATE =
+		OAuth2AuthorizationFinder.class.getName() + ".findByPurgeDate";
+
+	@Override
+	public Collection<OAuth2Authorization> findByPurgeDate(
+		Date purgeDate, int start, int end) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = _customSQL.get(getClass(), FIND_BY_PURGEDATE);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			q.addEntity("OAuth2Authorization", OAuth2AuthorizationImpl.class);
+
+			qPos.add(purgeDate);
+			qPos.add(purgeDate);
+
+			return (List<OAuth2Authorization>)QueryUtil.list(
+				q, getDialect(), start, end);
+		}
+		catch (Exception exception) {
+			throw new SystemException(exception);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@Override
+	public void removeByPurgeDate(Date purgeDate) {
+		for (OAuth2Authorization oAuth2Authorization :
+				findByPurgeDate(
+					purgeDate, QueryUtil.ALL_POS, QueryUtil.ALL_POS)) {
+
+			oAuth2AuthorizationPersistence.remove(oAuth2Authorization);
+		}
+	}
+
+	@Reference
+	private CustomSQL _customSQL;
+
+}
