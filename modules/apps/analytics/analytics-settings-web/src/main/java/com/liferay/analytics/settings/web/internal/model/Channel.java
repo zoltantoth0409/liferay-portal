1cf31f7f54a1f99b59c5a66a68f11a3120248cc1
Geyson Silva <geyson.silva@liferay.com>
2020-02-20 17:12:57 -0300

    LRAC-5187 Add id to Channel

--- modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/model/Channel.java
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/model/Channel.java
@@ -20,17 +20,27 @@ package com.liferay.analytics.settings.web.internal.model;
 public class Channel {
 
 	public Channel(Long id, String name) {
+		_id = id;
 		_name = name;
+	}
+
+	public Long getId() {
+		return _id;
 	}
 
 	public String getName() {
 		return _name;
 	}
 
+	public void setId(Long id) {
+		_id = id;
+	}
+
 	public void setName(String name) {
 		_name = name;
 	}
 
+	private Long _id;
 	private String _name;
 
 }
