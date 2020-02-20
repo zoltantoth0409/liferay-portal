7aa890e2ed3b0b4f1cd4264f0089ea97abb28747
Rubén Pulido <ruben.pulido@liferay.com>
2020-02-20 22:20:14 +0100

    LPS-108871 Create PageTemplateValidator

---
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/validator/PageTemplateValidator.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.validator;
+
+import com.liferay.layout.page.template.exception.PageTemplateValidatorException;
+import com.liferay.petra.json.validator.JSONValidator;
+import com.liferay.petra.json.validator.JSONValidatorException;
+import com.liferay.portal.kernel.util.Validator;
+
+/**
+ * @author Rubén Pulido
+ */
+public class PageTemplateValidator {
+
+	public static void validatePageTemplate(String pageTemplateJSON)
+		throws PageTemplateValidatorException {
+
+		if (Validator.isNull(pageTemplateJSON)) {
+			return;
+		}
+
+		try {
+			JSONValidator.validate(
+				pageTemplateJSON,
+				PageTemplateValidator.class.getResourceAsStream(
+					"dependencies/page_template_json_schema.json"));
+		}
+		catch (JSONValidatorException jsonValidatorException) {
+			throw new PageTemplateValidatorException(
+				jsonValidatorException.getMessage(), jsonValidatorException);
+		}
+	}
+
+}
