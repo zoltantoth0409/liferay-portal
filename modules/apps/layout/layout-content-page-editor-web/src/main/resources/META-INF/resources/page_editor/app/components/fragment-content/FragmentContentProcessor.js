6bf7e8631f0358cdbf2a7eb104fa873429deddbf
Sandro Chinea <sandrochinea@gmail.com>
2020-02-20 13:51:29 +0100

    LPS-108447 Remove unnecessary render

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment-content/FragmentContentProcessor.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment-content/FragmentContentProcessor.js
@@ -91,8 +91,6 @@ export default function FragmentContentProcessor({
 		processor.createEditor(
 			editableElement,
 			value => {
-				processor.render(editableElement, value, editableValue.config);
-
 				let nextEditableValue = {
 					...editableValue
 				};
