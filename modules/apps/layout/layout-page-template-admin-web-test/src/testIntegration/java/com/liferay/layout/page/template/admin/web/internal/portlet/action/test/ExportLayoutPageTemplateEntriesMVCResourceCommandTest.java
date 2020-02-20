d8c31f9d163f6c9c60c719fdc650f7727e4d7a7e
Rubén Pulido <ruben.pulido@liferay.com>
2020-02-20 13:39:49 +0100

    LPS-107723 testGetFile

---
+++ modules/apps/layout/layout-page-template-admin-web-test/src/testIntegration/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/test/ExportLayoutPageTemplateEntriesMVCResourceCommandTest.java
@@ -0,0 +1,259 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.admin.web.internal.portlet.action.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.model.LayoutPageTemplateCollection;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateCollectionLocalService;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayResourceRequest;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.io.File;
+
+import java.util.Enumeration;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import javax.portlet.ResourceRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Rubén Pulido
+ */
+@RunWith(Arquillian.class)
+public class ExportLayoutPageTemplateEntriesMVCResourceCommandTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group, TestPropsValues.getUserId());
+	}
+
+	@Test
+	public void testGetFile() throws Exception {
+		LayoutPageTemplateCollection layoutPageTemplateCollection =
+			_layoutPageTemplateCollectionLocalService.
+				addLayoutPageTemplateCollection(
+					TestPropsValues.getUserId(), _group.getGroupId(),
+					"Page Template Collection One", StringPool.BLANK,
+					_serviceContext);
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			_layoutPageTemplateEntryLocalService.addLayoutPageTemplateEntry(
+				_serviceContext.getUserId(), _serviceContext.getScopeGroupId(),
+				layoutPageTemplateCollection.
+					getLayoutPageTemplateCollectionId(),
+				"Page Template One",
+				LayoutPageTemplateEntryTypeConstants.TYPE_BASIC,
+				WorkflowConstants.STATUS_DRAFT, 0, _serviceContext);
+
+		_layoutPageTemplateStructureLocalService.addLayoutPageTemplateStructure(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			_portal.getClassNameId(Layout.class.getName()),
+			layoutPageTemplateEntry.getPlid(), _read("layout_data.json"),
+			_serviceContext);
+
+		File file = ReflectionTestUtil.invoke(
+			_mvcResourceCommand, "getFile",
+			new Class<?>[] {ResourceRequest.class},
+			_getMockResourceRequest(
+				layoutPageTemplateEntry.getLayoutPageTemplateEntryId()));
+
+		try (ZipFile zipFile = new ZipFile(file)) {
+			Enumeration<? extends ZipEntry> enumeration = zipFile.entries();
+
+			while (enumeration.hasMoreElements()) {
+				ZipEntry zipEntry = enumeration.nextElement();
+
+				_validateZipEntry(zipEntry, zipFile);
+			}
+
+			Assert.assertEquals(3, zipFile.size());
+		}
+	}
+
+	private MockResourceRequest _getMockResourceRequest(
+		long layoutPageTemplateEntryId) {
+
+		MockResourceRequest mockResourceRequest = new MockResourceRequest();
+
+		mockResourceRequest.addParameter(
+			"layoutPageTemplateEntryId",
+			String.valueOf(layoutPageTemplateEntryId));
+
+		return mockResourceRequest;
+	}
+
+	private boolean _isPageDefinitionFile(String path) {
+		String[] pathParts = StringUtil.split(path, CharPool.SLASH);
+
+		if ((pathParts.length == 4) &&
+			Objects.equals(pathParts[0], "page-templates") &&
+			Objects.equals(pathParts[3], "page-definition.json")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isPageTemplateCollectionFile(String path) {
+		String[] pathParts = StringUtil.split(path, CharPool.SLASH);
+
+		if ((pathParts.length == 3) &&
+			Objects.equals(pathParts[0], "page-templates") &&
+			Objects.equals(pathParts[2], "page-template-collection.json")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isPageTemplateFile(String path) {
+		String[] pathParts = StringUtil.split(path, CharPool.SLASH);
+
+		if ((pathParts.length == 4) &&
+			Objects.equals(pathParts[0], "page-templates") &&
+			Objects.equals(pathParts[3], "page-template.json")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private String _read(String fileName) throws Exception {
+		return new String(
+			FileUtil.getBytes(getClass(), "dependencies/" + fileName));
+	}
+
+	private void _validateContent(String content, String expectedFileName)
+		throws Exception {
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(content);
+
+		JSONObject expectedJSONObject = JSONFactoryUtil.createJSONObject(
+			_read(expectedFileName));
+
+		Assert.assertEquals(
+			expectedJSONObject.toJSONString(), jsonObject.toJSONString());
+	}
+
+	private void _validateZipEntry(ZipEntry zipEntry, ZipFile zipFile)
+		throws Exception {
+
+		if (_isPageDefinitionFile(zipEntry.getName())) {
+			_validateContent(
+				StringUtil.read(zipFile.getInputStream(zipEntry)),
+				"expected_page_definition.json");
+		}
+
+		if (_isPageTemplateCollectionFile(zipEntry.getName())) {
+			_validateContent(
+				StringUtil.read(zipFile.getInputStream(zipEntry)),
+				"expected_page_template_collection.json");
+		}
+
+		if (_isPageTemplateFile(zipEntry.getName())) {
+			_validateContent(
+				StringUtil.read(zipFile.getInputStream(zipEntry)),
+				"expected_page_template.json");
+		}
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private LayoutPageTemplateCollectionLocalService
+		_layoutPageTemplateCollectionLocalService;
+
+	@Inject
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
+
+	@Inject
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
+	@Inject(
+		filter = "mvc.command.name=/layout_page_template/export_layout_page_template_entry"
+	)
+	private MVCResourceCommand _mvcResourceCommand;
+
+	@Inject
+	private Portal _portal;
+
+	private ServiceContext _serviceContext;
+
+	private static class MockResourceRequest
+		extends MockLiferayResourceRequest {
+
+		public MockResourceRequest() {
+		}
+
+		@Override
+		public HttpServletRequest getHttpServletRequest() {
+			return new MockHttpServletRequest();
+		}
+
+	}
+
+}
