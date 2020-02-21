f3b40746d94c4e66e2ff133a67215084dea73c15
Victor Galan <victor.galan@liferay.com>
2020-02-21 14:06:46 +0100

    LPS-109156 Create component to show a list of fieldsets

---
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/components/fieldsets/Fieldsets.es.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React, {useContext} from 'react';
+
+import AppContext from '../../AppContext.es';
+import saveDefinitionAndLayout from '../../data-layout-builder/saveDefinitionAndLayout.es';
+import FieldType from '../field-types/FieldType.es';
+
+export default function FieldSets() {
+	const [{fieldsets}] = useContext(AppContext);
+
+	return (
+		<>
+			{fieldsets.map(fieldset => (
+				<FieldType
+					description={`${
+						fieldset.dataDefinitionFields.length
+					} ${Liferay.Language.get('fields')}`}
+					icon="forms"
+					key={fieldset.dataDefinitionKey}
+					label={fieldset.name[themeDisplay.getLanguageId()]}
+				/>
+			))}
+		</>
+	);
+}
