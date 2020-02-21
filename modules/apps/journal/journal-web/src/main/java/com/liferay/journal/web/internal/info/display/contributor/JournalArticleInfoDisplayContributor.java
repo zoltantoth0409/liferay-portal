43ac3659f05f0f9e9fda7dbc40e871223f12e173
Hugo Huijser <hugo.huijser@liferay.com>
2020-02-21 09:01:51 -0800

    LPS-108884 Missing import

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/display/contributor/JournalArticleInfoDisplayContributor.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/display/contributor/JournalArticleInfoDisplayContributor.java
@@ -47,6 +47,7 @@ import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portlet.display.template.PortletDisplayTemplate;
