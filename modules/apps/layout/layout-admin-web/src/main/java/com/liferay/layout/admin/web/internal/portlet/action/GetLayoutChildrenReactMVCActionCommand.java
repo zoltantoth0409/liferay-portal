ef5f8080e3aefd21fb2ae3a6c74362126b2f1e22
Carlos Lancha <carlos.lancha@liferay.com>
2020-02-22 22:49:31 +0100

    LPS-109145 Copy GetLayoutChildrenMVCActionCommand

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/GetLayoutChildrenReactMVCActionCommand.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.portlet.action;
+
+import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
+import com.liferay.layout.admin.web.internal.configuration.LayoutConverterConfiguration;
+import com.liferay.layout.admin.web.internal.configuration.LayoutEditorTypeConfiguration;
+import com.liferay.layout.admin.web.internal.display.context.LayoutsAdminReactDisplayContext;
+import com.liferay.layout.util.LayoutCopyHelper;
+import com.liferay.layout.util.template.LayoutConverterRegistry;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.staging.StagingGroupHelper;
+
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	configurationPid = {
+		"com.liferay.layout.admin.web.internal.configuration.LayoutConverterConfiguration",
+		"com.liferay.layout.admin.web.internal.configuration.LayoutEditorTypeConfiguration"
+	},
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + LayoutAdminPortletKeys.GROUP_PAGES,
+		"mvc.command.name=/layout/react/get_layout_children"
+	},
+	service = MVCActionCommand.class
+)
+public class GetLayoutChildrenReactMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_layoutConverterConfiguration = ConfigurableUtil.createConfigurable(
+			LayoutConverterConfiguration.class, properties);
+
+		_layoutEditorTypeConfiguration = ConfigurableUtil.createConfigurable(
+			LayoutEditorTypeConfiguration.class, properties);
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long plid = ParamUtil.getLong(actionRequest, "plid");
+
+		Layout layout = _layoutLocalService.fetchLayout(plid);
+
+		LayoutsAdminReactDisplayContext layoutsAdminReactDisplayContext =
+			new LayoutsAdminReactDisplayContext(
+				_layoutConverterConfiguration, _layoutConverterRegistry,
+				_layoutCopyHelper, _layoutEditorTypeConfiguration,
+				_portal.getLiferayPortletRequest(actionRequest),
+				_portal.getLiferayPortletResponse(actionResponse),
+				_stagingGroupHelper);
+
+		JSONArray jsonArray =
+			layoutsAdminReactDisplayContext.getLayoutsJSONArray(
+				layout.getLayoutId(), layout.isPrivateLayout());
+
+		JSONObject jsonObject = JSONUtil.put("children", jsonArray);
+
+		JSONPortletResponseUtil.writeJSON(
+			actionRequest, actionResponse, jsonObject);
+	}
+
+	private volatile LayoutConverterConfiguration _layoutConverterConfiguration;
+
+	@Reference
+	private LayoutConverterRegistry _layoutConverterRegistry;
+
+	@Reference
+	private LayoutCopyHelper _layoutCopyHelper;
+
+	private volatile LayoutEditorTypeConfiguration
+		_layoutEditorTypeConfiguration;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private StagingGroupHelper _stagingGroupHelper;
+
+}
