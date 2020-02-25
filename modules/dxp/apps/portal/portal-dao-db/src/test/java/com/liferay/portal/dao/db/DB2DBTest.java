ff33b189e0784f837a3529c2bbe12dbdd1e20ece
shuyangzhou <shuyang.zhou@liferay.com>
2020-02-25 10:59:48 -0800

    LPS-107396 Proper assert

--- modules/dxp/apps/portal/portal-dao-db/src/test/java/com/liferay/portal/dao/db/DB2DBTest.java
+++ modules/dxp/apps/portal/portal-dao-db/src/test/java/com/liferay/portal/dao/db/DB2DBTest.java
@@ -17,10 +17,6 @@ package com.liferay.portal.dao.db;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.test.BaseDBTestCase;
 
-import java.io.IOException;
-
-import java.sql.SQLException;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -48,25 +44,25 @@ public class DB2DBTest extends BaseDBTestCase {
 
 	@Test
 	public void testRewordAlterColumnTypeNotNull() throws Exception {
-		_nullableAlter =
-			"alter table DLFolder alter column userName set not null;";
-
 		Assert.assertEquals(
 			"alter table DLFolder alter column userName set data type " +
 				"varchar(75);\n",
 			buildSQL(
 				"alter_column_type DLFolder userName VARCHAR(75) not null;"));
+		Assert.assertEquals(
+			"alter table DLFolder alter column userName set not null;",
+			_nullableAlter);
 	}
 
 	@Test
 	public void testRewordAlterColumnTypeNull() throws Exception {
-		_nullableAlter =
-			"alter table DLFolder alter column userName drop not null;";
-
 		Assert.assertEquals(
 			"alter table DLFolder alter column userName set data type " +
 				"varchar(75);\n",
 			buildSQL("alter_column_type DLFolder userName VARCHAR(75) null;"));
+		Assert.assertEquals(
+			"alter table DLFolder alter column userName drop not null;",
+			_nullableAlter);
 	}
 
 	@Test
@@ -80,10 +76,8 @@ public class DB2DBTest extends BaseDBTestCase {
 		return new DB2DB(0, 0) {
 
 			@Override
-			public void runSQL(String template)
-				throws IOException, SQLException {
-
-				Assert.assertEquals(_nullableAlter, template);
+			public void runSQL(String template) {
+				_nullableAlter = template;
 			}
 
 		};
