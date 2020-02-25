29b19e066ecc8be91068930e858f6646897acaec
Pavel Savinov <pavel.savinov@liferay.com>
2020-02-25 17:21:56 +0100

    LPS-109133 Use existing DDM template handler to render link to page fields

--- modules/apps/layout/layout-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/layout/dynamic/data/mapping/form/field/type/internal/LayoutDDMFormFieldTypeSettings.java
+++ modules/apps/layout/layout-dynamic-data-mapping-form-field-type/src/main/java/com/liferay/layout/dynamic/data/mapping/form/field/type/internal/LayoutDDMFormFieldTypeSettings.java
@@ -65,6 +65,10 @@ import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 public interface LayoutDDMFormFieldTypeSettings
 	extends DefaultDDMFormFieldTypeSettings {
 
+	@DDMFormField(predefinedValue = "link-to-page")
+	@Override
+	public String dataType();
+
 	@DDMFormField(
 		dataType = "string", label = "%predefined-value",
 		type = "link_to_layout"
