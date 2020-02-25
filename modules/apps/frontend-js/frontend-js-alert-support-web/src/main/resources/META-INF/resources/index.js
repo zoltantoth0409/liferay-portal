14709172e1ec4ded9effb4135681eb75e08b690b
Marko Čikoš <marko.cikos@liferay.com>
2020-02-25 17:03:08 +0100

    LPS-108492 Add AlertProvider to replace Bootstrap Alert plugin

---
+++ modules/apps/frontend-js/frontend-js-alert-support-web/src/main/resources/META-INF/resources/index.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import dom from 'metal-dom';
+
+let handle;
+
+export default () => {
+	if (!handle) {
+		handle = dom.delegate(
+			document.body,
+			'click',
+			'[data-dismiss="liferay-alert"]',
+			event => {
+				event.preventDefault();
+
+				const container = dom.closest(event.delegateTarget, '.alert');
+
+				if (container) {
+					container.parentNode.removeChild(container);
+				}
+			}
+		);
+	}
+};
