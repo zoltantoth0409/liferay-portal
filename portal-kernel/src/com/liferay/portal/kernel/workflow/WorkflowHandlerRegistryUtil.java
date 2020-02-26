2831ad9e3258d616f4de85efc97710d14941e30d
Brian Chan <brian.chan@liferay.com>
2020-02-25 20:10:44 -0800

    LPS-109118 auto SF

--- portal-kernel/src/com/liferay/portal/kernel/workflow/WorkflowHandlerRegistryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/workflow/WorkflowHandlerRegistryUtil.java
@@ -42,7 +42,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.function.Predicate;
 
