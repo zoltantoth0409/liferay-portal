bf96c740f4cc89482f68224e777d35685afcfdf6
Brian Chan <brian.chan@liferay.com>
2020-02-26 11:40:15 -0800

    Revert "LPS-109118 auto SF"
    This reverts commit 2831ad9e3258d616f4de85efc97710d14941e30d.

--- portal-kernel/src/com/liferay/portal/kernel/workflow/WorkflowHandlerRegistryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/workflow/WorkflowHandlerRegistryUtil.java
@@ -42,6 +42,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.function.Predicate;
 
