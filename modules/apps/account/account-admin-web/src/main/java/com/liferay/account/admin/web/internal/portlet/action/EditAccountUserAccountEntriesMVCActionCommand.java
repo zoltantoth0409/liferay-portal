589a9c22507a80f2736716bb23343211373084a1
alee8888 <albert.lee@liferay.com>
2020-02-26 12:35:45 -0800

    LPS-106872 Add Account User Account Entries MVCActionCommand

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/EditAccountUserAccountEntriesMVCActionCommand.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.portlet.action;
+
+import com.liferay.account.constants.AccountPortletKeys;
+import com.liferay.account.service.AccountEntryUserRelLocalService;
+import com.liferay.petra.lang.SafeClosable;
+import com.liferay.portal.kernel.messaging.proxy.ProxyModeThreadLocal;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Albert Lee
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AccountPortletKeys.ACCOUNT_USERS_ADMIN,
+		"mvc.command.name=/account_admin/edit_account_user_account_entries"
+	},
+	service = MVCActionCommand.class
+)
+public class EditAccountUserAccountEntriesMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long[] addAccountEntryIds = ParamUtil.getLongValues(
+			actionRequest, "addAccountEntryIds");
+
+		long[] deleteAccountEntryIds = ParamUtil.getLongValues(
+			actionRequest, "deleteAccountEntryIds");
+
+		long accountUserId = ParamUtil.getLong(actionRequest, "accountUserId");
+
+		try (SafeClosable safeClosable =
+				ProxyModeThreadLocal.setWithSafeClosable(true)) {
+
+			_accountEntryUserRelLocalService.updateAccountEntryUserRels(
+				addAccountEntryIds, deleteAccountEntryIds, accountUserId);
+		}
+	}
+
+	@Reference
+	private AccountEntryUserRelLocalService _accountEntryUserRelLocalService;
+
+}
