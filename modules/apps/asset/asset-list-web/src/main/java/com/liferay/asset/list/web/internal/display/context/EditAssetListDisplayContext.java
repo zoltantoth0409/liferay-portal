6c3080e94fc96fe38ad1749bd85f260ca6e224dd
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-02-27 15:39:59 +0100

    LPS-109224 We should allow multiple group ids since we can scope several sites

--- modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/display/context/EditAssetListDisplayContext.java
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/display/context/EditAssetListDisplayContext.java
@@ -880,7 +880,7 @@ public class EditAssetListDisplayContext {
 			}
 
 			portletURL.setParameter(
-				"groupIds", String.valueOf(_themeDisplay.getScopeGroupId()));
+				"groupIds", StringUtil.merge(getSelectedGroupIds()));
 			portletURL.setParameter(
 				"eventName", _portletResponse.getNamespace() + "selectTag");
 			portletURL.setParameter("selectedTagNames", "{selectedTagNames}");
