20199f89f83498857bcc1aa0d6f985ad3f42f6c0
alee8888 <albert.lee@liferay.com>
2020-02-28 15:32:00 -0800

    LPS-106872 Use single thread when activating/deactivating Account Entry

--- modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/UpdateAccountEntryStatusMVCActionCommand.java
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/UpdateAccountEntryStatusMVCActionCommand.java
@@ -16,6 +16,8 @@ package com.liferay.account.admin.web.internal.portlet.action;
 
 import com.liferay.account.constants.AccountPortletKeys;
 import com.liferay.account.service.AccountEntryLocalService;
+import com.liferay.petra.lang.SafeClosable;
+import com.liferay.portal.kernel.messaging.proxy.ProxyModeThreadLocal;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.util.Constants;
@@ -52,10 +54,20 @@ public class UpdateAccountEntryStatusMVCActionCommand
 			actionRequest, "accountEntryIds");
 
 		if (cmd.equals(Constants.DEACTIVATE)) {
-			_accountEntryLocalService.deactivateAccountEntries(accountEntryIds);
+			try (SafeClosable safeClosable =
+					ProxyModeThreadLocal.setWithSafeClosable(true)) {
+
+				_accountEntryLocalService.deactivateAccountEntries(
+					accountEntryIds);
+			}
 		}
 		else if (cmd.equals(Constants.RESTORE)) {
-			_accountEntryLocalService.activateAccountEntries(accountEntryIds);
+			try (SafeClosable safeClosable =
+					ProxyModeThreadLocal.setWithSafeClosable(true)) {
+
+				_accountEntryLocalService.activateAccountEntries(
+					accountEntryIds);
+			}
 		}
 	}
 
