daf3ef76a288898a2c94e3432ce19f133410c69e
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-03-02 15:41:01 -0800

    LRAC-5266 add ExpandoRow model listener

---
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/ExpandoRowModelListener.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.message.sender.internal.model.listener;
+
+import com.liferay.analytics.message.sender.model.EntityModelListener;
+import com.liferay.expando.kernel.model.ExpandoRow;
+import com.liferay.expando.kernel.service.ExpandoRowLocalService;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component(
+	immediate = true, service = {EntityModelListener.class, ModelListener.class}
+)
+public class ExpandoRowModelListener
+	extends BaseEntityModelListener<ExpandoRow> {
+
+	@Override
+	public List<String> getAttributeNames() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	protected ExpandoRow getModel(long id) throws Exception {
+		return _expandoRowLocalService.getExpandoRow(id);
+	}
+
+	@Override
+	protected String getPrimaryKeyName() {
+		return "userId";
+	}
+
+	@Reference
+	private ExpandoRowLocalService _expandoRowLocalService;
+
+}
