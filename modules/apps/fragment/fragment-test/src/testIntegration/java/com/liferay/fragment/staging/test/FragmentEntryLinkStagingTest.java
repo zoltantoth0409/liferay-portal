a2834a0ffb8b8c35f803ab766fffbb0bb69bed9e
Pavel Savinov <pavel.savinov@liferay.com>
2020-03-02 11:03:32 +0100

    LPS-109584 Process only published layouts as a part of staging/export-import process

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/staging/test/FragmentEntryLinkStagingTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/staging/test/FragmentEntryLinkStagingTest.java
@@ -24,11 +24,11 @@ import com.liferay.fragment.service.FragmentEntryLocalService;
 import com.liferay.fragment.util.FragmentEntryTestUtil;
 import com.liferay.fragment.util.FragmentStagingTestUtil;
 import com.liferay.fragment.util.FragmentTestUtil;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -37,9 +37,15 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -67,12 +73,20 @@ public class FragmentEntryLinkStagingTest {
 			ServiceContextTestUtil.getServiceContext(
 				_liveGroup.getGroupId(), TestPropsValues.getUserId());
 
-		_layout = LayoutLocalServiceUtil.addLayout(
+		Map<Locale, String> nameMap = Collections.singletonMap(
+			LocaleUtil.getDefault(), RandomTestUtil.randomString());
+
+		UnicodeProperties typeSettings = new UnicodeProperties();
+
+		typeSettings.put("published", "true");
+
+		_layout = _layoutLocalService.addLayout(
 			TestPropsValues.getUserId(), _liveGroup.getGroupId(), false,
-			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
-			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
-			StringPool.BLANK, LayoutConstants.TYPE_CONTENT, false,
-			StringPool.BLANK, serviceContext);
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, 0, 0, nameMap, nameMap,
+			Collections.emptyMap(), Collections.emptyMap(),
+			Collections.emptyMap(), LayoutConstants.TYPE_CONTENT,
+			typeSettings.toString(), false, false, Collections.emptyMap(),
+			serviceContext);
 	}
 
 	@Test
@@ -176,6 +190,9 @@ public class FragmentEntryLinkStagingTest {
 	private FragmentEntryLocalService _fragmentEntryLocalService;
 
 	private Layout _layout;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
 
 	@DeleteAfterTestRun
 	private Group _liveGroup;
