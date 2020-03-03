e1e5e49618e714fb7e6546dbc8fce13605d0cf87
Rubén Pulido <ruben.pulido@liferay.com>
2020-03-03 23:59:29 +0100

    LPS-109642 Fix URL. It should be an InlineValue with a value attribute

--- modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/headless/delivery/dto/v1_0/PageDefinitionConverterUtil.java
+++ modules/apps/layout/layout-page-template-api/src/main/java/com/liferay/layout/page/template/headless/delivery/dto/v1_0/PageDefinitionConverterUtil.java
@@ -283,8 +283,11 @@ public class PageDefinitionConverterUtil {
 
 									return new FragmentImage() {
 										{
-											url = backgroundImageJSONObject.get(
-												"url");
+											url = new InlineValue() {
+												{
+													value = urlValue;
+												}
+											};
 
 											setTitle(
 												() -> {
