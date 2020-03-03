d89927859d0dff713cb3ff543fde77f314eafc40
Gabriel Ibson <gabriel.souza@liferay.com>
2020-03-03 18:05:46 -0300

    LPS-97802 buildService

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-uad/src/main/java/com/liferay/dynamic/data/mapping/uad/constants/DDMUADConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.uad.constants;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+public class DDMUADConstants {
+
+	public static final String[] USER_ID_FIELD_NAMES_DDM_FORM_INSTANCE = {
+		"userId", "versionUserId"
+	};
+
+	public static final String[] USER_ID_FIELD_NAMES_DDM_FORM_INSTANCE_RECORD =
+		{"userId", "versionUserId"};
+
+}
