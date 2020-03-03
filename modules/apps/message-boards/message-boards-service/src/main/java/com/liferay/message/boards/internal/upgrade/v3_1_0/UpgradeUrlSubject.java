8b1cf8c25978e4e0b62995d7e37940c5984afdd7
David Gomez G <dgomezg@gmail.com>
2020-03-03 05:20:54 +0100

    LPS-104321 Improved the UniqueURL generation to avoid multiple DB queries

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/upgrade/v3_1_0/UpgradeUrlSubject.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/upgrade/v3_1_0/UpgradeUrlSubject.java
@@ -61,9 +61,7 @@ public class UpgradeUrlSubject extends UpgradeProcess {
 
 			rs = ps.executeQuery();
 
-			if (!rs.next()) {
-				return urlSubject;
-			}
+			rs.first();
 
 			int mbMessageCount = rs.getInt(1);
 
@@ -71,7 +69,7 @@ public class UpgradeUrlSubject extends UpgradeProcess {
 				return urlSubject;
 			}
 
-			return null;
+			return urlSubject + StringPool.DASH + mbMessageCount;
 		}
 		finally {
 			DataAccess.cleanUp(ps);
@@ -138,11 +136,6 @@ public class UpgradeUrlSubject extends UpgradeProcess {
 		for (Map.Entry<Long, String> entry : urlSubjects.entrySet()) {
 			String uniqueUrlSubject = _findUniqueUrlSubject(
 				connection, entry.getValue());
-
-			for (int i = 1; uniqueUrlSubject == null; i++) {
-				uniqueUrlSubject = _findUniqueUrlSubject(
-					connection, entry.getValue() + StringPool.DASH + i);
-			}
 
 			_updateMBMessage(connection, entry.getKey(), uniqueUrlSubject);
 		}
