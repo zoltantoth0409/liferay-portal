3e2ff0625bd76440ae329cdf0f52478b6ad45f4a
Victor Galan <victor.galan@liferay.com>
2020-03-05 12:48:26 +0100

    LPS-109725 Wrap contents of collection in the ControlsIdConverteContextProvider so we can duplicate it and each will use different ids for controlling active/hover/processorenabled

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Collection.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Collection.js
@@ -14,16 +14,60 @@
 
 import React from 'react';
 
+import {ControlsIdConverterContextProvider} from '../ControlsIdConverterContext';
+
+const COLLECTION_ID_DIVIDER = '$';
+
+function getCollectionPrefix(collectionId, index) {
+	return `collection-${collectionId}-${index}${COLLECTION_ID_DIVIDER}`;
+}
+
+function getToControlsId(collectionId, index) {
+	return itemId => {
+		if (!itemId) {
+			return null;
+		}
+
+		return `${getCollectionPrefix(collectionId, index)}${itemId}`;
+	};
+}
+
+function fromControlsId(controlsItemId) {
+	if (!controlsItemId) {
+		return null;
+	}
+
+	const [, itemId] = controlsItemId.split(COLLECTION_ID_DIVIDER);
+
+	return itemId;
+}
+
 const NotMappedMessage = () => (
 	<div className="page-editor__collection__not-mapped-message">
 		{Liferay.Language.get('not-mapped')}
 	</div>
 );
 
-const Collection = React.forwardRef(({_props}, ref) => {
+const Collection = React.forwardRef(({children, item}, ref) => {
+	if (React.Children.count(children) == 0) {
+		return <NotMappedMessage />;
+	}
+
+	const child = React.Children.only(children);
+
 	return (
 		<div className="page-editor__collection" ref={ref}>
-			<NotMappedMessage></NotMappedMessage>
+			{Array.from({length: 3}).map((_element, idx) => (
+				<ControlsIdConverterContextProvider
+					key={idx}
+					value={{
+						fromControlsId,
+						toControlsId: getToControlsId(item.itemId, idx),
+					}}
+				>
+					{React.cloneElement(child)}
+				</ControlsIdConverterContextProvider>
+			))}
 		</div>
 	);
 });
