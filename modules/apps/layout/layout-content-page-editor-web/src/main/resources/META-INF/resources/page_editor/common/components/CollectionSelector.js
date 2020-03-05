bb447d2bb31fed0332c046fb9810e001e83119d1
Sandro Chinea <sandrochinea@gmail.com>
2020-03-05 13:57:28 +0100

    LPS-109777 Added CollectionSelector common component

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/CollectionSelector.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import {config} from '../../app/config/index';
+import ItemSelector from './ItemSelector';
+
+export default function CollectionSelector({
+	collectionTitle,
+	label,
+	onCollectionSelect,
+}) {
+	const eventName = `${config.portletNamespace}selectInfoList`;
+
+	return (
+		<ItemSelector
+			eventName={eventName}
+			itemSelectorURL={config.infoListSelectorURL}
+			label={label}
+			onItemSelect={onCollectionSelect}
+			selectedItemTitle={collectionTitle}
+		/>
+	);
+}
+
+CollectionSelector.propTypes = {
+	collectionTitle: PropTypes.string,
+	label: PropTypes.string,
+	onCollectionSelect: PropTypes.func.isRequired,
+};
