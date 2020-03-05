091bf6a11e607f516e7852e792d94d30387d5c55
Adolfo Pérez <adolfo.perez@liferay.com>
2020-03-05 12:02:14 +0100

    LPS-109315 Use existing method instead of regexp

--- modules/apps/upload/upload-web/src/main/java/com/liferay/upload/web/internal/DefaultUniqueFileNameProvider.java
+++ modules/apps/upload/upload-web/src/main/java/com/liferay/upload/web/internal/DefaultUniqueFileNameProvider.java
@@ -14,15 +14,13 @@
 
 package com.liferay.upload.web.internal;
 
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.upload.UploadServletRequestConfigurationHelper;
+import com.liferay.portal.kernel.util.File;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.upload.UniqueFileNameProvider;
 
 import java.util.function.Predicate;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -37,9 +35,9 @@ public class DefaultUniqueFileNameProvider implements UniqueFileNameProvider {
 	public String provide(String fileName, Predicate<String> predicate)
 		throws PortalException {
 
-		fileName = _removeParentheticalSuffix(fileName);
+		String baseFileName = _file.stripParentheticalSuffix(fileName);
 
-		String uniqueFileName = fileName;
+		String uniqueFileName = baseFileName;
 
 		int tries = 0;
 
@@ -48,37 +46,20 @@ public class DefaultUniqueFileNameProvider implements UniqueFileNameProvider {
 					_uploadServletRequestConfigurationHelper.getMaxTries()) {
 
 				throw new PortalException(
-					"Unable to get a unique file name for " + fileName);
+					"Unable to get a unique file name for " + baseFileName);
 			}
 
 			tries++;
 
 			uniqueFileName = FileUtil.appendParentheticalSuffix(
-				fileName, String.valueOf(tries));
+				baseFileName, String.valueOf(tries));
 		}
 
 		return uniqueFileName;
 	}
 
-	private String _removeParentheticalSuffix(String fileName) {
-		Matcher matcher = _pattern.matcher(fileName);
-
-		if (matcher.matches()) {
-			String name = matcher.group("name");
-			String extension = matcher.group("extension");
-
-			fileName = name;
-
-			if (extension != null) {
-				fileName += StringPool.PERIOD + extension;
-			}
-		}
-
-		return fileName;
-	}
-
-	private static final Pattern _pattern = Pattern.compile(
-		"(?<name>.+) \\(\\d+\\)(\\.(?<extension>[^.]+))?");
+	@Reference
+	private File _file;
 
 	@Reference
 	private UploadServletRequestConfigurationHelper
