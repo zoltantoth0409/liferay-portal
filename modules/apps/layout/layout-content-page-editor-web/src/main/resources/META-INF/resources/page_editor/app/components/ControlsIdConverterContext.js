db387b80451de2b69e8fa3c6e63474f7f5961c72
Victor Galan <victor.galan@liferay.com>
2020-03-05 11:26:14 +0100

    LPS-109725 Create a context that will provide two function for converting between controls id and the real id
    Currently, layout data items and editable ids are used also in the controls context to mark which items are active or hovered, but for collections it may be several fragments with the same id that can be activated separately

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/ControlsIdConverterContext.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React, {useContext} from 'react';
+
+const INITIAL_STATE = {
+	fromControlsId: itemId => itemId,
+	toControlsId: controlId => controlId,
+};
+
+const ControlsIdConverterContext = React.createContext(INITIAL_STATE);
+
+const ControlsIdConverterContextProvider = ControlsIdConverterContext.Provider;
+
+const useFromControlsId = () => {
+	const context = useContext(ControlsIdConverterContext);
+
+	return context.fromControlsId;
+};
+
+const useToControlsId = () => {
+	const context = useContext(ControlsIdConverterContext);
+
+	return context.toControlsId;
+};
+
+export {ControlsIdConverterContextProvider, useFromControlsId, useToControlsId};
