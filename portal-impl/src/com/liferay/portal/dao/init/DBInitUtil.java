f810bf9e718d3d211951629c87178076ffa39ee0
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-03-06 12:34:26 -0800

    LPS-108239 Wrap the connection

--- portal-impl/src/com/liferay/portal/dao/init/DBInitUtil.java
+++ portal-impl/src/com/liferay/portal/dao/init/DBInitUtil.java
@@ -17,6 +17,7 @@ package com.liferay.portal.dao.init;
 import com.liferay.petra.io.StreamUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.events.StartupHelperUtil;
+import com.liferay.portal.internal.db.partition.DBPartitionHelperUtil;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBManager;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
@@ -51,8 +52,9 @@ public class DBInitUtil {
 	}
 
 	public static void init() throws Exception {
-		_dataSource = DataSourceFactoryUtil.initDataSource(
-			PropsUtil.getProperties("jdbc.default.", true));
+		_dataSource = DBPartitionHelperUtil.wrapDataSource(
+			DataSourceFactoryUtil.initDataSource(
+				PropsUtil.getProperties("jdbc.default.", true)));
 
 		DB db = DBManagerUtil.getDB(
 			DBManagerUtil.getDBType(DialectDetector.getDialect(_dataSource)),
