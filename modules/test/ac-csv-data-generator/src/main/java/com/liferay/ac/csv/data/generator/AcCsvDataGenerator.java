108600f97fe7a1f2b04dd1edaeb4598ed7be1d03
Cheryl Tang <cheryl.tang@liferay.com>
2020-03-05 18:13:36 -0800

    LRAC-5372 Rename CsvUser to UserCsv and simplify entry point file

--- modules/test/ac-csv-data-generator/src/main/java/com/liferay/ac/csv/data/generator/AcCsvDataGenerator.java
+++ modules/test/ac-csv-data-generator/src/main/java/com/liferay/ac/csv/data/generator/AcCsvDataGenerator.java
@@ -15,34 +15,17 @@
 package com.liferay.ac.csv.data.generator;
 
 import com.liferay.ac.csv.data.generator.configuration.AcCsvDataGeneratorConfiguration;
-import com.liferay.ac.csv.data.generator.csv.CsvUser;
+import com.liferay.ac.csv.data.generator.csv.UserCsv;
 import com.liferay.ac.csv.data.generator.util.GeneratedDataUtil;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.model.Organization;
-import com.liferay.portal.kernel.model.Role;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.service.CompanyLocalService;
-import com.liferay.portal.kernel.service.OrganizationLocalService;
-import com.liferay.portal.kernel.service.RoleLocalService;
-import com.liferay.portal.kernel.service.UserGroupLocalService;
 
-import java.io.File;
-import java.io.IOException;
-
-import java.nio.charset.Charset;
-
-import java.util.Arrays;
 import java.util.Map;
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -62,74 +45,13 @@ public class AcCsvDataGenerator {
 
 	@Activate
 	protected void activate(Map<String, Object> properties) {
-		AcCsvDataGeneratorConfiguration acCsvDataGeneratorConfiguration =
-			ConfigurableUtil.createConfigurable(
-				AcCsvDataGeneratorConfiguration.class, properties);
+		_loadConfig(properties);
 
-		_verbose = acCsvDataGeneratorConfiguration.verbose();
+		_userCsv.addCsvUsers(_acCsvDataGeneratorConfiguration.pathToUserCsv());
 
-		try {
-			Company company = _companyLocalService.getCompanyByVirtualHost(
-				acCsvDataGeneratorConfiguration.virtualHostName());
-
-			_companyId = company.getPrimaryKey();
-
-			_generatedDataUtil.setCompanyId(_companyId);
-		}
-		catch (PortalException portalException) {
-			_log.error(portalException, portalException);
-		}
-
-		try {
-			File csvFile = new File(
-				acCsvDataGeneratorConfiguration.pathToUserCsv());
-
-			CSVParser csvParser = CSVParser.parse(
-				csvFile, Charset.defaultCharset(),
-				CSVFormat.DEFAULT.withFirstRecordAsHeader(
-				).withIgnoreSurroundingSpaces(
-				).withNullString(
-					""
-				));
-
-			for (CSVRecord csvRecord : csvParser) {
-				if (_generatedDataUtil.containsUserKey(
-						csvRecord.get("emailAddress"))) {
-
-					continue;
-				}
-
-				_csvUser.assignOrganizations(
-					_getIdArrayFromCell(csvRecord, "organizations"));
-
-				_csvUser.assignUserGroups(
-					_getIdArrayFromCell(csvRecord, "userGroups"));
-
-				_csvUser.assignRoles(_getIdArrayFromCell(csvRecord, "roles"));
-
-				try {
-					User user = _csvUser.addUser(csvRecord);
-
-					_generatedDataUtil.putUser(
-						user.getEmailAddress(), user.getPrimaryKey());
-
-					if (!_verbose.equalsIgnoreCase("false")) {
-						System.out.println(
-							"Created user: " + user.getEmailAddress());
-					}
-				}
-				catch (PortalException portalException) {
-					_log.error(portalException, portalException);
-				}
-			}
-
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					acCsvDataGeneratorConfiguration.customActivationMessage());
-			}
-		}
-		catch (IOException ioException) {
-			_log.error(ioException, ioException);
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				_acCsvDataGeneratorConfiguration.customActivationMessage());
 		}
 	}
 
@@ -143,103 +65,35 @@ public class AcCsvDataGenerator {
 		ModuleServiceLifecycle moduleServiceLifecycle) {
 	}
 
-	private long[] _getIdArrayFromCell(CSVRecord csvRecord, String headerName) {
-		String recordCell = csvRecord.get(headerName);
+	private void _loadConfig(Map<String, Object> properties) {
+		_acCsvDataGeneratorConfiguration = ConfigurableUtil.createConfigurable(
+			AcCsvDataGeneratorConfiguration.class, properties);
 
-		if (recordCell == null) {
-			return null;
+		try {
+			Company company = _companyLocalService.getCompanyByVirtualHost(
+				_acCsvDataGeneratorConfiguration.virtualHostName());
+
+			long companyId = company.getPrimaryKey();
+
+			_generatedDataUtil.setCompanyId(companyId);
 		}
-
-		String[] stringArray = recordCell.split(",");
-
-		long[] idArray = new long[stringArray.length];
-
-		for (int i = 0; i < stringArray.length; i++) {
-			String name = stringArray[i].trim();
-
-			try {
-				if (headerName.equalsIgnoreCase("organizations")) {
-					if (_generatedDataUtil.containsOrganizationKey(name)) {
-						idArray[i] = _generatedDataUtil.getOrganization(name);
-					}
-					else {
-						Organization newOrganization =
-							_organizationLocalService.addOrganization(
-								_generatedDataUtil.getDefaultUserId(), 0, name,
-								false);
-
-						idArray[i] = newOrganization.getPrimaryKey();
-
-						_generatedDataUtil.putOrganization(name, idArray[i]);
-					}
-				}
-				else if (headerName.equalsIgnoreCase("userGroups")) {
-					if (_generatedDataUtil.containsUserGroupKey(name)) {
-						idArray[i] = _generatedDataUtil.getUserGroup(name);
-					}
-					else {
-						UserGroup newUserGroup =
-							_userGroupLocalService.addUserGroup(
-								_generatedDataUtil.getDefaultUserId(),
-								_companyId, name, null, null);
-
-						idArray[i] = newUserGroup.getPrimaryKey();
-
-						_generatedDataUtil.putUserGroup(name, idArray[i]);
-					}
-				}
-				else if (headerName.equalsIgnoreCase("roles")) {
-					if (_generatedDataUtil.containsRoleKey(name)) {
-						idArray[i] = _generatedDataUtil.getRole(name);
-					}
-					else {
-						Role newRole = _roleLocalService.addRole(
-							_generatedDataUtil.getDefaultUserId(), null, 0,
-							name, null, null, 0, null, null);
-
-						idArray[i] = newRole.getPrimaryKey();
-
-						_generatedDataUtil.putRole(name, idArray[i]);
-					}
-				}
-			}
-			catch (PortalException portalException) {
-				_log.error(portalException, portalException);
-			}
+		catch (PortalException portalException) {
+			_log.error(portalException, portalException);
 		}
-
-		if (!_verbose.equalsIgnoreCase("false")) {
-			System.out.printf(
-				"Added %s to %s with ID's: %s%n", csvRecord.get("emailAddress"),
-				headerName, Arrays.toString(idArray));
-		}
-
-		return idArray;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		AcCsvDataGenerator.class);
 
-	private long _companyId;
+	private AcCsvDataGeneratorConfiguration _acCsvDataGeneratorConfiguration;
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
 
 	@Reference
-	private CsvUser _csvUser;
-
-	@Reference
 	private GeneratedDataUtil _generatedDataUtil;
 
 	@Reference
-	private OrganizationLocalService _organizationLocalService;
-
-	@Reference
-	private RoleLocalService _roleLocalService;
-
-	@Reference
-	private UserGroupLocalService _userGroupLocalService;
-
-	private String _verbose;
+	private UserCsv _userCsv;
 
 }
---
+++ modules/test/ac-csv-data-generator/src/main/java/com/liferay/ac/csv/data/generator/csv/UserCsv.java
@@ -0,0 +1,233 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ac.csv.data.generator.csv;
+
+import com.liferay.ac.csv.data.generator.util.GeneratedDataUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.service.OrganizationLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserGroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.charset.Charset;
+
+import java.util.Arrays;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cheryl Tang
+ */
+@Component(immediate = true, service = UserCsv.class)
+public class UserCsv {
+
+	public void addCsvUsers(String csvFilePath) {
+		File userCsvFile = new File(csvFilePath);
+
+		CSVParser csvParser = _getCSVParser(userCsvFile);
+
+		for (CSVRecord csvRecord : csvParser) {
+			String emailAddress = csvRecord.get("emailAddress");
+
+			if (_generatedDataUtil.containsUserKey(emailAddress)) {
+				continue;
+			}
+
+			try {
+				User user = _addCsvUser(csvRecord);
+
+				_generatedDataUtil.putUser(emailAddress, user.getPrimaryKey());
+
+				if (_log.isInfoEnabled()) {
+					_log.info("Created user: " + emailAddress);
+				}
+			}
+			catch (PortalException portalException) {
+				_log.error(portalException, portalException);
+			}
+		}
+	}
+
+	private User _addCsvUser(CSVRecord csvRecord) throws PortalException {
+		String emailAddress = csvRecord.get("emailAddress");
+		String password = csvRecord.get("password");
+		String screenName = csvRecord.get("screenName");
+		String firstName = csvRecord.get("firstName");
+		String middleName = csvRecord.get("middleName");
+		String lastName = csvRecord.get("lastName");
+		String jobTitle = csvRecord.get("jobTitle");
+
+		String gender = csvRecord.get("gender");
+
+		boolean male = gender.equalsIgnoreCase("male");
+
+		int javaMonthOffset = 1; //java.util.Date months start at 0 (meaning 0 for January)
+
+		int birthdayMonth =
+			GetterUtil.getInteger(csvRecord.get("birthdayMonth")) -
+				javaMonthOffset;
+
+		int birthdayDay = GetterUtil.getInteger(csvRecord.get("birthdayDay"));
+
+		int birthdayYear = GetterUtil.getInteger(csvRecord.get("birthdayYear"));
+
+		long[] groupIds = null;
+		long[] organizationIds = _getIdArrayFromRowCell(
+			csvRecord, "organizations");
+		long[] roleIds = _getIdArrayFromRowCell(csvRecord, "roles");
+		long[] userGroupIds = _getIdArrayFromRowCell(csvRecord, "userGroups");
+
+		return _userLocalService.addUser(
+			_generatedDataUtil.getDefaultUserId(),
+			_generatedDataUtil.getCompanyId(), false, password, password, false,
+			screenName, emailAddress, 0, StringPool.BLANK,
+			LocaleUtil.getDefault(), firstName, middleName, lastName, 0, 0,
+			male, birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds,
+			organizationIds, roleIds, userGroupIds, false,
+			new ServiceContext());
+	}
+
+	private CSVParser _getCSVParser(File csvFile) {
+		CSVParser csvParser = null;
+
+		try {
+			csvParser = CSVParser.parse(
+				csvFile, Charset.defaultCharset(),
+				CSVFormat.DEFAULT.withFirstRecordAsHeader(
+				).withIgnoreSurroundingSpaces(
+				).withNullString(
+					""
+				));
+		}
+		catch (IOException ioException) {
+			_log.error(ioException, ioException);
+		}
+
+		return csvParser;
+	}
+
+	private long[] _getIdArrayFromRowCell(CSVRecord csvRecord, String header) {
+		String recordCell = csvRecord.get(header);
+
+		if (recordCell == null) {
+			return null;
+		}
+
+		String[] stringArray = recordCell.split(",");
+
+		long[] idArray = new long[stringArray.length];
+
+		for (int i = 0; i < stringArray.length; i++) {
+			String name = stringArray[i].trim();
+
+			try {
+				if (header.equalsIgnoreCase("organizations")) {
+					if (_generatedDataUtil.containsOrganizationKey(name)) {
+						idArray[i] = _generatedDataUtil.getOrganization(name);
+					}
+					else {
+						Organization newOrganization =
+							_organizationLocalService.addOrganization(
+								_generatedDataUtil.getDefaultUserId(), 0, name,
+								false);
+
+						idArray[i] = newOrganization.getPrimaryKey();
+
+						_generatedDataUtil.putOrganization(name, idArray[i]);
+					}
+				}
+				else if (header.equalsIgnoreCase("userGroups")) {
+					if (_generatedDataUtil.containsUserGroupKey(name)) {
+						idArray[i] = _generatedDataUtil.getUserGroup(name);
+					}
+					else {
+						UserGroup newUserGroup =
+							_userGroupLocalService.addUserGroup(
+								_generatedDataUtil.getDefaultUserId(),
+								_generatedDataUtil.getCompanyId(), name, null,
+								null);
+
+						idArray[i] = newUserGroup.getPrimaryKey();
+
+						_generatedDataUtil.putUserGroup(name, idArray[i]);
+					}
+				}
+				else if (header.equalsIgnoreCase("roles")) {
+					if (_generatedDataUtil.containsRoleKey(name)) {
+						idArray[i] = _generatedDataUtil.getRole(name);
+					}
+					else {
+						Role newRole = _roleLocalService.addRole(
+							_generatedDataUtil.getDefaultUserId(), null, 0,
+							name, null, null, 0, null, null);
+
+						idArray[i] = newRole.getPrimaryKey();
+
+						_generatedDataUtil.putRole(name, idArray[i]);
+					}
+				}
+			}
+			catch (PortalException portalException) {
+				_log.error(portalException, portalException);
+			}
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Added %s to %s with ID's: %s%n",
+					csvRecord.get("emailAddress"), header,
+					Arrays.toString(idArray)));
+		}
+
+		return idArray;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(UserCsv.class);
+
+	@Reference
+	private GeneratedDataUtil _generatedDataUtil;
+
+	@Reference
+	private OrganizationLocalService _organizationLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+	@Reference
+	private UserGroupLocalService _userGroupLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
