0cac53a33eaeb0f508ba3ff0c3b1a49f45f3d58d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-03-08 12:01:13 +0100

    LPS-110077 Disable multi page and autocomplete for document library

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/servlet/taglib/definition/DocumentLibraryDataLayoutBuilderDefinition.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.servlet.taglib.definition;
+
+import com.liferay.data.engine.taglib.servlet.taglib.definition.DataLayoutBuilderDefinition;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true, property = "content.type=document-library",
+	service = DataLayoutBuilderDefinition.class
+)
+public class DocumentLibraryDataLayoutBuilderDefinition
+	implements DataLayoutBuilderDefinition {
+
+	@Override
+	public String[] getDisabledTabs() {
+		return new String[] {"Autocomplete"};
+	}
+
+	@Override
+	public boolean isMultiPage() {
+		return false;
+	}
+
+}
