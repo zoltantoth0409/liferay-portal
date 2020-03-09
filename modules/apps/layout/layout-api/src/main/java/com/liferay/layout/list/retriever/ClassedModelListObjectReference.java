8eb91aaf4dca93793e7becf09780c57f09303535
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-03-09 18:19:43 +0100

    LPS-110090  Implements generic ListObjectReferences

---
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/list/retriever/ClassedModelListObjectReference.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.list.retriever;
+
+import com.liferay.portal.kernel.json.JSONObject;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class ClassedModelListObjectReference implements ListObjectReference {
+
+	public ClassedModelListObjectReference(JSONObject jsonObject) {
+		_classPK = jsonObject.getLong("classPK");
+		_className = jsonObject.getString("className");
+		_title = jsonObject.getString("title");
+	}
+
+	public String getClassName() {
+		return _className;
+	}
+
+	public long getClassPK() {
+		return _classPK;
+	}
+
+	public String getTitle() {
+		return _title;
+	}
+
+	private final String _className;
+	private final long _classPK;
+	private final String _title;
+
+}
---
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/list/retriever/KeyListObjectReference.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.list.retriever;
+
+import com.liferay.portal.kernel.json.JSONObject;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class KeyListObjectReference implements ListObjectReference {
+
+	public KeyListObjectReference(JSONObject jsonObject) {
+		_key = jsonObject.getString("key");
+		_title = jsonObject.getString("title");
+	}
+
+	public String getKey() {
+		return _key;
+	}
+
+	public String getTitle() {
+		return _title;
+	}
+
+	private final String _key;
+	private final String _title;
+
+}
