19b1d59031fe7110699976b80e310aca64883ef6
Adolfo Pérez <adolfo.perez@liferay.com>
2020-03-09 15:38:04 +0100

    LPS-109842 Move rotate capability to web module

--- modules/apps/frontend-image-editor/frontend-image-editor-web/src/main/java/com/liferay/frontend/image/editor/web/internal/capability/rotate/ImageEditorCapabilityRotate.java
+++ modules/apps/frontend-image-editor/frontend-image-editor-web/src/main/java/com/liferay/frontend/image/editor/web/internal/capability/rotate/ImageEditorCapabilityRotate.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.frontend.image.editor.capability.rotate.internal;
+package com.liferay.frontend.image.editor.web.internal.capability.rotate;
 
 import com.liferay.frontend.image.editor.capability.BaseImageEditorCapability;
 import com.liferay.frontend.image.editor.capability.ImageEditorCapability;
@@ -61,7 +61,7 @@ public class ImageEditorCapabilityRotate extends BaseImageEditorCapability {
 	}
 
 	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.frontend.image.editor.capability.rotate)"
+		target = "(osgi.web.symbolicname=com.liferay.frontend.image.editor.web)"
 	)
 	private ServletContext _servletContext;
 
