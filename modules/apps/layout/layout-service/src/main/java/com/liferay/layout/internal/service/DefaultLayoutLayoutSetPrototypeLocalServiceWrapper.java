51163c6aa8b97945d4871693c0a9cf19bdb20752
Pavel Savinov <pavel.savinov@liferay.com>
2020-03-10 16:36:05 +0100

    LPS-109350 Import default layout definition JSOB to the default content layout

--- modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/service/DefaultLayoutLayoutSetPrototypeLocalServiceWrapper.java
+++ modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/service/DefaultLayoutLayoutSetPrototypeLocalServiceWrapper.java
@@ -12,8 +12,12 @@
  * details.
  */
 
-package com.liferay.layout.service;
+package com.liferay.layout.internal.service;
 
+import com.liferay.layout.page.template.importer.LayoutPageTemplatesImporter;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
@@ -24,6 +28,9 @@ import com.liferay.portal.kernel.service.LayoutSetPrototypeLocalServiceWrapper;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceWrapper;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.segments.constants.SegmentsExperienceConstants;
 
 import java.util.Locale;
 import java.util.Map;
@@ -66,12 +73,47 @@ public class DefaultLayoutLayoutSetPrototypeLocalServiceWrapper
 				userId, layoutSetPrototype.getGroupId(), true,
 				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "Home", null, null,
 				LayoutConstants.TYPE_CONTENT, false, "/home", serviceContext);
+
+			LayoutPageTemplateStructure layoutPageTemplateStructure =
+				_layoutPageTemplateStructureLocalService.
+					fetchLayoutPageTemplateStructure(
+						defaultLayout.getGroupId(),
+						_portal.getClassNameId(Layout.class.getName()),
+						defaultLayout.getPlid(), true);
+
+			LayoutStructure layoutStructure = LayoutStructure.of(
+				layoutPageTemplateStructure.getData(
+					SegmentsExperienceConstants.ID_DEFAULT));
+
+			try {
+				_layoutPageTemplatesImporter.importPageElement(
+					defaultLayout, layoutStructure,
+					layoutStructure.getMainItemId(), _DEFAULT_LAYOUT_DEFINITION,
+					0);
+			}
+			catch (Exception exception) {
+				throw new PortalException(exception);
+			}
 		}
 
 		return layoutSetPrototype;
 	}
 
+	private static final String _DEFAULT_LAYOUT_DEFINITION = StringUtil.read(
+		DefaultLayoutLayoutSetPrototypeLocalServiceWrapper.class,
+		"default-layout-definition.json");
+
 	@Reference
 	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private LayoutPageTemplatesImporter _layoutPageTemplatesImporter;
+
+	@Reference
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
+	@Reference
+	private Portal _portal;
 
 }
