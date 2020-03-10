97f76f286c811c4d6c7324be3bd6f5d53785e12f
André de Oliveira <andre.oliveira@liferay.com>
2020-03-10 00:16:04 -0700

    LPS-100523 asset-test: Test Organization Tags with Organization fixture
    https://issues.liferay.com/browse/LPS-100523

--- modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/tags/search/test/AssetTagNamesOrganizationSearchTest.java
+++ modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/tags/search/test/AssetTagNamesOrganizationSearchTest.java
@@ -16,27 +16,28 @@ package com.liferay.asset.tags.search.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.OrganizationConstants;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.search.SortFactoryUtil;
-import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.kernel.service.OrganizationLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
 import com.liferay.portal.kernel.workflow.WorkflowThreadLocal;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.users.admin.test.util.search.OrganizationBlueprint;
-import com.liferay.users.admin.test.util.search.UserSearchFixture;
+import com.liferay.users.admin.test.util.search.OrganizationBlueprint.OrganizationBlueprintBuilder;
+import com.liferay.users.admin.test.util.search.OrganizationSearchFixture;
 
 import java.util.LinkedHashMap;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -62,17 +63,11 @@ public class AssetTagNamesOrganizationSearchTest {
 	public void setUp() throws Exception {
 		WorkflowThreadLocal.setEnabled(false);
 
-		_userSearchFixture = new UserSearchFixture();
+		organizationSearchFixture = new OrganizationSearchFixture(
+			organizationLocalService, assetTagLocalService);
 
-		_userSearchFixture.setUp();
-
-		_assetTags = _userSearchFixture.getAssetTags();
-		_organizations = _userSearchFixture.getOrganizations();
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		_userSearchFixture.tearDown();
+		_assetTags = organizationSearchFixture.getAssetTags();
+		_organizations = organizationSearchFixture.getOrganizations();
 	}
 
 	@Test
@@ -81,12 +76,13 @@ public class AssetTagNamesOrganizationSearchTest {
 
 		String[] newAssetTagNames = {tag};
 
-		Organization organization = _userSearchFixture.addOrganization(
-			new OrganizationBlueprint() {
-				{
-					assetTagNames = newAssetTagNames;
-				}
-			});
+		OrganizationBlueprintBuilder organizationBlueprintBuilder =
+			OrganizationSearchFixture.getTestOrganizationBlueprintBuilder();
+
+		Organization organization = organizationSearchFixture.addOrganization(
+			organizationBlueprintBuilder.assetTagNames(
+				newAssetTagNames
+			).build());
 
 		assertCount(organization.getCompanyId(), 1, tag);
 	}
@@ -103,7 +99,7 @@ public class AssetTagNamesOrganizationSearchTest {
 		Sort sort = SortFactoryUtil.getSort(Organization.class, "name", "desc");
 
 		BaseModelSearchResult<Organization> baseModelSearchResult =
-			OrganizationLocalServiceUtil.searchOrganizations(
+			organizationLocalService.searchOrganizations(
 				companyId, OrganizationConstants.ANY_PARENT_ORGANIZATION_ID,
 				keywords, organizationParams, QueryUtil.ALL_POS,
 				QueryUtil.ALL_POS, sort);
@@ -113,12 +109,18 @@ public class AssetTagNamesOrganizationSearchTest {
 			baseModelSearchResult.getLength());
 	}
 
+	@Inject
+	protected AssetTagLocalService assetTagLocalService;
+
+	@Inject
+	protected OrganizationLocalService organizationLocalService;
+
+	protected OrganizationSearchFixture organizationSearchFixture;
+
 	@DeleteAfterTestRun
 	private List<AssetTag> _assetTags;
 
 	@DeleteAfterTestRun
 	private List<Organization> _organizations;
-
-	private UserSearchFixture _userSearchFixture;
 
 }
