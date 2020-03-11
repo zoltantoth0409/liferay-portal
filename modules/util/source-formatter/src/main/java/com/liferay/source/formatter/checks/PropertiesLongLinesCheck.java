95a1df0bf15fbf271e169c9414ff46caf00ba1ef
Alan Huang <alan.huang@liferay.com>
2020-03-11 23:06:05 +0800

    LPS-84119 Remove redundant logic

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesLongLinesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesLongLinesCheck.java
@@ -14,9 +14,6 @@
 
 package com.liferay.source.formatter.checks;
 
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
-import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.IOException;
@@ -41,20 +38,6 @@ public class PropertiesLongLinesCheck extends BaseFileCheck {
 			return content;
 		}
 
-		try (UnsyncBufferedReader unsyncBufferedReader =
-				new UnsyncBufferedReader(new UnsyncStringReader(content))) {
-
-			int lineNumber = 0;
-
-			String line = null;
-
-			while ((line = unsyncBufferedReader.readLine()) != null) {
-				lineNumber++;
-
-				_checkMaxLineLength(line, fileName, lineNumber);
-			}
-		}
-
 		Matcher matcher = _commentsPattern.matcher(content);
 
 		while (matcher.find()) {
@@ -71,43 +54,6 @@ public class PropertiesLongLinesCheck extends BaseFileCheck {
 		}
 
 		return content;
-	}
-
-	private void _checkMaxLineLength(
-		String line, String fileName, int lineNumber) {
-
-		String trimmedLine = StringUtil.trimLeading(line);
-
-		if (!trimmedLine.startsWith("# ")) {
-			return;
-		}
-
-		if (trimmedLine.matches("# Env: \\w*")) {
-			return;
-		}
-
-		int lineLength = getLineLength(line);
-
-		if (lineLength <= getMaxLineLength()) {
-			return;
-		}
-
-		int x = line.indexOf("# ");
-		int y = line.lastIndexOf(StringPool.SPACE, getMaxLineLength());
-
-		if ((x + 1) == y) {
-			return;
-		}
-
-		int z = line.indexOf(StringPool.SPACE, getMaxLineLength() + 1);
-
-		if (z == -1) {
-			z = lineLength;
-		}
-
-		if ((z - y + x + 2) <= getMaxLineLength()) {
-			addMessage(fileName, "> " + getMaxLineLength(), lineNumber);
-		}
 	}
 
 	private String _splitComment(String comment) {
