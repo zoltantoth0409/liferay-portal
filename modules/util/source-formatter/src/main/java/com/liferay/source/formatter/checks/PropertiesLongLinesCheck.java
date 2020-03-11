a64334005e53a28e4f99ab8ce2afad2627f9df8b
Alan Huang <alan.huang@liferay.com>
2020-03-11 17:13:17 +0800

    LPS-84119 Don't check formatted comments

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesLongLinesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesLongLinesCheck.java
@@ -134,6 +134,6 @@ public class PropertiesLongLinesCheck extends BaseFileCheck {
 	}
 
 	private static final Pattern _multiLineCommentsPattern = Pattern.compile(
-		"(    (?!# Env: )# .+)(\n    # .+)+");
+		"(    (?!# Env: )# (?! ).+)(\n    # (?! ).+)+");
 
 }
