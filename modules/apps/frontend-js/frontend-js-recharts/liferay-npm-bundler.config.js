d6a0fa9f7cc2fe0882aeb108203064feb539ac74
Iván Zaera Avellón <izaera@gmx.es>
2020-03-11 11:45:17 +0100

    LPS-109921 Use bundler 3 in frontend-js-recharts

---
+++ modules/apps/frontend-js/frontend-js-recharts/liferay-npm-bundler.config.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+module.exports = {
+	'create-jar': false,
+	exports: {
+		recharts: 'recharts',
+		recharts_lib_index: 'recharts/lib/index.js',
+	},
+	imports: {
+		'frontend-js-node-shims': {
+			events: '^1.0.0',
+		},
+		'frontend-js-react-web': {
+			react: '^16.0.0',
+			'react-dom': '^16.0.0',
+		},
+	},
+	output: 'build/node/packageRunBuild/resources',
+	'work-dir': 'build/node/bundler',
+};
