7983b3547da140e2a26208e3ca00adc738acc119
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-03-11 20:31:30 +0100

    LPS-107761 Adds logic for reading a folder structure and generate a zip file from it so as to later import it

--- modules/apps/layout/layout-page-template-test/src/testIntegration/java/com/liferay/layout/page/template/internal/importer/test/LayoutPageTemplatesImporterTest.java
+++ modules/apps/layout/layout-page-template-test/src/testIntegration/java/com/liferay/layout/page/template/internal/importer/test/LayoutPageTemplatesImporterTest.java
@@ -15,14 +15,30 @@
 package com.liferay.layout.page.template.internal.importer.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateExportImportConstants;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.zip.ZipWriter;
+import com.liferay.portal.kernel.zip.ZipWriterFactoryUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.net.URL;
+
+import java.util.Enumeration;
 
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * @author Jürgen Kappler
@@ -39,6 +55,89 @@ public class LayoutPageTemplatesImporterTest {
 
 	@Before
 	public void setUp() throws Exception {
+		_bundle = FrameworkUtil.getBundle(getClass());
 	}
+
+	private void _addZipWriterEntry(ZipWriter zipWriter, URL url)
+		throws IOException {
+
+		String entryPath = url.getPath();
+
+		String zipPath = StringUtil.removeSubstring(
+			entryPath, _LAYOUT_PATE_TEMPLATES_PATH);
+
+		zipWriter.addEntry(zipPath, url.openStream());
+	}
+
+	private File _generateZipFile(String type) throws Exception {
+		ZipWriter zipWriter = ZipWriterFactoryUtil.getZipWriter();
+
+		StringBuilder sb = new StringBuilder(3);
+
+		sb.append(_LAYOUT_PATE_TEMPLATES_PATH + type);
+		sb.append(StringPool.FORWARD_SLASH + _ROOT_FOLDER);
+		sb.append(StringPool.FORWARD_SLASH);
+
+		Enumeration<URL> enumeration = _bundle.findEntries(
+			sb.toString(),
+			LayoutPageTemplateExportImportConstants.
+				FILE_NAME_PAGE_TEMPLATE_COLLECTION,
+			true);
+
+		try {
+			while (enumeration.hasMoreElements()) {
+				URL url = enumeration.nextElement();
+
+				_populateZipWriter(zipWriter, url);
+			}
+
+			zipWriter.finish();
+
+			return zipWriter.getFile();
+		}
+		catch (Exception exception) {
+			throw new Exception(exception);
+		}
+	}
+
+	private void _populateZipWriter(ZipWriter zipWriter, URL url)
+		throws IOException {
+
+		String zipPath = StringUtil.removeSubstring(
+			url.getFile(), _LAYOUT_PATE_TEMPLATES_PATH);
+
+		zipWriter.addEntry(zipPath, url.openStream());
+
+		String path = FileUtil.getPath(url.getPath());
+
+		Enumeration<URL> enumeration = _bundle.findEntries(
+			path,
+			LayoutPageTemplateExportImportConstants.FILE_NAME_PAGE_TEMPLATE,
+			true);
+
+		while (enumeration.hasMoreElements()) {
+			URL elementUrl = enumeration.nextElement();
+
+			_addZipWriterEntry(zipWriter, elementUrl);
+		}
+
+		enumeration = _bundle.findEntries(
+			path,
+			LayoutPageTemplateExportImportConstants.FILE_NAME_PAGE_DEFINITION,
+			true);
+
+		while (enumeration.hasMoreElements()) {
+			URL elementUrl = enumeration.nextElement();
+
+			_addZipWriterEntry(zipWriter, elementUrl);
+		}
+	}
+
+	private static final String _LAYOUT_PATE_TEMPLATES_PATH =
+		"com/liferay/layout/page/template/internal/importer/test/dependencies/";
+
+	private static final String _ROOT_FOLDER = "page-templates";
+
+	private Bundle _bundle;
 
 }
