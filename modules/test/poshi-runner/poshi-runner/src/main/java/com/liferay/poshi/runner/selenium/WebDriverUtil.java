106e72aae2cffc036ee57d06893a4e9e5c5d19ec
Hugo Huijser <hugo.huijser@liferay.com>
2020-03-12 11:31:40 -0700

    LPS-109946 Remove unused methods

--- modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/WebDriverUtil.java
+++ modules/test/poshi-runner/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/WebDriverUtil.java
@@ -14,7 +14,6 @@
 
 package com.liferay.poshi.runner.selenium;
 
-import com.liferay.poshi.runner.util.FileUtil;
 import com.liferay.poshi.runner.util.OSDetector;
 import com.liferay.poshi.runner.util.PropsValues;
 import com.liferay.poshi.runner.util.StringPool;
@@ -32,12 +31,6 @@ import java.net.URL;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -101,52 +94,8 @@ public class WebDriverUtil extends PropsValues {
 		}
 	}
 
-	public static String getCSSSource(String htmlSource) throws Exception {
-		Document htmlDocument = Jsoup.parse(htmlSource);
-
-		Elements elements = htmlDocument.select("link[type=text/css]");
-
-		StringBuilder sb = new StringBuilder();
-
-		for (Element element : elements) {
-			String href = element.attr("href");
-
-			if (!href.contains(PropsValues.PORTAL_URL)) {
-				href = PropsValues.PORTAL_URL + href;
-			}
-
-			Connection connection = Jsoup.connect(href);
-
-			Document document = connection.get();
-
-			sb.append(document.text());
-
-			sb.append("\n");
-		}
-
-		return sb.toString();
-	}
-
 	public static WebDriver getWebDriver() {
 		return _webDriverUtil._getWebDriver();
-	}
-
-	public static void saveWebPage(String fileName, String htmlSource)
-		throws Exception {
-
-		if (!PropsValues.SAVE_WEB_PAGE) {
-			return;
-		}
-
-		StringBuilder sb = new StringBuilder(3);
-
-		sb.append("<style>");
-		sb.append(getCSSSource(htmlSource));
-		sb.append("</style></html>");
-
-		FileUtil.write(
-			fileName,
-			StringUtil.replace(htmlSource, "<\\html>", sb.toString()));
 	}
 
 	public static void startWebDriver() {
