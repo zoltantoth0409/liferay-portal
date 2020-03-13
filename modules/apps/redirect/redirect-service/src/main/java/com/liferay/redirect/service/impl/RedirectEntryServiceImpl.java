ab8b513a591c4760dea1798b1ee3be7409586c00
Brian Chan <brian.chan@liferay.com>
2020-03-13 15:38:36 -0700

    LPS-110195 SF

--- modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/service/impl/RedirectEntryServiceImpl.java
+++ modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/service/impl/RedirectEntryServiceImpl.java
@@ -36,17 +36,7 @@ import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
- * The implementation of the redirect entry remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.redirect.service.RedirectEntryService</code> interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see RedirectEntryServiceBaseImpl
  */
 @Component(
 	property = {
