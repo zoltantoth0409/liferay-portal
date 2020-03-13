e4a5cd6fa92d1f71c8c3205490f8a0e3216aea5d
drewbrokke <drew.brokke@liferay.com>
2020-03-13 13:00:54 -0500

    LPS-107151 segments-service - adds method to SegmentsEntryImpl to get the assigned role ids

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/model/impl/SegmentsEntryImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/model/impl/SegmentsEntryImpl.java
@@ -14,9 +14,14 @@
 
 package com.liferay.segments.model.impl;
 
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.segments.criteria.Criteria;
 import com.liferay.segments.criteria.CriteriaSerializer;
+import com.liferay.segments.model.SegmentsEntryRole;
+import com.liferay.segments.service.SegmentsEntryRoleLocalServiceUtil;
+
+import java.util.List;
 
 /**
  * @author Eduardo García
@@ -33,6 +38,16 @@ public class SegmentsEntryImpl extends SegmentsEntryBaseImpl {
 		}
 
 		return _criteria;
+	}
+
+	@Override
+	public long[] getRoleIds() {
+		List<SegmentsEntryRole> segmentsEntryRoles =
+			SegmentsEntryRoleLocalServiceUtil.getSegmentsEntryRoles(
+				getSegmentsEntryId());
+
+		return ListUtil.toLongArray(
+			segmentsEntryRoles, SegmentsEntryRole::getRoleId);
 	}
 
 	private Criteria _criteria;
