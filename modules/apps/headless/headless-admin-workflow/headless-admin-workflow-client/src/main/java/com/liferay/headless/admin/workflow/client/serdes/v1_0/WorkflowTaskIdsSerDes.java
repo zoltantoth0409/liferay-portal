5f446fcd757578048a632a15fa83df88392a13cf
Brian Chan <brian.chan@liferay.com>
2020-03-13 11:20:52 -0700

    LPS-108246 regen

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-client/src/main/java/com/liferay/headless/admin/workflow/client/serdes/v1_0/WorkflowTaskIdsSerDes.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-client/src/main/java/com/liferay/headless/admin/workflow/client/serdes/v1_0/WorkflowTaskIdsSerDes.java
@@ -190,10 +190,13 @@ public class WorkflowTaskIdsSerDes {
 
 				sb.append("]");
 			}
-			else {
+			else if (value instanceof String) {
 				sb.append("\"");
 				sb.append(_escape(entry.getValue()));
 				sb.append("\"");
+			}
+			else {
+				sb.append(String.valueOf(entry.getValue()));
 			}
 
 			if (iterator.hasNext()) {
