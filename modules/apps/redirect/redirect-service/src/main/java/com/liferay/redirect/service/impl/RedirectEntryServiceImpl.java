c537b71efe8bf90d96421441a1d53800300ef9ec
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-03-13 12:14:11 +0100

    LPS-110195 Implementing remote service methods

--- modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/service/impl/RedirectEntryServiceImpl.java
+++ modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/service/impl/RedirectEntryServiceImpl.java
@@ -15,9 +15,25 @@
 package com.liferay.redirect.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.redirect.constants.RedirectConstants;
+import com.liferay.redirect.model.RedirectEntry;
 import com.liferay.redirect.service.base.RedirectEntryServiceBaseImpl;
 
+import java.util.Date;
+import java.util.List;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * The implementation of the redirect entry remote service.
@@ -41,9 +57,109 @@ import org.osgi.service.component.annotations.Component;
 )
 public class RedirectEntryServiceImpl extends RedirectEntryServiceBaseImpl {
 
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use <code>com.liferay.redirect.service.RedirectEntryServiceUtil</code> to access the redirect entry remote service.
-	 */
+	@Override
+	public RedirectEntry addRedirectEntry(
+			long groupId, String destinationURL, Date expirationDate,
+			boolean permanent, String sourceURL, ServiceContext serviceContext)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId, ActionKeys.ADD_ENTRY);
+
+		return redirectEntryLocalService.addRedirectEntry(
+			groupId, destinationURL, expirationDate, permanent, sourceURL,
+			serviceContext);
+	}
+
+	@Override
+	public RedirectEntry deleteRedirectEntry(long redirectEntryId)
+		throws PortalException {
+
+		_redirectEntryModelResourcePermission.check(
+			getPermissionChecker(), redirectEntryId, ActionKeys.DELETE);
+
+		return redirectEntryLocalService.deleteRedirectEntry(redirectEntryId);
+	}
+
+	@Override
+	public RedirectEntry fetchRedirectEntry(long redirectEntryId)
+		throws PortalException {
+
+		RedirectEntry redirectEntry =
+			redirectEntryLocalService.fetchRedirectEntry(redirectEntryId);
+
+		if (redirectEntry != null) {
+			_redirectEntryModelResourcePermission.check(
+				getPermissionChecker(), redirectEntry, ActionKeys.VIEW);
+		}
+
+		return redirectEntry;
+	}
+
+	@Override
+	public List<RedirectEntry> getRedirectEntries(
+			long groupId, int start, int end,
+			OrderByComparator<RedirectEntry> obc)
+		throws PortalException {
+
+		PermissionChecker permissionChecker = getPermissionChecker();
+
+		if (!permissionChecker.hasPermission(
+				groupId, RedirectEntry.class.getName(),
+				RedirectEntry.class.getName(), ActionKeys.VIEW)) {
+
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, RedirectEntry.class.getName(),
+				RedirectEntry.class.getName(), ActionKeys.VIEW);
+		}
+
+		return redirectEntryLocalService.getRedirectEntries(
+			groupId, start, end, obc);
+	}
+
+	@Override
+	public int getRedirectEntriesCount(long groupId) throws PortalException {
+		PermissionChecker permissionChecker = getPermissionChecker();
+
+		if (!permissionChecker.hasPermission(
+				groupId, RedirectEntry.class.getName(),
+				RedirectEntry.class.getName(), ActionKeys.VIEW)) {
+
+			throw new PrincipalException.MustHavePermission(
+				permissionChecker, RedirectEntry.class.getName(),
+				RedirectEntry.class.getName(), ActionKeys.VIEW);
+		}
+
+		return redirectEntryLocalService.getRedirectEntriesCount(groupId);
+	}
+
+	@Override
+	public RedirectEntry updateRedirectEntry(
+			long redirectEntryId, String destinationURL, Date expirationDate,
+			boolean permanent, String sourceURL)
+		throws PortalException {
+
+		_redirectEntryModelResourcePermission.check(
+			getPermissionChecker(), redirectEntryId, ActionKeys.UPDATE);
+
+		return redirectEntryLocalService.updateRedirectEntry(
+			redirectEntryId, destinationURL, expirationDate, permanent,
+			sourceURL);
+	}
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(resource.name=" + RedirectConstants.RESOURCE_NAME + ")"
+	)
+	private volatile PortletResourcePermission _portletResourcePermission;
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(model.class.name=com.liferay.redirect.model.RedirectEntry)"
+	)
+	private volatile ModelResourcePermission<RedirectEntry>
+		_redirectEntryModelResourcePermission;
+
 }
