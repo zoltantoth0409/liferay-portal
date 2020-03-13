20382133fc7bdd8b5ba893fe54b09324168b95f4
Pavel Savinov <pavel.savinov@liferay.com>
2020-03-13 11:43:33 +0100

    LPS-110080 MVC action to move fragment compostion

---
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/MoveFragmentCompositionMVCActionCommand.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.portlet.action;
+
+import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.fragment.service.FragmentCompositionService;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + FragmentPortletKeys.FRAGMENT,
+		"mvc.command.name=/fragment/move_fragment_composition"
+	},
+	service = MVCActionCommand.class
+)
+public class MoveFragmentCompositionMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long fragmentCompositionId = ParamUtil.getLong(
+			actionRequest, "fragmentCompositionId");
+
+		long fragmentCollectionId = ParamUtil.getLong(
+			actionRequest, "fragmentCollectionId");
+
+		_fragmentCompositionService.moveFragmentComposition(
+			fragmentCompositionId, fragmentCollectionId);
+
+		LiferayPortletResponse liferayPortletResponse =
+			_portal.getLiferayPortletResponse(actionResponse);
+
+		PortletURL redirectURL = liferayPortletResponse.createRenderURL();
+
+		redirectURL.setParameter(
+			"fragmentCollectionId", String.valueOf(fragmentCollectionId));
+
+		sendRedirect(actionRequest, actionResponse, redirectURL.toString());
+	}
+
+	@Reference
+	private FragmentCompositionService _fragmentCompositionService;
+
+	@Reference
+	private Portal _portal;
+
+}
