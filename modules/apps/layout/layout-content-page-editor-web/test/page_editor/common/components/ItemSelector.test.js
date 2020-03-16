b162b6ad1a63c4817b44ffec54fc4050373dfd66
Sandro Chinea <sandrochinea@gmail.com>
2020-03-16 10:09:08 +0000

    LPS-110340 Added test file for ItemSelector component

---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/common/components/ItemSelector.test.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+
+import {StoreAPIContextProvider} from '../../../../src/main/resources/META-INF/resources/page_editor/app/store/index';
+import ItemSelector from '../../../../src/main/resources/META-INF/resources/page_editor/common/components/ItemSelector';
+
+jest.mock(
+	'../../../../src/main/resources/META-INF/resources/page_editor/app/config',
+	() => ({
+		config: {
+			infoItemSelectorUrl: 'infoItemSelectorUrl',
+			portletNamespace: 'portletNamespace',
+		},
+	})
+);
+
+function renderItemSelector() {
+	const state = {
+		mappedInfoItems: [],
+	};
+
+	return render(
+		<StoreAPIContextProvider dispatch={() => {}} getState={() => state}>
+			<ItemSelector label="itemSelectorLabel" onItemSelect={() => {}} />
+		</StoreAPIContextProvider>
+	);
+}
+
+describe('ItemSelector', () => {
+	afterEach(() => {
+		cleanup();
+	});
+
+	it('renders correctly', () => {
+		const {getByText} = renderItemSelector();
+
+		expect(getByText('itemSelectorLabel')).toBeInTheDocument();
+	});
+});
