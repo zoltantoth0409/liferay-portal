9aa14944d5635ff2c2103e2a2d408f6eeaf7ce36
Hugo Huijser <hugo.huijser@liferay.com>
2020-03-17 14:22:18 -0700

    LPS-108394 Use *ItemListBuilder

--- modules/apps/frontend-taglib/frontend-taglib-clay-sample-web/src/main/java/com/liferay/frontend/taglib/clay/sample/web/internal/display/context/CardsDisplayContext.java
+++ modules/apps/frontend-taglib/frontend-taglib-clay-sample-web/src/main/java/com/liferay/frontend/taglib/clay/sample/web/internal/display/context/CardsDisplayContext.java
@@ -17,7 +17,7 @@ package com.liferay.frontend.taglib.clay.sample.web.internal.display.context;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItem;
-import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItemList;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItemListBuilder;
 import com.liferay.portal.kernel.security.RandomUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 
@@ -51,35 +51,22 @@ public class CardsDisplayContext {
 	}
 
 	public List<LabelItem> getLabelItems() {
-		return new LabelItemList() {
-			{
-				int numItems = 1 + RandomUtil.nextInt(3);
+		int numItems = 1 + RandomUtil.nextInt(3);
 
-				LabelItem labelItem1 = new LabelItem();
-
-				labelItem1.setLabel("Approved");
-				labelItem1.setStyle("success");
-
-				add(labelItem1);
-
-				if (numItems > 1) {
-					LabelItem labelItem2 = new LabelItem();
-
-					labelItem2.setLabel("Pending");
-
-					add(labelItem2);
-				}
-
-				if (numItems > 2) {
-					LabelItem labelItem3 = new LabelItem();
-
-					labelItem3.setLabel("Canceled");
-					labelItem3.setStyle("danger");
-
-					add(labelItem3);
-				}
+		return LabelItemListBuilder.add(
+			labelItem -> {
+				labelItem.setLabel("Approved");
+				labelItem.setStyle("success");
 			}
-		};
+		).add(
+			() -> numItems > 1, labelItem -> labelItem.setLabel("Pending")
+		).add(
+			() -> numItems > 2,
+			labelItem -> {
+				labelItem.setLabel("Canceled");
+				labelItem.setStyle("danger");
+			}
+		).build();
 	}
 
 	public Map<String, String> getLabelStylesMap() {
