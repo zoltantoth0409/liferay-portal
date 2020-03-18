ba2078a09326176a3186dad12d75d36e8fdfd4b9
Yang Cao <yang.cao@liferay.com>
2020-03-19 06:53:28 +0800

    POSHI-57 Update script to make it robust

--- modules/test/poshi-runner/poshi-runner/src/main/resources/com/liferay/poshi/runner/dependencies/simulate_drag_and_drop.js
+++ modules/test/poshi-runner/poshi-runner/src/main/resources/com/liferay/poshi/runner/dependencies/simulate_drag_and_drop.js
@@ -10,7 +10,7 @@ Simulate = {
 				{
 					bubbles: true,
 					clientX: rect.left + (rect.width / 2),
-					clientY: rect.top + (rect.top / 2),
+					clientY: rect.top + (rect.height / 2),
 					dataTransfer: dataTransfer,
 					relatedTarget: element
 				});
