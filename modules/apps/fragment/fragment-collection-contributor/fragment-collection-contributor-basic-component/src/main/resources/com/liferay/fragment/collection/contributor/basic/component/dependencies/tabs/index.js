3d55ed5472e109fb20bce82b59982f01ef3c3a9e
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2020-03-19 11:43:51 +0100

    LPS-109298 Improving accessibility and adapting the code for ie11

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-basic-component/src/main/resources/com/liferay/fragment/collection/contributor/basic/component/dependencies/tabs/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-basic-component/src/main/resources/com/liferay/fragment/collection/contributor/basic/component/dependencies/tabs/index.js
@@ -1,72 +1,102 @@
 /*eslint-disable*/
-const tabItems = Array.from(fragmentElement.querySelectorAll('.nav-link'));
-const tabContentItems = Array.from(fragmentElement.querySelectorAll('.tab-content-item'));
-const dropdownButton = fragmentElement.querySelector('.navbar-toggler-link');
 const dropdown = fragmentElement.querySelector('.navbar-collapse');
+const dropdownButton = fragmentElement.querySelector('.navbar-toggler-link');
+const tabItems = [].slice.call(fragmentElement.querySelectorAll('.nav-link'));
+const tabPanelItems = [].slice.call(
+	fragmentElement.querySelectorAll('.tab-panel-item')
+);
 
-const activeTab = item => {
-	tabItems.forEach(tabItem => tabItem.classList.remove('active'));
+function activeTab(item) {
+	tabItems.forEach(function(tabItem) {
+		tabItem.setAttribute('aria-selected', false);
+		tabItem.classList.remove('active');
+	});
+	item.setAttribute('aria-selected', true);
 	item.classList.add('active');
-};
-
-const activeContentTab = item => {
-	tabContentItems.forEach(
-		tabContentItem => !tabContentItem.classList.contains('d-none') && tabContentItem.classList.add('d-none')
-	);
-	item.classList.remove('d-none');
-};
-
-const handleDropdown = item => {
-	if (dropdown.classList.contains('show')) {
-        dropdown.classList.remove('show');
-        if (item) {
-            handleDropdownButtonName(item);
-        }
-	} else {
-		dropdown.classList.add('show');
-	}
-};
-
-const handleDropdownButtonName = (item) => {
-    dropdownButton.querySelector('.navbar-text-truncate').innerHTML = item.querySelector('lfr-editable').textContent;
 }
 
-const openTab = (event, i) => {
+function activeTabPanel(item) {
+	tabPanelItems.forEach(function(tabPanelItem) {
+		if (!tabPanelItem.classList.contains('d-none')) {
+			tabPanelItem.classList.add('d-none');
+		}
+	});
+	item.classList.remove('d-none');
+}
+
+function handleDropdown({event, item}) {
+	event.preventDefault();
+	dropdown.classList.toggle('show');
+
+	const ariaExpanded = dropdownButton.getAttribute('aria-expanded');
+	dropdownButton.setAttribute(
+		'aria-expanded',
+		ariaExpanded === 'false' ? true : false
+	);
+
+	if (item) {
+		handleDropdownButtonName(item);
+	}
+}
+
+function handleDropdownButtonName(item) {
+	dropdownButton.querySelector(
+		'.navbar-text-truncate'
+	).innerHTML = item.querySelector('lfr-editable').textContent;
+}
+
+function openTabPanel(event, i) {
 	const {currentTarget, target} = event;
-	const isEditable = currentTarget.classList.contains('nav-link') &&
-		(target.classList.contains('page-editor__editable') || target.classList.contains('cke_editable'));
+	const isEditable =
+		target.classList.contains('page-editor__editable') ||
+		target.parentElement.classList.contains('page-editor__editable');
 
 	if (!isEditable) {
-		handleDropdown(currentTarget);
+		handleDropdown({event, item: currentTarget});
 	}
 
 	activeTab(currentTarget, i);
-	activeContentTab(tabContentItems[i]);
+	activeTabPanel(tabPanelItems[i]);
 
-	this.count = i;
-};
+	this.tabIndex = i;
+}
 
-const main = () => {
-    const notInitialState = this.count && this.count < tabItems.length;
-    dropdownButton.addEventListener('click', () => handleDropdown());
-    
-    notInitialState
-        ? handleDropdownButtonName(tabItems[this.count])
-        : handleDropdownButtonName(tabItems[0]);
+function main() {
+	const initialState = !this.tabIndex || this.tabIndex >= tabItems.length;
+	let tabItemSelected = tabItems[0];
 
-	tabItems.forEach((item, i) => {
-		notInitialState
-			? activeTab(tabItems[this.count])
-			: !i && activeTab(item);
+	if (initialState) {
+		tabItems.forEach(function(item, i) {
+			if (!i) {
+				activeTab(item);
+			}
+			item.addEventListener('click', function(event) {
+				openTabPanel(event, i);
+			});
+		});
+		tabPanelItems.forEach(function(item, i) {
+			if (!i) {
+				activeTabPanel(item);
+			}
+		});
+	}
+	else {
+		tabItemSelected = tabItems[this.tabIndex];
+		tabItems.forEach(function(item, i) {
+			activeTab(tabItems[this.tabIndex]);
+			item.addEventListener('click', function(event) {
+				openTabPanel(event, i);
+			});
+		});
+		tabPanelItems.forEach(function() {
+			activeTabPanel(tabPanelItems[this.tabIndex]);
+		});
+	}
 
-		item.addEventListener('click', event => openTab(event, i));
+	dropdownButton.addEventListener('click', function(event) {
+		handleDropdown({event});
 	});
-
-	tabContentItems.forEach((item, i) =>
-		notInitialState
-			? activeContentTab(tabContentItems[this.count])
-			: !i && activeContentTab(item)
-	);
-};
+	handleDropdownButtonName(tabItemSelected);
+}
 
 main();
