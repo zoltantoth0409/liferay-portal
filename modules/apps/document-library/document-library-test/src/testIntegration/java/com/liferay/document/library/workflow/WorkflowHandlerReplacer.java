de7d0f7cbfc0de12911d0306f6e8d3f70f982e2a
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-03-20 11:15:36 +0100

    LPS-109118 Fix way to replace by setting a higher ranking service.

--- modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/workflow/WorkflowHandlerReplacer.java
+++ modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/workflow/WorkflowHandlerReplacer.java
@@ -14,8 +14,13 @@
 
 package com.liferay.document.library.workflow;
 
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.workflow.WorkflowHandler;
-import com.liferay.portal.kernel.workflow.WorkflowHandlerRegistryUtil;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceRegistration;
+
+import java.util.Map;
 
 /**
  * @author Adolfo Pérez
@@ -25,23 +30,22 @@ public class WorkflowHandlerReplacer<T> implements AutoCloseable {
 	public WorkflowHandlerReplacer(
 		String className, WorkflowHandler<T> replacementWorkflowHandler) {
 
-		_replacementWorkflowHandler = replacementWorkflowHandler;
+		Registry registry = RegistryUtil.getRegistry();
 
-		_originalWorkflowHandler =
-			WorkflowHandlerRegistryUtil.getWorkflowHandler(className);
+		Map<String, Object> properties = HashMapBuilder.<String, Object>put(
+			"service.ranking", Integer.MAX_VALUE
+		).build();
 
-		WorkflowHandlerRegistryUtil.unregister(_originalWorkflowHandler);
-
-		WorkflowHandlerRegistryUtil.register(_replacementWorkflowHandler);
+		_serviceRegistration = registry.registerService(
+			(Class<WorkflowHandler<?>>)(Class<?>)WorkflowHandler.class,
+			replacementWorkflowHandler, properties);
 	}
 
 	@Override
 	public void close() throws Exception {
-		WorkflowHandlerRegistryUtil.unregister(_replacementWorkflowHandler);
-		WorkflowHandlerRegistryUtil.register(_originalWorkflowHandler);
+		_serviceRegistration.unregister();
 	}
 
-	private final WorkflowHandler<T> _originalWorkflowHandler;
-	private final WorkflowHandler<T> _replacementWorkflowHandler;
+	private final ServiceRegistration<WorkflowHandler<?>> _serviceRegistration;
 
 }
