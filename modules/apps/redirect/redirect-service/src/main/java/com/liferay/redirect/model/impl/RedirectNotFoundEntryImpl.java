7f95378a83ab76cf9453077a6124834edd828c83
Brian Chan <brian.chan@liferay.com>
2020-03-23 10:20:07 -0700

    LPS-110534 SF

--- modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/model/impl/RedirectNotFoundEntryImpl.java
+++ modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/model/impl/RedirectNotFoundEntryImpl.java
@@ -15,21 +15,10 @@
 package com.liferay.redirect.model.impl;
 
 /**
- * The extended model implementation for the RedirectNotFoundEntry service. Represents a row in the &quot;RedirectNotFoundEntry&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.redirect.model.RedirectNotFoundEntry</code> interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 public class RedirectNotFoundEntryImpl extends RedirectNotFoundEntryBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a redirect not found entry model instance should use the {@link com.liferay.redirect.model.RedirectNotFoundEntry} interface instead.
-	 */
 	public RedirectNotFoundEntryImpl() {
 	}
 
--- modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/service/impl/RedirectNotFoundEntryLocalServiceImpl.java
+++ modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/service/impl/RedirectNotFoundEntryLocalServiceImpl.java
@@ -22,17 +22,7 @@ import com.liferay.redirect.service.base.RedirectNotFoundEntryLocalServiceBaseIm
 import org.osgi.service.component.annotations.Component;
 
 /**
- * The implementation of the redirect not found entry local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.redirect.service.RedirectNotFoundEntryLocalService</code> interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Brian Wing Shun Chan
- * @see RedirectNotFoundEntryLocalServiceBaseImpl
  */
 @Component(
 	property = "model.class.name=com.liferay.redirect.model.RedirectNotFoundEntry",
@@ -54,7 +44,6 @@ public class RedirectNotFoundEntryLocalServiceImpl
 				counterLocalService.increment());
 
 			redirectNotFoundEntry.setGroupId(group.getGroupId());
-
 			redirectNotFoundEntry.setCompanyId(group.getCompanyId());
 			redirectNotFoundEntry.setUrl(url);
 
