73224904ce80df2f3875cb3a21ee5c6523797b4b
hai.yu <8219754@liferay.com>
2020-03-24 00:38:07 +0800

    LPS-110758 Deprecate API of MPI, remove impls and tests

--- portal-kernel/src/com/liferay/portal/kernel/resiliency/mpi/MPI.java
+++ portal-kernel/src/com/liferay/portal/kernel/resiliency/mpi/MPI.java
@@ -19,7 +19,9 @@ import java.rmi.RemoteException;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of Athanasius (7.3.x), with no direct replacement
  */
+@Deprecated
 public interface MPI extends Remote {
 
 	public boolean isAlive() throws RemoteException;
