10843768049098279dcf63361e6c286f2bf09640
Diego Nascimento <diego.nascimento@liferay.com>
2020-03-23 18:26:39 -0300

    LPS-110541 Use new adapter pattern for ImagePicker

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/ImagePicker/ImagePicker.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/ImagePicker/ImagePicker.es.js
@@ -12,160 +12,275 @@
  * details.
  */
 
-import '../FieldBase/FieldBase.es';
-
-import './ImagePickerAdapter.soy';
-
 import './ImagePickerRegister.soy';
 
-import './ReactImagePickerAdapter.es';
+import ClayButton from '@clayui/button';
+import ClayForm, {ClayInput} from '@clayui/form';
+import ClayIcon from '@clayui/icon';
+import ClayModal, {useModal} from '@clayui/modal';
+import {ItemSelectorDialog, cancelDebounce, debounce} from 'frontend-js-web';
+import React, {useEffect, useRef, useState} from 'react';
 
-import Component from 'metal-component';
-import Soy from 'metal-soy';
-import {Config} from 'metal-state';
+import getConnectedReactComponentAdapter from '../util/ReactComponentAdapter.es';
+import {connectStore} from '../util/connectStore.es';
+import templates from './ImagePickerAdapter.soy';
 
-import templates from './ImagePicker.soy';
+const useDebounceCallback = (callback, milliseconds) => {
+	const callbackRef = useRef(debounce(callback, milliseconds));
 
-class ImagePicker extends Component {
-	dispatchEvent(event, name, value) {
-		this.emit(name, {
-			fieldInstance: this,
-			originalEvent: event,
-			value,
-		});
-	}
-
-	_handleOnDispatch(event) {
-		switch (event.type) {
-			case 'value':
-				this.dispatchEvent(event, 'fieldEdited', event.payload);
-				break;
-			default:
-				console.error(new TypeError(`There is no type ${event.type}`));
-				break;
-		}
-	}
-}
-
-ImagePicker.STATE = {
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(string|undefined)}
-	 */
-
-	errorMessage: Config.string(),
-
-	/**
-	 * @default false
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?bool}
-	 */
-
-	evaluable: Config.bool().value(false),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(string|undefined)}
-	 */
-
-	fieldName: Config.string(),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(string|undefined)}
-	 */
-
-	label: Config.string(),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(string|undefined)}
-	 */
-
-	name: Config.string().required(),
-
-	/**
-	 * @default '000000'
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(string|undefined)}
-	 */
-
-	predefinedValue: Config.string(),
-
-	/**
-	 * @default false
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?bool}
-	 */
-
-	readOnly: Config.bool().value(false),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof FieldBase
-	 * @type {?(bool|undefined)}
-	 */
-
-	repeatable: Config.bool().value(false),
-
-	/**
-	 * @default false
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(bool|undefined)}
-	 */
-
-	required: Config.bool().value(false),
-
-	/**
-	 * @default true
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(bool|undefined)}
-	 */
-
-	showLabel: Config.bool().value(true),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(string|undefined)}
-	 */
-
-	spritemap: Config.string(),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(string|undefined)}
-	 */
-
-	tip: Config.string(),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof ImagePicker
-	 * @type {?(string|undefined)}
-	 */
-
-	value: Config.string(),
+	return [callbackRef.current, () => cancelDebounce(callbackRef.current)];
 };
 
-Soy.register(ImagePicker, templates);
-export {ImagePicker};
-export default ImagePicker;
+const ImagePicker = ({
+	id,
+	inputValue = '',
+	itemSelectorURL,
+	name,
+	onClearClick,
+	onDescriptionChange,
+	onFieldChanged,
+	portletNamespace,
+	readOnly,
+}) => {
+	const [imageValues, setImageValues] = useState({});
+	const [modalVisible, setModalVisible] = useState(false);
+
+	useEffect(() => {
+		setImageValues({
+			...{description: '', title: '', url: ''},
+			...JSON.parse(inputValue || '{}'),
+		});
+	}, [inputValue]);
+
+	const {observer} = useModal({
+		onClose: () => setModalVisible(false),
+	});
+
+	const _dispatchValue = ({clear, value}, callback) => {
+		setImageValues(oldValues => {
+			let mergedValues = {...oldValues, ...value};
+
+			mergedValues = clear ? {} : mergedValues;
+
+			if (callback) {
+				callback(mergedValues);
+			}
+
+			return mergedValues;
+		});
+	};
+
+	const _handleClearClick = event => {
+		_dispatchValue(
+			{clear: true, value: {description: '', event, title: '', url: ''}},
+			mergedValues => {
+				onClearClick(mergedValues);
+			}
+		);
+	};
+
+	const [debouncedTest] = useDebounceCallback(({event, value}) => {
+		_dispatchValue({value: {description: value, event}}, mergedValues => {
+			onDescriptionChange(mergedValues);
+		});
+	}, 500);
+
+	const _handleDescriptionChange = ({event, target: {value}}) =>
+		debouncedTest({event, value});
+
+	const _handleFieldChanged = event => {
+		const selectedItem = event.selectedItem;
+
+		if (selectedItem && selectedItem.value) {
+			const img = new Image();
+			const item = JSON.parse(selectedItem.value);
+			img.addEventListener('load', event => {
+				const {
+					target: {height, width},
+				} = event;
+
+				const imageData = {
+					...{
+						description: '',
+						event,
+						height,
+						title: '',
+						url: '',
+						width,
+					},
+					...item,
+				};
+
+				_dispatchValue({value: imageData}, mergedValues => {
+					onFieldChanged(mergedValues);
+				});
+			});
+			img.src = item.url;
+		}
+	};
+
+	const _handleItemSelectorTriggerClick = event => {
+		event.preventDefault();
+
+		const itemSelectorDialog = new ItemSelectorDialog({
+			eventName: `${portletNamespace}selectDocumentLibrary`,
+			singleSelect: true,
+			url: itemSelectorURL,
+		});
+
+		itemSelectorDialog.on('selectedItemChange', _handleFieldChanged);
+
+		itemSelectorDialog.open();
+	};
+
+	const spritemap =
+		Liferay.ThemeDisplay.getPathThemeImages() + '/lexicon/icons.svg';
+
+	const placeholder = readOnly
+		? ''
+		: Liferay.Language.get('add-image-description');
+
+	let {height = '0', width = '0'} = {
+		...JSON.parse(inputValue || '{}'),
+	};
+
+	height = `${height}px`;
+	width = `${width}px`;
+
+	return (
+		<>
+			<ClayForm.Group style={{marginBottom: '0.5rem'}}>
+				<input
+					id={id}
+					name={name}
+					type="hidden"
+					value={JSON.stringify(imageValues)}
+				/>
+				<ClayInput.Group>
+					<ClayInput.GroupItem className="d-none d-sm-block" prepend>
+						<ClayInput
+							className="bg-light"
+							disabled={readOnly}
+							onClick={_handleItemSelectorTriggerClick}
+							readOnly
+							type="text"
+							value={imageValues.title}
+						/>
+					</ClayInput.GroupItem>
+
+					<ClayInput.GroupItem append shrink>
+						<ClayButton
+							disabled={readOnly}
+							displayType="secondary"
+							onClick={_handleItemSelectorTriggerClick}
+							type="button"
+						>
+							{Liferay.Language.get('select')}
+						</ClayButton>
+					</ClayInput.GroupItem>
+
+					{imageValues.url && (
+						<ClayInput.GroupItem shrink>
+							<ClayButton
+								disabled={readOnly}
+								displayType="secondary"
+								onClick={_handleClearClick}
+								type="button"
+							>
+								{Liferay.Language.get('clear')}
+							</ClayButton>
+						</ClayInput.GroupItem>
+					)}
+				</ClayInput.Group>
+			</ClayForm.Group>
+
+			{imageValues.url && modalVisible && (
+				<ClayModal
+					className="image-picker-preview-modal"
+					observer={observer}
+					size="full-screen"
+					spritemap={spritemap}
+				>
+					<ClayModal.Header />
+					<ClayModal.Body>
+						<img
+							alt={imageValues.description}
+							className="d-block img-fluid mb-2 mx-auto rounded"
+							onClick={() => setModalVisible(false)}
+							src={imageValues.url}
+							style={{cursor: 'zoom-out', maxHeight: '95%'}}
+						/>
+						<p
+							className="font-weight-light text-center"
+							style={{color: '#FFFFFF'}}
+						>
+							{imageValues.description}
+						</p>
+					</ClayModal.Body>
+				</ClayModal>
+			)}
+
+			{imageValues.url && (
+				<>
+					<div className="image-picker-preview">
+						<img
+							alt={imageValues.description}
+							className="d-block img-fluid mb-2 rounded"
+							src={imageValues.url}
+						/>
+						<div
+							className="image-picker-priview-backdor"
+							onClick={() => setModalVisible(true)}
+							style={{
+								height,
+								width,
+							}}
+						>
+							<ClayIcon symbol="search" />
+						</div>
+					</div>
+
+					<ClayForm.Group>
+						<ClayInput
+							defaultValue={imageValues.description}
+							disabled={readOnly}
+							name={`${name}-description`}
+							onChange={_handleDescriptionChange}
+							placeholder={placeholder}
+							type="text"
+						/>
+					</ClayForm.Group>
+				</>
+			)}
+		</>
+	);
+};
+
+const ImagePickerProxy = connectStore(({emit, ...otherProps}) => (
+	<ImagePicker
+		{...otherProps}
+		onClearClick={data => {
+			const {event} = data;
+
+			emit('fieldEdited', event, data);
+		}}
+		onDescriptionChange={data => {
+			const {event} = data;
+
+			emit('fieldEdited', event, data);
+		}}
+		onFieldChanged={data => {
+			const {event} = data;
+
+			emit('fieldEdited', event, data);
+		}}
+	/>
+));
+
+const ReactImagePickerAdapter = getConnectedReactComponentAdapter(
+	ImagePickerProxy,
+	templates
+);
+
+export {ReactImagePickerAdapter};
+export default ReactImagePickerAdapter;
