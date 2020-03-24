3a02466b8d20509267cfcd343a7b830e72e44df8
hai.yu <8219754@liferay.com>
2020-03-24 08:41:25 +0800

    LPS-110758 Deprecate com.liferay.portal.kernel.resiliency.PortalResiliencyException

--- portal-kernel/src/com/liferay/portal/kernel/resiliency/PortalResiliencyException.java
+++ portal-kernel/src/com/liferay/portal/kernel/resiliency/PortalResiliencyException.java
@@ -16,7 +16,9 @@ package com.liferay.portal.kernel.resiliency;
 
 /**
  * @author Shuyang Zhou
+ * @deprecated As of Athanasius (7.3.x), with no direct replacement
  */
+@Deprecated
 public class PortalResiliencyException extends Exception {
 
 	public PortalResiliencyException(String message) {
