6828191881216483e6d8653fb41ce839c7038bb2
Renato Rego <renato.rego@liferay.com>
2020-03-25 18:43:47 -0300

    LPS-97802 Unnecessary code.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/AddFormInstanceRecordMVCActionCommand.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/AddFormInstanceRecordMVCActionCommand.java
@@ -167,28 +167,10 @@ public class AddFormInstanceRecordMVCActionCommand
 	}
 
 	@Reference(unbind = "-")
-	protected void setAddFormInstanceRecordMVCCommandHelper(
-		AddFormInstanceRecordMVCCommandHelper
-			addFormInstanceRecordMVCCommandHelper) {
-
-		_addFormInstanceMVCCommandHelper =
-			addFormInstanceRecordMVCCommandHelper;
-	}
-
-	@Reference(unbind = "-")
 	protected void setDDMFormInstanceRecordService(
 		DDMFormInstanceRecordService ddmFormInstanceRecordService) {
 
 		_ddmFormInstanceRecordService = ddmFormInstanceRecordService;
-	}
-
-	@Reference(unbind = "-")
-	protected void setDDMFormInstanceRecordVersionLocalService(
-		DDMFormInstanceRecordVersionLocalService
-			ddmFormInstanceRecordVersionLocalService) {
-
-		_ddmFormInstanceRecordVersionLocalService =
-			ddmFormInstanceRecordVersionLocalService;
 	}
 
 	@Reference(unbind = "-")
@@ -203,11 +185,6 @@ public class AddFormInstanceRecordMVCActionCommand
 		DDMFormValuesFactory ddmFormValuesFactory) {
 
 		_ddmFormValuesFactory = ddmFormValuesFactory;
-	}
-
-	@Reference(unbind = "-")
-	protected void setPortal(Portal portal) {
-		_portal = portal;
 	}
 
 	protected void validateCaptcha(
@@ -292,10 +269,15 @@ public class AddFormInstanceRecordMVCActionCommand
 		_addFormInstanceMVCCommandHelper;
 
 	private DDMFormInstanceRecordService _ddmFormInstanceRecordService;
+
+	@Reference
 	private DDMFormInstanceRecordVersionLocalService
 		_ddmFormInstanceRecordVersionLocalService;
+
 	private DDMFormInstanceService _ddmFormInstanceService;
 	private DDMFormValuesFactory _ddmFormValuesFactory;
+
+	@Reference
 	private Portal _portal;
 
 }
