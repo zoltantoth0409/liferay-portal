f67a825de0dd2f44f65701eaa60a20e0410c2009
Pavel Savinov <pavel.savinov@liferay.com>
2020-03-26 12:10:22 +0100

    LPS-110755 Use entity delete method

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/importer/LayoutPageTemplatesImporterImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/importer/LayoutPageTemplatesImporterImpl.java
@@ -905,9 +905,7 @@ public class LayoutPageTemplatesImporterImpl
 
 		if (layoutPageTemplateStructure != null) {
 			_layoutPageTemplateStructureLocalService.
-				deleteLayoutPageTemplateStructure(
-					layoutPageTemplateStructure.
-						getLayoutPageTemplateStructureId());
+				deleteLayoutPageTemplateStructure(layoutPageTemplateStructure);
 		}
 
 		_layoutPageTemplateStructureLocalService.addLayoutPageTemplateStructure(
