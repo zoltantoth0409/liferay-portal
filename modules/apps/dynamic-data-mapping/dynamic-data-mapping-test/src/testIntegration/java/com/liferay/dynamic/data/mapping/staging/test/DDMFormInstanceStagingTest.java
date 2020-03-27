c765a3a735da0ea6ccdbb7adf092c84fbcc48240
Marcos <marcos.martins@liferay.com>
2020-03-27 16:52:30 -0300

    LPS-110620 reactivating test from DDMFormInstanceStagingTest

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/staging/test/DDMFormInstanceStagingTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/staging/test/DDMFormInstanceStagingTest.java
@@ -41,7 +41,6 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -83,7 +82,6 @@ public class DDMFormInstanceStagingTest {
 				_stagingGroup.getGroupId()));
 	}
 
-	@Ignore
 	@Test
 	public void testPublishFormToRemoteStagingSite() throws Exception {
 		_remoteLiveGroup = GroupTestUtil.addGroup();
