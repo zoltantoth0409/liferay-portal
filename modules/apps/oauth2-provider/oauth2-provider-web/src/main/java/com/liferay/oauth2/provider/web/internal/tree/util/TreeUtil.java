de71548aed5259be5c9766526bd6c3c6abcfdb55
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-03-27 19:07:59 +0100

    LPS-105158 Add classes to build a tree from a set and a BiFunction
    the bifunction specifies if there is a hierarchical relation between
    any two nodes in the set.
    
    The BiFunction can't generate circular hierarchies. If it generate
    cycles an exception is thrown.

---
+++ modules/apps/oauth2-provider/oauth2-provider-web/src/main/java/com/liferay/oauth2/provider/web/internal/tree/util/TreeUtil.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.oauth2.provider.web.internal.tree.util;
+
+import com.liferay.oauth2.provider.web.internal.tree.Tree;
+import com.liferay.petra.string.StringBundler;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiPredicate;
+
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.AllDirectedPaths;
+import org.jgrapht.graph.DirectedAcyclicGraph;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class TreeUtil {
+
+	public static <T> Tree.Node<T> getTreeNode(
+		Set<T> set, T rootValue, BiPredicate<T, T> biPredicate) {
+
+		DirectedAcyclicGraph<T, String> directedAcyclicGraph =
+			new DirectedAcyclicGraph<>(String.class);
+
+		for (T vertex1 : set) {
+			directedAcyclicGraph.addVertex(vertex1);
+
+			for (T vertex2 : set) {
+				if (Objects.equals(vertex1, vertex2)) {
+					continue;
+				}
+
+				if (biPredicate.test(vertex1, vertex2)) {
+					directedAcyclicGraph.addVertex(vertex2);
+
+					directedAcyclicGraph.addEdge(
+						vertex1, vertex2,
+						StringBundler.concat(vertex1, "#", vertex2));
+				}
+			}
+		}
+
+		Set<T> endingVertices = new HashSet<>();
+		Set<T> initialVertices = new HashSet<>();
+
+		for (T vertex : set) {
+			if (directedAcyclicGraph.outDegreeOf(vertex) == 0) {
+				endingVertices.add(vertex);
+			}
+
+			if (directedAcyclicGraph.inDegreeOf(vertex) == 0) {
+				initialVertices.add(vertex);
+			}
+		}
+
+		_filterRedundantPaths(
+			directedAcyclicGraph, initialVertices, endingVertices);
+
+		return _createTreeNode(
+			directedAcyclicGraph, rootValue, initialVertices);
+	}
+
+	private static <T, E> Tree<T> _createTree(Graph<T, E> graph, T value) {
+		if (graph.outDegreeOf(value) == 0) {
+			return new Tree.Leaf<>(value);
+		}
+
+		Set<T> set = new HashSet<>();
+
+		for (E edge : graph.outgoingEdgesOf(value)) {
+			set.add(graph.getEdgeTarget(edge));
+		}
+
+		return _createTreeNode(graph, value, set);
+	}
+
+	private static <T> Tree.Node<T> _createTreeNode(
+		Graph<T, ?> graph, T value, Set<T> set) {
+
+		List<Tree<T>> trees = new ArrayList<>();
+
+		for (T childValue : set) {
+			trees.add(_createTree(graph, childValue));
+		}
+
+		return new Tree.Node<>(value, trees);
+	}
+
+	private static <T> void _filterRedundantPaths(
+		DirectedAcyclicGraph<T, String> directedAcyclicGraph,
+		Set<T> initialVertices, Set<T> endingVertices) {
+
+		AllDirectedPaths<T, String> allDirectedPaths = new AllDirectedPaths<>(
+			directedAcyclicGraph);
+
+		List<GraphPath<T, String>> allPaths = allDirectedPaths.getAllPaths(
+			initialVertices, endingVertices, true, null);
+
+		Comparator<GraphPath<?, ?>> comparator = Comparator.comparingInt(
+			GraphPath::getLength);
+
+		allPaths.sort(comparator.reversed());
+
+		HashMap<String, Set<String>> visitedEdgesMap = new HashMap<>();
+		HashMap<String, Set<T>> visitedVerticesMap = new HashMap<>();
+
+		for (GraphPath<T, String> path : allPaths) {
+			String pathKey = StringBundler.concat(
+				path.getStartVertex(), "#", path.getEndVertex());
+
+			Set<T> visitedVerticesSet = visitedVerticesMap.computeIfAbsent(
+				pathKey, key -> new HashSet<>());
+
+			List<T> vertexList = path.getVertexList();
+
+			Set<String> visitedEdgesSet = visitedEdgesMap.computeIfAbsent(
+				pathKey, key -> new HashSet<>());
+
+			List<String> edgeList = path.getEdgeList();
+
+			if (visitedVerticesSet.containsAll(vertexList)) {
+				for (String edge : edgeList) {
+					if (!visitedEdgesSet.contains(edge)) {
+						directedAcyclicGraph.removeEdge(edge);
+					}
+				}
+
+				continue;
+			}
+
+			visitedEdgesSet.addAll(edgeList);
+			visitedVerticesSet.addAll(vertexList);
+		}
+	}
+
+}
