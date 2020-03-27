cd6efd0ffc1262b1080b6f74bbe35c5f628acea6
Daniel Couso <daniel.couso@liferay.com>
2020-03-27 16:11:16 +0100

    LPS-110937 Exclude any system layout entry from the sitemap index

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/SitemapImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/SitemapImpl.java
@@ -269,6 +269,10 @@ public class SitemapImpl implements Sitemap {
 				entry.getKey());
 
 			for (Layout layout : layouts) {
+				if (layout.isSystem()) {
+					continue;
+				}
+
 				UnicodeProperties typeSettingsUnicodeProperties =
 					layout.getTypeSettingsProperties();
 
