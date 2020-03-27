7638bc1bf486a5a1fb5d145dd69e583d3748cb7c
Leonardo Barros <leonardo.barros@liferay.com>
2020-03-27 18:37:28 -0300

    LPS-109882 add tests

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/factory/test/DataDefinitionResourceFactoryImplTest.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/factory/test/DataDefinitionResourceFactoryImplTest.java
@@ -16,10 +16,30 @@ package com.liferay.data.engine.rest.resource.v2_0.factory.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.data.engine.rest.resource.v2_0.DataDefinitionResource;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeServicesTracker;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.AggregateResourceBundle;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -27,6 +47,8 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Brian Wing Shun Chan
@@ -45,17 +67,116 @@ public class DataDefinitionResourceFactoryImplTest {
 	}
 
 	@Test
-	public void testAcceptLanguage() throws Exception {
+	public void testHttpServletRequestLocale() throws Exception {
+		DataDefinitionResource dataDefinitionResource =
+			DataDefinitionResource.builder(
+			).httpServletRequest(
+				new MockHttpServletRequest() {
+
+					@Override
+					public Object getAttribute(String name) {
+						if (StringUtil.equals(name, WebKeys.LOCALE)) {
+							return LocaleUtil.BRAZIL;
+						}
+
+						return super.getAttribute(name);
+					}
+
+				}
+			).user(
+				_user
+			).build();
+
+		_assertDataDefinitionFieldTypes(
+			_jsonFactory.createJSONArray(
+				dataDefinitionResource.
+					getDataDefinitionDataDefinitionFieldFieldTypes()),
+			LocaleUtil.BRAZIL);
+	}
+
+	@Test
+	public void testPreferredLocale() throws Exception {
+		DataDefinitionResource dataDefinitionResource =
+			DataDefinitionResource.builder(
+			).preferredLocale(
+				LocaleUtil.BRAZIL
+			).user(
+				_user
+			).build();
+
+		_assertDataDefinitionFieldTypes(
+			_jsonFactory.createJSONArray(
+				dataDefinitionResource.
+					getDataDefinitionDataDefinitionFieldFieldTypes()),
+			LocaleUtil.BRAZIL);
+	}
+
+	@Test
+	public void testUserLocale() throws Exception {
 		DataDefinitionResource dataDefinitionResource =
 			DataDefinitionResource.builder(
 			).user(
 				_user
 			).build();
 
-		Assert.assertNotNull(
-			dataDefinitionResource.
-				getDataDefinitionDataDefinitionFieldFieldTypes());
+		_assertDataDefinitionFieldTypes(
+			_jsonFactory.createJSONArray(
+				dataDefinitionResource.
+					getDataDefinitionDataDefinitionFieldFieldTypes()),
+			LocaleUtil.US);
 	}
+
+	private void _assertDataDefinitionFieldTypes(
+		JSONArray jsonArray, Locale locale) {
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			Map<String, Object> properties =
+				_ddmFormFieldTypeServicesTracker.getDDMFormFieldTypeProperties(
+					jsonObject.getString("name"));
+
+			Assert.assertEquals(
+				_translate(
+					MapUtil.getString(
+						properties, "ddm.form.field.type.description"),
+					_getResourceBundle(jsonObject.getString("name"), locale)),
+				jsonObject.getString("description"));
+		}
+	}
+
+	private ResourceBundle _getResourceBundle(
+		String ddmFormFieldTypeName, Locale locale) {
+
+		DDMFormFieldType ddmFormFieldType =
+			_ddmFormFieldTypeServicesTracker.getDDMFormFieldType(
+				ddmFormFieldTypeName);
+
+		return new AggregateResourceBundle(
+			ResourceBundleUtil.getBundle(
+				"content.Language", locale, getClass()),
+			ResourceBundleUtil.getBundle(
+				"content.Language", locale, ddmFormFieldType.getClass()),
+			_portal.getResourceBundle(locale));
+	}
+
+	private String _translate(String key, ResourceBundle resourceBundle) {
+		if (Validator.isNull(key)) {
+			return StringPool.BLANK;
+		}
+
+		return GetterUtil.getString(
+			ResourceBundleUtil.getString(resourceBundle, key), key);
+	}
+
+	@Inject
+	private DDMFormFieldTypeServicesTracker _ddmFormFieldTypeServicesTracker;
+
+	@Inject
+	private JSONFactory _jsonFactory;
+
+	@Inject
+	private Portal _portal;
 
 	@DeleteAfterTestRun
 	private User _user;
