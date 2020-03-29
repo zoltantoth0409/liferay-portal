fea990d2784a92aab30e68fa3346fc4764214c62
Diego Nascimento <diego.nascimento@liferay.com>
2020-03-29 19:57:04 -0300

    LPS-110544 Migrate RichText DDM Field to React

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/RichText/RichText.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/RichText/RichText.es.js
@@ -12,170 +12,190 @@
  * details.
  */
 
-import '../FieldBase/FieldBase.es';
-
-import './RichTextAdapter.soy';
-
 import './RichTextRegister.soy';
 
-import './ReactRichTextAdapter.es';
+import {ClayInput} from '@clayui/form';
+import {Editor} from 'frontend-editor-ckeditor-web';
+import React, {useEffect, useRef, useState} from 'react';
 
-import Component from 'metal-component';
-import Soy from 'metal-soy';
-import {Config} from 'metal-state';
+import {FieldBaseProxy} from '../FieldBase/ReactFieldBase.es';
+import getConnectedReactComponentAdapter from '../util/ReactComponentAdapter.es';
+import {connectStore} from '../util/connectStore.es';
+import templates from './RichTextAdapter.soy';
 
-import templates from './RichText.soy';
-
-class RichText extends Component {
-	dispatchEvent(event, name, value) {
-		this.emit(name, {
-			fieldInstance: this,
-			originalEvent: event,
-			value,
-		});
-	}
-
-	_handleOnDispatch(event) {
-		switch (event.type) {
-			case 'value':
-				this.dispatchEvent(event, 'fieldEdited', event.payload);
-				break;
-			default:
-				console.error(new TypeError(`There is no type ${event.type}`));
-				break;
-		}
-	}
-}
-
-RichText.STATE = {
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof Text
-	 * @type {?(string|undefined)}
-	 */
-
-	errorMessage: Config.string(),
-
-	/**
-	 * @default false
-	 * @instance
-	 * @memberof RichText
-	 * @type {?bool}
-	 */
-
-	evaluable: Config.bool().value(false),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof Text
-	 * @type {?(string|undefined)}
-	 */
-
-	fieldName: Config.string(),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof Text
-	 * @type {?(string|undefined)}
-	 */
-
-	label: Config.string(),
-
-	/**
-	 * @default {}
-	 * @instance
-	 * @memberof RichText
-	 * @type {?(object|undefined)}
-	 */
-
-	localizedValue: Config.object().value({}),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof Text
-	 * @type {?(string|undefined)}
-	 */
-
-	name: Config.string().required(),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof Text
-	 * @type {?(string|undefined)}
-	 */
-
-	predefinedValue: Config.string(),
-
-	/**
-	 * @default false
-	 * @instance
-	 * @memberof Text
-	 * @type {?bool}
-	 */
-
-	readOnly: Config.bool().value(false),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof FieldBase
-	 * @type {?(bool|undefined)}
-	 */
-
-	repeatable: Config.bool().value(false),
-
-	/**
-	 * @default false
-	 * @instance
-	 * @memberof Text
-	 * @type {?(bool|undefined)}
-	 */
-
-	required: Config.bool().value(false),
-
-	/**
-	 * @default true
-	 * @instance
-	 * @memberof Text
-	 * @type {?(bool|undefined)}
-	 */
-
-	showLabel: Config.bool().value(true),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof Text
-	 * @type {?(string|undefined)}
-	 */
-
-	spritemap: Config.string(),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof Text
-	 * @type {?(string|undefined)}
-	 */
-
-	tip: Config.string(),
-
-	/**
-	 * @default undefined
-	 * @instance
-	 * @memberof Text
-	 * @type {?(string|undefined)}
-	 */
-
-	value: Config.string(),
+const CKEDITOR_CONFIG = {
+	toolbar: [
+		{items: ['Undo', 'Redo'], name: 'clipboard'},
+		'/',
+		{
+			items: [
+				'Bold',
+				'Italic',
+				'Underline',
+				'Strike',
+				'-',
+				'CopyFormatting',
+				'RemoveFormat',
+			],
+			name: 'basicstyles',
+		},
+		{
+			items: [
+				'NumberedList',
+				'BulletedList',
+				'-',
+				'Outdent',
+				'Indent',
+				'-',
+				'Blockquote',
+				'-',
+				'JustifyLeft',
+				'JustifyCenter',
+				'JustifyRight',
+				'JustifyBlock',
+			],
+			name: 'paragraph',
+		},
+		{items: ['Link', 'Unlink', 'Anchor'], name: 'links'},
+		{
+			items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar'],
+			name: 'insert',
+		},
+		'/',
+		{items: ['Styles', 'Format', 'Font', 'FontSize'], name: 'styles'},
+		{items: ['TextColor', 'BGColor'], name: 'colors'},
+		{items: ['Maximize'], name: 'tools'},
+		{
+			items: ['Source'],
+			name: 'document',
+		},
+	],
 };
 
-Soy.register(RichText, templates);
+/**
+ * Use Sync Value to synchronize the initial value with the current internal
+ * value, only update the internal value with the new initial value if the
+ * values are different and when the value is not changed for more than ms.
+ */
+const useSyncValue = newValue => {
+	// Maintains the reference of the last value to check in later renderings if the
+	// value is new or keeps the same, it covers cases where the value typed by
+	// the user is sent to LayoutProvider but it does not descend with the new changes.
+	const previousValueRef = useRef(newValue);
 
-export {RichText};
+	const [value, setValue] = useState(newValue);
+
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			if (value !== newValue && previousValueRef.current !== newValue) {
+				previousValueRef.current = newValue;
+				setValue(newValue);
+			}
+		}, 300);
+
+		return () => {
+			clearTimeout(handler);
+		};
+	}, [newValue, value]);
+
+	return [value, setValue];
+};
+
+const RichText = ({data, id, name, onEditorChange, readOnly}) => {
+	const [value, setValue] = useSyncValue(data);
+
+	const editorProps = {
+		config: CKEDITOR_CONFIG,
+		data: value,
+	};
+
+	if (readOnly) {
+		editorProps.readOnly = true;
+		editorProps.style = {pointerEvents: 'none'};
+	}
+	else {
+		editorProps.onChange = event => {
+			const newValue = event.editor.getData();
+
+			setValue(newValue);
+
+			onEditorChange({data: newValue, event});
+		};
+	}
+
+	const style = {
+		border: '1px solid #E7E7ED',
+		height: '148px',
+	};
+
+	return (
+		<>
+			{readOnly && data && (
+				<div
+					dangerouslySetInnerHTML={{
+						__html: data,
+					}}
+					name={`html_text_${name}`}
+					style={style}
+				></div>
+			)}
+
+			{readOnly && !data && (
+				<div
+					name={`html_text_${name}`}
+					style={{...style, color: '#A7A9BC', padding: '16px'}}
+				>
+					{Liferay.Language.get('click-to-add-a-rich-text')}
+				</div>
+			)}
+
+			{!readOnly && <Editor {...editorProps} />}
+
+			<ClayInput id={id} name={name} type="hidden" value={data} />
+		</>
+	);
+};
+
+const RichTextWithFieldBase = ({
+	data,
+	emit,
+	id,
+	name,
+	readOnly,
+	...otherProps
+}) => {
+	return (
+		<FieldBaseProxy {...otherProps} id={id} name={name} readOnly={readOnly}>
+			<RichText
+				data={data}
+				id={id}
+				name={name}
+				onEditorChange={({data, event}) =>
+					emit('fieldEdited', event, data)
+				}
+				readOnly={readOnly}
+			/>
+		</FieldBaseProxy>
+	);
+};
+
+const RichTextProxy = connectStore(
+	({emit, id, name, readOnly, localizedValue = {}, ...otherProps}) => (
+		<RichTextWithFieldBase
+			{...otherProps}
+			emit={emit}
+			id={id}
+			localizedValue={localizedValue}
+			name={name}
+			readOnly={readOnly}
+		/>
+	)
+);
+
+const ReactRichTextAdapter = getConnectedReactComponentAdapter(
+	RichTextProxy,
+	templates
+);
+
+export {ReactRichTextAdapter, RichTextWithFieldBase};
 export default RichText;
