e130e17a6f9cf4e55e49b03eaba67896f445599b
Sarai Díaz García <dgarciasarai@gmail.com>
2020-03-31 16:17:05 +0200

    LPS-109840 Add test for StringInput with disabled options

--- modules/apps/segments/segments-web/test/js/components/inputs/StringInput.es.js
+++ modules/apps/segments/segments-web/test/js/components/inputs/StringInput.es.js
@@ -83,4 +83,38 @@ describe('StringInput', () => {
 			value: defaultValue,
 		});
 	});
+
+	it('renders type string with options disabled', () => {
+		const mockOnChange = jest.fn();
+
+		const options = [
+			{
+				disabled: true,
+				label: 'Default Value',
+				value: 'defaultValue',
+			},
+			{
+				label: 'LIFERAY',
+				value: 'Liferay',
+			},
+		];
+
+		const {asFragment, getByTestId} = render(
+			<StringInput
+				onChange={mockOnChange}
+				options={options}
+				value={defaultValue}
+			/>
+		);
+
+		expect(asFragment()).toMatchSnapshot();
+
+		const element = getByTestId(OPTIONS_STRING_INPUT_TESTID);
+
+		testControlledInput({
+			element,
+			mockFunc: mockOnChange,
+			value: defaultValue,
+		});
+	});
 });
