5a3fa7f3b7bf03dae26693a00fa0803fe2082d74
hai.yu <8219754@liferay.com>
2020-03-31 17:03:05 +0800

    LPS-110758 Remove SPI modules(portlet and web plugin)

--- modules/dxp/apps/spi/spi-admin-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,3 +0,0 @@
-create index IX_C24AAC82 on SPIDefinition (companyId, name[$COLUMN_LENGTH:200$]);
-create index IX_3B4A76E9 on SPIDefinition (companyId, status);
-create index IX_1214B4BE on SPIDefinition (connectorAddress[$COLUMN_LENGTH:200$], connectorPort);
