891d7a7e661b417ace3c1eb537edcd77f24ba3f8
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-04-01 15:30:00 +0200

    LPS-111101 Integration test for portlet preferences of an widget tag. The idea is that if a single widget is included, the namespace will match the one from the fragment entry link. So we test that only one is created and such

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/FragmentEntryProcessorEditableTest.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/FragmentEntryProcessorEditableTest.java
@@ -18,6 +18,7 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.service.AssetEntryLocalService;
 import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.constants.FragmentEntryLinkConstants;
+import com.liferay.fragment.entry.processor.editable.test.constants.FragmentEntryLinkPortletKeys;
 import com.liferay.fragment.exception.FragmentEntryContentException;
 import com.liferay.fragment.model.FragmentCollection;
 import com.liferay.fragment.model.FragmentEntry;
@@ -37,18 +38,36 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.PortletPreferences;
+import com.liferay.portal.kernel.model.Theme;
+import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.LayoutSetLocalService;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ThemeLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -56,7 +75,13 @@ import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.io.IOException;
 
+import java.util.List;
 import java.util.Locale;
+
+import javax.portlet.Portlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -69,6 +94,9 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 /**
  * @author Eudaldo Alonso
@@ -87,6 +115,10 @@ public class FragmentEntryProcessorEditableTest {
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
 
+		_company = _companyLocalService.getCompany(_group.getCompanyId());
+
+		_layout = _addLayout(_group.getGroupId());
+
 		_processedHTML = _getProcessedHTML("processed_fragment_entry.html");
 
 		_originalSiteDefaultLocale = LocaleThreadLocal.getSiteDefaultLocale();
@@ -104,6 +136,52 @@ public class FragmentEntryProcessorEditableTest {
 		LocaleThreadLocal.setSiteDefaultLocale(_originalSiteDefaultLocale);
 		LocaleThreadLocal.setThemeDisplayLocale(
 			_originalThemeDisplayDefaultLocale);
+	}
+
+	@Test
+	public void testFragmentEntryLinkPortletPreferences() throws Exception {
+		FragmentEntry fragmentEntry = _addFragmentEntry(
+			"fragment_entry_with_widget_tag.html");
+
+		ServiceContext serviceContext = new MockServiceContext(
+			_layout, _getThemeDisplay());
+
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				fragmentEntry.getFragmentEntryId(),
+				_portal.getClassNameId(Layout.class), _layout.getPlid(),
+				fragmentEntry.getCss(), fragmentEntry.getHtml(),
+				fragmentEntry.getJs(), StringPool.BLANK, StringPool.BLANK,
+				StringPool.BLANK, 0, null, serviceContext);
+
+		List<PortletPreferences> portletPreferencesList =
+			_portletPreferencesLocalService.getPortletPreferences(
+				PortletKeys.PREFS_OWNER_ID_DEFAULT,
+				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, _layout.getPlid());
+
+		List<PortletPreferences> filteredPortletPreferencesList =
+			ListUtil.filter(
+				portletPreferencesList,
+				portletPreferences -> {
+					String portletId = portletPreferences.getPortletId();
+
+					return portletId.startsWith(
+						FragmentEntryLinkPortletKeys.
+							FRAGMENT_ENTRY_LINK_TEST_PORTLET);
+				});
+
+		Assert.assertEquals(
+			filteredPortletPreferencesList.toString(), 1,
+			filteredPortletPreferencesList.size());
+
+		PortletPreferences portletPreferences = portletPreferencesList.get(0);
+
+		String instanceId = PortletIdCodec.decodeInstanceId(
+			portletPreferences.getPortletId());
+
+		Assert.assertEquals(
+			fragmentEntryLink.getNamespace() + "widget", instanceId);
 	}
 
 	@Test
@@ -436,6 +514,19 @@ public class FragmentEntryProcessorEditableTest {
 			serviceContext);
 	}
 
+	private Layout _addLayout(long groupId) throws PortalException {
+		String name = RandomTestUtil.randomString();
+
+		String friendlyURL =
+			StringPool.SLASH + FriendlyURLNormalizerUtil.normalize(name);
+
+		return _layoutLocalService.addLayout(
+			TestPropsValues.getUserId(), groupId, false,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, name, null,
+			RandomTestUtil.randomString(), LayoutConstants.TYPE_PORTLET, false,
+			friendlyURL, ServiceContextTestUtil.getServiceContext());
+	}
+
 	private String _getFileAsString(String fileName) throws IOException {
 		Class<?> clazz = getClass();
 
@@ -494,8 +585,35 @@ public class FragmentEntryProcessorEditableTest {
 		return bodyElement.html();
 	}
 
+	private ThemeDisplay _getThemeDisplay() throws PortalException {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setCompany(_company);
+		themeDisplay.setLayout(_layout);
+
+		LayoutSet layoutSet = _layoutSetLocalService.getLayoutSet(
+			_group.getGroupId(), false);
+
+		Theme theme = _themeLocalService.getTheme(
+			_company.getCompanyId(), layoutSet.getThemeId());
+
+		themeDisplay.setLayoutTypePortlet(
+			(LayoutTypePortlet)_layout.getLayoutType());
+		themeDisplay.setLookAndFeel(theme, null);
+
+		themeDisplay.setRealUser(TestPropsValues.getUser());
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		return themeDisplay;
+	}
+
 	@Inject
 	private AssetEntryLocalService _assetEntryLocalService;
+
+	private Company _company;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
 
 	@Inject
 	private FragmentCollectionService _fragmentCollectionService;
@@ -512,9 +630,17 @@ public class FragmentEntryProcessorEditableTest {
 	@DeleteAfterTestRun
 	private Group _group;
 
+	private Layout _layout;
+
 	@Inject
 	private LayoutClassedModelUsageLocalService
 		_layoutClassedModelUsageLocalService;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private LayoutSetLocalService _layoutSetLocalService;
 
 	private Locale _originalSiteDefaultLocale;
 	private Locale _originalThemeDisplayDefaultLocale;
@@ -522,6 +648,46 @@ public class FragmentEntryProcessorEditableTest {
 	@Inject
 	private Portal _portal;
 
+	@Inject(
+		filter = "javax.portlet.name=" + FragmentEntryLinkPortletKeys.FRAGMENT_ENTRY_LINK_TEST_PORTLET
+	)
+	private final Portlet _portlet = null;
+
+	@Inject
+	private PortletPreferencesLocalService _portletPreferencesLocalService;
+
 	private String _processedHTML;
+
+	@Inject
+	private ThemeLocalService _themeLocalService;
+
+	private static class MockServiceContext extends ServiceContext {
+
+		public MockServiceContext(Layout layout, ThemeDisplay themeDisplay) {
+			_layout = layout;
+			_themeDisplay = themeDisplay;
+		}
+
+		@Override
+		public HttpServletRequest getRequest() {
+			HttpServletRequest httpServletRequest =
+				new MockHttpServletRequest();
+
+			httpServletRequest.setAttribute(WebKeys.LAYOUT, _layout);
+			httpServletRequest.setAttribute(
+				WebKeys.THEME_DISPLAY, _themeDisplay);
+
+			return httpServletRequest;
+		}
+
+		@Override
+		public HttpServletResponse getResponse() {
+			return new MockHttpServletResponse();
+		}
+
+		private final Layout _layout;
+		private final ThemeDisplay _themeDisplay;
+
+	}
 
 }
