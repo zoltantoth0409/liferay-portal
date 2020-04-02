5303a546ca161ca35521c7f6072a71ec93345257
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-04-02 11:59:52 +0200

    LPS-111407 Adding remote service tests

---
+++ modules/apps/redirect/redirect-test/src/testIntegration/java/com/liferay/redirect/service/test/RedirectEntryServiceTest.java
@@ -0,0 +1,274 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.redirect.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.RoleTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.redirect.constants.RedirectConstants;
+import com.liferay.redirect.model.RedirectEntry;
+import com.liferay.redirect.service.RedirectEntryService;
+import com.liferay.redirect.test.util.RedirectTestUtil;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alejandro Tardín
+ */
+@RunWith(Arquillian.class)
+public class RedirectEntryServiceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testAddRedirectEntryWithoutPermissions() throws Exception {
+		RedirectTestUtil.withRegularUser(
+			(user, role) ->
+				_redirectEntry = _redirectEntryService.addRedirectEntry(
+					_group.getGroupId(), "destinationURL", null, false,
+					"sourceURL", ServiceContextTestUtil.getServiceContext()));
+	}
+
+	@Test
+	public void testAddRedirectEntryWithPermissions() throws Exception {
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> {
+				RoleTestUtil.addResourcePermission(
+					role, RedirectConstants.RESOURCE_NAME,
+					ResourceConstants.SCOPE_COMPANY,
+					String.valueOf(TestPropsValues.getCompanyId()),
+					ActionKeys.ADD_ENTRY);
+
+				_redirectEntry = _redirectEntryService.addRedirectEntry(
+					_group.getGroupId(), "destinationURL", null, false,
+					"sourceURL", ServiceContextTestUtil.getServiceContext());
+
+				Assert.assertNotNull(_redirectEntry);
+			});
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testDeleteRedirectEntryWithoutPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> _redirectEntryService.deleteRedirectEntry(
+				_redirectEntry.getRedirectEntryId()));
+	}
+
+	@Test
+	public void testDeleteRedirectEntryWithPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> {
+				RoleTestUtil.addResourcePermission(
+					role, RedirectEntry.class.getName(),
+					ResourceConstants.SCOPE_COMPANY,
+					String.valueOf(TestPropsValues.getCompanyId()),
+					ActionKeys.DELETE);
+
+				Assert.assertNotNull(
+					_redirectEntryService.deleteRedirectEntry(
+						_redirectEntry.getRedirectEntryId()));
+
+				_redirectEntry = null;
+			});
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testFetchRedirectEntryWithoutPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> _redirectEntryService.fetchRedirectEntry(
+				_redirectEntry.getRedirectEntryId()));
+	}
+
+	@Test
+	public void testFetchRedirectEntryWithPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> {
+				RoleTestUtil.addResourcePermission(
+					role, RedirectEntry.class.getName(),
+					ResourceConstants.SCOPE_COMPANY,
+					String.valueOf(TestPropsValues.getCompanyId()),
+					ActionKeys.VIEW);
+
+				Assert.assertNotNull(
+					_redirectEntryService.fetchRedirectEntry(
+						_redirectEntry.getRedirectEntryId()));
+			});
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testGetRedirectEntriesCountWithoutPermissions()
+		throws Exception {
+
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> _redirectEntryService.getRedirectEntriesCount(
+				_group.getGroupId()));
+	}
+
+	@Test
+	public void testGetRedirectEntriesCountWithPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> {
+				RoleTestUtil.addResourcePermission(
+					role, RedirectEntry.class.getName(),
+					ResourceConstants.SCOPE_COMPANY,
+					String.valueOf(TestPropsValues.getCompanyId()),
+					ActionKeys.VIEW);
+
+				Assert.assertEquals(
+					1,
+					_redirectEntryService.getRedirectEntriesCount(
+						_group.getGroupId()));
+			});
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testGetRedirectEntriesWithoutPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> _redirectEntryService.getRedirectEntries(
+				_group.getGroupId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				null));
+	}
+
+	@Test
+	public void testGetRedirectEntriesWithPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> {
+				RoleTestUtil.addResourcePermission(
+					role, RedirectEntry.class.getName(),
+					ResourceConstants.SCOPE_COMPANY,
+					String.valueOf(TestPropsValues.getCompanyId()),
+					ActionKeys.VIEW);
+
+				List<RedirectEntry> redirectEntries =
+					_redirectEntryService.getRedirectEntries(
+						_group.getGroupId(), QueryUtil.ALL_POS,
+						QueryUtil.ALL_POS, null);
+
+				Assert.assertEquals(
+					redirectEntries.toString(), 1, redirectEntries.size());
+			});
+	}
+
+	@Test(expected = PrincipalException.MustHavePermission.class)
+	public void testUpdateRedirectEntryWithoutPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) ->
+				_redirectEntry = _redirectEntryService.updateRedirectEntry(
+					_redirectEntry.getRedirectEntryId(),
+					RandomTestUtil.randomString(), null,
+					RandomTestUtil.randomBoolean(),
+					RandomTestUtil.randomString()));
+	}
+
+	@Test
+	public void testUpdateRedirectEntryWithPermissions() throws Exception {
+		_redirectEntry = _redirectEntryService.addRedirectEntry(
+			_group.getGroupId(), "destinationURL", null, false, "sourceURL",
+			ServiceContextTestUtil.getServiceContext());
+
+		RedirectTestUtil.withRegularUser(
+			(user, role) -> {
+				RoleTestUtil.addResourcePermission(
+					role, RedirectEntry.class.getName(),
+					ResourceConstants.SCOPE_COMPANY,
+					String.valueOf(TestPropsValues.getCompanyId()),
+					ActionKeys.UPDATE);
+
+				_redirectEntry = _redirectEntryService.updateRedirectEntry(
+					_redirectEntry.getRedirectEntryId(),
+					RandomTestUtil.randomString(), null,
+					RandomTestUtil.randomBoolean(),
+					RandomTestUtil.randomString());
+
+				Assert.assertNotNull(_redirectEntry);
+			});
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@DeleteAfterTestRun
+	private RedirectEntry _redirectEntry;
+
+	@Inject
+	private RedirectEntryService _redirectEntryService;
+
+}
--- modules/apps/redirect/redirect-test/src/testIntegration/java/com/liferay/redirect/test/util/RedirectTestUtil.java
+++ modules/apps/redirect/redirect-test/src/testIntegration/java/com/liferay/redirect/test/util/RedirectTestUtil.java
@@ -14,8 +14,19 @@
 
 package com.liferay.redirect.test.util;
 
+import com.liferay.petra.function.UnsafeBiConsumer;
 import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.portal.configuration.test.util.ConfigurationTemporarySwapper;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.test.util.RoleTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 
 import java.util.Dictionary;
@@ -30,6 +41,13 @@ public class RedirectTestUtil {
 		throws Exception {
 
 		_withRedirect(false, unsafeRunnable);
+	}
+
+	public static void withRegularUser(
+			UnsafeBiConsumer<User, Role, Exception> unsafeBiConsumer)
+		throws Exception {
+
+		_withUser(unsafeBiConsumer, RoleConstants.TYPE_REGULAR);
 	}
 
 	private static void _withRedirect(
@@ -47,6 +65,33 @@ public class RedirectTestUtil {
 					dictionary)) {
 
 			unsafeRunnable.run();
+		}
+	}
+
+	private static void _withUser(
+			UnsafeBiConsumer<User, Role, Exception> unsafeBiConsumer,
+			int roleType)
+		throws Exception {
+
+		Role role = RoleTestUtil.addRole(roleType);
+		User user = UserTestUtil.addUser();
+
+		UserLocalServiceUtil.addRoleUser(role.getRoleId(), user);
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		try {
+			PermissionThreadLocal.setPermissionChecker(
+				PermissionCheckerFactoryUtil.create(user));
+
+			unsafeBiConsumer.accept(user, role);
+		}
+		finally {
+			PermissionThreadLocal.setPermissionChecker(permissionChecker);
+
+			RoleLocalServiceUtil.deleteRole(role);
+			UserLocalServiceUtil.deleteUser(user);
 		}
 	}
 
