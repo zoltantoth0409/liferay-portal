d3f254dd1c429bd57575b9848e8d968f061df768
Gabriel Ibson <gabriel.souza@liferay.com>
2020-04-02 17:54:41 -0300

    LPS-98417 Adapt current template to handle portal version 7.2

--- modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/java/form/field/__className__DDMFormFieldType.java
+++ modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/java/form/field/__className__DDMFormFieldType.java
@@ -3,8 +3,14 @@ package ${package}.form.field;
 
 import com.liferay.dynamic.data.mapping.form.field.type.BaseDDMFormFieldType;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
+#if (${liferayVersion.startsWith("7.2")})
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+#end
 
 import org.osgi.service.component.annotations.Component;
+#if (${liferayVersion.startsWith("7.2")})
+import org.osgi.service.component.annotations.Reference;
+#end
 
 /**
  * @author ${author}
@@ -14,23 +20,44 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 #if (${liferayVersion.startsWith("7.0")})
 		"ddm.form.field.type.display.order:Integer=9",
-#elseif (${liferayVersion.startsWith("7.1")})
+#elseif (${liferayVersion.startsWith("7.1")} || ${liferayVersion.startsWith("7.2")})
 		"ddm.form.field.type.description=${artifactId}-description",
 		"ddm.form.field.type.display.order:Integer=10",
+		"ddm.form.field.type.group=customized",
 #end
 		"ddm.form.field.type.icon=text",
+#if (${liferayVersion.startsWith("7.0")} || ${liferayVersion.startsWith("7.1")})
 		"ddm.form.field.type.js.class.name=Liferay.DDM.Field.${className}",
 		"ddm.form.field.type.js.module=${artifactId}-form-field",
+#end
 		"ddm.form.field.type.label=${artifactId}-label",
 		"ddm.form.field.type.name=${formFieldTypeName}"
 	},
 	service = DDMFormFieldType.class
 )
 public class ${className}DDMFormFieldType extends BaseDDMFormFieldType {
+#if (${liferayVersion.startsWith("7.2")})
+
+	@Override
+	public String getModuleName() {
+		return _npmResolver.resolveModuleName(
+			"dynamic-data-${artifactId}-form-field/${artifactId}.es");
+	}
+#end
 
 	@Override
 	public String getName() {
 		return "${formFieldTypeName}";
 	}
+#if (${liferayVersion.startsWith("7.2")})
+
+	@Override
+	public boolean isCustomDDMFormFieldType() {
+		return true;
+	}
+
+	@Reference
+	private NPMResolver _npmResolver;
+#end
 
 }
--- modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/resources/META-INF/resources/__artifactId__.es.js
+++ modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/resources/META-INF/resources/__artifactId__.es.js
@@ -1,6 +1,13 @@
+#if (${liferayVersion.startsWith("7.2")})
+import 'dynamic-data-mapping-form-field-type/FieldBase/FieldBase.es';
+import './${artifactId}Register.soy.js';
+import {Config} from 'metal-state';
+import templates from './${artifactId}.soy.js';
+#end
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 
+#if (!${liferayVersion.startsWith("7.2")})
 import templates from './${artifactId}.soy';
 
 /**
@@ -8,7 +15,7 @@ import templates from './${artifactId}.soy';
  */
 class ${className} extends Component {}
 
-// Register component
+//Register component
 Soy.register(${className}, templates, 'render');
 
 if (!window.DDM${className}) {
@@ -18,5 +25,48 @@ if (!window.DDM${className}) {
 }
 
 window.DDM${className}.render = ${className};
+#else
+/**
+ * ${className} Component
+ */
+class ${className} extends Component {
 
+    dispatchEvent(event, name, value) {
+        this.emit(name, {
+            fieldInstance: this,
+            originalEvent: event,
+            value
+        });
+    }
+
+    _handleFieldChanged(event) {
+        const {value} = event.target;
+
+        this.setState(
+            {
+                value
+            },
+            () => this.dispatchEvent(event, 'fieldEdited', value)
+        );
+    }
+}
+
+${className}.STATE = {
+
+    name: Config.string().required(),
+
+    predefinedValue: Config.oneOfType([Config.number(), Config.string()]),
+
+    required: Config.bool().value(false),
+
+    showLabel: Config.bool().value(true),
+
+    spritemap: Config.string(),
+
+    value: Config.string().value('')
+}
+
+// Register component
+Soy.register(${className}, templates);
+#end
 export default ${className};
