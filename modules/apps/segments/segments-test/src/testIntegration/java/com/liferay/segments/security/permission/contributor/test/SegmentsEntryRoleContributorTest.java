4f36fa80de1a32be0bef267c6dfe90472895209f
drewbrokke <drew.brokke@liferay.com>
2020-04-03 15:47:10 -0500

    LPS-107151 segments-test - adds test cases where a user should be de-provisioned
    This tests that a user is de-provisioned in three different cases:
    
    - When the SegmentsEntry is deleted
    - When the role is unassigned from the SegmentsEntry
    - When the site role is deleted

--- modules/apps/segments/segments-test/src/testIntegration/java/com/liferay/segments/security/permission/contributor/test/SegmentsEntryRoleContributorTest.java
+++ modules/apps/segments/segments-test/src/testIntegration/java/com/liferay/segments/security/permission/contributor/test/SegmentsEntryRoleContributorTest.java
@@ -15,7 +15,9 @@
 package com.liferay.segments.security.permission.contributor.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
@@ -25,6 +27,7 @@ import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.UserLocalService;
@@ -39,6 +42,7 @@ import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.segments.model.SegmentsEntry;
+import com.liferay.segments.service.SegmentsEntryLocalService;
 import com.liferay.segments.service.SegmentsEntryRoleLocalService;
 import com.liferay.segments.test.util.SegmentsTestUtil;
 
@@ -126,6 +130,45 @@ public class SegmentsEntryRoleContributorTest {
 		_assertHasPermission(actionKey, true);
 	}
 
+	@Test
+	public void testWhenDeleteSegmentsEntry() throws Exception {
+		_testDeprovisionUser(
+			() -> {
+				_segmentsEntryLocalService.deleteSegmentsEntry(
+					_segmentsEntry.getSegmentsEntryId());
+
+				_segmentsEntry = null;
+			});
+	}
+
+	@Test
+	public void testWhenDeleteSegmentsEntryRole() throws Exception {
+		_testDeprovisionUser(() -> _provisionSiteRoles(new long[0]));
+	}
+
+	@Test
+	public void testWhenDeleteSiteRole() throws Exception {
+		_testDeprovisionUser(
+			() -> {
+				_roleLocalService.deleteRole(_role.getRoleId());
+
+				_role = null;
+			});
+	}
+
+	private void _assertHasGroupPermission(boolean hasPermission)
+		throws Exception {
+
+		PermissionChecker permissionChecker =
+			PermissionCheckerFactoryUtil.create(_user);
+
+		Assert.assertEquals(
+			hasPermission,
+			permissionChecker.hasPermission(
+				TestPropsValues.getGroupId(), Group.class.getName(),
+				TestPropsValues.getGroupId(), _ACTION_KEY));
+	}
+
 	private void _assertHasPermission(String actionKey, boolean hasPermission)
 		throws Exception {
 
@@ -139,6 +182,72 @@ public class SegmentsEntryRoleContributorTest {
 				_organization.getOrganizationId(), actionKey));
 	}
 
+	private void _provisionSiteRoles(long[] siteRoleIds) throws Exception {
+		_segmentsEntryRoleLocalService.setSegmentsEntrySiteRoles(
+			_segmentsEntry.getSegmentsEntryId(), siteRoleIds,
+			ServiceContextTestUtil.getServiceContext());
+	}
+
+	private void _setUpSegmentsEntry(String criteriaString) throws Exception {
+		_segmentsEntry = SegmentsTestUtil.addSegmentsEntry(
+			TestPropsValues.getGroupId(),
+			JSONUtil.put(
+				"criteria",
+				JSONUtil.put(
+					"user",
+					JSONUtil.put(
+						"conjunction", "and"
+					).put(
+						"filterString",
+						String.format("(lastName eq '%s')", criteriaString)
+					).put(
+						"typeValue", "model"
+					))
+			).toString(),
+			User.class.getName());
+	}
+
+	private void _setUpSiteRole() throws Exception {
+		_role = RoleTestUtil.addRole(RoleConstants.TYPE_SITE);
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), Group.class.getName(),
+			ResourceConstants.SCOPE_GROUP,
+			String.valueOf(TestPropsValues.getGroupId()), _role.getRoleId(),
+			_ACTION_KEY);
+	}
+
+	private void _setUpUser(String lastName) throws Exception {
+		_user = UserTestUtil.addUser(TestPropsValues.getGroupId());
+
+		_user.setLastName(lastName);
+
+		_user = _userLocalService.updateUser(_user);
+	}
+
+	private void _testDeprovisionUser(UnsafeRunnable<Exception> unsafeRunnable)
+		throws Exception {
+
+		_setUpSiteRole();
+
+		String randomString = RandomTestUtil.randomString();
+
+		_setUpSegmentsEntry(randomString);
+		_setUpUser(randomString);
+
+		_assertHasGroupPermission(false);
+
+		_provisionSiteRoles(new long[] {_role.getRoleId()});
+
+		_assertHasGroupPermission(true);
+
+		unsafeRunnable.run();
+
+		_assertHasGroupPermission(false);
+	}
+
+	private static final String _ACTION_KEY = ActionKeys.UPDATE;
+
 	@DeleteAfterTestRun
 	private Organization _organization;
 
@@ -148,8 +257,14 @@ public class SegmentsEntryRoleContributorTest {
 	@DeleteAfterTestRun
 	private Role _role;
 
+	@Inject
+	private RoleLocalService _roleLocalService;
+
 	@DeleteAfterTestRun
 	private SegmentsEntry _segmentsEntry;
+
+	@Inject
+	private SegmentsEntryLocalService _segmentsEntryLocalService;
 
 	@Inject
 	private SegmentsEntryRoleLocalService _segmentsEntryRoleLocalService;
