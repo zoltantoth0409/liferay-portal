6cce38dc0addbdb703b25eb91e5c0cf6062d4507
jpince <jason.pince@liferay.com>
2020-04-03 15:47:11 -0700

    LRQA-56053 Update Release Date for GA3

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -62,6 +62,8 @@ public class ReleaseInfo {
 
 	public static final int RELEASE_7_3_1_BUILD_NUMBER = 7301;
 
+	public static final int RELEASE_7_3_2_BUILD_NUMBER = 7302;
+
 	public static final int RELEASE_7_3_10_BUILD_NUMBER = 7310;
 
 	public static final Date getBuildDate() {
