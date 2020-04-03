463fbb760d428cab006554e51c1f26f587a01edb
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-04-03 09:17:16 +0200

    LPS-103587 Returning a json object

--- modules/apps/flags/flags-web/src/main/java/com/liferay/flags/web/internal/portlet/action/EditEntryMVCActionCommand.java
+++ modules/apps/flags/flags-web/src/main/java/com/liferay/flags/web/internal/portlet/action/EditEntryMVCActionCommand.java
@@ -17,11 +17,19 @@ package com.liferay.flags.web.internal.portlet.action;
 import com.liferay.captcha.util.CaptchaUtil;
 import com.liferay.flags.service.FlagsEntryService;
 import com.liferay.flags.web.internal.constants.FlagsPortletKeys;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -47,33 +55,53 @@ public class EditEntryMVCActionCommand extends BaseMVCActionCommand {
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
-		CaptchaUtil.check(actionRequest);
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
-		String className = ParamUtil.getString(actionRequest, "className");
-		long classPK = ParamUtil.getLong(actionRequest, "classPK");
-		String reporterEmailAddress = ParamUtil.getString(
-			actionRequest, "reporterEmailAddress");
-		long reportedUserId = ParamUtil.getLong(
-			actionRequest, "reportedUserId");
-		String contentTitle = ParamUtil.getString(
-			actionRequest, "contentTitle");
-		String contentURL = ParamUtil.getString(actionRequest, "contentURL");
-		String reason = ParamUtil.getString(actionRequest, "reason");
+		try {
+			CaptchaUtil.check(actionRequest);
 
-		ServiceContext serviceContext = ServiceContextFactory.getInstance(
-			"com.liferay.portlet.flags.model.FlagsEntry", actionRequest);
+			String className = ParamUtil.getString(actionRequest, "className");
+			long classPK = ParamUtil.getLong(actionRequest, "classPK");
+			String reporterEmailAddress = ParamUtil.getString(
+				actionRequest, "reporterEmailAddress");
+			long reportedUserId = ParamUtil.getLong(
+				actionRequest, "reportedUserId");
+			String contentTitle = ParamUtil.getString(
+				actionRequest, "contentTitle");
+			String contentURL = ParamUtil.getString(
+				actionRequest, "contentURL");
+			String reason = ParamUtil.getString(actionRequest, "reason");
 
-		_flagsEntryService.addEntry(
-			className, classPK, reporterEmailAddress, reportedUserId,
-			contentTitle, contentURL, reason, serviceContext);
+			ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				"com.liferay.portlet.flags.model.FlagsEntry", actionRequest);
 
-		actionResponse.setRenderParameter("mvcPath", "/view.jsp");
+			_flagsEntryService.addEntry(
+				className, classPK, reporterEmailAddress, reportedUserId,
+				contentTitle, contentURL, reason, serviceContext);
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+			jsonObject.put(
+				"error",
+				LanguageUtil.get(
+					themeDisplay.getRequest(), "an-unexpected-error-occurred"));
+		}
+
+		JSONPortletResponseUtil.writeJSON(
+			actionRequest, actionResponse, jsonObject);
 	}
 
 	@Reference(unbind = "-")
 	protected void setFlagsEntryService(FlagsEntryService flagsEntryService) {
 		_flagsEntryService = flagsEntryService;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		EditEntryMVCActionCommand.class);
 
 	private FlagsEntryService _flagsEntryService;
 
