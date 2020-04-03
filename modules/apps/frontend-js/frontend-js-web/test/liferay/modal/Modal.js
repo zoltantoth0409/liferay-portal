067c90963f0f8407d9c18780bad84ec86e1992a6
Marko Čikoš <marko.cikos@liferay.com>
2020-04-03 15:41:44 +0200

    LPS-110925 Extend and update tests

--- modules/apps/frontend-js/frontend-js-web/test/liferay/modal/Modal.js
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/modal/Modal.js
@@ -12,11 +12,11 @@
  * details.
  */
 
-import {render} from '@testing-library/react';
+import {fireEvent, render} from '@testing-library/react';
 import React from 'react';
 import {act} from 'react-dom/test-utils';
 
-import {Modal} from '../../../src/main/resources/META-INF/resources/liferay/modal/Modal.es';
+import {Modal} from '../../../src/main/resources/META-INF/resources/liferay/modal/Modal';
 
 describe('Modal', () => {
 	beforeEach(() => {
@@ -38,5 +38,38 @@ describe('Modal', () => {
 		});
 
 		expect(baseElement).toMatchSnapshot();
+	});
+
+	it('renders in full screen, if url is set', () => {
+		const {baseElement} = render(<Modal url="https://www.sample.url" />);
+
+		expect(baseElement.querySelector('.modal-full-screen')).toBeTruthy();
+	});
+
+	it('renders in given size, even if url is set', () => {
+		const {baseElement} = render(
+			<Modal size="lg" url="https://www.sample.url" />
+		);
+
+		expect(baseElement.querySelector('.modal-lg')).toBeTruthy();
+	});
+
+	it('closes modal on cancel type button click', () => {
+		const onCloseCallback = jest.fn();
+
+		render(
+			<Modal
+				buttons={[{id: 'myButton', type: 'cancel'}]}
+				onClose={onCloseCallback}
+			/>
+		);
+
+		act(() => {
+			jest.runAllTimers();
+		});
+
+		fireEvent.click(document.getElementById('myButton'));
+
+		expect(onCloseCallback).toBeCalled();
 	});
 });
