8cf99bb9f77754743ea7c213ed1628be2faff4c8
Sarai Díaz García <dgarciasarai@gmail.com>
2020-04-06 13:21:33 +0200

    LPS-111527 Not needed

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/TimeSpanSelector.js
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/TimeSpanSelector.js
@@ -76,7 +76,6 @@ export default function TimeSpanSelector({
 }
 
 TimeSpanSelector.proptypes = {
-	chartState: PropTypes.object.isRequired,
 	disabledNextTimeSpan: PropTypes.bool.isRequired,
 	disabledPreviousPeriodButton: PropTypes.bool.isRequired,
 	onNextTimeSpanClick: PropTypes.func.isRequired,
