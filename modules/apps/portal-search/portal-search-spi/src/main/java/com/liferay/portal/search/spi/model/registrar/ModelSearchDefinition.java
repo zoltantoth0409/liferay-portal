7e8a5bfea11853f91ec99ab6192807c6cd32b61d
Bryan Engler <bryan.engler@liferay.com>
2020-04-06 13:14:51 -0700

    LPS-110420 portal-search-spi: Add method to set searchEngineId via custom registrars
    https://issues.liferay.com/browse/LPS-110420

--- modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/registrar/ModelSearchDefinition.java
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/registrar/ModelSearchDefinition.java
@@ -41,6 +41,8 @@ public interface ModelSearchDefinition {
 	public void setModelVisibilityContributor(
 		ModelVisibilityContributor modelVisibilityContributor);
 
+	public void setSearchEngineId(String searchEngineId);
+
 	public void setSearchResultPermissionFilterSuppressed(
 		boolean searchResultPermissionFilterSuppressed);
 
