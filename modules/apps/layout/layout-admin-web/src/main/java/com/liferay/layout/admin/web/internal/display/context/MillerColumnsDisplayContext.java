e1f4577d559308b8ae531f9b0cfb9d287d58396b
Victor Galan <victor.galan@liferay.com>
2020-04-06 10:16:24 +0200

    LPS-111523 For discarding a conversion the draft layout have to be removed not the published layout

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/MillerColumnsDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/MillerColumnsDisplayContext.java
@@ -419,7 +419,8 @@ public class MillerColumnsDisplayContext {
 							_httpServletRequest, "discard-conversion-draft")
 					).put(
 						"url",
-						_layoutsAdminDisplayContext.getDeleteLayoutURL(layout)
+						_layoutsAdminDisplayContext.getDeleteLayoutURL(
+							draftLayout)
 					));
 			}
 		}
