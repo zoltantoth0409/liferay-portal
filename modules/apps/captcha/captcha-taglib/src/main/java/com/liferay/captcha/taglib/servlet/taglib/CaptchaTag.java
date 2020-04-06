1e187948aa4d4bf2275f63475fdc7ad1224003f7
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-04-06 13:10:24 +0200

    LPS-111672 Serving the captcha image from the captcha module

--- modules/apps/captcha/captcha-taglib/src/main/java/com/liferay/captcha/taglib/servlet/taglib/CaptchaTag.java
+++ modules/apps/captcha/captcha-taglib/src/main/java/com/liferay/captcha/taglib/servlet/taglib/CaptchaTag.java
@@ -15,6 +15,9 @@
 package com.liferay.captcha.taglib.servlet.taglib;
 
 import com.liferay.captcha.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.taglib.util.IncludeTag;
 
 import javax.servlet.http.HttpServletRequest;
@@ -54,7 +57,20 @@ public class CaptchaTag extends IncludeTag {
 
 	@Override
 	protected void setAttributes(HttpServletRequest httpServletRequest) {
-		httpServletRequest.setAttribute("liferay-captcha:captcha:url", _url);
+		httpServletRequest.setAttribute(
+			"liferay-captcha:captcha:url", _getURL(httpServletRequest));
+	}
+
+	private String _getURL(HttpServletRequest httpServletRequest) {
+		if (Validator.isNotNull(_url)) {
+			return _url;
+		}
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return themeDisplay.getPathMain() + "/portal/captcha/get_image";
 	}
 
 	private static final String _PAGE = "/captcha/page.jsp";
