14ecc9d9db9c86f8926ce7b7e1c04d009c640e31
Alicia Garcia <alicia.garcia@liferay.com>
2020-04-06 12:55:11 +0200

    LPS-102880 Create method to get extension from MimeTypes

--- portal-kernel/src/com/liferay/portal/kernel/util/MimeTypesUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/MimeTypesUtil.java
@@ -17,6 +17,7 @@ package com.liferay.portal.kernel.util;
 import java.io.File;
 import java.io.InputStream;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -95,6 +96,10 @@ public class MimeTypesUtil {
 		return getMimeTypes().getExtensionContentType(extension);
 	}
 
+	public static String getExtensionMimeType(String extension) {
+		return _mimeTypes.getExtensionContentType(extension);
+	}
+
 	/**
 	 * Returns the possible file extensions for the content type.
 	 *
@@ -104,6 +109,16 @@ public class MimeTypesUtil {
 	 */
 	public static Set<String> getExtensions(String contentType) {
 		return getMimeTypes().getExtensions(contentType);
+	}
+
+	public static Set<String> getExtensionsMimeTypes(String[] extensions) {
+		Set<String> extensionsMimeTypes = new HashSet<>();
+
+		for (String extension : extensions) {
+			extensionsMimeTypes.add(getExtensionMimeType(extension));
+		}
+
+		return extensionsMimeTypes;
 	}
 
 	public static MimeTypes getMimeTypes() {
