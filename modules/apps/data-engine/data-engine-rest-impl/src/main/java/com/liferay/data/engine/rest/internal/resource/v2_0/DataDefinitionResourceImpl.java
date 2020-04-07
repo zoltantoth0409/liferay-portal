ee2a75a7ebd439e3c19be17de78082eadf4ac43a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-07 18:18:57 +0200

    LPS-111613 Call deleteStructure since we need to remove all related data

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataDefinitionResourceImpl.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/resource/v2_0/DataDefinitionResourceImpl.java
@@ -145,7 +145,7 @@ public class DataDefinitionResourceImpl
 
 		dataLayoutResource.deleteDataLayoutsDataDefinition(dataDefinitionId);
 
-		_ddmStructureLocalService.deleteDDMStructure(dataDefinitionId);
+		_ddmStructureLocalService.deleteStructure(dataDefinitionId);
 
 		List<DDMStructureVersion> ddmStructureVersions =
 			_ddmStructureVersionLocalService.getStructureVersions(
