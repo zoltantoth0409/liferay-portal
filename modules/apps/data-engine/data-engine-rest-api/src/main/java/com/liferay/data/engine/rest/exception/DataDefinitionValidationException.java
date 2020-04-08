e3d1fa0c4980e8eed655fff484fdbe6599cded56
Leonardo Barros <leonardo.barros@liferay.com>
2020-04-08 13:19:28 -0300

    LPS-111612 Add new exception class

---
+++ modules/apps/data-engine/data-engine-rest-api/src/main/java/com/liferay/data/engine/rest/exception/DataDefinitionValidationException.java
@@ -0,0 +1,356 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.exception;
+
+import com.liferay.petra.string.StringPool;
+
+import java.util.Locale;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DataDefinitionValidationException extends RuntimeException {
+
+	public DataDefinitionValidationException() {
+	}
+
+	public DataDefinitionValidationException(String msg) {
+		super(msg);
+	}
+
+	public DataDefinitionValidationException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public DataDefinitionValidationException(Throwable cause) {
+		super(cause);
+	}
+
+	public static class MustNotDuplicateFieldName
+		extends DataDefinitionValidationException {
+
+		public MustNotDuplicateFieldName(Set<String> duplicatedFieldNames) {
+			super(
+				String.format(
+					"Field names %s were defined more than once",
+					duplicatedFieldNames));
+
+			_duplicatedFieldNames = duplicatedFieldNames;
+		}
+
+		public Set<String> getDuplicatedFieldNames() {
+			return _duplicatedFieldNames;
+		}
+
+		private final Set<String> _duplicatedFieldNames;
+
+	}
+
+	public static class MustSetAvailableLocales
+		extends DataDefinitionValidationException {
+
+		public MustSetAvailableLocales() {
+			super(
+				"The available locales property was not set for the data " +
+					"definition");
+		}
+
+	}
+
+	public static class MustSetDefaultLocale
+		extends DataDefinitionValidationException {
+
+		public MustSetDefaultLocale() {
+			super(
+				"The default locale property was not set for the data " +
+					"definition");
+		}
+
+	}
+
+	public static class MustSetDefaultLocaleAsAvailableLocale
+		extends DataDefinitionValidationException {
+
+		public MustSetDefaultLocaleAsAvailableLocale(Locale defaultLocale) {
+			super(
+				String.format(
+					"The default locale %s must be set to a valid available " +
+						"locale",
+					defaultLocale));
+
+			_defaultLocale = defaultLocale;
+		}
+
+		public Locale getDefaultLocale() {
+			return _defaultLocale;
+		}
+
+		private final Locale _defaultLocale;
+
+	}
+
+	public static class MustSetFieldType
+		extends DataDefinitionValidationException {
+
+		public MustSetFieldType(String fieldName) {
+			super(
+				String.format(
+					"The field type was never set for the data definition " +
+						"field with the field name %s",
+					fieldName));
+
+			_fieldName = fieldName;
+		}
+
+		public String getFieldName() {
+			return _fieldName;
+		}
+
+		private String _fieldName;
+
+	}
+
+	public static class MustSetOptionsForField
+		extends DataDefinitionValidationException {
+
+		public MustSetOptionsForField(String fieldName) {
+			super(
+				String.format(
+					"At least one option must be set for field %s", fieldName));
+
+			_fieldName = fieldName;
+		}
+
+		public String getFieldName() {
+			return _fieldName;
+		}
+
+		private String _fieldName;
+
+	}
+
+	public static class MustSetValidAvailableLocalesForProperty
+		extends DataDefinitionValidationException {
+
+		public MustSetValidAvailableLocalesForProperty(
+			String fieldName, String property) {
+
+			super(
+				String.format(
+					"Invalid available locales set for the property '%s' of " +
+						"field name %s",
+					property, fieldName));
+
+			_fieldName = fieldName;
+			_property = property;
+		}
+
+		public String getFieldName() {
+			return _fieldName;
+		}
+
+		public String getProperty() {
+			return _property;
+		}
+
+		private String _fieldName;
+		private String _property;
+
+	}
+
+	public static class MustSetValidCharactersForFieldName
+		extends DataDefinitionValidationException {
+
+		public MustSetValidCharactersForFieldName(String fieldName) {
+			super(
+				String.format(
+					"Invalid characters entered for field name %s", fieldName));
+
+			_fieldName = fieldName;
+		}
+
+		public String getFieldName() {
+			return _fieldName;
+		}
+
+		private String _fieldName;
+
+	}
+
+	public static class MustSetValidCharactersForFieldType
+		extends DataDefinitionValidationException {
+
+		public MustSetValidCharactersForFieldType(String fieldType) {
+			super(
+				String.format(
+					"Invalid characters entered for field type %s", fieldType));
+
+			_fieldType = fieldType;
+		}
+
+		public String getFieldType() {
+			return _fieldType;
+		}
+
+		private final String _fieldType;
+
+	}
+
+	public static class MustSetValidDefaultLocaleForProperty
+		extends DataDefinitionValidationException {
+
+		public MustSetValidDefaultLocaleForProperty(
+			String fieldName, String property) {
+
+			super(
+				String.format(
+					"Invalid default locale set for the property '%s' of " +
+						"field name %s",
+					property, fieldName));
+
+			_fieldName = fieldName;
+			_property = property;
+		}
+
+		public String getFieldName() {
+			return _fieldName;
+		}
+
+		public String getProperty() {
+			return _property;
+		}
+
+		private String _fieldName;
+		private String _property;
+
+	}
+
+	public static class MustSetValidIndexType
+		extends DataDefinitionValidationException {
+
+		public MustSetValidIndexType(String fieldName) {
+			super(
+				String.format(
+					"Invalid index type set for field %s", fieldName));
+
+			_fieldName = fieldName;
+		}
+
+		public String getFieldName() {
+			return _fieldName;
+		}
+
+		private String _fieldName;
+
+	}
+
+	public static class MustSetValidName
+		extends DataDefinitionValidationException {
+
+		public MustSetValidName(String message) {
+			super(message);
+		}
+
+	}
+
+	public static class MustSetValidRuleExpression
+		extends DataDefinitionValidationException {
+
+		public MustSetValidRuleExpression(String expression, String message) {
+			super(message);
+
+			_expression = expression;
+		}
+
+		public String getExpression() {
+			return _expression;
+		}
+
+		private final String _expression;
+
+	}
+
+	public static class MustSetValidValidationExpression
+		extends MustSetValidFieldExpression {
+
+		public MustSetValidValidationExpression(
+			String fieldName, String expression) {
+
+			super(fieldName, "validation", expression);
+		}
+
+		public String getValidationExpression() {
+			return expression;
+		}
+
+		public String getValidationExpressionArgument() {
+			Matcher matcher = _validationExpressionPattern.matcher(expression);
+
+			if (matcher.find()) {
+				return matcher.group(3);
+			}
+
+			return StringPool.BLANK;
+		}
+
+		private static final Pattern _validationExpressionPattern =
+			Pattern.compile("(contains|match)\\((.+), \"(.+)\"\\)");
+
+	}
+
+	public static class MustSetValidVisibilityExpression
+		extends MustSetValidFieldExpression {
+
+		public MustSetValidVisibilityExpression(
+			String fieldName, String expression) {
+
+			super(fieldName, "visibility", expression);
+		}
+
+		public String getVisibilityExpression() {
+			return expression;
+		}
+
+	}
+
+	private static class MustSetValidFieldExpression
+		extends DataDefinitionValidationException {
+
+		public MustSetValidFieldExpression(
+			String fieldName, String expression, String message) {
+
+			super(message);
+
+			this.fieldName = fieldName;
+			this.expression = expression;
+		}
+
+		public String getExpression() {
+			return expression;
+		}
+
+		public String getFieldName() {
+			return fieldName;
+		}
+
+		protected String expression;
+		protected String fieldName;
+
+	}
+
+}
---
+++ modules/apps/data-engine/data-engine-rest-api/src/main/java/com/liferay/data/engine/rest/exception/DataLayoutValidationException.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.exception;
+
+import java.util.Set;
+
+/**
+ * @author Leonardo Barros
+ */
+public class DataLayoutValidationException extends RuntimeException {
+
+	public DataLayoutValidationException() {
+	}
+
+	public DataLayoutValidationException(String msg) {
+		super(msg);
+	}
+
+	public DataLayoutValidationException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public DataLayoutValidationException(Throwable cause) {
+		super(cause);
+	}
+
+	public static class InvalidColumnSize
+		extends DataLayoutValidationException {
+
+		public InvalidColumnSize() {
+			super(
+				"Column size must be positive and less than maximum row size " +
+					"of 12");
+		}
+
+	}
+
+	public static class InvalidRowSize extends DataLayoutValidationException {
+
+		public InvalidRowSize() {
+			super(
+				"The sum of all column sizes of a row must be less than the " +
+					"maximum row size of 12");
+		}
+
+	}
+
+	public static class MustNotDuplicateFieldName
+		extends DataLayoutValidationException {
+
+		public MustNotDuplicateFieldName(Set<String> duplicatedFieldNames) {
+			super(
+				String.format(
+					"Field names %s were defined more than once",
+					duplicatedFieldNames));
+
+			_duplicatedFieldNames = duplicatedFieldNames;
+		}
+
+		public Set<String> getDuplicatedFieldNames() {
+			return _duplicatedFieldNames;
+		}
+
+		private final Set<String> _duplicatedFieldNames;
+
+	}
+
+	public static class MustSetDefaultLocale
+		extends DataLayoutValidationException {
+
+		public MustSetDefaultLocale() {
+			super("Data layout does not have a default locale");
+		}
+
+	}
+
+	public static class MustSetEqualLocaleForLayoutAndTitle
+		extends DataLayoutValidationException {
+
+		public MustSetEqualLocaleForLayoutAndTitle() {
+			super(
+				"The default locale for the data layout's page title is not " +
+					"the same as the data layout's default locale");
+		}
+
+	}
+
+}
