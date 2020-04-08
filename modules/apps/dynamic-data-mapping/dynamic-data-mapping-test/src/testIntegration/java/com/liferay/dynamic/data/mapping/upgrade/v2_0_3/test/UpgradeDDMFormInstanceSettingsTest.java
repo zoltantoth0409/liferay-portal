cd8f063c97ee23123f92a57a44283dba56dfac9b
Leonardo Barros <leonardo.barros@liferay.com>
2020-04-08 14:06:27 -0300

    LPS-111493 Ignore Forms tests

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/upgrade/v2_0_3/test/UpgradeDDMFormInstanceSettingsTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/upgrade/v2_0_3/test/UpgradeDDMFormInstanceSettingsTest.java
@@ -41,6 +41,7 @@ import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -67,6 +68,7 @@ public class UpgradeDDMFormInstanceSettingsTest {
 		setUpUpgradeDDMFormInstanceSettings();
 	}
 
+	@Ignore
 	@Test
 	public void testAddRequireAuthenticationSetting() throws Exception {
 		String settings = createSettings(false);
@@ -87,6 +89,7 @@ public class UpgradeDDMFormInstanceSettingsTest {
 		Assert.assertTrue(containsField(fieldValues, "requireAuthentication"));
 	}
 
+	@Ignore
 	@Test
 	public void testEnableAutosaveSetting() throws Exception {
 		String settings = createSettings(false);
