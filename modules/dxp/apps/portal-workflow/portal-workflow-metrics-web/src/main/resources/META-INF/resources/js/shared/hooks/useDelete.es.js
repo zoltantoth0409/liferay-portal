719106555c3b17231829dc3b7667f126707460fb
Nara Andrade <nara.andrade@liferay.com>
2020-04-08 10:52:00 -0300

    LPS-109668 | LPS-110342 Create useDelete and usePut hooks

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/hooks/useDelete.es.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {useCallback, useContext} from 'react';
+
+import {AppContext} from '../../components/AppContext.es';
+
+const useDelete = ({admin} = {}) => {
+	const {getClient} = useContext(AppContext);
+
+	const client = getClient(admin);
+
+	return useCallback(url => client.delete(url), [client]);
+};
+
+export {useDelete};
---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/hooks/usePut.es.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {useCallback, useContext} from 'react';
+
+import {AppContext} from '../../components/AppContext.es';
+
+const usePut = ({body = {}, admin = false, url}) => {
+	const {getClient} = useContext(AppContext);
+
+	const client = getClient(admin);
+	const queryBodyStr = JSON.stringify(body);
+
+	const putData = useCallback(
+		() => client.put(url, body),
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[queryBodyStr, url, admin]
+	);
+
+	return {putData};
+};
+
+export {usePut};
