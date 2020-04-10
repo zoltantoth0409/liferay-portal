6f28d9df84d7a5cb56660188f9fec008adb2f432
Javier Gamarra <nhpatt@gmail.com>
2020-04-10 09:44:06 +0200

    LPS-107132 update tests

--- modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/internal/jaxrs/context/provider/test/AcceptLanguageContextProviderTest.java
+++ modules/apps/portal-vulcan/portal-vulcan-test/src/testIntegration/java/com/liferay/portal/vulcan/internal/jaxrs/context/provider/test/AcceptLanguageContextProviderTest.java
@@ -40,7 +40,7 @@ import java.util.Arrays;
 import java.util.Locale;
 import java.util.Set;
 
-import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.NotAcceptableException;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.HttpHeaders;
 
@@ -178,7 +178,7 @@ public class AcceptLanguageContextProviderTest {
 		}
 		catch (Exception exception) {
 			Assert.assertEquals(
-				ClientErrorException.class, exception.getClass());
+				NotAcceptableException.class, exception.getClass());
 			Assert.assertEquals(
 				"No locales match the accepted languages: es-es",
 				exception.getMessage());
