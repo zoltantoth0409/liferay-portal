1e95730b41e55402f81c09c730b0988ad73d7f6f
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2020-04-09 22:26:54 -0300

    LPS-111055 Remove unused method and file

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/util/i18n.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/util/i18n.es.js
@@ -12,8 +12,6 @@
  * details.
  */
 
-import {capitalize} from './strings.es';
-
 export const setValue = (obj, languageId, prop, val) => {
 	if (!obj[prop]) {
 		obj[prop] = {};
@@ -22,12 +20,4 @@ export const setValue = (obj, languageId, prop, val) => {
 	obj[prop][languageId] = val;
 
 	return obj;
-};
-
-export const setLocalizedValue = (obj, languageId, prop, val) => {
-	obj[prop] = val;
-
-	const localizedProperty = `localized${capitalize(prop)}`;
-
-	return setValue(obj, languageId, localizedProperty, val);
 };
