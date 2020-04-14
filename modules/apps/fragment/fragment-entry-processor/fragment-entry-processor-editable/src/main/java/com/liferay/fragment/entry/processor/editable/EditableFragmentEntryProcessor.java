87fcacd950628d9bf8af5a686af9cd153564f83b
Pavel Savinov <pavel.savinov@liferay.com>
2020-04-14 12:28:31 +0200

    LPS-111856 Make sure that both type and ID data attributes exist ffor the editable element

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/EditableFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/EditableFragmentEntryProcessor.java
@@ -455,7 +455,8 @@ public class EditableFragmentEntryProcessor implements FragmentEntryProcessor {
 		}
 
 		for (Element element :
-				document.getElementsByAttribute("data-lfr-editable-id")) {
+				document.select(
+					"*[data-lfr-editable-id],*[data-lfr-editable-type]")) {
 
 			_validateAttribute(element, "data-lfr-editable-id");
 			_validateAttribute(element, "data-lfr-editable-type");
