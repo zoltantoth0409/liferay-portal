268e65acfb9aeff6c34b8c139b8a451755f14b72
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-04-14 14:24:59 -0300

    LPS-107004 Add and map new DTOs exceptions

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/resource/exception/NoSuchTaskException.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.rest.internal.resource.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class NoSuchTaskException extends PortalException {
+
+	public NoSuchTaskException() {
+	}
+
+	public NoSuchTaskException(String msg) {
+		super(msg);
+	}
+
+	public NoSuchTaskException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public NoSuchTaskException(Throwable cause) {
+		super(cause);
+	}
+
+}
