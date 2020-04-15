c457f1a38e02a93b52307bd4d18c61bb806a7153
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-15 22:22:43 +0200

    LPS-112183 Removes no needed methods since we don't need to prefix editable values

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/segments/SegmentsExperienceUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/segments/SegmentsExperienceUtil.java
@@ -24,7 +24,6 @@ import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.layout.util.structure.LayoutStructureItem;
 import com.liferay.portal.kernel.comment.CommentManager;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.PortletPreferences;
@@ -32,12 +31,9 @@ import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
 import com.liferay.portal.kernel.service.PortletPreferencesLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.PortletKeys;
-import com.liferay.segments.constants.SegmentsExperienceConstants;
 import com.liferay.segments.util.SegmentsExperiencePortletUtil;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -62,117 +58,6 @@ public class SegmentsExperienceUtil {
 
 		_copyPortletPreferences(
 			classPK, sourceSegmentsExperienceId, targetSegmentsExperienceId);
-	}
-
-	protected static JSONObject copyEditableValues(
-		JSONObject editableValuesJSONObject, long sourceSegmentsExperienceId,
-		long targetSegmentsExperienceId) {
-
-		Iterator<String> keysIterator = editableValuesJSONObject.keys();
-
-		while (keysIterator.hasNext()) {
-			String editableProcessorKey = keysIterator.next();
-
-			JSONObject editableProcessorJSONObject =
-				editableValuesJSONObject.getJSONObject(editableProcessorKey);
-
-			if (editableProcessorJSONObject == null) {
-				continue;
-			}
-
-			Iterator<String> editableKeysIterator =
-				editableProcessorJSONObject.keys();
-
-			while (editableKeysIterator.hasNext()) {
-				String editableKey = editableKeysIterator.next();
-
-				if (editableKey.startsWith(
-						SegmentsExperienceConstants.ID_PREFIX)) {
-
-					JSONObject baseExperienceValueJSONObject =
-						editableProcessorJSONObject.getJSONObject(
-							SegmentsExperienceConstants.ID_PREFIX +
-								sourceSegmentsExperienceId);
-
-					editableProcessorJSONObject.put(
-						SegmentsExperienceConstants.ID_PREFIX +
-							targetSegmentsExperienceId,
-						baseExperienceValueJSONObject);
-
-					editableValuesJSONObject.put(
-						editableProcessorKey, editableProcessorJSONObject);
-
-					break;
-				}
-
-				JSONObject editableJSONObject =
-					editableProcessorJSONObject.getJSONObject(editableKey);
-
-				if ((editableJSONObject == null) ||
-					!editableJSONObject.has(
-						SegmentsExperienceConstants.ID_PREFIX +
-							sourceSegmentsExperienceId)) {
-
-					continue;
-				}
-
-				JSONObject valueJSONObject = editableJSONObject.getJSONObject(
-					SegmentsExperienceConstants.ID_PREFIX +
-						sourceSegmentsExperienceId);
-
-				editableJSONObject.put(
-					SegmentsExperienceConstants.ID_PREFIX +
-						targetSegmentsExperienceId,
-					valueJSONObject);
-
-				editableProcessorJSONObject.put(
-					editableKey, editableJSONObject);
-
-				editableValuesJSONObject.put(
-					editableProcessorKey, editableProcessorJSONObject);
-			}
-		}
-
-		return editableValuesJSONObject;
-	}
-
-	private static Map<Long, String> _copyFragmentEntryLinksEditableValues(
-			List<FragmentEntryLink> fragmentEntryLinks,
-			long sourceSegmentsExperienceId, long targetSegmentsExperienceId)
-		throws PortalException {
-
-		Map<Long, String> fragmentEntryLinksEditableValuesMap = new HashMap<>();
-
-		for (FragmentEntryLink fragmentEntryLink : fragmentEntryLinks) {
-			JSONObject editableValuesJSONObject =
-				JSONFactoryUtil.createJSONObject(
-					fragmentEntryLink.getEditableValues());
-
-			copyEditableValues(
-				editableValuesJSONObject, sourceSegmentsExperienceId,
-				targetSegmentsExperienceId);
-
-			fragmentEntryLinksEditableValuesMap.put(
-				fragmentEntryLink.getFragmentEntryLinkId(),
-				editableValuesJSONObject.toString());
-		}
-
-		return fragmentEntryLinksEditableValuesMap;
-	}
-
-	private static void _copyFragmentEntryLinksEditableValues(
-			long classNameId, long classPK, long groupId,
-			long sourceSegmentsExperienceId, long targetSegmentsExperienceId)
-		throws PortalException {
-
-		Map<Long, String> fragmentEntryLinksEditableValuesMap =
-			_copyFragmentEntryLinksEditableValues(
-				FragmentEntryLinkLocalServiceUtil.getFragmentEntryLinks(
-					groupId, classNameId, classPK),
-				sourceSegmentsExperienceId, targetSegmentsExperienceId);
-
-		FragmentEntryLinkLocalServiceUtil.updateFragmentEntryLinks(
-			fragmentEntryLinksEditableValuesMap);
 	}
 
 	private static void _copyLayoutData(
