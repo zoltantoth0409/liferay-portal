5ffcf5e50fcd0b48177281c2bd686b74b475f649
Inácio Nery <inacio.nery@liferay.com>
2020-04-15 15:13:07 -0300

    LPS-104155 We must use QueryFilter for String to avoid case sensitivity

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/search/spi/model/query/contributor/KaleoActionModelPreFilterContributor.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/search/spi/model/query/contributor/KaleoActionModelPreFilterContributor.java
@@ -14,9 +14,13 @@
 
 package com.liferay.portal.workflow.kaleo.internal.search.spi.model.query.contributor;
 
+import com.liferay.portal.kernel.search.BooleanQuery;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.search.filter.QueryFilter;
+import com.liferay.portal.kernel.search.generic.BooleanQueryImpl;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.spi.model.query.contributor.ModelPreFilterContributor;
 import com.liferay.portal.search.spi.model.registrar.ModelSearchSettings;
 
@@ -38,11 +42,15 @@ public class KaleoActionModelPreFilterContributor
 		BooleanFilter booleanFilter, ModelSearchSettings modelSearchSettings,
 		SearchContext searchContext) {
 
-		long executionType = GetterUtil.getLong(
+		String executionType = GetterUtil.getString(
 			searchContext.getAttribute("executionType"));
 
-		if (executionType > 0) {
-			booleanFilter.addRequiredTerm("executionType", executionType);
+		if (Validator.isNotNull(executionType)) {
+			BooleanQuery booleanQuery = new BooleanQueryImpl();
+
+			booleanQuery.addRequiredTerm("executionType", executionType);
+
+			booleanFilter.add(new QueryFilter(booleanQuery));
 		}
 
 		long kaleoClassPK = GetterUtil.getLong(
