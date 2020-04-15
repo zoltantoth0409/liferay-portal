a8354fc5ba67c5fdb6c5ea03dbf6f2c1455e3f69
Preston-Crary <preston.crary@liferay.com>
2020-04-15 14:11:03 -0700

    LPS-111986 Apply DSL to CTProcessFinderImpl.

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/persistence/impl/CTProcessFinderImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/persistence/impl/CTProcessFinderImpl.java
@@ -14,20 +14,23 @@
 
 package com.liferay.change.tracking.service.persistence.impl;
 
+import com.liferay.change.tracking.model.CTCollectionTable;
 import com.liferay.change.tracking.model.CTProcess;
+import com.liferay.change.tracking.model.CTProcessTable;
 import com.liferay.change.tracking.model.impl.CTProcessImpl;
 import com.liferay.change.tracking.service.persistence.CTProcessFinder;
-import com.liferay.petra.string.StringPool;
+import com.liferay.petra.sql.dsl.DSLFunctionFactoryUtil;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
+import com.liferay.petra.sql.dsl.expression.Predicate;
+import com.liferay.petra.sql.dsl.query.LimitStep;
+import com.liferay.portal.background.task.model.BackgroundTaskTable;
 import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
-import com.liferay.portal.kernel.dao.orm.QueryPos;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.SQLQuery;
 import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.dao.orm.WildcardMode;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.OrderByComparator;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.List;
@@ -42,9 +45,6 @@ import org.osgi.service.component.annotations.Reference;
 public class CTProcessFinderImpl
 	extends CTProcessFinderBaseImpl implements CTProcessFinder {
 
-	public static final String FIND_BY_C_U_N_D_S =
-		CTProcessFinder.class.getName() + ".findByC_U_N_D_S";
-
 	@Override
 	@SuppressWarnings("unchecked")
 	public List<CTProcess> findByC_U_N_D_S(
@@ -56,65 +56,88 @@ public class CTProcessFinderImpl
 		try {
 			session = openSession();
 
-			String sql = _customSQL.get(getClass(), FIND_BY_C_U_N_D_S);
-
-			if (userId <= 0) {
-				sql = StringUtil.removeSubstring(
-					sql, "AND (CTProcess.userId = ?)");
-			}
-
-			String[] names = _customSQL.keywords(
-				keywords, true, WildcardMode.SURROUND);
-			String[] descriptions = _customSQL.keywords(
-				keywords, true, WildcardMode.SURROUND);
-
-			boolean keywordsEmpty = Validator.isBlank(keywords);
-
-			if (keywordsEmpty) {
-				int x = sql.indexOf("AND (BackgroundTask.status = ?)") + 32;
-
-				int y = sql.indexOf("ORDER BY", x);
-
-				sql = sql.substring(0, x) + sql.substring(y);
-			}
-			else {
-				sql = _customSQL.replaceKeywords(
-					sql, "LOWER(CTCollection.name)", StringPool.LIKE, false,
-					names);
-				sql = _customSQL.replaceKeywords(
-					sql, "LOWER(CTCollection.description)", StringPool.LIKE,
-					true, descriptions);
-				sql = _customSQL.replaceAndOperator(sql, false);
-			}
-
-			if (status == WorkflowConstants.STATUS_ANY) {
-				sql = StringUtil.removeSubstring(
-					sql, "AND (BackgroundTask.status = ?)");
-			}
-
-			sql = _customSQL.replaceOrderBy(sql, orderByComparator);
-
-			SQLQuery sqlQuery = session.createSynchronizedSQLQuery(sql);
-
-			sqlQuery.addEntity("CTProcess", CTProcessImpl.class);
-
-			QueryPos queryPos = QueryPos.getInstance(sqlQuery);
-
-			queryPos.add(companyId);
+			Predicate predicate = CTProcessTable.INSTANCE.companyId.eq(
+				companyId);
 
 			if (userId > 0) {
-				queryPos.add(userId);
+				predicate = predicate.and(
+					CTProcessTable.INSTANCE.userId.eq(userId));
 			}
 
 			if (status != WorkflowConstants.STATUS_ANY) {
-				queryPos.add(status);
+				predicate = predicate.and(
+					BackgroundTaskTable.INSTANCE.status.eq(status));
 			}
 
-			if (!keywordsEmpty) {
-				queryPos.add(names, 2);
+			Predicate keywordsPredicate = null;
 
-				queryPos.add(descriptions, 2);
+			for (String keyword :
+					_customSQL.keywords(
+						keywords, true, WildcardMode.SURROUND)) {
+
+				if (keyword == null) {
+					continue;
+				}
+
+				Predicate keywordPredicate = DSLFunctionFactoryUtil.lower(
+					CTCollectionTable.INSTANCE.name
+				).like(
+					keyword
+				).or(
+					DSLFunctionFactoryUtil.lower(
+						CTCollectionTable.INSTANCE.description
+					).like(
+						keyword
+					)
+				);
+
+				if (keywordsPredicate == null) {
+					keywordsPredicate = keywordPredicate;
+				}
+				else {
+					keywordsPredicate = keywordsPredicate.or(keywordPredicate);
+				}
 			}
+
+			if (keywordsPredicate != null) {
+				predicate = predicate.and(keywordsPredicate.withParentheses());
+			}
+
+			SQLQuery sqlQuery = session.createSynchronizedSQLQuery(
+				DSLQueryFactoryUtil.select(
+					CTProcessTable.INSTANCE
+				).from(
+					CTProcessTable.INSTANCE
+				).innerJoinON(
+					CTCollectionTable.INSTANCE,
+					CTCollectionTable.INSTANCE.ctCollectionId.eq(
+						CTProcessTable.INSTANCE.ctCollectionId)
+				).innerJoinON(
+					BackgroundTaskTable.INSTANCE,
+					BackgroundTaskTable.INSTANCE.backgroundTaskId.eq(
+						CTProcessTable.INSTANCE.backgroundTaskId)
+				).where(
+					predicate
+				).orderBy(
+					orderByStep -> {
+						if (orderByComparator != null) {
+							LimitStep limitStep = orderByStep.orderBy(
+								CTCollectionTable.INSTANCE, orderByComparator);
+
+							if (limitStep == orderByStep) {
+								return orderByStep.orderBy(
+									CTProcessTable.INSTANCE, orderByComparator);
+							}
+
+							return limitStep;
+						}
+
+						return orderByStep.orderBy(
+							CTProcessTable.INSTANCE.createDate.descending());
+					}
+				));
+
+			sqlQuery.addEntity("CTProcess", CTProcessImpl.class);
 
 			return (List<CTProcess>)QueryUtil.list(
 				sqlQuery, getDialect(), start, end);
