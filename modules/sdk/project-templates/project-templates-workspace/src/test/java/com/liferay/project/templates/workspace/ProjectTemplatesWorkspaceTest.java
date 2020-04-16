8e0f82f6e5234650004ee06e060f7b251f41051d
Lawrence Lee <lawrence.lee@liferay.com>
2020-04-15 21:54:01 -0500

    LPS-110421 Test fixes for 7.3.1

--- modules/sdk/project-templates/project-templates-workspace/src/test/java/com/liferay/project/templates/workspace/ProjectTemplatesWorkspaceTest.java
+++ modules/sdk/project-templates/project-templates-workspace/src/test/java/com/liferay/project/templates/workspace/ProjectTemplatesWorkspaceTest.java
@@ -25,7 +25,9 @@ import java.io.FileOutputStream;
 
 import java.net.URI;
 
+import java.util.Optional;
 import java.util.Properties;
+import java.util.regex.Matcher;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -183,6 +185,189 @@ public class ProjectTemplatesWorkspaceTest
 			testExists(
 				mavenModulesDir, "foo-portlet/target/foo-portlet-1.0.0.jar");
 		}
+	}
+
+	@Test
+	public void testCompareAntBndPluginVersions() throws Exception {
+		String template = "mvc-portlet";
+		String name = "foo";
+
+		String liferayVersion = getDefaultLiferayVersion();
+
+		File workspaceDir = buildWorkspace(
+			temporaryFolder, "gradle", "gradleWS", liferayVersion,
+			mavenExecutor);
+
+		File modulesDir = new File(workspaceDir, "modules");
+
+		buildTemplateWithGradle(modulesDir, template, name);
+
+		Optional<String> gradleResult = executeGradle(
+			workspaceDir, true, _gradleDistribution,
+			":modules:" + name + GRADLE_TASK_PATH_BUILD);
+
+		String gradleAntBndVersion = null;
+
+		Matcher matcher =
+			BaseProjectTemplatesTestCase.antBndPluginVersionPattern.matcher(
+				gradleResult.get());
+
+		if (matcher.matches()) {
+			gradleAntBndVersion = matcher.group(1);
+		}
+
+		File mavenWorkspaceDir = buildWorkspace(
+			temporaryFolder, "maven", "mavenWS", liferayVersion, mavenExecutor);
+
+		File mavenModulesDir = new File(mavenWorkspaceDir, "modules");
+
+		File mavenProjectDir = buildTemplateWithMaven(
+			mavenModulesDir, mavenModulesDir, template, name, "com.test",
+			mavenExecutor, "-DclassName=foo");
+
+		testContains(
+			mavenProjectDir, "pom.xml",
+			"<artifactId>com.liferay.ant.bnd</artifactId>\n\t\t\t\t\t\t" +
+				"<version>" + gradleAntBndVersion);
+	}
+
+	@Test
+	public void testComparePortalToolsBundleSupportPluginVersions()
+		throws Exception {
+
+		File workspaceDir = buildWorkspace(
+			temporaryFolder, getDefaultLiferayVersion());
+
+		Optional<String> result = executeGradle(
+			workspaceDir, true, _gradleDistribution, ":tasks");
+
+		Matcher matcher =
+			BaseProjectTemplatesTestCase.portalToolsBundleSupportVersionPattern.
+				matcher(result.get());
+
+		String portalToolsBundleSupportVersion = null;
+
+		if (matcher.matches()) {
+			portalToolsBundleSupportVersion = matcher.group(1);
+		}
+
+		File mavenWorkspaceDir = buildTemplateWithMaven(
+			temporaryFolder, "workspace", "mavenWS", "com.test", mavenExecutor,
+			"-DliferayVersion=" + getDefaultLiferayVersion());
+
+		testContains(
+			mavenWorkspaceDir, "pom.xml",
+			"<artifactId>com.liferay.portal.tools.bundle.support</artifactId>" +
+				"\n\t\t\t\t<version>" + portalToolsBundleSupportVersion);
+	}
+
+	@Test
+	public void testSassCompilerMavenWorkspace() throws Exception {
+		File nativeSassWorkspaceDir = buildTemplateWithMaven(
+			temporaryFolder, "workspace", "nativeSassMavenWS", "com.test",
+			mavenExecutor, "-DliferayVersion=" + getDefaultLiferayVersion());
+
+		File nativeSassModulesDir = new File(nativeSassWorkspaceDir, "modules");
+
+		File nativeSassProjectDir = buildTemplateWithMaven(
+			nativeSassWorkspaceDir.getParentFile(), nativeSassModulesDir,
+			"mvc-portlet", "foo-portlet", "com.test", mavenExecutor,
+			"-DclassName=Foo", "-Dpackage=foo.portlet",
+			"-DprojectType=workspace");
+
+		String nativeSassOutput = executeMaven(
+			nativeSassProjectDir, mavenExecutor, MAVEN_GOAL_PACKAGE);
+
+		Assert.assertTrue(
+			nativeSassOutput,
+			nativeSassOutput.contains("Using native Sass compiler"));
+
+		File rubySassWorkspaceDir = buildTemplateWithMaven(
+			temporaryFolder, "workspace", "rubySassMavenWS", "com.test",
+			mavenExecutor, "-DliferayVersion=" + getDefaultLiferayVersion());
+
+		File rubySassModulesDir = new File(rubySassWorkspaceDir, "modules");
+
+		File rubySassProjectDir = buildTemplateWithMaven(
+			rubySassWorkspaceDir.getParentFile(), rubySassModulesDir,
+			"mvc-portlet", "foo-portlet", "com.test", mavenExecutor,
+			"-DclassName=Foo", "-Dpackage=foo.portlet",
+			"-DprojectType=workspace");
+
+		File pomXmlFile = new File(rubySassProjectDir, "pom.xml");
+
+		if (pomXmlFile.exists()) {
+			editXml(
+				pomXmlFile,
+				document -> addCssBuilderConfigurationElement(
+					document, "sassCompilerClassName", "ruby"));
+		}
+
+		String rubySassOutput = executeMaven(
+			rubySassProjectDir, mavenExecutor, MAVEN_GOAL_PACKAGE);
+
+		Assert.assertTrue(
+			rubySassOutput,
+			rubySassOutput.contains("Using Ruby Sass compiler"));
+
+		File nativeSassOutputFile = testExists(
+			nativeSassProjectDir, "target/foo-portlet-1.0.0.jar");
+		File rubySassOutputFile = testExists(
+			rubySassProjectDir, "target/foo-portlet-1.0.0.jar");
+
+		testBundlesDiff(nativeSassOutputFile, rubySassOutputFile);
+	}
+
+	@Test
+	public void testSassCompilerWorkspace() throws Exception {
+		String liferayVersion = getDefaultLiferayVersion();
+
+		File nativeSassWorkspaceDir = buildWorkspace(
+			temporaryFolder, "gradle", "nativeSassWorkspace", liferayVersion,
+			mavenExecutor);
+
+		File nativeSassModulesDir = new File(nativeSassWorkspaceDir, "modules");
+
+		File nativeSassProjectDir = buildTemplateWithGradle(
+			nativeSassModulesDir, "mvc-portlet", "foo-portlet");
+
+		Optional<String> nativeSassResult = executeGradle(
+			nativeSassWorkspaceDir, true, _gradleDistribution,
+			":modules:foo-portlet" + GRADLE_TASK_PATH_BUILD);
+
+		String nativeSassOutput = nativeSassResult.toString();
+
+		Assert.assertTrue(
+			nativeSassOutput,
+			nativeSassOutput.contains("Using native Sass compiler"));
+
+		File rubySassWorkspaceDir = buildTemplateWithGradle(
+			temporaryFolder, WorkspaceUtil.WORKSPACE, "rubySassWorkspace");
+
+		writeGradlePropertiesInWorkspace(
+			rubySassWorkspaceDir, "sass.compiler.class.name=ruby");
+
+		File rubySassModulesDir = new File(rubySassWorkspaceDir, "modules");
+
+		File rubySassProjectDir = buildTemplateWithGradle(
+			rubySassModulesDir, "mvc-portlet", "foo-portlet");
+
+		Optional<String> rubySassResult = executeGradle(
+			rubySassWorkspaceDir, true, _gradleDistribution,
+			":modules:foo-portlet" + GRADLE_TASK_PATH_BUILD);
+
+		String rubySassOutput = rubySassResult.toString();
+
+		Assert.assertTrue(
+			rubySassOutput,
+			rubySassOutput.contains("Using Ruby Sass compiler"));
+
+		File nativeSassOutputFile = testExists(
+			nativeSassProjectDir, "build/libs/foo.portlet-1.0.0.jar");
+		File rubySassOutputFile = testExists(
+			rubySassProjectDir, "build/libs/foo.portlet-1.0.0.jar");
+
+		testBundlesDiff(nativeSassOutputFile, rubySassOutputFile);
 	}
 
 	@Rule
--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/ProjectTemplatesTest.java
@@ -30,10 +30,8 @@ import java.nio.file.Path;
 
 import java.util.Collections;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.TreeMap;
-import java.util.regex.Matcher;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -344,81 +342,6 @@ public class ProjectTemplatesTest implements BaseProjectTemplatesTestCase {
 	}
 
 	@Test
-	public void testCompareAntBndPluginVersions() throws Exception {
-		String template = "mvc-portlet";
-		String name = "foo";
-
-		String liferayVersion = getDefaultLiferayVersion();
-
-		File workspaceDir = buildWorkspace(
-			temporaryFolder, "gradle", "gradleWS", liferayVersion,
-			mavenExecutor);
-
-		File modulesDir = new File(workspaceDir, "modules");
-
-		buildTemplateWithGradle(modulesDir, template, name);
-
-		Optional<String> gradleResult = executeGradle(
-			workspaceDir, true, ProjectTemplatesTest._gradleDistribution,
-			":modules:" + name + GRADLE_TASK_PATH_BUILD);
-
-		String gradleAntBndVersion = null;
-
-		Matcher matcher =
-			BaseProjectTemplatesTestCase.antBndPluginVersionPattern.matcher(
-				gradleResult.get());
-
-		if (matcher.matches()) {
-			gradleAntBndVersion = matcher.group(1);
-		}
-
-		File mavenWorkspaceDir = buildWorkspace(
-			temporaryFolder, "maven", "mavenWS", liferayVersion, mavenExecutor);
-
-		File mavenModulesDir = new File(mavenWorkspaceDir, "modules");
-
-		File mavenProjectDir = buildTemplateWithMaven(
-			mavenModulesDir, mavenModulesDir, template, name, "com.test",
-			mavenExecutor, "-DclassName=foo");
-
-		testContains(
-			mavenProjectDir, "pom.xml",
-			"<artifactId>com.liferay.ant.bnd</artifactId>\n\t\t\t\t\t\t" +
-				"<version>" + gradleAntBndVersion);
-	}
-
-	@Test
-	public void testComparePortalToolsBundleSupportPluginVersions()
-		throws Exception {
-
-		File workspaceDir = buildWorkspace(
-			temporaryFolder, getDefaultLiferayVersion());
-
-		Optional<String> result = executeGradle(
-			workspaceDir, true, ProjectTemplatesTest._gradleDistribution,
-			":tasks");
-
-		Matcher matcher =
-			BaseProjectTemplatesTestCase.portalToolsBundleSupportVersionPattern.
-				matcher(result.get());
-
-		String portalToolsBundleSupportVersion = null;
-
-		if (matcher.matches()) {
-			portalToolsBundleSupportVersion = matcher.group(1);
-		}
-
-		File mavenWorkspaceDir = _buildTemplateWithMaven(
-			"workspace", "mavenWS", "liferayMaven",
-			"-DliferayVersion=" + getDefaultLiferayVersion());
-
-		testContains(
-			mavenWorkspaceDir, "pom.xml",
-			"<artifactId>com.liferay.portal.tools.bundle.support</artifactId>" +
-				"\n\t\t\t\t<version>" + portalToolsBundleSupportVersion);
-	}
-
-	@Test
 	public void testListTemplates() throws Exception {
 		final Map<String, String> expectedTemplates = new TreeMap<>();
 
@@ -469,115 +392,6 @@ public class ProjectTemplatesTest implements BaseProjectTemplatesTestCase {
 			customTemplatesMap.size());
 	}
 
-	@Test
-	public void testSassCompilerMavenWorkspace() throws Exception {
-		File nativeSassWorkspaceDir = _buildTemplateWithMaven(
-			"workspace", "nativeSassMavenWS", "liferayMaven",
-			"-DliferayVersion=" + getDefaultLiferayVersion());
-
-		File nativeSassModulesDir = new File(nativeSassWorkspaceDir, "modules");
-
-		File nativeSassProjectDir = buildTemplateWithMaven(
-			nativeSassWorkspaceDir.getParentFile(), nativeSassModulesDir,
-			"mvc-portlet", "foo-portlet", "com.test", mavenExecutor,
-			"-DclassName=Foo", "-Dpackage=foo.portlet",
-			"-DprojectType=workspace");
-
-		String nativeSassOutput = executeMaven(
-			nativeSassProjectDir, mavenExecutor, MAVEN_GOAL_PACKAGE);
-
-		Assert.assertTrue(
-			nativeSassOutput,
-			nativeSassOutput.contains("Using native Sass compiler"));
-
-		File rubySassWorkspaceDir = _buildTemplateWithMaven(
-			"workspace", "rubySassMavenWS", "liferayMaven",
-			"-DliferayVersion=" + getDefaultLiferayVersion());
-
-		File rubySassModulesDir = new File(rubySassWorkspaceDir, "modules");
-
-		File rubySassProjectDir = buildTemplateWithMaven(
-			rubySassWorkspaceDir.getParentFile(), rubySassModulesDir,
-			"mvc-portlet", "foo-portlet", "com.test", mavenExecutor,
-			"-DclassName=Foo", "-Dpackage=foo.portlet",
-			"-DprojectType=workspace");
-
-		File pomXmlFile = new File(rubySassProjectDir, "pom.xml");
-
-		if (pomXmlFile.exists()) {
-			editXml(
-				pomXmlFile,
-				document -> addCssBuilderConfigurationElement(
-					document, "sassCompilerClassName", "ruby"));
-		}
-
-		String rubySassOutput = executeMaven(
-			rubySassProjectDir, mavenExecutor, MAVEN_GOAL_PACKAGE);
-
-		Assert.assertTrue(
-			rubySassOutput,
-			rubySassOutput.contains("Using Ruby Sass compiler"));
-
-		File nativeSassOutputFile = testExists(
-			nativeSassProjectDir, "target/foo-portlet-1.0.0.jar");
-		File rubySassOutputFile = testExists(
-			rubySassProjectDir, "target/foo-portlet-1.0.0.jar");
-
-		testBundlesDiff(nativeSassOutputFile, rubySassOutputFile);
-	}
-
-	@Test
-	public void testSassCompilerWorkspace() throws Exception {
-		String liferayVersion = getDefaultLiferayVersion();
-
-		File nativeSassWorkspaceDir = buildWorkspace(
-			temporaryFolder, "gradle", "nativeSassWorkspace", liferayVersion,
-			mavenExecutor);
-
-		File nativeSassModulesDir = new File(nativeSassWorkspaceDir, "modules");
-
-		File nativeSassProjectDir = buildTemplateWithGradle(
-			nativeSassModulesDir, "mvc-portlet", "foo-portlet");
-
-		Optional<String> nativeSassResult = executeGradle(
-			nativeSassModulesDir, true, _gradleDistribution,
-			":modules:foo-portlet" + GRADLE_TASK_PATH_BUILD);
-
-		String nativeSassOutput = nativeSassResult.toString();
-
-		Assert.assertTrue(
-			nativeSassOutput,
-			nativeSassOutput.contains("Using native Sass compiler"));
-
-		File rubySassWorkspaceDir = _buildTemplateWithGradle(
-			WorkspaceUtil.WORKSPACE, "rubySassWorkspace");
-
-		writeGradlePropertiesInWorkspace(
-			rubySassWorkspaceDir, "sass.compiler.class.name=ruby");
-
-		File rubySassModulesDir = new File(rubySassWorkspaceDir, "modules");
-
-		File rubySassProjectDir = buildTemplateWithGradle(
-			rubySassModulesDir, "mvc-portlet", "foo-portlet");
-
-		Optional<String> rubySassResult = executeGradle(
-			rubySassModulesDir, true, _gradleDistribution,
-			":modules:foo-portlet" + GRADLE_TASK_PATH_BUILD);
-
-		String rubySassOutput = rubySassResult.toString();
-
-		Assert.assertTrue(
-			rubySassOutput,
-			rubySassOutput.contains("Using Ruby Sass compiler"));
-
-		File nativeSassOutputFile = testExists(
-			nativeSassProjectDir, "build/libs/foo.portlet-1.0.0.jar");
-		File rubySassOutputFile = testExists(
-			rubySassProjectDir, "build/libs/foo.portlet-1.0.0.jar");
-
-		testBundlesDiff(nativeSassOutputFile, rubySassOutputFile);
-	}
-
 	@Rule
 	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
@@ -594,14 +408,6 @@ public class ProjectTemplatesTest implements BaseProjectTemplatesTestCase {
 		throws Exception {
 
 		return buildTemplateWithGradle(temporaryFolder, template, name, args);
-	}
-
-	private File _buildTemplateWithMaven(
-			String template, String name, String groupId, String... args)
-		throws Exception {
-
-		return buildTemplateWithMaven(
-			temporaryFolder, template, name, groupId, mavenExecutor, args);
 	}
 
 	private void _testBuildTemplateWithWorkspace(
