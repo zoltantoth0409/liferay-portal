7bb0bf9d05345eda7f0fb3eb4748af958ad0331e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-18 10:34:55 +0200

    LPS-112185 Adds new tests to take into account the different cases of editable values with segments experiences

---
+++ modules/apps/layout/layout-page-template-service/src/test/java/com/liferay/layout/page/template/internal/upgrade/v3_3_0/util/EditableValuesTransformerUtilTest.java
@@ -0,0 +1,176 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.internal.upgrade.v3_3_0.util;
+
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.util.FileImpl;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class EditableValuesTransformerUtilTest {
+
+	@Before
+	public void setUp() {
+		new FileUtil().setFile(new FileImpl());
+
+		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
+
+		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
+	}
+
+	@Test
+	public void testGetBackGroundImagesEditableValues() throws Exception {
+		String editableValues = _read(
+			"fragment_entry_link_background_images_editable_values.json");
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_background_images_editable_values_" +
+						"segments_experience_0.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 0));
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_background_images_editable_values_" +
+						"segments_experience_1.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 1));
+	}
+
+	@Test
+	public void testGetEditableEditableValues() throws Exception {
+		String editableValues = _read(
+			"fragment_entry_link_editable_editable_values.json");
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_editable_editable_values_segments_" +
+						"experience_0.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 0));
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_editable_editable_values_segments_" +
+						"experience_1.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 1));
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_editable_editable_values_segments_" +
+						"experience_2.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 2));
+	}
+
+	@Test
+	public void testGetEditableValuesWithoutDefaultExperience()
+		throws Exception {
+
+		String editableValues = _read(
+			"fragment_entry_link_editable_values_without_default_experience." +
+				"json");
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_editable_values_without_default_" +
+						"experience_segments_experience_0.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 0));
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_editable_values_without_default_" +
+						"experience_segments_experience_2.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 2));
+	}
+
+	@Test
+	public void testGetFreeMarkerEditableValues() throws Exception {
+		String editableValues = _read(
+			"fragment_entry_link_free_marker_editable_values.json");
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_free_marker_editable_values_" +
+						"segments_experience_0.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 0));
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_free_marker_editable_values_" +
+						"segments_experience_1.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 1));
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_free_marker_editable_values_" +
+						"segments_experience_2.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 2));
+	}
+
+	@Test
+	public void testGetMappedEditableValues() throws Exception {
+		String editableValues = _read(
+			"fragment_entry_link_mapped_editable_values.json");
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_mapped_editable_values_segments_" +
+						"experience_0.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 0));
+	}
+
+	@Test
+	public void testGetPortletEditableValues() throws Exception {
+		String editableValues = _read(
+			"fragment_entry_link_portlet_editable_values.json");
+
+		Assert.assertEquals(
+			_toString(
+				_read(
+					"fragment_entry_link_portlet_editable_values_segments_" +
+						"experience_0.json")),
+			EditableValuesTransformerUtil.getEditableValues(editableValues, 0));
+	}
+
+	private String _read(String fileName) throws Exception {
+		return new String(
+			FileUtil.getBytes(getClass(), "dependencies/" + fileName));
+	}
+
+	private String _toString(String editableValues) throws Exception {
+		JSONObject editableValuesJSONObject = JSONFactoryUtil.createJSONObject(
+			editableValues);
+
+		return editableValuesJSONObject.toJSONString();
+	}
+
+}
