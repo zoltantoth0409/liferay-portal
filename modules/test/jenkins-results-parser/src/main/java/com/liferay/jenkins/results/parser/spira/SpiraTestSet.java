d1b73f0df866a016c672f310d160f876c4c6ec8b
Michael Hashimoto <michael.hashimoto@liferay.com>
2020-04-21 13:53:30 -0700

    LRCI-1248 Add methods to create Spira Test Sets

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraTestSet.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraTestSet.java
@@ -19,11 +19,14 @@ import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil.HttpRequestMe
 import java.io.IOException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
+
+import org.apache.commons.lang.StringEscapeUtils;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -32,6 +35,51 @@ import org.json.JSONObject;
  * @author Michael Hashimoto
  */
 public class SpiraTestSet extends PathSpiraArtifact {
+
+	public static SpiraTestSet createSpiraTestSet(
+		SpiraProject spiraProject, String testSetName,
+		String testSetDescription) {
+
+		return _createSpiraTestSet(
+			spiraProject, testSetName, testSetDescription, null);
+	}
+
+	public static SpiraTestSet createSpiraTestSet(
+		SpiraProject spiraProject, String testSetName,
+		String testSetDescription, Integer parentTestSetFolderID) {
+
+		return _createSpiraTestSet(
+			spiraProject, testSetName, testSetDescription,
+			parentTestSetFolderID);
+	}
+
+	public static SpiraTestSet createSpiraTestSetByPath(
+		SpiraProject spiraProject, String testSetPath,
+		String testSetDescription) {
+
+		List<SpiraTestSet> spiraTestSets = spiraProject.getSpiraTestSetsByPath(
+			testSetPath);
+
+		if (!spiraTestSets.isEmpty()) {
+			return spiraTestSets.get(0);
+		}
+
+		String testSetName = getPathName(testSetPath);
+		String parentTestSetFolderPath = getParentPath(testSetPath);
+
+		if (parentTestSetFolderPath.isEmpty()) {
+			return createSpiraTestSet(
+				spiraProject, testSetName, testSetDescription);
+		}
+
+		SpiraTestSetFolder parentSpiraTestSetFolder =
+			SpiraTestSetFolder.createSpiraTestSetFolderByPath(
+				spiraProject, parentTestSetFolderPath);
+
+		return createSpiraTestSet(
+			spiraProject, testSetName, testSetDescription,
+			parentSpiraTestSetFolder.getID());
+	}
 
 	public SpiraTestSetFolder getParentSpiraTestSetFolder() {
 		if (_parentSpiraTestSetFolder != null) {
@@ -54,6 +102,38 @@ public class SpiraTestSet extends PathSpiraArtifact {
 			(Integer)testSetFolderID);
 
 		return _parentSpiraTestSetFolder;
+	}
+
+	public static enum Status {
+
+		BLOCKED(4), COMPLETED(3), DEFERRED(5), IN_PROGRESS(2), NOT_STARTED(1);
+
+		public Integer getID() {
+			return _id;
+		}
+
+		private Status(Integer id) {
+			_id = id;
+		}
+
+		private final Integer _id;
+
+	}
+
+	public static enum TestRunType {
+
+		AUTOMATED(2), MANUAL(1);
+
+		public Integer getID() {
+			return _id;
+		}
+
+		private TestRunType(Integer id) {
+			_id = id;
+		}
+
+		private final Integer _id;
+
 	}
 
 	protected static List<SpiraTestSet> getSpiraTestSets(
@@ -82,6 +162,52 @@ public class SpiraTestSet extends PathSpiraArtifact {
 			searchParameters);
 	}
 
+	protected SpiraTestSetTestCase assignSpiraTestCase(
+		SpiraTestCaseObject spiraTestCase) {
+
+		if (_spiraTestSetTestCases.containsKey(spiraTestCase.getID())) {
+			return _spiraTestSetTestCases.get(spiraTestCase.getID());
+		}
+
+		Map<String, String> urlPathReplacements = new HashMap<>();
+
+		SpiraProject spiraProject = getSpiraProject();
+
+		urlPathReplacements.put(
+			"project_id", String.valueOf(spiraProject.getID()));
+
+		urlPathReplacements.put(
+			"test_case_id", String.valueOf(spiraTestCase.getID()));
+
+		urlPathReplacements.put("test_set_id", String.valueOf(getID()));
+
+		JSONArray requestJSONArray = new JSONArray();
+
+		try {
+			JSONArray responseJSONArray = SpiraRestAPIUtil.requestJSONArray(
+				"projects/{project_id}/test-sets/{test_set_id}" +
+					"/test-case-mapping/{test_case_id}",
+				null, urlPathReplacements, HttpRequestMethod.POST,
+				requestJSONArray.toString());
+
+			for (int i = 0; i < responseJSONArray.length(); i++) {
+				SpiraTestSetTestCase spiraTestSetTestCase =
+					new SpiraTestSetTestCase(
+						responseJSONArray.getJSONObject(i));
+
+				_spiraTestSetTestCases.put(
+					spiraTestSetTestCase.getTestCaseID(), spiraTestSetTestCase);
+
+				return spiraTestSetTestCase;
+			}
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+
+		return null;
+	}
+
 	@Override
 	protected PathSpiraArtifact getParentSpiraArtifact() {
 		return getParentSpiraTestSetFolder();
@@ -90,6 +216,72 @@ public class SpiraTestSet extends PathSpiraArtifact {
 	protected static final String ARTIFACT_TYPE_NAME = "testset";
 
 	protected static final String ID_KEY = "TestSetId";
+
+	protected static class SpiraTestSetTestCase extends BaseSpiraArtifact {
+
+		protected Integer getTestCaseID() {
+			return jsonObject.getInt(SpiraTestCaseObject.ID_KEY);
+		}
+
+		protected static final String ID_KEY = "TestSetTestCaseId";
+
+		private SpiraTestSetTestCase(JSONObject jsonObject) {
+			super(jsonObject);
+		}
+
+	}
+
+	private static SpiraTestSet _createSpiraTestSet(
+		SpiraProject spiraProject, String testSetName,
+		String testSetDescription, Integer parentTestSetFolderID) {
+
+		List<SpiraTestSet> spiraTestSets = getSpiraTestSets(
+			spiraProject, new SearchQuery.SearchParameter("Name", testSetName),
+			new SearchQuery.SearchParameter(
+				"TestCaseFolderId", parentTestSetFolderID));
+
+		if (!spiraTestSets.isEmpty()) {
+			return spiraTestSets.get(0);
+		}
+
+		String urlPath = "projects/{project_id}/test-sets";
+
+		Map<String, String> urlPathReplacements = new HashMap<>();
+
+		urlPathReplacements.put(
+			"project_id", String.valueOf(spiraProject.getID()));
+
+		JSONObject requestJSONObject = new JSONObject();
+
+		requestJSONObject.put(
+			"Name", StringEscapeUtils.unescapeJava(testSetName));
+		requestJSONObject.put("TestRunTypeId", TestRunType.AUTOMATED.getID());
+		requestJSONObject.put("TestSetStatusId", Status.IN_PROGRESS.getID());
+
+		if ((parentTestSetFolderID != null) && (parentTestSetFolderID != 0)) {
+			requestJSONObject.put(
+				SpiraTestSetFolder.ID_KEY, parentTestSetFolderID);
+		}
+
+		if (testSetDescription == null) {
+			testSetDescription = "";
+		}
+
+		requestJSONObject.put(
+			"Description", StringEscapeUtils.unescapeJava(testSetDescription));
+
+		try {
+			JSONObject responseJSONObject = SpiraRestAPIUtil.requestJSONObject(
+				urlPath, null, urlPathReplacements, HttpRequestMethod.POST,
+				requestJSONObject.toString());
+
+			return spiraProject.getSpiraTestSetByID(
+				responseJSONObject.getInt(ID_KEY));
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+	}
 
 	private static List<JSONObject> _requestSpiraTestSets(
 		SpiraProject spiraProject,
@@ -115,8 +307,6 @@ public class SpiraTestSet extends PathSpiraArtifact {
 		for (SearchQuery.SearchParameter searchParameter : searchParameters) {
 			requestJSONArray.put(searchParameter.toFilterJSONObject());
 		}
-
-		System.out.println(requestJSONArray);
 
 		try {
 			JSONArray responseJSONArray = SpiraRestAPIUtil.requestJSONArray(
@@ -144,9 +334,39 @@ public class SpiraTestSet extends PathSpiraArtifact {
 	private SpiraTestSet(JSONObject jsonObject) {
 		super(jsonObject);
 
+		Map<String, String> urlPathReplacements = new HashMap<>();
+
+		SpiraProject spiraProject = getSpiraProject();
+
+		urlPathReplacements.put(
+			"project_id", String.valueOf(spiraProject.getID()));
+
+		urlPathReplacements.put("test_set_id", String.valueOf(getID()));
+
+		try {
+			JSONArray responseJSONArray = SpiraRestAPIUtil.requestJSONArray(
+				"projects/{project_id}/test-sets/{test_set_id}/test-cases",
+				null, urlPathReplacements, HttpRequestMethod.GET, null);
+
+			for (int i = 0; i < responseJSONArray.length(); i++) {
+				SpiraTestSetTestCase spiraTestSetTestCase =
+					new SpiraTestSetTestCase(
+						responseJSONArray.getJSONObject(i));
+
+				_spiraTestSetTestCases.put(
+					spiraTestSetTestCase.getTestCaseID(), spiraTestSetTestCase);
+			}
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+
 		cacheSpiraArtifact(SpiraTestSet.class, this);
 	}
 
 	private SpiraTestSetFolder _parentSpiraTestSetFolder;
+	private Map<Integer, SpiraTestSetTestCase> _spiraTestSetTestCases =
+		Collections.synchronizedMap(
+			new HashMap<Integer, SpiraTestSetTestCase>());
 
 }
