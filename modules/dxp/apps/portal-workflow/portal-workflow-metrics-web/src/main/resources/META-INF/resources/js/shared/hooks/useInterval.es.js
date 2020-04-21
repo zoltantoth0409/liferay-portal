1a2e29cbc80b45043263851f992f8969dfc937ec
Albino Junior <albino.junior@liferay.com>
2020-04-21 13:13:01 -0300

    LPS-107777 | LPS-110287 Create useInterval hook

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/hooks/useInterval.es.js
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {useIsMounted} from 'frontend-js-react-web';
+import {useCallback} from 'react';
+
+const useInterval = cancelOnDestroy => {
+	const isMounted = useIsMounted();
+
+	return useCallback(
+		(callback, ms) => {
+			const interval = setInterval(() => {
+				if (!cancelOnDestroy || isMounted()) {
+					callback();
+				}
+				else {
+					clearInterval(interval);
+				}
+			}, ms);
+
+			return () => clearInterval(interval);
+		},
+		[cancelOnDestroy, isMounted]
+	);
+};
+
+export {useInterval};
