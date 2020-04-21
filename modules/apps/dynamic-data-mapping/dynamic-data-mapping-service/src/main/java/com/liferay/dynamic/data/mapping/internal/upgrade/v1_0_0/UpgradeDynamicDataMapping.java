d99b4907052974087725f6e3be2a07c271c7eec0
Leonardo Barros <leonardo.barros@liferay.com>
2020-04-21 09:48:54 -0300

    LPS-111448 Rename

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_0/UpgradeDynamicDataMapping.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v1_0_0/UpgradeDynamicDataMapping.java
@@ -1871,7 +1871,7 @@ public class UpgradeDynamicDataMapping extends UpgradeProcess {
 				String fieldName, Element rootElement)
 			throws PortalException {
 
-			Value value = extractDDMFormFieldValueValues(
+			Value value = extractDDMFormFieldValue(
 				fieldName, ddmFormField, rootElement, ddmFieldsCounter);
 
 			if ((value == null) && !ddmFormField.isTransient()) {
@@ -1896,16 +1896,15 @@ public class UpgradeDynamicDataMapping extends UpgradeProcess {
 			return ddmFormFieldValue;
 		}
 
-		protected Value extractDDMFormFieldValueLocalizedValue(
-			Element dynamicElementElement, int index) {
+		protected Value extractDDMFormFieldLocalizedValue(
+			Element element, int index) {
 
-			Value value = new LocalizedValue(
-				getDefaultLocale(dynamicElementElement));
+			Value value = new LocalizedValue(getDefaultLocale(element));
 
-			Map<String, Integer> dynamicContentValuesMap = new HashMap<>();
+			Map<String, Integer> dynamicContentValues = new HashMap<>();
 
 			for (Element dynamicContentElement :
-					dynamicElementElement.elements("dynamic-content")) {
+					element.elements("dynamic-content")) {
 
 				String languageId = dynamicContentElement.attributeValue(
 					"language-id");
@@ -1925,8 +1924,7 @@ public class UpgradeDynamicDataMapping extends UpgradeProcess {
 					value.addString(locale, content);
 				}
 
-				dynamicContentValuesMap.put(
-					languageId, localizedContentIndex + 1);
+				dynamicContentValues.put(languageId, localizedContentIndex + 1);
 			}
 
 			return value;
@@ -1941,21 +1939,21 @@ public class UpgradeDynamicDataMapping extends UpgradeProcess {
 			return new UnlocalizedValue(valueString);
 		}
 
-		protected Value extractDDMFormFieldValueValues(
+		protected Value extractDDMFormFieldValue(
 			String fieldName, DDMFormField ddmFormField, Element rootElement,
 			DDMFieldsCounter ddmFieldsCounter) {
 
 			Value value = null;
 
-			Element dynamicElement = getDynamicElementElementByName(
+			Element element = getDynamicElementElementByName(
 				rootElement, fieldName);
 
 			if (Validator.isNotNull(ddmFormField.getDataType()) &&
-				(dynamicElement != null)) {
+				(element != null)) {
 
 				if (ddmFormField.isLocalizable()) {
-					value = extractDDMFormFieldValueLocalizedValue(
-						dynamicElement, ddmFieldsCounter.get(fieldName));
+					value = extractDDMFormFieldLocalizedValue(
+						element, ddmFieldsCounter.get(fieldName));
 				}
 				else {
 					value = extractDDMFormFieldValueUnlocalizedValue(
