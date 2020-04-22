8a24527bf750a029196ac3ff4ec9c62ca8e43637
drewbrokke <drew.brokke@liferay.com>
2020-04-22 14:41:56 -0500

    LPS-111626 headless-admin-user-impl - adds UserAccountBirthdayExceptionMapper

---
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/jaxrs/exception/mapper/UserAccountBirthdayExceptionMapper.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.user.internal.jaxrs.exception.mapper;
+
+import com.liferay.portal.kernel.exception.ContactBirthdayException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Drew Brokke
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.User)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.User.UserAccountBirthdayExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class UserAccountBirthdayExceptionMapper
+	extends BaseExceptionMapper<ContactBirthdayException> {
+
+	@Override
+	protected Problem getProblem(
+		ContactBirthdayException contactBirthdayException) {
+
+		return new Problem(
+			Response.Status.BAD_REQUEST, "Birthday cannot be in the future");
+	}
+
+}
