ab44307501af9e9423b14ebccbece7ab5c43208e
Preston-Crary <preston.crary@liferay.com>
2020-04-23 14:28:36 -0700

    LPS-107846 Need to do this because we do not have CT transaction AOP support yet and ResourcePermission is CT enabled.

--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/DeleteCTCollectionMVCActionCommand.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/DeleteCTCollectionMVCActionCommand.java
@@ -14,10 +14,13 @@
 
 package com.liferay.change.tracking.web.internal.portlet.action;
 
+import com.liferay.change.tracking.constants.CTConstants;
 import com.liferay.change.tracking.constants.CTPortletKeys;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
 import com.liferay.change.tracking.service.CTCollectionService;
+import com.liferay.petra.lang.SafeClosable;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -54,7 +57,12 @@ public class DeleteCTCollectionMVCActionCommand extends BaseMVCActionCommand {
 			ctCollectionId);
 
 		if (ctCollection != null) {
-			_ctCollectionService.deleteCTCollection(ctCollection);
+			try (SafeClosable safeClosable =
+					CTCollectionThreadLocal.setCTCollectionId(
+						CTConstants.CT_COLLECTION_ID_PRODUCTION)) {
+
+				_ctCollectionService.deleteCTCollection(ctCollection);
+			}
 		}
 
 		String redirect = ParamUtil.getString(actionRequest, "redirect");
--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/EditCTCollectionMVCActionCommand.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/portlet/action/EditCTCollectionMVCActionCommand.java
@@ -20,6 +20,8 @@ import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.model.CTPreferences;
 import com.liferay.change.tracking.service.CTCollectionService;
 import com.liferay.change.tracking.service.CTPreferencesLocalService;
+import com.liferay.petra.lang.SafeClosable;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
@@ -65,7 +67,10 @@ public class EditCTCollectionMVCActionCommand extends BaseMVCActionCommand {
 		String name = ParamUtil.getString(actionRequest, "name");
 		String description = ParamUtil.getString(actionRequest, "description");
 
-		try {
+		try (SafeClosable safeClosable =
+				CTCollectionThreadLocal.setCTCollectionId(
+					CTConstants.CT_COLLECTION_ID_PRODUCTION)) {
+
 			if (ctCollectionId > 0) {
 				_ctCollectionService.updateCTCollection(
 					themeDisplay.getUserId(), ctCollectionId, name,
