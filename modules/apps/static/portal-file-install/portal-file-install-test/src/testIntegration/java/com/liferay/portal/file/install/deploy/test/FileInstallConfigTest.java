38e423d348e35bccd4414f7dc8bef023ccaa9e09
tina.tian <tina.tian@liferay.com>
2020-04-22 22:45:13 -0700

    LPS-111752 Make sure database is cleaned

--- modules/apps/static/portal-file-install/portal-file-install-test/src/testIntegration/java/com/liferay/portal/file/install/deploy/test/FileInstallConfigTest.java
+++ modules/apps/static/portal-file-install/portal-file-install-test/src/testIntegration/java/com/liferay/portal/file/install/deploy/test/FileInstallConfigTest.java
@@ -17,6 +17,7 @@ package com.liferay.portal.file.install.deploy.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.test.rule.Inject;
@@ -30,6 +31,7 @@ import java.nio.file.Paths;
 import java.util.Dictionary;
 import java.util.concurrent.CountDownLatch;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -64,68 +66,71 @@ public class FileInstallConfigTest {
 		_bundleContext = bundle.getBundleContext();
 	}
 
+	@After
+	public void tearDown() throws Exception {
+		if (_configurationPath != null) {
+			Files.deleteIfExists(_configurationPath);
+		}
+
+		if (_configuration != null) {
+			ConfigurationTestUtil.deleteConfiguration(_configuration);
+		}
+	}
+
 	@Test
 	public void testConfigurationArrayValues() throws Exception {
 		String configurationPid = _CONFIGURATION_PID_PREFIX.concat(
 			".testConfigurationArrayValues");
 
-		Path path = Paths.get(
+		_configurationPath = Paths.get(
 			PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
 			configurationPid.concat(".config"));
 
-		try {
-			Configuration configuration = _createConfiguration(
-				configurationPid, path,
-				StringBundler.concat(
-					"configBooleanArray=B[\"True\",\"False\"]\n",
-					"configByteArray=X[\"1\",\"3\"]\n",
-					"configCharacterArray=C[\"A\",\"Z\"]\n",
-					"configDoubleArray=D[\"12.2\",\"12.3\"]\n",
-					"configFloatArray=F[\"12.2\",\"12.3\"]\n",
-					"configIntegerArray=I[\"20\",\"21\"]\n",
-					"configLongArray=L[\"30\",\"31\"]\n",
-					"configShortArray=S[\"2\",\"3\"]\n",
-					"configStringArray=T[\"testString\",\"testString2\"]\n",
-					"configUntypedStringArray=[\"testUntypedString\"",
-					",\"testUntypedString2\"]"));
+		_configuration = _createConfiguration(
+			configurationPid,
+			StringBundler.concat(
+				"configBooleanArray=B[\"True\",\"False\"]\n",
+				"configByteArray=X[\"1\",\"3\"]\n",
+				"configCharacterArray=C[\"A\",\"Z\"]\n",
+				"configDoubleArray=D[\"12.2\",\"12.3\"]\n",
+				"configFloatArray=F[\"12.2\",\"12.3\"]\n",
+				"configIntegerArray=I[\"20\",\"21\"]\n",
+				"configLongArray=L[\"30\",\"31\"]\n",
+				"configShortArray=S[\"2\",\"3\"]\n",
+				"configStringArray=T[\"testString\",\"testString2\"]\n",
+				"configUntypedStringArray=[\"testUntypedString\"",
+				",\"testUntypedString2\"]"));
 
-			Dictionary<String, Object> properties =
-				configuration.getProperties();
+		Dictionary<String, Object> properties = _configuration.getProperties();
 
-			Assert.assertArrayEquals(
-				new Boolean[] {true, false},
-				(Boolean[])properties.get("configBooleanArray"));
-			Assert.assertArrayEquals(
-				new Byte[] {0b1, 0b11},
-				(Byte[])properties.get("configByteArray"));
-			Assert.assertArrayEquals(
-				new Character[] {'A', 'Z'},
-				(Character[])properties.get("configCharacterArray"));
-			Assert.assertArrayEquals(
-				new Double[] {12.2D, 12.3D},
-				(Double[])properties.get("configDoubleArray"));
-			Assert.assertArrayEquals(
-				new Float[] {12.2F, 12.3F},
-				(Float[])properties.get("configFloatArray"));
-			Assert.assertArrayEquals(
-				new Integer[] {20, 21},
-				(Integer[])properties.get("configIntegerArray"));
-			Assert.assertArrayEquals(
-				new Long[] {30L, 31L},
-				(Long[])properties.get("configLongArray"));
-			Assert.assertArrayEquals(
-				new Short[] {(short)2, (short)3},
-				(Short[])properties.get("configShortArray"));
-			Assert.assertArrayEquals(
-				new String[] {"testString", "testString2"},
-				(String[])properties.get("configStringArray"));
-			Assert.assertArrayEquals(
-				new String[] {"testUntypedString", "testUntypedString2"},
-				(String[])properties.get("configUntypedStringArray"));
-		}
-		finally {
-			Files.deleteIfExists(path);
-		}
+		Assert.assertArrayEquals(
+			new Boolean[] {true, false},
+			(Boolean[])properties.get("configBooleanArray"));
+		Assert.assertArrayEquals(
+			new Byte[] {0b1, 0b11}, (Byte[])properties.get("configByteArray"));
+		Assert.assertArrayEquals(
+			new Character[] {'A', 'Z'},
+			(Character[])properties.get("configCharacterArray"));
+		Assert.assertArrayEquals(
+			new Double[] {12.2D, 12.3D},
+			(Double[])properties.get("configDoubleArray"));
+		Assert.assertArrayEquals(
+			new Float[] {12.2F, 12.3F},
+			(Float[])properties.get("configFloatArray"));
+		Assert.assertArrayEquals(
+			new Integer[] {20, 21},
+			(Integer[])properties.get("configIntegerArray"));
+		Assert.assertArrayEquals(
+			new Long[] {30L, 31L}, (Long[])properties.get("configLongArray"));
+		Assert.assertArrayEquals(
+			new Short[] {(short)2, (short)3},
+			(Short[])properties.get("configShortArray"));
+		Assert.assertArrayEquals(
+			new String[] {"testString", "testString2"},
+			(String[])properties.get("configStringArray"));
+		Assert.assertArrayEquals(
+			new String[] {"testUntypedString", "testUntypedString2"},
+			(String[])properties.get("configUntypedStringArray"));
 	}
 
 	@Test
@@ -133,43 +138,37 @@ public class FileInstallConfigTest {
 		String configurationPid = _CONFIGURATION_PID_PREFIX.concat(
 			".testConfigurationValues");
 
-		Path path = Paths.get(
+		_configurationPath = Paths.get(
 			PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
 			configurationPid.concat(".config"));
 
-		try {
-			Configuration configuration = _createConfiguration(
-				configurationPid, path,
-				StringBundler.concat(
-					"configBoolean=B\"True\"\n", "configByte=X\"1\"\n",
-					"configCharacter=C\"A\"\n", "configDouble=D\"12.2\"\n",
-					"configFloat=F\"12.2\"\n", "configInteger=I\"20\"\n",
-					"configLong=L\"30\"\n", "configShort=S\"2\"\n",
-					"configString=T\"testString\"\n",
-					"configUntypedString=\"testUntypedString\""));
+		_configuration = _createConfiguration(
+			configurationPid,
+			StringBundler.concat(
+				"configBoolean=B\"True\"\n", "configByte=X\"1\"\n",
+				"configCharacter=C\"A\"\n", "configDouble=D\"12.2\"\n",
+				"configFloat=F\"12.2\"\n", "configInteger=I\"20\"\n",
+				"configLong=L\"30\"\n", "configShort=S\"2\"\n",
+				"configString=T\"testString\"\n",
+				"configUntypedString=\"testUntypedString\""));
 
-			Dictionary<String, Object> properties =
-				configuration.getProperties();
+		Dictionary<String, Object> properties = _configuration.getProperties();
 
-			Assert.assertEquals(true, properties.get("configBoolean"));
-			Assert.assertEquals((byte)1, properties.get("configByte"));
-			Assert.assertEquals('A', properties.get("configCharacter"));
-			Assert.assertEquals(12.2D, properties.get("configDouble"));
-			Assert.assertEquals(12.2F, properties.get("configFloat"));
-			Assert.assertEquals(20, properties.get("configInteger"));
-			Assert.assertEquals(30L, properties.get("configLong"));
-			Assert.assertEquals((short)2, properties.get("configShort"));
-			Assert.assertEquals("testString", properties.get("configString"));
-			Assert.assertEquals(
-				"testUntypedString", properties.get("configUntypedString"));
-		}
-		finally {
-			Files.deleteIfExists(path);
-		}
+		Assert.assertEquals(true, properties.get("configBoolean"));
+		Assert.assertEquals((byte)1, properties.get("configByte"));
+		Assert.assertEquals('A', properties.get("configCharacter"));
+		Assert.assertEquals(12.2D, properties.get("configDouble"));
+		Assert.assertEquals(12.2F, properties.get("configFloat"));
+		Assert.assertEquals(20, properties.get("configInteger"));
+		Assert.assertEquals(30L, properties.get("configLong"));
+		Assert.assertEquals((short)2, properties.get("configShort"));
+		Assert.assertEquals("testString", properties.get("configString"));
+		Assert.assertEquals(
+			"testUntypedString", properties.get("configUntypedString"));
 	}
 
 	private Configuration _createConfiguration(
-			String configurationPid, Path path, String content)
+			String configurationPid, String content)
 		throws Exception {
 
 		CountDownLatch countDownLatch = new CountDownLatch(2);
@@ -184,7 +183,7 @@ public class FileInstallConfigTest {
 				properties);
 
 		try {
-			Files.write(path, content.getBytes());
+			Files.write(_configurationPath, content.getBytes());
 
 			countDownLatch.await();
 		}
@@ -203,5 +202,7 @@ public class FileInstallConfigTest {
 	private static ConfigurationAdmin _configurationAdmin;
 
 	private BundleContext _bundleContext;
+	private Configuration _configuration;
+	private Path _configurationPath;
 
 }
