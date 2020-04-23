33424aade7dff99856b8fb2250f01c742b8f82fb
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-04-23 16:30:12 +0200

    LPS-112397 Using friendly url entries as backup to make old urls still redirect to the right layout

---
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/service/FriendlyURLLayoutLocalServiceWrapper.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.friendly.url.internal.service;
+
+import com.liferay.friendly.url.model.FriendlyURLEntry;
+import com.liferay.friendly.url.service.FriendlyURLEntryLocalService;
+import com.liferay.portal.kernel.exception.NoSuchLayoutException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalServiceWrapper;
+import com.liferay.portal.kernel.service.ServiceWrapper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(immediate = true, service = ServiceWrapper.class)
+public class FriendlyURLLayoutLocalServiceWrapper
+	extends LayoutLocalServiceWrapper {
+
+	public FriendlyURLLayoutLocalServiceWrapper() {
+		super(null);
+	}
+
+	public FriendlyURLLayoutLocalServiceWrapper(
+		LayoutLocalService layoutLocalService) {
+
+		super(layoutLocalService);
+	}
+
+	@Override
+	public Layout fetchLayoutByFriendlyURL(
+		long groupId, boolean privateLayout, String friendlyURL) {
+
+		Layout layout = super.fetchLayoutByFriendlyURL(
+			groupId, privateLayout, friendlyURL);
+
+		if (layout != null) {
+			return layout;
+		}
+
+		return _fetchLayoutByFriendlyURL(groupId, friendlyURL);
+	}
+
+	@Override
+	public Layout getFriendlyURLLayout(
+			long groupId, boolean privateLayout, String friendlyURL)
+		throws PortalException {
+
+		try {
+			return super.getFriendlyURLLayout(
+				groupId, privateLayout, friendlyURL);
+		}
+		catch (NoSuchLayoutException noSuchLayoutException) {
+			Layout layout = _fetchLayoutByFriendlyURL(groupId, friendlyURL);
+
+			if (layout != null) {
+				return layout;
+			}
+
+			throw noSuchLayoutException;
+		}
+	}
+
+	private Layout _fetchLayoutByFriendlyURL(long groupId, String friendlyURL) {
+		FriendlyURLEntry friendlyURLEntry =
+			_friendlyURLEntryLocalService.fetchFriendlyURLEntry(
+				groupId, Layout.class, friendlyURL);
+
+		if (friendlyURLEntry != null) {
+			Layout layout = fetchLayout(friendlyURLEntry.getClassPK());
+
+			if (layout != null) {
+				return layout;
+			}
+		}
+
+		return null;
+	}
+
+	@Reference
+	private FriendlyURLEntryLocalService _friendlyURLEntryLocalService;
+
+}
