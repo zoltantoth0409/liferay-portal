28ffb8711edb58b6d6611e6be98e34cfd930903d
Adolfo Pérez <adolfo.perez@liferay.com>
2020-04-24 13:23:21 +0200

    LPS-112383 Assign new role to connected site members

--- modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/security/permission/contributor/DepotMemberRoleContributor.java
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/security/permission/contributor/DepotMemberRoleContributor.java
@@ -15,6 +15,9 @@
 package com.liferay.depot.internal.security.permission.contributor;
 
 import com.liferay.depot.constants.DepotRolesConstants;
+import com.liferay.depot.model.DepotEntryGroupRel;
+import com.liferay.depot.service.DepotEntryGroupRelLocalService;
+import com.liferay.depot.service.DepotEntryLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -27,6 +30,7 @@ import com.liferay.portal.kernel.security.permission.contributor.RoleContributor
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 
+import java.util.List;
 import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
@@ -40,25 +44,47 @@ public class DepotMemberRoleContributor implements RoleContributor {
 
 	@Override
 	public void contribute(RoleCollection roleCollection) {
-		if (roleCollection.getGroupId() <= 0) {
-			return;
-		}
-
 		try {
+			if (roleCollection.getGroupId() <= 0) {
+				return;
+			}
+
 			Group group = _groupLocalService.getGroup(
 				roleCollection.getGroupId());
 
+			if (!Objects.equals(GroupConstants.TYPE_DEPOT, group.getType())) {
+				return;
+			}
+
 			UserBag userBag = roleCollection.getUserBag();
 
-			if (Objects.equals(GroupConstants.TYPE_DEPOT, group.getType()) &&
-				(userBag.hasUserGroup(group) ||
-				 userBag.hasUserOrgGroup(group))) {
-
+			if (userBag.hasUserGroup(group)) {
 				Role role = _roleLocalService.getRole(
 					group.getCompanyId(),
 					DepotRolesConstants.ASSET_LIBRARY_MEMBER);
 
 				roleCollection.addRoleId(role.getRoleId());
+			}
+
+			List<DepotEntryGroupRel> depotEntryGroupRels =
+				_depotEntryGroupRelLocalService.getDepotEntryGroupRels(
+					_depotEntryLocalService.getGroupDepotEntry(
+						group.getGroupId()));
+
+			for (DepotEntryGroupRel depotEntryGroupRel : depotEntryGroupRels) {
+				if (userBag.hasUserGroup(
+						_groupLocalService.getGroup(
+							depotEntryGroupRel.getToGroupId()))) {
+
+					Role role = _roleLocalService.getRole(
+						group.getCompanyId(),
+						DepotRolesConstants.
+							ASSET_LIBRARY_CONNECTED_SITE_MEMBER);
+
+					roleCollection.addRoleId(role.getRoleId());
+
+					break;
+				}
 			}
 		}
 		catch (PortalException portalException) {
@@ -68,6 +94,12 @@ public class DepotMemberRoleContributor implements RoleContributor {
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DepotMemberRoleContributor.class);
+
+	@Reference
+	private DepotEntryGroupRelLocalService _depotEntryGroupRelLocalService;
+
+	@Reference
+	private DepotEntryLocalService _depotEntryLocalService;
 
 	@Reference
 	private GroupLocalService _groupLocalService;
