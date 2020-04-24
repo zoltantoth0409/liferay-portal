3e435e40089473d031db2111211f39c67e54b471
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-24 17:21:03 +0200

    LPS-112188 Adds tests to check if the _mergeEditableValuesJSONObject is returning the correct values when we have more elements in default editable values or we have more elements in editables values.
    Example:
    
    - If we have the same elements in editable values and default editable values
    
    	editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "8a1ef6dc-5b65-4792-afd2-90c0fc504f11",
    			"dropZone2": "1ac36b9b-3dde-4e20-9bc3-924da2d8bd5e",
    			"dropZone3": "7e83ae5e-2a77-43c5-8bc4-5393bcf1fc2a",
    			"dropZone4": "88bc2bf3-5895-4045-8d2b-7ab31d30f278"
    		}
    	}
    
    	default_editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "",
    			"dropZone2": "",
    			"dropZone3": "",
    			"dropZone4": ""
    		}
    	}
    
    	merged_editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "8a1ef6dc-5b65-4792-afd2-90c0fc504f11",
    			"dropZone2": "1ac36b9b-3dde-4e20-9bc3-924da2d8bd5e",
    			"dropZone3": "7e83ae5e-2a77-43c5-8bc4-5393bcf1fc2a",
    			"dropZone4": "88bc2bf3-5895-4045-8d2b-7ab31d30f278"
    		}
    	}
    
    - If we have more elements in editable values
    
    	editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "8a1ef6dc-5b65-4792-afd2-90c0fc504f11",
    			"dropZone2": "1ac36b9b-3dde-4e20-9bc3-924da2d8bd5e",
    			"dropZone3": "7e83ae5e-2a77-43c5-8bc4-5393bcf1fc2a",
    			"dropZone4": "88bc2bf3-5895-4045-8d2b-7ab31d30f278"
    		}
    	}
    
    	default_editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "",
    			"dropZone2": "",
    			"dropZone3": ""
    		}
    	}
    
    	merged_editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "8a1ef6dc-5b65-4792-afd2-90c0fc504f11",
    			"dropZone2": "1ac36b9b-3dde-4e20-9bc3-924da2d8bd5e",
    			"dropZone3": "7e83ae5e-2a77-43c5-8bc4-5393bcf1fc2a",
    			"dropZone4": "88bc2bf3-5895-4045-8d2b-7ab31d30f278"
    		}
    	}
    
    - If we have more elements in default editable values
    
    	editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "8a1ef6dc-5b65-4792-afd2-90c0fc504f11",
    			"dropZone2": "1ac36b9b-3dde-4e20-9bc3-924da2d8bd5e",
    			"dropZone3": "7e83ae5e-2a77-43c5-8bc4-5393bcf1fc2a"
    		}
    	}
    
    	default_editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "",
    			"dropZone2": "",
    			"dropZone3": "",
    			"dropZone4": ""
    		}
    	}
    
    	merged_editable_values:
    
    	{
    		"com.liferay.fragment.entry.processor.drop.zone.DropZoneFragmentEntryProcessor": {
    			"dropZone1": "8a1ef6dc-5b65-4792-afd2-90c0fc504f11",
    			"dropZone2": "1ac36b9b-3dde-4e20-9bc3-924da2d8bd5e",
    			"dropZone3": "7e83ae5e-2a77-43c5-8bc4-5393bcf1fc2a",
    			"dropZone4": ""
    		}
    	}

---
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/UpdateConfigurationValuesMVCActionCommandTest.java
@@ -0,0 +1,140 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.portlet.action.test;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class UpdateConfigurationValuesMVCActionCommandTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() {
+		_objectMapper = new ObjectMapper() {
+			{
+				configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+			}
+		};
+	}
+
+	@Test
+	public void testMergeEditableValuesJSONObject() throws Exception {
+		JSONObject defaultEditableValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_read("default_editable_values.json"));
+
+		JSONObject mergeEditableValuesJSONObject = ReflectionTestUtil.invoke(
+			_mvcActionCommand, "_mergeEditableValuesJSONObject",
+			new Class<?>[] {JSONObject.class, String.class},
+			defaultEditableValuesJSONObject, _read("editable_values.json"));
+
+		Assert.assertEquals(
+			_objectMapper.readTree(
+				_read("merged_editable_values_with_same_elements.json")),
+			_objectMapper.readTree(mergeEditableValuesJSONObject.toString()));
+	}
+
+	@Test
+	public void testMergeEditableValuesJSONObjectWithNewElementDefaultEditableValues()
+		throws Exception {
+
+		JSONObject defaultEditableValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_read("default_editable_values_with_new_element.json"));
+
+		JSONObject mergeEditableValuesJSONObject = ReflectionTestUtil.invoke(
+			_mvcActionCommand, "_mergeEditableValuesJSONObject",
+			new Class<?>[] {JSONObject.class, String.class},
+			defaultEditableValuesJSONObject, _read("editable_values.json"));
+
+		Assert.assertEquals(
+			_objectMapper.readTree(
+				_read(
+					"merged_editable_values_with_new_element_default_" +
+						"editable_values.json")),
+			_objectMapper.readTree(mergeEditableValuesJSONObject.toString()));
+	}
+
+	@Test
+	public void testMergeEditableValuesJSONObjectWithNewElementEditableValues()
+		throws Exception {
+
+		JSONObject defaultEditableValuesJSONObject =
+			JSONFactoryUtil.createJSONObject(
+				_read("default_editable_values.json"));
+
+		JSONObject mergeEditableValuesJSONObject = ReflectionTestUtil.invoke(
+			_mvcActionCommand, "_mergeEditableValuesJSONObject",
+			new Class<?>[] {JSONObject.class, String.class},
+			defaultEditableValuesJSONObject,
+			_read("editable_values_with_new_element.json"));
+
+		Assert.assertEquals(
+			_objectMapper.readTree(
+				_read(
+					"merged_editable_values_with_new_element_editable_values." +
+						"json")),
+			_objectMapper.readTree(mergeEditableValuesJSONObject.toString()));
+	}
+
+	private String _read(String fileName) throws Exception {
+		Class<?> clazz = getClass();
+
+		InputStream inputStream = clazz.getResourceAsStream(
+			"dependencies/" + fileName);
+
+		return StringUtil.read(inputStream);
+	}
+
+	@Inject(
+		filter = "mvc.command.name=/content_layout/update_configuration_values"
+	)
+	private MVCActionCommand _mvcActionCommand;
+
+	private ObjectMapper _objectMapper;
+
+}
