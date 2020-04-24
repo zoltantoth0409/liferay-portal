e9c4ec6a9484848234ab5f4a972d2b0db7dc42f5
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-04-24 23:28:13 +0200

    LPS-112372 Provide a helper provider class that allows obtaining all InfoItemFieldReaders that have been registered with a given className

---
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/fields/provider/ClassNameInfoItemFieldsProviderImpl.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.internal.fields.provider;
+
+import com.liferay.info.fields.InfoItemField;
+import com.liferay.info.fields.reader.InfoItemFieldReader;
+import com.liferay.info.fields.provider.ClassNameInfoItemFieldsProvider;
+import com.liferay.info.fields.reader.InfoItemFieldReaderTracker;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = ClassNameInfoItemFieldsProvider.class)
+public class ClassNameInfoItemFieldsProviderImpl implements
+	ClassNameInfoItemFieldsProvider {
+
+	@Override
+	public List<InfoItemField> getInfoItemFields(
+		String className) {
+		
+		List<InfoItemField> infoItemFields = new ArrayList<>();
+
+		List<InfoItemFieldReader> infoItemFieldReaders =
+			_infoItemFieldReaderTracker.getInfoItemFieldReader(
+				className);
+
+		for (InfoItemFieldReader infoItemFieldReader :
+			infoItemFieldReaders) {
+
+			infoItemFields.add(
+				infoItemFieldReader.getInfoItemField());
+		}
+
+		return infoItemFields;
+	}
+	
+	@Reference
+	private InfoItemFieldReaderTracker _infoItemFieldReaderTracker;
+
+}
