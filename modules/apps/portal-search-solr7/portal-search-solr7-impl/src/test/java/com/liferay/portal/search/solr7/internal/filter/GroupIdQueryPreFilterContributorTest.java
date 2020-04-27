5d5e12b47de65bdff762f2084e65e04fe63d6c0e
Joshua Cords <joshua.cords@liferay.com>
2020-04-27 15:23:27 -0700

    LPS-112407 portal-search-solr7-impl: add test
    https://issues.liferay.com/browse/LPS-112407

---
+++ modules/apps/portal-search-solr7/portal-search-solr7-impl/src/test/java/com/liferay/portal/search/solr7/internal/filter/GroupIdQueryPreFilterContributorTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.solr7.internal.filter;
+
+import com.liferay.portal.search.solr7.internal.SolrIndexingFixture;
+import com.liferay.portal.search.test.util.filter.groupid.BaseGroupIdQueryPreFilterContributorTestCase;
+import com.liferay.portal.search.test.util.indexing.IndexingFixture;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Joshua Cords
+ */
+public class GroupIdQueryPreFilterContributorTest
+	extends BaseGroupIdQueryPreFilterContributorTestCase {
+
+	@Ignore
+	@Test
+	public void testScopeEverythingWithInactiveGroups() {
+	}
+
+	@Override
+	protected IndexingFixture createIndexingFixture() throws Exception {
+		return new SolrIndexingFixture();
+	}
+
+}
