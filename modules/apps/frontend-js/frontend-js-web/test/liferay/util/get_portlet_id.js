770aba7589686394c540271d5a1fa081e2db5875
Krešimir Čoko <kresimir.coko@liferay.com>
2020-04-27 08:57:55 +0200

    LPS-101619 Remove usage of 'use-strict' in the test file and add two more tests for getPortletId

--- modules/apps/frontend-js/frontend-js-web/test/liferay/util/get_portlet_id.js
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/get_portlet_id.js
@@ -12,12 +12,20 @@
  * details.
  */
 
-'use strict';
-
 import getPortletId from '../../../src/main/resources/META-INF/resources/liferay/util/get_portlet_id';
 
 describe('Liferay.Util.getPortletId', () => {
 	it('returns the portlet id', () => {
 		expect(getPortletId('p_p_id_fooBar_')).toEqual('fooBar');
+	});
+
+	it('returns the portlet id without a leading p_p_id', () => {
+		expect(getPortletId('_fooBar_')).toEqual('fooBar');
+	});
+
+	it('returns the input as-is when no portlet ID can be extracted', () => {
+		expect(getPortletId('not-a-well-formed-id')).toBe(
+			'not-a-well-formed-id'
+		);
 	});
 });
