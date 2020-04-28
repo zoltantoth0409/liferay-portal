59d06f2ef2ee4d284364162fd7c7166107760776
Leonardo Barros <leonardo.barros@liferay.com>
2020-04-28 15:06:21 -0300

    LPS-112677 Add exception mapper

---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/jaxrs/exception/mapper/DataDefinitionMustSetValidContentTypeValidationExceptionMapper.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.data.engine.rest.resource.exception.DataDefinitionValidationException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Leonardo Barros
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Data.Engine.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Data.Engine.REST.DataDefinitionMustSetValidContentTypeValidationExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class DataDefinitionMustSetValidContentTypeValidationExceptionMapper
+	extends BaseExceptionMapper
+		<DataDefinitionValidationException.MustSetValidContentType> {
+
+	@Override
+	protected Problem getProblem(
+		DataDefinitionValidationException.MustSetValidContentType
+			mustSetValidContentType) {
+
+		return new Problem(
+			mustSetValidContentType.getContentType(),
+			Response.Status.BAD_REQUEST, mustSetValidContentType.getMessage(),
+			"MustSetValidContentType");
+	}
+
+}
