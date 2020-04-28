5c522a0d27cdaa1d59c8397bb764372a5f3956b8
Leonardo Barros <leonardo.barros@liferay.com>
2020-04-28 13:40:52 +0200

    LPS-109123 Define the structure layout as the default one by setting its classNameId and structureLayoutKey equals to structure's attributes

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMStructureLocalServiceImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMStructureLocalServiceImpl.java
@@ -44,6 +44,7 @@ import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.DDMFormLayout;
 import com.liferay.dynamic.data.mapping.model.DDMFormRule;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.DDMStructureLayout;
 import com.liferay.dynamic.data.mapping.model.DDMStructureVersion;
 import com.liferay.dynamic.data.mapping.model.DDMTemplate;
 import com.liferay.dynamic.data.mapping.security.permission.DDMPermissionSupport;
@@ -216,9 +217,16 @@ public class DDMStructureLocalServiceImpl
 		// Structure layout
 
 		if (ddmFormLayout != null) {
-			_ddmStructureLayoutLocalService.addStructureLayout(
-				userId, groupId, structureVersion.getStructureVersionId(),
-				ddmFormLayout, serviceContext);
+			DDMStructureLayout structureLayout =
+				_ddmStructureLayoutLocalService.addStructureLayout(
+					userId, groupId, structureVersion.getStructureVersionId(),
+					ddmFormLayout, serviceContext);
+
+			structureLayout.setClassNameId(structure.getClassNameId());
+			structureLayout.setStructureLayoutKey(structure.getStructureKey());
+
+			_ddmStructureLayoutLocalService.updateDDMStructureLayout(
+				structureLayout);
 		}
 
 		// Data provider instance links
