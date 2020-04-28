d5d02f6ab5040f740778483eb3528fa9c02e4415
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-28 14:05:17 +0200

    LPS-112595 Deprecates taglib

--- util-taglib/src/com/liferay/taglib/ui/SitesDirectoryTag.java
+++ util-taglib/src/com/liferay/taglib/ui/SitesDirectoryTag.java
@@ -20,7 +20,9 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Sergio González
+ * @deprecated As of Athanasius (7.3.x), with no direct replacement
  */
+@Deprecated
 public class SitesDirectoryTag extends IncludeTag {
 
 	public static final String SITES_CHILDREN = "children";
