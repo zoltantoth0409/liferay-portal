c56bc22f18326f4825a32bb7359ed5016d2fd360
Adriano Interaminense <adriano.interaminense@gmail.com>
2020-04-28 16:49:50 -0300

    LPS-112399 Overwrite default js-setup config in the AppBuilder

---
+++ modules/apps/app-builder/app-builder-web/jest-setup.config.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+const languageMap = {
+	default: 'Default',
+	'not-translated': 'Not Translated',
+	translated: 'Translated',
+};
+
+window.Liferay = {
+	...(window.Liferay || {}),
+	Language: {
+		...(window.Liferay.Language || {}),
+		available: {
+			ar_SA: 'Arabic (Saudi Arabia)',
+			ca_ES: 'Catalan (Spain)',
+			de_DE: 'German (Germany)',
+			en_US: 'English (United States)',
+			es_ES: 'Spanish (Spain)',
+			fi_FI: 'Finnish (Finland)',
+			fr_FR: 'French (France)',
+			hu_HU: 'Hungarian (Hungary)',
+			ja_JP: 'Japanese (Japan)',
+			nl_NL: 'Dutch (Netherlands)',
+			pt_BR: 'Portuguese (Brazil)',
+			sv_SE: 'Swedish (Sweden)',
+			zh_CN: 'Chinese (China)',
+		},
+		get: key => {
+			if (languageMap[key]) {
+				return languageMap[key];
+			}
+
+			return key;
+		},
+	},
+	ThemeDisplay: {
+		...(window.Liferay.ThemeDisplay || {}),
+		getLanguageId: () => 'en_US',
+	},
+};
