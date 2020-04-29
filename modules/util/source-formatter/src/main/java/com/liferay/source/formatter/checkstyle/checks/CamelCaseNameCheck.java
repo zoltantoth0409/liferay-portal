bc49625e46a568a371ec4856925ef701f93f0513
Hugo Huijser <hugo.huijser@liferay.com>
2020-04-29 13:46:07 +0800

    LPS-105380 Allow 'nonProxyHosts' because of System property 'nonProxyHosts'

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/CamelCaseNameCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/CamelCaseNameCheck.java
@@ -37,7 +37,7 @@ public class CamelCaseNameCheck extends BaseCheck {
 
 	@Override
 	protected void doVisitToken(DetailAST detailAST) {
-		_checkName(detailAST, "non");
+		_checkName(detailAST, "non", "nonProxyHost");
 		_checkName(detailAST, "re", "reCaptcha");
 		_checkName(detailAST, "sub", "subSelect");
 	}
