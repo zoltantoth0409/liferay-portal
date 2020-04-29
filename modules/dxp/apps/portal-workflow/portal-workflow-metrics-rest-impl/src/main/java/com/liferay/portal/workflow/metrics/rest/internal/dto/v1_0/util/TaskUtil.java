a3e08074dc8366b728b4f2461e7a31c8fbfdb504
Inácio Nery <inacio.nery@liferay.com>
2020-04-29 11:57:16 -0300

    LPS-112705 Set assetTitle_i18n and assetType_i18n for Task

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/dto/v1_0/util/TaskUtil.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-impl/src/main/java/com/liferay/portal/workflow/metrics/rest/internal/dto/v1_0/util/TaskUtil.java
@@ -14,22 +14,30 @@
 
 package com.liferay.portal.workflow.metrics.rest.internal.dto.v1_0.util;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.document.Field;
 import com.liferay.portal.workflow.metrics.rest.dto.v1_0.Task;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Rafael Praxedes
@@ -40,12 +48,13 @@ public class TaskUtil {
 		Document document, Language language, Locale locale, Portal portal,
 		ResourceBundle resourceBundle, Function<Long, User> userFunction) {
 
+		Map<String, String> assetTitleMap = _createMap(document, "assetTitle");
+		Map<String, String> assetTypeMap = _createMap(document, "assetType");
+
 		return new Task() {
 			{
-				assetTitle = document.getString(
-					_getLocalizedName(locale, "assetTitle"));
-				assetType = document.getString(
-					_getLocalizedName(locale, "assetType"));
+				assetTitle_i18n = assetTitleMap;
+				assetType_i18n = assetTypeMap;
 				className = document.getString("className");
 				classPK = document.getLong("classPK");
 				completed = document.getBoolean("completed");
@@ -93,8 +102,31 @@ public class TaskUtil {
 		};
 	}
 
-	private static String _getLocalizedName(Locale locale, String name) {
-		return Field.getLocalizedName(locale, name);
+	private static Map<String, String> _createMap(
+		Document document, String fieldName) {
+
+		return Stream.of(
+			document.getFields()
+		).map(
+			Map::entrySet
+		).flatMap(
+			Collection::stream
+		).filter(
+			entry ->
+				StringUtil.startsWith(
+					entry.getKey(), fieldName + StringPool.UNDERLINE) &&
+				!StringUtil.endsWith(entry.getKey(), "_sortable")
+		).collect(
+			Collectors.toMap(
+				entry -> _toLanguageTag(
+					StringUtil.removeSubstring(
+						entry.getKey(), fieldName + StringPool.UNDERLINE)),
+				entry -> {
+					Field field = entry.getValue();
+
+					return String.valueOf(field.getValue());
+				})
+		);
 	}
 
 	private static Date _parseDate(String dateString) {
@@ -109,6 +141,12 @@ public class TaskUtil {
 
 			return null;
 		}
+	}
+
+	private static String _toLanguageTag(String languageId) {
+		Locale locale = LocaleUtil.fromLanguageId(languageId);
+
+		return locale.toLanguageTag();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(TaskUtil.class);
