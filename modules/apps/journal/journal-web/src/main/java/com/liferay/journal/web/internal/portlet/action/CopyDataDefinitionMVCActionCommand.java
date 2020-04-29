630e0171b95d3ca569dbf8cbd37628863b2d70a3
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-04-29 09:50:21 +0200

    LPS-111614 Copy from CopyDDMStructureMVCActionCommand, no logic changes

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/CopyDataDefinitionMVCActionCommand.java
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.portlet.action;
+
+import com.liferay.data.engine.rest.dto.v2_0.DataDefinition;
+import com.liferay.data.engine.rest.resource.v2_0.DataDefinitionResource;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.DDMStructureLayout;
+import com.liferay.dynamic.data.mapping.model.DDMTemplateConstants;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLayoutLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMStructureService;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateService;
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + JournalPortletKeys.JOURNAL,
+		"mvc.command.name=/journal/copy_ddm_structure"
+	},
+	service = MVCActionCommand.class
+)
+public class CopyDataDefinitionMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public boolean processAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws PortletException {
+
+		return super.processAction(actionRequest, actionResponse);
+	}
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long ddmStructureId = ParamUtil.getLong(
+			actionRequest, "ddmStructureId");
+
+		Map<Locale, String> nameMap = LocalizationUtil.getLocalizationMap(
+			actionRequest, "name");
+		Map<Locale, String> descriptionMap =
+			LocalizationUtil.getLocalizationMap(actionRequest, "description");
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			DDMStructure.class.getName(), actionRequest);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		DDMStructure existingDDMStructure = _ddmStructureService.getStructure(
+			ddmStructureId);
+
+		DDMStructureLayout ddmStructureLayout =
+			_ddmStructureLayoutLocalService.fetchStructureLayout(
+				existingDDMStructure.getGroupId(),
+				existingDDMStructure.getClassNameId(),
+				existingDDMStructure.getStructureKey());
+
+		DDMStructure ddmStructure = null;
+
+		if (ddmStructureLayout != null) {
+			DataDefinitionResource dataDefinitionResource =
+				DataDefinitionResource.builder(
+				).user(
+					themeDisplay.getUser()
+				).build();
+
+			DataDefinition dataDefinition =
+				dataDefinitionResource.getDataDefinition(ddmStructureId);
+
+			dataDefinition.setDataDefinitionKey(StringPool.BLANK);
+
+			Map<String, Object> newNameMap = new HashMap<>();
+
+			for (Map.Entry<Locale, String> entry : nameMap.entrySet()) {
+				newNameMap.put(
+					LocaleUtil.toLanguageId(entry.getKey()), entry.getValue());
+			}
+
+			dataDefinition.setName(newNameMap);
+
+			Map<String, Object> newDescriptionMap = new HashMap<>();
+
+			for (Map.Entry<Locale, String> entry : descriptionMap.entrySet()) {
+				newDescriptionMap.put(
+					LocaleUtil.toLanguageId(entry.getKey()), entry.getValue());
+			}
+
+			dataDefinition.setDescription(newDescriptionMap);
+
+			DataDefinition newDataDefinition =
+				dataDefinitionResource.postSiteDataDefinitionByContentType(
+					themeDisplay.getScopeGroupId(), "journal", dataDefinition);
+
+			ddmStructure = _ddmStructureService.getStructure(
+				themeDisplay.getScopeGroupId(),
+				_portal.getClassNameId(JournalArticle.class),
+				newDataDefinition.getDataDefinitionKey());
+		}
+		else {
+			ddmStructure = _ddmStructureService.copyStructure(
+				ddmStructureId, nameMap, descriptionMap, serviceContext);
+		}
+
+		boolean copyTemplates = ParamUtil.getBoolean(
+			actionRequest, "copyTemplates");
+
+		if (copyTemplates) {
+			_ddmTemplateService.copyTemplates(
+				_portal.getClassNameId(DDMStructure.class), ddmStructureId,
+				_portal.getClassNameId(JournalArticle.class),
+				ddmStructure.getStructureId(),
+				DDMTemplateConstants.TEMPLATE_TYPE_DISPLAY, serviceContext);
+		}
+	}
+
+	@Reference
+	private DDMStructureLayoutLocalService _ddmStructureLayoutLocalService;
+
+	@Reference
+	private DDMStructureService _ddmStructureService;
+
+	@Reference
+	private DDMTemplateService _ddmTemplateService;
+
+	@Reference
+	private Portal _portal;
+
+}
