4fc757466f393984a6cc854c1b9986a377f64467
drewbrokke <drew.brokke@liferay.com>
2020-04-29 15:40:32 -0500

    LPS-112667 roles-admin-web - removes if check now that the basic RoleTypeContributors are implemented

--- modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/display/context/RoleDisplayContext.java
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/display/context/RoleDisplayContext.java
@@ -19,7 +19,6 @@ import com.liferay.frontend.taglib.clay.servlet.taglib.util.NavigationItemList;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.NavigationItemListBuilder;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Role;
-import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.PortletURLUtil;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
@@ -161,17 +160,6 @@ public class RoleDisplayContext {
 	}
 
 	public boolean isAutomaticallyAssigned(Role role) {
-		String name = role.getName();
-
-		if (name.equals(RoleConstants.GUEST) ||
-			name.equals(RoleConstants.ORGANIZATION_USER) ||
-			name.equals(RoleConstants.OWNER) ||
-			name.equals(RoleConstants.SITE_MEMBER) ||
-			name.equals(RoleConstants.USER)) {
-
-			return true;
-		}
-
 		List<RoleTypeContributor> roleTypeContributors =
 			RoleTypeContributorRetrieverUtil.getRoleTypeContributors(
 				_httpServletRequest);
