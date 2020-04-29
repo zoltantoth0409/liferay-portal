f744aeb44363160e96a46e45cf15cbb03fb164f0
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-04-29 14:54:03 +0200

    LPS-112722 Catch possible exceptions.

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/portlet/action/GetAnalyticsReportsTotalReadsMVCResourceCommand.java
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/portlet/action/GetAnalyticsReportsTotalReadsMVCResourceCommand.java
@@ -21,11 +21,18 @@ import com.liferay.layout.seo.kernel.LayoutSEOLinkManager;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.ResourceBundle;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -59,15 +66,39 @@ public class GetAnalyticsReportsTotalReadsMVCResourceCommand
 			_portal.getHttpServletRequest(resourceRequest), _language,
 			_layoutSEOLinkManager, _portal);
 
-		JSONObject jsonObject = JSONUtil.put(
-			"analyticsReportsTotalReads",
-			analyticsReportsDataProvider.getTotalReads(
-				_portal.getCompanyId(resourceRequest),
-				canonicalURLProvider.getCanonicalURL()));
+		try {
+			JSONObject jsonObject = JSONUtil.put(
+				"analyticsReportsTotalReads",
+				analyticsReportsDataProvider.getTotalReads(
+					_portal.getCompanyId(resourceRequest),
+					canonicalURLProvider.getCanonicalURL()));
 
-		JSONPortletResponseUtil.writeJSON(
-			resourceRequest, resourceResponse, jsonObject);
+			JSONPortletResponseUtil.writeJSON(
+				resourceRequest, resourceResponse, jsonObject);
+		}
+		catch (Exception exception) {
+			if (_log.isInfoEnabled()) {
+				_log.info(exception, exception);
+			}
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)resourceRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+				themeDisplay.getLocale(), getClass());
+
+			JSONPortletResponseUtil.writeJSON(
+				resourceRequest, resourceResponse,
+				JSONUtil.put(
+					"error",
+					ResourceBundleUtil.getString(
+						resourceBundle, "an-unexpected-error-occurred")));
+		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		GetAnalyticsReportsTotalReadsMVCResourceCommand.class);
 
 	@Reference
 	private Http _http;
