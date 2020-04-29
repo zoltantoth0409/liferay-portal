63da2884f64c9193819a4eec8b90bcddeeee25fe
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-04-29 10:04:58 +0200

    LPS-112721 Add AnalyticsReportsConfiguration

---
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/configuration/AnalyticsReportsConfiguration.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.analytics.reports.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Cristina González
+ */
+@ExtendedObjectClassDefinition(generateUI = false)
+@Meta.OCD(
+	id = "com.liferay.analytics.reports.web.internal.configuration.AnalyticsReportsConfiguration"
+)
+public interface AnalyticsReportsConfiguration {
+
+	@Meta.AD(deflt = "false", required = false)
+	public boolean readsEnabled();
+
+	@Meta.AD(deflt = "false", required = false)
+	public boolean trafficSourcesEnabled();
+
+}
