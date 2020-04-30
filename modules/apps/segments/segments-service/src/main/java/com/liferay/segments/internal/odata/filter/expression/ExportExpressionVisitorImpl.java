b483d0092216215b8e63d53e189286b4080439ba
Vendel Toreki <vendel.toreki@liferay.com>
2020-04-30 15:30:54 +0200

    LPS-112733 Use StagedModelDataHandlerUtil.exportReferenceStagedModel() method to correctly export the classed models used by user segment expressions. The reason is that Organization, Role, Site, Tag, User and UserGroup references should be exported as "missing-reference", while Team should be included in the export as it is in the same scope as the SegmentEntry itself.

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/odata/filter/expression/ExportExpressionVisitorImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/odata/filter/expression/ExportExpressionVisitorImpl.java
@@ -16,7 +16,10 @@ package com.liferay.segments.internal.odata.filter.expression;
 
 import com.liferay.expando.kernel.model.ExpandoColumn;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.ClassedModel;
 import com.liferay.portal.kernel.model.StagedModel;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -195,13 +198,23 @@ public class ExportExpressionVisitorImpl implements ExpressionVisitor<Object> {
 			return;
 		}
 
-		Element entityElement = _portletDataContext.getExportDataElement(
-			_stagedModel);
-
-		_portletDataContext.addReferenceElement(
-			_stagedModel, entityElement, classedModel,
-			PortletDataContext.REFERENCE_TYPE_DEPENDENCY, false);
+		try {
+			StagedModelDataHandlerUtil.exportReferenceStagedModel(
+				_portletDataContext, _stagedModel, (StagedModel)classedModel,
+				PortletDataContext.REFERENCE_TYPE_DEPENDENCY);
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to export classed model " +
+						classedModel.getModelClassName(),
+					exception);
+			}
+		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ExportExpressionVisitorImpl.class);
 
 	private final Map<String, EntityField> _customFieldEntityFields;
 	private final EntityModel _entityModel;
