c076813639e0fedc50be97af241b78124c959164
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-05-03 11:15:20 +0200

    LPS-112887 We want to remove any dependency from headless API in the service, so add a tracker for importer, and copy the interface for LayoutStructureItemHelper that we are going to use for this

---
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/LayoutStructureItemImporter.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.admin.web.internal.headless.delivery.dto.v1_0.structure;
+
+import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.processor.FragmentEntryProcessorRegistry;
+import com.liferay.fragment.renderer.FragmentRendererTracker;
+import com.liferay.fragment.validator.FragmentEntryValidator;
+import com.liferay.headless.delivery.dto.v1_0.PageElement;
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.layout.util.structure.LayoutStructureItem;
+import com.liferay.portal.kernel.model.Layout;
+
+/**
+ * @author Jürgen Kappler
+ */
+public interface LayoutStructureItemImporter {
+
+	public PageElement.Type getPageElementType();
+
+	public LayoutStructureItem addLayoutStructureItem(
+		FragmentCollectionContributorTracker
+			fragmentCollectionContributorTracker,
+		FragmentEntryProcessorRegistry fragmentEntryProcessorRegistry,
+		FragmentEntryValidator fragmentEntryValidator,
+		FragmentRendererTracker fragmentRendererTracker, Layout layout,
+		LayoutStructure layoutStructure, PageElement pageElement,
+		String parentItemId, int position)
+		throws Exception;
+
+}
