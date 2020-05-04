908211d4077562501e3d6e860ceb0ee052247fac
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-05-04 09:23:39 +0200

    LPS-112740 Also take the FragmentDropzoneLayoutStructureItem into account when importing a page definition

---
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/importer/FragmentDropZoneLayoutStructureItemImporter.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.admin.web.internal.headless.delivery.dto.v1_0.structure.importer;
+
+import com.liferay.headless.delivery.dto.v1_0.PageElement;
+import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.layout.util.structure.LayoutStructureItem;
+import com.liferay.portal.kernel.model.Layout;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(service = LayoutStructureItemImporter.class)
+public class FragmentDropZoneLayoutStructureItemImporter
+	implements LayoutStructureItemImporter {
+
+	@Override
+	public LayoutStructureItem addLayoutStructureItem(
+			Layout layout, LayoutStructure layoutStructure,
+			PageElement pageElement, String parentItemId, int position)
+		throws Exception {
+
+		return layoutStructure.addFragmentDropZoneLayoutStructureItem(
+			parentItemId, position);
+	}
+
+	@Override
+	public PageElement.Type getPageElementType() {
+		return PageElement.Type.FRAGMENT_DROP_ZONE;
+	}
+
+}
