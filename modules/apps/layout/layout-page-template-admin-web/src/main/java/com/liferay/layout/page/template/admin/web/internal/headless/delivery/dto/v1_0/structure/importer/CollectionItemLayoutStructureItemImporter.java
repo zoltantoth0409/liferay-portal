b96d438259da54647c9d0aa2ef69f554ed16942f
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-05-04 09:27:11 +0200

    LPS-112740 There's no need to extend base class in this case

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/importer/CollectionItemLayoutStructureItemImporter.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/importer/CollectionItemLayoutStructureItemImporter.java
@@ -26,7 +26,6 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(service = LayoutStructureItemImporter.class)
 public class CollectionItemLayoutStructureItemImporter
-	extends BaseLayoutStructureItemImporter
 	implements LayoutStructureItemImporter {
 
 	@Override
