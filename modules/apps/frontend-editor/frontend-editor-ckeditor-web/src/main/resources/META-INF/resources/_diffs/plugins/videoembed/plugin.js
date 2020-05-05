84dba03f9dcd7522aefe471ba045a16cac76dff0
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2020-05-05 16:05:31 +0200

    LPS-112367 Remove obsolete jsdoc

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/videoembed/plugin.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/plugins/videoembed/plugin.js
@@ -12,11 +12,6 @@
  * details.
  */
 
-/**
- * SPDX-FileCopyrightText: © 2014 Liferay, Inc. <https://liferay.com>
- * SPDX-License-Identifier: LGPL-3.0-or-later
- */
-
 if (!CKEDITOR.plugins.get('videoembed')) {
 	const REGEX_HTTP = /^https?/;
 
