424a113308085c0489299a35a976b9ebfdb1f389
pei-jung <pei-jung.lan@liferay.com>
2020-05-04 17:17:14 -0700

    LPS-112920 Fix upgrade query for PostgreSQL

--- modules/apps/account/account-service/src/main/java/com/liferay/account/internal/upgrade/v1_0_3/UpgradeRole.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/upgrade/v1_0_3/UpgradeRole.java
@@ -16,8 +16,12 @@ package com.liferay.account.internal.upgrade.v1_0_3;
 
 import com.liferay.account.constants.AccountRoleConstants;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * @author Pei-Jung Lan
@@ -29,15 +33,29 @@ public class UpgradeRole extends UpgradeProcess {
 		runSQL(
 			StringBundler.concat(
 				"update Role_ set type_ = ", RoleConstants.TYPE_ORGANIZATION,
-				" where name = \"",
-				AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER, "\""));
+				" where name = '",
+				AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER, "'"));
 
-		runSQL(
-			StringBundler.concat(
-				"delete ResourcePermission from ResourcePermission inner join ",
-				"Role_ on ResourcePermission.roleId = Role_.roleId where ",
-				"Role_.name = \"",
-				AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER, "\""));
+		try (PreparedStatement ps1 = connection.prepareStatement(
+				"select distinct Role_.roleId from Role_ where name = '" +
+					AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MANAGER +
+						"'");
+			PreparedStatement ps2 = AutoBatchPreparedStatementUtil.autoBatch(
+				connection.prepareStatement(
+					"delete from ResourcePermission where roleId = ?"))) {
+
+			try (ResultSet rs = ps1.executeQuery()) {
+				while (rs.next()) {
+					long roleId = rs.getLong("roleId");
+
+					ps2.setLong(1, roleId);
+
+					ps2.addBatch();
+				}
+
+				ps2.executeBatch();
+			}
+		}
 	}
 
 }
