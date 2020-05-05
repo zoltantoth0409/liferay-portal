34a6c6429ee275d823f5f636b015407ba3700faf
Brian Chan <brian.chan@liferay.com>
2020-05-04 19:14:01 -0700

    LPS-111626 simplify

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutCopyHelperImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutCopyHelperImpl.java
@@ -200,10 +200,10 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 					classNameId, targetLayout.getPlid(), null, serviceContext);
 		}
 
-		Map<Long, Long> segmentsExperienceIdsMap = _getSegmentsExperienceIdsMap(
+		Map<Long, Long> segmentsExperienceIds = _getSegmentsExperienceIds(
 			classNameId, sourceLayout, targetLayout, serviceContext);
 
-		for (Map.Entry<Long, Long> entry : segmentsExperienceIdsMap.entrySet()) {
+		for (Map.Entry<Long, Long> entry : segmentsExperienceIds.entrySet()) {
 			_copyLayoutPageTemplateStructureExperience(
 				layoutPageTemplateStructure, entry.getKey(), classNameId,
 				targetLayout, fragmentEntryLinkMap, entry.getValue(),
@@ -401,7 +401,7 @@ public class LayoutCopyHelperImpl implements LayoutCopyHelper {
 		}
 	}
 
-	private Map<Long, Long> _getSegmentsExperienceIdsMap(
+	private Map<Long, Long> _getSegmentsExperienceIds(
 		long classNameId, Layout sourceLayout, Layout targetLayout,
 		ServiceContext serviceContext) {
 
