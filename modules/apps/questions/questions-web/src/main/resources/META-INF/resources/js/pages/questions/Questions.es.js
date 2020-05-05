0f3de09c24ad261855bcca11d40d26a6af52b32d
Luismi Barcos <luismiguel.barcos@liferay.com>
2020-05-05 21:33:16 +0200

    LPS-112891 Add page as query parameter and handle it in questions

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Questions.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Questions.es.js
@@ -15,141 +15,165 @@
 import ClayLoadingIndicator from '@clayui/loading-indicator';
 import {ClayPaginationWithBasicItems} from '@clayui/pagination';
 import React, {useCallback, useContext, useEffect, useState} from 'react';
+import {withRouter} from 'react-router-dom';
 
 import {AppContext} from '../../AppContext.es';
 import Error from '../../components/Error.es';
 import QuestionRow from '../../components/QuestionRow.es';
+import useQuery from '../../hooks/useQuery.es';
 import {getRankedThreads, getThreads} from '../../utils/client.es';
-import {slugToText} from '../../utils/utils.es';
+import {historyPushWithSlug, slugToText} from '../../utils/utils.es';
 import QuestionsNavigationBar from '../QuestionsNavigationBar.es';
 
-export default ({
-	match: {
-		params: {creatorId, tag},
-	},
-}) => {
-	const [currentTag, setCurrentTag] = useState('');
-	const [error, setError] = useState({});
-	const [loading, setLoading] = useState(true);
-	const [page, setPage] = useState(1);
-	const [pageSize] = useState(20);
-	const [questions, setQuestions] = useState([]);
-	const [search, setSearch] = useState('');
-	const [section, setSection] = useState({});
+export default withRouter(
+	({
+		history,
+		location,
+		match: {
+			params: {creatorId, tag},
+		},
+	}) => {
+		const [currentTag, setCurrentTag] = useState('');
+		const [error, setError] = useState({});
+		const [loading, setLoading] = useState(true);
+		const [page, setPage] = useState(1);
+		const [pageSize] = useState(20);
+		const [questions, setQuestions] = useState([]);
+		const [search, setSearch] = useState('');
+		const [section, setSection] = useState({});
 
-	const context = useContext(AppContext);
+		const queryParams = useQuery(location);
 
-	const siteKey = context.siteKey;
+		const context = useContext(AppContext);
 
-	useEffect(() => {
-		setCurrentTag(tag ? slugToText(tag) : '');
-	}, [tag]);
+		const siteKey = context.siteKey;
 
-	useEffect(() => {
-		if (section.id) {
-			renderQuestions(loadThreads());
-		}
-	}, [
-		creatorId,
-		page,
-		pageSize,
-		section,
-		search,
-		siteKey,
-		currentTag,
-		loadThreads,
-	]);
+		const historyPushParser = historyPushWithSlug(history.push);
 
-	const renderQuestions = (questions) => {
-		questions
-			.then((data) => setQuestions(data || []))
-			.then(() => setLoading(false))
-			.catch((error) => {
-				if (process.env.NODE_ENV === 'development') {
-					console.error(error);
-				}
-				setLoading(false);
-				setError({message: 'Loading Questions', title: 'Error'});
-			});
-	};
+		useEffect(() => {
+			setCurrentTag(tag ? slugToText(tag) : '');
+		}, [tag]);
 
-	const loadThreads = useCallback(
-		(sort) =>
-			getThreads({
-				creatorId,
-				keywords: currentTag,
-				page,
-				pageSize,
-				search,
-				section,
-				siteKey,
-				sort,
-			}),
-		[creatorId, currentTag, page, pageSize, search, section, siteKey]
-	);
+		useEffect(() => {
+			const pageNumber = queryParams.get('page') || 1;
+			setPage(isNaN(pageNumber) ? 1 : parseInt(pageNumber, 10));
+		}, [queryParams]);
 
-	const filterChange = (type) => {
-		if (type === 'latest-edited') {
-			renderQuestions(loadThreads('dateModified:desc'));
-		}
-		else if (type === 'week') {
-			const date = new Date();
-			date.setDate(date.getDate() - 7);
+		useEffect(() => {
+			if (section.id) {
+				renderQuestions(loadThreads());
+			}
+		}, [
+			creatorId,
+			page,
+			pageSize,
+			section,
+			search,
+			siteKey,
+			currentTag,
+			loadThreads,
+		]);
 
-			renderQuestions(getRankedThreads(date, page, pageSize, section));
-		}
-		else if (type === 'month') {
-			const date = new Date();
-			date.setDate(date.getDate() - 31);
+		const renderQuestions = (questions) => {
+			questions
+				.then((data) => setQuestions(data || []))
+				.then(() => setLoading(false))
+				.catch((error) => {
+					if (process.env.NODE_ENV === 'development') {
+						console.error(error);
+					}
+					setLoading(false);
+					setError({message: 'Loading Questions', title: 'Error'});
+				});
+		};
 
-			renderQuestions(getRankedThreads(date, page, pageSize, section));
-		}
-		else {
-			renderQuestions(loadThreads('dateCreated:desc'));
-		}
-	};
+		const loadThreads = useCallback(
+			(sort) =>
+				getThreads({
+					creatorId,
+					keywords: currentTag,
+					page,
+					pageSize,
+					search,
+					section,
+					siteKey,
+					sort,
+				}),
+			[creatorId, currentTag, page, pageSize, search, section, siteKey]
+		);
 
-	return (
-		<section className="questions-section questions-section-list">
-			<div className="questions-container">
-				<div className="row">
-					<div className="c-mt-3 col col-xl-12">
-						<QuestionsNavigationBar
-							filterChange={filterChange}
-							searchChange={(search) => setSearch(search)}
-							sectionChange={(section) => setSection(section)}
-						/>
-					</div>
+		const filterChange = (type) => {
+			if (type === 'latest-edited') {
+				renderQuestions(loadThreads('dateModified:desc'));
+			}
+			else if (type === 'week') {
+				const date = new Date();
+				date.setDate(date.getDate() - 7);
 
-					<div className="c-mt-5 c-mx-auto c-px-0 col-xl-10">
-						{loading ? (
-							<ClayLoadingIndicator />
-						) : (
-							questions.items &&
-							questions.items.map((question) => (
-								<QuestionRow
-									key={question.id}
-									question={question}
-								/>
-							))
-						)}
+				renderQuestions(
+					getRankedThreads(date, page, pageSize, section)
+				);
+			}
+			else if (type === 'month') {
+				const date = new Date();
+				date.setDate(date.getDate() - 31);
 
-						{!!questions.totalCount &&
-							questions.totalCount > questions.pageSize && (
-								<ClayPaginationWithBasicItems
-									activePage={page}
-									ellipsisBuffer={2}
-									onPageChange={setPage}
-									totalPages={Math.ceil(
-										questions.totalCount /
-											questions.pageSize
-									)}
-								/>
+				renderQuestions(
+					getRankedThreads(date, page, pageSize, section)
+				);
+			}
+			else {
+				renderQuestions(loadThreads('dateCreated:desc'));
+			}
+		};
+
+		const changePage = (number) => {
+			setPage(number);
+			historyPushParser(`/questions/${context.section}?page=${number}`);
+		};
+
+		return (
+			<section className="questions-section questions-section-list">
+				<div className="questions-container">
+					<div className="row">
+						<div className="c-mt-3 col col-xl-12">
+							<QuestionsNavigationBar
+								filterChange={filterChange}
+								searchChange={(search) => setSearch(search)}
+								sectionChange={(section) => setSection(section)}
+							/>
+						</div>
+
+						<div className="c-mt-5 c-mx-auto c-px-0 col-xl-10">
+							{loading ? (
+								<ClayLoadingIndicator />
+							) : (
+								questions.items &&
+								questions.items.map((question) => (
+									<QuestionRow
+										key={question.id}
+										question={question}
+									/>
+								))
 							)}
-						<Error error={error} />
+
+							{!!questions.totalCount &&
+								questions.totalCount > questions.pageSize && (
+									<ClayPaginationWithBasicItems
+										activePage={page}
+										ellipsisBuffer={2}
+										onPageChange={changePage}
+										totalPages={Math.ceil(
+											questions.totalCount /
+												questions.pageSize
+										)}
+									/>
+								)}
+							<Error error={error} />
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-	);
-};
+			</section>
+		);
+	}
+);
