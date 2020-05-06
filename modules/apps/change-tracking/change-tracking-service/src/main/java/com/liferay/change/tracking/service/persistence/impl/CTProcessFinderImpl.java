a9ee3795929563e8ffe090f311836a4d9043b3b3
Preston-Crary <preston.crary@liferay.com>
2020-05-06 14:04:56 -0700

    LPS-112857 Simplify.

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/persistence/impl/CTProcessFinderImpl.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/service/persistence/impl/CTProcessFinderImpl.java
@@ -18,19 +18,15 @@ import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.model.CTCollectionTable;
 import com.liferay.change.tracking.model.CTProcess;
 import com.liferay.change.tracking.model.CTProcessTable;
-import com.liferay.change.tracking.model.impl.CTProcessImpl;
 import com.liferay.change.tracking.service.persistence.CTProcessFinder;
 import com.liferay.petra.sql.dsl.DSLFunctionFactoryUtil;
 import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.petra.sql.dsl.expression.Predicate;
+import com.liferay.petra.sql.dsl.query.DSLQuery;
 import com.liferay.petra.sql.dsl.query.LimitStep;
 import com.liferay.portal.background.task.model.BackgroundTaskTable;
 import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.dao.orm.SQLQuery;
-import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.dao.orm.WildcardMode;
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.security.permission.InlineSQLHelper;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -52,108 +48,91 @@ public class CTProcessFinderImpl
 		long companyId, long userId, String keywords, int status, int start,
 		int end, OrderByComparator<?> orderByComparator) {
 
-		Session session = null;
+		Predicate predicate = CTProcessTable.INSTANCE.companyId.eq(companyId);
 
-		try {
-			session = openSession();
+		if (userId > 0) {
+			predicate = predicate.and(
+				CTProcessTable.INSTANCE.userId.eq(userId));
+		}
 
-			Predicate predicate = CTProcessTable.INSTANCE.companyId.eq(
-				companyId);
+		if (status != WorkflowConstants.STATUS_ANY) {
+			predicate = predicate.and(
+				BackgroundTaskTable.INSTANCE.status.eq(status));
+		}
 
-			if (userId > 0) {
-				predicate = predicate.and(
-					CTProcessTable.INSTANCE.userId.eq(userId));
+		Predicate keywordsPredicate = null;
+
+		for (String keyword :
+				_customSQL.keywords(keywords, true, WildcardMode.SURROUND)) {
+
+			if (keyword == null) {
+				continue;
 			}
 
-			if (status != WorkflowConstants.STATUS_ANY) {
-				predicate = predicate.and(
-					BackgroundTaskTable.INSTANCE.status.eq(status));
-			}
-
-			Predicate keywordsPredicate = null;
-
-			for (String keyword :
-					_customSQL.keywords(
-						keywords, true, WildcardMode.SURROUND)) {
-
-				if (keyword == null) {
-					continue;
-				}
-
-				Predicate keywordPredicate = DSLFunctionFactoryUtil.lower(
-					CTCollectionTable.INSTANCE.name
+			Predicate keywordPredicate = DSLFunctionFactoryUtil.lower(
+				CTCollectionTable.INSTANCE.name
+			).like(
+				keyword
+			).or(
+				DSLFunctionFactoryUtil.lower(
+					CTCollectionTable.INSTANCE.description
 				).like(
 					keyword
-				).or(
-					DSLFunctionFactoryUtil.lower(
-						CTCollectionTable.INSTANCE.description
-					).like(
-						keyword
-					)
-				);
+				)
+			);
 
-				if (keywordsPredicate == null) {
-					keywordsPredicate = keywordPredicate;
-				}
-				else {
-					keywordsPredicate = keywordsPredicate.or(keywordPredicate);
-				}
+			if (keywordsPredicate == null) {
+				keywordsPredicate = keywordPredicate;
 			}
-
-			if (keywordsPredicate != null) {
-				predicate = predicate.and(keywordsPredicate.withParentheses());
+			else {
+				keywordsPredicate = keywordsPredicate.or(keywordPredicate);
 			}
+		}
 
-			predicate = predicate.and(
-				_inlineSQLHelper.getPermissionWherePredicate(
-					CTCollection.class,
-					CTCollectionTable.INSTANCE.ctCollectionId));
+		if (keywordsPredicate != null) {
+			predicate = predicate.and(keywordsPredicate.withParentheses());
+		}
 
-			SQLQuery sqlQuery = session.createSynchronizedSQLQuery(
-				DSLQueryFactoryUtil.select(
-					CTProcessTable.INSTANCE
-				).from(
-					CTProcessTable.INSTANCE
-				).innerJoinON(
-					CTCollectionTable.INSTANCE,
-					CTCollectionTable.INSTANCE.ctCollectionId.eq(
-						CTProcessTable.INSTANCE.ctCollectionId)
-				).innerJoinON(
-					BackgroundTaskTable.INSTANCE,
-					BackgroundTaskTable.INSTANCE.backgroundTaskId.eq(
-						CTProcessTable.INSTANCE.backgroundTaskId)
-				).where(
-					predicate
-				).orderBy(
-					orderByStep -> {
-						if (orderByComparator != null) {
-							LimitStep limitStep = orderByStep.orderBy(
-								CTCollectionTable.INSTANCE, orderByComparator);
+		predicate = predicate.and(
+			_inlineSQLHelper.getPermissionWherePredicate(
+				CTCollection.class, CTCollectionTable.INSTANCE.ctCollectionId));
 
-							if (limitStep == orderByStep) {
-								return orderByStep.orderBy(
-									CTProcessTable.INSTANCE, orderByComparator);
-							}
+		DSLQuery dslQuery = DSLQueryFactoryUtil.select(
+			CTProcessTable.INSTANCE
+		).from(
+			CTProcessTable.INSTANCE
+		).innerJoinON(
+			CTCollectionTable.INSTANCE,
+			CTCollectionTable.INSTANCE.ctCollectionId.eq(
+				CTProcessTable.INSTANCE.ctCollectionId)
+		).innerJoinON(
+			BackgroundTaskTable.INSTANCE,
+			BackgroundTaskTable.INSTANCE.backgroundTaskId.eq(
+				CTProcessTable.INSTANCE.backgroundTaskId)
+		).where(
+			predicate
+		).orderBy(
+			orderByStep -> {
+				if (orderByComparator != null) {
+					LimitStep limitStep = orderByStep.orderBy(
+						CTCollectionTable.INSTANCE, orderByComparator);
 
-							return limitStep;
-						}
-
+					if (limitStep == orderByStep) {
 						return orderByStep.orderBy(
-							CTProcessTable.INSTANCE.createDate.descending());
+							CTProcessTable.INSTANCE, orderByComparator);
 					}
-				));
 
-			sqlQuery.addEntity("CTProcess", CTProcessImpl.class);
+					return limitStep;
+				}
 
-			return (List<CTProcess>)QueryUtil.list(
-				sqlQuery, getDialect(), start, end);
-		}
-		catch (Exception exception) {
-			throw new SystemException(exception);
-		}
-		finally {
-			closeSession(session);
-		}
+				return orderByStep.orderBy(
+					CTProcessTable.INSTANCE.createDate.descending());
+			}
+		).limit(
+			start, end
+		);
+
+		return ctProcessPersistence.dslQuery(dslQuery);
 	}
 
 	@Reference
