f9bbc3aac2bebaaf2d89fee87b7c42d75d6015bf
drewbrokke <drew.brokke@liferay.com>
2020-05-06 16:51:17 -0500

    LPS-112410 headless-admin-user-test: re-adds partial setup logic
    It's important to make sure that the test user is not a member of the
    test group, otherwise it will be returned in the search results for a
    number of tests that do not expect it. This is outside the domain of
    what DataGuard is trying to accomplish, therefore it has to stay in the
    setup method.

--- modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/UserAccountResourceTest.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/UserAccountResourceTest.java
@@ -33,6 +33,8 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.OrganizationTestUtil;
@@ -72,6 +74,14 @@ public class UserAccountResourceTest extends BaseUserAccountResourceTestCase {
 
 		_testUser = _userLocalService.getUserByEmailAddress(
 			testGroup.getCompanyId(), "test@liferay.com");
+
+		_userLocalService.deleteGroupUser(
+			testGroup.getGroupId(), _testUser.getUserId());
+
+		Indexer<User> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+			_testUser.getModelClassName());
+
+		indexer.reindex(_testUser);
 	}
 
 	@Override
