39f610ab1ac60873d67f2f66337aae272cd6e2f4
Hugo Huijser <hugo.huijser@liferay.com>
2020-05-07 10:41:40 +0800

    LPS-113044 Ensure that the require-schema-version is always synchronized with the upgrade path

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDSchemaVersionCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDSchemaVersionCheck.java
@@ -14,12 +14,24 @@
 
 package com.liferay.source.formatter.checks;
 
+import aQute.bnd.version.Version;
+
 import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.SourceFormatterExcludes;
 import com.liferay.source.formatter.checks.util.BNDSourceUtil;
+import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+import com.liferay.source.formatter.parser.JavaClass;
+import com.liferay.source.formatter.parser.JavaClassParser;
+import com.liferay.source.formatter.parser.ParseException;
+import com.liferay.source.formatter.util.FileUtil;
+import com.liferay.source.formatter.util.SourceFormatterUtil;
 
 import java.io.File;
+import java.io.IOException;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -29,7 +41,8 @@ public class BNDSchemaVersionCheck extends BaseFileCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, String content) {
+			String fileName, String absolutePath, String content)
+		throws IOException, ParseException {
 
 		String schemaVersion = BNDSourceUtil.getDefinitionValue(
 			content, "Liferay-Require-SchemaVersion");
@@ -37,16 +50,17 @@ public class BNDSchemaVersionCheck extends BaseFileCheck {
 		if (GetterUtil.getBoolean(
 				BNDSourceUtil.getDefinitionValue(content, "Liferay-Service"))) {
 
-			if (schemaVersion == null) {
-				int pos = absolutePath.lastIndexOf(CharPool.SLASH);
+			if (schemaVersion != null) {
+				return _fixSchemaVersion(absolutePath, content, schemaVersion);
+			}
 
-				File serviceXMLfile = new File(
-					absolutePath.substring(0, pos + 1) + "service.xml");
+			int pos = absolutePath.lastIndexOf(CharPool.SLASH);
 
-				if (serviceXMLfile.exists()) {
-					addMessage(
-						fileName, "Missing 'Liferay-Require-SchemaVersion'");
-				}
+			File serviceXMLfile = new File(
+				absolutePath.substring(0, pos + 1) + "service.xml");
+
+			if (serviceXMLfile.exists()) {
+				addMessage(fileName, "Missing 'Liferay-Require-SchemaVersion'");
 			}
 		}
 		else if (schemaVersion != null) {
@@ -66,6 +80,87 @@ public class BNDSchemaVersionCheck extends BaseFileCheck {
 		}
 
 		return content;
+	}
+
+	private String _fixSchemaVersion(
+			String absolutePath, String content, String schemaVersion)
+		throws IOException, ParseException {
+
+		int x = absolutePath.lastIndexOf(CharPool.SLASH);
+
+		absolutePath.substring(0, x + 1);
+
+		List<String> upgradeFileNames = SourceFormatterUtil.scanForFiles(
+			absolutePath.substring(0, x + 1), new String[0],
+			new String[] {"**/upgrade/*.java", "**/upgrade/**/*.java"},
+			new SourceFormatterExcludes(), false);
+
+		String expectedSchemaVersion = _getExpectedSchemaVersion(
+			upgradeFileNames);
+
+		if ((expectedSchemaVersion == null) ||
+			schemaVersion.equals(expectedSchemaVersion)) {
+
+			return content;
+		}
+
+		return StringUtil.replace(
+			content, "Liferay-Require-SchemaVersion: " + schemaVersion,
+			"Liferay-Require-SchemaVersion: " + expectedSchemaVersion);
+	}
+
+	private String _getExpectedSchemaVersion(List<String> fileNames)
+		throws IOException, ParseException {
+
+		Version expectedSchemaVersion = null;
+
+		for (String fileName : fileNames) {
+			fileName = StringUtil.replace(
+				fileName, CharPool.BACK_SLASH, CharPool.SLASH);
+
+			File file = new File(fileName);
+
+			String content = FileUtil.read(file);
+
+			JavaClass javaClass = JavaClassParser.parseJavaClass(
+				fileName, content);
+
+			List<String> implementedClassNames =
+				javaClass.getImplementedClassNames();
+
+			if (!implementedClassNames.contains("UpgradeStepRegistrator")) {
+				continue;
+			}
+
+			int x = -1;
+
+			while (true) {
+				x = content.indexOf(".register(", x + 1);
+
+				if (x == -1) {
+					break;
+				}
+
+				List<String> parameterList = JavaSourceUtil.getParameterList(
+					content.substring(x - 1));
+
+				Version schemaVersion = new Version(
+					StringUtil.removeChar(
+						parameterList.get(1), CharPool.QUOTE));
+
+				if ((expectedSchemaVersion == null) ||
+					(expectedSchemaVersion.compareTo(schemaVersion) < 0)) {
+
+					expectedSchemaVersion = schemaVersion;
+				}
+			}
+		}
+
+		if (expectedSchemaVersion != null) {
+			return expectedSchemaVersion.toString();
+		}
+
+		return null;
 	}
 
 }
