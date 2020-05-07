0a435aef7b80006595991ad4abeff1316a3daffe
Bruno Farache <bruno.farache@liferay.com>
2020-05-07 13:34:56 -0300

    LPS-113080 Simplify show all component

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/components/Legend.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/components/Legend.es.js
@@ -18,105 +18,105 @@ import React, {useState} from 'react';
 
 import colors from '../../utils/colors.es';
 
-export default ({activeIndex, labels, onMouseOut, onMouseOver}) => {
-	const [showAll, setShowAll] = useState(false);
-	const [showLabel, setShowLabel] = useState(
-		Liferay.Language.get('show-all')
+const MAX_LABELS = 10;
+
+const ShowAll = ({expand, expanded, labelsLength}) => {
+	if (labelsLength < MAX_LABELS) {
+		return null;
+	}
+
+	const toggleExpanded = () => {
+		expand(!expanded);
+	};
+
+	return (
+		<div className="show-more-action">
+			<svg height="20" width="20">
+				<g>
+					<circle
+						cx="10"
+						cy="10"
+						fill={'#F0F5FF'}
+						r="9"
+						strokeWidth="3"
+					/>
+
+					<ClayIcon
+						symbol={expanded ? 'caret-top-l' : 'caret-bottom-l'}
+					/>
+				</g>
+			</svg>
+
+			<ClayButton
+				className="show-more-button"
+				displayType="unstyled"
+				onClick={() => toggleExpanded()}
+			>
+				{expanded
+					? Liferay.Language.get('show-less')
+					: Liferay.Language.get('show-all')}
+			</ClayButton>
+		</div>
 	);
-	const [showLimit, setShowLimit] = useState(10);
+};
+
+export default ({activeIndex, labels, onMouseOut, onMouseOver}) => {
+	const [isShowAllExpanded, expandShowAll] = useState(false);
 
 	const handleOnMouseOver = ({currentTarget}) => {
 		const item = currentTarget.closest('li').dataset.item;
 		onMouseOver(parseInt(item, 10));
 	};
 
-	const showMoreItems = () => {
-		setShowAll(!showAll);
-
-		if (!showAll && labels.length >= showLimit) {
-			setShowLabel(Liferay.Language.get('show-less'));
-			setShowLimit(labels.length);
-		}
-		else if (labels.length >= showLimit) {
-			setShowLimit(10);
-			setShowLabel(Liferay.Language.get('show-all'));
-		}
-	};
-
 	return (
 		<div className="legend-container well well-lg">
 			<div className="legend-list">
 				<ul style={{listStyleType: 'none'}}>
-					{labels.slice(0, showLimit).map((label, index) => (
-						<li
-							className={
-								activeIndex != null && activeIndex != index
-									? 'dim'
-									: ''
-							}
-							data-item={index}
-							key={`item-${index}`}
-						>
-							<svg height="20" width="20">
-								<circle
-									cx="10"
-									cy="10"
-									fill={colors(index)}
+					{labels
+						.slice(
+							0,
+							isShowAllExpanded ? labels.length : MAX_LABELS
+						)
+						.map((label, index) => (
+							<li
+								className={
+									activeIndex != null && activeIndex != index
+										? 'dim'
+										: ''
+								}
+								data-item={index}
+								key={`item-${index}`}
+							>
+								<svg height="20" width="20">
+									<circle
+										cx="10"
+										cy="10"
+										fill={colors(index)}
+										onMouseOut={onMouseOut}
+										onMouseOver={handleOnMouseOver}
+										r="9"
+										strokeWidth="3"
+									/>
+								</svg>
+
+								<text
 									onMouseOut={onMouseOut}
 									onMouseOver={handleOnMouseOver}
-									r="9"
-									strokeWidth="3"
-								/>
-							</svg>
+									x="0em"
+									y="1em"
+								>
+									<tspan dy="2em" x="2.5em">
+										{label}
+									</tspan>
+								</text>
+							</li>
+						))}
 
-							<text
-								onMouseOut={onMouseOut}
-								onMouseOver={handleOnMouseOver}
-								x="0em"
-								y="1em"
-							>
-								<tspan dy="2em" x="2.5em">
-									{label}
-								</tspan>
-							</text>
-						</li>
-					))}
-
-					<div
-						className={
-							labels.length < showLimit
-								? 'hide'
-								: 'show-more-action'
-						}
-					>
-						<svg height="20" width="20">
-							<g>
-								<circle
-									cx="10"
-									cy="10"
-									fill={'#F0F5FF'}
-									r="9"
-									strokeWidth="3"
-								/>
-
-								<ClayIcon
-									symbol={
-										!showAll
-											? 'caret-bottom-l'
-											: 'caret-top-l'
-									}
-								/>
-							</g>
-						</svg>
-
-						<ClayButton
-							className="show-more-button"
-							displayType="unstyled"
-							onClick={showMoreItems}
-						>
-							{showLabel}
-						</ClayButton>
-					</div>
+					<ShowAll
+						expand={expandShowAll}
+						expanded={isShowAllExpanded}
+						labelsLength={labels.length}
+					/>
 				</ul>
 			</div>
 		</div>
--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/index.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/index.es.js
@@ -16,8 +16,14 @@ import React from 'react';
 
 import App from './App.es';
 
-export default ({data}) => (
-	<div className="form-report">
-		<App data={JSON.parse(data)} />
-	</div>
-);
+export default ({data}) => {
+	if (!data) {
+		return null;
+	}
+
+	return (
+		<div className="form-report">
+			<App data={JSON.parse(data)} />
+		</div>
+	);
+};
