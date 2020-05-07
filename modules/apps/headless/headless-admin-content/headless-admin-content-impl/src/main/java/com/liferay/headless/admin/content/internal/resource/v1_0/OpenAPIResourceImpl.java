fd3d085204fcc888814d4614f1fbc14173c44225
Javier Gamarra <nhpatt@gmail.com>
2020-05-07 16:07:04 +0200

    LPS-112153 generate

--- modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/OpenAPIResourceImpl.java
+++ modules/apps/headless/headless-admin-content/headless-admin-content-impl/src/main/java/com/liferay/headless/admin/content/internal/resource/v1_0/OpenAPIResourceImpl.java
@@ -64,6 +64,8 @@ public class OpenAPIResourceImpl {
 
 	private final Set<Class<?>> _resourceClasses = new HashSet<Class<?>>() {
 		{
+			add(PageDefinitionResourceImpl.class);
+
 			add(OpenAPIResourceImpl.class);
 		}
 	};
