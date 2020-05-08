56209402cb21a4598f5b88f4643733857f05bfe2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-05-08 13:15:29 +0200

    LPS-113423 Adds new ProductNavigationControlMenuEntry for Global Menu

---
+++ modules/apps/product-navigation/product-navigation-global-menu-web/src/main/java/com/liferay/product/navigation/global/menu/web/internal/product/navigation/control/menu/GlobalMenuProductNavigationControlMenuEntry.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.product.navigation.global.menu.web.internal.product.navigation.control.menu;
+
+import com.liferay.application.list.PanelCategory;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.product.navigation.control.menu.BaseJSPProductNavigationControlMenuEntry;
+import com.liferay.product.navigation.control.menu.ProductNavigationControlMenuEntry;
+import com.liferay.product.navigation.control.menu.constants.ProductNavigationControlMenuCategoryKeys;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = {
+		"product.navigation.control.menu.category.key=" + ProductNavigationControlMenuCategoryKeys.USER,
+		"product.navigation.control.menu.entry.order:Integer=400"
+	},
+	service = ProductNavigationControlMenuEntry.class
+)
+public class GlobalMenuProductNavigationControlMenuEntry
+	extends BaseJSPProductNavigationControlMenuEntry {
+
+	@Override
+	public String getIconJspPath() {
+		return "/global_menu/global_menu.jsp";
+	}
+
+	@Reference(
+		target = "(panel.category.key=" + PanelCategoryKeys.HIDDEN + ")",
+		unbind = "-"
+	)
+	public void setPanelCategory(PanelCategory panelCategory) {
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.product.navigation.global.menu.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}
