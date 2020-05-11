7b7c4f37ff3ecc3ede31e088149ab98a8a5f1247
Inácio Nery <inacio.nery@liferay.com>
2020-05-11 17:23:02 -0300

    LPS-113314 SF

--- modules/apps/notifications/notifications-web/src/main/java/com/liferay/notifications/web/internal/portlet/NotificationsPortlet.java
+++ modules/apps/notifications/notifications-web/src/main/java/com/liferay/notifications/web/internal/portlet/NotificationsPortlet.java
@@ -400,10 +400,8 @@ public class NotificationsPortlet extends MVCPortlet {
 
 		String redirect = ParamUtil.getString(actionRequest, "redirect");
 
-		redirect = _portal.escapeRedirect(redirect);
-
 		if (Validator.isNotNull(redirect)) {
-			actionResponse.sendRedirect(redirect);
+			actionResponse.sendRedirect(_portal.escapeRedirect(redirect));
 		}
 	}
 
