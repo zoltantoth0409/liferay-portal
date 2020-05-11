9ce60cb658d870027dc31b8496bdb75768f8e499
Pavel Savinov <pavel.savinov@liferay.com>
2020-05-11 15:09:28 +0200

    LPS-113229 Base info taglib files

---
+++ modules/apps/info/info-taglib/src/main/java/com/liferay/info/taglib/internal/servlet/ServletContextUtil.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.taglib.internal.servlet;
+
+import com.liferay.info.item.renderer.InfoItemRendererTracker;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(immediate = true, service = {})
+public class ServletContextUtil {
+
+	public static final InfoItemRendererTracker getInfoItemRendererTracker() {
+		return _infoItemRendererTracker;
+	}
+
+	public static final ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(unbind = "-")
+	protected void setInfoItemRendererTracker(
+		InfoItemRendererTracker infoItemRendererTracker) {
+
+		_infoItemRendererTracker = infoItemRendererTracker;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.info.taglib)", unbind = "-"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private static InfoItemRendererTracker _infoItemRendererTracker;
+	private static ServletContext _servletContext;
+
+}
---
+++ modules/apps/info/info-taglib/src/main/resources/META-INF/resources/init.jsp
@@ -0,0 +1,28 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+
+<%@ page import="com.liferay.info.item.renderer.InfoItemRenderer" %>
+
+<%@ page import="java.util.List" %>
+
+<liferay-theme:defineObjects />
+
+<%
+InfoItemRenderer infoItemRenderer = (InfoItemRenderer)request.getAttribute("liferay-info:info-list-grid:infoItemRenderer");
+List<Object> infoListObjects = (List<Object>)request.getAttribute("liferay-info:info-list-grid:infoListObjects");
+%>
