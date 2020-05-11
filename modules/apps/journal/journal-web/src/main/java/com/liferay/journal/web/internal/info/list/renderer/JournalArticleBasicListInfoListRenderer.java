626ba2b1594bfb60bf020da6ea6e85b37bfd6398
Pavel Savinov <pavel.savinov@liferay.com>
2020-05-11 15:11:23 +0200

    LPS-113229 Implement basic list renderer for web content

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/list/renderer/JournalArticleBasicListInfoListRenderer.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.info.list.renderer;
+
+import com.liferay.info.list.renderer.InfoListRenderer;
+import com.liferay.info.taglib.servlet.taglib.InfoListBasicListTag;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.web.internal.info.item.renderer.JournalArticleAbstractInfoItemRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(immediate = true, service = InfoListRenderer.class)
+public class JournalArticleBasicListInfoListRenderer
+	implements InfoListRenderer<JournalArticle> {
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "basic-list");
+	}
+
+	@Override
+	public void render(
+		List<JournalArticle> articles, HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		InfoListBasicListTag infoListBasicListTag = new InfoListBasicListTag();
+
+		infoListBasicListTag.setItemRendererKey(
+			JournalArticleAbstractInfoItemRenderer.class.getName());
+		infoListBasicListTag.setInfoListObjects(articles);
+
+		try {
+			infoListBasicListTag.doTag(httpServletRequest, httpServletResponse);
+		}
+		catch (Exception exception) {
+			_log.error("Unable to render journal articles list", exception);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JournalArticleBasicListInfoListRenderer.class);
+
+}
