3e87f27e1a414ff5dfac8ec4a968cc91f646d12a
tina.tian <tina.tian@liferay.com>
2020-05-11 10:54:08 -0700

    LPS-112054 Remove definition, it is not used at all caused by inconsistent portlet name with AuditPortletKeys#AUDIT, add upgrade to remove corresponding entries in ResourceAction table

---
+++ modules/dxp/apps/portal-security-audit/portal-security-audit-web/src/main/java/com/liferay/portal/security/audit/web/internal/upgrade/PortalSecurityAuditWebUpgrade.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.security.audit.web.internal.upgrade;
+
+import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.security.audit.web.internal.upgrade.v1_0_0.UpgradeResourceActions;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Tina Tian
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class PortalSecurityAuditWebUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register("0.0.0", "1.0.0", new DummyUpgradeStep());
+
+		registry.register("0.0.1", "1.0.0", new UpgradeResourceActions());
+	}
+
+}
---
+++ modules/dxp/apps/portal-security-audit/portal-security-audit-web/src/main/java/com/liferay/portal/security/audit/web/internal/upgrade/v1_0_0/UpgradeResourceActions.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.security.audit.web.internal.upgrade.v1_0_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Tina Tian
+ */
+public class UpgradeResourceActions extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		runSQL(
+			"delete from ResourceAction where name = 'com_liferay_audit_web_" +
+				"portlet_AuditPortlet'");
+	}
+
+}
