a3ded75adb366906f7b47acbd790c99f5340a0cf
pei-jung <pei-jung.lan@liferay.com>
2020-05-11 17:24:29 -0700

    LPS-111303 Use AccountRoleConstants

--- modules/apps/account/account-service/src/main/java/com/liferay/account/internal/roles/admin/role/type/contributor/AccountRoleTypeContributor.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/roles/admin/role/type/contributor/AccountRoleTypeContributor.java
@@ -23,7 +23,6 @@ import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.roles.admin.role.type.contributor.RoleTypeContributor;
 
 import java.util.Locale;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
@@ -100,10 +99,7 @@ public class AccountRoleTypeContributor implements RoleTypeContributor {
 
 	@Override
 	public boolean isAutomaticallyAssigned(Role role) {
-		if (Objects.equals(
-				role.getName(),
-				AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_MEMBER)) {
-
+		if (AccountRoleConstants.isImpliedRole(role)) {
 			return true;
 		}
 
