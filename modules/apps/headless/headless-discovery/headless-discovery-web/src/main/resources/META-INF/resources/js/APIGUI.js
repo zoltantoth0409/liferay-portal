8aa6abe9d79e2c466dfcc4c5c7461df32edf96fe
Javier Gamarra <nhpatt@gmail.com>
2020-05-12 02:56:26 +0200

    LPS-111024 use GraphiQL and css and json loaders

--- modules/apps/headless/headless-discovery/headless-discovery-web/src/main/resources/META-INF/resources/js/APIGUI.js
+++ modules/apps/headless/headless-discovery/headless-discovery-web/src/main/resources/META-INF/resources/js/APIGUI.js
@@ -14,7 +14,8 @@
 
 import ClayAlert from '@clayui/alert';
 import ClayForm, {ClayInput, ClaySelect} from '@clayui/form';
-import React, {useEffect} from 'react';
+import GraphiQL from 'graphiql';
+import React, {useCallback, useEffect, useState} from 'react';
 
 import APIDisplay from './APIDisplay';
 import {spritemap} from './Icon';
@@ -24,6 +25,8 @@ import {useAppState} from './hooks/appState';
 import apiFetch from './util/apiFetch';
 import {setSearchParam} from './util/params';
 import {getCategoryURL} from './util/url';
+
+import 'graphiql/graphiql.css';
 
 const APIDisplayStyle = {
 	height: 'calc(100% - 190px)',
@@ -43,6 +46,8 @@ const APIGUI = () => {
 		schemas,
 		showSchemas,
 	} = state;
+
+	const [showGraphQL, setShowGraphQL] = useState(false);
 
 	useEffect(() => {
 		setSearchParam('category', categoryKey);
@@ -110,123 +115,155 @@ const APIGUI = () => {
 		};
 	}, [categoryKey, categories, dispatch]);
 
+	window.global = window;
+
+	const graphQLFetcher = useCallback(
+		(graphQLParams) =>
+			apiFetch('/o/graphql', 'post', graphQLParams, 'application/json'),
+		[]
+	);
+
 	return (
 		<div className="api-gui-root">
 			<div className="container container-fluid">
 				<div className="row">
-					<div className="border col col-md-5 overflow-auto p-0 vh-100">
-						<ClayForm.Group className="pt-3 px-3">
-							<label
-								className="d-flex justify-content-between"
-								htmlFor="categorySelect"
-							>
-								<span>
-									{Liferay.Language.get(
-										'select-api-category'
-									)}
-								</span>
-
-								{schemas && (
-									<button
-										onClick={() => {
-											dispatch({
-												type: 'TOGGLE_SCHEMAS',
-											});
-										}}
-									>
-										{showSchemas
-											? Liferay.Language.get(
-													'hide-schemas'
-											  )
-											: Liferay.Language.get(
-													'show-schemas'
-											  )}
-									</button>
-								)}
-							</label>
-							<ClaySelect
-								aria-label="Select API Category"
-								onChange={(e) => {
-									dispatch({
-										categoryKey: e.currentTarget.value,
-										type: 'SELECT_CATEGORY',
-									});
-								}}
-								value={categoryKey}
-							>
-								{categories &&
-									Object.keys(categories).map((key) => (
-										<ClaySelect.Option
-											key={key}
-											label={key}
-											value={key}
-										/>
-									))}
-							</ClaySelect>
-						</ClayForm.Group>
-
-						<ClayForm.Group className="pt-0 px-3">
-							<label htmlFor="filter">{'Filter'}</label>
-
-							<ClayInput
-								name="filter"
-								onChange={(event) => {
-									dispatch({
-										filter: event.target.value,
-										type: 'SET_FILTER',
-									});
-								}}
-								type="text"
-								value={filter}
-							/>
-						</ClayForm.Group>
-
-						<div
-							className="api-list border-top p-3"
-							style={APIDisplayStyle}
+					<div
+						className="col col-push-3"
+						style={{textAlign: 'right'}}
+					>
+						<button
+							onClick={() => {
+								setShowGraphQL(!showGraphQL);
+							}}
 						>
-							{paths && (
-								<PathList
-									baseURL={categoryKey}
-									curPath={path}
-									filter={filter}
-									onClick={(selPath) => {
+							{showGraphQL
+								? Liferay.Language.get('hide-graphql')
+								: Liferay.Language.get('show-graphql')}
+						</button>
+					</div>
+				</div>
+
+				{showGraphQL && (
+					<div className="row vh-100">
+						<GraphiQL fetcher={graphQLFetcher} />
+					</div>
+				)}
+				{!showGraphQL && (
+					<div className="row">
+						<div className="border col col-md-5 overflow-auto p-0 vh-100">
+							<ClayForm.Group className="pt-3 px-3">
+								<label
+									className="d-flex justify-content-between"
+									htmlFor="categorySelect"
+								>
+									<span>
+										{Liferay.Language.get(
+											'select-api-category'
+										)}
+									</span>
+
+									{schemas && (
+										<button
+											onClick={() => {
+												dispatch({
+													type: 'TOGGLE_SCHEMAS',
+												});
+											}}
+										>
+											{showSchemas
+												? Liferay.Language.get(
+														'hide-schemas'
+												  )
+												: Liferay.Language.get(
+														'show-schemas'
+												  )}
+										</button>
+									)}
+								</label>
+								<ClaySelect
+									aria-label="Select API Category"
+									onChange={(e) => {
 										dispatch({
-											path: selPath,
-											type: 'SELECT_PATH',
+											categoryKey: e.currentTarget.value,
+											type: 'SELECT_CATEGORY',
 										});
 									}}
-									paths={paths}
+									value={categoryKey}
+								>
+									{categories &&
+										Object.keys(categories).map((key) => (
+											<ClaySelect.Option
+												key={key}
+												label={key}
+												value={key}
+											/>
+										))}
+								</ClaySelect>
+							</ClayForm.Group>
+
+							<ClayForm.Group className="pt-0 px-3">
+								<label htmlFor="filter">{'Filter'}</label>
+
+								<ClayInput
+									name="filter"
+									onChange={(event) => {
+										dispatch({
+											filter: event.target.value,
+											type: 'SET_FILTER',
+										});
+									}}
+									type="text"
+									value={filter}
+								/>
+							</ClayForm.Group>
+
+							<div
+								className="api-list border-top p-3"
+								style={APIDisplayStyle}
+							>
+								{paths && (
+									<PathList
+										baseURL={categoryKey}
+										curPath={path}
+										filter={filter}
+										onClick={(selPath) => {
+											dispatch({
+												path: selPath,
+												type: 'SELECT_PATH',
+											});
+										}}
+										paths={paths}
+									/>
+								)}
+							</div>
+						</div>
+
+						<div className="border col col-md-7 overflow-auto p-3 vh-100">
+							{paths && path && method && !showSchemas && (
+								<APIDisplay />
+							)}
+
+							{!path && (
+								<ClayAlert
+									displayType="info"
+									spritemap={spritemap}
+									title="Info"
+								>
+									{Liferay.Language.get(
+										'please-select-an-api-from-the-list-on-the-left'
+									)}
+								</ClayAlert>
+							)}
+
+							{showSchemas && schemas && (
+								<SchemaExplorer
+									category={categoryKey}
+									schemas={schemas}
 								/>
 							)}
 						</div>
 					</div>
-
-					<div className="border col col-md-7 overflow-auto p-3 vh-100">
-						{paths && path && method && !showSchemas && (
-							<APIDisplay />
-						)}
-
-						{!path && (
-							<ClayAlert
-								displayType="info"
-								spritemap={spritemap}
-								title="Info"
-							>
-								{Liferay.Language.get(
-									'please-select-an-api-from-the-list-on-the-left'
-								)}
-							</ClayAlert>
-						)}
-
-						{showSchemas && schemas && (
-							<SchemaExplorer
-								category={categoryKey}
-								schemas={schemas}
-							/>
-						)}
-					</div>
-				</div>
+				)}
 			</div>
 		</div>
 	);
