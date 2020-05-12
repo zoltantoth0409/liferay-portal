eebfd2fd019b46e1cab02ed66b948171f96eaac6
JavierMoral <javier.moral@liferay.com>
2020-05-12 06:57:26 +0200

    LPS-113225 Get languages bag of group's company instead of using the company stored in CompanyThreadLocal

--- portal-impl/src/com/liferay/portal/language/LanguageImpl.java
+++ portal-impl/src/com/liferay/portal/language/LanguageImpl.java
@@ -1544,7 +1544,11 @@ public class LanguageImpl implements Language, Serializable {
 
 		try {
 			if (isInheritLocales(groupId)) {
-				return isAvailableLocale(languageId);
+				Group group = GroupLocalServiceUtil.getGroup(groupId);
+				CompanyLocalesBag companyLocalesBag = _getCompanyLocalesBag(
+					group.getCompanyId());
+
+				return companyLocalesBag.containsLanguageId(languageId);
 			}
 		}
 		catch (Exception exception) {
