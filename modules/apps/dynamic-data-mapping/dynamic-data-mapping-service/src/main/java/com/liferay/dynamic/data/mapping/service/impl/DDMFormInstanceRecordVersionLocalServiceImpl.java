858cbc637b6cfed2374be6d404b117e2e0fd3241
Marcos <marcos.martins@liferay.com>
2020-05-14 15:15:21 -0300

    LPS-112570 get latest form instance record version by form instance record id and status

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMFormInstanceRecordVersionLocalServiceImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMFormInstanceRecordVersionLocalServiceImpl.java
@@ -110,4 +110,29 @@ public class DDMFormInstanceRecordVersionLocalServiceImpl
 		return ddmFormInstanceRecordVersions.get(0);
 	}
 
+	@Override
+	public DDMFormInstanceRecordVersion getLatestFormInstanceRecordVersion(
+			long ddmFormInstanceRecordId, int status)
+		throws PortalException {
+
+		List<DDMFormInstanceRecordVersion> ddmFormInstanceRecordVersions =
+			ddmFormInstanceRecordVersionPersistence.findByF_S(
+				ddmFormInstanceRecordId, status);
+
+		if (ddmFormInstanceRecordVersions.isEmpty()) {
+			throw new NoSuchFormInstanceRecordVersionException(
+				"No form instance record versions found for form instance ID " +
+					ddmFormInstanceRecordId);
+		}
+
+		ddmFormInstanceRecordVersions = ListUtil.copy(
+			ddmFormInstanceRecordVersions);
+
+		Collections.sort(
+			ddmFormInstanceRecordVersions,
+			new FormInstanceRecordVersionVersionComparator());
+
+		return ddmFormInstanceRecordVersions.get(0);
+	}
+
 }
