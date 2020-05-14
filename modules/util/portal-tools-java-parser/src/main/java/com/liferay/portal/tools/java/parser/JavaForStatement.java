6dce52d8d350db450f50401d98cb59a670bcf927
Hugo Huijser <hugo.huijser@liferay.com>
2020-05-14 11:10:43 +0800

    LPS-105380 Improve parsing for for-statement with long expressions

--- modules/util/portal-tools-java-parser/src/main/java/com/liferay/portal/tools/java/parser/JavaForStatement.java
+++ modules/util/portal-tools-java-parser/src/main/java/com/liferay/portal/tools/java/parser/JavaForStatement.java
@@ -132,6 +132,13 @@ public class JavaForStatement extends BaseJavaLoopStatement {
 					sb.append(indent);
 				}
 			}
+			else if (getLineLength(getIndent(getLastLine(sb))) > getLineLength(
+						indent)) {
+
+				appendNewLine(
+					sb, _conditionJavaExpression, indent, "",
+					conditionJavaExpressionSuffix, maxLineLength);
+			}
 			else {
 				append(
 					sb, _conditionJavaExpression, indent, "",
@@ -140,9 +147,9 @@ public class JavaForStatement extends BaseJavaLoopStatement {
 		}
 
 		if (!_iteratorJavaExpressions.isEmpty()) {
-			String lastLineIndent = getIndent(getLastLine(sb));
+			if (getLineLength(getIndent(getLastLine(sb))) > getLineLength(
+					indent)) {
 
-			if (lastLineIndent.length() > indent.length()) {
 				appendNewLine(
 					sb, _iteratorJavaExpressions, indent,
 					_getIteratorJavaExpressionsPrefix(prefix), ")" + suffix,
