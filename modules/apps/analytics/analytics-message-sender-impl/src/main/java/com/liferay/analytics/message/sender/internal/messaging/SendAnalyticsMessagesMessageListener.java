953fe31c0de1f5a412cfa5a9fdb07b9687b6b4dc
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-05-13 19:40:50 -0700

    LPS-108588 We should get the companies from the company table in both cases

--- modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/messaging/SendAnalyticsMessagesMessageListener.java
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/messaging/SendAnalyticsMessagesMessageListener.java
@@ -29,6 +29,7 @@ import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
 import com.liferay.portal.kernel.scheduler.SchedulerEntry;
@@ -36,6 +37,7 @@ import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
 import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 
 import java.nio.charset.StandardCharsets;
 
@@ -85,10 +87,8 @@ public class SendAnalyticsMessagesMessageListener extends BaseMessageListener {
 			return;
 		}
 
-		for (long curCompanyId :
-				_analyticsMessageLocalService.getCompanyIds()) {
-
-			_process(curCompanyId);
+		for (Company company : _companyLocalService.getCompanies(false)) {
+			_process(company.getCompanyId());
 		}
 	}
 
@@ -190,6 +190,9 @@ public class SendAnalyticsMessagesMessageListener extends BaseMessageListener {
 
 	@Reference
 	private AnalyticsMessageSenderClient _analyticsMessageSenderClient;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
 
 	@Reference
 	private SchedulerEngineHelper _schedulerEngineHelper;
