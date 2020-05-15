4fce6f45ab60093bb39d8f8bb77d20ba14dfa2b0
Chema Balsas <jose.balsas@liferay.com>
2020-05-15 19:06:28 +0200

    LPS-113694 Adds clay:sheet-* set of tags

---
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/SheetHeaderTag.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.servlet.taglib;
+
+import com.liferay.frontend.taglib.clay.internal.servlet.taglib.BaseContainerTag;
+
+import java.util.Set;
+
+import javax.servlet.jsp.JspException;
+
+/**
+ * @author Chema Balsas
+ */
+public class SheetHeaderTag extends BaseContainerTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		return super.doStartTag();
+	}
+
+	@Override
+	protected String _getClassName(Set className) {
+		className.add("sheet-header");
+
+		return super._getClassName(className);
+	}
+
+	@Override
+	protected String getEndPage() {
+		return _END_PAGE;
+	}
+
+	@Override
+	protected String getStartPage() {
+		return _START_PAGE;
+	}
+
+	private static final String _ATTRIBUTE_NAMESPACE = "clay:sheet-header:";
+
+	private static final String _END_PAGE = "/sheet_header/end.jsp";
+
+	private static final String _START_PAGE = "/sheet_header/start.jsp";
+
+}
---
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/servlet/taglib/SheetTag.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.servlet.taglib;
+
+import com.liferay.frontend.taglib.clay.internal.servlet.taglib.BaseContainerTag;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Set;
+
+import javax.servlet.jsp.JspException;
+
+/**
+ * @author Chema Balsas
+ */
+public class SheetTag extends BaseContainerTag {
+
+	@Override
+	public int doStartTag() throws JspException {
+		setAttributeNamespace(_ATTRIBUTE_NAMESPACE);
+
+		return super.doStartTag();
+	}
+
+	public String getSize() {
+		return _size;
+	}
+
+	public void setSize(String size) {
+		_size = size;
+	}
+
+	@Override
+	protected String _getClassName(Set className) {
+		className.add("sheet");
+
+		if (Validator.isNotNull(_size)) {
+			className.add("sheet-" + _size);
+		}
+
+		return super._getClassName(className);
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_size = "lg";
+	}
+
+	@Override
+	protected String getEndPage() {
+		return _END_PAGE;
+	}
+
+	@Override
+	protected String getStartPage() {
+		return _START_PAGE;
+	}
+
+	private static final String _ATTRIBUTE_NAMESPACE = "clay:sheet:";
+
+	private static final String _END_PAGE = "/sheet/end.jsp";
+
+	private static final String _START_PAGE = "/sheet/start.jsp";
+
+	private String _size = "lg";
+
+}
