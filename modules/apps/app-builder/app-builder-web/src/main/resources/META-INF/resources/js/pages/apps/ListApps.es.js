ad5f3a99e6a2abb6afbb2555c7c75fd615c0690c
Gabriel Ramos <gabriel.ramos@liferay.com>
2020-05-15 10:12:24 -0300

    LPS-111264 | LPS-112846 apply NewAppPopover on ListApps

--- modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/apps/ListApps.es.js
+++ modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/apps/ListApps.es.js
@@ -13,26 +13,67 @@
  */
 
 import ClayLabel from '@clayui/label';
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import {Link} from 'react-router-dom';
 
 import {AppContext} from '../../AppContext.es';
 import Button from '../../components/button/Button.es';
 import ListView from '../../components/list-view/ListView.es';
 import useDeployApp from '../../hooks/useDeployApp.es';
+import isClickOutside from '../../utils/clickOutside.es';
 import {confirmDelete} from '../../utils/client.es';
 import {fromNow} from '../../utils/time.es';
 import {concatValues} from '../../utils/utils.es';
+import NewAppPopover from './NewAppPopover.es';
 import {DEPLOYMENT_ACTION, DEPLOYMENT_TYPES, STATUSES} from './constants.es';
 
 export default ({
+	history,
 	match: {
 		params: {dataDefinitionId, objectType},
 		url,
 	},
 }) => {
 	const {getStandaloneURL, userId} = useContext(AppContext);
+	const addButtonRef = useRef();
+	const emptyStateButtonRef = useRef();
+	const popoverRef = useRef();
+
 	const {deployApp, undeployApp} = useDeployApp();
+
+	const [alignElement, setAlignElement] = useState(addButtonRef.current);
+	const [isPopoverVisible, setPopoverVisible] = useState(false);
+
+	const onClickAddButton = ({currentTarget}) => {
+		setAlignElement(currentTarget);
+
+		if (isPopoverVisible && alignElement !== currentTarget) {
+			return;
+		}
+
+		setPopoverVisible(!isPopoverVisible);
+	};
+
+	const onCancel = () => setPopoverVisible(false);
+
+	useEffect(() => {
+		const handler = ({target}) => {
+			const isOutside = isClickOutside(
+				target,
+				addButtonRef.current,
+				emptyStateButtonRef.current,
+				popoverRef.current
+			);
+
+			if (isOutside) {
+				setPopoverVisible(false);
+			}
+		};
+
+		window.addEventListener('click', handler);
+
+		return () => window.removeEventListener('click', handler);
+	}, [addButtonRef, emptyStateButtonRef, popoverRef]);
 
 	const ACTIONS = [
 		{
@@ -87,11 +128,8 @@ export default ({
 		},
 	];
 
-	let EMPTY_STATE = {
-		title: Liferay.Language.get('there-are-no-apps-yet'),
-	};
-
 	let ENDPOINT = `/o/app-builder/v1.0/apps`;
+	let buttonProps;
 
 	const FILTERS = [
 		{
@@ -121,75 +159,98 @@ export default ({
 	];
 
 	if (dataDefinitionId) {
-		EMPTY_STATE = {
-			...EMPTY_STATE,
-			button: () => (
-				<Button displayType="secondary" href={`${url}/deploy`}>
-					{Liferay.Language.get('new-app')}
-				</Button>
-			),
-			description: Liferay.Language.get(
-				'select-the-form-and-table-view-you-want-and-deploy-your-app-as-a-widget-standalone-or-place-it-in-the-product-menu'
-			),
-		};
-
 		ENDPOINT = `/o/app-builder/v1.0/data-definitions/${dataDefinitionId}/apps`;
+		buttonProps = {href: `${url}/deploy`};
 	}
 	else {
 		const [firstColumn, ...otherColumns] = COLUMNS;
 
 		COLUMNS = [
 			firstColumn,
-			{key: 'dataDefinitionName', value: Liferay.Language.get('object')},
+			{
+				key: 'dataDefinitionName',
+				value: Liferay.Language.get('object'),
+			},
 			...otherColumns,
 		];
+		buttonProps = {
+			onClick: (currentTarget) => onClickAddButton(currentTarget),
+		};
 	}
 
+	const EMPTY_STATE = {
+		button: () => (
+			<Button
+				{...buttonProps}
+				displayType="secondary"
+				ref={emptyStateButtonRef}
+			>
+				{Liferay.Language.get('new-app')}
+			</Button>
+		),
+		description: Liferay.Language.get(
+			'select-the-form-and-table-view-you-want-and-deploy-your-app-as-a-widget-standalone-or-place-it-in-the-product-menu'
+		),
+		title: Liferay.Language.get('there-are-no-apps-yet'),
+	};
+
 	return (
-		<ListView
-			actions={ACTIONS}
-			addButton={
-				dataDefinitionId &&
-				(() => (
+		<>
+			<ListView
+				actions={ACTIONS}
+				addButton={() => (
 					<Button
 						className="nav-btn nav-btn-monospaced"
-						href={`${url}/deploy`}
+						ref={addButtonRef}
 						symbol="plus"
 						tooltip={Liferay.Language.get('new-app')}
+						{...buttonProps}
 					/>
-				))
-			}
-			columns={COLUMNS}
-			emptyState={EMPTY_STATE}
-			endpoint={ENDPOINT}
-			filters={!dataDefinitionId ? FILTERS : []}
-		>
-			{(item) => ({
-				...item,
-				active: item.active,
-				dateCreated: fromNow(item.dateCreated),
-				dateModified: fromNow(item.dateModified),
-				name: dataDefinitionId ? (
-					<Link
-						to={`/${objectType}/${dataDefinitionId}/apps/${item.id}`}
-					>
-						{item.name.en_US}
-					</Link>
-				) : (
-					item.name.en_US
-				),
-				nameText: item.name.en_US,
-				status: (
-					<ClayLabel
-						displayType={item.active ? 'success' : 'secondary'}
-					>
-						{STATUSES[item.active ? 'active' : 'inactive']}
-					</ClayLabel>
-				),
-				type: concatValues(
-					item.appDeployments.map(({type}) => DEPLOYMENT_TYPES[type])
-				),
-			})}
-		</ListView>
+				)}
+				columns={COLUMNS}
+				emptyState={EMPTY_STATE}
+				endpoint={ENDPOINT}
+				filters={!dataDefinitionId ? FILTERS : []}
+			>
+				{(item) => ({
+					...item,
+					dateCreated: fromNow(item.dateCreated),
+					dateModified: fromNow(item.dateModified),
+					name: dataDefinitionId ? (
+						<Link
+							to={`/${objectType}/${dataDefinitionId}/apps/${item.id}`}
+						>
+							{item.name.en_US}
+						</Link>
+					) : (
+						item.name.en_US
+					),
+					nameText: item.name.en_US,
+					status: (
+						<ClayLabel
+							displayType={item.active ? 'success' : 'secondary'}
+						>
+							{STATUSES[item.active ? 'active' : 'inactive']}
+						</ClayLabel>
+					),
+					type: concatValues(
+						item.appDeployments.map(
+							({type}) => DEPLOYMENT_TYPES[type]
+						)
+					),
+				})}
+			</ListView>
+
+			{!dataDefinitionId && (
+				<NewAppPopover
+					alignElement={alignElement}
+					history={history}
+					onCancel={onCancel}
+					ref={popoverRef}
+					setVisible={setPopoverVisible}
+					visible={isPopoverVisible}
+				/>
+			)}
+		</>
 	);
 };
