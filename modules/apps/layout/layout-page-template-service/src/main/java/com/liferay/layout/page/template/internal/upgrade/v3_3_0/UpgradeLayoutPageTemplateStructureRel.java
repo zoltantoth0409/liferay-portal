df2376aff9d1f77744f6eaf5996eb1b468ad13e0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-05-18 16:53:22 +0200

    LPS-112538 Don't update the classedModel

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/v3_3_0/UpgradeLayoutPageTemplateStructureRel.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/v3_3_0/UpgradeLayoutPageTemplateStructureRel.java
@@ -156,7 +156,8 @@ public class UpgradeLayoutPageTemplateStructureRel extends UpgradeProcess {
 					fragmentLayoutStructureItem.getFragmentEntryLinkId(),
 					EditableValuesTransformerUtil.getEditableValues(
 						fragmentEntryLink.getEditableValues(),
-						segmentsExperienceId));
+						segmentsExperienceId),
+					false);
 
 				continue;
 			}
