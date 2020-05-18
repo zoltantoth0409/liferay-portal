14966073488203a87e4c792440fccccd85039431
Victor Galan <victor.galan@liferay.com>
2020-05-18 12:34:04 +0200

    LPS-113654 Wait for a dispatch undo action to initiate the next. Otherwise we may end up dispatching several request at the same time and the server could potentially fail

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/thunks/undo.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/thunks/undo.js
@@ -15,6 +15,8 @@
 import {UPDATE_UNDO_ACTIONS} from '../actions/types';
 import {undoAction} from '../components/undo/undoActions';
 
+let promise = Promise.resolve();
+
 export default function undo({store}) {
 	return (dispatch) => {
 		if (!store.undoHistory || store.undoHistory.length === 0) {
@@ -25,6 +27,8 @@ export default function undo({store}) {
 
 		dispatch({type: UPDATE_UNDO_ACTIONS, undoHistory: undos});
 
-		undoAction({action: lastUndo, store})(dispatch);
+		promise = promise.then(() =>
+			undoAction({action: lastUndo, store})(dispatch)
+		);
 	};
 }
