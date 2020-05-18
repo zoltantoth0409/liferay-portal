e57aea6f70f610539ae89166bf3e69c9de4fcba1
Alicia Garcia <alicia.garcia@liferay.com>
2020-05-18 14:23:19 +0200

    LPS-113974 create a render command copy of the existent to data definition structures

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/portlet/action/EditFileEntryTypeDataDefinitionMVCRenderCommand.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.portlet.action;
+
+import com.liferay.document.library.constants.DLPortletKeys;
+import com.liferay.document.library.kernel.exception.NoSuchFileEntryTypeException;
+import com.liferay.document.library.kernel.model.DLFileEntryMetadata;
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.document.library.kernel.service.DLFileEntryTypeService;
+import com.liferay.document.library.kernel.util.DLUtil;
+import com.liferay.document.library.web.internal.constants.DLWebKeys;
+import com.liferay.document.library.web.internal.display.context.DLEditFileEntryTypeDisplayContext;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStorageLinkLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.util.DDM;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alexander Chow
+ * @author Sergio González
+ */
+@Component(
+	configurationPid = "com.liferay.document.library.configuration.FFDocumentLibraryDDMEditorConfiguration",
+	property = {
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
+		"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN,
+		"javax.portlet.name=" + DLPortletKeys.MEDIA_GALLERY_DISPLAY,
+		"mvc.command.name=/document_library/edit_file_entry_type_data_definition"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditFileEntryTypeDataDefinitionMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			renderRequest.setAttribute(
+				DLWebKeys.
+					DOCUMENT_LIBRARY_EDIT_EDIT_FILE_ENTRY_TYPE_DISPLAY_CONTEXT,
+				new DLEditFileEntryTypeDisplayContext(
+					_ddm, _ddmStorageLinkLocalService,
+					_ddmStructureLocalService, _language,
+					_portal.getLiferayPortletRequest(renderRequest),
+					_portal.getLiferayPortletResponse(renderResponse)));
+
+			long fileEntryTypeId = ParamUtil.getLong(
+				renderRequest, "fileEntryTypeId");
+
+			if (fileEntryTypeId <= 0) {
+				return "/document_library/edit_file_entry_type.jsp";
+			}
+
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+			DLFileEntryType dlFileEntryType =
+				_dlFileEntryTypeService.getFileEntryType(fileEntryTypeId);
+
+			_dlFileEntryTypeModelResourcePermission.check(
+				themeDisplay.getPermissionChecker(), dlFileEntryType,
+				ActionKeys.UPDATE);
+
+			renderRequest.setAttribute(
+				WebKeys.DOCUMENT_LIBRARY_FILE_ENTRY_TYPE, dlFileEntryType);
+
+			renderRequest.setAttribute(
+				WebKeys.DOCUMENT_LIBRARY_DYNAMIC_DATA_MAPPING_STRUCTURE,
+				_getDDMStructure(dlFileEntryType));
+
+			return "/document_library/edit_file_entry_type.jsp";
+		}
+		catch (NoSuchFileEntryTypeException | PrincipalException exception) {
+			SessionErrors.add(renderRequest, exception.getClass());
+
+			return "/document_library/error.jsp";
+		}
+		catch (PortalException portalException) {
+			throw new PortletException(portalException);
+		}
+	}
+
+	private DDMStructure _getDDMStructure(DLFileEntryType dlFileEntryType) {
+		DDMStructure ddmStructure = _ddmStructureLocalService.fetchStructure(
+			dlFileEntryType.getGroupId(),
+			_portal.getClassNameId(DLFileEntryMetadata.class),
+			DLUtil.getDDMStructureKey(dlFileEntryType));
+
+		if (ddmStructure == null) {
+			ddmStructure = _ddmStructureLocalService.fetchStructure(
+				dlFileEntryType.getGroupId(),
+				_portal.getClassNameId(DLFileEntryMetadata.class),
+				DLUtil.getDeprecatedDDMStructureKey(dlFileEntryType));
+		}
+
+		if (ddmStructure == null) {
+			ddmStructure = _ddmStructureLocalService.fetchStructure(
+				dlFileEntryType.getGroupId(),
+				_portal.getClassNameId(DLFileEntryMetadata.class),
+				dlFileEntryType.getFileEntryTypeKey());
+		}
+
+		return ddmStructure;
+	}
+
+	@Reference
+	private DDM _ddm;
+
+	@Reference
+	private DDMStorageLinkLocalService _ddmStorageLinkLocalService;
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.dynamic.data.mapping.model.DDMStructure)"
+	)
+	private ModelResourcePermission<DDMStructure>
+		_ddmStructureModelResourcePermission;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.document.library.kernel.model.DLFileEntryType)"
+	)
+	private volatile ModelResourcePermission<DLFileEntryType>
+		_dlFileEntryTypeModelResourcePermission;
+
+	@Reference
+	private DLFileEntryTypeService _dlFileEntryTypeService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
+}
