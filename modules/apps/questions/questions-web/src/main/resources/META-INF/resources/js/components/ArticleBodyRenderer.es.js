391a08c591a96c86244b56f56b06b9a894164d44
David Gomez G <dgomezg@gmail.com>
2020-05-19 15:27:39 +0200

    LPS-111902 Question is shown using CKEditor in readonly mode

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/ArticleBodyRenderer.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/ArticleBodyRenderer.es.js
@@ -12,17 +12,51 @@
  * details.
  */
 
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 import parser from 'bbcode-to-react';
+import {Editor} from 'frontend-editor-ckeditor-web';
 import React from 'react';
+
+export function getCKEditorConfig() {
+	const config = {
+		allowedContent: true,
+		autoGrow_minHeight: 200,
+		autoGrow_onStartup: true,
+		codeSnippet_theme: 'monokai_sublime',
+		extraPlugins: 'autogrow,codesnippet',
+		readOnly:true,
+		removePlugins: 'elementspath',
+	};
+
+	config.toolbar = [];
+
+	return config;
+}
 
 export default ({articleBody, encodingFormat, id, signature}) => (
 	<>
 		{encodingFormat === 'bbcode' && <p>{parser.toReact(articleBody)}</p>}
 		{encodingFormat !== 'bbcode' && (
-			<div
-				className={`questions-article-body-${id}`}
-				dangerouslySetInnerHTML={{__html: articleBody}}
-			/>
+			<div className={`cke_readonly questions-article-body-${id}`}>
+				<Editor
+					config={getCKEditorConfig()}
+					data={articleBody}
+					required
+				/>
+			</div>
 		)}
 
 		{signature && (
