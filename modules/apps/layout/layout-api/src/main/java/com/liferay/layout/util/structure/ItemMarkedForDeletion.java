e4e2720e6a69534abd720f6f9fe16092394910c6
Victor Galan <victor.galan@liferay.com>
2020-05-19 11:08:06 +0200

    LPS-113673 Create type that holds the data about a marked item for deletion. PortletIds are the portletIds of the instanceable portlet that the item holds as it can be a fragment or a container containing other fragments

---
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/ItemMarkedForDeletion.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.util.structure;
+
+import com.liferay.petra.lang.HashUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author Víctor Galán
+ */
+public class ItemMarkedForDeletion {
+
+	public static ItemMarkedForDeletion of(JSONObject jsonObject) {
+		if (jsonObject == null) {
+			return new ItemMarkedForDeletion(
+				StringPool.BLANK, Collections.emptySet());
+		}
+
+		return new ItemMarkedForDeletion(
+			jsonObject.getString("itemId"),
+			JSONUtil.toStringSet(jsonObject.getJSONArray("portletIds")));
+	}
+
+	public ItemMarkedForDeletion(String itemId, Set<String> portletIds) {
+		_itemId = itemId;
+		_portletIds = portletIds;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof CollectionLayoutStructureItem)) {
+			return false;
+		}
+
+		ItemMarkedForDeletion itemMarkedForDeletion =
+			(ItemMarkedForDeletion)obj;
+
+		if (Objects.equals(_itemId, itemMarkedForDeletion._itemId)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public String getItemId() {
+		return _itemId;
+	}
+
+	public Set<String> getPortletIds() {
+		return _portletIds;
+	}
+
+	@Override
+	public int hashCode() {
+		return HashUtil.hash(0, _itemId);
+	}
+
+	public JSONObject toJSONObject() {
+		return JSONUtil.put(
+			"itemId", _itemId
+		).put(
+			"portletIds", _portletIds
+		);
+	}
+
+	private final String _itemId;
+	private final Set<String> _portletIds;
+
+}
