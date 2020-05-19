125871900a28eca0d0b1a3791e470b4a31535870
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-05-19 12:22:53 -0300

    LPS-111266 | LPS-113551 Index the scope information as well

--- modules/apps/app-builder/app-builder-service/src/main/java/com/liferay/app/builder/internal/search/spi/model/index/contributor/AppBuilderAppModelDocumentContributor.java
+++ modules/apps/app-builder/app-builder-service/src/main/java/com/liferay/app/builder/internal/search/spi/model/index/contributor/AppBuilderAppModelDocumentContributor.java
@@ -74,6 +74,8 @@ public class AppBuilderAppModelDocumentContributor
 				appBuilderApp.getDefaultLanguageId(),
 				appBuilderApp.getGroupId()),
 			true, true);
+
+		document.addKeyword("scope", appBuilderApp.getScope());
 	}
 
 	protected String[] getLanguageIds(
