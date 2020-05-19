0b6881878171b8f1d721f0170492f46a7ab7b97a
drewbrokke <drew.brokke@liferay.com>
2020-05-19 17:11:46 -0500

    LPS-111508 account-admin-web: updates usages

--- modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/EditAccountEntryMVCActionCommand.java
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/EditAccountEntryMVCActionCommand.java
@@ -65,10 +65,11 @@ public class EditAccountEntryMVCActionCommand extends BaseMVCActionCommand {
 		String name = ParamUtil.getString(actionRequest, "name");
 		String description = ParamUtil.getString(actionRequest, "description");
 		String domains = ParamUtil.getString(actionRequest, "domains");
+		String taxId = ParamUtil.getString(actionRequest, "taxId");
 
 		return _accountEntryLocalService.addAccountEntry(
 			themeDisplay.getUserId(), parentAccountEntryId, name, description,
-			StringUtil.split(domains), _getLogoBytes(actionRequest),
+			StringUtil.split(domains), _getLogoBytes(actionRequest), taxId,
 			_getStatus(actionRequest),
 			ServiceContextFactory.getInstance(
 				AccountEntry.class.getName(), actionRequest));
@@ -130,10 +131,11 @@ public class EditAccountEntryMVCActionCommand extends BaseMVCActionCommand {
 		String description = ParamUtil.getString(actionRequest, "description");
 		boolean deleteLogo = ParamUtil.getBoolean(actionRequest, "deleteLogo");
 		String domains = ParamUtil.getString(actionRequest, "domains");
+		String taxId = ParamUtil.getString(actionRequest, "taxId");
 
 		_accountEntryLocalService.updateAccountEntry(
 			accountEntryId, parentAccountEntryId, name, description, deleteLogo,
-			StringUtil.split(domains), _getLogoBytes(actionRequest),
+			StringUtil.split(domains), _getLogoBytes(actionRequest), taxId,
 			_getStatus(actionRequest),
 			ServiceContextFactory.getInstance(
 				AccountEntry.class.getName(), actionRequest));
