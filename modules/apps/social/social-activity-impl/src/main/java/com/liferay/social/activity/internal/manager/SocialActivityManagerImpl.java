4871659c6da5fdbdf500eb13d6c841c8112b11cc
Adolfo Pérez <adolfo.perez@liferay.com>
2020-05-19 14:33:40 +0200

    LPS-110593 Disable tracking if not enabled

--- modules/apps/social/social-activity-impl/src/main/java/com/liferay/social/activity/internal/manager/SocialActivityManagerImpl.java
+++ modules/apps/social/social-activity-impl/src/main/java/com/liferay/social/activity/internal/manager/SocialActivityManagerImpl.java
@@ -17,14 +17,22 @@ package com.liferay.social.activity.internal.manager;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.ClassedModel;
 import com.liferay.portal.kernel.model.GroupedModel;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.social.BaseSocialActivityManager;
 import com.liferay.portal.kernel.social.SocialActivityManager;
+import com.liferay.social.activity.internal.configuration.SocialActivityCompanyConfiguration;
+import com.liferay.social.activity.internal.configuration.SocialActivitySystemConfiguration;
 import com.liferay.social.kernel.service.SocialActivityLocalService;
 
 import java.util.Date;
+import java.util.Map;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
@@ -35,7 +43,10 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Adolfo Pérez
  */
-@Component(service = SocialActivityManager.class)
+@Component(
+	configurationPid = "com.liferay.social.activity.internal.configuration.SocialActivitySystemConfiguration",
+	service = SocialActivityManager.class
+)
 public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 	implements SocialActivityManager<T> {
 
@@ -44,6 +55,10 @@ public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 			long userId, T model, int type, String extraData,
 			long receiverUserId)
 		throws PortalException {
+
+		if (!_isEnabled(model.getCompanyId())) {
+			return;
+		}
 
 		SocialActivityManager<T> socialActivityManager =
 			getSocialActivityManager(model.getModelClassName());
@@ -58,6 +73,10 @@ public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 			long receiverUserId)
 		throws PortalException {
 
+		if (!_isEnabled(model.getCompanyId())) {
+			return;
+		}
+
 		SocialActivityManager<T> socialActivityManager =
 			getSocialActivityManager(model.getModelClassName());
 
@@ -71,6 +90,10 @@ public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 			long receiverUserId)
 		throws PortalException {
 
+		if (!_isEnabled(model.getCompanyId())) {
+			return;
+		}
+
 		SocialActivityManager<T> socialActivityManager =
 			getSocialActivityManager(model.getModelClassName());
 
@@ -80,6 +103,10 @@ public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 
 	@Override
 	public void deleteActivities(T model) throws PortalException {
+		if (!_isEnabled(model.getCompanyId())) {
+			return;
+		}
+
 		SocialActivityManager<T> socialActivityManager =
 			getSocialActivityManager(model.getModelClassName());
 
@@ -91,6 +118,10 @@ public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 			long userId, T model, int type, Date createDate)
 		throws PortalException {
 
+		if (!_isEnabled(model.getCompanyId())) {
+			return;
+		}
+
 		SocialActivityManager<T> socialActivityManager =
 			getSocialActivityManager(model.getModelClassName());
 
@@ -99,7 +130,9 @@ public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 	}
 
 	@Activate
-	protected void activate(BundleContext bundleContext) {
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
 		_defaultSocialActivityManager = new BaseSocialActivityManager<T>() {
 
 			@Override
@@ -116,6 +149,10 @@ public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 			(Class<SocialActivityManager<T>>)(Class)SocialActivityManager.class,
 			"(model.class.name=*)",
 			new PropertyServiceReferenceMapper<>("model.class.name"));
+
+		_socialActivitySystemConfiguration =
+			ConfigurableUtil.createConfigurable(
+				SocialActivitySystemConfiguration.class, properties);
 	}
 
 	@Deactivate
@@ -136,11 +173,49 @@ public class SocialActivityManagerImpl<T extends ClassedModel & GroupedModel>
 		return _defaultSocialActivityManager;
 	}
 
+	private boolean _isEnabled(long companyId) {
+		try {
+			if (!_socialActivitySystemConfiguration.
+					enableUserSocialActivityTracking()) {
+
+				return false;
+			}
+
+			SocialActivityCompanyConfiguration
+				socialActivityCompanyConfiguration =
+					_configurationProvider.getCompanyConfiguration(
+						SocialActivityCompanyConfiguration.class, companyId);
+
+			if ((socialActivityCompanyConfiguration != null) &&
+				socialActivityCompanyConfiguration.
+					enableUserSocialActivityTracking()) {
+
+				return true;
+			}
+
+			return false;
+		}
+		catch (ConfigurationException configurationException) {
+			_log.error(configurationException, configurationException);
+
+			return false;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SocialActivityManagerImpl.class);
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
 	private SocialActivityManager<T> _defaultSocialActivityManager;
 	private ServiceTrackerMap<String, SocialActivityManager<T>>
 		_serviceTrackerMap;
 
 	@Reference
 	private SocialActivityLocalService _socialActivityLocalService;
+
+	private SocialActivitySystemConfiguration
+		_socialActivitySystemConfiguration;
 
 }
