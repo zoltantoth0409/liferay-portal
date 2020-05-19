ec78eac524dd8a05661fcdcd4a4943cce4089187
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-05-19 12:27:28 -0300

    LPS-111266 | LPS-113551 Create an upgrade to create and populate the "scope" column

--- modules/apps/app-builder/app-builder-service/src/main/java/com/liferay/app/builder/internal/upgrade/AppBuilderServiceUpgrade.java
+++ modules/apps/app-builder/app-builder-service/src/main/java/com/liferay/app/builder/internal/upgrade/AppBuilderServiceUpgrade.java
@@ -14,7 +14,6 @@
 
 package com.liferay.app.builder.internal.upgrade;
 
-import com.liferay.app.builder.internal.upgrade.v2_0_0.UpgradeAppBuilderApp;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
@@ -26,7 +25,15 @@ import org.osgi.service.component.annotations.Component;
 public class AppBuilderServiceUpgrade implements UpgradeStepRegistrator {
 
 	public void register(Registry registry) {
-		registry.register("1.0.0", "2.0.0", new UpgradeAppBuilderApp());
+		registry.register(
+			"1.0.0", "2.0.0",
+			new com.liferay.app.builder.internal.upgrade.v2_0_0.
+				UpgradeAppBuilderApp());
+
+		registry.register(
+			"2.0.0", "2.1.0",
+			new com.liferay.app.builder.internal.upgrade.v2_1_0.
+				UpgradeAppBuilderApp());
 	}
 
 }
---
+++ modules/apps/app-builder/app-builder-service/src/main/java/com/liferay/app/builder/internal/upgrade/v2_1_0/UpgradeAppBuilderApp.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.app.builder.internal.upgrade.v2_1_0;
+
+import com.liferay.app.builder.constants.AppBuilderAppConstants;
+import com.liferay.app.builder.internal.upgrade.v2_1_0.util.AppBuilderAppTable;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.sql.PreparedStatement;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class UpgradeAppBuilderApp extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		if (!hasColumn(AppBuilderAppTable.TABLE_NAME, "scope")) {
+			alter(
+				AppBuilderAppTable.class,
+				new AlterTableAddColumn("scope", "VARCHAR(75) null"));
+
+			try (PreparedStatement ps = connection.prepareStatement(
+					"update AppBuilderApp set scope = ?")) {
+
+				ps.setString(1, AppBuilderAppConstants.STANDARD_APP_SCOPE);
+
+				ps.execute();
+			}
+		}
+	}
+
+}
