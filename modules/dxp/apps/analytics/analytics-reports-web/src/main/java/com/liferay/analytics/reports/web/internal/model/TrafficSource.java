ba41676044002bd14bb8a1d1473520aa8d48abf7
David Arques <davidarchena@gmail.com>
2020-05-20 13:24:01 +0200

    LPS-110727 Include search keywords as part of traffic source

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/model/TrafficSource.java
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/model/TrafficSource.java
@@ -14,10 +14,13 @@
 
 package com.liferay.analytics.reports.web.internal.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -28,8 +31,12 @@ public class TrafficSource {
 	public TrafficSource() {
 	}
 
-	public TrafficSource(String name, int trafficAmount, double trafficShare) {
+	public TrafficSource(
+		String name, List<SearchKeyword> searchKeywords, int trafficAmount,
+		double trafficShare) {
+
 		_name = name;
+		_searchKeywords = searchKeywords;
 		_trafficAmount = trafficAmount;
 		_trafficShare = trafficShare;
 	}
@@ -47,6 +54,7 @@ public class TrafficSource {
 		TrafficSource trafficSource = (TrafficSource)obj;
 
 		if (Objects.equals(_name, trafficSource._name) &&
+			Objects.equals(_searchKeywords, trafficSource._searchKeywords) &&
 			Objects.equals(_trafficAmount, trafficSource._trafficAmount) &&
 			Objects.equals(_trafficShare, trafficSource._trafficShare)) {
 
@@ -60,6 +68,11 @@ public class TrafficSource {
 		return _name;
 	}
 
+	@JsonProperty("keywords")
+	public List<SearchKeyword> getSearchKeywords() {
+		return _searchKeywords;
+	}
+
 	public int getTrafficAmount() {
 		return _trafficAmount;
 	}
@@ -70,11 +83,16 @@ public class TrafficSource {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(_name, _trafficAmount, _trafficShare);
+		return Objects.hash(
+			_name, _searchKeywords, _trafficAmount, _trafficShare);
 	}
 
 	public void setName(String name) {
 		_name = name;
+	}
+
+	public void setSearchKeywords(List<SearchKeyword> searchKeywords) {
+		_searchKeywords = searchKeywords;
 	}
 
 	public void setTrafficAmount(int trafficAmount) {
@@ -104,6 +122,7 @@ public class TrafficSource {
 	}
 
 	private String _name;
+	private List<SearchKeyword> _searchKeywords;
 	private int _trafficAmount;
 	private double _trafficShare;
 
