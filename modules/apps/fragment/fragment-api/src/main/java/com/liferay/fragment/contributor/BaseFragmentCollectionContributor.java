03d28296b08005936d266227d91cb5274a59cba6
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-05-21 23:54:11 +0200

    LPS-113706 Read icon from config file

--- modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/contributor/BaseFragmentCollectionContributor.java
+++ modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/contributor/BaseFragmentCollectionContributor.java
@@ -299,6 +299,7 @@ public abstract class BaseFragmentCollectionContributor
 		fragmentEntry.setName(name);
 		fragmentEntry.setCss(css);
 		fragmentEntry.setHtml(html);
+		fragmentEntry.setIcon(jsonObject.getString("icon", "picture"));
 		fragmentEntry.setJs(js);
 		fragmentEntry.setConfiguration(configuration);
 		fragmentEntry.setType(type);
