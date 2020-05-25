2eb7d5ad10148d2dbdcc738a15d53ecfb5c34561
Matuzalém Teles <matuzalemteles@gmail.com>
2020-05-25 13:57:10 -0300

    LPS-112424 rewrites the Tabs container in React

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/components/Tabs.es.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayTabs from '@clayui/tabs';
+import React from 'react';
+
+import {PAGE_TYPES, usePage} from '../hooks/usePage.es';
+
+export const Tabs = ({activePage, pageIndex, pages}) => {
+	const {dispatch} = usePage();
+
+	return (
+		<nav className="component-tbar ddm-form-tabs mb-3 tbar">
+			<div className="container-fluid pr-0">
+				<ClayTabs>
+					{pages.map((page, index) => (
+						<ClayTabs.Item
+							active={pageIndex === activePage}
+							disabled={!page.enabled}
+							key={index}
+							onClick={() =>
+								dispatch({
+									payload: {pageIndex: index},
+									type: PAGE_TYPES.PAGINATION,
+								})
+							}
+						>
+							<span className="navbar-text-truncate">
+								{page.title}
+							</span>
+						</ClayTabs.Item>
+					))}
+				</ClayTabs>
+			</div>
+		</nav>
+	);
+};
