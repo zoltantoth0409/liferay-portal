8543eb906e6010598cd0229833870d4cb6a6ee55
Albino Junior <albino.junior@liferay.com>
2020-05-25 11:31:46 -0300

    LPS-111728 | LPS-114271 Add jest configuration

---
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/jest-setup.config.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+window.Liferay = {
+	...(window.Liferay || {}),
+	Language: {
+		...(window.Liferay.Language || {}),
+		available: {
+			ar_SA: 'Arabic (Saudi Arabia)',
+			ca_ES: 'Catalan (Spain)',
+			de_DE: 'German (Germany)',
+			en_US: 'English (United States)',
+			es_ES: 'Spanish (Spain)',
+			fi_FI: 'Finnish (Finland)',
+			fr_FR: 'French (France)',
+			hu_HU: 'Hungarian (Hungary)',
+			ja_JP: 'Japanese (Japan)',
+			nl_NL: 'Dutch (Netherlands)',
+			pt_BR: 'Portuguese (Brazil)',
+			sv_SE: 'Swedish (Sweden)',
+			zh_CN: 'Chinese (China)',
+		},
+	},
+	ThemeDisplay: {
+		...(window.Liferay.ThemeDisplay || {}),
+		getDefaultLanguageId: () => 'en_US',
+		getLanguageId: () => 'en_US',
+	},
+};
