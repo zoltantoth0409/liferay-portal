547c6674fcc1823c72f83052b616db6d73ed4dc0
Victor Galan <victor.galan@liferay.com>
2020-05-25 12:31:21 +0200

    LPS-113962 Add test for checking that all actions defined in the types.js file are taken into account for undo

---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/undo/useUndo.test.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {renderHook} from '@testing-library/react-hooks';
+
+import * as Actions from '../../../../../src/main/resources/META-INF/resources/page_editor/app/actions/types';
+import useUndo from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/undo/useUndo';
+import * as ExperienceActions from '../../../../../src/main/resources/META-INF/resources/page_editor/plugins/experience/actions';
+
+jest.mock(
+	'../../../../../src/main/resources/META-INF/resources/page_editor/app/config',
+	() => ({config: {undoEnabled: true}})
+);
+
+describe('useUndo', () => {
+	it('adds an undo action for all the registered actions', () => {
+
+		// List of the actions that do not need undo
+		// Before adding an action here blindly, check if
+		// something needs to be done to support undo for that action
+
+		const noUndoActions = [
+			Actions.ADD_FRAGMENT_COMPOSITION,
+			Actions.ADD_FRAGMENT_ENTRY_LINK_COMMENT,
+			Actions.ADD_MAPPED_INFO_ITEM,
+			Actions.ADD_UNDO_ACTION,
+			Actions.ADD_USED_WIDGET,
+			Actions.DELETE_FRAGMENT_ENTRY_LINK_COMMENT,
+			Actions.DELETE_WIDGETS,
+			Actions.EDIT_FRAGMENT_ENTRY_LINK_COMMENT,
+			Actions.LOAD_REDUCER,
+			Actions.SWITCH_SIDEBAR_PANEL,
+			Actions.SWITCH_VIEWPORT_SIZE,
+			Actions.TOGGLE_PERMISSION,
+			Actions.TOGGLE_SHOW_RESOLVED_COMMENTS,
+			Actions.UNLOAD_REDUCER,
+			Actions.UPDATE_COL_SIZE,
+			Actions.UPDATE_FRAGMENT_ENTRY_LINK_CONTENT,
+			Actions.UPDATE_LAYOUT_DATA,
+			Actions.UPDATE_MULTIPLE_UNDO_STATE,
+			Actions.UPDATE_NETWORK,
+			Actions.UPDATE_UNDO_ACTIONS,
+			Actions.UPDATE_PAGE_CONTENTS,
+			ExperienceActions.CREATE_SEGMENTS_EXPERIENCE,
+			ExperienceActions.DELETE_SEGMENTS_EXPERIENCE,
+			ExperienceActions.UPDATE_SEGMENTS_EXPERIENCE,
+			ExperienceActions.UPDATE_SEGMENTS_EXPERIENCE_PRIORITY,
+		];
+
+		const dispatch = jest.fn();
+
+		const {result} = renderHook(() => useUndo([{}, dispatch]));
+
+		const undoDispatch = result.current[1];
+
+		const allActions = {...Actions, ...ExperienceActions};
+
+		Object.keys(allActions)
+			.filter((action) => !noUndoActions.includes(action))
+			.forEach((action) => {
+				undoDispatch({type: action});
+
+				expect(dispatch).toBeCalledWith(
+					expect.objectContaining({type: Actions.ADD_UNDO_ACTION})
+				);
+
+				dispatch.mockClear();
+			});
+	});
+});
