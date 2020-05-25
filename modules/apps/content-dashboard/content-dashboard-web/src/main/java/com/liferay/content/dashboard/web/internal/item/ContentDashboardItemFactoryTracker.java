1537cf0edbb8082daae9036325453df277674677
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-05-25 18:13:20 +0200

    LPS-114383 New ContentDashboardItemFactoryTracker (tracker of ContentDashboardItemFactory)

---
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/ContentDashboardItemFactoryTracker.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.web.internal.item;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.petra.reflect.GenericUtil;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Cristina González
+ */
+@Component(service = ContentDashboardItemFactoryTracker.class)
+public class ContentDashboardItemFactoryTracker {
+
+	public Collection<String> getClassNames() {
+		return Collections.unmodifiableCollection(_serviceTrackerMap.keySet());
+	}
+
+	public <T> Optional<ContentDashboardItemFactory<T>>
+		getContentDashboardInfoItemFactoryOptional(String className) {
+
+		return Optional.ofNullable(
+			(ContentDashboardItemFactory<T>)_serviceTrackerMap.getService(
+				className));
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap =
+			(ServiceTrackerMap<String, ContentDashboardItemFactory<?>>)
+				(ServiceTrackerMap)ServiceTrackerMapFactory.openSingleValueMap(
+					bundleContext, ContentDashboardItemFactory.class, null,
+					ServiceReferenceMapperFactory.create(
+						bundleContext,
+						(contentDashboardInfoItem, emitter) -> emitter.emit(
+							GenericUtil.getGenericClass(
+								contentDashboardInfoItem))));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private volatile ServiceTrackerMap<String, ContentDashboardItemFactory<?>>
+		_serviceTrackerMap;
+
+}
