e56782dcf2f9669dc149454243b9a7d8a900853f
Matuzalém Teles <matuzalemteles@gmail.com>
2020-05-25 13:55:32 -0300

    LPS-112424 rewrites the PaginationControls container in React

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/components/PaginationControls.es.js
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+import React from 'react';
+
+import {PAGE_TYPES, usePage} from '../hooks/usePage.es';
+
+export const PaginationControls = ({
+	activePage,
+	readOnly,
+	showSubmitButton,
+	strings = null,
+	submitLabel,
+	total,
+}) => {
+	const {dispatch, store} = usePage();
+	const {cancelLabel, redirectURL, showCancelButton} = store;
+
+	return (
+		<div className="lfr-ddm-form-pagination-controls">
+			{activePage > 0 && (
+				<ClayButton
+					className="lfr-ddm-form-pagination-prev"
+					onClick={() =>
+						dispatch({type: PAGE_TYPES.PAGINATION_PREVIOUS})
+					}
+					type="button"
+				>
+					<ClayIcon symbol="angle-left" />
+					{strings !== null
+						? strings['previous']
+						: Liferay.Language.get('previous')}
+				</ClayButton>
+			)}
+
+			{activePage < total - 1 && (
+				<ClayButton
+					className="float-right lfr-ddm-form-pagination-next"
+					onClick={() => dispatch({type: PAGE_TYPES.PAGINATION_NEXT})}
+					type="button"
+				>
+					{strings !== null
+						? strings['next']
+						: Liferay.Language.get('next')}
+					<ClayIcon symbol="angle-right" />
+				</ClayButton>
+			)}
+
+			{activePage === total - 1 && !readOnly && showSubmitButton && (
+				<ClayButton
+					className="float-right lfr-ddm-form-submit"
+					type="submit"
+				>
+					{submitLabel}
+				</ClayButton>
+			)}
+
+			{showCancelButton && !readOnly && (
+				<div className="ddm-btn-cancel float-right">
+					<a
+						className="btn btn-cancel btn-secondary"
+						href={redirectURL}
+					>
+						{cancelLabel}
+					</a>
+				</div>
+			)}
+		</div>
+	);
+};
