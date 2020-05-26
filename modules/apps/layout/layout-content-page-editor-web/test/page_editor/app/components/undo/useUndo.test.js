7ec275198df3da818db6214c4fb4bffc6a040879
Pablo Molina <contact@pablomolina.me>
2020-05-26 12:40:00 +0200

    LPS-113962 Create a separated test for each action supporting undo

--- modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/undo/useUndo.test.js
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/undo/useUndo.test.js
@@ -24,51 +24,51 @@ jest.mock(
 );
 
 describe('useUndo', () => {
-	it('adds an undo action for all the registered actions', () => {
 
-		// List of the actions that do not need undo
-		// Before adding an action here blindly, check if
-		// something needs to be done to support undo for that action
+	// List of the actions that do not need undo
+	// Before adding an action here blindly, check if
+	// something needs to be done to support undo for that action
 
-		const noUndoActions = [
-			Actions.ADD_FRAGMENT_COMPOSITION,
-			Actions.ADD_FRAGMENT_ENTRY_LINK_COMMENT,
-			Actions.ADD_MAPPED_INFO_ITEM,
-			Actions.ADD_UNDO_ACTION,
-			Actions.ADD_USED_WIDGET,
-			Actions.DELETE_FRAGMENT_ENTRY_LINK_COMMENT,
-			Actions.DELETE_WIDGETS,
-			Actions.EDIT_FRAGMENT_ENTRY_LINK_COMMENT,
-			Actions.LOAD_REDUCER,
-			Actions.SWITCH_SIDEBAR_PANEL,
-			Actions.SWITCH_VIEWPORT_SIZE,
-			Actions.TOGGLE_PERMISSION,
-			Actions.TOGGLE_SHOW_RESOLVED_COMMENTS,
-			Actions.UNLOAD_REDUCER,
-			Actions.UPDATE_COL_SIZE,
-			Actions.UPDATE_FRAGMENT_ENTRY_LINK_CONTENT,
-			Actions.UPDATE_LAYOUT_DATA,
-			Actions.UPDATE_MULTIPLE_UNDO_STATE,
-			Actions.UPDATE_NETWORK,
-			Actions.UPDATE_UNDO_ACTIONS,
-			Actions.UPDATE_PAGE_CONTENTS,
-			ExperienceActions.CREATE_SEGMENTS_EXPERIENCE,
-			ExperienceActions.DELETE_SEGMENTS_EXPERIENCE,
-			ExperienceActions.UPDATE_SEGMENTS_EXPERIENCE,
-			ExperienceActions.UPDATE_SEGMENTS_EXPERIENCE_PRIORITY,
-		];
+	const noUndoActions = [
+		Actions.ADD_FRAGMENT_COMPOSITION,
+		Actions.ADD_FRAGMENT_ENTRY_LINK_COMMENT,
+		Actions.ADD_MAPPED_INFO_ITEM,
+		Actions.ADD_UNDO_ACTION,
+		Actions.ADD_USED_WIDGET,
+		Actions.DELETE_FRAGMENT_ENTRY_LINK_COMMENT,
+		Actions.DELETE_WIDGETS,
+		Actions.EDIT_FRAGMENT_ENTRY_LINK_COMMENT,
+		Actions.LOAD_REDUCER,
+		Actions.SWITCH_SIDEBAR_PANEL,
+		Actions.SWITCH_VIEWPORT_SIZE,
+		Actions.TOGGLE_PERMISSION,
+		Actions.TOGGLE_SHOW_RESOLVED_COMMENTS,
+		Actions.UNLOAD_REDUCER,
+		Actions.UPDATE_COL_SIZE,
+		Actions.UPDATE_FRAGMENT_ENTRY_LINK_CONTENT,
+		Actions.UPDATE_LAYOUT_DATA,
+		Actions.UPDATE_MULTIPLE_UNDO_STATE,
+		Actions.UPDATE_NETWORK,
+		Actions.UPDATE_UNDO_ACTIONS,
+		Actions.UPDATE_PAGE_CONTENTS,
+		ExperienceActions.CREATE_SEGMENTS_EXPERIENCE,
+		ExperienceActions.DELETE_SEGMENTS_EXPERIENCE,
+		ExperienceActions.UPDATE_SEGMENTS_EXPERIENCE,
+		ExperienceActions.UPDATE_SEGMENTS_EXPERIENCE_PRIORITY,
+	];
 
-		const dispatch = jest.fn();
+	const dispatch = jest.fn();
 
-		const {result} = renderHook(() => useUndo([{}, dispatch]));
+	const {result} = renderHook(() => useUndo([{}, dispatch]));
 
-		const undoDispatch = result.current[1];
+	const undoDispatch = result.current[1];
 
-		const allActions = {...Actions, ...ExperienceActions};
+	const allActions = {...Actions, ...ExperienceActions};
 
-		Object.keys(allActions)
-			.filter((action) => !noUndoActions.includes(action))
-			.forEach((action) => {
+	Object.keys(allActions)
+		.filter((action) => !noUndoActions.includes(action))
+		.forEach((action) => {
+			it(`is supported by "${action}" action`, () => {
 				undoDispatch({type: action});
 
 				expect(dispatch).toBeCalledWith(
@@ -77,5 +77,5 @@ describe('useUndo', () => {
 
 				dispatch.mockClear();
 			});
-	});
+		});
 });
