c3a4a23a4145e5dccedbfc24d8e5cd52152582a4
pei-jung <pei-jung.lan@liferay.com>
2020-05-26 15:56:46 -0700

    LPS-113856 Add Manage Suborganizations Accounts permission

--- modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountActionKeys.java
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/constants/AccountActionKeys.java
@@ -27,6 +27,9 @@ public class AccountActionKeys {
 
 	public static final String MANAGE_ORGANIZATIONS = "MANAGE_ORGANIZATIONS";
 
+	public static final String MANAGE_SUBORGANIZATIONS_ACCOUNTS =
+		"MANAGE_SUBORGANIZATIONS_ACCOUNTS";
+
 	public static final String VIEW_ORGANIZATIONS = "VIEW_ORGANIZATIONS";
 
 	public static final String VIEW_USERS = "VIEW_USERS";
