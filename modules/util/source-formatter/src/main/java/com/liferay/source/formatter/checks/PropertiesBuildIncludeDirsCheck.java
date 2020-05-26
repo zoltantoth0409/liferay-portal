94b98acd39dd9b4fce7adbfa8a1c84a810a31998
Hugo Huijser <hugo.huijser@liferay.com>
2020-05-26 11:56:18 +0800

    LPS-113715 Skip '.releng' directory

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesBuildIncludeDirsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesBuildIncludeDirsCheck.java
@@ -170,10 +170,10 @@ public class PropertiesBuildIncludeDirsCheck extends BaseFileCheck {
 		"buildIncludeCategoryNames";
 
 	private static final String[] _SKIP_DIR_NAMES = {
-		".git", ".gradle", ".idea", ".m2", ".settings", "bin", "build",
-		"classes", "dependencies", "node_modules", "node_modules_cache",
-		"private", "sql", "src", "test", "test-classes", "test-coverage",
-		"test-results", "tmp"
+		".git", ".gradle", ".idea", ".m2", ".releng", ".settings", "bin",
+		"build", "classes", "dependencies", "node_modules",
+		"node_modules_cache", "private", "sql", "src", "test", "test-classes",
+		"test-coverage", "test-results", "tmp"
 	};
 
 	private static final Pattern _pattern = Pattern.compile(
