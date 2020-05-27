bc5a1a61eb25d66b50e453b527b635d056e935b0
Michael Hashimoto <michael.hashimoto@liferay.com>
2020-05-26 17:18:36 -0700

    LRCI-1352 Add cucumber class to find feature files

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CucumberFeatureFile.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeoutException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class CucumberFeatureFile {
+
+	public CucumberFeatureFile(
+		File faroDir, CucumberFeatureResult cucumberFeatureResult,
+		CucumberScenarioResult cucumberScenarioResult) {
+
+		_faroDir = faroDir;
+		_cucumberFeatureResult = cucumberFeatureResult;
+		_cucumberScenarioResult = cucumberScenarioResult;
+	}
+
+	public File getFile() {
+		String targetFeaturePath = null;
+
+		for (String featurePath : _getFeaturePathsFromFeatureName()) {
+			Set<String> featurePaths = _getFeaturePathsFromScenarioName();
+
+			if (featurePaths.contains(featurePath)) {
+				targetFeaturePath = featurePath;
+
+				break;
+			}
+		}
+
+		if (targetFeaturePath == null) {
+			throw new RuntimeException("Could not find a matching file");
+		}
+
+		return new File(_faroDir, targetFeaturePath);
+	}
+
+	private Set<String> _getFeaturePaths(String name) {
+		Set<String> featurePaths = new HashSet<>();
+
+		try {
+			Process process = JenkinsResultsParserUtil.executeBashCommands(
+				false, _faroDir, 10000,
+				JenkinsResultsParserUtil.combine(
+					"git grep \"", name.replaceAll("\"", "\\\\\""), "\""));
+
+			try {
+				String gitGrepResults =
+					JenkinsResultsParserUtil.readInputStream(
+						process.getInputStream());
+
+				for (String gitGrepResult : gitGrepResults.split("\n")) {
+					Matcher matcher = _pattern.matcher(gitGrepResult);
+
+					if (!matcher.find()) {
+						continue;
+					}
+
+					featurePaths.add(matcher.group(1));
+				}
+			}
+			catch (IOException ioException) {
+				throw new RuntimeException(
+					"Unable to read process input stream", ioException);
+			}
+		}
+		catch (IOException | TimeoutException exception) {
+			throw new RuntimeException(exception);
+		}
+
+		return featurePaths;
+	}
+
+	private Set<String> _getFeaturePathsFromFeatureName() {
+		return _getFeaturePaths(_cucumberFeatureResult.getName());
+	}
+
+	private Set<String> _getFeaturePathsFromScenarioName() {
+		return _getFeaturePaths(_cucumberScenarioResult.getScenarioName());
+	}
+
+	private static final Pattern _pattern = Pattern.compile(
+		"([^\\.]+\\.feature)\\:.*");
+
+	private final CucumberFeatureResult _cucumberFeatureResult;
+	private final CucumberScenarioResult _cucumberScenarioResult;
+	private final File _faroDir;
+
+}
