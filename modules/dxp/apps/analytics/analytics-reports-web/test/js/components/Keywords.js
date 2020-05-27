9b27aba744eac1b586c57512e9f40b6485c79dc5
Sarai Díaz García <dgarciasarai@gmail.com>
2020-05-27 18:05:06 +0200

    LPS-113549 Add test

---
+++ modules/dxp/apps/analytics/analytics-reports-web/test/js/components/Keywords.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+
+import Keywords from '../../../src/main/resources/META-INF/resources/js/components/Keywords';
+
+import '@testing-library/jest-dom/extend-expect';
+
+describe('Keywords', () => {
+	afterEach(cleanup);
+
+	it('renders message no best keywords when content was published today', () => {
+		const mockCurrentPage = {
+			data: {
+				helpMessage:
+					'This number refers to the volume of people that find your page through a search engine.',
+				keywords: [],
+				name: 'organic',
+				share: 0,
+				title: 'Organic',
+				value: 0,
+			},
+			view: 'traffic-source-detail',
+		};
+
+		const {getByText} = render(
+			<Keywords currentPage={mockCurrentPage} languageTag={'en-US'} />
+		);
+
+		expect(getByText('there-are-no-best-keywords-yet')).toBeInTheDocument();
+	});
+});
