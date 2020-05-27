8c3e6b0ecce0dd39f9fc93bd957135e0db3e0ef2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-05-27 10:24:08 +0200

    LPS-114304 Check if we can register an application on GLOBAL_MENU_APPLICATIONS

---
+++ modules/apps/application-list/application-list-test/src/testIntegration/java/com/liferay/application/list/display/context/logic/test/PanelCategoryHelperTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.application.list.display.context.logic.test;
+
+import com.liferay.application.list.PanelAppRegistry;
+import com.liferay.application.list.PanelCategoryRegistry;
+import com.liferay.application.list.display.context.logic.PanelCategoryHelper;
+import com.liferay.application.list.display.context.logic.test.constants.GlobalMenuTestPortletKeys;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+public class PanelCategoryHelperTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testIsGlobalMenuApp() {
+		PanelCategoryHelper panelCategoryHelper = new PanelCategoryHelper(
+			_panelAppRegistry, _panelCategoryRegistry);
+
+		Assert.assertTrue(
+			panelCategoryHelper.isGlobalMenuApp(
+				GlobalMenuTestPortletKeys.GLOBAL_MENU_TEST_PORTLET));
+	}
+
+	@Inject
+	private PanelAppRegistry _panelAppRegistry;
+
+	@Inject
+	private PanelCategoryRegistry _panelCategoryRegistry;
+
+}
