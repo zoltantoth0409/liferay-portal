e64650c33480adbefe672c29a831ab7975ebaf67
Peter Shin <peter.shin@liferay.com>
2020-05-28 09:59:09 -0500

    LPS-88645 Use task provider

--- modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/JSModuleConfigGeneratorDefaultsPlugin.java
+++ modules/sdk/gradle-plugins/src/main/java/com/liferay/gradle/plugins/internal/JSModuleConfigGeneratorDefaultsPlugin.java
@@ -20,8 +20,10 @@ import com.liferay.gradle.plugins.js.module.config.generator.ConfigJSModulesTask
 import com.liferay.gradle.plugins.js.module.config.generator.JSModuleConfigGeneratorExtension;
 import com.liferay.gradle.plugins.js.module.config.generator.JSModuleConfigGeneratorPlugin;
 
+import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.tasks.TaskProvider;
 
 /**
  * @author Andrea Di Giorgi
@@ -38,7 +40,14 @@ public class JSModuleConfigGeneratorDefaultsPlugin
 		JSModuleConfigGeneratorPlugin jsModuleConfigGeneratorPlugin) {
 
 		_configureJSModuleConfigGenerator(project);
-		_configureTaskConfigJSModules(project);
+
+		TaskProvider<ConfigJSModulesTask> configJSModulesTaskProvider =
+			GradleUtil.getTaskProvider(
+				project,
+				JSModuleConfigGeneratorPlugin.CONFIG_JS_MODULES_TASK_NAME,
+				ConfigJSModulesTask.class);
+
+		_configureTaskConfigJSModulesProvider(configJSModulesTaskProvider);
 	}
 
 	@Override
@@ -61,16 +70,21 @@ public class JSModuleConfigGeneratorDefaultsPlugin
 		jsModuleConfigGeneratorExtension.setVersion(version);
 	}
 
-	private void _configureTaskConfigJSModules(Project project) {
-		ConfigJSModulesTask configJSModulesTask =
-			(ConfigJSModulesTask)GradleUtil.getTask(
-				project,
-				JSModuleConfigGeneratorPlugin.CONFIG_JS_MODULES_TASK_NAME);
+	private void _configureTaskConfigJSModulesProvider(
+		TaskProvider<ConfigJSModulesTask> configJSModulesTaskProvider) {
 
-		configJSModulesTask.setConfigVariable("");
-		configJSModulesTask.setIgnorePath(true);
-		configJSModulesTask.setModuleExtension("");
-		configJSModulesTask.setModuleFormat("/_/g,-");
+		configJSModulesTaskProvider.configure(
+			new Action<ConfigJSModulesTask>() {
+
+				@Override
+				public void execute(ConfigJSModulesTask configJSModulesTask) {
+					configJSModulesTask.setConfigVariable("");
+					configJSModulesTask.setIgnorePath(true);
+					configJSModulesTask.setModuleExtension("");
+					configJSModulesTask.setModuleFormat("/_/g,-");
+				}
+
+			});
 	}
 
 	private static final String _VERSION = "1.3.3";
