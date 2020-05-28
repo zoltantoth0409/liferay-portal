3e3d8afed6916c1546c3f6aaa85a1c4141468a86
Pavel Savinov <pavel.savinov@liferay.com>
2020-05-28 15:50:28 +0200

    LPS-114500 Adjust tests

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/GetCollectionFieldMVCResourceCommandTest.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/GetCollectionFieldMVCResourceCommandTest.java
@@ -138,12 +138,12 @@ public class GetCollectionFieldMVCResourceCommandTest {
 			_mvcResourceCommand, "_getCollectionFieldsJSONObject",
 			new Class<?>[] {
 				HttpServletRequest.class, HttpServletResponse.class,
-				String.class, String.class, String.class, Locale.class,
-				long.class, int.class
+				String.class, String.class, String.class, String.class,
+				Locale.class, long.class, int.class
 			},
 			new MockHttpServletRequest(), new MockHttpServletResponse(),
 			layoutObjectReferenceJSONObject.toString(), StringPool.BLANK,
-			StringPool.BLANK, LocaleUtil.US, 0, 1);
+			StringPool.BLANK, StringPool.BLANK, LocaleUtil.US, 0, 1);
 
 		Assert.assertEquals(1, jsonObject.getInt("length"));
 
@@ -183,12 +183,12 @@ public class GetCollectionFieldMVCResourceCommandTest {
 			_mvcResourceCommand, "_getCollectionFieldsJSONObject",
 			new Class<?>[] {
 				HttpServletRequest.class, HttpServletResponse.class,
-				String.class, String.class, String.class, Locale.class,
-				long.class, int.class
+				String.class, String.class, String.class, String.class,
+				Locale.class, long.class, int.class
 			},
 			new MockHttpServletRequest(), new MockHttpServletResponse(),
 			layoutObjectReferenceJSONObject.toString(), StringPool.BLANK,
-			StringPool.BLANK, LocaleUtil.US, 0, 2);
+			StringPool.BLANK, StringPool.BLANK, LocaleUtil.US, 0, 2);
 
 		Assert.assertEquals(2, jsonObject.getInt("length"));
 
@@ -234,12 +234,12 @@ public class GetCollectionFieldMVCResourceCommandTest {
 			_mvcResourceCommand, "_getCollectionFieldsJSONObject",
 			new Class<?>[] {
 				HttpServletRequest.class, HttpServletResponse.class,
-				String.class, String.class, String.class, Locale.class,
-				long.class, int.class
+				String.class, String.class, String.class, String.class,
+				Locale.class, long.class, int.class
 			},
 			new MockHttpServletRequest(), new MockHttpServletResponse(),
 			layoutObjectReferenceJSONObject.toString(), StringPool.BLANK,
-			StringPool.BLANK, LocaleUtil.US, 0, 1);
+			StringPool.BLANK, StringPool.BLANK, LocaleUtil.US, 0, 1);
 
 		Assert.assertEquals(2, jsonObject.getInt("length"));
 
