8dfb6593c309339bfd74a5df654219d2eed8555e
Marta Medio <marta.medio@liferay.com>
2020-05-28 10:51:01 +0200

    LPS-101717 Create new MFA headless checker

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-spi/src/main/java/com/liferay/multi/factor/authentication/spi/checker/headless/MFAHeadlessChecker.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.spi.checker.headless;
+
+import com.liferay.multi.factor.authentication.spi.checker.MFAChecker;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Tomas Polesovsky
+ * @author Marta Medio
+ */
+public interface MFAHeadlessChecker extends MFAChecker {
+
+	public boolean verifyHeadlessRequest(
+		HttpServletRequest httpServletRequest, long userId);
+
+}
