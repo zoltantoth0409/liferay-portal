045793443f58c12dea55291f2342cf2dac8b3932
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-05-28 15:17:13 +0200

    LPS-114521 Adding initial test to drive implementation

---
+++ modules/apps/info/info-test/src/testIntegration/java/com/liferay/info/field/exporter/test/XLIFFInfoFormTranslationExporterTest.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.field.exporter.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeResponse;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
+import com.liferay.info.field.exporter.InfoFormTranslationExporter;
+import com.liferay.info.item.provider.InfoItemFormProviderTracker;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.petra.io.StreamUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alejandro Tardín
+ */
+@RunWith(Arquillian.class)
+public class XLIFFInfoFormTranslationExporterTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule testRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testConvertReturnsTheXLIFFRepresentationOfAJournalArticle()
+		throws Exception {
+
+		Assert.assertEquals(
+			_readFileToString("dependencies/test-journal-article.xliff"),
+			StreamUtil.toString(
+				_xliffInfoFormTranslationExporter.export(
+					_infoItemFormProviderTracker.getInfoItemFormProvider(
+						JournalArticle.class.getName()),
+					_getJournalArticle(), LocaleUtil.getDefault(),
+					LocaleUtil.fromLanguageId("es_ES"))));
+	}
+
+	private JournalArticle _getJournalArticle() throws Exception {
+		DDMFormDeserializerDeserializeRequest.Builder builder =
+			DDMFormDeserializerDeserializeRequest.Builder.newBuilder(
+				_readFileToString("dependencies/test-ddm-form.json"));
+
+		DDMFormDeserializerDeserializeResponse
+			ddmFormDeserializerDeserializeResponse =
+				_ddmFormDeserializer.deserialize(builder.build());
+
+		DDMForm ddmForm = ddmFormDeserializerDeserializeResponse.getDDMForm();
+
+		DDMStructure ddmStructure = DDMStructureTestUtil.addStructure(
+			_group.getGroupId(), JournalArticle.class.getName(), ddmForm);
+
+		return JournalTestUtil.addArticleWithXMLContent(
+			_group.getGroupId(),
+			_readFileToString("dependencies/test-journal-content.xml"),
+			ddmStructure.getStructureKey(), null);
+	}
+
+	private String _readFileToString(String s) throws Exception {
+		return new String(FileUtil.getBytes(getClass(), s));
+	}
+
+	@Inject(filter = "ddm.form.deserializer.type=json")
+	private DDMFormDeserializer _ddmFormDeserializer;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private InfoItemFormProviderTracker _infoItemFormProviderTracker;
+
+	@Inject(filter = "content.type=application/xliff+xml")
+	private InfoFormTranslationExporter _xliffInfoFormTranslationExporter;
+
+}
