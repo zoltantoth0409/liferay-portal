a87b93739f47ad03ed5c728263ea59c6fe10e8bc
Albino Junior <albino.junior@liferay.com>
2020-05-29 16:11:18 -0300

    LPS-111728 | LPS-114190 Create ListApps page component and tests

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/apps/ListApps.es.js
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/apps/ListApps.es.js
@@ -9,8 +9,87 @@
  * distribution rights of the Software.
  */
 
-import React from 'react';
+import ClayPopover from '@clayui/popover';
+import {AppContext} from 'app-builder-web/js/AppContext.es';
+import Button from 'app-builder-web/js/components/button/Button.es';
+import ListApps from 'app-builder-web/js/pages/apps/ListApps.es';
+import {COLUMNS, FILTERS} from 'app-builder-web/js/pages/apps/constants.es';
+import {compile} from 'path-to-regexp';
+import React, {useContext, useState} from 'react';
 
-export default () => {
-	return <span>Workflow Powered Apps</span>;
+export default ({scope, ...props}) => {
+	const {userId} = useContext(AppContext);
+	const [showTooltip, setShowTooltip] = useState(false);
+
+	const [firstColumn, ...otherColumns] = COLUMNS;
+
+	const columns = [
+		firstColumn,
+		{
+			key: 'dataDefinitionName',
+			value: Liferay.Language.get('object'),
+		},
+		...otherColumns,
+	];
+
+	const newAppLink = compile(props.editPath[0])();
+
+	const emptyState = {
+		button: () => (
+			<Button displayType="secondary" href={newAppLink}>
+				{Liferay.Language.get('create-new-app')}
+			</Button>
+		),
+		description: Liferay.Language.get(
+			'integrate-the-data-collection-and-management-of-an-object-with-a-step-driven-workflow-process'
+		),
+		title: Liferay.Language.get('there-are-no-apps-yet'),
+	};
+
+	const filters = [
+		...FILTERS,
+		{
+			items: [
+				{
+					label: Liferay.Language.get('me'),
+					value: userId,
+				},
+			],
+			key: 'userIds',
+			multiple: true,
+			name: 'author',
+		},
+	];
+
+	return (
+		<ListApps
+			listViewProps={{
+				addButton: () => (
+					<ClayPopover
+						alignPosition="bottom-right"
+						header={Liferay.Language.get('workflow-powered-app')}
+						show={showTooltip}
+						trigger={
+							<Button
+								className="nav-btn nav-btn-monospaced"
+								href={newAppLink}
+								onMouseOut={() => setShowTooltip(false)}
+								onMouseOver={() => setShowTooltip(true)}
+								symbol="plus"
+							/>
+						}
+					>
+						{Liferay.Language.get(
+							'create-an-app-that-integrates-a-step-driven-workflow-process'
+						)}
+					</ClayPopover>
+				),
+				columns,
+				emptyState,
+				endpoint: `/o/app-builder/v1.0/apps?scope=${scope}`,
+				filters,
+			}}
+			{...props}
+		/>
+	);
 };
---
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/test/pages/apps/ListApps.es.js
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {waitForElementToBeRemoved} from '@testing-library/dom';
+import {fireEvent, render} from '@testing-library/react';
+import * as time from 'app-builder-web/js/utils/time.es';
+import React from 'react';
+
+import ListApps from '../../../src/main/resources/META-INF/resources/js/pages/apps/ListApps.es';
+import AppContextProviderWrapper from '../../AppContextProviderWrapper.es';
+import {RESPONSES} from '../../constants.es';
+
+const DROPDOWN_VALUES = {
+	items: [
+		{
+			id: 1,
+			name: {
+				'en-US': 'object',
+			},
+		},
+	],
+};
+
+const EDIT_PATH = ['/workflow/deploy', '/workflow/:appId(\\d+)'];
+
+const routeProps = {editPath: EDIT_PATH, match: {params: {}}};
+
+describe('ListApps', () => {
+	beforeEach(() => {
+		jest.spyOn(time, 'fromNow').mockImplementation(() => 'months ago');
+	});
+
+	afterEach(jest.restoreAllMocks);
+
+	it('renders with 5 apps on list and opens a new app tooltip', async () => {
+		fetch.mockResponseOnce(JSON.stringify(RESPONSES.MANY_ITEMS(5)));
+		fetch.mockResponse(JSON.stringify(DROPDOWN_VALUES));
+
+		const {container} = render(<ListApps {...routeProps} />, {
+			wrapper: AppContextProviderWrapper,
+		});
+
+		await waitForElementToBeRemoved(() =>
+			document.querySelector('span.loading-animation')
+		);
+
+		expect(container.querySelector('tbody').children.length).toEqual(5);
+
+		const newAppButton = document.querySelector(
+			'.nav-btn.nav-btn-monospaced.btn.btn-monospaced.btn-primary'
+		);
+
+		await fireEvent.mouseOver(newAppButton);
+
+		expect(
+			document.querySelector('.popover.clay-popover-bottom-right')
+				.textContent
+		).toContain(
+			'workflow-powered-appcreate-an-app-that-integrates-a-step-driven-workflow-process'
+		);
+
+		await fireEvent.mouseOut(newAppButton);
+
+		expect(
+			document.querySelector('.popover.clay-popover-bottom-right')
+		).not.toBeInTheDocument();
+	});
+
+	it('renders with empty state', async () => {
+		fetch.mockResponseOnce(JSON.stringify(RESPONSES.NO_ITEMS));
+		fetch.mockResponse(JSON.stringify(DROPDOWN_VALUES));
+
+		const {container, queryByText} = render(<ListApps {...routeProps} />, {
+			wrapper: AppContextProviderWrapper,
+		});
+
+		await waitForElementToBeRemoved(() =>
+			document.querySelector('span.loading-animation')
+		);
+
+		expect(
+			container.querySelector('.taglib-empty-result-message').textContent
+		).toContain(
+			'there-are-no-apps-yetintegrate-the-data-collection-and-management-of-an-object-with-a-step-driven-workflow-process'
+		);
+
+		const {parentElement} = queryByText('create-new-app');
+
+		expect(parentElement.getAttribute('href')).toBe('#/workflow/deploy');
+	});
+});
