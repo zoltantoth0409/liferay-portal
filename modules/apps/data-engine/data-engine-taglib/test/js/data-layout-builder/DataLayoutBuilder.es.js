b72bdc8c647116bf8dfb4788351b1bcd0d0f6c06
oliveiraaraujo <alexandre.araujo@liferay.com>
2020-05-29 16:11:36 -0300

    LPS-114196 Increase component frontend test.

--- modules/apps/data-engine/data-engine-taglib/test/js/data-layout-builder/DataLayoutBuilder.es.js
+++ modules/apps/data-engine/data-engine-taglib/test/js/data-layout-builder/DataLayoutBuilder.es.js
@@ -15,25 +15,137 @@
 import DataLayoutBuilder from '../../../src/main/resources/META-INF/resources/data_layout_builder/js/data-layout-builder/DataLayoutBuilder.es';
 
 let component;
-const spritemap = 'icons.svg';
+const props = {
+	appContext: [
+		{},
+		(action) => {
+			props.appContext[0].action = action;
+		},
+	],
+	config: {
+		allowFieldSets: true,
+		allowMultiplePages: false,
+		allowRules: false,
+		allowSuccessPage: false,
+		disabledProperties: ['predefinedValue'],
+		disabledTabs: ['Autocomplete'],
+		unimplementedProperties: [
+			'fieldNamespace',
+			'indexType',
+			'readOnly',
+			'validation',
+			'visibilityExpression',
+		],
+	},
+	context: {},
+	dataLayoutBuilderId:
+		'_com_liferay_journal_web_portlet_JournalPortlet_dataLayoutBuilder',
+	fieldTypes: [],
+	localizable: true,
+	portletNamespace: 'com_liferay_journal_web_portlet_JournalPortlet',
+};
 
 describe('DataLayoutBuilder', () => {
 	beforeEach(() => {
 		jest.useFakeTimers();
+		if (component && component.componentDidMount) {
+			component.componentDidMount();
+		}
 	});
 
 	afterEach(() => {
 		if (component && component.dispose) {
 			component.dispose();
 		}
+		else if (component && component.componentWillUnmount) {
+			component.componentWillUnmount();
+		}
 	});
 
 	it('is rendering', () => {
-		component = new DataLayoutBuilder({
-			readOnly: false,
-			spritemap,
-		});
+		component = new DataLayoutBuilder(props);
 
 		expect(component).toMatchSnapshot();
+	});
+
+	it('is emetting message', () => {
+		component = new DataLayoutBuilder(props);
+		const event = 'event:test';
+		const payload = {test: 'emeting'};
+
+		component.eventEmitter.once(event, expect(payload).toBe);
+
+		component.emit(event, payload);
+	});
+
+	it('is dispatching message', () => {
+		component = new DataLayoutBuilder(props);
+		component.componentDidMount();
+		const event = 'event:test';
+		const payload = {test: 'dispatching'};
+
+		component.eventEmitter.once(event, expect(payload).toBe);
+
+		component.dispatch(event, payload);
+	});
+
+	it('is removing listener after dispatching event', () => {
+		component = new DataLayoutBuilder(props);
+		component.componentDidMount();
+		const event = 'event:test';
+		const payload = {test: 'dispatching'};
+		component.on(event, (eventPayload) => {
+			component.removeEventListener(event);
+
+			expect(payload).toBe(eventPayload);
+		});
+		component.dispatch(event, payload);
+	});
+
+	it('is dispatching action', () => {
+		component = new DataLayoutBuilder(props);
+		const action = 'action:test';
+		component.dispatchAction(action);
+
+		expect(props.appContext[0].action).toBe(action);
+	});
+
+	it('is serializing pages', () => {
+		component = new DataLayoutBuilder(props);
+
+		expect(component.serialize([])).toMatchObject({
+			definition:
+				'{"availableLanguageIds":["en_US"],"dataDefinitionFields":[],"defaultLanguageId":"en_US"}',
+			layout: '{"dataLayoutPages":[],"paginationMode":"wizard"}',
+		});
+	});
+
+	it('is getting fieldTypes', () => {
+		component = new DataLayoutBuilder(props);
+
+		expect(props.fieldTypes).toBe(component.getFieldTypes());
+	});
+
+	it('is getting layoutProvider', () => {
+		component = new DataLayoutBuilder(props);
+		component.componentDidMount();
+
+		expect(component.getLayoutProvider()).toMatchObject({
+			...{ref: 'layoutProvider'},
+		});
+	});
+
+	it('is getting component state', () => {
+		component = new DataLayoutBuilder(props);
+		component.componentDidMount();
+
+		expect(component.getState()).toMatchObject(props.appContext[0]);
+	});
+
+	it('is getting component store', () => {
+		component = new DataLayoutBuilder(props);
+		component.componentDidMount();
+
+		expect(component.getStore()).toMatchObject({});
 	});
 });
