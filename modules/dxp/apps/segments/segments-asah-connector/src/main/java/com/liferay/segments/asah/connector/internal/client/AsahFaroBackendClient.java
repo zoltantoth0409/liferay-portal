ea92e0cd6d1282df51b8d7fb9e981ffb891aaa2e
David Arques <davidarchena@gmail.com>
2020-05-29 14:13:42 +0200

    LPS-114696 Expose companyId in AsahFaroBackendClient methods since we need to use different request parameters for each one depending on it

--- modules/dxp/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/client/AsahFaroBackendClient.java
+++ modules/dxp/apps/segments/segments-asah-connector/src/main/java/com/liferay/segments/asah/connector/internal/client/AsahFaroBackendClient.java
@@ -35,53 +35,60 @@ public interface AsahFaroBackendClient {
 	/**
 	 * Adds an {@link Experiment}.
 	 *
+	 * @param  companyId the company ID
 	 * @param  experiment experiment to be created
 	 * @return the created experiment
 	 * @review
 	 */
-	public Experiment addExperiment(Experiment experiment);
+	public Experiment addExperiment(long companyId, Experiment experiment);
 
 	/**
 	 * Calculates the estimated duration in days for the experiment
 	 * <code>experimentId</code> given a {@link ExperimentSettings}
 	 *
+	 * @param  companyId the company ID
 	 * @param  experimentId the ID of the experiment
 	 * @param  experimentSettings settings for the calculation
 	 * @return estimated duration for the experiment in days
 	 * @review
 	 */
 	public Long calculateExperimentEstimatedDaysDuration(
-		String experimentId, ExperimentSettings experimentSettings);
+		long companyId, String experimentId,
+		ExperimentSettings experimentSettings);
 
 	/**
 	 * Deletes an {@link Experiment}.
 	 *
+	 * @param  companyId the company ID
 	 * @param  experimentId the ID of the experiment to be removed
 	 * @review
 	 */
-	public void deleteExperiment(String experimentId);
+	public void deleteExperiment(long companyId, String experimentId);
 
 	/**
 	 * Returns the ID of the data source connected to the client.
 	 *
+	 * @param  companyId the company ID
 	 * @return the ID of the data source connected to the client
 	 */
-	public String getDataSourceId();
+	public String getDataSourceId(long companyId);
 
 	/**
 	 * Returns the individual matching the primary key for the data source
-	 * {@link #getDataSourceId()}.
+	 * {@link #getDataSourceId(long)}.
 	 *
+	 * @param  companyId the company ID
 	 * @param  individualPK the primary key of the individual
 	 * @return the individual matching the primary key, or {@code null} if no
 	 *         matches were found
 	 */
-	public Individual getIndividual(String individualPK);
+	public Individual getIndividual(long companyId, String individualPK);
 
 	/**
 	 * Returns the results of an individual that is a member of an individual
 	 * segment.
 	 *
+	 * @param  companyId the company ID
 	 * @param  individualSegmentId the individual segment's ID
 	 * @param  cur the current page (one-based numbering)
 	 * @param  delta the page size
@@ -90,44 +97,49 @@ public interface AsahFaroBackendClient {
 	 *         segment
 	 */
 	public Results<Individual> getIndividualResults(
-		String individualSegmentId, int cur, int delta,
+		long companyId, String individualSegmentId, int cur, int delta,
 		List<OrderByField> orderByFields);
 
 	/**
 	 * Returns the results of an active individual segment with members.
 	 *
+	 * @param  companyId the company ID
 	 * @param  cur the current page (one-based numbering)
 	 * @param  delta the page size
 	 * @param  orderByFields the sort fields
 	 * @return the results of an active individual segment with members
 	 */
 	public Results<IndividualSegment> getIndividualSegmentResults(
-		int cur, int delta, List<OrderByField> orderByFields);
+		long companyId, int cur, int delta, List<OrderByField> orderByFields);
 
 	/**
 	 * Returns the results for interest terms for the user.
 	 *
+	 * @param  companyId the company ID
 	 * @param  userId the user's ID
 	 * @return the results for interest terms for the user
 	 */
-	public Results<Topic> getInterestTermsResults(String userId);
+	public Results<Topic> getInterestTermsResults(
+		long companyId, String userId);
 
 	/**
 	 * Updates an {@link Experiment}.
 	 *
+	 * @param  companyId the company ID
 	 * @param  experiment experiment to be updated
 	 * @review
 	 */
-	public void updateExperiment(Experiment experiment);
+	public void updateExperiment(long companyId, Experiment experiment);
 
 	/**
 	 * Updates a set of {@link DXPVariants}.
 	 *
+	 * @param  companyId the company ID
 	 * @param  experimentId the experiment ID
 	 * @param  dxpVariants list of experiment variants
 	 * @review
 	 */
 	public void updateExperimentDXPVariants(
-		String experimentId, DXPVariants dxpVariants);
+		long companyId, String experimentId, DXPVariants dxpVariants);
 
 }
