d378bfe94f1d35497762a1431c17b1bffe65060e
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-05-29 11:01:53 -0300

    LPS-111266 | LPS-114453 Create an entity to represent a link between an App's step and a workflow task

---
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-service/src/main/java/com/liferay/app/builder/workflow/service/impl/AppBuilderWorkflowTaskLinkLocalServiceImpl.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.app.builder.workflow.service.impl;
+
+import com.liferay.app.builder.service.AppBuilderAppLocalService;
+import com.liferay.app.builder.workflow.exception.DuplicateAppBuilderWorkflowTaskLinkException;
+import com.liferay.app.builder.workflow.model.AppBuilderWorkflowTaskLink;
+import com.liferay.app.builder.workflow.service.base.AppBuilderWorkflowTaskLinkLocalServiceBaseImpl;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(
+	property = "model.class.name=com.liferay.app.builder.workflow.model.AppBuilderWorkflowTaskLink",
+	service = AopService.class
+)
+public class AppBuilderWorkflowTaskLinkLocalServiceImpl
+	extends AppBuilderWorkflowTaskLinkLocalServiceBaseImpl {
+
+	public AppBuilderWorkflowTaskLink addAppBuilderWorkflowTaskLink(
+			long appBuilderAppId, long companyId, long ddmStructureLayoutId,
+			String workflowTaskName)
+		throws PortalException {
+
+		AppBuilderWorkflowTaskLink appBuilderWorkflowTaskLink =
+			appBuilderWorkflowTaskLinkPersistence.fetchByA_D_W(
+				appBuilderAppId, ddmStructureLayoutId, workflowTaskName);
+
+		if (Objects.nonNull(appBuilderWorkflowTaskLink)) {
+			throw new DuplicateAppBuilderWorkflowTaskLinkException();
+		}
+
+		_appBuilderAppLocalService.getAppBuilderApp(appBuilderAppId);
+
+		appBuilderWorkflowTaskLink =
+			appBuilderWorkflowTaskLinkPersistence.create(
+				counterLocalService.increment());
+
+		appBuilderWorkflowTaskLink.setCompanyId(companyId);
+		appBuilderWorkflowTaskLink.setAppBuilderAppId(appBuilderAppId);
+		appBuilderWorkflowTaskLink.setDdmStructureLayoutId(
+			ddmStructureLayoutId);
+		appBuilderWorkflowTaskLink.setWorkflowTaskName(workflowTaskName);
+
+		return appBuilderWorkflowTaskLinkPersistence.update(
+			appBuilderWorkflowTaskLink);
+	}
+
+	public void deleteAppBuilderWorkflowTaskLinks(long appBuilderAppId) {
+		for (AppBuilderWorkflowTaskLink appBuilderWorkflowTaskLink :
+				getAppBuilderWorkflowTaskLinks(appBuilderAppId)) {
+
+			appBuilderWorkflowTaskLinkPersistence.remove(
+				appBuilderWorkflowTaskLink);
+		}
+	}
+
+	public List<AppBuilderWorkflowTaskLink> getAppBuilderWorkflowTaskLinks(
+		long appBuilderAppId) {
+
+		return appBuilderWorkflowTaskLinkPersistence.findByAppBuilderAppId(
+			appBuilderAppId);
+	}
+
+	public List<AppBuilderWorkflowTaskLink> getAppBuilderWorkflowTaskLinks(
+		long appBuilderAppId, String workflowTaskName) {
+
+		return appBuilderWorkflowTaskLinkPersistence.findByA_W(
+			appBuilderAppId, workflowTaskName);
+	}
+
+	@Reference
+	private AppBuilderAppLocalService _appBuilderAppLocalService;
+
+}
