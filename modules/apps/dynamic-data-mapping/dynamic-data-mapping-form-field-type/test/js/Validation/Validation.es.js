29312d20c76fea7088bec169e615d50291033019
Matuzalém Teles <matuzalemteles@gmail.com>
2020-06-02 19:03:15 -0300

    LPS-114427 Migrate Validation tests for testing-library with react

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/js/Validation/Validation.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/js/Validation/Validation.es.js
@@ -12,12 +12,12 @@
  * details.
  */
 
-import {act, fireEvent} from '@testing-library/react';
+import {act, cleanup, fireEvent, render} from '@testing-library/react';
+import {PageProvider} from 'dynamic-data-mapping-form-renderer';
+import React from 'react';
 
 import Validation from '../../../src/main/resources/META-INF/resources/Validation/Validation.es';
-import withContextMock from '../__mocks__/withContextMock.es';
 
-let component;
 const spritemap = 'icons.svg';
 const defaultValue = {
 	errorMessage: {},
@@ -25,121 +25,122 @@ const defaultValue = {
 	parameter: {},
 };
 
-const ValidationWithContextMock = withContextMock(Validation);
+const ValidationWithProvider = (props) => (
+	<PageProvider value={{editingLanguageId: 'en_US'}}>
+		<Validation {...props} />
+	</PageProvider>
+);
 
 describe('Validation', () => {
-	afterEach(() => {
-		if (component) {
-			component.dispose();
-		}
+	afterEach(cleanup);
+
+	beforeEach(() => {
+		jest.useFakeTimers();
+		fetch.mockResponseOnce(JSON.stringify({}));
 	});
 
 	it('renders checkbox to enable Validation', () => {
-		component = new ValidationWithContextMock({
-			dataType: 'string',
-			label: 'Validator',
-			name: 'validation',
-			spritemap,
-			value: defaultValue,
+		const {container} = render(
+			<ValidationWithProvider
+				dataType="string"
+				label="Validator"
+				name="validation"
+				spritemap={spritemap}
+				value={defaultValue}
+			/>
+		);
+
+		act(() => {
+			jest.runAllTimers();
 		});
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
-	it('enables validation after click on toogle', (done) => {
-		jest.useFakeTimers();
+	it('enables validation after click on toogle', () => {
+		const onChange = jest.fn();
 
-		const handleFieldEdited = (data) => {
-			expect(data.value).toEqual({
-				enableValidation: true,
-				errorMessage: {
-					en_US: undefined,
-				},
-				expression: {
-					name: 'notContains',
-					value: 'NOT(contains(textfield, "{parameter}"))',
-				},
-				parameter: {
-					en_US: undefined,
-				},
-			});
-			done();
-		};
-
-		const events = {fieldEdited: handleFieldEdited};
-
-		component = new ValidationWithContextMock({
-			defaultLanguageId: 'en_US',
-			editingLanguageId: 'en_US',
-			events,
-			expression: {},
-			label: 'Validator',
-			name: 'validation',
-			spritemap,
-			validation: {
-				dataType: 'string',
-				fieldName: 'textfield',
-			},
-			value: defaultValue,
-		});
-
-		const inputCheckbox = component.element.querySelector(
-			'input[type="checkbox"]'
+		const {container} = render(
+			<ValidationWithProvider
+				defaultLanguageId="en_US"
+				editingLanguageId="en_US"
+				expression={{}}
+				label="Validator"
+				name="validation"
+				onChange={onChange}
+				spritemap={spritemap}
+				validation={{
+					dataType: 'string',
+					fieldName: 'textfield',
+				}}
+				value={defaultValue}
+			/>
 		);
+
+		const inputCheckbox = container.querySelector('input[type="checkbox"]');
 
 		fireEvent.click(inputCheckbox);
 
 		act(() => {
 			jest.runAllTimers();
 		});
+
+		expect(onChange).toHaveBeenCalledWith(expect.any(Object), {
+			enableValidation: true,
+			errorMessage: {
+				en_US: undefined,
+			},
+			expression: {
+				name: 'notContains',
+				value: 'NOT(contains(textfield, "{parameter}"))',
+			},
+			parameter: {
+				en_US: undefined,
+			},
+		});
 	});
 
-	it('renders parameter field with Numeric element', (done) => {
-		jest.useFakeTimers();
+	it('renders parameter field with Numeric element', () => {
+		const onChange = jest.fn();
 
-		const handleFieldEdited = (data) => {
-			expect(data.value).toEqual({
-				enableValidation: true,
-				errorMessage: {
-					en_US: undefined,
-				},
-				expression: {
-					name: 'lt',
-					value: 'numericfield<{parameter}',
-				},
-				parameter: {
-					en_US: undefined,
-				},
-			});
-			done();
-		};
-
-		const events = {fieldEdited: handleFieldEdited};
-
-		component = new ValidationWithContextMock({
-			dataType: 'numeric',
-			defaultLanguageId: 'en_US',
-			editingLanguageId: 'en_US',
-			events,
-			expression: {},
-			label: 'Validator',
-			name: 'validation',
-			spritemap,
-			validation: {
-				dataType: 'integer',
-				fieldName: 'numericfield',
-			},
-			value: defaultValue,
-		});
-
-		const inputCheckbox = component.element.querySelector(
-			'input[type="checkbox"]'
+		const {container} = render(
+			<ValidationWithProvider
+				dataType="numeric"
+				defaultLanguageId="en_US"
+				editingLanguageId="en_US"
+				expression={{}}
+				label="Validator"
+				name="validation"
+				onChange={onChange}
+				spritemap={spritemap}
+				validation={{
+					dataType: 'integer',
+					fieldName: 'numericfield',
+				}}
+				value={defaultValue}
+			/>
 		);
+
+		const inputCheckbox = container.querySelector('input[type="checkbox"]');
 
 		fireEvent.click(inputCheckbox);
 
 		act(() => {
 			jest.runAllTimers();
+		});
+
+		expect(onChange).toHaveBeenCalledWith(expect.any(Object), {
+			enableValidation: true,
+			errorMessage: {
+				en_US: undefined,
+			},
+			expression: {
+				name: 'lt',
+				value: 'numericfield<{parameter}',
+			},
+			parameter: {
+				en_US: undefined,
+			},
 		});
 	});
 });
