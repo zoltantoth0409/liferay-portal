8040627e4a154e4faf87fae8e27f58951fdf22e6
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-06-02 16:58:12 -0700

    LPS-114384 Portal instances Headless Implementation

--- modules/apps/headless/headless-portal-instances/headless-portal-instances-impl/src/main/java/com/liferay/headless/portal/instances/internal/resource/v1_0/PortalInstanceResourceImpl.java
+++ modules/apps/headless/headless-portal-instances/headless-portal-instances-impl/src/main/java/com/liferay/headless/portal/instances/internal/resource/v1_0/PortalInstanceResourceImpl.java
@@ -14,9 +14,21 @@
 
 package com.liferay.headless.portal.instances.internal.resource.v1_0;
 
+import com.liferay.headless.portal.instances.dto.v1_0.PortalInstance;
 import com.liferay.headless.portal.instances.resource.v1_0.PortalInstanceResource;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.instances.service.PortalInstancesLocalService;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.servlet.ServletContextPool;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.vulcan.pagination.Page;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +39,124 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = PortalInstanceResource.class
 )
 public class PortalInstanceResourceImpl extends BasePortalInstanceResourceImpl {
+
+	@Override
+	public void deletePortalInstance(String portalInstanceId) throws Exception {
+		Company company = _companyLocalService.getCompanyByWebId(
+			portalInstanceId);
+
+		_companyLocalService.deleteCompany(company.getCompanyId());
+	}
+
+	@Override
+	public PortalInstance getPortalInstance(String portalInstanceId)
+		throws Exception {
+
+		return _toPortalInstance(
+			_companyLocalService.getCompanyByWebId(portalInstanceId));
+	}
+
+	@Override
+	public Page<PortalInstance> getPortalInstancesPage(Boolean skipDefault)
+		throws Exception {
+		
+		List<Company> companies = _companyLocalService.getCompanies(false);
+
+		skipDefault = GetterUtil.getBoolean(skipDefault);
+
+		List<PortalInstance> portalInstances = new ArrayList<>();
+
+		for (Company company : companies) {
+			if (skipDefault &&
+				(_portalInstancesLocalService.getDefaultCompanyId() ==
+					company.getCompanyId())) {
+
+				continue;
+			}
+
+			portalInstances.add(_toPortalInstance(company));
+		}
+
+		return Page.of(portalInstances);
+	}
+
+	@Override
+	public PortalInstance patchPortalInstance(
+			String portalInstanceId, PortalInstance portalInstance)
+		throws Exception {
+
+		Company company = _companyLocalService.getCompanyByWebId(
+			portalInstanceId);
+
+		String domain = GetterUtil.get(
+			portalInstance.getDomain(), company.getMx());
+
+		String virtualHostname = GetterUtil.get(
+			portalInstance.getVirtualHost(), company.getVirtualHostname());
+
+		return _toPortalInstance(
+			_companyLocalService.updateCompany(
+				company.getCompanyId(), virtualHostname, domain,
+				company.getMaxUsers(), company.isActive()));
+	}
+
+	@Override
+	public PortalInstance postPortalInstance(PortalInstance portalInstance)
+		throws Exception {
+
+		Company company = _companyLocalService.addCompany(
+			portalInstance.getCompanyId(), portalInstance.getPortalInstanceId(),
+			portalInstance.getVirtualHost(), portalInstance.getDomain(), false,
+			0, true);
+
+		_portalInstancesLocalService.initializePortalInstance(
+			ServletContextPool.get(StringPool.BLANK), company.getWebId());
+
+		_portalInstancesLocalService.synchronizePortalInstances();
+
+		return _toPortalInstance(company);
+	}
+
+	@Override
+	public void putPortalInstanceActivate(String portalInstanceId)
+		throws Exception {
+
+		Company company = _companyLocalService.getCompanyByWebId(
+			portalInstanceId);
+
+		_companyLocalService.updateCompany(
+			company.getCompanyId(), company.getVirtualHostname(),
+			company.getMx(), company.getMaxUsers(), true);
+	}
+
+	@Override
+	public void putPortalInstanceDeactivate(String portalInstanceId)
+		throws Exception {
+
+		Company company = _companyLocalService.getCompanyByWebId(
+			portalInstanceId);
+
+		_companyLocalService.updateCompany(
+			company.getCompanyId(), company.getVirtualHostname(),
+			company.getMx(), company.getMaxUsers(), false);
+	}
+
+	private PortalInstance _toPortalInstance(Company company) {
+		return new PortalInstance() {
+			{
+				active = company.isActive();
+				companyId = company.getCompanyId();
+				domain = company.getMx();
+				portalInstanceId = company.getWebId();
+				virtualHost = company.getVirtualHostname();
+			}
+		};
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private PortalInstancesLocalService _portalInstancesLocalService;
+
 }
