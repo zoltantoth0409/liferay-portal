92070af21cccb7deb0bb3825927afcb9a4cc699c
Javier Gamarra <nhpatt@gmail.com>
2020-06-02 18:57:36 +0200

    LPS-114737 avoid context

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/QuestionRow.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/QuestionRow.es.js
@@ -12,9 +12,8 @@
  * details.
  */
 
-import React, {useContext} from 'react';
+import React from 'react';
 
-import {AppContext} from '../AppContext.es';
 import {
 	dateToInternationalHuman,
 	normalizeRating,
@@ -27,98 +26,92 @@ import SectionLabel from './SectionLabel.es';
 import TagList from './TagList.es';
 import UserIcon from './UserIcon.es';
 
-export default ({question, showSectionLabel}) => {
-	const context = useContext(AppContext);
+export default ({question, showSectionLabel}) => (
+	<div className="c-mt-4 c-p-3 position-relative question-row text-secondary">
+		<div className="align-items-center d-flex flex-wrap justify-content-between">
+			<span>
+				{showSectionLabel && (
+					<SectionLabel section={question.messageBoardSection} />
+				)}
+			</span>
 
-	const sectionTitle = context.section;
+			<ul className="c-mb-0 d-flex flex-wrap list-unstyled stretched-link-layer">
+				<li>
+					<QuestionBadge
+						symbol={
+							normalizeRating(question.aggregateRating) < 0
+								? 'caret-bottom'
+								: 'caret-top'
+						}
+						value={normalizeRating(question.aggregateRating)}
+					/>
+				</li>
 
-	return (
-		<div className="c-mt-4 c-p-3 position-relative question-row text-secondary">
-			<div className="align-items-center d-flex flex-wrap justify-content-between">
-				<span>
-					{showSectionLabel && (
-						<SectionLabel section={question.messageBoardSection} />
-					)}
-				</span>
+				<li>
+					<QuestionBadge symbol="view" value={question.viewCount} />
+				</li>
 
-				<ul className="c-mb-0 d-flex flex-wrap list-unstyled stretched-link-layer">
-					<li>
-						<QuestionBadge
-							symbol={
-								normalizeRating(question.aggregateRating) < 0
-									? 'caret-bottom'
-									: 'caret-top'
-							}
-							value={normalizeRating(question.aggregateRating)}
-						/>
-					</li>
-
-					<li>
-						<QuestionBadge
-							symbol="view"
-							value={question.viewCount}
-						/>
-					</li>
-
-					<li>
-						<QuestionBadge
-							className={
-								question.hasValidAnswer
-									? 'alert-success border-0'
-									: ''
-							}
-							symbol={
-								question.hasValidAnswer
-									? 'check-circle-full'
-									: 'message'
-							}
-							value={question.numberOfMessageBoardMessages}
-						/>
-					</li>
-				</ul>
-			</div>
-
-			<Link
-				className="questions-title stretched-link"
-				to={`/questions/${sectionTitle}/${question.friendlyUrlPath}`}
-			>
-				<h2 className="c-mb-0 stretched-link-layer text-dark">
-					{question.headline}
-				</h2>
-			</Link>
-
-			<div className="c-mb-0 c-mt-3 question-row-article-body stretched-link-layer text-truncate">
-				<ArticleBodyRenderer
-					{...question}
-					articleBody={stripHTML(question.articleBody)}
-					compactMode={true}
-				/>
-			</div>
-
-			<div className="align-items-sm-center align-items-start d-flex flex-column-reverse flex-sm-row justify-content-between">
-				<div className="c-mt-3 c-mt-sm-0 stretched-link-layer">
-					<Link
-						to={`/questions/${sectionTitle}/creator/${question.creator.id}`}
-					>
-						<UserIcon
-							fullName={question.creator.name}
-							portraitURL={question.creator.image}
-							size="sm"
-							userId={String(question.creator.id)}
-						/>
-
-						<strong className="c-ml-2 text-dark">
-							{question.creator.name}
-						</strong>
-					</Link>
-
-					<span className="c-ml-2 small">
-						{'- ' + dateToInternationalHuman(question.dateModified)}
-					</span>
-				</div>
-
-				<TagList tags={question.keywords} />
-			</div>
+				<li>
+					<QuestionBadge
+						className={
+							question.hasValidAnswer
+								? 'alert-success border-0'
+								: ''
+						}
+						symbol={
+							question.hasValidAnswer
+								? 'check-circle-full'
+								: 'message'
+						}
+						value={question.numberOfMessageBoardMessages}
+					/>
+				</li>
+			</ul>
 		</div>
-	);
-};
+
+		<Link
+			className="questions-title stretched-link"
+			to={`/questions/${question.messageBoardSection.title}/${question.friendlyUrlPath}`}
+		>
+			<h2 className="c-mb-0 stretched-link-layer text-dark">
+				{question.headline}
+			</h2>
+		</Link>
+
+		<div className="c-mb-0 c-mt-3 question-row-article-body stretched-link-layer text-truncate">
+			<ArticleBodyRenderer
+				{...question}
+				articleBody={stripHTML(question.articleBody)}
+				compactMode={true}
+			/>
+		</div>
+
+		<div className="align-items-sm-center align-items-start d-flex flex-column-reverse flex-sm-row justify-content-between">
+			<div className="c-mt-3 c-mt-sm-0 stretched-link-layer">
+				<Link
+					to={`/questions/${question.messageBoardSection.title}/creator/${question.creator.id}`}
+				>
+					<UserIcon
+						fullName={question.creator.name}
+						portraitURL={question.creator.image}
+						size="sm"
+						userId={String(question.creator.id)}
+					/>
+
+					<strong className="c-ml-2 text-dark">
+						{question.creator.name}
+					</strong>
+				</Link>
+
+				<span className="c-ml-2 small">
+					{'- ' + dateToInternationalHuman(question.dateModified)}
+				</span>
+			</div>
+
+			<TagList
+				sectionTitle={question.messageBoardSection.title}
+				tags={question.keywords}
+			/>
+		</div>
+	</div>
+);
--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Question.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/Question.es.js
@@ -324,7 +324,13 @@ export default withRouter(
 								</div>
 
 								<div className="c-mt-4">
-									<TagList tags={question.keywords} />
+									<TagList
+										sectionTitle={
+											question.messageBoardSection &&
+											question.messageBoardSection.title
+										}
+										tags={question.keywords}
+									/>
 								</div>
 
 								<div className="c-mt-4 position-relative questions-creator text-center text-md-right">
