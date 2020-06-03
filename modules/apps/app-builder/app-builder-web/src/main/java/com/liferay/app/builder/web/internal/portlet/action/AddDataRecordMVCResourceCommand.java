c132d14d18fe70318c4029bc1ff9652676c01559
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-06-03 17:35:23 -0300

    LPS-111958 | LPS-114371 Create a resource command to create a dataRecord, link it with the App and start a workflow

---
+++ modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/portlet/action/AddDataRecordMVCResourceCommand.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.app.builder.web.internal.portlet.action;
+
+import com.liferay.app.builder.constants.AppBuilderPortletKeys;
+import com.liferay.app.builder.model.AppBuilderApp;
+import com.liferay.app.builder.service.AppBuilderAppDataRecordLinkLocalService;
+import com.liferay.app.builder.service.AppBuilderAppLocalService;
+import com.liferay.data.engine.rest.dto.v2_0.DataRecord;
+import com.liferay.data.engine.rest.resource.v2_0.DataRecordResource;
+import com.liferay.dynamic.data.lists.service.DDLRecordLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionConfig;
+import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowHandlerRegistryUtil;
+
+import javax.portlet.PortletException;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AppBuilderPortletKeys.APPS,
+		"javax.portlet.name=" + AppBuilderPortletKeys.OBJECTS,
+		"mvc.command.name=/app_builder/add_data_record"
+	},
+	service = MVCResourceCommand.class
+)
+public class AddDataRecordMVCResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		try {
+			TransactionInvokerUtil.invoke(
+				_transactionConfig,
+				() -> {
+					_addDataRecord(resourceRequest);
+
+					return null;
+				});
+		}
+		catch (Throwable throwable) {
+			if (throwable instanceof PortletException) {
+				throw (PortletException)throwable;
+			}
+
+			throw new PortletException(throwable);
+		}
+	}
+
+	private void _addDataRecord(ResourceRequest resourceRequest)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		AppBuilderApp appBuilderApp =
+			_appBuilderAppLocalService.getAppBuilderApp(
+				ParamUtil.getLong(resourceRequest, "appBuilderAppId"));
+
+		DataRecordResource dataRecordResource = DataRecordResource.builder(
+		).user(
+			themeDisplay.getUser()
+		).build();
+
+		DataRecord dataRecord = dataRecordResource.postDataDefinitionDataRecord(
+			appBuilderApp.getDdmStructureId(),
+			DataRecord.toDTO(
+				ParamUtil.getString(resourceRequest, "dataRecord")));
+
+		_appBuilderAppDataRecordLinkLocalService.addAppBuilderAppDataRecordLink(
+			appBuilderApp.getCompanyId(), appBuilderApp.getAppBuilderAppId(),
+			dataRecord.getId());
+
+		WorkflowHandlerRegistryUtil.startWorkflowInstance(
+			themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId(),
+			themeDisplay.getUserId(), AppBuilderApp.class.getName(),
+			dataRecord.getId(),
+			_ddlRecordLocalService.getDDLRecord(dataRecord.getId()),
+			new ServiceContext());
+	}
+
+	private static final TransactionConfig _transactionConfig;
+
+	static {
+		TransactionConfig.Builder builder = new TransactionConfig.Builder();
+
+		builder.setPropagation(Propagation.REQUIRES_NEW);
+		builder.setRollbackForClasses(Exception.class);
+
+		_transactionConfig = builder.build();
+	}
+
+	@Reference
+	private AppBuilderAppDataRecordLinkLocalService
+		_appBuilderAppDataRecordLinkLocalService;
+
+	@Reference
+	private AppBuilderAppLocalService _appBuilderAppLocalService;
+
+	@Reference
+	private DDLRecordLocalService _ddlRecordLocalService;
+
+}
