e8a0c4a96cee310132de10c0dae535f79f379748
Bruno Basto <bruno.basto@liferay.com>
2020-06-05 14:49:37 -0300

    LPS-114698 Brings exposed APIs from Commerce's Dataset Display

---
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/data/provider/ClayDataSetDataProvider.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.data.provider;
+
+import com.liferay.frontend.taglib.clay.data.Filter;
+import com.liferay.frontend.taglib.clay.data.Pagination;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.Sort;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.annotation.versioning.ConsumerType;
+
+/**
+ * @author Marco Leo
+ */
+@ConsumerType
+public interface ClayDataSetDataProvider<T> {
+
+	public int countItems(HttpServletRequest httpServletRequest, Filter filter)
+		throws PortalException;
+
+	public List<T> getItems(
+			HttpServletRequest httpServletRequest, Filter filter,
+			Pagination pagination, Sort sort)
+		throws PortalException;
+
+}
