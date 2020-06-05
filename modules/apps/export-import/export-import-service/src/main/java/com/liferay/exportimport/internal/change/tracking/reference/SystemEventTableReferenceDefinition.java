61713cdebc6096ae1daa93ca3f0a37722f159a47
Preston-Crary <preston.crary@liferay.com>
2020-06-05 11:47:39 -0700

    LPS-115009 SystemEventTableReferenceDefinition.

---
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/change/tracking/reference/SystemEventTableReferenceDefinition.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.exportimport.internal.change.tracking.reference;
+
+import com.liferay.change.tracking.reference.TableReferenceDefinition;
+import com.liferay.change.tracking.reference.builder.TableReferenceInfoBuilder;
+import com.liferay.portal.kernel.model.CompanyTable;
+import com.liferay.portal.kernel.model.GroupTable;
+import com.liferay.portal.kernel.model.SystemEventTable;
+import com.liferay.portal.kernel.model.UserTable;
+import com.liferay.portal.kernel.service.persistence.BasePersistence;
+import com.liferay.portal.kernel.service.persistence.SystemEventPersistence;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Preston Crary
+ */
+@Component(service = TableReferenceDefinition.class)
+public class SystemEventTableReferenceDefinition
+	implements TableReferenceDefinition<SystemEventTable> {
+
+	@Override
+	public void defineTableReferences(
+		TableReferenceInfoBuilder<SystemEventTable> tableReferenceInfoBuilder) {
+
+		tableReferenceInfoBuilder.singleColumnReference(
+			SystemEventTable.INSTANCE.groupId, GroupTable.INSTANCE.groupId
+		).singleColumnReference(
+			SystemEventTable.INSTANCE.companyId, CompanyTable.INSTANCE.companyId
+		).singleColumnReference(
+			SystemEventTable.INSTANCE.userId, UserTable.INSTANCE.userId
+		).nonreferenceColumns(
+			SystemEventTable.INSTANCE.userName,
+			SystemEventTable.INSTANCE.createDate,
+			SystemEventTable.INSTANCE.classNameId,
+			SystemEventTable.INSTANCE.classPK,
+			SystemEventTable.INSTANCE.classUuid,
+			SystemEventTable.INSTANCE.referrerClassNameId
+		).parentColumnReference(
+			SystemEventTable.INSTANCE.systemEventId,
+			SystemEventTable.INSTANCE.parentSystemEventId
+		).nonreferenceColumns(
+			SystemEventTable.INSTANCE.systemEventSetKey,
+			SystemEventTable.INSTANCE.type, SystemEventTable.INSTANCE.extraData
+		);
+	}
+
+	@Override
+	public BasePersistence<?> getBasePersistence() {
+		return _systemEventPersistence;
+	}
+
+	@Override
+	public SystemEventTable getTable() {
+		return SystemEventTable.INSTANCE;
+	}
+
+	@Reference
+	private SystemEventPersistence _systemEventPersistence;
+
+}
