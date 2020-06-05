2fdacc2c29afdb29a74009693a0bae7a16195f50
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2020-06-05 13:44:48 +0200

    LPS-114691 Modify structure, js and style of slider component

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-basic-component/src/main/resources/com/liferay/fragment/collection/contributor/basic/component/dependencies/slider/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-basic-component/src/main/resources/com/liferay/fragment/collection/contributor/basic/component/dependencies/slider/index.js
@@ -1,27 +1,74 @@
-const slide = fragmentElement.querySelector('.slide');
-const indicators = Array.from(
-	fragmentElement.querySelectorAll('.carousel-indicators > li')
+/*eslint-disable*/
+const MOVE_LEFT = 'move-left';
+const MOVE_RIGHT = 'move-right';
+
+const indicators = [].slice.call(
+	document.querySelectorAll('.carousel-indicators li')
 );
-const carouselControls = Array.from(
-	fragmentElement.querySelectorAll(
-		'.carousel-control-prev, .carousel-control-next'
-	)
-);
-const carouselId = fragmentElement.id + '-carousel';
+const items = [].slice.call(document.querySelectorAll('.carousel-item'));
 
-slide.id = carouselId;
-indicators.forEach(function(indicator) {
-	indicator.dataset.target = '#' + carouselId;
-});
-carouselControls.forEach(function(control) {
-	control.href = '#' + carouselId;
-});
+const next = document.querySelector('.carousel-control-next');
+const prev = document.querySelector('.carousel-control-prev');
 
-slide.classList.add('carousel');
+const getActiveElements = function () {
+	const activeIndicator = document.querySelector(
+		'.carousel-indicators .active'
+	);
+	const activeItem = document.querySelector('.carousel-item.active');
+	const indexActiveItem = items.indexOf(activeItem);
 
-if (document.querySelector('.has-edit-mode-menu')) {
-	$(slide).carousel('pause');
-	$('.carousel').off('keydown.bs.carousel');
-} else {
-	$(slide).carousel({ride: 'carousel'});
+	return [activeItem, indexActiveItem, activeIndicator];
+};
+
+const move = function (movement) {
+	const [activeItem, indexActiveItem, activeIndicator] = getActiveElements();
+	let nextItemIndex =
+		indexActiveItem < 1 ? items.length - 1 : indexActiveItem - 1;
+
+	if (movement === MOVE_RIGHT) {
+		nextItemIndex = indexActiveItem >= 2 ? 0 : indexActiveItem + 1;
+	}
+	const nextItem = items[nextItemIndex];
+
+	startMove({
+		activeIndicator,
+		activeItem,
+		movement,
+		nextItem,
+		nextItemIndex,
+	});
+	finishMove({activeItem, movement, nextItem});
+};
+
+const startMove = function ({
+	activeIndicator,
+	activeItem,
+	movement,
+	nextItem,
+	nextItemIndex,
+}) {
+	activeItem.classList.add(movement);
+	nextItem.classList.add(movement);
+	activeIndicator.classList.remove('active');
+	indicators[nextItemIndex].classList.add('active');
+};
+
+const finishMove = function ({activeItem, nextItem, movement}) {
+	setTimeout(function () {
+		activeItem.classList.remove('active', movement);
+		nextItem.classList.add('active');
+		nextItem.classList.remove(movement);
+	}, 600);
+};
+
+function main() {
+	prev.addEventListener('click', function () {
+		move(MOVE_LEFT);
+	});
+
+	next.addEventListener('click', function () {
+		move(MOVE_RIGHT);
+	});
 }
+
+main();
