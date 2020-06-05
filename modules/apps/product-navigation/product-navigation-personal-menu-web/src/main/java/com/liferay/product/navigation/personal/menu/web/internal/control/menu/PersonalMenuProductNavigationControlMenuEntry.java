0721f5db712ca1792d078494624087066af9bc67
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-06-05 11:26:49 +0100

    LPS-113981 User icon should be on the right

--- modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/control/menu/PersonalMenuProductNavigationControlMenuEntry.java
+++ modules/apps/product-navigation/product-navigation-personal-menu-web/src/main/java/com/liferay/product/navigation/personal/menu/web/internal/control/menu/PersonalMenuProductNavigationControlMenuEntry.java
@@ -44,7 +44,7 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"product.navigation.control.menu.category.key=" + ProductNavigationControlMenuCategoryKeys.USER,
-		"product.navigation.control.menu.entry.order:Integer=400"
+		"product.navigation.control.menu.entry.order:Integer=600"
 	},
 	service = ProductNavigationControlMenuEntry.class
 )
