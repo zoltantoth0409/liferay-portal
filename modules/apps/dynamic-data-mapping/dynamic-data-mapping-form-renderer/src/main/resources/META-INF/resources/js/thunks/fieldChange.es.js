e99a07c55e4b6556e976f2d746f5b3bf71e38ca0
Matuzalém Teles <matuzalemteles@gmail.com>
2020-06-05 21:25:11 -0300

    LPS-112426 WIP Adds fieldChange thunk

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/thunks/fieldChange.es.js
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {EVENT_TYPES} from '../actions/types.es';
+import {evaluate} from '../util/evaluation.es';
+import {PagesVisitor} from '../util/visitors.es';
+
+let REVALIDATE_UPDATES = [];
+
+const getEditedPages = ({editingLanguageId, name, pages, value}) => {
+	const pageVisitor = new PagesVisitor(pages);
+
+	return pageVisitor.mapFields(
+		(field) => {
+			if (field.name === name) {
+				return {
+					...field,
+					localizedValue: {
+						...field.localizedValue,
+						[editingLanguageId]: value,
+					},
+					value,
+				};
+			}
+
+			return field;
+		},
+		false,
+		true
+	);
+};
+
+export default function fieldChange({
+	defaultLanguageId,
+	editingLanguageId,
+	pages,
+	portletNamespace,
+	properties,
+}) {
+	return (dispatch) => {
+		const {fieldInstance, value} = properties;
+		const {evaluable, fieldName} = fieldInstance;
+
+		const editedPages = getEditedPages({
+			editingLanguageId,
+			name: fieldInstance.name,
+			pages,
+			value,
+		});
+
+		dispatch({payload: editedPages, type: EVENT_TYPES.FIELD_BLUR});
+
+		if (evaluable) {
+			evaluate(fieldName, {
+				defaultLanguageId,
+				editingLanguageId,
+				pages: editedPages,
+				portletNamespace,
+			})
+				.then((evaluatedPages) => {
+					if (REVALIDATE_UPDATES.length > 0) {
+
+						// All non-evaluable operations that were performed after the request
+						// was sent are used here to revalidate the new data.
+
+						REVALIDATE_UPDATES.forEach((item) => {
+							evaluatedPages = getEditedPages({
+								...item,
+								pages: evaluatedPages,
+							});
+						});
+
+						// Redefine the list of updates to avoid leaking memory and avoid
+						// more expensive operations in the next interactions
+
+						REVALIDATE_UPDATES = [];
+					}
+
+					return evaluatedPages;
+				})
+				.then((evaluatedPages) => {
+					if (fieldInstance.isDisposed()) {
+						return;
+					}
+
+					dispatch({
+						payload: evaluatedPages,
+						type: EVENT_TYPES.UPDATE_PAGES,
+					});
+					dispatch({
+						payload: evaluatedPages,
+						type: EVENT_TYPES.FIELD_EVALUATED,
+					});
+				})
+				.catch((error) =>
+					dispatch({
+						payload: error,
+						type: EVENT_TYPES.FIELD_EVALUATION_ERROR,
+					})
+				);
+		}
+		else {
+			REVALIDATE_UPDATES.push({
+				editingLanguageId,
+				name: fieldInstance.name,
+				value,
+			});
+		}
+	};
+}
