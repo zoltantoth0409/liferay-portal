49b5b940092b7bdabef23d3422f1c0f2d78350f4
Sarai Díaz García <dgarciasarai@gmail.com>
2020-06-08 12:17:46 +0200

    LPS-115043 Add test

---
+++ modules/apps/content-dashboard/content-dashboard-web/test/js/components/EmptyBarChart.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+
+import EmptyBarChart from '../../../src/main/resources/META-INF/resources/js/components/EmptyBarChart';
+
+import '@testing-library/jest-dom/extend-expect';
+
+describe('EmptyBarChart', () => {
+	afterEach(cleanup);
+
+	it('renders empty bar chart if there is no content labeled with marketing categories', () => {
+		const {getByText} = render(<EmptyBarChart />);
+
+		expect(getByText('there-is-no-data-yet')).toBeInTheDocument();
+		expect(
+			getByText(
+				'please-create-marketing-categories-to-label-and-audit-your-content'
+			)
+		).toBeInTheDocument();
+		expect(getByText('add-marketing-categories')).toBeInTheDocument();
+	});
+});
