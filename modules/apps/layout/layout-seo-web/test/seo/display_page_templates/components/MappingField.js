e240f40e51507d1284f0e62553775afb930df759
Jorge González <jorge.gonzalez@liferay.com>
2020-06-08 19:48:24 +0200

    LPS-115086 Add first test

---
+++ modules/apps/layout/layout-seo-web/test/seo/display_page_templates/components/MappingField.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+
+import MappingField from '../../../../src/main/resources/META-INF/resources/js/seo/display_page_templates/components/MappingField';
+
+const baseProps = {
+	fields: [
+		{key: 'field-1', label: 'Field 1'},
+		{key: 'field-2', label: 'Field 2'},
+	],
+	label: 'Label test mapping field',
+	name: 'testMappingField',
+	selectedField: {
+		key: 'field-2',
+		label: 'Field label',
+	},
+	selectedSource: {
+		classTypeLabel: 'Label source type',
+	},
+};
+
+const renderComponent = (props) =>
+	render(<MappingField {...baseProps} {...props} />);
+
+describe('MappingField', () => {
+	afterEach(cleanup);
+
+	describe('when rendered with the default props', () => {
+		let result;
+		let inputValue;
+
+		beforeEach(() => {
+			result = renderComponent();
+			inputValue = result.getByDisplayValue('field-2');
+		});
+
+		it('has an input hidden with the selected field key', () => {
+			expect(inputValue.type).toBe('hidden');
+		});
+
+		it('the input has the passed name', () => {
+			expect(inputValue.name).toBe('testMappingField');
+		});
+	});
+});
