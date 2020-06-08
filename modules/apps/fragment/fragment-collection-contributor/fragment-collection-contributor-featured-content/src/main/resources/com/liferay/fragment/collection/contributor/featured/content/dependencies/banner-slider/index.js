f98f415b5415224e4feba09022e2607098b0c83f
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2020-06-08 09:52:38 +0200

    LPS-114691 Modify structure, style and js for Banner Slider

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-featured-content/src/main/resources/com/liferay/fragment/collection/contributor/featured/content/dependencies/banner-slider/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-featured-content/src/main/resources/com/liferay/fragment/collection/contributor/featured/content/dependencies/banner-slider/index.js
@@ -1,27 +1,105 @@
-const slide = fragmentElement.querySelector('.slide');
-const indicators = Array.from(
-	fragmentElement.querySelectorAll('.carousel-indicators > li')
+/*eslint-disable*/
+const MOVE_LEFT = 'move-left';
+const MOVE_RIGHT = 'move-right';
+const INTERVAL = 5000;
+
+const editMode = document.body.classList.contains('has-edit-mode-menu');
+const indicators = [].slice.call(
+	document.querySelectorAll('.carousel-navigation button')
 );
-const carouselControls = Array.from(
-	fragmentElement.querySelectorAll(
-		'.carousel-control-prev, .carousel-control-next'
-	)
-);
-const carouselId = fragmentElement.id + '-carousel';
+const items = [].slice.call(document.querySelectorAll('.carousel-item'));
 
-slide.id = carouselId;
-indicators.forEach(function(indicator) {
-	indicator.dataset.target = '#' + carouselId;
-});
-carouselControls.forEach(function(control) {
-	control.href = '#' + carouselId;
-});
+const getActiveElements = function () {
+	const activeIndicator = document.querySelector(
+		'.carousel-navigation .active'
+	);
+	const activeItem = document.querySelector('.carousel-item.active');
+	const indexActiveItem = items.indexOf(activeItem);
 
-slide.classList.add('carousel');
+	return [activeItem, indexActiveItem, activeIndicator];
+};
 
-if (document.querySelector('.has-edit-mode-menu')) {
-	$(slide).carousel('pause');
-	$('.carousel').off('keydown.bs.carousel');
-} else {
-	$(slide).carousel({ride: 'carousel'});
+const startMove = function ({
+	activeIndicator,
+	activeItem,
+	movement,
+	nextItem,
+	nextItemIndex,
+}) {
+	activeItem.classList.add(movement);
+	nextItem.classList.add(movement);
+	activeIndicator.classList.remove('active');
+	indicators[nextItemIndex].classList.add('active');
+};
+
+const finishMove = function ({activeItem, nextItem, movement}) {
+	setTimeout(function () {
+		activeItem.classList.remove('active', movement);
+		nextItem.classList.add('active');
+		nextItem.classList.remove(movement);
+	}, 600);
+};
+
+const move = function (movement, index = null) {
+	const [activeItem, indexActiveItem, activeIndicator] = getActiveElements();
+	let nextItemIndex = index;
+
+	if (index === null) {
+		nextItemIndex = indexActiveItem >= 2 ? 0 : indexActiveItem + 1;
+	}
+
+	const nextItem = items[nextItemIndex];
+
+	startMove({
+		activeIndicator,
+		activeItem,
+		movement,
+		nextItem,
+		nextItemIndex,
+	});
+	finishMove({activeItem, movement, nextItem});
+};
+
+const resetInterval = function (interval, clear = true) {
+	if (clear) {
+		clearInterval(interval);
+	}
+	return setInterval(function () {
+		if (document.contains(items[0])) {
+			move(MOVE_RIGHT);
+		}
+		else {
+			clearInterval(interval);
+		}
+	}, INTERVAL);
+};
+
+function main() {
+	let interval = null;
+
+	if (!editMode) {
+		interval = resetInterval(interval, false);
+	}
+
+	indicators.forEach(function (indicator, index) {
+		indicator.addEventListener('click', function () {
+			const [, , activeIndicator] = getActiveElements();
+			const indexActiveIndicator = indicators.indexOf(activeIndicator);
+
+			if (index !== indexActiveIndicator) {
+				if (index < indexActiveIndicator) {
+					move(MOVE_LEFT, index);
+				}
+				else {
+					move(MOVE_RIGHT, index);
+				}
+			}
+
+			if (!editMode) {
+				interval = resetInterval(interval);
+			}
+		});
+	});
 }
+
+main();
