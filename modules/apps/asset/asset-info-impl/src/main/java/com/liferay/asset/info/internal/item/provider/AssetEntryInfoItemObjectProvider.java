b1433b756cd29953bf788e2715d21e55fb5e606a
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-06-08 10:47:02 +0100

    LPS-115059 Implements Object Provider for AssetEntry

---
+++ modules/apps/asset/asset-info-impl/src/main/java/com/liferay/asset/info/internal/item/provider/AssetEntryInfoItemObjectProvider.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.info.internal.item.provider;
+
+import com.liferay.asset.kernel.exception.NoSuchEntryException;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
+import com.liferay.info.item.NoSuchInfoItemException;
+import com.liferay.info.item.provider.InfoItemObjectProvider;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jorge Ferrer
+ */
+@Component(service = InfoItemObjectProvider.class)
+public class AssetEntryInfoItemObjectProvider
+	implements InfoItemObjectProvider<AssetEntry> {
+
+	@Override
+	public AssetEntry getInfoItem(long classPK) throws NoSuchInfoItemException {
+		try {
+			return _assetEntryLocalService.getEntry(classPK);
+		}
+		catch (NoSuchEntryException noSuchEntryException) {
+			throw new NoSuchInfoItemException(
+				"Unable to get AssetEntry " + classPK, noSuchEntryException);
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
+	}
+
+	@Reference
+	private AssetEntryLocalService _assetEntryLocalService;
+
+}
