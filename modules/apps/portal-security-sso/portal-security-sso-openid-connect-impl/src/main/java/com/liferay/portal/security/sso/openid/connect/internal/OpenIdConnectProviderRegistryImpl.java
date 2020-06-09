ab6d8dbb4041460f28c5462934db5a3ae2e35ba9
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2020-06-09 17:34:37 +0100

    LPS-115035 Rebuild only sees companies with at least one OpenId Connect provider configured at instance level

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectProviderRegistryImpl.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectProviderRegistryImpl.java
@@ -42,6 +42,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.osgi.framework.Constants;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedServiceFactory;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -105,7 +106,9 @@ public class OpenIdConnectProviderRegistryImpl
 					_companyIdProviderNameOpenIdConnectProviders.get(companyId);
 
 		if (openIdConnectProviderMap == null) {
-			return null;
+			openIdConnectProviderMap =
+				_companyIdProviderNameOpenIdConnectProviders.get(
+					CompanyConstants.SYSTEM);
 		}
 
 		return openIdConnectProviderMap.get(name);
@@ -120,7 +123,9 @@ public class OpenIdConnectProviderRegistryImpl
 					_companyIdProviderNameOpenIdConnectProviders.get(companyId);
 
 		if (openIdConnectProviderMap == null) {
-			return Collections.emptySet();
+			openIdConnectProviderMap =
+				_companyIdProviderNameOpenIdConnectProviders.get(
+					CompanyConstants.SYSTEM);
 		}
 
 		return Collections.unmodifiableSet(openIdConnectProviderMap.keySet());
@@ -155,6 +160,12 @@ public class OpenIdConnectProviderRegistryImpl
 		}
 
 		_rebuild(companyId);
+	}
+
+	@Activate
+	protected void activate() {
+		_companyIdProviderNameOpenIdConnectProviders.putIfAbsent(
+			CompanyConstants.SYSTEM, Collections.emptyMap());
 	}
 
 	protected OpenIdConnectProvider<OIDCClientMetadata, OIDCProviderMetadata>
