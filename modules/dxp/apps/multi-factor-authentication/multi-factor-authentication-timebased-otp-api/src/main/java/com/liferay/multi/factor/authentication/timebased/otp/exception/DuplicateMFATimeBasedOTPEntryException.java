cb0566314d82f53bb2ec366542fb651d439229d2
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-06-09 16:37:37 +0200

    LPS-101744 regen

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-timebased-otp-api/src/main/java/com/liferay/multi/factor/authentication/timebased/otp/exception/DuplicateMFATimeBasedOTPEntryException.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.timebased.otp.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Arthur Chan
+ */
+public class DuplicateMFATimeBasedOTPEntryException extends PortalException {
+
+	public DuplicateMFATimeBasedOTPEntryException() {
+	}
+
+	public DuplicateMFATimeBasedOTPEntryException(String msg) {
+		super(msg);
+	}
+
+	public DuplicateMFATimeBasedOTPEntryException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public DuplicateMFATimeBasedOTPEntryException(Throwable cause) {
+		super(cause);
+	}
+
+}
