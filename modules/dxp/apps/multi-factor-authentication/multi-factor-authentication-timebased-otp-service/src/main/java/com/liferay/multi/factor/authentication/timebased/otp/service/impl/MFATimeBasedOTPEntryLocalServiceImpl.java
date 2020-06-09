8df885eeb0780ec97264d401dc996c909b74ad9f
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-06-09 19:41:19 +0200

    LPS-101744 Auto SF

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-timebased-otp-service/src/main/java/com/liferay/multi/factor/authentication/timebased/otp/service/impl/MFATimeBasedOTPEntryLocalServiceImpl.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-timebased-otp-service/src/main/java/com/liferay/multi/factor/authentication/timebased/otp/service/impl/MFATimeBasedOTPEntryLocalServiceImpl.java
@@ -64,6 +64,7 @@ public class MFATimeBasedOTPEntryLocalServiceImpl
 		User user = userLocalService.getUserById(userId);
 
 		mfaTimeBasedOTPEntry.setCompanyId(user.getCompanyId());
+
 		mfaTimeBasedOTPEntry.setUserId(userId);
 		mfaTimeBasedOTPEntry.setUserName(user.getFullName());
 
