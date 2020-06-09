1bf102974e4025f4d4db89ac236ecf06b0a8f53e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-06-09 12:30:43 +0100

    LPS-116533 Adds widgets to react context

--- modules/apps/product-navigation/product-navigation-control-menu-web/src/main/java/com/liferay/product/navigation/control/menu/web/internal/display/context/AddContentPanelDisplayContext.java
+++ modules/apps/product-navigation/product-navigation-control-menu-web/src/main/java/com/liferay/product/navigation/control/menu/web/internal/display/context/AddContentPanelDisplayContext.java
@@ -14,17 +14,219 @@
 
 package com.liferay.product.navigation.control.menu.web.internal.display.context;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.PortletApp;
+import com.liferay.portal.kernel.model.PortletCategory;
+import com.liferay.portal.kernel.portlet.PortletConfigFactoryUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.PortletLocalServiceUtil;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalServiceUtil;
+import com.liferay.portal.kernel.service.permission.PortletPermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.util.comparator.PortletCategoryComparator;
+import com.liferay.portal.kernel.util.comparator.PortletTitleComparator;
+import com.liferay.portal.util.PortletCategoryUtil;
+import com.liferay.portal.util.WebAppPool;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.portlet.PortletConfig;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Eudaldo Alonso
  */
 public class AddContentPanelDisplayContext {
 
-	public AddContentPanelDisplayContext(HttpServletRequest httpServletRequest) {
+	public AddContentPanelDisplayContext(
+		HttpServletRequest httpServletRequest) {
+
 		_httpServletRequest = httpServletRequest;
+
+		_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
 	}
 
+	public Map<String, Object> getAddContentPanelData() throws Exception {
+		return HashMapBuilder.<String, Object>put(
+			"widgets", _getWidgets()
+		).build();
+	}
+
+	private String _getPortletCategoryTitle(PortletCategory portletCategory) {
+		for (String portletId :
+				PortletCategoryUtil.getFirstChildPortletIds(portletCategory)) {
+
+			Portlet portlet = PortletLocalServiceUtil.getPortletById(
+				_themeDisplay.getCompanyId(), portletId);
+
+			if (portlet == null) {
+				continue;
+			}
+
+			PortletApp portletApp = portlet.getPortletApp();
+
+			if (!portletApp.isWARFile()) {
+				continue;
+			}
+
+			PortletConfig portletConfig = PortletConfigFactoryUtil.create(
+				portlet, _httpServletRequest.getServletContext());
+
+			ResourceBundle portletResourceBundle =
+				portletConfig.getResourceBundle(_themeDisplay.getLocale());
+
+			String title = ResourceBundleUtil.getString(
+				portletResourceBundle, portletCategory.getName());
+
+			if (Validator.isNotNull(title)) {
+				return title;
+			}
+		}
+
+		return LanguageUtil.get(_httpServletRequest, portletCategory.getName());
+	}
+
+	private List<HashMap<String, Object>> _getPortlets(
+		PortletCategory portletCategory) {
+
+		Set<String> portletIds = portletCategory.getPortletIds();
+
+		Stream<String> stream = portletIds.stream();
+
+		HttpSession session = _httpServletRequest.getSession();
+
+		ServletContext servletContext = session.getServletContext();
+
+		return stream.map(
+			portletId -> PortletLocalServiceUtil.getPortletById(
+				_themeDisplay.getCompanyId(), portletId)
+		).filter(
+			portlet -> {
+				if (portlet == null) {
+					return false;
+				}
+
+				try {
+					return PortletPermissionUtil.contains(
+						_themeDisplay.getPermissionChecker(),
+						_themeDisplay.getLayout(), portlet,
+						ActionKeys.ADD_TO_PAGE);
+				}
+				catch (PortalException portalException) {
+					_log.error(
+						"Unable to check portlet permissions for " +
+							portlet.getPortletId(),
+						portalException);
+
+					return false;
+				}
+			}
+		).sorted(
+			new PortletTitleComparator(
+				servletContext, _themeDisplay.getLocale())
+		).map(
+			portlet -> HashMapBuilder.<String, Object>put(
+				"instanceable", portlet.isInstanceable()
+			).put(
+				"portletId", portlet.getPortletId()
+			).put(
+				"title",
+				PortalUtil.getPortletTitle(
+					portlet, servletContext, _themeDisplay.getLocale())
+			).put(
+				"used", _isUsed(portlet, _themeDisplay.getPlid())
+			).build()
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	private List<Map<String, Object>> _getWidgetCategories(
+		PortletCategory portletCategory) {
+
+		Collection<PortletCategory> portletCategories =
+			portletCategory.getCategories();
+
+		Stream<PortletCategory> stream = portletCategories.stream();
+
+		return stream.sorted(
+			new PortletCategoryComparator(_themeDisplay.getLocale())
+		).filter(
+			category -> !category.isHidden()
+		).map(
+			category -> HashMapBuilder.<String, Object>put(
+				"categories", _getWidgetCategories(category)
+			).put(
+				"path",
+				StringUtil.replace(
+					category.getPath(), new String[] {"/", "."},
+					new String[] {"-", "-"})
+			).put(
+				"portlets", _getPortlets(category)
+			).put(
+				"title", _getPortletCategoryTitle(category)
+			).build()
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	private List<Map<String, Object>> _getWidgets() throws Exception {
+		PortletCategory portletCategory = (PortletCategory)WebAppPool.get(
+			_themeDisplay.getCompanyId(), WebKeys.PORTLET_CATEGORY);
+
+		portletCategory = PortletCategoryUtil.getRelevantPortletCategory(
+			_themeDisplay.getPermissionChecker(), _themeDisplay.getCompanyId(),
+			_themeDisplay.getLayout(), portletCategory,
+			_themeDisplay.getLayoutTypePortlet());
+
+		return _getWidgetCategories(portletCategory);
+	}
+
+	private boolean _isUsed(Portlet portlet, long plid) {
+		if (portlet.isInstanceable()) {
+			return false;
+		}
+
+		long count =
+			PortletPreferencesLocalServiceUtil.getPortletPreferencesCount(
+				PortletKeys.PREFS_OWNER_TYPE_LAYOUT, plid,
+				portlet.getPortletId());
+
+		if (count > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AddContentPanelDisplayContext.class);
+
 	private final HttpServletRequest _httpServletRequest;
+	private final ThemeDisplay _themeDisplay;
 
 }
