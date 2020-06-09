55e0b80acc293057a788f02fb9c502b48f48ef2d
Leonardo Barros <leonardo.barros@liferay.com>
2020-06-09 14:51:34 -0300

    LPS-109496 Add value accessor implementation for Image field

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/image/ImageDDMFormFieldValueAccessor.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.image;
+
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldValueAccessor;
+import com.liferay.dynamic.data.mapping.model.Value;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Leonardo Barros
+ */
+@Component(
+	immediate = true, property = "ddm.form.field.type.name=image",
+	service = {
+		DDMFormFieldValueAccessor.class, ImageDDMFormFieldValueAccessor.class
+	}
+)
+public class ImageDDMFormFieldValueAccessor
+	implements DDMFormFieldValueAccessor<JSONObject> {
+
+	@Override
+	public JSONObject getValue(
+		DDMFormFieldValue ddmFormFieldValue, Locale locale) {
+
+		Value value = ddmFormFieldValue.getValue();
+
+		if (value == null) {
+			return jsonFactory.createJSONObject();
+		}
+
+		try {
+			return jsonFactory.createJSONObject(value.getString(locale));
+		}
+		catch (JSONException jsonException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to parse JSON object", jsonException);
+			}
+
+			return jsonFactory.createJSONObject();
+		}
+	}
+
+	@Override
+	public boolean isEmpty(DDMFormFieldValue ddmFormFieldValue, Locale locale) {
+		JSONObject jsonObject = getValue(ddmFormFieldValue, locale);
+
+		if (Validator.isNull(jsonObject.getString("description")) ||
+			Validator.isNull(jsonObject.getString("url"))) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	protected JSONFactory jsonFactory;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ImageDDMFormFieldValueAccessor.class);
+
+}
