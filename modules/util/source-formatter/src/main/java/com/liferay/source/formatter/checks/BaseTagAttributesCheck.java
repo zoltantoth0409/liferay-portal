078b4b05e97a17393c313edbd68f25fc4d023bbd
Hugo Huijser <hugo.huijser@liferay.com>
2020-06-10 15:19:16 +0800

    Revert "LPS-114570 Fix for inline Javascript that contains '""
    This reverts commit 5d25aeed002978925b6fd3ab61c0d7bc948f88a6.

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BaseTagAttributesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BaseTagAttributesCheck.java
@@ -268,11 +268,7 @@ public abstract class BaseTagAttributesCheck extends BaseFileCheck {
 				String attributeValue = entry.getValue();
 
 				if (_escapeQuotes ||
-					!attributeValue.contains(StringPool.QUOTE) ||
-					(attributeValue.contains(
-						StringPool.APOSTROPHE + StringPool.QUOTE) &&
-					 attributeValue.contains(
-						 StringPool.QUOTE + StringPool.APOSTROPHE))) {
+					!attributeValue.contains(StringPool.QUOTE)) {
 
 					delimeter = StringPool.QUOTE;
 				}
