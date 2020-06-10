6c9f0ee94397834928365ecd4d81a2db92df3bec
pei-jung <pei-jung.lan@liferay.com>
2020-06-10 12:54:55 -0700

    LPS-107597 Disable SPA for the account users portlet so the form is not cached and applies the validation

--- modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/AccountUsersAdminPortlet.java
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/AccountUsersAdminPortlet.java
@@ -31,6 +31,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.single-page-application=false",
 		"com.liferay.portlet.use-default-template=true",
 		"javax.portlet.display-name=Account Users",
 		"javax.portlet.expiration-cache=0",
