342d9193647a5c98675f2f101db2ab5f1ce78488
Inácio Nery <inacio.nery@liferay.com>
2020-06-10 10:58:29 -0300

    LPS-114374 | LPS-111958 Add Workflow tab

---
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/java/com/liferay/app/builder/workflow/web/internal/portlet/tab/WorkflowAppBuilderAppPortletTab.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.app.builder.workflow.web.internal.portlet.tab;
+
+import com.liferay.app.builder.portlet.tab.AppBuilderAppPortletTab;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Inácio Nery
+ */
+@Component(
+	immediate = true, property = "app.builder.app.tab.name=workflow",
+	service = AppBuilderAppPortletTab.class
+)
+public class WorkflowAppBuilderAppPortletTab
+	implements AppBuilderAppPortletTab {
+
+	@Override
+	public String getEditEntryPoint() {
+		return _npmResolver.resolveModuleName(
+			"app-builder-workflow-web/js/pages/entry/EditEntry.es");
+	}
+
+	@Override
+	public String getListEntryPoint() {
+		return _npmResolver.resolveModuleName(
+			"app-builder-workflow-web/js/pages/entry/ListEntries.es");
+	}
+
+	@Override
+	public String getViewEntryPoint() {
+		return _npmResolver.resolveModuleName(
+			"app-builder-workflow-web/js/pages/entry/ViewEntry.es");
+	}
+
+	@Reference
+	private NPMResolver _npmResolver;
+
+}
