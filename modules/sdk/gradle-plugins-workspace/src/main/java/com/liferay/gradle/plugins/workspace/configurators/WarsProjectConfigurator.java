182695c0a673d1a24643e8182192726ed7aa9235
Peter Shin <peter.shin@liferay.com>
2020-06-10 13:05:09 -0500

    LPS-111461 As used

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/WarsProjectConfigurator.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/WarsProjectConfigurator.java
@@ -104,17 +104,15 @@ public class WarsProjectConfigurator extends BaseProjectConfigurator {
 						Path dirPath, BasicFileAttributes basicFileAttributes)
 					throws IOException {
 
-					Path dirNamePath = dirPath.getFileName();
-
-					String dirName = dirNamePath.toString();
-
 					if (Files.isDirectory(dirPath.resolve("src/main/webapp"))) {
 						projectDirs.add(dirPath.toFile());
 
 						return FileVisitResult.SKIP_SUBTREE;
 					}
 
-					if (isExcludedDirName(dirName)) {
+					Path dirNamePath = dirPath.getFileName();
+
+					if (isExcludedDirName(dirNamePath.toString())) {
 						return FileVisitResult.SKIP_SUBTREE;
 					}
 
