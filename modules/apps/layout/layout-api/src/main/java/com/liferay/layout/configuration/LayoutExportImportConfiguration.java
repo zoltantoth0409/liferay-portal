e351a88bedf612e95e76e5686f1c7d8407d9f6bc
Pavel Savinov <pavel.savinov@liferay.com>
2020-06-15 15:02:44 +0200

    LPS-115387 Export draft layout by default keeping the option to disable it using the configuration file

--- modules/apps/layout/layout-api/src/main/java/com/liferay/layout/configuration/LayoutExportImportConfiguration.java
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/configuration/LayoutExportImportConfiguration.java
@@ -27,7 +27,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 )
 public interface LayoutExportImportConfiguration {
 
-	@Meta.AD(deflt = "false", required = false)
+	@Meta.AD(deflt = "true", required = false)
 	public boolean exportDraftLayout();
 
 }
