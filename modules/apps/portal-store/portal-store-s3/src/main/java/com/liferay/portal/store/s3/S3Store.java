81f0e6d535d23df59a03775696228720feec37a5
Eric Yan <eric.yan@liferay.com>
2020-06-15 11:59:26 -0700

    LPS-115509 portal-store-s3: Do clean-up before retrieving cache file

--- modules/apps/portal-store/portal-store-s3/src/main/java/com/liferay/portal/store/s3/S3Store.java
+++ modules/apps/portal-store/portal-store-s3/src/main/java/com/liferay/portal/store/s3/S3Store.java
@@ -161,12 +161,12 @@ public class S3Store implements Store {
 		throws PortalException {
 
 		try {
+			_s3FileCache.cleanUpCacheFiles();
+
 			S3Object s3Object = getS3Object(
 				companyId, repositoryId, fileName, versionLabel);
 
 			File file = _s3FileCache.getCacheFile(s3Object, fileName);
-
-			_s3FileCache.cleanUpCacheFiles();
 
 			return new FileInputStream(file);
 		}
