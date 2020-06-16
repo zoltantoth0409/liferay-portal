81a1298846a63a35b83652f9f63cce2b04688237
Laszlo Pap <laszlo.pap@liferay.com>
2020-06-16 19:18:21 +0200

    LPS-115597 Filter by Field is not working properly on Global Site page Template

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -6366,6 +6366,7 @@ public class PortalImpl implements Portal {
 		if (group.isLayoutPrototype() &&
 			layoutTypePortlet.hasPortletId(portletDisplay.getId()) &&
 			portletDisplay.isModeView() &&
+			!portletDisplay.isStatePopUp() &&
 			!portletName.equals(PortletKeys.NESTED_PORTLETS)) {
 
 			return true;
