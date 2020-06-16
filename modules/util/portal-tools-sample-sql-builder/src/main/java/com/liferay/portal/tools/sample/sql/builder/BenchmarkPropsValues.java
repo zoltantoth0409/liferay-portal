b6b0599250bda26a3ab675993b58e1390cb44450
shuyangzhou <shuyang.zhou@liferay.com>
2020-06-15 17:05:54 -0700

    LPS-104416 Merge into BenchmarkPropsValues

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarkPropsValues.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarkPropsValues.java
@@ -14,147 +14,224 @@
 
 package com.liferay.portal.tools.sample.sql.builder;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.FileReader;
+import java.io.Reader;
+
+import java.time.ZoneId;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.TimeZone;
 
 /**
  * @author Lily Chi
  */
 public class BenchmarkPropsValues {
 
+	public static final String ACTUAL_PROPERTIES_CONTENT =
+		PropertiesHolder._ACTUAL_PROPERTIES_CONTENT;
+
 	public static final DBType DB_TYPE = DBType.valueOf(
 		StringUtil.toUpperCase(
-			BenchmarkPropsUtil.get(BenchmarkPropsKeys.DB_TYPE)));
+			PropertiesHolder._get(BenchmarkPropsKeys.DB_TYPE)));
 
 	public static final int MAX_ASSET_CATEGORY_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_ASSET_CATEGORY_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_ASSET_CATEGORY_COUNT));
 
 	public static final int MAX_ASSET_ENTRY_TO_ASSET_CATEGORY_COUNT =
 		GetterUtil.getInteger(
-			BenchmarkPropsUtil.get(
+			PropertiesHolder._get(
 				BenchmarkPropsKeys.MAX_ASSET_ENTRY_TO_ASSET_CATEGORY_COUNT));
 
 	public static final int MAX_ASSET_ENTRY_TO_ASSET_TAG_COUNT =
 		GetterUtil.getInteger(
-			BenchmarkPropsUtil.get(
+			PropertiesHolder._get(
 				BenchmarkPropsKeys.MAX_ASSET_ENTRY_TO_ASSET_TAG_COUNT));
 
 	public static final int MAX_ASSET_TAG_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_ASSET_TAG_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_ASSET_TAG_COUNT));
 
 	public static final int MAX_ASSET_VUCABULARY_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_ASSET_VUCABULARY_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_ASSET_VUCABULARY_COUNT));
 
 	public static final int MAX_ASSETPUBLISHER_PAGE_COUNT =
 		GetterUtil.getInteger(
-			BenchmarkPropsUtil.get(
+			PropertiesHolder._get(
 				BenchmarkPropsKeys.MAX_ASSETPUBLISHER_PAGE_COUNT));
 
 	public static final int MAX_BLOGS_ENTRY_COMMENT_COUNT =
 		GetterUtil.getInteger(
-			BenchmarkPropsUtil.get(
+			PropertiesHolder._get(
 				BenchmarkPropsKeys.MAX_BLOGS_ENTRY_COMMENT_COUNT));
 
 	public static final int MAX_BLOGS_ENTRY_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_BLOGS_ENTRY_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_BLOGS_ENTRY_COUNT));
 
 	public static final int MAX_COMMERCE_PRODUCT_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_COMMERCE_PRODUCT_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_COMMERCE_PRODUCT_COUNT));
 
 	public static final int MAX_COMMERCE_PRODUCT_DEFINITION_COUNT =
 		GetterUtil.getInteger(
-			BenchmarkPropsUtil.get(
+			PropertiesHolder._get(
 				BenchmarkPropsKeys.MAX_COMMERCE_PRODUCT_DEFINITION_COUNT));
 
 	public static final int MAX_COMMERCE_PRODUCT_INSTANCE_COUNT =
 		GetterUtil.getInteger(
-			BenchmarkPropsUtil.get(
+			PropertiesHolder._get(
 				BenchmarkPropsKeys.MAX_COMMERCE_PRODUCT_INSTANCE_COUNT));
 
 	public static final int MAX_CONTENT_LAYOUT_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_CONTENT_LAYOUT_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_CONTENT_LAYOUT_COUNT));
 
 	public static final int MAX_DDL_CUSTOM_FIELD_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_DDL_CUSTOM_FIELD_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_DDL_CUSTOM_FIELD_COUNT));
 
 	public static final int MAX_DDL_RECORD_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_DDL_RECORD_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_DDL_RECORD_COUNT));
 
 	public static final int MAX_DDL_RECORD_SET_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_DDL_RECORD_SET_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_DDL_RECORD_SET_COUNT));
 
 	public static final int MAX_DL_FILE_ENTRY_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_DL_FILE_ENTRY_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_DL_FILE_ENTRY_COUNT));
 
 	public static final int MAX_DL_FILE_ENTRY_SIZE = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_DL_FILE_ENTRY_SIZE));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_DL_FILE_ENTRY_SIZE));
 
 	public static final int MAX_DL_FOLDER_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_DL_FOLDER_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_DL_FOLDER_COUNT));
 
 	public static final int MAX_DL_FOLDER_DEPTH = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_DL_FOLDER_DEPTH));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_DL_FOLDER_DEPTH));
 
 	public static final int MAX_GROUP_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_GROUP_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_GROUP_COUNT));
 
 	public static final int MAX_JOURNAL_ARTICLE_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_JOURNAL_ARTICLE_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_JOURNAL_ARTICLE_COUNT));
 
 	public static final int MAX_JOURNAL_ARTICLE_PAGE_COUNT =
 		GetterUtil.getInteger(
-			BenchmarkPropsUtil.get(
+			PropertiesHolder._get(
 				BenchmarkPropsKeys.MAX_JOURNAL_ARTICLE_PAGE_COUNT));
 
 	public static final int MAX_JOURNAL_ARTICLE_SIZE = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_JOURNAL_ARTICLE_SIZE));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_JOURNAL_ARTICLE_SIZE));
 
 	public static final int MAX_JOURNAL_ARTICLE_VERSION_COUNT =
 		GetterUtil.getInteger(
-			BenchmarkPropsUtil.get(
+			PropertiesHolder._get(
 				BenchmarkPropsKeys.MAX_JOURNAL_ARTICLE_VERSION_COUNT));
 
 	public static final int MAX_MB_CATEGORY_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_MB_CATEGORY_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_MB_CATEGORY_COUNT));
 
 	public static final int MAX_MB_MESSAGE_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_MB_MESSAGE_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_MB_MESSAGE_COUNT));
 
 	public static final int MAX_MB_THREAD_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_MB_THREAD_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_MB_THREAD_COUNT));
 
 	public static final int MAX_USER_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_USER_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_USER_COUNT));
 
 	public static final int MAX_USER_TO_GROUP_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_USER_TO_GROUP_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_USER_TO_GROUP_COUNT));
 
 	public static final int MAX_WIKI_NODE_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_WIKI_NODE_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_WIKI_NODE_COUNT));
 
 	public static final int MAX_WIKI_PAGE_COMMENT_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_WIKI_PAGE_COMMENT_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_WIKI_PAGE_COMMENT_COUNT));
 
 	public static final int MAX_WIKI_PAGE_COUNT = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.MAX_WIKI_PAGE_COUNT));
+		PropertiesHolder._get(BenchmarkPropsKeys.MAX_WIKI_PAGE_COUNT));
 
 	public static final int OPTIMIZE_BUFFER_SIZE = GetterUtil.getInteger(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.OPTIMIZE_BUFFER_SIZE));
+		PropertiesHolder._get(BenchmarkPropsKeys.OPTIMIZE_BUFFER_SIZE));
 
 	public static final String[] OUTPUT_CSV_FILE_NAMES = StringUtil.split(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.OUTPUT_CSV_FILE_NAMES));
+		PropertiesHolder._get(BenchmarkPropsKeys.OUTPUT_CSV_FILE_NAMES));
 
-	public static final String OUTPUT_DIR = BenchmarkPropsUtil.get(
+	public static final String OUTPUT_DIR = PropertiesHolder._get(
 		BenchmarkPropsKeys.OUTPUT_DIR);
 
 	public static final boolean OUTPUT_MERGE = GetterUtil.getBoolean(
-		BenchmarkPropsUtil.get(BenchmarkPropsKeys.OUTPUT_MERGE));
+		PropertiesHolder._get(BenchmarkPropsKeys.OUTPUT_MERGE));
 
-	public static final String SCRIPT = BenchmarkPropsUtil.get(
+	public static final String SCRIPT = PropertiesHolder._get(
 		BenchmarkPropsKeys.SCRIPT);
 
-	public static final String VIRTUAL_HOST_NAME = BenchmarkPropsUtil.get(
+	public static final String VIRTUAL_HOST_NAME = PropertiesHolder._get(
 		BenchmarkPropsKeys.VIRTUAL_HOST_NAME);
+
+	private static class PropertiesHolder {
+
+		private static String _get(String key) {
+			return _properties.getProperty(key);
+		}
+
+		private static final String _ACTUAL_PROPERTIES_CONTENT;
+
+		private static final Properties _properties;
+
+		static {
+			Properties properties = new Properties();
+
+			try (Reader reader = new FileReader(
+					System.getProperty("sample-sql-properties"))) {
+
+				properties.load(reader);
+
+				String timeZoneId = properties.getProperty(
+					"sample.sql.db.time.zone");
+
+				if (Validator.isNull(timeZoneId)) {
+					TimeZone timeZone = TimeZone.getDefault();
+
+					properties.setProperty(
+						"sample.sql.db.time.zone", timeZone.getID());
+				}
+				else {
+					TimeZone.setDefault(
+						TimeZone.getTimeZone(ZoneId.of(timeZoneId)));
+				}
+			}
+			catch (Exception exception) {
+				throw new ExceptionInInitializerError(exception);
+			}
+
+			List<String> propertyNames = new ArrayList<>(
+				properties.stringPropertyNames());
+
+			propertyNames.sort(null);
+
+			StringBundler sb = new StringBundler(propertyNames.size() * 4);
+
+			for (String propertyName : propertyNames) {
+				if (!propertyName.startsWith("sample.sql")) {
+					continue;
+				}
+
+				sb.append(propertyName);
+				sb.append(StringPool.EQUAL);
+				sb.append(properties.getProperty(propertyName));
+				sb.append(StringPool.NEW_LINE);
+			}
+
+			_ACTUAL_PROPERTIES_CONTENT = sb.toString();
+
+			_properties = properties;
+		}
+
+	}
 
 }
--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilder.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/SampleSQLBuilder.java
@@ -116,7 +116,7 @@ public class SampleSQLBuilder {
 			new File(
 				BenchmarkPropsValues.OUTPUT_DIR,
 				"benchmarks-actual.properties"),
-			BenchmarkPropsUtil.ACTUAL_PROPERTIES_CONTENT);
+			BenchmarkPropsValues.ACTUAL_PROPERTIES_CONTENT);
 	}
 
 	protected void compressSQL(
