46ebde424d5db9c7dca1eb36fe8eb4b0a1d86eb2
Sarai Díaz García <dgarciasarai@gmail.com>
2020-06-16 18:26:27 +0200

    LPS-115242 Calculate bars and data with useMemo

--- modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/AuditGraphApp.js
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/AuditGraphApp.js
@@ -12,71 +12,83 @@
  * details.
  */
 
-import React from 'react';
+import React, {useMemo} from 'react';
 
 import AuditBarChart from './components/AuditBarChart';
 import EmptyAuditBarChart from './components/EmptyAuditBarChart';
 
-const categories = [
-	{
-		children: [
-			{key: 'education', name: 'Education', value: 478},
-			{key: 'selection', name: 'Selection', value: 1055},
-			{key: 'selection', name: 'Selection', value: 822},
-		],
-		key: 'business-decision-maker',
-		name: 'Business Decision Maker',
-	},
-	{
-		children: [
-			{key: 'education', name: 'Education', value: 125},
-			{key: 'selection', name: 'Selection', value: 1906},
-			{key: 'solution', name: 'Solution', value: 987},
-		],
-		key: 'business-end-user',
-		name: 'Business End User',
-	},
-	{
-		children: [
-			{key: 'education', name: 'Education', value: 444},
-			{key: 'selection', name: 'Selection', value: 1733},
-			{key: 'solution', name: 'Solution', value: 1807},
-		],
-		key: 'technical-decision-maker',
-		name: 'Technical Decision Maker',
-	},
-	{
-		children: [
-			{key: 'education', name: 'Education', value: 125},
-			{key: 'selection', name: 'Selection', value: 317},
-			{key: 'solution', name: 'Solution', value: 187},
-		],
-		key: 'technical-end-user',
-		name: 'Technical End User',
-	},
-];
-
-const rtl = false;
-
 export default function () {
-	const bars = [];
-	const data = [];
+	const categories = [
+		{
+			children: [
+				{key: 'education', name: 'Education', value: 478},
+				{key: 'selection', name: 'Selection', value: 1055},
+				{key: 'selection', name: 'Selection', value: 822},
+			],
+			key: 'business-decision-maker',
+			name: 'Business Decision Maker',
+		},
+		{
+			children: [
+				{key: 'education', name: 'Education', value: 125},
+				{key: 'selection', name: 'Selection', value: 1906},
+				{key: 'solution', name: 'Solution', value: 987},
+			],
+			key: 'business-end-user',
+			name: 'Business End User',
+		},
+		{
+			children: [
+				{key: 'education', name: 'Education', value: 444},
+				{key: 'selection', name: 'Selection', value: 1733},
+				{key: 'solution', name: 'Solution', value: 1807},
+			],
+			key: 'technical-decision-maker',
+			name: 'Technical Decision Maker',
+		},
+		{
+			children: [
+				{key: 'education', name: 'Education', value: 125},
+				{key: 'selection', name: 'Selection', value: 317},
+				{key: 'solution', name: 'Solution', value: 187},
+			],
+			key: 'technical-end-user',
+			name: 'Technical End User',
+		},
+	];
 
-	categories.map((category) => {
-		var dataChild = {name: category.name};
+	const rtl = false;
 
-		const children = category.children;
+	const auditBarChartData = useMemo(() => {
+		const bars = [];
+		const data = [];
 
-		for (var i = 0; i < children.length; i++) {
-			const barChild = {dataKey: children[i].key, name: children[i].name};
-			if (!bars.some((bar) => bar.dataKey === children[i].key)) {
-				bars.push(barChild);
+		categories.map((category) => {
+			var dataChild = {name: category.name};
+
+			const children = category.children;
+
+			for (var i = 0; i < children.length; i++) {
+				const barChild = {
+					dataKey: children[i].key,
+					name: children[i].name,
+				};
+				if (!bars.some((bar) => bar.dataKey === children[i].key)) {
+					bars.push(barChild);
+				}
+
+				dataChild = {
+					...dataChild,
+					[children[i].key]: children[i].value,
+				};
 			}
+			data.push(dataChild);
+		});
 
-			dataChild = {...dataChild, [children[i].key]: children[i].value};
-		}
-		data.push(dataChild);
-	});
+		return {bars, data};
+	}, [categories]);
+
+	const {bars, data} = auditBarChartData;
 
 	return data.length ? (
 		<AuditBarChart bars={bars} data={data} rtl={rtl} />
