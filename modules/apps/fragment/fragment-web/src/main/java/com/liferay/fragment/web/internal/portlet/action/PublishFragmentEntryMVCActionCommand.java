41b76173220585b3c5c8e2229c690a3c6bf7a2d1
Rubén Pulido <ruben.pulido@liferay.com>
2020-06-17 01:39:42 +0200

    LPS-114252 Add PublishFragmentEntryMVCActionCommand

---
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/action/PublishFragmentEntryMVCActionCommand.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.web.internal.portlet.action;
+
+import com.liferay.fragment.constants.FragmentPortletKeys;
+import com.liferay.fragment.exception.NoSuchEntryException;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.service.FragmentEntryService;
+import com.liferay.fragment.web.internal.handler.FragmentEntryExceptionRequestHandler;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rubén Pulido
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + FragmentPortletKeys.FRAGMENT,
+		"mvc.command.name=/fragment/publish_fragment_entry"
+	},
+	service = AopService.class
+)
+public class PublishFragmentEntryMVCActionCommand
+	extends BaseMVCActionCommand implements AopService, MVCActionCommand {
+
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long fragmentEntryId = ParamUtil.getLong(
+			actionRequest, "fragmentEntryId");
+
+		try {
+			FragmentEntry draftFragmentEntry = null;
+
+			FragmentEntry fragmentEntry =
+				_fragmentEntryService.fetchFragmentEntry(fragmentEntryId);
+
+			if (fragmentEntry.isDraft()) {
+				draftFragmentEntry = fragmentEntry;
+			}
+			else {
+				draftFragmentEntry = _fragmentEntryService.fetchDraft(
+					fragmentEntryId);
+			}
+
+			if (draftFragmentEntry == null) {
+				throw new NoSuchEntryException();
+			}
+
+			draftFragmentEntry.setStatus(WorkflowConstants.STATUS_APPROVED);
+
+			FragmentEntry publishedFragmentEntry =
+				_fragmentEntryService.publishDraft(draftFragmentEntry);
+
+			JSONObject jsonObject = JSONUtil.put(
+				"redirectURL",
+				getRedirectURL(actionResponse, publishedFragmentEntry));
+
+			JSONPortletResponseUtil.writeJSON(
+				actionRequest, actionResponse, jsonObject);
+		}
+		catch (PortalException portalException) {
+			_fragmentEntryExceptionRequestHandler.handlePortalException(
+				actionRequest, actionResponse, portalException);
+		}
+	}
+
+	protected String getRedirectURL(
+		ActionResponse actionResponse, FragmentEntry fragmentEntry) {
+
+		LiferayPortletResponse liferayPortletResponse =
+			_portal.getLiferayPortletResponse(actionResponse);
+
+		PortletURL portletURL = liferayPortletResponse.createRenderURL();
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "/fragment/view_fragment_entries");
+		portletURL.setParameter(
+			"fragmentCollectionId",
+			String.valueOf(fragmentEntry.getFragmentCollectionId()));
+
+		return portletURL.toString();
+	}
+
+	@Reference
+	private FragmentEntryExceptionRequestHandler
+		_fragmentEntryExceptionRequestHandler;
+
+	@Reference
+	private FragmentEntryService _fragmentEntryService;
+
+	@Reference
+	private Portal _portal;
+
+}
