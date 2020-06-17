9f730113769954c57b191957aed4d2061fb11d6f
Preston-Crary <preston.crary@liferay.com>
2020-06-17 16:28:34 -0700

    LPS-115696 Remove the column and upgrade.

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/upgrade/MBServiceUpgrade.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/upgrade/MBServiceUpgrade.java
@@ -27,6 +27,7 @@ import com.liferay.message.boards.internal.upgrade.v2_0_0.util.MBThreadFlagTable
 import com.liferay.message.boards.internal.upgrade.v2_0_0.util.MBThreadTable;
 import com.liferay.message.boards.internal.upgrade.v3_0_0.UpgradeMBMessageTreePath;
 import com.liferay.message.boards.internal.upgrade.v3_1_0.UpgradeUrlSubject;
+import com.liferay.message.boards.internal.upgrade.v4_0_0.UpgradeMBCategoryThreadCount;
 import com.liferay.message.boards.model.MBThread;
 import com.liferay.portal.kernel.service.ResourceActionLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
@@ -74,6 +75,8 @@ public class MBServiceUpgrade implements UpgradeStepRegistrator {
 			new UpgradeMBMessageTreePath());
 
 		registry.register("3.0.0", "3.1.0", new UpgradeUrlSubject());
+
+		registry.register("3.1.0", "4.0.0", new UpgradeMBCategoryThreadCount());
 	}
 
 	@Reference
