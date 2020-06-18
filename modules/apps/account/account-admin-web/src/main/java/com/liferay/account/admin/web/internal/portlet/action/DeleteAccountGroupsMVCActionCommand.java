38724a3d725ae16e4107e1e0d4fa51d1fdcd20bd
alee8888 <albert.lee@liferay.com>
2020-06-17 18:01:07 -0700

    LPS-115250 Create MVC action command to delete an Account Group

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/DeleteAccountGroupsMVCActionCommand.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.portlet.action;
+
+import com.liferay.account.constants.AccountPortletKeys;
+import com.liferay.account.service.AccountGroupLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Albert Lee
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AccountPortletKeys.ACCOUNT_GROUPS_ADMIN,
+		"mvc.command.name=/account_groups_admin/delete_account_groups"
+	},
+	service = MVCActionCommand.class
+)
+public class DeleteAccountGroupsMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long[] accountGroupIds = ParamUtil.getLongValues(
+			actionRequest, "accountGroupIds");
+
+		for (long accountGroupId : accountGroupIds) {
+			_accountGroupLocalService.deleteAccountGroup(accountGroupId);
+		}
+
+		String redirect = ParamUtil.getString(actionRequest, "redirect");
+
+		if (Validator.isNotNull(redirect)) {
+			sendRedirect(actionRequest, actionResponse, redirect);
+		}
+	}
+
+	@Reference
+	private AccountGroupLocalService _accountGroupLocalService;
+
+}
