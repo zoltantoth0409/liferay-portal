b7460b85729f8b27366cb49e396537881cfb9fdd
Rubén Pulido <ruben.pulido@liferay.com>
2020-06-18 17:08:29 +0200

    LPS-114351 Import fragment as draft if its configuration is invalid

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/importer/FragmentsImporterImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/importer/FragmentsImporterImpl.java
@@ -21,6 +21,7 @@ import com.liferay.fragment.exception.DuplicateFragmentCollectionKeyException;
 import com.liferay.fragment.exception.DuplicateFragmentCompositionKeyException;
 import com.liferay.fragment.exception.DuplicateFragmentEntryKeyException;
 import com.liferay.fragment.exception.FragmentCollectionNameException;
+import com.liferay.fragment.exception.FragmentEntryConfigurationException;
 import com.liferay.fragment.importer.FragmentsImporter;
 import com.liferay.fragment.importer.FragmentsImporterResultEntry;
 import com.liferay.fragment.model.FragmentCollection;
@@ -270,6 +271,23 @@ public class FragmentsImporterImpl implements FragmentsImporter {
 
 			status = WorkflowConstants.STATUS_DRAFT;
 			errorMessage = portalException.getLocalizedMessage();
+		}
+
+		try {
+			_fragmentEntryValidator.validateConfiguration(configuration);
+		}
+		catch (FragmentEntryConfigurationException
+					fragmentEntryConfigurationException) {
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					fragmentEntryConfigurationException,
+					fragmentEntryConfigurationException);
+			}
+
+			status = WorkflowConstants.STATUS_DRAFT;
+			errorMessage =
+				fragmentEntryConfigurationException.getLocalizedMessage();
 		}
 
 		int type = FragmentConstants.getTypeFromLabel(
