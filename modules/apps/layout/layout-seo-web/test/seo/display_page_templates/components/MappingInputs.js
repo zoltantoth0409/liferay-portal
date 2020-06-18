ec0cb6d8a690e7b3f1d3a081805f92eb73134eb3
Jorge González <jorge.gonzalez@liferay.com>
2020-06-18 13:39:53 +0200

    LPS-115456 Add first test for MappingInputs

---
+++ modules/apps/layout/layout-seo-web/test/seo/display_page_templates/components/MappingInputs.js
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+
+import MappingInputs from '../../../../src/main/resources/META-INF/resources/js/seo/display_page_templates/components/MappingInputs';
+
+const baseProps = {
+	fields: [
+		{key: 'field-1', label: 'Field 1', type: 'text'},
+		{key: 'field-2', label: 'Field 2', type: 'text'},
+		{key: 'field-3', label: 'Field 3', type: 'image'},
+		{key: 'field-4', label: 'Field 4', type: 'text'},
+		{key: 'field-5', label: 'Field 5', type: 'text'},
+	],
+	inputs: [
+		{
+			fieldType: 'text',
+			helpMessage: 'help message for input title',
+			label: 'Title',
+			name: 'title',
+			selectedFieldKey: 'field-5',
+		},
+		{
+			fieldType: 'image',
+			helpMessage: 'help message for input image',
+			label: 'Image for social share',
+			name: 'image',
+		},
+	],
+	selectedSource: {
+		classTypeLabel: 'Label source type',
+	},
+};
+
+const renderComponent = (props) =>
+	render(<MappingInputs {...baseProps} {...props} />);
+
+describe('MappingInputs', () => {
+	afterEach(cleanup);
+
+	describe('when rendered', () => {
+		let result;
+		let feedbackTitleInput;
+		let feedbackImageInput;
+
+		beforeEach(() => {
+			result = renderComponent();
+			feedbackTitleInput = result.getByLabelText(
+				baseProps.inputs[0].label
+			);
+			feedbackImageInput = result.getByLabelText(
+				baseProps.inputs[1].label
+			);
+		});
+
+		it('has two read only inputs for user feedback with the selected field names', () => {
+			expect(feedbackTitleInput).toBeInTheDocument();
+			expect(feedbackImageInput).toBeInTheDocument();
+		});
+	});
+});
