ec5712a308b9f328a2498fa4c17390961c1b5cd5
Javier de Arcos <javier.dearcos@liferay.com>
2020-06-19 07:32:13 +0200

    LPS-105232 Create REST Builder Project Template

---
+++ modules/sdk/project-templates/project-templates-rest-builder/src/main/java/com/liferay/project/templates/rest/builder/internal/RestBuilderProjectTemplateCustomizer.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.project.templates.rest.builder.internal;
+
+import com.liferay.project.templates.extensions.ProjectTemplateCustomizer;
+import com.liferay.project.templates.extensions.ProjectTemplatesArgs;
+import com.liferay.project.templates.extensions.util.WorkspaceUtil;
+
+import java.io.File;
+
+import java.nio.file.Path;
+
+import java.util.Properties;
+
+import org.apache.maven.archetype.ArchetypeGenerationRequest;
+import org.apache.maven.archetype.ArchetypeGenerationResult;
+
+/**
+ * @author Javier de Arcos
+ */
+public class RestBuilderProjectTemplateCustomizer
+	implements ProjectTemplateCustomizer {
+
+	@Override
+	public String getTemplateName() {
+		return "rest-builder";
+	}
+
+	@Override
+	public void onAfterGenerateProject(
+		ProjectTemplatesArgs projectTemplatesArgs, File destinationDir,
+		ArchetypeGenerationResult archetypeGenerationResult) {
+	}
+
+	@Override
+	public void onBeforeGenerateProject(
+			ProjectTemplatesArgs projectTemplatesArgs,
+			ArchetypeGenerationRequest archetypeGenerationRequest)
+		throws Exception {
+
+		String artifactId = archetypeGenerationRequest.getArtifactId();
+
+		String apiPath = ":" + artifactId + "-api";
+
+		String clientPath = ":" + artifactId + "-client";
+
+		File destinationDir = new File(
+			archetypeGenerationRequest.getOutputDirectory());
+
+		File workspaceDir = WorkspaceUtil.getWorkspaceDir(destinationDir);
+
+		if (workspaceDir != null) {
+			Path destinationDirPath = destinationDir.toPath();
+			Path workspaceDirPath = workspaceDir.toPath();
+
+			destinationDirPath = destinationDirPath.toAbsolutePath();
+			workspaceDirPath = workspaceDirPath.toAbsolutePath();
+
+			String relativePath = String.valueOf(
+				workspaceDirPath.relativize(destinationDirPath));
+
+			relativePath = relativePath.replace(File.separatorChar, ':');
+
+			apiPath = ":" + relativePath + ":" + artifactId + apiPath;
+
+			clientPath = ":" + relativePath + ":" + artifactId + clientPath;
+		}
+
+		Properties properties = archetypeGenerationRequest.getProperties();
+
+		setProperty(properties, "apiPath", apiPath);
+		setProperty(properties, "clientPath", clientPath);
+	}
+
+}
