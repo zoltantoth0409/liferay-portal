118b65c49cdc0438a88f8ab85aa0d07d28c9834e
yichenroy <yi-chen.tsai@liferay.com>
2020-06-19 10:36:30 -0700

    LRCI-1434 Implement logic to split & write partitioned files

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestrayResultsParserUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestrayResultsParserUtil.java
@@ -14,18 +14,52 @@
 
 package com.liferay.jenkins.results.parser;
 
+import com.google.common.collect.Lists;
+
 import java.io.File;
+
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
 
 /**
  * @author Yi-Chen Tsai
  */
 public class TestrayResultsParserUtil {
 
-	public static void processTestrayResultFile(File file) {
+	public static void processTestrayResultFile(File file) throws Exception {
+		Document document = Dom4JUtil.parse(
+			JenkinsResultsParserUtil.read(file));
 
+		Element rootElement = document.getRootElement();
+
+		Element commonRootElement = _getCommonRootElement(rootElement);
+
+		int partitionID = 0;
+
+		List<Element> testcaseElements = rootElement.elements("testcase");
+
+		for (List<Element> testcaseElementsPartition :
+				Lists.partition(testcaseElements, _COUNT_MAX_TESTCASE)) {
+
+			Element partitionRootElement = commonRootElement.createCopy();
+
+			Dom4JUtil.addToElement(
+				partitionRootElement, testcaseElementsPartition.toArray());
+
+			Document partitionDocument = Dom4JUtil.parse(
+				Dom4JUtil.format(partitionRootElement));
+
+			JenkinsResultsParserUtil.write(
+				_getPartitionFilePath(file, partitionID),
+				partitionDocument.asXML());
+
+			partitionID++;
+		}
 	}
 
-	public static void processTestrayResultFiles(File dir) {
+	public static void processTestrayResultFiles(File dir) throws Exception {
 		for (File file : dir.listFiles()) {
 			if (file.isDirectory()) {
 				continue;
@@ -43,6 +77,32 @@ public class TestrayResultsParserUtil {
 		}
 	}
 
+	private static Element _getCommonRootElement(Element rootElement) {
+		Element commonRootElement = rootElement.createCopy();
+
+		commonRootElement.clearContent();
+
+		Element environmentsElement = rootElement.element("environments");
+		Element propertiesElement = rootElement.element("properties");
+
+		Dom4JUtil.addToElement(
+			commonRootElement, environmentsElement.createCopy(),
+			propertiesElement.createCopy());
+
+		return commonRootElement;
+	}
+
+	private static String _getPartitionFilePath(File file, int partitionID) {
+		String filePath = file.getAbsolutePath();
+
+		String partitionPredicate = JenkinsResultsParserUtil.combine(
+			"_partition_", String.valueOf(partitionID),
+			_EXTENSION_TESTRAY_RESULT_FILE);
+
+		return filePath.replace(
+			_EXTENSION_TESTRAY_RESULT_FILE, partitionPredicate);
+	}
+	
 	private static final long _BYTES_MAX_SIZE_TESTRAY_RESULT_FILE = 1024 * 200;
 
 	private static final int _COUNT_MAX_TESTCASE = 100;
