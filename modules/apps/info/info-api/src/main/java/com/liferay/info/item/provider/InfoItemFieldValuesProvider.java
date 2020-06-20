cf6ed38596f470c2e5056f5ba3f55418972570e2
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-06-20 18:28:49 +0200

    LPS-115840 Create new provider to obtain the InfoItemFieldValues for a given item object
    This provider will allow simplifying InfoItemFormProvider by removing the methods related to retrieving values

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/item/provider/InfoItemFieldValuesProvider.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.item.provider;
+
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.item.InfoItemFieldValues;
+import com.liferay.info.item.NoSuchClassTypeException;
+
+/**
+ * @author Jorge Ferrer
+ */
+public interface InfoItemFieldValuesProvider<T> {
+
+	public default InfoFieldValue<Object> getInfoItemFieldValue(
+		T t, String fieldName) {
+
+		InfoItemFieldValues infoItemFieldValues = getInfoItemFieldValues(t);
+
+		return infoItemFieldValues.getInfoFieldValue(fieldName);
+	}
+
+	public InfoItemFieldValues getInfoItemFieldValues(T t);
+
+}
