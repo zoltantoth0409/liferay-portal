349b57d8a2ffef1d1035f7832846bc9bf43838c0
Jonathan McCann <jonathan.mccann@liferay.com>
2020-06-23 09:00:44 -0700

    LPS-116033 Delegate the export to BaseLDAPExportModelListener so that any errors encountered in LDAP will cause the password update to fail and keep the database and LDAP in sync

--- modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/model/listener/UserModelListener.java
+++ modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/model/listener/UserModelListener.java
@@ -14,30 +14,23 @@
 
 package com.liferay.portal.security.ldap.internal.model.listener;
 
-import com.liferay.petra.lang.CentralizedThreadLocal;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.MembershipRequest;
 import com.liferay.portal.kernel.model.MembershipRequestConstants;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.security.auth.PasswordModificationThreadLocal;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.ldap.LDAPSettings;
 import com.liferay.portal.kernel.service.MembershipRequestLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.security.exportimport.UserExporter;
 import com.liferay.portal.security.ldap.internal.UserImportTransactionThreadLocal;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.Callable;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -105,50 +98,8 @@ public class UserModelListener extends BaseLDAPExportModelListener<User> {
 			user.getOriginalEmailAddress());
 	}
 
-	protected void exportToLDAP(final User user) {
-		if (user.isDefaultUser() ||
-			UserImportTransactionThreadLocal.isOriginatesFromImport()) {
-
-			return;
-		}
-
-		Callable<Void> callable = CallableUtil.getCallable(
-			expandoBridgeAttributes -> {
-				boolean oldPasswordModified =
-					PasswordModificationThreadLocal.isPasswordModified();
-
-				if (oldPasswordModified) {
-					String lastPasswordUnencrypted =
-						_lastPasswordUnencrypted.get();
-					String newPasswordUnencrypted =
-						PasswordModificationThreadLocal.
-							getPasswordUnencrypted();
-
-					boolean newPasswordModified = !Objects.equals(
-						lastPasswordUnencrypted, newPasswordUnencrypted);
-
-					_lastPasswordUnencrypted.set(newPasswordUnencrypted);
-
-					PasswordModificationThreadLocal.setPasswordModified(
-						newPasswordModified);
-				}
-
-				try {
-					_userExporter.exportUser(user, expandoBridgeAttributes);
-				}
-				catch (Exception exception) {
-					_log.error(
-						"Unable to export user with user ID " +
-							user.getUserId() + " to LDAP on after create",
-						exception);
-				}
-				finally {
-					PasswordModificationThreadLocal.setPasswordModified(
-						oldPasswordModified);
-				}
-			});
-
-		TransactionCommitCallbackUtil.registerCallback(callable);
+	protected void exportToLDAP(final User user) throws Exception {
+		exportToLDAP(user, _userExporter, _ldapSettings);
 	}
 
 	protected void updateMembershipRequestStatus(long userId, long groupId)
@@ -172,13 +123,6 @@ public class UserModelListener extends BaseLDAPExportModelListener<User> {
 				new ServiceContext());
 		}
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		UserModelListener.class);
-
-	private static final CentralizedThreadLocal<String>
-		_lastPasswordUnencrypted = new CentralizedThreadLocal<>(
-			UserModelListener.class.getName() + "._lastPasswordUnencrypted");
 
 	@Reference(
 		policy = ReferencePolicy.DYNAMIC,
