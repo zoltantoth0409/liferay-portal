65e53dc1592c5e6ed850edcd5e9f0edbfb9cbcc4
Luismi Barcos <luismiguel.barcos@liferay.com>
2020-06-23 19:45:40 +0200

    LPS-115395 Add test for list tags in Tags component

---
+++ modules/apps/questions/questions-web/test/js/pages/tags/Tags.es.js
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {cleanup} from '@testing-library/react';
+import React from 'react';
+
+import Tags from '../../../../src/main/resources/META-INF/resources/js/pages/tags/Tags.es';
+import {getTagsQuery} from '../../../../src/main/resources/META-INF/resources/js/utils/client.es';
+import {renderComponent} from '../../../helpers.es';
+
+import '@testing-library/jest-dom/extend-expect';
+
+const mockRequestVariables = {
+	page: 1,
+	pageSize: 20,
+	search: '',
+	siteKey: '20020',
+};
+
+const mocks = [
+	{
+		request: {
+			query: getTagsQuery,
+			variables: mockRequestVariables,
+		},
+		result: {
+			data: {
+				keywordsRanked: {
+					items: [
+						{
+							id: 37018,
+							keywordUsageCount: 1,
+							name: 'new',
+						},
+						{
+							id: 37019,
+							keywordUsageCount: 1,
+							name: 'osgi',
+						},
+						{
+							id: 37020,
+							keywordUsageCount: 1,
+							name: 'tag',
+						},
+					],
+					lastPage: 1,
+					page: 1,
+					pageSize: 20,
+					totalCount: 3,
+				},
+			},
+		},
+	},
+];
+
+describe('Tags', () => {
+	afterEach(cleanup);
+
+	it('Shows list of tags', async () => {
+		const {findByText} = renderComponent({
+			apolloMocks: mocks,
+			contextValue: {siteKey: mockRequestVariables.siteKey},
+			ui: <Tags />,
+		});
+
+		const firstTag = await findByText('new');
+		const secondTag = await findByText('osgi');
+		const thirdTag = await findByText('tag');
+
+		expect(firstTag).toBeInTheDocument();
+		expect(secondTag).toBeInTheDocument();
+		expect(thirdTag).toBeInTheDocument();
+	});
+});
