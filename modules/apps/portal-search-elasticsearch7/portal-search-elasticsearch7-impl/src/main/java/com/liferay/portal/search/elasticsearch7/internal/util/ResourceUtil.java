780000d2079162fe33a638480b84a2ef171c1486
Bryan Engler <bryan.engler@liferay.com>
2020-06-23 13:53:31 -0700

    LPS-106093 portal-search-elasticsearch7-impl: Delete embedded mode classes
    https://issues.liferay.com/browse/LPS-106093

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/util/ResourceUtil.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/util/ResourceUtil.java
@@ -14,14 +14,10 @@
 
 package com.liferay.portal.search.elasticsearch7.internal.util;
 
-import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.util.StringUtil;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-
-import org.apache.commons.io.FileUtils;
 
 /**
  * @author Michael C. Han
@@ -40,23 +36,6 @@ public class ResourceUtil {
 			throw new RuntimeException(
 				"Unable to load resource: " + resourceName, ioException);
 		}
-	}
-
-	public static File getResourceAsTempFile(Class<?> clazz, String name)
-		throws IOException {
-
-		int index = name.lastIndexOf(CharPool.PERIOD);
-
-		File file = File.createTempFile(
-			name.substring(0, index), name.substring(index));
-
-		file.deleteOnExit();
-
-		try (InputStream inputStream = clazz.getResourceAsStream(name)) {
-			FileUtils.copyInputStreamToFile(inputStream, file);
-		}
-
-		return file;
 	}
 
 }
