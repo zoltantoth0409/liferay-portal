03e6f2e75df585d0258ba1f4e52a11c73a5cd4b9
Brian Chan <brian.chan@liferay.com>
2020-06-23 10:11:21 -0700

    LPS-115691 Wordsmith

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/ShareFormInstanceMVCActionCommand.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/ShareFormInstanceMVCActionCommand.java
@@ -61,7 +61,7 @@ public class ShareFormInstanceMVCActionCommand extends BaseMVCActionCommand {
 				"successMessage",
 				LanguageUtil.get(
 					_portal.getHttpServletRequest(actionRequest),
-					"form-shared-via-email"));
+					"form-was-shared-via-email"));
 
 			JSONPortletResponseUtil.writeJSON(
 				actionRequest, actionResponse, jsonObject);
