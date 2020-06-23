f09e10ea6411f6361eedb6ea623fe185e785796b
Bryan Engler <bryan.engler@liferay.com>
2020-06-23 00:14:20 -0700

    LPS-112607 portal-search-elasticsearch7-impl: Use ActivationHandler for adding/removing multiple connections
    https://issues.liferay.com/browse/LPS-112607

---
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/ElasticsearchConnectionConfigurationActivationHandler.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.elasticsearch7.internal.connection;
+
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.search.elasticsearch7.configuration.ElasticsearchConnectionConfiguration;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Bryan Engler
+ */
+@Component(
+	configurationPid = "com.liferay.portal.search.elasticsearch7.configuration.ElasticsearchConnectionConfiguration",
+	service = {}
+)
+public class ElasticsearchConnectionConfigurationActivationHandler {
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		ElasticsearchConnectionConfiguration
+			elasticsearchConnectionConfiguration =
+				ConfigurableUtil.createConfigurable(
+					ElasticsearchConnectionConfiguration.class, properties);
+
+		ElasticsearchConnectionBuilder elasticsearchConnectionBuilder =
+			new ElasticsearchConnectionBuilder();
+
+		elasticsearchConnectionBuilder.active(
+			elasticsearchConnectionConfiguration.active()
+		).authenticationEnabled(
+			elasticsearchConnectionConfiguration.authenticationEnabled()
+		).connectionId(
+			elasticsearchConnectionConfiguration.connectionId()
+		).httpSSLEnabled(
+			elasticsearchConnectionConfiguration.httpSSLEnabled()
+		).networkHostAddresses(
+			elasticsearchConnectionConfiguration.networkHostAddresses()
+		).password(
+			elasticsearchConnectionConfiguration.password()
+		).truststorePassword(
+			elasticsearchConnectionConfiguration.truststorePassword()
+		).truststorePath(
+			elasticsearchConnectionConfiguration.truststorePath()
+		).truststoreType(
+			elasticsearchConnectionConfiguration.truststoreType()
+		).userName(
+			elasticsearchConnectionConfiguration.username()
+		);
+
+		elasticsearchConnectionManager.addElasticsearchConnection(
+			elasticsearchConnectionBuilder.build());
+	}
+
+	@Deactivate
+	protected void deactivate(Map<String, Object> properties) {
+		ElasticsearchConnectionConfiguration
+			elasticsearchConnectionConfiguration =
+				ConfigurableUtil.createConfigurable(
+					ElasticsearchConnectionConfiguration.class, properties);
+
+		elasticsearchConnectionManager.removeElasticsearchConnection(
+			elasticsearchConnectionConfiguration.connectionId());
+	}
+
+	@Reference
+	protected ElasticsearchConnectionManager elasticsearchConnectionManager;
+
+}
