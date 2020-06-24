f61fa0c73338838ff5b0963aa456a81565d31954
pei-jung <pei-jung.lan@liferay.com>
2020-06-23 18:11:58 -0700

    LPS-112519 Add configuration for email domain validation

---
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/configuration/AccountEntryEmailDomainsConfiguration.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@ExtendedObjectClassDefinition(
+	category = "accounts", scope = ExtendedObjectClassDefinition.Scope.COMPANY
+)
+@Meta.OCD(
+	id = "com.liferay.account.configuration.AccountEntryEmailDomainsConfiguration",
+	localization = "content/Language",
+	name = "account-entry-email-domains-configuration-name"
+)
+public interface AccountEntryEmailDomainsConfiguration {
+
+	@Meta.AD(
+		deflt = "false", description = "enable-email-domain-validation-help",
+		name = "enable-email-domain-validation", required = false
+	)
+	public boolean enableEmailDomainValidation();
+
+}
