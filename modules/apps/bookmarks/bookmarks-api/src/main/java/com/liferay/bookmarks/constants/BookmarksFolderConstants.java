b8e7b2d55c923a3cfdead672f9969093d3c920d9
Hugo Huijser <hugo.huijser@liferay.com>
2020-06-24 15:48:00 +0800

    LPS-115716 Deprecate class, add new class in  *.constants

--- modules/apps/bookmarks/bookmarks-api/src/main/java/com/liferay/bookmarks/constants/BookmarksFolderConstants.java
+++ modules/apps/bookmarks/bookmarks-api/src/main/java/com/liferay/bookmarks/constants/BookmarksFolderConstants.java
@@ -12,16 +12,13 @@
  * details.
  */
 
-package com.liferay.bookmarks.model;
+package com.liferay.bookmarks.constants;
 
 import com.liferay.petra.string.StringPool;
 
 /**
- * @author     Alexander Chow
- * @deprecated As of Athanasius (7.3.x), replaced by {@link
- *             com.liferay.bookmarks.constants.BookmarksFolderConstants}
+ * @author Alexander Chow
  */
-@Deprecated
 public class BookmarksFolderConstants {
 
 	public static final long DEFAULT_PARENT_FOLDER_ID = 0;
