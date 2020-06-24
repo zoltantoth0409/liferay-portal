d7884ac9b1e09c5787fe051c0f7da5e547be5da7
pei-jung <pei-jung.lan@liferay.com>
2020-06-23 17:43:09 -0700

    LPS-112519 Add configuration category for accounts

---
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/configuration/admin/category/AccountsConfigurationCategory.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.internal.configuration.admin.category;
+
+import com.liferay.configuration.admin.category.ConfigurationCategory;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(service = ConfigurationCategory.class)
+public class AccountsConfigurationCategory implements ConfigurationCategory {
+
+	@Override
+	public String getBundleSymbolicName() {
+		return "com.liferay.account.service";
+	}
+
+	@Override
+	public String getCategoryIcon() {
+		return "briefcase";
+	}
+
+	@Override
+	public String getCategoryKey() {
+		return "accounts";
+	}
+
+	@Override
+	public String getCategorySection() {
+		return "platform";
+	}
+
+}
