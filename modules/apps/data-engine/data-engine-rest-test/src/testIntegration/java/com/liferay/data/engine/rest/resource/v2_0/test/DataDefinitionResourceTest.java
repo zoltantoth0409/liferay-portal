56fcac9687236f4852294fe0b1315b018474cd21
Brian Chan <brian.chan@liferay.com>
2020-06-24 10:19:58 -0700

    LPS-114779 SF

--- modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/test/DataDefinitionResourceTest.java
+++ modules/apps/data-engine/data-engine-rest-test/src/testIntegration/java/com/liferay/data/engine/rest/resource/v2_0/test/DataDefinitionResourceTest.java
@@ -70,7 +70,6 @@ public class DataDefinitionResourceTest
 			204,
 			dataDefinitionResource.deleteDataDefinitionHttpResponse(
 				dataDefinition.getId()));
-
 		assertHttpResponseStatusCode(
 			404,
 			dataDefinitionResource.getDataDefinitionHttpResponse(
