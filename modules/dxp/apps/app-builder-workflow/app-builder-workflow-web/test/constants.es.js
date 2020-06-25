0b0e0c30e122fab2b85ab8ac150fbf0bf1928b8e
Albino Junior <albino.junior@liferay.com>
2020-06-25 14:18:10 -0300

    LPS-114374 | LPS-111958 Add entry response mock constants

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/test/constants.es.js
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/test/constants.es.js
@@ -34,6 +34,139 @@ const createItems = (size) => {
 	return items;
 };
 
+export const ENTRY = {
+	APP_WORKFLOW: {
+		appWorkflowDefinitionId: 1,
+		appWorkflowStates: [
+			{initial: true, name: 'Created'},
+			{initial: false, name: 'Closed'},
+		],
+	},
+	DATA_DEFINITION: {
+		availableLanguageIds: ['en_US'],
+		contentType: 'app-builder',
+		dataDefinitionFields: [
+			{
+				customProperties: {
+					autocomplete: false,
+					dataSourceType: 'manual',
+					dataType: 'string',
+					ddmDataProviderInstanceId: '[]',
+					ddmDataProviderInstanceOutput: '[]',
+					displayStyle: 'singleline',
+					fieldNamespace: '',
+					options: {
+						en_US: [
+							{
+								label: 'Option',
+								value: 'Option',
+							},
+						],
+					},
+					placeholder: {
+						en_US: '',
+					},
+					tooltip: {
+						en_US: '',
+					},
+					visibilityExpression: '',
+				},
+				defaultValue: {
+					en_US: '',
+				},
+				fieldType: 'text',
+				indexType: 'keyword',
+				indexable: true,
+				label: {
+					en_US: 'Name',
+				},
+				localizable: true,
+				name: 'Text',
+				nestedDataDefinitionFields: [],
+				readOnly: false,
+				repeatable: false,
+				required: true,
+				showLabel: true,
+				tip: {
+					en_US: '',
+				},
+			},
+		],
+		dataDefinitionKey: '38301',
+		dateCreated: '2020-06-22T21:14:23Z',
+		dateModified: '2020-06-22T21:14:44Z',
+		defaultLanguageId: 'en_US',
+		description: {},
+		id: 1,
+		name: {
+			en_US: 'Request',
+		},
+		siteId: 1,
+		storageType: 'json',
+		userId: 1,
+	},
+	DATA_LAYOUT: {
+		dataDefinitionId: 1,
+		dataLayoutKey: '38309',
+		dataLayoutPages: [
+			{
+				dataLayoutRows: [
+					{
+						dataLayoutColumns: [
+							{
+								columnSize: 12,
+								fieldNames: ['Text'],
+							},
+						],
+					},
+				],
+				description: {
+					en_US: '',
+				},
+				title: {
+					en_US: '',
+				},
+			},
+		],
+		dataRules: [],
+		dateCreated: '2020-06-22T21:14:44Z',
+		dateModified: '2020-06-22T21:14:44Z',
+		description: {},
+		id: 1,
+		name: {
+			en_US: 'Request Form',
+		},
+		paginationMode: 'wizard',
+	},
+	DATA_LIST_VIEW: {
+		appliedFilters: {},
+		dataDefinitionId: 1,
+		dateCreated: '2020-06-22T21:14:53Z',
+		dateModified: '2020-06-22T21:14:53Z',
+		fieldNames: ['Text'],
+		id: 1,
+		name: {
+			en_US: 'Table',
+		},
+		sortField: '',
+	},
+	DATA_RECORDS: (size = 1) => ({
+		items: Array.apply(null, Array(size)).map((_, id) => ({
+			dataRecordCollectionId: id,
+			dataRecordValues: {
+				Text: {
+					'en-US': `Name Test ${id}`,
+				},
+			},
+			id,
+		})),
+		lastPage: 1,
+		page: 1,
+		pageSize: size,
+		totalCount: size,
+	}),
+};
+
 export const ITEMS = {
 	MANY: (size) => createItems(size),
 	ONE: createItems(1),
