c80653109f8fcf34cd0f4fde2dfc8fc8a58be92a
Pablo Molina <contact@pablomolina.me>
2020-06-25 16:41:13 +0200

    LPS-116098 Update tests for ColumnWithControls

--- modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/layout-data-items/ColumnWithControls.test.js
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/layout-data-items/ColumnWithControls.test.js
@@ -13,9 +13,20 @@
  */
 
 import '@testing-library/jest-dom/extend-expect';
-import {cleanup} from '@testing-library/react';
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+import {DndProvider} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
+import {
+	ControlsProvider,
+	useSelectItem,
+} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/Controls';
+import {ColumnWithControls} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items';
 import {updateNewLayoutDataContext} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/ColumnWithControls';
+import {LAYOUT_DATA_ITEM_TYPES} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/config/constants/layoutDataItemTypes';
+import {VIEWPORT_SIZES} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/config/constants/viewportSizes';
+import {StoreAPIContextProvider} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/store';
 
 const LAYOUT_DATA = {
 	deletedItems: [],
@@ -44,140 +55,223 @@ const LAYOUT_DATA = {
 	},
 };
 
-describe('updateNewLayoutDataContext', () => {
-	afterEach(() => {
-		cleanup();
+const renderColumn = ({
+	activeItemId = 'row',
+	columnIndex = 1,
+	hasUpdatePermissions = true,
+	lockedExperience = false,
+} = {}) => {
+	const columnA = {
+		children: [],
+		config: {},
+		itemId: 'columnA',
+		parentId: 'row',
+		type: LAYOUT_DATA_ITEM_TYPES.column,
+	};
+
+	const columnB = {
+		children: [],
+		config: {},
+		itemId: 'columnB',
+		parentId: 'row',
+		type: LAYOUT_DATA_ITEM_TYPES.column,
+	};
+
+	const row = {
+		children: ['columnA', 'columnB'],
+		config: {},
+		itemId: 'row',
+		parentId: null,
+		type: LAYOUT_DATA_ITEM_TYPES.row,
+	};
+
+	const layoutData = {
+		items: {columnA, columnB, row},
+	};
+
+	const AutoSelect = () => {
+		useSelectItem()(activeItemId);
+
+		return null;
+	};
+
+	return render(
+		<DndProvider backend={HTML5Backend}>
+			<ControlsProvider>
+				<StoreAPIContextProvider
+					getState={() => ({
+						permissions: {
+							LOCKED_SEGMENTS_EXPERIMENT: lockedExperience,
+							UPDATE: hasUpdatePermissions,
+						},
+						selectedViewportSize: VIEWPORT_SIZES.desktop,
+					})}
+				>
+					<AutoSelect />
+					<ColumnWithControls
+						item={columnIndex ? columnB : columnA}
+						layoutData={layoutData}
+					/>
+				</StoreAPIContextProvider>
+			</ControlsProvider>
+		</DndProvider>
+	);
+};
+
+describe('ColumnWithControls', () => {
+	it('hides resize handler if parent row is not active', () => {
+		const {queryByTitle} = renderColumn({activeItemId: 'nothing'});
+
+		expect(queryByTitle('resize-column')).toBe(null);
 	});
 
-	it('allows changing module width for a viewport', async () => {
-		const columnConfig = {
-			'column-1': {
-				config: {
-					mobile: {},
-					size: 4,
-					tablet: {},
-				},
-				size: 7,
-			},
-			'column-2': {
-				config: {
-					mobile: {},
-					size: 4,
-					tablet: {},
-				},
-				size: 1,
-			},
-		};
+	it('hides resize handler if user has no permissions', () => {
+		const {queryByTitle} = renderColumn({hasUpdatePermissions: false});
 
-		expect(
-			updateNewLayoutDataContext(LAYOUT_DATA, columnConfig, 'desktop')
-		).toEqual({
-			...LAYOUT_DATA,
-			items: {
-				...LAYOUT_DATA.items,
+		expect(queryByTitle('resize-column')).toBe(null);
+	});
+
+	it('hides resize handler for first column', () => {
+		const {queryByTitle} = renderColumn({columnIndex: 0});
+
+		expect(queryByTitle('resize-column')).toBe(null);
+	});
+
+	describe('updateNewLayoutDataContext', () => {
+		afterEach(() => {
+			cleanup();
+		});
+
+		it('allows changing module width for a viewport', async () => {
+			const columnConfig = {
 				'column-1': {
 					config: {
 						mobile: {},
-						size: 7,
+						size: 4,
 						tablet: {},
 					},
+					size: 7,
 				},
 				'column-2': {
 					config: {
 						mobile: {},
-						size: 1,
+						size: 4,
 						tablet: {},
 					},
+					size: 1,
 				},
-			},
+			};
+
+			expect(
+				updateNewLayoutDataContext(LAYOUT_DATA, columnConfig, 'desktop')
+			).toEqual({
+				...LAYOUT_DATA,
+				items: {
+					...LAYOUT_DATA.items,
+					'column-1': {
+						config: {
+							mobile: {},
+							size: 7,
+							tablet: {},
+						},
+					},
+					'column-2': {
+						config: {
+							mobile: {},
+							size: 1,
+							tablet: {},
+						},
+					},
+				},
+			});
 		});
-	});
 
-	it('allows changing module without affecting previous viewports', async () => {
-		const columnConfig = {
-			'column-1': {
-				config: {
-					mobile: {},
-					size: 4,
-					tablet: {size: 8},
-				},
-				size: 6,
-			},
-			'column-2': {
-				config: {
-					mobile: {},
-					size: 4,
-					tablet: {size: 2},
-				},
-				size: 3,
-			},
-		};
-
-		expect(
-			updateNewLayoutDataContext(LAYOUT_DATA, columnConfig, 'mobile')
-		).toEqual({
-			...LAYOUT_DATA,
-			items: {
-				...LAYOUT_DATA.items,
+		it('allows changing module without affecting previous viewports', async () => {
+			const columnConfig = {
 				'column-1': {
+					config: {
+						mobile: {},
+						size: 4,
+						tablet: {size: 8},
+					},
+					size: 6,
+				},
+				'column-2': {
+					config: {
+						mobile: {},
+						size: 4,
+						tablet: {size: 2},
+					},
+					size: 3,
+				},
+			};
+
+			expect(
+				updateNewLayoutDataContext(LAYOUT_DATA, columnConfig, 'mobile')
+			).toEqual({
+				...LAYOUT_DATA,
+				items: {
+					...LAYOUT_DATA.items,
+					'column-1': {
+						config: {
+							mobile: {size: 6},
+							size: 4,
+							tablet: {size: 8},
+						},
+					},
+					'column-2': {
+						config: {
+							mobile: {size: 3},
+							size: 4,
+							tablet: {size: 2},
+						},
+					},
+				},
+			});
+		});
+
+		it('allows changing previously modified module without affecting the rest of viewports', async () => {
+			const columnConfig = {
+				'column-1': {
+					config: {
+						mobile: {size: 3},
+						size: 4,
+						tablet: {size: 2},
+					},
+					size: 1,
+				},
+				'column-2': {
 					config: {
 						mobile: {size: 6},
 						size: 4,
 						tablet: {size: 8},
 					},
+					size: 2,
 				},
-				'column-2': {
-					config: {
-						mobile: {size: 3},
-						size: 4,
-						tablet: {size: 2},
-					},
-				},
-			},
-		});
-	});
+			};
 
-	it('allows changing previously modified module without affecting the rest of viewports', async () => {
-		const columnConfig = {
-			'column-1': {
-				config: {
-					mobile: {size: 3},
-					size: 4,
-					tablet: {size: 2},
-				},
-				size: 1,
-			},
-			'column-2': {
-				config: {
-					mobile: {size: 6},
-					size: 4,
-					tablet: {size: 8},
-				},
-				size: 2,
-			},
-		};
-
-		expect(
-			updateNewLayoutDataContext(LAYOUT_DATA, columnConfig, 'tablet')
-		).toEqual({
-			...LAYOUT_DATA,
-			items: {
-				...LAYOUT_DATA.items,
-				'column-1': {
-					config: {
-						mobile: {size: 3},
-						size: 4,
-						tablet: {size: 1},
+			expect(
+				updateNewLayoutDataContext(LAYOUT_DATA, columnConfig, 'tablet')
+			).toEqual({
+				...LAYOUT_DATA,
+				items: {
+					...LAYOUT_DATA.items,
+					'column-1': {
+						config: {
+							mobile: {size: 3},
+							size: 4,
+							tablet: {size: 1},
+						},
+					},
+					'column-2': {
+						config: {
+							mobile: {size: 6},
+							size: 4,
+							tablet: {size: 2},
+						},
 					},
 				},
-				'column-2': {
-					config: {
-						mobile: {size: 6},
-						size: 4,
-						tablet: {size: 2},
-					},
-				},
-			},
+			});
 		});
 	});
 });
