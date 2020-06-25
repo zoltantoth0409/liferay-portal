1dbebe3d5e02c50a2379427fc46812051b163ed2
Peter Shin <peter.shin@liferay.com>
2020-06-25 10:51:24 -0500

    LPS-116161 Check if file exists

--- portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
+++ portal-kernel/test/unit/com/liferay/portal/modules/ModulesStructureTest.java
@@ -1049,8 +1049,10 @@ public class ModulesStructureTest {
 				buildGradle);
 		}
 
-		_testGradleBuildProperties(
-			dirPath, gradlePropertiesPath, dxpRepo, privateRepo);
+		if (Files.exists(gradlePropertiesPath)) {
+			_testGradleBuildProperties(
+				dirPath, gradlePropertiesPath, dxpRepo, privateRepo);
+		}
 
 		if (Files.notExists(dirPath.resolve("settings-ext.gradle"))) {
 			settingsGradleTemplate = StringUtil.removeSubstring(
