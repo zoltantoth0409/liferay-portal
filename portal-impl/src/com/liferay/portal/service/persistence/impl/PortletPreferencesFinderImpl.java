97b3da8d6845fa8f76695b08cb489586ca8c1045
tina.tian <tina.tian@liferay.com>
2020-06-25 10:58:47 -0700

    LPS-115687 Remove non-generated blocking cache protections.

--- portal-impl/src/com/liferay/portal/service/persistence/impl/PortletPreferencesFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/PortletPreferencesFinderImpl.java
@@ -299,9 +299,6 @@ public class PortletPreferencesFinderImpl
 					FINDER_PATH_FIND_BY_C_G_O_O_P_P, finderArgs, list);
 			}
 			catch (Exception exception) {
-				FinderCacheUtil.removeResult(
-					FINDER_PATH_FIND_BY_C_G_O_O_P_P, finderArgs);
-
 				throw new SystemException(exception);
 			}
 			finally {
