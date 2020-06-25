5a8208d90a20c16b939dc8c0498cb2b090ce28e5
Sarai Díaz García <dgarciasarai@gmail.com>
2020-06-25 12:16:18 +0200

    LPS-115418 Use reduce here too

--- modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/components/AuditBarChart.js
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/components/AuditBarChart.js
@@ -104,20 +104,22 @@ export default function AuditBarChart({rtl, vocabularies}) {
 			const style = document.createElement('style');
 			style.id = 'legend-checkboxes';
 			style.type = 'text/css';
-
-			Object.entries(colors).forEach(([dataKey, color]) => {
-				style.textContent += `
-					.custom-control-color-${dataKey}.custom-control-input:checked ~ 
-						.custom-control-label::before {
-							background-color: ${color};
-							border-color: ${color};
-					}
-					.custom-control-color-${dataKey}.custom-control-input:not(:checked) ~ 
-						.custom-control-label::before {
-							border-color: ${color};
-					}
-				`;
-			});
+			style.textContent = Object.entries(colors).reduce(
+				(acc, [dataKey, color]) => {
+					return acc.concat(`
+						.custom-control-color-${dataKey}.custom-control-input:checked ~ 
+							.custom-control-label::before {
+								background-color: ${color};
+								border-color: ${color};
+						}
+						.custom-control-color-${dataKey}.custom-control-input:not(:checked) ~ 
+							.custom-control-label::before {
+								border-color: ${color};
+						}
+					`);
+				},
+				''
+			);
 
 			document.head.appendChild(style);
 		}
