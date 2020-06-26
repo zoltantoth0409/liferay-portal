010bf2016a43a30b2bccaf2ff2f18a2447ab410d
Pablo Molina <contact@pablomolina.me>
2020-06-26 07:29:15 +0200

    LPS-116098 Add tests for ContainerWithControls

---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/layout-data-items/ContainerWithControls.test.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+import {DndProvider} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import {
+	ControlsProvider,
+	useSelectItem,
+} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/Controls';
+import {ContainerWithControls} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items';
+import {LAYOUT_DATA_ITEM_TYPES} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/config/constants/layoutDataItemTypes';
+import {VIEWPORT_SIZES} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/config/constants/viewportSizes';
+import {StoreAPIContextProvider} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/store';
+
+const renderContainer = ({
+	activeItemId = 'container',
+	hasUpdatePermissions = true,
+	lockedExperience = false,
+} = {}) => {
+	const container = {
+		children: [],
+		config: {},
+		itemId: 'container',
+		parentId: null,
+		type: LAYOUT_DATA_ITEM_TYPES.container,
+	};
+
+	const layoutData = {
+		items: {container},
+	};
+
+	const AutoSelect = () => {
+		useSelectItem()(activeItemId);
+
+		return null;
+	};
+
+	return render(
+		<DndProvider backend={HTML5Backend}>
+			<ControlsProvider>
+				<StoreAPIContextProvider
+					getState={() => ({
+						permissions: {
+							LOCKED_SEGMENTS_EXPERIMENT: lockedExperience,
+							UPDATE: hasUpdatePermissions,
+						},
+						selectedViewportSize: VIEWPORT_SIZES.desktop,
+					})}
+				>
+					<AutoSelect />
+					<ContainerWithControls
+						item={container}
+						layoutData={layoutData}
+					/>
+				</StoreAPIContextProvider>
+			</ControlsProvider>
+		</DndProvider>
+	);
+};
+
+describe('ContainerWithControls', () => {
+	afterEach(cleanup);
+
+	it('hides FloatingToolbar if user has no permissions', () => {
+		const {baseElement} = renderContainer({hasUpdatePermissions: false});
+
+		expect(
+			baseElement.querySelector('.page-editor__floating-toolbar')
+		).toBe(null);
+	});
+
+	it('does not add container class if user has no permissions', () => {
+		const {baseElement} = renderContainer({hasUpdatePermissions: false});
+
+		expect(baseElement.querySelector('.page-editor__container')).toBe(null);
+	});
+});
