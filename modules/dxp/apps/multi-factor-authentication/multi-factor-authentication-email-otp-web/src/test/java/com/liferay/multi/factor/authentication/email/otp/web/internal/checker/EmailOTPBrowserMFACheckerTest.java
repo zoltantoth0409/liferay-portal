e28c33dcb8d72f4c078c9a3906c3f123a54047b7
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2020-06-26 12:59:26 +0100

    LPS-115899 New test for email obfuscation

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-email-otp-web/src/test/java/com/liferay/multi/factor/authentication/email/otp/web/internal/checker/EmailOTPBrowserMFACheckerTest.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.email.otp.web.internal.checker;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Stian Sigvartsen
+ */
+public class EmailOTPBrowserMFACheckerTest {
+
+	@Test
+	public void testObfuscateEmailAddress() throws Exception {
+		Assert.assertEquals(
+			"*@liferay.com",
+			EmailOTPBrowserMFAChecker.obfuscateEmailAddress("t@liferay.com"));
+
+		Assert.assertEquals(
+			"**@liferay.com",
+			EmailOTPBrowserMFAChecker.obfuscateEmailAddress("te@liferay.com"));
+
+		Assert.assertEquals(
+			"***@liferay.com",
+			EmailOTPBrowserMFAChecker.obfuscateEmailAddress("tes@liferay.com"));
+
+		Assert.assertEquals(
+			"t***@liferay.com",
+			EmailOTPBrowserMFAChecker.obfuscateEmailAddress(
+				"test@liferay.com"));
+
+		Assert.assertEquals(
+			"t***1@liferay.com",
+			EmailOTPBrowserMFAChecker.obfuscateEmailAddress(
+				"test1@liferay.com"));
+
+		Assert.assertEquals(
+			"te***1@liferay.com",
+			EmailOTPBrowserMFAChecker.obfuscateEmailAddress(
+				"test11@liferay.com"));
+	}
+
+}
