8b937634f978c01edd5f429cd0b69a490985cb24
Rubén Pulido <ruben.pulido@liferay.com>
2020-06-27 01:30:49 +0200

    LPS-115958 Adapt ImportFragmentEntriesStrutsActionTest

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/struts/test/ImportFragmentEntriesStrutsActionTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/struts/test/ImportFragmentEntriesStrutsActionTest.java
@@ -144,7 +144,7 @@ public class ImportFragmentEntriesStrutsActionTest {
 		FragmentCollection fragmentCollection = fragmentCollections.get(0);
 
 		Assert.assertEquals(
-			1,
+			3,
 			_fragmentEntryLocalService.getFragmentEntriesCount(
 				fragmentCollection.getFragmentCollectionId()));
 
