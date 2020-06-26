64ea2362c62557fbd2fbc1dcbfb26c6af0b09d21
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-06-26 17:43:04 +0200

    LPS-116034 FormVariations provider for JournalArticle

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/item/provider/JournalArticleInfoItemFormVariationsProvider.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.info.item.provider;
+
+import com.liferay.dynamic.data.mapping.kernel.DDMStructure;
+import com.liferay.dynamic.data.mapping.kernel.DDMStructureManagerUtil;
+import com.liferay.info.item.InfoItemFormVariation;
+import com.liferay.info.item.provider.InfoItemFormVariationsProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.liferay.portal.kernel.util.PortalUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jorge Ferrer
+ */
+@Component(immediate = true, service = InfoItemFormVariationsProvider.class)
+public class JournalArticleInfoItemFormVariationsProvider
+	implements InfoItemFormVariationsProvider<JournalArticle> {
+
+	@Override
+	public Collection<InfoItemFormVariation> getInfoItemFormVariations(
+		long groupId) {
+
+		List<InfoItemFormVariation> infoItemFormVariations = new ArrayList<>();
+
+		try {
+			long[] groupIds =
+				_portal.getCurrentAndAncestorSiteGroupIds(groupId);
+
+			List<DDMStructure> ddmStructures =
+				DDMStructureManagerUtil.getStructures(
+					groupIds,
+					_portal.getClassNameId(JournalArticle.class.getName()));
+
+			for (DDMStructure ddmStructure : ddmStructures) {
+				infoItemFormVariations.add(
+					new InfoItemFormVariation(
+						String.valueOf(ddmStructure.getStructureId()),
+						InfoLocalizedValue.builder(
+						).addValues(
+							ddmStructure.getNameMap()
+						).build()));
+			}
+
+			return infoItemFormVariations;
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(
+				"An unexpected error occurred", portalException);
+		}
+	}
+
+	@Reference
+	private Portal _portal;
+
+}
