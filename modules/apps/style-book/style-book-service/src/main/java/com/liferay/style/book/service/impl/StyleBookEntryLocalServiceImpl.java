cfcd5a1366b874caa15ef60ca48c775b7d13f74f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-06-26 12:55:47 +0100

    LPS-116205 Adds new methods to allow adding/updating/removing style books

--- modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/service/impl/StyleBookEntryLocalServiceImpl.java
+++ modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/service/impl/StyleBookEntryLocalServiceImpl.java
@@ -14,22 +14,25 @@
 
 package com.liferay.style.book.service.impl;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ModelHintsUtil;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.style.book.exception.StyleBookEntryNameException;
+import com.liferay.style.book.model.StyleBookEntry;
 import com.liferay.style.book.service.base.StyleBookEntryLocalServiceBaseImpl;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * The implementation of the style book entry local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.style.book.service.StyleBookEntryLocalService</code> interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
+ * @author Eudaldo Alonso
  * @see StyleBookEntryLocalServiceBaseImpl
  */
 @Component(
@@ -39,10 +42,88 @@ import org.osgi.service.component.annotations.Component;
 public class StyleBookEntryLocalServiceImpl
 	extends StyleBookEntryLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Use <code>com.liferay.style.book.service.StyleBookEntryLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>com.liferay.style.book.service.StyleBookEntryLocalServiceUtil</code>.
-	 */
+	@Override
+	public StyleBookEntry addStyleBookEntry(
+			long userId, long groupId, String name,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		// Style book entry
+
+		User user = userLocalService.getUser(userId);
+
+		long companyId = user.getCompanyId();
+
+		if (serviceContext != null) {
+			companyId = serviceContext.getCompanyId();
+		}
+		else {
+			serviceContext = new ServiceContext();
+		}
+
+		validate(name);
+
+		long styleBookEntryId = counterLocalService.increment();
+
+		StyleBookEntry styleBookEntry = styleBookEntryPersistence.create(
+			styleBookEntryId);
+
+		styleBookEntry.setGroupId(groupId);
+		styleBookEntry.setCompanyId(companyId);
+		styleBookEntry.setUserId(user.getUserId());
+		styleBookEntry.setUserName(user.getFullName());
+		styleBookEntry.setCreateDate(serviceContext.getCreateDate(new Date()));
+		styleBookEntry.setName(name);
+
+		return styleBookEntryPersistence.update(styleBookEntry);
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public StyleBookEntry deleteStyleBookEntry(StyleBookEntry styleBookEntry)
+		throws PortalException {
+
+		// Style book entry
+
+		styleBookEntryPersistence.remove(styleBookEntry);
+
+		return styleBookEntry;
+	}
+
+	@Override
+	public StyleBookEntry updateStyleBookEntry(
+			long styleBookEntryId, String name)
+		throws PortalException {
+
+		StyleBookEntry styleBookEntry =
+			styleBookEntryPersistence.findByPrimaryKey(styleBookEntryId);
+
+		validate(name);
+
+		styleBookEntry.setName(name);
+
+		return styleBookEntryPersistence.update(styleBookEntry);
+	}
+
+	protected void validate(String name) throws PortalException {
+		if (Validator.isNull(name)) {
+			throw new StyleBookEntryNameException("Name must not be null");
+		}
+
+		if (name.contains(StringPool.PERIOD) ||
+			name.contains(StringPool.SLASH)) {
+
+			throw new StyleBookEntryNameException(
+				"Name contains invalid characters");
+		}
+
+		int nameMaxLength = ModelHintsUtil.getMaxLength(
+			StyleBookEntry.class.getName(), "name");
+
+		if (name.length() > nameMaxLength) {
+			throw new StyleBookEntryNameException(
+				"Maximum length of name exceeded");
+		}
+	}
 
 }
