50bec1e1cb496bf494aef55a5b9fd7361d9a5ec7
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2020-06-27 02:38:25 -0300

    LPS-115710 Create component to customize axis ticks

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/components/chart/bar/CustomizedAxisTick.es.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React from 'react';
+import {Text} from 'recharts';
+
+export default ({payload, x, y}) => {
+	return (
+		<Text
+			textAnchor="middle"
+			verticalAnchor="start"
+			width={180}
+			x={x}
+			y={y}
+		>
+			{payload.value.length > 34
+				? `${payload.value.substring(0, 34)}...`
+				: payload.value}
+		</Text>
+	);
+};
