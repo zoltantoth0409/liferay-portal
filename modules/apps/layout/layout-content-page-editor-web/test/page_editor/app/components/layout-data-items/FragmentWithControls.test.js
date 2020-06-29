da6ae1630edf44ed472859fd9abe2c41cbc1ebe4
Sandro Chinea <sandrochinea@gmail.com>
2020-06-29 13:11:58 +0100

    LPS-116098 Add tests for FragmentWithControls

---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/layout-data-items/FragmentWithControls.test.js
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {act, cleanup, render} from '@testing-library/react';
+import React from 'react';
+import {DndProvider} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import {
+	ControlsProvider,
+	useSelectItem,
+} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/Controls';
+import {EditableProcessorContextProvider} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/fragment-content/EditableProcessorContext';
+import FragmentWithControls from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/FragmentWithControls';
+import {LAYOUT_DATA_ITEM_TYPES} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/config/constants/layoutDataItemTypes';
+import {VIEWPORT_SIZES} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/config/constants/viewportSizes';
+import {StoreAPIContextProvider} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/store';
+
+const renderFragment = ({
+	activeItemId = 'fragment',
+	hasUpdatePermissions = true,
+	lockedExperience = false,
+} = {}) => {
+	const fragmentEntryLink = {
+		editableValues: {},
+		fragmentEntryLinkId: 'fragmentEntryLink',
+	};
+
+	const fragment = {
+		children: [],
+		config: {
+			fragmentEntryLinkId: fragmentEntryLink.fragmentEntryLinkId,
+		},
+		itemId: 'fragment',
+		parentId: null,
+		type: LAYOUT_DATA_ITEM_TYPES.fragment,
+	};
+
+	const layoutData = {
+		items: {fragment},
+	};
+
+	const AutoSelect = () => {
+		useSelectItem()(activeItemId);
+
+		return null;
+	};
+
+	return render(
+		<DndProvider backend={HTML5Backend}>
+			<ControlsProvider>
+				<StoreAPIContextProvider
+					getState={() => ({
+						fragmentEntryLinks: {
+							[fragmentEntryLink.fragmentEntryLinkId]: fragmentEntryLink,
+						},
+						permissions: {
+							LOCKED_SEGMENTS_EXPERIMENT: lockedExperience,
+							UPDATE: hasUpdatePermissions,
+						},
+						selectedViewportSize: VIEWPORT_SIZES.desktop,
+					})}
+				>
+					<EditableProcessorContextProvider>
+						<AutoSelect />
+						<FragmentWithControls
+							item={fragment}
+							layoutData={layoutData}
+						/>
+					</EditableProcessorContextProvider>
+				</StoreAPIContextProvider>
+			</ControlsProvider>
+		</DndProvider>
+	);
+};
+
+describe('FragmentWithControls', () => {
+	afterEach(cleanup);
+
+	it('hides FloatingToolbar if user has no permissions', async () => {
+		await act(async () => {
+			renderFragment({hasUpdatePermissions: false});
+		});
+
+		expect(
+			document.body.querySelector('.page-editor__floating-toolbar')
+		).toBe(null);
+	});
+});
