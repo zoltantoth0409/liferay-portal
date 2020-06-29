e885c764f9502db317f1f7aff590e2bb2d497560
Sandro Chinea <sandrochinea@gmail.com>
2020-06-29 17:15:57 +0100

    LPS-116098 Add tests for Topper and TopperEmpty

---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/TopperEmpty.test.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+import {DndProvider} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import {ControlsProvider} from '../../../../src/main/resources/META-INF/resources/page_editor/app/components/Controls';
+import TopperEmpty from '../../../../src/main/resources/META-INF/resources/page_editor/app/components/Topper';
+import {LAYOUT_DATA_ITEM_TYPES} from '../../../../src/main/resources/META-INF/resources/page_editor/app/config/constants/layoutDataItemTypes';
+import {StoreAPIContextProvider} from '../../../../src/main/resources/META-INF/resources/page_editor/app/store';
+
+const renderTopperEmpty = ({
+	hasUpdatePermissions = true,
+	lockedExperience = false,
+} = {}) => {
+	const row = {
+		children: [],
+		config: {},
+		itemId: 'row',
+		parentId: null,
+		type: LAYOUT_DATA_ITEM_TYPES.row,
+	};
+
+	const layoutData = {
+		items: {row},
+	};
+
+	return render(
+		<DndProvider backend={HTML5Backend}>
+			<ControlsProvider>
+				<StoreAPIContextProvider
+					getState={() => ({
+						permissions: {
+							LOCKED_SEGMENTS_EXPERIMENT: lockedExperience,
+							UPDATE: hasUpdatePermissions,
+						},
+					})}
+				>
+					<TopperEmpty item={row} layoutData={layoutData}>
+						<div></div>
+					</TopperEmpty>
+				</StoreAPIContextProvider>
+			</ControlsProvider>
+		</DndProvider>
+	);
+};
+
+describe('TopperEmpty', () => {
+	afterEach(cleanup);
+
+	it('does not render TopperEmpty if user has no permissions', () => {
+		const {baseElement} = renderTopperEmpty({hasUpdatePermissions: false});
+
+		expect(baseElement.querySelector('.page-editor__topper')).toBe(null);
+	});
+
+	it('renders empty TopperEmpty if user has permissions', () => {
+		const {baseElement} = renderTopperEmpty({});
+
+		expect(
+			baseElement.querySelector('.page-editor__topper')
+		).toBeInTheDocument();
+	});
+});
