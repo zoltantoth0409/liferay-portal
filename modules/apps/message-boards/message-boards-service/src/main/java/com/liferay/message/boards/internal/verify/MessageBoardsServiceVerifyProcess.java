4fa3674669414068dd66cc58b348428f945b24bd
Preston-Crary <preston.crary@liferay.com>
2020-06-29 11:33:43 -0700

    LPS-115699 Update release dependency.

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/verify/MessageBoardsServiceVerifyProcess.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/verify/MessageBoardsServiceVerifyProcess.java
@@ -43,7 +43,7 @@ public class MessageBoardsServiceVerifyProcess extends VerifyProcess {
 	}
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.message.boards.service)(&(release.schema.version>=4.0.0)(!(release.schema.version>=5.0.0))))",
+		target = "(&(release.bundle.symbolic.name=com.liferay.message.boards.service)(&(release.schema.version>=5.0.0)(!(release.schema.version>=6.0.0))))",
 		unbind = "-"
 	)
 	protected void setRelease(Release release) {
