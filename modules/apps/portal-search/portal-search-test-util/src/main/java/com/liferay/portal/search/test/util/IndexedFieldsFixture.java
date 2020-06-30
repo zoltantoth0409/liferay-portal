2b51b590479f2dd30c52750df35d269eb7ca3fb5
André de Oliveira <andre.oliveira@liferay.com>
2020-06-29 18:28:27 -0700

    LPS-100264 portal-search-test-util: Accept ctCollectionId
    https://issues.liferay.com/browse/LPS-100264
    
    Co-authored-by: Preston-Crary <preston.crary@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

--- modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/IndexedFieldsFixture.java
+++ modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/IndexedFieldsFixture.java
@@ -17,10 +17,12 @@ package com.liferay.portal.search.test.util;
 import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.change.tracking.constants.CTConstants;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.ClassedModel;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchEngine;
@@ -150,6 +152,18 @@ public class IndexedFieldsFixture {
 
 		if (uidm != null) {
 			map.put("uidm", uidm);
+		}
+
+		if (classedModel instanceof CTModel<?>) {
+			CTModel<?> ctModel = (CTModel<?>)classedModel;
+
+			if (ctModel.getCtCollectionId() !=
+					CTConstants.CT_COLLECTION_ID_PRODUCTION) {
+
+				map.put(
+					"ctCollectionId",
+					String.valueOf(ctModel.getCtCollectionId()));
+			}
 		}
 	}
 
