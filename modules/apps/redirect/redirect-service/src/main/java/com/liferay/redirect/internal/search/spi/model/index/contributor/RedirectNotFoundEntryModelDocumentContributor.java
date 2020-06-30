219e4600955be0c702c6f427b24f7891dbeb00de
Brian Chan <brian.chan@liferay.com>
2020-06-30 10:55:34 -0700

    LPS-116345 sort

--- modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/internal/search/spi/model/index/contributor/RedirectNotFoundEntryModelDocumentContributor.java
+++ modules/apps/redirect/redirect-service/src/main/java/com/liferay/redirect/internal/search/spi/model/index/contributor/RedirectNotFoundEntryModelDocumentContributor.java
@@ -37,9 +37,9 @@ public class RedirectNotFoundEntryModelDocumentContributor
 		Document document, RedirectNotFoundEntry redirectNotFoundEntry) {
 
 		document.addText(Field.URL, redirectNotFoundEntry.getUrl());
+		document.addKeyword("ignored", redirectNotFoundEntry.isIgnored());
 		document.addNumber(
 			"requestCount", redirectNotFoundEntry.getRequestCount());
-		document.addKeyword("ignored", redirectNotFoundEntry.isIgnored());
 	}
 
 }
