b43e47476f74cb1f4c655ddc16b939557ce203c3
Jonathan McCann <jonathan.mccann@liferay.com>
2020-06-30 12:51:21 -0700

    LPS-116382 Only check the domain if 'company.security.strangers' is enabled

--- portal-impl/src/com/liferay/portal/service/impl/UserServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserServiceImpl.java
@@ -2701,7 +2701,7 @@ public class UserServiceImpl extends UserServiceBaseImpl {
 			Company company = companyPersistence.findByPrimaryKey(
 				user.getCompanyId());
 
-			if (!company.isStrangersWithMx()) {
+			if (company.isStrangers() && !company.isStrangersWithMx()) {
 				throw new UserEmailAddressException.MustNotUseCompanyMx(
 					emailAddress);
 			}
