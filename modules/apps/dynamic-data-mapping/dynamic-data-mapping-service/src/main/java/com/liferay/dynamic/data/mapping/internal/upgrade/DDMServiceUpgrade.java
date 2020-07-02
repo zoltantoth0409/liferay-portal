c2978d81ed5d64db486376dd0fede050407a5d41
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2020-07-02 17:37:36 -0300

    LPS-116472 Create new upgrade process

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/DDMServiceUpgrade.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/DDMServiceUpgrade.java
@@ -231,8 +231,10 @@ public class DDMServiceUpgrade implements UpgradeStepRegistrator {
 
 		registry.register("2.0.7", "2.0.8", new DummyUpgradeStep());
 
+		registry.register("2.0.8", "2.0.9", new DummyUpgradeStep());
+
 		registry.register(
-			"2.0.8", "3.0.0",
+			"2.0.9", "3.0.0",
 			new BaseUpgradeSQLServerDatetime(
 				new Class<?>[] {
 					DDMContentTable.class, DDMDataProviderInstanceTable.class,
@@ -290,6 +292,8 @@ public class DDMServiceUpgrade implements UpgradeStepRegistrator {
 		registry.register(
 			"3.2.4", "3.2.5", new UpgradeDDMContent(_jsonFactory));
 
+		registry.register("3.2.4", "3.2.5", new DummyUpgradeStep());
+
 		registry.register(
 			"3.2.5", "3.3.0",
 			new UpgradeCTModel(
@@ -320,6 +324,11 @@ public class DDMServiceUpgrade implements UpgradeStepRegistrator {
 			"3.7.0", "3.7.1",
 			new UpgradeDDMStructureEmptyValidation(
 				ddmFormJSONDeserializer, ddmFormSerializer));
+
+		registry.register(
+			"3.7.1", "3.7.2",
+			new com.liferay.dynamic.data.mapping.internal.upgrade.v3_7_2.
+				UpgradeSchema());
 	}
 
 	@Activate
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v3_7_2/UpgradeSchema.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.internal.upgrade.v3_7_2;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.LoggingTimer;
+
+/**
+ * @author Rodrigo Paulino
+ */
+public class UpgradeSchema extends UpgradeProcess {
+
+	protected void alterTables() throws Exception {
+		try (LoggingTimer loggingTimer = new LoggingTimer()) {
+		}
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		alterTables();
+	}
+
+}
