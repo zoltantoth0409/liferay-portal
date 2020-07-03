41c17d00e83bcd52d70fd6f8a83e200f8394aeb9
Pavel Savinov <pavel.savinov@liferay.com>
2020-07-03 10:24:55 +0200

    LPS-116452 Generate data for staging/export/import performance tests

---
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/test/ExportImportPerformanceTest.java
@@ -0,0 +1,316 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
+import com.liferay.exportimport.kernel.service.StagingLocalService;
+import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.journal.constants.JournalFolderConstants;
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.util.LayoutCopyHelper;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PropertiesUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Pavel Savinov
+ */
+@RunWith(Arquillian.class)
+public class ExportImportPerformanceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		Class<?> clazz = ExportImportPerformanceTest.class;
+
+		Properties properties = PropertiesUtil.load(
+			clazz.getResourceAsStream("export-import-performance.properties"),
+			"utf-8");
+
+		_fragmentsPerPage = GetterUtil.getInteger(
+			properties.getProperty("fragments.per.page"));
+		_pagesCount = GetterUtil.getInteger(
+			properties.getProperty("pages.count"));
+		_pageType = properties.getProperty("page.type");
+		_portletsPerContentPage = GetterUtil.getInteger(
+			properties.getProperty("portlets.per.content.page"));
+		_portletsPerPortletPage = GetterUtil.getInteger(
+			properties.getProperty("portlets.per.portlet.page"));
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId());
+
+		ServiceContextThreadLocal.pushServiceContext(_serviceContext);
+
+		_createLayouts();
+	}
+
+	private void _createFragments(Layout layout) throws Exception {
+		Layout draftLayout = layout.fetchDraftLayout();
+
+		if (draftLayout == null) {
+			return;
+		}
+
+		for (int i = 0; i < _fragmentsPerPage; i++) {
+			JournalArticle journalArticle1 = JournalTestUtil.addArticle(
+				_group.getGroupId(),
+				JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+			JournalArticle journalArticle2 = JournalTestUtil.addArticle(
+				_group.getGroupId(),
+				JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+			JournalArticle journalArticle3 = JournalTestUtil.addArticle(
+				_group.getGroupId(),
+				JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+			FragmentEntry fragmentEntry =
+				_fragmentCollectionContributorTracker.getFragmentEntry(
+					_FRAGMENT_RENDERER_KEY);
+
+			Map<String, String> values = HashMapBuilder.put(
+				"classNameId",
+				String.valueOf(_portal.getClassNameId(JournalArticle.class))
+			).put(
+				"classPK_1",
+				String.valueOf(journalArticle1.getResourcePrimKey())
+			).put(
+				"classPK_2",
+				String.valueOf(journalArticle2.getResourcePrimKey())
+			).put(
+				"classPK_3",
+				String.valueOf(journalArticle3.getResourcePrimKey())
+			).build();
+
+			String editableValues = StringUtil.replace(
+				_FRAGMENT_EDITABLE_VALUES_TMPL, "${", "}", values);
+
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				fragmentEntry.getFragmentEntryId(), 0, draftLayout.getPlid(),
+				fragmentEntry.getCss(), fragmentEntry.getHtml(),
+				fragmentEntry.getJs(), fragmentEntry.getConfiguration(),
+				editableValues, StringPool.BLANK, 0, null, _serviceContext);
+		}
+
+		for (int i = 0; i < _portletsPerContentPage; i++) {
+			String instanceId = PortletIdCodec.generateInstanceId();
+
+			Map<String, String> values = HashMapBuilder.put(
+				"instanceId", instanceId
+			).put(
+				"portletName", JournalPortletKeys.JOURNAL
+			).build();
+
+			String editableValues = StringUtil.replace(
+				_FRAGMENT_PORTLET_TMPL, "${", "}", values);
+
+			_savePortletPreferencesWithArticle(
+				draftLayout,
+				PortletIdCodec.encode(JournalPortletKeys.JOURNAL, instanceId));
+
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0, 0, 0,
+				draftLayout.getPlid(), StringPool.BLANK, StringPool.BLANK,
+				StringPool.BLANK, StringPool.BLANK, editableValues,
+				StringPool.BLANK, 0, null, _serviceContext);
+		}
+
+		_layoutPageTemplateStructureLocalService.
+			rebuildLayoutPageTemplateStructure(
+				_group.getGroupId(), draftLayout.getPlid());
+
+		_layoutCopyHelper.copyLayout(draftLayout, layout);
+	}
+
+	private void _createLayouts() throws Exception {
+		for (int i = 0; i < _pagesCount; i++) {
+			Map<Locale, String> nameMap = HashMapBuilder.put(
+				LocaleUtil.US, "Layout_" + i
+			).build();
+
+			Layout layout = _layoutLocalService.addLayout(
+				TestPropsValues.getUserId(), _group.getGroupId(), false, 0, 0,
+				0, nameMap, new HashMap<>(), new HashMap<>(), new HashMap<>(),
+				new HashMap<>(), _pageType, StringPool.BLANK, false, false,
+				new HashMap<>(), 0, _serviceContext);
+
+			if (Objects.equals(_pageType, LayoutConstants.TYPE_CONTENT)) {
+				_createFragments(layout);
+			}
+			else {
+				_createPortlets(layout);
+			}
+		}
+	}
+
+	private void _createPortlets(Layout layout) throws Exception {
+		UnicodeProperties unicodeProperties =
+			layout.getTypeSettingsProperties();
+
+		String columnSettings = unicodeProperties.getProperty("column-1");
+
+		for (int i = 0; i < _portletsPerPortletPage; i++) {
+			String portletId = PortletIdCodec.encode(
+				JournalPortletKeys.JOURNAL,
+				PortletIdCodec.generateInstanceId());
+
+			_savePortletPreferencesWithArticle(layout, portletId);
+
+			if (Validator.isNotNull(columnSettings)) {
+				columnSettings += StringPool.COMMA;
+			}
+
+			columnSettings += portletId;
+		}
+
+		unicodeProperties.setProperty("column-1", columnSettings);
+
+		layout.setTypeSettingsProperties(unicodeProperties);
+
+		_layoutLocalService.updateLayout(layout);
+	}
+
+	private void _savePortletPreferencesWithArticle(
+			Layout layout, String portletId)
+		throws Exception {
+
+		JournalArticle journalArticle = JournalTestUtil.addArticle(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		AssetEntry assetEntry = _assetEntryLocalService.getEntry(
+			JournalArticle.class.getName(),
+			journalArticle.getResourcePrimKey());
+
+		Map<String, String> values = HashMapBuilder.put(
+			"articleId", journalArticle.getArticleId()
+		).put(
+			"assetEntryId", String.valueOf(assetEntry.getEntryId())
+		).put(
+			"groupId", String.valueOf(assetEntry.getGroupId())
+		).build();
+
+		String portletPreferencesXML = StringUtil.replace(
+			_PORTLET_PREFERENCES_TMPL, "${", "}", values);
+
+		PortletPreferencesFactoryUtil.getLayoutPortletSetup(
+			layout, portletId, portletPreferencesXML);
+	}
+
+	private static final String _FRAGMENT_EDITABLE_VALUES_TMPL =
+		StringUtil.read(
+			ExportImportPerformanceTest.class, "fragment-editable-values.tmpl");
+
+	private static final String _FRAGMENT_PORTLET_TMPL = StringUtil.read(
+		ExportImportPerformanceTest.class, "fragment-portlet.tmpl");
+
+	private static final String _FRAGMENT_RENDERER_KEY =
+		"FEATURED_CONTENT-highlights-circle";
+
+	private static final String _PORTLET_PREFERENCES_TMPL = StringUtil.read(
+		ExportImportPerformanceTest.class, "portlet-preferences.tmpl");
+
+	private static int _fragmentsPerPage;
+	private static int _pagesCount;
+	private static String _pageType;
+	private static int _portletsPerContentPage;
+	private static int _portletsPerPortletPage;
+
+	@Inject
+	private AssetEntryLocalService _assetEntryLocalService;
+
+	@Inject
+	private FragmentCollectionContributorTracker
+		_fragmentCollectionContributorTracker;
+
+	@Inject
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private LayoutCopyHelper _layoutCopyHelper;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
+	@Inject
+	private Portal _portal;
+
+	private ServiceContext _serviceContext;
+
+	@Inject
+	private StagingLocalService _stagingLocalService;
+
+}
