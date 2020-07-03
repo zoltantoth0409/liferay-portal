26c47a3ff4243d212caa8516f0bf73333c60ab33
Luismi Barcos <luismiguel.barcos@liferay.com>
2020-07-03 09:54:43 +0200

    LPS-115724 Add breadcrumb behavior depending on the number of sections. The current data are mocked.

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Breadcrumb.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Breadcrumb.es.js
@@ -12,32 +12,65 @@
  * details.
  */
 
-import ClayButton from '@clayui/button';
-import ClayDropDown from '@clayui/drop-down';
 import ClayIcon from '@clayui/icon';
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
+import BreadcrumbDropdown from './BreadcrumbDropdown.es';
 import Link from './Link.es';
-import ShortenedBreadcrumb from './ShortenedBreadcrumb.es';
 
 export default ({section}) => {
-	const MAX_SECTIONS_IN_BREADCRUMB = 8;
-	const [active, setActive] = useState(false);
+	const CURRENT_SECTION_ID = 37124;
+	const MAX_SECTIONS_IN_BREADCRUMB = 5;
+	const [breadcrumbNodes, setBreadcrumbNodes] = useState([]);
 
-	const getParentSubSections = () =>
-		(section &&
-			section.parentSection &&
-			section.parentSection.messageBoardSections.items) ||
-		[];
+	const getParentSubSections = (section) =>
+		(section && section.messageBoardSections.items) || [];
+
+	const getSectionById = (id) =>
+		mockApiResponse.messageBoardSections.items.find(
+			(section) => section.id === id
+		);
+
+	const createEllipsisSectionData = () => {
+		const categories = breadcrumbNodes
+			.slice(1, breadcrumbNodes.length - 1)
+			.map((section) => {
+				return {title: section.title};
+			});
+
+		return {subCategories: categories, title: ''};
+	};
+
+	const buildBreadcrumbNodesData = useCallback(
+		(sectionId = CURRENT_SECTION_ID, acc = []) => {
+			const section = getSectionById(sectionId);
+			acc.push({
+				subCategories: getParentSubSections(section),
+				title: section.title,
+			});
+
+			return section.parentMessageBoardSectionId === null
+				? acc.reverse()
+				: buildBreadcrumbNodesData(
+						section.parentMessageBoardSectionId,
+						acc
+				  );
+		},
+		[]
+	);
+
+	useEffect(() => {
+		setBreadcrumbNodes(buildBreadcrumbNodesData());
+	}, [buildBreadcrumbNodesData]);
 
 	return (
 		<section className="align-items-center d-flex">
 			<div className="questions-breadcrumb">
 				<ol className="breadcrumb mb-0 ml-2">
-					{hardcodedSections.length > MAX_SECTIONS_IN_BREADCRUMB ? (
-						ShortenedBreadcrumb()
+					{breadcrumbNodes.length > MAX_SECTIONS_IN_BREADCRUMB ? (
+						<ShortenedBreadcrumb />
 					) : (
-						<AllBreadcrumb sections={hardcodedSections} />
+						<AllBreadcrumb />
 					)}
 				</ol>
 			</div>
@@ -47,16 +80,15 @@ export default ({section}) => {
 	function AllBreadcrumb() {
 		return (
 			<>
-				<li className="breadcrumb-item breadcrumb-text-truncate">
-					<ClayIcon symbol="home" />
+				<li className="breadcrumb-item breadcrumb-text-truncate mr-0">
+					<Link
+						className="breadcrumb-item questions-breadcrumb-unstyled"
+						to={'/'}
+					>
+						<ClayIcon symbol="home" />
+					</Link>
 				</li>
-				{hardcodedSections.map((section) => (
-					<>
-						<li className="breadcrumb-item breadcrumb-text-truncate">
-							{section.title}
-						</li>
-					</>
-				))}
+				<BreadcrumbNode />
 			</>
 		);
 	}
@@ -64,303 +96,254 @@ export default ({section}) => {
 	function ShortenedBreadcrumb() {
 		return (
 			<>
-				<li className="breadcrumb-item breadcrumb-text-truncate">
-					<ClayIcon symbol="home" />
-				</li>
-				<li className="breadcrumb-item breadcrumb-text-truncate">
-					{hardcodedSections[0].title}
-				</li>
-				<li className="breadcrumb-item breadcrumb-text-truncate">
-					{hardcodedSections[1].title}
-				</li>
-				<li className="breadcrumb-item breadcrumb-text-truncate">
-					<ClayDropDown
-						active={active}
-						onActiveChange={setActive}
-						trigger={
-							<ClayButton
-								className="c-p-0 questions-breadcrumb-unstyled text-truncate"
-								displayType="unstyled"
-							>
-								<ClayIcon symbol="ellipsis-h" />
-								<ClayIcon symbol="caret-bottom-l" />
-							</ClayButton>
-						}
+				<li className="breadcrumb-item breadcrumb-text-truncate mr-0">
+					<Link
+						className="breadcrumb-item questions-breadcrumb-unstyled"
+						to={'/'}
 					>
-						<ClayDropDown.ItemList>
-							<ClayDropDown.Group>
-								{hardcodedSections.map((section, i) => (
-									<ClayDropDown.Item key={i}>
-										{section.title}
-									</ClayDropDown.Item>
-								))}
-							</ClayDropDown.Group>
-						</ClayDropDown.ItemList>
-					</ClayDropDown>
+						<ClayIcon symbol="home" />
+					</Link>
 				</li>
-				<li className="breadcrumb-item breadcrumb-text-truncate">
-					{hardcodedSections[hardcodedSections.length - 2].title}
+				<BreadcrumbNode end={1} start={0} />
+				<li className="breadcrumb-item breadcrumb-text-truncate mr-0">
+					<BreadcrumbDropdown
+						className="breadcrumb-item breadcrumb-text-truncate"
+						createLink={false}
+						section={createEllipsisSectionData()}
+					/>
 				</li>
-				<li className="breadcrumb-item breadcrumb-text-truncate">
-					{hardcodedSections[hardcodedSections.length - 1].title}
-				</li>
+				<BreadcrumbNode start={-1} />
 			</>
 		);
 	}
+
+	function BreadcrumbNode({
+		createLink,
+		end = breadcrumbNodes.length,
+		start = 0,
+	}) {
+		return breadcrumbNodes.slice(start, end).map((section, i) => (
+			<li
+				className="breadcrumb-item breadcrumb-text-truncate mr-0"
+				key={i}
+			>
+				{section.subCategories.length <= 0 ? (
+					section.title
+				) : (
+					<BreadcrumbDropdown
+						className="breadcrumb-item breadcrumb-text-truncate"
+						createLink={createLink}
+						section={section}
+					/>
+				)}
+			</li>
+		));
+	}
 };
 
-const hardcodedSections = [
-	{
-		id: 1,
-		parentSection: {
-			id: 1,
-			title: 'Root',
-		},
-		title: 'Root',
-	},
-	{
-		id: 2,
-		parentSection: {
-			id: 1,
-			title: 'Root',
-		},
-		title: 'Level 1',
-	},
-	{
-		id: 3,
-		parentSection: {
-			id: 2,
-			title: 'Level 1',
-		},
-		title: 'Level 2',
-	},
-	{
-		id: 4,
-		parentSection: {
-			id: 3,
-			title: 'Level 2',
-		},
-		title: 'Level 3',
-	},
-	{
-		id: 5,
-		parentSection: {
-			id: 4,
-			title: 'Level 3',
-		},
-		title: 'Level 4',
-	},
-	{
-		id: 6,
-		parentSection: {
-			id: 5,
-			title: 'Level 4',
-		},
-		title: 'Level 5',
-	},
-	{
-		id: 7,
-		parentSection: {
-			id: 6,
-			title: 'Level 5',
-		},
-		title: 'Level 6',
-	},
-	{
-		id: 8,
-		parentSection: {
-			id: 7,
-			title: 'Level 6',
-		},
-		title: 'Level 7',
-	},
-	{
-		id: 9,
-		parentSection: {
-			id: 8,
-			title: 'Level 7',
-		},
-		title: 'Level 8',
-	},
-	{
-		id: 10,
-		parentSection: {
-			id: 9,
-			title: 'Level 8',
-		},
-		title: 'Level 9',
-	},
-	{
-		id: 11,
-		parentSection: {
-			id: 10,
-			title: 'Level 9',
-		},
-		title: 'Level 10',
-	},
-	{
-		id: 12,
-		parentSection: {
-			id: 11,
-			title: 'Level 10',
-		},
-		title: 'Level 11',
-	},
-	{
-		id: 13,
-		parentSection: {
-			id: 12,
-			title: 'Level 11',
-		},
-		title: 'Level 12',
-	},
-	{
-		id: 14,
-		parentSection: {
-			id: 13,
-			title: 'Level 12',
-		},
-		title: 'Level 13',
-	},
-	{
-		id: 15,
-		parentSection: {
-			id: 14,
-			title: 'Level 13',
-		},
-		title: 'Level 14',
-	},
-	{
-		id: 16,
-		parentSection: {
-			id: 15,
-			title: 'Level 14',
-		},
-		title: 'Level 15',
-	},
-	{
-		id: 17,
-		parentSection: {
-			id: 16,
-			title: 'Level 15',
-		},
-		title: 'Level 16',
-	},
-	{
-		id: 18,
-		parentSection: {
-			id: 17,
-			title: 'Level 16',
-		},
-		title: 'Level 17',
-	},
-	{
-		id: 19,
-		parentSection: {
-			id: 18,
-			title: 'Level 17',
-		},
-		title: 'Level 18',
-	},
-	{
-		id: 20,
-		parentSection: {
-			id: 19,
-			title: 'Level 18',
-		},
-		title: 'Level 19',
-	},
-];
-
-const mockSection = [
-	{
-		data: {
-			messageBoardSections: {
-				items: [
-					{
-						id: 36812,
-						messageBoardSections: {
-							items: [
-								{
-									id: 36826,
-									numberOfMessageBoardSections: 2,
-									parentMessageBoardSectionId: 36812,
-									title: 'Collaboration',
-								},
-								{
-									id: 36827,
-									numberOfMessageBoardSections: 1,
-									parentMessageBoardSectionId: 36812,
-									title: 'Web Experience Management',
-								},
-								{
-									id: 36828,
-									numberOfMessageBoardSections: 0,
-									parentMessageBoardSectionId: 36812,
-									title: 'Headless',
-								},
-							],
+const mockApiResponse = {
+	messageBoardSections: {
+		items: [
+			{
+				id: 37116,
+				messageBoardSections: {
+					items: [
+						{
+							id: 37132,
+							numberOfMessageBoardSections: 0,
+							parentMessageBoardSectionId: 37116,
+							title: 'Site builder',
 						},
-						numberOfMessageBoardSections: 3,
-						parentMessageBoardSectionId: null,
-						title: 'Portal',
-					},
-				],
-			},
-		},
-	},
-	{
-		data: {
-			messageBoardSections: {
-				items: [
-					{
-						id: 36826,
-						messageBoardSections: {
-							items: [
-								{
-									id: 36829,
-									numberOfMessageBoardSections: 0,
-									parentMessageBoardSectionId: 36826,
-									title: 'Blogs',
-								},
-								{
-									id: 36830,
-									numberOfMessageBoardSections: 0,
-									parentMessageBoardSectionId: 36826,
-									title: 'Documents & Media',
-								},
-							],
+						{
+							id: 37130,
+							numberOfMessageBoardSections: 0,
+							parentMessageBoardSectionId: 37116,
+							title: 'Web content',
 						},
-						numberOfMessageBoardSections: 2,
-						parentMessageBoardSectionId: 36812,
-						title: 'Collaboration',
-					},
-				],
+					],
+				},
+				numberOfMessageBoardSections: 2,
+				parentMessageBoardSectionId: 36812,
+				title: 'Web Experience Management',
 			},
-		},
-	},
-	{
-		data: {
-			messageBoardSections: {
-				items: [
-					{
-						id: 36827,
-						messageBoardSections: {
-							items: [
-								{
-									id: 36831,
-									numberOfMessageBoardSections: 0,
-									parentMessageBoardSectionId: 36827,
-									title: 'Web Contents',
-								},
-							],
+			{
+				id: 37130,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37116,
+				title: 'Web content',
+			},
+			{
+				id: 37128,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37118,
+				title: 'Vulcan',
+			},
+			{
+				id: 37103,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37087,
+				title: 'Spain',
+			},
+			{
+				id: 37132,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37116,
+				title: 'Site builder',
+			},
+			{
+				id: 37099,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37085,
+				title: 'Shopping cart',
+			},
+			{
+				id: 37124,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37120,
+				title: 'Sharing',
+			},
+			{
+				id: 37126,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37118,
+				title: 'Rest builder',
+			},
+			{
+				id: 37105,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37087,
+				title: 'Portugal',
+			},
+			{
+				id: 36812,
+				messageBoardSections: {
+					items: [
+						{
+							id: 37114,
+							numberOfMessageBoardSections: 2,
+							parentMessageBoardSectionId: 36812,
+							title: 'Collaboration',
 						},
-						numberOfMessageBoardSections: 1,
-						parentMessageBoardSectionId: null,
-						title: 'Web Experience Management',
-					},
-				],
+						{
+							id: 37116,
+							numberOfMessageBoardSections: 2,
+							parentMessageBoardSectionId: 36812,
+							title: 'Web Experience Management',
+						},
+						{
+							id: 37118,
+							numberOfMessageBoardSections: 2,
+							parentMessageBoardSectionId: 36812,
+							title: 'Headless',
+						},
+					],
+				},
+				numberOfMessageBoardSections: 3,
+				parentMessageBoardSectionId: null,
+				title: 'Portal',
 			},
-		},
-	}
-];
+			{
+				id: 37118,
+				messageBoardSections: {
+					items: [
+						{
+							id: 37126,
+							numberOfMessageBoardSections: 0,
+							parentMessageBoardSectionId: 37118,
+							title: 'Rest builder',
+						},
+						{
+							id: 37128,
+							numberOfMessageBoardSections: 0,
+							parentMessageBoardSectionId: 37118,
+							title: 'Vulcan',
+						},
+					],
+				},
+				numberOfMessageBoardSections: 2,
+				parentMessageBoardSectionId: 36812,
+				title: 'Headless',
+			},
+			{
+				id: 37122,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37114,
+				title: 'Documents & Media',
+			},
+			{
+				id: 37101,
+				messageBoardSections: {
+					items: [],
+				},
+				numberOfMessageBoardSections: 0,
+				parentMessageBoardSectionId: 37085,
+				title: 'Commerce integration',
+			},
+			{
+				id: 37114,
+				messageBoardSections: {
+					items: [
+						{
+							id: 37122,
+							numberOfMessageBoardSections: 0,
+							parentMessageBoardSectionId: 37114,
+							title: 'Documents & Media',
+						},
+						{
+							id: 37120,
+							numberOfMessageBoardSections: 1,
+							parentMessageBoardSectionId: 37114,
+							title: 'Blogs',
+						},
+					],
+				},
+				numberOfMessageBoardSections: 2,
+				parentMessageBoardSectionId: 36812,
+				title: 'Collaboration',
+			},
+			{
+				id: 37120,
+				messageBoardSections: {
+					items: [
+						{
+							id: 37124,
+							numberOfMessageBoardSections: 0,
+							parentMessageBoardSectionId: 37120,
+							title: 'Sharing',
+						},
+					],
+				},
+				numberOfMessageBoardSections: 1,
+				parentMessageBoardSectionId: 37114,
+				title: 'Blogs',
+			},
+		],
+	},
+};
