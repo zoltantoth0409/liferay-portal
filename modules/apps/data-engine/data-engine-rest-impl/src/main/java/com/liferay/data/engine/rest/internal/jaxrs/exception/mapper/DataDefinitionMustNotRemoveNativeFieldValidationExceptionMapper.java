f2f46d8c39cb55c167727ab9103d5d9e2a589077
Leonardo Barros <leonardo.barros@liferay.com>
2020-07-06 09:18:45 -0300

    LPS-102926 Add exception mapper

---
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/jaxrs/exception/mapper/DataDefinitionMustNotRemoveNativeFieldValidationExceptionMapper.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.data.engine.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.data.engine.rest.resource.exception.DataDefinitionValidationException;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Leonardo Barros
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Data.Engine.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Data.Engine.REST.DataDefinitionMustNotRemoveNativeFieldValidationExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class DataDefinitionMustNotRemoveNativeFieldValidationExceptionMapper
+	extends BaseExceptionMapper
+		<DataDefinitionValidationException.MustNotRemoveNativeField> {
+
+	@Override
+	protected Problem getProblem(
+		DataDefinitionValidationException.MustNotRemoveNativeField
+			mustNotRemoveNativeField) {
+
+		Set<String> removedFieldNames =
+			mustNotRemoveNativeField.getRemovedFieldNames();
+
+		Stream<String> stream = removedFieldNames.stream();
+
+		return new Problem(
+			stream.collect(Collectors.joining(StringPool.COMMA)),
+			Response.Status.BAD_REQUEST, mustNotRemoveNativeField.getMessage(),
+			"MustNotRemoveNativeField");
+	}
+
+}
