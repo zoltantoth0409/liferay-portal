bc2ed6857d829d95229a4589457b20843a761df5
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-07-07 17:24:39 +0200

    LPS-116675 Simplifying

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/ImportTranslationMVCActionCommand.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/ImportTranslationMVCActionCommand.java
@@ -21,7 +21,6 @@ import com.liferay.journal.constants.JournalPortletKeys;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.service.JournalArticleService;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.servlet.SessionErrors;
@@ -30,23 +29,18 @@ import com.liferay.portal.kernel.upload.LiferayFileItemException;
 import com.liferay.portal.kernel.upload.UploadException;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.upload.UploadRequestSizeException;
-import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.translation.exception.XLIFFFileException;
 import com.liferay.translation.importer.TranslationInfoItemFieldValuesImporter;
 import com.liferay.translation.info.item.updater.InfoItemFieldValuesUpdater;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import java.util.Objects;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletURL;
-import javax.portlet.WindowStateException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -102,10 +96,10 @@ public class ImportTranslationMVCActionCommand extends BaseMVCActionCommand {
 		catch (Exception exception) {
 			SessionErrors.add(actionRequest, exception.getClass(), exception);
 
-			_sendRedirect(
-				actionRequest, actionResponse, article.getResourcePrimKey(),
-				article.getGroupId(), article.getArticleId(),
-				article.getVersion());
+			actionResponse.setRenderParameter(
+				"mvcPath", "/import_translation.jsp");
+
+			hideDefaultSuccessMessage(actionRequest);
 		}
 	}
 
@@ -144,30 +138,6 @@ public class ImportTranslationMVCActionCommand extends BaseMVCActionCommand {
 
 			throw new PortalException(cause);
 		}
-	}
-
-	private void _sendRedirect(
-			ActionRequest actionRequest, ActionResponse actionResponse,
-			long articleResourcePrimKey, long groupId, String articleId,
-			double version)
-		throws IOException, WindowStateException {
-
-		PortletURL portletURL = PortletURLFactoryUtil.create(
-			actionRequest, JournalPortletKeys.JOURNAL,
-			PortletRequest.RENDER_PHASE);
-
-		portletURL.setParameter("mvcPath", "/import_translation.jsp");
-		portletURL.setParameter(
-			"redirect", ParamUtil.getString(actionRequest, "redirect"));
-		portletURL.setParameter(
-			"articleResourcePrimKey", String.valueOf(articleResourcePrimKey));
-		portletURL.setParameter("groupId", String.valueOf(groupId));
-		portletURL.setParameter("articleId", articleId);
-		portletURL.setParameter("version", String.valueOf(version));
-
-		portletURL.setWindowState(actionRequest.getWindowState());
-
-		sendRedirect(actionRequest, actionResponse, portletURL.toString());
 	}
 
 	@Reference(
