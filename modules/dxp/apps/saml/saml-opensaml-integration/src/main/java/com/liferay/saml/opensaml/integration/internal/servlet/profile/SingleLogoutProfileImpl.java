fe3778d251db2bfa0961358f44430f2b923e8994
hudakl <laszlo.hudak@liferay.com>
2020-07-08 09:33:15 +0200

    LPS-115559 Sort

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/SingleLogoutProfileImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/SingleLogoutProfileImpl.java
@@ -935,9 +935,9 @@ public class SingleLogoutProfileImpl
 
 		logoutResponse.setDestination(singleLogoutService.getLocation());
 
-		outboundMessageContext.addSubcontext(samlSelfEntityContext);
-		outboundMessageContext.addSubcontext(samlPeerEntityContext);
 		outboundMessageContext.addSubcontext(samlBindingContext);
+		outboundMessageContext.addSubcontext(samlPeerEntityContext);
+		outboundMessageContext.addSubcontext(samlSelfEntityContext);
 
 		sendSamlMessage(messageContext, httpServletResponse);
 	}
