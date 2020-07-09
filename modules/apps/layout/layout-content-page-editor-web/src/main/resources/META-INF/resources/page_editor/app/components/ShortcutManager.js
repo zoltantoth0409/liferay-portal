a30ea965477c626f661990ea6fdac550515443e3
Sandro Chinea <sandrochinea@gmail.com>
2020-07-09 11:33:58 +0100

    LPS-116770 Remove useCallbacks and wrap keymap in ref

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/ShortcutManager.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/ShortcutManager.js
@@ -13,7 +13,7 @@
  */
 
 import {closest} from 'metal-dom';
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import {
 	ARROW_DOWN_KEYCODE,
@@ -81,200 +81,171 @@ export default function ShortcutManager() {
 		widgets,
 	} = state;
 
-	const duplicate = useCallback(
-		(event) => {
+	const duplicate = (event) => {
+		event.preventDefault();
+
+		const item = layoutData.items[activeItemId];
+
+		if (
+			item &&
+			canBeDuplicated(fragmentEntryLinks, item, layoutData, widgets) &&
+			document.activeElement === document.body
+		) {
+			dispatch(
+				duplicateItem({
+					itemId: activeItemId,
+					segmentsExperienceId,
+					selectItem,
+				})
+			);
+		}
+	};
+
+	const remove = (event) => {
+		if (
+			!isEditableCKEditor(event.target) &&
+			!isTextElement(event.target) &&
+			!isCommentsAlloyEditor(event.target)
+		) {
+			event.preventDefault();
+		}
+
+		const item = layoutData.items[activeItemId];
+
+		if (
+			item &&
+			canBeRemoved(item, layoutData) &&
+			document.activeElement === document.body
+		) {
+			dispatch(
+				deleteItem({
+					itemId: item.itemId,
+					selectItem,
+					store: state,
+				})
+			);
+		}
+	};
+
+	const save = (event) => {
+		event.preventDefault();
+
+		const item = layoutData.items[activeItemId];
+
+		if (
+			item &&
+			canBeSaved(item, layoutData) &&
+			document.activeElement === document.body
+		) {
+			setOpenSaveModal(true);
+		}
+	};
+
+	const undo = (event) => {
+		if (
+			!isTextElement(event.target) &&
+			!isCommentsAlloyEditor(event.target) &&
+			!isWithinIframe()
+		) {
 			event.preventDefault();
 
-			const item = layoutData.items[activeItemId];
-
-			if (
-				item &&
-				canBeDuplicated(
-					fragmentEntryLinks,
-					item,
-					layoutData,
-					widgets
-				) &&
-				document.activeElement === document.body
-			) {
-				dispatch(
-					duplicateItem({
-						itemId: activeItemId,
-						segmentsExperienceId,
-						selectItem,
-					})
-				);
+			if (event.shiftKey) {
+				dispatch(redoThunk({store: state}));
 			}
-		},
-		[
-			activeItemId,
-			dispatch,
-			fragmentEntryLinks,
-			layoutData,
-			segmentsExperienceId,
-			selectItem,
-			widgets,
-		]
-	);
-
-	const remove = useCallback(
-		(event) => {
-			if (
-				!isEditableCKEditor(event.target) &&
-				!isTextElement(event.target) &&
-				!isCommentsAlloyEditor(event.target)
-			) {
-				event.preventDefault();
+			else {
+				dispatch(undoThunk({store: state}));
 			}
+		}
+	};
 
-			const item = layoutData.items[activeItemId];
+	const move = (event) => {
+		const item = layoutData.items[activeItemId];
 
-			if (
-				item &&
-				canBeRemoved(item, layoutData) &&
-				document.activeElement === document.body
-			) {
-				dispatch(
-					deleteItem({
-						itemId: item.itemId,
-						selectItem,
-						store: state,
-					})
-				);
-			}
-		},
-		[activeItemId, dispatch, layoutData, selectItem, state]
-	);
-
-	const save = useCallback(
-		(event) => {
+		if (
+			item &&
+			!isTextElement(event.target) &&
+			!isEditableCKEditor(event.target) &&
+			!isCommentsAlloyEditor(event.target)
+		) {
 			event.preventDefault();
 
-			const item = layoutData.items[activeItemId];
+			const {itemId, parentId} = item;
+
+			const parentItem = layoutData.items[parentId];
+
+			const numChildren = parentItem.children.length;
+
+			const currentPosition = parentItem.children.indexOf(itemId);
+
+			const direction =
+				event.keyCode === ARROW_UP_KEYCODE
+					? MOVE_ITEM_DIRECTIONS.UP
+					: MOVE_ITEM_DIRECTIONS.DOWN;
 
 			if (
-				item &&
-				canBeSaved(item, layoutData) &&
-				document.activeElement === document.body
+				(direction === MOVE_ITEM_DIRECTIONS.UP &&
+					currentPosition === 0) ||
+				(direction === MOVE_ITEM_DIRECTIONS.DOWN &&
+					currentPosition === numChildren - 1)
 			) {
-				setOpenSaveModal(true);
+				return;
 			}
-		},
-		[activeItemId, layoutData]
-	);
 
-	const undo = useCallback(
-		(event) => {
-			if (
-				!isTextElement(event.target) &&
-				!isCommentsAlloyEditor(event.target) &&
-				!isWithinIframe()
-			) {
-				event.preventDefault();
+			let position;
 
-				if (event.shiftKey) {
-					dispatch(redoThunk({store: state}));
-				}
-				else {
-					dispatch(undoThunk({store: state}));
-				}
+			if (direction === MOVE_ITEM_DIRECTIONS.UP) {
+				position = currentPosition - 1;
 			}
-		},
-		[dispatch, state]
-	);
-
-	const move = useCallback(
-		(event) => {
-			const item = layoutData.items[activeItemId];
-
-			if (
-				item &&
-				!isTextElement(event.target) &&
-				!isEditableCKEditor(event.target) &&
-				!isCommentsAlloyEditor(event.target)
-			) {
-				event.preventDefault();
-
-				const {itemId, parentId} = item;
-
-				const parentItem = layoutData.items[parentId];
-
-				const numChildren = parentItem.children.length;
-
-				const currentPosition = parentItem.children.indexOf(itemId);
-
-				const direction =
-					event.keyCode === ARROW_UP_KEYCODE
-						? MOVE_ITEM_DIRECTIONS.UP
-						: MOVE_ITEM_DIRECTIONS.DOWN;
-
-				if (
-					(direction === MOVE_ITEM_DIRECTIONS.UP &&
-						currentPosition === 0) ||
-					(direction === MOVE_ITEM_DIRECTIONS.DOWN &&
-						currentPosition === numChildren - 1)
-				) {
-					return;
-				}
-
-				let position;
-
-				if (direction === MOVE_ITEM_DIRECTIONS.UP) {
-					position = currentPosition - 1;
-				}
-				else if (direction === MOVE_ITEM_DIRECTIONS.DOWN) {
-					position = currentPosition + 1;
-				}
-
-				dispatch(
-					moveItem({
-						itemId,
-						parentItemId: parentId,
-						position,
-						segmentsExperienceId,
-					})
-				);
+			else if (direction === MOVE_ITEM_DIRECTIONS.DOWN) {
+				position = currentPosition + 1;
 			}
-		},
-		[activeItemId, dispatch, layoutData.items, segmentsExperienceId]
-	);
 
-	const keymap = useMemo(() => {
-		return {
-			duplicate: {
-				action: duplicate,
-				isKeyCombination: (event) =>
-					ctrlOrMeta(event) && event.keyCode === D_KEYCODE,
-			},
-			move: {
-				action: move,
-				isKeyCombination: (event) =>
-					event.keyCode === ARROW_UP_KEYCODE ||
-					event.keyCode === ARROW_DOWN_KEYCODE,
-			},
-			remove: {
-				action: remove,
-				isKeyCombination: (event) =>
-					event.keyCode === BACKSPACE_KEYCODE,
-			},
-			save: {
-				action: save,
-				isKeyCombination: (event) =>
-					ctrlOrMeta(event) && event.keyCode === S_KEYCODE,
-			},
-			undo: {
-				action: undo,
-				isKeyCombination: (event) =>
-					ctrlOrMeta(event) &&
-					event.keyCode === Z_KEYCODE &&
-					!event.altKey,
-			},
-		};
-	}, [duplicate, move, remove, save, undo]);
+			dispatch(
+				moveItem({
+					itemId,
+					parentItemId: parentId,
+					position,
+					segmentsExperienceId,
+				})
+			);
+		}
+	};
+
+	const keymapRef = useRef(null);
+
+	keymapRef.current = {
+		duplicate: {
+			action: duplicate,
+			isKeyCombination: (event) =>
+				ctrlOrMeta(event) && event.keyCode === D_KEYCODE,
+		},
+		move: {
+			action: move,
+			isKeyCombination: (event) =>
+				event.keyCode === ARROW_UP_KEYCODE ||
+				event.keyCode === ARROW_DOWN_KEYCODE,
+		},
+		remove: {
+			action: remove,
+			isKeyCombination: (event) => event.keyCode === BACKSPACE_KEYCODE,
+		},
+		save: {
+			action: save,
+			isKeyCombination: (event) =>
+				ctrlOrMeta(event) && event.keyCode === S_KEYCODE,
+		},
+		undo: {
+			action: undo,
+			isKeyCombination: (event) =>
+				ctrlOrMeta(event) &&
+				event.keyCode === Z_KEYCODE &&
+				!event.altKey,
+		},
+	};
 
 	useEffect(() => {
 		const onKeyDown = (event) => {
-			const shortcut = Object.values(keymap).find((shortcut) =>
+			const shortcut = Object.values(keymapRef.current).find((shortcut) =>
 				shortcut.isKeyCombination(event)
 			);
 
@@ -288,7 +259,7 @@ export default function ShortcutManager() {
 		return () => {
 			window.removeEventListener('keydown', onKeyDown, true);
 		};
-	}, [activeItemId, keymap]);
+	}, []);
 
 	return <SaveFragmentCompositionModal open={openSaveModal} />;
 }
