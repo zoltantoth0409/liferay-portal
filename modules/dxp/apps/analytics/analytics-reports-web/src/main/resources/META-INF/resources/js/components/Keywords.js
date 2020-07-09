8e49e33e85852589253a36f9d836ba66261058f7
Sarai Díaz García <dgarciasarai@gmail.com>
2020-07-09 17:45:59 +0200

    LPS-116791 Add select to display keywords by country

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/Keywords.js
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/Keywords.js
@@ -11,11 +11,12 @@
 
 import ClayButton from '@clayui/button';
 import ClayDropDown from '@clayui/drop-down';
+import {ClaySelect} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import ClayList from '@clayui/list';
 import {ClayTooltipProvider} from '@clayui/tooltip';
 import PropTypes from 'prop-types';
-import React, {useContext, useState} from 'react';
+import React, {useContext, useMemo, useState} from 'react';
 
 import {StoreContext} from '../context/store';
 import {numberFormat} from '../utils/numberFormat';
@@ -38,122 +39,197 @@ export default function Keywords({currentPage, languageTag}) {
 
 	const [{publishedToday}] = useContext(StoreContext);
 
+	const keywordsData = useMemo(() => {
+		const dataKeys = new Set();
+
+		const countries = currentPage.data.countryKeywords.reduce(
+			(acc, {countryCode, countryName}) => {
+				if (dataKeys.has(countryCode)) {
+					return acc;
+				}
+
+				dataKeys.add(countryCode);
+
+				return acc.concat({countryCode, countryName});
+			},
+			[]
+		);
+
+		return {countries};
+	}, [currentPage.data.countryKeywords]);
+
+	const {countries} = keywordsData;
+
+	const [currentCountry, setCurrentCountry] = useState(
+		countries[0].countryCode
+	);
+
+	const countryKeywords = useMemo(() => {
+		const country =
+			currentCountry &&
+			currentPage.data.countryKeywords.find((c) => {
+				return c.countryCode === currentCountry;
+			});
+
+		return country?.keywords ?? [];
+	}, [currentPage.data.countryKeywords, currentCountry]);
+
+	const handleCountrySelection = (event) => {
+		const country = event.target.value;
+		setCurrentCountry(country);
+	};
+
 	return (
-		<ClayList className="list-group-keywords-list">
-			<ClayList.Item flex>
-				<ClayList.ItemField expand>
-					<ClayList.ItemTitle className="text-truncate-inline">
-						<span className="text-truncate">
-							{Liferay.Language.get('best-keyword')}
-							<span className="text-secondary">
-								<Hint
-									message={Liferay.Language.get(
-										'best-keyword-help'
-									)}
-									title={Liferay.Language.get('best-keyword')}
-								/>
-							</span>
-						</span>
-					</ClayList.ItemTitle>
-				</ClayList.ItemField>
-				<ClayList.ItemField>
-					<ClayDropDown
-						active={isDropdownOpen}
-						onActiveChange={(isActive) =>
-							setIsDropdownOpen(isActive)
-						}
-						trigger={
-							<ClayButton
-								className="px-0 text-body"
-								displayType="link"
-								small
-							>
-								<span className="font-weight-semi-bold">
-									<span className="pr-2">
-										{keywordValueType.label}
-									</span>
-									<ClayIcon symbol="caret-bottom" />
-								</span>
-							</ClayButton>
-						}
-					>
-						<ClayDropDown.ItemList>
-							{KEYWORD_VALUE_TYPE.map((valueType, index) => (
-								<ClayDropDown.Item
-									active={
-										valueType.name === keywordValueType.name
-									}
-									key={index}
-									onClick={() => {
-										setKeywordValueType(
-											KEYWORD_VALUE_TYPE.find(
-												(keywordValueType) => {
-													return (
-														keywordValueType.name ===
-														valueType.name
-													);
-												}
-											)
-										);
-										setIsDropdownOpen(false);
-									}}
-								>
-									{valueType.label}
-								</ClayDropDown.Item>
-							))}
-						</ClayDropDown.ItemList>
-					</ClayDropDown>
-				</ClayList.ItemField>
-			</ClayList.Item>
-			{!publishedToday &&
-				currentPage.data.keywords.length > 0 &&
-				currentPage.data.keywords.map((k) => {
-					return (
-						<ClayList.Item flex key={k.keyword}>
-							<ClayList.ItemField expand>
-								<ClayList.ItemText>
-									<ClayTooltipProvider>
-										<span
-											className="text-truncate-inline"
-											data-tooltip-align="top"
-											title={k.keyword}
-										>
-											<span className="text-truncate">
-												{k.keyword}
-											</span>
-										</span>
-									</ClayTooltipProvider>
-								</ClayList.ItemText>
-							</ClayList.ItemField>
-							<ClayList.ItemField expand>
-								<span className="align-self-end">
-									{numberFormat(
-										languageTag,
-										keywordValueType.name === 'traffic'
-											? k.traffic
-											: keywordValueType.name === 'volume'
-											? k.searchVolume
-											: k.position
-									)}
-								</span>
-							</ClayList.ItemField>
-						</ClayList.Item>
-					);
-				})}
-			{(publishedToday || currentPage.data.keywords.length === 0) && (
+		<>
+			<h5 className="mt-3 sheet-subtitle">
+				{Liferay.Language.get('best-keywords')}
+			</h5>
+
+			<div className="mb-3 text-secondary">
+				{Liferay.Language.get('best-keywords-description')}
+			</div>
+
+			<div className="form-group">
+				<label htmlFor="countrySelect">
+					{Liferay.Language.get('select-country')}
+				</label>
+				<ClaySelect
+					aria-label={Liferay.Language.get('select-country')}
+					className="bg-white"
+					defaultValue={currentCountry}
+					id="countrySelect"
+					onChange={handleCountrySelection}
+				>
+					{countries.map((item) => (
+						<ClaySelect.Option
+							key={item.countryCode}
+							label={item.countryName}
+							value={item.countryCode}
+						/>
+					))}
+				</ClaySelect>
+			</div>
+
+			<ClayList className="list-group-keywords-list">
 				<ClayList.Item flex>
 					<ClayList.ItemField expand>
-						<ClayList.ItemText>
-							<span className="text-secondary">
-								{Liferay.Language.get(
-									'there-are-no-best-keywords-yet'
-								)}
+						<ClayList.ItemTitle className="text-truncate-inline">
+							<span className="text-truncate">
+								{Liferay.Language.get('best-keyword')}
+								<span className="text-secondary">
+									<Hint
+										message={Liferay.Language.get(
+											'best-keyword-help'
+										)}
+										title={Liferay.Language.get(
+											'best-keyword'
+										)}
+									/>
+								</span>
 							</span>
-						</ClayList.ItemText>
+						</ClayList.ItemTitle>
+					</ClayList.ItemField>
+					<ClayList.ItemField>
+						<ClayDropDown
+							active={isDropdownOpen}
+							onActiveChange={(isActive) =>
+								setIsDropdownOpen(isActive)
+							}
+							trigger={
+								<ClayButton
+									className="px-0 text-body"
+									displayType="link"
+									small
+								>
+									<span className="font-weight-semi-bold">
+										<span className="pr-2">
+											{keywordValueType.label}
+										</span>
+										<ClayIcon symbol="caret-bottom" />
+									</span>
+								</ClayButton>
+							}
+						>
+							<ClayDropDown.ItemList>
+								{KEYWORD_VALUE_TYPE.map((valueType, index) => (
+									<ClayDropDown.Item
+										active={
+											valueType.name ===
+											keywordValueType.name
+										}
+										key={index}
+										onClick={() => {
+											setKeywordValueType(
+												KEYWORD_VALUE_TYPE.find(
+													(keywordValueType) => {
+														return (
+															keywordValueType.name ===
+															valueType.name
+														);
+													}
+												)
+											);
+											setIsDropdownOpen(false);
+										}}
+									>
+										{valueType.label}
+									</ClayDropDown.Item>
+								))}
+							</ClayDropDown.ItemList>
+						</ClayDropDown>
 					</ClayList.ItemField>
 				</ClayList.Item>
-			)}
-		</ClayList>
+				{!publishedToday &&
+					countryKeywords.length > 0 &&
+					countryKeywords.map((k) => {
+						return (
+							<ClayList.Item flex key={k.keyword}>
+								<ClayList.ItemField expand>
+									<ClayList.ItemText>
+										<ClayTooltipProvider>
+											<span
+												className="text-truncate-inline"
+												data-tooltip-align="top"
+												title={k.keyword}
+											>
+												<span className="text-truncate">
+													{k.keyword}
+												</span>
+											</span>
+										</ClayTooltipProvider>
+									</ClayList.ItemText>
+								</ClayList.ItemField>
+								<ClayList.ItemField expand>
+									<span className="align-self-end">
+										{numberFormat(
+											languageTag,
+											keywordValueType.name === 'traffic'
+												? k.traffic
+												: keywordValueType.name ===
+												  'volume'
+												? k.searchVolume
+												: k.position
+										)}
+									</span>
+								</ClayList.ItemField>
+							</ClayList.Item>
+						);
+					})}
+				{(publishedToday || countryKeywords.length === 0) && (
+					<ClayList.Item flex>
+						<ClayList.ItemField expand>
+							<ClayList.ItemText>
+								<span className="text-secondary">
+									{Liferay.Language.get(
+										'there-are-no-best-keywords-yet'
+									)}
+								</span>
+							</ClayList.ItemText>
+						</ClayList.ItemField>
+					</ClayList.Item>
+				)}
+			</ClayList>
+		</>
 	);
 }
 
