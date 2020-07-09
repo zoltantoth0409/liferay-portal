9b8956bf8b593934ca22192a4c00d4055a962154
drewbrokke <drew.brokke@liferay.com>
2020-07-09 13:26:20 -0500

    LPS-115260 account-admin-web: sets the modal state to open when clicking the add button in the modal

--- modules/apps/account/account-admin-web/src/main/resources/META-INF/resources/account_entries_admin/js/SelectAccountUsersManagementToolbarDefaultEventHandler.es.js
+++ modules/apps/account/account-admin-web/src/main/resources/META-INF/resources/account_entries_admin/js/SelectAccountUsersManagementToolbarDefaultEventHandler.es.js
@@ -13,9 +13,12 @@
  */
 
 import {DefaultEventHandler} from 'frontend-js-web';
+import {MODAL_STATE_ACCOUNT_USERS} from './SessionStorageKeys.es';
 
 class SelectAccountUsersManagementToolbarDefaultEventHandler extends DefaultEventHandler {
 	addAccountEntryUser({addAccountEntryUserURL}) {
+		window.sessionStorage.setItem(MODAL_STATE_ACCOUNT_USERS, 'open')
+
 		Liferay.Util.getTop().location.href = addAccountEntryUserURL;
 	}
 }
