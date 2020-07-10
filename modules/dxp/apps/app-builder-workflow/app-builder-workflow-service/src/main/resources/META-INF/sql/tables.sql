334f9a9ddf05a41233c136b564d866ff1c92bf0f
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-07-10 10:33:18 -0300

    LPS-116717 | LPS-112434 Fix generated *.sql

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,11 +1,3 @@
-create table AppBuilderAppWorkflowTaskLink (
-	abAppWorkflowTaskLinkId LONG not null primary key,
-	companyId LONG,
-	appBuilderAppId LONG,
-	ddmStructureLayoutId LONG,
-	workflowTaskName VARCHAR(75) null
-);
-
 create table AppBuilderWorkflowTaskLink (
 	mvccVersion LONG default 0 not null,
 	appBuilderWorkflowTaskLinkId LONG not null primary key,
