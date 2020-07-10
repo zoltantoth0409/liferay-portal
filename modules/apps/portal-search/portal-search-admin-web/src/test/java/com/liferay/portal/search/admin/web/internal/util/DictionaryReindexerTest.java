2b80c00e61f3ac81a6cbc1c9d86e4aa2ce175a68
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2020-07-10 20:10:53 -0300

    LPS-100169 portal-search-admin-web: Test for DictionaryReindexer
    https://issues.liferay.com/browse/LPS-100169

---
+++ modules/apps/portal-search/portal-search-admin-web/src/test/java/com/liferay/portal/search/admin/web/internal/util/DictionaryReindexerTest.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.admin.web.internal.util;
+
+import com.liferay.portal.instances.service.PortalInstancesLocalService;
+import com.liferay.portal.kernel.search.IndexWriterHelper;
+import com.liferay.portal.kernel.search.SearchException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+/**
+ * @author Adam Brandizzi
+ */
+public class DictionaryReindexerTest {
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+
+		Mockito.when(
+			_portalInstancesLocalService.getCompanyIds()
+		).thenReturn(
+			_COMPANY_IDS
+		);
+	}
+
+	@Test
+	public void testReindexAllCompaniesDictionaries() throws SearchException {
+		DictionaryReindexer dictionaryReindexer = new DictionaryReindexer(
+			_indexWriterHelper, _portalInstancesLocalService);
+
+		dictionaryReindexer.reindexDictionaries();
+
+		for (long companyId : _COMPANY_IDS) {
+			assertIndexWriterHelperReindexDictionariesWithCompanyId(companyId);
+		}
+	}
+
+	protected void assertIndexWriterHelperReindexDictionariesWithCompanyId(
+			long companyId)
+		throws SearchException {
+
+		Mockito.verify(
+			_indexWriterHelper
+		).indexSpellCheckerDictionaries(
+			companyId
+		);
+
+		Mockito.verify(
+			_indexWriterHelper
+		).indexQuerySuggestionDictionaries(
+			companyId
+		);
+	}
+
+	private static final long[] _COMPANY_IDS = {1001L, 2002L};
+
+	@Spy
+	private final IndexWriterHelper _indexWriterHelper = Mockito.mock(
+		IndexWriterHelper.class);
+
+	@Mock
+	private PortalInstancesLocalService _portalInstancesLocalService;
+
+}
