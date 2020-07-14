5fc445e5de4d1ea5250a1efd5896430343f72d80
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-07-14 15:17:08 +0200

    LPS-116444 Adding XLIFF 1.2 exporter

---
+++ modules/apps/translation/translation-test/src/testIntegration/java/com/liferay/translation/exporter/test/XLIFF12TranslationInfoItemFieldValuesExporterTest.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.translation.exporter.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormDeserializerDeserializeResponse;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.petra.io.StreamUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.translation.exporter.TranslationInfoItemFieldValuesExporter;
+import com.liferay.translation.test.util.TranslationTestUtil;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alejandro Tardín
+ */
+@RunWith(Arquillian.class)
+public class XLIFF12TranslationInfoItemFieldValuesExporterTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule testRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testExportReturnsTheXLIFFRepresentationOfAJournalArticle()
+		throws Exception {
+
+		InfoItemFieldValuesProvider<JournalArticle>
+			infoItemFieldValuesProvider =
+				(InfoItemFieldValuesProvider<JournalArticle>)
+					_infoItemServiceTracker.getFirstInfoItemService(
+						InfoItemFieldValuesProvider.class,
+						JournalArticle.class.getName());
+
+		JournalArticle journalArticle = _getJournalArticle();
+
+		Assert.assertEquals(
+			StringUtil.replace(
+				TranslationTestUtil.readFileToString(
+					"test-journal-article-v12.xlf"),
+				"$ARTICLE_ID",
+				String.valueOf(journalArticle.getResourcePrimKey())),
+			StreamUtil.toString(
+				_xliffTranslationInfoItemFieldValuesExporter.
+					exportInfoItemFieldValues(
+						infoItemFieldValuesProvider.getInfoItemFieldValues(
+							journalArticle),
+						LocaleUtil.getDefault(),
+						LocaleUtil.fromLanguageId("es_ES"))));
+	}
+
+	private JournalArticle _getJournalArticle() throws Exception {
+		DDMFormDeserializerDeserializeRequest.Builder builder =
+			DDMFormDeserializerDeserializeRequest.Builder.newBuilder(
+				TranslationTestUtil.readFileToString("test-ddm-form.json"));
+
+		DDMFormDeserializerDeserializeResponse
+			ddmFormDeserializerDeserializeResponse =
+				_ddmFormDeserializer.deserialize(builder.build());
+
+		DDMForm ddmForm = ddmFormDeserializerDeserializeResponse.getDDMForm();
+
+		DDMStructure ddmStructure = DDMStructureTestUtil.addStructure(
+			_group.getGroupId(), JournalArticle.class.getName(), ddmForm);
+
+		return JournalTestUtil.addArticleWithXMLContent(
+			_group.getGroupId(),
+			TranslationTestUtil.readFileToString("test-journal-content.xml"),
+			ddmStructure.getStructureKey(), null);
+	}
+
+	@Inject(filter = "ddm.form.deserializer.type=json")
+	private DDMFormDeserializer _ddmFormDeserializer;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private InfoItemServiceTracker _infoItemServiceTracker;
+
+	@Inject(filter = "content.type=application/x-xliff+xml")
+	private TranslationInfoItemFieldValuesExporter
+		_xliffTranslationInfoItemFieldValuesExporter;
+
+}
---
+++ modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/exporter/XLIFF12InfoFormTranslationExporter.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.translation.web.internal.exporter;
+
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.item.InfoItemClassPKReference;
+import com.liferay.info.item.InfoItemFieldValues;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
+import com.liferay.translation.exporter.TranslationInfoItemFieldValuesExporter;
+import com.liferay.translation.info.field.TranslationInfoFieldChecker;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.Collection;
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	property = "content.type=application/x-xliff+xml",
+	service = TranslationInfoItemFieldValuesExporter.class
+)
+public class XLIFF12InfoFormTranslationExporter
+	implements TranslationInfoItemFieldValuesExporter {
+
+	@Override
+	public InputStream exportInfoItemFieldValues(
+			InfoItemFieldValues infoItemFieldValues, Locale sourceLocale,
+			Locale targetLocale)
+		throws IOException {
+
+		Document document = SAXReaderUtil.createDocument();
+
+		Element xliffElement = document.addElement(
+			"xliff", "urn:oasis:names:tc:xliff:document:1.2");
+
+		xliffElement.addAttribute("version", "1.2");
+
+		Element fileElement = xliffElement.addElement("file");
+
+		fileElement.addAttribute("datatype", "plaintext");
+
+		InfoItemClassPKReference infoItemClassPKReference =
+			infoItemFieldValues.getInfoItemClassPKReference();
+
+		fileElement.addAttribute(
+			"original",
+			infoItemClassPKReference.getClassName() + StringPool.COLON +
+				infoItemClassPKReference.getClassPK());
+
+		fileElement.addAttribute(
+			"source-language", LocaleUtil.toBCP47LanguageId(sourceLocale));
+		fileElement.addAttribute(
+			"target-language", LocaleUtil.toBCP47LanguageId(targetLocale));
+		fileElement.addAttribute("tool", "Liferay");
+
+		Element bodyElement = fileElement.addElement("body");
+
+		Collection<InfoFieldValue<Object>> infoFieldValues =
+			infoItemFieldValues.getInfoFieldValues();
+
+		for (InfoFieldValue<Object> infoFieldValue : infoFieldValues) {
+			InfoField infoField = infoFieldValue.getInfoField();
+
+			if (!_translationInfoFieldChecker.isTranslatable(infoField)) {
+				continue;
+			}
+
+			Object sourceValue = infoFieldValue.getValue(sourceLocale);
+
+			if (Validator.isNull(sourceValue)) {
+				continue;
+			}
+
+			Element transUnitElement = bodyElement.addElement("trans-unit");
+
+			transUnitElement.addAttribute("id", infoField.getName());
+
+			Element sourceElement = transUnitElement.addElement("source");
+
+			sourceElement.addAttribute(
+				"xml:lang", fileElement.attributeValue("source-language"));
+			sourceElement.addCDATA(_getStringValue(sourceValue));
+
+			Element targetElement = transUnitElement.addElement("target");
+
+			targetElement.addAttribute(
+				"xml:lang", fileElement.attributeValue("target-language"));
+			targetElement.addCDATA(
+				_getStringValue(infoFieldValue.getValue(targetLocale)));
+		}
+
+		String formattedString = document.formattedString();
+
+		return new ByteArrayInputStream(formattedString.getBytes());
+	}
+
+	@Override
+	public String getMimeType() {
+		return "application/x-xliff+xml";
+	}
+
+	private String _getStringValue(Object value) {
+		if (value == null) {
+			return null;
+		}
+
+		return value.toString();
+	}
+
+	@Reference
+	private TranslationInfoFieldChecker _translationInfoFieldChecker;
+
+}
