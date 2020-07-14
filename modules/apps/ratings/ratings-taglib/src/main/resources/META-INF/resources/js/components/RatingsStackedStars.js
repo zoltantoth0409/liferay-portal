7bd040119003d34079d1ab4fa4a5f60837c32104
Jorge González <jorge.gonzalez@liferay.com>
2020-07-14 12:06:55 +0200

    LPS-116822 Use ClayTooltipProvider instead of lfr-portal-tooltip avoiding wired jumpings

--- modules/apps/ratings/ratings-taglib/src/main/resources/META-INF/resources/js/components/RatingsStackedStars.js
+++ modules/apps/ratings/ratings-taglib/src/main/resources/META-INF/resources/js/components/RatingsStackedStars.js
@@ -14,6 +14,7 @@
 
 import ClayButton from '@clayui/button';
 import ClayIcon from '@clayui/icon';
+import {ClayTooltipProvider} from '@clayui/tooltip';
 import classNames from 'classnames';
 import React, {Fragment} from 'react';
 
@@ -32,118 +33,125 @@ export default function RatingsStackedStars({
 	totalEntries,
 }) {
 	return (
-		<fieldset className="ratings-stacked-stars ratings-stars">
-			<div
-				className="ratings-stars-average"
-				title={averageScore.toFixed(1)}
-			>
-				<span className="inline-item inline-item-before">
-					{starScores.map(({label: score}, index, arr) => {
-						const previousScore =
-							arr[index - 1] && arr[index - 1].label;
-						let symbol = 'star-o';
-
-						if (averageScore >= score) {
-							symbol = 'star';
-						}
-						else if (averageScore > previousScore) {
-							symbol = 'star-half';
-						}
-
-						return (
-							<ClayIcon
-								className="ratings-stars-average-icon"
-								key={score}
-								symbol={symbol}
-							/>
-						);
-					})}
-				</span>
-				<span className="inline-item ratings-stars-average-text">
-					{!!totalEntries &&
-						` (${totalEntries} ${
-							totalEntries === 1
-								? Liferay.Language.get('vote')
-								: Liferay.Language.get('votes')
-						})`}
-				</span>
-				<span className="sr-only">{getSrAverageMessage()}</span>
-			</div>
-
-			<div
-				className={classNames({disabled}, 'ratings-stacked-stars-vote')}
-			>
+		<ClayTooltipProvider>
+			<fieldset className="ratings-stacked-stars ratings-stars">
 				<div
-					className="lfr-portal-tooltip ratings-stacked-stars-vote-stars"
-					title={getTitle()}
+					className="ratings-stars-average"
+					title={averageScore.toFixed(1)}
 				>
-					{starScores.reverse().map(({label, value}, index) => {
-						const id = `${randomNamespace}star${label}`;
-						const srMessage =
-							index === 0
-								? Liferay.Language.get(
-										'rate-this-x-star-out-of-x'
-								  )
-								: Liferay.Language.get(
-										'rate-this-x-stars-out-of-x'
-								  );
-						const full = label <= score;
+					<span className="inline-item inline-item-before">
+						{starScores.map(({label: score}, index, arr) => {
+							const previousScore =
+								arr[index - 1] && arr[index - 1].label;
+							let symbol = 'star-o';
 
-						return (
-							<Fragment key={value}>
-								<input
-									checked={label === score}
-									className="sr-only"
-									disabled={disabled}
-									id={id}
-									name={`${randomNamespace}rating`}
-									onChange={() => {
-										onVote(index);
-									}}
-									type="radio"
-									value={value}
+							if (averageScore >= score) {
+								symbol = 'star';
+							}
+							else if (averageScore > previousScore) {
+								symbol = 'star-half';
+							}
+
+							return (
+								<ClayIcon
+									className="ratings-stars-average-icon"
+									key={score}
+									symbol={symbol}
 								/>
-								<label
-									className={
-										full ? 'ratings-stars-star-full' : ''
-									}
-									htmlFor={id}
-								>
-									<ClayIcon
-										className="ratings-stars-icon-full"
-										symbol={'star'}
-									/>
-									<ClayIcon
-										className="ratings-stars-icon-empty"
-										symbol={'star-o'}
-									/>
-									<span className="sr-only">
-										{Lang.sub(srMessage, [
-											label,
-											numberOfStars,
-										])}
-									</span>
-								</label>
-							</Fragment>
-						);
-					})}
+							);
+						})}
+					</span>
+					<span className="inline-item ratings-stars-average-text">
+						{!!totalEntries &&
+							` (${totalEntries} ${
+								totalEntries === 1
+									? Liferay.Language.get('vote')
+									: Liferay.Language.get('votes')
+							})`}
+					</span>
+					<span className="sr-only">{getSrAverageMessage()}</span>
 				</div>
 
-				{score !== 0 && (
-					<ClayButton
-						className="lfr-portal-tooltip ratings-stacked-stars-delete"
-						disabled={disabled}
-						displayType="unstyled"
-						onClick={onVote}
-						title={Liferay.Language.get('delete')}
+				<div
+					className={classNames(
+						{disabled},
+						'ratings-stacked-stars-vote'
+					)}
+				>
+					<div
+						className="ratings-stacked-stars-vote-stars"
+						title={getTitle()}
 					>
-						<ClayIcon
-							className="lexicon-icon-vertical-align"
-							symbol="times-circle"
-						/>
-					</ClayButton>
-				)}
-			</div>
-		</fieldset>
+						{starScores.reverse().map(({label, value}, index) => {
+							const id = `${randomNamespace}star${label}`;
+							const srMessage =
+								index === 0
+									? Liferay.Language.get(
+											'rate-this-x-star-out-of-x'
+									  )
+									: Liferay.Language.get(
+											'rate-this-x-stars-out-of-x'
+									  );
+							const full = label <= score;
+
+							return (
+								<Fragment key={value}>
+									<input
+										checked={label === score}
+										className="sr-only"
+										disabled={disabled}
+										id={id}
+										name={`${randomNamespace}rating`}
+										onChange={() => {
+											onVote(index);
+										}}
+										type="radio"
+										value={value}
+									/>
+									<label
+										className={
+											full
+												? 'ratings-stars-star-full'
+												: ''
+										}
+										htmlFor={id}
+									>
+										<ClayIcon
+											className="ratings-stars-icon-full"
+											symbol={'star'}
+										/>
+										<ClayIcon
+											className="ratings-stars-icon-empty"
+											symbol={'star-o'}
+										/>
+										<span className="sr-only">
+											{Lang.sub(srMessage, [
+												label,
+												numberOfStars,
+											])}
+										</span>
+									</label>
+								</Fragment>
+							);
+						})}
+					</div>
+
+					{score !== 0 && (
+						<ClayButton
+							className="ratings-stacked-stars-delete"
+							disabled={disabled}
+							displayType="unstyled"
+							onClick={onVote}
+							title={Liferay.Language.get('delete')}
+						>
+							<ClayIcon
+								className="lexicon-icon-vertical-align"
+								symbol="times-circle"
+							/>
+						</ClayButton>
+					)}
+				</div>
+			</fieldset>
+		</ClayTooltipProvider>
 	);
 }
