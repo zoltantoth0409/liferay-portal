ce30c1a60e9f98d1a2caa5b3100a7eeea50cf6cb
drewbrokke <drew.brokke@liferay.com>
2020-07-14 09:37:14 -0500

    LPS-116757 configuration-admin-web: moves tracking of ConfigurationVisibilityController to a new static util

---
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/util/ConfigurationVisibilityUtil.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.configuration.admin.web.internal.util;
+
+import com.liferay.configuration.admin.display.ConfigurationVisibilityController;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+import java.io.Serializable;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Drew Brokke
+ */
+public class ConfigurationVisibilityUtil {
+
+	public static boolean isVisible(
+		String pid, ExtendedObjectClassDefinition.Scope scope,
+		Serializable scopePK) {
+
+		ConfigurationVisibilityController configurationVisibilityController =
+			_getVisibilityController(pid);
+
+		return configurationVisibilityController.isVisible(scope, scopePK);
+	}
+
+	private static ConfigurationVisibilityController _getVisibilityController(
+		String pid) {
+
+		if (_serviceTrackerMap.containsKey(pid)) {
+			return _serviceTrackerMap.getService(pid);
+		}
+
+		return _defaultVisibilityController;
+	}
+
+	private static final ConfigurationVisibilityController
+		_defaultVisibilityController = (scope, scopePK) -> true;
+	private static final ServiceTrackerMap
+		<String, ConfigurationVisibilityController> _serviceTrackerMap;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(
+			ConfigurationVisibilityController.class);
+
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundle.getBundleContext(), ConfigurationVisibilityController.class,
+			"configuration.pid");
+	}
+
+}
