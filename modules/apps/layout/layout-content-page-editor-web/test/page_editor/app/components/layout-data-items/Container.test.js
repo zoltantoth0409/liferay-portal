8aee300cee9c94938784bbb06623310e98b718a2
Pablo Molina <contact@pablomolina.me>
2020-07-14 15:13:14 +0200

    LPS-116923 Add container test for link configuration

---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/layout-data-items/Container.test.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+
+import Container from '../../../../../src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Container';
+import {LAYOUT_DATA_ITEM_TYPES} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/config/constants/layoutDataItemTypes';
+import {StoreAPIContextProvider} from '../../../../../src/main/resources/META-INF/resources/page_editor/app/store';
+
+const renderContainer = (config) => {
+	return render(
+		<StoreAPIContextProvider getState={() => ({languageId: 'en'})}>
+			<Container
+				data={{}}
+				item={{
+					children: [],
+					config: {...config},
+					itemId: 'containerId',
+					type: LAYOUT_DATA_ITEM_TYPES.container,
+				}}
+				withinTopper={false}
+			>
+				Container
+			</Container>
+		</StoreAPIContextProvider>
+	);
+};
+
+describe('Container', () => {
+	afterEach(cleanup);
+
+	it('wraps the container inside a link if configuration is specified', () => {
+		const link = renderContainer({
+			link: {
+				href: 'https://sandro.vero.victor.com',
+				target: '_blank',
+			},
+		}).getByRole('link');
+
+		expect(link.href).toBe('https://sandro.vero.victor.com/');
+		expect(link.target).toBe('_blank');
+		expect(link.textContent).toBe('Container');
+	});
+});
