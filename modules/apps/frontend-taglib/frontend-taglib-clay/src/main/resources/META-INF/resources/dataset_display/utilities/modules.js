35f6330ba3956a056f80c6b024b237acf7a243d7
Bruno Basto <bruno.basto@liferay.com>
2020-07-15 11:41:47 -0300

    LPS-117125 Avoids abbreviations

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/dataset_display/utilities/modules.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/dataset_display/utilities/modules.js
@@ -19,7 +19,7 @@ export function getLiferayJsModule(moduleUrl) {
 		Liferay.Loader.require(
 			moduleUrl,
 			(jsModule) => resolve(jsModule.default || jsModule),
-			(err) => reject(err)
+			(error) => reject(error)
 		);
 	});
 }
