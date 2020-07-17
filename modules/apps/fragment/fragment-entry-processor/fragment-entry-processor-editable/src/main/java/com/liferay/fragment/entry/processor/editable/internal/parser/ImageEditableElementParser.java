d4890cefc52fd3c2cc8bba656019703fddc5f107
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-07-17 10:03:19 +0200

    LPS-117275 In the case of display pages add a new configuration property so that we can check later on if we should show it or not

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/parser/ImageEditableElementParser.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/parser/ImageEditableElementParser.java
@@ -37,6 +37,7 @@ import com.liferay.portal.kernel.util.Validator;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -88,7 +89,11 @@ public class ImageEditableElementParser implements EditableElementParser {
 			}
 		}
 
-		return JSONUtil.put("alt", alt);
+		return JSONUtil.put(
+			"alt", alt
+		).put(
+			"altTemplate", _TMPL_IMAGE_FIELD_ALT_TEMPLATE
+		);
 	}
 
 	@Override
@@ -175,8 +180,11 @@ public class ImageEditableElementParser implements EditableElementParser {
 		}
 
 		String alt = configJSONObject.getString("alt");
+		String altTemplate = configJSONObject.getString("altTemplate");
 
-		if (Validator.isNotNull(alt)) {
+		if (Validator.isNotNull(alt) &&
+			!Objects.equals(altTemplate, _TMPL_IMAGE_FIELD_ALT_TEMPLATE)) {
+
 			replaceableElement.attr(
 				"alt", StringUtil.trim(_html.unescape(alt)));
 		}
