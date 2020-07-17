c2196117f7034a987b88ce51d283001ad5a35ba0
Marta Medio <marta.medio@liferay.com>
2020-07-17 10:00:41 +0200

    LPS-115803 Check AntiSamy configuration file and show an error if it's not found

---
+++ modules/apps/portal-security/portal-security-antisamy/src/main/java/com/liferay/portal/security/antisamy/internal/configuration/persistence/listener/AntiSamyClassNameConfigurationListener.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.antisamy.internal.configuration.persistence.listener;
+
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListenerException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.security.antisamy.configuration.AntiSamyClassNameConfiguration;
+
+import java.net.URL;
+
+import java.util.Dictionary;
+import java.util.ResourceBundle;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marta Medio
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.portal.security.antisamy.configuration.AntiSamyClassNameConfiguration",
+	service = ConfigurationModelListener.class
+)
+public class AntiSamyClassNameConfigurationListener
+	implements ConfigurationModelListener {
+
+	@Override
+	public void onBeforeSave(String pid, Dictionary<String, Object> properties)
+		throws ConfigurationModelListenerException {
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			AntiSamyClassNameConfiguration.class);
+
+		String configurationFileURL = GetterUtil.getString(
+			properties.get("configurationFileURL"));
+
+		URL url = bundle.getResource(configurationFileURL);
+
+		if (url == null) {
+			throw new ConfigurationModelListenerException(
+				ResourceBundleUtil.getString(
+					_getResourceBundle(), "configuration-file-not-found",
+					configurationFileURL),
+				AntiSamyClassNameConfiguration.class, getClass(), properties);
+		}
+	}
+
+	private ResourceBundle _getResourceBundle() {
+		return ResourceBundleUtil.getBundle(
+			LocaleThreadLocal.getThemeDisplayLocale(), getClass());
+	}
+
+}
