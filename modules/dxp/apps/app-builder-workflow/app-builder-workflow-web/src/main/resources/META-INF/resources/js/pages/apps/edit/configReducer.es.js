d913960f6434249f32d48bed2d413e3fd2c150fb
Gabriel Ramos <gabriel.ramos@liferay.com>
2020-07-17 17:37:43 -0300

    LPS-116467 | LPS-110982 Create REMOVE_STEP action in configReducer

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/apps/edit/configReducer.es.js
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/src/main/resources/META-INF/resources/js/pages/apps/edit/configReducer.es.js
@@ -16,6 +16,7 @@ import {getFormViewFields, validateSelectedFormViews} from './utils.es';
 export const ADD_STEP = 'ADD_STEP';
 export const ADD_STEP_ACTION = 'ADD_STEP_ACTION';
 export const ADD_STEP_FORM_VIEW = 'ADD_STEP_FORM_VIEW';
+export const REMOVE_STEP = 'REMOVE_STEP';
 export const REMOVE_STEP_ACTION = 'REMOVE_STEP_ACTION';
 export const REMOVE_STEP_FORM_VIEW = 'REMOVE_STEP_FORM_VIEW';
 export const UPDATE_CONFIG = 'UPDATE_CONFIG';
@@ -109,13 +110,13 @@ export default (state, action) => {
 						readOnly: true,
 					})
 				);
-
-				previousStep.appWorkflowTransitions.forEach((action) => {
-					if (action.primary) {
-						action.transitionTo = currentStep.name;
-					}
-				});
 			}
+
+			previousStep.appWorkflowTransitions.forEach((action) => {
+				if (action.primary) {
+					action.transitionTo = currentStep.name;
+				}
+			});
 
 			if (nextStep) {
 				nextStep.appWorkflowTransitions.forEach((action) => {
@@ -151,6 +152,49 @@ export default (state, action) => {
 			});
 
 			return {...state};
+		}
+		case REMOVE_STEP: {
+			let currentStep;
+			const workflowSteps = [...state.steps];
+			const previousStep = workflowSteps[action.stepIndex - 1];
+			const nextStep = workflowSteps[action.stepIndex + 1];
+
+			previousStep.appWorkflowTransitions[0].transitionTo = nextStep.name;
+
+			if (nextStep?.appWorkflowTransitions?.length > 1) {
+				if (action.stepIndex > 1) {
+					nextStep.appWorkflowTransitions.forEach((transition) => {
+						if (!transition.primary) {
+							transition.transitionTo = previousStep.name;
+						}
+					});
+				}
+				else {
+					nextStep.appWorkflowTransitions.forEach(
+						(transition, index) => {
+							if (!transition.primary) {
+								nextStep.appWorkflowTransitions.splice(
+									index,
+									1
+								);
+							}
+						}
+					);
+				}
+			}
+
+			if (state.stepIndex === action.stepIndex) {
+				currentStep = previousStep;
+			}
+
+			workflowSteps.splice(action.stepIndex, 1);
+
+			return {
+				...state,
+				currentStep,
+				stepIndex: state.stepIndex - 1,
+				steps: [...workflowSteps],
+			};
 		}
 		case REMOVE_STEP_ACTION: {
 			state.steps[state.stepIndex].appWorkflowTransitions.pop();
