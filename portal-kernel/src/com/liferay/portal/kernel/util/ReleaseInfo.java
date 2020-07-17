a2ffbb6f76cbaa738657c2b36fd5c1eb0387d37c
jpince <jason.pince@liferay.com>
2020-07-17 15:25:46 -0700

    RELEASE-2765 Release Info for GA5

--- portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -66,6 +66,8 @@ public class ReleaseInfo {
 
 	public static final int RELEASE_7_3_3_BUILD_NUMBER = 7303;
 
+	public static final int RELEASE_7_3_4_BUILD_NUMBER = 7304;
+
 	public static final int RELEASE_7_3_10_BUILD_NUMBER = 7310;
 
 	public static final Date getBuildDate() {
