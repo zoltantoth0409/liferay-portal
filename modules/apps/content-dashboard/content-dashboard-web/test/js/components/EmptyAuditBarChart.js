c305d83c42e929906a453c1a6df57eb8a7b5a7a4
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-07-18 10:13:45 +0100

    LPS-117015 Update tests

--- modules/apps/content-dashboard/content-dashboard-web/test/js/components/EmptyAuditBarChart.js
+++ modules/apps/content-dashboard/content-dashboard-web/test/js/components/EmptyAuditBarChart.js
@@ -19,13 +19,18 @@ import EmptyAuditBarChart from '../../../src/main/resources/META-INF/resources/j
 
 import '@testing-library/jest-dom/extend-expect';
 
+const LEARN_HOW_URL =
+	'https://help.liferay.com/hc/en-us/articles/360028820492-Defining-Categories-for-Content';
+
 describe('EmptyAuditBarChart', () => {
 	Liferay.Util.sub.mockImplementation((langKey) => langKey);
 
 	afterEach(cleanup);
 
 	it('renders empty bar chart if there is no content labeled with categories', () => {
-		const {getByText} = render(<EmptyAuditBarChart />);
+		const {getByText} = render(
+			<EmptyAuditBarChart learnHowURL={LEARN_HOW_URL} />
+		);
 
 		expect(getByText('there-is-no-data')).toBeInTheDocument();
 		expect(
