da1900218d6079609a1a253d695541b561aec6fc
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-07-20 13:20:46 +0200

    LPS-117481 Adding action command

---
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/action/UpdateDDMStructuresAvailableMVCActionCommand.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.depot.web.internal.portlet.action;
+
+import com.liferay.depot.service.DepotEntryGroupRelService;
+import com.liferay.depot.web.internal.constants.DepotPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + DepotPortletKeys.DEPOT_ADMIN,
+		"javax.portlet.name=" + DepotPortletKeys.DEPOT_SETTINGS,
+		"mvc.command.name=/depot/update_depot_entry_group_rel_ddm_structures_available"
+	},
+	service = MVCActionCommand.class
+)
+public class UpdateDDMStructuresAvailableMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws PortletException {
+
+		try {
+			_depotEntryGroupRelService.updateDDMStructuresAvailable(
+				ParamUtil.getLong(actionRequest, "depotEntryGroupRelId"),
+				ParamUtil.getBoolean(actionRequest, "ddmStructuresAvailable"));
+		}
+		catch (PortalException portalException) {
+			throw new PortletException(portalException);
+		}
+	}
+
+	@Reference
+	private DepotEntryGroupRelService _depotEntryGroupRelService;
+
+}
