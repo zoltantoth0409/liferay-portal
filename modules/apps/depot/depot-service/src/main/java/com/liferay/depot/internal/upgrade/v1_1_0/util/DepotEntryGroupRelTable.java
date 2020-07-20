606a9523b71ec72054c00258a225aed47a2583cf
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-07-20 13:40:07 +0200

    LPS-117481 Autogenerated

--- modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/upgrade/v1_1_0/util/DepotEntryGroupRelTable.java
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/internal/upgrade/v1_1_0/util/DepotEntryGroupRelTable.java
@@ -14,8 +14,54 @@
 
 package com.liferay.depot.internal.upgrade.v1_1_0.util;
 
+import java.sql.Types;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * @author Alejandro Tardín
+ * @author	  Alejandro Tardín
+ * @generated
  */
 public class DepotEntryGroupRelTable {
+
+	public static final String TABLE_NAME = "DepotEntryGroupRel";
+
+	public static final Object[][] TABLE_COLUMNS = {
+		{"mvccVersion", Types.BIGINT}, {"depotEntryGroupRelId", Types.BIGINT},
+		{"companyId", Types.BIGINT}, {"ddmStructuresAvailable", Types.BOOLEAN},
+		{"depotEntryId", Types.BIGINT}, {"searchable", Types.BOOLEAN},
+		{"toGroupId", Types.BIGINT}
+	};
+
+	public static final Map<String, Integer> TABLE_COLUMNS_MAP =
+new HashMap<String, Integer>();
+
+static {
+TABLE_COLUMNS_MAP.put("mvccVersion", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("depotEntryGroupRelId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("companyId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("ddmStructuresAvailable", Types.BOOLEAN);
+
+TABLE_COLUMNS_MAP.put("depotEntryId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("searchable", Types.BOOLEAN);
+
+TABLE_COLUMNS_MAP.put("toGroupId", Types.BIGINT);
+
+}
+	public static final String TABLE_SQL_CREATE =
+"create table DepotEntryGroupRel (mvccVersion LONG default 0 not null,depotEntryGroupRelId LONG not null primary key,companyId LONG,ddmStructuresAvailable BOOLEAN,depotEntryId LONG,searchable BOOLEAN,toGroupId LONG)";
+
+	public static final String TABLE_SQL_DROP = "drop table DepotEntryGroupRel";
+
+	public static final String[] TABLE_SQL_ADD_INDEXES = {
+		"create unique index IX_65D34444 on DepotEntryGroupRel (depotEntryId, toGroupId)",
+		"create index IX_C61C803B on DepotEntryGroupRel (searchable, toGroupId)",
+		"create index IX_DB75E9F1 on DepotEntryGroupRel (toGroupId)"
+	};
+
 }
