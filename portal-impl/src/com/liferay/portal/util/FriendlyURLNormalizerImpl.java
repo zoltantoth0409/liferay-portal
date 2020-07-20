2911494db2825fff64a2c6b41b0dd69bfadc75c9
Laszlo Pap <laszlo.pap@liferay.com>
2020-07-20 16:58:41 +0200

    LPS-117484 Creating a Web Content with a '%' and space character in the title results in 'is not a hex char' errors

--- portal-impl/src/com/liferay/portal/util/FriendlyURLNormalizerImpl.java
+++ portal-impl/src/com/liferay/portal/util/FriendlyURLNormalizerImpl.java
@@ -48,13 +48,9 @@ public class FriendlyURLNormalizerImpl implements FriendlyURLNormalizer {
 			return friendlyURL;
 		}
 
-		String decodedFriendlyURL = HttpUtil.decodePath(friendlyURL);
-
-		if (Validator.isNull(decodedFriendlyURL)) {
-			decodedFriendlyURL = HttpUtil.decodePath(
-				StringUtil.replace(
-					friendlyURL, CharPool.PERCENT, CharPool.POUND));
-		}
+		String decodedFriendlyURL = HttpUtil.decodePath(
+			StringUtil.replace(
+				friendlyURL, CharPool.PERCENT, CharPool.POUND));
 
 		StringBuilder sb = new StringBuilder(decodedFriendlyURL.length());
 
