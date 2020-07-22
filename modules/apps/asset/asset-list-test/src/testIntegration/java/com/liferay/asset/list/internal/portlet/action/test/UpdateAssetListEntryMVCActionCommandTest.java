fd3b48e08ed0d45c6b0cfab7be1c24f403da79b3
Yang Cao <yang.cao@liferay.com>
2020-07-23 06:00:05 +0800

    LPS-117530 Add case to test update asset list entry title

--- modules/apps/asset/asset-list-test/src/testIntegration/java/com/liferay/asset/list/internal/portlet/action/test/UpdateAssetListEntryMVCActionCommandTest.java
+++ modules/apps/asset/asset-list-test/src/testIntegration/java/com/liferay/asset/list/internal/portlet/action/test/UpdateAssetListEntryMVCActionCommandTest.java
@@ -15,15 +15,36 @@
 package com.liferay.asset.list.internal.portlet.action.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.asset.list.service.AssetListEntryLocalService;
+import com.liferay.asset.list.util.AssetListTestUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.io.Writer;
+
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.MutableRenderParameters;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderURL;
+import javax.portlet.WindowState;
+import javax.portlet.annotations.PortletSerializable;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -52,14 +73,141 @@ public class UpdateAssetListEntryMVCActionCommandTest {
 	}
 
 	@Test
-	public void testUpdateAssetListEntryTitle() {
-		Assert.assertTrue(true);
+	public void testUpdateAssetListEntryTitle() throws PortalException {
+		AssetListEntry assetListEntry = AssetListTestUtil.addAssetListEntry(
+			_group.getGroupId());
+
+		String title = RandomTestUtil.randomString();
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest();
+
+		mockLiferayPortletActionRequest.addParameter(
+			"assetListEntryId",
+			String.valueOf(assetListEntry.getAssetListEntryId()));
+
+		mockLiferayPortletActionRequest.addParameter("title", title);
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "doProcessAction",
+			new Class<?>[] {ActionRequest.class, ActionResponse.class},
+			mockLiferayPortletActionRequest, new MockActionResponse());
+
+		AssetListEntry persistedAssetListEntry =
+			_assetListEntryLocalService.getAssetListEntry(
+				assetListEntry.getAssetListEntryId());
+
+		Assert.assertEquals(title, persistedAssetListEntry.getTitle());
 	}
+
+	@Inject
+	private AssetListEntryLocalService _assetListEntryLocalService;
 
 	@DeleteAfterTestRun
 	private Group _group;
 
 	@Inject(filter = "mvc.command.name=/asset_list/update_asset_list_entry")
 	private MVCActionCommand _mvcActionCommand;
+
+	private static class MockActionResponse
+		extends MockLiferayPortletActionResponse {
+
+		@Override
+		public MockPortletURL createRenderURL() {
+			return new MockPortletURL();
+		}
+
+	}
+
+	private static class MockPortletURL implements PortletURL, RenderURL {
+
+		@Override
+		public void addProperty(String key, String value) {
+		}
+
+		@Override
+		public Appendable append(Appendable appendable) {
+			return null;
+		}
+
+		@Override
+		public Appendable append(Appendable appendable, boolean escapeXML) {
+			return null;
+		}
+
+		@Override
+		public String getFragmentIdentifier() {
+			return null;
+		}
+
+		@Override
+		public Map<String, String[]> getParameterMap() {
+			return null;
+		}
+
+		@Override
+		public PortletMode getPortletMode() {
+			return null;
+		}
+
+		@Override
+		public MutableRenderParameters getRenderParameters() {
+			return null;
+		}
+
+		@Override
+		public WindowState getWindowState() {
+			return null;
+		}
+
+		@Override
+		public void removePublicRenderParameter(String name) {
+		}
+
+		@Override
+		public void setBeanParameter(PortletSerializable portletSerializable) {
+		}
+
+		@Override
+		public void setFragmentIdentifier(String fragment) {
+		}
+
+		@Override
+		public void setParameter(String name, String value) {
+		}
+
+		@Override
+		public void setParameter(String name, String... values) {
+		}
+
+		@Override
+		public void setParameters(Map<String, String[]> map) {
+		}
+
+		@Override
+		public void setPortletMode(PortletMode portletMode) {
+		}
+
+		@Override
+		public void setProperty(String key, String value) {
+		}
+
+		@Override
+		public void setSecure(boolean secure) {
+		}
+
+		@Override
+		public void setWindowState(WindowState windowState) {
+		}
+
+		@Override
+		public void write(Writer writer) {
+		}
+
+		@Override
+		public void write(Writer writer, boolean escapeXML) {
+		}
+
+	}
 
 }
