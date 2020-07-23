674f958affe991e4a458295c6f998856e8dc1ff3
Brian Chan <brian.chan@liferay.com>
2020-07-22 22:52:09 -0700

    LPS-117540 auto SF

--- modules/apps/translation/translation-service/src/main/java/com/liferay/translation/service/impl/TranslationEntryLocalServiceImpl.java
+++ modules/apps/translation/translation-service/src/main/java/com/liferay/translation/service/impl/TranslationEntryLocalServiceImpl.java
@@ -34,7 +34,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Alejandro Tardín
+ * @author Alejandro TardÃ­n
  */
 @Component(
 	property = "model.class.name=com.liferay.translation.model.TranslationEntry",
