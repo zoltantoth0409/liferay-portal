5badc003cebd5e5e91cab0a3b55e381f0c5c01c1
Pavel Savinov <pavel.savinov@liferay.com>
2020-07-23 15:55:22 +0200

    LPS-116913 Portlet resource is needed to automatically add content to the page

--- modules/apps/product-navigation/product-navigation-control-menu-web/src/main/java/com/liferay/product/navigation/control/menu/web/internal/display/context/AddContentPanelDisplayContext.java
+++ modules/apps/product-navigation/product-navigation-control-menu-web/src/main/java/com/liferay/product/navigation/control/menu/web/internal/display/context/AddContentPanelDisplayContext.java
@@ -82,6 +82,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.portlet.PortletConfig;
+import javax.portlet.PortletURL;
 import javax.portlet.ResourceURL;
 
 import javax.servlet.ServletContext;
@@ -352,9 +353,19 @@ public class AddContentPanelDisplayContext {
 						curGroupId = group.getLiveGroupId();
 					}
 
+					PortletURL portletURL =
+						assetPublisherAddItemHolder.getPortletURL();
+
+					portletURL.setParameter(
+						"redirect",
+						HttpUtil.addParameter(
+							PortalUtil.getLayoutRelativeURL(
+								_themeDisplay.getLayout(), _themeDisplay),
+							"portletResource",
+							assetPublisherAddItemHolder.getPortletId()));
+
 					return _assetHelper.getAddURLPopUp(
-						curGroupId, _themeDisplay.getPlid(),
-						assetPublisherAddItemHolder.getPortletURL(), false,
+						curGroupId, _themeDisplay.getPlid(), portletURL, false,
 						_themeDisplay.getLayout());
 				}
 			).build()
