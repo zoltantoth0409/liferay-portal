e95ba9f0bc99a7ba39d4cce2be437198782b0ca0
Pavel Savinov <pavel.savinov@liferay.com>
2020-07-23 11:58:01 +0200

    LPS-117064 Update only new properties and set default value when available

--- modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/StyledLayoutStructureItem.java
+++ modules/apps/layout/layout-api/src/main/java/com/liferay/layout/util/structure/StyledLayoutStructureItem.java
@@ -16,6 +16,12 @@ package com.liferay.layout.util.structure;
 
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Pavel Savinov
@@ -28,22 +34,49 @@ public abstract class StyledLayoutStructureItem extends LayoutStructureItem {
 
 	@Override
 	public JSONObject getItemConfigJSONObject() {
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+		try {
+			Map<String, Object> defaultValues =
+				CommonStylesUtil.getDefaultStyleValues();
 
-		if (stylesJSONObject.length() > 0) {
-			jsonObject.put("styles", stylesJSONObject);
+			for (Map.Entry<String, Object> entry : defaultValues.entrySet()) {
+				if (!stylesJSONObject.has(entry.getKey())) {
+					stylesJSONObject.put(entry.getKey(), entry.getValue());
+				}
+			}
+		}
+		catch (Exception exception) {
+			_log.error("Unable to get default style values", exception);
 		}
 
-		return jsonObject;
+		return JSONUtil.put("styles", stylesJSONObject);
 	}
 
 	@Override
 	public void updateItemConfig(JSONObject itemConfigJSONObject) {
 		if (itemConfigJSONObject.has("styles")) {
-			stylesJSONObject = itemConfigJSONObject.getJSONObject("styles");
+			JSONObject newStylesJSONObject = itemConfigJSONObject.getJSONObject(
+				"styles");
+
+			try {
+				List<String> availableStyleNames =
+					CommonStylesUtil.getAvailableStyleNames();
+
+				for (String styleName : availableStyleNames) {
+					if (newStylesJSONObject.has(styleName)) {
+						stylesJSONObject.put(
+							styleName, newStylesJSONObject.get(styleName));
+					}
+				}
+			}
+			catch (Exception exception) {
+				_log.error("Unable to get available style names", exception);
+			}
 		}
 	}
 
 	protected JSONObject stylesJSONObject = JSONFactoryUtil.createJSONObject();
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		StyledLayoutStructureItem.class);
 
 }
