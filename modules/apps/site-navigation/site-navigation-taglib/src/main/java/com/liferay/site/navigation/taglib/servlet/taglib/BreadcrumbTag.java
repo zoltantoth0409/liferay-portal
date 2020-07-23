66f38178a74e58624e0b13c2b1c489fc9694b35c
Alicia Garcia <alicia.garcia@liferay.com>
2020-07-24 00:01:27 +0200

    LPS-115548 modify the Site-Navigation-taglib:BreadcrumbTag to take account of the BreadcrumbEntryContributor on the registry and modify the Breadcrumbs

--- modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/BreadcrumbTag.java
+++ modules/apps/site-navigation/site-navigation-taglib/src/main/java/com/liferay/site/navigation/taglib/servlet/taglib/BreadcrumbTag.java
@@ -14,7 +14,11 @@
 
 package com.liferay.site.navigation.taglib.servlet.taglib;
 
+import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.taglib.ui.BreadcrumbEntry;
+import com.liferay.portal.kernel.servlet.taglib.ui.BreadcrumbEntryContributor;
 import com.liferay.site.navigation.taglib.internal.servlet.ServletContextUtil;
 import com.liferay.taglib.util.IncludeTag;
 
@@ -23,6 +27,9 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
+
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @author Eudaldo Alonso
@@ -49,6 +56,7 @@ public class BreadcrumbTag extends IncludeTag {
 		super.cleanUp();
 
 		_breadcrumbEntries = new ArrayList<>();
+		_serviceTracker = null;
 	}
 
 	@Override
@@ -58,13 +66,33 @@ public class BreadcrumbTag extends IncludeTag {
 
 	@Override
 	protected void setAttributes(HttpServletRequest httpServletRequest) {
+		List<BreadcrumbEntry> breadcrumbEntries = _breadcrumbEntries;
+
+		try {
+			_serviceTracker = ServiceTrackerFactory.open(
+				FrameworkUtil.getBundle(BreadcrumbTag.class),
+				BreadcrumbEntryContributor.class);
+
+			BreadcrumbEntryContributor breadcrumbEntryContributor =
+				_serviceTracker.getService();
+
+			breadcrumbEntries = breadcrumbEntryContributor.getBreadcrumbEntries(
+				breadcrumbEntries, httpServletRequest);
+		}
+		catch (Exception exception) {
+			_log.error(exception.getMessage());
+		}
+
 		httpServletRequest.setAttribute(
 			"liferay-site-navigation:breadcrumb:breadcrumbEntries",
-			_breadcrumbEntries);
+			breadcrumbEntries);
 	}
 
 	private static final String _PAGE = "/breadcrumb/page.jsp";
 
+	private static final Log _log = LogFactoryUtil.getLog(BreadcrumbTag.class);
+
 	private List<BreadcrumbEntry> _breadcrumbEntries = new ArrayList<>();
+	private ServiceTracker<?, BreadcrumbEntryContributor> _serviceTracker;
 
 }
