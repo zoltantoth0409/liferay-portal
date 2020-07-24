8e23f67ad3f0c5a4d0e64337e1082864d1d1d6d8
shuyangzhou <shuyang.zhou@liferay.com>
2020-07-24 00:05:54 -0700

    LPS-77595 Unget what got get in addingService.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/template/TemplateHandlerRegistryImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/template/TemplateHandlerRegistryImpl.java
@@ -360,6 +360,8 @@ public class TemplateHandlerRegistryImpl implements TemplateHandlerRegistry {
 			if (serviceRegistration != null) {
 				serviceRegistration.unregister();
 			}
+
+			_bundleContext.ungetService(serviceReference);
 		}
 
 	}
