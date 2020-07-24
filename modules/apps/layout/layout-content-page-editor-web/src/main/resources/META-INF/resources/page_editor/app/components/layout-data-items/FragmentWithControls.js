c51849c65e25d8021c06861e177f691ed146e50a
Sandro Chinea <sandrochinea@gmail.com>
2020-07-24 14:36:34 +0100

    LPS-117069 Adapt fragments to common styles

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/FragmentWithControls.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/FragmentWithControls.js
@@ -12,26 +12,118 @@
  * details.
  */
 
-import React from 'react';
+import classNames from 'classnames';
+import React, {useEffect, useState} from 'react';
 
 import useSetRef from '../../../core/hooks/useSetRef';
 import {
 	LayoutDataPropTypes,
 	getLayoutDataItemPropTypes,
 } from '../../../prop-types/index';
+import loadBackgroundImage from '../../utils/loadBackgroundImage';
 import Topper from '../Topper';
 import FragmentContent from '../fragment-content/FragmentContent';
 
 const FragmentWithControls = React.forwardRef(({item, layoutData}, ref) => {
 	const [setRef, itemElement] = useSetRef(ref);
 
+	const {
+		backgroundColor,
+		backgroundImage,
+		borderColor,
+		borderRadius,
+		borderWidth,
+		fontFamily,
+		fontSize,
+		fontWeight,
+		height,
+		marginBottom,
+		marginLeft,
+		marginRight,
+		marginTop,
+		maxHeight,
+		maxWidth,
+		minHeight,
+		minWidth,
+		opacity,
+		overflow,
+		paddingBottom,
+		paddingLeft,
+		paddingRight,
+		paddingTop,
+		shadow,
+		textAlign,
+		textColor,
+		width,
+	} = item.config.styles;
+
+	const [backgroundImageValue, setBackgroundImageValue] = useState('');
+
+	useEffect(() => {
+		loadBackgroundImage(backgroundImage).then(setBackgroundImageValue);
+	}, [backgroundImage]);
+
+	const style = {};
+
+	if (backgroundImageValue) {
+		style.backgroundImage = `url(${backgroundImageValue})`;
+		style.backgroundPosition = '50% 50%';
+		style.backgroundRepeat = 'no-repeat';
+		style.backgroundSize = 'cover';
+	}
+
+	if (fontSize) {
+		style.fontSize = fontSize;
+	}
+
+	if (minHeight !== 'auto') {
+		style.minHeight = minHeight;
+	}
+
+	if (minWidth !== 'auto') {
+		style.minWidth = minWidth;
+	}
+
+	style.border = `solid ${borderWidth}px`;
+	style.maxHeight = maxHeight;
+	style.maxWidth = maxWidth;
+	style.opacity = opacity;
+	style.overflow = overflow;
+
 	return (
 		<Topper item={item} itemElement={itemElement} layoutData={layoutData}>
-			<FragmentContent
-				elementRef={setRef}
-				fragmentEntryLinkId={item.config.fragmentEntryLinkId}
-				itemId={item.itemId}
-			/>
+			<div
+				className={classNames(
+					fontWeight,
+					height,
+					`mb-${marginBottom}`,
+					`ml-${marginLeft}`,
+					`mr-${marginRight}`,
+					`mt-${marginTop}`,
+					`pb-${paddingBottom}`,
+					`pl-${paddingLeft}`,
+					`pr-${paddingRight}`,
+					`pt-${paddingTop}`,
+					shadow,
+					width,
+					{
+						[`bg-${backgroundColor?.cssClass}`]: backgroundColor,
+						[`border-${borderColor?.cssClass}`]: borderColor,
+						[borderRadius]: !!borderRadius,
+						[`text-${fontFamily}`]: fontFamily !== 'default',
+						'no-gutters': !item.config.gutters,
+						[textAlign]: textAlign !== 'none',
+						[`text-${textColor?.cssClass}`]: textColor,
+					}
+				)}
+				style={style}
+			>
+				<FragmentContent
+					elementRef={setRef}
+					fragmentEntryLinkId={item.config.fragmentEntryLinkId}
+					itemId={item.itemId}
+				/>
+			</div>
 		</Topper>
 	);
 });
