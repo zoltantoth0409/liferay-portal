177f24d8b1f66c08da20b56ca2252168e6a533ab
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-07-26 15:01:48 +0200

    LPS-117815 We just want to check that an infoItemFormProvider is registered under the className

--- modules/apps/info/info-test/src/testIntegration/java/com/liferay/info/display/contributor/test/InfoDisplayContributorTest.java
+++ modules/apps/info/info-test/src/testIntegration/java/com/liferay/info/display/contributor/test/InfoDisplayContributorTest.java
@@ -15,41 +15,29 @@
 package com.liferay.info.display.contributor.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.asset.kernel.model.ClassType;
-import com.liferay.asset.kernel.model.ClassTypeField;
 import com.liferay.info.display.contributor.InfoDisplayContributor;
 import com.liferay.info.display.contributor.InfoDisplayField;
 import com.liferay.info.display.contributor.InfoDisplayObjectProvider;
 import com.liferay.info.item.InfoItemServiceTracker;
 import com.liferay.info.item.provider.InfoItemFormProvider;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.ClassName;
-import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 import com.liferay.registry.ServiceRegistration;
 
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author Yang Cao
@@ -60,15 +48,7 @@ public class InfoDisplayContributorTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			PermissionCheckerMethodTestRule.INSTANCE);
-
-	@Before
-	public void setUp() throws Exception {
-		_className = _classNameLocalService.addClassName(
-			TestInfoDisplayContributor.class.getName());
-	}
+		new LiferayIntegrationTestRule();
 
 	@After
 	public void tearDown() {
@@ -78,59 +58,41 @@ public class InfoDisplayContributorTest {
 	}
 
 	@Test
-	public void testRegisterInfoDisplayContributerAsInfoItemFormProvider()
-		throws PortalException {
+	public void testRegisterInfoDisplayContributorAsInfoItemFormProvider() {
+		_registerInfoDisplayContributor();
 
-		_registerInfoDisplayContributor(Collections.emptyList());
+		InfoItemFormProvider<TestInfoDisplayContributor> infoItemFormProvider =
+			(InfoItemFormProvider<TestInfoDisplayContributor>)
+				_infoItemServiceTracker.getFirstInfoItemService(
+					InfoItemFormProvider.class,
+					TestInfoDisplayContributor.class.getName());
 
-		Assert.assertNotNull(_infoItemServiceTracker.getAllInfoItemServices(
-			InfoItemFormProvider.class));
+		Assert.assertNotNull(infoItemFormProvider);
 	}
 
-	private void _registerInfoDisplayContributor(List<Long> classTypeIds) {
-		TestInfoDisplayContributor infoDisplayContributor =
-			new TestInfoDisplayContributor(classTypeIds);
-
+	private void _registerInfoDisplayContributor() {
 		Registry registry = RegistryUtil.getRegistry();
 
 		_serviceRegistration = registry.registerService(
 			(Class<InfoDisplayContributor<?>>)
 				(Class<?>)InfoDisplayContributor.class,
-			infoDisplayContributor);
+			new TestInfoDisplayContributor());
 	}
 
 	@Inject
-	private static ClassNameLocalService _classNameLocalService;
-
-	@Inject
 	private InfoItemServiceTracker _infoItemServiceTracker;
-
-	@DeleteAfterTestRun
-	private ClassName _className;
 
 	private ServiceRegistration<InfoDisplayContributor<?>> _serviceRegistration;
 
 	private static class TestInfoDisplayContributor
 		implements InfoDisplayContributor<Object> {
 
-		public TestInfoDisplayContributor(List<Long> classTypeIds) {
-			_classTypeIds = classTypeIds;
+		public TestInfoDisplayContributor() {
 		}
 
 		@Override
 		public String getClassName() {
 			return TestInfoDisplayContributor.class.getName();
-		}
-
-		@Override
-		public List<ClassType> getClassTypes(long groupId, Locale locale) {
-			Stream<Long> stream = _classTypeIds.stream();
-
-			return stream.map(
-				this::_toClassType
-			).collect(
-				Collectors.toList()
-			);
 		}
 
 		@Override
@@ -165,46 +127,6 @@ public class InfoDisplayContributorTest {
 		public String getInfoURLSeparator() {
 			return RandomTestUtil.randomString();
 		}
-
-		private ClassType _toClassType(long classTypeId) {
-			return new ClassType() {
-
-				@Override
-				public ClassTypeField getClassTypeField(String fieldName) {
-					return null;
-				}
-
-				@Override
-				public List<ClassTypeField> getClassTypeFields() {
-					return null;
-				}
-
-				@Override
-				public List<ClassTypeField> getClassTypeFields(
-					int start, int end) {
-
-					return null;
-				}
-
-				@Override
-				public int getClassTypeFieldsCount() {
-					return 0;
-				}
-
-				@Override
-				public long getClassTypeId() {
-					return classTypeId;
-				}
-
-				@Override
-				public String getName() {
-					return null;
-				}
-
-			};
-		}
-
-		private final List<Long> _classTypeIds;
 
 	}
 
