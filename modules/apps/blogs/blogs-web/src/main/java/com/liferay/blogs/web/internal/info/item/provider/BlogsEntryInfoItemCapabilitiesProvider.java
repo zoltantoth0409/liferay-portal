8318a98a4754ec9297482c02683fb667e4be61ef
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-07-27 16:29:35 +0200

    LPS-117884  Implement a Capabilities Provider for BlogsEntry and JournalArticle to declare support for Display Pages

---
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/info/item/provider/BlogsEntryInfoItemCapabilitiesProvider.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.blogs.web.internal.info.item.provider;
+
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.info.item.capability.InfoItemCapability;
+import com.liferay.info.item.provider.InfoItemCapabilitiesProvider;
+import com.liferay.layout.page.template.info.item.capability.DisplayPageInfoItemCapability;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jorge Ferrer
+ */
+@Component(service = InfoItemCapabilitiesProvider.class)
+public class BlogsEntryInfoItemCapabilitiesProvider
+	implements InfoItemCapabilitiesProvider<BlogsEntry> {
+
+	@Override
+	public List<InfoItemCapability> getInfoItemCapabilities() {
+		ArrayList<InfoItemCapability> infoItemCapabilities = new ArrayList<>();
+
+		infoItemCapabilities.add(DisplayPageInfoItemCapability.INSTANCE);
+
+		return infoItemCapabilities;
+	}
+
+}
