87d4692769e5cba5d1ba43bace3bab261cd90a6a
Albino Junior <albino.junior@liferay.com>
2020-07-28 18:31:07 -0300

    LPS-117970 Remove unused function

--- modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/utils/utils.es.js
+++ modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/utils/utils.es.js
@@ -24,14 +24,3 @@ export const isEqualObjects = (firstObj = {}, secondObj = {}) => {
 
 	return JSON.stringify(firstObj) === JSON.stringify(secondObj);
 };
-
-export const getTranslatedValue = (item, propertyKey) => {
-	const {
-		defaultLanguageId = themeDisplay.getLanguageId(),
-		[propertyKey]: value,
-	} = item;
-
-	return typeof value === 'object'
-		? value[themeDisplay.getLanguageId()] || value[defaultLanguageId]
-		: value;
-};
