97b5ce977a26ae50e20a4df039517004a3da7e93
alee8888 <albert.lee@liferay.com>
2020-07-28 15:58:05 -0700

    LPS-118148 Add Management Toolbar for Account Entries that belong to an Account Group

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/display/context/ViewAccountGroupAccountEntriesManagementToolbarDisplayContext.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.display.context;
+
+import com.liferay.account.admin.web.internal.display.AccountEntryDisplay;
+import com.liferay.frontend.taglib.clay.servlet.taglib.display.context.SearchContainerManagementToolbarDisplayContext;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenuBuilder;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.PortletURLUtil;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Albert Lee
+ */
+public class ViewAccountGroupAccountEntriesManagementToolbarDisplayContext
+	extends SearchContainerManagementToolbarDisplayContext {
+
+	public ViewAccountGroupAccountEntriesManagementToolbarDisplayContext(
+		HttpServletRequest httpServletRequest,
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		SearchContainer<AccountEntryDisplay> searchContainer) {
+
+		super(
+			httpServletRequest, liferayPortletRequest, liferayPortletResponse,
+			searchContainer);
+	}
+
+	@Override
+	public String getClearResultsURL() {
+		PortletURL clearResultsURL = getPortletURL();
+
+		clearResultsURL.setParameter("keywords", StringPool.BLANK);
+
+		return clearResultsURL.toString();
+	}
+
+	@Override
+	public CreationMenu getCreationMenu() {
+		return CreationMenuBuilder.addPrimaryDropdownItem(
+			dropdownItem -> {
+				dropdownItem.putData(
+					"action", "selectAccountGroupAccountEntries");
+				dropdownItem.setLabel(
+					LanguageUtil.get(request, "assign-accounts"));
+			}
+		).build();
+	}
+
+	@Override
+	public String getDefaultEventHandler() {
+		return "ACCOUNT_GROUP_ACCOUNT_ENTRIES_MANAGEMENT_TOOLBAR_" +
+			"DEFAULT_EVENT_HANDLER";
+	}
+
+	@Override
+	public PortletURL getPortletURL() {
+		try {
+			return PortletURLUtil.clone(currentURLObj, liferayPortletResponse);
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(exception, exception);
+			}
+
+			return liferayPortletResponse.createRenderURL();
+		}
+	}
+
+	@Override
+	public String getSearchActionURL() {
+		PortletURL searchActionURL = getPortletURL();
+
+		return searchActionURL.toString();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ViewAccountGroupAccountEntriesManagementToolbarDisplayContext.class);
+
+}
