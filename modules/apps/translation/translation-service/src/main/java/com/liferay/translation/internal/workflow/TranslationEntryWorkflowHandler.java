57704b243528fa99e8d3bd3795c2437fe314c294
Alicia Garcia <alicia.garcia@liferay.com>
2020-07-28 14:42:37 +0200

    LPS-117840 update TranslationEntry status on WorkflowHandler

--- modules/apps/translation/translation-service/src/main/java/com/liferay/translation/internal/workflow/TranslationEntryWorkflowHandler.java
+++ modules/apps/translation/translation-service/src/main/java/com/liferay/translation/internal/workflow/TranslationEntryWorkflowHandler.java
@@ -18,9 +18,13 @@ import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.workflow.BaseWorkflowHandler;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.workflow.WorkflowHandler;
 import com.liferay.translation.model.TranslationEntry;
+import com.liferay.translation.service.TranslationEntryLocalService;
 
 import java.io.Serializable;
 
@@ -28,6 +32,7 @@ import java.util.Locale;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alicia Garcia
@@ -66,9 +71,22 @@ public class TranslationEntryWorkflowHandler
 			int status, Map<String, Serializable> workflowContext)
 		throws PortalException {
 
-		return null;
+		long userId = GetterUtil.getLong(
+			(String)workflowContext.get(WorkflowConstants.CONTEXT_USER_ID));
+		long classPK = GetterUtil.getLong(
+			(String)workflowContext.get(
+				WorkflowConstants.CONTEXT_ENTRY_CLASS_PK));
+
+		ServiceContext serviceContext = (ServiceContext)workflowContext.get(
+			"serviceContext");
+
+		return _translationEntryLocalService.updateStatus(
+			userId, classPK, status, serviceContext, workflowContext);
 	}
 
 	private static final boolean _VISIBLE = true;
+
+	@Reference
+	private TranslationEntryLocalService _translationEntryLocalService;
 
 }
