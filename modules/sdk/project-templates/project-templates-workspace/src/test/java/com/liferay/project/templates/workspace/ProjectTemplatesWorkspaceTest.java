bc67eaf2b61e7f842a16c028c9e4cc05d159d48b
Lawrence Lee <lawrence.lee@liferay.com>
2020-07-28 14:49:14 -0500

    LPS-117801 Update property name

--- modules/sdk/project-templates/project-templates-workspace/src/test/java/com/liferay/project/templates/workspace/ProjectTemplatesWorkspaceTest.java
+++ modules/sdk/project-templates/project-templates-workspace/src/test/java/com/liferay/project/templates/workspace/ProjectTemplatesWorkspaceTest.java
@@ -201,7 +201,7 @@ public class ProjectTemplatesWorkspaceTest
 		Assert.assertTrue(gradleProperties.exists());
 
 		String configLine =
-			System.lineSeparator() + "node.package.manager=yarn";
+			System.lineSeparator() + "liferay.workspace.node.package.manager=yarn";
 
 		Files.write(
 			gradleProperties.toPath(), configLine.getBytes(),
