e7f4531a928333fd2274d9407a07ea94c8651869
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2020-07-28 17:02:06 -0300

    LPS-117466 Add test to check if the fieldname keeps the same after a field label edition

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/test/js/components/LayoutProvider/LayoutProvider.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/test/js/components/LayoutProvider/LayoutProvider.es.js
@@ -465,6 +465,49 @@ describe('LayoutProvider', () => {
 			});
 		});
 
+		describe('fieldEdited', () => {
+			it('listens the fieldEdited event and edit the field label keeping the same fieldName', () => {
+				component = new Parent();
+
+				const {child, provider} = component.refs;
+				const mockEvent = {
+					data: {
+						parentFieldName: undefined,
+					},
+					fieldType: mockFieldType,
+					indexes: {
+						columnIndex: 0,
+						pageIndex: 0,
+						rowIndex: 0,
+					},
+				};
+
+				const {dispatch} = child.context;
+
+				dispatch('fieldAdded', mockEvent);
+
+				jest.runAllTimers();
+
+				const fieldNameCreated =
+					provider.state.pages[0].rows[0].columns[0].fields[0]
+						.fieldName;
+
+				const mockEvent2 = {
+					fieldName: fieldNameCreated,
+					propertyName: 'label',
+					propertyValue: 'newLabel',
+				};
+
+				dispatch('fieldEdited', mockEvent2);
+
+				jest.runAllTimers();
+
+				expect(
+					provider.state.pages[0].rows[0].columns[0].fields[0]
+						.fieldName
+				).toEqual(fieldNameCreated);
+			});
+		});
 		describe('fieldDuplicated', () => {
 			it('listens the duplicate field event and add this field in the pages', () => {
 				component = new Parent();
