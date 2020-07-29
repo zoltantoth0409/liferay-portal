4e4662a3a03533da84d38e60eb4fbed37bef3e0e
Victor Galan <victor.galan@liferay.com>
2020-07-29 15:41:36 +0200

    LPS-117535 Create method to update the style book entry id of a layout

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceImpl.java
@@ -3533,6 +3533,30 @@ public class LayoutLocalServiceImpl extends LayoutLocalServiceBaseImpl {
 		return layout;
 	}
 
+	/**
+	 * Updates the layout replacing its style book entry id.
+	 *
+	 * @param  groupId the primary key of the group
+	 * @param  privateLayout whether the layout is private to the group
+	 * @param  layoutId the layout ID of the layout
+	 * @param  styleBookEntryId the primary key of the style book entry
+	 * @return the updated layout
+	 * @throws PortalException if a portal exception occurred
+	 */
+	@Override
+	public Layout updateStyleBookEntryId(
+			long groupId, boolean privateLayout, long layoutId,
+			long styleBookEntryId)
+		throws PortalException {
+
+		Layout layout = layoutPersistence.findByG_P_L(
+			groupId, privateLayout, layoutId);
+
+		layout.setStyleBookEntryId(styleBookEntryId);
+
+		return layoutPersistence.update(layout);
+	}
+
 	@Override
 	public Layout updateType(long plid, String type) throws PortalException {
 		Layout layout = layoutPersistence.findByPrimaryKey(plid);
