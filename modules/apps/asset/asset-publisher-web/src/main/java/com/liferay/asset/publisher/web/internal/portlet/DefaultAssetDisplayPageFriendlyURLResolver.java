fd7c20d3435640cd95f947e63764437ea4cba391
Brian Chan <brian.chan@liferay.com>
2020-07-29 12:20:26 -0700

    LPS-109019 rename

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/portlet/DefaultAssetDisplayPageFriendlyURLResolver.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/portlet/DefaultAssetDisplayPageFriendlyURLResolver.java
@@ -130,9 +130,9 @@ public class DefaultAssetDisplayPageFriendlyURLResolver
 		HttpServletRequest httpServletRequest =
 			(HttpServletRequest)requestContext.get("request");
 
-		HttpSession session = httpServletRequest.getSession();
+		HttpSession httpSession = httpServletRequest.getSession();
 
-		Locale locale = (Locale)session.getAttribute(WebKeys.LOCALE);
+		Locale locale = (Locale)httpSession.getAttribute(WebKeys.LOCALE);
 
 		if (locale != null) {
 			Map<Locale, String> friendlyURLMap =
