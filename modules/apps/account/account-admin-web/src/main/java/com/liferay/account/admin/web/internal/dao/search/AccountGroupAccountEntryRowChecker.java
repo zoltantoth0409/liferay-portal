012a876b5cf71d2c3b12b375865c5a35f0b8e389
alee8888 <albert.lee@liferay.com>
2020-07-29 16:56:29 -0700

    LPS-118148 Add Row Checker class when assigning Account Entries to an Account Group

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/dao/search/AccountGroupAccountEntryRowChecker.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.dao.search;
+
+import com.liferay.account.admin.web.internal.display.AccountEntryDisplay;
+import com.liferay.account.model.AccountGroupAccountEntryRel;
+import com.liferay.account.service.AccountGroupAccountEntryRelLocalServiceUtil;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import javax.portlet.PortletResponse;
+
+/**
+ * @author Albert Lee
+ */
+public class AccountGroupAccountEntryRowChecker extends EmptyOnClickRowChecker {
+
+	public AccountGroupAccountEntryRowChecker(
+		PortletResponse portletResponse, long accountGroupId) {
+
+		super(portletResponse);
+
+		_accountGroupId = accountGroupId;
+	}
+
+	@Override
+	public boolean isDisabled(Object object) {
+		AccountEntryDisplay accountEntryDisplay = (AccountEntryDisplay)object;
+
+		try {
+			AccountGroupAccountEntryRel accountGroupAccountEntryRel =
+				AccountGroupAccountEntryRelLocalServiceUtil.
+					fetchAccountGroupAccountEntryRel(
+						_accountGroupId,
+						accountEntryDisplay.getAccountEntryId());
+
+			if (accountGroupAccountEntryRel != null) {
+				return true;
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(exception, exception);
+			}
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AccountGroupAccountEntryRowChecker.class);
+
+	private final long _accountGroupId;
+
+}
