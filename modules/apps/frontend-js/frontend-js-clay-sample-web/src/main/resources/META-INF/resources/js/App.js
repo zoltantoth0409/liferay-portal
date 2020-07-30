c0e0edabdb090dffbb6531785b796e59395f3ee5
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2020-07-30 13:46:31 -0700

    LPS-118237 - Create frontend-js-clay-sample-web module

---
+++ modules/apps/frontend-js/frontend-js-clay-sample-web/src/main/resources/META-INF/resources/js/App.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React from 'react';
+
+import Alert from './components/Alert';
+
+import '../css/main.scss';
+
+export default () => {
+	return (
+		<div>
+			<Alert />
+
+			<div className="clay-test-class">This is where your code goes</div>
+		</div>
+	);
+};
