86aa950d8d3b27bac971e71bd7b42895115516ad
arthurchan35 <arthurchan35@gmail.com>
2020-07-31 18:07:54 +0800

    LPS-111617 ManagedServiceFactory to build URLPatternMatchers
    Co-authored-by: Carlos Sierra Andrés <carlos.sierra@liferay.com>

--- modules/apps/portal-remote/portal-remote-cors-impl/src/main/java/com/liferay/portal/remote/cors/internal/servlet/filter/PortalCORSServletFilter.java
+++ modules/apps/portal-remote/portal-remote-cors-impl/src/main/java/com/liferay/portal/remote/cors/internal/servlet/filter/PortalCORSServletFilter.java
@@ -16,6 +16,7 @@ package com.liferay.portal.remote.cors.internal.servlet.filter;
 
 import com.liferay.oauth2.provider.scope.liferay.OAuth2ProviderScopeLiferayAccessControlContext;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -25,13 +26,21 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.servlet.BaseFilter;
 import com.liferay.portal.kernel.servlet.HttpMethods;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.remote.cors.configuration.PortalCORSConfiguration;
 import com.liferay.portal.remote.cors.internal.CORSSupport;
 import com.liferay.portal.remote.cors.internal.url.pattern.matcher.URLPatternMatcher;
+import com.liferay.portal.remote.cors.internal.url.pattern.matcher.URLPatternMatcherFactory;
 
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.servlet.Filter;
@@ -44,6 +53,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedServiceFactory;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -61,9 +72,30 @@ import org.osgi.service.component.annotations.Reference;
 		"dispatcher=REQUEST", "servlet-context-name=",
 		"servlet-filter-name=Portal CORS Servlet Filter", "url-pattern=/*"
 	},
-	service = Filter.class
+	service = {Filter.class, ManagedServiceFactory.class}
 )
-public class PortalCORSServletFilter extends BaseFilter {
+public class PortalCORSServletFilter
+	extends BaseFilter implements ManagedServiceFactory {
+
+	@Override
+	public void deleted(String pid) {
+		Dictionary<String, ?> properties = _configurationPidsProperties.remove(
+			pid);
+
+		long companyId = GetterUtil.getLong(properties.get("companyId"));
+
+		if (companyId == CompanyConstants.SYSTEM) {
+			_rebuild();
+		}
+		else {
+			_rebuild(companyId);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return StringPool.BLANK;
+	}
 
 	@Override
 	public void init(FilterConfig filterConfig) {
@@ -84,9 +116,45 @@ public class PortalCORSServletFilter extends BaseFilter {
 		return false;
 	}
 
+	@Override
+	public void updated(String pid, Dictionary<String, ?> properties)
+		throws ConfigurationException {
+
+		Dictionary<String, ?> oldProperties = _configurationPidsProperties.put(
+			pid, properties);
+
+		long companyId = GetterUtil.getLong(
+			properties.get("companyId"), CompanyConstants.SYSTEM);
+
+		if (companyId == CompanyConstants.SYSTEM) {
+			_rebuild();
+
+			return;
+		}
+
+		if (oldProperties != null) {
+			long oldCompanyId = GetterUtil.getLong(
+				oldProperties.get("companyId"));
+
+			if (oldCompanyId == CompanyConstants.SYSTEM) {
+				_rebuild();
+
+				return;
+			}
+
+			if (oldCompanyId != companyId) {
+				_rebuild(oldCompanyId);
+			}
+		}
+
+		_rebuild(companyId);
+	}
+
 	@Activate
 	protected void activate(
 		BundleContext bundleContext, Map<String, Object> properties) {
+
+		_defaultURLPatternMatcher = _buildDefaultURLPatternMatcher();
 	}
 
 	@Deactivate
@@ -163,6 +231,56 @@ public class PortalCORSServletFilter extends BaseFilter {
 		filterChain.doFilter(httpServletRequest, httpServletResponse);
 	}
 
+	private void _buildCORSSupports(
+		Map<String, CORSSupport> corsSupports,
+		PortalCORSConfiguration portalCORSConfiguration) {
+
+		Map<String, String> corsHeaders = CORSSupport.buildCORSHeaders(
+			portalCORSConfiguration.headers());
+
+		CORSSupport corsSupport = new CORSSupport();
+
+		corsSupport.setCORSHeaders(corsHeaders);
+
+		for (String urlPattern :
+				portalCORSConfiguration.filterMappingURLPatterns()) {
+
+			if (!corsSupports.containsKey(urlPattern)) {
+				corsSupports.put(urlPattern, corsSupport);
+			}
+		}
+	}
+
+	private URLPatternMatcher<CORSSupport> _buildDefaultURLPatternMatcher() {
+		Map<String, CORSSupport> corsSupports = new HashMap<>();
+
+		_buildCORSSupports(
+			corsSupports,
+			ConfigurableUtil.createConfigurable(
+				PortalCORSConfiguration.class, new HashMapDictionary<>()));
+
+		return URLPatternMatcherFactory.create(corsSupports);
+	}
+
+	private URLPatternMatcher<CORSSupport> _getURLPatternMatcher(
+		long companyId) {
+
+		URLPatternMatcher<CORSSupport> urlPatternMatcher =
+			_urlPatternMatchers.get(companyId);
+
+		if (urlPatternMatcher != null) {
+			return urlPatternMatcher;
+		}
+
+		urlPatternMatcher = _urlPatternMatchers.get(CompanyConstants.SYSTEM);
+
+		if (urlPatternMatcher != null) {
+			return urlPatternMatcher;
+		}
+
+		return _defaultURLPatternMatcher;
+	}
+
 	private boolean _isGuest() {
 		PermissionChecker permissionChecker =
 			PermissionThreadLocal.getPermissionChecker();
@@ -176,10 +294,65 @@ public class PortalCORSServletFilter extends BaseFilter {
 		return user.isDefaultUser();
 	}
 
+	private void _mergeCORSConfiguration(
+		Map<String, CORSSupport> corsSupports, long companyId) {
+
+		for (Dictionary<String, ?> properties :
+				_configurationPidsProperties.values()) {
+
+			if (companyId != GetterUtil.getLong(properties.get("companyId"))) {
+				continue;
+			}
+
+			PortalCORSConfiguration portalCORSConfiguration =
+				ConfigurableUtil.createConfigurable(
+					PortalCORSConfiguration.class, properties);
+
+			_buildCORSSupports(corsSupports, portalCORSConfiguration);
+		}
+	}
+
+	private void _rebuild() {
+		Map<String, CORSSupport> corsSupports = new HashMap<>();
+
+		_mergeCORSConfiguration(corsSupports, CompanyConstants.SYSTEM);
+
+		_urlPatternMatchers.put(
+			CompanyConstants.SYSTEM,
+			URLPatternMatcherFactory.create(corsSupports));
+
+		for (long companyId : _urlPatternMatchers.keySet()) {
+			if (companyId != CompanyConstants.SYSTEM) {
+				_rebuild(companyId);
+			}
+		}
+	}
+
+	private void _rebuild(long companyId) {
+		Map<String, CORSSupport> corsSupports = new HashMap<>();
+
+		_mergeCORSConfiguration(corsSupports, companyId);
+
+		if (corsSupports.isEmpty()) {
+			_urlPatternMatchers.remove(companyId);
+
+			return;
+		}
+
+		_mergeCORSConfiguration(corsSupports, CompanyConstants.SYSTEM);
+
+		_urlPatternMatchers.put(
+			companyId, URLPatternMatcherFactory.create(corsSupports));
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		PortalCORSServletFilter.class);
 
+	private final Map<String, Dictionary<String, ?>>
+		_configurationPidsProperties = Collections.synchronizedMap(
+			new LinkedHashMap<>());
 	private String _contextPath;
+	private URLPatternMatcher<CORSSupport> _defaultURLPatternMatcher;
 
 	@Reference
 	private Http _http;
@@ -189,5 +362,8 @@ public class PortalCORSServletFilter extends BaseFilter {
 
 	private ServiceRegistration<ConfigurationModelListener>
 		_serviceRegistration;
+	private final Map<Long, URLPatternMatcher<CORSSupport>>
+		_urlPatternMatchers = Collections.synchronizedMap(
+			new LinkedHashMap<>());
 
 }
