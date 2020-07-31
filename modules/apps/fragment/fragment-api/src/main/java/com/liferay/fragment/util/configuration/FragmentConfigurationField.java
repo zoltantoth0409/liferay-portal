48b5dd992116994ab07cde7a7bea02ceeea64d98
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-07-31 14:50:40 +0200

    LPS-118906 Fragment - Replace InfoDisplayContributor with LayoutDisplayPageProvider
    Note: A more precise solution could be to use InfoItemDetailsProvider and add getTitle() to InfoItemDetails

--- modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/util/configuration/FragmentConfigurationField.java
+++ modules/apps/fragment/fragment-api/src/main/java/com/liferay/fragment/util/configuration/FragmentConfigurationField.java
@@ -14,9 +14,10 @@
 
 package com.liferay.fragment.util.configuration;
 
-import com.liferay.info.display.contributor.InfoDisplayContributor;
-import com.liferay.info.display.contributor.InfoDisplayContributorTracker;
-import com.liferay.info.display.contributor.InfoDisplayObjectProvider;
+import com.liferay.info.item.InfoItemReference;
+import com.liferay.layout.display.page.LayoutDisplayPageObjectProvider;
+import com.liferay.layout.display.page.LayoutDisplayPageProvider;
+import com.liferay.layout.display.page.LayoutDisplayPageProviderTracker;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
@@ -107,26 +108,32 @@ public class FragmentConfigurationField {
 				String className = defaultValueJSONObject.getString(
 					"className");
 
-				InfoDisplayContributorTracker infoDisplayContributorTracker =
-					_serviceTracker.getService();
+				LayoutDisplayPageProviderTracker
+					layoutDisplayPageProviderTracker =
+						_serviceTracker.getService();
 
-				InfoDisplayContributor<?> infoDisplayContributor =
-					infoDisplayContributorTracker.getInfoDisplayContributor(
-						className);
+				LayoutDisplayPageProvider<?> layoutDisplayPageProvider =
+					layoutDisplayPageProviderTracker.
+						getLayoutDisplayPageProvider(className);
 
-				if (infoDisplayContributor == null) {
+				if (layoutDisplayPageProvider == null) {
 					return _defaultValue;
 				}
 
 				long classPK = defaultValueJSONObject.getLong("classPK");
 
-				InfoDisplayObjectProvider<?> infoDisplayObjectProvider =
-					infoDisplayContributor.getInfoDisplayObjectProvider(
-						classPK);
+				InfoItemReference infoItemReference = new InfoItemReference(
+					className, classPK);
+
+				LayoutDisplayPageObjectProvider<?>
+					layoutDisplayPageObjectProvider =
+						layoutDisplayPageProvider.
+							getLayoutDisplayPageObjectProvider(
+								infoItemReference);
 
 				defaultValueJSONObject.put(
 					"title",
-					infoDisplayObjectProvider.getTitle(
+					layoutDisplayPageObjectProvider.getTitle(
 						LocaleUtil.getMostRelevantLocale()));
 
 				return defaultValueJSONObject.toString();
@@ -144,7 +151,7 @@ public class FragmentConfigurationField {
 		FragmentConfigurationField.class);
 
 	private static final ServiceTracker
-		<InfoDisplayContributorTracker, InfoDisplayContributorTracker>
+		<LayoutDisplayPageProviderTracker, LayoutDisplayPageProviderTracker>
 			_serviceTracker;
 
 	static {
@@ -152,7 +159,7 @@ public class FragmentConfigurationField {
 			FragmentConfigurationField.class);
 
 		_serviceTracker = new ServiceTracker<>(
-			bundle.getBundleContext(), InfoDisplayContributorTracker.class,
+			bundle.getBundleContext(), LayoutDisplayPageProviderTracker.class,
 			null);
 
 		_serviceTracker.open();
