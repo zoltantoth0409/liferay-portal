17bf1dc8b4c00f295291b0d473b3db15853398e9
alee8888 <albert.lee@liferay.com>
2020-07-31 16:24:20 -0700

    LPS-118148 Add MVC action command to remove AccountGroupAccountEntries

---
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/portlet/action/RemoveAccountGroupAccountEntriesMVCActionCommand.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.account.admin.web.internal.portlet.action;
+
+import com.liferay.account.constants.AccountPortletKeys;
+import com.liferay.account.exception.NoSuchGroupAccountEntryRelException;
+import com.liferay.account.service.AccountGroupAccountEntryRelLocalService;
+import com.liferay.petra.lang.SafeClosable;
+import com.liferay.portal.kernel.messaging.proxy.ProxyModeThreadLocal;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Albert Lee
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AccountPortletKeys.ACCOUNT_GROUPS_ADMIN,
+		"mvc.command.name=/account_groups_admin/remove_account_group_account_entries"
+	},
+	service = MVCActionCommand.class
+)
+public class RemoveAccountGroupAccountEntriesMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try (SafeClosable safeClosable =
+				ProxyModeThreadLocal.setWithSafeClosable(true)) {
+
+			long[] accountEntryIds = ParamUtil.getLongValues(
+				actionRequest, "accountEntryIds");
+			long accountGroupId = ParamUtil.getLong(
+				actionRequest, "accountGroupId");
+
+			_accountGroupAccountEntryRelLocalService.
+				deleteAccountGroupAccountEntryRels(
+					accountGroupId, accountEntryIds);
+
+			String redirect = ParamUtil.getString(actionRequest, "redirect");
+
+			if (Validator.isNotNull(redirect)) {
+				sendRedirect(actionRequest, actionResponse, redirect);
+			}
+		}
+		catch (Exception exception) {
+			if (exception instanceof NoSuchGroupAccountEntryRelException) {
+				SessionErrors.add(actionRequest, exception.getClass());
+			}
+			else {
+				throw exception;
+			}
+
+			actionResponse.setRenderParameter(
+				"mvcPath", "/account_groups_admin/view.jsp");
+		}
+	}
+
+	@Reference
+	private AccountGroupAccountEntryRelLocalService
+		_accountGroupAccountEntryRelLocalService;
+
+}
