3c611eed3258d0b9360996409c3a2bc351ea8bc1
David Arques <david.arques@liferay.com>
2020-08-03 17:56:20 +0200

    LPS-113703 Get the namespace from the original fragment entry link in case it exists and it's related to the page

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-freemarker/src/main/java/com/liferay/fragment/entry/processor/freemarker/FreeMarkerFragmentEntryProcessor.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-freemarker/src/main/java/com/liferay/fragment/entry/processor/freemarker/FreeMarkerFragmentEntryProcessor.java
@@ -19,6 +19,7 @@ import com.liferay.fragment.exception.FragmentEntryContentException;
 import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.processor.FragmentEntryProcessor;
 import com.liferay.fragment.processor.FragmentEntryProcessorContext;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.fragment.util.configuration.FragmentEntryConfigurationParser;
 import com.liferay.petra.io.unsync.UnsyncStringWriter;
 import com.liferay.petra.string.StringPool;
@@ -27,8 +28,10 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.template.StringTemplateResource;
@@ -41,6 +44,7 @@ import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javax.servlet.http.HttpServletRequest;
@@ -123,7 +127,8 @@ public class FreeMarkerFragmentEntryProcessor
 			HashMapBuilder.<String, Object>put(
 				"configuration", configurationValuesJSONObject
 			).put(
-				"fragmentEntryLinkNamespace", fragmentEntryLink.getNamespace()
+				"fragmentEntryLinkNamespace",
+				_getFragmentEntryLinkNamespace(fragmentEntryLink)
 			).putAll(
 				_fragmentEntryConfigurationParser.getContextObjects(
 					configurationValuesJSONObject,
@@ -208,6 +213,15 @@ public class FreeMarkerFragmentEntryProcessor
 		}
 	}
 
+	private String _getFragmentEntryLinkNamespace(
+		FragmentEntryLink fragmentEntryLink) {
+
+		FragmentEntryLink originalFragmentEntryLink =
+			_getOriginalFragmentEntryLink(fragmentEntryLink);
+
+		return originalFragmentEntryLink.getNamespace();
+	}
+
 	private String _getMessage(TemplateException templateException) {
 		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
 			"content.Language", getClass());
@@ -226,6 +240,45 @@ public class FreeMarkerFragmentEntryProcessor
 		return message;
 	}
 
+	private FragmentEntryLink _getOriginalFragmentEntryLink(
+		FragmentEntryLink fragmentEntryLink) {
+
+		if (fragmentEntryLink.getOriginalFragmentEntryLinkId() <= 0) {
+			return fragmentEntryLink;
+		}
+
+		return Optional.ofNullable(
+			_fragmentEntryLinkLocalService.fetchFragmentEntryLink(
+				fragmentEntryLink.getOriginalFragmentEntryLinkId())
+		).filter(
+			originalFragmentEntryLink -> _isPlidRelatedToLayout(
+				_layoutLocalService.fetchLayout(fragmentEntryLink.getPlid()),
+				originalFragmentEntryLink.getPlid())
+		).map(
+			this::_getOriginalFragmentEntryLink
+		).orElse(
+			fragmentEntryLink
+		);
+	}
+
+	private boolean _isPlidRelatedToLayout(Layout layout, long plid) {
+		if (layout == null) {
+			return false;
+		}
+
+		if ((layout.getClassPK() == plid) || (layout.getPlid() == plid)) {
+			return true;
+		}
+
+		Layout draftLayout = layout.fetchDraftLayout();
+
+		if ((draftLayout != null) && (draftLayout.getPlid() == plid)) {
+			return true;
+		}
+
+		return false;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		FreeMarkerFragmentEntryProcessor.class);
 
@@ -234,5 +287,11 @@ public class FreeMarkerFragmentEntryProcessor
 
 	@Reference
 	private FragmentEntryConfigurationParser _fragmentEntryConfigurationParser;
+
+	@Reference
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
 
 }
