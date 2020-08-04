c256154a90261fdbbeed39acd78d33b4270b8e3e
Kevin Lee <kevin.lee@liferay.com>
2020-08-04 15:55:23 -0700

    LPS-118034 Use menu hierarchy instead of page hierarchy

--- modules/apps/site-navigation/site-navigation-menu-item-layout/src/main/java/com/liferay/site/navigation/menu/item/layout/internal/type/LayoutSiteNavigationMenuItemType.java
+++ modules/apps/site-navigation/site-navigation-menu-item-layout/src/main/java/com/liferay/site/navigation/menu/item/layout/internal/type/LayoutSiteNavigationMenuItemType.java
@@ -50,6 +50,7 @@ import com.liferay.site.navigation.constants.SiteNavigationWebKeys;
 import com.liferay.site.navigation.menu.item.layout.constants.SiteNavigationMenuItemTypeConstants;
 import com.liferay.site.navigation.menu.item.layout.internal.constants.SiteNavigationMenuItemTypeLayoutWebKeys;
 import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalService;
 import com.liferay.site.navigation.type.SiteNavigationMenuItemType;
 import com.liferay.site.navigation.type.SiteNavigationMenuItemTypeContext;
 
@@ -58,6 +59,7 @@ import java.io.IOException;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Stack;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.PortletURL;
@@ -387,9 +389,33 @@ public class LayoutSiteNavigationMenuItemType
 			Layout curLayout)
 		throws PortalException {
 
-		Layout layout = _fetchLayout(siteNavigationMenuItem);
+		if (!selectable) {
+			return false;
+		}
 
-		return layout.isChildSelected(selectable, curLayout);
+		Stack<SiteNavigationMenuItem> descendants = new Stack<>();
+
+		descendants.addAll(
+			_siteNavigationMenuItemLocalService.getSiteNavigationMenuItems(
+				siteNavigationMenuItem.getSiteNavigationMenuId(),
+				siteNavigationMenuItem.getSiteNavigationMenuItemId()));
+
+		while (!descendants.isEmpty()) {
+			SiteNavigationMenuItem descendant = descendants.pop();
+
+			Layout descendantLayout = _fetchLayout(descendant);
+
+			if (descendantLayout.getPlid() == curLayout.getPlid()) {
+				return true;
+			}
+
+			descendants.addAll(
+				_siteNavigationMenuItemLocalService.getSiteNavigationMenuItems(
+					descendant.getSiteNavigationMenuId(),
+					descendant.getSiteNavigationMenuItemId()));
+		}
+
+		return false;
 	}
 
 	@Override
@@ -398,10 +424,17 @@ public class LayoutSiteNavigationMenuItemType
 			Layout curLayout)
 		throws Exception {
 
+		if (!selectable) {
+			return false;
+		}
+
 		Layout layout = _fetchLayout(siteNavigationMenuItem);
 
-		return layout.isSelected(
-			selectable, curLayout, curLayout.getAncestorPlid());
+		if (layout.getPlid() == curLayout.getPlid()) {
+			return true;
+		}
+
+		return false;
 	}
 
 	@Override
@@ -548,5 +581,9 @@ public class LayoutSiteNavigationMenuItemType
 		unbind = "-"
 	)
 	private ServletContext _servletContext;
+
+	@Reference
+	private SiteNavigationMenuItemLocalService
+		_siteNavigationMenuItemLocalService;
 
 }
