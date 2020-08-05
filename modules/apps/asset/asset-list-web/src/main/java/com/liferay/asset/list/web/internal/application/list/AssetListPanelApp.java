2baaa0921a6be6a7be793cd39aa11b62a3fc6d6a
Pavel Savinov <pavel.savinov@liferay.com>
2020-08-05 11:38:41 +0200

    LPS-118214 Move collections to the site builder section instead of content & data

--- modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/application/list/AssetListPanelApp.java
+++ modules/apps/asset/asset-list-web/src/main/java/com/liferay/asset/list/web/internal/application/list/AssetListPanelApp.java
@@ -34,7 +34,7 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	property = {
 		"panel.app.order:Integer=1500",
-		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONTENT
+		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_BUILD
 	},
 	service = PanelApp.class
 )
