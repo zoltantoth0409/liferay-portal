adb67411ec1cd07c04df177f97bfdd9bb6468060
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-08-05 20:44:59 +0200

    LPS-118615 Support metrics action in journal articles.

---
+++ modules/dxp/apps/analytics/analytics-reports-journal-impl/src/main/java/com/liferay/analytics/reports/journal/internal/item/action/provider/ViewInPanelJournalArticleContentDashboardItemActionProvider.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.analytics.reports.journal.internal.item.action.provider;
+
+import com.liferay.analytics.reports.info.action.provider.AnalyticsReportsContentDashboardItemActionProvider;
+import com.liferay.content.dashboard.item.action.ContentDashboardItemAction;
+import com.liferay.content.dashboard.item.action.exception.ContentDashboardItemActionException;
+import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemActionProvider;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ */
+@Component(service = ContentDashboardItemActionProvider.class)
+public class ViewInPanelJournalArticleContentDashboardItemActionProvider
+	implements ContentDashboardItemActionProvider<JournalArticle> {
+
+	@Override
+	public ContentDashboardItemAction getContentDashboardItemAction(
+			JournalArticle journalArticle,
+			HttpServletRequest httpServletRequest)
+		throws ContentDashboardItemActionException {
+
+		return _analyticsReportsContentDashboardItemActionProvider.
+			getContentDashboardItemAction(
+				JournalArticle.class.getName(),
+				journalArticle.getResourcePrimKey(), httpServletRequest);
+	}
+
+	@Override
+	public String getKey() {
+		return "showMetrics";
+	}
+
+	@Override
+	public ContentDashboardItemAction.Type getType() {
+		return ContentDashboardItemAction.Type.VIEW_IN_PANEL;
+	}
+
+	@Override
+	public boolean isShow(
+		JournalArticle journalArticle, HttpServletRequest httpServletRequest) {
+
+		try {
+			return _analyticsReportsContentDashboardItemActionProvider.
+				isShowContentDashboardItemAction(
+					JournalArticle.class.getName(),
+					journalArticle.getResourcePrimKey(), httpServletRequest);
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException, portalException);
+
+			return false;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ViewInPanelJournalArticleContentDashboardItemActionProvider.class);
+
+	@Reference
+	private AnalyticsReportsContentDashboardItemActionProvider
+		_analyticsReportsContentDashboardItemActionProvider;
+
+}
