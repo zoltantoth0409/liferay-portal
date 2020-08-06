de697ecec1dc12010fd1ac1415aa883846357384
Alicia Garcia <alicia.garcia@liferay.com>
2020-08-06 18:47:23 +0200

    LPS-114310 creation of TranslationLanguagePortalInstanceLifecycleListener to dynamic include languages on permissions

---
+++ modules/apps/translation/translation-service/src/main/java/com/liferay/translation/service/internal/instance/lifecycle/TranslationLanguagePortalInstanceLifecycleListener.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.translation.service.internal.instance.lifecycle;
+
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.security.permission.ResourceActions;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.util.FileUtil;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alicia Garcia
+ */
+@Component(service = PortalInstanceLifecycleListener.class)
+public class TranslationLanguagePortalInstanceLifecycleListener
+	extends BasePortalInstanceLifecycleListener {
+
+	@Override
+	public void portalInstanceRegistered(Company company) throws Exception {
+
+	}
+
+}
