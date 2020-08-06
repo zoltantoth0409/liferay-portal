2eac706bf513b173f2c66297df672b1686abb100
Greg Hurrell <greg.hurrell@liferay.com>
2020-08-06 13:13:03 +0200

    LPS-118339 Add remote-app-support-web
    This provides the DXP-side support for remote apps communicating via the
    remote apps JS SDK.
    
    This is the result of squashing numerous commits; the pre-squashed
    history can be seen at:
    
    https://github.com/wincent/liferay-portal/pull/364

---
+++ modules/apps/remote-app/remote-app-support-web/src/main/resources/META-INF/resources/index.js
@@ -0,0 +1,377 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+const APPS_TO_IDS = new Map();
+
+const APP_IDS = new Set();
+
+const ERROR_CODES = {
+	/* eslint-disable sort-keys */
+	UNKNOWN: -100,
+	ALREADY_REGISTERED: -101,
+	/* eslint-enable sort-keys */
+};
+
+export const REMOTE_APP_PROTOCOL = 'com.liferay.remote.app.protocol';
+
+export const VERSION = 0;
+
+const RESPONSES = {};
+
+function escape(untrusted) {
+	return untrusted
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&#39;');
+}
+
+function getNumber(object, property) {
+	if (has(object, property) && typeof object[property] === 'number') {
+		return object[property];
+	}
+}
+
+function getString(object, property) {
+	if (has(object, property) && typeof object[property] === 'string') {
+		return object[property];
+	}
+}
+
+function has(object, property) {
+	return Object.prototype.hasOwnProperty.call(object, property);
+}
+
+const LOG_PREFIX = `[HOST: remote-app-support]`;
+
+function log(message, ...rest) {
+	if (process.env.NODE_ENV === 'development') {
+		const messages = [...rest];
+
+		if (typeof message === 'string') {
+			messages.unshift(`${LOG_PREFIX}: ${message}`);
+		}
+		else {
+			messages.unshift(LOG_PREFIX, message);
+		}
+
+		// eslint-disable-next-line no-console
+		console.log(...messages);
+	}
+}
+
+function warning(message, ...rest) {
+	if (process.env.NODE_ENV === 'development') {
+		const messages = [...rest];
+
+		if (typeof message === 'string') {
+			messages.unshift(`${LOG_PREFIX}: ${message}`);
+		}
+		else {
+			messages.unshift(LOG_PREFIX, message);
+		}
+
+		console.warn(...messages);
+	}
+}
+
+function postMessage(target, data) {
+	target.postMessage(
+		{
+			...data,
+			protocol: REMOTE_APP_PROTOCOL,
+			role: 'host',
+			version: VERSION,
+		},
+		'*'
+	);
+}
+
+function receiveMessage(event) {
+	const {data, source} = event;
+
+	if (
+		data &&
+		getString(data, 'protocol') === REMOTE_APP_PROTOCOL &&
+		getString(data, 'role') === 'client' &&
+		getNumber(data, 'version') === 0
+	) {
+		const appID = getString(data, 'appID');
+
+		if (APPS_TO_IDS.has(source) && APPS_TO_IDS.get(source) !== appID) {
+			const message = `Attempted to use new UUID ${appID} with application that is already registered`;
+
+			warning(message);
+
+			postMessage(source, {
+				appID,
+				code: ERROR_CODES.ALREADY_REGISTERED,
+				kind: 'error',
+				message,
+			});
+
+			return;
+		}
+
+		const command = getString(data, 'command');
+
+		switch (command) {
+			case 'fetch':
+				{
+					const {requestID} = data;
+
+					// TODO: more validation here
+
+					const resource = data.resource;
+					const init = data.init;
+
+					Liferay.Util.fetch(resource, init)
+						.then((response) => {
+							RESPONSES[requestID] = response;
+
+							postMessage(source, {
+								appID,
+								headers: Array.from(response.headers.entries()),
+								kind: 'fetch:resolve',
+								ok: response.ok,
+								redirected: response.redirected,
+								requestID,
+								status: response.status,
+								statusText: response.statusText,
+								type: response.type,
+								url: response.url,
+							});
+						})
+						.catch((error) => {
+							postMessage(source, {
+								appID,
+								error,
+								kind: 'fetch:reject',
+								requestID,
+							});
+						});
+				}
+				break;
+
+			case 'fetch:response:blob':
+				{
+					const {requestID} = data;
+
+					/* eslint-disable-next-line no-unused-expressions */
+					RESPONSES[requestID]
+						?.blob()
+						.then((blob) => {
+							postMessage(source, {
+								appID,
+								blob,
+								kind: 'fetch:response:blob:resolve',
+								requestID,
+							});
+						})
+						.catch((error) => {
+							postMessage(source, {
+								appID,
+								error,
+								kind: 'fetch:response:blob:reject',
+								requestID,
+							});
+						});
+				}
+				break;
+
+			case 'fetch:response:json':
+				{
+					const {requestID} = data;
+
+					/* eslint-disable-next-line no-unused-expressions */
+					RESPONSES[requestID]
+						?.json()
+						.then((json) => {
+							postMessage(source, {
+								appID,
+								json,
+								kind: 'fetch:response:json:resolve',
+								requestID,
+							});
+						})
+						.catch((error) => {
+							postMessage(source, {
+								appID,
+								error,
+								kind: 'fetch:response:json:reject',
+								requestID,
+							});
+						});
+				}
+				break;
+
+			case 'fetch:response:text':
+				{
+					const {requestID} = data;
+
+					/* eslint-disable-next-line no-unused-expressions */
+					RESPONSES[requestID]
+						?.text()
+						.then((text) => {
+							postMessage(source, {
+								appID,
+								kind: 'fetch:response:text:resolve',
+								requestID,
+								text,
+							});
+						})
+						.catch((error) => {
+							postMessage(source, {
+								appID,
+								error,
+								kind: 'fetch:response:text:reject',
+								requestID,
+							});
+						});
+				}
+				break;
+
+			case 'get':
+				{
+					const property = getString(data, 'property');
+
+					const {requestID} = data;
+
+					let value;
+
+					if (property === 'companyId') {
+						value = Liferay.ThemeDisplay.getCompanyId();
+					}
+					else if (property === 'css') {
+						value =
+							Liferay.ThemeDisplay.getCDNBaseURL() +
+							Liferay.ThemeDisplay.getPathThemeRoot() +
+							'/css/clay.css';
+					}
+					else if (property === 'defaultLanguageId') {
+						value = Liferay.ThemeDisplay.getDefaultLanguageId();
+					}
+					else if (property === 'isControlPanel') {
+
+						// Note: likely to always be `false`, because we only
+						// render remote apps in widgets on normal pages, for
+						// now.
+
+						value = Liferay.ThemeDisplay.isControlPanel();
+					}
+					else if (property === 'languageId') {
+						value = Liferay.ThemeDisplay.getLanguageId();
+					}
+					else if (property === 'isSignedIn') {
+						value = Liferay.ThemeDisplay.isSignedIn();
+					}
+					else if (property === 'userId') {
+						value = Liferay.ThemeDisplay.getUserId();
+					}
+					else if (property === 'userName') {
+						value = Liferay.ThemeDisplay.getUserName();
+					}
+
+					if (value !== undefined) {
+						postMessage(source, {
+							appID,
+							kind: 'get:resolve',
+							property,
+							requestID,
+							value,
+						});
+					}
+					else {
+						postMessage(source, {
+							appID,
+							error: `Unsupported property ${property}`,
+							kind: 'get:reject',
+							property,
+							requestID,
+						});
+					}
+				}
+				break;
+
+			// just a demo of a higher-level command
+
+			case 'openToast':
+				{
+					const type = getString(data, 'type') || 'info';
+					const message = getString(data, 'message');
+
+					if (message) {
+
+						// example of dealing with untrusted input
+
+						Liferay.Util.openToast({
+							message: escape(message),
+							type: escape(type),
+						});
+					}
+				}
+				break;
+
+			case 'register':
+				{
+					const uuid = getString(data, 'appID');
+
+					if (uuid) {
+						if (APP_IDS.has(uuid)) {
+							const message = `Attempted to register already registered UUID ${uuid}`;
+
+							warning(message);
+						}
+						else {
+							APPS_TO_IDS.set(source, uuid);
+
+							APP_IDS.add(uuid);
+
+							postMessage(source, {
+								appID: uuid,
+								kind: 'registered',
+							});
+						}
+					}
+				}
+				break;
+
+			case 'unregister':
+				{
+					if (APPS_TO_IDS.has(source) && APP_IDS.has(appID)) {
+						APPS_TO_IDS.delete(source);
+						APP_IDS.delete(appID);
+					}
+					else {
+						warning(
+							'Unable to unregister invalid source/ID combination'
+						);
+					}
+				}
+				break;
+
+			default:
+				warning(`Unexpected command: ${command}`);
+
+				break;
+		}
+	}
+}
+
+export default function init() {
+	log('init');
+
+	window.addEventListener('message', receiveMessage);
+}
---
+++ modules/apps/remote-app/remote-app-support-web/test/index.js
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {
+	REMOTE_APP_PROTOCOL,
+	VERSION,
+} from '../src/main/resources/META-INF/resources';
+
+const originalAddEventListener = window.__proto__.addEventListener;
+
+describe('remote-app-support-web', () => {
+	let iframe;
+	let init;
+	let ms;
+	let receiveMessage;
+	let reply;
+
+	beforeEach(() => {
+		jest.resetModules();
+
+		init = require('../src/main/resources/META-INF/resources').default;
+
+		iframe = document.createElement('iframe');
+
+		document.body.appendChild(iframe);
+
+		// Hack necessary "source" property into MessageEvent
+		// objects because jsdom sets it to null:
+		// https://github.com/jsdom/jsdom/blob/020539ed3f4/lib/jsdom/living/post-message.js
+
+		jest.spyOn(window, 'addEventListener').mockImplementation(
+			(name, callback) => {
+				originalAddEventListener.call(window, name, (event) => {
+					callback(
+						new window.MessageEvent(event.type, {
+							data: event.data,
+							lastEventId: event.lastEventId,
+							origin: event.origin,
+							ports: event.ports,
+							source: iframe.contentWindow,
+						})
+					);
+				});
+			}
+		);
+
+		ms = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+		let messagesReceived = 0;
+
+		reply = () => {
+			return new Promise((resolve) => {
+				(function (initialCount) {
+					const id = setInterval(() => {
+						if (messagesReceived > initialCount) {
+							clearTimeout(id);
+							resolve();
+						}
+					}, 15);
+				})(messagesReceived);
+			});
+		};
+
+		receiveMessage = jest.fn();
+
+		originalAddEventListener.call(
+			iframe.contentWindow,
+			'message',
+			(event) => {
+				receiveMessage(event);
+
+				messagesReceived++;
+			}
+		);
+
+		init();
+	});
+
+	describe('"register" command', () => {
+		it('acknowledges registration', async () => {
+			iframe.contentWindow.parent.postMessage(
+				{
+					appID: 'some UUID',
+					command: 'register',
+					protocol: REMOTE_APP_PROTOCOL,
+					role: 'client',
+					version: VERSION,
+				},
+				'*'
+			);
+
+			await reply();
+
+			expect(receiveMessage).toHaveBeenCalledTimes(1);
+			expect(receiveMessage.mock.calls[0][0].data).toEqual({
+				appID: 'some UUID',
+				kind: 'registered',
+				protocol: REMOTE_APP_PROTOCOL,
+				role: 'host',
+				version: VERSION,
+			});
+		});
+
+		it('does not register if protocol is incorrect', async () => {
+			iframe.contentWindow.parent.postMessage(
+				{
+					appID: 'some UUID',
+					command: 'register',
+					protocol: 'bogus protocol',
+					role: 'client',
+					version: VERSION,
+				},
+				'*'
+			);
+
+			await ms('15');
+
+			expect(receiveMessage).not.toHaveBeenCalled();
+		});
+
+		it('does not register if the version is unsupported', async () => {
+			iframe.contentWindow.parent.postMessage(
+				{
+					appID: 'some UUID',
+					command: 'register',
+					protocol: REMOTE_APP_PROTOCOL,
+					role: 'client',
+					version: 9000,
+				},
+				'*'
+			);
+
+			await ms('15');
+
+			expect(receiveMessage).not.toHaveBeenCalled();
+		});
+
+		it('does not register if the role is incorrect', async () => {
+			iframe.contentWindow.parent.postMessage(
+				{
+					appID: 'some UUID',
+					command: 'register',
+					protocol: REMOTE_APP_PROTOCOL,
+					role: 'junk',
+					version: VERSION,
+				},
+				'*'
+			);
+
+			await ms('15');
+
+			expect(receiveMessage).not.toHaveBeenCalled();
+		});
+	});
+});
