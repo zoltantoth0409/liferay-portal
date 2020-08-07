fc510b62a27234d07c9a786ae592aec933f14399
pei-jung <pei-jung.lan@liferay.com>
2020-08-06 17:05:50 -0700

    LPS-116762 Simplify

--- modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/model/listener/UserGroupModelListener.java
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/model/listener/UserGroupModelListener.java
@@ -24,13 +24,10 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.UserGroupLocalService;
-import com.liferay.portal.kernel.service.UserLocalService;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.LongStream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -42,14 +39,14 @@ import org.osgi.service.component.annotations.Reference;
 public class UserGroupModelListener extends BaseModelListener<UserGroup> {
 
 	@Override
-	public void onBeforeRemoveAssociation(
+	public void onAfterRemoveAssociation(
 			Object userGroupId, String associationClassName,
 			Object associationClassPK)
 		throws ModelListenerException {
 
 		try {
 			if (associationClassName.equals(User.class.getName())) {
-				unsubscribeDeletedGroupMemberFromSite(
+				_unsubscribeUserFromUserGroupGroups(
 					(long)associationClassPK, (long)userGroupId);
 			}
 		}
@@ -60,82 +57,41 @@ public class UserGroupModelListener extends BaseModelListener<UserGroup> {
 
 		try {
 			if (associationClassName.equals(Group.class.getName())) {
-				unsubscribeDeletedGroupFromSite(
+				_unsubscribeUserGroupUsersFromGroup(
 					(long)associationClassPK, (long)userGroupId);
 			}
 		}
 		catch (Exception exception) {
 			_log.error(
-				"Unsubscribe group from site failed because:\n" + exception);
+				"Unsubscribe user group from site failed because:\n" +
+					exception);
 		}
 	}
 
-	public void unsubscribeDeletedGroupFromSite(long groupId, long userGroupId)
-		throws PortalException {
-
-		long[] userIds = _userGroupLocalService.getUserPrimaryKeys(userGroupId);
-		long[] groupIds = {groupId};
-
-		unsubscribeUserFromSite(userGroupId, userIds, groupIds);
-	}
-
-	public void unsubscribeDeletedGroupMemberFromSite(
+	private void _unsubscribeUserFromUserGroupGroups(
 			long userId, long userGroupId)
 		throws PortalException {
 
-		long[] groupIds = _userGroupLocalService.getGroupPrimaryKeys(
+		List<Group> userGroupGroups = _groupLocalService.getUserGroupGroups(
 			userGroupId);
-		long[] userIds = {userId};
 
-		unsubscribeUserFromSite(userGroupId, userIds, groupIds);
+		userGroupGroups.removeAll(
+			_groupLocalService.getUserGroups(userId, true));
+
+		for (Group group : userGroupGroups) {
+			_blogsEntryLocalService.unsubscribe(userId, group.getGroupId());
+		}
 	}
 
-	public void unsubscribeUserFromSite(
-			long userGroupId, long[] userIds, long[] groupIds)
+	private void _unsubscribeUserGroupUsersFromGroup(
+			long groupId, long userGroupId)
 		throws PortalException {
 
-		for (long userId : userIds) {
-			Map<Long, long[]> userGroupGroupIds = new HashMap<>();
-			List<UserGroup> userGroups =
-				_userGroupLocalService.getUserUserGroups(userId);
+		for (long userId :
+				_userGroupLocalService.getUserPrimaryKeys(userGroupId)) {
 
-			for (UserGroup userGroup : userGroups) {
-				if (userGroup.getUserGroupId() != userGroupId) {
-					userGroupGroupIds.put(
-						userGroup.getPrimaryKey(),
-						_userGroupLocalService.getGroupPrimaryKeys(
-							userGroup.getUserGroupId()));
-				}
-			}
-
-			for (long groupId : groupIds) {
-				boolean siteContainsGroup = false;
-
-				for (Map.Entry<Long, long[]> entry :
-						userGroupGroupIds.entrySet()) {
-
-					if (LongStream.of(
-							entry.getValue()
-						).anyMatch(
-							x -> x == groupId
-						)) {
-
-						siteContainsGroup = true;
-					}
-				}
-
-				long[] groupIdUserIds = _userLocalService.getGroupUserIds(
-					groupId);
-
-				if (!siteContainsGroup &&
-					!LongStream.of(
-						groupIdUserIds
-					).anyMatch(
-						x -> x == userId
-					)) {
-
-					_blogsEntryLocalService.unsubscribe(userId, groupId);
-				}
+			if (!_groupLocalService.hasUserGroup(userId, groupId)) {
+				_blogsEntryLocalService.unsubscribe(userId, groupId);
 			}
 		}
 	}
@@ -147,9 +103,9 @@ public class UserGroupModelListener extends BaseModelListener<UserGroup> {
 	private BlogsEntryLocalService _blogsEntryLocalService;
 
 	@Reference
-	private UserGroupLocalService _userGroupLocalService;
+	private GroupLocalService _groupLocalService;
 
 	@Reference
-	private UserLocalService _userLocalService;
+	private UserGroupLocalService _userGroupLocalService;
 
 }
