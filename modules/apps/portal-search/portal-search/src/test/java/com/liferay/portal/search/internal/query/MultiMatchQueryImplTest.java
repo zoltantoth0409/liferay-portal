fb89be546a5e8a59eca245f160bc8b76af1f275b
Bryan Engler <bryan.engler@liferay.com>
2020-08-06 18:18:42 -0700

    LPS-117520 portal-search: Add MultiMatchQueryImplTest to test for UnsupportedOperationException in constructors
    https://issues.liferay.com/browse/LPS-117520

---
+++ modules/apps/portal-search/portal-search/src/test/java/com/liferay/portal/search/internal/query/MultiMatchQueryImplTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.query;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Bryan Engler
+ */
+public class MultiMatchQueryImplTest {
+
+	@Test
+	public void testMultiMatchQueryConstructor1() {
+		Object value = null;
+
+		Map<String, Float> fields = new HashMap<>();
+
+		fields.put("test", null);
+
+		MultiMatchQueryImpl multiMatchQueryImpl = new MultiMatchQueryImpl(
+			value, fields);
+
+		Assert.assertNotNull(multiMatchQueryImpl);
+	}
+
+	@Test
+	public void testMultiMatchQueryConstructor2() {
+		Object value = null;
+
+		Set<String> fields = new HashSet<>();
+
+		fields.add("test");
+
+		MultiMatchQueryImpl multiMatchQueryImpl = new MultiMatchQueryImpl(
+			value, fields);
+
+		Assert.assertNotNull(multiMatchQueryImpl);
+	}
+
+	@Test
+	public void testMultiMatchQueryConstructor3() {
+		Object value = null;
+		String[] fields = {"test"};
+
+		MultiMatchQueryImpl multiMatchQueryImpl = new MultiMatchQueryImpl(
+			value, fields);
+
+		Assert.assertNotNull(multiMatchQueryImpl);
+	}
+
+}
