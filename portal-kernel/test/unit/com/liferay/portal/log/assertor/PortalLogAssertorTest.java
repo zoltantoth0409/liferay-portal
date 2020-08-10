1ca907ef1231f5e31770d7ae7a463a530ad19236
jpince <jason.pince@liferay.com>
2020-08-10 10:32:03 -0700

    LPS-118753 Wordsmith

--- portal-kernel/test/unit/com/liferay/portal/log/assertor/PortalLogAssertorTest.java
+++ portal-kernel/test/unit/com/liferay/portal/log/assertor/PortalLogAssertorTest.java
@@ -160,7 +160,7 @@ public class PortalLogAssertorTest {
 					}
 
 					System.out.println(
-						"Detected error, dumping full log for reference:");
+						"Error detected, dumping the full log for reference:");
 
 					Files.copy(
 						Paths.get(
