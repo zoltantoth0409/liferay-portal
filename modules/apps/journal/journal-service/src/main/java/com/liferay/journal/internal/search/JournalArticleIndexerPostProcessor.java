6efd9bf1ee3a592df77658d027bacb61e08a4790
jesseyeh-liferay <jesse.yeh@liferay.com>
2020-08-10 16:33:34 -0700

    LPS-117494 Post-process Web Content summaries in search results

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalArticleIndexerPostProcessor.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.search;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.IndexerPostProcessor;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jesse Yeh
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.journal.model.JournalArticle",
+	service = IndexerPostProcessor.class
+)
+public class JournalArticleIndexerPostProcessor
+	implements IndexerPostProcessor {
+
+	@Override
+	public void postProcessContextBooleanFilter(
+			BooleanFilter booleanFilter, SearchContext searchContext)
+		throws Exception {
+	}
+
+	@Override
+	public void postProcessDocument(Document document, Object object)
+		throws Exception {
+	}
+
+	@Override
+	public void postProcessFullQuery(
+			BooleanQuery fullQuery, SearchContext searchContext)
+		throws Exception {
+	}
+
+	@Override
+	public void postProcessSearchQuery(
+			BooleanQuery searchQuery, BooleanFilter booleanFilter,
+			SearchContext searchContext)
+		throws Exception {
+	}
+
+	@Override
+	public void postProcessSummary(
+		Summary summary, Document document, Locale locale, String snippet) {
+
+		summary.setContent(
+			HtmlUtil.unescape(
+				StringUtil.replace(
+					summary.getContent(), "<br />", StringPool.NEW_LINE)));
+	}
+
+}
