95cf3c6e0177686e72f20f2b4ac06ac4330aa50b
Marcos <marcos.martins@liferay.com>
2020-08-10 16:10:13 -0300

    LPS-118759 add custom argument to form field template in order to enable react front end

---
+++ modules/sdk/project-templates/project-templates-form-field/src/main/java/com/liferay/project/templates/form/field/internal/FormFieldProjectTemplatesArgs.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.project.templates.form.field.internal;
+
+import com.beust.jcommander.Parameter;
+
+import com.liferay.project.templates.extensions.ProjectTemplatesArgsExt;
+
+/**
+ * @author Marcos Martins
+ */
+public class FormFieldProjectTemplatesArgs implements ProjectTemplatesArgsExt {
+
+	@Override
+	public String getTemplateName() {
+		return "form-field";
+	}
+
+	public boolean isReactTemplate() {
+		return _reactTemplate;
+	}
+
+	public void setReactTemplate(boolean reactTemplate) {
+		_reactTemplate = reactTemplate;
+	}
+
+	@Parameter(
+		description = "Specify if the form field front end template is written in React.",
+		names = "--react-template"
+	)
+	private boolean _reactTemplate;
+
+}
