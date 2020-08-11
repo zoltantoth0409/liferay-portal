fea5128e1114c9634bf0b78e3161600c5e50ac91
Bruno Basto <bruno.basto@liferay.com>
2020-08-10 23:01:00 -0300

    LPS-116361 Adds portlet registrar

---
+++ modules/apps/remote-app/remote-app-admin-web/src/main/java/com/liferay/remote/app/admin/web/internal/RemoteAppPortletRegistrar.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.remote.app.admin.web.internal;
+
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.remote.app.admin.web.internal.portlet.RemoteAppPortlet;
+import com.liferay.remote.app.model.RemoteAppEntry;
+import com.liferay.remote.app.service.RemoteAppEntryLocalService;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@Component(immediate = true, service = RemoteAppPortletRegistrar.class)
+public class RemoteAppPortletRegistrar {
+
+	public void registerPortlet(RemoteAppEntry remoteAppEntry) {
+		_registerPortlet(remoteAppEntry);
+	}
+
+	public void unregisterPortlet(RemoteAppEntry remoteAppEntry) {
+		_unregisterPortlet(remoteAppEntry.getEntryId());
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Starting remote apps");
+		}
+
+		for (RemoteAppEntry remoteAppEntry :
+				remoteAppEntryLocalService.getRemoteAppEntries(
+					QueryUtil.ALL_POS, QueryUtil.ALL_POS)) {
+
+			registerPortlet(remoteAppEntry);
+		}
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_log.isInfoEnabled()) {
+			_log.info("Stopping all remote apps");
+		}
+
+		Collection<RemoteAppPortlet> remoteAppPortlets;
+
+		synchronized (_remoteAppPortlets) {
+			remoteAppPortlets = _remoteAppPortlets.values();
+
+			_remoteAppPortlets.clear();
+		}
+
+		for (RemoteAppPortlet remoteAppPortlet : remoteAppPortlets) {
+			remoteAppPortlet.unregister();
+		}
+	}
+
+	@Reference
+	protected RemoteAppEntryLocalService remoteAppEntryLocalService;
+
+	private void _registerPortlet(RemoteAppEntry remoteAppEntry) {
+		RemoteAppPortlet remoteAppPortlet = new RemoteAppPortlet(
+			remoteAppEntry);
+
+		long remoteAppEntryId = remoteAppEntry.getEntryId();
+
+		synchronized (_remoteAppPortlets) {
+			if (_remoteAppPortlets.containsKey(remoteAppEntryId)) {
+				throw new IllegalStateException(
+					"Remote app " + remoteAppEntryId +
+						" is already registered.");
+			}
+
+			_remoteAppPortlets.put(remoteAppEntryId, remoteAppPortlet);
+
+			remoteAppPortlet.register(_bundleContext);
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Started remote app " + remoteAppPortlet.getName());
+		}
+	}
+
+	private void _unregisterPortlet(long id) {
+		RemoteAppPortlet remoteAppPortlet;
+
+		synchronized (_remoteAppPortlets) {
+			remoteAppPortlet = _remoteAppPortlets.remove(id);
+		}
+
+		if (remoteAppPortlet != null) {
+			remoteAppPortlet.unregister();
+
+			if (_log.isInfoEnabled()) {
+				_log.info("Stopped remote app " + remoteAppPortlet.getName());
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		RemoteAppPortletRegistrar.class);
+
+	private BundleContext _bundleContext;
+	private final Map<Long, RemoteAppPortlet> _remoteAppPortlets =
+		new HashMap<>();
+
+}
---
+++ modules/apps/remote-app/remote-app-admin-web/src/main/java/com/liferay/remote/app/admin/web/internal/portlet/RemoteAppPortlet.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.remote.app.admin.web.internal.portlet;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+import com.liferay.remote.app.model.RemoteAppEntry;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import javax.portlet.Portlet;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+public class RemoteAppPortlet extends MVCPortlet {
+
+	public RemoteAppPortlet(RemoteAppEntry remoteAppEntry) {
+		_remoteAppEntry = remoteAppEntry;
+	}
+
+	public String getName() {
+		return _remoteAppEntry.getNameCurrentLanguageId();
+	}
+
+	public String getName(Locale locale) {
+		return _remoteAppEntry.getName(locale);
+	}
+
+	public synchronized void register(BundleContext bundleContext) {
+		if (_serviceRegistration != null) {
+			throw new IllegalStateException("Portlet is already registered");
+		}
+
+		Dictionary<String, Object> properties = new Hashtable<>();
+
+		properties.put(
+			"com.liferay.portlet.css-class-wrapper", "portlet-remote-app");
+		properties.put(
+			"com.liferay.portlet.display-category", "category.sample");
+		properties.put(
+			"com.liferay.portlet.header-portlet-css", "/display/css/main.css");
+		properties.put("com.liferay.portlet.instanceable", true);
+		properties.put("javax.portlet.name", _getPortletName());
+		properties.put("javax.portlet.security-role-ref", "power-user,user");
+		properties.put(
+			"javax.portlet.resource-bundle", _getResourceBundleName());
+
+		_serviceRegistration = bundleContext.registerService(
+			Portlet.class, this, properties);
+
+		properties = new Hashtable<>();
+
+		properties.put("resource.bundle.base.name", _getResourceBundleName());
+		properties.put("servlet.context.name", "remote-app-admin-web");
+
+		_resourceBundleLoaderServiceRegistration =
+			bundleContext.registerService(
+				ResourceBundleLoader.class,
+				locale -> _getResourceBundle(locale), properties);
+	}
+
+	@Override
+	public void render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		try {
+			PrintWriter printWriter = renderResponse.getWriter();
+
+			printWriter.print(
+				"<iframe src=\"" + _remoteAppEntry.getUrl() + "\"></iframe>");
+
+			printWriter.flush();
+		}
+		catch (IOException ioException) {
+			_log.error("Unable to render HTML output", ioException);
+		}
+	}
+
+	public synchronized void unregister() {
+		if (_serviceRegistration == null) {
+			throw new IllegalStateException("Portlet is not registered");
+		}
+
+		_resourceBundleLoaderServiceRegistration.unregister();
+		_serviceRegistration.unregister();
+
+		_resourceBundleLoaderServiceRegistration = null;
+		_serviceRegistration = null;
+	}
+
+	private String _getPortletName() {
+		return "remote_app_" + _remoteAppEntry.getEntryId();
+	}
+
+	private ResourceBundle _getResourceBundle(Locale locale) {
+		return new ResourceBundle() {
+
+			@Override
+			public Enumeration<String> getKeys() {
+				return Collections.enumeration(_labels.keySet());
+			}
+
+			@Override
+			protected Object handleGetObject(String key) {
+				return _labels.get(key);
+			}
+
+			private final Map<String, String> _labels = HashMapBuilder.put(
+				"javax.portlet.title." + _getPortletName(),
+				_remoteAppEntry.getName(locale)
+			).build();
+
+		};
+	}
+
+	private String _getResourceBundleName() {
+		return _getPortletName() + ".Language";
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		RemoteAppPortlet.class);
+
+	private final RemoteAppEntry _remoteAppEntry;
+	private ServiceRegistration<ResourceBundleLoader>
+		_resourceBundleLoaderServiceRegistration;
+	private ServiceRegistration<Portlet> _serviceRegistration;
+
+}
