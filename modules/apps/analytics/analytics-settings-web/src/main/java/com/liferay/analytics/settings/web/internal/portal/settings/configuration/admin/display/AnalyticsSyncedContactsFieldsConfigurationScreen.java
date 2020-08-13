db09709800ed57e1db2a4f7bf91662f960f96b83
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-08-12 19:33:48 -0700

    LRAC-6436 add configuration screen

---
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/portal/settings/configuration/admin/display/AnalyticsSyncedContactsFieldsConfigurationScreen.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.web.internal.portal.settings.configuration.admin.display;
+
+import com.liferay.configuration.admin.display.ConfigurationScreen;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component(immediate = true, service = ConfigurationScreen.class)
+public class AnalyticsSyncedContactsFieldsConfigurationScreen
+	extends BaseAnalyticsConfigurationScreen {
+
+	@Override
+	public String getKey() {
+		return "synced-contact-data";
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/edit_synced_contacts_fields.jsp";
+	}
+
+	@Override
+	protected ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.analytics.settings.web)",
+		unbind = "-"
+	)
+	private ServletContext _servletContext;
+
+}
