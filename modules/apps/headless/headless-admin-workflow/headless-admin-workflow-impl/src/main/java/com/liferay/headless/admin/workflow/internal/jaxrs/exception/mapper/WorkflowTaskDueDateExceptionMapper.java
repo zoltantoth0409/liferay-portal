d0a3a75efb5bebbba28e6b9b735d310d805cc2f5
Inácio Nery <inacio.nery@liferay.com>
2020-08-13 18:28:09 -0300

    LPS-118985 Add WorkflowTaskDueDateExceptionMapper

---
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/jaxrs/exception/mapper/WorkflowTaskDueDateExceptionMapper.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.workflow.internal.jaxrs.exception.mapper;
+
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.workflow.WorkflowTaskDueDateException;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Inácio Nery
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.Workflow)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.Workflow.WorkflowTaskDueDateExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class WorkflowTaskDueDateExceptionMapper
+	extends BaseExceptionMapper<WorkflowTaskDueDateException> {
+
+	@Override
+	protected Problem getProblem(
+		WorkflowTaskDueDateException workflowTaskDueDateException) {
+
+		return new Problem(
+			Response.Status.BAD_REQUEST,
+			_language.get(
+				ResourceBundleUtil.getModuleAndPortalResourceBundle(
+					_acceptLanguage.getPreferredLocale(),
+					WorkflowTaskDueDateExceptionMapper.class),
+				"please-enter-a-valid-due-date"));
+	}
+
+	@Context
+	private AcceptLanguage _acceptLanguage;
+
+	@Reference
+	private Language _language;
+
+}
