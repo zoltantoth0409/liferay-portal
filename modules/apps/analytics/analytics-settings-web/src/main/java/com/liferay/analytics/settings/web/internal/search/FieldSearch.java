fb2516feac1985c0fa60b02ed53bfc48c9c4f6d3
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-08-12 18:53:45 -0700

    LRAC-6446 FieldSearch

---
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/search/FieldSearch.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.web.internal.search;
+
+import com.liferay.analytics.settings.web.internal.model.Field;
+import com.liferay.portal.kernel.dao.search.DisplayTerms;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+/**
+ * @author Rachael Koestartyo
+ */
+public class FieldSearch extends SearchContainer<Field> {
+
+	public static final String EMPTY_RESULTS_MESSAGE = "there-are-no-results";
+
+	public static List<String> headerNames = new ArrayList<String>() {
+		{
+			add("attribute");
+			add("category");
+			add("data-type");
+		}
+	};
+
+	public FieldSearch(
+		PortletRequest portletRequest, int delta, PortletURL iteratorURL) {
+
+		super(
+			portletRequest, new DisplayTerms(portletRequest),
+			new DisplayTerms(portletRequest), DEFAULT_CUR_PARAM, delta,
+			iteratorURL, headerNames, EMPTY_RESULTS_MESSAGE);
+	}
+
+	public FieldSearch(PortletRequest portletRequest, PortletURL iteratorURL) {
+		this(portletRequest, DEFAULT_DELTA, iteratorURL);
+	}
+
+}
