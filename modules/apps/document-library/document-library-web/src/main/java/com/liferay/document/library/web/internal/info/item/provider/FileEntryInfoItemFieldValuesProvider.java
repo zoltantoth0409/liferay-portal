e6d2441a80c6ec9b4ab2fc0a6b2661722457120a
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-08-17 14:22:18 +0200

    LPS-119054 Take into account all the fields that were present in info display contributor

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/info/item/provider/FileEntryInfoItemFieldValuesProvider.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/info/item/provider/FileEntryInfoItemFieldValuesProvider.java
@@ -18,6 +18,7 @@ import com.liferay.asset.info.item.provider.AssetEntryInfoItemFieldSetProvider;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFileEntryConstants;
 import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.util.DLURLHelper;
 import com.liferay.document.library.web.internal.info.item.FileEntryInfoItemFields;
 import com.liferay.expando.info.item.provider.ExpandoInfoItemFieldSetProvider;
 import com.liferay.info.exception.NoSuchInfoItemException;
@@ -26,9 +27,18 @@ import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.info.item.InfoItemReference;
 import com.liferay.info.item.field.reader.InfoItemFieldReaderFieldSetProvider;
 import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.info.type.WebImage;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Validator;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -47,14 +57,8 @@ public class FileEntryInfoItemFieldValuesProvider
 	public InfoItemFieldValues getInfoItemFieldValues(FileEntry fileEntry) {
 		try {
 			return InfoItemFieldValues.builder(
-			).infoFieldValue(
-				new InfoFieldValue<>(
-					FileEntryInfoItemFields.descriptionInfoField,
-					fileEntry.getDescription())
-			).infoFieldValue(
-				new InfoFieldValue<>(
-					FileEntryInfoItemFields.titleInfoField,
-					fileEntry.getTitle())
+			).infoFieldValues(
+				_getFileEntryInfoFieldValues(fileEntry)
 			).infoFieldValues(
 				_getAssetEntryInfoFieldValues(fileEntry)
 			).infoFieldValues(
@@ -118,9 +122,123 @@ public class FileEntryInfoItemFieldValuesProvider
 		return Collections.emptyList();
 	}
 
+	private List<InfoFieldValue<Object>> _getFileEntryInfoFieldValues(
+		FileEntry fileEntry) {
+
+		ThemeDisplay themeDisplay = _getThemeDisplay();
+
+		try {
+			List<InfoFieldValue<Object>> fileEntryFieldValues =
+				new ArrayList<>();
+
+			fileEntryFieldValues.add(
+				new InfoFieldValue<>(
+					FileEntryInfoItemFields.fileName, fileEntry.getFileName()));
+
+			String mimeType = fileEntry.getMimeType();
+
+			fileEntryFieldValues.add(
+				new InfoFieldValue<>(
+					FileEntryInfoItemFields.mimeType, mimeType));
+
+			if (mimeType.startsWith("image")) {
+				WebImage fileURLWebImage = new WebImage(
+					_dlURLHelper.getDownloadURL(
+						fileEntry, fileEntry.getFileVersion(), null,
+						StringPool.BLANK));
+
+				fileURLWebImage.setAlt(fileEntry.getTitle());
+
+				fileEntryFieldValues.add(
+					new InfoFieldValue<>(
+						FileEntryInfoItemFields.fileURL, fileURLWebImage));
+			}
+
+			fileEntryFieldValues.add(
+				new InfoFieldValue<>(
+					FileEntryInfoItemFields.titleInfoField,
+					fileEntry.getDescription()));
+			fileEntryFieldValues.add(
+				new InfoFieldValue<>(
+					FileEntryInfoItemFields.descriptionInfoField,
+					fileEntry.getDescription()));
+			fileEntryFieldValues.add(
+				new InfoFieldValue<>(
+					FileEntryInfoItemFields.versionInfoField,
+					fileEntry.getVersion()));
+			fileEntryFieldValues.add(
+				new InfoFieldValue<>(
+					FileEntryInfoItemFields.size, fileEntry.getSize()));
+
+			User user = _userLocalService.fetchUser(fileEntry.getUserId());
+
+			if (user != null) {
+				fileEntryFieldValues.add(
+					new InfoFieldValue<>(
+						FileEntryInfoItemFields.authorNameInfoField,
+						user.getFullName()));
+
+				if (themeDisplay != null) {
+					WebImage webImage = new WebImage(
+						user.getPortraitURL(themeDisplay));
+
+					webImage.setAlt(user.getFullName());
+
+					fileEntryFieldValues.add(
+						new InfoFieldValue<>(
+							FileEntryInfoItemFields.authorProfileImageInfoField,
+							webImage));
+				}
+			}
+
+			fileEntryFieldValues.add(
+				new InfoFieldValue<>(
+					FileEntryInfoItemFields.publishDateInfoField,
+					fileEntry.getLastPublishDate()));
+
+			String downloadURL = _dlURLHelper.getDownloadURL(
+				fileEntry, fileEntry.getFileVersion(), null, StringPool.BLANK);
+
+			if (Validator.isNotNull(downloadURL)) {
+				fileEntryFieldValues.add(
+					new InfoFieldValue<>(
+						FileEntryInfoItemFields.downloadURL, downloadURL));
+			}
+
+			WebImage imagePreviewURLWebImage = new WebImage(
+				_dlURLHelper.getImagePreviewURL(fileEntry, null));
+
+			imagePreviewURLWebImage.setAlt(fileEntry.getTitle());
+
+			fileEntryFieldValues.add(
+				new InfoFieldValue<>(
+					FileEntryInfoItemFields.previewImage,
+					imagePreviewURLWebImage));
+
+			return fileEntryFieldValues;
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	private ThemeDisplay _getThemeDisplay() {
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext != null) {
+			return serviceContext.getThemeDisplay();
+		}
+
+		return null;
+	}
+
 	@Reference
 	private AssetEntryInfoItemFieldSetProvider
 		_assetEntryInfoItemFieldSetProvider;
+
+	@Reference
+	private DLURLHelper _dlURLHelper;
 
 	@Reference
 	private ExpandoInfoItemFieldSetProvider _expandoInfoItemFieldSetProvider;
@@ -128,5 +246,8 @@ public class FileEntryInfoItemFieldValuesProvider
 	@Reference
 	private InfoItemFieldReaderFieldSetProvider
 		_infoItemFieldReaderFieldSetProvider;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
