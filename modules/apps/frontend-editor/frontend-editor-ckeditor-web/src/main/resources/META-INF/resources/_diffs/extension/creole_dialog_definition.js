077a33ba84bd609716fac7fb0e281b2a7aaebb66
Julien Castelain <julien.castelain@liferay.com>
2020-08-18 12:54:37 +0200

    LPS-118797 Use the priority argument of `CKEDITOR.on`

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/extension/creole_dialog_definition.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/extension/creole_dialog_definition.js
@@ -12,53 +12,63 @@
  * details.
  */
 
-CKEDITOR.on('dialogDefinition', (event) => {
-	if (event.editor === ckEditor) {
-		var dialogName = event.data.name;
+CKEDITOR.on(
+	'dialogDefinition',
+	(event) => {
+		if (event.editor === ckEditor) {
+			var dialogName = event.data.name;
 
-		var dialogDefinition = event.data.definition;
+			var dialogDefinition = event.data.definition;
 
-		var infoTab;
+			var infoTab;
 
-		if (dialogName === 'cellProperties') {
-			infoTab = dialogDefinition.getContents('info');
+			if (dialogName === 'cellProperties') {
+				infoTab = dialogDefinition.getContents('info');
 
-			infoTab.remove('bgColor');
-			infoTab.remove('bgColorChoose');
-			infoTab.remove('borderColor');
-			infoTab.remove('borderColorChoose');
-			infoTab.remove('colSpan');
-			infoTab.remove('hAlign');
-			infoTab.remove('height');
-			infoTab.remove('htmlHeightType');
-			infoTab.remove('rowSpan');
-			infoTab.remove('vAlign');
-			infoTab.remove('width');
-			infoTab.remove('widthType');
-			infoTab.remove('wordWrap');
+				infoTab.remove('bgColor');
+				infoTab.remove('bgColorChoose');
+				infoTab.remove('borderColor');
+				infoTab.remove('borderColorChoose');
+				infoTab.remove('colSpan');
+				infoTab.remove('hAlign');
+				infoTab.remove('height');
+				infoTab.remove('htmlHeightType');
+				infoTab.remove('rowSpan');
+				infoTab.remove('vAlign');
+				infoTab.remove('width');
+				infoTab.remove('widthType');
+				infoTab.remove('wordWrap');
 
-			dialogDefinition.minHeight = 40;
-			dialogDefinition.minWidth = 210;
+				dialogDefinition.minHeight = 40;
+				dialogDefinition.minWidth = 210;
+			}
+			else if (
+				dialogName === 'table' ||
+				dialogName === 'tableProperties'
+			) {
+				infoTab = dialogDefinition.getContents('info');
+
+				infoTab.remove('cmbAlign');
+				infoTab.remove('cmbWidthType');
+				infoTab.remove('cmbWidthType');
+				infoTab.remove('htmlHeightType');
+				infoTab.remove('txtBorder');
+				infoTab.remove('txtCellPad');
+				infoTab.remove('txtCellSpace');
+				infoTab.remove('txtHeight');
+				infoTab.remove('txtSummary');
+				infoTab.remove('txtWidth');
+
+				dialogDefinition.minHeight = 180;
+				dialogDefinition.minWidth = 210;
+			}
+			else if (dialogName === 'image') {
+				dialogDefinition.removeContents('Link');
+				dialogDefinition.removeContents('advanced');
+			}
 		}
-		else if (dialogName === 'table' || dialogName === 'tableProperties') {
-			infoTab = dialogDefinition.getContents('info');
-
-			infoTab.remove('cmbAlign');
-			infoTab.remove('cmbWidthType');
-			infoTab.remove('cmbWidthType');
-			infoTab.remove('htmlHeightType');
-			infoTab.remove('txtCellPad');
-			infoTab.remove('txtCellSpace');
-			infoTab.remove('txtHeight');
-			infoTab.remove('txtSummary');
-			infoTab.remove('txtWidth');
-
-			dialogDefinition.minHeight = 180;
-			dialogDefinition.minWidth = 210;
-		}
-		else if (dialogName === 'image') {
-			dialogDefinition.removeContents('Link');
-			dialogDefinition.removeContents('advanced');
-		}
-	}
-});
+	},
+	null,
+	null,
+	100
+);
