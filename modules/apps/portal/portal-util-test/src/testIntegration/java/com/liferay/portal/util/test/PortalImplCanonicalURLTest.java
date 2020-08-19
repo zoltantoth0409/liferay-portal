0afef23cb6b1659e4aaa105757a98cc5c033afb7
Attila Bakay <attila.bakay@liferay.com>
2020-08-19 15:56:19 +0200

    LPS-119160 adding new test case for blogs canonicalURL

--- modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplCanonicalURLTest.java
+++ modules/apps/portal/portal-util-test/src/testIntegration/java/com/liferay/portal/util/test/PortalImplCanonicalURLTest.java
@@ -230,6 +230,31 @@ public class PortalImplCanonicalURLTest {
 	}
 
 	@Test
+	public void testCanonicalURLWithFriendlyURLForBlogs() throws Exception {
+		String portalDomain = "localhost";
+
+		ThemeDisplay themeDisplay = _createThemeDisplay(
+			portalDomain, _group, 8080, false);
+
+		for (String urlSeparator :
+				FriendlyURLResolverRegistryUtil.getURLSeparators()) {
+
+			String completeURL = _generateURL(
+				portalDomain, "8080", StringPool.BLANK, _group.getFriendlyURL(),
+				_layout1.getFriendlyURL() + urlSeparator + "blogs/content-name",
+				false);
+
+			Assert.assertEquals(
+				completeURL,
+				_portal.getCanonicalURL(
+					_http.addParameter(
+						completeURL, "_ga",
+						"2.237928582.786466685.1515402734-1365236376"),
+					themeDisplay, _layout1, false, false));
+		}
+	}
+
+	@Test
 	public void testCanonicalURLWithoutQueryString() throws Exception {
 		String portalDomain = "localhost";
 
