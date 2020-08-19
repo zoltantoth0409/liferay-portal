69b9e45953fb32e0d8f6e3e96ab501c33c7ef946
arthurchan35 <arthurchan35@gmail.com>
2020-08-19 18:30:46 +0800

    LPS-119169 DO NOT MERGE - Add performance test for static impl

---
+++ modules/apps/portal-remote/portal-remote-cors-impl/src/test/java/com/liferay/portal/remote/cors/internal/FastURLToCORSSupportMapperPerformanceTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.remote.cors.internal;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Arthur Chan
+ */
+public class FastURLToCORSSupportMapperPerformanceTest
+	extends URLToCORSSupportMapperPerformanceTest {
+
+	@Before
+	@Override
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Override
+	@Test
+	public void testGet() {
+		super.testGet();
+	}
+
+	@Override
+	protected URLToCORSSupportMapper createURLToCORSSupportMapper(
+		Map<String, CORSSupport> corsSupports) {
+
+		return new FastURLToCORSSupportMapper(corsSupports);
+	}
+
+}
