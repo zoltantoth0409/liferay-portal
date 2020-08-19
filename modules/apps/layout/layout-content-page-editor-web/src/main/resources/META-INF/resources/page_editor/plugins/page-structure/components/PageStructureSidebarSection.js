acc775fdef78ee16a1ebe4bab3ff309bdbd82760
Pablo Molina <contact@pablomolina.me>
2020-08-19 08:21:33 +0200

    LPS-118271 Do not reset panel size once it has been resized

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/PageStructureSidebarSection.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/PageStructureSidebarSection.js
@@ -81,10 +81,6 @@ export default function PageStructureSidebarSection({
 		};
 	}, [handlerElement, panelElement]);
 
-	useEffect(() => {
-		setPanelHeight(null);
-	}, [resizable, size]);
-
 	return (
 		<>
 			{resizable && (
