9b98d2a7ac7f43f9feaadd25682401229db97b04
Bruno Basto <bruno.basto@liferay.com>
2020-08-20 16:54:07 -0300

    LPS-119519 Adds CheckBox Filter

---
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/data/set/filter/BaseCheckBoxClayDataSetFilter.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.data.set.filter;
+
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * @author Marco Leo
+ */
+public abstract class BaseCheckBoxClayDataSetFilter
+	implements ClayDataSetFilter {
+
+	public abstract List<CheckBoxClayDataSetFilterItem>
+		getCheckBoxClayDataSetFilterItems(Locale locale);
+
+	public String getOperator() {
+		return "eq";
+	}
+
+	public ResourceBundle getResourceBundle(Locale locale) {
+		return ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+	}
+
+	@Override
+	public String getType() {
+		return "checkbox";
+	}
+
+}
---
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/java/com/liferay/frontend/taglib/clay/internal/data/set/filter/CheckBoxClayDataSetFilterContextContributor.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.clay.internal.data.set.filter;
+
+import com.liferay.frontend.taglib.clay.data.set.filter.BaseCheckBoxClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.filter.CheckBoxClayDataSetFilterItem;
+import com.liferay.frontend.taglib.clay.data.set.filter.ClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.filter.ClayDataSetFilterContextContributor;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	property = "clay.data.set.filter.type=checkbox",
+	service = ClayDataSetFilterContextContributor.class
+)
+public class CheckBoxClayDataSetFilterContextContributor
+	implements ClayDataSetFilterContextContributor {
+
+	public Map<String, Object> getClayDataSetFilterContext(
+		ClayDataSetFilter clayDataSetFilter, Locale locale) {
+
+		if (clayDataSetFilter instanceof BaseCheckBoxClayDataSetFilter) {
+			return _serialize(
+				(BaseCheckBoxClayDataSetFilter)clayDataSetFilter, locale);
+		}
+
+		return Collections.emptyMap();
+	}
+
+	private Map<String, Object> _serialize(
+		BaseCheckBoxClayDataSetFilter baseCheckBoxClayDataSetFilter,
+		Locale locale) {
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		List<CheckBoxClayDataSetFilterItem> checkBoxClayDataSetFilterItems =
+			baseCheckBoxClayDataSetFilter.getCheckBoxClayDataSetFilterItems(
+				locale);
+
+		for (CheckBoxClayDataSetFilterItem checkBoxClayDataSetFilterItem :
+				checkBoxClayDataSetFilterItems) {
+
+			String label = LanguageUtil.get(
+				resourceBundle, checkBoxClayDataSetFilterItem.getLabel());
+
+			jsonArray.put(
+				JSONUtil.put(
+					"label", label
+				).put(
+					"value", checkBoxClayDataSetFilterItem.getValue()
+				));
+		}
+
+		return HashMapBuilder.<String, Object>put(
+			"items", jsonArray
+		).build();
+	}
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}
