c00941fdae8d443d13ade30e4a3c961cdd097089
Gabriel Ramos <gabriel.ramos@liferay.com>
2020-08-20 13:19:01 -0300

    LPS-118593 Increase EditApp coverage

--- modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/test/pages/apps/edit/EditApp.es.js
+++ modules/dxp/apps/app-builder-workflow/app-builder-workflow-web/test/pages/apps/edit/EditApp.es.js
@@ -11,14 +11,32 @@
 
 import '@testing-library/jest-dom/extend-expect';
 import {waitForElementToBeRemoved} from '@testing-library/dom';
-import {cleanup, fireEvent, render} from '@testing-library/react';
+import {act, cleanup, fireEvent, render} from '@testing-library/react';
 import React from 'react';
 
 import EditApp from '../../../../src/main/resources/META-INF/resources/js/pages/apps/edit/EditApp.es';
 import AppContextProviderWrapper from '../../../AppContextProviderWrapper.es';
 
-const history = {
-	goBack: jest.fn(),
+const app = {
+	active: false,
+	appDeployments: [
+		{
+			settings: {},
+			type: 'standalone',
+		},
+	],
+	dataDefinitionId: 37497,
+	dataDefinitionName: 'Object 01',
+	dataLayoutId: 37625,
+	dataListViewId: 37628,
+	dateCreated: '2020-06-08T12:13:14Z',
+	dateModified: '2020-06-08T12:13:14Z',
+	id: 37634,
+	name: {
+		en_US: 'Test',
+	},
+	siteId: 20124,
+	userId: 20126,
 };
 
 const customObjectItems = {
@@ -113,6 +131,25 @@ const formViewItems = {
 	],
 };
 
+const history = {
+	push: jest.fn(),
+};
+
+const roleItems = {
+	items: [
+		{
+			availableLanguages: ['en-US'],
+			dateCreated: '2020-07-01T13:25:25Z',
+			dateModified: '2020-07-01T13:25:25Z',
+			description:
+				'Account Managers who belong to an organization can administer all accounts associated to that organization.',
+			id: 37238,
+			name: 'Account Manager',
+			roleType: 'organization',
+		},
+	],
+};
+
 const tableViewItems = {
 	items: [
 		{
@@ -132,309 +169,362 @@ const tableViewItems = {
 	],
 };
 
-const roleItems = {
-	items: [
+const workflow = {
+	appId: 37634,
+	appWorkflowStates: [
 		{
-			availableLanguages: ['en-US'],
-			dateCreated: '2020-07-01T13:25:25Z',
-			dateModified: '2020-07-01T13:25:25Z',
-			description:
-				'Account Managers who belong to an organization can administer all accounts associated to that organization.',
-			id: 37238,
-			name: 'Account Manager',
-			roleType: 'organization',
+			appWorkflowTransitions: [
+				{
+					name: 'Submit',
+					primary: true,
+					transitionTo: 'Step 1',
+				},
+			],
+			initial: true,
+			name: 'Start',
+		},
+		{
+			appWorkflowTransitions: [],
+			initial: false,
+			name: 'Closed',
+		},
+	],
+	appWorkflowTasks: [
+		{
+			appWorkflowDataLayoutLinks: [
+				{
+					dataLayoutId: 37625,
+					readOnly: true,
+				},
+			],
+			appWorkflowRoleAssignments: [
+				{
+					roleId: 37238,
+					roleName: 'Account Manager',
+				},
+			],
+			appWorkflowTransitions: [
+				{
+					name: 'Close',
+					primary: true,
+					transitionTo: 'Closed',
+				},
+			],
+			name: 'Step 1',
 		},
 	],
 };
 
+const errorMessage = 'Error on saving the app';
+
+const mockFetch = jest
+	.fn()
+	.mockRejectedValueOnce({errorMessage})
+	.mockResolvedValue(app);
+const mockGetItem = jest
+	.fn()
+	.mockResolvedValueOnce(roleItems)
+	.mockResolvedValueOnce(customObjectItems)
+	.mockResolvedValueOnce(customObjectItems)
+	.mockResolvedValueOnce(formViewItems)
+	.mockResolvedValueOnce(tableViewItems)
+	.mockResolvedValueOnce(roleItems)
+	.mockResolvedValueOnce(customObjectItems)
+	.mockResolvedValueOnce(app)
+	.mockResolvedValueOnce(workflow)
+	.mockResolvedValueOnce(customObjectItems.items[0])
+	.mockResolvedValueOnce(formViewItems)
+	.mockResolvedValueOnce(tableViewItems)
+	.mockResolvedValueOnce(customObjectItems)
+	.mockResolvedValueOnce(formViewItems)
+	.mockResolvedValueOnce(tableViewItems)
+	.mockResolvedValueOnce(formViewItems);
+const mockToast = jest.fn();
+
+jest.mock('frontend-js-web', () => ({
+	createResourceURL: jest.fn(() => 'http://resource_url?'),
+	debounce: jest.fn().mockResolvedValue(),
+	fetch: () => mockFetch(),
+}));
+
+jest.mock('app-builder-web/js/utils/client.es', () => ({
+	getItem: () => mockGetItem(),
+	parseResponse: (response) => response,
+}));
+
+jest.mock('app-builder-web/js/utils/toast.es', () => ({
+	__esModule: true,
+	errorToast: (message) => mockToast(message),
+	successToast: (message) => mockToast(message),
+}));
+
 describe('EditApp', () => {
 	afterEach(() => {
-		cleanup();
 		jest.restoreAllMocks();
 	});
 
-	it('renders control menu, upperToolbar, sidebar and steps components correctly when creating a new app', async () => {
-		fetch.mockResponseOnce(JSON.stringify(roleItems));
-		fetch.mockResponseOnce(JSON.stringify(customObjectItems));
-		fetch.mockResponseOnce(JSON.stringify(customObjectItems));
-		fetch.mockResponseOnce(JSON.stringify(formViewItems));
-		fetch.mockResponseOnce(JSON.stringify(tableViewItems));
-
-		const routeProps = {
-			history,
-			match: {params: {}},
-		};
-
-		const {
-			container,
+	describe('Creating an app', () => {
+		let container,
 			getAllByText,
 			getAllByTitle,
 			getByDisplayValue,
 			getByLabelText,
 			getByPlaceholderText,
 			getByText,
-			queryByText,
-		} = render(<EditApp {...routeProps} />, {
-			wrapper: AppContextProviderWrapper,
+			getByTitle,
+			queryByText;
+
+		beforeAll(() => {
+			const params = {};
+
+			const routeProps = {
+				match: {params},
+			};
+
+			const renderResult = render(
+				<AppContextProviderWrapper
+					appContext={{baseResourceUrl: '', namespace: ''}}
+					history={history}
+				>
+					<EditApp {...routeProps} />
+				</AppContextProviderWrapper>
+			);
+
+			container = renderResult.container;
+			getAllByText = renderResult.getAllByText;
+			getAllByTitle = renderResult.getAllByTitle;
+			getByDisplayValue = renderResult.getByDisplayValue;
+			getByLabelText = renderResult.getByLabelText;
+			getByPlaceholderText = renderResult.getByPlaceholderText;
+			getByText = renderResult.getByText;
+			getByTitle = renderResult.getByTitle;
+			queryByText = renderResult.queryByText;
 		});
 
-		const deployButton = getByText('deploy');
-		const nameInput = getByPlaceholderText('untitled-app');
-		const saveButton = getByText('save');
-		let stepNameInput = container.querySelector(
-			'.form-group-outlined input'
-		);
-		const steps = container.querySelectorAll('.step');
+		it('renders control menu, upperToolbar, sidebar and steps components correctly when creating a new app', async () => {
+			const deployButton = getByText('deploy');
+			const nameInput = getByPlaceholderText('untitled-app');
+			let stepNameInput = container.querySelector(
+				'.form-group-outlined input'
+			);
+			const steps = container.querySelectorAll('.step');
 
-		expect(queryByText('step-configuration')).toBeTruthy();
-		expect(queryByText('new-workflow-powered-app')).toBeTruthy();
-		expect(queryByText('cancel')).toBeTruthy();
-		expect(steps.length).toBe(2);
-		expect(steps[0]).toHaveTextContent('initial-step');
-		expect(steps[1]).toHaveTextContent('final-step');
-		expect(stepNameInput.value).toBe('initial-step');
+			expect(queryByText('step-configuration')).toBeTruthy();
+			expect(queryByText('new-workflow-powered-app')).toBeTruthy();
+			expect(queryByText('cancel')).toBeTruthy();
+			expect(steps.length).toBe(2);
+			expect(steps[0]).toHaveTextContent('initial-step');
+			expect(steps[1]).toHaveTextContent('final-step');
+			expect(stepNameInput.value).toBe('initial-step');
 
-		expect(nameInput.value).toBe('');
-		expect(saveButton).toBeDisabled();
+			expect(nameInput.value).toBe('');
+			expect(getByText('save')).toBeDisabled();
 
-		await fireEvent.click(getByText('data-and-views'));
+			await fireEvent.click(getByText('data-and-views'));
 
-		const sidebarHeader = document.querySelector('div.tab-title');
+			const sidebarHeader = document.querySelector('div.tab-title');
 
-		expect(queryByText('step-configuration')).toBeNull();
-		expect(sidebarHeader.children.length).toBe(2);
-		expect(sidebarHeader.children[1]).toHaveTextContent('data-and-views');
+			expect(queryByText('step-configuration')).toBeNull();
+			expect(sidebarHeader.children.length).toBe(2);
+			expect(sidebarHeader.children[1]).toHaveTextContent(
+				'data-and-views'
+			);
 
-		await waitForElementToBeRemoved(() =>
-			document.querySelector('span.loading-animation')
-		);
-		await fireEvent.click(getByText('Object 01'));
+			await waitForElementToBeRemoved(() =>
+				document.querySelector('span.loading-animation')
+			);
+			await fireEvent.click(getByText('Object 01'));
 
-		expect(getByLabelText('main-data-object')).toHaveTextContent(
-			'Object 01'
-		);
-		expect(getByLabelText('form-view')).toHaveTextContent(
-			'select-a-form-view'
-		);
-		expect(getByLabelText('table-view')).toHaveTextContent(
-			'select-a-table-view'
-		);
+			expect(getByLabelText('main-data-object')).toHaveTextContent(
+				'Object 01'
+			);
+			expect(getByLabelText('form-view')).toHaveTextContent(
+				'select-a-form-view'
+			);
+			expect(getByLabelText('table-view')).toHaveTextContent(
+				'select-a-table-view'
+			);
 
-		await waitForElementToBeRemoved(() =>
-			document.querySelector('span.loading-animation')
-		);
+			await waitForElementToBeRemoved(() =>
+				document.querySelector('span.loading-animation')
+			);
 
-		await fireEvent.click(getByText('Form 01'));
-		await fireEvent.click(getByText('Table 01'));
+			await fireEvent.click(getByText('Form 01'));
+			await fireEvent.click(getByText('Table 01'));
 
-		expect(getByLabelText('form-view')).toHaveTextContent('Form 01');
-		expect(getByLabelText('table-view')).toHaveTextContent('Table 01');
+			expect(getByLabelText('form-view')).toHaveTextContent('Form 01');
+			expect(getByLabelText('table-view')).toHaveTextContent('Table 01');
 
-		await fireEvent.change(nameInput, {target: {value: 'Test'}});
+			await fireEvent.change(nameInput, {target: {value: 'Test'}});
 
-		expect(deployButton).toBeEnabled();
+			expect(deployButton).toBeEnabled();
 
-		await fireEvent.click(container.querySelector('.arrow-plus-button'));
+			await fireEvent.click(getByTitle('create-new-step'));
 
-		expect(deployButton).toBeDisabled();
+			expect(deployButton).toBeDisabled();
 
-		stepNameInput = container.querySelector('.form-group-outlined input');
+			stepNameInput = container.querySelector(
+				'.form-group-outlined input'
+			);
 
-		expect(stepNameInput.value).toBe('step-x');
+			expect(stepNameInput.value).toBe('step-x');
 
-		await fireEvent.mouseDown(getByText('Account Manager'));
+			await fireEvent.mouseDown(getByText('Account Manager'));
 
-		expect(
-			container.querySelector('.label-dismissible span')
-		).toHaveTextContent('Account Manager');
+			expect(
+				container.querySelector('.label-dismissible span')
+			).toHaveTextContent('Account Manager');
 
-		await fireEvent.click(getByText('data-and-views'));
+			await fireEvent.click(getByText('data-and-views'));
 
-		await fireEvent.click(getByText('add-new-form-view'));
+			await fireEvent.click(getByText('add-new-form-view'));
 
-		const stepFormViews = container.querySelectorAll('.step-form-view');
+			const stepFormViews = container.querySelectorAll('.step-form-view');
 
-		expect(stepFormViews[0]).toHaveTextContent('Form 01');
+			expect(stepFormViews[0]).toHaveTextContent('Form 01');
 
-		await fireEvent.click(getAllByText('Form 02')[1]);
+			await fireEvent.click(getAllByText('Form 02')[1]);
 
-		expect(stepFormViews[1]).toHaveTextContent('Form 02');
+			expect(stepFormViews[1]).toHaveTextContent('Form 02');
 
-		await fireEvent.click(getAllByTitle('remove')[1]);
+			await fireEvent.click(getAllByTitle('remove')[1]);
 
-		await fireEvent.click(container.querySelector('.arrow-plus-button'));
+			await fireEvent.click(getByTitle('create-new-step'));
 
-		await fireEvent.click(getByText('actions'));
+			await fireEvent.click(getByText('actions'));
 
-		await fireEvent.change(getByDisplayValue('submit'), {
-			target: {value: 'Submit to'},
+			await fireEvent.change(getByDisplayValue('submit'), {
+				target: {value: 'Submit to'},
+			});
+
+			await fireEvent.click(getByText('add-new-action'));
+
+			await fireEvent.click(getByText('remove'));
+
+			await fireEvent.click(getAllByText('delete-step')[1]);
+
+			expect(deployButton).toBeEnabled();
+
+			await fireEvent.click(getByText('save'));
 		});
 
-		await fireEvent.click(getByText('add-new-action'));
+		it('calls error toast after saving', async () => {
+			expect(mockToast).toHaveBeenCalledWith(errorMessage);
 
-		await fireEvent.click(getByText('remove'));
+			await fireEvent.click(getByText('save'));
+		});
 
-		await fireEvent.click(getAllByText('delete-step')[1]);
+		it('calls success toast after saving', () => {
+			expect(mockToast).toHaveBeenCalledWith(
+				'the-app-was-saved-successfully'
+			);
 
-		expect(deployButton).toBeEnabled();
+			expect(history.push).toHaveBeenCalled();
+			cleanup();
+		});
 	});
 
-	it('renders upperToolbar and data and views with respective infos when editing an app', async () => {
-		const app = {
-			active: false,
-			appDeployments: [
-				{
-					settings: {},
-					type: 'standalone',
-				},
-			],
-			dataDefinitionId: 37497,
-			dataDefinitionName: 'Object 01',
-			dataLayoutId: 37625,
-			dataListViewId: 37628,
-			dateCreated: '2020-06-08T12:13:14Z',
-			dateModified: '2020-06-08T12:13:14Z',
-			id: 37634,
-			name: {
-				en_US: 'Test',
-			},
-			siteId: 20124,
-			userId: 20126,
-		};
-
-		const workflow = {
-			appId: 123,
-			appWorkflowStates: [
-				{
-					appWorkflowTransitions: [
-						{
-							name: 'Submit',
-							primary: true,
-							transitionTo: 'Step 1',
-						},
-					],
-					initial: true,
-					name: 'Start',
-				},
-				{
-					appWorkflowTransitions: [],
-					initial: false,
-					name: 'Closed',
-				},
-			],
-			appWorkflowTasks: [
-				{
-					appWorkflowDataLayoutLinks: [
-						{
-							dataLayoutId: 37625,
-							readOnly: true,
-						},
-					],
-					appWorkflowRoleAssignments: [
-						{
-							roleId: 37238,
-							roleName: 'Account Manager',
-						},
-					],
-					appWorkflowTransitions: [
-						{
-							name: 'Close',
-							primary: true,
-							transitionTo: 'Closed',
-						},
-					],
-					name: 'Step 1',
-				},
-			],
-		};
-
-		fetch.mockResponseOnce(JSON.stringify(roleItems));
-		fetch.mockResponseOnce(JSON.stringify(customObjectItems));
-		fetch.mockResponseOnce(JSON.stringify(app));
-		fetch.mockResponseOnce(JSON.stringify(workflow));
-		fetch.mockResponseOnce(JSON.stringify(customObjectItems.items[0]));
-		fetch.mockResponseOnce(JSON.stringify(formViewItems));
-		fetch.mockResponseOnce(JSON.stringify(tableViewItems));
-		fetch.mockResponseOnce(JSON.stringify(customObjectItems));
-		fetch.mockResponseOnce(JSON.stringify(formViewItems));
-		fetch.mockResponseOnce(JSON.stringify(tableViewItems));
-		fetch.mockResponseOnce(JSON.stringify(formViewItems));
-
+	describe('Editing an existing app', () => {
 		const routeProps = {
-			history,
 			match: {params: {appId: '37634'}},
 		};
 
-		const {
-			container,
-			getByLabelText,
-			getByPlaceholderText,
-			getByText,
-		} = render(<EditApp {...routeProps} />, {
-			wrapper: AppContextProviderWrapper,
+		let container, getByLabelText, getByPlaceholderText, getByText;
+
+		beforeAll(() => {
+			const renderResult = render(
+				<AppContextProviderWrapper history={history}>
+					<EditApp {...routeProps} />
+				</AppContextProviderWrapper>
+			);
+
+			container = renderResult.container;
+			getByLabelText = renderResult.getByLabelText;
+			getByPlaceholderText = renderResult.getByPlaceholderText;
+			getByText = renderResult.getByText;
 		});
 
-		await waitForElementToBeRemoved(() =>
-			document.querySelector('span.loading-animation')
-		);
+		it('renders upperToolbar and data and views with respective infos when editing an app', async () => {
+			await waitForElementToBeRemoved(() =>
+				document.querySelector('span.loading-animation')
+			);
 
-		const steps = container.querySelectorAll('.step-card');
-		let stepNameInput = container.querySelector(
-			'.form-group-outlined input'
-		);
+			const steps = container.querySelectorAll('.step-card');
+			let stepNameInput = container.querySelector(
+				'.form-group-outlined input'
+			);
 
-		expect(steps.length).toBe(3);
-		expect(steps[0]).toHaveTextContent('Start');
-		expect(steps[1]).toHaveTextContent('Step 1');
-		expect(steps[2]).toHaveTextContent('Closed');
-		expect(stepNameInput.value).toBe('Start');
+			expect(steps.length).toBe(3);
+			expect(steps[0]).toHaveTextContent('Start');
+			expect(steps[1]).toHaveTextContent('Step 1');
+			expect(steps[2]).toHaveTextContent('Closed');
+			expect(stepNameInput.value).toBe('Start');
 
-		const dataAndViewsButton = container.querySelectorAll(
-			'.sidebar-body button'
-		)[0];
+			const dataAndViewsButton = container.querySelectorAll(
+				'.sidebar-body button'
+			)[0];
 
-		expect(dataAndViewsButton).toHaveTextContent('Form 01');
-		expect(dataAndViewsButton).toHaveTextContent('Table 01');
+			expect(dataAndViewsButton).toHaveTextContent('Form 01');
+			expect(dataAndViewsButton).toHaveTextContent('Table 01');
 
-		await fireEvent.click(dataAndViewsButton);
+			await fireEvent.click(dataAndViewsButton);
 
-		await waitForElementToBeRemoved(() =>
-			document.querySelector('span.loading-animation')
-		);
+			await waitForElementToBeRemoved(() =>
+				document.querySelector('span.loading-animation')
+			);
 
-		expect(getByPlaceholderText('untitled-app').value).toBe('Test');
-		expect(getByLabelText('main-data-object')).toHaveTextContent(
-			'Object 01'
-		);
-		expect(getByLabelText('form-view')).toHaveTextContent('Form 01');
-		expect(getByLabelText('table-view')).toHaveTextContent('Table 01');
-		expect(getByText('deploy')).toBeEnabled();
+			expect(getByPlaceholderText('untitled-app').value).toBe('Test');
+			expect(getByLabelText('main-data-object')).toHaveTextContent(
+				'Object 01'
+			);
+			expect(getByLabelText('form-view')).toHaveTextContent('Form 01');
+			expect(getByLabelText('table-view')).toHaveTextContent('Table 01');
+			expect(getByText('deploy')).toBeEnabled();
 
-		await fireEvent.click(steps[1]);
+			await fireEvent.click(steps[1]);
 
-		stepNameInput = container.querySelector('.form-group-outlined input');
+			stepNameInput = container.querySelector(
+				'.form-group-outlined input'
+			);
 
-		expect(stepNameInput.value).toBe('Step 1');
-		expect(container.querySelector('h3.title')).toHaveTextContent(
-			'step-configuration'
-		);
+			expect(stepNameInput.value).toBe('Step 1');
+			expect(container.querySelector('h3.title')).toHaveTextContent(
+				'step-configuration'
+			);
 
-		expect(
-			container.querySelector('.label-dismissible span')
-		).toHaveTextContent('Account Manager');
-		expect(
-			container.querySelectorAll('.tab-button span')[1].parentElement
-		).toHaveTextContent('Form 01');
+			expect(
+				container.querySelector('.label-dismissible span')
+			).toHaveTextContent('Account Manager');
+			expect(
+				container.querySelectorAll('.tab-button span')[1].parentElement
+			).toHaveTextContent('Form 01');
 
-		await fireEvent.click(steps[2]);
+			await fireEvent.click(steps[2]);
 
-		stepNameInput = container.querySelector('.form-group-outlined input');
+			stepNameInput = container.querySelector(
+				'.form-group-outlined input'
+			);
 
-		expect(stepNameInput.value).toBe('Closed');
+			expect(stepNameInput.value).toBe('Closed');
 
-		await fireEvent.change(stepNameInput, {target: {value: 'End'}});
+			await fireEvent.change(stepNameInput, {target: {value: 'End'}});
 
-		expect(stepNameInput.value).toBe('End');
-		expect(steps[2]).toHaveTextContent('End');
+			expect(stepNameInput.value).toBe('End');
+			expect(steps[2]).toHaveTextContent('End');
+
+			await fireEvent.click(getByText('save'));
+		});
+
+		it('clicks cancel and navigate to list apps', async () => {
+			jest.useFakeTimers();
+
+			act(() => {
+				jest.runAllTimers();
+			});
+
+			await fireEvent.click(getByText('cancel'));
+
+			expect(history.push).toHaveBeenCalled();
+		});
 	});
 });
