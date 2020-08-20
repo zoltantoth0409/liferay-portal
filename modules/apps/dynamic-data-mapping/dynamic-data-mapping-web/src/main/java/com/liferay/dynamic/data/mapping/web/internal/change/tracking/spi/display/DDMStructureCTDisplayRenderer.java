eb51b0b338ae40e1902a5c7d25ee5ee4c6d769da
David Truong <david.truong@liferay.com>
2020-08-20 17:44:16 -0500

    LPS-119506 ddm structure display renderer

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/java/com/liferay/dynamic/data/mapping/web/internal/change/tracking/spi/display/DDMStructureCTDisplayRenderer.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.web.internal.change.tracking.spi.display;
+
+import com.liferay.change.tracking.spi.display.BaseCTDisplayRenderer;
+import com.liferay.change.tracking.spi.display.CTDisplayRenderer;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Truong
+ */
+@Component(immediate = true, service = CTDisplayRenderer.class)
+public class DDMStructureCTDisplayRenderer
+	extends BaseCTDisplayRenderer<DDMStructure> {
+
+	@Override
+	public Class<DDMStructure> getModelClass() {
+		return DDMStructure.class;
+	}
+
+	@Override
+	public String getTitle(Locale locale, DDMStructure ddmStructure)
+		throws PortalException {
+
+		return ddmStructure.getName(locale);
+	}
+
+	@Override
+	protected void buildDisplay(DisplayBuilder<DDMStructure> displayBuilder) {
+		DDMStructure ddmStructure = displayBuilder.getModel();
+
+		Locale locale = displayBuilder.getLocale();
+
+		displayBuilder.display(
+			"name", ddmStructure.getName(locale)
+		).display(
+			"created-by", ddmStructure.getUserName()
+		).display(
+			"create-date", ddmStructure.getCreateDate()
+		).display(
+			"last-modified", ddmStructure.getModifiedDate()
+		).display(
+			"version", ddmStructure.getVersion()
+		).display(
+			"description", ddmStructure.getDescription(locale)
+		).display(
+			"definition", ddmStructure.getDefinition()
+		).display(
+			"storage-type", ddmStructure.getStorageType()
+		).display(
+			"type", ddmStructure.getType()
+		);
+	}
+
+	@Reference
+	private Language _language;
+
+}
