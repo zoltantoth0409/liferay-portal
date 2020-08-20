5f8f3bdea75fb73302c07a04159cb955a9429101
Brian Chan <brian.chan@liferay.com>
2020-08-19 20:51:22 -0700

    LPS-119269 SF

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
@@ -443,7 +443,6 @@ public class GraphQLServletExtender {
 
 				List<GraphQLType> childrenGraphQLType1 =
 					graphQLType1.getChildren();
-
 				List<GraphQLType> childrenGraphQLType2 =
 					graphQLType2.getChildren();
 
