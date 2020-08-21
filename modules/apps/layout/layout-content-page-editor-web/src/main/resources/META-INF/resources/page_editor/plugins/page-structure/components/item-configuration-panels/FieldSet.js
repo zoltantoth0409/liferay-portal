4a9e22e339356a5365152de0099a9a8f3f6427e3
Sandro Chinea <sandrochinea@gmail.com>
2020-08-21 13:17:25 +0100

    LPS-119558 Adjust styles panel spacing

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/item-configuration-panels/FieldSet.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page-structure/components/item-configuration-panels/FieldSet.js
@@ -45,8 +45,8 @@ export const FieldSet = ({fields, item = {}, label, onValueSelect, values}) => {
 		availableFields.length > 0 && (
 			<>
 				{label && (
-					<div className="align-items-center d-flex justify-content-between page-editor__sidebar__fieldset-label pt-3">
-						<p className="mb-3 text-uppercase">{label}</p>
+					<div className="align-items-center d-flex justify-content-between page-editor__sidebar__fieldset-label pt-2">
+						<p className="mb-2 text-uppercase">{label}</p>
 					</div>
 				)}
 
