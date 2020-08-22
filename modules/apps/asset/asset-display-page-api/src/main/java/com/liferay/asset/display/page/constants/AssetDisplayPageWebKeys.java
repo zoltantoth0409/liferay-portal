59196caf9c5bc659b0bb638880cfd3758a142423
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-08-22 15:01:41 +0200

    LPS-118906 Deprecate keys

--- modules/apps/asset/asset-display-page-api/src/main/java/com/liferay/asset/display/page/constants/AssetDisplayPageWebKeys.java
+++ modules/apps/asset/asset-display-page-api/src/main/java/com/liferay/asset/display/page/constants/AssetDisplayPageWebKeys.java
@@ -26,9 +26,17 @@ public class AssetDisplayPageWebKeys {
 	public static final String CURRENT_I18N_LANGUAGE_ID =
 		"CURRENT_I18N_LANGUAGE_ID";
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String INFO_DISPLAY_OBJECT_PROVIDER =
 		"INFO_DISPLAY_OBJECT_PROVIDER";
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String INFO_EDIT_URL_PROVIDER =
 		"INFO_EDIT_URL_PROVIDER";
 
