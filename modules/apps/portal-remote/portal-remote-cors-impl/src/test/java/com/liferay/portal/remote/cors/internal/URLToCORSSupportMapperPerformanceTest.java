f44b3b1e5896451e9510bc1b7359f8319ee226e4
Brian Chan <brian.chan@liferay.com>
2020-08-24 12:12:43 -0700

    LPS-119169 Do not obfuscate code like this. We had this same issue a few pulls ago that I had you fix. You need to remember it so that we stop repeating the same mistakes. Using arrays like this is a pain to change in the future because I have to up all the numbers if I insert something in the middle.

--- modules/apps/portal-remote/portal-remote-cors-impl/src/test/java/com/liferay/portal/remote/cors/internal/URLToCORSSupportMapperPerformanceTest.java
+++ modules/apps/portal-remote/portal-remote-cors-impl/src/test/java/com/liferay/portal/remote/cors/internal/URLToCORSSupportMapperPerformanceTest.java
@@ -148,161 +148,109 @@ public class URLToCORSSupportMapperPerformanceTest {
 		corsSupports.put(urlPattern, corsSupport);
 	}
 
-	/**
-	 * URL patterns that are neither wildcard nor extension, are considered
-	 * exact URL patterns.
-	 */
-	private static final String[] _URL_PATTERNS_EXACT = {
-		"url/some/random/pattern/one", "/url/some/random/pattern/one/",
-		"url/some/random/pattern/one/", "/url/some/random/pattern/one",
-		"url/some/random/pattern/two", "/url/some/random/pattern/two/",
-		"url/some/random/pattern/two/", "/url/some/random/pattern/two",
-		"url/some/random/pattern/three", "/url/some/random/pattern/three/",
-		"url/some/random/pattern/three/", "/url/some/random/pattern/three",
-		"url/some/random/pattern/four", "/url/some/random/pattern/four/",
-		"url/some/random/pattern/four/", "/url/some/random/pattern/four",
-		"/url/some/random/pattern/one/*/", "url/some/random/pattern/one/*",
-		"/url/some/random/pattern/one*", "/url/some/random/pattern/two/*/",
-		"url/some/random/pattern/two/*", "/url/some/random/pattern/two*",
-		"/url/some/random/pattern/one/*/do/test",
-		"/url/some/random/pattern/two/*/do/test",
-		"url/some/random/pattern/do/test.mp3/",
-		"/url/some/random/pattern/do/test.mp3/"
-	};
-
-	/**
-	 * URL patterns that start with "*.", and end with a valid file extension
-	 * are considered extension URL patterns.
-	 */
-	private static final String[] _URL_PATTERNS_EXTENSION = {
-		"*.mov", "*.xml", "*.cpp", "*.xpm", "*.pdf", "*.deb", "*.doc", "*.bin",
-		"*.zip", "*.mp3", "*.jpg", "*.png", "*.txt", "*.tar", "*.jsp", "*.jspf"
-	};
-
-	/**
-	 * URL patterns that start with '/', and end with "/*" are considered
-	 * wildcard URL patterns.
-	 */
-	private static final String[] _URL_PATTERNS_WILDCARD = {
-		"/url/some/random/pattern/one/one/one/*",
-		"/url/some/random/pattern/one/one/*", "/url/some/random/pattern/one/*",
-		"/url/some/random/pattern/two/two/two/*",
-		"/url/some/random/pattern/two/two/*", "/url/some/random/pattern/two/*",
-		"/url/some/random/pattern/three/three/three/*",
-		"/url/some/random/pattern/three/three/*",
-		"/url/some/random/pattern/three/*",
-		"/url/some/random/pattern/four/four/four/*",
-		"/url/some/random/pattern/four/four/*",
-		"/url/some/random/pattern/four/*", "/url/some/random/pattern/*",
-		"/url/*", "/*", "/*/*", "//*", "/*//*",
-		"/url/some/random/pattern/do/test.mp3/*", "/do/test.mp3/*",
-		"/some/random/pattern/*", "/one/*"
-	};
-
 	private final KeyValuePair[] _expectedMatches = {
 
 		// Exact matches
 
-		new KeyValuePair("url/some/random/pattern/one", _URL_PATTERNS_EXACT[0]),
+		new KeyValuePair("url/some/random/pattern/one", "url/some/random/pattern/one"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one/", _URL_PATTERNS_EXACT[1]),
+			"/url/some/random/pattern/one/", "/url/some/random/pattern/one/"),
 		new KeyValuePair(
-			"url/some/random/pattern/one/", _URL_PATTERNS_EXACT[2]),
+			"url/some/random/pattern/one/", "url/some/random/pattern/one/"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one", _URL_PATTERNS_EXACT[3]),
+			"/url/some/random/pattern/one", "/url/some/random/pattern/one"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one/*/", _URL_PATTERNS_EXACT[16]),
+			"/url/some/random/pattern/one/*/", "/url/some/random/pattern/one/*/"),
 		new KeyValuePair(
-			"url/some/random/pattern/one/*", _URL_PATTERNS_EXACT[17]),
+			"url/some/random/pattern/one/*", "url/some/random/pattern/one/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one*", _URL_PATTERNS_EXACT[18]),
+			"/url/some/random/pattern/one*", "/url/some/random/pattern/one*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one/*/do/test", _URL_PATTERNS_EXACT[22]),
+			"/url/some/random/pattern/one/*/do/test", "/url/some/random/pattern/one/*/do/test"),
 		new KeyValuePair(
-			"url/some/random/pattern/do/test.mp3/", _URL_PATTERNS_EXACT[24]),
+			"url/some/random/pattern/do/test.mp3/", "url/some/random/pattern/do/test.mp3/"),
 		new KeyValuePair(
-			"/url/some/random/pattern/do/test.mp3/", _URL_PATTERNS_EXACT[25]),
+			"/url/some/random/pattern/do/test.mp3/", "/url/some/random/pattern/do/test.mp3/"),
 
 		// Wildcard matches
 
 		new KeyValuePair(
 			"/url/some/random/pattern/one/one/one/./some/very/long/./do/test",
-			_URL_PATTERNS_WILDCARD[0]),
+			"/url/some/random/pattern/one/one/one/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/one/one/one/./some/very/long/./do/test/",
-			_URL_PATTERNS_WILDCARD[0]),
+			"/url/some/random/pattern/one/one/one/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/one/one/one/do/test",
-			_URL_PATTERNS_WILDCARD[0]),
+			"/url/some/random/pattern/one/one/one/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one/one/one/", _URL_PATTERNS_WILDCARD[0]),
+			"/url/some/random/pattern/one/one/one/", "/url/some/random/pattern/one/one/one/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one/one/one", _URL_PATTERNS_WILDCARD[0]),
+			"/url/some/random/pattern/one/one/one", "/url/some/random/pattern/one/one/one/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/one/one/do/test",
-			_URL_PATTERNS_WILDCARD[1]),
+			"/url/some/random/pattern/one/one/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one/one/", _URL_PATTERNS_WILDCARD[1]),
+			"/url/some/random/pattern/one/one/", "/url/some/random/pattern/one/one/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one/one", _URL_PATTERNS_WILDCARD[1]),
+			"/url/some/random/pattern/one/one", "/url/some/random/pattern/one/one/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/one/do/test", _URL_PATTERNS_WILDCARD[2]),
+			"/url/some/random/pattern/one/do/test", "/url/some/random/pattern/one/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/one/*/do/test/t",
-			_URL_PATTERNS_WILDCARD[2]),
+			"/url/some/random/pattern/one/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/one/*/do/test/",
-			_URL_PATTERNS_WILDCARD[2]),
+			"/url/some/random/pattern/one/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/two/two/two/./some/very/long/./do/test",
-			_URL_PATTERNS_WILDCARD[3]),
+			"/url/some/random/pattern/two/two/two/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/two/two/two/./some/very/long/./do/test/",
-			_URL_PATTERNS_WILDCARD[3]),
+			"/url/some/random/pattern/two/two/two/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/two/two/two/do/test",
-			_URL_PATTERNS_WILDCARD[3]),
+			"/url/some/random/pattern/two/two/two/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/two/two/two/", _URL_PATTERNS_WILDCARD[3]),
+			"/url/some/random/pattern/two/two/two/", "/url/some/random/pattern/two/two/two/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/two/two/two", _URL_PATTERNS_WILDCARD[3]),
+			"/url/some/random/pattern/two/two/two", "/url/some/random/pattern/two/two/two/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/two/two/do/test",
-			_URL_PATTERNS_WILDCARD[4]),
+			"/url/some/random/pattern/two/two/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/two/two/", _URL_PATTERNS_WILDCARD[4]),
+			"/url/some/random/pattern/two/two/", "/url/some/random/pattern/two/two/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/two/two", _URL_PATTERNS_WILDCARD[4]),
+			"/url/some/random/pattern/two/two", "/url/some/random/pattern/two/two/*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/two/do/test", _URL_PATTERNS_WILDCARD[5]),
+			"/url/some/random/pattern/two/do/test", "/url/some/random/pattern/two/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/two/*/do/test/t",
-			_URL_PATTERNS_WILDCARD[5]),
+			"/url/some/random/pattern/two/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/two/*/do/test/",
-			_URL_PATTERNS_WILDCARD[5]),
+			"/url/some/random/pattern/two/*"),
 		new KeyValuePair(
 			"/url/some/random/pattern/five/five/five/",
-			_URL_PATTERNS_WILDCARD[12]),
+			"/url/some/random/pattern/*"),
 		new KeyValuePair(
-			"/url/random/one/two/three", _URL_PATTERNS_WILDCARD[13]),
+			"/url/random/one/two/three", "/url/*"),
 		new KeyValuePair(
-			"/random/one/two/three", _URL_PATTERNS_WILDCARD[14]),
-		new KeyValuePair("/*", _URL_PATTERNS_WILDCARD[15]),
-		new KeyValuePair("/", _URL_PATTERNS_WILDCARD[16]),
-		new KeyValuePair("/*/", _URL_PATTERNS_WILDCARD[17]),
+			"/random/one/two/three", "/*"),
+		new KeyValuePair("/*", "/*/*"),
+		new KeyValuePair("/", "//*"),
+		new KeyValuePair("/*/", "/*//*"),
 		new KeyValuePair(
-			"/url/some/random/pattern/do/test.mp3", _URL_PATTERNS_WILDCARD[18]),
-		new KeyValuePair("/do/test.mp3/do/test", _URL_PATTERNS_WILDCARD[19]),
-		new KeyValuePair("/do/test.mp3/", _URL_PATTERNS_WILDCARD[19]),
-		new KeyValuePair("/do/test.mp3", _URL_PATTERNS_WILDCARD[19]),
+			"/url/some/random/pattern/do/test.mp3", "/url/some/random/pattern/do/test.mp3/*"),
+		new KeyValuePair("/do/test.mp3/do/test", "/do/test.mp3/*"),
+		new KeyValuePair("/do/test.mp3/", "/do/test.mp3/*"),
+		new KeyValuePair("/do/test.mp3", "/do/test.mp3/*"),
 
 		// Extension matches
 
-		new KeyValuePair("random/path/do/test.mp3", _URL_PATTERNS_EXTENSION[9]),
-		new KeyValuePair(
-			"random/path/do/test.jsp", _URL_PATTERNS_EXTENSION[14]),
-		new KeyValuePair("test.jsp", _URL_PATTERNS_EXTENSION[14]),
+		new KeyValuePair("random/path/do/test.mp3", "*.mp3"),
+		new KeyValuePair("random/path/do/test.jsp", "*.jsp"),
+		new KeyValuePair("test.jsp", "*.jsp"),
 	};
 
 	private URLToCORSSupportMapper _urlToCORSSupportMapper;
