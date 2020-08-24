9fddb3994a622b6ba33acc2c2c1c7ddac76a4be7
Preston-Crary <preston.crary@liferay.com>
2020-08-24 16:10:17 -0700

    LPS-119644 Build Service.

--- portal-kernel/src/com/liferay/portal/kernel/service/PermissionService.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/PermissionService.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.kernel.service;
 
+import com.liferay.portal.kernel.change.tracking.CTAware;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
@@ -34,6 +35,7 @@ import org.osgi.annotation.versioning.ProviderType;
  * @generated
  */
 @AccessControlled
+@CTAware
 @JSONWebService
 @ProviderType
 @Transactional(
