0b8dda3d16a3c6574cc2a90b857359792dc2f82f
Keven <keven.leone@liferay.com>
2020-08-24 11:38:22 -0300

    LPS-119651 add dataDefinition props on FieldSet renderer

--- modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/entry/ViewEntry.es.js
+++ modules/apps/app-builder/app-builder-web/src/main/resources/META-INF/resources/js/pages/entry/ViewEntry.es.js
@@ -81,6 +81,7 @@ export function ViewDataLayoutPageValues({
 						{fieldGroup.fields.map((field) => (
 							<FieldPreview
 								dataDefinition={{
+									...dataDefinition,
 									dataDefinitionFields:
 										fieldGroup.nestedDataDefinitionFields,
 								}}
