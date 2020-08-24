a1aeeeb05fd50e9c212d618571c8fbddfa9b944a
Marco Leo <marco.leo@liferay.com>
2020-08-24 17:09:13 +0200

    COMMERCE-4534 Use PortletResourcePermissionDefinition instead of manually registering the service

--- modules/apps/commerce/commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/security/permission/resource/CommerceNotificationPortletResourcePermissionDefinition.java
+++ modules/apps/commerce/commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/security/permission/resource/CommerceNotificationPortletResourcePermissionDefinition.java
@@ -15,11 +15,14 @@
 package com.liferay.commerce.notification.internal.security.permission.resource;
 
 import com.liferay.commerce.admin.constants.CommerceAdminPortletKeys;
+import com.liferay.commerce.constants.CommercePortletKeys;
 import com.liferay.commerce.notification.constants.CommerceNotificationConstants;
 import com.liferay.exportimport.kernel.staging.permission.StagingPermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionLogic;
 import com.liferay.portal.kernel.security.permission.resource.StagedPortletPermissionLogic;
+import com.liferay.portal.kernel.security.permission.resource.definition.PortletResourcePermissionDefinition;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 
 import java.util.Dictionary;
@@ -35,34 +38,25 @@ import org.osgi.service.component.annotations.Reference;
  * @author Alessio Antonio Rendina
  */
 @Component(
-	immediate = true,
-	service = CommerceNotificationPortletResourcePermissionRegistrar.class
+	immediate = true, service = PortletResourcePermissionDefinition.class
 )
-public class CommerceNotificationPortletResourcePermissionRegistrar {
+public class CommerceNotificationPortletResourcePermissionDefinition
+	implements PortletResourcePermissionDefinition {
 
-	@Activate
-	protected void activate(BundleContext bundleContext) {
-		Dictionary<String, Object> properties = new HashMapDictionary<>();
+	@Override
+	public PortletResourcePermissionLogic[]
+	getPortletResourcePermissionLogics() {
 
-		properties.put(
-			"resource.name", CommerceNotificationConstants.RESOURCE_NAME);
-
-		_serviceRegistration = bundleContext.registerService(
-			PortletResourcePermission.class,
-			PortletResourcePermissionFactory.create(
-				CommerceNotificationConstants.RESOURCE_NAME,
-				new StagedPortletPermissionLogic(
-					_stagingPermission,
-					CommerceAdminPortletKeys.COMMERCE_ADMIN)),
-			properties);
+		return new PortletResourcePermissionLogic[] {
+			new StagedPortletPermissionLogic(
+				_stagingPermission, CommerceAdminPortletKeys.COMMERCE_ADMIN)
+		};
 	}
 
-	@Deactivate
-	protected void deactivate() {
-		_serviceRegistration.unregister();
+	@Override
+	public String getResourceName() {
+		return CommerceNotificationConstants.RESOURCE_NAME;
 	}
-
-	private ServiceRegistration<PortletResourcePermission> _serviceRegistration;
 
 	@Reference
 	private StagingPermission _stagingPermission;
