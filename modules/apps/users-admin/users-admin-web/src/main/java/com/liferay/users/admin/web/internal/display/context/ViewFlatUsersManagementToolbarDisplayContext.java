97c376b71febb6e9404e1724cf45e4a8b3de0312
drewbrokke <drew.brokke@liferay.com>
2020-08-25 17:34:46 -0500

    LPS-119517 users-admin-web: creates a ManagementToolbarDisplayContext class to handle just the management toolbar data

---
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/display/context/ViewFlatUsersManagementToolbarDisplayContext.java
@@ -0,0 +1,272 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.web.internal.display.context;
+
+import com.liferay.frontend.taglib.clay.servlet.taglib.display.context.SearchContainerManagementToolbarDisplayContext;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenuBuilder;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemList;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItemListBuilder;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.permission.PortalPermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portlet.usersadmin.search.UserSearchTerms;
+
+import java.util.List;
+
+import javax.portlet.PortletURL;
+
+/**
+ * @author Pei-Jung Lan
+ */
+public class ViewFlatUsersManagementToolbarDisplayContext
+	extends SearchContainerManagementToolbarDisplayContext {
+
+	public ViewFlatUsersManagementToolbarDisplayContext(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		SearchContainer<User> searchContainer, boolean showDeleteButton,
+		boolean showRestoreButton) {
+
+		super(
+			liferayPortletRequest.getHttpServletRequest(),
+			liferayPortletRequest, liferayPortletResponse, searchContainer);
+
+		_domain = ParamUtil.getString(liferayPortletRequest, "domain");
+		_navigation = ParamUtil.getString(
+			liferayPortletRequest, "navigation", "active");
+		_showDeleteButton = showDeleteButton;
+		_showRestoreButton = showRestoreButton;
+	}
+
+	@Override
+	public List<DropdownItem> getActionDropdownItems() {
+		return DropdownItemListBuilder.add(
+			() -> _showRestoreButton,
+			dropdownItem -> {
+				dropdownItem.setHref(
+					StringBundler.concat(
+						"javascript:", liferayPortletResponse.getNamespace(),
+						"deleteUsers('", Constants.RESTORE, "');"));
+				dropdownItem.setIcon("undo");
+				dropdownItem.setLabel(LanguageUtil.get(request, "activate"));
+				dropdownItem.setQuickAction(true);
+			}
+		).add(
+			() -> _showDeleteButton,
+			dropdownItem -> {
+				UserSearchTerms userSearchTerms =
+					(UserSearchTerms)searchContainer.getSearchTerms();
+
+				String action = Constants.DELETE;
+				String icon = "times-circle";
+
+				if (userSearchTerms.isActive()) {
+					action = Constants.DEACTIVATE;
+					icon = "hidden";
+				}
+
+				dropdownItem.setHref(
+					StringBundler.concat(
+						"javascript:", liferayPortletResponse.getNamespace(),
+						"deleteUsers('", action, "');"));
+				dropdownItem.setIcon(icon);
+				dropdownItem.setLabel(LanguageUtil.get(request, action));
+				dropdownItem.setQuickAction(true);
+			}
+		).build();
+	}
+
+	@Override
+	public String getClearResultsURL() {
+		PortletURL clearResultsURL = getPortletURL();
+
+		clearResultsURL.setParameter("domain", (String)null);
+		clearResultsURL.setParameter("keywords", StringPool.BLANK);
+		clearResultsURL.setParameter("navigation", (String)null);
+
+		return clearResultsURL.toString();
+	}
+
+	@Override
+	public CreationMenu getCreationMenu() {
+		return CreationMenuBuilder.addPrimaryDropdownItem(
+			dropdownItem -> {
+				dropdownItem.setHref(
+					liferayPortletResponse.createRenderURL(),
+					"mvcRenderCommandName", "/users_admin/edit_user");
+				dropdownItem.setLabel(LanguageUtil.get(request, "add-user"));
+			}
+		).build();
+	}
+
+	@Override
+	public List<DropdownItem> getFilterDropdownItems() {
+		DropdownItemList filterDropdownItems =
+			(DropdownItemList)super.getFilterDropdownItems();
+
+		filterDropdownItems.addGroup(
+			dropdownGroupItem -> {
+				dropdownGroupItem.setDropdownItems(_getDomainDropdownItems());
+				dropdownGroupItem.setLabel(
+					LanguageUtil.get(request, "filter-by-domain"));
+			});
+
+		return filterDropdownItems;
+	}
+
+	@Override
+	public List<LabelItem> getFilterLabelItems() {
+		return LabelItemListBuilder.add(
+			() -> _domain.equals("account-users"),
+			labelItem -> {
+				PortletURL removeLabelURL = getPortletURL();
+
+				removeLabelURL.setParameter("domain", (String)null);
+
+				labelItem.putData("removeLabelURL", removeLabelURL.toString());
+
+				labelItem.setCloseable(true);
+
+				String label = String.format(
+					"%s: %s", LanguageUtil.get(request, "domain"),
+					LanguageUtil.get(request, _domain));
+
+				labelItem.setLabel(label);
+			}
+		).add(
+			() -> _domain.equals("all"),
+			labelItem -> {
+				PortletURL removeLabelURL = getPortletURL();
+
+				removeLabelURL.setParameter("domain", (String)null);
+
+				labelItem.putData("removeLabelURL", removeLabelURL.toString());
+
+				labelItem.setCloseable(true);
+
+				String label = String.format(
+					"%s: %s", LanguageUtil.get(request, "domain"),
+					LanguageUtil.get(request, _domain));
+
+				labelItem.setLabel(label);
+			}
+		).add(
+			() -> !_navigation.equals("active"),
+			labelItem -> {
+				PortletURL removeLabelURL = getPortletURL();
+
+				removeLabelURL.setParameter("navigation", (String)null);
+
+				labelItem.putData("removeLabelURL", removeLabelURL.toString());
+
+				labelItem.setCloseable(true);
+
+				String label = String.format(
+					"%s: %s", LanguageUtil.get(request, "status"),
+					LanguageUtil.get(request, _navigation));
+
+				labelItem.setLabel(label);
+			}
+		).build();
+	}
+
+	@Override
+	public String getSearchActionURL() {
+		PortletURL searchActionURL = getPortletURL();
+
+		return searchActionURL.toString();
+	}
+
+	@Override
+	public String getSearchFormName() {
+		return "searchFm";
+	}
+
+	@Override
+	public Boolean isDisabled() {
+		return false;
+	}
+
+	@Override
+	public Boolean isShowCreationMenu() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		return PortalPermissionUtil.contains(
+			themeDisplay.getPermissionChecker(), ActionKeys.ADD_USER);
+	}
+
+	@Override
+	protected String[] getDisplayViews() {
+		return new String[] {"icon", "descriptive", "list"};
+	}
+
+	@Override
+	protected String getFilterNavigationDropdownItemsLabel() {
+		return LanguageUtil.get(request, "filter-by-status");
+	}
+
+	@Override
+	protected String getNavigation() {
+		return _navigation;
+	}
+
+	@Override
+	protected String[] getNavigationKeys() {
+		return new String[] {"active", "inactive"};
+	}
+
+	@Override
+	protected String[] getOrderByKeys() {
+		return new String[] {"first-name", "last-name", "screen-name"};
+	}
+
+	@Override
+	protected PortletURL getPortletURL() {
+		PortletURL portletURL = super.getPortletURL();
+
+		portletURL.setParameter("orderByCol", getOrderByCol());
+		portletURL.setParameter("orderByType", getOrderByType());
+
+		return portletURL;
+	}
+
+	private List<DropdownItem> _getDomainDropdownItems() {
+		return getDropdownItems(
+			getDefaultEntriesMap(
+				new String[] {"all", "company-users", "account-users"}),
+			getPortletURL(), "domain", _domain);
+	}
+
+	private final String _domain;
+	private final String _navigation;
+	private final boolean _showDeleteButton;
+	private final boolean _showRestoreButton;
+
+}
