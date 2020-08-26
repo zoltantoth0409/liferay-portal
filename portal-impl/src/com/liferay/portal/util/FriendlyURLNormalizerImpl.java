9a53105e54d7cb253eeed96e5ae99a7308bcd96e
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-08-26 18:20:42 +0200

    LPS-119836 Revert "LPS-117484 SF"
    This reverts commit 324e8166

--- portal-impl/src/com/liferay/portal/util/FriendlyURLNormalizerImpl.java
+++ portal-impl/src/com/liferay/portal/util/FriendlyURLNormalizerImpl.java
@@ -49,7 +49,8 @@ public class FriendlyURLNormalizerImpl implements FriendlyURLNormalizer {
 		}
 
 		String decodedFriendlyURL = HttpUtil.decodePath(
-			StringUtil.replace(friendlyURL, CharPool.PERCENT, CharPool.POUND));
+			StringUtil.replace(
+				friendlyURL, CharPool.PERCENT, CharPool.POUND));
 
 		StringBuilder sb = new StringBuilder(decodedFriendlyURL.length());
 
