9a16886fe140359bc0d943aecf7225e27fbaa8d9
Luca Pellizzon <luca.pellizzon@liferay.com>
2020-08-31 10:12:28 +0200

    COMMERCE-4624 manage guest order in session not only with cookies

---
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/events/LogoutPreAction.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.events;
+
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.events.Action;
+import com.liferay.portal.kernel.events.LifecycleAction;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.CookieKeys;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	enabled = false, immediate = true, property = "key=logout.events.pre",
+	service = LifecycleAction.class
+)
+public class LogoutPreAction extends Action {
+
+	@Override
+	public void run(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		try {
+			String domain = CookieKeys.getDomain(httpServletRequest);
+
+			Cookie[] cookies = httpServletRequest.getCookies();
+
+			for (Cookie cookie : cookies) {
+				String name = cookie.getName();
+
+				if (name.startsWith(
+						CommerceOrder.class.getName() + StringPool.POUND)) {
+
+					CookieKeys.deleteCookies(
+						httpServletRequest, httpServletResponse, domain, name);
+
+					break;
+				}
+			}
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LogoutPreAction.class);
+
+}
