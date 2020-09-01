8c3ce5f4d31266f238af64ca9b306b0c4fb24dfb
Pavel Savinov <pavel.savinov@liferay.com>
2020-09-01 15:38:02 +0200

    LPS-120186 Check if some editable element was selected from the collection item

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/CollectionActiveItemContext.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/CollectionActiveItemContext.js
@@ -14,8 +14,11 @@
 
 import React, {useContext, useState} from 'react';
 
+import {ITEM_TYPES} from '../config/constants/itemTypes';
+import {LAYOUT_DATA_ITEM_TYPES} from '../config/constants/layoutDataItemTypes';
+import {useSelector} from '../store/index';
 import {CollectionItemContext, INITIAL_STATE} from './CollectionItemContext';
-import {useIsActive} from './Controls';
+import {useActiveItemId, useActiveItemType, useIsActive} from './Controls';
 
 const CollectionActiveItemDispatchContext = React.createContext(() => {});
 const CollectionActiveItemStateContext = React.createContext(INITIAL_STATE);
@@ -33,11 +36,21 @@ export function CollectionActiveItemContextProvider({children}) {
 }
 
 export function useSetCollectionActiveItemContext(itemId) {
+	const activeItemId = useActiveItemId();
+	const activeItemType = useActiveItemType();
 	const isActive = useIsActive();
 	const collectionContext = useContext(CollectionItemContext);
+	const layoutData = useSelector((state) => state.layoutData);
 	const setState = useContext(CollectionActiveItemDispatchContext);
 
-	if (isActive(itemId)) {
+	const item = layoutData.items[itemId];
+
+	if (
+		isActive(itemId) ||
+		(item.type === LAYOUT_DATA_ITEM_TYPES.fragment &&
+			activeItemType === ITEM_TYPES.editable &&
+			activeItemId.startsWith(item.config.fragmentEntryLinkId))
+	) {
 		setState(collectionContext);
 	}
 }
