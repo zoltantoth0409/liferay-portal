32bbd7f2f0f332b3c819e76d974ac4d0c2f65609
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-09-01 18:40:04 +0200

    LPS-119916 Use right userId

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/JournalArticleContentDashboardItem.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/JournalArticleContentDashboardItem.java
@@ -246,6 +246,10 @@ public class JournalArticleContentDashboardItem
 
 	@Override
 	public long getUserId() {
+		if (_latestApprovedJournalArticle != null) {
+			return _latestApprovedJournalArticle.getUserId();
+		}
+
 		return _journalArticle.getUserId();
 	}
 
