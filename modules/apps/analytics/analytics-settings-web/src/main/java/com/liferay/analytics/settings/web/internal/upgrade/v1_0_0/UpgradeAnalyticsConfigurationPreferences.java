37680f7980a412ca464e9ed7deecc613cd21db89
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-09-02 14:37:33 -0700

    LRAC-6448 select all user expando field names

--- modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/upgrade/v1_0_0/UpgradeAnalyticsConfigurationPreferences.java
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/upgrade/v1_0_0/UpgradeAnalyticsConfigurationPreferences.java
@@ -15,11 +15,18 @@
 package com.liferay.analytics.settings.web.internal.upgrade.v1_0_0;
 
 import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
+import com.liferay.analytics.settings.security.constants.AnalyticsSecurityConstants;
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 
+import java.io.Serializable;
+
 import java.util.Dictionary;
+import java.util.Map;
 
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
@@ -63,10 +70,31 @@ public class UpgradeAnalyticsConfigurationPreferences extends UpgradeProcess {
 			}
 
 			properties.put("syncedContactFieldNames", _CONTACT_FIELD_NAMES);
-			properties.put("syncedUserFieldNames", _USER_FIELD_NAMES);
+			properties.put(
+				"syncedUserFieldNames",
+				ArrayUtil.append(
+					_USER_FIELD_NAMES,
+					_getExpandoAttributeNames(
+						GetterUtil.getLong(properties.get("companyId")))));
 
 			configuration.update(properties);
 		}
+	}
+
+	private String[] _getExpandoAttributeNames(long companyId) {
+		User user = UserLocalServiceUtil.fetchUserByScreenName(
+			companyId, AnalyticsSecurityConstants.SCREEN_NAME_ANALYTICS_ADMIN);
+
+		if (user == null) {
+			return new String[0];
+		}
+
+		ExpandoBridge expandoBridge = user.getExpandoBridge();
+
+		Map<String, Serializable> attributes = expandoBridge.getAttributes(
+			false);
+
+		return ArrayUtil.toStringArray(attributes.keySet());
 	}
 
 	private static final String[] _CONTACT_FIELD_NAMES = {
