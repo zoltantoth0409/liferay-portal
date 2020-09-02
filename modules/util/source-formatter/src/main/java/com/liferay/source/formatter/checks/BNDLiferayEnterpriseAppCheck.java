6323d5b55a1595455da4bf9d2a16251f18f8d7d3
Alan Huang <alan.huang@liferay.com>
2020-09-02 17:46:14 +0800

    LPS-115714 Sort properties

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDLiferayEnterpriseAppCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDLiferayEnterpriseAppCheck.java
@@ -14,6 +14,9 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.NaturalOrderStringComparator;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.source.formatter.checks.util.BNDSourceUtil;
 
 import java.util.List;
@@ -59,13 +62,57 @@ public class BNDLiferayEnterpriseAppCheck extends BaseFileCheck {
 				addMessage(fileName, "Missing Liferay-Enterprise-App");
 			}
 
-			return content;
+			return BNDSourceUtil.updateInstruction(
+				content, "Liferay-Enterprise-App",
+				_sortProperties(liferayEnterpriseApp));
 		}
 
 		return content;
 	}
 
+	private String _sortProperties(String liferayEnterpriseApp) {
+		String[] propertiesArray = StringUtil.split(
+			liferayEnterpriseApp, StringPool.SEMICOLON);
+
+		PropertyComparator comparator = new PropertyComparator();
+
+		for (int i = 1; i < propertiesArray.length; i++) {
+			String property = propertiesArray[i];
+			String previousProperty = propertiesArray[i - 1];
+
+			int compare = comparator.compare(previousProperty, property);
+
+			if (compare > 0) {
+				liferayEnterpriseApp = StringUtil.replaceFirst(
+					liferayEnterpriseApp, previousProperty, property);
+				liferayEnterpriseApp = StringUtil.replaceLast(
+					liferayEnterpriseApp, property, previousProperty);
+			}
+		}
+
+		return liferayEnterpriseApp;
+	}
+
 	private static final String _ENTERPRISE_APP_MODULE_PATH_NAMES_KEY =
 		"enterpriseAppModulePathNames";
+
+	private class PropertyComparator extends NaturalOrderStringComparator {
+
+		public int compare(String property1, String property2) {
+			return super.compare(
+				_getPropertyName(property1), _getPropertyName(property2));
+		}
+
+		private String _getPropertyName(String property) {
+			int x = property.indexOf(StringPool.EQUAL);
+
+			if (x != -1) {
+				return property.substring(0, x);
+			}
+
+			return property;
+		}
+
+	}
 
 }
