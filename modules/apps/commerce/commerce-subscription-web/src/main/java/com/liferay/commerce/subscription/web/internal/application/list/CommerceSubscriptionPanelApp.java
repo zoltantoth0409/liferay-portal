2af4f9e9546649998fd2683351fae4da03078274
Alessio Rendina <asilcomando@gmail.com>
2020-09-03 15:11:16 +0200

    COMMERCE-4565 Refactor commerce subscription panel app

--- modules/apps/commerce/commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/application/list/CommerceSubscriptionPanelApp.java
+++ modules/apps/commerce/commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/application/list/CommerceSubscriptionPanelApp.java
@@ -25,12 +25,13 @@ import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Luca Pellizzon
+ * @author Alessio Antonio Rendina
  */
 @Component(
 	enabled = false, immediate = true,
 	property = {
-		"panel.app.order:Integer=900",
-		"panel.category.key=" + CommercePanelCategoryKeys.CONTROL_PANEL_COMMERCE
+		"panel.app.order:Integer=300",
+		"panel.category.key=" + CommercePanelCategoryKeys.COMMERCE_ORDERS_MANAGEMENT
 	},
 	service = PanelApp.class
 )
