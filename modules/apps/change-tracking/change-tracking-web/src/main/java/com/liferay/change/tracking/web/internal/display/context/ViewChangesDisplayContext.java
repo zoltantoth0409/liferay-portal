17b44945f2cd76c341bfaad424a01b0be554f912
Preston-Crary <preston.crary@liferay.com>
2020-09-03 15:01:59 -0700

    LPS-120073 Must render left side for published deletions.

--- modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/display/context/ViewChangesDisplayContext.java
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/display/context/ViewChangesDisplayContext.java
@@ -617,9 +617,17 @@ public class ViewChangesDisplayContext {
 					changeType = "deleted";
 				}
 
-				long ctCollectionId =
-					_ctDisplayRendererRegistry.getCtCollectionId(
-						_ctCollection, ctEntry);
+				long ctCollectionId = _ctCollection.getCtCollectionId();
+
+				if ((_ctCollection.getStatus() !=
+						WorkflowConstants.STATUS_APPROVED) ||
+					(ctEntry.getChangeType() !=
+						CTConstants.CT_CHANGE_TYPE_DELETION)) {
+
+					ctCollectionId =
+						_ctDisplayRendererRegistry.getCtCollectionId(
+							_ctCollection, ctEntry);
+				}
 
 				CTSQLModeThreadLocal.CTSQLMode ctSQLMode =
 					_ctDisplayRendererRegistry.getCTSQLMode(
