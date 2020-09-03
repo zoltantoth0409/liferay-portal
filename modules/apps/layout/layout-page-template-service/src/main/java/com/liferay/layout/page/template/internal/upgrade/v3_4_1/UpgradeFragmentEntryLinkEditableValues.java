9b51dae23d13aed608a088f976e13a075cb629be
Victor Galan <victor.galan@liferay.com>
2020-09-03 18:09:06 +0200

    LPS-120065 Replace also bottom spacing

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/v3_4_1/UpgradeFragmentEntryLinkEditableValues.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/upgrade/v3_4_1/UpgradeFragmentEntryLinkEditableValues.java
@@ -58,6 +58,7 @@ public class UpgradeFragmentEntryLinkEditableValues extends UpgradeProcess {
 				}
 
 				_replaceAlign(configurationJSONObject);
+				_replaceBottomSpacing(configurationJSONObject);
 				_replaceShadow(configurationJSONObject);
 				_replaceTextColor(configurationJSONObject);
 
@@ -92,6 +93,18 @@ public class UpgradeFragmentEntryLinkEditableValues extends UpgradeProcess {
 
 			break;
 		}
+	}
+
+	private void _replaceBottomSpacing(JSONObject configurationJSONObject) {
+		if (!configurationJSONObject.has("bottomSpacing")) {
+			return;
+		}
+
+		String bottomSpacing = GetterUtil.getString(
+			configurationJSONObject.remove("bottomSpacing"));
+
+		configurationJSONObject.put("marginBottom", bottomSpacing);
+		configurationJSONObject.remove("bottomSpacing");
 	}
 
 	private void _replaceShadow(JSONObject configurationJSONObject) {
