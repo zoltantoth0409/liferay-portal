b6259ca95bf14f79e32c4925f25a83b0fedd2951
pei-jung <pei-jung.lan@liferay.com>
2020-09-02 18:08:29 -0700

    LPS-119953 Enable upgrades

--- modules/apps/organizations/organizations-service/src/main/java/com/liferay/organizations/internal/upgrade/OrganizationServiceUpgrade.java
+++ modules/apps/organizations/organizations-service/src/main/java/com/liferay/organizations/internal/upgrade/OrganizationServiceUpgrade.java
@@ -28,9 +28,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Drew Brokke
  */
-@Component(
-	enabled = false, immediate = true, service = UpgradeStepRegistrator.class
-)
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
 public class OrganizationServiceUpgrade implements UpgradeStepRegistrator {
 
 	@Override
