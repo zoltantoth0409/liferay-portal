b842373b28062a72853ee92b7b835d1dc79377c2
Riccardo <riccardo.alberti@liferay.com>
2020-09-04 22:16:43 +0200

    COMMERCE-4629 Add DataGuard to subscription entry test

--- modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/subscription/test/CommerceSubscriptionEntryTest.java
+++ modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/subscription/test/CommerceSubscriptionEntryTest.java
@@ -46,6 +46,7 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
@@ -73,6 +74,7 @@ import org.junit.runner.RunWith;
 /**
  * @author Alessio Antonio Rendina
  */
+@DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class CommerceSubscriptionEntryTest {
 
