59e180ebb3116d95906823680313059c68151ca3
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-09-04 08:55:28 +0200

    LPS-120000 Language selector: Configuration was moved to internal package, so add a new upgrade step to correct this

--- modules/apps/site-navigation/site-navigation-language-web/src/main/java/com/liferay/site/navigation/language/web/internal/upgrade/SiteNavigationLanguageWebUpgrade.java
+++ modules/apps/site-navigation/site-navigation-language-web/src/main/java/com/liferay/site/navigation/language/web/internal/upgrade/SiteNavigationLanguageWebUpgrade.java
@@ -14,12 +14,15 @@
 
 package com.liferay.site.navigation.language.web.internal.upgrade;
 
+import com.liferay.portal.configuration.persistence.upgrade.ConfigurationUpgradeStepFactory;
 import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.site.navigation.language.web.internal.configuration.SiteNavigationLanguagePortletInstanceConfiguration;
 import com.liferay.site.navigation.language.web.internal.upgrade.v1_0_0.UpgradePortletId;
 import com.liferay.site.navigation.language.web.internal.upgrade.v1_0_0.UpgradePortletPreferences;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eudaldo Alonso
@@ -36,6 +39,17 @@ public class SiteNavigationLanguageWebUpgrade
 		registry.register(
 			"0.0.1", "1.0.0", new UpgradePortletId(),
 			new UpgradePortletPreferences());
+
+		registry.register(
+			"1.0.0", "1.0.1",
+			_configurationUpgradeStepFactory.createUpgradeStep(
+				"com.liferay.site.navigation.language.web.configuration." +
+					"SiteNavigationLanguagePortletInstanceConfiguration",
+				SiteNavigationLanguagePortletInstanceConfiguration.class.
+					getName()));
 	}
+
+	@Reference
+	private ConfigurationUpgradeStepFactory _configurationUpgradeStepFactory;
 
 }
