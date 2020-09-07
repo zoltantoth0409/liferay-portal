5711416e060ba06909f7d97cbf2a8f3d4c84e9cb
Alessio Rendina <asilcomando@gmail.com>
2020-09-07 17:34:36 +0200

    COMMERCE-4701 Fixed dataset action urls

--- modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/data_set_display/utilities/index.js
+++ modules/apps/frontend-taglib/frontend-taglib-clay/src/main/resources/META-INF/resources/data_set_display/utilities/index.js
@@ -115,14 +115,14 @@ export function formatActionURL(url, item) {
 	const replacedURL = url.replace(new RegExp('{(.*?)}', 'mg'), (matched) =>
 		getValueFromItem(
 			item,
-			matched.substring(1, matched.length - 1).split('|')
+			matched.substring(1, matched.length - 1).split('.')
 		)
 	);
 
 	return replacedURL.replace(new RegExp('(%7B.*?%7D)', 'mg'), (matched) =>
 		getValueFromItem(
 			item,
-			matched.substring(3, matched.length - 3).split('|')
+			matched.substring(3, matched.length - 3).split('.')
 		)
 	);
 }
