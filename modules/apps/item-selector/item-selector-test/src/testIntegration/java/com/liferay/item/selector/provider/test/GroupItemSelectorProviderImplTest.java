24e6e61d2bacd36b820dde543d866e736d008fd0
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-09-09 13:17:00 +0200

    LPS-106905 Adding more tests

--- modules/apps/item-selector/item-selector-test/src/testIntegration/java/com/liferay/item/selector/provider/test/GroupItemSelectorProviderImplTest.java
+++ modules/apps/item-selector/item-selector-test/src/testIntegration/java/com/liferay/item/selector/provider/test/GroupItemSelectorProviderImplTest.java
@@ -27,6 +27,7 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -75,6 +76,17 @@ public class GroupItemSelectorProviderImplTest {
 
 			Assert.assertTrue(groups.contains(company.getGroup()));
 		}
+	}
+
+	@Test
+	public void testGetIcon() {
+		Assert.assertEquals("sites", _groupItemSelectorProvider.getIcon());
+	}
+
+	@Test
+	public void testGetLabel() {
+		Assert.assertEquals(
+			"Site", _groupItemSelectorProvider.getLabel(LocaleUtil.US));
 	}
 
 	@DeleteAfterTestRun
