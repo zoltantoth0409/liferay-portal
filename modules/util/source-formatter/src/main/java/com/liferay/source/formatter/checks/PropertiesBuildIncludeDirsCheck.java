5ae7ee5f5c7cee1991e88e2a57930ff0d115b836
Alan Huang <alan.huang@liferay.com>
2020-09-10 06:56:33 +0800

    LPS-105380 Skip osb module

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesBuildIncludeDirsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PropertiesBuildIncludeDirsCheck.java
@@ -212,8 +212,8 @@ public class PropertiesBuildIncludeDirsCheck extends BaseFileCheck {
 	private static final String[] _SKIP_DIR_NAMES = {
 		".git", ".gradle", ".idea", ".m2", ".releng", ".settings", "bin",
 		"build", "classes", "dependencies", "node_modules",
-		"node_modules_cache", "private", "sql", "src", "test", "test-classes",
-		"test-coverage", "test-results", "tmp"
+		"node_modules_cache", "osb", "private", "sql", "src", "test",
+		"test-classes", "test-coverage", "test-results", "tmp"
 	};
 
 	private static final Pattern _ignoredModuleNamePattern = Pattern.compile(
