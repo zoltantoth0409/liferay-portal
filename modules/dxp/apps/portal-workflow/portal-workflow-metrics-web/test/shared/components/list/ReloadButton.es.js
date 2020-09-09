d18c7ab01d584c9fdb67ba8ebb3f8ec56b2ea6be
Albino Junior <albino.junior@liferay.com>
2020-09-09 17:27:17 -0300

    LPS-120643 Update test

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/test/shared/components/list/ReloadButton.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/test/shared/components/list/ReloadButton.es.js
@@ -9,21 +9,23 @@
  * distribution rights of the Software.
  */
 
+import {fireEvent, render} from '@testing-library/react';
 import React from 'react';
 
 import ReloadButton from '../../../../src/main/resources/META-INF/resources/js/shared/components/list/ReloadButton.es';
 
-test('Should component reload page', () => {
-	Object.defineProperty(window, 'location', {
-		value: {reload: jest.fn()},
-		writable: true,
+describe('ReloadButton', () => {
+	test('should call "reload" function when clicked', () => {
+		Object.defineProperty(window, 'location', {
+			value: {reload: jest.fn()},
+			writable: true,
+		});
+
+		const {getByText} = render(<ReloadButton />);
+		const reloadButton = getByText('reload-page');
+
+		fireEvent.click(reloadButton);
+
+		expect(window.location.reload).toHaveBeenCalled();
 	});
-
-	const component = shallow(<ReloadButton />);
-
-	const instance = component.instance();
-
-	instance.reloadPage();
-
-	expect(window.location.reload).toHaveBeenCalled();
 });
