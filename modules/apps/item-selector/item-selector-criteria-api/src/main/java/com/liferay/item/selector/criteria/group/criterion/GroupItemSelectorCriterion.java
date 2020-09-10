2fa03f4871a6fd0ef874551e3bd3180b6038a2ce
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-09-10 16:47:36 +0100

    LPS-116693 Adds new methods to GroupItemSelectorCriterion to allow show or not show the different tabs

--- modules/apps/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/group/criterion/GroupItemSelectorCriterion.java
+++ modules/apps/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/group/criterion/GroupItemSelectorCriterion.java
@@ -44,12 +44,32 @@ public class GroupItemSelectorCriterion extends BaseItemSelectorCriterion {
 		return _includeAllVisibleGroups;
 	}
 
+	public boolean isIncludeChildSites() {
+		return _includeChildSites;
+	}
+
 	public boolean isIncludeFormsSite() {
 		return _includeFormsSite;
 	}
 
+	public boolean isIncludeLayoutScopes() {
+		return _includeLayoutScopes;
+	}
+
+	public boolean isIncludeMySites() {
+		return _includeMySites;
+	}
+
+	public boolean isIncludeParentSites() {
+		return _includeParentSites;
+	}
+
 	public boolean isIncludeRecentSites() {
 		return _includeRecentSites;
+	}
+
+	public boolean isIncludeSitesThatIAdminister() {
+		return _includeSitesThatIAdminister;
 	}
 
 	public boolean isIncludeUserPersonalSite() {
@@ -68,12 +88,34 @@ public class GroupItemSelectorCriterion extends BaseItemSelectorCriterion {
 		_includeAllVisibleGroups = includeAllVisibleGroups;
 	}
 
+	public void setIncludeChildSites(boolean includeChildSites) {
+		_includeChildSites = includeChildSites;
+	}
+
 	public void setIncludeFormsSite(boolean includeFormsSite) {
 		_includeFormsSite = includeFormsSite;
 	}
 
+	public void setIncludeLayoutScopes(boolean includeLayoutScopes) {
+		_includeLayoutScopes = includeLayoutScopes;
+	}
+
+	public void setIncludeMySites(boolean includeMySites) {
+		_includeMySites = includeMySites;
+	}
+
+	public void setIncludeParentSites(boolean includeParentSites) {
+		_includeParentSites = includeParentSites;
+	}
+
 	public void setIncludeRecentSites(boolean includeRecentSites) {
 		_includeRecentSites = includeRecentSites;
+	}
+
+	public void setIncludeSitesThatIAdminister(
+		boolean includeSitesThatIAdminister) {
+
+		_includeSitesThatIAdminister = includeSitesThatIAdminister;
 	}
 
 	public void setIncludeUserPersonalSite(boolean includeUserPersonalSite) {
@@ -94,8 +136,13 @@ public class GroupItemSelectorCriterion extends BaseItemSelectorCriterion {
 
 	private boolean _allowNavigation = true;
 	private boolean _includeAllVisibleGroups;
+	private boolean _includeChildSites;
 	private boolean _includeFormsSite;
+	private boolean _includeLayoutScopes;
+	private boolean _includeMySites = true;
+	private boolean _includeParentSites;
 	private boolean _includeRecentSites = true;
+	private boolean _includeSitesThatIAdminister;
 	private boolean _includeUserPersonalSite;
 	private String _portletId;
 	private boolean _privateLayout;
