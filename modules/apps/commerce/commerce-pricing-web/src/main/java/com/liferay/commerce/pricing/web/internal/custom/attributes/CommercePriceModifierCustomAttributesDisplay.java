434d4337989f5bc89720b8f580ee58d050f1815d
Riccardo <riccardo.alberti@liferay.com>
2020-09-10 20:35:29 +0200

    COMMERCE-4767 Add custom attributes display for modifier and pricing class

---
+++ modules/apps/commerce/commerce-pricing-web/src/main/java/com/liferay/commerce/pricing/web/internal/custom/attributes/CommercePriceModifierCustomAttributesDisplay.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.pricing.web.internal.custom.attributes;
+
+import com.liferay.commerce.pricing.constants.CommercePricingPortletKeys;
+import com.liferay.commerce.pricing.model.CommercePriceModifier;
+import com.liferay.expando.kernel.model.BaseCustomAttributesDisplay;
+import com.liferay.expando.kernel.model.CustomAttributesDisplay;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Riccardo Alberti
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "javax.portlet.name=" + CommercePricingPortletKeys.COMMERCE_PRICE_LIST,
+	service = CustomAttributesDisplay.class
+)
+public class CommercePriceModifierCustomAttributesDisplay
+	extends BaseCustomAttributesDisplay {
+
+	@Override
+	public String getClassName() {
+		return CommercePriceModifier.class.getName();
+	}
+
+	@Override
+	public String getIconCssClass() {
+		return "tag";
+	}
+
+}
