8cc181ad0b4b37f11344789475711df393b03590
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-09-09 23:12:13 -0700

    LRAC-6450 missing memberships

--- modules/apps/analytics/analytics-message-sender-api/src/main/java/com/liferay/analytics/message/sender/model/listener/BaseEntityModelListener.java
+++ modules/apps/analytics/analytics-message-sender-api/src/main/java/com/liferay/analytics/message/sender/model/listener/BaseEntityModelListener.java
@@ -259,6 +259,7 @@ public abstract class BaseEntityModelListener<T extends BaseModel<T>>
 		List<String> attributeNames = new ArrayList<>();
 
 		attributeNames.add("expando");
+		attributeNames.add("memberships");
 
 		for (String name : _userAttributeNames) {
 			if (ArrayUtil.contains(
