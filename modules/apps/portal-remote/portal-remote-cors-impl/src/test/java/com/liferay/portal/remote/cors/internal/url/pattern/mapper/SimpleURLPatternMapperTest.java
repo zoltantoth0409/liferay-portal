64ae3ab318f2abf88bfa8036bb26c3fcd57d14b5
arthurchan35 <arthurchan35@gmail.com>
2020-09-11 16:50:42 +0800

    LPS-120269 Add test to show how star char acts as letter

--- modules/apps/portal-remote/portal-remote-cors-impl/src/test/java/com/liferay/portal/remote/cors/internal/url/pattern/mapper/SimpleURLPatternMapperTest.java
+++ modules/apps/portal-remote/portal-remote-cors-impl/src/test/java/com/liferay/portal/remote/cors/internal/url/pattern/mapper/SimpleURLPatternMapperTest.java
@@ -103,6 +103,8 @@ public class SimpleURLPatternMapperTest {
 		return new KeyValuePair[] {
 			new KeyValuePair("/", "//*"), new KeyValuePair("/*", "/*/*"),
 			new KeyValuePair("/*/", "/*//*"),
+			new KeyValuePair("/a/*/c/d", "/a/*/c/*"),
+			new KeyValuePair("/a/b", "/*"), new KeyValuePair("/a", "/*"),
 			new KeyValuePair("/c/portal/j_login", "/c/portal/j_login"),
 			new KeyValuePair("/documents", "/documents/*"),
 			new KeyValuePair("/documents/", "/documents/*"),
