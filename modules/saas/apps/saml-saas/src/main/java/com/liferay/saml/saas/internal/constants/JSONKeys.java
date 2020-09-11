3f3ce7b50429c70101df5c941bc8c69fef58b10a
Marta Medio <marta.medio@liferay.com>
2020-09-11 13:23:42 +0200

    LPS-118030 Add JSON Keys

---
+++ modules/saas/apps/saml-saas/src/main/java/com/liferay/saml/saas/internal/constants/JSONKeys.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.saml.saas.internal.constants;
+
+/**
+ * @author Marta Medio
+ */
+public class JSONKeys {
+
+	public static final String EXPANDO_VALUES = "expandoValues";
+
+	public static final String RESULT = "result";
+
+	public static final String RESULT_ERROR = "resultError";
+
+	public static final String RESULT_SUCCESS = "resultSuccess";
+
+	public static final String SAML_KEYSTORE = "samlKeystore";
+
+	public static final String SAML_PROVIDER_CONFIGURATION =
+		"samlProviderConfiguration";
+
+	public static final String SAML_SP_IDP_CONNECTIONS = "samlSpIdpConnections";
+
+}
