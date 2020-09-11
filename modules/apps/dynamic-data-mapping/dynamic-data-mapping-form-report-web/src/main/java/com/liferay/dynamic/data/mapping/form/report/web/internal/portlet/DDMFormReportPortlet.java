d17abeca762b809924b68ae386278bdcf7873317
Renato Rego <renato.rego@liferay.com>
2020-09-11 12:11:18 -0300

    LPS-119393 Add DDM prefix to CSS class to keep naming consistency.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/java/com/liferay/dynamic/data/mapping/form/report/web/internal/portlet/DDMFormReportPortlet.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/java/com/liferay/dynamic/data/mapping/form/report/web/internal/portlet/DDMFormReportPortlet.java
@@ -43,7 +43,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"com.liferay.portlet.css-class-wrapper=portlet-form-report",
+		"com.liferay.portlet.css-class-wrapper=portlet-ddm-form-report",
 		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.layout-cacheable=true",
