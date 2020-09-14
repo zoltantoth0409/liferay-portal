93e63aa0f9b586add21fab490649c1e7cbd38baf
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-09-14 22:52:12 +0200

    LPS-120853 New Benchmark property MAX_SEGMENTS_ENTRY_COUNT.

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsKeys.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsKeys.java
@@ -101,6 +101,9 @@ public interface BenchmarksPropsKeys {
 	public static final String MAX_MB_THREAD_COUNT =
 		"sample.sql.max.mb.thread.count";
 
+	public static final String MAX_SEGMENTS_ENTRY_COUNT =
+		"sample.sql.max.segments.entry.count";
+
 	public static final String MAX_USER_COUNT = "sample.sql.max.user.count";
 
 	public static final String MAX_USER_TO_GROUP_COUNT =
--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsValues.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/BenchmarksPropsValues.java
@@ -140,6 +140,9 @@ public class BenchmarksPropsValues {
 	public static final int MAX_MB_THREAD_COUNT = GetterUtil.getInteger(
 		PropertiesHolder._get(BenchmarksPropsKeys.MAX_MB_THREAD_COUNT));
 
+	public static final int MAX_SEGMENTS_ENTRY_COUNT = GetterUtil.getInteger(
+		PropertiesHolder._get(BenchmarksPropsKeys.MAX_SEGMENTS_ENTRY_COUNT));
+
 	public static final int MAX_USER_COUNT = GetterUtil.getInteger(
 		PropertiesHolder._get(BenchmarksPropsKeys.MAX_USER_COUNT));
 
