f07b9326ff7878922ab6a2a3d6e5186d4dcc46b3
shuyangzhou <shuyang.zhou@liferay.com>
2020-09-15 10:52:42 -0700

    LPS-120961 Lazy XmlMapperContextResolver's XmlMapper loading

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/context/resolver/XmlMapperContextResolver.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/context/resolver/XmlMapperContextResolver.java
@@ -30,15 +30,19 @@ public class XmlMapperContextResolver implements ContextResolver<XmlMapper> {
 
 	@Override
 	public XmlMapper getContext(Class<?> clazz) {
-		return _XML_MAPPER;
+		return XmlMapperHolder._XML_MAPPER;
 	}
 
-	private static final XmlMapper _XML_MAPPER = new XmlMapper() {
-		{
-			configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
-			setDateFormat(new ISO8601DateFormat());
-			setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-		}
-	};
+	private static class XmlMapperHolder {
+
+		private static final XmlMapper _XML_MAPPER = new XmlMapper() {
+			{
+				configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+				setDateFormat(new ISO8601DateFormat());
+				setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+			}
+		};
+
+	}
 
 }
