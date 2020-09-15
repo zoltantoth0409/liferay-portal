0104bb259321a5bc742570bca4253d26e7d0c5c4
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-09-15 16:25:21 +0200

    LPS-120773 Adds site initializer without any extra logic

---
+++ modules/apps/site/site-insurance-site-initializer/src/main/java/com/liferay/site/insurance/site/initializer/internal/InsuranceSiteInitializer.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.insurance.site.initializer.internal;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.site.exception.InitializationException;
+import com.liferay.site.initializer.SiteInitializer;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = "site.initializer.key=" + InsuranceSiteInitializer.KEY,
+	service = SiteInitializer.class
+)
+public class InsuranceSiteInitializer implements SiteInitializer {
+
+	public static final String KEY = "site-insurance-site-initializer";
+
+	@Override
+	public String getDescription(Locale locale) {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return _NAME;
+	}
+
+	@Override
+	public String getThumbnailSrc() {
+		return _servletContext.getContextPath() + "/images/thumbnail.png";
+	}
+
+	@Override
+	public void initialize(long groupId) throws InitializationException {
+		try {
+			_createServiceContext(groupId);
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+
+			throw new InitializationException(exception);
+		}
+	}
+
+	@Override
+	public boolean isActive(long companyId) {
+		return true;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	private void _createServiceContext(long groupId) throws Exception {
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+
+		User user = _userLocalService.getUser(PrincipalThreadLocal.getUserId());
+
+		Locale locale = LocaleUtil.getSiteDefault();
+
+		serviceContext.setLanguageId(LanguageUtil.getLanguageId(locale));
+
+		serviceContext.setScopeGroupId(groupId);
+		serviceContext.setTimeZone(user.getTimeZone());
+		serviceContext.setUserId(user.getUserId());
+
+		_serviceContext = serviceContext;
+	}
+
+	private static final String _NAME = "Insurance Demo Site";
+
+	private static final String _PATH =
+		"com/liferay/site/insurance/site/initializer/internal/dependencies";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		InsuranceSiteInitializer.class);
+
+	private Bundle _bundle;
+	private ServiceContext _serviceContext;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.site.insurance.site.initializer)"
+	)
+	private ServletContext _servletContext;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
