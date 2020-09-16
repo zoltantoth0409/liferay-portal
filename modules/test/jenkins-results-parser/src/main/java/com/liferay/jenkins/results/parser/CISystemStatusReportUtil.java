e739868fa7efcd8689548a1715f3e61738f81e2d
Kenji Heigel <kenji.heigel@liferay.com>
2020-09-16 16:30:15 -0700

    LRCI-1711 Create additional JavaScript file for Spira data for the CI System Status report

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CISystemStatusReportUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CISystemStatusReportUtil.java
@@ -14,7 +14,22 @@
 
 package com.liferay.jenkins.results.parser;
 
+import com.liferay.jenkins.results.parser.spira.SpiraProject;
+import com.liferay.jenkins.results.parser.spira.SpiraRelease;
+import com.liferay.jenkins.results.parser.spira.SpiraReleaseBuild;
+
 import java.io.IOException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * @author Kenji Heigel
@@ -30,6 +45,116 @@ public class CISystemStatusReportUtil {
 		jenkinsCohort.writeDataJavaScriptFile(filePath);
 	}
 
+	public static void writeSpiraDataJavaScriptFile(String filePath)
+		throws IOException {
+
+		SpiraProject spiraProject = SpiraProject.getSpiraProjectByID(
+			_LIFERAY_DXP_PROJECT_ID);
+
+		SpiraRelease spiraRelease = spiraProject.getSpiraReleaseByID(
+			_LIFERAY_DXP_73_PULL_REQUEST_RELEVANT_SUITE_RELEASE_ID);
+
+		List<SpiraReleaseBuild> spiraReleaseBuilds =
+			SpiraReleaseBuild.getSpiraReleaseBuilds(spiraProject, spiraRelease);
+
+		for (SpiraReleaseBuild spiraReleaseBuild : spiraReleaseBuilds) {
+			LocalDate localDate = LocalDate.parse(
+				spiraReleaseBuild.getCreationDate(),
+				DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+
+			if (_recentSpiraReleaseBuilds.containsKey(localDate)) {
+				List<SpiraReleaseBuild> builds = _recentSpiraReleaseBuilds.get(
+					localDate);
+
+				builds.add(spiraReleaseBuild);
+
+				_recentSpiraReleaseBuilds.put(localDate, builds);
+			}
+		}
+
+		JSONObject relevantSuiteBuildDataJSONObject = new JSONObject();
+
+		JSONArray datesJSONArray = new JSONArray();
+		JSONArray failedBuildsJSONArray = new JSONArray();
+		JSONArray passedBuildsJSONArray = new JSONArray();
+		JSONArray unstableBuildsJSONArray = new JSONArray();
+
+		List<LocalDate> dates = new ArrayList<>(
+			_recentSpiraReleaseBuilds.keySet());
+
+		Collections.sort(dates);
+
+		for (LocalDate date : dates) {
+			datesJSONArray.put(date.toString());
+
+			int failedBuilds = 0;
+			int passedBuilds = 0;
+			int unstableBuilds = 0;
+
+			for (SpiraReleaseBuild spiraReleaseBuild :
+					_recentSpiraReleaseBuilds.get(date)) {
+
+				String buildStatusName = spiraReleaseBuild.getBuildStatusName();
+
+				if (buildStatusName.equals("Failed")) {
+					failedBuilds++;
+
+					continue;
+				}
+
+				if (buildStatusName.equals("Succeeded")) {
+					passedBuilds++;
+
+					continue;
+				}
+
+				if (buildStatusName.equals("Unstable")) {
+					unstableBuilds++;
+				}
+			}
+
+			passedBuildsJSONArray.put(passedBuilds);
+			failedBuildsJSONArray.put(failedBuilds);
+			unstableBuildsJSONArray.put(unstableBuilds);
+		}
+
+		relevantSuiteBuildDataJSONObject.put("dates", datesJSONArray);
+		relevantSuiteBuildDataJSONObject.put("failed", failedBuildsJSONArray);
+		relevantSuiteBuildDataJSONObject.put(
+			"succeeded", passedBuildsJSONArray);
+		relevantSuiteBuildDataJSONObject.put(
+			"unstable", unstableBuildsJSONArray);
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("var relevantSuiteBuildData = ");
+		sb.append(relevantSuiteBuildDataJSONObject.toString());
+		sb.append(";");
+
+		JenkinsResultsParserUtil.write(filePath, sb.toString());
+	}
+
+	private static final int _DAYS_AGO = 28;
+
 	private static final String _DEFAULT_JENKINS_COHORT = "test-1";
+
+	private static final int
+		_LIFERAY_DXP_73_PULL_REQUEST_RELEVANT_SUITE_RELEASE_ID = 1312;
+
+	private static final int _LIFERAY_DXP_PROJECT_ID = 16;
+
+	private static final HashMap<LocalDate, List<SpiraReleaseBuild>>
+		_recentSpiraReleaseBuilds =
+			new HashMap<LocalDate, List<SpiraReleaseBuild>>() {
+				{
+					LocalDate localDate = LocalDate.now();
+
+					for (int i = 0; i < _DAYS_AGO; i++) {
+						put(
+							localDate.minusDays(i),
+							new ArrayList<SpiraReleaseBuild>());
+					}
+				}
+			};
 
 }
