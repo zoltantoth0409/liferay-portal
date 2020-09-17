3a0a6ed47e4d49ea666b6b3d9fe2703ca7e714cd
Kenji Heigel <kenji.heigel@liferay.com>
2020-09-17 09:37:33 -0700

    LRCI-1711 Calculate success rate data and print as a variable in JavaScript file

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CISystemStatusReportUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/CISystemStatusReportUtil.java
@@ -20,13 +20,18 @@ import com.liferay.jenkins.results.parser.spira.SpiraReleaseBuild;
 
 import java.io.IOException;
 
+import java.text.DecimalFormat;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -59,8 +64,7 @@ public class CISystemStatusReportUtil {
 
 		for (SpiraReleaseBuild spiraReleaseBuild : spiraReleaseBuilds) {
 			LocalDate localDate = LocalDate.parse(
-				spiraReleaseBuild.getCreationDate(),
-				DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+				spiraReleaseBuild.getCreationDate(), _dateTimeFormatter);
 
 			if (_recentSpiraReleaseBuilds.containsKey(localDate)) {
 				List<SpiraReleaseBuild> builds = _recentSpiraReleaseBuilds.get(
@@ -72,6 +76,36 @@ public class CISystemStatusReportUtil {
 			}
 		}
 
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("var relevantSuiteBuildData = ");
+
+		JSONObject relevantSuiteBuildDataJSONObject =
+			_getrelevantSuiteBuildDataJSONObject();
+
+		sb.append(relevantSuiteBuildDataJSONObject.toString());
+
+		sb.append(";\n var successRateData = ");
+
+		JSONArray successRateTableDataJSONArray =
+			_getSuccessRateDataJSONArray();
+
+		sb.append(successRateTableDataJSONArray.toString());
+
+		sb.append(";");
+
+		JenkinsResultsParserUtil.write(filePath, sb.toString());
+	}
+
+	private static String _getPercentage(Integer dividend, Integer divisor) {
+		double quotient = (double)dividend / (double)divisor;
+
+		DecimalFormat decimalFormat = new DecimalFormat("###.##%");
+
+		return decimalFormat.format(quotient);
+	}
+
+	private static JSONObject _getrelevantSuiteBuildDataJSONObject() {
 		JSONObject relevantSuiteBuildDataJSONObject = new JSONObject();
 
 		JSONArray datesJSONArray = new JSONArray();
@@ -125,13 +159,109 @@ public class CISystemStatusReportUtil {
 		relevantSuiteBuildDataJSONObject.put(
 			"unstable", unstableBuildsJSONArray);
 
-		StringBuilder sb = new StringBuilder();
+		return relevantSuiteBuildDataJSONObject;
+	}
 
-		sb.append("var relevantSuiteBuildData = ");
-		sb.append(relevantSuiteBuildDataJSONObject.toString());
-		sb.append(";");
+	private static JSONArray _getSuccessRateDataJSONArray() {
+		JSONArray successRateDataJSONArray = new JSONArray();
 
-		JenkinsResultsParserUtil.write(filePath, sb.toString());
+		JSONArray titlesJSONArray = new JSONArray();
+
+		titlesJSONArray.put("Time Period");
+		titlesJSONArray.put("Adjusted Success Rate");
+		titlesJSONArray.put("Success Rate");
+		titlesJSONArray.put("Builds Run");
+
+		successRateDataJSONArray.put(titlesJSONArray);
+
+		LocalDateTime currentLocalDateTime = LocalDateTime.now();
+
+		successRateDataJSONArray.put(
+			_getSuccessRateJSONArray(
+				"Last 24 Hours", currentLocalDateTime.minusDays(1),
+				currentLocalDateTime));
+		successRateDataJSONArray.put(
+			_getSuccessRateJSONArray(
+				"Last 7 Days", currentLocalDateTime.minusDays(7),
+				currentLocalDateTime));
+		successRateDataJSONArray.put(
+			_getSuccessRateJSONArray(
+				"Previous 7 Days", currentLocalDateTime.minusDays(14),
+				currentLocalDateTime.minusDays(7)));
+
+		return successRateDataJSONArray;
+	}
+
+	private static JSONArray _getSuccessRateJSONArray(
+		String title, LocalDateTime startLocalDateTime,
+		LocalDateTime endLocalDateTime) {
+
+		if (startLocalDateTime.compareTo(endLocalDateTime) >= 0) {
+			throw new IllegalArgumentException(
+				"Start time must preceed end time");
+		}
+
+		JSONArray successRateJSONArray = new JSONArray();
+
+		Set<LocalDate> localDates = new HashSet<>();
+
+		for (int i = 0;
+			 startLocalDateTime.compareTo(endLocalDateTime.minusDays(i)) <= 0;
+			 i++) {
+
+			LocalDateTime localDateTime = endLocalDateTime.minusDays(i);
+
+			localDates.add(localDateTime.toLocalDate());
+		}
+
+		int failedBuilds = 0;
+		int passedBuilds = 0;
+		int unstableBuilds = 0;
+
+		for (LocalDate localDate : localDates) {
+			for (SpiraReleaseBuild spiraReleaseBuild :
+					_recentSpiraReleaseBuilds.get(localDate)) {
+
+				LocalDateTime localDateTime = LocalDateTime.parse(
+					spiraReleaseBuild.getCreationDate(), _dateTimeFormatter);
+
+				if (startLocalDateTime.compareTo(localDateTime) >= 0) {
+					continue;
+				}
+
+				if (endLocalDateTime.compareTo(localDateTime) <= 0) {
+					continue;
+				}
+
+				String buildStatusName = spiraReleaseBuild.getBuildStatusName();
+
+				if (buildStatusName.equals("Failed")) {
+					failedBuilds++;
+
+					continue;
+				}
+
+				if (buildStatusName.equals("Succeeded")) {
+					passedBuilds++;
+
+					continue;
+				}
+
+				if (buildStatusName.equals("Unstable")) {
+					unstableBuilds++;
+				}
+			}
+		}
+
+		int totalBuilds = failedBuilds + passedBuilds + unstableBuilds;
+
+		successRateJSONArray.put(title);
+		successRateJSONArray.put(
+			_getPercentage(passedBuilds + unstableBuilds, totalBuilds));
+		successRateJSONArray.put(_getPercentage(passedBuilds, totalBuilds));
+		successRateJSONArray.put(totalBuilds);
+
+		return successRateJSONArray;
 	}
 
 	private static final int _DAYS_AGO = 28;
@@ -142,6 +272,9 @@ public class CISystemStatusReportUtil {
 		_LIFERAY_DXP_73_PULL_REQUEST_RELEVANT_SUITE_RELEASE_ID = 1312;
 
 	private static final int _LIFERAY_DXP_PROJECT_ID = 16;
+
+	private static final DateTimeFormatter _dateTimeFormatter =
+		DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
 	private static final HashMap<LocalDate, List<SpiraReleaseBuild>>
 		_recentSpiraReleaseBuilds =
