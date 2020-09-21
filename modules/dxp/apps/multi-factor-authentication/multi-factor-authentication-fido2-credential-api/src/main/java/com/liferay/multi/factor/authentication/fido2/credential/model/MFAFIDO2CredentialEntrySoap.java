brandonyee333/test

LPS-120265 Auto gen

modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-fido2-credential-api/src/main/java/com/liferay/multi/factor/authentication/fido2/credential/model/MFAFIDO2CredentialEntrySoap.java
@@ -1,0 +1,224 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.fido2.credential.model;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This class is used by SOAP remote services.
+ *
+ * @author Arthur Chan
+ * @deprecated As of Athanasius (7.3.x), with no direct replacement
+ * @generated
+ */
+@Deprecated
+public class MFAFIDO2CredentialEntrySoap implements Serializable {
+
+	public static MFAFIDO2CredentialEntrySoap toSoapModel(
+		MFAFIDO2CredentialEntry model) {
+
+		MFAFIDO2CredentialEntrySoap soapModel =
+			new MFAFIDO2CredentialEntrySoap();
+
+		soapModel.setMvccVersion(model.getMvccVersion());
+		soapModel.setMfaFIDO2CredentialEntryId(
+			model.getMfaFIDO2CredentialEntryId());
+		soapModel.setCompanyId(model.getCompanyId());
+		soapModel.setUserId(model.getUserId());
+		soapModel.setUserName(model.getUserName());
+		soapModel.setCreateDate(model.getCreateDate());
+		soapModel.setModifiedDate(model.getModifiedDate());
+		soapModel.setCredentialKey(model.getCredentialKey());
+		soapModel.setCredentialType(model.getCredentialType());
+		soapModel.setFailedAttempts(model.getFailedAttempts());
+		soapModel.setPublicKeyCose(model.getPublicKeyCose());
+		soapModel.setSignatureCount(model.getSignatureCount());
+
+		return soapModel;
+	}
+
+	public static MFAFIDO2CredentialEntrySoap[] toSoapModels(
+		MFAFIDO2CredentialEntry[] models) {
+
+		MFAFIDO2CredentialEntrySoap[] soapModels =
+			new MFAFIDO2CredentialEntrySoap[models.length];
+
+		for (int i = 0; i < models.length; i++) {
+			soapModels[i] = toSoapModel(models[i]);
+		}
+
+		return soapModels;
+	}
+
+	public static MFAFIDO2CredentialEntrySoap[][] toSoapModels(
+		MFAFIDO2CredentialEntry[][] models) {
+
+		MFAFIDO2CredentialEntrySoap[][] soapModels = null;
+
+		if (models.length > 0) {
+			soapModels =
+				new MFAFIDO2CredentialEntrySoap
+					[models.length][models[0].length];
+		}
+		else {
+			soapModels = new MFAFIDO2CredentialEntrySoap[0][0];
+		}
+
+		for (int i = 0; i < models.length; i++) {
+			soapModels[i] = toSoapModels(models[i]);
+		}
+
+		return soapModels;
+	}
+
+	public static MFAFIDO2CredentialEntrySoap[] toSoapModels(
+		List<MFAFIDO2CredentialEntry> models) {
+
+		List<MFAFIDO2CredentialEntrySoap> soapModels =
+			new ArrayList<MFAFIDO2CredentialEntrySoap>(models.size());
+
+		for (MFAFIDO2CredentialEntry model : models) {
+			soapModels.add(toSoapModel(model));
+		}
+
+		return soapModels.toArray(
+			new MFAFIDO2CredentialEntrySoap[soapModels.size()]);
+	}
+
+	public MFAFIDO2CredentialEntrySoap() {
+	}
+
+	public long getPrimaryKey() {
+		return _mfaFIDO2CredentialEntryId;
+	}
+
+	public void setPrimaryKey(long pk) {
+		setMfaFIDO2CredentialEntryId(pk);
+	}
+
+	public long getMvccVersion() {
+		return _mvccVersion;
+	}
+
+	public void setMvccVersion(long mvccVersion) {
+		_mvccVersion = mvccVersion;
+	}
+
+	public long getMfaFIDO2CredentialEntryId() {
+		return _mfaFIDO2CredentialEntryId;
+	}
+
+	public void setMfaFIDO2CredentialEntryId(long mfaFIDO2CredentialEntryId) {
+		_mfaFIDO2CredentialEntryId = mfaFIDO2CredentialEntryId;
+	}
+
+	public long getCompanyId() {
+		return _companyId;
+	}
+
+	public void setCompanyId(long companyId) {
+		_companyId = companyId;
+	}
+
+	public long getUserId() {
+		return _userId;
+	}
+
+	public void setUserId(long userId) {
+		_userId = userId;
+	}
+
+	public String getUserName() {
+		return _userName;
+	}
+
+	public void setUserName(String userName) {
+		_userName = userName;
+	}
+
+	public Date getCreateDate() {
+		return _createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		_createDate = createDate;
+	}
+
+	public Date getModifiedDate() {
+		return _modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		_modifiedDate = modifiedDate;
+	}
+
+	public String getCredentialKey() {
+		return _credentialKey;
+	}
+
+	public void setCredentialKey(String credentialKey) {
+		_credentialKey = credentialKey;
+	}
+
+	public int getCredentialType() {
+		return _credentialType;
+	}
+
+	public void setCredentialType(int credentialType) {
+		_credentialType = credentialType;
+	}
+
+	public int getFailedAttempts() {
+		return _failedAttempts;
+	}
+
+	public void setFailedAttempts(int failedAttempts) {
+		_failedAttempts = failedAttempts;
+	}
+
+	public String getPublicKeyCose() {
+		return _publicKeyCose;
+	}
+
+	public void setPublicKeyCose(String publicKeyCose) {
+		_publicKeyCose = publicKeyCose;
+	}
+
+	public long getSignatureCount() {
+		return _signatureCount;
+	}
+
+	public void setSignatureCount(long signatureCount) {
+		_signatureCount = signatureCount;
+	}
+
+	private long _mvccVersion;
+	private long _mfaFIDO2CredentialEntryId;
+	private long _companyId;
+	private long _userId;
+	private String _userName;
+	private Date _createDate;
+	private Date _modifiedDate;
+	private String _credentialKey;
+	private int _credentialType;
+	private int _failedAttempts;
+	private String _publicKeyCose;
+	private long _signatureCount;
+
+}

modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-fido2-credential-service/src/main/java/com/liferay/multi/factor/authentication/fido2/credential/model/impl/MFAFIDO2CredentialEntryCacheModel.java
@@ -1,0 +1,244 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.fido2.credential.model.impl;
+
+import com.liferay.multi.factor.authentication.fido2.credential.model.MFAFIDO2CredentialEntry;
+import com.liferay.petra.lang.HashUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.model.CacheModel;
+import com.liferay.portal.kernel.model.MVCCModel;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+import java.util.Date;
+
+/**
+ * The cache model class for representing MFAFIDO2CredentialEntry in entity cache.
+ *
+ * @author Arthur Chan
+ * @generated
+ */
+public class MFAFIDO2CredentialEntryCacheModel
+	implements CacheModel<MFAFIDO2CredentialEntry>, Externalizable, MVCCModel {
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof MFAFIDO2CredentialEntryCacheModel)) {
+			return false;
+		}
+
+		MFAFIDO2CredentialEntryCacheModel mfaFIDO2CredentialEntryCacheModel =
+			(MFAFIDO2CredentialEntryCacheModel)object;
+
+		if ((mfaFIDO2CredentialEntryId ==
+				mfaFIDO2CredentialEntryCacheModel.mfaFIDO2CredentialEntryId) &&
+			(mvccVersion == mfaFIDO2CredentialEntryCacheModel.mvccVersion)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int hashCode = HashUtil.hash(0, mfaFIDO2CredentialEntryId);
+
+		return HashUtil.hash(hashCode, mvccVersion);
+	}
+
+	@Override
+	public long getMvccVersion() {
+		return mvccVersion;
+	}
+
+	@Override
+	public void setMvccVersion(long mvccVersion) {
+		this.mvccVersion = mvccVersion;
+	}
+
+	@Override
+	public String toString() {
+		StringBundler sb = new StringBundler(25);
+
+		sb.append("{mvccVersion=");
+		sb.append(mvccVersion);
+		sb.append(", mfaFIDO2CredentialEntryId=");
+		sb.append(mfaFIDO2CredentialEntryId);
+		sb.append(", companyId=");
+		sb.append(companyId);
+		sb.append(", userId=");
+		sb.append(userId);
+		sb.append(", userName=");
+		sb.append(userName);
+		sb.append(", createDate=");
+		sb.append(createDate);
+		sb.append(", modifiedDate=");
+		sb.append(modifiedDate);
+		sb.append(", credentialKey=");
+		sb.append(credentialKey);
+		sb.append(", credentialType=");
+		sb.append(credentialType);
+		sb.append(", failedAttempts=");
+		sb.append(failedAttempts);
+		sb.append(", publicKeyCose=");
+		sb.append(publicKeyCose);
+		sb.append(", signatureCount=");
+		sb.append(signatureCount);
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Override
+	public MFAFIDO2CredentialEntry toEntityModel() {
+		MFAFIDO2CredentialEntryImpl mfaFIDO2CredentialEntryImpl =
+			new MFAFIDO2CredentialEntryImpl();
+
+		mfaFIDO2CredentialEntryImpl.setMvccVersion(mvccVersion);
+		mfaFIDO2CredentialEntryImpl.setMfaFIDO2CredentialEntryId(
+			mfaFIDO2CredentialEntryId);
+		mfaFIDO2CredentialEntryImpl.setCompanyId(companyId);
+		mfaFIDO2CredentialEntryImpl.setUserId(userId);
+
+		if (userName == null) {
+			mfaFIDO2CredentialEntryImpl.setUserName("");
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setUserName(userName);
+		}
+
+		if (createDate == Long.MIN_VALUE) {
+			mfaFIDO2CredentialEntryImpl.setCreateDate(null);
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setCreateDate(new Date(createDate));
+		}
+
+		if (modifiedDate == Long.MIN_VALUE) {
+			mfaFIDO2CredentialEntryImpl.setModifiedDate(null);
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setModifiedDate(new Date(modifiedDate));
+		}
+
+		if (credentialKey == null) {
+			mfaFIDO2CredentialEntryImpl.setCredentialKey("");
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setCredentialKey(credentialKey);
+		}
+
+		mfaFIDO2CredentialEntryImpl.setCredentialType(credentialType);
+		mfaFIDO2CredentialEntryImpl.setFailedAttempts(failedAttempts);
+
+		if (publicKeyCose == null) {
+			mfaFIDO2CredentialEntryImpl.setPublicKeyCose("");
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setPublicKeyCose(publicKeyCose);
+		}
+
+		mfaFIDO2CredentialEntryImpl.setSignatureCount(signatureCount);
+
+		mfaFIDO2CredentialEntryImpl.resetOriginalValues();
+
+		return mfaFIDO2CredentialEntryImpl;
+	}
+
+	@Override
+	public void readExternal(ObjectInput objectInput) throws IOException {
+		mvccVersion = objectInput.readLong();
+
+		mfaFIDO2CredentialEntryId = objectInput.readLong();
+
+		companyId = objectInput.readLong();
+
+		userId = objectInput.readLong();
+		userName = objectInput.readUTF();
+		createDate = objectInput.readLong();
+		modifiedDate = objectInput.readLong();
+		credentialKey = objectInput.readUTF();
+
+		credentialType = objectInput.readInt();
+
+		failedAttempts = objectInput.readInt();
+		publicKeyCose = objectInput.readUTF();
+
+		signatureCount = objectInput.readLong();
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput objectOutput) throws IOException {
+		objectOutput.writeLong(mvccVersion);
+
+		objectOutput.writeLong(mfaFIDO2CredentialEntryId);
+
+		objectOutput.writeLong(companyId);
+
+		objectOutput.writeLong(userId);
+
+		if (userName == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(userName);
+		}
+
+		objectOutput.writeLong(createDate);
+		objectOutput.writeLong(modifiedDate);
+
+		if (credentialKey == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(credentialKey);
+		}
+
+		objectOutput.writeInt(credentialType);
+
+		objectOutput.writeInt(failedAttempts);
+
+		if (publicKeyCose == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(publicKeyCose);
+		}
+
+		objectOutput.writeLong(signatureCount);
+	}
+
+	public long mvccVersion;
+	public long mfaFIDO2CredentialEntryId;
+	public long companyId;
+	public long userId;
+	public String userName;
+	public long createDate;
+	public long modifiedDate;
+	public String credentialKey;
+	public int credentialType;
+	public int failedAttempts;
+	public String publicKeyCose;
+	public long signatureCount;
+
+}

modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-fido2-credential-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,0 +1,14 @@
+create table MFAFIDO2CredentialEntry (
+	mvccVersion LONG default 0 not null,
+	mfaFIDO2CredentialEntryId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	credentialKey VARCHAR(128) null,
+	credentialType INTEGER,
+	failedAttempts INTEGER,
+	publicKeyCose VARCHAR(128) null,
+	signatureCount LONG
+);

zoltantoth0409/liferay-portal

modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-fido2-credential-api/src/main/java/com/liferay/multi/factor/authentication/fido2/credential/model/MFAFIDO2CredentialEntrySoap.java
@@ -1,0 +1,224 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.fido2.credential.model;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This class is used by SOAP remote services.
+ *
+ * @author Arthur Chan
+ * @deprecated As of Athanasius (7.3.x), with no direct replacement
+ * @generated
+ */
+@Deprecated
+public class MFAFIDO2CredentialEntrySoap implements Serializable {
+
+	public static MFAFIDO2CredentialEntrySoap toSoapModel(
+		MFAFIDO2CredentialEntry model) {
+
+		MFAFIDO2CredentialEntrySoap soapModel =
+			new MFAFIDO2CredentialEntrySoap();
+
+		soapModel.setMvccVersion(model.getMvccVersion());
+		soapModel.setMfaFIDO2CredentialEntryId(
+			model.getMfaFIDO2CredentialEntryId());
+		soapModel.setCompanyId(model.getCompanyId());
+		soapModel.setUserId(model.getUserId());
+		soapModel.setUserName(model.getUserName());
+		soapModel.setCreateDate(model.getCreateDate());
+		soapModel.setModifiedDate(model.getModifiedDate());
+		soapModel.setCredentialKey(model.getCredentialKey());
+		soapModel.setCredentialType(model.getCredentialType());
+		soapModel.setFailedAttempts(model.getFailedAttempts());
+		soapModel.setPublicKeyCode(model.getPublicKeyCode());
+		soapModel.setSignatureCount(model.getSignatureCount());
+
+		return soapModel;
+	}
+
+	public static MFAFIDO2CredentialEntrySoap[] toSoapModels(
+		MFAFIDO2CredentialEntry[] models) {
+
+		MFAFIDO2CredentialEntrySoap[] soapModels =
+			new MFAFIDO2CredentialEntrySoap[models.length];
+
+		for (int i = 0; i < models.length; i++) {
+			soapModels[i] = toSoapModel(models[i]);
+		}
+
+		return soapModels;
+	}
+
+	public static MFAFIDO2CredentialEntrySoap[][] toSoapModels(
+		MFAFIDO2CredentialEntry[][] models) {
+
+		MFAFIDO2CredentialEntrySoap[][] soapModels = null;
+
+		if (models.length > 0) {
+			soapModels =
+				new MFAFIDO2CredentialEntrySoap
+					[models.length][models[0].length];
+		}
+		else {
+			soapModels = new MFAFIDO2CredentialEntrySoap[0][0];
+		}
+
+		for (int i = 0; i < models.length; i++) {
+			soapModels[i] = toSoapModels(models[i]);
+		}
+
+		return soapModels;
+	}
+
+	public static MFAFIDO2CredentialEntrySoap[] toSoapModels(
+		List<MFAFIDO2CredentialEntry> models) {
+
+		List<MFAFIDO2CredentialEntrySoap> soapModels =
+			new ArrayList<MFAFIDO2CredentialEntrySoap>(models.size());
+
+		for (MFAFIDO2CredentialEntry model : models) {
+			soapModels.add(toSoapModel(model));
+		}
+
+		return soapModels.toArray(
+			new MFAFIDO2CredentialEntrySoap[soapModels.size()]);
+	}
+
+	public MFAFIDO2CredentialEntrySoap() {
+	}
+
+	public long getPrimaryKey() {
+		return _mfaFIDO2CredentialEntryId;
+	}
+
+	public void setPrimaryKey(long pk) {
+		setMfaFIDO2CredentialEntryId(pk);
+	}
+
+	public long getMvccVersion() {
+		return _mvccVersion;
+	}
+
+	public void setMvccVersion(long mvccVersion) {
+		_mvccVersion = mvccVersion;
+	}
+
+	public long getMfaFIDO2CredentialEntryId() {
+		return _mfaFIDO2CredentialEntryId;
+	}
+
+	public void setMfaFIDO2CredentialEntryId(long mfaFIDO2CredentialEntryId) {
+		_mfaFIDO2CredentialEntryId = mfaFIDO2CredentialEntryId;
+	}
+
+	public long getCompanyId() {
+		return _companyId;
+	}
+
+	public void setCompanyId(long companyId) {
+		_companyId = companyId;
+	}
+
+	public long getUserId() {
+		return _userId;
+	}
+
+	public void setUserId(long userId) {
+		_userId = userId;
+	}
+
+	public String getUserName() {
+		return _userName;
+	}
+
+	public void setUserName(String userName) {
+		_userName = userName;
+	}
+
+	public Date getCreateDate() {
+		return _createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		_createDate = createDate;
+	}
+
+	public Date getModifiedDate() {
+		return _modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		_modifiedDate = modifiedDate;
+	}
+
+	public String getCredentialKey() {
+		return _credentialKey;
+	}
+
+	public void setCredentialKey(String credentialKey) {
+		_credentialKey = credentialKey;
+	}
+
+	public int getCredentialType() {
+		return _credentialType;
+	}
+
+	public void setCredentialType(int credentialType) {
+		_credentialType = credentialType;
+	}
+
+	public int getFailedAttempts() {
+		return _failedAttempts;
+	}
+
+	public void setFailedAttempts(int failedAttempts) {
+		_failedAttempts = failedAttempts;
+	}
+
+	public String getPublicKeyCode() {
+		return _publicKeyCode;
+	}
+
+	public void setPublicKeyCode(String publicKeyCode) {
+		_publicKeyCode = publicKeyCode;
+	}
+
+	public long getSignatureCount() {
+		return _signatureCount;
+	}
+
+	public void setSignatureCount(long signatureCount) {
+		_signatureCount = signatureCount;
+	}
+
+	private long _mvccVersion;
+	private long _mfaFIDO2CredentialEntryId;
+	private long _companyId;
+	private long _userId;
+	private String _userName;
+	private Date _createDate;
+	private Date _modifiedDate;
+	private String _credentialKey;
+	private int _credentialType;
+	private int _failedAttempts;
+	private String _publicKeyCode;
+	private long _signatureCount;
+
+}

modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-fido2-credential-service/src/main/java/com/liferay/multi/factor/authentication/fido2/credential/model/impl/MFAFIDO2CredentialEntryCacheModel.java
@@ -1,0 +1,244 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.fido2.credential.model.impl;
+
+import com.liferay.multi.factor.authentication.fido2.credential.model.MFAFIDO2CredentialEntry;
+import com.liferay.petra.lang.HashUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.model.CacheModel;
+import com.liferay.portal.kernel.model.MVCCModel;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+import java.util.Date;
+
+/**
+ * The cache model class for representing MFAFIDO2CredentialEntry in entity cache.
+ *
+ * @author Arthur Chan
+ * @generated
+ */
+public class MFAFIDO2CredentialEntryCacheModel
+	implements CacheModel<MFAFIDO2CredentialEntry>, Externalizable, MVCCModel {
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof MFAFIDO2CredentialEntryCacheModel)) {
+			return false;
+		}
+
+		MFAFIDO2CredentialEntryCacheModel mfaFIDO2CredentialEntryCacheModel =
+			(MFAFIDO2CredentialEntryCacheModel)object;
+
+		if ((mfaFIDO2CredentialEntryId ==
+				mfaFIDO2CredentialEntryCacheModel.mfaFIDO2CredentialEntryId) &&
+			(mvccVersion == mfaFIDO2CredentialEntryCacheModel.mvccVersion)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		int hashCode = HashUtil.hash(0, mfaFIDO2CredentialEntryId);
+
+		return HashUtil.hash(hashCode, mvccVersion);
+	}
+
+	@Override
+	public long getMvccVersion() {
+		return mvccVersion;
+	}
+
+	@Override
+	public void setMvccVersion(long mvccVersion) {
+		this.mvccVersion = mvccVersion;
+	}
+
+	@Override
+	public String toString() {
+		StringBundler sb = new StringBundler(25);
+
+		sb.append("{mvccVersion=");
+		sb.append(mvccVersion);
+		sb.append(", mfaFIDO2CredentialEntryId=");
+		sb.append(mfaFIDO2CredentialEntryId);
+		sb.append(", companyId=");
+		sb.append(companyId);
+		sb.append(", userId=");
+		sb.append(userId);
+		sb.append(", userName=");
+		sb.append(userName);
+		sb.append(", createDate=");
+		sb.append(createDate);
+		sb.append(", modifiedDate=");
+		sb.append(modifiedDate);
+		sb.append(", credentialKey=");
+		sb.append(credentialKey);
+		sb.append(", credentialType=");
+		sb.append(credentialType);
+		sb.append(", failedAttempts=");
+		sb.append(failedAttempts);
+		sb.append(", publicKeyCode=");
+		sb.append(publicKeyCode);
+		sb.append(", signatureCount=");
+		sb.append(signatureCount);
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Override
+	public MFAFIDO2CredentialEntry toEntityModel() {
+		MFAFIDO2CredentialEntryImpl mfaFIDO2CredentialEntryImpl =
+			new MFAFIDO2CredentialEntryImpl();
+
+		mfaFIDO2CredentialEntryImpl.setMvccVersion(mvccVersion);
+		mfaFIDO2CredentialEntryImpl.setMfaFIDO2CredentialEntryId(
+			mfaFIDO2CredentialEntryId);
+		mfaFIDO2CredentialEntryImpl.setCompanyId(companyId);
+		mfaFIDO2CredentialEntryImpl.setUserId(userId);
+
+		if (userName == null) {
+			mfaFIDO2CredentialEntryImpl.setUserName("");
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setUserName(userName);
+		}
+
+		if (createDate == Long.MIN_VALUE) {
+			mfaFIDO2CredentialEntryImpl.setCreateDate(null);
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setCreateDate(new Date(createDate));
+		}
+
+		if (modifiedDate == Long.MIN_VALUE) {
+			mfaFIDO2CredentialEntryImpl.setModifiedDate(null);
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setModifiedDate(new Date(modifiedDate));
+		}
+
+		if (credentialKey == null) {
+			mfaFIDO2CredentialEntryImpl.setCredentialKey("");
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setCredentialKey(credentialKey);
+		}
+
+		mfaFIDO2CredentialEntryImpl.setCredentialType(credentialType);
+		mfaFIDO2CredentialEntryImpl.setFailedAttempts(failedAttempts);
+
+		if (publicKeyCode == null) {
+			mfaFIDO2CredentialEntryImpl.setPublicKeyCode("");
+		}
+		else {
+			mfaFIDO2CredentialEntryImpl.setPublicKeyCode(publicKeyCode);
+		}
+
+		mfaFIDO2CredentialEntryImpl.setSignatureCount(signatureCount);
+
+		mfaFIDO2CredentialEntryImpl.resetOriginalValues();
+
+		return mfaFIDO2CredentialEntryImpl;
+	}
+
+	@Override
+	public void readExternal(ObjectInput objectInput) throws IOException {
+		mvccVersion = objectInput.readLong();
+
+		mfaFIDO2CredentialEntryId = objectInput.readLong();
+
+		companyId = objectInput.readLong();
+
+		userId = objectInput.readLong();
+		userName = objectInput.readUTF();
+		createDate = objectInput.readLong();
+		modifiedDate = objectInput.readLong();
+		credentialKey = objectInput.readUTF();
+
+		credentialType = objectInput.readInt();
+
+		failedAttempts = objectInput.readInt();
+		publicKeyCode = objectInput.readUTF();
+
+		signatureCount = objectInput.readLong();
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput objectOutput) throws IOException {
+		objectOutput.writeLong(mvccVersion);
+
+		objectOutput.writeLong(mfaFIDO2CredentialEntryId);
+
+		objectOutput.writeLong(companyId);
+
+		objectOutput.writeLong(userId);
+
+		if (userName == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(userName);
+		}
+
+		objectOutput.writeLong(createDate);
+		objectOutput.writeLong(modifiedDate);
+
+		if (credentialKey == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(credentialKey);
+		}
+
+		objectOutput.writeInt(credentialType);
+
+		objectOutput.writeInt(failedAttempts);
+
+		if (publicKeyCode == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(publicKeyCode);
+		}
+
+		objectOutput.writeLong(signatureCount);
+	}
+
+	public long mvccVersion;
+	public long mfaFIDO2CredentialEntryId;
+	public long companyId;
+	public long userId;
+	public String userName;
+	public long createDate;
+	public long modifiedDate;
+	public String credentialKey;
+	public int credentialType;
+	public int failedAttempts;
+	public String publicKeyCode;
+	public long signatureCount;
+
+}

modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-fido2-credential-service/src/main/resources/META-INF/sql/tables.sql
@@ -1,0 +1,14 @@
+create table MFAFIDO2CredentialEntry (
+	mvccVersion LONG default 0 not null,
+	mfaFIDO2CredentialEntryId LONG not null primary key,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	credentialKey VARCHAR(128) null,
+	credentialType INTEGER,
+	failedAttempts INTEGER,
+	publicKeyCode VARCHAR(128) null,
+	signatureCount LONG
+);