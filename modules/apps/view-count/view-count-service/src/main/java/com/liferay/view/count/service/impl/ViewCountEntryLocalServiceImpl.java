3695870b77b4fcab531ea15e0c0bfeb6998414cc
Eric Yan <eric.yan@liferay.com>
2020-09-21 15:16:23 -0700

    LPS-121145 view-count-service: Implement view count configuration

--- modules/apps/view-count/view-count-service/src/main/java/com/liferay/view/count/service/impl/ViewCountEntryLocalServiceImpl.java
+++ modules/apps/view-count/view-count-service/src/main/java/com/liferay/view/count/service/impl/ViewCountEntryLocalServiceImpl.java
@@ -15,29 +15,42 @@
 package com.liferay.view.count.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.change.tracking.CTAware;
 import com.liferay.portal.kernel.increment.BufferedIncrement;
 import com.liferay.portal.kernel.increment.NumberIncrement;
+import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.module.framework.service.IdentifiableOSGiService;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalService;
 import com.liferay.portal.kernel.service.SQLStateAcceptor;
 import com.liferay.portal.kernel.spring.aop.Property;
 import com.liferay.portal.kernel.spring.aop.Retry;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.view.count.ViewCountManager;
 import com.liferay.portal.util.PropsValues;
+import com.liferay.view.count.configuration.ViewCountConfiguration;
 import com.liferay.view.count.model.ViewCountEntry;
 import com.liferay.view.count.service.ViewCountEntryLocalService;
 import com.liferay.view.count.service.base.ViewCountEntryLocalServiceBaseImpl;
 import com.liferay.view.count.service.persistence.ViewCountEntryPK;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Preston Crary
  */
 @Component(
+	configurationPid = "com.liferay.view.count.configuration.ViewCountConfiguration",
 	property = "model.class.name=com.liferay.view.count.model.ViewCountEntry",
 	service = AopService.class
 )
@@ -72,7 +85,7 @@ public class ViewCountEntryLocalServiceImpl
 	public long getViewCount(long companyId, long classNameId, long classPK) {
 		ViewCountEntry viewCountEntry = null;
 
-		if (PropsValues.VIEW_COUNT_ENABLED) {
+		if (isViewCountEnabled(classNameId)) {
 			viewCountEntry = viewCountEntryPersistence.fetchByPrimaryKey(
 				new ViewCountEntryPK(companyId, classNameId, classPK));
 		}
@@ -99,10 +112,48 @@ public class ViewCountEntryLocalServiceImpl
 	public void incrementViewCount(
 		long companyId, long classNameId, long classPK, int increment) {
 
-		if (PropsValues.VIEW_COUNT_ENABLED) {
+		if (isViewCountEnabled(classNameId)) {
 			viewCountEntryFinder.incrementViewCount(
 				companyId, classNameId, classPK, increment);
 		}
 	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_viewCountConfiguration = ConfigurableUtil.createConfigurable(
+			ViewCountConfiguration.class, properties);
+
+		_disabledClassNameIds.clear();
+
+		for (String entryClassName :
+				_viewCountConfiguration.disabledClassNames()) {
+
+			if (Validator.isNull(entryClassName)) {
+				continue;
+			}
+
+			ClassName className = _classNameLocalService.fetchClassName(
+				entryClassName);
+
+			if (entryClassName.equals(className.getValue())) {
+				_disabledClassNameIds.add(className.getClassNameId());
+			}
+		}
+	}
+
+	protected boolean isViewCountEnabled(long classNameId) {
+		if (_disabledClassNameIds.contains(classNameId)) {
+			return false;
+		}
+
+		return PropsValues.VIEW_COUNT_ENABLED;
+	}
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	private final Set<Long> _disabledClassNameIds = new HashSet<>();
+	private volatile ViewCountConfiguration _viewCountConfiguration;
 
 }
