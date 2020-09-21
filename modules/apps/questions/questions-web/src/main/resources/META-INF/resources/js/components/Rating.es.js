652d5c6bfce4f1924805c577949d78bb9680c484
Luismi Barcos <luismiguel.barcos@liferay.com>
2020-09-21 09:29:18 +0200

    LPS-121087 Disable rating buttons to guest users

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Rating.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Rating.es.js
@@ -79,7 +79,7 @@ export default ({
 				className={
 					'text-reset' + (userRating === 1 ? ' text-primary' : '')
 				}
-				disabled={disabled}
+				disabled={disabled || !Liferay.ThemeDisplay.isSignedIn()}
 				displayType="unstyled"
 				monospaced
 				onClick={() => voteChange(1)}
@@ -93,7 +93,7 @@ export default ({
 				className={
 					'text-reset' + (userRating === -1 ? ' text-primary' : '')
 				}
-				disabled={disabled}
+				disabled={disabled || !Liferay.ThemeDisplay.isSignedIn()}
 				displayType="unstyled"
 				monospaced
 				onClick={() => voteChange(-1)}
