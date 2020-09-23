c4fd4e984b0900c77b88d541de7e3442f34bd8c4
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-09-23 13:10:09 +0200

    Revert "LPS-120955 Forbidding connection with staged groups"
    This reverts commit 6cc9f5f574683fbf5d4788e90ecd35a84c88b260.

--- modules/apps/depot/depot-service/src/main/java/com/liferay/depot/service/impl/DepotEntryGroupRelLocalServiceImpl.java
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/service/impl/DepotEntryGroupRelLocalServiceImpl.java
@@ -14,20 +14,15 @@
 
 package com.liferay.depot.service.impl;
 
-import com.liferay.depot.exception.DepotEntryGroupRelStagedGroupException;
 import com.liferay.depot.model.DepotEntry;
 import com.liferay.depot.model.DepotEntryGroupRel;
 import com.liferay.depot.service.base.DepotEntryGroupRelLocalServiceBaseImpl;
-import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.service.GroupLocalService;
 
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -49,8 +44,6 @@ public class DepotEntryGroupRelLocalServiceImpl
 	@Override
 	public DepotEntryGroupRel addDepotEntryGroupRel(
 		long depotEntryId, long toGroupId, boolean searchable) {
-
-		_validate(toGroupId);
 
 		DepotEntryGroupRel depotEntryGroupRel =
 			depotEntryGroupRelPersistence.fetchByD_TGI(depotEntryId, toGroupId);
@@ -139,22 +132,5 @@ public class DepotEntryGroupRelLocalServiceImpl
 
 		return depotEntryGroupRelPersistence.update(depotEntryGroupRel);
 	}
-
-	private void _validate(long toGroupId) {
-		try {
-			Group group = _groupLocalService.getGroup(toGroupId);
-
-			if (group.isStaged()) {
-				throw new DepotEntryGroupRelStagedGroupException(
-					"Depot entry cannot be connected to a staged group");
-			}
-		}
-		catch (PortalException portalException) {
-			ReflectionUtil.throwException(portalException);
-		}
-	}
-
-	@Reference
-	private GroupLocalService _groupLocalService;
 
 }
