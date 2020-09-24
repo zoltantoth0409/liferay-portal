21fff4fb020ba8cce90d9c25bdbc240ef354eebe
Alan Huang <alan.huang@liferay.com>
2020-09-24 15:31:26 +0800

    LPS-105380 Add BNDCategoryCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDCategoryCheck.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.checks.util.BNDSourceUtil;
+
+import java.util.List;
+
+/**
+ * @author Alan Huang
+ */
+public class BNDCategoryCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isLiferaySourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		if (!absolutePath.endsWith("/app.bnd")) {
+			return content;
+		}
+
+		String liferayRelengCategory = BNDSourceUtil.getDefinitionValue(
+			content, "Liferay-Releng-Category");
+
+		if (Validator.isNull(liferayRelengCategory)) {
+			return content;
+		}
+
+		List<String> allowedLiferayRelengCategoryNames = getAttributeValues(
+			_ALLOWED_LIFERAY_RELENG_CATEGORY_NAMES_KEY, absolutePath);
+
+		if (!allowedLiferayRelengCategoryNames.isEmpty() &&
+			!allowedLiferayRelengCategoryNames.contains(
+				liferayRelengCategory)) {
+
+			String message = StringBundler.concat(
+				"The 'Liferay-Releng-Category' can be blank or one of the ",
+				"following values '",
+				StringUtil.merge(allowedLiferayRelengCategoryNames, ", "), "'");
+
+			addMessage(fileName, message);
+		}
+
+		return content;
+	}
+
+	private static final String _ALLOWED_LIFERAY_RELENG_CATEGORY_NAMES_KEY =
+		"allowedLiferayRelengCategoryNames";
+
+}
