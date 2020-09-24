b35242903f0068f6cad0ef9ba07da386b7147894
Adriano Interaminense <adriano.interaminense@gmail.com>
2020-09-24 11:55:03 -0300

    LPS-121230 Contribute to improve the editFormView unit test

--- modules/apps/app-builder/app-builder-web/test/js/pages/form-view/EditFormView.es.js
+++ modules/apps/app-builder/app-builder-web/test/js/pages/form-view/EditFormView.es.js
@@ -22,23 +22,21 @@ import {HTML5Backend} from 'react-dnd-html5-backend';
 import {AppContextProvider} from '../../../../src/main/resources/META-INF/resources/js/AppContext.es';
 import EditFormView from '../../../../src/main/resources/META-INF/resources/js/pages/form-view/EditFormView.es';
 import * as toast from '../../../../src/main/resources/META-INF/resources/js/utils/toast.es';
-import {dataLayoutBuilderConfig, formViewContext} from '../../constants.es';
+import {FORM_VIEW_CONTEXT, dataLayoutBuilderConfig} from '../../constants.es';
 
-const dataLayoutBuilder = new DataLayoutBuilder.default(
-	dataLayoutBuilderConfig
-);
-
-const props = {
-	basePortletURL: `localhost`,
-	customObjectSidebarElementId: `customObject`,
+const EDIT_FORM_VIEW_PROPS = {
+	basePortletURL: 'localhost',
+	customObjectSidebarElementId: 'customObject',
 	dataDefinitionId: 1,
-	dataLayoutBuilderElementId: ``,
+	dataLayoutBuilderElementId: '',
 	dataLayoutBuilderId: 1,
 	dataLayoutId: 1,
 	newCustomObject: true,
 };
 
-const AppContextProviderWrapper = (props) => <AppContextProvider {...props} />;
+const dataLayoutBuilder = new DataLayoutBuilder.default(
+	dataLayoutBuilderConfig
+);
 
 describe('EditFormView', () => {
 	let dataLayoutBuilderProps;
@@ -64,7 +62,7 @@ describe('EditFormView', () => {
 				}),
 			}),
 			getState: () => {
-				return formViewContext;
+				return FORM_VIEW_CONTEXT;
 			},
 			getStore: () => {
 				return {
@@ -81,12 +79,6 @@ describe('EditFormView', () => {
 			removeEventListener: jest.fn(),
 		};
 
-		function componentReady() {
-			return new Promise((resolve) => {
-				resolve(dataLayoutBuilderProps);
-			});
-		}
-
 		dataLayoutVisitorSpy = jest
 			.spyOn(DataLayoutVisitor, 'isDataLayoutEmpty')
 			.mockImplementation(() => false);
@@ -97,7 +89,11 @@ describe('EditFormView', () => {
 
 		window.Liferay = {
 			...window.Liferay,
-			componentReady,
+			componentReady: () => {
+				return new Promise((resolve) => {
+					resolve(dataLayoutBuilderProps);
+				});
+			},
 		};
 
 		jest.useFakeTimers();
@@ -105,6 +101,7 @@ describe('EditFormView', () => {
 
 	afterEach(() => {
 		cleanup();
+
 		jest.clearAllTimers();
 		jest.restoreAllMocks();
 	});
@@ -115,16 +112,21 @@ describe('EditFormView', () => {
 
 	it('renders', async () => {
 		const {asFragment} = render(
-			<AppContextProviderWrapper>
+			<AppContextProvider>
 				<div className="tools-control-group">
 					<div className="control-menu-level-1-heading" />
 				</div>
+
 				<DndProvider backend={HTML5Backend}>
-					<div id={props.customObjectSidebarElementId} />
-					<EditFormView {...props} />
+					<div
+						id={EDIT_FORM_VIEW_PROPS.customObjectSidebarElementId}
+					/>
+
+					<EditFormView {...EDIT_FORM_VIEW_PROPS} />
 				</DndProvider>
-			</AppContextProviderWrapper>
+			</AppContextProvider>
 		);
+
 		await act(async () => {
 			jest.runAllTimers();
 		});
@@ -134,18 +136,25 @@ describe('EditFormView', () => {
 
 	it('renders as edit-form-view and make actions', async () => {
 		const {queryByPlaceholderText, queryByText} = render(
-			<AppContextProviderWrapper>
+			<AppContextProvider>
 				<ClayModalProvider>
 					<div className="tools-control-group">
 						<div className="control-menu-level-1-heading" />
 					</div>
+
 					<DndProvider backend={HTML5Backend}>
-						<div id={props.customObjectSidebarElementId} />
-						<EditFormView {...props} />
+						<div
+							id={
+								EDIT_FORM_VIEW_PROPS.customObjectSidebarElementId
+							}
+						/>
+
+						<EditFormView {...EDIT_FORM_VIEW_PROPS} />
 					</DndProvider>
 				</ClayModalProvider>
-			</AppContextProviderWrapper>
+			</AppContextProvider>
 		);
+
 		await act(async () => {
 			jest.runAllTimers();
 		});
@@ -164,6 +173,7 @@ describe('EditFormView', () => {
 			},
 			type: 'ADD_CUSTOM_OBJECT_FIELD',
 		});
+
 		expect(dataLayoutBuilderProps.dispatchAction.mock.calls.length).toBe(2);
 
 		const formName = queryByPlaceholderText('untitled-form-view');
@@ -188,26 +198,34 @@ describe('EditFormView', () => {
 		expect(dataLayoutVisitorSpy.mock.calls.length).toBe(1);
 	});
 
-	it('renders as edit-form-view and simulate field add on layout and save it', async () => {
+	it('renders as edit-form-view, simulate a field added to the layout and save it', async () => {
 		const response = JSON.stringify({});
+
 		fetch
 			.mockResponseOnce(response)
 			.mockResponseOnce(response)
 			.mockResponseOnce(JSON.stringify({items: []}));
 
 		const {container, queryByText} = render(
-			<AppContextProviderWrapper>
+			<AppContextProvider>
 				<ClayModalProvider>
 					<div className="tools-control-group">
 						<div className="control-menu-level-1-heading" />
 					</div>
+
 					<DndProvider backend={HTML5Backend}>
-						<div id={props.customObjectSidebarElementId} />
-						<EditFormView {...props} />
+						<div
+							id={
+								EDIT_FORM_VIEW_PROPS.customObjectSidebarElementId
+							}
+						/>
+
+						<EditFormView {...EDIT_FORM_VIEW_PROPS} />
 					</DndProvider>
 				</ClayModalProvider>
-			</AppContextProviderWrapper>
+			</AppContextProvider>
 		);
+
 		await act(async () => {
 			jest.runAllTimers();
 		});
@@ -239,7 +257,7 @@ describe('EditFormView', () => {
 
 		expect(dataLayoutParams.method).toBe('POST');
 		expect(dataLayoutParams.body).toEqual(
-			JSON.stringify(formViewContext.dataLayout)
+			JSON.stringify(FORM_VIEW_CONTEXT.dataLayout)
 		);
 
 		const deleteFromObject = container.querySelector(
@@ -261,20 +279,25 @@ describe('EditFormView', () => {
 			queryAllByText,
 			queryByText,
 		} = render(
-			<AppContextProviderWrapper>
+			<AppContextProvider>
 				<div className="tools-control-group">
 					<div className="control-menu-level-1-heading" />
 				</div>
+
 				<DndProvider backend={HTML5Backend}>
-					<div id={props.customObjectSidebarElementId} />
+					<div
+						id={EDIT_FORM_VIEW_PROPS.customObjectSidebarElementId}
+					/>
+
 					<EditFormView
-						{...props}
+						{...EDIT_FORM_VIEW_PROPS}
 						dataLayoutId={0}
 						newCustomObject={false}
 					/>
 				</DndProvider>
-			</AppContextProviderWrapper>
+			</AppContextProvider>
 		);
+
 		await act(async () => {
 			jest.runAllTimers();
 		});
@@ -286,6 +309,7 @@ describe('EditFormView', () => {
 		fireEvent.click(searchButton);
 
 		const [search] = queryAllByPlaceholderText('search...');
+
 		expect(search.value).toBe('');
 
 		fireEvent.change(search, {target: {value: 'Number'}});
