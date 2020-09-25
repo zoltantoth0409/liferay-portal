d7be29a88eb408a628b01af4a0600a3415d64062
Lawrence Lee <lawrence.lee@liferay.com>
2020-09-25 11:24:41 -0500

    LPS-121466 Use GA6 in tooling

--- modules/util/portal-tools-bundle-support/src/main/java/com/liferay/portal/tools/bundle/support/constants/BundleSupportConstants.java
+++ modules/util/portal-tools-bundle-support/src/main/java/com/liferay/portal/tools/bundle/support/constants/BundleSupportConstants.java
@@ -33,8 +33,8 @@ public class BundleSupportConstants {
 	public static final String DEFAULT_BUNDLE_FORMAT = "tar.gz";
 
 	public static final String DEFAULT_BUNDLE_URL =
-		"https://releases-cdn.liferay.com/portal/7.3.3-ga4" +
-			"/liferay-ce-portal-tomcat-7.3.3-ga4-20200701015330959.tar.gz";
+		"https://releases-cdn.liferay.com/portal/7.3.5-ga6" +
+			"/liferay-ce-portal-tomcat-7.3.5-ga6-20200924034643403.tar.gz";
 
 	public static final URL DEFAULT_BUNDLE_URL_OBJECT;
 
