1e936066ad2071cca141fcd2c7109e4251502aec
alee8888 <albert.lee@liferay.com>
2020-09-25 11:37:43 -0700

    LPS-105517 Do not check permissions if the current user is the OmniAdmin

--- portal-impl/src/com/liferay/portal/service/permission/UserPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/UserPermissionImpl.java
@@ -100,6 +100,10 @@ public class UserPermissionImpl
 			if (userId != ResourceConstants.PRIMKEY_DNE) {
 				user = UserLocalServiceUtil.getUserById(userId);
 
+				if (permissionChecker.isOmniadmin()) {
+					return true;
+				}
+
 				if (!actionId.equals(ActionKeys.VIEW) &&
 					!permissionChecker.isOmniadmin() &&
 					(PortalUtil.isOmniadmin(user) ||
