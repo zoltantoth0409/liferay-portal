f8e4c0044d4ef30b541090d7643f1c9aa6c83a92
Brian Chan <brian.chan@liferay.com>
2020-09-27 19:52:51 -0700

    LPS-121352 Sort

--- modules/apps/portal/portal-aop-impl/src/main/java/com/liferay/portal/aop/internal/AopServiceManager.java
+++ modules/apps/portal/portal-aop-impl/src/main/java/com/liferay/portal/aop/internal/AopServiceManager.java
@@ -86,7 +86,7 @@ public class AopServiceManager {
 		_aopServiceServiceTracker;
 	private BundleContext _bundleContext;
 
-	@Reference(target = "(&(original.bean=true)(bean.id=transactionExecutor))")
+	@Reference(target = "(&(bean.id=transactionExecutor)(original.bean=true))")
 	private TransactionHandler _portalTransactionHandler;
 
 	private SynchronousBundleListener _synchronousBundleListener;
