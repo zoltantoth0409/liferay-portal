6c6d97945c1ac5cd514040dd1a9fbd5fa36d66da
Marcos <marcos.martins@liferay.com>
2020-09-29 14:56:18 -0300

    LPS-121417 unit tests

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/numeric/NumericDDMFormFieldValueLocalizerTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.internal.numeric;
+
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Marcos Martins
+ */
+public class NumericDDMFormFieldValueLocalizerTest {
+
+	@Test
+	public void testLocalize() {
+		String localizedValue = _numericDDMFormFieldValueLocalizer.localize(
+			"1000.95", LocaleUtil.US);
+
+		Assert.assertEquals("1000.95", localizedValue);
+	}
+
+	@Test
+	public void testLocalizeWithEditingValueEndingWithPeriod() {
+		_numericDDMFormFieldValueLocalizer.setEditing(true);
+
+		String localizedValue = _numericDDMFormFieldValueLocalizer.localize(
+			"1000.", LocaleUtil.US);
+
+		Assert.assertEquals("1000.", localizedValue);
+	}
+
+	@Test
+	public void testLocalizeWithNotEditingValueEndingWithPeriod() {
+		_numericDDMFormFieldValueLocalizer.setEditing(false);
+
+		String localizedValue = _numericDDMFormFieldValueLocalizer.localize(
+			"1000.", LocaleUtil.US);
+
+		Assert.assertEquals("1000", localizedValue);
+	}
+
+	private final NumericDDMFormFieldValueLocalizer
+		_numericDDMFormFieldValueLocalizer =
+			new NumericDDMFormFieldValueLocalizer();
+
+}
