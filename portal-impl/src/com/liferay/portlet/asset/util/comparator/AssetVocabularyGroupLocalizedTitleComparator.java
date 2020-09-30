d0106b5a9538df34cd856f1996178be82bf136ea
Roberto Díaz <roberto.diaz@liferay.com>
2020-09-30 17:21:10 +0200

    LPS-117838 Change condition

--- portal-impl/src/com/liferay/portlet/asset/util/comparator/AssetVocabularyGroupLocalizedTitleComparator.java
+++ portal-impl/src/com/liferay/portlet/asset/util/comparator/AssetVocabularyGroupLocalizedTitleComparator.java
@@ -53,7 +53,8 @@ public class AssetVocabularyGroupLocalizedTitleComparator
 			value = 1;
 		}
 		else if ((_groupId == 0) ||
-				 (assetVocabulary1.getGroupId() == _groupId)) {
+				 (assetVocabulary1.getGroupId() !=
+				  	assetVocabulary2.getGroupId())) {
 
 			value = Long.compare(
 				assetVocabulary1.getGroupId(), assetVocabulary2.getGroupId());
