0073c9574a81db9c18428155dec61929f787ee42
Daniel Couso <daniel.couso@liferay.com>
2020-10-01 12:24:25 +0200

    LPS-117838 SF

--- portal-impl/src/com/liferay/portlet/asset/util/comparator/AssetVocabularyGroupLocalizedTitleComparator.java
+++ portal-impl/src/com/liferay/portlet/asset/util/comparator/AssetVocabularyGroupLocalizedTitleComparator.java
@@ -48,13 +48,13 @@ public class AssetVocabularyGroupLocalizedTitleComparator
 			value = -1;
 		}
 		else if ((assetVocabulary1.getGroupId() != _groupId) &&
-			(assetVocabulary2.getGroupId() == _groupId)) {
+				 (assetVocabulary2.getGroupId() == _groupId)) {
 
 			value = 1;
 		}
 		else if ((_groupId == 0) ||
 				 (assetVocabulary1.getGroupId() !=
-				  	assetVocabulary2.getGroupId())) {
+					 assetVocabulary2.getGroupId())) {
 
 			value = Long.compare(
 				assetVocabulary1.getGroupId(), assetVocabulary2.getGroupId());
