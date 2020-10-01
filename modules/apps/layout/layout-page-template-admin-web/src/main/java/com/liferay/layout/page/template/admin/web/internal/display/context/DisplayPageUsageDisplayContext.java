e25b15ca98168904d6e0e60541c1fe13135ff468
Rubén Pulido <ruben.pulido@liferay.com>
2020-10-02 00:19:04 +0200

    LPS-121603 Create DisplayPageUsageDisplayContext

---
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/display/context/DisplayPageUsageDisplayContext.java
@@ -0,0 +1,162 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.admin.web.internal.display.context;
+
+import com.liferay.asset.display.page.model.AssetDisplayPageEntry;
+import com.liferay.asset.display.page.service.AssetDisplayPageEntryServiceUtil;
+import com.liferay.layout.page.template.admin.web.internal.util.comparator.AssetDisplayPageEntryModifiedDateComparator;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.List;
+
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Rubén Pulido
+ */
+public class DisplayPageUsageDisplayContext {
+
+	public DisplayPageUsageDisplayContext(
+		HttpServletRequest httpServletRequest, RenderRequest renderRequest,
+		RenderResponse renderResponse) {
+
+		_httpServletRequest = httpServletRequest;
+		_renderRequest = renderRequest;
+		_renderResponse = renderResponse;
+
+		_assetDisplayPageEntriesCount =
+			AssetDisplayPageEntryServiceUtil.
+				getAssetDisplayPageEntriesCountByLayoutPageTemplateEntryId(
+					getLayoutPageTemplateEntryId());
+	}
+
+	public int getAssetDisplayPageEntriesCount() {
+		return _assetDisplayPageEntriesCount;
+	}
+
+	public long getLayoutPageTemplateEntryId() {
+		if (Validator.isNotNull(_layoutPageTemplateEntryId)) {
+			return _layoutPageTemplateEntryId;
+		}
+
+		_layoutPageTemplateEntryId = ParamUtil.getLong(
+			_httpServletRequest, "layoutPageTemplateEntryId");
+
+		return _layoutPageTemplateEntryId;
+	}
+
+	public String getOrderByCol() {
+		if (Validator.isNotNull(_orderByCol)) {
+			return _orderByCol;
+		}
+
+		_orderByCol = ParamUtil.getString(
+			_renderRequest, "orderByCol", "modified-date");
+
+		return _orderByCol;
+	}
+
+	public String getOrderByType() {
+		if (Validator.isNotNull(_orderByType)) {
+			return _orderByType;
+		}
+
+		_orderByType = ParamUtil.getString(
+			_renderRequest, "orderByType", "asc");
+
+		return _orderByType;
+	}
+
+	public PortletURL getPortletURL() {
+		PortletURL portletURL = _renderResponse.createRenderURL();
+
+		portletURL.setParameter(
+			"layoutPageTemplateEntryId",
+			String.valueOf(getLayoutPageTemplateEntryId()));
+		portletURL.setParameter(
+			"mvcRenderCommandName",
+			"/layout_page_template/view_display_page_usages");
+		portletURL.setParameter("redirect", getRedirect());
+
+		return portletURL;
+	}
+
+	public String getRedirect() {
+		if (_redirect != null) {
+			return _redirect;
+		}
+
+		_redirect = ParamUtil.getString(_renderRequest, "redirect");
+
+		return _redirect;
+	}
+
+	public SearchContainer<AssetDisplayPageEntry> getSearchContainer() {
+		SearchContainer<AssetDisplayPageEntry> searchContainer =
+			new SearchContainer<>(
+				_renderRequest, getPortletURL(), null,
+				"there-are-no-display-page-template-usages");
+
+		searchContainer.setId(
+			"assetDisplayPageEntries" + getLayoutPageTemplateEntryId());
+
+		boolean orderByAsc = false;
+
+		String orderByType = getOrderByType();
+
+		if (orderByType.equals("asc")) {
+			orderByAsc = true;
+		}
+
+		OrderByComparator<AssetDisplayPageEntry> orderByComparator =
+			new AssetDisplayPageEntryModifiedDateComparator(orderByAsc);
+
+		searchContainer.setOrderByCol(getOrderByCol());
+		searchContainer.setOrderByComparator(orderByComparator);
+		searchContainer.setOrderByType(getOrderByType());
+
+		List<AssetDisplayPageEntry> assetDisplayPageEntries =
+			AssetDisplayPageEntryServiceUtil.
+				getAssetDisplayPageEntriesByLayoutPageTemplateEntryId(
+					getLayoutPageTemplateEntryId(), searchContainer.getStart(),
+					searchContainer.getEnd(), orderByComparator);
+
+		searchContainer.setResults(assetDisplayPageEntries);
+
+		searchContainer.setTotal(_assetDisplayPageEntriesCount);
+
+		_searchContainer = searchContainer;
+
+		return _searchContainer;
+	}
+
+	private final int _assetDisplayPageEntriesCount;
+	private final HttpServletRequest _httpServletRequest;
+	private Long _layoutPageTemplateEntryId;
+	private String _orderByCol;
+	private String _orderByType;
+	private String _redirect;
+	private final RenderRequest _renderRequest;
+	private final RenderResponse _renderResponse;
+	private SearchContainer<AssetDisplayPageEntry> _searchContainer;
+
+}
