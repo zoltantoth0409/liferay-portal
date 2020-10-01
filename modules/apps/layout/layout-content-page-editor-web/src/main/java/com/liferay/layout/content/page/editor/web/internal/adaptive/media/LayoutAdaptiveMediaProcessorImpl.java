d12e45b543dced9b0aecb02564aa7593f3a044e0
Pavel Savinov <pavel.savinov@liferay.com>
2020-10-01 15:10:23 +0200

    LPS-114283 Add custom adaptive media content when corresponding configuration is set

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/adaptive/media/LayoutAdaptiveMediaProcessorImpl.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/adaptive/media/LayoutAdaptiveMediaProcessorImpl.java
@@ -16,11 +16,32 @@ package com.liferay.layout.content.page.editor.web.internal.adaptive.media;
 
 import com.liferay.adaptive.media.content.transformer.ContentTransformerHandler;
 import com.liferay.adaptive.media.content.transformer.constants.ContentTransformerContentTypes;
+import com.liferay.adaptive.media.image.configuration.AMImageConfigurationEntry;
+import com.liferay.adaptive.media.image.configuration.AMImageConfigurationHelper;
+import com.liferay.adaptive.media.image.html.constants.AMImageHTMLConstants;
+import com.liferay.adaptive.media.image.url.AMImageURLFactory;
+import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.layout.adaptive.media.LayoutAdaptiveMediaProcessor;
 import com.liferay.layout.content.page.editor.web.internal.configuration.FFLayoutContentPageEditorConfiguration;
+import com.liferay.layout.responsive.ViewportSize;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
+import java.net.URI;
+
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Optional;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
@@ -42,8 +63,73 @@ public class LayoutAdaptiveMediaProcessorImpl
 			return content;
 		}
 
-		return _contentTransformerHandler.transform(
+		String processedContent = _contentTransformerHandler.transform(
 			ContentTransformerContentTypes.HTML, content);
+
+		Document document = Jsoup.parse(processedContent);
+
+		try {
+			for (ViewportSize viewportSize : ViewportSize.values()) {
+				Elements elements = document.getElementsByAttribute(
+					"data-" + viewportSize.getViewportSizeId() +
+						"-configuration");
+
+				Iterator<Element> iterator = elements.iterator();
+
+				while (iterator.hasNext()) {
+					Element element = iterator.next();
+
+					if (!StringUtil.equalsIgnoreCase(
+							element.tagName(), "img")) {
+
+						continue;
+					}
+
+					String configuration = element.attr(
+						"data-" + viewportSize.getViewportSizeId() +
+							"-configuration");
+
+					long fileEntryId = GetterUtil.getLong(
+						element.attr(
+							AMImageHTMLConstants.ATTRIBUTE_NAME_FILE_ENTRY_ID));
+
+					if (fileEntryId <= 0) {
+						continue;
+					}
+
+					FileEntry fileEntry = _dlAppService.getFileEntry(
+						fileEntryId);
+
+					Optional<AMImageConfigurationEntry>
+						amImageConfigurationEntryOptional =
+							_amImageConfigurationHelper.
+								getAMImageConfigurationEntry(
+									fileEntry.getCompanyId(), configuration);
+
+					if (!amImageConfigurationEntryOptional.isPresent()) {
+						continue;
+					}
+
+					AMImageConfigurationEntry amImageConfigurationEntry =
+						amImageConfigurationEntryOptional.get();
+
+					URI uri = _amImageURLFactory.createFileEntryURL(
+						fileEntry.getFileVersion(), amImageConfigurationEntry);
+
+					_appendSourceElement(document, element, uri, viewportSize);
+				}
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to process adaptive media content", exception);
+			}
+		}
+
+		Element bodyElement = document.body();
+
+		return bodyElement.html();
 	}
 
 	@Modified
@@ -53,8 +139,46 @@ public class LayoutAdaptiveMediaProcessorImpl
 				FFLayoutContentPageEditorConfiguration.class, properties);
 	}
 
+	private void _appendSourceElement(
+		Document document, Element element, URI uri,
+		ViewportSize viewportSize) {
+
+		Element sourceElement = document.createElement("source");
+
+		Element parentElement = element.parent();
+
+		StringBundler sb = new StringBundler(6);
+
+		sb.append("(min-width:");
+		sb.append(viewportSize.getMinWidth());
+		sb.append("px)");
+
+		if (viewportSize != ViewportSize.DESKTOP) {
+			sb.append(" and (max-width:");
+			sb.append(viewportSize.getMaxWidth());
+			sb.append("px)");
+		}
+
+		sourceElement.attr("media", sb.toString());
+		sourceElement.attr("srcset", uri.toString());
+
+		parentElement.prependChild(sourceElement);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutAdaptiveMediaProcessorImpl.class);
+
+	@Reference
+	private AMImageConfigurationHelper _amImageConfigurationHelper;
+
+	@Reference
+	private AMImageURLFactory _amImageURLFactory;
+
 	@Reference
 	private ContentTransformerHandler _contentTransformerHandler;
+
+	@Reference
+	private DLAppService _dlAppService;
 
 	private volatile FFLayoutContentPageEditorConfiguration
 		_ffLayoutContentPageEditorConfiguration;
