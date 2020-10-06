35082f7125587cae7e107079ff981e6a1f2b552d
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-10-06 14:06:19 +0200

    LPS-121528 Defining group capability API
    This new API will allow us to avoid adding dependencies to depot
    everywhere. We will now use these capability methods and add a custom
    depot implementation for depot groups.
    
    We can slowly add new methods when needed. For now we will use it to
    check for pages and portlet support for a given group.

---
+++ portal-kernel/src/com/liferay/portal/kernel/util/GroupCapabilityUtil.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.util;
+
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.registry.collections.ServiceTrackerCollections;
+import com.liferay.registry.collections.ServiceTrackerList;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Alejandro Tardín
+ */
+public class GroupCapabilityUtil {
+
+	public static boolean isSupportsPages(Group group) {
+		for (GroupCapability groupCapability : _getGroupCapabilities(group)) {
+			if (!groupCapability.isSupportsPages()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static boolean isSupportsPortlet(Group group, Portlet portlet) {
+		for (GroupCapability groupCapability : _getGroupCapabilities(group)) {
+			if (!groupCapability.isSupportPortlet(portlet)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static List<GroupCapability> _getGroupCapabilities(Group group) {
+		List<GroupCapability> groupCapabilities = new ArrayList<>();
+
+		if (group == null) {
+			return groupCapabilities;
+		}
+
+		for (GroupCapabilityContributor groupCapabilityContributor :
+				_groupCapabilityContributors) {
+
+			Optional<GroupCapability> capabilityOptional =
+				groupCapabilityContributor.getGroupCapabilityOptional(group);
+
+			capabilityOptional.ifPresent(groupCapabilities::add);
+		}
+
+		return groupCapabilities;
+	}
+
+	private static final ServiceTrackerList<GroupCapabilityContributor>
+		_groupCapabilityContributors = ServiceTrackerCollections.openList(
+			GroupCapabilityContributor.class);
+
+}
