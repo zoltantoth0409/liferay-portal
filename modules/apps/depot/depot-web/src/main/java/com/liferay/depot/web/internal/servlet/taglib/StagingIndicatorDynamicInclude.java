e43c360511e43502ef2b76b710bfa4281f7cbb0e
Adolfo Pérez <adolfo.perez@liferay.com>
2020-10-13 17:01:07 +0200

    LPS-121578 Add dropdown with available actions

--- modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/servlet/taglib/StagingIndicatorDynamicInclude.java
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/servlet/taglib/StagingIndicatorDynamicInclude.java
@@ -14,24 +14,42 @@
 
 package com.liferay.depot.web.internal.servlet.taglib;
 
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.depot.web.internal.constants.DepotPortletKeys;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.portlet.LiferayPortletURL;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.permission.PortletPermission;
 import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
 import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.template.react.renderer.ComponentDescriptor;
+import com.liferay.portal.template.react.renderer.ReactRenderer;
 import com.liferay.staging.constants.StagingProcessesPortletKeys;
 import com.liferay.taglib.util.HtmlTopTag;
 
 import java.io.IOException;
 import java.io.Writer;
+
+import java.util.Map;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -67,7 +85,7 @@ public class StagingIndicatorDynamicInclude extends BaseDynamicInclude {
 					ActionKeys.VIEW)) {
 
 				_includeStagingIndicator(
-					scopeGroup, httpServletRequest, httpServletResponse);
+					httpServletRequest, httpServletResponse, themeDisplay);
 			}
 		}
 		catch (JspException jspException) {
@@ -86,10 +104,126 @@ public class StagingIndicatorDynamicInclude extends BaseDynamicInclude {
 			"com.liferay.product.navigation.taglib#/page.jsp#pre");
 	}
 
+	private <T> JSONArray _createJSONArray(T... values) {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		for (T value : values) {
+			jsonArray.put(value);
+		}
+
+		return jsonArray;
+	}
+
+	private String _getDepotDashboardGroupURL(
+			Group group, HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		if (_depotEntryLocalService == null) {
+			return null;
+		}
+
+		PortletURL portletURL = _portal.getControlPanelPortletURL(
+			httpServletRequest, group, DepotPortletKeys.DEPOT_ADMIN, 0, 0,
+			PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "/depot/view_depot_dashboard");
+
+		DepotEntry depotEntry = _depotEntryLocalService.getGroupDepotEntry(
+			group.getGroupId());
+
+		portletURL.setParameter(
+			"depotEntryId", String.valueOf(depotEntry.getDepotEntryId()));
+
+		return portletURL.toString();
+	}
+
+	private String _getPublishToLiveURL(
+		Group group, HttpServletRequest httpServletRequest) {
+
+		LiferayPortletURL liferayPortletURL = PortletURLFactoryUtil.create(
+			httpServletRequest, StagingProcessesPortletKeys.STAGING_PROCESSES,
+			PortletRequest.RENDER_PHASE);
+
+		liferayPortletURL.setParameter(
+			"mvcRenderCommandName", "publishLayouts");
+
+		String cmd = Constants.PUBLISH_TO_LIVE;
+
+		if (group.isStagedRemotely()) {
+			cmd = Constants.PUBLISH_TO_REMOTE;
+		}
+
+		liferayPortletURL.setParameter(Constants.CMD, cmd);
+
+		liferayPortletURL.setParameter(
+			"groupId", String.valueOf(group.getGroupId()));
+
+		return liferayPortletURL.toString();
+	}
+
+	private Map<String, Object> _getReactData(
+			HttpServletRequest httpServletRequest, ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		Group scopeGroup = themeDisplay.getScopeGroup();
+
+		if (scopeGroup.isStagingGroup()) {
+			return HashMapBuilder.<String, Object>put(
+				"iconClass", "staging-indicator-icon-staging"
+			).put(
+				"iconName", "radio-button"
+			).put(
+				"items",
+				_createJSONArray(
+					JSONUtil.put(
+						"href",
+						_getDepotDashboardGroupURL(
+							scopeGroup.getLiveGroup(), httpServletRequest)
+					).put(
+						"label", _language.get(httpServletRequest, "live")
+					).put(
+						"symbolLeft", "radio-button"
+					),
+					JSONUtil.put(
+						"href",
+						_getPublishToLiveURL(scopeGroup, httpServletRequest)
+					).put(
+						"label",
+						_language.get(httpServletRequest, "publish-to-live")
+					).put(
+						"symbolLeft", "cards2"
+					))
+			).put(
+				"title", _language.get(httpServletRequest, "staging")
+			).build();
+		}
+
+		return HashMapBuilder.<String, Object>put(
+			"iconClass", "staging-indicator-icon-live"
+		).put(
+			"iconName", "simple-circle"
+		).put(
+			"items",
+			_createJSONArray(
+				JSONUtil.put(
+					"href",
+					_getDepotDashboardGroupURL(
+						scopeGroup.getStagingGroup(), httpServletRequest)
+				).put(
+					"label", _language.get(httpServletRequest, "staging")
+				).put(
+					"symbolLeft", "simple-circle"
+				))
+		).put(
+			"title", _language.get(httpServletRequest, "live")
+		).build();
+	}
+
 	private void _includeStagingIndicator(
-			Group group, HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws IOException, JspException {
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, ThemeDisplay themeDisplay)
+		throws IOException, JspException, PortalException {
 
 		Writer writer = httpServletResponse.getWriter();
 
@@ -120,7 +254,9 @@ public class StagingIndicatorDynamicInclude extends BaseDynamicInclude {
 				"<button class=\"staging-indicator-button\">" +
 					"<span className=\"staging-indicator-title\">");
 
-		if (group.isStagingGroup()) {
+		Group scopeGroup = themeDisplay.getScopeGroup();
+
+		if (scopeGroup.isStagingGroup()) {
 			writer.write(_language.get(httpServletRequest, "staging"));
 		}
 		else {
@@ -129,6 +265,18 @@ public class StagingIndicatorDynamicInclude extends BaseDynamicInclude {
 
 		writer.write("</span></button></div>");
 
+		String componentId =
+			_portal.getPortletNamespace(DepotPortletKeys.DEPOT_ADMIN) +
+				"IndicatorComponent";
+		String module =
+			_npmResolver.resolveModuleName("depot-web") +
+				"/dynamic_include/StagingIndicator";
+
+		_reactRenderer.renderReact(
+			new ComponentDescriptor(module, componentId),
+			_getReactData(httpServletRequest, themeDisplay), httpServletRequest,
+			writer);
+
 		writer.write("</div>");
 	}
 
@@ -136,13 +284,22 @@ public class StagingIndicatorDynamicInclude extends BaseDynamicInclude {
 		StagingIndicatorDynamicInclude.class);
 
 	@Reference
+	private DepotEntryLocalService _depotEntryLocalService;
+
+	@Reference
 	private Language _language;
+
+	@Reference
+	private NPMResolver _npmResolver;
 
 	@Reference
 	private Portal _portal;
 
 	@Reference
 	private PortletPermission _portletPermission;
+
+	@Reference
+	private ReactRenderer _reactRenderer;
 
 	@Reference(target = "(osgi.web.symbolicname=com.liferay.depot.web)")
 	private ServletContext _servletContext;
---
+++ modules/apps/depot/depot-web/src/main/resources/META-INF/resources/dynamic_include/StagingIndicator.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {Align, ClayDropDownWithItems} from '@clayui/drop-down';
+import ClayIcon from '@clayui/icon';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Component = ({iconClass, iconName, items, title}) => {
+	return (
+		<ClayDropDownWithItems
+			alignmentPosition={Align.BottomCenter}
+			items={items}
+			trigger={
+				<button className="staging-indicator-button">
+					<ClayIcon className={iconClass} symbol={iconName} />
+
+					<span className="staging-indicator-title">{title}</span>
+
+					<ClayIcon symbol="caret-bottom" />
+				</button>
+			}
+		/>
+	);
+};
+
+Component.propTypes = {
+	iconClass: PropTypes.string,
+	iconName: PropTypes.string,
+	title: PropTypes.string,
+};
+
+export default function (props) {
+	return <Component {...props} />;
+}
