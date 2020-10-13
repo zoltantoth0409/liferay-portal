24d3e936986ca9625e591acb543147aafdfe49d0
drewbrokke <drew.brokke@liferay.com>
2020-10-13 11:22:36 -0500

    LPS-119805 user-test: reproduces the issue with a test case

--- modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/service/test/UserLocalServiceTest.java
+++ modules/apps/user/user-test/src/testIntegration/java/com/liferay/user/service/test/UserLocalServiceTest.java
@@ -27,6 +27,9 @@ import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -57,6 +60,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.LongStream;
 
 import org.junit.Assert;
@@ -311,6 +315,33 @@ public class UserLocalServiceTest {
 			LongStream.rangeClosed(
 				1000, 3000
 			).toArray());
+	}
+
+	@Test
+	public void testSearchCountsUserRole() throws Exception {
+		Group group = GroupTestUtil.addGroup();
+
+		PermissionChecker oldPermissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(UserTestUtil.addUser()));
+
+		try {
+			Map<Long, Integer> counts = _userLocalService.searchCounts(
+				TestPropsValues.getCompanyId(),
+				WorkflowConstants.STATUS_APPROVED,
+				new long[] {group.getGroupId()});
+
+			Integer count = counts.get(group.getGroupId());
+
+			Assert.assertNotNull(count);
+
+			Assert.assertEquals(1, count.intValue());
+		}
+		finally {
+			PermissionThreadLocal.setPermissionChecker(oldPermissionChecker);
+		}
 	}
 
 	@Test
