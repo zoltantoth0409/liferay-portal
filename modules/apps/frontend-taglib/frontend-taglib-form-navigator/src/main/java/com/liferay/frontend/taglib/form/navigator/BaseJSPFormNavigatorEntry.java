babb593d6642acb44e870b3be310e2ac346d644f
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-10-14 16:19:59 +0100

    LPS-121606 Copy from portal kernel, no logic changes

---
+++ modules/apps/frontend-taglib/frontend-taglib-form-navigator/src/main/java/com/liferay/frontend/taglib/form/navigator/BaseJSPFormNavigatorEntry.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.form.navigator;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.PortletBag;
+import com.liferay.portal.kernel.portlet.PortletBagPool;
+import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.servlet.taglib.ui.FormNavigatorEntry;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Julio Camarero
+ */
+public abstract class BaseJSPFormNavigatorEntry<T>
+	extends BaseFormNavigatorEntry<T> implements FormNavigatorEntry<T> {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		ServletContext servletContext = getServletContext(httpServletRequest);
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher(getJspPath());
+
+		try {
+			requestDispatcher.include(httpServletRequest, httpServletResponse);
+		}
+		catch (ServletException servletException) {
+			_log.error(
+				"Unable to include JSP " + getJspPath(), servletException);
+
+			throw new IOException(
+				"Unable to include " + getJspPath(), servletException);
+		}
+	}
+
+	public void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	protected abstract String getJspPath();
+
+	protected ServletContext getServletContext(
+		HttpServletRequest httpServletRequest) {
+
+		if (_servletContext != null) {
+			return _servletContext;
+		}
+
+		String portletId = PortalUtil.getPortletId(httpServletRequest);
+
+		if (Validator.isNotNull(portletId)) {
+			String rootPortletId = PortletIdCodec.decodePortletName(portletId);
+
+			PortletBag portletBag = PortletBagPool.get(rootPortletId);
+
+			return portletBag.getServletContext();
+		}
+
+		return (ServletContext)httpServletRequest.getAttribute(WebKeys.CTX);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		BaseJSPFormNavigatorEntry.class);
+
+	private ServletContext _servletContext;
+
+}
