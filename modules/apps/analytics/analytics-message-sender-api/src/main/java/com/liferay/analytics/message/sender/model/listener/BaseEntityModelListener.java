072fdc0c25a7d98be8b59c02f07382cca3715ff2
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-10-14 12:02:13 -0700

    LRAC-6813 always send memberships, not just on initial sync

--- modules/apps/analytics/analytics-message-sender-api/src/main/java/com/liferay/analytics/message/sender/model/listener/BaseEntityModelListener.java
+++ modules/apps/analytics/analytics-message-sender-api/src/main/java/com/liferay/analytics/message/sender/model/listener/BaseEntityModelListener.java
@@ -36,10 +36,14 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
 import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.ShardedModel;
+import com.liferay.portal.kernel.model.Team;
 import com.liferay.portal.kernel.model.TreeModel;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.CompanyService;
@@ -60,10 +64,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 import org.osgi.annotation.versioning.ProviderType;
 import org.osgi.service.component.annotations.Reference;
@@ -397,6 +403,65 @@ public abstract class BaseEntityModelListener<T extends BaseModel<T>>
 						AnalyticsExpandoBridgeUtil.getAttributes(
 							baseModel.getExpandoBridge(), null));
 				}
+
+				continue;
+			}
+			else if (includeAttributeName.equals("memberships") &&
+					 StringUtil.equals(
+						 baseModel.getModelClassName(), User.class.getName())) {
+
+				Map<String, long[]> memberships = new HashMap<>();
+
+				User user = (User)baseModel;
+
+				try {
+					List<Group> groups = user.getSiteGroups();
+
+					Stream<Group> stream = groups.stream();
+
+					long[] membershipIds = stream.mapToLong(
+						Group::getGroupId
+					).toArray();
+
+					if (membershipIds.length != 0) {
+						memberships.put(Group.class.getName(), membershipIds);
+					}
+				}
+				catch (Exception exception) {
+					_log.error(exception, exception);
+				}
+
+				try {
+					long[] membershipIds = user.getOrganizationIds();
+
+					if (membershipIds.length != 0) {
+						memberships.put(
+							Organization.class.getName(), membershipIds);
+					}
+				}
+				catch (Exception exception) {
+					_log.error(exception, exception);
+				}
+
+				long[] membershipIds = user.getRoleIds();
+
+				if (membershipIds.length != 0) {
+					memberships.put(Role.class.getName(), membershipIds);
+				}
+
+				membershipIds = user.getTeamIds();
+
+				if (membershipIds.length != 0) {
+					memberships.put(Team.class.getName(), membershipIds);
+				}
+
+				membershipIds = user.getUserGroupIds();
+
+				if (membershipIds.length != 0) {
+					memberships.put(UserGroup.class.getName(), membershipIds);
+				}
+
+				jsonObject.put("memberships", memberships);
 
 				continue;
 			}
