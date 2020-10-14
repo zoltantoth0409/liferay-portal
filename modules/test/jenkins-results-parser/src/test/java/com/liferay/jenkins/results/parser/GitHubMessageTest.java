830ef47eac808035610ac461b44d539c644e655d
Kenji Heigel <kenji.heigel@liferay.com>
2020-10-13 19:14:50 -0700

    LRCI-1724 Use JUnit's @Parameterized.Parameters with GitHubMessageTest

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/GitHubMessageTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/GitHubMessageTest.java
@@ -16,51 +16,94 @@ package com.liferay.jenkins.results.parser;
 
 import java.io.File;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  * @author Peter Yoo
  */
+@RunWith(Parameterized.class)
 public class GitHubMessageTest extends BuildTest {
+
+	@Parameterized.Parameters(name = "{0}")
+	public static Collection<String[]> sampleData() {
+		return Arrays.asList(
+			new String[][] {
+				{
+					"test-jenkins-acceptance-pullrequest_passed", "117",
+					"test-jenkins-acceptance-pullrequest", "test-1-17"
+				},
+				{
+					"test-plugins-acceptance-pullrequest(ee-6.2.x)_passed",
+					"66", "test-plugins-acceptance-pullrequest(ee-6.2.x)",
+					"test-1-8"
+				},
+				{
+					"test-portal-acceptance-pullrequest(7.0.x)" +
+						"_unresolved-req-failure",
+					"103", "test-portal-acceptance-pullrequest(7.0.x)",
+					"test-1-14"
+				},
+				{
+					"test-portal-acceptance-pullrequest(ee-6.2.x)_passed",
+					"337", "test-portal-acceptance-pullrequest(ee-6.2.x)",
+					"test-1-17"
+				},
+				{
+					"test-portal-acceptance-pullrequest(master)" +
+						"_generic-failure",
+					"1375", "test-portal-acceptance-pullrequest(master)",
+					"test-1-1"
+				},
+				{
+					"test-portal-acceptance-pullrequest(master)" +
+						"_modules-compile-failure",
+					"999", "test-portal-acceptance-pullrequest(master)",
+					"test-1-21"
+				},
+				{
+					"test-portal-acceptance-pullrequest(master)_passed", "446",
+					"test-portal-acceptance-pullrequest(master)", "test-1-8"
+				},
+				{
+					"test-portal-acceptance-pullrequest(master)" +
+						"_poshi-test-failure",
+					"1268", "test-portal-acceptance-pullrequest(master)",
+					"test-1-9"
+				},
+				{
+					"test-portal-acceptance-pullrequest(master)" +
+						"_semantic_versioning_failure",
+					"2003", "test-portal-acceptance-pullrequest(master)",
+					"test-1-3"
+				},
+				{
+					"test-portal-source-format_source-format-failure", "587",
+					"test-portal-source-format", "test-1-22"
+				}
+			});
+	}
+
+	public GitHubMessageTest(
+		String sampleKey, String buildNumber, String jobName, String hostName) {
+
+		_sampleKey = sampleKey;
+		_buildNumber = buildNumber;
+		_jobName = jobName;
+		_hostName = hostName;
+	}
 
 	@Before
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
 
-		downloadSample(
-			"test-jenkins-acceptance-pullrequest_passed", "117",
-			"test-jenkins-acceptance-pullrequest", "test-1-17");
-		downloadSample(
-			"test-plugins-acceptance-pullrequest(ee-6.2.x)_passed", "66",
-			"test-plugins-acceptance-pullrequest(ee-6.2.x)", "test-1-8");
-		downloadSample(
-			"test-portal-acceptance-pullrequest(7.0.x)_unresolved-req-failure",
-			"103", "test-portal-acceptance-pullrequest(7.0.x)", "test-1-14");
-		downloadSample(
-			"test-portal-acceptance-pullrequest(ee-6.2.x)_passed", "337",
-			"test-portal-acceptance-pullrequest(ee-6.2.x)", "test-1-17");
-		downloadSample(
-			"test-portal-acceptance-pullrequest(master)_generic-failure",
-			"1375", "test-portal-acceptance-pullrequest(master)", "test-1-1");
-		downloadSample(
-			"test-portal-acceptance-pullrequest(master)" +
-				"_modules-compile-failure",
-			"999", "test-portal-acceptance-pullrequest(master)", "test-1-21");
-		downloadSample(
-			"test-portal-acceptance-pullrequest(master)_passed", "446",
-			"test-portal-acceptance-pullrequest(master)", "test-1-8");
-		downloadSample(
-			"test-portal-acceptance-pullrequest(master)_poshi-test-failure",
-			"1268", "test-portal-acceptance-pullrequest(master)", "test-1-9");
-		downloadSample(
-			"test-portal-acceptance-pullrequest(master)" +
-				"_semantic_versioning_failure",
-			"2003", "test-portal-acceptance-pullrequest(master)", "test-1-3");
-		downloadSample(
-			"test-portal-source-format_source-format-failure", "587",
-			"test-portal-source-format", "test-1-22");
+		downloadSample(_sampleKey, _buildNumber, _jobName, _hostName);
 	}
 
 	@Test
@@ -87,5 +130,10 @@ public class GitHubMessageTest extends BuildTest {
 		return new File(
 			testSample.getSampleDir(), "expected-github-message.html");
 	}
+
+	private final String _buildNumber;
+	private final String _hostName;
+	private final String _jobName;
+	private final String _sampleKey;
 
 }
