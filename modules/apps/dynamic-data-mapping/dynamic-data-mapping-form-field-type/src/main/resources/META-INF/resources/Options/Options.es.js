23faf8cae12303f496fb45a179fbdaf8cc2777f2
Marcos <marcos.martins@liferay.com>
2020-10-15 07:57:30 -0300

    LPS-121827 set reference initial value for options

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Options/Options.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Options/Options.es.js
@@ -67,20 +67,23 @@ const Option = React.forwardRef(
 );
 
 const getInitialOption = (generateOptionValueUsingOptionLabel) => {
-	return generateOptionValueUsingOptionLabel
-		? {
-				id: random(),
-				label: '',
-				value: '',
-		  }
-		: {
-				id: random(),
-				label: '',
-				value: getDefaultOptionValue(
-					generateOptionValueUsingOptionLabel,
-					''
-				),
-		  };
+	const optionValue = getDefaultOptionValue(
+		generateOptionValueUsingOptionLabel,
+		''
+	);
+
+	const initalOption = {
+		id: random(),
+		label: '',
+		reference: optionValue,
+		value: '',
+	};
+
+	if (!generateOptionValueUsingOptionLabel) {
+		initalOption.value = optionValue;
+	}
+
+	return initalOption;
 };
 
 const refreshFields = (
@@ -147,19 +150,29 @@ const Options = ({
 
 			formattedValue[languageId] = formattedValue[languageId].map(
 				(option) => {
-					return {
+					let newOption = {
 						id: random(),
 						...option,
-						value:
-							!option.value &&
-							option.label.toLowerCase() ===
-								Liferay.Language.get('option').toLowerCase()
-								? getDefaultOptionValue(
-										generateOptionValueUsingOptionLabel,
-										option.label
-								  )
-								: option.value,
 					};
+
+					if (
+						!option.value &&
+						option.label.toLowerCase() ===
+							Liferay.Language.get('option').toLowerCase()
+					) {
+						const optionValue = getDefaultOptionValue(
+							generateOptionValueUsingOptionLabel,
+							option.label
+						);
+
+						newOption = {
+							...newOption,
+							reference: optionValue,
+							value: optionValue,
+						};
+					}
+
+					return newOption;
 				}
 			);
 		});
