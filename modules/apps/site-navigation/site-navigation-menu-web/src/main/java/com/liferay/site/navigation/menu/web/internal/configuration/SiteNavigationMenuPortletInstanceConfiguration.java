4bcdb59bc3706091ba8dafb0569465ecd1d7a161
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-10-19 14:14:21 +0200

    LPS-122307 Removes deprecated methods

--- modules/apps/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/configuration/SiteNavigationMenuPortletInstanceConfiguration.java
+++ modules/apps/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/internal/configuration/SiteNavigationMenuPortletInstanceConfiguration.java
@@ -44,33 +44,11 @@ public interface SiteNavigationMenuPortletInstanceConfiguration {
 	@Meta.AD(deflt = "absolute", name = "root-menu-item-type", required = false)
 	public String rootMenuItemType();
 
-	/**
-	 * @deprecated As of Judson (7.1.x), replaced by {@link #rootMenuItemType()}
-	 */
-	@Deprecated
-	@Meta.AD(name = "root-layout-type", required = false)
-	public String rootLayoutType();
-
 	@Meta.AD(deflt = "0", name = "root-menu-item-level", required = false)
 	public int rootMenuItemLevel();
 
-	/**
-	 * @deprecated As of Judson (7.1.x), replaced by {@link
-	 *             #rootMenuItemLevel()}
-	 */
-	@Deprecated
-	@Meta.AD(deflt = "0", name = "root-layout-level", required = false)
-	public int rootLayoutLevel();
-
 	@Meta.AD(name = "root-menu-item-id", required = false)
 	public String rootMenuItemId();
-
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	@Meta.AD(name = "root-layout-uuid", required = false)
-	public String rootLayoutUuid();
 
 	@Meta.AD(deflt = "-1", name = "site-navigation-menu-type", required = false)
 	public int siteNavigationMenuType();
@@ -80,13 +58,6 @@ public interface SiteNavigationMenuPortletInstanceConfiguration {
 
 	@Meta.AD(deflt = "auto", name = "expand-sublevels", required = false)
 	public String expandedLevels();
-
-	/**
-	 * @deprecated As of Judson (7.1.x), replaced by {@link #expandedLevels()}
-	 */
-	@Deprecated
-	@Meta.AD(name = "included-layouts", required = false)
-	public String includedLayouts();
 
 	@Meta.AD(deflt = "preview", name = "preview", required = false)
 	public boolean preview();
