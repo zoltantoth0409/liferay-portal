a719b0faa7ca9f94ea8e22639e432ff26998a341
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-10-22 13:27:57 +0200

    LPS-89065 Upgrade process to delete AssetEntries_AssetCategories table. We have to do it in the module because of the way that the upgrade works

---
+++ modules/apps/asset/asset-entry-rel-service/src/main/java/com/liferay/asset/entry/rel/internal/upgrade/v3_0_0/UpgradeAssetEntryAssetCategoriesTable.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.entry.rel.internal.upgrade.v3_0_0;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class UpgradeAssetEntryAssetCategoriesTable extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		if (hasTable("AssetEntries_AssetCategories")) {
+			runSQL("drop table AssetEntries_AssetCategories");
+		}
+	}
+
+}
