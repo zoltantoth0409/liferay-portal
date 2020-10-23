f6349e98e0363db98864e357fee16e5a3f76ca5f
Hugo Huijser <hugo.huijser@liferay.com>
2020-10-23 11:27:15 +0800

    LPS-122572 Due to LPS-105380: Add upgrade process for moved configuration files

---
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/upgrade/PortalSearchUpgrade.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.internal.upgrade;
+
+import com.liferay.portal.configuration.persistence.upgrade.ConfigurationUpgradeStepFactory;
+import com.liferay.portal.search.internal.index.configuration.IndexStatusManagerInternalConfiguration;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Hugo Huijser
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class PortalSearchUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register(
+			"0.0.0", "1.0.0",
+			_configurationUpgradeStepFactory.createUpgradeStep(
+				"com.liferay.portal.search.internal.index." +
+					"IndexStatusManagerInternalConfiguration",
+				IndexStatusManagerInternalConfiguration.class.getName()));
+	}
+
+	@Reference
+	private ConfigurationUpgradeStepFactory _configurationUpgradeStepFactory;
+
+}
