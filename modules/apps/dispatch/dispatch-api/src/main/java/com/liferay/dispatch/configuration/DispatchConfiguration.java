1d7495bed4202bc2c30d17f64ec90b81cf89b026
Igor Beslic <igor.beslic@liferay.com>
2020-10-26 16:22:15 +0100

    COMMERCE-5042 dispatch-api - add configuration interface

---
+++ modules/apps/dispatch/dispatch-api/src/main/java/com/liferay/dispatch/configuration/DispatchConfiguration.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dispatch.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Igor Beslic
+ */
+@ExtendedObjectClassDefinition(category = "dispatch")
+@Meta.OCD(
+	id = "com.liferay.dispatch.configuration.DispatchConfiguration",
+	localization = "content/Language", name = "dispatch-configuration-name"
+)
+public interface DispatchConfiguration {
+
+	@Meta.AD(
+		deflt = ".zip,.rar,.jar,.properties", name = "file-extensions",
+		required = false
+	)
+	public String[] fileExtensions();
+
+	@Meta.AD(deflt = "50242880", name = "file-max-size", required = false)
+	public long fileMaxSize();
+
+}
