57afe8ea9feba9d61dce8e38f0a470e1dc1de88f
Brian Chan <brian.chan@liferay.com>
2020-10-29 14:25:10 -0700

    COMMERCE-4678 sort

--- modules/dxp/apps/commerce-demo-pack/commerce-theme-minium/commerce-theme-minium-full-site-initializer/src/main/java/com/liferay/commerce/theme/minium/full/site/initializer/internal/importer/CommerceMLRecommendationImporter.java
+++ modules/dxp/apps/commerce-demo-pack/commerce-theme-minium/commerce-theme-minium-full-site-initializer/src/main/java/com/liferay/commerce/theme/minium/full/site/initializer/internal/importer/CommerceMLRecommendationImporter.java
@@ -55,9 +55,9 @@ public class CommerceMLRecommendationImporter {
 
 		ServiceContext serviceContext = new ServiceContext();
 
+		serviceContext.setCompanyId(user.getCompanyId());
 		serviceContext.setScopeGroupId(scopeGroupId);
 		serviceContext.setUserId(userId);
-		serviceContext.setCompanyId(user.getCompanyId());
 
 		for (int i = 0; i < jsonArray.length(); i++) {
 			JSONObject jsonObject = jsonArray.getJSONObject(i);
