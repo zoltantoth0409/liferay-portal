23bb6a38f63775a8da364ff53828b929db6d900d
Jorge González <jorge.gonzalez@liferay.com>
2020-10-30 13:08:17 +0100

    LPS-118825 Use title instead of data-title a11y

--- modules/apps/adaptive-media/adaptive-media-web/src/main/resources/META-INF/resources/adaptive_media/js/form/Components.js
+++ modules/apps/adaptive-media/adaptive-media-web/src/main/resources/META-INF/resources/adaptive_media/js/form/Components.js
@@ -37,7 +37,7 @@ const HelpMessage = ({message}) => {
 	return (
 		<span
 			className="inline-item-after lfr-portal-tooltip tooltip-icon"
-			data-title={message}
+			title={message}
 		>
 			<ClayIcon symbol="question-circle-full" />
 		</span>
