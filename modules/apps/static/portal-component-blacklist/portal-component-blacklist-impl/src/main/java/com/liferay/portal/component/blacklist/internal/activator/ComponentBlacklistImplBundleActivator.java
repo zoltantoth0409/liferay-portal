57473b5b4f493266e649e0ee85da06e759005524
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-10-30 23:31:45 +0100

    LPS-122572 Since they are static modules, we can not use the upgrade api to define the upgrade process as usual. We use bundle activator instead

---
+++ modules/apps/static/portal-component-blacklist/portal-component-blacklist-impl/src/main/java/com/liferay/portal/component/blacklist/internal/activator/ComponentBlacklistImplBundleActivator.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.component.blacklist.internal.activator;
+
+import com.liferay.portal.component.blacklist.internal.configuration.ComponentBlacklistConfiguration;
+import com.liferay.portal.configuration.persistence.upgrade.ConfigurationUpgradeStepFactory;
+import com.liferay.portal.kernel.dao.db.DBContext;
+import com.liferay.portal.kernel.dao.db.DBProcessContext;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+
+import java.io.OutputStream;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * @author Alberto Chaparro
+ */
+public class ComponentBlacklistImplBundleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		ServiceReference<ConfigurationUpgradeStepFactory> serviceReference =
+			bundleContext.getServiceReference(
+				ConfigurationUpgradeStepFactory.class);
+
+		ConfigurationUpgradeStepFactory configurationUpgradeStepFactory =
+			bundleContext.getService(serviceReference);
+
+		UpgradeStep upgradeStep =
+			configurationUpgradeStepFactory.createUpgradeStep(
+				"com.liferay.portal.component.blacklist.internal." +
+					"ComponentBlacklistConfiguration",
+				ComponentBlacklistConfiguration.class.getName());
+
+		upgradeStep.upgrade(
+			new DBProcessContext() {
+
+				@Override
+				public DBContext getDBContext() {
+					return new DBContext();
+				}
+
+				@Override
+				public OutputStream getOutputStream() {
+					return null;
+				}
+
+			});
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+
+}
