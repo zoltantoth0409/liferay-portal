9c0daa95e22e09aad30df08de8214f0e1dea8634
drewbrokke <drew.brokke@liferay.com>
2020-11-03 17:23:32 -0600

    LPS-121999 portal-impl: adds RegionTable upgrade util class
    This was generated using the following commands from portal-impl:
    1. touch src/com/liferay/portal/upgrade/v7_4_x/util/RegionTable.java
    2. ant build-upgrade-table -Dupgrade.table.dir=src/com/liferay/portal/upgrade/v7_4_x/util
    
    See https://grow.liferay.com/people/Changing+the+model+in+an+Upgrade+Process

---
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/util/RegionTable.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_4_x.util;
+
+import java.sql.Types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author	  Brian Wing Shun Chan
+ * @generated
+ */
+public class RegionTable {
+
+	public static final String TABLE_NAME = "Region";
+
+	public static final Object[][] TABLE_COLUMNS = {
+		{"mvccVersion", Types.BIGINT}, {"regionId", Types.BIGINT},
+		{"companyId", Types.BIGINT}, {"userId", Types.BIGINT},
+		{"userName", Types.VARCHAR}, {"createDate", Types.TIMESTAMP},
+		{"modifiedDate", Types.TIMESTAMP}, {"countryId", Types.BIGINT},
+		{"regionCode", Types.VARCHAR}, {"name", Types.VARCHAR},
+		{"active_", Types.BOOLEAN}, {"position", Types.DOUBLE},
+		{"lastPublishDate", Types.TIMESTAMP}
+	};
+
+	public static final Map<String, Integer> TABLE_COLUMNS_MAP =
+new HashMap<String, Integer>();
+
+static {
+TABLE_COLUMNS_MAP.put("mvccVersion", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("regionId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("companyId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("userId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("userName", Types.VARCHAR);
+
+TABLE_COLUMNS_MAP.put("createDate", Types.TIMESTAMP);
+
+TABLE_COLUMNS_MAP.put("modifiedDate", Types.TIMESTAMP);
+
+TABLE_COLUMNS_MAP.put("countryId", Types.BIGINT);
+
+TABLE_COLUMNS_MAP.put("regionCode", Types.VARCHAR);
+
+TABLE_COLUMNS_MAP.put("name", Types.VARCHAR);
+
+TABLE_COLUMNS_MAP.put("active_", Types.BOOLEAN);
+
+TABLE_COLUMNS_MAP.put("position", Types.DOUBLE);
+
+TABLE_COLUMNS_MAP.put("lastPublishDate", Types.TIMESTAMP);
+
+}
+	public static final String TABLE_SQL_CREATE =
+"create table Region (mvccVersion LONG default 0 not null,regionId LONG not null primary key,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,countryId LONG,regionCode VARCHAR(75) null,name VARCHAR(75) null,active_ BOOLEAN,position DOUBLE,lastPublishDate DATE null)";
+
+	public static final String TABLE_SQL_DROP = "drop table Region";
+
+	public static final String[] TABLE_SQL_ADD_INDEXES = {
+		"create index IX_2D9A426F on Region (active_)",
+		"create index IX_11FB3E42 on Region (countryId, active_)",
+		"create unique index IX_A2635F5C on Region (countryId, regionCode[$COLUMN_LENGTH:75$])"
+	};
+
+}
