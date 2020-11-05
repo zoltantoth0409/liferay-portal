14339d9abe3dd6ff5552605c7a5ffc463c1c8772
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-11-05 12:28:30 +0100

    LPS-123361 Automatically creating DDM structure

---
+++ modules/apps/document-library/document-library-external-video/src/main/java/com/liferay/document/library/external/video/internal/util/ExternalVideoDLFileEntryTypeHelper.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.external.video.internal.util;
+
+import com.liferay.document.library.external.video.internal.constants.ExternalVideoConstants;
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.document.library.kernel.service.DLFileEntryTypeLocalService;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.util.DefaultDDMStructureHelper;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Iván Zaera
+ * @author Alejandro Tardín
+ */
+public class ExternalVideoDLFileEntryTypeHelper {
+
+	public ExternalVideoDLFileEntryTypeHelper(
+		Company company, DefaultDDMStructureHelper defaultDDMStructureHelper,
+		long dlFileEntryMetadataClassNameId,
+		DDMStructureLocalService ddmStructureLocalService,
+		DLFileEntryTypeLocalService dlFileEntryTypeLocalService,
+		UserLocalService userLocalService) {
+
+		_company = company;
+		_defaultDDMStructureHelper = defaultDDMStructureHelper;
+		_dlFileEntryMetadataClassNameId = dlFileEntryMetadataClassNameId;
+		_ddmStructureLocalService = ddmStructureLocalService;
+		_dlFileEntryTypeLocalService = dlFileEntryTypeLocalService;
+		_userLocalService = userLocalService;
+	}
+
+	public void addExternalVideoDLFileEntryType() throws Exception {
+		DDMStructure ddmStructure = _ddmStructureLocalService.fetchStructure(
+			_company.getGroupId(), _dlFileEntryMetadataClassNameId,
+			ExternalVideoConstants.DDM_STRUCTURE_KEY_EXTERNAL_VIDEO);
+
+		if (ddmStructure == null) {
+			ddmStructure = _addExternalVideoDDMStructure();
+		}
+
+		List<DLFileEntryType> dlFileEntryTypes =
+			_dlFileEntryTypeLocalService.getFileEntryTypes(
+				ddmStructure.getStructureId());
+
+		if (dlFileEntryTypes.isEmpty()) {
+			_addExternalVideoDLFileEntryType(ddmStructure.getStructureId());
+		}
+	}
+
+	private DDMStructure _addExternalVideoDDMStructure() throws Exception {
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setScopeGroupId(_company.getGroupId());
+
+		long defaultUserId = _userLocalService.getDefaultUserId(
+			_company.getCompanyId());
+
+		serviceContext.setUserId(defaultUserId);
+
+		Class<?> clazz = getClass();
+
+		_defaultDDMStructureHelper.addDDMStructures(
+			defaultUserId, _company.getGroupId(),
+			_dlFileEntryMetadataClassNameId, clazz.getClassLoader(),
+			"com/liferay/document/library/external/video/internal/util" +
+				"/dependencies/external-video-metadata-structure.xml",
+			serviceContext);
+
+		DDMStructure ddmStructure = _ddmStructureLocalService.getStructure(
+			_company.getGroupId(), _dlFileEntryMetadataClassNameId,
+			ExternalVideoConstants.DL_FILE_ENTRY_TYPE_KEY);
+
+		ddmStructure.setNameMap(_updateNameMap(ddmStructure.getNameMap()));
+
+		_ddmStructureLocalService.updateDDMStructure(ddmStructure);
+
+		return ddmStructure;
+	}
+
+	private void _addExternalVideoDLFileEntryType(long ddmStructureId)
+		throws Exception {
+
+		long defaultUserId = _userLocalService.getDefaultUserId(
+			_company.getCompanyId());
+
+		Map<Locale, String> nameMap = HashMapBuilder.put(
+			LocaleUtil.getDefault(),
+			ExternalVideoConstants.DL_FILE_ENTRY_TYPE_NAME
+		).build();
+
+		Map<Locale, String> descriptionMap = new HashMap<>();
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setScopeGroupId(_company.getGroupId());
+		serviceContext.setUserId(defaultUserId);
+
+		_dlFileEntryTypeLocalService.addFileEntryType(
+			defaultUserId, _company.getGroupId(),
+			ExternalVideoConstants.DL_FILE_ENTRY_TYPE_KEY, nameMap,
+			descriptionMap, new long[] {ddmStructureId}, serviceContext);
+	}
+
+	private Map<Locale, String> _updateNameMap(Map<Locale, String> nameMap) {
+		Map<Locale, String> updatedNameMap = new HashMap<>();
+
+		for (Map.Entry<Locale, String> entry : nameMap.entrySet()) {
+			updatedNameMap.put(
+				entry.getKey(),
+				LanguageUtil.get(entry.getKey(), "external-video-metadata"));
+		}
+
+		return updatedNameMap;
+	}
+
+	private final Company _company;
+	private final DDMStructureLocalService _ddmStructureLocalService;
+	private final DefaultDDMStructureHelper _defaultDDMStructureHelper;
+	private final long _dlFileEntryMetadataClassNameId;
+	private final DLFileEntryTypeLocalService _dlFileEntryTypeLocalService;
+	private final UserLocalService _userLocalService;
+
+}
