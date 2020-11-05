899079f8f3f1e7752e68b248ff2b271b7db10bdf
Brian Chan <brian.chan@liferay.com>
2020-11-05 13:03:37 -0800

    LPS-122134 More accurate, just one

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/importer/FragmentLayoutStructureItemImporter.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/headless/delivery/dto/v1_0/structure/importer/FragmentLayoutStructureItemImporter.java
@@ -385,7 +385,7 @@ public class FragmentLayoutStructureItemImporter
 		return jsonObject;
 	}
 
-	private JSONObject _createImageJSONObjectFromClassPKReferences(
+	private JSONObject _createImageJSONObjectFromClassPKReference(
 		Map<String, Object> map) {
 
 		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
@@ -833,7 +833,7 @@ public class FragmentLayoutStructureItemImporter
 								"fragmentImageClassPKReference");
 
 						baseFragmentFieldJSONObject =
-							_createImageJSONObjectFromClassPKReferences(
+							_createImageJSONObjectFromClassPKReference(
 								(Map<String, Object>)
 									fragmentImageClassPKReferenceMap.get(
 										"classPKReferences"));
