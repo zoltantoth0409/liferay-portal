1a989606a7dc551435e4c3ed0b21348851b3c2f3
Jorge Díaz <jorge.diaz@liferay.com>
2020-11-05 11:46:51 +0100

    Revert "LPS-120006 Model segments criteria only need to be queried with signed-in users"
    This reverts commit c1d4f9e64046a0fd54a9c1845868f0174dc18cdc.

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/provider/BaseSegmentsEntryProvider.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/provider/BaseSegmentsEntryProvider.java
@@ -21,7 +21,6 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModel;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -228,32 +227,26 @@ public abstract class BaseSegmentsEntryProvider
 		Criteria.Conjunction contextConjunction = getConjunction(
 			segmentsEntry, Criteria.Type.CONTEXT);
 
-		if (context != null) {
-			if (Validator.isNotNull(contextFilterString)) {
-				boolean matchesContext = false;
+		if ((context != null) && Validator.isNotNull(contextFilterString)) {
+			boolean matchesContext = false;
 
-				try {
-					matchesContext = oDataMatcher.matches(
-						contextFilterString, context);
-				}
-				catch (PortalException portalException) {
-					_log.error(portalException, portalException);
-				}
-
-				if (matchesContext &&
-					contextConjunction.equals(Criteria.Conjunction.OR)) {
-
-					return true;
-				}
-
-				if (!matchesContext &&
-					contextConjunction.equals(Criteria.Conjunction.AND)) {
-
-					return false;
-				}
+			try {
+				matchesContext = oDataMatcher.matches(
+					contextFilterString, context);
+			}
+			catch (PortalException portalException) {
+				_log.error(portalException, portalException);
 			}
 
-			if (!GetterUtil.getBoolean(context.get(Context.SIGNED_IN))) {
+			if (matchesContext &&
+				contextConjunction.equals(Criteria.Conjunction.OR)) {
+
+				return true;
+			}
+
+			if (!matchesContext &&
+				contextConjunction.equals(Criteria.Conjunction.AND)) {
+
 				return false;
 			}
 		}
