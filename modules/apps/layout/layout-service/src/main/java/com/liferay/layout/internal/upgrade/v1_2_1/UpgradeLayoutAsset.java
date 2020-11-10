ad58216416c86be506c3f9c8d5a7d1931b8aa46b
Brian Chan <brian.chan@liferay.com>
2020-11-10 13:13:18 -0800

    LPS-123148 SF

--- modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/upgrade/v1_2_1/UpgradeLayoutAsset.java
+++ modules/apps/layout/layout-service/src/main/java/com/liferay/layout/internal/upgrade/v1_2_1/UpgradeLayoutAsset.java
@@ -55,13 +55,10 @@ public class UpgradeLayoutAsset extends UpgradeProcess {
 
 				dynamicQuery.add(
 					typeProperty.ne(LayoutConstants.TYPE_ASSET_DISPLAY));
-
 				dynamicQuery.add(
 					typeProperty.ne(LayoutConstants.TYPE_COLLECTION));
-
 				dynamicQuery.add(typeProperty.ne(LayoutConstants.TYPE_CONTENT));
 			});
-
 		actionableDynamicQuery.setPerformActionMethod(
 			(Layout layout) -> _updateAsset(layout));
 
@@ -78,7 +75,6 @@ public class UpgradeLayoutAsset extends UpgradeProcess {
 
 		long[] assetCategoryIds = _assetCategoryLocalService.getCategoryIds(
 			Layout.class.getName(), layout.getPlid());
-
 		String[] assetTagNames = _assetTagLocalService.getTagNames(
 			Layout.class.getName(), layout.getPlid());
 
