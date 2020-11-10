aa206b02f90180bbb7a2827263e5df272907290e
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-11-10 12:07:50 -0300

    LPS-123225 Create a resource command to handle the DataRecord update with the ability to trigger a new workflow instance if needed.

---
+++ modules/apps/app-builder/app-builder-web/src/main/java/com/liferay/app/builder/web/internal/portlet/action/UpdateDataRecordMVCResourceCommand.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.app.builder.web.internal.portlet.action;
+
+import com.liferay.app.builder.constants.AppBuilderAppConstants;
+import com.liferay.app.builder.model.AppBuilderApp;
+import com.liferay.data.engine.rest.dto.v2_0.DataRecord;
+import com.liferay.data.engine.rest.resource.v2_0.DataRecordResource;
+import com.liferay.dynamic.data.lists.model.DDLRecord;
+import com.liferay.dynamic.data.lists.service.DDLRecordLocalService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowHandlerRegistryUtil;
+
+import java.io.Serializable;
+
+import java.util.Optional;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Nara Andrade
+ * @author Rafael Praxedes
+ */
+@Component(
+	immediate = true,
+	property = {
+		"app.builder.app.scope=" + AppBuilderAppConstants.SCOPE_STANDARD,
+		"mvc.command.name=/app_builder/update_data_record"
+	},
+	service = MVCResourceCommand.class
+)
+public class UpdateDataRecordMVCResourceCommand
+	extends BaseAppBuilderMVCResourceCommand<DataRecord> {
+
+	@Override
+	protected Optional<DataRecord> doTransactionalCommand(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		DataRecordResource dataRecordResource = DataRecordResource.builder(
+		).user(
+			themeDisplay.getUser()
+		).build();
+
+		DataRecord dataRecord = dataRecordResource.patchDataRecord(
+			ParamUtil.getLong(resourceRequest, "dataRecordId"),
+			DataRecord.toDTO(
+				ParamUtil.getString(resourceRequest, "dataRecord")));
+
+		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+
+		WorkflowHandlerRegistryUtil.startWorkflowInstance(
+			themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId(),
+			themeDisplay.getUserId(),
+			ResourceActionsUtil.getCompositeModelName(
+				AppBuilderApp.class.getName(), DDLRecord.class.getName()),
+			dataRecord.getId(),
+			_ddlRecordLocalService.getDDLRecord(dataRecord.getId()),
+			new ServiceContext(),
+			HashMapBuilder.<String, Serializable>put(
+				"plid", themeDisplay.getPlid()
+			).put(
+				"portletId", portletDisplay.getId()
+			).build());
+
+		return Optional.of(dataRecord);
+	}
+
+	@Reference
+	private DDLRecordLocalService _ddlRecordLocalService;
+
+}
