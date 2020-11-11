9718eada6d660d3cf5be81743850399c54d9071a
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2020-11-11 22:17:23 +0100

    COMMERCE-2912 Tests for Global Search react component

--- modules/apps/commerce/commerce-frontend-js/test/global_search/GlobalSearch.js
+++ modules/apps/commerce/commerce-frontend-js/test/global_search/GlobalSearch.js
@@ -19,7 +19,7 @@ import {
 	cleanup,
 	fireEvent,
 	render,
-	waitForElementToBeRemoved,
+	waitForElement,
 } from '@testing-library/react';
 import fetchMock from 'fetch-mock';
 import React from 'react';
@@ -30,47 +30,269 @@ import {
 	accountTemplate,
 	getAccounts,
 } from '../test_utilities/fake_data/accounts';
-import {getOrders} from '../test_utilities/fake_data/orders';
+import {getOrders, orderTemplate} from '../test_utilities/fake_data/orders';
+import {
+	getProducts,
+	productTemplate,
+} from '../test_utilities/fake_data/products';
 
-const ACCOUNTS_HEADLESS_API_ENDPOINT = ServiceProvider.AdminAccountAPI('v1')
-	.baseURL;
-const ORDERS_HEADLESS_API_ENDPOINT = ServiceProvider.AdminOrderAPI('v1')
-	.baseURL;
+const accountsEndpointRegexp = new RegExp(
+	ServiceProvider.AdminAccountAPI('v1').baseURL
+);
+
+const ordersEndpointRegexp = new RegExp(
+	ServiceProvider.AdminOrderAPI('v1').baseURL
+);
+
+const productsEndpointRegexp = new RegExp(
+	ServiceProvider.DeliveryCatalogAPI('v1').getBaseURL(11111)
+);
+
+const query = 'test';
 
 describe('Global Search', () => {
-	beforeEach(() => {
-		const accountsEndpointRegexp = new RegExp(
-			ACCOUNTS_HEADLESS_API_ENDPOINT
-		);
-		const ordersEndpointRegexp = new RegExp(ORDERS_HEADLESS_API_ENDPOINT);
-
-		fetchMock.mock(accountsEndpointRegexp, (url) => getAccounts(url));
-		fetchMock.mock(ordersEndpointRegexp, (url) => getOrders(url));
-	});
-
-	afterEach(() => {
-		fetchMock.restore();
-	});
-
-	describe('When input is empty', () => {
+	describe('When responses are ok', () => {
 		let renderedComponent;
 
 		beforeEach(() => {
+			fetchMock.mock(accountsEndpointRegexp, (url) => getAccounts(url));
+			fetchMock.mock(ordersEndpointRegexp, (url) => getOrders(url));
+			fetchMock.mock(productsEndpointRegexp, (url) => getProducts(url));
+
 			renderedComponent = render(
 				<GlobalSearch
+					accountURLTemplate="/account-page/{id}"
+					accountsSearchURLTemplate="/accounts?search={query}"
+					channelId={11111}
+					globalSearchURLTemplate="/global?search={query}"
+					orderURLTemplate="/order-page/{id}"
+					ordersSearchURLTemplate="/orders?search={query}"
+					productURLTemplate="/product-page/{id}"
+					productsSearchURLTemplate="/products?search={query}"
 					spritemap="./assets/icons.svg"
 				/>
 			);
 		});
 
 		afterEach(() => {
+			fetchMock.restore();
 			cleanup();
 		});
 
-		it('must display the accounts search autocomplete component"', () => {
-			expect(
-				renderedComponent.getByPlaceholderText(/search/)
-			).toBeInTheDocument();
+		describe('When input is empty', () => {
+			it('must display the global search input', () => {
+				expect(
+					renderedComponent.getByPlaceholderText(/search/)
+				).toBeInTheDocument();
+			});
+		});
+
+		describe('When input is filled', () => {
+			beforeEach(() => {
+				const input = renderedComponent.getByPlaceholderText(/search/);
+
+				fireEvent.change(input, {target: {value: query}});
+			});
+
+			it('must show 3 loaders', () => {
+				expect(
+					renderedComponent.baseElement.querySelectorAll(
+						'.loading-animation'
+					).length
+				).toBe(3);
+			});
+
+			describe('after the results are loaded', () => {
+				beforeEach(async () => {
+					await waitForElement(() =>
+						renderedComponent.getByText(
+							`search-${query}-in-accounts`
+						)
+					);
+					await waitForElement(() =>
+						renderedComponent.getByText(
+							`search-${query}-in-catalog`
+						)
+					);
+					await waitForElement(() =>
+						renderedComponent.getByText(`search-${query}-in-orders`)
+					);
+				});
+
+				it('must format the URL templates replacing the placeholders with entity properties', () => {
+					expect(
+						renderedComponent.getByText(`search-${query}-in-orders`)
+							.href
+					).toContain(`/orders?search=${query}`);
+
+					expect(
+						renderedComponent.getByText(
+							`search-${query}-in-accounts`
+						).href
+					).toContain(`/accounts?search=${query}`);
+
+					expect(
+						renderedComponent.getByText(
+							`search-${query}-in-catalog`
+						).href
+					).toContain(`/products?search=${query}`);
+
+					expect(
+						renderedComponent.getByText(`more-global-results`).href
+					).toContain(`/global?search=${query}`);
+				});
+
+				it('must show a product list', () => {
+					const products = renderedComponent.baseElement.querySelectorAll(
+						'.product-item'
+					);
+					const firstProduct = products[0];
+					const firstProductThumbnail = firstProduct.querySelector(
+						'img'
+					);
+
+					expect(products.length).toBe(4);
+
+					expect(firstProductThumbnail.src).toContain(
+						productTemplate.urlImage
+					);
+
+					expect(firstProductThumbnail.alt).toBe(
+						productTemplate.name
+					);
+
+					expect(firstProduct.href).toContain(
+						`/product-page/${productTemplate.id}`
+					);
+
+					expect(firstProduct.text).toBe(productTemplate.name);
+				});
+
+				it('must show an order list', () => {
+					const orders = renderedComponent.baseElement.querySelectorAll(
+						'.order-item'
+					);
+					const firstOrder = orders[0];
+
+					expect(orders.length).toBe(4);
+
+					expect(firstOrder.text).toContain(orderTemplate.id);
+
+					expect(firstOrder.href).toContain(
+						`/order-page/${orderTemplate.id}`
+					);
+				});
+
+				it('must show an account list', () => {
+					const accounts = renderedComponent.baseElement.querySelectorAll(
+						'.account-item'
+					);
+					const firstAccount = accounts[0];
+					const firstAccountThumbnail = firstAccount.querySelector(
+						'img'
+					);
+
+					expect(accounts.length).toBe(4);
+
+					expect(firstAccountThumbnail.src).toContain(
+						accountTemplate.logoURL
+					);
+
+					expect(firstAccountThumbnail.alt).toBe(
+						accountTemplate.name
+					);
+
+					expect(firstAccount.href).toContain(
+						`/account-page/${accountTemplate.id}`
+					);
+
+					expect(firstAccount.text).toBe(accountTemplate.name);
+				});
+			});
+		});
+	});
+
+	describe('When responses are not ok', () => {
+		let renderedComponent;
+		const toastErrorMessages = [];
+
+		beforeEach(() => {
+			window.Liferay.staticEnvTestUtils.print = (message) => {
+				toastErrorMessages.push(message);
+			};
+
+			fetchMock.mock(accountsEndpointRegexp, () => {
+				return Promise.reject({message: 'Error - accounts'});
+			});
+			fetchMock.mock(ordersEndpointRegexp, () => {
+				return Promise.reject({message: 'Error - orders'});
+			});
+			fetchMock.mock(productsEndpointRegexp, () => {
+				return Promise.reject({message: 'Error - products'});
+			});
+
+			renderedComponent = render(
+				<GlobalSearch
+					accountURLTemplate="/account-page/{id}"
+					accountsSearchURLTemplate="/accounts?search={query}"
+					channelId={11111}
+					globalSearchURLTemplate="/global?search={query}"
+					orderURLTemplate="/order-page/{id}"
+					ordersSearchURLTemplate="/orders?search={query}"
+					productURLTemplate="/product-page/{id}"
+					productsSearchURLTemplate="/products?search={query}"
+					spritemap="./assets/icons.svg"
+				/>
+			);
+		});
+
+		afterEach(() => {
+			fetchMock.restore();
+			cleanup();
+		});
+
+		describe('When input is filled', () => {
+			beforeEach(() => {
+				const input = renderedComponent.getByPlaceholderText(/search/);
+
+				fireEvent.change(input, {target: {value: query}});
+			});
+
+			describe('after the results are loaded', () => {
+				it('must show coherent messages', async () => {
+					expect(
+						await waitForElement(() =>
+							renderedComponent.getByText(`no-orders-were-found`)
+						)
+					).toBeInTheDocument();
+
+					expect(
+						await waitForElement(() =>
+							renderedComponent.getByText(
+								`no-products-were-found`
+							)
+						)
+					).toBeInTheDocument();
+
+					expect(
+						await waitForElement(() =>
+							renderedComponent.getByText(
+								`no-accounts-were-found`
+							)
+						)
+					).toBeInTheDocument();
+
+					expect(
+						renderedComponent.getByText(`more-global-results`).href
+					).toContain(`/global?search=${query}`);
+				});
+
+				it('must notify the user', () => {
+					expect(toastErrorMessages).toContain('Error - accounts');
+					expect(toastErrorMessages).toContain('Error - orders');
+					expect(toastErrorMessages).toContain('Error - products');
+				});
+			});
 		});
 	});
 });
--- modules/apps/commerce/commerce-frontend-js/test/test_utilities/fake_data/products.js
+++ modules/apps/commerce/commerce-frontend-js/test/test_utilities/fake_data/products.js
@@ -16,35 +16,34 @@ import faker from 'faker';
 
 import {getRandomInt, processFakeRequestData} from '../index';
 
-export let accounts = [];
+export let products = [];
 
-export const accountTemplate = {
-	emailAddresses: ['email@test.com'],
+export const productTemplate = {
 	id: 999999,
-	logoURL: '/test-logo-folder/test.jpg',
-	name: 'TEST ACCOUNT NAME',
+	name: 'TEST PRODUCT NAME',
+	productId: 999999,
+	urlImage: '/test-logo-folder/test.jpg',
 };
 
-export function generateFakeAccounts(total) {
-	accounts = [accountTemplate];
+export function generateFakeProducts(total) {
+	products = [productTemplate];
 
 	for (let i = 0; i < total - 1; i++) {
-		accounts.push({
-			...accountTemplate,
-			emailAddresses: new Array(getRandomInt(0, 2)).map(
-				faker.internet.email
-			),
+		products.push({
+			...productTemplate,
 			id: i,
-			name: faker.company.companyName(),
+			name: faker.commerce.productName(),
+			productId: i,
+			urlImage: faker.image.business(),
 		});
 	}
 
-	return accounts;
+	return products;
 }
 
-export function getAccounts(url, itemsLength = getRandomInt(40, 60)) {
-	if (!accounts.length) {
-		generateFakeAccounts(itemsLength);
+export function getProducts(url, itemsLength = getRandomInt(40, 60)) {
+	if (!products.length) {
+		generateFakeProducts(itemsLength);
 	}
 
 	const {
@@ -53,7 +52,7 @@ export function getAccounts(url, itemsLength = getRandomInt(40, 60)) {
 		page,
 		pageSize,
 		totalCount,
-	} = processFakeRequestData(url, accounts, [accountTemplate]);
+	} = processFakeRequestData(url, products, [productTemplate]);
 
 	return {
 		actions: {},
