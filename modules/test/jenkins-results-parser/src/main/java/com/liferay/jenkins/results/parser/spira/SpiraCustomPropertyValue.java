d7ab6c18651e6964e14f657db8bf9a18725ef010
Michael Hashimoto <michael.hashimoto@liferay.com>
2020-11-13 09:36:54 -0800

    LRCI-1810 Add ability to create a product version in property values

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraCustomPropertyValue.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraCustomPropertyValue.java
@@ -28,6 +28,15 @@ public abstract class SpiraCustomPropertyValue<T> extends BaseSpiraArtifact {
 			throw new RuntimeException("Invalid value " + value);
 		}
 
+		String spiraCustomPropertyName = spiraCustomProperty.getName();
+
+		if (spiraCustomPropertyName.equals(
+				SpiraTestCaseProductVersion.CUSTOM_PROPERTY_NAME)) {
+
+			return new SpiraTestCaseProductVersion(
+				spiraCustomProperty.getSpiraProject(), value);
+		}
+
 		SpiraCustomProperty.Type spiraCustomPropertyType =
 			spiraCustomProperty.getType();
 
@@ -66,6 +75,15 @@ public abstract class SpiraCustomPropertyValue<T> extends BaseSpiraArtifact {
 
 	public static SpiraCustomPropertyValue<?> getSpiraCustomPropertyValue(
 		SpiraCustomProperty spiraCustomProperty, JSONObject valueJSONObject) {
+
+		String spiraCustomPropertyName = spiraCustomProperty.getName();
+
+		if (spiraCustomPropertyName.equals(
+				SpiraTestCaseProductVersion.CUSTOM_PROPERTY_NAME)) {
+
+			return new SpiraTestCaseProductVersion(
+				valueJSONObject, spiraCustomProperty);
+		}
 
 		SpiraCustomProperty.Type spiraCustomPropertyType =
 			spiraCustomProperty.getType();
