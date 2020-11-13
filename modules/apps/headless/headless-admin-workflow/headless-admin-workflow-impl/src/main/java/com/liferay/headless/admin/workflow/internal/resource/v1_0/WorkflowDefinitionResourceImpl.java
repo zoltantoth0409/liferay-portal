26f93f5389336512d50c6cf136fca325a9ca832c
Nara Andrade <nara.andrade@liferay.com>
2020-11-13 12:38:44 -0300

    LPS-123224 Implement workflowDefinitionEntityModel and pass orderByComparator to getLatestWorkflowDefinitions method

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowDefinitionResourceImpl.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowDefinitionResourceImpl.java
@@ -15,16 +15,26 @@
 package com.liferay.headless.admin.workflow.internal.resource.v1_0;
 
 import com.liferay.headless.admin.workflow.dto.v1_0.WorkflowDefinition;
+import com.liferay.headless.admin.workflow.internal.odata.entity.v1_0.WorkflowDefinitionEntityModel;
 import com.liferay.headless.admin.workflow.resource.v1_0.WorkflowDefinitionResource;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.workflow.WorkflowDefinitionManager;
+import com.liferay.portal.kernel.workflow.comparator.WorkflowComparatorFactory;
+import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.resource.EntityModelResource;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 import java.util.Locale;
 import java.util.Map;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -38,7 +48,7 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = WorkflowDefinitionResource.class
 )
 public class WorkflowDefinitionResourceImpl
-	extends BaseWorkflowDefinitionResourceImpl {
+	extends BaseWorkflowDefinitionResourceImpl implements EntityModelResource {
 
 	@Override
 	public void deleteWorkflowDefinitionUndeploy(String name, String version)
@@ -47,6 +57,13 @@ public class WorkflowDefinitionResourceImpl
 		_workflowDefinitionManager.undeployWorkflowDefinition(
 			contextCompany.getCompanyId(), contextUser.getUserId(), name,
 			GetterUtil.getInteger(version));
+	}
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap)
+		throws Exception {
+
+		return _entityModel;
 	}
 
 	@Override
@@ -60,7 +77,7 @@ public class WorkflowDefinitionResourceImpl
 
 	@Override
 	public Page<WorkflowDefinition> getWorkflowDefinitionsPage(
-			Boolean active, Pagination pagination)
+			Boolean active, Pagination pagination, Sort[] sorts)
 		throws Exception {
 
 		return Page.of(
@@ -68,7 +85,7 @@ public class WorkflowDefinitionResourceImpl
 				_workflowDefinitionManager.getLatestWorkflowDefinitions(
 					active, contextCompany.getCompanyId(),
 					pagination.getStartPosition(), pagination.getEndPosition(),
-					null),
+					_toOrderByComparator((Sort)ArrayUtil.getValue(sorts, 0))),
 				this::_toWorkflowDefinition),
 			pagination,
 			_workflowDefinitionManager.getLatestWorkflowDefinitionsCount(
@@ -114,6 +131,24 @@ public class WorkflowDefinitionResourceImpl
 				GetterUtil.getInteger(version), active));
 	}
 
+	private OrderByComparator
+		<com.liferay.portal.kernel.workflow.WorkflowDefinition>
+			_toOrderByComparator(Sort sort) {
+
+		if (sort == null) {
+			return _workflowComparatorFactory.
+				getDefinitionModifiedDateComparator(false);
+		}
+
+		if (StringUtil.equals(sort.getFieldName(), "name")) {
+			return _workflowComparatorFactory.getDefinitionNameComparator(
+				!sort.isReverse());
+		}
+
+		return _workflowComparatorFactory.getDefinitionModifiedDateComparator(
+			!sort.isReverse());
+	}
+
 	private WorkflowDefinition _toWorkflowDefinition(
 		com.liferay.portal.kernel.workflow.WorkflowDefinition
 			workflowDefinition) {
@@ -139,6 +174,12 @@ public class WorkflowDefinitionResourceImpl
 			}
 		};
 	}
+
+	private static final EntityModel _entityModel =
+		new WorkflowDefinitionEntityModel();
+
+	@Reference
+	private WorkflowComparatorFactory _workflowComparatorFactory;
 
 	@Reference(target = "(proxy.bean=false)")
 	private WorkflowDefinitionManager _workflowDefinitionManager;
