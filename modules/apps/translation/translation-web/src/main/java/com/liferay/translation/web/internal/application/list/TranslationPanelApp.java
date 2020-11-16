affb0d96f6f184eb93baf65cacd194aa32981df4
Adolfo Pérez <adolfo.perez@liferay.com>
2020-11-16 13:59:28 +0100

    LPS-123529 Move app to Content menu section

--- modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/application/list/TranslationPanelApp.java
+++ modules/apps/translation/translation-web/src/main/java/com/liferay/translation/web/internal/application/list/TranslationPanelApp.java
@@ -29,8 +29,8 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.app.order:Integer=210",
-		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONFIGURATION
+		"panel.app.order:Integer=1300",
+		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_CONTENT
 	},
 	service = PanelApp.class
 )
