0c43236e70d5f219c88a3317dbc77db1f14f29ad
Adolfo Pérez <adolfo.perez@liferay.com>
2020-11-16 16:11:34 +0100

    LPS-123529 Index additional translation fields

--- modules/apps/translation/translation-service/src/main/java/com/liferay/translation/internal/search/spi/model/index/contributor/TranslationEntryModelDocumentContributor.java
+++ modules/apps/translation/translation-service/src/main/java/com/liferay/translation/internal/search/spi/model/index/contributor/TranslationEntryModelDocumentContributor.java
@@ -14,12 +14,23 @@
 
 package com.liferay.translation.internal.search.spi.model.index.contributor;
 
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 import com.liferay.translation.model.TranslationEntry;
 
+import java.util.Locale;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Adolfo Pérez
@@ -38,6 +49,45 @@ public class TranslationEntryModelDocumentContributor
 		document.addText(Field.CONTENT, translationEntry.getContent());
 		document.addDate(
 			Field.MODIFIED_DATE, translationEntry.getModifiedDate());
+		document.addKeyword(Field.STATUS, translationEntry.getStatus());
+
+		String title = _getTitle(translationEntry);
+
+		document.addText(Field.TITLE, title);
 	}
+
+	private Locale _getLocale(TranslationEntry translationEntry)
+		throws PortalException {
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext == null) {
+			return _portal.getSiteDefaultLocale(translationEntry.getGroupId());
+		}
+
+		return serviceContext.getLocale();
+	}
+
+	private String _getTitle(TranslationEntry translationEntry) {
+		try {
+			AssetRendererFactory<?> assetRendererFactory =
+				AssetRendererFactoryRegistryUtil.
+					getAssetRendererFactoryByClassName(
+						TranslationEntry.class.getName());
+
+			AssetRenderer<?> assetRenderer =
+				assetRendererFactory.getAssetRenderer(
+					translationEntry.getTranslationEntryId());
+
+			return assetRenderer.getTitle(_getLocale(translationEntry));
+		}
+		catch (Exception exception) {
+			return StringPool.BLANK;
+		}
+	}
+
+	@Reference
+	private Portal _portal;
 
 }
