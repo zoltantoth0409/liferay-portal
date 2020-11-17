2486db7db30c00a68da4c8b072a0f289c3fce957
Brian Chan <brian.chan@liferay.com>
2020-11-16 19:31:50 -0800

    LPS-121997 SF

--- portal-impl/src/com/liferay/portal/model/impl/CountryLocalizationImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/CountryLocalizationImpl.java
@@ -15,21 +15,10 @@
 package com.liferay.portal.model.impl;
 
 /**
- * The extended model implementation for the CountryLocalization service. Represents a row in the &quot;CountryLocalization&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.portal.kernel.model.CountryLocalization</code> interface.
- * </p>
- *
  * @author Brian Wing Shun Chan
  */
 public class CountryLocalizationImpl extends CountryLocalizationBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a country localization model instance should use the {@link com.liferay.portal.kernel.model.CountryLocalization} interface instead.
-	 */
 	public CountryLocalizationImpl() {
 	}
 
