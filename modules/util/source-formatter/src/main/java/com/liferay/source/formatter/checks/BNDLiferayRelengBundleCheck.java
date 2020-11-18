9187a0d6c8ac4aa55d32670f2be1dc3690c3e12f
Alan Huang <alan.huang@liferay.com>
2020-11-18 11:59:21 +0800

    LPS-123309 Add BNDLiferayRelengBundleCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDLiferayRelengBundleCheck.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.checks.util.BNDSourceUtil;
+
+import java.io.File;
+
+/**
+ * @author Alan Huang
+ */
+public class BNDLiferayRelengBundleCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isModuleSourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		if (!absolutePath.endsWith("/app.bnd")) {
+			return content;
+		}
+
+		String liferayRelengBundle = BNDSourceUtil.getDefinitionValue(
+			content, "Liferay-Releng-Bundle");
+
+		if (Validator.isNull(liferayRelengBundle) ||
+			liferayRelengBundle.equals("false") ||
+			!absolutePath.contains("/modules/dxp/apps/")) {
+
+			return content;
+		}
+
+		if (!_hasLfrbuildReleaseSrcFile(absolutePath)) {
+			addMessage(
+				fileName,
+				"Module in '/modules/dxp/apps/' directory and " +
+					"'Liferay-Enterprise-App' has value 'true' should have " +
+						"'.lfrbuild-release-src' file");
+		}
+
+		return content;
+	}
+
+	private boolean _hasLfrbuildReleaseSrcFile(String absolutePath) {
+		int pos = absolutePath.lastIndexOf(StringPool.SLASH);
+
+		File file = new File(
+			absolutePath.substring(0, pos + 1) + ".lfrbuild-release-src");
+
+		return file.exists();
+	}
+
+}
