bfc5bfbeefdcf07abdf11308f562e87cc92a07d0
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-11-18 12:38:46 +0100

    LPS-121571 We also want to track infoDisplayContributorField, so create a wrapper for them. This is mostly done for backwards compatibility issues
    Co-authored-by: Alejandro Tardín <alejandro.tardin@liferay.com>

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/item/field/reader/InfoItemFieldReaderTrackerImpl.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/item/field/reader/InfoItemFieldReaderTrackerImpl.java
@@ -14,21 +14,37 @@
 
 package com.liferay.info.internal.item.field.reader;
 
+import com.liferay.info.display.contributor.field.InfoDisplayContributorField;
+import com.liferay.info.display.contributor.field.InfoDisplayContributorFieldType;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.type.ImageInfoFieldType;
+import com.liferay.info.field.type.InfoFieldType;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.field.type.URLInfoFieldType;
 import com.liferay.info.item.field.reader.InfoItemFieldReader;
 import com.liferay.info.item.field.reader.InfoItemFieldReaderTracker;
+import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
 import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.reflect.GenericUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
 /**
  * @author Jürgen Kappler
@@ -45,11 +61,19 @@ public class InfoItemFieldReaderTrackerImpl
 		List<InfoItemFieldReader> infoItemFieldReaders =
 			_itemInfoItemFieldReaderServiceTrackerMap.getService(itemClassName);
 
-		if (infoItemFieldReaders != null) {
-			return new ArrayList<>(infoItemFieldReaders);
+		if (infoItemFieldReaders == null) {
+			infoItemFieldReaders = Collections.emptyList();
 		}
 
-		return Collections.emptyList();
+		List<InfoItemFieldReader> infoItemFieldReaderWrappers =
+			_infoItemFieldReaderWrappersMap.get(itemClassName);
+
+		if (infoItemFieldReaderWrappers == null) {
+			infoItemFieldReaderWrappers = Collections.emptyList();
+		}
+
+		return ListUtil.concat(
+			infoItemFieldReaders, infoItemFieldReaderWrappers);
 	}
 
 	@Activate
@@ -66,7 +90,128 @@ public class InfoItemFieldReaderTrackerImpl
 						"info.item.field.order")));
 	}
 
+	@Deactivate
+	protected void deactivate() {
+		_itemInfoItemFieldReaderServiceTrackerMap.close();
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC
+	)
+	protected void setInfoItemFieldReaderWrapper(
+		InfoDisplayContributorField<Object> infoDisplayContributorField,
+		Map<String, Object> properties) {
+
+		String className = (String)properties.get("model.class.name");
+
+		if (Validator.isNull(className)) {
+			return;
+		}
+
+		List<InfoItemFieldReader> infoItemFieldReaderWrappers =
+			_infoItemFieldReaderWrappersMap.computeIfAbsent(
+				className, itemClass -> new ArrayList<>());
+
+		infoItemFieldReaderWrappers.add(
+			new InfoItemFieldReaderWrapper(infoDisplayContributorField));
+	}
+
+	protected void unsetInfoItemFieldReaderWrapper(
+		InfoDisplayContributorField<?> infoDisplayContributorField,
+		Map<String, Object> properties) {
+
+		String className = (String)properties.get("model.class.name");
+
+		if (Validator.isNull(className)) {
+			return;
+		}
+
+		List<InfoItemFieldReader> infoItemFieldReaderWrappers =
+			_infoItemFieldReaderWrappersMap.get(className);
+
+		if (infoItemFieldReaderWrappers != null) {
+			for (InfoItemFieldReader infoItemFieldReader :
+					infoItemFieldReaderWrappers) {
+
+				InfoItemFieldReaderWrapper infoItemFieldReaderWrapper =
+					(InfoItemFieldReaderWrapper)infoItemFieldReader;
+
+				InfoDisplayContributorField<?>
+					existingInfoDisplayContributorField =
+						infoItemFieldReaderWrapper.
+							getInfoDisplayContributorField();
+
+				if (existingInfoDisplayContributorField ==
+						infoDisplayContributorField) {
+
+					infoItemFieldReaderWrappers.remove(
+						infoDisplayContributorField);
+				}
+			}
+		}
+	}
+
+	private final Map<String, List<InfoItemFieldReader>>
+		_infoItemFieldReaderWrappersMap = new ConcurrentHashMap<>();
 	private ServiceTrackerMap<String, List<InfoItemFieldReader>>
 		_itemInfoItemFieldReaderServiceTrackerMap;
+
+	private class InfoItemFieldReaderWrapper implements InfoItemFieldReader {
+
+		public InfoItemFieldReaderWrapper(
+			InfoDisplayContributorField<Object> infoDisplayContributorField) {
+
+			_infoDisplayContributorField = infoDisplayContributorField;
+		}
+
+		@Override
+		public InfoField<?> getField() {
+			InfoFieldType infoFieldType = TextInfoFieldType.INSTANCE;
+
+			InfoDisplayContributorFieldType infoDisplayContributorFieldType =
+				_infoDisplayContributorField.getType();
+
+			if (infoDisplayContributorFieldType ==
+					InfoDisplayContributorFieldType.IMAGE) {
+
+				infoFieldType = ImageInfoFieldType.INSTANCE;
+			}
+			else if (infoDisplayContributorFieldType ==
+						InfoDisplayContributorFieldType.URL) {
+
+				infoFieldType = URLInfoFieldType.INSTANCE;
+			}
+
+			return InfoField.builder(
+			).infoFieldType(
+				infoFieldType
+			).name(
+				getKey()
+			).labelInfoLocalizedValue(
+				(InfoLocalizedValue<String>)InfoLocalizedValue.function(
+					locale -> _infoDisplayContributorField.getLabel(locale))
+			).build();
+		}
+
+		public InfoDisplayContributorField<?> getInfoDisplayContributorField() {
+			return _infoDisplayContributorField;
+		}
+
+		@Override
+		public String getKey() {
+			return _infoDisplayContributorField.getKey();
+		}
+
+		@Override
+		public Object getValue(Object model) {
+			return InfoLocalizedValue.function(
+				locale -> _infoDisplayContributorField.getValue(model, locale));
+		}
+
+		private final InfoDisplayContributorField<Object>
+			_infoDisplayContributorField;
+
+	}
 
 }
