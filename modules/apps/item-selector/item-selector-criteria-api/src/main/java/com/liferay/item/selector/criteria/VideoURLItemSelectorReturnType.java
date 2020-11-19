995acf733aa40a5d6fdf91e5f6a271a7329a606b
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-11-19 17:45:18 +0100

    LPS-123729 Creating a specific return type for videos

---
+++ modules/apps/item-selector/item-selector-criteria-api/src/main/java/com/liferay/item/selector/criteria/VideoURLItemSelectorReturnType.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.item.selector.criteria;
+
+import com.liferay.item.selector.ItemSelectorReturnType;
+
+/**
+ * This return type should return the URL of a video as a string.
+ *
+ * If the video is an external video the URL will point to the
+ * external video service.
+ *
+ * @author Alejandro Tardín
+ */
+public class VideoURLItemSelectorReturnType implements ItemSelectorReturnType {
+}
