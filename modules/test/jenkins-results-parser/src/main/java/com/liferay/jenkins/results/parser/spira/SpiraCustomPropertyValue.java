57a739fafd423c7c2f410c3e5933ca611e2a8568
Michael Hashimoto <michael.hashimoto@liferay.com>
2020-11-18 16:56:34 -0800

    LRCI-1836 Add get name for spira custom property values

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraCustomPropertyValue.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraCustomPropertyValue.java
@@ -135,6 +135,13 @@ public abstract class SpiraCustomPropertyValue<T> extends BaseSpiraArtifact {
 		return true;
 	}
 
+	@Override
+	public String getName() {
+		SpiraCustomProperty spiraCustomProperty = getSpiraCustomProperty();
+
+		return spiraCustomProperty.getName();
+	}
+
 	public int getPropertyNumber() {
 		SpiraCustomProperty spiraCustomProperty = getSpiraCustomProperty();
 
