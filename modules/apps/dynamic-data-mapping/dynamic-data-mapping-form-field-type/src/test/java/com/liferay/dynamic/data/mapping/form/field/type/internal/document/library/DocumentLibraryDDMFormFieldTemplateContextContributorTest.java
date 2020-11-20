9cc4a53d23e9f2317aad38dd39bd630571629e1e
Carolina <carolina.barbosa@liferay.com>
2020-11-20 14:32:01 -0300

    LPS-123483 Fix tests

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/document/library/DocumentLibraryDDMFormFieldTemplateContextContributorTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/test/java/com/liferay/dynamic/data/mapping/form/field/type/internal/document/library/DocumentLibraryDDMFormFieldTemplateContextContributorTest.java
@@ -14,7 +14,9 @@
 
 package com.liferay.dynamic.data.mapping.form.field.type.internal.document.library;
 
+import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.dynamic.data.mapping.constants.DDMFormConstants;
 import com.liferay.dynamic.data.mapping.constants.DDMPortletKeys;
 import com.liferay.dynamic.data.mapping.form.field.type.BaseDDMFormFieldTypeSettingsTestCase;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
@@ -23,10 +25,14 @@ import com.liferay.portal.json.JSONFactoryImpl;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Repository;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepository;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletURL;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
@@ -89,7 +95,9 @@ public class DocumentLibraryDDMFormFieldTemplateContextContributorTest
 		setUpJSONFactory();
 		setUpJSONFactoryUtil();
 		setUpParamUtil();
+		setUpPortletFileRepository();
 		setUpRequestBackedPortletURLFactoryUtil();
+		setUpUserLocalService();
 	}
 
 	@Test
@@ -208,6 +216,30 @@ public class DocumentLibraryDDMFormFieldTemplateContextContributorTest
 		return spy;
 	}
 
+	protected Folder mockFolder() {
+		Folder folder = mock(Folder.class);
+
+		PowerMockito.when(
+			folder.getFolderId()
+		).thenReturn(
+			_FOLDER_ID
+		);
+
+		return folder;
+	}
+
+	protected Repository mockRepository() {
+		Repository repository = mock(Repository.class);
+
+		PowerMockito.when(
+			repository.getRepositoryId()
+		).thenReturn(
+			_REPOSITORY_ID
+		);
+
+		return repository;
+	}
+
 	protected RequestBackedPortletURLFactory
 		mockRequestBackedPortletURLFactory() {
 
@@ -323,6 +355,35 @@ public class DocumentLibraryDDMFormFieldTemplateContextContributorTest
 		PropsUtil.setProps(Mockito.mock(Props.class));
 	}
 
+	protected void setUpPortletFileRepository() throws Exception {
+		MemberMatcher.field(
+			DocumentLibraryDDMFormFieldTemplateContextContributor.class,
+			"_portletFileRepository"
+		).set(
+			_documentLibraryDDMFormFieldTemplateContextContributor,
+			_portletFileRepository
+		);
+
+		Repository repository = mockRepository();
+
+		PowerMockito.when(
+			_portletFileRepository.fetchPortletRepository(
+				_GROUP_ID, DDMFormConstants.SERVICE_NAME)
+		).thenReturn(
+			repository
+		);
+
+		Folder folder = mockFolder();
+
+		PowerMockito.when(
+			_portletFileRepository.getPortletFolder(
+				_REPOSITORY_ID, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+				DDMFormConstants.DDM_FORM_UPLOADED_FILES_FOLDER_NAME)
+		).thenReturn(
+			folder
+		);
+	}
+
 	protected void setUpRequestBackedPortletURLFactoryUtil() {
 		PowerMockito.mockStatic(RequestBackedPortletURLFactoryUtil.class);
 
@@ -337,14 +398,37 @@ public class DocumentLibraryDDMFormFieldTemplateContextContributorTest
 		);
 	}
 
+	protected void setUpUserLocalService() throws Exception {
+		MemberMatcher.field(
+			DocumentLibraryDDMFormFieldTemplateContextContributor.class,
+			"_userLocalService"
+		).set(
+			_documentLibraryDDMFormFieldTemplateContextContributor,
+			_userLocalService
+		);
+
+		User user = mockUser();
+
+		PowerMockito.when(
+			_userLocalService.getUserByScreenName(
+				_COMPANY_ID, DDMFormConstants.DDM_FORM_DEFAULT_USER_SCREEN_NAME)
+		).thenReturn(
+			user
+		);
+	}
+
 	private static final long _COMPANY_ID = RandomTestUtil.randomLong();
 
 	private static final String _FILE_ENTRY_UUID =
 		RandomTestUtil.randomString();
 
+	private static final long _FOLDER_ID = RandomTestUtil.randomLong();
+
 	private static final long _FORM_INSTANCE_ID = RandomTestUtil.randomLong();
 
 	private static final long _GROUP_ID = RandomTestUtil.randomLong();
+
+	private static final long _REPOSITORY_ID = RandomTestUtil.randomLong();
 
 	@Mock
 	private DLAppService _dlAppService;
@@ -360,6 +444,12 @@ public class DocumentLibraryDDMFormFieldTemplateContextContributorTest
 	private final JSONFactory _jsonFactory = new JSONFactoryImpl();
 
 	@Mock
+	private PortletFileRepository _portletFileRepository;
+
+	@Mock
 	private ResourceBundle _resourceBundle;
+
+	@Mock
+	private UserLocalService _userLocalService;
 
 }
