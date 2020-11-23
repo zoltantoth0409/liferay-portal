c9e1c88d91ccafcfc1c11c2d3008c345da87fdfc
Peter Shin <peter.shin@liferay.com>
2020-11-23 13:22:53 -0600

    LRCI-1827 Look directly for artifacts

--- modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/util/GradlePluginsDefaultsUtil.java
+++ modules/sdk/gradle-plugins-defaults/src/main/java/com/liferay/gradle/plugins/defaults/internal/util/GradlePluginsDefaultsUtil.java
@@ -31,6 +31,7 @@ import org.gradle.api.Project;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.artifacts.repositories.AuthenticationContainer;
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository.MetadataSources;
 import org.gradle.api.artifacts.repositories.PasswordCredentials;
 import org.gradle.internal.authentication.DefaultBasicAuthentication;
 
@@ -65,7 +66,22 @@ public class GradlePluginsDefaultsUtil {
 		RepositoryHandler repositoryHandler = project.getRepositories();
 
 		if (!Boolean.getBoolean("maven.local.ignore")) {
-			repositoryHandler.mavenLocal();
+			repositoryHandler.mavenLocal(
+				new Action<MavenArtifactRepository>() {
+
+					@Override
+					public void execute(
+						MavenArtifactRepository mavenArtifactRepository) {
+
+						MetadataSources metadataSources =
+							mavenArtifactRepository.getMetadataSources();
+
+						metadataSources.mavenPom();
+
+						metadataSources.artifact();
+					}
+
+				});
 
 			File tmpMavenRepositoryDir = null;
 
