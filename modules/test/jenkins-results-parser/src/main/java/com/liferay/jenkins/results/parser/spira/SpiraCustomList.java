9e7d1118978b3b946ab02183d0092be87467b48c
Michael Hashimoto <michael.hashimoto@liferay.com>
2020-11-22 16:44:07 -0800

    LRCI-1841 SF

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraCustomList.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraCustomList.java
@@ -20,7 +20,6 @@ import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil.HttpRequestMe
 import java.io.IOException;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
