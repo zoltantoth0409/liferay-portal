bf54d2abd684e39dfa6d6a84c5f57f541797d502
Luca Pellizzon <luca.pellizzon@liferay.com>
2020-11-24 17:06:51 +0100

    COMMERCE-3885 add date range filter on create date in orders

---
+++ modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/frontend/CommerceOrderDateRangeClayTableDataSetFilter.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.web.internal.frontend;
+
+import com.liferay.frontend.taglib.clay.data.set.filter.BaseDateRangeClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.filter.ClayDataSetFilter;
+import com.liferay.frontend.taglib.clay.data.set.filter.DateClayDataSetFilterItem;
+
+import java.util.Calendar;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "clay.data.set.display.name=" + CommerceOrderDataSetConstants.COMMERCE_DATA_SET_KEY_ALL_ORDERS,
+	service = ClayDataSetFilter.class
+)
+public class CommerceOrderDateRangeClayTableDataSetFilter
+	extends BaseDateRangeClayDataSetFilter {
+
+	@Override
+	public String getId() {
+		return "createDate";
+	}
+
+	@Override
+	public String getLabel() {
+		return "order-date-range";
+	}
+
+	public DateClayDataSetFilterItem getMaxDateClayDataSetFilterItem() {
+		Calendar calendar = Calendar.getInstance();
+
+		return new DateClayDataSetFilterItem(
+			calendar.get(Calendar.DAY_OF_MONTH),
+			calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR));
+	}
+
+	@Override
+	public DateClayDataSetFilterItem getMinDateClayDataSetFilterItem() {
+		return new DateClayDataSetFilterItem(0, 0, 0);
+	}
+
+}
