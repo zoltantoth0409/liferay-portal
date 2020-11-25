bfae7ed3a4b51d38d1633e67a716c2ce6334173f
Pavel Savinov <pavel.savinov@liferay.com>
2020-11-25 12:57:10 +0100

    LPS-123652 Remove padding for the borderless portlet decorator on the content pages as it was in 7.2

---
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/internal/servlet/taglib/PortletStyleCSSDynamicInclude.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.taglib.internal.servlet.taglib;
+
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(service = DynamicInclude.class)
+public class PortletStyleCSSDynamicInclude extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String dynamicIncludeKey)
+		throws IOException {
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		printWriter.print("<style data-senna-track=\"temporary\" ");
+		printWriter.print("type=\"text/css\">\n");
+		printWriter.print(".portlet-borderless .portlet-content {");
+		printWriter.print("padding: 0; } </style>\n");
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"com.liferay.layout,taglib#/render_fragment_layout/page.jsp#pre");
+	}
+
+}
