b041d76b031ac8f737f38814526c0aabf2c3e75d
Marcos <marcos.martins@liferay.com>
2020-11-26 08:32:19 -0300

    LPS-122337 prevent setting default localized value to editing localized value when its current value is an empty string

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/data-layout-builder/DataLayoutBuilder.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/data-layout-builder/DataLayoutBuilder.es.js
@@ -260,7 +260,9 @@ class DataLayoutBuilder extends React.Component {
 				if (localizable) {
 					if (this.props.contentType !== 'app-builder') {
 						availableLanguageIds.forEach((languageId) => {
-							if (!localizedValue[languageId]) {
+							if (
+								typeof localizedValue[languageId] !== 'string'
+							) {
 								localizedValue[languageId] =
 									localizedValue[defaultLanguageId] || '';
 							}
