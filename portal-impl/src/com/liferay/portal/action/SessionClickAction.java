bd5b5e2f370054ff0b2c8a7ff46f59200e5a1192
Jose A. Jimenez <jose.jimenez@liferay.com>
2020-11-26 09:52:49 +0100

    LPS-123739 Is a good practice to use the nullsafe util for string comparations

--- portal-impl/src/com/liferay/portal/action/SessionClickAction.java
+++ portal-impl/src/com/liferay/portal/action/SessionClickAction.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.SessionClicks;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.struts.Action;
 import com.liferay.portal.struts.model.ActionForward;
 import com.liferay.portal.struts.model.ActionMapping;
@@ -60,9 +61,9 @@ public class SessionClickAction implements Action {
 			while (enumeration.hasMoreElements()) {
 				String name = enumeration.nextElement();
 
-				if (!name.equals("cmd") &&
-					!name.equals("doAsUserId") &&
-					!name.equals("p_auth")) {
+				if (!StringUtil.equals(name, "cmd") &&
+					!StringUtil.equals(name, "doAsUserId") &&
+					!StringUtil.equals(name, "p_auth")) {
 
 					String value = ParamUtil.getString(
 						httpServletRequest, name);
@@ -82,7 +83,7 @@ public class SessionClickAction implements Action {
 				String cmd = ParamUtil.getString(
 					httpServletRequest, Constants.CMD);
 
-				if (cmd.equals("get")) {
+				if (StringUtil.equals(cmd, "get")) {
 					httpServletResponse.setContentType(ContentTypes.TEXT_PLAIN);
 				}
 				else {
@@ -114,7 +115,7 @@ public class SessionClickAction implements Action {
 		boolean useHttpSession = ParamUtil.getBoolean(
 			httpServletRequest, "useHttpSession");
 
-		if (cmd.equals("get")) {
+		if (StringUtil.equals(cmd, "get")) {
 			String key = ParamUtil.getString(httpServletRequest, "key");
 			String value = StringPool.BLANK;
 
@@ -127,7 +128,7 @@ public class SessionClickAction implements Action {
 
 			return value;
 		}
-		else if (cmd.equals("getAll")) {
+		else if (StringUtil.equals(cmd, "getAll")) {
 			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
 
 			String[] keys = httpServletRequest.getParameterValues("key");
