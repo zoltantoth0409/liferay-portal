d8bed3daca1083d681ddc76a42a1a569c597fd70
Pavel Savinov <pavel.savinov@liferay.com>
2020-11-26 17:25:38 +0100

    LPS-124057 We don't have file entry id on web image anymore

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/parser/ImageEditableElementParser.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/parser/ImageEditableElementParser.java
@@ -17,6 +17,9 @@ package com.liferay.fragment.entry.processor.editable.internal.parser;
 import com.liferay.fragment.entry.processor.editable.EditableFragmentEntryProcessor;
 import com.liferay.fragment.entry.processor.editable.parser.EditableElementParser;
 import com.liferay.fragment.exception.FragmentEntryContentException;
+import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.InfoItemIdentifier;
+import com.liferay.info.item.InfoItemReference;
 import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.info.type.WebImage;
 import com.liferay.layout.responsive.ViewportSize;
@@ -29,6 +32,7 @@ import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Html;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -86,7 +90,17 @@ public class ImageEditableElementParser implements EditableElementParser {
 				alt = altJSONObject.getString(LocaleUtil.toLanguageId(locale));
 			}
 
-			fileEntryId = fieldValueJSONObject.getLong("fileEntryId");
+			if (fieldValueJSONObject.has("fileEntryId")) {
+				fileEntryId = fieldValueJSONObject.getLong("fileEntryId");
+			}
+			else if (fieldValueJSONObject.has("className") &&
+					 fieldValueJSONObject.has("classPK") &&
+					 Objects.equals(
+						 fieldValueJSONObject.getString("className"),
+						 FileEntry.class.getName())) {
+
+				fileEntryId = fieldValueJSONObject.getLong("classPK");
+			}
 		}
 		else if (fieldValue instanceof WebImage) {
 			WebImage webImage = (WebImage)fieldValue;
@@ -101,7 +115,24 @@ public class ImageEditableElementParser implements EditableElementParser {
 				alt = infoLocalizedValue.getValue(locale);
 			}
 
-			fileEntryId = webImage.getFileEntryId();
+			InfoItemReference infoItemReference =
+				webImage.getInfoItemReference();
+
+			if ((infoItemReference != null) &&
+				Objects.equals(
+					infoItemReference.getClassName(),
+					FileEntry.class.getName())) {
+
+				InfoItemIdentifier infoItemIdentifier =
+					infoItemReference.getInfoItemIdentifier();
+
+				if (infoItemIdentifier instanceof ClassPKInfoItemIdentifier) {
+					ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
+						(ClassPKInfoItemIdentifier)infoItemIdentifier;
+
+					fileEntryId = classPKInfoItemIdentifier.getClassPK();
+				}
+			}
 		}
 
 		return JSONUtil.put(
