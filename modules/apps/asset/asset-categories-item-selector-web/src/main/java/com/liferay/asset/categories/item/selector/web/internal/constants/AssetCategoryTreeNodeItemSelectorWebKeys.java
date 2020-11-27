67250045f4af8b5f17c5e4e138b0db088022838c
Rubén Pulido <ruben.pulido@liferay.com>
2020-11-27 13:25:31 +0100

    LPS-123642 Web keys

---
+++ modules/apps/asset/asset-categories-item-selector-web/src/main/java/com/liferay/asset/categories/item/selector/web/internal/constants/AssetCategoryTreeNodeItemSelectorWebKeys.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.item.selector.web.internal.constants;
+
+/**
+ * @author Rubén Pulido
+ */
+public class AssetCategoryTreeNodeItemSelectorWebKeys {
+
+	public static final String
+		SELECT_ASSET_CATEGORY_TREE_NODE_ITEM_SELECTOR_DISPLAY_CONTEXT =
+			"SELECT_ASSET_CATEGORY_TREE_NODE_ITEM_SELECTOR_DISPLAY_CONTEXT";
+
+	public static final String SELECT_ASSET_VOCABULARY_DISPLAY_CONTEXT =
+		"SELECT_ASSET_VOCABULARY_DISPLAY_CONTEXT";
+
+}
