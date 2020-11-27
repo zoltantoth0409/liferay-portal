527ec3fa5df22ca58466d324990b3f103c73b923
Carolina <carolina.barbosa@liferay.com>
2020-11-27 13:40:10 -0300

    LPS-123543 Regen build-service-portlet-documentlibrary

---
+++ portal-kernel/src/com/liferay/document/library/kernel/exception/InvalidFileException.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.kernel.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class InvalidFileException extends PortalException {
+
+	public InvalidFileException() {
+	}
+
+	public InvalidFileException(String msg) {
+		super(msg);
+	}
+
+	public InvalidFileException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public InvalidFileException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
