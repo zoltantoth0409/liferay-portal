e8fbf94e7725a2bd79d087989142d9b8dcb6c743
Pavel Savinov <pavel.savinov@liferay.com>
2020-11-27 12:09:31 +0100

    LPS-124003 Upgrade basic section names since we split the original collection

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/upgrade/v2_5_0/UpgradeFragmentEntryLink.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/upgrade/v2_5_0/UpgradeFragmentEntryLink.java
@@ -15,9 +15,16 @@
 package com.liferay.fragment.internal.upgrade.v2_5_0;
 
 import com.liferay.fragment.internal.upgrade.v2_5_0.util.FragmentEntryLinkTable;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.Map;
 
 /**
  * @author Eudaldo Alonso
@@ -26,6 +33,41 @@ public class UpgradeFragmentEntryLink extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
+		upgradeRendererKey();
+		upgratePlid();
+	}
+
+	protected void upgradeRendererKey() throws Exception {
+		try (PreparedStatement ps1 = connection.prepareStatement(
+				"select fragmentEntryLinkId, rendererKey from " +
+					"FragmentEntryLink where rendererKey like " +
+						"'BASIC_SECTION%'");
+			ResultSet rs = ps1.executeQuery();
+			PreparedStatement ps2 = AutoBatchPreparedStatementUtil.autoBatch(
+				connection.prepareStatement(
+					"update FragmentEntryLink set rendererKey = ? where " +
+						"fragmentEntryLinkId = ?"))) {
+
+			while (rs.next()) {
+				long fragmentEntryLinkId = rs.getLong("fragmentEntryLinkId");
+
+				String rendererKey = rs.getString("rendererKey");
+
+				ps2.setString(
+					1,
+					_contributedFragmentKeys.getOrDefault(
+						rendererKey, rendererKey));
+
+				ps2.setLong(2, fragmentEntryLinkId);
+
+				ps2.addBatch();
+			}
+
+			ps2.executeBatch();
+		}
+	}
+
+	protected void upgratePlid() throws Exception {
 		if (!hasColumn("FragmentEntryLink", "plid")) {
 			alter(
 				FragmentEntryLinkTable.class,
@@ -36,5 +78,32 @@ public class UpgradeFragmentEntryLink extends UpgradeProcess {
 			"update FragmentEntryLink set plid = classPK where classNameId = " +
 				PortalUtil.getClassNameId(Layout.class.getName()));
 	}
+
+	private static final Map<String, String> _contributedFragmentKeys =
+		HashMapBuilder.put(
+			"BASIC_SECTION-banner", "FEATURED_CONTENT-banner"
+		).put(
+			"BASIC_SECTION-banner-center", "FEATURED_CONTENT-banner-center"
+		).put(
+			"BASIC_SECTION-banner-cover", "FEATURED_CONTENT-banner-cover"
+		).put(
+			"BASIC_SECTION-banner-cover-center",
+			"FEATURED_CONTENT-banner-cover-center"
+		).put(
+			"BASIC_SECTION-features", "FEATURED_CONTENT-features"
+		).put(
+			"BASIC_SECTION-footer-nav-dark", "FOOTERS-footer-nav-dark"
+		).put(
+			"BASIC_SECTION-footer-nav-light", "FOOTERS-footer-nav-dark"
+		).put(
+			"BASIC_SECTION-header-dark", "NAVIGATION_BARS-header-dark"
+		).put(
+			"BASIC_SECTION-header-light", "NAVIGATION_BARS-header-light"
+		).put(
+			"BASIC_SECTION-highlights", "FEATURED_CONTENT-highlights"
+		).put(
+			"BASIC_SECTION-highlights-circle",
+			"FEATURED_CONTENT-highlights-circle"
+		).build();
 
 }
