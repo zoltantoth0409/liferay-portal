bc4272dfce0da7f68d9c8d1e9f4c7f777a051e60
Rubén Pulido <ruben.pulido@liferay.com>
2020-11-30 09:29:41 +0100

    LPS-123388 Define WebKeys

---
+++ modules/apps/fragment/fragment-renderer-collection-filter-impl/src/main/java/com/liferay/fragment/renderer/collection/filter/internal/constants/CollectionFilterFragmentRendererWebKeys.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.renderer.collection.filter.internal.constants;
+
+/**
+ * @author Rubén Pulido
+ */
+public class CollectionFilterFragmentRendererWebKeys {
+
+	public static final String ASSET_CATEGORIES = "ASSET_CATEGORIES";
+
+	public static final String ASSET_VOCABULARY = "ASSET_VOCABULARY";
+
+	public static final String CATEGORY_ID = "categoryId";
+
+}
