3f4b5b0b762ea0c5359257aa5b0768c2971cd511
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-12-04 15:06:14 +0100

    LPS-123084 Add tests.

---
+++ modules/apps/segments/segments-test/src/testIntegration/java/com/liferay/segments/internal/events/test/SegmentsServicePreActionTest.java
@@ -0,0 +1,242 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.segments.internal.events.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.events.LifecycleAction;
+import com.liferay.portal.kernel.events.LifecycleEvent;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.registry.Filter;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceTracker;
+import com.liferay.segments.constants.SegmentsWebKeys;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * @author Cristina González
+ */
+@RunWith(Arquillian.class)
+public class SegmentsServicePreActionTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@BeforeClass
+	public static void setUpClass() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		Filter filter = registry.getFilter(
+			"(&(objectClass=" + LifecycleAction.class.getName() +
+				")(key=servlet.service.events.pre))");
+
+		_serviceTracker = registry.trackServices(filter);
+
+		_serviceTracker.open();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_serviceTracker.close();
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testProcessLifecycleEvent() throws Exception {
+		LifecycleAction lifecycleAction = _getLifecycleAction();
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		Map<Locale, String> nameMap = Collections.singletonMap(
+			LocaleUtil.getDefault(), RandomTestUtil.randomString());
+
+		UnicodeProperties typeSettingsUnicodeProperties =
+			new UnicodeProperties();
+
+		typeSettingsUnicodeProperties.put("published", "true");
+
+		Layout layout = _layoutLocalService.addLayout(
+			TestPropsValues.getUserId(), _group.getGroupId(), false,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, 0, 0, nameMap, nameMap,
+			Collections.emptyMap(), Collections.emptyMap(),
+			Collections.emptyMap(), LayoutConstants.TYPE_COLLECTION,
+			typeSettingsUnicodeProperties.toString(), false, false,
+			Collections.emptyMap(), 0, serviceContext);
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay(layout));
+
+		LifecycleEvent lifecycleEvent = new LifecycleEvent(
+			mockHttpServletRequest, new MockHttpServletResponse());
+
+		lifecycleAction.processLifecycleEvent(lifecycleEvent);
+
+		Assert.assertNotNull(
+			mockHttpServletRequest.getAttribute(
+				SegmentsWebKeys.SEGMENTS_ENTRY_IDS));
+
+		Assert.assertNotNull(
+			mockHttpServletRequest.getAttribute(
+				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS));
+	}
+
+	@Test
+	public void testProcessLifecycleEventWithoutContentLayout()
+		throws Exception {
+
+		LifecycleAction lifecycleAction = _getLifecycleAction();
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		Layout layout = LayoutTestUtil.addLayout(_group.getGroupId());
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay(layout));
+
+		LifecycleEvent lifecycleEvent = new LifecycleEvent(
+			mockHttpServletRequest, new MockHttpServletResponse());
+
+		lifecycleAction.processLifecycleEvent(lifecycleEvent);
+
+		Assert.assertNotNull(
+			mockHttpServletRequest.getAttribute(
+				SegmentsWebKeys.SEGMENTS_ENTRY_IDS));
+
+		Assert.assertNull(
+			mockHttpServletRequest.getAttribute(
+				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS));
+	}
+
+	@Test
+	public void testProcessLifecycleEventWithoutLayout() throws Exception {
+		LifecycleAction lifecycleAction = _getLifecycleAction();
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay(null));
+
+		LifecycleEvent lifecycleEvent = new LifecycleEvent(
+			mockHttpServletRequest, new MockHttpServletResponse());
+
+		lifecycleAction.processLifecycleEvent(lifecycleEvent);
+
+		Assert.assertNull(
+			mockHttpServletRequest.getAttribute(
+				SegmentsWebKeys.SEGMENTS_ENTRY_IDS));
+
+		Assert.assertNull(
+			mockHttpServletRequest.getAttribute(
+				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS));
+	}
+
+	private LifecycleAction _getLifecycleAction() {
+		Object[] services = _serviceTracker.getServices();
+
+		for (Object service : services) {
+			Class<?> clazz = service.getClass();
+
+			if (Objects.equals(
+					clazz.getName(),
+					"com.liferay.segments.internal.events." +
+						"SegmentsServicePreAction")) {
+
+				return (LifecycleAction)service;
+			}
+		}
+
+		throw new AssertionError("SegmentsServicePreAction is not registered");
+	}
+
+	private ThemeDisplay _getThemeDisplay(Layout layout) throws Exception {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		Company company = _companyLocalService.getCompany(
+			TestPropsValues.getCompanyId());
+
+		themeDisplay.setCompany(company);
+
+		themeDisplay.setLayout(layout);
+		themeDisplay.setLifecycleRender(true);
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		return themeDisplay;
+	}
+
+	private static ServiceTracker<LifecycleAction, LifecycleAction>
+		_serviceTracker;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private GroupLocalService _groupLocalService;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
+}
