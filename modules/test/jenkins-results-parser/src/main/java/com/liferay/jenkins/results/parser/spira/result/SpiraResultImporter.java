4b1e3c1d7f35c4b36f7700f07cae13212cfef88f
Michael Hashimoto <michael.hashimoto@liferay.com>
2020-12-08 14:58:33 -0800

    LRCI-1839 Add method to setup to the spira result importer

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/result/SpiraResultImporter.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/result/SpiraResultImporter.java
@@ -14,15 +14,31 @@
 
 package com.liferay.jenkins.results.parser.spira.result;
 
+import com.liferay.jenkins.results.parser.AnalyticsCloudBranchInformationBuild;
+import com.liferay.jenkins.results.parser.AntException;
+import com.liferay.jenkins.results.parser.AntUtil;
 import com.liferay.jenkins.results.parser.Build;
 import com.liferay.jenkins.results.parser.BuildFactory;
+import com.liferay.jenkins.results.parser.GitWorkingDirectory;
+import com.liferay.jenkins.results.parser.GitWorkingDirectoryFactory;
+import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil;
 import com.liferay.jenkins.results.parser.Job;
+import com.liferay.jenkins.results.parser.LocalGitBranch;
+import com.liferay.jenkins.results.parser.PluginsBranchInformationBuild;
+import com.liferay.jenkins.results.parser.PortalBranchInformationBuild;
+import com.liferay.jenkins.results.parser.PortalGitRepositoryJob;
+import com.liferay.jenkins.results.parser.PortalGitWorkingDirectory;
+import com.liferay.jenkins.results.parser.QAWebsitesBranchInformationBuild;
 import com.liferay.jenkins.results.parser.TopLevelBuild;
 import com.liferay.jenkins.results.parser.test.clazz.group.AxisTestClassGroup;
 import com.liferay.jenkins.results.parser.test.clazz.group.TestClassGroup;
 
+import java.io.File;
+import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author Michael Hashimoto
@@ -62,6 +78,200 @@ public class SpiraResultImporter {
 		for (SpiraTestResult spiraTestResult : spiraTestResults) {
 			spiraTestResult.record();
 		}
+	}
+
+	public void setup() {
+		_checkoutPortalBranch();
+
+		_checkoutPortalBaseBranch();
+
+		_checkoutOSBFaroBranch();
+
+		_checkoutPluginsBranch();
+
+		_checkoutQAWebsitesBranch();
+	}
+
+	private void _checkoutOSBFaroBranch() {
+		if (!(_topLevelBuild instanceof AnalyticsCloudBranchInformationBuild)) {
+			return;
+		}
+
+		AnalyticsCloudBranchInformationBuild
+			analyticsCloudBranchInformationBuild =
+				(AnalyticsCloudBranchInformationBuild)_topLevelBuild;
+
+		Build.BranchInformation branchInformation =
+			analyticsCloudBranchInformationBuild.getOSBFaroBranchInformation();
+
+		String upstreamBranchName = branchInformation.getUpstreamBranchName();
+
+		String upstreamDirPath = JenkinsResultsParserUtil.getProperty(
+			_getBuildProperties(), "osb.faro.dir", upstreamBranchName);
+		String upstreamRepository = JenkinsResultsParserUtil.getProperty(
+			_getBuildProperties(), "osb.faro.repository", upstreamBranchName);
+
+		GitWorkingDirectory gitWorkingDirectory =
+			GitWorkingDirectoryFactory.newGitWorkingDirectory(
+				upstreamBranchName, upstreamDirPath, upstreamRepository);
+
+		gitWorkingDirectory.checkoutLocalGitBranch(branchInformation);
+
+		gitWorkingDirectory.displayLog();
+	}
+
+	private void _checkoutPluginsBranch() {
+		if (!(_topLevelBuild instanceof PluginsBranchInformationBuild)) {
+			return;
+		}
+
+		PluginsBranchInformationBuild pluginsBranchInformationBuild =
+			(PluginsBranchInformationBuild)_topLevelBuild;
+
+		Build.BranchInformation branchInformation =
+			pluginsBranchInformationBuild.getPluginsBranchInformation();
+
+		String upstreamBranchName = branchInformation.getUpstreamBranchName();
+
+		String upstreamDirPath = JenkinsResultsParserUtil.getProperty(
+			_getBuildProperties(), "plugins.dir", upstreamBranchName);
+		String upstreamRepository = JenkinsResultsParserUtil.getProperty(
+			_getBuildProperties(), "plugins.repository", upstreamBranchName);
+
+		GitWorkingDirectory gitWorkingDirectory =
+			GitWorkingDirectoryFactory.newGitWorkingDirectory(
+				upstreamBranchName, upstreamDirPath, upstreamRepository);
+
+		gitWorkingDirectory.checkoutLocalGitBranch(branchInformation);
+
+		gitWorkingDirectory.displayLog();
+	}
+
+	private void _checkoutPortalBaseBranch() {
+		if (!(_topLevelBuild instanceof PortalBranchInformationBuild)) {
+			return;
+		}
+
+		PortalBranchInformationBuild portalBranchInformationBuild =
+			(PortalBranchInformationBuild)_topLevelBuild;
+
+		Build.BranchInformation branchInformation =
+			portalBranchInformationBuild.getPortalBaseBranchInformation();
+
+		if (branchInformation == null) {
+			return;
+		}
+
+		String upstreamBranchName = branchInformation.getUpstreamBranchName();
+
+		String upstreamDirPath = JenkinsResultsParserUtil.getProperty(
+			_getBuildProperties(), "portal.dir", upstreamBranchName);
+		String upstreamRepository = JenkinsResultsParserUtil.getProperty(
+			_getBuildProperties(), "portal.repository", upstreamBranchName);
+
+		GitWorkingDirectory portalBaseGitWorkingDirectory =
+			GitWorkingDirectoryFactory.newGitWorkingDirectory(
+				upstreamBranchName, upstreamDirPath, upstreamRepository);
+
+		LocalGitBranch portalBaseLocalGitBranch =
+			portalBaseGitWorkingDirectory.checkoutLocalGitBranch(
+				branchInformation);
+
+		portalBaseGitWorkingDirectory.displayLog();
+
+		PortalGitWorkingDirectory portalGitWorkingDirectory =
+			_getPortalGitWorkingDirectory();
+
+		portalGitWorkingDirectory.fetch(
+			portalBaseLocalGitBranch.getName(), portalBaseLocalGitBranch);
+
+		try {
+			JenkinsResultsParserUtil.write(
+				new File(
+					portalGitWorkingDirectory.getWorkingDirectory(),
+					"git-commit-portal"),
+				portalBaseLocalGitBranch.getSHA());
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+
+		try {
+			AntUtil.callTarget(
+				portalGitWorkingDirectory.getWorkingDirectory(),
+				"build-working-dir.xml", "prepare-working-dir");
+		}
+		catch (AntException antException) {
+			throw new RuntimeException(antException);
+		}
+	}
+
+	private void _checkoutPortalBranch() {
+		if (!(_topLevelBuild instanceof PortalBranchInformationBuild)) {
+			return;
+		}
+
+		PortalBranchInformationBuild portalBranchInformationBuild =
+			(PortalBranchInformationBuild)_topLevelBuild;
+
+		PortalGitWorkingDirectory portalGitWorkingDirectory =
+			_getPortalGitWorkingDirectory();
+
+		portalGitWorkingDirectory.checkoutLocalGitBranch(
+			portalBranchInformationBuild.getPortalBranchInformation());
+
+		portalGitWorkingDirectory.displayLog();
+	}
+
+	private void _checkoutQAWebsitesBranch() {
+		if (!(_topLevelBuild instanceof QAWebsitesBranchInformationBuild)) {
+			return;
+		}
+
+		QAWebsitesBranchInformationBuild qaWebsitesBranchInformationBuild =
+			(QAWebsitesBranchInformationBuild)_topLevelBuild;
+
+		Build.BranchInformation branchInformation =
+			qaWebsitesBranchInformationBuild.getQAWebsitesBranchInformation();
+
+		String upstreamBranchName = branchInformation.getUpstreamBranchName();
+
+		String upstreamDirPath = JenkinsResultsParserUtil.getProperty(
+			_getBuildProperties(), "qa.websites.dir", upstreamBranchName);
+		String upstreamRepository = JenkinsResultsParserUtil.getProperty(
+			_getBuildProperties(), "qa.websites.repository",
+			upstreamBranchName);
+
+		GitWorkingDirectory gitWorkingDirectory =
+			GitWorkingDirectoryFactory.newGitWorkingDirectory(
+				upstreamBranchName, upstreamDirPath, upstreamRepository);
+
+		gitWorkingDirectory.checkoutLocalGitBranch(branchInformation);
+
+		gitWorkingDirectory.displayLog();
+	}
+
+	private Properties _getBuildProperties() {
+		try {
+			return JenkinsResultsParserUtil.getBuildProperties();
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+	}
+
+	private PortalGitWorkingDirectory _getPortalGitWorkingDirectory() {
+		Job job = _topLevelBuild.getJob();
+
+		if (job instanceof PortalGitRepositoryJob) {
+			PortalGitRepositoryJob portalGitRepositoryJob =
+				(PortalGitRepositoryJob)job;
+
+			return portalGitRepositoryJob.getPortalGitWorkingDirectory();
+		}
+
+		return GitWorkingDirectoryFactory.newPortalGitWorkingDirectory(
+			"master");
 	}
 
 	private final SpiraBuildResult _spiraBuildResult;
