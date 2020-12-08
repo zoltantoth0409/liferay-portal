7495230fda0baa6086567cd38fd34e1f867ba114
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2020-12-08 12:52:18 +0100

    COMMERCE-5270 Unit tests for QuantitySelector component

---
+++ modules/apps/commerce/commerce-frontend-js/test/QuantitySelector/QuantitySelector.js
@@ -0,0 +1,285 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {act, cleanup, fireEvent, render, wait} from '@testing-library/react';
+import React from 'react';
+
+import QuantitySelector from '../../src/main/resources/META-INF/resources/components/quantity_selector/QuantitySelector';
+import * as Utils from '../../src/main/resources/META-INF/resources/components/quantity_selector/utils/index';
+
+jest.mock(
+	'../../src/main/resources/META-INF/resources/components/quantity_selector/utils/index'
+);
+
+describe('QuantitySelector', () => {
+	describe('by default', () => {
+		let Component;
+		let onUpdateSpy;
+
+		beforeEach(() => {
+			jest.resetAllMocks();
+
+			onUpdateSpy = jest.fn();
+			Component = render(<QuantitySelector onUpdate={onUpdateSpy} />);
+		});
+
+		afterEach(() => {
+			cleanup();
+
+			Component = null;
+			onUpdateSpy = null;
+		});
+
+		it('renders as an Input element with default attributes', () => {
+			const element = Component.container.querySelector('input');
+
+			const defaultProps = {
+				maxQuantity: 99,
+				minQuantity: 1,
+				multipleQuantity: 1,
+				quantity: 1,
+			};
+
+			expect(element).toBeInTheDocument();
+
+			expect(element.max).toEqual(defaultProps.maxQuantity.toString());
+			expect(element.min).toEqual(defaultProps.minQuantity.toString());
+			expect(element.step).toEqual(
+				defaultProps.multipleQuantity.toString()
+			);
+			expect(element.type).toEqual('number');
+			expect(element.value).toEqual(defaultProps.quantity.toString());
+		});
+
+		it('accepts only number-typed input values', async () => {
+			const element = Component.container.querySelector('input');
+			const updatedValue = 'abc';
+
+			const defaultProps = {
+				quantity: 1,
+			};
+
+			await act(async () => {
+				fireEvent.change(element, {target: {value: updatedValue}});
+			});
+
+			await wait(() => {
+				expect(element.value).not.toEqual(updatedValue);
+				expect(element.value).toEqual(defaultProps.quantity.toString());
+			});
+		});
+
+		it('updates the quantity if input value is changed', async () => {
+			const element = Component.container.querySelector('input');
+			const updatedValue = '2';
+
+			await act(async () => {
+				fireEvent.change(element, {target: {value: updatedValue}});
+			});
+
+			await wait(() => {
+				expect(element.value).toEqual(updatedValue);
+			});
+		});
+
+		it('updates the quantity to the default of 1 if input value is empty', async () => {
+			const element = Component.container.querySelector('input');
+			const updatedValue = '';
+
+			const defaultProps = {
+				quantity: 1,
+			};
+
+			await act(async () => {
+				fireEvent.change(element, {target: {value: updatedValue}});
+			});
+
+			await wait(() => {
+				expect(element.value).toEqual(defaultProps.quantity.toString());
+			});
+		});
+
+		it('calls the onUpdate callback on input value change after 500ms', async () => {
+			const element = Component.container.querySelector('input');
+			const updatedValue = 2;
+
+			await act(async () => {
+				fireEvent.change(element, {
+					target: {value: updatedValue.toString()},
+				});
+			});
+
+			await wait(
+				() => {
+					expect(onUpdateSpy).toHaveBeenCalledTimes(1);
+					expect(onUpdateSpy).toHaveBeenCalledWith(updatedValue);
+				},
+				{
+					timeout: Utils.UPDATE_AFTER,
+				}
+			);
+		});
+
+		it('calls the onUpdate callback only once if typed value did not change within 500ms', async () => {
+			const TIMES = 4;
+			const TYPING_THRESHOLD = 25;
+
+			const element = Component.container.querySelector('input');
+
+			let updatedValue = '1';
+
+			await act(async () => {
+				const updateByTyping = setInterval(() => {
+					if (updatedValue.length < TIMES) {
+						updatedValue += '0';
+
+						fireEvent.change(element, {
+							target: {value: updatedValue},
+						});
+					}
+					else {
+						clearInterval(updateByTyping);
+					}
+				}, TYPING_THRESHOLD);
+			});
+
+			await wait(
+				() => {
+					expect(onUpdateSpy).toHaveBeenCalledTimes(1);
+					expect(onUpdateSpy).toHaveBeenCalledWith(1000);
+				},
+				{
+					timeout: TIMES * TYPING_THRESHOLD + Utils.UPDATE_AFTER,
+				}
+			);
+		});
+	});
+
+	describe('by custom props', () => {
+		beforeEach(() => {
+			jest.resetAllMocks();
+		});
+
+		afterEach(() => {
+			cleanup();
+		});
+
+		it('renders as Select element with default number of Option elements if forceDropdown prop is true', () => {
+			const optionSettingsProps = {
+				allowedQuantities: [],
+				maxQuantity: 99,
+				minQuantity: 1,
+				multipleQuantity: 1,
+			};
+
+			const defaultProps = {
+				quantity: 1,
+				...optionSettingsProps,
+			};
+
+			const Component = render(
+				<QuantitySelector forceDropdown={true} {...defaultProps} />
+			);
+
+			const selectElement = Component.container.querySelector('select');
+
+			expect(selectElement).toBeInTheDocument();
+			expect(Utils.generateQuantityOptions).toHaveBeenCalledTimes(1);
+			expect(Utils.generateQuantityOptions).toHaveBeenCalledWith(
+				optionSettingsProps
+			);
+		});
+
+		it('renders as Select element with default number of Option elements if allowedQuantities prop is non-empty', () => {
+			const optionSettingsProps = {
+				allowedQuantities: [2, 3, 4, 5],
+				maxQuantity: 99,
+				minQuantity: 1,
+				multipleQuantity: 1,
+			};
+
+			const defaultProps = {
+				quantity: 1,
+				...optionSettingsProps,
+			};
+
+			const Component = render(<QuantitySelector {...defaultProps} />);
+
+			const selectElement = Component.container.querySelector('select');
+
+			expect(selectElement).toBeInTheDocument();
+			expect(Utils.generateQuantityOptions).toHaveBeenCalledTimes(1);
+			expect(Utils.generateQuantityOptions).toHaveBeenCalledWith(
+				optionSettingsProps
+			);
+		});
+
+		it('floors the input value to the closest lower multiple if multipleQuantity > 1', async () => {
+			const onUpdateSpy = jest.fn();
+
+			const optionSettingsProps = {
+				multipleQuantity: 2,
+			};
+
+			const defaultProps = {
+				quantity: 1,
+				...optionSettingsProps,
+			};
+
+			const Component = render(
+				<QuantitySelector onUpdate={onUpdateSpy} {...defaultProps} />
+			);
+
+			const element = Component.container.querySelector('input');
+			const updatedValue = 53;
+
+			await act(async () => {
+				fireEvent.change(element, {
+					target: {value: updatedValue.toString()},
+				});
+			});
+
+			await wait(
+				() => {
+					expect(onUpdateSpy).toHaveBeenCalledTimes(1);
+					expect(onUpdateSpy).toHaveBeenCalledWith(52);
+				},
+				{
+					timeout: Utils.UPDATE_AFTER,
+				}
+			);
+		});
+
+		it('disables the input element if required via prop', () => {
+			const Component = render(<QuantitySelector disabled={true} />);
+
+			const element = Component.container.querySelector('input');
+
+			expect(element).toBeInTheDocument();
+			expect(element).toBeDisabled();
+		});
+
+		it('disables the select element if required via prop', () => {
+			const Component = render(
+				<QuantitySelector disabled={true} forceDropdown={true} />
+			);
+
+			const element = Component.container.querySelector('select');
+
+			expect(element).toBeInTheDocument();
+			expect(element).toBeDisabled();
+		});
+	});
+});
---
+++ modules/apps/commerce/commerce-frontend-js/test/QuantitySelector/utils/index.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import * as Utils from '../../../src/main/resources/META-INF/resources/components/quantity_selector/utils/index';
+
+describe('QuantitySelector Util -> generateQuantityOptions', () => {
+	it('returns allowedQuantities, if non-empty, as formatted options', () => {
+		const allowedQuantities = [
+			2,
+			4,
+			42,
+			65,
+			33,
+			913,
+			267,
+			323,
+			122,
+			90,
+			113,
+		];
+
+		expect(Utils.generateQuantityOptions({allowedQuantities})).toEqual(
+			allowedQuantities.map((value) => ({
+				label: value.toString(),
+				value,
+			}))
+		);
+	});
+
+	it('returns formatted options range from minQuantity to maxQuantity if allowedQuantities is empty', () => {
+		expect(
+			Utils.generateQuantityOptions({
+				allowedQuantities: [],
+				maxQuantity: 5,
+				minQuantity: 2,
+			})
+		).toEqual(
+			[2, 3, 4, 5].map((value) => ({
+				label: value.toString(),
+				value,
+			}))
+		);
+	});
+
+	it('ignores minQuantity and maxQuantity and returns allowedQuantities as formatted options if it is not empty', () => {
+		const allowedQuantities = [3, 5, 7, 9];
+
+		expect(
+			Utils.generateQuantityOptions({
+				allowedQuantities,
+				maxQuantity: 9,
+				minQuantity: 1,
+				multipleQuantity: 2,
+			})
+		).toEqual(
+			allowedQuantities.map((value) => ({
+				label: value.toString(),
+				value,
+			}))
+		);
+	});
+
+	it('returns as formatted options computed quantities as multiples if multipleQuantity is greater than 1', () => {
+		expect(
+			Utils.generateQuantityOptions({
+				allowedQuantities: [],
+				maxQuantity: 4,
+				minQuantity: 1,
+				multipleQuantity: 2,
+			})
+		).toEqual(
+			[2, 4, 6, 8].map((value) => ({
+				label: value.toString(),
+				value,
+			}))
+		);
+	});
+
+	it('returns only one option if allowedQuantities, maxQuantity, minQuantity and multipleQuantity are not passed in', () => {
+		expect(Utils.generateQuantityOptions({})).toEqual(
+			[1].map((value) => ({
+				label: value.toString(),
+				value,
+			}))
+		);
+	});
+});
