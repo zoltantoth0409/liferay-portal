f52c38b49072ffcbbb27d9555c805272c0dce390
Michael Hashimoto <michael.hashimoto@liferay.com>
2020-12-09 14:42:55 -0800

    LRCI-1839 Add method to update spira custom property values using an array

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraTestCaseObject.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/spira/SpiraTestCaseObject.java
@@ -22,6 +22,7 @@ import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil.HttpRequestMe
 import java.io.IOException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -440,6 +441,13 @@ public class SpiraTestCaseObject extends PathSpiraArtifact {
 		jsonObject.put("CustomProperties", customPropertiesJSONArray);
 
 		_updateJSONObject(jsonObject);
+	}
+
+	public void updateSpiraCustomPropertyValues(
+		SpiraCustomPropertyValue... spiraCustomPropertyValues) {
+
+		updateSpiraCustomPropertyValues(
+			Arrays.asList(spiraCustomPropertyValues));
 	}
 
 	public void updateSpiraTestCaseComponents(
