a60125e1d7d6cb7efa1c5ed1699dce52322b788e
Riccardo <riccardo.alberti@liferay.com>
2020-12-09 14:25:45 +0100

    COMMERCE-5295 Add channel model listener to delete payment, shipping and tax methods on channel delete

---
+++ modules/apps/commerce/commerce-payment-service/src/main/java/com/liferay/commerce/payment/internal/model/listener/CommerceChannelModelListener.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.payment.internal.model.listener;
+
+import com.liferay.commerce.payment.service.CommercePaymentMethodGroupRelLocalService;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Riccardo Alberti
+ */
+@Component(enabled = false, immediate = true, service = ModelListener.class)
+public class CommerceChannelModelListener
+	extends BaseModelListener<CommerceChannel> {
+
+	@Override
+	public void onBeforeRemove(CommerceChannel commerceChannel) {
+		try {
+			_commercePaymentMethodGroupRelLocalService.
+				deleteCommercePaymentMethodGroupRels(
+					commerceChannel.getGroupId());
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException, portalException);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceChannelModelListener.class);
+
+	@Reference
+	private CommercePaymentMethodGroupRelLocalService
+		_commercePaymentMethodGroupRelLocalService;
+
+}
