3199581888796997d544ec65327031c8dc544bc0
Rafael Praxedes <rafael.praxedes@liferay.com>
2020-12-10 12:22:38 -0300

    LPS-124585 The Log's comment must be translated since we can store language key into it

--- modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowLogResourceImpl.java
+++ modules/apps/headless/headless-admin-workflow/headless-admin-workflow-impl/src/main/java/com/liferay/headless/admin/workflow/internal/resource/v1_0/WorkflowLogResourceImpl.java
@@ -19,9 +19,11 @@ import com.liferay.headless.admin.workflow.dto.v1_0.WorkflowLog;
 import com.liferay.headless.admin.workflow.internal.dto.v1_0.util.CreatorUtil;
 import com.liferay.headless.admin.workflow.internal.dto.v1_0.util.RoleUtil;
 import com.liferay.headless.admin.workflow.resource.v1_0.WorkflowLogResource;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.workflow.WorkflowLogManager;
 import com.liferay.portal.kernel.workflow.comparator.WorkflowComparatorFactoryUtil;
 import com.liferay.portal.vulcan.pagination.Page;
@@ -150,7 +152,11 @@ public class WorkflowLogResourceImpl extends BaseWorkflowLogResourceImpl {
 				auditPerson = CreatorUtil.toCreator(
 					_portal,
 					_userLocalService.fetchUser(workflowLog.getAuditUserId()));
-				commentLog = workflowLog.getComment();
+				commentLog = _language.get(
+					ResourceBundleUtil.getBundle(
+						"content.Language",
+						contextAcceptLanguage.getPreferredLocale(), getClass()),
+					workflowLog.getComment());
 				dateCreated = workflowLog.getCreateDate();
 				id = workflowLog.getWorkflowLogId();
 				person = CreatorUtil.toCreator(
@@ -193,6 +199,9 @@ public class WorkflowLogResourceImpl extends BaseWorkflowLogResourceImpl {
 
 	@Reference
 	private KaleoWorkflowModelConverter _kaleoWorkflowModelConverter;
+
+	@Reference
+	private Language _language;
 
 	@Reference
 	private Portal _portal;
