15c159cfee75315c03dcbe03085c568b387b2fc6
Alan Huang <alan.huang@liferay.com>
2020-12-15 16:21:33 +0800

    LPS-121804 Add PythonStylingCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PythonStylingCheck.java
@@ -0,0 +1,215 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.checks.util.PythonSourceUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Alan Huang
+ */
+public class PythonStylingCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		return _sortPythonStatements(fileName, content, StringPool.BLANK);
+	}
+
+	private List<String> _combineAnnotationsAndComments(
+		List<String> statements, String indent) {
+
+		List<String> statementsList = new ArrayList<>();
+
+		StringBundler sb = new StringBundler();
+
+		String previousStatement = StringPool.BLANK;
+
+		for (String statement : statements) {
+			if (statement.startsWith(indent + StringPool.AT) ||
+				statement.startsWith(indent + StringPool.POUND)) {
+
+				sb.append(statement);
+				sb.append("\n");
+			}
+			else if (previousStatement.startsWith(indent + StringPool.AT) ||
+					 previousStatement.startsWith(indent + StringPool.POUND)) {
+
+				sb.append(statement);
+
+				statementsList.add(sb.toString());
+
+				sb.setIndex(0);
+			}
+			else {
+				statementsList.add(statement);
+			}
+
+			previousStatement = statement;
+		}
+
+		if (sb.index() > 0) {
+			statementsList.add(StringUtil.trimTrailing(sb.toString()));
+		}
+
+		return statementsList;
+	}
+
+	private int _sortIdentifiers(String statement1, String statement2) {
+		String trimmedStatement1 = StringUtil.trimLeading(statement1);
+		String trimmedStatement2 = StringUtil.trimLeading(statement2);
+
+		trimmedStatement1 = trimmedStatement1.replaceAll(
+			"(\t*[#@].*(\\Z|\n))*(.*)", "$3");
+		trimmedStatement2 = trimmedStatement2.replaceAll(
+			"(\t*[#@].*(\\Z|\n))*(.*)", "$3");
+
+		if (Validator.isNull(trimmedStatement1) ||
+			Validator.isNull(trimmedStatement2)) {
+
+			return 0;
+		}
+
+		String[] trimmedStatement1Lines = trimmedStatement1.split("\n", 2);
+		String[] trimmedStatement2Lines = trimmedStatement2.split("\n", 2);
+
+		Matcher matcher = _identifierAndNamePattern.matcher(trimmedStatement1Lines[0]);
+
+		String identifierKey1 = null;
+		String name1 = null;
+		
+		if (matcher.find()) {
+			identifierKey1 = matcher.group(1);
+			name1 = matcher.group(2);
+		}
+		
+		matcher = _identifierAndNamePattern.matcher(trimmedStatement2Lines[0]);
+
+		String identifiersKey2 = null;
+		String name2 = null;
+		
+		if (matcher.find()) {
+			identifiersKey2 = matcher.group(1);
+			name2 = matcher.group(2);
+		}
+		
+		if (_identifiersKeyWeightMap.containsKey(identifierKey1) &&
+				_identifiersKeyWeightMap.containsKey(identifiersKey2)) {
+			
+			if (identifierKey1.equals(identifiersKey2)) {
+				return name1.compareTo(name2);
+			}
+			
+			int weight1 = _identifierKeyWeight(identifierKey1);
+			int weight2 = _identifierKeyWeight(identifiersKey2);
+
+			if ((weight1 != -1) || (weight2 != -1)) {
+				return weight1 - weight2;
+			}
+
+		}
+		
+		return 0;
+	}
+
+	private int _identifierKeyWeight(String definitionKey) {
+		if (_identifiersKeyWeightMap.containsKey(definitionKey)) {
+			return _identifiersKeyWeightMap.get(definitionKey);
+		}
+
+		return -1;
+	}
+
+	private String _sortPythonStatements(
+		String fileName, String content, String indent) {
+
+		List<String> statements = PythonSourceUtil.getPythonStatements(
+			content, indent);
+
+		statements = _combineAnnotationsAndComments(statements, indent);
+
+		List<String> oldStatements = new ArrayList<>(statements);
+
+		Collections.sort(
+			statements,
+			new Comparator<String>() {
+
+				@Override
+				public int compare(String statement1, String statement2) {
+					return _sortIdentifiers(statement1, statement2);
+				}
+
+			});
+
+		if (!oldStatements.equals(statements)) {
+			StringBundler sb = new StringBundler();
+
+			for (String statement : statements) {
+				sb.append(statement);
+				sb.append("\n");
+			}
+
+			sb.setIndex(sb.index() - 1);
+
+			String[] lines = content.split("\n");
+
+			if (!indent.equals("")) {
+				content = lines[0] + "\n" + sb.toString();
+			}
+			else {
+				content = sb.toString();
+			}
+		}
+
+		statements = PythonSourceUtil.getPythonStatements(content, indent);
+
+		for (String statement : statements) {
+			String nestedStatementIndent =
+				PythonSourceUtil.getNestedStatementIndent(statement);
+
+			if (!nestedStatementIndent.equals(StringPool.BLANK)) {
+				content = StringUtil.replaceFirst(
+					content, statement,
+					_sortPythonStatements(
+						fileName, statement, nestedStatementIndent));
+			}
+		}
+
+		return content;
+	}
+
+	private static final Map<String, Integer> _identifiersKeyWeightMap =
+		HashMapBuilder.put(
+			"class", 2
+		).put(
+			"def", 1
+		).build();
+	private static final Pattern _identifierAndNamePattern = Pattern.compile(
+			"(\\w+) (\\w+).*");
+
+}
---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/util/PythonSourceUtil.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks.util;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Alan Huang
+ */
+public class PythonSourceUtil {
+
+	public static String getNestedStatementIndent(String statement) {
+		String[] lines = StringUtil.splitLines(statement);
+
+		if (lines.length <= 1) {
+			return StringPool.BLANK;
+		}
+
+		for (int i = 1; i < lines.length; i++) {
+			String line = lines[i];
+
+			String indent = line.replaceFirst("^(\t+).+", "$1");
+
+			if (!indent.equals(line)) {
+				return indent;
+			}
+		}
+
+		return StringPool.BLANK;
+	}
+
+	public static List<String> getPythonStatements(
+		String content, String indent) {
+
+		List<String> statements = new ArrayList<>();
+
+		String[] lines = content.split("\n");
+
+		StringBundler sb = new StringBundler();
+
+		for (String line : lines) {
+			if (line.length() == 0) {
+				sb.append("\n");
+
+				continue;
+			}
+
+			if (!line.startsWith(indent)) {
+				continue;
+			}
+
+			String s = line.substring(indent.length(), indent.length() + 1);
+
+			if (!s.equals(StringPool.SPACE) && !s.equals(StringPool.TAB) &&
+				(sb.length() != 0)) {
+
+				sb.setIndex(sb.index() - 1);
+
+				statements.add(sb.toString());
+
+				sb.setIndex(0);
+			}
+
+			sb.append(line);
+			sb.append("\n");
+		}
+
+		statements.add(sb.toString());
+
+		return statements;
+	}
+
+}
