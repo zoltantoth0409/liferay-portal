893c051e4a78687dbf4d437d6ee76c098d212751
Carolina <carolina.barbosa@liferay.com>
2020-12-17 10:49:41 -0300

    LPS-124399 Include new methods to be used in the react taglib

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/DDMFormRenderer.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/DDMFormRenderer.java
@@ -17,10 +17,22 @@ package com.liferay.dynamic.data.mapping.form.renderer;
 import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormLayout;
 
+import java.util.Map;
+
+import org.osgi.annotation.versioning.ProviderType;
+
 /**
  * @author Marcellus Tavares
  */
+@ProviderType
 public interface DDMFormRenderer {
+
+	public String getModuleName();
+
+	public Map<String, Object> getReactData(
+			DDMForm ddmForm, DDMFormLayout ddmFormLayout,
+			DDMFormRenderingContext ddmFormRenderingContext)
+		throws Exception;
 
 	public String render(
 			DDMForm ddmForm, DDMFormLayout ddmFormLayout,
