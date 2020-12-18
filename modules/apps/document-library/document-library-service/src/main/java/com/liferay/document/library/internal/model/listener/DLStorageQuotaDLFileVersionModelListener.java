2442e34178ec4019c0423e4bc0138c5cc5b585e3
Adolfo Pérez <adolfo.perez@liferay.com>
2020-12-18 14:23:29 +0100

    LPS-125112 Keep size up to date when updating/removing versions

---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/model/listener/DLStorageQuotaDLFileVersionModelListener.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.model.listener;
+
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.service.DLStorageQuotaLocalService;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(service = ModelListener.class)
+public class DLStorageQuotaDLFileVersionModelListener
+	extends BaseModelListener<DLFileVersion> {
+
+	@Override
+	public void onAfterCreate(DLFileVersion dlFileVersion)
+		throws ModelListenerException {
+
+		super.onAfterCreate(dlFileVersion);
+
+		_dlStorageQuotaLocalService.incrementStorageSize(
+			dlFileVersion.getCompanyId(), dlFileVersion.getSize());
+	}
+
+	@Override
+	public void onAfterRemove(DLFileVersion dlFileVersion)
+		throws ModelListenerException {
+
+		super.onAfterRemove(dlFileVersion);
+
+		_dlStorageQuotaLocalService.incrementStorageSize(
+			dlFileVersion.getCompanyId(), -dlFileVersion.getSize());
+	}
+
+	@Reference
+	private DLStorageQuotaLocalService _dlStorageQuotaLocalService;
+
+}
