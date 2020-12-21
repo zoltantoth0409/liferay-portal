b5b1e71c92f124d94ec3acdb170efc5f9df85255
tina.tian <tina.tian@liferay.com>
2020-12-21 14:54:32 -0800

    LPS-124811 Add unit test

--- modules/apps/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/internal/FreeMarkerTemplateTest.java
+++ modules/apps/portal-template/portal-template-freemarker/src/test/java/com/liferay/portal/template/freemarker/internal/FreeMarkerTemplateTest.java
@@ -33,6 +33,7 @@ import freemarker.core.ParseException;
 import freemarker.ext.beans.BeansWrapper;
 
 import freemarker.template.Configuration;
+import freemarker.template.SimpleNumber;
 
 import java.io.IOException;
 import java.io.ObjectInput;
@@ -40,7 +41,10 @@ import java.io.ObjectOutput;
 import java.io.Reader;
 import java.io.StringReader;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -88,6 +92,8 @@ public class FreeMarkerTemplateTest {
 	public void setUp() throws Exception {
 		_configuration = new Configuration();
 
+		_configuration.setLogTemplateExceptions(false);
+
 		TemplateCache templateCache = new LiferayTemplateCache(
 			_configuration, _freeMarkerTemplateResourceLoader, null);
 
@@ -118,6 +124,65 @@ public class FreeMarkerTemplateTest {
 		String stringResult = (String)result;
 
 		Assert.assertEquals(_TEST_VALUE, stringResult);
+	}
+
+	@Test
+	public void testLoopCountThreshold() throws Exception {
+		_testLoopCountThreshold(
+			"<#list 1..3 as x>${x}</#list>", Collections.emptyMap(), "123");
+		_testLoopCountThreshold(
+			"<#list ['1', '2', '3'] as x>${x}</#list>", Collections.emptyMap(),
+			"123");
+		_testLoopCountThreshold(
+			"<#list 1..3><#items as x>${x}</#items></#list>",
+			Collections.emptyMap(), "123");
+		_testLoopCountThreshold(
+			"<#list 1..2 as i>${i}<#list 1..3 as j>${j}</#list></#list>",
+			Collections.emptyMap(), "11232123");
+		_testLoopCountThreshold(
+			"<#list {'1':1,'2':2,'3':3} as x, y>${x},${y};</#list>",
+			Collections.emptyMap(), "1,1;2,2;3,3;");
+
+		List<String> testList = new ArrayList<>();
+
+		testList.add("1");
+		testList.add("2");
+		testList.add("3");
+
+		_testLoopCountThreshold(
+			"<#list testList as x>${x}</#list>",
+			Collections.singletonMap("testList", testList), "123");
+		_testLoopCountThreshold(
+			"<#list testList><#items as x>${x}</#items></#list>",
+			Collections.singletonMap("testList", testList), "123");
+
+		Set<String> testSet = new HashSet<>();
+
+		testSet.add("1");
+		testSet.add("2");
+		testSet.add("3");
+
+		_testLoopCountThreshold(
+			"<#list testSet as x>${x}</#list>",
+			Collections.singletonMap("testSet", testSet), "123");
+		_testLoopCountThreshold(
+			"<#list testSet><#items as x>${x}</#items></#list>",
+			Collections.singletonMap("testSet", testSet), "123");
+
+		_testLoopCountThreshold(
+			"<#list testMap as x, y>${x},${y};</#list>",
+			Collections.singletonMap(
+				"testMap",
+				HashMapBuilder.put(
+					"1", 1
+				).put(
+					"2", 2
+				).put(
+					"3", 3
+				).build()),
+			"1,1;2,2;3,3;");
+
+		_configuration.setSharedVariable("loop-size-threshold", null);
 	}
 
 	@Test
@@ -316,6 +381,45 @@ public class FreeMarkerTemplateTest {
 		String result = unsyncStringWriter.toString();
 
 		Assert.assertEquals(_TEST_VALUE, result);
+	}
+
+	private void _testLoopCountThreshold(
+			String templateContent, Map<String, Object> context,
+			String expectedResult)
+		throws Exception {
+
+		Template template = new FreeMarkerTemplate(
+			new StringTemplateResource(_TEMPLATE_FILE_NAME, templateContent),
+			context, _configuration, _templateContextHelper,
+			_templateResourceCache, false,
+			(BeansWrapper)_configuration.getObjectWrapper(),
+			_freeMarkerManager);
+
+		_configuration.setSharedVariable(
+			"loop-count-threshold", new SimpleNumber(2));
+
+		UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
+
+		try {
+			template.processTemplate(unsyncStringWriter);
+
+			Assert.fail(
+				"Template processing should fail by exceeding threshold");
+		}
+		catch (Exception exception) {
+			String result = unsyncStringWriter.toString();
+
+			Assert.assertTrue(
+				result, result.contains("Loop count exceeds threshold 2"));
+		}
+
+		_configuration.setSharedVariable("loop-count-threshold", null);
+
+		unsyncStringWriter = new UnsyncStringWriter();
+
+		template.processTemplate(unsyncStringWriter);
+
+		Assert.assertEquals(expectedResult, unsyncStringWriter.toString());
 	}
 
 	private static final String _TEMPLATE_FILE_NAME = "test.ftl";
