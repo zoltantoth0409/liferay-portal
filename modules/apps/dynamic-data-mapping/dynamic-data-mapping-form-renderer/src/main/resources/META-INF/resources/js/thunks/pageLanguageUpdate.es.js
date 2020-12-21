8adfc2c13aef7842463309eb844007d7e7e19a51
Marcos <marcos.martins@liferay.com>
2020-12-21 16:17:35 -0300

    LPS-124995 rename

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/thunks/pageLanguageUpdate.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/thunks/pageLanguageUpdate.es.js
@@ -147,7 +147,7 @@ const getDataRecordValues = ({
 	return newDataRecordValues;
 };
 
-const getLocalizedValueEdited = (fieldName, pages) => {
+const getFieldProperties = (fieldName, pages) => {
 	const visitor = new PagesVisitor(pages);
 
 	const {localizedValueEdited} = visitor.findField(
@@ -241,7 +241,7 @@ export default function pageLanguageUpdate({
 
 						return {
 							...field,
-							...getLocalizedValueEdited(field.fieldName, pages),
+							...getFieldProperties(field.fieldName, pages),
 						};
 					},
 					true,
