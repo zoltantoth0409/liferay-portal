d7f44ea81485a162f27b6206dc93200de0dfff43
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-12-23 14:50:58 +0100

    LPS-104697 Implement endpoint to get the rendered pages

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/ContentPageResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/ContentPageResourceImpl.java
@@ -17,29 +17,46 @@ package com.liferay.headless.delivery.internal.resource.v1_0;
 import com.liferay.headless.delivery.dto.v1_0.ContentPage;
 import com.liferay.headless.delivery.internal.dto.v1_0.converter.ContentPageDTOConverter;
 import com.liferay.headless.delivery.resource.v1_0.ContentPageResource;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.events.ServicePreAction;
+import com.liferay.portal.events.ThemeServicePreAction;
 import com.liferay.portal.kernel.exception.NoSuchModelException;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.LayoutFriendlyURL;
+import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.service.LayoutFriendlyURLLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.LayoutService;
+import com.liferay.portal.kernel.servlet.DummyHttpServletResponse;
+import com.liferay.portal.kernel.servlet.DynamicServletRequest;
+import com.liferay.portal.kernel.servlet.ServletContextPool;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.vulcan.aggregation.Aggregation;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 import com.liferay.portal.vulcan.util.TransformUtil;
+import com.liferay.taglib.util.ThemeUtil;
 
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
 import javax.validation.constraints.NotNull;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -66,6 +83,14 @@ public class ContentPageResourceImpl extends BaseContentPageResourceImpl {
 	}
 
 	@Override
+	public String getSiteContentPagePrivateFriendlyUrlPathRenderedPage(
+			@NotNull Long siteId, @NotNull String friendlyUrlPath)
+		throws Exception {
+
+		return _getRenderedLayout(friendlyUrlPath, true, siteId);
+	}
+
+	@Override
 	public ContentPage getSiteContentPagePublicFriendlyUrlPath(
 			@NotNull Long siteId, @NotNull String friendlyUrlPath)
 		throws Exception {
@@ -73,6 +98,14 @@ public class ContentPageResourceImpl extends BaseContentPageResourceImpl {
 		return _toContentPage(
 			_getLayout(siteId, false, friendlyUrlPath),
 			"getSiteContentPagePublicFriendlyUrlPath");
+	}
+
+	@Override
+	public String getSiteContentPagePublicFriendlyUrlPathRenderedPage(
+			@NotNull Long siteId, @NotNull String friendlyUrlPath)
+		throws Exception {
+
+		return _getRenderedLayout(friendlyUrlPath, false, siteId);
 	}
 
 	@Override
@@ -112,7 +145,7 @@ public class ContentPageResourceImpl extends BaseContentPageResourceImpl {
 
 				sb.append("No ");
 
-				if(privateLayout) {
+				if (privateLayout) {
 					sb.append("private ");
 				}
 				else {
@@ -127,6 +160,73 @@ public class ContentPageResourceImpl extends BaseContentPageResourceImpl {
 				return new NoSuchModelException(sb.toString());
 			}
 		);
+	}
+
+	private String _getRenderedLayout(
+			String friendlyUrlPath, boolean privateLayout, Long groupId)
+		throws Exception {
+
+		Layout layout = _getLayout(groupId, privateLayout, friendlyUrlPath);
+
+		contextHttpServletRequest = DynamicServletRequest.addQueryString(
+			contextHttpServletRequest, "p_l_id=" + layout.getPlid(), false);
+
+		contextHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay(layout));
+
+		layout.includeLayoutContent(
+			contextHttpServletRequest, contextHttpServletResponse);
+
+		StringBundler sb =
+			(StringBundler)contextHttpServletRequest.getAttribute(
+				WebKeys.LAYOUT_CONTENT);
+
+		LayoutSet layoutSet = layout.getLayoutSet();
+
+		ServletContext servletContext = ServletContextPool.get(
+			StringPool.BLANK);
+
+		if (contextHttpServletRequest.getAttribute(WebKeys.CTX) == null) {
+			contextHttpServletRequest.setAttribute(WebKeys.CTX, servletContext);
+		}
+
+		Document document = Jsoup.parse(
+			ThemeUtil.include(
+				servletContext, contextHttpServletRequest,
+				contextHttpServletResponse, "portal_normal.ftl",
+				layoutSet.getTheme(), false));
+
+		Element bodyElement = document.body();
+
+		bodyElement.html(sb.toString());
+
+		return document.html();
+	}
+
+	private ThemeDisplay _getThemeDisplay(Layout layout) throws Exception {
+		ServicePreAction servicePreAction = new ServicePreAction();
+
+		HttpServletResponse httpServletResponse =
+			new DummyHttpServletResponse();
+
+		servicePreAction.servicePre(
+			contextHttpServletRequest, httpServletResponse, false);
+
+		ThemeServicePreAction themeServicePreAction =
+			new ThemeServicePreAction();
+
+		themeServicePreAction.run(
+			contextHttpServletRequest, httpServletResponse);
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)contextHttpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		themeDisplay.setLayout(layout);
+		themeDisplay.setScopeGroupId(layout.getGroupId());
+		themeDisplay.setSiteGroupId(layout.getGroupId());
+
+		return themeDisplay;
 	}
 
 	private ContentPage _toContentPage(Layout layout, String methodName)
