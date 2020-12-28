fb4c0bce26366cad30d52a8526d9f682e3eb12b2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-12-28 11:22:44 +0000

    LPS-125091 Implements necessary classes to upload file entries to the user personal folder, copy from DLUploadResponseHandler and DLUploadFileEntryHandler to keep the same behaviour, no logic changes

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/DDMUserPersonalFolderUploadFileEntryHandler.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.upload;
+
+import com.liferay.document.library.kernel.exception.NoSuchFileEntryException;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.service.DLAppService;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.document.library.kernel.util.DLValidator;
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.upload.UniqueFileNameProvider;
+import com.liferay.upload.UploadFileEntryHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	service = DDMUserPersonalFolderUploadFileEntryHandler.class
+)
+public class DDMUserPersonalFolderUploadFileEntryHandler
+	implements UploadFileEntryHandler {
+
+	@Override
+	public FileEntry upload(UploadPortletRequest uploadPortletRequest)
+		throws IOException, PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)uploadPortletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		long folderId = ParamUtil.getLong(uploadPortletRequest, "folderId");
+
+		ModelResourcePermissionUtil.check(
+			_folderModelResourcePermission, themeDisplay.getPermissionChecker(),
+			themeDisplay.getScopeGroupId(), folderId, ActionKeys.ADD_DOCUMENT);
+
+		String fileName = uploadPortletRequest.getFileName(_PARAMETER_NAME);
+		long size = uploadPortletRequest.getSize(_PARAMETER_NAME);
+
+		_dlValidator.validateFileSize(fileName, size);
+
+		try (InputStream inputStream = uploadPortletRequest.getFileAsStream(
+				_PARAMETER_NAME)) {
+
+			String uniqueFileName = _uniqueFileNameProvider.provide(
+				fileName,
+				curFileName -> _exists(
+					themeDisplay.getScopeGroupId(), folderId, curFileName));
+
+			return _dlAppService.addFileEntry(
+				themeDisplay.getScopeGroupId(), folderId, uniqueFileName,
+				uploadPortletRequest.getContentType(_PARAMETER_NAME),
+				uniqueFileName, _getDescription(uploadPortletRequest),
+				StringPool.BLANK, inputStream, size,
+				_getServiceContext(uploadPortletRequest));
+		}
+	}
+
+	private boolean _exists(long groupId, long folderId, String fileName) {
+		try {
+			if (_dlAppService.getFileEntry(groupId, folderId, fileName) !=
+					null) {
+
+				return true;
+			}
+
+			return false;
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException, portalException);
+			}
+
+			return false;
+		}
+	}
+
+	private FileEntry _fetchFileEntry(UploadPortletRequest uploadPortletRequest)
+		throws PortalException {
+
+		try {
+			long fileEntryId = GetterUtil.getLong(
+				uploadPortletRequest.getParameter("fileEntryId"));
+
+			if (fileEntryId == 0) {
+				return null;
+			}
+
+			return _dlAppService.getFileEntry(fileEntryId);
+		}
+		catch (NoSuchFileEntryException noSuchFileEntryException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(noSuchFileEntryException, noSuchFileEntryException);
+			}
+
+			return null;
+		}
+	}
+
+	private String _getDescription(UploadPortletRequest uploadPortletRequest)
+		throws PortalException {
+
+		FileEntry fileEntry = _fetchFileEntry(uploadPortletRequest);
+
+		if (fileEntry == null) {
+			return StringPool.BLANK;
+		}
+
+		return fileEntry.getDescription();
+	}
+
+	private ServiceContext _getServiceContext(
+			UploadPortletRequest uploadPortletRequest)
+		throws PortalException {
+
+		FileEntry fileEntry = _fetchFileEntry(uploadPortletRequest);
+
+		if ((fileEntry == null) ||
+			!(fileEntry.getModel() instanceof DLFileEntry)) {
+
+			return ServiceContextFactory.getInstance(
+				DLFileEntry.class.getName(), uploadPortletRequest);
+		}
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			DLFileEntry.class.getName(), uploadPortletRequest);
+
+		ExpandoBridge expandoBridge = fileEntry.getExpandoBridge();
+
+		serviceContext.setExpandoBridgeAttributes(
+			expandoBridge.getAttributes());
+
+		return serviceContext;
+	}
+
+	private static final String _PARAMETER_NAME = "imageSelectorFileName";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMUserPersonalFolderUploadFileEntryHandler.class);
+
+	@Reference
+	private DLAppService _dlAppService;
+
+	@Reference
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Reference
+	private DLValidator _dlValidator;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.portal.kernel.repository.model.Folder)"
+	)
+	private ModelResourcePermission<Folder> _folderModelResourcePermission;
+
+	@Reference
+	private UniqueFileNameProvider _uniqueFileNameProvider;
+
+}
---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/DDMUserPersonalFolderUploadResponseHandler.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.web.internal.upload;
+
+import com.liferay.document.library.util.DLURLHelper;
+import com.liferay.item.selector.ItemSelectorUploadResponseHandler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.upload.UploadResponseHandler;
+
+import javax.portlet.PortletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(service = DDMUserPersonalFolderUploadResponseHandler.class)
+public class DDMUserPersonalFolderUploadResponseHandler
+	implements UploadResponseHandler {
+
+	@Override
+	public JSONObject onFailure(
+			PortletRequest portletRequest, PortalException portalException)
+		throws PortalException {
+
+		return _itemSelectorUploadResponseHandler.onFailure(
+			portletRequest, portalException);
+	}
+
+	@Override
+	public JSONObject onSuccess(
+			UploadPortletRequest uploadPortletRequest, FileEntry fileEntry)
+		throws PortalException {
+
+		JSONObject jsonObject = _itemSelectorUploadResponseHandler.onSuccess(
+			uploadPortletRequest, fileEntry);
+
+		JSONObject fileJSONObject = jsonObject.getJSONObject("file");
+
+		fileJSONObject.put("url", _getURL(uploadPortletRequest, fileEntry));
+
+		return jsonObject;
+	}
+
+	private String _getURL(
+		UploadPortletRequest uploadPortletRequest, FileEntry fileEntry) {
+
+		try {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)uploadPortletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			return _dlURLHelper.getPreviewURL(
+				fileEntry, fileEntry.getLatestFileVersion(), themeDisplay,
+				StringPool.BLANK);
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"Unable to get URL for file entry " +
+						fileEntry.getFileEntryId(),
+					portalException);
+			}
+		}
+
+		return StringPool.BLANK;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DDMUserPersonalFolderUploadResponseHandler.class);
+
+	@Reference
+	private DLURLHelper _dlURLHelper;
+
+	@Reference
+	private ItemSelectorUploadResponseHandler
+		_itemSelectorUploadResponseHandler;
+
+}
