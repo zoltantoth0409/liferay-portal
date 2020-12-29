02f3e8795c14a9bd7fe3703a04c68efcaf2d5fa7
Marcos <marcos.martins@liferay.com>
2020-12-29 12:58:21 -0300

    LPS-125373 add default site language id into template context

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormTemplateContextFactoryImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormTemplateContextFactoryImpl.java
@@ -34,6 +34,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
@@ -152,6 +153,9 @@ public class DDMFormTemplateContextFactoryImpl
 		templateContext.put(
 			"defaultLanguageId",
 			LanguageUtil.getLanguageId(ddmForm.getDefaultLocale()));
+		templateContext.put(
+			"defaultSiteLanguageId",
+			LanguageUtil.getLanguageId(LocaleUtil.getSiteDefault()));
 		templateContext.put(
 			"editingLanguageId", LanguageUtil.getLanguageId(locale));
 		templateContext.put(
