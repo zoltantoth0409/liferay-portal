1a1601b77e322709a1acd8c2f623e059b77ef7bb
Chema Balsas <jose.balsas@liferay.com>
2020-12-30 18:07:46 +0000

    LPS-125169 Avoids unnecessary servlet request to retrieve AUI bundle configuration
    This commit removes the unnecessary indirection of /o/bundle_config inlining the configuration
    directly in the response.
    
    Doing this, we speed up initialization avoiding unnecessary network requests and make it easier to enable/disable
    AUI support from a single location

---
+++ modules/apps/frontend-js/frontend-js-bundle-config-extender/src/main/java/com/liferay/frontend/js/bundle/config/extender/internal/servlet/taglib/JSBundleConfigTopHeadDynamicInclude.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.bundle.config.extender.internal.servlet.taglib;
+
+import com.liferay.frontend.js.bundle.config.extender.internal.JSBundleConfigTracker;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import java.net.URL;
+
+import java.util.Collection;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Chema Balsas
+ */
+@Component(
+	immediate = true, property = "service.ranking:Integer=" + Integer.MIN_VALUE,
+	service = DynamicInclude.class
+)
+public class JSBundleConfigTopHeadDynamicInclude extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		Collection<JSBundleConfigTracker.JSConfig> jsConfigs =
+			_jsBundleConfigTracker.getJSConfigs();
+
+		if (!jsConfigs.isEmpty()) {
+			PrintWriter printWriter = httpServletResponse.getWriter();
+
+			printWriter.println("<script data-senna-track=\"temporary\" ");
+			printWriter.print("type=\"");
+			printWriter.print(ContentTypes.TEXT_JAVASCRIPT);
+			printWriter.print("\">");
+
+			for (JSBundleConfigTracker.JSConfig jsConfig : jsConfigs) {
+				URL url = jsConfig.getURL();
+
+				try (InputStream inputStream = url.openStream()) {
+					printWriter.println("try{");
+
+					ServletContext servletContext =
+						jsConfig.getServletContext();
+
+					printWriter.print(
+						StringBundler.concat(
+							"var MODULE_PATH='", _portal.getPathProxy(),
+							servletContext.getContextPath(), "';"));
+
+					printWriter.print(
+						StringUtil.removeSubstring(
+							StringUtil.read(inputStream),
+							"//# sourceMappingURL=config.js.map"));
+
+					printWriter.print("}catch(error){console.error(error);}");
+				}
+				catch (Exception exception) {
+					_log.error("Unable to open resource", exception);
+				}
+			}
+
+			printWriter.print("</script>");
+		}
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"/html/common/themes/top_js.jspf#resources");
+	}
+
+	protected JSBundleConfigTracker getJSBundleConfigTracker() {
+		return _jsBundleConfigTracker;
+	}
+
+	@Reference(unbind = "-")
+	protected void setJSBundleConfigTracker(
+		JSBundleConfigTracker jsBundleConfigTracker) {
+
+		_jsBundleConfigTracker = jsBundleConfigTracker;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		JSBundleConfigTopHeadDynamicInclude.class);
+
+	private JSBundleConfigTracker _jsBundleConfigTracker;
+
+	@Reference
+	private Portal _portal;
+
+}
