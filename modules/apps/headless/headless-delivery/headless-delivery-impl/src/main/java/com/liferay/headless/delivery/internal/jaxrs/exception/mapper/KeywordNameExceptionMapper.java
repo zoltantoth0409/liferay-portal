448fed6a785bfe35dad6c34ad3215359c59edf02
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-01-05 15:19:15 +0100

    LPS-125480 We have already an exception mapper for tags, but in a different application. So copy it over and adapt it for our application

---
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/jaxrs/exception/mapper/KeywordNameExceptionMapper.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.delivery.internal.jaxrs.exception.mapper;
+
+import com.liferay.asset.kernel.exception.AssetTagException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * Converts any {@code AssetTagException} to a {@code 400} error.
+ *
+ * @author Víctor Galán
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Delivery)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Delivery.KeywordNameExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class KeywordNameExceptionMapper
+	extends BaseExceptionMapper<AssetTagException> {
+
+	@Override
+	protected Problem getProblem(AssetTagException assetTagException) {
+		return new Problem(
+			Response.Status.BAD_REQUEST,
+			"Keyword name is too long or contains invalid characters: " +
+				assetTagException.getMessage());
+	}
+
+}
