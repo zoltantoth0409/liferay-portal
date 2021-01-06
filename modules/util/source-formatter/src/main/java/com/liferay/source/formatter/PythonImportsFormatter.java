8ba75a246cb064f49a50ff28cbcc5958f00d753e
Alan Huang <alan.huang@liferay.com>
2021-01-06 22:10:02 +0800

    LPS-121804 Add logic

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/PythonImportsFormatter.java
@@ -0,0 +1,169 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.tools.BaseImportsFormatter;
+import com.liferay.portal.tools.ImportPackage;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Alan Huang
+ */
+public class PythonImportsFormatter extends BaseImportsFormatter {
+
+	protected static List<String> getImportsList(
+		String content, Pattern importPattern) {
+
+		List<String> importsList = new ArrayList<>();
+
+		Matcher matcher = importPattern.matcher(content);
+
+		while (matcher.find()) {
+			importsList.add(matcher.group());
+		}
+
+		return importsList;
+	}
+
+	@Override
+	protected ImportPackage createImportPackage(String line) {
+		return _createPythonImportPackage(line);
+	}
+
+	@Override
+	protected String doFormat(
+			String content, Pattern importPattern, String packageDir,
+			String className)
+		throws IOException {
+
+		List<String> importsList = getImportsList(content, importPattern);
+
+		for (String imports : importsList) {
+			content = doFormat(content, packageDir, className, imports);
+		}
+
+		return content;
+	}
+
+	protected String doFormat(
+			String content, String packageDir, String className, String imports)
+		throws IOException {
+
+		if (Validator.isNull(imports)) {
+			return content;
+		}
+
+		String newImports = _mergeImports(imports);
+
+		newImports = sortAndGroupImports(newImports);
+
+		newImports = _splitImports(newImports);
+
+		if (!imports.equals(newImports)) {
+			content = StringUtil.replaceFirst(content, imports, newImports);
+		}
+
+		return content;
+	}
+
+	private ImportPackage _createPythonImportPackage(String line) {
+		Matcher matcher = _importPattern.matcher(line);
+
+		if (!matcher.find()) {
+			return null;
+		}
+
+		String importString = matcher.group(1);
+
+		return new ImportPackage(importString, false, line);
+	}
+
+	private String _mergeImports(String imports) {
+		imports = imports.replaceAll("\\\\\n", StringPool.BLANK);
+
+		Map<String, List<String>> packageImportsMap = new TreeMap<>();
+
+		Matcher matcher = _importPattern.matcher(imports);
+
+		List<String> importNamesList = new ArrayList<>();
+
+		while (matcher.find()) {
+			String importNames = matcher.group(2);
+			String packageName = matcher.group(1);
+
+			for (String importName : importNames.split(StringPool.COMMA)) {
+				importNamesList.add(importName.trim());
+			}
+
+			if (packageImportsMap.containsKey(packageName)) {
+				importNamesList.addAll(packageImportsMap.get(packageName));
+			}
+
+			packageImportsMap.put(packageName, importNamesList);
+
+			importNamesList = new ArrayList<>();
+		}
+
+		if (MapUtil.isEmpty(packageImportsMap)) {
+			return imports;
+		}
+
+		StringBundler sb = new StringBundler();
+
+		for (Map.Entry<String, List<String>> entry :
+				packageImportsMap.entrySet()) {
+
+			sb.append("from ");
+			sb.append(entry.getKey());
+			sb.append(" import ");
+
+			importNamesList = entry.getValue();
+
+			Collections.sort(importNamesList);
+
+			sb.append(
+				ListUtil.toString(
+					importNamesList, StringPool.BLANK,
+					StringPool.COMMA_AND_SPACE));
+
+			sb.append(StringPool.NEW_LINE);
+		}
+
+		return sb.toString();
+	}
+
+	private String _splitImports(String imports) {
+		return imports.replaceAll(", ", ", \\\\\n    ");
+	}
+
+	private static final Pattern _importPattern = Pattern.compile(
+		"from (.*) import (.*)");
+
+}
---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PythonImportsCheck.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.checks;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.tools.ImportsFormatter;
+import com.liferay.source.formatter.PythonImportsFormatter;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Alan Huang
+ */
+public class PythonImportsCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws IOException {
+
+		content = _formatModuleImports(content);
+
+		content = _formatPackageImports(content);
+
+		if (content.endsWith(StringPool.NEW_LINE)) {
+			content = content.substring(0, content.length() - 1);
+		}
+
+		return content;
+	}
+
+	private String _formatModuleImports(String content) {
+		Matcher matcher = _importsPattern1.matcher(content);
+
+		List<String> importNames = new ArrayList<>();
+
+		int startPosition = -1;
+
+		while (matcher.find()) {
+			importNames.add(matcher.group(1));
+
+			if (startPosition == -1) {
+				startPosition = matcher.start();
+			}
+
+			content = content.replaceFirst(matcher.group(), StringPool.BLANK);
+		}
+
+		if (ListUtil.isEmpty(importNames)) {
+			return content;
+		}
+
+		Collections.sort(importNames);
+
+		StringBundler sb = new StringBundler((importNames.size() * 3) + 1);
+
+		sb.append(StringPool.NEW_LINE);
+
+		for (String importName : importNames) {
+			sb.append(StringPool.NEW_LINE);
+			sb.append("import ");
+			sb.append(importName);
+		}
+
+		return StringUtil.insert(content, sb.toString(), startPosition);
+	}
+
+	private String _formatPackageImports(String content) throws IOException {
+		ImportsFormatter importsFormatter = new PythonImportsFormatter();
+
+		return importsFormatter.format(content, _importsPattern2);
+	}
+
+	private static final Pattern _importsPattern1 = Pattern.compile(
+		"\n+[ \t]*import (.*)");
+	private static final Pattern _importsPattern2 = Pattern.compile(
+		"(^[ \t]*from (.*) import (.+(\\\\\n[\\s\\S]+?)*)(?<!\\\\)(\n|\\Z))+",
+		Pattern.MULTILINE);
+
+}
