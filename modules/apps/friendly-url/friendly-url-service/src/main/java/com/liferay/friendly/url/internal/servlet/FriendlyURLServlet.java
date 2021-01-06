d45cc9b487c920a78bad84a9a6664ca84f45bed6
Brian Chan <brian.chan@liferay.com>
2021-01-06 06:48:55 -0300

    LPS-125475 inline

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
@@ -164,16 +164,14 @@ public class FriendlyURLServlet extends HttpServlet {
 			ServiceContextThreadLocal.pushServiceContext(serviceContext);
 		}
 
-		Map<String, String[]> params = httpServletRequest.getParameterMap();
-
 		Layout defaultLayout = null;
 
 		try {
 			LayoutFriendlyURLSeparatorComposite
 				layoutFriendlyURLSeparatorComposite =
 					portal.getLayoutFriendlyURLSeparatorComposite(
-						group.getGroupId(), _private, layoutFriendlyURL, params,
-						requestContext);
+						group.getGroupId(), _private, layoutFriendlyURL,
+						httpServletRequest.getParameterMap(), requestContext);
 
 			Layout layout = layoutFriendlyURLSeparatorComposite.getLayout();
 
@@ -310,7 +308,7 @@ public class FriendlyURLServlet extends HttpServlet {
 
 		String actualURL = portal.getActualURL(
 			group.getGroupId(), _private, Portal.PATH_MAIN, layoutFriendlyURL,
-			params, requestContext);
+			httpServletRequest.getParameterMap(), requestContext);
 		String portalURL = portal.getPortalURL(httpServletRequest);
 
 		if (actualURL.startsWith(portalURL)) {
