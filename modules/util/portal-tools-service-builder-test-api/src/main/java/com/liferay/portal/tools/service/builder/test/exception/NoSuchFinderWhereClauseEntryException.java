3fe405a63f341dd499c55bd2e9201213c42f0b71
shuyangzhou <shuyang.zhou@liferay.com>
2021-01-06 11:05:05 -0800

    LPS-62084 SF

--- modules/util/portal-tools-service-builder-test-api/src/main/java/com/liferay/portal/tools/service/builder/test/exception/NoSuchFinderWhereClauseEntryException.java
+++ modules/util/portal-tools-service-builder-test-api/src/main/java/com/liferay/portal/tools/service/builder/test/exception/NoSuchFinderWhereClauseEntryException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.portal.tools.service.builder.test.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -18,7 +19,8 @@ import com.liferay.portal.kernel.exception.NoSuchModelException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class NoSuchFinderWhereClauseEntryException extends NoSuchModelException {
+public class NoSuchFinderWhereClauseEntryException
+	extends NoSuchModelException {
 
 	public NoSuchFinderWhereClauseEntryException() {
 	}
@@ -27,7 +29,9 @@ public class NoSuchFinderWhereClauseEntryException extends NoSuchModelException 
 		super(msg);
 	}
 
-	public NoSuchFinderWhereClauseEntryException(String msg, Throwable throwable) {
+	public NoSuchFinderWhereClauseEntryException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
