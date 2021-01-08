20781261dc5a4611b7514195f0bc809ddc44ef60
Minhchau <minhchau.dang@liferay.com>
2021-01-07 20:24:26 -0800

    LPS-125516 Check against the same layout that is used in LayoutEditorToolbarControlMenuJSPDynamicInclude that shows the edit option

--- modules/apps/layout/layout-type-controller/layout-type-controller-content/src/main/java/com/liferay/layout/type/controller/content/internal/layout/type/controller/ContentLayoutTypeController.java
+++ modules/apps/layout/layout-type-controller/layout-type-controller-content/src/main/java/com/liferay/layout/type/controller/content/internal/layout/type/controller/ContentLayoutTypeController.java
@@ -90,6 +90,8 @@ public class ContentLayoutTypeController extends BaseLayoutTypeControllerImpl {
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
 
+		Boolean hasUpdatePermissions = null;
+
 		if (layout.getClassNameId() == _portal.getClassNameId(Layout.class)) {
 			Layout curLayout = _layoutLocalService.fetchLayout(
 				layout.getClassPK());
@@ -98,9 +100,10 @@ public class ContentLayoutTypeController extends BaseLayoutTypeControllerImpl {
 				curLayout = layout;
 			}
 
-			if (!_hasUpdatePermissions(
-					themeDisplay.getPermissionChecker(), curLayout)) {
+			hasUpdatePermissions = _hasUpdatePermissions(
+				themeDisplay.getPermissionChecker(), curLayout);
 
+			if (!hasUpdatePermissions) {
 				throw new PrincipalException.MustHavePermission(
 					themeDisplay.getPermissionChecker(), Layout.class.getName(),
 					layout.getLayoutId(), ActionKeys.UPDATE);
@@ -110,11 +113,15 @@ public class ContentLayoutTypeController extends BaseLayoutTypeControllerImpl {
 		String layoutMode = ParamUtil.getString(
 			httpServletRequest, "p_l_mode", Constants.VIEW);
 
-		if (layoutMode.equals(Constants.EDIT) &&
-			!_hasUpdatePermissions(
-				themeDisplay.getPermissionChecker(), layout)) {
+		if (layoutMode.equals(Constants.EDIT)) {
+			if (hasUpdatePermissions == null) {
+				hasUpdatePermissions = _hasUpdatePermissions(
+					themeDisplay.getPermissionChecker(), layout);
+			}
 
-			layoutMode = Constants.VIEW;
+			if (!hasUpdatePermissions) {
+				layoutMode = Constants.VIEW;
+			}
 		}
 
 		httpServletRequest.setAttribute(
