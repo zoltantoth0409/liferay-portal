fdee809522973e54705d8f942fd72ca0ffaf022e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-01-11 12:44:33 +0100

    LPS-125650 Adds constants for journal article form field types

---
+++ modules/apps/journal/journal-article-dynamic-data-mapping-form-field-type-api/src/main/java/com/liferay/journal/article/dynamic/data/mapping/form/field/type/constants/JournalArticleDDMFormFieldTypeConstants.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.article.dynamic.data.mapping.form.field.type.constants;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class JournalArticleDDMFormFieldTypeConstants {
+
+	public static final String JOURNAL_ARTICLE = "journal_article";
+
+}
