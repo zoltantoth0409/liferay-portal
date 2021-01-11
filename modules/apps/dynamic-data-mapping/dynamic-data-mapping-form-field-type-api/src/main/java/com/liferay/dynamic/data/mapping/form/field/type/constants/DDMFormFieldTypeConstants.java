1268e0f2d5e85218d9742aa2cda29ac15c95ace2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-01-11 11:00:34 +0100

    LPS-125650 Creates new constants class with a constant for every field (ddm.form.field.type.name) that we have in dynamic-data-mapping-form-field-type, in that way, it is going to be more clear the new fields that we have in new data engine editor

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type-api/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/constants/DDMFormFieldTypeConstants.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.form.field.type.constants;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class DDMFormFieldTypeConstants {
+
+	public static final String CAPTCHA = "captcha";
+
+	public static final String CHECKBOX = "checkbox";
+
+	public static final String CHECKBOX_MULTIPLE = "checkbox_multiple";
+
+	public static final String COLOR = "color";
+
+	public static final String DATE = "date";
+
+	public static final String DOCUMENT_LIBRARY = "document_library";
+
+	public static final String FIELDSET = "fieldset";
+
+	public static final String GEOLOCATION = "geolocation";
+
+	public static final String GRID = "grid";
+
+	public static final String IMAGE = "image";
+
+	public static final String KEY_VALUE = "key_value";
+
+	public static final String LOCALIZABLE_TEXT = "localizable_text";
+
+	public static final String NUMERIC = "numeric";
+
+	public static final String OPTIONS = "options";
+
+	public static final String PARAGRAPH = "paragraph";
+
+	public static final String PASSWORD = "password";
+
+	public static final String RADIO = "radio";
+
+	public static final String RICH_TEXT = "rich_text";
+
+	public static final String SELECT = "select";
+
+	public static final String SEPARATOR = "separator";
+
+	public static final String TEXT = "text";
+
+	public static final String VALIDATION = "validation";
+
+}
