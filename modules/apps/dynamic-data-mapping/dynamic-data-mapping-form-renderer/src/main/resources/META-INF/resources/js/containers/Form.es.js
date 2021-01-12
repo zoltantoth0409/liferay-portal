0d3d4d6da8f3e10dafbf20d9ff95215b65c783a6
Jorge González <jorge.gonzalez@liferay.com>
2021-01-12 18:10:21 +0100

    LPS-123711 Only submit a liferayForm if doesn't have any errors

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/containers/Form.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/containers/Form.es.js
@@ -88,6 +88,18 @@ const Form = React.forwardRef(
 				validate()
 					.then((validForm) => {
 						if (validForm) {
+							const liferayForm =
+								event.target.id &&
+								Liferay.Form.get(event.target.id);
+
+							const validLiferayForm = !Object.keys(
+								liferayForm?.formValidator?.errors ?? {}
+							).length;
+
+							if (!validLiferayForm) {
+								return;
+							}
+
 							Liferay.Util.submitForm(event.target);
 
 							Liferay.fire('ddmFormSubmit', {
