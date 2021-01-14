bf767a846d2fd278e9e788f0591b5f9b6879c491
Jorge González <jorge.gonzalez@liferay.com>
2021-01-14 18:06:24 +0100

    LPS-124975 Check the add button in the item selector component

---
+++ modules/apps/document-library/document-library-video/test/DLVideoExternalShortcutURLItemSelectorView.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {waitForElementToBeRemoved} from '@testing-library/dom';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import DLVideoExternalShortcutURLItemSelectorView from '../src/main/resources/META-INF/resources/js/DLVideoExternalShortcutURLItemSelectorView';
+
+const defaultProps = {
+	eventName: 'eventName',
+	getDLVideoExternalShortcutFieldsURL: '/getDLVideoExternalShortcutFieldsURL',
+	namespace: 'namespace',
+	returnType: 'returnType',
+};
+
+const renderComponent = (props) =>
+	render(<DLVideoExternalShortcutURLItemSelectorView {...props} />);
+
+describe('DLVideoExternalShortcutURLItemSelectorView', () => {
+	afterEach(cleanup);
+
+	describe('when rendered with the default props', () => {
+		let result;
+
+		beforeEach(() => {
+			result = renderComponent(defaultProps);
+		});
+
+		it('has an add button disabled', () => {
+			const add = result.getByRole('button');
+
+			expect(add).toBeInTheDocument();
+			expect(add).toBeDisabled();
+		});
+	});
+
+	describe('when there is a valid server response', () => {
+		const responseFields = {
+			DESCRIPTION: 'DESCRIPTION',
+			HTML: '<iframe data-video-liferay></iframe>',
+			THUMBNAIL_URL: 'https://thumbnail-url',
+			TITLE: 'VIDEO TITLE',
+			URL: 'https://video-url.com',
+		};
+
+		let result;
+
+		beforeEach(async () => {
+			jest.useFakeTimers();
+
+			fetch.mockResponseOnce(JSON.stringify(responseFields));
+
+			result = renderComponent(defaultProps);
+			const {getByLabelText} = result;
+
+			fireEvent.change(getByLabelText('video-url'), {
+				target: {value: 'https://video-url.com'},
+			});
+
+			jest.advanceTimersByTime(500);
+
+			await waitForElementToBeRemoved(() =>
+				document.querySelector('span.loading-animation')
+			);
+		});
+
+		afterEach(() => {
+			jest.clearAllTimers();
+			jest.clearAllMocks();
+		});
+
+		afterAll(() => {
+			jest.useRealTimers();
+		});
+
+		it('has an add button enabled', () => {
+			const add = result.getByRole('button');
+
+			expect(add).toBeInTheDocument();
+			expect(add).toBeEnabled();
+		});
+	});
+});
