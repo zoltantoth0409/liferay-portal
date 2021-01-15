673d4ccac8d789776fccf463b7b0e91d30a34684
Jose Abelenda <jose.abelenda@liferay.com>
2021-01-15 10:55:51 -0300

    LPS-125684 - Code review

--- modules/apps/nested-portlets/nested-portlets-web/src/main/java/com/liferay/nested/portlets/web/internal/upgrade/NestedPortletWebUpgrade.java
+++ modules/apps/nested-portlets/nested-portlets-web/src/main/java/com/liferay/nested/portlets/web/internal/upgrade/NestedPortletWebUpgrade.java
@@ -30,7 +30,7 @@ public class NestedPortletWebUpgrade implements UpgradeStepRegistrator {
 
 	@Override
 	public void register(Registry registry) {
-		registry.register("0.0.0", "1.0.0", new DummyUpgradeStep());
+		registry.register("0.0.0", "1.0.1", new DummyUpgradeStep());
 
 		registry.register("0.0.1", "1.0.0", new UpgradePortletId());
 
