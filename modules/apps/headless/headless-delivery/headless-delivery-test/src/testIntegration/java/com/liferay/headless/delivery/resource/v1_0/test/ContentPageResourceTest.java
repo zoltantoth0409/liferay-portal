7af081036150976169fbbdf5d81faa33c323d413
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-01-15 08:43:45 +0100

    LPS-125568 Implement tests

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/ContentPageResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/ContentPageResourceTest.java
@@ -18,12 +18,14 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.headless.delivery.client.dto.v1_0.ContentPage;
 import com.liferay.layout.page.template.importer.LayoutPageTemplatesImporter;
 import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructureRel;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureRelLocalService;
 import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -31,10 +33,16 @@ import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.Inject;
+import com.liferay.segments.constants.SegmentsEntryConstants;
+import com.liferay.segments.model.SegmentsEntry;
+import com.liferay.segments.model.SegmentsExperience;
+import com.liferay.segments.service.SegmentsEntryLocalService;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
 
 import java.io.InputStream;
 
@@ -48,6 +56,29 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 public class ContentPageResourceTest extends BaseContentPageResourceTestCase {
+
+	@Override
+	@Test
+	public void testGetSiteContentPageExperienceExperienceKeyRenderedPage()
+		throws Exception {
+
+		Layout layout = _addLayout(testGroup.getGroupId(), true);
+
+		SegmentsExperience segmentsExperience = _addSegmentsExperience(
+			layout,
+			ServiceContextTestUtil.getServiceContext(testGroup.getGroupId()));
+
+		String friendlyURL = layout.getFriendlyURL();
+
+		String siteContentPageExperienceExperienceKeyRenderedPage =
+			contentPageResource.
+				getSiteContentPageExperienceExperienceKeyRenderedPage(
+					testGroup.getGroupId(), friendlyURL.substring(1),
+					segmentsExperience.getSegmentsExperienceKey());
+
+		Assert.assertNotNull(
+			siteContentPageExperienceExperienceKeyRenderedPage);
+	}
 
 	@Override
 	@Test
@@ -76,6 +107,14 @@ public class ContentPageResourceTest extends BaseContentPageResourceTestCase {
 
 	@Override
 	protected ContentPage testGetSiteContentPage_addContentPage()
+		throws Exception {
+
+		return _addContentPage();
+	}
+
+	@Override
+	protected ContentPage
+			testGetSiteContentPageExperienceExperienceKey_addContentPage()
 		throws Exception {
 
 		return _addContentPage();
@@ -176,6 +215,50 @@ public class ContentPageResourceTest extends BaseContentPageResourceTestCase {
 		return layout;
 	}
 
+	private SegmentsExperience _addSegmentsExperience(
+			Layout layout, ServiceContext serviceContext)
+		throws Exception {
+
+		SegmentsEntry segmentsEntry =
+			_segmentsEntryLocalService.addSegmentsEntry(
+				null,
+				HashMapBuilder.put(
+					LocaleUtil.getDefault(), RandomTestUtil.randomString()
+				).build(),
+				null, true, null, User.class.getName(), serviceContext);
+
+		SegmentsExperience segmentsExperience =
+			_segmentsExperienceLocalService.addSegmentsExperience(
+				segmentsEntry.getSegmentsEntryId(),
+				_portal.getClassNameId(Layout.class.getName()),
+				layout.getPlid(),
+				HashMapBuilder.put(
+					LocaleUtil.getDefault(), RandomTestUtil.randomString()
+				).build(),
+				true, serviceContext);
+
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				fetchLayoutPageTemplateStructure(
+					testGroup.getGroupId(), layout.getPlid());
+
+		LayoutPageTemplateStructureRel layoutPageTemplateStructureRel =
+			_layoutPageTemplateStructureRelLocalService.
+				fetchLayoutPageTemplateStructureRel(
+					layoutPageTemplateStructure.
+						getLayoutPageTemplateStructureId(),
+					SegmentsEntryConstants.ID_DEFAULT);
+
+		layoutPageTemplateStructureRel.setSegmentsExperienceId(
+			segmentsExperience.getSegmentsExperienceId());
+
+		_layoutPageTemplateStructureRelLocalService.
+			updateLayoutPageTemplateStructureRel(
+				layoutPageTemplateStructureRel);
+
+		return segmentsExperience;
+	}
+
 	private String _read(String fileName) throws Exception {
 		Class<?> clazz = getClass();
 
@@ -198,5 +281,11 @@ public class ContentPageResourceTest extends BaseContentPageResourceTestCase {
 
 	@Inject
 	private Portal _portal;
+
+	@Inject
+	private SegmentsEntryLocalService _segmentsEntryLocalService;
+
+	@Inject
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
 
 }
