a3137cec204c201f1e14432e19e50b83cbb7d8b6
David Arques <david.arques@liferay.com>
2021-01-18 19:01:48 +0100

    LPS-125847 Add duplicateExperience thunk function

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/thunks/duplicateExperience.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ExperienceService from '../../../app/services/ExperienceService';
+import duplicateExperienceAction from '../actions/duplicateExperience';
+import selectExperienceAction from '../actions/selectExperience';
+
+export default function duplicateExperience({segmentsExperienceId}) {
+	return (dispatch) => {
+		return ExperienceService.duplicateExperience({
+			body: {
+				segmentsExperienceId,
+			},
+			dispatch,
+		}).then(({fragmentEntryLinks, layoutData, segmentsExperience}) => {
+			ExperienceService.selectExperience({
+				body: {
+					segmentsExperienceId:
+						segmentsExperience.segmentsExperienceId,
+				},
+				dispatch,
+			})
+				.then((portletIds) => {
+					return dispatch(
+						selectExperienceAction({
+							portletIds,
+							segmentsExperienceId:
+								segmentsExperience.segmentsExperienceId,
+						})
+					);
+				})
+				.catch((error) => {
+					return error;
+				});
+
+			return dispatch(
+				duplicateExperienceAction({
+					fragmentEntryLinks,
+					layoutData,
+					segmentsExperience,
+				})
+			);
+		});
+	};
+}
