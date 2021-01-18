05a8e04ef7d0ffb3432daa3ea665b07a61e8a433
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2021-01-18 13:53:07 +0100

    LPS-126040 Adds new test to check if the fields are migrating correctly from old ddm structure to the new data engine structure

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/util/test/DMMDataDefinitionConverterTest.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.util.test;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.util.DDMDataDefinitionConverter;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+public class DMMDataDefinitionConverterTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_objectMapper = new ObjectMapper() {
+			{
+				configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+			}
+		};
+	}
+
+	@Test
+	public void testConvertAllFields() throws Exception {
+		String dataDefinition = _ddmDataDefinitionConverter.convert(
+			_getFileAsString(
+				"ddm-data-definition-json-converter-all-fields.json"),
+			LocaleUtil.US);
+
+		Assert.assertEquals(
+			_objectMapper.readTree(
+				_getFileAsString(
+					"ddm-data-definition-json-converter-all-fields-expected-" +
+						"result.json")),
+			_objectMapper.readTree(dataDefinition));
+	}
+
+	private String _getFileAsString(String fileName) throws Exception {
+		Class<?> clazz = getClass();
+
+		return StringUtil.read(
+			clazz.getClassLoader(),
+			"com/liferay/dynamic/data/mapping/util/test/dependencies/" +
+				fileName);
+	}
+
+	@Inject
+	private static DDMDataDefinitionConverter _ddmDataDefinitionConverter;
+
+	private ObjectMapper _objectMapper;
+
+}
