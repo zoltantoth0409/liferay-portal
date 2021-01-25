11b58bf78e89354a16927cd66bb064f6e3f4bda3
Adriano Interaminense <adriano.interaminense@gmail.com>
2021-01-25 20:48:36 -0300

    LPS-126734 SF

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/components/field-sets/FieldSets.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/components/field-sets/FieldSets.es.js
@@ -121,7 +121,7 @@ export default function FieldSets({keywords}) {
 		setState({
 			childrenAppProps: {
 				...childrenAppProps,
-				contentType: 'data-engine-fieldset'
+				contentType: 'data-engine-fieldset',
 			},
 			defaultLanguageId,
 			editingDataDefinition,
