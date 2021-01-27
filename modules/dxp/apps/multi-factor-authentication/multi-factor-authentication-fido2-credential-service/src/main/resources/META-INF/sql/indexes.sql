0251b754c852ac8ef898a7041e1c7e22c5e1f308
Alberto Chaparro <alberto.chaparro@liferay.com>
2021-01-27 11:41:34 +0100

    LPS-125897 Remove indexes manually since buildService does not do it for us

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-fido2-credential-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-fido2-credential-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,4 +1,2 @@
-create index IX_2B0CF873 on MFAFIDO2CredentialEntry (credentialKey[$COLUMN_LENGTH:2000000$]);
 create index IX_A95911A1 on MFAFIDO2CredentialEntry (credentialKeyHash);
-create unique index IX_4C5F79F9 on MFAFIDO2CredentialEntry (userId, credentialKey[$COLUMN_LENGTH:2000000$]);
 create unique index IX_F2E36027 on MFAFIDO2CredentialEntry (userId, credentialKeyHash);
