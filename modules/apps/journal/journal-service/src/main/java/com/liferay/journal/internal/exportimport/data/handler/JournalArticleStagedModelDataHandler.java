2eb9623dd9f656732bfcd7d3ca27cb2f4f77fada
Vendel Toreki <vendel.toreki@liferay.com>
2021-01-29 16:04:32 +0100

    LPS-124827 Reverting LPS-125882. The reason is that the data engine requires the Layout to be imported before the JournalArticle that references it.

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
@@ -1246,7 +1246,7 @@ public class JournalArticleStagedModelDataHandler
 	protected String[] getSkipImportReferenceStagedModelNames() {
 		return new String[] {
 			AssetDisplayPageEntry.class.getName(),
-			FriendlyURLEntry.class.getName(), Layout.class.getName()
+			FriendlyURLEntry.class.getName()
 		};
 	}
 
